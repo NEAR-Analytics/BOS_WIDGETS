a78c06cf67bc4e58ae56f325ab24849e19573db8
root.near <root.near>
2022-11-21 17:13:44 +0000

    Update IdeasList by root.near at 2022-11-21T17:13:44.836Z

--- root.near/IdeasList/source_code.jsx
+++ root.near/IdeasList/source_code.jsx
@@ -1,20 +1,14 @@
 const ideas = Near.view("nearideas.near", "get_ideas");
 
-if (ideas) {
-  ideas.sort((a, b) => {
-    return parseInt(b.amount) - parseInt(a.amount);
-  });
-}
+ideas.sort((a, b) => {
+  return parseInt(b.amount) - parseInt(a.amount);
+});
 
 console.log(ideas);
 
 return (
   <div>
-    <h1>
-      Use <a href="https://devgovgigs.near.social/">Dev Gov Gigs</a>
-      instead
-    </h1>
-    <h2>Below are outdated</h2>
+    <h2>Ideas</h2>
     <p>
       <a
         className="btn btn-outline-primary ms-2"
