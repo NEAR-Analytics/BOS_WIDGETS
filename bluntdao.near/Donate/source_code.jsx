1bfd21dab81955bd309e4d2e31e5a8ae124deecf
bluntdao.near <bluntdao.near>
2022-11-22 20:54:23 +0000

    Update Donate by bluntdao.near at 2022-11-22T20:54:23.482Z

--- bluntdao.near/Donate/source_code.jsx
+++ bluntdao.near/Donate/source_code.jsx
@@ -1,30 +1,11 @@
-// let amount;
-// amount = props.amount;
-// let yoctoConvert = amount / 1e24;
-let hardcode = 420000000000000000000000; // .6
-// let yoctoConvert = props.amount * 1e24;
-// let yoctoConvert = hardcode / 1e24;
-let yoctoConvert = hardcode * 10;
-let reciever = props.reciever || "blunt.sputnik-dao.near";
-
-initState({ amount: 1, reciever });
-
-// yoctoConvert = state.amount;
-
 const donate = () => {
-  let yoctoConvert1 = state.amount * 1e24;
   Near.call(
-    state.reciever,
+    "bluntdao.near",
     "donate",
     {},
     "30000000000000",
-    state.amount * 1e24
+    "500000000000000000000000"
   );
-};
-const onChangeAmount = (amount) => {
-  State.update({
-    amount,
-  });
 };
 
 return (
@@ -37,19 +18,6 @@ return (
         alt="BluntDAO Donate"
       />
     </div>
-    <input type="number" onChange={(e) => onChangeAmount(e.target.value)} />
-    <button
-      disabled={context.loading}
-      onClick={donate}
-      className={`btn ${context.loading ? "btn-outline-dark" : "btn-primary"}`}
-    >
-      Donate {state.amount} NEAR to Blunt Treausrey
-    </button>
-    <a
-      className="btn btn-outline-secondary"
-      href="https://app.astrodao.com/dao/blunt.sputnik-dao.near/"
-    >
-      Check the Treasurey
-    </a>
+    <button onClick={donate}>Donate 0.5 NEAR to bluntdao.near</button>
   </div>
 );
