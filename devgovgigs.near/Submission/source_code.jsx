3a40eb5bcd0c03689b76f1cbb05c2a35dc2be9e7
devgovgigs.near <devgovgigs.near>
2022-12-07 00:04:27 +0000

    Update Submission by devgovgigs.near at 2022-12-07T00:04:27.367Z

--- devgovgigs.near/Submission/source_code.jsx
+++ devgovgigs.near/Submission/source_code.jsx
@@ -1,10 +1,15 @@
 const submission = props.submission;
 
+function readableDate(UNIX_timestamp) {
+  var a = new Date(parseInt(UNIX_timestamp) / 1000000);
+  return a.toDateString() + " " + a.toLocaleTimeString();
+}
+
 return (
   <div>
     <h3>{submission.name}</h3>
     <p>Author: {submission.account_id}</p>
-    <p>Timestamp: {submission.timestamp} </p>
+    <p>Timestamp: {readableDate(submission.timestamp)} </p>
     <p>{submission.description}</p>
   </div>
 );
