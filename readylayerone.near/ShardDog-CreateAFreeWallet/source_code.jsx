b585012a5f2e497e454e4a3fcb58f120aedeb9d0
readylayerone.near <readylayerone.near>
2022-12-11 02:30:37 +0000

    Update ShardDog-CreateAFreeWallet by readylayerone.near at 2022-12-11T02:30:37.955Z

--- readylayerone.near/ShardDog-CreateAFreeWallet/source_code.jsx
+++ readylayerone.near/ShardDog-CreateAFreeWallet/source_code.jsx
@@ -2,7 +2,7 @@ return (
   <>
     <h3>Need a NEAR Account?</h3>
     <p>Claim your name instantly and for free with ShardDog</p>
-    <img src="https://shard.dog/img/ShardDog.jpg" width="400" height="400" />
+    <img src="https://readylayerone.s3.amazonaws.com/SharddogUp_sm.png" />
     <br />
     <br />
     <a href="https://shard.dog/go?url=https://near.social">
