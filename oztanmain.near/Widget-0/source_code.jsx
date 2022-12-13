efc9cd7491e1228f1a311238040ba2c4d767029f
oztanmain.near <oztanmain.near>
2022-12-13 20:21:16 +0000

    Update Widget-0 by oztanmain.near at 2022-12-13T20:21:16.133Z

--- oztanmain.near/Widget-0/source_code.jsx
+++ oztanmain.near/Widget-0/source_code.jsx
@@ -1,9 +1,1 @@
-const accountId = context.accountId;
-
-if (context.loading) {
-  return "Loading . . . ";
-} else if (!accountId) {
-  return "Sign-in with NEAR using the profile icon in the top right";
-} else {
-  return `Hello ${accountId}!`;
-}
+return <div>Hello World</div>;
