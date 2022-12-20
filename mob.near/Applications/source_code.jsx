0d0510d0a2d2cf22a70426ac05d9e6be0c734208
mob.near <mob.near>
2022-12-20 23:49:44 +0000

    Update Applications by mob.near at 2022-12-20T23:49:44.875Z

--- mob.near/Applications/source_code.jsx
+++ mob.near/Applications/source_code.jsx
@@ -1,51 +1,33 @@
 return (
   <div>
-    <h5>Applications</h5>
+    <h4>Applications</h4>
     <div className="mb-2">
       <Widget
-        loading={
-          <div className="input-group">
-            <input type="text" className="form-control" />
-          </div>
-        }
         src="mob.near/widget/ComponentSearch"
         props={{
-          boostedTag: "app",
+          filterTag: "app",
           placeholder: "🔍 Search Applications",
           limit: 10,
-          onChange: ({ result }) => {
-            State.update({ apps: result });
-          },
+          onChange: ({ result }) => State.update({ apps: result }),
         }}
       />
     </div>
-    {state.apps && (
+    {state.apps && state.apps.length > 0 && (
       <div className="mb-2">
-        {state.apps.map((app, i) => (
-          <div key={i}>
-            <Widget
-              src="mob.near/widget/ComponentSearch.Item"
-              props={{
-                link: `/${app.widgetSrc}`,
-                accountId: app.accountId,
-                widgetName: app.widgetName,
-                onHide: () => State.update({ apps: null }),
-                // extraButtons: ({ widgetPath }) => (
-                //   <a
-                //     target="_blank"
-                //     className="btn btn-outline-secondary"
-                //     href={`/mob.near/widget/WidgetSource?src=${widgetPath}`}
-                //   >
-                //     Source
-                //   </a>
-                // ),
-              }}
-            />
-          </div>
-        ))}
+        <div className="w-100 p-2 gap-2 d-flex flex-nowrap overflow-auto mb-3">
+          {state.apps.map((app, i) => (
+            <div key={i}>
+              <Widget
+                src="mob.near/widget/ApplicationCard"
+                props={{ accountId: app.accountId, widgetName: app.widgetName }}
+              />
+            </div>
+          ))}
+        </div>
+        <hr />
       </div>
     )}
-    <Widget src="mob.near/widget/StarredApps" props={{ limit: 24 }} />
+
     <Widget
       src="mob.near/widget/WidgetIcons"
       props={{ tag: "app", limit: 24 }}
