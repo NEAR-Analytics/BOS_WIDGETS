9a444dceaf4c8d7ca2dc9e4a861757b997cb28a1
mob.near <mob.near>
2022-12-20 22:48:21 +0000

    Update People by mob.near at 2022-12-20T22:48:21.684Z

--- mob.near/People/source_code.jsx
+++ mob.near/People/source_code.jsx
@@ -1,13 +1,8 @@
 return (
   <div>
-    <h5>People</h5>
+    <h4>People</h4>
     <div className="mb-2">
       <Widget
-        loading={
-          <div className="input-group">
-            <input type="text" className="form-control" />
-          </div>
-        }
         src="mob.near/widget/ProfileSearch"
         props={{
           limit: 10,
@@ -18,11 +13,8 @@ return (
     {state.profiles && state.profiles.length > 0 && (
       <div className="mb-2">
         {state.profiles.map(({ accountId }, i) => (
-          <div
-            key={accountId}
-            className="d-flex justify-content-between align-items-center mb-3"
-          >
-            <div className="me-2 text-truncate">
+          <div key={i} className="d-flex justify-content-between mb-3">
+            <div className="me-4 text-truncate">
               <a
                 href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
                 className="text-decoration-none link-dark text-truncate"
@@ -46,6 +38,6 @@ return (
       </div>
     )}
 
-    <Widget src="mob.near/widget/LastProfilesImages" props={{ limit: 24 }} />
+    <Widget src="mob.near/widget/LastProfilesImages" />
   </div>
 );
