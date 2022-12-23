3fdf5a5d484645be0e79071c973c9ef8855dbe2f
create.near <create.near>
2022-12-23 18:51:33 +0000

    Update GroupEditor by create.near at 2022-12-23T18:51:33.709Z

--- create.near/GroupEditor/source_code.jsx
+++ create.near/GroupEditor/source_code.jsx
@@ -18,19 +18,48 @@ return (
   <div className="row">
     <div className="col-lg-6">
       <div>
-        <h4>Edit Group</h4>
+        <h2>Edit Group</h2>
       </div>
       <div className="mb-2">
         <Widget
-          src="create.near/widget/MetadataEditor"
+          src="mob.near/widget/MetadataEditor"
           props={{
             initialMetadata: group,
-            onChange: (group) => State.update({ group }),
+            onChange: (profile) => State.update({ group }),
             options: {
-              name: { label: "Name" },
-              description: { label: "About" },
-              image: { label: "Image" },
-              leaders: { label: "Leaders" },
+              name: { label: "Group Name" },
+              image: { label: "Group Image" },
+              backgroundImage: { label: "Background Image" },
+              description: { label: "About Your Group" },
+              tags: {
+                label: "Group Tags",
+                tagsPattern: "*/group/tags/*",
+                placeholder: "near, category, topic, project, event",
+              },
+              linktree: {
+                links: [
+                  {
+                    label: "Twitter",
+                    prefix: "https://twitter.com/",
+                    name: "twitter",
+                  },
+                  {
+                    label: "Github",
+                    prefix: "https://github.com/",
+                    name: "github",
+                  },
+                  {
+                    label: "Telegram",
+                    prefix: "https://t.me/",
+                    name: "telegram",
+                  },
+                  {
+                    label: "Website",
+                    prefix: "https://",
+                    name: "website",
+                  },
+                ],
+              },
             },
           }}
         />
@@ -48,7 +77,7 @@ return (
     <div className="col-lg-6">
       <div>
         <Widget
-          src="create.near/widget/Group"
+          src="create.near/widget/Connect"
           props={{ accountId, group: state.group }}
         />
       </div>
