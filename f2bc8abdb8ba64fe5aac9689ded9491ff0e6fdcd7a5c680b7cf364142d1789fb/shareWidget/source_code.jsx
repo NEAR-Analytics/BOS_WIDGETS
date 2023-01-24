9adf1a3ec739e29a3d57f065f37721d7eb6918b0
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-01-24 20:42:05 +0000

    Update shareWidget by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-01-24T20:42:05.858Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/shareWidget/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/shareWidget/source_code.jsx
@@ -1,142 +1,98 @@
 let blockHeightToShare = props.blockHeight;
-let shareingWidget = props.shareingWidget ?? "pollQuestionMasterHandler";
-let propName = props.propName ?? "sharedBlockHeight";
-
-let popUpDescription =
-  props.popUpDescription ??
-  "Use this link to share the poll with your participants";
-
-let widgetOwner =
-  props.widgetOwner ??
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
-
-let thisWidgetInlineStyles = props.allWidgetsInlineStyles.shareWidget ?? {
-  i: { cursor: "pointer" },
-  showShareOptionsContainer: {
-    position: "absolute",
-    left: "1rem",
-    bottom: "1rem",
-    backgroundColor: "#FFFFFF",
-    border: "1.5px solid #F0F4F7",
-    boxShadow: "0px 8px 28px rgba(43, 68, 106, 0.05)",
-    borderRadius: "28px",
-    zIndex: "1",
-    width: "40vw",
-    maxWidth: "100%",
-    minWidth: "240px",
-    padding: "1rem",
-    border: "1.5px solid #F0F4F7",
-  },
-  closeIcon: { cursor: "pointer" },
-  popUpDescription: {
-    color: "#474D55",
-    letterSpacing: "-0.01em",
-  },
-  showLinkShared: {
-    backgroundColor: "#F2F6FA",
-    padding: "1rem 2rem",
-    borderRadius: "17px",
-  },
-  linkShared: { color: "#0065FF", wordWrap: "anywhere" },
-  clipboardContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    marginLeft: "0.5rem",
-    minWidth: "2.5rem",
-  },
-  clipBoardIconCopied: {
-    color: "#0065FF",
-    transition: "color 0.3s linear",
-    cursor: "pointer",
-  },
-  clipBoardIconNotCopied: {
-    transition: "color 0.3s linear",
-    cursor: "pointer",
-    color: "black",
-  },
-  copiedFeedback: { fontSize: "0.7rem" },
-};
-
-const thisWidgetClassNames = props.allWidgetsClassNames.shareWidget ?? {
-  i: "bi bi-share",
-  closePopUpContainer: "d-flex flex-row-reverse",
-  closeIcon: "bi bi-x",
-  showLinkShared: "d-flex justify-content-between align-items-center",
-  clipboardIcon: "bi-clipboard",
-  copiedFeedback: "text-secondary",
-};
 
 State.init({
   showShareOptions: false,
   linkCopied: false,
 });
 
-function getLink() {
-  if (
-    typeof blockHeightToShare === "number" ||
-    blockHeightToShare.length == 1
-  ) {
-    return `https://near.social/#/${widgetOwner}/widget/${shareingWidget}?${propName}=${blockHeightToShare}`;
-  } else {
-    let link = `https://near.social/#/${widgetOwner}/widget/${shareingWidget}?`;
-    for (let i = 0; i < blockHeightToShare.length; i++) {
-      if (i != 0) {
-        link += "&";
-      }
-      link += `${propName[i]}=${blockHeightToShare[i]}`;
-    }
-    return link;
-  }
-}
+let widgetOwner = "silkking.near";
 
 return (
   <>
     <i
-      style={thisWidgetInlineStyles.i}
-      className={thisWidgetClassNames.i}
+      style={{ cursor: "pointer" }}
+      className="bi bi-share"
       onClick={() => {
         State.update({ showShareOptions: !state.showShareOptions });
       }}
     ></i>
     {state.showShareOptions && (
-      <div style={thisWidgetInlineStyles.showShareOptionsContainer}>
-        <div className={thisWidgetClassNames.closePopUpContainer}>
+      <div
+        style={{
+          position: "absolute",
+          left: "1rem",
+          backgroundColor: "#FFFFFF",
+          border: "1.5px solid #F0F4F7",
+          boxShadow: "0px 8px 28px rgba(43, 68, 106, 0.05)",
+          borderRadius: "28px",
+          zIndex: "1",
+          width: "40vw",
+          padding: "1rem",
+          border: "1.5px solid #F0F4F7",
+        }}
+      >
+        <div className="d-flex flex-row-reverse">
           <i
-            className={thisWidgetClassNames.closeIcon}
-            style={thisWidgetInlineStyles.closeIcon}
+            className="bi bi-x"
+            style={{ cursor: "pointer" }}
             onClick={() =>
               State.update({ showShareOptions: false, linkCopied: false })
             }
           ></i>
         </div>
         <h3>Share</h3>
-        <p style={thisWidgetInlineStyles.popUpDescription}>
-          {popUpDescription}
+        <p
+          style={{
+            color: "#474D55",
+            letterSpacing: "-0.01em",
+          }}
+        >
+          Use this link to share the poll with your participants
         </p>
         <div
-          className={thisWidgetClassNames.showLinkShared}
-          style={thisWidgetInlineStyles.showLinkShared}
+          className="d-flex justify-content-between align-items-center"
+          style={{
+            backgroundColor: "#F2F6FA",
+            padding: "1rem 2rem",
+            borderRadius: "17px",
+          }}
         >
-          <span style={thisWidgetInlineStyles.linkShared}>{getLink()}</span>
-          <div style={thisWidgetInlineStyles.clipboardContainer}>
+          <span
+            style={{ color: "#0065FF", wordWrap: "anywhere" }}
+          >{`https://near.social/#/${widgetOwner}/widget/pollQuestionMasterHandler?sharedBlockHeight=${blockHeightToShare}`}</span>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              marginLeft: "0.5rem",
+              minWidth: "2.5rem",
+            }}
+          >
             <i
-              className={thisWidgetClassNames.clipboardIcon}
+              className="bi-clipboard"
               style={
                 state.linkCopied
-                  ? thisWidgetInlineStyles.clipBoardIconCopied
-                  : thisWidgetInlineStyles.clipBoardIconNotCopied
+                  ? {
+                      color: "#0065FF",
+                      transition: "color 0.3s linear",
+                      cursor: "pointer",
+                    }
+                  : {
+                      transition: "color 0.3s linear",
+                      cursor: "pointer",
+                      color: "black",
+                    }
               }
               onClick={() => {
-                clipboard.writeText(getLink());
+                clipboard.writeText(
+                  `https://near.social/#/${widgetOwner}/widget/pollQuestionMasterHandler?sharedBlockHeight=${blockHeightToShare}`
+                );
                 State.update({ linkCopied: true });
               }}
             ></i>
             {state.linkCopied && (
-              <span
-                className={thisWidgetClassNames.copiedFeedback}
-                style={thisWidgetInlineStyles.copiedFeedback}
-              >
+              <span className="text-secondary" style={{ fontSize: "0.7rem" }}>
                 Copied!
               </span>
             )}
