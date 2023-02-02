babee2367de29d85e33fa4511ddfe655a5b1ffcc
events_v2.near <events_v2.near>
2023-02-02 15:59:33 +0000

    Update events__index__list by events_v2.near at 2023-02-02T15:59:33.223Z

--- events_v2.near/events__index__list/source_code.jsx
+++ events_v2.near/events__index__list/source_code.jsx
@@ -1,31 +1,59 @@
-const ANIMATION_DELAY = 100;
-
 let events = props.events || [];
 if (!events) {
-  return props.__engine.loading();
+  return '';
 }
 
 // if events are empty we want to show an empty list message
 if (events.length === 0) {
-  return props.__engine.loading();
+  return 'No events found';
 }
 
-const SlideInLeft = props.__engine.Components.SlideInLeft;
-const GridContainer = props.__engine.Components.GridContainer;
+const IndexList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+  justify-content: flex-start;
+
+  width: auto;
+  margin-left: -20px;
+  margin-right: -20px;
+
+  & > * {
+    margin: 20px 20px;
+    max-width: 520px;
+    min-width: 320px;
+    width: 240px;
+    flex-grow: 3;
+    flex-shrink: 3;
+  }
+`;
+
+const Pulse = styled.keyframes`
+  0% {
+    transform: scale(0.99);
+  }
+  70% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0.95);
+  }
+`;
 
 return (
-  <GridContainer itemWidth={'380px'}>
-    {events.map((event, idx) => {
-      const delay = `${(idx + 2) * ANIMATION_DELAY}ms`;
+  <IndexList>
+    {events.map((event) => {
       return (
-        <SlideInLeft key={event.event_id} delay={delay}>
-          {props.__engine.renderComponent('index.list_item', { event, delay })}
-        </SlideInLeft>
-      );
+        <div>
+          {props.__engine.renderComponent('index.list_item', {
+            event,
+            key: event.event_id,
+          })}
+        </div>
+      }</div>;
     })}
-
     <div>{/* spacer */}</div>
     <div>{/* spacer */}</div>
     <div>{/* spacer */}</div>
-  </GridContainer>
+  </IndexList>
 );
