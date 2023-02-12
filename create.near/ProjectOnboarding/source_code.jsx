25ab2bd02e399f724cceab13734a11ed6193b061
create.near <create.near>
2023-02-12 22:29:14 +0000

    Update ProjectOnboarding by create.near at 2023-02-12T22:29:14.452Z

--- create.near/ProjectOnboarding/source_code.jsx
+++ create.near/ProjectOnboarding/source_code.jsx
@@ -31,8 +31,8 @@ const editProjectButton = (
   <div>
     <h2>Welcome to Near Social!</h2>
     <p>
-      Everything about this platform is customizable, including this Project
-      Page, made for onboarding and ecosystem integrations.
+      Everything about this platform is customizable, including your specific
+      Project Page, made for onboarding and ecosystem integrations.
     </p>
     <Widget src="create.near/widget/ProjectEditor" />
   </div>
