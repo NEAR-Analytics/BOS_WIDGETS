21b68939066dc2803a6ed1561001da2239aa1b2c
create.near <create.near>
2023-02-12 18:37:37 +0000

    Update ProjectEditor by create.near at 2023-02-12T18:37:37.554Z

--- create.near/ProjectEditor/source_code.jsx
+++ create.near/ProjectEditor/source_code.jsx
@@ -12,9 +12,6 @@ State.init({
 
 return (
   <div className="row">
-    <div>
-      <h4>Edit Project</h4>
-    </div>
     <div className="mb-2">
       <Widget
         src="gov.near/widget/MetadataEditor"
@@ -22,41 +19,8 @@ return (
           initialMetadata: project,
           onChange: (project) => State.update({ project }),
           options: {
-            name: { label: "Project Name" },
             featuredWidget: {
               label: "Project Source: <accountId>.near/widget/<WidgetName>",
-            },
-            image: { label: "Widget Logo" },
-            backgroundImage: { label: "Background Image" },
-            description: { label: "About the Widget" },
-            tags: {
-              label: "Tags",
-              tagsPattern: "*/project/tags/*",
-              placeholder: "near, dev, art, edu, nft, defi, gov",
-            },
-            linktree: {
-              links: [
-                {
-                  label: "Twitter",
-                  prefix: "https://twitter.com/",
-                  name: "twitter",
-                },
-                {
-                  label: "Github",
-                  prefix: "https://github.com/",
-                  name: "github",
-                },
-                {
-                  label: "Telegram",
-                  prefix: "https://t.me/",
-                  name: "telegram",
-                },
-                {
-                  label: "Website",
-                  prefix: "https://",
-                  name: "website",
-                },
-              ],
             },
           },
         }}
