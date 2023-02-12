6a8fddab949376fa82fe56c7c46197a7673ccf29
create.near <create.near>
2023-02-12 15:38:05 +0000

    Update Onboarding.GetInvolved by create.near at 2023-02-12T15:38:05.507Z

---
+++ create.near/Onboarding.GetInvolved/source_code.jsx
@@ -0,0 +1,45 @@
+return (
+  <div>
+    <h5>Start Building on NEAR</h5>
+    <div className="mb-2 d-flex gap-2 flex-wrap">
+      <a className="btn btn-outline-primary" href="https://devs.near.social">
+        Registration
+      </a>
+      <a className="btn btn-outline-primary" href="">
+        Documentation
+      </a>
+    </div>
+    <div className="mb-2 d-flex gap-2 flex-wrap">
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="#/mob.near/widget/ProfilePage?accountId=self.social.near"
+      >
+        <i className="bi bi-person-circle"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://t.me/neardev"
+      >
+        <i className="bi bi-telegram"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://github.com/near"
+      >
+        <i className="bi bi-github"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://twitter.com/nearprotocol"
+      >
+        <i className="bi bi-twitter"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://thewiki.near.page"
+      >
+        <i className="bi bi-wikipedia"></i>
+      </a>
+    </div>
+  </div>
+);
