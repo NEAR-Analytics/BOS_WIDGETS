f266578c477dee2d5220567bad1905c0e41ce189
create.near <create.near>
2023-02-12 20:06:00 +0000

    Update PageTabs by create.near at 2023-02-12T20:06:00.814Z

--- create.near/PageTabs/source_code.jsx
+++ create.near/PageTabs/source_code.jsx
@@ -17,52 +17,20 @@ const showEditButton =
 
 return (
   <>
-    <ul className="nav nav-pills nav-fill mb-4" id="pills-tab" role="tablist">
-      <li className="nav-item" role="presentation">
-        <button
-          className="nav-link active"
-          id="pills-widget-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#pills-widget"
-          type="button"
-          role="tab"
-          aria-controls="pills-widget"
-          aria-selected="true"
-        >
-          Featured Widget
-        </button>
-      </li>
-      <li className="nav-item" role="presentation">
-        <button
-          className="nav-link"
-          id="pills-comments-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#pills-comments"
-          type="button"
-          role="tab"
-          aria-controls="pills-comments"
-          aria-selected="false"
-        >
-          Discussion
-        </button>
-      </li>
-    </ul>
-    <div className="tab-content" id="pills-tabContent">
-      <div
-        className="tab-pane fade in show active"
-        id="pills-widget"
-        role="tabpanel"
-        aria-labelledby="pills-widget-tab"
-      >
+    <div className="mt-3">
+      <Widget
+        src="create.near/widget/ProjectEditor"
+        props={{
+          accountId,
+          project,
+          link: true,
+          showEditButton: !props.project,
+        }}
+      />
+    </div>
+    <div className="mt-3">
+      <div>
         <Widget src={featuredWidget} />
-      </div>
-      <div
-        className="tab-pane fade comments"
-        id="pills-comments"
-        role="tabpanel"
-        aria-labelledby="pills-comments-tab"
-      >
-        <Widget src="mob.near/widget/MainPage.Content" props={{ accountId }} />
       </div>
     </div>
   </>
