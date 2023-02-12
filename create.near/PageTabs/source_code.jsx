88631ad3c1f33207e4c56e1a92816754ceb55182
create.near <create.near>
2023-02-12 20:13:58 +0000

    Update PageTabs by create.near at 2023-02-12T20:13:58.065Z

--- create.near/PageTabs/source_code.jsx
+++ create.near/PageTabs/source_code.jsx
@@ -2,8 +2,7 @@ const accountId = props.accountId ?? context.accountId;
 
 const project = props.project ?? Social.getr(`${accountId}/project`);
 
-const featuredWidget =
-  project.featuredWidget || "create.near/widget/ProjectEditor";
+const featuredWidget = project.featuredWidget || "create.near/widget/Builders";
 
 if (project === null) {
   return { showEditButton };
