b60de6bb21b84c51ff8878bddf058c3d184b7e57
create.near <create.near>
2023-02-12 19:25:49 +0000

    Update Builders by create.near at 2023-02-12T19:25:49.797Z

--- create.near/Builders/source_code.jsx
+++ create.near/Builders/source_code.jsx
@@ -5,7 +5,7 @@ return (
     <div className="row mb-3">
       <div>
         <h4>Activating Near Social Builders</h4>
-        <p>Step 1: Create a project page!</p>
+        <p>Step 1: Create a project page! #hackathon</p>
         <div className="mb-3"></div>
         <button href="https://devs.near.social">Example</button>
         <button href="https://hack.near.social">Guide</button>
