e0da29fa39a04fead60620fc0d561e29c5a44334
create.near <create.near>
2023-02-12 20:38:26 +0000

    Update PageTabs by create.near at 2023-02-12T20:38:26.871Z

--- create.near/PageTabs/source_code.jsx
+++ create.near/PageTabs/source_code.jsx
@@ -5,28 +5,11 @@ const project = props.project ?? Social.getr(`${accountId}/project`);
 const featuredWidget = project.featuredWidget || "create.near/widget/Builders";
 
 if (project === null) {
-  return { showEditButton };
+  return "Loading...";
 }
-
-const showEditButton =
-  project !== undefined &&
-  (!props.project || props.showEditButton) &&
-  accountId &&
-  accountId === context.accountId;
 
 return (
   <>
-    <div className="mt-3">
-      <Widget
-        src="create.near/widget/ProjectEditor"
-        props={{
-          accountId,
-          project,
-          link: true,
-          showEditButton: !props.project,
-        }}
-      />
-    </div>
     <div className="mt-3">
       <div>
         <Widget src={featuredWidget} />
