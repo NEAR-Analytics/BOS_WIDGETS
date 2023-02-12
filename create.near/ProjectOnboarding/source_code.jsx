bb0e414add32038db732d0677c45f2f6b5cee7ac
create.near <create.near>
2023-02-12 20:53:42 +0000

    Update ProjectOnboarding by create.near at 2023-02-12T20:53:42.161Z

--- create.near/ProjectOnboarding/source_code.jsx
+++ create.near/ProjectOnboarding/source_code.jsx
@@ -31,9 +31,10 @@ const editProjectButton = (
   <div>
     <h2>Welcome to Near Social!</h2>
     <p>
-      Everything about this platform is customizable, including your very own
-      project page! Click the button below to edit, and be sure to include a
-      Featured Widget.
+      Everything about this open web platform is customizable, including your
+      very own project page, designed to facilitate Near Social onboarding,
+      collaboration, and widget integrations. Click the button below to edit,
+      and be sure to include a Featured Widget.
     </p>
     <a className="btn btn-success" href="#/create.near/widget/PageEditor">
       Edit Your Project Page
