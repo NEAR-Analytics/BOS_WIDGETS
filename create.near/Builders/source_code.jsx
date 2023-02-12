1f4d7bd0c24dc2b75d1dc673f9ddfa253360d6d6
create.near <create.near>
2023-02-12 21:47:00 +0000

    Update Builders by create.near at 2023-02-12T21:47:00.939Z

--- create.near/Builders/source_code.jsx
+++ create.near/Builders/source_code.jsx
@@ -5,11 +5,62 @@ return (
     <div className="row mb-3">
       <div>
         <h4>Activating Near Social Development</h4>
-        <p>Step 1: Create a project page! #hackathon</p>
+        <p>Create a project page for the Near Social hackathon!</p>
         <div className="mb-3"></div>
-        <button href="https://devs.near.social">Example</button>
-        <button href="https://hack.near.social">Guide</button>
-        <button href="https://nearbuilders.com">Context</button>
+        <button href="https://near.social/#/create.near/widget/Onboarding">
+          Example
+        </button>
+        <button href="https://devgovgigs.near.social">Ideas</button>
+        <button href="https://nearbuilders.com">More Info</button>
+      </div>
+    </div>
+    <div className="row mb-3">
+      <div>
+        <h1>Guide for Builders</h1>
+        <p>
+          Together, open web devs are creating the future of decentralized
+          applications. We coordinate to build meaningful solutions for
+          everyone!
+        </p>
+        <h3>Why Participate?</h3>
+        <h4>Learn, Collaborate, Grow</h4>
+        <p>
+          Are you looking for opportunities to explore groundbreaking
+          technologies, connect with collaborators, and experiment with on-chain
+          frontend tools? Near Social hackathons provide a welcoming environment
+          for anyone to delve further into this open-source development platform
+          and the underlying data protocol.
+        </p>
+        <p>
+          Overall, the mission is to activate and support a growing community of
+          JavaScript programmers in the NEAR ecosystem.
+        </p>
+        <h2>How To Participate</h2>
+        <h3>1. EXPLORE IDEAS</h3>
+        <p>
+          Share potential project plans via the{" "}
+          <a href="https://devgovgigs.near.social">Gigs Board</a>. Use the
+          #hackathon tag, so your post will show up here. Add other tags to
+          facilitate discovery!
+        </p>
+        <h3> 2. FORM TEAMS</h3>
+        <p>
+          Review ideas on the hackathon ideas board to discover projects and
+          find collaborators. Reply to discuss plans with potential teammates.
+          Once you have agreed to work as a team, complete the following steps
+          to introduce your project:
+        </p>
+        <p>
+          First, create another NEAR account and make a profile for the
+          hackathon project. Then, follow each member of the team, and they can
+          follow back to indicate mutual support. Finally, be sure to save your
+          widget(s) to this profile.
+        </p>
+        <h3> 3. SUBMIT PROJECTS</h3>
+        <div className="mb-3"></div>
+        <button href="https://create.near.social">
+          Create Project Submission
+        </button>
       </div>
     </div>
   </div>
