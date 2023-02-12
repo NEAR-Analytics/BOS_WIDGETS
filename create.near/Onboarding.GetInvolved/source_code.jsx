08be7d5ad05b3b1ae5a40b9fa3f7a0c9f02a837b
create.near <create.near>
2023-02-12 15:51:47 +0000

    Update Onboarding.GetInvolved by create.near at 2023-02-12T15:51:47.657Z

--- create.near/Onboarding.GetInvolved/source_code.jsx
+++ create.near/Onboarding.GetInvolved/source_code.jsx
@@ -1,18 +1,18 @@
 return (
   <div>
-    <h5>Start Building on NEAR</h5>
+    <h5> Connect with NEAR Builders</h5>
     <div className="mb-2 d-flex gap-2 flex-wrap">
       <a className="btn btn-outline-primary" href="https://devs.near.social">
         Registration
       </a>
-      <a className="btn btn-outline-primary" href="">
+      <a className="btn btn-outline-primary" href="https://hack.near.social">
         Documentation
       </a>
     </div>
     <div className="mb-2 d-flex gap-2 flex-wrap">
       <a
         className="btn btn-outline-secondary border-0"
-        href="#/mob.near/widget/ProfilePage?accountId=self.social.near"
+        href="#/mob.near/widget/ProfilePage?accountId=devs.near"
       >
         <i className="bi bi-person-circle"></i>
       </a>
@@ -24,15 +24,15 @@ return (
       </a>
       <a
         className="btn btn-outline-secondary border-0"
-        href="https://github.com/near"
+        href="https://github.com/nearbuilders"
       >
         <i className="bi bi-github"></i>
       </a>
       <a
         className="btn btn-outline-secondary border-0"
-        href="https://twitter.com/nearprotocol"
+        href="https://www.nearbuilders.com"
       >
-        <i className="bi bi-twitter"></i>
+        <i className="bi bi-globe"></i>
       </a>
       <a
         className="btn btn-outline-secondary border-0"
