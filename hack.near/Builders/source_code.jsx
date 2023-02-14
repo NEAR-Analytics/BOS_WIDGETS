a2898f4719c039b04dd6567069684af78379b6e1
hack.near <hack.near>
2023-02-14 12:43:28 +0000

    Update Builders by hack.near at 2023-02-14T12:43:28.663Z

--- hack.near/Builders/source_code.jsx
+++ hack.near/Builders/source_code.jsx
@@ -1,139 +1,85 @@
-const data = Social.keys("*/widget", "final", {
-  return_type: "BlockHeight",
-});
-
-if (!data) {
-  return "Loading...";
-}
-
-State.init({
-  when: 102345888,
-  inverse: false,
-});
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  a {
-    width: 3em;
-    height: 3em;
-    display: inline-block;
-    overflow: hidden;
-
-    > img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  }
-`;
-
-const builders = new Set([...Object.keys(data)]);
-
-const accounts = Object.keys(data)
-  .filter((accountId) => builders.has(accountId))
-  .filter((accountId) => data[accountId].widget > state.when)
-  .map((accountId) => (
-    <a
-      title={accountId}
-      href={`/mob.near/widget/ProfilePage?accountId=${accountId}`}
-    >
-      <img
-        loading="lazy"
-        src={`https://i.near.social/magic/thumbnail/https://near.social/magic/img/account/${accountId}`}
-        alt={accountId}
-      />
-    </a>
-  ));
-
-const accountsInverse = Object.keys(data)
-  .filter((accountId) => builders.has(accountId))
-  .filter((accountId) => data[accountId].widget < state.when)
-  .map((accountId) => (
-    <a
-      title={accountId}
-      href={`/mob.near/widget/ProfilePage?accountId=${accountId}`}
-    >
-      <img
-        loading="lazy"
-        src={`https://i.near.social/magic/thumbnail/https://near.social/magic/img/account/${accountId}`}
-        alt={accountId}
-      />
-    </a>
-  ));
-
-let blockHeight = state.when;
-
-const block = Near.block(blockHeight);
-
-const timestampNanosec = block.header.timestamp_nanosec;
-
-const timeMs = parseFloat(timestampNanosec) / 1e6;
-const date = new Date(timeMs);
-
 return (
-  <Wrapper>
-    {state.inverse ? (
-      <div className="m-3">
-        <div className="row align-items-center">
-          <div className="col-5">
-            <h4>{accountsInverse.length} Active Builders</h4>
-            <p>
-              <i class="bi bi-clock-history px-2"></i>
-              before {block && <b>{date.toDateString()}</b>}
-            </p>
-          </div>
-          <div className="col-5 mb-2">
-            <h5>blockHeight:</h5>
-            <input
-              type="number"
-              value={state.when}
-              onChange={(e) => State.update({ when: e.target.value })}
-            />
-          </div>
-          <div className="col-2">
-            <button
-              className="btn btn-primary mt-4"
-              onClick={() => State.update({ inverse: !state.inverse })}
-            >
-              {state.inverse ? "After" : "Before"}
-            </button>
-          </div>
-        </div>
-        <hr />
-        <div>{accountsInverse}</div>
-      </div>
-    ) : (
-      <div className="m-3">
-        <div className="row align-items-center">
-          <div className="col-5">
-            <h4>{accounts.length} Active Builders</h4>
-            <p>
-              <i class="bi bi-clock-history px-2"></i>
-              after {block && <b>{date.toDateString()}</b>}
-            </p>
-          </div>
-          <div className="col-5 mb-2">
-            <h5>blockHeight:</h5>
-            <input
-              type="number"
-              value={state.when}
-              onChange={(e) => State.update({ when: e.target.value })}
-            />
-          </div>
-          <div className="col-2">
-            <button
-              className="btn btn-primary mt-4"
-              onClick={() => State.update({ inverse: !state.inverse })}
-            >
-              {state.inverse ? "After" : "Before"}
-            </button>
-          </div>
-        </div>
+  <div className="container">
+    <div className="row mb-3"></div>
 
-        <hr />
-        <div>{accounts}</div>
+    <div className="row mb-3">
+      <div>
+        <h1>Near Social Hackathon Guide</h1>
+        <p>
+          Submit your project by creating a custom page with a featured widget!
+        </p>
+        <div className="mb-3"></div> <div className="mb-3"></div>
+        <a
+          className="btn btn-outline-primary"
+          href="https://near.social/#/create.near/widget/Page?accountId=create.near"
+        >
+          Example
+        </a>
+        <a
+          className="btn btn-outline-primary"
+          href="https://devgovgigs.near.social"
+        >
+          Open Discussion
+        </a>
+        <a className="btn btn-outline-primary" href="https://nearbuilders.com">
+          Learn More
+        </a>
       </div>
-    )}
-  </Wrapper>
+    </div>
+    <div className="row mb-3">
+      <div>
+        <h2>Guide for Builders</h2>
+        <p>
+          Together, open web devs are creating the future of decentralized
+          applications. We coordinate to build meaningful solutions for
+          everyone!
+        </p>
+        <h3>Why Participate?</h3>
+        <h4>Learn, Collaborate, Grow</h4>
+        <p>
+          Are you looking for opportunities to explore groundbreaking
+          technologies, connect with collaborators, and experiment with on-chain
+          frontend tools?
+        </p>
+        <p>
+          Near Social hackathons provide a welcoming environment for anyone to
+          delve further into this open-source development platform and the
+          underlying data protocol.
+        </p>
+        <p>
+          Overall, the mission is to activate and support a growing community of
+          JavaScript programmers in the NEAR ecosystem.
+        </p>
+        <h2>How To Participate</h2>
+        <h3>1. EXPLORE IDEAS</h3>
+        <p>
+          Share potential project plans via the{" "}
+          <a href="https://devgovgigs.near.social">Gigs Board</a>. Use the
+          #hackathon tag, so your post will show up here. Add other tags to
+          facilitate discovery!
+        </p>
+        <h3> 2. FORM TEAMS</h3>
+        <p>
+          Review ideas on the hackathon ideas board to discover projects and
+          find collaborators. Reply to discuss plans with potential teammates.
+          Once you have agreed to work as a team, complete the final steps to
+          submit your project.
+        </p>
+        <h3> 3. SUBMIT PROJECTS</h3>
+        <p>
+          First, create a project-specific NEAR account and make a profile.
+          Then, follow each member of the team, and they can follow back to
+          indicate mutual support. Finally, be sure to save your widget(s) to
+          this profile.
+        </p>
+        <div className="mb-3"></div>
+        <a
+          className="btn btn-primary"
+          href="https://near.social/#/create.near/widget/PageEditor"
+        >
+          Create Project Page
+        </a>
+      </div>
+    </div>
+  </div>
 );
