cf88591df5526b7c310202bd7f5be3b7b19b97b7
adminalpha.near <adminalpha.near>
2023-03-15 20:55:14 +0000

    Update adminalpha.near.ModalTest by adminalpha.near at 2023-03-15T20:55:14.074Z

--- adminalpha.near/adminalpha.near.ModalTest/source_code.jsx
+++ adminalpha.near/adminalpha.near.ModalTest/source_code.jsx
@@ -1,5 +1,1 @@
-const agreementsForUser = Social.index("tosAccept", "kgrbg", {
-  accountId: context.accountId, // make sure it was written by the user in question test
-});
 
-return <>{JSON.stringify(agreementsForUser)}</>;
