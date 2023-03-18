3a96f06bfadda3e23c589cc963ec877583fc5206
hack.near <hack.near>
2023-03-18 19:50:50 +0000

    Update Commons by hack.near at 2023-03-18T19:50:50.936Z

--- hack.near/commons/source_code.jsx
+++ hack.near/commons/source_code.jsx
@@ -1,98 +1,73 @@
-const accountId = props.accountId ?? context.accountId ?? "every.near";
-const graphId = props.graphId ?? "commons";
+const composeData = () => {
+  const data = {
+    post: {
+      main: JSON.stringify(state.content),
+    },
+    index: {
+      post: JSON.stringify({
+        key: "main",
+        value: {
+          type: "md",
+        },
+      }),
+    },
+  };
 
-const attestations = Social.keys(`*/graph/${graphId}/*`, "final", {
-  values_only: true,
-});
-
-if (!attestations) {
-  return "";
-}
-
-const [matrix, setMatrix] = useState([]);
-const [attestors, setAttestors] = useState([]);
-const [builders, setBuilders] = useState([]);
-
-useEffect(() => {
-  const attestorSet = new Set();
-  const builderSet = new Set();
-
-  Object.entries(attestations).forEach(([attestor, data]) => {
-    attestorSet.add(attestor);
-    Object.keys(data.graph[graphId]).forEach((builder) => {
-      builderSet.add(builder);
-    });
+  const notifications = state.extractTagNotifications(state.content.text, {
+    type: "social",
+    path: `${context.accountId}/post/main`,
   });
 
-  setAttestors(Array.from(attestorSet));
-  setBuilders(Array.from(builderSet));
+  if (notifications.length) {
+    data.index.notify = JSON.stringify(
+      notifications.length > 1 ? notifications : notifications[0]
+    );
+  }
 
-  const newMatrix = Array.from(builderSet).map((builder) =>
-    Array.from(attestorSet).map((attestor) => ({
-      attestorId: attestor,
-      builderId: builder,
-    }))
-  );
+  return data;
+};
 
-  setMatrix(newMatrix);
-}, []);
+State.init({
+  onChange: ({ content }) => {
+    State.update({ content });
+  },
+});
 
 return (
-  <div className="m-2 p-1">
-    <div className="m-1">
-      <Widget src="hack.near/widget/commons.add" />
+  <div className="container">
+    <Widget src="mob.near/widget/ProfileOnboarding" />
+    <div className="row mb-3">
+      <div className="mb-3"></div>
+      <div>
+        <h1>Evolving Guides for Builders</h1>
+        <p>
+          Let's collaborate to gather knowledge and improve common resources for
+          open web developers. Choose your own adventure to discover what is
+          possible! #NEAR
+        </p>
+        <div className="mb-3"></div>
+        <a
+          className="btn btn-primary"
+          href="https://near.social/#/hack.near/widget/Dev"
+        >
+          Widgets
+        </a>
+        <a
+          className="btn btn-primary"
+          href="https://near.social/#/hack.near/widget/Docs"
+        >
+          APIs
+        </a>
+        <a
+          className="btn btn-primary"
+          href="https://near.social/#/hack.near/widget/Data"
+        >
+          Data
+        </a>
+      </div>
+      <div className="row mt-3">
+        <Widget src="hack.near/widget/Common.Component.Library" />
+      </div>
     </div>
-    <table style={{ width: "100%" }}>
-      <thead>
-        <tr>
-          <th style={{ padding: "8px" }}>
-            {context.accountId && (
-              <Widget
-                src="hack.near/widget/commons.join"
-                props={{
-                  accountId: "*",
-                  attestorId: accountId,
-                }}
-              />
-            )}
-          </th>
-          {attestors.map((attestor, index) => (
-            <th key={index} style={{ padding: "8px" }}>
-              {attestor}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {matrix.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <td style={{ padding: "8px" }}>
-              <b>{builders[rowIndex]}</b>
-            </td>
-            {row.map((cell, cellIndex) => (
-              <td style={{ padding: "8px" }} key={cellIndex}>
-                {accountId !== cell.attestorId ? (
-                  <Widget
-                    src="hack.near/widget/BuilderBadge"
-                    props={{
-                      accountId: cell.builderId,
-                      attestorId: cell.attestorId,
-                    }}
-                  />
-                ) : (
-                  <Widget
-                    src="hack.near/widget/attest"
-                    props={{
-                      accountId: cell.builderId,
-                      attestorId: cell.attestorId,
-                    }}
-                  />
-                )}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
   </div>
 );
