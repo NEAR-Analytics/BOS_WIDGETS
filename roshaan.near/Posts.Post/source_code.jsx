a4995858382fa45f52ab950ff2d5ca04e878ac70
roshaan.near <roshaan.near>
2023-03-21 16:21:02 +0000

    Update Posts.Post by roshaan.near at 2023-03-21T16:21:02.650Z

--- roshaan.near/Posts.Post/source_code.jsx
+++ roshaan.near/Posts.Post/source_code.jsx
@@ -1,92 +1,19 @@
 const accountId = props.accountId;
 const blockHeight =
   props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
-// const subscribe = !!props.subscribe;
+const subscribe = !!props.subscribe;
 const notifyAccountId = accountId;
-const postUrl = `https://alpha.near.org/#/roshaan.near/widget/PostPage?accountId=${accountId}&blockHeight=${blockHeight}`;
+const postUrl = `https://alpha.near.org/#/calebjacob.near/widget/PostPage?accountId=${accountId}&blockHeight=${blockHeight}`;
 
-State.init({
-  postExists: true,
-  comments: props.comments ?? undefined,
-  content: JSON.parse(props.content) ?? undefined,
-  likes: props.likes ?? undefined,
-});
+const content =
+  props.content ??
+  JSON.parse(Social.get(`${accountId}/post/main`, blockHeight) ?? "null");
 
 const item = {
   type: "social",
   path: `${accountId}/post/main`,
   blockHeight,
 };
-
-// Load post if not contents and comments are not passed in
-if (!state.content || !state.comments || !state.likes) {
-  console.log("making call again");
-  const postsQuery = `
-query IndexerQuery {
-  roshaan_near_alphaindexer_posts(
-    order_by: {block_height: desc}
-    where: {_and: {block_height: {_eq: ${blockHeight}}, account_id: {_eq: "${accountId}"}}}
-  ) {
-    account_id
-    block_height
-    block_timestamp
-    content
-    receipt_id
-    accounts_liked
-    comments(order_by: {block_height: asc}) {
-      account_id
-      block_height
-      block_timestamp
-      content
-    }
-  }
-}
-`;
-
-  function fetchGraphQL(operationsDoc, operationName, variables) {
-    return asyncFetch(
-      "https://query-api-hasura-vcqilefdcq-uc.a.run.app/v1/graphql",
-      {
-        method: "POST",
-        headers: { "x-hasura-role": "roshaan_near" },
-        body: JSON.stringify({
-          query: operationsDoc,
-          variables: variables,
-          operationName: operationName,
-        }),
-      }
-    );
-  }
-
-  fetchGraphQL(postsQuery, "IndexerQuery", {}).then((result) => {
-    if (result.status === 200) {
-      if (result.body.data) {
-        const posts = result.body.data.roshaan_near_alphaindexer_posts;
-        if (posts.length > 0) {
-          const post = posts[0];
-          let content = JSON.parse(post.content);
-          if (post.accounts_liked.length !== 0) {
-            post.accounts_liked = JSON.parse(post.accounts_liked);
-          }
-          const comments = post.comments;
-          State.update({
-            content: content,
-            comments: comments,
-            likes: post.accounts_liked,
-          });
-        } else {
-          State.update({
-            postExists: false,
-          });
-        }
-      }
-    }
-  });
-  if (state.postExists == false) {
-    return "Post does not exist.";
-  }
-  return "loading...";
-}
 
 const Post = styled.div`
   position: relative;
@@ -144,45 +71,7 @@ const Comments = styled.div`
     padding-top: 12px;
   }
 `;
-const CommentWrapper = styled.div`
-  > div:first-child {
-    > a:first-child {
-      display: inline-flex;
-      margin-bottom: 24px;
-      font-size: 14px;
-      line-height: 20px;
-      color: #687076;
-      outline: none;
-      font-weight: 600;
 
-      &:hover,
-      &:focus {
-        color: #687076;
-        text-decoration: underline;
-      }
-    }
-  }
-`;
-
-const renderComment = (a) => {
-  return (
-    <div key={JSON.stringify(a)}>
-      <Widget
-        src="roshaan.near/widget/Comments.Comment"
-        props={{
-          accountId: a.account_id,
-          blockHeight: a.block_height,
-          content: a.content,
-          highlight:
-            a.account_id === props.highlightComment?.accountId &&
-            a.block_height === props.highlightComment?.blockHeight,
-        }}
-      />
-    </div>
-  );
-};
-
-const renderedComments = state.comments.map(renderComment);
 return (
   <Post>
     <Header>
@@ -214,34 +103,31 @@ return (
     </Header>
 
     <Body>
-      {state.content && (
-        <Content>
-          {state.content.text && (
-            <Widget
-              src="calebjacob.near/widget/SocialMarkdown"
-              props={{ text: state.content.text }}
-            />
-          )}
+      <Content>
+        {content.text && (
+          <Widget
+            src="calebjacob.near/widget/SocialMarkdown"
+            props={{ text: content.text }}
+          />
+        )}
 
-          {state.content.image && (
-            <Widget
-              src="mob.near/widget/Image"
-              props={{
-                image: state.content.image,
-              }}
-            />
-          )}
-        </Content>
-      )}
+        {content.image && (
+          <Widget
+            src="mob.near/widget/Image"
+            props={{
+              image: content.image,
+            }}
+          />
+        )}
+      </Content>
 
       {blockHeight !== "now" && (
         <Actions>
           <Widget
-            src="roshaan.near/widget/LikeButton"
+            src="calebjacob.near/widget/LikeButton"
             props={{
               item,
               notifyAccountId,
-              likes: state.likes,
             }}
           />
           <Widget
@@ -272,11 +158,19 @@ return (
           />
         </div>
       )}
-      {renderedComments && (
-        <Comments>
-          <CommentWrapper>{renderedComments}</CommentWrapper>
-        </Comments>
-      )}
+
+      <Comments>
+        <Widget
+          src="roshaan.near/widget/Comments.Feed"
+          props={{
+            item,
+            highlightComment: props.highlightComment,
+            limit: props.commentsLimit,
+            subscribe,
+            raw,
+          }}
+        />
+      </Comments>
     </Body>
   </Post>
 );
