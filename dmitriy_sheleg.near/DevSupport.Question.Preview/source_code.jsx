97716f76ab5d761254981a017298bcacd819f81f
dmitriy_sheleg.near <dmitriy_sheleg.near>
2023-03-23 14:36:03 +0000

    Update DevSupport.Question.Preview by dmitriy_sheleg.near at 2023-03-23T14:36:03.890Z

--- dmitriy_sheleg.near/DevSupport.Question.Preview/source_code.jsx
+++ dmitriy_sheleg.near/DevSupport.Question.Preview/source_code.jsx
@@ -7,127 +7,73 @@ const is_hidden = Near.view(adminContract, "is_hidden", {
   id: { account_id: accountId, block_height: blockHeight },
 });
 
-if (is_hidden) {
-  return "";
-}
+if(is_hidden){return ""}
 
 const question = JSON.parse(
   Social.get(`${accountId}/question/main`, blockHeight) ?? "null"
 );
 
-const testQuestion = JSON.parse(
-  Social.get(`${accountId}/question`, blockHeight) ?? "null"
-);
-
-const link = `#/dmitriy_sheleg.near/widget/DevSupport.Question.Page?accountId=${accountId}&blockHeight=${blockHeight}&adminContract=${adminContract}`;
-
-const H2 = styled.h2`
-  font-size: 20px;
-  font-weight: 600;
-  color: #11181C;
-
-  a {
-    color: inherit;
-    transition: color .15s ease;
-    &:hover {
-      color: #30A46C;
-      text-decoration: none;
-
-      & + i {
-        visibility: visible;
-      }
-    }
-
-    & + i {
-      transition: visibility .1s ease-out;
-      visibility: hidden;
-      color: #30A46C;
-    }
-  }
-`;
-const H6 = styled.h6`
-  font-size: 14px;
-  font-weight: 500;
-  color: #687076;
-`;
-const Trancate = styled.span`
-  text-overflow: ellipsis;
-  overflow: hidden;
-`;
-const TopicName = styled.span`
-  color: #006ADC;
-  font-weight: 500;
-  font-size: 14px;
-  white-space: nowrap;
-`;
-const CardWrapper = styled.div`
-  z-index: 100;
-  padding: 6px;
-`;
-const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: stretch;
-  gap: 12px;
-  width: 275px;
-  border-radius: 12px;
-  z-index: 1070;
-  background: #fff;
-  border: 1px solid #eceef0;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-  padding: 12px;
-`;
+const link = `#/ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Page?accountId=${accountId}&blockHeight=${blockHeight}&adminContract=${adminContract}`;
 
 return (
-  <div className={`${display} p-3`}>
-    <div class="row">
-      <div class="col-1">
-        <Widget
-          src="mob.near/widget/ProfileImage"
-          props={{
-            accountId,
-            imageClassName: "rounded-circle w-100 h-100",
-          }}
-        />
-      </div>
-      <div class="col-1">
-        {/* Upvote Widget */}
-        <Widget
-          src="dmitriy_sheleg.near/widget/DevSupport.Question.Button.Upvote"
-          props={{ accountId, blockHeight }}
-        />
-      </div>
-      <div class="col-10">
-        <div class="row">
-          <H2>
-            <a href={link}>{question.title}</a>
-            <i class="bi bi-arrow-right" />
-          </H2>
-          <H6>
-            <div class="d-flex">
-              <Trancate>{accountId}</Trancate>
-              &nbsp;in&nbsp;
-              <TopicName>
-                <Widget
-                  src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.LabelsDisplay"
-                  props={{ labels: question.labels }}
-                />
-              </TopicName>
-              &nbsp;&#8226;&nbsp;
-              <Widget src="mob.near/widget/TimeAgo" props={{ blockHeight }} />
-              &nbsp;ago
-            </div>
-          </H6>
-        </div>
-        <div class="row">
-          <div class="col">
+  <div className={`border ${display} p-3`}>
+    <h2>
+      <a className="text-black" href={link}>
+        {question.title}
+      </a>
+    </h2>
+
+    <Widget
+      src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.LabelsDisplay"
+      props={{ labels: question.labels }}
+    />
+
+    <div className="pt-2">
+      <Widget
+        src="mob.near/widget/MainPage.Post.Content"
+        props={{ content: { text: question.content.text } }}
+      />
+    </div>
+
+    <div>
+      <small class="text-muted">
+        <div class="row justify-content-between">
+          <div class="col-8">
+            {/* Upvote Widget */}
             <Widget
-              src="dmitriy_sheleg.near/widget/DevSupport.Question.Button.Answers"
+              src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Button.Upvote"
               props={{ accountId, blockHeight }}
             />
+
+            {/* Flag question widget */}
+            <Widget
+              src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Button.Flag"
+              props={{ accountId, blockHeight }}
+            />
+
+            {/* Answers widget */}
+            <Widget
+              src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Button.Answers"
+              props={{ accountId, blockHeight }}
+            />
+
+            {/* Delete widget */}
+            <Widget
+              src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Button.Delete"
+              props={{ accountId, blockHeight, admins, adminContract }}
+            />
+          </div>
+
+          <div class="col-4">
+            <div class="d-flex justify-content-end">
+              <Widget
+                src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/BlockToDate"
+                props={{ blockHeight }}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </small>
     </div>
   </div>
 );
