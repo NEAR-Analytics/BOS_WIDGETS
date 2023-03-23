3e01e05d11e6e380eca2888c95761ef3a6b0e995
dmitriy_sheleg.near <dmitriy_sheleg.near>
2023-03-23 13:17:43 +0000

    Update DevSuport.Main by dmitriy_sheleg.near at 2023-03-23T13:17:43.996Z

--- dmitriy_sheleg.near/DevSuport.Main/source_code.jsx
+++ dmitriy_sheleg.near/DevSuport.Main/source_code.jsx
@@ -11,42 +11,32 @@ const toggleQuestion = () => {
 const adminContract = "admin.dev-support.near";
 const admins = Near.view(adminContract, "get_admins", {});
 
-const sortByCategories = ["Newest", "Popular"];
-const filterByTopic = ["one", "two", "three"];
-
-const SidebarWrapper = styled.div`
-  border-right: 1px solid #ECEEF0;
-`;
-
 return (
   <div class="container py-3 mb-5">
     <div class="row">
-      {/**
-    <SidebarWrapper className="col-2 pe-5">
-        <Widget
-          src="dmitriy_sheleg.near/widget/DevSupport.Main.Sidebar"
-          props={{ categories: sortByCategories, topics: filterByTopic }}
-        />
-      </SidebarWrapper>
-    */}
-
-      <div class="col-12 ps-5">
-        <Widget
-          src="dmitriy_sheleg.near/widget/DevSupport.Discussion.Title"
-          props={{ disabled: !context.accountId, onClick: toggleQuestion }}
-        />
-        {/* Widget to create the question */}
-        <div class="my-3">
-          {state.askQuestion && (
-            <Widget src="dmitriy_sheleg.near/widget/DevSupport.Question.Edit" />
-          )}
-        </div>
-        {/* Widget to display Feed of Questions */}
-        <Widget
-          src="dmitriy_sheleg.near/widget/DevSupport.Feed"
-          props={{ admins, adminContract: adminContract }}
-        />
+      <div class="col-8">
+        <h1>Developer Support</h1>
+      </div>
+      <div class="col-4 justify-content-end mt-2">
+        <button
+          class="btn btn-primary float-end"
+          disabled={!context.accountId}
+          onClick={toggleQuestion}
+        >
+          <i class="bi bi-chat-dots"></i> Ask a Question
+        </button>
       </div>
     </div>
+    {/* Widget to create the question */}
+    <div class="my-3">
+      {state.askQuestion && (
+        <Widget src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Question.Edit" />
+      )}
+    </div>
+    {/* Widget to display Feed of Questions */}
+    <Widget
+      src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Feed"
+      props={{ admins, adminContract: adminContract }}
+    />
   </div>
 );
