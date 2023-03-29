abc7b06f8301c8b6882a9f9664909b6e43611b38
create.near <create.near>
2023-03-29 23:37:05 +0000

    Update Followers by create.near at 2023-03-29T23:37:05.656Z

--- create.near/Followers/source_code.jsx
+++ create.near/Followers/source_code.jsx
@@ -41,6 +41,7 @@ if (followers !== null && followers.length === 0) {
 
 return (
   <Wrapper>
+    <h3>Followers</h3>
     {followers.map(([accountId], i) => (
       <Item key={i}>
         <Widget
