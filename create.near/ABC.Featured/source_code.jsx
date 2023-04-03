1e802acc690022996992added2f402a0f7a1f41d
create.near <create.near>
2023-04-03 05:51:34 +0000

    Update ABC.Featured by create.near at 2023-04-03T05:51:34.937Z

--- create.near/ABC.Featured/source_code.jsx
+++ create.near/ABC.Featured/source_code.jsx
@@ -37,7 +37,7 @@ function href(widgetName, linkProps) {
   const linkPropsQuery = Object.entries(linkProps)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
-  return `#/${nearDevGovGigsWidgetsAccountId}/widget/ABC.pages.${widgetName}${
+  return `#/${nearDevGovGigsWidgetsAccountId}/widget/ABC.${widgetName}${
     linkPropsQuery ? "?" : ""
   }${linkPropsQuery}`;
 }
