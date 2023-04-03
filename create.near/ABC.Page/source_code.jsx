ff87999620744ab041e97125c7f5413fb25ef78b
create.near <create.near>
2023-04-03 03:04:16 +0000

    Update ABC.Page by create.near at 2023-04-03T03:04:16.795Z

--- create.near/ABC.Page/source_code.jsx
+++ create.near/ABC.Page/source_code.jsx
@@ -11,7 +11,7 @@ const defaultWidgets = [
     src: "create.near/widget/Page.Header",
   },
   {
-    src: "create.near/widget/ABC.Feed",
+    src: "create.near/widget/Featured",
   },
 ];
 
@@ -32,23 +32,27 @@ const Div = styled.div`
 `;
 
 return (
-  <Div>
-    {context.accountId && (
-      <a
-        key="edit"
-        href={"#/create.near/widget/Custom.Page.Editor"}
-        className="edit-link position-absolute top-0 end-0 link-secondary me-2 mt-1"
-      >
-        <i class="bi bi-pencil" /> Edit Page
-      </a>
-    )}
-    {widgets.map(
-      ({ src, requiresLogin }, i) =>
-        (!requiresLogin || context.accountId) && (
-          <div key={i} className="text-bg-light rounded-4 p-3 mb-3">
-            <Widget src={src} />
-          </div>
-        )
-    )}
-  </Div>
+  <>
+    <Div>
+      {context.accountId && (
+        <a
+          key="edit"
+          href={"#/create.near/widget/Custom.Page.Editor"}
+          className="edit-link position-absolute top-0 end-0 link-secondary me-2 mt-1"
+        >
+          <i class="bi bi-pencil" /> Edit Page
+        </a>
+      )}
+    </Div>
+    <Div>
+      {widgets.map(
+        ({ src, requiresLogin }, i) =>
+          (!requiresLogin || context.accountId) && (
+            <div key={i} className="text-bg-light rounded-4 p-3 mb-3">
+              <Widget src={src} />
+            </div>
+          )
+      )}
+    </Div>
+  </>
 );
