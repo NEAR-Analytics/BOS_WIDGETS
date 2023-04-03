015ebeafb5d69b4f397a15626502a93454dea7f8
create.near <create.near>
2023-04-03 05:40:36 +0000

    Update ABC.Feed by create.near at 2023-04-03T05:40:36.987Z

--- create.near/ABC.Feed/source_code.jsx
+++ create.near/ABC.Feed/source_code.jsx
@@ -1,4 +1,4 @@
-const hashtag = props.hashtag ?? "abc";
+const hashtag = props.hashtag ?? "near";
 
 const index = {
   action: "hashtag",
@@ -11,7 +11,7 @@ const index = {
 };
 
 const renderItem = (a) =>
-  (a.value.type === "social" && `${a.accountId}/post/abc` === a.value.path && (
+  (a.value.type === "social" && `${a.accountId}/post/main` === a.value.path && (
     <div key={JSON.stringify(a)} className="mb-3">
       <Widget
         src="create.near/widget/Page.Post"
@@ -28,6 +28,66 @@ const renderItem = (a) =>
         />
       </div>
     ));
+
+const PillSelect = styled.div`
+  display: inline-flex;
+  align-items: center;
+
+  @media (max-width: 600px) {
+    width: 100%;
+
+    button {
+      flex: 1;
+    }
+  }
+`;
+
+const PillSelectButton = styled.button`
+  display: block;
+  position: relative;
+  border: 1px solid #e6e8eb;
+  border-right: none;
+  padding: 3px 24px;
+  border-radius: 0;
+  font-size: 12px;
+  line-height: 18px;
+  color: ${(p) => (p.selected ? "#fff" : "#687076")};
+  background: ${(p) => (p.selected ? "#006ADC !important" : "#FBFCFD")};
+  font-weight: 600;
+  transition: all 200ms;
+
+  &:hover {
+    background: #ecedee;
+    text-decoration: none;
+  }
+
+  &:focus {
+    outline: none;
+    border-color: #006adc !important;
+    box-shadow: 0 0 0 1px #006adc;
+    z-index: 5;
+  }
+
+  &:first-child {
+    border-radius: 6px 0 0 6px;
+  }
+  &:last-child {
+    border-radius: 0 6px 6px 0;
+    border-right: 1px solid #e6e8eb;
+  }
+`;
+
+const FeedWrapper = styled.div`
+  .post {
+    padding-left: 24px;
+    padding-right: 24px;
+
+    @media (max-width: 1200px) {
+      padding-left: 12px;
+      padding-right: 12px;
+    }
+  }
+`;
 
 return (
   <div>
