0a8471e4b124e2cceb282f4cf1e79726af888c83
create.near <create.near>
2023-04-03 00:56:48 +0000

    Update Page.Content by create.near at 2023-04-03T00:56:48.529Z

--- create.near/Page.Content/source_code.jsx
+++ create.near/Page.Content/source_code.jsx
@@ -1,65 +1,11 @@
-const hashtag = "abc";
+const accountId = props.accountId ?? context.accountId;
 
-if (!state || state.hashtag !== hashtag) {
-  State.update({
-    feedIndex: hashtag ? 2 : context.accountId ? 0 : 1,
-    hashtag,
-  });
+const pagepage = accountId
+  ? Social.get(`${accountId}/settings/near.social/page`)
+  : undefined;
+
+if (homepage === null) {
+  return "Loading...";
 }
 
-const options = [
-  {
-    title: "Your Network",
-    disabled: !context.accountId,
-  },
-];
-
-if (hashtag) {
-  options.push({
-    title: `#${hashtag}`,
-  });
-}
-
-let accounts = undefined;
-
-if (state.feedIndex === 0) {
-  const graph = Social.keys(`${context.accountId}/graph/follow/*`, "final");
-  if (graph !== null) {
-    accounts = Object.keys(graph[context.accountId].graph.follow || {});
-    accounts.push(context.accountId);
-  } else {
-    accounts = [];
-  }
-}
-
-return (
-  <>
-    {context.accountId && (
-      <div>
-        <div className="mb-3">
-          <Widget src="create.near/widget/Page.Header" />
-        </div>
-      </div>
-    )}
-    <ul className="nav nav-pills mb-3">
-      {options.map((option, i) => (
-        <li className="nav-item" key={i}>
-          <button
-            className={`nav-link ${state.feedIndex === i ? "active" : ""} ${
-              option.disabled ? "disabled" : ""
-            }`}
-            aria-disabled={!!option.disabled}
-            onClick={() => !option.disabled && State.update({ feedIndex: i })}
-          >
-            {option.title}
-          </button>
-        </li>
-      ))}
-    </ul>
-    {state.feedIndex === 2 ? (
-      <Widget src="create.near/widget/Hashtag.Feed" props={{ hashtag }} />
-    ) : (
-      <Widget src="create.near/widget/Page.Feed" props={{ accounts }} />
-    )}
-  </>
-);
+return <Widget src="create.near/widget/ABC.Content" props={props} />;
