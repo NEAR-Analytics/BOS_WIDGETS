cba6a0726a8ec954ab425207bab516310ecbea48
create.near <create.near>
2023-04-03 00:52:28 +0000

    Update Page.Content.Editor by create.near at 2023-04-03T00:52:28.778Z

--- create.near/Page.Content.Editor/source_code.jsx
+++ create.near/Page.Content.Editor/source_code.jsx
@@ -5,7 +5,7 @@ const defaultContent = "create.near/widget/Page.Content";
 const content = Social.get(`${accountId}/settings/near.social/page.content`);
 
 if (content === null) {
-  return "Loading...";
+  return "Loading";
 }
 
 initState({
@@ -23,23 +23,29 @@ return (
       <h4>Edit Community Page</h4>
     </div>
     <div className="mb-2">
-      <p className="mb-2">Update content by inputting a widget source:</p>
+      <p className="mb-2">
+        Customize your experience by inputting any widget source:
+      </p>
       <input type="text" value={state.content} placeholder={defaultContent} />
     </div>
     <div className="mb-2">
       <CommitButton
-        data={{ settings: { "near.social": { content: state.content } } }}
+        data={{
+          settings: {
+            "near.social": { "page.content": state.content },
+          },
+        }}
       >
-        Save Content
+        Save
       </CommitButton>
       {state.content !== defaultContent && (
         <button className="btn btn-outline-primary ms-2" onClick={resetContent}>
-          Reset Content
+          Reset
         </button>
       )}
       {content === state.content && (
         <a className="btn btn-outline-primary ms-2" href={`#/`}>
-          Open Content
+          Open
         </a>
       )}
     </div>
