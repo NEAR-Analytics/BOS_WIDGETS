6c13000af8908a9bfecd696fbb04e29b429b9d6c
create.near <create.near>
2023-04-03 06:27:27 +0000

    Update ABC.widgets.community.Featured by create.near at 2023-04-03T06:27:27.765Z

--- create.near/ABC.widgets.community.Featured/source_code.jsx
+++ create.near/ABC.widgets.community.Featured/source_code.jsx
@@ -1,3 +1,4 @@
+/* INCLUDE: "common.jsx" */
 const nearDevGovGigsContractAccountId =
   props.nearDevGovGigsContractAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
@@ -14,7 +15,7 @@ function widget(widgetName, widgetProps, key) {
   };
   return (
     <Widget
-      src={`create.near/widget/ABC.${widgetName}`}
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/ABC.${widgetName}`}
       props={widgetProps}
       key={key}
     />
@@ -37,60 +38,30 @@ function href(widgetName, linkProps) {
   const linkPropsQuery = Object.entries(linkProps)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
-  return `#/${nearDevGovGigsWidgetsAccountId}/widget/ABC.${widgetName}${
+  return `#/${nearDevGovGigsWidgetsAccountId}/widget/ABC.pages.${widgetName}${
     linkPropsQuery ? "?" : ""
   }${linkPropsQuery}`;
 }
+/* END_INCLUDE: "common.jsx" */
 
-const communities = {
-  bodega: {
-    icon: "https://pbs.twimg.com/profile_images/1633040448175366145/y0afLMb1_400x400.jpg",
-    cover:
-      "https://pbs.twimg.com/profile_banners/1591547314384035841/1678182210/1080x360",
-    title: "Bodega",
-    desc: "Dynamic NFT Launchpad & Trait Marketplace",
-  },
-  nearweek: {
-    icon: "https://pbs.twimg.com/profile_images/1590659386132860928/msOLWbog_400x400.jpg",
-    cover:
-      "https://pbs.twimg.com/profile_banners/1377963859730911232/1668081831/1500x500",
-    title: "NEARWEEK",
-    desc: "Ecosystem News & Community Platform",
-  },
-  "mr-brown": {
-    icon: "https://pbs.twimg.com/profile_images/1500252631704293376/WgOX5tCA_400x400.png",
-    cover:
-      "https://pbs.twimg.com/profile_banners/1443067475868860417/1640716128/1080x360",
-    title: "Mr. Brown",
-    desc: "Thousands of Mr. Brown's Imaginary Selves",
-  },
-  bluntdao: {
-    icon: "https://pbs.twimg.com/profile_images/1533663210531958785/nMdK8_mg_400x400.jpg",
-    cover:
-      "https://pbs.twimg.com/profile_banners/1486451185804623875/1654488944/1080x360",
-    title: "Blunt DAO",
-    desc: "IRL Onboarding Movement with Proof of Sesh",
-  },
-};
+const Hover = styled.div`
+  &:hover {
+    box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
+      0px 1px 2px rgba(16, 24, 40, 0.06);
+  }
+`;
 
 return (
-  <>
-    <div class="h5 pb-3">Featured Communities</div>
-    <div class="row">
-      {Object.entries(communities).map(([label, community]) => {
-        return (
-          <div class="col">
-            {widget(
-              "widgets.community.Featured",
-              {
-                label,
-                ...community,
-              },
-              label
-            )}
-          </div>
-        );
+  <Hover className="card">
+    <a
+      href={href("community.Guide", {
+        label: props.label,
       })}
-    </div>
-  </>
+      class="text-decoration-none text-reset"
+    >
+      <img src={props.cover} class="card-img-top"></img>
+      <div class="h5 pt-3 ps-3">{props.title}</div>
+      <div class="ps-3 pb-2 text-secondary">{props.desc}</div>
+    </a>
+  </Hover>
 );
