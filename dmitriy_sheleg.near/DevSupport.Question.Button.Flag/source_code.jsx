5f30157312173a3b8ce11caedaef29b28e5475e3
dmitriy_sheleg.near <dmitriy_sheleg.near>
2023-04-06 13:40:37 +0000

    Update DevSupport.Question.Button.Flag by dmitriy_sheleg.near at 2023-04-06T13:40:37.064Z

--- dmitriy_sheleg.near/DevSupport.Question.Button.Flag/source_code.jsx
+++ dmitriy_sheleg.near/DevSupport.Question.Button.Flag/source_code.jsx
@@ -1,6 +1,5 @@
 const admin = "gagdiez.near";
 const question = { accountId: props.accountId, blockHeight: props.blockHeight };
-const text = props.text || "Report";
 
 if (question.accountId === undefined || question.blockHeight === undefined) {
   return;
@@ -25,8 +24,12 @@ const data = {
 };
 
 return (
-  <CommitButton force className={`btn ${props.className}`} data={data}>
+  <CommitButton
+    force
+    className={`ms-3 border-0 bg-white ${props.className}`}
+    data={data}
+  >
     <i class="bi bi-exclamation-circle me-1" />
-    <span>{text}</span>
+    <span>Report</span>
   </CommitButton>
 );
