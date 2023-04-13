98299485b06d975e0c6aa23f4ecc01b311ee1cc7
efiz.near <efiz.near>
2023-04-13 21:43:59 +0000

    Update Posts.Feed by efiz.near at 2023-04-13T21:43:59.841Z

--- efiz.near/Posts.Feed/source_code.jsx
+++ efiz.near/Posts.Feed/source_code.jsx
@@ -1,27 +1,7 @@
-/**
- * Forked from near/widget/Posts.Feed
- *
- * Takes in a hashtag filter or a domain filter
- */
-const hashtagsFilter = props.hashtagsFilter || [];
-const domainsFilter = props.domainsFilter || ["post"];
-
-let index = [];
-
-if (hashtagsFilter.length) {
-  // either grab all posts matching this hashtag
-  // and filter them out in "renderItem"
-  index = hashtagsFilter.map((it) => ({
-    action: "hashtag",
-    key: it.toLowerCase(),
-    options: {
-      limit: 10,
-      order: "desc",
-      accountId: props.accounts,
-    },
-  }));
-} else {
-  index = domainsFilter.map((it) => ({
+const domains = props.domains;
+let index;
+if (domains) {
+  index = domains.map((it) => ({
     action: it,
     key: "main",
     options: {
@@ -30,6 +10,16 @@ if (hashtagsFilter.length) {
       accountId: props.accounts,
     },
   }));
+} else {
+  index = {
+    action: "post",
+    key: "main",
+    options: {
+      limit: 10,
+      order: "desc",
+      accountId: props.accounts,
+    },
+  };
 }
 
 const Post = styled.div`
@@ -41,55 +31,16 @@ const Post = styled.div`
   }
 `;
 
-const renderItem = (a) => {
-  if (domainsFilter.length) {
-    for (let i = 0; i < domainsFilter.length; i++) {
-      const domain = domainsFilter[i];
-      if (hashtagsFilter.length) {
-        if (
-          a.value.type === "social" &&
-          `${a.accountId}/${domain}/main` === a.value.path
-        ) {
-          return (
-            <div key={JSON.stringify(a)} className="mb-3">
-              <Widget
-                src="near/widget/Posts.Post"
-                props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
-              />
-            </div>
-          );
-        } else if (
-          a.value.type === "social" &&
-          `${a.accountId}/${domain}/comment` === a.value.path
-        ) {
-          return (
-            <div key={JSON.stringify(a)} className="mb-3">
-              <Widget
-                src="mob.near/widget/MainPage.Comment.Post"
-                props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
-              />
-            </div>
-          );
-        }
-      } else {
-        if (a.value.type === "md") {
-          return (
-            <Post className="post" key={JSON.stringify(a)}>
-              <Widget
-                src="near/widget/Posts.Post"
-                props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
-              />
-            </Post>
-          );
-        }
-      }
-    }
-  }
-};
+const renderItem = (a) =>
+  a.value.type === "md" && (
+    <Post className="post" key={JSON.stringify(a)}>
+      <Widget
+        src="near/widget/Posts.Post"
+        props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
+      />
+    </Post>
+  );
 
 return (
-  <Widget
-    src="efiz.near/widget/MergedIndexFeed"
-    props={{ index, renderItem, disableCaching: true }}
-  />
+  <Widget src="mob.near/widget/MergedIndexFeed" props={{ index, renderItem }} />
 );
