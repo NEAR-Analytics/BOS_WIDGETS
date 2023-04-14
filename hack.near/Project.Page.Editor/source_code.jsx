081738e6e7fa4e8b66c023a36198857fb2ad5b65
hack.near <hack.near>
2023-04-14 18:19:14 +0000

    Update Project.Page.Editor by hack.near at 2023-04-14T18:19:14.626Z

--- hack.near/Project.Page.Editor/source_code.jsx
+++ hack.near/Project.Page.Editor/source_code.jsx
@@ -4,7 +4,7 @@ if (!owner) {
   return "Please connect your NEAR account :)";
 }
 
-let page = Social.getr(`${accountId}/every/page`);
+let page = Social.getr(`${accountId}/page`);
 
 if (page === null) {
   return "Loading";
