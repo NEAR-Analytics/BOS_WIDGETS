8a2e15b0ed92021c12d560fd0ea2d2144fbc0886
juaner.near <juaner.near>
2023-04-14 03:05:23 +0000

    Update ref-market-supply-supply by juaner.near at 2023-04-14T03:05:23.670Z

--- juaner.near/ref-market-supply-supply/source_code.jsx
+++ juaner.near/ref-market-supply-supply/source_code.jsx
@@ -171,7 +171,7 @@ const expandToken = (value, decimals) => {
   return new Big(value).mul(new Big(10).pow(decimals));
 };
 const formatToken = (v) => Math.floor(v * 10_000) / 10_000;
-const { showModal, closeModal, selectedTokenId } = props;
+const { selectedTokenId, showModal, closeModal, selectedTokenMeta } = props;
 const {
   rewards,
   balances,
