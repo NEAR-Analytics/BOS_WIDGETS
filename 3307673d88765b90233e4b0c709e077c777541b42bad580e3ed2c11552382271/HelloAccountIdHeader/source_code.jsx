4879cfdba679e6c9b61fe14d6ba448487b1caeed
3307673d88765b90233e4b0c709e077c777541b42bad580e3ed2c11552382271 <3307673d88765b90233e4b0c709e077c777541b42bad580e3ed2c11552382271>
2023-04-14 23:04:51 +0000

    Update HelloAccountIdHeader by 3307673d88765b90233e4b0c709e077c777541b42bad580e3ed2c11552382271 at 2023-04-14T23:04:51.101Z

--- 3307673d88765b90233e4b0c709e077c777541b42bad580e3ed2c11552382271/HelloAccountIdHeader/source_code.jsx
+++ 3307673d88765b90233e4b0c709e077c777541b42bad580e3ed2c11552382271/HelloAccountIdHeader/source_code.jsx
@@ -9,5 +9,13 @@ if (!accountId) {
 }
 
 return (
-  <h1>{`Hello, Aadesh Mallya. This is your account ID: ${accountId}!`}</h1>
+  <div>
+    <h1> Hi this is Aadesh Mallya </h1>
+    <h3> Some details about me: </h3>
+    <ol>
+      <li> I am a CS grad at Northeastern University </li>
+      <li> I am a part of the NU Blockchain group </li>
+      <li> This is an intermediate challenge of NEAR wallet </li>
+    </ol>
+  </div>
 );
