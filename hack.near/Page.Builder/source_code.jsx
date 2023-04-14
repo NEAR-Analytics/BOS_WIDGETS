7353d3a768b6130c3f012f3ffc2cc903c6e7d2ea
hack.near <hack.near>
2023-04-14 21:50:33 +0000

    Update Page.Builder by hack.near at 2023-04-14T21:50:33.934Z

--- hack.near/Page.Builder/source_code.jsx
+++ hack.near/Page.Builder/source_code.jsx
@@ -1,27 +1,115 @@
+const accountId = props.accountId; // which account's Types to use
+const font = props.font || "Courier"; // custom font for H1
+const hashtag = props.hashtag || "page"; // where to index data from
+const text = props.text || hashtag; // text for H1
+const view = props.view || "WIDGETS";
+
+const pageTypes = [
+  {
+    name: "community",
+    hashtag: "community",
+    template: "hack.near/widget/Test.Page",
+  },
+  {
+    name: "guide",
+    hashtag: "guide",
+    template: "efiz.near/widget/Test.Page",
+  },
+];
+
+const H1 = styled.h1`
+  font-family: ${font}, Times, serif;
+  font-size: 4em;
+  line-height: 1.25;
+  font-weight: 400;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  align-items: center;
+  gap: 16px;
+  width: 100%;
+`;
+
+const Controller = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 4px;
+  margin: 0 4px;
+`;
+
+const Button = styled.button`
+  text-transform: lowercase !important;
+`;
+
+State.init({
+  title: text,
+  selectedTab: view,
+  type: null,
+});
+
+const renderView = () => {
+  switch (state.selectedTab) {
+    case "CREATE":
+      return (
+        <Widget
+          src={"hack.near/widget/Create.Page"}
+          props={{ typeTag: state.type.hashtag, template: state.type.template }}
+        />
+      );
+    case "WIDGETS":
+      return (
+        <Widget
+          src={"hack.near/widget/Test.Page"}
+          props={{ hashtag: state.type.hashtag || hashtag }}
+        />
+      );
+  }
+};
+
+const handleSelectType = (type) => {
+  State.update({
+    type,
+    title: type.name || text,
+    selectedTab: "WIDGETS",
+  });
+};
+
+const handlePageCreate = () => {
+  State.update({
+    selectedTab: "CREATE",
+  });
+};
+
 return (
-  <div className="d-flex flex-column">
-    <div className="mt-auto py-3">
-      <div className="container">
-        <div className="d-flex justify-content-end">
-          <a
-            href={
-              "https://temp.everything.dev/#/evrything.near/widget/Everything.View.Thing?src=evrything-docs.near/widget/Everything.View.Document&accountId=evrything.near&blockHeight=89268687"
-            }
-          >
-            docs
-          </a>
-        </div>
-      </div>
-    </div>
-    <Widget
-      src="hack.near/widget/Page.Template"
-      props={{
-        accountId: "hack.near",
-        font: "Courier",
-        type: "everything",
-        text: "build",
-        domain: "abc",
-      }}
-    />
-  </div>
+  <>
+    <Container>
+      <Controller>
+        <H1>{state.title}</H1>
+        <ButtonRow>
+          {state.type === null ? (
+            <>
+              {pageTypes.map((it) => (
+                <Button onClick={() => handleSelectType(it)}>{it.name}</Button>
+              ))}
+            </>
+          ) : (
+            <>
+              <Button onClick={() => handleSelectType(null)}>back</Button>
+              <Button onClick={() => handlePageCreate()}>create new</Button>
+            </>
+          )}
+        </ButtonRow>
+      </Controller>
+      {renderView()}
+    </Container>
+  </>
 );
