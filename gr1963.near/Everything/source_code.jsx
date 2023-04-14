da92bcf76b7c20a463b59c680cc4880217f66392
gr1963.near <gr1963.near>
2023-04-14 22:50:25 +0000

    Update Everything by gr1963.near at 2023-04-14T22:50:25.934Z

---
+++ gr1963.near/Everything/source_code.jsx
@@ -0,0 +1,22 @@
+return (
+  <div className="d-flex flex-column">
+    <div className="mt-auto py-3">
+      <div className="container">
+        <div className="d-flex justify-content-end">
+          <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
+            documentation
+          </a>
+        </div>
+      </div>
+    </div>
+    <Widget
+      src="evrything.near/widget/Everything.Template"
+      props={{
+        accountId: "gr1963.near", // which account's Types to use (your near account)
+        font: "Times New Roman", // select a web safe font
+        text: "documentation", // main title
+        domain: "everything-docs", // domain data should be saved to
+      }}
+    />
+  </div>
+);
