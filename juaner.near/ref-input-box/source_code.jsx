854e51856a3827200708535757fc15194d05a086
juaner.near <juaner.near>
2023-04-14 07:07:24 +0000

    Update ref-input-box by juaner.near at 2023-04-14T07:07:24.386Z

--- juaner.near/ref-input-box/source_code.jsx
+++ juaner.near/ref-input-box/source_code.jsx
@@ -1,11 +1,35 @@
 const Container = styled.div`
+    .inputArea{
+      position:relative;
+      display:flex;
+      align-items:center;
+    }
+    .maxButton{
+      display:flex;
+      align-items:center;
+      justify-content:center;
+      position:absolute;
+      right:14px;
+      width: 40px;
+      height: 25px;
+      font-size: 12px;
+      line-height: 16px;
+      color: #4B6778;
+      border: 1px solid #4B6778;
+      border-radius: 6px;
+      cursor:pointer;
+    }
+    .maxButton:hover{
+      color:#5f8399;
+      border: 1px solid #5f8399;
+    }
     input{
       background: #152528;
       border-radius: 12px;
       height: 55px;
       font-size:20px;
       color: #7E8A93;
-      padding:0 15px 0 15px;
+      padding:0 60px 0 15px;
       border:none;
       margin-bottom:4px;
     }
@@ -14,19 +38,14 @@ const Container = styled.div`
       background: #152528; 
       color: #7E8A93;
     }
-    .balance {
+    .valueArea {
+      display:flex;
+      align-items:center;
+      justify-content:space-between;
       font-size:12px;
       color:#4B6778;
-      margin-left:6px;
+      padding:0 6px;
     }
-    .balance .value {
-      cursor:pointer;
-      text-decoration:underline;
-    }
-    .balance .value:hover{
-        color:#7E8A93;
-    }
-
     input::-webkit-outer-spin-button,
     input::-webkit-inner-spin-button {
     -webkit-appearance: none !important;
@@ -54,12 +73,15 @@ function changeToMax() {
 const subBalance = Big(balance || "0").toFixed(4);
 return (
   <Container>
-    <input type="number" value={amount} onChange={changeAmount} />
-    <span class="balance">
-      Balance:{" "}
-      <label class="value" onClick={changeToMax}>
-        {subBalance}(${balance$ || "0"})
-      </label>
-    </span>
+    <div class="inputArea">
+      <input type="number" value={amount} onChange={changeAmount} />
+      <span class="maxButton" onClick={changeToMax}>
+        Max
+      </span>
+    </div>
+    <div class="valueArea">
+      <span class="value">${balance$ || "0"}</span>
+      <span>Balance:{subBalance}</span>
+    </div>
   </Container>
 );
