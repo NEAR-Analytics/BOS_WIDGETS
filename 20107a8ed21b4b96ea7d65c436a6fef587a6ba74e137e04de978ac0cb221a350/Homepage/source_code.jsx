80b2e44be38c70509b04f56bbd62ecf835503187
20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350 <20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350>
2023-04-15 22:14:46 +0000

    Update Homepage by 20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350 at 2023-04-15T22:14:46.793Z

---
+++ 20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350/Homepage/source_code.jsx
@@ -0,0 +1,21 @@
+const accountId = context.accountId;
+
+const homepage = accountId
+  ? Social.get(`${accountId}/settings/near.social/homepage`)
+  : undefined;
+
+if (homepage === null) {
+  return "Loading";
+}
+
+return (
+  <Widget
+    src="evrything.near/widget/Everything.Template"
+    props={{
+      accountId: "evrything-docs.near", // which account's Types to use (your near account)
+      font: "Times New Roman", // select a web safe font
+      text: "documentation", // main title
+      domain: "everything-docs", // domain data should be saved to
+    }}
+  />
+);
