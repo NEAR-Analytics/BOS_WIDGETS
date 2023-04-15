4a61a45f69b9097b59b317dd8d7e8e2d5d2ad568
snowman12312.near <snowman12312.near>
2023-04-15 21:26:51 +0000

    Update Everything by snowman12312.near at 2023-04-15T21:26:51.754Z

---
+++ snowman12312.near/Everything/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <Widget
+    src="evrything.near/widget/Everything.Template"
+    props={{
+      accountId: "snowman12312.near",
+      font: "Times New Roman",
+      text: "documentation",
+      domain: "snowman12312",
+    }}
+  />
+);
