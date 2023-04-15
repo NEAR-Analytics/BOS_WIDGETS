e3cc20c52e53afa979c7f370be411a8f8c21601d
rodrigos.near <rodrigos.near>
2023-04-15 19:00:29 +0000

    Update WikiOnSocialDB_ArticlesByAuthor by rodrigos.near at 2023-04-15T19:00:29.515Z

---
+++ rodrigos.near/WikiOnSocialDB_ArticlesByAuthor/source_code.jsx
@@ -0,0 +1,89 @@
+const addressForArticles = "wikiTest2Article";
+const authorForWidget = "rodrigos.near";
+const authorId = props.author;
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+
+const getDateLastEdit = (timestamp) => {
+  const date = new Date(Number(timestamp));
+  const dateString = `${date.toLocaleDateString()} / ${date.toLocaleTimeString()}`;
+  return dateString;
+};
+
+// ========== GET INDEX ARRAY FOR ARTICLES ==========
+const postsIndex = Social.index(addressForArticles, "main", {
+  order: "desc",
+  accountId: undefined,
+});
+// ========== GET ALL ARTICLES ==========
+const resultArticles =
+  postsIndex &&
+  postsIndex.reduce((acc, { accountId, blockHeight }) => {
+    const postData = Social.get(
+      `${accountId}/${addressForArticles}/main`,
+      blockHeight
+    );
+    const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
+    return [...acc, postDataWithBlockHeight];
+  }, []);
+// ========== FILTER DUBLICATES ==========
+const filteredArticles =
+  resultArticles.length &&
+  resultArticles.reduce((acc, article) => {
+    if (!acc.some(({ articleId }) => articleId === article.articleId)) {
+      return [...acc, article];
+    } else {
+      return acc;
+    }
+  }, []);
+
+const filteredArticlesByUser =
+  filteredArticles.length &&
+  filteredArticles.reduce((acc, article) => {
+    if (article.author === authorId) {
+      return [...acc, article];
+    } else {
+      return acc;
+    }
+  }, []);
+
+return (
+  <>
+    <Widget
+      src={`${authorForWidget}/widget/WikiOnSocialDB_MainNavigation`}
+      props={{ currentNavPill: "authors" }}
+    />
+    <div>
+      Author:
+      <a
+        href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${authorId}`}
+        target="_blank"
+      >
+        {authorId}
+      </a>
+      <ol>
+        {filteredArticlesByUser &&
+          filteredArticlesByUser.map((article, index) => (
+            <li key={article.articleId}>
+              <a
+                href={`#/${authorForWidget}/widget/WikiOnSocialDB_OneArticle?articleId=${article.articleId}&blockHeight=${article.blockHeight}&lastEditor=${article.lastEditor}
+            `}
+              >
+                {article.articleId}{" "}
+                <small>
+                  (last edited:
+                  {getDateLastEdit(article.timeLastEdit)})
+                </small>
+              </a>
+            </li>
+          ))}
+      </ol>
+    </div>
+  </>
+);
