bb732875921b7715476ea12196e870a7c8e33e85
snowman12312.near <snowman12312.near>
2023-04-15 21:45:34 +0000

    Update Everything by snowman12312.near at 2023-04-15T21:45:34.678Z

--- snowman12312.near/Everything/source_code.jsx
+++ snowman12312.near/Everything/source_code.jsx
@@ -4,6 +4,7 @@ return (
     props={{
       accountId: "snowman12312.near",
       font: "Times New Roman",
+      type: "everything",
       text: "documentation",
       domain: "snowman12312",
     }}
