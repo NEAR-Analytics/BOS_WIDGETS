306bde856b781db5edc7079a97ea06fc6acbd2bf
20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350 <20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350>
2023-04-15 22:14:28 +0000

    Update Everything by 20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350 at 2023-04-15T22:14:28.783Z

--- 20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350/Everything/source_code.jsx
+++ 20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350/Everything/source_code.jsx
@@ -1,23 +1,11 @@
 return (
-  <div className="d-flex flex-column">
-    <div className="mt-auto py-3">
-      <div className="container">
-        <div className="d-flex justify-content-end">
-          <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
-            documentation
-          </a>
-        </div>
-      </div>
-    </div>
-    <Widget
-      src="evrything.near/widget/Everything.Template"
-      props={{
-        accountId: "evrything.near",
-        font: "Times New Roman",
-        type: "everything",
-        text: "everything",
-        domain: "everything",
-      }}
-    />
-  </div>
+  <Widget
+    src="evrything.near/widget/Everything.Template"
+    props={{
+      accountId: "evrything-docs.near", // which account's Types to use (your near account)
+      font: "Times New Roman", // select a web safe font
+      text: "documentation", // main title
+      domain: "everything-docs", // domain data should be saved to
+    }}
+  />
 );
