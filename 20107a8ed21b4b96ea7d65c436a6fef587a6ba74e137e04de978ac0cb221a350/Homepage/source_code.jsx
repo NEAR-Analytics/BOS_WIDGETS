d72a32565c77bf2331d102ea41f011fbe6906b0b
20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350 <20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350>
2023-04-15 22:47:29 +0000

    Update Homepage by 20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350 at 2023-04-15T22:47:29.753Z

--- 20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350/Homepage/source_code.jsx
+++ 20107a8ed21b4b96ea7d65c436a6fef587a6ba74e137e04de978ac0cb221a350/Homepage/source_code.jsx
@@ -14,8 +14,9 @@ return (
     props={{
       accountId: "evrything-docs.near", // which account's Types to use (your near account)
       font: "Times New Roman", // select a web safe font
-      text: "documentation", // main title
+      text: "Everything", // main title
       domain: "everything-docs", // domain data should be saved to
+      video: "video",
     }}
   />
 );
