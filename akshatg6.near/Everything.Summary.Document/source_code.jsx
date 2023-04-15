8cc42269002a455caceb77f542830b64755ec4bb
akshatg6.near <akshatg6.near>
2023-04-15 06:42:12 +0000

    Update Everything.Summary.Document by akshatg6.near at 2023-04-15T06:42:12.057Z

---
+++ akshatg6.near/Everything.Summary.Document/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <Widget
+    src="evrything.near/widget/Everything.Template"
+    props={{
+      accountId: "evrything.near", // which account's Types to use (your near account)
+      font: "Times New Roman", // select a web safe font
+      text: "everything", // main title
+      domain: "everything", // domain data should be saved to
+    }}
+  />
+);
