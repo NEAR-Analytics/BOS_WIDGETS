3fc9ea6e3eb4d7abdec7aeaeadba064246fec194
akshatg6.near <akshatg6.near>
2023-04-15 06:42:44 +0000

    Update Everything.Create.Document by akshatg6.near at 2023-04-15T06:42:44.107Z

--- akshatg6.near/Everything.Create.Document/source_code.jsx
+++ akshatg6.near/Everything.Create.Document/source_code.jsx
@@ -2,9 +2,10 @@ return (
   <Widget
     src="evrything.near/widget/Everything.Template"
     props={{
-      accountId: "evrything.near", // which account's Types to use (your near account)
+      accountId: "akshatg6.near", // which account's Types to use (your near account)
       font: "Times New Roman", // select a web safe font
       text: "everything", // main title
       domain: "everything", // domain data should be saved to
     }}
   />
+);
