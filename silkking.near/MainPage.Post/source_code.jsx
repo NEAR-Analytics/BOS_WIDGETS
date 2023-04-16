b874934c969f6c44cdfe676902e14208b8ccfff4
silkking.near <silkking.near>
2023-04-16 21:16:40 +0000

    Update MainPage.Post by silkking.near at 2023-04-16T21:16:40.292Z

--- silkking.near/MainPage.Post/source_code.jsx
+++ silkking.near/MainPage.Post/source_code.jsx
@@ -1,20 +1,29 @@
-const updateGeneralState = props.updateGeneralState;
-const thisWidgetInlineStyles = props.allWidgetsInlineStyles.mainPage_post;
-const thisWidgetClassNames = props.allWidgetsClassNames.mainPage_post;
-const standardButtonStyles = props.allWidgetsInlineStyles.standardButtonStyles;
-const hoveringButtonStyles = props.allWidgetsInlineStyles.hoveringButtonStyles;
+function getContent() {
+  if (props.content) {
+    return props.content;
+  }
+  for (let i = 0; i < whitelistedApps.length; i++) {
+    const reposts = Social.index("repost", whitelistedApps[i]);
+    const content = reposts.find((r) => r.blockHeight === blockHeight);
+    if (content) {
+      return { text: content.value.answer };
+    }
+  }
+  const content = JSON.parse(Social.get(`${accountId}/post/main`, blockHeight));
+  if (content) {
+    return content;
+  }
 
-const content = props.content;
-const accountId = content.accountId;
-const blockHeight = parseInt(content.blockHeight);
+  return "null";
+}
 
-const widgetOwner = props.widgetOwner ?? context.accountId;
-
-State.init({
-  displayCommentBox: false,
-  answer: "",
-  hoveringElement: "",
-});
+const accountId = props.accountId;
+const blockHeight =
+  props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
+const whitelistedApps = props.whitelistedApps;
+const content = getContent();
+const subscribe = !!props.subscribe;
+const raw = !!props.raw;
 
 const notifyAccountId = accountId;
 const item = {
@@ -23,166 +32,80 @@ const item = {
   blockHeight,
 };
 
-const link = `#/mob.near/widget/MainPage.Post.Page?accountId=${accountId}&blockHeight=${blockHeight}`;
+const link = `#/mob.near/widget/MainPage.Post.Page?accountId=${accountId}&blockHeight=${blockHeight}&whitelistedApps=${whitelistedApps}`;
 
-const startCommentTo = () => {
-  State.update({ displayCommentBox: !state.displayCommentBox });
-};
-
-const RenderCommentInput = (blockHeight) => {
-  return state.displayCommentBox ? (
-    <div
-      style={thisWidgetInlineStyles.commentInput.container}
-      className={thisWidgetClassNames.commentInput.container}
-    >
-      <textarea
-        style={thisWidgetInlineStyles.commentInput.textArea}
-        rows="2"
-        value={state.commentTextMap[blockHeight]}
-        onChange={(e) => {
-          State.update({ answer: e.target.value });
-        }}
+return (
+  <div className="border rounded-4 p-3 pb-1">
+    <Widget
+      src="mob.near/widget/MainPage.Post.Header"
+      props={{ accountId, blockHeight, link, postType: "post", flagItem: item }}
+    />
+    <div className="mt-3 text-break">
+      <Widget
+        src="mob.near/widget/MainPage.Post.Content"
+        props={{ content, raw }}
       />
-      <CommitButton
-        style={
-          state.hoveringElement == "commitCommentButton"
-            ? hoveringButtonStyles
-            : standardButtonStyles
-        }
-        data={{
-          index: {
-            kudo: JSON.stringify(
-              {
-                key: "commentAnswers",
-                value: {
-                  commentAnswer: state.answer,
-                  blockHeight,
-                },
-              },
-              undefined,
-              0
-            ),
-          },
-        }}
-        onCommit={() => {
-          let ctm = state.commentTextMap;
-          ctm[blockHeight] = "";
-          State.update({
-            commentTextMap: ctm,
-            reloadData: true,
-          });
-        }}
-        onMouseEnter={() => {
-          State.update({ hoveringElement: "commitCommentButton" });
-        }}
-        onMouseLeave={() => {
-          State.update({ hoveringElement: "" });
-        }}
-      >
-        Comment
-      </CommitButton>
     </div>
-  ) : (
-    ""
-  );
-};
-
-/* START KudoBox */
-const RenderKudoBox = (d) => {
-  const text = `From @${d.accountId} Kudos ${d.value.answer} `;
-  const content = { text };
-  return (
-    <>
-      <div className={thisWidgetClassNames.headerContainer}>
-        <Widget
-          src={`${widgetOwner}/widget/MainPage.Post.Header`}
-          props={{
-            widgetOwner,
-            accountId,
-            blockHeight: d.blockHeight,
-            link,
-            postType: "post",
-            flagItem: item,
-          }}
-        />
-        <div>{/*Decorative div, do not delete*/}</div>
-      </div>
-      <div
-        className={thisWidgetClassNames.cardContent}
-        style={thisWidgetInlineStyles.cardContent}
-      >
-        <div style={thisWidgetInlineStyles.postContentContainer}>
-          <Widget
-            src="mob.near/widget/MainPage.Post.Content"
-            props={{ content, raw }}
-          />
-        </div>
-
-        <div>
+    {blockHeight !== "now" && (
+      <div className="mt-1 d-flex justify-content-between">
+        <div className="me-4">
           <Widget
             src="mob.near/widget/CommentButton"
             props={{
-              onClick: startCommentTo,
+              onClick: () =>
+                !state.showReply && State.update({ showReply: true }),
             }}
           />
         </div>
+        <div className="me-4">
+          <Widget
+            src="mob.near/widget/RepostButton"
+            props={{
+              notifyAccountId,
+              item,
+            }}
+          />
+        </div>
+        <div className="me-4">
+          <Widget
+            src="mob.near/widget/LikeButton"
+            props={{
+              notifyAccountId,
+              item,
+            }}
+          />
+        </div>
+        <div>
+          <Widget
+            src="mob.near/widget/MainPage.Post.ShareButton"
+            props={{ accountId, blockHeight, postType: "post" }}
+          />
+        </div>
       </div>
-      {d.value.url != "" && d.value.url && (
-        <div
-          style={thisWidgetInlineStyles.postUrl}
-          className={thisWidgetClassNames.postUrl}
-        >
-          <span style={thisWidgetInlineStyles.postUrlSpan}>Url:</span>
-          <a href={`${urlPrefix}${d.value.url}`} target="_blank">
-            {d.value.url}
-          </a>
+    )}
+    <div className="mt-3 ps-5">
+      {state.showReply && (
+        <div className="mb-2">
+          <Widget
+            src="mob.near/widget/MainPage.Comment.Compose"
+            props={{
+              notifyAccountId,
+              item,
+              onComment: () => State.update({ showReply: false }),
+            }}
+          />
         </div>
       )}
-      <div
-        style={thisWidgetInlineStyles.upVoteContainer}
-        className={thisWidgetClassNames.upVoteContainer}
-      >
-        <CommitButton
-          style={
-            state.hoveringElement == "upVoteButton"
-              ? hoveringButtonStyles
-              : standardButtonStyles
-          }
-          onMouseEnter={() => {
-            State.update({ hoveringElement: "upVoteButton" });
-          }}
-          onMouseLeave={() => {
-            State.update({ hoveringElement: "" });
-          }}
-          data={{
-            index: {
-              kudo: JSON.stringify(
-                {
-                  key: "upvote",
-                  value: {
-                    blockHeight: d.blockHeight,
-                  },
-                },
-                undefined,
-                0
-              ),
-            },
-          }}
-        >
-          Upvote
-        </CommitButton>
-        <span style={thisWidgetInlineStyles.upVoteCounter}>
-          {d.value.upvotes} {d.value.upvotes == 1 ? "upvote" : "upvotes"}
-        </span>
-      </div>
-      {RenderCommentInput(Number(d.blockHeight))}
-
-      {
-        //RenderAllCommentAnswerBox(d)
-      }
-    </>
-  );
-};
-/* END KudoBox  */
-
-return <>{RenderKudoBox(content)}</>;
+      <Widget
+        src="mob.near/widget/MainPage.Comment.Feed"
+        props={{
+          item,
+          highlightComment: props.highlightComment,
+          limit: props.commentsLimit,
+          subscribe,
+          raw,
+        }}
+      />
+    </div>
+  </div>
+);
