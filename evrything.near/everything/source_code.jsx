3d8b265b06b9d98d11313108bdedbfb99de5a2cc
evrything.near <evrything.near>
2023-04-16 17:10:10 +0000

    Update Everything by evrything.near at 2023-04-16T17:10:10.829Z

--- evrything.near/everything/source_code.jsx
+++ evrything.near/everything/source_code.jsx
@@ -2,71 +2,25 @@ const Button = styled.button`
   text-transform: lowercase !important;
 `;
 
-const Link = styled.a`
-  text-decoration: none;
-`;
-
-const everythingExists =
-  !!Social.get(`${context.accountId}/widget/everything`) ?? false;
-
-const createEverything = () => {
-  Social.set(
-    {
-      widget: {
-        everything: {
-          "": `return (<Widget src="efiz.near/widget/Tree" props={{ rootPath: "${context.accountId}", rootType: "account" }} />);`,
-          metadata: {
-            tags: {
-              everything: "",
-            },
-          },
-        },
-      },
-    },
-    {
-      force: true,
-    }
-  );
-};
-
-// <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
-//   <Button>documentation</Button>
-// </a>;
-
 return (
   <div className="d-flex flex-column">
     <div className="mt-auto py-3">
       <div className="container">
-        <div className="d-flex justify-content-end gap-2">
-          {context.accountId &&
-            (everythingExists ? (
-              <a href={`/#/${context.accountId}/widget/everything`}>
-                <Button>{context.accountId}'s everything</Button>
-              </a>
-            ) : (
-              <Button onClick={createEverything}>
-                create your own everything
-              </Button>
-            ))}
+        <div className="d-flex justify-content-end">
+          <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
+            <Button>documentation</Button>
+          </a>
         </div>
       </div>
     </div>
     <Widget
-      src="efiz.near/widget/Tree"
+      src="evrything.near/widget/Everything.Template"
       props={{
-        rootPath: "everything",
-        rootType: "thing",
-        rootNode: {
-          post: {
-            main: null,
-            dev: null,
-            bos: null,
-          },
-          account: {
-            "efiz.near": null,
-            [context.accountId]: null,
-          },
-        },
+        accountId: "evrything.near",
+        font: "Times New Roman",
+        type: "everything",
+        text: "everything",
+        domain: "everything",
       }}
     />
   </div>
