b437b1080d5fb739982f47341881f7788de1f2b4
ce90f4869315210948fc16fb0440731ca7deb9604d3860dca81d2bafa6ffb74e <ce90f4869315210948fc16fb0440731ca7deb9604d3860dca81d2bafa6ffb74e>
2023-04-16 01:03:11 +0000

    Update Everything by ce90f4869315210948fc16fb0440731ca7deb9604d3860dca81d2bafa6ffb74e at 2023-04-16T01:03:11.181Z

--- ce90f4869315210948fc16fb0440731ca7deb9604d3860dca81d2bafa6ffb74e/Everything/source_code.jsx
+++ ce90f4869315210948fc16fb0440731ca7deb9604d3860dca81d2bafa6ffb74e/Everything/source_code.jsx
@@ -12,8 +12,7 @@ return (
     <Widget
       src="evrything.near/widget/Everything.Template"
       props={{
-        accountId:
-          "ce90f4869315210948fc16fb0440731ca7deb9604d3860dca81d2bafa6ffb74e",
+        accountId: "evrything.near",
         font: "Times New Roman",
         type: "everything",
         text: "mahith",
