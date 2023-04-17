b28a16e36d5be773a0891c5bc6531d732d23bf09
bo.near <bo.near>
2023-04-17 04:23:28 +0000

    Update gigs-board.components.posts.List by bo.near at 2023-04-17T04:23:28.425Z

--- bo.near/gigs-board.components.posts.List/source_code.jsx
+++ bo.near/gigs-board.components.posts.List/source_code.jsx
@@ -423,10 +423,10 @@ return (
         {renderedItems}
       </InfiniteScroll>
     ) : (
-      <div>
+      <p class="text-secondary">
         No posts {props.searchResult ? "matches search" : ""} in{" "}
         {getPeriodText(state.period).toLowerCase()}
-      </div>
+      </p>
     )}
   </>
 );
