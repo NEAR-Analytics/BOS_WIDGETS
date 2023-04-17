3449acd5d23970b29533a5d7601e9c935649c9ed
dorgon108.near <dorgon108.near>
2023-04-17 23:23:15 +0000

    Update AccountProfile by dorgon108.near at 2023-04-17T23:23:15.958Z

--- dorgon108.near/AccountProfile/source_code.jsx
+++ dorgon108.near/AccountProfile/source_code.jsx
@@ -34,7 +34,7 @@ const Text = styled.p`
   margin: 0;
   font-size: 14px;
   line-height: 20px;
-  color: ${(p) => (p.bold ? "#FFFFFF" : "#FFFFFF")};
+  color: ${(p) => (p.bold ? "red" : "red")};
   font-weight: ${(p) => (p.bold ? "600" : "400")};
   font-size: ${(p) => (p.small ? "10px" : "14px")};
   overflow: ${(p) => (p.ellipsis ? "hidden" : "")};
