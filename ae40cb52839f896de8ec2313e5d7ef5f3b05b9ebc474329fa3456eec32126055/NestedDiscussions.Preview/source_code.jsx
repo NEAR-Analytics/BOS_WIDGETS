709e0bdc7cbe5136ccc9c367823d39ba20f77b07
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-04-18 13:16:29 +0000

    Update NestedDiscussions.Preview by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-04-18T13:16:29.645Z

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.Preview/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.Preview/source_code.jsx
@@ -130,6 +130,7 @@ return (
             src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/LikeButton"
             props={{
               item,
+              singlePageView,
               notifyAccountId,
             }}
           />
