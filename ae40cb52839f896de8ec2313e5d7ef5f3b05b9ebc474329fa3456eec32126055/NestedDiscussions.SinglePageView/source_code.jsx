6de4f1f672c9eca9db8f4fda817636ea7fe8a133
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-04-18 17:58:10 +0000

    Update NestedDiscussions.SinglePageView by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-04-18T17:58:10.044Z

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.SinglePageView/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.SinglePageView/source_code.jsx
@@ -1,10 +1,29 @@
 const accountId = props.accountId;
 const blockHeight = parseInt(props.blockHeight);
 const dbAction = props.dbAction;
+
+const composeWidget =
+  props.composeWidget ||
+  "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Compose";
+
 const previewWidget =
   props.previewWidget ||
   "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Preview";
 
+const singlePageView =
+  props.singlePageView ||
+  "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.SinglePageView";
+
 return (
-  <Widget src={previewWidget} props={{ accountId, blockHeight, dbAction }} />
+  <Widget
+    src={previewWidget}
+    props={{
+      accountId,
+      blockHeight,
+      dbAction,
+      composeWidget,
+      previewWidget,
+      singlePageView,
+    }}
+  />
 );
