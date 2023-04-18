e5bf38b2dd293166fc47e96b5dac2fff55476d66
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-04-18 18:07:08 +0000

    Update NestedDiscussions.SinglePageView by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-04-18T18:07:08.681Z

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.SinglePageView/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.SinglePageView/source_code.jsx
@@ -1,6 +1,7 @@
 const accountId = props.accountId;
 const blockHeight = props.blockHeight ?? parseInt(props.blockHeight);
 const dbAction = props.dbAction || "discuss";
+const identifier = props.identifier;
 
 const composeWidget =
   props.composeWidget ||
@@ -16,14 +17,13 @@ const singlePageView =
 
 return (
   <Widget
-    src={previewWidget}
+    src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Feed"
     props={{
-      accountId,
-      blockHeight,
       dbAction,
       composeWidget,
       previewWidget,
       singlePageView,
+      identifier,
     }}
   />
 );
