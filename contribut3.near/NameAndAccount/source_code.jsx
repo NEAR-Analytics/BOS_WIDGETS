fee48623d4e5e8d62687b16f72ca4f818ea1df42
contribut3.near <contribut3.near>
2023-04-19 09:31:36 +0000

    Update NameAndAccount by contribut3.near at 2023-04-19T09:31:36.861Z

--- contribut3.near/NameAndAccount/source_code.jsx
+++ contribut3.near/NameAndAccount/source_code.jsx
@@ -1,26 +1,27 @@
 const name = props.name ?? context.accountId;
 const accountId = props.accountId ?? context.accountId;
+const nameSize = props.nameSize ?? "1.625em";
+const accountSize = props.accountSize ?? "0.95em";
 
 const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
-  gap: .5em;
+  gap: 0.5em;
 `;
 
 const Name = styled.span`
   font-style: normal;
   font-weight: 700;
-  font-size: 1.625em;
-  // line-height: 2.5em;
+  font-size: ${nameSize};
   color: #101828;
 `;
 
 const Account = styled.span`
   font-style: normal;
   font-weight: 400;
-  font-size: .95em;
+  font-size: ${accountSize};
   line-height: 1.4em;
   color: #687076;
 `;
