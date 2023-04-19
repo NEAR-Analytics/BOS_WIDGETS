c549be28ae7b2a2df4a7298436c60d0cb583c4a3
contribut3.near <contribut3.near>
2023-04-19 09:55:07 +0000

    Update Project.List by contribut3.near at 2023-04-19T09:55:07.239Z

--- contribut3.near/Project.List/source_code.jsx
+++ contribut3.near/Project.List/source_code.jsx
@@ -7,26 +7,52 @@ State.init({
 });
 
 if (!state.itemsIsFetched) {
-  Near.asyncView(
-    ownerId,
-    "get_projects",
-    {},
-    "final",
-    false
-  ).then((items) => State.update({ items, itemsIsFetched: true }));
+  Near.asyncView(ownerId, "get_projects", {}, "final", false).then((items) =>
+    State.update({ items, itemsIsFetched: true })
+  );
 
   return <>Loading...</>;
 }
 
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: .75em .95em;
+  gap: .75em;
+  width: 100%;
+  background: #f9fafb;
+  border-bottom: 1px solid #eaecf0;
+`;
+
+const Name = styled.div`
+  width: 35%;
+`;
+
+const Other = styled.div`
+  width: 13%;
+`;
+
 return (
-  <Widget
-    src={`${ownerId}/widget/List`}
-    props={{
-      search,
-      items: state.items,
-      createItem: (accountId) => (
-        <Widget src={`${ownerId}/widget/Project.Card`} props={{ accountId }} />
-      ),
-    }}
-  />
+  <div>
+    <Header>
+      <Name>Name</Name>
+      <Other>Admins</Other>
+      <Other>Accelerator</Other>
+      <Other>Graduation</Other>
+      <Other>Created</Other>
+      <Other>Status</Other>
+    </Header>
+    <Widget
+      src={`${ownerId}/widget/List`}
+      props={{
+        search,
+        items: state.items,
+        createItem: (accountId) => (
+          <Widget src={`${ownerId}/widget/Project.Card`} props={{ accountId }} />
+        ),
+      }}
+    />
+  </div>
 );
