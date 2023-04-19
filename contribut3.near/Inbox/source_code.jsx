42e261f66e0ea6584624858b83f9bfa47446e7f8
contribut3.near <contribut3.near>
2023-04-19 08:42:51 +0000

    Update Inbox by contribut3.near at 2023-04-19T08:42:51.893Z

--- contribut3.near/Inbox/source_code.jsx
+++ contribut3.near/Inbox/source_code.jsx
@@ -1,30 +1,5 @@
 const ownerId = "contribut3.near";
 
-const availableContent = ["proposals", "invitations"];
-
-const getContent = (content) => {
-  if (!content || !availableContent.includes(content)) {
-    return "proposals";
-  }
-
-  return content;
-};
-
-State.init({
-  proposalsCount: null,
-  invitesCount: null,
-});
-
-if (state.proposalsCount === null) {
-  Near.asyncView(
-    ownerId,
-    "get_admin_proposals",
-    { account_id: context.accountId },
-    "final",
-    false
-  ).then((proposals) => State.update({ proposalsCount: proposals.length }));
-}
-
 const Header = styled.h1`
   font-style: normal;
   font-weight: 700;
