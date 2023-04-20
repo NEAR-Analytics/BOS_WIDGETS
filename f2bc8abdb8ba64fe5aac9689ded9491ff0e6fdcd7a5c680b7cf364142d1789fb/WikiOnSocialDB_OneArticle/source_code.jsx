f1798993361bfd64257012c58adbcd49cf6d319b
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-04-20 16:22:47 +0000

    Update WikiOnSocialDB_OneArticle by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-04-20T16:22:47.874Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_OneArticle/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_OneArticle/source_code.jsx
@@ -192,9 +192,11 @@ return (
         <hr />
         <div className="accordion accordion-flush" id="accordionFlushExample">
           {resultHeading.map((arrItem, index1) => {
+            console.log(1, arrItem, index1);
             return (
               <div className="accordion-item shadow-none ">
                 {arrItem.map((item, index2) => {
+                  console.log(2, item, index2);
                   if (index2 === 0) {
                     return (
                       <h5
