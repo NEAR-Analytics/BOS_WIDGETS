a22abc1465bc011e6961885a6857a842a14010e3
contribut3.near <contribut3.near>
2023-04-20 17:51:36 +0000

    Update Project.Form by contribut3.near at 2023-04-20T17:51:36.250Z

--- contribut3.near/Project.Form/source_code.jsx
+++ contribut3.near/Project.Form/source_code.jsx
@@ -130,7 +130,7 @@ return (
         props={{
           title: "Account ID of project",
           description: <>
-            It seems like you do not have permission to edit on behalf of <b>{state.accountId}</b> you need to grant permissions the account you are currently logged in with (and any other accounts that may want to allow to edit the projects profile).
+            You need to grant permissions the account you are currently logged in with (and any other accounts that may want to allow to edit the projects profile).
             You can do so by logging in with the account of your project and going to this <a href={`/${ownerId}/widget/Index?tab=permissions&accountId=${context.accountId}`}>link</a> to grant permissions.
             After you do this you can come back here and finish the set up process (with the account you are using now - <b>{context.accountId}</b>).
           </>
