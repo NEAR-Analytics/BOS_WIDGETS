eb622332ff6d5fc1f8a5cd3ac1f02e444deda75e
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-04-20 20:13:08 +0000

    Update WikiOnSocialDB_Authors by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-04-20T20:13:08.769Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_Authors/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_Authors/source_code.jsx
@@ -52,31 +52,40 @@ const authorsCountArray =
   filteredArticles.length && Object.entries(authorsCountObject);
 
 return (
-  <div className="container-fluid border-start border-end">
+  <div
+    className="container-fluid"
+    style={{
+      backgroundColor: "rgb(230, 230, 230)",
+      borderRadius: "20px",
+      padding: "0",
+    }}
+  >
     <Widget
       src={`${authorForWidget}/widget/WikiOnSocialDB_MainNavigation`}
       props={{ currentNavPill: "authors" }}
     />
-    <h6>Total authors: {authorsCountArray.length}</h6>
+    <div style={{ margin: "0 auto", width: "90%", minWidth: "360px" }}>
+      <h6>Total authors: {authorsCountArray.length}</h6>
 
-    <div className="row card-group py-3">
-      {authorsCountArray &&
-        authorsCountArray.map(([author, quantity]) => (
-          <div className="col-sm-12 col-lg-6 col-xl-4 gy-3">
-            <div className="card h-100 p-3" key={article.articleId}>
-              <Widget
-                src="mob.near/widget/Profile.ShortInlineBlock"
-                props={{ accountId: author, tooltip: true }}
-              />
-              <a
-                className="text-start mt-3"
-                href={`#/${authorForWidget}/widget/WikiOnSocialDB_ArticlesByAuthor?author=${author}`}
-              >
-                {quantity} articles
-              </a>
+      <div className="row card-group py-3">
+        {authorsCountArray &&
+          authorsCountArray.map(([author, quantity]) => (
+            <div className="col-sm-12 col-lg-6 col-xl-4 gy-3">
+              <div className="card h-100 p-3" key={article.articleId}>
+                <Widget
+                  src="mob.near/widget/Profile.ShortInlineBlock"
+                  props={{ accountId: author, tooltip: true }}
+                />
+                <a
+                  className="text-start mt-3"
+                  href={`#/${authorForWidget}/widget/WikiOnSocialDB_ArticlesByAuthor?author=${author}`}
+                >
+                  {quantity} articles
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+      </div>
     </div>
   </div>
 );
