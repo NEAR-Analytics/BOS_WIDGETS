de34bf27bbb93f070a1e53c6ca6a693f61f88f0f
contribut3.near <contribut3.near>
2023-04-20 13:39:51 +0000

    Update Project.Line by contribut3.near at 2023-04-20T13:39:51.519Z

--- contribut3.near/Project.Line/source_code.jsx
+++ contribut3.near/Project.Line/source_code.jsx
@@ -89,6 +89,12 @@ return (
         <AccountId>@{accountId}</AccountId>
       </Row>
       {props.tall ? <Tagline>{state.tagline}</Tagline> : null}
+      {props.tall && false ? (
+        <Widget
+          src={`${ownerId}/widget/BadgeList`}
+          props={{ badges: [{ value: "Verified" }] }}
+        />
+      ) : null}
     </Column>
   </Container>
 );
