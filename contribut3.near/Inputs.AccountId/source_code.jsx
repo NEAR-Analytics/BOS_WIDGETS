060f1de6b269568ed3f796042e58323218896e71
contribut3.near <contribut3.near>
2023-04-20 18:03:23 +0000

    Update Inputs.AccountId by contribut3.near at 2023-04-20T18:03:23.117Z

--- contribut3.near/Inputs.AccountId/source_code.jsx
+++ contribut3.near/Inputs.AccountId/source_code.jsx
@@ -2,8 +2,8 @@ const ownerId = "contribut3.near";
 const label = props.label ?? "Account ID";
 const placeholder = props.placeholder ?? "Enter your account ID";
 const value = props.value ?? "";
-const onChange = props.onChange ?? (() => { });
-const addInfo = props.addInfo ?? (() => { });
+const onChange = props.onChange ?? (() => {});
+const addInfo = props.addInfo ?? (() => {});
 const accountIdRegex =
   /^(([a-z\d]+[\-_])*[a-z\d]+\.)*([a-z\d]+[\-_])*[a-z\d]+$/;
 const canEdit = (accountId) => {
@@ -14,7 +14,7 @@ const canEdit = (accountId) => {
     "final",
     false
   );
-}
+};
 
 State.init({
   valid: true,
