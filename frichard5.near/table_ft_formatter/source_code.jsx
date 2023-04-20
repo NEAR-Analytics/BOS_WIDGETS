7e117b89c309f8a949ea4f0e95ec10fdbeff86b6
frichard5.near <frichard5.near>
2023-04-20 15:03:39 +0000

    Update table_ft_formatter by frichard5.near at 2023-04-20T15:03:39.101Z

--- frichard5.near/table_ft_formatter/source_code.jsx
+++ frichard5.near/table_ft_formatter/source_code.jsx
@@ -5,8 +5,12 @@ const numberWithCommas = (x) => {
   return JSON.stringify(x).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 const defaultFt = { icon: "", symbol: "" };
+const near = {
+  symbol: "NEAR",
+  icon: "https://s2.coinmarketcap.com/static/img/coins/64x64/6535.png",
+};
 const findFt = (ftAddress) => {
-  console.log(ftAddress);
+  if (ftAddress === "Near") return near;
   if (!ftList.length) return defaultFt;
   const ft = ftList.find((f) => ftAddress === f.token_account_id);
   return ft ? ft : defaultFt;
