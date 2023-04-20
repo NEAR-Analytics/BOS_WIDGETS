8fb006da302e917d5bfc6ab1e661b6be0944a7f7
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-04-20 20:15:08 +0000

    Update WikiOnSocialDB_ArticlesByAuthor by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-04-20T20:15:08.601Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_ArticlesByAuthor/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_ArticlesByAuthor/source_code.jsx
@@ -1,5 +1,6 @@
 const addressForArticles = "wikiTest2Article";
-const authorForWidget = "rodrigos.near";
+const authorForWidget =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 const authorId = props.author;
 const accountId = props.accountId ?? context.accountId;
 if (!accountId) {
