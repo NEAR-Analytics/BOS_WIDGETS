cd2566ed691abfdf6b5b32d8191e4a3da01f4a88
contribut3.near <contribut3.near>
2023-04-20 17:52:16 +0000

    Update Inputs.AccountId by contribut3.near at 2023-04-20T17:52:16.236Z

--- contribut3.near/Inputs.AccountId/source_code.jsx
+++ contribut3.near/Inputs.AccountId/source_code.jsx
@@ -69,25 +69,33 @@ const validate = async () => {
     return;
   }
 
-  if (!await canEdit(value)) {
-    State.update({
-      valid: false,
-      errorMessage: "You do not have permission to edit this account!",
-    });
-    addInfo(true);
-    return;
-  }
+  if (!props.addInfo) {
 
-  // if (forbiddenIds.has(value)) {
-  //   State.update({
-  //     valid: false,
-  //     errorMessage: "This account ID has already been used!",
-  //   });
-  //   return;
-  // }
-  //
-  State.update({ valid: true, errorMessage: "" });
-  addInfo(false);
+
+    // if (forbiddenIds.has(value)) {
+    //   State.update({
+    //     valid: false,
+    //     errorMessage: "This account ID has already been used!",
+    //   });
+    //   return;
+    // }
+    //
+    State.update({ valid: true, errorMessage: "" });
+    addInfo(false);
+  }
+  canEdit(value).then((editPermission) => {
+    if (!editPermission) {
+      State.update({
+        valid: false,
+        errorMessage: "You do not have permission to edit this account!",
+      });
+      addInfo(true);
+      return;
+    }
+
+    State.update({ valid: true, errorMessage: "" });
+    addInfo(false);
+  });
 };
 
 return (
