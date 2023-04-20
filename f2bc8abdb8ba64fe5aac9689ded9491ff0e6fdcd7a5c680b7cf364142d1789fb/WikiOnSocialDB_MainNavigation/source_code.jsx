e82ab756fe867ad29a7e14afd87acb43c2122073
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-04-20 16:36:04 +0000

    Update WikiOnSocialDB_MainNavigation by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-04-20T16:36:04.624Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_MainNavigation/source_code.jsx
@@ -0,0 +1,88 @@
+const currentPill = props.currentNavPill ?? "";
+const writersWhiteList = props.writersWhiteList ?? [
+  "testwiki.near",
+  "eugenewolf507.near",
+];
+const authorForWidget = "rodrigos.near";
+const pills = [
+  {
+    id: "articles",
+    title: "Articles",
+    widgetName: "WikiOnSocialDB",
+  },
+  {
+    id: "authors",
+    title: "Authors",
+    widgetName: "WikiOnSocialDB_Authors",
+  },
+];
+
+const accountId = props.accountId ?? context.accountId;
+
+return (
+  <div className="navbar navbar-expand-md border-bottom mb-3">
+    <div className="container-fluid">
+      <a
+        className="navbar-brand text-decoration-none"
+        href={`#/${authorForWidget}/widget/WikiOnSocialDB`}
+      >
+        {"<WikiOnSocialDB>"}
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="collapse navbar-collapse justify-content-center"
+        id="navbarNav"
+      >
+        <ul className="navbar-nav">
+          {pills.map(({ id, title, widgetName }, i) => (
+            <li className="nav-item">
+              <a
+                href={`#/${authorForWidget}/widget/${widgetName}`}
+                className={`nav-link ${
+                  id === currentPill
+                    ? "active text-decoration-underline"
+                    : "text-decoration-none"
+                } `}
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+          <div className="d-block d-md-none">
+            <a
+              className="btn btn-outline-dark"
+              href={`#/${authorForWidget}/widget/WikiOnSocialDB_CreateArticle`}
+            >
+              + Create Article
+            </a>
+          </div>
+        </ul>
+      </div>
+      <div className="mx-2 d-none d-md-block">
+        <a
+          className="btn btn-outline-dark"
+          href={`#/${authorForWidget}/widget/WikiOnSocialDB_CreateArticle`}
+        >
+          + Create Article
+        </a>
+      </div>
+
+      <div className="d-none d-md-block" style={{maxWidth="12vw"}}>
+        <Widget
+          src="mob.near/widget/Profile.ShortInlineBlock"
+          props={{ accountId, tooltip: true }}
+        />
+      </div>
+    </div>
+  </div>
+);
