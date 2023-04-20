c8f6545f2cf9e0e383c5fc8299420ad49b20bc6e
ref-admin.near <ref-admin.near>
2023-04-20 12:19:20 +0000

    Update Component.InlineBlock by ref-admin.near at 2023-04-20T12:19:20.719Z

--- ref-admin.near/Component.InlineBlock/source_code.jsx
+++ ref-admin.near/Component.InlineBlock/source_code.jsx
@@ -1,3 +1,6 @@
+const Line = styled.div`
+  // background-color:#fff;
+`;
 const accountId = props.accountId;
 const widgetName = props.widgetName;
 const widgetPath = `${accountId}/widget/${widgetName}`;
@@ -9,40 +12,41 @@ const description = metadata.description;
 const tags = Object.keys(metadata.tags ?? {});
 
 return (
-  <div className="d-flex flex-row">
-    <Widget
-      src="mob.near/widget/WidgetImage"
-      props={{
-        metadata,
-        accountId,
-        widgetName,
-        style: { height: "3em", width: "3em", minWidth: "3em" },
-        className: "me-2",
-        imageClassName: "w-100 h-100",
-      }}
-    />
-    <div className="text-truncate">
+  <Line>
+    <div className="d-flex flex-row">
+      <Widget
+        src="mob.near/widget/WidgetImage"
+        props={{
+          metadata,
+          accountId,
+          widgetName,
+          style: { height: "38px", width: "38px" },
+          className: "me-2",
+        }}
+      />
       <div className="text-truncate">
-        <span className="fw-bold">{name}</span>{" "}
-        <small>
-          <span className="font-monospace">{widgetPath}</span>
-        </small>
-      </div>
-      <div className="text-truncate text-muted">
-        {tags.length > 0 && (
-          <>
-            {tags.map((tag, i) => (
-              <span
-                key={i}
-                className="me-1 fw-light badge border border-secondary text-bg-light"
-              >
-                #{tag}
-              </span>
-            ))}
-          </>
-        )}
-        {description}
+        <div className="text-truncate">
+          <span className="fw-bold">{name}</span>{" "}
+          <small>
+            <span className="font-monospace">{widgetPath}</span>
+          </small>
+        </div>
+        <div className="text-truncate text-muted">
+          {tags.length > 0 && (
+            <>
+              {tags.map((tag, i) => (
+                <span
+                  key={i}
+                  className="me-1 fw-light badge border border-secondary text-bg-light"
+                >
+                  #{tag}
+                </span>
+              ))}
+            </>
+          )}
+          {description}
+        </div>
       </div>
     </div>
-  </div>
+  </Line>
 );
