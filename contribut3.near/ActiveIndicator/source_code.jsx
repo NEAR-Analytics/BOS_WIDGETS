7bf01aa238e8a2795fbb65b8b342a3a8c0f2bde1
contribut3.near <contribut3.near>
2023-04-21 06:03:56 +0000

    Update ActiveIndicator by contribut3.near at 2023-04-21T06:03:56.721Z

--- contribut3.near/ActiveIndicator/source_code.jsx
+++ contribut3.near/ActiveIndicator/source_code.jsx
@@ -4,33 +4,32 @@ const inactiveText = props.inactiveText ?? "Not active";
 
 const TextSpan = styled.span`
   font-size: 0.95em;
-  color: ${({ active }) => (active ? "#027a48" : "#b54708")};
   display: block;
   margin-left: 0.25em;
   white-space: nowrap;
 `;
 
 const Icon = styled.i`
-  background-color: ${({ active }) => (active ? "#12b76a" : "#f79009")};
+  background-color: rgb(25, 135, 84);
   border-radius: 100%;
   width: 0.6em;
   height: 0.6em;
+  display: ${({ active }) => (active ? "block" : "hidden")};
 `;
 
 const Container = styled.div`
+  margin-right: 0.75em;
   display: flex;
   flex-direction: row;
+  justify-content: flex-start;
   align-items: center;
-  padding: 0.125em 0.5em 0.125em 0.375em;
-  gap: 0.25em;
-  background-color: ${({ active }) => (active ? "#ecfdf3" : "#fffaeb")};
-  mix-blend-mode: multiply;
-  border-radius: 16px;
+  color: ${({ active }) =>
+    active ? "rgb(25, 135, 84)" : "rgba(173, 181, 189, 0.75)"};
 `;
 
 return (
   <Container active={active}>
     <Icon active={active} />
-    <TextSpan active={active}>{active ? activeText : inactiveText}</TextSpan>
+    <TextSpan>{active ? activeText : inactiveText}</TextSpan>
   </Container>
 );
