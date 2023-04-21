a6471538c8c300e5b1f0a2aa65d512495af6a128
dev-support.near <dev-support.near>
2023-04-21 15:13:07 +0000

    Update DevSupport.Answer by dev-support.near at 2023-04-21T15:13:07.670Z

--- dev-support.near/DevSupport.Answer/source_code.jsx
+++ dev-support.near/DevSupport.Answer/source_code.jsx
@@ -1,46 +1,165 @@
-const index = {
-  action: "answer",
-  key: props.item,
-  options: {
-    limit: props.limit ?? 3,
-    order: "desc",
-    accountId: props.accounts,
-    subscribe: true,
-  },
-};
-
-const nested = props.nested || false;
-const admins = props.admins;
+const accountId = props.accountId;
+const blockHeight =
+  props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
+const subscribe = !!props.subscribe;
+const admins = props.admins || [];
 const adminContract = props.adminContract;
 
-const renderItem = (a) =>
-  a.value.type === "md" && (
-    <div class="mb-3">
-      <Widget
-        src="dev-support.near/widget/DevSupport.Answer"
-        props={{
-          accountId: a.accountId,
-          blockHeight: a.blockHeight,
-          parent: item,
-          admins,
-          adminContract,
-          nested,
-        }}
-      />
-    </div>
-  );
+if (accountId === undefined || blockHeight === undefined) {
+  return;
+}
+
+State.init({ showAnswer: false, showOverlay: false });
+
+const answer = JSON.parse(
+  Social.get(`${accountId}/question/answer`, blockHeight) ?? "null"
+);
+
+const isUseful = Near.view(adminContract, "is_useful", {
+  id: { account_id: accountId, block_height: blockHeight },
+});
+
+const item = {
+  type: "social",
+  path: `${accountId}/question/main`,
+  blockHeight,
+};
+
+const Post = styled.div`
+  position: relative;
+  display: grid;
+
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 19px;
+    top: 52px;
+    bottom: 12px;
+    width: 2px;
+    background: ${isUseful ? "#30A46C" : "#eceef0"};
+  }
+`;
+const Header = styled.div`
+  margin-bottom: 0;
+  display: inline-flex;
+  justify-content: stretch;
+`;
+const Body = styled.div`
+  padding-left: 52px;
+  padding-bottom: 1px;
+`;
+const Content = styled.div`
+  img {
+    display: block;
+    max-width: 100%;
+    max-height: 80vh;
+    margin: 0 0 12px;
+  }
+`;
+const Text = styled.p`
+  display: block;
+  margin: 0;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 400;
+  color: #687076;
+  white-space: nowrap;
+`;
+const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  margin: -6px -6px 6px;
+`;
+
+const Comments = styled.div`
+  > div > div:first-child {
+    padding-top: 12px;
+  }
+`;
+
+const CorrectPost = styled.div`
+  position: absolute;
+  top: -0.7rem;
+  left: -0.5rem;
+  color: #30A46C;
+`;
 
 return (
-  <div>
-    <Widget
-      src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/DevSupport.Answer.ManualIndexFeed"
-      props={{
-        index,
-        renderItem,
-        nextLimit: 10,
-        loadMoreText: "See more answers",
-        nested,
-      }}
-    />
-  </div>
+  <Post>
+    {isUseful && (
+      <CorrectPost>
+        <i class="bi bi-check-circle-fill" />
+      </CorrectPost>
+    )}
+    <Header>
+      <Widget
+        src="dev-support.near/widget/AccountProfile"
+        props={{
+          accountId,
+          blockHeight,
+          includeValidButton: admins.includes(context.accountId),
+          adminContract,
+        }}
+      />
+    </Header>
+    <Body>
+      <Content className="mt-2">
+        {answer.text && (
+          <Widget
+            src="near/widget/SocialMarkdown"
+            props={{ text: answer.text }}
+          />
+        )}
+
+        {answer.image && (
+          <Widget
+            src="mob.near/widget/Image"
+            props={{
+              image: answer.image,
+            }}
+          />
+        )}
+      </Content>
+
+      <Actions>
+        <Widget
+          src="near/widget/NestedDiscussions.Preview.LikeButton"
+          props={{
+            item: { accountId, blockHeight },
+            notifyAccountId: accountId,
+            previewWidget: "dev-support.near/widget/DevSupport.Answer",
+          }}
+        />
+        <Widget
+          src="near/widget/CommentButton"
+          props={{
+            item,
+            onClick: () => State.update({ showReply: !state.showReply }),
+          }}
+        />
+      </Actions>
+
+      {state.showReply && (
+        <div className="mb-2">
+          <Widget
+            src="dev-support.near/widget/DevSupport.Answer.Edit"
+            props={{
+              notifyAccountId: accountId,
+              item,
+              onComment: () => State.update({ showReply: false }),
+            }}
+          />
+        </div>
+      )}
+
+      <Comments>
+        <Widget
+          src="dev-support.near/widget/DevSupport.Answer.Feed"
+          props={{ item, admins, adminContract, nested: true }}
+        />
+      </Comments>
+    </Body>
+  </Post>
 );
