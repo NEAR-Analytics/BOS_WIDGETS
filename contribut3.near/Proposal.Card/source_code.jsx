9322ced844d01578806597c59f66d4a95a087fcb
contribut3.near <contribut3.near>
2023-04-21 09:56:13 +0000

    Update Proposal.Card by contribut3.near at 2023-04-21T09:56:13.336Z

--- contribut3.near/Proposal.Card/source_code.jsx
+++ contribut3.near/Proposal.Card/source_code.jsx
@@ -113,7 +113,7 @@ const Price = styled.div`
   justify-content: flex-start;
   gap: 0.125em;
 
-  &span:first-child {
+  span:first-child {
     font-style: normal;
     font-weight: 400;
     font-size: .75em;
@@ -122,7 +122,7 @@ const Price = styled.div`
     color: #7e868c;
   }
 
-  &span:last-child {
+  span:last-child {
     font-style: normal;
     font-weight: 400;
     font-size: .75em;
