435f5002c7bbef361f8c3db993e1c07873f8f852
ref-admin.near <ref-admin.near>
2023-04-21 01:29:00 +0000

    Update WidgetMetadataEditor by ref-admin.near at 2023-04-21T01:29:00.304Z

--- ref-admin.near/WidgetMetadataEditor/source_code.jsx
+++ ref-admin.near/WidgetMetadataEditor/source_code.jsx
@@ -1,40 +1,38 @@
 const widgetPath = props.widgetPath;
 const onChange = props.onChange;
-console.log('-------WidgetMetadataEditor--------props', props)
+
 let metadata = Social.getr(`${widgetPath}/metadata`);
+
 if (metadata === null) {
   return "Loading";
 }
+
 return (
-  <div>
-    <Widget
-      key={widgetPath}
-      src="ref-admin.near/widget/MetadataEditor"
-      props={{
-        initialMetadata: metadata,
-        onChange,
-        options: {
-          name: { label: "Title" },
-          image: { label: "Icon" },
-          banner: { label: "Banner" },
-          description: { label: "Description" },
-          chain: { label: "Chain" },
-          tags: {
-            label: "Tags",
-            pattern: "*/widget/*/metadata/tags/*",
-            placeholder: "profile, editor, social, finance, app, image, nft",
-          },
-          linktree: {
-            links: [
-              {
-                label: "Website",
-                prefix: "https://",
-                name: "website",
-              },
-            ],
-          },
+  <Widget
+    key={widgetPath}
+    src="mob.near/widget/MetadataEditor"
+    props={{
+      initialMetadata: metadata,
+      onChange,
+      options: {
+        name: { label: "Title" },
+        image: { label: "Icon" },
+        description: { label: "Description" },
+        tags: {
+          label: "Tags",
+          pattern: "*/widget/*/metadata/tags/*",
+          placeholder: "profile, editor, social, finance, app, image, nft",
         },
-      }}
-    />
-  </div>
+        linktree: {
+          links: [
+            {
+              label: "Website",
+              prefix: "https://",
+              name: "website",
+            },
+          ],
+        },
+      },
+    }}
+  />
 );
