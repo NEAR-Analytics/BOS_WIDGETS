47a4fbbba6a7a82084e3100da43e7873b3e8b9c1
contribut3.near <contribut3.near>
2023-04-21 07:02:51 +0000

    Update CreateServiceRequest by contribut3.near at 2023-04-21T07:02:51.254Z

--- contribut3.near/CreateServiceRequest/source_code.jsx
+++ contribut3.near/CreateServiceRequest/source_code.jsx
@@ -1,83 +1,97 @@
 const ownerId = "contribut3.near";
 
-const STEP_1 = "step1";
-const STEP_2 = "step2";
-const propsData = {
-  step1: {
-    nextButtonText: "Publish request",
-    nextButtonIcon: "bi-send",
-    progress: 100,
-  },
-  step2: {
-    nextButtonText: "Publish request",
-    nextButtonIcon: "bi-send",
-    progress: 100,
-  },
-};
-
-const Footer = styled.div`
+const LineContainer = styled.div`
   display: flex;
   flex-direction: row;
-  row-gap: 16px;
-  justify-content: space-between;
-`;
-
-const CancelButton = styled.button`
-  background: #ffffff;
-  border: 1px solid #eceef0;
-  border-radius: 50px;
-  padding: 8px 16px;
-  gap: 8px;
   align-items: center;
+  justify-content: flex-start;
+  gap: 0.25em;
+
+  img {
+    vertical-align: top;
+  }
 `;
 
-const NextButton = styled.button`
-  color: #ffffff;
-  background: #161615;
-  border-radius: 50px;
-  padding: 8px 16px;
-  gap: 8px;
+const Name = styled.div`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 0.95em;
+  line-height: 1em;
+  color: #101828;
+`;
+
+const AccountId = styled.div`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.75em;
+  line-height: 1em;
+  color: #7e868c;
+`;
+
+const ImageCircle = styled.img`
+  background: #fafafa;
+  border-radius: 8px;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+`;
+
+const ImageContainer = styled.div`
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+`;
+
+const createProjectLine = (accountId, name, image) => {
+  const fullName = name ?? accountId;
+  const url =
+    (image.ipfs_cid
+      ? `https://ipfs.near.social/ipfs/${image.ipfs_cid}`
+      : image.url) || "https://thewiki.io/static/media/sasha_anon.6ba19561.png";
+  const imageSrc = `https://i.near.social/thumbnail/${url}`;
+
+  return (
+    <LineContainer>
+      <ImageContainer title={`${fullName} @${accountId}`}>
+        <ImageCircle src={imageSrc} alt="profile image" />
+      </ImageContainer>
+      <Name>{name}</Name>
+      <AccountId>@{accountId}</AccountId>
+    </LineContainer>
+  );
+};
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
   align-items: center;
+  justify-content: flex-start;
+  width: 60%;
+  gap: 1em;
 `;
 
-const FormContainer = styled.div`
-  justify-content: center;
+const FormHeader = styled.h3`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
   align-items: flex-start;
-  padding: 32px 0px 24px;
-  gap: 36px;
+  padding: 0px 0px 0.5em;
+  border-bottom: 1px solid #eceef0;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.125em;
+  line-height: 1.25em;
+  color: #000000;
+  width: 100%;
 `;
 
-State.init({
-  step: STEP_1,
-});
-
-const renderStepOne = () => {
-  return (
-    <Widget src={`${ownerId}/widget/CreateServiceRequest_Step1`} props={{}} />
-  );
-};
-
-const renderStepTwo = () => {
-  return (
-    <Widget src={`${ownerId}/widget/CreateServiceRequest_Step2`} props={{}} />
-  );
-};
-const renderForm = {
-  step1: renderStepOne(),
-  step2: renderStepTwo(),
-}[state.step];
-
-const onNextStepClick = () => {
-  //   if (state.step == STEP_1) {
-  //     State.update({ step: STEP_2, ...propsData.step2 })
-  //   } else {
-  //     // Call Publish request
-  //   }
-};
-
-const onPublishRequestClick = () => {
-  // Call Publish request
-};
+const FormFooter = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
 
 const Container = styled.div`
   display: flex;
@@ -132,6 +146,75 @@ const ProgressBar = styled.div`
   }
 `;
 
+State.init({
+  projectId: null,
+  projects: [],
+  projectsIsFetched: false,
+  tags: [],
+  title: "",
+  description: "",
+  requestType: null,
+  requestTypes: [],
+  paymentType: null,
+  paymentTypes: [],
+  paymentSource: null,
+  paymentSources: [],
+  budget: 0,
+  deadline: null,
+});
+
+if (!state.projectsIsFetched) {
+  Near.asyncView(ownerId, "get_payment_types", {}, "final", false).then(
+    (paymentTypes) =>
+      State.update({
+        paymentTypes: paymentTypes.map((value) => ({ value, text: value })),
+      })
+  );
+  Near.asyncView(ownerId, "get_payment_sources", {}, "final", false).then(
+    (paymentSources) =>
+      State.update({
+        paymentSources: paymentSources.map((value) => ({ value, text: value })),
+      })
+  );
+  Near.asyncView(ownerId, "get_request_types", {}, "final", false).then(
+    (requestTypes) =>
+      State.update({
+        requestTypes: requestTypes.map((value) => ({ value, text: value })),
+      })
+  );
+  Near.asyncView(
+    ownerId,
+    "get_admin_projects",
+    { account_id: context.accountId },
+    "final",
+    false
+  ).then((projects) => {
+    Near.asyncView(
+      "social.near",
+      "get",
+      { keys: projects.map((accountId) => `${accountId}/profile/**`) },
+      "final",
+      false
+    ).then((data) =>
+      State.update({
+        projects: projects.map((accountId) => ({
+          // text: <Widget
+          //   src={`${ownerId}/widget/Project.Line`}
+          //   props={{ accountId, size: "1em" }}
+          // />,
+          text: createProjectLine(
+            accountId,
+            data[accountId].profile.name,
+            data[accountId].profile.image
+          ),
+          value: accountId,
+        })),
+        projectsIsFetched: true,
+      })
+    );
+  });
+}
+
 return (
   <Container>
     {/*<ProgressBar className={state.step === "step1" ? "half" : ""}><div /><div /></ProgressBar>*/}
@@ -142,51 +225,160 @@ return (
         contributors and service providers with ease
       </SubHeader>
     </div>
-
-    <FormContainer>{renderForm}</FormContainer>
-    <Footer>
-      <CancelButton href={`/${ownerId}/widget/Index`}>Cancel</CancelButton>
+    <Form>
+      <FormHeader>Request details</FormHeader>
       <Widget
-        src={`${ownerId}/widget/Buttons.Green`}
+        src={`${ownerId}/widget/Inputs.Select`}
         props={{
-          text: (
-            <>
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7.87464 10.1251L15.7496 2.25013M7.97033 10.3712L9.94141 15.4397C10.1151 15.8862 10.2019 16.1094 10.327 16.1746C10.4354 16.2311 10.5646 16.2312 10.6731 16.1748C10.7983 16.1098 10.8854 15.8866 11.0596 15.4403L16.0023 2.77453C16.1595 2.37164 16.2381 2.1702 16.1951 2.04148C16.1578 1.92969 16.0701 1.84197 15.9583 1.80462C15.8296 1.76162 15.6281 1.84023 15.2252 1.99746L2.55943 6.94021C2.11313 7.11438 1.88997 7.20146 1.82494 7.32664C1.76857 7.43516 1.76864 7.56434 1.82515 7.67279C1.89033 7.7979 2.11358 7.88472 2.56009 8.05836L7.62859 10.0294C7.71923 10.0647 7.76455 10.0823 7.80271 10.1095C7.83653 10.1337 7.86611 10.1632 7.89024 10.1971C7.91746 10.2352 7.93508 10.2805 7.97033 10.3712Z"
-                  stroke="#11181C"
-                  stroke-width="1.66667"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-              Publish request
-            </>
-          ),
-          onClick: () => {
-            Near.call(ownerId, "add_request", {
-              request: {
-                project_id: state.projectId.value,
-                title: state.title,
-                description: state.description,
-                open: true,
-                request_type: state.requestType.value,
-                payment_type: state.paymentType.value,
-                tags: state.tags.map(({ value }) => value),
-                source: state.source.value,
-                deadline: `${new Date(state.deadline).getTime()}`,
-                budget: +state.budget,
-              },
-            });
-          },
+          label: "Request as *",
+          value: state.projectId,
+          options: state.projects,
+          onChange: (projectId) => State.update({ projectId }),
         }}
       />
-    </Footer>
+      <Widget
+        src={`${ownerId}/widget/Inputs.Text`}
+        props={{
+          label: "Title",
+          placeholder: "Looking for Rust developer to create smart contracts",
+          value: state.title,
+          onChange: (title) => State.update({ title }),
+        }}
+      />
+      <Widget
+        src={`${ownerId}/widget/Inputs.TextArea`}
+        props={{
+          label: "Description",
+          placeholder:
+            "Crypto ipsum bitcoin ethereum dogecoin litecoin. Holo stacks fantom kava flow algorand. Gala dogecoin gala XRP binance flow. Algorand polygon bancor arweave avalanche. Holo kadena telcoin kusama BitTorrent flow holo velas horizen. TerraUSD helium filecoin terra shiba-inu. Serum algorand horizen kava flow maker telcoin algorand enjin. Dai bitcoin.",
+          value: state.description,
+          onChange: (description) => State.update({ description }),
+        }}
+      />
+      <Widget
+        src={`${ownerId}/widget/Inputs.MultiSelect`}
+        props={{
+          label: "Tags",
+          placeholder: "DeFi, Gaming...",
+          options: [
+            { text: "Wallets", value: "wallets" },
+            { text: "Games", value: "games" },
+          ],
+          value: state.tags,
+          onChange: (tags) => State.update({ tags }),
+        }}
+      />
+      <Widget
+        src={`${ownerId}/widget/Inputs.Select`}
+        props={{
+          label: "Request type *",
+          options: state.requestTypes,
+          value: state.requestType,
+          onChange: (requestType) => State.update({ requestType }),
+        }}
+      />
+      <Widget
+        src={`${ownerId}/widget/Inputs.Select`}
+        props={{
+          label: "Payment type *",
+          options: state.paymentTypes,
+          value: state.paymentType,
+          onChange: (paymentType) => State.update({ paymentType }),
+        }}
+      />
+      <Widget
+        src={`${ownerId}/widget/Inputs.Number`}
+        props={{
+          label: "Budget *",
+          placeholder: 1500,
+          value: state.budget,
+          onChange: (budget) => State.update({ bugdet }),
+        }}
+      />
+      <Widget
+        src={`${ownerId}/widget/Inputs.Date`}
+        props={{
+          label: "Deadline *",
+          value: state.deadline,
+          onChange: (deadline) => State.update({ deadline }),
+        }}
+      />
+      <FormFooter>
+        <Widget
+          src={`${ownerId}/widget/Buttons.Green`}
+          props={{
+            onClick: () => {
+              Near.call([
+                {
+                  contractName: "social.near",
+                  methodName: "set",
+                  args: {
+                    data: {
+                      [state.accountId]: {
+                        profile: {
+                          name: state.name,
+                          tagline: state.tagline,
+                          description: state.description,
+                          tags: state.tags.map(
+                            (acc, { name }) =>
+                              Object.assign(acc, { [name]: "" }),
+                            {}
+                          ),
+                          linktree: {
+                            ...state.socials,
+                            website: state.website,
+                          },
+                          category: state.category,
+                          team: state.team,
+                          stage: state.dev,
+                        },
+                      },
+                    },
+                  },
+                },
+                {
+                  contractName: ownerId,
+                  methodName: "add_project",
+                  args: { account_id: state.accountId },
+                },
+                {
+                  contractName: ownerId,
+                  methodName: "edit_project",
+                  args: {
+                    account_id: state.accountId,
+                    project: {
+                      application: {
+                        integration: state.integration,
+                        geo: state.geo,
+                      },
+                    },
+                  },
+                },
+              ]);
+            },
+            text: (
+              <>
+                <svg
+                  width="17"
+                  height="18"
+                  viewBox="0 0 17 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.875 16.5V12.75M2.875 5.25V1.5M1 3.375H4.75M1 14.625H4.75M9.25 2.25L7.94937 5.63165C7.73786 6.18157 7.6321 6.45653 7.46765 6.68781C7.32189 6.8928 7.1428 7.07189 6.93781 7.21765C6.70653 7.3821 6.43157 7.48786 5.88165 7.69937L2.5 9L5.88165 10.3006C6.43157 10.5121 6.70653 10.6179 6.93781 10.7824C7.1428 10.9281 7.32189 11.1072 7.46765 11.3122C7.6321 11.5435 7.73786 11.8184 7.94937 12.3684L9.25 15.75L10.5506 12.3684C10.7621 11.8184 10.8679 11.5435 11.0324 11.3122C11.1781 11.1072 11.3572 10.9281 11.5622 10.7824C11.7935 10.6179 12.0684 10.5121 12.6184 10.3006L16 9L12.6184 7.69937C12.0684 7.48786 11.7935 7.3821 11.5622 7.21765C11.3572 7.07189 11.1781 6.8928 11.0324 6.68781C10.8679 6.45653 10.7621 6.18157 10.5506 5.63165L9.25 2.25Z"
+                    stroke="#11181C"
+                    stroke-width="1.66667"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+                Create project
+              </>
+            ),
+          }}
+        />
+      </FormFooter>
+    </Form>
   </Container>
 );
