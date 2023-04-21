1ab0a6563ee7a8c88eea6ccb905cc8ec8acdc8e9
dev-support.near <dev-support.near>
2023-04-21 15:28:18 +0000

    Update DevSupport.Question.Button.Answers by dev-support.near at 2023-04-21T15:28:18.591Z

---
+++ dev-support.near/DevSupport.Question.Button.Answers/source_code.jsx
@@ -0,0 +1,36 @@
+const accountId = props.accountId;
+const blockHeight = props.blockHeight;
+
+if (accountId === undefined || blockHeight === undefined) {
+  return;
+}
+
+const item = { accountId, blockHeight };
+
+const link = `#/dev-suppport.near/widget/DevSupport.Question.Page?accountId=${accountId}&blockHeight=${blockHeight}`;
+
+const likes = Social.index("answer", item);
+
+const Replies = styled.a`
+  display: inline-flex;
+  align-items: center;
+  color: #687076;
+  transition: color .15s ease;
+
+  &:hover {
+    text-decoration: none;
+    color: #30A46C;
+  }
+
+  i {
+    display: block;
+    padding-top: .3rem;
+  }
+`;
+
+return (
+  <Replies href={link}>
+    <i class="bi bi-chat-left-dots me-2" />
+    {likes.length}&nbsp;replies
+  </Replies>
+);
