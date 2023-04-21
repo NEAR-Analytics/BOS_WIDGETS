3f16cbfe9a419f0124bed10ebc4b995700fc823f
ref-admin.near <ref-admin.near>
2023-04-21 07:49:09 +0000

    Update TagsEditor by ref-admin.near at 2023-04-21T07:49:09.108Z

--- ref-admin.near/TagsEditor/source_code.jsx
+++ ref-admin.near/TagsEditor/source_code.jsx
@@ -1,3 +1,44 @@
+const Container = styled.div`
+  .rbt-input-multi{
+     display:flex;
+     align-items:center;
+     background-color: rgba(26, 46, 51, 0.25);
+     border: 1px solid rgba(255, 255, 255, 0.3);
+     border-radius: 10px;
+     height: 47px;
+     width:100%;
+     color:rgba(255, 255, 255, 0.3);
+     padding:0 10px;
+  }
+  .rbt-input-multi.focus{
+    box-shadow:none;
+  }
+
+  .dropdown-menu{
+    background: #13181A;
+    border: 1px solid #3A4244;
+    border-radius: 10px;
+  }
+  .dropdown-menu .dropdown-item {
+    font-weight: 500;
+    font-size: 16px;
+    color:#fff;
+    text-decoration:none;
+  }
+  .dropdown-menu .dropdown-item:hover{
+    background-color:#304352;
+  }
+  .rbt-token{
+    background-color:rgba(26, 46, 51, 0.25);
+    border: 0.5px solid rgba(255, 255, 255, 0.3);
+    border-radius: 38px;
+    color:#fff;
+  }
+  .btn-close{
+    color:#7E8A93;
+    font-weight: 700;
+  }
+`;
 const tagsPattern = props.tagsPattern ?? "*/profile/tags/*";
 const placeholder = props.placeholder ?? "Tags";
 const initialTagsObject = props.initialTagsObject || {};
@@ -72,7 +113,7 @@ const setTags = (tags) => {
 };
 
 return (
-  <>
+  <Container>
     <Typeahead
       id={state.id}
       multiple
@@ -90,5 +131,5 @@ return (
         <pre>{JSON.stringify(state.tags)}</pre>
       </div>
     )}
-  </>
+  </Container>
 );
