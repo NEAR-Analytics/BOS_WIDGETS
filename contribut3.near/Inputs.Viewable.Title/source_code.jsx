e715298880f15e0e66f282ef00586e4109a5dffc
contribut3.near <contribut3.near>
2023-04-21 08:57:45 +0000

    Update Inputs.Viewable.Title by contribut3.near at 2023-04-21T08:57:45.264Z

--- contribut3.near/Inputs.Viewable.Title/source_code.jsx
+++ contribut3.near/Inputs.Viewable.Title/source_code.jsx
@@ -1,7 +1,7 @@
 const ownerId = "contribut3.near";
 const id = props.id ?? "text";
 const value = props.value ?? "";
-const onSave = props.onSave ?? (() => { });
+const onSave = props.onSave ?? (() => {});
 const canEdit = props.canEdit;
 
 const LabelArea = styled.div`
