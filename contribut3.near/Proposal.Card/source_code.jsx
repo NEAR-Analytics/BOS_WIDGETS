f6d2fe21bed52bbfad04cb32ab65ebd1416d3c3a
contribut3.near <contribut3.near>
2023-04-21 09:42:32 +0000

    Update Proposal.Card by contribut3.near at 2023-04-21T09:42:32.900Z

--- contribut3.near/Proposal.Card/source_code.jsx
+++ contribut3.near/Proposal.Card/source_code.jsx
@@ -100,6 +100,7 @@ const body = (<Container>
   <Row></Row>
   <Column>
     <Row></Row>
+    <Widget src={`${ownerId}/widget/DescriptionArea`} props={{ description: state.proposal.description }} />
   </Column>
 </Container>);
 
