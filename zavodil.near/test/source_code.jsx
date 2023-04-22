b47cdc277a53e3e921e444642aad340db030e6d3
zavodil.near <zavodil.near>
2023-04-22 18:35:08 +0000

    Update test by zavodil.near at 2023-04-22T18:35:08.426Z

--- zavodil.near/test/source_code.jsx
+++ zavodil.near/test/source_code.jsx
@@ -1,23 +1,28 @@
 const { onLoad, value } = props;
 
-const found = (param) => {
-  return `Value found: ${value}. Param: ${param}`;
-};
+if (state.initialized === undefined) {
+  State.init({
+    initialized: true,
+  });
+  const found = (param) => {
+    return `Value found: ${value}. Param: ${param}`;
+  };
 
-const notFound = (param) => {
-  return `Value NOT found. Param: ${param}`;
-};
+  const notFound = (param) => {
+    return `Value NOT found. Param: ${param}`;
+  };
 
-if (typeof onLoad === "function") {
-  if (value) {
-    onLoad({
-      func: (param) => {
-        return found(param);
-      },
-    });
-  } else {
-    onLoad({
-      func: (param) => notFound(param),
-    });
+  if (typeof onLoad === "function") {
+    if (value) {
+      onLoad({
+        func: (param) => {
+          return found(param);
+        },
+      });
+    } else {
+      onLoad({
+        func: (param) => notFound(param),
+      });
+    }
   }
 }
