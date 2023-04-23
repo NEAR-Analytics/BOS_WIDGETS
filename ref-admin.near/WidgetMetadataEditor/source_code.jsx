39c99f2b14bd885dbd7da460750410f546b4acb1
ref-admin.near <ref-admin.near>
2023-04-23 12:18:56 +0000

    Update WidgetMetadataEditor by ref-admin.near at 2023-04-23T12:18:56.339Z

--- ref-admin.near/WidgetMetadataEditor/source_code.jsx
+++ ref-admin.near/WidgetMetadataEditor/source_code.jsx
@@ -2,36 +2,38 @@ const widgetPath = props.widgetPath;
 const onChange = props.onChange;
 
 let metadata = Social.getr(`${widgetPath}/metadata`);
-
+console.log("99999999999-meta", metadata);
 if (metadata === null) {
   return "Loading";
 }
 return (
-  <Widget
-    key={widgetPath}
-    src="ref-admin.near/widget/MetadataEditor"
-    props={{
-      initialMetadata: metadata,
-      onChange,
-      options: {
-        name: { label: "Title" },
-        image: { label: "Icon" },
-        description: { label: "Description" },
-        tags: {
-          label: "Tags",
-          pattern: "*/widget/*/metadata/tags/*",
-          placeholder: "profile, editor, social, finance, app, image, nft",
+  <div style={{ backgroundColor: "#000" }}>
+    <Widget
+      key={widgetPath}
+      src="ref-admin.near/widget/MetadataEditor"
+      props={{
+        initialMetadata: metadata,
+        onChange,
+        options: {
+          name: { label: "Title" },
+          image: { label: "Icon" },
+          description: { label: "Description" },
+          tags: {
+            label: "Tags",
+            pattern: "*/widget/*/metadata/tags/*",
+            placeholder: "profile, editor, social, finance, app, image, nft",
+          },
+          linktree: {
+            links: [
+              {
+                label: "Website",
+                prefix: "https://",
+                name: "website",
+              },
+            ],
+          },
         },
-        linktree: {
-          links: [
-            {
-              label: "Website",
-              prefix: "https://",
-              name: "website",
-            },
-          ],
-        },
-      },
-    }}
-  />
+      }}
+    />
+  </div>
 );
