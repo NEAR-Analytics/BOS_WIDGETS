5d8ac2d42b7cc98a7e060b927476d75d3b6ef425
juaner.near <juaner.near>
2023-04-24 08:03:19 +0000

    Update NotificationFeed by juaner.near at 2023-04-24T08:03:19.246Z

---
+++ juaner.near/NotificationFeed/source_code.jsx
@@ -0,0 +1,31 @@
+const accountId = context.accountId;
+
+if (!accountId) {
+  return "Sign in with NEAR Wallet";
+}
+
+const index = {
+  action: "notify",
+  key: accountId,
+  options: {
+    limit: 10,
+    order: "desc",
+    subscribe: true,
+  },
+};
+console.log("11111111-00000000-2222222");
+const renderItem = (item, i) => {
+  if (i === 0) {
+    Storage.set("lastBlockHeight", item.blockHeight);
+  }
+  return (
+    <Widget src="mob.near/widget/Notification.Item" key={i} props={item} />
+  );
+};
+
+return (
+  <Widget
+    src="mob.near/widget/FilteredIndexFeed"
+    props={{ index, renderItem }}
+  />
+);
