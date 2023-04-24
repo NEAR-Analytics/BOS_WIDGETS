61c5b09185932674da2dec11349deb9f3e347840
chaotictempest.near <chaotictempest.near>
2023-04-24 18:03:16 +0000

    Update AccountProfileCard by chaotictempest.near at 2023-04-24T18:03:16.923Z

--- chaotictempest.near/AccountProfileCard/source_code.jsx
+++ chaotictempest.near/AccountProfileCard/source_code.jsx
@@ -1,7 +1,7 @@
 const accountId = props.accountId;
 const profile = props.profile || Social.get(`${accountId}/profile/**`, "final");
 const tags = Object.keys(profile.tags || {});
-const profileUrl = `/#/calebjacob.near/widget/ProfilePage?accountId=${accountId}`;
+const profileUrl = `/near/widget/ProfilePage?accountId=${accountId}`;
 const onPointerUp =
   props.onClick ??
   ((event) => {
@@ -113,10 +113,7 @@ return (
 
         {tags.length > 0 && (
           <TagsWrapper>
-            <Widget
-              src="calebjacob.near/widget/Tags"
-              props={{ tags, scroll: true }}
-            />
+            <Widget src="near/widget/Tags" props={{ tags, scroll: true }} />
           </TagsWrapper>
         )}
       </div>
@@ -124,7 +121,7 @@ return (
 
     {!!context.accountId && context.accountId !== props.accountId && (
       <Widget
-        src="calebjacob.near/widget/FollowButton"
+        src="near/widget/FollowButton"
         props={{ accountId: props.accountId }}
       />
     )}
