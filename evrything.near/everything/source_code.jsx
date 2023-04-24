9163f19d77cca02013ba1b5a9bb2dc4e92851546
evrything.near <evrything.near>
2023-04-24 20:47:50 +0000

    Update Everything by evrything.near at 2023-04-24T20:47:50.921Z

--- evrything.near/everything/source_code.jsx
+++ evrything.near/everything/source_code.jsx
@@ -2,11 +2,36 @@ const Button = styled.button`
   text-transform: lowercase !important;
 `;
 
+const createEverything = () => {
+  Social.set(
+    {
+      widget: {
+        everything: {
+          "": `return (<Widget src="evrything.near/widget/Everything.Template" props={{ accountId: "${context.accountId}", font: "Times New Roman", type: "everything", text: "${context.accountId}", domain: "everything" }} />);`,
+          metadata: {
+            tags: {
+              everything: "",
+            },
+          },
+        },
+      },
+    },
+    {
+      force: true,
+    }
+  );
+};
+
 return (
   <div className="d-flex flex-column">
     <div className="mt-auto py-3">
       <div className="container">
-        <div className="d-flex justify-content-end">
+        <div className="d-flex justify-content-end gap-2">
+          {context.accountId && (
+            <Button onClick={createEverything}>
+              create your own everything
+            </Button>
+          )}
           <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
             <Button>documentation</Button>
           </a>
