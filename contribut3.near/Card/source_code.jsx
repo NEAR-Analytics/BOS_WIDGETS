b5d484cc2de7ecebfa4bb1fad154a833fb417fea
contribut3.near <contribut3.near>
2023-04-24 15:23:18 +0000

    Update Card by contribut3.near at 2023-04-24T15:23:18.928Z

--- contribut3.near/Card/source_code.jsx
+++ contribut3.near/Card/source_code.jsx
@@ -10,6 +10,16 @@ const CardContainer = styled.div`
   box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
     0px 1px 2px rgba(16, 24, 40, 0.06);
   border-radius: 8px;
+  flex-shrink: 0;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    width: 49%;
+  }
+
+  @media (min-width: 2560px) {
+    width: 31%;
+  }
 `;
 
 const CardHeader = styled.div`
