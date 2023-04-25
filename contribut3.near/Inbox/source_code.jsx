c7c459cf46463920b08912378e6218ecde78ebdd
contribut3.near <contribut3.near>
2023-04-25 08:18:26 +0000

    Update Inbox by contribut3.near at 2023-04-25T08:18:26.312Z

--- contribut3.near/Inbox/source_code.jsx
+++ contribut3.near/Inbox/source_code.jsx
@@ -1,19 +1,5 @@
 const ownerId = "contribut3.near";
 
-const Wrapper = styled.div`
-  padding-bottom: 48px;
-
-  > div {
-    > * {
-      margin-bottom: 12px;
-
-      &:last-child {
-        margin-bottom: 0;
-      }
-    }
-  }
-`;
-
 const Header = styled.h1`
   font-style: normal;
   font-weight: 700;
@@ -23,28 +9,4 @@ const Header = styled.h1`
 
 const header = <Header>Inbox</Header>;
 
-const index = {
-  action: "inbox",
-  key: context.accountId,
-  options: {
-    limit: 10,
-    order: "desc",
-    subscribe: true,
-  },
-};
-
-const renderItem = (item, i) => {
-  if (i === 0) {
-    Storage.set("lastBlockHeight", item.blockHeight);
-  }
-  return (
-    <Widget src={`${ownerId}/widget/Notification.Index`} key={i} props={item} />
-  );
-};
-
-return (
-  <Wrapper>
-    <Header>{header}</Header>
-    <Widget src="near/widget/IndexFeed" props={{ index, renderItem }} />
-  </Wrapper>
-);
+return <div>{header}</div>;
