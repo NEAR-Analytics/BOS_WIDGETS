da7c0d152d68dfc2a48912b09ea34517a21c099b
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-04-25 15:44:11 +0000

    Update NestedDiscussions by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-04-25T15:44:11.277Z

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions/source_code.jsx
@@ -1,18 +1,10 @@
+const moderatorAccount = props.moderatorAccount || "bosmod.near";
+
 const identifier = props.identifier;
 const notifyAccountId = props.notifyAccountId;
-const dbAction = props.dbAction || "discuss";
-const moderatorAccount = props.moderatorAccount || "bosmod.near";
-const composeWidget =
-  props.composeWidget ||
-  "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Compose";
-const previewWidget =
-  props.previewWidget ||
-  "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Preview";
 
 // discussions generally happen inside another widget
-const notificationWidget =
-  props.notificationWidget ||
-  "ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions";
+const parentComponent = props.parentComponent;
 const notificationWidgetParams = props.notificationWidgetParams || {
   identifier,
 };
@@ -55,9 +47,8 @@ return (
     {context.accountId ? (
       <ComposeWrapper>
         <Widget
-          src={composeWidget}
+          src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Compose"
           props={{
-            dbAction,
             identifier,
             notificationWidget,
             notificationParams: notificationWidgetParams,
@@ -72,7 +63,6 @@ return (
       <Widget
         src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Feed"
         props={{
-          dbAction,
           composeWidget,
           previewWidget,
           identifier,
