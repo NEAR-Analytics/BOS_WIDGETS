7a8cd29f2bfc35881e13fe9a123d2f19c65be529
ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 <ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055>
2023-04-25 17:58:26 +0000

    Update NestedDiscussions.Preview by ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055 at 2023-04-25T17:58:26.409Z

--- ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.Preview/source_code.jsx
+++ ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/NestedDiscussions.Preview/source_code.jsx
@@ -182,6 +182,7 @@ return (
           <Widget
             src="ae40cb52839f896de8ec2313e5d7ef5f3b05b9ebc474329fa3456eec32126055/widget/NestedDiscussions.Compose"
             props={{
+              indexKey,
               notifyAccountId: accountId,
               parentComponent,
               parentParams,
