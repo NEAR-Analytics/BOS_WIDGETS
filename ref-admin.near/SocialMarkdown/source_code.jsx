eaca9ffabf2d0cea8161d6f49c9e9e726153eed6
ref-admin.near <ref-admin.near>
2023-04-25 12:15:28 +0000

    Update SocialMarkdown by ref-admin.near at 2023-04-25T12:15:28.239Z

--- ref-admin.near/SocialMarkdown/source_code.jsx
+++ ref-admin.near/SocialMarkdown/source_code.jsx
@@ -53,7 +53,7 @@ const Wrapper = styled.div`
       strong{
           color:#7E8A93;
       }
-      tbody tr th{
+      thead tr th{
         border-bottom:1px solid rgba(48, 67, 82, 0.5);
       }
       tbody tr td{
