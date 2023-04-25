b7bc6d3724f1644c59ad280ab73a09b51c1b66c0
contribut3.near <contribut3.near>
2023-04-25 17:30:02 +0000

    Update IpfsFilesUpload by contribut3.near at 2023-04-25T17:30:02.504Z

--- contribut3.near/IpfsFilesUpload/source_code.jsx
+++ contribut3.near/IpfsFilesUpload/source_code.jsx
@@ -6,76 +6,45 @@ if (!props.update) return "Update function is required";
 
 State.init({
   uploading: false,
-  files: [],
+  cid: null,
 });
 
 const ipfsUrl = (cid) => `https://ipfs.near.social/ipfs/${cid}`;
 
-const filesOnChange = (files) => {
-  State.update({ uploading: true, files: [] });
-  if (files?.length > 0) {
-    files.map((file, index) => {
-      const body = file;
-      asyncFetch("https://ipfs.near.social/add", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-        },
-        body,
-      }).then((res) => {
-        const cid = res.body.cid;
-        const files = [...state.files, { index, name: file.name, cid }];
-        State.update({ files });
-        props.update(files);
-      });
-    });
-  } else {
-    State.update({ uploading: false, files: null });
-  }
-};
-
-const onClickDelete = (index) => {
-  const filesUpdated = state.files.filter((file) => file.index !== index);
-  State.update({ files: filesUpdated });
-};
-
-const filesUploaded = () => {
-  if (state.files.length > 0) {
-    return state.files.map((file) => (
-      <div class="d-flex flex-row gap-2 align-items-center">
-        <button
-          class="btn btn-danger rounded-0"
-          type="button"
-          data-toggle="tooltip"
-          data-placement="top"
-          title="Delete"
-          onClick={() => onClickDelete(file.index)}
-        >
-          <i class="bi bi-trash" />
-        </button>
-        <i class={`bi fs-3 ${fileIcon}`} />
-        <p>{file.name}</p>
-      </div>
-    ));
-  }
-  return <></>;
-};
-
 return (
-  <div className="d-inline-block">
-    {filesUploaded()}
+  <div>
+    <a href={ipfsUrl(state.cid)} download>Your file</a>
     <Files
-      multiple={true}
+      multiple={false}
       accepts={[fileAccept]}
       minFileSize={1}
       clickable
       className="btn btn-outline-primary"
-      onChange={filesOnChange}
+      onChange={(files) => {
+        if (!files) return;
+
+        const [body] = files;
+
+        State.update({ uploading: true, });
+        asyncFetch(
+          "https://ipfs.near.social/upload",
+          {
+            method: "POST",
+            headers: { Accept: "application/json" },
+            body,
+          }
+        ).then(
+          ({ body: { cid } }) => {
+            State.update({ cid, uploading: false });
+            props.update(cid);
+          }
+        );
+      }}
     >
       {state.uploading
         ? "Uploading"
-        : state.files.length > 0
-          ? "Replace All"
+        : state.cid
+          ? "Replace"
           : buttonText}
     </Files>
   </div>
