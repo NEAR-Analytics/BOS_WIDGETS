fcd75a11af895d3fb467eae55a5c678dc62e6fab
nearhorizon.near <nearhorizon.near>
2023-04-26 14:30:58 +0000

    Update Project.Form by nearhorizon.near at 2023-04-26T14:30:58.442Z

--- nearhorizon.near/Project.Form/source_code.jsx
+++ nearhorizon.near/Project.Form/source_code.jsx
@@ -204,7 +204,7 @@ return (
           placeholder: "Add tags",
           options: [{ name: "wallets" }, { name: "games" }],
           value: state.tags,
-          onChange: (tags) => State.update({ tags: tags.map(({ name }) => ({ name: "".trim().replaceAll(/\s+/g, "-") })) }),
+          onChange: (tags) => State.update({ tags: tags.map(({ name }) => ({ name: name.trim().replaceAll(/\s+/g, "-") })) }),
         }}
       />
       <Widget
