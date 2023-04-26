50dc941748cf451c64dc499ff96b9207c2db5e30
nearhorizon.near <nearhorizon.near>
2023-04-26 17:57:20 +0000

    Update Project.Form by nearhorizon.near at 2023-04-26T17:57:20.883Z

--- nearhorizon.near/Project.Form/source_code.jsx
+++ nearhorizon.near/Project.Form/source_code.jsx
@@ -363,6 +363,7 @@ return (
                                   : state.website,
                             } : {}),
                           } : {})
+
                         },
                       },
                     },
