19c7cb69b4d070570766416d2f7df16b9aafd3e6
frichard5.near <frichard5.near>
2023-04-27 11:34:32 +0000

    Update table_ft_formatter by frichard5.near at 2023-04-27T11:34:32.525Z

--- frichard5.near/table_ft_formatter/source_code.jsx
+++ frichard5.near/table_ft_formatter/source_code.jsx
@@ -10,20 +10,22 @@ const near = {
   icon: "https://s2.coinmarketcap.com/static/img/coins/64x64/6535.png",
 };
 const findFt = (ftAddress, amount) => {
-  if (ftAddress === "Near") return near;
+  if (ftAddress === "Near" || ftAdress === "") return near;
   if (!ftList.length) return defaultFt;
   const ft = ftList.find((f) => ftAddress === f.token_account_id);
   if (isParsed) return ft ? { ...ft, amount } : { ...defaultFt, amount };
   const decimals = ft ? Number(ft.decimals) : 24;
   const parsedAmount = Number(amount) / Math.pow(10, decimals);
-  return ft ? { ...ft, parsedAmount } : { ...defaultFt, parsedAmount };
+  return ft
+    ? { ...ft, amount: parsedAmount }
+    : { ...defaultFt, amount: parsedAmount };
 };
 
 const currentFt = findFt(ft, amount);
 
 return (
   <>
-    {numberWithCommas(parseInt(amount))}
+    {numberWithCommas(parseInt(currentFt.amount))}
     {currentFt.icon ? <img src={currentFt.icon} /> : ""}
     {currentFt.symbol}
   </>
