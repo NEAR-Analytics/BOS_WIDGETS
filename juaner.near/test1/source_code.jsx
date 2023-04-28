846c942c549ec42cb90df12bf0061488b705b8e4
juaner.near <juaner.near>
2023-04-28 02:36:34 +0000

    Update test1 by juaner.near at 2023-04-28T02:36:34.577Z

--- juaner.near/test1/source_code.jsx
+++ juaner.near/test1/source_code.jsx
@@ -1,11 +1,14 @@
 let greeting = "Have a great day";
-console.log("000000000000-test1组件更新了");
+Storage.get("buttonStatus", "juaner.near/widget/test2");
+const statusx = Storage.get("buttonStatus");
+console.log("000000000000-test1组件更新了-获取到test2的状态值是", statusx);
 return (
   <>
     <div class="container border border-info p-3 text-center">
       <h1>Hello {props.name}</h1>
 
       <p> {greeting} </p>
+      {statusx ? "拿到的是true" : "拿到的是false"}
     </div>
   </>
 );
