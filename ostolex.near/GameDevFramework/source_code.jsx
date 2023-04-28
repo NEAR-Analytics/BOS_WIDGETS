afbbb4ad8f77ab5804207962224c754db5e8804c
ostolex.near <ostolex.near>
2023-04-28 23:17:29 +0000

    Update GameDevFramework by ostolex.near at 2023-04-28T23:17:29.786Z

--- ostolex.near/GameDevFramework/source_code.jsx
+++ ostolex.near/GameDevFramework/source_code.jsx
@@ -1,12 +1,41 @@
-const data = `
-- [Widget for rolling dice](https://near.social/#/ostolex.near/widget/DiceWidget)
-- [Widget for matching users](https://near.social/#/let45fc.near/widget/UsersMatcher)
-- [LeaderBoard widget](https://near.social/#/ostolex.near/widget/LeaderBoardWidget)
-`;
-
 return (
   <>
-    <h1>List of widgets for gamedev</h1>
-    <Markdown text={data} />
+    <h1>GameDev widgets</h1>
+    <br />
+    <h3>Dice widget</h3>
+    <p>Really usefull widget if you have a game with dice.</p>
+    <Widget
+      src="near/widget/Onboarding.ComponentCard"
+      props={{ name: "DiceWidget", accountId: "ostolex.near" }}
+    />
+    <p class="mt-2">
+      {"Here's a full example showing all possibilities of DiceWidget:"}
+    </p>
+    <Widget
+      src="near/widget/Onboarding.ComponentCard"
+      props={{ name: "DiceWidgetDemo", accountId: "ostolex.near" }}
+    />
+    <br />
+    <h3>Users matcher</h3>
+    <p>If you have an online game, this widget might be usefull for you.</p>
+    <Widget
+      src="near/widget/Onboarding.ComponentCard"
+      props={{ name: "UsersMatcher", accountId: "let45fc.near" }}
+    />
+    <br />
+    <h3>Leaderboard</h3>
+    <p>
+      Userfull widget if you have a rating in your game and want to display it
+      in a few lines.
+    </p>
+    <Widget
+      src="near/widget/Onboarding.ComponentCard"
+      props={{ name: "LeaderBoardWidget", accountId: "ostolex.near" }}
+    />
+    <p class="mt-2">Demo of leaderboard widget:</p>
+    <Widget
+      src="near/widget/Onboarding.ComponentCard"
+      props={{ name: "LeaderBoardDemo", accountId: "ostolex.near" }}
+    />
   </>
 );
