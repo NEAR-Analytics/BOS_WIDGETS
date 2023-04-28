524ab2ce4f74fa34ba9851fe4d3c8597de5d13ad
juaner.near <juaner.near>
2023-04-28 04:18:34 +0000

    Update test2 by juaner.near at 2023-04-28T04:18:34.029Z

--- juaner.near/test2/source_code.jsx
+++ juaner.near/test2/source_code.jsx
@@ -1,18 +1,20 @@
-const { buttonStatus } = state;
+// const { buttonStatus } = state;
+Storage.get("buttonStatus");
 console.log("000000000000-test2组件更新了");
 return (
   <div>
     <button
       onClick={() => {
-        const latestStatus = !buttonStatus;
-        State.update({
-          buttonStatus: latestStatus,
-        });
-        Storage.set("buttonStatus", latestStatus);
-        console.log("000000000-buttonStatus", latestStatus);
+        const latestStatus = Math.random();
+        console.log("000000000-latestStatus", latestStatus);
+        // State.update({
+        //   buttonStatus: latestStatus,
+        // });
+        Storage.set("buttonStatus", "1");
+        // console.log("000000000-buttonStatus", latestStatus);
       }}
     >
-      click me {buttonStatus}
+      click me
     </button>
     Hello World
   </div>
