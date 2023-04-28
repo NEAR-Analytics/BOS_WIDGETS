d4a551f52497f0ef270a668dde2e05daddba08d8
juaner.near <juaner.near>
2023-04-28 02:16:35 +0000

    Update test2 by juaner.near at 2023-04-28T02:16:35.464Z

--- juaner.near/test2/source_code.jsx
+++ juaner.near/test2/source_code.jsx
@@ -1,1 +1,16 @@
-return <div>Hello World</div>;
+const { nyname } = state;
+console.log("000000000000-test2组件更新了");
+return (
+  <div>
+    <button
+      onClick={() => {
+        State.update({
+          nyname: "nature",
+        });
+      }}
+    >
+      {nyname} click me
+    </button>
+    Hello World
+  </div>
+);
