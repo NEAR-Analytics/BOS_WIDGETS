9669ead596b04e89c182f8a30fa5af3948909843
contribut3.near <contribut3.near>
2023-04-28 11:24:45 +0000

    Update Inputs.Viewable.AccountId by contribut3.near at 2023-04-28T11:24:45.188Z

--- contribut3.near/Inputs.Viewable.AccountId/source_code.jsx
+++ contribut3.near/Inputs.Viewable.AccountId/source_code.jsx
@@ -1,4 +1,4 @@
-const ownerId = "nearhorizon.near";
+const ownerId = "contribut3.near";
 const id = props.id ?? "text";
 const label = props.label ?? "Input";
 const value = props.value ?? "";
@@ -159,9 +159,7 @@ return (
             }}
           />
         </Link>
-      ) : (
-        <></>
-      ),
+      ) : null,
       canEdit,
     }}
   />
