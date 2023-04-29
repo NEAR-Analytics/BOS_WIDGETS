da467c3dcf0208e036054c38f90904945da5ab09
ref-admin.near <ref-admin.near>
2023-04-29 01:08:01 +0000

    Update test by ref-admin.near at 2023-04-29T01:08:01.813Z

--- ref-admin.near/test/source_code.jsx
+++ ref-admin.near/test/source_code.jsx
@@ -2,8 +2,24 @@ function getData() {
   // mob.near/widget/*
   return Social.get("ref-admin.near/widget/test/myname");
 }
-const data = getData();
+// const data = getData();
 console.log("7777777777777777-data", data);
+function test() {
+  Social.set(
+    {
+      myname: "nature",
+    },
+    {
+      force: true,
+      onCommit: () => {
+        // State.update({ commitLoading: false });
+      },
+      onCancel: () => {
+        // State.update({ commitLoading: false });
+      },
+    }
+  );
+}
 return (
   <div>
     <button
