52c0eeca43d62151511adb35a3e3d97a1ecfaea3
andyh.near <andyh.near>
2023-05-01 23:47:25 +0000

    Update RenderTestGrandchild by andyh.near at 2023-05-01T23:47:25.259Z

--- andyh.near/RenderTestGrandchild/source_code.jsx
+++ andyh.near/RenderTestGrandchild/source_code.jsx
@@ -2,6 +2,13 @@ State.init({ i: 0 });
 return (
   <div>
     I am a grandchild!! I am {state.i} yaars old
-    <button onClick={() => State.update({ i: state.i + 1 })}>age me</button>
+    <button
+      onClick={() => {
+        console.log("updating state " + state.i);
+        State.update({ i: state.i + 1 });
+      }}
+    >
+      age me
+    </button>
   </div>
 );
