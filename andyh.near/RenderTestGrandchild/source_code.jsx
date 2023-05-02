108dffb9ef832882059c72ff58c3f0f765b09994
andyh.near <andyh.near>
2023-05-02 20:59:27 +0000

    Update RenderTestGrandchild by andyh.near at 2023-05-02T20:59:27.355Z

--- andyh.near/RenderTestGrandchild/source_code.jsx
+++ andyh.near/RenderTestGrandchild/source_code.jsx
@@ -2,6 +2,7 @@ State.init({ k: 0 });
 return (
   <div>
     root: {props.i} child: {props.j} grandchild: {state.k}
+    <br />
     <button
       onClick={() => {
         State.update({ k: state.k + 1 });
