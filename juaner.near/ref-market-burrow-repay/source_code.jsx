f6041381e1a985582edb36ef748bf0fbcc900116
juaner.near <juaner.near>
2023-05-02 09:30:14 +0000

    Update ref-market-burrow-repay by juaner.near at 2023-05-02T09:30:14.736Z

--- juaner.near/ref-market-burrow-repay/source_code.jsx
+++ juaner.near/ref-market-burrow-repay/source_code.jsx
@@ -206,7 +206,7 @@ const storageBurrow = Near.view(BURROW_CONTRACT, "storage_balance_of", {
   account_id: accountId,
 });
 function getAdjustedSum(type, account) {
-  if (!assets || !account || account[type].length == 0) return B(1);
+  if (!assets || !account || account[type].length == 0) return 0;
   return account[type]
     .map((assetInAccount) => {
       const asset = assets.find((a) => a.token_id === assetInAccount.token_id);
@@ -236,6 +236,7 @@ const adjustedCollateralSum = getAdjustedSum("collateral", account);
 const adjustedBorrowedSum = getAdjustedSum("borrowed", account);
 
 function getHealthFactor() {
+  if (Big(adjustedBorrowedSum).eq(0)) return 10000;
   const healthFactor = B(adjustedCollateralSum)
     .div(B(adjustedBorrowedSum))
     .mul(100)
