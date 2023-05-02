ad6bb9a2b3fcadd1ceb315e55d3d7358dd86d395
hack.near <hack.near>
2023-05-02 22:43:25 +0000

    Update DAO by hack.near at 2023-05-02T22:43:25.043Z

--- hack.near/DAO/source_code.jsx
+++ hack.near/DAO/source_code.jsx
@@ -1,22 +1,11 @@
 const accountId = props.accountId ?? context.accountId;
-const daoId = props.daoId ?? "multi.sputnik-dao.near";
-const groupId = props.groupId ?? "council";
-const policy = Near.view(daoId, "get_policy");
 
-return (
-  <div className="mb-3">
-    <h2 className="mt-3">{daoId}</h2>
-    <div className="mt-3">
-      <Widget
-        src="hack.near/widget/Groups"
-        props={{ accountId, daoId, groupId }}
-      />
-    </div>
-    <div>
-      <Widget
-        src="hack.near/widget/GroupMembers"
-        props={{ accountId, daoId, groupId }}
-      />
-    </div>
-  </div>
-);
+const page = accountId
+  ? Social.get(`${accountId}/settings/dao/page`)
+  : undefined;
+
+if (page === null) {
+  return "Loading";
+}
+
+return <Widget src={page ?? "hack.near/widget/DAO.Profile"} props={props} />;
