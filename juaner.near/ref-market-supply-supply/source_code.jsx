cd5a71da8fef8e7f23eca92cfab9cbd2821e6aac
juaner.near <juaner.near>
2023-05-02 05:44:34 +0000

    Update ref-market-supply-supply by juaner.near at 2023-05-02T05:44:34.821Z

--- juaner.near/ref-market-supply-supply/source_code.jsx
+++ juaner.near/ref-market-supply-supply/source_code.jsx
@@ -260,7 +260,7 @@ const storageToken = selectedTokenId
   : null;
 
 const handleAmount = (value, isMax) => {
-  const amount = Number(value);
+  const amount = value;
   const newHF = recomputeHealthFactor(selectedTokenId, amount);
   State.update({
     amount,
