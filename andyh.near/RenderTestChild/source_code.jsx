ffcf4fd573f6bf96b3772f4aa2d399ba79750a62
andyh.near <andyh.near>
2023-05-02 18:58:03 +0000

    Update RenderTestChild by andyh.near at 2023-05-02T18:58:03.487Z

--- andyh.near/RenderTestChild/source_code.jsx
+++ andyh.near/RenderTestChild/source_code.jsx
@@ -1,6 +1,14 @@
 return (
   <div>
-    I am a child
+    I am a child! I am {state.i} yars older
+    <button
+      onClick={() => {
+        console.log("updating state " + state.i);
+        State.update({ i: state.i + 1 });
+      }}
+    >
+      age me
+    </button>
     <Widget src="andyh.near/widget/RenderTestGrandchild" />
   </div>
 );
