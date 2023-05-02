916fd9a0cc6e10111d8b15f6191939f4cd33f631
juaner.near <juaner.near>
2023-05-02 09:00:42 +0000

    Update ref-market-supply-adjust by juaner.near at 2023-05-02T09:00:42.823Z

--- juaner.near/ref-market-supply-adjust/source_code.jsx
+++ juaner.near/ref-market-supply-adjust/source_code.jsx
@@ -1,27 +1,27 @@
 const Container = styled.div`
-  .template {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-left: 6px;
-  }
-  .template_title {
-    font-size: 14px;
-    color: #7e8a93;
-  }
-  .template .value {
-    font-size: 14px;
-    color: #fff;
-  }
-  .template .usd {
-    color: #7e8a93;
-  }
-  .mt_25 {
-    margin-top: 25px;
-  }
-  .mt-10 {
-    margin-top: 10px;
-  }
+   .template{
+      display:flex;
+      align-items:center;
+      justify-content:space-between;
+      margin-left:6px;
+    }
+    .template .title{
+      font-size:14px;
+      color:#7E8A93;
+    }
+    .template .value{
+      font-size:14px;
+      color:#fff;
+    }
+    .template .usd{
+        color:#7E8A93;
+    }
+    .mt_25{
+      margin-top:25px;
+    }
+    .mt-10{
+      margin-top:10px;
+    }
 `;
 const Backdrop = styled.div`
   height: 100vh;
@@ -33,94 +33,85 @@ const Backdrop = styled.div`
   z-index: 1001;
 `;
 const Modal = styled.div`
-  background-color: #25283a;
-  border-radius: 12px;
-  position: fixed;
-  z-index: 1002;
-  width: 30rem;
+  background-color:#1A2E33;
+  border-radius:12px;
+  position:fixed;
+  z-index:1002;
+  width:30rem;
   max-width: 95vw;
   max-height: 80vh;
-  padding: 10px 0 20px 0;
-  animation: anishow 0.3s forwards ease-out;
-  left: 50%;
-  top: 50%;
+  padding:10px 0 20px 0;
+  animation:anishow 0.3s forwards ease-out;
+  left:50%;
+  top:50%;
   @keyframes anishow {
     from {
       opacity: 0;
-      transform: translate(-50%, -70%);
+      transform:translate(-50%,-70%);
     }
     to {
       opacity: 1;
-      transform: translate(-50%, -50%);
+      transform:translate(-50%,-50%);
     }
   }
-  .modal-header {
-    display: flex;
-    align-items: center;
-    justify-content: start;
-    color: #fff;
-    font-weight: 700;
-    font-size: 18px;
-    margin-bottom: 16px;
-    padding: 20px 20px 10px 20px;
-  }
-  .modal_title {
-    font-weight: 700;
-    font-size: 18px;
-    color: #fff;
-  }
-  .modal-header .btn-close {
-    position: absolute;
-    right: 28px;
-    margin: 0;
-  }
-  .modal-body {
-    padding: 0 16px;
-  }
-  .modal-body .tab {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 30px;
-  }
-  .modal-body .tab span {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 50%;
-    height: 40px;
-    border-radius: 6px;
-    font-weight: 700;
-    font-size: 18px;
-    cursor: pointer;
-    color: #fff;
-  }
-  .modal-body .tab span.active {
-    background: #304352;
-  }
-  .btn-close-custom {
-    position: absolute;
-    right: 28px;
-    width: 12px;
-    height: 12px;
-    cursor: pointer;
-  }
-  .px-3 {
-    padding-left: 30px;
-    padding-right: 30px;
-  }
-  .pb-2 {
-    padding-bottom: 20px;
-  }
+    .modal-header{
+      display:flex;
+      align-items:center;
+      justify-content:start;
+      color:#fff;
+      font-weight: 700;
+      font-size: 18px;
+      padding:12px 20px;
+      margin-bottom:16px;
+      border-bottom:2px solid rgba(48, 67, 82, 0.5);
+    } 
+    .modal-header .title{
+       font-weight: 700;
+       font-size: 18px;
+       color:#fff;
+    }
+    .modal-header .btn-close{
+      position:absolute;
+      right:28px;
+      margin:0;
+    }
+    .modal-body {
+        padding:0 16px;
+    }
+    .modal-body .tab{
+      display:flex;
+      align-items:center;
+      justify-content:space-between;
+      margin-bottom:30px;
+    }
+    .modal-body .tab span{
+      display:flex;
+      align-items:center;
+      justify-content:center;
+      width:50%;
+      height:40px;
+      border-radius: 6px;
+      font-weight: 700;
+      font-size: 18px;
+      cursor:pointer;
+      color:#fff;
+    }
+    .modal-body .tab span.active{
+      background: #304352;
+    }
+   .btn-close-custom{
+      position:absolute;
+      right:28px;
+      width:12px;
+      height:12px;
+      cursor:pointer;
+    }
 `;
 /** base tool start  */
 let accountId = context.accountId;
 if (!accountId) {
   return <Widget src="juaner.near/widget/ref_account-signin" />;
 }
-
-let BURROW_CONTRACT = "contract.main.burrow.near";
-
 let MAX_RATIO = 10_000;
 let B = Big();
 B.DP = 60; // set precision to 60 decimals
@@ -132,22 +123,13 @@ const shrinkToken = (value, decimals) => {
 const expandToken = (value, decimals) => {
   return new Big(value).mul(new Big(10).pow(decimals));
 };
-
-const getAccount = () => {
-  if (!accountId) return null;
-  const account = Near.view(BURROW_CONTRACT, "get_account", {
-    account_id: accountId,
-  });
-  return account;
-};
-
 const formatToken = (v) => Math.floor(v * 10_000) / 10_000;
 const { showModal, closeModal, selectedTokenId } = props;
 const {
   assets,
   rewards,
   balances,
-  account: accountProps,
+  account,
   amount,
   hasError,
   hasHFError,
@@ -155,20 +137,10 @@ const {
   closeButtonBase64,
   isMax,
 } = state;
-
-let account = accountProps;
-
-if (!account) {
-  account = getAccount();
-}
-
-State.init({
-  amount: "0",
-});
 const hasData = assets.length > 0 && rewards.length > 0;
 if (!showModal) {
   State.update({
-    amount: "0",
+    amount: "",
     hasError: false,
     hasHFError: false,
     newHealthFactor: "",
@@ -241,14 +213,13 @@ function getHealthFactor() {
   const healthFactor = B(adjustedCollateralSum)
     .div(B(adjustedBorrowedSum))
     .mul(100)
-    .toFixed(2);
+    .toFixed(0);
   return Number(healthFactor) < MAX_RATIO ? healthFactor : MAX_RATIO;
 }
-
-const oldHeathFactor = getHealthFactor();
+const healthFactor = getHealthFactor();
 
 const recomputeHealthFactor = (tokenId, amount) => {
-  if (!tokenId || !assets || !account) return null;
+  if (!tokenId || !amount || !assets) return null;
   const asset = assets.find((a) => a.token_id === tokenId);
   const decimals = asset.metadata.decimals + asset.config.extra_decimals;
   const accountCollateralAsset = account.collateral.find(
@@ -287,7 +258,7 @@ const recomputeHealthFactor = (tokenId, amount) => {
     newHealthFactor = B(adjustedCollateralSum)
       .div(B(adjustedBorrowedSum))
       .mul(100)
-      .toFixed(2);
+      .toFixed(0);
   }
   let hFErrorStatus = false;
   if (Number(newHealthFactor) >= 0 && Number(newHealthFactor) <= 105) {
@@ -297,14 +268,6 @@ const recomputeHealthFactor = (tokenId, amount) => {
     Number(newHealthFactor) < MAX_RATIO ? newHealthFactor : MAX_RATIO;
   return [newHealthFactorAmount, hFErrorStatus];
 };
-const [healthFactor, hFErrorStatus] = recomputeHealthFactor(
-  selectedTokenId,
-  amount || 0
-);
-State.update({
-  newHealthFactor: healthFactor,
-  hasHFError: hFErrorStatus,
-});
 function computeAdjustMaxAmount() {
   if (!assets || !selectedTokenId || !account) return "0";
   const asset = assets.find((a) => a.token_id === selectedTokenId);
@@ -338,7 +301,7 @@ const remainCollateral$ = B(asset.price.usd || 0)
   .mul(remainCollateral)
   .toFixed(2);
 const buttonDisabled = !(
-  Number(amount) >= 0 &&
+  Number(amount) &&
   !hasError &&
   Number(newHealthFactor) > 100
 );
@@ -356,66 +319,64 @@ return (
     {/** modal */}
     <Modal style={{ display: showModal ? "block" : "none" }}>
       <div class="modal-header">
-        <div class="modal_title">Adjust Collateral</div>
+        <div class="title">Adjust Collateral</div>
         <img
           class="btn-close-custom"
           src={closeButtonBase64}
           onClick={closeModal}
         />
       </div>
-      <div class="px-3">
-        <Widget
-          src="juaner.near/widget/ref-input-box"
-          props={{
-            amount,
-            handleAmount,
-            balance: availableBalance,
-            balance$: availableBalance$,
-            metadata: asset.metadata,
-            hideBalanceInputTop: true,
-          }}
-        />
-        {hasError && (
-          <p class="alert alert-danger mt-10" role="alert">
-            Amount greater than available
-          </p>
-        )}
-      </div>
-      <div class="separator" />
-      <div class="px-3 pb-2">
-        {hasHFError && (
-          <p class="alert alert-danger mt-10" role="alert">
-            Your health factor will be dangerously low and you're at risk of
-            liquidation
-          </p>
-        )}
-        <div class="template mt_25">
-          <span class="template_title">Health Factor</span>
-          <span class="value">{newHealthFactor || oldHeathFactor}%</span>
+      <div class="modal-body">
+        <div class="content">
+          <Widget
+            src="juaner.near/widget/ref-input-box"
+            props={{
+              amount,
+              handleAmount,
+              balance: availableBalance,
+              balance$: availableBalance$,
+            }}
+          />
+          {hasError && (
+            <p class="alert alert-danger mt-10" role="alert">
+              Amount greater than available
+            </p>
+          )}
+          {hasHFError && (
+            <p class="alert alert-danger mt-10" role="alert">
+              Your health factor will be dangerously low and you're at risk of
+              liquidation
+            </p>
+          )}
+          <div class="template mt_25">
+            <span class="title">Health Factor</span>
+            <span class="value">
+              {newHealthFactor ? newHealthFactor : healthFactor}%
+            </span>
+          </div>
+          <div class="template mt_25">
+            <span class="title">Use as Collateral</span>
+            <span class="value">
+              {B(remainCollateral).toFixed(4)}
+              <span class="usd">(${remainCollateral$ || "0"})</span>
+            </span>
+          </div>
+          <Widget
+            src="juaner.near/widget/ref-adjust-button"
+            props={{
+              onLoad,
+              selectedTokenId,
+              amount,
+              hasError,
+              buttonDisabled,
+              account,
+              onLoad,
+              assets,
+              availableBalance,
+              isMax,
+            }}
+          />
         </div>
-        <div class="template mt_25">
-          <span class="template_title">Use as Collateral</span>
-          <span class="value">
-            {B(remainCollateral).toFixed(4)}
-            <span class="usd">(${remainCollateral$ || "0"})</span>
-          </span>
-        </div>
-        <Widget
-          src="juaner.near/widget/ref-adjust-button"
-          props={{
-            onLoad,
-            selectedTokenId,
-            amount,
-            hasError,
-            buttonDisabled,
-            account,
-            onLoad,
-            assets,
-            availableBalance,
-            isMax,
-            closeModal,
-          }}
-        />
       </div>
     </Modal>
     <Backdrop
