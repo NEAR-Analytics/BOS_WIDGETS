fec8b72b0e0dbf65365f1bd663304f00a77fc1c6
andyh.near <andyh.near>
2023-05-02 20:33:50 +0000

    Update RenderTestRoot by andyh.near at 2023-05-02T20:33:50.718Z

--- andyh.near/RenderTestRoot/source_code.jsx
+++ andyh.near/RenderTestRoot/source_code.jsx
@@ -1,5 +1,17 @@
+State.init({ age: 0 });
 return (
   <div>
-    <Widget src="andyh.near/widget/RenderTestChild" />
+    I am {state.age} yars old
+    <button
+      onClick={() => {
+        State.update({ age: state.age + 1 });
+      }}
+    >
+      age me
+    </button>
+    <Widget
+      src="andyh.near/widget/RenderTestChild"
+      props={{ age: state.age }}
+    />
   </div>
 );
