9d2eaedf023d14b906426fdb03f6b749965917d8
neardigitalcollective.near <neardigitalcollective.near>
2023-05-03 02:05:50 +0000

    Update WikiOnSocialDB_Comment.Feed by neardigitalcollective.near at 2023-05-03T02:05:50.238Z

--- neardigitalcollective.near/WikiOnSocialDB_Comment.Feed/source_code.jsx
+++ neardigitalcollective.near/WikiOnSocialDB_Comment.Feed/source_code.jsx
@@ -1,5 +1,5 @@
 const addressForComments = "NDCDOCS-comments";
-const addressForArticles = "NDCDOCS";
+const addressForArticles = "ndcWikiArticles";
 const authorForWidget = "neardigitalcollective.near";
 const index = {
   action: addressForComments,
