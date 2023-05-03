a623a8d392710392c6aa62b91befcc0895a7e3ec
neardigitalcollective.near <neardigitalcollective.near>
2023-05-03 00:41:17 +0000

    Update WikiOnSocialDB_OneArticle.Footer by neardigitalcollective.near at 2023-05-03T00:41:17.684Z

---
+++ neardigitalcollective.near/WikiOnSocialDB_OneArticle.Footer/source_code.jsx
@@ -0,0 +1,32 @@
+const { author, lastEditor, timeLastEdit, version } = props;
+const getDate = (timestamp) => {
+  const date = new Date(Number(timestamp));
+  return date.toDateString();
+};
+
+return (
+  <div className="alert alert-secondary bg-white">
+    <div>
+      Created by{" "}
+      <a
+        href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${author}`}
+        target="_blank"
+        style={{ textDecoration: "underline" }}
+      >
+        {author}
+      </a>
+      <br />
+      Last edit by{" "}
+      <a
+        href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${lastEditor}`}
+        style={{ textDecoration: "underline" }}
+      >
+        {lastEditor}
+      </a>
+      <br />
+      Edited on {getDate(timeLastEdit)}
+      <br />
+      Edit versions: {version}
+    </div>
+  </div>
+);
