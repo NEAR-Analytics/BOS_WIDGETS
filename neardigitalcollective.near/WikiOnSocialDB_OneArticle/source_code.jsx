b9cd28d0df3ec3043e78320012b659906b8de967
neardigitalcollective.near <neardigitalcollective.near>
2023-05-03 02:21:12 +0000

    Update WikiOnSocialDB_OneArticle by neardigitalcollective.near at 2023-05-03T02:21:12.136Z

--- neardigitalcollective.near/WikiOnSocialDB_OneArticle/source_code.jsx
+++ neardigitalcollective.near/WikiOnSocialDB_OneArticle/source_code.jsx
@@ -1,5 +1,5 @@
 const addressForComments = "NDCDOCS-comments";
-const addressForArticles = "ndcWikiArticles";
+const addressForArticles = "ndcWikiArticle";
 const authorForWidget = "neardigitalcollective.near";
 const accountId = props.accountId ?? context.accountId;
 if (!accountId) {
