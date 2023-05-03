b574773268dd6bb89ddd7d42f4a9e6a79065dc18
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-05-03 23:25:45 +0000

    Update kudoBox by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-05-03T23:25:45.425Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/kudoBox/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/kudoBox/source_code.jsx
@@ -1,6 +1,8 @@
 const d = props.d;
 const index = props.index;
 
+const commentSharedBlockHeight = props.commentSharedBlockHeight ?? undefined;
+
 const tabs = props.tabs;
 const oppenedTab = props.oppenedTab;
 
@@ -12,93 +14,110 @@ const upvotes = props.upvotes;
 
 State.init({
   hoveringElement: "",
+  onlyShowShared: commentSharedBlockHeight ? true : false,
   showComments: false,
 });
 
-const item = {
-  type: "social",
-  path: `${d.accountId}/post/main`,
-  blockHeight: d.blockHeight,
-};
-
 const widgetOwner = props.widgetOwner;
 
-const RenderAllCommentAnswerBox = (d) => {
+function getAnswersContainerStyles() {
+  let styles = thisWidgetInlineStyles.allCommentAnswerBox.cardsContainer;
+
+  styles["zIndex"] = `${999999999 - index}`;
+  return styles;
+}
+
+function getAnswerContainerStyle() {
+  let style = thisWidgetInlineStyles.allCommentAnswerBox.cardContainer;
+
+  if (d.blockHeight == commentSharedBlockHeight) {
+    styles["boxShadow"] = `0px 0px 49px 1px rgba(45,255,51,0.47) inset`;
+  }
+
+  return style;
+}
+console.log(d);
+const RenderCommentAnswerBox = (d) => {
   return (
     <>
       {state.showComments && (
-        <div style={thisWidgetInlineStyles.allCommentAnswerBox.cardsContainer}>
+        <div style={getAnswersContainerStyles()}>
           {d.value.comments.map((c) => {
-            return (
-              <>
-                <div
-                  style={
-                    thisWidgetInlineStyles.allCommentAnswerBox.cardContainer
-                  }
-                  className={
-                    oppenedTab == tabs.KUDO.id
-                      ? thisWidgetClassNames.allCommentAnswerBox
-                          .cardContainerSingleCard
-                      : thisWidgetClassNames.allCommentAnswerBox.cardContainer
-                  }
-                >
+            if (
+              (state.onlyShowShared &&
+                d.blockHeight == commentSharedBlockHeight) ||
+              !state.onlyShowShared
+            ) {
+              return (
+                <>
                   <div
+                    style={getAnswerContainerStyle()}
                     className={
-                      thisWidgetClassNames.allCommentAnswerBox.userAnswerHeader
+                      oppenedTab == tabs.KUDO.id
+                        ? thisWidgetClassNames.allCommentAnswerBox
+                            .cardContainerSingleCard
+                        : thisWidgetClassNames.allCommentAnswerBox.cardContainer
                     }
                   >
-                    <Widget
-                      src="mob.near/widget/ProfileImage"
-                      props={{
-                        accountId: c.accountId,
-                        className: "d-inline-block",
-                        style:
-                          thisWidgetInlineStyles.allCommentAnswerBox
-                            .profileImageStyles,
-                      }}
-                    />
-                    <a
-                      style={
-                        thisWidgetInlineStyles.allCommentAnswerBox
-                          .commentUserNick
-                      }
-                      href={`#/mob.near/widget/ProfilePage?accountId=${c.accountId}`}
-                    >
-                      {c.accountId}
-                    </a>
                     <div
-                      style={
-                        props.allWidgetsInlineStyles.mainPage_post
-                          .followButtonContainer
+                      className={
+                        thisWidgetClassNames.allCommentAnswerBox
+                          .userAnswerHeader
                       }
                     >
                       <Widget
-                        src={`${widgetOwner}/widget/FollowButton`}
-                        props={{ accountId: c.accountId }}
+                        src="mob.near/widget/ProfileImage"
+                        props={{
+                          accountId: c.accountId,
+                          className: "d-inline-block",
+                          style:
+                            thisWidgetInlineStyles.allCommentAnswerBox
+                              .profileImageStyles,
+                        }}
                       />
+                      <a
+                        style={
+                          thisWidgetInlineStyles.allCommentAnswerBox
+                            .commentUserNick
+                        }
+                        href={`#/mob.near/widget/ProfilePage?accountId=${c.accountId}`}
+                      >
+                        {c.accountId}
+                      </a>
+                      <div
+                        style={
+                          props.allWidgetsInlineStyles.mainPage_post
+                            .followButtonContainer
+                        }
+                      >
+                        <Widget
+                          src={`${widgetOwner}/widget/FollowButton`}
+                          props={{ accountId: c.accountId }}
+                        />
+                      </div>
                     </div>
+                    <b
+                      style={thisWidgetInlineStyles.allCommentAnswerBox.comment}
+                    >
+                      {c.value.commentAnswer}&nbsp;&nbsp;&nbsp;
+                    </b>
                   </div>
-                  <b style={thisWidgetInlineStyles.allCommentAnswerBox.comment}>
-                    {c.value.commentAnswer}&nbsp;&nbsp;&nbsp;
-                  </b>
-                  {
-                    // <Widget
-                    //   src={`${widgetOwner}/widget/WikiOnSocialDB_Like`}
-                    //   props={{
-                    //     // notifyAccountId,
-                    //     item,
-                    //   }}
-                    // />
-                  }
-                </div>
-              </>
-            );
+                </>
+              );
+            }
           })}
         </div>
       )}
     </>
   );
 };
+
+function getKudoBoxContainerStyles() {
+  let styles = thisWidgetInlineStyles.renderKudoBox.cardContainer;
+
+  styles["zIndex"] = `${999999999 - index}`;
+  return styles;
+}
 
 function updateStateFunction(objetc) {
   State.update(objetc);
@@ -107,10 +126,7 @@ function updateStateFunction(objetc) {
 return (
   <>
     <div
-      style={{
-        ...thisWidgetInlineStyles.renderKudoBox.cardContainer,
-        zIndex: `${999999999 - index}`,
-      }}
+      style={getKudoBoxContainerStyles()}
       className={
         oppenedTab == tabs.ALL_kUDOS.id
           ? thisWidgetClassNames.renderKudoBox.cardContainer
@@ -129,18 +145,19 @@ return (
         }}
       />
 
-      <Widget
-        src={`${widgetOwner}/widget/showCommentsButton`}
-        props={{
-          thisWidgetInlineStyles,
-          thisWidgetClassNames,
-          fatherStateUpdate: updateStateFunction,
-          showComments: state.showComments,
-          d,
-        }}
-      />
-
-      {RenderAllCommentAnswerBox(d)}
+      {!state.onlyShowShared && (
+        <Widget
+          src={`${widgetOwner}/widget/showCommentsButton`}
+          props={{
+            thisWidgetInlineStyles,
+            thisWidgetClassNames,
+            fatherStateUpdate: updateStateFunction,
+            showComments: state.showComments,
+            d,
+          }}
+        />
+      )}
+      {RenderCommentAnswerBox(d)}
     </div>
   </>
 );
