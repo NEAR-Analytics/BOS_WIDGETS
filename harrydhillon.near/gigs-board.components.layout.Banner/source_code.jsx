9a9d60577b834057d2a6512dc00362ed9a681232
harrydhillon.near <harrydhillon.near>
2023-05-03 18:07:00 +0000

    Update gigs-board.components.layout.Banner by harrydhillon.near at 2023-05-03T18:07:00.821Z

--- harrydhillon.near/gigs-board.components.layout.Banner/source_code.jsx
+++ harrydhillon.near/gigs-board.components.layout.Banner/source_code.jsx
@@ -6,22 +6,6 @@ const nearDevGovGigsWidgetsAccountId =
   props.nearDevGovGigsWidgetsAccountId ||
   (context.widgetSrc ?? "harrydhillon.near").split("/", 1)[0];
 
-function widget(widgetName, widgetProps, key) {
-  widgetProps = {
-    ...widgetProps,
-    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
-    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
-    referral: props.referral,
-  };
-  return (
-    <Widget
-      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
-      props={widgetProps}
-      key={key}
-    />
-  );
-}
-
 function href(widgetName, linkProps) {
   linkProps = { ...linkProps };
   if (props.nearDevGovGigsContractAccountId) {
