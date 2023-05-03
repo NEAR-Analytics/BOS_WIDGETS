9785b3eb19e3a586c45648d5fde97168955b4d4a
frichard5.near <frichard5.near>
2023-05-03 15:49:06 +0000

    Update NDC-Page by frichard5.near at 2023-05-03T15:49:06.691Z

--- frichard5.near/NDC-Page/source_code.jsx
+++ frichard5.near/NDC-Page/source_code.jsx
@@ -1,6 +1,9 @@
 const widgetProvider = "frichard5.near";
 const tab = props.tab || "overview";
-const proposal_id = Number(props.proposal_id);
+const proposal_id =
+  props.proposal_id &&
+  !isNan(Number(props.proposal_id)) &&
+  Number(props.proposal_id);
 // Style
 const avenirFontFamily = fetch(
   "https://fonts.cdnfonts.com/css/avenir-lt-std"
