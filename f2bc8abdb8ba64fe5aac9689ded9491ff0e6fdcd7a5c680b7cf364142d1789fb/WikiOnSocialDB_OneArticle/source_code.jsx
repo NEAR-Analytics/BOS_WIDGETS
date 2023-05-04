7c60296231e77b3964410106423b8ed83e65a186
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-05-04 23:21:46 +0000

    Update WikiOnSocialDB_OneArticle by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-05-04T23:21:46.548Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_OneArticle/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_OneArticle/source_code.jsx
@@ -416,7 +416,7 @@ return (
             </div>
           )}
           {/* === CREATE COMMENT BUTTON === */}
-          {console.log(blockHeight)}
+          {state.showAllComments}
           {blockHeight !== "now" && (
             <div className="mt-1 d-flex justify-content-between">
               <Widget
