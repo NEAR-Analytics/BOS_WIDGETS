fb24866e56ad2d22d2582ecb849c3031cd406d80
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-05-04 21:57:43 +0000

    Update WikiOnSocialDB_Comment.Feed by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-05-04T21:57:43.903Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_Comment.Feed/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_Comment.Feed/source_code.jsx
@@ -1,5 +1,5 @@
-const addressForComments = "NDCDOCS-comments";
-const addressForArticles = "ndcWikiArticle";
+const addressForArticles = "wikiTest2Article";
+const addressForComments = "wikiTest2Comment";
 const authorForWidget =
   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 const index = {
