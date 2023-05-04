d904ac6df901d373c2b3fea81aa98000cd8f2241
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-05-04 23:15:21 +0000

    Update WikiOnSocialDB_Comment.Feed by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-05-04T23:15:21.060Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_Comment.Feed/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_Comment.Feed/source_code.jsx
@@ -1,6 +1,11 @@
 const addressForArticles = "wikiTest2Article";
 const addressForComments = "wikiTest2Comment";
 const lastEditor = props.lastEditor;
+const commentToShareBlockHeight = props.commentToShareBlockHeight
+  ? Number(props.commentToShareBlockHeight)
+  : false;
+
+const showAllComments = props.showAllComments;
 
 const authorForWidget =
   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
@@ -17,13 +22,14 @@ const index = {
 
 const raw = !!props.raw;
 
-const renderItem = (a) =>
-  a.value.type === "md" && (
+const renderItem = (a, showAllComments) =>
+  a.value.type === "md" &&
+  ((a.blockHeight === commentToShareBlockHeight && !showAllComments) ||
+    showAllComments) && (
     <div key={JSON.stringify(a)}>
       <Widget
         src={`${authorForWidget}/widget/WikiOnSocialDB_Comment`}
         props={{
-          commentToShareBlockHeight: props.commentToShareBlockHeight,
           lastEditorAccountId: lastEditor,
           wikiSiteBlockHeight: props.blockHeight,
           accountId: a.accountId,
