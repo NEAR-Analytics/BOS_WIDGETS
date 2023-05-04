cfa9f3e9c515f0b55d8d927ef585d034de66677b
nearhorizon.near <nearhorizon.near>
2023-05-04 07:49:57 +0000

    Update Project.List by nearhorizon.near at 2023-05-04T07:49:57.882Z

---
+++ nearhorizon.near/Project.List/source_code.jsx
@@ -0,0 +1,27 @@
+const ownerId = "nearhorizon.near";
+const search = props.search ?? "";
+
+State.init({
+  items: [],
+  itemsIsFetched: false,
+});
+
+if (!state.itemsIsFetched) {
+  Near.asyncView(ownerId, "get_projects", {}, "final", false).then((items) =>
+    State.update({ items, itemsIsFetched: true })
+  );
+
+  return <>Loading...</>;
+}
+
+return (
+  <Widget
+    src={`${ownerId}/widget/List`}
+    props={{
+      items: state.items.filter((accountId) => accountId.includes(search)),
+      createItem: (accountId) => (
+        <Widget src={`${ownerId}/widget/Project.Card`} props={{ accountId }} />
+      ),
+    }}
+  />
+);
