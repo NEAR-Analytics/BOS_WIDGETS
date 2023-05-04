ed87ddcfbaee50f3b38c6713093ea0fee9a96739
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-05-04 00:19:11 +0000

    Update Kudos.Styles by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-05-04T00:19:11.767Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Kudos.Styles/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Kudos.Styles/source_code.jsx
@@ -4,13 +4,15 @@ const metadata = Social.getr(`webuidl.near/widget/Kudos/metadata`);
 console.log(props);
 
 const sharedBlockHeight = Number(props.sharedBlockHeight);
-const commentSharedBlockHeight = Number(props.commentSharedBlockHeight)
-  ? undefined
-  : Number(props.commentSharedBlockHeight);
+const commentSharedBlockHeight = Number(props.commentSharedBlockHeight);
 
 const blockHeight = Number.isNaN(sharedBlockHeight)
   ? undefined
   : Number(sharedBlockHeight);
+
+const commentBlockHeight = Number.isNaN(commentSharedBlockHeight)
+  ? undefined
+  : Number(commentSharedBlockHeight);
 
 const widgetOwner =
   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
@@ -405,7 +407,7 @@ return (
       src={`${widgetOwner}/widget/Kudos`}
       props={{
         blockHeight,
-        commentSharedBlockHeight,
+        commentBlockHeight,
         widgetOwner,
         allWidgetsInlineStyles,
         allWidgetsClassNames,
