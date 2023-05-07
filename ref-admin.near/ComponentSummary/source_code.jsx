03f858f864218adf0821068608b4466a0326de9b
ref-admin.near <ref-admin.near>
2023-05-07 12:09:27 +0000

    Update ComponentSummary by ref-admin.near at 2023-05-07T12:09:27.005Z

--- ref-admin.near/ComponentSummary/source_code.jsx
+++ ref-admin.near/ComponentSummary/source_code.jsx
@@ -1,32 +1,20 @@
 if (!props.src) return "";
 
-const { customHomeLoading } = state;
 State.init({
   copiedShareUrl: false,
-  customHomeLoading: canCustomHome ? true : false,
 });
 
-const { canCustomHome, src } = props;
+const src = props.src;
 const primaryAction = props.primaryAction || "viewDetails";
-let myHomePagePath;
-if (canCustomHome || props.primaryAction) {
-  myHomePagePath = Social.get(`${context.accountId}/myHomePagePath`);
-}
-if (myHomePagePath !== null) {
-  State.update({
-    customHomeLoading: false,
-  });
-}
-if (customHomeLoading) return "";
-const finalSrc = canCustomHome ? myHomePagePath || src : src; // src 取url中的没有的话取默认值
-const [accountId, widget, widgetName] = finalSrc.split("/");
+const [accountId, widget, widgetName] = src.split("/");
 const data = Social.get(`${accountId}/widget/${widgetName}/metadata/**`);
 const metadata = data || {};
 const tags = Object.keys(metadata.tags || {});
-const appUrl = `/#/${finalSrc}`;
-const detailsUrl = `/#/ref-admin.near/widget/ComponentDetailsPage?src=${finalSrc}`;
-const shareUrl = `https://alpha.near.org${detailsUrl}`;
+const appUrl = `#/${src}`;
+const detailsUrl = `#/ref-admin.near/widget/ComponentDetailsPage?src=${src}`;
+const shareUrl = `https://near.org${detailsUrl}`;
 const size = props.size || "large";
+
 const primaryActions = {
   open: {
     display: "Open",
@@ -47,20 +35,11 @@ const sizes = {
   large: {
     gap: "16px",
     thumbnail: "100px",
-    title: "26px",
+    title: "32px",
   },
 };
-const Wrapper = styled.div`
-   background-repeat:no-repeat;
-   background-size: cover;
-   background-image:${() => {
-     return props.primaryAction
-       ? 'url("https://ipfs.near.social/ipfs/bafybeiduczlwb5wvqng2jjyifcyuyj4hs3mpfdgoex6xkswbqyviywkaje")'
-       : "none";
-   }};
-   border-radius:10px;
-   padding: 0 10px 16px 0;
-`;
+
+const Wrapper = styled.div``;
 
 const Header = styled.div`
   display: flex;
@@ -83,21 +62,17 @@ const TagsWrapper = styled.div`
 
 const Actions = styled.div`
   display: flex;
-  align-items:center;
-  justify-content:space-between;
+  gap: 12px;
   flex-wrap: wrap;
-  .actionsDiv{
-    display: "flex";
-    align-items:center;
-    gap:12px;
-  }
+  margin-bottom: 16px;
 `;
 
 const Title = styled.h1`
   font-size: ${(p) => sizes[p.size].title};
-  color: #fff;
+  line-height: 1.2em;
+  color: #11181c;
   margin: 0 0 8px;
-  font-weight: 500;
+  font-weight: 600;
 
   @media (max-width: 770px) {
     font-size: 16px;
@@ -109,6 +84,7 @@ const Thumbnail = styled.div`
   width: ${(p) => sizes[p.size].thumbnail};
   height: ${(p) => sizes[p.size].thumbnail};
   flex-shrink: 0;
+  border: 1px solid #eceef0;
   border-radius: 12px;
   overflow: hidden;
   box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
@@ -155,37 +131,26 @@ const sharedButtonStyles = `
 `;
 
 const Button = styled.button`
-//   ${sharedButtonStyles}
-//   color: ${(p) => (p.primary ? "#09342E" : "#11181C")} !important;
-//   background: ${(p) => (p.primary ? "#59E692" : "#FBFCFD")};
-//   border: ${(p) => (p.primary ? "none" : "1px solid #D7DBDF")};
+  ${sharedButtonStyles}
+  color: ${(p) => (p.primary ? "#09342E" : "#11181C")} !important;
+  background: ${(p) => (p.primary ? "#59E692" : "#FBFCFD")};
+  border: ${(p) => (p.primary ? "none" : "1px solid #D7DBDF")};
 
-//   &:hover,
-//   &:focus {
-//     background: ${(p) => (p.primary ? "rgb(112 242 164)" : "#ECEDEE")};
-//   }
-background: rgba(26, 46, 51, 0.25);
-  border: 0.5px solid rgba(255, 255, 255, 0.3);
-  border-radius: 38px;
-  color:#fff;
+  &:hover,
+  &:focus {
+    background: ${(p) => (p.primary ? "rgb(112 242 164)" : "#ECEDEE")};
+  }
 `;
 
 const ButtonLink = styled.a`
-//   ${sharedButtonStyles}
-//   color: ${(p) => (p.primary ? "#09342E" : "#11181C")} !important;
-//   background: ${(p) => (p.primary ? "#59E692" : "#FBFCFD")};
-//   border: ${(p) => (p.primary ? "none" : "1px solid #D7DBDF")};
+  ${sharedButtonStyles}
+  color: ${(p) => (p.primary ? "#09342E" : "#11181C")} !important;
+  background: ${(p) => (p.primary ? "#59E692" : "#FBFCFD")};
+  border: ${(p) => (p.primary ? "none" : "1px solid #D7DBDF")};
 
-//   &:hover,
-//   &:focus {
-//     background: ${(p) => (p.primary ? "rgb(112 242 164)" : "#ECEDEE")};
-//   }
-  background: rgba(26, 46, 51, 0.25);
-  border: 0.5px solid rgba(255, 255, 255, 0.3);
-  border-radius: 38px;
-  color:#fff;
-  &:hover{
-    color:#fff;
+  &:hover,
+  &:focus {
+    background: ${(p) => (p.primary ? "rgb(112 242 164)" : "#ECEDEE")};
   }
 `;
 
@@ -193,8 +158,8 @@ const Text = styled.p`
   margin: 0;
   font-size: 14px;
   line-height: 20px;
-  color: #fff;
-  font-weight: 500;
+  color: ${(p) => (p.bold ? "#11181C" : "#687076")};
+  font-weight: ${(p) => (p.bold ? "600" : "400")};
   font-size: ${(p) => (p.small ? "12px" : "14px")};
   overflow: ${(p) => (p.ellipsis ? "hidden" : "visible")};
   text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "unset")};
@@ -204,16 +169,13 @@ const Text = styled.p`
     margin-right: 4px;
   }
 `;
-const current_mode = Storage.get(
-  "ref-mode",
-  "ref-admin.near/widget/user-builder"
-);
+
 return (
   <Wrapper>
     <Header size={size}>
       <Thumbnail size={size}>
         <Widget
-          src="mob.near/widget/Image"
+          src="ref-admin.near/widget/Image"
           props={{
             image: metadata.image,
             fallbackUrl:
@@ -225,7 +187,7 @@ return (
 
       <div>
         <Title size={size}>{metadata.name || widgetName}</Title>
-        <Text ellipsis>{finalSrc}</Text>
+        <Text ellipsis>{src}</Text>
       </div>
     </Header>
 
@@ -239,59 +201,52 @@ return (
         />
       </TagsWrapper>
     )}
+
     <Actions>
-      <div class="actionsDiv">
-        <ButtonLink primary href={primaryActions[primaryAction].url}>
-          {primaryActions[primaryAction].display}
-        </ButtonLink>
+      <ButtonLink primary href={primaryActions[primaryAction].url}>
+        {primaryActions[primaryAction].display}
+      </ButtonLink>
 
-        <ButtonLink href={`/#/edit/${finalSrc}`}>
-          {context.accountId === accountId ? (
-            <>
-              <i className="bi bi-pencil-fill"></i> Edit
-            </>
-          ) : (
-            <>
-              <i className="bi bi-git"></i> Fork
-            </>
-          )}
-        </ButtonLink>
+      <ButtonLink href={`#/edit/${src}`}>
+        {context.accountId === accountId ? (
+          <>
+            <i className="bi bi-pencil-fill"></i> Edit
+          </>
+        ) : (
+          <>
+            <i className="bi bi-git"></i> Fork
+          </>
+        )}
+      </ButtonLink>
 
-        <ButtonLink href={`${detailsUrl}&tab=source`}>
-          <i className="bi bi-code-square"></i>
-          View Source
-        </ButtonLink>
+      <ButtonLink href={`${detailsUrl}&tab=source`}>
+        <i className="bi bi-code-square"></i>
+        View Source
+      </ButtonLink>
 
-        <OverlayTrigger
-          placement="top"
-          overlay={<Tooltip>Copy URL to clipboard</Tooltip>}
+      <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip>Copy URL to clipboard</Tooltip>}
+      >
+        <Button
+          type="button"
+          onMouseLeave={() => {
+            State.update({ copiedShareUrl: false });
+          }}
+          onClick={() => {
+            clipboard.writeText(shareUrl).then(() => {
+              State.update({ copiedShareUrl: true });
+            });
+          }}
         >
-          <Button
-            type="button"
-            onMouseLeave={() => {
-              State.update({ copiedShareUrl: false });
-            }}
-            onClick={() => {
-              clipboard.writeText(shareUrl).then(() => {
-                State.update({ copiedShareUrl: true });
-              });
-            }}
-          >
-            {state.copiedShareUrl ? (
-              <i className="bi bi-16 bi-check"></i>
-            ) : (
-              <i className="bi bi-16 bi-link-45deg"></i>
-            )}
-            Share
-          </Button>
-        </OverlayTrigger>
-      </div>
-      {props.primaryAction && (
-        <Widget
-          src="ref-admin.near/widget/apply-as-home-button"
-          props={{ src: finalSrc, istemplate: props.istemplate }}
-        ></Widget>
-      )}
+          {state.copiedShareUrl ? (
+            <i className="bi bi-16 bi-check"></i>
+          ) : (
+            <i className="bi bi-16 bi-link-45deg"></i>
+          )}
+          Share
+        </Button>
+      </OverlayTrigger>
     </Actions>
   </Wrapper>
 );
