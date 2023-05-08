5959b64c19602a544f44d8d2015fadf615dd6f12
jgodwill.near <jgodwill.near>
2023-05-08 16:10:24 +0000

    Update NftListingInput by jgodwill.near at 2023-05-08T16:10:24.798Z

--- jgodwill.near/NftListingInput/source_code.jsx
+++ jgodwill.near/NftListingInput/source_code.jsx
@@ -29,11 +29,81 @@ const Main = styled.div`
   width:100%;
   padding: 1rem;
 `;
+
+const ImgCard = styled.div`
+  height: 300px;
+`;
 // {props.state.tokenId && (
 return (
   <>
     <div className="container-fluid">
       <Main>
+        <BorderedShadowedCard>
+          <ImgCard className="shadow-sm p-3 mb-5 bg-body rounded">
+            <Widget
+              src="mob.near/widget/NftImage"
+              props={{
+                nft: {
+                  tokenId: props.state.tokenId,
+                  contractId: props.state.contractId,
+                },
+                className: "col-lg-12",
+              }}
+            />
+          </ImgCard>
+          <p>
+            Collection Name:{" "}
+            <span className="font-weight-bold">
+              {props.state.nftMetadata.name}
+            </span>
+          </p>
+          <p>
+            NFT Name:{" "}
+            <span className="">{props.state.tokenInfo.metadata.title}</span>
+          </p>
+          <p className="">
+            Description: {props.state.tokenInfo.metadata.description}
+          </p>
+          <p>
+            <a href={props.state.tokenInfo.media} target="_blank">
+              {props.state.tokenInfo.media}
+            </a>
+          </p>
+          {!props.state.ownsNFT && (
+            <div className="alert alert-danger">
+              <i className="bi bi-x"></i> You do not own this NFT & cannot list
+              or transfer it
+            </div>
+          )}
+          {props.state.ownsNFT && (
+            <div className="alert alert-success">
+              <i className="bi bi-x"></i> You own this NFT
+            </div>
+          )}
+          <div className="col-lg-12">
+            <h3> Listed Markets</h3>
+            <div>
+              <ul>
+                {typeof props.state.tokenInfo.approved_account_ids ===
+                  "object" &&
+                  Object.keys(props.state.tokenInfo.approved_account_ids).map(
+                    (key) => (
+                      <li>
+                        <a
+                          href={"https://explorer.near.org/accounts/" + key}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {key}:{" "}
+                          {props.state.tokenInfo.approved_account_ids[key]}
+                        </a>
+                      </li>
+                    )
+                  )}
+              </ul>
+            </div>
+          </div>
+        </BorderedShadowedCard>
         <div className="">
           <div className="">
             ContractID
@@ -139,72 +209,6 @@ return (
             </div>
           </div>
         </div>
-        <BorderedShadowedCard>
-          <div className="shadow-sm p-3 mb-5 bg-body rounded">
-            <Widget
-              src="mob.near/widget/NftImage"
-              props={{
-                nft: {
-                  tokenId: props.state.tokenId,
-                  contractId: props.state.contractId,
-                },
-                className: "col-lg-12",
-              }}
-            />
-          </div>
-          <p>
-            Collection Name:{" "}
-            <span className="font-weight-bold">
-              {props.state.nftMetadata.name}
-            </span>
-          </p>
-          <p>
-            NFT Name:{" "}
-            <span className="">{props.state.tokenInfo.metadata.title}</span>
-          </p>
-          <p className="">
-            Description: {props.state.tokenInfo.metadata.description}
-          </p>
-          <p>
-            <a href={props.state.tokenInfo.media} target="_blank">
-              {props.state.tokenInfo.media}
-            </a>
-          </p>
-          {!props.state.ownsNFT && (
-            <div className="alert alert-danger">
-              <i className="bi bi-x"></i> You do not own this NFT & cannot list
-              or transfer it
-            </div>
-          )}
-          {props.state.ownsNFT && (
-            <div className="alert alert-success">
-              <i className="bi bi-x"></i> You own this NFT
-            </div>
-          )}
-          <div className="col-lg-12">
-            <h3> Listed Markets</h3>
-            <div>
-              <ul>
-                {typeof props.state.tokenInfo.approved_account_ids ===
-                  "object" &&
-                  Object.keys(props.state.tokenInfo.approved_account_ids).map(
-                    (key) => (
-                      <li>
-                        <a
-                          href={"https://explorer.near.org/accounts/" + key}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {key}:{" "}
-                          {props.state.tokenInfo.approved_account_ids[key]}
-                        </a>
-                      </li>
-                    )
-                  )}
-              </ul>
-            </div>
-          </div>
-        </BorderedShadowedCard>
       </Main>
       <div className="row text-center">
         {props.state.ownsNFT && (
