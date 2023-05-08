906d41dd0b8557afc537ad54a4ce7d1efeaf838e
jgodwill.near <jgodwill.near>
2023-05-08 06:13:28 +0000

    Update NftListingInput by jgodwill.near at 2023-05-08T06:13:28.187Z

--- jgodwill.near/NftListingInput/source_code.jsx
+++ jgodwill.near/NftListingInput/source_code.jsx
@@ -32,206 +32,205 @@ const Main = styled.div`
   width:100%;
   padding: 1rem;
 `;
+// {props.state.tokenId && (
 return (
   <>
-    {!props.state.tokenId && (
-      <div className="container-fluid">
-        <Main>
+    <div className="container-fluid">
+      <Main>
+        <div className="">
           <div className="">
-            <div className="">
-              ContractID
-              <input
-                type="text"
-                placeholder={props.state.contractId}
-                onChange={(e) => props.onChangeContract(e.target.value)}
-              />
-            </div>
-            <div className="">
-              Token ID
-              <input
-                type="text"
-                placeholder={props.state.tokenId}
-                onChange={(e) => props.onChangeToken(e.target.value)}
-              />
-            </div>
+            ContractID
+            <input
+              type="text"
+              placeholder={props.state.contractId}
+              onChange={(e) => props.onChangeContract(e.target.value)}
+            />
+          </div>
+          <div className="">
+            Token ID
+            <input
+              type="text"
+              placeholder={props.state.tokenId}
+              onChange={(e) => props.onChangeToken(e.target.value)}
+            />
+          </div>
 
-            <div className="">
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  checked={props.state.fewfar}
-                  onChange={props.selectFewFar}
-                  id="fewfarbox"
-                />
-                <label className="form-check-label" htmlFor="myCheckbox">
-                  List to Few and Far
-                </label>
-              </div>
-            </div>
-            {false && (
-              <div className="">
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    checked={props.state.mintbase}
-                    onChange={props.selectMintbase}
-                    id="mintbasebox"
-                  />
-                  <label className="form-check-label" htmlFor="myCheckbox">
-                    List to Mintbase
-                  </label>
-                </div>
-              </div>
-            )}
-            <div className="">
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  checked={props.state.tradeport}
-                  onChange={props.selectTradeport}
-                  id="tradeportbox"
-                />
-                <label className="form-check-label" htmlFor="myCheckbox">
-                  List to Tradeport
-                </label>
-              </div>
-            </div>
-            <div className="">
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  checked={props.state.custom}
-                  onChange={props.selectCustom}
-                  id="custombox"
-                />
-                <label className="form-check-label" htmlFor="myCheckbox">
-                  Enter Custom Marketplace Address
-                </label>
-              </div>
-              {props.state.custom && (
-                <div className="">
-                  Custom Marketplace
-                  <input
-                    type="text"
-                    placeholder={props.state.customMarketLink}
-                    onChange={(e) => props.onChangeCustomMarket(e.target.value)}
-                  />
-                </div>
-              )}
-              {props.state.custom && !props.state.validMarketLink && (
-                <div className="alert alert-danger">
-                  <i className="bi bi-x"></i> Not a Valid NEAR Contract for your
-                  custom Marketplace
-                </div>
-              )}
-              <div className=" mb-2">
-                Enter Price You Want to List (In NEAR)
-                <input
-                  type="number"
-                  placeholder={props.state.amount / 1e24}
-                  onChange={(e) => props.onChangeAmount(e.target.value)}
-                />
-                <p>
-                  * You will pay some gas in Ⓝ to deposit NEAR to marketplace
-                  address then list your NFT
-                </p>
-              </div>
+          <div className="">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                checked={props.state.fewfar}
+                onChange={props.selectFewFar}
+                id="fewfarbox"
+              />
+              <label className="form-check-label" htmlFor="myCheckbox">
+                List to Few and Far
+              </label>
             </div>
           </div>
-          <BorderedShadowedCard>
-            <div className="shadow-sm p-3 mb-5 bg-body rounded">
-              <Widget
-                src="mob.near/widget/NftImage"
-                props={{
-                  nft: {
-                    tokenId: props.state.tokenId,
-                    contractId: props.state.contractId,
-                  },
-                  className: "col-lg-12",
-                }}
+          {false && (
+            <div className="">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  checked={props.state.mintbase}
+                  onChange={props.selectMintbase}
+                  id="mintbasebox"
+                />
+                <label className="form-check-label" htmlFor="myCheckbox">
+                  List to Mintbase
+                </label>
+              </div>
+            </div>
+          )}
+          <div className="">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                checked={props.state.tradeport}
+                onChange={props.selectTradeport}
+                id="tradeportbox"
               />
+              <label className="form-check-label" htmlFor="myCheckbox">
+                List to Tradeport
+              </label>
             </div>
-            <p>
-              Collection Name:{" "}
-              <span className="font-weight-bold">
-                {props.state.nftMetadata.name}
-              </span>
-            </p>
-            <p>
-              NFT Name:{" "}
-              <span className="">{props.state.tokenInfo.metadata.title}</span>
-            </p>
-            <p className="">
-              Description: {props.state.tokenInfo.metadata.description}
-            </p>
-            <p>
-              <a href={props.state.tokenInfo.media} target="_blank">
-                {props.state.tokenInfo.media}
-              </a>
-            </p>
-            {!props.state.ownsNFT && (
+          </div>
+          <div className="">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                checked={props.state.custom}
+                onChange={props.selectCustom}
+                id="custombox"
+              />
+              <label className="form-check-label" htmlFor="myCheckbox">
+                Enter Custom Marketplace Address
+              </label>
+            </div>
+            {props.state.custom && (
+              <div className="">
+                Custom Marketplace
+                <input
+                  type="text"
+                  placeholder={props.state.customMarketLink}
+                  onChange={(e) => props.onChangeCustomMarket(e.target.value)}
+                />
+              </div>
+            )}
+            {props.state.custom && !props.state.validMarketLink && (
               <div className="alert alert-danger">
-                <i className="bi bi-x"></i> You do not own this NFT & cannot
-                list or transfer it
+                <i className="bi bi-x"></i> Not a Valid NEAR Contract for your
+                custom Marketplace
               </div>
             )}
-            {props.state.ownsNFT && (
-              <div className="alert alert-success">
-                <i className="bi bi-x"></i> You own this NFT
-              </div>
-            )}
-            <div className="col-lg-12">
-              <h3> Listed Markets</h3>
-              <div>
-                <ul>
-                  {typeof props.state.tokenInfo.approved_account_ids ===
-                    "object" &&
-                    Object.keys(props.state.tokenInfo.approved_account_ids).map(
-                      (key) => (
-                        <li>
-                          <a
-                            href={"https://explorer.near.org/accounts/" + key}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            {key}:{" "}
-                            {props.state.tokenInfo.approved_account_ids[key]}
-                          </a>
-                        </li>
-                      )
-                    )}
-                </ul>
-              </div>
+            <div className=" mb-2">
+              Enter Price You Want to List (In NEAR)
+              <input
+                type="number"
+                placeholder={props.state.amount / 1e24}
+                onChange={(e) => props.onChangeAmount(e.target.value)}
+              />
+              <p>
+                * You will pay some gas in Ⓝ to deposit NEAR to marketplace
+                address then list your NFT
+              </p>
             </div>
-          </BorderedShadowedCard>
-        </Main>
-        <div className="row text-center">
-          {props.state.ownsNFT && (
-            <button className="btn btn-primary mt-3" onClick={props.list}>
-              List
-            </button>
-          )}
-
-          {!props.state.ownsNFT && (
-            <button className="btn btn-secondary mt-3">
-              You Can Only List An NFT You Own
-            </button>
-          )}
-
-          <a
-            href={props.state.tradeportLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            class="btn btn-dark mt-3"
-          >
-            View on Tradeport
-          </a>
+          </div>
         </div>
+        <BorderedShadowedCard>
+          <div className="shadow-sm p-3 mb-5 bg-body rounded">
+            <Widget
+              src="mob.near/widget/NftImage"
+              props={{
+                nft: {
+                  tokenId: props.state.tokenId,
+                  contractId: props.state.contractId,
+                },
+                className: "col-lg-12",
+              }}
+            />
+          </div>
+          <p>
+            Collection Name:{" "}
+            <span className="font-weight-bold">
+              {props.state.nftMetadata.name}
+            </span>
+          </p>
+          <p>
+            NFT Name:{" "}
+            <span className="">{props.state.tokenInfo.metadata.title}</span>
+          </p>
+          <p className="">
+            Description: {props.state.tokenInfo.metadata.description}
+          </p>
+          <p>
+            <a href={props.state.tokenInfo.media} target="_blank">
+              {props.state.tokenInfo.media}
+            </a>
+          </p>
+          {!props.state.ownsNFT && (
+            <div className="alert alert-danger">
+              <i className="bi bi-x"></i> You do not own this NFT & cannot list
+              or transfer it
+            </div>
+          )}
+          {props.state.ownsNFT && (
+            <div className="alert alert-success">
+              <i className="bi bi-x"></i> You own this NFT
+            </div>
+          )}
+          <div className="col-lg-12">
+            <h3> Listed Markets</h3>
+            <div>
+              <ul>
+                {typeof props.state.tokenInfo.approved_account_ids ===
+                  "object" &&
+                  Object.keys(props.state.tokenInfo.approved_account_ids).map(
+                    (key) => (
+                      <li>
+                        <a
+                          href={"https://explorer.near.org/accounts/" + key}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {key}:{" "}
+                          {props.state.tokenInfo.approved_account_ids[key]}
+                        </a>
+                      </li>
+                    )
+                  )}
+              </ul>
+            </div>
+          </div>
+        </BorderedShadowedCard>
+      </Main>
+      <div className="row text-center">
+        {props.state.ownsNFT && (
+          <button className="btn btn-primary mt-3" onClick={props.list}>
+            List
+          </button>
+        )}
+
+        {!props.state.ownsNFT && (
+          <button className="btn btn-secondary mt-3">
+            You Can Only List An NFT You Own
+          </button>
+        )}
+
+        <a
+          href={props.state.tradeportLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          class="btn btn-dark mt-3"
+        >
+          View on Tradeport
+        </a>
       </div>
-    )}
+    </div>
   </>
 );
