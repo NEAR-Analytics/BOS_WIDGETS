3fa549309e617ddd048e3d45bad325c3e5dd2058
contribut3.near <contribut3.near>
2023-05-08 17:20:38 +0000

    Update Card by contribut3.near at 2023-05-08T17:20:38.463Z

--- contribut3.near/Card/source_code.jsx
+++ contribut3.near/Card/source_code.jsx
@@ -65,7 +65,7 @@ const CardFooter = styled.div`
 `;
 
 return (
-  <CardContainer full={full} key={props.key}>
+  <CardContainer full={full}>
     <CardHeader show={!!props.header}>{header}</CardHeader>
     <CardBody>{body}</CardBody>
     <CardFooter>{footer}</CardFooter>
