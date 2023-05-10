d0b5001c8befade6eb3f60aa6a70d3e3bf5b2834
juaner.near <juaner.near>
2023-05-10 12:33:25 +0000

    Update ref-market-supply-withdraw by juaner.near at 2023-05-10T12:33:25.991Z

--- juaner.near/ref-market-supply-withdraw/source_code.jsx
+++ juaner.near/ref-market-supply-withdraw/source_code.jsx
@@ -220,7 +220,7 @@ function getHealthFactor() {
   const healthFactor = B(adjustedCollateralSum)
     .div(B(adjustedBorrowedSum))
     .mul(100)
-    .toFixed(0);
+    .toFixed(2);
   return Number(healthFactor) < MAX_RATIO ? healthFactor : MAX_RATIO;
 }
 const healthFactor = getHealthFactor();
@@ -274,7 +274,7 @@ const recomputeHealthFactor = (tokenId, amount) => {
     newHealthFactor = B(adjustedCollateralSum)
       .div(B(adjustedBorrowedSum))
       .mul(100)
-      .toFixed(0);
+      .toFixed(2);
   }
   return Number(newHealthFactor) < MAX_RATIO ? newHealthFactor : MAX_RATIO;
 };
