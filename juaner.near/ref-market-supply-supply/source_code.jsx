6f8cf4d8977f5c36fd8d83c06ad63b5840827ab0
juaner.near <juaner.near>
2023-05-10 12:32:23 +0000

    Update ref-market-supply-supply by juaner.near at 2023-05-10T12:32:23.990Z

--- juaner.near/ref-market-supply-supply/source_code.jsx
+++ juaner.near/ref-market-supply-supply/source_code.jsx
@@ -371,7 +371,7 @@ function getHealthFactor() {
   const healthFactor = B(adjustedCollateralSum)
     .div(B(adjustedBorrowedSum))
     .mul(100)
-    .toFixed(0);
+    .toFixed(2);
   return Number(healthFactor) < MAX_RATIO ? healthFactor : MAX_RATIO;
 }
 const healthFactor = getHealthFactor();
@@ -422,7 +422,7 @@ const recomputeHealthFactor = (tokenId, amount) => {
     newHealthFactor = B(adjustedCollateralSum)
       .div(B(adjustedBorrowedSum))
       .mul(100)
-      .toFixed(0);
+      .toFixed(2);
   }
   return Number(newHealthFactor) < MAX_RATIO ? newHealthFactor : MAX_RATIO;
 };
