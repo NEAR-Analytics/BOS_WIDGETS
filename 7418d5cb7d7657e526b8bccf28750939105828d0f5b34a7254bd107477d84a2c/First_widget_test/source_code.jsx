c4435e45fae2ba92affb239bb9ad04580e352f0e
7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c <7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c>
2023-05-10 11:33:14 +0000

    Update First_widget_test by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-05-10T11:33:14.534Z

--- 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/First_widget_test/source_code.jsx
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/First_widget_test/source_code.jsx
@@ -1,10 +1,3 @@
-const pathKeyFrames = (value) => keyframes` 
-0% {opacity: 0}
-50% {opacity: 0}
-80% {opacity: 0}
-95% {opacity: 1}
-100% { stroke-dashoffset: ${value}; opacity: 1;}`;
-
 const MeterStatusContainer = styled.div`
   position: relative;
   display: flex;
@@ -13,10 +6,10 @@ const MeterStatusContainer = styled.div`
   background-color: white;
   border-radius: 6px;
   box-shadow: 0 5px 5px -4px rgba(49, 65, 97, 0.08);
-  @media @media (min-width: 768px) {
+   @media (min-width: 768px) {
     flex-flow: row;
   }
-  @media @media (min-width: 1024px) {
+   @media (min-width: 1024px) {
     flex-flow: column;
     grid-area: heatstatus;
   }
@@ -84,14 +77,19 @@ const MeterWrapper = styled.div`
 
 const HeatSatWrapper = styled.div`
   position: relative;
-  width: 116px;
-  min-height: 80px;
-  height: 80px;
+  width: 126px;
+  min-height: 120px;
+  height: 120px;
   display: flex;
   align-items: flex-end;
   justify-content: center;
-  
-  
+  svg {
+    position: absolute;
+    z-index: 5;
+  }
+  #animated-circle {
+    z-index: 15;
+  }
 `;
 
 const HeatStatInfo = styled.div`
@@ -154,30 +152,105 @@ const GreyCircle = styled.div`
 `;
 
 let user_account = context.accountId;
+
 let register_users_num = Near.view("registry.i-am-human.near", "sbt_supply", {
   issuer: "gooddollar-v1.i-am-human.near",
 });
 
-function calcHeatStatusAnimationPercentage(value) {
-  let startingDashoffsetValue = 400;
-  let onePercentOfDashOffset = 2;
-  let totalAnimationDashOffsetValue =
-    startingDashoffsetValue + onePercentOfDashOffset * value;
-  return totalAnimationDashOffsetValue;
+let size = props.size;
+
+const sizeProps = {
+  small: {
+    widthAndHeight: 100,
+    d: "M 10,50 A 40,40 0 1 1 90,50 A 40,40 0 1 1 10,50 Z",
+    strokeAndFrom: 251.2,
+    strokeWidth: 5,
+  },
+  medium: {
+    widthAndHeight: 150,
+    d: "M 20,75 A 55,55 0 1 1 130,75 A 55,55 0 1 1 20,75 Z",
+    strokeAndFrom: 344.7,
+    strokeWidth: 8,
+  },
+  large: {
+    widthAndHeight: 200,
+  },
+};
+
+function calcHeatStatusAnimationPercentage(
+  totalNumberOfTokens,
+  totalIssuedTokens
+) {
+  let totalPercentageOfIssuedTokens = Math.round(
+    (totalIssuedTokens / totalNumberOfTokens) * 100
+  );
+  //10.3
+
+  let startingDashoffsetValue = sizeProps[size].strokeAndFrom;
+
+  let onePercentageOfDash = startingDashoffsetValue / 100;
+
+  let fullCircle = startingDashoffsetValue / 2 + onePercentageOfDash;
+  console.log("fullCircle", fullCircle);
+  let onePercentageOfCircle = fullCircle / 100;
+
+  console.log("fullCircle", fullCircle);
+  console.log("onePercentageOfCircle", onePercentageOfCircle);
+
+  let finalvalueOfCircle =
+    onePercentageOfCircle * totalPercentageOfIssuedTokens;
+
+  let fin = Math.round(startingDashoffsetValue - finalvalueOfCircle);
+  console.log("fin", fin);
+  return fin;
+  // let tokensPercentage = totalNumberOfTokens / 10; // calc percentage  x / 1000  * 100
+
+  // let onePercentOfDashOffset = startingDashoffsetValue / 100;
 }
 
+const value = calcHeatStatusAnimationPercentage(1000, 1000);
+//173
 return (
   <MeterWrapper>
     <MeterStatusContainer>
       <HeatStatusBox>
-        <HeatSatWrapper
-          heat={
-            register_users_num
-              ? calcHeatStatusAnimationPercentage(register_users_num)
-              : 0
-          }
-        >
-          <GreyCircle />
+        <HeatSatWrapper>
+          <svg
+            width={sizeProps[size].widthAndHeight}
+            height={sizeProps[size].widthAndHeight}
+            id="animated-circle"
+          >
+            <path
+              id="circle-path"
+              d={sizeProps[size].d}
+              stroke="#FFD50D"
+              stroke-width={sizeProps[size].strokeWidth + 1}
+              fill="none"
+              stroke-dasharray={sizeProps[size].strokeAndFrom}
+              stroke-dashoffset={sizeProps[size].strokeAndFrom}
+            >
+              <animate
+                attributeName="stroke-dashoffset"
+                from={sizeProps[size].strokeAndFrom}
+                to={value}
+                dur="1s"
+                fill="freeze"
+              />
+            </path>
+          </svg>
+
+          <svg
+            width={sizeProps[size].widthAndHeight}
+            height={sizeProps[size].widthAndHeight}
+          >
+            <path
+              d={sizeProps[size].d}
+              stroke="#4498E0"
+              stroke-width={sizeProps[size].strokeWidth}
+              fill="none"
+            />
+          </svg>
+
           <HeatInfoBox>
             <p>{register_users_num}</p>
             <p>/1000</p>
