1b354a2ba9b355300a86cfa3de2e13062ca05894
juaner.near <juaner.near>
2023-05-11 06:44:45 +0000

    Update ref-input-box by juaner.near at 2023-05-11T06:44:45.512Z

--- juaner.near/ref-input-box/source_code.jsx
+++ juaner.near/ref-input-box/source_code.jsx
@@ -1,143 +1,144 @@
 const Container = styled.div`
-  padding-bottom: 30px;
-  .inputArea {
-    position: relative;
-    display: flex;
-    align-items: center;
-    border-radius: 12px;
-    margin-bottom: 4px;
-    background-color: #373a53;
-    .tokenIcon {
-      width: 26px;
-      height: 26px;
-      border-radius: 100px;
-      margin-left: 16px;
+    padding-bottom:30px;
+    .inputArea{
+      position:relative;
+      display:flex;
+      align-items:center;
+      background: rgb(21, 37, 40);
+      border-radius: 12px;
+      margin-bottom:4px;
+      .tokenIcon{
+        width: 26px;
+        height: 26px;
+        border-radius:100px;
+        margin-left:16px;
+      }
     }
-  }
-  .maxButton {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    right: 14px;
-    width: 40px;
-    height: 25px;
-    font-size: 12px;
-    line-height: 16px;
-    color: #7c7f96;
-    border: 1px solid #7c7f96;
-    border-radius: 6px;
-    cursor: pointer;
-  }
-  .maxButton:hover {
-    color: #5f8399;
-    border: 1px solid #5f8399;
-  }
-  .normalInput {
-    width: 100%;
-    background-color: #373a53;
-    border-radius: 12px;
-    height: 55px;
-    font-size: 20px;
-    color: #fff;
-    padding: 0 60px 0 0;
-    padding: 0 60px 0 4px;
-    border: none;
-    outline: none;
-  }
-  .valueArea {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 14px;
-    color: #7c7f96;
-    .balance {
+    .maxButton{
+      display:flex;
+      align-items:center;
+      justify-content:center;
+      position:absolute;
+      right:14px;
+      width: 40px;
+      height: 25px;
+      font-size: 12px;
+      line-height: 16px;
+      color: #4B6778;
+      border: 1px solid #4B6778;
+      border-radius: 6px;
+      cursor:pointer;
+    }
+    .maxButton:hover{
+      color:#5f8399;
+      border: 1px solid #5f8399;
+    }
+    .normalInput{
+      width:100%;
+      background: #152528;
+      border-radius: 12px;
+      height: 55px;
+      font-size:20px;
+      color: #fff;
+      padding:0 60px 0 0;
+      padding:0 60px 0 4px;
+      border:none;
+    }
+    .normalInput:focus{
+      outline:none;
+      background: #152528; 
       color: #fff;
     }
-  }
-  .normalInput::-webkit-outer-spin-button,
-  .normalInput::-webkit-inner-spin-button {
+    .valueArea {
+      display:flex;
+      align-items:center;
+      justify-content:space-between;
+      font-size:12px;
+      color:#4B6778;
+      padding:0 6px;
+    }
+    .normalInput::-webkit-outer-spin-button,
+    .normalInput::-webkit-inner-spin-button {
     -webkit-appearance: none !important;
+    }
+    .mt_20 {
+      margin-top:20px;
+    }
+    .rangeInput{
+      width:100%;
+    }
+    input[type=range]{
+      padding:0;
+      -webkit-appearance: none;
+      background-color:transparent;
+    }
+   
+    input[type=range]::-webkit-slider-runnable-track {
+      height:6px;
+      background: #0F1D27;
+      border-radius: 5px;
+    }
+    input[type=range]::-webkit-slider-thumb {
+      position:relative;
+      -webkit-appearance: none;
+      height: 18px;
+      width: 18px;
+      margin-top: -5px;
+      background: #00D6AF;
+      border-radius: 50%;
+      cursor:pointer;
+      z-index:10;
   }
-  .mt_20 {
-    margin-top: 20px;
+  .rangeArea{
+    position:relative;
+    width:100%;
   }
-  .rangeInput {
-    width: 100%;
-    outline: none;
-  }
-  input[type="range"] {
-    padding: 0;
-    -webkit-appearance: none;
-    background-color: transparent;
-  }
-
-  input[type="range"]::-webkit-slider-runnable-track {
-    height: 6px;
-    background: #0f1d27;
+  .bgLine{
+    position:absolute;
+    left:0;
+    top:12px;
+    height:6px;
     border-radius: 5px;
+    background: #00D6AF;
+    pointer-events:none;
   }
-  input[type="range"]::-webkit-slider-thumb {
-    position: relative;
-    -webkit-appearance: none;
-    height: 18px;
-    width: 18px;
-    margin-top: -5px;
-    background: #00d6af;
-    border-radius: 50%;
-    cursor: pointer;
-    z-index: 10;
-  }
-  .rangeArea {
-    position: relative;
-    width: 100%;
-  }
-  .bgLine {
-    position: absolute;
-    left: 0;
-    top: 12px;
-    height: 6px;
-    border-radius: 5px;
-    background: #00d6af;
-    pointer-events: none;
-  }
-  .scale {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  .scale{
+    width:100%;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
   }
   .scale .item {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-size: 12px;
-    color: #7e8a93;
+    position:relative;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    font-size:12px;
+    color:#7E8A93;
   }
-  .scale .item:nth-of-type(n + 2) {
-    left: 5px;
+  .scale .item:nth-of-type(n+2) {
+    left:5px;
   }
-  .item::after {
-    content: "";
-    width: 1px;
-    height: 5px;
-    margin-top: 4px;
-    background-color: #7e8a93;
+  .item::after{
+    content: '';
+    width:1px;
+    height:5px;
+    margin-top:4px;
+    background-color:#7E8A93;
   }
-  .processSpan {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    height: 22px;
-    background: #00c6a2;
-    border-radius: 8px;
-    font-weight: 700;
-    font-size: 12px;
-    width: 40px;
-    color: #000;
-    top: 30px;
+  .processSpan{
+      display:flex;
+      align-items:center;
+      justify-content:center;
+      position:absolute;
+      height: 22px;
+      background: #00C6A2;
+      border-radius: 8px;
+      font-weight: 700;
+      font-size: 12px;
+      width:40px;
+      color:#000;
+      top:30px;
   }
 `;
 const { wnearbase64 } = state;
@@ -158,18 +159,6 @@ function changeRangeAmount(e) {
     .toFixed(4);
   handleAmount(amount, isMax);
 }
-
-function quickChangeRangeAmount(valueStr) {
-  const value = Number(valueStr);
-
-  const isMax = Big(value || 0).eq(subBalance || 0);
-  const amount = Big(subBalance || 0)
-    .mul(value || 0)
-    .div(100)
-    .toFixed(4);
-  handleAmount(amount, isMax);
-}
-
 function changeToMax() {
   handleAmount(Big(subBalance || 0).toFixed(), true);
 }
@@ -178,8 +167,16 @@ const rangeAmount =
 const bgLineWidth = rangeAmount + "%";
 const processMarginLeft = -10 - Big(18).mul(rangeAmount).div(100).toNumber();
 function displayAmount() {
-  if (isNaN(Number(amount))) return "";
-  return amount;
+  let result;
+  let v = (amount || 0).toString();
+  if (v.indexOf(".") > -1) {
+    // 小数
+    result = v.replace(/^0+\./g, "0.");
+  } else {
+    // 整数
+    result = v.replace(/^[0]+/, "");
+  }
+  return result || 0;
 }
 function getWnearIcon(icon) {
   State.update({
@@ -193,17 +190,6 @@ return (
       src="juaner.near/widget/ref-icons"
       props={{ getWnearIcon, getCloseButtonIcon }}
     />
-
-    {!props.hideBalanceInputTop && (
-      <div class="valueArea mb-2">
-        <div class="label">{props.label || "Available to supply"}</div>
-        <div>
-          <span class="balance">{subBalance}</span>
-          <span>(${balance$ || "0"})</span>
-        </div>
-      </div>
-    )}
-
     <div class="inputArea">
       <img src={metadata.icon || wnearbase64} class="tokenIcon" />
       <input
@@ -216,47 +202,16 @@ return (
         Max
       </span>
     </div>
-
-    {props.hideBalanceInputTop && (
-      <div
-        class="valueArea mb-2"
-        style={{
-          marginTop: "12px",
-        }}
-      >
-        <div class="">${balance$ || "0"}</div>
-        <div>
-          <span>Balance:</span>
-
-          <span
-            class="balance"
-            style={{
-              color: "#7c7f96",
-            }}
-          >
-            {subBalance}
-          </span>
-        </div>
-      </div>
-    )}
-
+    <div class="valueArea">
+      <span class="value">${balance$ || "0"}</span>
+      <span>Balance:{subBalance}</span>
+    </div>
     <div class="scale mt_20">
-      {["0", "25", "50", "75", "100"].map((p, i) => {
-        return (
-          <span
-            class="item"
-            style={{
-              cursor: "pointer",
-            }}
-            key={`percent-${p}-${i}`}
-            onClick={() => {
-              quickChangeRangeAmount(p);
-            }}
-          >
-            {p}%
-          </span>
-        );
-      })}
+      <span class="item">0%</span>
+      <span class="item">25%</span>
+      <span class="item">50%</span>
+      <span class="item">75%</span>
+      <span class="item">100%</span>
     </div>
     <div class="rangeArea">
       <input
