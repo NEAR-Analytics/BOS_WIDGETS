33ca1a1c7f9af9cabbc9fc20a8a677ec2c419a99
juaner.near <juaner.near>
2023-05-11 02:06:52 +0000

    Update ref-input-box by juaner.near at 2023-05-11T02:06:52.371Z

--- juaner.near/ref-input-box/source_code.jsx
+++ juaner.near/ref-input-box/source_code.jsx
@@ -40,7 +40,7 @@ const Container = styled.div`
       height: 55px;
       font-size:20px;
       color: #7E8A93;
-      padding:0 60px 0 5px;
+      padding:0 60px 0 0;
       border:none;
     }
     .normalInput:focus{
