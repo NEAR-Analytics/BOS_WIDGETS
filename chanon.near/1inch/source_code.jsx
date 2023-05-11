5a35cfc98973439b5e625166683151f3e1808e17
chanon.near <chanon.near>
2023-05-11 09:06:12 +0000

    Update 1inch by chanon.near at 2023-05-11T09:06:12.736Z

--- chanon.near/1inch/source_code.jsx
+++ chanon.near/1inch/source_code.jsx
@@ -1,21 +1,41 @@
-// TODO ::
+// << -- Change log V.0.0.1 -- >>
+// - handle error approval
+// - handle support 2 ETH and BSC
+// - handle support 2 1inch AggregationRouterV5 on ETH and BSC
+// - add more tokens
+// << -- Backlogs -- >>
+// - handle Native Gas need to wrap before swap
+// - handle Diffrent Token Decimal like USDC (6 Decimal) <=> USDT (18 Decimal)
 // - handle error pop up if an 1inch check allowance api does not work properly
 // - handle and display 1inch mutiple chain support not just only bsc
 // - Multiple Token Selection
 // - Max(helper button) to get max amount of token from wallet address
 // - error when user select from / to as same token
 // - error when user don't select destination token
-// - change allow token behavior to replace swap butto
-// - might add revork button
+// - change allow token behavior to replace swap button
+// - add revork button
 
 initState({
   toggleAmount: false,
   txHash: "",
   tokenDecimals: 18,
   fromTokenAmount: 0,
+  status: 0,
 });
 
 const signer = Ethers.send("eth_requestAccounts", [])[0];
+
+if (state.error) {
+  return (
+    <div>
+      Dear user, we regret to inform you that we have received an error callback
+      from the API. Our team is currently investigating the issue and working on
+      resolving it as soon as possible. We apologize for any inconvenience this
+      may have caused and thank you for your patience while we work to address
+      the problem.
+    </div>
+  );
+}
 
 if (!signer) {
   return (
@@ -40,18 +60,21 @@ if (
     });
 }
 
-if (state.chainId !== 56) {
+// Support ETH, BSC
+if (state.chainId !== 1 && state.chainId !== 56) {
   return (
     <div>
       <h3>
-        Wrong Network - We only support the Binance Smart Chain mainnet at this
-        time.
+        Wrong Network - We currently support the Binance Smart Chain and
+        Ethereum mainnets exclusively. Kindly confirm that you are connected to
+        the intended network before proceeding.
       </h3>
     </div>
   );
 }
 
-const setToken = (token) => {
+const setToken = (chain, token) => {
+  console.log("chain ", chain, " token ", token);
   State.update({ token });
   checkAllowance(token);
 };
@@ -69,29 +92,41 @@ function checkAllowance(token) {
       "&walletAddress=" +
       signer
   ).then((res) => {
-    console.log(res);
     if (res.status === 200) {
       {
         res.body.allowance > 0
           ? State.update({ toggleAmount: true })
           : State.update({ toggleAmount: false });
       }
-    } // TODO :: need to handle error ?
+    } else {
+      // TODO :: need to handle error ?
+      console.log(res);
+    }
   });
 }
 
-const tokens = {
+const bscTokens = {
   "Select Token": "",
   USDT: "0x55d398326f99059fF775485246999027B3197955",
   BUSD: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+  ONEINCH: "0x111111111117dC0aa78b770fA6A738034120C302",
 };
 
-const tokensMenuItems = Object.keys(tokens).map((token) => (
-  <option value={tokens[token]}>{token}</option>
+const ethTokens = {
+  "Select Token": "",
+  USDT: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+  USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+};
+
+const bscTokensMenuItems = Object.keys(bscTokens).map((token) => (
+  <option value={bscTokens[token]}>{token}</option>
 ));
 
-const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
-const USDT = "0x55d398326f99059fF775485246999027B3197955";
+const ethTokensMenuItems = Object.keys(ethTokens).map((token) => (
+  <option value={ethTokens[token]}>{token}</option>
+));
+
+// AggregationRouterV5 (BSC,ETH)
 const ROUTER = "0x1111111254eeb25477b68fb85ed929f73a960582";
 
 const MAX_AMOUNT =
@@ -158,19 +193,26 @@ const handleApprove = () => {
     Ethers.provider().getSigner()
   );
 
-  approveToken.approve(ROUTER, MAX_AMOUNT).then((tx) => {
-    State.update({ txHash: "https://bscscan.com/tx/" + tx.hash });
-    // console.log({
-    //   log: "The TX hash is: " + tx.hash,
-    //   explorerLink: "https://bscscan.com/tx/" + tx.hash,
-    // });
-  });
+  approveToken
+    .approve(ROUTER, MAX_AMOUNT)
+    .then((tx) => {
+      console.log("tx ", tx);
+      if (state.chainId === 1) {
+        State.update({ txHash: "https://etherscan.io/tx/" + tx.hash });
+      } else {
+        State.update({ txHash: "https://bscscan.com/tx/" + tx.hash });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      State.update({ error: true });
+    });
 };
 
 const handleSwap = () => {
   //   console.log("fromTokenAddress ", state.token);
   //   console.log("destinationToken ", state.destinationToken);
-  // getTokenBalance(signer).then((value) => {
+  //   getTokenBalance(signer).then((value) => {
   //   console.log(value);
   // });
 
@@ -201,25 +243,31 @@ const handleSwap = () => {
 
     // Function: swap(address executor,tuple desc,bytes permit,bytes data)
     oneInch.swap(r.executor, r.desc, r.permit, r.data).then((x) => {
-      //   console.log(x.hash);
-      State.update({ txHash: "https://bscscan.com/tx/" + x.hash });
+      if (state.chainId === 1) {
+        State.update({ txHash: "https://etherscan.io/tx/" + x.hash });
+      } else {
+        State.update({ txHash: "https://bscscan.com/tx/" + x.hash });
+      }
     });
   });
 };
 
 return (
   <div>
-    <h3>Swap BEP-20 tokens via 1inch V.5</h3>
+    <h3>
+      Swap {state.chainId === 1 ? "ERC-20" : "BEP-20"} tokens via 1inch
+      AggregationRouterV5 on {state.chainId === 1 ? "ETH" : "BSC"}
+    </h3>
     <div class="mb-3">
       <label for="selectToken">From token</label>
       <select
         class="form-select"
         id="selectToken"
         onChange={(e) => {
-          setToken(e.target.value);
+          setToken(state.chainId, e.target.value);
         }}
       >
-        {tokensMenuItems}
+        {state.chainId === 1 ? ethTokensMenuItems : bscTokensMenuItems}
       </select>
     </div>
 
@@ -247,7 +295,7 @@ return (
           setDestinationToken(e.target.value);
         }}
       >
-        {tokensMenuItems}
+        {state.chainId === 1 ? ethTokensMenuItems : bscTokensMenuItems}
       </select>
     </div>
     <button class="btn btn-success" onClick={handleSwap} disabled={false}>
