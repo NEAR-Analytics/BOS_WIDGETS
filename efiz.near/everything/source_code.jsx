f1d6e8af0032eb488c661d13ee7985ea32a39191
efiz.near <efiz.near>
2023-05-15 01:29:41 +0000

    Update Everything by efiz.near at 2023-05-15T01:29:41.424Z

--- efiz.near/everything/source_code.jsx
+++ efiz.near/everything/source_code.jsx
@@ -3,6 +3,18 @@
 return (
   <Widget
     src="efiz.near/widget/Everything.Tree"
-    props={{ rootPath: "evrything.near", type: "account" }}
+    props={{
+      rootPath: "everything",
+      type: "account",
+      styles: {
+        subject: {
+          fontFamily: "Times New Roman",
+          fontSize: "4em",
+          lineHeight: "1.25",
+          fontWeight: 400,
+          cursor: "pointer",
+        },
+      },
+    }}
   />
 );
