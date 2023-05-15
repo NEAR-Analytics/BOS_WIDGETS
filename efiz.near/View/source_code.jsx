2a02deb574fff68ae925c0c5e332ab5761f83763
efiz.near <efiz.near>
2023-05-15 15:56:19 +0000

    Update View by efiz.near at 2023-05-15T15:56:19.675Z

--- efiz.near/View/source_code.jsx
+++ efiz.near/View/source_code.jsx
@@ -1,76 +1,71 @@
 const path = props.path;
 const type = props.type;
 
-if (type === "type") {
-  if (path.endsWith("/")) {
-    path = path.slice(0, -1);
-  }
+if (path.endsWith("/")) {
+  path = path.slice(0, -1);
 }
 
 const parts = path.split("/");
 let value = {};
-console.log(path);
-if (parts.length < 2) {
-  if (type === "account") {
-    // return default profile or setting's profile
-  }
-} else {
-  if (type === "widget") {
-    if (path.endsWith("/")) {
-      path = path.slice(0, -1);
-    }
-    return <Widget src={path} />;
-  } else if (type === "graph") {
+
+// ACCOUNT //
+if (type === "account") {
+  return "VIEW: account";
+  // THING //
+} else if (type === "thing") {
+  // path: "everything"
+  // type: "thing"
+  return "VIEW: thing";
+  // WIDGET //
+} else if (type === "widget") {
+  return <Widget src={path} />;
+  // PROFILE //
+} else if (type === "graph") {
+  parts.push("**");
+  value = Social.get(parts.join("/"), "final");
+} else if (type === "index") {
+  value = Social.get(parts.join("/"), "final");
+} else if (type === "profile") {
+  value = Social.get(parts.join("/"), "final");
+  return <p>{value}</p>;
+} else if (type === "settings") {
+  if (parts.length === 3) {
     parts.push("**");
     value = Social.get(parts.join("/"), "final");
-  } else if (type === "index") {
-    value = Social.get(parts.join("/"), "final");
-  } else if (type === "profile") {
-    value = Social.get(parts.join("/"), "final");
-    return <p>{value}</p>;
-  } else if (type === "settings") {
-    if (parts.length === 3) {
-      parts.push("**");
-      value = Social.get(parts.join("/"), "final");
-    } else {
-      value = Social.get(parts.join("/"), "final");
-      value = JSON.parse(value);
-    }
-  } else if (type === "post") {
-    // return default post or post from settings
-    const index = {
-      action: parts[1],
-      key: parts[2],
-      options: {
-        limit: 10,
-        order: "desc",
-        accountId: parts[0].endsWith(".near") ? parts[0] : undefined,
-      },
-    };
-
-    function renderItem(a) {
-      if (a.value.type === "md") {
-        return (
-          <Widget
-            src="near/widget/Posts.Post"
-            props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
-          />
-        );
-      }
-    }
-    return (
-      <Widget
-        src="efiz.near/widget/MergedIndexFeed"
-        props={{ index, renderItem, disableCaching: true }}
-      />
-    );
-    //   value = Social.get(path, "final");
-    //   value = JSON.parse(value);
-    //   return <Widget src="efiz.near/widget/Post.View" props={{ value }} />;
   } else {
     value = Social.get(parts.join("/"), "final");
     value = JSON.parse(value);
   }
+} else if (type === "post") {
+  const index = {
+    action: parts[1],
+    key: parts[2],
+    options: {
+      limit: 10,
+      order: "desc",
+      accountId: parts[0].endsWith(".near") ? parts[0] : undefined,
+    },
+  };
+
+  function renderItem(a) {
+    if (a.value.type === "md") {
+      return (
+        <Widget
+          src="near/widget/Posts.Post"
+          props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
+        />
+      );
+    }
+  }
+  return (
+    <Widget
+      src="efiz.near/widget/MergedIndexFeed"
+      props={{ index, renderItem, disableCaching: true }}
+    />
+  );
+} else {
+  value = Social.get(parts.join("/"), "final");
+  value = JSON.parse(value);
 }
 
 const text = `
