5da4175027c1b270db9cf477308a2b6760741807
jgodwill.near <jgodwill.near>
2023-05-15 06:06:46 +0000

    Update NftListingInput by jgodwill.near at 2023-05-15T06:06:46.355Z

--- jgodwill.near/NftListingInput/source_code.jsx
+++ jgodwill.near/NftListingInput/source_code.jsx
@@ -321,7 +321,7 @@ return (
                       labelKey="search"
                       minLength={1}
                       options={allWidgets}
-                      onChange={(e) => props.onChangeReceiver(e.target.value)}
+                      onChange={(value) => props.onChangeReceiver(value)}
                       placeholder="🔍 Search for a Near user..."
                       // renderMenuItemChildren={(option: accountId) => (
                       //   <div className="mb-2">
