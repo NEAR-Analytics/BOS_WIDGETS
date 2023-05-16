5b0a89eea9b4a5fbc475772c54ff7728cecfcc93
root.akaia.near <root.akaia.near>
2023-05-16 02:01:32 +0000

    Update DevGov.Notification.Item.Right by root.akaia.near at 2023-05-16T02:01:32.262Z

--- root.akaia.near/DevGov.Notification.Item.Right/source_code.jsx
+++ root.akaia.near/DevGov.Notification.Item.Right/source_code.jsx
@@ -7,6 +7,30 @@ const nearDevGovGigsWidgetsAccountId =
   props.nearDevGovGigsWidgetsAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
 
+/**
+ * Reads a board config from DevHub contract storage.
+ * Currently a mock.
+ *
+ * Boards are indexed by their ids.
+ */
+const boardConfigByBoardId = ({ boardId }) => {
+  return {
+    probablyUUIDv4: {
+      id: "probablyUUIDv4",
+
+      columns: [
+        { title: "Draft", labelFilters: ["S-draft"] },
+        { title: "Review", labelFilters: ["S-review"] },
+      ],
+
+      dataTypes: { Issue: true, PullRequest: true },
+      description: "Latest NEAR Enhancement Proposals by status",
+      repoURL: "https://github.com/near/NEPs",
+      title: "NEAR Protocol NEPs",
+    },
+  }[boardId];
+};
+
 function widget(widgetName, widgetProps, key) {
   widgetProps = {
     ...widgetProps,
@@ -42,13 +66,23 @@ function href(widgetName, linkProps) {
   }
 
   const linkPropsQuery = Object.entries(linkProps)
-    .map(([key, value]) => `${key}=${value}`)
+    .map(([key, value]) => (value === null ? null : `${key}=${value}`))
+    .filter((nullable) => nullable !== null)
     .join("&");
 
-  return `#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
     linkPropsQuery ? "?" : ""
   }${linkPropsQuery}`;
 }
+
+const CompactContainer = styled.div`
+  width: fit-content !important;
+  max-width: 100%;
+`;
+
+const FormCheckLabel = styled.label`
+  white-space: nowrap;
+`;
 /* END_INCLUDE: "common.jsx" */
 
 return props.post === undefined ? (
