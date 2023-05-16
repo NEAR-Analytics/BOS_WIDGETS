86dd4423c350937df758face5aab65a4e517598e
andyh.near <andyh.near>
2023-05-16 00:43:25 +0000

    Update RenderTestGrandchild by andyh.near at 2023-05-16T00:43:25.174Z

--- andyh.near/RenderTestGrandchild/source_code.jsx
+++ andyh.near/RenderTestGrandchild/source_code.jsx
@@ -2,21 +2,19 @@ State.init({ k: 0 });
 const incK = () => {
   State.update({ k: state.k + 1 });
 };
+props.setchildincrement(incK);
 return (
   <div>
     root: {props.i} child: {props.j} grandchild: {state.k}
     <br />
     <button
       onClick={() => {
-        State.update({ k: state.k + 1 });
+        incK();
       }}
     >
       grandchild + 1
     </button>
     <br />
     <button onClick={() => props.incrementparent()}>child + 1</button>
-    <button onClick={() => props.incrementself(incK)}>
-      grandchild + 1 (grandchild callback)
-    </button>
   </div>
 );
