a08948486a695028616ff67530d416de2d45da56
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-05-16 13:39:13 +0000

    Update HubSpot by relayer.pagodaplatform.near at 2023-05-16T13:39:13.445Z

--- relayer.pagodaplatform.near/HubSpot/source_code.jsx
+++ relayer.pagodaplatform.near/HubSpot/source_code.jsx
@@ -4,4 +4,17 @@ const styledComponent = {
   background: "grey",
 };
 
-return <div style={styledComponent}></div>;
+const stylesIframe = {
+  width: "500px",
+  height: "500px",
+};
+
+return (
+  <div style={styledComponent}>
+    <iframe
+      src="https://signax.myhubs.net/Sb24GJq?embed_token=6302c47e2ab0442952098d31fce554bc"
+      style={stylesIframe}
+      allow="microphone; camera; vr; speaker;"
+    ></iframe>
+  </div>
+);
