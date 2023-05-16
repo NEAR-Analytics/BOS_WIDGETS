32a6f2f400a5764a9585622971db8331cdee4112
saidulbadhon.near <saidulbadhon.near>
2023-05-16 16:56:58 +0000

    Update ProfilePage.Main by saidulbadhon.near at 2023-05-16T16:56:58.335Z

--- saidulbadhon.near/ProfilePage.Main/source_code.jsx
+++ saidulbadhon.near/ProfilePage.Main/source_code.jsx
@@ -22,58 +22,69 @@ const totalCommits = Object.keys(widget)
 
 const widgets = Social.getr(`${accountId}/widget`) ?? {};
 
-const cssFont = fetch(
-  "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-).body;
+// const cssFont = fetch(
+//   "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+// ).body;
 
-const css = fetch(
-  props.theme.name === "dark"
-    ? "https://nearpad-images.s3.amazonaws.com/do_not_delete/profilepage_dark.css"
-    : "https://nearpad-images.s3.amazonaws.com/do_not_delete/profilepage_light.css"
-).body;
+// const css = fetch(
+//   props.theme.name === "dark"
+//     ? "https://nearpad-images.s3.amazonaws.com/do_not_delete/profilepage_dark.css"
+//     : "https://nearpad-images.s3.amazonaws.com/do_not_delete/profilepage_light.css"
+// ).body;
 
-const theme = props.theme.name;
+// const theme = props.theme.name;
 
-const Theme = styled.div`
-  font-family: "Open Sans", sans-serif;
-  ${cssFont}
-  ${css}
-`;
+// const Theme = styled.div`
+//   font-family: "Open Sans", sans-serif;
+//   ${cssFont}
+//   ${css}
+// `;
 
 return (
-  <Theme>
-    <div className="rightSection">
-      <div>
-        <h2>Widgets</h2>
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      gap: 40,
+    }}
+  >
+    <div>
+      <h2>Widgets</h2>
 
-        {Object.keys(widgets)?.length > 0 ? (
-          <div className="widgetsContainer">
-            {Object.keys(widgets)?.map((item, index) => (
-              <Widget
-                src="zahidulislam.near/widget/Profile.WidgetItem"
-                props={{
-                  name: item,
-                  accountId,
-                  commits:
-                    allWidgetsHistoryChangesBlocks[accountId].widget[item],
-                }}
-              />
-            ))}
-          </div>
-        ) : (
-          <p
-            style={{
-              padding: 20,
-              textAlign: "center",
-              color: "rgba(0,0,0,.75)",
-            }}
-          >
-            {profile?.name} does not have any widget.
-          </p>
-        )}
-      </div>
+      {Object.keys(widgets)?.length > 0 ? (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            marginTop: 20,
+            gap: 20,
+          }}
+        >
+          {Object.keys(widgets)?.map((item, index) => (
+            <Widget
+              src="zahidulislam.near/widget/Profile.WidgetItem"
+              props={{
+                name: item,
+                accountId,
+                commits: allWidgetsHistoryChangesBlocks[accountId].widget[item],
+              }}
+            />
+          ))}
+        </div>
+      ) : (
+        <p
+          style={{
+            padding: 20,
+            textAlign: "center",
+            color: "rgba(0,0,0,.75)",
+          }}
+        >
+          {profile?.name} does not have any widget.
+        </p>
+      )}
+    </div>
 
-      {/*<div>
+    {/*<div>
         <h2>{totalCommits.length} contributions</h2>
         <div style={{ marginTop: 20 }} />
         <Widget
@@ -81,6 +92,5 @@ return (
           props={{ theme: theme }}
         />
       </div>*/}
-    </div>
-  </Theme>
+  </div>
 );
