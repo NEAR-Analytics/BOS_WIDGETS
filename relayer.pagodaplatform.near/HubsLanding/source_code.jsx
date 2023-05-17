752e207a425c4c5fe3e25f80c77c70d419b5bf62
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-05-17 01:39:36 +0000

    Update HubsLanding by relayer.pagodaplatform.near at 2023-05-17T01:39:36.498Z

--- relayer.pagodaplatform.near/HubsLanding/source_code.jsx
+++ relayer.pagodaplatform.near/HubsLanding/source_code.jsx
@@ -21,85 +21,82 @@ const CustomContentDiv = styled.div`
 `;
 
 const CustomContentDivLeft = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    flex: 0.5;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex: 0.5;
 `;
 
 const ParagraphTag = styled.p`
-    color: white;
-    font-weight: 400;
-    margin: 0px 0px 0px 0px;
-    padding: 0px 0px 0px 0px;
-    color: #b6b6b6;
+  color: white;
+  font-weight: 400;
+  margin: 0px 0px 0px 0px;
+  padding: 0px 0px 0px 0px;
+  color: #b6b6b6;
 `;
 const HeadingTagOne = styled.h1`
-    color: white;
-    font-weight: bold;
-    margin: 0px 0px 0px 0px;
-    padding: 0px 0px 0px 0px;
+  color: white;
+  font-weight: bold;
+  margin: 0px 0px 0px 0px;
+  padding: 0px 0px 0px 0px;
 `;
 const FooterDivLeft = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    border-left: 1px solid yellow;
-    padding: 10px;
-    margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  border-left: 1px solid yellow;
+  padding: 10px;
+  margin-top: 30px;
 `;
 const ParagraphTagSecondary = styled.p`
-    color: white;
-    font-weight: 400;
-    font-size: 12px;
-    margin: 0px 0px 0px 0px;
-    padding: 0px 0px 0px 0px;
-    width: 100%;
-    color: #b6b6b6;
+  color: white;
+  font-weight: 400;
+  font-size: 12px;
+  margin: 0px 0px 0px 0px;
+  padding: 0px 0px 0px 0px;
+  width: 100%;
+  color: #b6b6b6;
 `;
 const ButtonGroup = styled.div`
-
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    margin-top: 20px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 20px;
 `;
 
 const ExploreButtonTag = styled.button`
-
-    background: transparent;
-    outline: none;
-    border: 1px solid white;
-    border-radius: 5px;
-    color: white;
-    padding: 5px;
-    width: 20%;
-    margin-right: 20px;
+  background: transparent;
+  outline: none;
+  border: 1px solid white;
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  width: 20%;
+  margin-right: 20px;
 `;
 
 const ExperienceButtonTag = styled.button`
-
-    background: yellow;
-    outline: none;
-    border: none;
-    border-radius: 5px;
-    color: black;
-    padding: 5px;
-    width: 30%;
-    margin-left: 20px;
+  background: yellow;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  color: black;
+  padding: 5px;
+  width: 30%;
+  margin-left: 20px;
 `;
 
 const CustomContentDivRight = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    flex: 0.4;
-    margin-right: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex: 0.4;
+  margin-right: 30px;
 `;
 
 const imageStyle = {
@@ -108,8 +105,23 @@ const imageStyle = {
   objectFit: "contain",
 };
 
+const AppbarLogo = styled.div`
+  width: 20%;
+  height: 50px;
+  padding: 10px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
 return (
   <CustomStyledDiv>
+    <AppbarLogo>
+      <img
+        src="https://asset-signax.b-cdn.net/signax_logo_white.png"
+        style={imageStyle}
+      />
+    </AppbarLogo>
     <CustomContentDiv>
       <CustomContentDivLeft>
         <ParagraphTag>Industrial Revolution</ParagraphTag>
