7386a55714fba0beead732ac1c47b224cb647541
thedarkmon.near <thedarkmon.near>
2023-05-17 19:24:09 +0000

    Update Untitled-0 by thedarkmon.near at 2023-05-17T19:24:09.430Z

--- thedarkmon.near/Untitled-0/source_code.jsx
+++ thedarkmon.near/Untitled-0/source_code.jsx
@@ -1,35 +1,23 @@
 return <div>Hello World</div>;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-public class TwoButtonsExample {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Пример двух кнопок");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new FlowLayout());
+import React from "react";
+import ReactDOM from "react-dom";
+class TwoButtons extends React.Component {
+  handleClick1() {
+    alert("Вы нажали кнопку 1!");
+  }
 
-        // Создание первой кнопки
-        JButton button1 = new JButton("Кнопка 1");
-        button1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Вы нажали кнопку 1!");
-            }
-        });
-        frame.add(button1);
+  handleClick2() {
+    alert("Вы нажали кнопку 2!");
+  }
 
-        // Создание второй кнопки
-        JButton button2 = new JButton("Кнопка 2");
-        button2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Вы нажали кнопку 2!");
-            }
-        });
-        frame.add(button2);
-
-        frame.pack();
-        frame.setVisible(true);
-    }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick1}>Кнопка 1</button>
+        <button onClick={this.handleClick2}>Кнопка 2</button>
+      </div>
+    );
+  }
 }
+
+ReactDOM.render(<TwoButtons />, document.getElementById("root"));
