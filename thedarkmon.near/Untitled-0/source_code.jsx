db02f16310a6c904d0452180557691b50bbce77f
thedarkmon.near <thedarkmon.near>
2023-05-17 19:19:46 +0000

    Update Untitled-0 by thedarkmon.near at 2023-05-17T19:19:46.246Z

--- thedarkmon.near/Untitled-0/source_code.jsx
+++ thedarkmon.near/Untitled-0/source_code.jsx
@@ -1,33 +1,28 @@
 return <div>Hello World</div>;
-import java.util.Random;
-import java.util.Scanner;
-
-public class GuessTheNumber {
+import java.util.Timer;
+import java.util.TimerTask;
+public class AlarmClock {
     public static void main(String[] args) {
-        Random random = new Random();
-        int number = random.nextInt(100) + 1; // Загадываем число от 1 до 100
-        int attempts = 0;
-        boolean guessed = false;
+        Timer timer = new Timer();
+        int seconds = 10; // Время до срабатывания будильника (в секундах)
 
-        System.out.println("Добро пожаловать в игру 'Угадай число'!");
+        System.out.println("Будильник установлен на через " + seconds + " секунд.");
 
-        Scanner scanner = new Scanner(System.in);
-
-        while (!guessed) {
-            System.out.print("Введите число: ");
-            int guess = scanner.nextInt();
-            attempts++;
-
-            if (guess == number) {
-                System.out.println("Поздравляю! Вы угадали число " + number + " с " + attempts + " попыток!");
-                guessed = true;
-            } else if (guess < number) {
-                System.out.println("Загаданное число больше!");
-            } else {
-                System.out.println("Загаданное число меньше!");
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Сработал будильник! Проснитесь!");
+                // Здесь можно добавить нужные действия, выполняемые при срабатывании будильника
             }
+        }, seconds * 1000); // Переводим время в миллисекунды
+
+        // Пауза для предотвращения завершения программы
+        try {
+            Thread.sleep((seconds + 1) * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        scanner.close();
+        timer.cancel(); // Отменяем таймер после срабатывания будильника
     }
 }
