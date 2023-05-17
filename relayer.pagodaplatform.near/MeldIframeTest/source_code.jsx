d74a38bcc98de0e9d22e199fdf5d8b7e30707c70
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-05-17 19:34:33 +0000

    Update MeldIframeTest by relayer.pagodaplatform.near at 2023-05-17T19:34:33.624Z

---
+++ relayer.pagodaplatform.near/MeldIframeTest/source_code.jsx
@@ -0,0 +1,4 @@
+const website =
+  "https://fluidmoney.xyz/?publicKey=WGuf4wZC2ASroLa3hLDN7a:BoYX7ook9aryMS4XMWDYrqwpjC4kciYnvbxDyeYP";
+
+return <iframe className="w-100" style={style} iframeResizer src={website} />;
