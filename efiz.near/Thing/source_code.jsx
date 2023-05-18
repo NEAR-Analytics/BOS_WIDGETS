8018a3ee8d0acdad826ba73b36e9dc63db211842
efiz.near <efiz.near>
2023-05-18 16:15:37 +0000

    Update Thing by efiz.near at 2023-05-18T16:15:37.072Z

--- efiz.near/Thing/source_code.jsx
+++ efiz.near/Thing/source_code.jsx
@@ -6,6 +6,20 @@ const Container = styled.div`
   margin-bottom: 10px;
 `;
 
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const Raw = styled.div`
+  display: ${({ collapsed }) => (collapsed ? "none" : "block")};
+`;
+
+const Button = styled.button`
+  text-transform: lowercase !important;
+`;
+
 const Row = styled.div`
   display: flex;
   margin-bottom: 5px;
@@ -40,36 +54,72 @@ function composeData() {
   };
 }
 
+State.init({ collapsed: true });
+
+const handleToggle = () => {
+  State.update({ collapsed: !state.collapsed });
+};
+
+let type = {};
+// This is just hardcoding Type.get(type)
 if (a.value.type === "image") {
-  return (
-    <Container>
-      <Row>
+  type = {
+    widgets: {
+      create: "efiz.near/widget/Create.Image",
+      view: "mob.near/widget/Image",
+    },
+  };
+} else if (a.value.type === "md") {
+  type = {
+    widgets: {
+      create: "efiz.near/widget/Posts.Compose",
+    },
+  };
+}
+
+const value = Social.get(`${a.accountId}/thing/${a.key}`, "final");
+value = JSON.parse(value);
+
+return (
+  <Container>
+    <Header>
+      <Button onClick={handleToggle}>show raw</Button>
+    </Header>
+    <Raw collapsed={state.collapsed}>
+      <div>
         <Key>accountId:</Key>
         <Value>{a.accountId}</Value>
-      </Row>
-      <Row>
+      </div>
+      <div>
         <Key>blockHeight:</Key>
         <Value>{a.blockHeight}</Value>
-      </Row>
-      <Row>
+      </div>
+      <div>
         <Key>value.type:</Key>
         <Value>{a.value.type}</Value>
-      </Row>
-      <Row>
+      </div>
+      <div>
         <Key>action:</Key>
         <Value>{a.action}</Value>
-      </Row>
-      <Row>
+      </div>
+      <div>
         <Key>key:</Key>
         <Value>{a.key}</Value>
-      </Row>
-      <Row>
+      </div>
+      <div>
         <Key>index:</Key>
         <Value>{a.index}</Value>
-      </Row>
-      <CommitButton force data={composeData} className="styless">
-        create
-      </CommitButton>
-    </Container>
-  );
-}
+      </div>
+    </Raw>
+    {type?.widgets?.view && (
+      <Widget
+        src={type.widgets.view}
+        props={{ image: { ipfs_cid: value.data } }}
+      />
+    )}
+    {/** this should check if it has been posted before */}
+    <CommitButton force data={composeData} className="styless">
+      post
+    </CommitButton>
+  </Container>
+);
