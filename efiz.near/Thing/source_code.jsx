8866f9840591bb7bbd6ad37aacd9f432775d5d66
efiz.near <efiz.near>
2023-05-18 15:20:14 +0000

    Update Thing by efiz.near at 2023-05-18T15:20:14.073Z

--- efiz.near/Thing/source_code.jsx
+++ efiz.near/Thing/source_code.jsx
@@ -1,5 +1,25 @@
 const a = props.a;
 
+const Container = styled.div`
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  margin-bottom: 5px;
+`;
+
+const Key = styled.span`
+  font-weight: bold;
+  margin-right: 5px;
+`;
+
+const Value = styled.span`
+  color: #888;
+`;
+
 function composeData(a) {
   return {
     post: {
