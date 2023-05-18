8c36e0fe05ffe9d960c5df924b5634cd7dafe215
efiz.near <efiz.near>
2023-05-18 15:19:22 +0000

    Update Thing by efiz.near at 2023-05-18T15:19:22.939Z

--- efiz.near/Thing/source_code.jsx
+++ efiz.near/Thing/source_code.jsx
@@ -1,10 +1,49 @@
-const accountId = props.accountId;
-const blockHeight =
-  props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
-const content =
-  props.content ??
-  JSON.parse(Social.get(`${accountId}/thing/main`, blockHeight) ?? "null");
+function composeData(a) {
+  return {
+    post: {
+      main: JSON.stringify({
+        data,
+        type: a.value.type,
+      }),
+    },
+    index: {
+      post: JSON.stringify({
+        key: "main",
+        value: {
+          type: a.value.type,
+        },
+      }),
+    },
+  };
+}
 
-return (
-  <div className="border rounded-4 p-3 pb-1">{JSON.stringify(content)}</div>
-);
+if (a.value.type === "image") {
+  return (
+    <Container>
+      <Row>
+        <Key>accountId:</Key>
+        <Value>{a.accountId}</Value>
+      </Row>
+      <Row>
+        <Key>blockHeight:</Key>
+        <Value>{a.blockHeight}</Value>
+      </Row>
+      <Row>
+        <Key>value.type:</Key>
+        <Value>{a.value.type}</Value>
+      </Row>
+      <Row>
+        <Key>action:</Key>
+        <Value>{a.action}</Value>
+      </Row>
+      <Row>
+        <Key>key:</Key>
+        <Value>{a.key}</Value>
+      </Row>
+      <Row>
+        <Key>index:</Key>
+        <Value>{a.index}</Value>
+      </Row>
+    </Container>
+  );
+}
