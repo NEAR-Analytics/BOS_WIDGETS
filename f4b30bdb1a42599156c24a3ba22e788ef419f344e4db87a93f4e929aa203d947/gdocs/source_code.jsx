4681c224fc4f26486865703d7cfcf834708e9cc8
f4b30bdb1a42599156c24a3ba22e788ef419f344e4db87a93f4e929aa203d947 <f4b30bdb1a42599156c24a3ba22e788ef419f344e4db87a93f4e929aa203d947>
2023-05-19 04:34:20 +0000

    Update gdocs by f4b30bdb1a42599156c24a3ba22e788ef419f344e4db87a93f4e929aa203d947 at 2023-05-19T04:34:20.102Z

---
+++ f4b30bdb1a42599156c24a3ba22e788ef419f344e4db87a93f4e929aa203d947/gdocs/source_code.jsx
@@ -0,0 +1,12 @@
+return (
+  <>
+    <iframe
+      className="container-fluid vh-100 border border-primary rounded"
+      src="https://docs.google.com/presentation/d/e/2PACX-1vRbSysyomzv9vAoiS-vIIvm_I3RzE8KptjfgnhfH9ssayd1ggeTU9oqWQZ2JbvfBmLfixm-fuofwxsn/embed?start=false&loop=false&delayms=3000"
+      frameborder="0"
+      allowfullscreen="true"
+      mozallowfullscreen="true"
+      webkitallowfullscreen="true"
+    ></iframe>
+  </>
+);
