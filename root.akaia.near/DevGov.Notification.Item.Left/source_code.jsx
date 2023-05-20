4cb55bc67877b6ae8592fba6139516c484c54624
root.akaia.near <root.akaia.near>
2023-05-20 01:57:04 +0000

    Update DevGov.Notification.Item.Left by root.akaia.near at 2023-05-20T01:57:04.191Z

--- root.akaia.near/DevGov.Notification.Item.Left/source_code.jsx
+++ root.akaia.near/DevGov.Notification.Item.Left/source_code.jsx
@@ -7,6 +7,25 @@ const nearDevGovGigsWidgetsAccountId =
   props.nearDevGovGigsWidgetsAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
 
+/**
+ * Reads a board config from DevHub contract storage.
+ * Currently a mock.
+ *
+ * Boards are indexed by their ids.
+ */
+const boardConfigByBoardId = ({ boardId }) => {
+  return {
+    probablyUUIDv4: {
+      id: "probablyUUIDv4",
+      columns: [{ title: "Draft", labelFilters: ["S-draft"] }],
+      dataTypes: { Issue: true, PullRequest: true },
+      description: "Latest NEAR Enhancement Proposals by status",
+      repoURL: "https://github.com/near/NEPs",
+      title: "NEAR Protocol NEPs",
+    },
+  }[boardId];
+};
+
 function widget(widgetName, widgetProps, key) {
   widgetProps = {
     ...widgetProps,
@@ -50,6 +69,15 @@ function href(widgetName, linkProps) {
     linkPropsQuery ? "?" : ""
   }${linkPropsQuery}`;
 }
+
+const CompactContainer = styled.div`
+  width: fit-content !important;
+  max-width: 100%;
+`;
+
+const FormCheckLabel = styled.label`
+  white-space: nowrap;
+`;
 /* END_INCLUDE: "common.jsx" */
 
 if (!props.type) {
@@ -60,16 +88,15 @@ const type = props.type.split("/")[1];
 return props.type ? (
   <>
     {type == "like"
-      ? "liked your"
+      ? "liked"
       : type == "reply"
-      ? "replied to your"
+      ? "replied"
       : type == "edit"
-      ? "edited your"
-      : type == "mention"
-      ? "mentioned you in their"
+      ? "edited"
       : "???"}{" "}
+    your
     <a className="fw-bold text-muted" href={href("Post", { id: props.post })}>
-      DevHub post
+      Developer Governance post
     </a>
   </>
 ) : (
