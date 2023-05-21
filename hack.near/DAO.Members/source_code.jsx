5334800651207c1257cb2b4613527109bafbf8e4
hack.near <hack.near>
2023-05-21 22:59:39 +0000

    Update DAO.Members by hack.near at 2023-05-21T22:59:39.311Z

--- hack.near/DAO.Members/source_code.jsx
+++ hack.near/DAO.Members/source_code.jsx
@@ -1,62 +1,30 @@
 const accountId = props.accountId ?? context.accountId;
 const daoId = props.daoId ?? "multi.sputnik-dao.near";
-const groupId = props.groupId ?? "council";
 
-const group2Id = props.group2Id ?? "community";
 const policy = Near.view(daoId, "get_policy");
 
-const group = policy.roles
-  .filter((role) => role.name === groupId)
-  .map((role) => {
-    const group = role.kind.Group;
-
-    return group;
-  });
-
-const group2 = policy.roles
-  .filter((role) => role.name === group2Id)
-  .map((role) => {
-    const group2 = role.kind.Group;
-
-    return group2;
-  });
+const groups = policy.roles
+  .filter((role) => role.kind.Group)
+  .map((role) => ({
+    name: role.name,
+    members: role.kind.Group,
+  }));
 
 return (
-  <>
-    <div>
-      <h3>{groupId}</h3>
-      <div>
-        {group.map((members, i) => (
-          <div key={i}>
-            {members.map((member, j) => (
-              <a
-                key={j}
-                className="text-decoration-none"
-                href={`#mob.near/widget/ProfilePage?accountId=${member}`}
-              >
-                <h4>{member}</h4>
-              </a>
-            ))}
-          </div>
+  <div>
+    {groups.map((group, i) => (
+      <div key={i}>
+        <h3>{group.name}</h3>
+        {group.members.map((member, j) => (
+          <a
+            key={j}
+            className="text-decoration-none"
+            href={`#mob.near/widget/ProfilePage?accountId=${member}`}
+          >
+            <h5>{member}</h5>
+          </a>
         ))}
       </div>
-
-      <h3>{group2Id}</h3>
-      <div>
-        {group2.map((members, i) => (
-          <div key={i}>
-            {members.map((member, j) => (
-              <a
-                key={j}
-                className="text-decoration-none"
-                href={`#mob.near/widget/ProfilePage?accountId=${member}`}
-              >
-                <h4>{member}</h4>
-              </a>
-            ))}
-          </div>
-        ))}
-      </div>
-    </div>
-  </>
+    ))}
+  </div>
 );
