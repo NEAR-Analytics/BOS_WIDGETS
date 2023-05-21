aa922562fa43d352f516086f9730e5959cc2e24e
efiz.near <efiz.near>
2023-05-21 04:08:32 +0000

    Update everything by efiz.near at 2023-05-21T04:08:32.219Z

--- efiz.near/everything/source_code.jsx
+++ efiz.near/everything/source_code.jsx
@@ -1,6 +1,6 @@
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 30vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -11,10 +11,15 @@ const Container = styled.div`
 `;
 
 const InnerContainer = styled.div`
-  width: 50%;
+  width: 80%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 20px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
 `;
 
 const Row = styled.div`
@@ -22,108 +27,75 @@ const Row = styled.div`
   flex-direction: row;
   width: 100%;
   border: 2px solid orange;
+
+  @media (max-width: 767px) {
+    flex-direction: column;
+  }
 `;
 
 const Column = styled.div`
-  flex: 1 1 200px;
+  flex: 1;
   display: flex;
   justify-content: center;
+  align-items: center;
   border: 2px solid green;
+  padding: 20px;
+
+  @media (max-width: 767px) {
+    border-top: none;
+    padding: 10px;
+  }
 `;
 
-const CenterColumn = styled.div`
-  flex: 2 1 300px;
-  display: flex;
-  justify-content: center;
-  border: 2px solid red;
+const IconBox = styled.div`
+  font-family: "Times New Roman";
+  font-size: 2em;
+  line-height: 1.25;
+  font-weight: 400;
+  cursor: pointer;
+
+  @media (max-width: 767px) {
+    font-size: 1.5em;
+  }
+`;
+
+const ActionButton = styled.button`
+  font-family: "Times New Roman";
+  font-size: 2em;
+  line-height: 1.25;
+  font-weight: 400;
+  cursor: pointer;
+
+  @media (max-width: 767px) {
+    font-size: 1.5em;
+  }
+`;
+
+const SubjectField = styled.input`
+  font-family: "Times New Roman";
+  font-size: 2em;
+  line-height: 1.25;
+  font-weight: 400;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  background: none;
+  width: 100%;
+
+  @media (max-width: 767px) {
+    font-size: 1.5em;
+  }
 `;
 
 return (
   <Container>
     <InnerContainer>
       <Row>
-        <Column>icon</Column>
-        <CenterColumn>subject</CenterColumn>
-        <Column>action</Column>
+        <IconBox>Icon</IconBox>
+        <SubjectField type="text" placeholder="Subject" />
+        <ActionButton>Action</ActionButton>
       </Row>
       <Row>advanced</Row>
     </InnerContainer>
   </Container>
-);
-
-const Button = styled.button`
-  text-transform: lowercase !important;
-`;
-
-const Link = styled.a`
-  text-decoration: none;
-`;
-
-const everythingExists =
-  !!Social.get(`${context.accountId}/widget/everything`) ?? false;
-
-const createEverything = () => {
-  Social.set(
-    {
-      widget: {
-        everything: {
-          "": `return (<Widget src="efiz.near/widget/Tree" props={{ rootPath: "${context.accountId}", rootType: "account" }} />);`,
-          metadata: {
-            tags: {
-              everything: "",
-            },
-          },
-        },
-      },
-    },
-    {
-      force: true,
-    }
-  );
-};
-
-// <a href={"/#/evrything-docs.near/widget/Everything.Documentation"}>
-//   <Button>documentation</Button>
-// </a>;
-
-return (
-  <div className="d-flex flex-column">
-    <div className="mt-auto py-3">
-      <div className="container">
-        <div className="d-flex justify-content-end gap-2">
-          {context.accountId &&
-            (everythingExists ? (
-              <a href={`/#/${context.accountId}/widget/everything`}>
-                <Button>{context.accountId}'s everything</Button>
-              </a>
-            ) : (
-              <Button onClick={createEverything}>
-                create your own everything
-              </Button>
-            ))}
-        </div>
-      </div>
-    </div>
-    <Container>
-      <Subject>everything</Subject>
-    </Container>
-    <Widget
-      src="efiz.near/widget/Tree"
-      props={{
-        rootPath: "everything",
-        rootType: "thing",
-        rootNode: {
-          post: {
-            main: null,
-            dev: null,
-            bos: null,
-          },
-          account: {
-            "efiz.near": null,
-            [context.accountId]: null,
-          },
-        },
-      }}
-    />
-  </div>
 );
