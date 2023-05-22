23d090d9075bbcb97abbd5517338288522a176b8
onboarder.near <onboarder.near>
2023-05-22 23:52:14 +0000

    Update BOSDirectory by onboarder.near at 2023-05-22T23:52:14.216Z

--- onboarder.near/BOSDirectory/source_code.jsx
+++ onboarder.near/BOSDirectory/source_code.jsx
@@ -1,3 +1,85 @@
+const Wrapper = styled.div`
+  --section-gap: 42px;
+  padding-top: 42px;
+
+  @media (max-width: 1160px) {
+    .line-rounded-corners {
+      display: none !important;
+    }
+  }
+
+  @media (max-width: 900px) {
+    padding-top: 0;
+  }
+`;
+
+const H1 = styled.h1`
+  font-family: "FK Grotesk", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 90px;
+  line-height: 1;
+  text-align: center;
+  letter-spacing: -0.03em;
+  color: #000;
+  margin: 0;
+  max-width: 700px;
+
+  span {
+    display: inline-block;
+    background: #6CE89F;
+    border-radius: 20px;
+    position: relative;
+    padding: 0.1em 0.2em 0;
+
+    svg {
+      position: absolute;
+      bottom: -8px;
+      right: -10px;
+      width: 24px;
+    }
+  }
+
+  @media (max-width: 900px) {
+    font-size: 50px;
+
+    span {
+      border-radius: 12px;
+      svg {
+        position: absolute;
+        bottom: -6px;
+        right: -7px;
+        width: 16px;
+      }
+    }
+  }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  gap: 15px;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: "nowrap";
+
+    @media (max-width: 900px) {
+    flex-direction: column;
+    gap: var(--section-gap);
+    }
+`;
+
+const Container = styled.div`
+  display: flex;
+  max-width: 1060px;
+  margin: 0 auto;
+  gap: var(--section-gap);
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: var(--section-gap) 24px;
+`;
+
 const social = {
   data: [
     {
@@ -89,19 +171,110 @@ const social = {
   ],
 };
 
+const Cards = styled.div`
+  display: flex;
+  gap: 1.4rem;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-top: 4rem;
+`;
+
+const Card = styled.div`
+  width: 25%;
+  min-width: 250px;
+  display: flex;
+   flex-flow: column nowrap;
+   -ms-flex-flow:column nowrap;
+   align-items:center;
+  //  background-color:#09011a;
+   border-radius: 10px;
+   border: 1.41429px solid rgba(28,27,28,.1);
+  box-shadow: 5.65714px 5.65714px 11.3143px rgba(28,27,28,.04);
+   padding: 8px;
+  //  color: #fff;
+   margin: 0 auto;
+   max-width: 400px;
+   flex:1;
+   &:hover img{
+     transform:scale(1.05);
+   }
+   & img{
+    width: 100%;
+    height: 100%;
+    object-fit:cover;
+    overflow: hidden;
+    transition: all 0.3s ease-in-out;
+   }
+`;
+
+const Hero = styled.div`
+  height: 20vh;
+  display: flex;
+  align-items:center;
+  justify-content: center;
+  align-content:center;
+  // background-color:#09011a;
+  // color: white;
+`;
+
+const CardHeading = styled.h5`
+  font-size: 1.25rem;
+  font-weght: 500;
+  color:#09011a;
+`;
+
+const Text = styled.div`
+  opacity: .6;
+`;
+
+const ImageCard = styled.div`
+  height:200px;
+  width: 100%;
+  border-radius: inherit;
+  overflow:hidden;
+  margin-bottom: .5rem;
+  &>img{
+  object-fit: cover;
+  transition: all 0.3s ease-in-out;
+  }
+  &>img:hover{
+    transform:scale(1.05);
+  }
+`;
+
 return (
   <div className="row">
-    <h1 className="align-text">🔎 BOS Viewer Directory</h1>
-    {social.data.map((item) => (
-      <div key={item.key} className="card rounded-3 col-lg-3 margin-1">
-        <a href={item.url} target="_blank" rel="noopener noreferrer">
-          <img src={item.image} class="card-img-top" alt="..."></img>
-        </a>
-        <div class="card-body">
-          <h2 class="card-title">{item.name}</h2>
-          <p class="card-text">{item.description}</p>
-        </div>
-      </div>
-    ))}
+    <Wrapper>
+      <Container>
+        <H1>
+          Gateway{" "}
+          <span>
+            {" "}
+            Directory{" "}
+            <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
+              <path
+                d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
+                fill="#323330"
+              />
+            </svg>
+          </span>
+        </H1>
+      </Container>
+    </Wrapper>
+    <Cards>
+      {social.data.map((item) => (
+        <Card key={item.key}>
+          <ImageCard>
+            <a href={item.url} target="_blank" rel="noopener noreferrer">
+              <img src={item.image} alt="..." />
+            </a>
+          </ImageCard>
+          <div class="card-body p-2 mt-3">
+            <CardHeading>{item.name}</CardHeading>
+            <Text class="ps-2  pb-3 text-secondary">{item.description}</Text>
+          </div>
+        </Card>
+      ))}
+    </Cards>
   </div>
 );
