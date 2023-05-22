83f64daf5c7fb26c7cba490c235204af514061df
hack.near <hack.near>
2023-05-22 05:30:59 +0000

    Update DAO.Groups by hack.near at 2023-05-22T05:30:59.255Z

--- hack.near/dao.groups/source_code.jsx
+++ hack.near/dao.groups/source_code.jsx
@@ -3,10 +3,6 @@ const daoId = props.daoId ?? "multi.sputnik-dao.near";
 
 const policy = Near.view(daoId, "get_policy");
 
-if (policy === null) {
-  return "";
-}
-
 const groups = policy.roles
   .filter((role) => role.kind.Group)
   .map((role) => ({
@@ -44,11 +40,13 @@ return (
   <div className="mb-3">
     <div className="row">
       <div>
-        <h1>membership</h1>
+        <h1>Membership</h1>
       </div>
       <div className="mb-2">
-        <h3>who?</h3>
-        <h5 className="mb-1">account id:</h5>
+        <h3 className="mb-3">
+          <b>Suggest Changes</b>
+        </h3>
+        <h5 className="mb-1">Potential Member ~ Account ID:</h5>
         <div className="mt-2">
           <input
             type="text"
@@ -60,25 +58,27 @@ return (
       </div>
       {!isMember && (
         <div className="mt-2">
-          <h5>nominate for a role:</h5>
-          <div className="mb-2 d-flex gap-2 flex-wrap">
-            {groups.map((group, i) => (
-              <Widget
-                key={i}
-                src="hack.near/widget/DAO.AddMember"
-                props={{
-                  daoId: daoId,
-                  accountId: accountId,
-                  memberId: state.memberId,
-                  roleId: group.name,
-                }}
-              />
-            ))}
-          </div>
+          {validAccount && <h5>Nominate for a Specific Role:</h5>}
+          {validAccount && (
+            <div className="mb-2 d-flex gap-2 flex-wrap">
+              {groups.map((group, i) => (
+                <Widget
+                  key={i}
+                  src="hack.near/widget/DAO.AddMember"
+                  props={{
+                    daoId: daoId,
+                    accountId: accountId,
+                    memberId: state.memberId,
+                    roleId: group.name,
+                  }}
+                />
+              ))}
+            </div>
+          )}
         </div>
       )}
       <div className="mt-2">
-        <h2 className="mb-2">groups</h2>
+        <h2 className="mb-2">Groups</h2>
         <Widget src="hack.near/widget/DAO.Members" props={{ daoId: daoId }} />
       </div>
     </div>
