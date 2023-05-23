49cadb78c9c472a6e65096825dddc65e2bd846f9
efiz.near <efiz.near>
2023-05-23 01:55:43 +0000

    Update Every.Thing.Create by efiz.near at 2023-05-23T01:55:43.492Z

--- efiz.near/every.thing.create/source_code.jsx
+++ efiz.near/every.thing.create/source_code.jsx
@@ -1,104 +1,49 @@
-const postThing = props.postThing;
-const pPath = props.pPath || null;
-const availableTypes = JSON.parse(props.availableTypes || "null");
-
-if (!availableTypes) {
-  const types = Social.get("every.near/type/**", "final");
-  if (!types) {
-    return <></>;
-  }
-  availableTypes = Object.keys(types).map((it) => `every.near/type/${it}`);
-}
+const action = props.action || "";
+const type = props.type || "";
 
 State.init({
-  selectedType: "",
-  expanded: false,
+  selectedAction: action,
+  selectedType: type,
 });
 
-function extractMentions(text) {
-  const mentionRegex =
-    /@((?:(?:[a-z\d]+[-_])*[a-z\d]+\.)*(?:[a-z\d]+[-_])*[a-z\d]+)/gi;
-  mentionRegex.lastIndex = 0;
-  const accountIds = new Set();
-  for (const match of text.matchAll(mentionRegex)) {
-    if (
-      !/[\w`]/.test(match.input.charAt(match.index - 1)) &&
-      !/[/\w`]/.test(match.input.charAt(match.index + match[0].length)) &&
-      match[1].length >= 2 &&
-      match[1].length <= 64
-    ) {
-      accountIds.add(match[1].toLowerCase());
-    }
-  }
-  return [...accountIds];
-}
-
-function extractTagNotifications(text, item) {
-  return extractMentions(text || "")
-    .filter((accountId) => accountId !== context.accountId)
-    .map((accountId) => ({
-      key: accountId,
-      value: {
-        type: "mention",
-        item,
-      },
-    }));
-}
-
 const composeData = () => {
-  // generate a random id
-  const thingId = state.thingId || Math.random();
-  const data = {
-    thing: {
-      ...state.extra,
-      [thingId]: JSON.stringify({
-        data: state.thing,
-        pPath,
-        type: state.selectedType,
-      }),
-    },
-    index: {
-      thing: JSON.stringify({
-        key: thingId,
-        value: {
+  // generate id
+  const thingId = "123";
+  return {
+    data: {
+      thing: {
+        [thingId]: JSON.stringify({
+          data: state.thing,
           type: state.selectedType,
-        },
-      }),
+        }),
+      },
+      index: {
+        thing: JSON.stringify({
+          key: thingId,
+          value: {
+            type: state.selectedType,
+          },
+        }),
+      },
     },
   };
-
-  // TODO: What other types can we extract mentions from?
-  // How can this be better associated with the type?
-  if (state.selectedType === "md") {
-    const notifications = extractTagNotifications(state.thing.text, {
-      type: "social",
-      path: `${context.accountId}/thing/${thingId}`,
-    });
-
-    if (notifications.length) {
-      data.index.notify = JSON.stringify(
-        notifications.length > 1 ? notifications : notifications[0]
-      );
-    }
-  }
-
-  if (postThing) {
-    data = postThing(data);
-  }
-  return data;
 };
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 4px;
-  margin: 20px;
+  align-items: center;
 `;
 
 const Row = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 10px;
+  width: 100%;
+`;
+
+const Select = styled.select`
+  padding: 5px;
   width: 100%;
 `;
 
@@ -118,99 +63,75 @@ const ButtonContainer = styled.div`
 `;
 
 const TextContainer = styled.div`
+
     margin-left: 4px;
 `;
 
-const FormContainer = styled.div`
-  margin: 20px;
-`;
+const handleActionChange = (e) => {
+  State.update({ selectedAction: e.target.value });
+};
 
-const Select = styled.select`
-  height: 30px;
-  width: 300px;
-`;
+const handleQuantityToggle = () => {
+  State.update({ isMultiple: !state.isMultiple });
+};
 
-const Button = styled.button`
-  height: 30px;
-`;
-
-const Text = styled.p`
-  display: inline-block;
-  margin-right: 10px;
-`;
-
-const Input = styled.input`
-  width: 300px;
-  height: 30px;
-`;
+// <ButtonContainer>
+//   <SwitchButton
+//     active={state.isMultiple}
+//     onClick={handleQuantityToggle}
+//   >
+//     {state.isMultiple ? "Many" : "One"}
+//   </SwitchButton>
+// </ButtonContainer>
 
 const handleTypeChange = (e) => {
   State.update({ selectedType: e.target.value });
 };
 
-const handleThingData = (value, extra) => {
-  State.update({ thing: value, extra });
-};
+type = JSON.parse(Social.get(state.selectedType, "final") || null);
 
-function RenderTypeCreate() {
-  if (state.selectedType !== "") {
-    const type = JSON.parse(Social.get(state.selectedType, "final") || "null");
-    const widgetSrc = type?.widgets?.create;
-    // it would be great to modify the onChange function
-    return (
-      <Widget
-        src={widgetSrc || "efiz.near/widget/create"}
-        props={{ onChange: handleThingData, type: state.selectedType }}
-      />
-    );
-  }
-}
+// then this has the create button
+// and builds the onChange state
+
+const handleThingData = (value) => {
+  console.log(value);
+  State.update({ thing: value });
+};
 
 return (
   <>
     <Container>
-      {props.type === undefined && availableTypes.length !== 1 ? (
-        <>
-          <Row>
-            <TextContainer>create a thing of type:</TextContainer>
-          </Row>
-          <Row>
-            <Select value={state.selectedType} onChange={handleTypeChange}>
-              <option value="">Select a type</option>
-              {availableTypes.map((it) => (
-                <option value={it} key={it}>
-                  {it}
-                </option>
-              ))}
-            </Select>
-          </Row>
-        </>
-      ) : null}
-
-      <RenderTypeCreate />
-      <div>
-        <Button onClick={() => State.update({ expanded: !state.expanded })}>
-          optional {state.expanded ? "-" : "+"}
-        </Button>
-        <Row>
-          {state.expanded ? (
-            <>
-              <Input
-                onChange={(e) => State.update({ thingId: e.target.value })}
-                placeholder="file name"
-              />
-            </>
-          ) : null}
-        </Row>
-        <CommitButton
-          force
-          data={composeData()}
-          disabled={!state.thing}
-          className="styless"
-        >
-          create
-        </CommitButton>
-      </div>
+      <Row>
+        <Select value={state.selectedAction} onChange={handleActionChange}>
+          <option value="">Select an action</option>
+          <option value="view">view</option>
+          <option value="create">create</option>
+          <option value="edit">edit</option>
+        </Select>
+      </Row>
+      <Row>
+        <TextContainer>a thing of type:</TextContainer>
+      </Row>
+      <Row>
+        <Select value={state.selectedType} onChange={handleTypeChange}>
+          <option value="">Select a type</option>
+          <option value="efiz.near/type/Image">image</option>
+        </Select>
+      </Row>
+      {type?.widgets[state.selectedAction] && (
+        <Widget
+          src={type?.widgets[state.selectedAction]}
+          props={{ onChange: handleThingData }} // onChange
+        />
+      )}
     </Container>
+    <CommitButton
+      force
+      data={composeData()}
+      disabled={!state.thing}
+      className="styless"
+    >
+      create
+    </CommitButton>
   </>
 );
