442453adf55bcbb0d3146d84a73beb170fbc863d
root.akaia.near <root.akaia.near>
2023-05-23 07:20:54 +0000

    Update gigs-board.entity.github-repo.board by root.akaia.near at 2023-05-23T07:20:54.598Z

--- root.akaia.near/gigs-board.entity.github-repo.board/source_code.jsx
+++ root.akaia.near/gigs-board.entity.github-repo.board/source_code.jsx
@@ -7,29 +7,6 @@ const nearDevGovGigsWidgetsAccountId =
   props.nearDevGovGigsWidgetsAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
 
-/**
- * Reads a board config from DevHub contract storage.
- * Currently a mock.
- *
- * Boards are indexed by their ids.
- */
-const boardConfigByBoardId = ({ boardId }) => {
-  return {
-    probablyUUIDv4: {
-      id: "probablyUUIDv4",
-      columns: [
-        { title: "Draft", labelFilters: ["S-draft"] },
-        { title: "Review", labelFilters: ["S-review"] },
-        { title: "HALP!", labelFilters: ["help"] },
-      ],
-      dataTypes: { Issue: true, PullRequest: true },
-      description: "Latest NEAR Enhancement Proposals by status",
-      repoURL: "https://github.com/near/NEPs",
-      title: "NEAR Protocol NEPs",
-    },
-  }[boardId];
-};
-
 function widget(widgetName, widgetProps, key) {
   widgetProps = {
     ...widgetProps,
@@ -73,32 +50,44 @@ function href(widgetName, linkProps) {
     linkPropsQuery ? "?" : ""
   }${linkPropsQuery}`;
 }
+/* END_INCLUDE: "common.jsx" */
 
-const formHandler =
-  ({ formStateKey }) =>
-  ({ fieldName, updateHandler }) =>
-  (input) =>
-    State.update((lastState) => ({
-      ...lastState,
-
-      [formStateKey]: {
-        ...lastState[formStateKey],
-
-        [fieldName]:
-          typeof updateHandler === "function"
-            ? updateHandler({
-                input: input?.target?.value ?? input ?? null,
-                lastState,
-              })
-            : input?.target?.value ?? input ?? null,
-      },
-    }));
+/* INCLUDE: "shared/lib/gui" */
+const Card = styled.div`
+  &:hover {
+    box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;
+  }
+`;
 
 const CompactContainer = styled.div`
   width: fit-content !important;
   max-width: 100%;
 `;
-/* END_INCLUDE: "common.jsx" */
+/* END_INCLUDE: "shared/lib/gui" */
+
+const dataToColumns = (data, columns) =>
+  Object.values(columns).reduce(
+    (registry, column) => ({
+      ...registry,
+
+      [column.id]: [
+        ...(registry[column.id] ?? []),
+
+        ...data.filter((ticket) =>
+          ticket.labels.some((label) =>
+            column?.labelTerms.some(
+              (searchTerm) =>
+                searchTerm.length > 0 && label.name.includes(searchTerm)
+            )
+          )
+        ),
+      ],
+    }),
+
+    {}
+  );
+
+const withType = (type) => (data) => ({ ...data, type });
 
 const GithubRepoBoard = ({
   columns,
@@ -109,32 +98,8 @@ const GithubRepoBoard = ({
   title,
 }) => {
   State.init({
-    pullRequestByColumn: {},
-    issueByColumn: {},
-    ticketByColumn: {},
+    ticketsByColumn: {},
   });
-
-  const dataToColumns = (data) =>
-    columns.reduce(
-      (registry, column) => ({
-        ...registry,
-
-        [column.title]: [
-          ...(registry[column.title] ?? []),
-
-          ...data.filter((pullRequest) =>
-            pullRequest.labels.some((label) =>
-              column?.labelFilters.some(
-                (searchTerm) =>
-                  searchTerm.length > 0 && label.name.includes(searchTerm)
-              )
-            )
-          ),
-        ],
-      }),
-
-      {}
-    );
 
   if (repoURL) {
     const pullRequests = dataTypes.PullRequest
@@ -145,7 +110,7 @@ const GithubRepoBoard = ({
               .slice(-2)
               .join("/")}/pulls`
           ).body ?? []
-        ).map((pullRequest) => ({ ...pullRequest, type: "PullRequest" }))
+        ).map(withType("PullRequest"))
       : [];
 
     const issues = dataTypes.Issue
@@ -156,73 +121,87 @@ const GithubRepoBoard = ({
               .slice(-2)
               .join("/")}/issues`
           ).body ?? []
-        ).map((issue) => ({ ...issue, type: "Issue" }))
+        ).map(withType("Issue"))
       : [];
 
     State.update({
-      ticketByColumn: dataToColumns([...issues, ...pullRequests]),
+      ticketsByColumn: dataToColumns([...issues, ...pullRequests], columns),
     });
   }
 
   return (
-    <div className="d-flex flex-column gap-3">
-      <div className="d-flex justify-content-between">
-        <i className="placeholder" />
-
-        <h5 className="h5 d-inline-flex gap-2 m-0">
-          <i className="bi bi-kanban-fill" />
-          <span>{title} board</span>
-        </h5>
-
-        {pageURL ? (
-          <a
-            className="card-link d-inline-flex"
-            href={pageURL}
-            rel="noreferrer"
-            role="button"
-            target="_blank"
-            title="Link to this board"
-          >
-            <span className="hstack gap-3">
-              <i className="bi bi-share" />
-              <span>Link to this board</span>
-            </span>
-          </a>
-        ) : (
+    <div className="d-flex flex-column gap-4 py-4">
+      <div className="d-flex flex-column gap-2">
+        <div className="d-flex justify-content-between">
           <i className="placeholder" />
-        )}
+
+          <h5 className="h5 d-inline-flex gap-2 m-0">
+            <i className="bi bi-kanban-fill" />
+            <span>{title}</span>
+          </h5>
+
+          {pageURL ? (
+            <a
+              className="card-link d-inline-flex"
+              href={pageURL}
+              rel="noreferrer"
+              role="button"
+              target="_blank"
+              title="Link to this board"
+            >
+              <span className="hstack gap-3">
+                <i className="bi bi-share" />
+                <span>Link to this board</span>
+              </span>
+            </a>
+          ) : (
+            <i className="placeholder" />
+          )}
+        </div>
+
+        <div className="py-1 text-secondary text-center">{description}</div>
       </div>
 
-      <div className="py-1 text-secondary text-center">{description}</div>
-
       <div className="d-flex gap-3" style={{ overflowX: "auto" }}>
-        {columns.length > 0 ? (
-          columns.map((column) => (
-            <div className="col-3" key={column.title}>
-              <CompactContainer className="card">
-                <CompactContainer className="card-body d-flex flex-column gap-3 border-secondary">
-                  <h6 className="card-title d-flex align-items-center gap-2">
+        {Object.keys(columns).length > 0 ? (
+          Object.values(columns).map((column) => (
+            <div className="col-3" key={column.id}>
+              <div className="card">
+                <div
+                  className={[
+                    "card-body d-flex flex-column gap-3",
+                    "border border-2 border-secondary rounded-2",
+                  ].join(" ")}
+                >
+                  <h6 className="card-title h6 d-flex align-items-center gap-2 m-0">
                     {column.title}
 
                     <span className="badge rounded-pill bg-secondary">
-                      {(state.ticketByColumn[column.title] ?? []).length}
+                      {(state.ticketsByColumn[column.id] ?? []).length}
                     </span>
                   </h6>
 
-                  {(state.ticketByColumn[column.title] ?? []).map((data) =>
-                    widget(
-                      "entity.github-repo.ticket",
-                      { data, format: "card" },
-                      data.id
-                    )
-                  )}
-                </CompactContainer>
-              </CompactContainer>
+                  <p class="text-secondary m-0">{column.description}</p>
+
+                  <div class="d-flex flex-column gap-3">
+                    {(state.ticketsByColumn[column.id] ?? []).map((data) =>
+                      widget(
+                        "entity.github-repo.ticket",
+                        { data, format: "card" },
+                        data.id
+                      )
+                    )}
+                  </div>
+                </div>
+              </div>
             </div>
           ))
         ) : (
           <div
-            className="d-flex align-items-center justify-content-center w-100 text-black-50 opacity-50"
+            className={[
+              "d-flex align-items-center justify-content-center",
+              "w-100 text-black-50 opacity-50",
+            ].join(" ")}
             style={{ height: 384 }}
           >
             No columns were created so far.
