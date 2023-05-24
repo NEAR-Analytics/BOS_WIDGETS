3ab61751e64c950c9a2a73200a576b99cda2fb9b
marmajgaming.near <marmajgaming.near>
2023-05-24 12:02:29 +0000

    Update MJG by marmajgaming.near at 2023-05-24T12:02:29.202Z

--- marmajgaming.near/MJG/source_code.jsx
+++ marmajgaming.near/MJG/source_code.jsx
@@ -1,43 +1,156 @@
-const Container = styled.div`
-  background-color: #2eabc4;
-  color: #000;
-  padding: 20px;
-}
-
-`;
-
-const Title = styled.h1`
-  color: #fff;
-  font-weight: 700;
-  font-size: 2em;
-  text-align: center;
-  text-transform: uppercase;
-  margin-bottom: 20px;
-`;
-
-const Description = styled.p`
-  color: #fff;
-  font-weight: 300;
-  text-align: center;
-  margin-bottom: 20px;
-`;
-
 return (
-  <Container>
-    <Widget
-      src="jay100.near/widget/CTTDescription"
-      props={{
-        title: "Marma J Gaming",
-        description: `Explore the crypto-based gaming world of Marma J Gaming where you can
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      maxWidth: "800px",
+      margin: "0 auto",
+      padding: "20px",
+    }}
+  >
+    <h1 style={{ marginBottom: "10px", textAlign: "center" }}>
+      <a
+        href="https://marmaj.org/gaming/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        Marma J Gaming
+      </a>
+    </h1>
+    <p
+      style={{
+        marginBottom: "20px",
+        fontSize: "1.3em",
+        fontWeight: "bold",
+      }}
+    >
+      EARN TO PLAY Games
+    </p>
+    <p
+      style={{
+        marginBottom: "20px",
+        width: "100%",
+        textAlign: "justify",
+      }}
+    >
+      Explore the crypto-based gaming world of Marma J Gaming where you can
       collaborate artistically with friends, gather your pets for a battle,
       outfit your hero to explore dungeons and take part in raids, and gather an
-      army to position yourself as the master tactician.`,
-      }}
-    />
-    <Widget src={"jay100.near/widget/PixelParty-Widget"} />
-    <Widget src={"jay100.near/widget/PixelPets-Widget"} />
-    <Widget src={"jay100.near/widget/CH-Widget"} />
-    <Widget src={"jay100.near/widget/CTT-Widget"} />
-    <Widget />
-  </Container>
+      army to position yourself as the master tactician.
+    </p>
+    <div style={{ marginBottom: "20px", width: "100%" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h3>Pixel Party</h3>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <a
+            href="https://pixelparty.marmaj.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>PLAY</button>
+          </a>
+          <a
+            href="https://testnet.pixelparty.marmaj.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ marginLeft: "10px" }}
+          >
+            <button>TEST</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div style={{ marginBottom: "20px", width: "100%" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h3>Pixel Pets</h3>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <a
+            href="https://pd.marmaj.org/pixelpets"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>PLAY</button>
+          </a>
+          <a
+            href="https://pd-testnet.marmaj.org/pixelpets/testnet.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ marginLeft: "10px" }}
+          >
+            <button>TEST</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div style={{ marginBottom: "20px", width: "100%" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h3>Crypto Heroes</h3>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <a
+            href="https://pd.marmaj.org/cryptoheroes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>PLAY</button>
+          </a>
+          <a
+            href="https://pd-testnet.marmaj.org/cryptoheroes/testnet.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ marginLeft: "10px" }}
+          >
+            <button>TEST</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div style={{ marginBottom: "20px", width: "100%" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h3>Chain Team Tactics</h3>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <a
+            href="https://pd.marmaj.org/chainteam"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>PLAY</button>
+          </a>
+          <a
+            href="https://pd-testnet.marmaj.org/chainteam/testnet.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ marginLeft: "10px" }}
+          >
+            <button>TEST</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 );
