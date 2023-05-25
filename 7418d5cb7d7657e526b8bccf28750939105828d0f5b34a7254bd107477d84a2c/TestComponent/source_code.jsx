a5e1953bae3f02ceabdface2940a83d5117a3757
7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c <7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c>
2023-05-25 10:06:42 +0000

    Update TestComponent by 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c at 2023-05-25T10:06:42.488Z

--- 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/TestComponent/source_code.jsx
+++ 7418d5cb7d7657e526b8bccf28750939105828d0f5b34a7254bd107477d84a2c/TestComponent/source_code.jsx
@@ -1,8 +1,9 @@
+let items = Social.get(`${accountId}/testPersons/**`);
 const myState = State.init({
   name: "",
   profession: "",
   description: "",
-  allPersons: Social.get(`${accountId}/testPersons/**`),
+  allPersons: items ? items : {},
 });
 
 //Upload data on chain
@@ -12,18 +13,40 @@ function uploadData() {
   });
 }
 
+//Add items to the local state
+function addItem() {
+  let currItems = myState.allItems;
+  //If key has space
+  let key = Object.keys(myState.allItems).length;
+
+  currItems[key] = {
+    name: myState.name,
+    description: myState.description,
+    profession: myState.profession,
+  };
+
+  State.update({
+    key: "",
+    value: "",
+    linkValue: "",
+    allItems: currItems,
+  });
+
+  console.log(myState);
+}
+
 return (
   <div>
     <label for="name">Name: </label>
     <input
-      type="number"
+      type="text"
       id="name"
       value={myState.name}
       onChange={(e) => State.update({ name: e.target.value })}
     />
     <label for="profession">Profession: </label>
     <input
-      type="number"
+      type="text"
       id="profession"
       value={myState.profession}
       onChange={(e) => State.update({ profession: e.target.value })}
@@ -31,11 +54,14 @@ return (
 
     <label for="description">Description: </label>
     <input
-      type="number"
+      type="text"
       id="description"
       value={myState.description}
       onChange={(e) => State.update({ description: e.target.value })}
     />
-    <button>Submut</button>
+    <button onClick={addItem}>Add item</button>
+    <CommitButton data={{ testWidget: myState.allItems }} onCommit={onCommit}>
+      Upload data
+    </CommitButton>
   </div>
 );
