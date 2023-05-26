e5c485a018422e5016d46be6a7ef214a83098e98
efiz.near <efiz.near>
2023-05-26 14:09:16 +0000

    Update Create by efiz.near at 2023-05-26T14:09:16.471Z

--- efiz.near/Create/source_code.jsx
+++ efiz.near/Create/source_code.jsx
@@ -1,42 +1,12 @@
-State.init({
-  cid: "",
-});
-
-const setImageCid = (cid) => {
-  State.update({ cid });
-};
-
 function composeData() {
-  const data = {
-    thing: {
-      123: JSON.stringify({
-        dataSource: "IFPS",
-        dataSourceArgs: {
-          arg1: state.cid,
-        },
-        type: "image",
-      }),
-    },
-    index: {
-      thing: JSON.stringify({
-        key: "123",
-        value: {
-          type: "image",
-        },
-      }),
-    },
-  };
+  const data = {};
 
   return data;
 }
 
 return (
   <>
-    <Widget
-      src="efiz.near/widget/Create.Image"
-      props={{ callback: setImageCid }}
-    />
-    <CommitButton force data={composeData} className="styless">
+    <CommitButton force data={composeData}>
       create
     </CommitButton>
   </>
