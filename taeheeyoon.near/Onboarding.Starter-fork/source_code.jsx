073572c41b3782fffbbf9b51353a822c7809cf8c
taeheeyoon.near <taeheeyoon.near>
2023-05-26 15:29:58 +0000

    Update Onboarding.Starter-fork by taeheeyoon.near at 2023-05-26T15:29:58.074Z

--- taeheeyoon.near/Onboarding.Starter-fork/source_code.jsx
+++ taeheeyoon.near/Onboarding.Starter-fork/source_code.jsx
@@ -1,9 +1,11 @@
 return (
   <>
     {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
-    <Widget src="near/widget/Onboarding.ComponentCard" props={{ accountId }} />
+    <Widget src="near/widget/Onboarding.ComponentCard" props={{}} />
     <br />
     {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
     <Widget src="near/widget/Onboarding.ComponentCollection" props={{}} />
+    <br />
+    <Widget src="hackerhouse.near/widget/EVMComponents" props={{}} />
   </>
 );
