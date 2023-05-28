6f846028b457bd2492fd8b43e9efd92d768eb35e
jgdev.near <jgdev.near>
2023-05-28 19:06:37 +0000

    Update DevGov.Notification.Item.Right by jgdev.near at 2023-05-28T19:06:37.459Z

--- jgdev.near/DevGov.Notification.Item.Right/source_code.jsx
+++ jgdev.near/DevGov.Notification.Item.Right/source_code.jsx
@@ -83,7 +83,8 @@ const WrapperWidget = ({ children, id, storageType }) => {
   return React.Children.map(children, (child) =>
     React.cloneElement(child, { handleStateChange, initState })
   );
-};/* END_INCLUDE: "common.jsx" */
+};
+/* END_INCLUDE: "common.jsx" */
 
 return props.post === undefined ? (
   "Loading ..."
