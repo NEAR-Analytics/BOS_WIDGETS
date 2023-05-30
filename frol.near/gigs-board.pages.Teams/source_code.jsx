1c43dd95b42609d6eaa78a1dae7c43fca38d9282
frol.near <frol.near>
2023-05-30 20:19:09 +0000

    Update gigs-board.pages.Teams by frol.near at 2023-05-30T20:19:09.204Z

--- frol.near/gigs-board.pages.Teams/source_code.jsx
+++ frol.near/gigs-board.pages.Teams/source_code.jsx
@@ -11,6 +11,7 @@ function widget(widgetName, widgetProps, key) {
     ...widgetProps,
     nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
     nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
   };
   return (
     <Widget
@@ -31,10 +32,13 @@ function href(widgetName, linkProps) {
     linkProps.nearDevGovGigsWidgetsAccountId =
       props.nearDevGovGigsWidgetsAccountId;
   }
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
   const linkPropsQuery = Object.entries(linkProps)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
-  return `#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
     linkPropsQuery ? "?" : ""
   }${linkPropsQuery}`;
 }
@@ -64,4 +68,6 @@ const pageContent = (
   </div>
 );
 
-return widget("components.layout.Page", { children: pageContent });
+return widget("components.layout.Page", {
+  children: pageContent,
+});
