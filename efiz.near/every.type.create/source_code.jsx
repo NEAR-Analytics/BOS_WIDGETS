688088d89e060cb45000dfbd79fc6bfe741df7f9
efiz.near <efiz.near>
2023-05-30 14:56:47 +0000

    Update every.type.create by efiz.near at 2023-05-30T14:56:47.685Z

--- efiz.near/every.type.create/source_code.jsx
+++ efiz.near/every.type.create/source_code.jsx
@@ -1,157 +1,189 @@
-// Repository: https://github.com/near-everything/type-creator
-const externalAppUrl = "https://type-creator.vercel.app/";
-const accountId = context.accountId;
+const type = props.type || null;
+const blockHeight = props.blockHeight || "final";
 
-/**
- * Initial Path (optional but recommended)
- */
-const path = props.path;
-/**
- * Initial view height (optional but recommended)
- */
-const initialViewHeight = 800;
-const initialPayload = {};
+if (type) {
+  const parts = type.split("/");
+  type = JSON.parse(Social.get(type, blockHeight) || null);
+  type.name = parts[2];
+}
 
-/**
- * Request Handlers - Backend.
- *
- * - request: payload sent by External App
- *
- * - response: method to send the answer back to the External App
- *
- * - utils: Utils features like
- *      - promisify: (caller, resolve, reject)
- *      There's no Promisse for some features yet, So this is util for when you need to get cached data using DiscoveryAPI, e.g:
- *      utils.promisify(() => Social.getr(`${context.accountId}/profile`), (res) => console.log(res), (err) => console.log(err))
- *
- * @param {{type: string, payload: {}}} request request with payload sent by External App
- * @param {(request) => {send: () => void}} response send the answer back to the External App
- * @param {{promisify:(caller: () => void, resolve: (data) => void, reject: (error) => void)}} utils Utils features like
- */
-const requestHandler = (request, response, Utils) => {
-  switch (request.type) {
-    case "create-type":
-      handleCreateType(request, response);
-      break;
-    case "get-types":
-      handleGetTypes(request, response, Utils);
-      break;
-    case "get-type-details":
-      handleGetTypeDetails(request, response, Utils);
-      break;
-  }
-};
+State.init({
+  typeName: type.name || "",
+  properties: type.properties || [],
+  newPropertyName: "",
+  newPropertyType: "string",
+  newPropertyRequired: false,
+});
 
-const handleCreateType = (request, response) => {
-  const { payload } = request;
-  if (payload) {
-    console.log(JSON.stringify(payload));
-    Social.set(
-      {
-        type: {
-          [payload.name]: {
-            "": JSON.stringify({
-              properties: payload.properties,
-              widgets: {
-                view: payload.widgets.view,
-                create: payload.widgets.create,
-              },
-            }),
-          },
-        },
-        // widget: {
-        //   [`Everything.Summary.${payload.name}`]: {
-        //     "": `const data= props.data; return (<><p>Configure <a href="/#/edit/${accountId}/widget/Everything.Summary.${payload.name}">this widget</a> to attractively display your data below:</p><p>{JSON.stringify(data)}</p></>);`,
-        //   },
-        //   [`Everything.View.${payload.name}`]: {
-        //     "": `const data= props.data; return (<><p>Configure <a href="/#/edit/${accountId}/widget/Everything.View.${payload.name}">this widget</a>to attractively display your data below:</p><p>{JSON.stringify(data)}</p></>);`,
-        //   },
-        //   [`Everything.Create.${payload.name}`]: {
-        //     "": `return (<><p>Click deploy below then put the app url in the <a href="/#/edit/${accountId}/widget/Everything.Create.${payload.name}">create widget</a>.</p><a href="https://vercel.com/new/clone?repository-url=https://github.com/near-everything/thing-creator" target="_blank"><img src="https://vercel.com/button" alt="Deploy with Vercel" /></a><Widget src={"evrything.near/widget/Bridge"} props={{ externalAppUrl: "", type: "${accountId}/type/${payload.name}" }} /></>);`,
-        //   },
-        // },
-      },
-      {
-        force: true,
-        onCommit: () => {
-          response(request).send({ success: true });
-        },
-        onCancel: () => {
-          response(request).send({ error: "the action was canceled" });
-        },
-      }
-    );
-    return;
-  }
-  // Error
-  response(request).send({
-    error: "type must be provided",
+const FormContainer = styled.div`
+  margin: 20px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  max-width: 200px;
+  margin-bottom: 10px;
+  height: 30px;
+`;
+
+const Select = styled.select`
+  height: 30px;
+`;
+
+const Button = styled.button`
+  height: 30px;
+`;
+
+const Text = styled.p`
+  display: inline-block;
+  margin-right: 10px;
+`;
+
+const handleAddProperty = () => {
+  if (state.newPropertyName.trim() === "") return;
+
+  const newProperty = {
+    name: state.newPropertyName,
+    type: state.newPropertyType,
+    required: state.newPropertyRequired,
+  };
+
+  State.update({
+    properties: [...state.properties, newProperty],
+    newPropertyName: "",
+    newPropertyType: "heading-one",
+    newPropertyRequired: false,
   });
 };
 
-const previewString = (str, maxLength) => {
-  if (str.length <= maxLength) {
-    return str;
-  }
-  const previewLength = Math.floor((maxLength - 3) / 2);
-  const previewStart = str.slice(0, previewLength);
-  const previewEnd = str.slice(str.length - previewLength);
-  return `${previewStart}...${previewEnd}`;
+const handleRemoveProperty = (index) => {
+  const updatedProperties = [...state.properties];
+  updatedProperties.splice(index, 1);
+  State.update({ properties: updatedProperties });
 };
 
-const handleGetTypes = (request, response, Utils) => {
-  const accountId = request.payload.accountId ?? "*";
-  return Utils.promisify(
-    // Cached data (may take a while to return a value)
-    () =>
-      Social.keys(`${accountId}/type/*`, "final", {
-        return_type: "BlockHeight",
-        values_only: true,
+const handlePropertyChange = (e, index) => {
+  const updatedProperties = [...state.properties];
+  updatedProperties[index].name = e.target.value.toLowerCase();
+  State.update({ properties: updatedProperties });
+};
+
+const handleTypeChange = (e, index) => {
+  const updatedProperties = [...state.properties];
+  updatedProperties[index].type = e.target.value;
+  State.update({ properties: updatedProperties });
+};
+
+const handleRequiredChange = (e, index) => {
+  const updatedProperties = [...state.properties];
+  updatedProperties[index].required = e.target.value === "true";
+  State.update({ properties: updatedProperties });
+};
+
+const handleTypeNameChange = (e) => {
+  State.update({ typeName: e.target.value.toLowerCase() });
+};
+
+const composeData = () => {
+  const data = {
+    type: {
+      [state.typeName]: JSON.stringify({
+        properties: state.properties,
       }),
-    (types) => {
-      types = Object.entries(types).flatMap(([accountId, { type }]) =>
-        Object.entries(type).map(([name]) => ({
-          accountId,
-          name,
-        }))
-      );
-      response(request).send({
-        types,
-      });
-    }
-  );
+    },
+  };
+  return data;
 };
 
-const handleGetTypeDetails = (request, response, Utils) => {
-  const type = request.payload.type;
-  console.log(JSON.stringify(type));
-  if (type) {
-    return Utils.promisify(
-      // Cached data (may take a while to return a value)
-      () => Social.get(`${type.accountId}/type/${type.name}`),
-      (details) => {
-        console.log(details);
-        response(request).send({
-          details,
-        });
-      }
-    );
-  } else {
-    response(request).send({
-      error: "Type not provided",
-    });
-  }
-};
+function TypeSelect({ value, onChange }) {
+  return (
+    <Select value={value} onChange={onChange}>
+      <option value="string">string</option>
+      <option value="heading-one">h1</option>
+      <option value="paragraph">paragraph</option>
+      <option value="code">code</option>
+      <option value="feed">feed</option>
+    </Select>
+  );
+}
+
+function RequiredSelect({ value, onChange }) {
+  return (
+    <Select value={value} onChange={onChange}>
+      <option value="true">true</option>
+      <option value="false">false</option>
+    </Select>
+  );
+}
 
 return (
-  <Widget
-    src={"wendersonpires.near/widget/NearSocialBridgeCore"}
-    props={{
-      externalAppUrl,
-      path,
-      initialViewHeight,
-      initialPayload,
-      requestHandler,
-    }}
-  />
+  <FormContainer>
+    <Row>
+      <Text>Type Name:</Text>
+      <Input
+        type="text"
+        placeholder="Type Name"
+        value={state.typeName}
+        onChange={handleTypeNameChange}
+      />
+    </Row>
+    <Text>Properties:</Text>
+    {state.properties.map((property, index) => (
+      <Row key={index}>
+        <Input
+          type="text"
+          value={property.name}
+          onChange={(e) => handlePropertyChange(e, index)}
+        />
+        <TypeSelect
+          value={property.type}
+          onChange={(e) => handleTypeChange(e, index)}
+        />
+        <RequiredSelect
+          value={property.required}
+          onChange={(e) => handleRequiredChange(e, index)}
+        />
+        <Button onClick={() => handleRemoveProperty(index)}>Remove</Button>
+      </Row>
+    ))}
+    <Row>
+      <Input
+        type="text"
+        placeholder="Property Name"
+        value={state.newPropertyName}
+        onChange={(e) =>
+          State.update({ newPropertyName: e.target.value.toLowerCase() })
+        }
+      />
+      <TypeSelect
+        value={state.newPropertyType}
+        onChange={(e) => State.update({ newPropertyType: e.target.value })}
+      />
+      <RequiredSelect
+        value={state.newPropertyRequired}
+        onChange={(e) =>
+          State.update({ newPropertyRequired: e.target.value === "true" })
+        }
+      />
+      <Button
+        onClick={handleAddProperty}
+        disabled={state.newPropertyName.trim() === ""}
+      >
+        Add Property
+      </Button>
+    </Row>
+    <CommitButton
+      force
+      data={composeData()}
+      disabled={state.properties.length === 0}
+      className="styless"
+    >
+      create
+    </CommitButton>
+  </FormContainer>
 );
