b8af4a9bbce4cadffb481f0af70e1e1f114309a3
testwiki.near <testwiki.near>
2023-05-31 14:35:26 +0000

    Update WikiOnSocialDB_Comment by testwiki.near at 2023-05-31T14:35:26.690Z

--- testwiki.near/WikiOnSocialDB_Comment/source_code.jsx
+++ testwiki.near/WikiOnSocialDB_Comment/source_code.jsx
@@ -1,7 +1,7 @@
 const addressForArticles = "wikiTest2Article";
 const addressForComments = "wikiTest2Comment";
 const authorForWidget = "testwiki.near";
-State.init({ showReply: false });
+State.init({ showReply: false, copiedShareUrl: false });
 const accountId = props.accountId;
 const blockHeight =
   props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
@@ -16,8 +16,42 @@ const content =
 const parentItem = content.item;
 const highlight = !!props.highlight;
 const raw = !!props.raw;
+const mainPartForSharingComment = props.mainPartForSharingComment;
+const shareUrl = `${mainPartForSharingComment}&commentAccountId=${accountId}&commentBlockHeight=${blockHeight}`;
 
-//TODO - adress should be changed
+const ShareButtonWrapper = styled.div`
+  .button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    padding: 8px 16px;
+    height: 32px;
+    border-radius: 100px;
+    font-weight: 600;
+    font-size: 12px;
+    text-align: center;
+    cursor: pointer;
+    background-color: transparent;
+    border: 0;
+    color: #11181c !important;
+
+    &:hover,
+    &:focus {
+      text-decoration: none;
+      outline: none;
+    }
+
+    i {
+      color: #000;
+    }
+
+    .bi-16 {
+      font-size: 21px;
+    }
+  }
+`;
+
 const link = `#/mob.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`;
 
 return (
@@ -38,7 +72,7 @@ return (
         />
       </div>
       {blockHeight !== "now" && (
-        <div className="mt-1 d-flex justify-content-between">
+        <div className="mt-1 d-flex justify-content-between align-items-center">
           {parentItem && (
             <Widget
               src="mob.near/widget/CommentButton"
@@ -47,6 +81,31 @@ return (
               }}
             />
           )}
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip>Copy URL to clipboard</Tooltip>}
+          >
+            <ShareButtonWrapper>
+              <button
+                className="button"
+                type="button"
+                onMouseLeave={() => {
+                  State.update({ copiedShareUrl: false });
+                }}
+                onClick={() => {
+                  clipboard.writeText(shareUrl).then(() => {
+                    State.update({ copiedShareUrl: true });
+                  });
+                }}
+              >
+                {state.copiedShareUrl ? (
+                  <i className="bi-16 bi bi-check"></i>
+                ) : (
+                  <i className="bi-16 bi-link-45deg"></i>
+                )}
+              </button>
+            </ShareButtonWrapper>
+          </OverlayTrigger>
         </div>
       )}
     </div>
