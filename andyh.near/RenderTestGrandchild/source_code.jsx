8833212ffa72c61bbb33034bb2c63c256c66ca48
andyh.near <andyh.near>
2023-06-01 20:46:41 +0000

    Update RenderTestGrandchild by andyh.near at 2023-06-01T20:46:41.083Z

--- andyh.near/RenderTestGrandchild/source_code.jsx
+++ andyh.near/RenderTestGrandchild/source_code.jsx
@@ -1,6 +1,5 @@
 State.init({ k: 0, isInitialized: false });
 const incK = (n, fn) => {
-  console.log("incrementing k by " + n + " typeof " + typeof fn);
   fn();
   State.update({ k: state.k + n });
 };
