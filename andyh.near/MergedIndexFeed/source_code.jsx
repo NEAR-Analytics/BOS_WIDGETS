bf0acb1d0c60eadbcabc444e27c0d10856a24f6f
andyh.near <andyh.near>
2023-06-01 23:05:12 +0000

    Update MergedIndexFeed by andyh.near at 2023-06-01T23:05:12.280Z

--- andyh.near/MergedIndexFeed/source_code.jsx
+++ andyh.near/MergedIndexFeed/source_code.jsx
@@ -234,7 +234,7 @@ if (reverse) {
 }
 
 const renderedItems = items.map(cachedRenderItem);
-console.log({ props, renderedItems });
+console.log({ props, state, renderedItems });
 return props.manual ? (
   <>
     {reverse && fetchMore}
