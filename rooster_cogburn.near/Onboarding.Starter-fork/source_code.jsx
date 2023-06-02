168eabdaa82eaac257ff359754bf89a4f524ef6d
rooster_cogburn.near <rooster_cogburn.near>
2023-06-02 15:54:42 +0000

    Update Onboarding.Starter-fork by rooster_cogburn.near at 2023-06-02T15:54:42.678Z

---
+++ rooster_cogburn.near/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,9 @@
+return (
+  <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
+    <Widget src="near/widget/Onboarding.ComponentCard" props={{ accountId }} />
+    <br />
+    {<Widget src="root.near/widget/Swap" props={{}} />}
+    <Widget />
+  </>
+);
