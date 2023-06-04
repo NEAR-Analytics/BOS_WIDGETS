e45680d771f4cb396d7bfa43281ca5f82317de66
efiz.near <efiz.near>
2023-06-04 18:48:08 +0000

    Update create by efiz.near at 2023-06-04T18:48:08.116Z

--- efiz.near/create/source_code.jsx
+++ efiz.near/create/source_code.jsx
@@ -8,7 +8,7 @@ function composeData() {
       core: {
         data: {
           name: "",
-          navigation: [
+          view: [
             {
               name: "kanban",
               src: "every.near/thing/kanban",
@@ -19,50 +19,11 @@ function composeData() {
             },
           ],
         },
-        type: "efiz.near/type/core",
+        type: "every.near/type/core",
       },
     },
   };
   return data;
-}
-
-function Thing() {
-  // Renders the path according to type (VM doesn't offer switch-case w/ default)
-  if (type === "widget") {
-    // TODO: Verify that props are passed
-    // How to allow user to lock at a specific height?
-    return <Widget src={path} props={props} />;
-  }
-  if (type === "settings") {
-    // Standardize path to {accountId}/settings/**
-    parts.splice(2);
-    parts.push("**");
-    path = parts.join("/");
-    return (
-      <Widget
-        src="efiz.near/widget/Every.Setting"
-        props={{ path, blockHeight }}
-      />
-    );
-  }
-  if (type === "thing") {
-    // get the thing data
-    const thing = Social.get(path, blockHeight);
-    thing = JSON.parse(thing || "null");
-    type = thing.type || null;
-    // get the type data
-    const type = JSON.parse(Social.get(type, blockHeight) || "null");
-    if (type === null) {
-      console.log(`edge case: thing ${path} had an invalid type: ${thingType}`);
-    }
-    // get the widget to render this thing
-    const widgetSrc = type?.widgets?.view;
-    return (
-      <Widget src={widgetSrc} props={{ data: thing.data, path, blockHeight }} />
-    );
-  }
-  // DEFAULT:
-  return <p>The type: {type} is not yet supported.</p>;
 }
 
 return (
