f7146fd5e7c0d8a9e5bef3fb7aea587a2b44aedc
bo.near <bo.near>
2023-06-05 12:45:22 +0000

    Update LocalStateWidget by bo.near at 2023-06-05T12:45:22.650Z

--- bo.near/LocalStateWidget/source_code.jsx
+++ bo.near/LocalStateWidget/source_code.jsx
@@ -38,23 +38,23 @@ const WrapperWidget = ({ children, id, storageType }) => {
 // This function handles the state change for the children widgets
 const handleStateChange = (key, value) => {
   // Use the unique identifier to create a unique storage key
-  const storageKey = `${key}`;
+  const storageKey = `${id}_${key}`;
 
-  // if (storageType === "local") {
-  // Update the local storage with the new state
-  Storage.set(storageKey, JSON.stringify(value));
-  // }
+  if (storageType === "local") {
+    // Update the local storage with the new state
+    Storage.set(storageKey, JSON.stringify(value));
+  }
 };
 
 // This function initializes the state of the children widgets
-const initializeState = (key, defaultValue) => {
+const initState = (key, defaultValue) => {
   // Use the unique identifier to create a unique storage key
-  const storageKey = `${key}`;
+  const storageKey = `${id}_${key}`;
 
   let storedValue;
-  // if (storageType === "local") {
-  storedValue = Storage.get(storageKey);
-  // }
+  if (storageType === "local") {
+    storedValue = Storage.get(storageKey);
+  }
 
   if (storedValue) {
     return JSON.parse(storedValue);
@@ -62,8 +62,8 @@ const initializeState = (key, defaultValue) => {
   return defaultValue;
 };
 
-State.init({ name: "" });
-State.update({ name: initializeState("name", "") });
+State.init({ name: initState("name", "") });
+
 return (
   <div className="col-lg-12  mb-2">
     Name:
@@ -71,7 +71,6 @@ return (
       type="text"
       value={state.name}
       onChange={(event) => {
-        console.log("aaaaaa");
         State.update({ name: event.target.value });
         handleStateChange("name", event.target.value);
       }}
