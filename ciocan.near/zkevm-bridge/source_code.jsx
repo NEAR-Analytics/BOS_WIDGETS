79f05b0425d78164aa2c7c3504a532c50453b120
ciocan.near <ciocan.near>
2023-06-05 12:31:36 +0000

    Update zkevm-bridge by ciocan.near at 2023-06-05T12:31:36.979Z

--- ciocan.near/zkevm-bridge/source_code.jsx
+++ ciocan.near/zkevm-bridge/source_code.jsx
@@ -14,35 +14,38 @@ const tokens = [
     chainId: 5,
     symbol: "ETH",
     decimals: 18,
-    logoURI: "",
+    logoURI: "https://assets.coingecko.com/coins/images/279/small/ethereum.png",
   },
   {
     address: "0x4701Aa9471d7bfAc765D87dcb1Ea6BB23AD32733",
     chainId: 5,
     symbol: "MATIC",
     decimals: 18,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/4713/small/matic-token-icon.png",
   },
   {
     address: "0xd35cceead182dcee0f148ebac9447da2c4d449c4",
     chainId: 5,
     symbol: "USDC",
     decimals: 6,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png",
   },
   {
     address: "0xD7E55eB808693D5Ff81a3391c59886C7E0449f35",
     chainId: 5,
     symbol: "DAI",
     decimals: 18,
-    logoURI: "",
+    logoURI: "https://assets.coingecko.com/coins/images/9956/small/4943.png",
   },
   {
     address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
     chainId: 5,
     symbol: "UNI",
     decimals: 18,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/12504/small/uniswap-uni.png",
   },
   // eth mainnet assets
   {
@@ -50,42 +53,45 @@ const tokens = [
     chainId: 1,
     symbol: "ETH",
     decimals: 18,
-    logoURI: "",
+    logoURI: "https://assets.coingecko.com/coins/images/279/small/ethereum.png",
   },
   {
     address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
     chainId: 1,
     symbol: "MATIC",
     decimals: 18,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/4713/small/matic-token-icon.png",
   },
   {
     address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     chainId: 1,
     symbol: "USDC",
     decimals: 6,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png",
   },
   {
     address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     symbol: "DAI",
     decimals: 18,
     chainId: 1,
-    logoURI: "",
+    logoURI: "https://assets.coingecko.com/coins/images/9956/small/4943.png",
   },
   {
     address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
     chainId: 1,
     symbol: "USDT",
     decimals: 6,
-    logoURI: "",
+    logoURI: "https://assets.coingecko.com/coins/images/325/small/Tether.png",
   },
   {
     address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
     chainId: 1,
     symbol: "WBTC",
     decimals: 8,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/7598/small/wrapped_bitcoin_wbtc.png",
   },
   // zkevm testnet assets
   {
@@ -93,21 +99,23 @@ const tokens = [
     chainId: 1442,
     symbol: "ETH",
     decimals: 18,
-    logoURI: "",
+    logoURI: "https://assets.coingecko.com/coins/images/279/small/ethereum.png",
   },
   {
     address: "0x8Ba0a934ef4C24e475C78072cCa3Ed306c1aBaDD",
     chainId: 1442,
     symbol: "USDC",
     decimals: 6,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png",
   },
   {
     address: "0x378588D64A464d61c646e5e86F4DA5277e65802C",
     chainId: 1442,
     symbol: "UNI",
     decimals: 18,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/12504/small/uniswap-uni.png",
   },
   // zkevm assets
   {
@@ -115,21 +123,23 @@ const tokens = [
     chainId: 1101,
     symbol: "ETH",
     decimals: 18,
-    logoURI: "",
+    logoURI: "https://assets.coingecko.com/coins/images/279/small/ethereum.png",
   },
   {
     address: "0xa2036f0538221a77A3937F1379699f44945018d0",
     chainId: 1101,
     symbol: "MATIC",
     decimals: 18,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/4713/small/matic-token-icon.png",
   },
   {
     address: "0xA8CE8aee21bC2A48a5EF670afCc9274C7bbbC035",
     chainId: 1101,
     symbol: "USDC",
     decimals: 18,
-    logoURI: "",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png",
   },
 ];
 
