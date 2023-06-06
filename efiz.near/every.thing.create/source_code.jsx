85e4dca279da399086c66fe593a3c53911dfe11a
efiz.near <efiz.near>
2023-06-06 23:45:08 +0000

    Update every.thing.create by efiz.near at 2023-06-06T23:45:08.446Z

--- efiz.near/every.thing.create/source_code.jsx
+++ efiz.near/every.thing.create/source_code.jsx
@@ -147,6 +147,27 @@ const Text = styled.p`
   margin-right: 10px;
 `;
 
+//   const Input = styled.input`
+//   width: 300px;
+//   height: 30px;
+// `;
+
+//   const Select = styled.select`
+//   height: 30px;
+//   width: 300px;
+// `;
+
+//   const Button = styled.button`
+//   text-transform: lowercase !important;
+//   padding: 8px;
+// `;
+
+//   const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 4px;
+// `;
+
 const handleTypeChange = (e) => {
   State.update({ selectedType: e.target.value });
 };
@@ -163,11 +184,69 @@ const handleThingData = (value, extra) => {
 };
 
 function RenderTypeCreate() {
+  const properties = type.properties || [];
+
+  const handleInputChange = (name, value) => {
+    State.update({ [name]: value });
+    if (props.onChange) {
+      props.onChange({ [name]: value });
+    }
+  };
+
+  function Property({ item }) {
+    if (item.type === "string") {
+      return (
+        <Input
+          key={item.name}
+          onChange={(e) => handleInputChange(item.name, e.target.value)}
+          value={state[item.name] || ""}
+          placeholder={item.name}
+        />
+      );
+    } else if (item.type === "boolean") {
+      return (
+        <Select
+          key={item.name}
+          onChange={(e) => handleInputChange(item.name, e.target.value)}
+          value={state[item.name] || ""}
+        >
+          <option value="true">true</option>
+          <option value="false">false</option>
+        </Select>
+      );
+    } else if (item.type === "number") {
+      return (
+        <Input
+          key={item.name}
+          type="number"
+          onChange={(e) =>
+            handleInputChange(item.name, parseInt(e.target.value, 10))
+          }
+          value={state[item.name] || ""}
+          placeholder={item.name}
+        />
+      );
+    } else {
+      const itemType = JSON.parse(Social.get(item.type, "final") || "null");
+      const widgetSrc = itemType?.widgets?.create;
+      // it would be great to modify the onChange function
+      return <Widget src={widgetSrc} onChange={onChange} />;
+    }
+  }
+
+  return (
+    <Container>
+      {properties.map((item) => (
+        <Property item={item} />
+      ))}
+    </Container>
+  );
+
   if (state.selectedType !== "") {
     return (
       <Widget
         src={type?.widgets?.create}
-        props={{ onChange: handleThingData }} // onChange
+        props={{ onChange: handleThingData, type }} // onChange
       />
     );
   }
