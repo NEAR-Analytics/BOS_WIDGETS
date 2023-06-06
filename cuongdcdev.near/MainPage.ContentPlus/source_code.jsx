609ee8e77e9a5070c8b623042ac1e18e660ef4c7
cuongdcdev.near <cuongdcdev.near>
2023-06-06 09:38:45 +0000

    Update MainPage.ContentPlus by cuongdcdev.near at 2023-06-06T09:38:45.730Z

--- cuongdcdev.near/MainPage.ContentPlus/source_code.jsx
+++ cuongdcdev.near/MainPage.ContentPlus/source_code.jsx
@@ -50,6 +50,7 @@ if (state.feedIndex === 0) {
   const graph = Social.keys(`${context.accountId}/graph/follow/*`, "final");
   if (graph !== null) {
     accounts = Object.keys(graph[context.accountId].graph.follow || {});
+    // console.log("Following Accounts: ", accounts, "Socials: ", Social);
     // console.log(
     //   "Following Accounts: ",
     //   accounts,
@@ -81,80 +82,28 @@ let WidgetFeed = (props) => {
       accountId: props.accounts,
     },
   };
-
-  const repostSvg = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      fill="currentColor"
-      viewBox="0 0 24 24"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M4.854 1.146a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L4 2.707V12.5A2.5 2.5 0 0 0 6.5 15h8a.5.5 0 0 0 0-1h-8A1.5 1.5 0 0 1 5 12.5V2.707l3.146 3.147a.5.5 0 1 0 .708-.708l-4-4z"
-        transform="rotate(180, 12, 12), translate(0, 4)"
-      />
-      <path
-        fill-rule="evenodd"
-        d="M4.854 1.146a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L4 2.707V12.5A2.5 2.5 0 0 0 6.5 15h8a.5.5 0 0 0 0-1h-8A1.5 1.5 0 0 1 5 12.5V2.707l3.146 3.147a.5.5 0 1 0 .708-.708l-4-4z"
-        transform="translate(0, 4)"
-      />
-    </svg>
-  );
-
-  const extractParentPost = (item) => {
-    if (!item || item.type !== "social" || !item.path || !item.blockHeight) {
-      return undefined;
+  //TODO: hide post from blocked list here
+  let renderItem = (a) => {
+    if (isInBlockedList(a.accountId)) {
+      console.log(a.accountId + " is BLOCKED");
+      return;
     }
-    const accountId = item.path.split("/")[0];
-    return `${accountId}/post/main` === item.path
-      ? { accountId, blockHeight: item.blockHeight }
-      : undefined;
-  };
-
-  const renderRepost = (a) => {
-    if (a.value.type !== "repost") {
-      return false;
-    }
-    const post = extractParentPost(a.value.item);
-    if (!post) {
-      return false;
-    }
-    const item = JSON.stringify(makePostItem(post));
-    if (item in renderedPosts) {
-      return false;
-    }
-    renderedPosts[item] = true;
 
     return (
-      <div key={JSON.stringify(a)} className="mb-3">
-        <div className="text-muted">
-          {repostSvg} Reposted by{" "}
+      a.value.type === "md" && (
+        <div key={JSON.stringify(a)} className="mb-3">
           <Widget
-            src="mob.near/widget/ProfileLine"
+            src="cuongdcdev.near/widget/MainPage.PostPlus"
             props={{
               accountId: a.accountId,
-              hideImage: true,
-              hideAccountId: true,
-              tooltip: true,
+              isPostBlocked: isInBlockedList(a.accountId),
+              blockHeight: a.blockHeight,
+              blockedListArr: blockedListArr,
             }}
           />
         </div>
-        <Widget
-          src="cuongdcdev.near/widget/MainPage.PostPlus"
-          props={{ accountId: post.accountId, blockHeight: post.blockHeight }}
-        />
-      </div>
+      )
     );
-  };
-
-  const renderItem = (item) => {
-    if (isInBlockedList(item.accountId)) {
-      console.log(item.accountId + " is BLOCKED");
-      return;
-    }
-    item.action === "post" ? renderPost(item) : renderRepost(item);
   };
 
   return (
