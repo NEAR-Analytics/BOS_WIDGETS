56c286b6d5eb4fb14a3351b919f1ff2d9b3192fb
contribut3.near <contribut3.near>
2023-06-07 12:31:59 +0000

    Update Admin.Applications by contribut3.near at 2023-06-07T12:31:59.573Z

--- contribut3.near/Admin.Applications/source_code.jsx
+++ contribut3.near/Admin.Applications/source_code.jsx
@@ -1,1 +1,32 @@
-return <>Credit system will be implemented soon...</>;
+const ownerId = "contribut3.near";
+const search = props.search ?? "";
+
+State.init({
+  items: [],
+  itemsIsFetched: false,
+});
+
+asyncFetch(
+  `https://api-staging-fur7.onrender.com/data/credits/applications?q=${search}`
+).then(({ body: items }) => State.update({ items, itemsIsFetched: true }));
+
+if (!state.itemsIsFetched) {
+  return <>Loading...</>;
+}
+
+return (
+  <Widget
+    src={`${ownerId}/widget/List`}
+    props={{
+      filter: (accountId) => state.items.includes(accountId),
+      items: state.items,
+      full: true,
+      createItem: (accountId) => (
+        <Widget
+          src={`${ownerId}/widget/Application.AdminCard`}
+          props={{ accountId }}
+        />
+      ),
+    }}
+  />
+);
