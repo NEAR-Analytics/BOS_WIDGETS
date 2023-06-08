b249308a5c237474213212d8f8eed97388c4bbb7
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-08 23:21:40 +0000

    Update general_neardigitalcollective_master-handler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-08T23:21:40.479Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/general_neardigitalcollective_master-handler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/general_neardigitalcollective_master-handler/source_code.jsx
@@ -51,6 +51,106 @@ const tabs = props.tabs ?? {
   },
 };
 
+const formatCard = props.formatCard ?? {
+  body: {
+    row1: {
+      rowType: "markdown",
+      contentData: "data",
+    },
+    row2: {
+      rowType: "text",
+      contentData: "data2",
+    },
+    row3: {
+      rowType: "flex",
+      flexClassName: "justify-content-between border rounded p-3",
+      contentData: [
+        {
+          type: "flex",
+          flexClassName:
+            "flex-column justify-content-start align-items-center border-right",
+          style: { width: "33%" },
+          content: [
+            {
+              type: "text",
+              value: "Created by",
+            },
+            {
+              type: "key",
+              value: ["accountId"],
+              style: {
+                maxWidth: "100%",
+                textOverflow: "ellipsis",
+                overflow: "hidden",
+                textWrap: "nowrap",
+              },
+            },
+          ],
+        },
+        {
+          type: "flex",
+          flexClassName:
+            "flex-column justify-content-start align-items-center border-right",
+          style: { width: "33%" },
+          content: [
+            {
+              type: "text",
+              value: "Started",
+            },
+            {
+              type: "timeStampKey",
+              value: "startTimestamp",
+            },
+          ],
+        },
+        {
+          type: "flex",
+          flexClassName:
+            "flex-column justify-content-start align-items-center border-right",
+          style: {
+            width: "33%",
+          },
+          content: [
+            {
+              type: "text",
+              value: "Ended",
+            },
+            {
+              type: "timeStampKey",
+              value: "endTimestamp",
+            },
+          ],
+        },
+      ],
+    },
+  },
+  footer: {},
+};
+
+const footerFormat = props.footerFormat ?? {
+  comment: {
+    status: true,
+    key: "kudo",
+    path: "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/Kudos",
+    pushDataModel:
+      '{"kudo":"{\\"key\\":\\"commentAnswers\\",\\"value\\":{\\"commentAnswer\\":\\"\\",\\"blockHeight\\":0}}"}',
+  },
+  repost: {
+    status: true,
+  },
+  upVoteButton: {
+    status: true,
+    pushDataModel:
+      '{"kudo":"{\\"key\\":\\"upvote\\",\\"value\\":{\\"blockHeight\\":0}}"}',
+  },
+  shareWidget: {
+    status: true,
+    popUpDescription: "Use this link to share the kudo",
+    shareingWidget: "Kudos.Styles",
+    propName: "sharedBlockHeight",
+  },
+};
+
 const cardHeaderWidgetName =
   props.cardHeaderWidgetName ?? "minimalistQuestionHeader";
 const cardContentWidgetName =
