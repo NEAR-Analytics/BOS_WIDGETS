ac5219d878aea389c9c97c7132b73a01843fcb19
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2023-06-10 12:38:11 +0000

    Update gigs-board.components.layout.SearchByLabel by devgovgigs.petersalomonsen.near at 2023-06-10T12:38:11.914Z

---
+++ devgovgigs.petersalomonsen.near/gigs-board.components.layout.SearchByLabel/source_code.jsx
@@ -0,0 +1,80 @@
+/* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+
+  const linkPropsQuery = Object.entries(linkProps)
+    .filter(([_key, nullable]) => (nullable ?? null) !== null)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
+
+const onSearchLabel = props.onSearchLabel;
+const selectedLabels = props.labelQuery?.label
+  ? [{ name: props.labelQuery.label }]
+  : [];
+
+const labels = Near.view(nearDevGovGigsContractAccountId, "get_all_labels");
+if (!labels) {
+  return <div>Loading ...</div>;
+}
+const wrappedLabels = labels.map((label) => ({ name: label }));
+
+const onChange = (selectedLabels) => {
+  onSearchLabel(selectedLabels[0]?.name);
+};
+
+return (
+  <Typeahead
+    clearButton
+    id="basic-typeahead-single"
+    labelKey="name"
+    onChange={onChange}
+    options={wrappedLabels}
+    placeholder="Search by tag"
+    selected={selectedLabels}
+  />
+);
