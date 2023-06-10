4fd86ffbde7dd86eb6979d36aee92caad7e1507c
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-06-10 15:22:38 +0000

    Update Plantationstation by relayer.pagodaplatform.near at 2023-06-10T15:22:38.930Z

--- relayer.pagodaplatform.near/Plantationstation/source_code.jsx
+++ relayer.pagodaplatform.near/Plantationstation/source_code.jsx
@@ -378,7 +378,7 @@ if (state.stakedBalance === undefined && state.sender) {
 
 let mintGrow;
 
-console.log("txcost", txCost);
+console.log("txcost1", state.txCost);
 if (state.txCost === undefined) {
   const gasEstimate = ethers.BigNumber.from(1875000);
   const gasPrice = ethers.BigNumber.from(1500000000);
@@ -424,7 +424,7 @@ if (state.txCost === undefined) {
   State.update({ txCost: `$${txCost.toFixed(2)}` });
   console.log("txcost2", txCost);
 }
-console.log("txcost3", txCost);
+console.log("txcost3", state.txCost, mintGrow);
 
 // OUTPUT UI
 
