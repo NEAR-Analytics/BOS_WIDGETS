8dbd2dedda53961e74f97c234ea470876eacf856
root.akaia.near <root.akaia.near>
2023-06-12 01:41:17 +0000

    Update gigs-board.feature.community-editor.branding-section by root.akaia.near at 2023-06-12T01:41:17.827Z

---
+++ root.akaia.near/gigs-board.feature.community-editor.branding-section/source_code.jsx
@@ -0,0 +1,87 @@
+/* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+
+  const linkPropsQuery = Object.entries(linkProps)
+    .filter(([_key, nullable]) => (nullable ?? null) !== null)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
+
+const CardCover = styled.div`
+  border-top-left-radius: var(--bs-border-radius-xl) !important;
+  border-top-right-radius: var(--bs-border-radius-xl) !important;
+  height: calc(100% - 84px);
+`;
+
+const banner_url =
+  "https://ipfs.near.social/ipfs/bafkreiaowjqxds24fwcliyriintjd4ucciprii2rdxjmxgi7f5dmzuscey";
+
+const CommunityEditorBrandingSection = ({ data }) => {
+  return (
+    <div
+      className="card shadow rounded-4 w-100"
+      style={{ maxWidth: 896, height: 320 }}
+    >
+      <CardCover
+        alt="Community banner preview"
+        className="card-img-top"
+        style={{
+          background: `center / cover no-repeat url(${banner_url})`,
+        }}
+      />
+
+      <div className="card-body p-4">
+        <h5 className="h5">{data.name}</h5>
+
+        <p className="card-text">{data.description}</p>
+      </div>
+    </div>
+  );
+};
+
+return CommunityEditorBrandingSection(props);
