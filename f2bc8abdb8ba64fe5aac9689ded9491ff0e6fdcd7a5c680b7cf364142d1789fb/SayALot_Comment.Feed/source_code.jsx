aa3f51e5f6d78fc6d8662bfd580b4c34a70e6dea
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-13 21:35:40 +0000

    Update SayALot_Comment.Feed by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-13T21:35:40.356Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_Comment.Feed/source_code.jsx
@@ -0,0 +1,48 @@
+const addressForComments = "sayalot-comments";
+const addressForArticles = "sayALotArticle";
+const authorForWidget =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const index = {
+  action: addressForComments,
+  key: props.item,
+  options: {
+    limit: props.limit ?? 3,
+    order: "desc",
+    accountId: props.accounts,
+    subscribe: props.subscribe,
+  },
+};
+
+const raw = !!props.raw;
+
+const renderItem = (a) =>
+  a.value.type === "md" && (
+    <div key={JSON.stringify(a)}>
+      <Widget
+        src={`${authorForWidget}/widget/SayALot_Comment`}
+        props={{
+          accountId: a.accountId,
+          blockHeight: a.blockHeight,
+          highlight:
+            a.accountId === props.highlightComment?.accountId &&
+            a.blockHeight === props.highlightComment?.blockHeight,
+          raw,
+        }}
+      />
+    </div>
+  );
+
+return (
+  <div>
+    <Widget
+      src={`${authorForWidget}/widget/SayALot_ManualIndexFeed`}
+      props={{
+        index,
+        reverse: true,
+        renderItem,
+        nextLimit: 10,
+        loadMoreText: "Show earlier comments...",
+      }}
+    />
+  </div>
+);
