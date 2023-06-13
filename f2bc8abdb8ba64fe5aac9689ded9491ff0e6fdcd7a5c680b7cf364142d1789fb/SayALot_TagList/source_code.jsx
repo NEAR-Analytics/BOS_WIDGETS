7fcc0e38ada8876683c883284fe9c8f25b0fe50f
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-13 21:24:27 +0000

    Update SayALot_TagList by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-13T21:24:27.126Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_TagList/source_code.jsx
@@ -0,0 +1,18 @@
+const authorForWidget =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const tagItemCss =
+  "me-1 text-primary bg-primary bg-opacity-10 position-relative fw-normal badge border border-primary text-decoration-none";
+return (
+  <>
+    {props.tags &&
+      props.tags.map((tag) => (
+        <a
+          key={tag}
+          href={`/#/${authorForWidget}/widget/SayALot_ArticlesByTag?tag=${tag}`}
+          className={tagItemCss}
+        >
+          #{tag}
+        </a>
+      ))}
+  </>
+);
