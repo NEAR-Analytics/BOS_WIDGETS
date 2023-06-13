1fb3b66a4de7366b09fda828899afc09b6c1f290
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-13 21:10:53 +0000

    Update SayALot by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-13T21:10:53.507Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot/source_code.jsx
@@ -0,0 +1,43 @@
+const addressForArticles = "ndcGigsArticle";
+const authorsWhitelist = [
+  "neardigitalcollective.near",
+  "blaze.near",
+  "jlw.near",
+  "kazanderdad.near",
+  "joep.near",
+  "sarahkornfeld.near",
+  "yuensid.near",
+  "shubham007.near",
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+];
+const authorForWidget = "sayalot.near";
+const accountId = props.accountId ?? context.accountId;
+// if (!accountId) {
+//   return "No account ID";
+// }
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+
+return (
+  <div
+    className="container-fluid"
+    style={{
+      backgroundColor: "rgb(230, 230, 230)",
+      borderRadius: "20px",
+      padding: "0",
+    }}
+  >
+    <Widget
+      src={`${authorForWidget}/widget/SayALot_MainNavigation`}
+      props={{ currentNavPill: "articles", writersWhiteList }}
+    />
+    <div style={{ margin: "0 auto", width: "90%", minWidth: "360px" }}>
+      <Widget
+        src={`${authorForWidget}/widget/SayALot_AllArticlesList`}
+        props={{ writersWhiteList }}
+      />
+    </div>
+  </div>
+);
