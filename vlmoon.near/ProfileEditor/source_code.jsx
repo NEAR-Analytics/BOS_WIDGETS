143027daed7097eda9a00e11f245fcd3435b75e0
vlmoon.near <vlmoon.near>
2023-06-13 16:17:51 +0000

    Update ProfileEditor by vlmoon.near at 2023-06-13T16:17:51.751Z

--- vlmoon.near/ProfileEditor/source_code.jsx
+++ vlmoon.near/ProfileEditor/source_code.jsx
@@ -14,85 +14,76 @@ State.init({
   profile,
 });
 
-const Wrapper = styled.div`
-  #pills-tab,
-  #pills-tabContent {
-    display: none;
-  }
-`;
-
 return (
-  <Wrapper>
-    <div className="row">
-      <div className="col-lg-6">
-        <div>
-          <h4>Edit profile of @{accountId}</h4>
-        </div>
-        <div className="mb-2">
-          <Widget
-            src="vlmoon.near/widget/MetadataEditor"
-            props={{
-              initialMetadata: state.profile,
-              onChange: (profile) => State.update({ profile }),
-              options: {
-                name: { label: "Name" },
-                image: { label: "Profile picture" },
-                backgroundImage: { label: "Background image" },
-                description: { label: "About" },
-                tags: {
-                  label: "Tags",
-                  tagsPattern: "*/profile/tags/*",
-                  placeholder:
-                    "rust, engineer, artist, humanguild, nft, learner, founder",
-                },
-                linktree: {
-                  links: [
-                    {
-                      label: "Twitter",
-                      prefix: "https://twitter.com/",
-                      name: "twitter",
-                    },
-                    {
-                      label: "Github",
-                      prefix: "https://github.com/",
-                      name: "github",
-                    },
-                    {
-                      label: "Telegram",
-                      prefix: "https://t.me/",
-                      name: "telegram",
-                    },
-                    {
-                      label: "Website",
-                      prefix: "https://",
-                      name: "website",
-                    },
-                  ],
-                },
-              },
-            }}
-          />
-        </div>
-        <div className="mb-2">
-          <CommitButton data={{ profile: state.profile }}>
-            Save profile
-          </CommitButton>
-          <a
-            className="btn btn-outline-primary ms-2"
-            href={`#/vlmoon.near/widget/ProfilePage?accountId=${accountId}`}
-          >
-            View profile
-          </a>
-        </div>
+  <div className="row">
+    <div className="col-lg-6">
+      <div>
+        <h4>Edit profile of @{accountId}</h4>
       </div>
-      <div className="col-lg-6">
-        <div>
-          <Widget
-            src="vlmoon.near/widget/ProfilePage"
-            props={{ accountId, profile: state.profile }}
-          />
-        </div>
+      <div className="mb-2">
+        <Widget
+          src="vlmoon.near/widget/MetadataEditor"
+          props={{
+            initialMetadata: profile,
+            onChange: (profile) => State.update({ profile }),
+            options: {
+              name: { label: "Name" },
+              image: { label: "Profile picture" },
+              backgroundImage: { label: "Background image" },
+              description: { label: "About" },
+              tags: {
+                label: "Tags",
+                tagsPattern: "*/profile/tags/*",
+                placeholder:
+                  "rust, engineer, artist, humanguild, nft, learner, founder",
+              },
+              linktree: {
+                links: [
+                  {
+                    label: "Twitter",
+                    prefix: "https://twitter.com/",
+                    name: "twitter",
+                  },
+                  {
+                    label: "Github",
+                    prefix: "https://github.com/",
+                    name: "github",
+                  },
+                  {
+                    label: "Telegram",
+                    prefix: "https://t.me/",
+                    name: "telegram",
+                  },
+                  {
+                    label: "Website",
+                    prefix: "https://",
+                    name: "website",
+                  },
+                ],
+              },
+            },
+          }}
+        />
+      </div>
+      <div className="mb-2">
+        <CommitButton data={{ profile: state.profile }}>
+          Save profile
+        </CommitButton>
+        <a
+          className="btn btn-outline-primary ms-2"
+          href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
+        >
+          View profile
+        </a>
       </div>
     </div>
-  </Wrapper>
+    <div className="col-lg-6">
+      <div>
+        <Widget
+          src="mob.near/widget/ProfilePage"
+          props={{ accountId, profile: state.profile }}
+        />
+      </div>
+    </div>
+  </div>
 );
