9536e5887b12a9df36e5e2c2be35a60481426bcc
mintbase.near <mintbase.near>
2023-06-14 13:27:11 +0000

    Update nft-marketplace by mintbase.near at 2023-06-14T13:27:11.352Z

--- mintbase.near/nft-marketplace/source_code.jsx
+++ mintbase.near/nft-marketplace/source_code.jsx
@@ -1,5 +1,15 @@
 const accountId = props.accountId || context.accountId;
+const contracts = props.contracts ||
+  context.contracts || [
+    "core.namesky.near",
+    "asac.near",
+    "mrbrownproject.near",
+    "spin-nft-contract.near",
+    "citizen.bodega-lab.near",
+    "ff.nekotoken.near",
+  ];
 const marketId = "simple.market.mintbase1.near";
+
 const AFFILIATE_ACCOUNT = props.affiliateAccount || "mintbase.near";
 
 const data = fetch("https://graph.mintbase.xyz", {
@@ -11,8 +21,8 @@ const data = fetch("https://graph.mintbase.xyz", {
   },
   body: JSON.stringify({
     query: `
-      query MyQuery {
-        mb_views_active_listings(limit: 100, order_by: {created_at: desc}, where: {market_id: {_eq: "simple.market.mintbase1.near"}}) {
+      query MyQuery($contracts: [String]) {
+        mb_views_active_listings_by_contract(limit: 100, order_by: {created_at: desc}, where: {market_id: {_eq: "simple.market.mintbase1.near"}, nft_contract_id: {_in: $contracts}}) {
             listed_by
             created_at
             price
@@ -22,6 +32,9 @@ const data = fetch("https://graph.mintbase.xyz", {
         }   
       }
 `,
+    variables: {
+      contracts: contracts,
+    },
   }),
 });
 
@@ -49,62 +62,78 @@ if (!data.ok) {
   return "Loading";
 }
 
-const size = "10em";
+const size = "20em";
 
 return data !== null ? (
   <>
-    <h1>Current Mintbase Listings</h1>
-    <p>
-      Buying from this widget will redirect 1.25% of the sale if you change
-      ?affiliateAccount=mynearaccount.near at the end of the URL.
-    </p>
+    <div>
+      <h4>NFT Marketplace List on Mintbase</h4>
+      <p>
+        The Mintbase Marketplace component is a customizable widget for
+        NFT-based dApps that allows developers to easily integrate a fully
+        functional NFT marketplace.
+      </p>
+    </div>
     <div className="d-flex gap-4 flex-wrap">
-      {data.body.data?.mb_views_active_listings.map((listing, i) => {
-        const priceYocto = listing.price.toLocaleString().replace(/,/g, "");
-        const priceNear = YoctoToNear(priceYocto);
+      {data.body.data?.mb_views_active_listings_by_contract.map(
+        (listing, i) => {
+          const priceYocto = listing.price.toLocaleString().replace(/,/g, "");
+          const priceNear = YoctoToNear(priceYocto);
 
-        return (
-          <div className="d-flex flex-column gap-1">
-            <a
-              href={`https://mintbase.xyz/meta/${listing.metadata_id}/`}
-              target="_blank"
-            >
-              <Widget
-                src="mob.near/widget/NftImage"
-                props={{
-                  nft: {
-                    tokenId: listing.token_id,
-                    contractId: listing.nft_contract_id,
-                  },
-                  style: {
-                    width: size,
-                    height: size,
-                    objectFit: "cover",
-                    minWidth: size,
-                    minHeight: size,
-                    maxWidth: size,
-                    maxHeight: size,
-                    overflowWrap: "break-word",
-                  },
-                  thumbnail: "thumbnail",
-                  className: "",
-                  fallbackUrl:
-                    "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
+          return (
+            <div className="d-flex flex-column gap-1 w-15 p-3">
+              <a
+                href={`https://mintbase.xyz/meta/${listing.metadata_id}/`}
+                target="_blank"
+              >
+                <Widget
+                  src="mob.near/widget/NftImage"
+                  props={{
+                    nft: {
+                      tokenId: listing.token_id,
+                      contractId: listing.nft_contract_id,
+                    },
+                    style: {
+                      width: size,
+                      height: size,
+                      objectFit: "cover",
+                      minWidth: size,
+                      minHeight: size,
+                      maxWidth: size,
+                      maxHeight: size,
+                      overflowWrap: "break-word",
+                    },
+                    thumbnail: "thumbnail",
+                    className: "",
+                    fallbackUrl:
+                      "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
+                  }}
+                />
+              </a>
+              <button
+                disabled={!accountId}
+                onClick={() => {
+                  if (!accountId) return;
+                  buy(priceYocto, listing.token_id, listing.nft_contract_id);
                 }}
-              />
-            </a>
-            <button
-              disabled={!accountId}
-              onClick={() => {
-                if (!accountId) return;
-                buy(priceYocto, listing.token_id, listing.nft_contract_id);
-              }}
-            >
-              Buy for {priceNear} N
-            </button>
-          </div>
-        );
-      })}
+                style={{
+                  border: "1px solid black",
+                  backgroundColor: "white",
+                  color: "black",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Buy {priceNear} N
+              </button>
+            </div>
+          );
+        }
+      )}
+    </div>
+
+    <div class="py-4">
+      <Widget src="mintbase.near/widget/BuiltWithMintbase" />
     </div>
   </>
 ) : (
