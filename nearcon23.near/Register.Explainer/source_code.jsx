e57f29ba38d4d6904e172491f026b090605e1002
nearcon23.near <nearcon23.near>
2023-06-14 08:41:43 +0000

    Update Register.Explainer by nearcon23.near at 2023-06-14T08:41:43.968Z

--- nearcon23.near/Register.Explainer/source_code.jsx
+++ nearcon23.near/Register.Explainer/source_code.jsx
@@ -5,7 +5,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
-  gap: 2rem;
+  gap: 3em;
 
   .red {
     color: #ff7966;
@@ -18,21 +18,6 @@ const Container = styled.div`
     font-size: 2em;
     line-height: 1.5em;
     color: #000000;
-  }
-
-  ul {
-    color: var(--black, #000);
-    font-size: 1rem;
-    font-family: Mona Sans;
-    font-weight: 600;
-    line-height: 170%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-    gap: 0.5em;
-    list-style-position: outside;
-    padding-left: 1.25em;
   }
 
   ol {
@@ -52,6 +37,8 @@ const Container = styled.div`
   }
 
   & > div {
+    display: flex;
+    flex-direction: column;
     align-items: flex-start;
     padding: 2em;
     gap: 1em;
@@ -66,97 +53,42 @@ const Container = styled.div`
       line-height: 150%;
       color: #a1a09a;
     }
+
+    & > div {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      gap: 2em;
+      width: 100%;
+    }
   }
 `;
 
 const mapImage = (src) => `https://ipfs.near.social/ipfs/${src}`;
 
-const ImageFlex = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 2fr;
-  padding-top:20px;
-  a {
-  display:block;
-   margin:10px;
-  }
-  @media only screen and (max-width: 1200px) {
-  a {
-    margin:10px;
-  }
-}
-
-`;
-
 return (
   <Container>
-    {/*<h2>What you get</h2>*/}
-    <h2>For $99, you get...</h2>
-    <ul>
-      <li>3 days of access to both Nearcon HQ and Hacker HQ venues</li>
-      <li>
-        200+ content sessions spanning Technical updates, AI, Gaming, Loyalty,
-        Regulation and much more
-      </li>
-      <li>Entry to Nearcon opening and closing parties</li>
-      <li>Exclusive swag, merchandise, and airdrops</li>
-      <li>Networking with founders, builders, and VCs</li>
-      <li>Some other surprises in store!</li>
-    </ul>
     <h2>How it works</h2>
     <ol>
-      <li>Complete the form</li>
+      <li>
+        Complete the form <span className="red">(All fields required)</span>
+      </li>
       <li>
         You will be redirected to complete payment (via crypto or fiat currency)
       </li>
-      <li>
-        You will receive a receipt of purchase and your ticket will be sent to
-        you later leading up to the event (because we’re working on some special
-        onboarding experiences)
-      </li>
+      <li>You will receive your NFT ticket via your wallet</li>
     </ol>
-    <div style={{ margin: "auto" }}>
-      <span>Ticketing solutions by</span>
-      <ImageFlex>
-        <div>
-          <a
-            href="https://veriken.com/"
-            target="_blank"
-            referrerPolicy="unsafe-url"
-          >
-            <Widget src={`${ownerId}/widget/Register.VerikenLogo`} />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://keypom.xyz/"
-            target="_blank"
-            referrerPolicy="unsafe-url"
-          >
-            <Widget src={`${ownerId}/widget/Register.KeypomLogo`} />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://www.mintbase.xyz/"
-            target="_blank"
-            referrerPolicy="unsafe-url"
-          >
-            <Widget src={`${ownerId}/widget/Register.MintbaseLogo`} />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://www.mintbase.xyz/"
-            target="_blank"
-            referrerPolicy="unsafe-url"
-          >
-            <img
-              style={{ width: "144px" }}
-              src="https://ipfs.near.social/ipfs/bafkreiacvdyffnb245npkf6oym6zgknrrxdid3zwoygksa66uozep4obya"
-            />
-          </a>
-        </div>
-      </ImageFlex>
+    <div>
+      <span>Ticketing fulfillment by</span>
+      <div>
+        <a href="#">
+          <Widget src={`${ownerId}/widget/Register.KeypomLogo`} />
+        </a>
+        <a href="#">
+          <Widget src={`${ownerId}/widget/Register.MintbaseLogo`} />
+        </a>
+      </div>
     </div>
   </Container>
 );
