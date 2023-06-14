a7240f3051631580e928b5cd9127bb6bf5fb5dc4
root.akaia.near <root.akaia.near>
2023-06-14 00:11:31 +0000

    Update gigs-board.feature.community-editor.branding-section by root.akaia.near at 2023-06-14T00:11:31.573Z

--- root.akaia.near/gigs-board.feature.community-editor.branding-section/source_code.jsx
+++ root.akaia.near/gigs-board.feature.community-editor.branding-section/source_code.jsx
@@ -55,33 +55,64 @@ function href(widgetName, linkProps) {
 const CardCover = styled.div`
   border-top-left-radius: var(--bs-border-radius-xl) !important;
   border-top-right-radius: var(--bs-border-radius-xl) !important;
-  height: calc(100% - 84px);
+  height: calc(100% - 100px);
 `;
 
-const banner_url =
-  "https://ipfs.near.social/ipfs/bafkreiaowjqxds24fwcliyriintjd4ucciprii2rdxjmxgi7f5dmzuscey";
+const CommunityEditorBrandingSection = ({
+  data: { description, name, ...data },
+  isEditingAllowed,
+  onSubmit,
+}) => {
+  return widget("components.organism.form", {
+    data: {
+      banner_url:
+        data.banner_url ??
+        "https://ipfs.near.social/ipfs/bafkreiaowjqxds24fwcliyriintjd4ucciprii2rdxjmxgi7f5dmzuscey",
 
-const CommunityEditorBrandingSection = ({ data }) => {
-  return (
-    <div
-      className="card shadow rounded-4 w-100"
-      style={{ maxWidth: 896, height: 320 }}
-    >
-      <CardCover
-        alt="Community banner preview"
-        className="card-img-top"
-        style={{
-          background: `center / cover no-repeat url(${banner_url})`,
-        }}
-      />
+      logo_url:
+        data.logo_url ??
+        "https://ipfs.near.social/ipfs/bafkreiaowjqxds24fwcliyriintjd4ucciprii2rdxjmxgi7f5dmzuscey",
+    },
 
-      <div className="card-body p-4">
-        <h5 className="h5">{data.name}</h5>
+    isMutable: isEditingAllowed,
+    noEditorFrame: true,
+    onSubmit,
 
-        <p className="card-text">{data.description}</p>
+    fieldsRender: ({
+      formState: { banner_url, logo_url },
+      formUpdate,
+      isEditable,
+      onFormSubmit,
+    }) => (
+      <div
+        className="card shadow rounded-4 w-100"
+        style={{ maxWidth: 896, height: 280 }}
+      >
+        <CardCover
+          alt="Community banner preview"
+          className="card-img-top"
+          style={{
+            background: `center / cover no-repeat url(${banner_url})`,
+          }}
+        />
+
+        <img
+          alt="Community logo preview"
+          class="img-fluid rounded-circle ms-5 border border-4 border-white"
+          src={logo_url}
+          style={{ marginTop: -64, width: 128, height: 128 }}
+        />
+
+        <div
+          className="card-body p-4"
+          style={{ marginTop: -64, marginLeft: 180, height: 84 }}
+        >
+          <h5 className="h5">{name}</h5>
+          <p className="card-text">{description}</p>
+        </div>
       </div>
-    </div>
-  );
+    ),
+  });
 };
 
 return CommunityEditorBrandingSection(props);
