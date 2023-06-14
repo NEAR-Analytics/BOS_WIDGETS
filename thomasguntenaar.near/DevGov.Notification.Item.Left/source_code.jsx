be4df854616ec706b464814695e2b6fa0dbf2c00
thomasguntenaar.near <thomasguntenaar.near>
2023-06-14 15:56:42 +0000

    Update DevGov.Notification.Item.Left by thomasguntenaar.near at 2023-06-14T15:56:42.448Z

--- thomasguntenaar.near/DevGov.Notification.Item.Left/source_code.jsx
+++ thomasguntenaar.near/DevGov.Notification.Item.Left/source_code.jsx
@@ -1,4 +1,56 @@
-const { href } = VM.require("thomasguntenaar.near/widget/core.lib.url") || (() => {});
+/* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+
+  const linkPropsQuery = Object.entries(linkProps)
+    .filter(([_key, nullable]) => (nullable ?? null) !== null)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
 
 if (!props.type) {
   return "Loading ...";
@@ -8,24 +60,15 @@ const type = props.type.split("/")[1];
 return props.type ? (
   <>
     {type == "like"
-      ? "liked your"
+      ? "liked"
       : type == "reply"
-      ? "replied to your"
+      ? "replied"
       : type == "edit"
-      ? "edited your"
-      : type == "mention"
-      ? "mentioned you in their"
+      ? "edited"
       : "???"}{" "}
-    <a
-      className="fw-bold text-muted"
-      href={href({
-        widgetSrc: "thomasguntenaar.near/widget/dh.post",
-        params: {
-          id: props.post,
-        },
-      })}
-    >
-      DevHub post
+    your
+    <a className="fw-bold text-muted" href={href("Post", { id: props.post })}>
+      Developer Governance post
     </a>
   </>
 ) : (
