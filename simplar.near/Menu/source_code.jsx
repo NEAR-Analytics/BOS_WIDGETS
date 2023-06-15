6303ad738480946451779e941c283cc89ffc5201
simplar.near <simplar.near>
2023-06-15 14:46:40 +0000

    Update Menu by simplar.near at 2023-06-15T14:46:40.965Z

--- simplar.near/Menu/source_code.jsx
+++ simplar.near/Menu/source_code.jsx
@@ -1,36 +1,23 @@
-if (!props.onSelect) { return "Please define onSelect" }
-
-function onSelect(src, prs) { props.onSelect(src, prs) };
-
-const homePage = props.homePage;
-const topics = props.topics; // ["overview", "updates", "reports"]
+const topics = props.topics;
+const menuLinks = props.menuLinks;
+const selected = props.selected;
 
 const capitalize = (words) => {
   return words.split(" ").map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(" ");
 }
-
+console.log(menuLinks)
 return (
   <>
     <div class="">
       <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
-          <a href="#" class="nav-link" aria-current="page" onClick={() => onSelect(homePage, {})}>
-            <i class="bi bi-house me-1" />
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link link-dark">
-            <i class="bi bi-mortarboard me-1" />
-            Learn
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link link-dark">
-            <i class="bi bi-calendar-date me-1" />
-            Events
-          </a>
-        </li>
+        {menuLinks.map(({ label, icon }) =>
+          <li class="nav-item">
+            <a href={`/?selected=${label}`} class={`nav-link ${selected !== label && "link-dark"}`}>
+              <i class={`bi bi-${icon} me-1`} />
+              {capitalize(label)}
+            </a>
+          </li>
+        )}
         <li>
           <a
             href="#"
@@ -40,15 +27,14 @@ return (
             aria-expanded="true"
           >
             <i class="bi bi-chat-right-text me-1" />
-            Discuss
+            Channels
           </a>
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 ps-4 small collapse show"
-            id="collapse" >
+          <ul class="list-unstyled fw-normal pb-1 ps-4 small show" >
 
             {
               topics.map(topic =>
                 <li>
-                  <a href="#" class="nav-link link-dark" onClick={() => onSelect("near/widget/NestedDiscussions", { identifier: topic })}>
+                  <a href={`/?selected=discussion&identifier=${topic}`} class={`nav-link ${selected !== topic && "link-dark"}`}>
                     {capitalize(topic)}
                   </a>
                 </li>
