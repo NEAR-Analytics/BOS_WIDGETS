488a059d142bf594f8629f9815853b6f7984a56e
nycdao.near <nycdao.near>
2023-06-15 15:22:26 +0000

    Update demo by nycdao.near at 2023-06-15T15:22:26.284Z

---
+++ nycdao.near/demo/source_code.jsx
@@ -0,0 +1,31 @@
+if (context.loading) {
+  return "";
+}
+
+const H1 = styled.h1`
+  color: #11181C;
+  font-size: 32px;
+  font-weight: 600;
+`;
+const H6 = styled.h6`
+  color: #687076;
+  font-size: 20px;
+  font-weight: 400
+`;
+
+return (
+  <div class="container-fluid py-3 mb-5">
+    <div class="row">
+      <div class="col-12">
+        <H1>Demo Day: Community Vote</H1>
+        <H6>Upvote any of the projects you like!</H6>
+        <div class="mt-5">
+          <Widget
+            src="nycdao.near/widget/projects"
+            props={{ admins, adminContract: adminContract }}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
