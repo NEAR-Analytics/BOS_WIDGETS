9263d0c9b68983364c942a4310c0cfd9cdf4d463
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-15 16:40:09 +0000

    Update Test2 by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-15T16:40:09.801Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/test2/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/test2/source_code.jsx
@@ -1,3 +1,14 @@
-props.fx({ a: "omgomg" });
+State.init({ articleId: "" });
 
-return <div style={{ color: red }}>omg</div>;
+return (
+  <input
+    className="form-control mt-2"
+    id="inputArticleId"
+    value={state.articleId}
+    onChange={(e) => {
+      State.update({
+        articleId: e.target.value.replace(/\s+/g, ""),
+      });
+    }}
+  />
+);
