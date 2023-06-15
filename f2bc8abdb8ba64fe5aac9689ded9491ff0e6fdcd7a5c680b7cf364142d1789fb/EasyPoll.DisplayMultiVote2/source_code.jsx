acfa1268dde16b771f19e85b14e10f2cdffc91da
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-15 19:59:25 +0000

    Update EasyPoll.DisplayMultiVote2 by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-15T19:59:25.653Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
@@ -66,21 +66,25 @@ return (
 
     {props.poll.value.questions.length > 1 && (
       <div className="d-flex justify-content-between">
-        {state.currentQuestion > 0 && (
+        {state.currentQuestion > 0 ? (
           <i
             onClick={() => {
               State.update({ currentQuestion: state.currentQuestion + 1 });
             }}
             className="bi bi-arrow-left"
           />
+        ) : (
+          <div style={{ minWidth: "1px" }}></div>
         )}
-        {state.currentQuestion < props.poll.value.questions.length && (
+        {state.currentQuestion < props.poll.value.questions.length ? (
           <i
             onClick={() => {
               State.update({ currentQuestion: state.currentQuestion - 1 });
             }}
             className="bi bi-arrow-right"
           />
+        ) : (
+          <div style={{ minWidth: "1px" }}></div>
         )}
       </div>
     )}
