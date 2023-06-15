f876bde894cc77835ae6af17a9fe05d85dcc8233
jgdev.near <jgdev.near>
2023-06-15 18:02:12 +0000

    Update gigs-board.components.layout.Controls by jgdev.near at 2023-06-15T18:02:12.746Z

--- jgdev.near/gigs-board.components.layout.Controls/source_code.jsx
+++ jgdev.near/gigs-board.components.layout.Controls/source_code.jsx
@@ -4,6 +4,7 @@ const nearDevGovGigsContractAccountId =
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
 const nearDevGovGigsWidgetsAccountId =
   props.nearDevGovGigsWidgetsAccountId ||
+  // (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
   (context.widgetSrc ?? "jgdev.near").split("/", 1)[0];
 
 function widget(widgetName, widgetProps, key) {
@@ -52,10 +53,7 @@ return (
         backgroundColor: "#0C7283",
         color: "#f3f3f3",
       }}
-      // href={href("Create", { labels: props.labels })}
-      href={href(
-        "https://near.social/#/devgovgigs.near/widget/gigs-board.pages.Create?labels={props.labels}"
-      )}
+      href={href("Create", { labels: props.labels })}
     >
       <i class="bi bi-plus-circle-fill"></i>
       Submit Idea / Solution
