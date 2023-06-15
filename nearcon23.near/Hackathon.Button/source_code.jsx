c141c0a344612f39727fa820f107da536e86d92d
nearcon23.near <nearcon23.near>
2023-06-15 10:05:46 +0000

    Update Hackathon.Button by nearcon23.near at 2023-06-15T10:05:46.787Z

---
+++ nearcon23.near/Hackathon.Button/source_code.jsx
@@ -0,0 +1,35 @@
+const ownerId = "nearcon23.near";
+
+const Link = styled.a`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+  width: 10.625em;
+  gap: 0.5em;
+  border: none;
+  border-radius: 50px;
+  background: #00ec97;
+  color: #11181c;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  text-align: center;
+  white-space: nowrap;
+  transition: all 0.3s ease-in-out;
+
+  &:hover,
+  &:focus,
+  &:active {
+    text-decoration: none;
+    background: #11181c;
+    color: #fff;
+  }
+`;
+
+return (
+  <Link href={props.href ?? `/${ownerId}/widget/Index?tab=hackathon`}>
+    Apply to hack
+  </Link>
+);
