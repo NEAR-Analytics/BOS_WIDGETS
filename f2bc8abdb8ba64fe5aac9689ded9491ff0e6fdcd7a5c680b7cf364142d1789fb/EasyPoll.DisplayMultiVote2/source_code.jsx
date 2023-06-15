9af1d2d1346dc6b0088c465a15d635241180ff73
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-15 22:31:03 +0000

    Update EasyPoll.DisplayMultiVote2 by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-15T22:31:03.734Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
@@ -1,3 +1,5 @@
+State.init({ currentQuestion: 0, vote: props.state.vote });
+
 const getFirstSBTToken = () => {
   const view = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
     account: `${context.accountId}`,
@@ -8,55 +10,118 @@ const getFirstSBTToken = () => {
 
 const hasSBTTokens = true || getFirstSBTToken() !== undefined;
 
+const renderTextInput = (questionNumber) => {
+  return (
+    <div>
+      {props.hasVoted ? (
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(2,1fr)" }}>
+          {props.renderAnswers(questionNumber)}
+        </div>
+      ) : (
+        <div>
+          <textarea
+            value={props.state.vote[questionNumber]}
+            onChange={(e) => {
+              let newVote = props.state.vote;
+              newVote[questionNumber] = e.target.value;
+
+              State.update({ vote: newVote });
+            }}
+            style={{ width: "100%" }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
 return (
   <>
     {props.poll.value.questions.map((question, questionNumber) => {
-      return (
-        <div
-          style={{
-            border: "1.5px solid rgb(206, 212, 218)",
-            borderRadius: "24px",
-            position: "relative",
-          }}
-          className="p-3 my-3"
-        >
-          <div className="d-flex">
-            <p
-              style={{
-                backgroundColor: "#353A40",
-                padding: "0.15rem 0.65rem",
-                borderRadius: "9px",
-                color: "white",
-              }}
-            >
-              {questionNumber + 1}
-            </p>
-            <h4 style={{ fontWeight: "700", marginLeft: "0.8rem" }}>
-              {question.question}
-            </h4>
-          </div>
+      {
+        questionNumber == state.currentQuestion && (
+          <div
+            style={{
+              border: "1.5px solid rgb(206, 212, 218)",
+              borderRadius: "24px",
+              position: "relative",
+            }}
+            className="p-3 my-3"
+          >
+            <div className="d-flex">
+              <p
+                style={{
+                  backgroundColor: "#353A40",
+                  padding: "0.15rem 0.65rem",
+                  borderRadius: "9px",
+                  color: "white",
+                }}
+              >
+                {questionNumber + 1}
+              </p>
+              <h4 style={{ fontWeight: "700", marginLeft: "0.8rem" }}>
+                {question.question}
+              </h4>
+            </div>
 
-          {!props.hasVoted &&
-          (question.questionType == "0" || question.questionType == "1") ? (
-            <p className="mb-1">Select one option:</p>
-          ) : !props.hasVoted && question.questionType == "2" ? (
-            <p className="mb-1">You can check multiple options:</p>
-          ) : (
-            !props.hasVoted && <p className="mb-1">Write your answer:</p>
-          )}
-          {question.questionType != "3"
-            ? question.choicesOptions.map((option, optionNumber) => {
-                return props.renderMultipleChoiceInput({
-                  questionNumber: questionNumber,
-                  questionType: question.questionType,
-                  option,
-                  optionNumber,
-                });
-              })
-            : props.renderTextInput(questionNumber)}
-        </div>
-      );
+            {!props.hasVoted &&
+            (question.questionType == "0" || question.questionType == "1") ? (
+              <p className="mb-1">Select one option:</p>
+            ) : !props.hasVoted && question.questionType == "2" ? (
+              <p className="mb-1">You can check multiple options:</p>
+            ) : (
+              !props.hasVoted && <p className="mb-1">Write your answer:</p>
+            )}
+            {question.questionType != "3"
+              ? question.choicesOptions.map((option, optionNumber) => {
+                  return props.renderMultipleChoiceInput({
+                    questionNumber: questionNumber,
+                    questionType: question.questionType,
+                    option,
+                    optionNumber,
+                  });
+                })
+              : renderTextInput(questionNumber)}
+          </div>
+        );
+      }
     })}
+
+    {props.poll.value.questions.length > 1 && (
+      <div className="d-flex justify-content-between">
+        {state.currentQuestion > 0 ? (
+          <div
+            className="d-flex aling-items-center"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              stateUpdate({ vote: state.vote });
+              State.update({ currentQuestion: state.currentQuestion - 1 });
+            }}
+          >
+            <i className="bi bi-arrow-left" />
+            <h6 className="mx-2">Previous question</h6>
+          </div>
+        ) : (
+          <div style={{ minWidth: "1px" }}></div>
+        )}
+        {state.currentQuestion < props.poll.value.questions.length ? (
+          <div
+            className="d-flex aling-items-center"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              stateUpdate({ vote: state.vote });
+              State.update({ currentQuestion: state.currentQuestion + 1 });
+            }}
+          >
+            <h6 className="mx-2">Next question</h6>
+            <i className="bi bi-arrow-right" />
+          </div>
+        ) : (
+          <div style={{ minWidth: "1px" }}></div>
+        )}
+      </div>
+    )}
+
     {props.isQuestionOpen ? (
       props.hasVoted ? (
         ""
