8b12f0ec4da37b5d5243820b338021d3dc15c616
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-15 20:11:49 +0000

    Update EasyPoll.DisplayMultiVote2 by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-15T20:11:49.472Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
@@ -75,7 +75,7 @@ return (
             }}
           >
             <i className="bi bi-arrow-left" />
-            <p className="m-2">Previous question</p>
+            <p className="mx-2">Previous question</p>
           </div>
         ) : (
           <div style={{ minWidth: "1px" }}></div>
@@ -88,7 +88,7 @@ return (
               State.update({ currentQuestion: state.currentQuestion + 1 });
             }}
           >
-            <p className="m-2">Next question</p>
+            <p className="mx-2">Next question</p>
             <i className="bi bi-arrow-right" />
           </div>
         ) : (
