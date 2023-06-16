2e0c91b5195ad2f62a755b23432880380736fb9e
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-16 01:36:08 +0000

    Update EasyPoll.DisplayMultiVote2 by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-16T01:36:08.591Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
@@ -148,81 +148,64 @@ const renderTextInput = (questionNumber) => {
 
 return (
   <>
-    {state.showVoteButton ? (
-      <h4
-        className="border border-top-0 rounded"
-        style={{
-          textDecoration: "underline",
-          cursor: "pointer",
-          backgroundColor: "rgb(242, 246, 250)",
-        }}
-        onClick={() =>
-          State.update({ showVoteButton: false, currentQuestion: 0 })
-        }
-      >
-        Change the votes
-      </h4>
-    ) : (
-      questions.map((question, questionNumber) => {
-        {
-          if (questionNumber == state.currentQuestion) {
-            return (
-              <div
-                style={{
-                  border: "1.5px solid rgb(206, 212, 218)",
-                  borderRadius: "24px",
-                  position: "relative",
-                }}
-                className="p-3 my-3"
-              >
-                <div className="d-flex">
-                  <p
-                    style={{
-                      backgroundColor: "#353A40",
-                      padding: "0.15rem 0.65rem",
-                      borderRadius: "9px",
-                      color: "white",
-                    }}
-                  >
-                    {questionNumber + 1}
-                  </p>
-                  <h4 style={{ fontWeight: "700", marginLeft: "0.8rem" }}>
-                    {question.question}
-                  </h4>
-                </div>
-
-                {!props.hasVoted &&
-                (question.questionType == "0" ||
-                  question.questionType == "1") ? (
-                  <p className="mb-1">Select one option:</p>
-                ) : !props.hasVoted && question.questionType == "2" ? (
-                  <p className="mb-1">You can check multiple options:</p>
-                ) : (
-                  !props.hasVoted && <p className="mb-1">Write your answer:</p>
-                )}
-                {question.questionType != "3"
-                  ? question.choicesOptions.map((option, optionNumber) => {
-                      return renderMultipleChoiceInput({
-                        questionNumber: questionNumber,
-                        questionType: question.questionType,
-                        option,
-                        optionNumber,
-                      });
-                    })
-                  : renderTextInput(questionNumber)}
+    {questions.map((question, questionNumber) => {
+      {
+        if (questionNumber == state.currentQuestion) {
+          return (
+            <div
+              style={{
+                border: "1.5px solid rgb(206, 212, 218)",
+                borderRadius: "24px",
+                position: "relative",
+              }}
+              className="p-3 my-3"
+              onMouseLeave={() => props.stateUpdate({ vote: state.vote })}
+            >
+              <div className="d-flex">
+                <p
+                  style={{
+                    backgroundColor: "#353A40",
+                    padding: "0.15rem 0.65rem",
+                    borderRadius: "9px",
+                    color: "white",
+                  }}
+                >
+                  {questionNumber + 1}
+                </p>
+                <h4 style={{ fontWeight: "700", marginLeft: "0.8rem" }}>
+                  {question.question}
+                </h4>
               </div>
-            );
-          } else <></>;
-        }
-      })
-    )}
 
-    {questions.length > 1 && !state.showVoteButton && (
+              {!props.hasVoted &&
+              (question.questionType == "0" || question.questionType == "1") ? (
+                <p className="mb-1">Select one option:</p>
+              ) : !props.hasVoted && question.questionType == "2" ? (
+                <p className="mb-1">You can check multiple options:</p>
+              ) : (
+                !props.hasVoted && <p className="mb-1">Write your answer:</p>
+              )}
+              {question.questionType != "3"
+                ? question.choicesOptions.map((option, optionNumber) => {
+                    return renderMultipleChoiceInput({
+                      questionNumber: questionNumber,
+                      questionType: question.questionType,
+                      option,
+                      optionNumber,
+                    });
+                  })
+                : renderTextInput(questionNumber)}
+            </div>
+          );
+        } else <></>;
+      }
+    })}
+
+    {questions.length > 1 && (
       <div className="d-flex justify-content-between">
         {state.currentQuestion > 0 ? (
           <ChangeQuestionContainer
             onClick={() => {
-              props.stateUpdate({ vote: state.vote });
               State.update({ currentQuestion: state.currentQuestion - 1 });
             }}
           >
@@ -237,7 +220,6 @@ return (
         {state.currentQuestion < questions.length - 1 ? (
           <ChangeQuestionContainer
             onClick={() => {
-              props.stateUpdate({ vote: state.vote });
               State.update({ currentQuestion: state.currentQuestion + 1 });
             }}
           >
@@ -247,112 +229,95 @@ return (
             </div>
           </ChangeQuestionContainer>
         ) : (
-          <ChangeQuestionContainer
-            onClick={() => {
-              props.stateUpdate({ vote: state.vote });
-              State.update({ showVoteButton: true });
-            }}
-          >
-            <div>
-              <h6 className="mx-2">Show vote button</h6>
-              <i className="bi bi-arrow-right" />
-            </div>
-          </ChangeQuestionContainer>
+          <div style={{ minWidth: "1px" }}></div>
         )}
       </div>
     )}
 
-    {state.showVoteButton ? (
-      props.isQuestionOpen ? (
-        props.hasVoted ? (
-          ""
-        ) : props.isVoteValid() && hasSBTTokens ? (
-          <CommitButton
-            className="w-100"
-            style={
-              state.hoveringElement != "voteButton"
-                ? {
-                    marginTop: "0.5rem",
-                    padding: "0.5rem",
-                    backgroundColor: "#000000",
-                    color: "#FFFFFF",
-                    fontSize: "1rem",
-                    borderRadius: "9px",
-                    border: "1.5px solid transparent",
-                  }
-                : {
-                    marginTop: "0.5rem",
-                    padding: "0.5rem",
-                    backgroundColor: "#FFFFFF",
-                    color: "#000000",
-                    fontSize: "1rem",
-                    borderRadius: "9px",
-                    border: "1.5px solid #000000",
-                  }
-            }
-            onMouseEnter={() => {
-              State.update({
-                hoveringElement: "voteButton",
-              });
-              props.stateUpdate({ vote: state.vote });
-            }}
-            onMouseLeave={() => State.update({ hoveringElement: "" })}
-            data={getPublicationParams()}
-          >
-            Vote
-          </CommitButton>
-        ) : (
-          <>
-            {hasSBTTokens ? (
-              <button
-                className="w-100"
-                style={
-                  state.hoveringElement != "voteButton"
-                    ? {
-                        marginTop: "0.5rem",
-                        padding: "0.5rem",
-                        backgroundColor: "#000000",
-                        color: "#FFFFFF",
-                        fontSize: "1rem",
-                        borderRadius: "9px",
-                        border: "1.5px solid transparent",
-                      }
-                    : {
-                        marginTop: "0.5rem",
-                        padding: "0.5rem",
-                        backgroundColor: "#FFFFFF",
-                        color: "#000000",
-                        fontSize: "1rem",
-                        borderRadius: "9px",
-                        border: "1.5px solid #000000",
-                      }
-                }
-                onMouseEnter={() =>
-                  State.update({ hoveringElement: "voteButton" })
-                }
-                onMouseLeave={() => State.update({ hoveringElement: "" })}
-                onClick={() => State.update({ showErrorsInForm: true })}
-              >
-                Vote
-              </button>
-            ) : (
-              <>
-                <p className="p-2">
-                  In order to vote get verified on{" "}
-                  <a href="https://i-am-human.app">i-am-human.app</a> and get a
-                  FV SBT
-                </p>
-              </>
-            )}
-            {state.showErrorsInForm && (
-              <span className="text-danger">
-                Please answer all the questions
-              </span>
-            )}
-          </>
-        )
-      ) : (
+    {props.isQuestionOpen ? (
+      props.hasVoted ? (
         ""
+      ) : props.isVoteValid() && hasSBTTokens ? (
+        <CommitButton
+          className="w-100"
+          style={
+            state.hoveringElement != "voteButton"
+              ? {
+                  marginTop: "0.5rem",
+                  padding: "0.5rem",
+                  backgroundColor: "#000000",
+                  color: "#FFFFFF",
+                  fontSize: "1rem",
+                  borderRadius: "9px",
+                  border: "1.5px solid transparent",
+                }
+              : {
+                  marginTop: "0.5rem",
+                  padding: "0.5rem",
+                  backgroundColor: "#FFFFFF",
+                  color: "#000000",
+                  fontSize: "1rem",
+                  borderRadius: "9px",
+                  border: "1.5px solid #000000",
+                }
+          }
+          onMouseEnter={() => {
+            State.update({
+              hoveringElement: "voteButton",
+            });
+          }}
+          onMouseLeave={() => State.update({ hoveringElement: "" })}
+          data={getPublicationParams()}
+        >
+          Vote
+        </CommitButton>
+      ) : (
+        <>
+          {hasSBTTokens ? (
+            <button
+              className="w-100"
+              style={
+                state.hoveringElement != "voteButton"
+                  ? {
+                      marginTop: "0.5rem",
+                      padding: "0.5rem",
+                      backgroundColor: "#000000",
+                      color: "#FFFFFF",
+                      fontSize: "1rem",
+                      borderRadius: "9px",
+                      border: "1.5px solid transparent",
+                    }
+                  : {
+                      marginTop: "0.5rem",
+                      padding: "0.5rem",
+                      backgroundColor: "#FFFFFF",
+                      color: "#000000",
+                      fontSize: "1rem",
+                      borderRadius: "9px",
+                      border: "1.5px solid #000000",
+                    }
+              }
+              onMouseEnter={() =>
+                State.update({ hoveringElement: "voteButton" })
+              }
+              onMouseLeave={() => State.update({ hoveringElement: "" })}
+              onClick={() => State.update({ showErrorsInForm: true })}
+            >
+              Vote
+            </button>
+          ) : (
+            <>
+              <p className="p-2">
+                In order to vote get verified on{" "}
+                <a href="https://i-am-human.app">i-am-human.app</a> and get a FV
+                SBT
+              </p>
+            </>
+          )}
+          {state.showErrorsInForm && (
+            <span className="text-danger">Please answer all the questions</span>
+          )}
+        </>
       )
     ) : (
       ""
