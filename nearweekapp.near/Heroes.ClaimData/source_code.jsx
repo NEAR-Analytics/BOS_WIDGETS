9ae66b240c6d170157097df3308ce504135e9d69
nearweekapp.near <nearweekapp.near>
2023-06-16 10:06:26 +0000

    Update Heroes.ClaimData by nearweekapp.near at 2023-06-16T10:06:26.860Z

--- nearweekapp.near/Heroes.ClaimData/source_code.jsx
+++ nearweekapp.near/Heroes.ClaimData/source_code.jsx
@@ -1,3 +1,42 @@
+State.init({
+  message: "",
+  timeUnit: null,
+  deadline: null,
+});
+
+const { contract, whitelistContract, kyc_config } = props;
+
+//get_tokens
+const tokens = Near.view(contract, "get_tokens", {
+  from_index: 0,
+});
+
+const kycRequired =
+  typeof kyc_config !== "string" && "KycRequired" in kyc_config;
+
+function mapTokenMetadata(tokens) {
+  let tokenMetaData = {};
+  if (tokens.length > 0) {
+    for (const token of tokens) {
+      const metadataResponse = Near.view(token[0], "ft_metadata", {});
+      if (metadataResponse) {
+        tokenMetaData[token[0]] = {
+          symbol: metadataResponse.symbol,
+          decimals: metadataResponse.decimals,
+        };
+      }
+    }
+  }
+  return tokenMetaData;
+}
+
+const isUserWhitelisted =
+  Near.view(props.whitelistContract, "is_whitelisted", {
+    account_id: context.accountId,
+  }) ?? false;
+
+const tokenMap = mapTokenMetadata(tokens);
+
 const tokenMap = {
   "a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near": {
     symbol: "USDC.e",
@@ -11,7 +50,21 @@ const tokenMap = {
     symbol: "DAI",
     decimals: 18,
   },
+  "wrap.near": {
+    symbol: "DAI",
+    decimals: 24,
+  },
 };
+
+function convertToNanoseconds(duration, unit) {
+  const mapUnits = {
+    months: 24 * 60 * 30,
+    weeks: 24 * 60 * 7,
+    days: 24 * 60,
+    hours: 1 * 60,
+  };
+  return (Number(duration) * mapUnits[unit] * 60 * 1000 * 1000000).toString();
+}
 
 function dayToEpoch(days) {
   const now = Date.now();
@@ -22,7 +75,6 @@ function dayToEpoch(days) {
   return epochSeconds.toString();
 }
 
-const kycMinAmount = 500;
 const matchToken = (token) => tokenMap[token].symbol;
 const convertAmount = (amount, token) =>
   amount / Math.pow(10, tokenMap[token].decimals);
@@ -31,17 +83,15 @@ const amountToReadable = (amount) =>
     .toFixed(0)
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
 const isDisabled =
-  convertAmount(props.amount, props.token) > kycMinAmount ||
+  (kycRequired && !isUserWhitelisted) ||
   props.bountyClaimed ||
-  !context.accountId
+  !context.accountId ||
+  !state.message ||
+  !state.deadline
     ? " disabled"
     : "";
-
-State.init({
-  message: "",
-  deadline: 1,
-});
 
 const onChangeMessage = (message) => {
   State.update({
@@ -49,20 +99,83 @@ const onChangeMessage = (message) => {
   });
 };
 
-const onChangeDeadline = (deadline) => {
-  State.update({
-    deadline,
-  });
+const ShieldIcon = () => {
+  return (
+    <svg
+      width="36"
+      height="36"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="#198754"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"
+      />
+    </svg>
+  );
+};
+
+const TimeCommitment = () => {
+  const onChangeTimeUnit = (timeUnit) => {
+    State.update({
+      timeUnit,
+    });
+  };
+
+  const onChangeDeadline = (deadline) => {
+    if (/^\+?([1-9]\d*)$/.test(deadline)) {
+      State.update({
+        deadline,
+      });
+    } else {
+      State.update({
+        deadline: null,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <span class="">Set time commitments</span>
+      <div class="input-group mb-3">
+        <select
+          id="units"
+          class="form-select"
+          aria-label="Default select example"
+          onChange={(e) => onChangeTimeUnit(e.target.value)}
+        >
+          <option value="">-Unit-</option>
+          <option value="hours">Hours</option>
+          <option value="days">Days</option>
+          <option value="weeks">Weeks</option>
+          <option value="months">Months</option>
+        </select>
+        <input
+          class="form-control"
+          min="1"
+          type="number"
+          disabled={!state.timeUnit}
+          onChange={(e) => onChangeDeadline(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
 
 const handleClick = () => {
   Near.call([
     {
-      contractName: props.contract,
+      contractName: contract,
       methodName: "bounty_claim",
       args: {
         id: props.bountyId,
-        deadline: dayToEpoch(state.deadline),
+        token: props.token,
+        deadline: convertToNanoseconds(state.deadline, state.timeUnit),
         description: state.message,
       },
       gas: 300000000000000,
@@ -75,21 +188,29 @@ return (
   <div css="min-width: 300px; max-width: 400px;">
     <div class="card">
       <div class="card-body">
-        <span class="fw-bold">{matchToken(props.token)} </span>
-        <span class="fw-bold fs-2">
-          ${amountToReadable(convertAmount(props.amount, props.token))}
-        </span>
+        <div class="d-flex align-items-baseline">
+          <span class="fw-bold">
+            {props.token in tokenMap && matchToken(props.token)}{" "}
+          </span>
+          {props.token in tokenMap ? (
+            <span class="d-flex fw-bold fs-2 align-items-center">
+              ${amountToReadable(convertAmount(props.amount, props.token))}
+              {kycRequired && <ShieldIcon />}
+            </span>
+          ) : (
+            "0"
+          )}
+        </div>
         <h6 class="card-title">{props.bountyTitle}</h6>
         <p class="card-text"></p>
-        {convertAmount(props.amount, props.token) > kycMinAmount &&
-        !props.bountyCompleted ? (
+        {kycRequired && !isUserWhitelisted && !props.bountyCompleted ? (
           <div>
-            <span class="badge text-bg-dark p-2">
+            {/*<span class="badge text-bg-dark p-2">
               <span class="fs-5">KYC requied</span>
-            </span>{" "}
+            </span>*/}
             <hr />
             <div class="blockquote-footer figcaption">
-              KYC is required for bounties over $500, please head to
+              KYC is required, please head to
               <a href="https://heroes.build" target="_blank">
                 https://heroes.build/
               </a>{" "}
@@ -101,39 +222,20 @@ return (
         )}
         {!props.bountyCompleted ? (
           <>
-            {!isDisabled ? (
-              <>
-                <div class="input-group mb-3">
-                  <span class="input-group-text" id="message">
-                    -
-                  </span>
-                  <input
-                    type="text"
-                    class="form-control"
-                    onChange={(e) => onChangeMessage(e.target.value)}
-                    placeholder="Enter short message"
-                    aria-label="message"
-                    aria-describedby="message"
-                  />
-                </div>
-                <select
-                  class="form-select mb-2"
-                  aria-label="Default select example"
-                  onChange={(e) => onChangeDeadline(e.target.value)}
-                >
-                  <option value="1" selected>
-                    Will complete in{" "}
-                  </option>
-                  <option value="1">1 Day</option>
-                  <option value="2">2 Days</option>
-                  <option value="3">3 Days</option>
-                  <option value="7">7 Days</option>
-                </select>
-              </>
-            ) : (
-              ""
-            )}
-
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="message">
+                -
+              </span>
+              <input
+                type="text"
+                class="form-control"
+                onChange={(e) => onChangeMessage(e.target.value)}
+                placeholder="Enter short message"
+                aria-label="message"
+                aria-describedby="message"
+              />
+            </div>
+            <TimeCommitment />
             <a
               class={"btn bg-warning float-end " + isDisabled}
               onClick={handleClick}
@@ -152,11 +254,8 @@ return (
             <hr />
             <div class="blockquote-footer figcaption">
               Bounty completed and can't be claimed any more, check details on
-              <a
-                href={"https://heroes.build/bounties/bounty/" + props.bountyId}
-                target="_blank"
-              >
-                https://heroes.build/
+              <a href="https://heroes.build/bounties" target="_blank">
+                https://heroes.build/bounties
               </a>{" "}
             </div>
           </>
