98dcc856df24bbb86893f2ca92f508f03c9921b3
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-16 20:31:27 +0000

    Update EasyPoll.Voting by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-16T20:31:27.029Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.Voting/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.Voting/source_code.jsx
@@ -318,7 +318,7 @@ function showDescription(description) {
 return (
   <>
     <Widget
-      src={`f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/EasyPoll.Voting2`}
+      src={`${widgetOwner}/widget/EasyPoll.Voting2`}
       props={{
         isTest,
         state,
