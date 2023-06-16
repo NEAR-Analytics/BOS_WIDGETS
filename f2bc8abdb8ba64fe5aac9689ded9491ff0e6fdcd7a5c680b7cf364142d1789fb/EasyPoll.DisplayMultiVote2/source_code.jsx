b4e841a6871e724b81f8028d2009bd4a0aca13ff
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-16 11:55:58 +0000

    Update EasyPoll.DisplayMultiVote2 by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-16T11:55:58.933Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayMultiVote2/source_code.jsx
@@ -147,89 +147,91 @@ return (
   <>
     {questions.map((question, questionNumber) => {
       {
-        if (questionNumber == state.currentQuestion) {
-          return (
-            <div
-              style={{
-                border: "1.5px solid rgb(206, 212, 218)",
-                borderRadius: "24px",
-                position: "relative",
-              }}
-              className="p-3 my-3"
-              onMouseLeave={() => props.stateUpdate({ vote: state.vote })}
-            >
-              <div className="d-flex">
-                <p
-                  style={{
-                    backgroundColor: "#353A40",
-                    padding: "0.15rem 0.65rem",
-                    borderRadius: "9px",
-                    color: "white",
-                  }}
-                >
-                  {questionNumber + 1}
-                </p>
-                <h4 style={{ fontWeight: "700", marginLeft: "0.8rem" }}>
-                  {question.question}
-                </h4>
-              </div>
-
-              {!props.hasVoted &&
-              (question.questionType == "0" || question.questionType == "1") ? (
-                <p className="mb-1">Select one option:</p>
-              ) : !props.hasVoted && question.questionType == "2" ? (
-                <p className="mb-1">You can check multiple options:</p>
-              ) : (
-                !props.hasVoted && <p className="mb-1">Write your answer:</p>
-              )}
-              {question.questionType != "3"
-                ? question.choicesOptions.map((option, optionNumber) => {
-                    return renderMultipleChoiceInput({
-                      questionNumber: questionNumber,
-                      questionType: question.questionType,
-                      option,
-                      optionNumber,
-                    });
-                  })
-                : renderTextInput(questionNumber)}
+        // if (questionNumber == state.currentQuestion) {
+        return (
+          <div
+            style={{
+              border: "1.5px solid rgb(206, 212, 218)",
+              borderRadius: "24px",
+              position: "relative",
+            }}
+            className="p-3 my-3"
+            onMouseLeave={() => props.stateUpdate({ vote: state.vote })}
+          >
+            <div className="d-flex">
+              <p
+                style={{
+                  backgroundColor: "#353A40",
+                  padding: "0.15rem 0.65rem",
+                  borderRadius: "9px",
+                  color: "white",
+                }}
+              >
+                {questionNumber + 1}
+              </p>
+              <h4 style={{ fontWeight: "700", marginLeft: "0.8rem" }}>
+                {question.question}
+              </h4>
             </div>
-          );
-        } else <></>;
+
+            {!props.hasVoted &&
+            (question.questionType == "0" || question.questionType == "1") ? (
+              <p className="mb-1">Select one option:</p>
+            ) : !props.hasVoted && question.questionType == "2" ? (
+              <p className="mb-1">You can check multiple options:</p>
+            ) : (
+              !props.hasVoted && <p className="mb-1">Write your answer:</p>
+            )}
+            {question.questionType != "3"
+              ? question.choicesOptions.map((option, optionNumber) => {
+                  return renderMultipleChoiceInput({
+                    questionNumber: questionNumber,
+                    questionType: question.questionType,
+                    option,
+                    optionNumber,
+                  });
+                })
+              : renderTextInput(questionNumber)}
+          </div>
+        );
+        // } else <></>;
       }
     })}
 
-    {questions.length > 1 && (
-      <div className="d-flex justify-content-between">
-        {state.currentQuestion > 0 ? (
-          <ChangeQuestionContainer
-            onClick={() => {
-              State.update({ currentQuestion: state.currentQuestion - 1 });
-            }}
-          >
-            <div>
-              <i className="bi bi-arrow-left" />
-              <h6 className="mx-2">Previous question</h6>
-            </div>
-          </ChangeQuestionContainer>
-        ) : (
-          <div style={{ minWidth: "1px" }}></div>
-        )}
-        {state.currentQuestion < questions.length - 1 ? (
-          <ChangeQuestionContainer
-            onClick={() => {
-              State.update({ currentQuestion: state.currentQuestion + 1 });
-            }}
-          >
-            <div>
-              <h6 className="mx-2">Next question</h6>
-              <i className="bi bi-arrow-right" />
-            </div>
-          </ChangeQuestionContainer>
-        ) : (
-          <div style={{ minWidth: "1px" }}></div>
-        )}
-      </div>
-    )}
+    {
+      //   questions.length > 1 && (
+      //   <div className="d-flex justify-content-between">
+      //     {state.currentQuestion > 0 ? (
+      //       <ChangeQuestionContainer
+      //         onClick={() => {
+      //           State.update({ currentQuestion: state.currentQuestion - 1 });
+      //         }}
+      //       >
+      //         <div>
+      //           <i className="bi bi-arrow-left" />
+      //           <h6 className="mx-2">Previous question</h6>
+      //         </div>
+      //       </ChangeQuestionContainer>
+      //     ) : (
+      //       <div style={{ minWidth: "1px" }}></div>
+      //     )}
+      //     {state.currentQuestion < questions.length - 1 ? (
+      //       <ChangeQuestionContainer
+      //         onClick={() => {
+      //           State.update({ currentQuestion: state.currentQuestion + 1 });
+      //         }}
+      //       >
+      //         <div>
+      //           <h6 className="mx-2">Next question</h6>
+      //           <i className="bi bi-arrow-right" />
+      //         </div>
+      //       </ChangeQuestionContainer>
+      //     ) : (
+      //       <div style={{ minWidth: "1px" }}></div>
+      //     )}
+      //   </div>
+      // )
+    }
 
     {props.isQuestionOpen ? (
       props.hasVoted ? (
