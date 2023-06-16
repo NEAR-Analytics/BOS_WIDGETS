b2360ead77b7c6b8960c7ef4c443c0a7d86d9181
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-16 00:40:35 +0000

    Update EasyPoll.DisplayVote2 by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-16T00:40:35.534Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayVote2/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayVote2/source_code.jsx
@@ -1,5 +1,108 @@
 const widgetOwner = "neardigitalcollective.near";
 
+let defaultVotes = [];
+for (let i = 0; i < props.poll.value.questions.length; i++) {
+  if (props.poll.value.questions[i].questionType == "2") {
+    defaultVotes.push([""]);
+  } else {
+    defaultVotes.push("");
+  }
+}
+
+State.init({
+  vote: defaultVotes,
+});
+
+function getInputStyles(questionType, questionNumber, optionNumber) {
+  if (questionType == "2") {
+    return state.vote[questionNumber].includes(optionNumber + "")
+      ? {
+          borderColor: "black",
+          backgroundColor: "black",
+          width: "1rem",
+          marginRight: "0.7rem",
+        }
+      : {
+          width: "1rem",
+          marginRight: "0.7rem",
+        };
+  } else {
+    return optionNumber + "" == state.vote[questionNumber]
+      ? {
+          borderColor: "black",
+          backgroundColor: "black",
+          width: "1rem",
+          marginRight: "0.7rem",
+        }
+      : {
+          width: "1rem",
+          marginRight: "0.7rem",
+        };
+  }
+}
+
+function getBgColor(index, isPrimary) {
+  let allColorsOfThisType =
+    state.vote != `${index}` && isPrimary ? allBgColors : allSecondaryBgColors;
+
+  return Number.isInteger((index + 1) / allColorsOfThisType.length)
+    ? allColorsOfThisType[0]
+    : allColorsOfThisType[
+        ((index + 1) / allColorsOfThisType.length -
+          Math.trunc((index + 1) / allColorsOfThisType.length)) *
+          allColorsOfThisType.length -
+          1
+      ];
+}
+
+function isVoteValid() {
+  let isValid = state.vote.length == props.poll.value.questions.length;
+  isValid = isValid && context.accountId;
+  for (let i = 0; i < state.vote.length; i++) {
+    const vote = state.vote[i];
+    // vote should always be a string, but in one case is treated as an array. Replace array with csv
+    if (Array.isArray(vote)) {
+      isValid = isValid && vote.filter((v) => v.trim() != "").length > 0;
+    } else {
+      isValid = isValid && vote.trim() != "";
+    }
+  }
+  return isValid;
+}
+
+function clickRadioInputHandler(questionNumber, optionNumber) {
+  return () => {
+    let newVote = state.vote;
+
+    newVote[questionNumber] = optionNumber + "";
+    State.update({ vote: newVote });
+  };
+}
+
+function clickCheckboxInputHandler(questionNumber, optionNumber) {
+  return () => {
+    let newVote = state.vote;
+
+    let oldQuestionVotes = newVote[questionNumber];
+    let newQuestionVotes = [];
+
+    if (!oldQuestionVotes.includes(optionNumber + "")) {
+      newQuestionVotes = oldQuestionVotes;
+      newQuestionVotes.push(optionNumber + "");
+    } else {
+      for (let i = 0; i < oldQuestionVotes.length; i++) {
+        if (oldQuestionVotes[i] != optionNumber + "") {
+          newQuestionVotes.push(oldQuestionVotes[i]);
+        }
+      }
+    }
+
+    newVote[questionNumber] = newQuestionVotes.filter((a) => a != "");
+
+    State.update({ votes: newVote });
+  };
+}
+
 const renderMultipleChoiceInput = ({
   questionNumber,
   questionType,
@@ -10,21 +113,16 @@ const renderMultipleChoiceInput = ({
     <Widget
       src={`${widgetOwner}/widget/EasyPoll.DisplayMultiVote`}
       props={{
-        state: props.state,
+        vote: state.vote,
         questionNumber,
         questionType,
         option,
         optionNumber,
         canVote: props.canVote,
-        getBgColor: props.getBgColor,
-        getFontColor: props.getFontColor,
+        getBgColor: getBgColor,
         countVotes: props.countVotes,
-        getBorderRadious: props.getBorderRadious,
-        calculatePercentageOfOption: props.calculatePercentageOfOption,
-        getBlockTimestamp: props.getBlockTimestamp,
-        clickCheckboxInputHandler: props.clickCheckboxInputHandler,
-        clickRadioInputHandler: props.clickRadioInputHandler,
-        getInputStyles: props.getInputStyles,
+        clickRadioInputHandler: clickRadioInputHandler,
+        getInputStyles: getInputStyles,
       }}
     />
   </>
@@ -35,15 +133,16 @@ return (
     <Widget
       src={`f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/EasyPoll.DisplayMultiVote2`}
       props={{
-        state: props.state,
+        poll: props.poll,
+        indexVersion: props.indexVersion,
+        vote: state.vote,
         questions: props.poll.value.questions,
         renderMultipleChoiceInput,
-        getInputStyles: props.getInputStyles,
+        getInputStyles: getInputStyles,
         hasVoted: props.hasVoted,
-        getPublicationParams: props.getPublicationParams,
-        stateUpdate: (data) => props.stateUpdate(data),
+        stateUpdate: (data) => State.update(data),
         isQuestionOpen: props.isQuestionOpen,
-        isVoteValid: props.isVoteValid,
+        isVoteValid: isVoteValid,
         validAnswersToThisPoll: props.validAnswersToThisPoll,
       }}
     />
