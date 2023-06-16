be5c7de1514d70a648d149c7631dc10f6dad0001
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-16 00:57:46 +0000

    Update EasyPoll.DisplayVote2 by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-16T00:57:46.387Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayVote2/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/EasyPoll.DisplayVote2/source_code.jsx
@@ -41,6 +41,78 @@ function getInputStyles(questionType, questionNumber, optionNumber) {
   }
 }
 
+// let secondaryColor = "#E9EBF8";
+// let fontColorBlue = "#003E9C";
+// let fontColorRed = "#9C2B2B";
+// let fontColorYellow = "#9C7B03";
+// let fontColorPurple = "#763E9C";
+// let fontColorGreen = "#009C6D";
+// let fontColorPink = "#9C0034";
+// let fontColorSkyBlue = "#007C9C";
+// let fontColorIndigo = "#006758";
+
+// let allFontColors = [
+//   fontColorBlue,
+//   fontColorRed,
+//   fontColorYellow,
+//   fontColorPurple,
+//   fontColorGreen,
+//   fontColorPink,
+//   fontColorSkyBlue,
+//   fontColorIndigo,
+// ];
+
+// function getFontColor(index) {
+//   return Number.isInteger((index + 1) / allFontColors.length)
+//     ? allFontColors[0]
+//     : allFontColors[
+//         ((index + 1) / allFontColors.length -
+//           Math.trunc((index + 1) / allFontColors.length)) *
+//           allFontColors.length -
+//           1
+//       ];
+// }
+
+let bgBlue = "#96C0FF";
+let bgRed = "#FFB4B4";
+let bgYellow = "#FFE999";
+let bgPurple = "#E6C0FF";
+let bgGreen = "#96FFE0";
+let bgPink = "#FF96B9";
+let bgSkyBlue = "#96EAFF";
+let bgIndigo = "#96DCD2";
+
+let allBgColors = [
+  bgBlue,
+  bgRed,
+  bgYellow,
+  bgPurple,
+  bgGreen,
+  bgPink,
+  bgSkyBlue,
+  bgIndigo,
+];
+
+let secondaryBgBlue = "#E6F0FF";
+let secondaryBgRed = "#FFEDED";
+let secondaryBgYellow = "#FFFAE6";
+let secondaryBgPurple = "#F9F0FF";
+let secondaryBgGreen = "#E6FFF7";
+let secondaryBgPink = "#FFE6EE";
+let secondaryBgSkyBlue = "#E6FAFF";
+let secondaryBgIndigo = "#E6F6F4";
+
+let allSecondaryBgColors = [
+  secondaryBgBlue,
+  secondaryBgRed,
+  secondaryBgYellow,
+  secondaryBgPurple,
+  secondaryBgGreen,
+  secondaryBgPink,
+  secondaryBgSkyBlue,
+  secondaryBgIndigo,
+];
+
 function getBgColor(index, isPrimary) {
   let allColorsOfThisType =
     state.vote != `${index}` && isPrimary ? allBgColors : allSecondaryBgColors;
@@ -111,7 +183,7 @@ const renderMultipleChoiceInput = ({
 }) => (
   <>
     <Widget
-      src={`${widgetOwner}/widget/EasyPoll.DisplayMultiVote`}
+      src={`f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/EasyPoll.DisplayMultiVote`}
       props={{
         vote: state.vote,
         questionNumber,
