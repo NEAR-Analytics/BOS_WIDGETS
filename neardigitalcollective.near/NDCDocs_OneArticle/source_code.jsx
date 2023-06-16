c39084e7726c42f0b2d368fc3b78398c8045cc6c
neardigitalcollective.near <neardigitalcollective.near>
2023-06-16 18:04:15 +0000

    Update NDCDocs_OneArticle by neardigitalcollective.near at 2023-06-16T18:04:15.571Z

--- neardigitalcollective.near/NDCDocs_OneArticle/source_code.jsx
+++ neardigitalcollective.near/NDCDocs_OneArticle/source_code.jsx
@@ -200,7 +200,7 @@ return (
       <div className="col-12 col-md-3 border-end">
         <h4
           className="text-center"
-          style={{ cursor: "pointer", fontSize: "1.2rem" }}
+          style={{ cursor: "pointer", fontSize: "1rem" }}
           onClick={() => {
             State.update({
               note: state.article.body,
