d4e337a5b83da5426f6b4769579a573260b07e82
jay100.near <jay100.near>
2023-06-17 17:17:25 +0000

    Update MJG-Widget by jay100.near at 2023-06-17T17:17:25.957Z

--- jay100.near/MJG-Widget/source_code.jsx
+++ jay100.near/MJG-Widget/source_code.jsx
@@ -143,67 +143,131 @@ const PixelContainer = styled.div`
   justify-content: center;
 `;
 
-const CarouselContainer = styled.div`
-    max-width: 1000px;
+const CarouselSrc = `
+<style>
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif; overflow: hidden;}
+.mySlides {display: none;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
   position: relative;
   margin: auto;
-`;
+}
 
-const Slide = styled.div`
-    display: none;
-  .fade {
-    animation-name: fade;
-    animation-duration: 1.5s;
-  }
-  @keyframes fade {
-    from {opacity: .4}
-    to {opacity: 1}
-  }
-`;
-
-const SlideImg = styled.img`
-  width: 100%
-`;
-
-const SlideText = styled.div`
+/* Caption text */
+.text {
   color: #f2f2f2;
   font-size: 15px;
   padding: 8px 12px;
-  position: absolute;
   bottom: 8px;
-  width: 100%;
+  width: 50%;
+  height: auto;
   text-align: center;
+}
+
+.active {
+  background-color: #717171;
+}
+
+.sliderStyle{
+  display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.welcomeTips{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.slideImg{
+    width: 50%;
+}
+
+/* Fading animation */
+.fade {
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+</style>
+
+
+<body>
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <img class = "slideImg" src="https://user-images.githubusercontent.com/100770363/241330819-010617e4-b2b3-4d34-a59c-c1b58bf92d8d.png">
+  <div class="welcomeTips"> 
+    <h2 style = "color: white;">The Basics</h2>
+  </div>
+</div>
+
+<div class="mySlides fade">
+  <img class = "slideImg" src="https://user-images.githubusercontent.com/100770363/241331374-56bd3f0c-7ed3-4923-bd48-4aac7367b280.png">
+  <div class="welcomeTips"> 
+    <h2 style = "color: white;">Main Menu</h2>
+    <span style = "color: white;">From the main menu, you can see all of the main things you can do in the game. Battle, Mint Unit, Check the Leaderboard, Check Active Rooms, and the market.
+On the right hand side of your image, this is where the troops that you own will be displayed.</span>
+  </div>
+</div>
+
+<div class="mySlides fade">
+  <img class = "slideImg" src="https://user-images.githubusercontent.com/100770363/241335354-eaf25884-de02-4d7a-a8f2-3c4e59214d5f.png">
+  <div class="welcomeTips"> 
+    <h2 style = "color: white;">Battles</h2>
+    <span style = "color: white;">This is what you'll see when you select battle. You will have the option to create a new challenge, or fight against someone's challenge. These matches are best 2 out of 3</span>
+  </div>
+</div>
+
+<div class="mySlides fade">
+  <img class = "slideImg" src="https://user-images.githubusercontent.com/100770363/241335498-727bf24d-a65c-4bff-b404-8bcadc558512.png">
+  <div class="welcomeTips"> 
+    <span style = "color: white; padding: 1rem;">This is the challenge you chose to accept. As you can see the opponent already set the field for the first round, and it is your job to assemble your own side so that you can attempt to take the first round.</span>
+  </div>
+</div>
+
+<div class="mySlides fade">
+  <img class = "slideImg" src="https://user-images.githubusercontent.com/100770363/241335871-e9c8e5fe-db38-4ada-b1ff-7d895e61f44e.png">
+  <div class="welcomeTips"> 
+    <span style = "color: white; padding: 1rem;">Thanks to the cheeky placement of the warlock, You were able to take the first round. From here, you will have to wait for the opponent to respond before you can attack again.</span>
+  </div>
+</div>
+
+
+</div>
+<br>
+
+<script>
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("sliderStyle");  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].classList.add("sliderStyle");  
+  setTimeout(showSlides, 7000); // Change image every 2 seconds
+}
+</script>
+
+</body>
+
 `;
-
-const SlideShow = ({ current_slides }) => {
-  let slideIndex = 0;
-
-  const showSlides = () => {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-  };
-
-  set;
-
-  return (
-    <CarouselContainer onChange={showSlides}>
-      {current_slides.map((slide, index) => (
-        <Slide className="mySlides">
-          <SlideImg src={slide.slideImage} />
-          <SlideText>{slide.slideInfo}</SlideText>
-        </Slide>
-      ))}
-    </CarouselContainer>
-  );
-};
 
 const WidgetCard = ({ title, coverSrc, description, actionButtons }) => {
   return (
@@ -333,7 +397,7 @@ return (
         ></div>
       </div>
     </Hero>
-    <SlideShow current_slides={Test_Slides} />
+    <iframe style={{ height: "70vh", width: "100%" }} srcDoc={CarouselSrc} />
     <CardList>
       {Mainnet.map((widget, index) => (
         <WidgetCard
