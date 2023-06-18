62397402610c6429ec8d644d01e0e3524f34468b
hack.near <hack.near>
2023-06-18 19:28:10 +0000

    Update commons by hack.near at 2023-06-18T19:28:10.789Z

--- hack.near/commons/source_code.jsx
+++ hack.near/commons/source_code.jsx
@@ -1,73 +1,25 @@
-const composeData = () => {
-  const data = {
-    post: {
-      main: JSON.stringify(state.content),
-    },
-    index: {
-      post: JSON.stringify({
-        key: "main",
-        value: {
-          type: "md",
-        },
-      }),
-    },
-  };
+const accountId = context.accountId;
 
-  const notifications = state.extractTagNotifications(state.content.text, {
-    type: "social",
-    path: `${context.accountId}/post/main`,
-  });
+if (!accountId) {
+  return <Widget src="mob.near/widget/ProfileOnboarding" />;
+}
 
-  if (notifications.length) {
-    data.index.notify = JSON.stringify(
-      notifications.length > 1 ? notifications : notifications[0]
-    );
-  }
+const library = accountId
+  ? Social.get(`${accountId}/settings/dev/library`)
+  : undefined;
 
-  return data;
-};
-
-State.init({
-  onChange: ({ content }) => {
-    State.update({ content });
-  },
-});
+if (library === null) {
+  return "Loading...";
+}
 
 return (
-  <div className="container">
-    <Widget src="mob.near/widget/ProfileOnboarding" />
-    <div className="row mb-3">
-      <div className="mb-3"></div>
-      <div>
-        <h1>Evolving Guides for Builders</h1>
-        <p>
-          Let's collaborate to gather knowledge and improve common resources for
-          open web developers. Choose your own adventure to discover what is
-          possible! #NEAR
-        </p>
-        <div className="mb-3"></div>
-        <a
-          className="btn btn-primary"
-          href="https://near.social/#/hack.near/widget/Dev"
-        >
-          Widgets
-        </a>
-        <a
-          className="btn btn-primary"
-          href="https://near.social/#/hack.near/widget/Docs"
-        >
-          APIs
-        </a>
-        <a
-          className="btn btn-primary"
-          href="https://near.social/#/hack.near/widget/Data"
-        >
-          Data
-        </a>
-      </div>
-      <div className="row mt-3">
-        <Widget src="hack.near/widget/Common.Component.Library" />
-      </div>
-    </div>
-  </div>
+  <>
+    <CommitButton data={{ settings: { dev: { library: data } } }}>
+      Save
+    </CommitButton>
+    <hr />
+    <p>{library}</p>
+    <hr />
+    <Widget src="hack.near/widget/dev.library" props={{ data: library }} />
+  </>
 );
