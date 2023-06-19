36e6a53f1bc5c94a0ad55c141cd33679fa948d96
neardigitalcollective.near <neardigitalcollective.near>
2023-06-19 16:14:00 +0000

    Update EasyPoll.DisplayVote2 by neardigitalcollective.near at 2023-06-19T16:14:00.644Z

--- neardigitalcollective.near/EasyPoll.DisplayVote2/source_code.jsx
+++ neardigitalcollective.near/EasyPoll.DisplayVote2/source_code.jsx
@@ -1,75 +1,171 @@
 const widgetOwner = "neardigitalcollective.near";
-const renderTextInput = (questionNumber) => {
-  return (
-    <div>
-      {props.hasVoted ? (
-        <div style={{ display: "grid", gridTemplateColumns: "repeat(2,1fr)" }}>
-          {props.renderAnswers(questionNumber)}
-        </div>
-      ) : (
-        <div>
-          <textarea
-            value={props.state.vote[questionNumber]}
-            onChange={(e) => {
-              let newVote = props.state.vote;
-              newVote[questionNumber] = e.target.value;
+const isTest = props.isTest;
 
-              props.stateUpdate({ vote: newVote });
-            }}
-            style={{ width: "100%" }}
-          />
-        </div>
-      )}
-    </div>
-  );
-};
+let defaultVotes = [];
+for (let i = 0; i < props.poll.value.questions.length; i++) {
+  if (props.poll.value.questions[i].questionType == "2") {
+    defaultVotes.push([""]);
+  } else {
+    defaultVotes.push("");
+  }
+}
 
-const renderMultipleChoiceInput = ({
-  questionNumber,
-  questionType,
-  option,
-  optionNumber,
-}) => (
-  <>
-    <Widget
-      src={`${widgetOwner}/widget/EasyPoll.DisplayMultiVote`}
-      props={{
-        state: props.state,
-        questionNumber,
-        questionType,
-        option,
-        optionNumber,
-        canVote: props.canVote,
-        getBgColor: props.getBgColor,
-        getFontColor: props.getFontColor,
-        countVotes: props.countVotes,
-        getBorderRadious: props.getBorderRadious,
-        calculatePercentageOfOption: props.calculatePercentageOfOption,
-        getBlockTimestamp: props.getBlockTimestamp,
-        clickCheckboxInputHandler: props.clickCheckboxInputHandler,
-        clickRadioInputHandler: props.clickRadioInputHandler,
-        getInputStyles: props.getInputStyles,
-      }}
-    />
-  </>
-);
+let userVote;
+
+State.init({
+  vote: userVote ?? defaultVotes,
+});
+
+function getInputStyles(questionType, questionNumber, optionNumber) {
+  if (questionType == "2") {
+    return state.vote[questionNumber].includes(optionNumber + "")
+      ? {
+          borderColor: "black",
+          borderWidth: "thick",
+          width: "1rem",
+          marginRight: "0.7rem",
+        }
+      : {
+          width: "1rem",
+          marginRight: "0.7rem",
+        };
+  } else {
+    return optionNumber + "" == state.vote[questionNumber]
+      ? {
+          borderColor: "black",
+          borderWidth: "thick",
+          width: "1rem",
+          marginRight: "0.7rem",
+        }
+      : {
+          width: "1rem",
+          marginRight: "0.7rem",
+        };
+  }
+}
+
+// let secondaryColor = "#E9EBF8";
+// let fontColorBlue = "#003E9C";
+// let fontColorRed = "#9C2B2B";
+// let fontColorYellow = "#9C7B03";
+// let fontColorPurple = "#763E9C";
+// let fontColorGreen = "#009C6D";
+// let fontColorPink = "#9C0034";
+// let fontColorSkyBlue = "#007C9C";
+// let fontColorIndigo = "#006758";
+
+// let allFontColors = [
+//   fontColorBlue,
+//   fontColorRed,
+//   fontColorYellow,
+//   fontColorPurple,
+//   fontColorGreen,
+//   fontColorPink,
+//   fontColorSkyBlue,
+//   fontColorIndigo,
+// ];
+
+// function getFontColor(index) {
+//   return Number.isInteger((index + 1) / allFontColors.length)
+//     ? allFontColors[0]
+//     : allFontColors[
+//         ((index + 1) / allFontColors.length -
+//           Math.trunc((index + 1) / allFontColors.length)) *
+//           allFontColors.length -
+//           1
+//       ];
+// }
+
+let bgBlue = "#96C0FF";
+let bgRed = "#FFB4B4";
+let bgYellow = "#FFE999";
+let bgPurple = "#E6C0FF";
+let bgGreen = "#96FFE0";
+let bgPink = "#FF96B9";
+let bgSkyBlue = "#96EAFF";
+let bgIndigo = "#96DCD2";
+
+let allBgColors = [
+  bgBlue,
+  bgRed,
+  bgYellow,
+  bgPurple,
+  bgGreen,
+  bgPink,
+  bgSkyBlue,
+  bgIndigo,
+];
+
+let secondaryBgBlue = "#E6F0FF";
+let secondaryBgRed = "#FFEDED";
+let secondaryBgYellow = "#FFFAE6";
+let secondaryBgPurple = "#F9F0FF";
+let secondaryBgGreen = "#E6FFF7";
+let secondaryBgPink = "#FFE6EE";
+let secondaryBgSkyBlue = "#E6FAFF";
+let secondaryBgIndigo = "#E6F6F4";
+
+let allSecondaryBgColors = [
+  secondaryBgBlue,
+  secondaryBgRed,
+  secondaryBgYellow,
+  secondaryBgPurple,
+  secondaryBgGreen,
+  secondaryBgPink,
+  secondaryBgSkyBlue,
+  secondaryBgIndigo,
+];
+
+function getBgColor(index, isPrimary) {
+  let allColorsOfThisType =
+    state.vote != `${index}` && isPrimary ? allBgColors : allSecondaryBgColors;
+
+  return Number.isInteger((index + 1) / allColorsOfThisType.length)
+    ? allColorsOfThisType[0]
+    : allColorsOfThisType[
+        ((index + 1) / allColorsOfThisType.length -
+          Math.trunc((index + 1) / allColorsOfThisType.length)) *
+          allColorsOfThisType.length -
+          1
+      ];
+}
+
+function isVoteValid() {
+  let isValid = state.vote.length == props.poll.value.questions.length;
+  isValid = isValid && context.accountId;
+  for (let i = 0; i < state.vote.length; i++) {
+    const vote = state.vote[i];
+    // vote should always be a string, but in one case is treated as an array. Replace array with csv
+    if (Array.isArray(vote)) {
+      isValid = isValid && vote.filter((v) => v.trim() != "").length > 0;
+    } else {
+      isValid = isValid && vote.trim() != "";
+    }
+  }
+  return isValid;
+}
 
 return (
   <>
     <Widget
       src={`${widgetOwner}/widget/EasyPoll.DisplayMultiVote2`}
       props={{
-        state: props.state,
+        isTest,
+        widgetOwner,
+        getBgColor,
         poll: props.poll,
-        renderTextInput,
+        indexVersion: props.indexVersion,
+        vote: state.vote,
+        questions: props.poll.value.questions,
         renderMultipleChoiceInput,
-        getInputStyles: props.getInputStyles,
+        getInputStyles: getInputStyles,
+        canVote: props.canVote,
         hasVoted: props.hasVoted,
-        getPublicationParams: props.getPublicationParams,
-        stateUpdate: (data) => props.stateUpdate(data),
+        stateUpdate: (data) => State.update(data),
         isQuestionOpen: props.isQuestionOpen,
-        isVoteValid: props.isVoteValid,
+        isVoteValid: isVoteValid,
         validAnswersToThisPoll: props.validAnswersToThisPoll,
+        countVotes: props.countVotes,
       }}
     />
   </>
