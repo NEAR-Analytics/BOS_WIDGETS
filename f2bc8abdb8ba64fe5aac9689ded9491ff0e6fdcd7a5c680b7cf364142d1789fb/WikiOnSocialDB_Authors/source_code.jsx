91053347fd02c04eb9283d5e79c08dee1b25af73
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-19 17:43:20 +0000

    Update WikiOnSocialDB_Authors by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-19T17:43:20.396Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_Authors/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_Authors/source_code.jsx
@@ -1,7 +1,7 @@
 const addressForArticles = "ndcWikiArticle";
 const authorForWidget =
   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
-const writersWhiteList = props.writersWhiteList ?? [
+const writersWhiteList = [
   "neardigitalcollective.near",
   "blaze.near",
   "jlw.near",
