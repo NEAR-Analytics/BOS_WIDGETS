a8a248a00f6f80c4ffc7b09a2b8585ec080b429d
neardigitalcollective.near <neardigitalcollective.near>
2023-06-19 16:12:51 +0000

    Update EasyPoll.DisplayVote by neardigitalcollective.near at 2023-06-19T16:12:51.341Z

--- neardigitalcollective.near/EasyPoll.DisplayVote/source_code.jsx
+++ neardigitalcollective.near/EasyPoll.DisplayVote/source_code.jsx
@@ -2,12 +2,14 @@ if (!props.isPreview && !props.poll) {
   return "Property poll not set";
 }
 
-if(!props.whitelist) {
+if (!props.whitelist) {
   return "Property whitelist not set";
 }
 
+const isTest = props.isTest;
+
 const isPreview = props.isPreview ?? false;
-const indexVersion = props.indexVersion ?? "3.2.0"
+const indexVersion = props.indexVersion ?? "3.2.0";
 const whitelist = props.whitelist;
 
 let widgetOwner = "neardigitalcollective.near";
@@ -15,136 +17,9 @@ let widgetOwner = "neardigitalcollective.near";
 // Getting question
 const poll = props.poll;
 
-let defaultVotes = [];
-for (let i = 0; i < poll.value.questions.length; i++) {
-  if (poll.value.questions[i].questionType == "2") {
-    defaultVotes.push([""]);
-  } else {
-    defaultVotes.push("");
-  }
-}
-
 State.init({
-  vote: userVote ?? defaultVotes,
   answers: {},
-  showErrorsInForm: false,
-  hoveringElement: "",
 });
-
-let bgBlue = "#96C0FF";
-let bgRed = "#FFB4B4";
-let bgYellow = "#FFE999";
-let bgPurple = "#E6C0FF";
-let bgGreen = "#96FFE0";
-let bgPink = "#FF96B9";
-let bgSkyBlue = "#96EAFF";
-let bgIndigo = "#96DCD2";
-
-let allBgColors = [
-  bgBlue,
-  bgRed,
-  bgYellow,
-  bgPurple,
-  bgGreen,
-  bgPink,
-  bgSkyBlue,
-  bgIndigo,
-];
-
-let secondaryBgBlue = "#E6F0FF";
-let secondaryBgRed = "#FFEDED";
-let secondaryBgYellow = "#FFFAE6";
-let secondaryBgPurple = "#F9F0FF";
-let secondaryBgGreen = "#E6FFF7";
-let secondaryBgPink = "#FFE6EE";
-let secondaryBgSkyBlue = "#E6FAFF";
-let secondaryBgIndigo = "#E6F6F4";
-
-let allSecondaryBgColors = [
-  secondaryBgBlue,
-  secondaryBgRed,
-  secondaryBgYellow,
-  secondaryBgPurple,
-  secondaryBgGreen,
-  secondaryBgPink,
-  secondaryBgSkyBlue,
-  secondaryBgIndigo,
-];
-
-let fontColorBlue = "#003E9C";
-let fontColorRed = "#9C2B2B";
-let fontColorYellow = "#9C7B03";
-let fontColorPurple = "#763E9C";
-let fontColorGreen = "#009C6D";
-let fontColorPink = "#9C0034";
-let fontColorSkyBlue = "#007C9C";
-let fontColorIndigo = "#006758";
-
-let allFontColors = [
-  fontColorBlue,
-  fontColorRed,
-  fontColorYellow,
-  fontColorPurple,
-  fontColorGreen,
-  fontColorPink,
-  fontColorSkyBlue,
-  fontColorIndigo,
-];
-
-let secondaryColor = "#E9EBF8";
-
-function getBgColor(index, isPrimary) {
-  let allColorsOfThisType =
-    state.vote != `${index}` && isPrimary ? allBgColors : allSecondaryBgColors;
-
-  return Number.isInteger((index + 1) / allColorsOfThisType.length)
-    ? allColorsOfThisType[0]
-    : allColorsOfThisType[
-        ((index + 1) / allColorsOfThisType.length -
-          Math.trunc((index + 1) / allColorsOfThisType.length)) *
-          allColorsOfThisType.length -
-          1
-      ];
-}
-
-function getFontColor(index) {
-  return Number.isInteger((index + 1) / allFontColors.length)
-    ? allFontColors[0]
-    : allFontColors[
-        ((index + 1) / allFontColors.length -
-          Math.trunc((index + 1) / allFontColors.length)) *
-          allFontColors.length -
-          1
-      ];
-}
-
-function getInputStyles(questionType, questionNumber, optionNumber) {
-  if (questionType == "2") {
-    return state.vote[questionNumber].includes(optionNumber + "")
-      ? {
-          borderColor: "black",
-          backgroundColor: "black",
-          width: "1rem",
-          marginRight: "0.7rem",
-        }
-      : {
-          width: "1rem",
-          marginRight: "0.7rem",
-        };
-  } else {
-    return optionNumber + "" == state.vote[questionNumber]
-      ? {
-          borderColor: "black",
-          backgroundColor: "black",
-          width: "1rem",
-          marginRight: "0.7rem",
-        }
-      : {
-          width: "1rem",
-          marginRight: "0.7rem",
-        };
-  }
-}
 
 // Utility function
 function getBlockTimestamp(blockHeight) {
@@ -201,7 +76,9 @@ function getOptionRelatedValidAnswers(answers) {
 }
 
 // Getting valid answers
-const answers = Social.index("poll_question", `answer-v${indexVersion}`);
+const answers = isTest
+  ? Social.index("poll_question", `test-answer-v${indexVersion}`)
+  : Social.index("poll_question", `answer-v${indexVersion}`);
 
 if (JSON.stringify(answers) != JSON.stringify(state.answers)) {
   State.update({ answers: answers });
@@ -218,7 +95,7 @@ const isUserAllowedToVote = (accountId) => {
   });
   const hasSBTToken = view?.[0]?.[1]?.[0];
   return hasSBTToken || whitelist.includes(accountId);
-}
+};
 
 const answersToThisPoll = state.answers.filter(
   (a) => a.value.questionBlockHeight == props.poll.blockHeight
@@ -228,7 +105,7 @@ function getValidAnswers() {
   let validOptionAndTimeAnswers =
     getOptionRelatedValidAnswers(validTimeAnswers);
   const validateAddresses = validOptionAndTimeAnswers.filter((item) => {
-    return isUserAllowedToVote(item.accountId)
+    return isUserAllowedToVote(item.accountId);
     // const view = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
     //   account: item.accountId,
     //   issuer: "fractal.i-am-human.near",
@@ -238,8 +115,6 @@ function getValidAnswers() {
   return validateAddresses;
 }
 const validAnswersToThisPoll = getValidAnswers(answersToThisPoll);
-
-let userVote;
 // Getting if user has already voted
 const currAccountId = context.accountId ?? "";
 function userHasVoted() {
@@ -271,140 +146,23 @@ function countVotes(questionNumber, questionType) {
   }, new Array(poll.value.questions[questionNumber].choicesOptions.length).fill(0));
 }
 
-//TODO review this!
-const getPublicationParams = () => {
-  return {
-    index: {
-      poll_question: JSON.stringify(
-        {
-          key: `answer-v${indexVersion}`,
-          value: {
-            answer: state.vote,
-            questionBlockHeight: props.poll.blockHeight,
-          },
-        },
-        undefined,
-        0
-      ),
-    },
-  };
-};
-
-function isVoteValid() {
-  let isValid = state.vote.length == poll.value.questions.length;
-  isValid = isValid && context.accountId;
-  for (let i = 0; i < state.vote.length; i++) {
-    const vote = state.vote[i];
-    // vote should always be a string, but in one case is treated as an array. Replace array with csv
-    if (Array.isArray(vote)) {
-      isValid = isValid && vote.filter((v) => v.trim() != "").length > 0;
-    } else {
-      isValid = isValid && vote.trim() != "";
-    }
-  }
-  return isValid;
-}
-
 function calculatePercentage(votesToThisOption) {
   if (validAnswersToThisPoll.length == 0) return 0;
   return ((votesToThisOption / validAnswersToThisPoll.length) * 100).toFixed(2);
-}
-
-function calculatePercentageOfOption(votes, index) {
-  const validAnswers = votes.reduce((acc, curr) => acc + curr, 0);
-
-  if (validAnswers == 0 || votes.length == 0) return 0;
-
-  const votesToThisOption = votes[index];
-  return ((votesToThisOption / validAnswers) * 100).toFixed(2);
-}
-
-function getBorderRadious(questionNumber, optionNumber) {
-  if (optionNumber == 0) {
-    return "12px 12px 4px 4px";
-  } else if (
-    optionNumber ==
-    poll.value.questions[questionNumber].choicesOptions.length - 1
-  ) {
-    return "4px 4px 12px 12px";
-  } else {
-    return "4px";
-  }
-}
-
-const isValidInput = () => {
-  let result = state.vote != "";
-  return result && !isPreview;
-};
-
-const renderAnswers = (questionNumber) => {
-  return (
-    <Widget
-      src={`${widgetOwner}/widget/EasyPoll.AnswerCommentsContainer`}
-      props={{
-        answers: validAnswersToThisPoll,
-        questionNumber,
-      }}
-    />
-  );
-};
-
-function clickRadioInputHandler(questionNumber, optionNumber) {
-  return () => {
-    let newVote = state.vote;
-
-    newVote[questionNumber] = optionNumber + "";
-    State.update({ vote: newVote });
-  };
-}
-
-function clickCheckboxInputHandler(questionNumber, optionNumber) {
-  return () => {
-    let newVote = state.vote;
-
-    let oldQuestionVotes = newVote[questionNumber];
-    let newQuestionVotes = [];
-
-    if (!oldQuestionVotes.includes(optionNumber + "")) {
-      newQuestionVotes = oldQuestionVotes;
-      newQuestionVotes.push(optionNumber + "");
-    } else {
-      for (let i = 0; i < oldQuestionVotes.length; i++) {
-        if (oldQuestionVotes[i] != optionNumber + "") {
-          newQuestionVotes.push(oldQuestionVotes[i]);
-        }
-      }
-    }
-
-    newVote[questionNumber] = newQuestionVotes.filter((a) => a != "");
-
-    State.update({ votes: newVote });
-  };
 }
 
 return (
   <Widget
     src={`${widgetOwner}/widget/EasyPoll.DisplayVote2`}
     props={{
-      state,
+      isTest,
       poll,
       canVote,
-      getBgColor,
-      getFontColor,
       countVotes,
-      getBorderRadious,
-      calculatePercentageOfOption,
-      getBlockTimestamp,
-      clickCheckboxInputHandler,
-      clickRadioInputHandler,
-      getInputStyles,
       hasVoted,
-      getPublicationParams,
-      stateUpdate: (data) => State.update(data),
+      indexVersion,
       isQuestionOpen,
-      isVoteValid,
       validAnswersToThisPoll,
-      renderAnswers,
       whitelist,
     }}
   />
