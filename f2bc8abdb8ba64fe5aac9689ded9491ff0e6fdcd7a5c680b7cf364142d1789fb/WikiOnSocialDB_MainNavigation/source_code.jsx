a6cc5d37524936b8be2e63ebfda80e3bffb8bbc7
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-19 17:41:04 +0000

    Update WikiOnSocialDB_MainNavigation by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-19T17:41:04.373Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_MainNavigation/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_MainNavigation/source_code.jsx
@@ -1,121 +1,42 @@
-const currentPill = props.currentNavPill ?? "";
-const writersWhiteList = props.writersWhiteList ?? [
-  "testwiki.near",
-  "eugenewolf507.near",
+const addressForArticles = "ndcWikiArticle";
+const writersWhiteList = [
+  "neardigitalcollective.near",
+  "blaze.near",
+  "jlw.near",
+  "kazanderdad.near",
+  "joep.near",
+  "sarahkornfeld.near",
+  "yuensid.near",
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
 ];
-const authorForWidget =
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
-const pills = [
-  {
-    id: "articles",
-    title: "Articles",
-    widgetName: "WikiOnSocialDB",
-  },
-  {
-    id: "authors",
-    title: "Authors",
-    widgetName: "WikiOnSocialDB_Authors",
-  },
-];
-
-const Button = styled.button`
-  margin: 0px 1rem;
-  padding: 0;
-  border: 0;
-  background-color: white;
-  
-  a {
-    display: inline-block;
-    text-align: center;
-    vertical-align: middle;
-    cursor: pointer;
-    user-select: none;
-    transition: color 0.15s ease-in-out,background-color 0.15s ease-in-out,border-color 0.15s ease-in-out,box-shadow 0.15s ease-in-out;
-
-    border: 2px solid transparent;
-    font-weight: 500;
-    padding: 0.3rem 0.5rem;
-    background-color: #010A2D;
-    border-radius: 12px;
-    color: white;
-    text-decoration: none;   
-  }
-
-  a:hover {
-    color: #010A2D;
-    background-color: white;
-  }
-`;
-
+const authorForWidget = "neardigitalcollective.near";
 const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
 
 return (
   <div
-    className="navbar navbar-expand-md border-bottom mb-3"
-    style={{ backgroundColor: "white" }}
+    className="container-fluid"
+    style={{
+      backgroundColor: "rgb(230, 230, 230)",
+      borderRadius: "20px",
+      padding: "0",
+    }}
   >
-    <div className="container-fluid">
-      <a
-        className="navbar-brand text-decoration-none"
-        href={`#/${authorForWidget}/widget/WikiOnSocialDB`}
-      >
-        {"<WikiOnSocialDB>"}
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div
-        className="collapse navbar-collapse justify-content-center"
-        id="navbarNav"
-      >
-        <ul className="navbar-nav">
-          {pills.map(({ id, title, widgetName }, i) => (
-            <li className="nav-item">
-              <a
-                href={`#/${authorForWidget}/widget/${widgetName}`}
-                className={`nav-link ${
-                  id === currentPill
-                    ? "active text-decoration-underline"
-                    : "text-decoration-none"
-                } `}
-              >
-                {title}
-              </a>
-            </li>
-          ))}
-          <div className="d-block d-md-none">
-            <Button>
-              <a
-                href={`#/${authorForWidget}/widget/WikiOnSocialDB_CreateArticle`}
-              >
-                + Create Article
-              </a>
-            </Button>
-          </div>
-        </ul>
-      </div>
-      <div className="mx-2 d-none d-md-block">
-        <Button>
-          <a href={`#/${authorForWidget}/widget/WikiOnSocialDB_CreateArticle`}>
-            + Create Article
-          </a>
-        </Button>
-      </div>
-
-      <div className="d-none d-md-block">
-        <Widget
-          src="mob.near/widget/Profile.ShortInlineBlock"
-          props={{ accountId, tooltip: true }}
-        />
-      </div>
+    <Widget
+      src={`${authorForWidget}/widget/WikiOnSocialDB_MainNavigation`}
+      props={{ currentNavPill: "articles", writersWhiteList }}
+    />
+    <div style={{ margin: "0 auto", width: "90%", minWidth: "360px" }}>
+      <Widget
+        src={`${authorForWidget}/widget/WikiOnSocialDB_AllArticlesList`}
+        props={{ writersWhiteList }}
+      />
     </div>
   </div>
 );
