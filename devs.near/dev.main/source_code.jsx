c443e4865fa31d8a1a8540a6a21e9eca29f0e080
devs.near <devs.near>
2023-06-19 13:50:26 +0000

    Update dev.main by devs.near at 2023-06-19T13:50:26.033Z

--- devs.near/dev.main/source_code.jsx
+++ devs.near/dev.main/source_code.jsx
@@ -52,7 +52,7 @@ return (
       {widgets.map(
         ({ src, requiresLogin }, i) =>
           (!requiresLogin || context.accountId) && (
-            <div key={i} className="text-bg-light rounded-4 p-3 mb-3">
+            <div key={i} className="p-3 mb-3">
               <Widget src={src} />
             </div>
           )
