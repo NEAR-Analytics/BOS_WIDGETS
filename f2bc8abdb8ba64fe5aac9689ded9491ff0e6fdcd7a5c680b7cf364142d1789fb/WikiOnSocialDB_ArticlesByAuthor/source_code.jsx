7dafc7915a72dd8ebacc800e57ee0b5bc42089e2
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-19 17:42:44 +0000

    Update WikiOnSocialDB_ArticlesByAuthor by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-19T17:42:44.047Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_ArticlesByAuthor/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_ArticlesByAuthor/source_code.jsx
@@ -1,151 +1,132 @@
-const addressForArticles = "wikiTest2Article";
+const currentPill = props.currentNavPill ?? "";
+const writersWhiteList = props.writersWhiteList ?? [
+  "neardigitalcollective.near",
+  "blaze.near",
+  "jlw.near",
+  "kazanderdad.near",
+  "joep.near",
+  "sarahkornfeld.near",
+  "yuensid.near",
+  "shubham007.near",
+  "psalm.near",
+  "fiftycent.near",
+];
 const authorForWidget =
   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
-const authorId = props.author;
+const pills = [
+  {
+    id: "articles",
+    title: "Articles",
+    widgetName: "NDCDocs",
+  },
+  {
+    id: "authors",
+    title: "Authors",
+    widgetName: "WikiOnSocialDB_Authors",
+  },
+];
+
+const Button = styled.button`
+  margin: 0px 1rem;
+  padding: 0;
+  border: 0;
+  background-color: white;
+  
+  a {
+    display: inline-block;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    user-select: none;
+    transition: color 0.15s ease-in-out,background-color 0.15s ease-in-out,border-color 0.15s ease-in-out,box-shadow 0.15s ease-in-out;
+
+    border: 2px solid transparent;
+    font-weight: 500;
+    padding: 0.3rem 0.5rem;
+    background-color: #010A2D;
+    border-radius: 12px;
+    color: white;
+    text-decoration: none;   
+  }
+
+  a:hover {
+    color: #010A2D;
+    background-color: white;
+  }
+`;
+
 const accountId = props.accountId ?? context.accountId;
-if (!accountId) {
-  return "No account ID";
-}
-const profile = props.profile ?? Social.getr(`${authorId}/profile`);
-if (profile === null) {
-  return "Loading";
-}
-
-const getDateLastEdit = (timestamp) => {
-  const date = new Date(Number(timestamp));
-  const dateString = {
-    date: date.toLocaleDateString(),
-    time: date.toLocaleTimeString(),
-  };
-  return dateString;
-};
-
-// ========== GET INDEX ARRAY FOR ARTICLES ==========
-const postsIndex = Social.index(addressForArticles, "main", {
-  order: "desc",
-  accountId: undefined,
-});
-// ========== GET ALL ARTICLES ==========
-const resultArticles =
-  postsIndex &&
-  postsIndex.reduce((acc, { accountId, blockHeight }) => {
-    const postData = Social.get(
-      `${accountId}/${addressForArticles}/main`,
-      blockHeight
-    );
-    const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
-    return [...acc, postDataWithBlockHeight];
-  }, []);
-// ========== FILTER DUBLICATES ==========
-const filteredArticles =
-  resultArticles.length &&
-  resultArticles.reduce((acc, article) => {
-    if (!acc.some(({ articleId }) => articleId === article.articleId)) {
-      return [...acc, article];
-    } else {
-      return acc;
-    }
-  }, []);
-
-const filteredArticlesByUser =
-  filteredArticles.length &&
-  filteredArticles.reduce((acc, article) => {
-    if (article.author === authorId) {
-      return [...acc, article];
-    } else {
-      return acc;
-    }
-  }, []);
-
-console.log(profile);
 
 return (
   <div
-    className="container-fluid"
-    style={{
-      backgroundColor: "rgb(230, 230, 230)",
-      borderRadius: "20px",
-      padding: "0",
-    }}
+    className="navbar navbar-expand-md border-bottom mb-3"
+    style={{ backgroundColor: "white" }}
   >
-    <Widget
-      src={`${authorForWidget}/widget/WikiOnSocialDB_MainNavigation`}
-      props={{ currentNavPill: "authors" }}
-    />
-    <div style={{ margin: "0 auto", width: "90%", minWidth: "360px" }}>
-      <div className="card">
-        <div className="d-flex justify-content-between">
-          {profile ? (
-            <Widget
-              key="image"
-              src="mob.near/widget/ProfileImage"
-              props={{
-                style: { width: "5em", height: "5em", marginRight: "0.1em" },
-                profile,
-                authorId,
-                className: "inline",
-                imageClassName: "rounded w-100 h-100 align-top",
-              }}
-            />
-          ) : (
-            <div style={{ width: "5em" }}></div>
-          )}
-          <div>
-            <h4 className="text-center my-1">{profile.name}</h4>
-            <h4 className="text-center">{authorId}</h4>
-          </div>
-          <div style={{ width: "5em" }}></div>
-        </div>
-      </div>
-      <div className="row card-group py-3">
-        {filteredArticlesByUser &&
-          filteredArticlesByUser.map((article) => (
-            <div className="col-sm-12 col-lg-6 col-2xl-4 gy-3">
-              <div className="card h-100" key={article.articleId}>
+    <div className="container-fluid">
+      <a
+        className="navbar-brand text-decoration-none"
+        href={`#/${authorForWidget}/widget/NDCDocs`}
+      >
+        {"NDCDocs📝 (neardocs v0.01)"}
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="collapse navbar-collapse justify-content-center"
+        id="navbarNav"
+      >
+        <ul className="navbar-nav">
+          {pills.map(({ id, title, widgetName }, i) => (
+            <li className="nav-item">
+              <a
+                href={`#/${authorForWidget}/widget/${widgetName}`}
+                className={`nav-link ${
+                  id === currentPill
+                    ? "active text-decoration-underline"
+                    : "text-decoration-none"
+                } `}
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+          {accountId &&
+            writersWhiteList.some((whiteAddr) => whiteAddr === accountId) && (
+              <div className="d-block d-md-none">
                 <a
-                  className="text-decoration-none text-dark"
-                  href={`#/${authorForWidget}/widget/WikiOnSocialDB_OneArticle?articleId=${article.articleId}&blockHeight=${article.blockHeight}&lastEditor=${article.lastEditor}
-            `}
+                  className="btn btn-outline-dark"
+                  href={`#/${authorForWidget}/widget/WikiOnSocialDB_CreateArticle`}
                 >
-                  <div className="card-body">
-                    <div className="row d-flex justify-content-center">
-                      <h5 className="card-title text-center pb-2 border-bottom">
-                        {article.articleId}
-                      </h5>
-                      <div className="col">
-                        <p className="card-subtitle text-muted text-start">
-                          {getDateLastEdit(article.timeCreate).date}
-                        </p>{" "}
-                      </div>
-                      <div className="col">
-                        <p className="card-subtitle text-muted text-end">
-                          {getDateLastEdit(article.timeCreate).time}
-                        </p>
-                      </div>
-                    </div>
-                    <div
-                      className="mt-3 alert alert-secondary"
-                      style={{ backgroundColor: "white" }}
-                    >
-                      <div>
-                        Last edit by{" "}
-                        <a
-                          href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${article.lastEditor}`}
-                          style={{ textDecoration: "underline" }}
-                        >
-                          {article.lastEditor}
-                        </a>
-                        <br />
-                        Edited on {getDateLastEdit(article.timeLastEdit).date}
-                        <br />
-                        Edit versions: {article.version}
-                      </div>
-                    </div>
-                  </div>
+                  + Create Article
                 </a>
               </div>
-            </div>
-          ))}
+            )}
+        </ul>
+      </div>
+      {accountId &&
+        writersWhiteList.some((whiteAddr) => whiteAddr === accountId) && (
+          <Button>
+            <a
+              href={`#/${authorForWidget}/widget/WikiOnSocialDB_CreateArticle`}
+            >
+              + Create Article
+            </a>
+          </Button>
+        )}
+      <div className="d-none d-md-block">
+        <Widget
+          src="mob.near/widget/Profile.ShortInlineBlock"
+          props={{ accountId, tooltip: true }}
+        />
       </div>
     </div>
   </div>
