67207116e78a73bb8f46e4491e759d8736309924
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-19 17:43:58 +0000

    Update WikiOnSocialDB_History.History by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-19T17:43:58.309Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_History.History/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/WikiOnSocialDB_History.History/source_code.jsx
@@ -2,8 +2,9 @@
 ---props---
 count(count: number)?: function,
 */
-const authorForWidget = "rodrigos.near";
-const addressForArticles = "wikiTest2Article";
+const addressForArticles = "ndcWikiArticle";
+const authorForWidget =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 const { articleId } = props;
 
 // if (typeof props.widgetPath !== "string")
