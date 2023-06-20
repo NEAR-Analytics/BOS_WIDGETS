c336eb99ba32732e4ccc5d7515bfbf68e6ca4b6c
marmajgaming.near <marmajgaming.near>
2023-06-20 21:08:02 +0000

    Update MJG by marmajgaming.near at 2023-06-20T21:08:02.493Z

--- marmajgaming.near/MJG/source_code.jsx
+++ marmajgaming.near/MJG/source_code.jsx
@@ -31,7 +31,7 @@ return (
     </Description>
     <Widget src={"shemar268abel.near/widget/PixelParty"} />
     <Widget src={"ihyshan.near/widget/MJG-Widget"} />
-    <Widget src={"jay100.near/widget/MJG-Widget"} />
     <Widget src={"quan15.near/widget/MJG-Widget"} />
+    <Widget src={"jay100.near/widget/MJG-Widget"} />
   </Container>
 );
