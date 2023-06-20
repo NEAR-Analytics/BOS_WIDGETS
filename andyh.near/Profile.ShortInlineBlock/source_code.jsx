716307ef586e702fa067ba4340e940c11c8e3568
andyh.near <andyh.near>
2023-06-20 23:47:46 +0000

    Update Profile.ShortInlineBlock by andyh.near at 2023-06-20T23:47:46.621Z

---
+++ andyh.near/Profile.ShortInlineBlock/source_code.jsx
@@ -0,0 +1,37 @@
+const accountId = props.accountId;
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const name = profile?.name;
+
+const inner = (
+  <div className="d-flex flex-row">
+    <Widget
+      src="mob.near/widget/ProfileImage"
+      props={{
+        metadata,
+        accountId,
+        widgetName,
+        style: { height: "2.5em", width: "2.5em", minWidth: "2.5em" },
+        className: "me-2",
+      }}
+    />
+    <div className="text-truncate lh-sm">
+      <div className="text-truncate fw-bold">{name}</div>
+      <div className="text-truncate text-muted">
+        <small>
+          <span className="font-monospace">@{accountId}</span>
+        </small>
+      </div>
+    </div>
+  </div>
+);
+
+return props.tooltip ? (
+  <Widget
+    src="mob.near/widget/Profile.OverlayTrigger"
+    props={{ accountId, children: inner }}
+  />
+) : (
+  inner
+);
