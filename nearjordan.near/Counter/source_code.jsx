646a159fc2825a297337c13d3d3b740ab7bd2b5a
nearjordan.near <nearjordan.near>
2023-06-21 22:42:26 +0000

    Update Counter by nearjordan.near at 2023-06-21T22:42:26.032Z

--- nearjordan.near/Counter/source_code.jsx
+++ nearjordan.near/Counter/source_code.jsx
@@ -1,4 +1,40 @@
 const sender = Ethers.send("eth_requestAccounts", [])[0];
 if (!sender) return <Web3Connect connectLabel="Connect with Web3" />;
 
-return <p>Account test2: {sender}</p>;
+const abi=[
+    {
+      "inputs": [],
+      "name": "increment",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "number",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "newNumber",
+          "type": "uint256"
+        }
+      ],
+      "name": "setNumber",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
+
+return <p>Account test2: {sender} {abi[0].name}</p>;
