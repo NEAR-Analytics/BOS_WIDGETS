d28a2f38a16254821a3decf9f277e5b0ee9a70d2
nearjordan.near <nearjordan.near>
2023-06-21 21:54:40 +0000

    Update Counter by nearjordan.near at 2023-06-21T21:54:40.554Z

--- nearjordan.near/Counter/source_code.jsx
+++ nearjordan.near/Counter/source_code.jsx
@@ -1,4 +1,4 @@
 const sender = Ethers.send("eth_requestAccounts", [])[0];
 if (!sender) return <Web3Connect connectLabel="Connect with Web3" />;
 
-return <p>Account test: {sender}</p>;
+return <p>Account test2: {sender}</p>;
