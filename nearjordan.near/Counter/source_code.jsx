67603255ed53d814bca3ebc52183fe5ed4fdce46
nearjordan.near <nearjordan.near>
2023-06-21 23:22:55 +0000

    Update Counter by nearjordan.near at 2023-06-21T23:22:55.006Z

--- nearjordan.near/Counter/source_code.jsx
+++ nearjordan.near/Counter/source_code.jsx
@@ -55,6 +55,8 @@ const Counter_address = "0x5277e186c1995375132bb559f3E3F94f450bC669";
 
 const Counter_contract = new ethers.Contract(Counter_address, Counter_abi, Ethers.provider().getSigner());
 
-const n = (async function() { return await Counter_contract.number(); }) ();
+let num = 99;
+
+Counter_contract.number().then((n) => { num = n; });
 
 return <p>Account test2: {sender} {abi[0].name} Number: {n}</p>;
