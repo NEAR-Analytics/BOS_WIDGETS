6b502731d582f8441d78942a8fad5eec47dc268b
andyh.near <andyh.near>
2023-06-21 04:24:45 +0000

    Update Profile.ShortInlineBlock by andyh.near at 2023-06-21T04:24:45.506Z

--- andyh.near/Profile.ShortInlineBlock/source_code.jsx
+++ andyh.near/Profile.ShortInlineBlock/source_code.jsx
@@ -1,37 +1,58 @@
-const accountId = props.accountId;
+if (props.now) {
+  return "now";
+}
 
-const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+const keyPath = props.keyPath;
+let blockHeight = props.blockHeight ? parseInt(props.blockHeight) : undefined;
 
-const name = profile?.name;
+if (blockHeight === undefined && keyPath) {
+  blockHeight = Social.keys(keyPath, undefined, {
+    return_type: "BlockHeight",
+  });
+  if (blockHeight === null) {
+    return "Loading";
+  }
+  keyPath.split("/").forEach((key) => {
+    blockHeight = blockHeight[key];
+  });
+}
 
-const inner = (
-  <div className="d-flex flex-row">
-    <Widget
-      src="mob.near/widget/ProfileImage"
-      props={{
-        metadata,
-        accountId,
-        widgetName,
-        style: { height: "2.5em", width: "2.5em", minWidth: "2.5em" },
-        className: "me-2",
-      }}
-    />
-    <div className="text-truncate lh-sm">
-      <div className="text-truncate fw-bold">{name}</div>
-      <div className="text-truncate text-muted">
-        <small>
-          <span className="font-monospace">@{accountId}</span>
-        </small>
-      </div>
-    </div>
-  </div>
-);
+if (!blockHeight) {
+  return "unknown";
+}
 
-return props.tooltip ? (
-  <Widget
-    src="mob.near/widget/Profile.OverlayTrigger"
-    props={{ accountId, children: inner }}
-  />
-) : (
-  inner
+const block = Near.block(blockHeight);
+
+if (block === null) {
+  return "Loading";
+}
+
+if (!block) {
+  return "unknown";
+}
+
+const timeMs = parseFloat(block.header.timestamp_nanosec) / 1e6;
+const date = new Date(timeMs);
+const title = `${date.toLocaleTimeString([], {
+  hour: "2-digit",
+  minute: "2-digit",
+})} ${date.toLocaleDateString([], {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+})}`;
+
+const timeAgo = (diffSec) =>
+  diffSec < 60000
+    ? `${(diffSec / 1000) | 0}s`
+    : diffSec < 3600000
+    ? `${(diffSec / 60000) | 0}m`
+    : diffSec < 86400000
+    ? `${(diffSec / 3600000) | 0}h`
+    : `${(diffSec / 86400000) | 0}d`;
+
+return (
+  <span className={props.className} title={title}>
+    {timeAgo(Date.now() - timeMs)}
+  </span>
 );
