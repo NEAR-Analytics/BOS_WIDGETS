10a676f2028cb809beb7f1935cacb3750b8909c3
kenrou-it.near <kenrou-it.near>
2023-06-21 22:39:41 +0000

    Update Test by kenrou-it.near at 2023-06-21T22:39:41.929Z

--- kenrou-it.near/Test/source_code.jsx
+++ kenrou-it.near/Test/source_code.jsx
@@ -1,1 +1,31 @@
-return <div>Hello World 1</div>;
+const { accountId } = props;
+
+if (!accountId) {
+  return "accountId is not set";
+}
+
+const widgets = Social.keys(`${accountId}/widget/*`)[accountId].widget;
+
+return (
+  <>
+    <h1>{accountId}´s widgets</h1>
+    <table>
+      <thead>
+        <th>
+          <td>Widget name</td>
+          <td>Is deleted</td>
+        </th>
+      </thead>
+      <tbody>
+        {Object.keys(widgets).map((key) => {
+          return (
+            <tr>
+              <td>{key}</td>
+              <td>{widgets[key].toString()}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  </>
+);
