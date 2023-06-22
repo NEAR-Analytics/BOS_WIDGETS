37336de207167f6ccecade25a7c637b19115b329
nearcon23.near <nearcon23.near>
2023-06-22 09:22:02 +0000

    Update Home.BlackButton by nearcon23.near at 2023-06-22T09:22:02.167Z

---
+++ nearcon23.near/Home.BlackButton/source_code.jsx
@@ -0,0 +1,25 @@
+const Anchor = styled.a`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0.625em 1.25em;
+  width: 10.625em;
+  gap: 0.5em;
+  border: none;
+  border-radius: 50px;
+  background: #161615;
+  color: #fff;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 0.95em;
+  line-height: 1em;
+  text-align: center;
+  white-space: nowrap;
+
+  &:disabled {
+    background: #e5e5e5;
+  }
+`;
+
+return <Anchor href={props.href ?? "#"}>{props.text}</Anchor>;
