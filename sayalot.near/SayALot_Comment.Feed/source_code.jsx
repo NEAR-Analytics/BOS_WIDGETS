4e7526570efa8d7b93e66634221ef0b3fe237583
sayalot.near <sayalot.near>
2023-06-22 18:01:29 +0000

    Update SayALot_Comment.Feed by sayalot.near at 2023-06-22T18:01:29.561Z

--- sayalot.near/SayALot_Comment.Feed/source_code.jsx
+++ sayalot.near/SayALot_Comment.Feed/source_code.jsx
@@ -20,8 +20,8 @@ const renderItem = (a) =>
       <Widget
         src={`${authorForWidget}/widget/SayALot_Comment`}
         props={{
-          accountId: a.accountId,
-          blockHeight: a.blockHeight,
+          articleAccountId: a.accountId,
+          articleBlockHeight: a.blockHeight,
           highlight:
             a.accountId === props.highlightComment?.accountId &&
             a.blockHeight === props.highlightComment?.blockHeight,
