b56216340ef32a486cc1bac0380e3946c48e3dd3
rubycop.near <rubycop.near>
2023-06-23 10:19:59 +0000

    Update NDC.Elections.Header by rubycop.near at 2023-06-23T10:19:59.182Z

--- rubycop.near/NDC.Elections.Header/source_code.jsx
+++ rubycop.near/NDC.Elections.Header/source_code.jsx
@@ -32,10 +32,10 @@ const timer = setInterval(() => {
 
   const diff = new Date(parseInt(end)).getTime() - new Date().getTime();
 
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  const days = Math.floor(diff / (60 * 60 * 24));
+  const hours = Math.floor((diff % (60 * 60 * 24)) / (60 * 60));
+  const minutes = Math.floor((diff % (60 * 60)) / 60);
+  const seconds = Math.floor(diff % 60);
 
   State.update({
     days: days,
