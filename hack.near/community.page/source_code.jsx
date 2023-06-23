107124be4cf5a1778b8a1d1a08b1e2a573d397f1
hack.near <hack.near>
2023-06-23 00:55:05 +0000

    Update community.page by hack.near at 2023-06-23T00:55:05.561Z

--- hack.near/community.page/source_code.jsx
+++ hack.near/community.page/source_code.jsx
@@ -14,7 +14,7 @@ if (props.tab && props.tab !== state.selectedTab) {
 }
 
 const profile = props.profile ?? Social.getr(`${accountId}/profile`);
-const accountUrl = `#/near/widget/ProfilePage?accountId=${accountId}`;
+const accountUrl = `#/hack.near/widget/community.page`;
 
 const Wrapper = styled.div`
   padding-bottom: 48px;
