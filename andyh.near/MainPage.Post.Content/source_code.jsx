1542dc269e74afe8870edd65b7e8dd4fa4ca6c18
andyh.near <andyh.near>
2023-06-23 21:03:26 +0000

    Update MainPage.Post.Content by andyh.near at 2023-06-23T21:03:26.386Z

--- andyh.near/MainPage.Post.Content/source_code.jsx
+++ andyh.near/MainPage.Post.Content/source_code.jsx
@@ -20,19 +20,38 @@ console.log({ raw, content });
         />
 */
 
-return content ? (
-  <>
-    {content.text &&
-      (raw ? (
-        <pre style={{ whiteSpace: "pre-wrap" }}>{content.text}</pre>
-      ) : (
-        <span>text here</span>
-      ))}
-  </>
-) : (
-  <span
-    className="spinner-grow spinner-grow-sm me-1"
-    role="status"
-    aria-hidden="true"
-  />
-);
+return <span>all good</span>;
+// return content ? (
+//   <>
+//     {content.text &&
+//       (raw ? (
+//         <pre style={{ whiteSpace: "pre-wrap" }}>{content.text}</pre>
+//       ) : (
+//         <span>text here</span>
+//       ))}
+//     {content.image &&
+//       (raw ? (
+//         <div>
+//           <pre>{/*JSON.stringify(content.image, undefined, 2)*/}hey</pre>
+//         </div>
+//       ) : (
+//         <div className="w-100 rounded-3 text-center">
+//           another one
+//           {/*<Widget
+//             src="mob.near/widget/Image"
+//             props={{
+//               image: content.image,
+//               className: "img-fluid rounded-3",
+//               style: { maxHeight: "100vh" },
+//             }}
+//           />*/}
+//         </div>
+//       ))}
+//   </>
+// ) : (
+//   <span
+//     className="spinner-grow spinner-grow-sm me-1"
+//     role="status"
+//     aria-hidden="true"
+//   />
+// );
