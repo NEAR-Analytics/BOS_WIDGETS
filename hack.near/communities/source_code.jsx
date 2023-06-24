bc9516b3fdbbe90c3488c7028d87d348b92198e0
hack.near <hack.near>
2023-06-24 23:40:05 +0000

    Update communities by hack.near at 2023-06-24T23:40:05.962Z

--- hack.near/communities/source_code.jsx
+++ hack.near/communities/source_code.jsx
@@ -125,7 +125,7 @@ const Flex = styled.div`
 
 const Container = styled.div`
   display: flex;
-  max-width: 1060px;
+  max-width: 333px;
   margin: 0 auto;
   flex-direction: column;
   align-items: center;
