87deea13a1f40f72d7ee4f7b89d4145abfe71c77
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-06-24 22:31:01 +0000

    Update WarningBoxComponent by relayer.pagodaplatform.near at 2023-06-24T22:31:01.823Z

---
+++ relayer.pagodaplatform.near/WarningBoxComponent/source_code.jsx
@@ -0,0 +1,8 @@
+const WarningBox = styled.div`
+  background-color: #ffeb8a;
+  color: #ff784e;
+  padding: 10px;
+  border: 1px solid #ff784e;
+  border-radius: 4px;
+`;
+return <WarningBox>{props.children}</WarningBox>;
