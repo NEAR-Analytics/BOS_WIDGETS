6ccd28a2bb129fc68b0c96154a6624ca3056047a
hack.near <hack.near>
2023-06-24 23:30:10 +0000

    Update communities by hack.near at 2023-06-24T23:30:10.040Z

--- hack.near/communities/source_code.jsx
+++ hack.near/communities/source_code.jsx
@@ -49,15 +49,6 @@ const handleJoin = () => {
   ]);
 };
 
-const Wrapper = styled.div`
-  --section-gap: 23px;
-  padding-top: 42px;
-
-  @media (max-width: 888px) {
-    padding-top: 0;
-  }
-`;
-
 const H1 = styled.h1`
   font-family: "FK Grotesk", sans-serif;
   font-style: normal;
@@ -145,90 +136,87 @@ const Container = styled.div`
   display: flex;
   max-width: 1060px;
   margin: 0 auto;
-  gap: ${(p) => p.gap ?? "var(--section-gap)"};
   flex-direction: column;
-  align-items: ${(p) => (p.center ? "center" : undefined)};
-  justify-content: ${(p) => (p.center ? "center" : undefined)};
-  text-align: ${(p) => (p.center ? "center" : undefined)};
-  padding: var(--section-gap) 23px;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 23px;
 `;
 
 return (
-  <Wrapper>
-    <Container center>
-      <Flex gap="23px" direction="column" alignItems="center">
-        <H1>
-          Regional Communities
-          <span>
-            DAO{" "}
-            <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
-              <path
-                d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
-                fill="#4D63EC"
-              />
-            </svg>
-          </span>
-        </H1>
+  <Container center>
+    <Flex gap="23px" direction="column" alignItems="center">
+      <H1>
+        Regional Communities
+        <span>
+          DAO{" "}
+          <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
+            <path
+              d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
+              fill="#4D63EC"
+            />
+          </svg>
+        </span>
+      </H1>
 
-        <Text size="29px" weight="600">
-          NEAR Ecosystem Governance
-        </Text>
-
-        {!accountId && (
-          <Widget
-            src="near/widget/DIG.Button"
-            props={{
-              href: "https://near.org/signup",
-              label: "Create Account",
-              variant: "outline-dark",
-              size: "large",
-            }}
-          />
-        )}
-
-        {(!human && (
-          <div>
-            <div className="mb-5">
-              <Widget src="hack.near/widget/communities.join" />
-            </div>
-            <div className="m-3">
-              <Widget
-                src="hack.near/widget/dao.proposals"
-                props={{ daoId, accountId, limit: 5 }}
-              />
-            </div>
-          </div>
-        )) || (
-          <div className="row">
-            <div className="col-6 mt-5">
-              <Widget
-                src="near/widget/DIG.Button"
-                props={{
-                  href: "https://i-am-human.app/?community=banyan&vertical=regionalcommunities",
-                  label: "Get Verified",
-                  variant: "outline-primary",
-                  size: "large",
-                }}
-              />
-            </div>
-            <div className="col-6">
-              <Widget src="hack.near/widget/gov.Badge" />
-            </div>
-          </div>
-        )}
-      </Flex>
-
-      <Text
-        size="14px"
-        weight="600"
-        style={{ textTransform: "uppercase", letterSpacing: "0.17em" }}
-      >
-        Made Possible by{" "}
-        <a href="https://near.org/blog/near-digital-collective-legal-framework/">
-          NDC
-        </a>
-        <Widget src="hack.near/widget/dev.Badge" />
+      <Text size="29px" weight="600">
+        NEAR Ecosystem Governance
       </Text>
-    </Container>
-  </Wrapper>
+
+      {!accountId && (
+        <Widget
+          src="near/widget/DIG.Button"
+          props={{
+            href: "https://near.org/signup",
+            label: "Create Account",
+            variant: "outline-dark",
+            size: "large",
+          }}
+        />
+      )}
+
+      {(!human && (
+        <div>
+          <div className="mb-5">
+            <Widget src="hack.near/widget/communities.join" />
+          </div>
+          <div className="m-3">
+            <Widget
+              src="hack.near/widget/dao.proposals"
+              props={{ daoId, accountId, limit: 5 }}
+            />
+          </div>
+        </div>
+      )) || (
+        <div className="row">
+          <div className="col-6 mt-5">
+            <Widget
+              src="near/widget/DIG.Button"
+              props={{
+                href: "https://i-am-human.app/?community=banyan&vertical=regionalcommunities",
+                label: "Get Verified",
+                variant: "outline-primary",
+                size: "large",
+              }}
+            />
+          </div>
+          <div className="col-6">
+            <Widget src="hack.near/widget/gov.Badge" />
+          </div>
+        </div>
+      )}
+    </Flex>
+
+    <Text
+      size="14px"
+      weight="600"
+      style={{ textTransform: "uppercase", letterSpacing: "0.17em" }}
+    >
+      Made Possible by{" "}
+      <a href="https://near.org/blog/near-digital-collective-legal-framework/">
+        NDC
+      </a>
+      <Widget src="hack.near/widget/dev.Badge" />
+    </Text>
+  </Container>
 );
