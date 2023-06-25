3b22525c830d5f46953c8d6f4a027f269341d404
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-06-25 05:06:57 +0000

    Update Tanso by relayer.pagodaplatform.near at 2023-06-25T05:06:57.317Z

--- relayer.pagodaplatform.near/Tanso/source_code.jsx
+++ relayer.pagodaplatform.near/Tanso/source_code.jsx
@@ -1,6 +1,7 @@
 return (
   <iframe
     iframeResizer
+    height="500000"
     src="https://sepolia.easscan.org/schema/view/0x447cefc057bdd611bc853756d3fd633e495d4d8d75a1aaed7ae5598573984c13"
   />
 );
