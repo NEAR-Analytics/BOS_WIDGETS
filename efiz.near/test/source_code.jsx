70278b06913acd4f8e988e3a9b04f60e5586876b
efiz.near <efiz.near>
2023-06-25 19:43:48 +0000

    Update test by efiz.near at 2023-06-25T19:43:48.729Z

--- efiz.near/test/source_code.jsx
+++ efiz.near/test/source_code.jsx
@@ -1,13 +1,18 @@
 function onCapture(src) {
-  console.log(src);
+  State.update({ imgSrc: src });
 }
 
 return (
   <>
-    <p>{JSON.stringify(src)}</p>
+    {state.imageSrc && (
+      <div>
+        <h3>Captured Image:</h3>
+        <img src={state.imageSrc} alt="Captured" />
+      </div>
+    )}
     <Widget
       src="efiz.near/widget/Common.Camera"
-      props={{ onCapture: onCapture }}
+      props={{ onCapture: onCapture, height: "800px" }}
     />
   </>
 );
