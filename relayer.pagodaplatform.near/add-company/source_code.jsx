adaad599b232a2ffba1a04760d11a786fe61e9e0
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-06-25 15:25:33 +0000

    Update add-company by relayer.pagodaplatform.near at 2023-06-25T15:25:33.244Z

--- relayer.pagodaplatform.near/add-company/source_code.jsx
+++ relayer.pagodaplatform.near/add-company/source_code.jsx
@@ -41,7 +41,7 @@ const addCompany = (domain) => {
   console.log("pass2");
 
   const domainBytes = Buffer.from(domain, "utf8");
-  console.log("pass3");
+  console.log("pass3",interface);
 
   const encodedData = interface.encodeFunctionData("add_Company", [
     domainBytes,
