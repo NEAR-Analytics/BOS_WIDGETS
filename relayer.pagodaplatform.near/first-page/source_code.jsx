3aca6985d6dc0aff19f2c9b18579e16d1b334adf
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-06-25 02:14:03 +0000

    Update first-page by relayer.pagodaplatform.near at 2023-06-25T02:14:03.217Z

---
+++ relayer.pagodaplatform.near/first-page/source_code.jsx
@@ -0,0 +1,50 @@
+const updatePage = props.updatePage
+  ? props.updatePage
+  : (page) => {
+      console.log("NO PAGE HANDLE", page);
+    };
+
+const BodyContent = styled.div`
+  padding: 32px;
+  text-align: center;
+`;
+const DeviderComponent = styled.div`
+  display:flex;
+  justify-content: space-around;
+`;
+return (
+  <div>
+    <Widget
+      src="s-farshad-k.near/widget/hero-header"
+      props={{
+        title: "EthWinner Employee check",
+        subtitle:
+          "We make finding a good colleague easier for your next project!",
+      }}
+    />
+    <DeviderComponent>
+      <div>
+        <Widget
+          src="a_liutiev.near/widget/button_general"
+          props={{
+            value: <span>Register a Company</span>,
+            handleButtonClick: () => {
+              updatePage("addCompany");
+            },
+          }}
+        />
+      </div>
+      <div>
+        <Widget
+          src="a_liutiev.near/widget/button_general"
+          props={{
+            value: <span>Register as an Employee</span>,
+            handleButtonClick: () => {
+              updatePage("addPerson");
+            },
+          }}
+        />
+      </div>
+    </DeviderComponent>
+  </div>
+);
