43fb286f6ea6eb9ae22859045f1ccc29227412be
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-06-26 21:14:45 +0000

    Update SayALot_AllArticlesList by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-06-26T21:14:45.437Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_AllArticlesList/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_AllArticlesList/source_code.jsx
@@ -9,13 +9,13 @@ const writersWhiteList = props.writersWhiteList ?? [
   "yuensid.near",
 ];
 const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
-const authorForWidget =
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const authorForWidget = "sayalot.near";
 // ========== GET INDEX ARRAY FOR ARTICLES ==========
 const postsIndex = Social.index(addressForArticles, "main", {
   order: "desc",
   accountId: undefined,
 });
+
 // ========== GET ALL ARTICLES ==========
 const resultArticles =
   postsIndex &&
@@ -31,6 +31,7 @@ const resultArticles =
     .filter((article) =>
       writersWhiteList.some((addr) => addr === article.author)
     )
+    .filter((article) => article.lastEditor === article.author)
     .filter((article) => !articleBlackList.includes(article.blockHeight));
 
 // ========== FILTER DUPLICATES ==========
