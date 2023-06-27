70e9a22646db07b1b271701d7816801ae5040012
hack.near <hack.near>
2023-06-27 16:53:28 +0000

    Update dao.join by hack.near at 2023-06-27T16:53:28.357Z

--- hack.near/dao.join/source_code.jsx
+++ hack.near/dao.join/source_code.jsx
@@ -1,123 +1,82 @@
-const accountId = props.accountId ?? context.accountId;
-const roleId = props.roleId ?? "community";
-const daoId = props.daoId ?? "build.sputnik-dao.near";
-const proposalId =
-  props.proposalId ?? Near.view(daoId, "get_last_proposal_id") - 1;
+const accountId = context.accountId;
+const memberId = props.memberId ?? context.accountId;
+const roleId = props.roleId ?? "Council";
+const daoId = props.daoId ?? "cannabis-genome.sputnik-dao.near";
 
-if (!accountId) {
-  return "";
-}
-
-State.init({
-  isMember: false,
-});
-
-// get DAO policy, deposit, and group
 const policy = Near.view(daoId, "get_policy");
 
 if (policy === null) {
   return "";
 }
-
 const deposit = policy.proposal_bond;
 
 const group = policy.roles
   .filter((role) => role.name === roleId)
   .map((role) => role.kind.Group);
 
-// get data from last proposal
-const proposal = Near.view(daoId, "get_proposal", {
-  id: proposalId,
+State.init({
+  isMember: false,
 });
-
-if (proposal === null) {
-  return "";
-}
-
-// check if the potential member submitted last proposal
-const canJoin = accountId && accountId !== proposal.proposer;
-
-const handleJoin = () => {
-  Near.call([
-    {
-      contractName: daoId,
-      methodName: "add_proposal",
-      args: {
-        proposal: {
-          description: `add ${accountId} to the ${roleId} group`,
-          kind: {
-            AddMemberToRole: {
-              member_id: accountId,
-              role: roleId,
-            },
-          },
-        },
-      },
-      gas: 219000000000000,
-      deposit: deposit,
-    },
-  ]);
-};
 
 const groupMembers = group.join(", ");
 
 const checkMembership = (groupMembers) => {
-  if (groupMembers.indexOf(accountId) !== -1) {
+  if (groupMembers.indexOf(memberId) !== -1) {
     return State.update({ isMember: true });
   }
 };
 
 const validMember = checkMembership(groupMembers);
 
-// check if the potential member is connected
-const connectEdge = Social.keys(
-  `${accountId}/graph/connect/${daoId}`,
-  undefined,
-  {
-    values_only: true,
-  }
-);
-
-const loading = connectEdge === null || inverseEdge === null;
-const isConnected = connectEdge && Object.keys(connectEdge).length;
+const handleProposal = () => {
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "add_proposal",
+      args: {
+        proposal: {
+          description: "add member to DAO",
+          kind: {
+            AddMemberToRole: {
+              member_id: memberId,
+              role: roleId,
+            },
+          },
+        },
+      },
+      gas: 300000000000000,
+      deposit: deposit,
+    },
+  ]);
+};
 
 return (
   <div>
-    {!isConnected ? (
-      <Widget src="hack.near/widget/dao.connect" props={{ daoId }} />
-    ) : (
-      <>
+    {accountId ? (
+      <div>
         {!validMember ? (
-          <>
-            {canJoin ? (
-              <button
-                className="btn btn-success m-1 rounded-5"
-                onClick={handleJoin}
-              >
-                Join
-              </button>
-            ) : (
-              <>
-                <button
-                  disabled={!canJoin}
-                  className="btn btn-success m-1 rounded-5"
-                >
-                  Pending
-                </button>
-              </>
-            )}
-          </>
+          <button className="btn btn-success m-1" onClick={handleProposal}>
+            Join DAO
+          </button>
         ) : (
-          <>
-            <button
-              disabled={validMember}
-              className="btn btn-success m-1 rounded-5"
-            >
-              Joined
-            </button>
-          </>
+          <a
+            className="btn btn-outline-success m-1"
+            href="#/hack.near/widget/verified.members"
+          >
+            Members
+          </a>
         )}
-      </>
+      </div>
+    ) : (
+      <Widget
+        src="near/widget/DIG.Button"
+        props={{
+          href: "https://near.org/signup",
+          label: "Create Account",
+          variant: "outline-dark",
+          size: "large",
+        }}
+      />
     )}
   </div>
 );
