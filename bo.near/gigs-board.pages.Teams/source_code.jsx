c2e9dd5c3bf85e665305aca82548213690891409
bo.near <bo.near>
2023-06-27 02:44:39 +0000

    Update gigs-board.pages.Teams by bo.near at 2023-06-27T02:44:39.571Z

--- bo.near/gigs-board.pages.Teams/source_code.jsx
+++ bo.near/gigs-board.pages.Teams/source_code.jsx
@@ -2,6 +2,7 @@
 const nearDevGovGigsContractAccountId =
   props.nearDevGovGigsContractAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
 const nearDevGovGigsWidgetsAccountId =
   props.nearDevGovGigsWidgetsAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
@@ -13,6 +14,7 @@ function widget(widgetName, widgetProps, key) {
     nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
     referral: props.referral,
   };
+
   return (
     <Widget
       src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
@@ -24,30 +26,73 @@ function widget(widgetName, widgetProps, key) {
 
 function href(widgetName, linkProps) {
   linkProps = { ...linkProps };
+
   if (props.nearDevGovGigsContractAccountId) {
     linkProps.nearDevGovGigsContractAccountId =
       props.nearDevGovGigsContractAccountId;
   }
+
   if (props.nearDevGovGigsWidgetsAccountId) {
     linkProps.nearDevGovGigsWidgetsAccountId =
       props.nearDevGovGigsWidgetsAccountId;
   }
+
   if (props.referral) {
     linkProps.referral = props.referral;
   }
+
   const linkPropsQuery = Object.entries(linkProps)
+    .filter(([_key, nullable]) => (nullable ?? null) !== null)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
+
   return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
     linkPropsQuery ? "?" : ""
   }${linkPropsQuery}`;
 }
 /* END_INCLUDE: "common.jsx" */
+/* INCLUDE: "core/adapter/dev-hub" */
+const contractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
 
-const access_info =
-  Near.view(nearDevGovGigsContractAccountId, "get_access_control_info") ?? null;
-const root_members =
-  Near.view(nearDevGovGigsContractAccountId, "get_root_members") ?? null;
+const DevHub = {
+  edit_community_github: ({ handle, github }) =>
+    Near.call(contractAccountId, "edit_community_github", { handle, github }) ??
+    null,
+
+  get_access_control_info: () =>
+    Near.view(contractAccountId, "get_access_control_info") ?? null,
+
+  get_all_authors: () =>
+    Near.view(contractAccountId, "get_all_authors") ?? null,
+
+  get_all_communities: () =>
+    Near.view(contractAccountId, "get_all_communities") ?? null,
+
+  get_all_labels: () => Near.view(contractAccountId, "get_all_labels") ?? null,
+
+  get_community: ({ handle }) =>
+    Near.view(contractAccountId, "get_community", { handle }) ?? null,
+
+  get_post: ({ post_id }) =>
+    Near.view(contractAccountId, "get_post", { post_id }) ?? null,
+
+  get_posts_by_author: ({ author }) =>
+    Near.view(contractAccountId, "get_posts_by_author", { author }) ?? null,
+
+  get_posts_by_label: ({ label }) =>
+    Near.view(nearDevGovGigsContractAccountId, "get_posts_by_label", {
+      label,
+    }) ?? null,
+
+  get_root_members: () =>
+    Near.view(contractAccountId, "get_root_members") ?? null,
+};
+/* END_INCLUDE: "core/adapter/dev-hub" */
+
+const access_info = DevHub.get_access_control_info() ?? null,
+  root_members = DevHub.get_root_members() ?? null;
 
 if (!access_info || !root_members) {
   return <div>Loading...</div>;
@@ -55,12 +100,12 @@ if (!access_info || !root_members) {
 
 const pageContent = (
   <div>
-    {widget("components.teams.LabelsPermissions", {
+    {widget("entity.team.LabelsPermissions", {
       rules: access_info.rules_list,
     })}
     {Object.keys(root_members).map((member) =>
       widget(
-        "components.teams.TeamInfo",
+        "entity.team.TeamInfo",
         { member, members_list: access_info.members_list },
         member
       )
