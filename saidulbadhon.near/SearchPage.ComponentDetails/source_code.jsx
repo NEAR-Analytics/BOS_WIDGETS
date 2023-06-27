abdc27a0d72059109647a4d0fb5db3b0f7dd8345
saidulbadhon.near <saidulbadhon.near>
2023-06-27 17:15:57 +0000

    Update SearchPage.ComponentDetails by saidulbadhon.near at 2023-06-27T17:15:57.513Z

--- saidulbadhon.near/SearchPage.ComponentDetails/source_code.jsx
+++ saidulbadhon.near/SearchPage.ComponentDetails/source_code.jsx
@@ -17,7 +17,8 @@ const code = Social.get(`${accountId}/widget/${widgetName}`);
 const data = Social.get(`${accountId}/widget/${widgetName}/**`);
 const metadata = data.metadata;
 const tags = Object.keys(metadata.tags || {});
-const detailsUrl = `/adminalpha.near/widget/ComponentDetailsPage?src=${src}`;
+// const detailsUrl = `/adminalpha.near/widget/ComponentDetailsPage?src=${src}`;
+const detailsUrl = `/search/${src}`;
 const shareUrl = `https://alpha.near.org${detailsUrl}`;
 
 const dependencyMatch =
