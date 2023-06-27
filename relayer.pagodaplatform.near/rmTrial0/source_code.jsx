c55073633b210ba40c327ca23e4e02c5ac9f5068
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-06-27 17:00:42 +0000

    Update rmTrial0 by relayer.pagodaplatform.near at 2023-06-27T17:00:42.631Z

--- relayer.pagodaplatform.near/rmTrial0/source_code.jsx
+++ relayer.pagodaplatform.near/rmTrial0/source_code.jsx
@@ -449,8 +449,8 @@ return (
       class="responsive-iframe"
       srcDoc={srcCode}
       style={{
-        height: "99vh",
-        width: "99vw",
+        height: "70vh",
+        width: "70vw",
       }}
     ></iframe>
   </>
