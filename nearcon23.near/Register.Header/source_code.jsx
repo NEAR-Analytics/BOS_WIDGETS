1c54b8bf8cdf9935261f649abc71979d1537d46b
nearcon23.near <nearcon23.near>
2023-06-27 15:25:28 +0000

    Update Register.Header by nearcon23.near at 2023-06-27T15:25:28.291Z

--- nearcon23.near/Register.Header/source_code.jsx
+++ nearcon23.near/Register.Header/source_code.jsx
@@ -1,12 +1,34 @@
-const desktopImage =
-  "https://ipfs.near.social/ipfs/bafkreicjciucingvja4hli3wx4pla7rh5dhyxgirdsjbceaz3cs63orezi";
-const mobileImage =
-  "https://ipfs.near.social/ipfs/bafkreig3kjljw3o5dld3yteq7hnoniglmzvhac7r5ezpbdfetwcmc3gyde";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 4.5em;
+  gap: 0.625em;
+  background: #000;
 
-const Header = styled.img`
-    @media (max-width: 650px) {    
-         content:url(${mobileImage});   
-    }
+  h1 {
+    font-family: "FK Grotesk";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 2.5em;
+    line-height: 1.25em;
+    color: #fff;
+  }
+
+  p {
+    font-family: "Mona Sans";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.25em;
+    line-height: 140%;
+    color: #00ec97;
+  }
 `;
 
-return <Header src={desktopImage} style={{ width: "100%" }} />;
+return (
+  <Container>
+    <h1>Register</h1>
+    <p>Early bird tickets are live for $79</p>
+  </Container>
+);
