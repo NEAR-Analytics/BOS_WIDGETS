14da7622dd1fead06ed35590b4ed77bc1dd77ddc
syi216.near <syi216.near>
2023-06-28 00:00:51 +0000

    Update NDC.Nomination.Page by syi216.near at 2023-06-28T00:00:51.279Z

--- syi216.near/NDC.Nomination.Page/source_code.jsx
+++ syi216.near/NDC.Nomination.Page/source_code.jsx
@@ -1,40 +1,24 @@
 // TODO: Should be grabbed from contract side
-const props = {
-  widgetProvider: "rubycop.near",
-  contractName: "elections-v2.gwg.testnet",
-  ndcOrganization: "test",
-  groups: [
+const contractProps = {
+  houses: [
     {
-      title: "Councile of Advisors",
-      src: "https://ipfs.near.social/ipfs/bafkreie4rfa63zedwnpbwm5lglqrwqhahcnf6slllqmq7sh46ngf5y4vsq",
-      leftVotes: 0,
-      maxVotes: 10,
-      submitted: true,
       id: 2,
       typ: "CouncileOfAdvisors",
       ref_link: "example.com",
+      start: 1689820065441,
+      end: 1690820065441,
       quorum: 100,
       voters_num: 150,
       seats: 10,
       result: [],
-      voters: [],
-      votes: {
-        available: 0,
-        total: 10,
-      },
     },
     {
-      title: "House of Merit",
-      src: "https://ipfs.near.social/ipfs/bafkreie4rfa63zedwnpbwm5lglqrwqhahcnf6slllqmq7sh46ngf5y4vsq",
-      leftVotes: 5,
-      maxVotes: 10,
-      submitted: false,
       id: 3,
       typ: "HouseOfMerit",
       title: "House Of Merit",
       ref_link: "example.com",
       start: 1685820065441,
-      end: 1686820065441,
+      end: 1696820065441,
       quorum: 100,
       voters_num: 150,
       seats: 10,
@@ -43,57 +27,32 @@ const props = {
         ["asfsdfsfdfddfsdfdfsdfdf.near", 150],
         ["blabla.near", 10],
         ["rubycop.near", 50],
-        ["candidate1.near", 0],
       ],
-      voters: [
-        {
-          accountId: "rubycop.near",
-          candidateId: "zomland.near",
-          txn_url: "3ZunLtfdnkAC1oTgUxy5KXJb7qQWULmcFpVvkaq2pd6b",
-        },
-        {
-          accountId: "voter1.near",
-          candidateId: "zomland.near",
-          txn_url: "3ZunLtfdnkAC1oTgUxy5KXJb7qQWULmcFpVvkaq2pd6b",
-        },
-        {
-          accountId: "voter1",
-          candidateId: "candidate1.near",
-          txn_url: "3ZunLtfdnkAC1oTgUxy5KXJb7qQWULmcFpVvkaq2pd6b",
-        },
-      ],
-      votes: {
-        available: 3,
-        total: 3,
-      },
     },
     {
-      title: "Transparancy Commision",
-      src: "https://ipfs.near.social/ipfs/bafkreie4rfa63zedwnpbwm5lglqrwqhahcnf6slllqmq7sh46ngf5y4vsq",
-      leftVotes: 10,
-      maxVotes: 10,
-      submitted: true,
       id: 4,
       typ: "TransparancyCommision",
       ref_link: "example.com",
-      start: 1685820065441,
-      end: 1686820065441,
+      start: 1655820065441,
+      end: 165820065441,
       quorum: 100,
       voters_num: 150,
       seats: 10,
       result: [],
-      voters: [],
-      votes: {
-        available: 0,
-        total: 1,
-      },
     },
   ],
 };
 
+const { houses } = contractProps;
+
+const electionContract = "elections-v2.gwg.testnet";
+const registryContract = "registry-unstable.i-am-human.testnet";
+const ndcOrganization = "test";
+
 // TODO: Should be grabbed from indexer
 const humanVoted = 800;
 const totalHumal = 1000;
+
 const myVotes = [
   { candidateId: "zomland.near", time: 1686820065441, typ: "House Of Merit" },
   {
@@ -106,25 +65,30 @@ const myVotes = [
     time: 1686820065441,
     typ: "Transparancy Commision",
   },
-  {
-    candidateId: "zomland.near",
-    time: 1686820065441,
-    typ: "Transparancy Commision",
-  },
 ];
 
-const { widgetProvider, groups } = props;
+const widgets = {
+  header: "syi216.near/widget/NDC.Nomination.Header",
+  houses: "rubycop.near/widget/NDC.Elections.Houses",
+  candidates: "rubycop.near/widget/NDC.Elections.Candidates",
+  statistic: "rubycop.near/widget/NDC.Elections.Statistic",
+  activities: "rubycop.near/widget/NDC.Elections.Activities",
+};
 
 State.init({
-  selectedGroup: groups[0].id,
+  selectedHouse: houses[0].id,
 });
 
 const handleSelect = (item) => {
-  State.update({ selectedGroup: item.id });
+  State.update({ selectedHouse: item.id });
 };
 
 const Container = styled.div`
   padding: 30px 0;
+`;
+
+const ActivityContainer = styled.div`
+  overflow-y: scroll;
 `;
 
 const Left = styled.div`
@@ -134,10 +98,6 @@ const Left = styled.div`
 `;
 
 const Center = styled.div`
-display: flex;
-flex-direction: row;
-flex-warp: wrap;
-gap: 16px;
 `;
 
 const Right = styled.div`
@@ -153,40 +113,42 @@ const H5 = styled.h5`
 
 return (
   <div>
-    {props.groups.map((group) => (
+    {contractProps.houses.map((group) => (
       <>
-        {group.id === state.selectedGroup && (
+        {group.id === state.selectedHouse && (
           <Widget
             key={i}
-            src={`syi216.near/widget/NDC-nomination-header`}
-            props={group}
+            src={widgets.header}
+            props={{
+              startTime: group.start,
+              endTime: group.end,
+              type: "Nomination",
+            }}
           />
         )}
       </>
     ))}
     <Container className="d-flex row">
-      <Left className="col-lg-3">
-        <H5>Houses</H5>
+      <Left className="col-lg">
+        <H5>To Vote</H5>
         <Widget
-          key={i}
-          src={`${widgetProvider}/widget/NDC.Elections.Houses`}
+          src={widgets.houses}
           props={{
-            selectedGroup: state.selectedGroup,
-            groups: groups,
+            selectedHouse: state.selectedHouse,
+            houses: houses,
             handleSelect: (item) => handleSelect(item),
           }}
         />
       </Left>
-      <Center className="col-lg-8 p-2 p-md-3">
-        <div className="d-flex flex-row flex-wrap justify-content-between gap-4">
-          <Widget key={i} src={`syi216.near/widget/NDC.nomination.card`} />
-          <Widget key={i} src={`syi216.near/widget/NDC.nomination.card`} />
-          <Widget key={i} src={`syi216.near/widget/NDC.nomination.card`} />
-          <Widget key={i} src={`syi216.near/widget/NDC.nomination.card`} />
-          <Widget key={i} src={`syi216.near/widget/NDC.nomination.card`} />
-          <Widget key={i} src={`syi216.near/widget/NDC.nomination.card`} />
-          <Widget key={i} src={`syi216.near/widget/NDC.nomination.card`} />
-        </div>
+      <Center className="col-lg-8 p-2 p-md-3 d-flex flex-row flex-wrap justify-content-center gap-4">
+        <Widget src={"syi216.near/widget/NDC.nomination.card"} />
+        <Widget src={"syi216.near/widget/NDC.nomination.card"} />
+        <Widget src={"syi216.near/widget/NDC.nomination.card"} />
+        <Widget src={"syi216.near/widget/NDC.nomination.card"} />
+        <Widget src={"syi216.near/widget/NDC.nomination.card"} />
+        <Widget src={"syi216.near/widget/NDC.nomination.card"} />
+        <Widget src={"syi216.near/widget/NDC.nomination.card"} />
+        <Widget src={"syi216.near/widget/NDC.nomination.card"} />
       </Center>
     </Container>
   </div>
