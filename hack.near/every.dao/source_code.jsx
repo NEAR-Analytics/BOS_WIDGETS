3ca6c800dc71425ff74105ab00747bb954f56822
hack.near <hack.near>
2023-06-28 05:14:35 +0000

    Update every.dao by hack.near at 2023-06-28T05:14:35.459Z

--- hack.near/every.dao/source_code.jsx
+++ hack.near/every.dao/source_code.jsx
@@ -20,15 +20,27 @@ const total_daos = daos.length;
 
 const filtered_daos = filteredDaos.length;
 
+const Container = styled.div`
+  display: flex;
+  max-width: 888px;
+  margin: 0 auto;
+  flex-direction: column;
+  padding: 8px;
+
+  @media (max-width: 480px) {
+    max-width: 333px;
+  }
+`;
+
 return (
-  <div className="m-1">
-    <h2 className="mb-2">Every DAO</h2>
+  <Container>
+    <h2 className="mb-2">every dao</h2>
     {!state.name ? (
-      <h5 className="mb-3">Total: {total_daos}</h5>
+      <h5 className="mb-3">total ~ {total_daos}</h5>
     ) : (
-      <h5 className="mb-3">Filtered: {filtered_daos}</h5>
+      <h5 className="mb-3">filtered ~ {filtered_daos}</h5>
     )}
-    <h4 className="mb-2">Search by Name</h4>
+    <h4 className="mb-2">search by name</h4>
     <input
       placeholder="<example>.sputnik-dao.near"
       type="text"
@@ -38,11 +50,13 @@ return (
     <div className="mt-3">
       {filteredDaos.map((dao, j) => (
         <div className="d-flex m-2">
-          <a key={j} href={`/#/hack.near/widget/DAO.Profile?daoId=${dao}`}>
-            {dao}
-          </a>
+          <Widget
+            key={j}
+            src="hack.near/widget/dao.profile.card"
+            props={{ daoId: dao }}
+          />
         </div>
       ))}
     </div>
-  </div>
+  </Container>
 );
