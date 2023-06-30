7119d3e6dce8aa5fc2e51ddbc55deb654f2d61f6
819c44a1bdd666dce2119a6e92f9d7643136e02fc577e6cd0542bb38f6172b4a <819c44a1bdd666dce2119a6e92f9d7643136e02fc577e6cd0542bb38f6172b4a>
2023-06-30 05:43:25 +0000

    Update nyc.dao by 819c44a1bdd666dce2119a6e92f9d7643136e02fc577e6cd0542bb38f6172b4a at 2023-06-30T05:43:25.382Z

--- 819c44a1bdd666dce2119a6e92f9d7643136e02fc577e6cd0542bb38f6172b4a/nyc.dao/source_code.jsx
+++ 819c44a1bdd666dce2119a6e92f9d7643136e02fc577e6cd0542bb38f6172b4a/nyc.dao/source_code.jsx
@@ -3,6 +3,10 @@ const daoId = props.daoId ?? "liberty.sputnik-dao.near";
 const role = props.role ?? "community";
 const contractId = "mint.sharddog.near";
 
+State.init({
+  selectedTab: props.tab || "overview",
+});
+
 const tab = props.tab === "following" ? props.tab : "members";
 
 const nftData = Near.view(contractId, "nft_supply_for_owner", {
@@ -14,11 +18,11 @@ const isNftHolder = false;
 if (nftData > 0) {
   isNftHolder = true;
 }
+const accountUrl = `#/819c44a1bdd666dce2119a6e92f9d7643136e02fc577e6cd0542bb38f6172b4a/widget/nyc.dao`;
 
 const Wrapper = styled.div`
   --section-gap: 23px;
   padding-top: 42px;
-  background: linear-gradient(89.97deg, #AE67FA 1.84%, #F49867 102.67%); ;
 
   @media (max-width: 1155px) {
     .line-rounded-corners {
@@ -39,13 +43,13 @@ const H1 = styled.h1`
   line-height: 1;
   text-align: center;
   letter-spacing: -0.03em;
-  color: #FFFFFF;
+  color: #000;
   margin: 0;
   max-width: 700px;
 
   span {
     display: inline-block;
-    background: black ;
+    background: linear-gradient(89.97deg, #AE67FA 1.84%, #F49867 102.67%);;
     border-radius: 20px;
     position: relative;
     padding: 0.1em 0.2em 0;
@@ -55,9 +59,6 @@ const H1 = styled.h1`
       bottom: -8px;
       right: -10px;
       width: 24px;
-      color: #FFFFFF
-
-      
     }
   }
 
@@ -81,7 +82,7 @@ const Text = styled.p`
   font-size: ${(p) => p.size ?? "18px"};
   line-height: ${(p) => p.lineHeight ?? "1.5"};
   font-weight: ${(p) => p.weight ?? "400"};
-  color: ${(p) => p.color ?? "#000"};
+  color: ${(p) => p.color ?? ""};
   margin: 0;
   max-width: 670px;
 
@@ -118,15 +119,81 @@ const Container = styled.div`
     padding: var(--section-gap) 12px;
   }
 `;
+const Content = styled.div`
+  .post {
+    padding-left: 0;
+    padding-right: 0;
+  }
+`;
+
+const Title = styled.h1`
+  font-weight: 600;
+  font-size: ${(p) => p.size || "25px"};
+  line-height: 1.2em;
+  color: linear-gradient(89.97deg, #AE67FA 1.84%, #F49867 102.67%);
+  margin: ${(p) => (p.margin ? "0 0 24px" : "0")};
+  overflow-wrap: anywhere;
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  height: 48px;
+  border-bottom: 1px solid #eceef0;
+  margin-bottom: 72px;
+  overflow: auto;
+  scroll-behavior: smooth;
+
+  @media (max-width: 1200px) {
+    background: #f8f9fa;
+    border-top: 1px solid #eceef0;
+    margin: 0 -12px 48px;
+
+    > * {
+      flex: 1;
+    }
+  }
+`;
+
+const TabsButton = styled.a`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  font-weight: 600;
+  font-size: 12px;
+  padding: 0 12px;
+  position: relative;
+  color: ${(p) => (p.selected ? "#11181C" : "#687076")};
+  background: none;
+  border: none;
+  outline: none;
+  text-align: center;
+  text-decoration: none !important;
+  &:hover {
+    color: #11181c;
+  }
+
+  &::after {
+    content: "";
+    display: ${(p) => (p.selected ? "block" : "none")};
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 3px;
+    background: linear-gradient(89.97deg, #AE67FA 1.84%, #F49867 102.67%);;
+  }
+ 
+`;
 
 return (
   <Wrapper>
     <Container>
       <Flex>
         <H1>
-          NYC
+          🗽 Liberty
           <span>
-            BOS DEVS{" "}
+            DAO{" "}
             <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
               <path
                 d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
@@ -137,8 +204,8 @@ return (
         </H1>
         <div className="mt-3">
           <Text style={{ maxWidth: "350px" }}>
-            Need help getting your frontend on BOS? Contract one of our DEVS
-            below
+            New Yorkers building a better future with our local and global
+            communities.
           </Text>
         </div>
         {context.accountId && (
@@ -152,15 +219,91 @@ return (
         <Widget src="nycdao.near/widget/dao.cta" props={{ accountId, daoId }} />
       </Flex>
     </Container>
+    <Content>
+      <Tabs>
+        <TabsButton
+          href={`${accountUrl}&tab=overview`}
+          selected={state.selectedTab === "overview"}
+        >
+          Overview
+        </TabsButton>
+
+        <TabsButton
+          href={`${accountUrl}&tab=apps`}
+          selected={state.selectedTab === "apps"}
+        >
+          Components
+        </TabsButton>
+
+        <TabsButton
+          href={`${accountUrl}&tab=nfts`}
+          selected={state.selectedTab === "nfts"}
+        >
+          NFTs
+        </TabsButton>
+
+        <TabsButton
+          href={`${accountUrl}&tab=following`}
+          selected={state.selectedTab === "following"}
+        >
+          Following
+        </TabsButton>
+
+        <TabsButton
+          href={`${accountUrl}&tab=followers`}
+          selected={state.selectedTab === "followers"}
+        >
+          Followers
+        </TabsButton>
+
+        <TabsButton
+          href={`${accountUrl}&tab=explorer`}
+          selected={state.selectedTab === "explorer"}
+        >
+          Explorer
+        </TabsButton>
+      </Tabs>
+      {state.selectedTab === "overview" && (
+        <Widget
+          src="near/widget/FollowersList"
+          props={{
+            accountId:
+              "819c44a1bdd666dce2119a6e92f9d7643136e02fc577e6cd0542bb38f6172b4a",
+          }}
+        />
+      )}
+
+      {state.selectedTab === "nfts" && (
+        <Widget src="near/widget/NFTCollection" props={{ accountId }} />
+      )}
+      {state.selectedTab === "apps" && (
+        <Widget src="near/widget/ComponentCollection" props={{ accountId }} />
+      )}
+      {state.selectedTab === "followers" && (
+        <Widget src="near/widget/FollowersList" props={{ accountId }} />
+      )}
+      {state.selectedTab === "following" && (
+        <Widget src="near/widget/FollowingList" props={{ accountId }} />
+      )}
+      {state.selectedTab === "explorer" && (
+        <Widget
+          src="near/widget/Explorer.Account"
+          props={{
+            accountId,
+            network: context.networkId,
+            language: "en",
+            baseUrl: props.baseUrl,
+          }}
+        />
+      )}
+    </Content>
     {isNftHolder && (
       <div className="m-2 mb-5">
         <h5 className="mb-3">Non-Fungible Things</h5>
         <Widget src="near/widget/NFTCollection" props={{ accountId }} />
       </div>
     )}
-    <div className="m-2">
-      <Widget src="nycdao.near/widget/nyc.people" />
-    </div>
+
     <br />
 
     <br />
