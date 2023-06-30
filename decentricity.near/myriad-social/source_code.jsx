c943d9cc8f2a75566f052ddf1d9c64e311ca1919
decentricity.near <decentricity.near>
2023-06-30 08:17:03 +0000

    Update myriad-social by decentricity.near at 2023-06-30T08:17:03.438Z

--- decentricity.near/myriad-social/source_code.jsx
+++ decentricity.near/myriad-social/source_code.jsx
@@ -1,35 +1,26 @@
-const css = styled.b`
-html {overflow: auto;}    
-html,
-body,
-div,
-iframe {
+const StyledIframe = styled.iframe`
     margin: 0px;
     padding: 0px;
     height: 100%;
     min-height: 5000px;
     border: none;
-}
-iframe {
     display: block;
     width: 100%;
     border: none;
     overflow-y: auto;
     overflow-x: hidden;
-}
 `;
+
 return (
   <>
-    <css>
-      <iframe
-        src={"https://app.myriad.social"}
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-        width="100%"
-        height="100%"
-        scrolling="auto"
-      ></iframe>
-    </css>
+    <StyledIframe
+      src={"https://myriad.town"}
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+      width="100%"
+      height="100%"
+      scrolling="auto"
+    ></StyledIframe>
   </>
 );
