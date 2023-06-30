59cda4a665aa7f1c47d1a8cdb3af7015213782ec
ihyshan.near <ihyshan.near>
2023-06-30 23:36:31 +0000

    Update Carousel by ihyshan.near at 2023-06-30T23:36:31.884Z

--- ihyshan.near/Carousel/source_code.jsx
+++ ihyshan.near/Carousel/source_code.jsx
@@ -41,7 +41,7 @@ const Carousel = () => {
 
     html, body{
         font-family: Arial;
-        font-size: 20;
+        font-size: 20px;
         overflow: hidden;
         width: 100%;
         height: 100%;
