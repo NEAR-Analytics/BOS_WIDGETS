561f57f606f1063050060ce4ee5ef26d2de4d361
efiz.near <efiz.near>
2023-07-01 18:41:17 +0000

    Update Plate by efiz.near at 2023-07-01T18:41:17.938Z

--- efiz.near/Plate/source_code.jsx
+++ efiz.near/Plate/source_code.jsx
@@ -1,1 +1,16 @@
-return <Plate />;
+const editableProps = {
+  placeholder: "Type...",
+};
+
+const initialValue = [
+  {
+    type: "p",
+    children: [
+      {
+        text: "This is editable plain text with react and history plugins, just like a <textarea>!",
+      },
+    ],
+  },
+];
+
+return <Plate editableProps={editableProps} initialValue={initialValue} />;
