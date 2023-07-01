4f15a8ed6a08b2828290de95c67b0673e428deee
hack.near <hack.near>
2023-07-01 18:13:54 +0000

    Update dao.election by hack.near at 2023-07-01T18:13:54.068Z

--- hack.near/dao.election/source_code.jsx
+++ hack.near/dao.election/source_code.jsx
@@ -9,112 +9,152 @@ if (policy === null) {
 
 return (
   <div className="m-2">
-    <h3>Test Candidates</h3>
-    <p>FOR DEMO PURPOSES ONLY</p>
-    <h5>Africa</h5>
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 41,
-        policy,
-        candidateId: "candidate10.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 40,
-        policy,
-        candidateId: "candidate9.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
-    <h5>Asia</h5>
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 39,
-        policy,
-        candidateId: "candidate8.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 38,
-        policy,
-        candidateId: "candidate7.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
-    <h5>Africa</h5>
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 37,
-        policy,
-        candidateId: "candidate6.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 36,
-        policy,
-        candidateId: "candidate5.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
-    <h5>Africa</h5>
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 35,
-        policy,
-        candidateId: "candidate4.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 34,
-        policy,
-        candidateId: "candidate3.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
-    <h5>Africa</h5>
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 33,
-        policy,
-        candidateId: "candidate2.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
-    <Widget
-      src="hack.near/widget/dao.candidate"
-      props={{
-        daoId,
-        id: 32,
-        policy,
-        candidateId: "candidate1.near",
-        postUrl: "https://social.near.page/p/rc-dao.near/94244727",
-      }}
-    />
+    <div className="mt-3">
+      <div className="mb-3">
+        <Widget
+          src="near/widget/AccountProfile"
+          props={{ accountId: "rc-dao.sputnik-dao.near" }}
+        />
+      </div>
+      <h3>
+        <b>Election Demo</b>
+      </h3>
+      <p>
+        <span style={{ fontSize: "0.8em" }}>
+          🚧 <i>UNDER CONSTRUCTION</i> 🚧
+        </span>
+      </p>
+    </div>
+    <hr />
+    <h3>Africa</h3>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 41,
+          policy,
+          candidateId: "candidate10.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 40,
+          policy,
+          candidateId: "candidate9.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <br />
+    <h3>Asia</h3>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 39,
+          policy,
+          candidateId: "candidate8.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 38,
+          policy,
+          candidateId: "candidate7.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <br />
+    <h3>Europe</h3>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 37,
+          policy,
+          candidateId: "candidate6.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 36,
+          policy,
+          candidateId: "candidate5.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <br />
+    <h3>North America</h3>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 35,
+          policy,
+          candidateId: "candidate4.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 34,
+          policy,
+          candidateId: "candidate3.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <br />
+    <h3>South America</h3>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 33,
+          policy,
+          candidateId: "candidate2.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <div className="mb-3">
+      <Widget
+        src="hack.near/widget/dao.candidate"
+        props={{
+          daoId,
+          id: 32,
+          policy,
+          candidateId: "candidate1.near",
+          postUrl: "https://social.near.page/p/rc-dao.near/94244727",
+        }}
+      />
+    </div>
+    <br />
   </div>
 );
