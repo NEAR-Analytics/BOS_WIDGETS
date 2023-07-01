c52fdad64844ad0f2314db84f906cc2bba671f3c
hack.near <hack.near>
2023-07-01 18:43:11 +0000

    Update dao.nominee by hack.near at 2023-07-01T18:43:11.232Z

---
+++ hack.near/dao.nominee/source_code.jsx
@@ -0,0 +1,40 @@
+const accountId = props.accountId ?? context.accountId;
+const { daoId, policy } = props;
+const candidateId = props.candidateId;
+
+const postUrl =
+  props.postUrl ?? "https://social.near.page/p/rc-dao.near/94244727";
+
+const Card = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 15px;
+  width: 100%;
+  border-radius: 9px;
+  background: #fff;
+  border: 1px solid #eceef0;
+  box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
+    0px 1px 2px rgba(16, 24, 40, 0.06);
+  overflow: hidden;
+  padding: 12px;
+  margin: 0.555em;
+`;
+
+return (
+  <Card>
+    <Widget
+      src="near/widget/AccountProfile"
+      props={{ accountId: candidateId }}
+    />
+    <a className="btn btn-outline-primary" href={postUrl}>
+      Campaign
+    </a>
+    <div className="m-2">
+      <Widget
+        src="mob.near/widget/FollowButton"
+        props={{ accountId: candidateId }}
+      />
+    </div>
+  </Card>
+);
