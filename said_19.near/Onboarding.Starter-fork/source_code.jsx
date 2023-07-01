84079f39af5f62e3fa4c6fe15bc1f72ed171992a
said_19.near <said_19.near>
2023-07-01 09:31:52 +0000

    Update Onboarding.Starter-fork by said_19.near at 2023-07-01T09:31:52.992Z

---
+++ said_19.near/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,14 @@
+return (
+  <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
+    <Widget
+      src="near/widget/Onboarding.ComponentCard"
+      props={{ accountId }}
+    />{" "}
+    <h1>Inserte su nombre: </h1>
+    <input />
+    <br />
+    <Widget src="near/widget/Onboarding.ComponentCollection" props={{}} />
+    <Widget />
+  </>
+);
