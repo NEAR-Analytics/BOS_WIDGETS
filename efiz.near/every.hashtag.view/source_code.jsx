59b14a3c56cdae1ba05855c58b702dfc01cc0879
efiz.near <efiz.near>
2023-07-02 02:33:32 +0000

    Update every.hashtag.view by efiz.near at 2023-07-02T02:33:32.094Z

--- efiz.near/every.hashtag.view/source_code.jsx
+++ efiz.near/every.hashtag.view/source_code.jsx
@@ -4,7 +4,7 @@ return (
   <Widget
     src="efiz.near/widget/every.feed.view"
     props={{
-      data: { hashtagWhitelist: [hashtag] },
+      data: { hashtagWhitelist: [hashtag], typeWhitelist: ["md"] },
     }}
   />
 );
