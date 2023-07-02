29118e76c25549b29542cd1478fb48f31d438eb0
mattb.near <mattb.near>
2023-07-02 23:55:46 +0000

    Update NDCDocs.Components.ArticlesList by mattb.near at 2023-07-02T23:55:46.059Z

--- mattb.near/NDCDocs.Components.ArticlesList/source_code.jsx
+++ mattb.near/NDCDocs.Components.ArticlesList/source_code.jsx
@@ -64,4 +64,153 @@ const getDateLastEdit = (timestamp) => {
   return dateString;
 };
 
-return <div>{state.loaded ? "Hello World" : "Loading..."}</div>;
+function getName(accountId) {
+  return Social.getr(`${accountId}/profile`).name || "-";
+}
+
+function getProfileImage(accountId) {
+  let image = Social.getr(`${accountId}/profile`).image || {
+    ipfs_cid: "",
+  };
+
+  return `https://ipfs.near.social/ipfs/${image.ipfs_cid}`;
+}
+
+const Main = styled.div`
+    width:100%;
+    height:100vh;
+`;
+
+const ArticlePill = styled.a`
+    display:block;
+    cursor:pointer;
+    width:100%;
+    max-width:300px;
+    height:200px;
+    background-color:rgba(0,0,0,.05);
+    border-radius:20px;
+    box-sizing:border-box;
+    padding:10px;
+    border: 2px solid transparent;
+    transition: all .2s;
+    margin:0 auto 20px;
+    text-decoration:none;
+    color:#000;
+
+    &:hover {
+        transition: all .2s;
+        border: 2px solid rgb(0,0,0,.02);
+    }
+`;
+
+const ArticleTitle = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    width:100%;
+    max-width:300px;
+    height:70%;
+    background-color:#fff;
+    border-radius:15px;
+    box-sizing:border-box;
+    padding:20px;
+
+    h1 {
+        max-width:200px;
+        overflow:hidden;
+        text-overflow:ellipsis;
+        font-size:1.5rem;
+        font-weight:bold;
+        margin:0;
+        padding:0;
+
+        & ~ p {
+          opacity:.4;
+          padding:0;
+          margin:0;
+          font-size:.7rem;
+        }
+    }
+`;
+
+const ArticleDetails = styled.div`
+    margin-top:5px;
+    display:flex;
+    align-items:center;
+    width:100%;
+    max-width:300px;
+    height:30%;
+`;
+
+const Wrapper = styled.div`
+    h2 {
+        font-size:.8rem;
+        letter-spacing:-.5px;
+        padding:0;
+        margin:0;
+        margin-left:5px;
+
+        & ~ p {
+          opacity:.4;
+          padding:0;
+          margin:0;
+          font-size:.7rem;
+          margin-left:5px;
+        }
+    }
+`;
+
+const Avatar = styled.div`
+    border-radius:100%;
+    width:40px;
+    height:40px;
+    background-color:rgba(0,0,0,.05);
+    background-position:center;
+    background-size:cover;
+    background-repeat:no-repeat;
+`;
+
+return (
+  <>
+    {state.loaded &&
+      FILTERED_ARTICLES.map((article) => (
+        <>
+          <ArticlePill
+            href={`#/neardigitalcollective.near/widget/NDCDocs_OneArticle?articleId=${article.articleId}&blockHeight=${article.blockHeight}&lastEditor=${article.lastEditor}
+            `}
+          >
+            <ArticleTitle>
+              <div>
+                <h1>{article.articleId}</h1>
+                <p>Published on {getDateLastEdit(article.timeCreate).date}</p>
+                <p>Last edit by {article.lastEditor}</p>
+              </div>
+              <div>
+                <img
+                  src="https://ipfs.near.social/ipfs/bafkreigygnp234eyi5ljtxf7czp5emmhihjxitbl6e4zzuol3wgxsvkhcu"
+                  style={{
+                    maxWidth: "20px",
+                  }}
+                />
+              </div>
+            </ArticleTitle>
+            <ArticleDetails>
+              <Wrapper>
+                <Avatar
+                  style={{
+                    "background-image": `url("${getProfileImage(
+                      article.author
+                    )}")`,
+                  }}
+                />
+              </Wrapper>
+              <Wrapper>
+                <h2>{getName(article.author)}</h2>
+                <p>@{article.author}</p>
+              </Wrapper>
+            </ArticleDetails>
+          </ArticlePill>
+        </>
+      ))}
+  </>
+);
