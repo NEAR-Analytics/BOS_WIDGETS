b279e35d011fb17e828436764673f5ae0d19131f
lso0.near <lso0.near>
2023-07-02 08:45:27 +0000

    Update EncodeNearHorizonHackathon by lso0.near at 2023-07-02T08:45:27.165Z

---
+++ lso0.near/EncodeNearHorizonHackathon/source_code.jsx
@@ -0,0 +1,17 @@
+return (
+  <>
+    <div class="container text-center">
+      <img
+        style={{ maxWidth: "400px" }}
+        class="d-block mx-auto p-3"
+        src="https://images.squarespace-cdn.com/content/v1/5f9bcc27c14fc6134658484b/ea79070d-81e8-47ed-b651-8f86b2856207/events_02.png"
+      />
+      <a
+        href="https://www.encode.club/encode-near-horizon-hackathon"
+        class="btn btn-outline-secondary rounded-pill text-decoration-none"
+      >
+        Register to the hackathon
+      </a>
+    </div>
+  </>
+);
