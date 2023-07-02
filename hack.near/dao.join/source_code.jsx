956e63e3616c4cac17dea8cc2eaa4372a308e018
hack.near <hack.near>
2023-07-02 15:56:51 +0000

    Update dao.join by hack.near at 2023-07-02T15:56:51.088Z

--- hack.near/dao.join/source_code.jsx
+++ hack.near/dao.join/source_code.jsx
@@ -1,7 +1,11 @@
 const accountId = context.accountId;
 const memberId = props.memberId ?? context.accountId;
-const roleId = props.roleId ?? "Council";
-const daoId = props.daoId ?? "cannabis-genome.sputnik-dao.near";
+const roleId = props.roleId ?? "voter";
+const daoId = props.daoId ?? "rc-dao.sputnik-dao.near";
+
+if (!accountId) {
+  return "";
+}
 
 const policy = Near.view(daoId, "get_policy");
 
@@ -17,16 +21,6 @@ const group = policy.roles
 State.init({
   isMember: false,
 });
-
-const groupMembers = group.join(", ");
-
-const checkMembership = (groupMembers) => {
-  if (groupMembers.indexOf(memberId) !== -1) {
-    return State.update({ isMember: true });
-  }
-};
-
-const validMember = checkMembership(groupMembers);
 
 const handleProposal = () => {
   Near.call([
@@ -50,33 +44,45 @@ const handleProposal = () => {
   ]);
 };
 
+const groupMembers = group.join(", ");
+
+const checkMembership = (groupMembers) => {
+  if (groupMembers.indexOf(memberId) !== -1) {
+    return State.update({ isMember: true });
+  }
+};
+
+const validMember = checkMembership(groupMembers);
+
+// IAH Verification
+let human = false;
+const userSBTs = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
+  account: memberId,
+});
+
+for (let i = 0; i < userSBTs.length; i++) {
+  if ("fractal.i-am-human.near" == userSBTs[i][0]) {
+    human = true;
+  }
+}
+
 return (
   <div>
-    {accountId ? (
-      <div>
-        {!validMember ? (
-          <button className="btn btn-success m-1" onClick={handleProposal}>
-            Join DAO
-          </button>
-        ) : (
-          <a
-            className="btn btn-outline-success m-1"
-            href="#/hack.near/widget/verified.members"
-          >
-            Members
-          </a>
-        )}
-      </div>
+    {!validMember ? (
+      <button
+        disabled={!human}
+        className="btn btn-success m-1"
+        onClick={handleProposal}
+      >
+        Join DAO
+      </button>
     ) : (
-      <Widget
-        src="near/widget/DIG.Button"
-        props={{
-          href: "https://near.org/signup",
-          label: "Create Account",
-          variant: "outline-dark",
-          size: "large",
-        }}
-      />
+      <a
+        className="btn btn-outline-success m-1"
+        href="#/hack.near/widget/verified.members"
+      >
+        Members
+      </a>
     )}
   </div>
 );
