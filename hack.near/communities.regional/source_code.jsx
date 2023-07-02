83b53b5cfe4e1b71f2ae3e05bccf509e186145cf
hack.near <hack.near>
2023-07-02 02:35:48 +0000

    Update communities.regional by hack.near at 2023-07-02T02:35:48.823Z

--- hack.near/communities.regional/source_code.jsx
+++ hack.near/communities.regional/source_code.jsx
@@ -23,6 +23,16 @@ State.init({
   isMember: false,
 });
 
+const groupMembers = group.join(", ");
+
+const checkMembership = (groupMembers) => {
+  if (groupMembers.indexOf(accountId) !== -1) {
+    return State.update({ isMember: true });
+  }
+};
+
+const validMember = checkMembership(groupMembers);
+
 const handleProposal = () => {
   Near.call([
     {
@@ -44,28 +54,6 @@ const handleProposal = () => {
     },
   ]);
 };
-
-const groupMembers = group.join(", ");
-
-const checkMembership = (groupMembers) => {
-  if (groupMembers.indexOf(memberId) !== -1) {
-    return State.update({ isMember: true });
-  }
-};
-
-const validMember = checkMembership(groupMembers);
-
-// IAH Verification
-let human = false;
-const userSBTs = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
-  account: memberId,
-});
-
-for (let i = 0; i < userSBTs.length; i++) {
-  if ("fractal.i-am-human.near" == userSBTs[i][0]) {
-    human = true;
-  }
-}
 
 return (
   <div>
