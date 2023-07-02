ca13a874ee59c25bcd66efc6f48da4f30d222f4c
hack.near <hack.near>
2023-07-02 04:30:59 +0000

    Update dao.candidate by hack.near at 2023-07-02T04:30:59.965Z

--- hack.near/dao.candidate/source_code.jsx
+++ hack.near/dao.candidate/source_code.jsx
@@ -1,5 +1,10 @@
-const { daoId, proposal, policy } = props;
-const candidateId = props.candidateId;
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
+const proposal = JSON.parse(JSON.stringify(props.proposal)) ?? {
+  id: 39,
+};
+
+const policy = props.policy;
+const candidateId = props.candidateId ?? "multi.near";
 
 const postUrl =
   props.postUrl ?? "https://social.near.page/p/rc-dao.near/94244727";
@@ -10,7 +15,7 @@ const handleApprove = () => {
       contractName: daoId,
       methodName: "act_proposal",
       args: {
-        id: JSON.parse(props.proposal.id),
+        id: proposal.id,
         action: "VoteApprove",
       },
       gas: 200000000000000,
@@ -18,11 +23,23 @@ const handleApprove = () => {
   ]);
 };
 
-const dao_proposal = JSON.parse(JSON.stringify(proposal));
+const proposalId = props.proposalId ?? 41;
 
 const alreadyVoted = props.proposal.votes[accountId];
 const canVote =
   !alreadyVoted && props.proposal.status === "In Progress" && accountId;
+
+// IAH Verification
+let human = false;
+const userSBTs = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
+  account: memberId,
+});
+
+for (let i = 0; i < userSBTs.length; i++) {
+  if ("fractal.i-am-human.near" == userSBTs[i][0]) {
+    human = true;
+  }
+}
 
 const Card = styled.div`
   display: flex;
@@ -49,14 +66,19 @@ return (
     <a className="btn flex-fill btn-outline-primary" href={postUrl}>
       Campaign
     </a>
-    <div className="m-2 d-flex flex-row gap-2">
-      <button
-        disabled={!canVote}
-        className="btn flex-fill btn-success"
-        onClick={handleApprove}
-      >
-        Vote
-      </button>
-    </div>
+    {!canVote ? (
+      <div className="m-2 d-flex flex-row gap-2">
+        <button className="btn flex-fill btn-success" onClick={handleApprove}>
+          Vote
+        </button>
+      </div>
+    ) : (
+      <div className="m-2 d-flex flex-row gap-2">
+        <Widget
+          src="mob.near/widget/FollowButton"
+          props={{ accountId: candidateId }}
+        />
+      </div>
+    )}
   </Card>
 );
