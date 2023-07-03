7bd051e6aa9f9ef36f5ef819264c71a5bf765606
a43dafc5a941e0f880ca76fb9447d85016d0c13df4124818ebbc0f73ec540391 <a43dafc5a941e0f880ca76fb9447d85016d0c13df4124818ebbc0f73ec540391>
2023-07-03 01:02:56 +0000

    Update bosStudy0RegenMeaning by a43dafc5a941e0f880ca76fb9447d85016d0c13df4124818ebbc0f73ec540391 at 2023-07-03T01:02:56.893Z

---
+++ a43dafc5a941e0f880ca76fb9447d85016d0c13df4124818ebbc0f73ec540391/bosStudy0RegenMeaning/source_code.jsx
@@ -0,0 +1,16 @@
+const regenText = props.regenText;
+
+State.init({ regenTitle: "Regeneration" });
+
+return (
+  <>
+    <h1>{state.regenTitle}</h1>
+
+    <div>{regenText}</div>
+
+    <input
+      type="text"
+      onChange={(e) => State.update({ regenTitle: e.target.value })}
+    />
+  </>
+);
