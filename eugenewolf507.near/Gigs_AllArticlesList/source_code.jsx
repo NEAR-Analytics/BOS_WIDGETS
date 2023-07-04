e2407d2bc5e1e241acb682f7ab560de564be7166
eugenewolf507.near <eugenewolf507.near>
2023-07-04 10:36:30 +0000

    Update Gigs_AllArticlesList by eugenewolf507.near at 2023-07-04T10:36:30.577Z

--- eugenewolf507.near/Gigs_AllArticlesList/source_code.jsx
+++ eugenewolf507.near/Gigs_AllArticlesList/source_code.jsx
@@ -1,7 +1,3 @@
-//TODO !!!!!! update authorForWidget and authorsWhitelist!!!
-// const authorForWidget = "neardigitalcollective.near";
-const authorForWidget = "eugenewolf507.near";
-const accountId = props.accountId ?? context.accountId;
 const addressForArticles = "ndcGigArticle";
 const authorsWhitelist = props.writersWhiteList ?? [
   "neardigitalcollective.near",
@@ -11,61 +7,45 @@ const authorsWhitelist = props.writersWhiteList ?? [
   "joep.near",
   "sarahkornfeld.near",
   "yuensid.near",
-  "james.near",
-  "ndcplug.near",
   "eugenewolf507.near",
 ];
-// const authorsWhitelist = props.writersWhiteList ?? [
-// "neardigitalcollective.near",
-// "blaze.near",
-// "jlw.near",
-// "kazanderdad.near",
-// "joep.near",
-// "sarahkornfeld.near",
-// "yuensid.near",
-// "james.near",
-// "ndcplug.near",
-// ];
-const sharedArticleId = props.articleId;
 const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
+const authorForWidget = "neardigitalcollective.near";
 const statusTagsArr = ["open", "claimed", "closed"];
+// ========== GET INDEX ARRAY FOR ARTICLES ==========
+const postsIndex = Social.index(addressForArticles, "main", {
+  order: "desc",
+  accountId: undefined,
+});
 
-const getActualArticles = () => {
-  // ========== GET INDEX ARRAY FOR ARTICLES ==========
-  const postsIndex = Social.index(addressForArticles, "main", {
-    order: "desc",
-    accountId: undefined,
-  });
-  // ========== GET ALL ARTICLES ==========
-  const resultArticles =
-    postsIndex &&
-    postsIndex
-      .reduce((acc, { accountId, blockHeight }) => {
-        const postData = Social.get(
-          `${accountId}/${addressForArticles}/main`,
-          blockHeight
-        );
-        const postDataWithBlockHeight = {
-          ...JSON.parse(postData),
-          blockHeight,
-        };
-        return [...acc, postDataWithBlockHeight];
-      }, [])
-      .filter((article) =>
-        authorsWhitelist.some((addr) => addr === article.author)
-      )
-      .filter((article) => !articleBlackList.includes(article.blockHeight));
-  // ========== FILTER DUPLICATES ==========
-  const filteredArticles =
-    resultArticles.length &&
-    resultArticles.reduce((acc, article) => {
-      if (!acc.some(({ articleId }) => articleId === article.articleId)) {
-        return [...acc, article];
-      } else {
-        return acc;
-      }
-    }, []);
+// ========== GET ALL ARTICLES ==========
+const resultArticles =
+  postsIndex &&
+  postsIndex
+    .reduce((acc, { accountId, blockHeight }) => {
+      const postData = Social.get(
+        `${accountId}/${addressForArticles}/main`,
+        blockHeight
+      );
+      const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
+      return [...acc, postDataWithBlockHeight];
+    }, [])
+    .filter((article) =>
+      authorsWhitelist.some((addr) => addr === article.author)
+    )
+    .filter((article) => !articleBlackList.includes(article.blockHeight));
+// ========== FILTER DUPLICATES ==========
+const filteredArticles =
+  resultArticles.length &&
+  resultArticles.reduce((acc, article) => {
+    if (!acc.some(({ articleId }) => articleId === article.articleId)) {
+      return [...acc, article];
+    } else {
+      return acc;
+    }
+  }, []);
 
+const sortArticlesByTag = () => {
   if (filteredArticles === 0 || filteredArticles === undefined) {
     return;
   }
@@ -73,17 +53,17 @@ const getActualArticles = () => {
     filteredArticles &&
     filteredArticles.reduce(
       (acc, article) => {
-        if (article.statusTag === "claimed") {
+        if (article.currentStatusTag === "claimed") {
           const claimed = [...acc.claimed, article];
           const tempRes = { claimed };
           return { ...acc, ...tempRes };
         }
-        if (article.statusTag === "closed") {
+        if (article.currentStatusTag === "closed") {
           const closed = [...acc.closed, article];
           const tempRes = { closed };
           return { ...acc, ...tempRes };
         }
-        const intermediateArticle = { ...article, statusTag: "open" };
+        const intermediateArticle = { ...article, currentStatusTag: "open" };
         const open = [...acc.open, intermediateArticle];
         const tempRes = { open };
         return { ...acc, ...tempRes };
@@ -93,141 +73,132 @@ const getActualArticles = () => {
   return result;
 };
 
-// ========== LOCAL STORAGE ==========
-const localStorageData = JSON.parse(
-  Storage.privateGet("sortedArticlesByTagFromStorage")
-);
-const doesDataFresh = localStorageData.time
-  ? Date.now() - localStorageData.time < 4000
-  : false;
-
-const initialCardWithOpenModal = { cardWithOpenModal: sharedArticleId };
-
-if (doesDataFresh && localStorageData.sortedArticlesByTag) {
-  // ========== STATE INIT ========== with articles from Storage
-  State.init({
-    ...localStorageData.sortedArticlesByTag,
-    ...initialCardWithOpenModal,
-  });
-} else {
-  Storage.privateSet("sortedArticlesByTagFromStorage", "");
-
-  // ========== STATE INIT ========== with articles from near.social
-  const sortedArticlesByTag = getActualArticles();
-  sortedArticlesByTag &&
-    State.init({ ...sortedArticlesByTag, ...initialCardWithOpenModal });
-
-  const dataForStorage = {
-    sortedArticlesByTag,
-    time: Date.now(),
-  };
-  Storage.privateSet(
-    "sortedArticlesByTagFromStorage",
-    JSON.stringify(dataForStorage)
-  );
-}
+// ========== STATE INIT ==========
+const sortedArticlesByTag = sortArticlesByTag();
+sortedArticlesByTag && State.init(sortedArticlesByTag);
 
 // ========== UTILS ==========
+const getDateLastEdit = (timestamp) => {
+  const date = new Date(Number(timestamp));
+  const dateString = {
+    date: date.toLocaleDateString(),
+    time: date.toLocaleTimeString(),
+  };
+  return dateString;
+};
+
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
-const composeData = (gigObject) => {
-  const data = {
-    ndcGigArticle: {
-      main: JSON.stringify(gigObject),
-    },
-    index: {
-      ndcGigArticle: JSON.stringify({
-        key: "main",
-        value: {
-          type: "md",
-        },
-      }),
-    },
-  };
-  return data;
-};
-
-const doesUserCanChangeStatus = authorsWhitelist.some(
-  (whiteAddr) => whiteAddr === accountId
-);
-
-// if (sharedArticleId) {
-//   State.update({ cardWithOpenModal: sharedArticleId });
-// }
-
 // ========== HANDLER ==========
-const openModalHandler = (text) => {
-  State.update({ cardWithOpenModal: text });
-};
-
-const closeModalHandler = () => {
-  State.update({ cardWithOpenModal: false });
-};
-
-const updateStatusHandler = (oldStatus, newStatus, articleId) => {
+const clickHandler = (oldStatus, newStatus, articleId) => {
   const actualTag = oldStatus.toLowerCase();
   const newTag = newStatus.toLowerCase();
   // Find the index of the object to be moved
   const objectIndex = state[actualTag].findIndex(
     (obj) => obj.articleId === articleId
   );
-  // Check if an object was found and user have permission to change status
-  if (objectIndex !== -1 && doesUserCanChangeStatus) {
-    const objectToMove = state[actualTag].slice(objectIndex, 1)[0];
+  // Check if an object was found
+  if (objectIndex !== -1) {
+    const objectToMove = state[actualTag].splice(objectIndex, 1)[0];
     const updatedObjectToMove = {
       ...objectToMove,
-      lastEditor: accountId,
-      timeLastEdit: Date.now(),
-      statusTag: newStatus,
+      currentStatusTag: newStatus,
     };
-    const newData = composeData(updatedObjectToMove);
-    Social.set(newData, {
-      force: true,
-      onCommit: () => {
-        state[actualTag].splice(objectIndex, 1);
-        state[newTag].unshift(updatedObjectToMove);
-        state.cardWithOpenModal = "";
-        State.update();
-      },
-    });
+    state[newTag].unshift(updatedObjectToMove);
+    State.update();
   }
 };
 
 // ========== JSX ==========
-const ScrollWrapper = styled.div`
-  overflow-x: scroll !important;
-  position: relative;
-  padding-bottom: 15px;
-`;
+const StatusTagGroup = ({ activeStatus, articleId }) => (
+  <div className="d-flex flex-row flex-nowrap justify-content-between px-3 pb-3 ">
+    {statusTagsArr.map((tag) => (
+      <button
+        onClick={() => clickHandler(activeStatus, tag, articleId)}
+        className={`btn btn-sm ${
+          activeStatus === tag ? "btn-primary" : "btn-outline-primary"
+        }`}
+        disabled={activeStatus === tag}
+      >
+        #{tag}
+      </button>
+    ))}
+  </div>
+);
+
+const Card = ({ article }) => (
+  <div className="col gy-3" key={article.articleId}>
+    <div className="card h-100">
+      <a
+        className="text-decoration-none text-dark"
+        href={`#/${authorForWidget}/widget/Gigs_OneArticle?articleId=${article.articleId}&blockHeight=${article.blockHeight}&lastEditor=${article.lastEditor}
+            `}
+      >
+        <div className="card-body">
+          <div className="row d-flex justify-content-center">
+            <h5 className="card-title text-center pb-2 border-bottom">
+              {article.articleId}
+            </h5>
+            <div className="col flex-grow-1">
+              <Widget
+                src="mob.near/widget/Profile.ShortInlineBlock"
+                props={{
+                  accountId: article.author,
+                  tooltip: true,
+                }}
+              />
+            </div>
+            <div className="col flex-grow-0">
+              <p className="card-subtitle text-muted text-end">
+                {getDateLastEdit(article.timeCreate).date}
+              </p>{" "}
+              <p className="card-subtitle text-muted text-end">
+                {getDateLastEdit(article.timeCreate).time}
+              </p>
+            </div>
+          </div>
+          <div
+            className="mt-3 mb-0 alert alert-secondary"
+            style={{ backgroundColor: "white" }}
+          >
+            <div>
+              Last edit by{" "}
+              <a
+                href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${article.lastEditor}`}
+                style={{ textDecoration: "underline" }}
+              >
+                {article.lastEditor}
+              </a>
+              <br />
+              Edited on {getDateLastEdit(article.timeLastEdit).date}
+              <br />
+              Edit versions: {article.version}
+            </div>
+          </div>
+        </div>
+      </a>
+      <StatusTagGroup
+        activeStatus={article.currentStatusTag}
+        articleId={article.articleId}
+      />
+    </div>
+  </div>
+);
 
 return (
-  <ScrollWrapper>
-    <div class="row gx-2 d-flex flex-nowrap">
+  <div>
+    <div class="row gx-2">
       {statusTagsArr.map((tag) => (
         <div class="col">
-          <div class="border border-dark rounded-2 px-3 px-xl-4">
+          <div class="border border-dark rounded-2 px-3 pb-3">
             <div className="row card-group">
               <h4 className="pt-2 text-center">{capitalize(tag)}</h4>
               {state[tag].length > 0 &&
-                state[tag].map((item) => (
-                  <Widget
-                    src={`${authorForWidget}/widget/Gigs_AllArticlesList.Card`}
-                    props={{
-                      article: item,
-                      statusChangeHandler: updateStatusHandler,
-                      statusTagsArr,
-                      sharedArticleId,
-                      doesUserCanChangeStatus,
-                      openModalHandler,
-                      closeModalHandler,
-                      cardWithOpenModal: state.cardWithOpenModal,
-                    }}
-                  />
-                ))}
+                state[tag].map((item) => <Card article={item} />)}
             </div>
           </div>
         </div>
       ))}
     </div>
-  </ScrollWrapper>
+  </div>
 );
