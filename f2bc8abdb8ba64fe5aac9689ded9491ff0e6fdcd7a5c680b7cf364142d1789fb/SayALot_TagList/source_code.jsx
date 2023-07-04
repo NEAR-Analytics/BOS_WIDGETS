4e1311ee2a6b1bbf2eca350c36b031af51e96b77
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-07-04 20:32:14 +0000

    Update SayALot_TagList by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-07-04T20:32:14.509Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_TagList/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_TagList/source_code.jsx
@@ -1,5 +1,7 @@
-const authorForWidget =
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const authorForWidget = "sayalot.near";
+
+const isDebbug = props.isDebbug;
+
 const tagItemCss =
   "me-1 text-primary bg-primary bg-opacity-10 position-relative fw-normal badge border border-primary text-decoration-none";
 return (
@@ -8,7 +10,11 @@ return (
       props.tags.map((tag) => (
         <a
           key={tag}
-          href={`/#/${authorForWidget}/widget/SayALot_ArticlesByTag?tag=${tag}`}
+          href={
+            isDebbug
+              ? `/#/${authorForWidget}/widget/SayALot_ArticlesByTag?tag=${tag}&isDebbug=true`
+              : `/#/${authorForWidget}/widget/SayALot_ArticlesByTag?tag=${tag}`
+          }
           className={tagItemCss}
         >
           #{tag}
