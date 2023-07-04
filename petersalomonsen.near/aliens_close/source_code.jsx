01b9c7b60a3aa0f50bea2d973d90e0e0391a4d94
petersalomonsen.near <petersalomonsen.near>
2023-07-04 17:45:59 +0000

    Update aliens_close by petersalomonsen.near at 2023-07-04T17:45:59.227Z

---
+++ petersalomonsen.near/aliens_close/source_code.jsx
@@ -0,0 +1,6 @@
+return (
+  <iframe
+    className="w-100 h-100"
+    src="https://ipfs.web4.near.page/ipfs/bafkreie24lixvfsfngeodjbhm65dge6tfltxvf6iccxwp33azbhi6eol5a?filename=index.bundle.html"
+  />
+);
