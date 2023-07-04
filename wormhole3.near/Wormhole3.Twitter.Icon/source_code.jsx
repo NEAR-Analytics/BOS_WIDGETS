c24a896dcaf9f58db2fa816ff37eb6169cba24d3
wormhole3.near <wormhole3.near>
2023-07-04 16:20:12 +0000

    Update Wormhole3.Twitter.Icon by wormhole3.near at 2023-07-04T16:20:12.501Z

---
+++ wormhole3.near/Wormhole3.Twitter.Icon/source_code.jsx
@@ -0,0 +1,11 @@
+const Icon = styled.img`
+  padding-right: 10px;
+`;
+
+return (
+  <Icon
+    height={25}
+    src="https://ipfs.near.social/ipfs/bafkreid7ylesl3az67xrqcdiezpxiychpzjgnkgd5npfsfams7slfysq7i"
+    alt="twitter icon"
+  />
+);
