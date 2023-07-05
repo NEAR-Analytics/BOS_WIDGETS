e42ec3d9dffdc6ed478494138e46c1e9181fda92
sking.near <sking.near>
2023-07-05 07:12:49 +0000

    Update EasyPoll.ViewPoll by sking.near at 2023-07-05T07:12:49.912Z

--- sking.near/EasyPoll.ViewPoll/source_code.jsx
+++ sking.near/EasyPoll.ViewPoll/source_code.jsx
@@ -3,8 +3,8 @@ State.init({
   poll: {},
   polls: [{}],
   profile: {},
-  pollsByThisCreator: [{}],
-  answers: [{}],
+  userAnswers: [],
+  pollAnswers: [],
 });
 
 if (!props.blockHeight) {
@@ -13,9 +13,10 @@ if (!props.blockHeight) {
 
 const widgetOwner = "sking.near";
 const indexVersion = props.indexVersion ?? "3.2.0";
-const canOperate = props.canOperate;
-const whitelist = props.whitelist;
 const tabs = props.tabs;
+const accountId = props.accountId ?? context.accountId;
+const isHuman = props.isHuman;
+const resultsHref = props.resultsHref ?? "";
 
 let questionBlockHeight = Number(props.blockHeight);
 
@@ -48,26 +49,58 @@ if (JSON.stringify(profile) != JSON.stringify(state.profile)) {
   State.update({ profile: profile });
 }
 
-// if (!profile) {
-//   return "Loading";
-// }
-
-let pollsByThisCreator = Social.index(
-  "poll_question",
-  `question-v${indexVersion}`,
-  {
-    accountId: state.poll.accountId,
-  }
+let answers = Social.index("poll_question", `answer-v${indexVersion}`, {
+  accountId: accountId,
+});
+answers = answers.filter(
+  (v) => Number(v.value.pollBlockHeight) == questionBlockHeight
 );
-
-if (
-  JSON.stringify(pollsByThisCreator) != JSON.stringify(state.pollsByThisCreator)
-) {
-  State.update({ pollsByThisCreator: pollsByThisCreator });
+if (JSON.stringify(answers) != JSON.stringify(state.userAnswers)) {
+  State.update({ userAnswers: answers });
 }
 
-if (!state.pollsByThisCreator) {
-  return "Loading";
+const isVerifiedHuman = (account) => {
+  const view = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
+    account: `${account}`,
+    issuer: "fractal.i-am-human.near",
+  });
+  return view?.[0]?.[1]?.[0];
+};
+const getValidAnswersOnly = (input) => {
+  const {
+    value: { verifiedHumansOnly, endTimestamp, startTimestamp },
+  } = state.poll;
+
+  // should be only right poll
+  input = input.filter(
+    (v) => Number(v.value.pollBlockHeight) == questionBlockHeight
+  );
+  let filtered = input
+    // should be 1 per user
+    .map((e) => e["accountId"])
+    .map((e, i, final) => final.indexOf(e) === i && i)
+    .filter((e) => input[e])
+    .map((e) => input[e])
+    //
+    .filter(async (v, i) => {
+      // should respect human only
+      if (verifiedHumansOnly && !isVerifiedHuman(v.accountId)) return false;
+      // should respect startTimestamp
+      if (v.value.timestamp < startTimestamp) return false;
+      // should respect endTimestamp
+      if (v.value.timestamp > endTimestamp) return false;
+
+      return true;
+    });
+
+  return filtered;
+};
+
+let allAnswers = Social.index("poll_question", `answer-v${indexVersion}`);
+if (!allAnswers) return "Loading";
+allAnswers = getValidAnswersOnly(allAnswers);
+if (JSON.stringify(allAnswers) != JSON.stringify(state.pollAnswers)) {
+  State.update({ pollAnswers: allAnswers });
 }
 
 return (
@@ -81,8 +114,11 @@ return (
         blockHeight: questionBlockHeight,
         questionsByCreator,
         indexVersion,
-        whitelist,
         href: tabs.VIEW_POLL.href + questionBlockHeight,
+        userAnswers: state.userAnswers,
+        isHuman,
+        pollAnswers: state.pollAnswers,
+        resultsHref,
       }}
     />
   </>
