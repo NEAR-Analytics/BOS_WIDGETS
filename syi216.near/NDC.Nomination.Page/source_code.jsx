b4409e987fc02dccd04d13ae84742ce7a8035cf7
syi216.near <syi216.near>
2023-07-05 19:12:46 +0000

    Update NDC.Nomination.Page by syi216.near at 2023-07-05T19:12:46.409Z

--- syi216.near/NDC.Nomination.Page/source_code.jsx
+++ syi216.near/NDC.Nomination.Page/source_code.jsx
@@ -512,7 +512,7 @@ return (
           )}
         </div>
       </Left>
-      <Center className="col-lg-9 p-2 p-md-3 d-flex flex-row flex-wrap justify-content-start gap-4">
+      <Center className="col-lg-9 px-2 px-md-3 d-flex flex-row flex-wrap justify-content-start gap-4">
         {state.nominations.length > 0 ? (
           state.nominations.map((data) => {
             return (
