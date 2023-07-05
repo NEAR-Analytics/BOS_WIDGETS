97eb1ca777c7247cea5ffc3f124d98dc3aa86e99
sharddog.near <sharddog.near>
2023-07-05 17:20:16 +0000

    Update Just-Feed by sharddog.near at 2023-07-05T17:20:16.252Z

--- sharddog.near/Just-Feed/source_code.jsx
+++ sharddog.near/Just-Feed/source_code.jsx
@@ -1,19 +1,24 @@
 return (
   <>
-    <div className="tab-content row" id="pills-tabContent">
-      <div
-        className="tab-pane d-lg-block col-lg-auto"
-        id="pills-graph"
-        role="tabpanel"
-        aria-labelledby="pills-graph-tab"
-      ></div>
-      <div
-        className="tab-pane show active d-lg-block col-lg-auto"
-        id="pills-posts"
-        role="tabpanel"
-        aria-labelledby="pills-posts-tab"
-      >
-        <Widget src="sharddog.near/widget/JustFeed.MainPage.Content" />
+    <div className="row">
+      <div className="col-sm-1 position-sticky top-0" id="sharddogs">
+        <Widget src="sharddog.near/widget/Sharddog" />
+      </div>
+      <div className="tab-content col-sm-10 border-left" id="pills-tabContent">
+        <div
+          className="tab-pane  d-lg-block "
+          id="pills-graph"
+          role="tabpanel"
+          aria-labelledby="pills-graph-tab"
+        ></div>
+        <div
+          className="tab-pane show active d-lg-block "
+          id="pills-posts"
+          role="tabpanel"
+          aria-labelledby="pills-posts-tab"
+        >
+          <Widget src="sharddog.near/widget/JustFeed.MainPage.Content" />
+        </div>
       </div>
     </div>
   </>
