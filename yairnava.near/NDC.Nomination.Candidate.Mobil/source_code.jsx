4dd855673da2ce723b603b480072619e5f274f21
yairnava.near <yairnava.near>
2023-07-05 01:59:12 +0000

    Update NDC.Nomination.Candidate.Mobil by yairnava.near at 2023-07-05T01:59:12.983Z

--- yairnava.near/NDC.Nomination.Candidate.Mobil/source_code.jsx
+++ yairnava.near/NDC.Nomination.Candidate.Mobil/source_code.jsx
@@ -1,5 +1,4 @@
 console.log(props);
-
 const DetailContent = styled.div`
 display: inline-flex;
 flex-direction: column;
@@ -16,20 +15,18 @@ gap: 16px;
 border-radius: 10px;
 background: #F8F8F9;
 `;
-
 const DetailHeader = styled.div`
 display: flex;
 width: 326px;
 align-items: center;
 gap: 12px;
 `;
-
 const ProfilePicture = styled.img`
+border-radius: 20px;
 width: 40px;
 height: 40px;
 flex-shrink: 0;
 `;
-
 const HeaderDetailContent = styled.div`
 display: flex;
 flex-direction: column;
@@ -37,14 +34,13 @@ align-items: flex-start;
 gap: 4px;
 flex: 1 0 0;
 `;
-
 const TagContainer = styled.div`
 display: flex;
 align-items: flex-start;
 gap: 4px;
 `;
-
 const HouseTagDiv = styled.div`
+width:100%;
 display: flex;
 padding: 4px 8px;
 justify-content: center;
@@ -53,16 +49,14 @@ gap: 10px;
 border-radius: 100px;
 background: var(--gradient-purple-gradient, linear-gradient(90deg, #9333EA 0%, #4F46E5 100%));
 `;
-
 const HouseTagText = styled.p`
 color: #FFF;
-font-size: 7px;
+font-size: 8px;
 font-family: Avenir;
 font-weight: 500;
 line-height: 120%;
 margin: 0px;
 `;
-
 const TagDiv = styled.div`
 display: flex;
 padding: 4px 8px;
@@ -76,7 +70,6 @@ background-image: linear-gradient(white, white), radial-gradient(circle at top l
 background-origin: border-box;
 background-clip: padding-box, border-box;
 `;
-
 const TagDivText = styled.p`
 font-size: 8px;
 font-family: Avenir;
@@ -89,13 +82,11 @@ background: linear-gradient(90deg, #9333EA 0%, #4F46E5 100%);
 background-clip: text;
 text-fill-color: transparent;
 `;
-
 const NominationTitleContainer = styled.div`
 display: flex;
 flex-direction: column;
 align-items: flex-start;
 `;
-
 const NominationTitle = styled.p`
 display: flex;
 flex-direction: column;
@@ -107,7 +98,6 @@ font-family: Avenir;
 font-weight: 500;
 line-height: 120%;
 `;
-
 const NominationUser = styled.p`
 display: flex;
 flex-direction: column;
@@ -118,7 +108,6 @@ font-size: 12px;
 font-family: Avenir;
 line-height: 120%;
 `;
-
 const UpvoteButton = styled.button`
 display: flex;
 padding: 2px 12px;
@@ -130,7 +119,6 @@ background-image: linear-gradient(white, white), radial-gradient(circle at top l
 background-origin: border-box;
 background-clip: padding-box, border-box;
 `;
-
 const UpvoteCount = styled.p`
 font-size: 12px;
 font-family: Avenir;
@@ -143,12 +131,10 @@ background: linear-gradient(90deg, #9333EA 0%, #4F46E5 100%);
 background-clip: text;
 text-fill-color: transparent;
 `;
-
 const UpvoteIcon = styled.img`
 width: 14px;
 height: 14px;
 `;
-
 const PlatformCard = styled.div`
 display: flex;
 width: 326px;
@@ -158,21 +144,18 @@ gap: 12px;
 border-radius: 6px;
 background: #FFF;
 `;
-
 const PlatformContent = styled.div`
 display: flex;
 flex-direction: column;
 align-items: flex-start;
 gap: 12px;
 `;
-
 const PlatformHeaderDiv = styled.div`
 display: flex;
 width: 302px;
 align-items: flex-start;
 gap: 12px;
 `;
-
 const PlatformHeaderText = styled.p`
 display: flex;
 flex-direction: column;
@@ -184,14 +167,12 @@ font-weight: 800;
 line-height: 120%;
 margin: 0px;
 `;
-
 const PlatformInfoDiv = styled.div`
 display: flex;
 flex-direction: column;
 align-items: flex-start;
 gap: 8px;
 `;
-
 const PlatformInfoHeader = styled.div`
 display: flex;
 width: 302px;
@@ -199,7 +180,6 @@ flex-direction: column;
 align-items: flex-start;
 gap: 2px;
 `;
-
 const PlatInforHeadText = styled.p`
 margin: 0px;
 color: var(--000000, #000);
@@ -208,15 +188,12 @@ font-family: Avenir;
 font-weight: 500;
 line-height: 120%;
 `;
-
 const PlatInfoHeadSeparator = styled.hr`
 width: 302px;
 height: 0px;
 margin: 8px 0 0 0;
-
 border: 1px solid rgba(208, 214, 217, 1);
 `;
-
 const KeyIssuesContainer = styled.div`
 display: flex;
 width: 302px;
@@ -224,7 +201,6 @@ flex-direction: column;
 align-items: flex-start;
 gap: 2px;
 `;
-
 const KeyIssueTitle = styled.p`
 color: var(--primary-000000, #000);
 font-size: 12px;
@@ -232,7 +208,6 @@ font-family: Avenir;
 line-height: 120%;
 margin: 0px;
 `;
-
 const KeyIssueDescription = styled.p`
 color: #828688;
 font-size: 12px;
@@ -240,7 +215,6 @@ font-family: Avenir;
 line-height: 120%;
 margin: 0px;
 `;
-
 const CandidateCard = styled.div`
 display: flex;
 padding: 16px 12px;
@@ -250,7 +224,6 @@ align-self: stretch;
 border-radius: 6px;
 background: #FFF;
 `;
-
 const CandidateContent = styled.div`
 display: flex;
 width: 302px;
@@ -259,14 +232,12 @@ justify-content: center;
 align-items: flex-start;
 gap: 4px;
 `;
-
 const CandidateHeader = styled.div`
 display: flex;
 align-items: center;
 gap: 12px;
 align-self: stretch;
 `;
-
 const CandidateHeaderText = styled.p`
 color: #000;
 font-size: 12px;
@@ -275,7 +246,6 @@ font-weight: 800;
 line-height: 120%;
 margin: 0px;
 `;
-
 const CandidateInfoDiv = styled.div`
 display: flex;
 width: 290px;
@@ -284,19 +254,16 @@ flex-direction: column;
 align-items: flex-start;
 gap: 16px;
 `;
-
 const CandidateInfoHeader = styled.div`
 display: flex;
 align-items: center;
 gap: 8px;
 align-self: stretch;
 `;
-
 const CandidateImage = styled.img`
 width: 32px;
 height: 32px;
 `;
-
 const CandidateInfoData = styled.div`
 display: flex;
 flex-direction: column;
@@ -304,7 +271,6 @@ align-items: flex-start;
 gap: 4px;
 flex: 1 0 0;
 `;
-
 const CandidateTagDiv = styled.div`
 display: flex;
 height: 20px;
@@ -316,7 +282,6 @@ border-radius: 100px;
 border: 1px solid var(--secondary-warning, #F19D38);
 background: #F0E1CE;
 `;
-
 const CandidateTagText = styled.p`
 color: var(--secondary-warning, #F19D38);
 font-size: 10px;
@@ -325,7 +290,6 @@ font-weight: 500;
 line-height: 120%;
 margin: 0px;
 `;
-
 const CandidateTime = styled.p`
 margin: 0px;
 color: var(--primary-828688, #828688);
@@ -334,7 +298,6 @@ font-family: Avenir;
 font-weight: 500;
 line-height: 120%;
 `;
-
 const CandidateTextInfo = styled.div`
 display: flex;
 flex-direction: column;
@@ -342,7 +305,6 @@ align-items: flex-start;
 gap: 4px;
 align-self: stretch;
 `;
-
 const CandidateTitle = styled.p`
 color: var(--000000, #000);
 font-size: 12px;
@@ -351,7 +313,6 @@ font-weight: 500;
 line-height: 120%;
 margin:0px;
 `;
-
 const CandidateDescription = styled.p`
 color: var(--primary-828688, #828688);
 font-size: 12px;
@@ -359,7 +320,6 @@ font-family: Avenir;
 line-height: 130%;
 margin: 0px;
 `;
-
 const DeclarationCard = styled.div`
 display: flex;
 padding: 16px 12px;
@@ -369,7 +329,6 @@ align-self: stretch;
 border-radius: 6px;
 background: #FFF;
 `;
-
 const DeclarationContent = styled.div`
 display: flex;
 width: 302px;
@@ -378,14 +337,12 @@ justify-content: center;
 align-items: flex-start;
 gap: 4px;
 `;
-
 const DeclarationHeader = styled.div`
 display: flex;
 align-items: center;
 gap: 12px;
 align-self: stretch;
 `;
-
 const DeclarationHeaderText = styled.p`
 color: #000;
 font-size: 12px;
@@ -393,7 +350,6 @@ font-family: Avenir;
 font-weight: 800;
 line-height: 120%;
 `;
-
 const DeclarationInfo = styled.div`
 display: flex;
 width: 290px;
@@ -404,19 +360,16 @@ gap: 16px;
 border-radius: 8px;
 background: #FFF;
 `;
-
 const DeclarationDescription = styled.p`
 color: var(--primary-828688, #828688);
 font-size: 12px;
 font-family: Avenir;
 line-height: 130%;
 `;
-
 const DeclarationImage = styled.img`
 width: 290px;
 height: 234px;
 `;
-
 const CommentSection = styled.div`
 display: flex;
 padding: 16px;
@@ -426,7 +379,6 @@ gap: 12px;
 border-radius: 10px;
 background: #F8F8F9;
 `;
-
 const CommentHeader = styled.div`
 display: flex;
 width: 326px;
@@ -434,7 +386,6 @@ align-items: center;
 justify-content: space-between;
 gap: 20px;
 `;
-
 const CommentHeaderText = styled.p`
 color: var(--000000, #000);
 font-size: 16px;
@@ -443,7 +394,6 @@ font-weight: 500;
 line-height: 120%;
 margin: 0px;
 `;
-
 const SortButtonDiv = styled.button`
 display: flex;
 width: 20px;
@@ -456,13 +406,11 @@ flex-shrink: 0;
 border-radius: 4px;
 background: var(--buttons-gradient-default, linear-gradient(90deg, #9333EA 0%, #4F46E5 100%));
 `;
-
 const SortIcon = styled.img`
 width: 14px;
 height: 14px;
 flex-shrink: 0;
 `;
-
 const CommentButton = styled.div`
 cursor: pointer;
 display: flex;
@@ -473,7 +421,6 @@ align-self: stretch;
 border-radius: 4px;
 background: var(--buttons-yellow-default, #FFD50D);
 `;
-
 const CommentText = styled.p`
 color: var(--primary-black, #000);
 font-size: 12px;
@@ -482,30 +429,63 @@ font-weight: 500;
 line-height: 24px;
 margin: 0px;
 `;
-
+let profileInfo = Social.getr(`${context.accountId}/profile`);
+console.log("profileInfo", profileInfo);
+let imageIsNFT = profileInfo.image.nft ? true : false;
+let nftData = profileInfo.image.nft;
+const getNftCid = Near.view(nftData.contractId, "nft_token", {
+  token_id: nftData.tokenId,
+});
+const CandidateProps = props.data.nominations;
+const comments = props.data.comments[0].comments;
+console.log("getNftCid", getNftCid);
+let isNFTCid = CandidateProps.img.cid
+  ? "https://nativonft.mypinata.cloud/ipfs/" + CandidateProps.img.cid
+  : "";
+console.log("CandidateComment", CandidateComment);
+console.log("CandidateProps", CandidateProps);
 return (
   <DetailContent>
     <DetailCard>
       <DetailHeader>
         <ProfilePicture
-          src="https://apricot-straight-eagle-592.mypinata.cloud/ipfs/QmZBPPMKLdZG2zVpYaf9rcbtNfAp7c3BtsvzxzBb9pNihm?_gl=1*6avmrp*rs_ga*MzkyOTE0Mjc4LjE2ODY4NjgxODc.*rs_ga_5RMPXG14TE*MTY4NjkzMzM2NC4zLjEuMTY4NjkzMzM4Ni4zOC4wLjA."
+          src={
+            isNFTCid
+              ? isNFTCid
+              : "https://apricot-straight-eagle-592.mypinata.cloud/ipfs/QmZBPPMKLdZG2zVpYaf9rcbtNfAp7c3BtsvzxzBb9pNihm?_gl=1*6avmrp*rs_ga*MzkyOTE0Mjc4LjE2ODY4NjgxODc.*rs_ga_5RMPXG14TE*MTY4NjkzMzM2NC4zLjEuMTY4NjkzMzM4Ni4zOC4wLjA."
+          }
           alt="pic"
         ></ProfilePicture>
         <HeaderDetailContent>
           <TagContainer>
             <HouseTagDiv>
-              <HouseTagText>House intended</HouseTagText>
+              <HouseTagText style={{ "font-size": "8px" }}>
+                {props.house == "HouseOfMerit"
+                  ? "House of Merit"
+                  : props.house == "CouncilOfAdvisors"
+                  ? "Council of Advisors"
+                  : "Transparency Commission"}
+              </HouseTagText>
             </HouseTagDiv>
-            <TagDiv>
-              <TagDivText>#amazing</TagDivText>
-            </TagDiv>
-            <TagDiv>
-              <TagDivText>#thank you</TagDivText>
-            </TagDiv>
+            {CandidateProps.tags.split(",").map((tag, index) => {
+              return tag ? (
+                <TagDiv key={index}>
+                  <TagDivText>{tag}</TagDivText>
+                </TagDiv>
+              ) : (
+                <></>
+              );
+            })}
           </TagContainer>
           <NominationTitleContainer>
-            <NominationTitle>Candidate Name</NominationTitle>
-            <NominationUser>candidate.near</NominationUser>
+            <NominationTitle>
+              {CandidateProps.name ? CandidateProps.name : "candidate name"}
+            </NominationTitle>
+            <NominationUser>
+              {CandidateProps.profileAccount
+                ? CandidateProps.profileAccount
+                : "@candidate.near"}
+            </NominationUser>
           </NominationTitleContainer>
         </HeaderDetailContent>
         <UpvoteButton>
@@ -529,27 +509,27 @@ return (
               <PlatInfoHeadSeparator></PlatInfoHeadSeparator>
             </PlatformInfoHeader>
             <KeyIssuesContainer>
-              <KeyIssueTitle>Restoring Community Funding</KeyIssueTitle>
+              <KeyIssueTitle>Key Issue 1</KeyIssueTitle>
               <KeyIssueDescription>
-                Candidate's Position Description
+                {CandidateProps.issued1}
               </KeyIssueDescription>
             </KeyIssuesContainer>
             <KeyIssuesContainer>
               <KeyIssueTitle>Key Issue 2</KeyIssueTitle>
               <KeyIssueDescription>
-                Candidate's Position Description
+                {CandidateProps.issued2}
               </KeyIssueDescription>
             </KeyIssuesContainer>
             <KeyIssuesContainer>
               <KeyIssueTitle>Key Issue 3</KeyIssueTitle>
               <KeyIssueDescription>
-                Candidate's Position Description
+                {CandidateProps.issued3}
               </KeyIssueDescription>
             </KeyIssuesContainer>
             <KeyIssuesContainer>
-              <KeyIssueTitle>Other Platform</KeyIssueTitle>
+              <KeyIssueTitle>Addition_platform</KeyIssueTitle>
               <KeyIssueDescription>
-                Candidate's Position Description
+                {CandidateProps.addition_platform}
               </KeyIssueDescription>
             </KeyIssuesContainer>
           </PlatformInfoDiv>
@@ -560,69 +540,38 @@ return (
           <CandidateHeader>
             <CandidateHeaderText>Candidate Affiliations</CandidateHeaderText>
           </CandidateHeader>
-          <CandidateInfoDiv>
-            <CandidateInfoHeader>
-              <CandidateImage
-                src="https://apricot-straight-eagle-592.mypinata.cloud/ipfs/QmZBPPMKLdZG2zVpYaf9rcbtNfAp7c3BtsvzxzBb9pNihm?_gl=1*6avmrp*rs_ga*MzkyOTE0Mjc4LjE2ODY4NjgxODc.*rs_ga_5RMPXG14TE*MTY4NjkzMzM2NC4zLjEuMTY4NjkzMzM4Ni4zOC4wLjA."
-                alt="pic"
-              ></CandidateImage>
-              <CandidateInfoData>
-                <CandidateTagDiv>
-                  <CandidateTagText>Near Foundation</CandidateTagText>
-                </CandidateTagDiv>
-                <CandidateTime>Oct 2018 - Present</CandidateTime>
-              </CandidateInfoData>
-            </CandidateInfoHeader>
-            <CandidateTextInfo>
-              <CandidateTitle>Lorem Ipsum Dolor Sit</CandidateTitle>
-              <CandidateDescription>
-                Lorem ipsum dolor sit amet consectetur. Turpis maecenas pulvinar
-                quis massa tristique velit.
-              </CandidateDescription>
-            </CandidateTextInfo>
-          </CandidateInfoDiv>
-          <CandidateInfoDiv>
-            <CandidateInfoHeader>
-              <CandidateImage
-                src="https://apricot-straight-eagle-592.mypinata.cloud/ipfs/QmZBPPMKLdZG2zVpYaf9rcbtNfAp7c3BtsvzxzBb9pNihm?_gl=1*6avmrp*rs_ga*MzkyOTE0Mjc4LjE2ODY4NjgxODc.*rs_ga_5RMPXG14TE*MTY4NjkzMzM2NC4zLjEuMTY4NjkzMzM4Ni4zOC4wLjA."
-                alt="pic"
-              ></CandidateImage>
-              <CandidateInfoData>
-                <CandidateTagDiv>
-                  <CandidateTagText>Near Foundation</CandidateTagText>
-                </CandidateTagDiv>
-                <CandidateTime>Oct 2018 - Present</CandidateTime>
-              </CandidateInfoData>
-            </CandidateInfoHeader>
-            <CandidateTextInfo>
-              <CandidateTitle>Lorem Ipsum Dolor Sit</CandidateTitle>
-              <CandidateDescription>
-                Lorem ipsum dolor sit amet consectetur. Turpis maecenas pulvinar
-                quis massa tristique velit.
-              </CandidateDescription>
-            </CandidateTextInfo>
-          </CandidateInfoDiv>
-          <CandidateInfoDiv>
-            <CandidateInfoHeader>
-              <CandidateImage
-                src="https://apricot-straight-eagle-592.mypinata.cloud/ipfs/QmZBPPMKLdZG2zVpYaf9rcbtNfAp7c3BtsvzxzBb9pNihm?_gl=1*6avmrp*rs_ga*MzkyOTE0Mjc4LjE2ODY4NjgxODc.*rs_ga_5RMPXG14TE*MTY4NjkzMzM2NC4zLjEuMTY4NjkzMzM4Ni4zOC4wLjA."
-                alt="pic"
-              ></CandidateImage>
-              <CandidateInfoData>
-                <CandidateTagDiv>
-                  <CandidateTagText>Near Foundation</CandidateTagText>
-                </CandidateTagDiv>
-                <CandidateTime>Oct 2018 - Present</CandidateTime>
-              </CandidateInfoData>
-            </CandidateInfoHeader>
-            <CandidateTextInfo>
-              <CandidateTitle>Lorem Ipsum Dolor Sit</CandidateTitle>
-              <CandidateDescription>
-                Lorem ipsum dolor sit amet consectetur. Turpis maecenas pulvinar
-                quis massa tristique velit.
-              </CandidateDescription>
-            </CandidateTextInfo>
-          </CandidateInfoDiv>
+          {JSON.parse(CandidateProps.afiliation).map((affiliation) => {
+            return (
+              <CandidateInfoDiv>
+                <CandidateInfoHeader>
+                  <CandidateImage
+                    src={
+                      isNFTCid
+                        ? isNFTCid
+                        : "https://apricot-straight-eagle-592.mypinata.cloud/ipfs/QmZBPPMKLdZG2zVpYaf9rcbtNfAp7c3BtsvzxzBb9pNihm?_gl=1*6avmrp*rs_ga*MzkyOTE0Mjc4LjE2ODY4NjgxODc.*rs_ga_5RMPXG14TE*MTY4NjkzMzM2NC4zLjEuMTY4NjkzMzM4Ni4zOC4wLjA."
+                    }
+                    alt="pic"
+                  ></CandidateImage>
+                  <CandidateInfoData>
+                    <CandidateTagDiv>
+                      <CandidateTagText>
+                        {affiliation.company_name}
+                      </CandidateTagText>
+                    </CandidateTagDiv>
+                    <CandidateTime>
+                      {affiliation.start_date} - {affiliation.end_date}
+                    </CandidateTime>
+                  </CandidateInfoData>
+                </CandidateInfoHeader>
+                <CandidateTextInfo>
+                  <CandidateTitle>Lorem Ipsum Dolor Sit</CandidateTitle>
+                  <CandidateDescription>
+                    {affiliation.role}
+                  </CandidateDescription>
+                </CandidateTextInfo>
+              </CandidateInfoDiv>
+            );
+          })}
         </CandidateContent>
       </CandidateCard>
       <DeclarationCard>
@@ -652,6 +601,7 @@ return (
           src={`dokxo.near/widget/CommentCard`}
           props={{
             candidateOrReplay: true,
+            username: props.candidate,
             onClickConfirm: () => State.update({ showModal: false }),
             onClickCancel: () => State.update({ showModal: false }),
           }}
@@ -668,12 +618,14 @@ return (
           <CommentText>Add a Comment +</CommentText>
         </CommentButton>
       </CommentHeader>
-      <Widget src={"syi216.near/widget/NDC.Nomination.CommentCard"} />
-      <Widget src={"syi216.near/widget/NDC.Nomination.CommentCard"} />
-      <Widget src={"syi216.near/widget/NDC.Nomination.CommentCard"} />
-      <Widget src={"syi216.near/widget/NDC.Nomination.CommentCard"} />
-      <Widget src={"syi216.near/widget/NDC.Nomination.CommentCard"} />
-      <Widget src={"syi216.near/widget/NDC.Nomination.CommentCard"} />
+      {comments.map((data) => {
+        return (
+          <Widget
+            props={{ data }}
+            src={"syi216.near/widget/NDC.Nomination.CommentCard"}
+          />
+        );
+      })}
     </CommentSection>
   </DetailContent>
 );
