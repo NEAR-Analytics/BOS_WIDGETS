09e37c0b15aef13bf55b35afd42c024b5697399a
yairnava.near <yairnava.near>
2023-07-05 23:36:59 +0000

    Update NDC.Nomination.Candidate.Desktop by yairnava.near at 2023-07-05T23:36:59.489Z

--- yairnava.near/NDC.Nomination.Candidate.Desktop/source_code.jsx
+++ yairnava.near/NDC.Nomination.Candidate.Desktop/source_code.jsx
@@ -653,20 +653,18 @@ return (
               {state.verified ? (
                 <UpvoteButton onClick={handleUpVote}>
                   <UpvoteCount>
-                    +
-                    {props.data.comments[0].upvotes
+                    {"+" + props.data.comments[0].upvotes
                       ? props.data.comments[0].upvotes
-                      : 0}
+                      : +0}
                   </UpvoteCount>
                   <Icon src="https://apricot-straight-eagle-592.mypinata.cloud/ipfs/QmXqGSZvrgGkVviBJirnBtT9krTHHsjPYX1UM8EWExFxCM?_gl=1*1hd2izc*rs_ga*MzkyOTE0Mjc4LjE2ODY4NjgxODc.*rs_ga_5RMPXG14TE*MTY4NjkzOTYyNC40LjAuMTY4NjkzOTYyNC42MC4wLjA."></Icon>
                 </UpvoteButton>
               ) : (
                 <UpvoteButtonDisabled>
                   <UpvoteCount style={{ filter: "grayscale(1)" }}>
-                    +
-                    {props.data.comments[0].upvotes
+                    {"+" + props.data.comments[0].upvotes
                       ? props.data.comments[0].upvotes
-                      : 0}
+                      : "'+'" + 0}
                   </UpvoteCount>
                   <Icon
                     style={{ filter: "grayscale(1)" }}
