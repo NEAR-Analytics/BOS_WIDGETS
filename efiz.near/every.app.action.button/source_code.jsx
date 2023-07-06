24e8289873dd80812b52ec3b4e988d25407dd606
efiz.near <efiz.near>
2023-07-06 14:44:50 +0000

    Update every.app.action.button by efiz.near at 2023-07-06T14:44:50.119Z

--- efiz.near/every.app.action.button/source_code.jsx
+++ efiz.near/every.app.action.button/source_code.jsx
@@ -1,41 +1,14 @@
 const StyledCreateWidget = styled.div`
-  // margin: 0 15px;
-  // background-color: var(--slate-dark-5);
   height: 60px;
   width: 60px;
   border-radius: 50%;
   font-weight: var(--font-weight-medium);
   text-transform: lowercase !important;
-  // display: inline-block;
   text-align: center;
   text-decoration: none;
   border: 2px outset #333;
   background-color: #f5f5f5;
   cursor: pointer;
-
-  > div,
-  a {
-    width: 100%;
-    height: 100%;
-  }
-
-  a {
-    color: black !important;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    i {
-      font-size: 18px !important;
-    }
-  }
-
-  :hover {
-    a,
-    i {
-      color: black;
-    }
-  }
 `;
 
 return (
