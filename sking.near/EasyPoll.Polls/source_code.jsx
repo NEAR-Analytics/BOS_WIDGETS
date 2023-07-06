037fde2ffead0e25db432b6ff67c1215618c1af5
sking.near <sking.near>
2023-07-06 14:11:18 +0000

    Update EasyPoll.Polls by sking.near at 2023-07-06T14:11:18.176Z

--- sking.near/EasyPoll.Polls/source_code.jsx
+++ sking.near/EasyPoll.Polls/source_code.jsx
@@ -32,11 +32,11 @@ const shouldDisplayUserQuestions = (accountId) => {
   return true;
 };
 
-let keys = `*/easypoll-${indexVersion}/*`;
+let keys = `*/easypoll-${indexVersion}/poll/*`;
 
 if (filterByUser) {
   keys = filterByUser.map((v) => {
-    return `${v}/easypoll-${indexVersion}/*`;
+    return `${v}/easypoll-${indexVersion}/poll/*`;
   });
 }
 
@@ -48,27 +48,28 @@ if (!results) {
 }
 
 const polls_keys = [];
+
+// TODO: should cache the logic bellow in state, polls_keys can be huge
 Object.keys(results).forEach((accountId) => {
-  return Object.keys(results[accountId][`easypoll-${indexVersion}`]).forEach(
-    (pollId) => {
-      polls_keys.push({
-        accountId,
-        pollId,
-        blockHeight: results[accountId][`easypoll-${indexVersion}`][pollId],
-      });
-    }
-  );
+  return Object.keys(
+    results[accountId][`easypoll-${indexVersion}`]["poll"]
+  ).forEach((pollId) => {
+    polls_keys.push({
+      accountId,
+      pollId,
+      blockHeight:
+        results[accountId][`easypoll-${indexVersion}`]["poll"][pollId],
+    });
+  });
 });
 polls_keys = polls_keys.sort((a, b) => b.blockHeight - a.blockHeight); // desc
 polls_keys = polls_keys.filter((p) => shouldDisplayUserQuestions(p.accountId));
-
-console.log(polls_keys);
 
 return (
   <>
     <div className="d-flex flex-column gap-4 mb-3">
       {polls_keys.map((p) => {
-        const src = `${p.accountId}/easypoll-${indexVersion}/${p.pollId}`;
+        const src = `${p.accountId}/easypoll-${indexVersion}/poll/${p.pollId}`;
         return (
           <Widget
             src={`${widgetOwner}/widget/EasyPoll.PollCard`}
@@ -78,6 +79,7 @@ return (
               href: tabs.VIEW_POLL.href(src, p.blockHeight),
               editHref: tabs.EDIT_POLL.href(src, p.blockHeight),
               deleteHref: tabs.DELETE_POLL.href(src, p.blockHeight),
+              indexVersion,
             }}
           />
         );
