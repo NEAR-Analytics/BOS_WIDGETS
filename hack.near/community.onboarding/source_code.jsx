7c1ca3b5dd78887ffb7e30a85ad1b94017e7d6bf
hack.near <hack.near>
2023-07-06 00:05:25 +0000

    Update community.onboarding by hack.near at 2023-07-06T00:05:25.262Z

--- hack.near/community.onboarding/source_code.jsx
+++ hack.near/community.onboarding/source_code.jsx
@@ -1,4 +1,5 @@
 const accountId = context.accountId;
+const memberId = props.memberId ?? context.accountId;
 const daoId = props.daoId ?? "rc-dao.sputnik-dao.near";
 const role = props.role ?? "voter";
 
@@ -48,62 +49,38 @@ const handleJoin = () => {
   ]);
 };
 
-const Container = styled.div`
-  display: flex;
-  max-width: 888px;
-  margin: 0 auto;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  padding: 8px;
-
-  @media (max-width: 480px) {
-    max-width: 333px;
-  }
-`;
-
 return (
-  <Container>
-    <div>
-      {!accountId && (
-        <Widget
-          src="near/widget/DIG.Button"
-          props={{
-            href: "https://near.org/signup",
-            label: "Create Account",
-            variant: "outline-dark",
-            size: "large",
-          }}
-        />
-      )}
+  <div className="m-2">
+    {!accountId && (
+      <Widget
+        src="near/widget/DIG.Button"
+        props={{
+          href: "https://near.org/signup",
+          label: "Create Account",
+          variant: "outline-dark",
+          size: "large",
+        }}
+      />
+    )}
 
-      {(human && (
-        <Widget
-          src="hack.near/widget/community.join"
-          props={{
-            memberId: accountId,
-            roleId,
-          }}
-        />
-      )) || (
-        <div className="row">
-          <div className="col-7 mt-5">
-            <Widget
-              src="near/widget/DIG.Button"
-              props={{
-                href: "https://i-am-human.app/?community=banyan&vertical=regionalcommunities",
-                label: "Get Verified",
-                variant: "outline-primary",
-                size: "large",
-              }}
-            />
-          </div>
-          <div className="col-5">
-            <Widget src="hack.near/widget/gov.Badge" />
-          </div>
-        </div>
-      )}
-    </div>
-  </Container>
+    {human ? (
+      <Widget
+        src="hack.near/widget/community.join"
+        props={{
+          memberId: accountId,
+          roleId,
+        }}
+      />
+    ) : (
+      <Widget
+        src="near/widget/DIG.Button"
+        props={{
+          href: "https://i-am-human.app/?community=banyan&vertical=regionalcommunities",
+          label: "Get Verified",
+          variant: "outline-primary",
+          size: "large",
+        }}
+      />
+    )}
+  </div>
 );
