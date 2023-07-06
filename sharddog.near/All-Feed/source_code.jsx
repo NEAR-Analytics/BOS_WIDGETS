959c063a4e4eb894ac5f0f38ab9a10869b91de8e
sharddog.near <sharddog.near>
2023-07-06 04:50:23 +0000

    Update All-Feed by sharddog.near at 2023-07-06T04:50:23.132Z

---
+++ sharddog.near/All-Feed/source_code.jsx
@@ -0,0 +1,25 @@
+return (
+  <>
+    <div className="row">
+      <div
+        className="tab-content col-sm-12 offset-sm-12 border-left top-0"
+        id="pills-tabContent"
+      >
+        <div
+          className="tab-pane  d-lg-block "
+          id="pills-graph"
+          role="tabpanel"
+          aria-labelledby="pills-graph-tab"
+        ></div>
+        <div
+          className="tab-pane show active d-lg-block "
+          id="pills-posts"
+          role="tabpanel"
+          aria-labelledby="pills-posts-tab"
+        >
+          <Widget src="sharddog.near/widget/AllFeed.Main.Content" />
+        </div>
+      </div>
+    </div>
+  </>
+);
