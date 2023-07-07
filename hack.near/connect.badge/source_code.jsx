6cf9d67ea31a9ebeb9774b6b38de55956eb934ba
hack.near <hack.near>
2023-07-07 09:18:12 +0000

    Update connect.badge by hack.near at 2023-07-07T09:18:12.051Z

--- hack.near/connect.badge/source_code.jsx
+++ hack.near/connect.badge/source_code.jsx
@@ -1,17 +1,39 @@
-if (!props.accountId || !context.accountId) {
-  return "";
-}
-
 const o = Social.keys(
-  `${props.accountId}/graph/connect/${context.accountId}`,
+  `build.sputnik-dao.near/graph/connect/${context.accountId}`,
   undefined,
   {
     values_only: true,
   }
 );
 
-return o && Object.keys(o).length ? (
-  <span className="badge bg-secondary fw-light">✅</span>
-) : (
-  ""
+const Badge = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 0.5em 1em;
+
+  .image {
+    display: block;
+    height: 8em;
+    margin: 0.5em;
+  }
+}`;
+
+return (
+  <Badge>
+    <Widget
+      src="mob.near/widget/Image"
+      props={{
+        image: {
+          ipfs_cid: "QmcxtybPoeXG1ypbDQDNe4U2c4cNFW4KeuQ5q42gZwnceV",
+        },
+        alt: "connected",
+        fallbackUrl:
+          "https://ipfs.near.social/ipfs/bafkreibiyqabm3kl24gcb2oegb7pmwdi6wwrpui62iwb44l7uomnn3lhbi",
+        className: "image",
+        thumbnail,
+      }}
+    />
+  </Badge>
 );
