cc3361f4769a94f98275d1b1d58dde90964bdeb5
sking.near <sking.near>
2023-07-07 14:17:23 +0000

    Update EasyPoll.ViewPoll by sking.near at 2023-07-07T14:17:23.625Z

--- sking.near/EasyPoll.ViewPoll/source_code.jsx
+++ sking.near/EasyPoll.ViewPoll/source_code.jsx
@@ -20,52 +20,10 @@ if (!poll) {
 poll = JSON.parse(poll);
 poll.accountId = src.split("/")[0];
 
-let profile = Social.getr(`${poll.accountId}/profile`);
-
 let userAnswers = Social.index(`easypoll-${indexVersion}-answer`, `${src}`, {
   accountId: accountId,
 });
-if (!userAnswers) return "Loading...";
-
-let allAnswers = Social.index(`easypoll-${indexVersion}-answer`, `${src}`);
-if (!allAnswers) return "Loading...";
-
-const isVerifiedHuman = (account) => {
-  const view = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
-    account: `${account}`,
-    issuer: "fractal.i-am-human.near",
-  });
-  return view?.[0]?.[1]?.[0];
-};
-const getValidAnswersOnly = (input) => {
-  const { verifiedHumansOnly, endTimestamp, startTimestamp } = poll;
-
-  // should be only right poll
-  let filtered = input
-    // should be 1 per user
-    .map((e) => e["accountId"])
-    .map((e, i, final) => final.indexOf(e) === i && i)
-    .filter((e) => input[e])
-    .map((e) => input[e])
-    //
-    .filter(async (v, i) => {
-      // should respect human only
-      if (verifiedHumansOnly && !isVerifiedHuman(v.accountId)) return false;
-      // should respect startTimestamp
-      if (v.value.timestamp < startTimestamp) return false;
-      // should respect endTimestamp
-      if (v.value.timestamp > endTimestamp) return false;
-
-      return true;
-    });
-
-  return filtered;
-};
-
-if (!state) {
-  const filteredAnswers = getValidAnswersOnly(allAnswers);
-  State.init({ filteredAnswers });
-}
+if (!userAnswers) return "";
 
 return (
   <>
@@ -80,9 +38,7 @@ return (
         blockHeight: blockHeight,
         isHuman,
         poll: poll,
-        profile,
         userAnswers: userAnswers,
-        pollAnswers: state.filteredAnswers,
         indexVersion,
         src,
       }}
