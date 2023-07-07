720c96f0b14f0bd102fd7666fb6c816ad1558c33
sharddog.near <sharddog.near>
2023-07-07 21:01:57 +0000

    Update Posts.Compose by sharddog.near at 2023-07-07T21:01:57.969Z

--- sharddog.near/Posts.Compose/source_code.jsx
+++ sharddog.near/Posts.Compose/source_code.jsx
@@ -492,6 +492,7 @@ return (
                             onClick={() =>
                               State.update({
                                 text: state.text + "\n" + gif.imageUrl,
+                                searchTerm: "",
                               })
                             }
                           >
