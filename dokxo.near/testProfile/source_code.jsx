810c85927b343c6b8cdba2b33bb08b69e723d90d
dokxo.near <dokxo.near>
2023-07-07 22:47:54 +0000

    Update testProfile by dokxo.near at 2023-07-07T22:47:54.797Z

--- dokxo.near/testProfile/source_code.jsx
+++ dokxo.near/testProfile/source_code.jsx
@@ -129,7 +129,7 @@ color: #828688;
   `;
 const Photopickersection = styled.div`
   padding: 10px;
-  width: 83.33333%; 
+  width:100%; 
   margin-left:10px;
 @media only screen and (max-width: 480px) {
   
