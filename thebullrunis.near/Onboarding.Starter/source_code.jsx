b5e6079c5f920266dc4647bff6930e388cc50e81
thebullrunis.near <thebullrunis.near>
2023-07-08 22:24:03 +0000

    Update Onboarding.Starter by thebullrunis.near at 2023-07-08T22:24:03.020Z

--- thebullrunis.near/Onboarding.Starter/source_code.jsx
+++ thebullrunis.near/Onboarding.Starter/source_code.jsx
@@ -1,35 +1,11 @@
-const contract = "hello.near-examples.near";
-const greeting = Near.view(contract, "get_greeting", {});
+let greeting = "Have a great day";
 
-// Define components
-const greetingForm = (
-  <>
-    <div class="border border-black p-3">
-      <label>Update greeting</label>
-      <input placeholder="Howdy" onChange={onInputChange} />
-      <button class="btn btn-primary mt-2" onClick={onBtnClick}>
-        Save
-      </button>
-    </div>
-  </>
-);
-
-const notLoggedInWarning = <p> Login to change the greeting </p>;
-
-// Render
 return (
   <>
-    <div class="container border border-info p-3">
-      <h3 class="text-center">
-        The contract says:
-        <span class="text-decoration-underline"> {greeting} </span>
-      </h3>
+    <div class="container border border-info p-3 text-center">
+      <h1>Hello {props.name}</h1>
 
-      <p class="text-center py-2">
-        Look at that! A greeting stored on the NEAR blockchain.
-      </p>
-
-      {context.accountId ? greetingForm : notLoggedInWarning}
+      <p> {greeting} </p>
     </div>
   </>
 );
