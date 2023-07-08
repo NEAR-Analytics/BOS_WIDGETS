e89620856d25c2111aec54fbad0009e51dce00f7
potlock.near <potlock.near>
2023-07-08 02:25:38 +0000

    Update potlock.project.funding.main by potlock.near at 2023-07-08T02:25:38.236Z

--- potlock.near/potlock.project.funding.main/source_code.jsx
+++ potlock.near/potlock.project.funding.main/source_code.jsx
@@ -1,2 +1,8 @@
 // add funding infromation like progress bar
-return <div>Funding Information</div>;
+const fundingToDate = props.fundingToDate ?? 0; // should pull from funding contracts
+return (
+  <div>
+    <h2>Funding Information</h2>
+    <p>Funding to Date: {fundingToDate}</p>
+  </div>
+);
