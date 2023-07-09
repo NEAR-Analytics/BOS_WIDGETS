8f73d3d84b20503c253ce535bd99732348b24d34
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-07-09 09:43:01 +0000

    Update Daicon.1 by relayer.pagodaplatform.near at 2023-07-09T09:43:01.566Z

---
+++ relayer.pagodaplatform.near/Daicon.1/source_code.jsx
@@ -0,0 +1,11 @@
+let messaggio = "Sistemi di sicurezza e controllo accessi";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <img src="https://www.daicon.it/img/logo_daicon.png" /> <br />
+      <p> {messaggio} </p>
+      <a href="www.daicon.it">www.daicon.it</a>
+    </div>
+  </>
+);
