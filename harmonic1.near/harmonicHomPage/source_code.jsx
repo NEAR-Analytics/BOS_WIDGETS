a3fb6e4449c65691e9af4be7253acc682332e7c8
harmonic1.near <harmonic1.near>
2023-07-09 11:09:40 +0000

    Update harmonicHomPage by harmonic1.near at 2023-07-09T11:09:40.167Z

--- harmonic1.near/harmonicHomPage/source_code.jsx
+++ harmonic1.near/harmonicHomPage/source_code.jsx
@@ -5,7 +5,7 @@ const containerStyles = {
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
-  height: "90vh", // Full viewport height
+  height: "100vh", // Full viewport height
   backgroundColor: "#fff", // White background
 };
 
@@ -42,19 +42,46 @@ return (
         <span style={blueTextStyles}>Harmonic Guild</span>
         <span>Gateway</span>
       </h1>
-      <p style={paragraphStyles}>
-        Check our latest Apps below and Stay Tuned for more.
-      </p>
+      <p style={paragraphStyles}>Check our latest fully onchain Apps below.</p>
     </div>
-    <div className="mb-5">
-      <Widget src="harmonic1.near/widget/MusicPlayer-Harmonic" />
-    </div>
-    <div className="mb-5">
-      <Widget src="harmonic1.near/widget/NFTMinter" />
-      <div className="my-3">
-        <Widget src="harmonic1.near/widget/NFTViewer" />
+    <div style={containerStyles}>
+      <div className="mb-5" style={{ display: "flex" }}>
+        <div style={{ flex: 1 }}>
+          <Widget src="harmonic1.near/widget/MusicPlayer-Harmonic" />
+        </div>
+        <div style={{ flex: 1, paddingLeft: "4rem", padding: "2rem" }}>
+          <p style={{ fontSize: "2rem", fontWeight: "bold" }}>
+            Fair pay for free music
+          </p>
+          <p style={{ fontSize: "1.5rem" }}>
+            Experience a truly decentralized music player fully onchain. Our
+            mission is to bring transparency to streaming and revolutionize
+            payments for musicians. With Harmonic Guild, we are returning
+            ownership to music, ensuring that artists are fairly compensated for
+            their creations.
+          </p>
+        </div>
       </div>
-
+    </div>
+    <div style={containerStyles}>
+      <div className="mb-5" style={{ display: "flex" }}>
+        <div style={{ flex: 1, paddingLeft: "4rem", padding: "2rem" }}>
+          <p style={{ fontSize: "2rem", fontWeight: "bold" }}>
+            Introducing Minter
+          </p>
+          <p style={{ fontSize: "1.5rem" }}>
+            Our minting sets the stage for future advancements, including the
+            exploration of encryption. As we continue to innovate, we are
+            actively working on integrating encryption technology to enhance the
+            security and privacy of your music. Stay tuned for upcoming updates
+            as we strive to bring cutting-edge encryption features to our
+            platform. Together, let's unlock the potential of encrypted music.
+          </p>
+        </div>
+        <div style={{ flex: 1 }}>
+          <Widget src="harmonic1.near/widget/NFTMinter" />
+        </div>
+      </div>
       <p className="mt-5" style={paragraphStyles2}>
         Contact us at hello@harmonicguild.io to get a trial account to use our
         Minter for free. No passphrase, no crypto needed.
