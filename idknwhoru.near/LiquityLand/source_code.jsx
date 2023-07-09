6fdf88b0d4e70a98b4ef14c3be52c3ce12f84cf7
idknwhoru.near <idknwhoru.near>
2023-07-09 05:20:07 +0000

    Update LiquityLand by idknwhoru.near at 2023-07-09T05:20:07.674Z

---
+++ idknwhoru.near/LiquityLand/source_code.jsx
@@ -0,0 +1,127 @@
+return (
+  <div class="Page_wrapper__1NXU_">
+    <div class="Tabs_tabs__2vVZO Page_tabs__1SuiH Tabs_threeTabs___2Rbb">
+      <div class="Tabs_tab__3AQLJ Tabs_tabActive__1Bljk">Deposit</div>
+      <div class="Tabs_tab__3AQLJ">Withdraw</div>
+      <div class="Tabs_tab__3AQLJ">Redemption</div>
+    </div>
+    <div class="Row_row__3aDaO">
+      <div class="Row_labelContainer__2XL8x">
+        <div>deposit</div>
+      </div>
+      <div class="StaticAmounts_staticAmounts__3vBy6 Page_staticAmount__2jGXR">
+        <div class="StaticAmounts_staticAmountsContent__3vSFa">
+          <p>0.000</p>&nbsp;<p>ETH</p>
+        </div>
+        <div class="ContentRight_contentRight__1NZpw ContentRight_contentRightNoInfo__1LyOe">
+          <div class="ContentRight_contentRightBottom__3tpIt">
+            <div class="ContentRight_contentRightValues__2sLJQ">
+              <img
+                src="/icons/ethereum-eth.svg"
+                alt="ETH"
+                class="ContentRight_contentRightIcon__2u_6r"
+              />
+              <p class="ContentRight_contentRightCurrency__2dVWW">ETH</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="Row_row__3aDaO">
+      <div class="Row_labelContainer__2XL8x">
+        <div>borrow</div>
+      </div>
+      <div class="StaticAmounts_staticAmounts__3vBy6 Page_staticAmount__2jGXR">
+        <div class="StaticAmounts_staticAmountsContent__3vSFa">
+          <p>0.000</p>&nbsp;<p>LUSD</p>
+        </div>
+        <div class="ContentRight_contentRight__1NZpw ContentRight_contentRightNoInfo__1LyOe">
+          <div class="ContentRight_contentRightBottom__3tpIt">
+            <div class="ContentRight_contentRightValues__2sLJQ">
+              <img
+                src="/icons/128-lusd-icon.svg"
+                alt="LUSD"
+                class="ContentRight_contentRightIcon__2u_6r"
+              />
+              <p class="ContentRight_contentRightCurrency__2dVWW">LUSD</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="Page_action__2kP_C">
+      <a
+        class="Button_wrapper__2_V6r Button_large__2ihrF Button_primary__2zal1"
+        href="https://app.liquityland.com/trove"
+      >
+        Connect wallet
+      </a>
+    </div>
+    <div class="StaticRow_staticRow__2UEni">
+      <p class="">Deposit</p>
+      <p class="">
+        {" "}
+        <span class="">0.0000</span>ETH{" "}
+      </p>
+    </div>
+    <div class="StaticRow_staticRow__2UEni">
+      <p class="">Liquidation Reserve</p>
+      <p class="">
+        {" "}
+        <span class="">200.00</span> LUSD{" "}
+      </p>
+    </div>
+    <div class="StaticRow_staticRow__2UEni">
+      <p class="">
+        Borrowing Fee
+        <span
+          style={{ display: "inline-flex", color: "var(--gray)" }}
+          aria-expanded="false"
+        >
+          &nbsp;
+        </span>
+      </p>
+      <p class="">
+        {" "}
+        <span class="">0.00</span> LUSD{" "}
+      </p>
+    </div>
+    <div class="StaticRow_staticRow__2UEni">
+      <p class="">Recieve</p>
+      <p class="">
+        {" "}
+        <span class="">0.00</span> LUSD{" "}
+      </p>
+    </div>
+    <div class="StaticRow_staticRow__2UEni">
+      <p class="">
+        Total Debt
+        <span
+          style={{ display: "inline-flex", color: "var(--gray)" }}
+          aria-expanded="false"
+        >
+          &nbsp;
+        </span>
+      </p>
+      <p class="">
+        {" "}
+        <span class="">0.00</span> LUSD{" "}
+      </p>
+    </div>
+    <div class="StaticRow_staticRow__2UEni">
+      <p class="">
+        Collateral Ratio
+        <span
+          style={{ display: "inline-flex", color: "var(--gray)" }}
+          aria-expanded="false"
+        >
+          &nbsp;
+        </span>
+      </p>
+      <p class="">
+        {" "}
+        <span class="">N/A</span>{" "}
+      </p>
+    </div>
+  </div>
+);
