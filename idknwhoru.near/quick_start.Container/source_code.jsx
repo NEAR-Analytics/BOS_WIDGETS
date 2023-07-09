7df3a84017c7c241608714cb1bf1c7c64b5acb94
idknwhoru.near <idknwhoru.near>
2023-07-09 05:33:36 +0000

    Update quick_start.Container by idknwhoru.near at 2023-07-09T05:33:36.601Z

---
+++ idknwhoru.near/quick_start.Container/source_code.jsx
@@ -0,0 +1,11 @@
+const user = "idknwhoru.near";
+
+return (
+    <>
+        <h3> Container </h3>
+        <p> Components can be composed.</p>
+        <hr />
+
+        <Widget src={`${user}/widget/quick_start.Greetings`} props={props} />
+    </>
+)
