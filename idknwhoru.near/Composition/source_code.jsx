4f80cc90d958a4445c4b04a03780ae302496cfa6
idknwhoru.near <idknwhoru.near>
2023-07-09 08:10:01 +0000

    Update Composition by idknwhoru.near at 2023-07-09T08:10:01.034Z

---
+++ idknwhoru.near/Composition/source_code.jsx
@@ -0,0 +1,11 @@
+const user = "idknwhoru.near";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+
+    <Widget src={`${user}/widget/Greeter`} props={props} />
+  </>
+);
