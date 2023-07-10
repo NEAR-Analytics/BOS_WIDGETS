1e27df9b03fa512b502c64a4c282d911a756e8a0
eugenewolf507.near <eugenewolf507.near>
2023-07-10 14:43:16 +0000

    Update Gigs_AllArticlesList by eugenewolf507.near at 2023-07-10T14:43:16.008Z

--- eugenewolf507.near/Gigs_AllArticlesList/source_code.jsx
+++ eugenewolf507.near/Gigs_AllArticlesList/source_code.jsx
@@ -14,6 +14,7 @@ const authorsWhitelist = props.writersWhiteList ?? [
   "eugenewolf507.near",
 ];
 const sharedArticleId = props.articleId;
+console.log("sharedArticleId = ", sharedArticleId);
 const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
 const statusTagsArr = ["open", "claimed", "closed"];
 // ========== GET INDEX ARRAY FOR ARTICLES ==========
