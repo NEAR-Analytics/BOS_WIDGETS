823dcdee52d8a91e2944f5ebcbbc98b8bd0d28ee
hack.near <hack.near>
2023-07-10 12:37:33 +0000

    Update dao.candidate by hack.near at 2023-07-10T12:37:33.451Z

--- hack.near/dao.candidate/source_code.jsx
+++ hack.near/dao.candidate/source_code.jsx
@@ -96,7 +96,7 @@ return (
     <a className="btn flex-fill btn-outline-primary " href={postUrl}>
       Discuss
     </a>
-    {!human ? (
+    {human ? (
       <div className="m-1 d-flex flex-row gap-1">
         {canVote ? (
           <button className="btn flex-fill btn-success" onClick={handleApprove}>
