5a0d1336e675f938edb409756be9fbe775e42b97
yairnava.near <yairnava.near>
2023-07-11 00:16:08 +0000

    Update NDC.Nomination.Candidate.Desktop by yairnava.near at 2023-07-11T00:16:08.176Z

--- yairnava.near/NDC.Nomination.Candidate.Desktop/source_code.jsx
+++ yairnava.near/NDC.Nomination.Candidate.Desktop/source_code.jsx
@@ -112,7 +112,7 @@ background: var(--gradient-purple-gradient, linear-gradient(90deg, #9333EA 0%, #
 const HouseTagText = styled.p`
 color: #FFF;
 font-size: 7px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 500;
 line-height: 120%;
 margin: 0px;
@@ -134,7 +134,7 @@ background-clip: padding-box, border-box;
 
 const TagDivText = styled.p`
 font-size: 8px;
-font-family: Avenir;
+font-family: Open Sans;
 margin: 0px;
 font-weight: 500;
 line-height: 120%;
@@ -158,7 +158,7 @@ justify-content: center;
 margin: 0px;
 color: #000;
 font-size: 14px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 500;
 line-height: 120%;
 `;
@@ -170,7 +170,7 @@ justify-content: center;
 color: #828688;
 margin:0px;
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 line-height: 120%;
 `;
 
@@ -199,7 +199,7 @@ background-clip: padding-box, border-box;
 
 const UpvoteCount = styled.p`
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 500;
 line-height: 24px;
 margin: 0px;
@@ -245,7 +245,7 @@ flex-direction: column;
 flex: 1 0 0;
 color: #000;
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 800;
 line-height: 120%;
 margin: 0px;
@@ -270,7 +270,7 @@ const PlatInforHeadText = styled.p`
 margin: 0px;
 color: var(--000000, #000);
 font-size: 10px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 500;
 line-height: 120%;
 `;
@@ -294,7 +294,7 @@ gap: 2px;
 const KeyIssueTitle = styled.p`
 color: var(--primary-000000, #000);
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 line-height: 120%;
 margin: 0px;
 `;
@@ -302,7 +302,7 @@ margin: 0px;
 const KeyIssueDescription = styled.p`
 color: #828688;
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 line-height: 120%;
 margin: 0px;
 `;
@@ -336,7 +336,7 @@ align-self: stretch;
 const CandidateHeaderText = styled.p`
 color: #000;
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 800;
 line-height: 120%;
 margin: 0px;
@@ -386,7 +386,7 @@ background: #F0E1CE;
 const CandidateTagText = styled.p`
 color: var(--secondary-warning, #F19D38);
 font-size: 10px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 500;
 line-height: 120%;
 margin: 0px;
@@ -396,7 +396,7 @@ const CandidateTime = styled.p`
 margin: 0px;
 color: var(--primary-828688, #828688);
 font-size: 10px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 500;
 line-height: 120%;
 `;
@@ -412,7 +412,7 @@ align-self: stretch;
 const CandidateTitle = styled.p`
 color: var(--000000, #000);
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 500;
 line-height: 120%;
 margin:0px;
@@ -421,7 +421,7 @@ margin:0px;
 const CandidateDescription = styled.p`
 color: var(--primary-828688, #828688);
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 line-height: 130%;
 margin: 0px;
 width: 258px;
@@ -458,7 +458,7 @@ align-self: stretch;
 const DeclarationHeaderText = styled.p`
 color: #000;
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 800;
 line-height: 120%;
 `;
@@ -477,7 +477,7 @@ background: #FFF;
 const DeclarationDescription = styled.p`
 color: black;
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 line-height: 130%;
 text-align: justify;
 `;
@@ -508,13 +508,14 @@ gap: 20px;
 const CommentHeaderText = styled.p`
 color: var(--000000, #000);
 font-size: 16px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 500;
 line-height: 120%;
 margin: 0px;
 `;
 
 const Container = styled.div`
+font-family: Open Sans;
 display: flex;
 justify-content: center;
 @media only screen and (max-width: 600px) {
@@ -568,7 +569,7 @@ cursor: default !important;
 const CommentText = styled.p`
 color: var(--primary-black, #000);
 font-size: 12px;
-font-family: Avenir;
+font-family: Open Sans;
 font-weight: 500;
 line-height: 24px;
 margin: 0px;
