115c849315e40286285be8f6cdd5f2930ec3170b
hack.near <hack.near>
2023-07-11 00:28:53 +0000

    Update widget.compare by hack.near at 2023-07-11T00:28:53.024Z

--- hack.near/widget.compare/source_code.jsx
+++ hack.near/widget.compare/source_code.jsx
@@ -1,10 +1,10 @@
-if (!props.updatedWidget) return "send updatedWidget in props";
+if (!props.updatedWidget) return "";
 
 const newCode = Social.get(`${props.updatedWidget}`);
 
 if (newCode === null) return "Loading...";
 
-if (!props.widgetPath) return "send widgetPath in props";
+if (!props.widgetPath) return "";
 
 const baseCode = Social.get(`${props.widgetPath}`);
 
