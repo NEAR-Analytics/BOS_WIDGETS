8c9efe225da72d591addd89e0001d2254828ed94
hack.near <hack.near>
2023-07-11 00:04:25 +0000

    Update widget.compare by hack.near at 2023-07-11T00:04:25.735Z

---
+++ hack.near/widget.compare/source_code.jsx
@@ -0,0 +1,18 @@
+if (!props.updatedWidget) return "send updatedWidget in props";
+
+const newCode = Social.get(`${props.updatedWidget}`);
+
+if (newCode === null) return "Loading...";
+
+if (!props.widgetPath) return "send updatedWidget in props";
+
+const baseCode = Social.get(`${props.widgetPath}`);
+
+if (baseCode === null) return "Loading...";
+
+return (
+  <Widget
+    src="bozon.near/widget/CodeDiff"
+    props={{ currentCode: newCode, prevCode: baseCode, ...props }}
+  />
+);
