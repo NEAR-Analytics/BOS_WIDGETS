aef11f16df035b40cd5f9f737b7084106ba14c14
efiz.near <efiz.near>
2023-07-11 17:30:15 +0000

    Update Livepeer.Player by efiz.near at 2023-07-11T17:30:15.413Z

--- efiz.near/Livepeer.Player/source_code.jsx
+++ efiz.near/Livepeer.Player/source_code.jsx
@@ -1,1 +1,4 @@
-return <LivepeerPlayer assetId={"2f1b6b13-e95c-4859-b51e-d74b0e7545ba"} />;
+const data = props.data;
+const assetId = "2f1b6b13-e95c-4859-b51e-d74b0e7545ba";
+
+return <LivepeerPlayer assetId={assetId} />;
