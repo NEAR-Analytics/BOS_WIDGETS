8d64338c5fa8c9a16e81cca0105a0617bf176d86
votanlean.near <votanlean.near>
2023-07-11 08:01:09 +0000

    Update Onboarding.Starter by votanlean.near at 2023-07-11T08:01:09.660Z

---
+++ votanlean.near/Onboarding.Starter/source_code.jsx
@@ -0,0 +1,9 @@
+let greet = "Have a great day!";
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <p> Hello {props.name} </p>
+      <p> {greet} </p>
+    </div>
+  </>
+);
