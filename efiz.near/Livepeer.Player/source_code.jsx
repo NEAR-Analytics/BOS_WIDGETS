50461114c4d795a72b8e6ac7c738d249bf90f709
efiz.near <efiz.near>
2023-07-11 17:32:09 +0000

    Update Livepeer.Player by efiz.near at 2023-07-11T17:32:09.549Z

--- efiz.near/Livepeer.Player/source_code.jsx
+++ efiz.near/Livepeer.Player/source_code.jsx
@@ -2,4 +2,9 @@ const data = props.data;
 const assetId = "2f1b6b13-e95c-4859-b51e-d74b0e7545ba";
 const title = "Hello Arina";
 
-return <LivepeerPlayer title={title} assetId={assetId} />;
+return (
+  <>
+    <h1>Hello</h1>
+    <LivepeerPlayer title={title} assetId={assetId} />
+  </>
+);
