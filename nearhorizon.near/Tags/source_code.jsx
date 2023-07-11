5f91695312214709b004853d0116c4b4e2574904
nearhorizon.near <nearhorizon.near>
2023-07-11 07:47:29 +0000

    Update Tags by nearhorizon.near at 2023-07-11T07:47:29.583Z

---
+++ nearhorizon.near/Tags/source_code.jsx
@@ -0,0 +1,39 @@
+const tags = Object.keys(props.tags || {});
+
+const TagItem = styled.span`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 3px 8px;
+  gap: 3px;
+  background: #fff;
+  border: 1px solid #e6e8eb;
+  border-radius: 6px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 18px;
+  text-align: center;
+  color: #475467;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  gap: 0.5em;
+  max-width: 100%;
+  overflow: hidden;
+`;
+
+return (
+  <Container>
+    {tags.slice(0, 6).map((tag) => (
+      <TagItem key={tag}>{tag}</TagItem>
+    ))}
+  </Container>
+);
