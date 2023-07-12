0c22ed786f2ce45979756ea44452f65484ba4af4
calebjacob.near <calebjacob.near>
2023-07-12 17:32:20 +0000

    Update ImageViewer360 by calebjacob.near at 2023-07-12T17:32:20.485Z

--- calebjacob.near/ImageViewer360/source_code.jsx
+++ calebjacob.near/ImageViewer360/source_code.jsx
@@ -56,8 +56,6 @@ function onPointerUp() {
 }
 
 function onPointerMove(e) {
-  e.preventDefault();
-
   if (state.pointerIsDown) {
     const diff = state.pointerStartPositionX - e.clientX;
 
@@ -76,7 +74,7 @@ const Wrapper = styled.div`
     width: 100%;
     max-width: 600px;
     position: relative;
-    background: #f00;
+    background: #000;
 `;
 
 const Images = styled.div`
