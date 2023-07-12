a8bbf9845b2a91225df15f526bf7ee64410ac628
ilyar.near <ilyar.near>
2023-07-12 20:42:15 +0000

    Update NDCDocs by ilyar.near at 2023-07-12T20:42:15.553Z

---
+++ ilyar.near/NDCDocs/source_code.jsx
@@ -0,0 +1,33 @@
+const addressForArticles = "ndcWikiArticle";
+const authorsWhitelist = ["ilyar.near"];
+const authorForWidget = "neardigitalcollective.near";
+const accountId = props.accountId ?? context.accountId;
+// if (!accountId) {
+//   return "No account ID";
+// }
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+
+return (
+  <div
+    className="container-fluid"
+    style={{
+      backgroundColor: "rgb(230, 230, 230)",
+      borderRadius: "20px",
+      padding: "0",
+    }}
+  >
+    <Widget
+      src={`${authorForWidget}/widget/WikiOnSocialDB_MainNavigation`}
+      props={{ currentNavPill: "articles", writersWhiteList }}
+    />
+    <div style={{ margin: "0 auto", width: "90%", minWidth: "360px" }}>
+      <Widget
+        src={`${authorForWidget}/widget/NDCDocs_AllArticlesList`}
+        props={{ writersWhiteList }}
+      />
+    </div>
+  </div>
+);
