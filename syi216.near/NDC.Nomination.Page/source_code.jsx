cfebc36633dee33db6d1c3da64740085e47984bb
syi216.near <syi216.near>
2023-07-12 22:34:00 +0000

    Update NDC.Nomination.Page by syi216.near at 2023-07-12T22:34:00.305Z

--- syi216.near/NDC.Nomination.Page/source_code.jsx
+++ syi216.near/NDC.Nomination.Page/source_code.jsx
@@ -79,16 +79,15 @@ function getVerifiedHuman() {
   });
 }
 
-const Page = styled.div``;
-// `
-// position: fixed;
-//   width: 100vw;
-//   height: calc(-98px + 100vh);
-//   top: 98px;
-//   left: 0;
-//   padding-left:20px;
-//   padding-right:20px;
-// `;
+const Page = styled.div`
+position: fixed;
+  width: 1500px;
+  height: calc(-98px + 100vh);
+  top: 98px;
+  left: 0;
+  padding-left:20px;
+  padding-right:20px;
+`;
 
 function getNominationInfo() {
   let nominationsArr = [];
