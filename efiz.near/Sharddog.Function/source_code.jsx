1a101e8a7ea91ce126532a5d8c7d9f2c0fa36517
efiz.near <efiz.near>
2023-07-12 01:55:19 +0000

    Update Sharddog.Function by efiz.near at 2023-07-12T01:55:19.361Z

--- efiz.near/Sharddog.Function/source_code.jsx
+++ efiz.near/Sharddog.Function/source_code.jsx
@@ -1,13 +1,14 @@
 function sharddogFetch(limit, offset) {
-  asyncFetch("https://graph.mintbase.xyz/mainnet", {
-    method: "POST",
-    headers: {
-      "mb-api-key": "omni-site",
-      "Content-Type": "application/json",
-      "x-hasura-role": "anonymous",
-    },
-    body: JSON.stringify({
-      query: `
+  function getItems() {
+    const res = fetch("https://graph.mintbase.xyz/mainnet", {
+      method: "POST",
+      headers: {
+        "mb-api-key": "omni-site",
+        "Content-Type": "application/json",
+        "x-hasura-role": "anonymous",
+      },
+      body: JSON.stringify({
+        query: `
           query MyQuery {
             mb_views_nft_tokens(
                 limit: ${limit},
@@ -20,14 +21,14 @@ function sharddogFetch(limit, offset) {
             }
           }
         `,
-    }),
-  }).then((res) => {
+      }),
+    });
     if (res.ok) {
       return res.body.data.mb_views_nft_tokens;
     } else {
       return [];
     }
-  });
+  }
+  return getItems();
 }
-
 return { sharddogFetch };
