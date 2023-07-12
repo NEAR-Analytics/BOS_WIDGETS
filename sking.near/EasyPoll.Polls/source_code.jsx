3f85aeac9acc5a5d830e37a6e69865134dedbf0f
sking.near <sking.near>
2023-07-12 08:01:42 +0000

    Update EasyPoll.Polls by sking.near at 2023-07-12T08:01:42.701Z

--- sking.near/EasyPoll.Polls/source_code.jsx
+++ sking.near/EasyPoll.Polls/source_code.jsx
@@ -2,15 +2,30 @@ const indexVersion = props.indexVersion ?? "4.0.0";
 const filterByUser = props.filterByUser;
 const skipHumanVericationFor = props.skipHumanVericationFor;
 const onlyVerifiedHumans = props.onlyVerifiedHumans ?? true;
+const onlyOfficialNDCPolls = props.onlyOfficialNDCPolls ?? true;
 const blackList = props.blackList;
 const tabs = props.tabs;
-const draft = props.draft ?? false;
+const customKeys = props.customKeys;
+const type = props.type ?? "poll"; // draft, poll, official
+
+const INITIAL_PAGE = 0;
+const PAGE_SIZE = 15;
+const widgetOwner = "sking.near";
 
 State.init({
-  polls: {},
+  currentPage: INITIAL_PAGE,
 });
 
-const widgetOwner = "sking.near";
+const onPageChange = (pageNumber) => {
+  pageNumber = pageNumber - 1;
+  if (pageNumber === state.currentPage) {
+    console.log(`Selected the same page number as before: ${pageNumber}`);
+    return;
+  }
+  State.update({
+    currentPage: pageNumber,
+  });
+};
 
 const getFirstSBTToken = (accountId) => {
   const view = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
@@ -33,17 +48,22 @@ const shouldDisplayUserQuestions = (accountId) => {
   return true;
 };
 
-let keys = `*/easypoll-${indexVersion}/${draft ? "draft" : "poll"}/*`;
+let keys = `*/easypoll-${indexVersion}/${type}/*`;
 
 if (filterByUser) {
   keys = filterByUser.map((v) => {
-    return `${v}/easypoll-${indexVersion}/${draft ? "draft" : "poll"}/*`;
+    return `${v}/easypoll-${indexVersion}/${type}/*`;
   });
+}
+
+if (customKeys) {
+  keys = customKeys;
 }
 
 let results = Social.keys(keys, "final", {
   return_type: "BlockHeight",
 });
+console.log("results", results);
 if (!results) {
   return "Loading...";
 }
@@ -52,29 +72,34 @@ const polls_keys = [];
 
 // TODO: should cache the logic bellow in state, polls_keys can be huge
 Object.keys(results).forEach((accountId) => {
-  return Object.keys(
-    results[accountId][`easypoll-${indexVersion}`][draft ? "draft" : "poll"]
-  ).forEach((pollId) => {
-    polls_keys.push({
-      accountId,
-      pollId,
-      blockHeight:
-        results[accountId][`easypoll-${indexVersion}`][
-          draft ? "draft" : "poll"
-        ][pollId],
-    });
-  });
+  return Object.keys(results[accountId][`easypoll-${indexVersion}`]).forEach(
+    (poll_type) => {
+      return Object.keys(
+        results[accountId][`easypoll-${indexVersion}`][poll_type]
+      ).forEach((pollId) => {
+        polls_keys.push({
+          accountId,
+          pollId,
+          blockHeight:
+            results[accountId][`easypoll-${indexVersion}`][poll_type][pollId],
+          type: poll_type,
+        });
+      });
+    }
+  );
 });
 polls_keys = polls_keys.sort((a, b) => b.blockHeight - a.blockHeight); // desc
 polls_keys = polls_keys.filter((p) => shouldDisplayUserQuestions(p.accountId));
 
+const start = state.currentPage * PAGE_SIZE;
+const end = start + PAGE_SIZE;
+const paginated_polls_keys = polls_keys.slice(start, end);
+
 return (
   <>
     <div className="d-flex flex-column gap-4 mb-3">
-      {polls_keys.map((p) => {
-        const src = `${p.accountId}/easypoll-${indexVersion}/${
-          draft ? "draft" : "poll"
-        }/${p.pollId}`;
+      {paginated_polls_keys.map((p) => {
+        const src = `${p.accountId}/easypoll-${indexVersion}/${p.type}/${p.pollId}`;
         return (
           <Widget
             src={`${widgetOwner}/widget/EasyPoll.PollCard`}
@@ -85,12 +110,26 @@ return (
               editHref: tabs.EDIT_POLL.href(src, p.blockHeight),
               deleteHref: tabs.DELETE_POLL.href(src, p.blockHeight),
               indexVersion,
+              onlyOfficialNDCPolls,
+              topLabel: p.type === "draft" ? "Draft" : undefined,
             }}
           />
         );
       })}
     </div>
-    {polls_keys.length < 1 && <div>Looks like there are no polls to show.</div>}
-    {/* TODO: NEED PAGINATION */}
+    {paginated_polls_keys.length < 1 && (
+      <div>Looks like there are no polls to show.</div>
+    )}
+    {polls_keys.length > PAGE_SIZE && (
+      <Widget
+        src={`${widgetOwner}/widget/Common.Paginate`}
+        props={{
+          totalCount: polls_keys.length,
+          pageSize: PAGE_SIZE,
+          onPageChange,
+          currentPage: state.currentPage + 1,
+        }}
+      />
+    )}
   </>
 );
