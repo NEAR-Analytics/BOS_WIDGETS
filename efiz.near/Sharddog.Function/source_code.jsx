3a1a5f5d8d2378d1ffbeadfc17d620e2f0b7d6ba
efiz.near <efiz.near>
2023-07-12 00:23:08 +0000

    Update Sharddog.Function by efiz.near at 2023-07-12T00:23:08.332Z

---
+++ efiz.near/Sharddog.Function/source_code.jsx
@@ -0,0 +1,27 @@
+function sharddogFetch(limit, offset) {
+  return fetch("https://graph.mintbase.xyz/mainnet", {
+    method: "POST",
+    headers: {
+      "mb-api-key": "omni-site",
+      "Content-Type": "application/json",
+      "x-hasura-role": "anonymous",
+    },
+    body: JSON.stringify({
+      query: `
+          query MyQuery {
+            mb_views_nft_tokens(
+                limit: ${limit},
+                offset: ${offset}
+              where: { nft_contract_id: { _eq: "mint.sharddog.near" }}
+              order_by: {minted_timestamp: desc}
+            ) {
+              media
+              owner
+            }
+          }
+        `,
+    }),
+  });
+}
+
+return { sharddogFetch };
