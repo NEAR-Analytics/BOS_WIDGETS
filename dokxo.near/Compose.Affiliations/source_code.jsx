597f1093238025b4fae9e84a749e74521a9db069
dokxo.near <dokxo.near>
2023-07-13 20:30:48 +0000

    Update Compose.Affiliations by dokxo.near at 2023-07-13T20:30:48.323Z

--- dokxo.near/Compose.Affiliations/source_code.jsx
+++ dokxo.near/Compose.Affiliations/source_code.jsx
@@ -238,198 +238,214 @@ const {
   handleAFFRole,
 } = props;
 // State
-
+const cssFont = fetch(
+  "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+).body;
+console.log(cssFont);
+if (!state.theme) {
+  State.update({
+    theme: styled.div`
+    font-family: Open Sans;
+    ${cssFont}
+`,
+  });
+}
+const Theme = state.theme;
 return (
-  <div
-    name={"aff"}
-    class="row col-sm-12 mx-auto    "
-    style={{
-      height: "auto",
-      "padding-left": "16px",
-      "padding-right": "16px",
-    }}
-  >
-    {" "}
-    <FormsectionAffiliation name="FormsectionAffiliation">
-      <AffiliationHead>
-        <AffiliationTitle>
-          <H2styled>{"Afiliations"}</H2styled>
-          <button
-            name={"addsfd"}
-            onClick={addFields}
-            style={{
-              background: "#FFD50D",
-              "border-color": "#00000000",
-              padding: "0px",
-              "margin-left": "20px",
-            }}
-          >
-            <label
+  <Theme>
+    <div
+      name={"aff"}
+      class="row col-sm-12 mx-auto    "
+      style={{
+        height: "auto",
+        "padding-left": "16px",
+        "padding-right": "16px",
+      }}
+    >
+      {" "}
+      <FormsectionAffiliation name="FormsectionAffiliation">
+        <AffiliationHead>
+          <AffiliationTitle>
+            <H2styled>{"Afiliations"}</H2styled>
+            <button
+              name={"addsfd"}
+              onClick={addFields}
               style={{
-                "font-size": "12px",
-                color: "#000000",
-                "padding-left": "5px",
+                background: "#FFD50D",
+                "border-color": "#00000000",
+                padding: "0px",
+                "margin-left": "20px",
               }}
             >
-              {" "}
-              Add an Affiliation
-            </label>
-            <img
-              style={{}}
-              src={`https://emerald-related-swordtail-341.mypinata.cloud/ipfs/QmbNqXEbUqSkGY7quBnyBqU6nKuXimYTnxTNsbYHnhdVXw?_gl=1*aubyn4*rs_ga*YTcxZDg1OTgtZTYyOC00M2U2LWE4MTctNzUzMDRkMjA3ZWVl*rs_ga_5RMPXG14TE*MTY4Njk0MjQwOC4xMS4xLjE2ODY5NDI0NzMuNTcuMC4w`}
-            />
-          </button>
-        </AffiliationTitle>
-        <div style={{ padding: "12px", width: "100%" }}>
-          <Separator />
-        </div>
-      </AffiliationHead>
-
-      <AffiliationBody>
-        {affiliations.map((form, index) => {
-          return (
-            <div
-              class="bg-white rounded"
-              style={{ width: "100%", "margin-top": "20px" }}
-            >
-              <div
-                class="row  col-sm-12     gap-1   "
+              <label
                 style={{
-                  "padding-left": "16px",
-                  "padding-top": "11px",
+                  "font-size": "12px",
+                  color: "#000000",
+                  "padding-left": "5px",
                 }}
               >
-                <div name="AFOrganization" class="row ">
-                  <CompanyTitle>{"Organization Name"}</CompanyTitle>
-                  <div>
-                    <CompanyInputName
-                      placeholder="Company Name"
-                      value={form.company_name}
-                      onChange={(event) => {
-                        let _param = { index, event };
-                        handleAFFCompanyName(_param);
-                      }}
-                    />
+                {" "}
+                Add an Affiliation
+              </label>
+              <img
+                style={{}}
+                src={`https://emerald-related-swordtail-341.mypinata.cloud/ipfs/QmbNqXEbUqSkGY7quBnyBqU6nKuXimYTnxTNsbYHnhdVXw?_gl=1*aubyn4*rs_ga*YTcxZDg1OTgtZTYyOC00M2U2LWE4MTctNzUzMDRkMjA3ZWVl*rs_ga_5RMPXG14TE*MTY4Njk0MjQwOC4xMS4xLjE2ODY5NDI0NzMuNTcuMC4w`}
+              />
+            </button>
+          </AffiliationTitle>
+          <div style={{ padding: "12px", width: "100%" }}>
+            <Separator />
+          </div>
+        </AffiliationHead>
+
+        <AffiliationBody>
+          {affiliations.map((form, index) => {
+            return (
+              <div
+                class="bg-white rounded"
+                style={{ width: "100%", "margin-top": "20px" }}
+              >
+                <div
+                  class="row  col-sm-12     gap-1   "
+                  style={{
+                    "padding-left": "16px",
+                    "padding-top": "11px",
+                  }}
+                >
+                  <div name="AFOrganization" class="row ">
+                    <CompanyTitle>{"Organization Name"}</CompanyTitle>
+                    <div>
+                      <CompanyInputName
+                        placeholder="Company Name"
+                        value={form.company_name}
+                        onChange={(event) => {
+                          let _param = { index, event };
+                          handleAFFCompanyName(_param);
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div class="row   ">
-                  {" "}
-                  <AFDates name="AFdates">
-                    <DateContItem>
-                      <CompanyTitle>{"Start date"}</CompanyTitle>
-                      <CompanyInput
-                        type="date"
-                        max={getCurdate()}
+                  <div class="row   ">
+                    {" "}
+                    <AFDates name="AFdates">
+                      <DateContItem>
+                        <CompanyTitle>{"Start date"}</CompanyTitle>
+                        <CompanyInput
+                          type="date"
+                          max={getCurdate()}
+                          onChange={(event) => {
+                            let _param = { index, event };
+                            handleAFFStartdate(_param);
+                          }}
+                        ></CompanyInput>
+                      </DateContItem>
+                      <DateContItem>
+                        <CompanyTitle>{"End date"}</CompanyTitle>
+                        <CompanyInput
+                          type="date"
+                          max={getCurdate()}
+                          onChange={(event) => {
+                            let _param = { index, event };
+                            handleAFFEnddate(_param);
+                          }}
+                        ></CompanyInput>
+                      </DateContItem>
+                    </AFDates>
+                  </div>
+
+                  <div class="row    py-2">
+                    <CompanyTitle>{"Role Description"}</CompanyTitle>
+
+                    <div>
+                      <FormsectionPlatformtextarea
+                        style={{
+                          "font-style": "normal",
+                          "font-weight": "400",
+                          "font-size": "12px",
+                          height: "72px",
+                        }}
+                        name="Description"
+                        id="Description"
+                        type="text"
+                        placeholder="Please describe your role at the organization"
+                        value={form.role}
                         onChange={(event) => {
                           let _param = { index, event };
-                          handleAFFStartdate(_param);
+                          handleAFFRole(_param);
                         }}
-                      ></CompanyInput>
-                    </DateContItem>
-                    <DateContItem>
-                      <CompanyTitle>{"End date"}</CompanyTitle>
-                      <CompanyInput
-                        type="date"
-                        max={getCurdate()}
-                        onChange={(event) => {
-                          let _param = { index, event };
-                          handleAFFEnddate(_param);
+                      />
+                      <div
+                        style={{
+                          "margin-top": "5px",
+
+                          order: "3",
+                          width: "100%",
+                          display: "flex",
+                          "justify-content": "end",
                         }}
-                      ></CompanyInput>
-                    </DateContItem>
-                  </AFDates>
-                </div>
+                      >
+                        <label
+                          style={{
+                            "font-size": "8px",
+                            display: "flex",
+                            "vertical-align": "top",
+                            "text-align": "center",
+                            color:
+                              form.role.length < 500
+                                ? "#00000075"
+                                : "#ff000075",
+                          }}
+                        >
+                          {form.role.length} - 500
+                        </label>
+                      </div>
+                    </div>
+                  </div>
 
-                <div class="row    py-2">
-                  <CompanyTitle>{"Role Description"}</CompanyTitle>
-
-                  <div>
-                    <FormsectionPlatformtextarea
+                  <div class="row flex justify-content-end my-2">
+                    <button
+                      onClick={() => removeField(index)}
                       style={{
-                        "font-style": "normal",
-                        "font-weight": "400",
-                        "font-size": "12px",
-                        height: "72px",
-                      }}
-                      name="Description"
-                      id="Description"
-                      type="text"
-                      placeholder="Please describe your role at the organization"
-                      value={form.role}
-                      onChange={(event) => {
-                        let _param = { index, event };
-                        handleAFFRole(_param);
-                      }}
-                    />
-                    <div
-                      style={{
-                        "margin-top": "5px",
-
-                        order: "3",
-                        width: "100%",
+                        background: "#F1D6D5",
+                        "border-color": "#C23F38",
+                        width: "137px",
+                        height: "28px",
                         display: "flex",
-                        "justify-content": "end",
+                        padding: " 2px 12px",
+
+                        gap: " 6px",
+                        "align-items": "center",
                       }}
                     >
                       <label
                         style={{
-                          "font-size": "8px",
+                          "font-style": "normal",
+                          "font-weight": "400",
+                          "font-size": "12px",
+                          "line-height": " 24px",
+                          color: "#C23F38",
+                          width: "136px",
+
                           display: "flex",
-                          "vertical-align": "top",
-                          "text-align": "center",
-                          color:
-                            form.role.length < 500 ? "#00000075" : "#ff000075",
                         }}
                       >
-                        {form.role.length} - 500
+                        Delete Affiliation
                       </label>
-                    </div>
+                      <img
+                        style={{
+                          width: "14px",
+                          height: "14px",
+                        }}
+                        src={`https://emerald-related-swordtail-341.mypinata.cloud/ipfs/QmRDcewbKHpy9SKAbskYYfRGfTXtUpunSY3f2D3aqyanJM?_gl=1*1vropbf*_ga*NTIwNjIxMzEyLjE2ODc4MTYwMTE.*_ga_5RMPXG14TE*MTY4NzgxNjAxNS4xLjEuMTY4NzgxNjExMi42MC4wLjA.`}
+                      />
+                    </button>
                   </div>
                 </div>
-
-                <div class="row flex justify-content-end my-2">
-                  <button
-                    onClick={() => removeField(index)}
-                    style={{
-                      background: "#F1D6D5",
-                      "border-color": "#C23F38",
-                      width: "137px",
-                      height: "28px",
-                      display: "flex",
-                      padding: " 2px 12px",
-
-                      gap: " 6px",
-                      "align-items": "center",
-                    }}
-                  >
-                    <label
-                      style={{
-                        "font-style": "normal",
-                        "font-weight": "400",
-                        "font-size": "12px",
-                        "line-height": " 24px",
-                        color: "#C23F38",
-                        width: "136px",
-
-                        display: "flex",
-                      }}
-                    >
-                      Delete Affiliation
-                    </label>
-                    <img
-                      style={{
-                        width: "14px",
-                        height: "14px",
-                      }}
-                      src={`https://emerald-related-swordtail-341.mypinata.cloud/ipfs/QmRDcewbKHpy9SKAbskYYfRGfTXtUpunSY3f2D3aqyanJM?_gl=1*1vropbf*_ga*NTIwNjIxMzEyLjE2ODc4MTYwMTE.*_ga_5RMPXG14TE*MTY4NzgxNjAxNS4xLjEuMTY4NzgxNjExMi42MC4wLjA.`}
-                    />
-                  </button>
-                </div>
               </div>
-            </div>
-          );
-        })}
-      </AffiliationBody>
-    </FormsectionAffiliation>
-  </div>
+            );
+          })}
+        </AffiliationBody>
+      </FormsectionAffiliation>
+    </div>
+  </Theme>
 );
