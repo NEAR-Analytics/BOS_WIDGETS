c77ac7472738b447b34d3097b4770d2029365259
harmonic1.near <harmonic1.near>
2023-07-13 11:51:44 +0000

    Update artist by harmonic1.near at 2023-07-13T11:51:44.522Z

--- harmonic1.near/artist/source_code.jsx
+++ harmonic1.near/artist/source_code.jsx
@@ -1,8 +1,39 @@
 const data = props.data;
 
+const containerStyle = {
+  textAlign: "center",
+  margin: "20px",
+};
+
+const headingStyle = {
+  color: "#333",
+};
+
+const imageStyle = {
+  width: "200px",
+  height: "200px",
+  borderRadius: "50%",
+  marginBottom: "10px",
+};
+
+const linkStyle = {
+  display: "inline-block",
+  margin: "5px",
+  color: "#333",
+  textDecoration: "none",
+};
+
 return (
-  <div>
-    <p>{data.name}</p>
-    <p>{data.description}</p>
+  <div style={containerStyle}>
+    <h1 style={headingStyle}>{data.name}</h1>
+    <img src={data.avatar} alt="Musician's Avatar" style={imageStyle} />
+    <p>Bio goes here...</p>
+    <div>
+      {data.links.map((link, index) => (
+        <a key={index} href={link.value} style={linkStyle}>
+          {link.key}
+        </a>
+      ))}
+    </div>
   </div>
 );
