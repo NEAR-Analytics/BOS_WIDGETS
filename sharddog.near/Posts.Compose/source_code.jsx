4dc369f317859e4a47f9b35d6baaf12e5f738ce7
sharddog.near <sharddog.near>
2023-07-13 19:59:29 +0000

    Update Posts.Compose by sharddog.near at 2023-07-13T19:59:29.916Z

--- sharddog.near/Posts.Compose/source_code.jsx
+++ sharddog.near/Posts.Compose/source_code.jsx
@@ -14,7 +14,9 @@ const autocompleteEnabled = true;
 
 const content = {
   type: "md",
-  image: state.image.cid ? { ipfs_cid: state.image.cid } : undefined,
+  images: Object.values(state).map((image) =>
+    image.cid ? { ipfs_cid: image.cid } : undefined
+  ),
   text: state.text,
 };
 
