7670205ee65e6249bcc73d112beb5f0612834792
efiz.near <efiz.near>
2023-07-13 14:12:49 +0000

    Update Livepeer.Player by efiz.near at 2023-07-13T14:12:49.823Z

--- efiz.near/Livepeer.Player/source_code.jsx
+++ efiz.near/Livepeer.Player/source_code.jsx
@@ -3,6 +3,6 @@ const assetId = "2f1b6b13-e95c-4859-b51e-d74b0e7545ba";
 
 return (
   <>
-    <LivepeerPlayer assetId={assetId} />
+    <LivepeerPlayer assetId={"2f1b6b13-e95c-4859-b51e-d74b0e7545ba"} />
   </>
 );
