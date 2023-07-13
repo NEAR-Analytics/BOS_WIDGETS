5d362072c0903bb84382dac4a8ebb6d37f12b062
harmonic1.near <harmonic1.near>
2023-07-13 12:00:40 +0000

    Update artist by harmonic1.near at 2023-07-13T12:00:40.082Z

--- harmonic1.near/artist/source_code.jsx
+++ harmonic1.near/artist/source_code.jsx
@@ -29,12 +29,14 @@ return (
     <h1 style={headingStyle}>{data.name}</h1>
     <img src={data.avatar} alt="Musician's Avatar" style={imageStyle} />
     <p>Bio goes here...</p>
-    <div>
-      {data.links.map((link, index) => (
-        <a key={index} href={link.value} style={linkStyle}>
-          {link.key}
-        </a>
-      ))}
-    </div>
+    {data.links && data.links.length > 0 && (
+      <div>
+        {data.links.map((link, index) => (
+          <a key={index} href={link.value} style={linkStyle}>
+            {link.key}
+          </a>
+        ))}
+      </div>
+    )}
   </div>
 );
