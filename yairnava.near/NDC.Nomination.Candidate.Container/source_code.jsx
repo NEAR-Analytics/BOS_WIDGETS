0860db08d0cfbb5625016cd9d01e71c824422491
yairnava.near <yairnava.near>
2023-07-13 22:21:59 +0000

    Update NDC.Nomination.Candidate.Container by yairnava.near at 2023-07-13T22:21:59.665Z

--- yairnava.near/NDC.Nomination.Candidate.Container/source_code.jsx
+++ yairnava.near/NDC.Nomination.Candidate.Container/source_code.jsx
@@ -41,7 +41,6 @@ justify-content: center;
 @media only screen and (min-width: 601px) {
   display: none !important;
 }
-font-family: Open Sans;
 `;
 
 const Desktop = styled.div`
@@ -50,7 +49,6 @@ justify-content: center;
 @media only screen and (max-width: 600px) {
   display: none !important;
 }
-font-family: Open Sans;
 `;
 
 return (
