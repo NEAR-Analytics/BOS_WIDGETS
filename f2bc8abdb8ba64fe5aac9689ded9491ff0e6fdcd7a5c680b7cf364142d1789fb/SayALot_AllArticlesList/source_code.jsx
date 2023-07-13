ecb25f7b0ac47b6128493692aa4924cbe8875baa
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-07-13 18:38:12 +0000

    Update SayALot_AllArticlesList by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-07-13T18:38:12.844Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_AllArticlesList/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_AllArticlesList/source_code.jsx
@@ -25,7 +25,8 @@ if (isDebug) {
 
 const addressForArticles = isDebug ? "test_sayALotArticle" : "sayALotArticle";
 const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
-const authorForWidget = "sayalot.near";
+const authorForWidget =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 // ========== GET INDEX ARRAY FOR ARTICLES ==========
 const postsIndex = Social.index(addressForArticles, "main", {
   order: "desc",
