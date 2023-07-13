8689e432e1a7028e75969566bb538bbed131b15d
syi216.near <syi216.near>
2023-07-13 21:15:58 +0000

    Update NDC.nomination.card by syi216.near at 2023-07-13T21:15:58.732Z

--- syi216.near/NDC.nomination.card/source_code.jsx
+++ syi216.near/NDC.nomination.card/source_code.jsx
@@ -5,7 +5,6 @@ State.init({
   voted: false,
   shareText: "Copy link to the clipboard",
 });
-console.log("fv verificado", props.verified);
 let nominationContract = "nominations-v1.gwg-testing.near";
 function getVerifiedHuman() {
   asyncFetch(
