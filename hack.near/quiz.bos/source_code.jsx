4d2c16b23d8fb3495ab8632ae77ae5b1d97a6072
hack.near <hack.near>
2023-07-13 21:15:43 +0000

    Update quiz.bos by hack.near at 2023-07-13T21:15:43.118Z

--- hack.near/quiz.bos/source_code.jsx
+++ hack.near/quiz.bos/source_code.jsx
@@ -45,6 +45,14 @@ const handleOptionD = (selectedOption) => {
   });
 };
 
+let showQuestion1 = false;
+
+if (
+  Social.keys(`${context.accountId}.near/graph/follow/academy.near`) === true
+) {
+  showQuestion1 = true;
+}
+
 let showQuestion2 = false;
 let showQuestion3 = false;
 let showQuestion4 = false;
@@ -68,30 +76,61 @@ if (state.answer4.toLowerCase() === "near") {
 
 return (
   <div>
-    <h1>BOS 101</h1>
-    <h3>Test Your Knowledge!</h3>
-    <h5>
-      First, read the <a href="https://docs.near.org/bos">docs</a> ⋈
-    </h5>
+    <div className="d-flex flex-wrap justify-content-between mb-3">
+      <div className="m-1">
+        <h2>
+          <b>BOS 101</b>
+        </h2>
+        <h4>Test Your Knowledge</h4>
+        <h5>
+          First, read the <a href="https://docs.near.org/bos">docs</a> ⋈
+        </h5>
+      </div>
+      <div className="m-2">
+        <Widget
+          src="mob.near/widget/Profile"
+          props={{ accountId: "academy.near" }}
+        />
+      </div>
+    </div>
     <hr />
     <div>
-      <h4>Question 1:</h4>
-      <p>What does the "BOS" acronym stand for?</p>
-      <input
-        className="form-control"
-        placeholder="Your answer goes here :)"
-        defaultValue={state.answer1}
-        onChange={(e) => {
-          State.update({
-            answer1: e.target.value,
-          });
-        }}
-      />
+      {context.accountId ? (
+        <Widget
+          src="hack.near/widget/connect.button"
+          props={{ accountId: "academy.near" }}
+        />
+      ) : (
+        <Widget
+          src="near/widget/DIG.Button"
+          props={{
+            href: "https://near.org/signup",
+            label: "Create Account",
+            variant: "outline-dark",
+          }}
+        />
+      )}
     </div>
+    {showQuestion1 && (
+      <div>
+        <h5>Question 1:</h5>
+        <p>What does the "BOS" acronym stand for?</p>
+        <input
+          className="form-control"
+          placeholder="Your answer goes here :)"
+          defaultValue={state.answer1}
+          onChange={(e) => {
+            State.update({
+              answer1: e.target.value,
+            });
+          }}
+        />
+      </div>
+    )}
     <br />
     {showQuestion2 && (
       <div>
-        <h4>Question 2:</h4>
+        <h5>Question 2:</h5>
         <p>What language is used to build on the BOS?</p>
         <input
           className="form-control"
@@ -108,7 +147,7 @@ return (
     <br />
     {showQuestion3 && (
       <div>
-        <h4>Question 3:</h4>
+        <h5>Question 3:</h5>
         <p>What is the path of this BOS component, which you are using now?</p>
         <input
           className="form-control"
@@ -125,7 +164,7 @@ return (
     <br />
     {showQuestion4 && (
       <div>
-        <h4>Question 4:</h4>
+        <h5>Question 4:</h5>
         <p>
           Where is the code actually stored for decentralized apps on the BOS?
         </p>
@@ -141,9 +180,10 @@ return (
         />
       </div>
     )}
+    <br />
     {showQuestion5 && (
       <div>
-        <h4>Question 5:</h4>
+        <h5>Question 5:</h5>
         <p>Which is NOT 1 of the 3 core pillars of the BOS?</p>
         <button onClick={() => handleAnswer("A")}>Components</button>
         <button onClick={() => handleAnswer("B")}>Smart Contracts</button>
@@ -155,6 +195,7 @@ return (
       <div>
         {state.answer === "A" && (
           <div>
+            <br />
             <h5>TRY AGAIN </h5>
             <p>
               Components are small web 3 applications that are stored entirely
@@ -165,11 +206,14 @@ return (
         )}
         {state.answer === "B" && (
           <div>
+            <br />
             <h5>CONGRATS 🎉</h5>
+            <p>You got it right!</p>
             <p>
-              Although BOS helps builders interact with smart contracts, they
-              are not considered one of the core pillars. However, you are just
-              now beginning a journey to understand the SocialDB contract...
+              Although BOS makes it easier for builders to facilitate user
+              interactions with smart contracts, they are not considered one of
+              the core pillars. However, you are just now beginning a journey to
+              understand the SocialDB contract...
             </p>
             <Widget
               src="hack.near/widget/claim.badge"
@@ -179,6 +223,8 @@ return (
         )}
         {state.answer === "C" && (
           <div>
+            <br />
+
             <h5>TRY AGAIN</h5>
             <p>
               Components can call functions on any blockchain, with current
@@ -190,6 +236,8 @@ return (
         )}
         {state.answer === "D" && (
           <div>
+            <br />
+
             <h5>TRY AGAIN</h5>
             <p>
               Gateways make locally-run, decentralized front-ends available to
@@ -202,5 +250,6 @@ return (
         )}
       </div>
     )}
+    <br />
   </div>
 );
