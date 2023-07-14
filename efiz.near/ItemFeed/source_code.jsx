610b3c1baea8725e77f990fe4de541f81ab52c36
efiz.near <efiz.near>
2023-07-14 01:11:58 +0000

    Update ItemFeed by efiz.near at 2023-07-14T01:11:58.244Z

---
+++ efiz.near/ItemFeed/source_code.jsx
@@ -0,0 +1,37 @@
+const items = props.items;
+const renderItem = props.renderItem;
+const perPage = props.perPage || 10;
+
+const jItems = JSON.stringify(items);
+if (state.jItems !== jItems) {
+  State.update({
+    widgets: 0,
+    jItems,
+  });
+}
+
+const makeMoreItems = () => {
+  State.update({
+    widgets: state.widgets + perPage,
+  });
+};
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  @media (hover: none) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+return (
+  <InfiniteScroll
+    pageStart={0}
+    loadMore={makeMoreItems}
+    hasMore={state.widgets < items.length}
+    loader={<div className="loader">Loading ...</div>}
+  >
+    <Grid>{items.slice(0, state.widgets).map(renderItem)}</Grid>
+  </InfiniteScroll>
+);
