bdce3fa87ace9d2f60fa27a1fdd6c57770bd29d6
nearefi.near <nearefi.near>
2023-07-14 17:20:10 +0000

    Update ReFI.Regen.sbtHolders by nearefi.near at 2023-07-14T17:20:10.671Z

---
+++ nearefi.near/ReFI.Regen.sbtHolders/source_code.jsx
@@ -0,0 +1,13 @@
+return (
+  <div>
+    <Widget
+      src="ndcplug.near/widget/ndc-badge-holders"
+      props={{
+        title: "NDC Proof of Regen",
+        issuer: "issuer.regens.near",
+        showProgress: false,
+        showDropdown: false,
+      }}
+    />
+  </div>
+);
