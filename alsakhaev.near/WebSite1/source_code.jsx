8082205cfb272a66a29a8867e0ec1aa02c6a6391
alsakhaev.near <alsakhaev.near>
2023-07-14 10:25:43 +0000

    Update WebSite1 by alsakhaev.near at 2023-07-14T10:25:43.901Z

---
+++ alsakhaev.near/WebSite1/source_code.jsx
@@ -0,0 +1,13 @@
+return (
+  <div>
+    <Widget src="alsakhaev.near/widget/HelloWorldText" />
+    <Widget
+      src="alsakhaev.near/widget/ExampleButton"
+      props={{ label: "Button 1" }}
+    />
+    <Widget
+      src="alsakhaev.near/widget/ExampleButton"
+      props={{ label: "Button 2" }}
+    />
+  </div>
+);
