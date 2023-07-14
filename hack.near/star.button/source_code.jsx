e5ec4e2f044b2a9302f210ba4200e8648f10ab84
hack.near <hack.near>
2023-07-14 21:00:24 +0000

    Update star.button by hack.near at 2023-07-14T21:00:24.190Z

---
+++ hack.near/star.button/source_code.jsx
@@ -0,0 +1,46 @@
+const accountId = props.accountId ?? context.accountId;
+
+const widgetPath = props.widgetPath ?? "devs.near/widget/dev.library";
+const [ownerId, widget, widgetName] = widgetPath.split("/");
+
+const starEdge = Social.keys(
+  `${accountId}/graph/star/${widgetPath}`,
+  undefined,
+  {
+    values_only: true,
+  }
+);
+
+const starred = starEdge && Object.keys(starEdge).length;
+
+const type = star ? "unstar" : "star";
+
+const data = {
+  graph: { star: { [widgetPath]: star ? null : "" } },
+  index: {
+    graph: JSON.stringify({
+      key: "star",
+      value: {
+        type,
+        src: widgetPath,
+      },
+    }),
+    notify: JSON.stringify({
+      key: ownerId,
+      value: {
+        type,
+      },
+    }),
+  },
+};
+
+return (
+  <CommitButton
+    disabled={!context.accountId}
+    className="btn btn-outline-secondary"
+    data={data}
+  >
+    <i className={`bi fs-5 ${starred ? "bi-star-fill" : "bi-star"}`} />
+    <span style={{ marginLeft: "0.2rem" }}>{starred ? "Starred" : "Star"}</span>
+  </CommitButton>
+);
