266482065e388fee372581e0add3c201399c57fa
yousouf.near <yousouf.near>
2023-07-14 17:33:05 +0000

    Update NearHausa by yousouf.near at 2023-07-14T17:33:05.019Z

--- yousouf.near/NearHausa/source_code.jsx
+++ yousouf.near/NearHausa/source_code.jsx
@@ -1,12 +1,144 @@
-let greeting = "Have a great day";
-let message = "lets build somethin";
-return (
-  <>
-    <div class="container border border-info p-3 text-center">
-      <h1>Hello {props.name}</h1>
+const accountId = props.accountId ?? context.accountId;
+const devId = props.devId ?? "yousouf.near";
+const role = props.role ?? "community";
 
-      <p> {greeting} </p>
-      <h4> {message} </h4>
+const tab = props.tab === "following" ? props.tab : "members";
+
+const Wrapper = styled.div`
+  --section-gap: 23px;
+  padding-top: 42px;
+
+  @media (max-width: 1155px) {
+    .line-rounded-corners {
+      display: none !important;
+    }
+  }
+
+  @media (max-width: 998px) {
+    padding-top: 0;
+  }
+`;
+
+const H1 = styled.h1`
+  font-family: "FK Grotesk", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 90px;
+  line-height: 1;
+  text-align: center;
+  letter-spacing: -0.03em;
+  color: #000;
+  margin: 0;
+  max-width: 700px;
+
+  span {
+    display: inline-block;
+    background: #96d2b7;
+    border-radius: 20px;
+    position: relative;
+    padding: 0.1em 0.2em 0;
+
+    svg {
+      position: absolute;
+      bottom: -8px;
+      right: -10px;
+      width: 24px;
+    }
+  }
+
+  @media (max-width: 900px) {
+    font-size: 50px;
+
+    span {
+      border-radius: 12px;
+      svg {
+        position: absolute;
+        bottom: -6px;
+        right: -7px;
+        width: 16px;
+      }
+    }
+  }
+`;
+
+const Text = styled.p`
+  font-family: "FK Grotesk", sans-serif;
+  font-size: ${(p) => p.size ?? "18px"};
+  line-height: ${(p) => p.lineHeight ?? "1.5"};
+  font-weight: ${(p) => p.weight ?? "400"};
+  color: ${(p) => p.color ?? "#000"};
+  margin: 0;
+  max-width: 670px;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: "nowrap";
+
+  @media (max-width: 998px) {
+    flex-direction: column;
+    gap: var(--section-gap);
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  max-width: 1080px;
+  margin: 0 auto;
+  gap: var(--section-gap);
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: var(--section-gap) 24px;
+
+  @media (max-width: 768px) {
+    padding: var(--section-gap) 12px;
+  }
+`;
+
+return (
+  <Wrapper>
+    <Container>
+      <Flex>
+        <H1>
+          💻 Near Hausa
+          <span>
+            BOS Builders{" "}
+            <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
+              <path
+                d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
+                fill="#323330"
+              />
+            </svg>
+          </span>
+        </H1>
+        <div className="mt-3">
+          <Text style={{ maxWidth: "350px" }}>
+            Building better future with HAUSA Near community.
+          </Text>
+        </div>
+      </Flex>
+    </Container>
+
+    <div className="m-2">
+      <Widget src="yousouf.near/widget/ProfilePage" />
     </div>
-  </>
+    <Flex>
+      <Widget
+        src="near/widget/AccountProfileCard"
+        props={{ accountId: devId }}
+      />
+    </Flex>
+    <br />
+
+    <br />
+  </Wrapper>
 );
