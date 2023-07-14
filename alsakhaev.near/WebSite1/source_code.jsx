60739ef4bbe029b039e9ccc19724cb9446667e92
alsakhaev.near <alsakhaev.near>
2023-07-14 10:31:16 +0000

    Update WebSite1 by alsakhaev.near at 2023-07-14T10:31:16.484Z

--- alsakhaev.near/WebSite1/source_code.jsx
+++ alsakhaev.near/WebSite1/source_code.jsx
@@ -1,5 +1,10 @@
 return (
   <div>
+    <h1>Website 1</h1>
+    <p>
+      Page 1 - consists of three elements. Text and Two buttons (different
+      customizations of the same widget).
+    </p>
     <Widget src="alsakhaev.near/widget/HelloWorldText" />
     <Widget
       src="alsakhaev.near/widget/ExampleButton"
