61759d61b5e6efc76c88f03a0400511f9a98b900
sayalot.near <sayalot.near>
2023-07-14 00:56:33 +0000

    Update SayALot_ArticlesByAuthor by sayalot.near at 2023-07-14T00:56:33.700Z

--- sayalot.near/SayALot_ArticlesByAuthor/source_code.jsx
+++ sayalot.near/SayALot_ArticlesByAuthor/source_code.jsx
@@ -1,171 +1,100 @@
-const addressForArticles = "ndcGigArticle";
+const addressForArticles = "sayALotArticle";
 const authorForWidget = "sayalot.near";
-const authorId = props.author;
+const tagSelected = props.tag;
 const accountId = props.accountId ?? context.accountId;
-
-const authorsWhitelist = props.writersWhiteList ?? [
-  "neardigitalcollective.near",
-  "blaze.near",
-  "jlw.near",
-  "kazanderdad.near",
-  "joep.near",
-  "sarahkornfeld.near",
-  "yuensid.near",
-];
-
-const articleBlackList = [
-  91092435, 91092174, 91051228, 91092223, 91051203, 91051228,
-];
-
 if (!accountId) {
   return "No account ID";
 }
-const profile = props.profile ?? Social.getr(`${authorId}/profile`);
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
 if (profile === null) {
   return "Loading";
 }
 
 const getDateLastEdit = (timestamp) => {
   const date = new Date(Number(timestamp));
-  const dateString = {
-    date: date.toLocaleDateString(),
-    time: date.toLocaleTimeString(),
-  };
+  const dateString = `${date.toLocaleDateString()} / ${date.toLocaleTimeString()}`;
   return dateString;
 };
+
+const articleBlackList = [
+  91092435, 91092174, 91051228, 91092223, 91051203, 96414482, 96402919,
+  96402330, 96401880, 96412953, 96412953, 95766840, 95413943,
+];
 
 // ========== GET INDEX ARRAY FOR ARTICLES ==========
 const postsIndex = Social.index(addressForArticles, "main", {
   order: "desc",
   accountId: undefined,
-}).filter((article) => !articleBlackList.includes(article.blockHeight));
-
+});
 // ========== GET ALL ARTICLES ==========
 const resultArticles =
   postsIndex &&
-  postsIndex
-    .reduce((acc, { accountId, blockHeight }) => {
-      const postData = Social.get(
-        `${accountId}/${addressForArticles}/main`,
-        blockHeight
-      );
-      const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
-      return [...acc, postDataWithBlockHeight];
-    }, [])
-    .filter((article) =>
-      authorsWhitelist.some((addr) => addr === article.author)
+  postsIndex.reduce((acc, { accountId, blockHeight }) => {
+    const postData = Social.get(
+      `${accountId}/${addressForArticles}/main`,
+      blockHeight
     );
+    const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
+    return [...acc, postDataWithBlockHeight];
+  }, []);
 // ========== FILTER DUBLICATES ==========
 const filteredArticles =
   resultArticles.length &&
-  resultArticles.reduce((acc, article) => {
-    if (!acc.some(({ articleId }) => articleId === article.articleId)) {
-      return [...acc, article];
-    } else {
-      return acc;
-    }
-  }, []);
+  resultArticles
+    .reduce((acc, article) => {
+      if (!acc.some(({ articleId }) => articleId === article.articleId)) {
+        return [...acc, article];
+      } else {
+        return acc;
+      }
+    }, [])
+    .filter((article) =>
+      writersWhiteList.some((addr) => addr === article.author)
+    )
+    .filter((article) => article.lastEditor === article.author)
+    .filter((article) => !articleBlackList.includes(article.blockHeight));
 
-const filteredArticlesByUser =
+const filteredArticlesByTag =
   filteredArticles.length &&
   filteredArticles.reduce((acc, article) => {
-    if (article.author === authorId) {
+    if (article.tags && article.tags.includes(tagSelected)) {
+      console.log(article);
       return [...acc, article];
     } else {
       return acc;
     }
   }, []);
 
-console.log(profile);
-
 return (
-  <div
-    className="container-fluid"
-    style={{
-      backgroundColor: "rgb(230, 230, 230)",
-      borderRadius: "20px",
-      padding: "0",
-    }}
-  >
+  <>
     <Widget
       src={`${authorForWidget}/widget/SayALot_MainNavigation`}
-      props={{ currentNavPill: "authors" }}
+      props={{ currentNavPill: "articles" }}
     />
-    <div style={{ margin: "0 auto", width: "90%", minWidth: "360px" }}>
-      <div className="card">
-        <div className="d-flex justify-content-between">
-          {profile ? (
-            <Widget
-              key="image"
-              src="mob.near/widget/ProfileImage"
-              props={{
-                style: { width: "5em", height: "5em", marginRight: "0.1em" },
-                profile,
-                authorId,
-                className: "inline",
-                imageClassName: "rounded w-100 h-100 align-top",
-              }}
-            />
-          ) : (
-            <div style={{ width: "5em" }}></div>
-          )}
-          <div>
-            <h4 className="text-center my-1">{profile.name}</h4>
-            <h4 className="text-center">{authorId}</h4>
-          </div>
-          <div style={{ width: "5em" }}></div>
-        </div>
-      </div>
-      <div className="row card-group py-3">
-        {filteredArticlesByUser &&
-          filteredArticlesByUser.map((article) => (
-            <div className="col-sm-12 col-lg-6 col-2xl-4 gy-3">
-              <div className="card h-100" key={article.articleId}>
-                <a
-                  className="text-decoration-none text-dark"
-                  href={`#/${authorForWidget}/widget/SayALot_OneArticle?articleId=${article.articleId}&blockHeight=${article.blockHeight}&lastEditor=${article.lastEditor}
-            `}
-                >
-                  <div className="card-body">
-                    <div className="row d-flex justify-content-center">
-                      <h5 className="card-title text-center pb-2 border-bottom">
-                        {article.articleId}
-                      </h5>
-                      <div className="col">
-                        <p className="card-subtitle text-muted text-start">
-                          {getDateLastEdit(article.timeCreate).date}
-                        </p>{" "}
-                      </div>
-                      <div className="col">
-                        <p className="card-subtitle text-muted text-end">
-                          {getDateLastEdit(article.timeCreate).time}
-                        </p>
-                      </div>
-                    </div>
-                    <div
-                      className="mt-3 alert alert-secondary"
-                      style={{ backgroundColor: "white" }}
-                    >
-                      <div>
-                        Last edit by{" "}
-                        <a
-                          href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${article.lastEditor}`}
-                          style={{ textDecoration: "underline" }}
-                        >
-                          {article.lastEditor}
-                        </a>
-                        <br />
-                        Edited on {getDateLastEdit(article.timeLastEdit).date}
-                        <br />
-                        Edit versions: {article.version}
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-          ))}
-      </div>
+    <div>
+      Tag:
+      {tagSelected}
     </div>
-  </div>
+    <ol>
+      {filteredArticlesByTag &&
+        filteredArticlesByTag.map((article, index) => (
+          <li key={article.articleId}>
+            <a
+              href={`#/${authorForWidget}/widget/SayALot_OneArticle?articleId=${article.articleId}&blockHeight=${article.blockHeight}&lastEditor=${article.lastEditor}
+            `}
+            >
+              {article.articleId}{" "}
+              <small>
+                (last edited:
+                {getDateLastEdit(article.timeLastEdit)})
+              </small>
+            </a>
+            <Widget
+              src={`${authorForWidget}/widget/SayALot_TagList`}
+              props={{ tags: article.tags }}
+            />
+          </li>
+        ))}
+    </ol>
+  </>
 );
