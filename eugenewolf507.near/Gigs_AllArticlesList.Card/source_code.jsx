b2cc1f4c9830be9f88ca28afb377ec3a2d9b4678
eugenewolf507.near <eugenewolf507.near>
2023-07-14 12:33:07 +0000

    Update Gigs_AllArticlesList.Card by eugenewolf507.near at 2023-07-14T12:33:07.157Z

--- eugenewolf507.near/Gigs_AllArticlesList.Card/source_code.jsx
+++ eugenewolf507.near/Gigs_AllArticlesList.Card/source_code.jsx
@@ -155,6 +155,7 @@ return (
               statusChangeHandler,
               statusTagsArr,
               doesUserCanChangeStatus,
+              closeModalHandler,
             }}
           />
         </Modal>
