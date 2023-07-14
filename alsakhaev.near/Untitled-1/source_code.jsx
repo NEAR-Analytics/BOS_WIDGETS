e31ff9fd07f60eb8966eed9e683268661a3094a5
alsakhaev.near <alsakhaev.near>
2023-07-14 12:40:32 +0000

    Update Untitled-1 by alsakhaev.near at 2023-07-14T12:40:32.701Z

--- alsakhaev.near/Untitled-1/source_code.jsx
+++ alsakhaev.near/Untitled-1/source_code.jsx
@@ -1,13 +1,1 @@
-return (
-  <div>
-    <Widget src="alsakhaev.near/widget/HelloWorldText" />
-    <Widget
-      src="alsakhaev.near/widget/ExampleButton"
-      props={{ label: "Button 1" }}
-    />
-    <Widget
-      src="alsakhaev.near/widget/ExampleButton"
-      props={{ label: "Button 2" }}
-    />
-  </div>
-);
+return <button>Hello World</button>;
