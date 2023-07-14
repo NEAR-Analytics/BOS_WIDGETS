fba2513871e75b4c3f90a7bf52001147cd67c94b
ndcplug.near <ndcplug.near>
2023-07-14 15:28:49 +0000

    Update ndc-badge-holders by ndcplug.near at 2023-07-14T15:28:49.206Z

--- ndcplug.near/ndc-badge-holders/source_code.jsx
+++ ndcplug.near/ndc-badge-holders/source_code.jsx
@@ -3,37 +3,130 @@ State.init({
   tokens: [],
   issuer: "community.i-am-human.near",
   collectionName: "OG",
+  sbtList: [],
 });
-// add conditional
-if (state.read_index != -1) {
-  const tokens = Near.view("registry.i-am-human.near", "sbt_tokens", {
-    issuer: state.issuer,
-    from_token: 1 + state.read_index * 100,
-    limit: 100,
-  });
 
-  if (tokens) {
-    if (!tokens.length) {
-      State.update({ read_index: -1 });
-    } else {
-      State.update({
-        read_index: state.read_index + 1,
-        tokens: [...state.tokens, ...tokens],
-      });
+// issuer.regens.near, community.i-am-human.near, fractal.i-am-human.near, issuer.proofofvibes.near
+// add conditional dropdown
+// future add classes
+// add drop down
+// add custom
+
+// Load badge helper functions
+function loadSBTs() {
+  const res = fetch(
+    "https://raw.githubusercontent.com/codingshot/ndc-landing/main/data/sbtList.json"
+  );
+  return res.body && JSON.parse(res.body);
+}
+const sbtList = loadSBTs();
+State.update({ sbtList: sbtList });
+if (!state.sbtList) {
+  return "⧗ Loading  SBT List...";
+}
+const handleIssuerChange = (e) => {
+  State.update({ issuer: e.target.value });
+  console.log("Issuer Address: " + state.issuer);
+  loadHolders();
+};
+
+const handleSbtChange = (e) => {
+  State.update({
+    issuer: sbtList
+      .filter((sbt) => sbt.title === e.target.value)
+      .map((el) => el.address)[0],
+  });
+  console.log(
+    sbtList
+      .filter((sbt) => sbt.title === e.target.value)
+      .map((el) => el.address)[0]
+  );
+  loadHolders();
+}; // need to change this around
+// if (state.read_index != -1) {
+//   const tokens = Near.view("registry.i-am-human.near", "sbt_tokens", {
+//     issuer: state.issuer,
+//     from_token: 1 + state.read_index * 100,
+//     limit: 100,
+//   });
+
+//   if (tokens) {
+//     if (!tokens.length) {
+//       State.update({ read_index: -1 });
+//     } else {
+//       State.update({
+//         read_index: state.read_index + 1,
+//         tokens: [...state.tokens, ...tokens],
+//       });
+//     }
+//   }
+// }
+// Load hodlers helper functions
+function renderHolders() {}
+function loadHolders() {
+  if (state.read_index != -1) {
+    const tokens = Near.view("registry.i-am-human.near", "sbt_tokens", {
+      issuer: state.issuer,
+      from_token: 1 + state.read_index * 100,
+      limit: 100,
+    });
+
+    if (tokens) {
+      if (!tokens.length) {
+        State.update({ read_index: -1 });
+      } else {
+        State.update({
+          read_index: state.read_index + 1,
+          tokens: [...state.tokens, ...tokens],
+        });
+      }
     }
   }
+  renderHolders();
 }
+loadHolders();
+
 console.log(tokens.length); // put in progress meter
 
 return (
   <div>
     <h1>NDC {state.collectionName} SBT Holders:</h1>
+    <label>Choose A NDC Community SBT Badge</label>
+    <div className="charities">
+      <select
+        className="form-select"
+        aria-label="select asset"
+        onChange={handleSbtChange}
+      >
+        <option selected disabled>
+          {" "}
+          Select a badge
+        </option>
+        {sbtList &&
+          sbtList.map((sbt) => (
+            <option value={sbt.title} selected={sbt.selected}>
+              {sbt.title}
+            </option>
+          ))}
+        // add receiver logic here
+      </select>
+    </div>
+    <div className="balance input-group">
+      <input
+        style={{ maxWidth: "100%" }}
+        type="string"
+        defaultValue={state.issuer} // feel like this wrong
+        value={state.issuer}
+        placeholder={state.issuer}
+        onChange={handleIssuerChange}
+      />
+    </div>
     <ol>
       {state.tokens.map((token) => (
         <li class="mb-2">
           <Widget
             src="chaotictempest.near/widget/AccountProfileCard"
-            props={{ accountId: token.owner, tooltip: true }}
+            props={{ accountId: token.owner }}
           />
         </li>
       ))}
