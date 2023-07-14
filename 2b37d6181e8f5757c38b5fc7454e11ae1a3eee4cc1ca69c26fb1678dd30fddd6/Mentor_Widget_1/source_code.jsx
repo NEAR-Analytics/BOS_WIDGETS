a5d860002da4cc8f09e78117a0389bc902a73d0e
2b37d6181e8f5757c38b5fc7454e11ae1a3eee4cc1ca69c26fb1678dd30fddd6 <2b37d6181e8f5757c38b5fc7454e11ae1a3eee4cc1ca69c26fb1678dd30fddd6>
2023-07-14 23:26:38 +0000

    Update Mentor_Widget_1 by 2b37d6181e8f5757c38b5fc7454e11ae1a3eee4cc1ca69c26fb1678dd30fddd6 at 2023-07-14T23:26:38.652Z

--- 2b37d6181e8f5757c38b5fc7454e11ae1a3eee4cc1ca69c26fb1678dd30fddd6/Mentor_Widget_1/source_code.jsx
+++ 2b37d6181e8f5757c38b5fc7454e11ae1a3eee4cc1ca69c26fb1678dd30fddd6/Mentor_Widget_1/source_code.jsx
@@ -110,7 +110,7 @@ const unlockedImage = "https://i.imgur.com/6qyuwCf.jpg";
 return (
   <div className="EventDetail container card shadow my-5 p-5">
     <div className="The image">
-      <img src="https://pasteboard.co/ov0QqqVG1lQs.png" />
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIFkJhfFnsBV7-Q-0v7mWIr7GCRKMsXdFCQQ&usqp=CAU" />
     </div>
     <h3
       style={{
