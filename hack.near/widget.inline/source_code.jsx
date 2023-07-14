754b46fec4ed0480a17588444ee6b3539f683642
hack.near <hack.near>
2023-07-14 21:08:16 +0000

    Update widget.inline by hack.near at 2023-07-14T21:08:16.952Z

--- hack.near/widget.inline/source_code.jsx
+++ hack.near/widget.inline/source_code.jsx
@@ -91,6 +91,18 @@ const Card = styled.div`
   padding: 23px;
 `;
 
+const StarButton = styled.div`
+  position: absolute;
+  top: 23px;
+  right: 18px;
+`;
+
+const ForkButton = styled.div`
+  position: absolute;
+  bottom: 23px;
+  right: 18px;
+`;
+
 return (
   <Card>
     <div className="m-1 mb-3 text-truncate">
@@ -131,5 +143,14 @@ return (
         />
       </small>
     </div>
+    <StarButton>
+      <Widget src="hack.near/widget/star.button" props={{ widgetPath }} />
+    </StarButton>
+    <ForkButton>
+      <a className="btn btn-outline-success" href={`#/edit/${widgetPath}`}>
+        <i className="bi bi-git me-1"></i>
+        {accountId === context.accountId ? "edit" : "fork"}
+      </a>
+    </ForkButton>
   </Card>
 );
