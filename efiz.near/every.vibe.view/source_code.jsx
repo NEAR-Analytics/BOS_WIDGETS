d9bc7bb2f22cbbd5ef89ec88b9f54fbb67613f1b
efiz.near <efiz.near>
2023-07-15 04:24:41 +0000

    Update every.vibe.view by efiz.near at 2023-07-15T04:24:41.229Z

--- efiz.near/every.vibe.view/source_code.jsx
+++ efiz.near/every.vibe.view/source_code.jsx
@@ -143,6 +143,10 @@ return (
         </div>
 
         <span className="text-nowrap text-muted">
+          <Widget
+            src="efiz.near/widget/SBT.Badge"
+            props={{ accountId: accountId }}
+          />
           <small>
             {blockHeight === "now" ? (
               "now"
@@ -152,31 +156,28 @@ return (
               </a>
             )}
           </small>
-          {metadata?.tastemaker?.length &&
-            metadata.tastmaker.includes(context.accountId) &&
-            blockHeight !== "now" && (
-              <span>
-                <a
-                  href="javascript:void"
-                  className="link-secondary ms-2"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <i className="fs-6 bi bi-three-dots" />
-                </a>
-                <ul className="dropdown-menu">
-                  <li className="dropdown-item">
-                    <a
-                      className="link-dark text-decoration-none"
-                      href={`${link}&raw=true`}
-                    >
-                      <i className="bi bi-emoji-sunglasses" /> Recommend as
-                      Vibee
-                    </a>
-                  </li>
-                </ul>
-              </span>
-            )}
+          {true && blockHeight !== "now" && (
+            <span>
+              <a
+                href="javascript:void"
+                className="link-secondary ms-2"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i className="fs-6 bi bi-three-dots" />
+              </a>
+              <ul className="dropdown-menu">
+                <li className="dropdown-item">
+                  <a
+                    className="link-dark text-decoration-none"
+                    href={`${link}&raw=true`}
+                  >
+                    <i className="bi bi-emoji-sunglasses" /> Recommend as Vibee
+                  </a>
+                </li>
+              </ul>
+            </span>
+          )}
         </span>
       </div>
     </div>
