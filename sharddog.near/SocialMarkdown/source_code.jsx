dd6c0a6834a04a5915dceb4b21cd3e59768c5c0c
sharddog.near <sharddog.near>
2023-07-15 11:50:33 +0000

    Update SocialMarkdown by sharddog.near at 2023-07-15T11:50:33.768Z

--- sharddog.near/SocialMarkdown/source_code.jsx
+++ sharddog.near/SocialMarkdown/source_code.jsx
@@ -15,6 +15,7 @@ const renderMention =
   ));
 
 const onHashtag = props.onHashtag;
+const onWidget = props.onWidget;
 
 const Wrapper = styled.div`
   word-break: break-word;
@@ -23,47 +24,13 @@ const Wrapper = styled.div`
   }
 `;
 
-const Embedded = styled.span`
-  white-space: normal;
-`;
-
-const blockquote = styled.span`
-  white-space: normal;
-`;
-
-const renderWidget =
-  props.renderWidget ??
-  props.onWidget ??
-  // URL pattern: scheme://authority/path?query#fragment
-  (({ url, scheme, authority, path, query }) => {
-    // widget URL now allows "bos" and "near" schemes
-    if (url && ["bos", "near"].includes(scheme) && authority && path) {
-      const location = authority + path;
-      const segments = location.split("/");
-      if (segments && segments.length >= 3) {
-        const src = segments.slice(segments.length - 3).join("/");
-        const props = {
-          ...{ markdown: props.text },
-          ...(query ?? {}),
-        };
-        return (
-          <Embedded className="embedded-widget">
-            <Widget key={url} src={src} props={props} />
-          </Embedded>
-        );
-      }
-    }
-    // If not a valid widget URL, return the original URL
-    return url;
-  });
-
 return (
   <Wrapper>
     <Markdown
       text={props.text}
       onMention={renderMention}
       onHashtag={onHashtag}
-      onWidget={renderWidget}
+      onWidget={onWidget}
     />
   </Wrapper>
 );
