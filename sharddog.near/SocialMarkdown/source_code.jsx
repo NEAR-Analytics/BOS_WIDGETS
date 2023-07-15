0da2c28f884bfacd48ca87302daf90fb4cf47396
sharddog.near <sharddog.near>
2023-07-15 11:50:33 +0000

    Update SocialMarkdown by sharddog.near at 2023-07-15T11:50:33.768Z

---
+++ sharddog.near/SocialMarkdown/source_code.jsx
@@ -0,0 +1,36 @@
+const renderMention =
+  props.renderMention ??
+  props.onMention ??
+  ((accountId) => (
+    <span key={accountId} className="d-inline-flex" style={{ fontWeight: 500 }}>
+      <Widget
+        src="mob.near/widget/ProfileLine"
+        props={{
+          accountId: accountId.toLowerCase(),
+          hideAccountId: true,
+          tooltip: true,
+        }}
+      />
+    </span>
+  ));
+
+const onHashtag = props.onHashtag;
+const onWidget = props.onWidget;
+
+const Wrapper = styled.div`
+  word-break: break-word;
+  p {
+    white-space: pre-line;
+  }
+`;
+
+return (
+  <Wrapper>
+    <Markdown
+      text={props.text}
+      onMention={renderMention}
+      onHashtag={onHashtag}
+      onWidget={onWidget}
+    />
+  </Wrapper>
+);
