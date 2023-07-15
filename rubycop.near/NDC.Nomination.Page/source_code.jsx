7f19a0b28443d271a9f1218efbf054abaeec3731
rubycop.near <rubycop.near>
2023-07-15 22:27:51 +0000

    Update NDC.Nomination.Page by rubycop.near at 2023-07-15T22:27:51.674Z

--- rubycop.near/NDC.Nomination.Page/source_code.jsx
+++ rubycop.near/NDC.Nomination.Page/source_code.jsx
@@ -20,9 +20,10 @@ const houses = [
 
 const widgets = {
   header: "syi216.near/widget/NDC.Nomination.Header",
+  card: "syi216.near/widget/NDC.nomination.card",
   houses: "rubycop.near/widget/NDC.Elections.Houses",
   filter: "rubycop.near/widget/NDC.Elections.Filter",
-  card: "syi216.near/widget/NDC.nomination.card",
+  button: "rubycop.near/widget/NDC.StyledComponents",
 };
 
 State.init({
@@ -182,17 +183,16 @@ const ActivityContainer = styled.div`
 
 const Left = styled.div`
   padding: 20px;
-  background: #F8F8F9;
+  background: #f8f8f9;
   border-radius: 8px;
 `;
 
-const Center = styled.div`
-`;
+const Center = styled.div``;
 
 const Right = styled.div`
   padding: 20px;
   margin-bottom: 20px;
-  background: #F8F8F9;
+  background: #f8f8f9;
   border-radius: 8px;
 `;
 
@@ -201,315 +201,252 @@ const H5 = styled.h5`
 `;
 
 const VerifiedDiv = styled.div`
-display: flex;
-width: 100%;
-padding: 16px;
-flex-direction: column;
-align-items: flex-start;
-gap: 20px;
-border-radius: 8px;
-background: var(--ffffff, #FFF);
-box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.10);
+  display: flex;
+  width: 100%;
+  padding: 16px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+  border-radius: 8px;
+  background: var(--ffffff, #fff);
+  box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.1);
 `;
 
 const VerifiedHeader = styled.div`
-display: flex;
-align-items: flex-start;
-gap: 16px;
-align-self: stretch;
+  display: flex;
+  align-items: flex-start;
+  gap: 16px;
+  align-self: stretch;
 `;
 
 const VerifiedHeaderContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: flex-start;
-gap: 4px;
-flex: 1 0 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+  flex: 1 0 0;
 `;
 
 const VerfiedTitle = styled.p`
-display: flex;
-width: 176px;
-flex-direction: column;
-justify-content: center;
-color: var(--000000, #000);
-font-size: 14px;
-font-style: normal;
-font-weight: 500;
-line-height: 120%;
-margin: 0px;
+  display: flex;
+  width: 176px;
+  flex-direction: column;
+  justify-content: center;
+  color: var(--000000, #000);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 120%;
+  margin: 0px;
 `;
 
 const VerifedDesc = styled.p`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-self: stretch;
-color: var(--primary-gray-dark, #828688);
-font-size: 12px;
-font-style: normal;
-font-weight: 500;
-line-height: 120%;
-margin: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: stretch;
+  color: var(--primary-gray-dark, #828688);
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 120%;
+  margin: 0px;
 `;
 
 const VerifyButton = styled.a`
-display: flex;
-padding: 8px 20px;
-justify-content: center;
-width: 100%;
-align-items: center;
-gap: 10px;
-align-self: stretch;
-border-radius: 10px;
-background: var(--ffd-50-d, #FFD50D);
-border:0px;
-text-decoration: none;
+  display: flex;
+  padding: 8px 20px;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  gap: 10px;
+  align-self: stretch;
+  border-radius: 10px;
+  background: var(--ffd-50-d, #ffd50d);
+  border: 0px;
+  text-decoration: none;
 `;
 
 const VerifyButtonText = styled.p`
-color: var(--primary-black, #000);
-font-size: 14px;
-font-style: normal;
-font-weight: 500;
-line-height: 24px;
-margin: 0px;
+  color: var(--primary-black, #000);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+  margin: 0px;
 `;
 
 const SortButton = styled.button`
-display: flex;
-width: 38px;
-height: 38px;
-padding: 8px 12px;
-justify-content: center;
-align-items: center;
-gap: 6px;
-border-radius: 6px;
-background: var(--buttons-gradient-default, linear-gradient(90deg, #9333EA 0%, #4F46E5 100%));
-border: 0px;
+  display: flex;
+  width: 38px;
+  height: 38px;
+  padding: 8px 12px;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  border-radius: 6px;
+  background: var(
+    --buttons-gradient-default,
+    linear-gradient(90deg, #9333ea 0%, #4f46e5 100%)
+  );
+  border: 0px;
 `;
 
 const ButtonNominateContainer = styled.div`
-display: flex;
-flex-direction:row;
-padding: 12px 16px;
-align-items: center;
-gap: 8px;
-border-radius: 8px;
-background: #F8F8F9;
-@media only screen and (max-width: 480px){
-  width: 100%;
-  flex-direction: column;
-}
-`;
-
-const ButtonDeleteDiv = styled.button`
-display: flex;
-height: 40px;
-padding: 8px 12px;
-align-items: center;
-gap: 6px;
-border-radius: 10px;
-border: 1px solid #C23F38;
-background: #F1D6D5;
-@media only screen and (max-width: 480px){
-  width: 100%;
-  justify-content: center;
-}
-`;
-
-const ButtonDeleteText = styled.p`
-color: #C23F38;
-font-size: 14px;
-font-style: normal;
-font-weight: 500;
-line-height: 24px;
-margin: 0px;
-`;
-
-const ButtonDeleteIcon = styled.img`
-width: 18px;
-height: 18px;
-`;
-
-const ButtonNominateDiv = styled.button`
-display: flex;
-height: 40px;
-padding: 8px 12px;
-align-items: center;
-gap: 6px;
-border: 1px solid #FFD50D;
-border-radius: 10px;
-background: var(--buttons-yellow-default, #FFD50D);
-@media only screen and (max-width: 480px){
-  width: 100%;
-  justify-content: center;
-}
-`;
-
-const ButtonNominateText = styled.p`
-margin: 0px;
-color: var(--primary-black, #000);
-font-size: 14px;
-font-style: normal;
-font-weight: 500;
-line-height: 24px;
-`;
-
-const ButtonNominateIcon = styled.img`
-width: 18px;
-height: 18px;
+  padding: 16px;
+  border-radius: 8px;
+  background: #f8f8f9;
 `;
 
 const Filter = styled.div`
   margin-top: 32px;
 
-  @media only screen and (max-width: 1061px){
+  @media only screen and (max-width: 1061px) {
     flex-direction: column;
   }
 `;
 
 const Toolbar = styled.div`
   margin-left: 20px;
-  @media only screen and (max-width: 1061px){
+  @media only screen and (max-width: 1061px) {
     margin: 10px 0 0 0;
   }
 `;
 
 return (
-  <div>
-    {houses.map((group) => (
-      <>
-        {group.id === state.selectedHouse && (
-          <Widget
-            key={i}
-            src={widgets.header}
-            props={{
-              startTime: group.start,
-              endTime: group.end,
-              type: "Nomination",
-            }}
-          />
-        )}
-      </>
-    ))}
-    <Filter className="d-flex">
-      <div className="w-100">
-        <Widget
-          src={widgets.filter}
-          props={{
-            handleFilter,
-            candidateId: state.candidateId,
-            placeholder: "Search by candidate name and affiliation",
-          }}
-        />
-      </div>
-      <Toolbar>
-        {state.showModal && (
-          <Widget
-            src={`dokxo.near/widget/NDC.Nomination.Compose`}
-            props={{
-              candidateOrReplay: true,
-              onClickConfirm: () => State.update({ showModal: false }),
-              onClickCancel: () => State.update({ showModal: false }),
-            }}
-          />
-        )}
-        {state.showModalDelete && (
-          <Widget
-            src={`dokxo.near/widget/NDC.Nomination.DeleteNomination`}
-            props={{
-              candidateOrReplay: true,
-              onClickConfirm: () => State.update({ showModalDelete: false }),
-              onClickCancel: () => State.update({ showModalDelete: false }),
-            }}
-          />
-        )}
-        {state.og ? (
-          <ButtonNominateContainer>
-            {state.selfNomination ? (
-              <ButtonDeleteDiv
-                onClick={async () => {
-                  console.log("entra a delete modal");
-                  State.update({ showModalDelete: true });
-                }}
-              >
-                <ButtonDeleteText>Delete Self Nomination</ButtonDeleteText>
-                <ButtonDeleteIcon
-                  src="https://apricot-straight-eagle-592.mypinata.cloud/ipfs/Qma7DF8kyoGN4Mf3Yty5uoP64RpZewCsZFawae4Ux4wBBF?_gl=1*6fastp*_ga*MzkyOTE0Mjc4LjE2ODY4NjgxODc.*_ga_5RMPXG14TE*MTY4ODQxMzUxMS43LjEuMTY4ODQxMzUzMi4zOS4wLjA."
-                  alt="pic"
-                ></ButtonDeleteIcon>
-              </ButtonDeleteDiv>
-            ) : (
-              <ButtonNominateDiv
-                onClick={async () => {
-                  !status.sbt ? State.update({ showModal: true }) : "";
-                }}
-              >
-                <ButtonNominateText>Self Nominate</ButtonNominateText>
-                <ButtonNominateIcon
-                  src="https://apricot-straight-eagle-592.mypinata.cloud/ipfs/QmPRtMgbzoPxsuLLYdntJzEUDLZdndSiWWvMw4VZYozd29?_gl=1*1loq8cw*_ga*MzkyOTE0Mjc4LjE2ODY4NjgxODc.*_ga_5RMPXG14TE*MTY4ODQxMzUxMS43LjEuMTY4ODQxNTA1MC42MC4wLjA."
-                  alt="pic"
-                ></ButtonNominateIcon>
-              </ButtonNominateDiv>
-            )}
-          </ButtonNominateContainer>
-        ) : (
-          <></>
-        )}
-      </Toolbar>
-    </Filter>
-    <Container className="d-flex row">
-      <Left className="col-lg">
-        <H5>Houses</H5>
-        <Widget
-          src={widgets.houses}
-          props={{
-            selectedHouse: state.selectedHouse,
-            houses: houses,
-            handleSelect: (item) => handleSelect(item),
-          }}
-        />
-        <div>
-          {state.sbt ? (
-            ""
-          ) : (
-            <VerifiedDiv>
-              <VerifiedHeader>
-                <VerifiedHeaderContainer>
-                  <VerfiedTitle>To Comment or to Upvote</VerfiedTitle>
-                  <VerifedDesc>
-                    Verify as a Human to comment or to upvote
-                  </VerifedDesc>
-                </VerifiedHeaderContainer>
-              </VerifiedHeader>
-              <VerifyButton
-                className="text-decoration-none"
-                href="https://i-am-human.app/"
-                target="_blank"
-                rel="noopener"
-              >
-                <VerifyButtonText>Verify as a Human</VerifyButtonText>
-              </VerifyButton>
-            </VerifiedDiv>
+  <>
+    <div>
+      {houses.map((group) => (
+        <>
+          {group.id === state.selectedHouse && (
+            <Widget
+              key={i}
+              src={widgets.header}
+              props={{
+                startTime: group.start,
+                endTime: group.end,
+                type: "Nomination",
+              }}
+            />
           )}
+        </>
+      ))}
+      <Filter className="d-flex">
+        <div className="w-100">
+          <Widget
+            src={widgets.filter}
+            props={{
+              handleFilter,
+              candidateId: state.candidateId,
+              placeholder: "Search by candidate name and affiliation",
+            }}
+          />
         </div>
-      </Left>
-      <Center className="col-lg-9 px-2 px-md-3 d-flex flex-row flex-wrap ">
-        {state.nominations.length > 0 ? (
-          state.nominations.map((data) => {
-            return (
-              <>
-                <Widget src={widgets.card} props={data} />
-              </>
-            );
-          })
-        ) : (
-          <div className="flex mt-10 container-fluid align-self-center">
-            <H5 className="text-center">{state.notFound}</H5>
+        <Toolbar>
+          {state.og && (
+            <ButtonNominateContainer>
+              {state.selfNomination ? (
+                <Widget
+                  src={widgets.button}
+                  props={{
+                    Button: {
+                      className: "danger",
+                      text: "Delete Self Nomination",
+                      onClick: () => State.update({ showModalDelete: true }),
+                      icon: <i class="bi bi-trash"></i>,
+                    },
+                  }}
+                />
+              ) : (
+                <Widget
+                  src={widgets.button}
+                  props={{
+                    Button: {
+                      text: "Self Nominate",
+                      onClick: () =>
+                        !status.sbt ? State.update({ showModal: true }) : "",
+                      icon: <i class="bi bi-plus-lg"></i>,
+                    },
+                  }}
+                />
+              )}
+            </ButtonNominateContainer>
+          )}
+        </Toolbar>
+      </Filter>
+      <Container className="d-flex row">
+        <Left className="col-lg">
+          <H5>Houses</H5>
+          <Widget
+            src={widgets.houses}
+            props={{
+              selectedHouse: state.selectedHouse,
+              houses: houses,
+              handleSelect: (item) => handleSelect(item),
+            }}
+          />
+          <div>
+            {state.sbt ? (
+              ""
+            ) : (
+              <VerifiedDiv>
+                <VerifiedHeader>
+                  <VerifiedHeaderContainer>
+                    <VerfiedTitle>To Comment or to Upvote</VerfiedTitle>
+                    <VerifedDesc>
+                      Verify as a Human to comment or to upvote
+                    </VerifedDesc>
+                  </VerifiedHeaderContainer>
+                </VerifiedHeader>
+                <VerifyButton
+                  className="text-decoration-none"
+                  href="https://i-am-human.app/"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <VerifyButtonText>Verify as a Human</VerifyButtonText>
+                </VerifyButton>
+              </VerifiedDiv>
+            )}
           </div>
-        )}
-      </Center>
-    </Container>
-  </div>
+        </Left>
+        <Center className="col-lg-9 px-2 px-md-3 d-flex flex-row flex-wrap ">
+          {state.nominations.length > 0 ? (
+            state.nominations.map((data) => {
+              return (
+                <>
+                  <Widget src={widgets.card} props={data} />
+                </>
+              );
+            })
+          ) : (
+            <div className="flex mt-10 container-fluid align-self-center">
+              <H5 className="text-center">{state.notFound}</H5>
+            </div>
+          )}
+        </Center>
+      </Container>
+    </div>
+
+    <>
+      {state.showModal && (
+        <Widget
+          src={`rubycop.near/widget/NDC.Nomination.Compose`}
+          props={{
+            candidateOrReplay: true,
+            handleClose: () => State.update({ showModal: false }),
+          }}
+        />
+      )}
+      {state.showModalDelete && (
+        <Widget
+          src={`rubycop.near/widget/NDC.Nomination.DeleteNomination`}
+          props={{
+            candidateOrReplay: true,
+            handleClose: () => State.update({ showModalDelete: false }),
+          }}
+        />
+      )}
+    </>
+  </>
 );
