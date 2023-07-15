ddf6973f9dda9fc4e1390c4cd61c430d43860915
mintlu.near <mintlu.near>
2023-07-15 16:58:12 +0000

    Update kp-ticket2 by mintlu.near at 2023-07-15T16:58:12.537Z

---
+++ mintlu.near/kp-ticket2/source_code.jsx
@@ -0,0 +1,36 @@
+const qrPayload = `${props.key}/${props.pk}`;
+
+const qrSourceImg = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${qrPayload}&format=svg`;
+
+return (
+  <div className="container row mt-3 mb-3">
+    <div align="center">
+      <div
+        style={{
+          backgroundColor: "#ecc2ff",
+          width: "300px",
+          height: "450px",
+          border: "5px solid black",
+          borderRadius: "25px",
+        }}
+      >
+        <br />
+        <br />
+        <text style={{ fontSize: 26, fontFamily: "Avenir" }}>
+          🎟️ This is your Ticket 🔑
+        </text>
+        <br />
+        <img src={qrSourceImg} alt="" title="" style={{ paddingTop: 15 }} />
+        <br />
+        <br />
+        <text style={{ fontSize: 18, fontFamily: "Avenir" }}>
+          Hosted by Keypom @ UW E7
+        </text>
+        <br></br>
+        <text style={{ fontSize: 16, fontFamily: "Avenir" }}>
+          June 22nd 2023 9pm
+        </text>
+      </div>
+    </div>
+  </div>
+);
