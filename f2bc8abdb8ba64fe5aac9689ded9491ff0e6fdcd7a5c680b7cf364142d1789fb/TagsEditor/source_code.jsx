b9e68b68c45f453759e81cdfa3a285560ee2a244
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-07-15 01:10:31 +0000

    Update TagsEditor by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-07-15T01:10:31.145Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/TagsEditor/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/TagsEditor/source_code.jsx
@@ -13,7 +13,7 @@ const normalizeTag = (tag) =>
     .trim("-");
 
 const hasModifications =
-  Object.keys(initialTagsObject).map(normalizeTag) != state.tags;
+  Object.keys(initialTagsObject).map(normalizeTag) !== state.tags;
 console.log(1, hasModifications);
 console.log(2, Object.keys(initialTagsObject).map(normalizeTag));
 console.log(3, state.tags);
