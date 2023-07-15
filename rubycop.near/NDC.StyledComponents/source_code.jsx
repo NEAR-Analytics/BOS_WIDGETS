93250c400789e49de4b3cebfba71ecd3d46ec0c4
rubycop.near <rubycop.near>
2023-07-15 13:07:52 +0000

    Update NDC.StyledComponents by rubycop.near at 2023-07-15T13:07:52.345Z

--- rubycop.near/NDC.StyledComponents/source_code.jsx
+++ rubycop.near/NDC.StyledComponents/source_code.jsx
@@ -22,27 +22,72 @@ const StyledButton = styled.button`
     border: 1px solid #FFD50D;
   }
 
+  &:disabled {
+    background: rgb(255 234 119);
+  }
+
   i {
     margin: 0 0 0 5px;
     &:before { vertical-align: -0.2rem; }
   }
 `;
 
+const Container = styled.div`
+  button {
+    margin-bottom:10px;
+  }
+`;
+
+const RenderButton = (props) => (
+  <StyledButton
+    size={props.size}
+    className={`align-items-center d-flex ${props.className ?? "primary"}`}
+    onClick={props.onClick}
+    disabled={props.disabled}
+  >
+    <div>{props.text}</div>
+    {props.icon && (
+      <div className={`${props.size === "sm" ? "fs-7" : "fs-5"}`}>
+        {props.icon}
+      </div>
+    )}
+  </StyledButton>
+);
+
 if (Button)
   return (
-    <StyledButton
+    <RenderButton
       size={Button.size}
-      className={`align-items-center d-flex ${Button.className ?? "primary"}`}
+      className={Button.className}
       onClick={Button.onClick}
       disabled={Button.disabled}
-    >
-      <div>{Button.text}</div>
-      {Button.icon && (
-        <div className={`${Button.size === "sm" ? "fs-7" : "fs-5"}`}>
-          {Button.icon}
-        </div>
-      )}
-    </StyledButton>
+      text={Button.text}
+      icon={Button.icon}
+    />
   );
 
-return "";
+return (
+  <Container>
+    <h4>Buttons</h4>
+    <RenderButton text="Primary" />
+    <RenderButton
+      text="Primary with Icon"
+      icon={<i class="bi bi-check-lg"></i>}
+    />
+    <RenderButton disabled={true} text="Primary disabled" />
+    <RenderButton size="sm" text="Primary small" />
+
+    <RenderButton className="secondary" text="Secondary" />
+    <RenderButton
+      className="secondary"
+      text="Secondary with Icon"
+      icon={<i class="bi bi-check-lg"></i>}
+    />
+    <RenderButton
+      className="secondary"
+      disabled={true}
+      text="Secondary disabled"
+    />
+    <RenderButton className="secondary" size="sm" text="Secondary small" />
+  </Container>
+);
