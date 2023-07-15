220e939f14449be61f79db4aa13620f27bd42db4
hack.near <hack.near>
2023-07-15 06:51:54 +0000

    Update widget.inline by hack.near at 2023-07-15T06:51:54.246Z

--- hack.near/widget.inline/source_code.jsx
+++ hack.near/widget.inline/source_code.jsx
@@ -126,7 +126,7 @@ return (
         </StarButton>
       </div>
     </div>
-    <div className="m-3">
+    <div className="m-3 mt-2">
       <h5 className="card-title mb-3">
         <a className="stretched-link" href={`#/${widgetPath}`}>
           {name}
