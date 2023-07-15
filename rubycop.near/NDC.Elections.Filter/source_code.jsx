6a93b274ca26bb51064e9640373dde06f5fd3f37
rubycop.near <rubycop.near>
2023-07-15 11:21:08 +0000

    Update NDC.Elections.Filter by rubycop.near at 2023-07-15T11:21:08.034Z

--- rubycop.near/NDC.Elections.Filter/source_code.jsx
+++ rubycop.near/NDC.Elections.Filter/source_code.jsx
@@ -1,18 +1,31 @@
-const { handleFilter, candidateId } = props;
+const { handleFilter, candidateId, placeholder } = props;
 
 const Container = styled.div`
     padding: 16px;
     border-radius: 8px;
     background: #F8F8F9;
+
+    i {
+      position: absolute;
+      top: 7px;
+      left: 15px;
+    }
+
+    input {
+      padding-left: 40px;
+    }
 `;
 
 return (
   <Container>
-    <input
-      placeholder="Search by candidate name"
-      className="form-control w-100"
-      value={candicateId}
-      onChange={handleFilter}
-    />
+    <div className="position-relative">
+      <i className="bi bi-search text-secondary"></i>
+      <input
+        placeholder={placeholder}
+        className="form-control w-100"
+        value={candicateId}
+        onChange={handleFilter}
+      />
+    </div>
   </Container>
 );
