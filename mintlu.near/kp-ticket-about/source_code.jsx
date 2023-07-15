9a656e7409869b6e17d7c8b6915e9b1648488298
mintlu.near <mintlu.near>
2023-07-15 19:07:34 +0000

    Update kp-ticket-about by mintlu.near at 2023-07-15T19:07:34.056Z

---
+++ mintlu.near/kp-ticket-about/source_code.jsx
@@ -0,0 +1,50 @@
+const textStyle = {
+  fontFamily: "Avenir, sans-serif",
+  fontSize: "28px",
+  color: "#333",
+  lineHeight: "1.5",
+  margin: "0",
+  padding: "0",
+};
+
+const textStyle2 = {
+  fontFamily: "Avenir, sans-serif",
+  fontSize: "20px",
+  color: "#333",
+  lineHeight: "1.5",
+  margin: "0",
+  padding: "0",
+};
+
+const textStyle3 = {
+  fontFamily: "Avenir, sans-serif",
+  fontSize: "18px",
+  color: "#333",
+  lineHeight: "1.5",
+  margin: "0",
+  padding: "0",
+};
+
+return (
+  <div style={{ textAlign: "center", background: "#fff", padding: "20px" }}>
+    <h2 style={textStyle}>Keypom BOS Ticketing dApp</h2>
+    <br></br>
+    <p style={textStyle2}>
+      The contents of this dApp are based on the
+      <a href="https://docs.keypom.xyz/docs/next/Tutorials/Advanced/ticketing/introduction">
+        Ticketing Tutorial
+      </a>
+    </p>
+    <br></br>
+    <p style={textStyle2}>
+      For Dev Support, join the{" "}
+      <a href="https://nearbuilders.com/tg-keypom">Keypom Builders</a> Telegram
+      chat.
+    </p>
+    <br></br>
+    <p style={textStyle3}>
+      Special Thanks to Kent from NEAR Vietnam <br></br>
+      He is a Legend 🐐
+    </p>
+  </div>
+);
