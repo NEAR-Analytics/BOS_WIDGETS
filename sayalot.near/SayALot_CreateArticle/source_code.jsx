3078743d2884e478bc59ff4a55d5af6aef40fefd
sayalot.near <sayalot.near>
2023-07-16 23:56:27 +0000

    Update SayALot_CreateArticle by sayalot.near at 2023-07-16T23:56:27.077Z

--- sayalot.near/SayALot_CreateArticle/source_code.jsx
+++ sayalot.near/SayALot_CreateArticle/source_code.jsx
@@ -1,4 +1,6 @@
-const addressForArticles = "sayALotArticle";
+const isDebug = props.isDebug;
+
+const addressForArticles = isDebug ? "test_sayALotArticle" : "sayALotArticle";
 const authorForWidget = "sayalot.near";
 const accountId = props.accountId ?? context.accountId;
 if (!accountId) {
@@ -11,6 +13,215 @@ if (profile === null) {
 
 const initialBody = `# h1 Heading 8-) 
 ## h2 Heading 
+### h3 Heading 
+#### h4 Heading 
+##### h5 Heading 
+###### h6 Heading 
+ 
+ 
+## Horizontal Rules 
+ 
+___ 
+ 
+--- 
+ 
+*** 
+ 
+ 
+## Typographic replacements 
+ 
+Enable typographer option to see result. 
+ 
+(c) (C) (r) (R) (tm) (TM) (p) (P) +- 
+ 
+test.. test... test..... test?..... test!.... 
+ 
+!!!!!! ???? ,,  -- --- 
+ 
+"Smartypants, double quotes" and 'single quotes' 
+ 
+ 
+## Emphasis 
+ 
+This is bold text 
+ 
+This is bold text 
+ 
+*This is italic text* 
+ 
+_This is italic text_ 
+ 
+Strikethrough 
+ 
+ 
+## Blockquotes 
+ 
+ 
+> Blockquotes can also be nested... 
+>> ...by using additional greater-than signs right next to each other... 
+> > > ...or with spaces between arrows. 
+ 
+ 
+## Lists 
+ 
+Unordered 
+ 
++ Create a list by starting a line with \`+\`, \`-\`, or \`*\` 
++ Sub-lists are made by indenting 2 spaces: 
+  - Marker character change forces new list start: 
+    * Ac tristique libero volutpat at 
+    + Facilisis in pretium nisl aliquet 
+    - Nulla volutpat aliquam velit 
++ Very easy! 
+ 
+Ordered 
+ 
+1. Lorem ipsum dolor sit amet 
+2. Consectetur adipiscing elit 
+3. Integer molestie lorem at massa 
+ 
+ 
+1. You can use sequential numbers... 
+1. ...or keep all the numbers as \`1.\` 
+ 
+Start numbering with offset: 
+ 
+57. foo 
+1. bar 
+ 
+ 
+## Code 
+ 
+Inline \`code\` 
+ 
+Indented code 
+ 
+    // Some comments 
+    line 1 of code 
+    line 2 of code 
+    line 3 of code 
+ 
+ 
+Block code "fences" 
+ 
+\`\`\` 
+Sample text here... 
+\`\`\` 
+ 
+Syntax highlighting 
+ 
+\`\`\` js 
+var foo = function (bar) { 
+  return bar++; 
+}; 
+ 
+console.log(foo(5)); 
+\`\`\` 
+ 
+## Tables 
+ 
+| Option | Description | 
+| ------ | ----------- | 
+| data   | path to data files to supply the data that will be passed into templates. | 
+| engine | engine to be used for processing templates. Handlebars is the default. | 
+| ext    | extension to be used for dest files. | 
+ 
+Right aligned columns 
+ 
+| Option | Description | 
+| ------:| -----------:| 
+| data   | path to data files to supply the data that will be passed into templates. | 
+| engine | engine to be used for processing templates. Handlebars is the default. | 
+| ext    | extension to be used for dest files. | 
+ 
+ 
+## Links 
+ 
+link text 
+ 
+link with title 
+ 
+Autoconverted link https://github.com/nodeca/pica (enable linkify to see) 
+ 
+ 
+## Images 
+ 
+!Minion 
+!Stormtroopocat 
+ 
+Like links, Images also have a footnote style syntax 
+ 
+![Alt text][id] 
+ 
+With a reference later in the document defining the URL location: 
+ 
+[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat" 
+ 
+### Emojies 
+ 
+> Classic markup: :wink: :crush: :cry: :tear: :laughing: :yum: 
+> 
+> Shortcuts (emoticons): :-) :-( 8-) ;) 
+ 
+see how to change output with twemoji. 
+ 
+ 
+### Subscript / Superscript 
+ 
+- 19^th^ 
+- H~2~O 
+ 
+ 
+### \<ins> 
+ 
+++Inserted text++ 
+ 
+ 
+### \<mark> 
+ 
+==Marked text== 
+ 
+ 
+### Footnotes 
+ 
+Footnote 1 link[^first]. 
+ 
+Footnote 2 link[^second]. 
+ 
+Inline footnote^[Text of inline footnote] definition. 
+ 
+Duplicated footnote reference[^second]. 
+ 
+[^first]: Footnote can have markup 
+ 
+    and multiple paragraphs. 
+ 
+[^second]: Footnote text. 
+ 
+ 
+### [Definition lists](https://github.com/markdown-it/markdown-it-deflist) 
+ 
+Term 1 
+ 
+:   Definition 1 
+with lazy continuation. 
+ 
+Term 2 with *inline markup* 
+ 
+:   Definition 2 
+ 
+        { some code, part of Definition 2 } 
+ 
+    Third paragraph of definition 2. 
+ 
+_Compact style:_ 
+ 
+Term 1 
+  ~ Definition 1 
+ 
+Term 2 
+  ~ Definition 2a 
+  ~ Definition 2b 
 `;
 
 const errTextNoBody = "ERROR: no article Body",
@@ -27,7 +238,7 @@ const initialCreateArticleState = {
 };
 
 State.init(initialCreateArticleState);
-const tagsArray = state.tags ? state.tags : [];
+const tagsArray = state.tags ? Object.keys(state.tags) : [];
 
 const getArticleData = () => {
   const args = {
@@ -45,19 +256,36 @@ const getArticleData = () => {
 };
 
 const composeData = () => {
-  const data = {
-    sayALotArticle: {
-      main: JSON.stringify(getArticleData()),
-    },
-    index: {
-      sayALotArticle: JSON.stringify({
-        key: "main",
-        value: {
-          type: "md",
-        },
-      }),
-    },
-  };
+  let data;
+  if (isDebug) {
+    data = {
+      test_sayALotArticle: {
+        main: JSON.stringify(getArticleData()),
+      },
+      index: {
+        test_sayALotArticle: JSON.stringify({
+          key: "main",
+          value: {
+            type: "md",
+          },
+        }),
+      },
+    };
+  } else {
+    data = {
+      sayALotArticle: {
+        main: JSON.stringify(getArticleData()),
+      },
+      index: {
+        sayALotArticle: JSON.stringify({
+          key: "main",
+          value: {
+            type: "md",
+          },
+        }),
+      },
+    };
+  }
 
   if (tagsArray.length) {
     data.index.tag = JSON.stringify(
@@ -79,9 +307,9 @@ const saveHandler = (e) => {
   });
   if (state.articleId && state.articleBody) {
     // TODO check it automaticle
-    const isArticleIdDuplicated = false;
+    const isArticleIdDublicated = false;
 
-    if (!isArticleIdDuplicated) {
+    if (!isArticleIdDublicated) {
       const newData = composeData();
 
       State.update({ saving: true });
@@ -165,7 +393,11 @@ return (
           width: "100%",
           backdropFilter: "blur(5px)",
         }}
-        href={`https://near.social/#/${authorForWidget}/widget/SayALot`}
+        href={
+          isDebug
+            ? `https://near.social/#/${authorForWidget}/widget/SayALot_OneArticle?articleId=${state.articleId}&lastEditor=${accountId}&isDebug=${isDebug}&blockHeight=now`
+            : `https://near.social/#/${authorForWidget}/widget/SayALot_OneArticle?articleId=${state.articleId}&lastEditor=${accountId}&blockHeight=now`
+        }
       >
         <div
           style={{
@@ -238,12 +470,12 @@ return (
         <div className="d-flex flex-column pt-3">
           {!state.saveComplete && (
             <Widget
-              src="mob.near/widget/TagsEditor"
+              src="f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/TagsEditor"
               props={{
                 initialTagsObject: state.tags,
                 placeholder: "Input tags",
                 setTagsObject: (tags) => {
-                  state.tags = Object.keys(tags);
+                  state.tags = tags;
                   State.update();
                 },
               }}
