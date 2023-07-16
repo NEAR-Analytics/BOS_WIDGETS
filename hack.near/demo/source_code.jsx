2f9bead44a982d4a207792bb5aa1d27ffb292588
hack.near <hack.near>
2023-07-16 06:12:44 +0000

    Update demo by hack.near at 2023-07-16T06:12:44.118Z

--- hack.near/demo/source_code.jsx
+++ hack.near/demo/source_code.jsx
@@ -1,2 +1,2 @@
-const accountId = props.accountId ?? context.accountId; const ownerId = props.ownerId ?? "hack.near"; const pageId = props.pageId ?? "community.page"; return <Widget src=hack.near/widget/demo props={{ accountId, communityId: multi.near, contractId: mint.sharddog.near, h1: Communities,
-        h2: DAO, tagline: Connect, Collaborate, and Create!, bgColor: , buttonText: Get Started, link: https://near.social }} />
+const accountId = props.accountId ?? context.accountId; const ownerId = props.ownerId ?? "hack.near"; const pageId = props.pageId ?? "community.page"; return <Widget src="hack.near/widget/community.page" props={{ accountId, communityId: multi.near, contractId: mint.sharddog.near, h1: Communities,
+        h2: DAO, tagline: Connect, Cooperate, and Create!, bgColor: , buttonText: Get Started, link: https://near.social }} />
