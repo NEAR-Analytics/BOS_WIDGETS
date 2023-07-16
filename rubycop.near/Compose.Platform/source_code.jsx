e2d04052a2b4bda858abcf8895472fceb0e1a66f
rubycop.near <rubycop.near>
2023-07-16 15:58:56 +0000

    Update Compose.Platform by rubycop.near at 2023-07-16T15:58:56.538Z

--- rubycop.near/Compose.Platform/source_code.jsx
+++ rubycop.near/Compose.Platform/source_code.jsx
@@ -3,24 +3,22 @@ const { inputs } = props;
 const Section = styled.div`
   margin-bottom: 10px;
 `;
-
+console.log(inputs);
 return (
   <div>
     {inputs.map((input, i) => {
-      <Section key={i}>
-        <Widget
-          src={"rubycop.near/widget/NDC.StyledComponents"}
-          props={{
-            TextArea: {
-              label: input.label,
-              placeholder: input.placeholder,
-              maxLength: 2000,
-              value: input.value,
-              handleChange: input.handleChange,
-            },
-          }}
-        />
-      </Section>;
+      <Widget
+        src={"rubycop.near/widget/NDC.StyledComponents"}
+        props={{
+          TextArea: {
+            label: "input.label",
+            placeholder: "input.placeholder",
+            maxLength: 2000,
+            value: "input.value",
+            handleChange: () => {},
+          },
+        }}
+      />;
     })}
   </div>
 );
