756f98d9339d52fe95d77c19dcf135afce643780
hack.near <hack.near>
2023-07-16 06:07:18 +0000

    Update demo by hack.near at 2023-07-16T06:07:18.298Z

---
+++ hack.near/demo/source_code.jsx
@@ -0,0 +1,2 @@
+const accountId = props.accountId ?? context.accountId; const ownerId = props.ownerId ?? "hack.near"; const pageId = props.pageId ?? "community.page"; return <Widget src=hack.near/widget/demo props={{ accountId, communityId: multi.near, contractId: mint.sharddog.near, h1: Communities,
+        h2: DAO, tagline: Connect, Collaborate, and Create!, bgColor: , buttonText: Get Started, link: https://near.social }} />
