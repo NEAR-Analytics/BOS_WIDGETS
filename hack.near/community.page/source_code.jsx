fd7e6180214ff2380d0b9dc94bec247eb2966931
hack.near <hack.near>
2023-07-16 04:11:04 +0000

    Update community.page by hack.near at 2023-07-16T04:11:04.817Z

--- hack.near/community.page/source_code.jsx
+++ hack.near/community.page/source_code.jsx
@@ -1,261 +1,199 @@
-State.init({
-  selectedTab: props.tab || "overview",
+const accountId = props.accountId ?? context.accountId;
+const daoId = props.daoId ?? "multi.sputnik-dao.near";
+const contractId = props.contractId ?? "mint.sharddog.near";
+
+const tab = props.tab === "following" ? props.tab : "members";
+
+const nftData = Near.view(contractId, "nft_supply_for_owner", {
+  account_id: accountId,
 });
 
-const accountId = props.accountId ?? context.accountId;
-if (!accountId) {
-  return "No account ID";
-}
+const isNftHolder = false;
 
-if (props.tab && props.tab !== state.selectedTab) {
-  State.update({
-    selectedTab: props.tab,
-  });
+if (nftData > 0) {
+  isNftHolder = true;
 }
-
-const profile = props.profile ?? Social.getr(`${accountId}/profile`);
-const accountUrl = `#/hack.near/widget/community.page`;
 
 const Wrapper = styled.div`
-  padding-bottom: 48px;
-`;
+  --section-gap: 23px;
+  padding-top: 42px;
 
-const Main = styled.div`
-  display: grid;
-  gap: 40px;
-  grid-template-columns: 352px minmax(0, 1fr);
-  align-items: start;
+  @media (max-width: 1155px) {
+    .line-rounded-corners {
+      display: none !important;
+    }
+  }
 
-  @media (max-width: 1200px) {
-    grid-template-columns: minmax(0, 1fr);
+  @media (max-width: 998px) {
+    padding-top: 0;
   }
 `;
 
-const BackgroundImage = styled.div`
-  height: 240px;
-  border-radius: 20px 20px 0 0;
-  overflow: hidden;
-  margin: 0 -12px;
-  background: #eceef0;
+const H1 = styled.h1`
+  font-family: "FK Grotesk", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 90px;
+  line-height: 1;
+  text-align: center;
+  letter-spacing: -0.03em;
+  color: ${props.h1Color ?? "#000"};
+  margin: 0;
+  max-width: 700px;
 
-  img {
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
+  @media (max-width: 900px) {
+    font-size: 50px;
   }
+`;
 
-  @media (max-width: 1200px) {
-    margin: calc(var(--body-top-padding) * -1) -12px 0;
-    border-radius: 0;
+const H2 = styled.h1`
+  font-family: "FK Grotesk", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 90px;
+  line-height: 1;
+  text-align: center;
+  letter-spacing: -0.03em;
+  color: ${props.bgColor ?? "#fff"};
+  margin: 0;
+  max-width: 700px;
+
+  span {
+    display: inline-block;
+    background: ${props.mainColor ?? "#000"};
+    border-radius: 20px;
+    position: relative;
+    padding: 0.1em 0.2em 0;
+
+    svg {
+      position: absolute;
+      bottom: -8px;
+      right: -10px;
+      width: 24px;
+    }
   }
 
   @media (max-width: 900px) {
-    height: 100px;
-  }
-`;
+    font-size: 50px;
 
-const SidebarWrapper = styled.div`
-  position: relative;
-  z-index: 5;
-  margin-top: -55px;
-
-  @media (max-width: 900px) {
-    margin-top: -40px;
-  }
-`;
-
-const Content = styled.div`
-  .post {
-    padding-left: 0;
-    padding-right: 0;
-  }
-`;
-
-const Title = styled.h1`
-  font-weight: 600;
-  font-size: ${(p) => p.size || "25px"};
-  line-height: 1.2em;
-  color: #11181c;
-  margin: ${(p) => (p.margin ? "0 0 24px" : "0")};
-  overflow-wrap: anywhere;
-`;
-
-const Tabs = styled.div`
-  display: flex;
-  height: 48px;
-  border-bottom: 1px solid #eceef0;
-  margin-bottom: 72px;
-  overflow: auto;
-  scroll-behavior: smooth;
-
-  @media (max-width: 1200px) {
-    background: #f8f9fa;
-    border-top: 1px solid #eceef0;
-    margin: 0 -12px 48px;
-
-    > * {
-      flex: 1;
+    span {
+      border-radius: 12px;
+      svg {
+        position: absolute;
+        bottom: -6px;
+        right: -7px;
+        width: 16px;
+      }
     }
   }
 `;
 
-const TabsButton = styled.a`
-  display: inline-flex;
+const Text = styled.p`
+  font-family: "Courier", sans-serif;
+  font-size: ${(p) => p.size ?? "16px"};
+  line-height: ${(p) => p.lineHeight ?? "1.5"};
+  font-weight: ${(p) => p.weight ?? "400"};
+  color: ${(p) => p.color ?? "#000"};
+  margin: 0;
+  max-width: 670px;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const HiddenText = styled.p`
+  font-family: "Courier", sans-serif;
+  font-size: ${(p) => p.size ?? "23px"};
+  line-height: ${(p) => p.lineHeight ?? "1.5"};
+  font-weight: ${(p) => p.weight ?? "400"};
+  color: ${(p) => p.color ?? "#000"};
+  margin: 0;
+  max-width: 670px;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: "nowrap";
+
+  @media (max-width: 998px) {
+    flex-direction: column;
+    gap: var(--section-gap);
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  max-width: 1080px;
+  margin: 0 auto;
+  gap: var(--section-gap);
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100%;
-  font-weight: 600;
-  font-size: 12px;
-  padding: 0 12px;
-  position: relative;
-  color: ${(p) => (p.selected ? "#11181C" : "#687076")};
-  background: none;
-  border: none;
-  outline: none;
   text-align: center;
-  text-decoration: none !important;
+  padding: var(--section-gap) 24px;
 
-  &:hover {
-    color: #11181c;
-  }
-
-  &::after {
-    content: "";
-    display: ${(p) => (p.selected ? "block" : "none")};
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 3px;
-    background: #59e692;
+  @media (max-width: 768px) {
+    padding: var(--section-gap) 12px;
   }
 `;
-
-const Bio = styled.div`
-  color: #11181c;
-  font-size: 14px;
-  line-height: 20px;
-  margin-bottom: 48px;
-
-  > *:last-child {
-    margin-bottom: 0 !important;
-  }
-
-  @media (max-width: 900px) {
-    margin-bottom: 48px;
-  }
-`;
-
-if (profile === null) {
-  return "Loading";
-}
-
-console.log(profile.backgroundImage);
 
 return (
   <Wrapper>
-    <BackgroundImage>
-      {profile.backgroundImage && (
-        <Widget
-          src="mob.near/widget/Image"
-          props={{
-            image: profile.backgroundImage,
-            alt: "profile background image",
-            fallbackUrl:
-              "https://ipfs.near.social/ipfs/bafkreibiyqabm3kl24gcb2oegb7pmwdi6wwrpui62iwb44l7uomnn3lhbi",
-          }}
-        />
-      )}
-    </BackgroundImage>
-
-    <Main>
-      <SidebarWrapper>
-        <Widget
-          src="hack.near/widget/ProfilePage.Sidebar"
-          props={{
-            accountId,
-            profile,
-          }}
-        />
-      </SidebarWrapper>
-
-      <Content>
-        <Tabs>
-          <TabsButton
-            href={`${accountUrl}&tab=overview`}
-            selected={state.selectedTab === "overview"}
-          >
-            Info
-          </TabsButton>
-
-          <TabsButton
-            href={`${accountUrl}&tab=apps`}
-            selected={state.selectedTab === "apps"}
-          >
-            Components
-          </TabsButton>
-
-          <TabsButton
-            href={`${accountUrl}&tab=tokens`}
-            selected={state.selectedTab === "tokens"}
-          >
-            Tokens
-          </TabsButton>
-
-          <TabsButton
-            href={`${accountUrl}&tab=following`}
-            selected={state.selectedTab === "following"}
-          >
-            Following
-          </TabsButton>
-
-          <TabsButton
-            href={`${accountUrl}&tab=followers`}
-            selected={state.selectedTab === "followers"}
-          >
-            Followers
-          </TabsButton>
-
-          <TabsButton
-            href={`${accountUrl}&tab=explorer`}
-            selected={state.selectedTab === "explorer"}
-          >
-            Explorer
-          </TabsButton>
-        </Tabs>
-
-        {state.selectedTab === "overview" && (
-          <Widget src="zavodil.near/widget/Lido" />
+    <Container>
+      <Flex>
+        <H1>{props.h1 ?? "Multi"}</H1>
+        <H2>
+          <span>{props.h2 ?? "DAO"}</span>
+        </H2>
+        <div className="mt-3">
+          <Text style={{ maxWidth: "350px" }}>
+            {props.tagline ?? "Everyone builds everything together!"}
+          </Text>
+        </div>
+        {context.accountId && (
+          <div className="m-3" style={{ maxWidth: "75%" }}>
+            <Widget
+              src="near/widget/AccountProfileCard"
+              props={{ accountId: daoId }}
+            />
+          </div>
         )}
-
-        {state.selectedTab === "tokens" && (
-          <Widget src="zavodil.near/widget/Lido" props={{ accountId }} />
-        )}
-
-        {state.selectedTab === "apps" && (
-          <Widget src="near/widget/ComponentCollection" props={{ accountId }} />
-        )}
-
-        {state.selectedTab === "followers" && (
-          <Widget src="near/widget/FollowersList" props={{ accountId }} />
-        )}
-
-        {state.selectedTab === "following" && (
-          <Widget src="near/widget/FollowingList" props={{ accountId }} />
-        )}
-
-        {state.selectedTab === "explorer" && (
-          <Widget
-            src="near/widget/Explorer.Account"
-            props={{
-              accountId,
-              network: context.networkId,
-              language: "en",
-              baseUrl: props.baseUrl,
-            }}
-          />
-        )}
-      </Content>
-    </Main>
+      </Flex>
+    </Container>
+    {isNftHolder && (
+      <HiddenText>
+        <div className="m-2 mb-5">
+          <h5 className="mb-3">Non-Fungible Things</h5>
+          <Widget src="near/widget/NFTCollection" props={{ accountId }} />
+        </div>
+      </HiddenText>
+    )}
+    <div className="m-2">
+      <Widget src="hack.near/widget/dao.people" props={{ daoId }} />
+    </div>
+    <br />
+    <br />
+    <Flex>
+      <Text
+        size="14px"
+        weight="600"
+        style={{
+          textTransform: "uppercase",
+          letterSpacing: "0.17em",
+          textAlign: "center",
+        }}
+      >
+        Made Possible by Collaboration
+      </Text>
+      <Widget src="hack.near/widget/dev.Badge" />
+    </Flex>
   </Wrapper>
 );
