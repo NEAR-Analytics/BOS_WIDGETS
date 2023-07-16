86061fd04dde3a590b8c7a10e5a2f8df575af049
idknwhoru.near <idknwhoru.near>
2023-07-16 12:05:26 +0000

    Update DeFiGame-CEX by idknwhoru.near at 2023-07-16T12:05:26.676Z

--- idknwhoru.near/DeFiGame-CEX/source_code.jsx
+++ idknwhoru.near/DeFiGame-CEX/source_code.jsx
@@ -1,40 +1,63 @@
+State.init({
+  activeTab: 1,
+});
+
+const activePurchase = (_) => {
+  State.update({
+    activeTab: 1,
+  });
+};
+
+const activeSale = (_) => {
+  State.update({
+    activeTab: 2,
+  });
+};
+
+const activeStake = (_) => {
+  State.update({
+    activeTab: 3,
+  });
+};
+
 return (
   <>
     <h1>CEX</h1>
-    <hr />
-    <div>
-      <h2>구매탭</h2>
-      <input type="number" />
-      <select>
-        <option>S토큰</option>
-        <option>L토큰</option>
-      </select>
-      <button>구매하기</button>
-    </div>
-    <hr />
-    <div>
-      <h2>인출탭</h2>
-      <input type="number" />
-      <select>
-        <option>S토큰</option>
-        <option>L토큰</option>
-      </select>
-      <button>인출하기</button>
-    </div>
-    <hr />
-    <div>
-      <h2>스테이킹탭</h2>
-      <input type="number" />
-      <button>스테이킹하기</button>
-    </div>
-    <hr />
+    <button onClick={activePurchase}>구매</button>
+    <button onClick={activeSale}>인출</button>
+    <button onClick={activeStake}>스테이크</button>
     <div>
       <h2>내 자산정보</h2>
       <ul>
-        <li>현금</li>
-        <li>S토큰</li>
-        <li>L토큰</li>
+        <li>현금: ?원</li>
+        <li>S토큰: ?개</li>
+        <li>L토큰: ?개</li>
       </ul>
     </div>
+    <hr />
+    {state.activeTab === 1 ? (
+      <div>
+        <input type="number" />
+        <select>
+          <option>S토큰</option>
+          <option>L토큰</option>
+        </select>
+        <button>구매하기</button>
+      </div>
+    ) : state.activeTab === 2 ? (
+      <div>
+        <input type="number" />
+        <select>
+          <option>S토큰</option>
+          <option>L토큰</option>
+        </select>
+        <button>인출하기</button>
+      </div>
+    ) : (
+      <div>
+        <input type="number" />
+        <button>스테이킹하기</button>
+      </div>
+    )}
   </>
 );
