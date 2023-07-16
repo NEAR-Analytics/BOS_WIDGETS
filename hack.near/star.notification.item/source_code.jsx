da5be549b5c79a3c9b84524d827b4d481b7b2e84
hack.near <hack.near>
2023-07-16 02:04:47 +0000

    Update star.notification.item by hack.near at 2023-07-16T02:04:47.098Z

--- hack.near/star.notification.item/source_code.jsx
+++ hack.near/star.notification.item/source_code.jsx
@@ -4,8 +4,7 @@ return (
   <Widget
     src="hack.near/widget/star.notification.item.LR"
     props={{
-      L:
-        value.type === "star" ? "starred your widget" : "unstarred your widget",
+      L: value.type === "star" && "starred your widget",
       R: <Widget src="mob.near/widget/FollowButton" props={{ accountId }} />,
       ...props,
     }}
