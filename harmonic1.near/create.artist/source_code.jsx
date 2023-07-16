f02cae65f83db0e048b4195bb135262286fc532b
harmonic1.near <harmonic1.near>
2023-07-16 14:48:15 +0000

    Update create.artist by harmonic1.near at 2023-07-16T14:48:15.354Z

---
+++ harmonic1.near/create.artist/source_code.jsx
@@ -0,0 +1,260 @@
+const data = props.data || {};
+const type = props.type || "";
+const typeSrc = props.typeSrc || "every.near";
+const buildEdges = props.buildEdges;
+const template = props.template || "";
+const thingId = props.thingId;
+//const defaultView = props.defaultView || "CREATE_THING";
+
+if (type !== "") {
+  const parts = type.split("/");
+  typeSrc = parts[0];
+}
+
+const Container = styled.div`
+    display: flex;
+  `;
+
+const SidePanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+  background-color: #f2f2f2;
+  width: auto;
+  z-index: 50;
+  min-width: 400px;
+`;
+
+const MainContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  `;
+
+const FormContainer = styled.div`
+  border: 1px solid #ccc;
+  padding: 20px;
+`;
+
+const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 20px;
+    gap: 8px;
+    background-color: #f2f2f2;
+    padding: 30px;
+  `;
+
+const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  align-self: flex-start;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  `;
+
+const LeftPanelItem = styled.div`
+    padding: 8px;
+    background-color: #ccc;
+    color: white;
+    border-radius: 4px;
+  `;
+
+const Select = styled.select`
+  `;
+
+const Label = styled.label`
+`;
+
+const Input = styled.input`
+  `;
+
+const ModalOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 100;
+`;
+
+const ModalContent = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 4px;
+  min-width: 500px;
+  height: 100%;
+  overflow: scroll;
+`;
+
+const ModalTitle = styled.h3`
+  margin-bottom: 10px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const CenteredDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  text-align: center;
+`;
+
+State.init({
+  data,
+  config: data,
+  isModalOpen: false,
+  typeSrc: "harmonic1.near",
+  selectedType: "harmonic1.near/type/artist",
+  //view: defaultView,
+  preview: "TEMPLATE",
+  template: "harmonic1.near/widget/artist",
+  templateVal: template,
+  thingId,
+});
+
+const handleOnChange = (value) => {
+  State.update({ data: { ...state.data, ...value } });
+};
+
+const handleApply = () => {
+  State.update({
+    config: state.data,
+    template: state.templateVal,
+  });
+  // set the props for the main content
+};
+
+const handleSave = () => {
+  // create the thing
+  State.update({ isModalOpen: false });
+  const thingId = state.thingId || Math.random();
+  let edges = [];
+  if (buildEdges) {
+    const newPath = `${context.accountId}/thing/${thingId}`;
+    edges = buildEdges(newPath, state.selectedType);
+  }
+
+  const data = {
+    thing: {
+      [thingId]: JSON.stringify({
+        data: state.config,
+        template: {
+          src: state.template,
+        },
+        type: state.selectedType,
+      }),
+    },
+    index: {
+      thing: JSON.stringify({
+        key: thingId,
+        value: {
+          type: state.selectedType,
+        },
+      }),
+    },
+  };
+  if (edges.length) {
+    data.index.edge = JSON.stringify(edges);
+  }
+  Social.set(data, {
+    onCommit: () => {
+      State.update({
+        data: {},
+        isModalOpen: false,
+        config: undefined,
+      });
+    },
+    onCancel: () => {
+      State.update({
+        isModalOpen: false,
+      });
+    },
+  });
+};
+
+let availableTypes = [];
+const types = Social.get(`${state.typeSrc}/type/**`, "final");
+if (types !== null) {
+  availableTypes =
+    Object.keys(types)?.map((it) => `${state.typeSrc}/type/${it}`) || [];
+}
+
+const handleTypeChange = (e) => {
+  State.update({ selectedType: e.target.value, templateVal: "", data: {} });
+};
+
+return (
+  <Container>
+    <SidePanel>
+      <>
+        <FormContainer>
+          <Widget
+            src="efiz.near/widget/create"
+            props={{
+              item: {
+                type: state.selectedType,
+                value: state.data,
+              },
+              onChange: handleOnChange,
+            }}
+          />
+        </FormContainer>
+        <Footer>
+          <Button onClick={() => handleApply()}>Preview</Button>
+          <Button
+            onClick={() => State.update({ isModalOpen: true })}
+            disabled={state.config === undefined}
+          >
+            Save
+          </Button>
+        </Footer>
+      </>
+    </SidePanel>
+    <MainContent>
+      <>
+        {(state.template && (
+          <Widget src={state.template} props={{ data: state.config }} />
+        )) || <CenteredDiv>Click on Preview to see your profile.</CenteredDiv>}
+      </>
+      )
+    </MainContent>
+    {state.isModalOpen && (
+      <ModalOverlay>
+        <ModalContent>
+          <ModalTitle>create thing</ModalTitle>
+          <p>option to provide a thing id</p>
+          <Row style={{ gap: "8px" }}>
+            <Input
+              value={state.thingId}
+              onChange={(e) => State.update({ thingId: e.target.value })}
+              placeholder="thing id"
+            />
+          </Row>
+          <Widget
+            src="efiz.near/widget/Every.Raw.View"
+            props={{
+              value: { data: state.config, template: { src: state.template } },
+            }}
+          />
+          <Button onClick={handleSave}>Save</Button>
+          <Button onClick={() => State.update({ isModalOpen: false })}>
+            Cancel
+          </Button>
+        </ModalContent>
+      </ModalOverlay>
+    )}
+  </Container>
+);
