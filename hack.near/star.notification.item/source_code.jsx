8692a00afa8dd27f9ce7148e020f3ee21e70740f
hack.near <hack.near>
2023-07-16 02:06:10 +0000

    Update star.notification.item by hack.near at 2023-07-16T02:06:10.245Z

--- hack.near/star.notification.item/source_code.jsx
+++ hack.near/star.notification.item/source_code.jsx
@@ -1,12 +1,18 @@
 const { accountId, value } = props;
 
 return (
-  <Widget
-    src="hack.near/widget/star.notification.item.LR"
-    props={{
-      L: value.type === "star" && "starred your widget",
-      R: <Widget src="mob.near/widget/FollowButton" props={{ accountId }} />,
-      ...props,
-    }}
-  />
+  <div>
+    {value.type === "star" && (
+      <Widget
+        src="hack.near/widget/star.notification.item.LR"
+        props={{
+          L: value.type === "star" && "starred your widget",
+          R: (
+            <Widget src="mob.near/widget/FollowButton" props={{ accountId }} />
+          ),
+          ...props,
+        }}
+      />
+    )}
+  </div>
 );
