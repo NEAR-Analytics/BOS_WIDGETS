077264ca6cd1c918a71885bef3b501d8724506d9
idknwhoru.near <idknwhoru.near>
2023-07-16 08:51:23 +0000

    Update DeFiGame-CEX by idknwhoru.near at 2023-07-16T08:51:23.492Z

---
+++ idknwhoru.near/DeFiGame-CEX/source_code.jsx
@@ -0,0 +1,40 @@
+return (
+  <>
+    <h1>CEX</h1>
+    <hr />
+    <div>
+      <h2>구매탭</h2>
+      <input type="number" />
+      <select>
+        <option>S토큰</option>
+        <option>L토큰</option>
+      </select>
+      <button>구매하기</button>
+    </div>
+    <hr />
+    <div>
+      <h2>인출탭</h2>
+      <input type="number" />
+      <select>
+        <option>S토큰</option>
+        <option>L토큰</option>
+      </select>
+      <button>인출하기</button>
+    </div>
+    <hr />
+    <div>
+      <h2>스테이킹탭</h2>
+      <input type="number" />
+      <button>스테이킹하기</button>
+    </div>
+    <hr />
+    <div>
+      <h2>내 자산정보</h2>
+      <ul>
+        <li>현금</li>
+        <li>S토큰</li>
+        <li>L토큰</li>
+      </ul>
+    </div>
+  </>
+);
