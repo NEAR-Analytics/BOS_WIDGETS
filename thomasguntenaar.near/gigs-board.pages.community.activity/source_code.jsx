9c958cdee468ceb22ffd56307ea4611402dc36db
thomasguntenaar.near <thomasguntenaar.near>
2023-07-16 14:51:35 +0000

    Update gigs-board.pages.community.activity by thomasguntenaar.near at 2023-07-16T14:51:35.318Z

--- thomasguntenaar.near/gigs-board.pages.community.activity/source_code.jsx
+++ thomasguntenaar.near/gigs-board.pages.community.activity/source_code.jsx
@@ -52,33 +52,34 @@ function href(widgetName, linkProps) {
 }
 /* END_INCLUDE: "common.jsx" */
 /* INCLUDE: "core/adapter/dev-hub" */
-const devHubAccountId =
+const contractAccountId =
   props.nearDevGovGigsContractAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
 
 const DevHub = {
   edit_community_github: ({ handle, github }) =>
-    Near.call(devHubAccountId, "edit_community_github", { handle, github }) ??
+    Near.call(contractAccountId, "edit_community_github", { handle, github }) ??
     null,
 
   get_access_control_info: () =>
-    Near.view(devHubAccountId, "get_access_control_info") ?? null,
+    Near.view(contractAccountId, "get_access_control_info") ?? null,
 
-  get_all_authors: () => Near.view(devHubAccountId, "get_all_authors") ?? null,
+  get_all_authors: () =>
+    Near.view(contractAccountId, "get_all_authors") ?? null,
 
   get_all_communities: () =>
-    Near.view(devHubAccountId, "get_all_communities") ?? null,
+    Near.view(contractAccountId, "get_all_communities") ?? null,
 
-  get_all_labels: () => Near.view(devHubAccountId, "get_all_labels") ?? null,
+  get_all_labels: () => Near.view(contractAccountId, "get_all_labels") ?? null,
 
   get_community: ({ handle }) =>
-    Near.view(devHubAccountId, "get_community", { handle }) ?? null,
+    Near.view(contractAccountId, "get_community", { handle }) ?? null,
 
   get_post: ({ post_id }) =>
-    Near.view(devHubAccountId, "get_post", { post_id }) ?? null,
+    Near.view(contractAccountId, "get_post", { post_id }) ?? null,
 
   get_posts_by_author: ({ author }) =>
-    Near.view(devHubAccountId, "get_posts_by_author", { author }) ?? null,
+    Near.view(contractAccountId, "get_posts_by_author", { author }) ?? null,
 
   get_posts_by_label: ({ label }) =>
     Near.view(nearDevGovGigsContractAccountId, "get_posts_by_label", {
@@ -86,39 +87,21 @@ const DevHub = {
     }) ?? null,
 
   get_root_members: () =>
-    Near.view(devHubAccountId, "get_root_members") ?? null,
-
-  useQuery: ({ name, params }) => {
-    const initialState = { data: null, error: null, isLoading: true };
-
-    const cacheState = useCache(
-      () =>
-        Near.asyncView(devHubAccountId, ["get", name].join("_"), params ?? {})
-          .then((response) => ({
-            ...initialState,
-            data: response ?? null,
-            isLoading: false,
-          }))
-          .catch((error) => ({
-            ...initialState,
-            error: props?.error ?? error,
-            isLoading: false,
-          })),
-
-      JSON.stringify({ name, params }),
-      { subscribe: true }
-    );
-
-    return cacheState === null
-      ? { ...cacheState, ...initialState }
-      : cacheState;
-  },
+    Near.view(contractAccountId, "get_root_members") ?? null,
 };
 /* END_INCLUDE: "core/adapter/dev-hub" */
 
 const CommunityActivityPage = ({ handle }) => {
-  const communityData = DevHub.get_community({ handle });
+  if (!handle) {
+    return (
+      <div class="alert alert-danger" role="alert">
+        Error: community handle not found in URL parameters
+      </div>
+    );
+  }
 
+  const communityData = DevHub.get_community({ handle });
+  console.log({ communityData });
   if (communityData === null) {
     return <div>Loading...</div>;
   }
@@ -129,30 +112,32 @@ const CommunityActivityPage = ({ handle }) => {
 
     children:
       communityData !== null ? (
-        <div>
-          <div class="row mb-2">
-            <div class="col text-center">
-              <small class="text-muted">
-                <span>Required tags:</span>
-
-                <a
-                  href={href("Feed", { tag: communityData.tag })}
-                  key={communityData.tag}
-                >
-                  <span class="badge text-bg-primary me-1">
-                    {communityData.tag}
-                  </span>
-                </a>
-              </small>
+        <div class="row">
+          <div class="col-md-9">
+            <div class="row mb-2">
+              <div class="col">
+                <small class="text-muted">
+                  <span>Required tags:</span>
+                  {widget("components.atom.tag", { label: communityData.tag })}
+                </small>
+              </div>
+            </div>
+            {widget("components.layout.Controls", {
+              labels: communityData.tag,
+            })}
+            <div class="row">
+              <div class="col">
+                {widget("entity.post.List", { tag: communityData.tag })}
+              </div>
             </div>
           </div>
-
-          {widget("components.layout.Controls", { labels: communityData.tag })}
-
-          <div class="row">
-            <div class="col">
-              {widget("entity.post.List", { tag: communityData.tag })}
-            </div>
+          <div class="col-md-3 container-fluid">
+            <Widget
+              src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.community.sidebar`}
+              props={{
+                label: communityData.tag,
+              }}
+            />
           </div>
         </div>
       ) : (
