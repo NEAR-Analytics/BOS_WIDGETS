104ce0935844c1e7bca7077b648ae76832426283
hack.near <hack.near>
2023-07-16 01:34:31 +0000

    Update star.notification by hack.near at 2023-07-16T01:34:31.745Z

---
+++ hack.near/star.notification/source_code.jsx
@@ -0,0 +1,14 @@
+const { value } = props;
+
+return (
+  <div className="mb-3">
+    {value.type === "star" || value.type === "unstar" ? (
+      <Widget src="mob.near/widget/star.notification.item" props={props} />
+    ) : (
+      <div>
+        Unknown notification:{" "}
+        <span className="font-monospace">{JSON.stringify(value)}</span>
+      </div>
+    )}
+  </div>
+);
