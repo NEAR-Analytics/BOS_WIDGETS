dba6e6cd706714a3ed67c3972e8124465f93be49
hack.near <hack.near>
2023-07-16 02:19:45 +0000

    Update star.notification by hack.near at 2023-07-16T02:19:45.980Z

--- hack.near/star.notification/source_code.jsx
+++ hack.near/star.notification/source_code.jsx
@@ -1,7 +1,32 @@
-const { value } = props;
+const accountId = context.accountId;
 
-return (
-  <div className="mb-3">
-    <Widget src="hack.near/widget/star.notification.item" props={props} />
-  </div>
-);
+if (!accountId) {
+  return "Sign in with NEAR Wallet";
+}
+
+const index = {
+  action: "notify",
+  key: accountId,
+  options: {
+    limit: 10,
+    order: "desc",
+    subscribe: true,
+  },
+};
+
+const renderItem = (item, i) => {
+  if (i === 0) {
+    Storage.set("lastBlockHeight", item.blockHeight);
+  }
+  return (
+    <div className="m-2 mt-3">
+      <Widget
+        src="hack.near/widget/star.notification.item"
+        key={i}
+        props={item}
+      />
+    </div>
+  );
+};
+
+return <Widget src="mob.near/widget/IndexFeed" props={{ index, renderItem }} />;
