5522f432449c333744997efe518ed086ebf9c4fc
idknwhoru.near <idknwhoru.near>
2023-07-16 12:16:37 +0000

    Update DeFiGame-DEX by idknwhoru.near at 2023-07-16T12:16:37.711Z

--- idknwhoru.near/DeFiGame-DEX/source_code.jsx
+++ idknwhoru.near/DeFiGame-DEX/source_code.jsx
@@ -14,31 +14,56 @@ const activeStake = (_) => {
   });
 };
 
+const TokenWrapper = styled.div`
+    display: flex;
+
+    .token-quantity {
+        width: 100%;
+        text-align: end;
+    }
+`;
+
 return (
   <>
     <h1>DEX</h1>
+    <div>
+      <h2>내 자산정보</h2>
+      <ul>
+        <li>현금: ?원</li>
+        <li>S토큰: ?개</li>
+        <li>L토큰: ?개</li>
+      </ul>
+    </div>
     <button onClick={activeSwap}>SWAP</button>
     <button onClick={activeStake}>Stake</button>
     <hr />
     {state.activeTab === 1 ? (
       <div>
-        <h2>스왑 탭</h2>
-        <input type="number" />
-        <select>
-          <option>S토큰</option>
-          <option>L토큰</option>
-        </select>
-        <input type="number" />
-        <select>
-          <option>S토큰</option>
-          <option>L토큰</option>
-        </select>
+        <TokenWrapper>
+          <input type="number" className="token-quantity" min={0} value={0} />
+          <select>
+            <option>S토큰</option>
+            <option>L토큰</option>
+          </select>
+        </TokenWrapper>
+        <TokenWrapper>
+          <input
+            type="number"
+            className="token-quantity"
+            min={0}
+            value={0}
+            disabled={true}
+          />
+          <select disabled={true}>
+            <option>S토큰</option>
+            <option>L토큰</option>
+          </select>
+        </TokenWrapper>
         <button>스왑하기</button>
       </div>
     ) : (
       <div>
-        <h2>스테이크</h2>
-        <h3>스테이크 비율 21 : 10</h3>
+        <h2>스테이크 비율 21 : 10</h2>
         <input type="number" />
         <button>스테이킹하기</button>
       </div>
