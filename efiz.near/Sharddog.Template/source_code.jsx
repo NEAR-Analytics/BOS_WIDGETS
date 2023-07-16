126f0be034faa86ed0625d4f7bb6604167f353c3
efiz.near <efiz.near>
2023-07-16 00:22:26 +0000

    Update Sharddog.Template by efiz.near at 2023-07-16T00:22:26.230Z

--- efiz.near/Sharddog.Template/source_code.jsx
+++ efiz.near/Sharddog.Template/source_code.jsx
@@ -2,28 +2,27 @@ function Sharddog({ owner, media }) {
   const size = "144px";
 
   return (
-    <Widget
-      src="mob.near/widget/Image"
-      props={{
-        image: {
-          url: media,
-        },
-        style: {
-          width: size,
-          height: size,
-          objectFit: "cover",
-          minWidth: size,
-          minHeight: size,
-          maxWidth: size,
-          maxHeight: size,
-          overflowWrap: "break-word",
-        },
-      }}
-    />
+    <Link to={`/mob.near/widget/ProfilePage?accountId=${owner}`}>
+      <Widget
+        src="mob.near/widget/Image"
+        props={{
+          image: {
+            url: media,
+          },
+          style: {
+            width: size,
+            height: size,
+            objectFit: "cover",
+            minWidth: size,
+            minHeight: size,
+            maxWidth: size,
+            maxHeight: size,
+            overflowWrap: "break-word",
+          },
+        }}
+      />
+    </Link>
   );
 }
 
 return { Sharddog };
-
-// <Link to={`/mob.near/widget/ProfilePage?accountId=${owner}`}>
-// </Link>
