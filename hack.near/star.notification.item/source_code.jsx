88cae18e7973a8c914722562a9641d0768d91aa6
hack.near <hack.near>
2023-07-16 02:26:52 +0000

    Update star.notification.item by hack.near at 2023-07-16T02:26:52.406Z

--- hack.near/star.notification.item/source_code.jsx
+++ hack.near/star.notification.item/source_code.jsx
@@ -1,7 +1,7 @@
 const { accountId, value } = props;
 
 return (
-  <div>
+  <div className="m-2">
     {value.type === "star" && (
       <Widget
         src="hack.near/widget/star.notification.item.LR"
