f68e349914751f3a8834ed72cb9d73c3dc00dc2c
idknwhoru.near <idknwhoru.near>
2023-07-16 10:25:21 +0000

    Update DeFiGame-DEX by idknwhoru.near at 2023-07-16T10:25:21.087Z

--- idknwhoru.near/DeFiGame-DEX/source_code.jsx
+++ idknwhoru.near/DeFiGame-DEX/source_code.jsx
@@ -1,27 +1,47 @@
+State.init({
+  activeTab: 1,
+});
+
+const activeSwap = (_) => {
+  State.update({
+    activeTab: 1,
+  });
+};
+
+const activeStake = (_) => {
+  State.update({
+    activeTab: 2,
+  });
+};
+
 return (
   <>
     <h1>DEX</h1>
+    <button onClick={activeSwap}>SWAP</button>
+    <button onClick={activeStake}>Stake</button>
     <hr />
-    <div>
-      <h2>스왑 탭</h2>
-      <input type="number" />
-      <select>
-        <option>S토큰</option>
-        <option>L토큰</option>
-      </select>
-      <input type="number" />
-      <select>
-        <option>S토큰</option>
-        <option>L토큰</option>
-      </select>
-      <button>스왑하기</button>
-    </div>
-    <hr />
-    <div>
-      <h2>스테이킹 탭</h2>
-      <h3>스테이킹 비율 21:10</h3>
-      <input type="number" />
-      <button>스테이킹하기</button>
-    </div>
+    {state.activeTab === 1 ? (
+      <div>
+        <h2>스왑 탭</h2>
+        <input type="number" />
+        <select>
+          <option>S토큰</option>
+          <option>L토큰</option>
+        </select>
+        <input type="number" />
+        <select>
+          <option>S토큰</option>
+          <option>L토큰</option>
+        </select>
+        <button>스왑하기</button>
+      </div>
+    ) : (
+      <div>
+        <h2>스테이크</h2>
+        <h3>스테이크 비율</h3>
+        <input type="number" />
+        <button>스테이킹하기</button>
+      </div>
+    )}
   </>
 );
