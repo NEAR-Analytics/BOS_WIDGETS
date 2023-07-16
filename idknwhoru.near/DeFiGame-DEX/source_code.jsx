e4d000a460f9c0830a929bdceb330e5377f70758
idknwhoru.near <idknwhoru.near>
2023-07-16 09:41:55 +0000

    Update DeFiGame-DEX by idknwhoru.near at 2023-07-16T09:41:55.079Z

---
+++ idknwhoru.near/DeFiGame-DEX/source_code.jsx
@@ -0,0 +1,27 @@
+return (
+  <>
+    <h1>DEX</h1>
+    <hr />
+    <div>
+      <h2>스왑 탭</h2>
+      <input type="number" />
+      <select>
+        <option>S토큰</option>
+        <option>L토큰</option>
+      </select>
+      <input type="number" />
+      <select>
+        <option>S토큰</option>
+        <option>L토큰</option>
+      </select>
+      <button>스왑하기</button>
+    </div>
+    <hr />
+    <div>
+      <h2>스테이킹 탭</h2>
+      <h3>스테이킹 비율 21:10</h3>
+      <input type="number" />
+      <button>스테이킹하기</button>
+    </div>
+  </>
+);
