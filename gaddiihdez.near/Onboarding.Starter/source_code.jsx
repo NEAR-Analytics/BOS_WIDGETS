364b199da4dbde5e465d1de474815acfb0da8523
gaddiihdez.near <gaddiihdez.near>
2023-07-17 20:33:13 +0000

    Update Onboarding.Starter by gaddiihdez.near at 2023-07-17T20:33:13.617Z

---
+++ gaddiihdez.near/Onboarding.Starter/source_code.jsx
@@ -0,0 +1,75 @@
+return (
+  <>
+    <div
+      class="navbar navbar-expand-lg navbar-light shadow-sm"
+      style={{ background: "#f7efd4" }}
+    >
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i class="fas fa-bars"></i>
+        </button>
+
+        <div class="" id="navbarSupportedContent">
+          <div class="navbar-brand mt-2 mt-lg-0">
+            <img
+              src="https://res.cloudinary.com/dgcxcqu6p/image/upload/v1685034388/milestone_logo_verde_rqzu5a.png"
+              height="15"
+              alt="MDB Logo"
+              loading="lazy"
+            />
+          </div>
+          <ul class="navbar-nav mt-1 me-auto mb-2 mb-lg-0">
+            <li
+              class="nav-item"
+              onClick={() => {
+                props.onSelectedOption("project");
+              }}
+            >
+              <a class="nav-link" style={{ color: "#665a2c" }}>
+                Proyecto
+              </a>
+            </li>
+            <li
+              class="nav-item"
+              onClick={() => {
+                props.onSelectedOption("buys");
+              }}
+            >
+              <a class="nav-link" style={{ color: "#665a2c" }}>
+                Mis compras
+              </a>
+            </li>
+            <li
+              class="nav-item"
+              onClick={() => {
+                props.onSelectedOption("rewards");
+              }}
+            >
+              <a class="nav-link" style={{ color: "#665a2c" }}>
+                Mis recompenzas
+              </a>
+            </li>
+            <li
+              class="nav-item"
+              onClick={() => {
+                props.onSelectedOption("metapool");
+              }}
+            >
+              <a class="nav-link" style={{ color: "#665a2c" }}>
+                Meta Pool
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </>
+);
