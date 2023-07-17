49bca21dd54fc94f3fd0bb3e5bc4c1e88b65d093
iamwho.near <iamwho.near>
2023-07-17 17:27:24 +0000

    Update oxggjcwti9vbb0y by iamwho.near at 2023-07-17T17:27:24.767Z

--- iamwho.near/oxggjcwti9vbb0y/source_code.jsx
+++ iamwho.near/oxggjcwti9vbb0y/source_code.jsx
@@ -15,23 +15,26 @@ const handleSubmit = (value) => {
 };
 
 const searchContracts = (value) => {
-  try {
-    const res = Near.view("v1.sourcescan.near", "search", {
-      key: value,
-      from_index: props.from_index || 0,
-      limit: props.limit || 10,
-    });
+  while (true) {
+    try {
+      const res = Near.view("v1.sourcescan.near", "search", {
+        key: value,
+        from_index: props.from_index || 0,
+        limit: props.limit || 10,
+      });
 
-    if (!res) return;
-
-    State.update({
-      tableProps: {
-        ...state.tableProps,
-        contracts: res[0],
-      },
-    });
-  } catch {
-    console.log("error");
+      if (res) {
+        State.update({
+          tableProps: {
+            ...state.tableProps,
+            contracts: res[0],
+          },
+        });
+        break;
+      }
+    } catch {
+      console.log("error");
+    }
   }
 };
 
