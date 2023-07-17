babaf427200ee52aebce05f2cea38e65e8a82ffb
mattlock.near <mattlock.near>
2023-07-17 15:03:16 +0000

    Update zk-evm-lp by mattlock.near at 2023-07-17T15:03:16.094Z

--- mattlock.near/zk-evm-lp/source_code.jsx
+++ mattlock.near/zk-evm-lp/source_code.jsx
@@ -62,6 +62,7 @@ const Theme = styled.div`
   margin: 0 auto;
   background: rgb(0,0,0);
   background: linear-gradient(135deg, rgba(0,0,0,1) 50%, rgba(130,71,229,1) 100%);
+  background-attachment: fixed;
 
   > .logo-wrap {
     text-align: center;
