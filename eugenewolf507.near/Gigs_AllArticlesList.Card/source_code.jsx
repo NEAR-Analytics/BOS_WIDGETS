128ffbd87a24050ec157852dc572f12962080f45
eugenewolf507.near <eugenewolf507.near>
2023-07-17 20:07:02 +0000

    Update Gigs_AllArticlesList.Card by eugenewolf507.near at 2023-07-17T20:07:02.870Z

--- eugenewolf507.near/Gigs_AllArticlesList.Card/source_code.jsx
+++ eugenewolf507.near/Gigs_AllArticlesList.Card/source_code.jsx
@@ -16,21 +16,21 @@ const mainPartForSharingGig =
   "https://near.social/#/eugenewolf507.near/widget/Gigs_AllArticlesList";
 const shareUrl = `${mainPartForSharingGig}?articleId=${article.articleId}`;
 
-// if (
-//   (sharedArticleId === article.articleId && sharedArticleId) ||
-//   cardWithOpenModal === article.articleId
-// ) {
-//   State.update({ showModal: true });
-// } else {
-//   State.update({ showModal: false });
-// }
+if (
+  (sharedArticleId === article.articleId && sharedArticleId) ||
+  cardWithOpenModal === article.articleId
+) {
+  State.update({ showModal: true });
+} else {
+  State.update({ showModal: false });
+}
 
-if (sharedArticleId === article.articleId && sharedArticleId) {
-  State.update({ showModal: true });
-}
-if (cardWithOpenModal === article.articleId) {
-  State.update({ showModal: true });
-}
+// if (sharedArticleId === article.articleId && sharedArticleId) {
+//   State.update({ showModal: true });
+// }
+// if (cardWithOpenModal === article.articleId) {
+//   State.update({ showModal: true });
+// }
 
 // ========== UTILS ==========
 const getDateLastEdit = (timestamp) => {
