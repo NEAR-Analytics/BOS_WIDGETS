e6c6bd7233d09c9443548b7405ecdcd087baf884
iamwho.near <iamwho.near>
2023-07-17 10:29:13 +0000

    Update szlhyjoIhy by iamwho.near at 2023-07-17T10:29:13.127Z

--- iamwho.near/szlhyjoIhy/source_code.jsx
+++ iamwho.near/szlhyjoIhy/source_code.jsx
@@ -8,19 +8,17 @@ const handleChange = (e) => {
 
 return (
   <div class="w-screen flex flex-col items-center">
-    <div class="flex flex-row justify-center items-center">
-      <input
-        class="rounded border px-3 py-1 w-10vw"
-        placeholder={props.placeHolder}
-        value={state.value}
-        onChange={handleChange}
-      />
-      <button
-        onClick={() => props.handleSubmit(state.value)}
-        class="rounded bg-transparent border border-gray-600 text-gray-800 px-2 py-1"
-      >
-        Search
-      </button>
-    </div>
+    <input
+      class="rounded border px-3 py-1 w-10vw"
+      placeholder={props.placeHolder}
+      value={state.value}
+      onChange={handleChange}
+    />
+    <button
+      onClick={() => props.handleSubmit(state.value)}
+      class="rounded bg-transparent border border-gray-600 text-gray-800 px-2 py-1"
+    >
+      Search
+    </button>
   </div>
 );
