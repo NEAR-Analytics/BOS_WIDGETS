f8d19447582ae57ea6fcfbf5d5836fb4bcf5c570
jay100.near <jay100.near>
2023-07-17 11:36:53 +0000

    Update CTT-Button by jay100.near at 2023-07-17T11:36:53.149Z

--- jay100.near/CTT-Button/source_code.jsx
+++ jay100.near/CTT-Button/source_code.jsx
@@ -12,7 +12,7 @@ const ButtonContainer = styled.div`
 `;
 
 const ButtonText = styled.span`
-    background: rgb(14, 14, 30);
+    background: green;
 	display: inline-block;
 	position: relative;
 	text-align: center;
