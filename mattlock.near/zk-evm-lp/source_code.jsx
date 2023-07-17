9e88a29a97536fbfe663e9fcf94e76c459552b20
mattlock.near <mattlock.near>
2023-07-17 15:26:47 +0000

    Update zk-evm-lp by mattlock.near at 2023-07-17T15:26:47.354Z

--- mattlock.near/zk-evm-lp/source_code.jsx
+++ mattlock.near/zk-evm-lp/source_code.jsx
@@ -88,17 +88,6 @@ const Theme = styled.div`
       flex-direction: row-reverse !important;
       flex-flow: wrap-reverse !important;
     }
-    .grid {
-      > .center {
-        > .component {
-          > .center {
-            > div {
-              margin: 0 !important;
-            }
-          }
-        }
-      }
-    }
   }
   
   .swap-main-column {
