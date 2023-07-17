e91c043c176d36df1003cc8628c1c22e14a4541f
pikqi.near <pikqi.near>
2023-07-17 16:35:26 +0000

    Update XAVA-Testnet-Staking by pikqi.near at 2023-07-17T16:35:26.944Z

--- pikqi.near/XAVA-Testnet-Staking/source_code.jsx
+++ pikqi.near/XAVA-Testnet-Staking/source_code.jsx
@@ -5,48 +5,21 @@ if (state.user === undefined) {
 }
 if (!state.user) return <Web3Connect />;
 
-if (state.chainId === undefined && ethers !== undefined && state.user) {
-  Ethers.provider()
-    .getNetwork()
-    .then((chainIdData) => {
-      if (chainIdData?.chainId) {
-        State.update({ chainId: chainIdData.chainId });
-      }
-    });
-}
-
-console.log(Ethers.provider().getSigner());
-console.log(state.chainId);
-if (state.chainId !== undefined && state.chainId != 43113) {
-  Ethers.send("wallet_switchEthereumChain", [
-    {
-      chainId: "0xA869", // Hex representation of the desired chainId (43113)
-    },
-  ]);
-  return (
-    <p>
-      Your current network is not supported please switch to Avalanche Fuji
-      C-Chain
-    </p>
-  );
-}
-
-// if(state.chainId !== "dd")
 const XAVA_ADDRESS = "0x64E7AB33C8764a9285cbd48A3b5977c51D2eE645";
 const XAVA_STAKING = "0x8BeBB9907c72708442BD4195Bd354A9bAdC2a816";
 const XAVA_ABIS = fetch(
-  "https://raw.githubusercontent.com/avalaunch-app/xava-protocol/master/deployments/contract-abis.json",
+  "https://raw.githubusercontent.com/avalaunch-app/xava-protocol/master/deployments/contract-abis.json"
 );
 if (!XAVA_ABIS.ok) {
   return "loading";
 }
 
 const xavaAllocationIface = new ethers.utils.Interface(
-  JSON.parse(XAVA_ABIS.body).fuji.AllocationStaking,
+  JSON.parse(XAVA_ABIS.body).fuji.AllocationStaking
 );
 
 const ERC20_ABI = fetch(
-  "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json",
+  "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json"
 );
 if (!ERC20_ABI.ok) {
   a;
@@ -66,7 +39,7 @@ const getXavaBalance = () => {
     .then((rawBalance) => {
       const receiverBalanceHex = tokenIface.decodeFunctionResult(
         "balanceOf",
-        rawBalance,
+        rawBalance
       );
 
       return Big(receiverBalanceHex.toString()).div(Big(10).pow(18)).toFixed(2);
@@ -88,7 +61,7 @@ const getStakedBalance = () => {
     .then((rawBalance) => {
       const receiverBalanceHex = xavaAllocationIface.decodeFunctionResult(
         "deposited",
-        rawBalance,
+        rawBalance
       );
 
       return Big(receiverBalanceHex.toString()).div(Big(10).pow(18)).toFixed(2);
@@ -127,7 +100,7 @@ const handleStakeXava = () => {
   let contract = new ethers.Contract(
     XAVA_STAKING,
     xavaAllocationIface,
-    Ethers.provider().getSigner(),
+    Ethers.provider().getSigner()
   );
   contract
     .deposit("0", convertToWei(state.xavaToStake))
@@ -142,31 +115,17 @@ const handleStakeXava = () => {
 };
 
 const css = fetch(
-  "https://gist.githubusercontent.com/Pikqi/658b6ee444d26dd69f0d5150797077dd/raw/d8f929729176bb30d86e2839443fddb83a87a685/tw-all-classes.css",
+  "https://gist.githubusercontent.com/Pikqi/658b6ee444d26dd69f0d5150797077dd/raw/d8f929729176bb30d86e2839443fddb83a87a685/tw-all-classes.css"
 );
 
-if (!css.ok) {
-  return null;
-}
+console.log(css.body);
 
 if (!state.styles) {
   State.update({
     styles: styled.div`
-      font-family:
-        Manrope,
-        -apple-system,
-        BlinkMacSystemFont,
-        Segoe UI,
-        Roboto,
-        Oxygen,
-        Ubuntu,
-        Cantarell,
-        Fira Sans,
-        Droid Sans,
-        Helvetica Neue,
-        sans-serif;
-      ${css.body}
-    `,
+    font-family: Manrope, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    ${css.body}
+`,
   });
 }
 const TWStyles = state.styles;
