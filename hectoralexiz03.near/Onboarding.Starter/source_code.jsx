548ef65c70ff5a7417f1f2209ad1be72cd987b5b
hectoralexiz03.near <hectoralexiz03.near>
2023-07-17 20:40:47 +0000

    Update Onboarding.Starter by hectoralexiz03.near at 2023-07-17T20:40:47.489Z

---
+++ hectoralexiz03.near/Onboarding.Starter/source_code.jsx
@@ -0,0 +1,57 @@
+const user = "gagdiez.near";
+State.init({ nombres: [] });
+State.update({ nombres: props.nombres });
+console.log(props.nombres);
+const props = {
+  name: "What programming language would you like to learn so far?",
+};
+State.init({
+  img: null,
+});
+
+return (
+  <>
+    <div class="container min-vw-100">
+      <div className="d-flex justify-content-center">
+        <h3>PROGRAMMING WORLD</h3>
+      </div>
+      <div className="d-flex p-2">
+        {state.nombres.map((data) => {
+          return <p className="mx-4"> {data.nombre} </p>;
+        })}
+      </div>
+      <p>
+        Learn new programming languages to improve your developing skills to
+        reach a higher sucess in your professional life.
+      </p>
+      <hr />
+      <Widget src={`${user}/widget/Greetings`} props={props} />
+    </div>
+    <div className="d-grid gap-2">
+      <button class="btn btn-primary" type="button">
+        JUNIOR DEVELOPER
+      </button>
+      <button class="btn btn-primary" type="button">
+        SENIOR DEVELOPER
+      </button>
+    </div>
+    <div className="container row">
+      <div>
+        Image upload: <br />
+        <IpfsImageUpload image={state.img} />
+      </div>
+      <div>
+        Raw State:
+        <pre>{JSON.stringify(state)}</pre>
+      </div>
+      <div className="mt-2">
+        {state.img.cid && (
+          <img
+            src={`https://ipfs.near.social/ipfs/${state.img.cid}`}
+            alt="uploaded"
+          />
+        )}
+      </div>
+    </div>
+  </>
+);
