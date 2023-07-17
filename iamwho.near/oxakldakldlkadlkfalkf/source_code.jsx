6428b21ac95d0e23374d46f7fe09320295c89c0e
iamwho.near <iamwho.near>
2023-07-17 15:24:18 +0000

    Update oxakldakldlkadlkfalkf by iamwho.near at 2023-07-17T15:24:18.677Z

--- iamwho.near/oxakldakldlkadlkfalkf/source_code.jsx
+++ iamwho.near/oxakldakldlkadlkfalkf/source_code.jsx
@@ -23,28 +23,26 @@ const fetchContracts = () => {
 if (!contracts) fetchContracts();
 
 return (
-  <div class="flex">
-    <table class="table-auto w-full border border-gray-300 p-4">
-      <thead>
-        <tr>
-          <th class="px-4 py-2">Contract</th>
-          <th class="px-4 py-2">Lang</th>
-          <th class="px-4 py-2">Source</th>
-          <th class="px-4 py-2">Approved</th>
-        </tr>
-      </thead>
-      <tbody>
-        {state.contracts
-          ? state.contracts.map((contract) => (
-              <tr>
-                <td class="border px-4 py-2">{contract[0]}</td>
-                <td class="border px-4 py-2">{contract[1].lang}</td>
-                <td class="border px-4 py-2">{contract[1].cid}</td>
-                <td class="border px-4 py-2">{}</td>
-              </tr>
-            ))
-          : null}
-      </tbody>
-    </table>
-  </div>
+  <table class="table-auto w-full border border-gray-300 p-4">
+    <thead>
+      <tr>
+        <th class="px-4 py-2">Contract</th>
+        <th class="px-4 py-2">Lang</th>
+        <th class="px-4 py-2">Source</th>
+        <th class="px-4 py-2">Approved</th>
+      </tr>
+    </thead>
+    <tbody>
+      {state.contracts
+        ? state.contracts.map((contract) => (
+            <tr>
+              <td class="border px-4 py-2">{contract[0]}</td>
+              <td class="border px-4 py-2">{contract[1].lang}</td>
+              <td class="border px-4 py-2">{contract[1].cid}</td>
+              <td class="border px-4 py-2">{}</td>
+            </tr>
+          ))
+        : null}
+    </tbody>
+  </table>
 );
