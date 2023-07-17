fc3364dcf5714bd7126317a508c017976bdebf6f
rubycop.near <rubycop.near>
2023-07-17 11:49:08 +0000

    Update NDC.Nomination.Page by rubycop.near at 2023-07-17T11:49:08.309Z

--- rubycop.near/NDC.Nomination.Page/source_code.jsx
+++ rubycop.near/NDC.Nomination.Page/source_code.jsx
@@ -398,7 +398,7 @@ return (
             )}
           </div>
         </Left>
-        <Center className="col-lg-9 px-2 px-md-3 d-flex flex-row flex-wrap gap-4">
+        <Center className="col-lg-9 px-2 px-md-3 d-flex flex-row flex-wrap">
           {state.nominations.length > 0 ? (
             state.nominations.map((data) => (
               <Widget src={widgets.card} props={data} />
