d3ce508b09fa1f75a80c452bfe3b252198ecb4dc
microchipgnu.near <microchipgnu.near>
2023-07-17 11:42:21 +0000

    Update TailwindTest by microchipgnu.near at 2023-07-17T11:42:21.241Z

---
+++ microchipgnu.near/TailwindTest/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <div>
+    <Widget
+      src="igris.near/widget/TailwindWrapper"
+      props={{
+        children: <div class="h-screen w-full bg-red-500"></div>,
+      }}
+    ></Widget>
+  </div>
+);
