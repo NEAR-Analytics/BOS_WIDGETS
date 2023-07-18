6eba2104e5dea6b00e07fa856e2c5a29e8754cff
iamwho.near <iamwho.near>
2023-07-18 17:53:17 +0000

    Update szlhyjoIhy by iamwho.near at 2023-07-18T17:53:17.963Z

--- iamwho.near/szlhyjoIhy/source_code.jsx
+++ iamwho.near/szlhyjoIhy/source_code.jsx
@@ -1,1 +1,5 @@
-return <div class="flex justify-center items-center h-screen"></div>;
+return (
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-6xl text-center">Hello World</h1>
+  </div>
+);
