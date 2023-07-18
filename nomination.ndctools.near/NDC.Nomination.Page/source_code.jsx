cec2fc60b3a80bef20f70bec0bca09a1f3b4d640
nomination.ndctools.near <nomination.ndctools.near>
2023-07-18 15:49:19 +0000

    Update NDC.Nomination.Page by nomination.ndctools.near at 2023-07-18T15:49:19.268Z

--- nomination.ndctools.near/NDC.Nomination.Page/source_code.jsx
+++ nomination.ndctools.near/NDC.Nomination.Page/source_code.jsx
@@ -1,32 +1,39 @@
-let { ids, dev } = props;
-ids = props.ids ? ids : [1, 2, 3];
+// TODO: Should be grabbed from contract side
+let {
+  ids,
+  election_contract,
+  registry_contract,
+  nomination_contract,
+  api_key,
+} = props;
+ids = props.ids ? ids : [1, 2, 3]; // for testing purposes
 
-const electionContract = dev
-  ? "elections-v1.gwg-testing.near"
-  : "elections.ndc-gwg.near";
-const registryContract = dev
-  ? "registry-v1.gwg-testing.near"
-  : "registry.i-am-human.near";
-const issuer = dev ? "fractal.i-am-human.near" : "community.i-am-human.near";
-const nominationContract = dev
-  ? "nominations-v1.gwg-testing.near"
-  : "nominations.ndc-gwg.near";
-const apiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
+// https://near.org/rubycop.near/widget/NDC.Nomination.Page?registry_contract=registry-v1.i-am-human.near&nomination_contract=nominations-v1.gwg-testing.near&election_contract=elections-v1.gwg-testing.near&api_key=36f2b87a-7ee6-40d8-80b9-5e68e587a5b5
+
+const electionContract = election_contract ?? "elections-v1.gwg-testing.near";
+const registryContract = registry_contract ?? "registry.i-am-human.near";
+const nominationContract = nomination_contract ?? "nominations.ndc-gwg.near";
+const apiKey = api_key ?? "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
 
 function handleSelfRevoke() {
   Near.call(nominationContract, "self_revoke");
 }
 
+const houses = [
+  Near.view(electionContract, "proposal", { prop_id: ids[0] }),
+  Near.view(electionContract, "proposal", { prop_id: ids[1] }),
+  Near.view(electionContract, "proposal", { prop_id: ids[2] }),
+];
+
 const widgets = {
-  header: "election.ndctools.near/widget/NDC.Elections.Header",
-  card: "nomination.ndctools.near/widget/NDC.Nomination.Card",
-  houses: "election.ndctools.near/widget/NDC.Elections.Houses",
-  filter: "election.ndctools.near/widget/NDC.Elections.Filter",
-  styledComponents: "nomination.ndctools.near/widget/NDC.StyledComponents",
-  verifyHuman: "nomination.ndctools.near/widget/NDC.VerifyHuman",
-  compose: "nomination.ndctools.near/widget/NDC.Nomination.Compose",
-  deleteNomination:
-    "nomination.ndctools.near/widget/NDC.Nomination.DeleteNomination",
+  header: "rubycop.near/widget/NDC.Elections.Header",
+  card: "rubycop.near/widget/NDC.Nomination.Card",
+  houses: "rubycop.near/widget/NDC.Elections.Houses",
+  filter: "rubycop.near/widget/NDC.Elections.Filter",
+  styledComponents: "rubycop.near/widget/NDC.StyledComponents",
+  verifyHuman: "rubycop.near/widget/NDC.VerifyHuman",
+  compose: "rubycop.near/widget/NDC.Nomination.Compose",
+  deleteNomination: "rubycop.near/widget/NDC.Nomination.DeleteNomination",
 };
 
 State.init({
@@ -41,52 +48,49 @@ State.init({
   candidateId: "",
   originNominations: [],
   notFound: "There are no active nominations at the moment",
-  loading: false,
 });
-
-const time = Near.view(nominationContract, "active_time", {});
 
 const httpRequestOpt = {
   headers: { "x-api-key": apiKey },
 };
 
-const baseApi = "https://api.pikespeak.ai";
-
-const endpoints = {
-  candidateComments: `${baseApi}/nominations/candidates-comments-and-upvotes?candidate=${context.accountId}&contract=${nominationContract}`,
-  houseNominations: (house) =>
-    `${baseApi}/nominations/house-nominations?house=${house}&contract=${nominationContract}`,
-};
-
-const getVerifiedHuman = () => {
-  const isHuman = Near.view(registryContract, "is_human", {
-    account: context.accountId,
-  });
-  const ogTokens = Near.view(registryContract, "sbt_tokens", {
-    issuer,
-  });
-
-  asyncFetch(endpoints.candidateComments, httpRequestOpt).then((res) => {
+function getVerifiedHuman() {
+  asyncFetch(
+    `https://api.pikespeak.ai/sbt/sbt-by-owner?holder=${context.accountId}&class_id=1&issuer=fractal.i-am-human.near&with_expired=false`,
+    httpRequestOpt
+  ).then((res) => {
     if (res.body.length > 0) {
-      State.update({ selfNomination: true, start: false });
+      State.update({ sbt: true });
     }
   });
-
-  State.update({
-    og: ogTokens.some((sbt) => sbt.owner === context.accountId),
-    sbt: isHuman[0][1].length > 0,
-    start: false,
+  asyncFetch(
+    `https://api.pikespeak.ai/sbt/sbt-by-owner?holder=${context.accountId}&class_id=2&issuer=fractal.i-am-human.near&with_expired=false`,
+    httpRequestOpt
+  ).then((res) => {
+    if (res.body.length > 0) {
+      State.update({ og: true });
+    }
   });
-};
+  asyncFetch(
+    `https://api.pikespeak.ai/nominations/candidates-comments-and-upvotes?candidate=${context.accountId}`,
+    httpRequestOpt
+  ).then((res) => {
+    if (res.body.length > 0) {
+      State.update({ selfNomination: true });
+    }
+  });
+}
 
-const getNominationInfo = (house) => {
+function getNominationInfo() {
   let nominationsArr = [];
+  asyncFetch(
+    `https://api.pikespeak.ai/nominations/house-nominations?house=${state.house}`,
+    httpRequestOpt
+  ).then((res) => {
+    console.log(res.body);
 
-  State.update({ loading: true, start: false });
-
-  asyncFetch(endpoints.houseNominations(house), httpRequestOpt).then((res) => {
     if (res.body.length <= 0) {
-      State.update({ nominations: [], loading: false, start: false });
+      State.update({ nominations: [] });
       return;
     }
 
@@ -95,7 +99,7 @@ const getNominationInfo = (house) => {
       let nominee = data.nominee;
 
       asyncFetch(
-        `${baseApi}/nominations/candidates-comments-and-upvotes?candidate=${data.nominee}&contract=${nominationContract}`,
+        `https://api.pikespeak.ai/nominations/candidates-comments-and-upvotes?candidate=${data.nominee}`,
         httpRequestOpt
       ).then((info) => {
         let upVoteInfo = info.body[0];
@@ -106,13 +110,10 @@ const getNominationInfo = (house) => {
         setTimeout(() => {
           profileData = Social.getr(`${nominee}/profile`);
           nominationData = Social.getr(`${nominee}/nominations`);
-        }, 2000);
+        }, 1000);
 
         setTimeout(() => {
-          if (data.is_revoked || !profileData || !nominationData) {
-            State.update({ loading: false, start: false });
-            return;
-          }
+          if (data.is_revoked || !profileData || !nominationData) return;
 
           objCard = {
             profileData: profileData,
@@ -122,74 +123,66 @@ const getNominationInfo = (house) => {
           };
 
           nominationsArr.push(objCard);
-          nominationsArr = nominationsArr.sort(
-            (a, b) =>
-              new Date(b.indexerData.timestamp).getTime() -
-              new Date(a.indexerData.timestamp).getTime()
-          );
 
-          State.update({
-            nominations: nominationsArr,
-            originNominations: nominationsArr,
-            loading: false,
-            start: false,
-          });
-        }, 2000);
+          State.update({ nominations: nominationsArr });
+        }, 1000);
       });
     }
   });
-};
+}
+
+if (state.start) {
+  getNominationInfo();
+  getVerifiedHuman();
+  State.update({
+    start: false,
+  });
+}
 
 const handleSelect = (item) => {
   switch (item.id) {
-    case 1:
-      getNominationInfo("HouseOfMerit");
-      break;
     case 2:
-      getNominationInfo("CouncilOfAdvisors");
+      State.update({ house: "CouncilOfAdvisors" });
+      getNominationInfo();
+      break;
+    case 1:
+      State.update({ house: "HouseOfMerit" });
+      getNominationInfo();
       break;
     case 3:
-      getNominationInfo("TransparencyCommission");
+      State.update({ house: "TransparencyCommission" });
+      getNominationInfo();
       break;
   }
   State.update({ selectedHouse: item.id });
 };
 
-const handleFilter = (e) => {
+function handleFilter(e) {
   const text = e.target.value;
 
   State.update({ candidateId: text });
 
-  if (text.length > 0) {
-    let filtered = state.originNominations.filter((data) => {
-      const affiliations = JSON.parse(data.nominationData.afiliation);
-      const companyNames =
-        affiliations?.length > 0 &&
-        affiliations.map((af) => af.company_name.toLowerCase());
-
-      return (
-        data.profileData.name.toLowerCase().includes(text.toLowerCase()) ||
-        (companyNames &&
-          companyNames.some((c) => c.includes(text.toLowerCase())))
-      );
-    });
-
-    if (filtered.length > 0) State.update({ nominations: filtered });
-    else
-      State.update({
-        notFound: "There are no such candidates or affiliations",
-        nominations: [],
-      });
-  } else {
-    State.update({ nominations: state.originNominations });
+  if (!state.search) {
+    State.update({ originNominations: state.nominations, search: true });
   }
-};
-
-if (state.start) {
-  getVerifiedHuman();
-  getNominationInfo("HouseOfMerit");
-
-  State.update({ start: false });
+  if (text.length > 0) {
+    if (state.nominations.length) {
+      State.update({
+        notFound: "There are no such nominations",
+      });
+    }
+    let filtered = state.nominations.filter((data) =>
+      data.profileData.name.toLowerCase().includes(text.toLowerCase())
+    );
+    State.update({ nominations: filtered });
+  } else {
+    State.update({ notFound: "There are no active nominations at the moment" });
+    State.update({
+      nominations: state.originNominations,
+      originNominations: [],
+      search: false,
+    });
+  }
 }
 
 const Container = styled.div`
@@ -209,8 +202,107 @@ const Left = styled.div`
 
 const Center = styled.div``;
 
+const Right = styled.div`
+  padding: 20px;
+  margin-bottom: 20px;
+  background: #f8f8f9;
+  border-radius: 8px;
+`;
+
 const H5 = styled.h5`
   margin-bottom: 20px;
+`;
+
+const VerifiedDiv = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 16px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+  border-radius: 8px;
+  background: var(--ffffff, #fff);
+  box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const VerifiedHeader = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 16px;
+  align-self: stretch;
+`;
+
+const VerifiedHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+  flex: 1 0 0;
+`;
+
+const VerfiedTitle = styled.p`
+  display: flex;
+  width: 176px;
+  flex-direction: column;
+  justify-content: center;
+  color: var(--000000, #000);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 120%;
+  margin: 0px;
+`;
+
+const VerifedDesc = styled.p`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: stretch;
+  color: var(--primary-gray-dark, #828688);
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 120%;
+  margin: 0px;
+`;
+
+const VerifyButton = styled.a`
+  display: flex;
+  padding: 8px 20px;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  gap: 10px;
+  align-self: stretch;
+  border-radius: 10px;
+  background: var(--ffd-50-d, #ffd50d);
+  border: 0px;
+  text-decoration: none;
+`;
+
+const VerifyButtonText = styled.p`
+  color: var(--primary-black, #000);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+  margin: 0px;
+`;
+
+const SortButton = styled.button`
+  display: flex;
+  width: 38px;
+  height: 38px;
+  padding: 8px 12px;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  border-radius: 6px;
+  background: var(
+    --buttons-gradient-default,
+    linear-gradient(90deg, #9333ea 0%, #4f46e5 100%)
+  );
+  border: 0px;
 `;
 
 const ButtonNominateContainer = styled.div`
@@ -234,25 +326,24 @@ const Toolbar = styled.div`
   }
 `;
 
-const Loader = () => (
-  <span
-    className="spinner-grow spinner-grow-sm me-1"
-    role="status"
-    aria-hidden="true"
-  />
-);
-
 return (
   <>
     <div>
-      <Widget
-        src={widgets.header}
-        props={{
-          startTime: time ? time[0] : 0,
-          endTime: time ? time[1] : 0,
-          type: "Nomination",
-        }}
-      />
+      {houses.map((group) => (
+        <>
+          {group.id === state.selectedHouse && (
+            <Widget
+              key={i}
+              src={widgets.header}
+              props={{
+                startTime: group.start,
+                endTime: group.end,
+                type: "Nomination",
+              }}
+            />
+          )}
+        </>
+      ))}
       <Filter className="d-flex">
         <div className="w-100">
           <Widget
@@ -264,16 +355,15 @@ return (
             }}
           />
         </div>
-        {state.og && (
-          <Toolbar>
+        <Toolbar>
+          {state.og && (
             <ButtonNominateContainer>
               {state.selfNomination ? (
                 <Widget
                   src={widgets.styledComponents}
                   props={{
                     Button: {
-                      disabled: true,
-                      className: "danger primary",
+                      className: "danger",
                       text: "Delete Self Nomination",
                       onClick: () => State.update({ showModalDelete: true }),
                       icon: <i class="bi bi-trash"></i>,
@@ -285,7 +375,6 @@ return (
                   src={widgets.styledComponents}
                   props={{
                     Button: {
-                      disabled: true,
                       text: "Self Nominate",
                       onClick: () => State.update({ showModal: true }),
                       icon: <i class="bi bi-plus-lg"></i>,
@@ -294,8 +383,8 @@ return (
                 />
               )}
             </ButtonNominateContainer>
-          </Toolbar>
-        )}
+          )}
+        </Toolbar>
       </Filter>
       <Container className="d-flex row justify-content-between w-100">
         <Left className="col-lg">
@@ -304,7 +393,7 @@ return (
             src={widgets.houses}
             props={{
               selectedHouse: state.selectedHouse,
-              electionContract,
+              houses: houses,
               handleSelect: (item) => handleSelect(item),
             }}
           />
@@ -313,32 +402,28 @@ return (
               <div className="mt-5">
                 <Widget
                   src={widgets.verifyHuman}
-                  props={{
-                    title: "To Comment or to Upvote",
-                    description: "Verify as a Human to comment or to Upvote",
-                    small: true,
-                  }}
+                  props={{ title: "Want to upvote?", small: true }}
                 />
               </div>
             )}
           </div>
         </Left>
         <Center className="col-lg-9 px-2 px-md-3 d-flex flex-row flex-wrap">
-          {state.loading ? (
-            <Loader />
-          ) : state.nominations.length > 0 ? (
+          {state.nominations.length > 0 ? (
             state.nominations.map((data) => (
-              <Widget
-                src={widgets.card}
-                props={{
-                  data,
-                  registry_contract: registryContract,
-                  nomination_contract: nominationContract,
-                  election_contract: electionContract,
-                  api_key: apiKey,
-                  dev,
-                }}
-              />
+              <>
+                {console.log(data)}
+                <Widget
+                  src={widgets.card}
+                  props={{
+                    data,
+                    registry_contract: registryContract,
+                    nomination_contract: nominationContract,
+                    election_contract: electionContract,
+                    api_key: apiKey,
+                  }}
+                />
+              </>
             ))
           ) : (
             <div className="flex mt-10 container-fluid align-self-center">
