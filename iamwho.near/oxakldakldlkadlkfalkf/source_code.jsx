20ff9e9169cb9753893e3c2a3d1574a2a7b96e82
iamwho.near <iamwho.near>
2023-07-18 21:51:59 +0000

    Update oxakldakldlkadlkfalkf by iamwho.near at 2023-07-18T21:51:59.041Z

--- iamwho.near/oxakldakldlkadlkfalkf/source_code.jsx
+++ iamwho.near/oxakldakldlkadlkfalkf/source_code.jsx
@@ -1,20 +1,9 @@
-State.init({
-  contracts: props.contracts,
-});
-
-if (props.contracts)
-  State.update({
-    contracts: props.contracts,
-  });
-
 return (
-  <table class="table border border-gray-300">
+  <table class="table">
     <thead>
       <tr>
-        <th class="px-4 py-2">Contract</th>
-        <th class="px-4 py-2">Lang</th>
-        <th class="px-4 py-2">Source</th>
-        <th class="px-4 py-2">Approved</th>
+        <th class="px-4 py-2">Kek</th>
+        <th class="px-4 py-2">Lol</th>
       </tr>
     </thead>
     <tbody>
