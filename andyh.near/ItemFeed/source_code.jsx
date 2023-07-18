c44947a30dabf9a11c38cb01f4b1b9211aba4e9a
andyh.near <andyh.near>
2023-07-18 21:27:43 +0000

    Update ItemFeed by andyh.near at 2023-07-18T21:27:43.472Z

--- andyh.near/ItemFeed/source_code.jsx
+++ andyh.near/ItemFeed/source_code.jsx
@@ -28,6 +28,6 @@ return (
     hasMore={state.widgets < items.length}
     loader={<div className="loader">Loading ...</div>}
   >
-    {await Promise.all(items.slice(0, state.widgets).map(renderItem))}
+    {items.slice(0, state.widgets).map(renderItem)}
   </InfiniteScroll>
 );
