986c358d7b705b9841f0d94703eb408c14bb66e2
manzanal.near <manzanal.near>
2023-07-18 21:22:07 +0000

    Update TokenWrapper.v2.Index by manzanal.near at 2023-07-18T21:22:07.036Z

--- manzanal.near/TokenWrapper.v2.Index/source_code.jsx
+++ manzanal.near/TokenWrapper.v2.Index/source_code.jsx
@@ -5,28 +5,40 @@ const ETH_CHAINID = 1;
 State.init({
   network: null,
   networkIsLoaded: false,
+  error: null,
 });
 
+console.log("ethers", ethers);
 if (!state.networkIsLoaded) {
-  if (!ethers !== undefined && Ethers.send("eth_requestAccounts", [])[0]) {
+  if (ethers !== undefined && Ethers.send("eth_requestAccounts", [])[0]) {
     Ethers.provider()
       .getNetwork()
       .then((chainIdData) => {
         console.log("chainId", chainIdData.chainId);
         if (chainIdData.chainId === ETH_CHAINID) {
           // ETH
-          State.update({ network: NETWORK_ETH, networkIsLoaded: true });
+          State.update({
+            network: NETWORK_ETH,
+            networkIsLoaded: true,
+            error: null,
+          });
         } else {
-          State.update({ network: NETWORK_NEAR, networkIsLoaded: true });
+          State.update({
+            network: null,
+            networkIsLoaded: true,
+            error: "Wrong network. Please connect to Ethereum mainnet",
+          });
         }
       });
   } else {
     // ethers not supported on this gateway
-    State.update({ network: NETWORK_NEAR, networkIsLoaded: true });
+    State.update({ network: NETWORK_NEAR, networkIsLoaded: true, error: null });
   }
 }
 
+console.log("NETWORK", state.network);
 if (!state.networkIsLoaded) return <>Loading</>;
+if (state.error) return <>{state.error}</>;
 const getContent = {
   NEAR: (
     <Widget src={`${authorId}/widget/TokenWrapper.v2.NearWrapper`} props={{}} />
