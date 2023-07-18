604e174ee361ab8bb53899533ed21e2725ddd69c
iamwho.near <iamwho.near>
2023-07-18 21:52:19 +0000

    Update oxakldakldlkadlkfalkf by iamwho.near at 2023-07-18T21:52:19.097Z

--- iamwho.near/oxakldakldlkadlkfalkf/source_code.jsx
+++ iamwho.near/oxakldakldlkadlkfalkf/source_code.jsx
@@ -6,17 +6,6 @@ return (
         <th class="px-4 py-2">Lol</th>
       </tr>
     </thead>
-    <tbody>
-      {state.contracts
-        ? state.contracts.map((contract) => (
-            <tr>
-              <td class="border px-4 py-2">{contract[0]}</td>
-              <td class="border px-4 py-2">{contract[1].lang}</td>
-              <td class="border px-4 py-2">{contract[1].cid}</td>
-              <td class="border px-4 py-2">{}</td>
-            </tr>
-          ))
-        : null}
-    </tbody>
+    <tbody></tbody>
   </table>
 );
