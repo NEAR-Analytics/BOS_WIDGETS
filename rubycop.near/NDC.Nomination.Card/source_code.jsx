de84453b38d9fd4c7fc7f816efd9830960396f00
rubycop.near <rubycop.near>
2023-07-18 14:55:33 +0000

    Update NDC.Nomination.Card by rubycop.near at 2023-07-18T14:55:33.037Z

--- rubycop.near/NDC.Nomination.Card/source_code.jsx
+++ rubycop.near/NDC.Nomination.Card/source_code.jsx
@@ -38,9 +38,9 @@ function getVerifiedHuman() {
 }
 
 if (state.start) {
-  consolele.log("--0");
+  console.log("--0");
   getVerifiedHuman();
-  consolele.log("--1");
+  console.log("--1");
   State.update({ start: false });
 }
 
