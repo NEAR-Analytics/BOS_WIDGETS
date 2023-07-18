a3e116dd6e206b7c54219b1ec41058e51b76de82
iamwho.near <iamwho.near>
2023-07-18 14:03:33 +0000

    Update oxaoiriaowkldaklf by iamwho.near at 2023-07-18T14:03:33.083Z

---
+++ iamwho.near/oxaoiriaowkldaklf/source_code.jsx
@@ -0,0 +1,6 @@
+return (
+  <div class="border rounded flex items-center justify-center z-0">
+    <p class="z-0 position-absolute">Select a folder</p>
+    <input class="opacity-0 z-10" directory="" webkitdirectory="" type="file" />
+  </div>
+);
