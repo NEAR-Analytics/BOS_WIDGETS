0d38c593a30b92624ed64b90f31e2701d264e463
manzanal.near <manzanal.near>
2023-07-18 21:31:50 +0000

    Update TokenWrapper.v2.Index by manzanal.near at 2023-07-18T21:31:50.840Z

--- manzanal.near/TokenWrapper.v2.Index/source_code.jsx
+++ manzanal.near/TokenWrapper.v2.Index/source_code.jsx
@@ -10,26 +10,39 @@ State.init({
 
 console.log("ethers", ethers);
 if (!state.networkIsLoaded) {
-  if (ethers !== undefined && Ethers.send("eth_requestAccounts", [])[0]) {
-    Ethers.provider()
-      .getNetwork()
-      .then((chainIdData) => {
-        console.log("chainId", chainIdData.chainId);
-        if (chainIdData.chainId === ETH_CHAINID) {
-          // ETH
-          State.update({
-            network: NETWORK_ETH,
-            networkIsLoaded: true,
-            error: null,
-          });
-        } else {
-          State.update({
-            network: null,
-            networkIsLoaded: true,
-            error: "Wrong network. Please connect to Ethereum mainnet",
-          });
-        }
+  if (ethers !== undefined) {
+    console.log(
+      "eth_requestAccounts",
+      Ethers.send("eth_requestAccounts", [])[0]
+    );
+    if (Ethers.send("eth_requestAccounts", [])[0]) {
+      Ethers.provider()
+        .getNetwork()
+        .then((chainIdData) => {
+          console.log("chainId", chainIdData.chainId);
+          if (chainIdData.chainId === ETH_CHAINID) {
+            // ETH
+            State.update({
+              network: NETWORK_ETH,
+              networkIsLoaded: true,
+              error: null,
+            });
+          } else {
+            State.update({
+              network: null,
+              networkIsLoaded: true,
+              error: "Wrong network. Please connect to Ethereum mainnet",
+            });
+          }
+        });
+    } else {
+      // ethers not supported on this gateway
+      State.update({
+        network: NETWORK_NEAR,
+        networkIsLoaded: true,
+        error: null,
       });
+    }
   } else {
     // ethers not supported on this gateway
     State.update({ network: NETWORK_NEAR, networkIsLoaded: true, error: null });
