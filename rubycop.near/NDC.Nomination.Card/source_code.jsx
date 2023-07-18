03db7c29077598345e866e16246beb1eab5c0efa
rubycop.near <rubycop.near>
2023-07-18 15:26:12 +0000

    Update NDC.Nomination.Card by rubycop.near at 2023-07-18T15:26:12.359Z

--- rubycop.near/NDC.Nomination.Card/source_code.jsx
+++ rubycop.near/NDC.Nomination.Card/source_code.jsx
@@ -665,7 +665,7 @@ return (
                       text: `+${data.upVoteData.comments.length ?? 0} Comments`,
                       disabled: !state.verified,
                       size: "sm",
-                      className: "secondary dark",
+                      className: "secondary dark w-100 justify-content-center",
                       onClick: () => {
                         !data.preview ? State.update({ showModal: true }) : "";
                       },
