f4e9a7504fd8a2b4d0fbba75ce2d9ac944d9e646
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-07-18 15:28:38 +0000

    Update SayALot_Reactions by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-07-18T15:28:38.539Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_Reactions/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_Reactions/source_code.jsx
@@ -367,7 +367,7 @@ const Overlay = () => (
     show={state.show}
   >
     {emojiArray &&
-      emojiArray.map((item, index) => (
+      emojiArray.map((item) => (
         <SmallButton onClick={() => clickHandler(item)} isHeart={index === 0}>
           <OverlayTrigger
             placement="top"
@@ -484,19 +484,33 @@ const Spinner = () => {
   );
 };
 
+console.log("UE:", userEmoji);
+
 return (
   <EmojiWrapper>
     <Button onMouseEnter={handleOnMouseEnter} onMouseLeave={handleOnMouseLeave}>
       {state.loading && <Spinner />}
-      {!userEmoji ? initialEmoji : <Stats />}
+      {!userEmoji
+        ? initialEmoji
+        : state.likesStatistics &&
+          state.likesStatistics.map((item) => (
+            <span className="ps-3">
+              <Widget
+                src={`testwiki.near/widget/WikiOnSocialDB_TooltipProfiles`}
+                props={{ accounts: item.accounts, emoji: item.emoji }}
+              />
+            </span>
+          ))}
     </Button>
-    {!userEmoji ? (
-      <Reactions>
-        <Stats />
-      </Reactions>
-    ) : (
-      <></>
-    )}
     <Overlay />
+    {state.likesStatistics &&
+      state.likesStatistics.map((item) => (
+        <span className="ps-3">
+          <Widget
+            src={`testwiki.near/widget/WikiOnSocialDB_TooltipProfiles`}
+            props={{ accounts: item.accounts, emoji: item.emoji }}
+          />
+        </span>
+      ))}
   </EmojiWrapper>
 );
