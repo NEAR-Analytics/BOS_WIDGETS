7e06b46b2c8e5c577fc2c89c7dc6eb66be5c4806
manzanal.near <manzanal.near>
2023-07-18 20:41:48 +0000

    Update TokenWrapper.v2.Index by manzanal.near at 2023-07-18T20:41:48.890Z

--- manzanal.near/TokenWrapper.v2.Index/source_code.jsx
+++ manzanal.near/TokenWrapper.v2.Index/source_code.jsx
@@ -1,7 +1,31 @@
 const authorId = "manzanal.near";
 const NETWORK_NEAR = "NEAR";
 const NETWORK_ETH = "ETH";
+const ETH_CHAINID = 1;
+State.init({
+  network: null,
+  networkIsLoaded: false,
+});
 
+console.log("ethers", ethers);
+if (!state.networkIsLoaded) {
+  if (!ethers !== undefined && Ethers.send("eth_requestAccounts", [])[0]) {
+    Ethers.provider()
+      .getNetwork()
+      .then((chainIdData) => {
+        console.log("chainId", chainIdData.chainId);
+        if (chainIdData.chainId === ETH_CHAINID) {
+          // ETH
+          State.update({ network: NETWORK_ETH, networlIsLoaded: true });
+        } else {
+          State.update({ network: NETWORK_NEAR, networlIsLoaded: true });
+        }
+      });
+  } else {
+    // ethers not supported on this gateway
+    State.update({ network: NETWORK_NEAR, networlIsLoaded: true });
+  }
+}
 const getContent = {
   NETWORK_NEAR: (
     <Widget src={`${authorId}/widget/TokenWrapper.v2.NearWrapper`} props={{}} />
@@ -12,5 +36,5 @@ const getContent = {
 }[state.network];
 
 console.log("NETWORK", state.network);
-
-return getContent;
+if (!state.networkIsLoaded) return <>Loading</>;
+return <>{getContent}</>;
