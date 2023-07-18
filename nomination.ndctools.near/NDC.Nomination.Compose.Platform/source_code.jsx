3b25a3a5fc721f26a009cdb7af8421eb0820c748
nomination.ndctools.near <nomination.ndctools.near>
2023-07-18 16:09:17 +0000

    Update NDC.Nomination.Compose.Platform by nomination.ndctools.near at 2023-07-18T16:09:17.723Z

--- nomination.ndctools.near/NDC.Nomination.Compose.Platform/source_code.jsx
+++ nomination.ndctools.near/NDC.Nomination.Compose.Platform/source_code.jsx
@@ -5,7 +5,7 @@ const Section = styled.div`
 `;
 
 const widgets = {
-  styledComponents: "rubycop.near/widget/NDC.StyledComponents",
+  styledComponents: "nomination.ndctools.near/widget/NDC.StyledComponents",
 };
 
 return (
