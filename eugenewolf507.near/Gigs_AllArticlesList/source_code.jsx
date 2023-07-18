28e549731c861a180dc95e07522644ecb983a5d6
eugenewolf507.near <eugenewolf507.near>
2023-07-18 12:25:11 +0000

    Update Gigs_AllArticlesList by eugenewolf507.near at 2023-07-18T12:25:11.504Z

--- eugenewolf507.near/Gigs_AllArticlesList/source_code.jsx
+++ eugenewolf507.near/Gigs_AllArticlesList/source_code.jsx
@@ -12,6 +12,7 @@ const authorsWhitelist = props.writersWhiteList ?? [
   "sarahkornfeld.near",
   "yuensid.near",
   "eugenewolf507.near",
+  "inc4social.near",
 ];
 const sharedArticleId = props.articleId;
 const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
