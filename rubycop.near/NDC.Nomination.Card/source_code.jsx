5e3d3fd794b9d1b4cc4fe35d6de855ea6abcc074
rubycop.near <rubycop.near>
2023-07-18 12:31:47 +0000

    Update NDC.Nomination.Card by rubycop.near at 2023-07-18T12:31:47.422Z

--- rubycop.near/NDC.Nomination.Card/source_code.jsx
+++ rubycop.near/NDC.Nomination.Card/source_code.jsx
@@ -677,7 +677,7 @@ return (
                       text: "View",
                       size: "sm",
                       className: "secondary dark w-100 justify-content-center",
-                      href: `${candidatePage}?house=${data.indexerData.house}&candidate=${data.indexerData.nominee}`,
+                      href: `${widgets.candidatePage}?house=${data.indexerData.house}&candidate=${data.indexerData.nominee}`,
                       icon: <i className="bi bi-eye fs-6"></i>,
                     },
                   }}
