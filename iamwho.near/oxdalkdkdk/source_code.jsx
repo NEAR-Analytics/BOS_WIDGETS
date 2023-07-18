5f419672e5d1e50cbc82ffc3b3c87c5a8bc7174c
iamwho.near <iamwho.near>
2023-07-18 16:54:50 +0000

    Update oxdalkdkdk by iamwho.near at 2023-07-18T16:54:50.764Z

---
+++ iamwho.near/oxdalkdkdk/source_code.jsx
@@ -0,0 +1,36 @@
+State.init({ files: [] });
+
+const handleAddFile = (files) => {
+  files.forEach((file) => {
+    console.log("file", file);
+    State.update((prev) => {
+      return {
+        files: [...prev.files, file],
+      };
+    });
+  });
+};
+
+console.log("state_files: ", state.files);
+
+return (
+  <div className="d-inline-block">
+    <Files
+      multiple={true}
+      accepts={[".lock", ".json", ".toml", ".ts", ".rs"]}
+      minFileSize={1}
+      clickable
+      className="btn btn-outline-primary"
+      onChange={(files) => handleAddFile(files)}
+    >
+      {"Add file"}
+    </Files>
+    {state.files.length > 0 ? (
+      <ul class="list-disc list-inside">
+        {state.files.map((file) => (
+          <li>{file.name}</li>
+        ))}
+      </ul>
+    ) : null}
+  </div>
+);
