c49ec7a432c648aa930bc0df23b9178fce15521c
nomination.ndctools.near <nomination.ndctools.near>
2023-07-19 13:01:35 +0000

    Update NDC.Nomination.Compose by nomination.ndctools.near at 2023-07-19T13:01:35.983Z

--- nomination.ndctools.near/NDC.Nomination.Compose/source_code.jsx
+++ nomination.ndctools.near/NDC.Nomination.Compose/source_code.jsx
@@ -128,7 +128,7 @@ const Modal = styled.div`
 
 const ComponentWrapper = styled.div`
   display: flex;
-  width: 70%;
+  width: 80%;
   height: 80%;
   flex-direction: column;
   align-items: flex-start;
