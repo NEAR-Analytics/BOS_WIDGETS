40a03334c373fba5f8cefdd235e543a02f7b86da
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-07-19 16:41:48 +0000

    Update SayALot_Comment.Compose by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-07-19T16:41:48.984Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_Comment.Compose/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_Comment.Compose/source_code.jsx
@@ -3,6 +3,7 @@ const isDebug = props.isDebug;
 const addressForComments = isDebug
   ? "test_sayalot-comments"
   : "sayalot-comments";
+
 const addressForArticles = isDebug ? "test_sayALotArticle" : "sayALotArticle";
 const authorForWidget =
   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
