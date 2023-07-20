fa9b3fdd91bae8f4b14efed942f54a08e8c797d7
dev-queryapi.dataplatform.near <dev-queryapi.dataplatform.near>
2023-07-20 19:04:38 +0000

    Update QueryApi.dev.Feed by dev-queryapi.dataplatform.near at 2023-07-20T19:04:38.812Z

--- dev-queryapi.dataplatform.near/QueryApi.dev.Feed/source_code.jsx
+++ dev-queryapi.dataplatform.near/QueryApi.dev.Feed/source_code.jsx
@@ -1,14 +1,25 @@
 const GRAPHQL_ENDPOINT =
-  "https://queryapi-hasura-graphql-vcqilefdcq-ew.a.run.app";
+  "https://near-queryapi.dev.api.pagoda.co";
 const APP_OWNER = "dev-queryapi.dataplatform.near";
 
+let accountsFollowing = undefined;
+
+if (context.accountId) {
+  const graph = Social.keys(`${context.accountId}/graph/follow/*`, "final");
+  console.log('graph', graph)
+  if (graph !== null) {
+    accountsFollowing = Object.keys(graph[context.accountId].graph.follow || {});
+    accountsFollowing.push(context.accountId);
+  }
+}
 
 return (
   <Widget
-    src={`${APP_OWNER}/widget/QueryApi.Examples.Feed`}
+    src={`${APP_OWNER}/widget/QueryApi.Examples.Feed.Posts`}
     props={{
       GRAPHQL_ENDPOINT,
       APP_OWNER,
+      accountsFollowing
     }}
   />
 );
