11725d95dd5ef3557650b305b3bb017cb7ea151e
dokxo.near <dokxo.near>
2023-07-20 21:58:04 +0000

    Update FASTSBT by dokxo.near at 2023-07-20T21:58:04.369Z

--- dokxo.near/FASTSBT/source_code.jsx
+++ dokxo.near/FASTSBT/source_code.jsx
@@ -521,6 +521,7 @@ return (
                     width: "100%",
                     height: "100%",
                     "object-fit": "cover",
+                    "vertical-align": " initial",
                   }}
                   src={CheckIcon}
                 />
