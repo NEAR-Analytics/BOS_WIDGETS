ee5f5d0142dc0d362f2936d9929e1909b294c4b8
election.ndctools.near <election.ndctools.near>
2023-07-20 16:04:36 +0000

    Update NDC.Elections.Candidates by election.ndctools.near at 2023-07-20T16:04:36.395Z

--- election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
@@ -378,7 +378,6 @@ const CandidateList = ({ candidateId, votes }) => (
               style: { width: "24px", height: "24px", marginRight: 4 },
             }}
           />
-          sdf
           <UserLink
             src={`https://www.near.org/near/widget/ProfilePage?accountId=${candidateId}`}
             title={candidateId}
