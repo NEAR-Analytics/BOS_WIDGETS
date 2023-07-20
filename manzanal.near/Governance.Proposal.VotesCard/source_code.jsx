508923ef239a28dbb789fcbbfe001eb0cae6dde9
manzanal.near <manzanal.near>
2023-07-20 20:28:06 +0000

    Update Governance.Proposal.VotesCard by manzanal.near at 2023-07-20T20:28:06.513Z

--- manzanal.near/Governance.Proposal.VotesCard/source_code.jsx
+++ manzanal.near/Governance.Proposal.VotesCard/source_code.jsx
@@ -1,6 +1,6 @@
 const accountId = context.accountId;
 const authorId = "manzanal.near";
-const contractId = props.contractId || "v004.mpip.near";
+const contractId = props.contractId || "v005.mpip.near";
 const accountVotes = props.accountVotes;
 console.log("votes", accountVotes);
 const Container = styled.div`
