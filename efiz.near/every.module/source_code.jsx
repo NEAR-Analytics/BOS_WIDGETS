045f2062cc2c4e06b52b7a3cc01bcc68da1a3b36
efiz.near <efiz.near>
2023-07-20 16:54:09 +0000

    Update every.module by efiz.near at 2023-07-20T16:54:09.499Z

--- efiz.near/every.module/source_code.jsx
+++ efiz.near/every.module/source_code.jsx
@@ -36,4 +36,37 @@ function normalizePath(path) {
   return path.replace(/\//g, "_");
 }
 
+function generateUID() {
+  return (
+    Math.random().toString(16).slice(2) +
+    Date.now().toString(36) +
+    Math.random().toString(16).slice(2)
+  );
+}
+
+function createThing({ data, type, thingId, onCommit, onCancel }) {
+  thingId = thingId || generateUID();
+
+  const save = {
+    every: {
+      thing: {
+        [thingId]: JSON.stringify(data),
+      },
+    },
+    index: {
+      every: JSON.stringify({
+        key: type,
+        value: {
+          thingId,
+          type,
+        },
+      }),
+    },
+  };
+  Social.set(save, {
+    onCommit,
+    onCancel,
+  });
+}
+
 return { mergeItems, Grid, normalizePath };
