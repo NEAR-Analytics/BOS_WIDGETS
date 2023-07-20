1a90bff6aadc61830177f29aa82546d7ea70d8f4
dev-queryapi.dataplatform.near <dev-queryapi.dataplatform.near>
2023-07-20 19:14:03 +0000

    Update QueryApi.dev.Feed by dev-queryapi.dataplatform.near at 2023-07-20T19:14:03.054Z

--- dev-queryapi.dataplatform.near/QueryApi.dev.Feed/source_code.jsx
+++ dev-queryapi.dataplatform.near/QueryApi.dev.Feed/source_code.jsx
@@ -1,12 +1,11 @@
 const GRAPHQL_ENDPOINT =
-  "https://near-queryapi.dev.api.pagoda.co";
+  "https://near-queryapi.api.pagoda.co";
 const APP_OWNER = "dev-queryapi.dataplatform.near";
 
 let accountsFollowing = undefined;
 
 if (context.accountId) {
   const graph = Social.keys(`${context.accountId}/graph/follow/*`, "final");
-  console.log('graph', graph)
   if (graph !== null) {
     accountsFollowing = Object.keys(graph[context.accountId].graph.follow || {});
     accountsFollowing.push(context.accountId);
