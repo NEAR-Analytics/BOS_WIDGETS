9beee750e6ffc8543b016f7ebf88c21527706d92
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-07-20 01:02:41 +0000

    Update SayALot by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-07-20T01:02:41.160Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot/source_code.jsx
@@ -59,7 +59,7 @@ return (
     <div style={{ margin: "0 auto", width: "90%", minWidth: "360px" }}>
       {author && (
         <h4
-          style={{ marginTop: "1.5rem" }}
+          style={{ margin: "1.5rem 0 0 0" }}
         >{`${authorProfile.name}'s articles`}</h4>
       )}
       <Widget
