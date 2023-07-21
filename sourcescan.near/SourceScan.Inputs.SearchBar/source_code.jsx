e6decf2e9b38d657da0ce6cab89ca45271b8ae73
sourcescan.near <sourcescan.near>
2023-07-21 17:08:30 +0000

    Update SourceScan.Inputs.SearchBar by sourcescan.near at 2023-07-21T17:08:30.254Z

---
+++ sourcescan.near/SourceScan.Inputs.SearchBar/source_code.jsx
@@ -0,0 +1,56 @@
+State.init({
+  theme: props.theme || "light",
+  value: "",
+});
+
+const dark = {
+  bg: "#28282b",
+  color: "#e6eaee",
+  border: "#748094",
+};
+
+const light = {
+  bg: "#e3e8ef",
+  color: "#4c5566",
+  border: "#748094",
+};
+
+const useTheme = (light, dark) => {
+  return state.theme === "light" ? light : dark;
+};
+
+const handleChange = (e) => {
+  State.update({ value: e.target.value });
+};
+
+const SearchInput = styled.input`
+  border-radius: 5px;
+  padding-left: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  border: 1px solid ${useTheme(light, border, dark.border)};
+  color: ${useTheme(light.color, dark.color)};
+  background-color: ${useTheme(light.bg, dark.bg)}; 
+`;
+
+const SearchButton = styled.button`
+  border-radius: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  border: 1px solid ${useTheme(light, border, dark.border)};
+  color: ${useTheme(light.color, dark.color)};
+  background-color: ${useTheme(light.bg, dark.bg)}; 
+`;
+
+return (
+  <>
+    <SearchInput
+      placeholder={"Account ID"}
+      value={state.value}
+      onChange={handleChange}
+    />
+    <SearchButton onClick={() => props.handleSubmit(state.value)}>
+      Search
+    </SearchButton>
+  </>
+);
