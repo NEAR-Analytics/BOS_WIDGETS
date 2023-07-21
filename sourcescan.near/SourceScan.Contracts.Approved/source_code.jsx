756bf74228be0b3f30ae7d8622289b8aac04f37b
sourcescan.near <sourcescan.near>
2023-07-21 21:29:15 +0000

    Update SourceScan.Contracts.Approved by sourcescan.near at 2023-07-21T21:29:15.498Z

---
+++ sourcescan.near/SourceScan.Contracts.Approved/source_code.jsx
@@ -0,0 +1,35 @@
+const compareWasm = () => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: "dontcare",
+      method: "query",
+      params: {
+        request_type: "view_code",
+        finality: "final",
+        account_id: props.accountId,
+      },
+    }),
+  };
+  asyncFetch(props.rpcUrl, options)
+    .then((rpc_res) => {
+      asyncFetch(`props.apiHost/ipfs/${props.cid}/wasm_code_base64`)
+        .then((ipfs_res) => {
+          return rpc_res.data === ipfs_res.data.result.code_base64;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+console.log(props.cid, compareWasm());
+
+return <></>;
