806385e3c2d74d365a8b0fc2147ef33ca6b09d60
efiz.near <efiz.near>
2023-07-21 15:02:17 +0000

    Update Modal by efiz.near at 2023-07-21T15:02:17.406Z

--- efiz.near/Modal/source_code.jsx
+++ efiz.near/Modal/source_code.jsx
@@ -1,1 +1,45 @@
-return <div>Hello World</div>;
+const ModalBackdrop = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// Modal content styled component
+const ModalContent = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+// Close button styled component
+const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: transparent;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <ModalBackdrop>
+      <ModalContent>
+        <CloseButton onClick={onClose}>X</CloseButton>
+        {children}
+      </ModalContent>
+    </ModalBackdrop>
+  );
+};
+
+return { Modal };
