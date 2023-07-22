573def6293e7aead7457c5f8d31051acc3579a39
4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd <4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd>
2023-07-22 14:55:45 +0000

    Update Merchant.info by 4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd at 2023-07-22T14:55:45.741Z

---
+++ 4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd/Merchant.info/source_code.jsx
@@ -0,0 +1,133 @@
+if (!props.collectionAddress || !props.abi) {
+  return (
+    <tr>
+      <td colspan="6">Data unavailable</td>
+    </tr>
+  );
+}
+
+const contract = props.collectionAddress;
+const abi = props.abi;
+
+const iface = new ethers.utils.Interface(abi);
+
+const collectionContract = new ethers.Contract(
+  contract,
+  abi,
+  Ethers.provider().getSigner()
+);
+
+State.init({
+  circulating_supply: 0,
+  max_supply: 0,
+  price: 0,
+  total_earnings: 0,
+});
+
+const getTotalEarnings = () => {
+  const tierValue = 0;
+  const tierHex = "0x" + tierValue.toString(16).padStart(64, "0");
+  const filter = {
+    address: contractAddress,
+    topics: [
+      ethers.utils.id("SubscriptionUpdate(uint256,address,uint256)"),
+      tierHex,
+    ],
+    fromBlock: 0,
+    toBlock: "latest",
+  };
+  Ethers.provider()
+    .getLogs(filter)
+    .then((logs) => {
+      const tot = state.price * logs.length;
+      //console.log("Total earning: ", tot);
+      State.update({ total_earnings: tot });
+    })
+    .catch((err) => console.log(err));
+};
+
+const getCirculatingSupply = () => {
+  const encodedData = iface.encodeFunctionData("getTierSupply", [0, false]);
+
+  return Ethers.provider()
+    .call({
+      to: contract,
+      data: encodedData,
+    })
+    .then((circulating) => {
+      const circulating_supply = iface
+        .decodeFunctionResult("getTierSupply", circulating)
+        .toString();
+      //console.log("Circulating supply: ", circulating_supply);
+      State.update({ circulating_supply: circulating_supply });
+    });
+};
+
+const getMaxSupply = () => {
+  const encodedData = iface.encodeFunctionData("getTierSupply", [0, true]);
+
+  return Ethers.provider()
+    .call({
+      to: contract,
+      data: encodedData,
+    })
+    .then((total) => {
+      const max_supply = iface
+        .decodeFunctionResult("getTierSupply", total)
+        .toString();
+      //console.log("Total supply: ", max_supply);
+      State.update({ max_supply: max_supply == -1 ? "-" : max_supply });
+    });
+};
+
+const getCollectionName = () => {
+  const encodedData = iface.encodeFunctionData("collectionName", []);
+
+  return Ethers.provider()
+    .call({
+      to: contract,
+      data: encodedData,
+    })
+    .then((n) => {
+      const name = iface.decodeFunctionResult("collectionName", n)[0];
+      //console.log("Name: ", name);
+      State.update({ name: name });
+    });
+};
+
+const getPrice = () => {
+  const encodedData = iface.encodeFunctionData("getTierPrice", [0]);
+
+  return Ethers.provider()
+    .call({
+      to: contract,
+      data: encodedData,
+    })
+    .then((p) => {
+      const price = iface.decodeFunctionResult("getTierPrice", p);
+      const _price = Big(price.toString()).div(Big(10).pow(18)).toString();
+      //console.log("Price: ", _price);
+      State.update({ price: _price });
+    });
+};
+
+if (Ethers.provider()) {
+  getTotalEarnings();
+  getCirculatingSupply();
+  getMaxSupply();
+  getCollectionName();
+  getPrice();
+} else return <Web3Connect connectLabel="Connect with Web3" />;
+
+return (
+  <tr>
+    <th scope="row" class="text-white">
+      {state.name}
+    </th>
+    <td class="text-white">{state.circulating_supply}</td>
+    <td class="text-white">{state.max_supply}</td>
+    <td class="text-white">{state.price}</td>
+    <td class="text-white">{state.total_earnings}</td>
+    <td class="text-white">{new Date().toLocaleDateString()}</td>
+  </tr>
+);
