81f6e9b3eb2ca891ac3bbdde0d49be2389fbcefc
4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd <4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd>
2023-07-22 22:51:36 +0000

    Update Merchant.view by 4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd at 2023-07-22T22:51:36.000Z

--- 4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd/Merchant.view/source_code.jsx
+++ 4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd/Merchant.view/source_code.jsx
@@ -26,7 +26,7 @@ if (!factoryABI.ok) {
 
 if (!Ethers.provider()) {
   return (
-    <div class="row d-flex justify-content-center">
+    <div class="row d-flex justify-content-center h-100 align-items-center">
       <div class="col-8 col-md-4 text-center">
         <Web3Connect connectLabel="Connect with Web3" />
       </div>
