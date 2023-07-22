8f4b045eb92c7a9ce155eeb5da13b261e31993f8
achildhoodhero.near <achildhoodhero.near>
2023-07-22 13:56:15 +0000

    Update bosweek.menu.content by achildhoodhero.near at 2023-07-22T13:56:15.126Z

---
+++ achildhoodhero.near/bosweek.menu.content/source_code.jsx
@@ -0,0 +1,45 @@
+const defaultWidgets = [
+  {
+    src: "nearweekapp.near/widget/nearweek-newsletter",
+  },
+  {
+    src: "achildhoodhero.near/widget/hero.certified-apps.bos",
+  },
+  {
+    src: "nearweekapp.near/widget/NEARWEEKNews",
+    requiresLogin: false,
+  },
+  {
+    src: "achildhoodhero.near/widget/NEARWEEK-DAO-Submission-fork",
+  },
+];
+
+// Ignore any existing menu and only use your own components
+const widgets = defaultWidgets;
+
+const Div = styled.div`
+  position: relative;
+  @media (hover: hover) {
+    > .edit-link {
+      display: none;
+    }
+  }
+  &:hover {
+    > .edit-link {
+      display: inline;
+    }
+  }
+`;
+
+return (
+  <Div>
+    {widgets.map(
+      ({ src, requiresLogin }, i) =>
+        (!requiresLogin || context.accountId) && (
+          <div key={i} className="text-bg-light rounded-4 p-3 mb-3">
+            <Widget src={src} />
+          </div>
+        )
+    )}
+  </Div>
+);
