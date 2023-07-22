fd395979360c7a3380d0ad006f392a6aaf17284e
mattb.near <mattb.near>
2023-07-22 15:06:12 +0000

    Update Geoquete.Views.Home by mattb.near at 2023-07-22T15:06:12.032Z

---
+++ mattb.near/Geoquete.Views.Home/source_code.jsx
@@ -0,0 +1,19 @@
+let views = {
+  home: (
+    <>
+      <Widget src="mattb.near/widget/Geoquete.Components.CreateQuest" />
+    </>
+  ),
+  join: (
+    <>
+      <Widget src="mattb.near/widget/Geoquete.Components.QuestList" />
+    </>
+  ),
+};
+
+return (
+  <>
+    <Widget src="mattb.near/widget/Geoquete.Components.Header" />
+    {views["join"]}
+  </>
+);
