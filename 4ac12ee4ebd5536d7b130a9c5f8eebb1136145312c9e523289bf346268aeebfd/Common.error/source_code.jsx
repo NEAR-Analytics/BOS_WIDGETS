88540a493e887922c3d2ceca49f117cbed3b33c8
4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd <4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd>
2023-07-22 22:59:15 +0000

    Update Common.error by 4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd at 2023-07-22T22:59:15.994Z

--- 4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd/Common.error/source_code.jsx
+++ 4ac12ee4ebd5536d7b130a9c5f8eebb1136145312c9e523289bf346268aeebfd/Common.error/source_code.jsx
@@ -1,5 +1,9 @@
 return (
-  <span class="alert alert-danger">
-    <b>Error</b>: {props.message ?? "An error occurred."}
-  </span>
+  <div class="row justify-content-center d-flex py-3">
+    <div class="col-10 col-md-4 text-center">
+      <span class="alert alert-danger">
+        <b>Error</b>: {props.message ?? "An error occurred."}
+      </span>
+    </div>
+  </div>
 );
