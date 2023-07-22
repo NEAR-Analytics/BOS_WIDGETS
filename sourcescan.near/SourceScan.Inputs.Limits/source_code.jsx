517dc6240bb66ba759a57f19905f41f1c4148a89
sourcescan.near <sourcescan.near>
2023-07-22 13:39:07 +0000

    Update SourceScan.Inputs.Limits by sourcescan.near at 2023-07-22T13:39:07.720Z

--- sourcescan.near/SourceScan.Inputs.Limits/source_code.jsx
+++ sourcescan.near/SourceScan.Inputs.Limits/source_code.jsx
@@ -1,4 +1,6 @@
 State.init({
+  placement: props.placement || "top",
+  label: props.label || "Contracts per page",
   selectedLimit: props.selectedLimit || props.limits[0] || 10,
   limits: props.limits || [10, 20, 50],
   theme: props.theme || "light",
@@ -30,11 +32,17 @@ const Select = styled.select`
 `;
 
 return (
-  <Select onChange={(e) => props.handleOptionsChange(e)}>
-    {state.limits.map((limit) => (
-      <option value={limit} selected={state.selectedLimit === limit}>
-        {limit}
-      </option>
-    ))}
-  </Select>
+  <OverlayTrigger
+    key={state.placement}
+    placement={state.placement}
+    overlay={<Tooltip id={`tooltip-${placement}`}>{state.label}</Tooltip>}
+  >
+    <Select onChange={(e) => props.handleOptionsChange(e)}>
+      {state.limits.map((limit) => (
+        <option value={limit} selected={state.selectedLimit === limit}>
+          {limit}
+        </option>
+      ))}
+    </Select>
+  </OverlayTrigger>
 );
