bb9556c8a05270198b2180805d962c235105ad27
ngmiai.near <ngmiai.near>
2023-07-22 21:07:25 +0000

    Update minotaur by ngmiai.near at 2023-07-22T21:07:25.584Z

--- ngmiai.near/minotaur/source_code.jsx
+++
@@ -1,8 +0,0 @@
-return (
-  <div style={{ width: "1000px", height: "700px", overflow: "auto" }}>
-    <iframe
-      src="https://gateway.lighthouse.storage/ipfs/QmbJSBJvVnG1hGUzC9tcV68KY44oSYVTqm8CfDZ4y8DccL/"
-      style={{ width: "100%", height: "100%", border: "none" }}
-    />
-  </div>
-);
