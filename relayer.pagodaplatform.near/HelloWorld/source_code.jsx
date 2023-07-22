058b63df65066124b185fb093fe1e0efaaaf5a81
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-07-22 07:39:17 +0000

    Update HelloWorld by relayer.pagodaplatform.near at 2023-07-22T07:39:17.081Z

--- relayer.pagodaplatform.near/HelloWorld/source_code.jsx
+++ relayer.pagodaplatform.near/HelloWorld/source_code.jsx
@@ -1,35 +1,6 @@
-const sender = Ethers.send("eth_requestAccounts", [])[0];
-if (!sender) return "You need to login first";
-const nftCollectionAddress = "0xE652f7eF26D4Ed34de2d83d4EEBf291BD60ed2AF";
-const nftCollectionABI = [
-  {
-    inputs: [{ internalType: "address", name: "owner", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-];
-
-const getNftContract = async () => {
-  const signer = Ethers.provider().getSigner();
-  const nftContract = new ethers.Contract(
-    nftCollectionAddress,
-    nftCollectionABI,
-    signer
-  );
-  nftContract
-    .balanceOf(sender)
-    .then((res) => {
-      console.log(parseInt(res["_hex"], 16));
-    })
-    .catch((error) => console.log(error));
-};
-
 return (
-  <div>
-    <h1>NFT Gateway</h1>
-    <p>You are logged in as : {sender}</p>
-    <button onClick={getNftContract}>Login to DAO</button>
-  </div>
+  <iframe
+    iframeResizer
+    src="https://gateway.lighthouse.storage/ipfs/QmZb4fkZ8rkse3Qsqv589CL9crDV4tfTCN2Qsv77fmiZch/"
+  />
 );
