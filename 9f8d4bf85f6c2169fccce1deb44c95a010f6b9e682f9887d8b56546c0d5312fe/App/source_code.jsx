767be7a556a8be62060c88d641b25c4de62f85ea
9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe <9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe>
2023-07-22 20:26:06 +0000

    Update App by 9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe at 2023-07-22T20:26:06.656Z

---
+++ 9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/App/source_code.jsx
@@ -0,0 +1,35 @@
+const sender = Ethers.send("eth_requestAccounts", [])[0];
+
+State.init({
+  page: "default",
+});
+
+const props = {
+  updatePage: () => {
+    State.update({
+      page: "mint",
+    });
+  },
+};
+
+if (!sender) State.update({ page: "default" });
+if (sender && state.page === "default") State.update({ page: "score" });
+
+if (state.page === "default") {
+  return (
+    <div>
+      <Widget src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/MainSPPage" />
+    </div>
+  );
+}
+
+if (state.page === "score") {
+  return (
+    <Widget
+      src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/ScorerPage"
+      props={props}
+    />
+  );
+}
+
+return state.page;
