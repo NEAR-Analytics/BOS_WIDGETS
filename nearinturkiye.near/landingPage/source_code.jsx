a6a9241bdaf680f4709a5f5dd45d1d47021ea35b
nearinturkiye.near <nearinturkiye.near>
2023-07-22 18:46:35 +0000

    Update landingPage by nearinturkiye.near at 2023-07-22T18:46:35.737Z

--- nearinturkiye.near/landingPage/source_code.jsx
+++ nearinturkiye.near/landingPage/source_code.jsx
@@ -1,57 +1,228 @@
-State.init({
-  value: "value to update",
-});
+const fontUrl = ``;
 
-console.log(Near.view("nearinturkiye.near", "socialMedia"));
+const className = "image-container";
 
-const testCall = () => {
-  return Near.call("nearinturkiye.near", "socialMedia", {
-    message: "Hi Near Social",
+const css = `
+@font-face {
+    font-family: "Pixter";
+    src: url("${fontUrl}");
+}
+
+a, a:focus, a:visited, a:hover {
+  color: white !important;
+}
+
+.apps {
+  margin-top: 32px;
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+}
+
+
+
+
+
+
+.image-container {
+  z-index: 2;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 0;
+  left: 0;
+ clip-path: polygon(
+    0 10%,
+    10% 10%,
+    10% 0,
+    90% 0,
+    90% 10%,
+    100% 10%,
+    100% 90%,
+    90% 90%,
+    90% 100%,
+    10% 100%,
+    10% 90%,
+    0% 90%,
+    0% 10%
+  );
+ > img {
+  height: 100%;
+  object-fit: contain;
+  overflow: hidden;
+  background: white;
+ }
+}
+
+.header {
+    position: relative;
+    display: flex;
+    overflow: hidden;
+    width: 100%;
+
+    > div {
+        padding: 36px;
+        h1 {
+        font-size: 3rem;
+        }
+        h2 {
+            color: #ddd;
+        }
+        > div {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            height: 100%;
+        }
+    }
+}
+
+.header-left {
+    background: linear-gradient(253.16deg, #FF0000 1.53%, #FF0000 86.25%);
+    width: 55%;
+    height: 260px;
+  transform-origin: 100% 0;
+  transform: skew(-10deg);
+  border-right: 4px solid black;
+}
+
+.header-right {
+    margin-left: -100px;
+    background: linear-gradient(255.45deg, #00000 12.67%, #00000 87.81%);
+    width: calc(45% + 100px);
+    height: 260px;
+}
+
+.noise {
+    mix-blend-mode: soft-light;
+    position: absolute;
+    z-index: 1;
+    opacity: 0.5;
+  width: 100vw;
+  height: 260px;
+  background: url('https://ipfs.near.social/ipfs/bafybeib7kr66dgccanis5n3q5fhmwg2ff42n2csy6peoge4buylwzkj3qa');
+ }
+
+ .header-content-left {
+     position: absolute;
+     z-index: 2;
+     height: 260px;
+ }
+ .header-content-right {
+    text-align: right;
+     position: absolute;
+     height: 260px;
+     right: 0;
+     z-index: 2;
+ }
+
+ .main {
+     padding: 32px;
+     p {
+         color: #ddd;
+     }
+ }
+
+ footer {
+  padding-top: 2rem;
+  height: 2rem;
+}
+
+footer a, span {
+  color: rgba(255, 255, 255, 0.2);
+}
+
+
+@media only screen and (max-width: 700px) {
+  .header {
+    flex-direction: column;
+  }
+  .header-left {
+    transform: skew(0);
+  }
+  .header-left, .header-right {
+    width: 100%;
+    margin-left: 0;
+    border: none;
+  }
+  .header-content-right {
+    top: 260px;
+  }
+  .noise {
+    height: 520px;
+  }
+}
+`;
+
+if (!state.theme) {
+  State.update({
+    theme: styled.div`
+
+    background: black;
+    color: white;
+    ${css}
+`,
   });
-};
+}
+const Theme = state.theme;
 
 return (
-  <>
-    <div class="container border border-info p-3 text-center min-vw-100">
-      <h1>Welcome to Near Türkiye</h1>
-      <p>{user_account}</p>
+  <Theme>
+    <div class="header">
+      <div class="header-left"></div>
+      <div class="header-content-left">
+        <div>
+          <div>
+            <h1>Near Türkiye</h1>
+          </div>
+          <div>
+            <h2>Social Media</h2>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2> Landing Page</h2>
+      </div>
     </div>
-    <br></br>
-    <br></br>
-    <br></br>
-    <h2>Near Türkiye official social media channels: </h2>
-    <button OnClick="https://www.twitter.com/near_turkey">
-      NEAR Türkiye Twitter
-    </button>
-    <br></br>
-    <br></br>
-    <button href="https://www.google.com">NEAR Türkiye Telegram</button>
-    <br></br>
-    <br></br>
-    <button OnClick=" location.href='https://www.google.com' ">
-      NEAR Türkiye Near Social
-    </button>
-    <br></br>
-    <br></br>
-    <button OnClick="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">
-      NEAR Türkiye Youtube
-    </button>
-    <br></br>
-    <input
-      type="button"
-      onclick="location.href='https://google.com';"
-      value="Go to Google"
-    />
-    <div>
-      <iframe
-        iframeResizer={{
-          onResized: ({ width, height }) => {
-            console.log("iframe resized", width, height);
-          },
-        }}
-        onLoad={() => console.log("iframe loaded")}
-        src="http://twitter.com"
-      />
+
+    <div class="main">
+      <a href="https://twitter.com/near_turkey" target="_blank">
+        <h2>Twitter</h2>
+      </a>
     </div>
-  </>
+
+    <div class="main">
+      <a href="https://twitter.com/near_tr" target="_blank">
+        <h2>Telegram</h2>
+      </a>
+    </div>
+
+    <div class="main">
+      <a href="https://youtube.com/near_turkey" target="_blank">
+        <h2>Youtube</h2>
+      </a>
+    </div>
+
+    <div class="main">
+      <a
+        href="https://near.social/#/mob.near/widget/ProfilePage?accountId=nearinturkiye.near"
+        target="_blank"
+      >
+        <h2>Near Social</h2>
+      </a>
+    </div>
+
+    <div class="main">
+      <a href="https://nearzombie.io" target="_blank">
+        <h2>Nearzombies</h2>
+      </a>
+    </div>
+
+    <div class="footer">
+      <div className="text-center">
+        <a href="/#/thebos.near/widget/Terms">Terms of Use</a> <span>|</span>{" "}
+        <a href="/#/thebos.near/widget/Privacy">Privacy Policy</a>
+      </div>
+    </div>
+  </Theme>
 );
