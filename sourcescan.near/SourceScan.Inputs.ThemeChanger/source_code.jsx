002e66707530ad2765ab9eb110e8c7405defd909
sourcescan.near <sourcescan.near>
2023-07-22 09:22:40 +0000

    Update SourceScan.Inputs.ThemeChanger by sourcescan.near at 2023-07-22T09:22:40.642Z

---
+++ sourcescan.near/SourceScan.Inputs.ThemeChanger/source_code.jsx
@@ -0,0 +1,80 @@
+State.init({
+  theme: props.theme || "light",
+});
+
+const useTheme = (light, dark) => {
+  return state.theme === "light" ? light : dark;
+};
+
+const Button = styled.button`
+  border-radius: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  border: 1px solid transparent;
+  color: ${useTheme(light.color, dark.color)};
+  background-color: ${useTheme(light.button.bg, dark.button.bg)}; 
+  transition: background-color 0.1s ease-in-out;
+
+  :hover {
+    background-color: ${useTheme(light.bg, dark.bg)};
+  }
+`;
+
+const Moon = (width, height) => {
+  const SVG = styled.svg`
+    width: ${width}
+    height: ${height}
+  `;
+
+  return (
+    <SVG
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+      />
+    </SVG>
+  );
+};
+
+const Sun = (width, height) => {
+  const SVG = styled.svg`
+    width: ${width}
+    height: ${height}
+  `;
+
+  return (
+    <SVG
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+      />
+    </SVG>
+  );
+};
+
+return (
+  <>
+    <Button onClick={props.switchTheme}>
+      {useTheme(
+        <Sun width={"25px"} height={"25px"} />,
+        <Moon width={"25px"} height={"25px"} />
+      )}
+    </Button>
+  </>
+);
