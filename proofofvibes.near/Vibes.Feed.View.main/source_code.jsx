83f37707085d66c437855138875686588f2ccd2b
proofofvibes.near <proofofvibes.near>
2023-07-22 13:05:00 +0000

    Update Vibes.Feed.View.main by proofofvibes.near at 2023-07-22T13:05:00.619Z

--- proofofvibes.near/Vibes.Feed.View.main/source_code.jsx
+++ proofofvibes.near/Vibes.Feed.View.main/source_code.jsx
@@ -333,7 +333,9 @@ const nftMint = () => {
       name: state.title,
       description: state.description,
       properties: [],
-      image: `ipfs://${state.image.ipfs_cid}`,
+      image: `${state.image.url}`,
+
+      //   image: `ipfs://${state.image.ipfs_cid}`,
       //   image: `ipfs://${state.image.cid}`,
       //   image: `ipfs://${state.imageCid}`,
     };
@@ -360,8 +362,9 @@ const nftMint = () => {
               description: state.description,
               //   media: `https://ipfs.io/ipfs/${state.imageCid}`,
               // media: `https://ipfs.io/ipfs/${state.image.cid}`,
+              media: `${state.image.url}`,
 
-              media: `https://ipfs.io/ipfs/${state.image.ipfs_cid}`,
+              //   media: `https://ipfs.io/ipfs/${state.image.ipfs_cid}`,
               reference: `ipfs://${cid}`,
               //   reference: `ipfs://${state.cid}`,
             },
