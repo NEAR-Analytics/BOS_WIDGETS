3e8c0e735566951a0b9226de52c3fc170eb96173
mattb.near <mattb.near>
2023-07-22 14:33:42 +0000

    Update Geoquete.Components.Header by mattb.near at 2023-07-22T14:33:42.262Z

--- mattb.near/Geoquete.Components.Header/source_code.jsx
+++ mattb.near/Geoquete.Components.Header/source_code.jsx
@@ -1,3 +1,5 @@
+let { onRefresh } = props;
+
 const HeaderBox = styled.div`
     display:flex;
     justify-content:space-between;
@@ -24,20 +26,64 @@ const Heading = styled.p`
   font-family: "SF Pro Display",sans-serif;
 `;
 
+const Links = styled.ul`
+  display:flex;
+  padding:0;
+  margin:0;
+  list-style:none;
+  justify-content:center;
+
+  li {
+    display:block;
+    background-color:rgba(0,0,0,.02);
+    border:1px solid rgba(0,0,0,.05);
+    border-radius:10px;
+    padding: .2rem 1rem;
+    margin-top: 20px;
+    transition:all .2s;
+    cursor:pointer;
+    font-size:.8rem;
+
+    &:hover {
+      transition:all .2s;
+      background-color:rgba(0,0,0,.07);
+    }
+
+    &:not(:last-of-type) {
+      margin-right:10px;
+    }
+  }
+
+`;
+
+let tabs = {
+  home: "Home",
+  create: "Create quest",
+  join: "Join quest",
+  review: "Review quests",
+};
+
 return (
-  <HeaderBox>
-    <Heading
-      style={{
-        "text-align": "left",
-      }}
-      className="fw-bold"
-    >
-      GeoQuête
-    </Heading>
-    <Web3Connect
-      className="connect-wallet"
-      connectLabel="Connect wallet"
-      disconnectLabel="Disconnect"
-    />
-  </HeaderBox>
+  <>
+    <HeaderBox>
+      <Heading
+        style={{
+          "text-align": "left",
+        }}
+        className="fw-bold"
+      >
+        GeoQuête
+      </Heading>
+      <Web3Connect
+        className="connect-wallet"
+        connectLabel="Connect wallet"
+        disconnectLabel="Disconnect"
+      />
+    </HeaderBox>
+    <Links>
+      {Object.keys(tabs).map((tab) => (
+        <li onClick={() => onRefresh(tab)}>{tabs[tab]}</li>
+      ))}
+    </Links>
+  </>
 );
