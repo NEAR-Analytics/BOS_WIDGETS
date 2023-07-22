53d0233eae2a102b4eb9a94b286f9471b4fb12d4
mattb.near <mattb.near>
2023-07-22 15:32:04 +0000

    Update Geoquete.Views.Home by mattb.near at 2023-07-22T15:32:04.587Z

--- mattb.near/Geoquete.Views.Home/source_code.jsx
+++ mattb.near/Geoquete.Views.Home/source_code.jsx
@@ -1,5 +1,14 @@
+State.init({
+    currentView: "home"
+});
+
 let views = {
   home: (
+    <>
+      <Widget src="mattb.near/widget/Geoquete.Components.CreateQuest" />
+    </>
+  ),
+  create: (
     <>
       <Widget src="mattb.near/widget/Geoquete.Components.CreateQuest" />
     </>
@@ -13,7 +22,11 @@ let views = {
 
 return (
   <>
-    <Widget src="mattb.near/widget/Geoquete.Components.Header" />
-    {views["join"]}
+    <Widget src="mattb.near/widget/Geoquete.Components.Header" 
+        props={{
+            onRefresh: (tab) => State.update({ currentView: tab })
+        }}
+    />
+    {state.currentView in views ? views[state.currentView] : "404"}
   </>
 );
