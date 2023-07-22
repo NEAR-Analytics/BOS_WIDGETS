2b8e52299feda8b596af00afc9bf032c15f11743
proofofvibes.near <proofofvibes.near>
2023-07-22 22:35:58 +0000

    Update Vibes.Feed.View.main by proofofvibes.near at 2023-07-22T22:35:58.803Z

--- proofofvibes.near/Vibes.Feed.View.main/source_code.jsx
+++ proofofvibes.near/Vibes.Feed.View.main/source_code.jsx
@@ -27,7 +27,7 @@ const accountLoggedIn = context.accountId; // use this just in case
 const postUrl = `https://near.org#/near/widget/PostPage?accountId=${accountId}&blockHeight=${blockHeight}`;
 const profile = Social.getr(`${accountId}/profile`);
 const profileName = accountId;
-if (profile) {
+if (!!profile) {
   profileName = profile.name; // profile name
   console.log("Profile Name: " + profileName);
 }
