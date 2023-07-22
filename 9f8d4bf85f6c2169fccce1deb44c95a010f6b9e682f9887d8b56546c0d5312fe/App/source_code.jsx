466697a18e531bd2f627f6b343e96caf9cb9ca14
9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe <9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe>
2023-07-22 22:55:49 +0000

    Update App by 9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe at 2023-07-22T22:55:49.743Z

--- 9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/App/source_code.jsx
+++ 9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/App/source_code.jsx
@@ -25,10 +25,21 @@ if (state.page === "default") {
 
 if (state.page === "score") {
   return (
-    <Widget
-      src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/ScorerPage"
-      props={props}
-    />
+    <div
+      onClick={() => {
+        State.update({
+          page: "mint",
+        });
+      }}
+    >
+      <Widget src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/ScorerPage" />
+    </div>
+  );
+}
+
+if (state.page === "mint") {
+  return (
+    <Widget src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/MintScore" />
   );
 }
 
