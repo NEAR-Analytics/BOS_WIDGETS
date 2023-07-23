851d23822c3daddb8615702b5623d7221711ab53
jay100.near <jay100.near>
2023-07-23 17:04:05 +0000

    Update Carousel by jay100.near at 2023-07-23T17:04:05.548Z

--- jay100.near/Carousel/source_code.jsx
+++ jay100.near/Carousel/source_code.jsx
@@ -1,37 +1,5 @@
 const Carousel = () => {
-  const slide_data = [
-    {
-      img_url:
-        "https://user-images.githubusercontent.com/100770363/241330819-010617e4-b2b3-4d34-a59c-c1b58bf92d8d.png",
-      title: "The Basics",
-    },
-    {
-      img_url:
-        "https://user-images.githubusercontent.com/100770363/241331374-56bd3f0c-7ed3-4923-bd48-4aac7367b280.png",
-      title: "Main Menu",
-      description:
-        "From the main menu, you can see all of the main things you can do in the game. Battle, Mint Unit, Check the Leaderboard, Check Active Rooms, and the market. On the right hand side of your image, this is where the troops that you own will be displayed.",
-    },
-    {
-      img_url:
-        "https://user-images.githubusercontent.com/100770363/241335354-eaf25884-de02-4d7a-a8f2-3c4e59214d5f.png",
-      title: "Battles",
-      description:
-        "This is what you'll see when you select battle. You will have the option to create a new challenge, or fight against someone's challenge. These matches are best 2 out of 3",
-    },
-    {
-      img_url:
-        "https://user-images.githubusercontent.com/100770363/241335498-727bf24d-a65c-4bff-b404-8bcadc558512.png",
-      description:
-        "This is the challenge you chose to accept. As you can see the opponent already set the field for the first round, and it is your job to assemble your own side so that you can attempt to take the first round.",
-    },
-    {
-      img_url:
-        "https://user-images.githubusercontent.com/100770363/241335871-e9c8e5fe-db38-4ada-b1ff-7d895e61f44e.png",
-      description:
-        "Thanks to the cheeky placement of the warlock, You were able to take the first round. From here, you will have to wait for the opponent to respond before you can attack again.",
-    },
-  ];
+  const slide_data = props;
   const CarouselSrc = `
   <head>
     <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>
