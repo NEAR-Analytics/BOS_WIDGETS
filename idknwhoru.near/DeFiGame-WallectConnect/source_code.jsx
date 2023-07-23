0125203f7f8720e1a39def688a3671d1bf075e7c
idknwhoru.near <idknwhoru.near>
2023-07-23 09:55:52 +0000

    Update DeFiGame-WallectConnect by idknwhoru.near at 2023-07-23T09:55:52.914Z

---
+++ idknwhoru.near/DeFiGame-WallectConnect/source_code.jsx
@@ -0,0 +1,12 @@
+return (
+  <>
+    <h1>지갑을 선택해주세요</h1>
+    <ul>
+      <li onClick={(_) => console.log("a")}>A 지갑</li>
+      <li>B 지갑</li>
+      <li>C 지갑</li>
+      <li>D 지갑</li>
+      <li>E 지갑</li>
+    </ul>
+  </>
+);
