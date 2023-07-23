a6b477a2dd056a63c64355b69c13bd4851c7c88d
knwtechs.near <knwtechs.near>
2023-07-23 03:31:07 +0000

    Update Customer.view by knwtechs.near at 2023-07-23T03:31:07.267Z

--- knwtechs.near/Customer.view/source_code.jsx
+++ knwtechs.near/Customer.view/source_code.jsx
@@ -95,7 +95,7 @@ const getSubscriptionEvents = (user) => {
 getSubscriptionEvents(props.sender);
 
 return (
-  <div class="container-fluid bg-dark h-100">
+  <div class="container-fluid h-100">
     <div class="row d-flex justify-content-center mt-2">
       {state.widgets.map((w) => (
         <div class="col-10 col-md-4 mt-2">{w}</div>
