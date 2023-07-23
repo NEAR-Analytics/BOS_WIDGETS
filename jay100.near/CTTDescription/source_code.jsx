a0c1fa52d0a864cd98c257ba46c6d8774b4ff87a
jay100.near <jay100.near>
2023-07-23 16:07:27 +0000

    Update CTTDescription by jay100.near at 2023-07-23T16:07:27.652Z

--- jay100.near/CTTDescription/source_code.jsx
+++ jay100.near/CTTDescription/source_code.jsx
@@ -1,63 +1,30 @@
+const pixelFont = fetch(
+  "https://fonts.googleapis.com/css?family=Press+Start+2P"
+).body;
+
 const GameDescription = () => {
-  const code = `
-    <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-        <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>
-    </head>
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Press+Start+2P');
+  const DescriptionContainer = styled.div`
+  font-family: "Pixel Emulator", "Press Start 2P", "Courier new", "monospace";
+  ${pixelFont}
+  background: rgb(14, 14, 30);
+  color: white;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  `;
 
-        body {
-            padding: 5px;
-            background: rgb(14, 14, 30);
-            font-family: "Pixel Emulator", "Press Start 2P", Courier new, monospace;
-            color: #f8f8f8;
-            text-shadow: 2px 0 0 #000, 0 2px 0 #000;
-        }
+  const Title = styled.h3`
+    text-align: center;
+  `;
 
-        .gameDescription{
-            display: flex;
-            flex-direction: column;
-        }
-
-        .title{
-            text-align: center;
-        }
-
-        .description{
-            font-size: 0.75rem;
-            text-align: justify;
-        }
-    </style>
-
-    <body>
-        <div class="gameDescription">
-        </div>
-    </body>
-    <script>
-        window.addEventListener("message", (event) => {
-            const title = event.data.data.title;
-            const description = event.data.data.description;
-
-            const descriptionHeader = $("<h4></h4>").text(title).addClass("title");
-            const gameDescription = $("<p></p>").text(description).addClass("description");
-
-            $(".gameDescription").append(descriptionHeader, gameDescription);
-        });
-    </script> 
-`;
-  const data = props;
+  const Description = styled.p`
+    padding: 1rem;
+  `;
   return (
-    <div>
-      <div style={{ backgroundColor: "white" }} className="d-flex">
-        <iframe
-          style={{ height: "30vh", width: "100%" }}
-          srcDoc={code}
-          message={{ data: data || "No Data" }}
-        />
-      </div>
-    </div>
+    <DescriptionContainer className="col">
+      <Title>{props.title}</Title>
+      <Description>{props.description}</Description>
+    </DescriptionContainer>
   );
 };
 
