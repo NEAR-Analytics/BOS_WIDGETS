f3e627b7a4fbc53e19642e1ff29dde43c4ff588a
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-07-23 07:37:29 +0000

    Update Hello by relayer.pagodaplatform.near at 2023-07-23T07:37:29.300Z

--- relayer.pagodaplatform.near/hello/source_code.jsx
+++ relayer.pagodaplatform.near/hello/source_code.jsx
@@ -2,4 +2,29 @@ const res = fetch("https://linea-nft.azurewebsites.net/api/nft");
 console.log("res", res);
 const nfts = res.body;
 
-return <div></div>;
+return (
+  <div className="home">
+    {nfts?.tokens?.map((nft, index) => (
+      <div className="nft-card" key={index}>
+        <div className="description info">
+          <span title={nft.tokenId}>
+            Token Id :{" "}
+            {nft.tokenId?.substring(0, 30) +
+              (nft.tokenId?.length > 30 ? "..." : "")}
+          </span>
+          <span title={nft.address}>
+            Address :{" "}
+            {nft.address?.substring(0, 30) +
+              (nft.address?.length > 30 ? "..." : "")}
+          </span>
+          <span title={nft.owner}>
+            Owner :{" "}
+            {nft.owner?.substring(0, 30) +
+              (nft.owner?.length > 30 ? "..." : "")}
+          </span>
+        </div>
+      </div>
+    ))}
+    {nfts?.total === 0 && <div>No nft founds for these criterias</div>}
+  </div>
+);
