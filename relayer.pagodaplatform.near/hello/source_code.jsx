6061097dfdfa55c66d37d372cb20542fc8b7fbf3
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-07-23 07:42:42 +0000

    Update Hello by relayer.pagodaplatform.near at 2023-07-23T07:42:42.601Z

--- relayer.pagodaplatform.near/hello/source_code.jsx
+++ relayer.pagodaplatform.near/hello/source_code.jsx
@@ -2,10 +2,20 @@ const res = fetch("https://linea-nft.azurewebsites.net/api/nft");
 console.log("res", res);
 const nfts = res.body;
 
+const AsyncImage = (props) => {
+  const datas = JSON.parse(props.metadatas);
+  if (datas.image) {
+    const show = { src: datas.image };
+    return <img height="200" {...show} />;
+  }
+  return <img height="200" />;
+};
+
 return (
   <div className="home">
     {nfts?.tokens?.map((nft, index) => (
       <div className="nft-card" key={index}>
+        <AsyncImage {...nft} />
         <div className="description info">
           <span title={nft.tokenId}>
             Token Id :{" "}
