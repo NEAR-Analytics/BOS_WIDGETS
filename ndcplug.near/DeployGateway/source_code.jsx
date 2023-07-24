816271ee1457664fee6deacce430dd0079d916f6
ndcplug.near <ndcplug.near>
2023-07-24 22:15:08 +0000

    Update DeployGateway by ndcplug.near at 2023-07-24T22:15:08.047Z

--- ndcplug.near/DeployGateway/source_code.jsx
+++ ndcplug.near/DeployGateway/source_code.jsx
@@ -17,44 +17,5 @@ return (
     >
       <img src="https://vercel.com/button" alt="Deploy with Vercel" />
     </a>
-    {showHeader && (
-      <div>
-        <h1>
-          Deploy{" "}
-          <a
-            href="https://github.com/NEARDeFi/bos-gateway-template"
-            target="_blank"
-          >
-            BOS Gateway Template
-          </a>{" "}
-          (EVM Landing Page)
-        </h1>
-      </div>
-    )}
-    <a
-      href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fneardefi%2Fbos-gateway-template&build-command=pnpm%20run%20build"
-      target="_blank"
-    >
-      <img src="https://vercel.com/button" alt="Deploy with Vercel" />
-    </a>
-    {showHeader && (
-      <div>
-        <h1>
-          deploy{" "}
-          <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fnear-everything%2Fviewer%2Ftree%2Fmaster%2Fsrc"
-            target="_blank"
-          >
-            something
-          </a>
-        </h1>
-      </div>
-    )}
-    <a
-      href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fnear-everything%2Fviewer%2Ftree%2Fmaster%2Fsrc"
-      target="_blank"
-    >
-      <img src="https://vercel.com/button" alt="Deploy with Vercel" />
-    </a>
   </div>
 );
