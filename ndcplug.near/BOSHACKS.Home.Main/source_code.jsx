e8edae4b54bc586061729015825e4e5e0bc6f01b
ndcplug.near <ndcplug.near>
2023-07-24 17:58:44 +0000

    Update BOSHACKS.Home.Main by ndcplug.near at 2023-07-24T17:58:44.976Z

--- ndcplug.near/BOSHACKS.Home.Main/source_code.jsx
+++ ndcplug.near/BOSHACKS.Home.Main/source_code.jsx
@@ -2,6 +2,8 @@ return (
   <>
     <Widget src={`ndcplug.near/widget/BOSHACKS.Home.Header`} />
     <Widget src={`ndcplug.near/widget/BOSHACKS.Home.SubHeader`} />
+    <Widget src={`ndcplug.near/widget/BOSHACKS.Home.Schedule`} />
+
     <Widget src={`ndcplug.near/widget/BOSHACKS.Home.Sponsors`} />
   </>
 );
