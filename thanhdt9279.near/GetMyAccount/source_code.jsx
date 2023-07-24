c292ea51a8b9e665fa404fba4a3ace49bd453f53
thanhdt9279.near <thanhdt9279.near>
2023-07-24 18:16:08 +0000

    Update GetMyAccount by thanhdt9279.near at 2023-07-24T18:16:08.578Z

---
+++ thanhdt9279.near/GetMyAccount/source_code.jsx
@@ -0,0 +1,10 @@
+let user_account = context.accountId;
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center min-vw-100">
+      <h1>Hello</h1>
+      <p> {user_account} </p>
+    </div>
+  </>
+);
