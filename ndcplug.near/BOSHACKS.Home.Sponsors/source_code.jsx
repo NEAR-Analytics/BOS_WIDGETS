504ec61e359481ff683779998ae0ff50bfbea142
ndcplug.near <ndcplug.near>
2023-07-24 17:20:53 +0000

    Update BOSHACKS.Home.Sponsors by ndcplug.near at 2023-07-24T17:20:53.740Z

---
+++ ndcplug.near/BOSHACKS.Home.Sponsors/source_code.jsx
@@ -0,0 +1,103 @@
+const ownerId = "nearcon23.near";
+const apiKey =
+  "patWQQ6FY8H5O8wTY.4b08b48ac31aa13eb9fea974cfa60e103ae7297c010d4fe752e1abb37bd24c9d";
+
+const Section = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 3.125rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 3rem;
+
+  & > div:first-child {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    align-self: stretch;
+
+    & > h2 {
+      display: flex;
+      flex-direction: column;
+      color: var(--black, #000);
+      font-size: 2rem;
+      font-family: FK Grotesk;
+      font-weight: 500;
+    }
+
+    @media screen and (max-width: 768px) {
+      flex-direction: column;
+      align-items: center;
+      gap: 1.5rem;
+    }
+  }
+`;
+
+const Logos = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  width: 100%;
+  gap: 3rem;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (max-width: 480px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  & > a > img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
+const sponsors = [
+  {
+    url: "https://genadrop.io",
+    name: "GenaDrop",
+    image:
+      "https://ipfs.near.social/ipfs/bafkreib2sswfwxwwsaaegzhuc454rgkdyycg6dx4gprxgwk5vy6ysrkfpm",
+  },
+  {
+    url: "https://proofofvibes.com",
+    name: "Proof Of Vibes",
+    image:
+      "https://ipfs.near.social/ipfs/bafkreibmc23xhip63mxv2mulb7xko5htpiqszrch4fo3optszuctjtlrau",
+  },
+  {
+    url: "https://nearefi.org",
+    name: "NEAR ReFi",
+    image:
+      "https://ipfs.io/ipfs/bafkreibg5iyag6xaqi7xrmizqqx4uuvro7fnaw3ulnqkhltwivmwqmqxhu",
+  },
+  {
+    url: "https://indexer.xyz",
+    name: "Indexer.xyz",
+    image:
+      "https://ipfs.near.social/ipfs/bafkreifkghgzfygpha2ylolki5mhsbfir4hrhnv27kjyw4tqa3lyshrqnm",
+  },
+];
+
+return (
+  <Section>
+    <div>
+      <h2>Bounty Sponsors</h2>
+      <Widget
+        src={`ndcplug.near/widget/BOSHACKS.Home.Button`}
+        props={{
+          text: "Become a Sponsor/Partner",
+          href: "http://nearbuilders.com/boshacks-sponsor",
+        }}
+      />
+    </div>
+    <Logos>
+      {sponsors.map((sponsor) => (
+        <a href={sponsor.url} target="blank">
+          <img src={sponsor.image} />
+        </a>
+      ))}
+    </Logos>
+  </Section>
+);
