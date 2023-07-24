8aa217a7abfc69301b60f29d222c0b2704a67c0f
ndcplug.near <ndcplug.near>
2023-07-24 16:56:57 +0000

    Update BOSHACKS.Home.Main by ndcplug.near at 2023-07-24T16:56:57.453Z

---
+++ ndcplug.near/BOSHACKS.Home.Main/source_code.jsx
@@ -0,0 +1,12 @@
+const ownerId = "nearcon23.near";
+
+return (
+  <>
+    <Widget src={`${ownerId}/widget/Home.Header`} />
+    <Widget src={`${ownerId}/widget/Home.SubHeader`} />
+    // <Widget src={`${ownerId}/widget/Home.Experience`} />
+    // <Widget src={`${ownerId}/widget/Home.Tracks`} />
+    <Widget src={`${ownerId}/widget/Home.Sponsors`} />
+    <Widget src={`${ownerId}/widget/Home.More`} />
+  </>
+);
