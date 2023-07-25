87a131c5b95e5a572fa70e3bab3f2844bb859326
eugenewolf507.near <eugenewolf507.near>
2023-07-25 14:12:57 +0000

    Update Gigs by eugenewolf507.near at 2023-07-25T14:12:57.550Z

---
+++ eugenewolf507.near/Gigs/source_code.jsx
@@ -0,0 +1,62 @@
+const addressForArticles = "ndcGigsArticle";
+// const authorsWhitelist = [
+//   "neardigitalcollective.near",
+//   "blaze.near",
+//   "jlw.near",
+//   "kazanderdad.near",
+//   "joep.near",
+//   "sarahkornfeld.near",
+//   "yuensid.near",
+//   "james.near",
+//   "ndcplug.near",
+// ];
+const writersWhiteList = [
+  "neardigitalcollective.near",
+  "blaze.near",
+  "jlw.near",
+  "kazanderdad.near",
+  "joep.near",
+  "sarahkornfeld.near",
+  "yuensid.near",
+  "james.near",
+  "ndcplug.near",
+  "eugenewolf507.near",
+];
+const authorForWidget1 = "neardigitalcollective.near";
+const authorForWidget2 = "eugenewolf507.near";
+const accountId = props.accountId ?? context.accountId;
+// if (!accountId) {
+//   return "No account ID";
+// }
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+if (profile === null) {
+  return "Loading";
+}
+
+const Wrapper = styled.div`
+  padding: 0;
+  border-radius: 20px;
+  background-color: #fff;
+  background-image: linear-gradient(180deg, #fafcfd 0%, #b6dbfc 100%);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -2;
+`;
+
+return (
+  <Wrapper className="container-fluid">
+    <Widget
+      src={`${authorForWidget1}/widget/Gigs_MainNavigation`}
+      props={{ currentNavPill: "articles", writersWhiteList }}
+    />
+    <div style={{ margin: "0 auto", width: "98%", minWidth: "360px" }}>
+      <Widget
+        src={`${authorForWidget2}/widget/Gigs_AllArticlesList`}
+        props={{ writersWhiteList }}
+      />
+    </div>
+  </Wrapper>
+);
