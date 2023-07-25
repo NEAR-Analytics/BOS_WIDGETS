a48e0c9007affea3a5f50f972dbdee9a45fb2131
ndcplug.near <ndcplug.near>
2023-07-25 13:28:46 +0000

    Update BOSHACKS.Members.Header by ndcplug.near at 2023-07-25T13:28:46.793Z

---
+++ ndcplug.near/BOSHACKS.Members.Header/source_code.jsx
@@ -0,0 +1,35 @@
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 4.5em;
+  gap: 0.625em;
+  background: #000;
+
+  h1 {
+    font-family: "FK Grotesk";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 2.5em;
+    line-height: 1.25em;
+    color: #fff;
+  }
+
+  p {
+    font-family: "Mona Sans";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.25em;
+    line-height: 140%;
+    color: #00ec97;
+    max-width: 13em;
+  }
+`;
+
+return (
+  <Container>
+    <h1>All the People at BOS Hacks</h1>
+    <p>Explore judges, mentors, and hackers</p>
+  </Container>
+);
