18aac6b2d3d6356b2ff8d9635e16985591c7a6ef
ndcplug.near <ndcplug.near>
2023-07-25 02:53:41 +0000

    Update BOSHACKS.Home.Sponsors by ndcplug.near at 2023-07-25T02:53:41.629Z

--- ndcplug.near/BOSHACKS.Home.Sponsors/source_code.jsx
+++ ndcplug.near/BOSHACKS.Home.Sponsors/source_code.jsx
@@ -52,11 +52,18 @@ const Logos = styled.div`
 
 const sponsors = [
   {
+    url: "https://osec.io",
+    name: "Ottersec",
+    image:
+      "https://pbs.twimg.com/profile_images/1628618156611870720/3dq8OUcH_400x400.jpg",
+  },
+  {
     url: "https://genadrop.io",
     name: "GenaDrop",
     image:
       "https://ipfs.near.social/ipfs/bafkreib2sswfwxwwsaaegzhuc454rgkdyycg6dx4gprxgwk5vy6ysrkfpm",
   },
+
   {
     url: "https://proofofvibes.com",
     name: "Proof Of Vibes",
@@ -77,24 +84,84 @@ const sponsors = [
   },
 ];
 
+const partners = [
+  {
+    url: "https://shard.dog/go",
+    name: "ShardDog",
+    image:
+      "https://ipfs.near.social/ipfs/bafkreibh4jzae37fo6cpvasg7e6qxfmxjuku5mi3lfow4tni2why632gei",
+  },
+  {
+    url: "https://near.foundation",
+    name: "NEAR Foundation",
+    image:
+      "https://pbs.twimg.com/profile_images/1509554611841114124/ma08E72p_400x400.jpg",
+  },
+  {
+    url: "https://minorityprogrammers.org",
+    name: "Minority Programmers",
+    image:
+      "https://ipfs.near.social/ipfs/bafkreibmrhijv6etdsbvs4na5cndpge5nbjm2fflcldtpvscesdk2e2pt4",
+  },
+  {
+    url: "https://nearnyc.org",
+    name: "NEAR NYC",
+    image:
+      "https://pbs.twimg.com/profile_images/1535356873846755329/8K3CE_86_400x400.jpg",
+  },
+  {
+    url: "https://nearsf.org",
+    name: "NEAR SF",
+    image:
+      "https://ipfs.near.social/ipfs/bafkreicxkl63msjcqn37ooqq6wxz5xrjwralwtraldcj2e3tmhgftsu4fm",
+  },
+  // {
+  //   url: "https://nearweek.com",
+  //   name: "NEARWEEK",
+  //   image:
+  //     "https://ipfs.near.social/ipfs/bafkreifkghgzfygpha2ylolki5mhsbfir4hrhnv27kjyw4tqa3lyshrqnm",
+  // },
+];
+
 return (
-  <Section>
-    <div>
-      <h2>Bounty Sponsors</h2>
-      <Widget
-        src={`ndcplug.near/widget/BOSHACKS.Home.Button`}
-        props={{
-          text: "Become a Sponsor/Partner",
-          href: "http://nearbuilders.com/sponsor",
-        }}
-      />
-    </div>
-    <Logos>
-      {sponsors.map((sponsor) => (
-        <a href={sponsor.url} target="blank">
-          <img src={sponsor.image} />
-        </a>
-      ))}
-    </Logos>
-  </Section>
+  <>
+    <Section>
+      <div>
+        <h2>Bounty Sponsors</h2>
+        <Widget
+          src={`ndcplug.near/widget/BOSHACKS.Home.Button`}
+          props={{
+            text: "💰 See Bounties",
+            href: "http://nearbuilders.com/bounty",
+          }}
+        />
+      </div>
+      <Logos>
+        {sponsors.map((sponsor) => (
+          <a href={sponsor.url} target="blank">
+            <img src={sponsor.image} />
+          </a>
+        ))}
+      </Logos>
+    </Section>
+    <Section>
+      <div>
+        <Widget
+          src={`ndcplug.near/widget/BOSHACKS.Home.Button`}
+          props={{
+            text: "Become a Sponsor/Partner",
+            href: "http://nearbuilders.com/sponsor",
+          }}
+        />
+        <h2>Community Partners</h2>
+      </div>
+      <Logos>
+        {partners.map((partner) => (
+          <a href={partner.url} target="blank">
+            <img src={partner.image} />
+          </a>
+        ))}
+      </Logos>
+    </Section>
+  </>
 );
