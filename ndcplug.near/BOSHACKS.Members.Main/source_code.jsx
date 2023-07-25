a7572920dce4840d96065d1bab41898922b7e56e
ndcplug.near <ndcplug.near>
2023-07-25 13:29:15 +0000

    Update BOSHACKS.Members.Main by ndcplug.near at 2023-07-25T13:29:15.717Z

---
+++ ndcplug.near/BOSHACKS.Members.Main/source_code.jsx
@@ -0,0 +1,8 @@
+return (
+  <>
+    <Widget src={`ndcplug.near/widget/BOSHACKS.Members.Header`} />
+    <Widget src={`ndcplug.near/widget/BOSHACKS.Members.Judges`} />
+
+    <Widget src={`ndcplug.near/widget/BOSHACKS.Members.Hackers`} />
+  </>
+);
