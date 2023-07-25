ec7eb56486be7ce5a91f08d3b7213ef29c8f9ff8
efiz.near <efiz.near>
2023-07-25 20:52:21 +0000

    Update every.thing by efiz.near at 2023-07-25T20:52:21.040Z

--- efiz.near/every.thing/source_code.jsx
+++ efiz.near/every.thing/source_code.jsx
@@ -8,6 +8,15 @@ State.init({
 
 let keys = `${state.accountId ?? "*"}/${typeName}/*`;
 
+function handleFilter({ accountId, tag }) {
+  if (accountId) {
+    State.update({ accountId });
+  }
+  if (tag) {
+    State.update({ tag });
+  }
+}
+
 if (state.tag) {
   const taggedWidgets = Social.keys(
     `${state.accountId ?? "*"}/${typeName}/*/metadata/tags/${state.tag}`,
@@ -82,61 +91,10 @@ if (JSON.stringify(data) !== JSON.stringify(state.data || {})) {
 }
 return (
   <div className="px-2 mx-auto">
-    <div className="mb-2 d-flex align-items-baseline gap-2">
-      Filter:
-      {state.accountId ? (
-        <a className="btn btn-outline-primary">
-          <Widget
-            src="mob.near/widget/ProfileLine"
-            props={{ accountId: state.accountId, link: false }}
-          />
-          <i
-            className="bi bi-x-square"
-            onClick={() => State.update({ accountId: null })}
-          ></i>
-        </a>
-      ) : (
-        <>
-          <input
-            className="form-control d-inline-block w-auto"
-            placeholder={"accountId"}
-            value={state.accountIdVal}
-            onChange={(e) =>
-              State.update({
-                accountIdVal: e.target.value,
-              })
-            }
-          />
-          <button
-            onClick={() => State.update({ accountId: state.accountIdVal })}
-          >
-            apply
-          </button>
-        </>
-      )}
-      {state.tag ? (
-        <a className="btn btn-outline-primary">
-          <span className="badge text-bg-secondary">#{state.tag}</span>
-          <i className="bi bi-x-square"></i>
-        </a>
-      ) : (
-        <>
-          <input
-            className="form-control d-inline-block w-auto"
-            placeholder={"tag"}
-            value={state.tag}
-            onChange={(e) =>
-              State.update({
-                tagVal: e.target.value,
-              })
-            }
-          />
-          <button onClick={() => State.update({ tag: state.tagVal })}>
-            apply
-          </button>
-        </>
-      )}
-    </div>
+    <Widget
+      src="efiz.near/widget/every.thing.filters"
+      props={{ handleFilter: handleFilter }}
+    />
     <Widget
       src="efiz.near/widget/ItemFeed"
       props={{ items: state.allItems || [], renderItem }}
