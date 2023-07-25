10df13baa9d060a8b850ddb91e09a347d1372c87
ref-bigboss.near <ref-bigboss.near>
2023-07-25 12:47:59 +0000

    Update ZKEVM.AAVE.Modal.RepayModal by ref-bigboss.near at 2023-07-25T12:47:59.672Z

--- ref-bigboss.near/ZKEVM.AAVE.Modal.RepayModal/source_code.jsx
+++ ref-bigboss.near/ZKEVM.AAVE.Modal.RepayModal/source_code.jsx
@@ -33,7 +33,6 @@ const {
   variableBorrows,
   name: tokenName,
   balance,
-  supportPermit,
 } = data;
 
 const RepayContainer = styled.div`
@@ -68,9 +67,9 @@ const GrayTexture = styled.div`
   font-size: 12px;
   font-weight: 500;
   color: #7c7c86;
-  .balanceValue {
-    text-decoration: underline;
-    cursor: pointer;
+  .balanceValue{
+    text-decoration:underline;
+    cursor:pointer;
   }
 `;
 
@@ -98,8 +97,8 @@ const WhiteTexture = styled.div`
 `;
 const DeepPurpleTexture = styled.div`
   font-size: 14px;
-  color: #7c7f96;
-`;
+  color: #7C7F96;
+`
 const TransactionOverviewContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -133,20 +132,20 @@ const Max = styled.span`
 `;
 
 const InputContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: rgba(53, 55, 73, 0.5);
-  height: 55px;
-  border-radius: 10px;
-  padding: 0 12px;
-`;
+   display: flex;
+   justify-content: space-between;
+   align-items:center;
+   background-color:rgba(53, 55, 73, 0.5);
+   height:55px;
+   border-radius:10px;
+   padding:0 12px;
+`
 const BalanceContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 10px;
-`;
+   display: flex;
+   justify-content: space-between;
+   align-items:center;  
+   margin-top:10px;
+`
 
 State.init({
   amount: "",
@@ -154,12 +153,10 @@ State.init({
   loading: false,
   newHealthFactor: "-",
   gas: "-",
-  allowanceAmount: "0",
-  needApprove: false,
 });
 
 function updateGas() {
-  if (symbol === config.nativeCurrency.symbol) {
+  if (["ETH", "WETH"].includes(symbol)) {
     repayETHGas().then((value) => {
       State.update({ gas: value });
     });
@@ -171,9 +168,7 @@ function updateGas() {
 }
 
 updateGas();
-const questionSwitch = Storage.get("zkevm-aave-question-switch", "ref-bigboss.near/widget/ZKEVM.switch_quest_card");
 const eth_account_id = Ethers.send("eth_requestAccounts", [])[0];
-
 function bigMin(_a, _b) {
   const a = Big(_a);
   const b = Big(_b);
@@ -181,7 +176,7 @@ function bigMin(_a, _b) {
 }
 
 function getAvailableBalance() {
-  if (symbol === config.nativeCurrency.symbol) {
+  if (["ETH", "WETH"].includes(symbol)) {
     const newBalance = Number(balance) - 0.01;
     if (newBalance <= 0) {
       return 0;
@@ -215,7 +210,7 @@ const shownMaxValue =
  * @returns
  */
 function getNewHealthFactor(chainId, address, asset, action, amount) {
-  const url = `${config.AAVE_API_BASE_URL}/${chainId}/health/${address}`;
+  const url = `https://aave-api.pages.dev/${chainId}/health/${address}`;
   return asyncFetch(`${url}?asset=${asset}&action=${action}&amount=${amount}`);
 }
 
@@ -244,7 +239,7 @@ const updateNewHealthFactor = debounce(() => {
         "repay",
         state.amountInUSD
       ).then((response) => {
-        const newHealthFactor = formatHealthFactor(response.body);
+        const newHealthFactor = formatHealthFactor(JSON.parse(response.body));
         State.update({ newHealthFactor });
       });
     });
@@ -276,79 +271,6 @@ function getNonce(tokenAddress, userAddress) {
 
   return token.nonces(userAddress).then((nonce) => nonce.toNumber());
 }
-
-function getAllowance() {
-  const tokenAddress = underlyingAsset;
-  Ethers.provider()
-    .getSigner()
-    .getAddress()
-    .then((userAddress) => {
-      const token = new ethers.Contract(
-        tokenAddress,
-        config.erc20Abi.body,
-        Ethers.provider().getSigner()
-      );
-      token
-        .allowance(userAddress, config.aavePoolV3Address)
-        .then((allowanceAmount) => allowanceAmount.toString())
-        .then((allowanceAmount) => {
-          State.update({
-            allowanceAmount: Big(allowanceAmount)
-              .div(Big(10).pow(decimals))
-              .toFixed(),
-          });
-        });
-    });
-}
-getAllowance();
-
-function repayFromApproval(amount) {
-  const tokenAddress = underlyingAsset;
-  const pool = new ethers.Contract(
-    config.aavePoolV3Address,
-    config.aavePoolV3ABI.body,
-    Ethers.provider().getSigner()
-  );
-
-  return Ethers.provider()
-    .getSigner()
-    .getAddress()
-    .then((userAddress) => {
-      return pool["repay(address,uint256,uint256,address)"](
-        tokenAddress,
-        amount,
-        2, // variable interest rate
-        userAddress
-      );
-    });
-}
-
-function approve(amount) {
-  const tokenAddress = underlyingAsset;
-  const token = new ethers.Contract(
-    tokenAddress,
-    config.erc20Abi.body,
-    Ethers.provider().getSigner()
-  );
-  return token["approve(address,uint256)"](config.aavePoolV3Address, amount);
-}
-
-function update() {
-  if (supportPermit) {
-    return;
-  }
-  if (
-    !isValid(state.amount) ||
-    !isValid(state.allowanceAmount) ||
-    Number(state.allowanceAmount) < Number(state.amount) ||
-    Number(state.amount) === 0
-  ) {
-    State.update({ needApprove: true });
-  } else {
-    State.update({ needApprove: false });
-  }
-}
-update();
 
 /**
  *
@@ -418,11 +340,35 @@ function repayERC20(shownAmount, actualAmount) {
     .getSigner()
     .getAddress()
     .then((address) => {
-      if (!supportPermit) {
-        repayFromApproval(actualAmount)
-          .then((tx) => {
+      return signERC20Approval(
+        address,
+        asset,
+        tokenName,
+        actualAmount,
+        deadline
+      )
+        .then((rawSig) => {
+          const sig = ethers.utils.splitSignature(rawSig);
+          const pool = new ethers.Contract(
+            config.aavePoolV3Address,
+            config.aavePoolV3ABI.body,
+            Ethers.provider().getSigner()
+          );
+
+          return pool[
+            "repayWithPermit(address,uint256,uint256,address,uint256,uint8,bytes32,bytes32)"
+          ](
+            asset,
+            actualAmount,
+            2, // variable interest rate
+            address,
+            deadline,
+            sig.v,
+            sig.r,
+            sig.s
+          ).then((tx) => {
             tx.wait().then((res) => {
-              const { status, transactionHash } = res;
+              const { status, blockHash } = res;
               if (status === 1) {
                 onActionSuccess({
                   msg: `You repaid ${Big(shownAmount).toFixed(8)} ${symbol}`,
@@ -441,84 +387,20 @@ function repayERC20(shownAmount, actualAmount) {
                 console.log("tx failed", res);
               }
               add_action({
-                action_title: `Repay ${symbol} on AAVE`,
-                action_type: "Repay",
-                action_tokens: JSON.stringify([`${symbol}`]),
-                action_amount: null,
-                account_id: eth_account_id,
-                account_info: "",
-                template: "AAVE",
-                action_switch: questionSwitch == "on" ? '1': '0',
-                action_status: status === 1 ? "Success" : "Failed",
-                tx_id: transactionHash,
-              });
+                "action_title": `Repay ${symbol} on AAVE`,
+                "action_type": "Repay",
+                "action_tokens": JSON.stringify([`${symbol}`]),
+                "action_amount": null,
+                "account_id": eth_account_id,
+                "account_info": "",
+                "template": "AAVE",
+                "action_status": status === 1 ? "Success": "Failed",
+                "tx_id": blockHash,
+              })
             });
-          })
-          .catch(() => State.update({ loading: false }));
-      } else {
-        return signERC20Approval(
-          address,
-          asset,
-          tokenName,
-          actualAmount,
-          deadline
-        )
-          .then((rawSig) => {
-            const sig = ethers.utils.splitSignature(rawSig);
-            const pool = new ethers.Contract(
-              config.aavePoolV3Address,
-              config.aavePoolV3ABI.body,
-              Ethers.provider().getSigner()
-            );
-
-            return pool[
-              "repayWithPermit(address,uint256,uint256,address,uint256,uint8,bytes32,bytes32)"
-            ](
-              asset,
-              actualAmount,
-              2, // variable interest rate
-              address,
-              deadline,
-              sig.v,
-              sig.r,
-              sig.s
-            ).then((tx) => {
-              tx.wait().then((res) => {
-                const { status, transactionHash } = res;
-                if (status === 1) {
-                  onActionSuccess({
-                    msg: `You repaid ${Big(shownAmount).toFixed(8)} ${symbol}`,
-                    callback: () => {
-                      onRequestClose();
-                      State.update({
-                        loading: false,
-                      });
-                    },
-                  });
-                  console.log("tx succeeded", res);
-                } else {
-                  State.update({
-                    loading: false,
-                  });
-                  console.log("tx failed", res);
-                }
-                add_action({
-                  action_title: `Repay ${symbol} on AAVE`,
-                  action_type: "Repay",
-                  action_tokens: JSON.stringify([`${symbol}`]),
-                  action_amount: null,
-                  account_id: eth_account_id,
-                  account_info: "",
-                  template: "AAVE",
-                  action_switch: questionSwitch == "on" ? '1': '0',
-                  action_status: status === 1 ? "Success" : "Failed",
-                  tx_id: transactionHash,
-                });
-              });
-            });
-          })
-          .catch(() => State.update({ loading: false }));
-      }
+          });
+        })
+        .catch(() => State.update({ loading: false }));
     })
     .catch(() => State.update({ loading: false }));
 }
@@ -547,7 +429,7 @@ function repayETH(shownAmount, actualAmount) {
         )
         .then((tx) => {
           tx.wait().then((res) => {
-            const { status, transactionHash } = res;
+            const { status, blockHash } = res;
             if (status === 1) {
               onActionSuccess({
                 msg: `You repaid ${Big(shownAmount).toFixed(8)} ${symbol}`,
@@ -566,61 +448,61 @@ function repayETH(shownAmount, actualAmount) {
               console.log("tx failed", res);
             }
             add_action({
-              action_title: `Repay ${symbol} on AAVE`,
-              action_type: "Repay",
-              action_tokens: JSON.stringify([`${symbol}`]),
-              action_amount: null,
-              account_id: eth_account_id,
-              account_info: "",
-              template: "AAVE",
-              action_switch: questionSwitch == "on" ? '1': '0',
-              action_status: status === 1 ? "Success" : "Failed",
-              tx_id: transactionHash,
-            });
+              "action_title": `Repay ${symbol} on AAVE`,
+              "action_type": "Repay",
+              "action_tokens": JSON.stringify([`${symbol}`]),
+              "action_amount": null,
+              "account_id": eth_account_id,
+              "account_info": "",
+              "template": "AAVE",
+              "action_status": status === 1 ? "Success": "Failed",
+              "tx_id": blockHash,
+            })
           });
         })
         .catch(() => State.update({ loading: false }));
     })
     .catch(() => State.update({ loading: false }));
 }
+
 function add_action(param_body) {
-  asyncFetch("https://bos-api.delink.one/add-action-data", {
-    method: "post",
+  asyncFetch("http://139.162.85.48:8100/add-action-data", {
+    method: 'post',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(param_body),
-  });
+    body: JSON.stringify(param_body)
+  })
 }
-const is_disabled = state.loading || Big(balance || 0).lte(0) || Big(state.amount || 0).lte(0);
+
 return (
   <>
     <Widget
-      src={`ref-bigboss.near/widget/ZKEVM.AAVE.Modal.BaseModal`}
+      src={`ref-admin.near/widget/ZKEVM.AAVE.Modal.BaseModal`}
       props={{
         title: `Repay ${symbol}`,
         onRequestClose: onRequestClose,
         children: (
           <RepayContainer>
             <Widget
-              src={`ref-bigboss.near/widget/ZKEVM.AAVE.Modal.RoundedCard`}
+              src={`ref-admin.near/widget/ZKEVM.AAVE.Modal.RoundedCard`}
               props={{
                 title: "Amount",
                 config,
                 children: (
                   <>
-                    <InputContainer>
-                      <TokenTexture>
-                        <Input
-                          type="number"
-                          value={state.amount}
-                          onChange={(e) => {
-                            changeValue(e.target.value);
-                          }}
-                          placeholder="0"
-                        />
-                      </TokenTexture>
-                      <TokenWrapper>
+                  <InputContainer>
+                    <TokenTexture>
+                      <Input
+                        type="number"
+                        value={state.amount}
+                        onChange={(e) => {
+                          changeValue(e.target.value);
+                        }}
+                        placeholder="0"
+                      />
+                    </TokenTexture>
+                    <TokenWrapper>
                         <img
                           width={26}
                           height={26}
@@ -628,32 +510,22 @@ return (
                         />
                         <TokenTexture>{symbol}</TokenTexture>
                       </TokenWrapper>
-                    </InputContainer>
-                    <BalanceContainer>
-                      <GrayTexture>${state.amountInUSD}</GrayTexture>
-                      <GrayTexture>
-                        Balance:{" "}
-                        <span
-                          className="balanceValue"
-                          onClick={() => {
-                            changeValue(shownMaxValue);
-                          }}
-                        >
-                          {balance === "" || !isValid(balance)
-                            ? "-"
-                            : Number(balance).toFixed(7)}
-                        </span>
-                      </GrayTexture>
-                    </BalanceContainer>
+                  </InputContainer>
+                  <BalanceContainer>
+                    <GrayTexture>${state.amountInUSD}</GrayTexture>
+                    <GrayTexture>
+                      Balance: <span className="balanceValue"  onClick={() => {changeValue(shownMaxValue);}}>{balance === "" || !isValid(balance)? "-": Number(balance).toFixed(7)}</span>
+                    </GrayTexture>
+                  </BalanceContainer>
                   </>
                 ),
               }}
             />
             <div className="splitDiv">
-              <div className="splitLine"></div>
+               <div className="splitLine"></div>
             </div>
             <Widget
-              src={`ref-bigboss.near/widget/ZKEVM.AAVE.Modal.RoundedCard`}
+              src={`ref-admin.near/widget/ZKEVM.AAVE.Modal.RoundedCard`}
               props={{
                 title: "Transaction Overview",
                 config,
@@ -733,73 +605,38 @@ return (
                 ),
               }}
             />
-           <div className="splitDiv">
-              <div className="splitLine"></div>
+            <div className="splitDiv">
+               <div className="splitLine"></div>
             </div>
-            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <div style={{display:'flex',justifyContent:'flex-end'}}> 
               <Widget
                 src={`${config.ownerId}/widget/AAVE.GasEstimation`}
                 props={{ gas: state.gas, config }}
               />
             </div>
-            {state.needApprove && (
-              <Widget
-                src={`rref-bigboss.near/widget/ZKEVM.AAVE.ModalPrimaryButton`}
-                props={{
-                  config,
-                  loading: state.loading,
-                  children: `Approve ${symbol}`,
-                  disabled,
-                  onClick: () => {
-                    State.update({
-                      loading: true,
-                    });
-                    const amount = Big(state.amount)
-                      .mul(Big(10).pow(decimals))
-                      .toFixed(0);
-                    approve(amount)
-                      .then((tx) => {
-                        tx.wait().then((res) => {
-                          const { status } = res;
-                          if (status === 1) {
-                            State.update({
-                              needApprove: false,
-                              loading: false,
-                            });
-                          }
-                        });
-                      })
-                      .catch(() => State.update({ loading: false }));
-                  },
-                }}
-              />
-            )}
-            {!state.needApprove && (
-              <Widget
-                src={`ref-bigboss.near/widget/ZKEVM.AAVE.ModalPrimaryButton`}
-                props={{
-                  config,
-                  children: `Repay ${symbol}`,
-                  loading: state.loading,
-                  disabled:is_disabled,
-                  onClick: () => {
-                    const actualAmount = Big(
-                      state.amount === shownMaxValue
-                        ? actualMaxValue
-                        : state.amount
-                    )
-                      .mul(Big(10).pow(decimals))
-                      .toFixed(0);
-                    const shownAmount = state.amount;
-                    if (symbol === config.nativeCurrency.symbol) {
-                      repayETH(shownAmount, actualAmount);
-                    } else {
-                      repayERC20(shownAmount, actualAmount);
-                    }
-                  },
-                }}
-              />
-            )}
+            <Widget
+              src={`ref-admin.near/widget/ZKEVM.AAVE.ModalPrimaryButton`}
+              props={{
+                config,
+                children: `Repay ${symbol}`,
+                loading: state.loading,
+                onClick: () => {
+                  const actualAmount = Big(
+                    state.amount === shownMaxValue
+                      ? actualMaxValue
+                      : state.amount
+                  )
+                    .mul(Big(10).pow(decimals))
+                    .toFixed(0);
+                  const shownAmount = state.amount;
+                  if (symbol === "ETH" || symbol === "WETH") {
+                    repayETH(shownAmount, actualAmount);
+                  } else {
+                    repayERC20(shownAmount, actualAmount);
+                  }
+                },
+              }}
+            />
           </RepayContainer>
         ),
         config,
