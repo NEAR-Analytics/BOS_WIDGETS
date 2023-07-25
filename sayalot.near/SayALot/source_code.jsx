f656257cd55dca846600e30b50ce1f8f189f8950
sayalot.near <sayalot.near>
2023-07-25 21:14:26 +0000

    Update SayALot by sayalot.near at 2023-07-25T21:14:26.119Z

--- sayalot.near/SayALot/source_code.jsx
+++ sayalot.near/SayALot/source_code.jsx
@@ -1,7 +1,9 @@
+State.init({ start: Date.now() });
+
 const isDebug = props.isDebug;
 const author = props.author;
 
-let writersWhiteList = [
+const writersWhiteList = props.writersWhiteList ?? [
   "neardigitalcollective.near",
   "blaze.near",
   "jlw.near",
@@ -12,6 +14,7 @@ let writersWhiteList = [
   "shubham007.near",
   "fiftycent.near",
   "chloe.near",
+  "ozymandius.near",
 ];
 
 const sayALotWorkers = [
@@ -26,48 +29,138 @@ if (isDebug) {
   writersWhiteList = sayALotWorkers;
 }
 
+const addressForArticles = isDebug ? "test_sayALotArticle" : "sayALotArticle";
+const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
 const authorForWidget = "sayalot.near";
 // const authorForWidget =
 // "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
-const accountId = props.accountId ?? context.accountId;
+// ========== GET INDEX ARRAY FOR ARTICLES ==========
+const postsIndex = Social.index(addressForArticles, "main", {
+  order: "desc",
+  accountId: undefined,
+});
 
-// if (!accountId) {
-//   return "No account ID";
-// }
-
-const profile = props.profile ?? Social.getr(`${accountId}/profile`);
-if (profile === null) {
-  return "Loading";
-}
-
-const authorProfile = Social.getr(`${author}/profile`);
-if (author && !authorProfile) {
+if (!postsIndex) {
   return "Loading...";
 }
 
+// ========== GET ALL ARTICLES ==========
+const resultArticles =
+  postsIndex &&
+  postsIndex
+    .reduce((acc, { accountId, blockHeight }) => {
+      const postData = Social.get(
+        `${accountId}/${addressForArticles}/main`,
+        blockHeight
+      );
+      const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
+      return [...acc, postDataWithBlockHeight];
+    }, [])
+    .filter((article) =>
+      writersWhiteList.some((addr) => addr === article.author)
+    )
+    .filter((article) => article.lastEditor === article.author)
+    .filter((article) => !articleBlackList.includes(article.blockHeight));
+
+if (resultArticles.length === 0) {
+  return "No articles found yet";
+}
+
+// ========== FILTER DUPLICATES ==========
+let filteredArticles =
+  resultArticles.length &&
+  resultArticles.reduce((acc, article) => {
+    if (!acc.some(({ articleId }) => articleId === article.articleId)) {
+      return [...acc, article];
+    } else {
+      return acc;
+    }
+  }, []);
+
+if (author) {
+  filteredArticles = filteredArticles.filter(
+    (article) => article.author === author
+  );
+}
+
+const getDateLastEdit = (timestamp) => {
+  const date = new Date(Number(timestamp));
+  const dateString = {
+    date: date.toLocaleDateString(),
+    time: date.toLocaleTimeString(),
+  };
+  return dateString;
+};
+
 return (
-  <div
-    className="container-fluid"
-    style={{
-      backgroundColor: "rgb(230, 230, 230)",
-      borderRadius: "20px",
-      padding: "0",
-    }}
-  >
-    <Widget
-      src={`${authorForWidget}/widget/SayALot_MainNavigation`}
-      props={{ currentNavPill: "articles", writersWhiteList, isDebug }}
-    />
-    <div style={{ margin: "0 auto", width: "90%", minWidth: "360px" }}>
-      {author && (
-        <h4
-          style={{ margin: "1.5rem 0 0 0" }}
-        >{`${authorProfile.name}'s articles`}</h4>
-      )}
-      <Widget
-        src={`${authorForWidget}/widget/SayALot_AllArticlesList`}
-        props={{ writersWhiteList, isDebug, author }}
-      />
-    </div>
+  <div className="row card-group py-3">
+    {filteredArticles.length > 0 &&
+      filteredArticles.map((article) => {
+        // If some widget posts data different than an array it will be ignored
+        if (!Array.isArray(article.tags)) article.tags = [];
+        return (
+          <div
+            className="col-sm-12 col-lg-6 col-2xl-4 gy-3"
+            key={article.articleId}
+          >
+            <div className="card h-100">
+              <a
+                className="text-decoration-none text-dark"
+                href={
+                  isDebug
+                    ? `#/${authorForWidget}/widget/SayALot_OneArticle?articleId=${article.articleId}&blockHeight=${article.blockHeight}&lastEditor=${article.lastEditor}&isDebug=true
+            `
+                    : `#/${authorForWidget}/widget/SayALot_OneArticle?articleId=${article.articleId}&blockHeight=${article.blockHeight}&lastEditor=${article.lastEditor}
+            `
+                }
+              >
+                <div className="card-body">
+                  <div className="row d-flex justify-content-center">
+                    <h5 className="card-title text-center pb-2 border-bottom">
+                      {article.articleId}
+                    </h5>
+                    <div className="col flex-grow-1">
+                      <Widget
+                        src="mob.near/widget/Profile.ShortInlineBlock"
+                        props={{ accountId: article.author, tooltip: true }}
+                      />
+                    </div>
+                    <div className="col flex-grow-0">
+                      <p className="card-subtitle text-muted text-end">
+                        {getDateLastEdit(article.timeCreate).date}
+                      </p>{" "}
+                      <p className="card-subtitle text-muted text-end">
+                        {getDateLastEdit(article.timeCreate).time}
+                      </p>
+                    </div>
+                  </div>
+                  <div
+                    className="mt-3 alert alert-secondary"
+                    style={{ backgroundColor: "white" }}
+                  >
+                    <div>
+                      Last edit by{" "}
+                      <a
+                        href={`https://near.social/#/mob.near/widget/ProfilePage?accountId=${article.lastEditor}`}
+                        style={{ textDecoration: "underline" }}
+                      >
+                        {article.lastEditor}
+                      </a>
+                      <br />
+                      Edited on {getDateLastEdit(article.timeLastEdit).date}
+                      <br />
+                      Edit versions: {article.version}
+                    </div>
+                    <Widget
+                      src={`${authorForWidget}/widget/SayALot_TagList`}
+                      props={{ tags: article.tags, isDebug }}
+                    />
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        );
+      })}
   </div>
 );
