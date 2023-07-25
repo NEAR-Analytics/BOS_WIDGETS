bfeaee23fc40589bed1e1d7bb12e41818088aab1
eugenewolf507.near <eugenewolf507.near>
2023-07-25 15:54:05 +0000

    Update CloseIcon by eugenewolf507.near at 2023-07-25T15:54:05.259Z

---
+++ eugenewolf507.near/CloseIcon/source_code.jsx
@@ -0,0 +1,14 @@
+return (
+  <svg
+    width="22"
+    height="22"
+    viewBox="0 0 22 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4.59662 19L3 17.4L9.3865 11L3 4.6L4.59662 3L10.9831 9.4L17.3696 3L18.9662 4.6L12.5797 11L18.9662 17.4L17.3696 19L10.9831 12.6L4.59662 19Z"
+      fill="black"
+    />
+  </svg>
+);
