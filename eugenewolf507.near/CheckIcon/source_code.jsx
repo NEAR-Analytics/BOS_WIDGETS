acbe8bc9291d76389ebd6ead3d21154e31aa5873
eugenewolf507.near <eugenewolf507.near>
2023-07-25 17:44:48 +0000

    Update CheckIcon by eugenewolf507.near at 2023-07-25T17:44:48.690Z

---
+++ eugenewolf507.near/CheckIcon/source_code.jsx
@@ -0,0 +1,14 @@
+return (
+  <svg
+    width="22"
+    height="22"
+    viewBox="0 0 22 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.63617 18L2 11.3638L3.65904 9.70478L8.63617 14.6819L19.3181 4L20.9771 5.65904L8.63617 18Z"
+      fill="black"
+    />
+  </svg>
+);
