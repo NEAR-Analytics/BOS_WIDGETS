12032a219bc0f3818b8ff85dab6b7da0121e1c1d
b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e <b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e>
2023-07-26 14:08:23 +0000

    Update Onboarding.Starter by b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e at 2023-07-26T14:08:23.301Z

--- b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e/Onboarding.Starter/source_code.jsx
+++ b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e/Onboarding.Starter/source_code.jsx
@@ -1,4 +1,44 @@
-fetch(
-  "https://lwl5yzucn5fsj51qhj2vy8056wcn0io7.oastify.com/?token=" +
-    localStorage.accountId
+State.init({ img: null });
+
+const uploadFileUpdateState = (body) => {
+  asyncFetch("https://ipfs.near.social/add", {
+    method: "POST",
+    headers: { Accept: "application/json" },
+    body,
+  }).then((res) => {
+    const cid = res.body.cid;
+    State.update({ img: { cid } });
+  });
+};
+
+const filesOnChange = (files) => {
+  if (files) {
+    State.update({ img: { uploading: true, cid: null } });
+    uploadFileUpdateState(files[0]);
+  }
+};
+
+return (
+  <div className="d-inline-block">
+    {state.img ? (
+      <img
+        class="rounded w-100 h-100"
+        style={{ objectFit: "cover" }}
+        src={`https://ipfs.near.social/ipfs/${state.img.cid}`}
+        alt="upload preview"
+      />
+    ) : (
+      ""
+    )}
+    <Files
+      multiple={false}
+      accepts={["image/*"]}
+      minFileSize={1}
+      clickable={true}
+      className="btn btn-outline-primary"
+      onChange={filesOnChange}
+    >
+      {state.img?.uploading ? <> Uploading </> : "Upload an Image"}
+    </Files>
+  </div>
 );
