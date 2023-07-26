694b287971233c959b68a59569579be5b130b4f0
ndcplug.near <ndcplug.near>
2023-07-26 23:17:10 +0000

    Update BOSHACKS.Members.Mentors by ndcplug.near at 2023-07-26T23:17:10.935Z

--- ndcplug.near/BOSHACKS.Members.Mentors/source_code.jsx
+++ ndcplug.near/BOSHACKS.Members.Mentors/source_code.jsx
@@ -29,13 +29,6 @@ const Container = styled.div`
 const judges = [
   {
     image:
-      "https://ipfs.near.social/ipfs/bafkreigla4huhv2flyu5jyjemtf3fa6lhu5vnpixnrszhw3346ihofaefm",
-    name: "Evgeny Kuzyakov",
-    org: "Founder @ NEAR.Social",
-    accountId: "mob.near",
-  },
-  {
-    image:
       "https://ipfs.near.social/ipfs/bafkreicahodss6wyvssxbiewergh2ewk6wsnsvqftaktjbff6mcbtp6g3e",
     name: "Alan Estrada",
     org: "Open Web Academy",
