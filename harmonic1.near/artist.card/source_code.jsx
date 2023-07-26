bac68c2925e031e664e5a7845a5c01627a2a5334
harmonic1.near <harmonic1.near>
2023-07-26 11:23:40 +0000

    Update artist.card by harmonic1.near at 2023-07-26T11:23:40.728Z

--- harmonic1.near/artist.card/source_code.jsx
+++ harmonic1.near/artist.card/source_code.jsx
@@ -41,7 +41,7 @@ const Card = styled.div`
 return (
   // Have to rewrite thing veiwer to do so.
   <a
-    href={`every.near/widget/every.thing.view?path=${path}/${handle}`}
+    href={`/#/every.near/widget/every.thing.view?path=${path}/${handle}`}
     style={{
       color: "black",
     }}
