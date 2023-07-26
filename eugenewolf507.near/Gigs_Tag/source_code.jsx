209e725eed400dad457c96fb8caf077984572b18
eugenewolf507.near <eugenewolf507.near>
2023-07-26 20:39:39 +0000

    Update Gigs_Tag by eugenewolf507.near at 2023-07-26T20:39:39.428Z

---
+++ eugenewolf507.near/Gigs_Tag/source_code.jsx
@@ -0,0 +1,22 @@
+const variant = props.variant ?? "";
+const className = props.className ?? "";
+
+const Tag = styled.span`
+display: inline-block;
+padding: 4px 8px;
+border-radius: 100px;
+background: var(--gradient-purple-gradient, linear-gradient(90deg, #9333EA 0%, #4F46E5 100%));
+color: #FFF;
+font-family: Open Sans;
+font-size: 8px;
+font-style: normal;
+font-weight: 400;
+line-height: 120%; /* 9.6px */
+
+&.outline { 
+      background: transparent;
+      color: #4f46e5;
+      border: 1px solid #4f46e5;
+    }
+`;
+return <Tag className={`${variant} ${className}`}>Hello World</Tag>;
