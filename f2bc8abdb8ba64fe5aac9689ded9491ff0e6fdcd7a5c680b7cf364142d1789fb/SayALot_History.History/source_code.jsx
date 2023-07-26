2326a70aee02dce53955cb084b0b6cb92c170dd4
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-07-26 17:49:45 +0000

    Update SayALot_History.History by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-07-26T17:49:45.696Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_History.History/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_History.History/source_code.jsx
@@ -5,9 +5,38 @@ count(count: number)?: function,
 
 const isDebug = props.isDebug;
 const addressForArticles = isDebug ? "test_sayALotArticle" : "sayALotArticle";
+
+const author = props.author;
+
 const authorForWidget =
   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 const { articleId } = props;
+
+const writersWhiteList = props.writersWhiteList ?? [
+  "neardigitalcollective.near",
+  "blaze.near",
+  "jlw.near",
+  "kazanderdad.near",
+  "joep.near",
+  "sarahkornfeld.near",
+  "yuensid.near",
+  "shubham007.near",
+  "fiftycent.near",
+  "chloe.near",
+  "ozymandius.near",
+];
+
+const sayALotWorkers = [
+  "silkking.near",
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+  "blaze.near",
+  "ayelen.near",
+  "kenrou-it.near",
+];
+
+if (isDebug) {
+  writersWhiteList = sayALotWorkers;
+}
 
 // if (typeof props.widgetPath !== "string")
 //   return "send {widgetPath} as string in props";
@@ -17,37 +46,173 @@ State.init({
   selectedBlockHeight: null,
 });
 
-// ========== GET INDEX ARRAY FOR ARTICLES ==========
-const postsIndex = Social.index(addressForArticles, "main", {
-  order: "desc",
-  accountId: undefined,
-});
-// ========== GET ALL ARTICLES ==========
-const resultArticles =
-  postsIndex &&
-  postsIndex.reduce((acc, { accountId, blockHeight }) => {
-    const postData = Social.get(
-      `${accountId}/${addressForArticles}/main`,
-      blockHeight
+// // ========== GET INDEX ARRAY FOR ARTICLES ==========
+// const postsIndex = Social.index(addressForArticles, "main", {
+//   order: "desc",
+//   accountId: undefined,
+// });
+// // ========== GET ALL ARTICLES ==========
+// const resultArticles =
+//   postsIndex &&
+//   postsIndex.reduce((acc, { accountId, blockHeight }) => {
+//     const postData = Social.get(
+//       `${accountId}/${addressForArticles}/main`,
+//       blockHeight
+//     );
+//     const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
+//     return [...acc, postDataWithBlockHeight];
+//   }, []);
+// if (resultArticles === null) return "loading...";
+// // ========== FIND ALL VERSIONS OF ONE ARTICLE ==========
+// const filteredArticles =
+//   resultArticles.length &&
+//   resultArticles.reduce((acc, article) => {
+//     if (article.articleId === articleId) {
+//       return [...acc, article];
+//     } else {
+//       return acc;
+//     }
+//   }, []);
+
+const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
+
+function getLastEditionsByArticle() {
+  const allArticles = Social.index(addressForArticles, "main", {
+    order: "desc",
+    accountId: author,
+  });
+
+  const oldFormatArticlesTestBasicDataArray = [
+    [
+      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+      97325392,
+    ],
+    [
+      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+      97317287,
+    ],
+    ["ayelen.near", 96927579],
+    ["kenrou-it.near", 96924422],
+    [
+      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+      96879470,
+    ],
+    [
+      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+      96878182,
+    ],
+    [
+      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
+      96643643,
+    ],
+    ["silkking.near", 96491128],
+  ];
+
+  const oldFormatArticlesMainBasicDataArray = [
+    ["ozymandius.near", 97329049],
+    ["fiftycent.near", 97322138],
+    ["blaze.near", 97255023],
+    ["jlw.near", 97250015],
+    ["kazanderdad.near", 96692435],
+    ["blaze.near", 96414482],
+    ["blaze.near", 96412953],
+    ["sarahkornfeld.near", 96402919],
+    ["sarahkornfeld.near", 96402476],
+    ["sarahkornfeld.near", 96402330],
+    ["sarahkornfeld.near", 96401880],
+    ["ozymandius.near", 95810612],
+    ["blaze.near", 95766756],
+    ["blaze.near", 95766700],
+    ["jlw.near", 95705034],
+    ["blaze.near", 95413943],
+    ["blaze.near", 94936576],
+    ["yuensid.near", 94866690],
+    ["sarahkornfeld.near", 94863580],
+    ["blaze.near", 94801223],
+    ["sarahkornfeld.near", 94344236],
+    ["sarahkornfeld.near", 94188387],
+    ["jlw.near", 93986868],
+    ["blaze.near", 92999498],
+  ];
+
+  const oldFormatArticlesBasicDataArray = isDebug
+    ? oldFormatArticlesTestBasicDataArray
+    : oldFormatArticlesMainBasicDataArray;
+
+  if (author) {
+    oldFormatArticlesBasicDataArray = oldFormatArticlesBasicDataArray.filter(
+      (articleBasicData) => articleBasicData[0] === author
     );
-    const postDataWithBlockHeight = { ...JSON.parse(postData), blockHeight };
-    return [...acc, postDataWithBlockHeight];
-  }, []);
-if (resultArticles === null) return "loading...";
-// ========== FIND ALL VERSIONS OF ONE ARTICLE ==========
-const filteredArticles =
-  resultArticles.length &&
-  resultArticles.reduce((acc, article) => {
-    if (article.articleId === articleId) {
-      return [...acc, article];
-    } else {
-      return acc;
+  }
+
+  let oldFormatArticlesArray = oldFormatArticlesBasicDataArray.map(
+    (oldFormatBasicArticleData) => {
+      let article = Social.get(
+        `${oldFormatBasicArticleData[0]}/${addressForArticles}/main`,
+        oldFormatBasicArticleData[1]
+      );
+
+      let articleParsed = JSON.parse(article);
+      articleParsed.blockHeight = oldFormatBasicArticleData[1];
+
+      return articleParsed;
     }
-  }, []);
+  );
+
+  let newFormatArticlesData = allArticles
+    .filter((articleIndex) => articleIndex.value.id)
+    .filter(
+      (articleIndex) =>
+        articleIndex.value.id.split("-")[0] === articleIndex.accountId
+    )
+    .filter((articleIndex) => writersWhiteList.includes(articleIndex.accountId))
+    .filter(
+      (articleIndex) => !articleBlackList.includes(articleIndex.blockHeight)
+    );
+
+  let lastestEditArticlesDataArray = newFormatArticlesData.filter(
+    (articleData) => {
+      const latestEditForThisArticle = newFormatArticlesData.find(
+        (newArticleData) => newArticleData.value.id
+      );
+      return (
+        JSON.stringify(articleData) === JSON.stringify(latestEditForThisArticle)
+      );
+    }
+  );
+
+  let finalNewFormatArticles = lastestEditArticlesDataArray.map(
+    (latestEditArticle) => {
+      const article = Social.get(
+        `${latestEditArticle.accountId}/${addressForArticles}/main`,
+        latestEditArticle.blockHeight
+      );
+
+      let articleParsed = JSON.parse(article);
+      articleParsed.blockHeight = latestEditArticle.blockHeight;
+
+      return articleParsed;
+    }
+  );
+
+  let finalOldFormatArticles = oldFormatArticlesArray.filter(
+    (oldFormatArticle) => {
+      return !finalNewFormatArticles.find(
+        (newFormatArticle) =>
+          newFormatArticle.articleId === oldFormatArticle.articleId
+      );
+    }
+  );
+
+  let finalArticles = finalNewFormatArticles.concat(finalOldFormatArticles);
+
+  return finalArticles;
+}
+const finalArticles = getLastEditionsByArticle();
 // ========== GET ARRAY OF BLOCK HEIGHT AND LAST EDITOR ==========
 let blocksChanges =
-  filteredArticles &&
-  filteredArticles.map((item) => ({
+  finalArticles &&
+  finalArticles.map((item) => ({
     blockHeight: item.blockHeight,
     lastEditor: item.lastEditor,
   }));
@@ -212,7 +377,7 @@ return (
           <div>
             {blockHeightToWidgetRender(
               state.selectedBlockHeight,
-              filteredArticles
+              finalArticles
             )}
           </div>
         )}
