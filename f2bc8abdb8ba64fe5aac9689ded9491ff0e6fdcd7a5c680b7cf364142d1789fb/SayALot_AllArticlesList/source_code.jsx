6b9e75fd5649618b62704852093c89a95ec6d20e
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-07-26 17:53:30 +0000

    Update SayALot_AllArticlesList by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-07-26T17:53:30.281Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_AllArticlesList/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot_AllArticlesList/source_code.jsx
@@ -29,9 +29,9 @@ if (isDebug) {
 }
 
 const addressForArticles = isDebug ? "test_sayALotArticle" : "sayALotArticle";
-const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
 const authorForWidget =
   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
 
 function getLastEditionsByArticle() {
   const allArticles = Social.index(addressForArticles, "main", {
