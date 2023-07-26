a94b7c4e56e4e81fc04633643060989878999e03
ndcplug.near <ndcplug.near>
2023-07-26 23:15:01 +0000

    Update BOSHACKS.Members.Mentors by ndcplug.near at 2023-07-26T23:15:01.360Z

--- ndcplug.near/BOSHACKS.Members.Mentors/source_code.jsx
+++ ndcplug.near/BOSHACKS.Members.Mentors/source_code.jsx
@@ -29,6 +29,27 @@ const Container = styled.div`
 const judges = [
   {
     image:
+      "https://ipfs.near.social/ipfs/bafkreigla4huhv2flyu5jyjemtf3fa6lhu5vnpixnrszhw3346ihofaefm",
+    name: "Evgeny Kuzyakov",
+    org: "Founder @ NEAR.Social",
+    accountId: "mob.near",
+  },
+  {
+    image:
+      "https://ipfs.near.social/ipfs/bafkreicahodss6wyvssxbiewergh2ewk6wsnsvqftaktjbff6mcbtp6g3e",
+    name: "Alan Estrada",
+    org: "Open Web Academy",
+    accountId: "alan777.near",
+  },
+  {
+    image:
+      "https://ipfs.near.social/ipfs/bafkreig3ikeylk2xb3qhjm73hg33zaagqyjavonepcf2x5m3hqwjtfz6ou",
+    name: "Yair Nava",
+    org: "Open Web Academy",
+    accountId: "yairnava.near",
+  },
+  {
+    image:
       "https://ipfs.near.social/ipfs/bafkreigxnzkbaal7c3gaxqga6tslcssteekj5gu75hmi6z6635m6e4i7k4",
     name: "Web3Plug",
     org: "The Crypto Streets",
