3961cac70323f093405a5aeaf5ddb50936708e74
sking.near <sking.near>
2023-07-27 15:09:00 +0000

    Update module-test-hello by sking.near at 2023-07-27T15:09:00.617Z

--- sking.near/module-test-hello/source_code.jsx
+++ sking.near/module-test-hello/source_code.jsx
@@ -2,15 +2,10 @@ const hello = () => {
   console.log("hello");
 };
 
-State.init({
-  name: "bobo",
-});
-
 const setName = (v) => State.update({ name: v });
 
 return {
   hello,
   user: context.accountId,
-  name,
   setName,
 };
