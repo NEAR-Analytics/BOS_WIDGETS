b1b0888371a91e0da9bc6deaf0447ba8e5f6505a
rodrigos.near <rodrigos.near>
2023-07-27 03:01:12 +0000

    Update MetaPoolStakeEth.YouWillGet by rodrigos.near at 2023-07-27T03:01:12.348Z

--- rodrigos.near/MetaPoolStakeEth.YouWillGet/source_code.jsx
+++ rodrigos.near/MetaPoolStakeEth.YouWillGet/source_code.jsx
@@ -113,7 +113,7 @@ return (
         <svg
           width="40"
           height="40"
-          viewBox="0 0 40 40"
+          viewBox="0 0 204 204"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
