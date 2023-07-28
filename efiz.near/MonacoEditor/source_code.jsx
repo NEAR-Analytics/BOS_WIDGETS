31c14f09b6b6939477fbd695ae6d4f17bc007cf9
efiz.near <efiz.near>
2023-07-28 19:29:32 +0000

    Update MonacoEditor by efiz.near at 2023-07-28T19:29:32.287Z

--- efiz.near/MonacoEditor/source_code.jsx
+++ efiz.near/MonacoEditor/source_code.jsx
@@ -2,6 +2,7 @@ const path = props.path;
 const code = props.code;
 const language = props.language;
 const onChange = props.onChange;
+
 const height = props.height || "500px";
 const Container = styled.div`
     height: ${height};
