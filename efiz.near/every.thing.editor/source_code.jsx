7026eaaa1f7e821e3009c156f0c6e50d0c6b3507
efiz.near <efiz.near>
2023-07-28 18:48:57 +0000

    Update every.thing.editor by efiz.near at 2023-07-28T18:48:57.798Z

--- efiz.near/every.thing.editor/source_code.jsx
+++ efiz.near/every.thing.editor/source_code.jsx
@@ -2,20 +2,13 @@ const path = props.path;
 const blockHeight = props.blockHeight || "final";
 
 const jThing = Social.get(path, blockHeight);
-const thing = JSON.parse(jThing);
-
-const typePath = thing.type;
-const jType = Social.get(typePath, "final");
-
-const widgetPath = thing.template.src;
-const jWidget = Social.get(widgetPath, "final");
 
 if (!jThing) {
   return <></>;
 }
 
 State.init({
-  view: "THING",
+  view: "TYPE",
 });
 
 const Button = styled.button``;
@@ -29,16 +22,18 @@ switch (state.view) {
     code = jThing;
     break;
   }
-  case "TYPE": {
-    language = "json";
-    path = typePath;
-    code = jType;
-    break;
-  }
+  // case "TYPE": {
+  //   language = "json";
+  //   const thing = JSON.parse(jThing);
+  //   path = thing.type;
+  //   code = Social.get(path, "final");
+  //   break;
+  // }
   case "WIDGET": {
     language = "javascript";
-    path = widgetPath;
-    code = jWidget;
+    const thing = JSON.parse(jThing);
+    path = thing.template.src;
+    code = Social.get(path, "final");
     break;
   }
 }
@@ -47,11 +42,12 @@ if (!code || !path || !language) {
   return <></>;
 }
 
+// <Button onClick={() => State.update({ view: "TYPE" })}>type</Button>
+
 return (
   <div>
     <div>
       <Button onClick={() => State.update({ view: "THING" })}>thing</Button>
-      <Button onClick={() => State.update({ view: "TYPE" })}>type</Button>
       <Button onClick={() => State.update({ view: "WIDGET" })}>widget</Button>
     </div>
     <Widget
