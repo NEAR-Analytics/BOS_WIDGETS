1158003b0db5bda85e9598891fff3b26b285dab0
mob.near <mob.near>
2023-07-28 20:50:45 +0000

    Update TimeAgo by mob.near at 2023-07-28T20:50:45.925Z

--- mob.near/TimeAgo/source_code.jsx
+++ mob.near/TimeAgo/source_code.jsx
@@ -31,13 +31,27 @@ if (!res.ok || res.body === "null") {
 
 const timeMs = parseFloat(res.body);
 
+const date = new Date(timeMs);
+const title = `${date.toLocaleTimeString([], {
+  hour: "2-digit",
+  minute: "2-digit",
+})} ${date.toLocaleDateString([], {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+})}`;
+
 const timeAgo = (diffSec) =>
   diffSec < 60000
-    ? `${(diffSec / 1000) | 0} seconds ago`
+    ? `${(diffSec / 1000) | 0}s`
     : diffSec < 3600000
-    ? `${(diffSec / 60000) | 0} minutes ago`
+    ? `${(diffSec / 60000) | 0}m`
     : diffSec < 86400000
-    ? `${(diffSec / 3600000) | 0} hours ago`
-    : `${(diffSec / 86400000) | 0} days ago`;
+    ? `${(diffSec / 3600000) | 0}h`
+    : `${(diffSec / 86400000) | 0}d`;
 
-return timeAgo(Date.now() - timeMs);
+return (
+  <span className={props.className} title={title}>
+    {timeAgo(Date.now() - timeMs)}
+  </span>
+);
