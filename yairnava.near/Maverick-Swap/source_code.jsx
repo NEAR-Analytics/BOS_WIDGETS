6251149fc19bd01ccdf447e29c786c43be3000b6
yairnava.near <yairnava.near>
2023-07-28 21:17:15 +0000

    Update Maverick-Swap by yairnava.near at 2023-07-28T21:17:15.358Z

--- yairnava.near/Maverick-Swap/source_code.jsx
+++ yairnava.near/Maverick-Swap/source_code.jsx
@@ -135,6 +135,7 @@ const getErc20Balance = (tokenId, receiver, decimals, asset) => {
           inputBalance: parseFloat(
             ethers.utils.formatUnits(balance, decimals)
           ).toFixed(6),
+          unFixedInputBalance: balance.toHexString(),
         });
       });
   } else {
@@ -152,7 +153,10 @@ const getErc20Balance = (tokenId, receiver, decimals, asset) => {
         );
         contract.balanceOf(receiver).then((res) => {
           let balance = ethers.utils.formatUnits(res, decimals);
-          State.update({ inputBalance: parseFloat(balance).toFixed(6) });
+          State.update({
+            inputBalance: parseFloat(balance).toFixed(6),
+            unFixedInputBalance: res.toHexString(),
+          });
         });
       });
   }
@@ -224,7 +228,6 @@ const tokenInApprovaleNeededCheck = (data) => {
 };
 
 const approveErc20Token = () => {
-  console.log("AQUI");
   asyncFetch(
     "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json"
   ).then((res) => {
