18e968bbb1ccae9e334ca2645e8885ab56210042
efiz.near <efiz.near>
2023-07-28 05:28:08 +0000

    Update every.thing by efiz.near at 2023-07-28T05:28:08.273Z

--- efiz.near/every.thing/source_code.jsx
+++ efiz.near/every.thing/source_code.jsx
@@ -1,12 +1,65 @@
-const typeName = props.typeName || "widget";
+const typeName = props.typeName || "thing";
 const metadataTemplate = props.metadataTemplate || "efiz.near/widget/docs.card";
 
 State.init({
   accountId: props.accountId || null,
   tag: props.tag || null,
+  selectedElements: [],
+  isExpanded: false,
+  selectedOption: "all",
 });
 
-let keys = `${state.accountId ?? "*"}/${typeName}/*`;
+const ArrowIcon = styled.span`
+  display: inline-block;
+  width: 0;
+  height: 0;
+  border-left: 6px solid transparent;
+  border-right: 6px solid transparent;
+  border-top: 6px solid ${(props) => (props.isExpanded ? "#fff" : "#000")};
+  margin-right: 5px;
+  transition: transform 0.3s;
+  transform: rotate(${(props) => (props.isExpanded ? "180deg" : "0")});
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: ${(props) => (props.isExpanded ? "#fff" : "#000")};
+  margin-top: 10px;
+  transition: background-color 0.3s;
+`;
+
+const AdvancedContainer = styled.div`
+  margin: 20px 0;
+`;
+
+const AdvancedButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  border: none;
+  background-color: ${(props) => (props.isExpanded ? "#007bff" : "#28a745")};
+  color: #fff;
+  font-size: 16px;
+  text-align: left;
+  cursor: pointer;
+`;
+
+const FiltersContainer = styled.div`
+  margin-top: 10px;
+  display: ${(props) => (props.isExpanded ? "block" : "none")};
+`;
+
+const RadioButton = styled.div`
+  margin-top: 10px;
+`;
+
+const handleToggle = () => {
+  State.update({ isExpanded: !state.isExpanded });
+};
+
+const handleRadioChange = (event) => {
+  State.update({ selectedOption: event.target.value });
+};
 
 function handleFilter({ accountId, tag }) {
   if (accountId) {
@@ -17,36 +70,148 @@ function handleFilter({ accountId, tag }) {
   }
 }
 
-if (state.tag) {
-  const taggedWidgets = Social.keys(
-    `${state.accountId ?? "*"}/${typeName}/*/metadata/tags/${state.tag}`,
-    "final"
+function Advanced() {
+  return (
+    <AdvancedContainer>
+      <AdvancedButton onClick={handleToggle} isExpanded={state.isExpanded}>
+        <ArrowIcon onClick={handleToggle} isExpanded={state.isExpanded} />
+        {isExpanded ? "Hide Advanced" : "Show Advanced"}
+      </AdvancedButton>
+      <FiltersContainer isExpanded={state.isExpanded}>
+        <Widget
+          src="efiz.near/widget/every.thing.filters"
+          props={{ handleFilter }}
+        />
+        <RadioButton>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="filterOption"
+              id="allOption"
+              value="all"
+              checked={state.selectedOption === "all"}
+              onChange={handleRadioChange}
+            />
+
+            <label htmlFor="allOption">All</label>
+          </div>
+        </RadioButton>
+        <RadioButton>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="filterOption"
+              id="taggedByCreatorOption"
+              value="taggedByCreator"
+              checked={state.selectedOption === "taggedByCreator"}
+              onChange={handleRadioChange}
+            />
+
+            <label htmlFor="taggedByCreatorOption">Tagged by Creator</label>
+          </div>
+        </RadioButton>
+        <RadioButton>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="filterOption"
+              id="taggedByCommunityOption"
+              value="taggedByCommunity"
+              checked={state.selectedOption === "taggedByCommunity"}
+              onChange={handleRadioChange}
+            />
+
+            <label htmlFor="taggedByCommunityOption">Tagged by Community</label>
+          </div>
+        </RadioButton>
+        <RadioButton>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="filterOption"
+              id="curatedByCategoryOption"
+              value="curatedByCategory"
+              checked={state.selectedOption === "curatedByCategory"}
+              onChange={handleRadioChange}
+            />
+
+            <label htmlFor="curatedByCategoryOption">Curated by Category</label>
+          </div>
+        </RadioButton>
+      </FiltersContainer>
+    </AdvancedContainer>
   );
-
-  if (taggedWidgets === null) {
-    return <p>Loading tags</p>;
-  }
-
-  keys = Object.entries(taggedWidgets)
-    .map((kv) =>
-      Object.keys(kv[1][typeName]).map((w) => `${kv[0]}/${typeName}/${w}`)
-    )
-    .flat();
-
-  if (!keys.length) {
-    return (
-      <>
-        <p>No types found by tag #{state.tag}</p>
-        <button onClick={() => State.update({ tag: null })}>reset</button>
-      </>
-    );
-  }
 }
 
-const data = Social.keys(keys, "final", {
-  return_type: "BlockHeight",
-  limit: 1,
-});
+const data = {};
+
+switch (state.selectedOption) {
+  case "all": {
+    const pattern = `${state.accountId ?? "*"}/${typeName}/*`;
+    data = Social.keys(pattern, "final", {
+      return_type: "BlockHeight",
+      limit: 1,
+    });
+    break;
+  }
+  case "taggedByCreator": {
+    const pattern = `${state.accountId ?? "*"}/${typeName}/*/metadata/tags/${
+      state.tag ?? "*"
+    }`;
+    const keys = Social.keys(pattern, "final");
+
+    if (keys) {
+      keys = Object.entries(keys).flatMap(([key, value]) =>
+        Object.keys(value[typeName]).map((w) => `${key}/${typeName}/${w}`)
+      );
+      data = Social.keys(keys, "final", {
+        return_type: "BlockHeight",
+        limit: 1,
+      });
+    }
+    break;
+  }
+  case "taggedByCommunity": {
+    const pattern = `*/every/${typeName}/${state.accountId ?? "*"}/*/tags/${
+      state.tag ?? "*"
+    }`;
+    const keys = Social.keys(pattern, "final");
+
+    if (keys) {
+      keys = Object.entries(keys).flatMap(([key, value]) =>
+        Object.entries(value.every[typeName]).flatMap(([w, it]) =>
+          Object.keys(it).map((itKey) => `${w}/${typeName}/${itKey}`)
+        )
+      );
+      data = Social.keys(keys, "final", {
+        return_type: "BlockHeight",
+        limit: 1,
+      });
+    }
+    break;
+  }
+  case "curatedByCategory": {
+    const category = "test";
+    const pattern = `*/test/thing/*/categories/${category}`;
+    const keys = Social.get(pattern, "final");
+    if (keys) {
+      keys = Object.entries(keys).flatMap(([k, v]) => {
+        return Object.entries(v.test[typeName]).flatMap(([ka, kn]) =>
+          Object.entries(kn.categories).flatMap(([key, value]) => value)
+        );
+      });
+      data = Social.keys(keys, "final", {
+        return_type: "BlockHeight",
+        limit: 1,
+      });
+    }
+    break;
+  }
+}
 
 const processData = (data) => {
   const accounts = Object.entries(data);
@@ -66,34 +231,58 @@ const processData = (data) => {
   return allItems;
 };
 
+const handleSelect = (id) => {
+  if (state.selectedElements.includes(id)) {
+    State.update({
+      selectedElements: state.selectedElements.filter(
+        (elementId) => elementId !== id
+      ),
+    });
+  } else {
+    State.update({
+      selectedElements: [...state.selectedElements, id],
+    });
+  }
+};
+
 const renderItem = (a) => {
+  const path = `${a.accountId}/${typeName}/${a.typeName}`;
   return (
     <div className="m-1" key={JSON.stringify(a)} style={{ minHeight: "10em" }}>
+      <div className="form-check">
+        <input
+          type="checkbox"
+          checked={state.selectedElements.includes(path)}
+          onChange={() => handleSelect(path)}
+          className="form-check-input"
+        />
+      </div>
       <Widget
         src={metadataTemplate}
         props={{
-          path: `${a.accountId}/${typeName}/${a.typeName}`,
+          path,
         }}
       />
     </div>
   );
 };
 
-if (JSON.stringify(data) !== JSON.stringify(state.data || {})) {
+if (data && JSON.stringify(data) !== JSON.stringify(state.data || {})) {
   State.update({
     data,
     allItems: processData(data),
   });
 }
+
 return (
-  <div className="px-2 mx-auto">
-    <Widget
-      src="efiz.near/widget/every.thing.filters"
-      props={{ handleFilter: handleFilter }}
-    />
-    <Widget
-      src="efiz.near/widget/ItemFeed"
-      props={{ items: state.allItems || [], renderItem }}
-    />
+  <div>
+    <Advanced />
+    <div className="px-2 mx-auto">
+      <button onClick={() => Social.set(state.selectedElements)}>save</button>
+      <Widget
+        src="efiz.near/widget/ItemFeed"
+        props={{ items: state.allItems || [], renderItem }}
+      />
+    </div>
   </div>
 );
