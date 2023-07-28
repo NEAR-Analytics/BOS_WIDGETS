1c37ba6468990954bf6ee3da3ac3ded102260780
ndcplug.near <ndcplug.near>
2023-07-28 17:15:23 +0000

    Update Deploy.GithubButton by ndcplug.near at 2023-07-28T17:15:23.063Z

---
+++ ndcplug.near/Deploy.GithubButton/source_code.jsx
@@ -0,0 +1,12 @@
+const link = props.link ?? "https://github.com/NEARSocial/viewer";
+
+return (
+  <div>
+    <a href={link} target="_blank">
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreig2zdryahlj4v6lr6ib62ddjel6dekogsicgagdj3fbvw2nupj2iy"
+        alt="Check On Gtihub"
+      />
+    </a>
+  </div>
+);
