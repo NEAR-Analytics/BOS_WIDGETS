9e428896cb46ecc982ac67bbf08d2e4249aa9efa
proofofvibes.near <proofofvibes.near>
2023-07-29 02:38:46 +0000

    Update Vibes.Feed.Post.Location by proofofvibes.near at 2023-07-29T02:38:46.694Z

--- proofofvibes.near/Vibes.Feed.Post.Location/source_code.jsx
+++ proofofvibes.near/Vibes.Feed.Post.Location/source_code.jsx
@@ -1,3 +1,5 @@
+const onLocate = props.onLocate;
+
 State.init({
   location: "",
 });
@@ -6,11 +8,9 @@ const findLocation = () => {
   asyncFetch(
     "https://api.geoapify.com/v1/ipinfo?&apiKey=0485481476634b4d98f7d337d4821f52"
   ).then((data) => {
-    State.update({
-      location: data.body.city.name + ", " + data.body.country.name,
-    });
+    onLocate(data.body);
   });
 };
 findLocation();
 
-return <>{state.location}</>;
+return <></>;
