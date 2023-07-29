e6d8f482bb0dad96d82b9822b495073b133539cc
agruzdev.near <agruzdev.near>
2023-07-29 15:21:14 +0000

    Update CompTree by agruzdev.near at 2023-07-29T15:21:14.069Z

--- agruzdev.near/CompTree/source_code.jsx
+++ agruzdev.near/CompTree/source_code.jsx
@@ -1,1 +1,17 @@
-return <div>Hello World</div>;
+State.init({
+  components: null,
+});
+
+const onChange = (value) => {
+  State.update({ components: value?.result });
+};
+
+return (
+  <>
+    <Widget
+      src={"hack.near/widget/ComponentSearch"}
+      props={{ onChange: onChange }}
+    />
+    {state.components && state.components.length !== 0 ? <></> : null}
+  </>
+);
