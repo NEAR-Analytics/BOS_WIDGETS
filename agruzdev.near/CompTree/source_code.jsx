e4dc7fcc43134204e0619f4dad51925127628068
agruzdev.near <agruzdev.near>
2023-07-29 15:24:32 +0000

    Update CompTree by agruzdev.near at 2023-07-29T15:24:32.576Z

--- agruzdev.near/CompTree/source_code.jsx
+++ agruzdev.near/CompTree/source_code.jsx
@@ -6,12 +6,33 @@ const onChange = (value) => {
   State.update({ components: value?.result });
 };
 
+const Main = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+
+const Item = styled.div`
+  margin-top: 10px;
+`;
+
 return (
-  <>
-    <Widget
-      src={"hack.near/widget/ComponentSearch"}
-      props={{ onChange: onChange }}
-    />
-    {state.components && state.components.length !== 0 ? <></> : null}
-  </>
+  <Main>
+    <Item>
+      <Widget
+        src={"hack.near/widget/ComponentSearch"}
+        props={{ onChange: onChange }}
+      />
+    </Item>
+    {state.components && state.components.length !== 0
+      ? state.components.map((component) => (
+          <Item>
+            <Widget
+              src={"agruzdev.near/widget/CompTree.View.ComponentCard"}
+              props={{ src: component.widgetSrc }}
+            />
+          </Item>
+        ))
+      : null}
+  </Main>
 );
