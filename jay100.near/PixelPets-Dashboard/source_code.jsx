251877deb2fdb0c9f8804d686812020ef3bf3d6f
jay100.near <jay100.near>
2023-07-30 18:52:36 +0000

    Update PixelPets-Dashboard by jay100.near at 2023-07-30T18:52:36.898Z

--- jay100.near/PixelPets-Dashboard/source_code.jsx
+++ jay100.near/PixelPets-Dashboard/source_code.jsx
@@ -2,13 +2,9 @@ const pixelFont = fetch(
   "https://fonts.googleapis.com/css?family=Press+Start+2P"
 ).body;
 
-State.init({
-  display_weapons: true,
-});
-
 const PlayerDashboard = () => {
   let playerData = Near.view("pixeltoken.near", "get_player_data", {
-    account_id: "chloe.near",
+    account_id: context.accountId,
   });
 
   let player_balance = playerData.balance;
@@ -37,7 +33,7 @@ const PlayerDashboard = () => {
     {}
   ).decimals;
 
-  console.log(playerData.playerdata);
+  console.log(pets_data);
 
   const DashBoardContainer = styled.div`
     font-family: "Pixel Emulator", "Press Start 2P", "Courier new", "monospace";
@@ -48,7 +44,7 @@ const PlayerDashboard = () => {
     overflow-x: hidden;
   `;
 
-  const WeaponsContainer = styled.div`
+  const PetsContainer = styled.div`
      display: flex;
      align-items: center;
      justify-content: space-around;
@@ -56,15 +52,7 @@ const PlayerDashboard = () => {
     height: 80vh;
   `;
 
-  const CharactersContainer = styled.div`
-     display: flex;
-     align-items: center;
-     justify-content: space-around;
-    overflow-y: scroll;
-    height: 80vh;
-  `;
-
-  const WeaponDataContainer = styled.div`
+  const PetDataContainer = styled.div`
     background-color: rgb(40, 40, 128);
     box-shadow: rgb(200, 224, 224) 0px 0px 0px 1px, rgb(128, 128, 128) 0px 0px 0px 2px, rgb(40, 48, 48) 0px 0px 0px 3px, rgb(48, 48, 136) 0px 0px 0px 1px inset;
     border-radius: 5px;
@@ -76,23 +64,7 @@ const PlayerDashboard = () => {
     height: fit-content;
   `;
 
-  const CharacterDataContainer = styled.div`
-    background-color: rgb(40, 40, 128);
-    box-shadow: rgb(200, 224, 224) 0px 0px 0px 1px, rgb(128, 128, 128) 0px 0px 0px 2px, rgb(40, 48, 48) 0px 0px 0px 3px, rgb(48, 48, 136) 0px 0px 0px 1px inset;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    margin: 0.5rem;
-    width: fit-content;
-    height: fit-content;
-  `;
-
-  const CharacterDataList = styled.ul`
-    padding: 0.2rem;
-  `;
-
-  const WeaponDataList = styled.ul`
+  const PetDataList = styled.ul`
     display: flex;
     align-items: center;
     justify-content: center;
@@ -100,41 +72,18 @@ const PlayerDashboard = () => {
     padding: 0.2rem;
   `;
 
-  const CharacterDetailItem = styled.li`
-      list-style: inside url(data:image/gif;base64,R0lGODlhEAAQAJEAAAAAAP///////wAAACH5BAEAAAIALAAAAAAQABAAQAIOlI+py+0Po5y02ouzPgUAOw==) square;
-    font-size: 0.85rem;
-    padding: 0.3rem;
-  `;
-
-  const WeaponDetailItem = styled.li`
+  const PetDetailItem = styled.li`
       list-style: inside url(data:image/gif;base64,R0lGODlhEAAQAJEAAAAAAP///////wAAACH5BAEAAAIALAAAAAAQABAAQAIOlI+py+0Po5y02ouzPgUAOw==) square;
     font-size: 0.85rem;
     padding: 0.3rem;
     display: flex;
   `;
 
-  const WeaponButton = styled.button`
-    width: 100%;
-    padding: 0.5rem;
-    border-radius: 8px;
-    border: none;
-    color: white;
-    background-color: red;
-  `;
-  const CharacterButton = styled.button`
-    width: 100%;
-    padding: 0.5rem;
-    border-radius: 8px;
-    border: none;
-    color: white;
-    background-color: blue;
-  `;
-
   return (
     <DashBoardContainer>
       <div className="row">
         <div className="col">
-          <p>Player:chloe.near</p>
+          <p>Player:{context.accountId}</p>
           <p>
             PixelToken:
             {(parseInt(player_balance.pixeltoken) / 1000000).toFixed(
@@ -153,6 +102,39 @@ const PlayerDashboard = () => {
           <p>Legendary Eggs:{player_balance.egg_rare}</p>
         </div>
       </div>
+
+      <div className="row">
+        <div className="col">
+          <p>Matches Won:{matches_won}</p>
+          <p>Matches Lost:{matches_lost}</p>
+        </div>
+        <div className="col">
+          <p>Rating:{rating}</p>
+          <p>Fight Balance:{fight_balance}</p>
+        </div>
+      </div>
+
+      <PetsContainer className="row">
+        {pets_data.map((pet) => {
+          return (
+            <PetDataContainer>
+              <PetDataList>
+                <PetDetailItem>Token ID:{pet.token_id}</PetDetailItem>
+                <PetDetailItem>Pet Type:{pet.pet_type}</PetDetailItem>
+                <PetDetailItem>Train Level:{pet.train_level}</PetDetailItem>
+                <PetDetailItem>Exp:{pet.xp}</PetDetailItem>
+                <PetDetailItem>Level:{pet.level}</PetDetailItem>
+                <PetDetailItem>Power Level:{pet.power_level}</PetDetailItem>
+                <PetDetailItem>Price:{pet.price}</PetDetailItem>
+                <PetDetailItem>Rarity:{pet.rarity}</PetDetailItem>
+                <PetDetailItem>State:{pet.state}</PetDetailItem>
+                <PetDetailItem>State Timer:</PetDetailItem>
+                <PetDetailItem>{pet.state_timer}</PetDetailItem>
+              </PetDataList>
+            </PetDataContainer>
+          );
+        })}
+      </PetsContainer>
     </DashBoardContainer>
   );
 };
