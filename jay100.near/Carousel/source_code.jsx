b1be1ae074e443d826880bb2055d8f05241e0c27
jay100.near <jay100.near>
2023-07-30 20:45:14 +0000

    Update Carousel by jay100.near at 2023-07-30T20:45:14.913Z

--- jay100.near/Carousel/source_code.jsx
+++ jay100.near/Carousel/source_code.jsx
@@ -8,7 +8,26 @@ const Carousel = async () => {
     auto: false,
   });
 
-  const slide_data = props;
+  const slide_data = [
+    {
+      img_url: "https://pd.marmaj.org/pixelparty2.jpg",
+      title: "Who made Pixelparty?",
+      description:
+        "PixelParty was created by a talented team of developers and artists who poured their passion and expertise into bringing this extraordinary NFT Frame showcase to life. The collective efforts of these visionary individuals, fueled by their unwavering dedication and innovative spirit, culminated in the birth of PixelParty, an immersive platform that seamlessly blends technology, art, and creativity to redefine the boundaries of the NFT space.",
+    },
+    {
+      img_url:
+        "https://user-images.githubusercontent.com/93423666/238833011-3538bfcb-082f-41bb-8a12-8670735f6b12.png",
+      description:
+        "Each token you possess grants you the privilege of unleashing your creative prowess on a well designed canvas, carefully measured at 20x20px, affording you the space to weave your artistic vision, imprinting a mesmerizing tapestry of boundless imagination and boundless possibilities.",
+    },
+    {
+      img_url:
+        "https://user-images.githubusercontent.com/93423666/238837072-34abe80a-a9c5-4ade-8b52-5cd359c7d723.png",
+      description:
+        "Should the bounds of a singular 20x20px frame fail to contain your grand vision, fear not, for a world of expansive artistic possibilities beckons. By acquiring multiple interconnected frames, seamlessly merging together in harmonious unity, you transcend the limitations of a confined canvas, creating a vast expanse where your creativity knows no bounds. With each additional frame, your artistic dominion expands, allowing you to fashion a larger, more captivating frame that embodies the magnitude of your imagination, enabling your artistic dreams to unfurl on an awe-inspiring scale that leaves onlookers spellbound in wonderment.",
+    },
+  ];
 
   const CarouselContainer = styled.div`
     @keyframes fade {
@@ -34,11 +53,13 @@ const Carousel = async () => {
 
   const Title = styled.h5`
     color: white;
+    text-align: center;
   `;
 
   const Description = styled.p`
     color: white;
     font-size: 0.8rem;
+    text-align: center;
   `;
 
   const ArrowLeftContainer = styled.div`
