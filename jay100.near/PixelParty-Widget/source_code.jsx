09a064657d7bceb3d9abb3f96f4e0700a877365b
jay100.near <jay100.near>
2023-07-30 19:12:00 +0000

    Update PixelParty-Widget by jay100.near at 2023-07-30T19:12:00.078Z

--- jay100.near/PixelParty-Widget/source_code.jsx
+++ jay100.near/PixelParty-Widget/source_code.jsx
@@ -1,40 +1,226 @@
-const Container = styled.div`
-  background-color: #000;
-  color: #fff;
-`;
-
-const Title = styled.h1`
+const Title = styled.h5`
   color: #fff;
   font-weight: 700;
-  font-size: 2em;
+  font-size: 16px;
   text-align: center;
   text-transform: uppercase;
-  margin-bottom: 20px;
+`;
+
+const Cover = styled.img`
+  border-radius: 5px;
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
 `;
 
 const Description = styled.p`
   color: #fff;
-  font-weight: 300;
-  text-align: center;
-  margin-bottom: 20px;
+  font-weight: 500;
 `;
 
+const FakeButton = styled.a`
+  border-radius: 0px;
+  width: 100%;
+  text-transform: uppercase;
+  padding: 10px 14px;
+  background: rgba(155, 155, 155, 0.2);
+  color: #fff;
+  cursor: pointer;
+  border: 1px solid #000;
+  outline: 0;
+  font-weight: 600;
+  :hover {
+    opacity: 0.8;
+    text-decoration: none;
+    color: #fff;
+  }
+`;
+
+const Card = styled.div`
+  border-radius: 8px;
+  color: #0c0c0c;
+  background: #000;
+  align-items: center;
+  justify-content: center;
+  max-width: 210px;
+  padding: 25px 32px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Hero = styled.div`
+  display: flex;
+  flex-direction: row;
+  border-radius: 15px;
+  text-align: center;
+  justify-content: center;
+  padding: 15px;
+  background-color: #0c0c1f;
+  color: #fff;
+`;
+
+const CardList = styled.div`
+  display: grid;
+  justify-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-rows: repeat(200px, 1fr);
+  gap: 0.5rem;
+`;
+
+const WidgetCard = (props) => {
+  const { title, coverSrc, description, actionButtons } = props;
+
+  const handleButtonClick = (url) => {
+    if (url) {
+      window.open(url, "_blank");
+    }
+  };
+
+  return (
+    <Card>
+      <Title>{title}</Title>
+      <Cover src={coverSrc} alt={title} />
+      <Description>{description}</Description>
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "row wrap",
+          justifyContent: "center",
+        }}
+      >
+        {actionButtons.map((button, index) => (
+          <FakeButton
+            key={index}
+            onClick={() => handleButtonClick(button.url)}
+            href={button.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {button.label}
+          </FakeButton>
+        ))}
+      </div>
+    </Card>
+  );
+};
+
+const WidgetImages = [
+  {
+    title: "Pixel Party Image",
+    url: "https://pd.marmaj.org/pixelparty2.jpg",
+  },
+];
+
+const Mainnet = [
+  { title: "Pixel Party", url: "https://pixelparty.marmaj.org/" },
+];
+
+const Testnet = [
+  { title: "Pixel Party", url: "https://testnet.pixelparty.marmaj.org/" },
+];
+
+const Wiki = [
+  {
+    title: "Pixel Party Wiki",
+    url: "https://github.com/MarmaJFoundation/pixelparty-wiki/wiki",
+  },
+];
+
+const NFT = [
+  {
+    title: "Pixel Party NFT",
+    url: "https://www.mintbase.xyz/contract/mjr.mintbase1.near/token/0",
+  },
+];
+
 return (
-  <Container>
-    <Widget
-      src="jay100.near/widget/CTTDescription"
-      props={{
-        title: "Marma J Gaming",
-        description: `Explore the crypto-based gaming world of Marma J Gaming where you can
-      collaborate artistically with friends, gather your pets for a battle,
-      outfit your hero to explore dungeons and take part in raids, and gather an
-      army to position yourself as the master tactician.`,
+  <div
+    style={{
+      display: "flex",
+      flexFlow: "column",
+      alignItems: "space-evenly",
+      backgroundColor: "#0e0e1e",
+      padding: "20px",
+    }}
+  >
+    <Hero>
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "column",
+          alignItems: "space-evenly",
+          backgroundColor: "#0e0e1e",
+          padding: "20px",
+        }}
+      >
+        <Cover
+          src={WidgetImages[0].url}
+          alt={WidgetImages[0].title}
+          style={{ width: "100%" }}
+        />
+        <h1 style={{ marginBottom: "10px", textAlign: "center" }}>
+          <a
+            href="https://pixelparty.marmaj.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            PIXEL PARTY
+          </a>
+        </h1>
+        <p
+          style={{
+            marginBottom: "20px",
+            fontSize: "1.3em",
+            fontWeight: "bold",
+          }}
+        >
+          NFT FRAME SHOWCASE
+        </p>
+
+        <Description
+          style={{ marginBottom: "20px", width: "100%", textAlign: "justify" }}
+        >
+          PixelParty, a groundbreaking and innovative NFT Frame showcase
+          meticulously constructed on the NEAR Protocol, embodies the fusion of
+          cutting-edge technology and artistic brilliance, boasting a truly
+          remarkable total supply of 600 tokens that serve as a testament to its
+          exceptional rarity and exclusivity.
+        </Description>
+      </div>
+    </Hero>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        marginLeft: "15px",
       }}
-    />
-    <Widget src={"shemar268abel.near/widget/PixelParty"} />
-    <Widget src={"ihyshan.near/widget/MJG-Widget"} />
-    <Widget src={"quan15.near/widget/MJG-Widget"} />
-    <Widget src={"jay100.near/widget/CTT-Widget"} />
-    <Widget />
-  </Container>
+    >
+      <FakeButton
+        href={Mainnet[0].url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        Play{" "}
+      </FakeButton>
+      <FakeButton
+        href={Testnet[0].url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        Test{" "}
+      </FakeButton>
+      <FakeButton href={Wiki[0].url} target="_blank" rel="noopener noreferrer">
+        {" "}
+        Wiki{" "}
+      </FakeButton>
+      <FakeButton href={NFT[0].url} target="_blank" rel="noopener noreferrer">
+        {" "}
+        NFT{" "}
+      </FakeButton>
+    </div>
+  </div>
 );
