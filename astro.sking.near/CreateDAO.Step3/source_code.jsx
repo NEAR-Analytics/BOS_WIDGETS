188f10544fe3697a02f546aaeca8494f91920b4f
astro.sking.near <astro.sking.near>
2023-07-31 01:37:08 +0000

    Update CreateDAO.Step3 by astro.sking.near at 2023-07-31T01:37:08.949Z

--- astro.sking.near/CreateDAO.Step3/source_code.jsx
+++ astro.sking.near/CreateDAO.Step3/source_code.jsx
@@ -1,1 +1,36 @@
-return "3";
+return (
+  <div className="d-flex flex-column gap-4">
+    <div>
+      <h2 className="h5 fw-bold">
+        <span
+          className="rounded-circle d-inline-flex align-items-center justify-content-center fw-bolder h5 me-2"
+          style={{
+            width: "48px",
+            height: "48px",
+            border: "1px solid #82E299",
+          }}
+        >
+          3
+        </span>
+        Cool Down Period
+      </h2>
+      <p className="text-black-50 fw-light small">
+        Setup the period between when a proposal is approved and is executed.
+      </p>
+    </div>
+
+    <Widget
+      src="nui.sking.near/widget/Input.Text"
+      props={{
+        label: "Define Period",
+        placeholder: "Enter days",
+        size: "lg",
+        inputProps: {
+          type: "number",
+          min: 0,
+          max: 3650,
+        },
+      }}
+    />
+  </div>
+);
