8a3501b36d0e36530d55ae05eaa7c26d344888e7
giuliani.near <giuliani.near>
2023-07-31 12:55:05 +0000

    Update TestName by giuliani.near at 2023-07-31T12:55:05.572Z

---
+++ giuliani.near/TestName/source_code.jsx
@@ -0,0 +1,14 @@
+return (
+  <>
+    <br />
+    ComponentCard:
+    <Widget
+      src="near/widget/Onboarding.ComponentCard"
+      props={{ accountId: accountId }}
+    />
+    <br />
+    ComponentCollection:
+    {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
+    <Widget src="near/widget/Onboarding.ComponentCollection" props={{}} />
+  </>
+);
