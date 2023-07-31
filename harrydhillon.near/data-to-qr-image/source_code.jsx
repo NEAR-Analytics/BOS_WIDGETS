042b3f519acee24aee9713cb158f6df7513935b5
harrydhillon.near <harrydhillon.near>
2023-07-31 13:51:08 +0000

    Update data-to-qr-image by harrydhillon.near at 2023-07-31T13:51:08.913Z

---
+++ harrydhillon.near/data-to-qr-image/source_code.jsx
@@ -0,0 +1,25 @@
+State.init({
+  img: null,
+});
+
+const dataToEndcode = async () => {
+  asyncFetch(
+    "https://nodejs-production-9e04.up.railway.app/getbase64image?data=https://www.google.com",
+    {
+      method: "GET",
+    }
+  ).then((res) => {
+    const { qrcodedata } = res.body;
+    console.log(qrcodedata);
+    if (state.img === null) {
+      State.update({ img: qrcodedata });
+    }
+  });
+};
+dataToEndcode();
+
+return (
+  <div>
+    <img style={{ width: 300 }} src={state.img} />
+  </div>
+);
