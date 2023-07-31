157aa76d30662add0fa66f3dc9802714ee7e38e7
giuliani.near <giuliani.near>
2023-07-31 09:16:31 +0000

    Update Onboarding.Starter-fork by giuliani.near at 2023-07-31T09:16:31.660Z

---
+++ giuliani.near/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,14 @@
+return (
+  <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
+    ComponentCard:
+    <Widget
+      src="near/widget/Onboarding.ComponentCard"
+      props={{ accountId: accountId, name: "Onboarding.Starter-fork" }}
+    />
+    <br />
+    ComponentCollection:
+    {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
+    <Widget src="near/widget/Onboarding.ComponentCollection" props={{}} />
+  </>
+);
