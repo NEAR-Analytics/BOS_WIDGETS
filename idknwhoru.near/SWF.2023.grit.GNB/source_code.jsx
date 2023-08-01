f6b526f8cb75fcfed84b5f73fbe786c2ad9a83d7
idknwhoru.near <idknwhoru.near>
2023-08-01 06:13:10 +0000

    Update SWF.2023.grit.GNB by idknwhoru.near at 2023-08-01T06:13:10.497Z

--- idknwhoru.near/SWF.2023.grit.GNB/source_code.jsx
+++ idknwhoru.near/SWF.2023.grit.GNB/source_code.jsx
@@ -1,13 +1,16 @@
 const GNBWrapper = styled.div`
+  width: 100vw;
   background-color: #fff;
+  display: flex;
 `;
 
 return (
   <GNBWrapper>
     <h1>KGRIT</h1>
-    <h2>선수투자</h2>
-    <h2>판타지풋볼</h2>
-    <h2>AI NFT</h2>
+    <ul>
+      <h2>선수투자</h2>
+      <h2>AI NFT</h2>
+    </ul>
     <Web3Connect connectLabel="Connect To Wallet" />
   </GNBWrapper>
 );
