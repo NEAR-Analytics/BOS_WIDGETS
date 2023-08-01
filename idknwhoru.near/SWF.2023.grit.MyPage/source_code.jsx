5f39d54a85a2ce281f4f9e0b917bf8d9f4d0c22f
idknwhoru.near <idknwhoru.near>
2023-08-01 11:38:45 +0000

    Update SWF.2023.grit.MyPage by idknwhoru.near at 2023-08-01T11:38:45.216Z

--- idknwhoru.near/SWF.2023.grit.MyPage/source_code.jsx
+++ idknwhoru.near/SWF.2023.grit.MyPage/source_code.jsx
@@ -1,4 +1,4 @@
-const address = "0xC6A3f8A89136fede4BD4CA36a1864bDA811937c9";
+const address = "0x21Ddc0803447C70b38121766CA99C827aD6f02a8";
 
 const abi = [
   {
@@ -13,6 +13,7 @@ const abi = [
           { type: "address", name: "player", internalType: "address" },
           { type: "string", name: "playerName", internalType: "string" },
           { type: "string", name: "playerKoreanName", internalType: "string" },
+          { type: "string", name: "position", internalType: "string" },
           { type: "uint256", name: "amount", internalType: "uint256" },
           { type: "address", name: "donor", internalType: "address" },
         ],
@@ -49,6 +50,15 @@ if (Ethers.provider()) {
 
 getMyPlayers();
 
+const CardSelectWrapper = styled.div`
+  display: flex;
+`;
+
+const CardWrapper = styled.div`
+  width: 50%;
+  border: 1px solid;
+`;
+
 const Player = () => {
   if (state.players === undefined || state.players.length === 0) {
     return "후원한 선수가 없습니다.";
@@ -56,14 +66,24 @@ const Player = () => {
 
   return (
     <div>
-      <h1>선수 후원금액 목록</h1>
-      {state.players.map((player) => {
-        return (
-          <p>
-            {player[1]} ({player[0]}): {ethers.utils.formatEther(player[3])} CRO
-          </p>
-        );
-      })}
+      <h1>마이페이지</h1>
+      <h2>5명을 선택해 라인업을 구성하세요.</h2>
+      <CardSelectWrapper>
+        {state.players.map((player) => {
+          console.log(player);
+          return (
+            <CardWrapper>
+              <p>
+                {player[1]} ({player[0]}): {ethers.utils.formatEther(player[3])}{" "}
+                CRO
+              </p>
+              <button>상세보기</button>
+            </CardWrapper>
+          );
+        })}
+      </CardSelectWrapper>
+      <button>원오원리그 바로참가하기</button>
+      <div></div>
     </div>
   );
 };
