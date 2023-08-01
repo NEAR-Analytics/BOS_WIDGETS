ea91d9dbbfefa66c25be75814d3011a8c603b380
kenrou-it.near <kenrou-it.near>
2023-08-01 01:20:03 +0000

    Update TestLib by kenrou-it.near at 2023-08-01T01:20:03.225Z

--- kenrou-it.near/TestLib/source_code.jsx
+++ kenrou-it.near/TestLib/source_code.jsx
@@ -1,14 +1,39 @@
-const { stateUpdate, key, functionName } = props;
-console.log(2);
-if (functionName === "get1") {
-  console.log(3);
-  stateUpdate({
-    [key]: 1,
-  });
-} else if (functionName === "getWritersWhitelist") {
-  stateUpdate({
-    [key]: ["kenrou-it.near", "ayelen.near", "martinbarba.near"],
-  });
+const { stateUpdate, libCalls } = props;
+
+// const initLibCalls = [
+//   {
+//     functionName: "get1",
+//     key: "test",
+//     props: {},
+//   },
+//   {
+//     functionName: "getWritersWhitelist",
+//     key: "writersWhitelist",
+//     props: { env: "test" },
+//   },
+// ];
+
+function getWritersWhitelist(env) {
+  if (env === "test") {
+    return ["kenrou-it.near", "ayelen.near", "martinbarba.near"];
+  } else {
+    return ["blaze.near"];
+  }
 }
+
+function libCall(call) {
+  if (call.functionName === "get1") {
+    return 1;
+  } else if (call.functionName === "getWritersWhitelist") {
+    return getWritersWhitelist(call.props.env);
+  }
+}
+
+const updateObj = {};
+libCalls.forEach((call) => {
+  updateObj[call.key] = libCall(call);
+});
+
+stateUpdate(updateObj);
 
 return <></>;
