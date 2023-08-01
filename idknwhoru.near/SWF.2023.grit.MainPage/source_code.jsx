0d9f8b7f914cd5ad9faa8de34b4d6b54ee9809f3
idknwhoru.near <idknwhoru.near>
2023-08-01 06:08:46 +0000

    Update SWF.2023.grit.MainPage by idknwhoru.near at 2023-08-01T06:08:46.692Z

--- idknwhoru.near/SWF.2023.grit.MainPage/source_code.jsx
+++ idknwhoru.near/SWF.2023.grit.MainPage/source_code.jsx
@@ -1,6 +1,12 @@
+const ThemeWrapper = styled.div`
+    background-color: #202123;
+`;
+
 return (
   <>
-    <Widget src={`idknwhoru.near/widget/SWF.2023.grit.GNB`} />
-    <Widget src={`idknwhoru.near/widget/SWF.2023.grit.PlayerList`} />
+    <ThemeWrapper>
+      <Widget src={`idknwhoru.near/widget/SWF.2023.grit.GNB`} />
+      <Widget src={`idknwhoru.near/widget/SWF.2023.grit.PlayerList`} />
+    </ThemeWrapper>
   </>
 );
