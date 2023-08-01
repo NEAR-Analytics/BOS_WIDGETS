cbc3fd02a5ac362d0b4a745f5672b8e78fee4a6f
ihyshan.near <ihyshan.near>
2023-08-01 17:22:17 +0000

    Update NRC.DAO.main by ihyshan.near at 2023-08-01T17:22:17.440Z

--- ihyshan.near/NRC.DAO.main/source_code.jsx
+++ ihyshan.near/NRC.DAO.main/source_code.jsx
@@ -44,7 +44,7 @@ if (props.tab && props.tab !== state.selectedTab) {
 
 const widgetOwner = "ihyshan.near";
 const profile = props.profile ?? Social.getr(`${state.daoId}/profile`);
-const accountUrl = `#/${widgetOwner}/widget/ReFi.DAO.main?daoId=${daoId}&issuer=${issuer}&accountId=${accountId}&role=${role}`;
+const accountUrl = `#/${widgetOwner}/widget/NRC.DAO.main?daoId=${daoId}&issuer=${issuer}&accountId=${accountId}&role=${role}`;
 
 const Wrapper = styled.div`
   padding-bottom: 48px;
