900c3b8ffc9b4847b36aa73bbf9e1f76cf5136c6
andyh.near <andyh.near>
2023-08-01 23:11:54 +0000

    Update CallbackRenderChild by andyh.near at 2023-08-01T23:11:54.088Z

--- andyh.near/CallbackRenderChild/source_code.jsx
+++ andyh.near/CallbackRenderChild/source_code.jsx
@@ -1,13 +1,10 @@
-console.log("[NSCOMP:CallbackRenderChild]");
-console.log("----- awaiting");
+console.log({ props });
 const component = await props.renderComponent("i am the child", () =>
   console.log("all done")
 );
-console.log("----- awaited");
 return (
   <div>
     something really cool is loading:
     {component}
   </div>
 );
-// return <div>i am the child</div>;
