87093bc1fe9a1b41121d01d92e2453405108b946
idknwhoru.near <idknwhoru.near>
2023-08-01 04:44:59 +0000

    Update SWF.2023.grit.PlayerList by idknwhoru.near at 2023-08-01T04:44:59.160Z

--- idknwhoru.near/SWF.2023.grit.PlayerList/source_code.jsx
+++ idknwhoru.near/SWF.2023.grit.PlayerList/source_code.jsx
@@ -1,5 +1,16 @@
 const cronosContractAddress = "0xC6A3f8A89136fede4BD4CA36a1864bDA811937c9";
 
+const images = [
+  "https://ipfs.near.social/ipfs/bafkreifdek56fab4d55vun6to66dzizhy76ly65gdy3eo6lv5vnzbxgrty",
+  "https://ipfs.near.social/ipfs/bafkreibslifqs6pz37vy2mrrlmogtatlg5s2d5fc4bfiv4rzcgzubgekeu",
+  "https://ipfs.near.social/ipfs/bafkreibshwzbeoxguy5mot6nilssix3ilmpt6w5xv6bazvt2edbnaa5spe",
+  "https://ipfs.near.social/ipfs/bafkreic5tco2sgvxdacogftgk3rlsjohb3ptqomr4o5vwtkrzvlfajnl7y",
+  "https://ipfs.near.social/ipfs/bafkreigpfirgs42t4rfbbixixm4w5ukqxsbflvvn2nxl7zdktalncxqh2a",
+  "https://ipfs.near.social/ipfs/bafkreias744aadzv7ivxuyoqtzwbbd6lawph5hewidapwwgyj5rnt73iri",
+  "https://ipfs.near.social/ipfs/bafkreicl2wdomm22rlz2h3gt62uilbharqgh4gmfbx7j7mbnbrkkfxq564",
+  "https://ipfs.near.social/ipfs/bafkreiew5g7eizlh6ms7al5y23g7juioejle2bhv3cbyqoq55upxt2dlyu",
+];
+
 const cronosContractABI = [
   {
     type: "function",
@@ -60,8 +71,8 @@ if (state.players === undefined) {
 console.log(state.players);
 
 const Card = styled.div`
-    width: 150px;
-    height: 200px;
+    width: 25%;
+    height: 100%;
     border: solid 1px #bbb;
     cursor: pointer;
 `;
@@ -69,9 +80,21 @@ const Card = styled.div`
 const Cards = () => {
   if (state.players !== undefined) {
     return (
-      <div style={{ display: "flex" }}>
-        {state.players.map((player) => {
-          return <Card>{player[1]}</Card>;
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {state.players.map((player, idx) => {
+          if (idx > 7) {
+            return null;
+          }
+          return (
+            <Card>
+              <img src={images[idx]} width={"100%"}></img>
+              <h1>{player[1]}</h1>
+              <h2>{ethers.utils.formatEther(player[6])} CRO</h2>
+              <h3>{player[5]}</h3>
+              <h3>{player[4]}</h3>
+              <h3>{player[3]}</h3>
+            </Card>
+          );
         })}
       </div>
     );
