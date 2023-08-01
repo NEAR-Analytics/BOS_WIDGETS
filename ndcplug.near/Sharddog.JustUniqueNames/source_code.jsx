b84f57c486524766e3ac60fbdf0817c8a00fe27a
ndcplug.near <ndcplug.near>
2023-08-01 18:12:30 +0000

    Update Sharddog.JustUniqueNames by ndcplug.near at 2023-08-01T18:12:30.820Z

---
+++ ndcplug.near/Sharddog.JustUniqueNames/source_code.jsx
@@ -0,0 +1,147 @@
+const limit = 20;
+const series = props.series ?? "167"; // add series filter
+const title = props.title ?? "Total Holders";
+const showHeader = props.showHeader ?? true;
+const showImage = props.showImage ?? true;
+// add what nft they have and then add filter of unique people, add link to collection
+// add condition if no props fetch all collection
+State.init({
+  offset: 0,
+  tokens: [],
+  hasMore: true,
+});
+
+function fetchTokens() {
+  asyncFetch("https://graph.mintbase.xyz/mainnet", {
+    method: "POST",
+    headers: {
+      "mb-api-key": "omni-site",
+      "Content-Type": "application/json",
+      "x-hasura-role": "anonymous",
+    },
+    body: JSON.stringify({
+      query: `
+          query MyQuery {
+            mb_views_nft_tokens(
+
+              where: { nft_contract_id: { _eq: "mint.sharddog.near" } token_id: {_regex: "^${series}:"}}
+              order_by: {minted_timestamp: desc}
+            ) {
+              media
+              owner
+            }
+          }
+        `,
+    }),
+  }).then((res) => {
+    if (res.ok) {
+      const tokens = res.body.data.mb_views_nft_tokens;
+      if (tokens.length > 0) {
+        State.update({
+          tokens: [...state.tokens, ...tokens],
+          offset: state.offset + limit,
+          hasMore: true,
+        });
+      } else {
+        State.update({
+          hasMore: false,
+        });
+      }
+    }
+  });
+}
+
+function Sharddog({ owner, media }) {
+  const size = "100px";
+
+  return (
+    <div className="row">
+      <div className="col-sm-3">
+        {showImage && (
+          <Widget
+            src="mob.near/widget/Image"
+            props={{
+              image: {
+                url: media,
+              },
+              style: {
+                width: size,
+                height: size,
+                objectFit: "cover",
+                minWidth: size,
+                minHeight: size,
+                maxWidth: size,
+                maxHeight: size,
+                overflowWrap: "break-word",
+              },
+            }}
+          />
+        )}
+      </div>
+      <div className="col-sm-12">
+        <Widget
+          src="near/widget/AccountProfileCard"
+          props={{
+            accountId: owner,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+const size = "144px";
+
+const Grid = styled.div`
+  display: row;
+`;
+
+const loader = (
+  <div className="loader" key={"loader"}>
+    <span
+      className="spinner-grow spinner-grow-sm me-1"
+      role="status"
+      aria-hidden="true"
+    />
+    Loading ...
+  </div>
+);
+const uniqueName = (
+  <div className="loader" key={"loader"}>
+    <span
+      className="spinner-grow spinner-grow-sm me-1"
+      role="status"
+      aria-hidden="true"
+    />
+    Loading ...
+  </div>
+);
+
+const owners = new Set();
+// state.tokens?.map((it) => {
+//   owners.add(it.owner);
+// });
+
+return (
+  <>
+    {showHeader && (
+      <h1>
+        {title}: {state.tokens.length}
+      </h1>
+    )}
+
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={fetchTokens}
+      hasMore={state.hasMore}
+      loader={loader}
+    >
+      <Grid>
+        {state.tokens?.map((it) => {
+          owners.add(it.owner);
+          return <p>{it.owner}</p>;
+        })}
+      </Grid>
+    </InfiniteScroll>
+  </>
+);
