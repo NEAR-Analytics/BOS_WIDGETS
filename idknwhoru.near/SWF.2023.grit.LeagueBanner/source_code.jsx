b9b37047e29a5d80fe50d72bdf0a69dbd0695bb0
idknwhoru.near <idknwhoru.near>
2023-08-01 13:43:07 +0000

    Update SWF.2023.grit.LeagueBanner by idknwhoru.near at 2023-08-01T13:43:07.360Z

---
+++ idknwhoru.near/SWF.2023.grit.LeagueBanner/source_code.jsx
@@ -0,0 +1,22 @@
+return (
+  <>
+    <div>
+      <img
+        src={`https://ipfs.near.social/ipfs/bafkreidcivpo3eh3m2voreinoq2adx2yutndmgldzn4c6mmxqyfxhxzcda`}
+      />
+      <div>
+        <h1>
+          1등 우승: 500만원 상당의 상품권, 2등 우승: 300만원 상당의 상품권
+        </h1>
+        <h2>참가자들: 1311명</h2>
+        <h3>우승 추첨: 2023년 8월 14일 21:00시</h3>
+        <button>참가하기</button>
+      </div>
+      <div>
+        <img
+          src={`https://ipfs.near.social/ipfs/bafkreifxtf2zrapxri4vlbkxo2qjbkyxnafqr3cri2ceurj6erqueqhna4`}
+        />
+      </div>
+    </div>
+  </>
+);
