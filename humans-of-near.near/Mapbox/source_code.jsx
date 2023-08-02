516631e9062d6657b1104aeff3f5ebd8a15df439
humans-of-near.near <humans-of-near.near>
2023-08-02 15:26:56 +0000

    Update Mapbox by humans-of-near.near at 2023-08-02T15:26:56.790Z

--- humans-of-near.near/Mapbox/source_code.jsx
+++ humans-of-near.near/Mapbox/source_code.jsx
@@ -1,201 +1,65 @@
-const Owner = "humans-of-near.near";
-const accountId = context.accountId;
+const ACCESS_TOKEN =
+  props.accessToken ||
+  "pk.eyJ1IjoiZWpsYnJhZW0iLCJhIjoiY2xrbmIwaW53MGE0NTNtbGsydWd2MmpyZSJ9.m1ZfEqv2fGet2zblGknT8A";
+const styleUrl = props.styleUrl || "mapbox://styles/mapbox/streets-v12"; // see https://docs.mapbox.com/api/maps/styles/#mapbox-styles
+const center = props.center || [-87.6298, 41.8781]; // starting position [lng, lat]
+const zoom = props.zoom || 9; // starting zoom
+const markers = props.markers || []; // map markers, array of objects that look like: { lng: -87.641, lat: 41.874 }
 
-state = State.init({
-  opened: false,
-  url: "",
-});
+const code = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
+    
+    <link href="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css" rel="stylesheet">
+    
+    <script src="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.js"></script>
+    
+    <style>
+      body { margin: 0; padding: 0; }
+      #map { position: absolute; top: 0; bottom: 0; width: 100%; }
+    </style>
+  </head>
+  <body>
+
+    <div id="map"></div>
+
+    <script>
+
+    mapboxgl.accessToken = "${ACCESS_TOKEN}";
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: '${styleUrl}',
+        center: [${center[0]}, ${center[1]}], 
+        zoom: ${zoom}
+    });
+
+    // Function to populate markers to the map
+    function populateMarkers() {
+        const markersData = ${JSON.stringify(markers)};
+        markersData.forEach(marker => {
+        new mapboxgl.Marker()
+            .setLngLat([marker.lng, marker.lat])
+            .addTo(map);
+        });
+    }
+
+    populateMarkers();
+    
+    </script>
+  </body>
+</html>
+  `;
 
 const Container = styled.div`
-height: 100%;
-display: flex;
-
-button,
-fieldset,
-input {
-  all: unset;
-}
-
-.DialogOverlay {
-  background-color: var(--blackA9);
-  position: fixed;
-  inset: 0;
-  animation: overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
-}
-
-.DialogContent {
-  background-color: white;
-  border-radius: 6px;
-  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 90vw;
-  max-width: 450px;
-  max-height: 85vh;
-  padding: 25px;
-  animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
-}
-.DialogContent:focus {
-  outline: none;
-}
-
-.DialogTitle {
-  margin: 0;
-  font-weight: 500;
-  color: var(--mauve12);
-  font-size: 17px;
-}
-
-.DialogDescription {
-  margin: 10px 0 20px;
-  color: var(--mauve11);
-  font-size: 15px;
-  line-height: 1.5;
-}
-
-.Button {
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 4px;
-  padding: 0 15px;
-  font-size: 15px;
-  line-height: 1;
-  font-weight: 500;
-  height: 35px;
-}
-.Button.violet {
-  background-color: white;
-  color: var(--violet11);
-  box-shadow: 0 2px 10px var(--blackA7);
-}
-.Button.violet:hover {
-  background-color: var(--mauve3);
-}
-.Button.violet:focus {
-  box-shadow: 0 0 0 2px black;
-}
-.Button.green {
-  background-color: var(--green4);
-  color: var(--green11);
-}
-.Button.green:hover {
-  background-color: var(--green5);
-}
-.Button.green:focus {
-  box-shadow: 0 0 0 2px var(--green7);
-}
-
-.IconButton {
-  font-family: inherit;
-  border-radius: 100%;
-  height: 25px;
-  width: 25px;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  color: var(--violet11);
-  position: absolute;
-  top: 10px;
-  right: 10px;
-}
-.IconButton:hover {
-  background-color: var(--violet4);
-}
-.IconButton:focus {
-  box-shadow: 0 0 0 2px var(--violet7);
-}
-
-.Fieldset {
-  display: flex;
-  gap: 20px;
-  align-items: center;
-  margin-bottom: 15px;
-}
-
-.Label {
-  font-size: 15px;
-  color: var(--violet11);
-  width: 90px;
-  text-align: right;
-}
-
-.Input {
-  width: 100%;
-  flex: 1;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 4px;
-  padding: 0 10px;
-  font-size: 15px;
-  line-height: 1;
-  color: var(--violet11);
-  box-shadow: 0 0 0 1px var(--violet7);
-  height: 35px;
-}
-.Input:focus {
-  box-shadow: 0 0 0 2px var(--violet8);
-}
-
-@keyframes overlayShow {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-@keyframes contentShow {
-  from {
-    opacity: 0;
-    transform: translate(-50%, -48%) scale(0.96);
-  }
-  to {
-    opacity: 1;
-    transform: translate(-50%, -50%) scale(1);
-  }
-}
+    height: 100vh;
 `;
 
 return (
   <Container>
-    <Dialog.Root open={state.opened}>
-      <Dialog.Overlay className="DialogOverlay" />
-      <Dialog.Content className="DialogContent">
-        <Dialog.Title className="DialogTitle">Redirect</Dialog.Title>
-        <Dialog.Description className="DialogDescription">
-          Click{" "}
-          <a href={state.url} target="_blank">
-            {" "}
-            here{" "}
-          </a>{" "}
-          to open the link in new tab.
-        </Dialog.Description>
-        <Dialog.Close asChild>
-          <button
-            className="IconButton"
-            aria-label="Close"
-            onClick={() => {
-              State.update({ opened: false });
-            }}
-          >
-            X
-          </button>
-        </Dialog.Close>
-      </Dialog.Content>
-    </Dialog.Root>
-    <Widget
-      src={`${Owner}/widget/Map-Iframe`}
-      props={{
-        ...props,
-        onLink: (response) => {
-          State.update({ opened: true, url: response });
-        },
-      }}
-    />
+    <iframe className="w-100 h-100" srcDoc={code} />
   </Container>
 );
