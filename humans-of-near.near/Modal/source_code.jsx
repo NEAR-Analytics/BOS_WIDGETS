812ed110565ee037fb75a5dfd20e73e9ad00c8b1
humans-of-near.near <humans-of-near.near>
2023-08-02 13:23:39 +0000

    Update Modal by humans-of-near.near at 2023-08-02T13:23:39.366Z

--- humans-of-near.near/Modal/source_code.jsx
+++ humans-of-near.near/Modal/source_code.jsx
@@ -1,13 +1,9 @@
 const ModalOverlay = styled.div`
   position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+  left: "58px",
+  top: "150px",
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
-  align-items: center;
-  justify-content: center;
   z-index: 100;
 `;
 
