f40ed9cdb699170a6f1823f109fa21cb5ace3618
nearpavel.near <nearpavel.near>
2023-08-02 02:20:21 +0000

    Update Analytics.ActiveDevelopers by nearpavel.near at 2023-08-02T02:20:21.868Z

---
+++ nearpavel.near/Analytics.ActiveDevelopers/source_code.jsx
@@ -0,0 +1,82 @@
+const fetchResult = fetch(
+  "https://storage.googleapis.com/databricks-near-query-runner/output/near_bos_monthly_active_developers_ec.json"
+);
+if (!fetchResult.ok) {
+  return "failed to fetch data";
+}
+const data = JSON.parse(fetchResult.body).sort(
+  (a, b) => a.collected_for_day - b.collected_for_day
+);
+
+const option = {
+  title: {
+    text: "NEAR BOS Active Developers",
+    subtext: "subtext",
+  },
+  tooltip: {
+    trigger: "axis",
+  },
+  legend: {
+    data: [
+      "full_time_devs",
+      "part_time_devs",
+      "one_time_devs",
+      "monthly_active_devs",
+    ],
+    top: "50",
+  },
+  grid: {
+    left: "3%",
+    right: "4%",
+    bottom: "3%",
+    top: "100",
+    containLabel: true,
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {},
+    },
+  },
+  xAxis: {
+    type: "category",
+    boundaryGap: false,
+    data: data.map((r) => new Date(r.collected_for_day).toLocaleDateString()),
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      name: "full_time_devs",
+      type: "line",
+      stack: "Total",
+      data: data.map((r) => r.full_time_devs),
+    },
+    {
+      name: "part_time_devs",
+      type: "line",
+      stack: "Total",
+      data: data.map((r) => r.part_time_devs),
+    },
+    {
+      name: "one_time_devs",
+      type: "line",
+      stack: "Total",
+      data: data.map((r) => r.one_time_devs),
+    },
+    {
+      name: "monthly_active_devs",
+      type: "line",
+      data: data.map((r) => r.monthly_active_devs),
+    },
+  ],
+};
+
+return (
+  <div>
+    <Widget
+      src={`nearpavel.near/widget/EChart`}
+      props={{ definition: option }}
+    />
+  </div>
+);
