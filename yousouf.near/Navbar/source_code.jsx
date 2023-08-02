476a0cec7706af2a97e56e3e029218de0d513926
yousouf.near <yousouf.near>
2023-08-02 08:31:48 +0000

    Update Navbar by yousouf.near at 2023-08-02T08:31:48.123Z

--- yousouf.near/Navbar/source_code.jsx
+++ yousouf.near/Navbar/source_code.jsx
@@ -1,14 +1,35 @@
 State.init({
-  view: "type",
+  view: "",
 });
 
 const Button = styled.button``;
 
 return (
   <div>
-    <Button onClick={() => State.update({ view: "thing" })}>thing</Button>
-    <Button onClick={() => State.update({ view: "type" })}>type</Button>
-    <Button onClick={() => State.update({ view: "widget" })}>widget</Button>
-    <h5>{`every ${state.view}`}</h5>
+    <Button onClick={() => State.update({ view: "thing will change" })}>
+      Team
+    </Button>
+
+    <Button
+      onClick={() =>
+        State.update({
+          view: "Goals and Objectives    ====>  1 - Education and awareness 2 - Networking and Connection  3 - Socials Club 4 - Community based Projects 5 - Inclusivity and Collaboration 6 - Bridge Language Barrier 7 - Knowledge Sharing ",
+        })
+      }
+    >
+      Our Mission
+    </Button>
+
+    <Button
+      onClick={() =>
+        State.update({
+          view: "The northern part of Nigeria is home to 60–75% of the Nigerian population, 90% of whom are Hausa speakers, with around 3-4 million active blockchain/crypto users and native influencers, content creators, and developers. The region has had a well established Web2 tech community for more than a decade and is ranked as one of the regions with the highest tech specialists in the country (Nigeria), a community with a population of more than 150 million people, yet they don't have a better idea or knowledge of the Near Protocol blockchain, or Web3, and its impact within our region. The Near Hausa Community (NHC) has set clear goals and objectives to promote education, awareness, and adoption of the Near Protocol blockchain and Web3 technology within the Hausa-speaking community in the northern part of Nigeria.",
+        })
+      }
+    >
+      {" "}
+      About Us
+    </Button>
+    <h5>{state.view}</h5>
   </div>
 );
