78e27bd8df06d73e16985abb2905e42a7cacb381
guessme.near <guessme.near>
2023-08-02 02:58:17 +0000

    Update ZKEVM.GAMMA by guessme.near at 2023-08-02T02:58:17.722Z

--- guessme.near/ZKEVM.GAMMA/source_code.jsx
+++ guessme.near/ZKEVM.GAMMA/source_code.jsx
@@ -1,12 +1,51 @@
-const ALL_DATA_URL = "https://wire2.gamma.xyz/quickswap/polygon-zkevm/hypervisors/allData";
+State.init({
+  activePair: defaultPair,
+  isPoolFetching: false,
+  isUserPositionsFetching: false,
+  chainId: '',
+});
 
-const USER_DATA_BASE = "https://wire2.gamma.xyz/quickswap/polygon-zkevm/user/";
+const sender = Ethers.send("eth_requestAccounts", [])[0];
+const ContainerLogin = styled.div`
+  .web3-connect{
+    width:480px;
+    height:60px;
+    border-radius:10px;
+    background-color:#fff;
+    color:#0F1126;
+    font-size:18px;
+    font-weight:500;
+    border:none;
+    margin-top:20px;
+  }
+`
+if (!sender)
+  return (
+    <ContainerLogin
+      style={{
+        display: "flex",
+        maxWidth: "500px",
+        flexDirection: "column",
+        margin: "80px auto auto auto",
+      }}
+    >
+      <img src="https://ipfs.near.social/ipfs/bafkreibmhq4fseqpcbywiq4hfojghxvhj47mjsri2trggt7d5h5od4y6kq"></img>
+      
+      <Web3Connect className="web3-connect" connectLabel="Connect ETH wallet" />
+    </ContainerLogin>
+  );
 
+// const { chainId } = Ethers.getNetwork();
+Ethers.provider().getNetwork().then((data) => {
+  State.update({
+    chainId: data.chainId
+  })
+})
+const chainIdToSwitch = "0x44D";
 
-const MAINNET_CHAIN_ID = 1101;
-
-
-const CHAIN_CONFIG = {
+const switchChain = () => {
+  const zkevmChain = {
+    chainId: "0x44D",
     chainName: "Polygon zkEVM",
     nativeCurrency: {
       name: "Ethereum",
@@ -15,158 +54,93 @@ const CHAIN_CONFIG = {
     },
     rpcUrls: ["https://zkevm-rpc.com"],
     blockExplorerUrls: ["https://zkevm.polygonscan.com"],
+  };
+
+  Ethers.send("wallet_addEthereumChain", [zkevmChain]);
 };
 
-const addresses = {
-  Chef: "0x1e2d8f84605d32a2cbf302e30bfd2387badf35dd",
-  DAI: "0xc5015b9d9161dca7e18e32f6f25c4ad850731fd4",
-  MATIC: "0xa2036f0538221a77a3937f1379699f44945018d0",
-  "N MATIC-USDC": "0x19f4ebc0a1744b93a355c2320899276ae7f79ee5",
-  "N USDC-WBTC": "0x9783c45564232c0aff8dc550a9c247c42e8c8b98",
-  "N WETH-MATIC": "0x2f39293c9ed046822c014143fb18d5ae0479be93",
-  "N WETH-USDC": "0x04c6b11e1ffe1f1032bd62adb343c9d07767489c",
-  "N WETH-WBTC": "0x1cc4ee0cb063e9db36e51f5d67218ff1f8dbfa0f",
-  USDC: "0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035",
-  "USDC-DAI": "0xafad6e114cfbc8a19e91b8d7d04da740a7698595",
-  USDT: "0x1e4a5963abfd975d8c9021ce480b42188849d41d",
-  "USDT-DAI": "0xcd36b8a47a072e3e05e894b6ec89d294bec3d3ed",
-  "USDT-USDC": "0x145d55ae4848f9782efcac785a655e3e5dce1bcd",
-  "W MATIC-USDC": "0x8462e4173d63f8769f94bf7ae5bc1ac7ab5d96e3",
-  "W USDC-WBTC": "0x83de646a7125ac04950fea7e322481d4be66c71d",
-  "W WETH-MATIC": "0x5ada298913d53aa823824de69b4a6e790aed9327",
-  "W WETH-USDC": "0xfb3a24c0f289e695ceb87b32fc18a2b8bd896167",
-  "W WETH-WBTC": "0x64e78e990b2a45fad8b64b43e62a67d69a156042",
-  WBTC: "0xea034fb02eb1808c2cc3adbc15f447b93cbe08e1",
-  WETH: "0x4f9a0e7fd2bf6067db6994cf12e4495df938e6e9",
-  stMATIC: "0x83b874c1e09d316059d929da402dcb1a98e92082",
-  "stMATIC-MATIC": "0x9616052273a598bc04bd1ad7f7a753157c24f77e",
+const SwitchWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Inter';
+  max-width: 500px;
+  color: #fff;
+  margin: auto;
+  text-align: center;
+  border-radius: 12px;
+  padding: 16px 0;
+  align-items: center;
+  gap: 12px;
+  margin-top:80px;
+  button {
+    background: #8247E5;
+    border: none;
+    &:hover {
+      background: #8257F5;
+    }
+  }
+`;
+
+if (state.chainId !== 1101) {
+  return (
+    <SwitchWrapper>
+      <img src="https://ipfs.near.social/ipfs/bafkreibmhq4fseqpcbywiq4hfojghxvhj47mjsri2trggt7d5h5od4y6kq"></img>
+      <h4>Please switch to Polygon zkEVM</h4>
+      <button onClick={switchChain}>Switch to Polygon zkEVM</button>
+      <p>**Please refresh once after switch chain**</p>
+    </SwitchWrapper>
+  );
+}
+
+const fetchPoolsData = () => {
+  State.update({
+    isPoolFetching: true,
+  });
+  asyncFetch(
+    "https://wire2.gamma.xyz/quickswap/polygon-zkevm/hypervisors/allData"
+  ).then((res) => {
+    if (!res.ok) return;
+
+    State.update({
+      poolsData: res.body,
+      isPoolFetching: false,
+    });
+  });
 };
 
-const pairs = [
-  {
-    id: "W WETH-USDC",
-    strategy: "Dynamic",
-    strategy2: "Wide",
-    token0: "WETH",
-    token1: "USDC",
-    decimals0: 18,
-    decimals1: 6,
-  },
-  {
-    id: "N WETH-USDC",
-    strategy: "Dynamic",
-    strategy2: "Narrow",
-    token0: "WETH",
-    token1: "USDC",
-    decimals0: 18,
-    decimals1: 6,
-  },
-  {
-    id: "W WETH-MATIC",
-    strategy: "Dynamic",
-    strategy2: "Wide",
-    token0: "WETH",
-    token1: "MATIC",
-    decimals0: 18,
-    decimals1: 18,
-  },
-  {
-    id: "N WETH-MATIC",
-    strategy: "Dynamic",
-    strategy2: "Narrow",
-    token0: "WETH",
-    token1: "MATIC",
-    decimals0: 18,
-    decimals1: 18,
-  },
-  {
-    id: "W WETH-WBTC",
-    strategy: "Dynamic",
-    strategy2: "Wide",
-    token0: "WETH",
-    token1: "WBTC",
-    decimals0: 18,
-    decimals1: 18,
-  },
-  {
-    id: "N WETH-WBTC",
-    strategy: "Dynamic",
-    strategy2: "Narrow",
-    token0: "WETH",
-    token1: "WBTC",
-    decimals0: 18,
-    decimals1: 18,
-  },
-  {
-    id: "W USDC-WBTC",
-    strategy: "Dynamic",
-    strategy2: "Wide",
-    token0: "USDC",
-    token1: "WBTC",
-    decimals0: 6,
-    decimals1: 18,
-  },
-  {
-    id: "N USDC-WBTC",
-    strategy: "Dynamic",
-    strategy2: "Narrow",
-    token0: "USDC",
-    token1: "WBTC",
-    decimals0: 6,
-    decimals1: 18,
-  },
-  {
-    id: "USDT-USDC",
-    strategy: "Stable",
-    token0: "USDT",
-    token1: "USDC",
-    decimals0: 6,
-    decimals1: 6,
-  },
-  {
-    id: "USDC-DAI",
-    strategy: "Stable",
-    token0: "USDC",
-    token1: "DAI",
-    decimals0: 6,
-    decimals1: 18,
-  },
-  {
-    id: "USDT-DAI",
-    strategy: "Stable",
-    token0: "USDT",
-    token1: "DAI",
-    decimals0: 6,
-    decimals1: 18,
-  },
-  {
-    id: "stMATIC-MATIC",
-    strategy: "Pegged Price",
-    token0: "stMATIC",
-    token1: "MATIC",
-    decimals0: 18,
-    decimals1: 18,
-  },
-  {
-    id: "N MATIC-USDC",
-    strategy: "Dynamic",
-    strategy2: "Narrow",
-    token0: "MATIC",
-    token1: "USDC",
-    decimals0: 18,
-    decimals1: 6,
-  },
-  {
-    id: "W MATIC-USDC",
-    strategy: "Dynamic",
-    strategy2: "Wide",
-    token0: "MATIC",
-    token1: "USDC",
-    decimals0: 18,
-    decimals1: 6,
-  },
-];
-const proxyAddress = "0x8480199e5d711399abb4d51bda329e064c89ad77";
+const fetchUserData = () => {
+  State.update({
+    isUserPositionsFetching: true,
+  });
+  asyncFetch(
+    `https://wire2.gamma.xyz/quickswap/polygon-zkevm/user/${sender}`
+  ).then((res) => {
+    if (!res.ok) return;
+
+    State.update({
+      userPositions: res.body[sender],
+      isUserPositionsFetching: false,
+    });
+  });
+};
+
+if (state.poolsData === undefined) {
+  fetchPoolsData();
+}
+
+if (sender && state.userPositions === undefined) {
+  fetchUserData();
+}
+
+const refetch = () => {
+  fetchPoolsData();
+  fetchUserData();
+  console.log("refetching");
+};
+
+const postRefetch = () => {
+  setTimeout(() => refetch(), 45_000);
+};
 
 const defaultPair = {
   id: "N WETH-USDC",
@@ -176,18 +150,79 @@ const defaultPair = {
   decimals1: 6,
 };
 
+const Wrapper = styled.div`
+    display:flex;
+    align-items: start;
+    gap: 24px;
+`;
+const VStack = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  .tableTitle{
+    font-size:18px;
+    color:#7C7F96;
+    font-weight:500;
+    padding-left:16px;
+    margin-bottom:5px;
+  }
+`;
+const Button = styled.button`
+    background: #1d1e1f;
+    border-radius: 4px;
+    border: none;
+    max-width: 640px;
+    width: 100%;
+    color: #fff;
+    padding: 8px 0;
+    font-weight: 600;
+    font-size: 14px;
+    position: relative;
+    min-height: 37px;
+    &:hover {
+      background: #2b2a2b;
+    }
+    &:disabled {
+      background: #333;
+      color: #ccc;
+    }
+`;
+
+const handlePairClick = (pair) => {
+  State.update({
+    activePair: pair,
+  });
+};
+
+const {
+  activePair,
+  poolsData,
+  userPositions,
+  isPoolFetching,
+  isUserPositionsFetching,
+} = state;
+
 return (
-  <Widget
-    src="bluebiu.near/widget/Linea.Liquidity.GAMMA.GAMMAConnector"
-    props={{
-      addresses,
-      pairs,
-      proxyAddress,
-      defaultPair,
-      CHAIN_CONFIG,
-      ALL_DATA_URL,
-      USER_DATA_BASE,
-      MAINNET_CHAIN_ID,
-    }}
-  ></Widget>
+  <VStack>
+    {/* <Button
+      disabled={isPoolFetching || isUserPositionsFetching}
+      onClick={refetch}
+    >
+      {isPoolFetching || isUserPositionsFetching
+        ? "Fetching Table Data..."
+        : "Refresh"}
+    </Button> */}
+    <div className="tableTitle">Active Liquidity</div>
+    <Wrapper>
+      <Widget
+        src="guessme.near/widget/ZKEVM.gamma-zkevm-table"
+        props={{ handlePairClick, poolsData, userPositions }}
+      />
+
+      <Widget
+        src="guessme.near/widget/ZKEVM.gamma-zkevm-vault"
+        props={{ pair: activePair, refetch: postRefetch }}
+      />
+    </Wrapper>
+  </VStack>
 );
