3f4387cbd8196dac3d93c622f250a47274ab1abb
astro.sking.near <astro.sking.near>
2023-08-02 05:41:40 +0000

    Update CreateDAO.Step3 by astro.sking.near at 2023-08-02T05:41:40.053Z

--- astro.sking.near/CreateDAO.Step3/source_code.jsx
+++ astro.sking.near/CreateDAO.Step3/source_code.jsx
@@ -1,36 +1,62 @@
+const { formState, errors, renderFooter } = props;
+
+const initialAnswers = {
+  coolDownPeriod: formState.coolDownPeriod,
+};
+
+State.init({
+  answers: initialAnswers,
+});
+
+const update = (key, value) =>
+  State.update({
+    answers: {
+      ...state.answers,
+      [key]: value,
+    },
+  });
+
 return (
-  <div className="d-flex flex-column gap-4">
-    <div>
-      <h2 className="h5 fw-bold">
-        <span
-          className="rounded-circle d-inline-flex align-items-center justify-content-center fw-bolder h5 me-2"
-          style={{
-            width: "48px",
-            height: "48px",
-            border: "1px solid #82E299",
-          }}
-        >
-          3
-        </span>
-        Cool Down Period
-      </h2>
-      <p className="text-black-50 fw-light small">
-        Setup the period between when a proposal is approved and is executed.
-      </p>
+  <div className="mt-4 ndc-card p-4">
+    <div className="d-flex flex-column gap-4">
+      <div>
+        <h2 className="h5 fw-bold">
+          <span
+            className="rounded-circle d-inline-flex align-items-center justify-content-center fw-bolder h5 me-2"
+            style={{
+              width: "48px",
+              height: "48px",
+              border: "1px solid #82E299",
+            }}
+          >
+            3
+          </span>
+          Cool Down Period
+        </h2>
+        <p className="text-black-50 fw-light small">
+          Setup the period between when a proposal is approved and is executed.
+        </p>
+      </div>
+
+      <Widget
+        src="nui.sking.near/widget/Input.ExperimentalText"
+        props={{
+          label: "Define Period",
+          placeholder: "Enter days",
+          size: "lg",
+          inputProps: {
+            type: "number",
+            min: 0,
+            max: 3650,
+            name: "coolDownPeriod",
+            defaultValue: state.answers.coolDownPeriod,
+          },
+          error: errors["coolDownPeriod"],
+          onChange: (v) => update("coolDownPeriod", parseInt(v)),
+        }}
+      />
     </div>
 
-    <Widget
-      src="nui.sking.near/widget/Input.Text"
-      props={{
-        label: "Define Period",
-        placeholder: "Enter days",
-        size: "lg",
-        inputProps: {
-          type: "number",
-          min: 0,
-          max: 3650,
-        },
-      }}
-    />
+    {renderFooter(state.answers)}
   </div>
 );
