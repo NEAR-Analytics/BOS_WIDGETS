020b4ce6d7703a5d5081132fcca3f8f857973d61
harrydhillon.near <harrydhillon.near>
2023-08-03 16:49:51 +0000

    Update data-to-qr-image by harrydhillon.near at 2023-08-03T16:49:51.754Z

--- harrydhillon.near/data-to-qr-image/source_code.jsx
+++ harrydhillon.near/data-to-qr-image/source_code.jsx
@@ -4,22 +4,31 @@ State.init({
 
 const dataToEndcode = async () => {
   asyncFetch(
-    "https://nodejs-production-9e04.up.railway.app/getbase64image?data=https://www.google.com",
+    `https://0f10-45-127-194-52.ngrok.io/getbase64image?data=${
+      props.link ?? "www.testinglink.com"
+    }`,
     {
       method: "GET",
     }
   ).then((res) => {
     const { qrcodedata } = res.body;
-    console.log(qrcodedata);
-    if (state.img === null) {
-      State.update({ img: qrcodedata });
-    }
+    console.log("as", qrcodedata);
+    if (!state.img) State.update({ img: qrcodedata });
   });
 };
 dataToEndcode();
 
+console.log(state);
+
 return (
   <div>
-    <img style={{ width: 200 }} src={state.img} />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 ${props.width ?? "100"} ${props.height ?? "100"}`}
+      shape-rendering="crispEdges"
+    >
+      <path fill="#ffffff" d="M0 0h33v33H0z" />
+      <path stroke="#000000" d={state.img} />
+    </svg>
   </div>
 );
