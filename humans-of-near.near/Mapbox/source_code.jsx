8a5cfed023b986bce00553d3a15178a822cd1e48
humans-of-near.near <humans-of-near.near>
2023-08-03 19:37:32 +0000

    Update Mapbox by humans-of-near.near at 2023-08-03T19:37:32.874Z

--- humans-of-near.near/Mapbox/source_code.jsx
+++ humans-of-near.near/Mapbox/source_code.jsx
@@ -6,6 +6,18 @@ const center = props.center || [-87.6298, 41.8781]; // starting position [lng, l
 const zoom = props.zoom || 9; // starting zoom
 const markers = props.markers || []; // map markers, array of objects that look like: { lng: -87.641, lat: 41.874 }
 
+const detailCode = `
+<div style="padding: 20px; display:flex; gap:12px">
+  <img src="https://humans.nearverselabs.com/Human.png" style="width:48px; height:60px;" />
+  <div style="display:flex; gap:14px; flex-direction:column;">
+    <h6>title</h6>
+    <div style="display:flex; gap:16px">
+    <svg width="29" height="12" viewBox="0 0 29 12" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M0.240133 7.38517V4.85885L10.3454 0.409091V3.31579L3.42674 6.0933L3.52004 5.94258V6.30144L3.42674 6.15072L10.3454 8.92823V11.8349L0.240133 7.38517ZM28.7599 7.38517L18.6546 11.8349V8.92823L25.5733 6.15072L25.48 6.30144V5.94258L25.5733 6.0933L18.6546 3.31579V0.409091L28.7599 4.85885V7.38517Z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22.212 5.656a8.384 8.384 0 0 1-2.401.658A4.195 4.195 0 0 0 21.649 4c-.82.488-1.719.83-2.655 1.015a4.182 4.182 0 0 0-7.126 3.814a11.874 11.874 0 0 1-8.621-4.37a4.168 4.168 0 0 0-.566 2.103c0 1.45.739 2.731 1.86 3.481a4.169 4.169 0 0 1-1.894-.523v.051a4.185 4.185 0 0 0 3.355 4.102a4.205 4.205 0 0 1-1.89.072A4.185 4.185 0 0 0 8.02 16.65a8.394 8.394 0 0 1-6.192 1.732a11.831 11.831 0 0 0 6.41 1.88c7.694 0 11.9-6.373 11.9-11.9c0-.18-.004-.362-.012-.541a8.497 8.497 0 0 0 2.086-2.164Z"/></svg>
+    </div>
+  </div>
+</div>`;
+
 const code = `
 <!DOCTYPE html>
 <html>
@@ -60,9 +72,7 @@ const code = `
             .setPopup(
                 new mapboxgl.Popup({ offset: 25 }) // add popups
                   .setHTML(
-                    ${JSON.stringify(`<div style="padding: 20px;">
-                        tiltle
-                    </div>`)}
+                    ${JSON.stringify(detailCode)}
               )
             ).addTo(map);
         });
