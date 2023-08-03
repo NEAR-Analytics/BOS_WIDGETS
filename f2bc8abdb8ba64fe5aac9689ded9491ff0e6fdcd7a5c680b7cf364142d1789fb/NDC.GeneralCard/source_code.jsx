3305020684e020b37532b80b8dcea6afb8f4884e
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-03 20:40:48 +0000

    Update NDC.GeneralCard by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-03T20:40:48.436Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.GeneralCard/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.GeneralCard/source_code.jsx
@@ -626,7 +626,7 @@ const renderTags = () => {
 const renderArticleBody = () => {
   let displayedContent = state.sliceContent ? content.slice(0, 1000) : content;
   return (
-    <div className="mx-1">
+    <div className="mr-2">
       <Widget
         src="mob.near/widget/SocialMarkdown"
         props={{
