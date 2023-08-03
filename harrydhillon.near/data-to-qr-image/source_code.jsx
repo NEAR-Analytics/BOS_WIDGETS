677e9e32944dae5455fde0a5d63ac49485bc2f6b
harrydhillon.near <harrydhillon.near>
2023-08-03 17:18:23 +0000

    Update data-to-qr-image by harrydhillon.near at 2023-08-03T17:18:23.441Z

--- harrydhillon.near/data-to-qr-image/source_code.jsx
+++ harrydhillon.near/data-to-qr-image/source_code.jsx
@@ -23,11 +23,11 @@ return (
   <div style={{ width: 300 }}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      {...props.svgProps}
       width="100%"
       height="auto"
       viewBox="0 0 34 34"
       shape-rendering="crispEdges"
+      {...props.svgProps}
     >
       <path stroke="#000000" d={state.img} />
     </svg>
