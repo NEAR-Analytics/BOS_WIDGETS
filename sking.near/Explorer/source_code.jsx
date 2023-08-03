550962c5c699b51ede02eee394d91553a80c34ba
sking.near <sking.near>
2023-08-03 10:11:53 +0000

    Update Explorer by sking.near at 2023-08-03T10:11:53.079Z

--- sking.near/Explorer/source_code.jsx
+++ sking.near/Explorer/source_code.jsx
@@ -4,21 +4,9 @@ State.init({
 
 const value = Social.get(state.path, "final");
 
-function isJSON(str) {
-  if (typeof str !== "string") {
-    return false;
-  }
-
-  str = str.trim();
-  return (
-    (str.startsWith("{") && str.endsWith("}")) ||
-    (str.startsWith("[") && str.endsWith("]"))
-  );
-}
-
 const text = `
 \`\`\`json
-${JSON.stringify(isJSON(value) ? JSON.parse(value) : value, undefined, 2)}
+${JSON.stringify(value, undefined, 2)}
 \`\`\`
 `;
 
