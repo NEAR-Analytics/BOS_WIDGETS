189771a719a769ae36ab10cff498a8bce488093f
hack.near <hack.near>
2023-08-03 23:14:02 +0000

    Update recent.widgets by hack.near at 2023-08-03T23:14:02.130Z

--- hack.near/recent.widgets/source_code.jsx
+++ hack.near/recent.widgets/source_code.jsx
@@ -1,37 +1,32 @@
-const data = Social.keys("*/widget/*", "final");
+const data = Social.keys("*/widget/*", "final", {
+  return_type: "History",
+});
 
 if (!data) {
-  return "Loading";
+  return "Loading...";
 }
 
-const accounts = Object.entries(data);
-const allWidgets = [];
+const recentWidgets = [];
 
-for (let i = 0; i < accounts.length; ++i) {
-  const accountId = accounts[i][0];
-  const names = Object.keys(accounts[i][1].widget);
+console.log(widgets);
+
+for (let i = 0; i < widgets.length; ++i) {
+  const [accountId, type, name] = widgets[i].split("/");
+  const array = data[accountId][type][name];
+  const lastItem = array[array.length - 1];
   const widgets = [];
 
-  for (let j = 0; j < names.length; ++j) {
-    const array = [accountId]["widget"][names[j]];
-    const lastItem = array[array.length - 1];
-
-    console.log(lastItem);
-
-    if (lastItem > 95428279) {
-      widgets.push(
-        <div>
-          <li>
-            <a href={`/${accountId}/widget/${names[j]}`}>
-              {names[j] || <i>Noname widget</i>}
-            </a>
-          </li>
-        </div>
-      );
-    }
+  if (lastItem > 95428279) {
+    widgets.push(
+      <div>
+        <li>
+          <a href={`/${widgets[i]}`}>{widgets[i]}</a>
+        </li>
+      </div>
+    );
   }
 
-  allWidgets.push(
+  recentWidgets.push(
     <div className="col">
       <div className="card h-100">
         <div className="card-body">{widgets}</div>
@@ -42,6 +37,6 @@ for (let i = 0; i < accounts.length; ++i) {
 
 return (
   <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 row-cols-xll-4 g-2">
-    {allWidgets}
+    {recentWidgets}
   </div>
 );
