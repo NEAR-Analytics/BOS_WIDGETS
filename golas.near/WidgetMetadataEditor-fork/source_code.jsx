22e86ac20d0750fd475d6103fc64a447e96e4551
golas.near <golas.near>
2023-08-04 14:56:51 +0000

    Update WidgetMetadataEditor-fork by golas.near at 2023-08-04T14:56:51.036Z

--- golas.near/WidgetMetadataEditor-fork/source_code.jsx
+++ golas.near/WidgetMetadataEditor-fork/source_code.jsx
@@ -10,7 +10,7 @@ if (metadata === null) {
 return (
   <Widget
     key={widgetPath}
-    src="mob.near/widget/MetadataEditor"
+    src="near/widget/MetadataEditor"
     props={{
       initialMetadata: metadata,
       onChange,
