ff870b49f75e25d19b38613d440678aeb601dce7
golas.near <golas.near>
2023-08-04 14:55:37 +0000

    Update WidgetMetadataEditor-fork by golas.near at 2023-08-04T14:55:37.110Z

--- golas.near/WidgetMetadataEditor-fork/source_code.jsx
+++ golas.near/WidgetMetadataEditor-fork/source_code.jsx
@@ -7,52 +7,41 @@ if (metadata === null) {
   return "Loading";
 }
 
-const ccc = "cccc";
-
-const kkk = (metadata) => {
-  console.log("kkk");
-  console.log("onChange", onChange);
-  props.onChange(metadata);
-};
-
 return (
-  <div>
-    <div onClick={kkk}>+++</div>
-    <Widget
-      key={widgetPath}
-      src="golas.near/widget/MetadataEditor-fork"
-      props={{
-        initialMetadata: metadata,
-        onChange,
-        options: {
-          name: { label: "Titlex" },
-          image: { label: "Icon" },
-          description: { label: "Description" },
-          tags: {
-            label: (
-              <>
-                Tags{" "}
-                <span className="text-secondary">
-                  {" "}
-                  (add relevant tags to ensure the best discoverability of your
-                  app/component)
-                </span>
-              </>
-            ),
-            pattern: "*/widget/*/metadata/tags/*",
-            placeholder: "profile, editor, social, finance, app, image, nft",
-          },
-          linktree: {
-            links: [
-              {
-                label: "Website",
-                prefix: "https://",
-                name: "website",
-              },
-            ],
-          },
+  <Widget
+    key={widgetPath}
+    src="mob.near/widget/MetadataEditor"
+    props={{
+      initialMetadata: metadata,
+      onChange,
+      options: {
+        name: { label: "Title" },
+        image: { label: "Icon" },
+        description: { label: "Description" },
+        tags: {
+          label: (
+            <>
+              Tags{" "}
+              <span className="text-secondary">
+                {" "}
+                (add relevant tags to ensure the best discoverability of your
+                app/component)
+              </span>
+            </>
+          ),
+          pattern: "*/widget/*/metadata/tags/*",
+          placeholder: "profile, editor, social, finance, app, image, nft",
         },
-      }}
-    />
-  </div>
+        linktree: {
+          links: [
+            {
+              label: "Website",
+              prefix: "https://",
+              name: "website",
+            },
+          ],
+        },
+      },
+    }}
+  />
 );
