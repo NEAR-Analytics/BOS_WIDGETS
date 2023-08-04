363e7fa115b9a690c29c3851186dc1574c367c7f
kenrou-it.near <kenrou-it.near>
2023-08-04 21:50:25 +0000

    Update SayALot.lib.comment by kenrou-it.near at 2023-08-04T21:50:25.467Z

---
+++ kenrou-it.near/SayALot.lib.comment/source_code.jsx
@@ -0,0 +1,66 @@
+const { isTest, stateUpdate, libCalls } = props;
+
+const prodAction = "SayALotComments";
+const testAction = "test_SayALotComments";
+const action = isTest ? testAction : prodAction;
+
+// const initLibCalls = [
+//   {
+//     functionName: "get1",
+//     key: "test",
+//     props: {},
+//   },
+//   {
+//     functionName: "getWritersWhitelist",
+//     key: "writersWhitelist",
+//     props: { env: "test" },
+//   },
+// ];
+
+function getWritersWhitelist(env) {
+  if (env === "test") {
+    return ["kenrou-it.near", "ayelen.near", "martinbarba.near"];
+  } else {
+    return ["blaze.near"];
+  }
+}
+
+function libCall(call) {
+  if (call.functionName === "getComments") {
+    return getComments(call.props);
+  } else if (call.functionName === "setComment") {
+    return setComment(call.props);
+  } else if (call.functionName === "getWritersWhitelist") {
+    return getWritersWhitelist(call.props.env);
+  }
+}
+
+function getComments(args) {
+  const { realArticleId } = args;
+  const key = realArticleId;
+  return Social.index(action, key);
+}
+
+function setComment(args) {
+  const { realArticleId, comment } = args;
+  const data = {
+    index: {
+      [action]: JSON.stringify({
+        key: realArticleId,
+        value: {
+          text: comment,
+        },
+      }),
+    },
+  };
+  Social.set(data);
+}
+
+const updateObj = {};
+libCalls.forEach((call) => {
+  updateObj[call.key] = libCall(call);
+});
+
+stateUpdate(updateObj);
+
+return <></>;
