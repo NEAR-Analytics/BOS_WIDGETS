1b317c24e2f80e3a64fb822714cd89a7c7074141
humans-of-near.near <humans-of-near.near>
2023-08-04 05:31:57 +0000

    Update Mapbox by humans-of-near.near at 2023-08-04T05:31:57.533Z

--- humans-of-near.near/Mapbox/source_code.jsx
+++ humans-of-near.near/Mapbox/source_code.jsx
@@ -141,7 +141,7 @@ const code = `
               accountId : "${accountId}"
             };
 
-            asyncFetch("${API_URL}/location/bos", {
+            fetch("${API_URL}/location/bos", {
               method: "PUT",
               headers: {
                 "Content-Type": "application/json",
