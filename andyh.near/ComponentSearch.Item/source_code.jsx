e3979916d9385afa2d0ec3d803850927ed93384e
andyh.near <andyh.near>
2023-08-04 23:24:48 +0000

    Update ComponentSearch.Item by andyh.near at 2023-08-04T23:24:48.880Z

--- andyh.near/ComponentSearch.Item/source_code.jsx
+++ andyh.near/ComponentSearch.Item/source_code.jsx
@@ -16,13 +16,13 @@ return (
         {link ? (
           <a href={link} className="text-decoration-none link-dark">
             <Widget
-              src="mob.near/widget/Component.InlineBlock"
+              src="andyh.near/widget/Component.InlineBlock"
               props={{ accountId, widgetName }}
             />
           </a>
         ) : (
           <Widget
-            src="mob.near/widget/Component.InlineBlock"
+            src="andyh.near/widget/Component.InlineBlock"
             props={{ accountId, widgetName }}
           />
         )}
