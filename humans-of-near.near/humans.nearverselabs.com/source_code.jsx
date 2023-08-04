c80e20fa1b965755b8ef4400f0f33067a457594c
humans-of-near.near <humans-of-near.near>
2023-08-04 06:48:44 +0000

    Update humans.nearverselabs.com by humans-of-near.near at 2023-08-04T06:48:44.540Z

--- humans-of-near.near/humans.nearverselabs.com/source_code.jsx
+++ humans-of-near.near/humans.nearverselabs.com/source_code.jsx
@@ -1,305 +1,245 @@
+const Owner = "humans-of-near.near";
+const API_URL = "https://humans.nearverselabs.com/api";
+const MAP_STYLE = "mapbox://styles/mapbox/dark-v10";
+const MAP_TOKEN =
+  "pk.eyJ1IjoidGVqMDEiLCJhIjoiY2xqcHZ2dGpkMDB5azNsbzQ0bmMwNjRjaCJ9.FVv2zRPaLwzZMgagbI2YZw";
+
+const center = [0, 30];
+const zoom = 1.7;
+const accountId = context.accountId;
+
+State.init({
+  showModal: false,
+  edit: false,
+  user: {
+    name: "",
+    social: "",
+    twitter: "",
+  },
+  loocations: [],
+  humanAlert: true,
+});
+
+//Styles
+
 const Wrapper = styled.div`
-  --section-gap: 42px;
-  padding-top: 42px;
-
-  @media (max-width: 1160px) {
-    .line-rounded-corners {
-      display: none !important;
-    }
-  }
-
-  @media (max-width: 900px) {
-    padding-top: 0;
-  }
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: stretch;
+  flex-direction: column;
+  background: black;
 `;
 
-const H1 = styled.h1`
-  font-family: "FK Grotesk", sans-serif;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 90px;
-  line-height: 1;
-  text-align: center;
-  letter-spacing: -0.03em;
-  color: #000;
-  margin: 0;
-  max-width: 700px;
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  position: absolute;
+  padding: 30px 40px;
+`;
 
-  span {
-    display: inline-block;
-    background: #6CE89F;
-    border-radius: 20px;
-    position: relative;
-    padding: 0.1em 0.2em 0;
+const BtnStyle = {
+  background: "#191a1a",
+  padding: "10px 22px",
+  borderRadius: "6px",
+  border: "1px solid rgb(255, 255, 255)",
+  color: "white",
+  position: "absolute",
+  left: "58px",
+  top: "130px",
+  zIndex: 1,
+};
 
-    svg {
-      position: absolute;
-      bottom: -8px;
-      right: -10px;
-      width: 24px;
-    }
-  }
+const BtnStyle2 = {
+  background: "unset",
+  padding: "10px 22px",
+  borderRadius: "6px",
+  border: "1px solid rgb(255, 255, 255)",
+  color: "white",
+  position: "absolute",
+  bottom: 50,
+  zIndex: 1,
+};
 
-  @media (max-width: 900px) {
-    font-size: 50px;
+const BtnStyle2_act = {
+  background: "white",
+  padding: "10px 22px",
+  borderRadius: "6px",
+  border: "1px solid rgb(255, 255, 255)",
+  color: "#191a1a",
+  position: "absolute",
+  bottom: 50,
+  zIndex: 1,
+};
 
-    span {
-      border-radius: 12px;
-      svg {
-        position: absolute;
-        bottom: -6px;
-        right: -7px;
-        width: 16px;
+const getFirstSBTToken = () => {
+  const view = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
+    account: `${context.accountId}`,
+    issuer: "fractal.i-am-human.near",
+  });
+  return view?.[0]?.[1]?.[0];
+};
+
+const hasSBTToken = getFirstSBTToken() !== undefined;
+
+const getMyData = () => {
+  return asyncFetch(API_URL + `/auth/account?accountId=${accountId}`).then(
+    (res) => {
+      if (res.ok) {
+        return res.body.user;
       }
     }
-  }
-`;
+  );
+};
 
-const Flex = styled.div`
-  display: flex;
-  gap: 15px;
-  align-items: center;
-  flex-direction: column;
-  flex-wrap: "nowrap";
-
-    @media (max-width: 900px) {
-    flex-direction: column;
-    gap: var(--section-gap);
+const getLocations = () => {
+  return asyncFetch(API_URL + `/location`).then((res) => {
+    if (res.ok) {
+      return res.body;
     }
-`;
+  });
+};
 
-const Container = styled.div`
-  display: flex;
-  max-width: 1060px;
-  margin: 0 auto;
-  gap: var(--section-gap);
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  padding: var(--section-gap) 24px;
-`;
+const getMyInfor = async () => {
+  getMyData().then((user) => {
+    State.update({
+      user,
+    });
+  });
+};
 
-const social = {
-  data: [
-    {
-      key: 1,
-      name: "HUMANS of near - Under Construction",
-      url: "https://cdn.pixabay.com/photo/2017/10/26/17/51/under-construction-2891888_1280.jpg",
-      image:
-        "https://humans.nearverselabs.com/static/media/logo.00ce86b56e35961cfdc360d439c05fb7.svg",
-      description: "DM Punter if you have questions",
-      category: ["NEAR", "HUMANS", "Social"],
-      // deploy:
-      //   "https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fnearsocial%2Fviewer&build-command=npm%20run%20build&install-command=npm%20--force%20install&output-directory=dist",
-      // github: "https://github.com/NEARSocial/viewer",
+const getLocationsData = async () => {
+  getLocations().then((data) => {
+    State.update({
+      locations: data,
+    });
+  });
+};
+
+const onClose = () => {
+  State.update({ showModal: false });
+};
+
+const onHumanClose = () => {
+  State.update({ humanAlert: false });
+};
+
+const handleSaveLocation = () => {
+  asyncFetch(`${API_URL}/location/bos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-  ],
-};
-
-State.init({ uniqueCategories: null, searchValue: null });
-const setCategories = () => {
-  const categoriesList = [];
-  social.data.map((gateway) => categoriesList.push(...gateway.category));
-
-  // console.log(categoriesList);
-  const cats = [
-    "All",
-    ...categoriesList.filter(
-      (gateway, index) => categoriesList.indexOf(gateway) === index
-    ),
-  ];
-  //   console.log(cats);
-  State.update({
-    uniqueCategories: cats,
-  });
-};
-setCategories();
-
-const Cards = styled.div`
-  display: flex;
-  gap: 1.4rem;
-  flex-wrap: wrap;
-  justify-content: center;
-  padding-top: 4rem;
-`;
-
-const Card = styled.div`
-  width: 25%;
-  min-width: 250px;
-  display: flex;
-   flex-flow: column nowrap;
-   -ms-flex-flow:column nowrap;
-   align-items:center;
-  //  background-color:#09011a;
-   border-radius: 10px;
-   border: 1.41429px solid rgba(28,27,28,.1);
-  box-shadow: 5.65714px 5.65714px 11.3143px rgba(28,27,28,.04);
-   padding: 8px;
-  //  color: #fff;
-   margin: 0 auto;
-   max-width: 400px;
-   flex:1;
-   &:hover img{
-     transform:scale(1.05);
-   }
-   & img{
-    width: 100%;
-    height: 100%;
-    background:black;
-    overflow: hidden;
-    transition: all 0.3s ease-in-out;
-   }
-`;
-
-const Hero = styled.div`
-  height: 20vh;
-  display: flex;
-  align-items:center;
-  justify-content: center;
-  align-content:center;
-  // background-color:#09011a;
-  // color: white;
-`;
-
-const CardHeading = styled.h5`
-  font-size: 1.25rem;
-  font-weght: 500;
-  color:#09011a;
-`;
-
-const Text = styled.div`
-  opacity: .6;
-`;
-
-const ImageCard = styled.div`
-  height:200px;
-  width: 100%;
-  border-radius: inherit;
-  overflow:hidden;
-  margin-bottom: .5rem;
-  &>img{
-    background: black;
-  transition: all 0.3s ease-in-out;
-  }
-  &>img:hover{
-    transform:scale(1.05);
-  }
-`;
-
-const displayCategories = (value) => {
-  state.searchValue !== null;
-  value = value.join(" ");
-  State.update({ searchValue: value });
-  console.log("search value", state.searchValue);
-
-  const newArray = social.data.filter((item) =>
-    item.category.join(" ").includes(state.searchValue)
-  );
-  //   social.data.filter((item) =>
-  //     item.category.some((cat) => {
-  //       //   console.log("cat", cat);
-  //       return cat.includes(state?.searchValue);
-  //     })
-  //   );
-  console.log(
-    "searched",
-    // social.data.filter((item) =>
-    //   item.category.join(" ").includes(state.searchValue)
-    // )
-    // currentCat.filter((cat) => cat.includes(state.searchValue))
-    newArray
-  );
-  const dataNow = allCategories(newArray);
-  //   console.log(
-  //     "data:",
-  //     social.data.map((item) => item.category.join(" "))
-  //   );
-  State.update({
-    viewableCats: dataNow,
+    body: JSON.stringify({ accountId }),
+  }).then((res) => {
+    State.update({ edit: !state.edit });
   });
 };
 
-const words = ["hello", "world", "hi", "there"];
-const search = "hello";
+getMyInfor();
+getLocationsData();
 
-const result = words.filter((word) => search.includes(word));
-console.log(result);
-
-const allCategories = (filteredCats) =>
-  filteredCats.map((gateway) => (
-    <Card key={gateway.key}>
-      <ImageCard>
-        <a href={gateway.url} target="_blank" rel="noopener noreferrer">
-          <img src={gateway.image} alt="..." />
-        </a>
-      </ImageCard>
-      <div className="card-body p-2 mt-3">
-        <CardHeading>{gateway.name}</CardHeading>
-        <Text className="pb-3 text-secondary">
-          {`${gateway.description.trim().slice(0, 36)}...`}
-        </Text>
-      </div>
-      {false && <div>{gateway.category.map((cat) => cat).join(" ")}</div>}
-
-      <div className="row my-3">
-        <div className="d-flex justify-content-between">
-          <div className="float-left mx-3">
-            {gateway.github && (
-              <Widget
-                src="ndcplug.near/widget/Deploy.GithubButton"
-                props={{ link: gateway.github }}
-              />
-            )}
-          </div>
-          {gateway.deploy && (
-            <Widget
-              src="ndcplug.near/widget/Deploy.VercelButton"
-              props={{ link: gateway.deploy }}
-            />
-          )}
-        </div>
-      </div>
-    </Card>
-  ));
-
-const dispData = null;
-if (!state.searchValue || state.searchValue === "") {
-  dispData = allCategories(social.data);
-} else if (state.viewableCats.length > 0) {
-  dispData = state.viewableCats;
-} else {
-  dispData = "No gateways found with all these categories";
-}
 return (
-  <div className="row">
-    <Wrapper>
-      <Container>
-        <H1>
-          Gateway{" "}
-          <span>
-            {" "}
-            Directory{" "}
-            <svg viewBox="0 0 26 24" fill="none" aria-hidden="true">
+  <Wrapper>
+    <Header>
+      <Widget src={`${Owner}/widget/Header`} />
+    </Header>
+
+    {accountId && hasSBTToken && (
+      <div>
+        <button
+          class="btn"
+          style={BtnStyle}
+          onClick={() => {
+            State.update({ showModal: true });
+          }}
+        >
+          {`Profile`}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 48 48"
+          >
+            <mask id="ipSDownOne0">
               <path
-                d="M24.3767 8.06326L1.51965 0.0649912C1.10402 -0.0830767 0.639031 0.026026 0.327308 0.340346C0.0181841 0.657263 -0.0831256 1.12225 0.0701378 1.53788L8.071 23.2519C8.23726 23.7013 8.66587 24 9.14385 24H9.14644C9.62702 24 10.0556 23.6961 10.2167 23.2441L13.734 13.495L24.3325 10.2349C24.8053 10.0895 25.13 9.65824 25.1378 9.16468C25.1482 8.67112 24.8391 8.22691 24.3715 8.06326H24.3767Z"
-                fill="#323330"
+                fill="#fff"
+                stroke="#fff"
+                stroke-linejoin="round"
+                stroke-width="4"
+                d="M36 19L24 31L12 19h24Z"
               />
-            </svg>
-          </span>
-        </H1>
-      </Container>
-    </Wrapper>
-    <div className="input-group  row w-75 text-center mx-auto">
-      <Typeahead
-        options={state.uniqueCategories.slice(1)}
-        multiple
-        onChange={(value) => {
-          displayCategories(value);
+            </mask>
+            <path
+              fill="currentColor"
+              d="M0 0h48v48H0z"
+              mask="url(#ipSDownOne0)"
+            />
+          </svg>
+        </button>
+      </div>
+    )}
+
+    {accountId && hasSBTToken && (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-        placeholder="Choose a tag to filter..."
+      >
+        <button
+          class="btn"
+          style={state.edit ? BtnStyle2_act : BtnStyle2}
+          onClick={handleSaveLocation}
+        >
+          {`${!state.edit ? "Edit" : "Save"} location`}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-linejoin="round"
+              stroke-width="2"
+            >
+              <path d="M13 9a1 1 0 1 1-2 0a1 1 0 0 1 2 0Z" />
+              <path d="M17.5 9.5c0 3.038-2 6.5-5.5 10.5c-3.5-4-5.5-7.462-5.5-10.5a5.5 5.5 0 1 1 11 0Z" />
+            </g>
+          </svg>
+        </button>
+      </div>
+    )}
+
+    {accountId && hasSBTToken && state.showModal && (
+      <Widget
+        src={`${Owner}/widget/Modal`}
+        props={{ onClose, API_URL, user: state.user, getMyInfor }}
       />
-    </div>
-    <Cards>{dispData}</Cards>
-  </div>
+    )}
+
+    {accountId && !hasSBTToken && state.humanAlert && (
+      <Widget
+        src={`${Owner}/widget/HumanAlert`}
+        props={{ onClose: onHumanClose }}
+      />
+    )}
+
+    <Widget
+      src={`${Owner}/widget/Mapbox`}
+      props={{
+        API_URL,
+        accessToken: MAP_TOKEN,
+        styleUrl: MAP_STYLE,
+        center,
+        zoom,
+        markers: state.locations,
+        edit: state.edit,
+      }}
+    />
+  </Wrapper>
 );
