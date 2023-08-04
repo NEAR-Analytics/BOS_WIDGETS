b6bcb6ec6cd0656700ad33c392e8f54c2d2b5d95
duocelot.near <duocelot.near>
2023-08-04 20:55:18 +0000

    Update AddImage_gpux by duocelot.near at 2023-08-04T20:55:18.421Z

--- duocelot.near/AddImage_gpux/source_code.jsx
+++ duocelot.near/AddImage_gpux/source_code.jsx
@@ -1,72 +1,6 @@
 const accountId = context.accountId;
 
-if (!accountId) {
-  return (
-    <div
-      style={{
-        width: "512px",
-        height: "512px",
-        backgroundSize: "100%",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundColor: "white",
-        position: "flex",
-        fontFamily: '"Press Start 2P", sans-serif',
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <img
-        src="https://bafybeiep6yscx365ici64ce2o7ktr3vewuyk7rwj4kxpwrf4ugeloxgfri.ipfs.w3s.link/error-001.jpg"
-        alt="ERROR 001"
-        style={{
-          width: "340px",
-          margin: "50px 40px 10px 80px",
-          position: "flex",
-        }}
-      />
-      <h2
-        style={{
-          fontFamily: "Press Start 2P",
-          fontSize: "14px",
-          color: "black",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        ERROR 001: NO NEAR WALLET CONNECTED.{" "}
-      </h2>{" "}
-      <h3
-        style={{
-          fontFamily: "Press Start 2P",
-          fontSize: "12px",
-          color: "black",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {" "}
-        Please connect with your near wallet or create one for free
-      </h3>
-      <a
-        href="https://shard.dog/humansofbrazil"
-        style={{
-          fontFamily: "Press Start 2P",
-          fontSize: "14px",
-          color: "black",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          margin: "40px 1opx 10px 10px",
-        }}
-      >
-        HERE
-      </a>
-    </div>
-  );
-}
+<Widget src=“duocelot.near/widget/error_001”/>
 
 initState({
   img: {},
@@ -136,49 +70,6 @@ const txt2 = {
   position: "absolute",
   margin: "-44px 4px 4px 230px",
 };
-
-const Smoke = styled.div`
-  position: relative;
-  width: 100px;
-  height: 90px;
-  &:before,
-  &:after {
-    position: absolute;
-    content: '';
-    left: 70px;
-    top: 0;
-    width: 50px;
-    height: 80px;
-    background: grey;
-    border-radius: 50px 50px 20px 50px;
-    transform: rotate(-45deg);
-    transform-origin: 0 100%;
-  }
-  &:after {
-    left: 0;
-    transform: rotate(45deg);
-    transform-origin: 100% 100%;
-  }
-`;
-
-const SmokeFade = styled.keyframes`
-  0%{
-        transform: rotate(0deg) translateX(100px);
-        opacity: 1;
-        filter:blur(1px);
-    }
-    100% {
-      transform: rotate(35deg) translateY(-80px) translateX(-20px);
-        opacity: 0;
-        filter:blur(20px);
-`;
-
-const AnimatedSmoke = styled(Smoke)`
-  animation: ${SmokeFade} 1200ms ease-out infinite;
-  `;
-const AnimatedSmoke2 = styled(Smoke)`
-  animation: ${SmokeFade} 1200ms ease-out infinite;
-   animation-delay: 500ms;`;
 
 return (
   <div
@@ -557,9 +448,7 @@ return (
       }}
     ></div>
     <div style={{ backgroundColor: "red", width: "100%", height: "80px" }}>
-      {" "}
-      <AnimatedSmoke />
-      <AnimatedSmoke2 />
+      
     </div>{" "}
     <div
       style={{
