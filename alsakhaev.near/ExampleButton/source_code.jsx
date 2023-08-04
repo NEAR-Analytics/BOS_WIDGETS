6611730ab135e7ce796484c7f538b56416b5c69f
alsakhaev.near <alsakhaev.near>
2023-08-04 15:04:42 +0000

    Update ExampleButton by alsakhaev.near at 2023-08-04T15:04:42.558Z

--- alsakhaev.near/ExampleButton/source_code.jsx
+++ alsakhaev.near/ExampleButton/source_code.jsx
@@ -1,6 +1,21 @@
 return (
   <div>
-    <button onClick={() => console.log(props.label)}>
+    <button
+      onClick={() => console.log(props.label)}
+      style={{ backgroundColor: "#0f0" }}
+    >
+      {props.label ?? "Default Label"}
+    </button>
+    <button
+      onClick={() => console.log(props.label)}
+      style={{ backgroundColor: "#f00" }}
+    >
+      {props.label ?? "Default Label"}
+    </button>
+    <button
+      onClick={() => console.log(props.label)}
+      style={{ backgroundColor: "#00f" }}
+    >
       {props.label ?? "Default Label"}
     </button>
   </div>
