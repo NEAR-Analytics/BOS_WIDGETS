ae30eaea2a8e5df29df91f2e2dc03f6a69a25da8
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-04 19:21:15 +0000

    Update sayALot.generalHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-04T19:21:15.252Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/sayALot.generalHandler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/sayALot.generalHandler/source_code.jsx
@@ -55,6 +55,7 @@ if (isTest) {
 
 const widgets = {
   thisWidget: `${authorForWidget}/widget/${thisWidgetName}`,
+  create: `${authorForWidget}/widgets/NDC.Create`,
   styledComponents: "rubycop.near/widget/NDC.StyledComponents",
   header: `${authorForWidget}/widget/NDC.NavBar`,
   showArticlesList: `${authorForWidget}/widget/SayALot.AllArticlesList`,
@@ -93,6 +94,226 @@ const navigationPills = [
 const navigationButtons = [
   { id: ARTICLE_WORKSHOP.id, title: "+Create article" },
 ];
+
+const initialBody = `# h1 Heading 8-) 
+## h2 Heading 
+### h3 Heading 
+#### h4 Heading 
+##### h5 Heading 
+###### h6 Heading 
+ 
+ 
+## Horizontal Rules 
+ 
+___ 
+ 
+--- 
+ 
+*** 
+ 
+ 
+## Typographic replacements 
+ 
+Enable typographer option to see result. 
+ 
+(c) (C) (r) (R) (tm) (TM) (p) (P) +- 
+ 
+test.. test... test..... test?..... test!.... 
+ 
+!!!!!! ???? ,,  -- --- 
+ 
+"Smartypants, double quotes" and 'single quotes' 
+ 
+ 
+## Emphasis 
+ 
+This is bold text 
+ 
+This is bold text 
+ 
+*This is italic text* 
+ 
+_This is italic text_ 
+ 
+Strikethrough 
+ 
+ 
+## Blockquotes 
+ 
+ 
+> Blockquotes can also be nested... 
+>> ...by using additional greater-than signs right next to each other... 
+> > > ...or with spaces between arrows. 
+ 
+ 
+## Lists 
+ 
+Unordered 
+ 
++ Create a list by starting a line with \`+\`, \`-\`, or \`*\` 
++ Sub-lists are made by indenting 2 spaces: 
+  - Marker character change forces new list start: 
+    * Ac tristique libero volutpat at 
+    + Facilisis in pretium nisl aliquet 
+    - Nulla volutpat aliquam velit 
++ Very easy! 
+ 
+Ordered 
+ 
+1. Lorem ipsum dolor sit amet 
+2. Consectetur adipiscing elit 
+3. Integer molestie lorem at massa 
+ 
+ 
+1. You can use sequential numbers... 
+1. ...or keep all the numbers as \`1.\` 
+ 
+Start numbering with offset: 
+ 
+57. foo 
+1. bar 
+ 
+ 
+## Code 
+ 
+Inline \`code\` 
+ 
+Indented code 
+ 
+    // Some comments 
+    line 1 of code 
+    line 2 of code 
+    line 3 of code 
+ 
+ 
+Block code "fences" 
+ 
+\`\`\` 
+Sample text here... 
+\`\`\` 
+ 
+Syntax highlighting 
+ 
+\`\`\` js 
+var foo = function (bar) { 
+  return bar++; 
+}; 
+ 
+console.log(foo(5)); 
+\`\`\` 
+ 
+## Tables 
+ 
+| Option | Description | 
+| ------ | ----------- | 
+| data   | path to data files to supply the data that will be passed into templates. | 
+| engine | engine to be used for processing templates. Handlebars is the default. | 
+| ext    | extension to be used for dest files. | 
+ 
+Right aligned columns 
+ 
+| Option | Description | 
+| ------:| -----------:| 
+| data   | path to data files to supply the data that will be passed into templates. | 
+| engine | engine to be used for processing templates. Handlebars is the default. | 
+| ext    | extension to be used for dest files. | 
+ 
+ 
+## Links 
+ 
+link text 
+ 
+link with title 
+ 
+Autoconverted link https://github.com/nodeca/pica (enable linkify to see) 
+ 
+ 
+## Images 
+ 
+!Minion 
+!Stormtroopocat 
+ 
+Like links, Images also have a footnote style syntax 
+ 
+![Alt text][id] 
+ 
+With a reference later in the document defining the URL location: 
+ 
+[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat" 
+ 
+### Emojies 
+ 
+> Classic markup: :wink: :crush: :cry: :tear: :laughing: :yum: 
+> 
+> Shortcuts (emoticons): :-) :-( 8-) ;) 
+ 
+see how to change output with twemoji. 
+ 
+ 
+### Subscript / Superscript 
+ 
+- 19^th^ 
+- H~2~O 
+ 
+ 
+### \<ins> 
+ 
+++Inserted text++ 
+ 
+ 
+### \<mark> 
+ 
+==Marked text== 
+ 
+ 
+### Footnotes 
+ 
+Footnote 1 link[^first]. 
+ 
+Footnote 2 link[^second]. 
+ 
+Inline footnote^[Text of inline footnote] definition. 
+ 
+Duplicated footnote reference[^second]. 
+ 
+[^first]: Footnote can have markup 
+ 
+    and multiple paragraphs. 
+ 
+[^second]: Footnote text. 
+ 
+ 
+### [Definition lists](https://github.com/markdown-it/markdown-it-deflist) 
+ 
+Term 1 
+ 
+:   Definition 1 
+with lazy continuation. 
+ 
+Term 2 with *inline markup* 
+ 
+:   Definition 2 
+ 
+        { some code, part of Definition 2 } 
+ 
+    Third paragraph of definition 2. 
+ 
+_Compact style:_ 
+ 
+Term 1 
+  ~ Definition 1 
+ 
+Term 2 
+  ~ Definition 2a 
+  ~ Definition 2b 
+`;
+
+const initialCreateState = {
+  articleId: "",
+  articleBody: initialBody,
+  tags: {},
+  saveComplete: false,
+};
 
 //=================================================END CONSTS=======================================================
 
@@ -251,9 +472,131 @@ function getLastEditionsByArticle() {
 }
 
 const finalArticles = getLastEditionsByArticle() ?? [];
+
+if (
+  state.renderLastOfThisAccountId &&
+  state.renderLastOfThisAccountId.length > 0
+) {
+  const renderLastOfThisAccountId = state.renderLastOfThisAccountId;
+  State.update({
+    renderLastOfThisAccountId: "",
+    articleToRenderData: finalArticles.find(
+      (article) => article.author == renderLastOfThisAccountId
+    ),
+  });
+}
 //===============================================END GET DATA=======================================================
 
 //=================================================FUNCTIONS========================================================
+//==Post creation funtcions
+const getCreateData = (articleId, accountId, articleBody, tagsArray) => {
+  const args = {
+    articleId: articleId,
+    author: accountId,
+    lastEditor: accountId,
+    timeLastEdit: Date.now(),
+    timeCreate: Date.now(),
+    body: articleBody,
+    version: 0,
+    navigation_id: null,
+    tags: tagsArray,
+  };
+  return args;
+};
+
+const composeData = (articleId, accountId, articleBody, tagsArray) => {
+  let data;
+  if (isTest) {
+    data = {
+      test_sayALotArticle: {
+        main: JSON.stringify(
+          getCreateData(articleId, accountId, articleBody, tagsArray)
+        ),
+      },
+      index: {
+        test_sayALotArticle: JSON.stringify({
+          key: "main",
+          value: {
+            type: "md",
+            id: `${context.accountId}-${Date.now()}`,
+          },
+        }),
+      },
+    };
+  } else {
+    data = {
+      sayALotArticle: {
+        main: JSON.stringify(getArticleData()),
+      },
+      index: {
+        sayALotArticle: JSON.stringify({
+          key: "main",
+          value: {
+            type: "md",
+            id: `${context.accountId}-${Date.now()}`,
+          },
+        }),
+      },
+    };
+  }
+
+  if (tagsArray.length) {
+    data.index.tag = JSON.stringify(
+      tagsArray.map((tag) => ({
+        key: tag,
+        value: item,
+      }))
+    );
+  }
+
+  return data;
+};
+
+const createHandler = (e, articleId, accountId, articleBody, tagsArray) => {
+  State.update({
+    createErrorId: "",
+    createErrorBody: "",
+  });
+  if (state.articleId && state.articleBody) {
+    // TODO check it automaticle
+    const isArticleIdDublicated = false;
+
+    if (!isArticleIdDublicated) {
+      const newData = composeData(articleId, accountId, articleBody, tagsArray);
+
+      State.update({ createIsSaving: true });
+
+      Social.set(newData, {
+        force: true,
+        onCommit: () => {
+          State.update({
+            createIsSaving: false,
+            displayedTabId: tabs.SHOW_ARTICLE.id,
+            renderLastOfThisAccountId: accountId,
+          });
+        },
+        onCancel: () => {
+          State.update({ createIsSaving: false });
+        },
+      });
+    } else {
+      State.update({
+        createErrorId: errTextDublicatedId,
+      });
+    }
+  } else {
+    if (!articleId || articleId == "") {
+      State.update({
+        createErrorId: errTextNoId,
+      });
+    }
+    if (!articleBody || articleBody == "") {
+      State.update({ createErrorBody: errTextNoBody });
+    }
+  }
+};
+
+//==End post creation functions
 
 function stateUpdate(obj) {
   State.update(obj);
@@ -335,6 +678,22 @@ return (
           handleFilterArticles,
           articleToRenderData: state.articleToRenderData,
           authorForWidget,
+        }}
+      />
+    )}
+
+    {state.displayedTabId == tabs.ARTICLE_WORKSHOP.id && (
+      <Widget
+        src={widgets.create}
+        props={{
+          isTest,
+          addressForArticles,
+          authorForWidget,
+          initialCreateState,
+          createHandler,
+          createIsSaving: state.createIsSaving,
+          createErrorId: createErrorId,
+          createErrorBody: createErrorBody,
         }}
       />
     )}
