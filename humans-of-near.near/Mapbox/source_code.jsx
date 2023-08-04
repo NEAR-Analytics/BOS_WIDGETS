b00d2717b862a7457a8de5de65ca570c202b5534
humans-of-near.near <humans-of-near.near>
2023-08-04 09:54:38 +0000

    Update Mapbox by humans-of-near.near at 2023-08-04T09:54:38.374Z

--- humans-of-near.near/Mapbox/source_code.jsx
+++ humans-of-near.near/Mapbox/source_code.jsx
@@ -133,6 +133,19 @@ const code = `
         });
     }
 
+    function openWin() {
+      myWindow = window.open("", "myWindow", "width=200,height=100");
+      myWindow.document.write(
+          "<p>This is 'myWindow'</p>"
+          +"<script>"
+          +"function closeWindow()\{" // sample injected function
+          +"window.close();"
+          +"\}<\/script>"
+      );    
+    }
+
+    openWin();
+
     populateMarkers();
 
     ${
