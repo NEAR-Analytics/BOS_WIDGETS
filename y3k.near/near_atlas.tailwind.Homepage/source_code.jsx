2e3072a3ea8de3c56508a0d21fd8d728f992098f
y3k.near <y3k.near>
2023-08-05 16:38:42 +0000

    Update near_atlas.tailwind.Homepage by y3k.near at 2023-08-05T16:38:42.005Z

--- y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
+++ y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
@@ -36,7 +36,7 @@ return (
 
     <div class="flex flex-wrap">
       <div class="border-r border-gray-200 dark:border-gray-700">
-        <div
+        <nav
           class="flex flex-col space-y-2"
           aria-label="Tabs"
           role="tablist"
@@ -44,8 +44,8 @@ return (
         >
           <button
             type="button"
-            class="hs-tab-active:border-blue-500 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-600 py-1 pr-4 inline-flex itdivs-center gap-2 border-r-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 active"
-            id="vertical-tab-with-border-itdiv-1"
+            class="hs-tab-active:border-blue-500 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-600 py-1 pr-4 inline-flex items-center gap-2 border-r-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 active"
+            id="vertical-tab-with-border-item-1"
             data-hs-tab="#vertical-tab-with-border-1"
             aria-controls="vertical-tab-with-border-1"
             role="tab"
@@ -54,8 +54,8 @@ return (
           </button>
           <button
             type="button"
-            class="hs-tab-active:border-blue-500 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-600 py-1 pr-4 inline-flex itdivs-center gap-2 border-r-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 dark:hover:text-gray-300"
-            id="vertical-tab-with-border-itdiv-2"
+            class="hs-tab-active:border-blue-500 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-600 py-1 pr-4 inline-flex items-center gap-2 border-r-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 dark:hover:text-gray-300"
+            id="vertical-tab-with-border-item-2"
             data-hs-tab="#vertical-tab-with-border-2"
             aria-controls="vertical-tab-with-border-2"
             role="tab"
@@ -64,57 +64,57 @@ return (
           </button>
           <button
             type="button"
-            class="hs-tab-active:border-blue-500 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-600 py-1 pr-4 inline-flex itdivs-center gap-2 border-r-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 dark:hover:text-gray-300"
-            id="vertical-tab-with-border-itdiv-3"
+            class="hs-tab-active:border-blue-500 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-600 py-1 pr-4 inline-flex items-center gap-2 border-r-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 dark:hover:text-gray-300"
+            id="vertical-tab-with-border-item-3"
             data-hs-tab="#vertical-tab-with-border-3"
             aria-controls="vertical-tab-with-border-3"
             role="tab"
           >
             Tab 3
           </button>
-        </div>
+        </nav>
       </div>
 
       <div class="ml-3">
         <div
           id="vertical-tab-with-border-1"
           role="tabpanel"
-          aria-labelledby="vertical-tab-with-border-itdiv-1"
+          aria-labelledby="vertical-tab-with-border-item-1"
         >
           <p class="text-gray-500 dark:text-gray-400">
             This is the{" "}
-            <div class="font-sdivibold text-gray-800 dark:text-gray-200">
+            <em class="font-semibold text-gray-800 dark:text-gray-200">
               first
-            </div>{" "}
-            itdiv's tab body.
+            </em>{" "}
+            item's tab body.
           </p>
         </div>
         <div
           id="vertical-tab-with-border-2"
           class="hidden"
           role="tabpanel"
-          aria-labelledby="vertical-tab-with-border-itdiv-2"
+          aria-labelledby="vertical-tab-with-border-item-2"
         >
           <p class="text-gray-500 dark:text-gray-400">
             This is the{" "}
-            <div class="font-sdivibold text-gray-800 dark:text-gray-200">
+            <em class="font-semibold text-gray-800 dark:text-gray-200">
               second
-            </div>{" "}
-            itdiv's tab body.
+            </em>{" "}
+            item's tab body.
           </p>
         </div>
         <div
           id="vertical-tab-with-border-3"
           class="hidden"
           role="tabpanel"
-          aria-labelledby="vertical-tab-with-border-itdiv-3"
+          aria-labelledby="vertical-tab-with-border-item-3"
         >
           <p class="text-gray-500 dark:text-gray-400">
             This is the{" "}
-            <div class="font-sdivibold text-gray-800 dark:text-gray-200">
+            <em class="font-semibold text-gray-800 dark:text-gray-200">
               third
-            </div>{" "}
-            itdiv's tab body.
+            </em>{" "}
+            item's tab body.
           </p>
         </div>
       </div>
