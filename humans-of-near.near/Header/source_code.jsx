85ac619d768f3a252d465ab844cd108b9b3d3327
humans-of-near.near <humans-of-near.near>
2023-08-05 07:38:40 +0000

    Update Header by humans-of-near.near at 2023-08-05T07:38:40.499Z

--- humans-of-near.near/Header/source_code.jsx
+++ humans-of-near.near/Header/source_code.jsx
@@ -1,8 +1,12 @@
+const Logo = styled.img`
+  position: absolute;
+  top: 20px;
+  left: 40px;
+  
+`;
+
 return (
   <div className="logo">
-    <img
-      style={{ position: "absolute", top: 20, left: 40 }}
-      src="https://humans.nearverselabs.com/static/media/logo.00ce86b56e35961cfdc360d439c05fb7.svg"
-    />
+    <Logo src="https://humans.nearverselabs.com/static/media/logo.00ce86b56e35961cfdc360d439c05fb7.svg" />
   </div>
 );
