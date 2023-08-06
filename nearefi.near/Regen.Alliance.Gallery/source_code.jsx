0d9ad25acbca3fa50bd4d8bb9ea51511b01d0c8c
nearefi.near <nearefi.near>
2023-08-06 09:50:15 +0000

    Update Regen.Alliance.Gallery by nearefi.near at 2023-08-06T09:50:15.690Z

---
+++ nearefi.near/Regen.Alliance.Gallery/source_code.jsx
@@ -0,0 +1,116 @@
+const limit = 20;
+const nftContract = props.nftContract ?? "nft.regens.near";
+State.init({
+  offset: 0,
+  tokens: [],
+  hasMore: true,
+});
+
+function fetchTokens() {
+  asyncFetch("https://graph.mintbase.xyz/mainnet", {
+    method: "POST",
+    headers: {
+      "mb-api-key": "omni-site",
+      "Content-Type": "application/json",
+      "x-hasura-role": "anonymous",
+    },
+    body: JSON.stringify({
+      query: `
+          query MyQuery {
+            mb_views_nft_tokens(
+                limit: ${limit},
+                offset: ${state.offset}
+              where: { nft_contract_id: { _eq: "${nftContract}" }}
+              order_by: {minted_timestamp: desc}
+            ) {
+              media
+              owner
+            }
+          }
+        `,
+    }),
+  }).then((res) => {
+    if (res.ok) {
+      const tokens = res.body.data.mb_views_nft_tokens;
+      if (tokens.length > 0) {
+        State.update({
+          tokens: [...state.tokens, ...tokens],
+          offset: state.offset + limit,
+          hasMore: true,
+        });
+      } else {
+        State.update({
+          hasMore: false,
+        });
+      }
+    }
+  });
+}
+
+function Sharddog({ owner, media, token }) {
+  const size = "144px";
+
+  return (
+    <Link
+      to={`mob.near/widget/NftImage?tokenId=${token}&contractId=${nftContract}`}
+    >
+      <Widget
+        src="mob.near/widget/Image"
+        props={{
+          image: {
+            url: media,
+          },
+          style: {
+            width: size,
+            height: size,
+            objectFit: "cover",
+            minWidth: size,
+            minHeight: size,
+            maxWidth: size,
+            maxHeight: size,
+            overflowWrap: "break-word",
+          },
+        }}
+      />
+    </Link>
+  );
+}
+
+const size = "144px";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(143px, 0.1fr));
+
+  @media (hover: none) {
+    grid-template-columns: repeat(auto-fill, minmax(143px, 0.1fr));
+  }
+`;
+
+const loader = (
+  <div className="loader" key={"loader"}>
+    <span
+      className="spinner-grow spinner-grow-sm me-1"
+      role="status"
+      aria-hidden="true"
+    />
+    Loading ...
+  </div>
+);
+
+return (
+  <InfiniteScroll
+    pageStart={0}
+    loadMore={fetchTokens}
+    hasMore={state.hasMore}
+    loader={loader}
+  >
+    <Grid>
+      {state.tokens?.map((it) => {
+        return (
+          <Sharddog owner={it.owner} media={it.media} token={it.tokenId} />
+        );
+      })}
+    </Grid>
+  </InfiniteScroll>
+);
