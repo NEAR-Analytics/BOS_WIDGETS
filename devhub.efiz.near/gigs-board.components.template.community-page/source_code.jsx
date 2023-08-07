0efb4cd024d89c0b45a1437f1c70ff1aea3599c9
devhub.efiz.near <devhub.efiz.near>
2023-08-07 21:49:47 +0000

    Update gigs-board.components.template.community-page by devhub.efiz.near at 2023-08-07T21:49:47.286Z

---
+++ devhub.efiz.near/gigs-board.components.template.community-page/source_code.jsx
@@ -0,0 +1,73 @@
+d; /* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+
+  const linkPropsQuery = Object.entries(linkProps)
+    .filter(([_key, nullable]) => (nullable ?? null) !== null)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
+
+const CommunityPageTemplate = ({ children, handle, path, title }) => (
+  <div className="w-100 h-100">
+    {widget("components.layout.app-header")}
+    {/* TODO: Add breadcrumbs rendered from path prop */}
+
+    {typeof handle === "string" ? (
+      <>
+        {widget("entity.community.header", { activeTabTitle: title, handle })}
+        <div style={{ padding: "0 32px" }}>{children}</div>
+      </>
+    ) : (
+      <div class="alert alert-danger" role="alert">
+        Error: community handle not found in URL parameters
+      </div>
+    )}
+  </div>
+);
+
+return <CommunityPageTemplate {...props} />;
