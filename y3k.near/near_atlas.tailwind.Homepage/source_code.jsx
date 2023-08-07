3170206b309cccbc1764a78a6b8ab480fec7bfe9
y3k.near <y3k.near>
2023-08-07 20:29:25 +0000

    Update near_atlas.tailwind.Homepage by y3k.near at 2023-08-07T20:29:25.597Z

--- y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
+++ y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
@@ -44,5 +44,93 @@ return (
           </div>
         )}
     </div>
+
+    <div className="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl">
+        <div className="hidden sm:block">
+          <nav className="flex border-b border-white/10 py-4">
+            <ul
+              role="list"
+              className="flex min-w-full flex-none gap-x-6 px-2 text-sm font-semibold leading-6 text-gray-400"
+            >
+              <li>
+                <button
+                  className="text-indigo-400"
+                  id="pills-widget-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#pills-widget"
+                  type="button"
+                  role="tab"
+                  aria-controls="pills-widget"
+                  aria-selected="true"
+                >
+                  Widget
+                </button>
+              </li>
+              <li>
+                <button
+                  className="text-gray-400"
+                  id="pills-groups-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#pills-groups"
+                  type="button"
+                  role="tab"
+                  aria-controls="pills-groups"
+                  aria-selected="false"
+                  onClick={() => {
+                    !state.loadGroups && State.update({ loadGroups: true });
+                  }}
+                >
+                  Groups
+                </button>
+              </li>
+              <li>
+                <button
+                  className="text-gray-400"
+                  id="pills-comments-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#pills-comments"
+                  type="button"
+                  role="tab"
+                  aria-controls="pills-comments"
+                  aria-selected="false"
+                  onClick={() => {
+                    !state.loadComments && State.update({ loadComments: true });
+                  }}
+                >
+                  Comments
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="tab-content" id="pills-tabContent">
+          <div
+            className="tab-pane fade in show active"
+            id="pills-widget"
+            role="tabpanel"
+            aria-labelledby="pills-widget-tab"
+          >
+            <Widget src={featuredWidget} />
+          </div>
+          <div
+            className="tab-pane fade groups"
+            id="pills-groups"
+            role="tabpanel"
+            aria-labelledby="pills-groups-tab"
+          >
+            <Widget src="gov.near/widget/FollowTabs" props={{ accountId }} />
+          </div>
+          <div
+            className="tab-pane fade comments"
+            id="pills-comments"
+            role="tabpanel"
+            aria-labelledby="pills-comments-tab"
+          >
+            <Widget src="gov.near/widget/Comments" props={{ accountId }} />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
