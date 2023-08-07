779d5bf2ba9f2e808b92e597f8168d30959a58cd
kudos.ndctools.near <kudos.ndctools.near>
2023-08-07 18:47:29 +0000

    Update NDC.Kudos.Header by kudos.ndctools.near at 2023-08-07T18:47:29.114Z

---
+++ kudos.ndctools.near/NDC.Kudos.Header/source_code.jsx
@@ -0,0 +1,40 @@
+const { isIAmHuman } = props;
+
+const widget = {
+  styledComponents: "kudos.ndctools.near/widget/NDC.StyledComponents",
+};
+
+const Header = styled.div`
+  background: linear-gradient(90deg, #9333ea 0%, #4f46e5 100%);
+`;
+
+return (
+  <Header className="d-flex p-3 px-4 align-items-center rounded justify-content-between">
+    <Widget
+      src="mob.near/widget/Image"
+      props={{
+        image: {
+          url: "https://bafkreicrlj3lgygabo37j6gelyamwvm5qj4vtd3sid62dlbr7s6wi3qjhm.ipfs.nftstorage.link/",
+        },
+        alt: "kudos",
+        style: {
+          height: "30px",
+          objectFit: "cover",
+        },
+        fallbackUrl:
+          "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
+      }}
+    />
+    {!isIAmHuman && (
+      <Widget
+        src={widget.styledComponents}
+        props={{
+          Link: {
+            text: "Verify as Human",
+            href: "https://i-am-human.app/",
+          },
+        }}
+      />
+    )}
+  </Header>
+);
