7050dd51e4493ec54a63b6dd478dd00d64ac225a
y3k.near <y3k.near>
2023-08-07 15:25:55 +0000

    Update near_atlas.tailwind.Homepage by y3k.near at 2023-08-07T15:25:55.531Z

--- y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
+++ y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
@@ -17,13 +17,11 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Documents", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
+const pills = [
+  { id: "overview", title: "Overview" },
+  { id: "posts", title: "Posts" },
+  { id: "nfts", title: "NFTs" },
+  { id: "widget", title: "Widgets" },
 ];
 
 return (
@@ -47,24 +45,89 @@ return (
         )}
     </div>
 
-    <nav className="flex flex-1 flex-col" aria-label="Sidebar">
-      <ul role="list" className="-mx-2 space-y-1">
-        {navigation.map((item) => (
-          <li key={item.name}>
-            <a
-              href={item.href}
-              className={
-                (item.current
-                  ? "bg-gray-50 text-indigo-600"
-                  : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50") +
-                " group flex gap-x-3 rounded-md p-2 pl-3 text-sm leading-6 font-semibold"
-              }
-            >
-              {item.name}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <ul
+      className="flex flex-1 flex-col block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+      id="pills-tab"
+      role="tablist"
+      aria-label="Sidebar"
+    >
+      {pills.map(({ id, title }, i) => (
+        <li className="nav-item" role="presentation" key={i}>
+          <button
+            className={`nav-link ${i === 0 ? "active" : ""}`}
+            id={`pills-${id}-tab`}
+            data-bs-toggle="pill"
+            data-bs-target={`#pills-${id}`}
+            type="button"
+            role="tab"
+            aria-controls={`pills-${id}`}
+            aria-selected={i === 0}
+            onClick={() => {
+              const key = `load${id}`;
+              !state[key] && State.update({ [key]: true });
+            }}
+          >
+            {title}
+          </button>
+        </li>
+      ))}
+    </ul>
+
+    <div class="-mb-px flex space-x-8" aria-label="Tabs">
+      <div
+        className="tab-pane fade "
+        id="pills-posts"
+        role="tabpanel"
+        aria-labelledby="pills-posts-tab"
+      >
+        <div className="col-lg-8 mx-auto">
+          {description && (
+            <div className="border rounded-4 p-3 pb-0 mb-3">
+              <h4>
+                <i class="bi bi-pin-angle" /> Bio
+              </h4>
+              <Markdown text={description} />
+            </div>
+          )}
+          <Widget
+            src="mob.near/widget/MainPage.Feed"
+            props={{ accounts: [accountId] }}
+          />
+        </div>
+      </div>
+      <div
+        className="tab-pane fade show active"
+        id="pills-overview"
+        role="tabpanel"
+        aria-labelledby="pills-overview-tab"
+      >
+        <div>
+          <Widget
+            src="y3k.near/widget/Profile.RightSection"
+            props={{ accountId, profile, theme }}
+          />
+        </div>
+      </div>
+      <div
+        className="tab-pane fade"
+        id="pills-nfts"
+        role="tabpanel"
+        aria-labelledby="pills-nfts-tab"
+      >
+        {state.loadnfts && (
+          <Widget src="mob.near/widget/YourNFTs" props={{ accountId }} />
+        )}
+      </div>
+      <div
+        className="tab-pane fade widget"
+        id="pills-widget"
+        role="tabpanel"
+        aria-labelledby="pills-widget-tab"
+      >
+        {state.loadwidget && (
+          <Widget src="mob.near/widget/LastWidgets" props={{ accountId }} />
+        )}
+      </div>
+    </div>
   </div>
 );
