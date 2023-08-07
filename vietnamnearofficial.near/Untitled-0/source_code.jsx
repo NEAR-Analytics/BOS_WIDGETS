af6e9d9410a034662e5302e3610aafaefb849684
vietnamnearofficial.near <vietnamnearofficial.near>
2023-08-07 14:51:18 +0000

    Update Untitled-0 by vietnamnearofficial.near at 2023-08-07T14:51:18.537Z

---
+++ vietnamnearofficial.near/Untitled-0/source_code.jsx
@@ -0,0 +1,12 @@
+let greeting = "Hoàng sa, Trường sa là của Viet Nam";
+let question = "Do you know that ?";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1> HELLO {props.name}</h1>
+      <h1> {question} </h1>
+      <p> {greeting} </p>
+    </div>
+  </>
+);
