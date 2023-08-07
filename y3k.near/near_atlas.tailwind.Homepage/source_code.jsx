7dddb14f6b99feff63af0ef8490e6fe822a7da21
y3k.near <y3k.near>
2023-08-07 14:22:19 +0000

    Update near_atlas.tailwind.Homepage by y3k.near at 2023-08-07T14:22:19.565Z

--- y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
+++ y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
@@ -47,28 +47,24 @@ return (
         )}
     </div>
 
-    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      {/* Content goes here */}
-
-      <nav className="flex flex-1 flex-col" aria-label="Sidebar">
-        <ul role="list" className="-mx-2 space-y-1">
-          {navigation.map((item) => (
-            <li key={item.name}>
-              <a
-                href={item.href}
-                className={
-                  (item.current
-                    ? "bg-gray-50 text-indigo-600"
-                    : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50") +
-                  " group flex gap-x-3 rounded-md p-2 pl-3 text-sm leading-6 font-semibold"
-                }
-              >
-                {item.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+    <nav className="flex flex-1 flex-col" aria-label="Sidebar">
+      <ul role="list" className="-mx-2 space-y-1">
+        {navigation.map((item) => (
+          <li key={item.name}>
+            <a
+              href={item.href}
+              className={
+                (item.current
+                  ? "bg-white-50 text-indigo-600"
+                  : "text-white-700 hover:text-indigo-600 hover:bg-white-50") +
+                " group flex gap-x-3 rounded-md p-2 pl-3 text-sm leading-6 font-semibold"
+              }
+            >
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   </div>
 );
