9eb6cc2ae2d67dbc73169cba481db70febaa78ed
andyh.near <andyh.near>
2023-08-08 22:17:08 +0000

    Update Component.InlineBlock by andyh.near at 2023-08-08T22:17:08.314Z

--- andyh.near/Component.InlineBlock/source_code.jsx
+++ andyh.near/Component.InlineBlock/source_code.jsx
@@ -1,6 +1,6 @@
 const accountId = props.accountId;
 const widgetName = props.widgetName;
-console.log({ props });
+console.log({ props: Object.fromEntries(Object.entries(props)) });
 const widgetPath = `${accountId}/widget/${widgetName}`;
 const blockHeight = props.blockHeight;
 const metadata = props.metadata ?? Social.getr(`${widgetPath}/metadata`);
