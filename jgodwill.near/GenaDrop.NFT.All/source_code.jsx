1229d7ce30d157061551b845f34df8b6c1723eaa
jgodwill.near <jgodwill.near>
2023-08-08 08:38:15 +0000

    Update GenaDrop.NFT.All by jgodwill.near at 2023-08-08T08:38:15.004Z

---
+++ jgodwill.near/GenaDrop.NFT.All/source_code.jsx
@@ -0,0 +1,127 @@
+const limit = props.limit ?? 475;
+const nftContract = props.nftContract ?? "nft.genadrop.near";
+State.init({
+  offset: 0,
+  tokens: [],
+  hasMore: true,
+});
+
+function fetchTokens() {
+  asyncFetch("https://graph.mintbase.xyz/mainnet", {
+    method: "POST",
+    headers: {
+      "mb-api-key": "omni-site",
+      "Content-Type": "application/json",
+      "x-hasura-role": "anonymous",
+    },
+    body: JSON.stringify({
+      query: `
+          query MyQuery {
+            mb_views_nft_tokens(
+                limit: ${limit},
+                offset: ${state.offset}
+              where: { nft_contract_id: { _eq: "${nftContract}" }}
+              order_by: {minted_timestamp: desc}
+            ) {
+            
+              media
+              owner
+              token_id
+            }
+          }
+        `,
+    }),
+  }).then((res) => {
+    if (res.ok) {
+      const tokens = res.body.data.mb_views_nft_tokens;
+      if (tokens.length > 0) {
+        State.update({
+          tokens: [...state.tokens, ...tokens],
+          offset: state.offset + limit,
+          hasMore: true,
+        });
+      } else {
+        State.update({
+          hasMore: false,
+        });
+      }
+    }
+  });
+}
+
+function Sharddog({ owner, media, token }) {
+  const size = "144px";
+
+  return (
+    <a
+      href={`/mob.near/widget/NftImage?tokenId=${token}&contractId=${nftContract}`}
+      target="_blank"
+    >
+      <Widget
+        src="mob.near/widget/Image"
+        props={{
+          image: {
+            url: media,
+          },
+          style: {
+            width: size,
+            height: size,
+            objectFit: "cover",
+            minWidth: size,
+            minHeight: size,
+            maxWidth: size,
+            maxHeight: size,
+            overflowWrap: "break-word",
+          },
+        }}
+      />
+    </a>
+  );
+}
+
+const size = "144px";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(143px, 0.1fr));
+
+  @media (hover: none) {
+    grid-template-columns: repeat(auto-fill, minmax(143px, 0.1fr));
+  }
+  margin: 0 auto;
+`;
+
+const Title = styled.h4`
+text-align: center;
+padding: 1rem 0;
+`;
+
+const loader = (
+  <div className="loader" key={"loader"}>
+    <span
+      className="spinner-grow spinner-grow-sm me-1"
+      role="status"
+      aria-hidden="true"
+    />
+    Loading ...
+  </div>
+);
+
+return (
+  <InfiniteScroll
+    pageStart={0}
+    loadMore={fetchTokens}
+    hasMore={state.hasMore}
+    loader={loader}
+  >
+    <Title>NFT Count: {state?.tokens.length}</Title>
+    <Grid>
+      {state.tokens?.map((it) => {
+        console.log("It " + it.tokenid);
+        return (
+          <Sharddog owner={it.owner} media={it.media} token={it.token_id} />
+        );
+      })}
+    </Grid>
+  </InfiniteScroll>
+);
