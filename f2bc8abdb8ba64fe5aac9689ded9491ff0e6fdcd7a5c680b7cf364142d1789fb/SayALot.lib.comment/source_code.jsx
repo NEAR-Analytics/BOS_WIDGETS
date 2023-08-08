3bfa6e92445fd71a6efddda69d64130910f25bdf
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-08 20:14:28 +0000

    Update SayALot.lib.comment by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-08T20:14:28.222Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.lib.comment/source_code.jsx
@@ -0,0 +1,76 @@
+const { isTest, stateUpdate, libCalls } = props;
+console.log("in");
+
+const prodAction = "SayALotComments";
+const testAction = "test_SayALotComments";
+const action = isTest ? testAction : prodAction;
+
+// const initLibCalls = [
+//   {
+//     functionName: "get1",
+//     key: "test",
+//     props: {},
+//   },
+//   {
+//     functionName: "getWritersWhitelist",
+//     key: "writersWhitelist",
+//     props: { env: "test" },
+//   },
+// ];
+
+function getWritersWhitelist(env) {
+  if (env === "test") {
+    return ["kenrou-it.near", "ayelen.near", "martinbarba.near"];
+  } else {
+    return ["blaze.near"];
+  }
+}
+
+function libCall(call) {
+  if (call.functionName === "getComments") {
+    return getComments(call.props);
+  } else if (call.functionName === "setComment") {
+    return setComment(call.props);
+  } else if (call.functionName === "getWritersWhitelist") {
+    return getWritersWhitelist(call.props.env);
+  }
+}
+
+function getComments(args) {
+  const { realArticleId } = args;
+  const key = realArticleId;
+  return Social.index(action, key);
+}
+
+function setComment(args) {
+  const { realArticleId, text, previousCommentId } = args;
+  const data = {
+    index: {
+      [action]: JSON.stringify({
+        key: realArticleId,
+        value: {
+          text,
+          id: `${realArticleId}-${Date.now()}`,
+          previousCommentId,
+        },
+      }),
+    },
+  };
+  Social.set(data);
+
+  resultLibCalls = resultLibCalls.filter((call) => {
+    return call.functionName !== "setComment";
+  });
+
+  return text;
+}
+
+const updateObj = {};
+const resultLibCalls = [...libCalls];
+libCalls.forEach((call) => {
+  updateObj[call.key] = libCall(call);
+});
+
+updateObj.libCalls = resultLibCalls(updateObj);
+stateUpdate;
+return <></>;
