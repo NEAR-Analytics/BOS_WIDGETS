9cc49c626287b22565773d9b97b26e3b9e6d9205
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-08 01:17:09 +0000

    Update NDC.NavBar by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-08T01:17:09.905Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.NavBar/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.NavBar/source_code.jsx
@@ -177,6 +177,7 @@ return (
         <ul className="navbar-nav">
           {pills &&
             pills.map((pill, i) => {
+              console.log(pill);
               return !(pill.id + "") || !pill.title ? (
                 <p className="text-danger border">Pill passed wrong</p>
               ) : (
