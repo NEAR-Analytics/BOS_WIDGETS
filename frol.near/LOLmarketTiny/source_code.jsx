c4b4b238eb718dd88448e667bd057a580d842fd2
frol.near <frol.near>
2023-08-08 01:23:00 +0000

    Update LOLmarketTiny by frol.near at 2023-08-08T01:23:00.516Z

--- frol.near/LOLmarketTiny/source_code.jsx
+++ frol.near/LOLmarketTiny/source_code.jsx
@@ -5,8 +5,56 @@ if (!tokens || !tokensOnSale) {
   return <></>;
 }
 
+const sellForOne = (tokenId) => {
+  Near.call(
+    "lolmarket.qbit.near",
+    "nft_put_on_sale",
+    {
+      token_id: tokenId,
+      price: "100",
+    },
+    75000000000000,
+    0 //1
+  );
+};
+
 return (
   <>
-    {JSON.stringify(tokens)} {JSON.stringify(tokens)}
+    <div className="row">
+      {Object.entries(tokensOnSale).map(([tokenId, price]) => (
+        <div className="col col-sm">
+          <Widget
+            src="mob.near/widget/NftImage"
+            props={{
+              contractId: "lolmarket.qbit.near",
+              tokenId,
+            }}
+          />
+          <button onClick={() => sellForOne(token.token_id)}>
+            Buy for {parseFloat(price) / 100} LOL
+          </button>
+        </div>
+      ))}
+    </div>
+
+    <div className="row">
+      {tokens.map((token) => (
+        <div className="col col-sm">
+          <Widget
+            src="mob.near/widget/NftImage"
+            props={{
+              //   nft: {
+              contractId: "lolmarket.qbit.near",
+              tokenId: token.token_id,
+              //tokenMetadata: { media: token.metadata.media },
+              //   },
+            }}
+          />
+          <button onClick={() => sellForOne(token.token_id)}>
+            Sell for 1 LOL
+          </button>
+        </div>
+      ))}
+    </div>
   </>
 );
