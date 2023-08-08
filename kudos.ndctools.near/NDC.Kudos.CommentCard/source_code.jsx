3d5608a53c66ca267788caac4945ed5903559a4b
kudos.ndctools.near <kudos.ndctools.near>
2023-08-08 11:11:35 +0000

    Update NDC.Kudos.CommentCard by kudos.ndctools.near at 2023-08-08T11:11:35.437Z

--- kudos.ndctools.near/NDC.Kudos.CommentCard/source_code.jsx
+++ kudos.ndctools.near/NDC.Kudos.CommentCard/source_code.jsx
@@ -125,7 +125,7 @@ const base64decode = (encodedValue) => {
 };
 
 const formatMsg = (message) =>
-  message.replace(/\\u([0-9A-F]{4})/gi, (_, g) =>
+  message.replace(/\\\\u([0-9A-F]{4})/gi, (_, g) =>
     String.fromCharCode(`0x${g}`)
   );
 
