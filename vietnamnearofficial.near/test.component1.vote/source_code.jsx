7ee87e56ac7f2d13cf128a0e7036852c729e1e84
vietnamnearofficial.near <vietnamnearofficial.near>
2023-08-08 03:00:31 +0000

    Update test.component1.vote by vietnamnearofficial.near at 2023-08-08T03:00:31.103Z

--- vietnamnearofficial.near/test.component1.vote/source_code.jsx
+++ vietnamnearofficial.near/test.component1.vote/source_code.jsx
@@ -1,226 +1,458 @@
-const accountId = context.accountId;
-const daoId = props.daoId ?? "nearvietnamofficial.near";
-const vote_counts = props.proposal.vote_counts ?? {
-  // yes, no, abstain
-  community: [7, 1, 3],
-  council: [3, 2, 5],
-};
+if (
+  state.networkId === undefined &&
+  ethers !== undefined &&
+  Ethers.send("eth_requestAccounts", [])[0]
+) {
+  Ethers.provider()
+    .getNetwork()
+    .then((chainIdData) => {
+      if (chainIdData?.chainId) {
+        State.update({ networkId: chainIdData.chainId });
+      }
+    });
+}
+if (state.networkId !== undefined && state.networkId !== 1) {
+  return <p>Switch to Ethereum Mainnet</p>;
+}
 
-const userVote = props.proposal.votes[accountId];
-const canVote =
-  true || (!userVote && props.proposal.status === "In Progress" && accountId);
-const yesWin = props.proposal.status === "Approved";
-const noWin = props.proposal.status === "Rejected";
-
-let totalYesVotes = 0;
-let totalNoVotes = 0;
-let totalAbstainVotes = 0;
-Object.keys(vote_counts).forEach((key) => {
-  totalYesVotes += vote_counts[key][0];
-  totalNoVotes += vote_counts[key][1];
-  totalAbstainVotes += vote_counts[key][2];
+State.init({
+  unwrap: false,
+  balanceToken: null,
+  balanceWrapToken: null,
+  intervalStarted: false,
+  amountIn: null,
+  amountOut: null,
+  swapButtonText: null,
+  swapReady: false,
+  sender: null,
 });
-const totalVotes = totalYesVotes + totalNoVotes + totalAbstainVotes;
 
-// Functions
+const ownerId = "manzanal.near";
+const wethAddress =
+  props.wethAddress || "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
+const tokenDecimals = 18;
+const wethAbiUrl =
+  props.wethAbiUrl ||
+  "https://raw.githubusercontent.com/leomanza/chain-agnostic-bos-workshop/main/ABIs/WETH.abi.json";
+const wethAbi = fetch(wethAbiUrl);
 
-const handleApprove = () => {
-  Near.call([
-    {
-      contractName: daoId,
-      methodName: "act_proposal",
-      args: {
-        id: JSON.parse(props.proposal.id),
-        action: "VoteApprove",
-      },
-      gas: 100000000000000,
-    },
-  ]);
+if (!wethAbi.ok) {
+  return "Loading";
+}
+const wethAbiBody = wethAbi.body;
+const signer = Ethers.provider() ? Ethers.provider().getSigner() : null;
+const wethContract = new ethers.Contract(wethAddress, wethAbiBody, signer);
+
+if (!state.sender || state.sender === null) {
+  const accounts = Ethers.send("eth_requestAccounts", []);
+  if (accounts.length) {
+    State.update({ sender: accounts[0] });
+  }
+}
+
+const parseToUnits = (amount) => {
+  const tokenDecimals = 18;
+  return ethers.utils.parseUnits(amount, tokenDecimals);
+};
+const formatUnitsFn = (amount) => {
+  const tokenDecimals = 18;
+  return ethers.utils.formatUnits(amount, tokenDecimals);
+};
+const isValidAmout = (amount) => {
+  if (!amount) return false;
+  if (isNaN(Number(amount))) return false;
+  if (amount === "") return false;
+  if (amount < 0) return false;
+  return true;
 };
 
-const handleReject = () => {
-  Near.call([
-    {
-      contractName: daoId,
-      methodName: "act_proposal",
-      args: {
-        id: JSON.parse(props.proposal.id),
-        action: "VoteReject",
-      },
-      gas: 100000000000000,
-    },
-  ]);
+const imgWrapTokenSvg = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+    <g fill="none" fill-rule="evenodd">
+      <circle cx="16" cy="16" r="16" fill="#627EEA" />
+      <g fill="#FFF" fill-rule="nonzero">
+        <path fill-opacity=".602" d="M16.498 4v8.87l7.497 3.35z" />
+        <path d="M16.498 4L9 16.22l7.498-3.35z" />
+        <path fill-opacity=".602" d="M16.498 21.968v6.027L24 17.616z" />
+        <path d="M16.498 27.995v-6.028L9 17.616z" />
+        <path fill-opacity=".2" d="M16.498 20.573l7.497-4.353-7.497-3.348z" />
+        <path fill-opacity=".602" d="M9 16.22l7.498 4.353v-7.701z" />
+      </g>
+    </g>
+  </svg>
+);
+const imgTokenSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.2"
+    viewBox="0 0 21 21"
+    width="35"
+    height="35"
+  >
+    <path
+      fill-rule="evenodd"
+      d="m10.7 21c-5.8 0-10.5-4.7-10.5-10.5 0-5.8 4.7-10.5 10.5-10.5 5.7 0 10.4 4.7 10.4 10.5 0 5.8-4.7 10.5-10.4 10.5zm3.9-9.6l-4.1 2.4-4-2.4 4 5.8zm0-0.8l-4-6.8-4 6.8 4 2.3z"
+    />
+  </svg>
+);
+
+const updateBalances = (address) => {
+  if (!address) return;
+  Ethers.provider()
+    .getBalance(address)
+    .then((balance) =>
+      State.update({
+        balanceToken: balance,
+      })
+    );
+
+  wethContract.balanceOf(address).then((balance) =>
+    State.update({
+      balanceWrapToken: balance,
+    })
+  );
 };
 
-const handleAbstain = () => {
-  Near.call([
-    {
-      contractName: daoId,
-      methodName: "act_proposal",
-      args: {
-        id: JSON.parse(props.proposal.id),
-        action: "VoteRemove",
-      },
-      gas: 100000000000000,
-    },
-  ]);
+if (!state.intervalStarted) {
+  State.update({ intervalStarted: true });
+  updateBalances(state.sender);
+
+  setInterval(() => {
+    updateBalances(state.sender);
+  }, 2000);
+}
+
+const updateSwapButton = () => {
+  State.update({ swapReady: false });
+  if (!!state.sender) {
+    State.update({ swapButtonText: "Connect Wallet" });
+    return;
+  }
+
+  if (!state.amountIn) {
+    return;
+  }
+
+  let amountIn = state.amountIn;
+  if (!isValidAmout(amountIn)) {
+    State.update({ swapButtonText: "Invalid Amount" });
+    return;
+  }
+
+  let limit = state.unwrap ? state.balanceWrapToken : state.balanceToken;
+
+  if (limit && amountIn < limit) {
+    State.update({
+      swapButtonText: state.unwrap ? `Unwrap ETH` : `Wrap ETH`,
+      swapReady: true,
+    });
+  } else {
+    State.update({
+      swapButtonText: `Insufficient ${state.unwrap ? "WETH" : "ETH"} Balance`,
+    });
+  }
 };
 
-const VoteButton = styled.button`
-  border-radius: 20px;
-  border: none;
+const swapInputOnChange = (event) => {
+  let re = /^[0-9]*[.,]?[0-9]*$/;
+  if (re.test(event.target.value)) {
+    State.update({
+      amountIn: event.target.value,
+      amountOut: event.target.value,
+      swapButtonText: null,
+    });
+
+    updateSwapButton();
+  }
+};
+
+const swapButtonOnClick = () => {
+  const amount = parseToUnitsFn(amountIn);
+  if (state.unwrap) {
+    wethContract.withdraw(amount);
+  } else {
+    wethContract.deposit({ value: amount });
+  }
+};
+
+const Card = styled.div`
+  font-family: 'Inter custom',sans-serif;
+  font-size: 16px;
+  font-variant: none;
+  -webkit-font-smoothing: antialiased;
+  -webkit-tap-highlight-color: transparent;
+  color: rgb(255, 255, 255);
+  box-sizing: border-box;
+ 
+  background: rgb(13, 17, 28);
+  border-radius: 16px;
+  border: 1px solid rgb(27, 34, 54);
+  padding: 8px;
+  z-index: 1;
+  transition: transform 250ms ease 0s;
+  display: block;
+`;
+
+const SwapContainer = styled.div`
+  background-color: rgb(19, 26, 42);
+  border-radius: 12px;
+  padding: 16px;
+`;
+
+const SwapContainerOuter = styled.div`
   display: flex;
-  padding: 0;
+  flex-flow: column nowrap;
+  border-radius: 20px;
+`;
+
+const SwapContainerInner = styled.div`
+  border-radius: 20px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SwapInput = styled.input`
+  color: rgb(255, 255, 255);
   position: relative;
-  background: #f3f3f2;
-  width: 100%;
-  margin-bottom: 14px;
-  cursor: pointer;
+  font-weight: 400;
+  outline: none;
+  border: none;
+  flex: 1 1 auto;
+  background-color: transparent;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 0px;
+  appearance: textfield;
+  filter: none;
+  opacity: 1;
+  transition: opacity 0.2s ease-in-out 0s;
+  text-align: left;
+  font-size: 36px;
+  line-height: 44px;
+  font-variant: small-caps;
+`;
 
-  .button {
-    border-radius: 20px;
-    padding: 12px 20px;
-    text-align: left;
-    font-weight: 600;
-    font-size: 15px;
-    transition: all 0.4s ease-in-out;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    min-width: 90px;
-    width: 90px;
-
-    @media (max-width: 600px) {
-      justify-content: start;
-    }
-  }
-
-  .vote {
-    opacity: 0;
-    transition: all 0.4s ease-in-out;
-    max-width: 0;
-    display: block;
-    margin-right: 3px;
-    position: relative;
-    z-index: 1;
-  }
-
-  .votes {
-    text-align: right;
-    padding: 12px 16px;
-    position: absolute;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    color: rgb(27, 27, 24);
-  }
-
-  .preview {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    border-radius: 20px;
-    transition: all 0.4s ease-in-out;
-    z-index: 0;
-  }
-
-  &:hover {
-    .button {
-      width: 100%;
-    }
-    .vote {
-      opacity: 1;
-      max-width: 100px;
-    }
-  }
-
-  &[disabled] {
-    cursor: not-allowed;
-    opacity: 0.8;
-
-    .vote {
-      opacity: 0;
-      max-width: 0;
-    }
-
-    &.yes .button {
-      width: ${(totalYesVotes / totalVotes) * 100 || 0}%;
-    }
-    &.no .button {
-      width: ${(totalNoVotes / totalVotes) * 100 || 0}%;
-    }
-    &.abstain .button {
-      width: ${(totalAbstainVotes / totalVotes) * 100 || 0}%;
-    }
-  }
-
-  &.yes {
-    .button {
-      background-color: #59e662;
-      color: #000;
-    }
-    .preview {
-      background-color: #54e69220;
-      width: ${(totalYesVotes / totalVotes) * 100 || 0}%;
-    }
-  }
-
-  &.no {
-    .button {
-      background-color: #BBBBBB;
-      color: #fff;
-    }
-    .preview {
-      background-color: #e5484d20;
-      width: ${(totalNoVotes / totalVotes) * 100 || 0}%;
-    }
-  }
-
-  &.abstain {
-    .button {
-      background-color: #CCFF00;
-      color: #000;
-    }
-    .preview {
-      background-color: #ffda1920;
-      width: ${(totalAbstainVotes / totalVotes) * 100 || 0}%;
-    }
+const SwapArrowContainer = styled.div`
+  border-radius: 12px;
+  height: 40px;
+  width: 40px;
+  position: relative;
+  margin: -18px auto;
+  background-color: rgb(41, 50, 73);
+  border: 4px solid rgb(13, 17, 28);
+  z-index: 2;
+ 
+  :hover {
+    opacity: 0.75;
   }
 `;
 
+const SwapArrowWrapper = styled.div`
+  display: inline-flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+`;
+
+const CurrencyPillContainer = styled.div`
+  text-decoration: none;
+  background-color: rgb(41, 50, 73);
+  color: rgb(255, 255, 255);
+  border-radius: 16px;
+  padding: 4px 8px 4px 4px;
+  margin-left: 12px;
+`;
+
+const CurrencyPillWrapper = styled.div`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: start;
+  justify-content: flex-start;
+`;
+
+const CurrencyPillImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CurrencyPillSvgImageContainer = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  margin-right: 2px;
+`;
+
+const CurrencyPillImage = styled.img`
+  color: rgb(255, 255, 255);
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  margin-right: 2px;
+`;
+
+const CurrencyPillText = styled.span`
+  margin: 0px 4px;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const SwapDetailsContainer = styled.div`
+  padding: 8px 0px 0px;
+`;
+
+const SwapDetailsWrapper = styled.div`
+  display: flex;
+  gap: 10;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: justify;
+  justify-content: end;
+`;
+
+const TextSmall = styled.div`
+  color: rgb(152, 161, 192);
+  line-height: 1rem;
+  box-sizing: border-box;
+  font-weight: 400;
+  font-size: 14px;
+`;
+
+const SwapButtonWrapper = styled.div`
+  margin-top: 12px;
+`;
+
+const SwapButton = styled.button`
+  background-color: ${(props) =>
+    props.disabled ? "rgb(41, 50, 73)" : "#1758FE"};
+  padding: 16px;
+  font-size: 20px;
+  font-weight: 600;
+  color: rgb(245, 246, 252);
+  width: 100%;
+  text-align: center;
+  border-radius: 20px;
+  outline: none;
+  border: 1px solid transparent;
+  text-decoration: none;
+  position: relative;
+  z-index: 1;
+  cursor: pointer;
+`;
+
+const TitleWrapper = styled.div`
+  padding: 8px 12px;
+  margin: 0px 0px 8px 0px;
+  color: rgb(255, 255, 255);
+  font-weight: 500;
+  font-size: 16px;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 32px;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+  `;
+
+const renderSwapContainer = (
+  state,
+  showTokenSvg,
+  showTokenName,
+  swapInputOnChange
+) => (
+  <SwapContainer>
+    <SwapContainerOuter>
+      <SwapContainerInner>
+        <InputContainer>
+          <SwapInput
+            inputmode="decimal"
+            autocomplete="off"
+            autocorrect="off"
+            type="text"
+            placeholder="0"
+            minLength="1"
+            maxLength="79"
+            spellcheck="false"
+            value={state.amountIn}
+            onChange={swapInputOnChange}
+          />
+          <CurrencyPillContainer>
+            <CurrencyPillWrapper>
+              <CurrencyPillImageWrapper>
+                <CurrencyPillSvgImageContainer>
+                  {showTokenSvg()}
+                </CurrencyPillSvgImageContainer>
+                <CurrencyPillText>{showTokenName()}</CurrencyPillText>
+              </CurrencyPillImageWrapper>
+            </CurrencyPillWrapper>
+          </CurrencyPillContainer>
+        </InputContainer>
+        <SwapDetailsContainer>
+          <SwapDetailsWrapper>
+            {state.unwrap ? (
+              state.balanceWrapToken ? (
+                <TextSmall>Balance: {state.balanceWrapToken}</TextSmall>
+              ) : null
+            ) : state.balanceToken ? (
+              <TextSmall>Balance: {state.balanceToken}</TextSmall>
+            ) : null}
+          </SwapDetailsWrapper>
+        </SwapDetailsContainer>
+      </SwapContainerInner>
+    </SwapContainerOuter>
+  </SwapContainer>
+);
+
 return (
-  <>
-    <VoteButton onClick={handleApprove} disabled={!canVote} className="yes">
-      <span className="button">
-        <span className="vote">Vote </span> Yes
-      </span>
-      <span className="votes">
-        <span className="preview" />
-        {totalYesVotes} Votes (
-        {Math.round((totalYesVotes / totalVotes) * 100 || 0)}%)
-      </span>
-    </VoteButton>
-    <VoteButton onClick={handleReject} disabled={!canVote} className="no">
-      <span className="button no">
-        <span className="vote">Vote </span> No
-      </span>
-      <span className="votes">
-        <span className="preview" />
-        {totalNoVotes} Votes (
-        {Math.round((totalNoVotes / totalVotes) * 100 || 0)}%)
-      </span>
-    </VoteButton>
-    <VoteButton onClick={handleAbstain} disabled={!canVote} className="abstain">
-      <span className="button abstain">
-        <span className="vote">Vote </span> Abstain
-      </span>
-      <span className="votes">
-        <span className="preview" />
-        {totalAbstainVotes} Votes (
-        {Math.round((totalAbstainVotes / totalVotes) * 100 || 0)}%)
-      </span>
-    </VoteButton>
-  </>
+  <Wrapper>
+    <Card>
+      <TitleWrapper>
+        <p style={{ margin: 0 }}>ETH/WETH Wrapper</p>
+      </TitleWrapper>
+      {renderSwapContainer(
+        state,
+        () => (state.unwrap ? imgWrapTokenSvg : imgTokenSvg),
+        () => (state.unwrap ? "WETH" : "ETH"),
+        swapInputOnChange
+      )}
+      <SwapArrowContainer
+        onClick={() => {
+          State.update({
+            unwrap: !state.unwrap,
+            amountIn: state.amountOut,
+            amountOut: state.amountIn,
+          });
+          updateSwapButton();
+        }}
+      >
+        <SwapArrowWrapper>
+          <i class="bi bi-arrow-down-short" />
+        </SwapArrowWrapper>
+      </SwapArrowContainer>
+      {renderSwapContainer(
+        state,
+        () => (!state.unwrap ? imgWrapTokenSvg : imgTokenSvg),
+        () => (!state.unwrap ? "WETH" : "ETH"),
+        swapInputOnChange
+      )}
+      <SwapButtonWrapper>
+        <SwapButton disabled={!state.swapReady} onClick={swapButtonOnClick}>
+          {state.swapButtonText ??
+            `Enter ${state.unwrap ? "WETH" : "ETH"} Amount`}
+        </SwapButton>
+      </SwapButtonWrapper>
+    </Card>
+  </Wrapper>
 );
