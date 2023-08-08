76847c592a1e8b1de64ceda2efb88bc1b84422c6
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-08 15:53:18 +0000

    Update DiscoverPage by relayer.pagodaplatform.near at 2023-08-08T15:53:18.353Z

--- relayer.pagodaplatform.near/DiscoverPage/source_code.jsx
+++ relayer.pagodaplatform.near/DiscoverPage/source_code.jsx
@@ -2,18 +2,27 @@ const DiscoverPage = styled.div`
     background-image: src()
 `;
 
+const HightLightText = styled.h1`
+  background: -webkit-linear-gradient(#0019FF, #931FD4);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
 return (
-  <div className="w-100 container-fluid">
+  <div className="w-100 container-fluid text">
     <div className="d-flex flex-row row">
-      <div className="col-lg-4">
+      <div className="col-lg-5">
         <img
           style={{ maxWidth: "100%" }}
-          src={`https://ipfs.near.social/ipfs/bafybeifemkrxbsify74bvnvxklukkz4q7om4cpo7cvtm6igdsqducp345u`}
+          src={`https://ipfs.near.social/ipfs/bafybeiejengae7z4qa3uxq6yk7pttdqfq5ryasaoyillpkabfqhbzckzta`}
           alt="uploaded"
         />
       </div>
-      <div className="d-flex flex-column col-lg-8 justify-content-center">
-        <h1>Welcome to CLOUDY</h1>
+      <div className="d-flex flex-column col-lg-7 justify-content-center">
+        <span className="mb-5 d-flex flex-row">
+          <h1 className="me-2">Welcome To</h1>
+          <HightLightText>WeData</HightLightText>
+        </span>
         <p>
           Cloudy is a free platform for everyone to share, exchange and
           contribute the data. Cloudy provides a large storage capacity beyond
