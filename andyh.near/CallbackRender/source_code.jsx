d6082c57f6be4b01d5e51af606b277fbb7ac0faa
andyh.near <andyh.near>
2023-08-08 23:32:08 +0000

    Update CallbackRender by andyh.near at 2023-08-08T23:32:08.687Z

--- andyh.near/CallbackRender/source_code.jsx
+++ andyh.near/CallbackRender/source_code.jsx
@@ -34,5 +34,31 @@ return (
         },
       }}
     />
+    {state.apps && (
+      <div className="mb-2">
+        {state.apps.map((app, i) => (
+          <div key={i}>
+            <Widget
+              src="andyh.near/widget/ComponentSearch.Item"
+              props={{
+                link: `#/${app.widgetSrc}`,
+                accountId: app.accountId,
+                widgetName: app.widgetName,
+                onHide: () => State.update({ apps: null }),
+                extraButtons: ({ widgetPath }) => (
+                  <a
+                    target="_blank"
+                    className="btn btn-outline-secondary"
+                    href={`#/mob.near/widget/WidgetSource?src=${widgetPath}`}
+                  >
+                    Source
+                  </a>
+                ),
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    )}
   </div>
 );
