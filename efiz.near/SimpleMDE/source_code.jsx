3147b17f3c02f073d5d70d5cb2972d02a1c777c3
efiz.near <efiz.near>
2023-08-08 18:51:36 +0000

    Update SimpleMDE by efiz.near at 2023-08-08T18:51:36.063Z

--- efiz.near/SimpleMDE/source_code.jsx
+++ efiz.near/SimpleMDE/source_code.jsx
@@ -1,7 +1,14 @@
+/**
+ * iframe embedding a SimpleMDE component
+ * https://github.com/sparksuite/simplemde-markdown-editor
+ */
 const data = props.data;
 const onChange = props.onChange;
 const height = props.height ?? "500px";
 
+function togglePreview() {
+  State.update({ showPreview: !state.showPreview });
+}
 const code = `
 <style>
 #react-root {
@@ -21,19 +28,64 @@ const code = `
 function MarkdownEditor(props) {
     const [value, setValue] = React.useState(props.initialText || "");
     React.useEffect(() => {
+        // edit options here
         const simplemde = new SimpleMDE({
-            element: document.getElementById('markdown-input'),
+            element: document.getElementById("markdown-input"),
             autofocus: true,
             renderingConfig: {
                 singleLineBreaks: false,
                 codeSyntaxHighlighting: true,
             },
-            showIcons: ["code", "table"],
+            toolbar: ["heading", "bold", "italic", "quote", "code", "link", "unordered-list", "ordered-list",
+                {
+                    name: "custom",
+                    action: function customFunction(editor){
+                        
+                        const cursorPos = editor.codemirror.getCursor();
+                        const lineText = editor.codemirror.getLine(cursorPos.line);
+                        if (lineText.trim() === "") {
+                            editor.codemirror.replaceRange('- [ ] ', cursorPos);
+                        } else {
+                            editor.codemirror.replaceRange('\\n - [ ] ', cursorPos);
+                        }
+                    },
+                    className: "fa fa-star",
+                    title: "Custom Button",
+                },
+                {
+                    name: 'upload',
+                    action: () => this.showDialogUpload(),
+                    className: 'fa fa-upload',
+                    title: 'Upload File',
+                },
+                {
+                    name: 'table',
+                    action: SimpleMDE.drawTable,
+                    className: 'fa fa-table',
+                    title: 'Insert Table',
+                },
+                {
+                    name: 'preview',
+                    action: () => {window.parent.postMessage({ handler: "preview" }, "*")},
+                    className: 'fa fa-eye no-disable',
+                    title: 'Toggle Preview',
+                }, {
+                    name: 'side-by-side',
+                    action: SimpleMDE.toggleSideBySide,
+                    className: 'fa fa-columns no-disable ',
+                    title: 'Toggle Side by Side',
+                }, {
+                    name: 'fullscreen',
+                    action: SimpleMDE.toggleFullScreen,
+                    className: 'fa fa-arrows-alt no-disable',
+                    title: 'Toggle Fullscreen',
+                }   
+            ],
         });
 
         simplemde.codemirror.on('change', () => {
             const content = simplemde.value();
-            window.parent.postMessage({ content }, "*");
+            window.parent.postMessage({ handler: "update", content }, "*");
         });
 
         setValue(props.initialText || "");
@@ -53,15 +105,33 @@ window.addEventListener("message", (event) => {
 </script>
 `;
 return (
-  <iframe
-    className="w-100"
-    style={{
-      height: height,
-    }}
-    srcDoc={code}
-    message={data.content ?? ""}
-    onMessage={(event) => {
-      onChange({ content: event.data.content });
-    }}
-  />
+  <>
+    {state.showPreview ? (
+      <Widget
+        src="efiz.near/widget/SocialMarkdown"
+        props={{ text: state.text }}
+      />
+    ) : (
+      <iframe
+        className="w-100"
+        style={{
+          height: height,
+        }}
+        srcDoc={code}
+        message={data.content ?? ""}
+        onMessage={(e) => {
+          console.log(e.handler);
+          switch (e.handler) {
+            case "update": {
+              State.update({ text: event.content });
+              //   onChange({ content: event.content });
+            }
+            case "preview": {
+              togglePreview();
+            }
+          }
+        }}
+      />
+    )}
+  </>
 );
