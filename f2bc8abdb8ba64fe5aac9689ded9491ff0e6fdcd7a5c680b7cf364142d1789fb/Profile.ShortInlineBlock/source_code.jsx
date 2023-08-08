5a6f0c8944e068af2effed6146e1baef9dd398b8
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-08 02:10:48 +0000

    Update Profile.ShortInlineBlock by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-08T02:10:48.509Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Profile.ShortInlineBlock/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Profile.ShortInlineBlock/source_code.jsx
@@ -2,28 +2,26 @@ const accountId = props.accountId;
 
 const profile = props.profile ?? Social.getr(`${accountId}/profile`);
 
-const maxNameLength = props.maxNameLength;
+const maxNameLength = props.maxNameLength ?? 12;
 
-const widgets = props.widgets;
-
-let name = profile.name;
+const name =
+  profile.name.slice(0, maxNameLength) ?? accountId.slice(0, maxNameLength);
 
 const maxWidth = props.maxWidth ?? "60%";
+
+const widgetOwner =
+  props.widgetOwner ??
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 
 const inner = (
   <div className="d-flex flex-row" style={{ maxWidth: "100%" }}>
     <Widget
-      src={widgets.views.standardWidgets.profileImage}
+      src="mob.near/widget/ProfileImage"
       props={{
         metadata,
         accountId,
         widgetName,
-        style: {
-          height: "2.5em",
-          width: "2.5em",
-          minWidth: "2.5em",
-          borderRadius: "20px",
-        },
+        style: { height: "2.5em", width: "2.5em", minWidth: "2.5em" },
         className: "me-2",
       }}
     />
@@ -32,11 +30,7 @@ const inner = (
         className="text-truncate fw-bold"
         style={{ maxWidth: "90%", textAlign: "start" }}
       >
-        {name
-          ? name
-          : maxNameLength
-          ? accountId.slice(0, maxNameLength) + "..."
-          : accountId}
+        {name}
       </div>
       <div className="text-truncate text-muted" style={{ maxWidth: "90%" }}>
         <small>
@@ -54,7 +48,7 @@ return (
   >
     {props.tooltip ? (
       <Widget
-        src={widgets.standardWidgets.profileOverlayTrigger}
+        src={`${widgetOwner}/widget/Profile.OverlayTrigger`}
         props={{ accountId, children: inner, maxWidth: `${maxWidth}` }}
       />
     ) : (
