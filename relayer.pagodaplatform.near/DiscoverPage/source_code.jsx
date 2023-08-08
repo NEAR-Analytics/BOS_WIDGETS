381609d28aaa7a70f2087e6814f49911378df6e0
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-08 14:45:06 +0000

    Update DiscoverPage by relayer.pagodaplatform.near at 2023-08-08T14:45:06.546Z

--- relayer.pagodaplatform.near/DiscoverPage/source_code.jsx
+++ relayer.pagodaplatform.near/DiscoverPage/source_code.jsx
@@ -1,1 +1,20 @@
-return <div>Hello World</div>;
+const DiscoverPage = styled.div`
+    background-image: src()
+`;
+
+return (
+  <div className="w-100">
+    <div className="d-flex flex-row">
+      <div></div>
+      <div className="d-flex flex-column">
+        <h1>Welcome to CLOUDY</h1>
+        <p>
+          Cloudy is a free platform for everyone to share, exchange and
+          contribute the data. Cloudy provides a large storage capacity beyond
+          that of blockchain. Still decentralized - totally. And sharing
+          instantly.
+        </p>
+      </div>
+    </div>
+  </div>
+);
