3177996f7aabf0bfa7bfd237dfc09566c0320295
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-09 10:32:46 +0000

    Update Onboarding.Starter by relayer.pagodaplatform.near at 2023-08-09T10:32:46.159Z

--- relayer.pagodaplatform.near/Onboarding.Starter/source_code.jsx
+++ relayer.pagodaplatform.near/Onboarding.Starter/source_code.jsx
@@ -1,11 +1,5 @@
-let greeting = "Have a great day";
-
 return (
   <>
-    <div class="container border border-info p-3 text-center">
-      <h1>Hello {props.name}</h1>
-
-      <p> {greeting} </p>
-    </div>
+    <div>Hello World</div>
   </>
 );
