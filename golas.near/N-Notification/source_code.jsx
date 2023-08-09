1ec5dca7c4bf7f7fa2e2a511f5948c2169339825
golas.near <golas.near>
2023-08-09 19:55:06 +0000

    Update N-Notification by golas.near at 2023-08-09T19:55:06.480Z

--- golas.near/N-Notification/source_code.jsx
+++ golas.near/N-Notification/source_code.jsx
@@ -8,46 +8,25 @@ const componentName = props.componentName;
 const timestamp = props.timestamp;
 const desc = props.desc;
 
-const Avatar = styled.a`
-  > img {
-    width: 60px;
-    height: 60px;
-    flex-shrink: 0;
-    border: 1px solid #eceef0;
-    overflow: hidden;
-    border-radius: 56px;
-    transition: border-color 200ms;
-
-    img {
-      object-fit: cover;
-      width: 100%;
-      height: 100%;
-    }
-
-    &:hover,
-    &:focus {
-      border-color: #d0d5dd;
-    }
-  }
+const Username = styled.span`
+  font: var(--text-s);
+  font-weight: 600;
 `;
 
 return (
   <div>
     <div>
-      <Avatar href={profileUrl}>
-        <Widget
-          src="mob.near/widget/Image"
-          props={{
-            image: profile.image,
-            alt: profile.name,
-            fallbackUrl:
-              "https://ipfs.near.social/ipfs/bafkreibiyqabm3kl24gcb2oegb7pmwdi6wwrpui62iwb44l7uomnn3lhbi",
-          }}
-        />
-      </Avatar>
+      <Widget
+        src="near/widget/DIG.Avatar"
+        props={{
+          alt: accountId,
+          image: profile.image,
+          size: "small",
+        }}
+      />
     </div>
     <div>
-      <span>{username}</span>
+      <Username>{username}</Username>
       <span>{action}</span>
       <span>{componentName}</span>
       <span>{timestamp}</span>
