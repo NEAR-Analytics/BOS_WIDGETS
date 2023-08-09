634b5955bd4f8831019a6429660a953291949fde
andyh.near <andyh.near>
2023-08-09 00:03:45 +0000

    Update CallbackRenderChild by andyh.near at 2023-08-09T00:03:45.442Z

--- andyh.near/CallbackRenderChild/source_code.jsx
+++ andyh.near/CallbackRenderChild/source_code.jsx
@@ -2,9 +2,11 @@ console.log({ props });
 const component = props.renderComponent("i am the child", () =>
   console.log("all done")
 );
+const overlaid = <h2>overlaid!!</h2>;
 return (
   <div>
     something really cool is loading:
     {component}
+    <OverlayTrigger>{overlaid}</OverlayTrigger>
   </div>
 );
