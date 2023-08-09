1d343f5dc6c2b15a5bfc3b46570ca03ad10ab561
dangquangvu.near <dangquangvu.near>
2023-08-09 05:14:47 +0000

    Update LoginWithCss by dangquangvu.near at 2023-08-09T05:14:47.192Z

---
+++ dangquangvu.near/LoginWithCss/source_code.jsx
@@ -0,0 +1,65 @@
+const sender = Ethers.send("eth_requestAccounts", [])[0];
+
+const ContainerLogin = styled.div`
+  .web3-connect{
+    width:480px;
+    height:60px;
+    border-radius:10px;
+    background-color:#f22;
+    color:#0F1126;
+    font-size:18px;
+    font-weight:500;
+    border:none;
+    margin-top:20px;
+  }
+  .address{
+    border-radius:10px;
+    background-color:#f55f12;
+    color:#f1f1f1;
+    font-size:14px;
+    font-weight:500;
+    border:none;
+    padding: 8px 40px;
+  }
+`;
+
+if (!sender)
+  return (
+    <ContainerLogin
+      style={{
+        display: "flex",
+        maxWidth: "500px",
+        flexDirection: "column",
+        margin: "80px auto auto auto",
+      }}
+    >
+      <img src="https://ipfs.near.social/ipfs/bafkreibmhq4fseqpcbywiq4hfojghxvhj47mjsri2trggt7d5h5od4y6kq"></img>
+
+      <Web3Connect className="web3-connect" connectLabel="Connect ETH wallet" />
+    </ContainerLogin>
+  );
+
+return (
+  <div>
+    <ContainerLogin
+      style={{
+        display: "flex",
+        maxWidth: "500px",
+        flexDirection: "column",
+        margin: "80px auto auto auto",
+      }}
+    >
+      <Web3Connect className="web3-connect" connectLabel="Connect ETH wallet" />
+      <div
+        style={{
+          display: "flex",
+          maxWidth: "500px",
+          flexDirection: "column",
+          margin: "20px auto auto auto",
+        }}
+      >
+        <h5 className="address">{sender}</h5>
+      </div>
+    </ContainerLogin>
+  </div>
+);
