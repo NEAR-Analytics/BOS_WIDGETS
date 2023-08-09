18cb16513e2fab588a352d4a96e519f607475527
kenrou-it.near <kenrou-it.near>
2023-08-09 21:22:35 +0000

    Update SayALot.lib.article by kenrou-it.near at 2023-08-09T21:22:35.666Z

--- kenrou-it.near/SayALot.lib.article/source_code.jsx
+++ kenrou-it.near/SayALot.lib.article/source_code.jsx
@@ -80,7 +80,10 @@ function getArticlesIndexes(props) {
 }
 
 function getArticleBlackListByBlockHeight() {
-  return [91092435, 91092174, 91051228, 91092223, 91051203, 98372095];
+  return [
+    91092435, 91092174, 91051228, 91092223, 91051203, 98372095, 96414482,
+    96412953,
+  ];
 }
 
 function getArticleBlackListByRealArticleId() {
