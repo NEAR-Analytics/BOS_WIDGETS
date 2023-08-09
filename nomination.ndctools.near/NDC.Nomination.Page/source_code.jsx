782402bc707c2fb8dc14455454b39bbb6fe13e7e
nomination.ndctools.near <nomination.ndctools.near>
2023-08-09 07:36:03 +0000

    Update NDC.Nomination.Page by nomination.ndctools.near at 2023-08-09T07:36:03.856Z

--- nomination.ndctools.near/NDC.Nomination.Page/source_code.jsx
+++ nomination.ndctools.near/NDC.Nomination.Page/source_code.jsx
@@ -87,71 +87,66 @@ function getVerifiedHuman() {
   });
 }
 
-function getNominationInfo(house) {
-  let nominationsArr = [];
+let objCard = {
+  profileData: [],
+  nominationData: [],
+  upVoteData: [],
+  indexerData: [],
+};
+let nominationsArr = [];
 
-  State.update({ loading: true });
-
+const getNominationInfo = (house) => {
   asyncFetch(endpoints.houseNominations(house), httpRequestOpt).then((res) => {
-    if (res.body.length <= 0) {
-      State.update({ nominations: [], loading: false });
+    nominationsArr = res.body.entries().map(([i, data]) => {
+      objCard.indexerData = data;
+      return objCard;
+    });
+  });
+};
+
+function getCommentsUpvotes(nominee) {
+  nominationsArr = [];
+
+  asyncFetch(
+    `${baseApi}/nominations/candidates-comments-and-upvotes?candidate=${nominee}&contract=${nominationContract}`,
+    httpRequestOpt
+  ).then((info) => {
+    let upVoteInfo = info.body[0];
+    let profileData;
+    let nominationData;
+
+    profileData = Social.getr(`${nominee}/profile`);
+    nominationData = Social.getr(`${nominee}/nominations`);
+
+    if (data.is_revoked || !profileData || !nominationData) {
+      State.update({ loading: false });
       return;
     }
 
-    console.log(res.body);
-
-    for (const [i, data] of res.body.entries()) {
-      let objCard = { indexerData: data };
-      let nominee = data.nominee;
-
-      asyncFetch(
-        `${baseApi}/nominations/candidates-comments-and-upvotes?candidate=${data.nominee}&contract=${nominationContract}`,
-        httpRequestOpt
-      ).then((info) => {
-        let upVoteInfo = info.body[0];
-        let profileData;
-        let nominationData;
-        Social.getr(`${nominee}/profile`);
-        Social.getr(`${nominee}/nominations`);
-        setTimeout(() => {
-          profileData = Social.getr(`${nominee}/profile`);
-          nominationData = Social.getr(`${nominee}/nominations`);
-        }, 1000);
-
-        setTimeout(() => {
-          if (data.is_revoked || !profileData || !nominationData) {
-            State.update({ loading: false });
-            return;
-          }
-
-          objCard = {
-            profileData: profileData,
-            nominationData: nominationData,
-            upVoteData: upVoteInfo,
-            ...objCard,
-          };
-          nominationsArr.push(objCard);
-
-          State.update({
-            nominations: nominationsArr,
-            originNominations: nominationsArr,
-            loading: false,
-          });
-        }, 1000);
-      });
-    }
+    objCard = {
+      profileData: profileData,
+      nominationData: nominationData,
+      upVoteData: upVoteInfo,
+      indexerData: data,
+    };
+    nominationsArr.push(objCard);
   });
 }
 
-if (state.start) {
-  getNominationInfo("HouseOfMerit");
-  getVerifiedHuman();
+getVerifiedHuman();
 
-  State.update({ start: false });
-}
+State.update({ loading: true });
+
+getNominationInfo("HouseOfMerit");
+state.nominations.map((data) => getCommentsUpvotes(data.nominee));
+
+State.update({
+  nominations: nominationsArr,
+  originNominations: nominationsArr,
+  loading: false,
+});
 
 const handleSelect = (item) => {
-  console.log("id", item.id);
   switch (item.id) {
     case 1:
       getNominationInfo("HouseOfMerit");
