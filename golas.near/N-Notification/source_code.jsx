f9a3414282ac5e545aa4c8c6f469f3aeac93ef6c
golas.near <golas.near>
2023-08-09 19:20:27 +0000

    Update N-Notification by golas.near at 2023-08-09T19:20:27.933Z

--- golas.near/N-Notification/source_code.jsx
+++ golas.near/N-Notification/source_code.jsx
@@ -1,12 +1,51 @@
+const accountId = props.accountId;
+const profile = props.profile || Social.get(`${accountId}/profile/**`, "final");
+const profileUrl = `#/near/widget/ProfilePage?accountId=${accountId}`;
+
 const username = props.username;
 const action = props.action;
 const componentName = props.componentName;
 const timestamp = props.timestamp;
 const desc = props.desc;
 
+const Avatar = styled.a`
+  > img {
+    width: 60px;
+    height: 60px;
+    flex-shrink: 0;
+    border: 1px solid #eceef0;
+    overflow: hidden;
+    border-radius: 56px;
+    transition: border-color 200ms;
+
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+
+    &:hover,
+    &:focus {
+      border-color: #d0d5dd;
+    }
+  }
+`;
+
 return (
   <div>
-    <div>icon</div>
+    <div>
+      <Avatar href={profileUrl}>
+        <Widget
+          src="mob.near/widget/Image"
+          props={{
+            image: profile.image,
+            alt: profile.name,
+            fallbackUrl:
+              "https://ipfs.near.social/ipfs/bafkreibiyqabm3kl24gcb2oegb7pmwdi6wwrpui62iwb44l7uomnn3lhbi",
+          }}
+        />
+      </Avatar>
+    </div>
     <div>
       <span>{username}</span>
       <span>{action}</span>
