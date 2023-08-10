96f3dee6f59e16cfe79fc4efcc9dab96be235563
harrydhillon.near <harrydhillon.near>
2023-08-10 12:36:45 +0000

    Update Keypom.Eventview by harrydhillon.near at 2023-08-10T12:36:45.176Z

---
+++ harrydhillon.near/Keypom.Eventview/source_code.jsx
@@ -0,0 +1,43 @@
+const TicketDropBackground = styled.div`
+background: lightgray;
+background: rgb(220,244,251);
+background: linear-gradient(0deg, rgba(220,244,251,1) 0%, rgba(251,254,255,1) 95%, rgba(255,255,255,1) 100%);
+`;
+
+const ContentDiv = styled.div`
+    padding:10px;
+    max-width: 1200px;
+    margin:auto;
+`;
+
+const Heading = styled.div`
+font-size: 30px;
+font-style: normal;
+font-weight: 500;
+margin-top:-10px;
+line-height: normal;
+letter-spacing: -0.72px;
+`;
+
+return (
+  <>
+    <TicketDropBackground>
+      <ContentDiv>
+        <Widget src="harrydhillon.near/widget/Keypom.Header" />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "25vh",
+          }}
+        >
+          <p style={{ fontSize: 22, fontWeight: "600" }}>My Event</p>
+        </div>
+      </ContentDiv>
+    </TicketDropBackground>
+    <div style={{ maxWidth: 500, margin: "auto", marginTop: 20 }}>
+      <p style={{ fontWeight: "600" }}>Event Details</p>
+    </div>
+  </>
+);
