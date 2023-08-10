29d2dc900a7df070d316777ca14b3abc19d2ab39
nearcon23.near <nearcon23.near>
2023-08-10 13:23:18 +0000

    Update Hackathon.Header by nearcon23.near at 2023-08-10T13:23:18.717Z

--- nearcon23.near/Hackathon.Header/source_code.jsx
+++ nearcon23.near/Hackathon.Header/source_code.jsx
@@ -1,11 +1,3 @@
 const desktopImage = "https://nearcon.s3.amazonaws.com/hackathon-hero.png";
-const mobileImage =
-  "https://nearcon.s3.amazonaws.com/hackathon-hero-mobile.png";
 
-const Header = styled.img`
-    @media (max-width: 650px) {    
-         content:url(${mobileImage});   
-    }
-`;
-
-return <Header src={desktopImage} style={{ width: "100%" }} />;
+return <img src={desktopImage} style={{ width: "100%" }} />;
