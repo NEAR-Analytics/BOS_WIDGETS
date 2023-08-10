e78bc9fba30cd5dac1922fb9dcc5bf17aeea19f3
harrydhillon.near <harrydhillon.near>
2023-08-10 11:41:46 +0000

    Update Keypom.Header by harrydhillon.near at 2023-08-10T11:41:46.275Z

---
+++ harrydhillon.near/Keypom.Header/source_code.jsx
@@ -0,0 +1,39 @@
+const HeaderContainer = styled.div`
+    display: flex;
+    padding-top: 10px;
+    justify-content: space-between;
+    background-color:transparent;
+`;
+
+const LinkButton = styled.button`
+    background-color:transparent;
+    border-width:0px;
+    padding:10px;
+`;
+
+const AccountButton = styled.button`
+  padding: 10px 20px;
+  background-color: #fff;
+  font-size: 16px;
+  border-radius: 12px;
+border: 2px solid var(--blue-400-focus-ring, #30C9F3);
+box-shadow: 0px 1px 2px 0px rgba(14, 74, 110, 0.05);
+`;
+
+console.log(props);
+
+return (
+  <>
+    <HeaderContainer>
+      <div>
+        <Widget src="nearcon23.near/widget/Register.KeypomLogo" />
+      </div>
+      <div>
+        <LinkButton>Docs</LinkButton>
+        <LinkButton>Get In Touch</LinkButton>
+        <LinkButton>My Drops</LinkButton>
+        <AccountButton>harrydhillon.near</AccountButton>
+      </div>
+    </HeaderContainer>
+  </>
+);
