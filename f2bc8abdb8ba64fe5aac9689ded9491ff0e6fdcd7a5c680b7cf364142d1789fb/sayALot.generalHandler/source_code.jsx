d9db84816e0180278e9404f2bfe87e0eb1259fb1
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-10 22:59:08 +0000

    Update sayALot.generalHandler by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-10T22:59:08.750Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/sayALot.generalHandler/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/sayALot.generalHandler/source_code.jsx
@@ -1,8 +1,6 @@
 //===============================================INITIALIZATION=====================================================
 let { sharedBlockHeight, tagShared, isTest, accountId } = props;
 
-const libSrcArray = ["kenrou-it.near/widget/SayALot.lib.article"];
-
 const initLibCalls = [
   {
     functionName: "getLastEditArticles",
@@ -39,6 +37,9 @@ State.init({
 //const authorForWidget = "sayalot.near";
 const authorForWidget =
   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const libSrcArray = [
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/SayALot.lib.article",
+];
 const thisWidgetName = "sayALot.generalHandler";
 
 // let writersWhiteList = [
