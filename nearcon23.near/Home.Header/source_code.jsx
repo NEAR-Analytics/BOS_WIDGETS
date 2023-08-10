d459c7ef56de9c91243cc16d3877f63dc19518f7
nearcon23.near <nearcon23.near>
2023-08-10 13:23:18 +0000

    Update Home.Header by nearcon23.near at 2023-08-10T13:23:18.717Z

--- nearcon23.near/Home.Header/source_code.jsx
+++ nearcon23.near/Home.Header/source_code.jsx
@@ -1,14 +1,3 @@
-const desktopImage =
-  "https://ipfs.near.social/ipfs/bafkreifukyxgva3dftljkxpg3vnq46lwwhkhlq55xwi5qqin3chendnm6e";
-//   "https://ipfs.near.social/ipfs/bafkreiga7r5nutmlf66qkfvgkzbq2orbqtlmvgshq47355um2ttqybt4pe";
-const mobileImage =
-  "https://ipfs.near.social/ipfs/bafkreialqekphpdhx4dplb3sjqz5tya5quz633cljeida7eefqylhmmg6m";
-//   "https://ipfs.near.social/ipfs/bafkreiddafldobfu4tcqmeyhxgxdkwp7mhfb5keottpe6lir67j6zgout4";
+const desktopImage = "https://nearcon.s3.amazonaws.com/home-hero.png";
 
-const MyImage = styled.img`
-    @media (max-width: 650px) {    
-         content:url(${mobileImage});   
-    }
-`;
-
-return <MyImage src={desktopImage} style={{ width: "100%" }} />;
+return <img src={desktopImage} style={{ width: "100%" }} />;
