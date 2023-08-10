af4d8b799d8c6ed766bf07038b1ce862a7c8a2c3
agwaze.near <agwaze.near>
2023-08-10 10:31:42 +0000

    Update GenaDrop.index by agwaze.near at 2023-08-10T10:31:42.318Z

--- agwaze.near/GenaDrop.index/source_code.jsx
+++ agwaze.near/GenaDrop.index/source_code.jsx
@@ -75,7 +75,7 @@ const ContentContainer = styled.div`
   }
 `;
 const tabContentWidget = {
-  create: "jgodwill.near/widget/GenaDropmultichainminter",
+  create: "agwaze.near/widget/GenaDrop.Create",
   list: "jgodwill.near/widget/GenaDrop.MultiListing",
   nearNFTs: "jgodwill.near/widget/GenaDrop.Explore",
   evmNFTs: "0xprometheus.near/widget/Eth-Explore",
