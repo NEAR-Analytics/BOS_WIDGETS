f57b32591f83fd690970bdbff7a14bf6cb2415b1
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-10 21:26:02 +0000

    Update SayALot.Create by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-10T21:26:02.085Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.Create/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.Create/source_code.jsx
@@ -21,6 +21,7 @@ const tagsArray = state.tags;
 const accountId = context.accountId;
 
 const getArticleData = () => {
+  console.log(editArticleData);
   const args = {
     articleId: editArticleData.articleId ?? state.articleId,
     author: editArticleData.author ?? accountId,
