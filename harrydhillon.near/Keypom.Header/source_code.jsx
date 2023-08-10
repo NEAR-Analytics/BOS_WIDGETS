c990ddce2c02807e8dcf8295244a03e654cca05d
harrydhillon.near <harrydhillon.near>
2023-08-10 12:49:08 +0000

    Update Keypom.Header by harrydhillon.near at 2023-08-10T12:49:08.436Z

--- harrydhillon.near/Keypom.Header/source_code.jsx
+++ harrydhillon.near/Keypom.Header/source_code.jsx
@@ -1,7 +1,7 @@
 const HeaderContainer = styled.div`
     display: flex;
-    transform: ${(props) => (props.scaleDown ? "scale(0.8)" : "")};
     padding-top: 10px;
+    align-items:center;
     justify-content: space-between;
     background-color:transparent;
 `;
@@ -9,13 +9,14 @@ const HeaderContainer = styled.div`
 const LinkButton = styled.button`
     background-color:transparent;
     border-width:0px;
-    padding:10px;
+    padding: ${(props) => (props?.scaleDown ? "5px" : "10px")};
+    font-size: ${(props) => (props.scaleDown ? "10px" : "16px")};
 `;
 
 const AccountButton = styled.button`
-  padding: 10px 20px;
+  padding: ${(props) => (props?.scaleDown ? "5px 10px" : "10px 20px")};
   background-color: #fff;
-  font-size: 16px;
+  font-size: ${(props) => (props.scaleDown ? "10px" : "16px")};
   border-radius: 12px;
 border: 2px solid var(--blue-400-focus-ring, #30C9F3);
 box-shadow: 0px 1px 2px 0px rgba(14, 74, 110, 0.05);
@@ -23,17 +24,28 @@ box-shadow: 0px 1px 2px 0px rgba(14, 74, 110, 0.05);
 
 console.log(props);
 
+const scaledown = props.scaleDown;
+
 return (
   <>
-    <HeaderContainer scaleDown={props.scaleDown}>
-      <div>
+    <HeaderContainer scaleDown={scaledown}>
+      <div
+        style={
+          scaledown
+            ? {
+                width: 120,
+                transform: "scale(0.5)",
+              }
+            : {}
+        }
+      >
         <Widget src="nearcon23.near/widget/Register.KeypomLogo" />
       </div>
       <div>
-        <LinkButton>Docs</LinkButton>
-        <LinkButton>Get In Touch</LinkButton>
-        <LinkButton>My Drops</LinkButton>
-        <AccountButton>harrydhillon.near</AccountButton>
+        <LinkButton scaleDown={scaledown}>Docs</LinkButton>
+        <LinkButton scaleDown={scaledown}>Get In Touch</LinkButton>
+        <LinkButton scaleDown={scaledown}>My Drops</LinkButton>
+        <AccountButton scaleDown={scaledown}>harrydhillon.near</AccountButton>
       </div>
     </HeaderContainer>
   </>
