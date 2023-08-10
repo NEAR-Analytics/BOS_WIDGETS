6fea5c363258e2cabe15a13cb028d29db4f272f6
mob.near <mob.near>
2023-08-10 19:08:17 +0000

    Update Profile.ShortInlineBlock by mob.near at 2023-08-10T19:08:17.766Z

--- mob.near/Profile.ShortInlineBlock/source_code.jsx
+++ mob.near/Profile.ShortInlineBlock/source_code.jsx
@@ -4,7 +4,6 @@ const profile = props.profile ?? Social.getr(`${accountId}/profile`);
 const fast = !!props.fast || (!props.profile && accountId);
 
 const name = profile.name;
-const isPremium = !!props.isPremium;
 
 const inner = (
   <div className="d-flex flex-row">
@@ -12,7 +11,7 @@ const inner = (
       src="mob.near/widget/ProfileImage"
       props={{
         fast,
-        profile,
+        metadata,
         accountId,
         widgetName,
         style: { height: "2.5em", width: "2.5em", minWidth: "2.5em" },
@@ -20,14 +19,7 @@ const inner = (
       }}
     />
     <div className="text-truncate lh-sm">
-      <div className="text-truncate fw-bold">
-        {name}
-        <Widget
-          loading={""}
-          src="mob.near/widget/Checkmark"
-          props={{ isPremium, accountId }}
-        />
-      </div>
+      <div className="text-truncate fw-bold">{name}</div>
       <div className="text-truncate text-muted">
         <small>
           <span className="font-monospace">@{accountId}</span>
