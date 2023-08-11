ad581cdb7fdc636bdb1550fed6688fe567a01319
andyh.near <andyh.near>
2023-08-11 20:22:27 +0000

    Update Applications by andyh.near at 2023-08-11T20:22:27.460Z

--- andyh.near/Applications/source_code.jsx
+++ andyh.near/Applications/source_code.jsx
@@ -7,7 +7,7 @@ return (
         props={{
           boostedTag: "app",
           placeholder: "🔍 Search Applications",
-          limit: 5,
+          limit: 10,
           onChange: ({ result }) => {
             State.update({ apps: result });
           },
@@ -43,7 +43,7 @@ return (
 
     <Widget
       src="andyh.near/widget/WidgetIcons"
-      props={{ tag: "app", limit: 5 }}
+      props={{ tag: "app", limit: 24 }}
     />
   </div>
 );
