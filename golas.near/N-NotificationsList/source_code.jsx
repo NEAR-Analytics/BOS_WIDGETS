43e728945b0511a8bdb31b0827b8b9dece987c8d
golas.near <golas.near>
2023-08-11 17:58:44 +0000

    Update N-NotificationsList by golas.near at 2023-08-11T17:58:44.717Z

---
+++ golas.near/N-NotificationsList/source_code.jsx
@@ -0,0 +1,13 @@
+return (
+  <Widget
+    src="golas.near/widget/N-Notification"
+    props={{
+      username: "{username}",
+      accountId: "cameron_banyan.near",
+      action: "{action}",
+      componentName: "{componentName}",
+      timestamp: "{timestamp}",
+      desc: "desc desc desc desc desc desc desc desc desc desc desc ",
+    }}
+  />
+);
