51f52e3d7a143f2d1c42452ea6d65e25e762cd0b
astraplusplus.ndctools.near <astraplusplus.ndctools.near>
2023-08-11 06:09:27 +0000

    Update Feed.index by astraplusplus.ndctools.near at 2023-08-11T06:09:27.199Z

--- astraplusplus.ndctools.near/Feed.index/source_code.jsx
+++ astraplusplus.ndctools.near/Feed.index/source_code.jsx
@@ -1,16 +1,23 @@
-const accountId = props.accountId ?? context.accountId ?? "sking.near";
+const getFollowedDAOs = (accountId) => {
+  let following = Social.keys(`${accountId}/graph/follow/*`, "final", {
+    return_type: "BlockHeight",
+  });
 
-let following = Social.keys(`${accountId}/graph/follow/*`, "final", {
-  return_type: "BlockHeight",
-});
+  following =
+    following === null
+      ? null
+      : Object.keys(following[accountId].graph.follow).filter((account) =>
+          account.endsWith(".sputnik-dao.near"),
+        );
+  return following;
+};
+
+
+const following = getFollowedDAOs(props.accountId ?? context.accountId ?? "");
 
 if (following === null) {
   return "";
 }
-
-following = Object.keys(following[accountId].graph.follow).filter((account) =>
-  account.endsWith(".sputnik-dao.near"),
-);
 
 const feeds = [
   {
