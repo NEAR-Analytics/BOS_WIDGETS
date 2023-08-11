f140bada5d54fac67e0201bdda4711538a5ecd55
andyh.near <andyh.near>
2023-08-11 20:02:37 +0000

    Update ApplicationsAsync by andyh.near at 2023-08-11T20:02:37.943Z

---
+++ andyh.near/ApplicationsAsync/source_code.jsx
@@ -0,0 +1,49 @@
+return (
+  <div>
+    <h5>Applications</h5>
+    <div className="mb-2">
+      <Widget
+        src="andyh.near/widget/ComponentSearchAsync"
+        props={{
+          boostedTag: "app",
+          placeholder: "🔍 Search Applications",
+          limit: 5,
+          onChange: ({ result }) => {
+            State.update({ apps: result });
+          },
+        }}
+      />
+    </div>
+    {state.apps && (
+      <div className="mb-2">
+        {state.apps.map((app, i) => (
+          <div key={i}>
+            <Widget
+              src="andyh.near/widget/ComponentSearchAsync.Item"
+              props={{
+                link: `#/${app.widgetSrc}`,
+                accountId: app.accountId,
+                widgetName: app.widgetName,
+                onHide: () => State.update({ apps: null }),
+                extraButtons: ({ widgetPath }) => (
+                  <a
+                    target="_blank"
+                    className="btn btn-outline-secondary"
+                    href={`#/mob.near/widget/WidgetSource?src=${widgetPath}`}
+                  >
+                    Source
+                  </a>
+                ),
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    )}
+
+    <Widget
+      src="andyh.near/widget/WidgetIconsAsync"
+      props={{ tag: "app", limit: 5 }}
+    />
+  </div>
+);
