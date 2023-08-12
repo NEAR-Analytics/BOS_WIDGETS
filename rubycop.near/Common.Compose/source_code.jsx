ce4de286f586e21de59863c912a7eac643179995
rubycop.near <rubycop.near>
2023-08-12 20:15:04 +0000

    Update Common.Compose by rubycop.near at 2023-08-12T20:15:04.662Z

--- rubycop.near/Common.Compose/source_code.jsx
+++ rubycop.near/Common.Compose/source_code.jsx
@@ -1,91 +1,149 @@
-const { handleChange, rows, placeholder, type, withoutSeparator, getMentions } =
-  props;
+const autocompleteEnabled = props.autocompleteEnabled ?? true;
 
-State.init({
-  text: props.initialText || "",
-});
+if (state.image === undefined) {
+  State.init({
+    image: {},
+    text: props.initialText || "",
+  });
 
-const onSelect = (id) => {
-  let text = id;
+  if (props.onHelper) {
+    const extractMentions = (text) => {
+      const mentionRegex =
+        /@((?:(?:[a-z\d]+[-_])*[a-z\d]+\.)*(?:[a-z\d]+[-_])*[a-z\d]+)/gi;
+      mentionRegex.lastIndex = 0;
+      const accountIds = new Set();
+      for (const match of text.matchAll(mentionRegex)) {
+        if (
+          !/[\w`]/.test(match.input.charAt(match.index - 1)) &&
+          !/[/\w`]/.test(match.input.charAt(match.index + match[0].length)) &&
+          match[1].length >= 2 &&
+          match[1].length <= 64
+        ) {
+          accountIds.add(match[1].toLowerCase());
+        }
+      }
+      return [...accountIds];
+    };
 
-  if (!withoutSeparator) {
-    text = state.text.replace(/[\s]{0,1}@[^\s]*$/, "");
-    text = `${text} @${id}`.trim() + " ";
+    const extractHashtags = (text) => {
+      const hashtagRegex = /#(\w+)/gi;
+      hashtagRegex.lastIndex = 0;
+      const hashtags = new Set();
+      for (const match of text.matchAll(hashtagRegex)) {
+        if (
+          !/[\w`]/.test(match.input.charAt(match.index - 1)) &&
+          !/[/\w`]/.test(match.input.charAt(match.index + match[0].length))
+        ) {
+          hashtags.add(match[1].toLowerCase());
+        }
+      }
+      return [...hashtags];
+    };
+
+    const extractMentionNotifications = (text, item) =>
+      extractMentions(text || "")
+        .filter((accountId) => accountId !== context.accountId)
+        .map((accountId) => ({
+          key: accountId,
+          value: {
+            type: "mention",
+            item,
+          },
+        }));
+
+    props.onHelper({
+      extractHashtags,
+      extractMentions,
+      extractTagNotifications: extractMentionNotifications,
+      extractMentionNotifications,
+    });
   }
-  State.update({ text, showAccountAutocomplete: false });
+}
 
-  if (getMentions) getMentions(id);
-  if (handleChange) handleChange(text);
+const content = (state.text || state.image.cid) && {
+  type: "md",
+  text: state.text,
+  image: state.image.cid ? { ipfs_cid: state.image.cid } : undefined,
 };
+
+if (content && props.extraContent) {
+  Object.assign(content, props.extraContent);
+}
+
+function autoCompleteAccountId(id) {
+  let text = state.text.replace(/[\s]{0,1}@[^\s]*$/, "");
+  text = `${text} @${id}`.trim() + " ";
+  State.update({ text, showAccountAutocomplete: false });
+}
 
 const onChange = (text) => {
-  const showAccountAutocomplete = withoutSeparator
-    ? true
-    : /@[\w][^\s]*$/.test(text);
+  const showAccountAutocomplete = /@[\w][^\s]*$/.test(text);
   State.update({ text, showAccountAutocomplete });
-
-  if (handleChange) handleChange(text);
 };
 
-const Input = styled.input`
-  padding: 8px 10px;
-  width: 100%;
-  background: #ffffff;
-  border: 1px solid #d0d6d9;
-  border-radius: 8px;
-  font-size: 14px;
-  color: #828688;
-`;
+const jContent = JSON.stringify(content);
+if (props.onChange && jContent !== state.jContent) {
+  State.update({
+    jContent,
+  });
+  props.onChange({ content });
+}
 
-const Textarea = styled.textarea`
-  padding: 8px 10px;
-  width: 100%;
-  background: #ffffff;
-  border: 1px solid #d0d6d9;
-  border-radius: 8px;
-  font-size: 14px;
-  color: #828688;
-`;
-
-const InputField = () => {
-  return type === "input" ? (
-    <Input
-      value={state.text || ""}
-      onInput={(event) => onChange(event.target.value)}
-      onKeyUp={(event) => {
-        if (event.key === "Escape")
-          State.update({ showAccountAutocomplete: false });
-      }}
-      placeholder={placeholder ?? ""}
-    />
-  ) : (
-    <Textarea
-      value={state.text || ""}
-      onInput={(event) => onChange(event.target.value)}
-      onKeyUp={(event) => {
-        if (event.key === "Escape")
-          State.update({ showAccountAutocomplete: false });
-      }}
-      rows={rows ?? 5}
-      placeholder={placeholder ?? ""}
-    />
-  );
+const onCompose = () => {
+  State.update({
+    image: {},
+    text: "",
+  });
 };
+
+const TextareaWrapper = styled.div`
+  display: grid;
+  vertical-align: top;
+  align-items: center;
+  position: relative;
+  align-items: stretch;
+
+  &::after,
+  textarea {
+    padding: 8px 10px;
+    width: 100%;
+    background: #ffffff;
+    border: 1px solid #d0d6d9;
+    border-radius: 8px;
+    font-size: 14px;
+    color: #828688;
+  }
+
+  &::after {
+    content: attr(data-value) ' ';
+    visibility: hidden;
+    white-space: pre-wrap;
+  }
+`;
 
 return (
-  <>
-    <InputField />
-    {state.showAccountAutocomplete && (
+  <TextareaWrapper className="p-3" data-value={state.text || ""}>
+    <textarea
+      value={state.text || ""}
+      onInput={(event) => onChange(event.target.value)}
+      onKeyUp={(event) => {
+        if (event.key === "Escape") {
+          State.update({ showAccountAutocomplete: false });
+        }
+      }}
+      placeholder={props.placeholder ?? "What's happening?"}
+    />
+    {autocompleteEnabled && state.showAccountAutocomplete && (
       <div className="pt-1 w-100 overflow-hidden">
         <Widget
-          src="rubycop.near/widget/AccountAutocomplete"
+          src="mob.near/widget/AccountAutocomplete"
           props={{
-            term: withoutSeparator ? state.text : state.text.split("@").pop(),
-            onSelect: onSelect,
+            term: state.text.split("@").pop(),
+            onSelect: autoCompleteAccountId,
             onClose: () => State.update({ showAccountAutocomplete: false }),
           }}
         />
       </div>
     )}
-  </>
+  </TextareaWrapper>
 );
