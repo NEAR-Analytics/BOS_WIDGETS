da6e422011fe8a4f65cf4f93d06a49197e59758f
y3k.near <y3k.near>
2023-08-13 21:08:45 +0000

    Update near_atlas.tailwind.React.Table.TopDapps by y3k.near at 2023-08-13T21:08:45.043Z

--- y3k.near/near_atlas.tailwind.React.Table.TopDapps/source_code.jsx
+++ y3k.near/near_atlas.tailwind.React.Table.TopDapps/source_code.jsx
@@ -1,3 +1,4 @@
+// Monthly Active Accounts Example
 let rawData = fetch(
   "https://api.flipsidecrypto.com/api/v2/queries/2122b458-2138-4d4b-b030-efa784fc04d3/data/latest",
   {
@@ -12,9 +13,13 @@ let rawData = fetch(
 // data.body = data.body.sort((a, b) => new Date(a.MONTH) - new Date(b.MONTH));
 
 let Style = styled.div`
-
-          `;
-
+  .table-header {
+    background-color: #000000; /* Set this to the desired dark color for the header */
+    color: #ffffff;
+    text-align: center;
+    font-weight: 600;
+  }
+`;
 let nodes = rawData.body || [];
 
 const sub_widget_map = {
@@ -28,7 +33,7 @@ function formatPercentNew(text) {
 
 function formatNumber(num) {
   return (
-    <span className="text-white">
+    <span className="text-white text-sm p-2">
       {num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")}
     </span>
   );
@@ -74,44 +79,76 @@ const data = {
 
 const COLUMNS = [
   {
-    label: <p className="text-center text-white text-wrap">Project</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap">
+        Project
+      </p>
+    ),
     renderCell: (item) => formatCell(item["Contract Address"]),
     sort: { sortKey: "ContractAddress" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">Past 30 Days</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        Past 30 Days
+      </p>
+    ),
     renderCell: (item) => formatNumber(item["Past 30 Days"]),
     sort: { sortKey: "Past 30 Days" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">Last Month</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        Last Month
+      </p>
+    ),
     renderCell: (item) => formatNumber(item["30-60 Days Ago"]),
     sort: { sortKey: "30-60 Days Ago" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">M/M</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        M/M
+      </p>
+    ),
     renderCell: (item) => formatText(item["M/M"]),
     sort: { sortKey: "M/M" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">New MAAs</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        New MAAs
+      </p>
+    ),
     renderCell: (item) => formatNumber(item["New MAAs"]),
     sort: { sortKey: "New MAAs" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">M2 Retention</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        M2 Retention
+      </p>
+    ),
     renderCell: (item) => formatPercentNew(item["M2 Retention"]),
     sort: { sortKey: "M2 Retention" },
   },
 
   {
-    label: <p className="text-center text-white text-wrap ">Average DAU</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        Average DAU
+      </p>
+    ),
     renderCell: (item) => formatNumberDecimal(item["Daily Average"]),
     sort: { sortKey: "Daily Average" },
   },
 
   {
-    label: <p className="text-center text-white text-wrap ">Stickiness</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        Stickiness
+      </p>
+    ),
     renderCell: (item) => formatPercentNew(item["DAU / MAU"]),
     sort: { sortKey: "DAU / MAU" },
   },
@@ -135,13 +172,40 @@ const sortFns = {
 };
 
 return (
-  <div className="text-bg-dark rounded-4 mb-12">
+  <div className="bg-dark rounded-lg mb-12 overflow-hidden w-full">
     {data !== null ? (
-      <div Style={{ "min-width": "780px" }} className="bg-dark">
-        <BasicTable columns={COLUMNS} data={data} sortFns={sortFns} />
+      <div style={{ "min-width": "780px" }} className="bg-dark w-full">
+        <table className="table-auto w-full">
+          <thead className="bg-gray-700">
+            <tr>
+              {COLUMNS.map((column, index) => (
+                <th
+                  key={index}
+                  className="text-white text-center font-semibold p-2 border-b border-gray-300"
+                >
+                  {column.label}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {nodes.map((row, rowIndex) => (
+              <tr
+                key={rowIndex}
+                className={rowIndex % 2 === 0 ? "bg-gray-800" : "bg-gray-900"}
+              >
+                {COLUMNS.map((column, colIndex) => (
+                  <td key={colIndex} className="text-white text-center p-2">
+                    {column.renderCell(row)}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     ) : (
-      <div>Loading ...</div>
+      <div className="text-white text-center p-4">Loading ...</div>
     )}
   </div>
 );
