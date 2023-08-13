a80be19f3c771c2ed3aa85840b07cb5948ec8a5b
rubycop.near <rubycop.near>
2023-08-13 13:35:37 +0000

    Update Common.Compose by rubycop.near at 2023-08-13T13:35:37.044Z

--- rubycop.near/Common.Compose/source_code.jsx
+++ rubycop.near/Common.Compose/source_code.jsx
@@ -1,4 +1,4 @@
-const { handleChange, rows, placeholder } = props;
+const { handleChange, rows, placeholder, type } = props;
 
 State.init({
   text: props.initialText || "",
@@ -19,6 +19,16 @@ const onChange = (text) => {
   if (handleChange) handleChange(text);
 };
 
+const Input = styled.input`
+  padding: 8px 10px;
+  width: 100%;
+  background: #ffffff;
+  border: 1px solid #d0d6d9;
+  border-radius: 8px;
+  font-size: 14px;
+  color: #828688;
+`;
+
 const Textarea = styled.textarea`
   padding: 8px 10px;
   width: 100%;
@@ -29,8 +39,18 @@ const Textarea = styled.textarea`
   color: #828688;
 `;
 
-return (
-  <>
+const InputField = () => {
+  return type === "input" ? (
+    <Input
+      value={state.text || ""}
+      onInput={(event) => onChange(event.target.value)}
+      onKeyUp={(event) => {
+        if (event.key === "Escape")
+          State.update({ showAccountAutocomplete: false });
+      }}
+      placeholder={placeholder ?? ""}
+    />
+  ) : (
     <Textarea
       value={state.text || ""}
       onInput={(event) => onChange(event.target.value)}
@@ -41,6 +61,12 @@ return (
       rows={rows ?? 5}
       placeholder={placeholder ?? ""}
     />
+  );
+};
+
+return (
+  <>
+    <InputField />
     {state.showAccountAutocomplete && (
       <div className="pt-1 w-100 overflow-hidden">
         <Widget
