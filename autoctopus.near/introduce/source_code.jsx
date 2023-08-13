170a845f1019528a865e2cb9b5fb9c9377512b1c
autoctopus.near <autoctopus.near>
2023-08-13 12:02:40 +0000

    Update introduce by autoctopus.near at 2023-08-13T12:02:40.871Z

---
+++ autoctopus.near/introduce/source_code.jsx
@@ -0,0 +1,9 @@
+let a = `# Function
+- Automatically claim staking rewards from Octopus Network.
+
+# How to access service?
+1. Participate in the monthly Octopus community call to obtain POAP. Each POAP can grant one month's access to any account.(By default, the POAP will automatically charge for owner account, you can change config in profile page).
+2. You can purchase service duration on the Charge page.
+3. You can mint VIP NFT to obtain permanent access rights. The owner account of the VIP NFT will enjoy automatic reward claiming service, and the VIP NFTs are tradable. The minting fee will increase with each VIP NFT minted.
+`;
+return <Markdown text={a} />;
