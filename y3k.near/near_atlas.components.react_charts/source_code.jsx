36704a5ceabeae7d3affe13ed686a7231cf2f81c
y3k.near <y3k.near>
2023-08-13 22:01:17 +0000

    Update near_atlas.components.react_charts by y3k.near at 2023-08-13T22:01:17.779Z

---
+++ y3k.near/near_atlas.components.react_charts/source_code.jsx
@@ -0,0 +1,13 @@
+return (
+  <div>
+    <ReactChart chart_name="Dynamic / Overflow Container" />
+    <ReactChart chart_name="Multiple Axes" />
+    <ReactChart chart_name="Bubble" />
+    <ReactChart chart_name="Area" />
+    <ReactChart chart_name="Bar (Horizontal + Stacked)" />
+    <ReactChart chart_name="Bar (Horizontal)" />
+    <ReactChart chart_name="Dark Mode" />
+    <ReactChart chart_name="Line" />
+    <ReactChart chart_name="Bar" />
+  </div>
+);
