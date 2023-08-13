c7c6f11bb3fa918ad6da9f9fc16cde7a4966f2ef
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-13 06:17:11 +0000

    Update UploadFileModal by relayer.pagodaplatform.near at 2023-08-13T06:17:11.320Z

--- relayer.pagodaplatform.near/UploadFileModal/source_code.jsx
+++ relayer.pagodaplatform.near/UploadFileModal/source_code.jsx
@@ -1,14 +1,16 @@
 const Overlay = styled.div`
-    position: fixed; /* Sit on top of the page content */
-    ${props.modalState ? "display: flex;" : "display: none;"}
+    // position: fixed; /* Sit on top of the page content */
+    position: relative;
+    display: flex;
+    // ${props.modalState ? "display: flex;" : "display: none;"}
     justify-content: center;
     align-items: center;
-    width: 100%; /* Full width (cover the whole page) */
-    height: 100%; /* Full height (cover the whole page) */
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+    width: 100vw; /* Full width (cover the whole page) */
+    height: 100vh; /* Full height (cover the whole page) */
+    // top: 0;
+    // left: 0;
+    // right: 0;
+    // bottom: 0;
     background-color: rgba(0,0,0,0.5); /* Black background with opacity */
     z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
     cursor: pointer; /* Add a pointer on hover */
@@ -19,11 +21,45 @@ console.log(props);
 const Modal = styled.div`
     width: 50%;
     height: 50%;
-    background-color: white;
+    background-color: #D9D9D9;
+    cursor: default
 `;
+
+const CloseButton = styled.div`
+  cursor: pointer
+`;
+
+const fileChangeHandle = (e) => {
+  console.log(e.target.files);
+};
 
 return (
   <Overlay onClick={props.turnOffModal}>
-    <Modal></Modal>
+    <Modal className="rounded d-flex align-items-center flex-column">
+      <div className="w-100 d-flex justify-content-end px-3">
+        <CloseButton onClick={props.turnOffModal} className="fs-1">
+          &#215;
+        </CloseButton>
+      </div>
+      <div className="d-flex align-items-center justify-content-center w-75 bg-white rounded py-5">
+        <label
+          htmlFor="dropzone-file"
+          className="d-flex flex-column align-items-center justify-content-center w-100 rounded"
+        >
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <p className="mb-2">
+              <span className="fw-bold fs-2">Drag & Drop</span>
+            </p>
+            <p className="">Support svg</p>
+          </div>
+          <input
+            onChange={fileChangeHandle}
+            id="dropzone-file"
+            type="file"
+            className="visually-hidden"
+          />
+        </label>
+      </div>
+    </Modal>
   </Overlay>
 );
