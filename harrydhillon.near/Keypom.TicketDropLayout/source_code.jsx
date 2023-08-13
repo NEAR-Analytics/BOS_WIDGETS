012205f4738ef8e0a29cd1add13e2d4da3da20a2
harrydhillon.near <harrydhillon.near>
2023-08-13 10:11:24 +0000

    Update Keypom.TicketDropLayout by harrydhillon.near at 2023-08-13T10:11:24.996Z

--- harrydhillon.near/Keypom.TicketDropLayout/source_code.jsx
+++ harrydhillon.near/Keypom.TicketDropLayout/source_code.jsx
@@ -1,34 +1,97 @@
-const TicketDropBackground = styled.div`
-background: lightgray;
-min-height:100vh;
-background: rgb(220,244,251);
-background: linear-gradient(0deg, rgba(220,244,251,1) 0%, rgba(251,254,255,1) 95%, rgba(255,255,255,1) 100%);
+const FormBackground = styled.div`
+background-color:white;
+padding:10px;
+padding-top:30px;
+margin-top:40px;
+margin-bottom:60px;
+border-radius:10px;
+  border: 2px solid #B6E8F7;
 `;
+State.init({
+  eventName: "",
+  location: "",
+  description: "",
+  date: null,
+  isSingleDateEvent: false,
+});
 
-const ContentDiv = styled.div`
-    padding:10px;
-    max-width: 1200px;
-    margin:auto;
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;  // By default, 1 item per row
+    gap: 16px;  // Gap between rows and columns
+
+    @media (min-width: 1000px) {
+        grid-template-columns: 1fr 1fr;  // 2 items per row if width is greater than 1000px
+    }
 `;
-
-const Heading = styled.div`
-font-size: 30px;
+const Label = styled.p`
+font-size: 16px;
+ translate: 3px 8px;
 font-style: normal;
 font-weight: 500;
-margin-top:-10px;
-line-height: normal;
-letter-spacing: -0.72px;
 `;
 
+const AllSteps = [
+  {
+    label: "Event Info",
+    active: true,
+  },
+  {
+    label: "Collect Info",
+  },
+  {
+    label: "Tickets",
+  },
+  {
+    label: "Review",
+  },
+];
+
+const formContent = () => {
+  return (
+    <FormBackground>
+      <div
+        style={{
+          width: "fit-content",
+          margin: "auto",
+          transform: "scale(0.8)",
+          marginTop: -65,
+        }}
+      >
+        <Widget src="harrydhillon.near/widget/Keypom.TicketLogo" />
+      </div>
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          alignItems: "center",
+          width: "fit-content",
+          justifyContent: "space-evently",
+        }}
+      >
+        {AllSteps.map((item, idx) => (
+          <div style={{ marginRight: 20 }}>
+            <Widget
+              src="harrydhillon.near/widget/Keypom.StepLabel"
+              props={{
+                stepNumber: idx + 1,
+                label: item.label,
+                active: item?.active,
+              }}
+            />
+          </div>
+        ))}
+      </div>
+      {props?.content && props.content()}
+    </FormBackground>
+  );
+};
+
 return (
-  <TicketDropBackground>
-    <ContentDiv>
-      <p style={{ fontSize: 12, marginTop: 20 }}>
-        <span style={{ color: "#94A3B8" }}>All Drops</span> {">"} New Ticket
-        Drop{" "}
-      </p>
-      <Heading>Enter the details for your new Ticket Drop</Heading>
-      {props?.content && props?.content()}
-    </ContentDiv>
-  </TicketDropBackground>
+  <Widget
+    src="harrydhillon.near/widget/Keypom.TicketDropLayout"
+    props={{
+      content: formContent,
+    }}
+  />
 );
