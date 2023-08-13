f6aa79717050779ff45f80e98631a4ff5ddb6b29
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-13 09:05:21 +0000

    Update UploadFileModal by relayer.pagodaplatform.near at 2023-08-13T09:05:21.768Z

--- relayer.pagodaplatform.near/UploadFileModal/source_code.jsx
+++ relayer.pagodaplatform.near/UploadFileModal/source_code.jsx
@@ -92,11 +92,10 @@ return (
           </div>
         </Files>
       </div>
-      {state.file && (
-        <div id="fileName" className="px-3 py-1 mt-3 bg-white rounded-pill">
-          {state.file?.name}
-        </div>
-      )}
+
+      <div id="fileName" className="px-3 py-1 mt-3 bg-white rounded-pill">
+        {state.file?.name}
+      </div>
       <div>
         <UploadButton className="btn px-5 py-2 mt-3">Upload</UploadButton>
       </div>
