61600e2128081c637b9cb404fac84085e4fd190c
shemar268abel.near <shemar268abel.near>
2023-08-13 23:19:52 +0000

    Update Welcome.GetInvolved by shemar268abel.near at 2023-08-13T23:19:52.920Z

---
+++ shemar268abel.near/Welcome.GetInvolved/source_code.jsx
@@ -0,0 +1,36 @@
+return (
+  <div>
+    <h5>Learn More</h5>
+    <div className="mb-2 d-flex gap-2 flex-wrap">
+      <a className="btn btn-outline-primary" href="#">
+        What's Bac Inc?
+      </a>
+      <a className="btn btn-outline-primary" href="https://bac-inc.near.social">
+        Dao Page
+      </a>
+    </div>
+    <div className="mb-2 d-flex gap-2 flex-wrap">
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="#/mob.near/widget/ProfilePage?accountId=bac-inc.near"
+      >
+        <i className="bi bi-person-circle"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://t.me/BAC_inc/1"
+      >
+        <i className="bi bi-telegram"></i>
+      </a>
+      <a
+        className="btn btn-outline-secondary border-0"
+        href="https://github.com/MarmaJFoundation"
+      >
+        <i className="bi bi-github"></i>
+      </a>
+      <a className="btn btn-outline-secondary border-0" href="#">
+        <i className="bi bi-twitter"></i>
+      </a>
+    </div>
+  </div>
+);
