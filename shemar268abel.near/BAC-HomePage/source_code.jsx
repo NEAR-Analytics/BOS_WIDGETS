10a5d09c1ae1f3fcac5667ec1128e942ca33a3d3
shemar268abel.near <shemar268abel.near>
2023-08-14 06:53:54 +0000

    Update BAC-HomePage by shemar268abel.near at 2023-08-14T06:53:54.968Z

--- shemar268abel.near/BAC-HomePage/source_code.jsx
+++ shemar268abel.near/BAC-HomePage/source_code.jsx
@@ -137,8 +137,8 @@ return (
 
     <Main>
       <Section active={state.selectedTab === "apps"}>
-        {/*<Widget src="near/widget/FeaturedComponents" />
-        <Widget src="near/widget/LatestComponents" />*/}
+        <Widget src="near/widget/FeaturedComponents" />
+        <Widget src="near/widget/LatestComponents" />
       </Section>
       <Section negativeMargin primary active={state.selectedTab === "posts"}>
         <Widget src="shemar268abel.near/widget/MainPage.Content" />
