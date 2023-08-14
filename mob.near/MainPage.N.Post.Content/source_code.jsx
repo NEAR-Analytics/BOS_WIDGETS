d9e5ce7b003a3915bb34dcec1846c73fdab0539f
mob.near <mob.near>
2023-08-14 19:51:47 +0000

    Update MainPage.N.Post.Content by mob.near at 2023-08-14T19:51:47.516Z

---
+++ mob.near/MainPage.N.Post.Content/source_code.jsx
@@ -0,0 +1,46 @@
+const content = props.content;
+const raw = !!props.raw;
+
+return content ? (
+  <>
+    {content.text &&
+      (raw ? (
+        <pre style={{ whiteSpace: "pre-wrap" }}>{content.text}</pre>
+      ) : (
+        <Widget
+          src="mob.near/widget/SocialMarkdown"
+          props={{
+            text: content.text,
+            onHashtag: (hashtag) => (
+              <span
+                key={hashtag}
+                className="d-inline-flex"
+                style={{ fontWeight: 500 }}
+              >
+                <a href={`#/?hashtag=${hashtag}`}>#{hashtag}</a>
+              </span>
+            ),
+          }}
+        />
+      ))}
+    {content.image &&
+      (raw ? (
+        <div>
+          <pre>{JSON.stringify(content.image, undefined, 2)}</pre>
+        </div>
+      ) : (
+        <div className="w-100 rounded-3 text-center">
+          <Widget
+            src="mob.near/widget/Image"
+            props={{
+              image: content.image,
+              className: "img-fluid rounded-3",
+              style: { maxHeight: "100vh" },
+            }}
+          />
+        </div>
+      ))}
+  </>
+) : (
+  <div className="w-100" style={{ minHeight: "100px" }} />
+);
