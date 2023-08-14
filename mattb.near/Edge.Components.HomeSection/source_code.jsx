b6bf5929698ab47315ee1a0326e5e9f3133b6596
mattb.near <mattb.near>
2023-08-14 20:42:30 +0000

    Update Edge.Components.HomeSection by mattb.near at 2023-08-14T20:42:30.785Z

--- mattb.near/Edge.Components.HomeSection/source_code.jsx
+++ mattb.near/Edge.Components.HomeSection/source_code.jsx
@@ -87,7 +87,7 @@ const Jumbotron = styled.div`
       position:absolute;
       top:0;
       left:0;
-      background-color:rgba(255,255,255,.45);
+      background-color:rgba(255,255,255,.6);
       width:100%;
       height:100%;
       backdrop-filter:invert(100%);
