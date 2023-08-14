0747b398b62d3b00f8bc97cdb5dedea3b4868b46
harrydhillon.near <harrydhillon.near>
2023-08-14 18:21:27 +0000

    Update Keypom.Components.Toggle by harrydhillon.near at 2023-08-14T18:21:27.760Z

--- harrydhillon.near/Keypom.Components.Toggle/source_code.jsx
+++ harrydhillon.near/Keypom.Components.Toggle/source_code.jsx
@@ -1,1 +1,31 @@
-return <div>Hello World</div>;
+const SwitchContainer = styled.div`
+  width: 60px;
+  height: 30px;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  padding: 0 5px;
+  cursor: pointer;
+  background-color: ${(props) => (props.isOn ? "#30C9F3" : "#CCC")};
+  transition-property: background-color;
+  transition-duration: 2s;
+  transition-timing-function: ease;
+  transition-delay: 1s;
+`;
+
+const SwitchKnob = styled.div`
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  background-color: white;
+  transform: ${(props) =>
+    props.isOn ? "translateX(26px)" : "translateX(-4px)"};
+`;
+
+const Switch = (isOn, handleToggle) => (
+  <SwitchContainer isOn={isOn} onClick={handleToggle}>
+    <SwitchKnob isOn={isOn} />
+  </SwitchContainer>
+);
+
+return Switch(!!props.isToggled, props.handleToggle);
