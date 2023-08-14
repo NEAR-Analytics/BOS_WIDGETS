0e4337d62bf73aa8eb332fb829c660a4d787e16b
sharddog.near <sharddog.near>
2023-08-14 16:11:57 +0000

    Update UserRep.Filter by sharddog.near at 2023-08-14T16:11:57.832Z

---
+++ sharddog.near/UserRep.Filter/source_code.jsx
@@ -0,0 +1,34 @@
+const { handleWalletChange, wallet, placeholder } = props;
+
+const Container = styled.div`
+  padding: 16px;
+  border-radius: 8px;
+  background: #f8f8f9;
+
+  i {
+    position: absolute;
+    top: 8px;
+    left: 16px;
+    font-size: 14px;
+  }
+
+  input {
+    padding: 8px;
+    padding-left: 40px;
+    font-size: 14px;
+  }
+`;
+
+return (
+  <Container>
+    <div className="position-relative">
+      <i className="bi bi-search text-secondary"></i>
+      <input
+        placeholder={placeholder}
+        className="form-control w-100"
+        value={wallet}
+        onChange={handleWalletChange}
+      />
+    </div>
+  </Container>
+);
