9ae4db28728bf9442f5df1701da4fe1b542c814a
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-14 20:08:27 +0000

    Update NDC.General.ArticleView by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-14T20:08:27.605Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.General.ArticleView/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.General.ArticleView/source_code.jsx
@@ -675,7 +675,6 @@ return (
                     props={{
                       widgets,
                       isTest,
-                      item,
                       authorForWidget,
                     }}
                   />
