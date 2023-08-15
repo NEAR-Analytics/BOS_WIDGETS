8998b882616a315e7111c8641cefff989945b006
vlmoon.near <vlmoon.near>
2023-08-15 20:54:04 +0000

    Update BOSModuleA by vlmoon.near at 2023-08-15T20:54:04.012Z

--- vlmoon.near/BOSModuleA/source_code.jsx
+++ vlmoon.near/BOSModuleA/source_code.jsx
@@ -1,7 +1,8 @@
-const getDependencies = props.dependencies;
+const dependencies = props.dependencies;
 
 return (
   <>
     <h1>Module A</h1>
+    <h2>{dependencies.routes ?? "no routes"}</h2>
   </>
 );
