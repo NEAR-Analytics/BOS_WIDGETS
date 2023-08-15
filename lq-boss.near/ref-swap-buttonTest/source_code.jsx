b34f4fdafe0bb72639fb8c601ae164a7333ecc2f
lq-boss.near <lq-boss.near>
2023-08-15 14:11:18 +0000

    Update ref-swap-buttonTest by lq-boss.near at 2023-08-15T14:11:18.643Z

---
+++ lq-boss.near/ref-swap-buttonTest/source_code.jsx
@@ -0,0 +1,46 @@
+const { accountId, notEnough, canSwap, callTx, requestSignIn, noPool } = props;
+
+const ButtonWrapper = styled.button`
+  border-radius: 12px;
+
+  background-color: ${(props) =>
+    props.notEnough ? "rgba(0,255,163, 0.5)" : `rgba(0,255,163, 1)`};
+  font-weight: 700;
+  font-size: 18px;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  color: black;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  outline: none;
+  border: none;
+  padding: 8px 0px;
+  margin-top: 26px;
+  height:60px;
+`;
+
+return (
+  <ButtonWrapper
+    notEnough={notEnough && accountId && !noPool}
+    disabled={!accountId ? false : !canSwap || (notEnough && accountId)}
+    onClick={() => {
+      // if (!accountId) {
+      //   return requestSignIn();
+      // }
+
+      if (!canSwap || notEnough) return;
+
+      callTx();
+    }}
+  >
+    {noPool
+      ? "No pool"
+      : !accountId
+      ? "Connect wallet"
+      : notEnough
+      ? "Insufficient Balance"
+      : "Swap"}
+  </ButtonWrapper>
+);
+1;
