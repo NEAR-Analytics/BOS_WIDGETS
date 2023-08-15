af0eebcf3931c22a07a3b8a979477b9c5c4fc8c4
mattb.near <mattb.near>
2023-08-15 21:57:59 +0000

    Update Edge.Views.Home by mattb.near at 2023-08-15T21:57:59.556Z

--- mattb.near/Edge.Views.Home/source_code.jsx
+++ mattb.near/Edge.Views.Home/source_code.jsx
@@ -25,6 +25,7 @@ return (
     <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.HostsSection`} />
     <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.SponsorsSection`} />
     <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.ProgramSection`} />
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.TicketsSection`} />
     <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.Footer`} />
   </Main>
 );
