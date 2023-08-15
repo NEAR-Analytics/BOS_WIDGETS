65d42cfc8025678fec3c3f1376250dbb4373aadd
andyh.near <andyh.near>
2023-08-15 20:59:24 +0000

    Update HooksRoot by andyh.near at 2023-08-15T20:59:24.892Z

--- andyh.near/HooksRoot/source_code.jsx
+++ andyh.near/HooksRoot/source_code.jsx
@@ -1,3 +1,6 @@
-const renderComponent = (a) => <h2>{a}</h2>;
+const renderComponent = (a) => {
+  console.log("rendering compd!");
+  return <h2>{a}</h2>;
+};
 
 return <Widget src="andyh.near/widget/Hooks" props={{ renderComponent }} />;
