3c3478313f19e08ce4d98bcc913ac2ad1949984d
harrydhillon.near <harrydhillon.near>
2023-08-15 08:25:15 +0000

    Update Keypom.CollectInfo.AddCustomFieldModal by harrydhillon.near at 2023-08-15T08:25:15.868Z

---
+++ harrydhillon.near/Keypom.CollectInfo.AddCustomFieldModal/source_code.jsx
@@ -0,0 +1,67 @@
+State.init({fieldVal:""})
+
+const AddCustomFieldModalJsx = (
+  <>
+    <p style={{ fontWeight: "500", marginBottom: -5 }}>Field</p>
+    <Widget
+      props={{
+        label: "Add a field you’d like attendees to fill out for this event.",
+        inputProps: {
+          placeholder: "",
+          value:state.fieldVal,
+          onChange:(e)=>State.update({fieldVal:e.target.value})
+        },
+        labelProps: {
+          style: {
+            fontWeight: "400",
+            fontSize: 13,
+          },
+        },
+      }}
+      src="harrydhillon.near/widget/Keypom.Components.Input"
+    />
+    <button
+    onClick={()=>props.onSave(state)}
+      style={{
+        width: "100%",
+        backgroundColor: "black",
+        fontSize: 14,
+        borderRadius: 10,
+        borderWidth: 0,
+        marginTop: 10,
+      }}
+    >
+      Save Field
+    </button>
+    <button
+    onClick={props.onClose}
+      style={{
+        width: "100%",
+        backgroundColor: "white",
+        fontSize: 14,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "lightgray",
+        marginTop: 5,
+        color: "black",
+      }}
+    >
+      Cancel
+    </button>
+  </>
+);
+
+return (
+  <Widget
+    src="harrydhillon.near/widget/Keypom.Components.Modal"
+    props={{
+      children: AddCustomFieldModalJsx,
+      isOpen: props.isOpen,
+      contentStyles: {
+        style: {
+          width: 400,
+        },
+      },
+    }}
+  />
+);
