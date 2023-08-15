daeca25acafa7d4c26baac471f1861b57bebeace
mattb.near <mattb.near>
2023-08-15 00:46:50 +0000

    Update Edge.Components.HomeSection by mattb.near at 2023-08-15T00:46:50.746Z

--- mattb.near/Edge.Components.HomeSection/source_code.jsx
+++ mattb.near/Edge.Components.HomeSection/source_code.jsx
@@ -7,7 +7,7 @@ const Jumbotron = styled.div`
     align-items:center;
     justify-content:center;
     width:100%;
-    height:80vh;
+    height:100vh;
     background-position:center;
     background-size:cover;
     background-repeat:no-repeat;
