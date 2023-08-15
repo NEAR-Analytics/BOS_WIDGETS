d0962952ae5dfd5be1a44bf296488b8fa77e693e
easypoll-v0.ndc-widgets.near <easypoll-v0.ndc-widgets.near>
2023-08-15 06:27:35 +0000

    Update EasyPoll.Data.GetAnswers by easypoll-v0.ndc-widgets.near at 2023-08-15T06:27:35.229Z

--- easypoll-v0.ndc-widgets.near/EasyPoll.Data.GetAnswers/source_code.jsx
+++ easypoll-v0.ndc-widgets.near/EasyPoll.Data.GetAnswers/source_code.jsx
@@ -1,7 +1,7 @@
 const src = props.src;
 const blocking = props.blocking ?? true;
 const poll = props.poll;
-const { verifiedHumansOnly, endTimestamp, startTimestamp } = poll;
+const { verifiedHumansOnly, endTimestamp, startTimestamp, ogHumansOnly } = poll;
 const indexVersion = props.indexVersion ?? "4.0.0";
 const Children = props.children ?? (() => <></>);
 const Loading = props.Loading ?? (() => <></>);
@@ -39,6 +39,28 @@ const expensiveWork = () => {
       const res = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
         account: `${v}`,
         issuer: "fractal.i-am-human.near",
+      });
+
+      if (res === null) {
+        finish = false;
+      }
+      const isVerified = res !== null ? res?.[0]?.[1]?.[0] : null;
+
+      if (!isVerified) {
+        filtered.splice(i, 1);
+        i--;
+      }
+    }
+  }
+
+  // should respect og only
+  if (ogHumansOnly) {
+    for (let i = 0; i < filtered.length; i++) {
+      const v = filtered[i].accountId;
+
+      const res = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
+        account: `${v}`,
+        issuer: "community.i-am-human.near",
       });
 
       if (res === null) {
