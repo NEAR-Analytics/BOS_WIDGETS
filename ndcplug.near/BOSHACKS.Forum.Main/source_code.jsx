6353b4d58bdfb8697399bcc4b4024efb7b19355a
ndcplug.near <ndcplug.near>
2023-08-15 00:45:04 +0000

    Update BOSHACKS.Forum.Main by ndcplug.near at 2023-08-15T00:45:04.009Z

--- ndcplug.near/BOSHACKS.Forum.Main/source_code.jsx
+++ ndcplug.near/BOSHACKS.Forum.Main/source_code.jsx
@@ -1,6 +1,10 @@
 return (
   <div>
     <Widget
+      src="ndcplug.near/widget/BOSHACKS.Forum.Create"
+      props={{ tag: "boshacks" }}
+    />
+    <Widget
       src="devgovgigs.near/widget/gigs-board.entity.post.List"
       props={{ tag: "boshacks" }}
     />
