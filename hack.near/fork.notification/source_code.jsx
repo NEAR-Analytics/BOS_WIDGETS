1a1ef34cc0f2d8d2914de319af6b424864a55e74
hack.near <hack.near>
2023-08-15 13:08:42 +0000

    Update fork.notification by hack.near at 2023-08-15T13:08:42.128Z

--- hack.near/fork.notification/source_code.jsx
+++ hack.near/fork.notification/source_code.jsx
@@ -1,157 +1,23 @@
-const { accountId, value } = props;
-
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 24px;
-  border: 1px solid #eceef0;
-  box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
-    0px 1px 2px rgba(16, 24, 40, 0.06);
-  padding: 12px;
-  border-radius: 12px;
-  transition: background-color 200ms;
-
-  &:hover {
-    background: #eefeef;
-  }
-
-  > *:first-child {
-    width: 200px;
-    border-right: 1px solid #eceef0;
-    padding-right: 24px;
-  }
-
-  > *:last-child {
-    margin-left: auto;
-  }
-
-  @media (max-width: 600px) {
-    flex-direction: column;
-    align-items: start;
-    gap: 8px;
-
-    > *:first-child {
-      width: 100%;
-      border-right: none;
-      padding-right: 0;
-    }
-
-    > *:last-child {
-      margin-left: 0;
-    }
-  }
-`;
-
-const Text = styled.p`
-  margin: 0;
-  line-height: 1.5rem;
-  color: ${(p) => (p.bold ? "#11181C" : "#687076")} !important;
-  font-weight: 400;
-  font-size: ${(p) => (p.small ? "12px" : "14px")};
-  overflow: ${(p) => (p.ellipsis ? "hidden" : "")};
-  text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "")};
-  white-space: ${(p) => (p.ellipsis ? "nowrap" : "")};
-  overflow-wrap: anywhere;
-
-  b {
-    font-weight: 600;
-    color: #11181c;
-  }
-
-  &[href] {
-    font-weight: 600;
-    color: #006adc !important;
-    display: inline-flex;
-    gap: 0.25rem;
-
-    &:hover,
-    &:focus {
-      text-decoration: underline;
-    }
-  }
-`;
-
-const Button = styled.a`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  gap: 8px;
-  padding: 8px 16px;
-  height: 32px;
-  border-radius: 100px;
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 15px;
-  text-align: center;
-  cursor: pointer;
-  background: #fbfcfd;
-  border: 1px solid #d7dbdf;
-  color: #006adc !important;
-  white-space: nowrap;
-
-  &.button--primary {
-    width: 100%;
-    color: #006adc !important;
-
-    @media (max-width: 1024px) {
-      width: auto;
-    }
-  }
-
-  &:hover,
-  &:focus {
-    background: #ecedee;
-    text-decoration: none;
-    outline: none;
-  }
-
-  i {
-    color: #7e868c;
-  }
-
-  .bi-16 {
-    font-size: 16px;
-  }
-`;
+const { accountId, blockHeight, value } = props;
 
 return (
-  <>
-    <Wrapper>
-      <div>
-        <Widget
-          src="mob.near/widget/ProfileLine"
-          props={{ accountId: props.accountId }}
-        />
-      </div>
-
-      <Text bold>
-        <Text as={actionable ? "a" : "p"} href={actionable ? postUrl : ""}>
-          {notificationMessage[type]}
-        </Text>
-        <Widget
-          src="mob.near/widget/TimeAgo"
-          props={{ blockHeight: props.blockHeight }}
-        />
-        ago
-      </Text>
-
-      <div>
-        {type === "fork" && (
-          <Widget
-            src="near/widget/FollowButton"
-            props={{ accountId: props.accountId }}
-          />
-        )}
-
-        {type === "request" && (
-          <Widget
-            src="hack.near/widget/update"
-            props={{ accountId: props.accountId }}
-          />
-        )}
-
-        {actionable && <Button href={postUrl}>View</Button>}
-      </div>
-    </Wrapper>
-  </>
+  <div className="m-2">
+    {value.type === "fork" && (
+      <Widget
+        src="hack.near/widget/notification.item"
+        props={{
+          L: value.type === "fork" && "forked your widget",
+          R: (
+            <div className="m-2">
+              <Widget
+                src="mob.near/widget/FollowButton"
+                props={{ accountId }}
+              />
+            </div>
+          ),
+          ...props,
+        }}
+      />
+    )}
+  </div>
 );
