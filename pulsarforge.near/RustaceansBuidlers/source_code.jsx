943852ffcd6977151c2786dc5e00910227d04250
pulsarforge.near <pulsarforge.near>
2023-08-15 13:03:37 +0000

    Update RustaceansBuidlers by pulsarforge.near at 2023-08-15T13:03:37.055Z

---
+++ pulsarforge.near/RustaceansBuidlers/source_code.jsx
@@ -0,0 +1,29 @@
+return (
+  <div>
+    <a
+      href="https://keypom.xyz/claim/v2.keypom.near#5FxjxU7GxU3wqYMDeoTta5gXcds5J2ruA4oPvf7Pn1adYSGaajaTHmEGhGqdU3e3NmmPJ8oDJPsbE53ZPVXgFxJm"
+      onClick={handleLightbox}
+    >
+      <div className="details">
+        <h1>WOAPS - Life events stored on chain</h1>
+        <h2>Inmutable Proof of Achievements - Collectibles</h2>
+        <br />
+      </div>
+      <div>
+        <img
+          style={{ borderRadius: 30 }}
+          src="https://cloudflare-ipfs.com/ipfs/bafybeieqeh3pcbsnj7wabm6uqfglht5lyzh7djki2uxd77tb75g6dr5rbi"
+          alt="Rustacean Builder Evolution"
+          width={750}
+          height={750}
+        />
+
+        <h3 style={{ marginTop: 30 }}>Rustaceans Buidlers</h3>
+        <p>
+          Note: Keypom link drop updated periodically, claimed if you identify
+          with built on Rust
+        </p>
+      </div>
+    </a>
+  </div>
+);
