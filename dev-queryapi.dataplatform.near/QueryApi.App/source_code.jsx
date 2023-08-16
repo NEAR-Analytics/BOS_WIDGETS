ba6e914a2c5f308670d9b6ad2b113e8b491e3922
dev-queryapi.dataplatform.near <dev-queryapi.dataplatform.near>
2023-08-16 13:06:58 +0000

    Update QueryApi.App by dev-queryapi.dataplatform.near at 2023-08-16T13:06:58.186Z

--- dev-queryapi.dataplatform.near/QueryApi.App/source_code.jsx
+++ dev-queryapi.dataplatform.near/QueryApi.App/source_code.jsx
@@ -1,21 +1,13 @@
-const GRAPHQL_ENDPOINT =
-  "https://queryapi-hasura-graphql-24ktefolwq-ew.a.run.app";
-const APP_OWNER = "dataplatform.near";
-const EXTERNAL_APP_URL = "https://queryapi-frontend-24ktefolwq-ew.a.run.app";
-const REGISTRY_CONTRACT_ID = "queryapi.dataplatform.near";
 const view = props.view;
 const path = props.path;
 const tab = props.tab;
 const selectedIndexerPath = props.selectedIndexerPath;
+console.log(`dev-queryapi.dataplatform.near`, "loaded the account")
 
 return (
   <Widget
-    src={`${APP_OWNER}/widget/QueryApi.Dashboard`}
+    src={`dev-queryapi.dataplatform.near/widget/QueryApi.Dashboard`}
     props={{
-      GRAPHQL_ENDPOINT,
-      APP_OWNER,
-      EXTERNAL_APP_URL,
-      REGISTRY_CONTRACT_ID,
       view,
       path,
       tab,
