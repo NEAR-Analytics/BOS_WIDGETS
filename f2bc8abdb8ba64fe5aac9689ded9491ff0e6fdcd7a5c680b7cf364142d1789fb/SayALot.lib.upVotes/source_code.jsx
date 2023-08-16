11d0bb97a17b6838e3bb8b68e10fc33a3b45d4ac
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-16 18:50:59 +0000

    Update SayALot.lib.upVotes by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-16T18:50:59.952Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.lib.upVotes/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.lib.upVotes/source_code.jsx
@@ -4,12 +4,95 @@ const prodAction = "sayALotUpVote";
 const testAction = `test_${prodAction}`;
 const action = isTest ? testAction : prodAction;
 
-function getUpVote(props) {
-  console.log("getUpVote props: ", props);
+function getUpVotes(props) {
+  const { elementReactedId, createdInteraction } = props;
+
+  const allVotes = Social.index(action, elementReactedId, {
+    order: "desc",
+  });
+
+  const uniqueAccounts = [];
+  let arrayLastInteractionForEachUser =
+    allVotes &&
+    allVotes.filter((obj) => {
+      if (!uniqueAccounts.includes(obj.accountId)) {
+        uniqueAccounts.push(obj.accountId);
+        return true;
+      }
+      return false;
+    });
+
+  const userInteraction =
+    arrayLastInteractionForEachUser &&
+    arrayLastInteractionForEachUser.find((obj) => {
+      return obj.accountId === context.accountId;
+    });
+
+  if (userInteraction && createdInteraction) {
+    const newArrayOfLastInteractions = arrayLastInteractionForEachUser
+      .filter((obj) => {
+        return obj.accountId !== context.accountId;
+      })
+      .push({
+        accountId: context.accountId,
+        value: {
+          type: "md",
+          deleteReaction: createdInteraction.value.deleteReaction,
+        },
+      });
+
+    arrayLastInteractionForEachUser = newArrayOfLastInteractions;
+  }
+
+  // ========= GET UPVOTES STATISTICS =========
+  function countUpVotes() {
+    let upVotes =
+      arrayLastInteractionForEachUser &&
+      arrayLastInteractionForEachUser.filter(
+        (interaction) => !interaction.value.deleteReaction
+      );
+    return upVotes.length;
+  }
+
+  return { reactionsStatistics: countUpVotes(), userInteraction };
 }
 
 function addUpVote(props) {
-  console.log("addUpVote props: ", props);
+  const { isDelete, elementReactedId, onCommit, onCancel } = props;
+
+  saveUpVote(isDelete, elementReactedId, onCommit, onCancel);
+
+  resultLibCalls = resultLibCalls.filter((call) => {
+    return call.functionName !== "addUpVote";
+  });
+
+  return upVote;
+}
+
+function composeUpVoteData(isDelete, elementReactedId) {
+  const data = {
+    index: {
+      [action]: JSON.stringify({
+        key: elementReactedId,
+        value: {
+          type: "md",
+          deleteReaction: isDelete,
+        },
+      }),
+    },
+  };
+
+  return data;
+}
+
+function saveUpVote(isDelete, elementReactedId, onCommit, onCancel) {
+  const newData = composeUpVoteData(isDelete, elementReactedId);
+
+  Social.set(newData, {
+    force: true,
+    onCommit,
+    onCancel,
+  });
 }
 
 function libCall(call) {
