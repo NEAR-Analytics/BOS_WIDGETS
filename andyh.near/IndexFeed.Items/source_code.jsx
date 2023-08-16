0aef14664aacfb96bcd6c7114766851cdc50670d
andyh.near <andyh.near>
2023-08-16 22:54:02 +0000

    Update IndexFeed.Items by andyh.near at 2023-08-16T22:54:02.575Z

---
+++ andyh.near/IndexFeed.Items/source_code.jsx
@@ -0,0 +1,30 @@
+const { manual, reverse, fetchMore, items, hasMore } = props;
+const makeMoreItems = useComponentCallback(props.makeMoreItems);
+const renderItems = useComponentCallback(props.renderItems, items);
+return props.manual ? (
+  <>
+    {reverse && fetchMore}
+    {renderedItems}
+    {!reverse && fetchMore}
+  </>
+) : (
+  <InfiniteScroll
+    pageStart={0}
+    loadMore={makeMoreItems}
+    hasMore={hasMore}
+    loader={
+      <div className="loader">
+        <span
+          className="spinner-grow spinner-grow-sm me-1"
+          role="status"
+          aria-hidden="true"
+        />
+        Loading ...
+      </div>
+    }
+  >
+    {props.headerElement}
+    {renderItems()}
+    {props.footerElement}
+  </InfiniteScroll>
+);
