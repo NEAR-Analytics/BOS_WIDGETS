7828cab0d5f743507ac174dd736db1f1d2d938b0
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-16 05:46:13 +0000

    Update Onboarding.Starter-fork by relayer.pagodaplatform.near at 2023-08-16T05:46:13.711Z

--- relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
+++ relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
@@ -7,25 +7,25 @@ State.init({
   chainId: undefined,
 });
 
-// if (
-//   state.chainId === undefined &&
-//   ethers !== undefined &&
-//   Ethers.send("eth_requestAccounts", [])[0]
-// ) {
-//   Ethers.provider()
-//     .getNetwork()
-//     .then((chainIdData) => {
-//       if (chainIdData?.chainId) {
-//         State.update({ chainId: chainIdData.chainId });
-//       }
-//     });
-//   console.log("here");
-// } else {
-//   console.log("hehe");
-// }
-// if (state.chainId !== undefined && state.chainId !== 11155111) {
-//   return <p>Switch to Ethereum Sepolia</p>;
-// }
+if (
+  state.chainId === undefined &&
+  ethers !== undefined &&
+  Ethers.send("eth_requestAccounts", [])[0]
+) {
+  Ethers.provider()
+    .getNetwork()
+    .then((chainIdData) => {
+      if (chainIdData?.chainId) {
+        State.update({ chainId: chainIdData.chainId });
+      }
+    });
+  console.log("here");
+} else {
+  console.log("hehe");
+}
+if (state.chainId !== undefined && state.chainId !== 11155111) {
+  return <p>Switch to Ethereum Sepolia</p>;
+}
 
 const createEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
