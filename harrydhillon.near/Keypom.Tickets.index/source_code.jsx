4120fd2a29029e852f6f34a4be2a5e48c82677ef
harrydhillon.near <harrydhillon.near>
2023-08-16 11:43:42 +0000

    Update Keypom.Tickets.index by harrydhillon.near at 2023-08-16T11:43:42.613Z

---
+++ harrydhillon.near/Keypom.Tickets.index/source_code.jsx
@@ -0,0 +1,46 @@
+const CreateTicketContainer = styled.div`
+display:flex;
+justify-content:space-between;
+align-items:center;
+`;
+State.init({ isCreateTicketModalOpen: true });
+
+const createTickets = () => {
+  return (
+    <CreateTicketContainer>
+      <div>
+        <p style={{ marginBottom: 0, fontSize: 20, fontWeight: "600" }}>
+          Create Tickets *
+        </p>
+        <p style={{ marginBottom: 0 }}>
+          Create custom tickets for [event name].
+        </p>
+      </div>
+      <button
+        onClick={() => {
+          State.update({
+            isCreateTicketModalOpen: true,
+          });
+        }}
+        style={{ backgroundColor: "black", borderWidth: 0 }}
+      >
+        Create Ticket
+      </button>
+      {state.isAddCustomFieldModalOpen && (
+        <Widget
+          src="harrydhillon.near/widget/Keypom.CollectInfo.AddCustomFieldModal"
+          props={{
+            isOpen: state.isCreateTicketModalOpen,
+            onClose: () => {
+              State.update({
+                isCreateTicketModalOpen: false,
+              });
+            },
+          }}
+        />
+      )}
+    </CreateTicketContainer>
+  );
+};
+
+return <div style={{ padding: 10 }}>{createTickets()}</div>;
