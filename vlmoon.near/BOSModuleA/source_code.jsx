33e44a6c5665bf45080cd303912853ca694da1e9
vlmoon.near <vlmoon.near>
2023-08-16 11:31:39 +0000

    Update BOSModuleA by vlmoon.near at 2023-08-16T11:31:39.136Z

--- vlmoon.near/BOSModuleA/source_code.jsx
+++ vlmoon.near/BOSModuleA/source_code.jsx
@@ -3,25 +3,17 @@ const routesNavigator = props.routesNavigator;
 const appThemeService = props.appThemeService;
 const appTheme = appThemeService.getTheme();
 
-const Body = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 800px;
-  wifth : 300px;
-  self-align: center;
-  background-color: ${appTheme.colors().backgroundColor};
-`;
-
 return (
-  <Body>
+  <>
     <h1>Module A</h1>
     <h2>{JSON.stringify(dependencies)}</h2>
     <button onClick={routesNavigator.moduleB}>Go to the next module</button>
+    <h6>Current Colors : {appTheme.colors()}</h6>
     <button onClick={appThemeService.switchToLightTheme}>
       Change App Theme Mode To Light
     </button>
     <button onClick={appThemeService.switchToDarkTheme}>
       Change App Theme Mode To Dark
     </button>
-  </Body>
+  </>
 );
