7d12a51d28536ea5f04023eb9583b0dbd99ec064
edgeai.near <edgeai.near>
2023-08-16 18:23:18 +0000

    Update Edge.Views.Home by edgeai.near at 2023-08-16T18:23:18.230Z

---
+++ edgeai.near/Edge.Views.Home/source_code.jsx
@@ -0,0 +1,32 @@
+const WIDGET_OWNER = "mattb.near";
+
+const Main = styled.div`
+  position:relative;
+`;
+const HeaderWrapper = styled.div`
+  position:sticky;
+  top:0;
+  left:0;
+`;
+const JumbotronWrapper = styled.div`
+`;
+
+return (
+  <Main>
+    <HeaderWrapper>
+      <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.Header`} />
+    </HeaderWrapper>
+    <JumbotronWrapper>
+      <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.HomeSection`} />
+    </JumbotronWrapper>
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.StorySection`} />
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.MainTopicsSection`} />
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.SpeakersSection`} />
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.HostsSection`} />
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.SponsorsSection`} />
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.ProgramSection`} />
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.TicketsSection`} />
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.FaqSection`} />
+    <Widget src={`${WIDGET_OWNER}/widget/Edge.Components.Footer`} />
+  </Main>
+);
