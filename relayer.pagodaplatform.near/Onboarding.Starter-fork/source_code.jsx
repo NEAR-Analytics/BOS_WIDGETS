1ed1fe4fddc9b6a55dd98fa8bbd9c97f2162ea88
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-16 10:07:12 +0000

    Update Onboarding.Starter-fork by relayer.pagodaplatform.near at 2023-08-16T10:07:12.391Z

--- relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
+++ relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
@@ -202,11 +202,7 @@ const contract = new ethers.Contract(
   wallet.abi,
   Ethers.provider().getSigner()
 );
-if (state.transfers.length === 0) {
-  State.update({
-    transfers: contract.getTransfers(),
-  });
-}
+console.log(contract.getTransfers());
 // console.log(state.transfers.length === 0, sender, contract);
 // let tmp = true;
 // if (tmp) {
