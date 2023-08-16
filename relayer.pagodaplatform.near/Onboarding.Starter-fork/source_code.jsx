5789838f9cbccd6b03ebfb1deb2e3a7d888920d1
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-16 09:42:01 +0000

    Update Onboarding.Starter-fork by relayer.pagodaplatform.near at 2023-08-16T09:42:01.368Z

--- relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
+++ relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
@@ -206,7 +206,7 @@ console.log(state.transfers.length === 0, sender, contract);
 if (state.transfers.length === 0) {
   console.log("jjj");
   try {
-    contract
+    contract.methods
       .createTransfer(0.1, "0xF0DB85E02DBC2d2c9b86dFC245cd9C2CAF9a901B")
       .send({ from: sender });
     State.update({
