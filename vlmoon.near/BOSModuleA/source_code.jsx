1317d09e6f2760658a4d8e78349fe69d563e83eb
vlmoon.near <vlmoon.near>
2023-08-16 18:33:01 +0000

    Update BOSModuleA by vlmoon.near at 2023-08-16T18:33:01.136Z

--- vlmoon.near/BOSModuleA/source_code.jsx
+++ vlmoon.near/BOSModuleA/source_code.jsx
@@ -2,6 +2,7 @@ const dependencies = props.dependencies || ["No dependencies"];
 const routesNavigator = props.routesNavigator;
 const appThemeService = props.appThemeService;
 const appTheme = appThemeService.getTheme();
+const uiKitComponents = props.uiKitComponents;
 
 const H6Text = styled.h6`
   margin: 0;
@@ -14,6 +15,7 @@ return (
     <h2>{JSON.stringify(dependencies)}</h2>
     <button onClick={routesNavigator.moduleB}>Go to the next module</button>
     <H6Text>Current Colors : {JSON.stringify(appTheme.colors())}</H6Text>
+    <uiKitComponents.button>"UI Kit Button"</uiKitComponents.button>
     <button onClick={appThemeService.switchToLightTheme}>
       Change App Theme Mode To Light
     </button>
