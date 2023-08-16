dd8c16bd4c0efc08fb694639d8223a3d77875719
hack.near <hack.near>
2023-08-16 12:55:51 +0000

    Update communities.page by hack.near at 2023-08-16T12:55:51.250Z

--- hack.near/communities.page/source_code.jsx
+++ hack.near/communities.page/source_code.jsx
@@ -1,2 +1,22 @@
-const accountId = props.accountId ?? context.accountId; const ownerId = props.ownerId ?? "hack.near"; const pageId = props.pageId ?? "community.page"; return <Widget src="hack.near/widget/community.page" props={{ accountId, communityId: "hack.near", contractId: "mint.sharddog.near", h1: "RC DAO",
-        h2: "2.0", tagline: "building better support systems", mainColor: "#888", buttonText: "Join", link: "https://near.social/rc-dao.near/widget/com.page" }} />
+const accountId = props.accountId ?? context.accountId;
+
+const ownerId = props.ownerId ?? "hack.near";
+
+const pageId = props.pageId ?? "community.page";
+
+return (
+  <Widget
+    src="hack.near/widget/community.page"
+    props={{
+      accountId,
+      communityId: "hack.near",
+      contractId: "mint.sharddog.near",
+      h1: "Regional Community",
+      h2: "DAOs",
+      tagline: "building better support systems",
+      mainColor: "#888",
+      buttonText: "Join",
+      link: "https://near.social/rc-dao.near/widget/com.page",
+    }}
+  />
+);
