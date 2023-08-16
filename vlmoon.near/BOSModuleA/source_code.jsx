36c05d9dbf7160081a6e633ac399431d716fa033
vlmoon.near <vlmoon.near>
2023-08-16 01:13:44 +0000

    Update BOSModuleA by vlmoon.near at 2023-08-16T01:13:44.603Z

--- vlmoon.near/BOSModuleA/source_code.jsx
+++ vlmoon.near/BOSModuleA/source_code.jsx
@@ -4,5 +4,8 @@ return (
   <>
     <h1>Module A</h1>
     <h2>{JSON.stringify(dependencies.routes) ?? "no routes"}</h2>
+    <button onClick={dependencies.routesNavigator.moduleB}>
+      Go to the next module
+    </button>
   </>
 );
