6d76aa928fae8583a2287ff60933fdd2680733ff
harrydhillon.near <harrydhillon.near>
2023-08-16 13:27:14 +0000

    Update Keypom.Tickets.AddTicketModal by harrydhillon.near at 2023-08-16T13:27:14.988Z

--- harrydhillon.near/Keypom.Tickets.AddTicketModal/source_code.jsx
+++ harrydhillon.near/Keypom.Tickets.AddTicketModal/source_code.jsx
@@ -1,12 +1,35 @@
-State.init({ fieldVal: "" });
+State.init({ fieldVal: "", selected: 20 });
 const Label = styled.p`
 font-size: 16px;
  translate: 3px 8px;
 font-style: normal;
 font-weight: 500;
 `;
+
+const NearAmountBackground = styled.button`
+  color:${(props) => (props.selected ? "#00A7E4" : "#475569")};
+  padding: 5px 20px;
+  margin-right:10px;
+  border-radius:10px;
+  background-color:  ${(props) => (props.selected ? "#EFFAFD" : "#F8FAFC")};
+  border:${(props) => (props.selected ? "2px solid #00A7E4 " : "0px")};
+`;
+
+const nearAmount = [20, 50, 100, 200];
+
+const nearLabel = (amount, selected) => (
+  <NearAmountBackground
+    onClick={() => {
+      State.update({ selected: amount });
+    }}
+    selected={selected}
+  >
+    {amount}
+  </NearAmountBackground>
+);
+
 const AddTicketModal = (
-  <div style={{maxHeight:'80vh',overflowY:"auto"}}>
+  <div style={{ maxHeight: "80vh", overflowY: "auto" }}>
     <Widget
       props={{
         label: "Ticket Name *",
@@ -121,15 +144,40 @@ const AddTicketModal = (
       }}
       src="harrydhillon.near/widget/Keypom.Components.Input"
     />
-    <p style={{ fontWeight: "500", marginBottom: 0 }}>
+    <p style={{ fontWeight: "500", marginBottom: 5 }}>
       Price per ticket (NEAR) *
     </p>
-    <p style={{ color: "gray" }}>
-      Your receive 20 NEAR. Buyer pays 20.187 NEAR.
+    <div style={{ display: "flex", alignItems: "center", marginBottom: 10 }}>
+      {nearAmount.map((item) => (
+        <div>{nearLabel(item, parseInt(item) === state.selected)}</div>
+      ))}
+     {nearLabel("Custom Amount",state.selected==="Custom Amount")}
+    </div>
+    {state.selected==="Custom Amount" && <>
+        <Widget
+      props={{
+        label: "Custom Near Amount",
+        inputProps: {
+          placeholder: "Please enter the near amount you want to sell your tickets for",
+          required: true,
+          value: state.ticketName,
+          onChange: (e) => State.update({ nearAmountForTicket: e.target.value }),
+        },
+        labelProps: {
+          style: {
+            fontWeight: "500",
+          },
+        },
+      }}
+      src="harrydhillon.near/widget/Keypom.Components.Input"
+    />
+    </>}
+    {state.selected!=='Custom Amount'&& (
+          <p style={{ color: "gray" }}>
+      Your receive {state.selected} NEAR. Buyer pays {state.selected}.187 NEAR.
     </p>
-     <p style={{ fontWeight: "500", marginBottom: 0 }}>
-      Ticket Artwork
-    </p>
+    )}
+    <p style={{ fontWeight: "500", marginBottom: 0 }}>Ticket Artwork</p>
     <Widget src="harrydhillon.near/widget/Keypom.Components.Imageupload" />
     <button
       onClick={() => {
