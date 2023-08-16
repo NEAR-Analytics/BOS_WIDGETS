10468e77b43ed2b9fd4d0210f00cd27a89208e37
vlmoon.near <vlmoon.near>
2023-08-16 11:29:13 +0000

    Update BOSModuleA by vlmoon.near at 2023-08-16T11:29:13.947Z

--- vlmoon.near/BOSModuleA/source_code.jsx
+++ vlmoon.near/BOSModuleA/source_code.jsx
@@ -1,17 +1,25 @@
 const dependencies = props.dependencies || ["No dependencies"];
 const routesNavigator = props.routesNavigator;
 const appThemeService = props.appThemeService;
+const appTheme = appThemeService.getTheme();
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 800px;
+  background-color: ${appTheme.colors().backgroundColor};
+`;
 
 return (
-  <>
+  <Body>
     <h1>Module A</h1>
     <h2>{JSON.stringify(dependencies)}</h2>
     <button onClick={routesNavigator.moduleB}>Go to the next module</button>
-    <button onClick={appThemeService.lightTheme}>
+    <button onClick={appThemeService.switchToLightTheme}>
       Change App Theme Mode To Light
     </button>
-    <button onClick={appThemeService.dartkTheme}>
+    <button onClick={appThemeService.switchToDarkTheme}>
       Change App Theme Mode To Dark
     </button>
-  </>
+  </Body>
 );
