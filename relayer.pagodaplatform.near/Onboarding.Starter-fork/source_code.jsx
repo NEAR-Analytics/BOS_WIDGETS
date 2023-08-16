50e57058fb796316f4b8a69a19dbc711113adf7d
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-16 05:45:02 +0000

    Update Onboarding.Starter-fork by relayer.pagodaplatform.near at 2023-08-16T05:45:02.069Z

--- relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
+++ relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
@@ -7,47 +7,47 @@ State.init({
   chainId: undefined,
 });
 
-if (
-  state.chainId === undefined &&
-  ethers !== undefined &&
-  Ethers.send("eth_requestAccounts", [])[0]
-) {
-  Ethers.provider()
-    .getNetwork()
-    .then((chainIdData) => {
-      if (chainIdData?.chainId) {
-        State.update({ chainId: chainIdData.chainId });
-      }
-    });
-  console.log("here");
-} else {
-  console.log("hehe");
-}
-if (state.chainId !== undefined && state.chainId !== 11155111) {
-  return <p>Switch to Ethereum Sepolia</p>;
-}
-
-// const createEthereumContract = () => {
-//   const provider = new ethers.providers.Web3Provider(window.ethereum);
-//   const signer = provider.getSigner();
-//   const walletContract = new ethers.Contract(
-//     contractAddress,
-//     contractABI,
-//     signer
-//   );
-
-//   return walletContract;
-// };
-
-// if (state.contract === undefined) {
-//   createEthereumContract().then((walletContract) => {
-//     State.update({
-//       contract: walletContract,
+// if (
+//   state.chainId === undefined &&
+//   ethers !== undefined &&
+//   Ethers.send("eth_requestAccounts", [])[0]
+// ) {
+//   Ethers.provider()
+//     .getNetwork()
+//     .then((chainIdData) => {
+//       if (chainIdData?.chainId) {
+//         State.update({ chainId: chainIdData.chainId });
+//       }
 //     });
-//   });
+//   console.log("here");
+// } else {
+//   console.log("hehe");
 // }
+// if (state.chainId !== undefined && state.chainId !== 11155111) {
+//   return <p>Switch to Ethereum Sepolia</p>;
+// }
+
+const createEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const walletContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+
+  return walletContract;
+};
+
+if (state.contract === undefined) {
+  createEthereumContract().then((walletContract) => {
+    State.update({
+      contract: walletContract,
+    });
+  });
+}
 return (
   <>
-    <p>{state.chainId ? state.chainId : "hh"}</p>
+    <p>{"hh"}</p>
   </>
 );
