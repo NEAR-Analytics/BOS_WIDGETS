dbe4c013ee67c9905a19b578243fd0d76d4e0312
vlmoon.near <vlmoon.near>
2023-08-16 09:30:04 +0000

    Update BOSModuleB by vlmoon.near at 2023-08-16T09:30:04.789Z

--- vlmoon.near/BOSModuleB/source_code.jsx
+++ vlmoon.near/BOSModuleB/source_code.jsx
@@ -4,6 +4,7 @@ const routesNavigator = props.routesNavigator;
 return (
   <>
     <h1>Module B</h1>
+    <h2>{JSON.stringify(dependencies)}</h2>
     <button onClick={routesNavigator.moduleC}>Go to the next module</button>
   </>
 );
