2bfea3861d14f7108719c1108ed0b8e2f1cdab69
mob.near <mob.near>
2023-08-16 18:50:23 +0000

    Update MainPage.N.Content by mob.near at 2023-08-16T18:50:23.698Z

--- mob.near/MainPage.N.Content/source_code.jsx
+++ mob.near/MainPage.N.Content/source_code.jsx
@@ -35,23 +35,39 @@ if (state.feedIndex === 0) {
   }
 }
 
+const Nav = styled.div`
+  .nav-pills {
+    font-weight: 500;
+    --bs-nav-pills-border-radius: 0;
+    --bs-nav-pills-link-active-color: var(--bs-nav-link-color);
+    --bs-nav-pills-link-active-bg: rgba(13, 110, 253, 0.1);
+  }
+  .nav-item:hover {
+    background: rgba(13, 110, 253, 0.15);
+  }
+
+  border-bottom: 1px solid #eee;
+`;
+
 return (
   <>
-    <ul className="nav nav-pills mb-3">
-      {options.map((option, i) => (
-        <li className="nav-item" key={i}>
-          <button
-            className={`nav-link ${state.feedIndex === i ? "active" : ""} ${
-              option.disabled ? "disabled" : ""
-            }`}
-            aria-disabled={!!option.disabled}
-            onClick={() => !option.disabled && State.update({ feedIndex: i })}
-          >
-            {option.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <Nav>
+      <ul className="nav nav-pills nav-fill">
+        {options.map((option, i) => (
+          <li className="nav-item" key={i}>
+            <button
+              className={`nav-link ${state.feedIndex === i ? "active" : ""} ${
+                option.disabled ? "disabled" : ""
+              }`}
+              aria-disabled={!!option.disabled}
+              onClick={() => !option.disabled && State.update({ feedIndex: i })}
+            >
+              {option.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Nav>
     {context.accountId && (
       <Widget src="mob.near/widget/MainPage.N.Compose" props={{}} />
     )}
