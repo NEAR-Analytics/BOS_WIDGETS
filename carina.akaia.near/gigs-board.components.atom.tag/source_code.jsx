725c03285abd1d3d44517252876d413dc8cde228
carina.akaia.near <carina.akaia.near>
2023-08-16 16:24:33 +0000

    Update gigs-board.components.atom.tag by carina.akaia.near at 2023-08-16T16:24:33.014Z

--- carina.akaia.near/gigs-board.components.atom.tag/source_code.jsx
+++ carina.akaia.near/gigs-board.components.atom.tag/source_code.jsx
@@ -52,13 +52,13 @@ function href(widgetName, linkProps) {
 }
 /* END_INCLUDE: "common.jsx" */
 
-const Tag = ({ linkTo, tag }) => (
-  <a href={href(linkTo, { tag })}>
+const Tag = ({ label }) => (
+  <a href={href("Feed", { label }, label)}>
     <span
       class="badge me-1 text-dark fw-normal"
       style={{ border: "1px solid #D0D5DD" }}
     >
-      {tag}
+      {label}
     </span>
   </a>
 );
