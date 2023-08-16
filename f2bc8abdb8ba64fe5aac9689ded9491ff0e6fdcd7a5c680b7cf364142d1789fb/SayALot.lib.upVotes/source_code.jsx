2f15ff53f41d4bdb87225aa5a1cd13403052decd
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-16 20:44:15 +0000

    Update SayALot.lib.upVotes by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-16T20:44:15.005Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.lib.upVotes/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.lib.upVotes/source_code.jsx
@@ -5,6 +5,7 @@ const testAction = `test_${prodAction}`;
 const action = isTest ? testAction : prodAction;
 
 function getUpVotes(props) {
+  console.log("in");
   const { elementReactedId, createdInteraction } = props;
 
   const allVotes = Social.index(action, elementReactedId, {
