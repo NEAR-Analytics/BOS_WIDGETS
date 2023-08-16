1619e6843b66813449fefde9d8f8b01329b861da
mob.near <mob.near>
2023-08-16 19:42:06 +0000

    Update Welcome.N by mob.near at 2023-08-16T19:42:06.013Z

--- mob.near/Welcome.N/source_code.jsx
+++ mob.near/Welcome.N/source_code.jsx
@@ -1,39 +1,51 @@
+const Nav = styled.div`
+  .nav-pills {
+    background: rgba(0, 0, 0, 0.02);
+    font-weight: 500;
+    --bs-nav-pills-border-radius: 0;
+    --bs-nav-pills-link-active-color: var(--bs-nav-link-color);
+    --bs-nav-pills-link-active-bg: rgba(13, 110, 253, 0.1);
+  }
+  .nav-item:hover {
+    background: rgba(13, 110, 253, 0.15);
+  }
+
+  border-bottom: 2px solid #ccc;
+`;
 return (
   <>
-    <ul
-      className="nav nav-pills nav-fill mb-3 d-lg-none"
-      id="pills-tab"
-      role="tablist"
-    >
-      <li className="nav-item" role="presentation">
-        <button
-          className="nav-link active"
-          id="pills-feed-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#pills-feed"
-          type="button"
-          role="tab"
-          aria-controls="pills-feed"
-          aria-selected="true"
-        >
-          Feed
-        </button>
-      </li>
-      <li className="nav-item" role="presentation">
-        <button
-          className="nav-link"
-          id="pills-explore-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#pills-explore"
-          type="button"
-          role="tab"
-          aria-controls="pills-explore"
-          aria-selected="false"
-        >
-          Explore
-        </button>
-      </li>
-    </ul>
+    <Nav className="">
+      <ul className="nav nav-pills nav-fill" id="pills-tab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link active"
+            id="pills-feed-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-feed"
+            type="button"
+            role="tab"
+            aria-controls="pills-feed"
+            aria-selected="true"
+          >
+            Feed
+          </button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link"
+            id="pills-explore-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-explore"
+            type="button"
+            role="tab"
+            aria-controls="pills-explore"
+            aria-selected="false"
+          >
+            Menu
+          </button>
+        </li>
+      </ul>
+    </Nav>
     <div className="tab-content row p-0" id="pills-tabContent">
       <div
         className="tab-pane show active d-lg-block col-lg-8"
