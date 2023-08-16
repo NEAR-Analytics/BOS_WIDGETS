47859a3df7f43cf0fecdc3c10efb74b8adeb6f3c
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-16 05:43:26 +0000

    Update Onboarding.Starter-fork by relayer.pagodaplatform.near at 2023-08-16T05:43:26.921Z

--- relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
+++ relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
@@ -1,39 +1,31 @@
-// const { contractAddress, contractABI } = fetch(
-//   "https://raw.githubusercontent.com/test1883/walley/main/build/contracts/contracts/Wallet.sol/Wallet.json"
-// ).body;
+const { contractAddress, contractABI } = fetch(
+  "https://raw.githubusercontent.com/test1883/walley/main/build/contracts/contracts/Wallet.sol/Wallet.json"
+).body;
 
-// State.init({
-//   contract: undefined,
-//   chainId: undefined,
-// });
-const sender = "0xf0db85e02dbc2d2c9b86dfc245cd9c2caf9a901b";
-if (state.balance === undefined && sender) {
+State.init({
+  contract: undefined,
+  chainId: undefined,
+});
+
+if (
+  state.chainId === undefined &&
+  ethers !== undefined &&
+  Ethers.send("eth_requestAccounts", [])[0]
+) {
   Ethers.provider()
-    .getBalance(sender)
-    .then((balance) => {
-      State.update({ balance: Big(balance).div(Big(10).pow(18)).toFixed(2) });
+    .getNetwork()
+    .then((chainIdData) => {
+      if (chainIdData?.chainId) {
+        State.update({ chainId: chainIdData.chainId });
+      }
     });
+  console.log("here");
+} else {
+  console.log("hehe");
 }
-
-// if (
-//   state.chainId === undefined &&
-//   ethers !== undefined &&
-//   Ethers.send("eth_requestAccounts", [])[0]
-// ) {
-//   Ethers.provider()
-//     .getNetwork()
-//     .then((chainIdData) => {
-//       if (chainIdData?.chainId) {
-//         State.update({ chainId: chainIdData.chainId });
-//       }
-//     });
-//   console.log("here");
-// } else {
-//   console.log("hehe");
-// }
-// if (state.chainId !== undefined && state.chainId !== 11155111) {
-//   return <p>Switch to Ethereum Sepolia</p>;
-// }
+if (state.chainId !== undefined && state.chainId !== 11155111) {
+  return <p>Switch to Ethereum Sepolia</p>;
+}
 
 // const createEthereumContract = () => {
 //   const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -56,6 +48,6 @@ if (state.balance === undefined && sender) {
 // }
 return (
   <>
-    <p>{state.balance}</p>
+    <p>{state.chainId ? state.chainId : "hh"}</p>
   </>
 );
