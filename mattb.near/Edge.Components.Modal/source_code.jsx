b2f6a44b9ac6ccdc806f1e7c11c88265eb38000d
mattb.near <mattb.near>
2023-08-17 22:20:32 +0000

    Update Edge.Components.Modal by mattb.near at 2023-08-17T22:20:32.829Z

--- mattb.near/Edge.Components.Modal/source_code.jsx
+++ mattb.near/Edge.Components.Modal/source_code.jsx
@@ -1,6 +1,18 @@
-const { slot } = props;
+const { slot, onClose, initState } = props;
+
+State.init({
+  closed: initState || true,
+});
 
 const Slot = () => <>{slot}</>;
+
+const Box = styled.div`
+    background-color:#fff;
+    padding:1.5rem;
+    min-height:200px;
+    width:100%;
+    max-width:400px;
+`;
 
 const Overlay = styled.div`
     width:100%;
@@ -8,11 +20,25 @@ const Overlay = styled.div`
     display:flex;
     align-items:center;
     justify-content:center;
-
+    background-color:rgba(0,0,0,.5);
 `;
 
 return (
-  <Overlay>
-    <Slot />
-  </Overlay>
+  <>
+    {!state.closed && (
+      <Overlay
+        onClick={() => {
+          State.update({ closed: !state.closed });
+
+          if (typeof onClose === "function") {
+            onClose(state.closed);
+          }
+        }}
+      >
+        <Box>
+          <Slot />
+        </Box>
+      </Overlay>
+    )}
+  </>
 );
