398bd43b918c69424e7db0ac9dec6bc2ddfca5c3
harrydhillon.near <harrydhillon.near>
2023-08-17 14:24:28 +0000

    Update Keypom.Tickets.index by harrydhillon.near at 2023-08-17T14:24:28.118Z

--- harrydhillon.near/Keypom.Tickets.index/source_code.jsx
+++ harrydhillon.near/Keypom.Tickets.index/source_code.jsx
@@ -3,43 +3,179 @@ display:flex;
 justify-content:space-between;
 align-items:center;
 `;
-State.init({ isCreateTicketModalOpen: false });
+
+const Table = styled.table`
+  width: 100%;
+  border-radius: 8px;
+  overflow:hidden;
+  border:0.1px solid #30C9F3;
+  margin: 20px 0;
+  border-collapse: separate;
+  border-spacing: 0;
+`;
+
+const TableHead = styled.thead`
+  background-color: #F8FAFC;
+`;
+
+const TableRow = styled.tr`
+
+`;
+
+const TableHeader = styled.th`
+  padding: 8px;
+  font-weight:500;
+  text-align: left;
+`;
+
+const TableCell = styled.td`
+  padding: 8px;
+`;
+
+const UnstyledButton = styled.button`
+  background-color:transparent;
+  border-width:0px;
+  color:#00A7E4;
+`;
+
+const ActionButton = styled.button`
+  color: black;
+  padding: 5px 10px;
+  padding-bottom:8px;
+  background-color:transparent;
+  border:1px solid lightgray;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+State.init({
+  isCreateTicketModalOpen: false,
+  tickets: [],
+});
 
 const createTickets = () => {
   return (
-    <CreateTicketContainer>
-      <div>
-        <p style={{ marginBottom: 0, fontSize: 20, fontWeight: "600" }}>
-          Create Tickets *
-        </p>
-        <p style={{ marginBottom: 0 }}>
-          Create custom tickets for [event name].
-        </p>
-      </div>
-      <button
-        onClick={() => {
-          State.update({
-            isCreateTicketModalOpen: true,
-          });
-        }}
-        style={{ backgroundColor: "black", borderWidth: 0 }}
-      >
-        Create Ticket
-      </button>
-      {state.isCreateTicketModalOpen && (
-        <Widget
-          src="harrydhillon.near/widget/Keypom.Tickets.AddTicketModal"
-          props={{
-            isOpen: state.isCreateTicketModalOpen,
-            onClose: () => {
-              State.update({
-                isCreateTicketModalOpen: false,
-              });
-            },
+    <>
+      <CreateTicketContainer>
+        <div>
+          <p style={{ marginBottom: 0, fontSize: 20, fontWeight: "600" }}>
+            Create Tickets *
+          </p>
+          <p style={{ marginBottom: 0 }}>
+            Create custom tickets for [event name].
+          </p>
+        </div>
+        <button
+          onClick={() => {
+            State.update({
+              isCreateTicketModalOpen: true,
+            });
           }}
-        />
+          style={{ backgroundColor: "black", borderWidth: 0 }}
+        >
+          Create Ticket
+        </button>
+        {state.isCreateTicketModalOpen && (
+          <Widget
+            src="harrydhillon.near/widget/Keypom.Tickets.AddTicketModal"
+            props={{
+              isOpen: state.isCreateTicketModalOpen,
+              onSave: (ticketData) => {
+                if (state.edit) {
+                  const allTickets = [...state.tickets];
+                  allTickets[state.ticketToEdit] = ticketData;
+                  State.update({
+                    tickets: allTickets,
+                    isCreateTicketModalOpen: false,
+                    editMode: false,
+                    editVal: null,
+                  });
+                } else {
+                  State.update({
+                    tickets: [...state.tickets, ticketData],
+                    isCreateTicketModalOpen: false,
+                  });
+                }
+              },
+              editMode: state.editMode,
+              editVal: state.editVal,
+              onClose: () => {
+                State.update({
+                  isCreateTicketModalOpen: false,
+                });
+              },
+            }}
+          />
+        )}
+      </CreateTicketContainer>
+      {state.tickets.length !== 0 && (
+        <div style={{ marginTop: 10 }}>
+          <p style={{ marginBottom: 0, fontSize: 20, fontWeight: "600" }}>
+            Your Tickets
+          </p>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableHeader>Ticket</TableHeader>
+                <TableHeader />
+                <TableHeader># of tickets</TableHeader>
+                <TableHeader>Price (NEAR)</TableHeader>
+                <TableHeader />
+              </TableRow>
+            </TableHead>
+            <tbody style={{ borderRadius: 10 }}>
+              {state.tickets.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell style={{ width: "30%" }}>
+                    {item.ticketName}
+                  </TableCell>
+                  <TableCell>
+                    <UnstyledButton>Preview Ticket</UnstyledButton>
+                  </TableCell>
+                  <TableCell>{item.numberOfTickets}</TableCell>
+                  <TableCell>{item.ticketPricing}</TableCell>
+                  <TableCell>
+                    <ActionButton
+                      onClick={() => {
+                        State.update({
+                          tickets: state.tickets.filter(
+                            (item, idx) => idx !== index
+                          ),
+                        });
+                      }}
+                    >
+                      <Widget src="harrydhillon.near/widget/Keypom.Tickets.DeleteSVG" />
+                    </ActionButton>
+                    <ActionButton
+                      onClick={() => {
+                        const currentTicket = state.tickets[index];
+                        State.update({
+                          tickets: [...state.tickets, currentTicket],
+                        });
+                      }}
+                    >
+                      <Widget src="harrydhillon.near/widget/Keypom.Tickets.CloneSVG" />
+                    </ActionButton>
+                    <ActionButton
+                      onClick={() => {
+                        State.update({
+                          isCreateTicketModalOpen: true,
+                          editMode: true,
+                          ticketToEdit: index,
+                          editVal: item,
+                        });
+                      }}
+                    >
+                      <Widget src="harrydhillon.near/widget/Keypom.Tickets.EditSVG" />
+                    </ActionButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </tbody>
+          </Table>
+        </div>
       )}
-    </CreateTicketContainer>
+    </>
   );
 };
 
