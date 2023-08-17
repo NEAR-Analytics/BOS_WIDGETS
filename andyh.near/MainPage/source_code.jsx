69b9ebe2cf54c4d639c6b852f9b971d652b818d7
andyh.near <andyh.near>
2023-08-17 20:53:19 +0000

    Update MainPage by andyh.near at 2023-08-17T20:53:19.672Z

--- andyh.near/MainPage/source_code.jsx
+++ andyh.near/MainPage/source_code.jsx
@@ -69,7 +69,7 @@ return (
     <div className="row mb-3">
       <div className="col-md-8">
         <h4>Follow activity</h4>
-        <Widget src="andyh.near/widget/FollowFeed" isTrusted={true} />
+        {/*<Widget src="andyh.near/widget/FollowFeed" isTrusted={true} />*/}
       </div>
       <div className="col-md-4">
         <h4>Poke activity</h4>
