bdc869a35829f05214172566448178b70f94b24c
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-17 15:05:26 +0000

    Update NDC.Reactions by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-17T15:05:26.648Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.Reactions/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.Reactions/source_code.jsx
@@ -61,13 +61,6 @@ State.init({
   libCalls,
 });
 
-if (
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb-1691530843649" ===
-  elementReactedId
-) {
-  console.log("reactionsData: ", state.reactionsData);
-}
-
 // ========= UPDATE REACTION STATISTICS IF USER VOTED RIGHT NOW=========
 function updateReactionsStatisticsIfUserVoted(newEmoji) {
   let currentReactionsStatistics = state.reactionsData.reactionsStatistics;
@@ -110,10 +103,6 @@ function updateReactionsStatisticsIfUserVoted(newEmoji) {
       quantity: oldReactionStat.quantity - 1,
       text: oldReactionStat.text,
     };
-
-    console.log("everyOtherReactionStat: ", everyOtherReactionStat);
-    console.log("getNewStatForEmojiReacted: ", getNewStatForEmojiReacted());
-    console.log("newValueForOldReactionStat: ", newValueForOldReactionStat);
 
     newReactionsStatistics = everyOtherReactionStat
       ? [
