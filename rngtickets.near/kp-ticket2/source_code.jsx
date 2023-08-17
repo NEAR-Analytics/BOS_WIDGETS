c87d7cf0cbddeb80400cbe44c495232814335f94
rngtickets.near <rngtickets.near>
2023-08-17 22:48:27 +0000

    Update kp-ticket2 by rngtickets.near at 2023-08-17T22:48:27.894Z

--- rngtickets.near/kp-ticket2/source_code.jsx
+++ rngtickets.near/kp-ticket2/source_code.jsx
@@ -3,23 +3,36 @@ const qrPayload = `v2.keypom.near/${props.pk}`;
 const qrSourceImg = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${qrPayload}&format=svg`;
 
 return (
-  <div className="container row mt-1 mb-1">
+  <div className="container row mt-3 mb-3">
     <div align="center">
       <div
         style={{
-          backgroundColor: "#fff",
+          backgroundColor: "#ecc2ff",
           width: "300px",
-          height: "400px",
+          height: "450px",
           border: "5px solid black",
           borderRadius: "25px",
         }}
       >
+        <br />
         <br />
         <text style={{ fontSize: 26, fontFamily: "Avenir" }}>
           🎟️ This is your Ticket 🔑
         </text>
         <br />
         <img src={qrSourceImg} alt="" title="" style={{ paddingTop: 15 }} />
+        <br />
+        <br />
+        <text style={{ fontSize: 18, fontFamily: "Avenir" }}>
+          NEAR Toronto: Rooftop Rendezvous🦩
+        </text>
+        <br></br>
+        <text style={{ fontSize: 11, fontFamily: "Avenir" }}>
+          In collaboration with: NEAR Foundation, TENAMINT, Seahorse Social, KPW
+          Comms, Proof of Vibes, Marma J Foundation, NEAR Foundation, Music
+          Feast, GDS Consulting, Brothers Grimm Ultra Premium Vodka,
+          CreativesDAO.
+        </text>
       </div>
     </div>
   </div>
