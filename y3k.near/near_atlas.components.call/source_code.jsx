e15de4ad24c3e65c2422165ce8820ad22b6e3b18
y3k.near <y3k.near>
2023-08-18 15:15:30 +0000

    Update near_atlas.components.call by y3k.near at 2023-08-18T15:15:30.878Z

---
+++ y3k.near/near_atlas.components.call/source_code.jsx
@@ -0,0 +1,11 @@
+let user_account = context.accountId;
+
+console.log(context);
+return (
+  <>
+    <div class="container border border-info p-3 text-center min-vw-100">
+      <h1>Hello</h1>
+      <p> {user_account} </p>
+    </div>
+  </>
+);
