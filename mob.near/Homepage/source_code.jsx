834f748b85340286bbb0b8e06e339bb69df2ac2f
mob.near <mob.near>
2023-08-18 23:47:23 +0000

    Update Homepage by mob.near at 2023-08-18T23:47:23.072Z

--- mob.near/Homepage/source_code.jsx
+++ mob.near/Homepage/source_code.jsx
@@ -8,4 +8,4 @@ if (homepage === null) {
   return "Loading";
 }
 
-return <Widget src={homepage ?? "mob.near/widget/Welcome"} props={props} />;
+return <Widget src={homepage ?? "mob.near/widget/N"} props={props} />;
