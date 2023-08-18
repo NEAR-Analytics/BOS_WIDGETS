86083229370815670be2c9dc945ce322150ec0f3
golas.near <golas.near>
2023-08-18 18:21:37 +0000

    Update N-NotificationsSettingsHeader by golas.near at 2023-08-18T18:21:37.129Z

---
+++ golas.near/N-NotificationsSettingsHeader/source_code.jsx
@@ -0,0 +1,27 @@
+const Header = styled.div`
+    display: flex;
+    padding: 56px 0px 48px 0px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 24px; 
+`;
+
+const Title = styled.div`
+    font: var(--text-xl);
+    color: var(--sand-light-12, #1B1B18); 
+    font-weight: 500;
+`;
+
+const Text = styled.div`
+    font: var(--text-base);
+    color: var(--sand-light-11, #706F6C); 
+    font-weight: 450; 
+`;
+
+return (
+  <Header>
+    <Title>{props.title}</Title>
+    <Text>{props.text}</Text>
+  </Header>
+);
