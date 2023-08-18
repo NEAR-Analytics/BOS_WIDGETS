5ca66b00c40cde74e6c9e22c50c1ed45701171a2
golas.near <golas.near>
2023-08-18 18:25:04 +0000

    Update N-NotificationsSettingsListItem by golas.near at 2023-08-18T18:25:04.610Z

---
+++ golas.near/N-NotificationsSettingsListItem/source_code.jsx
@@ -0,0 +1,62 @@
+const Card = styled.div`
+  display: flex;
+  width: 506px;
+  padding: 32px 0px;
+  align-items: flex-start;
+  gap: 64px; 
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 24px;
+  flex: 1 0 0; 
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px; 
+  justify-content: center;
+  align-items: center;
+  gap: 8px; 
+`;
+
+const Text = styled.div`
+  justify-content: start;
+  align-items: center;
+
+  & > div:nth-of-type(1) {
+    color: var(--sand-light-12, #1B1B18); 
+    font-weight: 600; 
+    line-height: 150%;
+  }
+  & > div:nth-of-type(2) {
+    color: var(--sand-light-11, #706F6C); 
+    font-weight: 450; 
+    line-height: 150%;
+  }
+`;
+
+const Switch = styled.div`
+  display: flex;
+  width: 40px;
+  height: 16px;
+  align-items: center;
+  gap: 8px; 
+`;
+
+return (
+  <Card>
+    <Content>
+      <Icon>X</Icon>
+      <Text>
+        <div>Push notifications</div>
+        <div>
+          Push notifications are delivered in real-time to your enabled browser
+          or device.
+        </div>
+      </Text>
+    </Content>
+    <Switch>S</Switch>
+  </Card>
+);
