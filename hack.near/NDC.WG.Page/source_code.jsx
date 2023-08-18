939a519c0b7dd1cea7b54282de3285a52717c644
hack.near <hack.near>
2023-08-18 20:14:27 +0000

    Update NDC.WG.Page by hack.near at 2023-08-18T20:14:27.271Z

--- hack.near/NDC.WG.Page/source_code.jsx
+++ hack.near/NDC.WG.Page/source_code.jsx
@@ -103,6 +103,14 @@ if (state.start) {
   State.update({ start: false });
 }
 
+const widget = {
+  styledComponents: "nomination.ndctools.near/widget/NDC.StyledComponents",
+};
+
+const Header = styled.div`
+  background: black;
+`;
+
 const Container = styled.div`
   padding: 30px 0;
   margin: 0;
@@ -156,14 +164,61 @@ const Loader = () => (
 return (
   <>
     <div>
-      <Widget
-        src={widgets.header}
-        props={{
-          type: "WG",
-          isVerified: !sbt,
-          showModal: state.showModal,
-        }}
-      />
+      <Header className="d-flex p-3 px-4 align-items-center rounded justify-content-between">
+        <Widget
+          src="mob.near/widget/Image"
+          props={{
+            image: {
+              url: "https://pbs.twimg.com/profile_images/1622941553839816707/nmf3MWw1_400x400.jpg",
+            },
+            alt: "ndc",
+            style: {
+              height: "42px",
+              objectFit: "cover",
+            },
+            fallbackUrl:
+              "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
+          }}
+        />
+        {isVerified ? (
+          <Widget
+            src={widget.styledComponents}
+            props={{
+              Link: {
+                text: "Verify as Human",
+                href: "https://i-am-human.app/",
+              },
+            }}
+          />
+        ) : (
+          <Toolbar>
+            {props.createdGroup ? (
+              <Widget
+                src={widget.styledComponents}
+                props={{
+                  Button: {
+                    className: "danger primary",
+                    text: "Delete Work Group",
+                    onClick: () => State.update({ showModalDelete: true }),
+                    icon: <i class="bi bi-trash"></i>,
+                  },
+                }}
+              />
+            ) : (
+              <Widget
+                src={widget.styledComponents}
+                props={{
+                  Button: {
+                    text: "Create Work Group",
+                    onClick: () => State.update({ showModal: true }),
+                    icon: <i class="bi bi-plus-lg"></i>,
+                  },
+                }}
+              />
+            )}
+          </Toolbar>
+        )}
+      </Header>
       <Container className="d-flex row justify-content-between w-100">
         <Left className="col-lg">
           <H5>Learning Together</H5>
@@ -175,15 +230,45 @@ return (
             }}
           />
           <div>
-            {!state.sbt && (
+            {!state.sbt ? (
               <div className="mt-5">
                 <Widget
                   src={widgets.verifyHuman}
                   props={{
-                    title: "To Create a Work Group",
+                    title: "Participate in a Work Group",
                     small: true,
                   }}
                 />
+              </div>
+            ) : (
+              <div className="mt-5">
+                <Toolbar>
+                  {props.createdGroup ? (
+                    <Widget
+                      src={widget.styledComponents}
+                      props={{
+                        Button: {
+                          className: "danger primary",
+                          text: "Delete Work Group",
+                          onClick: () =>
+                            State.update({ showModalDelete: true }),
+                          icon: <i class="bi bi-trash"></i>,
+                        },
+                      }}
+                    />
+                  ) : (
+                    <Widget
+                      src={widget.styledComponents}
+                      props={{
+                        Button: {
+                          text: "Create Work Group",
+                          onClick: () => State.update({ showModal: true }),
+                          icon: <i class="bi bi-plus-lg"></i>,
+                        },
+                      }}
+                    />
+                  )}
+                </Toolbar>
               </div>
             )}
           </div>
