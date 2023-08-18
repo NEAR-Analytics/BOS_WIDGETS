91944ee7370a40be3dc68f73ad9d463cf192d528
golas.near <golas.near>
2023-08-18 18:55:55 +0000

    Update N-NotificationsSettingsApplications by golas.near at 2023-08-18T18:55:55.219Z

--- golas.near/N-NotificationsSettingsApplications/source_code.jsx
+++ golas.near/N-NotificationsSettingsApplications/source_code.jsx
@@ -1,1 +1,19 @@
-return <div>Hello World</div>;
+const applicationNotificationsTypes = props.applicationNotificationsTypes || [];
+
+return (
+  <>
+    <Widget
+      src="golas.near/widget/N-NotificationsSettingsHeader"
+      props={{
+        title: "Application Notifications",
+        text: "Turn notifications on or off for specific applications. New applications you’ve connected to will have notifications turned on by default.",
+      }}
+    />
+    {applicationNotificationsTypes.map((type) => (
+      <Widget
+        src="golas.near/widget/N-NotificationsSettingsListItem"
+        props={type}
+      />
+    ))}
+  </>
+);
