d8e36eff56b32c44e68f8d4f33b155acecf3920e
zavodil.near <zavodil.near>
2023-08-18 22:52:06 +0000

    Update subscribe by zavodil.near at 2023-08-18T22:52:06.888Z

--- zavodil.near/subscribe/source_code.jsx
+++ zavodil.near/subscribe/source_code.jsx
@@ -1,10 +1,11 @@
-// read previousTimestamp
-// premium.social.near/badge/premium/
-// premium.social.near/badge/premium/accounts/${context.accountId}
-const previousTimestamp = Social.get(
+const previousTimestampString = Social.get(
   `premium.social.near/badge/premium/accounts/${context.accountId}`,
   "final"
 );
+
+const previousTimestamp = previousTimestampString
+  ? Number(previousTimestampString)
+  : null;
 
 const price = "96000000000000000000000000";
 const priceWholesale = "84000000000000000000000000";
@@ -12,7 +13,6 @@ const priceWholesale = "84000000000000000000000000";
 const yearInMs = 31556926000;
 
 const getEndDate = (purchasedPeriod) => {
-  console.log("purchasedPeriod", purchasedPeriod);
   const previousTimestamp = state.previousTimestamp ?? Date.now();
   const nextTimestamp = previousTimestamp + purchasedPeriod;
   const dateObj = new Date(nextTimestamp);
@@ -47,171 +47,26 @@ State.init({
   priceWholesale,
 });
 
-const Theme = styled.div`
-    .container {
-background-color:#fff;
-background-position:0 -100px;
-background-repeat:no-repeat;
-background-image:url(https://pluminite.mypinata.cloud/ipfs/QmPDQvqLE9ZHAMZjPSYLybHprVfiEPCfo9LHy9bVfZZRtd);
-background-size:cover;
-background-position:bottom;
-height:400px;
-padding:30px 12px 100px
+if (state.theme === undefined) {
+  const css = fetch(
+    "https://ipfs.near.social/ipfs/bafkreickjanlocwi253a2qcqiep6dfh4pnvytiwdmi3jv4b7wylcu4nc3u"
+  ).body;
+  if (!css) return "";
+
+  State.update({ theme: styled.div`${css}` });
 }
 
-.container-block {
-margin:0 auto;
-max-width:500px
-}
+const Theme = state.theme;
 
-.header {
-border-radius:12px;
-background-color:#000;
-color:#fff;
-padding-top:12px;
-padding-right:16px;
-font-size:20px;
-font-weight:800;
-display:flex;
-gap:10px;
-padding-left:16px;
-width:100%
-}
-
-.header-text {
-flex:4;
-font-family:inherit;
-word-wrap:break-word;
-padding-bottom:10px
-}
-
-.illustration {
-flex:1;
-width:150px;
-min-height:50px;
-display:inline-block
-}
-
-.illustration img {
-width:100%
-}
-
-.radiogroup {
-margin-top:24px;
-margin-bottom:12px;
-width:100%
-}
-
-.selector {
-display:flex;
-width:100%;
-background-color:#2a2d30;
-padding:10px;
-border-radius:12px;
-align:center
-}
-
-.menu-active {
-background-color:#000;
-color:rgba(231,233,234,1.00)!important;
-border-radius:12px
-}
-
-.menu-active .menu-item-text {
-color:rgba(231,233,234,1.00)!important
-}
-
-.menu-item .sale {
-color:#c2f1dc;
-line-height:12px;
-font-size:11px;
-font-weight:700;
-margin-left:10px
-}
-
-.menu-item {
-padding:4px;
-flex:1 1 0;
--webkit-box-align:center;
-align-items:center;
-justify-content:center;
-display:flex;
-cursor:pointer;
-text-align:center;
-font-weight:700
-}
-
-.menu-item .menu-item-text {
-color:#71767b;
-justify-content:center;
-display:flex;
-align-items:center
-}
-
-.disabled {
-pointer-events:none
-}
-
-.subscribe {
-border:#000 1px solid;
-margin:24px auto 0;
-background-color:#eff3f4;
-width:75%;
-min-height:36px;
--webkit-box-align:center;
-text-align:center;
-border-radius:999px;
-display:flex;
-justify-content:center;
-align-items:center
-}
-
-.subscribe-text {
-cursor:pointer;
-justify-content:center;
-font-weight:700;
-flex-grow:1;
-color:#0f1419;
-flex-direction:row
-}
-
-.old-price {
-text-decoration-line:line-through;
-font-weight:initial
-}
-
-.subscribe-amount-block {
-width:75%;
-color:#fff;
-font-weight:700;
-text-align:center;
-display:flex;
-justify-content:flex-start;
-align-items:center;
-flex-direction:column;
-margin:0 auto;
-margin-top:24px
-}
-
-.subscribe-amount-text {
-margin-bottom:8px;
-color:#71767b;
-font-size:15px
-}
-
-.subscribe-amount {
-width:100%;
-text-align:center
-}
-
-.action {
-}
-
-.premium-details {
-padding-top:20px;
-text-align:center
-}
-`;
+const customOnChange = (e) => {
+  const customAmount = isNumber(e.target.value)
+    ? parseFloat(e.target.value)
+    : "";
+  State.update({
+    purchasedPeriod: customAmount ? convertNearToMs(customAmount) : 0,
+    customAmount,
+  });
+};
 
 const isNumber = (n) => {
   return !isNaN(parseFloat(n)) && isFinite(n);
@@ -238,8 +93,6 @@ const convertNearToMs = (amount) => {
 
   const res = amountYocto.mul(Big(yearInMs)).div(price).toFixed(0);
 
-  console.log("r", res);
-
   return Number(res);
 };
 
@@ -254,7 +107,7 @@ return (
           <div class="illustration">
             <img
               draggable="false"
-              src="https://pluminite.mypinata.cloud/ipfs/QmeaN2XXfa9fkaVw9QBpew69MKT7SxJtsgedoqttTfyGLm"
+              src="https://ipfs.near.social/ipfs/bafkreihfjwfijsedf4r75iwn77yua5ey3ufv6swgcjupqeigh2fw4wqm4a"
             />
           </div>
         </div>
@@ -272,7 +125,7 @@ return (
                   State.update({ type: "annually", purchasedPeriod: yearInMs })
                 }
               >
-                Annually<div class="sale">SAVE 12%</div>
+                Year<div class="sale">SAVE 12%</div>
               </div>
             </div>
             <div
@@ -286,11 +139,13 @@ return (
                 })
               }
             >
-              <div class="menu-item-text">Monthly</div>
+              <div class="menu-item-text">Month</div>
             </div>
             <div
               class={`menu-item ${state.type == "custom" ? "menu-active" : ""}`}
-              onClick={() => State.update({ type: "custom" })}
+              onClick={() =>
+                State.update({ type: "custom", purchasedPeriod: 0 })
+              }
             >
               <div class="menu-item-text">Custom</div>
             </div>
@@ -322,17 +177,7 @@ return (
                 type="text"
                 class="subscribe-amount"
                 value={state.customAmount}
-                onChange={(e) => {
-                  const customAmount = isNumber(e.target.value)
-                    ? parseFloat(e.target.value)
-                    : "";
-                  State.update({
-                    customAmount,
-                    purchasedPeriod: customAmount
-                      ? convertNearToMs(customAmount)
-                      : 0,
-                  });
-                }}
+                onChange={customOnChange}
               />
               <div class="subscribe">
                 <div
