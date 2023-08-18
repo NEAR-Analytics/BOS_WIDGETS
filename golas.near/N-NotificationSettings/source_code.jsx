0e9afbd590a2a8de07871396f3e2fe2ccd3465c5
golas.near <golas.near>
2023-08-18 18:25:45 +0000

    Update N-NotificationSettings by golas.near at 2023-08-18T18:25:45.977Z

--- golas.near/N-NotificationSettings/source_code.jsx
+++ golas.near/N-NotificationSettings/source_code.jsx
@@ -1,27 +1,18 @@
-const Header = styled.div`
-    display: flex;
-    padding: 56px 0px 48px 0px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 24px; 
-`;
-
-const Title = styled.div`
-    font: var(--text-xl);
-    color: var(--sand-light-12, #1B1B18); 
-    font-weight: 500;
-`;
-
-const Text = styled.div`
-    font: var(--text-base);
-    color: var(--sand-light-11, #706F6C); 
-    font-weight: 450; 
-`;
-
 return (
-  <Header>
-    <Title>Notification Settings</Title>
-    <Text>Configure your notifications for activity on near.org</Text>
-  </Header>
+  <>
+    <Widget
+      src="golas.near/widget/N-NotificationsSettingsHeader"
+      props={{
+        title: "Notification Settings",
+        text: "Configure your notifications for activity on near.org",
+      }}
+    />
+    <Widget
+      src="golas.near/widget/N-NotificationsSettingsListItem"
+      props={{
+        title: "Push notifications",
+        text: "Push notifications are delivered in real-time to your enabled browser or device.",
+      }}
+    />
+  </>
 );
