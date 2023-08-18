785c392917cf81257f30bdc583e7d99f42290f60
guessme.near <guessme.near>
2023-08-18 05:01:51 +0000

    Update ZKEVMSwap.zkevm-swap by guessme.near at 2023-08-18T05:01:51.923Z

--- guessme.near/ZKEVMSwap.zkevm-swap/source_code.jsx
+++ guessme.near/ZKEVMSwap.zkevm-swap/source_code.jsx
@@ -566,6 +566,8 @@ const switchNetwork = (chainId, dex) => {
   State.update({
     selectedDex: dex,
     forceReload: true,
+    inputAssetTokenId: "0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035",
+    outputAssetTokenId: "0x4f9a0e7fd2bf6067db6994cf12e4495df938e6e9",
   });
 };
 
