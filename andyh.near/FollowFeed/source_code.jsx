99595f9d190381a70de578f3e61bd9079328dcb0
andyh.near <andyh.near>
2023-08-18 23:07:43 +0000

    Update FollowFeed by andyh.near at 2023-08-18T23:07:43.345Z

--- andyh.near/FollowFeed/source_code.jsx
+++ andyh.near/FollowFeed/source_code.jsx
@@ -43,6 +43,10 @@ const renderItem = (a) => {
 
 return (
   <div>
-    <Widget src="andyh.near/widget/IndexFeed" props={{ index, renderItem }} />
+    <Widget
+      src="andyh.near/widget/IndexFeed"
+      props={{ index, renderItem }}
+      isTrusted={true}
+    />
   </div>
 );
