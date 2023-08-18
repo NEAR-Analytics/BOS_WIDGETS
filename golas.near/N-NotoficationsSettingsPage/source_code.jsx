cc6742a83e420ab0bc587c1aafda1edf53d6dc83
golas.near <golas.near>
2023-08-18 18:52:54 +0000

    Update N-NotoficationsSettingsPage by golas.near at 2023-08-18T18:52:54.904Z

--- golas.near/N-NotoficationsSettingsPage/source_code.jsx
+++ golas.near/N-NotoficationsSettingsPage/source_code.jsx
@@ -39,22 +39,7 @@ const applicationNotificationsTypes = [
 return (
   <Frame>
     <Notification>
-      <Widget
-        src="golas.near/widget/N-NotificationsSettingsHeader"
-        props={{
-          title: "Notification Settings",
-          text: "Configure your notifications for activity on near.org",
-        }}
-      />
-      <Widget
-        src="golas.near/widget/N-NotificationsSettingsListItem"
-        props={{
-          title: "Push notifications",
-          text: "Push notifications are delivered in real-time to your enabled browser or device.",
-          // temporary
-          icon: "X",
-        }}
-      />
+      <Widget src="golas.near/widget/N-NotificationsSettingsPush" props={{}} />
     </Notification>
     <Application>
       <Widget
