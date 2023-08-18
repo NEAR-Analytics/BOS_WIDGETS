21fbc0f6187db8dadc296f1e56d5429d02ff4187
golas.near <golas.near>
2023-08-18 18:51:17 +0000

    Update N-NotificationSettings by golas.near at 2023-08-18T18:51:17.662Z

--- golas.near/N-NotificationSettings/source_code.jsx
+++ golas.near/N-NotificationSettings/source_code.jsx
@@ -1,75 +1,20 @@
-const Frame = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 40px; 
-`;
-
-const Notification = styled.div``;
-
-const Application = styled.div``;
-
-const applicationNotificationsTypes = [
-  {
-    icon: "A",
-    title: "{ApplicationName}",
-    text: "",
-    state: 1,
-  },
-  {
-    icon: "B",
-    title: "{ApplicationName}",
-    text: "",
-    state: 1,
-  },
-  {
-    icon: "C",
-    title: "{ApplicationName}",
-    text: "",
-    state: 0,
-  },
-  {
-    icon: "D",
-    title: "{ApplicationName}",
-    text: "",
-    state: 0,
-  },
-];
-
 return (
-  <Frame>
-    <Notification>
-      <Widget
-        src="golas.near/widget/N-NotificationsSettingsHeader"
-        props={{
-          title: "Notification Settings",
-          text: "Configure your notifications for activity on near.org",
-        }}
-      />
-      <Widget
-        src="golas.near/widget/N-NotificationsSettingsListItem"
-        props={{
-          title: "Push notifications",
-          text: "Push notifications are delivered in real-time to your enabled browser or device.",
-          // temporary
-          icon: "X",
-        }}
-      />
-    </Notification>
-    <Application>
-      <Widget
-        src="golas.near/widget/N-NotificationsSettingsHeader"
-        props={{
-          title: "Application Notifications",
-          text: "Turn notifications on or off for specific applications. New applications you’ve connected to will have notifications turned on by default.",
-        }}
-      />
-      {applicationNotificationsTypes.map((type) => (
-        <Widget
-          src="golas.near/widget/N-NotificationsSettingsListItem"
-          props={type}
-        />
-      ))}
-    </Application>
-  </Frame>
+  <>
+    <Widget
+      src="golas.near/widget/N-NotificationsSettingsHeader"
+      props={{
+        title: "Notification Settings",
+        text: "Configure your notifications for activity on near.org",
+      }}
+    />
+    <Widget
+      src="golas.near/widget/N-NotificationsSettingsListItem"
+      props={{
+        title: "Push notifications",
+        text: "Push notifications are delivered in real-time to your enabled browser or device.",
+        // temporary
+        icon: "X",
+      }}
+    />
+  </>
 );
