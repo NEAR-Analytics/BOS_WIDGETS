c7a0f861fcc98e6129cee776250950166c2e0ca3
golas.near <golas.near>
2023-08-18 18:20:00 +0000

    Update N-NotificationSettings by golas.near at 2023-08-18T18:20:00.930Z

--- golas.near/N-NotificationSettings/source_code.jsx
+++ golas.near/N-NotificationSettings/source_code.jsx
@@ -1,21 +1,23 @@
 const Header = styled.div`
-display: flex;
-padding: 56px 0px 48px 0px;
-flex-direction: column;
-justify-content: center;
-align-items: flex-start;
-gap: 24px; 
+    display: flex;
+    padding: 56px 0px 48px 0px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 24px; 
 `;
 
 const Title = styled.div`
-align-self: stretch; 
-
-color: var(--sand-light-12, #1B1B18); 
-font-weight: 500;
-line-height: 130%;
+    font: var(--text-xl);
+    color: var(--sand-light-12, #1B1B18); 
+    font-weight: 500;
 `;
 
-const Text = styled.div``;
+const Text = styled.div`
+    font: var(--text-base);
+    color: var(--sand-light-11, #706F6C); 
+    font-weight: 450; 
+`;
 
 return (
   <Header>
