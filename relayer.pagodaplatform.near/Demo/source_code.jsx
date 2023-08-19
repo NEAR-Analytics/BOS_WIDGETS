fce56646cc3f33a49f5f4d8df7860284533fee32
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-19 02:48:50 +0000

    Update Demo by relayer.pagodaplatform.near at 2023-08-19T02:48:50.219Z

---
+++ relayer.pagodaplatform.near/Demo/source_code.jsx
@@ -0,0 +1,11 @@
+const ownerId = "nnmchau.near";
+
+return (
+  <>
+    <h3> Demo </h3>
+    <p> Components can be composed </p>
+    <hr />
+
+    <Widget src={`${ownerId}/widget/Greater`} props={props} />
+  </>
+);
