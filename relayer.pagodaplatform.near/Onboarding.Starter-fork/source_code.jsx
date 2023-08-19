624e8bfcf8b2c1ae634ed8c7e00f8a7d0410455f
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-19 06:57:29 +0000

    Update Onboarding.Starter-fork by relayer.pagodaplatform.near at 2023-08-19T06:57:29.739Z

--- relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
+++ relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
@@ -905,7 +905,7 @@ const approveTransaction = (tokenId, totalAmount, amount) => {
   console.log(parseFloat(amount) - parseFloat(totalAmount));
   console.log(amount);
   console.log(Big(totalAmount).toFixed(20));
-  console.log(amount - Big(totalAmount).toFixed(20));
+  console.log(Big(amount).toFixed(20) - Big(totalAmount).toFixed(20));
   console.log(
     ethers.utils.parseUnits(
       `${Big(amount).toFIxed(20) - Big(totalAmount).toFixed(20)}`,
