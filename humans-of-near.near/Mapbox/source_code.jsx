98591b5bbbee5ecc06d1e3b03f109beb03ba956a
humans-of-near.near <humans-of-near.near>
2023-08-19 11:54:11 +0000

    Update Mapbox by humans-of-near.near at 2023-08-19T11:54:11.306Z

--- humans-of-near.near/Mapbox/source_code.jsx
+++ humans-of-near.near/Mapbox/source_code.jsx
@@ -138,9 +138,10 @@ const code = `
       if(user.social !== "https://social.near.page/u/"){
         state.social = true
       }
+      const profileImageURL = "https://i.near.social/magic/large/https://near.social/magic/img/account/"+ user.accountId;
       
        const HTML = '<div class="popup">'+
-          '<img src="https://humans.nearverselabs.com/Human.png" class="logo" />'+
+          '<img src={profileImageURL} class="logo" />'+
           '<div class="gap-14">'+
             '<h6>'+title+'</h6>'+
             '<div class="gap-16">'+
@@ -148,7 +149,8 @@ const code = `
             '<svg width="29" height="12" viewBox="0 0 29 12" fill="'+(state.social?'white':'grey')+'" xmlns="http://www.w3.org/2000/svg"><path d="M0.240133 7.38517V4.85885L10.3454 0.409091V3.31579L3.42674 6.0933L3.52004 5.94258V6.30144L3.42674 6.15072L10.3454 8.92823V11.8349L0.240133 7.38517ZM28.7599 7.38517L18.6546 11.8349V8.92823L25.5733 6.15072L25.48 6.30144V5.94258L25.5733 6.0933L18.6546 3.31579V0.409091L28.7599 4.85885V7.38517Z"></path></svg>'+
              (state.social? '</a>' : '') +
              (state.twitter? "<a href='"+user.twitter+"' target='_blank' onclick='handleLink("+JSON.stringify(user.twitter)+")'   >  " : '') +
-            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="'+(state.twitter?'white':'grey')+'" d="M22.212 5.656a8.384 8.384 0 0 1-2.401.658A4.195 4.195 0 0 0 21.649 4c-.82.488-1.719.83-2.655 1.015a4.182 4.182 0 0 0-7.126 3.814a11.874 11.874 0 0 1-8.621-4.37a4.168 4.168 0 0 0-.566 2.103c0 1.45.739 2.731 1.86 3.481a4.169 4.169 0 0 1-1.894-.523v.051a4.185 4.185 0 0 0 3.355 4.102a4.205 4.205 0 0 1-1.89.072A4.185 4.185 0 0 0 8.02 16.65a8.394 8.394 0 0 1-6.192 1.732a11.831 11.831 0 0 0 6.41 1.88c7.694 0 11.9-6.373 11.9-11.9c0-.18-.004-.362-.012-.541a8.497 8.497 0 0 0 2.086-2.164Z"/></svg>'+
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="'+(state.twitter?'white':'grey')+'" d="M283.94,167.31l386.39,516.64L281.5,1104h87.51l340.42-367.76L984.48,1104h297.8L874.15,558.3l361.92-390.99
+		h-87.51l-313.51,338.7l-253.31-338.7H283.94z M412.63,231.77h136.81l604.13,807.76h-136.81L412.63,231.77z"/></svg>'+
              (state.twitter? '</a>' : '') +
            '</div>'+
           '</div>'+
