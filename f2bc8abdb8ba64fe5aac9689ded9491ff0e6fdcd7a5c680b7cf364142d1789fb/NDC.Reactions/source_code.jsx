da33ac909195108ca2bb115ba131a956b0d4d1cb
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-08-20 05:13:11 +0000

    Update NDC.Reactions by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-08-20T05:13:11.200Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.Reactions/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.Reactions/source_code.jsx
@@ -64,7 +64,13 @@ State.init({
   loading: false,
   libCalls,
 });
-console.log("reactionsData: ", state.reactionsData);
+
+if (
+  elementReactedId ==
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb-1691530843649"
+) {
+  console.log("reactionsData: ", state.reactionsData);
+}
 
 // ========= UPDATE REACTION STATISTICS IF USER VOTED RIGHT NOW=========
 // function updateReactionsStatisticsIfUserVoted(newEmoji) {
