7894462197a7851da5de4941da533a45d1a2e47e
sharddog.near <sharddog.near>
2023-08-20 21:12:21 +0000

    Update Notification.Item.Like by sharddog.near at 2023-08-20T21:12:21.784Z

---
+++ sharddog.near/Notification.Item.Like/source_code.jsx
@@ -0,0 +1,64 @@
+const { accountId, value } = props;
+
+return (
+  <Widget
+    src="mob.near/widget/Notification.Item.LR"
+    props={{
+      L: (
+        <>
+          liked your
+          {value.item.path === `${context.accountId}/post/main` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`sharddog.near/widget/MainPage.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+            >
+              post
+            </a>
+          ) : value.item.path === `${context.accountId}/post/comment` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`sharddog.near/widget/MainPage.Comment.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+            >
+              comment
+            </a>
+          ) : value.item.path === `${context.accountId}/post/insta` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`sharddog.near/widget/Insta.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+            >
+              insta
+            </a>
+          ) : (
+            "item???"
+          )}
+        </>
+      ),
+      R:
+        value.item.path === `${context.accountId}/post/main` ? (
+          <a
+            className="btn btn-outline-dark"
+            href={`sharddog.near/widget/MainPage.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+          >
+            View post
+          </a>
+        ) : value.item.path === `${context.accountId}/post/comment` ? (
+          <a
+            className="btn btn-outline-dark"
+            href={`sharddog.near/widget/MainPage.Comment.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+          >
+            View comment
+          </a>
+        ) : value.item.path === `${context.accountId}/post/insta` ? (
+          <a
+            className="btn btn-outline-dark"
+            href={`sharddog.near/widget/Insta.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+          >
+            View insta
+          </a>
+        ) : (
+          ""
+        ),
+      ...props,
+    }}
+  />
+);
