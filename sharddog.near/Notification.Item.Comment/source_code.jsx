c242846a6605d0f4db858d5cda80d3b7ffc0ffc2
sharddog.near <sharddog.near>
2023-08-20 21:11:49 +0000

    Update Notification.Item.Comment by sharddog.near at 2023-08-20T21:11:49.449Z

---
+++ sharddog.near/Notification.Item.Comment/source_code.jsx
@@ -0,0 +1,41 @@
+const { accountId, blockHeight, value } = props;
+
+return (
+  <Widget
+    src="mob.near/widget/Notification.Item.LR"
+    props={{
+      L: (
+        <>
+          <a
+            className="fw-bold text-muted"
+            href={`sharddog.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+          >
+            replied
+          </a>{" "}
+          to your
+          {value.item.path === `${context.accountId}/post/main` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`sharddog.near/widget/MainPage.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+            >
+              post
+            </a>
+          ) : (
+            "item???"
+          )}
+        </>
+      ),
+      R: (
+        <>
+          <a
+            className="btn btn-outline-dark"
+            href={`sharddog.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+          >
+            View comment
+          </a>
+        </>
+      ),
+      ...props,
+    }}
+  />
+);
