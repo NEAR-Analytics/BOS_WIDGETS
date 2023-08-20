f1798e8dc3a0df4449aefc2065914a9902bf19ec
sharddog.near <sharddog.near>
2023-08-20 21:08:33 +0000

    Update Notification.Item.Mention by sharddog.near at 2023-08-20T21:08:33.489Z

---
+++ sharddog.near/Notification.Item.Mention/source_code.jsx
@@ -0,0 +1,50 @@
+const { accountId, value, blockHeight } = props;
+
+return (
+  <Widget
+    src="mob.near/widget/Notification.Item.LR"
+    props={{
+      L: (
+        <>
+          mention you in their
+          {value.item.path === `${accountId}/post/main` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`sharddog.near/widget/MainPage.Post.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+            >
+              post
+            </a>
+          ) : value.item.path === `${accountId}/post/comment` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`sharddog.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+            >
+              comment
+            </a>
+          ) : (
+            "item???"
+          )}
+        </>
+      ),
+      R:
+        value.item.path === `${accountId}/post/main` ? (
+          <a
+            className="btn btn-outline-dark"
+            href={`sharddog.near/widget/MainPage.Post.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+          >
+            View post
+          </a>
+        ) : value.item.path === `${accountId}/post/comment` ? (
+          <a
+            className="btn btn-outline-dark"
+            href={`sharddog.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+          >
+            View comment
+          </a>
+        ) : (
+          ""
+        ),
+      ...props,
+    }}
+  />
+);
