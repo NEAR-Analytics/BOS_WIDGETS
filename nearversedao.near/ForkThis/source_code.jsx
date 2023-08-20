70f4287595b9e8601ebcfd02bfd104d6f3aaa22b
nearversedao.near <nearversedao.near>
2023-08-20 13:02:58 +0000

    Update ForkThis by nearversedao.near at 2023-08-20T13:02:58.233Z

--- nearversedao.near/ForkThis/source_code.jsx
+++ nearversedao.near/ForkThis/source_code.jsx
@@ -213,15 +213,4 @@ Don't forget to save!
 
 */
 
-return (
-  <Wrapper>
-    <iframe
-      src="https://discord.com/widget?id=950312985337937941&theme=dark"
-      width="350"
-      height="500"
-      allowtransparency="true"
-      frameborder="0"
-      sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-    ></iframe>
-  </Wrapper>
-);
+return <Wrapper></Wrapper>;
