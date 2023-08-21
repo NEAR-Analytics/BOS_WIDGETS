0d52621a3302453b1b81d982fc341ac46733c9bf
saidulbadhon.near <saidulbadhon.near>
2023-08-21 18:57:24 +0000

    Update Template.LandingPage.ContentItem by saidulbadhon.near at 2023-08-21T18:57:24.617Z

--- saidulbadhon.near/Template.LandingPage.ContentItem/source_code.jsx
+++ saidulbadhon.near/Template.LandingPage.ContentItem/source_code.jsx
@@ -1,11 +1,14 @@
-const Container = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 16px;
+const ItemContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px;
 
-    min-height: 500px;
-    height: 100%;
+  min-height: 500px;
+  height: 100%;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 // const HeaderContainer = styled.div`
@@ -18,32 +21,61 @@ const Container = styled.div`
 //   background-color: blue;
 // `;
 
-const ImageContainer = styled.div`
+const ItemImageContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+`;
 
-    flex: 1;
+const ItemTextContainer = styled.div`
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
 `;
 
 return (
-  <Container>
-    <ImageContainer>
-      <img
-        style={{
-          maxHeight: 400,
-          //   width: "100%",
-          objectFit: "cover",
-          aspectRatio: 1 / 1,
-        }}
-        src={props.image}
-        alt="art"
-      />
-    </ImageContainer>
+  <ItemContainer>
+    {props.rtl ? (
+      <>
+        <ItemImageContainer style={{ backgroundColor: "green" }}>
+          <img
+            style={{
+              maxHeight: 400,
+              objectFit: "cover",
+              aspectRatio: 1 / 1,
+            }}
+            src={props.image}
+            alt="art"
+          />
+        </ItemImageContainer>
 
-    <div style={{ flex: 1 }}>
-      <h2>{props.name}</h2>
-      <p>{props.description}</p>
-    </div>
-  </Container>
+        <ItemTextContainer>
+          <h2>{props.name}</h2>
+          <p>{props.description}</p>
+        </ItemTextContainer>
+      </>
+    ) : (
+      <>
+        <ItemTextContainer>
+          <h2>{props.name}</h2>
+          <p>{props.description}</p>
+        </ItemTextContainer>
+
+        <ItemImageContainer style={{ backgroundColor: "green" }}>
+          <img
+            style={{
+              maxHeight: 400,
+              objectFit: "cover",
+              aspectRatio: 1 / 1,
+            }}
+            src={props.image}
+            alt="art"
+          />
+        </ItemImageContainer>
+      </>
+    )}
+  </ItemContainer>
 );
