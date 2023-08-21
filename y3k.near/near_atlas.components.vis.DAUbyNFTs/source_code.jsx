c4ab0181633618f97d8e7989b0afb34915910a73
y3k.near <y3k.near>
2023-08-21 16:15:20 +0000

    Update near_atlas.components.vis.DAUbyNFTs by y3k.near at 2023-08-21T16:15:20.126Z

---
+++ y3k.near/near_atlas.components.vis.DAUbyNFTs/source_code.jsx
@@ -0,0 +1,111 @@
+let raw_data = fetch(
+  "https://api.flipsidecrypto.com/api/v2/queries/6728e603-7140-48b7-b735-20c0cf6e54ee/data/latest",
+  {
+    subscribe: true,
+    method: "GET",
+    headers: {
+      Accept: "*/*",
+    },
+  }
+);
+
+const data = raw_data.body || [];
+
+let Style = styled.div`
+                `;
+
+// logic start
+
+const sortedData = data.sort((a, b) => {
+  return new Date(a["DATE"]) - new Date(b["DATE"]);
+});
+
+const N_USERS = {};
+const TXS = {};
+const dates = [];
+
+sortedData.map((entry) => {
+  const date = entry["DATE"];
+  N_USERS[date] = entry["N_USERS"];
+  TXS[date] = entry["TXS"];
+  dates.push(date);
+});
+
+// const sortedEntries_N_USERS = Object.entries(N_USERS).sort((a, b) => {
+//   return new Date(a[0]) - new Date(b[0]);
+// });
+// const sortedN_USERS = Object.fromEntries(sortedEntries_N_USERS);
+
+// const sortedEntries_TXS = Object.entries(TXS).sort((a, b) => {
+//   return new Date(a[0]) - new Date(b[0]);
+// });
+// const sortedTXS = Object.fromEntries(sortedEntries_TXS);
+// N_USERS = N_USERS.sort((a, b) => {
+//   return new Date(a["DATE"]) - new Date(b["DATE"]);
+// });
+
+// TXS = TXS.sort((a, b) => {
+//   return new Date(a["DATE"]) - new Date(b["DATE"]);
+// });
+
+// const dates = sortedData.map((entry) => entry["DATE"]);
+
+// console.log(N_USERS);
+
+// logic part-2
+
+const stacked_options = {
+  responsive: true,
+  scales: {
+    y: {
+      stacked: true,
+      grid: {
+        color: "rgb(41,51,64)", // This will change the gridline color
+        borderColor: "rgb(240,255,240)",
+      },
+      ticks: {
+        color: "rgb(240,255,240)", // This will change the axis text label color
+      },
+    },
+    x: {
+      stacked: true,
+      grid: {
+        color: "rgb(41,51,64)", // This will change the gridline color
+      },
+      ticks: {
+        color: "rgb(240,255,240)", // This will change the axis text label color
+      },
+    },
+  },
+};
+const stacked_bar_data = {
+  dates,
+  datasets: [
+    {
+      label: "N# USERS",
+      data: N_USERS,
+      backgroundColor: "rgb(250,164,58)",
+    },
+    {
+      label: "TXS",
+      data: TXS,
+      backgroundColor: "rgb(13,131,171)",
+    },
+  ],
+};
+
+return (
+  <Style>
+    <div className="text-bg-dark container">
+      {data !== null ? (
+        <div className="rounded-4 p-3 mb-4">
+          <div className="">
+            <BarEl options={stacked_options} data={stacked_bar_data} />
+          </div>
+        </div>
+      ) : (
+        <div>Loading ...</div>
+      )}
+    </div>
+  </Style>
+);
