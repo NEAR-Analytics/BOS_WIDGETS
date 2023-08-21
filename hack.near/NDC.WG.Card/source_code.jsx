d1456827a2757ff31174cd9d2da736157e714d05
hack.near <hack.near>
2023-08-21 13:05:28 +0000

    Update NDC.WG.Card by hack.near at 2023-08-21T13:05:28.480Z

--- hack.near/NDC.WG.Card/source_code.jsx
+++ hack.near/NDC.WG.Card/source_code.jsx
@@ -1,4 +1,16 @@
-const { data, api_key, dev } = props;
+const groupData = props.data ?? {
+  title: "Regional Communities",
+  creatorId: "rc-dao.near",
+  groupId: "RC",
+  members: [
+    "abbakaka.near",
+    "igboze_builder.near",
+    "inspiratibiz.near",
+    "james.near",
+    "yuenisd.near",
+  ],
+  daoId: "rc-dao.sputnik-dao.near",
+};
 
 State.init({
   verified: false,
@@ -6,9 +18,8 @@ State.init({
 });
 
 const widgets = {
-  styledComponents: "#/hack.near/widget/NDC.StyledComponents",
-  groupPage: "#/hack.near/widget/NDC.WG.Group.Page",
-  addComment: "#/hack.near/widget/NDC.WG.AddComment",
+  styledComponents: "hack.near/widget/NDC.StyledComponents",
+  groupPage: "hack.near/widget/NDC.WG.Group.Page",
 };
 
 const isHuman = Near.view(registry_contract, "is_human", {
@@ -16,15 +27,9 @@ const isHuman = Near.view(registry_contract, "is_human", {
 });
 State.update({ verified: isHuman[0][1].length > 0 });
 
-const httpRequestOpt = {
-  headers: { "x-api-key": api_key },
-};
-
 function handleShare() {
   State.update({ shareText: "Copied" });
-  clipboard.writeText(
-    "https://near.org/#/hack.near/widget/NDC.WG.Page"
-  );
+  clipboard.writeText("#/hack.near/widget/NDC.WG.Page");
 }
 
 const Card = styled.div`
@@ -252,21 +257,6 @@ const TimestampText = styled.div`
     font-weight: 600;
   }
 `;
-const CommentsCounter = styled.p`
-  width: 96px;
-  height: 24px;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 12px;
-  line-height: 24px;
-  margin: 0px;
-  text-align: right;
-  background: linear-gradient(90deg, #9333ea 0%, #4f46e5 100%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-clip: text;
-  text-fill-color: transparent;
-`;
 const ButtonsContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -351,11 +341,6 @@ const Wrapper = styled.div`
   }
 `;
 
-const canUpvote = () =>
-  state.verified &&
-  context.accountId &&
-  context.accountId != data.?.creator;
-
 const trimText = (text, limit) => {
   if (!text) return "";
 
@@ -366,187 +351,34 @@ const trimText = (text, limit) => {
   return `${trimmed}${ending}`;
 };
 
-const groupThing = [
-  {
-    title: "Charter",
-    desc: data.groupData.Charter,
-  },
-  {
-    title: "Strategy to develop the NEAR ecosystem",
-    desc: data.groupData.Twitter,
-  },
-  {
-    title: "View and pledge on the issue of User Experience and Accessibility",
-    desc: data.groupData.,
-  },
-  {
-    title: "View and pledge on the issue of Economic Growth and Innovation",
-    desc: data.groupData.Key_Issue_2,
-  },
-  {
-    title: "View and pledge on the issue of Marketing and Outreach",
-    desc: data.groupData.Key_Issue_3,
-  },
-  {
-    title: "Other Platform",
-    desc: data.groupData.addition_platform,
-  },
-];
-
 return (
   <Wrapper className="p-2 col-lg-4 col-md-6 col-sm-12">
     <Card>
-      {state.showModal && (
-        <Widget
-          src={widgets.addComment}
-          props={{
-            candidateOrReplay: true,
-            username: data.creator,
-            onClickConfirm: () => State.update({ showModal: false }),
-            onClickCancel: () => State.update({ showModal: false }),
-          }}
-        />
-      )}
       <HeaderCard className="d-flex justify-content-between w-100">
         <div className="d-flex align-items-center gap-2 w-100 justify-content-between">
           <Widget
             src="mob.near/widget/ProfileImage"
             props={{
-              accountId: data.groupData?.profileAccount.substring(1),
+              accountId: groupData.accountId,
               imageClassName: "rounded-circle w-100 h-100",
               style: { minWidth: "45px", height: "45px" },
             }}
           />
           <HeaderContent>
             <UserLink
-              href={`${widgets.groupPage}?group=${
-                data.group
-              }&accountId=${data.group.creator}${dev ? "&dev=true" : ""}`}
+              href={`${widgets.groupPage}?group=${groupData.groupId}&accountId=${groupData.creatorId}`}
             >
-              <GroupName>{data.profileData?.name}</GroupName>
-              <GroupCreator>{data.creator}</GroupCreator>
+              <GroupName>{groupData.title}</GroupName>
+              <GroupCreator>{groupData.creatorId}</GroupCreator>
             </UserLink>
           </HeaderContent>
-
-          <Widget
-            src={widgets.styledComponents}
-            props={{
-              Button: {
-                disabled: !canUpvote(),
-                text: `+${data.upVoteData?.upvotes ?? 0}`,
-                className: `${
-                  context.accountId && state.voted ? "primary" : "secondary"
-                } dark`,
-                size: "sm",
-                onClick: handleUpVote,
-                icon: <i className="bi bi-hand-thumbs-up"></i>,
-              },
-            }}
-          />
         </div>
       </HeaderCard>
-      <CollapseGroup className="w-100">
-        <CollapseGroupContent>
-          <CollapseGroupText>Group Affiliations</CollapseGroupText>
-          <GroupTagContainer className="w-100 d-flex flex-wrap">
-            {JSON.parse(data.groupData?.afiliation).map((data) => (
-              <>
-                {data.company_name && (
-                  <Widget
-                    src={widgets.styledComponents}
-                    props={{
-                      Tag: { title: data.company_name },
-                    }}
-                  />
-                )}
-              </>
-            ))}
-          </GroupTagContainer>
-        </CollapseGroupContent>
-      </CollapseGroup>
-      <Groups>
-        <GroupsContent>
-          <GroupsHeader>
-            <GroupsTitle>Platform</GroupsTitle>
-          </GroupsHeader>
-          <GroupsContainer>
-            {groupThing.map((issue, i) => (
-              <div className="w-100" key={i}>
-                <GroupTitle>{issue.title}</GroupTitle>
-                <GroupDescription className="text-secondary">
-                  <Widget
-                    src="mob.near/widget/SocialMarkdown"
-                    props={{ text: trimText(issue.desc) }}
-                  />
-                </GroupDescription>
-                <GroupSeparator />
-              </div>
-            ))}
-          </GroupsContainer>
-        </GroupsContent>
-      </Groups>
       <LowerSection>
         <LowerSectionContainer>
-          {data.groupData.tags.length > 0 && (
-            <Groups>
-              <GroupsContent>
-                <GroupsHeader>
-                  <GroupsTitle>Tags</GroupsTitle>
-                </GroupsHeader>
-                <div className="d-flex w-100">
-                  <TagSection>
-                    {data.groupData.tags
-                      .trim()
-                      .split(",")
-                      .map((data) => (
-                        <>
-                          {data && (
-                            <Widget
-                              src={widgets.styledComponents}
-                              props={{
-                                Tag: { title: data },
-                              }}
-                            />
-                          )}
-                        </>
-                      ))}
-                  </TagSection>
-                </div>
-              </GroupsContent>
-            </Groups>
-          )}
-          <ButtonsLowerSection>
-            <TextLowerSectionContainer className="align-items-center">
-              <i className="bi bi-clock"></i>
-              {data.timestamp && (
-                <TimestampText>
-                  <span>
-                    {new Date(data.timestamp).toDateString()}
-                  </span>
-                  <span>by</span>
-                  <b>{data.creator}</b>
-                </TimestampText>
-              )}
-            </TextLowerSectionContainer>
-          </ButtonsLowerSection>
           <div className="d-flex w-100 align-items-center">
-            {!data.preview && (
+            {!groupData.preview && (
               <div className="d-flex w-100 gap-2 justify-content-between">
-                <Widget
-                  src={widgets.styledComponents}
-                  props={{
-                    Button: {
-                      disabled: !state.verified,
-                      size: "sm",
-                      className:
-                        "secondary dark w-100 justify-content-center text-nowrap",
-                      onClick: () => {
-                        !data.preview ? State.update({ showModal: true }) : "";
-                      },
-                      icon: <i className="bi bi-chat-square-text-fill"></i>,
-                    },
-                  }}
-                />
                 <Widget
                   src={widgets.styledComponents}
                   props={{
@@ -554,12 +386,8 @@ return (
                       text: "View",
                       size: "sm",
                       className: "primary w-100 justify-content-center",
-                      href: `${widgets.groupPage}?group=${
-                        data.group
-                      }&accountId=${data.creator}${
-                        dev ? "&dev=true" : ""
-                      }`,
-                      icon: <i className="bi bi-eye fs-6"></i>,
+                      href: `${widgets.groupPage}?group=${groupData.groupId}&accountId=${groupData.creatorId}`,
+                      icon: <i className="bi bi-eye-fill fs-6"></i>,
                     },
                   }}
                 />
