b408f93d7394d0a723b0093e9ed57db04490c573
alsakhaev.near <alsakhaev.near>
2023-08-21 17:22:05 +0000

    Update ExampleButton by alsakhaev.near at 2023-08-21T17:22:05.129Z

--- alsakhaev.near/ExampleButton/source_code.jsx
+++ alsakhaev.near/ExampleButton/source_code.jsx
@@ -1,5 +1,5 @@
 return (
-  <button style={{ backgroundColor: "#0f0" }} ref="forwardedRef">
+  <button style={{ backgroundColor: "#0f0" }} onClick={props.onClick}>
     {props.label ?? "Default Label"}
   </button>
 );
