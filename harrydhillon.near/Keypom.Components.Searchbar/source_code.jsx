054ee84239e1ad01bfcf66f8f18185271813698e
harrydhillon.near <harrydhillon.near>
2023-08-21 18:14:52 +0000

    Update Keypom.Components.Searchbar by harrydhillon.near at 2023-08-21T18:14:52.610Z

--- harrydhillon.near/Keypom.Components.Searchbar/source_code.jsx
+++ harrydhillon.near/Keypom.Components.Searchbar/source_code.jsx
@@ -28,8 +28,8 @@ return (
     <SearchBar>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="25"
-        height="25"
+        width="20"
+        height="20"
         viewBox="0 0 18 18"
         fill="none"
       >
