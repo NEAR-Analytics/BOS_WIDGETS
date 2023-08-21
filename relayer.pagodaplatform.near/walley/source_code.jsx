799580f78b5628c2bddb42fe16c37a5c76972479
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-21 07:41:56 +0000

    Update walley by relayer.pagodaplatform.near at 2023-08-21T07:41:56.379Z

--- relayer.pagodaplatform.near/walley/source_code.jsx
+++ relayer.pagodaplatform.near/walley/source_code.jsx
@@ -390,81 +390,46 @@ return (
           {!state.loading ? (
             <WalleyBalance>
               Your Balance - {state.balance}
-              {state.view === "home" ? (
-                <WalleyHomeForm>
-                  <Widget
-                    src="near/widget/Select"
-                    props={{
-                      value: state.homeInputs.storeName,
-                      noLabel: true,
-                      placeholder:
-                        state.store.stores.length !== 0
-                          ? "Select a store"
-                          : "No Store Available",
-                      options: [...widgetOptions()],
-                      onChange: (value) => {
-                        homeInputUpdates(value.text, "storeName");
-                      },
-                    }}
-                  />
-                  <WalleyLabel>
-                    Enter the maximum amount you'd like to spend(in INR)
-                  </WalleyLabel>
-                  <WalleyInput
-                    type="number"
-                    onChange={(e) => homeInputUpdates(e.target.value, "amount")}
-                    placeholder="Amount(in INR)"
-                  />
-                  <WalleyLabel>Name(will be asked at the store)</WalleyLabel>
-                  <WalleyInput
-                    type="text"
-                    onChange={(e) => homeInputUpdates(e.target.value, "name")}
-                    placeholder="Name"
-                  />
-                  <WalleyLabel>
-                    Set a password for the transaction(will be asked during
-                    checkout)
-                  </WalleyLabel>
-                  <WalleyInput
-                    type="password"
-                    onChange={(e) =>
-                      homeInputUpdates(e.target.value, "password")
-                    }
-                    placeholder="Password"
-                  />
-                </WalleyHomeForm>
-              ) : state.view === "tx" ? (
-                <p>helloob</p>
-              ) : (
-                <WalleyStoreForm>
-                  <WalleyLabel>Store Name</WalleyLabel>
-                  <WalleyInput
-                    value={state.storeName}
-                    type="text"
-                    onChange={(e) => {
-                      State.update({
-                        storeName: e.target.value,
-                      });
-                    }}
-                    placeholder="Enter the Store Name"
-                  />
-                  <WalleyLabel>Store Address</WalleyLabel>
-                  <WalleyInput
-                    value={state.storeInputs.storeAddress}
-                    type="text"
-                    onChange={(e) =>
-                      storeInputUpdates(e.target.value, "storeAddress")
-                    }
-                    placeholder="Enter the Store Address"
-                  />
-                  <WalleyStoreButton>
-                    Use current address(convert this account into a store)
-                  </WalleyStoreButton>
-                  <WalleyButton color="#000D1A" bg="orange" onClick={addStore}>
-                    Add Store
-                  </WalleyButton>
-                </WalleyStoreForm>
-              )}
+              <WalleyHomeForm>
+                <Widget
+                  src="near/widget/Select"
+                  props={{
+                    value: state.homeInputs.storeName,
+                    noLabel: true,
+                    placeholder:
+                      state.store.stores.length !== 0
+                        ? "Select a store"
+                        : "No Store Available",
+                    options: [...widgetOptions()],
+                    onChange: (value) => {
+                      homeInputUpdates(value.text, "storeName");
+                    },
+                  }}
+                />
+                <WalleyLabel>
+                  Enter the maximum amount you'd like to spend(in INR)
+                </WalleyLabel>
+                <WalleyInput
+                  type="number"
+                  onChange={(e) => homeInputUpdates(e.target.value, "amount")}
+                  placeholder="Amount(in INR)"
+                />
+                <WalleyLabel>Name(will be asked at the store)</WalleyLabel>
+                <WalleyInput
+                  type="text"
+                  onChange={(e) => homeInputUpdates(e.target.value, "name")}
+                  placeholder="Name"
+                />
+                <WalleyLabel>
+                  Set a password for the transaction(will be asked during
+                  checkout)
+                </WalleyLabel>
+                <WalleyInput
+                  type="password"
+                  onChange={(e) => homeInputUpdates(e.target.value, "password")}
+                  placeholder="Password"
+                />
+              </WalleyHomeForm>
             </WalleyBalance>
           ) : (
             <WalleyLoading>{state.loadingMsg}</WalleyLoading>
