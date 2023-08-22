032c989be6db0da78d5f8a7d36b5237e44af8409
mob.near <mob.near>
2023-08-22 01:29:22 +0000

    Update Notification.Item.Like by mob.near at 2023-08-22T01:29:22.662Z

--- mob.near/Notification.Item.Like/source_code.jsx
+++ mob.near/Notification.Item.Like/source_code.jsx
@@ -10,21 +10,21 @@ return (
           {value.item.path === `${context.accountId}/post/main` ? (
             <a
               className="fw-bold text-muted"
-              href={`#/mob.near/widget/MainPage.N.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+              href={`/mob.near/widget/MainPage.N.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
             >
               post
             </a>
           ) : value.item.path === `${context.accountId}/post/comment` ? (
             <a
               className="fw-bold text-muted"
-              href={`#/mob.near/widget/MainPage.N.Comment.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+              href={`/mob.near/widget/MainPage.N.Comment.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
             >
               comment
             </a>
           ) : value.item.path === `${context.accountId}/post/insta` ? (
             <a
               className="fw-bold text-muted"
-              href={`#/mob.near/widget/Insta.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+              href={`/mob.near/widget/Insta.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
             >
               insta
             </a>
@@ -36,22 +36,22 @@ return (
       R:
         value.item.path === `${context.accountId}/post/main` ? (
           <a
-            className="btn btn-outline-dark"
-            href={`#/mob.near/widget/MainPage.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+            className="btn btn-outline-dark rounded-5"
+            href={`/mob.near/widget/MainPage.N.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
           >
             View post
           </a>
         ) : value.item.path === `${context.accountId}/post/comment` ? (
           <a
-            className="btn btn-outline-dark"
-            href={`#/mob.near/widget/MainPage.Comment.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+            className="btn btn-outline-dark rounded-5"
+            href={`/mob.near/widget/MainPage.N.Comment.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
           >
             View comment
           </a>
         ) : value.item.path === `${context.accountId}/post/insta` ? (
           <a
-            className="btn btn-outline-dark"
-            href={`#/mob.near/widget/Insta.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+            className="btn btn-outline-dark rounded-5"
+            href={`/mob.near/widget/Insta.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
           >
             View insta
           </a>
