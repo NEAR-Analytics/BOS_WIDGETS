50a8d5d34d28cd34d26d96b9655f48583dfe420a
dapplets.near <dapplets.near>
2023-08-22 13:28:31 +0000

    Update Web3Hackfest-Paywall-Content by dapplets.near at 2023-08-22T13:28:31.843Z

--- dapplets.near/Web3Hackfest-Paywall-Content/source_code.jsx
+++ dapplets.near/Web3Hackfest-Paywall-Content/source_code.jsx
@@ -1,1 +1,11 @@
-return <div>Hello World</div>;
+const Wrapper = styled.div`
+  .text {
+    color: #f00;
+  }
+`;
+
+return (
+  <Wrapper>
+    <div className="text">Hello World</div>
+  </Wrapper>
+);
