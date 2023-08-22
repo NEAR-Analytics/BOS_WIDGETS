ef0892acf843c7a50e0bd02dd807bfd3537d5635
dapplets.near <dapplets.near>
2023-08-22 14:21:43 +0000

    Update Web3Hackfest-Paywall-Content by dapplets.near at 2023-08-22T14:21:43.240Z

--- dapplets.near/Web3Hackfest-Paywall-Content/source_code.jsx
+++ dapplets.near/Web3Hackfest-Paywall-Content/source_code.jsx
@@ -1,11 +1,27 @@
+State.init({ isVisible: false });
+
 const Wrapper = styled.div`
   .text {
     color: #f00;
   }
 `;
 
+if (props.post.id !== "1691462269182611456") {
+  return null;
+}
+
 return (
   <Wrapper>
-    <div className="text">{props.post.id}</div>
+    {state.isVisible ? (
+      <img
+        onClick={() => State.update({ isVisible: false })}
+        src={`https://miscellaneous.s3-website.fr-par.scw.cloud/web3hackfest-2023/${props.post.id}-original.png`}
+      />
+    ) : (
+      <img
+        onClick={() => State.update({ isVisible: true })}
+        src={`https://miscellaneous.s3-website.fr-par.scw.cloud/web3hackfest-2023/${props.post.id}-blur.png`}
+      />
+    )}
   </Wrapper>
 );
