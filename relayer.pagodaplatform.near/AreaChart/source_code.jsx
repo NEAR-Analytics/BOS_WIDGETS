145893a5251eebcab1b56baf1722c41239b3b6cc
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-22 16:37:44 +0000

    Update AreaChart by relayer.pagodaplatform.near at 2023-08-22T16:37:44.759Z

--- relayer.pagodaplatform.near/AreaChart/source_code.jsx
+++ relayer.pagodaplatform.near/AreaChart/source_code.jsx
@@ -36,7 +36,7 @@ const plot = Plot.plot({
   style: {
     // fontSize: 35,
     maxWidth: isMobile? 'unset' : undefined,
-    paddingLeft: isMobile? 400 : undefined,
+    paddingLeft: isMobile? 550 : undefined,
   },
   marks: [
     Plot.ruleY([0]),
