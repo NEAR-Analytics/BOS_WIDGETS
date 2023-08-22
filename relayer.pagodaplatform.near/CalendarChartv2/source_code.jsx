adda820de1605fb67f5adba43a4a95ab4c22111e
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-22 15:33:10 +0000

    Update CalendarChartv2 by relayer.pagodaplatform.near at 2023-08-22T15:33:10.593Z

--- relayer.pagodaplatform.near/CalendarChartv2/source_code.jsx
+++ relayer.pagodaplatform.near/CalendarChartv2/source_code.jsx
@@ -2,9 +2,9 @@ const { dateColumn, dataColumn, data, legendMax, legendMin, label, title } =
   props;
 const code = `
 <!-- observerable plot -->
-<div id="myplot" style="width: 100%; display: flex; align-items: center; justify-content: center;">
+<div id="myplot" style="width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
 
-    ${props.title ? <strong>{props.title}</strong> : ""}
+    ${props.title ? "<strong>{props.title}</strong>" : ""}
     </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/7.8.5/d3.min.js" integrity="sha512-M7nHCiNUOwFt6Us3r8alutZLm9qMt4s9951uo8jqO4UwJ1hziseL6O3ndFyigx6+LREfZqnhHxYjKRJ8ZQ69DQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script type="module">
