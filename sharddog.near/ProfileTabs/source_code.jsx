76f45bc89ad7e0b13301810b8465c2a755321bfd
sharddog.near <sharddog.near>
2023-08-22 03:17:49 +0000

    Update ProfileTabs by sharddog.near at 2023-08-22T03:17:49.211Z

---
+++ sharddog.near/ProfileTabs/source_code.jsx
@@ -0,0 +1,37 @@
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+if (profile === null) {
+  return "Loading";
+}
+
+const description = profile.description;
+
+const pills = [
+  { id: "posts", title: "Posts" },
+  { id: "nfts", title: "NFTs" },
+  { id: "widget", title: "Widgets" },
+];
+
+return (
+  <>
+    <div className="col-lg-8 mx-auto">
+      {description && (
+        <div className="border rounded-4 p-3 pb-0 mb-3 text-break">
+          <h4>
+            <i class="bi bi-pin-angle" /> Bio
+          </h4>
+          <Markdown text={description} />
+        </div>
+      )}
+      <Widget
+        src="sharddog.near/widget/MainPage.Feed.Beta"
+        props={{ accounts: [accountId] }}
+      />
+    </div>
+  </>
+);
