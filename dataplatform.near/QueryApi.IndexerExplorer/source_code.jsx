a757dd3d572f8e5106cb82ecf566eb4627cb7f09
dataplatform.near <dataplatform.near>
2023-08-22 22:55:33 +0000

    Update QueryApi.IndexerExplorer by dataplatform.near at 2023-08-22T22:55:33.657Z

--- dataplatform.near/QueryApi.IndexerExplorer/source_code.jsx
+++ dataplatform.near/QueryApi.IndexerExplorer/source_code.jsx
@@ -1,70 +1,207 @@
-const accountId = props.accountId || context.accountId;
-const indexerName = props.indexerName;
-const editUrl = `https://near.org/#/dataplatform.near/widget/QueryApi.App?selectedIndexerPath=${accountId}/${indexerName}&view=editor-window`;
-const statusUrl = `https://near.org/#/dataplatform.near/widget/QueryApi.App?selectedIndexerPath=${accountId}/${indexerName}&view=indexer-status`;
-const playgroundLink = `https://cloud.hasura.io/public/graphiql?endpoint=https://near-queryapi.api.pagoda.co/v1/graphql&header=x-hasura-role%3A${accountId.replaceAll(
-  ".",
-  "_"
-)}`;
-const Card = styled.div`
-  position: relative;
-  width: 100%;
-  border-radius: 12px;
-  background: #fff;
-  border: 1px solid #eceef0;
-  box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
-    0px 1px 2px rgba(16, 24, 40, 0.06);
-  overflow: hidden;
+const limitPerPage = 5;
+let totalIndexers = 0;
+const accountId = context.accountId;
+State.init({
+  currentPage: 0,
+  selectedTab: props.tab || "all",
+  total_indexers: 0,
+  my_indexers: [],
+  all_indexers: [],
+});
+
+if (props.tab && props.tab !== state.selectedTab) {
+  State.update({
+    selectedTab: props.tab,
+  });
+}
+
+Near.asyncView(`queryapi.dataplatform.near`, "list_indexer_functions").then(
+  (data) => {
+    const indexers = [];
+    const total_indexers = 0;
+    Object.keys(data.All).forEach((accountId) => {
+      Object.keys(data.All[accountId]).forEach((functionName) => {
+        indexers.push({
+          accountId: accountId,
+          indexerName: functionName,
+        });
+        total_indexers += 1;
+      });
+    });
+
+    let my_indexers = indexers.filter(
+      (indexer) => indexer.accountId === accountId
+    );
+    // const results = indexers.slice(
+    //   0,
+    //   state.currentPage * limitPerPage + limitPerPage
+    // );
+    State.update({
+      my_indexers: my_indexers,
+      all_indexers: indexers,
+      total_indexers: total_indexers,
+    });
+  }
+);
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+  padding-bottom: 4px;
+  padding-top: 4px;
 `;
 
-const CardBody = styled.div`
-  padding: 16px;
+const Header = styled.div`
   display: flex;
-  gap: 16px;
-  align-items: center;
+  flex-direction: column;
+  gap: 12px;
+  min-width: 200px;
+  max-width: 500px;
+`;
 
-  > * {
-    min-width: 0;
+const H1 = styled.h1`
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 39px;
+  color: #11181c;
+  margin: 0;
+`;
+
+const H2 = styled.h2`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  color: #687076;
+  margin: 0;
+`;
+
+const Text = styled.p`
+  margin: 0;
+  line-height: 1.5rem;
+  color: ${(p) => (p.bold ? "#11181C" : "#687076")} !important;
+  font-weight: ${(p) => (p.bold ? "600" : "400")};
+  font-size: ${(p) => (p.small ? "12px" : "14px")};
+  overflow: ${(p) => (p.ellipsis ? "hidden" : "")};
+  text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "")};
+  white-space: ${(p) => (p.ellipsis ? "nowrap" : "")};
+  overflow-wrap: anywhere;
+
+  b {
+    font-weight: 600;
+    color: #11181c;
+  }
+
+  &[href] {
+    display: inline-flex;
+    gap: 0.25rem;
+
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
   }
 `;
 
-const CardContent = styled.div`
-  width: 100%;
-`;
-
-const CardFooter = styled.div`
+const Items = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 16px;
-  padding: 16px;
-  border-top: 1px solid #eceef0;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 24px;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media (max-width: 800px) {
+    grid-template-columns: minmax(0, 1fr);
+  }
 `;
 
-const CardTag = styled.p`
-  margin: 0;
-  font-size: 9px;
-  line-height: 14px;
-  background: #eceef0;
-  color: #687076;
-  font-weight: 400;
-  white-space: nowrap;
-  position: absolute;
-  top: 0;
-  right: 0;
-  border-bottom-left-radius: 3px;
-  padding: 0 4px;
+const Item = styled.div``;
 
-  i {
-    margin-right: 3px;
+const Button = styled.button`
+  display: block;
+  width: 100%;
+  padding: 8px;
+  height: 32px;
+  background: #fbfcfd;
+  border: 1px solid #d7dbdf;
+  border-radius: 50px;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 15px;
+  text-align: center;
+  cursor: pointer;
+  color: #11181c !important;
+  margin: 0;
+
+  &:hover,
+  &:focus {
+    background: #ecedee;
+    text-decoration: none;
+    outline: none;
+  }
+
+  span {
+    color: #687076 !important;
+  }
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  height: 48px;
+  border-bottom: 1px solid #eceef0;
+  overflow: auto;
+  scroll-behavior: smooth;
+
+  @media (max-width: 1200px) {
+    background: #f8f9fa;
+    border-top: 1px solid #eceef0;
+    margin-left: -12px;
+    margin-right: -12px;
+
+    > * {
+      flex: 1;
+    }
+  }
+`;
+
+const TabsButton = styled.a`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  font-weight: 600;
+  font-size: 12px;
+  padding: 0 12px;
+  position: relative;
+  color: ${(p) => (p.selected ? "#11181C" : "#687076")};
+  background: none;
+  border: none;
+  outline: none;
+  text-align: center;
+  text-decoration: none !important;
+  pointer: cursor;
+  &:hover {
+    color: #11181c;
+  }
+
+  &::after {
+    content: "";
+    display: ${(p) => (p.selected ? "block" : "none")};
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 3px;
+    background: #59e692;
   }
 `;
 
 const TextLink = styled.a`
-  display: block;
   margin: 0;
   font-size: 14px;
-  line-height: 18px;
-  color: ${(p) => (p.bold ? "#11181C !important" : "#687076 !important")};
+  line-height: 20px;
   font-weight: ${(p) => (p.bold ? "600" : "400")};
   font-size: ${(p) => (p.small ? "12px" : "14px")};
   overflow: ${(p) => (p.ellipsis ? "hidden" : "visible")};
@@ -78,112 +215,75 @@ const TextLink = styled.a`
   }
 `;
 
-const Text = styled.p`
-  margin: 0;
-  font-size: 14px;
-  line-height: 20px;
-  color: ${(p) => (p.bold ? "#11181C" : "#687076")};
-  font-weight: ${(p) => (p.bold ? "600" : "400")};
-  font-size: ${(p) => (p.small ? "12px" : "14px")};
-  overflow: ${(p) => (p.ellipsis ? "hidden" : "")};
-  text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "")};
-  white-space: nowrap;
-
-  i {
-    margin-right: 3px;
-  }
-`;
-
-const Thumbnail = styled.a`
-  display: block;
-  width: 60px;
-  height: 60px;
-  flex-shrink: 0;
-  border: 1px solid #eceef0;
-  border-radius: 8px;
-  overflow: hidden;
-  outline: none;
-  transition: border-color 200ms;
-
-  &:focus,
-  &:hover {
-    border-color: #d0d5dd;
-  }
-
-  img {
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const TagsWrapper = styled.div`
-  position: relative;
-  margin-top: 4px;
-`;
-
-const ButtonLink = styled.a`
-  padding: 8px;
-  height: 32px;
-  border: 1px solid #d7dbdf;
-  border-radius: 100px;
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 15px;
-  text-align: center;
-  cursor: pointer;
-  color: ${(p) => (p.primary ? "#006ADC" : "#11181C")} !important;
-  background: #fbfcfd;
-  white-space: nowrap;
-
-  &:hover,
-  &:focus {
-    background: #ecedee;
-    text-decoration: none;
-    outline: none;
-  }
-`;
-
 return (
-  <Card>
-    <CardBody>
-      <Thumbnail>
-        <Widget
-          src="mob.near/widget/Image"
-          props={{
-            image: metadata.image,
-            fallbackUrl:
-              "https://upload.wikimedia.org/wikipedia/commons/8/86/Database-icon.svg",
-            alt: "Near QueryApi indexer",
-          }}
-        />
-      </Thumbnail>
-
-      <div>
-        <TextLink as="a" bold ellipsis>
-          {indexerName}
-        </TextLink>
-        <TextLink as="a" ellipsis>
-          @{accountId}
-        </TextLink>
-      </div>
-    </CardBody>
-
-    <CardFooter className="flex justify-center items-center">
-      <ButtonLink href={playgroundLink} target="_blank">
-        View In Playground
-      </ButtonLink>
-      <ButtonLink
-        primary
-        href={editUrl}
-        onClick={() =>
-          State.update({
-            activeTab: "editor-window",
-          })
-        }
+  <Wrapper className="container-xl">
+    <Tabs>
+      <TabsButton
+        onClick={() => State.update({ selectedTab: "my-indexers" })}
+        selected={state.selectedTab === "my-indexers"}
       >
-        View Indexer
-      </ButtonLink>
-    </CardFooter>
-  </Card>
+        My Indexers
+      </TabsButton>
+      <TabsButton
+        onClick={() => State.update({ selectedTab: "all" })}
+        selected={state.selectedTab === "all"}
+      >
+        All
+      </TabsButton>
+    </Tabs>
+
+    {state.selectedTab === "all" && (
+      <>
+        <Items>
+          {state.all_indexers.map((indexer, i) => (
+            <Item>
+              <Widget
+                src={`dataplatform.near/widget/QueryApi.IndexerCard`}
+                props={{
+                  accountId: indexer.accountId,
+                  indexerName: indexer.indexerName,
+                }}
+              />
+            </Item>
+          ))}
+        </Items>
+      </>
+    )}
+    {state.selectedTab == "my-indexers" && state.my_indexers.length == 0 && (
+      <Header>
+        <H2>
+          QueryAPI streamlines the process of querying specific data from the
+          Near Blockchain. Explore new Indexers and fork them to try it out!
+        </H2>
+        <H2>
+          To learn more about QueryAPI, visit
+          <TextLink
+            target="_blank"
+            href="https://docs.near.org/bos/community/indexers"
+            as="a"
+            bold
+          >
+            QueryAPI Docs
+          </TextLink>
+        </H2>
+      </Header>
+    )}
+    <Items>
+      {state.selectedTab == "my-indexers" && (
+        <>
+          {state.my_indexers.map((indexer, i) => (
+            <Item>
+              <Widget
+                src={`dataplatform.near/widget/QueryApi.IndexerCard`}
+                props={{
+                  accountId: indexer.accountId,
+                  indexerName: indexer.indexerName,
+                }}
+              />
+            </Item>
+          ))}
+        </>
+      )}
+    </Items>
+  </Wrapper>
 );
