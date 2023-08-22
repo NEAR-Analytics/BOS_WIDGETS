5fc191e1ff71b4951460540eaed34f1eca9bdaaf
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-22 12:50:44 +0000

    Update CalendarChartv2 by relayer.pagodaplatform.near at 2023-08-22T12:50:44.384Z

--- relayer.pagodaplatform.near/CalendarChartv2/source_code.jsx
+++ relayer.pagodaplatform.near/CalendarChartv2/source_code.jsx
@@ -96,7 +96,7 @@ return (
     {props.title && <strong>{props.title}</strong>}
     <iframe
       className="w-100"
-      style={{ height: 250 * props.heightMultiplier }}
+      style={{ height: 350 + 250 * props.heightMultiplier }}
       srcDoc={code}
     />
   </div>
