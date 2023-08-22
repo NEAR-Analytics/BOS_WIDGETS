bcdfc6c248c52dfe1f951d574a4a95c45bee29c4
mob.near <mob.near>
2023-08-22 01:31:51 +0000

    Update Notification.Item.Mention by mob.near at 2023-08-22T01:31:51.717Z

--- mob.near/Notification.Item.Mention/source_code.jsx
+++ mob.near/Notification.Item.Mention/source_code.jsx
@@ -1,73 +1,29 @@
 const { accountId, value, blockHeight } = props;
 
-const overlayStyle = {
-  maxWidth: "30em",
-  zIndex: 1070,
-  maxHeight: "24em",
-  overflow: "hidden",
-};
-
-const { content, popup } =
-  value.item.path === `${accountId}/post/main`
-    ? {
-        content: (
-          <a
-            className="fw-bold text-muted"
-            href={`/mob.near/widget/MainPage.N.Post.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
-          >
-            post
-          </a>
-        ),
-        popup: (
-          <Widget
-            src="mob.near/widget/MainPage.N.Post"
-            props={{
-              accountId,
-              blockHeight,
-              hideComments: true,
-            }}
-          />
-        ),
-      }
-    : value.item.path === `${accountId}/post/comment`
-    ? {
-        content: (
-          <a
-            className="fw-bold text-muted"
-            href={`/mob.near/widget/MainPage.N.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
-          >
-            comment
-          </a>
-        ),
-        popup: (
-          <Widget
-            src="mob.near/widget/MainPage.N.Comment.Full"
-            props={{
-              accountId,
-              blockHeight,
-            }}
-          />
-        ),
-      }
-    : { content: "item???" };
-
 return (
   <Widget
-    loading={props.loading}
     src="mob.near/widget/Notification.Item.LR"
     props={{
       L: (
         <>
-          mentioned you in their
-          <Widget
-            loading={content}
-            src="mob.near/widget/N.Common.OverlayTrigger"
-            props={{
-              overlayStyle,
-              popup,
-              children: content,
-            }}
-          />
+          mention you in their
+          {value.item.path === `${accountId}/post/main` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`/mob.near/widget/MainPage.N.Post.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+            >
+              post
+            </a>
+          ) : value.item.path === `${accountId}/post/comment` ? (
+            <a
+              className="fw-bold text-muted"
+              href={`/mob.near/widget/MainPage.N.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`}
+            >
+              comment
+            </a>
+          ) : (
+            "item???"
+          )}
         </>
       ),
       R:
