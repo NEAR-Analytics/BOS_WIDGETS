e523bc2e91bee5591850e96ab048ac20fb0b991d
dapplets.near <dapplets.near>
2023-08-22 15:59:12 +0000

    Update Web3Hackfest-Paywall-Button by dapplets.near at 2023-08-22T15:59:12.604Z

---
+++ dapplets.near/Web3Hackfest-Paywall-Button/source_code.jsx
@@ -0,0 +1,87 @@
+const Wrapper = styled.div`
+.button-default {
+  cursor: pointer;
+  user-select: none;
+
+  box-sizing: border-box;
+  height: 35px;
+  margin: 0 8px 12px 0;
+  padding: 0 10px;
+
+  font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial,
+    sans-serif;
+  font-size: 15px;
+  font-weight: 600;
+  line-height: 33px;
+
+  background: inherit;
+  border-radius: 9999px;
+
+  transition: background-color 0.2s ease-in-out;
+  display: flex;
+}
+
+.button-light {
+  color: #000;
+  border: 1px solid rgb(207 217 222);
+}
+
+.button-light:hover {
+  background-color: rgb(15 20 25 / 10%);
+}
+
+.button-dark {
+  color: #fff;
+  border: 1px solid rgb(83 100 113);
+}
+
+.button-dark:hover {
+  background-color: rgb(239 243 244 / 10%);
+}
+
+.button-basic {
+  padding: 0;
+  border: none;
+  border-radius: 7px;
+}
+
+.button-img-display {
+  fill: currentcolor;
+  height: auto;
+  max-width: 100%;
+  position: relative;
+  vertical-align: text-bottom;
+  width: 1.25em;
+  margin: auto 0;
+  display: flex !important;
+  align-items: center !important;
+}
+`;
+
+return (
+  <Wrapper>
+    <div
+      role="button"
+      className="button-default"
+      onClick={() => props.onClick()}
+      styles={{
+        opacity: props.disabled ? ".5" : "1",
+      }}
+      disabled={props.disabled}
+    >
+      <div
+        styles={{
+          opacity: props.disabled ? ".5" : "1",
+          display: "flex",
+          "align-items": "center",
+          "margin-right": props.label?.toString() ? "12px" : 0,
+        }}
+      >
+        <img src={props.icon} class="button-img-display" />
+      </div>
+      <div>
+        <span>{props.label}</span>
+      </div>
+    </div>
+  </Wrapper>
+);
