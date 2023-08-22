0560c87dc4fd0b0368264fe549995914ef236d38
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-22 10:14:30 +0000

    Update walley by relayer.pagodaplatform.near at 2023-08-22T10:14:30.082Z

--- relayer.pagodaplatform.near/walley/source_code.jsx
+++ relayer.pagodaplatform.near/walley/source_code.jsx
@@ -460,29 +460,29 @@ const initTransaction = () => {
 
 const cancelTransaction = (tokenId) => {
   walleyContract.returnPass(tokenId).then((password) => {
-    // if (state.user.cancelPassword === password) {
-    State.update({
-      loading: true,
-      loadingMsg: "Cancelling your transaction - Pay for the gas",
-    });
-    nftContract
-      .cancelTransaction(walleyAddress, tokenId, { from: sender })
-      .then((tx) => {
-        State.update({ loadingMsg: "Refunding your amount" });
-        tx.wait().then((r) => {
-          const transactions = state.user.userPendingTransactions.filter(
-            (txn) => Big(txn[1]).toFixed(0) !== tokenId
-          );
-          State.update({
-            loading: false,
-            loadingMsg: "",
-            user: { userPendingTransactions: transactions },
+    if (state.user.cancelPassword === password) {
+      State.update({
+        loading: true,
+        loadingMsg: "Cancelling your transaction - Pay for the gas",
+      });
+      nftContract
+        .cancelTransaction(walleyAddress, tokenId, { from: sender })
+        .then((tx) => {
+          State.update({ loadingMsg: "Refunding your amount" });
+          tx.wait().then((r) => {
+            const transactions = state.user.userPendingTransactions.filter(
+              (txn) => Big(txn[1]).toFixed(0) !== tokenId
+            );
+            State.update({
+              loading: false,
+              loadingMsg: "",
+              user: { userPendingTransactions: transactions },
+            });
           });
         });
-      });
-    // } else {
-    //   console.log("Please re-check the password");
-    // }
+    } else {
+      console.log("Please re-check the password");
+    }
   });
 };
 
@@ -702,8 +702,8 @@ return (
         ) : (
           <WalleyStoreBody>
             <WalleyTransactions>
-              {state.user.userPendingTransactions.length !== 0
-                ? state.user.userPendingTransactions.map((tx) => (
+              {state.user.storePendingTransactions.length !== 0
+                ? state.user.storePendingTransactions.map((tx) => (
                     <TransactionCard>
                       <WalleyStoreImage
                         src={`https://ipfs.near.social/ipfs/${
