3a298bd66f89d5141115805001220f7e51c0fbfe
efiz.near <efiz.near>
2023-08-22 16:39:46 +0000

    Update ShoppingCartDemo.cart by efiz.near at 2023-08-22T16:39:46.424Z

--- efiz.near/ShoppingCartDemo.cart/source_code.jsx
+++ efiz.near/ShoppingCartDemo.cart/source_code.jsx
@@ -2,6 +2,6 @@ const items = JSON.parse(Storage.get("demo-cart-items") || "null");
 
 return (
   <div className="border">
-    <p>Cart: {items.length}</p>
+    <p>Cart: {items.length || 0}</p>
   </div>
 );
