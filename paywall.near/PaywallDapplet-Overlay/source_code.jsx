7f7ff60af25d6e376007449d3f803a08893fc323
paywall.near <paywall.near>
2023-08-23 21:39:34 +0000

    Update PaywallDapplet-Overlay by paywall.near at 2023-08-23T21:39:34.158Z

--- paywall.near/PaywallDapplet-Overlay/source_code.jsx
+++ paywall.near/PaywallDapplet-Overlay/source_code.jsx
@@ -1,15 +1,19 @@
-const { accountId } = props;
+/**
+ * We use payerAccountId instead of accountId because it's reserved parameter in embeded pages
+ * https://github.com/near/near-discovery/blob/dc58aa1c8ef5d4c0a5e19839230148009834088a/src/pages/embed/%5BaccountId%5D/widget/%5BcomponentName%5D.tsx#L16
+ */
+const { payerAccountId } = props;
 
-if (!accountId) {
+if (!payerAccountId) {
   return <div>Connect your Wallet</div>;
 }
 
-const purchases = accountId
+const purchases = payerAccountId
   ? Near.view(
       "app.paywall.near",
       "purchases",
       {
-        account_id: accountId,
+        account_id: payerAccountId,
       },
       "final",
       true
