93b79cb3f466b31163b6f3092bc1416884ca603e
guessme.near <guessme.near>
2023-08-23 03:50:22 +0000

    Update ZKEVM.Template by guessme.near at 2023-08-23T03:50:22.178Z

--- guessme.near/ZKEVM.Template/source_code.jsx
+++ guessme.near/ZKEVM.Template/source_code.jsx
@@ -4,6 +4,41 @@ const Container = styled.div`
   gap: 30px 36px;
   color: #fff;
 `;
+
+const Search = styled.div`
+display: flex;
+position: fixed;
+top: 112px;
+right: 32px;
+  input{
+     color: #ffffff;
+     background: transparent;
+     border: none;
+     border-bottom: 1px #373A53 solid;
+     padding-right: 24px;
+   }
+   input:focus {
+     outline: none;
+     color: #ffffff;
+     background: transparent;
+     border: none;
+     box-shadow: none;
+     border-bottom: 1px #373A53 solid;
+   }
+   svg {
+    margin-left: -24px;
+    margin-top:10px;
+  }
+`;
+
+const SearchIcon = (
+  <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="7.01829" cy="7.01829" r="6.01829" stroke="#EBF479" strokeWidth="2" />
+    <rect x="14.9141" y="9.64941" width="6.141" height="2.63186" rx="1.31593" transform="rotate(30 14.9141 9.64941)" fill="#EBF479" />
+  </svg>
+
+);
+
 const templates = [
   {
     src: "bluebiu.near/widget/ZKEVM-all-in-one",
@@ -31,9 +66,43 @@ const templates = [
       "https://ipfs.near.social/ipfs/bafkreibmqzfwki7zye6ruqi3hifgexs6g5nv4qvzcmrm73lnra5mbkdrxe",
   },
 ];
+
+State.init({
+  searchValue: "",
+})   // 初始化状态
+
+const handleSearchChange = (e) => {
+  const value = e.target.value;
+  State.update({
+    searchValue: value,
+  }) // 更新状态值
+};
+
+const metadataPromises = templates.map(({ src }) =>
+  Social.get(`${src}/metadata/**`, 'final')
+);
+
+const updatedTemplates = templates.map((template, index) => ({
+  ...template,
+  name: metadataPromises[index]?.name || '',
+}));
+
+const filteredTemplates = updatedTemplates.filter(({ name }) =>
+  name.toLowerCase().includes(state.searchValue.toLowerCase())
+);
+
+
 return (
   <Container>
-    {templates.map(({ src, bannerImg }, index) => (
+    <Search>
+      <input
+        type="text"
+        value={searchValue}
+        onChange={handleSearchChange}
+      />
+      {SearchIcon}
+    </Search>
+    {filteredTemplates.map(({ src, bannerImg }, index) => (
       <Widget
         src="guessme.near/widget/ZKEVM.Template-card"
         key={index}
