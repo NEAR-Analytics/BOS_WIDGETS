8610cbb2ebaef0da68f2f7d0546e2d68ed1f3ea7
harrydhillon.near <harrydhillon.near>
2023-08-23 21:27:34 +0000

    Update Keypom.Landing.TicketDetails.Preview by harrydhillon.near at 2023-08-23T21:27:34.094Z

---
+++ harrydhillon.near/Keypom.Landing.TicketDetails.Preview/source_code.jsx
@@ -0,0 +1,128 @@
+function Chip({ label, color, backgroundColor }) {
+  const style = {
+    padding: "8px 16px",
+    borderRadius: "100px",
+    display: "inline-block",
+    marginRight: "10px",
+    backgroundColor: backgroundColor,
+    color: color,
+    fontWeight: "500",
+  };
+
+  return <span style={style}>{label}</span>;
+}
+
+function ChipDisplay(name) {
+  const items = {
+    Purchased: {
+      fontColor: "#475569",
+      backgroundColor: "#F8FAFC",
+    },
+    Claimed: {
+      fontColor: "#16A34A",
+      backgroundColor: "#F0FDF4",
+    },
+    Scanned: {
+      backgroundColor: "#F9F6E9",
+      fontColor: "#9E811B",
+    },
+  };
+
+  return (
+    <div style={{ transform: "scale(0.9)" }}>
+      <Chip
+        label={name}
+        color={items[name].fontColor}
+        backgroundColor={items[name].backgroundColor}
+      />
+    </div>
+  );
+}
+
+const PreviewTicketModal = (
+  <>
+    <p style={{ fontWeight: "500" }}>Attendee Details</p>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <div>
+        <p style={{ marginBottom: 0 }}>Link</p>
+        <p style={{ fontSize: 14, color: "#1E293B" }}>{props.link}</p>
+      </div>
+      {ChipDisplay("Claimed")}
+    </div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <div style={{ width: "50%" }}>
+        <p style={{ marginBottom: 0 }}>Full Name</p>
+        <p style={{ fontSize: 14, color: "#1E293B" }}>{props.name}</p>
+      </div>
+      <div style={{ width: "50%" }}>
+        <p style={{ marginBottom: 0 }}>Email</p>
+        <p style={{ fontSize: 14, color: "#1E293B" }}>{props.email}</p>
+      </div>
+    </div>
+    <p style={{ fontSize: 18 }}>Attendee questions and responses</p>
+    <div>
+      <p style={{ marginBottom: 0, fontWeight: "500" }}>
+        How did you hear about Keypom?
+      </p>
+      <p style={{ fontSize: 14, color: "#1E293B" }}>
+        Inmensae subtilitatis, obscuris et malesuada fames.
+      </p>
+    </div>
+    <div>
+      <p style={{ marginBottom: 0, fontWeight: "500" }}>
+        Why did you buy tickets to this event?
+      </p>
+      <p style={{ fontSize: 14, color: "#1E293B" }}>
+        I know someone in the band.
+      </p>
+    </div>
+    <div>
+      <p style={{ marginBottom: 0, fontWeight: "500" }}>Question</p>
+      <p style={{ fontSize: 14, color: "#1E293B" }}>
+        Inmensae subtilitatis, obscuris et malesuada fames.
+      </p>
+    </div>
+    <button
+      onClick={props.onClose}
+      style={{
+        width: "100%",
+        backgroundColor: "white",
+        fontSize: 14,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: "lightgray",
+        marginTop: 5,
+        color: "black",
+      }}
+    >
+      Close
+    </button>
+  </>
+);
+
+return (
+  <Widget
+    src="harrydhillon.near/widget/Keypom.Components.Modal"
+    props={{
+      children: PreviewTicketModal,
+      isOpen: props.isOpen,
+      contentStyles: {
+        style: {
+          width: 500,
+        },
+      },
+    }}
+  />
+);
