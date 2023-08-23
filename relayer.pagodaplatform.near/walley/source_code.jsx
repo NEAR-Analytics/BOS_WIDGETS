85ef0e67c10395609f89a76c9de4f4ee9915695f
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-23 18:02:16 +0000

    Update walley by relayer.pagodaplatform.near at 2023-08-23T18:02:16.175Z

--- relayer.pagodaplatform.near/walley/source_code.jsx
+++ relayer.pagodaplatform.near/walley/source_code.jsx
@@ -220,7 +220,7 @@ const WalleyBalance = styled.span`
   padding: 5px;
   font-weight: 500;
   font-size: 15px;
-  width: 100px;
+  width: 150px;
   background-color: #4169E1;
   border-radius: 30px;
   &>svg {
