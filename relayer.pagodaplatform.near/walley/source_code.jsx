d2d9f25f243bdc5256e29e33db245c6360046f6e
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-23 13:22:42 +0000

    Update walley by relayer.pagodaplatform.near at 2023-08-23T13:22:42.599Z

--- relayer.pagodaplatform.near/walley/source_code.jsx
+++ relayer.pagodaplatform.near/walley/source_code.jsx
@@ -103,7 +103,7 @@ const WalleyIndexDesc = styled.div`
     font-size: 60px;
     padding-bottom: 10px;
     @media screen and (max-width:600px) {
-      font-size: 50px;
+      font-size: 100px;
     }
 `;
 
