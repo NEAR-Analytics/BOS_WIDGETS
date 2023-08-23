5d52259c4cbd3024fa88603ac7f4e044eeda05db
juaner.near <juaner.near>
2023-08-23 14:14:13 +0000

    Update ref-market-supply-withdraw by juaner.near at 2023-08-23T14:14:13.145Z

--- juaner.near/ref-market-supply-withdraw/source_code.jsx
+++ juaner.near/ref-market-supply-withdraw/source_code.jsx
@@ -116,7 +116,8 @@ const Modal = styled.div`
 /** base tool start  */
 let accountId = context.accountId;
 if (!accountId) {
-  return <Widget src="juaner.near/widget/ref_account-signin" />;
+  // return <Widget src="juaner.near/widget/ref_account-signin" />;
+  return <div></div>;
 }
 let BURROW_CONTRACT = "contract.main.burrow.near";
 let ORACLE_CONTRACT = "priceoracle.near";
