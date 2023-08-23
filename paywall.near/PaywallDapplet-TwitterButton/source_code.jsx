be110822d615b63e88c08226b93090da0b3ffc43
paywall.near <paywall.near>
2023-08-23 11:03:23 +0000

    Update PaywallDapplet-TwitterButton by paywall.near at 2023-08-23T11:03:23.270Z

--- paywall.near/PaywallDapplet-TwitterButton/source_code.jsx
+++ paywall.near/PaywallDapplet-TwitterButton/source_code.jsx
@@ -1,55 +1,55 @@
 const Wrapper = styled.div`
-.button-default {
-  cursor: pointer;
-  user-select: none;
+  .button-default {
+    cursor: pointer;
+    user-select: none;
 
-  box-sizing: border-box;
-  height: 35px;
-  margin: 0 8px 12px 0;
-  padding: 0 10px;
+    box-sizing: border-box;
+    height: 35px;
+    margin: 0 8px 12px 0;
+    padding: 0 10px;
 
-  font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial,
-    sans-serif;
-  font-size: 15px;
-  font-weight: 600;
-  line-height: 33px;
+    font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Helvetica, Arial, sans-serif;
+    font-size: 15px;
+    font-weight: 600;
+    line-height: 33px;
 
-  background: inherit;
-  border-radius: 9999px;
+    background: inherit;
+    border-radius: 9999px;
 
-  transition: background-color 0.2s ease-in-out;
-  display: flex;
-}
+    transition: background-color 0.2s ease-in-out;
+    display: flex;
+  }
 
-.button-light {
-  color: #000;
-  border: 1px solid rgb(207 217 222);
-}
+  .button-light {
+    color: #000;
+    border: 1px solid rgb(207 217 222);
+  }
 
-.button-light:hover {
-  background-color: rgb(15 20 25 / 10%);
-}
+  .button-light:hover {
+    background-color: rgb(15 20 25 / 10%);
+  }
 
-.button-dark {
-  color: #fff;
-  border: 1px solid rgb(83 100 113);
-}
+  .button-dark {
+    color: #fff;
+    border: 1px solid rgb(83 100 113);
+  }
 
-.button-dark:hover {
-  background-color: rgb(239 243 244 / 10%);
-}
+  .button-dark:hover {
+    background-color: rgb(239 243 244 / 10%);
+  }
 
-.button-img-display {
-  fill: currentcolor;
-  height: auto;
-  max-width: 100%;
-  position: relative;
-  vertical-align: text-bottom;
-  width: 1.25em;
-  margin: auto 0;
-  display: flex !important;
-  align-items: center !important;
-}
+  .button-img-display {
+    fill: currentcolor;
+    height: auto;
+    max-width: 100%;
+    position: relative;
+    vertical-align: text-bottom;
+    width: 1.25em;
+    margin: auto 0;
+    display: flex !important;
+    align-items: center !important;
+  }
 `;
 
 return (
@@ -59,7 +59,7 @@ return (
       className={`button-default ${
         props.dark ? "button-dark" : "button-light"
       }`}
-      onClick={() => props.onClick()}
+      onClick={() => props.onClick?.()}
       style={{
         opacity: props.disabled ? ".5" : "1",
       }}
