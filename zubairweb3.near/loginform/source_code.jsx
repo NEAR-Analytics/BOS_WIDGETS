b51d1bad7ccc69cd5fca108e129fe50161a3af26
zubairweb3.near <zubairweb3.near>
2023-08-24 10:23:06 +0000

    Update loginform by zubairweb3.near at 2023-08-24T10:23:06.999Z

--- zubairweb3.near/loginform/source_code.jsx
+++ zubairweb3.near/loginform/source_code.jsx
@@ -1,35 +1,22 @@
-const Wrapper = styled.div`
-background-image: linear-gradient(to right, #c79081, #dfa579);
-width: 80%;
-padding: 20px;
-margin: auto;
-border-radius: 15px;
-height: 100vh;
-display: flex;
-justify-content: center;
-flex-direction: column;
-`;
-
 const Form = styled.div`
     display: flex;
     flex-direction: column;
-    width: 50%;
+    width: 30%;
     margin: 20px auto;
-    color: #5d4157;
     
 `;
 
 const FormHeader = styled.h3`
 text-align: center;
 font-weight: bold;
-color: #5d4157;
+color: #0d6efd;
 font-family: arial, sans-serif;
 font-size: 1.5rem;
 `;
 
 return (
-  <Wrapper>
-    <FormHeader>Login</FormHeader>
+  <div>
+    <FormHeader>Login Form</FormHeader>
     <Form>
       <div class="mb-3">
         <label for="email" class="form-label">
@@ -42,6 +29,9 @@ return (
           aria-describedby="emailHelp"
           required
         />
+        <div id="emailHelp" class="form-text">
+          We'll never share your email with anyone else.
+        </div>
       </div>
       <div class="mb-3">
         <label for="password" class="form-label">
@@ -55,13 +45,9 @@ return (
           Remember me
         </label>
       </div>
-      <button
-        type="submit"
-        style={{ backgroundColor: "#5d4157", color: "#fff" }}
-        class="btn"
-      >
+      <button type="submit" class="btn btn-primary">
         Submit
       </button>
     </Form>
-  </Wrapper>
+  </div>
 );
