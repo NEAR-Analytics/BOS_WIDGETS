98231c9a825742adc7a0383cf0cfba042ce995fc
longx30.near <longx30.near>
2023-08-24 08:29:40 +0000

    Update TestIframe by longx30.near at 2023-08-24T08:29:40.784Z

---
+++ longx30.near/TestIframe/source_code.jsx
@@ -0,0 +1,9 @@
+return (
+  <div>
+    <h1>The iframe element</h1>
+    <iframe
+      src="https://www.w3schools.com"
+      title="W3Schools Free Online Web Tutorials"
+    ></iframe>
+  </div>
+);
