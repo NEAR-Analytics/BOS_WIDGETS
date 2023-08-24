83e9b177d7a28372199c835030d58231b4d43a35
sourcescan.near <sourcescan.near>
2023-08-24 12:52:00 +0000

    Update SourceScan.Contracts.Approved by sourcescan.near at 2023-08-24T12:52:00.417Z

--- sourcescan.near/SourceScan.Contracts.Approved/source_code.jsx
+++ sourcescan.near/SourceScan.Contracts.Approved/source_code.jsx
@@ -1,6 +1,7 @@
 State.init({
   placement: props.placement || "top",
   value: null,
+  error: false,
 });
 
 const compareWasm = () => {
@@ -24,9 +25,27 @@ const compareWasm = () => {
     .then((rpc_res) => {
       asyncFetch(`${props.apiHost}/ipfs/${props.cid}/wasm_code_base64`)
         .then((ipfs_res) => {
-          State.update({
-            value: rpc_res.body.result.code_base64 === ipfs_res.body,
-          });
+          if (rpc_res.body.result.code_base64 !== ipfs_res.body) {
+            State.update({
+              value: false,
+            });
+            return;
+          } else {
+            asyncFetch(`${props.apiHost}/api/ipfs/getTxHash`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: `{ "cid": "${props.cid}" }`,
+            })
+              .then((res) => {
+                State.update({ value: res.body.tx_hash === props.deploy_tx });
+              })
+              .catch((err) => {
+                console.log(err);
+                State.update({ error: true });
+              });
+          }
         })
         .catch((err) => {
           console.log(err);
@@ -48,13 +67,12 @@ const CheckIcon = (width, height) => {
   return (
     <SVG
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
+      viewBox="0 0 20 20"
       fill="currentColor"
-      class="w-6 h-6"
     >
       <path
         fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
         clip-rule="evenodd"
       />
     </SVG>
@@ -70,15 +88,10 @@ const CrossIcon = (width, height) => {
   return (
     <SVG
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
+      viewBox="0 0 20 20"
       fill="currentColor"
-      class="w-6 h-6"
     >
-      <path
-        fill-rule="evenodd"
-        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
-        clip-rule="evenodd"
-      />
+      <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
     </SVG>
   );
 };
@@ -86,23 +99,23 @@ const CrossIcon = (width, height) => {
 return (
   <>
     {state.value === null ? (
-      "Loading..."
+      <Widget src={`${props.ownerId}/widget/SourceScan.Common.Spinner`} />
     ) : state.value === true ? (
       <OverlayTrigger
         key={state.placement}
         placement={state.placement}
-        overlay={
-          <Tooltip id={`tooltip-${placement}`}>{"Wasm code matches"}</Tooltip>
-        }
+        overlay={<Tooltip id={`tooltip-${placement}`}>{"Approved"}</Tooltip>}
       >
-        <CheckIcon width={"32px"} height={"32px"} />
+        <CheckIcon width={"20px"} height={"20px"} />
       </OverlayTrigger>
     ) : (
       <OverlayTrigger
         key={state.placement}
         placement={state.placement}
         overlay={
-          <Tooltip id={`tooltip-${placement}`}>{"Wasm code mismatch"}</Tooltip>
+          <Tooltip id={`tooltip-${placement}`}>
+            {state.error ? "Error" : "Not approved"}
+          </Tooltip>
         }
       >
         <CrossIcon width={"32px"} height={"32px"} />
