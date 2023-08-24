efbb0f768c43eb7bd707e0f5254b9c895042b658
lord1.near <lord1.near>
2023-08-24 18:51:33 +0000

    Update im-human by lord1.near at 2023-08-24T18:51:33.711Z

--- lord1.near/im-human/source_code.jsx
+++ lord1.near/im-human/source_code.jsx
@@ -139,12 +139,7 @@ return (
           src="https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/lightmode.svg"
         />
       </div>
-      <div
-        style={{
-          background: `${state.light ? "rgb(241,242,245)" : "rgb(25,33,50)"}`,
-        }}
-        className="sticky-top p-4"
-      >
+      <div>
         <div>
           <Container>
             <ul className="tabContent">
@@ -179,8 +174,6 @@ return (
             </ul>
           </Container>
         </div>
-      </div>
-      <div>
         <div className="content">
           {state.tab === "sbt-area" && (
             <Widget src="lord1.near/widget/sbt-area" />
