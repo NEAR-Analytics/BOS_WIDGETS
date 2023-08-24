1190962b34bb7448b6a3e50d90e3629e4d8baa5f
crowdtestify.near <crowdtestify.near>
2023-08-24 04:23:23 +0000

    Update Inputs.Skills by crowdtestify.near at 2023-08-24T04:23:23.561Z

---
+++ crowdtestify.near/Inputs.Skills/source_code.jsx
@@ -0,0 +1,49 @@
+const ownerId = "crowdtestify.near";
+const skillsArray = [
+  { name: "ProgrammingBasics" },
+  { name: "TestingBasics" },
+  { name: "FunctionalTesting" },
+  { name: "LoadTesting" },
+  { name: "IntegrationTesting" },
+  { name: "UnitTesting" },
+  { name: "SecurityTesting" },
+  { name: "Selenium" },
+  { name: "Playwright" },
+  { name: "JUnit" },
+  { name: "TestNG" },
+  { name: "PythonAutomation" },
+  { name: "JavaScriptAutomation" },
+  { name: "MobileAppTesting" },
+  { name: "WebAppTesting" },
+  { name: "ApiTesting" },
+  { name: "DatabaseTesting" },
+  { name: "CloudTesting" },
+  { name: "AgileTesting" },
+  { name: "ContinuousIntegration" },
+  { name: "ContinuousDeployment" },
+  { name: "PerformanceTesting" },
+  { name: "UITesting" },
+  { name: "UXTesting" },
+  { name: "CrossPlatformTesting" },
+  { name: "VRTesting" },
+  { name: "BlockchainTesting" },
+  { name: "TesIoTTestingtinBasics" },
+  { name: "MicroservicesTesting" },
+];
+return (
+  <Widget
+    src={`${ownerId}/widget/Inputs.MultiSelect`}
+    props={{
+      label: "Skills",
+      placeholder: "Add skills",
+      options: skillsArray,
+      value: state.skills,
+      onChange: (skills) =>
+        State.update({
+          skills: skills.map(({ name }) => ({
+            name: name.trim().replaceAll(/\s+/g, "-"),
+          })),
+        }),
+    }}
+  />
+);
