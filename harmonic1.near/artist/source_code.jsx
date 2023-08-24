db2646d291d67b3de3d68cf9633e4445aa2b2de9
harmonic1.near <harmonic1.near>
2023-08-24 16:32:30 +0000

    Update artist by harmonic1.near at 2023-08-24T16:32:30.184Z

--- harmonic1.near/artist/source_code.jsx
+++ harmonic1.near/artist/source_code.jsx
@@ -1,47 +1,3 @@
-// const data = props.data;
-// console.log(data);
-
-// const containerStyle = {
-//   textAlign: "center",
-//   margin: "20px",
-// };
-
-// const headingStyle = {
-//   color: "#333",
-// };
-
-// const imageStyle = {
-//   width: "200px",
-//   height: "200px",
-//   borderRadius: "50%",
-//   marginBottom: "10px",
-// };
-
-// const linkStyle = {
-//   display: "inline-block",
-//   margin: "5px",
-//   color: "blue",
-//   textDecoration: "none",
-//   fontSize: "20px",
-// };
-
-// return (
-//   <div style={containerStyle}>
-//     <h1 style={headingStyle}>{data.name}</h1>
-//     <img src={data.avatar} alt="Musician's Avatar" style={imageStyle} />
-//     <h4>{data.bio}</h4>
-//     {data.links && data.links.length > 0 && (
-//       <div>
-//         {data.links.map((link, index) => (
-//           <a key={index} href={link.value} style={linkStyle}>
-//             {link.key}
-//           </a>
-//         ))}
-//       </div>
-//     )}
-//   </div>
-// );
-
 const data = props.data;
 console.log(data);
 
@@ -50,7 +6,12 @@ const containerStyle = {
   flexDirection: "column",
   alignItems: "center", // Vertically centering the contents
   justifyContent: "center",
-  margin: "20px",
+  paddingTop: "30px",
+  backgroundImage:
+    "url('https://ik.imagekit.io/n7h27i0lh/v960-ning-08-klhoizzv.jpeg?updatedAt=1692893730584')",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
 };
 
 const profileInfoStyle = {
@@ -60,27 +21,50 @@ const profileInfoStyle = {
 };
 
 const headingStyle = {
-  color: "#333",
+  color: "#1f355c",
+};
+
+const bioStyle = {
+  color: "#1f355c",
 };
 
 const imageStyle = {
-  width: "300px",
-  height: "300px",
+  width: "200px",
+  height: "200px",
   //borderRadius: "100%",
   borderRadius: "1rem",
   marginBottom: "10px",
 };
 
+// const linkCardStyle = {
+//   display: "inline-block",
+//   margin: "5px",
+//   padding: "10px", // Add padding for a card effect
+//   backgroundColor: "#4472c4", // Background color for the card
+//   borderRadius: "5px", // Rounded corners for the card
+//   color: "white",
+//   textDecoration: "none",
+//   fontSize: "20px",
+//   boxShadow: "0 4px 8px rgba(0,0,0,0.1)", // Optional: adds a soft shadow for depth
+// };
+
 const linkCardStyle = {
-  display: "inline-block",
-  margin: "5px",
-  padding: "10px", // Add padding for a card effect
-  backgroundColor: "#4472c4", // Background color for the card
-  borderRadius: "5px", // Rounded corners for the card
-  color: "white",
+  display: "block", // Changed to block
+  margin: "10px auto", // Center the block and increase spacing
+  padding: "10px 10px", // Increased padding for top and bottom
+  width: "350px", // Set to take most of the parent's width, adjust as necessary
+  backgroundColor: "rgba(255, 255, 255, 0.8)", //"#4472c4",
+  borderRadius: "10px", // Increased border-radius
+  color: "#1f355c",
   textDecoration: "none",
   fontSize: "20px",
-  boxShadow: "0 4px 8px rgba(0,0,0,0.1)", // Optional: adds a soft shadow for depth
+  textAlign: "center", // Center the text inside the tile
+  boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+  //opacity: "0.8",
+};
+
+const linkArray = {
+  marginTop: "40px",
 };
 
 return (
@@ -88,9 +72,11 @@ return (
     <img src={data.avatar} alt="Musician's Avatar" style={imageStyle} />
     <div style={profileInfoStyle}>
       <h1 style={headingStyle}>{data.name}</h1>
-      <h4>{data.bio}</h4>
+      <hr style={{ width: "100%", margin: "10px auto" }} />{" "}
+      {/* Added this line for the horizontal rule */}
+      <h4 style={bioStyle}>{data.bio}</h4>
       {data.links && data.links.length > 0 && (
-        <div>
+        <div style={linkArray}>
           {data.links.map((link, index) => (
             <a key={index} href={link.value} style={linkCardStyle}>
               {link.key}
