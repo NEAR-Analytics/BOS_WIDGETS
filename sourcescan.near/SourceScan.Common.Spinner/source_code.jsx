c87a50b1df395d05521586cbbaa8b0b6121e2671
sourcescan.near <sourcescan.near>
2023-08-24 12:25:31 +0000

    Update SourceScan.Common.Spinner by sourcescan.near at 2023-08-24T12:25:31.651Z

--- sourcescan.near/SourceScan.Common.Spinner/source_code.jsx
+++ sourcescan.near/SourceScan.Common.Spinner/source_code.jsx
@@ -1,39 +1,45 @@
+State.init({
+  width: props.width || "20px",
+  height: props.height || "20px",
+});
+
+const useTheme = (light, dark) => {
+  return state.theme === "light" ? light : dark;
+};
+
+const dark = {
+  bg: "#28282b",
+  color: "#e6eaee",
+  border: "#748094",
+};
+
+const light = {
+  bg: "#e3e8ef",
+  color: "#4c5566",
+  border: "#748094",
+};
+
 const Spinner = styled.div`
-  lds-ring {
-    display: inline-block;
-    position: relative;
-    width: 80px;
-    height: 80px;
-  }
-  .lds-ring div {
-    box-sizing: border-box;
-    display: block;
-    position: absolute;
-    width: 64px;
-    height: 64px;
-    margin: 8px;
-    border: 8px solid #fff;
-    border-radius: 50%;
-    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    border-color: #fff transparent transparent transparent;
-  }
-  .lds-ring div:nth-child(1) {
-    animation-delay: -0.45s;
-  }
-  .lds-ring div:nth-child(2) {
-    animation-delay: -0.3s;
-  }
-  .lds-ring div:nth-child(3) {
-    animation-delay: -0.15s;
-  }
-  @keyframes lds-ring {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @keyframes spinner {
+    to {
       transform: rotate(360deg);
     }
   }
+
+  :before {
+    content: "";
+    box-sizing: border-box;
+    width: ${state.width};
+    height: ${state.height};
+    border-radius: 50%;
+    border: 2px solid ${useTheme(light.border, dark.border)};
+    border-top-color: ${useTheme(light.color, dark.color)};
+    animation: spinner 0.6s linear infinite;
+  }
 `;
 
-return <div class="lds-ring" />;
+return <Spinner />;
