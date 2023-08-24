43df37103ba31d501bb153f48baf1aa2b05f5d66
sourcescan.near <sourcescan.near>
2023-08-24 13:00:34 +0000

    Update SourceScan.Contracts.Table by sourcescan.near at 2023-08-24T13:00:34.173Z

--- sourcescan.near/SourceScan.Contracts.Table/source_code.jsx
+++ sourcescan.near/SourceScan.Contracts.Table/source_code.jsx
@@ -94,6 +94,27 @@ const LinkIcon = (width, height) => {
   );
 };
 
+const InfoIcon = (width, height) => {
+  const SVG = styled.svg`
+    width: ${width}
+    height: ${height}
+  `;
+
+  return (
+    <SVG
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill={`${useTheme(light.color, dark.color)}`}
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clip-rule="evenodd"
+      />
+    </SVG>
+  );
+};
+
 const A = styled.a`
   text-decoration: none;
   color: ${useTheme(light.color, dark.color)};
@@ -135,6 +156,7 @@ return (
             <th>IPFS</th>
             <th>Github</th>
             <th>Approved</th>
+            <th></th>
           </tr>
         </THead>
         <tbody>
@@ -184,9 +206,19 @@ return (
                             apiHost: props.apiHost,
                             accountId: contractId,
                             cid: cid,
+                            ownerId: state.ownerId,
+                            deploy_tx: deploy_tx,
                           }}
                         />
                       </Center>
+                    </td>
+                    <td>
+                      <A
+                        href={`/${state.ownerId}/widget/Contracts.Info?contractId=${contractId}`}
+                        target={"_blank"}
+                      >
+                        <InfoIcon width={"20px"} height={"20px"} />
+                      </A>
                     </td>
                   </tr>
                 );
