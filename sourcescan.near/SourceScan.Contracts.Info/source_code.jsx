1fd76c8dd0ac450b19bc4533cae192640fed2da7
sourcescan.near <sourcescan.near>
2023-08-24 16:27:43 +0000

    Update SourceScan.Contracts.Info by sourcescan.near at 2023-08-24T16:27:43.380Z

--- sourcescan.near/SourceScan.Contracts.Info/source_code.jsx
+++ sourcescan.near/SourceScan.Contracts.Info/source_code.jsx
@@ -1,1 +1,21 @@
-return <></>;
+const useNetwork = (mainnet, testnet) => {
+  return context.networkId === "mainnet" ? mainnet : testnet;
+};
+
+State.init({
+  contractId: props.contractId,
+  source: context.networkId,
+});
+
+return (
+  <>
+    <Widget
+      src={`${state.config.ownerId}/widget/SourceScan.Layout.Navbar`}
+      props={{
+        app: state.config.app,
+        theme: state.theme,
+        switchTheme: switchTheme,
+      }}
+    />
+  </>
+);
