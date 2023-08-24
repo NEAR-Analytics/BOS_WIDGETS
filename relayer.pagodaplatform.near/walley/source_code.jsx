99fcec1c6151a9be93748e189a91d158bf25ecaf
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-24 14:24:00 +0000

    Update walley by relayer.pagodaplatform.near at 2023-08-24T14:24:00.968Z

--- relayer.pagodaplatform.near/walley/source_code.jsx
+++ relayer.pagodaplatform.near/walley/source_code.jsx
@@ -404,7 +404,7 @@ const WalleySearch = styled.div`
 
 const TransactionCard = styled.div`
   background: rgb(66, 66, 66);
-  width: 50%;
+  width: 100%;
   box-shadow: rgb(0, 0, 0, 0.19) 0px 10px 20px, rgb(0, 0, 0, 0.23) 0px 6px 6px;
   border-radius: 100px;
   padding: 30px;
