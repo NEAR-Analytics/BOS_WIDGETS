8a2f712254271d6b77f31213080957808b023888
chloe.near <chloe.near>
2023-08-25 00:02:28 +0000

    Update DAO.Social.Post by chloe.near at 2023-08-25T00:02:28.292Z

--- chloe.near/DAO.Social.Post/source_code.jsx
+++ chloe.near/DAO.Social.Post/source_code.jsx
@@ -179,10 +179,6 @@ return (
       </div>
     )}
     <div className="me-3">
-      //{" "}
-      <button className="btn btn-primary m-2" onClick={handleProposal}>
-        // Submit to NEARWEEK //{" "}
-      </button>
       <button className="btn btn-success m-2" onClick={handleBoostProposal}>
         Request a Boost
       </button>
