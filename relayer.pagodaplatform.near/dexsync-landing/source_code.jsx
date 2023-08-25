c558399eee4f7efbb72c3ca7d77edd679254c6dd
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-25 09:07:48 +0000

    Update dexsync-landing by relayer.pagodaplatform.near at 2023-08-25T09:07:48.911Z

--- relayer.pagodaplatform.near/dexsync-landing/source_code.jsx
+++ relayer.pagodaplatform.near/dexsync-landing/source_code.jsx
@@ -2,9 +2,17 @@ const Container = styled.div`
 background-color: #141414;
   width: 102%;
     height: 100vh;
+    display:flex;
+`;
+
+const HeroSection = styled.div`
+margin-top: 100px;
 `;
 return (
   <Container>
     <Widget src={`0xchirag.near/widget/dexsync-navbar`} props={props} />
+    <HeroSection>
+      <Widget src={`0xchirag.near/widget/dexsync-herosection`} props={props} />
+    </HeroSection>
   </Container>
 );
