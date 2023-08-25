881550a3e6aaed6b1b3f06ac015e7941fc88d478
andyh.near <andyh.near>
2023-08-25 22:57:08 +0000

    Update TrustedStateChild by andyh.near at 2023-08-25T22:57:08.330Z

--- andyh.near/TrustedStateChild/source_code.jsx
+++ andyh.near/TrustedStateChild/source_code.jsx
@@ -1,4 +1,11 @@
+const pre = state.value;
 State.init({ value: -1 });
+const mid = state.value;
 State.update({ value: props.value });
+const fin = state.value;
 
-return <h1>{state.value}</h1>;
+return (
+  <h1>
+    {pre} {"->"} {mid} {"->"} {fin}
+  </h1>
+);
