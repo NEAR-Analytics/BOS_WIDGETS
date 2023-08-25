937fdba8ccd6120dd014075d796aa28a5d5a4a01
3c0b9f03eec241d9ca34f260477bbe4a28ab0f96735aa182a85dec66e61d95d9 <3c0b9f03eec241d9ca34f260477bbe4a28ab0f96735aa182a85dec66e61d95d9>
2023-08-25 11:21:04 +0000

    Update F3Demo by 3c0b9f03eec241d9ca34f260477bbe4a28ab0f96735aa182a85dec66e61d95d9 at 2023-08-25T11:21:04.389Z

--- 3c0b9f03eec241d9ca34f260477bbe4a28ab0f96735aa182a85dec66e61d95d9/F3Demo/source_code.jsx
+++ 3c0b9f03eec241d9ca34f260477bbe4a28ab0f96735aa182a85dec66e61d95d9/F3Demo/source_code.jsx
@@ -118,7 +118,7 @@ return (
         props={{
           placeholder: "Receive Wallet",
           titleBtn: "Mint NFT",
-          tokenSeriesId: "497770",
+          tokenSeriesId: "497771",
         }}
       />
     </ContainerBorder>
