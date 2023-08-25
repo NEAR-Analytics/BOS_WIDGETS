04f4bfdaaee106c417d5663a10fadbcd3bcc0e87
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-25 08:40:29 +0000

    Update dexsync-landing by relayer.pagodaplatform.near at 2023-08-25T08:40:29.612Z

---
+++ relayer.pagodaplatform.near/dexsync-landing/source_code.jsx
@@ -0,0 +1,10 @@
+const Container = styled.div`
+background-color: #141414;
+  width: 102%;
+    height: 100vh;
+`;
+return (
+  <Container>
+    <Widget src={`0xchirag.near/widget/dexsync-navbar`} props={props} />
+  </Container>
+);
