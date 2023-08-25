fed39e052c961da2c54833388630f324d289156e
chloe.near <chloe.near>
2023-08-25 20:40:34 +0000

    Update ProfileEditor by chloe.near at 2023-08-25T20:40:34.100Z

--- chloe.near/ProfileEditor/source_code.jsx
+++ chloe.near/ProfileEditor/source_code.jsx
@@ -1,13 +1,11 @@
+// Initialize state and variables
 const accountId = context.accountId;
-
 if (!accountId) {
   return "Please sign in with NEAR wallet to edit your profile";
 }
 
 let daoId = "research-collective.sputnik-dao.near";
-
 let profile = Social.getr(`${daoId}/profile`);
-
 if (profile === null) {
   return "Loading";
 }
@@ -16,15 +14,54 @@ State.init({
   profile,
 });
 
-const Wrapper = styled.div`
-  #pills-tab,
-  #pills-tabContent {
-    display: none;
-  }
-`;
+// Function to handle the proposal for updating the DAO profile
+const handleProfileUpdateProposal = () => {
+  const profile_update_args = JSON.stringify({
+    data: {
+      [daoId]: {
+        profile: {
+          main: JSON.stringify(state.profile),
+        },
+      },
+    },
+  });
 
+  const proposal_update_args = Buffer.from(
+    profile_update_args,
+    "utf-8"
+  ).toString("base64");
+
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "add_proposal",
+      args: {
+        proposal: {
+          description: "Update DAO profile on NEAR Social",
+          kind: {
+            FunctionCall: {
+              receiver_id: "social.near",
+              actions: [
+                {
+                  method_name: "set",
+                  args: proposal_update_args,
+                  deposit: "10000000000000000000000", // 0.1 NEAR
+                  gas: "219000000000000", // As per your usual setting
+                },
+              ],
+            },
+          },
+        },
+      },
+      deposit: "10000000000000000000000", // 0.1 NEAR
+      gas: "219000000000000", // As per your usual setting
+    },
+  ]);
+};
+
+// Main JSX component
 return (
-  <Wrapper>
+  <div>
     <div className="row">
       <div className="col-lg-6">
         <div>
@@ -36,49 +73,11 @@ return (
             props={{
               initialMetadata: profile,
               onChange: (profile) => State.update({ profile }),
-              options: {
-                name: { label: "Name" },
-                image: { label: "Profile picture" },
-                backgroundImage: { label: "Background image" },
-                description: { label: "About" },
-                tags: {
-                  label: "Tags",
-                  tagsPattern: "*/profile/tags/*",
-                  placeholder:
-                    "rust, engineer, artist, humanguild, nft, learner, founder",
-                },
-                linktree: {
-                  links: [
-                    {
-                      label: "Twitter",
-                      prefix: "https://twitter.com/",
-                      name: "twitter",
-                    },
-                    {
-                      label: "Github",
-                      prefix: "https://github.com/",
-                      name: "github",
-                    },
-                    {
-                      label: "Telegram",
-                      prefix: "https://t.me/",
-                      name: "telegram",
-                    },
-                    {
-                      label: "Website",
-                      prefix: "https://",
-                      name: "website",
-                    },
-                  ],
-                },
-              },
             }}
           />
         </div>
         <div className="mb-2">
-          <CommitButton data={{ profile: state.profile }}>
-            Save profile
-          </CommitButton>
+          <button onClick={handleProfileUpdateProposal}>Save profile</button>
           <a
             className="btn btn-outline-primary ms-2"
             href={`#/near/widget/ProfilePage?accountId=${daoId}`}
@@ -96,5 +95,5 @@ return (
         </div>
       </div>
     </div>
-  </Wrapper>
+  </div>
 );
