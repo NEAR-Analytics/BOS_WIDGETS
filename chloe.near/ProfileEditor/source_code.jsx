0ab8e34cf5660df5a62bd58eae6dad7f19155276
chloe.near <chloe.near>
2023-08-25 20:50:36 +0000

    Update ProfileEditor by chloe.near at 2023-08-25T20:50:36.620Z

--- chloe.near/ProfileEditor/source_code.jsx
+++ chloe.near/ProfileEditor/source_code.jsx
@@ -1,73 +1,24 @@
-// State and variable initialization
+// Initialize state and variables
 const accountId = context.accountId;
 
-// Enforce NEAR wallet authentication
+// Check for NEAR wallet authentication
 if (!accountId) {
   return "Please sign in with NEAR wallet to edit your profile";
 }
 
-// DAO and profile information retrieval
 let daoId = "research-collective.sputnik-dao.near";
 let profile = Social.getr(`${daoId}/profile`);
 
-// Return a loading state if the profile is null
+// Display a loading state if the profile is null
 if (profile === null) {
   return "Loading";
 }
 
-// Initialize the state with the profile
+// Initialize the state
 State.init({
   profile,
 });
 
-// Function to create a DAO proposal for updating the profile
-const handleProfileUpdateProposal = () => {
-  // Serialize the profile state to a base64 encoded string
-  const profile_update_args = JSON.stringify({
-    data: {
-      [daoId]: {
-        profile: {
-          main: JSON.stringify(state.profile),
-        },
-      },
-    },
-  });
-
-  const proposal_update_args = Buffer.from(
-    profile_update_args,
-    "utf-8"
-  ).toString("base64");
-
-  // NEAR blockchain call to add the proposal
-  Near.call([
-    {
-      contractName: daoId,
-      methodName: "add_proposal",
-      args: {
-        proposal: {
-          description: "Update DAO profile on NEAR Social",
-          kind: {
-            FunctionCall: {
-              receiver_id: "social.near",
-              actions: [
-                {
-                  method_name: "set",
-                  args: proposal_update_args,
-                  deposit: "10000000000000000000000", // 0.1 NEAR
-                  gas: "219000000000000", // Specified gas amount
-                },
-              ],
-            },
-          },
-        },
-      },
-      deposit: "10000000000000000000000", // 0.1 NEAR
-      gas: "219000000000000", // Specified gas amount
-    },
-  ]);
-};
-
-// Main React JSX component for the profile editing interface
 return (
   <div>
     <div className="row">
@@ -98,7 +49,7 @@ return (
         <div>
           <Widget
             src="near/widget/ProfilePage"
-            props={{ daoId, profile: state.profile }}
+            props={{ accountId: daoId, profile: state.profile }}
           />
         </div>
       </div>
