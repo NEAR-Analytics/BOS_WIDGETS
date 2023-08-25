ebba20bb3735cc445b1fb6247f7a6b29e7e4f9a8
bluebiu.near <bluebiu.near>
2023-08-25 04:39:22 +0000

    Update Mantle.Quoter by bluebiu.near at 2023-08-25T04:39:22.404Z

--- bluebiu.near/Mantle.Quoter/source_code.jsx
+++ bluebiu.near/Mantle.Quoter/source_code.jsx
@@ -1,54 +1,4 @@
-const abi = [
-  {
-    type: "constructor",
-    inputs: [
-      { type: "address", name: "_deployer", internalType: "address" },
-      { type: "address", name: "_factory", internalType: "address" },
-      { type: "address", name: "_WMNT", internalType: "address" },
-    ],
-  },
-  {
-    type: "function",
-    stateMutability: "view",
-    outputs: [{ type: "address", name: "", internalType: "address" }],
-    name: "WMNT",
-    inputs: [],
-  },
-  {
-    type: "function",
-    stateMutability: "view",
-    outputs: [],
-    name: "agniSwapCallback",
-    inputs: [
-      { type: "int256", name: "amount0Delta", internalType: "int256" },
-      { type: "int256", name: "amount1Delta", internalType: "int256" },
-      { type: "bytes", name: "path", internalType: "bytes" },
-    ],
-  },
-  {
-    type: "function",
-    stateMutability: "view",
-    outputs: [{ type: "address", name: "", internalType: "address" }],
-    name: "deployer",
-    inputs: [],
-  },
-  {
-    type: "function",
-    stateMutability: "view",
-    outputs: [{ type: "address", name: "", internalType: "address" }],
-    name: "factory",
-    inputs: [],
-  },
-  {
-    type: "function",
-    stateMutability: "nonpayable",
-    outputs: [{ type: "uint256", name: "amountOut", internalType: "uint256" }],
-    name: "quoteExactInput",
-    inputs: [
-      { type: "bytes", name: "path", internalType: "bytes" },
-      { type: "uint256", name: "amountIn", internalType: "uint256" },
-    ],
-  },
+const abiAgni = [
   {
     type: "function",
     stateMutability: "nonpayable",
@@ -64,76 +14,210 @@ const abi = [
   },
 ];
 
+const abiFusion_Ammos = [
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "tokenIn",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "tokenOut",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "amountIn",
+            type: "uint256",
+          },
+          {
+            internalType: "uint24",
+            name: "fee",
+            type: "uint24",
+          },
+          {
+            internalType: "uint160",
+            name: "sqrtPriceLimitX96",
+            type: "uint160",
+          },
+        ],
+        internalType: "struct IQuoterV2.QuoteExactInputSingleParams",
+        name: "params",
+        type: "tuple",
+      },
+    ],
+    name: "quoteExactInputSingle",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "amountOut",
+        type: "uint256",
+      },
+      {
+        internalType: "uint160",
+        name: "sqrtPriceX96After",
+        type: "uint160",
+      },
+      {
+        internalType: "uint32",
+        name: "initializedTicksCrossed",
+        type: "uint32",
+      },
+      {
+        internalType: "uint256",
+        name: "gasEstimate",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
 const { amountIn, tokenIn, tokenOut, loadAmountOut, config, selectedDex } =
   props;
+
+const feeList = [100, 500, 3000, 10000];
 
 const queryString = `${tokenIn.address}-${tokenOut.address}-${selectedDex}-${amountIn}`;
 
 if (state.cacheString !== queryString) {
   State.update({
     cacheString: queryString,
+    quoteDone: false,
+    quoting: false,
   });
-} else {
-  return <div></div>;
 }
 
-const quoteStirng = `${amountIn} ${tokenIn.symbol} -> ${tokenOut.symbol} `;
+const getAbi = (selectedDex) => {
+  if (selectedDex === "Agni Finance") {
+    return abiAgni;
+  } else if (selectedDex !== "iZiSwap") {
+    return abiFusion_Ammos;
+  }
+};
 
 const selectedDexItem = config.dapps.find((dapp) => dapp.name === selectedDex);
 
-const quote = (amountIn, tokenIn, tokenOut) => {
+const quoteSingle = (amountIn, tokenIn, tokenOut, fee, finalList) => {
+  const abi = getAbi(selectedDex);
+
+  console.log("selectedDexItem.quoter: ", selectedDexItem.quoter);
   const iface = new ethers.utils.Interface(abi);
 
-  const path = [tokenIn.address, tokenOut.address];
-  console.log(
-    "tokenIn.address, tokenOut.address: ",
-    tokenIn.address,
-    tokenOut.address
-  );
-  console.log("path: ", path);
+  const inputs =
+    selectedDex === "Agni Finance"
+      ? [
+          tokenIn.address,
+          tokenOut.address,
+          fee,
+          ethers.utils.parseUnits(amountIn, tokenIn.decimals),
+          0,
+        ]
+      : [
+          {
+            tokenIn: tokenIn.address,
+            tokenOut: tokenOut.address,
+            amountIn: ethers.utils.parseUnits(amountIn, tokenIn.decimals),
+            fee: fee,
+            sqrtPriceLimitX96: 0,
+          },
+        ];
 
-  const pathBytes = "0x" + path.map((address) => address.substr(2)).join("");
-  console.log("pathBytes: ", pathBytes);
-
-  const inputs = [
-    pathBytes,
-    Big(amountIn).times(Big(10).pow(tokenIn.decimals)).toFixed(0),
-  ];
-
-  const encodedData = iface.encodeFunctionData("quoteExactInput", inputs);
+  const encodedData = iface.encodeFunctionData("quoteExactInputSingle", inputs);
 
   const quoterContractId = selectedDexItem.quoter;
 
-  Ethers.provider()
+  return Ethers.provider()
     .call({
-      to: quoterContractId.trim(),
+      to: quoterContractId,
       data: encodedData,
     })
     .then((data) => {
-      const decodedData = iface.decodeFunctionResult("quoteExactInput", data);
-      console.log("decodedData: ", decodedData);
+      const res = iface.decodeFunctionResult("quoteExactInputSingle", data);
+      console.log("res: ", res);
 
-      const amountOut = decodedData[0];
-      const fee = decodedData[1];
+      const rawAmountOut = Big(Number(res.amountOut._hex)).toFixed();
 
-      const estimate = Big(amountOut.toString())
+      const parsedAmountOut = new Big(rawAmountOut)
         .div(Big(10).pow(tokenOut.decimals))
-        .toFixed(18);
+        .toFixed();
 
-      loadAmountOut({
-        amountOut: estimate,
-        fee,
-      });
+      return [
+        ...finalList,
+        {
+          fee: fee,
+          amountOut: parsedAmountOut,
+          success: true,
+        },
+      ];
     })
     .catch((e) => {
-      console.log(e, "error");
+      console.log(" single quote: ", e);
+      return [
+        ...finalList,
+        {
+          fee: fee,
+          amountOut: "0",
+          success: false,
+        },
+      ];
     });
 };
 
-if (Number(amountIn) > 0) {
-  console.log("quote 111");
+const quoteAll = () => {
+  State.update({
+    quoting: true,
+  });
 
-  quote(amountIn, tokenIn, tokenOut);
+  quoteSingle(amountIn, tokenIn, tokenOut, feeList[0], []).then(
+    (finalList0) => {
+      return quoteSingle(
+        amountIn,
+        tokenIn,
+        tokenOut,
+        feeList[1],
+        finalList0
+      ).then((finalList1) => {
+        return quoteSingle(
+          amountIn,
+          tokenIn,
+          tokenOut,
+          feeList[2],
+          finalList1
+        ).then((finalList2) => {
+          return quoteSingle(
+            amountIn,
+            tokenIn,
+            tokenOut,
+            feeList[3],
+            finalList2
+          ).then((finalList3) => {
+            const maxAmountOutEstimate = finalList3.reduce((prev, current) => {
+              if (Number(prev.amountOut) > Number(current.amountOut)) {
+                return prev;
+              } else {
+                return current;
+              }
+            }, finalList3[0]);
+
+            State.update({ quoteDone: true, quoting: false });
+            loadAmountOut({
+              ...maxAmountOutEstimate,
+            });
+          });
+        });
+      });
+    }
+  );
+};
+
+if (Number(amountIn) > 0 && !state.quoteDone && !state.quoting) {
+  quoteAll();
 }
 
 return <div></div>;
