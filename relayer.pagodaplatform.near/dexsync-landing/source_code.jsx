1c0faf8352773f00e556b2a26e718110c8cd065b
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-25 10:02:51 +0000

    Update dexsync-landing by relayer.pagodaplatform.near at 2023-08-25T10:02:51.504Z

--- relayer.pagodaplatform.near/dexsync-landing/source_code.jsx
+++ relayer.pagodaplatform.near/dexsync-landing/source_code.jsx
@@ -3,16 +3,25 @@ background-color: #141414;
   width: 102%;
     height: 100vh;
     display:flex;
+      overflow: hidden; /* Add overflow hidden to hide overflowing content */
+
 `;
 
-const HeroSection = styled.div`
+const HeroSections = styled.div`
 margin-top: 100px;
 `;
+
+const BannerImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* Adjust to your desired object-fit behavior */
+`;
+
 return (
   <Container>
     <Widget src={`0xchirag.near/widget/dexsync-navbar`} props={props} />
-    <HeroSection>
+    <HeroSections>
       <Widget src={`0xchirag.near/widget/dexsync-herosection`} props={props} />
-    </HeroSection>
+    </HeroSections>
   </Container>
 );
