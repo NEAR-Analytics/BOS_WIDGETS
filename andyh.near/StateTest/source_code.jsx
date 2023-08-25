d76f39cf586e023db8b6f9d3762fbe5a5218f8b4
andyh.near <andyh.near>
2023-08-25 23:51:25 +0000

    Update StateTest by andyh.near at 2023-08-25T23:51:25.787Z

--- andyh.near/StateTest/source_code.jsx
+++ andyh.near/StateTest/source_code.jsx
@@ -2,17 +2,17 @@ State.init({ x: -1, y: -1, z: -1 });
 
 return (
   <>
-    <span>x: ${state.x}</span>{" "}
+    <span>x: {state.x}</span>{" "}
     <button onClick={() => State.update({ x: state.x + 1 })}>
       increment x
     </button>
     <br />
-    <span>x: ${state.y}</span>{" "}
+    <span>y: {state.y}</span>{" "}
     <button onClick={() => State.update({ y: state.y + 1 })}>
       increment y
     </button>
     <br />
-    <span>x: ${state.z}</span>{" "}
+    <span>z: {state.z}</span>{" "}
     <button onClick={() => State.update({ z: state.z + 1 })}>
       increment z
     </button>
