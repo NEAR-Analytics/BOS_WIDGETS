3169817d18a2d6dd51cbd06f1224ad462f261152
chloe.near <chloe.near>
2023-08-25 20:26:28 +0000

    Update ProfileEditor by chloe.near at 2023-08-25T20:26:28.021Z

--- chloe.near/ProfileEditor/source_code.jsx
+++ chloe.near/ProfileEditor/source_code.jsx
@@ -1,19 +1,13 @@
-// Styled component
-const Wrapper = styled.div`
-  #pills-tab,
-  #pills-tabContent {
-    display: none;
-  }
-`;
-
-// Initialize state and variables
 const accountId = context.accountId;
+
 if (!accountId) {
   return "Please sign in with NEAR wallet to edit your profile";
 }
 
-const daoId = "research-collective.sputnik-dao.near";
-let profile = Social.getr(`${daoId}/profile`);
+let profile = Social.getr(
+  `${research - collective.sputnik - dao.near}/profile`
+);
+
 if (profile === null) {
   return "Loading";
 }
@@ -22,64 +16,26 @@ State.init({
   profile,
 });
 
-// Function to handle the proposal for updating the DAO profile
-const handleProfileUpdateProposal = () => {
-  const profile_update_args = JSON.stringify({
-    data: {
-      [daoId]: {
-        profile: {
-          main: JSON.stringify(state.profile),
-        },
-      },
-    },
-  });
-
-  const proposal_update_args = Buffer.from(
-    profile_update_args,
-    "utf-8"
-  ).toString("base64");
-
-  Near.call([
-    {
-      contractName: daoId,
-      methodName: "add_proposal",
-      args: {
-        proposal: {
-          description: "Update DAO profile on NEAR Social",
-          kind: {
-            FunctionCall: {
-              receiver_id: "social.near",
-              actions: [
-                {
-                  method_name: "set",
-                  args: proposal_update_args,
-                  deposit: "10000000000000000000000", // 0.1 NEAR
-                  gas: "219000000000000", // Gas limit allocated for the smart contract execution
-                },
-              ],
-            },
-          },
-        },
-      },
-      deposit: "10000000000000000000000", // 0.1 NEAR
-      gas: "219000000000000", // Gas limit allocated for the smart contract execution
-    },
-  ]);
-};
+const Wrapper = styled.div`
+  #pills-tab,
+  #pills-tabContent {
+    display: none;
+  }
+`;
 
 return (
   <Wrapper>
     <div className="row">
       <div className="col-lg-6">
         <div>
-          <h4>Edit profile of @{daoId}</h4>
+          <h4>Edit profile of @{accountId}</h4>
         </div>
         <div className="mb-2">
           <Widget
             src="near/widget/MetadataEditor"
             props={{
               initialMetadata: profile,
-              onChange: (newProfile) => State.update({ profile: newProfile }),
+              onChange: (profile) => State.update({ profile }),
               options: {
                 name: { label: "Name" },
                 image: { label: "Profile picture" },
@@ -108,7 +64,11 @@ return (
                       prefix: "https://t.me/",
                       name: "telegram",
                     },
-                    { label: "Website", prefix: "https://", name: "website" },
+                    {
+                      label: "Website",
+                      prefix: "https://",
+                      name: "website",
+                    },
                   ],
                 },
               },
@@ -116,10 +76,12 @@ return (
           />
         </div>
         <div className="mb-2">
-          <button onClick={handleProfileUpdateProposal}>Save profile</button>
+          <CommitButton data={{ profile: state.profile }}>
+            Save profile
+          </CommitButton>
           <a
             className="btn btn-outline-primary ms-2"
-            href={`#/near/widget/ProfilePage?accountId=${daoId}`}
+            href={`#/near/widget/ProfilePage?accountId=${accountId}`}
           >
             View profile
           </a>
@@ -129,7 +91,7 @@ return (
         <div>
           <Widget
             src="near/widget/ProfilePage"
-            props={{ accountId: daoId, profile: state.profile }}
+            props={{ accountId, profile: state.profile }}
           />
         </div>
       </div>
