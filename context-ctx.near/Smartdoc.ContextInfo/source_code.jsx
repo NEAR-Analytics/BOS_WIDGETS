2358a0af1474c9aab3c808fa47ef95b00a1df020
context-ctx.near <context-ctx.near>
2023-08-25 09:38:14 +0000

    Update Smartdoc.ContextInfo by context-ctx.near at 2023-08-25T09:38:14.490Z

---
+++ context-ctx.near/Smartdoc.ContextInfo/source_code.jsx
@@ -0,0 +1,60 @@
+// Styles
+const Title = styled.div`
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #345AD5; 
+`;
+const Container = styled.div`
+  position: relative;
+  margin-top: 5px;
+  width: 100%;
+  padding: 10px 40px;
+  height: auto;
+  min-height: 80px;
+  overflow: hidden;
+`;
+
+// States
+
+/**
+ * Render
+ */
+let ctxRpc;
+let ctxApp;
+switch (props.network) {
+  case `devnet`:
+    ctxRpc = `http://localhost:4000`;
+    ctxApp = `http://localhost:4100/d`;
+    break;
+  case `testnet`:
+    ctxRpc = `https://testrpc.ctx.xyz`;
+    ctxApp = `https://app.ctx.xyz/d`;
+    break;
+  default:
+    ctxRpc = `https://ctx.xyz`;
+    ctxApp = `https://app.ctx.xyz/d`;
+    break;
+}
+ctxRpc += `/near/${context.accountId}`;
+ctxApp += `/near/${context.accountId}?network=${props.network}`;
+
+return (
+  <>
+    <div class="container">
+      <Title>Context links</Title>
+      <Container>
+        <div>
+          <a href={ctxRpc} target={`_blank`}>
+            {ctxRpc}
+          </a>
+        </div>
+        <div>
+          <a href={ctxApp} target={`_blank`}>
+            {ctxApp}
+          </a>
+        </div>
+      </Container>
+    </div>
+  </>
+);
