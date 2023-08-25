805dad6db58b702e596652cafd6b4005b7212fa3
hack.near <hack.near>
2023-08-25 13:21:45 +0000

    Update group.edit by hack.near at 2023-08-25T13:21:45.206Z

---
+++ hack.near/group.edit/source_code.jsx
@@ -0,0 +1,78 @@
+const creatorId = props.creatorId ?? context.accountId;
+const groupId = props.groupId;
+
+if (!creatorId) {
+  return "Please connect your NEAR account :)";
+}
+
+State.init({
+  groupId,
+  group,
+});
+
+return (
+  <>
+    <div className="row">
+      <div className="col-lg-6 mt-2">
+        <div className="mb-3">
+          <h5>Details</h5>
+        </div>
+        <Widget
+          src="hack.near/widget/group.card"
+          props={{ creatorId, groupId, group: state.group }}
+        />
+        <div className="mb-2 mt-3">
+          <Widget
+            src="near/widget/MetadataEditor"
+            props={{
+              initialMetadata: group,
+              onChange: (group) => State.update({ group }),
+              options: {
+                name: { label: "Name" },
+                image: { label: "Logo" },
+                description: { label: "About" },
+                tags: {
+                  label: "Tags",
+                  tagsPattern: `*/${groupId}/tags/*`,
+                  placeholder: "art, gov, edu, dev, com, nft, ai, social",
+                },
+                linktree: {
+                  links: [
+                    {
+                      label: "Twitter",
+                      prefix: "https://twitter.com/",
+                      name: "twitter",
+                    },
+                    {
+                      label: "Github",
+                      prefix: "https://github.com/",
+                      name: "github",
+                    },
+                    {
+                      label: "Telegram",
+                      prefix: "https://t.me/",
+                      name: "telegram",
+                    },
+                    {
+                      label: "Website",
+                      prefix: "https://",
+                      name: "website",
+                    },
+                  ],
+                },
+              },
+            }}
+          />
+        </div>
+      </div>
+      <div className="col-lg-6">
+        <div className="m-2">
+          <Widget
+            src="hack.near/widget/group.list"
+            props={{ creatorId, groupId: state.groupId }}
+          />
+        </div>
+      </div>
+    </div>
+  </>
+);
