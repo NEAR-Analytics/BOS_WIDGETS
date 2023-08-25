2aa28416354d14de3c798185051956e8198c5bef
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-25 09:40:05 +0000

    Update dexsync-herosection by relayer.pagodaplatform.near at 2023-08-25T09:40:05.721Z

--- relayer.pagodaplatform.near/dexsync-herosection/source_code.jsx
+++ relayer.pagodaplatform.near/dexsync-herosection/source_code.jsx
@@ -1,6 +1,7 @@
 const HeroSection = styled.div`
 display: flex;
-margin-top: 50px
+margin-top: 50px;
+position: relative;
 `;
 
 const LeftDiv = styled.div`
@@ -48,10 +49,31 @@ const Mesh = styled.div`
   color: #6F3CE4; /* Set your desired color */
   width: 610px;
   height: 489px;
-  padding-top: 100px; /* Adjust the padding as needed */
   position: absolute;
   right: -250px; /* Position it to the left, one full width away */
   top: 0;
+`;
+
+const MeshImage = styled.img`
+  width: 100%;
+  height: 100%;
+    border: 1px solid #6F3CE4; /* Add border at the bottom */
+
+
+  object-fit: cover; /* Adjust to your desired object-fit behavior */
+`;
+
+const Banner = styled.div`
+  width: 340px;
+  position: absolute;
+  margin-left: 50px;
+  top: 0;
+`;
+
+const BannerImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* Adjust to your desired object-fit behavior */
 `;
 
 return (
@@ -68,8 +90,12 @@ return (
 
     <RightDiv>
       <Mesh>
-        <img src="https://i.ibb.co/8gF6vxB/Frame-3.png" />
+        <MeshImage src="https://i.ibb.co/8gF6vxB/Frame-3.png" />
       </Mesh>
+
+      <Banner>
+        <BannerImage src="https://i.ibb.co/r2F3T6D/Group-10.png" />
+      </Banner>
     </RightDiv>
   </HeroSection>
 );
