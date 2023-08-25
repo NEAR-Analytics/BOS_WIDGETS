ffa217cd15b525aa63d139d25361efe670efb86f
election.ndctools.near <election.ndctools.near>
2023-08-25 19:19:17 +0000

    Update NDC.Elections.Candidates by election.ndctools.near at 2023-08-25T19:19:17.130Z

--- election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
@@ -686,14 +686,16 @@ const ALink = ({ title, href }) => (
   </a>
 );
 
-function rand(array) {
+const rand = (array) => {
   for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
     var temp = array[i];
     array[i] = array[j];
     array[j] = temp;
   }
-}
+
+  return array;
+};
 
 return (
   <>
