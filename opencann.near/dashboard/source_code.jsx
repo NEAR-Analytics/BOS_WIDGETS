8230861056a807776ed540f899f9683d7d108cbd
opencann.near <opencann.near>
2023-08-26 02:31:13 +0000

    Update dashboard by opencann.near at 2023-08-26T02:31:13.296Z

--- opencann.near/dashboard/source_code.jsx
+++ opencann.near/dashboard/source_code.jsx
@@ -4,7 +4,7 @@ State.init({
 
 const accountId = props.accountId ?? context.accountId;
 const daoId = props.daoId ?? "cannabis-genome.sputnik-dao.near";
-const ownerId = props.ownerId ?? "opencann.near";
+const addressId = props.addressId ?? "opencann.near";
 
 const page = accountId
   ? Social.get(`${accountId}/settings/dao/page`)
@@ -28,7 +28,7 @@ if (props.tab && props.tab !== state.selectedTab) {
   });
 }
 
-const profile = props.profile ?? Social.getr(`${ownerId}/profile`);
+const profile = props.profile ?? Social.getr(`${addressId}/profile`);
 const accountUrl = `#/opencann.near/widget/dashboard?`;
 
 const Wrapper = styled.div`
@@ -218,7 +218,7 @@ return (
         <Widget
           src={sidebarTemplate}
           props={{
-            accountId,
+            addressId,
             profile,
           }}
         />
