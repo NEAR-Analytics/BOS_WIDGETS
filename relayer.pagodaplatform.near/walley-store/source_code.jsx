e681014c2f38087712c79806cfa03182550c7780
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-26 09:46:14 +0000

    Update walley-store by relayer.pagodaplatform.near at 2023-08-26T09:46:14.970Z

--- relayer.pagodaplatform.near/walley-store/source_code.jsx
+++ relayer.pagodaplatform.near/walley-store/source_code.jsx
@@ -1,10 +1,11 @@
 const Styles = {
-  Style: styled.div`
-        display: block;
-        width:100px;
-        height: 100px;
-        background: red;
-    `,
+  Style: styled("div")(
+    [],
+    "display: block",
+    "width:100px",
+    "height: 100px",
+    "background: red"
+  ),
 };
 
 return <Styles.Style></Styles.Style>;
