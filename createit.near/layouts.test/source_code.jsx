ed93cb5e6bd891cb08137462fb07be57d59aa6c1
createit.near <createit.near>
2023-08-26 11:33:05 +0000

    Update layouts.test by createit.near at 2023-08-26T11:33:05.772Z

---
+++ createit.near/layouts.test/source_code.jsx
@@ -0,0 +1,20 @@
+/**
+ * Check this out
+ */
+return (
+  <Widget
+    src="createit.near/widget/layouts.Container"
+    props={{
+      Left: () => (
+        <>
+          <Widget src="createit.near/widget/layouts.placeholder" props={{}} />
+        </>
+      ),
+      Right: () => (
+        <>
+          <Widget src="createit.near/widget/layouts.placeholder" props={{}} />
+        </>
+      ),
+    }}
+  />
+);
