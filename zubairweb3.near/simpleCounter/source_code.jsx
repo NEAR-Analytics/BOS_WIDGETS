1d1f365e5ae6db12b2a72321a53886fc6b099ee7
zubairweb3.near <zubairweb3.near>
2023-08-26 09:39:46 +0000

    Update simpleCounter by zubairweb3.near at 2023-08-26T09:39:46.299Z

---
+++ zubairweb3.near/simpleCounter/source_code.jsx
@@ -0,0 +1,36 @@
+State.init({ count: 0 });
+
+let increaseCount = () => {
+  State.update({ count: state.count + 1 });
+};
+
+let decreaseCount = () => {
+  State.update({ count: state.count - 1 });
+};
+
+let resetCount = () => {
+  State.update({ count: (state.count = 0) });
+};
+
+return (
+  <div
+    class="container-fluid d-flex justify-content-center align-items-center"
+    style={{
+      backgroundImage: "linear-gradient(to right, #FF61D2, #FE9090)",
+      height: "100vh",
+      width: "70%",
+      margin: "auto",
+      borderRadius: "15px",
+    }}
+  >
+    <div style={{ textAlign: "center" }}>
+      <h2 style={{ color: "#fff" }}>Simple Counter App</h2>
+      <p style={{ fontSize: "4rem", color: "grey" }}>{state.count}</p>
+      <button onClick={decreaseCount}>Decrease</button>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={resetCount} class="btn btn-secondary">
+        Reset
+      </button>
+    </div>
+  </div>
+);
