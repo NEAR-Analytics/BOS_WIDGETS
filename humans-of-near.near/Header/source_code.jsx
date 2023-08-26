cf3dfce69ffdf7967b90f0c78b438fcbcbb9a198
humans-of-near.near <humans-of-near.near>
2023-08-26 12:14:21 +0000

    Update Header by humans-of-near.near at 2023-08-26T12:14:21.333Z

--- humans-of-near.near/Header/source_code.jsx
+++ humans-of-near.near/Header/source_code.jsx
@@ -21,7 +21,7 @@ return (
       //fill="none"
       //xmlns="http://www.w3.org/2000/svg"
 
-      viewBox="0 0 800 800"
+      viewBox="0 0 135 135"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
