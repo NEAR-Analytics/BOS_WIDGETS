ec2bdacc084aea2cb480f057a1db95e9f361c6aa
createit.near <createit.near>
2023-08-26 11:33:05 +0000

    Update Layout by createit.near at 2023-08-26T11:33:05.772Z

---
+++ createit.near/Layout/source_code.jsx
@@ -0,0 +1,25 @@
+const widget = (src, props, other) => (
+  <Widget src={src} props={props} {...other} />
+);
+
+
+const { pages, onPageChange, templates, Children } = props;
+
+State.init({
+  mobileNavbarOpen: false,
+});
+
+const update = (k, v) => State.update({ [k]: v });
+
+return (
+  <>
+  {/* I'm passing in a template that could be configured in the app provider? */}
+    {widget(templates["NAVBAR"] ?? "createit.near/widget/templates.ui.navbar.default", {
+      open: state.mobileNavbarOpen,
+      setOpen: (v) => update("mobileNavbarOpen", v),
+      pages,
+      onPageChange,
+      Children
+    })}
+  </>
+);
