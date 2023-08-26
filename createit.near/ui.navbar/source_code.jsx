43b2597d5b41e6e6df3512bf9dffa0640d636e99
createit.near <createit.near>
2023-08-26 11:33:05 +0000

    Update ui.navbar by createit.near at 2023-08-26T11:33:05.772Z

---
+++ createit.near/ui.navbar/source_code.jsx
@@ -0,0 +1,23 @@
+const widget = (src, props, other) => (
+  <Widget src={src} props={props} {...other} />
+);
+
+
+const { pages, onPageChange, template } = props;
+
+State.init({
+  mobileNavbarOpen: false,
+});
+
+const update = (k, v) => State.update({ [k]: v });
+
+return (
+  <>
+    {widget(template ?? "createit.near/widget/templates.ui.navbar.default", {
+      open: state.mobileNavbarOpen,
+      setOpen: (v) => update("mobileNavbarOpen", v),
+      pages,
+      onPageChange
+    })}
+  </>
+);
