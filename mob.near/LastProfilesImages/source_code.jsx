526e797364caa9e8179cae6f213f04e992a9d683
mob.near <mob.near>
2023-08-27 20:05:36 +0000

    Update LastProfilesImages by mob.near at 2023-08-27T20:05:36.635Z

--- mob.near/LastProfilesImages/source_code.jsx
+++ mob.near/LastProfilesImages/source_code.jsx
@@ -24,7 +24,7 @@ for (let i = 0; i < accounts.length; ++i) {
       <Widget
         loading={
           <div
-            className="placeholder d-inline-block rounded-circle"
+            className="placeholder rounded-circle"
             style={{ width: "3em", height: "3em" }}
           />
         }
