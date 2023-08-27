cb09f43cba231f70be93da0693d6680e2be1ee79
mob.near <mob.near>
2023-08-27 19:06:34 +0000

    Update MainPage.N.Post.Left by mob.near at 2023-08-27T19:06:34.082Z

--- mob.near/MainPage.N.Post.Left/source_code.jsx
+++ mob.near/MainPage.N.Post.Left/source_code.jsx
@@ -3,19 +3,17 @@ const accountId = props.accountId;
 const imgWrapperStyle = { width: "40px", height: "40px" };
 
 return (
-  <div className="left">
-    <a href={`/mob.near/widget/ProfilePage?accountId=${accountId}`}>
-      <Widget
-        src="mob.near/widget/ProfileImage"
-        loading={<div style={imgWrapperStyle} />}
-        props={{
-          accountId,
-          tooltip: true,
-          link: true,
-          style: imgWrapperStyle,
-          imageClassName: "rounded-circle w-100 h-100",
-        }}
-      />
-    </a>
-  </div>
+  <a href={`/mob.near/widget/ProfilePage?accountId=${accountId}`}>
+    <Widget
+      src="mob.near/widget/ProfileImage"
+      loading={<div style={imgWrapperStyle} />}
+      props={{
+        accountId,
+        tooltip: true,
+        link: true,
+        style: imgWrapperStyle,
+        imageClassName: "rounded-circle w-100 h-100",
+      }}
+    />
+  </a>
 );
