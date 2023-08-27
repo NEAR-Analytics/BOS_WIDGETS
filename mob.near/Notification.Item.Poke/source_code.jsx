f74f7e119e0189221ad089bc0b26862df94d249a
mob.near <mob.near>
2023-08-27 19:51:53 +0000

    Update Notification.Item.Poke by mob.near at 2023-08-27T19:51:53.691Z

--- mob.near/Notification.Item.Poke/source_code.jsx
+++ mob.near/Notification.Item.Poke/source_code.jsx
@@ -2,11 +2,13 @@ const { accountId } = props;
 
 return (
   <Widget
+    loading={props.loading}
     src="mob.near/widget/Notification.Item.LR"
     props={{
       L: "poked you",
       R: (
         <Widget
+          loading=""
           src="mob.near/widget/PokeButton"
           props={{ accountId, back: true }}
         />
