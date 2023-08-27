0d175475c20df8df8152eba285b29de2ad362239
thanhhoa214.near <thanhhoa214.near>
2023-08-27 18:08:54 +0000

    Update decarbon by thanhhoa214.near at 2023-08-27T18:08:54.838Z

--- thanhhoa214.near/decarbon/source_code.jsx
+++ thanhhoa214.near/decarbon/source_code.jsx
@@ -34,24 +34,7 @@ return (
         marginBottom: "1rem",
       }}
     >
-      <label
-        htmlFor="address"
-        style={{ fontWeight: 600, marginBottom: "0.25rem" }}
-      >
-        Address
-      </label>
-      <div style={{ display: "flex", gap: "0.5rem" }}>
-        <input
-          id="address"
-          type="text"
-          placeholder="Enter an address to check carbon emission"
-          style={{
-            padding: "0.5rem 0.75rem",
-            borderRadius: "0.25rem",
-            border: "1px solid #e5e7eb",
-          }}
-        />
-      </div>
+      <Widget src="thanhhoa214.near/widget/carbon-filter" />
     </div>
     <div style={cardStyle}>
       <Widget src="thanhhoa214.near/widget/carbon-table" />
