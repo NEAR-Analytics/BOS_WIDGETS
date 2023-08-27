7a212478ed886b9e90a15446cf29fdb12ed3b269
mob.near <mob.near>
2023-08-27 19:23:50 +0000

    Update MainPage.N.Feed by mob.near at 2023-08-27T19:23:50.545Z

--- mob.near/MainPage.N.Feed/source_code.jsx
+++ mob.near/MainPage.N.Feed/source_code.jsx
@@ -40,11 +40,7 @@ const renderPost = (a) => {
   return (
     <div key={JSON.stringify(a)}>
       <Widget
-        loading={
-          <div className="w-100" style={{ minHeight: "200px" }}>
-            Post
-          </div>
-        }
+        loading={<div className="w-100" style={{ minHeight: "200px" }} />}
         src="mob.near/widget/MainPage.N.Post"
         props={{ accountId: a.accountId, blockHeight: a.blockHeight }}
       />
