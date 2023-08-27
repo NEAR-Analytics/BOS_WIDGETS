6143f7ff5d3042b41c7364da71d3c4804dee1148
mob.near <mob.near>
2023-08-27 19:51:05 +0000

    Update Notification.Item.Follow by mob.near at 2023-08-27T19:51:05.569Z

--- mob.near/Notification.Item.Follow/source_code.jsx
+++ mob.near/Notification.Item.Follow/source_code.jsx
@@ -1,7 +1,8 @@
-const { accountId, value } = props;
+const { accountId, value, loading } = props;
 
 return (
   <Widget
+    loading={loading}
     src="mob.near/widget/Notification.Item.LR"
     props={{
       L: value.type === "follow" ? "followed you" : "unfollowed you",
