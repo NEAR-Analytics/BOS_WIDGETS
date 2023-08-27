d8be72edae10598d79a677cc1efd5317161aae23
mob.near <mob.near>
2023-08-27 19:46:07 +0000

    Update Notification.Item by mob.near at 2023-08-27T19:46:07.630Z

--- mob.near/Notification.Item/source_code.jsx
+++ mob.near/Notification.Item/source_code.jsx
@@ -1,23 +1,56 @@
 const { value } = props;
 
+const placeholder = (
+  <div className="placeholder-glow" style={{ minHeight: "48px" }} />
+);
+
 return (
   <div className="mb-3">
     {value.type === "follow" || value.type === "unfollow" ? (
-      <Widget src="mob.near/widget/Notification.Item.Follow" props={props} />
+      <Widget
+        loading={placeholder}
+        src="mob.near/widget/Notification.Item.Follow"
+        props={props}
+      />
     ) : value.type === "poke" ? (
-      <Widget src="mob.near/widget/Notification.Item.Poke" props={props} />
+      <Widget
+        loading={placeholder}
+        src="mob.near/widget/Notification.Item.Poke"
+        props={props}
+      />
     ) : value.type === "like" ? (
-      <Widget src="mob.near/widget/Notification.Item.Like" props={props} />
+      <Widget
+        loading={placeholder}
+        src="mob.near/widget/Notification.Item.Like"
+        props={props}
+      />
     ) : value.type === "comment" ? (
-      <Widget src="mob.near/widget/Notification.Item.Comment" props={props} />
+      <Widget
+        loading={placeholder}
+        src="mob.near/widget/Notification.Item.Comment"
+        props={props}
+      />
     ) : value.type && value.type?.startsWith("devgovgigs/") ? (
-      <Widget src="mob.near/widget/Notification.Item.DevGov" props={props} />
+      <Widget
+        loading={placeholder}
+        src="mob.near/widget/Notification.Item.DevGov"
+        props={props}
+      />
     ) : value.type === "mention" ? (
-      <Widget src="mob.near/widget/Notification.Item.Mention" props={props} />
+      <Widget
+        loading={placeholder}
+        src="mob.near/widget/Notification.Item.Mention"
+        props={props}
+      />
     ) : value.type === "repost" ? (
-      <Widget src="mob.near/widget/Notification.Item.Repost" props={props} />
+      <Widget
+        loading={placeholder}
+        src="mob.near/widget/Notification.Item.Repost"
+        props={props}
+      />
     ) : value.type === "chess-game" ? (
       <Widget
+        loading={placeholder}
         src="chess-game.near/widget/Notification.Item.ChessGame@98857466"
         props={props}
       />
