74ee04b4038205e870b40f6574ebfd838052e7ce
vlmoon.near <vlmoon.near>
2023-08-27 22:33:40 +0000

    Update RecommendationFeed by vlmoon.near at 2023-08-27T22:33:40.468Z

--- vlmoon.near/RecommendationFeed/source_code.jsx
+++ vlmoon.near/RecommendationFeed/source_code.jsx
@@ -1,1 +1,58 @@
-return <div>Hello World</div>;
+const GRAPHQL_ENDPOINT =
+  props.GRAPHQL_ENDPOINT ||
+  "https://queryapi-hasura-graphql-24ktefolwq-ew.a.run.app";
+
+State.init({
+  posts: [],
+});
+
+const createQuery = () => {
+  const indexerQueries = `
+  query GetMyFeed {
+    dataplatform_near_social_feed_posts(order_by: {block_height: desc}) {
+      account_id
+      block_timestamp
+      content
+      comments{account_id,block_height}
+      post_likes{account_id,block_height}
+    }
+  }
+`;
+  return indexerQueries;
+};
+
+function fetchGraphQL(operationsDoc, operationName, variables) {
+  return asyncFetch(`${GRAPHQL_ENDPOINT}/v1/graphql`, {
+    method: "POST",
+    headers: { "x-hasura-role": "dataplatform_near" },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
+}
+
+fetchGraphQL(createQuery(type), queryName, {}).then((result) => {
+  if (result.status === 200 && result.body) {
+    if (result.body.errors) {
+      console.log("error:", result.body.errors);
+      return;
+    }
+    let data = result.body.data;
+    if (data) {
+      const newPosts = data.dataplatform_near_social_feed_posts;
+      State.update({
+        posts: [...newPosts],
+      });
+    }
+  }
+});
+
+const renderData = (a) => {
+  return <div key={JSON.stringify(a)}>{JSON.stringify(a)}</div>;
+};
+
+const renderedData = state.posts.map(renderData);
+
+return { renderedData };
