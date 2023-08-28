312c4ace1660475e1ff80634a45da07972e46f47
manzanal.near <manzanal.near>
2023-08-28 13:19:45 +0000

    Update Governance.ProposalsTable by manzanal.near at 2023-08-28T13:19:45.864Z

--- manzanal.near/Governance.ProposalsTable/source_code.jsx
+++ manzanal.near/Governance.ProposalsTable/source_code.jsx
@@ -1,6 +1,6 @@
 let proposals = props.proposals;
 const authorId = props.authorId || "manzanal.near";
-const contractId = props.contractId || "v006.mpip.near";
+const contractId = props.contractId || "mpip.meta-pool-dao.near";
 State.init({});
 
 // sort proposals by mpip_id descending
