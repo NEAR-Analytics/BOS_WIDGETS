6ddb8fa9021c9dd96757b803e4f8beb60beaad0f
y3k.near <y3k.near>
2023-08-29 12:38:46 +0000

    Update near_atlas.components.detail_chart by y3k.near at 2023-08-29T12:38:46.738Z

--- y3k.near/near_atlas.components.detail_chart/source_code.jsx
+++ y3k.near/near_atlas.components.detail_chart/source_code.jsx
@@ -2,6 +2,7 @@ const initialState = {
   selectedMetric: "MAU",
   processedData: processedData,
   metric_period: "Monthly",
+  project_name: props.project_name || "Sweat Economy",
 };
 
 state = State.init(initialState);
@@ -133,11 +134,11 @@ if (!finalData) {
   return <h1> 🪄 Loading MAGIC 🪄</h1>;
 }
 
-const project_name = props.project_name || "Sweat Economy";
+// const project_name = props.project_name || "Sweat Economy";
 
-const METRIC_NAME = `"${project_name}'s ${state.metric_period}"`;
+const METRIC_NAME = `"${state.project_name}'s ${state.metric_period}"`;
 
-const filteredData = filterByProjectName(finalData, project_name) || [];
+const filteredData = filterByProjectName(finalData, state.project_name) || [];
 
 const filteredSortedData = sortByActivityDate(filteredData) || [];
 
@@ -239,13 +240,6 @@ const v_bar_options = {
 
 // logic part-3
 
-const handleDropdownChange = (e) => {
-  console.log("selectedMetric:", e.target.value);
-  State.update({
-    selectedMetric: e.target.value,
-  });
-};
-
 const header_map = {
   DAA: "Daily Active Accounts",
   WAU: "Weekly Active Accounts",
@@ -254,7 +248,42 @@ const header_map = {
   NEW_MAA: "New MAAs",
   PERCENT_NEW: "% New Accounts",
   STICKINESS: "DAA / MAA",
+  TXNS: "Transactions",
+  //   TX_30_Days: "Transactions - 30 Days",
+  //   AVG_TXT_MAU: "Avg Txn per MAU",
 };
+
+// adding project selector:
+
+// 1. Create a function to extract unique project names:
+function getUniqueProjectNames(data) {
+  const projectNamesSet = new Set();
+  data.forEach((item) => {
+    if (item.PROJECT_NAME) {
+      projectNamesSet.add(item.PROJECT_NAME);
+    }
+  });
+  return [...projectNamesSet];
+}
+
+// 2. Get the unique project names from `finalData`:
+const uniqueProjectNames = getUniqueProjectNames(finalData);
+
+// 3. Create a handler for the project name dropdown change:
+const handleProjectDropdownChange = (e) => {
+  console.log("selectedProject:", e.target.value);
+  State.update({
+    project_name: e.target.value,
+  });
+};
+
+const handleDropdownChange = (e) => {
+  // console.log("selectedMetric:", e.target.value);
+  State.update({
+    selectedMetric: e.target.value,
+  });
+};
+
 const getBarData = () => {
   const { selectedMetric } = state;
   console.log(selectedMetric);
@@ -291,7 +320,7 @@ return (
 
                 <label
                   htmlFor="metric-dropdown"
-                  className="block text-sm font-medium text-gray-700"
+                  className="text-white block text-sm font-medium"
                 >
                   Select metric:{" "}
                 </label>
@@ -308,8 +337,27 @@ return (
                   <option value="NEW_MAA">New MAAs</option>
                   <option value="PERCENT_NEW">% New Accounts</option>
                   <option value="STICKINESS">DAA / MAA</option>
+                  <option value="TXNS">Transactions</option>
                 </select>
-
+                <label
+                  htmlFor="project-dropdown"
+                  htmlFor="metric-dropdown"
+                  className="text-white block text-sm font-medium"
+                >
+                  Select Project:{" "}
+                </label>
+                <select
+                  id="project-dropdown"
+                  value={project_name}
+                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring focus:border-blue-300 sm:text-sm rounded-md"
+                  onChange={handleProjectDropdownChange}
+                >
+                  {uniqueProjectNames.map((name) => (
+                    <option key={name} value={name}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
                 <BarEl options={v_bar_options} data={getBarData()} />
               </div>
             </div>
