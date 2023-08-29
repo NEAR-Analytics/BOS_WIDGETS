c0bb44e86d01b3d36f78b1e4753d4d3b162abfe5
y3k.near <y3k.near>
2023-08-29 18:35:46 +0000

    Update near_atlas.components.resources_page by y3k.near at 2023-08-29T18:35:46.459Z

--- y3k.near/near_atlas.components.resources_page/source_code.jsx
+++ y3k.near/near_atlas.components.resources_page/source_code.jsx
@@ -48,53 +48,49 @@ const cards = [
 ];
 
 return (
-  <Style>
-    <div>
-      <div className="flex flex-col bg-white py-4 px-12">
-        <div className="py-4">
-          <div className="pl-[116px] pr-[205px] py-8">
-            <div className="text-7xl text-black">{mainTitle}</div>
-            <div className="lead-xl font-light">{mainSubtitle}</div>
-          </div>
+  <div className="bg-gray-900 text-white">
+    <div className="flex flex-col py-4 px-12">
+      <div className="py-4">
+        <div className="pl-[116px] pr-[205px] py-8">
+          <div className="text-7xl">{mainTitle}</div>
+          <div className="text-xl font-light">{mainSubtitle}</div>
         </div>
-        <div className="flex flex-col px-20 md:px-10 md:flex-row items-center justify-center gap-6">
-          {cards.map((card, index) => (
-            <div key={index}>
-              <img
-                src={card.imageSrc}
-                alt={card.imageAlt}
-                className="rounded-t-xl"
-              />
-              <div className={`px-9 pt-10 pb-14 ${card.bgColor} rounded-b-lg`}>
-                <div className="text-white space-y-4">
-                  <h3 className="text-xl font-bold lead-xl bold">
-                    {card.title}
-                  </h3>
-                  <div className="text-lg font-light">{card.subtitle}</div>
-                </div>
-                <div className="flex justify-between pt-8">
-                  <ul className="flex flex-col gap-y-2.5">
-                    {card.items.map((item, i) => (
-                      <li key={i} className="flex space-x-3 text-white">
-                        {/* Removed icon as you requested */}
-                        <span className="paragraph-l font-bold">{item}</span>
-                      </li>
-                    ))}
-                  </ul>
-                  <div className="flex flex-col justify-end">
-                    <a
-                      href="#"
-                      className="py-3 px-6 bg-white text-primary-200 paragraph-m rounded-full"
-                    >
-                      Learn More
-                    </a>
-                  </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-20 md:px-10">
+        {cards.map((card, index) => (
+          <div key={index}>
+            <img
+              src={card.imageSrc}
+              alt={card.imageAlt}
+              className="rounded-t-xl"
+            />
+            <div className={`px-9 pt-10 pb-14 ${card.bgColor} rounded-b-lg`}>
+              <div className="text-white space-y-4">
+                <h3 className="text-xl font-bold">{card.title}</h3>
+                <div className="text-lg font-light">{card.subtitle}</div>
+              </div>
+              <div className="flex justify-between pt-8">
+                <ul className="flex flex-col gap-y-2.5">
+                  {card.items.map((item, i) => (
+                    <li key={i} className="flex space-x-3">
+                      {/* Removed icon as you requested */}
+                      <span className="font-bold">{item}</span>
+                    </li>
+                  ))}
+                </ul>
+                <div className="flex flex-col justify-end">
+                  <a
+                    href="#"
+                    className="py-3 px-6 bg-white text-black rounded-full"
+                  >
+                    Learn More
+                  </a>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
-  </Style>
+  </div>
 );
