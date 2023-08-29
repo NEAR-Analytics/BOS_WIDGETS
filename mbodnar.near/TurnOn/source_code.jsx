1701035de5b22872bb22bcbef4eb3ee5de95319d
mbodnar.near <mbodnar.near>
2023-08-29 18:45:58 +0000

    Update TurnOn by mbodnar.near at 2023-08-29T18:45:58.622Z

--- mbodnar.near/TurnOn/source_code.jsx
+++ mbodnar.near/TurnOn/source_code.jsx
@@ -1,7 +1,7 @@
-const handleTurnOn = props.handleTurnOn;
-const isNotificationSupported = props.isNotificationSupported;
-const isPushManagerSupported = props.isPushManagerSupported;
-const isPermisionGranted = props.isPermisionGranted;
+const handleTurnOn = props?.handleTurnOn;
+const isNotificationSupported = props?.isNotificationSupported;
+const isPushManagerSupported = props?.isPushManagerSupported;
+const isPermisionGranted = props?.isPermisionGranted;
 
 console.log("xxx");
 
@@ -9,10 +9,16 @@ return (
   <div>
     <button onClick={handleTurnOn}>Turn On</button>
     <br />
-    <span>Notifications supported: {isNotificationSupported()}</span>
+    <span>
+      Notifications supported:{" "}
+      {isNotificationSupported && isNotificationSupported()}
+    </span>
     <br />
-    <span>PushManager supported: {isPushManagerSupported()}</span>
+    <span>
+      PushManager supported:{" "}
+      {isPushManagerSupported && isPushManagerSupported()}
+    </span>
     <br />
-    <span>permision granted: {isPermisionGranted()}</span>
+    <span>permision granted: {isPermisionGranted && isPermisionGranted()}</span>
   </div>
 );
