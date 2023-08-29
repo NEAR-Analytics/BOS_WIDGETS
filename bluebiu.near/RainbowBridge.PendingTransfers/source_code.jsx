fc2c8b2d763ed01f3ba6403baf088be44305a9dc
bluebiu.near <bluebiu.near>
2023-08-29 03:38:01 +0000

    Update RainbowBridge.PendingTransfers by bluebiu.near at 2023-08-29T03:38:01.156Z

---
+++ bluebiu.near/RainbowBridge.PendingTransfers/source_code.jsx
@@ -0,0 +1,29 @@
+const { bothConnected, tokenList } = props;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 736px;
+  color: #787da1;
+
+  .bridge-title {
+    color: #787da1;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 22px;
+    letter-spacing: 0em;
+    text-align: left;
+    padding-left: 8px;
+    padding-bottom: 8px;
+    padding-top: 8px;
+  }
+`;
+
+return (
+  <div>
+    <div>Pending Transfers</div>
+
+    {/* get api from back end */}
+  </div>
+);
