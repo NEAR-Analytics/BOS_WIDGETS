0d9941adba9aa57553f9eb3a8700bf6be7c3fe79
mbodnar.near <mbodnar.near>
2023-08-29 18:36:38 +0000

    Update TurnOn by mbodnar.near at 2023-08-29T18:36:38.702Z

--- mbodnar.near/TurnOn/source_code.jsx
+++ mbodnar.near/TurnOn/source_code.jsx
@@ -1,3 +1,16 @@
 const handleTurnOn = props.handleTurnOn;
+const isNotificationSupported = props.isNotificationSupported;
+const isPushManagerSupported = props.isPushManagerSupported;
+const isPermisionGranted = props.isPermisionGranted;
 
-return <button onClick={handleTurnOn}>Turn On</button>;
+return (
+  <div>
+    <button onClick={handleTurnOn}>Turn On</button>
+    <br />
+    <span>Notifications supported: {isNotificationSupported()}</span>
+    <br />
+    <span>PushManager supported: {isPushManagerSupported()}</span>
+    <br />
+    <span>permision granted: {isPermisionGranted()}</span>
+  </div>
+);
