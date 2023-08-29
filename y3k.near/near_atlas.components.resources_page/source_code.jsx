52edad2d771dce8960c68e3eca04d97b3d1e7354
y3k.near <y3k.near>
2023-08-29 18:42:59 +0000

    Update near_atlas.components.resources_page by y3k.near at 2023-08-29T18:42:59.045Z

--- y3k.near/near_atlas.components.resources_page/source_code.jsx
+++ y3k.near/near_atlas.components.resources_page/source_code.jsx
@@ -36,15 +36,6 @@ const cards = [
     bgColor: "bg-yellow-500",
     items: ["Item 1", "Item 2", "Item 3"],
   },
-
-  {
-    imageSrc: "https://picsum.photos/seed/picsum/200/300",
-    imageAlt: "Your Alt Text",
-    title: "Your Card Title",
-    subtitle: "Your Card Subtitle",
-    bgColor: "bg-yellow-500",
-    items: ["Item 1", "Item 2", "Item 3"],
-  },
 ];
 
 return (
@@ -59,11 +50,13 @@ return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-20 md:px-10">
         {cards.map((card, index) => (
           <div key={index}>
-            <img
-              src={card.imageSrc}
-              alt={card.imageAlt}
-              className="rounded-t-xl"
-            />
+            {/*
+              <img
+                src={card.imageSrc}
+                alt={card.imageAlt}
+                className="rounded-t-xl"
+              />
+            */}
             <div className={`px-9 pt-10 pb-14 ${card.bgColor} rounded-b-lg`}>
               <div className="text-white space-y-4">
                 <h3 className="text-xl font-bold">{card.title}</h3>
