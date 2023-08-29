242827f308fb3c64c9b563ec035a73855ace31b5
evangel.near <evangel.near>
2023-08-29 19:11:43 +0000

    Update test1 by evangel.near at 2023-08-29T19:11:43.722Z

--- evangel.near/test1/source_code.jsx
+++ evangel.near/test1/source_code.jsx
@@ -5,9 +5,11 @@ State.init({
   answer2: "",
   answer3: "",
   answer4: "",
+  answer5: "",
   optionA: "",
   optionB: "",
   optionC: "",
+  optionD: "",
 });
 
 const handleAnswer = (selectedAnswer) => {
@@ -57,6 +59,7 @@ let showQuestion2 = false;
 let showQuestion3 = false;
 let showQuestion4 = false;
 let showQuestion5 = false;
+let showQuestion6 = false;
 
 if (state.answer1.toLowerCase() === "blockchain operating system") {
   showQuestion2 = true;
@@ -81,6 +84,14 @@ return (
         <h2>
           <b>test</b>
         </h2>
+        <h4>Test Your Knowledge</h4>
+        <h5>
+          First, read the{" "}
+          <a href="https://medium.com/near-protocol-ua-eng/but-who-is-your-ndc-and-what-kind-of-election-b1552874745e">
+            article
+          </a>{" "}
+          ⋈
+        </h5>
       </div>
       <div className="m-2">
         <Widget
@@ -89,7 +100,7 @@ return (
         />
         <br></br>
         <Widget
-          src="near/widget/ShareButton"
+          src="evangel.near/widget/ShareButton"
           props={{ accountId: "evangel.near" }}
         />
         Share
@@ -99,8 +110,8 @@ return (
     <div>
       {context.accountId ? (
         <Widget
-          src="hack.near/widget/connect.button"
-          props={{ accountId: "academy.near" }}
+          src="evangel.near/widget/ShareButtonUA"
+          props={{ accountId: "evangel.near" }}
         />
       ) : (
         <Widget
@@ -115,11 +126,11 @@ return (
     </div>
     <br />
     <div>
-      <h5>Soru 1:</h5>
-      <p>"BOS" kısaltması ne anlama geliyor?</p>
+      <h5>Question 1:</h5>
+      <p>What does the "BOS" acronym stand for?</p>
       <input
         className="form-control"
-        placeholder="Cevabınızı buraya yazın :)"
+        placeholder="Your answer goes here :)"
         defaultValue={state.answer1}
         onChange={(e) => {
           State.update({
@@ -128,6 +139,59 @@ return (
         }}
       />
     </div>
+    <br />
+    {showQuestion2 && (
+      <div>
+        <h5>Question 2:</h5>
+        <p>What language is used to build on the BOS?</p>
+        <input
+          className="form-control"
+          placeholder="Your answer goes here :)"
+          defaultValue={state.answer2}
+          onChange={(e) => {
+            State.update({
+              answer2: e.target.value,
+            });
+          }}
+        />
+      </div>
+    )}
+    <br />
+    {showQuestion3 && (
+      <div>
+        <h5>Question 3:</h5>
+        <p>What is the path of this BOS component, which you are using now?</p>
+        <input
+          className="form-control"
+          placeholder="Your answer goes here :)"
+          defaultValue={state.answer3}
+          onChange={(e) => {
+            State.update({
+              answer3: e.target.value,
+            });
+          }}
+        />
+      </div>
+    )}
+    <br />
+    {showQuestion4 && (
+      <div>
+        <h5>Question 4:</h5>
+        <p>
+          Where is the code actually stored for decentralized apps on the BOS?
+        </p>
+        <input
+          className="form-control"
+          placeholder="Your answer goes here :)"
+          defaultValue={state.answer4}
+          onChange={(e) => {
+            State.update({
+              answer4: e.target.value,
+            });
+          }}
+        />
+      </div>
+    )}
     <br />
     {showQuestion5 && (
       <div>
@@ -144,155 +208,32 @@ return (
         {state.answer === "A" && (
           <div>
             <br />
-            <h5>TRY AGAIN </h5>
-            <p>
-              Components are small web 3 applications that are stored entirely
-              on-chain. Developers can fork these apps and compose them to
-              create full web applications.
-            </p>
+            <h5>HAHA LOL </h5>
+            <p>HAHA</p>
           </div>
         )}
         {state.answer === "B" && (
           <div>
             <br />
-            <h5>CONGRATS 🎉</h5>
-            <p>You got it right!</p>
-            <p>
-              Although BOS makes it easier for builders to facilitate user
-              interactions with smart contracts, they are not considered one of
-              the core pillars. However, you are just now beginning a journey to
-              understand the SocialDB contract...
-            </p>
-            <Widget
-              src="hack.near/widget/claim.badge"
-              props={{ memberId: context.accountId }}
-            />
+            <P>
+              <H5>KEK LOL </H5>
+            </P>
           </div>
         )}
         {state.answer === "C" && (
           <div>
             <br />
-
-            <h5>TRY AGAIN</h5>
-            <p>
-              Components can call functions on any blockchain, with current
-              support for all EVM chains (e.g. Polygon, zkSync) and NEAR. The
-              source code for the apps is on NEAR, due to its ability to very
-              cheaply store HTML/CSS/JS (a few cents).
-            </p>
+            <P>
+              <H5>ez lol </H5>
+            </P>
           </div>
         )}
         {state.answer === "D" && (
           <div>
             <br />
-
-            <h5>TRY AGAIN</h5>
-            <p>
-              Gateways make locally-run, decentralized front-ends available to
-              the masses. A gateway consists of a specially designed virtual
-              machine that loads and runs frontends for protocols built on
-              Ethereum, L2s, and other Layer 1s like NEAR. The code for these
-              frontends is stored on the NEAR blockchain.
-            </p>
-          </div>
-        )}
-      </div>
-    )}
-    <br />
-    {showQuestion4 && (
-      <div>
-        <h5>Soru 4:</h5>
-        <p>BOS'ta depolanan merkezi olmayan uygulamalar için kod nerede?</p>
-        <input
-          className="form-control"
-          placeholder="Cevabınızı buraya yazın :)"
-          defaultValue={state.answer4}
-          onChange={(e) => {
-            State.update({
-              answer4: e.target.value,
-            });
-          }}
-        />
-      </div>
-    )}
-    <br />
-    {showQuestion5 && (
-      <div>
-        <h5>Soru 5:</h5>
-        <p>Hangisi BOS'un 3 temel direğinden biri DEĞİLDİR?</p>
-        <button onClick={() => handleAnswer("A")}>Components</button>
-        <button onClick={() => handleAnswer("B")}>Smart Contracts</button>
-        <button onClick={() => handleAnswer("C")}>Gateways</button>
-        <button onClick={() => handleAnswer("D")}>Blockchains</button>
-      </div>
-    )}
-    {state.showOptions && (
-      <div>
-        {state.answer === "A" && (
-          <div>
-            <br />
-            <h5>TEKRAR DENEYİN </h5>
-            <p>
-              Bileşenler, tamamen depolanan küçük web 3 uygulamalarıdır. zincir
-              üzerinde. Geliştiriciler bu uygulamaları çatallayabilir ve tam web
-              uygulamaları oluşturur.
-            </p>
-          </div>
-        )}
-        {state.answer === "B" && (
-          <div>
-            <br />
-            <h5>TEBRIKLER 🎉</h5>
-            <p>
-              doğru cevapladın! Ödül için bizimle iletişime geç 👇
-              <a href="https://t.me/near_tr" target="_blank">
-                <h3>Near Türkiye Telegram</h3>
-              </a>
-              <a
-                href="https://keypom.xyz/claim/v2.keypom.near#2vSPzAZVi4P1KGf15hiD1n9zhP4g1A3V4CgAC8CLiwmdj13TnaggpQdgQsFPLADDtaJi4JvqPd4pSSdXgWRE6xLs"
-                target="_blank"
-              >
-                <h3> KEYPOM ÖDÜL LINKI ICIN TIKLA 🎁 </h3>
-              </a>
-            </p>
-            <p>
-              BOS, geliştiricilerin akıllı sözleşmelerle kullanıcı
-              etkileşimlerini kolaylaştırmasını kolaylaştırsa da, sütunlardan
-              biri olarak kabul edilmez. Ancak şimdi, Social DB sözleşmesini
-              anlama yolculuğuna çıkın...
-            </p>
-            <Widget
-              src="hack.near/widget/claim.badge"
-              props={{ memberId: context.accountId }}
-            />
-          </div>
-        )}
-        {state.answer === "C" && (
-          <div>
-            <br />
-
-            <h5>TEKRAR DENEYİN</h5>
-            <p>
-              Bileşenler, herhangi bir blok zincirindeki işlevleri çağırabilir,
-              şu anda tüm EVM zincirleri (ör. Polygon, zkSync) ve NEAR
-              desteklenmektedir. Uygulamaların kaynak kodu NEAR'da, çünkü
-              HTML/CSS/JS'yi çok ucuza (birkaç sent) depolayabilir.
-            </p>
-          </div>
-        )}
-        {state.answer === "D" && (
-          <div>
-            <br />
-
-            <h5>TEKRAR DENEYIN </h5>
-            <p>
-              Ağ geçitleri ( Gateway ), yerel olarak yürütülen, merkezi olmayan
-              ön uçları geniş kitlelerin kullanımına sunar. Bir ağ geçidi,
-              Ethereum, L2'ler ve NEAR gibi diğer Katman 1'ler üzerine kurulu
-              protokoller için ön uçları yükleyen ve çalıştıran özel olarak
-              tasarlanmış bir sanal makineden oluşur. Bu ön uçların kodu NEAR
-              blok zincirinde saklanır.
-            </p>
+            <P>
+              <H5>CONGRATS </H5>
+            </P>
           </div>
         )}
       </div>
