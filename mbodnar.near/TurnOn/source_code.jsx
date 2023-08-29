afe1282cf873277d2f3da5d86ecec990a2555a4f
mbodnar.near <mbodnar.near>
2023-08-29 19:00:03 +0000

    Update TurnOn by mbodnar.near at 2023-08-29T19:00:03.195Z

--- mbodnar.near/TurnOn/source_code.jsx
+++ mbodnar.near/TurnOn/source_code.jsx
@@ -3,23 +3,33 @@ const isNotificationSupported = props?.isNotificationSupported;
 const isPushManagerSupported = props?.isPushManagerSupported;
 const isPermisionGranted = props?.isPermisionGranted;
 
+State.init({
+  notificationSupported: "",
+  pushManagerSupported: "",
+  permisionGranted: "",
+});
+
 console.log("xxx");
 console.log("isNotificationSupported", isNotificationSupported);
+
+const turnOn = async () => {
+  await handleTurnOn();
+
+  State.update({
+    notificationSupported: isNotificationSupported(),
+    pushManagerSupported: isPushManagerSupported(),
+    permisionGranted: isPermisionGranted(),
+  });
+};
 
 return (
   <div>
     <button onClick={handleTurnOn}>Turn On</button>
     <br />
-    <span>
-      Notifications supported:{" "}
-      {isNotificationSupported && isNotificationSupported()}
-    </span>
+    <span>Notifications supported: {state.notificationSupported}</span>
     <br />
-    <span>
-      PushManager supported:{" "}
-      {isPushManagerSupported && isPushManagerSupported()}
-    </span>
+    <span>PushManager supported: {state.pushManagerSupported}</span>
     <br />
-    <span>permision granted: {isPermisionGranted && isPermisionGranted()}</span>
+    <span>permision granted: {state.permisionGranted}</span>
   </div>
 );
