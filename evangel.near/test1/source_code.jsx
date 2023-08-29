ac64adc3ed2c5085a23a4bdc61d27356a203cde6
evangel.near <evangel.near>
2023-08-29 21:10:07 +0000

    Update test1 by evangel.near at 2023-08-29T21:10:07.751Z

--- evangel.near/test1/source_code.jsx
+++ evangel.near/test1/source_code.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 State.init({
   answer: "",
   showOptions: false,
@@ -6,11 +7,14 @@ State.init({
   answer3: "",
   answer4: "",
   answer5: "",
+  answer6: "",
   optionA: "",
   optionB: "",
   optionC: "",
   optionD: "",
 });
+
+const [showContinueButton, setShowContinueButton] = useState(false);
 
 const handleAnswer = (selectedAnswer) => {
   State.update({
@@ -47,6 +51,20 @@ const handleOptionD = (selectedOption) => {
   });
 };
 
+const handleOption45 = (selectedOption) => {
+  State.update({
+    answer: selectedOption,
+    showOptions: false,
+  });
+};
+
+const handleOption56 = (selectedOption) => {
+  State.update({
+    answer: selectedOption,
+    showOptions: false,
+  });
+};
+
 let showQuestion1 = false;
 
 if (
@@ -61,20 +79,24 @@ let showQuestion4 = false;
 let showQuestion5 = false;
 let showQuestion6 = false;
 
-if (state.answer1.toLowerCase() === "blockchain operating system") {
+if (state.answer1.toLowerCase() === "1") {
   showQuestion2 = true;
 }
 
-if (state.answer2.toLowerCase() === "javascript") {
+if (state.answer2.toLowerCase() === "1") {
   showQuestion3 = true;
 }
 
-if (state.answer3 === "evangel.near/widget/test") {
+if (state.answer3.toLowerCase() === "1") {
   showQuestion4 = true;
 }
 
-if (state.answer4.toLowerCase() === "near") {
+if (state.answer4.toLowerCase() === "1") {
   showQuestion5 = true;
+}
+
+if (state.answer5.toLowerCase() === "1") {
+  showQuestion6 = true;
 }
 
 return (
@@ -197,10 +219,10 @@ return (
       <div>
         <h5>Question 5:</h5>
         <p>Which is NOT 1 of the 3 core pillars of the BOS?</p>
-        <button onClick={() => handleAnswer("A")}>Components</button>
-        <button onClick={() => handleAnswer("B")}>Smart Contracts</button>
-        <button onClick={() => handleAnswer("C")}>Gateways</button>
-        <button onClick={() => handleAnswer("D")}>Blockchains</button>
+        <button onClick={() => handleAnswer("A")}>1</button>
+        <button onClick={() => handleAnswer("B")}>2</button>
+        <button onClick={() => handleAnswer("C")}>3</button>
+        <button onClick={() => handleAnswer("D")}>4</button>
       </div>
     )}
     {state.showOptions && (
@@ -208,34 +230,69 @@ return (
         {state.answer === "A" && (
           <div>
             <br />
-            <h5>HAHA LOL </h5>
-            <p>HAHA</p>
+            <h5>заглавное </h5>
+            <p>объяснение</p>
           </div>
         )}
         {state.answer === "B" && (
           <div>
             <br />
-            <P>
-              <H5>KEK LOL </H5>
-            </P>
+            <p>
+              <h5>KEK LOL </h5>
+            </p>
           </div>
         )}
         {state.answer === "C" && (
           <div>
             <br />
-            <P>
-              <H5>ez lol </H5>
-            </P>
+            <p>
+              <h5>ez lol </h5>
+            </p>
           </div>
         )}
         {state.answer === "D" && (
           <div>
             <br />
-            <P>
-              <H5>CONGRATS </H5>
-            </P>
+            <p>
+              <h5>CONGRATS </h5>
+            </p>
           </div>
         )}
+      </div>
+    )}
+    <br />
+    {showQuestion6 && (
+      <div>
+        <h5>Question 6:</h5>
+        <p>Which option do you choose?</p>
+        <button onClick={() => handleOption45("45")}>Option 1</button>
+        <button onClick={() => handleOption56("56")}>Option 2</button>
+      </div>
+    )}
+
+    {/* Explanation for Question 6 */}
+    {state.answer6 === "45" && (
+      <div>
+        <br />
+        <p>
+          <h5>Explanation for Option 1</h5>
+          This is the explanation for Option 1.
+        </p>
+      </div>
+
+      {showContinueButton && (
+  <div>
+    <button onClick={handleContinueButton}>Continue</button>
+  </div>
+    )}
+
+    {state.answer6 === "56" && (
+      <div>
+        <br />
+        <p>
+          <h5>Explanation for Option 2</h5>
+          This is the explanation for Option 2.
+        </p>
       </div>
     )}
     <br />
