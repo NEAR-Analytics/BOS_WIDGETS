2a3315122db1ac826f3033c47d7e70b29e2ba79d
lord1.near <lord1.near>
2023-08-30 14:06:22 +0000

    Update im-human by lord1.near at 2023-08-30T14:06:22.362Z

--- lord1.near/im-human/source_code.jsx
+++ lord1.near/im-human/source_code.jsx
@@ -1,3 +1,226 @@
+const darkColors = {
+  page_bg: "rgb(25,33,50)",
+  header_bg: "rgb(49,62,89)",
+  sideBar: {
+    sideBar_bg: "rgb(49,62,89)",
+    sideBar_color: "#fff",
+  },
+  footer: {
+    titlenelowBackground: "#806ce1",
+    titleBackground: "#fff",
+    fromBackground: "rgb(55,72,107)",
+    toBackground: "rgb(55,72,107)",
+    belowBackground: "rgb(210, 202, 250)",
+  },
+  dynamic_header: {
+    afterbrandcolor: "",
+    color1brand: "#fff",
+    color2brand: "rgb(210, 202, 250)",
+    colordescription: "rgb(210, 202, 250)",
+    background:
+      "radial-gradient(circle, rgb(49,62,89) 0%, rgba(230,230,231,0.01) 0%, rgb(49,62,89) 100%, rgb(49,62,89) 100%, rgb(49,62,89) 100%, rgba(46,52,90,1) 100%);",
+  },
+  search_sbt: {
+    section_bg: "transparent",
+    card_bg: "transparent)",
+    search_btn_bg: "rgb(49,62,89)",
+    search_btn_bg_hover: "rgba(49,62,89,0.8)",
+    search_btn_text: "rgb(255,255,255)",
+    input_bg: "rgb(49,62,89)",
+    input_bg_hover: "rgba(49,62,89,0.8)",
+    input_text_color: "rgb(255,255,255)",
+    input_border: "rgba(49,62,89,0.8)",
+    table_bg: "transparent",
+    table_color: "rgb(255,255,255)",
+    table_border_color: "",
+    table_accent_bg: "",
+    table_striped_color: "rgb(255,255,255)",
+    table_striped_bg: "",
+    table_hover_color: "rgb(255,255,255)",
+    table_hover_bg: "",
+  },
+  sbt_info: {
+    section_bg: "transparent",
+    card_bg: "rgb(49, 62, 89)",
+    card_title_color: "#806ce1",
+    card_content_color: "#fff",
+  },
+  tab_sbt: {
+    backgroundColor: "rgb(49,62,89)",
+    textColor: "#fff",
+    headerColor: "#806ce1",
+    numberColor: "#fff",
+  },
+  sbt_area: {
+    section_bg: "transparent",
+    card_bg: "rgb(49, 62, 89)",
+    card_title_color: "#806ce1",
+  },
+  table_pagination: {
+    table_bg: "rgb(49,62,89)",
+    table_color: "rgb(255,255,255)",
+    table_border_color: "",
+    table_accent_bg: "",
+    table_striped_color: "rgb(255,255,255)",
+    table_striped_bg: "",
+    table_hover_color: "rgb(255,255,255)",
+    table_hover_bg: "",
+    btn_border: "rgb(25,33,50)",
+    btn_bg: "rgb(49,62,89)",
+    btn_bg_active: "rgb(25,33,50)",
+    btn_color: "#fff",
+  },
+  chart: {
+    title: "rgb(255,255,255)",
+    subtitle: "rgba(255,255,255,0.7)",
+    xAxis: "rgb(255,255,255)",
+    yAxis: "rgb(255,255,255)",
+    legend: "rgba(255,255,255,0.7)",
+    legendHover: "rgb(255,255,255)",
+    rangeSelector: {
+      labels: "rgba(255,255,255,0.7)",
+      inputColor: "rgb(255,255,255)",
+      btn_bg: "rgb(49,62,89)",
+      btn_color: "rgb(255,255,255)",
+      btn_hover_bg: "rgba(49,62,89,0.7)",
+      btn_hover_color: "rgba(255,255,255,0.7)",
+      btn_active_bg: "rgb(25,33,50)",
+      btn_active_color: "rgb(255,255,255)",
+    },
+  },
+  spinnerColors: ["#6F61C0", "#241468"],
+  chartColor: [
+    "#F79BD3",
+    "#A084E8",
+    "#EA1179",
+    "#F79BD3",
+    "#A084E8",
+    "#6F61C0",
+    "#241468",
+    "#9F0D7F",
+  ],
+};
+const lightColors = {
+  page_bg: "rgb(241,242,245)",
+  header_bg: "rgb(210, 202, 250)",
+  sideBar: {
+    sideBar_bg: "rgb(210, 202, 250)",
+    sideBar_color: "#fff",
+  },
+  footer: {
+    titlenelowBackground: "#806ce1",
+    titleBackground: "#fff",
+    fromBackground: "rgb(210, 202, 250)",
+    toBackground: "rgb(210, 202, 250)",
+    belowBackground: "#806ce1",
+  },
+  dynamic_header: {
+    afterbrandcolor: "#789efb",
+    color1brand: "#000",
+    color2brand: "#806ce1",
+    colordescription: "#806ce1",
+    background:
+      "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
+  },
+  search_sbt: {
+    section_bg: "rgb(235, 231, 253)",
+    card_bg: "",
+    search_btn_bg: "rgb(210, 202, 250)",
+    search_btn_bg_hover: "rgba(210, 202, 250,0.8)",
+    search_btn_text: "rgb(0,0,0)",
+    input_bg: "rgba(210, 202, 250,0.2)",
+    input_bg_hover: "rgba(210, 202, 250,0.4)",
+    input_text_color: "rgb(0,0,0)",
+    input_border: "rgba(210, 202, 250,0.4)",
+    table_bg: "transparent",
+    table_color: "rgb(0,0,0)",
+    table_border_color: "",
+    table_accent_bg: "",
+    table_striped_color: "rgb(0,0,0)",
+    table_striped_bg: "",
+    table_hover_color: "rgb(0,0,0)",
+    table_hover_bg: "",
+  },
+  sbt_info: {
+    section_bg: "rgb(235, 231, 253)",
+    card_bg: "rgb(255, 255, 255)",
+    card_title_color: "#806ce1",
+    card_content_color: "#000",
+  },
+  tab_sbt: {
+    backgroundColor: "rgb(210, 202, 250)",
+    textColor: "#fff",
+    headerColor: "#806ce1",
+    numberColor: "#fff",
+  },
+  sbt_area: {
+    section_bg: "rgb(235, 231, 253)",
+    card_bg: "rgb(255, 255, 255)",
+    card_title_color: "#806ce1",
+  },
+  table_pagination: {
+    table_bg: "rgb(255,255,255)",
+    table_color: "rgb(0,0,0)",
+    table_border_color: "",
+    table_accent_bg: "",
+    table_striped_color: "rgb(0,0,0)",
+    table_striped_bg: "",
+    table_hover_color: "rgb(0,0,0)",
+    table_hover_bg: "",
+    btn_border: "#000",
+    btn_bg: "#fff",
+    btn_bg_active: "rgb(235, 231, 253)",
+    btn_color: "#000",
+  },
+  chart: {
+    // title: "red",
+    // subtitle: "blue",
+    // xAxis: "red",
+    // yAxis: "blue",
+    // legend: "green",
+    // legendHover: "blue",
+    rangeSelector: {
+      //   labels: "red",
+      //   inputColor: "blue",
+      //   btn_bg: "red",
+      //   btn_color: "blue",
+      //   btn_hover_bg: "red",
+      //   btn_hover_color: "blue",
+      //   btn_active_bg: "red",
+      //   btn_active_color: "blue",
+    },
+  },
+  spinnerColors: ["#6F61C0", "#241468"],
+  chartColor: [
+    "#F79BD3",
+    "#A084E8",
+    "#EA1179",
+    "#F79BD3",
+    "#A084E8",
+    "#6F61C0",
+    "#241468",
+    "#9F0D7F",
+  ],
+};
+
+const tabs = {
+  area: "sbt-area",
+  info: "sbt-info",
+  search: "search-sbt",
+};
+
+State.init({
+  value: "acceleration",
+  light: true,
+  tab: "sbt-area",
+});
+const themeColor = state.light ? lightColors : darkColors;
+const handelLight = () => {
+  let bool = state.light;
+  State.update({ light: !bool });
+};
+const setTab = (tab) => State.update({ tab });
+
 const Container = styled.div`
   &&{text-align:center};
   .tabContent{
@@ -25,82 +248,77 @@ const Container = styled.div`
   }
 `;
 
-const tabs = {
-  area: "sbt-area",
-  info: "sbt-info",
-  search: "search-sbt",
-};
-
-State.init({
-  value: "acceleration",
-  light: true,
-  //   selectedTab="1",
-  tab: "sbt-area",
-});
-const handelLight = () => {
-  let bool = state.light;
-  State.update({ light: !bool });
-  console.log(bool);
-};
-const setTab = (tab) => State.update({ tab });
-
 return (
   <div
     style={{
-      background: `${state.light ? "rgb(241,242,245)" : "rgb(25,33,50)"}`,
+      background: themeColor?.page_bg,
     }}
   >
-    <Widget
-      src="lord1.near/widget/sidebar"
-      props={{
-        headerIcon:
-          "https://yt3.googleusercontent.com/zkArEwljuLKjF7S1rbXoyQWW1VC8QzgVzrFP7KKqOypFtSv0cKgIXbfOBdIFO3ZoBD_wJJUyyw=s900-c-k-c0x00ffffff-no-rj",
-        headerText: "Flipside",
-        footerIcon:
-          "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/house-solid.svg",
-        footerText: "",
-        headerLink: "test",
-        footerLink: "test",
-        links: [
-          {
-            text: "Home",
-            link: "../../lord1.near/widget/home",
-            image:
-              "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/windows.svg",
-          },
-          {
-            text: "Dashboards",
-            link: "../../lord1.near/widget/Flipside-Home-page",
-            image:
-              "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/users-solid.svg",
-          },
-          {
-            text: "Contracts",
-            link: "../../leslug.near/widget/NearContractVisualizerV2",
-            image:
-              "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/people-arrows-solid.svg",
-          },
-          {
-            text: "Im human",
-            link: "../../lord1.near/widget/im-human",
-            image:
-              "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/1309168682.svg",
-          },
-          {
-            text: "Tracker",
-            link: "http://flipsidecrypto.xyz/",
-            image:
-              "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/icons8-home.svg",
-          },
-        ],
-        backgroundColor: `${state.light ? "#d2cafa" : "rgb(49,62,89)"}`,
-        textcolor: "#fff",
-      }}
-    />
+    <div style={{ position: "relative", zIndex: 2 }}>
+      <Widget
+        src="lord1.near/widget/sidebar"
+        props={{
+          headerIcon:
+            "https://yt3.googleusercontent.com/zkArEwljuLKjF7S1rbXoyQWW1VC8QzgVzrFP7KKqOypFtSv0cKgIXbfOBdIFO3ZoBD_wJJUyyw=s900-c-k-c0x00ffffff-no-rj",
+          headerText: "Flipside",
+          footerIcon:
+            "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/house-solid.svg",
+          footerText: "",
+          headerLink: "test",
+          footerLink: "test",
+          links: [
+            {
+              text: "Home",
+              link: "home",
+              image:
+                "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/windows.svg",
+            },
+            {
+              text: "Dashboards",
+              link: "Flipside-Home-page",
+              image:
+                "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/users-solid.svg",
+            },
+            {
+              text: "Contracts",
+              link: "../../leslug.near/widget/NearContractVisualizerV2",
+              image:
+                "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/people-arrows-solid.svg",
+            },
+            {
+              text: "Network",
+              link: "network",
+              image:
+                "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/svgexport-8.svg",
+            },
+            {
+              text: "Im human",
+              link: "im-human",
+              image:
+                "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/1309168682.svg",
+            },
+            {
+              text: "Near social",
+              link: "near-social",
+              image:
+                "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/svgexport-9.svg",
+            },
+            {
+              text: "Tracker",
+              link: "http://flipsidecrypto.xyz/",
+              image:
+                "https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/icons8-home.svg",
+            },
+          ],
+          backgroundColor: themeColor?.sideBar?.sideBar_bg,
+          textcolor: themeColor?.sideBar?.sideBar_color,
+        }}
+      />
+    </div>
     <div style={{ marginLeft: "6rem" }}>
       <div
         style={{
-          backgroundColor: `${state.light ? "#d2cafa" : "rgb(49,62,89)"}`,
+          backgroundColor: themeColor?.header_bg,
           borderRadius: "15px",
           display: "flex",
           justifyContent: "space-between",
@@ -114,7 +332,7 @@ return (
             text: "Broaden your horizon with Flipside",
             fontFamily: "Arial",
             fontSize: "20px",
-            backgroundColor: `${state.light ? "#d2cafa" : "rgb(49,62,89)"}`,
+            backgroundColor: themeColor?.header_bg,
             height: "60px",
             width: "100%",
             textColor: "white",
@@ -127,7 +345,13 @@ return (
           src="https://raw.githubusercontent.com/lordking1234/blockchain-icon/main/lightmode.svg"
         />
       </div>
-      <div>
+      <div
+        style={{
+          background: themeColor.page_bg,
+          zIndex: 1,
+        }}
+        className="sticky-top p-4"
+      >
         <div>
           <Container>
             <ul className="tabContent">
@@ -162,15 +386,26 @@ return (
             </ul>
           </Container>
         </div>
+      </div>
+      <div>
         <div className="content">
           {state.tab === "sbt-area" && (
-            <Widget src="lord1.near/widget/sbt-area" />
+            <Widget
+              src="lord1.near/widget/sbt-area"
+              props={{ themeColor: themeColor }}
+            />
           )}
           {state.tab === "sbt-info" && (
-            <Widget src="lord1.near/widget/sbt-info" />
+            <Widget
+              src="lord1.near/widget/sbt-info"
+              props={{ themeColor: themeColor }}
+            />
           )}
           {state.tab === "search-sbt" && (
-            <Widget src="lord1.near/widget/search-sbt" />
+            <Widget
+              src="lord1.near/widget/search-sbt"
+              props={{ themeColor: themeColor }}
+            />
           )}
         </div>
       </div>
@@ -179,12 +414,12 @@ return (
         src="lord1.near/widget/footer"
         props={{
           beBackground: "#fff",
-          titlenelowBackground: `${state.light ? "#806ce1" : "#806ce1"}`,
-          titleBackground: `${state.light ? "#fff" : "#fff"}`,
+          titlenelowBackground: themeColor?.footer?.titlenelowBackground,
+          titleBackground: themeColor?.footer?.titleBackground,
           svgBackground: "#806ce1",
-          fromBackground: `${state.light ? "#d2cafa" : "rgb(55,72,107)"}`,
-          toBackground: `${state.light ? "#d2cafa" : "rgb(55,72,107)"}`,
-          belowBackground: `${state.light ? "#806ce1" : "#d2cafa"}`,
+          fromBackground: themeColor?.footer?.fromBackground,
+          toBackground: themeColor?.footer?.toBackground,
+          belowBackground: themeColor?.footer?.belowBackground,
         }}
       />
     </div>
