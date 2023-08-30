0327afa7b4835a9460ca8606c04342958491cb7f
hack.near <hack.near>
2023-08-30 20:47:14 +0000

    Update Notification.Item by hack.near at 2023-08-30T20:47:14.704Z

--- hack.near/notification.item/source_code.jsx
+++ hack.near/notification.item/source_code.jsx
@@ -1,25 +1,37 @@
-const { accountId, blockHeight, value } = props;
+const { value } = props;
+
+const loading = <div className="placeholder" style={{ height: "48px" }} />;
+
+const widgetSrc =
+  value.type === "follow" || value.type === "unfollow"
+    ? "mob.near/widget/Notification.Item.Follow"
+    : value.type === "poke"
+    ? "mob.near/widget/Notification.Item.Poke"
+    : value.type === "like"
+    ? "mob.near/widget/Notification.Item.Like"
+    : value.type === "comment"
+    ? "mob.near/widget/Notification.Item.Comment"
+    : value.type && value.type?.startsWith("devgovgigs/")
+    ? "mob.near/widget/Notification.Item.DevGov"
+    : value.type === "mention"
+    ? "mob.near/widget/Notification.Item.Mention"
+    : value.type === "repost"
+    ? "mob.near/widget/Notification.Item.Repost"
+    : value.type === "chess-game"
+    ? "chess-game.near/widget/Notification.Item.ChessGame@98857466"
+    : value.type === "request"
+    ? "hack.near/widget/Notification.Item.Request"
+    : null;
 
 return (
-  <div className="d-flex justify-content-between row">
-    <div className="me-2 text-truncate col-auto">
-      <div className="text-truncate row">
-        <div className="text-truncate col-auto">
-          <Widget
-            src="mob.near/widget/Profile"
-            props={{ accountId, tooltip: true }}
-          />
-        </div>
-        <div className="text-truncate col-auto">{props.R}</div>
+  <div className="mb-3">
+    {widgetSrc ? (
+      <Widget loading={loading} src={widgetSrc} props={{ loading, ...props }} />
+    ) : (
+      <div>
+        Unknown notification:{" "}
+        <span className="font-monospace">{JSON.stringify(value)}</span>
       </div>
-    </div>
-    <div className="text-nowrap col-auto">
-      <div className="text-truncate text-muted">
-        {" "}
-        {props.L}
-        <Widget src="mob.near/widget/TimeAgo" props={{ blockHeight }} />
-        ago
-      </div>
-    </div>
+    )}
   </div>
 );
