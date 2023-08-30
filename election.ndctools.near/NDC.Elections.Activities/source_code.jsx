2a6f89693556947fb93211e424e2dbd1d00df8c9
election.ndctools.near <election.ndctools.near>
2023-08-30 21:00:03 +0000

    Update NDC.Elections.Activities by election.ndctools.near at 2023-08-30T21:00:03.885Z

--- election.ndctools.near/NDC.Elections.Activities/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Activities/source_code.jsx
@@ -37,56 +37,58 @@ const widgets = {
 
 return (
   <List>
-    {myVotes.map((vote) => (
-      <VoteRow
-        href={`https://explorer.mainnet.near.org/transactions/${vote.transaction_id}`}
-        className="d-flex justify-content-between align-items-center"
-      >
-        <div className="d-flex align-items-center">
-          {vote.house !== "SetupPackage" ? (
-            <Widget
-              src="mob.near/widget/ProfileImage"
-              props={{
-                accountId: vote.candidate,
-                imageClassName: "rounded-circle w-100",
-                style: { width: "32px", height: "32px", marginRight: 8 },
-              }}
-            />
-          ) : (
-            <BPImg
-              className="rounded-circle"
-              src="https://bafkreidsg3gntb4grebr6rpvffhzkwdt2siel7ucl3hpsj5i7qqu426dgq.ipfs.nftstorage.link"
-            />
-          )}
-          <div className="row">
-            <StyledLink
-              href={`https://near.org/near/widget/ProfilePage?accountId=${vote.candidate}`}
-            >
-              {vote.candidate}
-            </StyledLink>
-            <small className="text-secondary">
-              {new Date(vote.timestamp).toLocaleDateString("en-US", {
-                day: "2-digit",
-                month: "short",
-              })}
-              &middot;
-              {new Date(vote.timestamp).toLocaleTimeString("en-US", {
-                hour: "2-digit",
-              })}
-            </small>
+    {myVotes
+      .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp))
+      .map((vote) => (
+        <VoteRow
+          href={`https://explorer.mainnet.near.org/transactions/${vote.transaction_id}`}
+          className="d-flex justify-content-between align-items-center"
+        >
+          <div className="d-flex align-items-center">
+            {vote.house !== "SetupPackage" ? (
+              <Widget
+                src="mob.near/widget/ProfileImage"
+                props={{
+                  accountId: vote.candidate,
+                  imageClassName: "rounded-circle w-100",
+                  style: { width: "32px", height: "32px", marginRight: 8 },
+                }}
+              />
+            ) : (
+              <BPImg
+                className="rounded-circle"
+                src="https://bafkreidsg3gntb4grebr6rpvffhzkwdt2siel7ucl3hpsj5i7qqu426dgq.ipfs.nftstorage.link"
+              />
+            )}
+            <div className="row">
+              <StyledLink
+                href={`https://near.org/near/widget/ProfilePage?accountId=${vote.candidate}`}
+              >
+                {vote.candidate}
+              </StyledLink>
+              <small className="text-secondary">
+                {new Date(vote.timestamp).toLocaleDateString("en-US", {
+                  day: "2-digit",
+                  month: "short",
+                })}
+                &middot;
+                {new Date(vote.timestamp).toLocaleTimeString("en-US", {
+                  hour: "2-digit",
+                })}
+              </small>
+            </div>
           </div>
-        </div>
-        <Widget
-          src={widgets.styledComponents}
-          props={{
-            Tag: {
-              title:
-                vote.house === "SetupPackage" ? "BudgetPackage" : vote.house,
-              className: "dark",
-            },
-          }}
-        />
-      </VoteRow>
-    ))}
+          <Widget
+            src={widgets.styledComponents}
+            props={{
+              Tag: {
+                title:
+                  vote.house === "SetupPackage" ? "BudgetPackage" : vote.house,
+                className: "dark",
+              },
+            }}
+          />
+        </VoteRow>
+      ))}
   </List>
 );
