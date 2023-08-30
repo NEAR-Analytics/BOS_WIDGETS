2636ae6e3ccc004175662224d3b8d9e9688ed086
dapplets.near <dapplets.near>
2023-08-30 17:38:47 +0000

    Update LevelThree by dapplets.near at 2023-08-30T17:38:47.519Z

---
+++ dapplets.near/LevelThree/source_code.jsx
@@ -0,0 +1,23 @@
+const Wrapper = styled.div`
+    display: flex;
+    padding: 16px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 16px;
+    align-self: stretch;
+
+    border-radius: 4px;
+    background: #DBDBFF;
+`;
+
+const Label = styled.div`
+    color: #5748FF;
+    font-size: 14px;
+    font-weight: 400;
+`;
+
+return (
+  <Wrapper>
+    <Label>dapplets.near/widget/LevelThree</Label>
+  </Wrapper>
+);
