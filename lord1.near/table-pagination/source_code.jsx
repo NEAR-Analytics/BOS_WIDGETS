f298803b07219866f13870489b823acd7a4c5370
lord1.near <lord1.near>
2023-08-30 12:36:24 +0000

    Update table-pagination by lord1.near at 2023-08-30T12:36:24.933Z

--- lord1.near/table-pagination/source_code.jsx
+++ lord1.near/table-pagination/source_code.jsx
@@ -4,6 +4,7 @@ if (!props.data || !props.columns) {
 State.init({ currentPage: 1 });
 const data = props.data || [];
 const rowsCount = props.rowsCount || null;
+const themeColor = props.themeColor;
 
 const handlePagination = () => {
   if (!rowsCount) return { table: data };
@@ -17,10 +18,19 @@ const handlePagination = () => {
   buttons[currentPage - 1] = 1;
   return { buttons, table: currentTableData };
 };
-
+const Table = styled.table`
+  --bs-table-color: ${themeColor?.table_pagination?.table_color};
+  --bs-table-bg: ${themeColor?.table_pagination?.table_bg};
+  --bs-table-border-color: ${themeColor?.table_pagination?.table_border_color};
+  --bs-table-accent-bg: ${themeColor?.table_pagination?.table_accent_bg};
+  --bs-table-striped-color: ${themeColor?.table_pagination?.table_striped_color};
+  --bs-table-striped-bg: ${themeColor?.table_pagination?.table_striped_bg};
+  --bs-table-hover-color: ${themeColor?.table_pagination?.table_hover_color};
+  --bs-table-hover-bg:${themeColor?.table_pagination?.table_hover_bg}
+`;
 return (
-  <div className=" table-responsive">
-    <table
+  <div className="table-responsive">
+    <Table
       className={`table table-hover table-striped table-borderless ${props.className}`}
     >
       <thead>
@@ -45,7 +55,7 @@ return (
             );
           })}
       </tbody>
-    </table>
+    </Table>
     {!props.rowsCount ? (
       ""
     ) : (
@@ -58,9 +68,18 @@ return (
                   <li key={i} className="page-item">
                     <button
                       onClick={() => State.update({ currentPage: i + 1 })}
-                      className={`page-link btn ${
-                        btn ? "text-bg-secondary" : "text-bg-light"
-                      } border-dark`}
+                      className="page-link btn"
+                      style={{
+                        borderColor:
+                          themeColor?.table_pagination?.btn_border ?? "#000",
+                        color:
+                          themeColor?.table_pagination?.btn_color ?? "#000",
+                        backgroundColor: btn
+                          ? themeColor?.table_pagination?.btn_bg_active ??
+                            "gray"
+                          : themeColor?.table_pagination?.btn_bg ??
+                            "transparent",
+                      }}
                     >
                       {i + 1}
                     </button>
@@ -82,4 +101,20 @@ return (
 //   ],
 //   rowsCount: 2, // if zero or null , the whole table will be render
 //   className: "table-bordered",
+//   themeColor: {
+//     table_pagination: {
+//       table_bg: "rgb(25,33,50)",
+//       table_color: "rgb(255,255,255)",
+//       table_border_color: "",
+//       table_accent_bg: "",
+//       table_striped_color: "rgb(255,255,255)",
+//       table_striped_bg: "",
+//       table_hover_color: "rgb(255,255,255)",
+//       table_hover_bg: "",
+//       btn_border: "#fff",
+//       btn_bg: "rgba(49, 62, 89,0.5)",
+//       btn_bg_active: "rgb(25,33,50)",
+//       btn_color: "#fff",
+//     },
+//   },
 // };
