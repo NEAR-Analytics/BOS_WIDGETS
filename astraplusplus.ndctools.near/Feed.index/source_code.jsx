e4a3bf2491f639573d72af66fbfef019fe00dcbc
astraplusplus.ndctools.near <astraplusplus.ndctools.near>
2023-08-30 08:30:33 +0000

    Update Feed.index by astraplusplus.ndctools.near at 2023-08-30T08:30:33.108Z

--- astraplusplus.ndctools.near/Feed.index/source_code.jsx
+++ astraplusplus.ndctools.near/Feed.index/source_code.jsx
@@ -6,7 +6,7 @@ const getFollowedDAOs = (accountId) => {
   following =
     following === null
       ? null
-      : Object.keys(following[accountId].graph.follow).filter((account) =>
+      : Object.keys(following[accountId].graph.follow || {}).filter((account) =>
           account.endsWith(".sputnik-dao.near"),
         );
   return following;
