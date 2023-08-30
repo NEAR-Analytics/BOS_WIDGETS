4d03e890afea0621a7f95c25898f61b6e15dff5f
jgodwill.near <jgodwill.near>
2023-08-30 18:55:01 +0000

    Update GenaDrop.Home.PartnersCarousel by jgodwill.near at 2023-08-30T18:55:01.650Z

--- jgodwill.near/GenaDrop.Home.PartnersCarousel/source_code.jsx
+++ jgodwill.near/GenaDrop.Home.PartnersCarousel/source_code.jsx
@@ -81,7 +81,7 @@ const Partners = styled.div`
   display: -ms-flexbox;
   display: flex;
   height: 100%;
-  width: 100%;
+  width: 2526px;
   margin: 0 !important;
   padding: 0 !important;
    animation: moveSlideshow 60s linear infinite;
