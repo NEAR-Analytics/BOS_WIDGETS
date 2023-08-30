fc9ccc19fc537fc5861912e4b15b6e2bf8f80b01
y3k.near <y3k.near>
2023-08-30 15:33:08 +0000

    Update near_atlas.components.resources_page by y3k.near at 2023-08-30T15:33:08.908Z

--- y3k.near/near_atlas.components.resources_page/source_code.jsx
+++ y3k.near/near_atlas.components.resources_page/source_code.jsx
@@ -13,31 +13,33 @@ const cards = [
   {
     imageSrc: "https://picsum.photos/seed/picsum/200/300",
     imageAlt: "Your Alt Text",
-    title: "Your Card Title",
-    subtitle: "Your Card Subtitle",
+    title: "Artemis",
+    subtitle: "Artemis for data comparing NEAR with other chains",
     bgColor: "bg-yellow-500",
-    items: ["Item 1", "Item 2", "Item 3"],
+    href: "https://app.artemis.xyz/overview",
+    items: [],
   },
 
   {
     imageSrc: "https://picsum.photos/seed/picsum/200/300",
     imageAlt: "Your Alt Text",
-    title: "Your Card Title",
-    subtitle: "Your Card Subtitle",
+    title: "Tie Terminal",
+    subtitle: "The Tie Terminal for data on price",
     bgColor: "bg-yellow-500",
-    items: ["Item 1", "Item 2", "Item 3"],
+    href: "https://www.thetie.io/",
+    items: [],
   },
 
   {
     imageSrc: "https://picsum.photos/seed/picsum/200/300",
     imageAlt: "Your Alt Text",
-    title: "Your Card Title",
-    subtitle: "Your Card Subtitle",
+    title: "Flipside",
+    subtitle: "Flipside NEAR dashboard for a snapshot of on-chain activity",
     bgColor: "bg-yellow-500",
-    items: ["Item 1", "Item 2", "Item 3"],
+    href: "https://flipsidecrypto.xyz/MoDeFi/near-dashboard-fSCUR1",
+    items: [],
   },
 ];
-
 return (
   <div className="bg-gray-900 text-white">
     <div className="flex flex-col py-4 px-12">
@@ -47,16 +49,9 @@ return (
           <div className="text-xl font-light">{mainSubtitle}</div>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-20 md:px-10">
+      <div className="flex flex-col gap-6 px-20 md:px-10">
         {cards.map((card, index) => (
           <div key={index}>
-            {/*
-              <img
-                src={card.imageSrc}
-                alt={card.imageAlt}
-                className="rounded-t-xl"
-              />
-            */}
             <div className={`px-9 pt-10 pb-14 ${card.bgColor} rounded-b-lg`}>
               <div className="text-white space-y-4">
                 <h3 className="text-xl font-bold">{card.title}</h3>
@@ -73,7 +68,8 @@ return (
                 </ul>
                 <div className="flex flex-col justify-end">
                   <a
-                    href="#"
+                    taget="_blank"
+                    href={card.href}
                     className="py-3 px-6 bg-white text-black rounded-full"
                   >
                     Learn More
