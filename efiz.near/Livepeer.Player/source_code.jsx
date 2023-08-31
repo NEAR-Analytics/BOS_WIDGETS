52d23200fc4eb3774cd3d6614891ffa0af08595c
efiz.near <efiz.near>
2023-08-31 19:12:02 +0000

    Update Livepeer.Player by efiz.near at 2023-08-31T19:12:02.897Z

--- efiz.near/Livepeer.Player/source_code.jsx
+++ efiz.near/Livepeer.Player/source_code.jsx
@@ -1,8 +1,26 @@
-const data = props.data;
-const assetId = "2f1b6b13-e95c-4859-b51e-d74b0e7545ba";
+/**
+ * Livepeer Player
+ *
+ * Widget for playing a video from Livepeer
+ * (https://docs.livepeer.org/reference/livepeer-js/player/usePlayer)
+ * 
+ * Props:
+ * @prop {String} title - the title of the video
+ * @prop {String} playbackId - the playbackId of the video
+ * @prop {Image} PosterImage - The image object for the poster image.
+ * @prop {Boolean} showPipButton - whether to show the picture-in-picture button
+ * @prop {String} objectFit - the object fit for the video
+ * @prop {Boolean} priority - whether to prioritize the video
+ */
 
 return (
-  <>
-    <LivepeerPlayer assetId={"2f1b6b13-e95c-4859-b51e-d74b0e7545ba"} />
-  </>
+  <LivepeerPlayer
+    title={props.title}
+    playbackId={props.playbackId}
+    poster={props.PosterImage || <></>}
+    showPipButton={props.showPipButton && true}
+    objectFit={props.objectFit || "cover"}
+    priority={props.priority && true}
+    {...props}
+  />
 );
