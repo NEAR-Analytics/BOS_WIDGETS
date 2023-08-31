dfad742d24bfc4eea2441c4f36310067e6e84d9f
58423e7fe4bbc6956ea637d23cbeedee8ec23873fcd93bafb69086af625563e9 <58423e7fe4bbc6956ea637d23cbeedee8ec23873fcd93bafb69086af625563e9>
2023-08-31 11:56:01 +0000

    Update GreeterToday by 58423e7fe4bbc6956ea637d23cbeedee8ec23873fcd93bafb69086af625563e9 at 2023-08-31T11:56:01.298Z

--- 58423e7fe4bbc6956ea637d23cbeedee8ec23873fcd93bafb69086af625563e9/GreeterToday/source_code.jsx
+++ 58423e7fe4bbc6956ea637d23cbeedee8ec23873fcd93bafb69086af625563e9/GreeterToday/source_code.jsx
@@ -3,7 +3,7 @@ return (
     <div class="container border border-info p-3 text-center">
       <h1>Hello {props.name}</h1>
 
-      <p> Today is {props.date} </p>
+      <p> Today is {props.date}</p>
     </div>
   </>
 );
