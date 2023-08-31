f2d76ed0a51367303f83a1673f17550893efd1aa
andyh.near <andyh.near>
2023-08-31 18:43:38 +0000

    Update ProfileOnboarding by andyh.near at 2023-08-31T18:43:38.043Z

--- andyh.near/ProfileOnboarding/source_code.jsx
+++ andyh.near/ProfileOnboarding/source_code.jsx
@@ -57,7 +57,7 @@ if (
 }
 
 const dismissed = Storage.get("dismissed");
-const defaultHomepage = "mob.near/widget/N";
+const defaultHomepage = "andyh.near/widget/N";
 const homepage = Social.get(`${accountId}/settings/near.social/homepage`);
 
 if (homepage === null) {
