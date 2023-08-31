53da79832bad0648e62f04072891f03d4b7f0caa
election.ndctools.near <election.ndctools.near>
2023-08-31 13:53:59 +0000

    Update NDC.Elections.Candidates by election.ndctools.near at 2023-08-31T13:53:59.150Z

--- election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
@@ -447,7 +447,7 @@ function fetchGraphQL(series) {
       query: `
         query MyQuery {
           nft_tokens(
-            where: {nft_contract_id: {_eq: "mint.sharddog.near"}, token_id: {_regex: "^${series}:"}, owner: {_eq: "orangejoe.near"}}
+            where: {nft_contract_id: {_eq: "mint.sharddog.near"}, token_id: {_regex: "^${series}:"}, owner: {_eq: "${context.accountId}"}}
             order_by: {minted_timestamp: asc}
           ) {
             last_transfer_timestamp
@@ -459,13 +459,13 @@ function fetchGraphQL(series) {
     }),
   });
 }
-console.log(state);
+
 const processNFTAvailability = (result, key) => {
   if (result.status === 200) {
     let data = result.body.data;
     if (data) {
       const tokens = data.nft_tokens;
-      console.log(key, tokens);
+      console.log(tokens);
       State.update({
         [key]: tokens.length > 0 && tokens[0].last_transfer_timestamp === null,
       });
@@ -510,15 +510,15 @@ State.init({
   winnerIds: [],
 });
 
-fetchGraphQL(NFT_SERIES[0]).then((result) =>
-  processNFTAvailability(result, "hasPolicyNFT")
-);
-
-fetchGraphQL(NFT_SERIES[1]).then((result) =>
-  processNFTAvailability(result, "hasIVotedNFT")
-);
-
 if (state.reload) {
+  fetchGraphQL(NFT_SERIES[0]).then((result) =>
+    processNFTAvailability(result, "hasPolicyNFT")
+  );
+
+  fetchGraphQL(NFT_SERIES[1]).then((result) =>
+    processNFTAvailability(result, "hasIVotedNFT")
+  );
+
   const electionStatus = Near.view(electionContract, "proposal_status", {
     prop_id: props.id,
   });
