94e10d73d6138e2415e27231bda909a9958460ba
election.ndctools.near <election.ndctools.near>
2023-08-31 12:03:14 +0000

    Update NDC.Elections.Activities by election.ndctools.near at 2023-08-31T12:03:14.100Z

--- election.ndctools.near/NDC.Elections.Activities/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Activities/source_code.jsx
@@ -51,21 +51,23 @@ return (
           className="d-flex justify-content-between align-items-center"
         >
           <div className="d-flex align-items-center w-50">
-            {vote.house !== "SetupPackage" ? (
-              <Widget
-                src="mob.near/widget/ProfileImage"
-                props={{
-                  accountId: vote.candidate,
-                  imageClassName: "rounded-circle w-100 mh-100",
-                  style: { width: "32px", height: "32px", marginRight: 8 },
-                }}
-              />
-            ) : (
-              <BPImg
-                className="rounded-circle"
-                src="https://bafkreidsg3gntb4grebr6rpvffhzkwdt2siel7ucl3hpsj5i7qqu426dgq.ipfs.nftstorage.link"
-              />
-            )}
+            <div>
+              {vote.house !== "SetupPackage" ? (
+                <Widget
+                  src="mob.near/widget/ProfileImage"
+                  props={{
+                    accountId: vote.candidate,
+                    imageClassName: "rounded-circle w-100 mh-100",
+                    style: { width: "32px", height: "32px", marginRight: 8 },
+                  }}
+                />
+              ) : (
+                <BPImg
+                  className="rounded-circle"
+                  src="https://bafkreidsg3gntb4grebr6rpvffhzkwdt2siel7ucl3hpsj5i7qqu426dgq.ipfs.nftstorage.link"
+                />
+              )}
+            </div>
             <div className="row">
               <StyledLink
                 href={`https://near.org/near/widget/ProfilePage?accountId=${vote.candidate}`}
