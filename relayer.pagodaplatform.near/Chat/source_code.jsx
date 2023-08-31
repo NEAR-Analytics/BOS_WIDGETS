9c174d1774e84d4e4677deaee979467ffa2a2ef6
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-08-31 10:23:33 +0000

    Update Chat by relayer.pagodaplatform.near at 2023-08-31T10:23:33.992Z

--- relayer.pagodaplatform.near/Chat/source_code.jsx
+++ relayer.pagodaplatform.near/Chat/source_code.jsx
@@ -5,7 +5,7 @@ State.init({
   message: "",
 });
 
-const messages = Near.view(contract, "get_messages", { limit: 100 });
+const messages = Near.view(contract, "get_messages", { limit: 3 });
 
 const Message = styled.div`
   display: flex;
@@ -28,6 +28,16 @@ const sendMessage = () => {
 
 return (
   <>
+    <pre>
+      {JSON.stringify(
+        Social.index("post", "main", {
+          accountId: "nectar.near",
+        }),
+        null,
+        2
+      )}
+    </pre>
+    <pre>{JSON.stringify(Social.get(`mob.near/widget/**`), null, 2)}</pre>
     {messages.reverse().map((message) => (
       <Message>
         <Widget
