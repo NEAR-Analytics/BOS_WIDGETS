64530b0df530f218e9818e1fea8faf82cf0670a5
jgodwill.near <jgodwill.near>
2023-09-01 05:31:59 +0000

    Update GenaDrop.Partner by jgodwill.near at 2023-09-01T05:31:59.302Z

---
+++ jgodwill.near/GenaDrop.Partner/source_code.jsx
@@ -0,0 +1,13 @@
+const url = "https://docs.google.com/forms/d/e/1FAIpQLSexslat5Sca-I0M0Tmrq0HFNo8Z4JXDKfeMUIJ7kcaWoQvzBA/formResponse?embedded=true";
+return (
+  <>
+    <iframe
+      src={url}
+      width="640"
+      height="959"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+    />
+  </>
+);
