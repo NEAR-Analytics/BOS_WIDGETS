47cc8599cbb2531e7778d5b08d0a95150dea9711
jgodwill.near <jgodwill.near>
2023-09-01 05:35:37 +0000

    Update GenaDrop.Partner by jgodwill.near at 2023-09-01T05:35:37.754Z

--- jgodwill.near/GenaDrop.Partner/source_code.jsx
+++ jgodwill.near/GenaDrop.Partner/source_code.jsx
@@ -3,11 +3,7 @@ return (
   <>
     <iframe
       src={url}
-      width="640"
-      height="959"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
+      style={{width: "100%", height: "100%"}}
     />
   </>
 );
