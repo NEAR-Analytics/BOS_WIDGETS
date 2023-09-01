17e34ab5f2df6f13a192da65857a530c25586ad8
hack.near <hack.near>
2023-09-01 22:35:00 +0000

    Update NDC.WG.Data by hack.near at 2023-09-01T22:35:00.618Z

--- hack.near/NDC.WG.Data/source_code.jsx
+++ hack.near/NDC.WG.Data/source_code.jsx
@@ -1,125 +1,98 @@
-const groups = {
-  RC: {
-    title: "Regional Communities",
-    creatorId: "rc-dao.near",
-    members: {
-      "abbakaka.near": "",
-      "igboze_builder.near": "",
-      "inspiratibiz.near": "",
-      "james.near": "",
+const daoId = props.daoId ?? "hack.near";
+
+const groups = Social.get(`${daoId}/directory/**`);
+
+const directory = Social.get(`${daoId}/thing/directory`);
+
+function generateUID() {
+  return (
+    Math.random().toString(16).slice(2) +
+    Date.now().toString(36) +
+    Math.random().toString(16).slice(2)
+  );
+}
+
+const groupId = props.groupId ?? generateUID();
+
+State.init({
+  directory,
+  groupTitle: "",
+  groupCreator: "",
+  groupMembers: [],
+  groupAccount: "",
+});
+
+const newGroup = {
+  groupId,
+  title: state.groupTitle,
+  creatorId: state.groupCreator,
+  members: [state.groupMembers],
+  daoId: state.groupAccount,
+};
+
+function addGroup(newGroupId) {
+  State.update({
+    groups: { ...state.directory, newGroupId },
+  });
+}
+
+const handleCreate = () => {
+  const groupData = {
+    thing: {
+      [groupId]: newGroup,
     },
-    daoId: "rc-dao.sputnik-dao.near",
-  },
-  NFT: {
-    title: "Non-Fungible Tokens",
-    creatorId: "nearnftwg.near",
-    members: {
-      "aescobar.near": "",
-      "krikkraktrak.near": "",
-      "nearversedao.near": "",
-    },
-    daoId: "nearnftwg.sputnik-dao.near",
-  },
-  DeFi: {
-    title: "Decentralized Finance",
-    creatorId: "defindc.near",
-    groupId: "DeFi",
-    members: { "ntare.near": "" },
-    daoId: "defi-ndc.sputnik-dao.near",
-  },
-  AI: {
-    title: "Artificial Intelligence",
-    creatorId: "ai-dao.near",
-    members: { "damboy22.near": "" },
-    daoId: "ai-dao.sputnik-dao.near",
-  },
-  Gaming: {
-    title: "Gaming",
-    creatorId: "haenko.near",
-    members: { "haenko.near": "", "dazo_gaming.near": "", "jeffgold.near": "" },
-  },
-  Events: {
-    title: "Events",
-    creatorId: "neardigitalcollective.near",
-    members: { "ogruss.near": "" },
-  },
-  FDAO: {
-    title: "Freelancers",
-    creatorId: "freelancerdao.near",
-    members: {
-      "blaze.near": "",
-      "fiftycent.near": "",
-      "atrox1382.near": "",
-      "robert.near": "",
-      "kazanderdad.near": "",
-    },
-    daoId: "freelancerdao.sputnik-dao.near",
-  },
-  NRC: {
-    title: "Research",
-    creatorId: "research-collective.near",
-    members: {
-      "chloe.near": "",
-      "earnestetim.near": "",
-      "xvii.near": "",
-    },
-    daoId: "research-collective.sputnik-dao.near",
-  },
-  ReFi: {
-    title: "Regenerative Finance",
-    creatorId: "nearefi.near",
-    members: {
-      "earnestetim.near": "",
-      "liight.near": "",
-      "trophy001.near": "",
-      "skyempire.near": "",
-      "ndcplug.near": "",
-      "ntare.near": "",
-      "wolfwood.near": "",
-    },
-    daoId: "refi.sputnik-dao.near",
-  },
-  Aurora: {
-    title: "Aurora",
-    creatorId: "ac-dao.near",
-    members: { "whendacha.near": "", "techdir.near": "", "johanga.near": "" },
-    daoId: "aurora-community-dao.sputnik-dao.near",
-  },
-  Tech: {
-    title: "Technology",
-    creatorId: "neardigitalcollective.near",
-    members: {
-      "robert.near": "",
-      "jlw.near": "",
-      "blaze.near": "",
-    },
-    daoId: "ndc-techwg.sputnik-dao.near",
-  },
-  GWG: {
-    title: "Governance",
-    creatorId: "govworkinggroup.near",
-    members: {
-      "yuensid.near": "",
-      "fiftycent.near": "",
-      "atrox1382.near": "",
-      "robert.near": "",
-      "kazanderdad.near": "",
-    },
-    daoId: "gwg.sputnik-dao.near",
-  },
+  };
+
+  Social.set(groupData);
 };
 
 const handleSave = () => {
-  const data = {
-    directory: groups,
+  const groupData = {
+    thing: {
+      directory: { ...directory, newGroupId },
+    },
   };
 
-  Social.set(data);
+  Social.set(groupData);
 };
 
 return (
-  <>
-    <p>{JSON.stringify(groups)}</p>
+  <div className="m-2">
+    <input
+      placeholder="Group Title"
+      onChange={(e) => State.update({ groupTitle: e.target.value })}
+    />
+    <br />
+    <input
+      placeholder="Group Creator"
+      onChange={(e) => State.update({ groupCreator: e.target.value })}
+    />
+    <br />
+    <input
+      placeholder="Group Members"
+      onChange={(e) => State.update({ groupMembers: e.target.value })}
+    />
+    <br />
+    <input
+      placeholder="DAO"
+      onChange={(e) => State.update({ groupAccount: e.target.value })}
+    />
+    <br />
+    <p>Group ID: {newGroup.groupId}</p>
+    <p>Group Creator: {state.groupCreator}</p>
+    <p>Group Members: {state.groupMembers}</p>
+    <p>DAO: {state.groupAccount}</p>
+    <button onClick={handleCreate}>Create</button>
+    <hr />
+    <input
+      placeholder={newGroup.groupId}
+      onChange={(e) => State.update({ newGroupId: e.target.value })}
+    />
+    <br />
+    <p style={{ wordWrap: "break-word", maxWidth: "100%" }}>
+      {state.directory}
+    </p>{" "}
     <button onClick={handleSave}>Save</button>
-  </>
+    <button onClick={addGroup}>Add</button>
+  </div>
 );
