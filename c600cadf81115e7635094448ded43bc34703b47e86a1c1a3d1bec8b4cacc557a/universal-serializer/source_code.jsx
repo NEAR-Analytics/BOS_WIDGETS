bab5a2b771acc9816ba24afafb03481176ac507e
c600cadf81115e7635094448ded43bc34703b47e86a1c1a3d1bec8b4cacc557a <c600cadf81115e7635094448ded43bc34703b47e86a1c1a3d1bec8b4cacc557a>
2023-09-01 07:41:22 +0000

    Update universal-serializer by c600cadf81115e7635094448ded43bc34703b47e86a1c1a3d1bec8b4cacc557a at 2023-09-01T07:41:22.085Z

---
+++ c600cadf81115e7635094448ded43bc34703b47e86a1c1a3d1bec8b4cacc557a/universal-serializer/source_code.jsx
@@ -0,0 +1,34 @@
+const onChange = ({ target }) => {
+  State.update({ json: target.value });
+};
+
+const onClick = () => {
+  asyncFetch("http://localhost:3001/near", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(JSON.parse(state.json)),
+  })
+    .then((res) => {
+      State.update({ result: res.body.serializedTx });
+    })
+    .catch(() => {
+      State.update({ result: "errors" });
+    });
+};
+
+return (
+  <>
+    <div class="container">
+      <p>
+        <b> Input Tx Info (JSON): </b>
+      </p>
+      <textarea rows="4" cols="50" onChange={onChange} />
+      <button id="clickButton" onClick={onClick}>
+        Send
+      </button>
+      <p>
+        <b> Serialized Tx: </b> {state.result}{" "}
+      </p>
+    </div>
+  </>
+);
