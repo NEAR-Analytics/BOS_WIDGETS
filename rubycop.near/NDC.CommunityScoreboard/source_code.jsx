abf26bbe4c5582b4b27ddf8d791b01f922c7deff
rubycop.near <rubycop.near>
2023-09-01 12:48:42 +0000

    Update NDC.CommunityScoreboard by rubycop.near at 2023-09-01T12:48:42.207Z

--- rubycop.near/NDC.CommunityScoreboard/source_code.jsx
+++ rubycop.near/NDC.CommunityScoreboard/source_code.jsx
@@ -59,77 +59,84 @@ const communityData = state.data.filter(
   (i) => i["community-name"] === state.community
 );
 
+const getCommunities = () => {
+  const data = state.data.map((item) => item["community-name"]);
+  return [...new Set(data)].sort((a, b) => a - b);
+};
+
 return (
   <div className="d-flex justify-content-center w-100">
     <div className="w-50">
-      <Widget
-        src={"near/widget/Select"}
-        props={{
-          label: "Select community",
-          value: state.community,
-          onChange: (item) => State.update({ community: item }),
-          options: state.data.map((item) => {
-            return {
-              text: item["community-name"],
-              value: item["community-name"],
-            };
-          }),
-        }}
-      />
       <h1>NDC Community users</h1>
-      <div className="d-flex justify-content-between align-items-center">
-        <H3>
-          <b>{state.community}</b>
-        </H3>
-        <Tag>{communityData[0].vertical}</Tag>
+      <div className="my-2">
+        <Widget
+          src={"near/widget/Select"}
+          props={{
+            label: "Select community",
+            value: state.community,
+            onChange: (item) => State.update({ community: item }),
+            options: getCommunities().map((community) => {
+              return {
+                text: community,
+                value: community,
+              };
+            }),
+          }}
+        />
       </div>
-      <small>Total users: {communityData.length}</small>
-      <hr className="my-2" />
-      {communityData.length > 0 ? (
-        communityData
-          .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
-          .map((user) => (
-            <div className="d-flex justify-content-between align-items-center p-1">
-              <Widget
-                src="near/widget/AccountProfileOverlay"
-                props={{
-                  accountId: user.account,
-                  children: (
-                    <div className="d-flex justify-items-center">
-                      <Widget
-                        src="mob.near/widget/ProfileImage"
-                        props={{
-                          accountId: user.account,
-                          imageClassName: "rounded-circle w-100 h-100",
-                          style: {
-                            width: "24px",
-                            height: "24px",
-                            marginRight: 5,
-                          },
-                        }}
-                      />
-                      <UserName>{user.account}</UserName>
-                    </div>
-                  ),
-                }}
-              />
 
-              <small className="text-secondary">
-                {new Date(user.created_at).toLocaleDateString("en-US", {
-                  day: "2-digit",
-                  month: "short",
-                })}
-                &middot;
-                {new Date(user.created_at).toLocaleTimeString("en-US", {
-                  hour: "2-digit",
-                })}
-              </small>
-            </div>
-          ))
-      ) : (
-        <div>
-          <Loader /> Fetching users
-        </div>
+      {communityData.length > 0 && (
+        <>
+          <div className="d-flex justify-content-between align-items-center">
+            <H3>
+              <b>{state.community}</b>
+            </H3>
+            <Tag>{communityData[0].vertical}</Tag>
+          </div>
+          <small>Total users: {communityData.length}</small>
+          <hr className="my-2" />
+
+          {communityData
+            .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+            .map((user) => (
+              <div className="d-flex justify-content-between align-items-center p-1">
+                <Widget
+                  src="near/widget/AccountProfileOverlay"
+                  props={{
+                    accountId: user.account,
+                    children: (
+                      <div className="d-flex justify-items-center">
+                        <Widget
+                          src="mob.near/widget/ProfileImage"
+                          props={{
+                            accountId: user.account,
+                            imageClassName: "rounded-circle w-100 h-100",
+                            style: {
+                              width: "24px",
+                              height: "24px",
+                              marginRight: 5,
+                            },
+                          }}
+                        />
+                        <UserName>{user.account}</UserName>
+                      </div>
+                    ),
+                  }}
+                />
+
+                <small className="text-secondary">
+                  {new Date(user.created_at).toLocaleDateString("en-US", {
+                    day: "2-digit",
+                    month: "short",
+                  })}
+                  &middot;
+                  {new Date(user.created_at).toLocaleTimeString("en-US", {
+                    hour: "2-digit",
+                  })}
+                </small>
+              </div>
+            ))}
+        </>
       )}
     </div>
   </div>
