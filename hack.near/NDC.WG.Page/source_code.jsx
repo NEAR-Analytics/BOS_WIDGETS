491f987172c8870defc779a8583f18436a9fdc92
hack.near <hack.near>
2023-09-01 15:02:07 +0000

    Update NDC.WG.Page by hack.near at 2023-09-01T15:02:07.564Z

--- hack.near/NDC.WG.Page/source_code.jsx
+++ hack.near/NDC.WG.Page/source_code.jsx
@@ -1,3 +1,5 @@
+const daoId = "hack.near";
+
 const registryContract = "registry.i-am-human.near";
 const issuer = "fractal.i-am-human.near";
 
@@ -11,105 +13,7 @@ const widgets = {
   deleteGroup: "hack.near/widget/NDC.WG.Delete",
 };
 
-const groups = {
-  RC: {
-    title: "Regional Communities",
-    creatorId: "rc-dao.near",
-    members: [
-      "abbakaka.near",
-      "igboze_builder.near",
-      "inspiratibiz.near",
-      "james.near",
-      "yuenisd.near",
-    ],
-    daoId: "rc-dao.sputnik-dao.near",
-  },
-  NFT: {
-    title: "Non-Fungible Tokens",
-    creatorId: "nearnftwg.near",
-    members: ["aescobar.near", "krikkraktrak.near", "nearversedao.near"],
-    daoId: "nearnftwg.sputnik-dao.near",
-  },
-  DeFi: {
-    title: "Decentralized Finance",
-    creatorId: "defindc.near",
-    groupId: "DeFi",
-    members: ["ntare.near"],
-    daoId: "defi-ndc.sputnik-dao.near",
-  },
-  AI: {
-    title: "Artificial Intelligence",
-    creatorId: "ai-dao.near",
-    members: ["damboy22.near"],
-    daoId: "ai-dao.sputnik-dao.near",
-  },
-  Gaming: {
-    title: "Gaming",
-    creatorId: "haenko.near",
-    members: ["haenko.near", "dazo_gaming.near", "jeffgold.near"],
-  },
-  Events: {
-    title: "Events",
-    creatorId: "neardigitalcollective.near",
-    members: ["ogruss.near"],
-  },
-  FDAO: {
-    title: "Freelancers",
-    creatorId: "freelancerdao.near",
-    members: [
-      "blaze.near",
-      "fiftycent.near",
-      "atrox1382.near",
-      "robert.near",
-      "kazanderdad.near",
-    ],
-    daoId: "freelancerdao.sputnik-dao.near",
-  },
-  NRC: {
-    title: "Research",
-    creatorId: "research-collective.near",
-    members: ["chloe.near", "earnestetim.near", "xvii.near"],
-    daoId: "research-collective.sputnik-dao.near",
-  },
-  ReFi: {
-    title: "Regenerative Finance",
-    creatorId: "nearrefi.near",
-    members: [
-      "earnestetim.near",
-      "liight.near",
-      "trophy001.near",
-      "skyempire.near",
-      "ndcplug.near",
-      "ntare.near",
-      "wolfwood.near",
-    ],
-    daoId: "refi.sputnik-dao.near",
-  },
-  Aurora: {
-    title: "Aurora",
-    creatorId: "ac-dao.near",
-    members: ["whendacha.near", "techdir.near", "johanga.near"],
-    daoId: "aurora-community-dao.sputnik-dao.near",
-  },
-  Tech: {
-    title: "Technology",
-    creatorId: "neardigitalcollective.near",
-    members: ["robert.near", "jlw.near", "blaze.near"],
-    daoId: "ndc-techwg.sputnik-dao.near",
-  },
-  GWG: {
-    title: "Governance",
-    creatorId: "govworkinggroup.near",
-    members: [
-      "yuensid.near",
-      "fiftycent.near",
-      "atrox1382.near",
-      "robert.near",
-      "kazanderdad.near",
-    ],
-    daoId: "gwg.sputnik-dao.near",
-  },
-};
+const groups = Social.get(`${daoId}/directory/**`);
 
 State.init({
   start: true,
@@ -136,8 +40,6 @@ const getVerifiedHuman = () => {
     sbt: isHuman[0][1].length > 0,
   });
 };
-
-const getGroupData = (group) => Social.get(`${daoId}/groups/${group}`);
 
 // const handleSelect = (item) => {
 //   switch (item.id) {
@@ -226,7 +128,7 @@ return (
               "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
           }}
         />
-        {isVerified ? (
+        {!isVerified ? (
           <Widget
             src={widget.styledComponents}
             props={{
@@ -269,49 +171,6 @@ return (
         <Left className="col-lg mb-3">
           <H5>Learning Together</H5>
           <Widget src={widgets.about} />
-          <div>
-            {!state.sbt ? (
-              <div className="mt-5">
-                <Widget
-                  src={widgets.verifyHuman}
-                  props={{
-                    title: "Get Verified To Create a WG",
-                    small: true,
-                  }}
-                />
-              </div>
-            ) : (
-              <div className="mt-5">
-                <Toolbar>
-                  {props.createdGroup ? (
-                    <Widget
-                      src={widget.styledComponents}
-                      props={{
-                        Button: {
-                          className: "danger primary",
-                          text: "Delete Work Group",
-                          onClick: () =>
-                            State.update({ showModalDelete: true }),
-                          icon: <i class="bi bi-trash"></i>,
-                        },
-                      }}
-                    />
-                  ) : (
-                    <Widget
-                      src={widget.styledComponents}
-                      props={{
-                        Button: {
-                          text: "Create Work Group",
-                          onClick: () => State.update({ showModal: true }),
-                          icon: <i class="bi bi-plus-lg"></i>,
-                        },
-                      }}
-                    />
-                  )}
-                </Toolbar>
-              </div>
-            )}
-          </div>
         </Left>
         <Center className="col-lg-9 px-2 px-md-3 d-flex flex-column">
           <h2>NDC Work Groups</h2>
