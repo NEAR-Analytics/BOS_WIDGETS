be62790107e0736942586406e3112cc7b7708a60
guessme.near <guessme.near>
2023-09-01 07:30:51 +0000

    Update ZKEVM.Template by guessme.near at 2023-09-01T07:30:51.540Z

--- guessme.near/ZKEVM.Template/source_code.jsx
+++ guessme.near/ZKEVM.Template/source_code.jsx
@@ -6,40 +6,59 @@ const Container = styled.div`
 `;
 
 const Search = styled.div`
-display: flex;
-position: fixed;
-top: 112px;
-right: 32px;
-  input{
-     color: #ffffff;
-     background: transparent;
-     border: none;
-     border-bottom: 1px #373A53 solid;
-     padding-right: 24px;
-   }
-   input:focus {
-     outline: none;
-     color: #ffffff;
-     background: transparent;
-     border: none;
-     box-shadow: none;
-     border-bottom: 1px #373A53 solid;
-   }
-   svg {
+  display: flex;
+  position: fixed;
+  top: 112px;
+  right: 32px;
+  input {
+    color: #ffffff;
+    background: transparent;
+    border: none;
+    border-bottom: 1px #373a53 solid;
+    padding-right: 24px;
+  }
+  input:focus {
+    outline: none;
+    color: #ffffff;
+    background: transparent;
+    border: none;
+    box-shadow: none;
+    border-bottom: 1px #373a53 solid;
+  }
+  svg {
     margin-left: -24px;
-    margin-top:10px;
+    margin-top: 10px;
   }
   @media (max-width: 900px) {
-    display:none;
+    display: none;
   }
 `;
 
 const SearchIcon = (
-  <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="7.01829" cy="7.01829" r="6.01829" stroke="#EBF479" strokeWidth="2" />
-    <rect x="14.9141" y="9.64941" width="6.141" height="2.63186" rx="1.31593" transform="rotate(30 14.9141 9.64941)" fill="#EBF479" />
+  <svg
+    width="21"
+    height="15"
+    viewBox="0 0 21 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="7.01829"
+      cy="7.01829"
+      r="6.01829"
+      stroke="#EBF479"
+      strokeWidth="2"
+    />
+    <rect
+      x="14.9141"
+      y="9.64941"
+      width="6.141"
+      height="2.63186"
+      rx="1.31593"
+      transform="rotate(30 14.9141 9.64941)"
+      fill="#EBF479"
+    />
   </svg>
-
 );
 
 const templates = [
@@ -68,41 +87,41 @@ const templates = [
     bannerImg:
       "https://ipfs.near.social/ipfs/bafkreibmqzfwki7zye6ruqi3hifgexs6g5nv4qvzcmrm73lnra5mbkdrxe",
   },
+  {
+    src: "bluebiu.near/widget/0vix.Lending",
+    bannerImg:
+      "https://ipfs.near.social/ipfs/bafkreie6mpwolgngjicyeh332egvshqd76m7zo5waxdgqwcwid4tdwgbtq",
+  },
 ];
 
 State.init({
   searchValue: "",
-})   // 初始化状态
+}); // 初始化状态
 
 const handleSearchChange = (e) => {
   const value = e.target.value;
   State.update({
     searchValue: value,
-  }) // 更新状态值
+  }); // 更新状态值
 };
 
 const metadataPromises = templates.map(({ src }) =>
-  Social.get(`${src}/metadata/**`, 'final')
+  Social.get(`${src}/metadata/**`, "final")
 );
 
 const updatedTemplates = templates.map((template, index) => ({
   ...template,
-  name: metadataPromises[index]?.name || '',
+  name: metadataPromises[index]?.name || "",
 }));
 
 const filteredTemplates = updatedTemplates.filter(({ name }) =>
   name.toLowerCase().includes(state.searchValue.toLowerCase())
 );
 
-
 return (
   <Container>
     <Search>
-      <input
-        type="text"
-        value={searchValue}
-        onChange={handleSearchChange}
-      />
+      <input type="text" value={searchValue} onChange={handleSearchChange} />
       {SearchIcon}
     </Search>
     {filteredTemplates.map(({ src, bannerImg }, index) => (
