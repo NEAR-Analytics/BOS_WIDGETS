0b78b3b88628ccb4923fa0c8ba6112107c7f6d22
hack.near <hack.near>
2023-09-02 14:53:57 +0000

    Update group by hack.near at 2023-09-02T14:53:57.086Z

--- hack.near/group/source_code.jsx
+++ hack.near/group/source_code.jsx
@@ -1,8 +1,6 @@
-const creatorId = props.creatorId ?? "james.near";
 const groupId = props.groupId ?? "6fd36ddf4884flm20pbe91e7b208b88d16";
 
-const group =
-  props.group ?? Social.get(`${creatorId}/thing/${groupId}/**`, "final");
+const group = props.group ?? Social.get(`*/thing/${groupId}/**`, "final");
 
 if (group === null) {
   return "Loading...";
@@ -19,17 +17,13 @@ return (
       <Widget
         src="hack.near/widget/group.card"
         props={{
-          creatorId,
           groupId,
         }}
       />
       <br />
       <div className="m-2">
         <h3 className="mb-3">Members</h3>
-        <Widget
-          src="hack.near/widget/group.members"
-          props={{ creatorId, groupId }}
-        />
+        <Widget src="hack.near/widget/group.members" props={{ groupId }} />
       </div>
     </div>
   </Container>
