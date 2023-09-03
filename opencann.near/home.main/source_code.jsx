5998215910f191e5d39cb5f551f48f9d17862be9
opencann.near <opencann.near>
2023-09-03 23:37:56 +0000

    Update home.main by opencann.near at 2023-09-03T23:37:56.770Z

---
+++ opencann.near/home.main/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <>
+    <Widget src={`opencann.near/widget/home.header`} />
+    <Widget src={`ndcplug.near/widget/BOSHACKS.Home.SubHeader`} />
+    <Widget src={`ndcplug.near/widget/BOSHACKS.Home.Countdown`} />
+
+    <Widget src={`ndcplug.near/widget/BOSHACKS.Home.Schedule`} />
+
+    <Widget src={`ndcplug.near/widget/BOSHACKS.Home.Sponsors`} />
+  </>
+);
