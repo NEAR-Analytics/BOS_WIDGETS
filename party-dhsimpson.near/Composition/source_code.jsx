f2345f567ed6d599d6a75f48c120caf1dfbecff0
party-dhsimpson.near <party-dhsimpson.near>
2023-09-03 06:05:43 +0000

    Update Composition by party-dhsimpson.near at 2023-09-03T06:05:43.697Z

---
+++ party-dhsimpson.near/Composition/source_code.jsx
@@ -0,0 +1,13 @@
+// user의 값은 자신의 지갑 주소를 입력해주세요.
+const user = "party-dhsimpson.near";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+    {/** 자신이 만든 Greeter 컴포넌트를 불러올 수 있습니다. **/}
+    // <Widget src={`${user}/widget/Greeter`} props={{ name: user }} />
+    <Widget src="sniped.near/widget/Shop.wallets.link" props={{}} />
+  </>
+);
