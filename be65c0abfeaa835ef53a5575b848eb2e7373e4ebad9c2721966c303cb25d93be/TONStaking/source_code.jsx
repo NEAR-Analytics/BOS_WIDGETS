c98055fad469a24953627f66c5edafa5f6d55abf
be65c0abfeaa835ef53a5575b848eb2e7373e4ebad9c2721966c303cb25d93be <be65c0abfeaa835ef53a5575b848eb2e7373e4ebad9c2721966c303cb25d93be>
2023-09-03 07:23:59 +0000

    Update TONStaking by be65c0abfeaa835ef53a5575b848eb2e7373e4ebad9c2721966c303cb25d93be at 2023-09-03T07:23:59.736Z

--- be65c0abfeaa835ef53a5575b848eb2e7373e4ebad9c2721966c303cb25d93be/TONStaking/source_code.jsx
+++ be65c0abfeaa835ef53a5575b848eb2e7373e4ebad9c2721966c303cb25d93be/TONStaking/source_code.jsx
@@ -1,80 +1,76 @@
-if (
-  state.chainId === undefined &&
-  ethers !== undefined &&
-  Ethers.send("eth_requestAccounts", [])[0]
-) {
-  Ethers.provider()
-    .getNetwork()
-    .then((chainIdData) => {
-      if (chainIdData?.chainId) {
-        State.update({ chainId: chainIdData.chainId });
-      }
-    });
-}
-if (state.chainId !== undefined && state.chainId !== 1) {
-  return <p>Switch to Ethereum Mainnet</p>;
-}
-
-// setup constants
-const lockTOSDividendProxyAddress =
-  "0x17332F84Cc0bbaD551Cd16675F406A0a2c55E28C";
-const WTONAddress = "0xc4a11aaf6ea915ed7ac194161d2fc9384f15bff2";
-const wtonDecimals = 27;
-
-const handleClaim = () => {
-  if (Number(state.claimable) === 0) return;
-
-  const ABI = ["function claim(address) external"];
-
-  const lockTOSDividendProxyContract = new ethers.Contract(
-    lockTOSDividendProxyAddress,
-    ABI,
-    Ethers.provider().getSigner()
-  );
-
-  lockTOSDividendProxyContract.claim(WTONAddress).then((transactionHash) => {
-    State.update({ tx: transactionHash });
-    console.log("transactionHash is " + transactionHash);
-  });
-};
-
-// DETECT SENDER
-if (state.sender === undefined) {
-  const accounts = Ethers.send("eth_requestAccounts", []);
-  if (accounts.length) {
-    State.update({ sender: accounts[0] });
-    console.log("set sender", accounts[0]);
-  }
-}
-
-// FETCH CLAIMABLE WTON AMOUNT
-if (state.claimable === undefined && state.sender) {
-  const iface = new ethers.utils.Interface([
-    "function claimable(address, address) external view returns (uint256)",
-  ]);
-  const encodedData = iface.encodeFunctionData("claimable", [
-    state.sender,
-    WTONAddress,
-  ]);
-  return Ethers.provider()
-    .call({
-      to: lockTOSDividendProxyAddress,
-      data: encodedData,
-    })
-    .then((rawdata) => {
-      const claimableHex = iface.decodeFunctionResult("claimable", rawdata);
-
-      console.log(claimableHex);
-
-      const claimable = Big(claimableHex.toString())
-        .div(Big(10).pow(wtonDecimals))
-        .toFixed(2)
-        .replace(/\d(?=(\d{3})+\.)/g, "$&,");
-
-      State.update({
-        claimable: Big(claimable).div(Big(10).pow(wtonDecimals)).toFixed(2),
+if (Ethers.provider() !== null) {
+  if (state.chainId === undefined) {
+    Ethers.provider()
+      .getNetwork()
+      .then((chainIdData) => {
+        if (chainIdData?.chainId) {
+          State.update({ chainId: chainIdData.chainId });
+        }
       });
-    });
+  }
+
+  if (state.chainId !== undefined && state.chainId !== 1) {
+    return <p>Switch to Ethereum Mainnet</p>;
+  }
+
+  // DETECT SENDER
+  if (state.sender === undefined) {
+    const accounts = Ethers.send("eth_requestAccounts", []);
+    if (accounts.length) {
+      State.update({ sender: accounts[0] });
+      console.log("set sender", accounts[0]);
+    }
+  }
+
+  const handleClaim = () => {
+    if (Number(state.claimable) === 0) return;
+
+    const ABI = ["function claim(address) external"];
+
+    const lockTOSDividendProxyContract = new ethers.Contract(
+      props.LOCK_TOS_DIVIDEND_PROXY_ADDRESS,
+      ABI,
+      Ethers.provider().getSigner()
+    );
+
+    lockTOSDividendProxyContract
+      .claim(props.WTON_ADDRESS)
+      .then((transactionHash) => {
+        State.update({ tx: transactionHash });
+        console.log("transactionHash is " + transactionHash);
+      });
+  };
+
+  // FETCH CLAIMABLE WTON AMOUNT
+  if (state.claimable === undefined && state.sender) {
+    const iface = new ethers.utils.Interface([
+      "function claimable(address, address) external view returns (uint256)",
+    ]);
+    const encodedData = iface.encodeFunctionData("claimable", [
+      state.sender,
+      props.WTON_ADDRESS,
+    ]);
+
+    return Ethers.provider()
+      .call({
+        to: props.LOCK_TOS_DIVIDEND_PROXY_ADDRESS,
+        data: encodedData,
+      })
+      .then((rawdata) => {
+        const claimableHex = iface.decodeFunctionResult("claimable", rawdata);
+
+        const claimable = Big(claimableHex.toString())
+          .div(Big(10).pow(props.WTON_DECIMALS))
+          .toFixed(2)
+          .replace(/\d(?=(\d{3})+\.)/g, "$&,");
+
+        State.update({
+          claimable: Big(claimable)
+            .div(Big(10).pow(props.WTON_DECIMALS))
+            .toFixed(2),
+        });
+      });
+  }
 }
 
 // FETCH CSS
@@ -90,10 +86,12 @@ if (!cssFont || !css) return "";
 if (!state.theme) {
   State.update({
     theme: styled.div`
-    font-family: Manrope, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    ${cssFont}
-    ${css}
-`,
+      font-family: Manrope, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+        Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+        sans-serif;
+      ${cssFont}
+      ${css}
+    `,
   });
 }
 const Theme = state.theme;
