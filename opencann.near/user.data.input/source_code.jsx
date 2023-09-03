87cced2438abd32dac0372136fc59a24a562e9bc
opencann.near <opencann.near>
2023-09-03 00:12:22 +0000

    Update user.data.input by opencann.near at 2023-09-03T00:12:22.524Z

--- opencann.near/user.data.input/source_code.jsx
+++ opencann.near/user.data.input/source_code.jsx
@@ -1,6 +1,23 @@
 const accountId = props.accountId ?? context.accountId;
 const onChange = props.onChange;
 const options = props.options;
+
+if (context.loading) {
+  return (
+    <>
+      <h2>)"Loading..."</h2>
+    </>
+  );
+}
+
+if (!accountId) {
+  return (
+    <>
+      <h2>"Please sign in with a NEAR ecosystem wallet to use this widget"</h2>
+    </>
+  );
+}
+
 let optInInfo = {};
 let changeLog = [];
 
@@ -86,79 +103,91 @@ function handleChangeOnInput(event) {
   });
 }
 
-return (
-  <div id="optInDataForm">
-    {options.birthday.label ?? "Birthday"}
-    <input
-      id="birthday"
-      type="date"
-      value={state.birthday}
-      onChange={(event) => handleChangeOnInput(event)}
-    />
-    {options.astrological_sign.label ?? "Astrological Sign"}
-    <input
-      id="astrological_sign"
-      type="text"
-      value={state.astrological_sign}
-      onChange={(event) => handleChangeOnInput(event)}
-    />
-    {options.age.label ?? "Age"}
-    <input
-      id="age"
-      type="text"
-      value={state.age}
-      onChange={(event) => handleChangeOnInput(event)}
-    />
-    {options.gender.label ?? "Gender"}
-    <input
-      id="gender"
-      type="text"
-      value={state.gender}
-      onChange={(event) => handleChangeOnInput(event)}
-    />
-    {options.profession.label ?? "Profession"}
-    <input
-      id="profession"
-      type="text"
-      value={state.profession}
-      onChange={(event) => handleChangeOnInput(event)}
-    />
-    {options.income.label ?? "Income"}
-    <input
-      id="income"
-      type="text"
-      value={state.income}
-      onChange={(event) => handleChangeOnInput(event)}
-    />
-    {options.preferred_wallet.label ?? "Preferred Wallet"}
-    <input
-      id="preferred_wallet"
-      type="text"
-      value={state.preferred_wallet}
-      onChange={(event) => handleChangeOnInput(event)}
-    />
-    {options.preferred_nft_marketplace.label ?? "Preferred NFT Marketplace"}
-    <input
-      id="preferred_nft_marketplace"
-      type="text"
-      value={state.preferred_nft_marketplace}
-      onChange={(event) => handleChangeOnInput(event)}
-    />
-    {options.preferred_crypto.label ?? "Preferred Crypto"}
-    <input
-      id="preferred_crypto"
-      type="text"
-      value={state.preferred_crypto}
-      onChange={(event) => handleChangeOnInput(event)}
-    />
+let page = (
+  <div>
+    <h1>{`Hello, ${accountId}!`}</h1>
+    <p>
+      {`Please fill out the following information. This data is private!
+      Only include the data you are willing to share with analytics queries. 
+      Everytime your data is accessed in a query, you will earn NEAR!`}
+    </p>
     <p></p>
-    <CommitButton
-      data={{ optInInfoFormStatus: state.saveState }}
-      onClick={handleOnClick}
-      onCommit={handleOnCommit}
-    >
-      Save Opt-In Info
-    </CommitButton>
-    <button onClick={() => State.update(initialState)}>Reset Form</button>
+    <div id="optInDataForm">
+      {options.birthday.label ?? "Birthday"}
+      <input
+        id="birthday"
+        type="date"
+        value={state.birthday}
+        onChange={(event) => handleChangeOnInput(event)}
+      />
+      {options.astrological_sign.label ?? "Astrological Sign"}
+      <input
+        id="astrological_sign"
+        type="text"
+        value={state.astrological_sign}
+        onChange={(event) => handleChangeOnInput(event)}
+      />
+      {options.age.label ?? "Age"}
+      <input
+        id="age"
+        type="text"
+        value={state.age}
+        onChange={(event) => handleChangeOnInput(event)}
+      />
+      {options.gender.label ?? "Gender"}
+      <input
+        id="gender"
+        type="text"
+        value={state.gender}
+        onChange={(event) => handleChangeOnInput(event)}
+      />
+      {options.profession.label ?? "Profession"}
+      <input
+        id="profession"
+        type="text"
+        value={state.profession}
+        onChange={(event) => handleChangeOnInput(event)}
+      />
+      {options.income.label ?? "Income"}
+      <input
+        id="income"
+        type="text"
+        value={state.income}
+        onChange={(event) => handleChangeOnInput(event)}
+      />
+      {options.preferred_wallet.label ?? "Preferred Wallet"}
+      <input
+        id="preferred_wallet"
+        type="text"
+        value={state.preferred_wallet}
+        onChange={(event) => handleChangeOnInput(event)}
+      />
+      {options.preferred_nft_marketplace.label ?? "Preferred NFT Marketplace"}
+      <input
+        id="preferred_nft_marketplace"
+        type="text"
+        value={state.preferred_nft_marketplace}
+        onChange={(event) => handleChangeOnInput(event)}
+      />
+      {options.preferred_crypto.label ?? "Preferred Crypto"}
+      <input
+        id="preferred_crypto"
+        type="text"
+        value={state.preferred_crypto}
+        onChange={(event) => handleChangeOnInput(event)}
+      />
+      <p></p>
+      <CommitButton
+        data={{ optInInfoFormStatus: state.saveState }}
+        onClick={handleOnClick}
+        onCommit={handleOnCommit}
+      >
+        Save Opt-In Info
+      </CommitButton>
+      <button onClick={() => State.update(initialState)}>Reset Form</button>
+    </div>
+    <p></p>
   </div>
 );
+
+return page;
