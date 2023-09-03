bf54980d3cbc2195b8fdae3bf794b6151b4585d1
opencann.near <opencann.near>
2023-09-03 23:37:25 +0000

    Update home.header by opencann.near at 2023-09-03T23:37:25.969Z

---
+++ opencann.near/home.header/source_code.jsx
@@ -0,0 +1,34 @@
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 4.5em;
+  gap: 0.625em;
+  background: #000;
+
+  h1 {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 2.5em;
+    line-height: 1.25em;
+    color: #fff;
+  }
+
+  p {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.25em;
+    line-height: 140%;
+    color: #00ec97;
+  }
+`;
+
+return (
+  <Container>
+    <image src="https://pbs.twimg.com/profile_banners/1688015970911367169/1691627385/1500x500"></image>
+
+    <h1>Find, fund, and do cannabis research</h1>
+    <p>OpenCann Community Marketplace</p>
+  </Container>
+);
