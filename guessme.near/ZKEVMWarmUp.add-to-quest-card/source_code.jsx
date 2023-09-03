605f01c1a23fc1ed6ef6504784e8bc0d923d5760
guessme.near <guessme.near>
2023-09-03 15:02:41 +0000

    Update ZKEVMWarmUp.add-to-quest-card by guessme.near at 2023-09-03T15:02:41.068Z

--- guessme.near/ZKEVMWarmUp.add-to-quest-card/source_code.jsx
+++ guessme.near/ZKEVMWarmUp.add-to-quest-card/source_code.jsx
@@ -135,6 +135,7 @@ const { add, onChangeAdd, source, hide } = props;
 
 const onAdd = () => {
   Storage.set("storeCheckTip", 1);
+  Storage.privateSet("store_add", "true");
   State.update({
     storeShowTip: false,
   });
@@ -143,11 +144,20 @@ const onAdd = () => {
 
 const onCancel = () => {
   Storage.set("storeCheckTip", 1);
+  Storage.privateSet("store_add", "false");
+
   State.update({
     storeShowTip: false,
   });
   onChangeAdd(false);
 };
+
+const store_add = Storage.privateGet("store_add");
+console.log("store_add: ", store_add);
+
+if (store_add === "true") {
+  onAdd();
+}
 
 const storeCheckTip = Storage.get(
   "storeCheckTip",
