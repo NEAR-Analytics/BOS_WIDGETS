e00155f64bbed52ec9375adec3c3fff1157e985c
opencann.near <opencann.near>
2023-09-03 03:03:23 +0000

    Update mydata by opencann.near at 2023-09-03T03:03:23.384Z

--- opencann.near/mydata/source_code.jsx
+++ opencann.near/mydata/source_code.jsx
@@ -149,6 +149,15 @@ return (
         NOT publish any sensitive or proprietary information.
       </p>
       <hr></hr>
+      <Widget
+        src="near/widget/Explorer.Account"
+        props={{
+          accountId,
+          network: context.networkId,
+          language: "en",
+          baseUrl: props.baseUrl,
+        }}
+      />
       <Widget src="opencann.near/widget/user.data.input" props={{ ownerId }} />
     </div>
     <Items>
