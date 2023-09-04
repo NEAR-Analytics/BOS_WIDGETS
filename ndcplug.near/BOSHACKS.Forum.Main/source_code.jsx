e1926a4251728d8402e8c364f3bfdb66dd56b7b9
ndcplug.near <ndcplug.near>
2023-09-04 11:49:29 +0000

    Update BOSHACKS.Forum.Main by ndcplug.near at 2023-09-04T11:49:29.740Z

--- ndcplug.near/BOSHACKS.Forum.Main/source_code.jsx
+++ ndcplug.near/BOSHACKS.Forum.Main/source_code.jsx
@@ -2,7 +2,7 @@ return (
   <div>
     <Widget
       src="ndcplug.near/widget/BOSHACKS.Forum.Create"
-      props={{ referral: "boshacks" }}
+      props={{ referral: "boshacks", labels: "hackathon" }}
     />
     <Widget
       src="devgovgigs.near/widget/gigs-board.entity.post.List"
