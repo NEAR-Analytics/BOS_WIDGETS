b50ea1cd1f4572354c3d9757e33cf455e9c9b650
f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a <f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a>
2023-09-04 13:18:54 +0000

    Update Composition by f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a at 2023-09-04T13:18:54.118Z

---
+++ f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a/Composition/source_code.jsx
@@ -0,0 +1,13 @@
+// user의 값은 자신의 지갑 주소를 입력해주세요.
+
+const user = "f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+    {/** 자신이 만든 Greeter 컴포넌트를 불러올 수 있습니다. **/}
+    <Widget src={`${user}/widget/Greeter`} props={props} />
+  </>
+);
