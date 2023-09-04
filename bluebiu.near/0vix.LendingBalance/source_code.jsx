441b162d4ebcae6ad5a6ce93729092d068daf733
bluebiu.near <bluebiu.near>
2023-09-04 07:25:07 +0000

    Update 0vix.LendingBalance by bluebiu.near at 2023-09-04T07:25:07.143Z

--- bluebiu.near/0vix.LendingBalance/source_code.jsx
+++ bluebiu.near/0vix.LendingBalance/source_code.jsx
@@ -1,14 +1,19 @@
 const Balance = styled.div`
   font-weight: 500;
-  font-size: 18px;
   color: #fff;
   display: flex;
   align-items: baseline;
-  .f_16 {
-    font-size: 16px;
-  }
-  .f_24 {
+  .integer_size {
     font-size: 24px;
+    @media (max-width: 640px) {
+      font-size: 20px;
+    }
+  }
+  .float_size {
+    font-size: 16px;
+    @media (max-width: 640px) {
+      font-size: 12px;
+    }
   }
 `;
 const { balance } = props;
@@ -16,8 +21,8 @@ if (!balance || isNaN(balance)) return <Balance>$0</Balance>;
 const splits = balance.split(".");
 return (
   <Balance>
-    <div className="f_24">${Number(splits[0]).toLocaleString()}</div>
-    {splits[1] && <div className="f_24">.</div>}
-    <div className="f_16">{splits[1]}</div>
+    <div className="integer_size">${Number(splits[0]).toLocaleString()}</div>
+    {splits[1] && <div className="integer_size">.</div>}
+    <div className="float_size">{splits[1]}</div>
   </Balance>
 );
