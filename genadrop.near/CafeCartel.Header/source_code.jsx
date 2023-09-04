8a1b032d974770aff34d6879f2afefe82d4c5169
genadrop.near <genadrop.near>
2023-09-04 18:31:43 +0000

    Update CafeCartel.Header by genadrop.near at 2023-09-04T18:31:43.146Z

--- genadrop.near/CafeCartel.Header/source_code.jsx
+++ genadrop.near/CafeCartel.Header/source_code.jsx
@@ -1,1 +1,36 @@
-return <div>Hello World</div>;
+
+const BackgroundImage = styled.div`
+  height: 240px;
+  border-radius: 20px 20px 0 0;
+  overflow: hidden;
+  margin: 0 -12px;
+  background: #eceef0;
+
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (max-width: 1024px) {
+    margin: calc(var(--body-top-padding) * -1) -12px 0;
+    border-radius: 0;
+  }
+
+  @media (max-width: 1024px) {
+    height: 100px;
+  }
+`;
+return <div>
+    <BackgroundImage>
+        <Widget
+          src="mob.near/widget/Image"
+          props={{
+            image: "https://ipfs.near.social/ipfs/bafkreiggzwt5isdtkd3uggctgglbe6dt4phlwmecm4mjswnsbvd5d4noum",
+            alt: "Cafe Cartel header Image",
+            fallbackUrl:
+              "https://ipfs.near.social/ipfs/bafkreiggzwt5isdtkd3uggctgglbe6dt4phlwmecm4mjswnsbvd5d4noum",
+          }}
+        />
+ 
+    </BackgroundImage></div>;
