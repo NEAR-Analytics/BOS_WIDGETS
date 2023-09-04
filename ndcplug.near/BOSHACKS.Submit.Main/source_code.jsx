1fe21d97d24b1a5c6eb138003c43306c148730dc
ndcplug.near <ndcplug.near>
2023-09-04 12:43:20 +0000

    Update BOSHACKS.Submit.Main by ndcplug.near at 2023-09-04T12:43:20.051Z

--- ndcplug.near/BOSHACKS.Submit.Main/source_code.jsx
+++ ndcplug.near/BOSHACKS.Submit.Main/source_code.jsx
@@ -5,9 +5,10 @@
  */
 return (
   <div>
+  <Widget src="ndcplug.near/widget/BOSHACKS.Home.Countdown"/>
     <Widget
       src="ndcplug.near/widget/BOSHACKS.Submit.Create"
-      props={{ referral: "boshacks-submission" }}
+      props={{ referral: "boshacks-submission", labels: "boshacks, hackathon" }}
     />
     <Widget
       src="devgovgigs.near/widget/gigs-board.entity.post.List"
