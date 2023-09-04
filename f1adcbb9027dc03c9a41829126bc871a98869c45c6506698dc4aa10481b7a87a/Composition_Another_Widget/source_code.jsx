f6b11d518c770cd75f81278d6808320f088f9034
f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a <f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a>
2023-09-04 13:31:30 +0000

    Update Composition_Another_Widget by f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a at 2023-09-04T13:31:30.938Z

---
+++ f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a/Composition_Another_Widget/source_code.jsx
@@ -0,0 +1,11 @@
+const user = "f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+
+    <Widget src={`${user}/widget/harmonicHomPage`} props={props} />
+  </>
+);
