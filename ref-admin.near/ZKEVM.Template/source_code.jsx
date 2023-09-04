d375e8ee33d625a8198505657df9c99f8a03e6a2
ref-admin.near <ref-admin.near>
2023-09-04 13:47:35 +0000

    Update ZKEVM.Template by ref-admin.near at 2023-09-04T13:47:35.704Z

--- ref-admin.near/ZKEVM.Template/source_code.jsx
+++ ref-admin.near/ZKEVM.Template/source_code.jsx
@@ -1,29 +1,38 @@
-
 const Container = styled.div`
-  display:flex;
-  flex-wrap:wrap;
-  gap:30px 36px;
-  color:#fff;
-`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px 36px;
+  color: #fff;
+
+  @media (max-width: 900px) {
+    padding-left: 12px;
+    padding-right: 12px;
+  }
+`;
 const templates = [
   {
-    src: 'ref-admin.near/widget/ZKEVMSwap.zkevm-bridge',
+    src: "ref-admin.near/widget/ZKEVMSwap.zkevm-bridge",
   },
   {
-    src: 'ref-admin.near/widget/ZKEVMSwap.zkevm-swap',
+    src: "ref-admin.near/widget/ZKEVMSwap.zkevm-swap",
   },
   {
-    src: 'ref-admin.near/widget/ZKEVM.GAMMA',
+    src: "ref-admin.near/widget/ZKEVM.GAMMA",
   },
   {
-    src: 'ref-admin.near/widget/ZKEVM.AAVE',
-  }
-]
-return <Container>
-  {
-    templates.map(({src}, index) => <Widget src="ref-admin.near/widget/ZKEVM.Template-card" key={index} props={{
-      src,
-    }}/>)
-  }
-  
-</Container>
+    src: "ref-admin.near/widget/ZKEVM.AAVE",
+  },
+];
+return (
+  <Container>
+    {templates.map(({ src }, index) => (
+      <Widget
+        src="ref-admin.near/widget/ZKEVM.Template-card"
+        key={index}
+        props={{
+          src,
+        }}
+      />
+    ))}
+  </Container>
+);
