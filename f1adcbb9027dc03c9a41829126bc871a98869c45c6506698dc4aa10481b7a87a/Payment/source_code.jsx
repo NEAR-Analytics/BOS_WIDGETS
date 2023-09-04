791b825267c43e06aaca9c21f1bc099906bfc984
f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a <f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a>
2023-09-04 13:37:52 +0000

    Update Payment by f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a at 2023-09-04T13:37:52.862Z

---
+++ f1adcbb9027dc03c9a41829126bc871a98869c45c6506698dc4aa10481b7a87a/Payment/source_code.jsx
@@ -0,0 +1,11 @@
+const user = "harmonic1.near";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+
+    <Widget src={`${user}/widget/harmonicHomPage`} props={props} />
+  </>
+);
