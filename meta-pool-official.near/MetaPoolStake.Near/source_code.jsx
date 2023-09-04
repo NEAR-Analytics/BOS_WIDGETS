23773993e31480c5e59b6e0d8f76bc2b536cce84
meta-pool-official.near <meta-pool-official.near>
2023-09-04 18:35:50 +0000

    Update MetaPoolStake.Near by meta-pool-official.near at 2023-09-04T18:35:50.270Z

--- meta-pool-official.near/MetaPoolStake.Near/source_code.jsx
+++ meta-pool-official.near/MetaPoolStake.Near/source_code.jsx
@@ -20,7 +20,7 @@ State.init({
   stNearBalance: null,
   stNearBalanceIsFetched: false,
   dataIntervalStarted: false,
-  action: props.action || "stake",
+  action: "stake",
 });
 
 function isValid(a) {
@@ -239,4 +239,160 @@ const render = {
   ),
 }[state.action];
 
-return <>{render}</>;
+const SelectionContainer = styled.div`
+    width: 100%;
+    max-width: 600px;
+    align-self: center;
+    background-color: white;
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 1.6em;
+    border-radius: 20px;
+    padding: 12px 26px;
+    box-shadow: none;
+    color: #fff;    
+    margin-bottom: 1em;
+    padding: 12px 26px 32px 26px;
+  `;
+
+const ActionItem = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 8px 16px;
+  width: 14em;
+  height:5em;;
+  text-align: left;
+  align-items: center;
+  border: 0.8px solid rgb(215, 224, 228);
+  background: rgb(247, 249, 251);
+  opacity: 0.8;
+
+  border-radius: 24px;
+
+  ${({ active }) =>
+    active
+      ? `
+    background: rgb(206, 255, 26);
+  `
+      : `
+    :hover {
+      background: rgb(215, 224, 228);
+    }
+  `}
+
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const Text = styled.p`
+  color:#000000;
+  font-size: 14px;
+  line-height: 21px;
+`;
+
+const SelectAction = styled.div`
+border-bottom-left-radius: 0px;
+border-bottom-right-radius: 0px;
+border-radius: 20px;
+display: flex;
+flex-direction: column;
+width: 100%;
+`;
+
+const TokensList = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+const renderActions = (
+  <SelectAction>
+    <Text>Select action</Text>
+    <TokensList>
+      <ActionItem
+        onClick={() => {
+          State.update({ action: "stake" });
+        }}
+        active={state.action == "stake"}
+      >
+        <div>Stake</div>
+        <div>
+          <svg
+            focusable="false"
+            preserveAspectRatio="xMidYMid meet"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            width="24"
+            height="24"
+            viewBox="0 0 32 32"
+            aria-hidden="true"
+          >
+            <path d="M16,7,6,17l1.41,1.41L15,10.83V28H2v2H15a2,2,0,0,0,2-2V10.83l7.59,7.58L26,17Z"></path>
+            <path d="M6,8V4H26V8h2V4a2,2,0,0,0-2-2H6A2,2,0,0,0,4,4V8Z"></path>
+          </svg>
+        </div>
+      </ActionItem>
+      <ActionItem
+        onClick={() => {
+          State.update({ action: "fast" });
+        }}
+        active={state.action == "fast"}
+      >
+        <div>Fast Unstake</div>
+        <div>
+          <svg
+            focusable="false"
+            preserveAspectRatio="xMidYMid meet"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            width="24"
+            height="24"
+            viewBox="0 0 32 32"
+            aria-hidden="true"
+          >
+            <path d="M18,30H4a2,2,0,0,1-2-2V14a2,2,0,0,1,2-2H18a2,2,0,0,1,2,2V28A2,2,0,0,1,18,30ZM4,14V28H18V14Z"></path>
+            <path d="M25,23H23V9H9V7H23a2,2,0,0,1,2,2Z"></path>
+            <path d="M30,16H28V4H16V2H28a2,2,0,0,1,2,2Z"></path>
+          </svg>
+        </div>
+      </ActionItem>
+
+      <ActionItem
+        onClick={() => {
+          State.update({ action: "delayed" });
+        }}
+        active={state.action == "delayed"}
+      >
+        <div>Delayed Unstake</div>
+        <div>
+          <svg
+            focusable="false"
+            preserveAspectRatio="xMidYMid meet"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            width="24"
+            height="24"
+            viewBox="0 0 32 32"
+            aria-hidden="true"
+          >
+            <path d="M15 19H17V21H15zM15 23H17V25H15z"></path>
+            <path d="M23,11.67V4h3V2H6V4H9v7.67a2,2,0,0,0,.4,1.2L11.75,16,9.4,19.13a2,2,0,0,0-.4,1.2V28H6v2H26V28H23V20.33a2,2,0,0,0-.4-1.2L20.25,16l2.35-3.13A2,2,0,0,0,23,11.67ZM21,4v7H11V4Zm0,16.33V28H11V20.33L14.25,16,12,13h8l-2.25,3Z"></path>
+          </svg>
+        </div>
+      </ActionItem>
+    </TokensList>
+  </SelectAction>
+);
+
+return (
+  <>
+    <SelectionContainer>{renderActions}</SelectionContainer>
+    {render}
+  </>
+);
