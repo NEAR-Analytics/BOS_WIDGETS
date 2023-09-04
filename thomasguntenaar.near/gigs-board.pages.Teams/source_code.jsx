d43404b68aea0714533e8db899a74699003bce6d
thomasguntenaar.near <thomasguntenaar.near>
2023-09-04 09:53:02 +0000

    Update gigs-board.pages.Teams by thomasguntenaar.near at 2023-09-04T09:53:02.626Z

--- thomasguntenaar.near/gigs-board.pages.Teams/source_code.jsx
+++ thomasguntenaar.near/gigs-board.pages.Teams/source_code.jsx
@@ -61,6 +61,33 @@ const DevHub = {
     Near.call(devHubAccountId, "edit_community_github", { handle, github }) ??
     null,
 
+  create_workspace: ({ author_community_handle, metadata }) =>
+    Near.call(devHubAccountId, "create_workspace", {
+      author_community_handle,
+      metadata,
+    }) ?? null,
+
+  delete_workspace: ({ id }) =>
+    Near.call(devHubAccountId, "delete_workspace", { id }) ?? null,
+
+  update_workspace_metadata: ({ metadata }) =>
+    Near.call(devHubAccountId, "update_workspace_metadata", { metadata }) ??
+    null,
+
+  get_workspace_views_metadata: ({ workspace_id }) =>
+    Near.view(devHubAccountId, "get_workspace_views_metadata", {
+      workspace_id,
+    }) ?? null,
+
+  create_workspace_view: ({ view }) =>
+    Near.call(devHubAccountId, "create_workspace_view", { view }) ?? null,
+
+  update_workspace_view: ({ view }) =>
+    Near.call(devHubAccountId, "update_workspace_view", { view }) ?? null,
+
+  delete_workspace_view: ({ id }) =>
+    Near.call(devHubAccountId, "delete_workspace_view", { id }) ?? null,
+
   get_access_control_info: () =>
     Near.view(devHubAccountId, "get_access_control_info") ?? null,
 
@@ -113,39 +140,6 @@ const DevHub = {
   },
 };
 /* END_INCLUDE: "core/adapter/dev-hub" */
-/* INCLUDE: "entity/viewer" */
-const access_control_info = DevHub.useQuery({
-  name: "access_control_info",
-});
-
-const Viewer = {
-  can: {
-    editCommunity: (communityData) =>
-      Struct.typeMatch(communityData) &&
-      (communityData.admins.includes(context.accountId) ||
-        Viewer.role.isDevHubModerator),
-  },
-
-  role: {
-    isDevHubModerator:
-      access_control_info.data === null || access_control_info.isLoading
-        ? false
-        : access_control_info.data.members_list[
-            "team:moderators"
-          ]?.children?.includes?.(context.accountId) ?? false,
-  },
-};
-/* END_INCLUDE: "entity/viewer" */
-
-const isContractOwner = nearDevGovGigsContractAccountId == context.accountId;
-
-State.init({
-  labelData: null,
-  teamData: null,
-  createTeam: false,
-  createLabel: false,
-  isEditorActive: false,
-});
 
 const access_info = DevHub.get_access_control_info() ?? null,
   root_members = DevHub.get_root_members() ?? null;
@@ -154,176 +148,18 @@ if (!access_info || !root_members) {
   return <div>Loading...</div>;
 }
 
-function addLabel(labelData) {
-  Near.call([
-    {
-      contractName: nearDevGovGigsContractAccountId,
-      methodName: "set_restricted_rules",
-      args: {
-        rules: {
-          [labelData.name]: {
-            description: labelData.description,
-            rule_metadata_version: "V0",
-          },
-        },
-      },
-      deposit: Big(0).pow(21),
-      gas: Big(10).pow(12).mul(100),
-    },
-  ]);
-}
-
-function addTeam(teamData) {
-  let permissions = {};
-  let labels = teamData.label.split(",");
-  labels.forEach((element) => {
-    permissions[element] = ["edit-post", "use-labels"];
-  });
-  Near.call([
-    {
-      contractName: nearDevGovGigsContractAccountId,
-      methodName: "add_member",
-      args: {
-        member: `team:${teamData.name}`,
-        metadata: {
-          member_metadata_version: "V0",
-          description: teamData.description,
-          permissions,
-          children: [],
-          parents: [],
-        },
-      },
-      deposit: Big(0).pow(21),
-      gas: Big(10).pow(12).mul(100),
-    },
-  ]);
-}
-
 const pageContent = (
-  <div className="pt-3">
-    {(Viewer.role.isDevHubModerator || isContractOwner) &&
-      widget("components.layout.Controls", {
-        title: "Create Restricted labels",
-        onClick: () => {
-          State.update({
-            createLabel: !state.createLabel,
-          });
-        },
-      })}
-    <div className="pt-3">
-      {widget("entity.team.LabelsPermissions", {
-        rules: access_info.rules_list,
-      })}
-    </div>
-    {state.createLabel &&
-      widget("components.organism.editor", {
-        classNames: {
-          submit: "btn-primary",
-          submitAdornment: "bi-check-circle-fill",
-        },
-        heading: "Restricted labels",
-        isEditorActive: state.isEditorActive,
-        isEditingAllowed: Viewer.role.isDevHubModerator || isContractOwner,
-        onChangesSubmit: addLabel,
-        submitLabel: "Accept",
-        data: state.labelData,
-        schema: {
-          name: {
-            inputProps: {
-              min: 2,
-              max: 30,
-              placeholder: "Label name (starts-with:<label>  or <label>)",
-              required: true,
-            },
-            label: "Name",
-            order: 1,
-          },
-          description: {
-            inputProps: {
-              min: 2,
-              max: 60,
-              placeholder: "Label description",
-              required: true,
-            },
-            label: "Description",
-            order: 2,
-          },
-        },
-      })}
-    {(Viewer.role.isDevHubModerator || isContractOwner) && (
-      <div class="pt-3">
-        {widget("components.layout.Controls", {
-          title: "Create Team",
-          onClick: () => {
-            State.update({
-              createTeam: !state.createTeam,
-            });
-          },
-        })}
-      </div>
+  <div>
+    {widget("entity.team.LabelsPermissions", {
+      rules: access_info.rules_list,
+    })}
+    {Object.keys(root_members).map((member) =>
+      widget(
+        "entity.team.TeamInfo",
+        { member, members_list: access_info.members_list },
+        member
+      )
     )}
-    {state.createTeam &&
-      widget("components.organism.editor", {
-        classNames: {
-          submit: "btn-primary",
-          submitAdornment: "bi-check-circle-fill",
-        },
-        heading: "Team info",
-        isEditorActive: state.isEditorActive,
-        isEditingAllowed: Viewer.role.isDevHubModerator || isContractOwner,
-        onChangesSubmit: addTeam,
-        submitLabel: "Accept",
-        data: state.teamData,
-        schema: {
-          name: {
-            inputProps: {
-              min: 2,
-              max: 30,
-              placeholder: "Team name",
-              required: true,
-            },
-            label: "Name",
-            order: 1,
-          },
-          description: {
-            inputProps: {
-              min: 2,
-              max: 60,
-              placeholder: "Team description",
-              required: true,
-            },
-            label: "Description",
-            order: 2,
-          },
-          label: {
-            label: "Labels",
-            order: 3,
-            format: "comma-separated",
-            inputProps: {
-              min: 2,
-              max: 60,
-              placeholder: Object.keys(access_info.rules_list).join(","),
-              required: true,
-            },
-          },
-        },
-      })}
-    {root_members
-      ? Object.keys(root_members).map((member) =>
-          widget(
-            "entity.team.TeamInfo",
-            {
-              member,
-              members_list: access_info.members_list,
-              rules_list: access_info.rules_list,
-              teamLevel: true,
-              root_members,
-              teamId: member,
-            },
-            member
-          )
-        )
-      : null}
   </div>
 );
 
