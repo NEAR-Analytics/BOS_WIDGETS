f618a12ffc53ffcc14a79af10e14c581948ecd48
smleekr.near <smleekr.near>
2023-09-05 04:16:16 +0000

    Update Composition by smleekr.near at 2023-09-05T04:16:16.523Z

---
+++ smleekr.near/Composition/source_code.jsx
@@ -0,0 +1,12 @@
+// user의 값은 자신의 지갑 주소를 입력해주세요.
+const user = "smleekr.near";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+    {/** 자신이 만든 Greeter 컴포넌트를 불러올 수 있습니다. **/}
+    <Widget src={`${user}/widget/Greeter`} props={props} />
+  </>
+);
