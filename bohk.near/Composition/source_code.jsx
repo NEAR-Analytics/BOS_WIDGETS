cccb174d8a488dcda19a9b31ef2fb4aac221cb68
bohk.near <bohk.near>
2023-09-05 12:28:32 +0000

    Update Composition by bohk.near at 2023-09-05T12:28:32.534Z

---
+++ bohk.near/Composition/source_code.jsx
@@ -0,0 +1,10 @@
+const user = "bohk.near";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+    <Widget src={`${user}/widget/Greeter`} props={props} />
+  </>
+);
