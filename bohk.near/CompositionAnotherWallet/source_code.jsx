4620a0171b5ad683d60b20422b8b5775ff29339a
bohk.near <bohk.near>
2023-09-05 12:34:41 +0000

    Update CompositionAnotherWallet by bohk.near at 2023-09-05T12:34:41.757Z

---
+++ bohk.near/CompositionAnotherWallet/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+    <Widget
+      src={`election.ndctools.near/widget/NDC.Elections.Header`}
+      props={props}
+    />
+  </>
+);
