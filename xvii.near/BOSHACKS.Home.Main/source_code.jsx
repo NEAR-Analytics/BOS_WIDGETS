f7019cc07ad3d2d84edcf7f0a6a802581fee3ba0
xvii.near <xvii.near>
2023-09-05 18:28:53 +0000

    Update BOSHACKS.Home.Main by xvii.near at 2023-09-05T18:28:53.488Z

--- xvii.near/BOSHACKS.Home.Main/source_code.jsx
+++ xvii.near/BOSHACKS.Home.Main/source_code.jsx
@@ -3,5 +3,7 @@ return (
     <Widget src={`xvii.near/widget/BOSHACKS.Home.Header`} />
     <Widget src={`xvii.near/widget/BOSHACKS.Home.SubHeader`} />
     <Widget src={`xvii.near/widget/BOSHACKS.Home.Countdown`} />
+    <Widget src={`xvii.near/widget/BOSHACKS.Home.Schedule`} />
+    <Widget src={`xvii.near/widget/BOSHACKS.Home.Sponsers`} />
   </>
 );
