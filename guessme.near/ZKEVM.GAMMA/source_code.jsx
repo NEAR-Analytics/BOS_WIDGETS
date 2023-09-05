2ab90b64bfab429a7f4e3415306152dd9fe66eb1
guessme.near <guessme.near>
2023-09-05 08:16:18 +0000

    Update ZKEVM.GAMMA by guessme.near at 2023-09-05T08:16:18.313Z

--- guessme.near/ZKEVM.GAMMA/source_code.jsx
+++ guessme.near/ZKEVM.GAMMA/source_code.jsx
@@ -102,6 +102,7 @@ const fetchPoolsData = () => {
     "https://wire2.gamma.xyz/quickswap/polygon-zkevm/hypervisors/allData"
   ).then((res) => {
     if (!res.ok) return;
+    console.log("res: ", res.body);
 
     State.update({
       poolsData: res.body,
