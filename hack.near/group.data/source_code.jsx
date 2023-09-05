c956c19d3e4536a70bd900b6a2cd37742ad1cc84
hack.near <hack.near>
2023-09-05 19:34:21 +0000

    Update group.data by hack.near at 2023-09-05T19:34:21.642Z

--- hack.near/group.data/source_code.jsx
+++ hack.near/group.data/source_code.jsx
@@ -4,15 +4,23 @@ if (!groupId) {
   return "group not found";
 }
 
-const initMembers =
-  Social.get(`${context.accountId}/thing/${groupId}/members`) || [];
-
 State.init({
   groupId,
   group,
-  members: initMembers,
+  members: null,
   newMember: "",
 });
+
+function init() {
+  if (state.members === null) {
+    const initMembers =
+      Social.get(`${context.accountId}/thing/${groupId}/members`) || [];
+    return <p>JSON.stringify(initMembers)</p>;
+    Social.set({ members: initMembers });
+  }
+}
+
+init();
 
 function addMember(newMember) {
   state.members.push(newMember);
@@ -55,36 +63,20 @@ const memberId = props.memberId || state.newMember;
 const isValid = isNearAddress(memberId);
 
 const handleCreate = () => {
-  let Group_Payload = {
-    contractName: "social.near",
-    methodName: "set",
-    args: {
-      data: {
-        [context.accountId]: {
-          thing: {
-            [state.groupId]: {
-              ...state.group,
-              members: { ...state.members },
-            },
-          },
-          graph: {
-            [state.groupId]: {
-              ...state.members,
-            },
-          },
-        },
+  Social.set({
+    thing: {
+      [state.groupId]: {
+        ...state.group,
+        members: [...state.members],
       },
     },
-    gas: 300000000000000,
-    deposit: 100000000000000000000000,
-  };
-
-  Near.call(Group_Payload).then(() => handleClose());
+    graph: {
+      [state.groupId]: {
+        ...state.members,
+      },
+    },
+  });
 };
-
-const groupData = Social.get(
-  `${context.accountId}/thing/${state.groupId}/members`
-);
 
 return (
   <>
@@ -95,92 +87,90 @@ return (
           placeholder={groupId}
           onChange={(e) => State.update({ groupId: e.target.value })}
         />
-        {groupData && (
-          <>
-            <div className="mt-3">
-              <button className="btn btn-success me-2" onClick={handleCreate}>
-                save
-              </button>
-              <button
-                className="btn btn-secondary me-2"
-                href={`/hack.near/widget/group?groupId=${groupId}`}
-              >
-                view
-              </button>
-            </div>
-            <div className="mt-4">
-              <h5>Details</h5>
-              <Widget
-                src="near/widget/MetadataEditor"
-                props={{
-                  initialMetadata: group,
-                  onChange: (group) => State.update({ group }),
-                  options: {
-                    name: {
-                      label: "Name",
-                    },
-                    image: { label: "Logo" },
-                    description: { label: "About" },
-                    tags: {
-                      label: "Tags",
-                      tagsPattern: `*/${groupId}/tags/*`,
-                      placeholder: "art, gov, edu, dev, com, nft, ai, social",
-                    },
-                    linktree: {
-                      links: [
-                        {
-                          label: "Twitter",
-                          prefix: "https://twitter.com/",
-                          name: "twitter",
-                        },
-                        {
-                          label: "Github",
-                          prefix: "https://github.com/",
-                          name: "github",
-                        },
-                        {
-                          label: "Telegram",
-                          prefix: "https://t.me/",
-                          name: "telegram",
-                        },
-                        {
-                          label: "Website",
-                          prefix: "https://",
-                          name: "website",
-                        },
-                      ],
-                    },
+        <>
+          <div className="mt-3">
+            <button className="btn btn-success me-2" onClick={handleCreate}>
+              save
+            </button>
+            <button
+              className="btn btn-secondary me-2"
+              href={`/hack.near/widget/group?groupId=${groupId}`}
+            >
+              view
+            </button>
+          </div>
+          <div className="mt-4">
+            <h5>Details</h5>
+            <Widget
+              src="near/widget/MetadataEditor"
+              props={{
+                initialMetadata: group,
+                onChange: (group) => State.update({ group }),
+                options: {
+                  name: {
+                    label: "Name",
                   },
-                }}
-              />
-            </div>
-          </>
-        )}
+                  image: { label: "Logo" },
+                  description: { label: "About" },
+                  tags: {
+                    label: "Tags",
+                    tagsPattern: `*/${groupId}/tags/*`,
+                    placeholder: "art, gov, edu, dev, com, nft, ai, social",
+                  },
+                  linktree: {
+                    links: [
+                      {
+                        label: "Twitter",
+                        prefix: "https://twitter.com/",
+                        name: "twitter",
+                      },
+                      {
+                        label: "Github",
+                        prefix: "https://github.com/",
+                        name: "github",
+                      },
+                      {
+                        label: "Telegram",
+                        prefix: "https://t.me/",
+                        name: "telegram",
+                      },
+                      {
+                        label: "Website",
+                        prefix: "https://",
+                        name: "website",
+                      },
+                    ],
+                  },
+                },
+              }}
+            />
+          </div>
+        </>
       </div>
       <div className="col-lg-6">
-        {groupData && (
-          <>
-            <div>
-              <h5>Add Members</h5>
-              <input
-                placeholder="<example>.near"
-                onChange={(e) => State.update({ newMember: e.target.value })}
-              />
-              <div className="d-flex align-items-center mt-2">
-                <button
-                  className="btn btn-primary mt-2"
-                  onClick={() => addMember(state.newMember)}
-                >
-                  add
-                </button>
-              </div>
+        <>
+          <div>
+            <h5>Add Members</h5>
+            <input
+              placeholder="<example>.near"
+              onChange={(e) => State.update({ newMember: e.target.value })}
+            />
+            <div className="d-flex align-items-center mt-2">
+              <button
+                className="btn btn-primary mt-2"
+                onClick={() => addMember(state.newMember)}
+              >
+                add
+              </button>
             </div>
-            <br />
-            {state.groupId === groupId ? (
-              <div>
-                <h5>Profiles</h5>
-
-                {JSON.parse(state.members).map((a, i) => {
+          </div>
+          <br />
+          {state.groupId === groupId && (
+            <div>
+              <h5>Profiles</h5>
+              <p>{JSON.parse(state.members)}</p>
+              {state.members &&
+                state.members.map((a, i) => {
                   return (
                     <div
                       key={i}
@@ -200,36 +190,9 @@ return (
                     </div>
                   );
                 })}
-              </div>
-            ) : (
-              <>
-                <div>
-                  <h5>Profiles</h5>
-                  {JSON.parse(groupData).map((a, i) => {
-                    return (
-                      <div
-                        key={i}
-                        className="d-flex m-2 p-2 justify-content-between align-items-center"
-                      >
-                        <Widget
-                          key={i}
-                          src="mob.near/widget/Profile"
-                          props={{ accountId: a }}
-                        />
-                        <button
-                          className="btn btn-danger m-1"
-                          onClick={() => removeMember(a)}
-                        >
-                          remove
-                        </button>
-                      </div>
-                    );
-                  })}
-                </div>
-              </>
-            )}
-          </>
-        )}
+            </div>
+          )}
+        </>
       </div>
     </div>
   </>
