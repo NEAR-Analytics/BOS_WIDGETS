aaaf0484229617272806b1c245e0a6e717424f63
sunny0827.near <sunny0827.near>
2023-09-05 13:59:35 +0000

    Update Composition by sunny0827.near at 2023-09-05T13:59:35.835Z

--- sunny0827.near/Composition/source_code.jsx
+++ sunny0827.near/Composition/source_code.jsx
@@ -1,11 +1,10 @@
-const user = "idknwhoru.near";
+const user = "sunny0827.near";
 
 return (
   <>
     <h3> Composition </h3>
     <p> Components can be composed </p>
     <hr />
-    {/** 자신이 만든 Greeter 컴포넌트를 불러올 수 있습니다. **/}
     <Widget src={`${user}/widget/Greeter`} props={props} />
   </>
 );
