ab1d6e787dfe2e81a45cf386c6a01b5186be3e4a
duocelot.near <duocelot.near>
2023-09-05 19:27:37 +0000

    Update onboard by duocelot.near at 2023-09-05T19:27:37.719Z

---
+++ duocelot.near/onboard/source_code.jsx
@@ -0,0 +1,32 @@
+const ImageLinks = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreib56aciji2mgnwhqt3nkmutxqfednxqllgzvd5gm6d4mcjbumnxzm"
+        alt="Image 1"
+        style={{ width: "100px", margin: "10px" }}
+      />
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreifikdi444catqds54ulo3kwujvetmng7pwmr2tmg7hadtxmfguyeu"
+        alt="Image 2"
+        style={{ width: "100px", margin: "10px" }}
+      />
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreigqamafvyfqcwsp4gl4nyh4wof7ldrz7oloj5fb22lqbu77fz7pda"
+        alt="Image 3"
+        style={{ width: "100px", margin: "10px" }}
+      />
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreiae77ymuywxv5iqofxygb26iy5b44qtao2gp4ipbxbrzadjf2rpwy"
+        alt="Image 4"
+        style={{ width: "100px", margin: "10px" }}
+      />
+    </div>
+  );
+};
