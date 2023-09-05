4b65227239a81189cc200451d0f30ba02982d524
sunny0827.near <sunny0827.near>
2023-09-05 07:40:23 +0000

    Update Greeter by sunny0827.near at 2023-09-05T07:40:23.461Z

---
+++ sunny0827.near/Greeter/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>
+        Welcome to <div style={{ color: "blue" }}>{props.program}</div> Gateway
+      </h1>
+
+      <p> {props.greeting} </p>
+    </div>
+  </>
+);
