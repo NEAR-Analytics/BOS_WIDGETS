fe6edea469348ab9809c66493febbe94caa2b422
andyh.near <andyh.near>
2023-09-05 20:08:32 +0000

    Update WidgetIcons by andyh.near at 2023-09-05T20:08:32.987Z

--- andyh.near/WidgetIcons/source_code.jsx
+++ andyh.near/WidgetIcons/source_code.jsx
@@ -1,6 +1,6 @@
 const accountId = props.accountId;
 const tag = props.tag;
-console.log({ props });
+console.log({ a: props.accountId, t: props.tag });
 let keys = `${accountId ?? "*"}/widget/*`;
 
 if (tag) {
