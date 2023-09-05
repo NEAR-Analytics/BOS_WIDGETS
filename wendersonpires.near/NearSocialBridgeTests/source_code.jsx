2625f2352aa23781b811c41e97c220238b2fc725
wendersonpires.near <wendersonpires.near>
2023-09-05 18:52:38 +0000

    Update NearSocialBridgeTests by wendersonpires.near at 2023-09-05T18:52:38.011Z

---
+++ wendersonpires.near/NearSocialBridgeTests/source_code.jsx
@@ -0,0 +1,46 @@
+const externalAppUrl = "https://near-social-bridge-tests.vercel.app";
+const initialViewHeight = 700;
+const initialPayload = {
+  myNiceProp: "fake value",
+  numberProp: 33,
+};
+
+/**
+ * Request Handlers.
+ */
+const requestHandler = (request, response, Utils) => {
+  switch (request.type) {
+    case "request-01":
+      exampleHandler(request, response);
+      break;
+    default:
+      defaultRequestHandler(request, response);
+  }
+};
+
+const exampleHandler = (request, response) => {
+  response(request).send({
+    ok: true,
+    for: "request-01",
+    data_received: request.payload,
+  });
+};
+
+const defaultRequestHandler = (request, response) => {
+  response(request).send({
+    ok: false,
+    msg: `request handler for "${request.type}" not found.`,
+  });
+};
+
+return (
+  <Widget
+    src="wendersonpires.near/widget/NearSocialBridgeCore"
+    props={{
+      externalAppUrl,
+      initialViewHeight,
+      initialPayload,
+      requestHandler,
+    }}
+  />
+);
