696af9b381527bdcaba2ecd3e82bff42026789ff
sourcescan.near <sourcescan.near>
2023-09-05 15:17:40 +0000

    Update SourceScan.Contracts.Approved by sourcescan.near at 2023-09-05T15:17:40.915Z

--- sourcescan.near/SourceScan.Contracts.Approved/source_code.jsx
+++ sourcescan.near/SourceScan.Contracts.Approved/source_code.jsx
@@ -1,5 +1,9 @@
+const useNetwork = (mainnet, testnet) => {
+  return context.networkId === "mainnet" ? mainnet : testnet;
+};
+
 State.init({
-  placement: props.placement || "top",
+  ownerId: useNetwork("sourcescan.near", "sourcescan.testnet"),
   value: null,
   error: false,
 });
@@ -58,68 +62,31 @@ const compareWasm = () => {
 
 compareWasm();
 
-const CheckIcon = (width, height) => {
-  const SVG = styled.svg`
-    width: ${width}
-    height: ${height}
-  `;
-
-  return (
-    <SVG
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-        clip-rule="evenodd"
-      />
-    </SVG>
-  );
-};
-
-const CrossIcon = (width, height) => {
-  const SVG = styled.svg`
-    width: ${width}
-    height: ${height}
-  `;
-
-  return (
-    <SVG
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-    </SVG>
-  );
-};
-
 return (
   <>
     {state.value === null ? (
       <Widget src={`${props.ownerId}/widget/SourceScan.Common.Spinner`} />
     ) : state.value === true ? (
-      <OverlayTrigger
-        key={state.placement}
-        placement={state.placement}
-        overlay={<Tooltip id={`tooltip-${placement}`}>{"Approved"}</Tooltip>}
-      >
-        <CheckIcon width={"20px"} height={"20px"} />
-      </OverlayTrigger>
+      <Widget
+        src={`${state.ownerId}/widget/SourceScan.Common.Icons.CheckIcon`}
+        props={{
+          width: "20px",
+          height: "20px",
+          tooltip: { placement: props.placement, label: "Approved" },
+        }}
+      />
     ) : (
-      <OverlayTrigger
-        key={state.placement}
-        placement={state.placement}
-        overlay={
-          <Tooltip id={`tooltip-${placement}`}>
-            {state.error ? "Error" : "Not approved"}
-          </Tooltip>
-        }
-      >
-        <CrossIcon width={"32px"} height={"32px"} />
-      </OverlayTrigger>
+      <Widget
+        src={`${state.ownerId}/widget/SourceScan.Common.Icons.CrossIcon`}
+        props={{
+          width: "20px",
+          height: "20px",
+          tooltip: {
+            placement: props.placement,
+            label: state.error ? "Error" : "Not approved",
+          },
+        }}
+      />
     )}
   </>
 );
