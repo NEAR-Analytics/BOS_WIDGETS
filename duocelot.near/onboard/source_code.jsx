85c0f418fb4bfad758178dace47fac359dae78ec
duocelot.near <duocelot.near>
2023-09-05 20:31:28 +0000

    Update onboard by duocelot.near at 2023-09-05T20:31:28.918Z

--- duocelot.near/onboard/source_code.jsx
+++ duocelot.near/onboard/source_code.jsx
@@ -1,32 +1,71 @@
-const ImageLinks = () => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <img
-        src="https://ipfs.near.social/ipfs/bafkreib56aciji2mgnwhqt3nkmutxqfednxqllgzvd5gm6d4mcjbumnxzm"
-        alt="Image 1"
-        style={{ width: "100px", margin: "10px" }}
-      />
-      <img
-        src="https://ipfs.near.social/ipfs/bafkreifikdi444catqds54ulo3kwujvetmng7pwmr2tmg7hadtxmfguyeu"
-        alt="Image 2"
-        style={{ width: "100px", margin: "10px" }}
-      />
-      <img
-        src="https://ipfs.near.social/ipfs/bafkreigqamafvyfqcwsp4gl4nyh4wof7ldrz7oloj5fb22lqbu77fz7pda"
-        alt="Image 3"
-        style={{ width: "100px", margin: "10px" }}
-      />
-      <img
-        src="https://ipfs.near.social/ipfs/bafkreiae77ymuywxv5iqofxygb26iy5b44qtao2gp4ipbxbrzadjf2rpwy"
-        alt="Image 4"
-        style={{ width: "100px", margin: "10px" }}
-      />
+const accountId = context.accountId;
+
+<Widget src="duocelot.near/widget/error_001" />;
+
+const MenuImage = styled.img`
+  width: 150px;
+  height: auto;  // Adjusted from '100%px' which isn't a valid value
+  border-radius: 30px;
+  margin: 0 10px;  // This adds a margin of 10px to the left and right of each image
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+return (
+  <div
+    style={{
+      width: "100%",
+      height: "100%",
+      background: "linear-gradient(to right, white, white, white)",
+      backgroundSize: "100% 100%",
+      backgroundPosition: "center",
+      position: "relative",
+      overflow: "hidden",
+      zIndex: 0,
+    }}
+  >
+    <div style={{ width: "100%" }}>
+      <div
+        style={{
+          backgroundImage: "url()",
+          backgroundSize: "auto",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "100%",
+          width: "100%",
+          color: "#fff",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <MenuImage
+          id
+          src="https://ipfs.near.social/ipfs/bafkreib56aciji2mgnwhqt3nkmutxqfednxqllgzvd5gm6d4mcjbumnxzm"
+          alt="btn"
+        />
+        <MenuImage
+          src="https://ipfs.near.social/ipfs/bafkreifikdi444catqds54ulo3kwujvetmng7pwmr2tmg7hadtxmfguyeu"
+          alt="btn"
+        />
+        <MenuImage
+          src="https://ipfs.near.social/ipfs/bafkreigqamafvyfqcwsp4gl4nyh4wof7ldrz7oloj5fb22lqbu77fz7pda"
+          alt="btn"
+        />
+        <MenuImage
+          src="https://ipfs.near.social/ipfs/bafkreiae77ymuywxv5iqofxygb26iy5b44qtao2gp4ipbxbrzadjf2rpwy"
+          alt="btn"
+        />
+        <MenuImage
+          src="https://ipfs.near.social/ipfs/bafkreifzp5dafotrrzitwrp2op6shyavpad4nx6rxl2wk2xf343a6vtgqa"
+          alt="btn"
+        />
+        <MenuImage
+          src="https://ipfs.near.social/ipfs/bafkreiesatthnz7gp25slanw3nlhztrvuqllmqyxng62x4kg7jxaerclba"
+          alt="btn"
+        />
+      </div>
     </div>
-  );
-};
+  </div>
+);
