06f683b035881f297bb89930c98dac5d685d1ce7
sharddog.near <sharddog.near>
2023-09-06 14:40:42 +0000

    Update LocationCheck.View by sharddog.near at 2023-09-06T14:40:42.650Z

---
+++ sharddog.near/LocationCheck.View/source_code.jsx
@@ -0,0 +1,9 @@
+<Widget
+  src="sharddog.near/widget/LocationCheck"
+  props={{
+    latRange: "{[38.692, 38.794]}",
+    lonRange: "{[-9.177, -9.099]}",
+    locationName: "Lisbon, Portugal",
+    IPIPI_TOKEN: "YOUR_IPINFO_TOKEN",
+  }}
+/>;
