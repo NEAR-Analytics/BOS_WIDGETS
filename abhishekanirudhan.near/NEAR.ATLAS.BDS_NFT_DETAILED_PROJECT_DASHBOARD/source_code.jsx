ba08eb79003e6bbe43460d267169510296b92686
abhishekanirudhan.near <abhishekanirudhan.near>
2023-09-06 05:58:43 +0000

    Update NEAR.ATLAS.BDS_NFT_DETAILED_PROJECT_DASHBOARD by abhishekanirudhan.near at 2023-09-06T05:58:43.031Z

--- abhishekanirudhan.near/NEAR.ATLAS.BDS_NFT_DETAILED_PROJECT_DASHBOARD/source_code.jsx
+++ abhishekanirudhan.near/NEAR.ATLAS.BDS_NFT_DETAILED_PROJECT_DASHBOARD/source_code.jsx
@@ -2,6 +2,7 @@ const initialState = {
   selectedMetric: "MAU",
   processedData: processedData,
   metric_period: "Monthly",
+  project_name: props.project_name || "SailGP",
 };
 
 state = State.init(initialState);
@@ -137,7 +138,7 @@ const project_name = props.project_name || "SailGP";
 
 const METRIC_NAME = `"${project_name}'s ${state.metric_period}"`;
 
-const filteredData = filterByProjectName(finalData, project_name) || [];
+const filteredData = filterByProjectName(finalData, state.project_name) || [];
 
 const filteredSortedData = sortByActivityDate(filteredData) || [];
 
@@ -203,6 +204,27 @@ const v_bar_data = {
 
 const v_bar_options = {
   responsive: true,
+  scales: {
+    y: {
+      stacked: true,
+      grid: {
+        color: "rgb(41,51,64)", // This will change the gridline color
+        borderColor: "rgb(240,255,240)",
+      },
+      ticks: {
+        color: "rgb(240,255,240)", // This will change the axis text label color
+      },
+    },
+    x: {
+      stacked: true,
+      grid: {
+        color: "rgb(41,51,64)", // This will change the gridline color
+      },
+      ticks: {
+        color: "rgb(240,255,240)", // This will change the axis text label color
+      },
+    },
+  },
   plugins: {
     legend: {
       position: "top",
@@ -218,13 +240,6 @@ const v_bar_options = {
 
 // logic part-3
 
-const handleDropdownChange = (e) => {
-  console.log("selectedMetric:", e.target.value);
-  State.update({
-    selectedMetric: e.target.value,
-  });
-};
-
 const header_map = {
   DAU: "Daily Active Accounts",
   MAU: "Monthly Active Accounts",
@@ -232,6 +247,36 @@ const header_map = {
   NFT_MINTS: "NFT Mints",
   U_MINTERS: "Unique Minters",
 };
+
+// 1. Create a function to extract unique project names:
+function getUniqueProjectNames(data) {
+  const projectNamesSet = new Set();
+  data.forEach((item) => {
+    if (item.PROJECT_NAME) {
+      projectNamesSet.add(item.PROJECT_NAME);
+    }
+  });
+  return [...projectNamesSet];
+}
+
+// 2. Get the unique project names from `finalData`:
+const uniqueProjectNames = getUniqueProjectNames(finalData);
+
+// 3. Create a handler for the project name dropdown change:
+const handleProjectDropdownChange = (e) => {
+  // console.log("selectedProject:", e.target.value);
+  State.update({
+    project_name: e.target.value,
+  });
+};
+
+const handleDropdownChange = (e) => {
+  // console.log("selectedMetric:", e.target.value);
+  State.update({
+    selectedMetric: e.target.value,
+  });
+};
+
 const getBarData = () => {
   const { selectedMetric } = state;
   console.log(selectedMetric);
@@ -258,17 +303,23 @@ const getBarData = () => {
 
 return (
   <Style>
-    <div className="text-bg-light rounded-4 p-3 mb-4">
+    <div className="bg-dark container rounded-4 p-3 mb-4">
       {data !== null ? (
         <p>
           <div class="">
             <div class="">
               <div>
-                <h2 className="text-black">Metric: {METRIC_NAME}</h2>
+                <h2 className="text-white">Metric: {METRIC_NAME}</h2>
 
-                <label htmlFor="metric-dropdown">Select metric: </label>
+                <label
+                  htmlFor="metric-dropdown"
+                  className="text-white block text-sm font-medium"
+                >
+                  Select metric:{" "}
+                </label>
                 <select
                   id="metric-dropdown"
+                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring focus:border-blue-300 sm:text-sm rounded-md"
                   value={selectedMetric}
                   onChange={handleDropdownChange}
                 >
@@ -277,6 +328,23 @@ return (
                   <option value="ACCTS_CREATED">Accounts Created</option>
                   <option value="NFT_MINTS">NFT Mints</option>
                   <option value="U_MINTERS">Unique Minters</option>
+                </select>
+
+                <select
+                  id="project-dropdown"
+                  value={project_name}
+                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring focus:border-blue-300 sm:text-sm rounded-md"
+                  onChange={handleProjectDropdownChange}
+                >
+                  {uniqueProjectNames.map((name) => (
+                    <option
+                      key={name}
+                      value={name}
+                      selected={name === state.project_name}
+                    >
+                      {name}
+                    </option>
+                  ))}
                 </select>
 
                 <BarEl options={v_bar_options} data={getBarData()} />
