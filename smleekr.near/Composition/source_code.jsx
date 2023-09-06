7e319764ff90c0626007a28951dad60469577061
smleekr.near <smleekr.near>
2023-09-06 04:57:25 +0000

    Update Composition by smleekr.near at 2023-09-06T04:57:25.537Z

--- smleekr.near/Composition/source_code.jsx
+++ smleekr.near/Composition/source_code.jsx
@@ -7,6 +7,6 @@ return (
     <p> Components can be composed </p>
     <hr />
     {/** 자신이 만든 Greeter 컴포넌트를 불러올 수 있습니다. **/}
-    <Widget src={`${user}/widget/Greeter`} props={props} />
+    <Widget src={`${user}/widget/GreeterToday`} props={props} />
   </>
 );
