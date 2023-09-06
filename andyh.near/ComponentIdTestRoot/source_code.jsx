0769582216ce6dbd88828b21a26592fa4547406f
andyh.near <andyh.near>
2023-09-06 22:55:35 +0000

    Update ComponentIdTestRoot by andyh.near at 2023-09-06T22:55:35.389Z

--- andyh.near/ComponentIdTestRoot/source_code.jsx
+++ andyh.near/ComponentIdTestRoot/source_code.jsx
@@ -2,5 +2,6 @@ return (
   <>
     <Widget src="andyh.near/widget/ComponentITestChild" id={0} />
     <Widget src="andyh.near/widget/ComponentITestChild" id={1} />
+    <Widget src="andyh.near/widget/ComponentITestChild" id={2} />
   </>
 );
