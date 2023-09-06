117310ae3e13e2faaa013d86fde199bda4245520
frol.near <frol.near>
2023-09-06 07:26:33 +0000

    Update devhub.hackbox by frol.near at 2023-09-06T07:26:33.044Z

--- frol.near/devhub.hackbox/source_code.jsx
+++ frol.near/devhub.hackbox/source_code.jsx
@@ -4,7 +4,8 @@ return (
     style={{
       width: "1px",
       minWidth: "100%",
-      height: "calc(100vh - 78px - var(--bs-gutter-y))",
+      height:
+        "calc(100vh - 78px - var(--bs-gutter-y) - var(--body-top-padding))",
     }}
     src="https://the-hackbox.super.site/"
   />
