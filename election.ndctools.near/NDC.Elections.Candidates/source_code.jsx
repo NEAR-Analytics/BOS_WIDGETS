2474b96da66617ab956c164381f3cfa05c956309
election.ndctools.near <election.ndctools.near>
2023-09-06 12:30:55 +0000

    Update NDC.Elections.Candidates by election.ndctools.near at 2023-09-06T12:30:55.926Z

--- election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
@@ -652,7 +652,11 @@ const CandidateItem = ({ candidateId, votes }) => (
           <Votes>
             <input
               id="input"
-              disabled={alreadyVotedForHouse() || blacklisted}
+              disabled={
+                alreadyVotedForHouse() ||
+                blacklisted ||
+                electionStatus !== "ONGOING"
+              }
               onClick={() => handleSelectCandidate(candidateId)}
               className="form-check-input"
               type="checkbox"
