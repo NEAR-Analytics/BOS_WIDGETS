0b3efa92c93c9dc26b3eb297756ab2babc3214ce
duocelot.near <duocelot.near>
2023-09-06 00:32:25 +0000

    Update onboard by duocelot.near at 2023-09-06T00:32:25.064Z

--- duocelot.near/onboard/source_code.jsx
+++ duocelot.near/onboard/source_code.jsx
@@ -3,14 +3,55 @@ const accountId = context.accountId;
 <Widget src="duocelot.near/widget/error_001" />;
 
 const MenuImage = styled.img`
+  box-sizing: border-box;
   width: 150px;
-  height: auto;  // Adjusted from '100%px' which isn't a valid value
+  height: auto;
   border-radius: 30px;
-  margin: 0 10px;  // This adds a margin of 10px to the left and right of each image
+  margin: 0 10px;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
+
+const handleOnMouseEnter = (url) => {
+  State.update({ hoveredImg: url });
+};
+
+const handleOnMouseLeave = () => {
+  State.update({ hoveredImg: null });
+};
+
+const getOverlayMessage = (url) => {
+  const messages = {
+    "https://ipfs.near.social/ipfs/bafkreib56aciji2mgnwhqt3nkmutxqfednxqllgzvd5gm6d4mcjbumnxzm":
+      {
+        title: "Title for Image 1",
+        content: "Content for Image 1",
+      },
+    // Add more messages for other images...
+  };
+  const message = messages[url] || {
+    title: "Default Title",
+    content: "Default message",
+  };
+
+  return (
+    <div
+      className="border m-3 p-3 rounded-4 bg-white shadow"
+      style={{
+        maxWidth: "200px",
+        width: "200px",
+        height: "200px",
+        zIndex: 1,
+      }}
+      onMouseEnter={() => handleOnMouseEnter(url)}
+      onMouseLeave={handleOnMouseLeave}
+    >
+      <h5>{message.title}</h5>
+      <p>{message.content}</p>
+    </div>
+  );
+};
 
 return (
   <div
@@ -40,31 +81,30 @@ return (
           justifyContent: "center",
         }}
       >
-        <MenuImage
-          id
-          src="https://ipfs.near.social/ipfs/bafkreib56aciji2mgnwhqt3nkmutxqfednxqllgzvd5gm6d4mcjbumnxzm"
-          alt="btn"
-        />
-        <MenuImage
-          src="https://ipfs.near.social/ipfs/bafkreifikdi444catqds54ulo3kwujvetmng7pwmr2tmg7hadtxmfguyeu"
-          alt="btn"
-        />
-        <MenuImage
-          src="https://ipfs.near.social/ipfs/bafkreigqamafvyfqcwsp4gl4nyh4wof7ldrz7oloj5fb22lqbu77fz7pda"
-          alt="btn"
-        />
-        <MenuImage
-          src="https://ipfs.near.social/ipfs/bafkreiae77ymuywxv5iqofxygb26iy5b44qtao2gp4ipbxbrzadjf2rpwy"
-          alt="btn"
-        />
-        <MenuImage
-          src="https://ipfs.near.social/ipfs/bafkreifzp5dafotrrzitwrp2op6shyavpad4nx6rxl2wk2xf343a6vtgqa"
-          alt="btn"
-        />
-        <MenuImage
-          src="https://ipfs.near.social/ipfs/bafkreiesatthnz7gp25slanw3nlhztrvuqllmqyxng62x4kg7jxaerclba"
-          alt="btn"
-        />
+        {[
+          "https://ipfs.near.social/ipfs/bafkreib56aciji2mgnwhqt3nkmutxqfednxqllgzvd5gm6d4mcjbumnxzm",
+          "https://ipfs.near.social/ipfs/bafkreifikdi444catqds54ulo3kwujvetmng7pwmr2tmg7hadtxmfguyeu",
+          "https://ipfs.near.social/ipfs/bafkreigqamafvyfqcwsp4gl4nyh4wof7ldrz7oloj5fb22lqbu77fz7pda",
+          "https://ipfs.near.social/ipfs/bafkreiae77ymuywxv5iqofxygb26iy5b44qtao2gp4ipbxbrzadjf2rpwy",
+          "https://ipfs.near.social/ipfs/bafkreifzp5dafotrrzitwrp2op6shyavpad4nx6rxl2wk2xf343a6vtgqa",
+          "https://ipfs.near.social/ipfs/bafkreiesatthnz7gp25slanw3nlhztrvuqllmqyxng62x4kg7jxaerclba",
+        ].map((url) => (
+          <OverlayTrigger
+            key={url}
+            show={state.hoveredImg === url}
+            trigger={["hover", "focus"]}
+            delay={{ show: 250, hide: 300 }}
+            placement="auto"
+            overlay={getOverlayMessage(url)}
+          >
+            <MenuImage
+              src={url}
+              alt="btn"
+              onMouseEnter={() => handleOnMouseEnter(url)}
+              onMouseLeave={handleOnMouseLeave}
+            />
+          </OverlayTrigger>
+        ))}
       </div>
     </div>
   </div>
