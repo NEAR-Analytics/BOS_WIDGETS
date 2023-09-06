cd09ac528df539db8b5c3c165664fab9eb7f3bed
sharddog.near <sharddog.near>
2023-09-06 16:52:43 +0000

    Update LocationCheck by sharddog.near at 2023-09-06T16:52:43.894Z

---
+++ sharddog.near/LocationCheck/source_code.jsx
@@ -0,0 +1,83 @@
+const latRange = props.latRange;
+const lonRange = props.lonRange;
+const locationName = props.locationName;
+const IPAPI_TOKEN = props.IPAPI_TOKEN;
+
+State.init({
+  locationStatus: "UNDETERMINED", // UNDETERMINED, IN_LOCATION, OUTSIDE_LOCATION
+  message: "",
+});
+
+const checkIfInLocation = (lat, lon) => {
+  if (
+    lat >= latRange[0] &&
+    lat <= latRange[1] &&
+    lon >= lonRange[0] &&
+    lon <= lonRange[1]
+  ) {
+    State.update({ locationStatus: "IN_LOCATION", message: "" });
+  } else {
+    State.update({
+      locationStatus: "OUTSIDE_LOCATION",
+      message: `Sorry, you are not in ${locationName}.`,
+    });
+  }
+};
+
+const requestLocation = () => {
+   console.log("Geolocation"); 
+  if ("geolocation" in navigator) {
+    console.log("Geolocation is available in this browser."); 
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+        checkIfInLocation(lat, lon);
+      },
+      (error) => {
+        if (error.code === error.PERMISSION_DENIED) {
+          // Use IP-based geolocation as a fallback
+          fetch(`https://pro.ip-api.com/json/?key=${IPAPI_TOKEN}`)
+            .then((response) => response.json())
+            .then((data) => {
+              const [lat, lon] = data.loc.split(",");
+              checkIfInLocation(parseFloat(lat), parseFloat(lon));
+            })
+            .catch((err) => {
+              State.update({ message: "Unable to determine location." });
+            });
+        }
+      }
+    );
+  } else {
+      console.log("Geolocation is not available in this browser."); 
+    State.update({
+      message: "Geolocation is not supported by this browser.",
+    });
+  }
+};
+
+return (
+  <div>
+    {state.locationStatus === "UNDETERMINED" ? (
+      <div id="overlay">
+        <div id="location-explainer">
+          <p>
+            We request your location to ensure you're in {locationName} in order
+            to claim this. We respect your privacy and won't share or save your
+            location.
+          </p>
+          <button onClick={requestLocation}>Allow Location</button>
+          <p>{state.message}</p>
+        </div>
+      </div>
+    ) : (
+      <div id="main-content">
+        {state.locationStatus === "IN_LOCATION"  (
+          <p>{state.message}</p>
+        )}
+      </div>
+    )}
+  </div>
+);
