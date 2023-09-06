02c82d6a7b4cbffd74220520b81f6855f620a4e6
abhishekanirudhan.near <abhishekanirudhan.near>
2023-09-06 05:57:45 +0000

    Update NEAR.ATLAS.BDS_NFT_PROJECTS by abhishekanirudhan.near at 2023-09-06T05:57:45.047Z

--- abhishekanirudhan.near/NEAR.ATLAS.BDS_NFT_PROJECTS/source_code.jsx
+++ abhishekanirudhan.near/NEAR.ATLAS.BDS_NFT_PROJECTS/source_code.jsx
@@ -9,10 +9,37 @@ let rawData = fetch(
   }
 );
 
+State.init({
+  setSortConfig: { key: "", direction: "asc" },
+  currentPage: 1,
+});
+
+function getNodesForCurrentPage(allNodes) {
+  const startIndex = (state.currentPage - 1) * 20;
+  const endIndex = startIndex + 20;
+  return allNodes.slice(startIndex, endIndex);
+}
+
+function nextPage() {
+  if (state.currentPage < Math.ceil(sortedNodes.length / 10)) {
+    State.update({ currentPage: state.currentPage + 1 });
+  }
+}
+
+function previousPage() {
+  if (state.currentPage > 1) {
+    State.update({ currentPage: state.currentPage - 1 });
+  }
+}
+
 let Style = styled.div`
-
-          `;
-
+  .table-header {
+    background-color: #000000; /* Set this to the desired dark color for the header */
+    color: #ffffff;
+    text-align: center;
+    font-weight: 600;
+  }
+`;
 let nodes = rawData.body || [];
 
 function formatPercentNew(text) {
@@ -68,30 +95,44 @@ const data = {
 
 const COLUMNS = [
   {
-    label: <p className="text-center text-white text-wrap">Project Name</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap">
+        Project Name
+      </p>
+    ),
     renderCell: (item) => formatCell(item["Project Name"]),
     sort: { sortKey: "Project Name" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">DAU</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        DAU
+      </p>
+    ),
     renderCell: (item) => formatNumber(item["DAU"]),
     sort: { sortKey: "DAU" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">MAU</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        MAU
+      </p>
+    ),
     renderCell: (item) => formatNumber(item["MAU"]),
     sort: { sortKey: "MAU" },
   },
   {
     label: (
-      <p className="text-center text-white text-wrap ">Accounts Created</p>
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        Accounts Created
+      </p>
     ),
     renderCell: (item) => formatNumber(item["Accounts Created"]),
     sort: { sortKey: "Accounts Created" },
   },
   {
     label: (
-      <p className="text-center text-white text-wrap ">
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
         Accounts Created - Past 30 Days
       </p>
     ),
@@ -99,18 +140,26 @@ const COLUMNS = [
     sort: { sortKey: "Accounts Created - 30d" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">NFT Mints</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        NFT Mints
+      </p>
+    ),
     renderCell: (item) => formatNumber(item["NFT Mints"]),
     sort: { sortKey: "NFT Mints" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">Unique Minters</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        Unique Minters
+      </p>
+    ),
     renderCell: (item) => formatNumber(item["Unique Minters"]),
     sort: { sortKey: "Unique Minters" },
   },
   {
     label: (
-      <p className="text-center text-white text-wrap ">
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
         NFT Mints - Past 30 Days
       </p>
     ),
@@ -119,7 +168,7 @@ const COLUMNS = [
   },
   {
     label: (
-      <p className="text-center text-white text-wrap ">
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
         Unique Minters - Past 30 Days
       </p>
     ),
@@ -127,46 +176,149 @@ const COLUMNS = [
     sort: { sortKey: "Unique Minters - 30d" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">Transactions</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        Transactions
+      </p>
+    ),
     renderCell: (item) => formatNumber(item["TXNS"]),
     sort: { sortKey: "TXNS" },
   },
   {
-    label: <p className="text-center text-white text-wrap ">Transactions</p>,
+    label: (
+      <p className="text-center text-white text-sm font-semibold p-2 text-wrap ">
+        Transactions
+      </p>
+    ),
     renderCell: (item) => formatNumber(item["TXNS_30D"]),
     sort: { sortKey: "TXNS_30D" },
   },
 ];
-const sortFns = {
-  ContractAddress: (array) =>
-    array.sort((a, b) => a["Project Name"].localeCompare(b["Project Name"])),
-  DAU: (array) => array.sort((a, b) => a["DAU"] - b["DAU"]),
-  MAU: (array) => array.sort((a, b) => a["MAU"] - b["MAU"]),
-  "Accounts Created": (array) =>
-    array.sort((a, b) => a["Accounts Created"] - b["Accounts Created"]),
-  "Accounts Created - 30d": (array) =>
-    array.sort(
-      (a, b) => a["Accounts Created - 30d"] - b["Accounts Created - 30d"]
-    ),
-  "NFT Mints": (array) => array.sort((a, b) => a["NFT Mints"] - b["NFT Mints"]),
-  "Unique Minters": (array) =>
-    array.sort((a, b) => a["Unique Minters"] - b["Unique Minters"]),
-  "NFT Mints - 30d": (array) =>
-    array.sort((a, b) => a["NFT Mints - 30d"] - b["NFT Mints - 30d"]),
-  "Unique Minters - 30d": (array) =>
-    array.sort((a, b) => a["Unique Minters - 30d"] - b["Unique Minters - 30d"]),
-  TXNS: (array) => array.sort((a, b) => a["TXNS"] - b["TXNS"]),
-  TXNS_30d: (array) => array.sort((a, b) => a["TXNS_30d"] - b["TXNS_30d"]),
-};
+
+function sort_update(sortKey, direction) {
+  // Update the sort state
+  State.update({
+    setSortConfig: { key: sortKey, direction },
+  });
+}
+function getSortedNodes() {
+  // Get the current sort configuration from the state
+  const sortConfig = state.setSortConfig;
+
+  // Check if there's a sort key and direction
+  if (sortConfig && sortConfig.key && sortConfig.direction) {
+    const sortedNodes = [...(nodes || [])];
+
+    sortedNodes.sort((a, b) => {
+      // Check if the data type is string
+      if (typeof a[sortConfig.key] === "string") {
+        return sortConfig.direction === "asc"
+          ? a[sortConfig.key].localeCompare(b[sortConfig.key])
+          : b[sortConfig.key].localeCompare(a[sortConfig.key]);
+      } else {
+        // For other data types (numbers, dates, etc.)
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "asc" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "asc" ? 1 : -1;
+        }
+      }
+      return 0; // If values are equal
+    });
+
+    return sortedNodes;
+  }
+
+  // If no sort configuration, return original nodes
+  return nodes;
+}
+
+// Function to get the top 100 sorted nodes based on TRANSFERS_3D
+function getTop100SortedNodes() {
+  // First, ensure nodes is defined and is an array
+  if (!nodes || !Array.isArray(nodes)) {
+    return [];
+  }
+
+  // Sort based on TRANSFERS_3D in descending order
+  const sortedNodes = [...nodes].sort((a, b) => {
+    return b["DAU"] - a["DAU"];
+  });
+
+  // Return top 100 items after sorting
+  return sortedNodes.slice(0, 100);
+}
+
+const sortedNodes = getSortedNodes();
+const nodesForRendering = getNodesForCurrentPage(sortedNodes);
 
 return (
-  <div className="text-bg-dark rounded-4 mb-12">
+  <div className="bg-dark rounded-lg mb-12 overflow-hidden w-full">
     {data !== null ? (
-      <div Style={{ "min-width": "780px" }} className="bg-dark">
-        <BasicTable columns={COLUMNS} data={data} sortFns={sortFns} />
+      <div
+        style={{ "min-width": "780px" }}
+        className="bg-dark w-full overflow-x-auto overflow-y-auto"
+      >
+        <table className="table-auto w-full overflow-scroll">
+          <thead className="bg-gray-700">
+            <tr>
+              {COLUMNS.map((column) => (
+                <th
+                  className="text-white text-center font-semibold p-2 border-b border-gray-300 cursor-pointer"
+                  onClick={() => {
+                    const direction =
+                      state.setSortConfig.direction === "asc" ? "desc" : "asc";
+                    sort_update(column.sort.sortKey, direction);
+                  }}
+                >
+                  {column.label}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {nodesForRendering.map((row, rowIndex) => (
+              <tr
+                className={rowIndex % 2 === 0 ? "bg-gray-800" : "bg-gray-900"}
+              >
+                {COLUMNS.map((column) => (
+                  <td
+                    className={`text-white text-center p-2 ${
+                      column.sort.sortKey === "Project Name" ? "max-w-xs" : ""
+                    }`}
+                  >
+                    {column.renderCell(row)}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="px-5 py-5 bg-gray-800 border-t flex flex-col xs:flex-row items-center xs:justify-between">
+          <span className="text-xs xs:text-sm text-white">
+            Showing {(state.currentPage - 1) * 10 + 1} to
+            {Math.min(state.currentPage * 10, sortedNodes.length)} of{" "}
+            {sortedNodes.length} Entries
+          </span>
+          <div className="inline-flex mt-2 xs:mt-0">
+            <button
+              onClick={previousPage}
+              className="text-sm bg-gray-700 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded-l"
+            >
+              Prev
+            </button>
+            <button
+              onClick={nextPage}
+              className="text-sm bg-gray-700 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded-r"
+            >
+              Next
+            </button>
+          </div>
+        </div>
       </div>
     ) : (
-      <div>Loading ...</div>
+      <div className="text-white text-center p-4">Loading ...</div>
     )}
   </div>
 );
