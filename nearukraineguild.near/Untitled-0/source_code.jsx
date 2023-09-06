aad58d400b4fb7c3c20522d031e17de24a9b324d
nearukraineguild.near <nearukraineguild.near>
2023-09-06 11:38:04 +0000

    Update Untitled-0 by nearukraineguild.near at 2023-09-06T11:38:04.501Z

--- nearukraineguild.near/Untitled-0/source_code.jsx
+++ nearukraineguild.near/Untitled-0/source_code.jsx
@@ -1,197 +1,160 @@
-const TableWrapper = styled.div`
-  overflow-x: auto;
-  max-width: 100%;
-`;
+const baseApi = "https://api.pikespeak.ai";
 
-const Table = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-`;
-
-const TableRow = styled.tr`
-  border-bottom: 1px solid #ddd;
-`;
-
-const TableCell = styled.td`
-  padding: 8px;
-  white-space: nowrap;
-  text-align: left;
-
-  @media (max-width: 768px) {
-    display: block;
-    padding: 6px;
-    font-size: 14px;
-    text-align: left;
-  }
-`;
-
-const CandidateName = styled.span`
-  color: #007bff;
-  text-decoration: underline;
-  cursor: pointer;
-`;
-
-const VotesTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  margin-top: 16px;
-`;
-
-const VotesTableRow = styled.tr`
-  border-bottom: 1px solid #ddd;
-`;
-
-const VotesTableCell = styled.td`
-  padding: 8px;
-  text-align: left;
-`;
-
-const candidates = Near.view("rc-dao.sputnik-dao.near", "get_proposals", {
-  from_index: 911,
-  limit: 26,
-});
-console.log("candidates", candidates);
-
-const extractCandidateInfo = (description) => {
-  const startIndex = description.indexOf("Candidate:") + 11;
-  const endIndex = description.indexOf("(");
-  const candidateName = description.substring(startIndex, endIndex).trim();
-  const nameWithoutLink = candidateName.replace(/ *\([^)]*\) */g, "");
-  const link = description
-    .substring(endIndex + 1, description.length - 1)
-    .trim();
-
-  return {
-    candidateName: nameWithoutLink,
-    link,
-  };
+const apiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
+const nominationContract = "nominations.ndc-gwg.near";
+const httpRequestOpt = {
+  headers: { "x-api-key": apiKey },
 };
 
-const europe = [];
-const asia = [];
-const afrika = [];
-const northAmerica = [];
-const southAmerica = [];
-const avb = [];
+const fetchData = (account) => {
+  const res = fetch(
+    `https://api.pikespeak.ai/account/tx-count/${account}`,
+    httpRequestOpt
+  );
 
-State.init({ selectedCandidate: null });
-
-candidates.map((candidate) => {
-  let votesCount = 0;
-  const keys = Object.keys(candidate.votes);
-
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    votesCount++;
-  }
-
-  // Check if the description contains the name of a continent
-
-  const candidateName = extractCandidateInfo(candidate.description);
-  const updatedCandidate = {
-    ...candidate,
-    votesCount,
-    ...candidateName,
-  };
-  const description = candidate.description.toLowerCase();
-  if (description.includes("europe")) {
-    europe.push(updatedCandidate);
-  } else if (description.includes("asia")) {
-    asia.push(updatedCandidate);
-  } else if (description.includes("africa")) {
-    afrika.push(updatedCandidate);
-  } else if (description.includes("north america")) {
-    northAmerica.push(updatedCandidate);
-  } else if (description.includes("south america")) {
-    southAmerica.push(updatedCandidate);
-  } else {
-    avb.push(updatedCandidate);
-  }
-  return;
-});
-
-const handleCandidateClick = (candidate) => {
-  State.update({ selectedCandidate: candidate });
+  return res.body;
 };
 
-console.log(selectedCandidate);
+const Input = styled.input`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0.5em 0.75em;
+  gap: 0.5em;
+  background: #ffffff;
+  border: 1px solid #d0d5dd;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  border-radius: 4px;
+`;
 
-const renderVotesTable = () => {
-  if (!selectedCandidate) {
-    return null;
+const LabelArea = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 0.25em;
+  margin-bottom: 0.5em;
+  flex-wrap: wrap;
+`;
+
+const Error = styled.small`
+  color: red;
+`;
+
+State.init({
+  valid: true,
+  accountId: "",
+  errorMessage: <></>,
+});
+
+const validate = (accountId) => {
+  const accountIdRegex =
+    /^(([a-z\d]+[\-_])*[a-z\d]+\.)*([a-z\d]+[\-_])*[a-z\d]+$/;
+
+  if (typeof accountId !== "string") {
+    State.update({
+      accountId: "",
+      valid: false,
+      errorMessage: "Account ID must be a text value!",
+    });
+    return;
   }
 
-  const votesKeys = Object.keys(selectedCandidate.votes);
+  if (accountId.length < 2) {
+    State.update({
+      accountId: "",
+      valid: false,
+      errorMessage: "Account ID must be at least 2 characters long!",
+    });
+    return;
+  }
 
+  if (accountId.length > 64) {
+    State.update({
+      accountId: "",
+      valid: false,
+      errorMessage: "Account ID must be at most 64 characters long!",
+    });
+    return;
+  }
+
+  if (!accountIdRegex.test(accountId)) {
+    State.update({
+      accountId: "",
+      valid: false,
+      errorMessage: (
+        <>
+          Account ID must follow the rules specified{" "}
+          <a href="https://nomicon.io/DataStructures/Account#account-id-rules">
+            here
+          </a>
+          and ends on .near!
+        </>
+      ),
+    });
+    return;
+  }
+
+  State.update({ valid: true, errorMessage: "", accountId });
+};
+
+const accountId = props.accountId ?? "Login with NEAR Wallet";
+
+const getFirstSBTToken = (accountId) => {
+  const view = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
+    account: `${accountId}`,
+    issuer: "fractal.i-am-human.near",
+  });
+  return view?.[0]?.[1]?.[0];
+};
+
+const hasSBTToken = getFirstSBTToken(state.accountId) !== undefined;
+const txCount = fetchData(state.accountId) !== undefined;
+
+const Card = ({ children, className }) => {
   return (
-    <VotesTable>
-      <tbody>
-        {votesKeys.map((voteKey, index) => (
-          <VotesTableRow key={index}>
-            <VotesTableCell>{voteKey}</VotesTableCell>
-          </VotesTableRow>
-        ))}
-      </tbody>
-    </VotesTable>
+    <div
+      className={`card ${className}`}
+      style={{ maxWidth: "400px", padding: "2px" }}
+    >
+      <div className="card-body p-2">{children}</div>
+    </div>
   );
 };
 
-const CandidateTable = ({ title, candidates }) => (
-  <div>
-    <h2>{title}</h2>
-    <TableWrapper>
-      <Table>
-        <tbody>
-          {candidates.map((candidate, index) => (
-            <TableRow
-              key={index}
-              onClick={() => handleCandidateClick(candidate)}
-            >
-              <TableCell>
-                <CandidateName>
-                  <a
-                    href={candidate.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {candidate.candidateName}
-                  </a>
-                </CandidateName>
-              </TableCell>
-              <TableCell>Votes: {candidate.votesCount}</TableCell>
-            </TableRow>
-          ))}
-        </tbody>
-      </Table>
-    </TableWrapper>
-  </div>
-);
-
-const sortedEurope = europe.slice().sort((a, b) => b.votesCount - a.votesCount);
-const sortedAsia = asia.slice().sort((a, b) => b.votesCount - a.votesCount);
-const sortedAfrika = afrika.slice().sort((a, b) => b.votesCount - a.votesCount);
-const sortedNorthAmerica = northAmerica
-  .slice()
-  .sort((a, b) => b.votesCount - a.votesCount);
-const sortedSouthAmerica = southAmerica
-  .slice()
-  .sort((a, b) => b.votesCount - a.votesCount);
-const sortedAvb = avb.slice().sort((a, b) => b.votesCount - a.votesCount);
+const CardTitle = ({ children, className }) => {
+  return <p className={`card-title ${className}`}>{children}</p>;
+};
 
 return (
-  <div>
-    {renderVotesTable()}
-    <CandidateTable title="Europe Candidates" candidates={sortedEurope} />
-    <CandidateTable title="Asia Candidates" candidates={sortedAsia} />
-    <CandidateTable title="Africa Candidates" candidates={sortedAfrika} />
-    <CandidateTable
-      title="North America Candidates"
-      candidates={sortedNorthAmerica}
-    />
-    <CandidateTable
-      title="South America Candidates"
-      candidates={sortedSouthAmerica}
-    />
-    <CandidateTable title="Available Candidates" candidates={sortedAvb} />
+  <div className="d-flex flex-column align-items-center">
+    <p>
+      Type the account which you want to validate 'XXX.near' and it will check
+      automatically
+    </p>
+    <LabelArea>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <Input
+          id
+          type="text"
+          value={v}
+          onChange={(e) => validate(e.target.value)}
+          className="w-100"
+          style={{ maxWidth: "200px" }}
+        />
+        <Error>{state.valid ? <></> : state.errorMessage}</Error>
+      </div>
+    </LabelArea>
+    <div>Transactions count{txCount}</div>
+    {hasSBTToken ? (
+      <Card className="border-success">
+        <CardTitle className="text-success m-0">Verified</CardTitle>
+      </Card>
+    ) : (
+      <Card className="border-danger">
+        <CardTitle className="text-danger m-0">Not Verified</CardTitle>
+      </Card>
+    )}
   </div>
 );
