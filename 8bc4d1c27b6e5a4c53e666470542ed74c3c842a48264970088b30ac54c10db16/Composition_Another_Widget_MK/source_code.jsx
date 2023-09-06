365f0731b8e2b77ce7914555684de6b1f94923f3
8bc4d1c27b6e5a4c53e666470542ed74c3c842a48264970088b30ac54c10db16 <8bc4d1c27b6e5a4c53e666470542ed74c3c842a48264970088b30ac54c10db16>
2023-09-06 10:36:05 +0000

    Update Composition_Another_Widget_MK by 8bc4d1c27b6e5a4c53e666470542ed74c3c842a48264970088b30ac54c10db16 at 2023-09-06T10:36:05.361Z

---
+++ 8bc4d1c27b6e5a4c53e666470542ed74c3c842a48264970088b30ac54c10db16/Composition_Another_Widget_MK/source_code.jsx
@@ -0,0 +1,11 @@
+const user = "8bc4d1c27b6e5a4c53e666470542ed74c3c842a48264970088b30ac54c10db16";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+
+    <Widget src={`${user}/widget/harmonicHomePage`} props={props} />
+  </>
+);
