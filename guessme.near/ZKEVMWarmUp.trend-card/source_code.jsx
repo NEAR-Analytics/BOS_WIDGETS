98492a6dcfc8ff88d84dc24b7287d9322057d5f5
guessme.near <guessme.near>
2023-09-06 09:03:40 +0000

    Update ZKEVMWarmUp.trend-card by guessme.near at 2023-09-06T09:03:40.633Z

--- guessme.near/ZKEVMWarmUp.trend-card/source_code.jsx
+++ guessme.near/ZKEVMWarmUp.trend-card/source_code.jsx
@@ -61,6 +61,10 @@ const Wrapper = styled.div`
     }
   }
 
+  .trend-card-execute-mobile {
+    display: none;
+  }
+
   .trend-card-execute-date {
     font-size: 14px;
     font-weight: 400;
@@ -74,10 +78,85 @@ const Wrapper = styled.div`
     justify-content: space-between;
     align-items: center;
   }
+
+  @media (max-width: 900px) {
+    .trend-card {
+      width: 100%;
+      height: auto;
+      align-items: start;
+      padding: 12px 14px;
+      position: relative;
+      .trend-card-text {
+        height: auto;
+        margin-bottom: 6px;
+        text-align: left;
+        font-size: 16px;
+      }
+      .trend-card-dapp-name {
+        display: inline-block;
+        font-size: 14px;
+      }
+      .trend-card-execute-mobile {
+        display: inline-block;
+        color: #979abe;
+        font-size: 12px;
+        position: absolute;
+        right: 14px;
+        bottom: 12px;
+      }
+    }
+    .trend-card-execute-date {
+      display: none;
+    }
+  }
 `;
 
 const iconMap = {
   "ZkEvm-bridge": (
+    <svg
+      width="26"
+      height="26"
+      viewBox="0 0 26 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="trend-card-dapp-name-icon"
+    >
+      <rect
+        width="26"
+        height="26"
+        rx="8"
+        transform="matrix(1 0 0 -1 0 26)"
+        fill="url(#paint0_linear_0_141)"
+      />
+      <path
+        d="M8.47059 8.5V6.82609L7.80227 6.41856C7.48913 6.22761 7.0966 6.2234 6.77944 6.40759L4.4978 7.73264C4.18964 7.91161 4 8.24104 4 8.5974V11.3067C4 11.6443 4.17038 11.9591 4.45305 12.1438L6.77804 13.6628C7.09445 13.8695 7.50037 13.8802 7.8272 13.6904L10.0904 12.3761C10.3986 12.1971 10.5882 11.8677 10.5882 11.5113V6.45334C10.5882 6.09264 10.7825 5.75989 11.0966 5.58255L12.184 4.96859C12.5053 4.78718 12.9004 4.79744 13.2118 4.99527L14.1244 5.57495C14.4133 5.75842 14.5882 6.07686 14.5882 6.41906V7.47225C14.5882 7.81445 14.4133 8.13288 14.1244 8.31636L13.187 8.91181C12.8888 9.10123 12.5127 9.11924 12.1978 8.9592L11.2941 8.5V10.1739L12.221 10.6449C12.5233 10.7985 12.8829 10.7885 13.1762 10.6182L15.5022 9.26736C15.8104 9.08839 16 8.75896 16 8.4026V5.4887C16 5.13234 15.8104 4.80291 15.5022 4.62395L13.1912 3.28185C12.8898 3.10679 12.5189 3.10133 12.2125 3.26744L9.69994 4.62931C9.37738 4.80414 9.17647 5.14157 9.17647 5.50847V10.5809C9.17647 10.9231 9.00149 11.2416 8.71264 11.4251L7.80004 12.0047C7.48859 12.2026 7.0935 12.2128 6.77221 12.0314L5.68481 11.4175C5.37072 11.2401 5.17647 10.9074 5.17647 10.5467V9.56203C5.17647 9.20133 5.37072 8.86859 5.68481 8.69124L6.78299 8.0712C7.09857 7.89302 7.48585 7.89952 7.79527 8.0882L8.47059 8.5Z"
+        fill="white"
+      />
+      <path d="M18 19L14 16.6244L18 10V19Z" fill="white" />
+      <path d="M18 23L14 17L18 19.5082V23Z" fill="white" />
+      <path d="M18 19L22 16.6244L18 10V19Z" fill="white" />
+      <path d="M18 23L22 17L18 19.5082V23Z" fill="white" />
+      <path
+        d="M7 15C7 19 11.5 21 14 19.6947"
+        stroke="white"
+        stroke-width="1.5"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_0_141"
+          x1="13"
+          y1="0"
+          x2="13"
+          y2="26"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#982ECC" />
+          <stop offset="1" stop-color="#7C3FE4" />
+        </linearGradient>
+      </defs>
+    </svg>
+  ),
+  "zkEVM-bridge": (
     <svg
       width="26"
       height="26"
@@ -191,49 +270,32 @@ const iconMap = {
       src="https://repository-images.githubusercontent.com/440462673/6872d684-f7ed-463c-9a5c-76542eddbcc4"
     />
   ),
-  "native bridge": (
-    <svg
-      width="26"
-      height="26"
-      viewBox="0 0 26 26"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+
+  "0vix Lending": (
+    <img
       className="trend-card-dapp-name-icon"
-    >
-      <rect
-        width="26"
-        height="26"
-        rx="8"
-        transform="matrix(1 0 0 -1 0 26)"
-        fill="url(#paint0_linear_0_141)"
-      />
-      <path
-        d="M8.47059 8.5V6.82609L7.80227 6.41856C7.48913 6.22761 7.0966 6.2234 6.77944 6.40759L4.4978 7.73264C4.18964 7.91161 4 8.24104 4 8.5974V11.3067C4 11.6443 4.17038 11.9591 4.45305 12.1438L6.77804 13.6628C7.09445 13.8695 7.50037 13.8802 7.8272 13.6904L10.0904 12.3761C10.3986 12.1971 10.5882 11.8677 10.5882 11.5113V6.45334C10.5882 6.09264 10.7825 5.75989 11.0966 5.58255L12.184 4.96859C12.5053 4.78718 12.9004 4.79744 13.2118 4.99527L14.1244 5.57495C14.4133 5.75842 14.5882 6.07686 14.5882 6.41906V7.47225C14.5882 7.81445 14.4133 8.13288 14.1244 8.31636L13.187 8.91181C12.8888 9.10123 12.5127 9.11924 12.1978 8.9592L11.2941 8.5V10.1739L12.221 10.6449C12.5233 10.7985 12.8829 10.7885 13.1762 10.6182L15.5022 9.26736C15.8104 9.08839 16 8.75896 16 8.4026V5.4887C16 5.13234 15.8104 4.80291 15.5022 4.62395L13.1912 3.28185C12.8898 3.10679 12.5189 3.10133 12.2125 3.26744L9.69994 4.62931C9.37738 4.80414 9.17647 5.14157 9.17647 5.50847V10.5809C9.17647 10.9231 9.00149 11.2416 8.71264 11.4251L7.80004 12.0047C7.48859 12.2026 7.0935 12.2128 6.77221 12.0314L5.68481 11.4175C5.37072 11.2401 5.17647 10.9074 5.17647 10.5467V9.56203C5.17647 9.20133 5.37072 8.86859 5.68481 8.69124L6.78299 8.0712C7.09857 7.89302 7.48585 7.89952 7.79527 8.0882L8.47059 8.5Z"
-        fill="white"
-      />
-      <path d="M18 19L14 16.6244L18 10V19Z" fill="white" />
-      <path d="M18 23L14 17L18 19.5082V23Z" fill="white" />
-      <path d="M18 19L22 16.6244L18 10V19Z" fill="white" />
-      <path d="M18 23L22 17L18 19.5082V23Z" fill="white" />
-      <path
-        d="M7 15C7 19 11.5 21 14 19.6947"
-        stroke="white"
-        stroke-width="1.5"
-      />
-      <defs>
-        <linearGradient
-          id="paint0_linear_0_141"
-          x1="13"
-          y1="0"
-          x2="13"
-          y2="26"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#982ECC" />
-          <stop offset="1" stop-color="#7C3FE4" />
-        </linearGradient>
-      </defs>
-    </svg>
+      src="https://ipfs.near.social/ipfs/bafkreigyodedyhiqmstq3g5edcqw25yyari4y3rcbsnqtxldb2zb2vpah4"
+    />
+  ),
+
+  "0vix": (
+    <img
+      className="trend-card-dapp-name-icon"
+      src="https://ipfs.near.social/ipfs/bafkreigyodedyhiqmstq3g5edcqw25yyari4y3rcbsnqtxldb2zb2vpah4"
+    />
+  ),
+
+  Gamma: (
+    <img
+      className="trend-card-dapp-name-icon"
+      src="https://ipfs.near.social/ipfs/bafkreial4i3eb5uuxkhecn7nwos76km3qvb7jzxmups57rkxizr5i7dyaa"
+    />
+  ),
+  "native bridge": (
+    <img
+      className="trend-card-dapp-name-icon"
+      src="https://ipfs.near.social/ipfs/bafkreigawbz26l7mhfewlxwnjomos6njdkchnfnw2dnb6xtzf7j2t6jdxm"
+    />
   ),
   "Pancake Swap": (
     <img
@@ -272,20 +334,33 @@ const isSwap = arr[0].toLowerCase() === "swap";
 const isBorrow = arr[0].toLowerCase() === "borrow";
 const isRepay = arr[0].toLowerCase() === "repay";
 const isSupply = arr[0].toLowerCase() === "supply";
+const isWithdraw = arr[0].toLowerCase() === "withdraw";
+
+const isDeposit = arr[0].toLowerCase() === "deposit";
+
+const isWithdrawGamma = isWithdraw && arr?.[3]?.toLowerCase() === "gamma";
 
 if (isBridge) {
   link = "/guessme.near/widget/ZKEVMSwap.zkevm-bridge";
 }
 if (isSwap) {
-  link = "/guessme.near/widget/ZKEVMSwap.zkevm-swap";
+  link = "/guessme.near/widget/ZKEVMSwap.zkevm-swap?source=trend";
 }
 
-if (isSupply) {
-  link = "/guessme.near/widget/ZKEVM.AAVE";
+if (isDeposit) {
+  link = "/guessme.near/widget/ZKEVM.GAMMA?tab=deposit";
+}
+
+if (isWithdrawGamma) {
+  link = "/guessme.near/widget/ZKEVM.GAMMA?tab=withdraw";
+}
+
+if (isSupply || (isWithdraw && !isWithdrawGamma)) {
+  link = "/bluebiu.near/widget/0vix.Lending?tab=supply";
 }
 
 if (isBorrow || isRepay) {
-  link = "/guessme.near/widget/ZKEVM.AAVE?tab=borrow";
+  link = "/bluebiu.near/widget/0vix.Lending?tab=borrow";
 }
 
 const onSaveParams = () => {
@@ -302,7 +377,6 @@ const onSaveParams = () => {
     const [action_type, amount, symbol, on, dexName1, dexName2] = arr;
 
     const token = SwapTokens.find((item) => item.symbol === symbol);
-    console.log("token: ", token);
 
     Storage.set("zk-evm-swap-params", {
       amount,
@@ -333,6 +407,10 @@ return (
         <div className="trend-card-dapp-name">
           {" "}
           {iconMap[item.template]} {item.template}
+        </div>
+
+        <div className="trend-card-execute-mobile">
+          {item.count_number} times
         </div>
       </a>
     </div>
