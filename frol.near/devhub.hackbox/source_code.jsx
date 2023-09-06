95a57f189fe8f1a3da697581aa9f79bfe7c17bb8
frol.near <frol.near>
2023-09-06 07:22:48 +0000

    Update devhub.hackbox by frol.near at 2023-09-06T07:22:48.314Z

--- frol.near/devhub.hackbox/source_code.jsx
+++ frol.near/devhub.hackbox/source_code.jsx
@@ -1,14 +1,11 @@
 return (
-  <div className="d-flex flex-column w-100 h-100" style={{ minHeight: "86vh" }}>
-    <iframe
-      iframeResizer
-      frameborder="0"
-      // width and minWidth required by iframeResizer
-      style={{
-        width: "1px",
-        minWidth: "100%",
-      }}
-      src="https://e.notionhero.io/e1/p/b3d90b9-5c12664e48e2e6116e4f6fc41e40dac"
-    />
-  </div>
+  <iframe
+    frameborder="0"
+    style={{
+      width: "1px",
+      minWidth: "100%",
+      height: "calc(100vh - 72px)",
+    }}
+    src="https://the-hackbox.super.site/"
+  />
 );
