bc140d3ec0f82981a17fb61576c0b2483ddc93e1
abhishekanirudhan.near <abhishekanirudhan.near>
2023-09-06 05:55:29 +0000

    Update NEART.ATLAS.BDS_DETAILED_PROJECT_DASHBOARD by abhishekanirudhan.near at 2023-09-06T05:55:29.631Z

--- abhishekanirudhan.near/NEART.ATLAS.BDS_DETAILED_PROJECT_DASHBOARD/source_code.jsx
+++ abhishekanirudhan.near/NEART.ATLAS.BDS_DETAILED_PROJECT_DASHBOARD/source_code.jsx
@@ -2,6 +2,7 @@ const initialState = {
   selectedMetric: "MAU",
   processedData: processedData,
   metric_period: "Monthly",
+  project_name: props.project_name || "Sweat Economy",
 };
 
 state = State.init(initialState);
@@ -133,11 +134,11 @@ if (!finalData) {
   return <h1> 🪄 Loading MAGIC 🪄</h1>;
 }
 
-const project_name = props.project_name || "Sweat Economy";
+// const project_name = props.project_name || "Sweat Economy";
 
-const METRIC_NAME = `"${project_name}'s ${state.metric_period}"`;
+const METRIC_NAME = `"${state.project_name}'s ${state.metric_period}"`;
 
-const filteredData = filterByProjectName(finalData, project_name) || [];
+const filteredData = filterByProjectName(finalData, state.project_name) || [];
 
 const filteredSortedData = sortByActivityDate(filteredData) || [];
 
@@ -203,6 +204,27 @@ const v_bar_data = {
 
 const v_bar_options = {
   responsive: true,
+  scales: {
+    y: {
+      stacked: true,
+      grid: {
+        color: "rgb(41,51,64)", // This will change the gridline color
+        borderColor: "rgb(240,255,240)",
+      },
+      ticks: {
+        color: "rgb(240,255,240)", // This will change the axis text label color
+      },
+    },
+    x: {
+      stacked: true,
+      grid: {
+        color: "rgb(41,51,64)", // This will change the gridline color
+      },
+      ticks: {
+        color: "rgb(240,255,240)", // This will change the axis text label color
+      },
+    },
+  },
   plugins: {
     legend: {
       position: "top",
@@ -217,6 +239,28 @@ const v_bar_options = {
 // ...Other code...
 
 // logic part-3
+
+// 1. Create a function to extract unique project names:
+function getUniqueProjectNames(data) {
+  const projectNamesSet = new Set();
+  data.forEach((item) => {
+    if (item.PROJECT_NAME) {
+      projectNamesSet.add(item.PROJECT_NAME);
+    }
+  });
+  return [...projectNamesSet];
+}
+
+// 2. Get the unique project names from `finalData`:
+const uniqueProjectNames = getUniqueProjectNames(finalData);
+
+// 3. Create a handler for the project name dropdown change:
+const handleProjectDropdownChange = (e) => {
+  // console.log("selectedProject:", e.target.value);
+  State.update({
+    project_name: e.target.value,
+  });
+};
 
 const handleDropdownChange = (e) => {
   console.log("selectedMetric:", e.target.value);
@@ -260,19 +304,25 @@ const getBarData = () => {
 
 return (
   <Style>
-    <div className="text-bg-light rounded-4 p-3 mb-4">
+    <div className="bg-dark container rounded-4 p-3 mb-4">
       {data !== null ? (
         <p>
           <div class="">
             <div class="">
               <div>
-                <h2 className="text-black">Metric: {METRIC_NAME}</h2>
+                <h2 className="text-white">Metric: {METRIC_NAME}</h2>
 
-                <label htmlFor="metric-dropdown">Select metric: </label>
+                <label
+                  htmlFor="metric-dropdown"
+                  className="text-white block text-sm font-medium"
+                >
+                  Select metric:{" "}
+                </label>
                 <select
                   id="metric-dropdown"
                   value={selectedMetric}
                   onChange={handleDropdownChange}
+                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring focus:border-blue-300 sm:text-sm rounded-md"
                 >
                   <option value="DAA">DAA</option>
                   <option value="WAU">WAU</option>
@@ -282,7 +332,29 @@ return (
                   <option value="PERCENT_NEW">Percent New Accounts</option>
                   <option value="STICKINESS">Stickiness</option>
                 </select>
-
+                <label
+                  htmlFor="project-dropdown"
+                  htmlFor="metric-dropdown"
+                  className="text-white block text-sm font-medium"
+                >
+                  Select Project:{" "}
+                </label>
+                <select
+                  id="project-dropdown"
+                  value={project_name}
+                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring focus:border-blue-300 sm:text-sm rounded-md"
+                  onChange={handleProjectDropdownChange}
+                >
+                  {uniqueProjectNames.map((name) => (
+                    <option
+                      key={name}
+                      value={name}
+                      selected={name === state.project_name}
+                    >
+                      {name}
+                    </option>
+                  ))}
+                </select>
                 <BarEl options={v_bar_options} data={getBarData()} />
               </div>
             </div>
