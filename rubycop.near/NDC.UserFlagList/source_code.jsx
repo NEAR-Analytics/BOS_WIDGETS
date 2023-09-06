43677ef5b96f4bdb08e9603a6166875d01ee1153
rubycop.near <rubycop.near>
2023-09-06 23:13:39 +0000

    Update NDC.UserFlagList by rubycop.near at 2023-09-06T23:13:39.447Z

---
+++ rubycop.near/NDC.UserFlagList/source_code.jsx
@@ -0,0 +1,106 @@
+State.init({
+  flagged,
+  account: "",
+});
+
+function checkAvailability() {
+  const flagged = Near.view("registry.i-am-human.near", "account_flagged", {
+    account: state.account,
+  });
+
+  State.update({ flagged });
+}
+
+State.update({ account: state.account || context.accountId });
+
+const H3 = styled.h3`
+  text-transform: capitalize;
+`;
+
+const Container = styled.div`
+  flex-direction: row;
+
+  @media (max-width: 1200px) {
+    flex-direction: column;
+  }
+`;
+
+const Section = styled.div`
+  background: #fdfeff;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  padding: 20px;
+  width: 400px;
+  font-size: 14px;
+
+  @media (max-width: 1200px) {
+    width: 100%;
+  }
+`;
+
+const Row = styled.div`
+  border-bottom: 1px solid #ececec;
+  padding: 5px 7px;
+  margin: 3px 0;
+  border-radius: 5px;
+  font-weight: ${(props) => (props.index < 10 ? "600" : "inherit")};
+`;
+const Header = styled.div`
+  background: black;
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const Status = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
+
+return (
+  <div className="d-flex flex-column justify-content-center w-100">
+    <Header className="d-flex p-3 px-4 align-items-center rounded gap-2">
+      <img
+        height="30px"
+        src="https://pbs.twimg.com/profile_images/1622941553839816707/nmf3MWw1_400x400.jpg"
+      />
+      <span>User status</span>
+    </Header>
+    <Container className="d-flex justify-content-center mt-3 mb-3 w-100">
+      <Section className="d-flex flex-column gap-2 align-items-center">
+        <Widget
+          src={"nearui.near/widget/Input.Text"}
+          props={{
+            label: "Check availability",
+            placeholder: "Input your wallet",
+            value: state.account,
+            onChange: (value) => State.update({ account: value }),
+          }}
+        />
+        <Widget
+          src={"nearui.near/widget/Input.Button"}
+          props={{
+            children: "Check",
+            className: "bg-primary text-white w-100 mt-2",
+            onClick: checkAvailability,
+          }}
+        />
+        <Status
+          className={`d-flex justify-content-center align-items-center p-2 mt-3 rounded-circle bg-${
+            state.flagged === "Blacklisted"
+              ? "black text-danger"
+              : state.flagged === "Verified"
+              ? "bg-white"
+              : "secondary text-white"
+          }`}
+        >
+          <div className="text-center">
+            You are <br />
+            {state.flagged ?? "Greylisted"}
+          </div>
+        </Status>
+      </Section>
+    </Container>
+  </div>
+);
