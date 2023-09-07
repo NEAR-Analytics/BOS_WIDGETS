1c57588abad6d16fbce97b202c4bc2c9b435db55
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-09-07 15:34:10 +0000

    Update SayALot.UpVoteButton by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-09-07T15:34:10.812Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.UpVoteButton/source_code.jsx
@@ -0,0 +1,108 @@
+const { isTest, authorForWidget, reactedElementData, widgets } = props;
+
+const data = reactedElementData;
+
+const libSrcArray = [`${authorForWidget}/widget/SayALot.lib.upVotes`];
+
+const libCalls = [
+  {
+    functionName: "getUpVotes",
+    key: "upVotes",
+    props: {
+      realArticleId: data.realArticleId ?? `${data.author}-${data.timeCreate}`,
+    },
+  },
+];
+
+State.init({
+  libCalls,
+  upVotes: [],
+});
+
+let userVote = state.upVotes.find(
+  (vote) => vote.accountId === context.accountId
+);
+
+let hasUserVoted = userVote !== undefined;
+
+function getUpVoteButtonClass() {
+  if (hasUserVoted) {
+    return "primary";
+  } else {
+    return "secondary dark";
+  }
+}
+
+function callLibs(srcArray, stateUpdate, libCalls) {
+  return (
+    <>
+      {srcArray.map((src) => {
+        return (
+          <Widget
+            src={src}
+            props={{
+              isTest,
+              stateUpdate,
+              libCalls,
+            }}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+function stateUpdate(obj) {
+  State.update(obj);
+}
+
+function upVoteButtonListener() {
+  let newLibCalls = [...state.libCalls];
+
+  if (!hasUserVoted) {
+    newLibCalls.push({
+      functionName: "addVote",
+      key: "newVote",
+      props: {
+        realArticleId:
+          data.realArticleId ?? `${data.author}-${data.timeCreate}`,
+      },
+    });
+  } else {
+    newLibCalls.push({
+      functionName: "deleteVote",
+      key: "deletedVote",
+      props: {
+        realArticleId:
+          data.realArticleId ?? `${data.author}-${data.timeCreate}`,
+        upVoteId: userVote.value.upVoteId,
+      },
+    });
+  }
+  State.update({ libCalls: newLibCalls });
+}
+
+const CallLibrary = styled.div`
+  display: none;
+`;
+
+return (
+  <>
+    <Widget
+      src={widgets.styledComponents}
+      props={{
+        Button: {
+          text: `+${state.upVotes.length}`,
+          className: `${getUpVoteButtonClass()}`,
+          size: "sm",
+          onClick: upVoteButtonListener,
+          icon: <i className="bi bi-hand-thumbs-up"></i>,
+        },
+      }}
+    />
+
+    <CallLibrary>
+      {callLibs(libSrcArray, stateUpdate, state.libCalls)}
+    </CallLibrary>
+  </>
+);
