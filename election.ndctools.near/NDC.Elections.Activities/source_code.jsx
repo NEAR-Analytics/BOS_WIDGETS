449bbe7e4674ed8df4fa6dfbd595041fda50f9f5
election.ndctools.near <election.ndctools.near>
2023-09-07 15:58:48 +0000

    Update NDC.Elections.Activities by election.ndctools.near at 2023-09-07T15:58:48.589Z

--- election.ndctools.near/NDC.Elections.Activities/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Activities/source_code.jsx
@@ -58,7 +58,7 @@ const housesMapping = {
   TransparencyCommission:
     "https://bafkreihcog3rs2gj4wgwfixk6yqir7k3csyaqiqwcvm2gedlh6dlvr7ik4.ipfs.nftstorage.link",
   SetupPackage:
-    "https://bafkreidsg3gntb4grebr6rpvffhzkwdt2siel7ucl3hpsj5i7qqu426dgq.ipfs.nftstorage.link",
+    "https://ipfs.near.social/ipfs/bafkreicljooupjpwmdlja2ocjg3sljvknlq5iriahqbqiwob635l2vszqa",
 };
 
 return (
