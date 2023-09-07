a87b261fbed1e0cef9de889bb012f52cc4119214
sharddog.near <sharddog.near>
2023-09-07 05:20:34 +0000

    Update JustFeed.MainPage.Content by sharddog.near at 2023-09-07T05:20:34.177Z

--- sharddog.near/JustFeed.MainPage.Content/source_code.jsx
+++ sharddog.near/JustFeed.MainPage.Content/source_code.jsx
@@ -38,14 +38,18 @@ if (state.feedIndex === 0) {
 if (!context.accountId) {
   return (
     <>
-      <div className="tab-content col-sm-10 " style={{ textAlign: "center," }}>
+      <div className="tab-content col-sm-10 " style={{textAlign: "center" }}>
         <h1>Welcome to ShardDog Social</h1>
         <p>
           <i>The future home of the Dog Park</i>
         </p>
         A platform built on NEAR
         <br />
-        That's designed for <span style={ textDecoration="line-through"}>projects, brands, </span>ANYONE
+        That's designed for{" "}
+        <span style={{textDecoration: "line-through"}}>
+          projects, brands,{" "}
+        </span>
+        ANYONE
         <br />
         To easily connect & build their AUDIENCE
         <br />
