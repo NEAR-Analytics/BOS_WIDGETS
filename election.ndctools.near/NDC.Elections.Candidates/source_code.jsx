e9133419e7651333c291094ed77883611bfe3ebb
election.ndctools.near <election.ndctools.near>
2023-09-07 20:10:38 +0000

    Update NDC.Elections.Candidates by election.ndctools.near at 2023-09-07T20:10:38.651Z

--- election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Candidates/source_code.jsx
@@ -103,13 +103,13 @@ const CandidateItemRow = styled.div`
         : props.selected
         ? "#4aa6ee"
         : "#d4e4f461"};
-  
+  }
+
   .form-check-input:checked {
     background-color: ${(props) =>
       props.winnerId ? "#239f28" : "#0d6efd"} !important;
     border-color: ${(props) =>
       props.winnerId ? "#239f28" : "#0d6efd"} !important;
-  }
   
   @media (max-width: 400px) {
     padding: 0 10px;
