2fc44b7a4fe8a1741ee11e4c13f95cb201070b55
hackbox.near <hackbox.near>
2023-09-07 16:46:24 +0000

    Update home by hackbox.near at 2023-09-07T16:46:24.324Z

---
+++ hackbox.near/home/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <iframe
+    frameborder="0"
+    style={{
+      width: "1px",
+      minWidth: "100%",
+      height: "calc(100vh - 78px - 10px - var(--body-top-padding))",
+    }}
+    src="https://the-hackbox.super.site/"
+  />
+);
