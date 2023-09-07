816fe7d53de1b524d580d4f01f36c55142214f14
hack.near <hack.near>
2023-09-07 21:14:10 +0000

    Update CreateGroup.Form by hack.near at 2023-09-07T21:14:10.952Z

---
+++ hack.near/CreateGroup.Form/source_code.jsx
@@ -0,0 +1,162 @@
+const { typeToEmptyData, validateType, types } = props;
+
+const initialFormState = typeToEmptyData(types["hack.near/type/community"]);
+
+// Set default values here
+initialFormState.profileImage =
+  "https://ipfs.near.social/ipfs/bafkreiad5c4r3ngmnm7q6v52joaz4yti7kgsgo6ls5pfbsjzclljpvorsu";
+initialFormState.coverImage =
+  "https://ipfs.near.social/ipfs/bafkreicd7wmjfizslx72ycmnsmo7m7mnvfsyrw6wghsaseq45ybslbejvy";
+
+State.init({
+  step: 0,
+  form: initialFormState,
+  errors: null,
+});
+
+const handleStepComplete = (value) => {
+  const stepValid = true;
+  Object.keys(value).forEach((key) => {
+    const properties = types["hack.near/type/community"].properties.find(
+      (p) => p.name === key
+    );
+    const validation = validateType(properties.type, value[key], properties);
+    if (validation) {
+      State.update({
+        errors: {
+          ...state.errors,
+          [key]: validation,
+        },
+      });
+      stepValid = false;
+    } else {
+      State.update({
+        errors: {
+          ...state.errors,
+          [key]: null,
+        },
+      });
+    }
+  });
+
+  if (!stepValid) return;
+
+  if (state.step === 5) {
+    const finalAnswers = {
+      ...state.form,
+      ...value,
+    };
+
+    State.update({
+      step: state.step,
+      form: finalAnswers,
+    });
+    handleFormComplete(finalAnswers);
+    return;
+  }
+  State.update({
+    step: state.step + 1,
+    form: {
+      ...state.form,
+      ...value,
+    },
+  });
+};
+
+function generateUID() {
+  return (
+    Math.random().toString(16).slice(2) +
+    Date.now().toString(36) +
+    Math.random().toString(16).slice(2)
+  );
+}
+
+const groupId = props.groupId ?? generateUID();
+
+function handleFormComplete(value) {
+  Social.set({
+    thing: {
+      [groupId]: {
+        description:
+          typeof value.description === "string" ? value.description : "",
+      },
+    },
+    graph: {
+      [groupId]: {
+        ...state.members,
+      },
+    },
+  });
+}
+
+const steps = [
+  {
+    title: "Basics",
+    active: state.step === 0,
+    icon: state.step > 0 ? <i className="bi bi-check2"></i> : undefined,
+    className: state.step > 0 ? "active-outline" : undefined,
+  },
+  {
+    title: "Membership",
+    active: state.step === 1,
+    icon: state.step > 1 ? <i className="bi bi-check2"></i> : undefined,
+    className: state.step > 1 ? "active-outline" : undefined,
+  },
+  {
+    title: "Create",
+    active: state.step === 2,
+    icon: state.step > 2 ? <i className="bi bi-check2"></i> : undefined,
+    className: state.step > 2 ? "active-outline" : undefined,
+  },
+];
+
+return (
+  <>
+    <h1 className="h3 fw-bold mb-4">create a group</h1>
+    <Widget
+      src={`nearui.near/widget/Navigation.Steps`}
+      props={{
+        steps: steps,
+        onClick: (i) => {
+          if (i > state.step) return;
+          State.update({
+            step: i,
+          });
+        },
+      }}
+    />
+    <Widget
+      src={`hack.near/widget/CreateGroup.Step${state.step + 1}`}
+      props={{
+        formState: state.form,
+        onComplete: handleStepComplete,
+        errors: state.errors,
+        renderFooter: (stepState, otherProps) => (
+          <Widget
+            src={`hack.near/widget/CreateGroup.Footer`}
+            props={{
+              isLast: state.step >= steps.length - 1,
+              hasPrevious: state.step > 0,
+              onNext: () => {
+                handleStepComplete(stepState);
+              },
+              onPrevious: () => {
+                State.update({
+                  step: state.step - 1,
+                });
+              },
+              onReset: () => {
+                State.update({
+                  step: 0,
+                  form: initialFormState,
+                  errors: null,
+                });
+              },
+              ...otherProps,
+            }}
+          />
+        ),
+      }}
+    />
+  </>
+);
