9eb4dd7da6969272e0eea102fcea9798284ed6f5
election.ndctools.near <election.ndctools.near>
2023-09-07 19:01:20 +0000

    Update NDC.Elections.Houses by election.ndctools.near at 2023-09-07T19:01:20.415Z

--- election.ndctools.near/NDC.Elections.Houses/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Houses/source_code.jsx
@@ -1,130 +1,64 @@
-let { houses, ids, electionContract, selectedHouse, handleSelect, votesLeft } =
-  props;
-ids = ids ?? [1, 2, 3, 4];
+const { houses, votesLeft } = props;
 
-State.init({ houses: houses ?? [] });
-
-// for nominations (only 3 houses to display)
-if (!houses && electionContract) {
-  const contractHouses = [
-    Near.view(electionContract, "proposal", { prop_id: ids[0] }),
-    Near.view(electionContract, "proposal", { prop_id: ids[1] }),
-    Near.view(electionContract, "proposal", { prop_id: ids[2] }),
-  ];
-
-  State.update({ houses: contractHouses });
-}
-
-const housesMapping = {
-  CouncilOfAdvisors: {
-    title: "Council Of Advisors",
-    src: "https://bafkreidejnek5zzwlhd3lxnr7s3tvtrgul6jobfpikbs7zjkpuovxdz7je.ipfs.nftstorage.link",
-  },
-  HouseOfMerit: {
-    title: "House of Merit",
-    src: "https://bafkreihoomeeaeyqerqftn3n7yb2jrnmqtpwgpsl3xpelek6qmly3qzob4.ipfs.nftstorage.link",
-  },
-  TransparencyCommission: {
-    title: "Transparency Commission",
-    src: "https://bafkreihcog3rs2gj4wgwfixk6yqir7k3csyaqiqwcvm2gedlh6dlvr7ik4.ipfs.nftstorage.link",
-  },
-  SetupPackage: {
-    title: "Budget Package",
-    src: "https://ipfs.near.social/ipfs/bafkreicljooupjpwmdlja2ocjg3sljvknlq5iriahqbqiwob635l2vszqa",
-  },
-};
-
-const Loader = () => (
-  <span
-    className="spinner-grow spinner-grow-sm me-1"
-    role="status"
-    aria-hidden="true"
-  />
-);
-
-const Small = styled.small`
-  margin-top: 10px;
-  font-weight: 400;
-`;
-
-const H6 = styled.h6`
-  margin-top: 5px;
-  margin-bottom: 0;
-`;
-
-const ImgContainer = styled.div`
-  margin-right: 20px;
-
-  img {
-    border-radius: 50%;
-    height: 40px;
-    max-height: "40px";
-  }
-`;
-
-const CompletedIcon = styled.i`
-  border-radius: 50%;
-  padding-bottom: 0;
-  color: #239f28;
-  background: #cee9cf;
-
-  &:before {
-    vertical-align: -0.2rem;
-  }
-`;
-
-const ItemContainer = styled.div`
-  box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.05);
+const Stepper = styled.div`
+  background: #fdfeff;
+  box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
   border-radius: 8px;
-  background: ${(props) => (props.selected ? "#4BA6EE" : "#fff")};
-  color: ${(props) => (props.selected ? "white" : "inherit")};
+  padding: 16px;
 
-  &:hover {
-    background: ${(props) => (props.selected ? "#4BA6EE" : "#fff")};
-    box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.05);
+  h3,
+  h4 {
+    margin: 0 3px;
   }
 `;
 
-const HouseItem = ({ house }) => (
-  <ItemContainer
-    role="button"
-    className="d-flex p-3 px-4 align-items-center mb-3 justify-content-between"
-    onClick={() => handleSelect(house)}
-    selected={selectedHouse === house.id}
-  >
-    <div className="d-flex align-items-center">
-      <ImgContainer>
-        <img
-          src={housesMapping[house.typ].src}
-          alt={housesMapping[house.typ].title}
-        />
-      </ImgContainer>
-      <div>
-        <H6>{housesMapping[house.typ].title}</H6>
-        {votesLeft && (
-          <Small>
-            {votesLeft(house)} / {house.seats} votes left
-          </Small>
-        )}
-      </div>
-    </div>
-    {votesLeft && (
-      <div>
-        {votesLeft(house) < house.seats && (
-          <CompletedIcon
-            className="bi bi-check-circle fs-5"
-            selected={selectedHouse === house.id}
-          />
-        )}
-      </div>
-    )}
-  </ItemContainer>
-);
+const ProgressBar = styled.div`
+  margin-top: 8px;
+  border-radius: 100px;
+  background: #f5f5f5;
+  padding: 3px;
+  height: 21px;
+`;
+
+const DoneBar = styled.div`
+  border-radius: 100px;
+  background: #239f28;
+  height: 100%;
+`;
+
+const CompleteText = styled.span`
+  color: #239f28;
+  margin-bottom: 0px;
+  font-weight: 900;
+  font-size: 22px;
+`;
+
+State.init({ step: 0 });
+
+const step = 0;
+houses.map((house) => {
+  step += votesLeft(house) === house.seats ? 0 : 1;
+});
+State.update({ step });
 
 return (
-  <div>
-    {state.houses.map((house) => (
-      <HouseItem house={house} />
-    ))}
-  </div>
+  <Stepper>
+    <div className="d-flex justify-content-between align-items-center">
+      <div>Voting progress</div>
+      <div>
+        <CompleteText> {state.step}</CompleteText>/<span>{houses.length}</span>
+        <span className="text-secondary">Houses</span>
+      </div>
+    </div>
+    <ProgressBar className="position-relative">
+      {state.step > 0 && <DoneBar className={`col-${state.step * 3}`} />}
+      <div className="position-absolute top-0 start-50">
+        <small>
+          <b className={`${state.step > 1 ? "text-light" : "inherit"}`}>
+            {state.step === 4 ? 100 : state.step * 25} %
+          </b>
+        </small>
+      </div>
+    </ProgressBar>
+  </Stepper>
 );
