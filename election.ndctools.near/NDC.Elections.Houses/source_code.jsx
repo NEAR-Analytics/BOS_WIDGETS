cba0c390389678b49e33f036cce6c5ec112a9df7
election.ndctools.near <election.ndctools.near>
2023-09-07 19:26:31 +0000

    Update NDC.Elections.Houses by election.ndctools.near at 2023-09-07T19:26:31.893Z

--- election.ndctools.near/NDC.Elections.Houses/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Houses/source_code.jsx
@@ -88,12 +88,12 @@ const ItemContainer = styled.a`
 
 const buildURL = () => {
   const arr = [];
-  if (props.ids) arr.push(`ids=${ids}`);
+  if (props.ids) arr.push(`ids=[${props.ids}]`);
   if (props.election_contract)
-    arr.push(`election_contract=${election_contract}`);
+    arr.push(`election_contract=${props.election_contract}`);
   if (props.registry_contract)
-    arr.push(`registry_contract=${registry_contract}`);
-  if (props.house) arr.push(`house=${house}`);
+    arr.push(`registry_contract=${props.registry_contract}`);
+  if (props.house) arr.push(`house=${props.house}`);
 
   return "?" + arr.join("&");
 };
