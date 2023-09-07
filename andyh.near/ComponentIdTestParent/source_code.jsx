4a229a52961f23c5a08d6d75b2255408c4d52cec
andyh.near <andyh.near>
2023-09-07 23:32:51 +0000

    Update ComponentIdTestParent by andyh.near at 2023-09-07T23:32:51.727Z

---
+++ andyh.near/ComponentIdTestParent/source_code.jsx
@@ -0,0 +1,23 @@
+State.init({
+  a: { id: "a", value: 0 },
+  b: { id: "b", value: 0 },
+  c: { id: "c", value: 0 },
+});
+
+const components = [state.a, state.b, state.c];
+return (
+  <>
+    {components.map(({ id, value }) => (
+      <Widget
+        src="andyh.near/widget/ComponentIdTestChild"
+        id={id}
+        isTrusted={false}
+        props={{
+          value,
+          id,
+          update: () => State.update({ [id]: { id, value: value + 1 } }),
+        }}
+      />
+    ))}
+  </>
+);
