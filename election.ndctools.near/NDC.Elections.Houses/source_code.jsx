8eb344cb5e00b2bbbb8c54f6e8034a60736db70c
election.ndctools.near <election.ndctools.near>
2023-09-07 19:37:16 +0000

    Update NDC.Elections.Houses by election.ndctools.near at 2023-09-07T19:37:16.034Z

--- election.ndctools.near/NDC.Elections.Houses/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Houses/source_code.jsx
@@ -82,6 +82,7 @@ const ItemContainer = styled.a`
 
   &:hover {
     text-decoration: none;
+    color: black;
     background: ${(props) => (props.selected ? "#4BA6EE" : "#fff")};
     box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.05);
   }
