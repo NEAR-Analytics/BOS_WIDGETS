18ba3e5e2e92f6fc826d8404999e75aee09b889c
hack.near <hack.near>
2023-09-07 21:26:40 +0000

    Update CreateGroup.Step1 by hack.near at 2023-09-07T21:26:40.881Z

--- hack.near/CreateGroup.Step1/source_code.jsx
+++ hack.near/CreateGroup.Step1/source_code.jsx
@@ -95,7 +95,7 @@ return (
     </div>
     <br />
     <div>
-      <div className="d-flex gap-2 justify-content-between">
+      <div className="d-flex gap-2 justify-content-between mb-3">
         <h2 className="h5 fw-bold">
           Links <span className="text-black-50 fw-light small">~ optional</span>
         </h2>
