5d877143e76578db1eceef0493267c2db55e1c3a
election.ndctools.near <election.ndctools.near>
2023-09-07 10:27:51 +0000

    Update NDC.Elections.Voters by election.ndctools.near at 2023-09-07T10:27:51.392Z

--- election.ndctools.near/NDC.Elections.Voters/source_code.jsx
+++ election.ndctools.near/NDC.Elections.Voters/source_code.jsx
@@ -1,4 +1,4 @@
-const { candidateId, iahToken, electionContract, ids } = props;
+const { candidateId, isIAmHuman, electionContract, ids } = props;
 const apiKey = api_key ?? "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
 
 State.init({
@@ -91,7 +91,7 @@ return (
       <VoterItem className="d-flex align-items-center justify-content-between">
         <div className="d-flex align-items-center w-100">
           <Expand />
-          {iahToken && <Bookmark />}
+          {isIAmHuman && <Bookmark />}
           <Widget
             src="mob.near/widget/ProfileImage"
             props={{
