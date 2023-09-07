985cd66ca6d86d0ffe04f8d6f36c993ba2d7d271
guessme.near <guessme.near>
2023-09-07 05:57:27 +0000

    Update ZKEVMSwap.zkevm-connect by guessme.near at 2023-09-07T05:57:27.460Z

---
+++ guessme.near/ZKEVMSwap.zkevm-connect/source_code.jsx
@@ -0,0 +1,48 @@
+const { title, src, imgStyle } = props;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  .bridge-text {
+    font-size: 32px;
+    letter-spacing: 0em;
+    color: #ffffff;
+    @media (max-width: 900px) {
+      font-size: 24px;
+      font-weight: 700;
+      line-height: 29px;
+      letter-spacing: 0em;
+      text-align: center;
+    }
+  }
+
+  .connect-button {
+    background: #794fdd;
+    width: 488px;
+    height: 60px;
+    border-radius: 10px;
+    color: #ffffff;
+    border: none;
+
+    @media (max-width: 900px) {
+      width: 100%;
+    }
+  }
+`;
+
+return (
+  <Wrapper>
+    <img
+      src={src}
+      style={{
+        ...imgStyle,
+      }}
+    />
+    <div className="bridge-text">{title}</div>
+
+    <Web3Connect className="connect-button" connectLabel="Connect ETH Wallet" />
+  </Wrapper>
+);
