e97c79858d11ed26e332576dbd13434695f75aba
mattlock.near <mattlock.near>
2023-09-07 16:48:21 +0000

    Update svg-bg-not-working by mattlock.near at 2023-09-07T16:48:21.064Z

---
+++ mattlock.near/svg-bg-not-working/source_code.jsx
@@ -0,0 +1,47 @@
+const bg = (
+  <svg
+    width="768"
+    height="943"
+    viewBox="0 0 768 943"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g style="mix-blend-mode:color-dodge" filter="url(#filter0_f_0_281)">
+      <g style="mix-blend-mode:color-burn">
+        <path
+          d="M-320.01 -149.898C-548.839 -242.949 -657.477 18.6375 -683.192 161.062L207.751 341.862C352.021 216.054 575.443 -26.9379 314.965 7.56036C-10.633 50.6832 -33.9748 -33.5846 -320.01 -149.898Z"
+          fill="#005345"
+        />
+      </g>
+      <path
+        d="M-901.881 560.314C-884.206 920.938 -363.714 1119.12 -138.82 1158.3C473.354 1226.89 204.99 402.326 -17.0312 241.101C-297.252 207.131 -929.552 -4.23881 -901.881 560.314Z"
+        fill="#00EBDD"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_f_0_281"
+        x="-1235.76"
+        y="-502.451"
+        width="2003.76"
+        height="1997.79"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feGaussianBlur
+          stdDeviation="166.5"
+          result="effect1_foregroundBlur_0_281"
+        />
+      </filter>
+    </defs>
+  </svg>
+);
+
+return <div>Hello World</div>;
