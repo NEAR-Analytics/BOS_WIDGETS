5daa8c19d6939ec08ca30b9f191ca61b432ffd70
harrydhillon.near <harrydhillon.near>
2023-09-08 19:15:36 +0000

    Update Keypom.Components.Modal by harrydhillon.near at 2023-09-08T19:15:36.876Z

--- harrydhillon.near/Keypom.Components.Modal/source_code.jsx
+++ harrydhillon.near/Keypom.Components.Modal/source_code.jsx
@@ -18,10 +18,10 @@ const ModalContent = styled.div`
   padding: 20px;
   min-width:350px;
   border-radius: 4px;
+  overflow: visible;
 `;
 
 const Modal = ({ isOpen, children, overlayStyles, contentStyles }) => {
-
   return (
     <MUIDialog open={isOpen} {...overlayStyles}>
       <ModalContent {...contentStyles}>{children}</ModalContent>
