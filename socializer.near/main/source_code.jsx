35aa0f37a9b544623cf81eb9ab7fcb85ad049366
socializer.near <socializer.near>
2023-09-08 13:57:17 +0000

    Update main by socializer.near at 2023-09-08T13:57:17.265Z

---
+++ socializer.near/main/source_code.jsx
@@ -0,0 +1,38 @@
+const Owner = "socializer.near";
+
+State.init({
+  sate: false,
+});
+
+//Styles
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  align-items: stretch;
+  flex-direction: column;
+  background: white;
+  overflow: auto;
+  position: relative;
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  height: calc(100vh - 96px);
+  flex-direction: row;
+  background: white;
+  overflow: auto;
+  position: relative;
+`;
+
+return (
+  <Wrapper>
+    <Widget src={`${Owner}/widget/Header`} />
+    <Content>
+      <Widget src={`${Owner}/widget/Sidebar`} />
+      <Widget src={`${Owner}/widget/Dashboard`} />
+    </Content>
+  </Wrapper>
+);
