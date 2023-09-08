e750e7da15eeaa25d4b2e421ce1700b3deea3a78
paywall.near <paywall.near>
2023-09-08 12:00:58 +0000

    Update PaywallDapplet-Content by paywall.near at 2023-09-08T12:00:58.887Z

--- paywall.near/PaywallDapplet-Content/source_code.jsx
+++ paywall.near/PaywallDapplet-Content/source_code.jsx
@@ -71,40 +71,6 @@ const Wrapper = styled.div`
     position: relative;
   }
 
-  .unlock-content-overlay {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 16px;
-    background-color: rgba(255, 255, 255, 0.7);
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-
-  .unlock-content-overlay > .text {
-    color: #000;
-    text-align: center;
-    font-family: Helvetica;
-    font-size: 15px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-  }
-
-  .unlock-content-overlay > .price {
-    color: #000;
-    text-align: center;
-    font-family: Helvetica;
-    font-size: 28px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-  }
-
   .content-image {
     width: 100%;
   }
@@ -118,33 +84,18 @@ return (
       ) : (
         <>
           <img className="content-image" src={content.blurred} />
-          {accountId ? (
-            <div className="unlock-content-overlay">
-              <div className="text">Unlock this Tweet</div>
-              <div className="price">{price} $NEAR</div>
-              <Widget
-                src="paywall.near/widget/PaywallDapplet-Button"
-                props={{
-                  label: "Buy",
-                  onClick: () => onBuy?.({ contentId, price }),
-                  loading,
-                }}
-              />
-            </div>
-          ) : (
-            <div className="unlock-content-overlay">
-              <div className="text">Unlock this Tweet</div>
-              <div className="price">{price} $NEAR</div>
-              <Widget
-                src="paywall.near/widget/PaywallDapplet-Button"
-                props={{
-                  label: "Connect Wallet",
-                  onClick: () => onConnect?.(),
-                  loading,
-                }}
-              />
-            </div>
-          )}
+          <Widget
+            src="paywall.near/widget/PaywallDapplet-Overlap"
+            props={{
+              text: "Unlock this Tweet",
+              price,
+              label: accountId ? "Buy" : "Connect Wallet",
+              onClick: accountId
+                ? () => onBuy?.({ contentId, price })
+                : () => onConnect?.(),
+              loading,
+            }}
+          />
         </>
       )}
     </div>
