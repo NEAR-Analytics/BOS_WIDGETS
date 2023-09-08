54a355cfbe0a639540601435981ae2019396ab37
dima_sheleg.near <dima_sheleg.near>
2023-09-08 16:47:28 +0000

    Update Onboarding.Starter-fork by dima_sheleg.near at 2023-09-08T16:47:28.092Z

---
+++ dima_sheleg.near/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,9 @@
+return (
+  <>
+    {/* Paste src="near/widget/Onboarding.ComponentCard" below */}
+    <Widget src="near/widget/Onboarding.ComponentCard" />
+    <br />
+    {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
+    <Widget src="near/widget/Onboarding.ComponentCollection" />
+  </>
+);
