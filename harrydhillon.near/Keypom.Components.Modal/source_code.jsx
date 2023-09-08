52f5b69ae7697df564bfa5ac06d99462c4697136
harrydhillon.near <harrydhillon.near>
2023-09-08 19:22:26 +0000

    Update Keypom.Components.Modal by harrydhillon.near at 2023-09-08T19:22:26.932Z

--- harrydhillon.near/Keypom.Components.Modal/source_code.jsx
+++ harrydhillon.near/Keypom.Components.Modal/source_code.jsx
@@ -1,18 +1,3 @@
-const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height:100vh;
-  background-color: ${props.background ? "" : "rgba(0, 0, 0, 0.5)"};
-  background:${props.background};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 100;
-`;
-
 const ModalContent = styled.div`
   background-color: white;
   padding: 20px;
