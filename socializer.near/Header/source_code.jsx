be40461337b1acc036bc28875ba328917603f2be
socializer.near <socializer.near>
2023-09-08 11:15:51 +0000

    Update Header by socializer.near at 2023-09-08T11:15:51.229Z

---
+++ socializer.near/Header/source_code.jsx
@@ -0,0 +1,10 @@
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  height: 96px;
+  background: red;
+  flex-direction: column;
+  position: absolute;
+`;
+
+return <Header>header</Header>;
