32d0ea7e5bc7f67b21992eab49f0731a7f3fcc94
golas.near <golas.near>
2023-09-08 18:30:47 +0000

    Update N-Notification-2 by golas.near at 2023-09-08T18:30:47.287Z

--- golas.near/N-Notification-2/source_code.jsx
+++ golas.near/N-Notification-2/source_code.jsx
@@ -28,128 +28,15 @@ const actionable =
   type === "custom";
 
 let notificationMessage = {
-  follow: "Followed you",
-  unfollow: "Unfollowed you",
-  poke: "Poked you",
-  like: isPost ? "Liked your post" : isComment ? "Liked your comment" : "",
-  comment: "Commented on your post",
-  mention: "Mentioned you",
+  like: isPost ? "liked your post" : isComment ? "liked your comment" : "",
+  // fork: '',
+  follow: "followed you",
+  unfollow: "unfollowed you",
+  comment: "replied to your post",
+  mention: "mentioned you in their post",
+  poke: "poked you",
   custom: value.message ?? "",
 };
-
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 24px;
-  border: 1px solid #eceef0;
-  box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
-    0px 1px 2px rgba(16, 24, 40, 0.06);
-  padding: 12px;
-  border-radius: 12px;
-  transition: background-color 200ms;
-
-  &:hover {
-    background: #eefeef;
-  }
-
-  > *:first-child {
-    width: 200px;
-    border-right: 1px solid #eceef0;
-    padding-right: 24px;
-  }
-
-  > *:last-child {
-    margin-left: auto;
-  }
-
-  @media (max-width: 600px) {
-    flex-direction: column;
-    align-items: start;
-    gap: 8px;
-
-    > *:first-child {
-      width: 100%;
-      border-right: none;
-      padding-right: 0;
-    }
-
-    > *:last-child {
-      margin-left: 0;
-    }
-  }
-`;
-
-const Text = styled.p`
-  margin: 0;
-  line-height: 1.5rem;
-  color: ${(p) => (p.bold ? "#11181C" : "#687076")} !important;
-  font-weight: 400;
-  font-size: ${(p) => (p.small ? "12px" : "14px")};
-  overflow: ${(p) => (p.ellipsis ? "hidden" : "")};
-  text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "")};
-  white-space: ${(p) => (p.ellipsis ? "nowrap" : "")};
-  overflow-wrap: anywhere;
-
-  b {
-    font-weight: 600;
-    color: #11181c;
-  }
-
-  &[href] {
-    font-weight: 600;
-    color: #006adc !important;
-    display: inline-flex;
-    gap: 0.25rem;
-
-    &:hover,
-    &:focus {
-      text-decoration: underline;
-    }
-  }
-`;
-
-const Button = styled.a`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  gap: 8px;
-  padding: 8px 16px;
-  height: 32px;
-  border-radius: 100px;
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 15px;
-  text-align: center;
-  cursor: pointer;
-  background: #fbfcfd;
-  border: 1px solid #d7dbdf;
-  color: #006adc !important;
-  white-space: nowrap;
-
-  &.button--primary {
-    width: 100%;
-    color: #006adc !important;
-
-    @media (max-width: 1024px) {
-      width: auto;
-    }
-  }
-
-  &:hover,
-  &:focus {
-    background: #ecedee;
-    text-decoration: none;
-    outline: none;
-  }
-
-  i {
-    color: #7e868c;
-  }
-
-  .bi-16 {
-    font-size: 16px;
-  }
-`;
 
 // DevGov handles their own type
 if (type && type.startsWith("devgovgigs/")) {
@@ -163,6 +50,36 @@ if (!(type in notificationMessage) || !notificationMessage[type]) return <></>;
 
 return (
   <>
+    <Notification>
+      <Icon>{icon}</Icon>
+      <Content>
+        <Widget
+          src="near/widget/DIG.Avatar"
+          props={{
+            alt: accountId,
+            image: profile.image,
+            size: "small",
+          }}
+        />
+        <div>
+          <Username>{profile.name || accountId.split(".near")[0]}</Username>
+          <Action>{action}</Action>
+        //   <ComponentName>{componentName}</ComponentName>
+          <Timestamp>
+            <Widget
+            src="mob.near/widget/TimeAgo@97556750"
+            props={{ blockHeight: props.blockHeight }}
+            />
+        </Timestamp>
+        </div>
+        <Desc>{desc}</Desc>
+      </Content>
+    </Notification>
+
+
+
+
+    {/*
     <Wrapper>
       <div>
         <Widget
@@ -200,5 +117,6 @@ return (
         {actionable && <Button href={postUrl}>View</Button>}
       </div>
     </Wrapper>
+    */}
   </>
 );
