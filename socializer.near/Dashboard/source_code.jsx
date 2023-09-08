f850ac401b1967f4132cad836f8687e157d8e9c3
socializer.near <socializer.near>
2023-09-08 13:55:37 +0000

    Update Dashboard by socializer.near at 2023-09-08T13:55:37.679Z

---
+++ socializer.near/Dashboard/source_code.jsx
@@ -0,0 +1,9 @@
+const MainComponent = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background: #FAFAFA;
+`;
+
+return <MainComponent>Dashboard</MainComponent>;
