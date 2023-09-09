8a1dfdbce6b275d960248b82ed8ff82aa39c788b
duocelot.near <duocelot.near>
2023-09-09 05:13:44 +0000

    Update digifihackathon by duocelot.near at 2023-09-09T05:13:44.414Z

--- duocelot.near/digifihackathon/source_code.jsx
+++ duocelot.near/digifihackathon/source_code.jsx
@@ -3,7 +3,6 @@ const profile = props.profile ?? Social.getr(`${accountId}/profile`);
 
 // Initialize state
 State.init({
-  isLoaded: false,
   entries: [],
   prompt: "",
 });
@@ -14,23 +13,12 @@ const handleApply = () => {
   }
 };
 
-function convertToCSV(objArray) {
-  const array = typeof objArray !== "object" ? JSON.parse(objArray) : objArray;
-  let str = "";
-
-  for (let i = 0; i < array.length; i++) {
-    let line = "";
-    for (let index in array[i]) {
-      if (line !== "") line += ",";
-      line += array[i][index];
-    }
-    str += line + "\r\n";
-  }
-  return str;
+function convertToCSV(array) {
+  return array.join("\r\n");
 }
 
-function downloadCSV(tags) {
-  let csv = convertToCSV(tags);
+function downloadCSV() {
+  let csv = convertToCSV(state.entries);
   let blob = new Blob(["\ufeff" + csv], { type: "text/csv;charset=utf-8;" });
   let link = document.createElement("a");
 
@@ -44,75 +32,6 @@ function downloadCSV(tags) {
     document.body.removeChild(link);
   }
 }
-
-const Tabs = styled.div`
-  display: flex;
-  height: 48px;
-  border-bottom: 1px solid #eceef0;
-  margin-bottom: 72px;
-  overflow: auto;
-  scroll-behavior: smooth;
-
-  @media (max-width: 1024px) {
-    background: #f8f9fa;
-    border-top: 1px solid #eceef0;
-    margin: 0 -12px 48px;
-
-    > * {
-      flex: 1;
-    }
-  }
-`;
-
-const TabsButton = styled.a`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  font-weight: 600;
-  font-size: 12px;
-  padding: 0 12px;
-  position: relative;
-  color: ${(p) => (p.selected ? "#11181C" : "#687076")};
-  background: none;
-  border: none;
-  outline: none;
-  text-align: center;
-  text-decoration: none !important;
-
-  &:hover {
-    color: #11181c;
-  }
-
-  &::after {
-    content: "";
-    display: ${(p) => (p.selected ? "block" : "none")};
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 3px;
-    background: #59e692;
-  }
-`;
-
-const allNumbers = Array.from(Array(100).keys());
-
-State.init({
-  displayNums: [],
-  lastNumber: 0,
-});
-
-const loadNumbers = (page) => {
-  allNumbers
-    .slice(state.lastNumber, state.lastNumber + 10)
-    .map((n) => numberToElem(n))
-    .forEach((i) => state.displayNums.push(i));
-  state.lastNumber += 10;
-  State.update();
-};
-
-const numberToElem = (number) => <div> {number} </div>;
 
 return (
   <div
@@ -132,6 +51,7 @@ return (
     }}
   >
     <div style={{ width: "80%" }}>
+      {/* Image Section */}
       <div
         style={{
           background: "#415697",
@@ -152,6 +72,8 @@ return (
           }}
         />
       </div>
+
+      {/* Welcome Section */}
       <div
         style={{
           background: "#1b2441",
@@ -163,20 +85,10 @@ return (
           justifyContent: "center",
         }}
       >
-        <Widget
-          src="mob.near/widget/ProfileImage"
-          props={{
-            profile,
-            fast,
-            accountId,
-            style: { width: "4rem", height: "4rem", marginRight: "10px" },
-            className: "mb-2",
-            imageClassName: "rounded-circle w-100 h-100 img-thumbnail d-block",
-            thumbnail: false,
-          }}
-        />
         <span>Bem Vindo {accountId}</span>
       </div>
+
+      {/* Input Section */}
       <div
         style={{
           backgroundColor: "white",
@@ -241,8 +153,10 @@ return (
             APLICAR
           </button>
         </div>
-        <button onClick={() => downloadCSV(state.tags)}>Export CSV</button>
+        <button onClick={downloadCSV}>Export CSV</button>
       </div>
+
+      {/* Tags Display Section */}
       <div
         style={{
           background: "orange",
@@ -252,7 +166,7 @@ return (
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          justifyContent: "flex-start", // Changed to flex-start
+          justifyContent: "flex-start",
           padding: "10px 0",
           overflowY: "auto",
         }}
@@ -278,6 +192,8 @@ return (
           </div>
         ))}
       </div>
+
+      {/* Widget Section */}
       <div
         style={{
           background: "#415697",
@@ -289,22 +205,16 @@ return (
           justifyContent: "center",
         }}
       >
-        <div>
-          <InfiniteScroll
-            loadMore={loadNumbers}
-            hasMore={state.displayNums.length < allNumbers.length}
-          >
-            <p>{state.displayNums}</p>
-            <Widget
-              src="near/widget/Explorer.Account"
-              props={{
-                accountId,
-                network: context.networkId,
-                language: "en",
-                baseUrl: props.baseUrl,
-              }}
-            />
-          </InfiniteScroll>
+        <div style={{ width: "100%", height: "100%", overflow: "hidden" }}>
+          <Widget
+            src="near/widget/Explorer.Account"
+            props={{
+              accountId,
+              network: context.networkId,
+              language: "en",
+              baseUrl: props.baseUrl,
+            }}
+          />
         </div>
       </div>
     </div>
