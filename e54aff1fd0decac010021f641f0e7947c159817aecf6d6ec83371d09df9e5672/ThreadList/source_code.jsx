57700bcd669b8892d69ba04b3cd0f1f7bdc97e54
e54aff1fd0decac010021f641f0e7947c159817aecf6d6ec83371d09df9e5672 <e54aff1fd0decac010021f641f0e7947c159817aecf6d6ec83371d09df9e5672>
2023-09-09 08:18:43 +0000

    Update ThreadList by e54aff1fd0decac010021f641f0e7947c159817aecf6d6ec83371d09df9e5672 at 2023-09-09T08:18:43.299Z

---
+++ e54aff1fd0decac010021f641f0e7947c159817aecf6d6ec83371d09df9e5672/ThreadList/source_code.jsx
@@ -0,0 +1,23 @@
+return (
+  <div className="">
+    <div className="card" style={{ width: "18rem" }}>
+      <img
+        src="https://as1.ftcdn.net/v2/jpg/05/11/42/14/1000_F_511421460_kHuEhWfSJVgr9oXsl4SnmZCpBycKxDzC.jpg"
+        className="card-img-top"
+        alt="..."
+      />
+      <div className="card-body">
+        <h5 className="card-title">Space Name</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <div class="d-grid gap-2">
+          <button class="btn btn-primary" type="button">
+            Follow
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
