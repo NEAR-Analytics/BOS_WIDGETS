a6c1af0fb835bd93a6ee2f4510fca5183c52b2c5
vadim.near <vadim.near>
2023-09-09 21:08:22 +0000

    Update elections by vadim.near at 2023-09-09T21:08:22.542Z

--- vadim.near/elections/source_code.jsx
+++ vadim.near/elections/source_code.jsx
@@ -11,11 +11,6 @@ if (data.ok) {
       .split("\n")
       .map((line) => line.split("|"))
       .filter((data) => data.length === 5)
-    /*.sort((a, b) => {
-        if (a[0] < b[0]) return -1;
-        if (a[0] > b[0]) return 1;
-        return 0;
-      })*/
   ).map((item) => {
     const account_id = item[0];
     if (voters[account_id] == undefined) {
@@ -27,13 +22,18 @@ if (data.ok) {
   State.update({ voters });
 } else return "Loading";
 
-const countKeysRecursive = (obj) => {
+const countKeys = (obj) => {
   return Object.keys(obj).reduce((count, key) => {
     const value = obj[key];
 
     return count + JSON.parse(value).length;
   }, 0);
 };
+
+const candidateFriendsObject = state.accountId
+  ? Social.get(`${state.accountId}/graph/follow/*`, "final") ?? {}
+  : {};
+const candidateFriends = Object.keys(candidateFriendsObject);
 
 const accounts = Object.keys(state.voters ?? []).map((account_id) => {
   return (
@@ -61,16 +61,43 @@ const accounts = Object.keys(state.voters ?? []).map((account_id) => {
               hideImage: false,
             }}
           />{" "}
-          ({countKeysRecursive(state.voters[account_id])})
+          ({countKeys(state.voters[account_id])})
         </a>
       </div>
     </div>
   );
 });
 
+const followerSVG = (
+  <svg
+    x="00px"
+    y="0px"
+    viewBox="0 0 256 256"
+    enable-background="new 0 0 256 256"
+    width="14px"
+    height="14px"
+  >
+    <g>
+      <g>
+        <path
+          fill="#000000"
+          d="M100,20.6c0,0-49,4.5-46.6,58.4c1.4,11.6,9,70.4,47.8,80.2c4.3,0.8,37.2,9.4,55.5-44.1c1.2-3.5,16.1-40.6,2.2-67.4C155.5,41.6,143.5,17.1,100,20.6z"
+        />
+        <path
+          fill="#000000"
+          d="M140.2,173.9c0,0-44.4-6.1-91.1,5.5c-11.2,3.3-38.6,9.6-39,32.7c-0.4,23.1,0,23.7,0,23.7h168C178.2,235.8,139.4,220.7,140.2,173.9z"
+        />
+        <path fill="#000000" d="M160.5,165.5H246v25.1h-85.5V165.5z" />
+        <path fill="#000000" d="M190.5,135.5H216v85.3h-25.5V135.5z" />
+      </g>
+    </g>
+  </svg>
+);
+
 const getVotes = (house) => {
   return (JSON.parse(state.voters[state.accountId][house]) ?? []).map(
     (account_id) => {
+      currentAccountVotes.push(account_id);
       return (
         <div class="row align-items-start">
           <div class="col ps-2 overflow-hidden">
@@ -78,11 +105,12 @@ const getVotes = (house) => {
               src="mob.near/widget/N.ProfileLine"
               props={{
                 accountId: account_id,
-                link: false,
+                link: true,
                 hideAccountId: true,
                 hideImage: false,
               }}
             />
+            {candidateFriends.includes(account_id) ? followerSVG : ""}
           </div>
         </div>
       );
@@ -94,11 +122,63 @@ const titles = [
   "Council Of Advisors",
   "Transparency Commission",
 ];
+
+const currentAccountVotes = [];
+const userSimilarity = [];
 const votes = Object.keys(state.voters[state.accountId] ?? []).map((house) => {
   return (
     <div class="col col-4 overflow-hidden">
       <h5>{titles[house - 1]}</h5>
       {getVotes(house)}
+    </div>
+  );
+});
+
+Object.keys(state.voters ?? []).map((accountId) => {
+  const accountVotes = Object.keys(state.voters[accountId]).reduce(
+    (votes, house) => {
+      return [...JSON.parse(state.voters[accountId][house]), ...votes];
+    },
+    []
+  );
+
+  const commonVotes = accountVotes.filter((vote) =>
+    currentAccountVotes.includes(vote)
+  ).length;
+  const accountVotesNumber = accountVotes.length;
+  const currentAccountVotesNumber = currentAccountVotes.length;
+  if (accountId !== state.accountId) {
+    userSimilarity.push({
+      accountId,
+      similarity:
+        commonVotes / Math.max(accountVotesNumber, currentAccountVotesNumber),
+      commonVotes,
+      accountVotesNumber,
+      currentAccountVotesNumber,
+    });
+  }
+});
+
+userSimilarity = Object.values(
+  userSimilarity.sort((a, b) => b.similarity - a.similarity)
+);
+
+const userWithSimilarVotes = userSimilarity.slice(0, 30).map((item) => {
+  return (
+    <div class="col col-md-4 ps-2 overflow-hidden">
+      <Widget
+        src="mob.near/widget/N.ProfileLine"
+        props={{
+          accountId: item.accountId,
+          link: true,
+          hideAccountId: true,
+          hideImage: false,
+        }}
+      />
+      {candidateFriends.includes(item.accountId) ? followerSVG : ""}
+      <span class="text-secondary">
+        ({item.commonVotes} / {item.accountVotesNumber})
+      </span>
     </div>
   );
 });
@@ -114,8 +194,40 @@ return (
         {accounts}
       </div>
       <div class="col col-9 align-self-start text-center">
-        {state.accountId && <h4>Votes of {state.accountId}</h4>}
-        <div class="row text-start">{votes}</div>
+        {!state.accountId && (
+          <h6 class="pt-3">
+            Select a user from the left to view their voting history
+          </h6>
+        )}
+        {state.accountId && (
+          <>
+            <h4>
+              Votes of{" "}
+              <Widget
+                src="mob.near/widget/N.ProfileLine"
+                props={{
+                  accountId: state.accountId,
+                  link: true,
+                  hideAccountId: true,
+                  hideImage: false,
+                }}
+              />
+              :
+            </h4>
+            <hr />
+
+            <div class="row text-start">{votes}</div>
+
+            <h6 class="pt-5">Users with similar vote sequence:</h6>
+            <div class="row text-start">{userWithSimilarVotes}</div>
+            <div class="row text-center text-secondary mt-3">
+              <small>(number of similar votes / number of user votes)</small>
+              <small>
+                {state.accountId}'s followers marked {followerSVG}
+              </small>
+            </div>
+          </>
+        )}
       </div>
     </div>
 
