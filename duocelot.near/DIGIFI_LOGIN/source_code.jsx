7bc8bca360f7350a1bce38045a9ee0e831bd8dfa
duocelot.near <duocelot.near>
2023-09-09 06:54:10 +0000

    Update DIGIFI_LOGIN by duocelot.near at 2023-09-09T06:54:10.626Z

--- duocelot.near/DIGIFI_LOGIN/source_code.jsx
+++ duocelot.near/DIGIFI_LOGIN/source_code.jsx
@@ -1,68 +1,69 @@
 const accountId = context.accountId;
 
-if (!accountId) {
-  return (
+return (
+  <div
+    style={{
+      width: "100vw",
+      height: "100vh",
+      backgroundSize: "100%",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      backgroundColor: "white",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: '"Press Start 2P", sans-serif',
+    }}
+  >
     <div
       style={{
-        width: "100vw",
-        height: "100vh",
-        backgroundSize: "100%",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundColor: "white",
+        background: "#415697",
+        height: "262px",
+        width: "100%",
+        color: "#fff",
         display: "flex",
-        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        fontFamily: '"Press Start 2P", sans-serif',
       }}
     >
-      <div
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreid25lsb6p73u3zpb7et23qc66fe63knsi22xneuzo3of4m4dnjqgu"
+        alt="coinTAG"
         style={{
-          background: "#415697",
-          height: "262px",
-          width: "100%",
-          color: "#fff",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          maxHeight: "200px",
+          maxWidth: "200px",
         }}
-      >
-        <img
-          src="https://ipfs.near.social/ipfs/bafkreid25lsb6p73u3zpb7et23qc66fe63knsi22xneuzo3of4m4dnjqgu"
-          alt="ERROR 001"
-          style={{
-            maxHeight: "200px",
-            maxWidth: "200px",
-          }}
-        />
-      </div>
-      <h2 style={{ fontSize: "14px", color: "black" }}>
-        VOCE NAO ESTA LOGADO.
-      </h2>
-      <h3 style={{ fontSize: "12px", color: "black" }}>
-        ENTRE COM SUA CARTEIRA NEAR. SE VC NAO TEM CLIQUE
-      </h3>
-      <a
-        href="https://shard.dog/humansofbrazil"
-        style={{ fontSize: "14px", color: "black" }}
-      >
-        AQUI
-      </a>
-      {/* Placeholder for QR Code */}
-      <div style={{ marginTop: "20px" }}>
-        <img
-          src="YOUR_QR_CODE_IMAGE_URL"
-          alt="QR Code"
-          style={{ width: "100px", height: "100px" }}
-        />
-      </div>
+      />
     </div>
-  );
-}
 
-return (
-  <div>
-    <p> VOCE ESTA LOGADO</p>
+    {!accountId ? (
+      <>
+        <h2 style={{ fontSize: "14px", color: "black" }}>
+          VOCE NAO ESTA LOGADO.
+        </h2>
+        <h3 style={{ fontSize: "12px", color: "black" }}>
+          ENTRE COM SUA CARTEIRA NEAR. SE VC NAO TEM CLIQUE
+        </h3>
+        <a
+          href="https://shard.dog/humansofbrazil"
+          style={{ fontSize: "14px", color: "black" }}
+        >
+          AQUI
+        </a>
+        {/* Placeholder for QR Code */}
+        <div style={{ marginTop: "20px" }}>
+          <img
+            src="YOUR_QR_CODE_IMAGE_URL"
+            alt="QR Code"
+            style={{ width: "100px", height: "100px" }}
+          />
+        </div>
+      </>
+    ) : (
+      <div>
+        <p> VOCE ESTA LOGADO</p>
+      </div>
+    )}
   </div>
 );
