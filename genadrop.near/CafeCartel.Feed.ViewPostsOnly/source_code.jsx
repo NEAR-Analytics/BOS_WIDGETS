12ca308c3841de5820ff5fbda4a92d91eab544b2
genadrop.near <genadrop.near>
2023-09-09 09:10:27 +0000

    Update CafeCartel.Feed.ViewPostsOnly by genadrop.near at 2023-09-09T09:10:27.633Z

---
+++ genadrop.near/CafeCartel.Feed.ViewPostsOnly/source_code.jsx
@@ -0,0 +1,20 @@
+const data = props.data;
+
+return (
+  <>
+    
+      <Widget
+        src="efiz.near/widget/every.post"
+        props={{
+          sources: data.sources,
+          typeWhitelist: data.typeWhitelist,
+          hashtagWhitelist: data.hashtagWhitelist,
+          hashtagBlacklist: data.hashtagBlacklist,
+          accountWhitelist: data.accountWhitelist,
+          accountBlacklist: data.accountBlacklist,
+          disableCaching: data.disableCaching,
+          postTemplate: data.postTemplate,
+        }}
+      />
+  </>
+);
