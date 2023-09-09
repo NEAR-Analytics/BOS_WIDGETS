0d4d9e7858d49d7a13e8fb25004dddc54657f47b
duocelot.near <duocelot.near>
2023-09-09 07:30:08 +0000

    Update digifihackathon by duocelot.near at 2023-09-09T07:30:08.141Z

--- duocelot.near/digifihackathon/source_code.jsx
+++ duocelot.near/digifihackathon/source_code.jsx
@@ -1,7 +1,67 @@
-<Widget src="duocelot.near/widget/DIGIFI_LOGIN" />;
-
 const accountId = props.accountId ?? context.accountId;
 const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const commonStyles = {
+  width: "100%",
+  height: "100vh",
+  background: "#415697",
+  display: "flex",
+  flexDirection: "column",
+  fontFamily: '"Press Start 2P", sans-serif',
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+if (!accountId) {
+  return (
+    <div style={commonStyles}>
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreid25lsb6p73u3zpb7et23qc66fe63knsi22xneuzo3of4m4dnjqgu"
+        alt="coinTAG"
+        style={{
+          height: "200px",
+          width: "200px",
+          marginBottom: "20px",
+        }}
+      />
+      <h2 style={{ fontSize: "14px", color: "white" }}>
+        VOCE NAO ESTA LOGADO.
+      </h2>
+      <h3 style={{ fontSize: "16px", color: "white", textAlign: "center" }}>
+        CONECTE COM SUA
+      </h3>
+      <h3 style={{ fontSize: "16px", color: "white", textAlign: "center" }}>
+        CARTEIRA NEAR
+      </h3>
+      <h3 style={{ fontSize: "16px", color: "white", textAlign: "center" }}>
+        CRIE UMA CLICANDO
+      </h3>
+      <a
+        href="https://shard.dog/humansofbrazil"
+        style={{
+          fontSize: "24px",
+          color: "white",
+          margin: "20px 10px",
+          textDecoration: "underline",
+        }}
+      >
+        AQUI
+      </a>
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreiex77ly3hboiiyile6j2v3cjwhuaang2r4lqagdbld6fyvnzm4u6i"
+        alt="QR Code"
+        style={{
+          width: "200px",
+          height: "AUTO",
+          marginTop: "20px",
+        }}
+      />
+      <h3 style={{ fontSize: "16px", color: "white", textAlign: "center" }}>
+        OU SCANEIE O QRCODE
+      </h3>
+    </div>
+  );
+}
 
 // Initialize state
 State.init({
