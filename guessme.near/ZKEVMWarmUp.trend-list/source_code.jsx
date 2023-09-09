5fbdad920073c5525aeb64a96fcd76ec89265e92
guessme.near <guessme.near>
2023-09-09 06:48:19 +0000

    Update ZKEVMWarmUp.trend-list by guessme.near at 2023-09-09T06:48:19.556Z

--- guessme.near/ZKEVMWarmUp.trend-list/source_code.jsx
+++ guessme.near/ZKEVMWarmUp.trend-list/source_code.jsx
@@ -16,8 +16,25 @@ const trend_url = "https://bos-api.delink.one/get-special-action";
 let trendList = [];
 
 const resTrend = JSON.parse(fetch(trend_url).body);
+console.log("resTrend: ", resTrend);
 
 if (Number(resTrend.code) == 0) {
+  resTrend.data[0].action_title = "Bridge 0.01 ETH to Polygon zkEVM";
+
+  resTrend.data[0].action_amount = "0.01";
+
+  resTrend.data[0].template = "ETH-Polygon zkEVM Bridge";
+
+  resTrend.data[1].action_title = "Swap 0.01 ETH to USDC on QuickSwap";
+
+  resTrend.data[1].action_amount = "0.01";
+
+  resTrend.data[1].template = "Polygon zkEVM Dex";
+
+  resTrend.data[2].action_title = "Supply 0.01 ETH on 0vix";
+
+  resTrend.data[2].template = "0vix Lending";
+
   trendList = resTrend.data;
 }
 
