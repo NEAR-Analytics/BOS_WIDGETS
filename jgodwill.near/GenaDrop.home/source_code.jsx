9a9aac4c62fb827875ea462d4f8567e0f37c4de5
jgodwill.near <jgodwill.near>
2023-09-09 20:31:18 +0000

    Update GenaDrop.Home by jgodwill.near at 2023-09-09T20:31:18.985Z

--- jgodwill.near/GenaDrop.home/source_code.jsx
+++ jgodwill.near/GenaDrop.home/source_code.jsx
@@ -751,101 +751,12 @@ return (
       <div className="FAQ_FQAs__10snz">
         <div className="FAQCard_container__1Ei5V">
           <div className="FAQCard_question__3a_rG false">
-            <p className="FAQCard_title__3XC11">
-              What is a generative collection?
-            </p>
+            <p className="FAQCard_title__3XC11"></p>
             <span>
               <img src="/static/media/open-icon.7ae4273f.svg" alt="" />
             </span>
           </div>
-          <div className="FAQCard_answer__3-7tF false">
-            A generative collection is a group of NFTs that use a computer
-            program for the rendering of their art. This is commonly done with
-            profile pictures that choose different traits for the same layer at
-            different rarities that the computer then layers and outputs a
-            unique image. Generative collections have also been used to
-            reference AI generated art.
-          </div>
-        </div>
-        <div className="FAQCard_container__1Ei5V">
-          <div className="FAQCard_question__3a_rG false">
-            <p className="FAQCard_title__3XC11">
-              What does GenaDrop mean when it says it's "multi-chain"?
-            </p>
-            <span>
-              <img src="/static/media/open-icon.7ae4273f.svg" alt="" />
-            </span>
-          </div>
-          <div className="FAQCard_answer__3-7tF false">
-            GenaDrop allows users to mint NFTs (or publish them to the public)
-            to a number of different blockchains, or decentralized databases.
-            This means you get to choose which blockchain you want to publish
-            your work on. We at the Minority Programmers have a chain-agnostic
-            approach to building Web3 dApps and will continue to add different
-            chains onto the platform at the request of members of the Minority
-            Programmers DAO.
-          </div>
-        </div>
-        <div className="FAQCard_container__1Ei5V">
-          <div className="FAQCard_question__3a_rG false">
-            <p className="FAQCard_title__3XC11">
-              How does GenaDrop store my images?
-            </p>
-            <span>
-              <img src="/static/media/open-icon.7ae4273f.svg" alt="" />
-            </span>
-          </div>
-          <div className="FAQCard_answer__3-7tF false">
-            GenaDrop uses a form of decentralized storage called IPFS to mint
-            your NFTs.
-          </div>
-        </div>
-        <div className="FAQCard_container__1Ei5V">
-          <div className="FAQCard_question__3a_rG false">
-            <p className="FAQCard_title__3XC11">
-              How do I start using Genadrop?
-            </p>
-            <span>
-              <img src="/static/media/open-icon.7ae4273f.svg" alt="" />
-            </span>
-          </div>
-          <div className="FAQCard_answer__3-7tF false">
-            When you’re making a generative NFT collection, each final image
-            will be made out of different traits that can be mixed and matched.
-            First of all you must have basic assets for layer combination
-            generation. Example: the main background layer, body, and few
-            traits. For more information watch this demo
-          </div>
-        </div>
-        <div className="FAQCard_container__1Ei5V">
-          <div className="FAQCard_question__3a_rG false">
-            <p className="FAQCard_title__3XC11">How do I mint my collection?</p>
-            <span>
-              <img src="/static/media/open-icon.7ae4273f.svg" alt="" />
-            </span>
-          </div>
-          <div className="FAQCard_answer__3-7tF false">
-            Connect your wallet and upload your collection zip folder,
-            previously made in the create section of the Genadrop dApp. As a
-            JSON file after which you will now upload your Metadata to IPFS and
-            select the blockchain of your choice, add price and Mint.
-          </div>
-        </div>
-
-        <div className="FAQCard_container__1Ei5V">
-          <div className="FAQCard_question__3a_rG false">
-            <p className="FAQCard_title__3XC11">
-              What type of NFT(s) can I mint?
-            </p>
-            <span>
-              <img src="/static/media/open-icon.7ae4273f.svg" alt="" />
-            </span>
-          </div>
-          <div className="FAQCard_answer__3-7tF false">
-            Genadrop supports Minting of 1 of 1 images and collections. To Mint
-            a 1 of 1 NFT select your NFT file[supported file format: png] ,
-            enter metadata and click mint.
-          </div>
+          <div className="FAQCard_answer__3-7tF false"></div>
         </div>
       </div>
     </FAQWrapper>
