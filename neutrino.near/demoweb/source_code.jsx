998f83e4168a8e5a58d3bca6ce4915fef272d008
neutrino.near <neutrino.near>
2023-09-09 15:16:02 +0000

    Update demoweb by neutrino.near at 2023-09-09T15:16:02.164Z

---
+++ neutrino.near/demoweb/source_code.jsx
@@ -0,0 +1,11 @@
+const user = "gagdiez.near";
+
+return (
+  <>
+    <h3> Composition </h3>
+    <p> Components can be composed </p>
+    <hr />
+
+    <Widget src={`neutrino.near/widget/helloworld`} props={props} />
+  </>
+);
