b5d0a1320e18f2a35262a45d203d9cefac192241
socializer.near <socializer.near>
2023-09-11 13:44:39 +0000

    Update Profile by socializer.near at 2023-09-11T13:44:39.470Z

--- socializer.near/Profile/source_code.jsx
+++ socializer.near/Profile/source_code.jsx
@@ -1,5 +1,51 @@
 const Owner = "socializer.near";
 
+const columns = [
+  {
+    title: "Token Name",
+    key: "name",
+    width: 25,
+    align: "left",
+  },
+  {
+    title: "Token Contract Address",
+    key: "contract",
+    width: 25,
+    align: "center",
+  },
+  {
+    title: "Token Deposited",
+    key: "deposited",
+    width: 25,
+    align: "center",
+  },
+  {
+    title: "",
+    key: "action",
+    width: 25,
+    align: "center",
+    action: true,
+  },
+];
+
+const tokenData = [
+  {
+    name: "$NVRS",
+    contract: "nearverse-token.near",
+    deposited: "12,000",
+  },
+  {
+    name: "$NEKO",
+    contract: "v2-nekotoken.near",
+    deposited: "20,000",
+  },
+  {
+    name: "$NAUT",
+    contract: "nautstaking.near",
+    deposited: "400",
+  },
+];
+
 const Wrapper = styled.div`
   display: flex;
   width: 100%;
@@ -22,11 +68,44 @@ const WalletComponent = styled.div`
   gap: 20px;
 `;
 
+const TokenComponent = styled.div`
+  display: flex;
+  width: 100%;
+  background: #F3F3F3;
+  flex-direction: column;
+  padding: 6px 24px;
+  border-radius: 8px;
+  border: 1px solid var(--light_90, #E6E6E6);
+  gap: 20px;
+`;
+
 const Avatar = styled.img`
   display: flex;
   width: 100px;
   height: 100px;
   border-radius: 100px;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 8px 9px 8px 28px;
+  display: flex;
+  gap: 7px;
+  border-radius: 6px;
+  border: 1px solid var(--light_90, #E6E6E6);
+  background: var(--Dark, #121212);
+  text-align: center;
+  color: var(--light_95, #F3F3F3);
+  font-size: 12px;
+  font-weight: 600;
+  text-transform: capitalize;
+  margin: auto;
+`;
+
+const Table = styled.table`
+  border-radius: 8px;
+  background: #F5F1F1;
+  margin: 0;
 `;
 
 return (
@@ -38,12 +117,155 @@ return (
     <WalletComponent>
       <div className="d-flex " style={{ borderBottom: "1px solid #808080" }}>
         <h5 style={{ fontWeight: 700, fontSize: 18, lineHeight: "150%" }}>
-          Near Wallet
+          {"Near Wallet"}
         </h5>
       </div>
       <div className="d-flex ">
         <h5 style={{ fontWeight: 600, fontSize: 18 }}>skylar.near</h5>
       </div>
     </WalletComponent>
+    <TokenComponent>
+      <div
+        className="d-flex justify-content-between"
+        style={{
+          borderBottom: "1px solid #808080",
+          gap: 12,
+        }}
+      >
+        <div
+          className="d-flex"
+          style={{
+            gap: 12,
+            flexDirection: "column",
+          }}
+        >
+          <h5 style={{ fontWeight: 700, fontSize: 18, lineHeight: "150%" }}>
+            {"Token Balances"}
+          </h5>
+          <p style={{ fontSize: 12 }}>
+            {
+              "These NEP Tokens Can be allocated as bounty for users that engage with your tweets. These can be withdrawn to you wallet at any time"
+            }
+          </p>
+        </div>
+        <div className="d-flex" style={{ minWidth: 150 }}>
+          <Button>
+            {"Select Token"}
+            <span style={{ height: 18 }}>
+              <svg
+                width="30"
+                height="26"
+                viewBox="0 0 30 26"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g filter="url(#filter0_d_58_4394)">
+                  <rect
+                    x="4"
+                    y="18"
+                    width="18"
+                    height="22"
+                    rx="8"
+                    transform="rotate(-90 4 18)"
+                    fill="#141522"
+                    shape-rendering="crispEdges"
+                  />
+                  <path
+                    d="M19 5.27337L18.06 4.33337L15 7.38671L11.94 4.33337L11 5.27337L15 9.27337L19 5.27337Z"
+                    fill="white"
+                  />
+                  <path
+                    d="M19 9.66668L18.06 8.72668L15 11.78L11.94 8.72669L11 9.66669L15 13.6667L19 9.66668Z"
+                    fill="white"
+                  />
+                  <rect
+                    x="4.5"
+                    y="17.5"
+                    width="17"
+                    height="21"
+                    rx="7.5"
+                    transform="rotate(-90 4.5 17.5)"
+                    stroke="#141522"
+                    shape-rendering="crispEdges"
+                  />
+                </g>
+              </svg>
+            </span>
+          </Button>
+        </div>
+      </div>
+
+      <div className="d-flex ">
+        <Table
+          className={`table table-hover table-striped table-borderless ${props.className}`}
+        >
+          <thead>
+            <tr>
+              {columns.map((th) => (
+                <th
+                  key={th.title}
+                  className="col-1"
+                  style={{
+                    fontSize: 16,
+                    width: `${th.width}%`,
+                    verticalAlign: "middle",
+                    textAlign: th.align,
+                  }}
+                  scope="col"
+                >
+                  <div>
+                    <span>{th.title}</span>
+                  </div>
+                </th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody>
+            {tokenData.length > 0 &&
+              tokenData.map((row, i) => {
+                return (
+                  <tr key={row.key}>
+                    {columns.map((td) => {
+                      const key = td.key ? row[td.key] : i + 1;
+                      return (
+                        <td
+                          style={{
+                            color:
+                              td.colors ||
+                              themeColor?.table_pagination?.columntextcolor,
+                            fontSize: 16,
+                            textAlign: td.align,
+                            verticalAlign: "middle",
+                          }}
+                        >
+                          {td.action ? (
+                            <div
+                              style={{
+                                display: "flex",
+                                gap: 10,
+                                alignItems: "center",
+                              }}
+                            >
+                              <a href="#" className="text-decoration-underline">
+                                Deposit
+                              </a>
+                              <a href="#" className="text-decoration-underline">
+                                Withdraw
+                              </a>
+                            </div>
+                          ) : (
+                            key
+                          )}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+          </tbody>
+        </Table>
+      </div>
+    </TokenComponent>
   </Wrapper>
 );
