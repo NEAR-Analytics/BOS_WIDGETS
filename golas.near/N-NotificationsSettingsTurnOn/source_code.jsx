0686072e831e574e74d003d765ad4b114c999019
golas.near <golas.near>
2023-09-11 18:38:34 +0000

    Update N-NotificationsSettingsTurnOn by golas.near at 2023-09-11T18:38:34.425Z

---
+++ golas.near/N-NotificationsSettingsTurnOn/source_code.jsx
@@ -0,0 +1,29 @@
+const Card = styled.div`
+
+`;
+
+const Header = styled.div`
+
+`;
+
+const Title = styled.div`
+
+`;
+
+const Text = styled.div`
+
+`;
+
+const Button = styled.div`
+
+`;
+
+return (
+  <Card>
+    <Header>
+      <Title></Title>
+      <Text></Text>
+      <Button></Button>
+    </Header>
+  </Card>
+);
