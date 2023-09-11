ed3d34a1aa1dc5d2aa8b046153e3c8be0128678b
hack.near <hack.near>
2023-09-11 14:53:39 +0000

    Update every.group by hack.near at 2023-09-11T14:53:39.601Z

--- hack.near/every.group/source_code.jsx
+++ hack.near/every.group/source_code.jsx
@@ -1,9 +1,9 @@
 const accountId = props.accountId ?? context.accountId;
+const groupId = props.groupId ?? "f8ad9d1a76259lmdpjnd74e69162a0a014";
 const creatorId = props.creatorId ?? "hack.near";
 
 const widgets = {
-  header: "hack.near/widget/page.navbar",
-  card: "hack.near/widget/group.card",
+  group: "hack.near/widget/group.card",
   create: "hack.near/widget/group.create",
   edit: "hack.near/widget/group.edit",
 };
@@ -17,45 +17,41 @@ if (!groups) {
 const { isVerified } = props;
 
 const isMember = Social.keys(
-  `${context.accountId}/graph/${groupId}/${context.accountId}`,
+  `${creatorId}/graph/${groupId}/${accountId}`,
   undefined,
   {
     values_only: true,
   }
 );
 
+const type = join ? "leave" : "join";
+
+const handleJoin = () => {
+  Social.set({
+    graph: { [groupId]: { [accountId]: "" } },
+    index: {
+      graph: JSON.stringify({
+        key: groupId,
+        value: {
+          type,
+          accountId,
+        },
+      }),
+      notify: JSON.stringify({
+        key: creatorId,
+        value: {
+          type,
+          accountId,
+          message: "everyone is growing!",
+        },
+      }),
+    },
+  });
+};
+
 const Container = styled.div`
   padding: 30px 0;
   margin: 0;
-`;
-
-const ActivityContainer = styled.div`
-  overflow-y: scroll;
-`;
-
-const Left = styled.div`
-  padding: 20px;
-  background: #f8f8f9;
-  border-radius: 8px;
-`;
-
-const Center = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  grid-gap: 20px;
-`;
-
-const H5 = styled.h5`
-  margin-bottom: 20px;
-`;
-
-const ButtonContainer = styled.div`
-  color: #fff;
-  background: #4ba6ee;
-
-  &:hover {
-    background: #3b86cb;
-  }
 `;
 
 const Header = styled.div`
@@ -67,29 +63,64 @@ const Header = styled.div`
   }
 `;
 
-const Toolbar = styled.div`
+const Navbar = styled.div`
   margin-left: 20px;
   @media only screen and (max-width: 1061px) {
     margin: 10px 0 0 0;
   }
 `;
 
+const Center = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
+
 return (
   <>
     <div>
-      <Widget
-        src={widgets.header}
-        props={{
-          accountId,
-        }}
-      />
+      <Header className="d-flex p-3 px-4 align-items-center rounded justify-content-between">
+        <Widget
+          src="mob.near/widget/Image"
+          props={{
+            image: {
+              url: "https://pbs.twimg.com/profile_images/1690850854457204736/KUXVTpZt_400x400.png",
+            },
+            alt: "Near Builders",
+            style: {
+              height: "42px",
+              objectFit: "cover",
+            },
+          }}
+        />
+        {!context.accountId ? (
+          <a href="https://shard.dog/nearweek" className="btn btn-success">
+            news
+          </a>
+        ) : (
+          <Navbar>
+            {isMember && Object.keys(isMember).length ? (
+              <button
+                onClick={() => State.update({ showModal: true })}
+                className="btn btn-success"
+              >
+                create
+              </button>
+            ) : (
+              <button onClick={handleJoin} className="btn btn-success">
+                start
+              </button>
+            )}
+          </Navbar>
+        )}
+      </Header>
       <Container className="d-flex justify-content-between w-100">
         <Center className="px-2 px-md-3 d-flex flex-column">
           <h4 className="mb-1">Discover Groups</h4>
           {groups.map((group, i) => (
             <Widget
               key={i}
-              src={widgets.card}
+              src={widgets.group}
               props={{
                 groupId: group.value.id,
                 creatorId: group.accountId,
