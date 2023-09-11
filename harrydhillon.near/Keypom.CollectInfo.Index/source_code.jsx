645b7f165d953aa7ea56738a6923a0a373efe899
harrydhillon.near <harrydhillon.near>
2023-09-11 13:39:44 +0000

    Update Keypom.CollectInfo.Index by harrydhillon.near at 2023-09-11T13:39:44.973Z

--- harrydhillon.near/Keypom.CollectInfo.Index/source_code.jsx
+++ harrydhillon.near/Keypom.CollectInfo.Index/source_code.jsx
@@ -2,7 +2,7 @@ State.init({
   data: [
     { question: "Email", isRequired: true },
     { question: "Full Name", isRequired: false },
-    { question: "How did you find out about this event", isRequired: false },
+    { question: "How did you find out about this event?", isRequired: false },
   ],
   isAddCustomFieldModalOpen: false,
   editMode: false,
