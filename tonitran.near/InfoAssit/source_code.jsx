9f2960bec7aceb22af3778484be3ca3bffd3d898
tonitran.near <tonitran.near>
2023-09-11 07:56:39 +0000

    Update InfoAssit by tonitran.near at 2023-09-11T07:56:39.762Z

---
+++ tonitran.near/InfoAssit/source_code.jsx
@@ -0,0 +1,12 @@
+State.init({
+  data: null,
+  isFetched: false,
+  isModalOpen: false,
+  isButtonShow: true,
+  selectingId: "",
+  selectingIdContent: "",
+  selectingIdUrl: "",
+  chatcontent: "",
+  queryText: "",
+  isSending: false,
+});
