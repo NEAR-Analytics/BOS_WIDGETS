15c8bbdb0b2a7eeb9bf94c7be8236a72e34bc00e
opencann.near <opencann.near>
2023-09-11 22:55:36 +0000

    Update publish by opencann.near at 2023-09-11T22:55:36.017Z

--- opencann.near/publish/source_code.jsx
+++ opencann.near/publish/source_code.jsx
@@ -150,7 +150,7 @@ const onChangePurpose = (purpose) => {
 
 return (
   <div className="mb-3">
-    <h3>Publish a New Data Set</h3>
+    <h3>Publish New Data</h3>
     <a href="#/flowscience.near/widget/create.hypercert">
       <i>Click here to publish an impact certficate</i>
     </a>
