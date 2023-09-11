38746e37fa0dd1b3f8962b576b17005157d6bf33
efiz.near <efiz.near>
2023-09-11 21:17:07 +0000

    Update Module.DropdownMenu by efiz.near at 2023-09-11T21:17:07.941Z

---
+++ efiz.near/Module.DropdownMenu/source_code.jsx
@@ -0,0 +1,21 @@
+function DropdownMenu({ TriggerElement, TriggerChildren, items }) {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger as={TriggerElement}>
+        <TriggerChildren />
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        {items &&
+          items.map((item, index) => {
+            return (
+              <DropdownMenu.Item key={index} onSelect={item.onSelect}>
+                {item.Children}
+              </DropdownMenu.Item>
+            );
+          })}
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+}
+
+return { DropdownMenu };
