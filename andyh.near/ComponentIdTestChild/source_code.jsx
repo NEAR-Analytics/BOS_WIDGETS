3affc15429c74bb0347faaca5b67cc7c06d7ffae
andyh.near <andyh.near>
2023-09-11 23:29:49 +0000

    Update ComponentIdTestChild by andyh.near at 2023-09-11T23:29:49.044Z

--- andyh.near/ComponentIdTestChild/source_code.jsx
+++ andyh.near/ComponentIdTestChild/source_code.jsx
@@ -3,7 +3,11 @@ State.init({ value: props.value });
 return (
   <div>
     Hello {props.id}
-    <button onClick={() => State.update({ value: state.value + 1 })}>
+    <button
+      onClick={() => {
+        State.update({ value: state.value + 1 });
+      }}
+    >
       increment {state.value}
     </button>
   </div>
