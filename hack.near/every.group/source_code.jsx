1c1f851083daf29598a5fe6d25e368e49ca71766
hack.near <hack.near>
2023-09-11 21:40:39 +0000

    Update every.group by hack.near at 2023-09-11T21:40:39.625Z

--- hack.near/every.group/source_code.jsx
+++ hack.near/every.group/source_code.jsx
@@ -50,8 +50,42 @@ const handleJoin = () => {
 };
 
 const Container = styled.div`
-  padding: 30px 0;
+  padding: 23px 0;
   margin: 0;
+
+  .top-right {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  .bell-icon {
+    font-size: 23px;
+    color: #fff;
+    margin-left: 5px;
+    text-decoration: none;
+    transition: color 0.3s ease-in-out;
+  }
+
+  .bell-icon:hover {
+    color: #fff;
+  }
+
+  .bell-icon .bi-bell {
+    display: inline;
+  }
+
+  .bell-icon .bi-bell-fill {
+    display: none;
+  }
+
+  .bell-icon:hover .bi-bell {
+    display: none;
+  }
+
+  .bell-icon:hover .bi-bell-fill {
+    display: inline;
+  }
 `;
 
 const Header = styled.div`
@@ -84,21 +118,34 @@ const GroupCard = styled.div`
 
 return (
   <>
-    <div>
+    <Container>
       <Header className="d-flex p-3 px-4 align-items-center rounded justify-content-between">
-        <Widget
-          src="mob.near/widget/Image"
-          props={{
-            image: {
-              url: "https://pbs.twimg.com/profile_images/1690850854457204736/KUXVTpZt_400x400.png",
-            },
-            alt: "Near Builders",
-            style: {
-              height: "42px",
-              objectFit: "cover",
-            },
-          }}
-        />
+        <a href="/near/widget/ProfilePage?accountId=devs.near">
+          <Widget
+            src="mob.near/widget/Image"
+            props={{
+              image: {
+                url: "https://pbs.twimg.com/profile_images/1690850854457204736/KUXVTpZt_400x400.png",
+              },
+              alt: "Near Builders",
+              style: {
+                height: "42px",
+                objectFit: "cover",
+              },
+            }}
+          />
+        </a>
+        <div className="ms-auto me-0 me-md-2 d-flex align-items-center">
+          <div className="top-right">
+            <a
+              href={`/hack.near/widget/group.index?groupId=${groupId}`}
+              className="bell-icon"
+            >
+              <i className="bi bi-bell"></i>
+              <i className="bi bi-bell-fill"></i>
+            </a>
+          </div>
+        </div>
         {!context.accountId ? (
           <a href="https://shard.dog/nearweek" className="btn btn-success">
             news
@@ -120,26 +167,24 @@ return (
           </Navbar>
         )}
       </Header>
-      <Container>
-        <Center className="px-2 px-md-3 d-flex flex-column justify-content-between w-100">
-          <h4 className="mb-1">Discover Groups</h4>
-          <div className="row">
-            {groups.map((group, i) => (
-              <div className="col-auto m-2">
-                <Widget
-                  key={i}
-                  src={widgets.group}
-                  props={{
-                    groupId: group.value.id,
-                    creatorId: group.accountId,
-                  }}
-                />
-              </div>
-            ))}{" "}
-          </div>
-        </Center>
-      </Container>
-    </div>
+      <Center className="px-2 px-md-3 d-flex flex-column justify-content-between w-100">
+        <h4 className="mb-1 mt-5">Discover Groups</h4>
+        <div className="row">
+          {groups.map((group, i) => (
+            <div className="col-auto m-2">
+              <Widget
+                key={i}
+                src={widgets.group}
+                props={{
+                  groupId: group.value.id,
+                  creatorId: group.accountId,
+                }}
+              />
+            </div>
+          ))}{" "}
+        </div>
+      </Center>
+    </Container>
 
     <>
       {state.showModal && (
