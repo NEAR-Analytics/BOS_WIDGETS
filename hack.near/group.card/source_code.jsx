162d376bfea2e7caf69d52de06941dcf1b1a9512
hack.near <hack.near>
2023-09-11 22:05:47 +0000

    Update group.card by hack.near at 2023-09-11T22:05:47.398Z

--- hack.near/group.card/source_code.jsx
+++ hack.near/group.card/source_code.jsx
@@ -30,6 +30,42 @@ const Card = styled.div`
   padding: 16px;
 `;
 
+const Bell = styled.div`
+  .bell {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  .bell-icon {
+    font-size: 23px;
+    color: #000;
+    margin-left: 5px;
+    text-decoration: none;
+    transition: color 0.3s ease-in-out;
+  }
+
+  .bell-icon:hover {
+    color: #000;
+  }
+
+  .bell-icon .bi-bell {
+    display: inline;
+  }
+
+  .bell-icon .bi-bell-fill {
+    display: none;
+  }
+
+  .bell-icon:hover .bi-bell {
+    display: none;
+  }
+
+  .bell-icon:hover .bi-bell-fill {
+    display: inline;
+  }
+`;
+
 const CardLeft = styled.div`
   display: flex;
   gap: 16px;
@@ -116,11 +152,24 @@ return (
         )}
       </div>
     </CardLeft>
+    <Bell>
+      <div className="ms-autome-md-2 d-flex align-items-center">
+        <div className="bell">
+          <a
+            href={`/hack.near/widget/group.index?groupId=${groupId}`}
+            className="bell-icon"
+          >
+            <i className="bi bi-bell"></i>
+            <i className="bi bi-bell-fill"></i>
+          </a>
+        </div>
+      </div>
+    </Bell>
     {canJoin && context.accountId && (
       <>
         {groupKey === context.accountId ? (
           <a
-            className="btn btn-outline-primary"
+            className="btn btn-outline-dark"
             href={`/hack.near/widget/group.edit?groupId=${groupId}`}
           >
             edit
