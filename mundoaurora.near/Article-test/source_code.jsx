0da0778e3f1b25ae6f8cf2870dcd5dfa03601543
mundoaurora.near <mundoaurora.near>
2023-09-11 20:27:35 +0000

    Update Article-test by mundoaurora.near at 2023-09-11T20:27:35.335Z

---
+++ mundoaurora.near/Article-test/source_code.jsx
@@ -0,0 +1,66 @@
+let text = "Ten un lindo día";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-right">
+      <h1>Mundo Aurora ES Near</h1>
+      <h2>La Constitución en Constante Evolución de NEAR</h2>
+
+      <p>
+        {" "}
+        NEAR Protocol está marcando un hito en el mundo de las criptomonedas con
+        su enfoque innovador en la gobernanza a través de su Constitución. A
+        diferencia de muchas otras blockchains, NEAR considera su Constitución
+        como un documento vivo y en constante evolución. En este nota,
+        exploraremos cómo esta Constitución está transformando la toma de
+        decisiones en el ecosistema NEAR.
+      </p>
+      <h2>Los Principios Clave de la Gobernanza en NEAR</h2>
+      <p>
+        La base de la gobernanza en NEAR son una serie de principios clave que
+        todos los participantes deben respetar y seguir. Estos principios son
+        fundamentales para garantizar que la toma de decisiones sea justa,
+        inclusiva y transparente. Veamos algunos de estos principios:
+      </p>
+      <h3>Inclusividad:</h3>
+      <p>
+        La gobernanza en NEAR busca mejorar el ecosistema sin infringir en
+        ningún proyecto o aplicación construida sobre NEAR. No muestra
+        favoritismo en la selección de miembros o en las sesiones de
+        retroalimentación. Además, cuenta con una comisión de transparencia para
+        garantizar imparcialidad.
+      </p>
+
+      <h3>Adaptabilidad:</h3>
+      <p>
+        El mundo de las criptomonedas es rápido y volátil, y la gobernanza debe
+        ser capaz de adaptarse a los cambios. NEAR se compromete a ser
+        antifrágil, superando desafíos y problemas para construir un ecosistema
+        robusto.
+      </p>
+
+      <h3>Transparencia:</h3>
+      <p>
+        NEAR, con uno de los tesoros en cadena más grandes en el mundo cripto,
+        se siente responsable ante su comunidad y futuros interesados. La
+        transparencia es una expectativa para todos los participantes en la
+        gobernanza a través del NEAR Digital Collective.
+      </p>
+
+      <h3>Ecosistema en Primer Lugar:</h3>
+      <p>
+        En un mundo donde los mercenarios son comunes, NEAR prioriza el
+        ecosistema. Los contribuyentes deben demostrar su valía antes de esperar
+        recompensas. La NDC se trata de una cultura y una visión, no solo un
+        trabajo.
+      </p>
+      <h3>Anti-Cartel:</h3>
+      <p>
+        Se espera que los participantes declaren cualquier conflicto de interés
+        antes de involucrarse. Un NDC exitoso requiere actores honestos y
+        auténticos que trabajen en beneficio del ecosistema en su conjunto.
+      </p>
+      <p> Borrador y prueba. America/cripteros.near</p>
+    </div>
+  </>
+);
