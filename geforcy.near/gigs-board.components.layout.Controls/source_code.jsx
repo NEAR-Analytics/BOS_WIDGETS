f28f5d3245e986b67be67f3cfb3c6206056d71a9
geforcy.near <geforcy.near>
2023-09-12 21:21:56 +0000

    Update gigs-board.components.layout.Controls by geforcy.near at 2023-09-12T21:21:56.563Z

---
+++ geforcy.near/gigs-board.components.layout.Controls/source_code.jsx
@@ -0,0 +1,80 @@
+/* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+
+  const linkPropsQuery = Object.entries(linkProps)
+    .filter(([_key, nullable]) => (nullable ?? null) !== null)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
+
+const buttonStyle = {
+  backgroundColor: "#0C7283",
+  color: "#f3f3f3",
+};
+
+return (
+  <div class="d-flex flex-row-reverse" className={props.className}>
+    {props.href ? (
+      <a class="btn btn-light" style={buttonStyle} href={props.href}>
+        <i
+          class="bi"
+          className={props.icon ? props.icon : "bi-plus-circle-fill"}
+        ></i>
+        {props.title}
+      </a>
+    ) : (
+      <button class="btn btn-light" style={buttonStyle} onClick={props.onClick}>
+        <i
+          class="bi"
+          className={props.icon ? props.icon : "bi-plus-circle-fill"}
+        ></i>
+        {props.title || "Post"}
+      </button>
+    )}
+  </div>
+);
