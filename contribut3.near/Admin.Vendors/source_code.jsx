1be29dcb42b580003d86267951b75598d0345634
contribut3.near <contribut3.near>
2023-09-12 14:16:02 +0000

    Update Admin.Vendors by contribut3.near at 2023-09-12T14:16:02.614Z

--- contribut3.near/Admin.Vendors/source_code.jsx
+++ contribut3.near/Admin.Vendors/source_code.jsx
@@ -1,4 +1,5 @@
 const ownerId = "contribut3.near";
+const apiUrl = "https://api-staging-fur7.onrender.com";
 const search = props.search ?? "";
 
 State.init({
@@ -7,7 +8,7 @@ State.init({
 });
 
 asyncFetch(
-  `https://api-staging-fur7.onrender.com/data/vendors?sort=timedesc&verified=false&q=${search}`
+  `${apiUrl}/data/vendors?sort=timedesc&verified=false&q=${search}`,
 ).then(({ body: items }) => State.update({ items, itemsIsFetched: true }));
 
 if (!state.itemsIsFetched) {
