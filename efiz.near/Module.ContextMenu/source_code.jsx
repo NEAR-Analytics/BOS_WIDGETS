f27c807b2cf1da56a7a70b65778365f382d1349e
efiz.near <efiz.near>
2023-09-12 05:29:47 +0000

    Update Module.ContextMenu by efiz.near at 2023-09-12T05:29:47.976Z

--- efiz.near/Module.ContextMenu/source_code.jsx
+++ efiz.near/Module.ContextMenu/source_code.jsx
@@ -37,6 +37,22 @@ const Wrapper = styled.div`
 `;
 
 function ContextMenu({ Item, passProps, handlers, items }) {
+  
+  if (!handlers || typeof handlers !== 'object') {
+    console.warn('ContextMenu: handlers prop is missing or not an object.');
+    return null;
+  }
+
+  if (!passProps || typeof passProps !== 'object') {
+    console.warn('ContextMenu: passProps prop is missing or not an object.');
+    return null;
+  }
+
+  if (!items || typeof items !== 'object') {
+    console.warn('ContextMenu: items prop is missing or not an object.');
+    return null;
+  }
+  
   return (
     <ContextMenu.Root>
       <ContextMenu.Trigger asChild>
@@ -46,15 +62,31 @@ function ContextMenu({ Item, passProps, handlers, items }) {
       </ContextMenu.Trigger>
       <ContextMenu.Content sideOffset={5} align="end" asChild>
         <Wrapper>
-          {handlers &&
-            Object.keys(handlers).map((key) => (
+          {handlers && Object.keys(handlers).map((key) => {
+            if (!handlers[key]) {
+              console.warn(`ContextMenu: handler for key "${key}" is missing.`);
+              return null;
+            }
+
+            if (!passProps[key]) {
+              console.warn(`ContextMenu: passProps for key "${key}" is missing.`);
+              return null;
+            }
+
+            if (!items[key]) {
+              console.warn(`ContextMenu: item for key "${key}" is missing.`);
+              return null;
+            }
+
+            return (
               <ContextMenu.Item
                 className="menu__item"
                 onSelect={() => handlers[key](passProps[key])}
               >
                 {items[key]()}
               </ContextMenu.Item>
-            ))}
+            );
+          })}
         </Wrapper>
       </ContextMenu.Content>
     </ContextMenu.Root>
