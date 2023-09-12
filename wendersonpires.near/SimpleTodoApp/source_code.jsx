536755a3cb592a15c45a9361504120527249f3be
wendersonpires.near <wendersonpires.near>
2023-09-12 01:17:41 +0000

    Update SimpleTodoApp by wendersonpires.near at 2023-09-12T01:17:41.721Z

---
+++ wendersonpires.near/SimpleTodoApp/source_code.jsx
@@ -0,0 +1,12 @@
+// The link provided by thirdapp pointing to the react app stored inside IPFS
+const externalAppUrl =
+  "https://bafybeigadizblnffuidc5jmrnhmtd64bfzmapaoohb6twpcmbguluw25kq.ipfs.cf-ipfs.com/";
+
+return (
+  <Widget
+    src={"wendersonpires.near/widget/NearSocialBridgeCore"}
+    props={{
+      externalAppUrl,
+    }}
+  />
+);
