e97eae23991a3250bc5d2a671a725c1ad15e7546
socializer.near <socializer.near>
2023-09-12 15:27:08 +0000

    Update Select by socializer.near at 2023-09-12T15:27:08.669Z

--- socializer.near/Select/source_code.jsx
+++ socializer.near/Select/source_code.jsx
@@ -122,7 +122,7 @@ const Item = styled.button`
   background: transparent;
   border: none;
   transition: background 0.2s ease-in-out;
-
+  width : ${width}px;
   &:nth-child(n + 1) {
     border-top: 1px solid #d0d5dd;
   }
