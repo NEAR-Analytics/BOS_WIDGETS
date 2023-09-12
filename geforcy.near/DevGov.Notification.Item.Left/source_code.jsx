664c93c3d74777a8a292b38151b033fd10c50e99
geforcy.near <geforcy.near>
2023-09-12 21:21:56 +0000

    Update DevGov.Notification.Item.Left by geforcy.near at 2023-09-12T21:21:56.563Z

--- geforcy.near/DevGov.Notification.Item.Left/source_code.jsx
+++ geforcy.near/DevGov.Notification.Item.Left/source_code.jsx
@@ -1,4 +1,56 @@
-const { href } = VM.require("geforcy.near/widget/core.lib.url") || (() => {});
+/* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+
+  const linkPropsQuery = Object.entries(linkProps)
+    .filter(([_key, nullable]) => (nullable ?? null) !== null)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
 
 if (!props.type) {
   return "Loading ...";
@@ -16,16 +68,7 @@ return props.type ? (
       : type == "mention"
       ? "mentioned you in their"
       : "???"}{" "}
-    <a
-      className="fw-bold text-muted"
-      href={href({
-        widgetSrc: "geforcy.near/widget/app",
-        params: {
-          page: "post",
-          id: props.post,
-        },
-      })}
-    >
+    <a className="fw-bold text-muted" href={href("Post", { id: props.post })}>
       DevHub post
     </a>
   </>
