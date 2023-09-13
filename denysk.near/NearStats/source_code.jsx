b561b61c75e595aa862f558f77cec1faa264fc3d
denysk.near <denysk.near>
2023-09-13 09:32:53 +0000

    Update NearStats by denysk.near at 2023-09-13T09:32:53.016Z

---
+++ denysk.near/NearStats/source_code.jsx
@@ -0,0 +1,85 @@
+const data = fetch("https://front.near-staking.com/api/near", {
+  subscribe: true,
+  method: "GET",
+  headers: {
+    Accept: "*/*",
+  },
+});
+
+if (data !== null && data.ok === false) {
+  return (
+    <div className="text-bg-light rounded-4 p-3 mb-3">
+      <p>houston we have a problem, wen mon ser?</p>
+    </div>
+  );
+} else {
+  return (
+    <div className="text-bg-light rounded-4 p-3 mb-3">
+      {data !== null ? (
+        <p>
+          <div class="d-flex clearfix flex-wrap flex-column flex-sm-row">
+            <div class="p-2">
+              <div>TPS Now</div>
+              <span class="text-success h3">
+                <b>{JSON.parse(data.body.tps).toFixed(0)}</b>
+              </span>
+              <small>block: </small>
+              <b>{JSON.parse(data.body.tps_block)}</b>
+            </div>
+            <div class="p-2">
+              <div>TPS 30D Peak</div>
+              <span class="text-success h3">
+                <b>{JSON.parse(data.body.highest_tps).toFixed(0)}</b>
+              </span>
+              <small>in block: </small>
+              <a
+                target="_blank"
+                href={
+                  "https://nearblocks.io/blocks/" +
+                  data.body.highest_tps_block_hash
+                }
+              >
+                {JSON.parse(data.body.highest_tps_block)}
+              </a>
+            </div>
+            <div class="p-2">
+              <div>Average Block Time (ABT) Now</div>
+              <small>last 30 seconds</small>
+              <span class="text-success h3">
+                <b>{JSON.parse(data.body.avg_block_time).toFixed(2)}</b> sec
+              </span>
+            </div>
+            <div class="p-2">
+              <div>ABT 30D High</div>
+              <span class="h5">
+                <b>{JSON.parse(data.body.block_time_high).toFixed(2)}</b> sec
+              </span>
+            </div>
+            <div class="p-2">
+              <div>ABT 30D Low</div>
+              <span class="h5">
+                <b>{JSON.parse(data.body.block_time_low).toFixed(2)}</b> sec
+              </span>
+            </div>
+          </div>
+          <div class="p-2">
+            <small>
+              widget sponsored by{" "}
+              <a
+                target="_blank"
+                style={{ color: "inherit" }}
+                variant="caption"
+                rel="nofollow"
+                href="https://www.nearweek.com"
+              >
+                <b>NEARWEEK</b>
+              </a>
+            </small>
+          </div>
+        </p>
+      ) : (
+        <div>Loading ...</div>
+      )}
+    </div>
+  );
+}
