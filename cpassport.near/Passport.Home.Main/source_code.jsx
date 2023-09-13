d6514e1ba72a3a7c8a0e000307819a51a9e828c4
cpassport.near <cpassport.near>
2023-09-13 08:28:44 +0000

    Update Passport.Home.Main by cpassport.near at 2023-09-13T08:28:44.986Z

--- cpassport.near/Passport.Home.Main/source_code.jsx
+++ cpassport.near/Passport.Home.Main/source_code.jsx
@@ -1,1 +1,17 @@
-return <div>Add navigation to passport here</div>;
+/**ToDo
+ * Add navigation and add font
+ */
+return (
+  <>
+    <h1>AROUND THE WORLD</h1>
+    <p className="instructions">
+      <ul>
+        <li>Visit with Artists</li>
+        <li>Scan the QR Codes</li>
+        <li>Collect Stamps</li>
+        <li>Unlock Prizes</li>
+      </ul>
+    </p>
+    <div>Add navigation to passport here</div>
+  </>
+);
