85e72138574cb3eca8f36eb3926d5c92bc70a798
cpassport.near <cpassport.near>
2023-09-13 08:28:26 +0000

    Update Passport.Leaderboard.Main by cpassport.near at 2023-09-13T08:28:26.499Z

--- cpassport.near/Passport.Leaderboard.Main/source_code.jsx
+++ cpassport.near/Passport.Leaderboard.Main/source_code.jsx
@@ -1,1 +1,11 @@
-return <div>Leaderboard, add series here</div>;
+/**
+ * Frequent flyers
+ * add name of person and last stamps
+ * need to count whther a person is in shard dog mint and then have has or not and display grayed out
+ * create iamge of each passport with opacity to default
+ * going to be challnege to see who ahs most in a series
+ */
+return <>
+<h1>Frequent Flyers</h1>
+<div>Leaderboard, add series here
+</div></>;
