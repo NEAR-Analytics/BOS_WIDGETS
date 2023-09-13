3430f7a4f820b251dec83a2172db3212bc25f1ce
harrydhillon.near <harrydhillon.near>
2023-09-13 15:09:32 +0000

    Update QRcodeReader by harrydhillon.near at 2023-09-13T15:09:32.161Z

--- harrydhillon.near/QRcodeReader/source_code.jsx
+++ harrydhillon.near/QRcodeReader/source_code.jsx
@@ -17,19 +17,20 @@ return (
       }}
     >
       <div>
-
-          <QRCodeReader
-            facingMode={"front"}
-            onScan={(data) => {
-              if (!state?.text) State.update({ text: data?.text });
-            }}
-            style={{ width: 300, height: 250 }}
-          />
-        
+        <QRCodeReader
+          key="environment"
+          constraints={{
+    audio: false,
+    video: { facingMode: "environment" }}}
+          onScan={(data) 
+          => {
+            if (!state?.text) State.update({ text: data?.text });
+          }}
+          style={{ width: 300, height: 250 }}
+        />
 
         <p>QR Code Result : {state.text ?? "Result Placeholder Text"}</p>
       </div>
     </div>
-  
   </>
 );
