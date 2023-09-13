95b646e8aff2dd7caa5bc53264bbfb9cf8354831
andyh.near <andyh.near>
2023-09-13 00:39:36 +0000

    Update ComponentIdTestChild by andyh.near at 2023-09-13T00:39:36.961Z

--- andyh.near/ComponentIdTestChild/source_code.jsx
+++ andyh.near/ComponentIdTestChild/source_code.jsx
@@ -6,7 +6,7 @@ return (
     Hello {props.id}
     <button
       onClick={() => {
-        console.log(componentInstanceId, typeof __bweMeta);
+        // console.log(componentInstanceId, typeof __bweMeta);
         State.update({ value: state.value + 1 });
         props.update(props.id);
       }}
