a1eb6c0501b04da9cdeb4190a7bd6dc0f18f59b5
chloe.near <chloe.near>
2023-09-13 19:51:54 +0000

    Update iframe by chloe.near at 2023-09-13T19:51:54.991Z

--- chloe.near/iframe/source_code.jsx
+++ chloe.near/iframe/source_code.jsx
@@ -1,19 +1,58 @@
-// Assuming styled-components is imported
+// Your original styled components
 const Container = styled.div`
   background-color: #000;
   color: #fff;
 `;
 
-// Your React component
-const YourComponent = () => {
-  return (
-    <Container>
+const Title = styled.h1`
+  color: #fff;
+  font-weight: 700;
+  font-size: 2em;
+  text-align: center;
+  text-transform: uppercase;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.p`
+  color: #fff;
+  font-weight: 300;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+// Style for Iframe container
+const IframeContainer = styled.div`
+  position: relative;
+  overflow: hidden;
+  padding-top: 56.25%;
+`;
+
+return (
+  <Container>
+    {/* Embedding the iframe */}
+    <IframeContainer>
       <iframe
-        src="https://pd.marmaj.org/pixelpets"
-        width="800"
-        height="600"
-        title="PixelPets-Marmaj-iframe"
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0",
+          width: "100%",
+          height: "100%",
+        }}
+        src="https://pd.marmaj.org/"
+        title="Marma-J-Gaming-iframe"
       ></iframe>
-    </Container>
-  );
-};
+    </IframeContainer>
+    <Widget
+      src="jay100.near/widget/CTTDescription"
+      props={{
+        title: "Marma J Gaming",
+        description: `Explore the crypto-based gaming world of Marma J Gaming where you can collaborate artistically with friends, gather your pets for a battle, outfit your hero to explore dungeons and take part in raids, and gather an army to position yourself as the master tactician.`,
+      }}
+    />
+    <Widget src={"jay100.near/widget/PixelParty-Widget"} />
+    <Widget src={"jay100.near/widget/PixelPets-Widget"} />
+    <Widget src={"jay100.near/widget/CH-Widget"} />
+    <Widget src={"jay100.near/widget/CTT-Widget"} />
+  </Container>
+);
