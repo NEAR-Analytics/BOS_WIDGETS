4a6eefcce2ed977416836d65bf46ac38d625d986
joelblenman21.near <joelblenman21.near>
2023-09-13 16:17:36 +0000

    Update Greeter by joelblenman21.near at 2023-09-13T16:17:36.353Z

---
+++ joelblenman21.near/Greeter/source_code.jsx
@@ -0,0 +1,9 @@
+// define the HTML and JavaScript code for the canvas element
+
+const url = "https://pixelparty.marmaj.org/";
+
+return (
+  <div>
+    <iframe iframeResizer src={url} />
+  </div>
+);
