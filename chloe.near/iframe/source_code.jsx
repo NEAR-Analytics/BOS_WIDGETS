e47f52cf6d3a0e771a01d453cc501e71086a5187
chloe.near <chloe.near>
2023-09-13 19:45:03 +0000

    Update iframe by chloe.near at 2023-09-13T19:45:03.649Z

--- chloe.near/iframe/source_code.jsx
+++ chloe.near/iframe/source_code.jsx
@@ -1,56 +1,19 @@
-// Styled-components
-const ResponsiveIframe = styled.div`
-  position: relative;
-  overflow: hidden;
-  padding-top: 56.25%;
-  iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-`;
-
+// Assuming styled-components is imported
 const Container = styled.div`
   background-color: #000;
   color: #fff;
-`;
-
-const Title = styled.h1`
-  color: #fff;
-  font-weight: 700;
-  font-size: 2em;
-  text-align: center;
-  text-transform: uppercase;
-  margin-bottom: 20px;
-`;
-
-const Description = styled.p`
-  color: #fff;
-  font-weight: 300;
-  text-align: center;
-  margin-bottom: 20px;
 `;
 
 // Your React component
 const YourComponent = () => {
   return (
     <Container>
-      <Title>Marma J Gaming</Title>
-      <Description>
-        Explore the crypto-based gaming world of Marma J Gaming...
-      </Description>
-      <ResponsiveIframe>
-        <iframe
-          src="https://pd.marmaj.org/pixelpets"
-          title="PixelPets-Marmaj-iframe"
-        ></iframe>
-      </ResponsiveIframe>
-      <Widget src={"jay100.near/widget/PixelParty-Widget"} />
-      <Widget src={"jay100.near/widget/PixelPets-Widget"} />
-      <Widget src={"jay100.near/widget/CH-Widget"} />
-      <Widget src={"jay100.near/widget/CTT-Widget"} />
+      <iframe
+        src="https://pd.marmaj.org/pixelpets"
+        width="800"
+        height="600"
+        title="PixelPets-Marmaj-iframe"
+      ></iframe>
     </Container>
   );
 };
