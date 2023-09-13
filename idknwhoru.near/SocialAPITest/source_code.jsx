55bb886773332dc53e7afc48ae02475fbb545f1a
idknwhoru.near <idknwhoru.near>
2023-09-13 14:33:17 +0000

    Update SocialAPITest by idknwhoru.near at 2023-09-13T14:33:17.701Z

--- idknwhoru.near/SocialAPITest/source_code.jsx
+++ idknwhoru.near/SocialAPITest/source_code.jsx
@@ -3,7 +3,14 @@ const getSocial = Social.get(["hangil11.near/collegium/post/*"]);
 // const getSocial = Social.get(["hangil11.near/collegium/post/*", "junho9341.near/collegium/post/*"])
 
 const getr = Social.getr("*/collegium/post", "final");
-const index = Social.index("collegium.post", "main");
+const index = Social.index("collegium.post", "main", {
+  order: "desc",
+  limit: 10
+});
+
+const posts = index.filter(({value}) => value.title !== undefined);
+
+console.log(posts)
 
 const keys = Social.keys("*/collegium/post/*", "final", {
   return_type: "BlockHeight",
@@ -15,25 +22,17 @@ const nearGet = Near.view("social.near", "get", {
 
 console.log({ getSocial, getr, index, keys, nearGet });
 
-return <>
-<h1>Social.get</h1>
-<div>
-{JSON.stringify(getSocial)}
-</div>
-<h1>Social.getr</h1>
-<div>
-{JSON.stringify(getr)}
-</div>
-<h1>Social.keys</h1>
-<div>
-{JSON.stringify(keys)}
-</div>
-<h1>Social.index</h1>
-<div>
-{JSON.stringify(index)}
-</div>
-<h1>Near.view</h1>
-<div>
-{JSON.stringify(nearGet)}
-</div>
-</>;
+return (
+  <>
+    <h1>Social.get</h1>
+    <div>{JSON.stringify(getSocial)}</div>
+    <h1>Social.getr</h1>
+    <div>{JSON.stringify(getr)}</div>
+    <h1>Social.keys</h1>
+    <div>{JSON.stringify(keys)}</div>
+    <h1>Social.index</h1>
+    <div>{JSON.stringify(index)}</div>
+    <h1>Near.view</h1>
+    <div>{JSON.stringify(nearGet)}</div>
+  </>
+);
