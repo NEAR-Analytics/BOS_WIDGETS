8b7237763dc875d53fbe7faf86326e43c294d877
opencann.near <opencann.near>
2023-09-13 23:59:50 +0000

    Update profile.page by opencann.near at 2023-09-13T23:59:50.689Z

---
+++ opencann.near/profile.page/source_code.jsx
@@ -0,0 +1,35 @@
+const accountId = props.accountId ?? context.accountId;
+if (!accountId) {
+  return "No account ID";
+}
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+const fast = !props.profile;
+
+if (profile === null) {
+  return "Loading";
+}
+
+return (
+  <div className="py-1 px-1">
+    <div className="mx-auto">
+      <Widget
+        src="opencann.near/widget/profile.header"
+        props={{
+          accountId,
+          profile,
+          link: true,
+          fast,
+          showEditButton: !props.profile,
+        }}
+      />
+
+      <div className="mt-3">
+        <Widget
+          src="mob.near/widget/ProfileTabs"
+          props={{ accountId, profile }}
+        />
+      </div>
+    </div>
+  </div>
+);
