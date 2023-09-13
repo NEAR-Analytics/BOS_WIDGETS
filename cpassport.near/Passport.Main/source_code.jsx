de69e953d674aba14b792ae1a942673e997bebcc
cpassport.near <cpassport.near>
2023-09-13 01:35:01 +0000

    Update Passport.Main by cpassport.near at 2023-09-13T01:35:01.793Z

---
+++ cpassport.near/Passport.Main/source_code.jsx
@@ -0,0 +1,8 @@
+/**
+ * put cdao passport with all sharddog links to different passport
+ * put a query for gallery 
+ * get series 
+ */
+const accountId = props.accountId ?? "sharddog.near";
+
+return <div>{accountId}</div>;
