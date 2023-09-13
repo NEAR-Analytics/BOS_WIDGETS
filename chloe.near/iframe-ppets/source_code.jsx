842814359daef69d9152c3c69669e5c32ce8b453
chloe.near <chloe.near>
2023-09-13 19:40:40 +0000

    Update iframe-ppets by chloe.near at 2023-09-13T19:40:40.968Z

--- chloe.near/iframe-ppets/source_code.jsx
+++ chloe.near/iframe-ppets/source_code.jsx
@@ -1,9 +1,16 @@
 return (
-  <div>
+  <div
+    style={{ position: "relative", overflow: "hidden", paddingTop: "56.25%" }}
+  >
     <iframe
+      style={{
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+      }}
       src="https://pd.marmaj.org/pixelpets"
-      width="800"
-      height="600"
       title="PixelPets-Marmaj-iframe"
     ></iframe>
   </div>
