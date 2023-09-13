c32cf63c2dc8bee93d9dfb06aa12800c3058ec79
chloe.near <chloe.near>
2023-09-13 19:41:35 +0000

    Update iframe by chloe.near at 2023-09-13T19:41:35.117Z

--- chloe.near/iframe/source_code.jsx
+++ chloe.near/iframe/source_code.jsx
@@ -1,10 +1,17 @@
 return (
-  <div>
+  <div
+    style={{ position: "relative", overflow: "hidden", paddingTop: "56.25%" }}
+  >
     <iframe
-      src="https://pd.marmaj.org/"
-      width="800"
-      height="600"
-      title="Marmaj-iframe"
+      style={{
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+      }}
+      src="https://pd.marmaj.org"
+      title="PixelPets-Marmaj-iframe"
     ></iframe>
   </div>
 );
