8eb1ba0830495bbb564e436078260d77fb7167ef
andyh.near <andyh.near>
2023-09-13 18:12:39 +0000

    Update MainPage by andyh.near at 2023-09-13T18:12:39.455Z

--- andyh.near/MainPage/source_code.jsx
+++ andyh.near/MainPage/source_code.jsx
@@ -1,17 +1,74 @@
-// console.log(`RENDERING ${props.id}`, { ...props });
-State.init({ value: props.value || 0 });
-
 return (
-  <div>
-    Hello {props.id}
-    <button
-      onClick={() => {
-        // console.log(componentInstanceId, typeof __bweMeta);
-        State.update({ value: state.value + 1 });
-        props.update(props.id);
-      }}
-    >
-      increment {state.value}
-    </button>
-  </div>
+  <>
+    {context.accountId && <Widget src="andyh.near/widget/ProfileOnboarding" />}
+    <Widget src="andyh.near/widget/MainPage.FeaturedApps" props={{}} />
+    <div className="row mb-3">
+      <Widget src="andyh.near/widget/Applications" />
+    </div>
+    <div className="row mb-3">
+      <Widget src="andyh.near/widget/People" />
+    </div>
+    <div className="row mb-3">
+      <div>
+        <h4>Get involved</h4>
+        <div className="mb-2 d-flex gap-2 flex-wrap">
+          <a
+            className="btn btn-outline-primary"
+            href="https://thewiki.near.page/PastPresentAndFutureOfNearSocial"
+          >
+            What's Near Social?
+          </a>
+          <a
+            className="btn btn-outline-primary"
+            href="https://thewiki.near.page/near.social_docs"
+          >
+            Documentation
+          </a>
+        </div>
+        <div className="mb-2 d-flex gap-2 flex-wrap">
+          <a
+            className="btn btn-outline-secondary border-0"
+            href="#/andyh.near/widget/ProfilePage?accountId=self.social.near"
+          >
+            <i className="bi bi-person-circle"></i>
+          </a>
+          <a
+            className="btn btn-outline-secondary border-0"
+            href="https://t.me/NearSocial"
+          >
+            <i className="bi bi-telegram"></i>
+          </a>
+          <a
+            className="btn btn-outline-secondary border-0"
+            href="https://github.com/NearSocial"
+          >
+            <i className="bi bi-github"></i>
+          </a>
+          <a
+            className="btn btn-outline-secondary border-0"
+            href="https://twitter.com/NearSocial_"
+          >
+            <i className="bi bi-twitter"></i>
+          </a>
+          <a
+            className="btn btn-outline-secondary border-0"
+            href="https://thewiki.near.page/near.social"
+          >
+            <i className="bi bi-wikipedia"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div className="row mb-3">
+      <div className="col-md-8">
+        <h4>Follow activity</h4>
+        <Widget src="andyh.near/widget/FollowFeed" />
+      </div>
+      <div className="col-md-4">
+        <h4>Poke activity</h4>
+        <Widget src="andyh.near/widget/PokeFeed" />
+      </div>
+    </div>
+  </>
 );
