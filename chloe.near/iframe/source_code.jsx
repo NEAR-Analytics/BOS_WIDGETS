c91340505150c8d944355f2b67a2f2471d0f852c
chloe.near <chloe.near>
2023-09-13 19:44:42 +0000

    Update iframe by chloe.near at 2023-09-13T19:44:42.751Z

--- chloe.near/iframe/source_code.jsx
+++ chloe.near/iframe/source_code.jsx
@@ -1,17 +1,56 @@
-return (
-  <div
-    style={{ position: "relative", overflow: "hidden", paddingTop: "56.25%" }}
-  >
-    <iframe
-      style={{
-        position: "absolute",
-        top: "0",
-        left: "0",
-        width: "100%",
-        height: "100%",
-      }}
-      src="https://pd.marmaj.org"
-      title="PixelPets-Marmaj-iframe"
-    ></iframe>
-  </div>
-);
+// Styled-components
+const ResponsiveIframe = styled.div`
+  position: relative;
+  overflow: hidden;
+  padding-top: 56.25%;
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Container = styled.div`
+  background-color: #000;
+  color: #fff;
+`;
+
+const Title = styled.h1`
+  color: #fff;
+  font-weight: 700;
+  font-size: 2em;
+  text-align: center;
+  text-transform: uppercase;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.p`
+  color: #fff;
+  font-weight: 300;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+// Your React component
+const YourComponent = () => {
+  return (
+    <Container>
+      <Title>Marma J Gaming</Title>
+      <Description>
+        Explore the crypto-based gaming world of Marma J Gaming...
+      </Description>
+      <ResponsiveIframe>
+        <iframe
+          src="https://pd.marmaj.org/pixelpets"
+          title="PixelPets-Marmaj-iframe"
+        ></iframe>
+      </ResponsiveIframe>
+      <Widget src={"jay100.near/widget/PixelParty-Widget"} />
+      <Widget src={"jay100.near/widget/PixelPets-Widget"} />
+      <Widget src={"jay100.near/widget/CH-Widget"} />
+      <Widget src={"jay100.near/widget/CTT-Widget"} />
+    </Container>
+  );
+};
