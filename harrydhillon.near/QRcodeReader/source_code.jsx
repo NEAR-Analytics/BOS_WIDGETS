9bc1e50a635b44bef1c561f019c14c2fdd59fbd4
harrydhillon.near <harrydhillon.near>
2023-09-13 14:33:25 +0000

    Update QRcodeReader by harrydhillon.near at 2023-09-13T14:33:25.034Z

--- harrydhillon.near/QRcodeReader/source_code.jsx
+++ harrydhillon.near/QRcodeReader/source_code.jsx
@@ -17,18 +17,7 @@ return (
       }}
     >
       <div>
-        {state.facingBack ? (
-          <>
-          <QRCodeReader
-            facingMode={"rear"}
-            onScan={(data) => {
-              if (!state?.text) State.update({ text: data?.text });
-            }}
-            style={{ width: 300, height: 250 }}
-          />
-          <p>back camera</p>
-          </>
-        ) : (
+
           <QRCodeReader
             facingMode={"front"}
             onScan={(data) => {
@@ -36,18 +25,11 @@ return (
             }}
             style={{ width: 300, height: 250 }}
           />
-        )}
+        
 
         <p>QR Code Result : {state.text ?? "Result Placeholder Text"}</p>
       </div>
     </div>
-    <Widget
-      src="nui.sking.near/widget/Input.Checkbox"
-      props={{
-        label: "Facing Back Camera",
-        checked: state.facingBack,
-        onChange: () => State.update({ facingBack: !state.facingBack }),
-      }}
-    />
+  
   </>
 );
