a4f8b742296ca6ab04eac31247da28a9a600edb3
let45fc.near <let45fc.near>
2023-09-14 16:00:47 +0000

    Update LaserChess3D by let45fc.near at 2023-09-14T16:00:47.549Z

--- let45fc.near/LaserChess3D/source_code.jsx
+++
@@ -1,8 +0,0 @@
-return (
-  <h1>
-    Redeployed to{" "}
-    <a href="https://near.org/ostolex.near/widget/LaserChess3D">
-      https://near.org/ostolex.near/widget/LaserChess3D
-    </a>
-  </h1>
-);
