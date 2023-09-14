8a48a675e9980754446c0991939983bbd194c203
opencann.near <opencann.near>
2023-09-14 17:45:11 +0000

    Update substanceDB.cannabis by opencann.near at 2023-09-14T17:45:11.925Z

---
+++ opencann.near/substanceDB.cannabis/source_code.jsx
@@ -0,0 +1,106 @@
+const cannabis = {
+  consumptionMethod,
+  physicalEffects,
+  visualEffects,
+  auditoryEffects,
+  cognitiveEffects,
+  multisensoryEffects,
+};
+
+const consumptionMethod = {
+  consumptionMethod: [
+    "Smoke",
+    "Vapor (dry herb)",
+    "Vapor (concentrate)",
+    "Edible",
+    "Drink",
+    "Tincture",
+    "Topical",
+    "Sublingual/buccal",
+  ],
+};
+
+const physicalEffects = {
+  physicalEffects: [
+    "Appetite enhancement",
+    "Appetite suppression",
+    "Bodily pressures (e.g. eye)",
+    "Changes felt in gravity",
+    "Decreased blood pressure",
+    "Dizziness",
+    "Dehydration",
+    "Dry mount",
+    "Gustatory (taste) enhancement",
+    "Increased heart rate",
+    "Increased perspiration",
+    "Insomnia",
+    "Loss of motor control",
+    "Muscle relaxation",
+    "Muscle spasms",
+    "Nausea",
+    "Nausea suppression",
+    "Pain relief",
+    "Physical euphoria",
+    "Red eye",
+    "Sedation",
+    "Seizure suppression",
+    "Spontaneous bodily sensations",
+    "Tactile enhancement",
+  ],
+};
+const visualEffects = {
+  visualEffects: [
+    "Color enhancement",
+    "Blurry vision",
+    "Brightness alteration",
+    "Pattern recognition enhancement",
+    "Tracers",
+    "Geometry (visuals)",
+    "Internal hallucination",
+  ],
+};
+
+const auditoryEffects = {
+  auditoryEffects: ["Enhancements", "Distortions", "Hallucinations"],
+};
+
+const cognitiveEffects = {
+  cognitiveEffects: [
+    "Anxiety",
+    "Anxiety suppression",
+    "Analysis enhancement",
+    "Analysis suppression",
+    "Conceptual thinking",
+    "Cognitive euphoria",
+    "Creativity enhancement",
+    "Delusion",
+    "Depersonalization",
+    "Dream suppression",
+    "Decreased libido",
+    "Increased libido",
+    "Emotion enhancement",
+    "Feelings of impending doom",
+    "Focus suppression",
+    "Focus enhancement",
+    "Immersion enhancement",
+    "Increased music appreciation",
+    "Increased sense of humor",
+    "Laughter fits",
+    "Memory suppression",
+    "Mindfulness",
+    "Motivation suppression",
+    "Novelty enhancement",
+    "Paranoia",
+    "Personal meaning enhancement",
+    "Psychosis",
+    "Sleepiness",
+    "Suggestibility enhancement",
+    "Thought connectivity",
+    "Thought deceleration",
+    "Time distortion",
+  ],
+};
+
+const multisensoryEffects = { multisensoryEffects: ["Synaesthesia"] };
+
+const category = [];
