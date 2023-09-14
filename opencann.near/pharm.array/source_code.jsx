93d056571b84952678c1acff12f5e643dc643d4e
opencann.near <opencann.near>
2023-09-14 08:04:04 +0000

    Update pharm.array by opencann.near at 2023-09-14T08:04:04.619Z

---
+++ opencann.near/pharm.array/source_code.jsx
@@ -0,0 +1,328 @@
+const label = props.label ?? "Label";
+const placeholder = props.placeholder ?? "Placeholder";
+const value = props.value ?? "";
+const options = props.options ?? pharmArray;
+const onChange = props.onChange ?? (() => {});
+const validate = props.validate ?? (() => {});
+const error = props.error ?? "";
+const labelKey = props.labelKey ?? "name";
+
+const pharmArray = [
+  { indication: "Antidepressants",
+    { category: "Atypical",
+        drugs: [
+  { name: "Agomelatine" },
+  { name: "Bupropion (Wellbutrin)" },
+  { name: "Lamotrigine" },
+  { name: "Mirtazapine (Remeron)" },
+  { name: "Tianeptine" },
+  { name: "Tianeptine ethyl ester" },
+  { name: "Trazodone" },
+]},
+    { category: "SSRI",
+       drugs: [
+  { name: "Citalopram" },
+  { name: "Bescitalopram (Lexapro)" },
+  { name: "Fluoxetine (Prozac)" },
+  { name: "Fluvoxamine" },
+  { name: "Paroxetine (Paxil)" },
+  { name: "Sertraline (Zoloft)" },
+]}}
+
+"
+SNRI
+Atomoxetine (Strattera)
+Desvenlafaxine
+Venlafaxine (Effexor)
+Tricyclic antidepressants
+Amitriptyline
+MAOI/RIMA
+Moclobemide
+Phenelzine
+Selegiline
+Common medicineChain-broken.svg
+Antipsychotics
+First generation
+Chlorprothixene (Truxal)
+Haloperidol (Haldol)
+Levomepromazine (Nozinan)
+Prochlorperazine
+Promazine
+Second generation (atypical antipsychotics)
+Aripiprazole (Abilify)
+Clotiapine (Entumine)
+Clozapine
+Olanzapine (Zyprexa)
+Quetiapine (Seroquel)
+Risperidone (Risperdal)
+Nonspecific antidotes
+Activated charcoal † -- for orally ingested substances
+
+Specific antidotes
+Acetylcysteine †
+For acetaminophen overdose
+Atropine †
+For nerve agent poisoning
+Calcium gluconate †
+Deferoxamine †
+Dimercaprol †
+Ethanol
+For methanol toxicity
+Flumazenil
+For benzodiazepine overdose
+Fomepizole †
+For methanol toxicity
+Methylthioninium chloride (methylene blue) †
+Note: A MAOI sold as dying agent (easy to obtain)
+Nalmefene
+For opioid overdose
+Naloxone †
+For opioid overdose
+Penicillamine †
+Physostigmine: For overdose on deliriants (e.g. diphenhydramine)
+Prussian blue †
+Sodium calcium edetate
+Sodium nitrite †
+Sodium thiosulfate †
+Succimer †
+Afinils
+Armodafinil
+Modafinil
+Anticonvulsants/antiepileptics
+Carbamazepine †
+Diazepam † -- Adults: Severe acute anxiety or agitation: 10 mg IV or IM injection.[1]
+Ethosuximide †
+Lamotrigine †
+Lorazepam †
+Magnesium sulfate †
+Midazolam †
+Phenobarbital †
+Phenytoin †
+Valproic acid †
+Antihypertensive drugs
+Antidotes to reverse the severe peripheral vasoconstriction which can occur as a result of overdose with certain 5-HT2A agonist drugs such as 25I-NBOMe, DOB, and Bromo-DragonFLY)
+
+Ketanserin ‡ (for hypertension caused by serotonergic agents)
+Sodium nitroprusside †, nitroprusside ‡. Nonsympatholytic vasodilatory antihypertensives
+Tolazoline ‡ (for hypertension caused by adrenergic agents
+Antipsychotics
+First-Generation (Typical) Antipsychotics:
+
+Chlorpromazine †‡
+Haloperidol (Haldol) †‡
+Prochlorperazine ‡
+Second-Generation (Atypical) Antipsychotics:
+
+Clozapine †‡
+Fluphenazine †‡
+Olanzapine (Zyprexa) ‡
+Risperidone (Risperdal) †‡
+Quetiapine (Seroquel) ‡
+Third-Generation
+
+Aripiprazole (Abilify) ‡
+Brexpiprazole ‡
+Cariprazine ‡
+Lurasidone (Latuda) ‡
+Anxiolytics
+Benzodiazepines
+Alprazolam (Xanax)‡
+Diazepam †‡
+Serotonin syndrome treatment
+Cyproheptadine[2]
+Dantrolene
+Methysergide[2]
+SSRIs
+Fluoxetine †‡
+Sertraline †‡
+Misc
+Antidementia agent: Ergoloid
+Combination drugs
+Deudextromethorphan/quinidine (d-DXM/Q) (see also: DXM)
+Dextromethorphan/bupropion (Auvelity) (see also: DXM)
+Dextromethorphan/quinidine (Nuedexta) (see also: DXM)
+Recreational/entheogenic use
+This list is synchronized with Erowid Pharmaceutical Vaults.
+
+Substances frequently used outside indication. They may be used in conjunction with other substances (example, MAOIs), or produce a different effect than indicated in heavy doses.
+
+UnsortedChain-broken.svg
+Bupropion
+MAOI: Pharmahuasca is a pharmaceutical version of the entheogenic brew ayahuasca. Traditional ayahuasca is made by brewing the MAOI-containing Banisteriopsis caapi vine with a DMT-containing plant, such as Psychotria viridis. Pharmahuasca refers to a similar combination that uses a pharmaceutical MAOI instead of a plant.
+
+DissociativesChain-broken.svg
+Arylcyclohexylamines
+Ketamine
+Morphinans
+Dextromethorphan (DXM)
+Others
+Memantine
+DeliriantsSpider.svg
+Tropane alkaloids
+Atropine
+Hyoscyamine
+Scopolamine
+Antihistamines
+Diphenhydramine (DPH)
+Doxylamine
+Others
+Benzydamine
+Biperiden
+Cyclizine
+Dimenhydrinate
+Mirtazapine
+CannabinoidsCannabis leaf (1).svg
+Phytocannabinoids
+Cannabidiol (CBD) (oral): Firstly: Vaped or smoked CBD heated to 250-300 C will partially be converted to THC.[3] Thus it's inconvenient to smoke or vape CBD to relieve a THC toxicity. Secondly: 50 times more CBD than THC is needed to reduce acute effects of THC,[4] which makes it practically impossible to administer proper dosage by inhalation. Also, the quantity of CBD converted into THC will increase a lot with the required ratio, and excessive harshness in the lungs may escalate a bad trip.
+Tetrahydrocannabinol (THC)
+MiscellaneousCogs.svg
+GABAA-ρ receptor agonists
+Zolpidem
+Zopiclone
+Oneirogens
+Galantamine
+Melatonin
+Sigmaergics
+Dextromethorphan (DXM)
+Combinations
+8-Chlorotheophylline & dimenhydrinate
+THC/CBD
+Inhalants
+Nitrous oxide
+Xenon
+DepressantsArrow-down.svg
+Misc
+Clonidine
+Promethazine
+Triazolam
+Benzodiazepines
+Alprazolam (Xanax)
+Clonazepam (Klonopin)
+Diazepam (Valium)
+Etizolam
+Flunitrazepam (Rohypnol)
+Lorazepam (Ativan)
+Midazolam
+Oxazepam
+Phenazepam
+Temazepam
+Thienodiazepines
+Etizolam
+Barbiturates
+Amobarbital
+Pentobarbital
+Phenobarbital
+Secobarbital
+Opioids
+Buprenorphine
+Codeine
+Dextropropoxyphene (Darvon)
+Diacetylmorphine (Diamorphine / Heroin)
+Dihydrocodeine
+Ethylmorphine (pharmaceutical?)
+Fentanyl
+Hydrocodone (Vicodin)
+Hydromorphone (Dilaudid)
+Methadone
+Morphine
+Oxycodone (Oxycontin)
+Oxymorphone (Opana)
+Pethidine (Meperidine)
+Sufentanil
+Tapentadol
+Tramadol
+GABAergics
+Alcohol
+Baclofen
+Carisoprodol
+GHB
+Methaqualone
+Tiagabine
+Gabapentinoids
+Gabapentin
+Phenibut
+Pregabalin"
+];
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 0px;
+  gap: 0.45em;
+  width: 100%;
+
+  .typeahead {
+    width: 100%;
+
+    & > div {
+      padding: 0.5em 0.75em;
+      background: #ffffff;
+      border: 1px solid #d0d5dd;
+      box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+      border-radius: 4px;
+      color: #101828;
+    }
+
+    .rbt-token {
+      background: #f2f4f7;
+      border: 1px solid #d0d5dd;
+      border-radius: 3px;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 0.95em;
+      line-height: 1.25em;
+      text-align: center;
+      color: #344054;
+
+      & > button {
+        font-size: 1.25em;
+        color: #98a2b3;
+      }
+    }
+  }
+`;
+
+const Label = styled.label`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 0.95em;
+  line-height: 1.25em;
+  color: #344054;
+`;
+
+const Error = styled.span`
+  display: inline-block;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.75em;
+  line-height: 1.25em;
+  color: #ff4d4f;
+  height: 0;
+  overflow: hidden;
+  transition: height 0.3s ease-in-out;
+
+  &.show {
+    height: 1.25em;
+  }
+`;
+
+return (
+  <Container>
+    {props.noLabel ? <></> : <Label>{label}</Label>}
+    <Typeahead
+      id
+      placeholder={placeholder}
+      labelKey={labelKey}
+      onChange={onChange}
+      options={options}
+      selected={value}
+      className="typeahead"
+      positionFixed
+      multiple
+      allowNew
+    />
+    <Error className={error ? "show" : ""}>{error}</Error>
+  </Container>
+);
