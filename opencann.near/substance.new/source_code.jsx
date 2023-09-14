d542e63038d27d89ba56626f3369593443e7aee6
opencann.near <opencann.near>
2023-09-14 17:39:37 +0000

    Update substance.new by opencann.near at 2023-09-14T17:39:37.655Z

--- opencann.near/substance.new/source_code.jsx
+++ opencann.near/substance.new/source_code.jsx
@@ -1,7 +1,3 @@
-const widget = (src, props, other) => (
-  <Widget src={src} props={props} {...other} />
-);
-
 const UUID = {
   generate: (template) => {
     if (typeof template !== "string") {
@@ -15,86 +11,48 @@ const UUID = {
   },
 };
 
-const { handleCreateSubstance, defaultSubstance, buttonChildren, buttonProps } =
-  props;
+const accountId = context.accountId;
 
-State.init({
-  error: undefined,
-  substance: defaultSubstance ?? {
-    id: UUID.generate(),
-    logo: undefined,
-    title: undefined,
-    description: undefined,
-    tags: [],
-  },
-});
+if (!accountId) {
+  return "Please connect your NEAR account";
+}
 
-const update = (k, v) => State.update({ [k]: v });
-const updateP = (k, v) => update("substance", { ...state.substance, [k]: v });
+const addSubstance = (target) => {
+  const thingId = target.data.id;
 
-const beforeHandleCreateSubstance = () => {
-  update("error", undefined);
-  if (!state.substance.title) {
-    update("error", "Name is required");
-  }
-  if (!state.error) {
-    handleCreateSubstance(state.substance);
-  }
+  // index new substance
+  Social.set({
+    thing: {
+      [thingId]: target,
+    },
+    index: {
+      every: JSON.stringify({
+        key: "every.near/type/substance",
+        value: {
+          type: "every.near/type/substance",
+          id: thingId,
+        },
+      }),
+    },
+  });
+
+  State.update({ substance: target });
 };
 
-const IT = "nearui.near/widget/Input.ExperimentalText";
-const SI = "nearui.near/widget/Social.ImageUpload";
-const IB = "nearui.near/widget/Input.Button";
+const proposeSubstance = (target) => {
+  console.log(target);
+};
+
 return (
-  <div className="p-4 bg-white rounded-4">
-    <div className="d-flex flex-column gap-3">
-      <h3>{buttonChildren || "Create Substance"}</h3>
-      {widget(IT, {
-        label: "Substance Name",
-        placeholder: "New substance",
-        inputProps: {
-          defaultValue: state.substance.title,
-        },
-        onChange: (v) => updateP("title", v),
-      })}
-      {widget(IT, {
-        label: "Description",
-        placeholder: "Describe this substance",
-        textarea: true,
-        inputProps: {
-          defaultValue: state.substance.description,
-        },
-        onChange: (v) => updateP("description", v),
-      })}
-      {widget(IT, {
-        label: "Tags",
-        placeholder: "Separate with commas",
-        inputProps: {
-          defaultValue: state.substance.tags.join(", "),
-        },
-        onChange: (v) =>
-          updateP(
-            "tags",
-            (v || "")
-              .split(",")
-              .filter((v) => v !== "")
-              .map((v) => v.trim())
-          ),
-      })}
-      <h6 className="mb-0">Logo</h6>
-      {state.substance.logo && (
-        <img src={state.substance.logo} alt="" height={100} width={100} />
-      )}
-      {widget(SI, {
-        onChange: (v) => updateP("logo", v),
-        value: state.substance.logo,
-      })}
-      <div className="text-danger mt-2">{state.error}</div>
-      {widget(IB, {
-        children: buttonChildren ?? "Create Substance",
-        variant: "success",
-        onClick: () => beforeHandleCreateSubstance(),
-      })}
+  <>
+    <div>
+      <Widget
+        src="itexpert120-contra.near/widget/EventForm"
+        props={{
+          addSubstance: addSubstance,
+          proposeSubstance: proposeSubstance,
+        }}
+      />
     </div>
-  </div>
+  </>
 );
