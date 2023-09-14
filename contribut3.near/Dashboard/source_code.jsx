8b1d75bbdec688cf7bfe2594b69fbfef5fb86f0c
contribut3.near <contribut3.near>
2023-09-14 12:26:22 +0000

    Update Dashboard by contribut3.near at 2023-09-14T12:26:22.326Z

--- contribut3.near/Dashboard/source_code.jsx
+++ contribut3.near/Dashboard/source_code.jsx
@@ -1,4 +1,5 @@
 const ownerId = "contribut3.near";
+const apiUrl = "https://api-staging-fur7.onrender.com";
 
 const Header = styled.div`
   display: flex;
@@ -145,7 +146,7 @@ State.init({
   requestsTodayCount: 0,
 });
 
-asyncFetch("https://api-staging-fur7.onrender.com/transactions/stats").then(
+asyncFetch(`${apiUrl}/transactions/stats`).then(
   (response) =>
     response.ok &&
     State.update({
@@ -156,19 +157,19 @@ asyncFetch("https://api-staging-fur7.onrender.com/transactions/stats").then(
     }),
 );
 
-asyncFetch("https://api-staging-fur7.onrender.com/data/projects?sort=timedesc").then(
+asyncFetch(`${apiUrl}/data/projects?sort=timedesc`).then(
   (response) => response.ok && State.update({ projects: response.body }),
 );
 
-asyncFetch("https://api-staging-fur7.onrender.com/data/requests?sort=timedesc").then(
+asyncFetch(`${apiUrl}/data/requests?sort=timedesc`).then(
   (response) => response.ok && State.update({ requests: response.body }),
 );
 
-asyncFetch("https://api-staging-fur7.onrender.com/data/vendors?sort=timedesc").then(
+asyncFetch(`${apiUrl}/data/vendors?sort=timedesc`).then(
   (response) => response.ok && State.update({ vendors: response.body }),
 );
 
-asyncFetch("https://api-staging-fur7.onrender.com/data/investors?sort=timedesc").then(
+asyncFetch(`${apiUrl}/data/investors?sort=timedesc`).then(
   (response) => response.ok && State.update({ investors: response.body }),
 );
 
