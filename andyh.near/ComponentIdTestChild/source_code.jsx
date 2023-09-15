379a30e4336fbfe07c2ed2fbd51563a1d410cedb
andyh.near <andyh.near>
2023-09-15 22:02:37 +0000

    Update ComponentIdTestChild by andyh.near at 2023-09-15T22:02:37.675Z

--- andyh.near/ComponentIdTestChild/source_code.jsx
+++ andyh.near/ComponentIdTestChild/source_code.jsx
@@ -1,4 +1,3 @@
-// console.log(`RENDERING ${props.id} - ${componentInstanceId}`, { ...props });
 State.init({ value: props.value || 0, n: 0, id: props.id });
 State.update({ id: props.id + "_state" });
 
@@ -7,7 +6,6 @@ return (
     Hello {state.id}
     <button
       onClick={() => {
-        console.log("sent from " + componentInstanceId);
         const newValue = state.value + 1;
         State.update({ value: newValue });
         props.update(newValue);
