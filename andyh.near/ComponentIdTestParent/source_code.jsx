53a152014c1ca29e19569d7ded32be796953f3a3
andyh.near <andyh.near>
2023-09-15 22:05:41 +0000

    Update ComponentIdTestParent by andyh.near at 2023-09-15T22:05:41.568Z

--- andyh.near/ComponentIdTestParent/source_code.jsx
+++ andyh.near/ComponentIdTestParent/source_code.jsx
@@ -10,7 +10,7 @@ const components = [state.a, state.b, state.c];
 return (
   <>
     <h4>{props.title}</h4>
-    <button onClick={() => State.update({ x: state.x + 1 })}>
+    <button className="btn" onClick={() => State.update({ x: state.x + 1 })}>
       increment parent {state.x}
     </button>
     {/*
