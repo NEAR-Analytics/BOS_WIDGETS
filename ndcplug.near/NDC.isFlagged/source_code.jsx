d8cac4dc794148a6658a88108a101a181cf9507e
ndcplug.near <ndcplug.near>
2023-09-15 19:25:38 +0000

    Update NDC.isFlagged by ndcplug.near at 2023-09-15T19:25:38.705Z

---
+++ ndcplug.near/NDC.isFlagged/source_code.jsx
@@ -0,0 +1,140 @@
+
+/**
+ * Check is human
+ * 
+ */
+let accountId = props.accountId ?? context.accountId;
+
+State.init({
+  accountId,
+});
+
+function getAccountsWithProfile() {
+  const data = Social.keys("*/profile", "final");
+
+  if (!data) {
+    return "Loading";
+  }
+
+  const accounts = Object.entries(data);
+
+  const allProfiles = [];
+
+  for (let i = 0; i < accounts.length; ++i) {
+    const accountId = accounts[i][0];
+    allProfiles.push(accountId);
+  }
+  return allProfiles;
+}
+
+const onChangeRecipient = (recipient) => {
+            console.log("Recipient : " + recipient ); 
+
+    isFlagged(recipient);
+  State.update({
+    accountId: recipient,
+  });
+    
+
+};
+
+function isFlagged(account) {
+            console.log("Starting is flagged check : " + account ); 
+
+    const view = Near.view("registry.i-am-human.near", "account_flagged", {
+      account: `${account}`,
+    });
+        console.log("View: "  + view?.[0]?.[1]?.[0]); 
+
+
+}
+
+
+
+const Main = styled.div`
+  display: grid;
+  gap: 3rem;
+  align-content: center;
+  grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));
+  justify-content: center;
+  margin-top: 5px;
+  width: 100%;
+  padding: 1rem;
+  .button {
+    padding: 0.75em 2em;
+    border-radius: 0.7em;
+    border: 1px solid #0d99ff;
+    transition: all 0.3s;
+    cursor: pointer;
+    color: #fff;
+    background: #0d99ff;
+    &:hover {
+      color: #0d99ff;
+      background: #fff;
+    }
+    @media screen and (max-width: 540px) {
+      padding: 0.5em 2em;
+    }
+  }
+`;
+
+
+
+const Card = styled.div`
+  padding: 1em;
+  border: 1px solid #e5e8eb;
+  gap: 2em;
+  margin: 10px auto;
+  border-radius: 0.7em;
+  & input {
+    display: block;
+    padding: 0.5em;
+    width: 100%;
+    border: 1px solid #e5e8eb;
+    border-radius: 10px;
+    outline: none;
+    background: #f4f5f6;
+    color: #525c76;
+    :focus {
+      box-shadow: none;
+      border: 1px solid #0d99ff;
+    }
+    &::placeholder {
+      color: palevioletred;
+    }
+  }
+`;
+
+
+
+
+
+
+return (
+  <>
+
+
+ 
+    
+
+              <Card>
+                Search NEAR Account
+                <Typeahead
+                  id="async-example"
+                  className="type-ahead"
+                  isLoading={isLoading}
+                  labelKey="search"
+                  minLength={1}
+                  options={getAccountsWithProfile()}
+                  onChange={(value) => onChangeRecipient(value)}
+                  placeholder={state.accountId
+                  }
+                />
+              </Card>
+
+    
+
+      <Widget src="nearefi.near/widget/ReFi.DAO.memberCard" props={{accountId: state.accountId}}/>
+
+  </>
+);
