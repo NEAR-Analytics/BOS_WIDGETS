3b33a67ea3c38cacbac470ec044f041769f00a91
harrydhillon.near <harrydhillon.near>
2023-09-15 19:28:27 +0000

    Update ProfilePage.PullRequestIcon by harrydhillon.near at 2023-09-15T19:28:27.062Z

---
+++ harrydhillon.near/ProfilePage.PullRequestIcon/source_code.jsx
@@ -0,0 +1,56 @@
+return (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="26"
+    height="28"
+    viewBox="0 0 26 28"
+    fill="none"
+  >
+    <g clip-path="url(#clip0_1837_12266)">
+      <path
+        d="M21.3097 26.7274C22.7218 26.7274 23.8665 25.5877 23.8665 24.1819C23.8665 22.7761 22.7218 21.6365 21.3097 21.6365C19.8976 21.6365 18.7529 22.7761 18.7529 24.1819C18.7529 25.5877 19.8976 26.7274 21.3097 26.7274Z"
+        stroke="#8F8F8F"
+        stroke-width="2.5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4.6903 6.36386C6.10239 6.36386 7.24712 5.22422 7.24712 3.8184C7.24712 2.41259 6.10239 1.27295 4.6903 1.27295C3.27821 1.27295 2.13348 2.41259 2.13348 3.8184C2.13348 5.22422 3.27821 6.36386 4.6903 6.36386Z"
+        stroke="#8F8F8F"
+        stroke-width="2.5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4.6903 26.7274C6.10239 26.7274 7.24712 25.5877 7.24712 24.1819C7.24712 22.7761 6.10239 21.6365 4.6903 21.6365C3.27821 21.6365 2.13348 22.7761 2.13348 24.1819C2.13348 25.5877 3.27821 26.7274 4.6903 26.7274Z"
+        stroke="#8F8F8F"
+        stroke-width="2.5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4.69034 6.36377V21.6365"
+        stroke="#8F8F8F"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13 5.09082H18.7528C20.1649 5.09082 21.3097 6.23046 21.3097 7.63627V21.6363"
+        stroke="#8F8F8F"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M16.8352 8.90931L13 5.09113L16.8352 1.27295"
+        stroke="#8F8F8F"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_1837_12266">
+        <rect width="25" height="28" fill="white" transform="translate(0.5)" />
+      </clipPath>
+    </defs>
+  </svg>
+);
