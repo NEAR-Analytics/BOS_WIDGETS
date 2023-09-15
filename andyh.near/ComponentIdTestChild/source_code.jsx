9b4d53fdf82ab95ec4a52a3ac6f9e937a942b345
andyh.near <andyh.near>
2023-09-15 22:17:59 +0000

    Update ComponentIdTestChild by andyh.near at 2023-09-15T22:17:59.375Z

--- andyh.near/ComponentIdTestChild/source_code.jsx
+++ andyh.near/ComponentIdTestChild/source_code.jsx
@@ -5,7 +5,7 @@ return (
   <div>
     Hello {state.id}
     <button
-      className="btn btn-info"
+      className="btn btn-info btn-sm"
       type="button"
       onClick={() => {
         const newValue = state.value + 1;
