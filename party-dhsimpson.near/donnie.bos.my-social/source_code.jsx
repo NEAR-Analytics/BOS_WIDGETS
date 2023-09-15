d3594ac1088658a8af52a129a8218de2426cd0bb
party-dhsimpson.near <party-dhsimpson.near>
2023-09-15 19:16:32 +0000

    Update donnie.bos.my-social by party-dhsimpson.near at 2023-09-15T19:16:32.824Z

--- party-dhsimpson.near/donnie.bos.my-social/source_code.jsx
+++ party-dhsimpson.near/donnie.bos.my-social/source_code.jsx
@@ -1,6 +1,6 @@
 const account_id = context.accountId ?? props.accountId;
 
-const profileData = props.profile ?? Social.getr(`idknwhoru.near/profile`);//`idknwhoru.near/profile`
+const profileData = props.profile ?? Social.getr(`idknwhoru.near/profile`); //`idknwhoru.near/profile`
 
 if (profileData === null) {
   return "Loading";
@@ -14,7 +14,7 @@ const MyPageWrapper = styled.div`
 const nearIpfsBaseUrl = "https://ipfs.near.social/ipfs/";
 
 function MyProfile() {
-    const ProfileWrapper = styled.div`
+  const ProfileWrapper = styled.div`
         width: 100%;
         height: 300px;
         background-color: rgba(0,0,0,0.2);
@@ -24,32 +24,31 @@ function MyProfile() {
         margin-bottom: 30px;
     `;
 
-    const ProfileImage = styled.img`
+  const ProfileImage = styled.img`
         width: 150px;
         height: 150px;
         margin: 0 20px;
         border-radius: 100%;
     `;
 
-    const MyName = styled.h2`
+  const MyName = styled.h2`
     `;
 
-    const MyWallet = styled.p`
+  const MyWallet = styled.p`
     `;
 
-    return (
-        <ProfileWrapper>
-            <ProfileImage src={`${nearIpfsBaseUrl}${profileData.image.ipfs_cid}`} alt="profile-image"/>
-            <div>
-                <MyName>
-                    {profileData.name}
-                </MyName>
-                <MyWallet>
-                    {account_id}
-                </MyWallet>
-            </div>
-        </ProfileWrapper>
-    )
+  return (
+    <ProfileWrapper>
+      <ProfileImage
+        src={`${nearIpfsBaseUrl}${profileData.image.ipfs_cid}`}
+        alt="profile-image"
+      />
+      <div>
+        <MyName>{profileData.name}</MyName>
+        <MyWallet>{account_id}</MyWallet>
+      </div>
+    </ProfileWrapper>
+  );
 }
 
 const MySocialDataWrapper = styled.div`
@@ -59,99 +58,132 @@ const MySocialDataWrapper = styled.div`
 `;
 
 function PostData() {
-    const postIdxData = Social.index("post", "main", {limit: 30, order: "desc", accountId: [account_id]});
+  const postIdxData = Social.index("post", "main", {
+    limit: 30,
+    order: "desc",
+    accountId: [account_id],
+  });
 
-    const postBlockHashList = postIdxData.map(idxData => {
-        return idxData.blockHeight
-    });
+  const postBlockHashList = postIdxData.map((idxData) => {
+    return idxData.blockHeight;
+  });
 
-    const postList = postBlockHashList.map(blockHash => {
-        return Social.get(`${account_id}/post/main`, blockHash);
-    });
+  const postList = postBlockHashList.map((blockHash) => {
+    return Social.get(`${account_id}/post/main`, blockHash);
+  });
 
-    function RenderPostListItem(key, value) {
-        const {text, image} = JSON.parse(value);
-        return (
-            <>
-                <p>{text}</p>
-                {image && <img src={`${nearIpfsBaseUrl}${image.ipfs_cid}`} alt="profile-image"/>}
-            </>
-        )
-    }
+  function RenderPostListItem(key, value, idx) {
+    const { text, image } = JSON.parse(value);
+    return (
+      <>
+        <p>
+          <b>#{idx}</b>
+          {text}
+        </p>
+        {image && (
+          <img
+            src={`${nearIpfsBaseUrl}${image.ipfs_cid}`}
+            alt="profile-image"
+          />
+        )}
+      </>
+    );
+  }
 
-    return <SocialData data={postList} renderer={RenderPostListItem} />
+  return <SocialData data={postList} renderer={RenderPostListItem} />;
 }
 
 function WidgetData() {
-    const componentData = Social.getr(`${account_id}/widget`);
+  const componentData = Social.getr(`${account_id}/widget`);
 
-    function RenderWidgetListItem(key, value) {
-        const widgetBaseUrl = `https://near.social/${account_id}/widget/`;
+  function RenderWidgetListItem(key, value) {
+    const widgetBaseUrl = `https://near.social/${account_id}/widget/`;
 
-        const Link = styled.a`
+    const Link = styled.a`
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        text-decoration: none;
+        color: black;
+
+        &:hover, &:focus {
             text-decoration: none;
             color: black;
+        }
+    `;
 
-            &:hover, &:focus {
-                text-decoration: none;
-                color: black;
-            }
-        `;
-        
-        return (
-            <Link href={`${widgetBaseUrl}${key}`} target='_blank'>
-                {key}
-            </Link>
-        )
-    }
+    return (
+      <Link href={`${widgetBaseUrl}${key}`} target="_blank">
+        <p>{key}</p>
+        <Widget
+          src="party-dhsimpson.near/widget/gotoSvg"
+          props={{ width: 15, heigh: 15 }}
+        />
+      </Link>
+    );
+  }
 
-    return <SocialData data={componentData} renderer={RenderWidgetListItem} />
+  return <SocialData data={componentData} renderer={RenderWidgetListItem} />;
 }
 
-function SocialData({data, renderer}) {
-    const SocialDataListItem = styled.li`
-        list-style-type: none;
+function SocialData({ data, renderer }) {
+  const DataHead = styled.h3`
+        border-bottom: 1px solid #DDD;
+        line-height: 2;
+        padding-bottom: 10px;
     `;
-    const MySocialData = styled.div`
+
+  const SocialDataList = styled.ul`
+        padding: 0;
+        margin-top: 20px;
+    `;
+
+  const SocialDataListItem = styled.li`
+        list-style-type: none;
+        width: 100%;
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    `;
+  const MySocialData = styled.div`
         width: 45%;
         min-height: 500px;
     `;
-    return (
-        <MySocialData>
-            <h4>
-                Number of widgets: {Object.keys(data).length}
-            </h4>
-            <ul>
-                {Object.entries(data).map(([key, value])=>{
-                    return (
-                        <SocialDataListItem>
-                            {renderer(key, value)}
-                        </SocialDataListItem>
-                    )
-                })}
-            </ul>
-        </MySocialData>
-    )
+  return (
+    <MySocialData>
+      <DataHead>Number of widgets: {Object.keys(data).length}</DataHead>
+      <SocialDataList>
+        {Object.entries(data).map(([key, value], idx) => {
+          return (
+            <SocialDataListItem>{renderer(key, value, idx)}</SocialDataListItem>
+          );
+        })}
+      </SocialDataList>
+    </MySocialData>
+  );
 }
 
 return (
-    <>
+  <>
     <MyPageWrapper>
-        <MyProfile/>
-        <MySocialDataWrapper>
-            <PostData/>
-            <WidgetData/>
-        </MySocialDataWrapper>
+      <MyProfile />
+      <MySocialDataWrapper>
+        <PostData />
+        <WidgetData />
+      </MySocialDataWrapper>
     </MyPageWrapper>
-  <div>
-    {/*{JSON.stringify(profileData)} */}
-    <br />
-    {/*{JSON.stringify(componentData)}*/}
-    <br />
-    <br />
-    {/*{JSON.stringify(postData)} */}
-    {/*{JSON.stringify(idxData)} */}
-    {/*{JSON.stringify(postList)} */}
-  </div>
+    <div>
+      {/*{JSON.stringify(profileData)} */}
+      <br />
+      {/*{JSON.stringify(componentData)}*/}
+      <br />
+      <br />
+      {/*{JSON.stringify(postData)} */}
+      {/*{JSON.stringify(idxData)} */}
+      {/*{JSON.stringify(postList)} */}
+    </div>
   </>
 );
