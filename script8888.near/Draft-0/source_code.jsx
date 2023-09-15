2fbe86b47900715e73dedafd640edda91b366653
script8888.near <script8888.near>
2023-09-15 14:13:30 +0000

    Update Draft-0 by script8888.near at 2023-09-15T14:13:30.197Z

--- script8888.near/Draft-0/source_code.jsx
+++ script8888.near/Draft-0/source_code.jsx
@@ -1,1 +1,35 @@
-return <div>Hello World</div>;
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #001e00;
+  background-color: #000;
+`;
+
+const Wrapper = styled.div`
+  flex-grow: 1;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 62em;
+  margin: 0 auto;
+`;
+
+const Nav = styled.div`
+`;
+
+return (
+  <Body>
+    <Container>
+      <div>
+        <Nav>
+          <img
+            style={{ maxWidth: "5rem", width: "100%" }}
+            src={props.logo ?? "https://i.ibb.co/tmrN68r/Logo.png"}
+            alt={`${props.brandName ?? "Company"} Logo`}
+          />
+        </Nav>
+      </div>
+    </Container>
+  </Body>
+);
