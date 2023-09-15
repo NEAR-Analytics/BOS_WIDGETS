bc76a6c94ebba3051ec43ae01a47c47894b301db
joelblenman21.near <joelblenman21.near>
2023-09-15 02:03:56 +0000

    Update iframe-ppets by joelblenman21.near at 2023-09-15T02:03:56.754Z

---
+++ joelblenman21.near/iframe-ppets/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <div>
+    <iframe
+      src="https://pd.marmaj.org/pixelpets"
+      width="800"
+      height="600"
+      title="PixelPets-Marmaj-iframe"
+    ></iframe>
+  </div>
+);
