9484989a4649e883780a44c16e58dee07489bdf6
bluebiu.near <bluebiu.near>
2023-09-15 07:16:53 +0000

    Update Arbitrum.Pendle.TradeSwapAction by bluebiu.near at 2023-09-15T07:16:53.379Z

--- bluebiu.near/Arbitrum.Pendle.TradeSwapAction/source_code.jsx
+++ bluebiu.near/Arbitrum.Pendle.TradeSwapAction/source_code.jsx
@@ -813,207 +813,217 @@ const {
 
 if (!swapping || !tradeInfo) return;
 
-const RouterContract = new ethers.Contract(
-  routerAddress,
-  ROUTER_ABI,
-  Ethers.provider().getSigner()
-);
+const handleSwap = () => {
+  const RouterContract = new ethers.Contract(
+    routerAddress,
+    ROUTER_ABI,
+    Ethers.provider().getSigner()
+  );
 
-if (inputCurrency.address === "native")
-  inputCurrency.address = "0x0000000000000000000000000000000000000000";
+  if (inputCurrency.address === "native")
+    inputCurrency.address = "0x0000000000000000000000000000000000000000";
 
-const amount = ethers.utils.parseUnits(
-  inputCurrencyAmount,
-  inputCurrency.decimals
-);
-console.log("amount", amount, tradeInfo.netOut);
-const minOutputAmount = Big(
-  ethers.utils.parseUnits(tradeInfo.netOut, outputCurrency.decimals)
-)
-  .mul(1 - 1 * 0.005)
-  .toFixed(0);
+  const amount = ethers.utils.parseUnits(
+    inputCurrencyAmount,
+    inputCurrency.decimals
+  );
+  console.log("amount", amount, tradeInfo.netOut);
+  const minOutputAmount = Big(
+    ethers.utils.parseUnits(tradeInfo.netOut, outputCurrency.decimals)
+  )
+    .mul(1 - 1 * 0.005)
+    .toFixed(0);
 
-const getMethod = () => {
-  if (inputCurrency.baseType === "PT") {
-    return outputCurrency.baseType === "YT"
-      ? "swapExactPtForYt"
-      : outputCurrency.baseType === "SY"
-      ? "swapExactPtForSy"
-      : "swapExactPtForToken";
-  }
-  if (outputCurrency.baseType === "PT") {
-    return inputCurrency.baseType === "YT"
-      ? "swapExactYtForPt"
-      : inputCurrency.baseType === "SY"
-      ? "swapExactSyForPt"
-      : "swapExactTokenForPt";
-  }
-  if (inputCurrency.baseType === "YT") {
-    return inputCurrency.baseType === "PT"
-      ? "swapExactYtForPt"
-      : inputCurrency.baseType === "SY"
-      ? "swapExactYtForSy"
-      : "swapExactYtForToken";
-  }
-  if (outputCurrency.baseType === "YT") {
-    return inputCurrency.baseType === "PT"
-      ? "swapExactPtForYt"
-      : inputCurrency.baseType === "SY"
-      ? "swapExactSyForYt"
-      : "swapExactTokenForYt";
-  }
-};
-const method = getMethod();
-console.log("method", method);
-const calcMaxIteration = () => {
-  const x = Big(6 * 0.005).div("10000000000000000000000");
-  if (!x.gt(1)) return 3;
-  return Math.ceil(Math.log2(x)) + 3;
-};
-const getPullApproxParams = ({ minOut, decimals }) => {
-  return [
-    Big(ethers.utils.parseUnits(minOut, decimals))
-      .mul(1 - 1 * 0.005)
-      .toFixed(0),
-    Big(ethers.utils.parseUnits(minOut, decimals))
-      .mul(1 - 5 * 0.005)
-      .toFixed(0),
-    Big(ethers.utils.parseUnits(minOut, decimals)).toFixed(0),
-    calcMaxIteration(),
-    "100000000000000",
-  ];
-};
-const getPushApproxParams = ({ minOut, decimals }) => {
-  return [
-    Big(ethers.utils.parseUnits(minOut, decimals))
-      .mul(1 + 5 * 0.005)
-      .toFixed(0),
-    Big(ethers.utils.parseUnits(minOut, decimals))
-      .mul(1 + 1 * 0.005)
-      .toFixed(0),
-    Big(ethers.utils.parseUnits(minOut, decimals)).toFixed(0),
-    calcMaxIteration(),
-    "100000000000000",
-  ];
-};
+  const getMethod = () => {
+    if (inputCurrency.baseType === "PT") {
+      return outputCurrency.baseType === "YT"
+        ? "swapExactPtForYt"
+        : outputCurrency.baseType === "SY"
+        ? "swapExactPtForSy"
+        : "swapExactPtForToken";
+    }
+    if (outputCurrency.baseType === "PT") {
+      return inputCurrency.baseType === "YT"
+        ? "swapExactYtForPt"
+        : inputCurrency.baseType === "SY"
+        ? "swapExactSyForPt"
+        : "swapExactTokenForPt";
+    }
+    if (inputCurrency.baseType === "YT") {
+      return inputCurrency.baseType === "PT"
+        ? "swapExactYtForPt"
+        : inputCurrency.baseType === "SY"
+        ? "swapExactYtForSy"
+        : "swapExactYtForToken";
+    }
+    if (outputCurrency.baseType === "YT") {
+      return inputCurrency.baseType === "PT"
+        ? "swapExactPtForYt"
+        : inputCurrency.baseType === "SY"
+        ? "swapExactSyForYt"
+        : "swapExactTokenForYt";
+    }
+  };
+  const method = getMethod();
+  console.log("method", method);
+  const calcMaxIteration = () => {
+    const x = Big(6 * 0.005).div("10000000000000000000000");
+    if (!x.gt(1)) return 3;
+    return Math.ceil(Math.log2(x)) + 3;
+  };
+  const getPullApproxParams = ({ minOut, decimals }) => {
+    return [
+      Big(ethers.utils.parseUnits(minOut, decimals))
+        .mul(1 - 1 * 0.005)
+        .toFixed(0),
+      Big(ethers.utils.parseUnits(minOut, decimals))
+        .mul(1 - 5 * 0.005)
+        .toFixed(0),
+      Big(ethers.utils.parseUnits(minOut, decimals)).toFixed(0),
+      calcMaxIteration(),
+      "100000000000000",
+    ];
+  };
+  const getPushApproxParams = ({ minOut, decimals }) => {
+    return [
+      Big(ethers.utils.parseUnits(minOut, decimals))
+        .mul(1 + 5 * 0.005)
+        .toFixed(0),
+      Big(ethers.utils.parseUnits(minOut, decimals))
+        .mul(1 + 1 * 0.005)
+        .toFixed(0),
+      Big(ethers.utils.parseUnits(minOut, decimals)).toFixed(0),
+      calcMaxIteration(),
+      "100000000000000",
+    ];
+  };
 
-const getParams = (inputData) => {
-  switch (method) {
-    case "swapExactYtForPt": // 1
-      return [
-        account,
-        market.address,
-        amount,
-        minOutputAmount,
-        getPushApproxParams({
-          minOut: tradeInfo.totalSwapped,
-          decimals: inputCurrency.decimals,
-        }),
-      ];
-    case "swapExactSyForPt": // 1
-    case "swapExactSyForYt": // 1
-      return [
-        account,
-        market.address,
-        amount,
-        minOutputAmount,
-        getPullApproxParams({
-          minOut: tradeInfo.netOut,
-          decimals: outputCurrency.decimals,
-        }),
-      ];
-    case "swapExactPtForYt": // 1
-      return [
-        account,
-        market.address,
-        amount,
-        minOutputAmount,
-        getPushApproxParams({
-          minOut: tradeInfo.totalSwapped,
-          decimals: inputCurrency.decimals,
-        }),
-      ];
-    case "swapExactYtForSy": // 1
-    case "swapExactPtForSy": // 1
-      return [account, market.address, amount, minOutputAmount];
-    case "swapExactTokenForPt": // 1
-    case "swapExactTokenForYt": // 1
-      return [
-        account,
-        market.address,
-        minOutputAmount,
-        getPullApproxParams({
-          minOut: tradeInfo.netOut,
-          decimals: outputCurrency.decimals,
-        }),
-        inputData,
-      ];
-    case "swapExactPtForToken": // 1
-    case "swapExactYtForToken": // 1
-      return [account, market.address, amount, inputData];
-    default:
-      return [];
+  const getParams = (inputData) => {
+    switch (method) {
+      case "swapExactYtForPt": // 1
+        return [
+          account,
+          market.address,
+          amount,
+          minOutputAmount,
+          getPushApproxParams({
+            minOut: tradeInfo.totalSwapped,
+            decimals: inputCurrency.decimals,
+          }),
+        ];
+      case "swapExactSyForPt": // 1
+      case "swapExactSyForYt": // 1
+        return [
+          account,
+          market.address,
+          amount,
+          minOutputAmount,
+          getPullApproxParams({
+            minOut: tradeInfo.netOut,
+            decimals: outputCurrency.decimals,
+          }),
+        ];
+      case "swapExactPtForYt": // 1
+        return [
+          account,
+          market.address,
+          amount,
+          minOutputAmount,
+          getPushApproxParams({
+            minOut: tradeInfo.totalSwapped,
+            decimals: inputCurrency.decimals,
+          }),
+        ];
+      case "swapExactYtForSy": // 1
+      case "swapExactPtForSy": // 1
+        return [account, market.address, amount, minOutputAmount];
+      case "swapExactTokenForPt": // 1
+      case "swapExactTokenForYt": // 1
+        return [
+          account,
+          market.address,
+          minOutputAmount,
+          getPullApproxParams({
+            minOut: tradeInfo.netOut,
+            decimals: outputCurrency.decimals,
+          }),
+          inputData,
+        ];
+      case "swapExactPtForToken": // 1
+      case "swapExactYtForToken": // 1
+        return [account, market.address, amount, inputData];
+      default:
+        return [];
+    }
+  };
+  if (
+    ![
+      "swapExactTokenForPt",
+      "swapExactTokenForYt",
+      "swapExactPtForToken",
+      "swapExactYtForToken",
+    ].includes(method)
+  ) {
+    console.log("simpleParams", getParams());
+    RouterContract[method](...getParams(), { gasLimit: 5000000 })
+      .then((tx) => {
+        onSuccess(tx);
+      })
+      .catch((err) => {
+        console.log("error", err);
+        onError(err);
+      });
+    return;
   }
-};
-if (
-  ![
-    "swapExactTokenForPt",
-    "swapExactTokenForYt",
-    "swapExactPtForToken",
-    "swapExactYtForToken",
-  ].includes(method)
-) {
-  console.log("simpleParams", getParams());
-  RouterContract[method](...getParams(), { gasLimit: 5000000 })
+  let inputData = [];
+  if (["swapExactTokenForPt", "swapExactTokenForYt"].includes(method)) {
+    inputData = [
+      inputCurrency.address,
+      Big(
+        ethers.utils.parseUnits(inputCurrencyAmount, inputCurrency.decimals)
+      ).toFixed(),
+      inputCurrency.address,
+      "0x0000000000000000000000000000000000000000",
+      "0x38812c3ac3563bf200482ac9d096952d7cb55f9b",
+      [0, "0x0000000000000000000000000000000000000000", "0x00", false],
+    ];
+  }
+  if (["swapExactPtForToken", "swapExactYtForToken"].includes(method)) {
+    inputData = [
+      outputCurrency.address,
+      minOutputAmount,
+      outputCurrency.address,
+      "0x0000000000000000000000000000000000000000",
+      "0x38812c3ac3563bf200482ac9d096952d7cb55f9b",
+      [0, "0x0000000000000000000000000000000000000000", "0x00", false],
+    ];
+  }
+  console.log("token params", getParams(inputData));
+  const itf = new ethers.utils.Interface(ROUTER_ABI);
+  const encodeData = itf.encodeFunctionData(method, getParams(inputData));
+
+  RouterContract[method](...getParams(inputData), {
+    gasLimit: 5000000,
+    value:
+      inputCurrency.address === "0x0000000000000000000000000000000000000000"
+        ? amount
+        : 0,
+  })
     .then((tx) => {
       onSuccess(tx);
     })
     .catch((err) => {
-      console.log("error", err);
       onError(err);
     });
-  return;
-}
-let inputData = [];
-if (["swapExactTokenForPt", "swapExactTokenForYt"].includes(method)) {
-  inputData = [
-    inputCurrency.address,
-    Big(
-      ethers.utils.parseUnits(inputCurrencyAmount, inputCurrency.decimals)
-    ).toFixed(),
-    inputCurrency.address,
-    "0x0000000000000000000000000000000000000000",
-    "0x38812c3ac3563bf200482ac9d096952d7cb55f9b",
-    [0, "0x0000000000000000000000000000000000000000", "0x00", false],
-  ];
-}
-if (["swapExactPtForToken", "swapExactYtForToken"].includes(method)) {
-  inputData = [
-    outputCurrency.address,
-    minOutputAmount,
-    outputCurrency.address,
-    "0x0000000000000000000000000000000000000000",
-    "0x38812c3ac3563bf200482ac9d096952d7cb55f9b",
-    [0, "0x0000000000000000000000000000000000000000", "0x00", false],
-  ];
-}
-console.log("token params", getParams(inputData));
-const itf = new ethers.utils.Interface(ROUTER_ABI);
-const encodeData = itf.encodeFunctionData(method, getParams(inputData));
+};
 
-RouterContract[method](...getParams(inputData), {
-  gasLimit: 5000000,
-  value:
-    inputCurrency.address === "0x0000000000000000000000000000000000000000"
-      ? amount
-      : 0,
-})
-  .then((tx) => {
-    onSuccess(tx);
-  })
-  .catch((err) => {
-    console.log("error", err);
-    onError(err);
+if (swapping === state.swapping) {
+  return;
+} else {
+  State.update({
+    swapping,
   });
+  handleSwap();
+}
 
 return "";
