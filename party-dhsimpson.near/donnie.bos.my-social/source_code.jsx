4dd30b0e5158cfe47841cb425d7ce6f0266252a5
party-dhsimpson.near <party-dhsimpson.near>
2023-09-15 16:24:58 +0000

    Update donnie.bos.my-social by party-dhsimpson.near at 2023-09-15T16:24:58.269Z

--- party-dhsimpson.near/donnie.bos.my-social/source_code.jsx
+++ party-dhsimpson.near/donnie.bos.my-social/source_code.jsx
@@ -1,26 +1,23 @@
 const account_id = context.accountId ?? props.accountId;
-const profileData = Social.getr(`${account_id}/profile`);
+
+const profileData = props.profile ?? Social.getr(`idknwhoru.near/profile`);
+const fast = !props.profile;
+
+if (profileData === null) {
+  return "Loading";
+}
 const componentData = Social.getr(`${account_id}/widget`);
 const postData = Social.getr(`${account_id}/post`);
 
-const [num, setNum] = useState(0);
-
-const logData = () => {
-    setNum(num+1);
-    console.log("profileData");
-    // console.log(profileData);
-    console.log("componentData");
-    // console.log(componentData);
-};
 
 return (
   <div>
-    Hello World
-    <button onClick={logData}>mydata</button>
     {JSON.stringify(profileData)}
-    <br/><br/>
+    <br />
+    <br />
     {JSON.stringify(Object.keys(componentData))}
-    <br/><br/>
+    <br />
+    <br />
     {JSON.stringify(postData)}
   </div>
 );
