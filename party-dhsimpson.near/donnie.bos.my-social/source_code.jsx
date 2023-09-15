235933f58046f9f3bf225e27ffba240a32a6675e
party-dhsimpson.near <party-dhsimpson.near>
2023-09-15 16:38:18 +0000

    Update donnie.bos.my-social by party-dhsimpson.near at 2023-09-15T16:38:18.047Z

--- party-dhsimpson.near/donnie.bos.my-social/source_code.jsx
+++ party-dhsimpson.near/donnie.bos.my-social/source_code.jsx
@@ -9,8 +9,49 @@ if (profileData === null) {
 const componentData = Social.getr(`${account_id}/widget`);
 const postData = Social.getr(`${account_id}/post`);
 
+const MyPageWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+`;
+
+const ProfileWrapper = styled.div`
+    width: 100%;
+    height: 300px;
+    background-color: black;
+    display: flex;
+    align-items: center;
+`;
+
+const ProfileImage = styled.img`
+    width: 150px;
+    height: 150px;
+    margin-right: 20px;
+`;
+
+const nearIpfsBaseUrl = "https://ipfs.near.social/ipfs/";
+
+const MyName = styled.h2`
+`;
+
+const MyWallet = styled.p`
+`;
 
 return (
+    <>
+    <MyPageWrapper>
+        <ProfileWrapper>
+            <ProfileImage src={`${nearIpfsBaseUrl}${profileData.image.ipfs_cid}`} alt="profile-image"/>
+            <div>
+                <MyName>
+                    {profileData.name}
+                </MyName>
+                <MyWallet>
+                    {account_id}
+                </MyWallet>
+            </div>
+        </ProfileWrapper>
+    </MyPageWrapper>
   <div>
     {JSON.stringify(profileData)}
     <br />
@@ -20,4 +61,5 @@ return (
     <br />
     {JSON.stringify(postData)}
   </div>
+  </>
 );
