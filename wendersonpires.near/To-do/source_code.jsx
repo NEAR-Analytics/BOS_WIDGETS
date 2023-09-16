33cff943a3816d474a96522e06958a0d24d888cd
wendersonpires.near <wendersonpires.near>
2023-09-16 20:31:57 +0000

    Update To-do by wendersonpires.near at 2023-09-16T20:31:57.551Z

---
+++ wendersonpires.near/To-do/source_code.jsx
@@ -0,0 +1,10 @@
+const externalAppUrl =
+  "https://bafybeiccsdfqed2khf3yzneifm44sahmtubmriydcstihj3fsk4stwdnhm.ipfs.cf-ipfs.com/";
+const initialViewHeight = 500;
+
+return (
+  <Widget
+    src={"wendersonpires.near/widget/NearSocialBridgeCore"}
+    props={{ externalAppUrl, initialViewHeight }}
+  />
+);
