b806f7bad7d22dc4b016e45b25fb64336e2f784e
saidulbadhon.near <saidulbadhon.near>
2023-09-16 16:28:40 +0000

    Update CodeViewer by saidulbadhon.near at 2023-09-16T16:28:40.598Z

--- saidulbadhon.near/CodeViewer/source_code.jsx
+++ saidulbadhon.near/CodeViewer/source_code.jsx
@@ -1,13 +1,13 @@
 const Content = styled.div`
   display: grid;
-  grid-template-columns: minmax(0, 1fr) 336px;
-  gap: 64px;
+  // grid-template-columns: minmax(0, 1fr) 336px;
+  // gap: 64px;
   width: 100%;
 
-  @media (max-width: 995px) {
-    grid-template-columns: 1fr;
-    gap: 24px;
-  }
+  // @media (max-width: 995px) {
+  //   grid-template-columns: 1fr;
+  //   gap: 24px;
+  // }
 `;
 
 const sourceCode = `
