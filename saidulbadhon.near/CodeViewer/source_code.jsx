5b6166ae6f6f810e63996fcc6ded2a0439bc3ff3
saidulbadhon.near <saidulbadhon.near>
2023-09-16 16:22:43 +0000

    Update CodeViewer by saidulbadhon.near at 2023-09-16T16:22:43.897Z

---
+++ saidulbadhon.near/CodeViewer/source_code.jsx
@@ -0,0 +1,22 @@
+const Content = styled.div`
+  display: grid;
+  grid-template-columns: minmax(0, 1fr) 336px;
+  gap: 64px;
+
+  @media (max-width: 995px) {
+    grid-template-columns: 1fr;
+    gap: 24px;
+  }
+`;
+
+const sourceCode = `
+\`\`\`jsx
+${props.code}
+\`\`\`
+`;
+
+return (
+  <Content>
+    <Markdown text={sourceCode} />
+  </Content>
+);
