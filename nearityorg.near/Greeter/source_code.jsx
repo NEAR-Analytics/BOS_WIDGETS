2af0dec8c49e23bf21c261345a711eaf1c8a6d5d
nearityorg.near <nearityorg.near>
2023-09-16 10:04:52 +0000

    Update Greeter by nearityorg.near at 2023-09-16T10:04:52.473Z

---
+++ nearityorg.near/Greeter/source_code.jsx
@@ -0,0 +1,10 @@
+let greeting = "Have a great day";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>YO YO!!! Hello {props.name}!!!</h1>
+      <p> {greeting} </p>
+    </div>
+  </>
+);
