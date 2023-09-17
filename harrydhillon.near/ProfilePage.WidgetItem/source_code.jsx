3459d8a8e0781618627f538c9bfb2f00ffb2ea33
harrydhillon.near <harrydhillon.near>
2023-09-17 19:18:37 +0000

    Update ProfilePage.WidgetItem by harrydhillon.near at 2023-09-17T19:18:37.289Z

--- harrydhillon.near/ProfilePage.WidgetItem/source_code.jsx
+++ harrydhillon.near/ProfilePage.WidgetItem/source_code.jsx
@@ -1,9 +1,5 @@
 const gateway = "https://near.social/#/";
 
-initState({
-  imageData: [],
-});
-
 const WidgetItem = styled.a`
   border: 1px solid ${props.theme.borderColor};
 
@@ -25,38 +21,6 @@ const WidgetItem = styled.a`
   }
 `;
 
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  asyncFetch("https://near-queryapi.api.pagoda.co/v1/graphql", {
-    method: "POST",
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-    headers: {
-      "x-hasura-role": "harrydhillon_near",
-      "content-type": "application/json",
-    },
-  }).then(({ body: { data } }) => {
-    State.update({ imageData: Object.values(data) });
-  });
-}
-
-const operationsDoc = `
-query MyQuery {
-    harrydhillon_near_jutsu_widget_activity_search_widget_activity(
-      where: {widget_search_term: {_iregex: "${props.accountId}.${props.name}"}, _and: {widget_image: {_neq: ""}}}
-    ) {
-      widget_image
-    }
-  }
-`;
-
-function fetchMyQuery() {
-  return fetchGraphQL(operationsDoc, "MyQuery", {});
-}
-
-fetchMyQuery();
 
 return (
   <WidgetItem
@@ -64,11 +28,11 @@ return (
     target="_blank"
   >
     <div style={{ display: "flex", justifyContent: "center" }}>
-      {state.imageData[0]?.[0]?.widget_image?.ipfs_cid ? (
+      {props.image ? (
         <img
           style={{ width: 200, height: 200 }}
           src={
-            `https://ipfs.near.social/ipfs/${state.imageData[0]?.[0]?.widget_image?.ipfs_cid}` ??
+            `https://ipfs.near.social/ipfs/${props.image}` ??
             ``
           }
         />
