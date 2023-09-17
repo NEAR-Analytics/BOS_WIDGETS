f6e160a82f448e2898cbbb83003e19c74b9319a6
14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a <14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a>
2023-09-17 02:30:42 +0000

    Update my_widget by 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a at 2023-09-17T02:30:42.163Z

---
+++ 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a/my_widget/source_code.jsx
@@ -0,0 +1,88 @@
+const account_id = context.accountId ?? props.accountId;
+
+const data = Social.getr(`${account_id}/widget/*`, "final", {
+  subscribe: true,
+});
+
+const widgetEntries =
+  data === undefined || data === null ? [] : Object.entries(data);
+
+const widgets = widgetEntries.map(([title]) => {
+  return (
+    <div
+      style={{
+        padding: "15px 15px 15px 15px",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <a
+          href={`/near/widget/ComponentDetailsPage?src=${account_id}/widget/${title}`}
+          style={{
+            padding: "15px",
+            border: "solid 2px #bbb",
+            backgroundColor: "#fff",
+            color: "#0d6efd",
+            fontSize: "2rem",
+            fontWeight: "bold",
+          }}
+        >
+          &lt;&nbsp;&gt;
+        </a>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <a
+            href={`/${account_id}/widget/${title}`}
+            style={{
+              padding: "0 0 0 15px",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+              fontSize: "2em",
+              fontWeight: "bold",
+              color: "#000",
+              wordBreak: "break-all",
+            }}
+          >
+            {title}
+          </a>
+          <a
+            href={`/near/widget/ProfilePage?accountId=${account_id}`}
+            style={{
+              padding: "0 0 0 14px",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+              fontSize: "1.3em",
+              color: "#888",
+              wordBreak: "break-all",
+            }}
+          >
+            @{account_id}
+          </a>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+});
+
+return (
+  <>
+    <div>
+      <h1>Your Widgets: {widgetEntries.length}</h1>
+      <hr />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {widgets}
+      </div>
+    </div>
+  </>
+);
