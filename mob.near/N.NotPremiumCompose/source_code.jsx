f88db5760112f23c80200a8c41125c46e63b1b77
mob.near <mob.near>
2023-09-18 23:03:57 +0000

    Update N.NotPremiumCompose by mob.near at 2023-09-18T23:03:57.722Z

--- mob.near/N.NotPremiumCompose/source_code.jsx
+++ mob.near/N.NotPremiumCompose/source_code.jsx
@@ -21,7 +21,7 @@ const Checkmark = (
 
 return (
   <Wrapper className="d-flex align-items-center flex-row p-2">
-    <div className="">
+    <div className="me-3">
       <svg
         viewBox="0 0 22 22"
         xmlns="http://www.w3.org/2000/svg"
