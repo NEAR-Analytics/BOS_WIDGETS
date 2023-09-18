1a32d531b34cdf667884213f09093c564425cced
y3k.near <y3k.near>
2023-09-18 15:58:48 +0000

    Update near_atlas.tailwind.Homepage by y3k.near at 2023-09-18T15:58:48.292Z

--- y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
+++ y3k.near/near_atlas.tailwind.Homepage/source_code.jsx
@@ -166,6 +166,7 @@ const tabsData = [
     widgets: [
       "y3k.near/widget/near_atlas.components.MonthlyActiveAcounts",
       "y3k.near/widget/near_atlas.tailwind.React.Table.TopDapps",
+      "y3k.near/widget/near_atlas.tailwind.Aurora.Table.TopDapps",
     ],
   },
   {
