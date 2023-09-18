00ada011b93db46219ab0a82c35cd92b00293a0d
nearityorg.near <nearityorg.near>
2023-09-18 12:05:40 +0000

    Update welcome-message by nearityorg.near at 2023-09-18T12:05:40.295Z

---
+++ nearityorg.near/welcome-message/source_code.jsx
@@ -0,0 +1,10 @@
+let greeting = "Have a great day";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center min-100">
+      <h1>Hello</h1>
+      <p> {greeting} </p>
+    </div>
+  </>
+);
