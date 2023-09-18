224470ffa7ad14e868bbe8e70298007acf1cc7c7
c600cadf81115e7635094448ded43bc34703b47e86a1c1a3d1bec8b4cacc557a <c600cadf81115e7635094448ded43bc34703b47e86a1c1a3d1bec8b4cacc557a>
2023-09-18 05:16:08 +0000

    Update universal-serializer by c600cadf81115e7635094448ded43bc34703b47e86a1c1a3d1bec8b4cacc557a at 2023-09-18T05:16:08.846Z

--- c600cadf81115e7635094448ded43bc34703b47e86a1c1a3d1bec8b4cacc557a/universal-serializer/source_code.jsx
+++ c600cadf81115e7635094448ded43bc34703b47e86a1c1a3d1bec8b4cacc557a/universal-serializer/source_code.jsx
@@ -2,7 +2,103 @@ const onChange = ({ target }) => {
   State.update({ json: target.value });
 };
 
-const onClick = () => {
+const onHandleNear = () => {
+  State.update({ result: "" });
+  const json = `{
+    "id": 0,
+    "jsonrpc": "2.0",
+    "method": "dapp:serializeTransaction",
+    "params": [
+      {
+          "network": "near:test",
+          "from": {
+              "address": "**YOUR ACCOUNT**",
+              "publicKey": "**YOUR ACCESSKEY**"
+          },
+          "to": "dsrv-kms.testnet",
+          "txs": [
+              {
+                  "method": "functionCall",
+                  "params": [
+                    "increment",
+                                  {},
+                    "30000000000000",
+                                  "0"
+                  ]
+              }
+          ]
+      }
+    ]
+  }`;
+  State.update({ json });
+};
+
+const onHandleEthereum = () => {
+  State.update({ result: "" });
+  const json = `{
+    "id": 0,
+    "jsonrpc": "2.0",
+    "method": "dapp:serializeTransaction",
+    "params": [
+      {
+          "network": "sui:test",
+          "from": {
+              "address": "**YOUR ADDRESS**"
+          },
+          "txs": [
+              {
+                  "method": "functionCall",
+                  "params": [
+                      {
+                          "type": "moveCall",
+                          "args": [
+                              "**PACKAGE ADDRESS**::**MODULE**::**FUNCTION**",
+                              [0]
+                          ]
+                      }
+                  ]
+              }
+          ]
+      }
+    ]
+  }`;
+  State.update({ json });
+};
+
+const onHandleSui = () => {
+  State.update({ result: "" });
+  const json = `{
+    "id": 0,
+    "jsonrpc": "2.0",
+    "method": "dapp:serializeTransaction",
+    "params": [
+      {
+          "network": "ethereum:0x5",
+          "from": {
+              "address": "**YOUR ADDRESS**"
+          },
+          "to": "**ANY ADDRESS**",
+          "txs": [
+              {
+                  "method": "transfer",
+                  "params": [
+                    "1"
+                  ]
+              },
+              {
+                  "method": "functionCall",
+                  "params": [
+                    [ "uint", "string" ], [ 1234, "Hello World" ]
+                  ]
+              }
+          ]
+      }
+    ]
+  }`;
+  State.update({ json });
+};
+
+const onHandleRun = () => {
   asyncFetch("https://api.welldonestudio.io/universal-tx-serializer", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -22,10 +118,18 @@ return (
       <p>
         <b> Input Tx Info (JSON): </b>
       </p>
-      <textarea rows="5" cols="50" onChange={onChange} />
-      <button id="clickButton" onClick={onClick}>
-        Next
-      </button>
+      <textarea value={state.json} rows="15" cols="50" onChange={onChange} />
+      <br />
+      <div>
+        <p>
+          <b> Examples: </b>
+        </p>
+        <button onClick={onHandleNear}>Near (JSON)</button>
+        <button onClick={onHandleSui}>Ethereum (JSON)</button>
+        <button onClick={onHandleEthereum}>Sui (JSON)</button>
+      </div>
+      <br />
+      <button onClick={onHandleRun}>Run</button>
       <p>
         <b> Serialized Tx: </b> {state.result}{" "}
       </p>
