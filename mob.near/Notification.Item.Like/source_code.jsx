f3c524c107c8bdf21a28918edfa0eadaa133b03a
mob.near <mob.near>
2023-09-19 18:17:03 +0000

    Update Notification.Item.Like by mob.near at 2023-09-19T18:17:03.472Z

--- mob.near/Notification.Item.Like/source_code.jsx
+++ mob.near/Notification.Item.Like/source_code.jsx
@@ -1,5 +1,67 @@
 const { accountId, value } = props;
 
+const overlayStyle = {
+  maxWidth: "30em",
+  zIndex: 1070,
+  maxHeight: "24em",
+  overflow: "hidden",
+};
+
+const { content, popup } =
+  value.item.path === `${context.accountId}/post/main`
+    ? {
+        content: (
+          <a
+            className="fw-bold text-muted"
+            href={`/mob.near/widget/MainPage.N.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+          >
+            post
+          </a>
+        ),
+        popup: (
+          <Widget
+            src="mob.near/widget/MainPage.N.Post"
+            props={{
+              accountId: context.accountId,
+              blockHeight: value.item.blockHeight,
+              hideComments: true,
+            }}
+          />
+        ),
+      }
+    : value.item.path === `${context.accountId}/post/comment`
+    ? {
+        content: (
+          <a
+            className="fw-bold text-muted"
+            href={`/mob.near/widget/MainPage.N.Comment.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+          >
+            comment
+          </a>
+        ),
+        popup: (
+          <Widget
+            src="mob.near/widget/MainPage.N.Comment.Full"
+            props={{
+              accountId: context.accountId,
+              blockHeight: value.item.blockHeight,
+            }}
+          />
+        ),
+      }
+    : value.item.path === `${context.accountId}/post/insta`
+    ? {
+        content: (
+          <a
+            className="fw-bold text-muted"
+            href={`/mob.near/widget/Insta.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+          >
+            insta
+          </a>
+        ),
+      }
+    : { content: "item???" };
+
 return (
   <Widget
     loading={props.loading}
@@ -8,30 +70,15 @@ return (
       L: (
         <>
           liked your
-          {value.item.path === `${context.accountId}/post/main` ? (
-            <a
-              className="fw-bold text-muted"
-              href={`/mob.near/widget/MainPage.N.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
-            >
-              post
-            </a>
-          ) : value.item.path === `${context.accountId}/post/comment` ? (
-            <a
-              className="fw-bold text-muted"
-              href={`/mob.near/widget/MainPage.N.Comment.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
-            >
-              comment
-            </a>
-          ) : value.item.path === `${context.accountId}/post/insta` ? (
-            <a
-              className="fw-bold text-muted"
-              href={`/mob.near/widget/Insta.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
-            >
-              insta
-            </a>
-          ) : (
-            "item???"
-          )}
+          <Widget
+            loading={content}
+            src="mob.near/widget/N.Common.OverlayTrigger"
+            props={{
+              overlayStyle,
+              popup,
+              children: content,
+            }}
+          />
         </>
       ),
       R:
