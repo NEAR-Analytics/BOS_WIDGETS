972da5cdae369ba06acb9499da45965b1758b865
hackbox.near <hackbox.near>
2023-09-19 19:38:41 +0000

    Update home by hackbox.near at 2023-09-19T19:38:41.347Z

--- hackbox.near/home/source_code.jsx
+++ hackbox.near/home/source_code.jsx
@@ -1,11 +1,309 @@
+// return (
+//   <iframe
+//     frameborder="0"
+//     style={{
+//       width: "1px",
+//       minWidth: "100%",
+//       height: "calc(100vh - 78px - 10px - var(--body-top-padding))",
+//     }}
+//     src="https://taikai.network/hackbox/about"
+//   />
+// );
+
+const Layout = styled.div`
+    background: #191919;
+    color: #ffffffcf;
+
+    a {
+        color: #c6c6c6;
+        border-bottom: 1px solid #c6c6c6;
+    }
+`;
+
 return (
-  <iframe
-    frameborder="0"
-    style={{
-      width: "1px",
-      minWidth: "100%",
-      height: "calc(100vh - 78px - 10px - var(--body-top-padding))",
-    }}
-    src="https://the-hackbox.super.site/"
-  />
+  <Layout>
+    <div
+      style={{
+        marginBottom: "5em",
+      }}
+    >
+      <img
+        src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/e866dceb-2765-4129-bd78-9ab424d5f4d7/TAIKAI_-_HB_cover/w=2048,quality=80"
+        style={{
+          width: "100%",
+        }}
+      />
+    </div>
+    <div className="p-4">
+      <h1 style={{ fontWeight: "bold" }}>Hackbox Home</h1>
+      <h2>Welcome to Hackbox: Your Path to Community-Led Hackathons</h2>
+    </div>
+    <div className="container">
+      <div className="row">
+        <div
+          className="col-sm"
+          style={{
+            background: "#252525",
+          }}
+        >
+          <div className="bg-gray-light row p-2">
+            <div className="col-auto">
+              <span
+                className="notion-icon text"
+                style={{
+                  width: "1.5rem",
+                  height: "1.5rem",
+                  fill: "var(--color-text-default-light)",
+                }}
+              >
+                👋
+              </span>
+            </div>
+            <div className="col-sm">
+              <div>
+                <p>
+                  <strong>Introduction</strong>
+                </p>
+                <p>
+                  Hackbox is a revolutionary plug-and-play solution designed to
+                  empower local leads and community stewards in hosting
+                  hackathons easily and efficiently.
+                </p>
+                <p>
+                  Read all about what’s in the box in our{" "}
+                  <a href="/#/devgovgigs.near/widget/gigs-board.pages.community.activity?handle=hacks">
+                    official blog
+                  </a>
+                </p>
+                <p>
+                  <a
+                    href="https://forms.gle/cBa24dmZj4WvNzHq7"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <strong>⚡️ APPLY AS A HOST TO ACCESS HACKBOX </strong>
+                  </a>
+                </p>
+              </div>
+              <div>
+                <p className="notion-text__content">
+                  <span className="notion-semantic-string">
+                    <span>
+                      <strong></strong>
+                    </span>
+                  </span>
+                </p>
+              </div>
+
+              <div
+                id="block-4468be77671d4a64bcde717cf39534dc"
+                className="notion-image normal"
+              >
+                <span
+                  style={{
+                    boxSizing: "border-box",
+                    display: "inline-block",
+                    overflow: "hidden",
+                    width: "initial",
+                    height: "initial",
+                    background: "none",
+                    border: 0,
+                    margin: 0,
+                    padding: 0,
+                    position: "relative",
+                    maxWidth: "100%",
+                  }}
+                >
+                  <span
+                    style={{
+                      boxSizing: "border-box",
+                      display: "block",
+                      width: "initial",
+                      height: "initial",
+                      background: "none",
+                      border: 0,
+                      margin: 0,
+                      padding: 0,
+                      maxWidth: "100%",
+                    }}
+                  >
+                    <img
+                      style={{
+                        display: "block",
+                        maxWidth: "100%",
+                        width: "initial",
+                        height: "initial",
+                        background: "none",
+                        opacity: 1,
+                        border: 0,
+                        margin: 0,
+                        padding: 0,
+                      }}
+                      alt=""
+                      aria-hidden="true"
+                      src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27384%27%20height=%27216%27/%3e"
+                    />
+                  </span>
+                  <img
+                    alt="image"
+                    src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/a8fe6e2e-9cff-41f7-8164-a4c512e44d78/TAIKAI_HB_Card/w=828,quality=80"
+                    decoding="async"
+                    data-nimg="intrinsic"
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      bottom: 0,
+                      right: 0,
+                      boxSizing: "border-box",
+                      padding: 0,
+                      border: "none",
+                      margin: "auto",
+                      display: "block",
+                      width: 0,
+                      height: 0,
+                      minWidth: "100%",
+                      maxWidth: "100%",
+                      minHeight: "100%",
+                      maxHeight: "100%",
+                    }}
+                    srcset="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/a8fe6e2e-9cff-41f7-8164-a4c512e44d78/TAIKAI_HB_Card/w=640,quality=80 1x, https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/a8fe6e2e-9cff-41f7-8164-a4c512e44d78/TAIKAI_HB_Card/w=828,quality=80 2x"
+                  />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm">
+          <div className="row p-2">
+            <div className="col-auto">
+              <span
+                className="notion-icon text"
+                style={{
+                  width: "1.5rem",
+                  height: "1.5rem",
+                  fill: "var(--color-text-default-light)",
+                }}
+              >
+                💡
+              </span>
+            </div>
+            <div className="col-sm">
+              <p>
+                <strong>Why use Hackbox?</strong>
+              </p>
+              <p>
+                <strong>💎&nbsp;Financial Support:</strong>
+              </p>
+              <p>
+                Receive an allowance of $5,000 to $10,000 (excluding bounty
+                pool) to fuel your hackathon's success.
+              </p>
+              <p>
+                <strong>💎&nbsp;Guided Excellence:</strong>
+              </p>
+              <p>
+                A step-by-step guide for hosting seamless NEAR hackathons,
+                whether you're a seasoned organizer or a newbie.
+              </p>
+              <p>
+                <strong>💎&nbsp;Impactful Design:</strong>
+              </p>
+              <p>
+                Access a variety of design assets to captivate participants and
+                drive engagement across platforms.
+              </p>
+              <p>
+                <strong>💎&nbsp;Expert Mentorship:</strong>
+              </p>
+              <p>
+                Gain 4 hours of personalized guidance from NEAR hackathon
+                experts to enhance your event's quality.
+              </p>
+              <p>
+                <strong>💎&nbsp;Effortless Planning:</strong>
+              </p>
+              <p>
+                Pre-designed budgeting, marketing, and scheduling templates
+                streamline your organization process.
+              </p>
+              <p>
+                <strong>💎&nbsp;Innovation Catalyst:</strong>
+              </p>
+              <p>
+                Follow clear bounty and judging guidelines to encourage
+                groundbreaking ideas
+              </p>
+              <p>
+                <strong>💎&nbsp;Seamless Hosting:</strong>
+              </p>
+              <p>
+                Utilize TAIKAI's platform for a user-friendly and exceptional
+                hackathon experience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="container mt-5" style={{ background: "#222b26" }}>
+      <div className="row p-2">
+        <div className="col-auto">
+          <span
+            className="notion-icon text"
+            style={{
+              width: "1.5rem",
+              height: "1.5rem",
+              fill: "var(--color-text-default-light)",
+            }}
+          >
+            🔗
+          </span>
+        </div>
+        <div className="col-sm">
+          <p>
+            <strong>Key Resources</strong>
+          </p>
+          <p>
+            <a href="https://taikai.network/hackbox/about">Hackbox on TAIKAI</a>
+          </p>
+          <p>
+            <a href="/#/hackbox.near/widget/home">Hackbox on BOS</a>
+          </p>
+          <p>
+            <a href="https://the-hackbox.notion.site/FAQ-5c00cb10fbb84a5caf8b0ec2b83d21c0?pvs=25">
+              FAQ
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div
+      className="mt-5 mx-auto"
+      style={{
+        width: "600px",
+      }}
+    >
+      <img
+        loading="lazy"
+        src="https://the-hackbox.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fbe13ddc9-81c2-427d-9425-7ed7f0cae488%2FHB_logo_full_inv.png?table=block&id=0e50367e-33d9-4a27-9955-c2413412cc43&spaceId=35ee01d9-6396-4a87-897a-acb52739b445&width=1730&userId=&cache=v2"
+        referrerpolicy="same-origin"
+        className="w-100"
+      />
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeHhbYRLplL--fni03TqlvxUJRNUv7FZf4nUlBpJl2CvgC4pA/viewform?usp=send_form&amp;embedded=true&amp;usp=embed_googleplus"
+        title="docs.google.com"
+        sandbox="allow-scripts allow-popups allow-forms allow-same-origin allow-top-navigation-by-user-activation"
+        allowfullscreen=""
+        loading="lazy"
+        frameborder="0"
+        style={{
+          width: "600px",
+          height: "2000px",
+        }}
+      />
+    </div>
+  </Layout>
 );
