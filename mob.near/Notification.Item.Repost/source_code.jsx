925ca1b28fabf66c4b377435160dbc703946ec10
mob.near <mob.near>
2023-09-19 19:24:27 +0000

    Update Notification.Item.Repost by mob.near at 2023-09-19T19:24:27.599Z

--- mob.near/Notification.Item.Repost/source_code.jsx
+++ mob.near/Notification.Item.Repost/source_code.jsx
@@ -1,5 +1,36 @@
 const { accountId, value } = props;
 
+const overlayStyle = {
+  maxWidth: "30em",
+  zIndex: 1070,
+  maxHeight: "24em",
+  overflow: "hidden",
+};
+
+const { content, popup } =
+  value.item.path === `${context.accountId}/post/main`
+    ? {
+        content: (
+          <a
+            className="fw-bold text-muted"
+            href={`/mob.near/widget/MainPage.N.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
+          >
+            post
+          </a>
+        ),
+        popup: (
+          <Widget
+            src="mob.near/widget/MainPage.N.Post"
+            props={{
+              accountId: context.accountId,
+              blockHeight: value.item.blockHeight,
+              hideComments: true,
+            }}
+          />
+        ),
+      }
+    : { content: "item???" };
+
 return (
   <Widget
     loading={props.loading}
@@ -8,16 +39,15 @@ return (
       L: (
         <>
           reposted your
-          {value.item.path === `${context.accountId}/post/main` ? (
-            <a
-              className="fw-bold text-muted"
-              href={`/mob.near/widget/MainPage.N.Post.Page?accountId=${context.accountId}&blockHeight=${value.item.blockHeight}`}
-            >
-              post
-            </a>
-          ) : (
-            "item???"
-          )}
+          <Widget
+            loading={content}
+            src="mob.near/widget/N.Common.OverlayTrigger"
+            props={{
+              overlayStyle,
+              popup,
+              children: content,
+            }}
+          />
         </>
       ),
       R:
