b3fdaceaec45fec8cbbf0aea7d0d3988b6a6a58e
y3k.near <y3k.near>
2023-09-19 02:08:30 +0000

    Update near_atlas.components.vis.total_wallets by y3k.near at 2023-09-19T02:08:30.908Z

--- y3k.near/near_atlas.components.vis.total_wallets/source_code.jsx
+++ y3k.near/near_atlas.components.vis.total_wallets/source_code.jsx
@@ -1,5 +1,3 @@
-// Monthly Active Accounts Example
-// Monthly Active Accounts Example
 let raw_data = fetch(
   "https://api.flipsidecrypto.com/api/v2/queries/91b91aba-f307-48c9-9731-c3d475838f1f/data/latest",
   {
@@ -11,64 +9,69 @@ let raw_data = fetch(
   }
 );
 
+const initialState = {
+  selectedDateRange: "1Y",
+  rawData: [],
+};
+
+state = State.init(initialState);
+
+const handleDateRangeChange = (range) => {
+  State.update({
+    selectedDateRange: range,
+  });
+};
+
 const data = raw_data.body || [];
+State.update({ rawData: data });
 
 let Style = styled.div`
                 `;
 
 // logic start
 
-const sortedData = data.sort((a, b) => {
-  return new Date(a["DAY"]) - new Date(b["DAY"]);
-});
+const processData = (rawData, dateRange) => {
+  const endDate = new Date();
+  let startDate = new Date();
+
+  switch (dateRange) {
+    case "1M":
+      startDate.setMonth(endDate.getMonth() - 1);
+      break;
+    case "3M":
+      startDate.setMonth(endDate.getMonth() - 3);
+      break;
+    case "YTD":
+      startDate = new Date(endDate.getFullYear(), 0, 1); // start of the year
+      break;
+    case "1Y":
+      startDate.setFullYear(endDate.getFullYear() - 1);
+      break;
+    case "3Y":
+      startDate.setFullYear(endDate.getFullYear() - 3);
+      break;
+  }
+
+  const processedData = rawData.filter((entry) => {
+    const entryDate = new Date(entry["DAY"]);
+    return entryDate >= startDate && entryDate <= endDate;
+  });
+
+  // Sort the processed data by date
+  return processedData.sort((a, b) => new Date(a["DAY"]) - new Date(b["DAY"]));
+};
+
+const dataToDisplay = processData(state.rawData, state.selectedDateRange);
+
+const dates = dataToDisplay.map((entry) => entry["DAY"]);
 
 const WALLETS_CREATED = {};
 const TOTAL_WALLETS = {};
 
-data.map((entry) => {
+dataToDisplay.forEach((entry) => {
   WALLETS_CREATED[entry["DAY"]] = entry["WALLETS_CREATED"];
   TOTAL_WALLETS[entry["DAY"]] = entry["TOTAL_WALLETS"];
 });
-
-// console.log(data);
-// console.log(TOTAL_WALLETS);
-
-const dates = data.map((entry) => entry["DAY"]);
-
-// console.log(processedData);
-
-// logic part-2
-
-// const stacked_options = {
-//   maintainAspectRatio: true,
-//   plugins: {
-//     legend: {
-//       display: true,
-//       position: "bottom",
-//     },
-//   },
-//   scales: {
-//     y: {
-//       stacked: true,
-//       grid: {
-//         color: "rgb(41,51,64)", // This will change the gridline color
-//         borderColor: "rgb(240,255,240)",
-//       },
-//       ticks: {
-//         color: "rgb(240,255,240)", // This will change the axis text label color
-//       },
-//     },
-//     x: {
-//       stacked: true,
-//       grid: {
-//         color: "rgb(41,51,64)", // This will change the gridline color
-//       },
-//       ticks: {
-//         color: "rgb(240,255,240)", // This will change the axis text label color
-//       },
-//     },
-//   },
-// };
 
 const stacked_options = {
   maintainAspectRatio: true,
@@ -141,14 +144,32 @@ const stacked_bar_data = {
 
 return (
   <Style>
-    <div className="text-bg-dark rounded-4 p-3 mb-4">
-      {data !== null ? (
-        <div>
-          <BarEl options={stacked_options} data={stacked_bar_data} />
-        </div>
-      ) : (
-        <div>Loading ...</div>
-      )}
+    <div className="relative text-bg-dark rounded-4 p-3 mb-4">
+      <div className="absolute top-0 right-0 flex space-x-2 p-3">
+        {["1M", "3M", "YTD", "1Y", "3Y"].map((range) => (
+          <button
+            key={range}
+            onClick={() => handleDateRangeChange(range)}
+            className={`px-3 py-1 rounded transition-colors duration-200 ease-in ${
+              state.selectedDateRange === range
+                ? "bg-blue-500 text-white"
+                : "bg-gray-800 text-gray-400 hover:bg-gray-700"
+            }`}
+          >
+            {range}
+          </button>
+        ))}
+      </div>
+      <div className="rounded-4 p-3 mb-4 pt-16">
+        {" "}
+        {data !== null ? (
+          <div>
+            <BarEl options={stacked_options} data={stacked_bar_data} />
+          </div>
+        ) : (
+          <div>Loading ...</div>
+        )}
+      </div>
     </div>
   </Style>
 );
