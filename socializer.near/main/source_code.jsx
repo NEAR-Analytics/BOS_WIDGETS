8e987f3b51a0e5539e635233c61bc6f228fdb45b
socializer.near <socializer.near>
2023-09-19 08:40:24 +0000

    Update main by socializer.near at 2023-09-19T08:40:24.578Z

--- socializer.near/main/source_code.jsx
+++ socializer.near/main/source_code.jsx
@@ -1,5 +1,5 @@
 const Owner = "socializer.near";
-const API_URL = "http://localhost:3000";
+const API_URL = "https://e2e.nearverselabs.com";
 
 State.init({
   sate: false,
@@ -127,7 +127,7 @@ const changeTab = (value) => {
 };
 
 return (
-  <Wrapper>
+  <Wrapper className="root">
     <Widget src={`${Owner}/widget/Header`} />
     <Content>
       <Widget
