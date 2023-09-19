28388945b221c9ac645b3b685b453646c3fbbff5
lord1.near <lord1.near>
2023-09-19 17:59:31 +0000

    Update election-scan by lord1.near at 2023-09-19T17:59:31.529Z

--- lord1.near/election-scan/source_code.jsx
+++ lord1.near/election-scan/source_code.jsx
@@ -1,4 +1,5 @@
-const themeColor = props.themeColor;
+const themeColor = lightColors;
+// const themeColor = props.themeColor;
 const generaltheme = {
   height: "110px",
   align: "center",
@@ -18,15 +19,44 @@ const generaltheme = {
     "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
 };
 
-const queryHashes = [{ id: 4, hash: "2286f370-240d-4e62-a301-3331ec2e2ef0" }];
+const queryHashes = [{ id: 1, hash: "2286f370-240d-4e62-a301-3331ec2e2ef0" }];
 
 State.init({
-  light: true,
+  filter: "",
   data: [],
+  filteredData: [],
   isLoading: true,
   error: [],
 });
 
+const Input = styled.input`
+color:${themeColor?.search_sbt?.input_text_color};
+background-color:${themeColor?.search_sbt?.input_bg};
+border: 1px solid ${themeColor?.search_sbt?.input_border};
+&:focus{
+  background-color:${themeColor?.search_sbt?.input_bg};
+  color:${themeColor?.search_sbt?.input_text_color};
+border: 1px solid ${themeColor?.search_sbt?.input_border};
+};
+&:hover{
+  background-color:${themeColor?.search_sbt?.input_bg_hover}
+};
+
+`;
+const Button = styled.button`
+    color: ${themeColor?.search_sbt?.search_btn_text};
+    font-size: 16px;
+    padding: 0.5rem 1rem;
+    font-weight: 400;
+    background-color: ${themeColor?.search_sbt?.search_btn_bg};
+    &:hover {background-color: ${themeColor?.search_sbt?.search_btn_bg_hover}};
+    border: 1px solid ${themeColor?.search_sbt?.search_btn_bg};
+    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.02);
+    min-height: calc(1.5em + 1rem + 2px);
+    border-radius: 40px;
+    line-height: 29px;
+    letter-spacing: 0.01em;
+`;
 const handleData = () => {
   const data = {};
   const errors = [];
@@ -42,12 +72,35 @@ const handleData = () => {
   if (Object.values(data).every((d) => !d.isLoading)) {
     State.update({
       data: data,
+      filteredData: data.hash1.data,
       error: [...state.error, ...errors],
       isLoading: false,
     });
   }
 };
+const inputHandler = ({ target }) => {
+  const filter = target.value.toLowerCase().trim();
+  State.update({ filter: filter });
+};
 
+const filterData = () => {
+  if (!state.filter.length) {
+    State.update({ error: [...state.error, "please insert an address"] });
+    return;
+  }
+  if (state.data === state.filter) {
+    State.update({ error: [...state.error, "please insert a new address"] });
+    return;
+  }
+
+  const filteredData = state.data.hash1?.data?.filter((row) =>
+    Object.values(row)
+      .join()
+      .toLocaleLowerCase()
+      .includes(state.filter.toLocaleLowerCase())
+  );
+  State.update({ filteredData: filteredData || [] });
+};
 const fetchData = (hash) => {
   const data = fetch(
     `https://api.flipsidecrypto.com/api/v2/queries/${hash}/data/latest`,
@@ -82,57 +135,99 @@ if (state.error.length > 0) {
 }
 
 return (
-  <div className="container-fluid py-2">
+  <div
+    style={{ backgroundColor: themeColor.page_bg }}
+    className="container-fluid py-2"
+  >
     <div className="pl-2">
       <div
-        style={{ background: themeColor?.sbt_area?.card_bg }}
+        style={{ background: themeColor?.sbt_area?.section_bg }}
         className="shadow-sm rounded-2 overflow-auto"
       >
         <Widget src="lord1.near/widget/header-dynamic" props={generaltheme} />
-        <Widget
-          src="lord1.near/widget/table-pagination"
-          props={{
-            themeColor: { table_pagination: themeColor.table_pagination },
-            data: state.data?.hash4?.data,
-            columns: [
-              { title: "Number", key: "rank", colors: "#806ce1" },
-              {
-                title: "Status",
-                key: "status",
-              },
-              { title: "Timestamp", key: "time" },
-              {
-                title: "Signer",
-                key: "SINGER",
-                link: "yes",
-                beforehref:
-                  "https://near.social/mob.near/widget/ProfilePage?accountId=",
-                hyperlink: "yes",
-              },
-              { title: "Function ", key: "function" },
-              {
-                title: "Target",
-                key: "target",
-                explain: "yes",
-              },
-              {
-                title: "Info",
-                key: "PROP",
-                explain: "yes",
-              },
-              { title: "Fee(Near)", key: "fee" },
 
-              {
-                title: "Hash",
-                key: "trxs",
-                link: "yes",
-                beforehref: "https://nearblocks.io/txns/",
-                afterhref: "",
-              },
-            ],
-            rowsCount: 20,
-          }}
-        />
+        <div className="search w-100 pl-4 py-4">
+          <div className="row">
+            <div className="col-8 ">
+              <Input
+                onBlur={inputHandler}
+                defaultValue={state.filter}
+                type="input"
+                className="form-control form-control-lg rounded-4"
+                id="address"
+                placeholder="address/hash/time/function ..."
+              />
+            </div>
+            <div className="col-4 col-lg-auto">
+              <Button
+                disabled={state.isLoading}
+                onClick={filterData}
+                className="btn-lg"
+                type="button"
+                style={{ "margin-left": "1%" }}
+              >
+                {state.isLoading ? (
+                  <div className="text-center px-4">
+                    <div
+                      className="spinner-border spinner-border-sm"
+                      role="status"
+                    >
+                      <span className="visually-hidden">Loading...</span>
+                    </div>
+                  </div>
+                ) : (
+                  "search"
+                )}
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="w-100 p-2" style={{ "border-radius": "100px" }}>
+          <Widget
+            src="lord1.near/widget/table-pagination"
+            props={{
+              themeColor: { table_pagination: themeColor.table_pagination },
+              data: state.filteredData,
+              columns: [
+                { title: "Number", key: "rank", colors: "#806ce1" },
+                {
+                  title: "Status",
+                  key: "status",
+                },
+                { title: "Timestamp", key: "time" },
+                {
+                  title: "Signer",
+                  key: "SINGER",
+                  link: "yes",
+                  beforehref:
+                    "https://near.social/mob.near/widget/ProfilePage?accountId=",
+                  hyperlink: "yes",
+                },
+                { title: "Function ", key: "function" },
+                {
+                  title: "Target",
+                  key: "target",
+                  explain: "yes",
+                },
+                {
+                  title: "Info",
+                  key: "PROP",
+                  explain: "yes",
+                },
+                { title: "Fee(Near)", key: "fee" },
+
+                {
+                  title: "Hash",
+                  key: "trxs",
+                  link: "yes",
+                  beforehref: "https://nearblocks.io/txns/",
+                  afterhref: "",
+                },
+              ],
+              rowsCount: 20,
+            }}
+          />
+        </div>
       </div>
       <div className="toast-container position-fixed bottom-0 end-0 p-3">
         {state.error.length > 0 &&
