1cb9687f5f5b4965bfc408dbd13007240a2a699c
lord1.near <lord1.near>
2023-09-19 15:30:35 +0000

    Update election-rate by lord1.near at 2023-09-19T15:30:35.959Z

--- lord1.near/election-rate/source_code.jsx
+++ lord1.near/election-rate/source_code.jsx
@@ -1,362 +1,267 @@
 const themeColor = props.themeColor;
-
-const queryHashes = [{ id: 1, hash: "c178fbd9-697e-4e36-9770-8c6162fa0004" }];
-
-State.init({
-  light: true,
-  data: [],
-  isLoading: true,
-  error: [],
-});
-
-const getMixProps = (data, dateKey, serieses, colors, chartOption) => {
-  data = data || [];
-  serieses = serieses || [{ key: "", seriesName: "", type: "", id: 1 }];
-  colors = colors || [];
-  chartOption = chartOption || {};
-
-  const dataFormat = serieses.map((series) => {
-    const dataFormated = data.map((d) => [d[dateKey] * 1000, d[series.key]]);
-    return {
-      data: dataFormated,
-      name: series.seriesName,
-      type: series.type,
-      axisId: series.id,
-    };
-  });
-  const props = {
-    series: dataFormat,
-    colors: colors,
-    chartOption: {
-      title: "chart title",
-      subtitle: "chart subtitle",
-      legend: true,
-      stacking: "false",
-      ...chartOption,
-    },
-
-    overrideOptions: {
-      plotOptions: {
-        column: {
-          stacking: "false",
-        },
-        series: {
-          dataGrouping: { enabled: false },
-        },
-      },
-    },
-    themeColor: { chart: themeColor.chart },
-    spinnerColors: themeColor.spinnerColors,
-  };
-  return props;
+const generaltheme = {
+  height: "160px",
+  align: "center",
+  description:
+    "What percentage of the ballots in each household were converted into votes? For each voter, there are 15 ballots in HOM, 7 ballots in COA, 15 ballots in TC, and 1 ballot in BP.",
+  brand: "Participantion Rate",
+  fontsize: "100",
+  fontweight: "25px",
+  afterbrand: "",
+  afterbrandcolor: themeColor?.dynamic_header?.afterbrandcolor || "#789efb",
+  fontbrand: " Arial, sans-serif",
+  color1brand: themeColor?.dynamic_header?.color1brand || "#000",
+  color2brand: themeColor?.dynamic_header?.color2brand || "#806ce1",
+  colordescription: themeColor?.dynamic_header?.colordescription || "#806ce1",
+  fontsubtitle: " Arial, sans-serif",
+  background:
+    themeColor?.dynamic_header?.background ||
+    "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
+};
+const theme1 = {
+  height: "150px",
+  align: "center",
+  description: "House Of Merit",
+  brand: "HOM",
+  fontsize: "100",
+  fontweight: "25px",
+  afterbrand: "",
+  afterbrandcolor: themeColor?.dynamic_header?.afterbrandcolor || "#789efb",
+  fontbrand: " Arial, sans-serif",
+  color1brand: themeColor?.dynamic_header?.color1brand || "#000",
+  color2brand: themeColor?.dynamic_header?.color2brand || "#806ce1",
+  colordescription: themeColor?.dynamic_header?.colordescription || "#806ce1",
+  fontsubtitle: " Arial, sans-serif",
+  background:
+    themeColor?.dynamic_header?.background ||
+    "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
+};
+const theme2 = {
+  height: "150px",
+  align: "center",
+  description: "Council Of Advisors",
+  brand: "COA",
+  fontsize: "100",
+  fontweight: "25px",
+  afterbrand: "",
+  afterbrandcolor: themeColor?.dynamic_header?.afterbrandcolor || "#789efb",
+  fontbrand: " Arial, sans-serif",
+  color1brand: themeColor?.dynamic_header?.color1brand || "#000",
+  color2brand: themeColor?.dynamic_header?.color2brand || "#806ce1",
+  colordescription: themeColor?.dynamic_header?.colordescription || "#806ce1",
+  fontsubtitle: " Arial, sans-serif",
+  background:
+    themeColor?.dynamic_header?.background ||
+    "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
+};
+const theme3 = {
+  height: "150px",
+  align: "center",
+  description: "Transparency Commission",
+  brand: "TC",
+  fontsize: "100",
+  fontweight: "25px",
+  afterbrand: "",
+  afterbrandcolor: themeColor?.dynamic_header?.afterbrandcolor || "#789efb",
+  fontbrand: " Arial, sans-serif",
+  color1brand: themeColor?.dynamic_header?.color1brand || "#000",
+  color2brand: themeColor?.dynamic_header?.color2brand || "#806ce1",
+  colordescription: themeColor?.dynamic_header?.colordescription || "#806ce1",
+  fontsubtitle: " Arial, sans-serif",
+  background:
+    themeColor?.dynamic_header?.background ||
+    "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
+};
+const theme4 = {
+  height: "150px",
+  align: "center",
+  description: "Budget Package .",
+  brand: "BD",
+  fontsize: "10",
+  fontweight: "25px",
+  afterbrand: "",
+  afterbrandcolor: themeColor?.dynamic_header?.afterbrandcolor || "#789efb",
+  fontbrand: " Arial, sans-serif",
+  color1brand: themeColor?.dynamic_header?.color1brand || "#000",
+  color2brand: themeColor?.dynamic_header?.color2brand || "#806ce1",
+  colordescription: themeColor?.dynamic_header?.colordescription || "#806ce1",
+  fontsubtitle: " Arial, sans-serif",
+  background:
+    themeColor?.dynamic_header?.background ||
+    "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
 };
 
-const handleData = () => {
-  const data = {};
-  const errors = [];
-  queryHashes.forEach(({ hash, id }) => {
-    const result = fetchData(hash);
-    if (result.error) errors.push(`hash${id} : ${result.error}`);
-    data[`hash${id}`] = {
-      ...result,
-      id,
-    };
-  });
+const Right = styled.div`
+  padding: 2px;
+  margin-bottom: 10px;
+  border-radius: 8px;
+`;
 
-  if (Object.values(data).every((d) => !d.isLoading)) {
-    State.update({
-      data: data,
-      error: [...state.error, ...errors],
-      isLoading: false,
-    });
+const H5 = styled.h5`
+  color:${themeColor.election?.textColor};
+  text-align: center;
+`;
+const ChartContainer = styled.div`
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 0.5s;
   }
-};
-
-const fetchData = (hash) => {
-  const data = fetch(
-    `https://api.flipsidecrypto.com/api/v2/queries/${hash}/data/latest`,
-    {
-      subscribe: true,
-      method: "GET",
-      headers: {
-        Accept: "*/*",
-      },
-    }
-  );
-  const result = {
-    data: (data && data.body) || null,
-    error: (data && !data.ok && (data.status || data.error)) || null,
-    isLoading: !data && !error,
-  };
-  return result;
-};
-
-if (state.isLoading) {
-  handleData();
-}
-
-if (state.error.length > 0) {
-  function hide() {
-    const errors = state.error;
-    errors.shift();
-    if (errors.length > 0) setTimeout(hide, 2500);
-    State.update({ error: errors });
+`;
+let data = fetch(
+  "https://api.flipsidecrypto.com/api/v2/queries/805aa65d-7ace-401c-8f1b-fb6c82f730bb/data/latest",
+  {
+    subscribe: true,
+    method: "GET",
+    headers: {
+      Accept: "*/*",
+    },
   }
-  setTimeout(hide, 2500);
-}
+);
+const datas = data.body;
 
-return (
-  <div className="container-fluid py-2">
-    <div>
-      <div className="row md-12">
-        <div calseeName="row col-md-12">
-          <div className="col-12">
-            <div
-              style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
-              className="shadow-sm rounded-4"
-            >
-              <Widget
-                src="lord1.near/widget/mix-chart"
-                props={getMixProps(
-                  state.data?.hash1?.data,
-                  "time",
-                  [
-                    {
-                      key: "total_ballots_cum",
-                      seriesName: "Max ballots",
-                      type: "areaspline",
-                      id: 2,
-                    },
-                    {
-                      key: "total_vote_cum",
-                      seriesName: "Cumulative vote",
-                      type: "areaspline",
-                      id: 2,
-                    },
-
-                    {
-                      key: "singers",
-                      seriesName: "Daily voter",
-                      type: "column",
-                      id: 1,
-                    },
-                  ],
-                  themeColor.chartColor,
-                  {
-                    title: "Vote Status (Total)",
-                    subtitle: "",
-                    stacking: "normal",
-                  }
-                )}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="py-2"></div>
-
-        <div className="col-lg-6">
-          <div
-            style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
-            className="shadow-sm rounded-4"
-          >
-            <Widget
-              src="lord1.near/widget/mix-chart"
-              props={getMixProps(
-                state.data?.hash1?.data,
-                "time",
-                [
-                  {
-                    key: "hom_rate_cum",
-                    seriesName: "Max ballots",
-                    type: "areaspline",
-                    id: 2,
-                  },
-                  {
-                    key: "hom_cum",
-                    seriesName: "Cumulative vote",
-                    type: "areaspline",
-                    id: 2,
-                  },
-                  {
-                    key: "hom",
-                    seriesName: "Daily vote",
-                    type: "column",
-                    id: 1,
-                  },
-                ],
-                themeColor.chartColor,
-                {
-                  title: "Vote Status (House of Merit)",
-                  subtitle: "",
-                  stacking: "normal",
-                }
-              )}
-            />
-          </div>
-        </div>
-        <div className="col-lg-6">
-          <div
-            style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
-            className="shadow-sm rounded-4"
-          >
-            <Widget
-              src="lord1.near/widget/mix-chart"
-              props={getMixProps(
-                state.data?.hash1?.data,
-                "time",
-                [
-                  {
-                    key: "coa_rate_cum",
-                    seriesName: "Max ballots",
-                    type: "areaspline",
-                    id: 2,
-                  },
-                  {
-                    key: "coa_cum",
-                    seriesName: "Cumulative vote",
-                    type: "areaspline",
-                    id: 2,
-                  },
-                  {
-                    key: "coa",
-                    seriesName: "Daily vote",
-                    type: "column",
-                    id: 1,
-                  },
-                ],
-                themeColor.chartColor,
-                {
-                  title: "Vote Status (Council Of Advisors)",
-                  subtitle: "",
-                  stacking: "normal",
-                }
-              )}
-            />
-          </div>
-        </div>
+const WidgetComponent = ({ label, voted, total }) => (
+  <div className="col-md-12">
+    <Right className="col">
+      <H5>{label}</H5>
+      <div className="d-flex justify-content-center">
+        <ChartContainer>
+          <Widget
+            src="lord1.near/widget/pie-percentage"
+            props={{
+              themeColor,
+              voted: `${voted}`,
+              total: `${total}`,
+              percent: 50,
+            }}
+          />
+        </ChartContainer>
       </div>
-      <div className="py-2"></div>
-      <div className="row md-12">
-        <div className="col-lg-6">
-          <div
-            style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
-            className="shadow-sm rounded-4"
-          >
-            <Widget
-              src="lord1.near/widget/mix-chart"
-              props={getMixProps(
-                state.data?.hash1?.data,
-                "time",
-                [
-                  {
-                    key: "tc_rate_cum",
-                    seriesName: "Max ballots",
-                    type: "areaspline",
-                    id: 2,
-                  },
-                  {
-                    key: "tc_cum",
-                    seriesName: "Cumulative vote",
-                    type: "areaspline",
-                    id: 2,
-                  },
-                  {
-                    key: "tc",
-                    seriesName: "Daily vote",
-                    type: "column",
-                    id: 1,
-                  },
-                ],
-                themeColor.chartColor,
-                {
-                  title: "Vote Status (Transparency Commission)",
-                  subtitle: "",
-                  stacking: "normal",
-                }
-              )}
-            />
-          </div>
-        </div>
-        <div className="col-lg-6">
-          <div
-            style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
-            className="shadow-sm rounded-4"
-          >
-            <Widget
-              src="lord1.near/widget/mix-chart"
-              props={getMixProps(
-                state.data?.hash1?.data,
-                "time",
-                [
-                  {
-                    key: "bp_rate_cum",
-                    seriesName: "Max ballots",
-                    type: "areaspline",
-                    id: 2,
-                  },
-                  {
-                    key: "bp_cum",
-                    seriesName: "Cumulative vote",
-                    type: "areaspline",
-                    id: 2,
-                  },
-                  {
-                    key: "bp",
-                    seriesName: "Daily vote",
-                    type: "column",
-                    id: 1,
-                  },
-                ],
-                themeColor.chartColor,
-                {
-                  title: "Vote Status (Budget Package)",
-                  subtitle: "",
-                  stacking: "normal",
-                }
-              )}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="toast-container position-fixed bottom-0 end-0 p-3">
-        {state.error.length > 0 &&
-          state.error.map((er) => (
-            <div
-              className="toast show align-items-center text-bg-danger border-0"
-              role="alert"
-              aria-live="assertive"
-              aria-atomic="true"
-            >
-              <div className="d-flex">
-                <div className="toast-body">{er}</div>
-              </div>
-            </div>
-          ))}
-      </div>
-    </div>
+    </Right>
   </div>
 );
 
-// const props = {
-//   themeColor: {
-//     dynamic_header: {
-//       afterbrandcolor: "#789efb",
-//       color1brand: "#000",
-//       color2brand: "#806ce1",
-//       colordescription: "#806ce1",
-//       background:
-//         "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
-//     },
-//     tab_sbt: {
-//       backgroundColor: "rgb(49,62,89)",
-//       textColor: "#fff",
-//       headerColor: "#806ce1",
-//       numberColor: "#fff",
-//     },
-//     sbt_area: {
-//       section_bg: "rgba(25,33,80)",
-//       card_bg: "rgb(49, 62, 89)",
-//       card_title_color: "#806ce1",
-//     },
-//   },
-// };
+const renderWidgets = (datas, labels) => {
+  return labels.map((label) => (
+    <div className="col-md-6 col-lg-3">
+      <WidgetComponent
+        label={label}
+        voted={datas[0][`${label.toLowerCase()}`]}
+        total={datas[0][`${label.toLowerCase()}_rate`]}
+      />
+    </div>
+  ));
+};
+55;
+const renderHomWidgets = (datas, labels) => {
+  return labels.map((label) => (
+    <div className="col-md-3 col-xl-2">
+      <div className="col-md-12">
+        <Right className="col">
+          <div className="d-flex justify-content-center">
+            <ChartContainer>
+              <Widget
+                src="lord1.near/widget/pie-percentage"
+                props={{
+                  themeColor,
+                  voted: `${datas[0][`${label.toLowerCase()}`]}`,
+                  total: `${datas[0].singers}`,
+                  description: `${label.toLowerCase().split("_")[1]}`,
+                  percent: 10,
+                }}
+              />
+            </ChartContainer>
+          </div>
+        </Right>
+      </div>
+    </div>
+  ));
+};
+
+const topLabels = ["HOM", "COA", "TC", "BP"];
+const homLabels = ["hom_og", "hom_vibes", "hom_regens", "hom_fractal"];
+const coaLabels = ["coa_og", "coa_vibes", "coa_regens", "coa_fractal"];
+const tcLabels = ["tc_og", "tc_vibes", "tc_regens", "tc_fractal"];
+const bpLabels = ["bp_og", "bp_vibes", "bp_regens", "bp_fractal"];
+
+return (
+  <div style={{ backgroundColor: themeColor.page_bg }}>
+    <div
+      style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
+      className="shadow-sm rounded-4"
+    >
+      <Widget src="lord1.near/widget/header-dynamic" props={generaltheme} />
+      <div className="row">{renderWidgets(datas, topLabels)}</div>
+    </div>
+
+    <div style={{ width: "100%", height: "85px" }}></div>
+    <div
+      style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
+      className="shadow-sm rounded-4 "
+      style={{ "padding-left": "1.5%", "padding-right": "1.5%" }}
+    >
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+            {renderHomWidgets(datas, homLabels)}
+            <div className="order-first order-xl-last col-xl-4 ">
+              <Widget src="lord1.near/widget/header-dynamic" props={theme1} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
+      className="shadow-sm rounded-4 "
+      style={{ "padding-left": "1.5%", "padding-right": "1.5%" }}
+    >
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+            {renderHomWidgets(datas, coaLabels)}
+            <div className="order-first order-xl-last col-xl-4 ">
+              <Widget src="lord1.near/widget/header-dynamic" props={theme2} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
+      className="shadow-sm rounded-4 "
+      style={{ "padding-left": "1.5%", "padding-right": "1.5%" }}
+    >
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+            {renderHomWidgets(datas, tcLabels)}
+            <div className="order-first order-xl-last col-xl-4 ">
+              <Widget src="lord1.near/widget/header-dynamic" props={theme3} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      style={{ backgroundColor: themeColor?.sbt_info?.card_bg }}
+      className="shadow-sm rounded-4 "
+      style={{ padding: "1.5%", "padding-right": "1.5%" }}
+    >
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+            {renderHomWidgets(datas, bpLabels)}
+            <div className="order-first order-xl-last col-xl-4 ">
+              <Widget src="lord1.near/widget/header-dynamic" props={theme4} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Widget
+      src="lord1.near/widget/election-rate-chart"
+      props={{ themeColor }}
+    />
+  </div>
+);
