81c5aa01b610423b838334875972b56bd0190762
socializer.near <socializer.near>
2023-09-19 13:39:11 +0000

    Update WinnersModal by socializer.near at 2023-09-19T13:39:11.925Z

--- socializer.near/WinnersModal/source_code.jsx
+++ socializer.near/WinnersModal/source_code.jsx
@@ -145,7 +145,7 @@ return (
           const profile = Social.getr(`${item.accountId}/profile`);
           return (
             <p>{`Social Username ${profile.name || item.accountId} ${
-              i + 1 < 10 ? `0${i}` : i
+              i + 1 < 10 ? `0${i + 1}` : i + 1
             }`}</p>
           );
         })}
