c93c7f3c2af8f673e303b8a409fa2dae32af56b8
y3k.near <y3k.near>
2023-09-19 02:21:56 +0000

    Update near_atlas.components.Vis.Social_Activity_Overtime by y3k.near at 2023-09-19T02:21:56.770Z

--- y3k.near/near_atlas.components.Vis.Social_Activity_Overtime/source_code.jsx
+++ y3k.near/near_atlas.components.Vis.Social_Activity_Overtime/source_code.jsx
@@ -1,5 +1,3 @@
-// Monthly Active Accounts Example
-// Monthly Active Accounts Example
 let raw_data = fetch(
   "https://api.flipsidecrypto.com/api/v2/queries/f22d1d23-8993-45ea-9cbb-d27eba5b106d/data/latest",
   {
@@ -11,28 +9,71 @@ let raw_data = fetch(
   }
 );
 
+const initialState = {
+  selectedDateRange: "1Y",
+  rawData: [],
+};
+
+state = State.init(initialState);
+
+const handleDateRangeChange = (range) => {
+  State.update({
+    selectedDateRange: range,
+  });
+};
+
 const data = raw_data.body || [];
+State.update({ rawData: data });
 
 let Style = styled.div`
                 `;
 
-// logic start
+const processData = (rawData, dateRange) => {
+  const endDate = new Date();
+  let startDate = new Date();
 
-const sortedData = data.sort((a, b) => {
-  return new Date(a["DATE"]) - new Date(b["DATE"]);
-});
+  switch (dateRange) {
+    case "1M":
+      startDate.setMonth(endDate.getMonth() - 1);
+      break;
+    case "3M":
+      startDate.setMonth(endDate.getMonth() - 3);
+      break;
+    case "YTD":
+      startDate = new Date(endDate.getFullYear(), 0, 1); // start of the year
+      break;
+    case "1Y":
+      startDate.setFullYear(endDate.getFullYear() - 1);
+      break;
+    case "3Y":
+      startDate.setFullYear(endDate.getFullYear() - 3);
+      break;
+  }
+
+  const processedData = rawData.filter((entry) => {
+    const entryDate = new Date(entry["DATE"]);
+    return entryDate >= startDate && entryDate <= endDate;
+  });
+
+  // Sort the processed data by date
+  return processedData.sort(
+    (a, b) => new Date(a["DATE"]) - new Date(b["DATE"])
+  );
+};
+
+const dataToDisplay = processData(state.rawData, state.selectedDateRange);
+
+const dates = dataToDisplay.map((entry) => entry["DATE"]);
 
 const POSTS_COUNT = {};
 const LIKES_COUNT = {};
 const REPLIES_COUNT = {};
 
-data.map((entry) => {
+dataToDisplay.forEach((entry) => {
   POSTS_COUNT[entry["DATE"]] = entry["POSTS_COUNT"];
   LIKES_COUNT[entry["DATE"]] = entry["LIKES_COUNT"];
   REPLIES_COUNT[entry["DATE"]] = entry["REPLIES_COUNT"];
 });
-
-const dates = data.map((entry) => entry["DATE"]);
 
 const stacked_options = {
   maintainAspectRatio: true,
@@ -125,14 +166,32 @@ const stacked_bar_data = {
 
 return (
   <Style>
-    <div className="text-bg-dark rounded-4 p-3 mb-4">
-      {data !== null ? (
-        <div>
-          <BarEl options={stacked_options} data={stacked_bar_data} />
-        </div>
-      ) : (
-        <div>Loading ...</div>
-      )}
+    <div className="relative text-bg-dark rounded-4 p-3 mb-4">
+      <div className="absolute top-0 right-0 flex space-x-2 p-3">
+        {["1M", "3M", "YTD", "1Y", "3Y"].map((range) => (
+          <button
+            key={range}
+            onClick={() => handleDateRangeChange(range)}
+            className={`px-3 py-1 rounded transition-colors duration-200 ease-in ${
+              state.selectedDateRange === range
+                ? "bg-blue-500 text-white"
+                : "bg-gray-800 text-gray-400 hover:bg-gray-700"
+            }`}
+          >
+            {range}
+          </button>
+        ))}
+      </div>
+      <div className="rounded-4 p-3 mb-4 pt-16">
+        {" "}
+        {data !== null ? (
+          <div>
+            <BarEl options={stacked_options} data={stacked_bar_data} />
+          </div>
+        ) : (
+          <div>Loading ...</div>
+        )}
+      </div>
     </div>
   </Style>
 );
