563863ec1225dae9f7220e58ef5b47a8ccac87f0
y3k.near <y3k.near>
2023-09-19 13:26:10 +0000

    Update near_atlas.components.vis.total_wallets by y3k.near at 2023-09-19T13:26:10.870Z

--- y3k.near/near_atlas.components.vis.total_wallets/source_code.jsx
+++ y3k.near/near_atlas.components.vis.total_wallets/source_code.jsx
@@ -159,6 +159,30 @@ return (
             {range}
           </button>
         ))}
+
+        <button className="px-3 py-1 rounded transition-colors duration-200 ease-in bg-gray-800 text-gray-400 hover:bg-gray-700">
+          <a
+            href="https://api.flipsidecrypto.com/api/v2/queries/91b91aba-f307-48c9-9731-c3d475838f1f/data/latest"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-red-50 whitespace-normal break-words block max-w-xs"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+              />
+            </svg>
+          </a>
+        </button>
       </div>
       <div className="rounded-4 p-3 mb-4 pt-16">
         {" "}
