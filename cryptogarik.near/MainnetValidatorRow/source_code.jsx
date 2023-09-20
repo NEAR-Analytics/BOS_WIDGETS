88f8e1510d9bfdca52927e963fdf7c77401a392e
cryptogarik.near <cryptogarik.near>
2023-09-20 22:41:57 +0000

    Update MainnetValidatorRow by cryptogarik.near at 2023-09-20T22:41:57.378Z

---
+++ cryptogarik.near/MainnetValidatorRow/source_code.jsx
@@ -0,0 +1,48 @@
+const accountId = props.accountId || "";
+const isSlashed = props.isSlashed || true;
+const numExpectedBlocks = props.numExpectedBlocks || 0;
+const numExpectedChunks = props.numExpectedChunks || 0;
+const numProducedBlocks = props.numProducedBlocks || 0;
+const numProducedChunks = props.numProducedChunks || 0;
+const stake = props.stake || "0";
+
+const nearInYocto = 1000000000000000000000000;
+
+return (
+  <table className="table table-striped">
+    <tbody>
+      //{" "}
+      <tr>
+        // <td>Pool</td>
+        // <td>Active</td>
+        // <td>Stake</td>
+        // <td>Expected blocks</td>
+        // <td>Produced blocks</td>
+        // <td>Uptime %</td>
+        // <td>Expected chunks</td>
+        // <td>Produced chunks</td>
+        // <td>Uptime %</td>
+        //{" "}
+      </tr>{" "}
+      <tr>
+        <td>{accountId}</td>
+        <td>{(!isSlashed).toString()}</td>
+        <td>{stake && (stake / nearInYocto).toFixed(0)}</td>
+        <td>{numExpectedBlocks}</td>
+        <td>{numProducedBlocks}</td>
+        <td>
+          {numExpectedBlocks &&
+            ((numExpectedBlocks / numProducedBlocks) * 100).toFixed(0)}{" "}
+          %
+        </td>
+        <td>{numExpectedChunks}</td>
+        <td>{numProducedChunks}</td>
+        <td>
+          {numExpectedChunks &&
+            ((numExpectedChunks / numProducedChunks) * 100).toFixed(0)}{" "}
+          %
+        </td>
+      </tr>
+    </tbody>
+  </table>
+);
