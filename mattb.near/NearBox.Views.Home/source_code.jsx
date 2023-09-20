35de2f2d008081194659a3c207c3846597432d82
mattb.near <mattb.near>
2023-09-20 22:01:19 +0000

    Update NearBox.Views.Home by mattb.near at 2023-09-20T22:01:19.288Z

--- mattb.near/NearBox.Views.Home/source_code.jsx
+++ mattb.near/NearBox.Views.Home/source_code.jsx
@@ -13,23 +13,8 @@ State.init({
   },
   logged: false,
   disconnected: false,
-  loadedCredentials: false
+  loadedCredentials: false,
 });
-
-if (!state.logged && !state.disconnected && !state.loadedCredentials) {
-  Storage.privateGet("user");
-  
-  setTimeout(() => {
-    State.update({ user: Storage.privateGet("user") || {
-    mail: "",
-    phrase: "",
-  }, loadedCredentials: true });
-
-    if (!state.logged && state.user.mail && state.user.phrase) {
-      login();
-    }
-  }, 200);
-}
 
 const login = () => {
   MailChain.connect(state.user)
