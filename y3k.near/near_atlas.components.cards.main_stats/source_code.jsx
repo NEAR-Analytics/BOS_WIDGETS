ea5ed955125c183a07ca86f9eee9d1cfceb69da5
y3k.near <y3k.near>
2023-09-20 15:40:43 +0000

    Update near_atlas.components.cards.main_stats by y3k.near at 2023-09-20T15:40:43.724Z

--- y3k.near/near_atlas.components.cards.main_stats/source_code.jsx
+++ y3k.near/near_atlas.components.cards.main_stats/source_code.jsx
@@ -62,22 +62,21 @@ return (
       <div class="bg-gray shadow-xl rounded-lg p-6 w-1/2 bg-gray-800">
         <div class="flex flex-col items-center">
           <h2 class="text-2xl font-semibold mb-4 text-white">
+            Monthly Aactive Accounts
+          </h2>
+          <span class="text-4xl font-bold text-gray-200">
+            {formatNumber(MAA)}
+          </span>
+        </div>
+      </div>
+      <div class="bg-gray shadow-xl rounded-lg p-6 w-1/2 bg-gray-800">
+        <div class="flex flex-col items-center">
+          <h2 class="text-2xl font-semibold mb-4 text-white">
             Wallets Created
           </h2>
 
           <span class="text-4xl font-bold text-gray-200">
             {formatNumber(WALLETS_CREATED)}
-          </span>
-        </div>
-      </div>
-
-      <div class="bg-gray shadow-xl rounded-lg p-6 w-1/2 bg-gray-800">
-        <div class="flex flex-col items-center">
-          <h2 class="text-2xl font-semibold mb-4 text-white">
-            Monthly Aactive Accounts
-          </h2>
-          <span class="text-4xl font-bold text-gray-200">
-            {formatNumber(MAA)}
           </span>
         </div>
       </div>
