ec2ae8b93ba9504c85fd46028f2abe71fe9fa087
bluebiu.near <bluebiu.near>
2023-09-20 09:36:52 +0000

    Update Linea.Dapps by bluebiu.near at 2023-09-20T09:36:52.702Z

---
+++ bluebiu.near/Linea.Dapps/source_code.jsx
@@ -0,0 +1,115 @@
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px 36px;
+  color: #fff;
+  padding: 0 12px;
+`;
+
+const Search = styled.div`
+  display: flex;
+  position: fixed;
+  top: 112px;
+  right: 32px;
+  input {
+    color: #ffffff;
+    background: transparent;
+    border: none;
+    border-bottom: 1px #373a53 solid;
+    padding-right: 24px;
+  }
+  input:focus {
+    outline: none;
+    color: #ffffff;
+    background: transparent;
+    border: none;
+    box-shadow: none;
+    border-bottom: 1px #373a53 solid;
+  }
+  svg {
+    margin-left: -24px;
+    margin-top: 10px;
+  }
+  @media (max-width: 900px) {
+    display: none;
+  }
+`;
+
+const SearchIcon = (
+  <svg
+    width="21"
+    height="15"
+    viewBox="0 0 21 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="7.01829"
+      cy="7.01829"
+      r="6.01829"
+      stroke="#EBF479"
+      strokeWidth="2"
+    />
+    <rect
+      x="14.9141"
+      y="9.64941"
+      width="6.141"
+      height="2.63186"
+      rx="1.31593"
+      transform="rotate(30 14.9141 9.64941)"
+      fill="#EBF479"
+    />
+  </svg>
+);
+
+const dapps = [
+  {
+    src: "bluebiu.near/widget/Linea.Swap.Dex",
+    bannerImg:
+      "https://ipfs.near.social/ipfs/bafkreihlp4eqtmee2etir6abfw7chjydyc27mt2i4sefxdejyhx3jwpl34",
+    icon: "https://ipfs.near.social/ipfs/bafkreicjziybpod3isxnkw3q6w4ld57bacs4hddmipzx57k6ebv4wtzx4e",
+    tags: ["Dexes"],
+  },
+];
+
+State.init({
+  searchValue: "",
+});
+
+const handleSearchChange = (e) => {
+  const value = e.target.value;
+  State.update({
+    searchValue: value,
+  });
+};
+
+const metadataPromises = dapps.map(({ src }) =>
+  Social.get(`${src}/metadata/**`, "final")
+);
+
+const updatedTemplates = dapps.map((template, index) => ({
+  ...template,
+  name: metadataPromises[index]?.name || "",
+}));
+
+const filteredTemplates = updatedTemplates.filter(({ name }) =>
+  name.toLowerCase().includes(state.searchValue.toLowerCase())
+);
+
+return (
+  <Container>
+    <Search>
+      <input type="text" value={searchValue} onChange={handleSearchChange} />
+      {SearchIcon}
+    </Search>
+    {filteredTemplates.map((dapp) => (
+      <Widget
+        src="bluebiu.near/widget/Arbitrum.DappCard"
+        key={dapp.src}
+        props={{
+          ...dapp,
+        }}
+      />
+    ))}
+  </Container>
+);
