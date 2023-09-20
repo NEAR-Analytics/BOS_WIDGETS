a251fc4e7138b8265f19a307628a8459c70e9ad4
mattb.near <mattb.near>
2023-09-20 21:54:30 +0000

    Update NearBox.Views.App by mattb.near at 2023-09-20T21:54:30.185Z

--- mattb.near/NearBox.Views.App/source_code.jsx
+++ mattb.near/NearBox.Views.App/source_code.jsx
@@ -109,7 +109,6 @@ return (
         <Widget src={`${WIDGET_OWNER}/widget/NearBox.Components.Logo`} />
 
         <Logout>
-          <p>{!!MailChain && MailChain.getUserAddress()}</p>
           <img
             src="https://ipfs.near.social/ipfs/bafkreiem7zs4oxkkgsr2hgret2z2h3fj76kngsbmkdstfijcsyakioklzu"
             onClick={() => onLogout({ logged: false })}
