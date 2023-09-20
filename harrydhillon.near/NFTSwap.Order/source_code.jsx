dae3d561079a50693f57cb2c01283560f2efafc9
harrydhillon.near <harrydhillon.near>
2023-09-20 22:14:03 +0000

    Update NFTSwap.Order by harrydhillon.near at 2023-09-20T22:14:03.546Z

---
+++ harrydhillon.near/NFTSwap.Order/source_code.jsx
@@ -0,0 +1,86 @@
+const accountId = context.accountId;
+
+initState({ allTransactions: [] });
+
+useEffect(() => {
+  const allUserTransactionHashes = Near.view(
+    "swap.genadrop.near",
+    "get_hashes_for_owner",
+    {
+      owner_id: accountId,
+    }
+  );
+
+  const allTransactionData = [];
+
+  allUserTransactionHashes.map((item) => {
+    allTransactionData.push(
+      Near.view("swap.genadrop.near", "get_transaction_data", {
+        hash: item,
+      })
+    );
+  });
+
+  function processNFTs(nfts) {
+    let arrayToReturn = [];
+    nfts.map(async (nft) => {
+      const nftContract = nft.contract_id;
+      const tokenId = nft.token_id;
+      const metadata = Near.view(nftContract, "nft_metadata", {
+        token_id: tokenId,
+      });
+      const baseUri = metadata.base_uri || "";
+
+      const nftMetadata = Near.view(nftContract, "nft_token", {
+        token_id: tokenId,
+      });
+      const media = nftMetadata.metadata.media;
+      const image =
+        media.startsWith("https") || media.startsWith("http")
+          ? media
+          : `${baseUri}/${media}`;
+
+      let collection = "";
+
+      if (nftContract === "x.paras.near") {
+        const response = fetch(
+          `https://api-v2-mainnet.paras.id/token?token_id=${tokenId}`
+        );
+        collection = response.data.results[0].metadata.collection_id;
+      }
+
+      arrayToReturn.push({
+        title: nftMetadata.metadata.title,
+        image: image,
+        token_id: tokenId,
+        contract_id: nftContract,
+        collection: collection ? collection : metadata.name,
+      });
+    });
+    return arrayToReturn;
+  }
+
+  State.update({ allTransactions: allTransactionData });
+
+  const nftData = [];
+  allTransactionData.map((item) => {
+    const senderNFTs = processNFTs(item.sender_nfts);
+    const receiverNFTs = processNFTs(item.receiver_nfts);
+    nftData.push({ senderNFTs, receiverNFTs });
+  });
+  State.update({ nftData });
+}, []);
+
+return (
+  <div
+    style={{
+      border: "1px solid #000",
+      width: "100%",
+      borderRadius: 5,
+      padding: 10,
+    }}
+  >
+    <p>Reciever NFT</p>
+    <p>Sender NFT</p>
+  </div>
+);
