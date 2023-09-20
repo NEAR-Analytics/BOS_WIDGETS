a9d80d38d5af170dc089da039fa29d344ce0e598
libertydao.near <libertydao.near>
2023-09-20 21:39:43 +0000

    Update supporting.proposal by libertydao.near at 2023-09-20T21:39:43.081Z

--- libertydao.near/supporting.proposal/source_code.jsx
+++ libertydao.near/supporting.proposal/source_code.jsx
@@ -1,24 +1,15 @@
+const handleSubmit = props.handleSubmit || (() => {});
+
 const Container = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
   width: 100%;
   max-width: 800px;
-  // align-items: center;
   margin: 0 auto;
   background-color: var(--light-color);
   padding-bottom: 80px;
 `;
-
-// const Container = styled.div`
-//   width: 100%;
-//   max-width: 400px;
-//   margin: 0 auto;
-//   padding: 20px;
-//   background-color: #f5f5f5;
-//   border-radius: 8px;
-//   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-// `;
 
 const Header = styled.div`
   font-size: 18px;
@@ -49,26 +40,15 @@ const SubmitButton = styled.button`
   cursor: pointer;
 `;
 
-const [proposal, setProposal] = useState("");
-const [what, setWhat] = useState("");
-const [forWho, setForWho] = useState("");
-const [why, setWhy] = useState("");
-const [status, setStatus] = useState("");
-const [timeline, setTimeline] = useState("");
-const [needs, setNeeds] = useState("");
-
-const handleSubmit = () => {
-  // Handle form submission here using the individual state variables.
-  console.log({
-    proposal,
-    what,
-    forWho,
-    why,
-    status,
-    timeline,
-    needs,
-  });
-};
+State.init({
+  proposal: "",
+  what: "",
+  forWho: "",
+  why: "",
+  status: "",
+  timeline: "",
+  needs: "",
+});
 
 return (
   <Container>
@@ -84,52 +64,52 @@ return (
     <FormLabel>Proposal:</FormLabel>
     <TextInput
       type="text"
-      value={proposal}
-      onChange={(e) => setProposal(e.target.value)}
+      value={state.proposal}
+      onChange={(e) => State.update({ proposal: e.target.value })}
     />
 
     <FormLabel>What:</FormLabel>
     <TextInput
       type="text"
-      value={what}
-      onChange={(e) => setWhat(e.target.value)}
+      value={state.what}
+      onChange={(e) => State.update({ what: e.target.value })}
     />
 
     <FormLabel>For Who:</FormLabel>
     <TextInput
       type="text"
-      value={forWho}
-      onChange={(e) => setForWho(e.target.value)}
+      value={state.forWho}
+      onChange={(e) => State.update({ forWho: e.target.value })}
     />
 
     <FormLabel>Why:</FormLabel>
     <TextInput
       type="text"
-      value={why}
-      onChange={(e) => setWhy(e.target.value)}
+      value={state.why}
+      onChange={(e) => State.update({ why: e.target.value })}
     />
 
     <FormLabel>Status:</FormLabel>
     <TextInput
       type="text"
-      value={status}
-      onChange={(e) => setStatus(e.target.value)}
+      value={state.status}
+      onChange={(e) => State.update({ status: e.target.value })}
     />
 
     <FormLabel>Timeline:</FormLabel>
     <TextInput
       type="text"
-      value={timeline}
-      onChange={(e) => setTimeline(e.target.value)}
+      value={state.timeline}
+      onChange={(e) => State.update({ timeline: e.target.value })}
     />
 
     <FormLabel>Needs:</FormLabel>
     <TextInput
       type="text"
-      value={needs}
-      onChange={(e) => setNeeds(e.target.value)}
+      value={state.needs}
+      onChange={(e) => State.update({ needs: e.target.value })}
     />
 
-    <SubmitButton onClick={handleSubmit}>Submit</SubmitButton>
+    <SubmitButton onClick={handleSubmit(state)}>Submit</SubmitButton>
   </Container>
 );
