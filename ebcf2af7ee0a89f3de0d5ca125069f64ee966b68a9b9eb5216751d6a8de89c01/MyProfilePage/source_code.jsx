0a6280e849f574a52de7d955af77d2f9d3ffe347
ebcf2af7ee0a89f3de0d5ca125069f64ee966b68a9b9eb5216751d6a8de89c01 <ebcf2af7ee0a89f3de0d5ca125069f64ee966b68a9b9eb5216751d6a8de89c01>
2023-09-20 09:08:37 +0000

    Update MyProfilePage by ebcf2af7ee0a89f3de0d5ca125069f64ee966b68a9b9eb5216751d6a8de89c01 at 2023-09-20T09:08:37.842Z

---
+++ ebcf2af7ee0a89f3de0d5ca125069f64ee966b68a9b9eb5216751d6a8de89c01/MyProfilePage/source_code.jsx
@@ -0,0 +1,90 @@
+const account_id = context.accountId ?? props.accountId;
+
+const profileData = Social.getr(`${account_id}/profile`);
+
+console.log(profileData);
+
+const ProfileWrapper = styled.div`
+    padding: 0 0 30px 0;
+    .banner-wrapper { padding: 15px; }
+    .banner { width: 100%; aspect-ratio:1; border-radius: 20px; }
+    .banner-image {
+      border-radius: 20px;
+    }
+    .profile-wrapper { padding: 0 30px; margin-top: -55px; }
+    .avatar-wrapper {  width: 250px; height: 250px; border-radius: 70%; border: 3px solid green; overflow: hidden;}
+    .avatar { width: 100%; height: 100%; object-fit: cover; }
+    .name-wrapper { display: flex; flex-direction: column; justify-content: center; padding: 0 0 0 30px;}
+    .profile-name { }
+    .account-id { color: #656d76; font-size: 1.4rem; }
+`;
+
+const ActivityWrapper = styled.div`
+     display: flex;
+     .post-wrapper { padding: 30px; width: 50%; }
+     .vertical-line { border: 1px solid green; transform: scaleX(0.5);}
+     .widget-wrapper { padding: 30px; width: 50%; }
+`;
+
+const VR = styled.div` `;
+
+const Banner = () => {
+  return (
+    <>
+      <div className="banner-wrapper">
+        <div className="banner">
+          <img
+            className="banner-image"
+            src={
+              `https://ipfs.near.social/ipfs/${profileData.backgroundImage.ipfs_cid}` ??
+              `https://ipfs.near.social/ipfs/${profileData.image.ipfs_cid}`
+            }
+          ></img>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Profile = () => {
+  return (
+    <>
+      <Banner />
+      <div className="profile-wrapper">
+        <div className="avatar-wrapper">
+          <img
+            className="avatar"
+            src={`https://ipfs.near.social/ipfs/${profileData.image.ipfs_cid}`}
+          ></img>
+        </div>
+        <div className="name-wrapper">
+          <h1 className="profile-name">{profileData.name}</h1>
+          <h2 className="account-id">@{account_id}</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+return (
+  <>
+    <ProfileWrapper>
+      <Profile />
+    </ProfileWrapper>
+    <ActivityWrapper>
+      <div className="post-wrapper">
+        <Widget
+          src={`ebcf2af7ee0a89f3de0d5ca125069f64ee966b68a9b9eb5216751d6a8de89c01/widget/collegium.bos.wk3.MyPosts`}
+          props={props}
+        />
+      </div>
+      <div className="vertical-line" />
+      <div className="widget-wrapper">
+        <Widget
+          src={`ebcf2af7ee0a89f3de0d5ca125069f64ee966b68a9b9eb5216751d6a8de89c01/widget/collegium.bos.wk3.SocialWidget`}
+          props={props}
+        />
+      </div>
+    </ActivityWrapper>
+  </>
+);
