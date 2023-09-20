7f766b29625021cdae0a480c6a575ac5d5108305
cryptogarik.near <cryptogarik.near>
2023-09-20 22:47:52 +0000

    Update MainnetValidatorRow by cryptogarik.near at 2023-09-20T22:47:52.355Z

--- cryptogarik.near/MainnetValidatorRow/source_code.jsx
+++ cryptogarik.near/MainnetValidatorRow/source_code.jsx
@@ -1,5 +1,5 @@
 const accountId = props.accountId || "";
-const isSlashed = props.isSlashed || true;
+const active = props.active || true;
 const numExpectedBlocks = props.numExpectedBlocks || 0;
 const numExpectedChunks = props.numExpectedChunks || 0;
 const numProducedBlocks = props.numProducedBlocks || 0;
@@ -11,7 +11,7 @@ const nearInYocto = 1000000000000000000000000;
 return (
   <tr>
     <td>{accountId}</td>
-    <td>{(!isSlashed).toString()}</td>
+    <td>{(!!active).toString()}</td>
     <td>{stake && (stake / nearInYocto).toFixed(0)}</td>
     <td>{numExpectedBlocks}</td>
     <td>{numProducedBlocks}</td>
