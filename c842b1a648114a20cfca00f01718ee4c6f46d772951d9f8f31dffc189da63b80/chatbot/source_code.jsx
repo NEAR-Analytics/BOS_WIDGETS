c15ec6b4cbcd8ae299687b0228374026b68c92db
c842b1a648114a20cfca00f01718ee4c6f46d772951d9f8f31dffc189da63b80 <c842b1a648114a20cfca00f01718ee4c6f46d772951d9f8f31dffc189da63b80>
2023-09-21 05:35:27 +0000

    Update chatbot by c842b1a648114a20cfca00f01718ee4c6f46d772951d9f8f31dffc189da63b80 at 2023-09-21T05:35:27.393Z

---
+++ c842b1a648114a20cfca00f01718ee4c6f46d772951d9f8f31dffc189da63b80/chatbot/source_code.jsx
@@ -0,0 +1,24 @@
+return (
+  <>
+    <div style={{ backgroundColor: "#bacee0", width: "100%", height: "100%" }}>
+      <div
+        style={{ position: "fixed", bottom: "50px", width: "100%" }}
+        onSubmit={handleSubmit}
+      >
+        <input style={{}} />
+        <button
+          type="submit"
+          style={{
+            position: "absolute",
+            bottom: "0",
+            right: "0",
+            height: "100%",
+            width: "60px",
+          }}
+        >
+          전송
+        </button>
+      </div>
+    </div>
+  </>
+);
