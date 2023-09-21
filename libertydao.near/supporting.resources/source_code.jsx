094b05ee52ecc0776e02772fe01ac3b7debdf657
libertydao.near <libertydao.near>
2023-09-21 15:52:54 +0000

    Update supporting.resources by libertydao.near at 2023-09-21T15:52:54.350Z

--- libertydao.near/supporting.resources/source_code.jsx
+++ libertydao.near/supporting.resources/source_code.jsx
@@ -1,10 +1,58 @@
+const data = [
+  {
+    title: "Banyan Collective",
+    img: "path/to/banyan-collective-img.jpg",
+    link: "https://www.banyan.gg/",
+  },
+  {
+    title: "Shard Dog",
+    img: "path/to/shard-dog-img.jpg",
+    link: "https://shard.dog/",
+  },
+  {
+    title: "everything",
+    img: "path/to/everything-img.jpg",
+    link: "https://everything.dev",
+  },
+  {
+    title: "Lynkable",
+    img: "path/to/lynkable-img.jpg",
+    link: "https://lynkable.near.social/",
+  },
+  {
+    title: "NEAR NYC",
+    img: "path/to/near-nyc-img.jpg",
+    link: "https://near.org/nyc",
+  },
+  {
+    title: "Republic Crypto",
+    img: "path/to/republic-crypto-img.jpg",
+    link: "https://republiccrypto.com/",
+  },
+  {
+    title: "Techquity Labs",
+    img: "path/to/techquitylabs-img.jpg",
+    link: "https://techquitylabs.io/",
+  },
+  {
+    title: "Sparx Labs",
+    img: "path/to/sparxlabs-img.jpg",
+    link: "https://www.sparxlabs.io/",
+  },
+  {
+    title: "Blue Collar Blockchain",
+    img: "path/to/blue-collar-blockchain-img.jpg",
+    link: "https://bc2bc.org/",
+  },
+];
+
 const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: var(--light-color);
   flex: 1;
   display: flex;
   flex-direction: column;
-  width: 100%;
-  background-color: var(--light-color);
-  margin: 0 auto;
 `;
 
 const Header = styled.div`
@@ -27,9 +75,12 @@ const Navbar = styled.div`
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 0.1fr));
+  gap: 8px;
   width: 100%;
+  padding: 8px;
   background-color: var(--secondary-color);
 `;
 
@@ -40,8 +91,7 @@ const TopBanner = styled.div`
   font-size: 16px;
 `;
 
-
-const CardContainer = styled.div`
+const CardContainer = styled.a`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -50,10 +100,15 @@ const CardContainer = styled.div`
   border: 1px solid #ccc;
   border-radius: 8px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  width: 100%;
+  width: 300px;
+  height: 200px;
   cursor: pointer;
   transition: transform 0.2s;
   background-color: var(--light-color);
+  background-image: url(${(props) => props.img});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
 
   &:hover {
     transform: scale(1.05);
@@ -64,63 +119,47 @@ const Title = styled.h2`
   font-size: 20px;
   font-weight: bold;
   margin-bottom: 10px;
+  color: #fff; // White color for better visibility on images
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7); // Shadow for better readability
 `;
 
-const Description = styled.p`
-  font-size: 16px;
-  text-align: center;
-`;
-
-function Card({ title, img }) {
+function Card({ title, img, link }) {
   return (
-    <CardContainer>
+    <CardContainer
+      href={link}
+      img={img}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <Title>{title}</Title>
     </CardContainer>
   );
 }
 
-const data = [
-  {
-    title: "Banyan Collective",
-    img: "path/to/banyan-collective-img.jpg",
-  },
-  {
-    title: "Shard Dog",
-    img: "path/to/shard-dog-img.jpg",
-  },
-  {
-    title: "everything",
-    img: "path/to/everything-img.jpg",
-  },
-  {
-    title: "Lynkable",
-    img: "path/to/lynkable-img.jpg",
-  },
-  {
-    title: "NEAR NYC",
-    img: "path/to/near-nyc-img.jpg",
-  },
-  {
-    title: "Republic Crypto",
-    img: "path/to/republic-crypto-img.jpg",
-  },
-];
-
 return (
-  <Container>
-    <Header>Resource Network</Header>
-    <TopBanner>
-      <p>Liberty doesn't come without a cost. We're here to help with that.</p>
-      <p>
-        People, skills, time, partners, investors, opportunities, futures, teams
-        — everything comes through the right connections. And we're working on
-        those.
-      </p>
-    </TopBanner>
-    <Grid>
-      {data.map((it) => (
-        <Card key={index} {...it} />
-      ))}
-    </Grid>
+  <Container className="row">
+    <div className="col">
+      <Header>Resource Network</Header>
+      <TopBanner>
+        <p>
+          Liberty doesn't come without a cost. We're here to help with that.
+        </p>
+        <p>
+          People, skills, time, partners, investors, opportunities, futures,
+          teams — everything comes through the right connections. And we're
+          working on those.
+        </p>
+        <p>
+          Support doesn't just flow from us; it flows through us. Below you will
+          find an interactive catalog of organizations that believe in
+          supporting people like you.
+        </p>
+      </TopBanner>
+      <Grid>
+        {data.map((it) => (
+          <Card key={index} {...it} />
+        ))}
+      </Grid>
+    </div>
   </Container>
 );
