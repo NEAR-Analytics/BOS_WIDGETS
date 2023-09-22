dec474a29d0137ed84c856d056d1bcbea9014c22
saidulbadhon.near <saidulbadhon.near>
2023-09-22 19:55:58 +0000

    Update ProfilePage.Contributions.Data by saidulbadhon.near at 2023-09-22T19:55:58.041Z

--- saidulbadhon.near/ProfilePage.Contributions.Data/source_code.jsx
+++ saidulbadhon.near/ProfilePage.Contributions.Data/source_code.jsx
@@ -12,59 +12,56 @@ const allWidgetsHistoryChangesBlocks = Social.keys(
   }
 );
 
-console.log(
-  "allWidgetsHistoryChangesBlocks : ",
-  allWidgetsHistoryChangesBlocks
-);
+// console.log(
+//   "allWidgetsHistoryChangesBlocks : ",
+//   allWidgetsHistoryChangesBlocks
+// );
 
-// if (allWidgetsHistoryChangesBlocks === null) return "Loading...";
+if (allWidgetsHistoryChangesBlocks === null) return "Loading...";
 
-// const widget = allWidgetsHistoryChangesBlocks[accountId].widget;
+const widget = allWidgetsHistoryChangesBlocks[accountId].widget;
 
-// const totalCommits = Object.keys(widget)
-//   .map((key) => widget[key])
-//   .flat()
-//   .map((blockHeight) => {
-//     const xs = Near.block(blockHeight);
+const totalCommits = Object.keys(widget)
+  .map((key) => widget[key])
+  .flat()
+  .map((blockHeight) => {
+    const xs = Near.block(blockHeight);
 
-//     const timeMs = parseFloat(xs.header.timestamp_nanosec) / 1e6;
+    const timeMs = parseFloat(xs.header.timestamp_nanosec) / 1e6;
 
-//     return new Date(timeMs).toLocaleDateString();
-//   });
+    return new Date(timeMs).toLocaleDateString();
+  });
 
-// const checkOccurrence = (array, element) => {
-//   let counter = 0;
-//   for (let i = 0; i <= array.length; i++) {
-//     if (array[i] == element) {
-//       counter++;
-//     }
-//   }
-//   return counter;
-// };
+const checkOccurrence = (array, element) => {
+  let counter = 0;
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] == element) {
+      counter++;
+    }
+  }
+  return counter;
+};
 
-// const generate = () => {
-//   // Create an empty array to hold the objects
-//   const dataArray = [] || [{ empty: true }];
+const generate = () => {
+  // Create an empty array to hold the objects
+  const dataArray = [] || [{ empty: true }];
 
-//   // Loop through each day of 2023
-//   for (let i = 0; i < 365; i++) {
-//     // Create a new date object for the current day
-//     const date = new Date(2023, 0, i + 1);
+  // Loop through each day of 2023
+  for (let i = 0; i < 365; i++) {
+    // Create a new date object for the current day
+    const date = new Date(2023, 0, i + 1);
 
-//     const value = checkOccurrence(totalCommits, date.toLocaleDateString());
+    const value = checkOccurrence(totalCommits, date.toLocaleDateString());
 
-//     // Create a new object with the date and value and add it to the array
-//     dataArray.push({ date: date.toLocaleDateString(), value });
-//   }
+    // Create a new object with the date and value and add it to the array
+    dataArray.push({ date: date.toLocaleDateString(), value });
+  }
 
-//   return dataArray;
-// };
+  return dataArray;
+};
 
 // const array = generate();
 
 return (
-  <div
-    id="Getting-Contributions"
-    onClick={() => props.responce(allWidgetsHistoryChangesBlocks)}
-  />
+  <div id="Getting-Contributions" onClick={() => props.responce(generate())} />
 );
