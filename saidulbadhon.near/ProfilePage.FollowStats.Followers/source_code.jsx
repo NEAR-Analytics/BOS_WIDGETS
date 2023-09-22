666be2eafac9b8c5a7e8d48f50b8194fadebab32
saidulbadhon.near <saidulbadhon.near>
2023-09-22 18:58:52 +0000

    Update ProfilePage.FollowStats.Followers by saidulbadhon.near at 2023-09-22T18:58:52.091Z

---
+++ saidulbadhon.near/ProfilePage.FollowStats.Followers/source_code.jsx
@@ -0,0 +1,17 @@
+const accountId = props.accountId;
+
+if (!accountId) {
+  return "";
+}
+
+const following = Social.keys(`${accountId}/graph/follow/*`, "final", {
+  return_type: "BlockHeight",
+  values_only: true,
+});
+
+const numFollowing = following
+  ? Object.keys(following[accountId].graph.follow || {}).length
+  : null;
+const numFollowers = followers ? Object.keys(followers || {}).length : null;
+
+return numFollowing !== null ? <span>{numFollowing}</span> : "?";
