5d070f020124e551d4e2e8d2c1fab91936a555eb
andyh.near <andyh.near>
2023-09-22 22:31:14 +0000

    Update LandingPage by andyh.near at 2023-09-22T22:31:14.764Z

--- andyh.near/LandingPage/source_code.jsx
+++ andyh.near/LandingPage/source_code.jsx
@@ -81,10 +81,8 @@ return (
       </div>
     </div>
     */}
-    {/*
     <footer className="pt-5 my-5 text-muted border-top">
       Created by the Bootstrap team &middot; &copy; 2021
     </footer>
-    */}
   </div>
 );
