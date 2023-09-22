faf2397d10928a422d29878166c6755b03d33c36
saidulbadhon.near <saidulbadhon.near>
2023-09-22 19:06:20 +0000

    Update ProfilePage.FollowStats.Followers by saidulbadhon.near at 2023-09-22T19:06:20.952Z

--- saidulbadhon.near/ProfilePage.FollowStats.Followers/source_code.jsx
+++ saidulbadhon.near/ProfilePage.FollowStats.Followers/source_code.jsx
@@ -4,14 +4,11 @@ if (!accountId) {
   return "";
 }
 
-const following = Social.keys(`${accountId}/graph/follow/*`, "final", {
+const followers = Social.keys(`*/graph/follow/${accountId}`, "final", {
   return_type: "BlockHeight",
   values_only: true,
 });
 
-const numFollowing = following
-  ? Object.keys(following[accountId].graph.follow || {}).length
-  : null;
 const numFollowers = followers ? Object.keys(followers || {}).length : null;
 
-return numFollowing !== null ? <span>{numFollowing}</span> : "?";
+return numFollowers !== null ? <span>{numFollowers}</span> : "";
