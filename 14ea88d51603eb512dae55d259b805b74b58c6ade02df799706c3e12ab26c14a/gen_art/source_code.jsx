cc1a2f015d206d77086d763cf44feeb8dafc0895
14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a <14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a>
2023-09-23 20:32:19 +0000

    Update gen_art by 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a at 2023-09-23T20:32:19.735Z

--- 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a/gen_art/source_code.jsx
+++ 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a/gen_art/source_code.jsx
@@ -5,13 +5,12 @@ const srcData = `
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
 
-<script language="javascript" type="text/javascript" src="/path/to/p5.scribble.js"></script>
-
+ <script src="https://cdn.rawgit.com/generative-light/p5.scribble.js/master/p5.scribble.js"></script>
 
 <script>
 function setup() {
       createCanvas(1000, 1000); // 캔버스 크기 설정
-      strokeWeight(5); // 선의 두께 설정
+      strokeWeight(1); // 선의 두께 설정
       background(240); // 배경색 설정
     }
 
@@ -21,14 +20,34 @@ function draw() {
   let r = random(255); // 빨간색 성분
   let g = random(255); // 초록색 성분
   let b = random(255); // 파란색 성분
-  fill(r, g, b); // 랜덤한 색상 설정
-
+  stroke(r, g, b);
   // 랜덤한 위치 생성
   let x = random(width);
   let y = random(height);
   let size = random(width/10);
+  var scribble       = new Scribble();
+        scribble.bowing    = 0.1;
+
+ // scribble.scribbleEllipse( x, y, size, size );
+let numPoints = 40; // 효과 선분의 개수
+  let angleIncrement = TWO_PI / numPoints;
+  let radius = size / 2 + 10; // 원의 반지름 + 여백
+
+  let xCoords = [];
+  let yCoords = [];
+
+  for (let i = 0; i < numPoints; i++) {
+    let angle = i * angleIncrement;
+    let xPos = x + cos(angle) * radius;
+    let yPos = y + sin(angle) * radius;
+    xCoords.push(xPos);
+    yCoords.push(yPos);
+  }
+  var gap = 2.1;
+          // the angle of the hachure in degrees
+  let angle = random(90);
+  scribble.scribbleFilling( xCoords, yCoords, gap, angle );
   
-  ellipse(x, y, size, size); // 원 그리기
   
 }
 
