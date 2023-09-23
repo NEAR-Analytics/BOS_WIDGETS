1f7768cfca149fe143fbe0de61bbc2c04772716a
azbang.near <azbang.near>
2023-09-23 23:13:53 +0000

    Update plaid by azbang.near at 2023-09-23T23:13:53.183Z

--- azbang.near/plaid/source_code.jsx
+++ azbang.near/plaid/source_code.jsx
@@ -80,11 +80,11 @@ const VerifyButton = styled.button`
 `;
 
 const accessToken = state.accessToken || Storage.privateGet("plaidAccessToken");
-if (accessToken === null) return "No access token";
+if (accessToken === null) return null;
 
 if (accessToken) {
   const response = fetch(`${PLAID_API}/transactions?token=${accessToken}`);
-  if (!response.ok) return "Transactions failed";
+  if (!response.ok) return null;
 
   const handleVerify = () => {
     if (state.verifing) return;
