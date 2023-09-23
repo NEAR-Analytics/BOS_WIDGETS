819c4bcff60d606ac359d5d88f66104c165b4ef8
andyh.near <andyh.near>
2023-09-23 06:28:08 +0000

    Update ShapeSet by andyh.near at 2023-09-23T06:28:08.651Z

--- andyh.near/ShapeSet/source_code.jsx
+++ andyh.near/ShapeSet/source_code.jsx
@@ -6,8 +6,30 @@ return (
         props={{
           color: "#C1200B",
           iconColor: "white",
-          icon: "1-circle-fill",
+          icon: "circle",
           radius: "25px",
+        }}
+      />
+    </div>
+    <div className="row">
+      <Widget
+        src="andyh.near/widget/Square"
+        props={{
+          color: "#4A825A",
+          iconColor: "white",
+          icon: "square",
+          length: "50px",
+        }}
+      />
+    </div>
+    <div className="row">
+      <Widget
+        src="andyh.near/widget/Triangle"
+        props={{
+          color: "#0A81D1",
+          iconColor: "white",
+          icon: "triangle",
+          height: "50px",
         }}
       />
     </div>
