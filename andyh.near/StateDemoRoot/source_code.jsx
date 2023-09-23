012c7501e4f4df5f7c5f1604aad6e204a01325bf
andyh.near <andyh.near>
2023-09-23 05:18:49 +0000

    Update StateDemoRoot by andyh.near at 2023-09-23T05:18:49.905Z

--- andyh.near/StateDemoRoot/source_code.jsx
+++ andyh.near/StateDemoRoot/source_code.jsx
@@ -1,26 +1,30 @@
-State.init({
-  red: { a: 0, b: 0, c: 0 },
-  green: { a: 0, b: 0, c: 0 },
-  blue: { a: 0, b: 0, c: 0 },
-});
+State.init({ root: 0, parent: 0, child: 0 });
 
 const roots = { red: state.red, green: state.green, blue: state.blue };
 
 return (
-  <div className="row">
+  <div className="col">
     {Object.entries(roots).map(([color, colorState], i) => {
       const children = (
         <span style={{ padding: "50% 50%", color: "white" }}>xyz</span>
       );
 
       return (
-        <div className="col" key={i}>
-          {color === "red" && (
-            <Widget
-              src="andyh.near/widget/Circle"
-              props={{ radius: 50, color: "red", children }}
-            />
-          )}
+        <div className="row" key={i}>
+          <div className="col">
+            <div className="row">
+              <Widget
+                src="andyh.near/widget/Circle"
+                props={{ radius: 50, color: "#C1200B", children }}
+              />
+            </div>
+            <div className="row">
+              <Widget
+                src="andyh.near/widget/Circle"
+                props={{ radius: 50, color: "red", children }}
+              />
+            </div>
+          </div>
         </div>
       );
     })}
