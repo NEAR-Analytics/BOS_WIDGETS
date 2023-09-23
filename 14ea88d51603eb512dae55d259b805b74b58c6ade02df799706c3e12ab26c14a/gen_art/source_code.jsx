e369a4b2d51b8ee7cee2b72d0d8af8286123f642
14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a <14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a>
2023-09-23 14:26:41 +0000

    Update gen_art by 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a at 2023-09-23T14:26:41.488Z

---
+++ 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a/gen_art/source_code.jsx
@@ -0,0 +1,46 @@
+const srcData = `
+
+<style>
+
+
+</style>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
+
+<script>
+function setup() {
+      createCanvas(1200, 1200); // 캔버스 크기 설정
+      background(220); // 배경색 설정
+    }
+
+
+function draw() {
+  // 랜덤한 색상 생성
+  let r = random(255);
+  let g = random(255);
+  let b = random(255);
+  fill(r, g, b); // 랜덤한 색상 설정
+
+  // 랜덤한 위치 생성
+  let x = random(width);
+  let y = random(height);
+  
+  ellipse(x, y, 100, 100); // 원 그리기
+}
+
+</script>
+`;
+
+return (
+  <>
+    <iframe
+      srcDoc={srcData}
+      
+      style={{
+        height: "100%",
+        width: "100%",
+      }}
+    />
+  </>
+);
