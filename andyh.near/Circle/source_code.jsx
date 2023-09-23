f62cabc4d96e03eca0a9e4c049bc517eb058544d
andyh.near <andyh.near>
2023-09-23 04:34:31 +0000

    Update Circle by andyh.near at 2023-09-23T04:34:31.489Z

--- andyh.near/Circle/source_code.jsx
+++ andyh.near/Circle/source_code.jsx
@@ -1,6 +1,13 @@
 const diameterPx = `${props.radius * 2}px`;
 return (
-  <div style={{ width: diameterPx, height: diameterPx, borderRadius: "100%" }}>
-    Hello World
+  <div
+    style={{
+      width: diameterPx,
+      height: diameterPx,
+      borderRadius: "100%",
+      backgroundColor: props.color,
+    }}
+  >
+    {props.children}
   </div>
 );
