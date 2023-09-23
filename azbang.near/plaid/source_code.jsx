8b7bfb3e5d093f30dde433f6c8d710783fecd0bd
azbang.near <azbang.near>
2023-09-23 13:18:47 +0000

    Update plaid by azbang.near at 2023-09-23T13:18:47.690Z

--- azbang.near/plaid/source_code.jsx
+++ azbang.near/plaid/source_code.jsx
@@ -1,8 +1,28 @@
-console.log(context);
+State.init({
+  origin: "",
+});
+
+const src = `
+<script>
+const origin = document.location.ancestorOrigins[0];
+window.top.postMessage(origin, "*")
+</script>
+`;
 
 return (
   <div>
-    <p>{context.widgetSrc}</p>
+    <p>
+      {state.origin}/{context.widgetSrc}
+    </p>
     <a href={`http://localhost:3000?`}>Connect bank</a>
+
+    <iframe
+      style={{ display: "none" }}
+      onMessage={(origin) => {
+        console.log(origin);
+        State.update({ origin });
+      }}
+      srcDoc={src}
+    ></iframe>
   </div>
 );
