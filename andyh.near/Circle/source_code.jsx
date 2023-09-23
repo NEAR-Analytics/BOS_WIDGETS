e7b4fa81cac228a5fec336803d133f33207e1aa1
andyh.near <andyh.near>
2023-09-23 06:39:59 +0000

    Update Circle by andyh.near at 2023-09-23T06:39:59.016Z

--- andyh.near/Circle/source_code.jsx
+++ andyh.near/Circle/source_code.jsx
@@ -7,6 +7,8 @@ return (
       borderRadius: "100%",
       backgroundColor: props.color,
       verticalAlign: "center",
+      position: "relative",
+      top: "calc(50% - 16px)",
     }}
   >
     <i className={`bi-${props.icon}`} style={{ color: props.iconColor }} />
