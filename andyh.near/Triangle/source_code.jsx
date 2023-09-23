c537cc93c65dbd15029111c0ab515bd5e6878c23
andyh.near <andyh.near>
2023-09-23 06:43:21 +0000

    Update Triangle by andyh.near at 2023-09-23T06:43:21.011Z

--- andyh.near/Triangle/source_code.jsx
+++ andyh.near/Triangle/source_code.jsx
@@ -6,8 +6,16 @@ return (
       borderLeft: `${props.height / 2}px solid transparent`,
       borderRight: `${props.height / 2}px solid transparent`,
       borderBottom: `${props.height}px solid ${props.color}`,
+      textAlign: "center",
     }}
   >
-    <i className={`bi-${props.icon}`} style={{ color: props.iconColor }} />
+    <i
+      className={`bi-${props.icon}`}
+      style={{
+        color: props.iconColor,
+        position: "relative",
+        top: "calc(50% - 16px)",
+      }}
+    />
   </div>
 );
