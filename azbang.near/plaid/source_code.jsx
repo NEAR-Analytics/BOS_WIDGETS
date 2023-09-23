caf36b82140e9e46374a67f3f193ed25746d4567
azbang.near <azbang.near>
2023-09-23 20:23:36 +0000

    Update plaid by azbang.near at 2023-09-23T20:23:36.214Z

--- azbang.near/plaid/source_code.jsx
+++ azbang.near/plaid/source_code.jsx
@@ -105,6 +105,18 @@ if (accessToken) {
     });
   };
 
+  const signedMessage =
+    "localhost wants you to sign in with your Ethereum account:\n" +
+    "0xf915Aa479b06d4c81cbba39EB939E4c2EF27ADae\n" +
+    "\n" +
+    "This is a test statement.  You can put anything you want here.\n" +
+    "\n" +
+    "URI: https://localhost/login\n" +
+    "Version: 1\n" +
+    "Chain ID: 1\n" +
+    "Nonce: qkyHZMeNi5oGjroWK\n" +
+    "Issued At: 2023-09-23T20:16:52.328Z";
+
   const iframeSrc = `
     <script>
         let litNodeClient;
@@ -134,16 +146,7 @@ if (accessToken) {
                 authSig: {
                     sig: '0x90fd44268518c4e7eb28a879f71af07bb4b8722e0c52c7537f616665c368c3761f7275e311f14a5a5a4eacc61f223b4e2a233adadc110a8d58d827029a1a146c1b',
                     derivedVia: 'web3.eth.personal.sign',
-                    signedMessage: 'localhost wants you to sign in with your Ethereum account:\n' +
-                        '0xf915Aa479b06d4c81cbba39EB939E4c2EF27ADae\n' +
-                        '\n' +
-                        'This is a test statement.  You can put anything you want here.\n' +
-                        '\n' +
-                        'URI: https://localhost/login\n' +
-                        'Version: 1\n' +
-                        'Chain ID: 1\n' +
-                        'Nonce: qkyHZMeNi5oGjroWK\n' +
-                        'Issued At: 2023-09-23T20:16:52.328Z',
+                    signedMessage: ${signedMessage},
                     address: '0xf915Aa479b06d4c81cbba39EB939E4c2EF27ADae'
                 },
                 jsParams: {
