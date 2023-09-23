78c0143390abbdcb1af1bb5633ceebeda4f349a8
andyh.near <andyh.near>
2023-09-23 04:48:19 +0000

    Update StateDemoRoot by andyh.near at 2023-09-23T04:48:19.161Z

--- andyh.near/StateDemoRoot/source_code.jsx
+++ andyh.near/StateDemoRoot/source_code.jsx
@@ -9,7 +9,9 @@ const roots = { red: state.red, green: state.green, blue: state.blue };
 return (
   <div className="row">
     {Object.entries(roots).map(([color, colorState], i) => {
-      const children = <span style={{ color: "white" }}>xyz</span>;
+      const children = (
+        <span style={{ padding: "50% 50%", color: "white" }}>xyz</span>
+      );
 
       return (
         <div className="col" key={i}>
