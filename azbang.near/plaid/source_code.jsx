a301c34b67734d5af082d7324bbf51cd405f7e6f
azbang.near <azbang.near>
2023-09-23 14:32:54 +0000

    Update plaid by azbang.near at 2023-09-23T14:32:54.872Z

--- azbang.near/plaid/source_code.jsx
+++ azbang.near/plaid/source_code.jsx
@@ -11,24 +11,20 @@ window.top.postMessage(origin, "*")
 </script>
 `;
 
-if (props.public_token) {
-  let accessToken = Storage.privateGet("plaidAccessToken");
-  if (accessToken == null) {
-    const { access_token } = fetch(`${PLAID_API}/exchange-public-token`, {
-      body: JSON.stringify({ public_token: props.public_token }),
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-    });
-
-    if (access_token == null) {
-      return <p>Bank connecting</p>;
-    }
-
-    Storage.privateSet("plaidAccessToken", access_token);
-    accessToken = access_token;
-  }
-
+let accessToken = Storage.privateGet("plaidAccessToken");
+if (accessToken) {
   return <p>Bank connected {accessToken}</p>;
+}
+
+if (props.public_token) {
+  const { access_token } = fetch(`${PLAID_API}/exchange-public-token`, {
+    body: JSON.stringify({ public_token: props.public_token }),
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+  });
+
+  Storage.privateSet("plaidAccessToken", access_token);
+  return <p>Bank connecting...</p>;
 }
 
 return (
