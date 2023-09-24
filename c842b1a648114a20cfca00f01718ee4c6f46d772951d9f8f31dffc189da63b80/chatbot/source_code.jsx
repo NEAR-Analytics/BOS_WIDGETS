beb61652d7bc942ea139a1ad4f81cbdd12a11bed
c842b1a648114a20cfca00f01718ee4c6f46d772951d9f8f31dffc189da63b80 <c842b1a648114a20cfca00f01718ee4c6f46d772951d9f8f31dffc189da63b80>
2023-09-24 02:17:45 +0000

    Update chatbot by c842b1a648114a20cfca00f01718ee4c6f46d772951d9f8f31dffc189da63b80 at 2023-09-24T02:17:45.543Z

--- c842b1a648114a20cfca00f01718ee4c6f46d772951d9f8f31dffc189da63b80/chatbot/source_code.jsx
+++ c842b1a648114a20cfca00f01718ee4c6f46d772951d9f8f31dffc189da63b80/chatbot/source_code.jsx
@@ -1,23 +1,91 @@
+const [messages, setMessages] = useState([]);
+const [inputText, setInputText] = useState("");
+const [scrollToBottom, setScrollToBottom] = useState(true);
+
+const handleInputChange = (e) => {
+  setInputText(e.target.value);
+};
+
+const addMessage = (text, isUser) => {
+  const newMessage = { text, isUser };
+  setMessages([...messages, newMessage]);
+};
+
+const handleSendMessage = () => {
+  if (inputText.trim() === "") return;
+
+  // Add user message
+  addMessage(inputText, true);
+  // Simulate bot response
+  setTimeout(() => {
+    const userMessage = inputText.trim();
+    let botResponse = "죄송해요, 이해하지 못했어요."; // Default bot response
+
+    if (userMessage === "안녕") {
+      botResponse = "안녕하세요";
+    } else if (userMessage.includes("날씨")) {
+      botResponse = "오늘 날씨는 맑아요.";
+    } else if (userMessage.includes("시간")) {
+      const now = new Date();
+      const hours = now.getHours();
+      const minutes = now.getMinutes();
+      botResponse = `현재 시간은 ${hours}시 ${minutes}분이에요.`;
+    }
+
+    addMessage(botResponse, true);
+
+    // Set scrollToBottom to trigger scrolling
+    setScrollToBottom(true);
+  }, 1000); // Simulate a delay for the bot's response
+
+  setInputText("");
+};
+
 return (
   <>
-    <div style={{ backgroundColor: "#bacee0", width: "100%", height: "100%" }}>
-      <div
-        style={{ position: "fixed", bottom: "50px", width: "100%" }}
-        onSubmit={handleSubmit}
-      >
-        <input style={{}} />
-        <button
-          type="submit"
-          style={{
-            position: "absolute",
-            bottom: "0",
-            right: "0",
-            height: "100%",
-            width: "60px",
-          }}
+    <div
+      class="App"
+      style={{ backgroundColor: "#bacee0", width: "100%", height: "100%" }}
+    >
+      <div class="chat-window" style={{}}>
+        <div class="messages">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              class={`message ${message.isUser ? "user" : "bot"}`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <div
+          class="input-box"
+          style={{ position: "fixed", bottom: "50px", width: "100%" }}
         >
-          전송
-        </button>
+          <input
+            style={{}}
+            type="text"
+            value={inputText}
+            onChange={handleInputChange}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                handleSendMessage();
+              }
+            }}
+          />
+          <button
+            onClick={handleSendMessage}
+            style={{
+              position: "absolute",
+              bottom: "0",
+              right: "0",
+              height: "100%",
+              width: "60px",
+            }}
+          >
+            전송
+          </button>
+        </div>
       </div>
     </div>
   </>
