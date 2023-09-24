ca1985f83e9d6bf5e4ac747df9a68e2c40cd3897
azbang.near <azbang.near>
2023-09-24 07:01:54 +0000

    Update peer2peer by azbang.near at 2023-09-24T07:01:54.777Z

--- azbang.near/peer2peer/source_code.jsx
+++ azbang.near/peer2peer/source_code.jsx
@@ -1,3 +1,7 @@
+State.init({
+  amount: 10,
+});
+
 const sender = Ethers.send("eth_requestAccounts", [])[0];
 if (!sender) return "Please login first";
 
@@ -23,17 +27,94 @@ const Button = styled.button`
   }
 `;
 
-const CONTRACT = "0x79b78D8CCCAccCA749D6dEa6BF5457793D2db34c";
+const Container = styled.div`
+    display: flex;
+    height: 600px;
+    margin: 48px auto;
+    justify-content: center;
+    flex-direction: column;
+    width: 400px;
+    gap: 16px;
+
+    button {
+      width: 100%;
+    }
+
+    input {
+      height: 56px;
+    }
+
+`;
+
+const AmountButton = styled(Button)`
+  background-color: ${(p) => (p.selected ? "green" : "#255ff4")}
+`;
+
+const CONTRACT = "0x8c9e6c40d3402480ACE624730524fACC5482798c";
 const tokenAddress = "0x8c9e6c40d3402480ACE624730524fACC5482798c";
-const abi = ["function approve(address _spender, uint _value)"];
+const erc20abi = ["function approve(address _spender, uint _value)"];
+const abi = [
+  "function createPaymentRequest(uint256 amt, bytes32 emailHash)",
+  "function getPaymentRequestsByIds(uint256[] memory requestIds) external view returns (PaymentRequest[] memory)",
+];
 
 const signer = Ethers.provider().getSigner();
-const contract = new ethers.Contract(tokenAddress, abi, signer);
+const erc20 = new ethers.Contract(tokenAddress, erc20abi, signer);
+const peer2peer = new ethers.Contract(CONTRACT, abi, signer);
+const daiDecimal = 18;
 
 const handleCreate = () => {
-  contract.approve(CONTRACT, 10).then((tx) => {
-    tx.wait().then(console.log);
+  State.update({ loading: true });
+  const emailHash = ethers.utils.sha256(ethers.utils.toUtf8Bytes(state.amount));
+  const amount = ethers.utils.parseUnits(state.amount.toString(), daiDecimal);
+
+  erc20.approve(CONTRACT, 10).then((tx) => {
+    tx.wait().then(() => {
+      peer2peer.createPaymentRequest(amount, emailHash).then((tx) => {
+        tx.wait().then(() => {
+          State.update({ loading: false });
+        });
+      });
+    });
   });
 };
 
-return <Button onClick={() => handleCreate()}>Create order</Button>;
+return (
+  <Container>
+    <input
+      value={state.email}
+      placeholder="Enter your Zelly email"
+      onChange={(e) => State.update({ email: e.target.value })}
+    />
+
+    <div style={{ display: "flex", gap: 12 }}>
+      <AmountButton selected onClick={() => State.update({ amount: 10 })}>
+        $10
+      </AmountButton>
+      <AmountButton onClick={() => State.update({ amount: 50 })}>
+        $50
+      </AmountButton>
+      <AmountButton onClick={() => State.update({ amount: 100 })}>
+        $100
+      </AmountButton>
+    </div>
+
+    <Button onClick={() => handleCreate()}>
+      {state.loading ? (
+        <Widget
+          src="azbang.near/widget/dots-spinner"
+          props={{
+            style: {
+              height: 32,
+              display: "flex",
+              alignItems: "center",
+              margin: "auto",
+            },
+          }}
+        />
+      ) : (
+        "Sell DAI"
+      )}
+    </Button>
+  </Container>
+);
