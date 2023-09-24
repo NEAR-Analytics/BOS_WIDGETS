ab6d4fa5498c7d668e55b5ecedb53f6298a7d442
joelblenman21.near <joelblenman21.near>
2023-09-24 07:54:30 +0000

    Update ItemComponent by joelblenman21.near at 2023-09-24T07:54:30.399Z

--- joelblenman21.near/ItemComponent/source_code.jsx
+++ joelblenman21.near/ItemComponent/source_code.jsx
@@ -1,16 +1,22 @@
 const Picture = styled.div`
   border-radius: var(--br-xs);
-  background-color: blue;
- 
   height: 173px;
+  overflow: hidden; // Prevents the image from overflowing its container
+  
+  img {
+    width: 100%; // Make the image stretch or shrink to fit the width of the container
+    height: 100%; // Make the image stretch or shrink to fit the height of the container
+    object-fit: cover; // Cover the entire content box, potentially cutting off the image
+    object-position: center; // Center the image within its container
+  }
 `;
 
-const ItemDescription = styled.div`
+const ItemTitle = styled.div`
   font-size: 16px;
   font-weight: 500;
 `;
 
-const PleaseAddYour = styled.div`
+const ItemDescription = styled.div`
   font-family: var(--font-roboto-slab);
   color: #666;
   width: 256px;
@@ -31,10 +37,9 @@ const ItemComponent1 = styled.div`
 `;
 
 const Tag = styled.div`
-  color: #fff;
+  color: #000;
   display: flex;
   justify-content: flex-end;
-  background-color: #3838f4;
   width: 72px;
   padding: var(--padding-5xs) 0;
 `;
@@ -53,7 +58,6 @@ const Content = styled.div`
 
 const ItemComponent = styled.div`
   border-radius: var(--br-xs);
-  width: 100%;
   padding: 24px;
   box-sizing: border-box;
   text-align: left;
@@ -66,31 +70,84 @@ const ItemComponent = styled.div`
 `;
 
 const Container = styled.div`
-  background-color: red;
-  height: 100%
+  background-color: #f2f2f2;
+  height: 100%;
   display: flex;
+  width:fit-content;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width:fit-content
+  border-radius: 12px;
+  border-width:solid;
+  border-size: 2;
+`;
+const AddToCartButton = styled.button`
+  margin-top: 16px;
+  padding: 12px 24px;
+  background-color: #3838f4; // or any desired color
+  color: #fff;
+  border: none;
+  border-radius: var(--br-xs);
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #2727d4; // a slightly darker shade for hover effect
+  }
 `;
 
-// Use the styled components in the React components
+State.init({
+  selectedItems: [],
+});
+const handleAddToCart = props.handleAddToCart || (() => {});
+// const handleAddToCart = (title) => {
+//   console.log("This is the title: ", title);
+//   // check if already selected
+//   if (state.selectedElements.includes(title)) {
+//     // if already selected, remove check
+//     const updatedItems = state.selectedElements.filter(
+//       (elementId) => elementId !== title
+//     );
+//     // update in local storage so it can be picked up by the cart
+//     Storage.set("cart-items", JSON.stringify(updatedElements));
+//     // update in state, so there is a smooth experience
+//     State.update({
+//       selectedElements: updatedItems,
+//     });
+//   } else {
+//     // not selected, so add to array
+//     const updatedElements = [...state.selectedElements, title];
+//     // update in local storage so it can be picked up by the cart
+//     Storage.set("cart-items", JSON.stringify(updatedElements));
+//     // update in state, so there is a smooth experience
+//     State.update({
+//       selectedElements: updatedElements,
+//     });
+//   }
+// };
+
+State.init({
+  selectedItems: [],
+});
+
 return (
   <Container>
     <ItemComponent>
       <Content>
-        <Picture />
+        <Picture>
+          <img src={props.item.image} />
+        </Picture>
         <ItemDescriptionParent>
-          <ItemDescription>Item description</ItemDescription>
-          <PleaseAddYour>
-            Please add your content here. Keep it short and simple. And smile :)
-          </PleaseAddYour>
+          <ItemTitle>{props.item.title}</ItemTitle>
+          <ItemDescription>{props.item.description}</ItemDescription>
           <ItemComponent1>
             <Tag>
-              <TagOrButton>Price</TagOrButton>
+              <TagOrButton>{props.item.price}</TagOrButton>
             </Tag>
           </ItemComponent1>
+          <AddToCartButton onClick={() => handleAddToCart(props.item.title)}>
+            Add to Cart
+          </AddToCartButton>
         </ItemDescriptionParent>
       </Content>
     </ItemComponent>
