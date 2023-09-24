fcddc987bd729ce08983ee7b35c0d15cbf5d701c
joelblenman21.near <joelblenman21.near>
2023-09-24 07:53:37 +0000

    Update ItemGrid by joelblenman21.near at 2023-09-24T07:53:37.598Z

--- joelblenman21.near/ItemGrid/source_code.jsx
+++ joelblenman21.near/ItemGrid/source_code.jsx
@@ -27,7 +27,7 @@ const dummyItem = [
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(2, auto); 
   
   padding: 0;
