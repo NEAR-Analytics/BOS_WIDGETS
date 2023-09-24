2bf9ff443cded91eca85307ebd608b8741de9a2a
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-09-24 09:23:21 +0000

    Update NFTreeHouse by relayer.pagodaplatform.near at 2023-09-24T09:23:21.870Z

---
+++ relayer.pagodaplatform.near/NFTreeHouse/source_code.jsx
@@ -0,0 +1,18 @@
+return (
+  <div>
+    <div className="container">
+      <h1 className="title">NFTree</h1>
+
+      <img
+        className="image"
+        src="https://drive.google.com/file/d/1Vnfp6m-RW1X6yG2-1v3QhsrLppy0ywPS/"
+        alt="A beautiful treehouse in the forest"
+      />
+
+      <div className="button-container">
+        <button className="button">Adopt a Tree</button>
+        <button className="button">Become a Validator</button>
+      </div>
+    </div>
+  </div>
+);
