3b110086425ef5df576ac77ee62daae367670347
14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a <14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a>
2023-09-24 05:29:31 +0000

    Update gen_art by 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a at 2023-09-24T05:29:31.490Z

--- 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a/gen_art/source_code.jsx
+++ 14ea88d51603eb512dae55d259b805b74b58c6ade02df799706c3e12ab26c14a/gen_art/source_code.jsx
@@ -1,19 +1,16 @@
 const srcData = `
-
-<style>
-</style>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
 
- <script src="https://cdn.rawgit.com/generative-light/p5.scribble.js/master/p5.scribble.js"></script>
+<script src="https://cdn.rawgit.com/generative-light/p5.scribble.js/master/p5.scribble.js"></script>
 
 <script>
+let flag = true;
 function setup() {
+    console.log('hello world');
       createCanvas(1000, 1000); // 캔버스 크기 설정
       strokeWeight(1); // 선의 두께 설정
       background(240); // 배경색 설정
     }
-
 
 function draw() {
   // 랜덤한 색상 생성
@@ -26,13 +23,11 @@ function draw() {
   let y = random(height);
   let size = random(width/10);
   var scribble       = new Scribble();
-        scribble.bowing    = 0.1;
-
- // scribble.scribbleEllipse( x, y, size, size );
-let numPoints = 40; // 효과 선분의 개수
+  scribble.bowing    = 0.1;
+  let numPoints = 40; // 효과 선분의 개수
   let angleIncrement = TWO_PI / numPoints;
   let radius = size / 2 + 10; // 원의 반지름 + 여백
-
+    
   let xCoords = [];
   let yCoords = [];
 
@@ -44,13 +39,20 @@ let numPoints = 40; // 효과 선분의 개수
     yCoords.push(yPos);
   }
   var gap = 2.1;
-          // the angle of the hachure in degrees
-  let angle = random(90);
+  let angle = random(90)
+    if(!flag) {
   scribble.scribbleFilling( xCoords, yCoords, gap, angle );
   
-  
-}
+    }
+    if(flag) {
+       scribble.scribbleEllipse( x, y, size, size ); 
 
+    }
+}
+function mouseClicked() {
+  // 캔버스를 클릭할 때 실행할 코드를 여기에 작성
+  flag = !flag;
+}
 </script>
 `;
 
