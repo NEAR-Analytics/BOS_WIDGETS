9f0ee86f6f0cb8960648186907f8a270bb5a0765
azbang.near <azbang.near>
2023-09-24 08:15:10 +0000

    Update peer2peer by azbang.near at 2023-09-24T08:15:10.450Z

--- azbang.near/peer2peer/source_code.jsx
+++ azbang.near/peer2peer/source_code.jsx
@@ -1,5 +1,5 @@
 State.init({
-  amount: "0.01",
+  amount: "0.001",
 });
 
 const sender = Ethers.send("eth_requestAccounts", [])[0];
@@ -34,7 +34,6 @@ const Container = styled.div`
     }
 
     display: flex;
-    height: 600px;
     margin: 48px auto;
     justify-content: center;
     flex-direction: column;
@@ -67,12 +66,36 @@ const Info = styled.div`
 
 `;
 
-const CONTRACT = "0x99726c79719664f68f12274A4Ef432579891C0df";
+const Transaction = styled.div`
+  padding: 16px;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  cursor: pointer;
+  transition: 0.2s box-shadow;
+  p { margin: 0; }
+  margin-bottom: 12px;
+
+  &:hover {
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.1);
+  }
+`;
+
+const CONTRACT = "0xa3724030aA74016d1AE1e1B54bD79608F0E5866F";
 const tokenAddress = "0x8c9e6c40d3402480ACE624730524fACC5482798c";
 const erc20abi = ["function approve(address _spender, uint _value)"];
+
+const PaymentRequest =
+  "(address payable requester, uint256 amount, bytes32 zelleEmailHash, address payable executor)";
+
 const abi = [
-  "function createPaymentRequest(uint256 amt, bytes32 emailHash)",
-  "function getPaymentRequestsByIds(uint256[] memory requestIds) external view returns (PaymentRequest[] memory)",
+  "function createPaymentRequest(uint256 amount, bytes32 _zelleEmailHash) external returns (uint256)",
+  `function getPaymentRequestsByIds(uint256[] memory requestIds) external view returns (${PaymentRequest}[] memory)`,
+  "function reservePayment(uint256 requestId)",
+  "function confirmPayment(uint256 requestId, uint256 transactionId)",
+  "event PaymentReserved(uint256 requestId, address indexed reserver)",
 ];
 
 const signer = Ethers.provider().getSigner();
@@ -80,16 +103,30 @@ const erc20 = new ethers.Contract(tokenAddress, erc20abi, signer);
 const peer2peer = new ethers.Contract(CONTRACT, abi, signer);
 const daiDecimal = 18;
 
+peer2peer.getPaymentRequestsByIds(["1", "2", "3"]).then((data) => {
+  State.update({
+    orders: data.map((t) => ({
+      requester: t[0],
+      amount: ethers.utils.formatEther(t[1], daiDecimal),
+      zelleHash: data[2],
+      executor: data[3],
+    })),
+  });
+});
+
+console.log(state.orders);
+
 const handleCreate = () => {
   State.update({ loading: true });
   const emailHash = ethers.utils.sha256(ethers.utils.toUtf8Bytes(state.amount));
   const amount = ethers.utils.parseUnits(state.amount.toString(), daiDecimal);
 
-  erc20.approve(CONTRACT, 10).then((tx) => {
+  erc20.approve(CONTRACT, amount).then((tx) => {
     tx.wait().then(() => {
       console.log("createPaymentRequest");
-      peer2peer.createPaymentRequest(10, emailHash).then((tx) => {
-        tx.wait().then(() => {
+      peer2peer.createPaymentRequest(amount, emailHash).then((tx) => {
+        tx.wait().then((result) => {
+          console.log(tx, result);
           State.update({ loading: false });
         });
       });
@@ -97,21 +134,44 @@ const handleCreate = () => {
   });
 };
 
+const handleBuy = (order) => {
+  peer2peer.reservePayment(order).then((tx) => {
+    tx.wait().then(() => {});
+  });
+};
+
+const OrdersView = (
+  <div style={{ marginTop: 24 }}>
+    <h4>Buy DAI</h4>
+    {state.orders.map((order) => (
+      <Transaction onClick={() => handleBuy(order)}>
+        <div style={{ flex: 1 }}>
+          <p>
+            {order.requester.slice(0, 8)}...{order.requester.slice(-8)}
+          </p>
+          <p>{order.amount} DAI</p>
+        </div>
+        <Button style={{ height: 48, width: 100 }}>Buy</Button>
+      </Transaction>
+    ))}
+  </div>
+);
+
 return (
   <Container>
     <input
       value={state.email}
-      placeholder="Enter your Zelly email"
+      placeholder="Enter your Zelle email"
       onChange={(e) => State.update({ email: e.target.value })}
     />
 
     <Info>
-      Make sure that you indicate the email that is linked to your Zeally
+      Make sure that you indicate the email that is linked to your Zelle
       account, at this address validators verify the payment from the buyer
     </Info>
 
     <div style={{ display: "flex", gap: 12 }}>
-      {["0.01", "50", "100"].map((amount) => (
+      {["0.001", "50", "100"].map((amount) => (
         <AmountButton
           className={amount == state.amount && "active"}
           onClick={() => State.update({ amount })}
@@ -138,5 +198,7 @@ return (
         "Sell DAI"
       )}
     </Button>
+
+    {OrdersView}
   </Container>
 );
