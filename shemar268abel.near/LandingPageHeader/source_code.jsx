3f379225436b0121ec4681b949956389bbbec8e1
shemar268abel.near <shemar268abel.near>
2023-09-24 12:31:39 +0000

    Update LandingPageHeader by shemar268abel.near at 2023-09-24T12:31:39.623Z

---
+++ shemar268abel.near/LandingPageHeader/source_code.jsx
@@ -0,0 +1,53 @@
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 18px;
+  background-color: #f2f2f2;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+`;
+
+const Branding = styled.div`
+  display: flex;
+  align-items: baseline;
+  gap: 8px;
+  cursor: pointer;
+
+  svg {
+    width: auto;
+    height: 1em;
+    vertical-align: middle;
+  }
+`;
+
+const MainText = styled.span`
+  font-size: 2em;
+  font-weight: bold;
+  margin-right: 8px;
+`;
+
+const SubText = styled.span`
+  font-size: 0.8em;
+  color: grey;
+  margin-left: 4px;
+`;
+
+const Button = styled.button`
+  height: 100%;
+
+`;
+
+return (
+  <Header className="classic">
+    <Branding>
+      <MainText>The People's Place on Polygon zkEVM</MainText>
+    </Branding>
+    <ButtonRow>
+      <Button>Checkout</Button>
+    </ButtonRow>
+  </Header>
+);
