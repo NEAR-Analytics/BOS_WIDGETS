ce85912df70670725c17fd8be3a819b86ebb2882
6adbe3aca46fef824ed95c24b625c18fd7c6d978ff713ea11a22cd5b028940f2 <6adbe3aca46fef824ed95c24b625c18fd7c6d978ff713ea11a22cd5b028940f2>
2023-09-24 02:14:23 +0000

    Update KoreanHackmanGame by 6adbe3aca46fef824ed95c24b625c18fd7c6d978ff713ea11a22cd5b028940f2 at 2023-09-24T02:14:23.137Z

--- 6adbe3aca46fef824ed95c24b625c18fd7c6d978ff713ea11a22cd5b028940f2/KoreanHackmanGame/source_code.jsx
+++ 6adbe3aca46fef824ed95c24b625c18fd7c6d978ff713ea11a22cd5b028940f2/KoreanHackmanGame/source_code.jsx
@@ -6,24 +6,43 @@ const style = {
     textAlign: "center",
   },
   startButton: {
-    transform: "translate(-50%, -50%)",
+    transform: "translate(-50%)",
     position: "absolute",
     left: "50%",
-    top: "50vh",
+    top: "60vw",
   },
 };
 
 const developerAccountId =
   "6adbe3aca46fef824ed95c24b625c18fd7c6d978ff713ea11a22cd5b028940f2";
 
-const database = ["ㄱㅣㅁㅅㅔㅈㅓㅇ", "ㅅㅓㅎㅕㄴㅈㅣㄴ"];
-const randomIndex = Math.floor(Math.random() * database.length);
-const answer = database[randomIndex];
+function getAnswer() {
+  //
+  const defaultDb = ["ㄱㅣㅁㅅㅔㅈㅓㅇ", "ㅅㅓㅎㅕㄴㅈㅣㄴ"];
+  
+  const res = fetch(
+    "https://raw.githubusercontent.com/mydreamis-18/Ludium-2023-collegium-BOS-KoreanHackmanGame/main/%EB%B0%B0%EC%9A%B0%20%EC%9D%B4%EB%A6%84.csv"
+  );
 
+  const db = res.body.split(",") || defaultDb;
+
+  const randomIndex = Math.floor(Math.random() * db.length);
+
+  return db[randomIndex];
+}
+
+/**
+ * answer: db 배열에서 랜덤 인덱스로 뽑은 정답 단어
+ * subject: db 단어의 주제
+ * isStart: 게임 시작 여부 (버튼과 연동)
+ */
 State.init({
-  answer,
+  answer: getAnswer(),
+  subject: "배우 이름",
   isStart: false,
 });
+
+// console.log(state.answer);
 
 const startGame = () => {
   State.update({
@@ -50,7 +69,7 @@ return (
     {state.isStart && (
       <Widget
         src={`${developerAccountId}/widget/KoreanHackman`}
-        props={{ answer: state.answer }}
+        props={{ answer: state.answer, subject }}
       />
     )}
   </>
