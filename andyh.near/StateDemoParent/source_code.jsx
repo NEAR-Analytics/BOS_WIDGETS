347f824a65b90551d2c6f6c7631a6b00ddc0ef88
andyh.near <andyh.near>
2023-09-25 15:22:33 +0000

    Update StateDemoParent by andyh.near at 2023-09-25T15:22:33.888Z

---
+++ andyh.near/StateDemoParent/source_code.jsx
@@ -0,0 +1,10 @@
+State.init({ parent: 0, child: 0 });
+
+return (
+  <div>
+    <Widget
+      src="andyh.near/widget/Square"
+      props={{ radius: 50, color: "#4A825A", children: "y" }}
+    />
+  </div>
+);
