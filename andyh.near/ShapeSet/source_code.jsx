b815d98335c5dc36f39be8767c0ff43ec4f4e5a9
andyh.near <andyh.near>
2023-09-25 21:33:20 +0000

    Update ShapeSet by andyh.near at 2023-09-25T21:33:20.954Z

--- andyh.near/ShapeSet/source_code.jsx
+++ andyh.near/ShapeSet/source_code.jsx
@@ -2,6 +2,7 @@ return (
   <div className="row">
     <div className="col">
       <Widget
+        id="circle"
         src="andyh.near/widget/Circle"
         props={{
           color: "#C1200B",
@@ -14,6 +15,7 @@ return (
     </div>
     <div className="col">
       <Widget
+        id="square"
         src="andyh.near/widget/Square"
         props={{
           color: "#4A825A",
@@ -26,6 +28,7 @@ return (
     </div>
     <div className="col">
       <Widget
+        id="triangle"
         src="andyh.near/widget/Triangle"
         props={{
           color: "#0A81D1",
