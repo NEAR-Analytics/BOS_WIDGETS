f671841b05bdbf7d5dabbccf05b10f3aaf29e1d9
mob.near <mob.near>
2023-09-25 20:11:02 +0000

    Update Profile.N.OverlayTrigger by mob.near at 2023-09-25T20:11:02.656Z

--- mob.near/Profile.N.OverlayTrigger/source_code.jsx
+++ mob.near/Profile.N.OverlayTrigger/source_code.jsx
@@ -1,42 +1,15 @@
-const handleOnMouseEnter = () => {
-  State.update({ show: true });
-};
-const handleOnMouseLeave = () => {
-  State.update({ show: false });
-};
-
-State.init({
-  show: false,
-});
-
-const overlay = (
-  <div
-    className="border m-3 p-3 rounded-4 bg-white shadow"
-    style={{ maxWidth: "24em", zIndex: 1070 }}
-    onMouseEnter={handleOnMouseEnter}
-    onMouseLeave={handleOnMouseLeave}
-  >
-    <Widget
-      src="mob.near/widget/Profile.Popover"
-      props={{ accountId: props.accountId }}
-    />
-  </div>
-);
-
 return (
-  <OverlayTrigger
-    show={state.show}
-    trigger={["hover", "focus"]}
-    delay={{ show: 250, hide: 300 }}
-    placement="auto"
-    overlay={overlay}
-  >
-    <span
-      className="d-inline-flex mw-100"
-      onMouseEnter={handleOnMouseEnter}
-      onMouseLeave={handleOnMouseLeave}
-    >
-      {props.children}
-    </span>
-  </OverlayTrigger>
+  <Widget
+    loading={props.children}
+    src="mob.near/widget/N.Common.OverlayTrigger"
+    props={{
+      popup: (
+        <Widget
+          src="mob.near/widget/Profile.Popover"
+          props={{ accountId: props.accountId }}
+        />
+      ),
+      ...props,
+    }}
+  />
 );
