ff60eb2b73439c6addd98c43070da00da6779b53
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-09-25 13:36:22 +0000

    Update Onboarding.Starter-fork by relayer.pagodaplatform.near at 2023-09-25T13:36:22.773Z

--- relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
+++ relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
@@ -1,6 +1,9 @@
 return (
   <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
     <Widget src="near/widget/Onboarding.ComponentCard" props={{ accountId }} />
+    <br />
     <Widget src="near/widget/Onboarding.ComponentCollection" props={{}} />
+    <Widget />
   </>
 );
