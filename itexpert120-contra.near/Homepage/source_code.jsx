2c5c58b1cf6197bb188619ba8494f3d8afbd1f41
itexpert120-contra.near <itexpert120-contra.near>
2023-09-25 15:01:44 +0000

    Update Homepage by itexpert120-contra.near at 2023-09-25T15:01:44.092Z

--- itexpert120-contra.near/Homepage/source_code.jsx
+++ itexpert120-contra.near/Homepage/source_code.jsx
@@ -1,13 +1,5 @@
-const accountId = context.accountId;
-
-const homepage = Social.get(`${accountId}/settings/discover.bos/homepage`);
-
-if (homepage === null) {
-  return "loading...";
-}
-
-State.init({
-  homepage: homepage ?? "devs.near/widget/Home",
-});
-
-return <Widget src={state.homepage} />;
+return (
+  <div>
+    <h2>Sample Homepage</h2>
+  </div>
+);
