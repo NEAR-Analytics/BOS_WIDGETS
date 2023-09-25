90b191253c13449d51051c8323c1b8f15cc7ac24
andyh.near <andyh.near>
2023-09-25 18:04:32 +0000

    Update StateDemo by andyh.near at 2023-09-25T18:04:32.118Z

--- andyh.near/StateDemo/source_code.jsx
+++ andyh.near/StateDemo/source_code.jsx
@@ -5,6 +5,7 @@ return (
         id="trusted"
         src="andyh.near/widget/StateDemoRoot"
         isTrusted={true}
+        props={{ a: 1 }}
       />
     </div>
     <div className="col">
@@ -12,6 +13,7 @@ return (
         id="sandboxed"
         src="andyh.near/widget/StateDemoRoot"
         isTrusted={false}
+        props={{ b: 2 }}
       />
     </div>
   </div>
