98d3dc4610b204046317505d1365d6e3e061d106
astraplusplus.ndctools.near <astraplusplus.ndctools.near>
2023-09-26 14:08:08 +0000

    Update DAO.index by astraplusplus.ndctools.near at 2023-09-26T14:08:08.841Z

--- astraplusplus.ndctools.near/DAO.index/source_code.jsx
+++ astraplusplus.ndctools.near/DAO.index/source_code.jsx
@@ -1,7 +1,7 @@
 const widgetOwner = props.widgetOwner ?? "astraplusplus.ndctools.near";
 
 State.init({
-    tab: props.tab ?? "home",
+    tab: props.tab ?? "proposals",
     accountId: props.accountId ?? context.accountId,
     daoId: props.daoId,
     proposalId: props.proposalId
