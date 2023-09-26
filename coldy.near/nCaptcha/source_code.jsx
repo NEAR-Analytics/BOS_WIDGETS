01c51ee29760509ec2aa136e4c40d0660b195cb0
coldy.near <coldy.near>
2023-09-26 13:29:18 +0000

    Update nCaptcha by coldy.near at 2023-09-26T13:29:18.378Z

--- coldy.near/nCaptcha/source_code.jsx
+++ coldy.near/nCaptcha/source_code.jsx
@@ -1,6 +1,6 @@
 const contract = "v1.ncaptcha.near";
 
-const ratingMessage = Near.call(contract, "get_rating_for_account", {
+const ratingMessage = Near.view(contract, "get_rating_for_account", {
   account: "coldy.near",
 });
 
