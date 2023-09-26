f3acee7ab3c7fd692a116cf2266398e9b90000b6
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-09-26 18:16:10 +0000

    Update SayALot.Create by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-09-26T18:16:10.688Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.Create/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.Create/source_code.jsx
@@ -119,7 +119,7 @@ const SecondContainer = styled.div`
 `;
 
 return (
-  <div className="border rounded w-75">
+  <div className="border rounded mx-3 container-fluid">
     {
       //   {state.createdArticle && state.showCreatedArticle ? (
       //   <Widget
