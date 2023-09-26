cb45da67223cc94e682f9e7dbf143a0d67455bd1
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-09-26 16:08:21 +0000

    Update user-activity-feed by relayer.pagodaplatform.near at 2023-09-26T16:08:21.262Z

--- relayer.pagodaplatform.near/user-activity-feed/source_code.jsx
+++ relayer.pagodaplatform.near/user-activity-feed/source_code.jsx
@@ -1,140 +1,127 @@
-const data = fetch("https://api.coingecko.com/api/v3/coins/near", {
-  subscribe: true,
-  method: "GET",
-  headers: {
-    Accept: "*/*",
-  },
+const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql/`;
+
+State.init({
+  data: [],
 });
 
-if (data !== null && data.ok === false) {
-  return (
-    <div className="text-bg-light rounded-4 p-3 mb-3">
-      <p>
-        It seems like our pricing data provider, CoinGecko
-        (https://www.coingecko.com), is experiencing some issues at the moment.
-        You can keep an eye on their status here:{" "}
-        <a href="https://status.coingecko.com" target="_blank">
-          https://status.coingecko.com
-        </a>
-      </p>
-      <p>
-        Thanks for your patience, and we hope they'll resolve the issue soon.
-      </p>
-    </div>
-  );
-} else {
-  return (
-    <div className="text-bg-light rounded-4 p-3 mb-3">
-      {data !== null ? (
-        <p>
-          <div class="d-flex clearfix flex-wrap flex-column flex-sm-row">
-            <div class="p-2">
-              <div>NEAR/USD</div>
-              <h1>
-                ${data.body.market_data.current_price.usd}
-                <small
-                  class={
-                    data.body.market_data
-                      .price_change_percentage_24h_in_currency.usd < 0
-                      ? "text-danger"
-                      : "text-success"
-                  }
-                  style={{ "font-size": "12px" }}
-                >
-                  {data.body.market_data.price_change_percentage_24h_in_currency.usd.toFixed(
-                    2
-                  ) + "%"}
-                </small>
-              </h1>
-            </div>
-            <div class="p-2">
-              ATH
-              <p class="text-success">
-                ${data.body.market_data.ath.usd.toFixed(2)}
-              </p>
-            </div>
-            <div class="p-2">
-              24h high
-              <p>
-                {" "}
-                <b>${data.body.market_data.high_24h.usd.toFixed(2)} </b>
-              </p>
-            </div>
-            <div class="p-2">
-              24h low
-              <p>
-                {" "}
-                <b>${data.body.market_data.low_24h.usd.toFixed(2)} </b>
-              </p>
-            </div>
-            <div class="p-2">
-              Market Cap
-              <p>
-                <b>
-                  {"$" +
-                    data.body.market_data.market_cap.usd
-                      .toFixed(0)
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                </b>
-              </p>
-            </div>
-            <div class="p-2">
-              Volume
-              <p>
-                <b>
-                  {"$" +
-                    data.body.market_data.total_volume.usd
-                      .toFixed(0)
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                </b>
-              </p>
-            </div>
-            <div class="p-2">
-              Circulating Supply
-              <p>
-                <b>
-                  {"$" +
-                    data.body.market_data.circulating_supply
-                      .toFixed(0)
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
-                </b>
-              </p>
-            </div>
-          </div>
-          <div>
-            <small>
-              pricing information provided by{" "}
-              <a
-                target="_blank"
-                style={{ color: "inherit" }}
-                variant="caption"
-                rel="nofollow"
-                href="https://www.coingecko.com/en/api_terms"
-              >
-                CoinGecko
-              </a>
-            </small>
-            <small>|</small>
-            <small>
-              widget sponsored by{" "}
-              <a
-                target="_blank"
-                style={{ color: "inherit" }}
-                variant="caption"
-                rel="nofollow"
-                href="https://www.nearweek.com"
-              >
-                NEARWEEK
-              </a>
-            </small>
-          </div>
-        </p>
-      ) : (
-        <div>Loading ...</div>
-      )}
-    </div>
-  );
+const query = `query user_activity {
+    roshaan_near_user_activity_user_last_activity(order_by: {block_height: desc} limit: 50) {
+      activity_id
+      block_height
+      account_id
+      activity_type
+      block_timestamp
+    }
+  }`;
+
+function fetchGraphQL(operationsDoc, operationName, variables) {
+  return asyncFetch(QUERYAPI_ENDPOINT, {
+    method: "POST",
+    headers: { "x-hasura-role": `roshaan_near` },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
 }
+
+fetchGraphQL(query, "user_activity", {}).then((result) => {
+  if (result.status === 200) {
+    if (result.body.data) {
+      const data =
+        result.body.data.roshaan_near_user_activity_user_last_activity;
+      State.update({ data });
+    }
+  }
+});
+
+const Activity = styled.div`
+  position: relative;
+
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 19px;
+    top: 52px;
+    bottom: 12px;
+    width: 2px;
+
+    background: #eceef0;
+  }
+`;
+
+const Header = styled.div`
+  margin-bottom: 0;
+  display: inline-flex;
+`;
+
+const Body = styled.div`
+  padding-left: 52px;
+  padding-bottom: 1px;
+`;
+
+const Content = styled.div`
+  img {
+    display: block;
+    max-width: 100%;
+    max-height: 80vh;
+    margin: 0 0 12px;
+  }
+`;
+
+const Text = styled.p`
+  display: block;
+  margin: 0;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 400;
+  color: #687076;
+  white-space: nowrap;
+`;
+
+const renderData = (a) => {
+  const timeMs = parseFloat(a.block_timestamp) / 1e6;
+  const date = new Date(timeMs);
+
+  return (
+    <Activity style={{ margin: "10px" }}>
+      <Header>
+        <div className="row">
+          <div className="col-auto">
+            <Widget
+              src="near/widget/AccountProfile"
+              props={{
+                accountId: a.account_id,
+                hideAccountId: false,
+                inlineContent: (
+                  <>
+                    <Text as="span">･</Text>
+                    <Text className="col-auto">
+                      Last Online: {date.toDateString()}
+                    </Text>
+                    <>
+                      <Widget
+                        src="mob.near/widget/TimeAgo"
+                        props={{ blockHeight: a.block_height }}
+                      />{" "}
+                      ago
+                    </>
+                  </>
+                ),
+              }}
+            />
+          </div>
+        </div>
+      </Header>
+    </Activity>
+  );
+};
+
+const renderedData = state.data.map(renderData);
+return (
+  <div style={{ border: "0.5px solid black", borderRadius: "20px" }}>
+    {renderedData}
+  </div>
+);
