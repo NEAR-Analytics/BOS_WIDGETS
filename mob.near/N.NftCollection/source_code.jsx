a0422b6cf4b966ab21f855235cd71d6c251ac00c
mob.near <mob.near>
2023-09-26 22:37:07 +0000

    Update N.NftCollection by mob.near at 2023-09-26T22:37:07.960Z

--- mob.near/N.NftCollection/source_code.jsx
+++ mob.near/N.NftCollection/source_code.jsx
@@ -1,4 +1,4 @@
-const accountId = props.accountId || context.accountId;
+const accountId = props.accountId;
 const contractId = props.contractId;
 
 if (!contractId) {
@@ -22,33 +22,14 @@ if (!nfts) {
 return (
   <>
     {nfts.map((nft, i) => (
-      <a
-        key={i}
-        className="text-decoration-none"
-        href={`#mob.near/widget/NftImage?tokenId=${nft.token_id}&contractId=${contractId}`}
-      >
-        {nft.token_id}
-        <Widget
-          src="mob.near/widget/NftImage"
-          props={{
-            nft: { tokenId: nft.token_id, contractId },
-            style: {
-              width: "10em",
-              height: "10em",
-              objectFit: "cover",
-              minWidth: "10em",
-              minHeight: "10em",
-              maxWidth: "10em",
-              maxHeight: "10em",
-              overflowWrap: "break-word",
-            },
-            className: "img-thumbnail",
-            fallbackUrl:
-              "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
-            alt: `NFT ${contractId} ${nft.token_id}`,
-          }}
-        />
-      </a>
+      <Widget
+        key={nft.token_id}
+        loading=""
+        src="mob.near/widget/NFT"
+        props={{
+          nft: { tokenId: nft.token_id, contractId },
+        }}
+      />
     ))}
   </>
 );
