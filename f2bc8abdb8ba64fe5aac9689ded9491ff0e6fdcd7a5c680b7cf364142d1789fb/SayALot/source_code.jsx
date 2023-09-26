1ef618647b6fd58ce56d8d5cf5182d8b0cdc7637
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-09-26 17:59:19 +0000

    Update SayALot by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-09-26T17:59:19.307Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot/source_code.jsx
@@ -123,223 +123,207 @@ const navigationPills = [
 ];
 
 const navigationButtons = [
-  { id: tabs.ARTICLE_WORKSHOP.id, title: "+Create article" },
+  // { id: tabs.ARTICLE_WORKSHOP.id, title: "+Create article" },
 ];
 
-const initialBodyAtCreation =
-  state.editArticleData.body ??
-  `# h1 Heading 8-) 
-## h2 Heading 
-### h3 Heading 
-#### h4 Heading 
-##### h5 Heading 
-###### h6 Heading 
- 
- 
-## Horizontal Rules 
- 
-___ 
- 
---- 
- 
-*** 
- 
- 
-## Typographic replacements 
- 
-Enable typographer option to see result. 
- 
-(c) (C) (r) (R) (tm) (TM) (p) (P) +- 
- 
-test.. test... test..... test?..... test!.... 
- 
-!!!!!! ???? ,,  -- --- 
- 
-"Smartypants, double quotes" and 'single quotes' 
- 
- 
-## Emphasis 
- 
-This is bold text 
- 
-This is bold text 
- 
-*This is italic text* 
- 
-_This is italic text_ 
- 
-Strikethrough 
- 
- 
-## Blockquotes 
- 
- 
-> Blockquotes can also be nested... 
->> ...by using additional greater-than signs right next to each other... 
-> > > ...or with spaces between arrows. 
- 
- 
-## Lists 
- 
-Unordered 
- 
-+ Create a list by starting a line with \`+\`, \`-\`, or \`*\` 
-+ Sub-lists are made by indenting 2 spaces: 
-  - Marker character change forces new list start: 
-    * Ac tristique libero volutpat at 
-    + Facilisis in pretium nisl aliquet 
-    - Nulla volutpat aliquam velit 
-+ Very easy! 
- 
-Ordered 
- 
-1. Lorem ipsum dolor sit amet 
-2. Consectetur adipiscing elit 
-3. Integer molestie lorem at massa 
- 
- 
-1. You can use sequential numbers... 
-1. ...or keep all the numbers as \`1.\` 
- 
-Start numbering with offset: 
- 
-57. foo 
-1. bar 
- 
- 
-## Code 
- 
-Inline \`code\` 
- 
-Indented code 
- 
-    // Some comments 
-    line 1 of code 
-    line 2 of code 
-    line 3 of code 
- 
- 
-Block code "fences" 
- 
-\`\`\` 
-Sample text here... 
-\`\`\` 
- 
-Syntax highlighting 
- 
-\`\`\` js 
-var foo = function (bar) { 
-  return bar++; 
-}; 
- 
-console.log(foo(5)); 
-\`\`\` 
- 
-## Tables 
- 
-| Option | Description | 
-| ------ | ----------- | 
-| data   | path to data files to supply the data that will be passed into templates. | 
-| engine | engine to be used for processing templates. Handlebars is the default. | 
-| ext    | extension to be used for dest files. | 
- 
-Right aligned columns 
- 
-| Option | Description | 
-| ------:| -----------:| 
-| data   | path to data files to supply the data that will be passed into templates. | 
-| engine | engine to be used for processing templates. Handlebars is the default. | 
-| ext    | extension to be used for dest files. | 
- 
- 
-## Links 
- 
-link text 
- 
-link with title 
- 
-Autoconverted link https://github.com/nodeca/pica (enable linkify to see) 
- 
- 
-## Images 
- 
-!Minion 
-!Stormtroopocat 
- 
-Like links, Images also have a footnote style syntax 
- 
-![Alt text][id] 
- 
-With a reference later in the document defining the URL location: 
- 
-[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat" 
- 
-### Emojies 
- 
-> Classic markup: :wink: :crush: :cry: :tear: :laughing: :yum: 
-> 
-> Shortcuts (emoticons): :-) :-( 8-) ;) 
- 
-see how to change output with twemoji. 
- 
- 
-### Subscript / Superscript 
- 
-- 19^th^ 
-- H~2~O 
- 
- 
-### \<ins> 
- 
-++Inserted text++ 
- 
- 
-### \<mark> 
- 
-==Marked text== 
- 
- 
-### Footnotes 
- 
-Footnote 1 link[^first]. 
- 
-Footnote 2 link[^second]. 
- 
-Inline footnote^[Text of inline footnote] definition. 
- 
-Duplicated footnote reference[^second]. 
- 
-[^first]: Footnote can have markup 
- 
-    and multiple paragraphs. 
- 
-[^second]: Footnote text. 
- 
- 
-### [Definition lists](https://github.com/markdown-it/markdown-it-deflist) 
- 
-Term 1 
- 
-:   Definition 1 
-with lazy continuation. 
- 
-Term 2 with *inline markup* 
- 
-:   Definition 2 
- 
-        { some code, part of Definition 2 } 
- 
-    Third paragraph of definition 2. 
- 
-_Compact style:_ 
- 
-Term 1 
-  ~ Definition 1 
- 
-Term 2 
-  ~ Definition 2a 
-  ~ Definition 2b 
-`;
+const initialBodyAtCreation = state.editArticleData.body;
+//    ??
+//   `# h1 Heading 8-)
+// ## h2 Heading
+// ### h3 Heading
+// #### h4 Heading
+// ##### h5 Heading
+// ###### h6 Heading
+
+// ## Horizontal Rules
+
+// ___
+
+// ---
+
+// ***
+
+// ## Typographic replacements
+
+// Enable typographer option to see result.
+
+// (c) (C) (r) (R) (tm) (TM) (p) (P) +-
+
+// test.. test... test..... test?..... test!....
+
+// !!!!!! ???? ,,  -- ---
+
+// "Smartypants, double quotes" and 'single quotes'
+
+// ## Emphasis
+
+// This is bold text
+
+// This is bold text
+
+// *This is italic text*
+
+// _This is italic text_
+
+// Strikethrough
+
+// ## Blockquotes
+
+// > Blockquotes can also be nested...
+// >> ...by using additional greater-than signs right next to each other...
+// > > > ...or with spaces between arrows.
+
+// ## Lists
+
+// Unordered
+
+// + Create a list by starting a line with \`+\`, \`-\`, or \`*\`
+// + Sub-lists are made by indenting 2 spaces:
+//   - Marker character change forces new list start:
+//     * Ac tristique libero volutpat at
+//     + Facilisis in pretium nisl aliquet
+//     - Nulla volutpat aliquam velit
+// + Very easy!
+
+// Ordered
+
+// 1. Lorem ipsum dolor sit amet
+// 2. Consectetur adipiscing elit
+// 3. Integer molestie lorem at massa
+
+// 1. You can use sequential numbers...
+// 1. ...or keep all the numbers as \`1.\`
+
+// Start numbering with offset:
+
+// 57. foo
+// 1. bar
+
+// ## Code
+
+// Inline \`code\`
+
+// Indented code
+
+//     // Some comments
+//     line 1 of code
+//     line 2 of code
+//     line 3 of code
+
+// Block code "fences"
+
+// \`\`\`
+// Sample text here...
+// \`\`\`
+
+// Syntax highlighting
+
+// \`\`\` js
+// var foo = function (bar) {
+//   return bar++;
+// };
+
+// console.log(foo(5));
+// \`\`\`
+
+// ## Tables
+
+// | Option | Description |
+// | ------ | ----------- |
+// | data   | path to data files to supply the data that will be passed into templates. |
+// | engine | engine to be used for processing templates. Handlebars is the default. |
+// | ext    | extension to be used for dest files. |
+
+// Right aligned columns
+
+// | Option | Description |
+// | ------:| -----------:|
+// | data   | path to data files to supply the data that will be passed into templates. |
+// | engine | engine to be used for processing templates. Handlebars is the default. |
+// | ext    | extension to be used for dest files. |
+
+// ## Links
+
+// link text
+
+// link with title
+
+// Autoconverted link https://github.com/nodeca/pica (enable linkify to see)
+
+// ## Images
+
+// !Minion
+// !Stormtroopocat
+
+// Like links, Images also have a footnote style syntax
+
+// ![Alt text][id]
+
+// With a reference later in the document defining the URL location:
+
+// [id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
+
+// ### Emojies
+
+// > Classic markup: :wink: :crush: :cry: :tear: :laughing: :yum:
+// >
+// > Shortcuts (emoticons): :-) :-( 8-) ;)
+
+// see how to change output with twemoji.
+
+// ### Subscript / Superscript
+
+// - 19^th^
+// - H~2~O
+
+// ### \<ins>
+
+// ++Inserted text++
+
+// ### \<mark>
+
+// ==Marked text==
+
+// ### Footnotes
+
+// Footnote 1 link[^first].
+
+// Footnote 2 link[^second].
+
+// Inline footnote^[Text of inline footnote] definition.
+
+// Duplicated footnote reference[^second].
+
+// [^first]: Footnote can have markup
+
+//     and multiple paragraphs.
+
+// [^second]: Footnote text.
+
+// ### [Definition lists](https://github.com/markdown-it/markdown-it-deflist)
+
+// Term 1
+
+// :   Definition 1
+// with lazy continuation.
+
+// Term 2 with *inline markup*
+
+// :   Definition 2
+
+//         { some code, part of Definition 2 }
+
+//     Third paragraph of definition 2.
+
+// _Compact style:_
+
+// Term 1
+//   ~ Definition 1
+
+// Term 2
+//   ~ Definition 2a
+//   ~ Definition 2b
+// `;
 
 //=================================================END CONSTS=======================================================
 
@@ -486,6 +470,11 @@ return (
           handleOpenArticle,
           handleFilterArticles,
           authorForWidget,
+          initialCreateState,
+          editArticleData: state.editArticleData,
+          callLibs,
+          handleFilterArticles,
+          handleEditArticle,
         }}
       />
     )}
@@ -518,24 +507,24 @@ return (
       />
     )}
 
-    {state.displayedTabId == tabs.ARTICLE_WORKSHOP.id && (
-      <Widget
-        src={widgets.create}
-        props={{
-          isTest,
-          addressForArticles,
-          authorForWidget,
-          stateUpdate,
-          widgets,
-          initialBody: initialBodyAtCreation,
-          initialCreateState,
-          editArticleData: state.editArticleData,
-          callLibs,
-          handleFilterArticles,
-          handleEditArticle,
-        }}
-      />
-    )}
+    // {state.displayedTabId == tabs.ARTICLE_WORKSHOP.id && (
+    //   <Widget
+    //     src={widgets.create}
+    //     props={{
+    //       isTest,
+    //       addressForArticles,
+    //       authorForWidget,
+    //       stateUpdate,
+    //       widgets,
+    //       initialBody: initialBodyAtCreation,
+    //       initialCreateState,
+    //       editArticleData: state.editArticleData,
+    //       callLibs,
+    //       handleFilterArticles,
+    //       handleEditArticle,
+    //     }}
+    //   />
+    // )}
     <CallLibrary>
       {callLibs(libSrcArray, stateUpdate, state.libCalls)}
     </CallLibrary>
