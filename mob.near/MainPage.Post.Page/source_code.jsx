67c88aa0a4ca7541c2f314dac2ab209eb9198b68
mob.near <mob.near>
2023-09-26 16:35:12 +0000

    Update MainPage.Post.Page by mob.near at 2023-09-26T16:35:12.615Z

--- mob.near/MainPage.Post.Page/source_code.jsx
+++ mob.near/MainPage.Post.Page/source_code.jsx
@@ -1,6 +1,12 @@
 return (
   <Widget
-    src="mob.near/widget/MainPage.Post"
-    props={{ ...props, commentsLimit: 30, subscribe: true }}
+    src="mob.near/widget/MainPage.N.Post"
+    props={{
+      ...props,
+      noBorder: true,
+      commentsLimit: 30,
+      subscribe: true,
+      truncateContent: false,
+    }}
   />
 );
