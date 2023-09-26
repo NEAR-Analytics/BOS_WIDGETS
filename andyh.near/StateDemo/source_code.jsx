8e47fdb8d6e5c18c03e39d76cccfe61d2d0009e8
andyh.near <andyh.near>
2023-09-26 18:08:41 +0000

    Update StateDemo by andyh.near at 2023-09-26T18:08:41.914Z

--- andyh.near/StateDemo/source_code.jsx
+++ andyh.near/StateDemo/source_code.jsx
@@ -1,20 +1,29 @@
 return (
-  <div className="row">
-    <div className="col">
-      <Widget
-        id="trusted"
-        src="andyh.near/widget/StateDemoRoot"
-        isTrusted={true}
-        props={{ title: "State across Trusted Components" }}
-      />
+  <>
+    <div className="row">
+      <p>
+        Click on any shape to trigger a state change in the root Component.
+        These state changes will then propagate down to its descendants as
+        props.
+      </p>
     </div>
-    <div className="col">
-      <Widget
-        id="sandboxed"
-        src="andyh.near/widget/StateDemoRoot"
-        isTrusted={false}
-        props={{ title: "State within Sandboxed Components" }}
-      />
+    <div className="row">
+      <div className="col">
+        <Widget
+          id="trusted"
+          src="andyh.near/widget/StateDemoRoot"
+          isTrusted={true}
+          props={{ title: "State across Trusted Components" }}
+        />
+      </div>
+      <div className="col">
+        <Widget
+          id="sandboxed"
+          src="andyh.near/widget/StateDemoRoot"
+          isTrusted={false}
+          props={{ title: "State within Sandboxed Components" }}
+        />
+      </div>
     </div>
-  </div>
+  </>
 );
