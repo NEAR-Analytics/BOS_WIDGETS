98cb410bd1c6b659176201fec170586956459235
cpassport.near <cpassport.near>
2023-09-27 06:18:35 +0000

    Update Passport.Home.Main by cpassport.near at 2023-09-27T06:18:35.881Z

--- cpassport.near/Passport.Home.Main/source_code.jsx
+++ cpassport.near/Passport.Home.Main/source_code.jsx
@@ -1,17 +1,36 @@
-/**ToDo
- * Add navigation and add font
- */
+const DivBackground = styled.div`
+  background-color: #FDF3DD;
+  height: 100vh;
+  display: flex;
+  padding-bottom:20vh;
+  align-items:center;
+  justify-content:center;
+`;
+
+const GridView = styled.div`
+  display: grid;
+  width:100%;
+  grid-template-columns: 40% 60%;
+`;
+
 return (
-  <>
-    <h1>AROUND THE WORLD</h1>
-    <p className="instructions">
-      <ul>
-        <li>Visit with Artists</li>
-        <li>Scan the QR Codes</li>
-        <li>Collect Stamps</li>
-        <li>Unlock Prizes</li>
-      </ul>
-    </p>
-    <div>Add navigation to passport here</div>
-  </>
+  <DivBackground>
+    <div>
+      <h1>AROUND THE WORLD</h1>
+      <GridView>
+        <div>
+          <div style={{textAlign:"center"}}>
+            <>
+              <p style={{ fontSize: 20 }}>Visit with Artists</p>
+              <p style={{ fontSize: 20 }}>Scan the QR Codes</p>
+              <p style={{ fontSize: 20 }}>Collect Stamps</p>
+              <p style={{ fontSize: 20 }}>Unlock Prizes</p>
+            </>
+          </div>
+          <div>Add navigation to passport here</div>
+        </div>
+        <div></div>
+      </GridView>
+    </div>
+  </DivBackground>
 );
