ab7380ea9314716ed55a1717f1404ac4469c3ed1
sharddog.near <sharddog.near>
2023-09-27 14:08:02 +0000

    Update Image.Minted by sharddog.near at 2023-09-27T14:08:02.868Z

--- sharddog.near/Image.Minted/source_code.jsx
+++ sharddog.near/Image.Minted/source_code.jsx
@@ -35,7 +35,7 @@ function fetchContentType(url) {
     }
 
     // Get the final URL after redirection
-    const finalUrl = response.location;
+    const finalUrl = response.headers.location;
     console.log("final URL: " + finalUrl);
     // Fetch the content type from the final URL
     response = asyncFetch(finalUrl, { method: "HEAD" });
