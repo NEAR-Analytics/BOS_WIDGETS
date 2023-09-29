d32bcd4fd8ec898eb9a4e8eb6ea08e97d5ce6a78
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-09-29 15:05:49 +0000

    Update AICharacterWithAttributesChatBot by relayer.pagodaplatform.near at 2023-09-29T15:05:49.135Z

--- relayer.pagodaplatform.near/AICharacterWithAttributesChatBot/source_code.jsx
+++ relayer.pagodaplatform.near/AICharacterWithAttributesChatBot/source_code.jsx
@@ -111,7 +111,7 @@ const fetchResponseFromChatGPT = () => {
       conversationHistoryStr: conversationHistoryToString(),
     });
   });
-  Storage.set(props.NEAR_ACC, JSON.stringify(state));
+  Storage.privateSet(props.NEAR_ACC, JSON.stringify(state));
 };
 
 return (
