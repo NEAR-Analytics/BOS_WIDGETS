a1660fed3d38e17440095a24b82d800ef46932e8
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-09-29 12:57:26 +0000

    Update AICharacterWithAttributesChatBot by relayer.pagodaplatform.near at 2023-09-29T12:57:26.313Z

--- relayer.pagodaplatform.near/AICharacterWithAttributesChatBot/source_code.jsx
+++ relayer.pagodaplatform.near/AICharacterWithAttributesChatBot/source_code.jsx
@@ -105,9 +105,9 @@ const fetchResponseFromChatGPT = () => {
 };
 
 return (
-  <div className="border border-2 border-black m-2">
+  <div className="border border-2 border-black m-2 w-100">
     <div style={{ height: "500px" }}>
-      <div className="overflow-auto h-100 w-100 mb-3 border border-2 border-dark m-1">
+      <div className="overflow-auto h-100 mb-3 border border-2 border-dark m-1">
         <div>
           {state.conversationHistory.map((conversation) => (
             <div className="p-2">
