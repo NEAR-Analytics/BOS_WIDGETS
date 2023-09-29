bf63f81cc37fa0cbbcf827a11d5d12f95ccd9c3b
mob.near <mob.near>
2023-09-29 19:04:38 +0000

    Update ProfileLarge by mob.near at 2023-09-29T19:04:38.690Z

--- mob.near/ProfileLarge/source_code.jsx
+++ mob.near/ProfileLarge/source_code.jsx
@@ -34,6 +34,21 @@ const Wrapper = styled.div`
   margin: 0 -12px; 
 `;
 
+const shareSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    stroke="currentColor"
+    strokeWidth="0.363"
+  >
+    <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+    <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
+  </svg>
+);
+
 return (
   <Wrapper>
     <div className="px-4 pt-0 pb-5 bg-dark position-relative">
@@ -146,7 +161,7 @@ return (
             props={{
               text: link,
               label: "Share",
-              clipboardIcon: <i className="bi bi-share" />,
+              clipboardIcon: shareSvg,
             }}
           />
         </div>
