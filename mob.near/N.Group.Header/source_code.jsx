81d39e23e46f7bb8f8660174e105308c47903756
mob.near <mob.near>
2023-09-29 19:00:34 +0000

    Update N.Group.Header by mob.near at 2023-09-29T19:00:34.157Z

--- mob.near/N.Group.Header/source_code.jsx
+++ mob.near/N.Group.Header/source_code.jsx
@@ -1,5 +1,5 @@
 const groupId = props.groupId;
-if (!accountId) {
+if (!groupId) {
   return "No groupId";
 }
 
@@ -10,15 +10,16 @@ const link =
     : props.link);
 
 const nftMetadata = props.nftMetadata ?? Near.view(groupId, "nft_metadata");
+const owners = props.owners;
 
 if (nftMetadata === null) {
   return "Loading";
 }
 
 const name = nftMetadata.name || "Unnamed group";
-const image = profile.image;
-const backgroundImage = profile.backgroundImage;
-const tags = Object.keys(profile.tags ?? {});
+const imageUrl = nftMetadata.icon;
+// const backgroundImage = nftMetadata.backgroundImage;
+// const tags = Object.keys(nftMetadata.tags ?? {});
 
 const nameHeader = <h4 className="mt-0 mb-0 text-truncate">{name}</h4>;
 
@@ -26,6 +27,21 @@ const Wrapper = styled.div`
   overflow: hidden;
   margin: 0 -12px; 
 `;
+
+const shareSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    stroke="currentColor"
+    strokeWidth="0.363"
+  >
+    <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+    <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
+  </svg>
+);
 
 return (
   <Wrapper>
@@ -43,31 +59,17 @@ return (
           }}
         />
       )}
-      {showEditButton && (
-        <a
-          href="#/mob.near/widget/ProfileEditor"
-          className="btn mt-4 btn-outline-light float-end position-relative"
-          style={{ zIndex: 1 }}
-        >
-          Edit profile
-        </a>
-      )}
       <div
         className="profile-picture d-inline-block"
         style={{ transform: "translateY(7rem)" }}
       >
-        <Widget
-          src="mob.near/widget/ProfileImage"
-          props={{
-            profile,
-            fast,
-            accountId,
-            style: { width: "10rem", height: "10rem" },
-            className: "mb-2",
-            imageClassName: "rounded-circle w-100 h-100 img-thumbnail d-block",
-            thumbnail: false,
-          }}
-        />
+        <div className="mb-2" style={{ width: "10rem", height: "10rem" }}>
+          <img
+            src={imageUrl}
+            className="rounded-circle w-100 h-100 img-thumbnail d-block"
+            style={{ objectFit: "cover" }}
+          />
+        </div>
       </div>
     </div>
     <div className="px-4 pb-4">
@@ -83,43 +85,57 @@ return (
                 nameHeader
               )}
               <div className="small text-truncate">
-                <i className="bi bi-person-fill text-secondary"></i>
-                {accountId}
+                <i className="bi bi-buildings-fill text-secondary"></i>
+                {groupId}
                 <Widget
                   src="mob.near/widget/CopyButton"
                   props={{
-                    text: accountId,
+                    text: groupId,
                     className: "btn btn-sm btn-outline-dark border-0",
                   }}
                 />
-                <Widget
-                  src="mob.near/widget/FollowsYouBadge"
-                  props={{ accountId }}
-                />
               </div>
-            </div>
-
-            <div>
-              <Widget
-                src="mob.near/widget/FollowButton"
-                props={{ accountId }}
-              />
-              <Widget src="mob.near/widget/PokeButton" props={{ accountId }} />
             </div>
           </div>
           <div>
-            <Widget src="mob.near/widget/FollowStats" props={{ accountId }} />
+            <div className="d-flex flex-row">
+              <div className="me-4">
+                <a
+                  href={`#/mob.near/widget/FollowPage?accountId=${accountId}&tab=following`}
+                  className="text-dark"
+                >
+                  {owners && owners.length > 0 ? (
+                    <span className="fw-bolder">{owners.length}</span>
+                  ) : (
+                    "?"
+                  )}{" "}
+                  <span className="text-muted">Members</span>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
+        {/*
         <div style={{ minWidth: "12rem" }}>
           <Widget
             src="mob.near/widget/LinkTree"
             props={{ linktree: profile.linktree }}
           />
+        </div>*/}
+        <div className="float-end">
+          <Widget
+            src="mob.near/widget/CopyButton"
+            props={{
+              text: link,
+              label: "Share",
+              clipboardIcon: shareSvg,
+            }}
+          />
         </div>
       </div>
 
-      {tags.length > 0 && (
+      {/*
+      tags.length > 0 && (
         <div>
           {tags.map((tag, i) => (
             <span
@@ -130,34 +146,8 @@ return (
             </span>
           ))}
         </div>
-      )}
-
-      <div>
-        <div className="float-end">
-          <Widget
-            src="mob.near/widget/CopyButton"
-            props={{
-              text: link,
-              label: "Share",
-              clipboardIcon: <i className="bi bi-share" />,
-            }}
-          />
-        </div>
-        <div className="public-tags collapse show">
-          <button
-            className="btn btn-sm btn-outline-secondary border-0"
-            data-bs-toggle="collapse"
-            data-bs-target={`.public-tags`}
-            aria-expanded="false"
-            aria-controls={"public-tags"}
-          >
-            <i className="bi bi-arrows-angle-expand me-1"></i>Show public tags
-          </button>
-        </div>
-        <div className="collapse public-tags">
-          <Widget src="mob.near/widget/PublicTags" props={{ accountId }} />
-        </div>
-      </div>
+      )
+      */}
     </div>
   </Wrapper>
 );
