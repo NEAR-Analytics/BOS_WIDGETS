db63fef600769c2ab86b68bfa7731176f2b68ba8
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-09-29 15:29:17 +0000

    Update SayALot.lib.SBT by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-09-29T15:29:17.934Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.lib.SBT/source_code.jsx
@@ -0,0 +1,38 @@
+const { isTest, stateUpdate, libCalls } = props;
+
+function isValidUser(accountId, sbtName) {
+  const userSBTs = Near.view(
+    "registry.i-am-human.near",
+    "sbt_tokens_by_owner",
+    {
+      account: accountId,
+    }
+  );
+
+  return (
+    userSBTs.find((sbt) => {
+      return sbt[0] === sbtName;
+    }) !== undefined
+  );
+}
+
+function libCall(call) {
+  if (call.functionName === "isValidUser") {
+    return isValidUser(call.props);
+  }
+}
+
+let resultLibCalls = [];
+
+if (libCalls && libCalls.length > 0) {
+  const updateObj = {};
+  resultLibCalls = [...libCalls];
+  libCalls.forEach((call) => {
+    updateObj[call.key] = libCall(call);
+  });
+
+  updateObj.libCalls = resultLibCalls;
+  stateUpdate(updateObj);
+}
+
+return <></>;
