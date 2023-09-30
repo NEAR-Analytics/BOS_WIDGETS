e23c4e6673799f93f54050415199f1720c267324
kurodenjiro.near <kurodenjiro.near>
2023-09-30 03:27:15 +0000

    Update abi2form-builder by kurodenjiro.near at 2023-09-30T03:27:15.907Z

---
+++ kurodenjiro.near/abi2form-builder/source_code.jsx
@@ -0,0 +1,6 @@
+State.init({
+  contractAddress: "",
+  rpcUrl: "https://rpc.near.org/", //https://rpc.testnet.near.org
+  archivalRpc: "https://archival-rpc.mainnet.near.org", //https://archival-rpc.testnet.near.org
+  nearBlockRpc: "https://api.nearblocks.io/", // https://api-testnet.nearblocks.io
+
