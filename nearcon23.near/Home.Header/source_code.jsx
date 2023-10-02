267192bcc5c9106e200b0a1e9f147f4ebe2bc724
nearcon23.near <nearcon23.near>
2023-10-02 04:04:08 +0000

    Update Home.Header by nearcon23.near at 2023-10-02T04:04:08.829Z

--- nearcon23.near/Home.Header/source_code.jsx
+++ nearcon23.near/Home.Header/source_code.jsx
@@ -1,3 +1,12 @@
-const desktopImage = "https://nearcon.s3.amazonaws.com/home-hero.png";
+const desktopImage =
+  "https://ipfs.near.social/ipfs/bafkreiga7r5nutmlf66qkfvgkzbq2orbqtlmvgshq47355um2ttqybt4pe";
+const mobileImage =
+  "https://ipfs.near.social/ipfs/bafkreiddafldobfu4tcqmeyhxgxdkwp7mhfb5keottpe6lir67j6zgout4";
 
-return <img src={desktopImage} style={{ width: "100%" }} />;
+const MyImage = styled.img`
+    @media (max-width: 650px) {    
+         content:url(${mobileImage});   
+    }
+`;
+
+return <MyImage src={desktopImage} style={{ width: "100%" }} />;
