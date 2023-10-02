f4fd89c05b711ee2902e72d810f59727e242c8ce
nearcon23.near <nearcon23.near>
2023-10-02 06:37:55 +0000

    Update Ticket.Page by nearcon23.near at 2023-10-02T06:37:55.018Z

---
+++ nearcon23.near/Ticket.Page/source_code.jsx
@@ -0,0 +1,8 @@
+const ownerId = "nearcon23.near";
+
+return (
+  <>
+    <Widget src={`${ownerId}/widget/Ticket.Header`} />
+    <Widget src={`${ownerId}/widget/Ticket.Content`} props={props} />
+  </>
+);
