be00bc791355fe303cd8241dce7af4deef70e23e
mob.near <mob.near>
2023-10-02 22:04:28 +0000

    Update Profile.InlineBlock by mob.near at 2023-10-02T22:04:28.342Z

--- mob.near/Profile.InlineBlock/source_code.jsx
+++ mob.near/Profile.InlineBlock/source_code.jsx
@@ -1,26 +1,38 @@
 const accountId = props.accountId ?? context.accountId;
 
 const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+const fast = props.fast ?? !props.profile;
 
 const name = profile.name;
 const description = profile.description;
 const tags = Object.keys(profile.tags ?? {});
 
+const imgWrapperStyle = { height: "3em", width: "3em" };
+
 return (
   <div className="d-flex flex-row">
-    <Widget
-      src="mob.near/widget/ProfileImage"
-      props={{
-        metadata,
-        accountId,
-        widgetName,
-        style: { height: "3em", width: "3em", minWidth: "3em" },
-        className: "me-2",
-      }}
-    />
+    <div className="me-2">
+      <Widget
+        src="mob.near/widget/ProfileImage"
+        loading={<div style={imgWrapperStyle} />}
+        props={{
+          fast,
+          profile,
+          accountId,
+          widgetName,
+          style: imgWrapperStyle,
+          imageClassName: "rounded-circle w-100 h-100",
+        }}
+      />
+    </div>
     <div className="text-truncate">
       <div className="text-truncate">
         <span className="fw-bold">{name}</span>{" "}
+        <Widget
+          loading=""
+          src="mob.near/widget/Checkmark"
+          props={{ accountId }}
+        />
         <small>
           <span className="font-monospace">@{accountId}</span>
         </small>
