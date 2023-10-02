aa34c828bdc4bc0b16bc26735ebbb34d0983ecaf
nearcon23.near <nearcon23.near>
2023-10-02 04:59:48 +0000

    Update Register.Header by nearcon23.near at 2023-10-02T04:59:48.238Z

--- nearcon23.near/Register.Header/source_code.jsx
+++ nearcon23.near/Register.Header/source_code.jsx
@@ -1,3 +1,12 @@
-const desktopImage = "https://nearcon.s3.amazonaws.com/register-hero.png";
+const desktopImage =
+  "https://ipfs.near.social/ipfs/bafkreicjciucingvja4hli3wx4pla7rh5dhyxgirdsjbceaz3cs63orezi";
+const mobileImage =
+  "https://ipfs.near.social/ipfs/bafkreig3kjljw3o5dld3yteq7hnoniglmzvhac7r5ezpbdfetwcmc3gyde";
 
-return <img src={desktopImage} style={{ width: "100%" }} />;
+const Header = styled.img`
+    @media (max-width: 650px) {    
+         content:url(${mobileImage});   
+    }
+`;
+
+return <Header src={desktopImage} style={{ width: "100%" }} />;
