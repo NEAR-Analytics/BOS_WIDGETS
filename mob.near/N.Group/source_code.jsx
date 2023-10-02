4596241ec6d4fc9137d4b7b3e4dabd5517818ce7
mob.near <mob.near>
2023-10-02 19:09:32 +0000

    Update N.Group by mob.near at 2023-10-02T19:09:32.774Z

--- mob.near/N.Group/source_code.jsx
+++ mob.near/N.Group/source_code.jsx
@@ -67,7 +67,6 @@ return (
         groupId,
         commentAccounts: owners,
         permissions: {
-          disableRepost: !isMember,
           disableComment: !isMember,
         },
       }}
