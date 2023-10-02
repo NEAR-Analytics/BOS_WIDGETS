47d77447f27c2889b7596fcde2aeb03ee01ea839
mob.near <mob.near>
2023-10-02 18:15:22 +0000

    Update MainPage.N.Post.Left by mob.near at 2023-10-02T18:15:22.759Z

--- mob.near/MainPage.N.Post.Left/source_code.jsx
+++ mob.near/MainPage.N.Post.Left/source_code.jsx
@@ -1,19 +1,47 @@
 const accountId = props.accountId;
+const groupId = props.groupId;
 
 const imgWrapperStyle = { width: "40px", height: "40px" };
 
+const GroupLink = styled.a`
+  margin-top: -10px;
+  display: block;
+  div > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 8px;
+  }
+`;
+
 return (
-  <a href={`/mob.near/widget/ProfilePage?accountId=${accountId}`}>
-    <Widget
-      src="mob.near/widget/ProfileImage"
-      loading={<div style={imgWrapperStyle} />}
-      props={{
-        accountId,
-        tooltip: true,
-        link: true,
-        style: imgWrapperStyle,
-        imageClassName: "rounded-circle w-100 h-100",
-      }}
-    />
-  </a>
+  <>
+    <a
+      className="d-block position-relative"
+      style={{ zIndex: 1 }}
+      href={`/mob.near/widget/ProfilePage?accountId=${accountId}`}
+    >
+      <Widget
+        src="mob.near/widget/ProfileImage"
+        loading={<div style={imgWrapperStyle} />}
+        props={{
+          accountId,
+          tooltip: true,
+          link: true,
+          style: imgWrapperStyle,
+          imageClassName: "rounded-circle w-100 h-100",
+        }}
+      />
+    </a>
+    {groupId && (
+      <GroupLink href={`?groupId=${groupId}`}>
+        <div style={imgWrapperStyle}>
+          <img
+            src={`https://i.near.social/magic/thumbnail/https://near.social/magic/img/nft/${groupId}`}
+            alt={`Group icon: ${groupId}`}
+          />
+        </div>
+      </GroupLink>
+    )}
+  </>
 );
