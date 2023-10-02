d0616ee71d67172ee62e8f99a186806abb2d035c
wendersonpires.near <wendersonpires.near>
2023-10-02 21:21:39 +0000

    Update HelloWorld by wendersonpires.near at 2023-10-02T21:21:39.059Z

--- wendersonpires.near/HelloWorld/source_code.jsx
+++ wendersonpires.near/HelloWorld/source_code.jsx
@@ -1,3 +1,15 @@
+const test = Storage.get(
+  "my-storage-key",
+  "wendersonpires.near/widget/NearSocialBridgeTests"
+);
+console.log("Test:", test);
+
+const test2 = Storage.get(
+  "lastBlockHeight",
+  "mob.near/widget/NotificationFeed"
+);
+console.log("Test2:", test2);
+
 return (
   <>
     <p>
@@ -5,3 +17,22 @@ return (
     </p>
   </>
 );
+
+// // The link provided by thirdapp pointing to the react app stored inside IPFS
+// const externalAppUrl =
+//   "https://bafybeigadizblnffuidc5jmrnhmtd64bfzmapaoohb6twpcmbguluw25kq.ipfs.cf-ipfs.com/";
+
+// asyncFetch(
+//   "https://bafybeigadizblnffuidc5jmrnhmtd64bfzmapaoohb6twpcmbguluw25kq.ipfs.cf-ipfs.com/"
+// ).then((res) => {
+//   console.log("UI:", res);
+// });
+
+// return (
+//   <Widget
+//     src={"wendersonpires.near/widget/NearSocialBridgeCore"}
+//     props={{
+//       externalAppUrl,
+//     }}
+//   />
+// );
