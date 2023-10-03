cc8ae1fc7bd246a6a21668d80b27ed7989109bae
racheludoka.near <racheludoka.near>
2023-10-03 14:54:11 +0000

    Update racheludoka.near by racheludoka.near at 2023-10-03T14:54:11.679Z

--- racheludoka.near/racheludoka.near/source_code.jsx
+++ racheludoka.near/racheludoka.near/source_code.jsx
@@ -1,1 +1,11 @@
-return <div>Hello World</div>;
+const accountId = context.accountId;
+
+const homepage = accountId
+  ? Social.get(`${accountId}/settings/near.social/homepage`)
+  : undefined;
+
+if (homepage === null) {
+  return "Loading";
+}
+
+return <Widget src={homepage ?? "mob.near/widget/N"} props={props} />;
