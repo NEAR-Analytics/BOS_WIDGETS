47990d75361e69b749d8dd38e7cf201802927fd5
mob.near <mob.near>
2023-10-03 18:15:31 +0000

    Update N.CommentButton by mob.near at 2023-10-03T18:15:31.227Z

--- mob.near/N.CommentButton/source_code.jsx
+++ mob.near/N.CommentButton/source_code.jsx
@@ -1,26 +1,40 @@
-const Button = styled.button`
-  border: 0 !important;
+const Button = styled.div`
+  line-height: 20px;
+  min-height: 20px;
   display: inline-flex;
   align-items: center;
   justify-content: left;
-  position: relative;
   background: inherit;
   color: inherit;
-  height: 1em;
-  svg {
-    margin-top: -2px;
+
+  font-size: 16px;
+  .icon {
+    position: relative;
+    &:before {
+      margin: -8px;
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      border-radius: 50%;
+    }
   }
+
+  .count {
+    margin-left: 8px;
+  }
+
+  &:not([disabled]) {
+    cursor: pointer;
+  }
+
   &:not([disabled]):hover {
     opacity: 1 !important;
     color: DeepSkyBlue;
 
-    &:before {
-      content: "";
-      position: absolute;
-      left: 1px;
-      border-radius: 50%;
-      width: 2.1875em;
-      height: 2.1875em;
+    .icon:before {
       background: rgba(0, 191, 255, 0.1);
     }
   }
@@ -30,10 +44,10 @@ const CommentIcon = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="currentColor"
-    viewBox="-2 -2 20 20"
+    viewBox="-2 0 20 20"
     stroke="currentColor"
     strokeWidth="0.5"
-    style={{ height: "1.5em" }}
+    style={{ width: "1.25em" }}
   >
     <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
   </svg>
@@ -42,10 +56,10 @@ const CommentIcon = (
 return (
   <Button
     disabled={!context.accountId || props.disabled}
-    title={"Comment"}
+    title={props.title ?? "Comment"}
     onClick={props.onClick}
   >
-    <span>{CommentIcon}</span>
+    <span className="icon">{CommentIcon}</span>
     <span className="count" />
   </Button>
 );
