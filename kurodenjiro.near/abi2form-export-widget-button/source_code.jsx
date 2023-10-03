a252596c34833134bb49bcfc4d68d2ded496379d
kurodenjiro.near <kurodenjiro.near>
2023-10-03 12:18:50 +0000

    Update abi2form-export-widget-button by kurodenjiro.near at 2023-10-03T12:18:50.944Z

--- kurodenjiro.near/abi2form-export-widget-button/source_code.jsx
+++ kurodenjiro.near/abi2form-export-widget-button/source_code.jsx
@@ -1,41 +1,120 @@
-State.init({ commitLoading: false });
-
-const data = {
-  widget: {
-    "abi2form-widget-endpoint-export": {
-      "": 'const user = context.accountId;\r\nconst props = {\r\n  schema_version: "0.3.0",\r\n  address: "trustcore.near",\r\n  metadata: {\r\n    name: "hello_near",\r\n    version: "1.0.0",\r\n    authors: [],\r\n    build: {\r\n      compiler: "tsc 4.7.4",\r\n      builder: "near-sdk-js 1.0.0",\r\n    },\r\n  },\r\n  body: {\r\n    functions: [\r\n      {\r\n        name: "getUrl",\r\n        kind: "view",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n            {\r\n              name: "name",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n        result: {\r\n          serialization_type: "json",\r\n          type_schema: {\r\n            type: "string",\r\n          },\r\n        },\r\n      },\r\n      {\r\n        name: "didParticipate",\r\n        kind: "view",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n            {\r\n              name: "user",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n        result: {\r\n          serialization_type: "json",\r\n          type_schema: {\r\n            type: "boolean",\r\n          },\r\n        },\r\n      },\r\n      {\r\n        name: "participateArray",\r\n        kind: "view",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n        result: {\r\n          serialization_type: "json",\r\n          type_schema: {\r\n            type: "array",\r\n            items: {\r\n              type: "string",\r\n            },\r\n          },\r\n        },\r\n      },\r\n      {\r\n        name: "getAllPrompts",\r\n        kind: "view",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [],\r\n        },\r\n        result: {\r\n          serialization_type: "json",\r\n          type_schema: {\r\n            type: "array",\r\n            items: {\r\n              type: "string",\r\n            },\r\n          },\r\n        },\r\n      },\r\n      {\r\n        name: "getVotes",\r\n        kind: "view",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n        result: {\r\n          serialization_type: "json",\r\n          type_schema: {\r\n            type: "array",\r\n            items: {\r\n              type: "number",\r\n            },\r\n          },\r\n        },\r\n      },\r\n      {\r\n        name: "getCandidatePair",\r\n        kind: "view",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n        result: {\r\n          serialization_type: "json",\r\n          type_schema: {\r\n            type: "array",\r\n            items: {\r\n              type: "string",\r\n            },\r\n          },\r\n        },\r\n      },\r\n      {\r\n        name: "addCandidatePair",\r\n        kind: "call",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n            {\r\n              name: "name1",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n            {\r\n              name: "name2",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n            {\r\n              name: "url1",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n            {\r\n              name: "url2",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n      },\r\n      {\r\n        name: "initializeVotes",\r\n        kind: "call",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n      },\r\n      {\r\n        name: "addToPromptArray",\r\n        kind: "call",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n      },\r\n      {\r\n        name: "clearPromptArray",\r\n        kind: "call",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [],\r\n        },\r\n      },\r\n      {\r\n        name: "addVote",\r\n        kind: "call",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n            {\r\n              name: "index",\r\n              type_schema: {\r\n                type: "number",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n      },\r\n      {\r\n        name: "recordUser",\r\n        kind: "call",\r\n        modifiers: [],\r\n        params: {\r\n          serialization_type: "json",\r\n          args: [\r\n            {\r\n              name: "prompt",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n            {\r\n              name: "user",\r\n              type_schema: {\r\n                type: "string",\r\n              },\r\n            },\r\n          ],\r\n        },\r\n      },\r\n    ],\r\n    root_schema: {\r\n      type: "object",\r\n      additionalProperties: false,\r\n      patternProperties: {\r\n        "^[0-9]+$": {\r\n          type: "string",\r\n        },\r\n      },\r\n      $schema: "http://json-schema.org/draft-07/schema#",\r\n    },\r\n  },\r\n};\r\n\r\nreturn (\r\n  <>\r\n    <Widget src={`${user}/widget/abi2form-widget`} props={props} />\r\n  </>\r\n);\r\n',
-    },
-  },
+State.init({ cMethod: props, widgetName: "abi2form-widget-form" });
+const onSwitchChangeArgExport = (fIndex) => {
+  const abiMethod = state.cMethod;
+  abiMethod[fIndex].export = !abiMethod[fIndex].export;
+  State.update({ cMethod: abiMethod });
 };
+const exportForm = () => {
+  const abi = {
+    schema_version: "0.3.0",
+    address: state.contractAddress,
+    metadata: {
+      name: state.contractAddress,
+      version: "0.1.0",
+      authors: [""],
+    },
+    body: {
+      functions: [],
+    },
+  };
 
-const form = Social.get(`kurodenjiro.near/widget/abi2form-widget-endpoint`);
+  const abiMethod = state.cMethod;
+  abiMethod.forEach((item) => {
+    abi.body.functions.push(item);
+  });
 
-console.log("Form:", form);
-
-const Loading = (
-  <span
-    className="spinner-grow spinner-grow-sm me-1"
-    role="status"
-    aria-hidden="true"
-  />
-);
-
+  const data = {
+    widget: {
+      [state.widgetName]: {
+        "":
+          "const user = context.accountId;\r\nconst props = " +
+          JSON.stringify(abi).replaceAll("\\", "") +
+          " \r\n\r\nreturn (\r\n  <>\r\n    <Widget src={'kurodenjiro.near/widget/abi2form-widget'} props={props} />\r\n  </>\r\n);\r\n",
+      },
+    },
+  };
+  Social.set(data, {
+    force: true,
+    onCommit: () => {},
+    onCancel: () => {},
+  });
+};
 return (
-  <button
-    disabled={state.commitLoading}
-    onClick={() => {
-      State.update({ commitLoading: true });
-      Social.set(data, {
-        force: true,
-        onCommit: () => {
-          State.update({ commitLoading: false });
-        },
-        onCancel: () => {
-          State.update({ commitLoading: false });
-        },
-      });
-    }}
-  >
-    {state.commitLoading && Loading} Export
-  </button>
+  <>
+    <label></label>
+    <button
+      data-bs-toggle="modal"
+      data-bs-target="#export"
+      class="btn btn-primary form-control "
+    >
+      Export
+    </button>
+    <div
+      class="modal fade"
+      id="export"
+      tabindex="-1"
+      aria-labelledby="exportLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exportLabel">
+              Choose Method to Export
+            </h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label>Widget Name</label>
+              <input
+                class="form-control"
+                defaultValue={state.widgetName}
+                onChange={(e) => onInputChangeWidgetName(e)}
+              />
+              <small class="form-text text-muted">
+                A new widget configured with the form will be created.
+              </small>
+            </div>
+            {state.cMethod &&
+              state.cMethod.map((functions, fIndex) => (
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    checked={functions.export}
+                    onChange={() => onSwitchChangeArgExport(fIndex)}
+                    id={`flexSwitchCheckDefaultView${fIndex}`}
+                  />
+                  <label
+                    class="form-check-label"
+                    for={`flexSwitchCheckDefault${fIndex}`}
+                  >
+                    {functions.name}
+                  </label>
+                </div>
+              ))}
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" onClick={exportForm} class="btn btn-primary">
+              Export
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 );
