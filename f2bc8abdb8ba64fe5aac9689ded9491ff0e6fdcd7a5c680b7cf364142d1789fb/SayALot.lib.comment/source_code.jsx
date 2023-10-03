096e9824a882e5283b12bf8a734d771b907867f1
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-10-03 04:41:55 +0000

    Update SayALot.lib.comment by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-10-03T04:41:55.748Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.lib.comment/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot.lib.comment/source_code.jsx
@@ -4,6 +4,81 @@ const prodAction = "sayALotComment";
 const testAction = `test_${prodAction}`;
 const action = isTest ? testAction : prodAction;
 
+const authorForWidget =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+//const authorForWidget = "sayalot.near";
+// const authorForWidget = "kenrou-it.near";
+const libSrcArray = [`${authorForWidget}/widget/SayALot.lib.SBT`];
+
+State.init({ libCalls: [] });
+
+function libStateUpdate(obj) {
+  State.update(obj);
+}
+
+function setAreValidUsers(accountIds, sbtName) {
+  const newLibCalls = [...state.libCalls];
+  accountIds.forEach((accountId) => {
+    const isCallPushed =
+      newLibCalls.find((libCall) => {
+        return (
+          libCall.functionName === "isValidUser" &&
+          libCall.props.accountId === accountId
+        );
+      }) !== undefined;
+    const isCallReturned = state[`isValidUser-${accountId}`] !== undefined;
+
+    if (isCallPushed || isCallReturned) {
+      return;
+    }
+
+    newLibCalls.push({
+      functionName: "isValidUser",
+      key: `isValidUser-${accountId}`,
+      props: {
+        accountId,
+        sbtName,
+      },
+    });
+  });
+  State.update({ libCalls: newLibCalls });
+}
+
+function callLibs(srcArray, stateUpdate, libCalls) {
+  return (
+    <>
+      {srcArray.map((src) => {
+        return (
+          <Widget
+            src={src}
+            props={{
+              isTest,
+              stateUpdate,
+              libCalls,
+            }}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+function canUserCreateComment(props) {
+  const { env, accountId, sbtName } = props;
+
+  setAreValidUsers([accountId], sbtName);
+
+  const result = state[`isValidUser-${accountId}`];
+
+  resultLibCalls = resultLibCalls.filter((call) => {
+    const discardCondition =
+      call.functionName === "canUserCreateComment" && result !== undefined;
+    return !discardCondition;
+  });
+
+  return result;
+}
+
 function createComment(props) {
   const { comment, onClick, onCommit, onCancel } = props;
 
@@ -45,8 +120,7 @@ function saveComment(comment, onCommit, onCancel) {
   }
 }
 
-function getComments(props) {
-  const { realArticleId } = props;
+function getComments(realArticleId) {
   return Social.index(action, realArticleId, {
     order: "desc",
     subscribe: true,
@@ -65,10 +139,38 @@ function filterInvalidArticlesIndexes(commentIndexes) {
 }
 
 function getValidComments(props) {
-  const commentIndexes = getComments(props);
-  const validCommentsIndexes = filterInvalidArticlesIndexes(commentIndexes);
+  const { realArticleId, articleSbts } = props;
+  const commentIndexes = getComments(realArticleId);
+  const blacklistFilteredComments =
+    filterInvalidArticlesIndexes(commentIndexes);
 
-  return validCommentsIndexes;
+  let finalComments = blacklistFilteredComments;
+  if (articleSbts.length > 0) {
+    const blacklistFilteredCommentsAuthors = blacklistFilteredComments.map(
+      (comment) => {
+        return comment.accountId;
+      }
+    );
+
+    setAreValidUsers(blacklistFilteredCommentsAuthors, articleSbts);
+
+    const validAuthors = blacklistFilteredCommentsAuthors.filter((author) => {
+      return state[`isValidUser-${author}`] === true;
+    });
+
+    resultLibCalls = resultLibCalls.filter((call) => {
+      const discardCondition =
+        call.functionName === "getValidComments" &&
+        state[`isValidUser-${call.props.accountId}`] !== undefined;
+      return !discardCondition;
+    });
+
+    finalComments = blacklistFilteredCommentsAuthors.filter((comment) => {
+      return validAuthors.includes(comment.accountId);
+    });
+  }
+
+  return finalComments;
 }
 
 function libCall(call) {
@@ -76,6 +178,8 @@ function libCall(call) {
     return createComment(call.props);
   } else if (call.functionName === "getValidComments") {
     return getValidComments(call.props);
+  } else if (call.functionName === "canUserCreateComment") {
+    return canUserCreateComment(call.props);
   }
 }
 
@@ -95,4 +199,4 @@ if (libCalls && libCalls.length > 0) {
   stateUpdate(updateObj);
 }
 
-return <>{}</>;
+return <>{callLibs(libSrcArray, libStateUpdate, state.libCalls)}</>;
