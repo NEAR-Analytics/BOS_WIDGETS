ba2a8e31389c359ea970b80671435cc2c91219b2
mob.near <mob.near>
2023-10-03 23:01:17 +0000

    Update N.Lightbox by mob.near at 2023-10-03T23:01:17.782Z

--- mob.near/N.Lightbox/source_code.jsx
+++ mob.near/N.Lightbox/source_code.jsx
@@ -1,33 +1,37 @@
 const show = props.show;
 const hide = props.onHide ?? (() => {});
 
+const closeSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    style={{
+      width: "4em",
+      stroke: "rgba(255, 255, 255, 0.5)",
+      strokeWidth: "0.5px",
+    }}
+    fill="black"
+    viewBox="0 0 16 16"
+  >
+    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+  </svg>
+);
+
 return show ? (
   <>
     <div key="backdrop" className="fade modal-backdrop show" />
     <div
       role="dialog"
       aria-modal="true"
-      className="fade modal show"
+      className="fade modal show lightbox"
       style={{ display: "block" }}
       onClick={() => hide()}
     >
-      <div
-        className="modal-dialog modal-xl modal-dialog-centered"
-        onClick={(e) => {
-          e.stopPropagation && e.stopPropagation();
-        }}
-      >
-        <div className="modal-content position-relative">
-          <div className="position-absolute top-0 end-0">
-            <button
-              type="button"
-              className="btn-close"
-              onClick={() => hide()}
-            ></button>
-          </div>
-          {props.children}
-        </div>
+      <div className="position-absolute top-0 end-0">
+        <button className="btn border-0" title="Close" onClick={() => hide()}>
+          {closeSvg}
+        </button>
       </div>
+      {props.children}
     </div>
   </>
 ) : (
