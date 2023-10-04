2bffb76260139d329143fb2b77e7c4eb6c4d30c5
andyh.near <andyh.near>
2023-10-04 18:00:43 +0000

    Update TrustDemoChild by andyh.near at 2023-10-04T18:00:43.228Z

--- andyh.near/TrustDemoChild/source_code.jsx
+++ andyh.near/TrustDemoChild/source_code.jsx
@@ -1,1 +1,20 @@
-return <>child</>;
+return (
+  <div className="row">
+    <div className="col" style={{ border: "1px solid orange" }}>
+      <h6>trusted grandchild</h6>
+      <Widget
+        id="trusted-grandchild"
+        isTrusted
+        src="andyh.near/widget/TrustDemoGrandChild"
+      />
+    </div>
+    <div className="col">
+      <h6>sandboxed grandchild</h6>
+      <Widget
+        id="sandboxed-grandchild"
+        isTrusted={false}
+        src="andyh.near/widget/TrustDemoGrandChild"
+      />
+    </div>
+  </div>
+);
