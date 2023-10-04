e8ad9f0e555224a472e16b17deaf0ec6e9a0edc0
yairnava.near <yairnava.near>
2023-10-04 18:19:55 +0000

    Update ToDoList by yairnava.near at 2023-10-04T18:19:55.587Z

--- yairnava.near/ToDoList/source_code.jsx
+++ yairnava.near/ToDoList/source_code.jsx
@@ -10,12 +10,11 @@ if (!todolistAbi.ok) {
 
 const iface = new ethers.utils.Interface(todolistAbi.body);
 
-if (state.sender === undefined) {
-  const accounts = Ethers.send("eth_requestAccounts", []);
-  if (accounts.length) {
-    State.update({ sender: accounts[0] });
-  }
-}
+const contract = new ethers.Contract(
+  todolistContract,
+  todolistAbi.body,
+  Ethers.provider().getSigner()
+);
 
 State.init({
   getTasks: true,
@@ -27,12 +26,6 @@ const submitTask = () => {
     return console.log("El nombre de la tarea no debe estar vacia");
   }
 
-  const contract = new ethers.Contract(
-    todolistContract,
-    todolistAbi.body,
-    Ethers.provider().getSigner()
-  );
-
   contract.addTask(state.strTask).then((transactionHash) => {
     setTimeout(() => {
       getTasks();
@@ -40,39 +33,20 @@ const submitTask = () => {
   });
 };
 
-const finishTask = (task) => {
-  const contract = new ethers.Contract(
-    todolistContract,
-    todolistAbi.body,
-    Ethers.provider().getSigner()
-  );
-
-  contract.updateStatus(task.id, true).then((transactionHash) => {
-    setTimeout(() => {
-      getTasks();
-    }, 5000);
-  });
-};
-
 const getTasks = () => {
-  const contract = new ethers.Contract(
-    todolistContract,
-    todolistAbi.body,
-    Ethers.provider().getSigner()
-  );
-
   contract.getTaskCount().then((res) => {
     const countTasks = res.toNumber();
     let tasks = [];
     for (let i = 0; i < countTasks; i++) {
-      const taskNumber = i;
       contract.getTask(i).then((res) => {
-        const newTask = { id: taskNumber, name: res[0], status: res[1] };
+        const newTask = { name: res[0], status: res[1] };
+        console.log(newTask);
         tasks.push(newTask);
       });
     }
     setTimeout(() => {
       State.update({ user_tasks: tasks, getTasks: false });
+      console.log(tasks);
     }, 10000);
   });
 };
@@ -84,7 +58,7 @@ if (state.sender === undefined) {
   }
 }
 
-if (state.user_tasks.length == 0 && state.sender) {
+if (state.tasks === undefined && state.sender) {
   getTasks();
 }
 
@@ -121,7 +95,7 @@ return (
     <div class="LidoContainer">
       <div class="Header">Lista de Tareas</div>
 
-      {state.sender ? (
+      {!!state.sender ? (
         <>
           <div class="SubHeader">Crear nueva tarea </div>
           <div class="LidoStakeFormInputContainer">
@@ -145,37 +119,11 @@ return (
             {state.getTasks ? (
               <span>Consultando tareas...</span>
             ) : state.user_tasks.length > 0 ? (
-              <table className="table table-hover table-sm">
-                <thead>
-                  <tr class="p-3 mb-2 bg-primary bg-gradient text-white rounded-5 text-center">
-                    <th>Nombre</th>
-                    <th>Terminada</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {state.user_tasks.map((data, key) => {
-                    return (
-                      <tr class="text-center">
-                        <td>{data.name}</td>
-                        <td>{data.status ? "Si" : "No"}</td>
-                        <td>
-                          {!data.status && (
-                            <button
-                              class="btn btn-primary"
-                              onClick={async () => {
-                                finishTask(data);
-                              }}
-                            >
-                              Finalizar
-                            </button>
-                          )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+              <ul>
+                {state.user_tasks.map((item) => {
+                  return <li>{item.name}</li>;
+                })}
+              </ul>
             ) : (
               <span>No hay tareas guardadas</span>
             )}
