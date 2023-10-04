a46c7812bec18227c2261505edd14309c01fe538
mob.near <mob.near>
2023-10-04 22:45:02 +0000

    Update CopyButton by mob.near at 2023-10-04T22:45:02.257Z

--- mob.near/CopyButton/source_code.jsx
+++ mob.near/CopyButton/source_code.jsx
@@ -1,3 +1,19 @@
+const copySvg = (
+  <svg
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    style={{ width: "1em" }}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect height="14" rx="2" ry="2" width="14" x="8" y="8" />
+    <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+  </svg>
+);
+
 return (
   <OverlayTrigger
     placement="auto"
@@ -23,8 +39,7 @@ return (
         </>
       ) : (
         <>
-          {props.clipboardIcon ?? <i className="bi bi-clipboard" />}{" "}
-          {props.label}
+          {props.clipboardIcon ?? copySvg} {props.label}
         </>
       )}
     </button>
