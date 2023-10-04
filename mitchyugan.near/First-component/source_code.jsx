8212f51c218f6f4ccf25d35b6456dbf66c10fd0a
mitchyugan.near <mitchyugan.near>
2023-10-04 10:46:08 +0000

    Update First-component by mitchyugan.near at 2023-10-04T10:46:08.032Z

---
+++ mitchyugan.near/First-component/source_code.jsx
@@ -0,0 +1,34 @@
+State.init({ user: "user", memo: "Have a great day", memoInputValue: "" });
+const onclick = () => {
+  State.update({ user: context.accountId });
+};
+
+const onchange = ({ target }) => {
+  State.update({ memoInputValue: target.value });
+};
+
+const updateMemo = () => {
+  State.update({ memo: state.memoInputValue });
+};
+
+return (
+  <>
+    <div>
+      <h4>Welcome {state.user}</h4>
+      <button onClick={onclick}>Login</button>
+    </div>
+    <br />
+    <br />
+    <br />
+    <div>
+      <h4>Memo: {state.memo}</h4>
+      <input onChange={onchange} placeholder="Change Memo" />
+      <br />
+      <button onClick={updateMemo}>Update Memo</button>
+    </div>
+    <br />
+    <br />
+    <br />
+    <Widget src="mitchyugan.near/widget/ImageUploader" />
+  </>
+);
