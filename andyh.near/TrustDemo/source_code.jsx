33b635125305f6d1a0faa4c019d1eb5357e94cfc
andyh.near <andyh.near>
2023-10-04 16:58:46 +0000

    Update TrustDemo by andyh.near at 2023-10-04T16:58:46.435Z

--- andyh.near/TrustDemo/source_code.jsx
+++ andyh.near/TrustDemo/source_code.jsx
@@ -1,11 +1,15 @@
 return (
   <div className="row">
     <div className="col">
+      <h2>trusted root</h2>
       <Widget
         id="trusted-root"
         isTrusted
         src="andyh.near/widget/TrustDemoRoot"
       />
+    </div>
+    <div className="col">
+      <h2>sandboxed root</h2>
       <Widget
         id="sandboxed-root"
         isTrusted={false}
