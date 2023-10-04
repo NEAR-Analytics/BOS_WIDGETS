d9ad983c733a56127ff190ad810c905983443f94
andyh.near <andyh.near>
2023-10-04 17:01:33 +0000

    Update TrustDemoParent by andyh.near at 2023-10-04T17:01:33.071Z

---
+++ andyh.near/TrustDemoParent/source_code.jsx
@@ -0,0 +1,20 @@
+return (
+  <div className="row">
+    <div className="col">
+      <h6>trusted child</h6>
+      <Widget
+        id="trusted-child"
+        isTrusted
+        src="andyh.near/widget/TrustDemoChild"
+      />
+    </div>
+    <div className="col">
+      <h6>sandboxed child</h6>
+      <Widget
+        id="sandboxed-child"
+        isTrusted={false}
+        src="andyh.near/widget/TrustDemoChild"
+      />
+    </div>
+  </div>
+);
