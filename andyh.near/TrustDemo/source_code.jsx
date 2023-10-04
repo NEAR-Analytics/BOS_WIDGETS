f43629d929c79e638a3a983855555af5404c9785
andyh.near <andyh.near>
2023-10-04 16:56:24 +0000

    Update TrustDemo by andyh.near at 2023-10-04T16:56:24.286Z

---
+++ andyh.near/TrustDemo/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <>
+    <Widget id="trusted-root" isTrusted src="andyh.near/widget/TrustDemoRoot" />
+    <Widget
+      id="sandboxed-root"
+      isTrusted
+      src="andyh.near/widget/TrustDemoRoot"
+    />
+  </>
+);
