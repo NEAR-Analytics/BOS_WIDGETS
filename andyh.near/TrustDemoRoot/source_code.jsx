72f1905cef98c519614e50d6dbb1aef68cfba324
andyh.near <andyh.near>
2023-10-04 17:58:30 +0000

    Update TrustDemoRoot by andyh.near at 2023-10-04T17:58:30.610Z

--- andyh.near/TrustDemoRoot/source_code.jsx
+++ andyh.near/TrustDemoRoot/source_code.jsx
@@ -1,6 +1,6 @@
 return (
   <div className="row">
-    <div className="col">
+    <div className="col" style={{ border: "1px solid orange" }}>
       <h6>trusted parent</h6>
       <Widget
         id="trusted-parent"
