3d1bde8f4f53d3a784603a92c878825eca31d7b9
andyh.near <andyh.near>
2023-10-04 05:20:00 +0000

    Update Test.StateDemoParent by andyh.near at 2023-10-04T05:20:00.097Z

---
+++ andyh.near/Test.StateDemoParent/source_code.jsx
@@ -0,0 +1,29 @@
+// State.init({ parent: 0, child: 0 });
+
+const {
+  circle,
+  square,
+  triangle,
+  updateCircle,
+  updateSquare,
+  updateTriangle,
+  id,
+} = props;
+
+return (
+  <div>
+    <Widget
+      id={id}
+      isTrusted
+      src="andyh.near/widget/ShapeSet"
+      props={{
+        circle,
+        square,
+        triangle,
+        updateCircle,
+        updateSquare,
+        updateTriangle,
+      }}
+    />
+  </div>
+);
