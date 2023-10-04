0c70551a4a42cfbc98460e0f0ee906a87755e019
potlock.near <potlock.near>
2023-10-04 18:47:35 +0000

    Update Inputs.AccountId by potlock.near at 2023-10-04T18:47:35.218Z

--- potlock.near/Inputs.AccountId/source_code.jsx
+++ potlock.near/Inputs.AccountId/source_code.jsx
@@ -1,4 +1,4 @@
-const ownerId = "potlock.near";
+const ownerId = "nearhorizon.near";
 const label = props.label ?? "Account ID";
 const placeholder = props.placeholder ?? "Enter your account ID";
 const value = props.value ?? "";
