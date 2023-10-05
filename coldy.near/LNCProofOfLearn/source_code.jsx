310925ee1cbd2bb333bf7c9b8020bf28bdb0e8a9
coldy.near <coldy.near>
2023-10-05 13:52:31 +0000

    Update LNCProofOfLearn by coldy.near at 2023-10-05T13:52:31.702Z

--- coldy.near/LNCProofOfLearn/source_code.jsx
+++ coldy.near/LNCProofOfLearn/source_code.jsx
@@ -1,13 +1,45 @@
-const data = fetch(
-  `https://learnnear.club/wp-json/api/lnw-proof-of-learns-data?wallet=${context.accountId}`,
-  {
-    method: "GET",
+const fetchData = async () => {
+  try {
+    const response = await fetch(
+      `https://learnnear.club/wp-json/api/lnw-proof-of-learns-data?wallet=${context.accountId}`,
+      {
+        method: "GET",
+      }
+    );
+    const data = await response.json();
+    const tableData = data.data;
+
+    const table = (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Credentials</th>
+            <th>Message</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.creds}</td>
+              <td>{item.message}</td>
+              <td>{item.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+
+    ReactDOM.render(table, document.getElementById("table-container"));
+  } catch (error) {
+    console.error("Error fetching data:", error);
   }
-);
+};
 
 return (
   <>
     <div class="container border border-info p-3">
+      {fetchData()}
       <h3 class="text-center">
         <span class="text-decoration-underline"> {context.accountId} </span>
       </h3>
