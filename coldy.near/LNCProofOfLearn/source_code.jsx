6316fa61b500f05ff01b820d3ab4dcaf785f2c12
coldy.near <coldy.near>
2023-10-05 13:44:45 +0000

    Update LNCProofOfLearn by coldy.near at 2023-10-05T13:44:45.188Z

---
+++ coldy.near/LNCProofOfLearn/source_code.jsx
@@ -0,0 +1,9 @@
+const data = fetch(
+  `https://learnnear.club/wp-json/api/lnw-proof-of-learns-data?wallet${context.accountId}`,
+  {
+    //subscribe: true,
+    method: "GET",
+  }
+);
+
+console.log(data);
