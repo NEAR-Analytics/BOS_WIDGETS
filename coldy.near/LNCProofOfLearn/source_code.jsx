0b518bb4296ec9c7946261c1d769efd8343246ac
coldy.near <coldy.near>
2023-10-05 14:02:55 +0000

    Update LNCProofOfLearn by coldy.near at 2023-10-05T14:02:55.215Z

--- coldy.near/LNCProofOfLearn/source_code.jsx
+++ coldy.near/LNCProofOfLearn/source_code.jsx
@@ -11,7 +11,7 @@ return (
   <>
     <div class="container border border-info p-3">
       <h3 class="text-center">
-        <span class="text-decoration-underline"> {context.accountId} </span>
+        <span class="text-decoration-underline"> </span>
       </h3>
     </div>
   </>
