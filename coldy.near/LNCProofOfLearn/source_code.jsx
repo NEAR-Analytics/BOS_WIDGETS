55942109bf26424d94f8fcb20f3e29a11de68a2d
coldy.near <coldy.near>
2023-10-05 14:14:32 +0000

    Update LNCProofOfLearn by coldy.near at 2023-10-05T14:14:32.928Z

--- coldy.near/LNCProofOfLearn/source_code.jsx
+++ coldy.near/LNCProofOfLearn/source_code.jsx
@@ -24,10 +24,18 @@ console.log(data);
 
 return (
   <>
-    <div class="container border border-info p-3">
-      <h3 class="text-center">
-        <span class="text-decoration-underline"> </span>
-      </h3>
-    </div>
+    {data !== null ? (
+      <div>
+        {data.body.map((item) => (
+          <div>
+            <td>{item.creds}</td>
+            <td>{item.message}</td>
+            <td>{item.date}</td>
+          </div>
+        ))}
+      </div>
+    ) : (
+      <div>Loading ...</div>
+    )}
   </>
 );
