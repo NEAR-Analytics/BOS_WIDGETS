624f944af37a3d712ab50b019263e3e144bcbe87
bluebiu.near <bluebiu.near>
2023-10-05 16:53:22 +0000

    Update Mantle.Lending.LendleClaimHandler by bluebiu.near at 2023-10-05T16:53:22.458Z

---
+++ bluebiu.near/Mantle.Lending.LendleClaimHandler/source_code.jsx
@@ -0,0 +1,34 @@
+const CLAIM_ABI = [
+  {
+    type: "function",
+    stateMutability: "nonpayable",
+    outputs: [],
+    name: "claim",
+    inputs: [
+      { type: "address", name: "_user", internalType: "address" },
+      { type: "address[]", name: "_tokens", internalType: "address[]" },
+    ],
+  },
+];
+const account = Ethers.send("eth_requestAccounts", [])[0];
+const { loading, market, dapp, onSuccess, onError } = props;
+console.log("market: ", market);
+
+if (!loading) return "";
+
+const CollateralContract = new ethers.Contract(
+  dapp.incentiveController,
+  CLAIM_ABI,
+  Ethers.provider().getSigner()
+);
+CollateralContract.claim(account, market.allPools)
+  .then((tx) => {
+    tx.wait().then((res) => {
+      onSuccess(res);
+    });
+  })
+  .catch(() => {
+    onError();
+  });
+
+return "";
