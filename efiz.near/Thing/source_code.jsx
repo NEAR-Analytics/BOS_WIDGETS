bf23ffc16d0145e2f548f292dd5f3225086f2f18
efiz.near <efiz.near>
2023-10-06 04:31:37 +0000

    Update Thing by efiz.near at 2023-10-06T04:31:37.614Z

--- efiz.near/Thing/source_code.jsx
+++ efiz.near/Thing/source_code.jsx
@@ -1,5 +1,33 @@
-const path = props.path || "james.near/thing/page/one";
-const template = props.template || "hack.near/widget/community.page";
-const thing = JSON.parse(Social.get(path) || "null");
+function create(key, value) {
+  // or it could be front loaded
+}
 
-return <Widget src={thing.template || template} props={thing} />;
+function set(key, value) {
+  // Validate the type
+  const typeObj = JSON.parse(type);
+  const typeOfValue = typeof value;
+  if (typeObj[key] !== typeOfValue) {
+    console.log(`Expected ${typeObj[key]} but got ${typeOfValue}`);
+    // can't throw error, but could return or display something on screen
+    // handle error?
+  }
+
+  Social.set({
+    thing: {
+      [key]: {
+        "": JSON.stringify(value),
+      },
+    },
+    // and index?
+  });
+}
+
+function get(key) {
+  return JSON.parse(Social.get(key, "final") || "null");
+}
+
+function view() {
+  // check Type for default view, or use a Template
+}
+
+return { create, set, get, view };
