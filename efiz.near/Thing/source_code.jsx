dd89f6d21a86cbc7928c3a7c0862f6402e9cfab2
efiz.near <efiz.near>
2023-10-06 04:38:36 +0000

    Update Thing by efiz.near at 2023-10-06T04:38:36.615Z

--- efiz.near/Thing/source_code.jsx
+++ efiz.near/Thing/source_code.jsx
@@ -1,33 +1,34 @@
-function create(key, value) {
-  // or it could be front loaded
-}
-
-function set(key, value) {
-  // Validate the type
-  const typeObj = JSON.parse(type);
-  const typeOfValue = typeof value;
-  if (typeObj[key] !== typeOfValue) {
-    console.log(`Expected ${typeObj[key]} but got ${typeOfValue}`);
-    // can't throw error, but could return or display something on screen
-    // handle error?
+function Thing({ type, Template }) {
+  function create(key, value) {
+    // or it could be front loaded
+    console.log("create");
   }
 
-  Social.set({
-    thing: {
-      [key]: {
-        "": JSON.stringify(value),
+  function set(key, value) {
+    // Validate the type
+    const typeObj = JSON.parse(type);
+    const typeOfValue = typeof value;
+    if (typeObj[key] !== typeOfValue) {
+      console.log(`Expected ${typeObj[key]} but got ${typeOfValue}`);
+      // can't throw error, but could return or display something on screen
+      // handle error?
+    }
+
+    Social.set({
+      thing: {
+        [key]: {
+          "": JSON.stringify(value),
+        },
       },
-    },
-    // and index?
-  });
+      // and index?
+    });
+  }
+
+  function get(key) {
+    return JSON.parse(Social.get(key, "final") || "null");
+  }
+
+  return <Template create={create} set={set} get={get} />;
 }
 
-function get(key) {
-  return JSON.parse(Social.get(key, "final") || "null");
-}
-
-function view() {
-  // check Type for default view, or use a Template
-}
-
-return { create, set, get, view };
+return { Thing };
