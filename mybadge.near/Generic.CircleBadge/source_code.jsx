bafeb3f989eae2430e59a902782e04d9b243011f
mybadge.near <mybadge.near>
2023-10-06 14:17:46 +0000

    Update Generic.CircleBadge by mybadge.near at 2023-10-06T14:17:46.166Z

--- mybadge.near/Generic.CircleBadge/source_code.jsx
+++ mybadge.near/Generic.CircleBadge/source_code.jsx
@@ -1,3 +1,26 @@
+const DEFAULT_COLOR = "gray";
+
+const palette = {
+  red: {
+    background: "#DB504A",
+    text: "#222222",
+  },
+  green: {
+    background: "#16AD38",
+    text: "#222222",
+  },
+  blue: {
+    background: "#4A73DB",
+    text: "#222222",
+  },
+  gray: {
+    background: "#E7ECEF",
+    text: "#222222",
+  },
+};
+
+const colors = palette[props.color ?? DEFAULT_COLOR];
+
 const IconWrapper = styled.div`
     display: flex;
     width: 16px;
@@ -5,10 +28,10 @@ const IconWrapper = styled.div`
     justify-content: center;
     align-items: center;
     border-radius: 16px;
-    background: ${props.backgroundColor};
+    background: ${colors.background};
 
     * {
-        fill: white;
+        fill: ${colors.text};
     }
 `;
 
