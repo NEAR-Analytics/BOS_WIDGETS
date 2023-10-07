e79825ee8597d80fe61ecd3ada205f7bf661f15d
c5d50293c3a3ed146051462e6e02e469acda10b517bfffeb3d34652076f0cb7c <c5d50293c3a3ed146051462e6e02e469acda10b517bfffeb3d34652076f0cb7c>
2023-10-07 08:18:52 +0000

    Update Hero by c5d50293c3a3ed146051462e6e02e469acda10b517bfffeb3d34652076f0cb7c at 2023-10-07T08:18:52.103Z

---
+++ c5d50293c3a3ed146051462e6e02e469acda10b517bfffeb3d34652076f0cb7c/Hero/source_code.jsx
@@ -0,0 +1,10 @@
+const Hero = ({}) => {
+  return (
+    <Web3Connect
+      className="swap-button-enabled swap-button-text p-2"
+      connectLabel="Connect with wallet"
+    />
+  );
+};
+
+return <Hero {...props} />;
