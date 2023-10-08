f0b64e8529c74b7ecd18edab5232e24f82c65450
sayalot.near <sayalot.near>
2023-10-08 21:12:02 +0000

    Update SayALot.lib.SBT by sayalot.near at 2023-10-08T21:12:02.812Z

---
+++ sayalot.near/SayALot.lib.SBT/source_code.jsx
@@ -0,0 +1,66 @@
+const { isTest, stateUpdate, libCalls } = props;
+
+let resultLibCalls = [];
+
+function isValidUser(props) {
+  const { accountId, sbtsNames } = props;
+  const userSBTs = Near.view(
+    "registry.i-am-human.near",
+    "sbt_tokens_by_owner",
+    {
+      account: accountId,
+    }
+  );
+
+  const result =
+    userSBTs.find((sbt) => {
+      return sbt[0] === sbtsNames[0];
+    }) !== undefined;
+  resultLibCalls = resultLibCalls.filter((call) => {
+    return call.functionName !== "isValidUser";
+  });
+
+  // return true;
+
+  return result;
+}
+
+function getLoggedUserSbts(props) {
+  const { accountId } = props;
+  const userSBTs = Near.view(
+    "registry.i-am-human.near",
+    "sbt_tokens_by_owner",
+    {
+      account: accountId,
+    }
+  );
+
+  if (userSBTs) {
+    resultLibCalls = resultLibCalls.filter((call) => {
+      return call.functionName !== "getLoggedUserSbts";
+    });
+  }
+
+  return userSBTs;
+}
+
+function libCall(call) {
+  if (call.functionName === "isValidUser") {
+    return isValidUser(call.props);
+  } else if (call.functionName === "getLoggedUserSbts") {
+    return getLoggedUserSbts(call.props);
+  }
+}
+
+if (libCalls && libCalls.length > 0) {
+  const updateObj = {};
+  resultLibCalls = [...libCalls];
+  libCalls.forEach((call) => {
+    updateObj[call.key] = libCall(call);
+  });
+
+  updateObj.libCalls = resultLibCalls;
+  stateUpdate(updateObj);
+}
+
+return <></>;
