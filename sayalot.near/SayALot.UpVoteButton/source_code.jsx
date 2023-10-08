1788d021ad1fd57ff5a2c7b4fb8d6b05f40c16b5
sayalot.near <sayalot.near>
2023-10-08 21:19:26 +0000

    Update SayALot.UpVoteButton by sayalot.near at 2023-10-08T21:19:26.654Z

---
+++ sayalot.near/SayALot.UpVoteButton/source_code.jsx
@@ -0,0 +1,139 @@
+const {
+  isTest,
+  authorForWidget,
+  reactedElementData,
+  widgets,
+  disabled,
+  articleSbts,
+  upVotes,
+} = props;
+
+const data = reactedElementData;
+
+const libSrcArray = [widgets.libUpVotes];
+
+const initLibCalls = [
+  {
+    functionName: "getUpVotes",
+    key: "upVotes",
+    props: {
+      realArticleId: data.realArticleId ?? `${data.author}-${data.timeCreate}`,
+      articleSbts,
+    },
+  },
+];
+
+if (!upVotes) {
+  State.init({
+    libCalls: initLibCalls,
+    upVotes: [],
+  });
+}
+
+let upVotesData = upVotes ?? state.upVotes;
+
+let userVote = upVotesData.find((vote) => vote.accountId === context.accountId);
+
+let hasUserVoted = userVote !== undefined;
+
+function getUpVoteButtonClass() {
+  if (hasUserVoted) {
+    return "primary";
+  } else {
+    return "secondary dark";
+  }
+}
+
+function callLibs(srcArray, stateUpdate, libCalls) {
+  return (
+    <>
+      {srcArray.map((src) => {
+        return (
+          <Widget
+            src={src}
+            props={{
+              isTest,
+              stateUpdate,
+              libCalls,
+            }}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+function stateUpdate(obj) {
+  State.update(obj);
+}
+
+function upVoteButtonListener() {
+  let newLibCalls = [...state.libCalls];
+
+  if (!hasUserVoted) {
+    newLibCalls.push({
+      functionName: "addVote",
+      key: "newVote",
+      props: {
+        realArticleId:
+          data.realArticleId ?? `${data.author}-${data.timeCreate}`,
+      },
+    });
+  } else {
+    newLibCalls.push({
+      functionName: "deleteVote",
+      key: "deletedVote",
+      props: {
+        realArticleId:
+          data.realArticleId ?? `${data.author}-${data.timeCreate}`,
+        upVoteId: userVote.value.upVoteId,
+      },
+    });
+  }
+  State.update({ libCalls: newLibCalls });
+}
+
+const IconContainer = styled.div`
+  transform: rotate(-90deg);
+`;
+
+const Icon = styled.i`
+  margin: 0px !important;
+`;
+
+const CallLibrary = styled.div`
+  display: none;
+`;
+
+return (
+  <>
+    <Widget
+      src={widgets.styledComponents}
+      props={{
+        Button: {
+          text: `+${upVotesData.length}`,
+          disabled,
+          className: `${getUpVoteButtonClass()}`,
+          size: "sm",
+          onClick: upVoteButtonListener,
+          icon: (
+            <IconContainer>
+              <Icon className="bi bi-fast-forward-fill"></Icon>
+            </IconContainer>
+          ),
+          // icon: (
+          //   <IconContainer>
+          //     <Icon className="bi bi-fast-forward-button"></Icon>
+          //   </IconContainer>
+          // ),
+          // icon: <i className="bi bi-hand-thumbs-up"></i>,
+          // icon: "⏫",
+        },
+      }}
+    />
+
+    <CallLibrary>
+      {callLibs(libSrcArray, stateUpdate, state.libCalls)}
+    </CallLibrary>
+  </>
+);
