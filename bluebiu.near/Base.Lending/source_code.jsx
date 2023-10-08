8fe242e5e40da3a505c8c2db359e431af4624d7b
bluebiu.near <bluebiu.near>
2023-10-08 02:42:15 +0000

    Update Base.Lending by bluebiu.near at 2023-10-08T02:42:15.638Z

--- bluebiu.near/Base.Lending/source_code.jsx
+++ bluebiu.near/Base.Lending/source_code.jsx
@@ -16,6 +16,7 @@ const Theme = styled.div`
   --switch-color: #004bfc;
   --switch-border-color: #32496a;
   --secondary-border-color: #3f577b;
+  --yours-table-title: #ffffff;
 `;
 const CHAIN_ID = 8453;
 const CHAIN_NAME = "Base";
