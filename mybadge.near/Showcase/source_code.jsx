84f08d82f5dfdf115a09721b8226b8ecd5a8b712
mybadge.near <mybadge.near>
2023-10-09 16:51:38 +0000

    Update Showcase by mybadge.near at 2023-10-09T16:51:38.252Z

---
+++ mybadge.near/Showcase/source_code.jsx
@@ -0,0 +1,19 @@
+const Showcase = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+`;
+
+return (
+  <Showcase>
+    <Widget src="mybadge.near/widget/Near.SmallAvatarBadge" />
+    <Widget src="mybadge.near/widget/Near.BigAvatarBadge" />
+    <Widget src="mybadge.near/widget/Dapplets.DappCreatorBadge" />
+    <Widget src="mybadge.near/widget/Near.VeteranBadge" />
+    <Widget src="mybadge.near/widget/LNC.EliteBadge" />
+    <Widget src="mybadge.near/widget/DevHacks.CircleBadge" />
+    <Widget src="mybadge.near/widget/LNC.CircleBadge" />
+    <Widget src="mybadge.near/widget/Dapplets.CircleBadge" />
+  </Showcase>
+);
