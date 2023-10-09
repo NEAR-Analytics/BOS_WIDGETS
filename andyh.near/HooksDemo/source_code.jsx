93d8f89af0ef33fe7eb60263b22a19f2a2475f84
andyh.near <andyh.near>
2023-10-09 21:12:04 +0000

    Update HooksDemo by andyh.near at 2023-10-09T21:12:04.317Z

--- andyh.near/HooksDemo/source_code.jsx
+++ andyh.near/HooksDemo/source_code.jsx
@@ -1,9 +1,18 @@
 const [value, setValue] = useState(0);
+const [isInit, setIsInit] = useState(false);
 // const [value, setValue] = [4, console.log];
+
+useEffect(() => {
+  if (!isInit) {
+    setTimeout(() => setValue(500), 500);
+    setIsInit(true);
+  }
+}, [isInit]);
 
 return (
   <>
-    <span>{value}</span>
+    <span>{isInit.toString()}</span>
+    <span>{value.toString()}</span>
     <button className="btn" onClick={() => setValue(value + 1)}>
       Increment Value
     </button>
