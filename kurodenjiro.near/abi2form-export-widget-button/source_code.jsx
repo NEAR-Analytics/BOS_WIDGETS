34badb843dd982ae1d932b87a23195725035550b
kurodenjiro.near <kurodenjiro.near>
2023-10-09 19:21:44 +0000

    Update abi2form-export-widget-button by kurodenjiro.near at 2023-10-09T19:21:44.881Z

--- kurodenjiro.near/abi2form-export-widget-button/source_code.jsx
+++ kurodenjiro.near/abi2form-export-widget-button/source_code.jsx
@@ -42,40 +42,6 @@ const exportForm = () => {
     onCancel: () => {},
   });
 };
-
-const saveClient = () => {
-  const abi = {
-    schema_version: "0.3.0",
-    address: props.contractAddress,
-    metadata: {
-      name: "",
-      version: "0.1.0",
-      authors: [""],
-    },
-    body: {
-      functions: [],
-    },
-  };
-
-  const abiMethod = state.cMethod;
-  abiMethod.forEach((item) => {
-    abi.body.functions.push(item);
-  });
-  const data = {
-    magicbuild: {
-      client: {
-        [props.id]: {
-          abi: JSON.stringify(abi),
-        },
-      },
-    },
-  };
-  Social.set(data, {
-    force: true,
-    onCommit: () => {},
-    onCancel: () => {},
-  });
-};
 return (
   <>
     <label></label>
@@ -146,16 +112,6 @@ return (
             >
               Close
             </button>
-            {props.id && (
-              <button
-                type="button"
-                onClick={saveClient}
-                class="btn btn-primary"
-              >
-                Save Client
-              </button>
-            )}
-
             <button type="button" onClick={exportForm} class="btn btn-primary">
               Export
             </button>
