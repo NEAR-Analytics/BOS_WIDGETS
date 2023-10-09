7afd36d60b24b8fd6fdd130b96fdbd57bfd7f5ef
andyh.near <andyh.near>
2023-10-09 23:58:00 +0000

    Update HooksDemo by andyh.near at 2023-10-09T23:58:00.612Z

--- andyh.near/HooksDemo/source_code.jsx
+++ andyh.near/HooksDemo/source_code.jsx
@@ -1,4 +1,5 @@
 const [value, setValue] = useState(0);
+const [text, setText] = useState("");
 const [isInit, setIsInit] = useState(false);
 // const [value, setValue] = [4, console.log];
 
@@ -18,5 +19,8 @@ return (
     <button className="btn" onClick={() => setValue(value + 1)}>
       Increment Value
     </button>
+    <br />
+    <h2>{text}</h2>
+    <input type="text" onChange={(e) => setText(e.target.value)} />
   </>
 );
