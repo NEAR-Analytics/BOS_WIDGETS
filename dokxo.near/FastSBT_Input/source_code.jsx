4252e6ca262e5718a3b45343938dc8b6a0304091
dokxo.near <dokxo.near>
2023-10-10 23:42:13 +0000

    Update FastSBT_Input by dokxo.near at 2023-10-10T23:42:13.398Z

---
+++ dokxo.near/FastSBT_Input/source_code.jsx
@@ -0,0 +1,65 @@
+const FormInput = styled.input`
+box-sizing: border-box;
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 0px 10px;
+width: ${props.width ? props.width : "100%"};
+height:${props.height ? props.height : "30px"};
+background:${props.background ? props.background : "#FFFFFF"}; 
+border: 1px solid #D0D6D9;
+border-radius: 8px;
+flex: none;
+order: 1;
+flex-grow: 0;
+font-family: Open Sans;
+font-style: normal;
+font-weight: 400;
+font-size: ${props.fontSize ? props.fontSize : "10px"};
+line-height: 120%;
+display: flex;
+align-items: center;
+color:  ${props.fontColor ? props.fontColor : "#828688"};
+`;
+
+const H1styled = styled.input`
+
+width: 100%;
+height: 10px;
+font-family: Open Sans;
+font-style: normal;
+font-weight: 500;
+font-size: 12px;
+line-height: 120%;
+color: #000000;
+
+`;
+const Colcont = styled.h1`
+
+margin-top:.5rem;
+ width:33.3%;
+ @media only screen and (max-width: 820px) {
+ width:100%;
+}
+@media only screen and (max-width: 480px) {
+ width:100%;
+ 
+}
+
+`;
+
+const onChangeFunction = props.handleDao_Contract;
+return (
+  <Colcont>
+    <H1styled>{props.title}</H1styled>
+    <FormInput
+      type={props.type}
+      value={props.value}
+      placeholder={props.placeholder}
+      onChange={(e) => {
+        console.log("hijo", e.target.value);
+        onChangeFunction(e.target.value);
+      }}
+    ></FormInput>
+  </Colcont>
+);
