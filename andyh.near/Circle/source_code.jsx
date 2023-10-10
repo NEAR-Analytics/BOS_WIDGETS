3dd16f9266a2d880d634f60f3fb101977c2cc405
andyh.near <andyh.near>
2023-10-10 20:24:36 +0000

    Update Circle by andyh.near at 2023-10-10T20:24:36.915Z

--- andyh.near/Circle/source_code.jsx
+++ andyh.near/Circle/source_code.jsx
@@ -15,7 +15,7 @@ return (
       style={{
         color: props.iconColor,
         position: "relative",
-        bottom: "calc(15%)",
+        bottom: "calc(50% - 16px)",
       }}
     />
   </div>
