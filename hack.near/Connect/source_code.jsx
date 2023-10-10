a3821a2d951a8a2ffeb9333a224159c062d1e7ed
hack.near <hack.near>
2023-10-10 17:33:52 +0000

    Update Connect by hack.near at 2023-10-10T17:33:52.938Z

--- hack.near/Connect/source_code.jsx
+++ hack.near/Connect/source_code.jsx
@@ -173,7 +173,7 @@ return (
       <CardStyled name="add">
         <div className="d-flex flex-column">
           <CardForm>
-            <div className="d-flex">
+            <div className="d-flex align-items-center justify-content-center">
               <H1>Builders</H1>
               <Submitcontainer>
                 <button
