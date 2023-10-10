39e8e4d470bbb77c3ee92803763bcef8d3a27504
kurodenjiro.near <kurodenjiro.near>
2023-10-10 08:28:40 +0000

    Update abi2form-widget-form by kurodenjiro.near at 2023-10-10T08:28:40.202Z

--- kurodenjiro.near/abi2form-widget-form/source_code.jsx
+++ kurodenjiro.near/abi2form-widget-form/source_code.jsx
@@ -1,5 +1,5 @@
 const user = context.accountId;
-const props = {"schema_version":"0.3.0","address":"chatme.near","metadata":{"name":"","version":"0.1.0","authors":[""]},"body":{"functions":[{"name":"a1","kind":"call","label":"Nhập ô","button":"Xác nhận","export":true,"params":{"serialization_type":"json","args":[]},"deposit":0,"gas":30000000000000},{"name":"a2","kind":"call","label":"Nhập B","button":"Xác nhận","export":true,"params":{"serialization_type":"json","args":[]},"deposit":0,"gas":30000000000000}]}} 
+const props = {"schema_version":"0.3.0","address":"chatme.near","metadata":{"name":"","version":"0.1.0","authors":[""]},"body":{"functions":[{"name":"hello","kind":"view","label":"Nhập ô","button":"Xác Nhập","export":true,"params":{"serialization_type":"json","args":[{"name":"Name","label":"Tên","type_schema":{"type":"string"},"value":""},{"name":"class","label":"Trường","type_schema":{"type":"string"},"value":""},{"name":"phone","label":"Số điện thoại","type_schema":{"type":"string"},"value":""}]},"deposit":0,"gas":30000000000000}]}} 
 
 return (
   <>
