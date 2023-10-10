cf7e604275e4908efe7c44ecc47a21c7ebe4ae89
agwaze.near <agwaze.near>
2023-10-10 20:14:35 +0000

    Update CreativeDAO.index by agwaze.near at 2023-10-10T20:14:35.813Z

--- agwaze.near/CreativeDAO.index/source_code.jsx
+++ agwaze.near/CreativeDAO.index/source_code.jsx
@@ -1,109 +1,13 @@
-const ownerId = "agwaze.near";
-const availableTabs = ["create", "list", "nearNFTs", "evmNFTs, ai, socials"];
-
-const getTab = (tab) => {
-  if (!tab || !availableTabs.includes(tab)) {
-    return "create";
-  }
-
-  return tab;
-};
-
-State.init({
-  tab: getTab(state.tab),
-});
-
-const showSidebar = ![].includes(state.tab);
-const isForm = [].includes(state.tab);
-
-const update = (state) => State.update(state);
-
-const Sidebar = styled.div`
-  display: ${({ show }) => (show ? "flex" : "none")};
-  flex-direction: row;
-  position: sticky;
-  top: 0;
-
-  @media screen and (max-width: 768px) {
-    & > div {
-      &:last-child {
-        display: none;
-      }
-    }
-  }
-
-  @media screen and (min-width: 768px) {
-    & > div {
-      &:first-child {
-        display: none;
-      }
-    }
-  }
-`;
-
-const Content = styled.div`
+const Root = styled.div`
   display: flex;
-  flex-direction: row;
-  position: relative;
   width: 100%;
-  margin-top: 40px;
-
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-  }
+  height: 500px;
 `;
-
-const ContentContainer = styled.div`
-  width: 100%;
-  background: #ffffff;
-  border: 1px solid #eceef0;
-  border-radius: 24px;
-  padding: 0;
-  overflow: hidden;
-
-  @media screen and (max-width: 768px) {
-    width: 100%;
-  }
-
-  &.form {
-    border: none;
-    background: #fafafa;
-  }
-
-  * {
-    margin: 0;
-  }
-`;
-
-//jgodwill.near/widget/GenaDrop.MultiListing
-const tabContentWidget = {
-  feeds: "genadrop.near/widget/GenaDrop.Home",
-  polling: "agwaze.near/widget/CreativeDAO.EasyPoll",
-  discussions: "agwaze.near/widget/CreativeDAO.Community",
-}[state.tab];
-
-const tabContent = <Widget src={tabContentWidget} props={{ update }} />;
 
 return (
-  <Content>
-    <Sidebar show={showSidebar}>
-      <Widget
-        src={`${ownerId}/widget/CreativeDAO.Navbar`}
-        props={{
-          tab: state.tab,
-          update,
-          collapsible: true,
-        }}
-      />
-      <Widget
-        src={`${ownerId}/widget/CreativeDAO.Navbar`}
-        props={{
-          tab: state.tab,
-          update,
-          collapsible: false,
-        }}
-      />
-    </Sidebar>
-    <ContentContainer>{tabContent}</ContentContainer>
-  </Content>
+  <>
+    <Widget src="agwaze.near/widget/CreativeDAO.Navbar" />
+    <Root></Root>
+    <Widget src="agwaze.near/widget/CreativeDAO.Footer" />
+  </>
 );
