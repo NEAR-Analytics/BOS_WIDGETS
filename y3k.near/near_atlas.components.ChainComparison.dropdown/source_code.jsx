90a7ad1468a538e11511a1e3319b2bd1ea2b292e
y3k.near <y3k.near>
2023-10-10 12:39:46 +0000

    Update near_atlas.components.ChainComparison.dropdown by y3k.near at 2023-10-10T12:39:46.137Z

---
+++ y3k.near/near_atlas.components.ChainComparison.dropdown/source_code.jsx
@@ -0,0 +1,47 @@
+const initialState = {
+  selectedWidget: "A", // "A" represents the MonthlyActiveAcounts and "B" represents MainChainActiveAcounts
+};
+
+State.init(initialState);
+
+const Style = styled.div`
+  /* Add any styles if needed */
+`;
+
+const handleDropdownChange = (e) => {
+  State.update({ selectedWidget: e.target.value });
+};
+
+return (
+  <Style>
+    <div className="bg-dark container rounded-4 p-3 mb-4">
+      <div>
+        <label
+          htmlFor="widget-dropdown"
+          className="text-white block text-sm font-medium"
+        ></label>
+        <select
+          id="widget-dropdown"
+          value={state.selectedWidget}
+          onChange={handleDropdownChange}
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring focus:border-blue-300 sm:text-sm rounded-md"
+        >
+          <option value="A">Daily Active Addresses (90 days)</option>
+          <option value="B">Daily Transactions (90 days)</option>
+        </select>
+
+        {state.selectedWidget === "A" ? (
+          <Widget
+            src="y3k.near/widget/near_atlas.components.DAAChainComparison"
+            props={{}}
+          />
+        ) : (
+          <Widget
+            src="y3k.near/widget/near_atlas.components.DTChainComparison"
+            props={{}}
+          />
+        )}
+      </div>
+    </div>
+  </Style>
+);
