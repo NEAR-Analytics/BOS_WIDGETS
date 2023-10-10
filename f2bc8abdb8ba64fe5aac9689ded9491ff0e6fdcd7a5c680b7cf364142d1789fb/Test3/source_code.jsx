b3af4d97a84cd436721f4d4479573353f787394b
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-10-10 21:37:39 +0000

    Update Test3 by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-10-10T21:37:39.450Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Test3/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Test3/source_code.jsx
@@ -1,166 +1,35 @@
-const isDebug = props.isDebug;
-const addressForArticles = isDebug ? "test_sayALotArticle" : "sayALotArticle";
-const author = props.author;
+const accountId = "ayelen.near";
+const sbtsNames = ["fractal.i-am-human.near - class 1"];
 
-const writersWhiteList = props.writersWhiteList ?? [
-  "neardigitalcollective.near",
-  "blaze.near",
-  "jlw.near",
-  "kazanderdad.near",
-  "joep.near",
-  "sarahkornfeld.near",
-  "yuensid.near",
-  "shubham007.near",
-  "fiftycent.near",
-  "ozymandius.near",
-];
+console.log("aID: ", accountId);
+console.log("sbtsNames: ", sbtsNames);
 
-const sayALotWorkers = [
-  "silkking.near",
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
-  "blaze.near",
-  "ayelen.near",
-  "kenrou-it.near",
-];
+const userSBTs = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
+  account: accountId,
+});
+console.log("userSBTs: ", userSBTs);
 
-if (isDebug) {
-  writersWhiteList = sayALotWorkers;
-}
+const sbtsData = sbtsNames.map((sbt) => {
+  console.log("SBT: ", sbt);
+  const data = sbt.split(" - class ");
+  console.log("split: ", data);
+  return { name: data[0], classNumber: data[1] };
+});
 
-const articleBlackList = [91092435, 91092174, 91051228, 91092223, 91051203];
+console.log("sbtsData: ", sbtsData);
 
-function getLastEditionsByArticle() {
-  const allArticles = Social.index(addressForArticles, "main", {
-    order: "desc",
-    accountId: author,
-  });
+const sbtsFiltered = userSBTs.filter((sbt) => {
+  return sbt[0] === sbtsData[0].name;
+});
 
-  const oldFormatArticlesTestBasicDataArray = [
-    [
-      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
-      97325392,
-    ],
-    [
-      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
-      97317287,
-    ],
-    ["ayelen.near", 96927579],
-    ["kenrou-it.near", 96924422],
-    [
-      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
-      96879470,
-    ],
-    [
-      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
-      96878182,
-    ],
-    [
-      "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb",
-      96643643,
-    ],
-    ["silkking.near", 96491128],
-  ];
+console.log("sbtsFiltered: ", sbtsFiltered);
 
-  const oldFormatArticlesMainBasicDataArray = [
-    ["ozymandius.near", 97329049],
-    ["fiftycent.near", 97322138],
-    ["blaze.near", 97255023],
-    ["jlw.near", 97250015],
-    ["kazanderdad.near", 96692435],
-    ["blaze.near", 96414482],
-    ["blaze.near", 96412953],
-    ["sarahkornfeld.near", 96402919],
-    ["sarahkornfeld.near", 96402476],
-    ["sarahkornfeld.near", 96402330],
-    ["sarahkornfeld.near", 96401880],
-    ["ozymandius.near", 95810612],
-    ["blaze.near", 95766756],
-    ["blaze.near", 95766700],
-    ["jlw.near", 95705034],
-    ["blaze.near", 95413943],
-    ["blaze.near", 94936576],
-    ["yuensid.near", 94866690],
-    ["sarahkornfeld.near", 94863580],
-    ["blaze.near", 94801223],
-    ["sarahkornfeld.near", 94344236],
-    ["sarahkornfeld.near", 94188387],
-    ["jlw.near", 93986868],
-    ["blaze.near", 92999498],
-  ];
+const result =
+  sbtsFiltered[0][1].find((sbt) => {
+    console.log(1, "SBT: ", sbt);
+    return Number(sbt.metadata["class"]) === Number(sbtsData[0].classNumber);
+  }) !== undefined;
 
-  const oldFormatArticlesBasicDataArray = isDebug
-    ? oldFormatArticlesTestBasicDataArray
-    : oldFormatArticlesMainBasicDataArray;
+console.log("result: ", result);
 
-  if (author) {
-    oldFormatArticlesBasicDataArray = oldFormatArticlesBasicDataArray.filter(
-      (articleBasicData) => articleBasicData[0] === author
-    );
-  }
-
-  let oldFormatArticlesArray = oldFormatArticlesBasicDataArray.map(
-    (oldFormatArticleBasicData) => {
-      let article = Social.get(
-        `${oldFormatArticleBasicData[0]}/${addressForArticles}/main`,
-        oldFormatArticleBasicData[1]
-      );
-
-      let articleParsed = JSON.parse(article);
-      if (articleParsed) {
-        articleParsed.blockHeight = oldFormatArticleBasicData[1];
-      }
-
-      return articleParsed;
-    }
-  );
-
-  let newFormatArticlesData = allArticles
-    .filter((articleIndex) => articleIndex.value.id)
-    .filter(
-      (articleIndex) =>
-        articleIndex.value.id.split("-")[0] === articleIndex.accountId
-    )
-    .filter((articleIndex) => writersWhiteList.includes(articleIndex.accountId))
-    .filter(
-      (articleIndex) => !articleBlackList.includes(articleIndex.blockHeight)
-    );
-
-  let lastestEditArticlesDataArray = newFormatArticlesData.filter(
-    (articleData) => {
-      const latestEditForThisArticle = newFormatArticlesData.find(
-        (newArticleData) => newArticleData.value.id
-      );
-      return (
-        JSON.stringify(articleData) === JSON.stringify(latestEditForThisArticle)
-      );
-    }
-  );
-
-  let finalNewFormatArticles = lastestEditArticlesDataArray.map(
-    (lastestEditArticles) => {
-      const article = Social.get(
-        `${lastestEditArticles.accountId}/${addressForArticles}/main`,
-        lastestEditArticles.blockHeight
-      );
-
-      return JSON.parse(article);
-    }
-  );
-
-  let finalOldFormatArticles = oldFormatArticlesArray.filter(
-    (oldFormatArticle) => {
-      return !finalNewFormatArticles.find(
-        (newFormatArticle) =>
-          newFormatArticle.articleId === oldFormatArticle.articleId
-      );
-    }
-  );
-
-  let finalArticles = finalNewFormatArticles.concat(finalOldFormatArticles);
-
-  return finalArticles;
-}
-
-console.log(getLastEditionsByArticle());
-
-return <div>{JSON.stringify(getLastEditionsByArticle().length)}</div>;
+return <></>;
