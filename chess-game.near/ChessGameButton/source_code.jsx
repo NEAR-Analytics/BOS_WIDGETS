dc4f6f5429f70b6eeda8d39e1edef8464502a703
chess-game.near <chess-game.near>
2023-10-10 18:54:18 +0000

    Update ChessGameButton by chess-game.near at 2023-10-10T18:54:18.374Z

--- chess-game.near/ChessGameButton/source_code.jsx
+++ chess-game.near/ChessGameButton/source_code.jsx
@@ -1,8 +1,17 @@
-const { onClick, content, inline, fontSize, alignSelf, fontWeight } = props;
+const {
+  onClick,
+  content,
+  inline,
+  fontSize,
+  alignSelf,
+  fontWeight,
+  flexDirection,
+} = props;
 
 const Button = styled.button`
   display: ${inline ? "inline-flex" : "flex"};
   gap: 0.5rem;
+  flex-direction: ${flexDirection ? flexDirection : "row"};
   align-self: ${alignSelf ? alignSelf : "unset"};
   border-radius: 4px;
   border: 1px solid rgb(68, 152, 224);
