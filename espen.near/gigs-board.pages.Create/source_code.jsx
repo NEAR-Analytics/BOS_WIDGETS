a0683ab7b6b9ff7ac4d00ffd7dc0edd2efd7cc10
espen.near <espen.near>
2023-10-11 16:29:27 +0000

    Update gigs-board.pages.Create by espen.near at 2023-10-11T16:29:27.269Z

--- espen.near/gigs-board.pages.Create/source_code.jsx
+++ espen.near/gigs-board.pages.Create/source_code.jsx
@@ -413,13 +413,15 @@ const fundraisingDiv = (
       Currency
       <select
         onChange={(event) => State.update({ token: event.target.value })}
-        class="form-select"
-        aria-label="Default select"
+        className="form-select"
+        aria-label="Select currency"
+        value={state.token}
       >
         <option selected value="USDT">
           USDT
         </option>
         <option value="NEAR">NEAR</option>
+        <option value="USDC">USDC</option>
       </select>
     </div>
     <div className="col-lg-6 mb-2">
