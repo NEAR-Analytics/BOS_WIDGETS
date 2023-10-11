88c720bcece9635a1965ba5b4bdd65282b72fd1a
espen.near <espen.near>
2023-10-11 16:25:43 +0000

    Update gigs-board.entity.post.Post by espen.near at 2023-10-11T16:25:43.503Z

--- espen.near/gigs-board.entity.post.Post/source_code.jsx
+++ espen.near/gigs-board.entity.post.Post/source_code.jsx
@@ -474,11 +474,6 @@ const tokenMapping = {
       address: "usdt.tether-token.near",
     },
   },
-  USDC: {
-    NEP141: {
-      address: "usdc.tether-token.near",
-    },
-  },
   // Add more tokens here as needed
 };
 
@@ -569,7 +564,7 @@ function Editor() {
               name: post.snapshot.name,
               description: post.snapshot.description,
               amount: post.snapshot.amount,
-              token: tokenResolver(post.snapshot.sponsorship_token),
+              token: post.snapshot.sponsorship_token,
               supervisor: post.snapshot.supervisor,
               githubLink: post.snapshot.github_link,
               onDraftStateChange: props.onDraftStateChange,
