dff83e0593e5596bd51b6f731dd3186f27f88fbb
agwaze.near <agwaze.near>
2023-10-11 06:08:40 +0000

    Update CPlanet.index by agwaze.near at 2023-10-11T06:08:40.322Z

---
+++ agwaze.near/CPlanet.index/source_code.jsx
@@ -0,0 +1,13 @@
+const Root = styled.div`
+  display: flex;
+  width: 100%;
+  height: 500px;
+`;
+
+return (
+  <>
+    <Widget src="agwaze.near/widget/CreativeDAO.Navbar" />
+    <Root></Root>
+    <Widget src="agwaze.near/widget/CreativeDAO.Footer" />
+  </>
+);
