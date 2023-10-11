f588c4f8e7aa0534085ff8795edf0d4262da4060
andyh.near <andyh.near>
2023-10-11 18:17:06 +0000

    Update HooksDemo by andyh.near at 2023-10-11T18:17:06.246Z

--- andyh.near/HooksDemo/source_code.jsx
+++ andyh.near/HooksDemo/source_code.jsx
@@ -29,6 +29,21 @@ return (
     <input type="text" onChange={(e) => setText(e.target.value)} />
 
     <div class="input-group mb-3">
+      <button
+        class="btn btn-outline-secondary"
+        type="button"
+        id="button-addon1"
+      >
+        Delay
+      </button>
+      <input
+        type="text"
+        class="form-control"
+        placeholder=""
+        aria-label="Example text with button addon"
+        aria-describedby="button-addon1"
+      />
+
       <label class="input-group-text" for="inputGroupSelect01">
         Options
       </label>
