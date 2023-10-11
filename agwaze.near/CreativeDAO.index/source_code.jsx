e888bcf5971978a435c2ab7bfe47ad3b6a9668dd
agwaze.near <agwaze.near>
2023-10-11 09:07:34 +0000

    Update CreativeDAO.index by agwaze.near at 2023-10-11T09:07:34.178Z

--- agwaze.near/CreativeDAO.index/source_code.jsx
+++ agwaze.near/CreativeDAO.index/source_code.jsx
@@ -90,7 +90,7 @@ const tabContent = <Widget src={tabContentWidget} props={{ update }} />;
 
 return (
   <Root>
-    <h1>Drop Flow Skeletons</h1>
+    <h1>Drop Flow</h1>
     <Content>
       <Sidebar show={showSidebar}>
         <Widget
