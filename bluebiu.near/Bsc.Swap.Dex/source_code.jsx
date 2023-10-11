16f4edaa9be199d4d2634c4c214eb52348baec78
bluebiu.near <bluebiu.near>
2023-10-11 04:29:49 +0000

    Update Bsc.Swap.Dex by bluebiu.near at 2023-10-11T04:29:49.256Z

--- bluebiu.near/Bsc.Swap.Dex/source_code.jsx
+++ bluebiu.near/Bsc.Swap.Dex/source_code.jsx
@@ -16,6 +16,61 @@ const Tokens = {
     name: "Binance-Peg Ethereum Token",
     icon: "https://ipfs.near.social/ipfs/bafkreibspnls7q67q25r2ifv2rrfmvzl744pzuh3s5ekigeqkmyycl2auq",
   },
+
+  "0xF4C8E32EaDEC4BFe97E0F595AdD0f4450a863a11": {
+    chainId: CHAIN_ID,
+    address: "0xF4C8E32EaDEC4BFe97E0F595AdD0f4450a863a11",
+    decimals: 18,
+    symbol: "THE",
+    name: "THENA",
+    icon: "https://ipfs.near.social/ipfs/bafkreig4qvfooeyjhjhfobueyipuxxzxdxf5cqycsup35wll6qc3qijghi",
+  },
+
+  "0xCdC3A010A3473c0C4b2cB03D8489D6BA387B83CD": {
+    chainId: CHAIN_ID,
+    address: "0xCdC3A010A3473c0C4b2cB03D8489D6BA387B83CD",
+    decimals: 18,
+    symbol: "liveTHE",
+    name: "liveThe",
+    icon: "https://ipfs.near.social/ipfs/bafkreicsajtjaej7icx44jaflxswdkdlpbb62ywtucfsgvd4drhwn5smqy",
+  },
+
+  "0x52F24a5e03aee338Da5fd9Df68D2b6FAe1178827": {
+    chainId: CHAIN_ID,
+    address: "0x52F24a5e03aee338Da5fd9Df68D2b6FAe1178827",
+    decimals: 18,
+    symbol: "ankrBNB",
+    name: "Ankr Staked BNB",
+    icon: "https://ipfs.near.social/ipfs/bafkreiazxoiq645ur3vv2cyc2mhh47ke3bhlahg3hccka64q6s4ydnvit4",
+  },
+
+  "0x64048A7eEcF3a2F1BA9e144aAc3D7dB6e58F555e": {
+    chainId: CHAIN_ID,
+    address: "0x64048A7eEcF3a2F1BA9e144aAc3D7dB6e58F555e",
+    decimals: 18,
+    symbol: "frxETH",
+    name: "Frax Ether",
+    icon: "https://ipfs.near.social/ipfs/bafkreig2u6c72b4gy5nu3d57xlqzjb6ycf2luqnofdzq6btqzav3tqrqnq",
+  },
+
+  "0x1bdd3Cf7F79cfB8EdbB955f20ad99211551BA275": {
+    chainId: CHAIN_ID,
+    address: "0x1bdd3Cf7F79cfB8EdbB955f20ad99211551BA275",
+    decimals: 18,
+    symbol: "BNBx",
+    name: "Liquid Staking BNB",
+    icon: "https://ipfs.near.social/ipfs/bafkreiaw2yydqsipgor2jilgogehuitxhe7hodgw5fyi5j2q7zfhpau22y",
+  },
+
+  "0xc2E9d07F66A89c44062459A47a0D2Dc038E4fb16": {
+    chainId: CHAIN_ID,
+    address: "0xc2E9d07F66A89c44062459A47a0D2Dc038E4fb16",
+    decimals: 18,
+    symbol: "stkBNB",
+    name: "Staked BNB",
+    icon: "https://ipfs.near.social/ipfs/bafkreideov3wvuqwgwu5irionwnkka7jqeflqoedzqpf6m2ylqewmto4ei",
+  },
+
   "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c": {
     chainId: CHAIN_ID,
     address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
@@ -210,9 +265,35 @@ return (
               Tokens["0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"],
             ],
           },
+          "THENA V1": {
+            name: "THENA V1",
+            logo: "https://ipfs.near.social/ipfs/bafkreidps2jipljarabsxbqtiuj6lyupfhvpsesiffmsjzjt4zb73r7qfq",
+            factoryAddress: "0xAAA35aaEa18B0187E82A3A7f2996C9ee7Bad9696",
+            routerAddress: "0xd4ae6eca985340dd434d38f470accce4dc78d109",
+            uniType: "solidly",
+            defaultCurrencies: {
+              input: Tokens["native"],
+              output: Tokens["0x55d398326f99059fF775485246999027B3197955"],
+            },
+            tokens: [
+              Tokens["native"],
+              Tokens["0xF4C8E32EaDEC4BFe97E0F595AdD0f4450a863a11"],
+              Tokens["0x55d398326f99059fF775485246999027B3197955"],
+              Tokens["0xCdC3A010A3473c0C4b2cB03D8489D6BA387B83CD"],
+              Tokens["0x52F24a5e03aee338Da5fd9Df68D2b6FAe1178827"],
+              Tokens["0x2170Ed0880ac9A755fd29B2688956BD959F933F8"],
+              Tokens["0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"],
+              Tokens["0x64048A7eEcF3a2F1BA9e144aAc3D7dB6e58F555e"],
+              Tokens["0x1bdd3Cf7F79cfB8EdbB955f20ad99211551BA275"],
+              Tokens["0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c"],
+              Tokens["0xc2E9d07F66A89c44062459A47a0D2Dc038E4fb16"],
+            ],
+          },
         },
         amountOutFn: "bluebiu.near/widget/Arbitrum.Swap.AmountOutV2",
         handlerV2: "bluebiu.near/widget/Arbitrum.Swap.HandlerV2",
+        QuoterSolidly: "bluebiu.near/widget/Arbitrum.Swap.QuoterSolidly",
+        handlerSolidly: "bluebiu.near/widget/Arbitrum.Swap.handlerSolidly",
       }}
     />
   </Container>
