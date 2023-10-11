eece50a74c214440eeee77f08e4417156acc26f9
a_liutiev.near <a_liutiev.near>
2023-10-11 05:11:54 +0000

    Update encodexnear23-uniswap by a_liutiev.near at 2023-10-11T05:11:54.101Z

--- a_liutiev.near/encodexnear23-uniswap/source_code.jsx
+++ a_liutiev.near/encodexnear23-uniswap/source_code.jsx
@@ -50,15 +50,6 @@ if (!state.theme) {
 const Theme = state.theme;
 const web3connectLabel = state.web3connectLabel || "n/a";
 
-// FRONT END CONTROLS
-// const handleIncrement = () => {
-//   State.update((prev) => ({ price: prev.price + 1 }));
-// };
-
-// const handleDecrement = () => {
-//   if (state.price > 0) State.update((prev) => ({ price: prev.price - 1 }));
-// };
-
 const handleMaxClick = () => {
   console.log("MAX clicked!");
   // Add functionality for max click here
@@ -69,67 +60,8 @@ const handleApprove = () => {
   // Add functionality for approve here
 };
 
-const updateOptionPrice = (name, newPrice) => {
-  const updatedOptions = state.options.map((option) => {
-    if (option.name === name) {
-      return { ...option, price: newPrice };
-    }
-    return option;
-  });
-  State.update({ options: updatedOptions });
-};
-
 const toggleShowButtons = () => {
   State.update({ showButtons: !state.showButtons });
-};
-
-// HELPER FUNCTIONS/STATE
-if (state.txCost === undefined) {
-  const gasEstimate = ethers.BigNumber.from(1875000);
-  const gasPrice = ethers.BigNumber.from(1500000000);
-
-  const gasCostInWei = gasEstimate.mul(gasPrice);
-  const gasCostInEth = ethers.utils.formatEther(gasCostInWei);
-
-  let responseGql = fetch(
-    "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: `{
-          bundle(id: "1" ) {
-            ethPrice
-          }
-        }`,
-      }),
-    }
-  );
-
-  if (!responseGql) return "";
-
-  const ethPriceInUsd = responseGql.body.data.bundle.ethPrice;
-
-  const txCost = Number(gasCostInEth) * Number(ethPriceInUsd);
-
-  State.update({ txCost: `$${txCost.toFixed(2)}` });
-}
-
-if (state.sender === undefined) {
-  const accounts = Ethers.send("eth_requestAccounts", []);
-  if (accounts.length) {
-    State.update({ sender: accounts[0] });
-    console.log("set sender", accounts[0]);
-  }
-}
-
-// getters
-const getSender = () => {
-  return !state.sender
-    ? ""
-    : state.sender.substring(0, 6) +
-        "..." +
-        state.sender.substring(state.sender.length - 4, state.sender.length);
 };
 
 return (
@@ -171,7 +103,7 @@ return (
         </div>
         <div class="card-footer">
           <div className="centered-container">
-            <p>{getSender()}</p>
+            <p>{}</p>
           </div>
         </div>
       </div>
@@ -209,28 +141,14 @@ return (
                 );
                 State.update({ fruitSelection1: selectedOption });
               }}
-            >
-              {state.options.map((option) => (
-                <option value={option.name} key={option.name}>
-                  {option.name}
-                </option>
-              ))}
-            </select>
+            ></select>
 
             <select
               value={state.fruitSelection2}
               onChange={(e) =>
                 State.update({ fruitSelection2: e.target.value })
               }
-            >
-              {state.options
-                .filter((option) => option.name === state.fruitSelection2.name)
-                .map((option) => (
-                  <option value={option.name} key={option.name}>
-                    {option.name}
-                  </option>
-                ))}
-            </select>
+            ></select>
           </div>
           <div className="container">
             <input
@@ -305,7 +223,7 @@ return (
             </a>
           </div>
           <div>
-            <p>Estimated Transaction Cost: {state.txCost}</p>
+            <p>Estimated Transaction Cost: {}</p>
           </div>
         </div>
 
