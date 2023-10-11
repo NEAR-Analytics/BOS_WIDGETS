cc14e962604f803f90240b654a3e8d91b5380c9b
scopalaffairs.near <scopalaffairs.near>
2023-10-11 09:59:45 +0000

    Update Recommender.Views.TrendingUsersSidebar by scopalaffairs.near at 2023-10-11T09:59:45.621Z

--- scopalaffairs.near/Recommender.Views.TrendingUsersSidebar/source_code.jsx
+++ scopalaffairs.near/Recommender.Views.TrendingUsersSidebar/source_code.jsx
@@ -42,7 +42,7 @@ return (
   <>
     <TrendingUsersView>
       <Widget
-        src={`${REPL_ACCOUNT}/widget/Recommender.Service.RecommendedUsers`}
+        src={`scopalaffairs.near/widget/Recommender.Service.RecommendedUsers`}
         props={{
           dataset: dataset,
           sidebar: props.sidebar,
@@ -54,7 +54,7 @@ return (
     </TrendingUsersView>
     {props.sidebar && (
       <>
-        <ButtonLink href="#/${REPL_ACCOUNT}/widget/PeoplePage?tab=trending">
+        <ButtonLink href="#/scopalaffairs.near/widget/PeoplePage?tab=trending">
           View Trending Users
         </ButtonLink>
       </>
