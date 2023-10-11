f7eb293f84a93643bc818081bddf22de32bf5d57
agwaze.near <agwaze.near>
2023-10-11 06:53:10 +0000

    Update CreativeDAO.index by agwaze.near at 2023-10-11T06:53:10.816Z

--- agwaze.near/CreativeDAO.index/source_code.jsx
+++ agwaze.near/CreativeDAO.index/source_code.jsx
@@ -1,13 +1,110 @@
-const Root = styled.div`
-  display: flex;
-  width: 100%;
-  height: 500px;
+const ownerId = "agwaze.near";
+const availableTabs = ["create", "list", "nearNFTs", "evmNFTs, ai, socials"];
+
+const getTab = (tab) => {
+  if (!tab || !availableTabs.includes(tab)) {
+    return "create";
+  }
+
+  return tab;
+};
+
+State.init({
+  tab: getTab(state.tab),
+});
+
+const showSidebar = ![].includes(state.tab);
+const isForm = [].includes(state.tab);
+
+const update = (state) => State.update(state);
+
+const Sidebar = styled.div`
+  display: ${({ show }) => (show ? "flex" : "none")};
+  flex-direction: row;
+  position: sticky;
+  top: 0;
+
+  @media screen and (max-width: 768px) {
+    & > div {
+      &:last-child {
+        display: none;
+      }
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    & > div {
+      &:first-child {
+        display: none;
+      }
+    }
+  }
 `;
 
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  width: 100%;
+  margin-top: 40px;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const ContentContainer = styled.div`
+  width: 100%;
+  background: #ffffff;
+  border: 1px solid #eceef0;
+  border-radius: 24px;
+  padding: 0;
+  overflow: hidden;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+
+  &.form {
+    border: none;
+    background: #fafafa;
+  }
+
+  * {
+    margin: 0;
+  }
+`;
+
+//jgodwill.near/widget/GenaDrop.MultiListing
+const tabContentWidget = {
+  feeds: "https://near.social/astraplusplus.ndctools.near/widget/Feed.index",
+  discussion: "https://near.social/mob.near/widget/N.Group?groupId=nft.genadrop.near",
+  collaboration: "https://near.org/nearhorizon.near/widget/Index?tab=profile&content=founders",
+  polling: "agwaze.near/widget/CreativeDAO.EasyPoll",
+}[state.tab];
+
+const tabContent = <Widget src={tabContentWidget} props={{ update }} />;
+
 return (
-  <>
-    <Widget src="agwaze.near/widget/CreativeDAO.Navbar" />
-    <Root></Root>
-    <Widget src="agwaze.near/widget/CreativeDAO.Footer" />
-  </>
+  <Content>
+    <Sidebar show={showSidebar}>
+      <Widget
+        src={`${ownerId}/widget/GenaDrop.NavBar`}
+        props={{
+          tab: state.tab,
+          update,
+          collapsible: true,
+        }}
+      />
+      <Widget
+        src={`${ownerId}/widget/GenaDrop.NavBar`}
+        props={{
+          tab: state.tab,
+          update,
+          collapsible: false,
+        }}
+      />
+    </Sidebar>
+    <ContentContainer>{tabContent}</ContentContainer>
+  </Content>
 );
