25326a794443617174d4e37e195e4462cc08d6b5
relayer-demo.near <relayer-demo.near>
2023-10-11 14:39:48 +0000

    Update RelayerJokes by relayer-demo.near at 2023-10-11T14:39:48.751Z

--- relayer-demo.near/RelayerJokes/source_code.jsx
+++ relayer-demo.near/RelayerJokes/source_code.jsx
@@ -1,1 +1,58 @@
-return <div>Helo from Relayer Jokes</div>;
+State.init({
+  jokes: [],
+  newMessage: "",
+});
+
+const contract = "guest-book.near";
+const messages = Near.view(
+  contract,
+  "getMessages",
+  {}
+).reverse();
+
+const addNewMessage = () => {
+  if (state.newMessage.trim() === "") {
+    console.log("No message to add");
+    return;
+  }
+
+  Near.call(contract, "addMessage", {
+    text: state.newMessage,
+  });
+  // State.update({ newMessage: "" });
+};
+
+return (
+  <div>
+    <h1>Relayer Jokes</h1>
+    <Widget
+      src="near/widget/DIG.Input"
+      props={{
+        placeholder: "Post your joke or just say GM!",
+        onInput: async (e) =>
+          State.update({ newMessage: e.target.value }),
+        value: state.newMessage,
+      }}
+    />
+    <Widget
+      src="near/widget/DIG.Button"
+      props={{
+        label: "Submit",
+        onClick: (e) => addNewMessage(e),
+        size: "large",
+      }}
+    />
+    <div>
+      <ul>
+        {messages.map((data, key) => {
+          return (
+            <li class="text-center">
+              <div>{data.sender}</div>
+              <div>{data.text}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  </div>
+);
