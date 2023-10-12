ebf3c5b1468205cbffa16895d6e95dfd3a5ed8cd
nftwg_home.near <nftwg_home.near>
2023-10-12 17:51:40 +0000

    Update OurGoals1 by nftwg_home.near at 2023-10-12T17:51:40.002Z

---
+++ nftwg_home.near/OurGoals1/source_code.jsx
@@ -0,0 +1,234 @@
+const OurGoalsContainer = styled.div`
+position: absolute;
+width: 1440px;
+height: 1551px;
+left: 0px;
+top: 2500px;
+
+
+`;
+const StarForOurGoals = styled.div`
+
+position: absolute;
+width: 112px;
+height: 112px;
+left: 960px;
+top: 50px;
+
+`;
+
+const OurGoals = styled.div`
+/* Our Goals */
+
+position: absolute;
+width: 280px;
+height: 72px;
+left: calc(50% - 280px/2);
+top: 100px;
+
+font-family: inherit;
+font-style: normal;
+font-weight: 700;
+font-size: 60px;
+line-height: 120%;
+/* identical to box height, or 72px */
+text-align: center;
+white-space: nowrap;
+
+color: #333333;
+
+
+
+`;
+const Container = styled.div`
+position: absolute;
+width: 610px;
+height: 243.99px;
+left: 30px;
+top: 2.01px;
+`;
+
+const IconContainer = styled.div`
+
+/* Container */
+
+position: absolute;
+width: 80px;
+height: 80px;
+left: 80px;
+top: 300px;
+
+background: #FFB6AB;
+border-radius: 9.19427px;
+
+`;
+
+const Icon = styled.div`
+position: absolute;
+width: 80px;
+height: 80px;
+left: 20px;
+top: 21.01px;
+
+`;
+const TextOurGoalsContainer = styled.div`
+position: absolute;
+width: 610px;
+height: 134px;
+left: 413px;
+top: 400px;
+`;
+
+const TextOurGoals = styled.div`
+position: absolute;
+width: 291px;
+height: 24px;
+left: calc(50% - 291px/2 - 494.5px);
+top: 22px;
+
+font-family: inherit;
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 120%;
+/* identical to box height, or 24px */
+white-space:nowrap;
+color: #333333;
+`;
+
+return (
+  <OurGoalsContainer>
+    <Container style={{ left: 30, top: 400 }}>
+      <IconContainer style={{ background: "#967CFF" }}>
+        <Icon>
+          <svg
+            width="43"
+            height="43"
+            viewBox="0 0 43 43"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_144_136)">
+              <path
+                d="M34.7175 13.8611C34.1055 10.8647 32.4726 8.17356 30.0975 6.24698C27.7224 4.32041 24.7523 3.27781 21.6941 3.29714C18.6359 3.31647 15.6793 4.39652 13.3287 6.35296C10.9781 8.30941 9.37941 11.021 8.80536 14.0248C6.36783 14.4587 4.18134 15.7902 2.67708 17.7566C1.17282 19.7231 0.459983 22.1818 0.679142 24.6479C0.898301 27.114 2.03355 29.4085 3.86101 31.0789C5.68847 32.7493 8.07549 33.6743 10.5513 33.6715H11.2117V31.0301H10.5513C8.6661 31.035 6.85293 30.3063 5.49549 28.9981C4.13806 27.6898 3.34286 25.9048 3.27817 24.0207C3.21347 22.1366 3.88437 20.3012 5.14888 18.903C6.41339 17.5047 8.1723 16.6533 10.0534 16.5289L11.1588 16.4536L11.2777 15.3535C11.5452 12.8968 12.6661 10.6118 14.4452 8.89657C16.2243 7.18137 18.5487 6.14468 21.0135 5.96712C23.4784 5.78955 25.9274 6.48237 27.9339 7.92486C29.9404 9.36735 31.3773 11.4681 31.9942 13.8611H34.7175Z"
+                fill="white"
+              />
+              <path
+                d="M37.6247 16.5015H24.4178C23.7174 16.5022 23.046 16.7807 22.5508 17.2759C22.0556 17.7711 21.7771 18.4425 21.7764 19.1429V24.4257H16.4936C15.7932 24.4264 15.1218 24.7049 14.6266 25.2001C14.1314 25.6953 13.8529 26.3667 13.8522 27.067V40.274H40.2661V19.1429C40.2654 18.4425 39.9869 17.7711 39.4917 17.2759C38.9965 16.7807 38.3251 16.5022 37.6247 16.5015ZM16.4936 37.6326V27.067H21.7764V37.6326H16.4936ZM37.6247 37.6326H24.4178V19.1429H37.6247V37.6326Z"
+                fill="white"
+              />
+              <path
+                d="M27.0591 21.7842H29.7005V27.067H27.0591V21.7842Z"
+                fill="white"
+              />
+              <path
+                d="M32.3419 21.7842H34.9833V27.067H32.3419V21.7842Z"
+                fill="white"
+              />
+              <path
+                d="M27.0591 29.7085H29.7005V34.9913H27.0591V29.7085Z"
+                fill="white"
+              />
+              <path
+                d="M32.3419 29.7085H34.9833V34.9913H32.3419V29.7085Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_144_136">
+                <rect
+                  width="42.2623"
+                  height="42.2623"
+                  fill="white"
+                  transform="translate(0.645142 0.653076)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </Icon>
+      </IconContainer>
+      <TextOurGoalsContainer>
+        <TextOurGoals>Infrastructure : Internal Development</TextOurGoals>
+        <TextOurGoals
+          style={{
+            top: 50,
+            whiteSpace: "normal",
+            fontWeight: 400,
+            width: 610,
+            lineHeight: 2,
+          }}
+        >
+          Building sustainable innovations under the NFT WG brand to provide
+          end-to-end resources to new businesses to kickstart their NFT journey
+          on NEAR & acquiring existing infrastructure from partners.
+        </TextOurGoals>
+      </TextOurGoalsContainer>
+    </Container>
+    <Container style={{ left: 750, top: 400 }}>
+      <IconContainer style={{ background: "B5E4CA" }}>
+        <Icon>
+          <svg
+            width="43"
+            height="43"
+            viewBox="0 0 43 43"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_144_136)">
+              <path
+                d="M34.7175 13.8611C34.1055 10.8647 32.4726 8.17356 30.0975 6.24698C27.7224 4.32041 24.7523 3.27781 21.6941 3.29714C18.6359 3.31647 15.6793 4.39652 13.3287 6.35296C10.9781 8.30941 9.37941 11.021 8.80536 14.0248C6.36783 14.4587 4.18134 15.7902 2.67708 17.7566C1.17282 19.7231 0.459983 22.1818 0.679142 24.6479C0.898301 27.114 2.03355 29.4085 3.86101 31.0789C5.68847 32.7493 8.07549 33.6743 10.5513 33.6715H11.2117V31.0301H10.5513C8.6661 31.035 6.85293 30.3063 5.49549 28.9981C4.13806 27.6898 3.34286 25.9048 3.27817 24.0207C3.21347 22.1366 3.88437 20.3012 5.14888 18.903C6.41339 17.5047 8.1723 16.6533 10.0534 16.5289L11.1588 16.4536L11.2777 15.3535C11.5452 12.8968 12.6661 10.6118 14.4452 8.89657C16.2243 7.18137 18.5487 6.14468 21.0135 5.96712C23.4784 5.78955 25.9274 6.48237 27.9339 7.92486C29.9404 9.36735 31.3773 11.4681 31.9942 13.8611H34.7175Z"
+                fill="white"
+              />
+              <path
+                d="M37.6247 16.5015H24.4178C23.7174 16.5022 23.046 16.7807 22.5508 17.2759C22.0556 17.7711 21.7771 18.4425 21.7764 19.1429V24.4257H16.4936C15.7932 24.4264 15.1218 24.7049 14.6266 25.2001C14.1314 25.6953 13.8529 26.3667 13.8522 27.067V40.274H40.2661V19.1429C40.2654 18.4425 39.9869 17.7711 39.4917 17.2759C38.9965 16.7807 38.3251 16.5022 37.6247 16.5015ZM16.4936 37.6326V27.067H21.7764V37.6326H16.4936ZM37.6247 37.6326H24.4178V19.1429H37.6247V37.6326Z"
+                fill="white"
+              />
+              <path
+                d="M27.0591 21.7842H29.7005V27.067H27.0591V21.7842Z"
+                fill="white"
+              />
+              <path
+                d="M32.3419 21.7842H34.9833V27.067H32.3419V21.7842Z"
+                fill="white"
+              />
+              <path
+                d="M27.0591 29.7085H29.7005V34.9913H27.0591V29.7085Z"
+                fill="white"
+              />
+              <path
+                d="M32.3419 29.7085H34.9833V34.9913H32.3419V29.7085Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_144_136">
+                <rect
+                  width="42.2623"
+                  height="42.2623"
+                  fill="white"
+                  transform="translate(0.645142 0.653076)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </Icon>
+      </IconContainer>
+      <TextOurGoalsContainer>
+        <TextOurGoals>Infrastructure : External Projects</TextOurGoals>
+        <TextOurGoals
+          style={{
+            top: 50,
+            whiteSpace: "normal",
+            fontWeight: 400,
+            width: 610,
+            lineHeight: 2,
+          }}
+        >
+          We support builders who create and deploy applications around NFTs on
+          NEAR by offering guidance and financial resources subject to approval
+          from NDC’s House of Merit.
+        </TextOurGoals>
+      </TextOurGoalsContainer>
+    </Container>
+  </OurGoalsContainer>
+);
