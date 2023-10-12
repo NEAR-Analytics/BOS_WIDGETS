157704f1f26fc02f65fbfe28b761136a60f227a4
nftwg_home.near <nftwg_home.near>
2023-10-12 18:34:39 +0000

    Update DaapCentral by nftwg_home.near at 2023-10-12T18:34:39.916Z

---
+++ nftwg_home.near/DaapCentral/source_code.jsx
@@ -0,0 +1,341 @@
+const DaapCentralContainer = styled.div`
+
+position:absolute;
+width:1600px;
+height:1338px;
+top:5400px;
+background: #FFFBDA;
+
+`;
+
+const DaapCentralHeading = styled.div`
+width: 867px
+height: 168px
+top: 4518px
+left: 286px
+`;
+const DaapCentralHeadingLine = styled.div`
+font-family: inherit;
+font-size: 70px;
+font-weight: 700;
+line-height: 84px;
+letter-spacing: 0em;
+text-align: center;
+`;
+
+const ToggleContainer = styled.div`
+/* Frame 57 */
+
+box-sizing: border-box;
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 8px 16px;
+gap: 16px;
+
+position: absolute;
+width: 436px;
+height: 56px;
+
+
+border: 1px solid #333333;
+border-radius: 8px;
+`;
+const ToggleBox = styled.div`
+/* Frame 54 */
+
+box-sizing: border-box;
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 8px 12px;
+gap: 10px;
+
+width: 179px;
+height: 40px;
+
+border: 1px solid #333333;
+border-radius: 8px;
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+`;
+const ToggleText1 = styled.div`
+
+
+/* NFT WG Projects */
+
+width: 164px;
+height: 24px;
+cursor:pointer;
+
+font-family: inherit;
+font-style: normal;
+font-weight: 600;
+font-size: 20px;
+line-height: 120%;
+/* identical to box height, or 24px */
+white-space:nowrap;
+color: #333333;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+`;
+
+const ToggleText2 = styled.div`
+/* Community Innovations */
+
+width: 209px;
+height: 24px;
+cursor:pointer;
+
+font-family: 'inherit';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 120%;
+/* identical to box height, or 24px */
+
+color: #333333;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+`;
+
+const SearchContainer = styled.div`
+/* Group 1000004191 */
+
+position: absolute;
+width: 1281px;
+height: 56px;
+
+
+`;
+const Filter = styled.div`
+
+/* Frame 2 */
+
+box-sizing: border-box;
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
+align-items: center;
+padding: 16px 48px;
+gap: 16px;
+cursor:pointer;
+
+position: absolute;
+width: 203px;
+height: 56px;
+
+
+background: #333333;
+border: 1px solid #333333;
+border-radius: 24px;
+`;
+
+const FilterText = styled.div`
+
+/* Filters */
+
+width: 67px;
+height: 24px;
+
+font-family: inherit;
+font-style: normal;
+font-weight: 500;
+font-size: 24px;
+line-height: 100%;
+/* identical to box height, or 24px */
+
+color: #F0F0F0;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+`;
+
+const SearchBar = styled.div`
+/* Group 1000004190 */
+
+position: absolute;
+
+
+
+
+
+/* Rectangle 23798 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 1037px;
+height: 56px;
+
+background: #FFFFFF;
+border: 1px solid #A0A0A0;
+border-radius: 6px;
+`;
+
+const ListMyAppHereContainer = styled.div`
+/* Frame 3 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
+align-items: center;
+padding: 16px 40px;
+gap: 10px;
+cursor:pointer;
+
+position: absolute;
+width: 201px;
+height: 48px;
+
+
+background: #6333DD;
+border-radius: 4px;
+`;
+const ListMyAppHereText = styled.div`
+
+/* List My App Here */
+
+width: 121px;
+height: 16px;
+
+font-family: inherit;
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 100%;
+/* identical to box height, or 16px */
+
+color: #FFFFFF;
+white-space:nowrap;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+`;
+
+const INeedSupportContainer = styled.div`
+/* Frame 58 */
+
+box-sizing: border-box;
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
+align-items: center;
+padding: 16px 40px;
+gap: 10px;
+white-space:nowrap;
+cursor:pointer;
+
+position: absolute;
+width: 186px;
+height: 48px;
+
+
+background: #FFFFFF;
+border-radius: 4px;
+
+`;
+const INeedSupportText = styled.div`
+/* I Need Support */
+
+width: 106px;
+height: 16px;
+
+font-family: inherit;
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 100%;
+/* identical to box height, or 16px */
+
+color: #1B1B1B;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+`;
+return (
+  <DaapCentralContainer>
+    <DaapCentralHeading>
+      <DaapCentralHeadingLine id="nftdaapcentral">
+        DApp Central
+      </DaapCentralHeadingLine>
+      <DaapCentralHeadingLine
+        style={{ fontWeight: 400, fontSize: 20, left: 395 }}
+      >
+        NEAR centric innovations built by the Work Group and the wider Near
+        Projects / Communities
+      </DaapCentralHeadingLine>
+    </DaapCentralHeading>
+
+    <ToggleContainer style={{ left: 50 }}>
+      <ToggleBox>
+        <ToggleText1>NFT WG Projects</ToggleText1>
+      </ToggleBox>
+      <ToggleText2>Community Innovations</ToggleText2>
+    </ToggleContainer>
+
+    <SearchContainer style={{ top: 300, left: 50 }}>
+      <SearchBar>
+        <input style={{ width: 1037, height: 56 }} type="search" />
+      </SearchBar>
+
+      <Filter style={{ left: 1070 }}>
+        <svg
+          width="24"
+          height="14"
+          viewBox="0 0 24 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M22.125 2.875H1.875C1.57663 2.875 1.29048 2.75647 1.0795 2.5455C0.868526 2.33452 0.75 2.04837 0.75 1.75C0.75 1.45163 0.868526 1.16548 1.0795 0.954505C1.29048 0.743526 1.57663 0.625 1.875 0.625H22.125C22.4234 0.625 22.7095 0.743526 22.9205 0.954505C23.1315 1.16548 23.25 1.45163 23.25 1.75C23.25 2.04837 23.1315 2.33452 22.9205 2.5455C22.7095 2.75647 22.4234 2.875 22.125 2.875ZM18.375 8.125H5.625C5.32663 8.125 5.04048 8.00647 4.8295 7.79549C4.61853 7.58452 4.5 7.29837 4.5 7C4.5 6.70163 4.61853 6.41548 4.8295 6.2045C5.04048 5.99353 5.32663 5.875 5.625 5.875H18.375C18.6734 5.875 18.9595 5.99353 19.1705 6.2045C19.3815 6.41548 19.5 6.70163 19.5 7C19.5 7.29837 19.3815 7.58452 19.1705 7.79549C18.9595 8.00647 18.6734 8.125 18.375 8.125ZM13.875 13.375H10.125C9.82663 13.375 9.54048 13.2565 9.3295 13.0455C9.11853 12.8345 9 12.5484 9 12.25C9 11.9516 9.11853 11.6655 9.3295 11.4545C9.54048 11.2435 9.82663 11.125 10.125 11.125H13.875C14.1734 11.125 14.4595 11.2435 14.6705 11.4545C14.8815 11.6655 15 11.9516 15 12.25C15 12.5484 14.8815 12.8345 14.6705 13.0455C14.4595 13.2565 14.1734 13.375 13.875 13.375Z"
+            fill="#F0F0F0"
+          />
+        </svg>
+        <FilterText>Filters</FilterText>
+      </Filter>
+    </SearchContainer>
+
+    <ListMyAppHereContainer style={{ top: 1000, left: 30 }}>
+      <ListMyAppHereText>List My App Here</ListMyAppHereText>
+    </ListMyAppHereContainer>
+    <INeedSupportContainer style={{ top: 1000, left: 270 }}>
+      <INeedSupportText>I Need Support</INeedSupportText>
+    </INeedSupportContainer>
+  </DaapCentralContainer>
+);
