9e1798041dcc777ffde705ad48a0cfe922bcc0ae
nftwg_home.near <nftwg_home.near>
2023-10-12 18:12:05 +0000

    Update MeetTheTeam1 by nftwg_home.near at 2023-10-12T18:12:05.240Z

---
+++ nftwg_home.near/MeetTheTeam1/source_code.jsx
@@ -0,0 +1,246 @@
+const MeetTheTeamContainer = styled.div`
+/* Rectangle 23793 */
+
+position: absolute;
+width: 1600px;
+height: 1551px;
+left: 0px;
+top: 3700px;
+
+background: #1B1B1B;
+
+`;
+const CharterContainer = styled.div`
+position: absolute;
+width: 1280px;
+height: 165px;
+left: 80px;
+top: px;
+`;
+const CharterContainer1 = styled.div`
+/* NFTs will lead the Web 3 evolution with limitless utility and endless fun! */
+
+position: absolute;
+width: 700px;
+height: 165px;
+left: 0px;
+top: 230px;
+
+font-family: inherit;
+font-style: normal;
+font-weight: 700;
+font-size: 44px;
+line-height: 125.5%;
+/* or 55px */
+
+color: #FFFFFF;
+`;
+
+const CharterBtnContainer = styled.div`
+/* Frame 2 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
+align-items: center;
+padding: 16px 40px;
+gap: 10px;
+
+position: absolute;
+width: 137px;
+height: 48px;
+left: 980px;
+top: 340px;
+
+background: #FEFEFE;
+border-radius: 4px;
+cursor:pointer;
+
+
+`;
+const Charter = styled.div`
+
+width: 57px;
+height: 16px;
+
+font-family: inherit;
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 100%;
+/* identical to box height, or 16px */
+
+color: #7B7B7B;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+`;
+const StarForCharter = styled.div`
+
+/* Star 6 */
+
+position: absolute;
+width: 65px;
+height: 65px;
+left: 128px;
+top: 30px;
+
+`;
+
+const MeetTeamContainer = styled.div`
+position: absolute;
+width: 1464px;
+height: 725px;
+left: 500px;
+top: 780px;
+`;
+
+const CoreTeamContainer = styled.div`
+top: 450px, 
+left: 700px,
+position: absolute;
+width: 946px;
+height: 534px;
+top: 353px;
+
+`;
+const CoreText = styled.div`
+position: absolute;
+width: 1205px;
+height: 60px;
+left: 200px;
+top: 1410px;
+
+font-family: inherit;
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 150.5%;
+/* or 30px */
+text-align: center;
+
+color: #F2F2F2;
+`;
+
+const SocialContainer = styled.div`
+
+position: absolute;
+width: 200px;
+height: 60px;
+top: 40px;
+display: flex;
+justify-content: space-between; /* Use 'justify-content' for spacing between flex items */
+flex-direction: row;
+`;
+const SocialIcon = styled.div`
+
+left:20;
+width:20;
+height:20;
+`;
+return (
+  <MeetTheTeamContainer>
+    <MeetTeamContainer style={{ top: 1000, left: 240 }}>
+      <div>
+        <img
+          style={{ width: 280, height: 280 }}
+          src="https://cdn.discordapp.com/attachments/1132232613210357780/1160924867080753203/Aurobot.webp?ex=65366e89&is=6523f989&hm=de32abee5e8d7a308c515a7f4913978ad81230286594124230f51a977f7960ab&"
+        />
+      </div>
+    </MeetTeamContainer>
+    <MeetTeamContainer style={{ top: 1000, left: 640 }}>
+      <div>
+        <img
+          style={{ width: 280, height: 280 }}
+          src="https://cdn.discordapp.com/attachments/1132232613210357780/1160922786118766703/bafkreihsi6z7t2g2blipzzfgmbamfaqjfiifoyfrc47qjt3a3u7p3rbpce.png?ex=65366c99&is=6523f799&hm=532962b74dfd141d6879d8fb0e3a2ac6abdc6389936e998fc9430069c8c3f1fa&
+"
+        />
+      </div>
+    </MeetTeamContainer>
+    <MeetTeamContainer style={{ top: 1000, left: 1030 }}>
+      <div>
+        <img
+          style={{ width: 280, height: 280 }}
+          src="https://cdn.discordapp.com/attachments/932622483662733332/1161374194144133190/1651673561336.jpeg?ex=65381101&is=65259c01&hm=b17f6e79be503b9c50ad09e5ee774f4055698f198f5dae68d71f7a4cf41cc32f&
+"
+        />
+      </div>
+    </MeetTeamContainer>
+    <SocialContainer style={{ top: 1350, left: 270 }}>
+      <SocialIcon>
+        <a href="https://near.social/mob.near/widget/ProfilePage?accountId=krikkraktrak.near">
+          <img
+            style={{ width: 20, height: 20 }}
+            src="https://near.social/favicon.png
+"
+          />
+        </a>
+      </SocialIcon>
+      <SocialIcon>
+        <a href="https://gov.near.org/t/self-nomination-nft-revival-dao-krik/35332">
+          <img
+            style={{ width: 20, height: 20 }}
+            src="https://cdn.discordapp.com/attachments/1135845507189702748/1161713477589667850/Self-Intro-icon.svg?ex=65394cfc&is=6526d7fc&hm=78beed194efa12f9e31d0f6c456f54a2a900c4a5b6c0327684cb1003a87509f4&
+"
+          />
+        </a>
+      </SocialIcon>
+    </SocialContainer>
+
+    <SocialContainer style={{ top: 1350, left: 680 }}>
+      <SocialIcon>
+        <a href="https://near.social/mob.near/widget/ProfilePage?accountId=escobarindo.near">
+          <img
+            style={{ width: 20, height: 20 }}
+            src="https://near.social/favicon.png
+"
+          />
+        </a>
+      </SocialIcon>
+      <SocialIcon>
+        <a href="https://gov.near.org/t/self-nomination-nft-wg-core-contributors/35359">
+          <img
+            style={{ width: 20, height: 20 }}
+            src="https://cdn.discordapp.com/attachments/1135845507189702748/1161713477589667850/Self-Intro-icon.svg?ex=65394cfc&is=6526d7fc&hm=78beed194efa12f9e31d0f6c456f54a2a900c4a5b6c0327684cb1003a87509f4&
+"
+          />
+        </a>
+      </SocialIcon>
+    </SocialContainer>
+
+    <SocialContainer style={{ top: 1350, left: 1080 }}>
+      <SocialIcon>
+        <a href="https://near.social/mob.near/widget/ProfilePage?accountId=nearversedao.near">
+          <img
+            style={{ width: 20, height: 20 }}
+            src="https://near.social/favicon.png
+"
+          />
+        </a>
+      </SocialIcon>
+      <SocialIcon>
+        <a href="https://gov.near.org/t/self-nomination-nft-wg-election/35357">
+          <img
+            style={{ width: 20, height: 20 }}
+            src="https://cdn.discordapp.com/attachments/1135845507189702748/1161713477589667850/Self-Intro-icon.svg?ex=65394cfc&is=6526d7fc&hm=78beed194efa12f9e31d0f6c456f54a2a900c4a5b6c0327684cb1003a87509f4&
+"
+          />
+        </a>
+      </SocialIcon>
+    </SocialContainer>
+    <CoreText>
+      {" "}
+      The core contributors team is a diverse group comprising individuals with
+      varied experiences. These contributors have been democratically elected by
+      the community, sharing a common commitment to enhance innovations in Near
+      NFTs. You can view the detailed election results by following this link :{" "}
+      <a href="https://gov.near.org/t/nft-wg-election-results/35493">
+        Election Results
+      </a>
+    </CoreText>
+  </MeetTheTeamContainer>
+);
