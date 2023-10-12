8360ef68ca54492adc360e50e4603504aebab25c
nathanx.near <nathanx.near>
2023-10-12 09:06:03 +0000

    Update HEREWallet by nathanx.near at 2023-10-12T09:06:03.334Z

---
+++ nathanx.near/HEREWallet/source_code.jsx
@@ -0,0 +1,65 @@
+const accountId = context.accountId;
+
+return (
+  <>
+    <div class="container min-vw-80">
+      <h1>{`Hello, ${accountId}!👋`}</h1>
+      <strong> Welcome to to all things HERE Wallet! </strong>
+      <br></br>
+      <hr />
+      <img
+        src="https://www.herewallet.app/logo.162cb67d.svg"
+        alt="HERE"
+        width="195"
+        height="40"
+      ></img>
+      <br></br>
+      <strong>The only wallet to use on $NEAR.</strong>
+      <br></br>
+      <br></br>
+      <h3>HERE Wallet Links</h3>
+      <a href="https://www.herewallet.app/">🔗---HERE Wallet Website</a>
+      <br></br>
+      <a href="https://discord.gg/8Q3gw3gsD2">🔗---HERE Wallet Discord</a>
+      <br></br>
+      <a href="https://t.me/herewallet">🔗---HERE Wallet Telegram</a>
+      <br></br>
+      <a href="https://www.tradeport.xyz/near/collection/nft.herewallet.near">
+        🔗---HERE NFT Collection on Tradeport - Holders get an extra 1% APY
+        staking!
+      </a>
+      <br></br>
+      <br></br>
+      <strong>What is HERE Wallet?</strong>
+      <p>
+        NEAR is a fast-growing ecosystem with over 20 million users. The main
+        wallet for NEAR is the NEAR Wallet, but it is only available in web
+        mode. Until a certain time, there was no convenient mobile wallet for
+        NEAR. HERE Wallet focuses on the use of NEAR instead of storing it. You
+        can stake, buy NEAR for BTC and other crypto and fiat, swap and more
+        just in one app!
+      </p>
+      <hr />
+      <h3>Benefits of HERE NFT🤝</h3>
+      <strong>Governance</strong>
+      <br></br>
+      <i>
+        Access to development and airdrops. Opportunity to participate in HERE
+        DAO in the future. The more NFTs you get, the more governance power you
+        will have.
+      </i>
+      <br></br>
+      <strong>NFT flex</strong>
+      <br></br>
+      <i>
+        A special role in HERE discord. Access to developer chat. NFT Avatar
+        which will be displayed to other users.
+      </i>
+      <br></br>
+      <strong>Early access</strong>
+      <br></br>
+      <i>Access to B-versions with new features. Get rewards for feedback</i>
+      <hr />
+    </div>
+  </>
+);
