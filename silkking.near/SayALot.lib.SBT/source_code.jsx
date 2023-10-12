bc9431fd5dc4973c3b672bd75a907574eff943fb
silkking.near <silkking.near>
2023-10-12 15:56:23 +0000

    Update SayALot.lib.SBT by silkking.near at 2023-10-12T15:56:23.312Z

--- silkking.near/SayALot.lib.SBT/source_code.jsx
+++ silkking.near/SayALot.lib.SBT/source_code.jsx
@@ -4,7 +4,7 @@ let resultLibCalls = [];
 
 function isValidUser(props) {
   const { accountId, sbtsNames } = props;
-  console.log(5);
+  console.log(1, props);
   const userSBTs = Near.view(
     "registry.i-am-human.near",
     "sbt_tokens_by_owner",
@@ -17,12 +17,10 @@ function isValidUser(props) {
     const data = sbt.split(" - class ");
     return { name: data[0], classNumber: Number(data[1]) };
   });
-  console.log(4, sbtsNames);
   const usersValidityBySBT = {};
   sbtsNames.forEach((sbtName, index) => {
     const userValidityBySBT =
       userSBTs.find((userSbt) => {
-        console.log(3, userSbt, sbtsData);
         return (
           userSbt[0] === sbtsData[index].name &&
           userSbt[1].find(
@@ -33,34 +31,12 @@ function isValidUser(props) {
       }) !== undefined;
     usersValidityBySBT[sbtName] = userValidityBySBT;
   });
-  // const sbtsFiltered = userSBTs.filter((sbt) => {
-  //   return sbt[0] === sbtsData[0].name;
-  // });
 
-  // const classFiltered = sbtsFiltered[0][1]
-  //   ? sbtsFiltered[0][1].find((sbt) => {
-  //       return sbt.metadata["class"] === sbtsData[0].classNumber;
-  //     })
-  //   : undefined;
-
-  // const result = classFiltered !== undefined;
-
-  // const result =
-  //   userSBTs
-  //     .filter((sbt) => {
-  //       return sbt[0] === sbtsData[0].name;
-  //     })
-  //     .find((sbt) => {
-  //       return (
-  //         Number(sbt[1].metadata["class"]) === Number(sbtsData[0].classNumber)
-  //       );
-  //     }) !== undefined;
   resultLibCalls = resultLibCalls.filter((call) => {
     return call.functionName !== "isValidUser";
   });
 
   // return true;
-  console.log(2, usersValidityBySBT);
   return { ...usersValidityBySBT };
 }
 
