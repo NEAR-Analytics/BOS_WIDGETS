5cc19342e96193b6bdb013280f94f7a92026bbb5
saidulbadhon.near <saidulbadhon.near>
2023-10-12 17:58:58 +0000

    Update helloWorld by saidulbadhon.near at 2023-10-12T17:58:58.951Z

--- saidulbadhon.near/helloWorld/source_code.jsx
+++ saidulbadhon.near/helloWorld/source_code.jsx
@@ -1,27 +1,19 @@
 return (
-    <div>
-    <h1>
-    asdasdads
-    </h1>
-    <h1>
-    asdasdads
-    </h1>
-    <h1>
-    asdasdads
-    </h1>
-    <h1>
-    asdasdads
-    </h1>
-    <h1>
-    asdasdads
-    </h1>
+  <div>
+    <h1>1</h1>
+    <h1>2</h1>
+    <h1>3</h1>
+    <h1>4</h1>
+    <h1>5</h1>
 
+<a href="geo:124.028582,-29.201930" target="_blank">Click here for map</a>
 
-  <a href="https://maps.google.com/?q=addressgoeshere">
-    <a href="https://maps.apple.com/maps?q=addressgoeshere">both</a>
-  </a>
-  <a href="https://maps.google.com/?q=addressgoeshere">
-    google
-  </a>
-    </div>
+    <a href="https://maps.google.com/?q=addressgoeshere">google</a>
+
+    <h1>6</h1>
+    <h1>7</h1>
+    <h1>8</h1>
+    <h1>9</h1>
+    <h1>10</h1>
+  </div>
 );
