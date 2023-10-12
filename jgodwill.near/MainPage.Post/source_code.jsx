e2cab9abb246b5572275f795966c62265ddfe225
jgodwill.near <jgodwill.near>
2023-10-12 09:47:51 +0000

    Update MainPage.Post by jgodwill.near at 2023-10-12T09:47:51.308Z

--- jgodwill.near/MainPage.Post/source_code.jsx
+++ jgodwill.near/MainPage.Post/source_code.jsx
@@ -182,7 +182,7 @@ return (
         />
         <div className="mt-3 text-break">
           <Widget
-            src="mob.near/widget/MainPage.Post.Content"
+            src="jgodwill.near/widget/MainPage.Post.Content"
             props={{ content, raw }}
           />
         </div>
