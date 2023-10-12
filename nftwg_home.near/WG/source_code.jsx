9134ad88d45641c00da54150acfd17d6e33356ba
nftwg_home.near <nftwg_home.near>
2023-10-12 19:18:37 +0000

    Update WG by nftwg_home.near at 2023-10-12T19:18:37.555Z

--- nftwg_home.near/WG/source_code.jsx
+++ nftwg_home.near/WG/source_code.jsx
@@ -1,8 +1,8 @@
 const Owner = "nftwg_home.near";
 const Layout = styled.div`
 position: relative;
-width: 1440px;
-height: 6060px;
+width: 1100px;
+height: 5660px;
 background: #FFFBDA;
 `;
 return (
