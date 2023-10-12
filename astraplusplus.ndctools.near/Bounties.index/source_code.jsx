7c91f701ac6a38d24a83cda31e6ca2bcd22865d4
astraplusplus.ndctools.near <astraplusplus.ndctools.near>
2023-10-12 09:39:14 +0000

    Update Bounties.index by astraplusplus.ndctools.near at 2023-10-12T09:39:14.063Z

---
+++ astraplusplus.ndctools.near/Bounties.index/source_code.jsx
@@ -0,0 +1,55 @@
+const defaultFilters = props.defaultFilters ?? {};
+
+const bountiesLink = "#/astraplusplus.ndctools.near/widget/home?tab=bounties";
+
+const publicApiKey = "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5";
+const resPerPage = 10;
+
+// dummy data
+const bounties = [
+  {
+    description: "description 1",
+    token: "megha19.near",
+    amount: 100,
+    times: 3,
+    max_deadline: 33,
+    name: "name",
+    id: 222,
+  },
+  {
+    description: "description 1",
+    token: "megha19.near",
+    amount: 100,
+    times: 3,
+    max_deadline: 33,
+    name: "dddd",
+    id: 22,
+  },
+  {
+    description: "description 1",
+    token: "megha19.near",
+    amount: 100,
+    times: 3,
+    max_deadline: 33,
+    name: "ssss",
+    id: 2,
+  },
+];
+
+const renderBounties = () => {
+  return (
+    <Widget
+      src="astraplusplus.ndctools.near/widget/Bounties.List"
+      props={{
+        bounties,
+      }}
+    />
+  );
+};
+
+return (
+  <div>
+    <h2 className="h2">Bounties</h2>
+    {renderBounties()}
+  </div>
+);
