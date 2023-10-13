c2d37c62d1bd6023fc5a38582d1bd85f947314f3
mybadge.near <mybadge.near>
2023-10-13 19:04:02 +0000

    Update Dapplets.DappCreatorBadge by mybadge.near at 2023-10-13T19:04:02.099Z

--- mybadge.near/Dapplets.DappCreatorBadge/source_code.jsx
+++ mybadge.near/Dapplets.DappCreatorBadge/source_code.jsx
@@ -2,7 +2,7 @@ return (
   <Widget
     src="mybadge.near/widget/Generic.DetailedBadge"
     props={{
-      primaryText: "Dapp Creator",
+      primaryText: "DAPP CREATOR",
       secondaryText: "Dapplets Project",
       iconSrc: "mybadge.near/widget/Dapplets.LogoIcon",
       color: "red",
