2e4b9cb2d85b57c3a780a56fba1b48ee0ac40742
nftwg_home.near <nftwg_home.near>
2023-10-13 16:24:47 +0000

    Update Footer by nftwg_home.near at 2023-10-13T16:24:47.720Z

--- nftwg_home.near/Footer/source_code.jsx
+++ nftwg_home.near/Footer/source_code.jsx
@@ -335,24 +335,33 @@ return (
         </defs>
       </svg>
     </FooterLogo>
+    //{" "}
     <FooterWrapper>
+      //{" "}
       <home>
-        <a href="#home"> Home </a>
+        // <a href="#home"> Home </a>
+        //{" "}
       </home>
+      //{" "}
       <aboutUs>
-        {" "}
-        <a href="#aboutus">About Us </a>
+        // // <a href="#aboutus">About Us </a>
+        //{" "}
       </aboutUs>
+      //{" "}
       <ourGoals>
-        <a href="#ourgoals"> Our Goals </a>
+        // <a href="#ourgoals"> Our Goals </a>
+        //{" "}
       </ourGoals>
+      //{" "}
       <ourTeam>
-        {" "}
-        <a href="#ourteam">Our Team</a>{" "}
+        // // <a href="#ourteam">Our Team</a> //{" "}
       </ourTeam>
+      //{" "}
       <nftDappCentral>
-        <a href="#nftdaapcentral"> NFT DApp Central </a>
+        // <a href="#nftdaapcentral"> NFT DApp Central </a>
+        //{" "}
       </nftDappCentral>
+      //{" "}
     </FooterWrapper>
     <FooterRightsWrapper>
       <FooterLine>
