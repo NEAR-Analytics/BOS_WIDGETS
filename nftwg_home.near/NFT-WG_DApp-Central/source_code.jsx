0d26a97225ef78774eb13fc4326f63c881f76fa5
nftwg_home.near <nftwg_home.near>
2023-10-13 16:06:24 +0000

    Update NFT-WG_DApp-Central by nftwg_home.near at 2023-10-13T16:06:24.834Z

---
+++ nftwg_home.near/NFT-WG_DApp-Central/source_code.jsx
@@ -0,0 +1,19 @@
+const Owner = "nftwg_home.near";
+const Layout = styled.div`
+position: relative;
+width: 1200px;
+height: 5510px;
+background: #FFFBDA;
+`;
+return (
+  <Layout>
+    <Widget src={`${Owner}/widget/Header`} />
+    <Widget src={`${Owner}/widget/Hero`} />
+    <Widget src={`${Owner}/widget/NFTStats`} />
+    <Widget src={`${Owner}/widget/AboutUs`} />
+    <Widget src={`${Owner}/widget/OurGoals`} />
+    <Widget src={`${Owner}/widget/MeetTheTeam`} />
+    <Widget src={`${Owner}/widget/DaapCentral`} />
+    <Widget src={`${Owner}/widget/Footer`} />
+  </Layout>
+);
