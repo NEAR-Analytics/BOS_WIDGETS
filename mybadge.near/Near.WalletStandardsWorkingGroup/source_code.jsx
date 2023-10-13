9c6e144bb581a792eed1bce8bd6f6a4308805eb5
mybadge.near <mybadge.near>
2023-10-13 17:32:54 +0000

    Update Near.WalletStandardsWorkingGroup by mybadge.near at 2023-10-13T17:32:54.326Z

---
+++ mybadge.near/Near.WalletStandardsWorkingGroup/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <Widget
+    src="mybadge.near/widget/Generic.DetailedBadge"
+    props={{
+      primaryText: "Wallet Standards",
+      secondaryText: "Working Group",
+      iconSrc: "mybadge.near/widget/Near.LogoIcon",
+    }}
+  />
+);
