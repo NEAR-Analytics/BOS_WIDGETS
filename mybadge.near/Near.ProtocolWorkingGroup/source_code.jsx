1dd7d89b9346512954f4b1ec03855cde7bc4f763
mybadge.near <mybadge.near>
2023-10-13 17:31:38 +0000

    Update Near.ProtocolWorkingGroup by mybadge.near at 2023-10-13T17:31:38.135Z

---
+++ mybadge.near/Near.ProtocolWorkingGroup/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <Widget
+    src="mybadge.near/widget/Generic.DetailedBadge"
+    props={{
+      primaryText: "Protocol",
+      secondaryText: "Working Group",
+      iconSrc: "mybadge.near/widget/Near.LogoIcon",
+    }}
+  />
+);
