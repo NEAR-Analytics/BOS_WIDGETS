2964cd7cfda2cd927bae28328593ff443c238ae7
bo.near <bo.near>
2023-10-13 03:53:37 +0000

    Update gigs-board.pages.Create by bo.near at 2023-10-13T03:53:37.600Z

--- bo.near/gigs-board.pages.Create/source_code.jsx
+++ bo.near/gigs-board.pages.Create/source_code.jsx
@@ -89,7 +89,7 @@ const labels = labelStrings.map((s) => {
 
 initState({
   seekingFunding: false,
-  //
+
   author_id: context.accountId,
   // Should be a list of objects with field "name".
   labels,
@@ -146,7 +146,8 @@ const onSubmit = () => {
       state.description,
       state.amount,
       state.token,
-      state.supervisor
+      state.supervisor,
+      state.seekingFunding
     ),
   };
 
@@ -334,19 +335,13 @@ const nameDiv = (
 const descriptionDiv = (
   <div className="col-lg-12 mb-2">
     <p className="fs-6 fw-bold mb-1">Description</p>
-    <textarea
-      value={state.description}
-      type="text"
-      rows={6}
-      className="form-control"
-      onInput={(event) => textareaInputHandler(event.target.value)}
-      onKeyUp={(event) => {
-        if (event.key === "Escape") {
-          State.update({ showAccountAutocomplete: false });
-        }
-      }}
-      onChange={(event) => State.update({ description: event.target.value })}
-    />
+    {widget("components.molecule.markdown-editor", {
+      data: { handler: state.handler, content: state.description },
+      onChange: (content) => {
+        State.update({ description: content, handler: "update" });
+        textareaInputHandler(content);
+      },
+    })}
     {autocompleteEnabled && state.showAccountAutocomplete && (
       <AutoComplete>
         <Widget
@@ -462,10 +457,13 @@ const fundraisingDiv = (
   </div>
 );
 
-function generateDescription(text, amount, token, supervisor) {
-  const funding = `###### Requested amount: ${amount} ${token}\n###### Requested sponsor: @${supervisor}\n`;
-  if (amount > 0 && token && supervisor) return funding + text;
-  return text;
+function generateDescription(text, amount, token, supervisor, seekingFunding) {
+  const fundingText =
+    amount > 0 && token ? `###### Requested amount: ${amount} ${token}\n` : "";
+  const supervisorText = supervisor
+    ? `###### Requested sponsor: @${supervisor}\n`
+    : "";
+  return seekingFunding ? `${fundingText}${supervisorText}${text}` : text;
 }
 
 return (
@@ -577,6 +575,9 @@ return (
                   backgroundColor: "#0C7283",
                   color: "#f3f3f3",
                 }}
+                disabled={
+                  state.seekingFunding && (!state.amount || state.amount < 1)
+                }
                 className="btn btn-light mb-2 p-3"
                 onClick={onSubmit}
               >
@@ -602,7 +603,8 @@ return (
                         state.description,
                         state.amount,
                         state.token,
-                        state.supervisor
+                        state.supervisor,
+                        state.seekingFunding
                       ),
                       github_link: state.githubLink,
                     },
