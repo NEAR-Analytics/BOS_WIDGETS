2cfd8d26f146becef3bc59560359425615683dec
mybadge.near <mybadge.near>
2023-10-13 19:07:23 +0000

    Update Near.VeteranBadge by mybadge.near at 2023-10-13T19:07:23.310Z

--- mybadge.near/Near.VeteranBadge/source_code.jsx
+++ mybadge.near/Near.VeteranBadge/source_code.jsx
@@ -3,7 +3,7 @@ return (
     src="mybadge.near/widget/Generic.TextBadge"
     props={{
       iconSrc: "mybadge.near/widget/Near.LogoIcon",
-      label: "Veteran",
+      label: "VETERAN",
       color: "blue",
     }}
   />
