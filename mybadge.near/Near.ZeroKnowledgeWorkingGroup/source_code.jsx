0df9b6cefaed4e0433e8376df21e2c9ceb9e8f0e
mybadge.near <mybadge.near>
2023-10-13 19:07:51 +0000

    Update Near.ZeroKnowledgeWorkingGroup by mybadge.near at 2023-10-13T19:07:51.616Z

--- mybadge.near/Near.ZeroKnowledgeWorkingGroup/source_code.jsx
+++ mybadge.near/Near.ZeroKnowledgeWorkingGroup/source_code.jsx
@@ -2,7 +2,7 @@ return (
   <Widget
     src="mybadge.near/widget/Generic.DetailedBadge"
     props={{
-      primaryText: "Zero Knowledge",
+      primaryText: "ZERO KNOWLEDGE",
       secondaryText: "Working Group",
       iconSrc: "mybadge.near/widget/Near.LogoIcon",
     }}
