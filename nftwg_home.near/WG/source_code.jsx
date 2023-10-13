4c59d8c1ca2950a8cc029c5393ef50d4c149db3c
nftwg_home.near <nftwg_home.near>
2023-10-13 09:39:17 +0000

    Update WG by nftwg_home.near at 2023-10-13T09:39:17.401Z

--- nftwg_home.near/WG/source_code.jsx
+++ nftwg_home.near/WG/source_code.jsx
@@ -1,13 +1,22 @@
 const Owner = "nftwg_home.near";
 const Layout = styled.div`
 position: relative;
-width: 1200px;
-height: 6060px;
+width: 1300px;
+height: 6800px;
 background: #FFFBDA;
+padding:30px;
+
+
+`;
+const HeaderContainer = styled.div`
+position:relative;
+width:200px;
+height:80px;
 `;
 return (
   <Layout>
     <Widget src={`${Owner}/widget/Header`} />
+
     <Widget src={`${Owner}/widget/Hero`} />
     <Widget src={`${Owner}/widget/NFTStats`} />
     <Widget src={`${Owner}/widget/AboutUs`} />
