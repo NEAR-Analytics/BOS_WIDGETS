826deb3bc132b9796619ec7cdee9841e50894fc5
amandaalexbr.near <amandaalexbr.near>
2023-10-14 16:43:25 +0000

    Update VexAboutUs by amandaalexbr.near at 2023-10-14T16:43:25.461Z

---
+++ amandaalexbr.near/VexAboutUs/source_code.jsx
@@ -0,0 +1,46 @@
+const Main = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: #06050b;
+gap: 2rem;
+padding: 3rem 1rem;
+`;
+
+const aboutTitle = { text: "About Us" };
+
+const lolPlayerImg =
+  "https://aquamarine-official-jay-421.mypinata.cloud/ipfs/QmULvhbPXzRW9kmpPqvHM4bphDy7RkmDcMyX3fcCBXrCdX";
+
+const TitleImage = styled.img`
+  max-width: 50%;
+  height: auto;
+  border-radius: 10px;    /* Example border radius */
+  box-shadow: 5px 5px 10px black; /* Example box-shadow */
+`;
+
+const AboutHero = styled.div`
+    display: flex;
+    color: white;
+`;
+
+return (
+  <Main>
+    <AboutHero>
+      <div>
+        <Widget src="amandaalexbr.near/widget/VexTitle" props={aboutTitle} />
+
+        <p>
+          VEX is in development stage. We are currently looking for investors
+          and contributors.
+        </p>
+
+        <p>
+          <a href="https://twitter.com/betvex">Connect with us via Twitter.</a>
+        </p>
+      </div>
+
+      <TitleImage src={lolPlayerImg} />
+    </AboutHero>
+  </Main>
+);
