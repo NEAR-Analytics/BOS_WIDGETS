75be05f5d738dc2a9d93171c47854d6317a98bc4
amandaalexbr.near <amandaalexbr.near>
2023-10-14 14:47:50 +0000

    Update VexHero by amandaalexbr.near at 2023-10-14T14:47:50.917Z

---
+++ amandaalexbr.near/VexHero/source_code.jsx
@@ -0,0 +1,58 @@
+const BannerBg =
+  "https://aquamarine-official-jay-421.mypinata.cloud/ipfs/QmP9WkFCfYPy4LxyReTrHMaH45im1avLXjhCk9M25iXMns";
+
+const Hero = styled.div`
+border: 0;
+box-sizing: border-box;
+margin: 0;
+background: black;
+background-image: url(${BannerBg});
+background-size: cover;
+background-position: right;
+width: 100%;
+min-height: 80vh;
+display: flex;
+flex-direction:column;
+justify-content: center;
+align-items: left;
+padding: 1.5rem;
+gap: 0.5rem;
+`;
+
+const Buttons = styled.div`
+display: flex;
+justify-content: left;
+align-items: center;
+gap: 0.5rem;
+`;
+
+return (
+  <Hero>
+    <Widget src="amandaalexbr.near/widget/VexTitle" props="text: VEX Esports" />
+    <Widget
+      src="amandaalexbr.near/widget/VexSubtitle"
+      props="text: Esports betting for gamers, by gamers."
+    />
+
+    <Buttons>
+      <Widget
+        src="near/widget/DIG.Button"
+        props={{
+          href: "https://twitter.com/betvex",
+          iconLeft: "ph-fill ph-twitter-logo",
+          label: "Twitter",
+          size: "large",
+        }}
+      />
+
+      <Widget
+        src="near/widget/DIG.Button"
+        props={{
+          href: "https://test.near.org/vex-hackathon.testnet/widget/MatchList",
+          label: "Place your bets",
+          size: "large",
+        }}
+      />
+    </Buttons>
+  </Hero>
+);
