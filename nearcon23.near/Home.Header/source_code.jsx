1df4c56c77059568f1cff0082cdc07bcdb12424a
nearcon23.near <nearcon23.near>
2023-10-14 19:31:42 +0000

    Update Home.Header by nearcon23.near at 2023-10-14T19:31:42.589Z

--- nearcon23.near/Home.Header/source_code.jsx
+++ nearcon23.near/Home.Header/source_code.jsx
@@ -1,7 +1,9 @@
 const desktopImage =
-  "https://ipfs.near.social/ipfs/bafkreiga7r5nutmlf66qkfvgkzbq2orbqtlmvgshq47355um2ttqybt4pe";
+  "https://ipfs.near.social/ipfs/bafkreifukyxgva3dftljkxpg3vnq46lwwhkhlq55xwi5qqin3chendnm6e";
+//   "https://ipfs.near.social/ipfs/bafkreiga7r5nutmlf66qkfvgkzbq2orbqtlmvgshq47355um2ttqybt4pe";
 const mobileImage =
-  "https://ipfs.near.social/ipfs/bafkreiddafldobfu4tcqmeyhxgxdkwp7mhfb5keottpe6lir67j6zgout4";
+  "https://ipfs.near.social/ipfs/bafkreialqekphpdhx4dplb3sjqz5tya5quz633cljeida7eefqylhmmg6m";
+//   "https://ipfs.near.social/ipfs/bafkreiddafldobfu4tcqmeyhxgxdkwp7mhfb5keottpe6lir67j6zgout4";
 
 const MyImage = styled.img`
     @media (max-width: 650px) {    
