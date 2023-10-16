4b96f62dea960043b734fe3860a564cb27f13dc3
mob.near <mob.near>
2023-10-16 21:54:00 +0000

    Update MainPage.N.Post.Embed by mob.near at 2023-10-16T21:54:00.392Z

---
+++ mob.near/MainPage.N.Post.Embed/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <Widget
+    src="mob.near/widget/MainPage.N.Post"
+    props={{
+      ...props,
+      noBorder: true,
+      commentsLimit: 0,
+      truncateContent: true,
+    }}
+  />
+);
