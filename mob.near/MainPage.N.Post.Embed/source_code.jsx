a29a24f9e97c00daeb411dbd98a6820f8f3f060a
mob.near <mob.near>
2023-10-16 22:23:32 +0000

    Update MainPage.N.Post.Embed by mob.near at 2023-10-16T22:23:32.803Z

--- mob.near/MainPage.N.Post.Embed/source_code.jsx
+++ mob.near/MainPage.N.Post.Embed/source_code.jsx
@@ -1,11 +1,20 @@
+const Wrapper = styled.div`
+  margin: 0 12px;
+`;
+
 return (
-  <Widget
-    src="mob.near/widget/MainPage.N.Post"
-    props={{
-      ...props,
-      noBorder: true,
-      commentsLimit: 0,
-      truncateContent: true,
-    }}
-  />
+  <Wrapper>
+    <Widget
+      src="mob.near/widget/MainPage.N.Post"
+      props={{
+        ...props,
+        noBorder: true,
+        hideComments: true,
+        truncateContent: true,
+        noEmbed: true,
+        hideButtons: true,
+        hideMenu: true,
+      }}
+    />
+  </Wrapper>
 );
