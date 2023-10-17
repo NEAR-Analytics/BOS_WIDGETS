e4cd23b359fde4241979d0d6cf7e0c13b113a592
y3k.near <y3k.near>
2023-10-17 15:21:37 +0000

    Update apps.ama.button.question.upvote by y3k.near at 2023-10-17T15:21:37.872Z

--- y3k.near/apps.ama.button.question.upvote/source_code.jsx
+++ y3k.near/apps.ama.button.question.upvote/source_code.jsx
@@ -1,209 +1,101 @@
 const accountId = props.accountId;
-const blockHeight = parseInt(props.blockHeight);
-const admins = props.admins;
-const adminContract = "y3k.near";
+const blockHeight = props.blockHeight;
 
-const is_hidden = Near.view(adminContract, "is_hidden", {
-  id: { account_id: accountId, block_height: blockHeight },
-});
-
-if (is_hidden) {
-  return "";
+if (accountId === undefined || blockHeight === undefined) {
+  return;
 }
 
-const question = JSON.parse(
-  Social.get(`${accountId}/question/y3k`, blockHeight) ?? "null"
-);
+const item = {
+  accountId,
+  blockHeight,
+};
 
-const testQuestion = JSON.parse(
-  Social.get(`${accountId}/question`, blockHeight) ?? "null"
-);
+const likes = Social.index("upvote", item, { subscribe: true });
 
-const link = `#/y3k.near/widget/apps.ama.post.page?accountId=${accountId}&blockHeight=${blockHeight}&adminContract=${adminContract}`;
+const dataLoading = likes === null;
 
-const H2 = styled.h2`
-  font-size: 20px;
-  font-weight: 600;
-  color: #11181C;
+let upvotes = 0;
+let downvotes = 0;
 
-  a {
-    color: inherit;
-    transition: color .15s ease;
-    &:hover {
-      color: #30A46C;
-      text-decoration: none;
-
-      & + i {
-        visibility: visible;
-      }
-    }
-
-    & + i {
-      transition: visibility .1s ease-out;
-      visibility: hidden;
-      color: #30A46C;
-    }
+(likes || []).forEach((like) => {
+  if (like.value.type === "upvote") {
+    upvotes += 1;
   }
-`;
-const H6 = styled.h6`
-  font-size: 14px;
-  font-weight: 500;
-  color: #687076;
-`;
-const Trancate = styled.span`
-  text-overflow: ellipsis;
-  overflow: hidden;
-`;
-const TopicName = styled.span`
-  color: #006ADC;
-  font-weight: 500;
-  font-size: 14px;
-  white-space: nowrap;
-`;
-const CardWrapper = styled.div`
-  z-index: 100;
-  padding: 6px;
-`;
-const Card = styled.div`
+  if (like.value.type === "downvote") {
+    downvotes += 1;
+  }
+});
+
+// state.hasLike === true
+
+// const hasLike = context.accountId && !!likesByUsers[context.accountId];
+
+const likeClick = () => {
+  if (state.loading) {
+    return;
+  }
+  State.update({
+    loading: true,
+  });
+  const data = {
+    index: {
+      upvote: JSON.stringify({
+        key: item,
+        value: {
+          type: hasLike ? "downvote" : "upvote",
+        },
+      }),
+    },
+  };
+
+  if (!hasLike && props.notifyAccountId) {
+    data.index.notify = JSON.stringify({
+      key: props.notifyAccountId,
+      value: {
+        type: "upvote",
+        item,
+      },
+    });
+  }
+  Social.set(data, {
+    onCommit: () => State.update({ loading: false, hasLike: !hasLike }),
+    onCancel: () => State.update({ loading: false }),
+  });
+};
+
+const Button = styled.button`
   display: flex;
   flex-direction: column;
-  justify-content: stretch;
-  gap: 12px;
-  width: 275px;
-  border-radius: 12px;
-  z-index: 1070;
-  background: #fff;
-  border: 1px solid #eceef0;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-  padding: 12px;
-`;
-const Item = styled.div`
-  padding: 0;
-  .btn {
-    width: 100%;
-    text-align: left;
-    &:hover,
-    &:focus {
-      background-color: #ECEDEE;
-      text-decoration: none;
-      outline: none;
-    }
+  align-items: center;
+  background: transparent;
+  border: 1px solid #DFE3E6;
+  border-radius: 50px;
+  color: #687076;
+  padding: 8px 12px 12px;
+  transition: all .15s ease;
 
-    &.report-btn, &.hide-btn {
-      i {
-        color: #7E868C;
-      }
-    }
-    span {
-      font-weight: 500;
-    }
+  &:hover {
+    color: #30A46C;
+    border-color: #30A46C;
   }
 `;
 
 return (
-  <div class="row">
-    <div class="col-1 d-sm-block d-none">
-      <a href={`#/near/widget/ProfilePage?accountId=${accountId}`}>
-        <Widget
-          src="dev-support.near/widget/ProfileImage"
-          props={{
-            accountId,
-            imageClassName: "rounded-circle w-100 h-100",
-          }}
+  <div className="d-inline-flex">
+    <Button
+      disabled={state.loading || dataLoading || !context.accountId}
+      onClick={likeClick}
+    >
+      {state.loading || dataLoading ? (
+        <span
+          class="spinner-grow spinner-grow-sm p-2"
+          role="status"
+          aria-hidden="true"
         />
-      </a>
-    </div>
-    <div class="col-sm-1 col-2">
-      <Widget
-        src="y3k.near/widget/apps.ama.button.question.upvote"
-        props={{ accountId, blockHeight }}
-      />
-    </div>
-    <div class="col-10">
-      <div class="row">
-        <div class="col">
-          <div class="row">
-            <div class="col">
-              <H2>
-                <a href={link}>{question.title}</a>
-                <i class="bi bi-arrow-right" />
-              </H2>
-              <p>{question.content.text}</p>
-            </div>
-            <div class="col-auto">
-              <div class="dropdown ms-auto">
-                <button
-                  class="btn border-0 p-0"
-                  type="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <i class="bi bi-three-dots" />
-                </button>
-                <ul class="dropdown-menu">
-                  <li>
-                    <Item>
-                      <Widget
-                        src="y3k.near/widget/apps.ama.button.question.flag"
-                        props={{
-                          accountId,
-                          blockHeight,
-                          className: "btn report-btn",
-                        }}
-                      />
-                    </Item>
-                  </li>
-                  <li>
-                    <Item>
-                      {/* Delete widget */}
-                      <Widget
-                        src="y3k.near/widget/apps.ama.button.question.delete"
-                        props={{
-                          accountId,
-                          blockHeight,
-                          admins,
-                          adminContract,
-                          className: "hide-btn",
-                          text: "Hide",
-                        }}
-                      />
-                    </Item>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <H6>
-              <div class="d-flex">
-                <Trancate>
-                  <a href={`#/near/widget/ProfilePage?accountId=${accountId}`}>
-                    {accountId}
-                  </a>
-                </Trancate>{" "}
-                &nbsp;in&nbsp;
-                <TopicName>
-                  <Widget
-                    src="y3k.near/widget/apps.ama.post.labels"
-                    props={{ labels: question.labels }}
-                  />
-                </TopicName>
-                &nbsp;&#8226;&nbsp;
-                <Widget src="mob.near/widget/TimeAgo" props={{ blockHeight }} />
-                &nbsp;ago
-              </div>
-            </H6>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <Widget
-            src="y3k.near/widget/apps.ama.comment.repliesButton"
-            props={{ accountId, blockHeight, adminContract, link }}
-          />
-        </div>
-      </div>
-    </div>
+      ) : (
+        <i class="bi bi-caret-up-fill" />
+      )}
+      {upvotes}
+    </Button>
   </div>
 );
