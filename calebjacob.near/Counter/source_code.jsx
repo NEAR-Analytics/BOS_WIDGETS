4faaceb04f236be5349875ee7ec31abe853a1de9
calebjacob.near <calebjacob.near>
2023-10-17 16:18:27 +0000

    Update Counter by calebjacob.near at 2023-10-17T16:18:27.514Z

--- calebjacob.near/Counter/source_code.jsx
+++ calebjacob.near/Counter/source_code.jsx
@@ -1,26 +1,22 @@
-// const [count, setCount] = useState(0);
-
-State.init({
-  count: 0,
-});
-
+const [count, setCount] = useState(0);
 const items = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
 
 return (
   <div>
-    <p>Count: {state.count}</p>
+    <p>Count: {count}</p>
 
-    <button
-      type="button"
-      onClick={() => State.update({ count: state.count + 1 })}
-    >
+    <button type="button" onClick={() => setCount(count + 1)}>
       +
     </button>
 
     <hr />
 
     {items.map((item) => (
-      <Widget src="calebjacob.near/widget/Simple" key={item.id} />
+      <Widget
+        src="calebjacob.near/widget/Simple"
+        key={item.id}
+        props={{ item }}
+      />
     ))}
   </div>
 );
