93cec99f6e729a343204b75a9f44c3720808963d
dapdapbos.near <dapdapbos.near>
2023-10-17 15:38:16 +0000

    Update Uniswap.Swap.AmountOutV2 by dapdapbos.near at 2023-10-17T15:38:16.227Z

--- dapdapbos.near/Uniswap.Swap.AmountOutV2/source_code.jsx
+++ dapdapbos.near/Uniswap.Swap.AmountOutV2/source_code.jsx
@@ -6,136 +6,214 @@ const {
   outputCurrency,
   inputCurrencyAmount,
   outputCurrencyAmount,
-  tradeType,
   onLoad,
   chainId,
 } = props;
 
-if (!update) return "";
+const router02List = [56, 8453];
 
-if (!inputCurrency.address || !outputCurrency.address || !inputCurrencyAmount) {
-  onLoad({});
-  return;
-}
+const is02 = router02List.indexOf(chainId) > -1;
 
-const wrapType =
-  inputCurrency.address === "native" && outputCurrency.address === wethAddress
-    ? 1
-    : inputCurrency.address === wethAddress &&
-      outputCurrency.address === "native"
-    ? 2
-    : 0;
-if (wrapType) {
-  onLoad(
-    tradeType === "in"
-      ? {
-          outputCurrencyAmount: inputCurrencyAmount,
-          loading: false,
-          noPair: false,
-        }
-      : {
-          inputCurrencyAmount: outputCurrencyAmount,
-          loading: false,
-          noPair: false,
-        }
-  );
-  return;
-}
 const currentCurrency = tradeType === "in" ? inputCurrency : outputCurrency;
 
-const currentAmount = Big(
-  tradeType === "in" ? inputCurrencyAmount : outputCurrencyAmount
-)
-  .mul(0.995)
-  .toFixed(5);
-const outCurrency = tradeType === "in" ? outputCurrency : inputCurrency;
-const RouterContract = new ethers.Contract(
-  routerAddress,
-  [
-    {
-      inputs: [
-        { internalType: "uint256", name: "amountIn", type: "uint256" },
-        { internalType: "address[]", name: "path", type: "address[]" },
-      ],
-      name: "getAmountsOut",
-      outputs: [
-        { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-  ],
-  Ethers.provider().getSigner()
-);
-const path = [
-  currentCurrency.address === "native" ? wethAddress : currentCurrency.address,
-  outCurrency.address === "native" ? wethAddress : outCurrency.address,
+const WETH_ADDRESS = props.wethAddress;
+
+const swapQuoterAbi = [
+  {
+    inputs: [
+      { internalType: "address", name: "tokenIn", type: "address" },
+      { internalType: "address", name: "tokenOut", type: "address" },
+      { internalType: "uint24", name: "fee", type: "uint24" },
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "uint160", name: "sqrtPriceLimitX96", type: "uint160" },
+    ],
+    name: "quoteExactInputSingle",
+    outputs: [{ internalType: "uint256", name: "amountOut", type: "uint256" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
 
-asyncFetch("https://api.uniswap.org/v2/quote", {
-  method: "post",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    tokenInChainId: chainId,
-    tokenIn: path[0],
-    tokenOutChainId: chainId,
-    tokenOut: path[1],
-    amount: ethers.utils
-      .parseUnits(currentAmount, currentCurrency.decimals)
-      .toString(),
-    sendPortionEnabled: false,
-    type: "EXACT_INPUT",
-    configs: [
+const swapQuoterAbi02 = [
+  {
+    inputs: [
       {
-        protocols: ["V3"],
-        enableUniversalRouter: false,
-        routingType: "CLASSIC",
-        recipient: "0xC25d79fc4970479B88068Ce8891eD9bE5799210D",
+        components: [
+          { internalType: "address", name: "tokenIn", type: "address" },
+          { internalType: "address", name: "tokenOut", type: "address" },
+          { internalType: "uint256", name: "amountIn", type: "uint256" },
+          { internalType: "uint24", name: "fee", type: "uint24" },
+          {
+            internalType: "uint160",
+            name: "sqrtPriceLimitX96",
+            type: "uint160",
+          },
+        ],
+        internalType: "struct IQuoterV2.QuoteExactInputSingleParams",
+        name: "params",
+        type: "tuple",
       },
     ],
-  }),
-})
-  .then((res) => {
-    const parsedRes = res.body;
+    name: "quoteExactInputSingle",
+    outputs: [
+      { internalType: "uint256", name: "amountOut", type: "uint256" },
+      { internalType: "uint160", name: "sqrtPriceX96After", type: "uint160" },
+      {
+        internalType: "uint32",
+        name: "initializedTicksCrossed",
+        type: "uint32",
+      },
+      { internalType: "uint256", name: "gasEstimate", type: "uint256" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
-    if (!parsedRes) throw new Error();
+const getQuoterAbi = () => {
+  if (!is02) {
+    return swapQuoterAbi;
+  }
+  return swapQuoterAbi02;
+};
 
-    const outputCurrencyAmount = parsedRes.quote.quoteDecimals;
+const { amountIn, tokenIn, tokenOut, loadAmountOut, quoterAddress, tradeType } =
+  props;
 
-    onLoad({
-      outputCurrencyAmount,
-      noPair: false,
-      fee: parsedRes.quote.route[0][0].fee,
+const feeList = [100, 500, 3000, 10000];
+
+const queryString = `${tokenIn.address}-${tokenOut.address}-${amountIn}`;
+
+if (state.cacheString !== queryString) {
+  State.update({
+    cacheString: queryString,
+    quoteDone: false,
+    quoting: false,
+  });
+}
+
+const quoteSingle = (amountIn, tokenIn, tokenOut, fee, finalList) => {
+  const iface = new ethers.utils.Interface(getQuoterAbi());
+
+  const inputs = !is02
+    ? [
+        tokenIn.address === "native" ? WETH_ADDRESS : tokenIn.address,
+        tokenOut.address === "native" ? WETH_ADDRESS : tokenOut.address,
+        fee,
+        ethers.utils.parseUnits(amountIn, tokenIn.decimals),
+        0,
+      ]
+    : [
+        {
+          tokenIn:
+            tokenIn.address === "native" ? WETH_ADDRESS : tokenIn.address,
+          tokenOut:
+            tokenOut.address === "native" ? WETH_ADDRESS : tokenOut.address,
+          amountIn: ethers.utils.parseUnits(amountIn, tokenIn.decimals),
+          fee,
+          sqrtPriceLimitX96: 0,
+        },
+      ];
+
+  const encodedData = iface.encodeFunctionData("quoteExactInputSingle", inputs);
+  console.log("encodedData: ", encodedData);
+
+  // const provider = new ethers.providers.JsonRpcProvider();
+
+  return Ethers.provider()
+    .call({
+      to: quoterAddress,
+      data: encodedData,
+    })
+    .then((data) => {
+      console.log("data: ", data);
+      const res = iface.decodeFunctionResult("quoteExactInputSingle", data);
+      console.log("res: ", res);
+
+      const rawAmountOut = Big(res.amountOut.toString()).toFixed();
+
+      const parsedAmountOut = new Big(rawAmountOut)
+        .div(Big(10).pow(tokenOut.decimals))
+        .toFixed();
+
+      return [
+        ...finalList,
+        {
+          fee: fee,
+          amountOut: parsedAmountOut,
+          success: true,
+        },
+      ];
+    })
+    .catch((e) => {
+      console.log("e1111: ", e);
+      return [
+        ...finalList,
+        {
+          fee: fee,
+          amountOut: "0",
+          success: false,
+        },
+      ];
     });
-  })
-  .catch((err) => {
-    onLoad({
-      loading: false,
-      noPair: true,
-      fee: undefined,
-    });
+};
+
+const quoteAll = () => {
+  State.update({
+    quoting: true,
   });
 
-// RouterContract.getAmountsOut(
-//   ethers.utils.parseUnits(currentAmount, currentCurrency.decimals),
-//   path
-// )
-//   .then((res) => {
-//     const amount = Big(
-//       ethers.utils.formatUnits(res[1], outCurrency.decimals)
-//     ).toPrecision(10);
-//     onLoad({
-//       outputCurrencyAmount: Big(amount).gt(0.01)
-//         ? amount
-//         : Big(amount).toFixed(10),
-//       noPair: false,
-//     });
-//   })
-//   .catch((err) => {
-//     onLoad({
-//       loading: false,
-//       noPair: true,
-//     });
-//   });
+  quoteSingle(amountIn, tokenIn, tokenOut, feeList[0], [])
+    .then((finalList0) => {
+      return quoteSingle(amountIn, tokenIn, tokenOut, feeList[1], finalList0);
+    })
+    .then((finalList1) => {
+      return quoteSingle(amountIn, tokenIn, tokenOut, feeList[2], finalList1);
+    })
+    .then((finalList2) => {
+      return quoteSingle(amountIn, tokenIn, tokenOut, feeList[3], finalList2);
+    })
+    .then((finalList3) => {
+      const maxAmountOutEstimate = finalList3.reduce((prev, current) => {
+        if (Number(prev.amountOut) > Number(current.amountOut)) {
+          return prev;
+        } else {
+          return current;
+        }
+      }, finalList3[0]);
+
+      console.log("maxAmountOutEstimate: ", maxAmountOutEstimate);
+
+      State.update({ quoteDone: true, quoting: false });
+      loadAmountOut({
+        ...maxAmountOutEstimate,
+        outputCurrencyAmount: maxAmountOutEstimate.amountOut,
+        quoteDone,
+        quoting,
+      });
+    });
+};
+
+const wrapType =
+  tokenIn.address === "native" && tokenOut.symbol === "WETH"
+    ? 1
+    : tokenIn.symbol === "WETH" && tokenOut.address === "native"
+    ? 2
+    : 0;
+
+console.log("amountIn: ", amountIn);
+
+if (Number(amountIn) > 0 && !state.quoteDone && !state.quoting) {
+  if (wrapType > 0) {
+    loadAmountOut({
+      amountOut: amountIn,
+      fee: 0,
+      success: true,
+      quoteDone: true,
+      quoting: false,
+    });
+  } else {
+    quoteAll();
+  }
+}
+return <div></div>;
