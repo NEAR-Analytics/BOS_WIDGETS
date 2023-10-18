c912b75cf7decf14e85f499ac5c95d795d43015b
jgodwill.near <jgodwill.near>
2023-10-18 10:58:41 +0000

    Update Mintbase.StoreDeploy by jgodwill.near at 2023-10-18T10:58:41.037Z

--- jgodwill.near/Mintbase.StoreDeploy/source_code.jsx
+++ jgodwill.near/Mintbase.StoreDeploy/source_code.jsx
@@ -54,6 +54,17 @@ border-radius: .7em;
   }
 `;
 
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  background: rgba(214, 214, 214, 0.2);
+  border-radius: 10px;
+  justify-content: center;
+  padding-top: 3em;
+  box-shadow: 0 0.05rem 0.05rem rgb(34 34 34 / 5%), 0 0.2rem 0.8rem rgb(34 34 34 / 8%);
+`;
+
 function fetchData(accountAddress) {
   if (!state.account) {
     State.update({
@@ -85,8 +96,40 @@ function fetchData(accountAddress) {
 
 fetchData(state.account);
 
+const fetchStoreFrontData = (accountAddress) => {
+  const response2 = fetch("https://graph.mintbase.xyz/mainnet", {
+    method: "POST",
+    headers: {
+      "mb-api-key": "anon",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `query MyQuery {
+  mb_views_nft_tokens(
+    where: {owner: {_eq: "nate.near"}, _and: {burned_timestamp: {_is_null: true}, last_transfer_timestamp: {_is_null: false}}, nft_contract_id: {_eq: "${accountAddress}"}}
+    limit: 30
+    order_by: {last_transfer_timestamp: desc}
+  ) {
+    nft_contract_id
+    title
+    description
+    media
+    last_transfer_receipt_id
+  }
+}
+`,
+    }),
+  });
+
+  State.update({
+    storeContracts: response2.body.data.mb_views_nft_tokens,
+  });
+  console.log("running2", state.storeContracts);
+};
+
 // console.log("response", state.account);
 const storeAddressChangeHandler = (storeAddress) => {
+  fetchStoreFrontData(storeAddress);
   console.log("store address: ", storeAddress);
   state.update({
     storeAddress: storeAddress,
@@ -123,6 +166,18 @@ return (
       </Card>
     ) : (
       <p>No contracts found for {state.account}</p>
+    )}
+    {state.storeContracts.length > 0 ? (
+      <Cards>
+        {state.storeContracts.map((store) => (
+          <Widget
+            src="jgodwill.near/widget/Mintbase.StoreFront"
+            props={{ storeInfo: store }}
+          />
+        ))}
+      </Cards>
+    ) : (
+      <p>No NFTs found for {state.storeAddress}</p>
     )}
   </div>
 );
