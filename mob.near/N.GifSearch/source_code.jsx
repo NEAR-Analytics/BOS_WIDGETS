8fa45cf2aea7c4d4daa973f847763867737ec19f
mob.near <mob.near>
2023-10-18 19:49:23 +0000

    Update N.GifSearch by mob.near at 2023-10-18T19:49:23.027Z

--- mob.near/N.GifSearch/source_code.jsx
+++ mob.near/N.GifSearch/source_code.jsx
@@ -1,84 +1,52 @@
-const image = props.image;
+const [show, setShow] = useState(true);
+const onHide = props.onHide ?? (() => setShow(false));
+const [searchTerm, setSearchTerm] = useState("");
 
 const Wrapper = styled.div`
   white-space: normal;
   .lightbox {
     backdrop-filter: blur(5px);
-
-    img {
-      width: 100vw;
-      height: 100vh;
-      max-width: 100%;
-      max-height: 100%;
-      object-fit: contain;
-    }
-  }
-
-  .img-wrapper {
-    width: 100%;
-    border-radius: 0.5em;
-    text-align: center;
-    overflow: hidden;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    align-content: center;
-    max-height: 24em;
-    @media(max-width: 991px) {
-      max-height: 20em;
-    }
-    cursor: pointer;
-    .img-box {
-      flex-grow: 1;
-      overflow: hidden;
-      img {
-        max-height: 24em;
-        @media(max-width: 991px) {
-          max-height: 20em;
-        }
-        width: 100%;
-        object-fit: contain;
-        border-radius: 0.5em;
-      }
-    }
-    
   }
 `;
 
-const [showLightbox, setShowLightbox] = useState(false);
-
-const imageUrl =
-  image.nft.contractId && image.nft.tokenId
-    ? `https://i.near.social/magic/large/https://near.social/magic/img/nft/${image.nft.contractId}/${image.nft.tokenId}`
-    : image.ipfs_cid
-    ? `https://ipfs.near.social/ipfs/${image.ipfs_cid}`
-    : image.url;
-
-const innerImage = (
-  <div className="img-box">
-    <img src={imageUrl} alt={props.alt} loading="lazy" />
+const content = (
+  <div
+    className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable"
+    onClick={(e) => {
+      e.stopPropagation();
+    }}
+  >
+    <div className="modal-content">
+      <div className="modal-header">
+        <div className="modal-title h4">GIF Search</div>
+        <button
+          type="button"
+          className="btn-close"
+          aria-label="Close"
+          onClick={onHide}
+        ></button>
+      </div>
+      <div className="modal-body">
+        <input
+          autoFocus
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
   </div>
 );
 
 return (
   <Wrapper>
-    <div
-      key="c-img"
-      className="img-wrapper"
-      onClick={() => setShowLightbox(true)}
-    >
-      {innerImage}
-    </div>
     <Widget
-      key="img-lightbox"
+      key="gif-search"
       src="mob.near/widget/N.Lightbox"
       loading=""
       props={{
-        show: showLightbox,
-        onHide: () => {
-          setShowLightbox(false);
-        },
-        children: innerImage,
+        show,
+        onHide,
+        children: content,
       }}
     />
   </Wrapper>
