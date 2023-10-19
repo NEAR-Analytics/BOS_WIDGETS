010448485c2679a53fbc2a007e7c9abf82a3450e
38f93b56ac78a5ce761ffd5ba4da963903f8dc81e5d0c587463787cc04365b23 <38f93b56ac78a5ce761ffd5ba4da963903f8dc81e5d0c587463787cc04365b23>
2023-10-19 02:51:23 +0000

    Update RejuvenateAI by 38f93b56ac78a5ce761ffd5ba4da963903f8dc81e5d0c587463787cc04365b23 at 2023-10-19T02:51:23.267Z

---
+++ 38f93b56ac78a5ce761ffd5ba4da963903f8dc81e5d0c587463787cc04365b23/RejuvenateAI/source_code.jsx
@@ -0,0 +1,12 @@
+// The link provided by Thirdweb pointing to the react app stored inside IPFS
+const externalAppUrl =
+  "https://bafybeidohn72k73tdzszurwxmzdmm3jg5d5xy3bibsjxig2dce762zasia.ipfs.cf-ipfs.com/";
+
+return (
+  <Widget
+    src={"wendersonpires.near/widget/NearSocialBridgeCore"}
+    props={{
+      externalAppUrl,
+    }}
+  />
+);
