9b677cd1c29686f4df221a2ec00f1906b7455736
lord1.near <lord1.near>
2023-10-19 05:01:06 +0000

    Update Leaderboard by lord1.near at 2023-10-19T05:01:06.811Z

---
+++ lord1.near/Leaderboard/source_code.jsx
@@ -0,0 +1,151 @@
+const themeColor = props.themeColor;
+
+const projecttheme = {
+  height: "130px",
+  align: "center",
+  description: " ",
+  brand: "Leaderboard of",
+  fontsize: "40px",
+  fontweight: "40px",
+  afterbrand: "active registorer",
+  afterbrandcolor: themeColor?.dynamic_header?.afterbrandcolor || "#789efb",
+  fontbrand: " Arial, sans-serif",
+  color1brand: themeColor?.dynamic_header?.color1brand || "#000",
+  color2brand: themeColor?.dynamic_header?.color2brand || "#806ce1",
+  colordescription: themeColor?.dynamic_header?.colordescription || "#806ce1",
+  fontsubtitle: " Arial, sans-serif",
+  background:
+    themeColor?.dynamic_header?.background ||
+    "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
+};
+
+const queryHashes = [{ id: 1, hash: "026a231f-2ab3-4eaf-ad76-aea14326c3ee" }];
+
+State.init({
+  light: true,
+  data: [],
+  isLoading: true,
+  error: [],
+});
+
+const formatNumber = (num) => {
+  if ((num ?? NaN) === NaN) return "-";
+  if (num >= 1000000000) {
+    return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "b";
+  }
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "m";
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "k";
+  }
+  return num.toFixed(4);
+};
+
+const handleData = () => {
+  const data = {};
+  const errors = [];
+  queryHashes.forEach(({ hash, id }) => {
+    const result = fetchData(hash);
+    if (result.error) errors.push(`hash${id} : ${result.error}`);
+    data[`hash${id}`] = {
+      ...result,
+      id,
+    };
+  });
+
+  if (Object.values(data).every((d) => !d.isLoading)) {
+    State.update({
+      data: data,
+      error: [...state.error, ...errors],
+      isLoading: false,
+    });
+  }
+};
+
+const fetchData = (hash) => {
+  const data = fetch(
+    `https://api.flipsidecrypto.com/api/v2/queries/${hash}/data/latest`,
+    {
+      subscribe: true,
+      method: "GET",
+      headers: {
+        Accept: "*/*",
+      },
+    }
+  );
+  const result = {
+    data: (data && data.body) || null,
+    error: (data && !data.ok && (data.status || data.error)) || null,
+    isLoading: !data && !error,
+  };
+  return result;
+};
+
+if (state.isLoading) {
+  handleData();
+}
+
+if (state.error.length > 0) {
+  function hide() {
+    const errors = state.error;
+    errors.shift();
+    if (errors.length > 0) setTimeout(hide, 2500);
+    State.update({ error: errors });
+  }
+  setTimeout(hide, 2500);
+}
+
+return (
+  <div className="container-fluid py-2">
+    <div className="pl-2">
+      <div
+        className="my-4 shadow-sm  rounded-4"
+        style={{ background: themeColor?.sbt_area?.section_bg }}
+      >
+        <Widget src="lord1.near/widget/header-dynamic" props={projecttheme} />
+        <div className="row g-4 w-100 pb-2 mx-0">
+          <div className="col-12">
+            <div
+              style={{ background: themeColor?.sbt_area?.card_bg }}
+              className="shadow-sm rounded-2 overflow-auto"
+            >
+              <Widget
+                src="lord1.near/widget/table-pagination"
+                props={{
+                  themeColor: { table_pagination: themeColor.table_pagination },
+                  data: state.data?.hash1?.data,
+                  columns: [
+                    { title: "Rank", key: "rank" },
+                    { title: "Wallet", key: "wallet" },
+                    { title: "Submissions", key: "submissions" },
+                    { title: "Candidates", key: "sbt" },
+                    { title: "# Accepted", key: "number_accepted" },
+                    { title: "% Accepted", key: "percent_accepted" },
+                  ],
+                  rowsCount: 15,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="toast-container position-fixed bottom-0 end-0 p-3">
+        {state.error.length > 0 &&
+          state.error.map((er) => (
+            <div
+              className="toast show align-items-center text-bg-danger border-0"
+              role="alert"
+              aria-live="assertive"
+              aria-atomic="true"
+            >
+              <div className="d-flex">
+                <div className="toast-body">{er}</div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  </div>
+);
