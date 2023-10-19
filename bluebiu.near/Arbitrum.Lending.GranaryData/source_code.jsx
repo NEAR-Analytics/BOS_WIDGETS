926821f5ec1ffb161a4031938313927722c22d24
bluebiu.near <bluebiu.near>
2023-10-19 17:01:43 +0000

    Update Arbitrum.Lending.GranaryData by bluebiu.near at 2023-10-19T17:01:43.640Z

--- bluebiu.near/Arbitrum.Lending.GranaryData/source_code.jsx
+++ bluebiu.near/Arbitrum.Lending.GranaryData/source_code.jsx
@@ -469,10 +469,15 @@ const formateTokenReserveData = (
 
 const formatUserReserveData = (data, address) => {
   const underlyingAsset = Tokens[address];
+
+  const scaledATokenBalance = Big(data[0].toString())
+    .div(Big(10).pow(underlyingAsset.decimals))
+    .toFixed();
+
   const scaledATokenBalanceUsd = Big(data[0].toString())
     .div(Big(10).pow(underlyingAsset.decimals))
     .times(state.tokensPrice[address])
-    .toFixed(4);
+    .toFixed();
 
   const aTokenBalance = Big(data[0].toString())
     .div(Big(10).pow(underlyingAsset.decimals))
@@ -483,17 +488,18 @@ const formatUserReserveData = (data, address) => {
   const scaledVariableDebt = Big(data[2].toString())
     .div(Big(10).pow(underlyingAsset.decimals))
 
-    .toFixed(4);
+    .toFixed();
 
   const scaledVariableDebtUsd = Big(data[2].toString())
     .div(Big(10).pow(underlyingAsset.decimals))
     .times(state.tokensPrice[address])
-    .toFixed(4);
+    .toFixed();
 
   const userReserveParsed = {
     address,
     underlyingAsset,
     scaledATokenBalanceUsd,
+    scaledATokenBalance,
     usageAsCollateralEnabledOnUser,
     scaledVariableDebt,
     scaledVariableDebtUsd,
@@ -703,7 +709,7 @@ if (
     marketData[address] = {
       ...marketData[address],
       ...d,
-      userSupply: d.scaledATokenBalanceUsd,
+      userSupply: d.scaledATokenBalance,
       userBorrow: d.scaledVariableDebt,
     };
   });
