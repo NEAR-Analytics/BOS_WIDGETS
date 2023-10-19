4cd56948c491b015fce5fae01252ab3760374709
bo.near <bo.near>
2023-10-19 05:15:40 +0000

    Update gigs-board.feature.post-search.by-author by bo.near at 2023-10-19T05:15:40.971Z

--- bo.near/gigs-board.feature.post-search.by-author/source_code.jsx
+++ bo.near/gigs-board.feature.post-search.by-author/source_code.jsx
@@ -134,9 +134,7 @@ const DevHub = {
 };
 /* END_INCLUDE: "core/adapter/dev-hub" */
 
-const selectedAuthors = props.author
-  ? [{ name: props.author }]
-  : [];
+const selectedAuthors = props.author ? [{ name: props.author }] : [];
 
 const authors = DevHub.get_all_authors();
 
