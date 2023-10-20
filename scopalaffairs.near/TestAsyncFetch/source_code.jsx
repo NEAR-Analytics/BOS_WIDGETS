17a29d79faf39fe4d48282d50df6d1fabddcd425
scopalaffairs.near <scopalaffairs.near>
2023-10-20 17:53:44 +0000

    Update TestAsyncFetch by scopalaffairs.near at 2023-10-20T17:53:44.170Z

--- scopalaffairs.near/TestAsyncFetch/source_code.jsx
+++
@@ -1,8 +0,0 @@
-function reportUptime() {
-  asyncFetch("https://rpc.mainnet.near.org/status").then((res) => {
-    const uptime = res.body.uptime_sec;
-    Near.call("uptime.near", "reportUptime", { uptime });
-  });
-}
-
-return <button onClick={reportUptime}>Report Uptime</button>;
