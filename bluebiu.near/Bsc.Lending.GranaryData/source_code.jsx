802205a4467f167d4d4d2e4d99dc9ba6d8c1089f
bluebiu.near <bluebiu.near>
2023-10-20 09:49:22 +0000

    Update Bsc.Lending.GranaryData by bluebiu.near at 2023-10-20T09:49:22.543Z

--- bluebiu.near/Bsc.Lending.GranaryData/source_code.jsx
+++ bluebiu.near/Bsc.Lending.GranaryData/source_code.jsx
@@ -647,13 +647,8 @@ if (
 
   let userTotalSupplyUsd = Big(0);
   let userTotalBorrowUsd = Big(0);
-  let totalCollateralUsd = Big(0);
 
   parsedData.forEach((data) => {
-    if (data.usageAsCollateralEnabledOnUser) {
-      totalCollateralUsd = totalCollateralUsd.plus(data.scaledATokenBalanceUsd);
-    }
-
     userTotalSupplyUsd = userTotalSupplyUsd.plus(data.scaledATokenBalanceUsd);
 
     userTotalBorrowUsd = userTotalBorrowUsd.plus(data.scaledVariableDebtUsd);
@@ -663,7 +658,6 @@ if (
     userData: {
       userTotalSupplyUsd: userTotalSupplyUsd.toString(),
       userTotalBorrowUsd: userTotalBorrowUsd.toString(),
-      totalCollateralUsd: totalCollateralUsd.toString(),
       parsedData,
     },
   });
@@ -699,7 +693,9 @@ if (
     };
   });
 
-  Object.keys(marketData).forEach((address) => {
+  let totalCollateralUsd = Big(0);
+
+  Object.keys(marketData).forEach((address, i) => {
     const market = marketData[address];
 
     const { netApy: netApyRaw } = market;
@@ -712,7 +708,18 @@ if (
     market.wethGateway = wethGateway;
 
     market.address = market.aTokenAddress;
+
+    if (userData.parsedData[i]) {
+      const data = userData.parsedData[i];
+      if (data.usageAsCollateralEnabledOnUser) {
+        totalCollateralUsd = totalCollateralUsd
+          .plus(data.scaledATokenBalanceUsd)
+          .times(market.loanToValue / 100);
+      }
+    }
   });
+
+  userData.totalCollateralUsd = totalCollateralUsd.toFixed();
 
   userData.netApy = netApy.toFixed(2);
 
