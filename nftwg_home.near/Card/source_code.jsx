dabf74c1202ff80faab297d610e12dc128410869
nftwg_home.near <nftwg_home.near>
2023-10-20 11:54:24 +0000

    Update Card by nftwg_home.near at 2023-10-20T11:54:24.315Z

--- nftwg_home.near/Card/source_code.jsx
+++ nftwg_home.near/Card/source_code.jsx
@@ -1,83 +1,102 @@
+const Owner = "nftwg_home.near";
+const props = [
+  {
+    title: "Socializer",
+    imageSrc:
+      "https://cdn.discordapp.com/attachments/1135845507189702748/1162116592906293341/image.png?ex=653ac46a&is=65284f6a&hm=65dad1554346d62e5e55c6917328b4a1d5ffad8c1352329ada45b47c85bdaeff&",
+    description: "Socializer is an interactive rewards portal for Near Social.",
+    buttonText: "In Progress",
+    fontSize: 20,
+  },
+  {
+    title: "Humans Of Near",
+    imageSrc: "https://humans.nearverselabs.com/logo.png",
+    description:
+      "Humans of Near is a Map of IAM Human SBT holders. Built by the NFT Workgroup.",
+    buttonText: "Completed",
+    fontSize: 20,
+    href: "https://near.org/humans-of-near.near/widget/humans.nearverselabs.com",
+  },
+];
 const Card = styled.div`
-box-sizing: border-box;
-position: absolute;
-width: 400px;
-height: 313px;
-background: #FFFFFF;
-border: 1px solid #A0A0A0;
-border-radius: 10px;
-background: #D9D9D9;
-
-
+  box-sizing: border-box;
+  width: 400px;
+  height: 313px;
+  background: #FFFFFF;
+  border: 1px solid #A0A0A0;
+  border-radius: 10px;
+  background: #D9D9D9;
+  margin: 10px; /* Add margin for spacing between cards */
+ position: relative;
 `;
+
 const CardContent = styled.div`
-position: absolute;
-width: 400px;
-height: 13px;
+  width: 400px;
+  height: 13px;
+  display:flex;
+  position: relative;
+  
 `;
+
 const AppLinkButton = styled.div`
-/* Frame 2 */
-
-box-sizing: border-box;
-
-/* Auto layout */
-display: flex;
-flex-direction: row;
-justify-content: flex-end;
-align-items: center;
-padding: 16px 132px;
-gap: 10px;
-cursor:pointer;
-
-position: absolute;
-width: 352px;
-height: 48px;
-
-
-background: #FFFFFF;
-border: 1px solid #F0F0F0;
-border-radius: 79px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 16px 132px;
+  gap: 10px;
+  cursor: pointer;
+  width: 352px;
+  height: 48px;
+  background: #FFFFFF;
+  border: 1px solid #F0F0F0;
+  border-radius: 79px;
+  position: relative;
 `;
+
 const ApplinkText = styled.div`
-/* View Details */
-
-width: 88px;
-height: 16px;
-
-font-family: inherit;
-font-style: normal;
-font-weight: 400;
-font-size: 16px;
-line-height: 100%;
-/* identical to box height, or 16px */
-
-color: #333333;
-
-
-/* Inside auto layout */
-flex: none;
-order: 0;
-flex-grow: 0;
-
+  width: 88px;
+  height: 16px;
+  font-family: inherit;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 100%;
+  color: #333333;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  position: relative;
 `;
+
 return (
-  <Card style={{ top: 400, left: 470 }}>
-    <CardContent style={{ top: 50, left: 210, fontSize: 20 }}>
-      Socializer.
-    </CardContent>
-    <CardContent style={{ top: -10, left: 10, fontSize: 20 }}>
-      <img
-        style={{ width: 132, height: 120 }}
-        src="https://cdn.discordapp.com/attachments/1135845507189702748/1162116592906293341/image.png?ex=653ac46a&is=65284f6a&hm=65dad1554346d62e5e55c6917328b4a1d5ffad8c1352329ada45b47c85bdaeff&"
-      />
-    </CardContent>
-    <CardContent style={{ top: 140, left: 15 }}>
-      Socializer is an interactive rewards portal for Near Socaial
-    </CardContent>
-    <CardContent style={{ top: 240, left: 20 }}>
-      <AppLinkButton>
-        <ApplinkText>In Progress</ApplinkText>
-      </AppLinkButton>
-    </CardContent>
-  </Card>
+  <div style={{ display: "flex" }}>
+    {props.map((item, index) => (
+      <Card key={index}>
+        <CardContent style={{ top: 10, left: 10 }}>
+          <img
+            style={{ width: 200, height: 90 }}
+            src={item.imageSrc}
+            alt={item.title}
+          />
+        </CardContent>
+        <CardContent style={{ fontSize: 20, top: 50, left: 230 }}>
+          {item.title}
+        </CardContent>
+
+        <CardContent style={{ fontSize: 16, top: 150, left: 10 }}>
+          {item.description}
+        </CardContent>
+        <CardContent>
+          <AppLinkButton style={{ top: 200, left: 20 }}>
+            <a href={item.href}>
+              {" "}
+              <ApplinkText>{item.buttonText}</ApplinkText>
+            </a>
+          </AppLinkButton>
+        </CardContent>
+      </Card>
+    ))}
+  </div>
 );
