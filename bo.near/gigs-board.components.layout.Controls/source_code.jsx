81cc8126be99a3ca7569bf364421c340dbb1d849
bo.near <bo.near>
2023-10-20 03:20:02 +0000

    Update gigs-board.components.layout.Controls by bo.near at 2023-10-20T03:20:02.877Z

--- bo.near/gigs-board.components.layout.Controls/source_code.jsx
+++ bo.near/gigs-board.components.layout.Controls/source_code.jsx
@@ -2,6 +2,7 @@
 const nearDevGovGigsContractAccountId =
   props.nearDevGovGigsContractAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
 const nearDevGovGigsWidgetsAccountId =
   props.nearDevGovGigsWidgetsAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
@@ -13,6 +14,7 @@ function widget(widgetName, widgetProps, key) {
     nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
     referral: props.referral,
   };
+
   return (
     <Widget
       src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
@@ -24,38 +26,65 @@ function widget(widgetName, widgetProps, key) {
 
 function href(widgetName, linkProps) {
   linkProps = { ...linkProps };
+
   if (props.nearDevGovGigsContractAccountId) {
     linkProps.nearDevGovGigsContractAccountId =
       props.nearDevGovGigsContractAccountId;
   }
+
   if (props.nearDevGovGigsWidgetsAccountId) {
     linkProps.nearDevGovGigsWidgetsAccountId =
       props.nearDevGovGigsWidgetsAccountId;
   }
+
   if (props.referral) {
     linkProps.referral = props.referral;
   }
+
   const linkPropsQuery = Object.entries(linkProps)
+    .filter(([_key, nullable]) => (nullable ?? null) !== null)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
+
   return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
     linkPropsQuery ? "?" : ""
   }${linkPropsQuery}`;
 }
 /* END_INCLUDE: "common.jsx" */
 
+const buttonStyle = {
+  backgroundColor: "#0C7283",
+  color: "#f3f3f3",
+};
+
 return (
-  <div class="d-flex flex-row-reverse">
-    <a
-      class="btn btn-light"
-      style={{
-        backgroundColor: "#0C7283",
-        color: "#f3f3f3",
-      }}
-      href={href("Create", { labels: props.labels })}
-    >
-      <i class="bi bi-plus-circle-fill"></i>
-      Submit Idea / Solution
-    </a>
+  <div className="d-flex flex-row-reverse" className={props.className}>
+    {props.href ? (
+      <a
+        className="btn btn-light text-decoration-none"
+        style={buttonStyle}
+        href={props.href}
+      >
+        <i
+          className="bi"
+          className={props.icon ? props.icon : "bi-plus-circle-fill"}
+        />
+
+        {props.title}
+      </a>
+    ) : (
+      <button
+        className="btn btn-light"
+        style={buttonStyle}
+        onClick={props.onClick}
+      >
+        <i
+          className="bi"
+          className={props.icon ? props.icon : "bi-plus-circle-fill"}
+        />
+
+        {props.title}
+      </button>
+    )}
   </div>
 );
