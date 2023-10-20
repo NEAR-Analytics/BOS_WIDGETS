720f51b125a8045ed6b866b3b2ef546dfd912286
silkking.near <silkking.near>
2023-10-20 23:34:49 +0000

    Update lib.comment by silkking.near at 2023-10-20T23:34:49.871Z

--- silkking.near/lib.comment/source_code.jsx
+++ silkking.near/lib.comment/source_code.jsx
@@ -1,24 +1,13 @@
-const { isTest, stateUpdate, functionsToCallByLibrary, callLibs, baseAction } =
-  props;
-const functionsToCall = functionsToCallByLibrary.comment;
-
-const currentVersion = "0.0.2";
-
-const prodAction = `${baseAction}_v${currentVersion}`;
-const testAction = `test_${prodAction}`;
-const action = isTest ? testAction : prodAction;
-
-// const authorForWidget =
-//   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
-// const authorForWidget = "sayalot.near";
-// const authorForWidget = "kenrou-it.near";
-const authorForWidget = "silkking.near";
-const libSrcArray = [`${authorForWidget}/widget/SayALot.lib.SBT`];
-
-State.init({
-  libCalls: [],
-  libsCalls: { SBT: [] },
-});
+const {
+  isTest,
+  stateUpdate,
+  functionsToCallByLibrary,
+  /*callLibs,*/
+  baseAction,
+  widgets,
+} = props;
+const libName = "comment"; // EDIT: set lib name
+const functionsToCall = functionsToCallByLibrary[libName];
 
 let resultFunctionsToCallByLibrary = Object.assign(
   {},
@@ -26,10 +15,48 @@ let resultFunctionsToCallByLibrary = Object.assign(
 );
 let resultFunctionsToCall = [];
 
+const currentVersion = "0.0.2"; // EDIT: Set version
+
+// const prodAction = `${baseAction}_v${currentVersion}`; // TODO consider versions
+const prodAction = `${baseAction}`;
+const testAction = `test_${prodAction}`;
+const action = isTest ? testAction : prodAction;
+
+// START LIB CALLS SECTION
+// interface FunctionCall {
+//     functionName: string,
+//     key: string, // The state of the caller will be updated with this string as a key
+//     props: Record<string, any> // function parameters as object
+// }
+
+// type LibsCalls = Record<string, FunctionCall> // Key is lib name after lib.
+
+const libSrcArray = [widgets.libSBT]; // string to lib widget // EDIT: set libs to call
+
+const libsCalls = {};
+libSrcArray.forEach((libSrc) => {
+  const libName = libSrc.split("lib.")[1];
+  libsCalls[libName] = [];
+});
+
+State.init({
+  libsCalls, // is a LibsCalls object
+});
+// END LIB CALLS SECTION
+
+function log(message) {
+  console.log(`lib.${libName}`, message);
+}
+
+function logError(message) {
+  console.error(`lib.${libName}`, message);
+}
+
 function libStateUpdate(obj) {
   State.update(obj);
 }
 
+// START LIB FUNCTIONS: EDIT set functions you need
 function setAreValidUsers(accountIds, sbtsNames) {
   const newLibCalls = Object.assign({}, state.libsCalls);
   accountIds.forEach((accountId) => {
@@ -169,7 +196,9 @@ function getValidComments(props) {
   }
   return finalComments;
 }
+// END LIB FUNCTIONS
 
+// EDIT: set functions you want to export
 function callFunction(call) {
   if (call.functionName === "createComment") {
     return createComment(call.props);
@@ -180,6 +209,43 @@ function callFunction(call) {
   }
 }
 
+// EDIT: set versions you want to handle, considering their action to Social.index and the way to transform to one version to another (normalization)
+const versions = {
+  old: {
+    normalizationFunction: normalizeOldToV_0_0_1,
+    action: baseAction,
+  },
+  "v0.0.1": {
+    normalizationFunction: normalizeFromV0_0_1ToV0_0_2,
+    action: `${baseAction}_v0.0.1`,
+  },
+  "v0.0.2": {
+    normalizationFunction: normalizeFromV0_0_2ToV0_0_3,
+    action: `${baseAction}_v0.0.2`,
+  },
+};
+
+function normalizeLibData(libDataByVersion) {
+  let libData;
+
+  Object.keys(versions).forEach((version, index, array) => {
+    const normFn = versions[version].normalizationFunction;
+    const normLibData = libDataByVersion[index].map((libData, i) => {
+      return normFn(libData);
+    });
+
+    if (index + 1 === array.length) {
+      // Last index
+      libData = normLibData;
+      return;
+    }
+    libDataByVersion[index + 1] =
+      libDataByVersion[index + 1].concat(normLibData);
+  });
+
+  return libData;
+}
+
 if (functionsToCall && functionsToCall.length > 0) {
   const updateObj = Object.assign({}, functionsToCallByLibrary);
   resultFunctionsToCall = [...functionsToCall];
@@ -187,15 +253,59 @@ if (functionsToCall && functionsToCall.length > 0) {
     updateObj[call.key] = callFunction(call);
   });
 
-  resultFunctionsToCallByLibrary.article = resultFunctionsToCall;
+  resultFunctionsToCallByLibrary[libName] = resultFunctionsToCall;
   updateObj.functionsToCallByLibrary = resultFunctionsToCallByLibrary;
   stateUpdate(updateObj);
 }
 
+const a = getValidComments({
+  id: "",
+  articleSbts: ["public"],
+});
+
+function callLibs(
+  src,
+  stateUpdate,
+  functionsToCallByLibrary,
+  extraProps,
+  callerWidget
+) {
+  // if (callerWidget === "All articles list") {
+  // console.log(
+  //   -1,
+  //   `Call libs props ${callerWidget}: `,
+  //   src,
+  //   functionsToCallByLibrary,
+  //   callLibs
+  // );
+  // }
+
+  return (
+    <Widget
+      src={src}
+      props={{
+        isTest,
+        stateUpdate,
+        functionsToCallByLibrary,
+        callLibs,
+        widgets,
+        ...extraProps,
+      }}
+    />
+  );
+}
+log(a);
+
 return (
   <>
     {libSrcArray.map((src) => {
-      return callLibs(src, libStateUpdate, state.libsCalls, "lib.comment");
+      return callLibs(
+        src,
+        libStateUpdate,
+        state.libsCalls,
+        {},
+        `lib.${libName}`
+      );
     })}
   </>
 );
