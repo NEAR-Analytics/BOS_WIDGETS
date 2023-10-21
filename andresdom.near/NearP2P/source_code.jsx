262df417fd09ef9a7f3ba9a1ce88c3df2f5375ba
andresdom.near <andresdom.near>
2023-10-21 18:11:17 +0000

    Update NearP2P by andresdom.near at 2023-10-21T18:11:17.650Z

--- andresdom.near/NearP2P/source_code.jsx
+++ andresdom.near/NearP2P/source_code.jsx
@@ -5,7 +5,6 @@ return (
     <div class="container border border-info p-1 text-center">
       <h1>Hello</h1>
       <p> {greeting} </p>
-      <a className="btn mb-2 mt-2 px-8 py-1">Sell</a>
     </div>
   </>
 );
