18295a5e2da70339d92aaca2eb2730131ef53964
andresdom.near <andresdom.near>
2023-10-21 17:48:12 +0000

    Update NearP2P by andresdom.near at 2023-10-21T17:48:12.316Z

--- andresdom.near/NearP2P/source_code.jsx
+++ andresdom.near/NearP2P/source_code.jsx
@@ -2,7 +2,7 @@ let greeting = "Hello develpers";
 
 return (
   <>
-    <div class="container border border-success p-1 text-center">
+    <div class="container border border-info p-1 text-center">
       <h1>Hello</h1>
       <p> {greeting} </p>
     </div>
