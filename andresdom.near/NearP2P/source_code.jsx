ac404d9135db10d40e3453af0e436510affe86ee
andresdom.near <andresdom.near>
2023-10-21 17:54:05 +0000

    Update NearP2P by andresdom.near at 2023-10-21T17:54:05.400Z

--- andresdom.near/NearP2P/source_code.jsx
+++ andresdom.near/NearP2P/source_code.jsx
@@ -1,10 +1,6 @@
 let greeting = "Hello develpers";
 
 const Button = styled.button`
-  /* Adapt the colors based on primary prop */
-  background: ${(props) => (props.primary ? "palevioletred" : "white")};
-  color: ${(props) => (props.primary ? "white" : "palevioletred")};
-
   background-color: rgb(118, 207, 118);
   border-color: rgb(118, 207, 118);
   color: white;
