19e7c7156420b79ab52374ef21b7f2e176116bf2
andresdom.near <andresdom.near>
2023-10-21 17:52:19 +0000

    Update NearP2P by andresdom.near at 2023-10-21T17:52:19.020Z

--- andresdom.near/NearP2P/source_code.jsx
+++ andresdom.near/NearP2P/source_code.jsx
@@ -5,11 +5,12 @@ const Button = styled.button`
   background: ${(props) => (props.primary ? "palevioletred" : "white")};
   color: ${(props) => (props.primary ? "white" : "palevioletred")};
 
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 10px;
+  background-color: rgb(118, 207, 118);
+  border-color: rgb(118, 207, 118);
+  color: white;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 5px;
 `;
 
 return (
