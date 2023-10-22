9d5fc65a5783f1953cb05cd762afe0a84da46f7d
magicbuild.near <magicbuild.near>
2023-10-22 18:48:03 +0000

    Update builder by magicbuild.near at 2023-10-22T18:48:03.958Z

--- magicbuild.near/builder/source_code.jsx
+++ magicbuild.near/builder/source_code.jsx
@@ -317,7 +317,7 @@ const getArgsFromMethod = (fName, fIndex) => {
                           [argName]: typeItem.value,
                         })
                       ).toString("base64"),
-                      finality: "optimistic",
+                      finality: "final",
                     },
                     id: 154,
                     jsonrpc: "2.0",
