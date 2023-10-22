ff7d65f252a012c45a4ffe0d4e470706757a1c01
potlock.near <potlock.near>
2023-10-22 21:18:47 +0000

    Update Project.ListPage by potlock.near at 2023-10-22T21:18:47.391Z

---
+++ potlock.near/Project.ListPage/source_code.jsx
@@ -0,0 +1,199 @@
+const ownerId = "potlock.near";
+const registryId = "registry1.tests.potlock.near"; // TODO: update when registry is deployed
+
+const IPFS_BASE_URL = "https://nftstorage.link/ipfs/";
+const DEFAULT_BANNER_IMAGE_URL =
+  IPFS_BASE_URL + "bafkreih4i6kftb34wpdzcuvgafozxz6tk6u4f5kcr2gwvtvxikvwriteci";
+const DEFAULT_PROFILE_IMAGE_URL =
+  IPFS_BASE_URL + "bafkreibwq2ucyui3wmkyowtzau6txgbsp6zizy4l2s5hkymsyv6tc75j3u";
+
+const getImageUrlFromSocialImage = (image) => {
+  if (image.url) {
+    return image.url;
+  } else if (image.ipfs_cid) {
+    return IPFS_BASE_URL + image.ipfs_cid;
+  }
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const HeroOuter = styled.div`
+  padding: 136px 64px;
+`;
+
+const HeroInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Separator = styled.div`
+  width: 100%;
+  height: 96px;
+  background-color: #f8f8f8;
+`;
+
+const SectionHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+
+const SectionTitle = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  color: #2e2e2e;
+  font-family: Mona-Sans;
+`;
+
+const ProjectsCount = styled.div`
+  color: #7b7b7b;
+  font-size: 24px;
+  font-weight: 400;
+  margin-left: 32px;
+`;
+
+const ProjectsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 96px 64px;
+`;
+
+State.init({
+  registeredProjects: null, // TODO: change this back to null
+  // registeredProjects: sampleProjects,
+  getRegisteredProjectsError: "",
+});
+
+if (context.accountId && !state.registeredProjects) {
+  Near.asyncView(registryId, "get_projects", {})
+    .then((projects) => {
+      // get social data for each project
+      // name
+      // description
+      // bannerImage
+      // profileImage
+      // category
+      // horizon stuff, e.g. tags
+      Near.asyncView("social.near", "get", {
+        keys: projects.map((project) => `${project.id}/profile/**`),
+      }).then((socialData) => {
+        console.log("social data: ", socialData);
+        const formattedProjects = projects.map((project) => {
+          console.log("project line 153: ", project);
+          const profileData = socialData[project.id]?.profile;
+          console.log("profileData line 154: ", profileData);
+          let profileImageUrl = DEFAULT_PROFILE_IMAGE_URL;
+          if (profileData.image) {
+            const imageUrl = getImageUrlFromSocialImage(profileData.image);
+            if (imageUrl) profileImageUrl = imageUrl;
+          }
+          // get banner image URL
+          let bannerImageUrl = DEFAULT_BANNER_IMAGE_URL;
+          if (profileData.backgroundImage) {
+            const imageUrl = getImageUrlFromSocialImage(profileData.backgroundImage);
+            if (imageUrl) bannerImageUrl = imageUrl;
+          }
+          const formatted = {
+            id: project.id,
+            name: profileData.name ?? "",
+            description: profileData.description ?? "",
+            bannerImageUrl,
+            profileImageUrl,
+            tags: [profileData.category.text ?? ""], // TODO: change this to get tags from horizon/social
+          };
+          console.log("formatted: ", formatted);
+          return formatted;
+        });
+        console.log("formatted projects: ", formattedProjects);
+        State.update({ registeredProjects: formattedProjects });
+      });
+    })
+    .catch((e) => {
+      console.log("error getting projects: ", e);
+      State.update({ getRegisteredProjectsError: e });
+    });
+}
+
+console.log("state in ListPage: ", state);
+
+return (
+  <Container>
+    {!state.registeredProjects ? (
+      <div class="spinner-border text-secondary" role="status" />
+    ) : (
+      <>
+        <Widget
+          src={`${ownerId}/widget/Components.Header`}
+          props={{
+            title1: "Transforming",
+            title2: "Funding for Public Goods",
+            description:
+              "Lorem ipsum dolor sit amet consectetur. Vel sit nunc in nunc. Viverra arcu eu sed consequat.",
+            centered: true,
+            buttonPrimary: (
+              <Widget
+                src={`${ownerId}/widget/Buttons.ActionButton`}
+                props={{
+                  type: "primary",
+                  text: "Explore projects",
+                  disabled: false,
+                }}
+              />
+            ),
+            buttonSecondary: (
+              <Widget
+                src={`${ownerId}/widget/Buttons.NavigationButton`}
+                props={{
+                  type: "secondary",
+                  text: "Create project",
+                  disabled: false,
+                  href: `?tab=createproject`,
+                }}
+              />
+            ),
+          }}
+        />
+        <Separator />
+        {/* <SectionTitle>Featured projects</SectionTitle> */}
+        {/* <Widget
+      src={`${ownerId}/widget/Project.Carousel`}
+      props={{
+        projects: sampleProjects,
+      }}
+    /> */}
+        <ProjectsContainer>
+          <SectionHeader>
+            <SectionTitle>All projects</SectionTitle>
+            <ProjectsCount>{state.registeredProjects.length}</ProjectsCount>
+          </SectionHeader>
+          <Widget
+            src={`${ownerId}/widget/Project.ListSection`}
+            props={{
+              projects: state.registeredProjects,
+              renderItem: (project) => (
+                <Widget
+                  src={`${ownerId}/widget/Project.Card`}
+                  props={{
+                    project,
+                  }}
+                />
+              ),
+            }}
+          />
+        </ProjectsContainer>
+      </>
+    )}
+  </Container>
+);
