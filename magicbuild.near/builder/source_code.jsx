11a517b448d91774500b7dc20ef6ae930157c33f
magicbuild.near <magicbuild.near>
2023-10-23 18:56:00 +0000

    Update builder by magicbuild.near at 2023-10-23T18:56:00.751Z

---
+++ magicbuild.near/builder/source_code.jsx
@@ -0,0 +1,435 @@
+State.init({
+  id: props.id ? props.id : null,
+  contractAddress: props.address ? props.address : "",
+  cMethod: props.abi.body.functions ? props.abi.body.functions : [],
+  rpcUrl: "https://rpc.near.org/",
+  archivalRpc: "https://archival-rpc.mainnet.near.org",
+  nearBlockRpc: "https://api.nearblocks.io/",
+  fName,
+  fAction: "view",
+  fLabel,
+  cMerr,
+  res,
+  cAerr,
+});
+const header = {
+  "Content-Type": "application/json",
+};
+const opGet = {
+  headers: header,
+  method: "GET",
+};
+const asyncIntervals = [];
+
+const runAsyncInterval = (cb, interval, intervalIndex) => {
+  cb();
+  if (asyncIntervals[intervalIndex].run) {
+    asyncIntervals[intervalIndex].id = setTimeout(
+      () => runAsyncInterval(cb, interval, intervalIndex),
+      interval
+    );
+  }
+};
+const setAsyncInterval = (cb, interval) => {
+  if (cb && typeof cb === "function") {
+    const intervalIndex = asyncIntervals.length;
+    asyncIntervals.push({ run: true, id: id });
+    runAsyncInterval(cb, interval, intervalIndex);
+    return intervalIndex;
+  } else {
+    throw new Error("Callback must be a function");
+  }
+};
+const clearAsyncInterval = (intervalIndex) => {
+  if (asyncIntervals[intervalIndex].run) {
+    clearTimeout(asyncIntervals[intervalIndex].id);
+    asyncIntervals[intervalIndex].run = false;
+  }
+};
+const cFunc = (e, type) => {
+  const data = e.target.value;
+  if (type == "name") State.update({ fName: data });
+  if (type == "label") State.update({ fLabel: data });
+  if (type == "action") State.update({ fAction: data });
+  if (type == "address") State.update({ contractAddress: data.toLowerCase() });
+};
+const cep = "magicbuild.near";
+const onCreateArgs = (fName, fIndex) => {
+  State.update({ cAerr: { [fName]: null } });
+  const arg = {
+    name: "",
+    label: "",
+    type_schema: {
+      type: "string",
+    },
+    value: "",
+  };
+  const abiMethod = state.cMethod;
+  abiMethod[fIndex].params.args.push(arg);
+  State.update({ cMethod: abiMethod });
+};
+const cMLabel = (e, fIdx, type) => {
+  const value = e.target.value;
+  const a = state.cMethod;
+  if (type == "method") a[fIdx].label = value;
+  if (type == "button") a[fIdx].button = value;
+  if (type == "gas") a[fIdx].gas = parseInt(value);
+  if (type == "deposit") a[fIdx].deposit = parseInt(value);
+  if (type == "remove") a.splice(fIdx, 1);
+  State.update({ cMethod: a });
+};
+const cAD = (e, fIdx, aIdx, type) => {
+  const value = e.target.value;
+  const a = state.cMethod;
+  if (type == "name") a[fIdx].params.args[aIdx].name = value;
+  if (type == "label") a[fIdx].params.args[aIdx].label = value;
+  if (type == "type") a[fIdx].params.args[aIdx].type_schema.type = value;
+  if (type == "value") {
+    if (a[fIdx].params.args[aIdx].type_schema.type == "integer") {
+      a[fIdx].params.args[aIdx].value = parseInt(value);
+    }
+    if (a[fIdx].params.args[aIdx].type_schema.type == "array") {
+      a[fIdx].params.args[aIdx].value = value.split("|"); //check valid
+    }
+    if (a[fIdx].params.args[aIdx].type_schema.type == "boolean") {
+      a[fIdx].params.args[aIdx].value = Boolean(value);
+    }
+    if (a[fIdx].params.args[aIdx].type_schema.type == "json") {
+      a[fIdx].params.args[aIdx].value = JSON.parse(value); //check valid
+    }
+    if (a[fIdx].params.args[aIdx].type_schema.type == "string") {
+      a[fIdx].params.args[aIdx].value = value; //check valid
+    }
+    if (a[fIdx].params.args[aIdx].type_schema.type == "enum") {
+      a[fIdx].params.args[aIdx].value = value; //check valid
+    }
+    if (a[fIdx].params.args[aIdx].type_schema.type == "$ref") {
+      a[fIdx].params.args[aIdx].value = value; //check account valid
+    }
+  }
+  if (type == "remove") a[fIdx].params.args.splice(aIdx, 1);
+  State.update({ cMethod: a });
+};
+const onCreateMethod = () => {
+  if (state.fName.length > 0) {
+    State.update({ cMerr: null });
+    const method = {
+      name: state.fName,
+      kind: state.fAction,
+      label: state.fLabel,
+      button: "",
+      export: true,
+      params: {
+        serialization_type: "json",
+        args: [],
+      },
+      deposit: 0,
+      gas: 30000000000000,
+    };
+    const abiMethod = state.cMethod;
+    const isExistFunction = false;
+    abiMethod.forEach((item) => {
+      if (item.name == state.fName) {
+        isExistFunction = true;
+      }
+    });
+    if (!isExistFunction) {
+      abiMethod.push(method);
+      State.update({ cMethod: abiMethod });
+    } else {
+      State.update({ cMerr: "Method Exist!" });
+    }
+  } else {
+    State.update({ cMerr: "Please Input Method Name!" });
+  }
+};
+const getMethodFromSource = () => {
+  State.update({ cMerr: null, cMethod: [] });
+  asyncFetch(state.rpcUrl, {
+    body: JSON.stringify({
+      method: "query",
+      params: {
+        request_type: "view_code",
+        account_id: state.contractAddress,
+        finality: "final",
+      },
+      id: 154,
+      jsonrpc: "2.0",
+    }),
+    headers: header,
+    method: "POST",
+  }).then((res) => {
+    let abiMethod = [];
+    const resb = res.body;
+    if (resb.result) {
+      const data = Buffer(resb.result.code_base64, "base64").toString("ascii");
+      const fist = data.indexOf("memory");
+      let second =
+        data.indexOf("__data_end") !== -1
+          ? data.indexOf("__data_end")
+          : data.indexOf("P]");
+      if (fist !== -1 && second !== -1) {
+        const functionsData = data
+          .substring(fist, second)
+          .replace(/[^\w ]/g, " ")
+          .split(" ");
+        const filterFunction = [];
+        functionsData.forEach((item, index) => {
+          if (index > 0 && item.length > 1) {
+            if (
+              !/^[A-Z]+(?:_[A-Z]+)*$/m.test(item) &&
+              !/^[0-9]*$/.test(string)
+            ) {
+              filterFunction.push(item);
+            }
+          }
+        });
+        filterFunction.forEach((item) => {
+          const res = fetch(
+            `${state.nearBlockRpc}v1/account/${state.contractAddress}/txns?method=${item}&order=desc&page=1&per_page=25`,
+            opGet
+          );
+          const method = {
+            name: item,
+            kind: "view",
+            export: true,
+            params: {
+              serialization_type: "json",
+              args: [],
+            },
+            deposit: 0,
+            gas: 30000000000000,
+          };
+          if (res.body.txns.length > 0) {
+            const isScs = false;
+            res.body.txns.forEach((item) => {
+              if (item.outcomes.status) {
+                isScs = true;
+              }
+            });
+            if (isScs) {
+              method.kind = "call";
+            }
+          }
+          abiMethod.push(method);
+        });
+
+        State.update({ cMethod: abiMethod });
+        abiMethod.forEach((item, index) => {
+          getArgsFromMethod(item.name, index);
+        });
+      } else {
+        State.update({ cMerr: "Unable to detect Method!" });
+      }
+    } else {
+      State.update({ cMerr: "Unable to detect Method!" });
+    }
+  });
+};
+const getArgsFromMethod = (fName, fIndex) => {
+  asyncFetch(
+    `${state.nearBlockRpc}v1/account/${state.contractAddress}/txns?method=${fName}&order=desc&page=1&per_page=1`,
+    opGet
+  ).then((res) => {
+    const restxns = res.body.txns[0];
+    if (restxns.outcomes.status && restxns.logs.length > 0) {
+      const argsData = JSON.parse(
+        restxns.logs[0].replace("EVENT_JSON:", "").replaceAll("\\", "")
+      );
+      const args = argsData.data[0] || argsData;
+      if (Object.keys(args).length > 0) {
+        const abiMethod = state.cMethod;
+        abiMethod[fIndex].params.args = [];
+        Object.keys(args).forEach((item) => {
+          const arg = {
+            name: item,
+            type_schema: {
+              type:
+                typeof args[item] == "number"
+                  ? "integer"
+                  : typeof args[item] == "object"
+                  ? "json"
+                  : typeof args[item],
+            },
+            value: "",
+          };
+          abiMethod[fIndex].kind = "call";
+          abiMethod[fIndex].params.args.push(arg);
+          State.update({ cMethod: abiMethod });
+        });
+      }
+    } else {
+      const getArg = setInterval(() => {
+        const abiMethod = state.cMethod;
+        const argsArr = abiMethod[fIndex].params.args;
+        const argMap = argsArr.map(({ name, value }) => ({ [name]: value }));
+        const args = {};
+        argMap.forEach((item) => {
+          Object.assign(args, item);
+        });
+
+        asyncFetch(state.rpcUrl, {
+          body: JSON.stringify({
+            method: "query",
+            params: {
+              request_type: "call_function",
+              account_id: state.contractAddress,
+              method_name: fName,
+              args_base64: new Buffer.from(JSON.stringify(args)).toString(
+                "base64"
+              ),
+              finality: "final",
+            },
+            id: 154,
+            jsonrpc: "2.0",
+          }),
+          headers: header,
+          method: "POST",
+        }).then((res) => {
+          const strErr = res.body.result.error;
+          if (strErr && strErr.includes("missing field")) {
+            const argName = strErr.substring(
+              strErr.indexOf("`") + 1,
+              strErr.lastIndexOf("`")
+            );
+            const checkType = [
+              { value: "", type: "string" },
+              { value: 0, type: "integer" },
+              { value: [], type: "array" },
+              { value: true, type: "boolean" },
+              { value: {}, type: "json" },
+              { value: state.contractAddress, type: "$ref" },
+            ];
+            const isCheck = false;
+            checkType.forEach((typeItem) => {
+              if (isCheck == false) {
+                asyncFetch(state.rpcUrl, {
+                  body: JSON.stringify({
+                    method: "query",
+                    params: {
+                      request_type: "call_function",
+                      account_id: state.contractAddress,
+                      method_name: fName,
+                      args_base64: new Buffer.from(
+                        JSON.stringify({
+                          [argName]: typeItem.value,
+                        })
+                      ).toString("base64"),
+                      finality: "final",
+                    },
+                    id: 154,
+                    jsonrpc: "2.0",
+                  }),
+                  headers: header,
+                  method: "POST",
+                }).then((res) => {
+                  const uS = (argName, type, value) => {
+                    isCheck = true;
+                    const arg = {
+                      name: argName,
+                      type_schema: {
+                        type: type,
+                      },
+                      value: type == "enum" ? value[0] : value,
+                    };
+                    if (type == "enum") {
+                      arg.enum = value;
+                    }
+                    const isExist = false;
+                    abiMethod[fIndex].params.args.forEach((item) => {
+                      if (item.name == argName) {
+                        isExist = true;
+                      }
+                    });
+                    if (isExist == false) {
+                      abiMethod[fIndex].params.args.push(arg);
+                      State.update({ cMethod: abiMethod });
+                    }
+                  };
+                  if (res.body.result.result) {
+                    clearInterval(getArg);
+                  }
+                  const ftch = res.body.result.error;
+
+                  if (ftch) {
+                    if (ftch.includes("Option::unwrap()`")) {
+                      uS(argName, typeItem.type, typeItem.value);
+                      abiMethod[fIndex].kind = "call";
+                      State.update({ cMethod: abiMethod });
+                      clearInterval(getArg);
+                    }
+                    if (ftch.includes("the account ID")) {
+                      uS(argName, "$ref", state.contractAddress);
+                    }
+                    if (ftch.includes("invalid type: sequence, expected u64")) {
+                      uS(argName, "number", 300);
+                    }
+                    if (ftch.includes("invalid digit found")) {
+                      uS(argName, "string", "300");
+                    }
+                    if (
+                      ftch.includes("invalid type: sequence, expected a string")
+                    ) {
+                      uS(argName, "string", "wrap.near");
+                      clearInterval(getArg);
+                    }
+                    if (
+                      ftch.includes(
+                        "data did not match any variant of untagged enum"
+                      )
+                    ) {
+                      uS(argName, typeItem.type, ["300", "300"]);
+                      clearInterval(getArg);
+                    }
+
+                    if (ftch.includes("not implemented")) {
+                      uS(argName, typeItem.type, ["300", "300"]);
+                      // clearInterval(getArg);
+                    }
+                    if (ftch.includes("invalid token id")) {
+                      uS(argName, "$ref", "wrap.near");
+                    }
+                    if (ftch.includes("integer from empty string")) {
+                      uS(argName, typeItem.type, "300");
+                    }
+                    if (ftch.includes("unknown variant")) {
+                      isCheck = true;
+                      const getEnum = ftch.match(/\`(.*?)\`/g);
+
+                      const enumList = [];
+                      getEnum.forEach((item, index) => {
+                        if (index !== 0) {
+                          enumList.push(item.replaceAll("`", ""));
+                        }
+                      });
+                      uS(argName, "enum", enumList);
+                    }
+                    if (ftch.includes("missing field")) {
+                      uS(argName, typeItem.type, typeItem.value);
+                    }
+                    if (ftch.includes("Requires attached deposit")) {
+                      uS(argName, typeItem.type, typeItem.value);
+                      abiMethod[fIndex].kind = "call";
+                      abiMethod[fIndex].deposit = parseInt(
+                        strErr.match(/\d+/)[0]
+                      );
+                      State.update({ cMethod: abiMethod });
+                      clearInterval(getArg);
+                    }
+
+                    if (
+                      fetch.includes("missing field") &&
+                      argName ==
+                        fetch.match(/\`(.*?)\`/g)[0].replaceAll("`", "")
+                    ) {
+                      uS(argName, typeItem.type, typeItem.value);
+                      clearInterval(getArg);
+                    }
+                  } else {
+                    uS(argName, typeItem.type, typeItem.value);
+                    clearInterval(getArg);
+                  }
+                });
+              }
+            });
+          }
