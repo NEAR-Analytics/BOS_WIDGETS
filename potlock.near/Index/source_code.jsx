36b3c4aadb1c555e6cb1815c79ea143b94ec58ec
potlock.near <potlock.near>
2023-10-23 13:59:05 +0000

    Update Index by potlock.near at 2023-10-23T13:59:05.803Z

--- potlock.near/Index/source_code.jsx
+++ potlock.near/Index/source_code.jsx
@@ -1,162 +1,87 @@
 const ownerId = "potlock.near";
-const registryContractId = "registry.potlock.near";
-const donationContractId = "donate.potlock.near";
 
 const CREATE_PROJECT_TAB = "createproject";
-const EDIT_PROJECT_TAB = "editproject";
-const PROJECTS_LIST_TAB = "projects";
 const PROJECT_DETAIL_TAB = "project";
-const CART_TAB = "cart";
-const FEED_TAB = "feed";
-
-const Theme = styled.div`
-  * {
-    font-family: "Mona-Sans";
-    font-style: normal;
-    font-weight: 400;
-  }
-  @font-face {
-    font-family: mona-sans;
-    font-style: normal;
-    font-weight: 400;
-    src: local("Mona-Sans"),
-      url(https://fonts.cdnfonts.com/s/91271/Mona-Sans-Regular.woff) format("woff");
-  }
-  @font-face {
-    font-family: mona-sans;
-    font-style: normal;
-    font-weight: 500;
-    src: local("Mona-Sans"),
-      url(https://fonts.cdnfonts.com/s/91271/Mona-Sans-Medium.woff) format("woff");
-  }
-  @font-face {
-    font-family: mona-sans;
-    font-style: normal;
-    font-weight: 600;
-    src: local("Mona-Sans"),
-      url(https://fonts.cdnfonts.com/s/91271/Mona-Sans-SemiBold.woff) format("woff");
-  }
-  @font-face {
-    font-family: mona-sans;
-    font-style: normal;
-    font-weight: 700;
-    src: local("Mona-Sans"),
-      url(https://fonts.cdnfonts.com/s/91271/Mona-Sans-Bold.woff) format("woff");
-  }
-`;
-
-const NEAR_ACCOUNT_ID_REGEX = /^(?=.{2,64}$)(?!.*\.\.)(?!.*-$)(?!.*_$)[a-z\d._-]+$/i;
 
 State.init({
-  registeredProjects: null,
-  cart: null,
-  checkoutSuccess: false,
-  checkoutSuccessTxHash: null,
-  donations: null,
-  // previousCart: null,
-  nearToUsd: null,
-  isCartModalOpen: false,
-  isNavMenuOpen: false,
-  registryAdmins: null,
-  registeredProjects: null,
+  tnc: true,
+  tncIsFetched: false,
+  tosAccept: true,
 });
 
-if (!state.nearToUsd) {
-  asyncFetch("https://api.coingecko.com/api/v3/simple/price?ids=near&vs_currencies=usd").then(
-    (res) => {
-      if (res.ok) {
-        State.update({ nearToUsd: res.body.near.usd });
-      }
-    }
-  );
-}
-
-// console.log("state in Index: ", state);
-
-if (!state.registeredProjects) {
-  State.update({ registeredProjects: Near.view(registryContractId, "get_projects", {}) });
-}
-
-if (!state.registeredProjects) return "";
-
-if (!state.donations) {
-  State.update({
-    donations: Near.view(donationContractId, "get_donations", {}), // TODO: ADD PAGINATION
-  });
-}
-
-const IPFS_BASE_URL = "https://nftstorage.link/ipfs/";
-
-const getImageUrlFromSocialImage = (image) => {
-  if (image.url) {
-    return image.url;
-  } else if (image.ipfs_cid) {
-    return IPFS_BASE_URL + image.ipfs_cid;
-  }
-};
-
-if (!state.registeredProjects) {
-  Near.asyncView(registryContractId, "get_projects", {})
-    .then((projects) => {
-      // get social data for each project
-      Near.asyncView("social.near", "get", {
-        keys: projects.map((project) => `${project.id}/profile/**`),
-      }).then((socialData) => {
-        const formattedProjects = projects.map((project) => {
-          const profileData = socialData[project.id]?.profile;
-          let profileImageUrl = DEFAULT_PROFILE_IMAGE_URL;
-          if (profileData.image) {
-            const imageUrl = getImageUrlFromSocialImage(profileData.image);
-            if (imageUrl) profileImageUrl = imageUrl;
-          }
-          // get banner image URL
-          let bannerImageUrl = DEFAULT_BANNER_IMAGE_URL;
-          if (profileData.backgroundImage) {
-            const imageUrl = getImageUrlFromSocialImage(profileData.backgroundImage);
-            if (imageUrl) bannerImageUrl = imageUrl;
-          }
-          const formatted = {
-            id: project.id,
-            name: profileData.name ?? "",
-            description: profileData.description ?? "",
-            bannerImageUrl,
-            profileImageUrl,
-            status: project.status,
-            tags: [profileData.category.text ?? CATEGORY_MAPPINGS[profileData.category] ?? ""],
-          };
-          return formatted;
-        });
-        State.update({
-          registeredProjects: formattedProjects,
-        });
-      });
-    })
-    .catch((e) => {
-      console.log("error getting projects: ", e);
-      State.update({ getRegisteredProjectsError: e });
-    });
-}
-
-if (state.registryAdmins === null) {
-  const registryAdmins = Near.view(registryContractId, "get_admins", {});
-  State.update({ registryAdmins });
-}
+// if (context.accountId && !state.tncIsFetched) {
+//   Near.asyncView(
+//     "social.near",
+//     "get",
+//     {
+//       keys: [
+//         `${context.accountId}/profile/horizon_tnc`,
+//         `${context.accountId}/index/tosAccept`,
+//       ],
+//     },
+//     "final",
+//     false
+//   ).then((data) =>
+//     State.update({
+//       tnc: data[context.accountId]?.profile?.horizon_tnc === "true",
+//       tosAccept:
+//         data[context.accountId]?.index?.tosAccept &&
+//         data[context.accountId]?.index?.tosAccept.length > 0,
+//       tncIsFetched: true,
+//     })
+//   );
+// }
 
 const tabContentWidget = {
+  // home: "Dashboard",
+  // inbox: "Inbox",
+  // manage: "Manage",
+  // project: "Project.Page",
+  // request: "Request.Page",
+  // vendor: "Vendor.Page",
+  // backer: "Investor.Page",
+  // contribution: "Contribution.Page",
   [CREATE_PROJECT_TAB]: "Project.Create",
-  [EDIT_PROJECT_TAB]: "Project.Create",
-  [PROJECTS_LIST_TAB]: "Project.ListPage",
+  // createrequest: "Request.Form",
+  // createvendor: "Vendor.Form",
+  // createbacker: "Investor.Form",
+  // permissions: "Inputs.SetUpPermissions",
+  // learn: "Learn.Page",
+  // faq: "FAQ.Page",
+  // help: "Help.Page",
+  // legal: "TNCPage",
+  // admin: "Admin.Page",
+  projects: "Project.ListPage",
   [PROJECT_DETAIL_TAB]: "Project.Detail",
-  [CART_TAB]: "Cart.Checkout",
-  [FEED_TAB]: "Components.Feed",
+  // investors: "Investor.ListPage",
+  // backers: "Investor.ListPage",
+  // vendors: "Vendor.ListPage",
+  // contributors: "Vendor.ListPage",
+  // requests: "Request.ListPage",
+  // partners: "Application.Page",
+  // partner: "Application.DetailPage",
+  // "my-projects": "Manage.Projects",
+  // "my-requests": "Manage.Requests",
+  // "my-contracts": "Manage.Contracts",
+  // "my-applications": "Manage.Applications",
+  // events: "Events.Page",
+  // perks: "Perks.Page",
+  // profile: "Profile.Page",
 };
 
+const successfulRegistration = props.tab == CREATE_PROJECT_TAB && props.transactionHashes;
+
 const getWidget = (props) => {
-  if (props.tab in tabContentWidget) {
+  if (successfulRegistration) {
+    // user just successfully registered their project
+    // navigate to the project page
+    // get the project ID (do this in getWidgetProps)
+    return tabContentWidget[PROJECT_DETAIL_TAB];
+  } else if (props.tab in tabContentWidget) {
     return tabContentWidget[props.tab];
   }
-  // default tab is projects list
-  return tabContentWidget[PROJECTS_LIST_TAB];
+  // backup (TODO: review)
+  return "Project.ListPage";
 };
 
 const getTabWidget = (tab) => {
@@ -164,129 +89,51 @@ const getTabWidget = (tab) => {
     return tabContentWidget[tab];
   }
 
-  return tabContentWidget[PROJECTS_LIST_TAB];
+  return "Project.ListPage";
 };
 
-const CART_KEY = "cart";
-// const PREVIOUS_CART_KEY = "previousCart";
-const storageCart = Storage.get(CART_KEY);
-// const storagePreviousCart = Storage.get(PREVIOUS_CART_KEY);
-const DEFAULT_CART = {};
-
-const props = {
-  ...props,
-  ...state,
-  addProjectsToCart: (projects) => {
-    const cart = state.cart ?? {};
-    projects.forEach(({ id, amount, ft, referrerId }) => {
-      cart[id] = { amount, ft: ft ?? "NEAR", referrerId }; // default to NEAR
-    });
-    State.update({ cart });
-    Storage.set(CART_KEY, JSON.stringify(cart));
-  },
-  removeProjectsFromCart: (projectIds) => {
-    const cart = state.cart ?? {};
-    projectIds.forEach((projectId) => {
-      delete cart[projectId];
-    });
-    State.update({ cart });
-    Storage.set(CART_KEY, JSON.stringify(cart));
-  },
-  updateCartItem: (projectId, amount, ft, referrerId) => {
-    const cart = state.cart ?? {};
-    const updated = {};
-    // if (amount === "") updated.amount = "0";
-    if (amount || amount === "") updated.amount = amount;
-    if (ft) updated.ft = ft;
-    if (referrerId) updated.referrerId = referrerId;
-    cart[projectId] = updated;
-    State.update({ cart });
-    Storage.set(CART_KEY, JSON.stringify(cart));
-  },
-  clearCart: () => {
-    State.update({ cart: {} });
-    Storage.set(CART_KEY, JSON.stringify(DEFAULT_CART));
-  },
-  setCheckoutSuccess: (checkoutSuccess) => {
-    State.update({ checkoutSuccess });
-  },
-  setIsCartModalOpen: (isOpen) => {
-    State.update({ isCartModalOpen: isOpen });
-  },
-  setIsNavMenuOpen: (isOpen) => {
-    State.update({ isNavMenuOpen: isOpen });
-  },
-  validateNearAddress: (address) => {
-    let isValid = NEAR_ACCOUNT_ID_REGEX.test(address);
-    // Additional ".near" check for IDs less than 64 characters
-    if (address.length < 64 && !address.endsWith(".near")) {
-      isValid = false;
-    }
-    return isValid;
-  },
-  CATEGORY_MAPPINGS: {
-    "social-impact": "Social Impact",
-    "non-profit": "NonProfit",
-    climate: "Climate",
-    "public-good": "Public Good",
-    "de-sci": "DeSci",
-    "open-source": "Open Source",
-    community: "Community",
-    education: "Education",
-  },
-  PROJECT_STATUSES: ["Submitted", "InReview", "Approved", "Rejected", "Graylisted", "Blacklisted"],
-  SUPPORTED_FTS: {
-    // TODO: move this to state to handle selected FT once we support multiple FTs
-    NEAR: {
-      iconUrl: IPFS_BASE_URL + "bafkreicwkm5y7ojxnnfnmuqcs6ykftq2jvzg6nfeqznzbhctgl4w3n6eja",
-      toIndivisible: (amount) => new Big(amount).mul(new Big(10).pow(24)),
-      fromIndivisible: (amount) => amount / 10e24,
-    },
-  },
-};
-
-if (props.transactionHashes && props.tab === CART_TAB) {
-  // if transaction hashes are in URL but haven't been added to props, override state:
-  props.checkoutSuccessTxHash = props.transactionHashes;
-  props.checkoutSuccess = true;
-}
-
-if (state.cart === null && storageCart !== null) {
-  // cart hasn't been set on state yet, and storageCart has been fetched
-  // if storageCart isn't undefined, set it on state
-  // otherwise, set default cart on state
-  let cart = DEFAULT_CART;
-  if (storageCart) {
-    cart = JSON.parse(storageCart);
+const getWidgetProps = () => {
+  const props = {
+    ...props,
+    urlProps: props,
+  };
+  if (successfulRegistration) {
+    props.successfulRegistration = true;
+    // get the project ID
+    // get list of all registered projects
+    // find the one that matches context.accountId
+    // pass this project in props to the project page
   }
-  State.update({ cart });
-}
+  return {
+    ...props,
+    urlProps: props,
+    successfulRegistration,
+  };
+}; // TODO: IMPLEMENT
 
-// if (state.previousCart === null && storagePreviousCart !== null) {
-//   // previousCart hasn't been set on state yet, and storagePreviousCart has been fetched
-//   // if storagePreviousCart isn't undefined, set it on state
-//   if (storagePreviousCart && Object.keys(JSON.parse(storagePreviousCart)).length > 0) {
-//     console.log("updating previous cart");
-//     State.update({ previousCart: JSON.parse(storagePreviousCart) });
-//   }
-// }
+console.log("Index props: ", props);
 
-// console.log("state in Index: ", state);
+const tabContent = (
+  <Widget
+    src={`${ownerId}/widget/${
+      successfulRegistration ? tabContentWidget.projects : getTabWidget(props.tab)
+    }`}
+    props={{
+      ...props,
+      urlProps: props,
+      successfulRegistration,
+    }}
+  />
+);
 
-if (state.checkoutSuccessTxHash && state.cart && Object.keys(state.cart).length > 0) {
-  // if checkout was successful after wallet redirect, clear cart
-  // store previous cart in local storage to show success message
-  // console.log("previous cart: ", state.cart);
-  props.clearCart();
-}
-
-if (props.tab === EDIT_PROJECT_TAB) {
-  props.edit = true;
-}
-
-const tabContent = <Widget src={`${ownerId}/widget/${getTabWidget(props.tab)}`} props={props} />;
+// const Page = styled.div`
+//   width: 100%;
+//   border-radius: 0.5rem;
+//   border: 1px solid #eaeaea;
+// `;
 
 const Content = styled.div`
+  padding-top: 110px; // navbar height
   width: 100%;
   height: 100%;
   background: #ffffff;
@@ -301,15 +148,35 @@ const Content = styled.div`
   }
 `;
 
-const isForm = [CREATE_PROJECT_TAB].includes(props.tab);
+// const showSidebar = ![
+//   "createproject",
+//   "createrequest",
+//   "createvendor",
+//   "createbacker",
+//   "permissions",
+//   "legal",
+// ].includes(props.tab);
+const isForm = [
+  CREATE_PROJECT_TAB,
+  // "createrequest",
+  // "createvendor",
+  // "createbacker",
+].includes(props.tab);
 
-if (!state.cart || !state.registeredProjects) {
-  return "";
-}
+// const showTncDialog = !state.tnc && state.tosAccept && props.tab !== "legal";
 
 return (
-  <Theme>
-    <Widget src={`${ownerId}/widget/Components.Nav`} props={props} />
+  <>
+    <Widget src={`${ownerId}/widget/Nav`} />
+    {/* <Widget
+      src={`${ownerId}/widget/Components.Header`}
+      props={{
+        title: "Create new project",
+        description:
+          "Lorem ipsum dolor sit amet consectetur. Vel sit nunc in nunc. Viverra arcu eu sed consequat.",
+      }}
+    /> */}
+    {/* <Widget src={`${ownerId}/widget/Project.Form`} /> */}
     <Content className={isForm ? "form" : ""}>{tabContent}</Content>
-  </Theme>
+  </>
 );
