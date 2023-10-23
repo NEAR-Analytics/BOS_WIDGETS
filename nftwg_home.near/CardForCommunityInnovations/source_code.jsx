e99277ce38727a2cc8a2c0ec6b1bf470adebef7a
nftwg_home.near <nftwg_home.near>
2023-10-23 07:39:38 +0000

    Update CardForCommunityInnovations by nftwg_home.near at 2023-10-23T07:39:38.490Z

--- nftwg_home.near/CardForCommunityInnovations/source_code.jsx
+++ nftwg_home.near/CardForCommunityInnovations/source_code.jsx
@@ -26,6 +26,14 @@ const props = [
     buttonText: "In Progress",
     fontSize: 20,
   },
+  {
+    title: "MNC",
+    imageSrc:
+      "https://cdn.discordapp.com/attachments/1135845507189702748/1162116592906293341/image.png?ex=653ac46a&is=65284f6a&hm=65dad1554346d62e5e55c6917328b4a1d5ffad8c1352329ada45b47c85bdaeff&",
+    description: "Socializer is an interactive rewards portal for Near Social.",
+    buttonText: "In Progress",
+    fontSize: 20,
+  },
 ];
 const Card = styled.div`
   box-sizing: border-box;
