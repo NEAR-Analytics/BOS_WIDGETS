b919f6def58047eccfaaeadf0759fc6667bb84a5
magicbuild.near <magicbuild.near>
2023-10-23 08:24:32 +0000

    Update builder by magicbuild.near at 2023-10-23T08:24:32.941Z

--- magicbuild.near/builder/source_code.jsx
+++ magicbuild.near/builder/source_code.jsx
@@ -377,6 +377,7 @@ const getArgsFromMethod = (fName, fIndex) => {
                       uS(argName, typeItem.type, "300");
                     }
                     if (ftch.includes("unknown variant")) {
+                      isCheck = true;
                       const getEnum = ftch.match(/\`(.*?)\`/g);
                       console.log("getEnum", getEnum);
                       getEnum.forEach((item) => {
@@ -384,7 +385,7 @@ const getArgsFromMethod = (fName, fIndex) => {
                       });
                       console.log("enumList", enumList);
                       //magicbuild.near/widget/builder change_state wasm execution failed with error: HostError(GuestPanic { panic_msg: "panicked at 'Failed to deserialize input from JSON.: Error(\"unknown variant `ailedwitherror:HostError(GuestPanic{panic_msg:\\\"panickedat'FailedtodeserializeinputfromJSON.:Error(\\\\\\\"unknownvariantv2.ref-finance.near`, expected `Running` or `Paused`\", line: 1, column: 147)', ref-exchange/src/owner.rs:11:1" })
-                      isCheck = true;
+
                       uS(argName, "enum", enumList);
                     }
                     if (ftch.includes("missing field")) {
