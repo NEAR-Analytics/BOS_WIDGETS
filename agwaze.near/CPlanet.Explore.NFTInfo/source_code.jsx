79992278bb5665141714d82bfe81fe79ae949614
agwaze.near <agwaze.near>
2023-10-23 00:52:44 +0000

    Update CPlanet.Explore.NFTInfo by agwaze.near at 2023-10-23T00:52:44.354Z

--- agwaze.near/CPlanet.Explore.NFTInfo/source_code.jsx
+++ agwaze.near/CPlanet.Explore.NFTInfo/source_code.jsx
@@ -198,6 +198,7 @@ const currentChainProps = {
     chain: "near",
     id: "1112",
     explorer: "https://explorer.near.org/?query=",
+    explorerTx: "https://explorer.near.org/?query=",
     logoUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrJuxjGxj4QmyreE6ix4ygqm5pK9Nn_rdc8Ndw6lmJcd0SSnm2zBIc2xJ_My1V0WmK2zg&usqp=CAU",
   },
