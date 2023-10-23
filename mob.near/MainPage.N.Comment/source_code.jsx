4bfd99c572d11476f45407d4330c77d3f39c4b6d
mob.near <mob.near>
2023-10-23 15:45:36 +0000

    Update MainPage.N.Comment by mob.near at 2023-10-23T15:45:36.291Z

--- mob.near/MainPage.N.Comment/source_code.jsx
+++ mob.near/MainPage.N.Comment/source_code.jsx
@@ -103,7 +103,7 @@ return (
     {state.showReply && (
       <div className="mb-2" key="reply">
         <Widget
-          src="mob.near/widget/MainPage.Comment.Compose"
+          src="mob.near/widget/MainPage.N.Comment.Compose"
           props={{
             initialText: `@${accountId}, `,
             notifyAccountId: extractNotifyAccountId(parentItem),
