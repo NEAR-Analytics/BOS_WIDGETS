bbd124ad19fe281d8cab32496a973fc8ac97def3
magicbuild.near <magicbuild.near>
2023-10-23 08:26:59 +0000

    Update builder by magicbuild.near at 2023-10-23T08:26:59.469Z

--- magicbuild.near/builder/source_code.jsx
+++ magicbuild.near/builder/source_code.jsx
@@ -380,8 +380,11 @@ const getArgsFromMethod = (fName, fIndex) => {
                       isCheck = true;
                       const getEnum = ftch.match(/\`(.*?)\`/g);
                       console.log("getEnum", getEnum);
-                      getEnum.forEach((item) => {
-                        enumList.push(item.replaceAll("`", ""));
+                      const enumList = [];
+                      getEnum.forEach((item, index) => {
+                        if (index !== 0) {
+                          enumList.push(item.replaceAll("`", ""));
+                        }
                       });
                       console.log("enumList", enumList);
                       //magicbuild.near/widget/builder change_state wasm execution failed with error: HostError(GuestPanic { panic_msg: "panicked at 'Failed to deserialize input from JSON.: Error(\"unknown variant `ailedwitherror:HostError(GuestPanic{panic_msg:\\\"panickedat'FailedtodeserializeinputfromJSON.:Error(\\\\\\\"unknownvariantv2.ref-finance.near`, expected `Running` or `Paused`\", line: 1, column: 147)', ref-exchange/src/owner.rs:11:1" })
