3aa0b4143c7801fed15c52353a71556345f4a732
bluebiu.near <bluebiu.near>
2023-10-23 03:28:29 +0000

    Update Base.Bridge.CheckedIcon by bluebiu.near at 2023-10-23T03:28:29.758Z

---
+++ bluebiu.near/Base.Bridge.CheckedIcon/source_code.jsx
@@ -0,0 +1,18 @@
+const Size = props.size || 14;
+
+return (
+  <svg
+    width={Size}
+    height={Size / 1.2}
+    viewBox="0 0 12 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1 4.29412L4.46154 8L11 1"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+);
