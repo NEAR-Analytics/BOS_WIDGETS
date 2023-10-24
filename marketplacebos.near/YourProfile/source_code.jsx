9b538c464470e0df757b64903938056d1bed64bd
marketplacebos.near <marketplacebos.near>
2023-10-24 09:28:26 +0000

    Update YourProfile by marketplacebos.near at 2023-10-24T09:28:26.068Z

---
+++ marketplacebos.near/YourProfile/source_code.jsx
@@ -0,0 +1,53 @@
+const accountId = props.accountId ?? context.accountId;
+const link = props.link ?? true;
+const hideAccountId = props.hideAccountId;
+const hideName = props.hideName;
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+
+const name = profile.name ?? accountId;
+const title = props.title ?? `${name} @${accountId}`;
+const tooltip =
+  props.tooltip && (props.tooltip === true ? title : props.tooltip);
+
+let inner = (
+  <>
+   <Widget
+      src="mob.near/widget/ProfileImage"
+      props={{
+        style: { width: "2em", height: "2em" },
+        profile,
+        accountId,
+        className: "d-inline-block",
+        imageClassName: "rounded w-100 h-100 align-top",
+      }}
+    />
+    {!hideName && name}
+    {!hideAccountId && <span className="text-muted">@{accountId}</span>}
+  </>
+);
+
+inner = link ? (
+  <a
+    href={
+      link !== true
+        ? link
+        : `#/mob.near/widget/ProfilePage?accountId=${accountId}`
+    }
+    className="link-dark text-truncate"
+  >
+    {inner}
+  </a>
+) : (
+  <span className="text-truncate">{inner}</span>
+);
+
+if (tooltip) {
+  inner = (
+    <OverlayTrigger placement="auto" overlay={<Tooltip>{tooltip}</Tooltip>}>
+      {inner}
+    </OverlayTrigger>
+  );
+}
+
+return inner;
