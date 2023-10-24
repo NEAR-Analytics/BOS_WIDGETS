61a3d112e4f9165d5f506e276ed9ed50a251f4ec
mob.near <mob.near>
2023-10-24 00:19:17 +0000

    Update N.StarButton by mob.near at 2023-10-24T00:19:17.508Z

--- mob.near/N.StarButton/source_code.jsx
+++ mob.near/N.StarButton/source_code.jsx
@@ -35,7 +35,7 @@ const starSvg = (
     fill="currentColor"
     stroke="currentColor"
     strokeWidth="0.5"
-    viewBox="-2 -2 20 20"
+    viewBox="-2 -1 20 20"
     style={{ width: "1.25em" }}
   >
     <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
@@ -48,7 +48,7 @@ const starFillSvg = (
     fill="currentColor"
     stroke="currentColor"
     strokeWidth="0.5"
-    viewBox="-2 -2 20 20"
+    viewBox="-2 -1 20 20"
     style={{ width: "1.25em" }}
   >
     <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -126,41 +126,33 @@ const starClick = () => {
   State.update({
     loading: true,
   });
+  const type = hasStar ? "unstar" : "star";
   const data = {
     index: {
+      store: JSON.stringify({
+        key: {
+          type: "star",
+          accountId: context.accountId,
+        },
+        value: {
+          type,
+          item,
+        },
+      }),
       star: JSON.stringify({
         key: item,
         value: {
-          type: hasStar ? "unstar" : "star",
+          type,
         },
       }),
     },
   };
 
-  // Check if it's a widget, then we'll add it to a graph
-  // Temporary remove it. Only add to the index.
-  /*
-  if (item.type === "social" && typeof item.path === "string") {
-    const keys = item.path.split("/");
-    if (keys.length === 3 && keys[0] && keys[1] === "widget" && keys[2]) {
-      data.graph = {
-        star: {
-          [keys[0]]: {
-            [keys[1]]: {
-              [keys[2]]: hasStar ? null : "",
-            },
-          },
-        },
-      };
-    }
-  }
-  */
-
   if (!hasStar && props.notifyAccountId) {
     data.index.notify = JSON.stringify({
       key: props.notifyAccountId,
       value: {
-        type: "star",
+        type,
         item,
       },
     });
@@ -171,7 +163,9 @@ const starClick = () => {
   });
 };
 
-const title = hasStar ? "Unstar" : "Star";
+const title = hasStar
+  ? props.titleUnstar ?? "Unstar"
+  : props.titleStar ?? "Star";
 
 return (
   <div className="d-inline-flex align-items-center">
