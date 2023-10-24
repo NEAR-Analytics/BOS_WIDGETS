c6982a0c4db0df08ea553ed5d95203e6ab43ac7a
toch.near <toch.near>
2023-10-24 04:19:30 +0000

    Update commentHackathonBtn by toch.near at 2023-10-24T04:19:30.322Z

--- toch.near/commentHackathonBtn/source_code.jsx
+++ toch.near/commentHackathonBtn/source_code.jsx
@@ -1,4 +1,19 @@
-const item = props.item;
+// if (!context.accountId) {
+//   return "";
+// }
+// const blockHeight =
+//   props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
+
+// const item = props.item;
+const item = {
+  type: "social",
+  path: `${accountId}/post/main`,
+  blockHeight,
+};
+
+// if (!context.accountId) {
+//   return "";
+// }
 
 const composeData = () => {
   const data = {
@@ -15,42 +30,42 @@ const composeData = () => {
     },
   };
 
-  const thisItem = {
-    type: "social",
-    path: `${context.accountId}/post/comment`,
-  };
+  // const thisItem = {
+  //   type: "social",
+  //   path: `${context.accountId}/post/comment`,
+  // };
 
-  const notifications = state.extractMentionNotifications(
-    state.content.text,
-    thisItem
-  );
+  // const notifications = state.extractMentionNotifications(
+  //   state.content.text,
+  //   thisItem
+  // );
 
-  if (props.notifyAccountId && props.notifyAccountId !== context.accountId) {
-    notifications.push({
-      key: props.notifyAccountId,
-      value: {
-        type: "comment",
-        item,
-      },
-    });
-  }
+  // if (props.notifyAccountId && props.notifyAccountId !== context.accountId) {
+  //   notifications.push({
+  //     key: props.notifyAccountId,
+  //     value: {
+  //       type: "comment",
+  //       item,
+  //     },
+  //   });
+  // }
 
-  if (notifications.length) {
-    data.index.notify = JSON.stringify(
-      notifications.length > 1 ? notifications : notifications[0]
-    );
-  }
+  // if (notifications.length) {
+  //   data.index.notify = JSON.stringify(
+  //     notifications.length > 1 ? notifications : notifications[0]
+  //   );
+  // }
 
-  const hashtags = state.extractHashtags(state.content.text);
+  // const hashtags = state.extractHashtags(state.content.text);
 
-  if (hashtags.length) {
-    data.index.hashtag = JSON.stringify(
-      hashtags.map((hashtag) => ({
-        key: hashtag,
-        value: thisItem,
-      }))
-    );
-  }
+  // if (hashtags.length) {
+  //   data.index.hashtag = JSON.stringify(
+  //     hashtags.map((hashtag) => ({
+  //       key: hashtag,
+  //       value: thisItem,
+  //     }))
+  //   );
+  // }
 
   return data;
 };
@@ -63,29 +78,30 @@ State.init({
 
 return (
   <>
-    <CommitButton
-      disabled={!state.content}
-      force
-      className="btn btn-primary rounded-5"
-      data={composeData}
-      onCommit={() => {
-        onCompose();
-        props.onComment && props.onComment(state.content);
+    <Widget
+      src="mob.near/widget/MainPage.N.Common.Compose"
+      props={{
+        placeholder: "Reply",
+        initialText: props.initialText,
+        // onChange: state.onChange,
+        // onHelper: ({ extractMentionNotifications, extractHashtags }) => {
+        //   State.update({ extractMentionNotifications, extractHashtags });
+        // },
+        composeButton: (onCompose) => (
+          <CommitButton
+            disabled={!state.content}
+            force
+            className="btn btn-primary rounded-5"
+            data={composeData}
+            onCommit={() => {
+              onCompose();
+              props.onComment && props.onComment(state.content);
+            }}
+          >
+            Comment
+          </CommitButton>
+        ),
       }}
-    >
-      Comment
-    </CommitButton>
-
-    {state.content && (
-      <Widget
-        src="mob.near/widget/MainPage.N.Comment"
-        props={{
-          item,
-          accountId: context.accountId,
-          content: state.content,
-          blockHeight: "now",
-        }}
-      />
-    )}
+    />
   </>
 );
