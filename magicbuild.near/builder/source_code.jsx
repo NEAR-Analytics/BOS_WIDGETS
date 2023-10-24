68de85a849d0fc6e38a50feb85c26b4e3ba4b520
magicbuild.near <magicbuild.near>
2023-10-24 08:15:33 +0000

    Update builder by magicbuild.near at 2023-10-24T08:15:33.532Z

--- magicbuild.near/builder/source_code.jsx
+++ magicbuild.near/builder/source_code.jsx
@@ -603,36 +603,15 @@ return (
         </div>
       </div>
       <div class="row">
-        <div class="form-group col-md-4">
-          <h6>Method Name</h6>
-          <input
-            type="text"
-            onChange={(e) => cFunc(e, "name")}
-            class="form-control"
-          />
-        </div>
-        <div class="form-group col-md-4">
-          <h6>Label</h6>
-          <input
-            type="text"
-            onChange={(e) => cFunc(e, "label")}
-            class="form-control"
-          />
-        </div>
-        <div class="form-group col-md-2">
-          <h6>Action</h6>
-          <select class="form-control" onChange={(e) => cFunc(e, "action")}>
-            <option value="view" selected>
-              View
-            </option>
-            <option value="call">Call</option>
-          </select>
-        </div>
-        <div class="form-group col-md-2">
-          <label></label>
-          <button onClick={onCreateMethod} class="btn btn-dark form-control ">
-            Create
-          </button>
+        <div class="progress">
+          <div
+            class="progress-bar progress-bar-striped progress-bar-animated"
+            role="progressbar"
+            aria-valuenow="75"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            style="width: 75%"
+          ></div>
         </div>
       </div>
       <div class="row">
