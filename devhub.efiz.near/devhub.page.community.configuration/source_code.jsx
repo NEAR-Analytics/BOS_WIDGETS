eb17eeea439cace71a3b46888b369b354c2b7b00
devhub.efiz.near <devhub.efiz.near>
2023-10-24 23:20:13 +0000

    Update devhub.page.community.configuration by devhub.efiz.near at 2023-10-24T23:20:13.534Z

--- devhub.efiz.near/devhub.page.community.configuration/source_code.jsx
+++ devhub.efiz.near/devhub.page.community.configuration/source_code.jsx
@@ -2,10 +2,17 @@ const { Tile } =
   VM.require("devhub.efiz.near/widget/devhub.components.molecule.Tile") ||
   (() => <></>);
 
-const { permissions, handle, community, deleteCommunity, updateCommunity } =
-  props;
+const {
+  permissions,
+  handle,
+  community,
+  setCommunityAddons,
+  deleteCommunity,
+  updateCommunity,
+} = props;
 
 const [communityData, setCommunityData] = useState(community);
+const [selectedAddon, setSelectedAddon] = useState(null);
 const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
 
 const sectionSubmit = (sectionData) => {
@@ -117,6 +124,19 @@ return (
         }}
       />
     </Tile>
+    {hasConfigurePermissions && (
+      <Tile className={"p-3"}>
+        <Widget
+          src={
+            "devhub.efiz.near/widget/devhub.entity.community.configuration.Addons"
+          }
+          props={{
+            data: communityData.addons || [],
+            onSubmit: (v) => setCommunityAddons({ handle, addons: v }),
+          }}
+        />
+      </Tile>
+    )}
     {hasDeletePermissions && (
       <div
         className="d-flex justify-content-center gap-4 p-4 w-100"
