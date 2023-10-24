a6b345161aac20054fb1fa521357d73a49dc4af7
devhub.efiz.near <devhub.efiz.near>
2023-10-24 18:01:48 +0000

    Update devhub.entity.community.configuration.AboutConfigurator by devhub.efiz.near at 2023-10-24T18:01:48.506Z

--- devhub.efiz.near/devhub.entity.community.configuration.AboutConfigurator/source_code.jsx
+++ devhub.efiz.near/devhub.entity.community.configuration.AboutConfigurator/source_code.jsx
@@ -52,7 +52,7 @@ function handleOnSubmit(v) {
 
 return (
   <Widget
-    src={"${REPL_DEVHUB}/widget/devhub.components.organism.Configurator"}
+    src={"devhub.efiz.near/widget/devhub.components.organism.Configurator"}
     props={{
       externalState: data,
       schema: CommunityAboutSchema,
