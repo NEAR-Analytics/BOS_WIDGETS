4d956373584cbbe5ee00ac13399729822f6bd47a
devhub.efiz.near <devhub.efiz.near>
2023-10-24 17:58:04 +0000

    Update devhub.components.molecule.Button by devhub.efiz.near at 2023-10-24T17:58:04.554Z

--- devhub.efiz.near/devhub.components.molecule.Button/source_code.jsx
+++ devhub.efiz.near/devhub.components.molecule.Button/source_code.jsx
@@ -130,7 +130,7 @@ const Button = ({ classNames, icon: iconProps, label, type, ...restProps }) => {
         typeof iconProps === "object" &&
         !Array.isArray(iconProps) && (
           <Widget
-            src={"devhub.efiz.near/widget/devhub.components.atom.Icon"}
+            src={"${REPL_DEVHUB}/widget/devhub.components.atom.Icon"}
             props={iconProps}
           />
         )}
