adf340e8238222a8f70533de9828d070c5b73c03
astraplusplus.ndctools.near <astraplusplus.ndctools.near>
2023-10-24 19:43:53 +0000

    Update DAO.Proposal.Common.CongressHouseDropdown by astraplusplus.ndctools.near at 2023-10-24T19:43:53.988Z

---
+++ astraplusplus.ndctools.near/DAO.Proposal.Common.CongressHouseDropdown/source_code.jsx
@@ -0,0 +1,46 @@
+const label = props.label;
+const placeholder = props.placeholder;
+const onUpdate = props.onUpdate;
+
+const CoADaoId = props.dev
+    ? "coa.gwg-testing.near"
+    : "congress-coa-v1.ndc-gwg.near";
+const VotingBodyDaoId = props.dev
+    ? "voting-body-v1.gwg-testing.near"
+    : "";
+const TCDaoId = props.dev
+    ? "tc.gwg-testing.near"
+    : "congress-tc-v1.ndc-gwg.near";
+const HoMDaoId = props.dev
+    ? "hom.gwg-testing.near"
+    : "congress-hom-v1.ndc-gwg.near";
+
+State.init({
+    house: null
+});
+
+return (
+    <div className="mb-3">
+        <Widget
+            src={`sking.near/widget/Common.Inputs.Select`}
+            props={{
+                label: label,
+                noLabel: false,
+                placeholder: placeholder,
+                options: [
+                    { text: CoADaoId, value: CoADaoId },
+                    { text: HoMDaoId, value: HoMDaoId },
+                    { text: TCDaoId, value: TCDaoId }
+                ],
+                value: state.house,
+                onChange: (house) => {
+                    onUpdate(house.value);
+                    State.update({
+                        house: house.value
+                    });
+                },
+                error: undefined
+            }}
+        />
+    </div>
+);
