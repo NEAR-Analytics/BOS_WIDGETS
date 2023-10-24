7dbda3fc828d41c6affbcdb7a7f98fb9843f1941
bluebiu.near <bluebiu.near>
2023-10-24 02:22:24 +0000

    Update Gnosis.Lending by bluebiu.near at 2023-10-24T02:22:24.970Z

--- bluebiu.near/Gnosis.Lending/source_code.jsx
+++ bluebiu.near/Gnosis.Lending/source_code.jsx
@@ -43,6 +43,7 @@ return (
           chainId: CHAIN_ID,
           chainName: CHAIN_NAME,
         },
+        defaultDapp: "Agave",
         dapps: {
           Agave: {
             name: "Agave",
