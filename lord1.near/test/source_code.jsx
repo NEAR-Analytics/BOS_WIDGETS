be21f7b5956984b6b7fbf2d621603a5cf6349ed0
lord1.near <lord1.near>
2023-10-24 21:50:05 +0000

    Update test by lord1.near at 2023-10-24T21:50:05.498Z

--- lord1.near/test/source_code.jsx
+++ lord1.near/test/source_code.jsx
@@ -1,72 +1,33 @@
-const label = props.label ?? "Label";
-const placeholder = props.placeholder ?? "Placeholder";
-const value = props.value ?? "";
-const onChange = props.onChange ?? (() => {});
-const validate = props.validate ?? (() => {});
-const error = props.error ?? "";
-const labelcolor = props.labelcolor ?? "#000";
-const inputbackgroundcolor = props.inputbackgroundcolor ?? "#000";
-const inputcolor = props.inputcolor ?? "#000";
+const nn = {
+  display: "flex",
+  "flex-direction": "row",
+  "justify-content": "center",
+  "align-items": "center",
+  padding: "0.75em 1em",
+  gap: "0.5em",
+  border: "none",
+  "border-radius": "50px",
+  background: "#00ec97",
+  color: "#11181c",
+  "font-style": "normal",
+  "font-weight": "600",
+  "font-size": "0.95em",
+  "line-height": "1em",
+  "text-align": "center",
+  "white-space": "nowrap",
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-  padding: 0px;
-  gap: 0.45em;
-  width: 100%;
-`;
-
-const Label = styled.label`
-  font-style: normal;
-  font-weight: 600;
-  font-size: 0.95em;
-  line-height: 1.25em;
-  color: rgb(120, 158, 251);
-`;
-
-const Error = styled.span`
-  display: inline-block;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 0.75em;
-  line-height: 1.25em;
-  color: #ff4d4f;
-  height: 0;
-  overflow: hidden;
-  transition: height 0.3s ease-in-out;
-
-  &.show {
-    height: 1.25em;
-  }
-`;
-
-const Input = styled.input`
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0.5em 0.75em;
-  gap: 0.5em;
-  background:  rgb(210, 202, 250);
-  border: 1px solid #d0d5dd;
-  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
-  border-radius: 4px;
-  color: #394869;
-  width: 100%;
-`;
+  "&:disabled": {
+    background: "#e5e5e5",
+  },
+};
 
 return (
-  <Container>
-    <Label>{label}</Label>
-    <Input
-      type="text"
-      placeholder={placeholder}
-      value={value}
-      onChange={({ target: { value } }) => onChange(value)}
-      onBlur={() => validate()}
-    />
-    <Error className={error ? "show" : ""}>{error}</Error>
-  </Container>
+  <CommitButton
+    stype={nn}
+    onClick={props.onClick}
+    onCommit={props.onCommit}
+    disabled={props.disabled}
+  >
+    {props.text}
+  </CommitButton>
 );
