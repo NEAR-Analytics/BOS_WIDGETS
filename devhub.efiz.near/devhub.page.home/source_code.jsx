75bafb429a93660193af20397dfec60fbd493309
devhub.efiz.near <devhub.efiz.near>
2023-10-24 17:58:04 +0000

    Update devhub.page.home by devhub.efiz.near at 2023-10-24T17:58:04.554Z

--- devhub.efiz.near/devhub.page.home/source_code.jsx
+++ devhub.efiz.near/devhub.page.home/source_code.jsx
@@ -4,7 +4,7 @@ return (
   <>
     {HomeSections.map((it) => (
       <Widget
-        src={`devhub.efiz.near/widget/devhub.components.island.${it}`}
+        src={`${REPL_DEVHUB}/widget/devhub.components.island.${it}`}
         props={{ ...props }}
       />
     ))}
