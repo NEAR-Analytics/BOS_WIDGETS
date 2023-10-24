ee37087af7aabdfac925f40f3af0f94913ef4a40
marketplacebos.near <marketplacebos.near>
2023-10-24 09:10:54 +0000

    Update Index by marketplacebos.near at 2023-10-24T09:10:54.565Z

--- marketplacebos.near/Index/source_code.jsx
+++ marketplacebos.near/Index/source_code.jsx
@@ -22,7 +22,7 @@ const Tabs = styled.div`
   border-right: 1px solid #eceef0;
   padding-right: 24px;
   overflow-y: auto;
-  max-height: 1000px;
+  max-height: 700px;
   position: sticky;
   top: 0; 
   @media (max-width: 1200px) {
@@ -46,18 +46,16 @@ const TabsButton = styled.button`
   font-size: 23px;
   padding: 12px 0;
   position: relative;
-  color: white;
-  background-image: linear-gradient(90deg, rgba(251,136,255,1) 0%, rgba(252,176,69,1) 100%);
-  border: #ffac32ff;
+  color: ${(p) => (p.selected ? "black" : "white")};
+  background: none !important;
+  border: none !important;
   outline: none;
-  text-align: center;
+  text-align: left;
   text-decoration: none !important;
 
   &:hover {
-    background: #ffd83e;
+    color: #11181c;
     cursor: pointer;
-    color:white;
-
   }
 
   &::after {
@@ -68,46 +66,26 @@ const TabsButton = styled.button`
     right: 0;
     bottom: 0;
     width: 6px;
-    background: #ffd83e;
-   
+    background: orange;
+    background: 
+      radial-gradient(rgba(63,94,251,1), rgba(63,94,251,1) 40%, rgba(63,94,251,1) 40%, rgba(252,70,107,1) 60%, rgba(252,70,107,1)100%), 
+      linear-gradient(to right, rgb(255, 217, 0), rgb(255, 217, 0), rgb(255, 217, 0) 10%, rgb(230, 54, 97), rgb(157, 0, 253), rgb(230, 54, 97), rgb(255, 217, 0) 90%, rgb(255, 217, 0), rgb(255, 217, 0));
+    background-color: rgba(255, 255, 255, 0.5);
+    background-size: 200% 100%, 200% 100%;
+    background-position: 100% 0, 100% 0;
+
+    animation: moveLines 3s linear infinite;
+
+    @keyframes moveLines {
+      0% {
+        background-position: 200% 0, 200% 0;
+      }
+      100% {
+        background-position: -200% 0, -200% 0;
+      }
+    }
 }
 `;
-
-const Game = styled.button`
-  display: inline-flex;
-  align-items: center;
-  justify-content: flex-start;
-  font-weight: 600;
-  font-size: 23px;
-  padding: 12px 0;
-  position: relative;
-  color: white;
-  background-image: linear-gradient(90deg, rgba(251,136,255,1) 0%, rgba(252,176,69,1) 100%);
-  border: #ffac32ff;
-  outline: none;
-  text-align: center;
-  text-decoration: none !important;
-
-  &:hover {
-    background: #ffd83e;
-    cursor: pointer;
-    color:white;
-
-  }
-
-  &::after {
-    content: "";
-    display: ${(p) => (p.selected ? "block" : "none")};
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    width: 6px;
-    background: #ffd83e;
-   
-}
-`;
-
 const handleTabClick = (tab) => {
   State.update({
     selectedTab: tab,
@@ -120,7 +98,7 @@ return (
     </>
     <div class="row">
       <div class="col-md-3">
-        <Wrapper style={{ cursor: "pointer" }}>
+        <Wrapper>
           <Tabs>
             <TabsButton
               onClick={() => handleTabClick("buttons")}
@@ -185,31 +163,6 @@ return (
               <Title>Patterns</Title>
             </TabsButton>
             <TabsButton
-              onClick={() => handleTabClick("feedbacks")}
-              selected={state.selectedTab === "feedbacks"}
-            >
-              <Title>Feedback Apps</Title>
-            </TabsButton>
-
-            <TabsButton
-              onClick={() => handleTabClick("fonts")}
-              selected={state.selectedTab === "fonts"}
-            >
-              <Title>Fonts Style</Title>
-            </TabsButton>
-            <TabsButton
-              onClick={() => handleTabClick("IPFS")}
-              selected={state.selectedTab === "IPFS"}
-            >
-              <Title>IPFS uploads</Title>
-            </TabsButton>
-            <TabsButton
-              onClick={() => handleTabClick("games")}
-              selected={state.selectedTab === "games"}
-            >
-              <Title>BOS Games</Title>
-            </TabsButton>
-            <TabsButton
               onClick={() => handleTabClick("myfavorites")}
               selected={state.selectedTab === "myfavorites"}
             >
@@ -222,47 +175,47 @@ return (
         {state.selectedTab === "buttons" && (
           <>
             <Widget src="marketplacebos.near/widget/Page.ButtonPage1" />
+            <br />
+            <br />
           </>
         )}
         {state.selectedTab === "checkboxes" && (
           <>
-            <Widget src="marketplacebos.near/widget/Page.CheckBoxPage1" />
             <br />
             <br />
           </>
         )}
         {state.selectedTab === "toggleswitches" && (
           <>
-            <Widget src="marketplacebos.near/widget/Page.ToggleSwitchPage1" />
+     
             <br />
             <br />
           </>
         )}
         {state.selectedTab === "cards" && (
           <>
-            <Widget src="marketplacebos.near/widget/Page.CardPage1" />
-
+       
             <br />
             <br />
           </>
         )}
         {state.selectedTab === "loaders" && (
           <>
-            <Widget src="marketplacebos.near/widget/Page.LoaderPage1" />
+     
             <br />
             <br />
           </>
         )}{" "}
         {state.selectedTab === "inputs" && (
           <>
-            <Widget src="marketplacebos.near/widget/Page.InputPage1" />
+         
             <br />
             <br />
           </>
         )}{" "}
         {state.selectedTab === "radiobuttons" && (
           <>
-            <Widget src="marketplacebos.near/widget/Page.RadioButtonPage1" />
+         
             <br />
             <br />
           </>
@@ -270,7 +223,7 @@ return (
         {""}
         {state.selectedTab === "forms" && (
           <>
-            <Widget src="marketplacebos.near/widget/Page.FormPage1" />
+           
             <br />
             <br />
           </>
@@ -278,15 +231,7 @@ return (
         {""}
         {state.selectedTab === "charts" && (
           <>
-            <Widget src="marketplacebos.near/widget/Page.ChartPage1" />
-            <br />
-            <br />
-          </>
-        )}
-        {""}
-        {state.selectedTab === "feedbacks" && (
-          <>
-            <Widget src="marketplacebos.near/widget/Feedback.Widget.Index" />
+         
             <br />
             <br />
           </>
@@ -294,15 +239,7 @@ return (
         {""}
         {state.selectedTab === "patterns" && (
           <>
-            <Widget src="marketplacebos.near/widget/Page.PatternPage1" />
-            <br />
-            <br />
-          </>
-        )}
-        {""}
-        {state.selectedTab === "fonts" && (
-          <>
-            <Widget src="marketplacebos.near/widget/Page.FontPage1" />
+        
             <br />
             <br />
           </>
@@ -310,7 +247,7 @@ return (
         {state.selectedTab === "myfavorites" && (
           <>
             {" "}
-            <Widget src="marketplacebos.near/widget/Page.MyFavouritesPage" />
+            
             <br />
             <br />
           </>
