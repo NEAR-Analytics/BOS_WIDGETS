dfb7a023595818df171972ef107b3e0612afdd02
nfcommunity.near <nfcommunity.near>
2023-10-25 10:49:35 +0000

    Update Untitled-0 by nfcommunity.near at 2023-10-25T10:49:35.296Z

--- nfcommunity.near/Untitled-0/source_code.jsx
+++ nfcommunity.near/Untitled-0/source_code.jsx
@@ -1,6 +1,6 @@
 const accountId = props.accountId ?? "*";
 
-const data = Social.keys(`${accountId}/post/nqn18`, "final", {
+const data = Social.keys(`${accountId}/post/nqn19`, "final", {
   return_type: "History",
 });
 
@@ -15,7 +15,7 @@ const processData = (data) => {
   const allItems = accounts
     .map((account) => {
       const accountId = account[0];
-      const blockHeights = account[1].post.nqn18;
+      const blockHeights = account[1].post.nqn19;
       return blockHeights.map((blockHeight) => ({
         accountId,
         blockHeight,
@@ -34,12 +34,12 @@ return (
   <div>
     <h1>🤓 Near Quiz Night</h1>
     <h3>
-      Click to confirm your attendance for the October 18 2023 Near Quiz Night!
+      Click to confirm your attendance for the October 25 2023 Near Quiz Night!
     </h3>
     <CommitButton
       data={{
         post: {
-          nqn18: "I participated in the October 18 2023 Near Quiz Night",
+          nqn19: "I participated in the October 25 2023 Near Quiz Night",
         },
       }}
       onCommit={() => {
@@ -62,7 +62,7 @@ return (
       {allItems
         ? allItems.map(({ accountId }) => (
             <div>
-              I participated in the October 18 2023 Near Quiz Night
+              I participated in the October 25 2023 Near Quiz Night
               <Widget
                 src="williamxx.near/widget/Attribution"
                 props={{
