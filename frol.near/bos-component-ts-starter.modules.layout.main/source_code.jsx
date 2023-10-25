0795af086ed1ff6d89bbc45f8137dc8aa681f839
frol.near <frol.near>
2023-10-25 20:01:59 +0000

    Update bos-component-ts-starter.modules.layout.main by frol.near at 2023-10-25T20:01:59.499Z

--- frol.near/bos-component-ts-starter.modules.layout.main/source_code.jsx
+++ frol.near/bos-component-ts-starter.modules.layout.main/source_code.jsx
@@ -5,8 +5,26 @@ Homepage: https://github.com/frol/bos-component-ts-starter#readme
 */
 
 function AppLayout(props, context) {
+  const cssFont = fetch(
+    "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800"
+  ).body;
+  const css = fetch(
+    "https://pluminite.mypinata.cloud/ipfs/Qmboz8aoSvVXLeP5pZbRtNKtDD3kX5D9DEnfMn2ZGSJWtP"
+  ).body;
+
+  if (!state.theme) {
+    State.update({
+      theme: styled.div`
+      font-family: Manrope, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      ${cssFont}
+      ${css}
+  `,
+    });
+  }
+  const Theme = state.theme;
+
   return (
-    <>
+    <Theme>
       <h1>DEMO. You are at "{props.title}" page</h1>
       <ul>
         <li>
@@ -26,7 +44,7 @@ function AppLayout(props, context) {
         </li>
       </ul>
       {props.children}
-    </>
+    </Theme>
   );
 }
 
