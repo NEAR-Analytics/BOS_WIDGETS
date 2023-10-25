8c465b6581dc9de1c391fba8e633b33da2465077
frol.near <frol.near>
2023-10-25 18:33:36 +0000

    Update bos-component-ts-starter.components.layout.main by frol.near at 2023-10-25T18:33:36.085Z

--- frol.near/bos-component-ts-starter.components.layout.main/source_code.jsx
+++ frol.near/bos-component-ts-starter.components.layout.main/source_code.jsx
@@ -12,17 +12,23 @@ function MainComponent(props, context) {
   return (
     <>
       <h1>DEMO. You are at "{props.title}" page</h1>
-      <nav>
-        <a href="/frol.near/widget/bos-component-ts-starter.pages.page-a">
-          Go to Page A
-        </a>
-        <a href="/frol.near/widget/bos-component-ts-starter.pages.page-b">
-          Go to Page B
-        </a>
-        <a href="/frol.near/widget/bos-component-ts-starter.pages.page-a?customWelcomeMessage=heyoo">
-          Go to Page A with custom message
-        </a>
-      </nav>
+      <ul>
+        <li>
+          <a href="/frol.near/widget/bos-component-ts-starter.pages.page-a">
+            Go to Page A
+          </a>
+        </li>
+        <li>
+          <a href="/frol.near/widget/bos-component-ts-starter.pages.page-b">
+            Go to Page B
+          </a>
+        </li>
+        <li>
+          <a href="/frol.near/widget/bos-component-ts-starter.pages.page-a?customWelcomeMessage=heyoo">
+            Go to Page A with custom message
+          </a>
+        </li>
+      </ul>
       {props.children}
     </>
   );
