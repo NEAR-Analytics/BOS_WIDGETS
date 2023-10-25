2de623f429c4aeaa2d775e54c43c31075d5c8791
pivortex.near <pivortex.near>
2023-10-25 11:15:21 +0000

    Update Untitled-0 by pivortex.near at 2023-10-25T11:15:21.465Z

---
+++ pivortex.near/Untitled-0/source_code.jsx
@@ -0,0 +1,14 @@
+const user = "gagdiez.near";
+const props = { name: "Anna" };
+
+return (
+  <>
+    <div class="container min-vw-100">
+      <h3> Composing Widgets </h3>
+      <p> Widgets can be composed </p>
+      <hr />
+
+      <Widget src={`${user}/widget/Greetings`} props={props} />
+    </div>
+  </>
+);
