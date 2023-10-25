cc066e5b9936993693740e4c3d51fcd8c24e5ab6
frol.near <frol.near>
2023-10-25 18:43:28 +0000

    Update bos-component-ts-starter.modules.layout.main by frol.near at 2023-10-25T18:43:28.759Z

---
+++ frol.near/bos-component-ts-starter.modules.layout.main/source_code.jsx
@@ -0,0 +1,33 @@
+/*
+License: MIT
+Author: frol.near
+Homepage: https://github.com/frol/bos-component-ts-starter#readme
+*/
+
+function MainComponent(props, context) {
+  return (
+    <>
+      <h1>DEMO. You are at "{props.title}" page</h1>
+      <ul>
+        <li>
+          <a href="/frol.near/widget/bos-component-ts-starter.pages.page-a">
+            Go to Page A
+          </a>
+        </li>
+        <li>
+          <a href="/frol.near/widget/bos-component-ts-starter.pages.page-b">
+            Go to Page B
+          </a>
+        </li>
+        <li>
+          <a href="/frol.near/widget/bos-component-ts-starter.pages.page-a?customWelcomeMessage=heyoo">
+            Go to Page A with custom message
+          </a>
+        </li>
+      </ul>
+      {props.children}
+    </>
+  );
+}
+
+return { MainComponent };
