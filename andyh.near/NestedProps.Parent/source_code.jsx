d3512ddda50ec450546a58f64256d024592b42e3
andyh.near <andyh.near>
2023-10-26 23:59:33 +0000

    Update NestedProps.Parent by andyh.near at 2023-10-26T23:59:33.956Z

--- andyh.near/NestedProps.Parent/source_code.jsx
+++ andyh.near/NestedProps.Parent/source_code.jsx
@@ -7,7 +7,7 @@ return (
       src="andyh.near/widget/NestedProps.Child"
       props={{
         incrementByOne: () => setValue((v) => v + 1),
-        arr: ["oh", "hi", "there"],
+        arr: ["oh", "hi", () => "there"],
         // byTen: {
         //   increment: () => setValue((v) => v + 10),
         // },
