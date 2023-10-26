8dd7c1ea7c76efafab238daaa2763fc4a48c7a4a
marketplacebos.near <marketplacebos.near>
2023-10-26 08:10:28 +0000

    Update Button.ButtonP.Button0016 by marketplacebos.near at 2023-10-26T08:10:28.543Z

--- marketplacebos.near/Button.ButtonP.Button0016/source_code.jsx
+++ marketplacebos.near/Button.ButtonP.Button0016/source_code.jsx
@@ -1,13 +1,13 @@
 const Button0016 = styled.button`
-  width: ${(props) => props.width||"150px"};
-  height: ${(props) => props.height||"60px"};
+  width: ${(props) => props.width || "150px"};
+  height: ${(props) => props.height || "60px"};
   border: 3px solid #315cfd;
   border-radius: 45px;
   transition: all 0.3s;
   cursor: pointer;
   background: white;
-  font-size: 1.2em;
-  font-weight: 550;
+  font-size: ${(props) => props.fontsize || "1.2em"};
+  font-weight:  ${(props) => props.fontweight || "550"};
   font-family: 'Montserrat', sans-serif;
 
   &:hover {
@@ -15,7 +15,13 @@ const Button0016 = styled.button`
     color: white;
     font-size: 1.5em;
   }`;
-  const text = props.text || "Button0016";
-  const width = props.width
-  const height = props.height
-return <Button0016 width ={width} height={height}>{text}</Button0016>;
+const text = props.text || "Button0016";
+const width = props.width;
+const height = props.height;
+const fontsize = props.fontsize;
+const fontweight = props.fontweight;
+return (
+  <Button0016 width={width} height={height}>
+    {text}
+  </Button0016>
+);
