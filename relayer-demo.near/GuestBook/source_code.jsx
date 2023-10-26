2bfbfaad3d2a3963d543ec9e1fd77bd92e897a75
relayer-demo.near <relayer-demo.near>
2023-10-26 10:14:10 +0000

    Update GuestBook by relayer-demo.near at 2023-10-26T10:14:10.283Z

--- relayer-demo.near/GuestBook/source_code.jsx
+++ relayer-demo.near/GuestBook/source_code.jsx
@@ -1,5 +1,69 @@
+const contract = "guest-book.near";
+const messages = Near.view(contract, "getMessages", {}).reverse();
+console.log(messages);
+
+State.init({
+  newMessage: "",
+});
+
+const addNewMessage = () => {
+  if (state.newMessage.trim() == "") {
+    return;
+  }
+
+  Near.call(contract, "addMessage", {
+    text: state.newMessage,
+  });
+};
+
 return (
-  <a href="https://near.org/relayer-demo.near/widget/RelayerMessageDemo">
-    RelayerMessageDemo
-  </a>
+  <div class="p-3">
+    <h3 class="text-center">Relayer Demo</h3>
+    <br />
+    {context.accountId ? (
+      <div class="border border-black p-3">
+        <h3>Add a message</h3>
+        <div class="row">
+          <div>
+            <input
+              placeholder="Message"
+              onChange={(e) => State.update({ newMessage: e.target.value })}
+            />
+          </div>
+        </div>
+        <button
+          class="btn btn-primary mt-2"
+          onClick={async () => {
+            addNewMessage();
+          }}
+        >
+          Add Message
+        </button>
+      </div>
+    ) : (
+      <p class="text-center py-2">Log in to add a message</p>
+    )}
+    <br />
+    <div class="border border-black p-3">
+      <h3>Messages</h3>
+      <table className="table table-sm">
+        <thead>
+          <tr class="p-3 mb-2 bg-primary text-white text-center">
+            <th>Account ID</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {messages.map((data, key) => {
+            return (
+              <tr class="text-center">
+                <td>{data.sender}</td>
+                <td>{data.text}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  </div>
 );
