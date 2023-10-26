6d501d95583ca2b6c51d967cccbe0da6f075db34
jass.near <jass.near>
2023-10-26 07:17:24 +0000

    Update calendarembed by jass.near at 2023-10-26T07:17:24.136Z

---
+++ jass.near/calendarembed/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <iframe
+    src="https://calendar.google.com/calendar/embed?height=800&wkst=2&bgcolor=%23f4f4f4&ctz=UTC&mode=AGENDA&showTitle=0&showNav=0&showPrint=0&showTabs=0&showCalendars=0&showDate=0&showTz=1&src=Y19mNTRlZDM3ZmQ5MjMyN2FjZGM3ZTQzNDNmZTQwNzIyYWU1Nzk3YjZjODI5MjliYTkzZTlmM2E4OWM2OTY1N2FiQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23616161"
+    style="border-width:0"
+    width="1116"
+    height="800"
+    frameborder="0"
+    scrolling="no"
+  ></iframe>
+);
