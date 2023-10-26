711812cd1d971e975b311e50e4ccc4f365b04060
andyh.near <andyh.near>
2023-10-26 23:46:31 +0000

    Update NestedProps.Parent by andyh.near at 2023-10-26T23:46:31.020Z

--- andyh.near/NestedProps.Parent/source_code.jsx
+++ andyh.near/NestedProps.Parent/source_code.jsx
@@ -1,13 +1,10 @@
 const [value, setValue] = useState(null);
 
 return (
-  <>
-    <Widget
-      src="andyh.near/widget/NestedProps.Child"
-      props={{
-        spaghetti: 1,
-        //   incrementByOne: () => setValue((v) => v + 1),
-      }}
-    />
-  </>
+  <Widget
+    src="andyh.near/widget/NestedProps.Child"
+    props={{
+      incrementByOne: () => setValue((v) => v + 1),
+    }}
+  />
 );
