8004f33fa19ba91292955bfd6888ce378591fa37
andyh.near <andyh.near>
2023-10-26 01:10:44 +0000

    Update BWE.NewlineBug by andyh.near at 2023-10-26T01:10:44.064Z

--- andyh.near/BWE.NewlineBug/source_code.jsx
+++ andyh.near/BWE.NewlineBug/source_code.jsx
@@ -2,7 +2,7 @@ return (
   <div>
     <Widget
       src="mphere.near/widget/BWE.StringRender"
-      props={{ content: `"New\nLine - C\rR - T\tab"` }}
+      props={{ content: "New\nLine - C\rR - T\tab" }}
     />
   </div>
 );
