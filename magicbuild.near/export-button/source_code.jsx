ef09fa20f210bc0b2c563edc43b1e02c218d37b0
magicbuild.near <magicbuild.near>
2023-10-26 14:00:16 +0000

    Update export-button by magicbuild.near at 2023-10-26T14:00:16.162Z

--- magicbuild.near/export-button/source_code.jsx
+++ magicbuild.near/export-button/source_code.jsx
@@ -115,25 +115,7 @@ return (
                 A new widget configured with the form will be created.
               </small>
             </div>
-            {state.cMethod &&
-              state.cMethod.map((functions, fIndex) => (
-                <div class="form-check form-switch">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    role="switch"
-                    checked={functions.export}
-                    onChange={() => onSwitchChangeArgExport(fIndex)}
-                    id={`flexSwitchCheckDefaultView${fIndex}`}
-                  />
-                  <label
-                    class="form-check-label"
-                    for={`flexSwitchCheckDefault${fIndex}`}
-                  >
-                    {functions.name}
-                  </label>
-                </div>
-              ))}
+
             {state.export && state.widgetName && (
               <>
                 <hr />
