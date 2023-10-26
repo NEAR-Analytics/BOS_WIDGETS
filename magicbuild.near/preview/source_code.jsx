ea022e792f596208ec65b4449c8462b2181e552d
magicbuild.near <magicbuild.near>
2023-10-26 09:26:30 +0000

    Update preview by magicbuild.near at 2023-10-26T09:26:30.287Z

--- magicbuild.near/preview/source_code.jsx
+++ magicbuild.near/preview/source_code.jsx
@@ -40,6 +40,7 @@ const onBtnClickCall = (fName, action, fIndex) => {
       if (item.type == "array") {
         item.value = item.value.split("|");
       }
+      console.log(item.type);
       if (item.type == "json") {
         item.value = JSON.parse(item.value);
       }
