4608b14dc0a571370759f877a5ee99f6668497a2
devhub.efiz.near <devhub.efiz.near>
2023-10-26 01:02:32 +0000

    Update devhub.components.templates.AppLayout by devhub.efiz.near at 2023-10-26T01:02:32.799Z

--- devhub.efiz.near/devhub.components.templates.AppLayout/source_code.jsx
+++ devhub.efiz.near/devhub.components.templates.AppLayout/source_code.jsx
@@ -107,17 +107,9 @@ const AppHeader = ({ page }) => {
     </StyledHeader>
   );
 };
-// const AppHeader = ({ page }) => (
-//   <Widget
-//     src="devhub.efiz.near/widget/devhub.components.organism.Navbar"
-//     props={{
-//       page: page,
-//       ...props,
-//     }}
-//   />
-// );
 
 function AppLayout({ page, children }) {
+  console.log("page", page);
   return (
     <Container>
       <AppHeader page={page} />
