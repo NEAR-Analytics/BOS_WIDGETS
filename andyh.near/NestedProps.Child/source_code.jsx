ef43f5bc51e90280dc60eb8ebe330ff1393024e9
andyh.near <andyh.near>
2023-10-27 17:43:11 +0000

    Update NestedProps.Child by andyh.near at 2023-10-27T17:43:11.665Z

--- andyh.near/NestedProps.Child/source_code.jsx
+++ andyh.near/NestedProps.Child/source_code.jsx
@@ -3,5 +3,8 @@ return (
     <button className="btn btn-warning" onClick={() => props.incrementByOne()}>
       +1
     </button>
+    <button className="btn btn-danger" onClick={() => props.byTen.increment()}>
+      +10
+    </button>
   </>
 );
