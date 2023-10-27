70d2a1a79f143aa60745a1fc460c202f93c1a439
dapdapbos.near <dapdapbos.near>
2023-10-27 02:15:35 +0000

    Update Swap.QuoterSolidly by dapdapbos.near at 2023-10-27T02:15:35.339Z

---
+++ dapdapbos.near/Swap.QuoterSolidly/source_code.jsx
@@ -0,0 +1,99 @@
+const {
+  update,
+  routerAddress,
+  wethAddress,
+  inputCurrency,
+  outputCurrency,
+  inputCurrencyAmount,
+  outputCurrencyAmount,
+  tradeType,
+  onLoad,
+} = props;
+console.log("props quoter : ", props);
+
+if (!update) return;
+
+if (!inputCurrency.address || !outputCurrency.address || !inputCurrencyAmount) {
+  onLoad({});
+  return;
+}
+const wrapType =
+  inputCurrency.address === "native" && outputCurrency.address === wethAddress
+    ? 1
+    : inputCurrency.address === wethAddress &&
+      outputCurrency.address === "native"
+    ? 2
+    : 0;
+if (wrapType) {
+  onLoad(
+    tradeType === "in"
+      ? {
+          outputCurrencyAmount: inputCurrencyAmount,
+          loading: false,
+          noPair: false,
+        }
+      : {
+          inputCurrencyAmount: outputCurrencyAmount,
+          loading: false,
+          noPair: false,
+        }
+  );
+  return;
+}
+const currentCurrency = tradeType === "in" ? inputCurrency : outputCurrency;
+
+const currentAmount = Big(
+  tradeType === "in" ? inputCurrencyAmount : outputCurrencyAmount
+).toFixed();
+const outCurrency = tradeType === "in" ? outputCurrency : inputCurrency;
+
+const RouterContract = new ethers.Contract(
+  routerAddress,
+  [
+    {
+      inputs: [
+        { internalType: "uint256", name: "amountIn", type: "uint256" },
+        { internalType: "address", name: "tokenIn", type: "address" },
+        { internalType: "address", name: "tokenOut", type: "address" },
+      ],
+      name: "getAmountOut",
+      outputs: [
+        { internalType: "uint256", name: "amount", type: "uint256" },
+        { internalType: "bool", name: "stable", type: "bool" },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ],
+  Ethers.provider().getSigner()
+);
+const path = [
+  currentCurrency.address === "native" ? wethAddress : currentCurrency.address,
+  outCurrency.address === "native" ? wethAddress : outCurrency.address,
+];
+
+const currentAmountUnit = Big(currentAmount)
+  .times(Big(10).pow(currentCurrency.decimals))
+  .toFixed(0);
+
+RouterContract.getAmountOut(currentAmountUnit, ...path)
+  .then((res) => {
+    const amount = Big(
+      ethers.utils.formatUnits(res[0], outCurrency.decimals)
+    ).toFixed();
+
+    onLoad({
+      outputCurrencyAmount: Big(amount).gt(0.01)
+        ? amount
+        : Big(amount).toFixed(),
+      noPair: !Big(amount || "").gt(0),
+      stable: res[1],
+    });
+  })
+  .catch((err) => {
+    onLoad({
+      loading: false,
+      noPair: true,
+      stable: false,
+    });
+  });
