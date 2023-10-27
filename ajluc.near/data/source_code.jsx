08030cb84806448bc03213e5a8675944c5d2ea45
ajluc.near <ajluc.near>
2023-10-27 19:07:50 +0000

    Update data by ajluc.near at 2023-10-27T19:07:50.419Z

--- ajluc.near/data/source_code.jsx
+++ ajluc.near/data/source_code.jsx
@@ -1,5 +1,237 @@
 return [
   {
+    zip: 10001,
+    address: "246 10TH AVE",
+    city: "NEW YORK",
+    county: 62,
+    latitude: 40.7484452,
+    longitude: -74.0032123,
+    built: 1910,
+    onMarket: false,
+    units: [
+      {
+        unit: "4C",
+        beds: 2,
+        baths: 2,
+        onMarket: false,
+        verified: false,
+        rating: 4.3,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 10002,
+    address: "205 ALLEN ST",
+    city: "NEW YORK",
+    county: 62,
+    latitude: 40.7226818,
+    longitude: -73.9890289,
+    built: 1900,
+    onMarket: false,
+    units: [
+      {
+        unit: "3A",
+        beds: 3,
+        baths: 2,
+        onMarket: false,
+        verified: true,
+        rating: 2.3,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 10003,
+    address: "228 E 10TH ST",
+    city: "NEW YORK",
+    county: 62,
+    latitude: 40.7289841,
+    longitude: -73.985488,
+    built: 1900,
+    onMarket: true,
+    units: [
+      {
+        unit: "3A",
+        beds: 3,
+        baths: 2,
+        onMarket: false,
+        verified: true,
+        rating: 2.3,
+        priceHistory: {
+          2023: 4300,
+        },
+        unit: "1A",
+        beds: 1,
+        baths: 1,
+        onMarket: true,
+        verified: true,
+        rating: 4.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 10004,
+    address: "66 PEARL ST",
+    city: "NEW YORK",
+    county: 62,
+    latitude: 40.7035127,
+    longitude: -74.0109006,
+    built: 1880,
+    onMarket: false,
+    units: [
+      {
+        unit: "3A",
+        beds: 3,
+        baths: 2,
+        onMarket: false,
+        verified: true,
+        rating: 2.3,
+        priceHistory: {
+          2023: 4300,
+        },
+        unit: "1A",
+        beds: 1,
+        baths: 1,
+        onMarket: false,
+        verified: true,
+        rating: 4.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 10005,
+    address: "10 HANOVER SQ",
+    city: "NEW YORK",
+    county: 62,
+    latitude: 40.705085,
+    longitude: -74.0091652,
+    built: 1900,
+    onMarket: false,
+    units: [
+      {
+        unit: "3A",
+        beds: 3,
+        baths: 2,
+        onMarket: false,
+        verified: false,
+        rating: 4.3,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 10006,
+    address: "120 LIBERTY ST",
+    city: "NEW YORK",
+    county: 62,
+    latitude: 43.6390742,
+    longitude: -79.4176324,
+    built: 1900,
+    onMarket: true,
+    units: [
+      {
+        unit: "3A",
+        beds: 2,
+        baths: 2,
+        onMarket: true,
+        verified: true,
+        rating: 3.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 10007,
+    address: "94 CHAMBERS ST",
+    city: "NEW YORK",
+    county: 62,
+    latitude: 40.7145201,
+    longitude: -74.0075922,
+    built: 1915,
+    onMarket: true,
+    units: [
+      {
+        unit: "3A",
+        beds: 2,
+        baths: 2,
+        onMarket: true,
+        verified: true,
+        rating: 3.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 10009,
+    address: "16 1ST AVE",
+    city: "NEW YORK",
+    county: 62,
+    latitude: 40.7233826,
+    longitude: -73.9879728,
+    built: 1900,
+    onMarket: true,
+    units: [
+      {
+        unit: "1A",
+        beds: 2,
+        baths: 1,
+        onMarket: true,
+        verified: true,
+        rating: 3.9,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 10010,
+    address: "141 E 23RD ST",
+    city: "NEW YORK",
+    county: 62,
+    latitude: 40.7395731,
+    longitude: -73.9841793,
+    built: 1900,
+    onMarket: true,
+    units: [
+      {
+        unit: "1A",
+        beds: 2,
+        baths: 1,
+        onMarket: true,
+        verified: true,
+        rating: 3.9,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+    reviews: [
+      {
+        author: "Jane Doe",
+        review: "Clean enough. OK management company.",
+      },
+    ],
+  },
+
+  {
     zip: 11201,
     address: "436 ALBEE SQ",
     city: "BROOKLYN",
@@ -7,6 +239,7 @@ return [
     latitude: 40.690732,
     longitude: -73.983675,
     built: 2016,
+    onMarket: true,
     units: [
       {
         unit: "2A",
@@ -39,7 +272,6 @@ return [
       },
     ],
   },
-
   {
     zip: 11203,
     address: "974 NEW YORK AVE",
@@ -48,6 +280,7 @@ return [
     latitude: 40.64957,
     longitude: -73.94683,
     built: 1931,
+    onMarket: false,
     units: [
       {
         unit: "12A",
@@ -83,6 +316,7 @@ return [
     latitude: 40.6320333,
     longitude: -73.9800672,
     built: 1930,
+    onMarket: false,
     units: [
       {
         unit: "12A",
@@ -104,6 +338,135 @@ return [
         onMarket: false,
         verified: true,
         rating: 1.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 11205,
+    address: "2 ADELPHI ST",
+    city: "BROOKLYN",
+    county: 61,
+    latitude: 40.6976823,
+    longitude: -73.9729259,
+    built: 1905,
+    onMarket: false,
+    units: [
+      {
+        unit: "12A",
+        beds: 2,
+        baths: 2,
+        onMarket: false,
+        verified: true,
+        rating: 4.6,
+        priceHistory: {
+          2023: 3800,
+          2021: 3650,
+        },
+        reviews: [],
+      },
+      {
+        unit: "1F",
+        beds: 1,
+        baths: 1,
+        onMarket: false,
+        verified: true,
+        rating: 1.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 11207,
+    address: "19 ELDERT ST",
+    city: "BROOKLYN",
+    county: 61,
+    latitude: 40.6858765,
+    longitude: -73.9142539,
+    built: 2004,
+    onMarket: true,
+    units: [
+      {
+        unit: "3D",
+        beds: 3,
+        baths: 2,
+        onMarket: true,
+        verified: false,
+        rating: 4.75,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 11208,
+    address: "53 BERRIMAN ST",
+    city: "BROOKLYN",
+    county: 61,
+    latitude: 40.6776628,
+    longitude: -73.8805298,
+    built: 2004,
+    onMarket: false,
+    units: [
+      {
+        unit: "#2",
+        beds: 2,
+        baths: 2,
+        onMarket: false,
+        verified: false,
+        rating: 4.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+
+  {
+    zip: 10451,
+    address: "3055 3RD AVE",
+    city: "BRONX",
+    county: 60,
+    latitude: 40.8204962,
+    longitude: -73.9131823,
+    built: 2007,
+    onMarket: false,
+    units: [
+      {
+        unit: "#2",
+        beds: 2,
+        baths: 2,
+        onMarket: false,
+        verified: false,
+        rating: 4.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 10452,
+    address: "27 E 169TH ST",
+    city: "BRONX",
+    county: 60,
+    latitude: 40.8376961,
+    longitude: -73.9185991,
+    built: 2012,
+    onMarket: true,
+    units: [
+      {
+        unit: "2E",
+        beds: 2,
+        baths: 2,
+        onMarket: true,
+        verified: true,
+        rating: 3.5,
         priceHistory: {
           2023: 4300,
         },
