d1ae5ccff177774cecc6f497a343f1df0d8bb1f6
alsakhaev.near <alsakhaev.near>
2023-10-27 14:16:33 +0000

    Update Homepage by alsakhaev.near at 2023-10-27T14:16:33.048Z

---
+++ alsakhaev.near/Homepage/source_code.jsx
@@ -0,0 +1,11 @@
+const accountId = context.accountId;
+
+const homepage = accountId
+  ? Social.get(`${accountId}/settings/near.social/homepage`)
+  : undefined;
+
+if (homepage === null) {
+  return "Loading";
+}
+
+return <div>Test</div>;
