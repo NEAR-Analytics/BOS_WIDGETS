c1da5871fccf8980b31310c53d5078774b32a60e
ajluc.near <ajluc.near>
2023-10-27 17:15:33 +0000

    Update data by ajluc.near at 2023-10-27T17:15:33.764Z

---
+++ ajluc.near/data/source_code.jsx
@@ -0,0 +1,113 @@
+return [
+  {
+    zip: 11201,
+    address: "436 ALBEE SQ",
+    city: "BROOKLYN",
+    county: 61,
+    latitude: 40.690732,
+    longitude: -73.983675,
+    built: 2016,
+    units: [
+      {
+        unit: "2A",
+        beds: 2,
+        baths: 1,
+        onMarket: true,
+        verified: true,
+        rating: 4.6,
+        priceHistory: {
+          2023: 3800,
+          2021: 3650,
+        },
+        reviews: [
+          {
+            author: "Joe Smith",
+            review: "The landlord is friendly.",
+          },
+        ],
+      },
+      {
+        unit: "4C",
+        beds: 2,
+        baths: 2,
+        onMarket: false,
+        verified: false,
+        rating: 4.3,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+
+  {
+    zip: 11203,
+    address: "974 NEW YORK AVE",
+    city: "BROOKLYN",
+    county: 61,
+    latitude: 40.64957,
+    longitude: -73.94683,
+    built: 1931,
+    units: [
+      {
+        unit: "12A",
+        beds: 2,
+        baths: 2,
+        onMarket: false,
+        verified: true,
+        rating: 4.6,
+        priceHistory: {
+          2023: 3800,
+          2021: 3650,
+        },
+        reviews: [],
+      },
+      {
+        unit: "1F",
+        beds: 1,
+        baths: 1,
+        onMarket: false,
+        verified: true,
+        rating: 1.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+  {
+    zip: 11204,
+    address: "4401 17TH AVE",
+    city: "BROOKLYN",
+    county: 61,
+    latitude: 40.6320333,
+    longitude: -73.9800672,
+    built: 1930,
+    units: [
+      {
+        unit: "12A",
+        beds: 2,
+        baths: 2,
+        onMarket: false,
+        verified: true,
+        rating: 4.6,
+        priceHistory: {
+          2023: 3800,
+          2021: 3650,
+        },
+        reviews: [],
+      },
+      {
+        unit: "1F",
+        beds: 1,
+        baths: 1,
+        onMarket: false,
+        verified: true,
+        rating: 1.5,
+        priceHistory: {
+          2023: 4300,
+        },
+      },
+    ],
+  },
+];
