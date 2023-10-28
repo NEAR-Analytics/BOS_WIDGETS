5d8aa5583fc3409fbe24e67328948003fd35cbd5
puzzle0.near <puzzle0.near>
2023-10-28 18:21:11 +0000

    Update Counter by puzzle0.near at 2023-10-28T18:21:11.660Z

--- puzzle0.near/Counter/source_code.jsx
+++ puzzle0.near/Counter/source_code.jsx
@@ -4,6 +4,47 @@ const [payment, setPayment] = useState(0);
 
 let cost_per_item = 0.05;
 
+const Button0024 = styled.button`
+  font-size: 16px;
+  font-weight: 700;
+  color: #ff7576;
+  background-color: #2b3044;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 12px 24px;
+  position: relative;
+  line-height: 24px;
+  border-radius: 9px;
+  box-shadow: 0px 1px 2px #2b3044, 0px 4px 16px #2b3044;
+  transform-style: preserve-3d;
+  transform: scale(var(--s, 1)) perspective(600px)
+    rotateX(var(--rx, 0deg)) rotateY(var(--ry, 0deg));
+  perspective: 600px;
+  transition: transform 0.1s;
+
+  .sp {
+    background: linear-gradient(
+      90deg,
+      #866ee7,
+      #ea60da,
+      #ed8f57,
+      #fbd41d,
+      #2cca91
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    text-fill-color: transparent;
+    display: block;
+  }
+
+  &:active {
+    transition: 0.3s;
+    transform: scale(0.93);
+  }
+`;
+
 const getTotalRewards = () => {
   return count * cost_per_item;
 };
@@ -32,7 +73,7 @@ function Page() {
       return (
         <div>
           <h1>Start A Recycling Session</h1>
-          <button onClick={() => handleStartNew()}>Start New</button>
+          <Button0024 onClick={() => handleStartNew()}>Start New</Button0024>
         </div>
       );
     }
@@ -42,13 +83,13 @@ function Page() {
           <h1>Recycling for Bitcoin</h1>
           <p>Insert Can or Bottle</p>
           <p>Count: {count}</p>
-          <button onClick={() => setCount(count + 1)}>+</button>
-          <button onClick={() => setPage(page - 1)} disabled={page === 0}>
+          <Button0024 onClick={() => setCount(count + 1)}>+</Button0024>
+          <Button0024 onClick={() => setPage(page - 1)} disabled={page === 0}>
             Back
-          </button>
-          <button onClick={() => setPage(page + 1)} disabled={count === 0}>
+          </Button0024>
+          <Button0024 onClick={() => setPage(page + 1)} disabled={count === 0}>
             Next
-          </button>
+          </Button0024>
         </div>
       );
     }
@@ -62,8 +103,8 @@ function Page() {
           <p>Bitcoin Price: ${bitcoinPrice()}</p>
           <p>BTC Rewards Amount: {getBitcoinRewards()} Satoshis</p>
           <p>Select Payment Type:</p>
-          <button onClick={() => setPage(page + 1)}>Cash</button>
-          <button onClick={() => setPage(page + 2)}>Bitcoin</button>
+          <Button0024 onClick={() => setPage(page + 1)}>Cash</Button0024>
+          <Button0024 onClick={() => setPage(page + 2)}>Bitcoin</Button0024>
         </div>
       );
     }
@@ -73,7 +114,7 @@ function Page() {
           <h2>Cash</h2>
           <p>Bottle Refund Amount: ${getTotalRewards()}</p>
           <p>Printing Receipt</p>
-          <button onClick={() => setPage(0)}>Complete</button>
+          <Button0024 onClick={() => setPage(0)}>Complete</Button0024>
         </div>
       );
     }
@@ -90,7 +131,7 @@ function Page() {
             />
           </a>
           <p></p>
-          <button onClick={() => setPage(0)}>Print</button>
+          <Button0024 onClick={() => setPage(0)}>Print</Button0024>
         </div>
       );
     }
