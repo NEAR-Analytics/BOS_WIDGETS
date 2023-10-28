016045e5c9d6405a7dacc44ba04f77746df489c1
imagin.near <imagin.near>
2023-10-28 23:26:30 +0000

    Update NICEcards by imagin.near at 2023-10-28T23:26:30.553Z

--- imagin.near/NICEcards/source_code.jsx
+++ imagin.near/NICEcards/source_code.jsx
@@ -1,124 +1,208 @@
-const header = props.header ?? <></>;
-const body = props.body ?? <></>;
-const footer = props.footer;
+//const Flows = var ;
+//if Flows < 1 {Flows=1};
+
+const SideBar = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+background: #fff;
+border: 1px solid #eceef0;
+box-shadow:
+0px 1px 3px rgba(16, 24, 40, 0.1),
+0px 1px 2px rgba(16, 24, 40, 0.06);
+border-radius: 8px;
+flex-shrink: 0;
+width: 35%;
+`;
+
+const Sortspace = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+background: #fff;
+border: 1px solid #eceef0;
+box-shadow:
+0px 1px 3px rgba(16, 24, 40, 0.1),
+0px 1px 2px rgba(16, 24, 40, 0.06);
+border-radius: 8px;
+flex-shrink: 0;
+width: 100%;
+`;
+
+const App = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+background: #fff;
+border: 1px solid #eceef0;
+box-shadow:
+0px 1px 3px rgba(16, 24, 40, 0.1),
+0px 1px 2px rgba(16, 24, 40, 0.06);
+border-radius: 8px;
+flex-shrink: 0;
+width: 100%;
+`;
 
 const CardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: #fff;
-  border: 1px solid #eceef0;
-  box-shadow:
-    0px 1px 3px rgba(16, 24, 40, 0.1),
-    0px 1px 2px rgba(16, 24, 40, 0.06);
-  border-radius: 8px;
-  flex-shrink: 0;
-  width: 100%;
+display: flex;
+flex-direction: column;
+background: #fff;
+border: 1px solid #eceef0;
+box-shadow:
+0px 1px 3px rgba(16, 24, 40, 0.1),
+0px 1px 2px rgba(16, 24, 40, 0.06);
+border-radius: 8px;
+flex-shrink: 0;
+width: 30%;
 `;
 
 const CardHeader = styled.div`
-  display: ${({ show }) => (show ? "flex" : "none")};
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 0.5em 0;
-  gap: 0.675em;
-  background: #fff9ed;
-  flex: none;
-  order: 0;
-  align-self: stretch;
-  flex-grow: 0;
+display: ${({ show }) => (show ? "flex" : "none")};
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 0.5em 0;
+gap: 0.675em;
+background: #fff9ed;
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
 `;
 
 const CardBody = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
-  padding: 1.5em 1.5em 1em;
-  gap: 0.675em;
-  flex: none;
-  order: 0;
-  align-self: stretch;
-  flex-grow: 1;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+justify-content: space-between;
+padding: 1.5em 1.5em 1em;
+gap: 0.675em;
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 1;
 `;
 
 const CardFooter = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  padding: 1.5em;
-  gap: 1em;
-  border-top: 1px solid #eceef0;
-  flex: none;
-  order: 1;
-  align-self: stretch;
-  flex-grow: 0;
-  flex-shrink: 1;
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 1.5em;
+gap: 1em;
+border-top: 1px solid #eceef0;
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
+flex-shrink: 1;
 `;
 
-return (
-  <CardContainer>
-    <CardHeader show={!!props.header}>{header}</CardHeader>
-    <CardBody>{body}</CardBody>
-    {footer ? <CardFooter>{footer}</CardFooter> : <></>}
-  </CardContainer>
-);
-
-
-
-
-
-const Card = ({ description }) => {
+const Card = ({ description, footer }) => {
   return (
-    <div className="card">
-      <p>{description}</p>
-    </div>
+    <CardContainer>
+      <CardHeader show={!!props.header}>{header}</CardHeader>
+      <CardBody>{description}</CardBody>
+      {footer ? <CardFooter>{footer}</CardFooter> : <></>}
+    </CardContainer>
   );
 };
 
-export default Card;
+const [cards, setCards] = useState([]);
+const [newCardDescription, setNewCardDescription] = useState("");
+const [searchTerm, setSearchTerm] = useState("");
+const [sortByDescription, setSortByDescription] = useState(false);
 
+// State to hold the link input
+const [newLink, setNewLink] = useState("");
 
-.card {
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin: 10px;
-}
+const addCard = () => {
+  if (newCardDescription.trim() === "") {
+    return;
+  }
+  const newCard = {
+    id: Date.now(),
+    description: newCardDescription,
+  };
+  setCards([...cards, newCard]);
+  setNewCardDescription("");
+};
 
-input[type="text"] {
-  padding: 5px;
-  margin-right: 10px;
-}
+// Function to add a card based on the link input
+const addCardFromLink = () => {
+  if (newLink.trim() === "") {
+    return;
+  }
+  const newCard = {
+    id: Date.now(),
+    description: newLink, // Use the link as the description
+    isLink: true, // Add a flag to indicate it's a link card
+  };
+  setCards([...cards, newCard]);
+  setNewLink("");
+};
 
-.card-list {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
+const deleteCard = (id) => {
+  const updatedCards = cards.filter((card) => card.id !== id);
+  setCards(updatedCards);
+};
 
-/* App.css */
-.App {
-  text-align: center;
-  font-family: Arial, sans-serif;
-  margin: 20px;
-}
+const toggleSort = () => {
+  setSortByDescription(!sortByDescription);
+};
 
-.card {
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin: 10px;
-}
+const sortedCards = sortByDescription
+  ? [...cards]
+      .filter((card) =>
+        card.description.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .sort((a, b) => a.description.localeCompare(b.description))
+  : cards;
 
-input[type="text"] {
-  padding: 5px;
-  margin-right: 10px;
-}
+return (
+  <App>
+    <SideBar>
+      <h1>NICE Form</h1>
+      <div>
+        {/* Input for adding a new link */}
+        <input
+          type="text"
+          placeholder="Link"
+          value={newLink}
+          onChange={(e) => setNewLink(e.target.value)}
+        />
+        <button onClick={addCardFromLink}>Add Link Card</button>
+      </div>
+      <input
+        type="text"
+        placeholder="Description"
+        value={newCardDescription}
+        onChange={(e) => setNewCardDescription(e.target.value)}
+      />
+      <button onClick={addCard}>Add Card</button>
+    </SideBar>
+    <Sortspace>
+      <button onClick={toggleSort}>
+        {sortByDescription ? "Sort by ID" : "Sort by Description"}
+      </button>
+      <input
+        type="text"
+        placeholder="Search by Description"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div className="card-space">
+        {sortedCards.map((card) => (
+          <Card
+            key={card.id}
+            description={card.description}
+            isLink={card.isLink} // Pass the isLink flag to the Card component
+            onDelete={() => deleteCard(card.id)}
+          />
+        ))}
+      </div>
+    </Sortspace>
+  </App>
+);
 
-.card-list {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-//not yet functioning, but will allow user to populate a set of cards individually or from an array of descriptions, then search and sort the set by attributes of the linked objects.
-//based on selected sort option, cards will then self-sort into the selected number of columns, and be displayed with a vertical heirarchy based on the selected attribute.
+//Still a WIP, but will allow user to populate a set of cards individually or from an array of descriptions, then search and sort the set by attributes of the linked objects.
+//based on selected sort option, cards will then self-sort into the selected number of Flows (columns), and be displayed with a vertical heirarchy based on the selected attribute.
