368a06a85dc5966b77a30af9472d36670d8358ff
coldy.near <coldy.near>
2023-10-29 09:58:41 +0000

    Update bugey-demo by coldy.near at 2023-10-29T09:58:41.993Z

--- coldy.near/bugey-demo/source_code.jsx
+++ coldy.near/bugey-demo/source_code.jsx
@@ -1,231 +1,25 @@
 State.init({
-  projectId: 1,
-  currentProject: null,
-  pendingProposals: {
-    pages: 1,
-    proposals: [],
-  },
-  approvedProposals: {
-    pages: 1,
-    proposals: [],
-  },
-  declinedProposals: {
-    pages: 1,
-    proposals: [],
-  },
-  activeTab: "pending",
+  projects: [],
 });
+
+const getProjectsList = (page, limit) => {
+  return Near.view("bugeye.learnclub.near", "get_projects", {
+    page,
+    limit,
+  });
+};
 
 const contractId = "bugeye.learnclub.near";
 
-const getProjectsList = (page, limit) => {
-  return Near.view(contractId, "get_projects", {
-    page,
-    limit,
-  });
-};
-
-const getProposalsByProject = (project_id, status, page, limit) => {
-  return Near.view(contractId, "get_proposals_by_project", {
-    project_id,
-    status,
-    page,
-    limit,
-  });
-};
-
 const testView = () => {
-  const projects = getProjectsList(1, 50);
-  if (projects[0]) {
-    c;
-  }
-  console.log(projects);
-};
-
-const tabStyle = {
-  display: "flex",
-  border: "1px solid #ccc",
-  backgroundColor: "#f1f1f1",
-};
-
-const tabButtonStyle = {
-  background: "inherit",
-  border: "none",
-  outline: "none",
-  cursor: "pointer",
-  padding: "14px 16px",
-  transition: "0.3s",
-  fontSize: "17px",
-  flex: "1",
-  boxSizing: "border-box",
-};
-
-const tabButtonHoverStyle = {
-  backgroundColor: "#ddd",
-};
-
-const tabButtonActiveStyle = {
-  backgroundColor: "#ccc",
-};
-
-const tabcontentStyle = {
-  display: "none",
-  padding: "6px 12px",
-  border: "1px solid #ccc",
-  borderTop: "none",
-};
-
-const openTab = (tabName) => {
   State.update({
-    activeTab: tabName,
+    projects: getProjectsList(1, 10),
   });
-  console.log(state.activeTab);
+  console.log(state.projects);
 };
-
-const setPendingProposals = () => {
-  const pendingProposalsData = getProposalsByProject(state.projectId, 0, 1, 10);
-  if (pendingProposalsData[0] && pendingProposalsData[1]) {
-    State.update({
-      pendingProposals: {
-        pages: pendingProposalsData[1],
-        proposals: pendingProposalsData[0],
-      },
-    });
-  }
-};
-
-const viewProposals = (proposals) => {
-  if (proposals.length > 0) {
-    const proposalElements = proposals.map((proposal) => (
-      <div key={proposal.id} id={proposal.id} className="proposal">
-        <div className="id">
-          Issue Id: <strong>{proposal.id}</strong>
-        </div>
-        <div className="reward">
-          Requested Reward:{" "}
-          <strong>
-            {proposal.price} Near{" "}
-            <a
-              href={`https://explorer.mainnet.near.org/accounts/${proposal.author_wallet}`}
-              target="_blank"
-            >
-              (by {proposal.author_wallet})
-            </a>
-          </strong>
-        </div>
-        <div className="project">
-          Project: <strong>{proposal.project_id}</strong>
-        </div>
-        <div className="description">Proposal: {proposal.text}</div>
-      </div>
-    ));
-
-    return <div>{proposalElements}</div>;
-  } else {
-    return <div>No issues</div>;
-  }
-};
-
-const setInitial = () => {
-  setPendingProposals();
-};
-
-setInitial();
 
 return (
   <div>
-    <div id="approvals-block">
-      <h1 id="bug-eye-project-title">Test</h1>
-      <div style={tabStyle} className="tab">
-        <button
-          id="pending-tab"
-          className={`tablinks ${
-            state.activeTab === "pending" ? "active" : ""
-          }`}
-          style={
-            state.activeTab === "pending"
-              ? { ...tabButtonStyle, ...tabButtonActiveStyle }
-              : tabButtonStyle
-          }
-          onClick={() => openTab("pending")}
-        >
-          Pending Issues
-        </button>
-        <button
-          id="approval-tab"
-          className={`tablinks ${
-            state.activeTab === "approved" ? "active" : ""
-          }`}
-          style={
-            state.activeTab === "approved"
-              ? { ...tabButtonStyle, ...tabButtonActiveStyle }
-              : tabButtonStyle
-          }
-          onClick={() => openTab("approved")}
-        >
-          Approved Issues
-        </button>
-        <button
-          id="declined-tab"
-          className={`tablinks ${
-            state.activeTab === "declined" ? "active" : ""
-          }`}
-          style={
-            state.activeTab === "declined"
-              ? { ...tabButtonStyle, ...tabButtonActiveStyle }
-              : tabButtonStyle
-          }
-          onClick={() => openTab("declined")}
-        >
-          Declined Issues
-        </button>
-      </div>
-      <div
-        id="pending"
-        className={`tabcontent ${
-          state.activeTab === "pending" ? "activeTab" : ""
-        }`}
-      >
-        {state.activeTab === "pending" && (
-          <div>
-            <h3>Pending Issues</h3>
-            <div id="proposals-pending-content">
-              {viewProposals(state.pendingProposals.proposals)}
-            </div>
-          </div>
-        )}
-      </div>
-      <div
-        id="approved"
-        className={`tabcontent ${
-          state.activeTab === "approved" ? "activeTab" : ""
-        }`}
-      >
-        {state.activeTab === "approved" && (
-          <div>
-            <h3>Approved Issues</h3>
-            <div id="proposals-approved-content">
-              Content for Approved Issues
-            </div>
-          </div>
-        )}
-      </div>
-      <div
-        id="declined"
-        className={`tabcontent ${
-          state.activeTab === "declined" ? "activeTab" : ""
-        }`}
-      >
-        {state.activeTab === "declined" && (
-          <div>
-            <h3>Declined Issues</h3>
-            <div id="proposals-declined-content">
-              Content for Declined Issues
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
     <button onClick={testView}>test view</button>
   </div>
 );
