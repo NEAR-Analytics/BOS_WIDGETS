020b6c57abdf466925bed59071d1dc36c2fceb10
coldy.near <coldy.near>
2023-10-29 10:07:49 +0000

    Update bugey-demo by coldy.near at 2023-10-29T10:07:49.116Z

--- coldy.near/bugey-demo/source_code.jsx
+++ coldy.near/bugey-demo/source_code.jsx
@@ -10,10 +10,20 @@ const getProjectsList = (page, limit) => {
   });
 };
 
+const getProposalsByProject = (project_id, status, page, limit) => {
+  return Near.view(contractId, "get_proposals_by_project", {
+    project_id,
+    status,
+    page,
+    limit,
+  });
+};
+
 const testView = () => {
   State.update({
     projects: getProjectsList(1, 10),
   });
+  console.log(getProposalsByProject(1, 1, 1, 10));
   console.log(state.projects);
 };
 
