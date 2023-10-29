0f0f3f29798655cd71bb1d50a4e5317157b35ef1
coldy.near <coldy.near>
2023-10-29 09:38:02 +0000

    Update bugey-demo by coldy.near at 2023-10-29T09:38:02.927Z

--- coldy.near/bugey-demo/source_code.jsx
+++ coldy.near/bugey-demo/source_code.jsx
@@ -1,8 +1,37 @@
 State.init({
-  value: "value to update",
+  projects: [],
 });
 
-console.log(Near.view("nearsocialexamples.near", "get_greeting"));
+const contractId = 'bugeye.learnclub.near';
+
+const getProjects = async () => {
+  let i = 1;
+  let limit = 20;
+  let projects = {};
+  while (i >= 1) {
+    const data = Near.view({
+      contractId: contractId,
+      method: "get_projects",
+      args: {
+        page: i,
+        limit: limit,
+        account_id: window.mainWallet.accountId,
+      },
+    });
+    const arrayAtIndex0 = data[0];
+    for (let project of arrayAtIndex0) {
+      projects[project["name"]] = project;
+    }
+    if (data[1] <= i) {
+      break;
+    }
+    if (data[1] > i) {
+      i++;
+    }
+  }
+  return projects;
+};
+
 
 const testCall = () => {
   return Near.call("nearsocialexamples.near", "set_greeting", {
@@ -12,15 +41,12 @@ const testCall = () => {
 
 const testView = () => {
   State.update({
-    value: Near.view("nearsocialexamples.near", "get_greeting"),
+    prpjects: await getProjects(),
   });
 };
 
 return (
   <div>
     <button onClick={testCall}>test call</button>
-    <button onClick={testView}>test view</button>
-
-    <div>{state.value}</div>
   </div>
 );
