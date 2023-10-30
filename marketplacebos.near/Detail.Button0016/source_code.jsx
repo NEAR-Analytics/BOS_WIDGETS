238b257862680465a5e4e1f80e864a0664c9e450
marketplacebos.near <marketplacebos.near>
2023-10-30 15:01:28 +0000

    Update Detail.Button0016 by marketplacebos.near at 2023-10-30T15:01:28.808Z

--- marketplacebos.near/Detail.Button0016/source_code.jsx
+++ marketplacebos.near/Detail.Button0016/source_code.jsx
@@ -13,11 +13,11 @@ const props = {
     <Widget src="marketplacebos.near/widget/Button.ButtonP.Button0016" />
   ),
   text: `const props ={
-      width: "150px",
-      height: "60px",
-      fontsize: "1.2em",
-      fontweight: "550",
-      text:"Button0016"
+  width: "150px",
+  height: "60px",
+  fontsize: "1.2em",
+  fontweight: "550",
+  text:"Button0016"
   };
   return(
 <Widget src="marketplacebos.near/widget/Button.ButtonP.Button0016" props={props}/>
