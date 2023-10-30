267372b69b6d58214588519a2ee622d908e27704
bluebiu.near <bluebiu.near>
2023-10-30 11:48:53 +0000

    Update Base.Lending by bluebiu.near at 2023-10-30T11:48:53.899Z

--- bluebiu.near/Base.Lending/source_code.jsx
+++ bluebiu.near/Base.Lending/source_code.jsx
@@ -1,3 +1,5 @@
+const { defaultDapp } = props;
+
 const Theme = styled.div`
   --button-text-color: #fff;
   --button-color: #004bfc;
@@ -43,7 +45,7 @@ return (
           chainId: CHAIN_ID,
           chainName: CHAIN_NAME,
         },
-        defaultDapp: "Moonwell",
+        defaultDapp: defaultDapp || "Moonwell",
         dapps: {
           Moonwell: {
             name: "Moonwell",
