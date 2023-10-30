d166d57163433178ae3efeee0c16ab7e71840e76
marketplacebos.near <marketplacebos.near>
2023-10-30 17:36:18 +0000

    Update Detail.Button0027 by marketplacebos.near at 2023-10-30T17:36:18.548Z

---
+++ marketplacebos.near/Detail.Button0027/source_code.jsx
@@ -0,0 +1,29 @@
+const props = {
+  copyBtn: `const props ={
+      fontsize: "17px",
+      fontweight: "600",
+      text:"Button0027"
+    };
+    return(
+  <Widget src="marketplacebos.near/widget/Button.ButtonP.Button0027" props={props}/>
+      );`,
+  component: (
+    <Widget src="marketplacebos.near/widget/Button.ButtonP.Button0027" />
+  ),
+  text: `const props ={
+      fontsize: "17px",
+      fontweight: "600",
+      text:"Button0027"
+    };
+return(
+  <Widget src="marketplacebos.near/widget/Button.ButtonP.Button0027" props={props}/>
+      );`,
+};
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/CardMain.CardDetail"
+      props={props}
+    />
+  </>
+);
