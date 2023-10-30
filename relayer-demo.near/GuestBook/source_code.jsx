a2cbe3390c4201392503b2de0dc3eb1c2a62990e
relayer-demo.near <relayer-demo.near>
2023-10-30 12:55:21 +0000

    Update GuestBook by relayer-demo.near at 2023-10-30T12:55:21.625Z

--- relayer-demo.near/GuestBook/source_code.jsx
+++ relayer-demo.near/GuestBook/source_code.jsx
@@ -1,5 +1,9 @@
 const contract = "guest-book.near";
-const messages = Near.view(contract, "getMessages", {}).reverse();
+const messages = Near.view(
+  contract,
+  "getMessages",
+  {}
+).reverse();
 console.log(messages);
 
 State.init({
@@ -16,24 +20,28 @@ const addNewMessage = () => {
   });
 };
 
-const userAccountStatus = fetch("https://rpc.mainnet.near.org", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    jsonrpc: "2.0",
-    id: "dontcare",
-    method: "query",
-    params: {
-      request_type: "view_account",
-      finality: "final",
-      account_id: context.accountId,
+const userAccountStatus = fetch(
+  "https://rpc.mainnet.near.org",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-  }),
-});
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: "dontcare",
+      method: "query",
+      params: {
+        request_type: "view_account",
+        finality: "final",
+        account_id: context.accountId,
+      },
+    }),
+  }
+);
 
-const nearAmount = (yocto) => (parseInt(yocto) / Math.pow(10, 24)).toFixed(2);
+const nearAmount = (yocto) =>
+  (parseInt(yocto) / Math.pow(10, 24)).toFixed(2);
 
 return (
   <div class="p-3">
@@ -42,14 +50,18 @@ return (
     {context.accountId ? (
       <div class="border border-black p-3">
         <h3>
-          Add a message as {context.accountId} with a balance of{" "}
-          {nearAmount(userAccountStatus.body.result.amount)} NEAR
+          Add a message as {context.accountId} with a
+          balance of{" "}
+          {nearAmount(userAccountStatus.body.result.amount)}{" "}
+          NEAR
         </h3>
         <div class="row">
           <div>
             <input
               placeholder="Message"
-              onChange={(e) => State.update({ newMessage: e.target.value })}
+              onChange={(e) =>
+                State.update({ newMessage: e.target.value })
+              }
             />
           </div>
         </div>
@@ -63,7 +75,9 @@ return (
         </button>
       </div>
     ) : (
-      <p class="text-center py-2">Log in to add a message</p>
+      <p class="text-center py-2">
+        Log in to add a message
+      </p>
     )}
     <br />
     <div class="border border-black p-3">
@@ -78,7 +92,7 @@ return (
         <tbody>
           {messages.map((data, key) => {
             return (
-              <tr class="text-center">
+              <tr class="text-center" key={key}>
                 <td>{data.sender}</td>
                 <td>{data.text}</td>
               </tr>
