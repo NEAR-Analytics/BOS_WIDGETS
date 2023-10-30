0a75766c377a082902508c09c4e7904bedfc46b7
jgodwill.near <jgodwill.near>
2023-10-30 14:37:24 +0000

    Update CPlanet.MainPage.Social by jgodwill.near at 2023-10-30T14:37:24.174Z

---
+++ jgodwill.near/CPlanet.MainPage.Social/source_code.jsx
@@ -0,0 +1,72 @@
+const hashtag = props.hashtag;
+
+if (!state || state.hashtag !== hashtag) {
+  State.update({
+    feedIndex: hashtag ? 1 : context.accountId ? 0 : 1,
+    hashtag,
+  });
+}
+
+const options = [
+  {
+    title: "DAOs Feed",
+  },
+  {
+    title: "My Feed",
+    disabled: !context.accountId,
+  },
+  {
+    title: "All Posts",
+  },
+];
+
+if (hashtag) {
+  options.push({
+    title: `#${hashtag}`,
+  });
+}
+
+let accounts = undefined;
+
+if (state.feedIndex === 0) {
+  const graph = Social.keys(`${context.accountId}/graph/follow/*`, "final");
+  if (graph !== null) {
+    accounts = Object.keys(graph[context.accountId].graph.follow || {});
+    accounts.push(context.accountId);
+  } else {
+    accounts = [];
+  }
+}
+
+return (
+  <>
+    {context.accountId && (
+      <div className="mb-3">
+        <Widget src="jgodwill.near/widget/MainPage.Compose" props={{}} />
+      </div>
+    )}
+    <ul className="nav nav-pills mb-3">
+      {options.reverse().map((option, i) => (
+        <li className="nav-item" key={i}>
+          <button
+            className={`nav-link ${state.feedIndex === i ? "active" : ""} ${
+              option.disabled ? "disabled" : ""
+            }`}
+            aria-disabled={!!option.disabled}
+            onClick={() => !option.disabled && State.update({ feedIndex: i })}
+          >
+            {option.title}
+          </button>
+        </li>
+      ))}
+    </ul>
+    {state.feedIndex === 2 ? (
+      <Widget src="mob.near/widget/Hashtag.Feed" props={{ hashtag }} />
+    ) : (
+      <Widget
+        src="jgodwill.near/widget/MainPage.Feed.Beta"
+        props={{ accounts }}
+      />
+    )}
+  </>
+);
