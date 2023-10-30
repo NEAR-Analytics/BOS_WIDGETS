6e4202e09724ddf697312c1c63c4cd87c2c50ef3
bluebiu.near <bluebiu.near>
2023-10-30 11:48:53 +0000

    Update Optimism.Lending by bluebiu.near at 2023-10-30T11:48:53.899Z

--- bluebiu.near/Optimism.Lending/source_code.jsx
+++ bluebiu.near/Optimism.Lending/source_code.jsx
@@ -1,3 +1,5 @@
+const { defaultDapp } = props;
+
 const Theme = styled.div`
   --button-text-color: #fff;
   --button-color: #ea3431;
@@ -43,7 +45,7 @@ return (
           chainId: CHAIN_ID,
           chainName: CHAIN_NAME,
         },
-        defaultDapp: "Sonne",
+        defaultDapp: defaultDapp || "Sonne",
         dapps: {
           Sonne: {
             name: "Sonne",
