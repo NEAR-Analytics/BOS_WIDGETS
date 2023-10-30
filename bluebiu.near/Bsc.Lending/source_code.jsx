0785d48429f2156aa5972f71dbc4dda676dc72c7
bluebiu.near <bluebiu.near>
2023-10-30 11:48:53 +0000

    Update Bsc.Lending by bluebiu.near at 2023-10-30T11:48:53.899Z

--- bluebiu.near/Bsc.Lending/source_code.jsx
+++ bluebiu.near/Bsc.Lending/source_code.jsx
@@ -1,3 +1,5 @@
+const { defaultDapp } = props;
+
 const Theme = styled.div`
   --button-text-color: #000;
   --button-color: #f3ba2f;
@@ -43,7 +45,7 @@ return (
           chainId: CHAIN_ID,
           chainName: CHAIN_NAME,
         },
-        defaultDapp: "Venus",
+        defaultDapp: defaultDapp || "Venus",
         dapps: {
           Venus: {
             name: "Venus",
