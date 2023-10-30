d10538f1c442e4cc98619efd1eb3dda9c7f39ede
bluebiu.near <bluebiu.near>
2023-10-30 11:48:53 +0000

    Update Mantle.Lending by bluebiu.near at 2023-10-30T11:48:53.899Z

--- bluebiu.near/Mantle.Lending/source_code.jsx
+++ bluebiu.near/Mantle.Lending/source_code.jsx
@@ -1,3 +1,5 @@
+const { defaultDapp } = props;
+
 const Theme = styled.div`
   --button-text-color: #0f1126;
   --button-color: #00ffe0;
@@ -43,7 +45,7 @@ return (
           chainId: CHAIN_ID,
           chainName: CHAIN_NAME,
         },
-        defalutDapp: "Lendle",
+        defaultDapp: defaultDapp || "Lendle",
         dapps: {
           Lendle: {
             name: "Lendle",
