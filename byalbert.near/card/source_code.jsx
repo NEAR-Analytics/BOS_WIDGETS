f5752ea6230a051523ee52e226221c47f2cd0870
byalbert.near <byalbert.near>
2023-10-31 01:13:08 +0000

    Update card by byalbert.near at 2023-10-31T01:13:08.846Z

---
+++ byalbert.near/card/source_code.jsx
@@ -0,0 +1,61 @@
+const { accountId } = props;
+const { id, img, description } = JSON.parse(
+  Social.get(`${accountId}/post/main`, blockHeight) || "null"
+);
+if (!description) {
+  return <h2>No Reviews yet</h2>;
+}
+return (
+  <div
+    style={{
+      padding: "20px",
+      border: "1px solid #e0e0e0",
+      borderRadius: "5px",
+      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    }}
+  >
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <div
+        style={{
+          width: "50px",
+          height: "50px",
+          bordeRadius: "50%",
+          overflow: "hidden",
+          marginRight: "10px",
+        }}
+      >
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1361/1361913.png"
+          alt="Author's Avatar"
+          style={{ width: "90%", height: "90%", objectFit: "cover" }}
+        />
+      </div>
+      <div style={{ fontSize: "21px", fontWeight: "bold" }}>{accountId}</div>
+    </div>
+    <p
+      style={{
+        fontSize: "15px",
+        textAlign: "justify",
+        letterSpacing: "1px",
+        wordSpacing: "2px",
+      }}
+    >
+      {description ? description : "Not Reviews"}
+    </p>
+    <Widget
+      src="mob.near/widget/Image"
+      props={{
+        image: {
+          ipfs_cid: img.cid,
+        },
+        alt: "profile background",
+        className: "w-100 h-100",
+        style: { left: 0, top: 0 },
+      }}
+    />
+    <Widget
+      src="rats.near/widget/voteButton"
+      props={{ item: JSON.parse(id) }}
+    />
+  </div>
+);
