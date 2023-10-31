b286c21435eba0eecfe87de90e2d98a35d859ba6
mob.near <mob.near>
2023-10-31 00:16:58 +0000

    Update Near.FM.Header by mob.near at 2023-10-31T00:16:58.240Z

--- mob.near/Near.FM.Header/source_code.jsx
+++ mob.near/Near.FM.Header/source_code.jsx
@@ -2,9 +2,31 @@ const accountId = props.accountId;
 const isValidAccount =
   accountId && accountId.endsWith(".near") && accountId.split(".").length === 2;
 
+const imgWrapperStyle = { height: "1em", width: "1em" };
+
 return (
   <div className="text-center my-3">
-    <h1>{isValidAccount ? `${accountId}.fm` : "near.fm"}</h1>
+    <h1>
+      {isValidAccount ? (
+        <>
+          <Widget
+            src="mob.near/widget/ProfileImage"
+            loading={<div style={imgWrapperStyle} />}
+            props={{
+              fast,
+              profile,
+              accountId,
+              widgetName,
+              style: imgWrapperStyle,
+              imageClassName: "rounded-circle w-100 h-100",
+            }}
+          />
+          {accountId}.fm
+        </>
+      ) : (
+        "near.fm"
+      )}
+    </h1>
     {isValidAccount ? (
       <div>Your personal premium URL shortener</div>
     ) : (
