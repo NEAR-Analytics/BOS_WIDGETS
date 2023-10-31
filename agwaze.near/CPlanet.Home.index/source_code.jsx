5a66874307c4b7c52b6a99a683eaa4622d8b897d
agwaze.near <agwaze.near>
2023-10-31 14:49:56 +0000

    Update CPlanet.Home.index by agwaze.near at 2023-10-31T14:49:56.739Z

--- agwaze.near/CPlanet.Home.index/source_code.jsx
+++ agwaze.near/CPlanet.Home.index/source_code.jsx
@@ -1,16 +1,47 @@
 const Root = styled.div`
+  .hero {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-bottom: 50px;
 
+    .text:first-letter {
+      color: #FFF;
+      font-family: Helvetica Neue;
+      font-size: 288px;
+      font-style: italic;
+      font-weight: 200;
+      line-height: normal;
+    }
+    .text {
+      color: #FFF;
+      font-family: Helvetica Neue;
+      font-size: 288px;
+      font-style: normal;
+      text-align: right;
+      font-weight: 400;
+      line-height: normal;
+    }
+  }
 `;
 
 return (
   <Root>
     <div
+      className="hero"
       style={{
-        backgroundImage: `url("https://ipfs.near.social/ipfs/")`,
+        backgroundImage: `url("https://ipfs.near.social/ipfs/bafkreicyp6dvoorfcavir27knzieosjjwaslbam73an4ciscx6opbs4wsm")`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
+        height: "900px",
       }}
-    ></div>
+    >
+      <Widget
+        src="agwaze.near/widget/CPlanet.Navbar.index"
+        props={{ isHome: true }}
+      />
+      <h1 className="text">CPLANET</h1>
+    </div>
   </Root>
 );
