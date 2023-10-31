4841b58a1ecc1a36b8888f98af3b74b9d135578a
agwaze.near <agwaze.near>
2023-10-31 14:49:07 +0000

    Update CPlanet.Home.index by agwaze.near at 2023-10-31T14:49:07.310Z

---
+++ agwaze.near/CPlanet.Home.index/source_code.jsx
@@ -0,0 +1,16 @@
+const Root = styled.div`
+
+`;
+
+return (
+  <Root>
+    <div
+      style={{
+        backgroundImage: `url("https://ipfs.near.social/ipfs/")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    ></div>
+  </Root>
+);
