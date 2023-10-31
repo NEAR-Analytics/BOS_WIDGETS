e81f0afbea2565ead9df40eca42f2687b395d184
mob.near <mob.near>
2023-10-31 00:01:17 +0000

    Update Near.FM.Remote by mob.near at 2023-10-31T00:01:17.605Z

--- mob.near/Near.FM.Remote/source_code.jsx
+++ mob.near/Near.FM.Remote/source_code.jsx
@@ -1,7 +1,7 @@
 return (
   <div className="container-xl">
     <div className="row">
-      <Widget src="mob.near/widget/Near.FM.Header" props={{}} />
+      <Widget src="mob.near/widget/Near.FM.Header" props={props} />
       <div className="text-center">
         <a
           className="btn btn-lg btn-primary rounded-5"
