929de26cedec3879cea69f652c253a329478f380
marketplacebos.near <marketplacebos.near>
2023-10-31 15:15:19 +0000

    Update Pattern.Pattern0007 by marketplacebos.near at 2023-10-31T15:15:19.595Z

---
+++ marketplacebos.near/Pattern.Pattern0007/source_code.jsx
@@ -0,0 +1,64 @@
+const Container = styled.div`
+  border-radius: 20px;
+  width: 100%;
+  height: 96%;
+  --color: #a748f5;
+  background: linear-gradient(95deg, var(--color) 25%, transparent 25%) -50px 0,
+        linear-gradient(-15deg, var(--color) 25%, transparent 25%) -50px 0,
+        linear-gradient(25deg, transparent 75%, var(--color) 75%) -50px 0,
+        linear-gradient(-45deg, transparent 75%, var(--color) 75%) -50px 0,
+        linear-gradient(-15deg, transparent 75%, var(--color) 75%) -50px 0;
+  background-color: #b669fd;
+  background-size: 40px 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.p`
+  color:black;
+`;
+const props = {
+  copyBtn:
+    "const Container = styled.div`\n" +
+    `   border-radius: 20px;
+  width: 100%;
+  height: 100%;
+  --color: #a748f5;
+  background: linear-gradient(95deg, var(--color) 25%, transparent 25%) -50px 0,
+        linear-gradient(-15deg, var(--color) 25%, transparent 25%) -50px 0,
+        linear-gradient(25deg, transparent 75%, var(--color) 75%) -50px 0,
+        linear-gradient(-45deg, transparent 75%, var(--color) 75%) -50px 0,
+        linear-gradient(-15deg, transparent 75%, var(--color) 75%) -50px 0;
+  background-color: #b669fd;
+  background-size: 40px 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+` +
+    "\n`;" +
+    "const Text = styled.p`\n" +
+    `  color: black;
+` +
+    "\n`;" +
+    `\n return(
+         <Container>
+      {" "}
+      <Text>Add some text or content here to test!</Text>
+    </Container>  )`,
+  component: (
+    <Container>
+      {" "}
+      <Text>Add some text or content here to test!</Text>
+    </Container>
+  ),
+  detailLink: "https://near.org/marketplacebos.near/widget/Detail.P0007",
+};
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/CardMain.CardPattern"
+      props={props}
+    />
+  </>
+);
