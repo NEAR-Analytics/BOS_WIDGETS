158c851c448ee242bbaf4a9adf9be22b1254d211
guessme.near <guessme.near>
2023-10-31 03:05:08 +0000

    Update ZKEVM.ExecuteRecords by guessme.near at 2023-10-31T03:05:08.101Z

--- guessme.near/ZKEVM.ExecuteRecords/source_code.jsx
+++ guessme.near/ZKEVM.ExecuteRecords/source_code.jsx
@@ -1,5 +1,6 @@
 const Container = styled.div`
   padding-bottom: 65px;
+  margin: 0 8%;
   .pageTitle {
     display: flex;
     align-items: center;
@@ -667,14 +668,10 @@ const AccessKey = Storage.get(
   "guessme.near/widget/ZKEVMWarmUp.add-to-quest-card"
 );
 
+console.log("AccessKey: ", AccessKey);
+
 function get_my_records_list_by_condition() {
-  const params_str = `account_id=${eth_account_id}&page=${
-    state.current_page
-  }&size=${state.page_size}&action_type=${state.search_action}&action_status=${
-    state.search_status
-  }&template=${state.search_template}&account_info=${
-    eth_account_id ? "" : uuid
-  }&action_network_id=zkEVM`;
+  const params_str = `account_id=${eth_account_id}&page=${state.current_page}&size=${state.page_size}&action_type=${state.search_action}&action_status=${state.search_status}&template=${state.search_template}&account_info=${uuid}&action_network_id=zkEVM`;
   asyncFetch(
     `https://test-api.dapdap.net/api/action/get-action-records-by-account?${params_str}`,
     {
