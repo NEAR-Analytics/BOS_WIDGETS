2e4e99159d8a4897bd6aca63f41fb2ea4013a078
bwe-demos.near <bwe-demos.near>
2023-10-31 17:54:14 +0000

    Update Posts.Post by bwe-demos.near at 2023-10-31T17:54:14.795Z

--- bwe-demos.near/Posts.Post/source_code.jsx
+++ bwe-demos.near/Posts.Post/source_code.jsx
@@ -1,52 +1,25 @@
-interface Props {
-  account_id: string;
-  block_timestamp: number;
-  content: string;
+const { content: contentRaw, account_id: author, block_timestamp } = props;
+
+let content = "bad content";
+console.log(contentRaw);
+try {
+  content = JSON.parse(contentRaw).text;
+} catch (e) {
+  console.log("error parsing content", e);
 }
 
-export function BWEComponent(props: Props) {
-  const { content: contentRaw, account_id: author, block_timestamp } = props;
+// debugger;
 
-  let content = 'bad content';
-  console.log(contentRaw);
-  try {
-    content = JSON.parse(contentRaw).text;
-  } catch (e) {
-    console.log('error parsing content', e);
-  }
-
-  // debugger;
-
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        maxWidth: '100%',
-        borderBottom: '1px solid #e7e7e7',
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          columnGap: '0.5rem',
-          alignItems: 'center',
-        }}
-      >
-        <Component
-          src="bwe-demos.near/Posts.Account"
-          props={{ account: author }}
-        />
-        <span style={{ color: 'gray' }}>
-          {new Date(block_timestamp / 1000000).toLocaleString()}
-        </span>
-      </div>
-      <Component
-        src="bwe-demos.near/Posts.Markdown"
-        props={{ content }}
-        // id={post.receipt_id}
-      />
+return (
+  <div style={{ display: "flex", flexDirection: "column", maxWidth: "100%" }}>
+    <div style={{ display: "flex", flexDirection: "row", columnGap: "0.5rem" }}>
+      <span>{author}</span>
+      <span>{new Date(block_timestamp / 1000000).toLocaleString()}</span>
     </div>
-  );
-}
+    <Widget
+      src="bwetest.near/widget/Posts.Markdown"
+      props={{ content }}
+      // id={post.receipt_id}
+    />
+  </div>
+);
