f7e89961211c20b0c6ee0fe305d996a7b626d3f8
mob.near <mob.near>
2023-10-31 00:00:45 +0000

    Update Near.FM.Header by mob.near at 2023-10-31T00:00:45.145Z

--- mob.near/Near.FM.Header/source_code.jsx
+++ mob.near/Near.FM.Header/source_code.jsx
@@ -1,6 +1,14 @@
+const accountId = props.accountId;
+const isValidAccount =
+  accountId && accountId.endsWith(".near") && accountId.split(".").length === 2;
+
 return (
   <div className="text-center my-3">
-    <h1>near.fm</h1>
-    <p>Shorten your long URLs</p>
+    <h1>{isValidAccount ? `${accountId}.fm` : "near.fm"}</h1>
+    {isValidAccount ? (
+      <div>Your personal premium URL shortener</div>
+    ) : (
+      <p>Shorten your long URLs</p>
+    )}
   </div>
 );
