1d49f1fae5ad047e2bee5827d71ed59ef8bdb339
owa-is-bos.near <owa-is-bos.near>
2023-10-31 03:14:46 +0000

    Update Maverick-LP-Addition by owa-is-bos.near at 2023-10-31T03:14:46.123Z

--- owa-is-bos.near/Maverick-LP-Addition/source_code.jsx
+++ owa-is-bos.near/Maverick-LP-Addition/source_code.jsx
@@ -757,13 +757,21 @@ const approveErc20Token = (mode) => {
     "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json"
   ).then((res) => {
     let value, token;
+
     if (mode == "TA") {
-      value = state.tokenABalance.unfixed;
+      value = floatToFixed(
+        state.tokenABalance.fixed,
+        state.poolSelected.tokenA.decimals
+      );
       token = state.poolSelected.tokenA;
     } else {
-      value = state.tokenBBalance.unfixed;
+      value = floatToFixed(
+        state.tokenBBalance.fixed,
+        state.poolSelected.tokenB.decimals
+      );
       token = state.poolSelected.tokenB;
     }
+
     const approveContract = new ethers.Contract(
       token.address,
       res.body,
