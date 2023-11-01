5d68aed070aee0ee23067da4190ae6740862ed0d
nearcon23.near <nearcon23.near>
2023-11-01 17:37:08 +0000

    Update Ticket.QRCode by nearcon23.near at 2023-11-01T17:37:08.952Z

--- nearcon23.near/Ticket.QRCode/source_code.jsx
+++ nearcon23.near/Ticket.QRCode/source_code.jsx
@@ -1,6 +1,10 @@
 const { contractId, secretKey } = props;
+const ownerId = "nearcon23.near";
+const baseUrl = "https://nearcon.app";
 
-const qrPayload = `https://keypom.xyz/nearcon/${contractId}:${secretKey}`;
+// const qrPayload = `https://keypom.xyz/nearcon/${contractId}:${secretKey}`;
+
+const qrPayload = `${baseUrl}/${ownerId}/widget/Ticket.Preview?privateKey=${secretKey}`;
 
 const SVG_CONTENT_TYPE = "image/svg+xml";
 
@@ -12,20 +16,29 @@ const imageToBase64 = (data, type) => {
 const srcData = `
 <html>
 <body>
-  <div id="qrcode"></div>
+  <div id="qrcode" style="display: flex; flex-direction: column; justify-content: center; align-items: center;"></div>
+
   <script src="https://cdn.jsdelivr.net/npm/easyqrcodejs@4.5.0/dist/easy.qrcode.min.js"></script>
   <script type="text/javascript">
     new QRCode(document.getElementById("qrcode"), {
     text: "${qrPayload}",
-  width: 290,
-      height: 290,
-      colorDark: "#000000",
-      colorLight: "#ffffff",
-      correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
+    width: 290,
+    height: 290,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
 
-      logo: "https://nearcon.app/favicon.png",
-});
+    //  backgroundImage: "https://nearpad-images.s3.amazonaws.com/nearcon_small.png",
+    logo: "https://nearpad-images.s3.amazonaws.com/nearcon_small.png",
+    logoBackgroundTransparent: true,
+    logoWidth: 150, 
+    logoHeight: 150,
 
+
+    dotScale: .6,
+    dotScaleTiming: .6,
+    dotScaleA:.6,
+  });
    </script> 
 </body>
 </html>
@@ -36,10 +49,14 @@ return (
     <iframe
       srcDoc={srcData}
       onMessage={(data) => {
-        console.log(data);
         State.update({ qrCodeData: data });
       }}
-      style={{ width: 320, height: 320 }}
+      style={{
+        width: 320,
+        minWidth: 320,
+        height: 320,
+        minHeight: 320,
+      }}
     />
   </div>
 );
