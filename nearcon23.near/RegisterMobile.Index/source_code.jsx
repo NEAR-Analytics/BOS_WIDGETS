5d821dcb2fa637ae2d4fb720d6774262e6a7a74c
nearcon23.near <nearcon23.near>
2023-11-01 20:59:45 +0000

    Update RegisterMobile.Index by nearcon23.near at 2023-11-01T20:59:45.287Z

--- nearcon23.near/RegisterMobile.Index/source_code.jsx
+++ nearcon23.near/RegisterMobile.Index/source_code.jsx
@@ -1,4 +1,6 @@
 const ownerId = "nearcon23.near";
+const apiUrl =
+  "https://gqqkd7l7mk.execute-api.us-east-1.amazonaws.com/mainnet/api/v1";
 
 const Container = styled.div`
     width: 100%;
@@ -13,16 +15,14 @@ const Content = styled.div`
     position: relative;
     width: 100%;
     max-width: 700px;
-    // background-color: rgb(133, 235, 154);
     background-color: #00EC97;
-
     height: 100%;
-    // min-height: 100vh;
     min-height: 836px;
     div {
       width: 100%;
     }
 `;
+
 const SubmitButton = styled.button`
   display: flex;
   flex-direction: row;
@@ -53,45 +53,65 @@ const SubmitButton = styled.button`
 State.init({
   username: "",
   usernameError: "",
-  lastName: "",
-  lastNameError: "",
-  email: "",
-  emailError: "",
-  persona: null,
-  personaError: "",
-  accountId: context.accountId,
-  jobTitle: "",
-  jobTitleError: "",
-  projectOrCompany: "",
-  projectOrCompanyError: "",
-  country: null,
-  countryError: "",
-  age: null,
-  ageError: "",
-  goal: "",
-  goalError: "",
-  twitter: "",
-  twitterError: "",
-  telegram: "",
-  telegramError: "",
-  referral: "",
-  referralError: "",
 });
 
 const isValid = () => {
   return state.username && state.usernameError === "";
 };
 
+const createAccount = () => {
+  State.update({ loading: true });
+
+  asyncFetch(`${apiUrl}/accounts/createAccount`, {
+    body: JSON.stringify({
+      secretKey: props?.secretKey,
+      accountId: `${state.username}`,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  }).then((res) => {
+    const { body } = res;
+    const { newPrivateKey, newPublicKey, accountId } = body;
+    Storage.set("newPrivateKey", newPrivateKey);
+    Storage.set("newPublicKey", newPublicKey);
+    Storage.set("accountId", accountId);
+
+    State.update({ loading: false, redirectToHome: newPrivateKey });
+  });
+};
+
+const valid = (username, cb) => {
+  const apiURL = `${apiUrl}/accounts/${username}/exists`;
+  asyncFetch(apiURL).then(({ body }) => {
+    cb(!!body?.block_hash);
+  });
+};
+
+if (state.redirectToHome) {
+  return <Redirect to={`/mobile?secretkey=${state.redirectToHome}`} />;
+}
+
+const validateInput = () => {
+  if (state.username.length < 3) {
+    State.update({
+      usernameError: "First name must be at least 3 characters",
+    });
+    return false;
+  }
+  if (state.username.length > 100) {
+    State.update({
+      usernameError: "First name must be less than 100 characters",
+    });
+    return false;
+  }
+  return true;
+};
+
 return (
   <Container>
     <Content>
-      <div style={{ zIndex: 2, padding: "0 20px", backgroundColor: "#ffffff" }}>
-        <Widget
-          src={`${ownerId}/widget/Navbar`}
-          // props={{ update, showSidebar, collapsible: state.collapsible }}
-        />
-      </div>
-
       <div
         style={{
           display: "flex",
@@ -111,6 +131,7 @@ return (
           flexDirection: "column",
           justifyContent: "space-between",
           alignItems: "center",
+
           margin: 20,
           width: "calc(100% - 40px)",
         }}
@@ -119,7 +140,7 @@ return (
           <h1
             style={{
               fontSize: 42,
-              fontWeight: 500,
+              fontWeight: 400,
               lineHeight: 1.5,
               textAlign: "center",
             }}
@@ -145,32 +166,89 @@ return (
               label: "Username",
               value: state.username,
               error: state.usernameError,
-              placeholder: "jondow.nearcon23",
-              onChange: (username) => State.update({ username }),
+              placeholder: "johndoe",
+              success: state.username && !state.usernameError,
+              onChange: (username) => {
+                State.update({ username: username?.toLowerCase().trim() });
+                const data = valid(
+                  `${username}.ncon-factory.keypom.testnet`,
+                  (d) => {
+                    const inputValid = validateInput();
+                    if (d) {
+                      State.update({
+                        usernameError: "Username is already taken",
+                        usernameExists: true,
+                      });
+                    } else {
+                      if (inputValid) {
+                        State.update({
+                          usernameError: "",
+                          usernameExists: false,
+                        });
+                      }
+                    }
+                    validateInput();
+                  }
+                );
+                validateInput();
+              },
               validate: () => {
-                if (state.username.length < 3) {
-                  State.update({
-                    usernameError: "First name must be at least 3 characters",
-                  });
-                  return;
-                }
-
-                if (state.username.length > 100) {
-                  State.update({
-                    usernameError:
-                      "First name must be less than 100 characters",
-                  });
-                  return;
-                }
-
-                State.update({ usernameError: "" });
+                validateInput();
               },
             }}
           />
+          <p style={{ marginLeft: 2 }}>
+            {state.username ? `${state.username}.nearcon23.near` : ""}
+          </p>
         </div>
-
-        <SubmitButton className={isValid() ? "" : "disabled"}>
-          Continue
+        <SubmitButton
+          onClick={() => {
+            valid(`${state.username}.nearcon23.near`, (d) => {
+              if (!d && validateInput() === true) {
+                createAccount();
+              } else {
+                if (d) {
+                  State.update({
+                    usernameError: "Username is already taken",
+                    usernameExists: true,
+                  });
+                }
+              }
+            });
+          }}
+          className={isValid() ? "" : "disabled"}
+        >
+          {state.loading ? (
+            <div
+              style={{ height: 20, width: 40, transform: "translateY(-10px)" }}
+            >
+              <svg
+                version="1.1"
+                id="L9"
+                x="0px"
+                y="0px"
+                viewBox="0 0 100 100"
+                enableBackground="new 0 0 0 0"
+              >
+                <path
+                  fill="#fff"
+                  d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+                >
+                  <animateTransform
+                    attributeName="transform"
+                    attributeType="XML"
+                    type="rotate"
+                    dur="1s"
+                    from="0 50 50"
+                    to="360 50 50"
+                    repeatCount="indefinite"
+                  />
+                </path>
+              </svg>
+            </div>
+          ) : (
+            "Submit"
+          )}
         </SubmitButton>
       </div>
     </Content>
