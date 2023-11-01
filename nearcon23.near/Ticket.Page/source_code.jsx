17e53c2d93b49a025a6c9b53792cbba162fa8f01
nearcon23.near <nearcon23.near>
2023-11-01 17:43:47 +0000

    Update Ticket.Page by nearcon23.near at 2023-11-01T17:43:47.760Z

--- nearcon23.near/Ticket.Page/source_code.jsx
+++ nearcon23.near/Ticket.Page/source_code.jsx
@@ -1,8 +1,71 @@
 const ownerId = "nearcon23.near";
+const baseUrl =
+  "https://gqqkd7l7mk.execute-api.us-east-1.amazonaws.com/mainnet/api/v1";
+const {
+  props: { contractId, secretKey },
+} = props;
 
-return (
+const [isValidTicket, setValidTicket] = useState(false);
+const [redirectToMobile, setRedirectToMobile] = useState(false);
+const [redirectCreateAccount, setRedirectCreateAccount] = useState(false);
+
+const apiURL = `${baseUrl}/accounts/${secretKey}/validate`;
+const apiURL2 = `${baseUrl}/accounts/auth/${secretKey}`;
+
+const redirect = () => {
+  asyncFetch(apiURL)
+    .then(({ body }) => {
+      if (body.redirectTo === "CREATE_ACCOUNT_PAGE") {
+        setRedirectCreateAccount(true);
+      } else if (body.redirectTo === "QR_CODE_PAGE") {
+        setValidTicket(true);
+      }
+    })
+    .catch((err) => {
+      setValidTicket(false);
+    });
+};
+
+const checkIfValidAccount = () => {
+  asyncFetch(apiURL2).then(({ body }) => {
+    const { nearconId, newPrivateKey, newPublicKey } = body;
+    if (newPrivateKey) {
+      Storage.set("newPrivateKey", newPrivateKey);
+      Storage.set("newPublicKey", newPublicKey);
+      Storage.set("accountId", nearconId);
+      setRedirectToMobile(true);
+    }
+  });
+};
+
+useEffect(() => {
+  redirect();
+  checkIfValidAccount();
+  const interval = setInterval(() => redirect(), 5000);
+  return () => {
+    clearInterval(interval);
+  };
+}, []);
+
+if (redirectToMobile) {
+  return <Redirect to="/mobile" />;
+}
+
+return redirectCreateAccount ? (
+  <>
+    <Widget
+      props={{
+        secretKey,
+      }}
+      src={`${ownerId}/widget/RegisterMobile.Index`}
+    />
+  </>
+) : (
   <>
     <Widget src={`${ownerId}/widget/Ticket.Header`} />
-    <Widget src={`${ownerId}/widget/Ticket.Content`} props={props} />
+    <Widget
+      src={`${ownerId}/widget/Ticket.Content`}
+      props={{ isValidTicket, contractId, secretKey }}
+    />
   </>
 );
