7fdc4189235f545d744c931fd11fbfe1760cfa76
nearcon23.near <nearcon23.near>
2023-11-01 18:13:38 +0000

    Update Mobile.Home.Send by nearcon23.near at 2023-11-01T18:13:38.487Z

--- nearcon23.near/Mobile.Home.Send/source_code.jsx
+++ nearcon23.near/Mobile.Home.Send/source_code.jsx
@@ -1,23 +1,26 @@
 const theme = props.theme;
 const accountId = props.accountId || context.accountId;
 const ownerId = "nearcon23.near";
+const apiUrl =
+  "https://gqqkd7l7mk.execute-api.us-east-1.amazonaws.com/mainnet/api/v1";
+const splitSubstring = ".nearcon23.near";
 
 const receiverId = props.receiverId || context.receiverId;
 
+const [loading, setLoading] = useState(false);
 const [value, setValue] = useState(null);
 
 const Container = styled.div`
    width: 100%;
+   padding-top:30px;
 `;
 const Content = styled.div`
    width: 100%;
-
    display: flex;
    flex-direction: column;
    justify-content: center;
    align-items: center;
   gap:20px;
-
   padding-left: 20px;
   padding-right : 20px;
 `;
@@ -65,8 +68,10 @@ const Button = styled.button`
   text-align: center;
   white-space: nowrap;
   transition: all 0.3s ease-in-out;
+  font-family: Mona Sans;
 
   width:100%;
+  height:  48px;
 
   &:hover,
   &:focus,
@@ -76,6 +81,31 @@ const Button = styled.button`
     border: 1px solid #11181c;
     color: #fff;
   }
+`;
+
+const DisabledButton = styled.div`
+ display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+  min-width: 10.625em;
+  gap: 0.5em;
+  opacity:0.5;
+  border: 1px solid #00ec97;
+  border-radius: 50px;
+  background: #00ec97;
+  color: #11181c;
+  font-family: Mona Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  text-align: center;
+  white-space: nowrap;
+  transition: all 0.3s ease-in-out;
+
+  width:100%;
+  height:  48px;
 `;
 
 const Button2 = styled.button`
@@ -92,6 +122,7 @@ const Button2 = styled.button`
   color: #11181c;
   font-style: normal;
   font-weight: 600;
+  font-family: Mona Sans;
   font-size: 18px;
   text-align: center;
   white-space: nowrap;
@@ -109,72 +140,204 @@ const Button2 = styled.button`
   }
 `;
 
+function isPositiveFloatOrEmpty(value) {
+  const regex = /^(?:\d+\.\d*|\.\d+|\d+)?$/;
+  return regex.test(value);
+}
+
+const { secretkey } = props;
+const storedSecretKey = Storage.get(
+  "newPrivateKey",
+  `${ownerId}/widget/Ticket.Page`
+)
+  ? Storage.get("newPrivateKey", `${ownerId}/widget/Ticket.Page`)
+  : Storage.get("newPrivateKey", `${ownerId}/widget/RegisterMobile.Index`);
+const key = secretkey ? secretkey : storedSecretKey;
+const fetchData = () => {
+  asyncFetch(`${apiUrl}/accounts/auth/${key}`).then(({ body }) => {
+    State.update({ userData: body });
+  });
+};
+
+useEffect(() => {
+  fetchData();
+}, [secretkey, storedSecretKey]);
+
+if (state.redirectToSuccess) {
+  return (
+    <Redirect
+      to={`/${ownerId}/widget/Mobile.Home.SendSuccess?receiverId=${receiverId}&amount=${value}`}
+    />
+  );
+}
+
+const isValid = () => {
+  const balance = state?.userData?.balance
+    ? parseFloat(state?.userData?.balance)
+    : 0;
+  if (balance >= parseFloat(value)) {
+    return true;
+  }
+  return false;
+};
+
+const ComponentToRender = isValid() ? Button : DisabledButton;
+
+function splitStringAtSubstring(str, substring) {
+  const splitIndex = str?.indexOf?.(substring);
+  if (splitIndex === -1) {
+    // Substring not found, return the original string in an array
+    return [str];
+  }
+
+  // Split the string into the part before the substring and the substring with the rest
+  return [str?.substring?.(0, splitIndex), str?.substring?.(splitIndex)];
+}
+
+// Test the function
+const originalString = receiverId;
+const result = splitStringAtSubstring(originalString, splitSubstring);
+
 return (
-  <Container>
-    <Widget src={`${ownerId}/widget/Navbar`} />
-
-    <Content>
-      <h4
-        style={{
-          fontWeight: 500,
-          color: theme.textColor3,
-          textAlign: "center",
-        }}
-      >
-        To <span style={{ color: theme.textColor }}>{receiverId}</span>
-      </h4>
-
-      <div
-        style={{
-          borderRadius: 8,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-
-          aspectRatio: 3 / 1,
-        }}
-      >
-        <Input
-          type="number"
-          placeholder="0"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-
-        <h3
+  <>
+    <Widget
+      props={{
+        nearconId: state.userData.nearconId,
+        cid: state.userData.cid,
+      }}
+      src={`${ownerId}/widget/Navbar`}
+    />
+    <Container>
+      <Content>
+        <h4
           style={{
-            fontSize: 24,
+            fontFamily: "FK Grotesk",
             fontWeight: 500,
-            margin: 0,
-            color: "#00EC97",
+            color: theme.textColor3,
+            textAlign: "center",
           }}
         >
-          NCON
-        </h3>
-      </div>
+          To{" "}
+          <span style={{ color: theme.textColor }}>
+            {result[0]}
+            <span style={{ color: "#868682" }}>{result[1]}</span>
+          </span>
+        </h4>
 
-      <Link
-        to={`/nearcon23/home/${ownerId}/widget/Mobile.Home.SendSuccess?receiverId=${receiverId}&amount=${value}`}
-      >
-        <Button
-        // href={props.href ?? `/${ownerId}/widget/Index?tab=register`}
-        >
-          Send
-        </Button>
-      </Link>
-
-      <Link to={`/nearcon23/home/${ownerId}/widget/Mobile.Home`}>
-        <Button2
+        <div
           style={{
-            backgroundColor: "#FFFFFF",
-            color: "#000000 !important",
-            border: "none",
+            borderRadius: 8,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+
+            aspectRatio: 3 / 1,
           }}
         >
-          Cancel
-        </Button2>
-      </Link>
-    </Content>
-  </Container>
+          <Input
+            inputmode="numeric"
+            pattern="[0-9]*"
+            type="number"
+            placeholder="0"
+            value={value}
+            onChange={(e) => {
+              let value = e.target.value;
+              let formattedValue = parseFloat(value);
+
+              if (isNaN(formattedValue) && formattedValue < 0) {
+                formattedValue = "";
+              } else {
+                formattedValue = formattedValue.toString();
+              }
+
+              setValue(formattedValue);
+            }}
+          />
+
+          <h3
+            style={{
+              fontSize: 24,
+              fontWeight: 500,
+              margin: 0,
+              color: "#00EC97",
+            }}
+          >
+            NCON
+          </h3>
+        </div>
+
+        <ComponentToRender
+          disabled={loading}
+          onClick={() => {
+            if (isValid()) {
+              setLoading(true);
+              console.log(state.userData);
+              asyncFetch(
+                `https://21mqgszhf3.execute-api.us-east-1.amazonaws.com/testnet/api/v1/accounts/transfer`,
+                {
+                  body: JSON.stringify({
+                    accountId: state.userData.nearconId,
+                    secretKey: key,
+                    sendTo: receiverId,
+                    amount: `${parseFloat(value)}`,
+                  }),
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  method: "POST",
+                }
+              ).then((res) => {
+                const { body } = res;
+                const { newPrivateKey, newPublicKey, accountId } = body;
+                Storage.set("newPrivateKey", newPrivateKey);
+                Storage.set("newPublicKey", newPublicKey);
+                Storage.set("accountId", accountId);
+                State.update({
+                  redirectToSuccess: true,
+                });
+                setLoading(false);
+              });
+            }
+          }}
+        >
+          {loading ? (
+            <div class="spinner-border text-light" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          ) : (
+            "Send"
+          )}
+        </ComponentToRender>
+
+        <Link to={`/mobile`}>
+          <Button2
+            style={{
+              backgroundColor: "#FFFFFF",
+              color: "#000000 !important",
+              border: "none",
+            }}
+          >
+            Cancel
+          </Button2>
+        </Link>
+
+        <p
+          style={{
+            color: "#90908C",
+            fontFamily: "Mona Sans",
+            fontSize: 12,
+            fontStyle: "normal",
+            fontWeight: 400,
+            lineHeight: 1.5,
+
+            textAlign: "center",
+          }}
+        >
+          All NCON expire at the end of the conference <br /> and are worth no
+          monetary value.
+        </p>
+      </Content>
+    </Container>
+  </>
 );
