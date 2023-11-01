6e11219275de5a1f7a7551f8f5f7dfb407cd4d21
nearcon23.near <nearcon23.near>
2023-11-01 18:07:30 +0000

    Update Mobile.Home.SendSuccess by nearcon23.near at 2023-11-01T18:07:30.594Z

--- nearcon23.near/Mobile.Home.SendSuccess/source_code.jsx
+++ nearcon23.near/Mobile.Home.SendSuccess/source_code.jsx
@@ -9,6 +9,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  height: 100vh;
 `;
 
 const Content = styled.div`
@@ -18,9 +19,7 @@ const Content = styled.div`
   width: 100%;
   max-width: 700px;
   background-color: #000000;
-
   overflow: hidden;
-
   height: 100%;
   min-height: 836px;
   div {
@@ -30,7 +29,6 @@ const Content = styled.div`
 
 const H1 = styled.h1`
   margin-top:40px;
-
   color: #00EC97;
   text-align: center;
   leading-trim: both;
@@ -82,88 +80,124 @@ const SubmitButton = styled.button`
   }
 `;
 
+const { secretkey } = props;
+const storedSecretKey = Storage.get(
+  "newPrivateKey",
+  `${ownerId}/widget/Ticket.Page`
+)
+  ? Storage.get("newPrivateKey", `${ownerId}/widget/Ticket.Page`)
+  : Storage.get("newPrivateKey", `${ownerId}/widget/RegisterMobile.Index`);
+const key = secretkey ? secretkey : storedSecretKey;
+const fetchData = () => {
+  asyncFetch(
+    `https://gqqkd7l7mk.execute-api.us-east-1.amazonaws.com/mainnet/api/v1/accounts/auth/${key}`
+  ).then(({ body }) => {
+    State.update({ userData: body });
+  });
+};
+
+useEffect(() => {
+  fetchData();
+}, [secretkey, storedSecretKey]);
+
 return (
-  <Container>
-    <Content>
-      <div style={{ zIndex: 2 }}>
-        <Widget
-          src={`${ownerId}/widget/Navbar`}
-          // props={{ update, showSidebar, collapsible: state.collapsible }}
-        />
-      </div>
-
-      <div
-        style={{
-          display: "flex",
-          position: "absolute",
-          justifyContnet: "center",
-          alignItems: "center",
-        }}
-      >
-        <Widget src={`${ownerId}/widget/Icons.NearLogo`} />
-      </div>
-
-      <div
-        style={{
-          flex: 1,
-          zIndex: 3,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          alignItems: "center",
-
-          margin: 20,
-          width: "calc(100% - 40px)",
-        }}
-      >
+  <>
+    <Widget
+      props={{
+        nearconId: state.userData.nearconId,
+        cid: state.userData.cid,
+      }}
+      src={`${ownerId}/widget/Navbar`}
+    />
+    <Container>
+      <Content>
         <div
           style={{
-            flex: 1,
-            height: "100%",
-            width: "100%",
             display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
+            position: "absolute",
+            justifyContnet: "center",
             alignItems: "center",
           }}
         >
-          <svg
-            width="161"
-            height="161"
-            viewBox="0 0 161 161"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="0.5"
-              y="0.5"
-              width="160"
-              height="160"
-              rx="80"
-              fill="#00EC97"
-            />
-            <path
-              d="M120.657 48.4254L68.0159 103.418C66.517 104.984 64.0393 105.058 62.4495 103.585L38.9581 81.8146C37.8156 80.7558 36.0892 80.6378 34.8133 81.5313C33.2778 82.6066 32.9421 84.7418 34.0736 86.2364L62.8321 124.221C64.0884 125.88 66.5736 125.905 67.8633 124.272L124.86 52.0813C125.78 50.9153 125.625 49.2323 124.506 48.2545C123.384 47.273 121.688 47.3483 120.657 48.4254Z"
-              fill="white"
-            />
-          </svg>
-          <H1>{amount}</H1>
-          <H4 style={{ fontSize: 25, marginTop: -10 }}>NCON</H4>
-          <p style={{ color: "#868682", textAlign: "center" }}>
-            Sent to
-            <br />
-            <span style={{ color: "white" }}>{receiverId}</span>
-            {/*.nearcon23*/}
-          </p>
+          <Widget src={`${ownerId}/widget/Icons.NearLogo`} />
         </div>
 
-        <Link
-          style={{ width: "100%" }}
-          to={`${prefix}/${ownerId}/widget/Mobile.Home`}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            margin: 20,
+            height: "100vh",
+            width: "calc(100% - 40px)",
+          }}
         >
-          <SubmitButton>Home</SubmitButton>
-        </Link>
-      </div>
-    </Content>
-  </Container>
+          <div
+            style={{
+              flex: 1,
+              height: "100%",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <svg
+              width="161"
+              height="161"
+              viewBox="0 0 161 161"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                x="0.5"
+                y="0.5"
+                width="160"
+                height="160"
+                rx="80"
+                fill="#00EC97"
+              />
+              <path
+                d="M120.657 48.4254L68.0159 103.418C66.517 104.984 64.0393 105.058 62.4495 103.585L38.9581 81.8146C37.8156 80.7558 36.0892 80.6378 34.8133 81.5313C33.2778 82.6066 32.9421 84.7418 34.0736 86.2364L62.8321 124.221C64.0884 125.88 66.5736 125.905 67.8633 124.272L124.86 52.0813C125.78 50.9153 125.625 49.2323 124.506 48.2545C123.384 47.273 121.688 47.3483 120.657 48.4254Z"
+                fill="white"
+              />
+            </svg>
+            <H1>{amount}</H1>
+            <H4 style={{ fontSize: 25, marginTop: -10 }}>NCON</H4>
+            <p
+              style={{
+                color: "#868682",
+                textAlign: "center",
+                fontFamily: "FK Grotesk",
+                fontWeight: "500",
+              }}
+            >
+              Sent to
+              <br />
+              {!!receiverId && (
+                <div
+                  style={{
+                    color: "white",
+                    display: "flex",
+                  }}
+                >
+                  {receiverId?.replace?.(".ncon-factory.keypom.testnet", "")}
+                  <p style={{ color: "#868682" }}>
+                    {".ncon-factory.keypom.testnet"}
+                  </p>
+                </div>
+              )}
+              {/*.nearcon23*/}
+            </p>
+          </div>
+
+          <Link style={{ width: "100%", zIndex: 100 }} to={`/${prefix}`}>
+            <SubmitButton>Home</SubmitButton>
+          </Link>
+        </div>
+      </Content>
+    </Container>
+  </>
 );
