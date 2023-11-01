f632406ead5df8bd886e86e3f3d4a9fc485a7417
mob.near <mob.near>
2023-11-01 22:21:45 +0000

    Update ProfileImage by mob.near at 2023-11-01T22:21:45.902Z

--- mob.near/ProfileImage/source_code.jsx
+++ mob.near/ProfileImage/source_code.jsx
@@ -2,7 +2,7 @@ const accountId = props.accountId ?? context.accountId;
 const className = props.className ?? "profile-image d-inline-block";
 const style = props.style ?? { width: "3em", height: "3em" };
 const imageStyle = props.imageStyle ?? { objectFit: "cover" };
-const imageClassName = props.imageClassName ?? "rounded w-100 h-100";
+const imageClassName = props.imageClassName ?? "rounded-circle w-100 h-100";
 const thumbnail = props.thumbnail ?? "thumbnail";
 
 const profile = props.profile ?? Social.getr(`${accountId}/profile`);
