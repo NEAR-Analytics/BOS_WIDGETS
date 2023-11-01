ac4e0a1614d95ce36ea8f8288fff8d9dd3bb6ebc
nearcon23.near <nearcon23.near>
2023-11-01 19:00:27 +0000

    Update Home.Page by nearcon23.near at 2023-11-01T19:00:27.193Z

---
+++ nearcon23.near/Home.Page/source_code.jsx
@@ -0,0 +1,14 @@
+const ownerId = "nearcon23.near";
+
+return (
+  <>
+    <Widget src={`${ownerId}/widget/Home.Header`} />
+    <Widget src={`${ownerId}/widget/Home.SubHeader`} />
+    <Widget src={`${ownerId}/widget/Home.Venue`} />
+    <Widget src={`${ownerId}/widget/Home.Experience`} />
+    <Widget src={`${ownerId}/widget/Home.Tracks`} />
+    <Widget src={`${ownerId}/widget/Home.Partners`} />
+    <Widget src={`${ownerId}/widget/Home.Sponsors`} />
+    <Widget src={`${ownerId}/widget/Home.More`} />
+  </>
+);
