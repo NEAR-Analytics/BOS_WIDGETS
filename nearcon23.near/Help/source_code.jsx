4b669508150055346b09c069c1717a2564f39500
nearcon23.near <nearcon23.near>
2023-11-01 21:08:09 +0000

    Update Help by nearcon23.near at 2023-11-01T21:08:09.060Z

---
+++ nearcon23.near/Help/source_code.jsx
@@ -0,0 +1,13 @@
+return (
+  <div style={{ backgroundColor: "#3D434D" }}>
+    <img
+      style={{
+        objectFit: "contain",
+        width: "100%",
+        height: "80vh",
+        padding: 10,
+      }}
+      src="https://ipfs.near.social/ipfs/bafkreigqoosos445vb7swngiq4zod5lpkunnwhcla5fwthnrg2acqph724"
+    />
+  </div>
+);
