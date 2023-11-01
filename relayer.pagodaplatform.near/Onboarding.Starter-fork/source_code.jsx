238e4cb2f21af359de537298eee25ba051bca7d7
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-11-01 14:43:50 +0000

    Update Onboarding.Starter-fork by relayer.pagodaplatform.near at 2023-11-01T14:43:50.261Z

--- relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
+++ relayer.pagodaplatform.near/Onboarding.Starter-fork/source_code.jsx
@@ -1,16 +1,9 @@
-npm install near-api-js@^0.44.2
-npm install @near-wallet-selector/core
-npm install \
-  @near-wallet-selector/near-wallet \
-  @near-wallet-selector/my-near-wallet \
-  @near-wallet-selector/sender \
-  @near-wallet-selector/nearfi \
-  @near-wallet-selector/here-wallet \
-  @near-wallet-selector/math-wallet \
-  @near-wallet-selector/nightly \
-  @near-wallet-selector/meteor-wallet \
-  @near-wallet-selector/ledger \
-  @near-wallet-selector/wallet-connect \
-  @near-wallet-selector/nightly-connect \
-  @near-wallet-selector/default-wallets \
-  @near-wallet-selector/coin98-wallet
+return (
+  <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
+    <Widget />
+    <br />
+    {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
+    <Widget />
+  </>
+);
