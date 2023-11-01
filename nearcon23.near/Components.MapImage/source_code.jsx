40f1da9f8167d4ef145cba9e5acf3f21cb27e721
nearcon23.near <nearcon23.near>
2023-11-01 21:06:27 +0000

    Update Components.MapImage by nearcon23.near at 2023-11-01T21:06:27.574Z

---
+++ nearcon23.near/Components.MapImage/source_code.jsx
@@ -0,0 +1,13 @@
+return (
+  <img
+    src={images[activeTab]}
+    style={{
+      width: "100%",
+      height: "100vh",
+      objectFit: "contain",
+      top: 0,
+      left: 0,
+      backgroundColor: "black",
+    }}
+  />
+);
