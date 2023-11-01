84362bbbfad0937a2dd8a0987bfec3236e5a3c80
nearcon23.near <nearcon23.near>
2023-11-01 19:07:41 +0000

    Update Home.Partners by nearcon23.near at 2023-11-01T19:07:41.221Z

--- nearcon23.near/Home.Partners/source_code.jsx
+++ nearcon23.near/Home.Partners/source_code.jsx
@@ -1,16 +1,62 @@
 const ownerId = "nearcon23.near";
 
 const desktopImage =
-  "https://ipfs.near.social/ipfs/bafkreieh454tbxt6jxikkmzdvytvrujoglmiuruplwarojm3tkke6ru4s4";
+  "https://ipfs.near.social/ipfs/bafkreic2w3bmzfmnc7e6qohtigrclmvnwgmu6xa3iaerzs5rjxxalqo6he";
 
 const mobileImage =
-  "https://ipfs.near.social/ipfs/bafkreicim25ai3n2zybevbr3p4vxkwujd3r6htclphmnc5n2n57gx2aaji";
+  "https://ipfs.near.social/ipfs/bafkreidqjgiygcw7ejw2hpufp46tueacznpu3mskbjhh46cplfsqptqqey";
 
 const MyImage = styled.img`
-    width:100%;
-    @media (max-width: 650px) {    
-            content:url(${mobileImage});   
-    }
+  width:100%;
+  @media (max-width: 650px) {    
+    content:url(${mobileImage});   
+  }
 `;
 
-return <MyImage src={desktopImage} />;
+const Section = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: flex-start;
+
+  & > div:first-child {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    align-self: stretch;
+
+    & > h2 {
+      display: flex;
+      flex-direction: column;
+      color: var(--black, #000);
+      font-size: 2rem;
+      font-family: FK Grotesk;
+      font-weight: 500;
+    }
+
+    @media screen and (max-width: 768px) {
+      flex-direction: column;
+      align-items: center;
+      gap: 1.5rem;
+    }
+  }
+  @media screen and (max-width: 768px) {
+    padding: 1rem;
+
+  }
+`;
+
+return (
+  <Section>
+    <h2
+      style={{
+        padding: "3.125rem",
+        paddingBottom: 0
+      }}
+    >
+      Media Partners
+    </h2>
+
+    <MyImage src={desktopImage} />
+  </Section>
+);
