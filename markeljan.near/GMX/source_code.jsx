cfb5179a1ff8a7fa03c183e66b32103a53d8edc7
markeljan.near <markeljan.near>
2023-11-02 16:11:59 +0000

    Update GMX by markeljan.near at 2023-11-02T16:11:59.319Z

--- markeljan.near/GMX/source_code.jsx
+++ markeljan.near/GMX/source_code.jsx
@@ -470,7 +470,7 @@ function handleChangePosition(isLong) {
 
 return (
   <DaisyUIWrapper>
-    <div class="card bg-neutral-focus text-white">
+    <div class="card max-w-2xl mx-auto bg-neutral-focus text-white">
       <div class="px-4 pt-4">
         <div class="relative flex justify-center">
           <LogoGMX />
