2649c9bceb69e4a7c2fadc0f87079faa113e5c53
jgodwill.near <jgodwill.near>
2023-11-02 14:46:02 +0000

    Update CPlanet.TopCreator.Card by jgodwill.near at 2023-11-02T14:46:02.691Z

--- jgodwill.near/CPlanet.TopCreator.Card/source_code.jsx
+++ jgodwill.near/CPlanet.TopCreator.Card/source_code.jsx
@@ -18,6 +18,9 @@ const CardRoot = styled.div`
     margin-bottom: 20px;
     display: flex;
     flex-direction: column;
+    *{
+      font-family: Helvetica Neue; 
+    }
     &>a{
       text-decoration: none;
     }
@@ -78,128 +81,51 @@ const ImageProfile = styled.div`
 `;
 
 const HeaderText = styled.div`
-margin-top: 40px;
-border-right: 1px dashed #ccc;
-    margin-bottom: 10px;
-    flex: 0.5;
-    padding: 20px 40px 10px 0;
+margin-top: 30px;
+border-right: 1px dashed rgba(0, 0, 0, 0.30);
+    flex: 1;
+    padding: 20px 40px 10px 0px;
   p {
-    overflow: hidden;
-    color: #B0B0B0;
-    text-overflow: ellipsis;
-    font-family: Helvetica Neue;
+    color: #000; 
     font-size: 16px;
     font-style: normal;
-    font-weight: 400;
-    line-height: normal;
+    font-weight: 700;
+    line-height: 160%; /* 25.6px */ 
   }
 `;
 
 const CardBody = styled.div`
 display: flex;
 padding: 0 16px;
-  h1 {
-    color: #000;
-font-family: Helvetica Neue;
-font-size: 20px;
-font-style: normal;
-text-transform: lowercase;
-font-weight: 700;
-line-height: normal;
-  }
-  h3 {
-    overflow: hidden;
-color: #000;
-text-overflow: ellipsis;
-whitespace: nowrap;
-font-family: Helvetica Neue;
-font-size: 16px;
-font-style: normal;
-font-weight: 400;
-line-height: 148%; 
-  }
+justify-content: space-between;
 `;
 
 const AmountSec = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  gap: 20px;
-  &>div {
-margin: 20px 0; 
+  flex: 1;
+   margin: 45px 0; 
+  &>.nfts {
     display: flex;
     flex-direction: column;
     text-align: center;
     justify-content: center;
     align-items: center;
-    gap: .5rem;
+    // gap: .5rem;
     *{margin: unset;}
-    span {
-      color: #B0B0B0;
-      font-family: Helvetica Neue;
-      font-size: 8px;
-      font-style: normal;
-      font-weight: 400;
-      line-height: normal;
-      text-transform: uppercase;
-    }
-    p {
+    .title {
         color: #000;
         font-family: Helvetica Neue;
-        font-size: 16px;
-        font-style: normal;
-        font-weight: 700;
-        line-height: normal;
-        text-transform: uppercase;
-        display: flex;
-        align-items: center;
-        gap: 5px;
-        span {
-          color: #000;
-          font-family: Helvetica Neue;
-          font-size: 12px;
-          font-style: normal;
-          font-weight: 400;
-          line-height: normal;
-          text-transform: uppercase;
-        }
-    }
-  }
-`;
-
-const Tags = styled.div`
-    display:flex;
-    gap: 7px;
-    margin-left: 10px;
-    margin-top: 40px;
-    .tag {
-         color: #FFF;
-        font-family: Helvetica Neue;
-        font-size: 24px;
-        font-style: normal;
-        font-weight: 700;
-        line-height: normal;
-        border-radius: 50px;
-        background: #F8F8F8;
-        width: max-content;
-        color: #B0B0B0;
-        text-align: center;
-        font-family: Helvetica Neue;
-        font-size: 10px;
+        font-size: 12px;
         font-style: normal;
         font-weight: 500;
-        line-height: 150%; /* 15px */
-        padding: 3px 10px;
+        line-height: 160%; /* 19.2px */ 
     }
-    .no-tag {
-      opacity: 0;
+    .details{
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 300;
+      line-height: 160%; /* 19.2px */ 
     }
-`;
-
-const Footer = styled.div`
-  display: flex;
-  flex-direction: column;
+  }
 `;
 
 const profile = props.profile ?? Social.getr(`${accountId}/profile`);
@@ -224,10 +150,13 @@ const inner = (
     className="text-decoration-none"
   >
     <HeaderText>
-      <h1>{accountId ? makeAccountIdShorter(accountId) : `Name`}</h1>
+      <p>{accountId ? makeAccountIdShorter(accountId) : `Name`}</p>
     </HeaderText>
   </a>
 );
+
+const nftCount = 12;
+const s = nftCount > 1 ? "s" : "";
 return (
   <CardRoot>
     <Top>
@@ -262,7 +191,10 @@ return (
           inner
         )}
         <AmountSec>
-          <div></div>
+          <div className="nfts">
+            <p className="title">Created</p>
+            <p className="details">{`${nftCount} NFT${s}`}</p>
+          </div>
         </AmountSec>
       </CardBody>
     </Bottom>
