b1600282b6b7a3073c5620837954bff9ad7d82a3
y3k.near <y3k.near>
2023-11-02 04:35:45 +0000

    Update near_atlas.components.apps.embed by y3k.near at 2023-11-02T04:35:45.795Z

--- y3k.near/near_atlas.components.apps.embed/source_code.jsx
+++ y3k.near/near_atlas.components.apps.embed/source_code.jsx
@@ -2,7 +2,7 @@ if (!props.docId) {
   props.docId = "1WAYL7Tohgssnta6pBQB0flb37fdtYZxh2l9zrHOeS9Y";
   //   return <div className="">No document ID provided.</div>;
 }
-const url = `https://docs.google.com/presentation/d/${props.docId}/?embedded=true`;
+const url = `https://docs.google.com/presentation/d/${props.docId}/embed`;
 return (
   <>
     <iframe
