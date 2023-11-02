7108b895fcace633c7342597122f423064eec423
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2023-11-02 19:57:10 +0000

    Update devhub.entity.post.Postv2 by devgovgigs.petersalomonsen.near at 2023-11-02T19:57:10.192Z

--- devgovgigs.petersalomonsen.near/devhub.entity.post.Postv2/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.entity.post.Postv2/source_code.jsx
@@ -1,5 +1,5 @@
 const { getPost } =
-  VM.require("devgovgigs.petersalomonsen.near/widget/core.adapter.devhub-contract") ||
+  VM.require("${REPL_DEVHUB}/widget/core.adapter.devhub-contract") ||
   (() => {});
 
 const { postKey, template } = props;
