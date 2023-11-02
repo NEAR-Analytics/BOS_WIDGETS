9d74fc86936acdc658fa7a324cb980cca35aa06d
hack.near <hack.near>
2023-11-02 21:20:59 +0000

    Update project.curation.edit by hack.near at 2023-11-02T21:20:59.042Z

---
+++ hack.near/project.curation.edit/source_code.jsx
@@ -0,0 +1,27 @@
+const curatorId = props.curatorId ?? context.accountId ?? "discover.near";
+
+const projects = Social.get(`${curatorId}/project/**`);
+
+if (projects == null) {
+  return "none found";
+}
+
+const newData = fetch(
+  "https://raw.githubusercontent.com/NEARBuilders/discoverbos/data/build/discoverbos/projects.json"
+);
+
+const handleUpdate = () => {
+  Social.set(`${newData.body}`);
+};
+
+return (
+  <div className="mb-3">
+    <button
+      className="btn text-light btn-info m-2"
+      disabled={!context.accountId}
+      onClick={handleUpdate}
+    >
+      Update Curation
+    </button>
+  </div>
+);
