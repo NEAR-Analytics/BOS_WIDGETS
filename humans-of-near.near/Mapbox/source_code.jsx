ec6bb84aff197371e83f37f2db22840ea0d625df
humans-of-near.near <humans-of-near.near>
2023-11-02 12:17:59 +0000

    Update Mapbox by humans-of-near.near at 2023-11-02T12:17:59.303Z

--- humans-of-near.near/Mapbox/source_code.jsx
+++ humans-of-near.near/Mapbox/source_code.jsx
@@ -157,6 +157,7 @@ const code = `
              (state.twitter? "<a href='"+user.twitter+"' target='_blank' onclick='handleLink("+JSON.stringify(user.twitter)+")'   >  " : '') +
             '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24"><path fill="'+(state.twitter?'white':'grey')+'" d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>'+
              (state.twitter? '</a>' : '') +
+             'Nulla vitae risus lacus. Ut ut libero in est condimentum laoreet. Donec magna justo, finibus ac dapibus nec, euismod non nisi ondimentum laore.'+
            '</div>'+
           '</div>'+
         '</div>';
