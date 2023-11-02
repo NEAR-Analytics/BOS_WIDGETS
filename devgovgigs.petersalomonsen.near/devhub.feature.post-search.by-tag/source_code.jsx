bb7b1ce9b244b6a6b6376cbf72ac94c770f829f7
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2023-11-02 19:57:10 +0000

    Update devhub.feature.post-search.by-tag by devgovgigs.petersalomonsen.near at 2023-11-02T19:57:10.192Z

--- devgovgigs.petersalomonsen.near/devhub.feature.post-search.by-tag/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.feature.post-search.by-tag/source_code.jsx
@@ -1,5 +1,5 @@
 const { getAllLabels } = VM.require(
-  "devgovgigs.petersalomonsen.near/widget/core.adapter.devhub-contract"
+  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
 );
 
 if (!getAllLabels) {
