450d2eab7192d27b73daab3b7e5312fd7f6ad0db
communityvoice.ndctools.near <communityvoice.ndctools.near>
2023-11-02 21:54:14 +0000

    Update CommunityVoice.ArticleView by communityvoice.ndctools.near at 2023-11-02T21:54:14.036Z

--- communityvoice.ndctools.near/CommunityVoice.ArticleView/source_code.jsx
+++ communityvoice.ndctools.near/CommunityVoice.ArticleView/source_code.jsx
@@ -583,7 +583,7 @@ return (
                         onClick: () =>
                           handleShareButton(true, {
                             type: "sharedBlockHeight",
-                            value: data.blockHeight,
+                            value: articleToRenderData.blockHeight,
                           }),
                       }}
                     />
