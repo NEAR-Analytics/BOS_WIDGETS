b78b3f930f3a3ffb99f97ed46816ae9859d7ff10
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2023-11-02 19:40:46 +0000

    Update devhub.entity.community.Teams by devgovgigs.petersalomonsen.near at 2023-11-02T19:40:46.554Z

--- devgovgigs.petersalomonsen.near/devhub.entity.community.Teams/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.entity.community.Teams/source_code.jsx
@@ -1,11 +1,11 @@
 const { handle } = props;
 
 const { Tile } =
-  VM.require("${REPL_DEVHUB}/widget/devhub.components.molecule.Tile") ||
+  VM.require("devgovgigs.petersalomonsen.near/widget/devhub.components.molecule.Tile") ||
   (() => <></>);
 
 const { getCommunity } = VM.require(
-  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
+  "devgovgigs.petersalomonsen.near/widget/core.adapter.devhub-contract"
 );
 
 const communityData = getCommunity({ handle });
