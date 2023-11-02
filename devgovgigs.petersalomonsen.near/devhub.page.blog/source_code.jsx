20f7a065fffc19f60ae4a3063f9c8527533e719a
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2023-11-02 19:40:46 +0000

    Update devhub.page.blog by devgovgigs.petersalomonsen.near at 2023-11-02T19:40:46.554Z

--- devgovgigs.petersalomonsen.near/devhub.page.blog/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.page.blog/source_code.jsx
@@ -1,12 +1,12 @@
 const { id } = props;
 
 const { Page } =
-  VM.require("${REPL_DEVHUB}/widget/devhub.entity.addon.blog.Page") ||
+  VM.require("devgovgigs.petersalomonsen.near/widget/devhub.entity.addon.blog.Page") ||
   (() => <></>);
 
 return (
   <Widget
-    src="${REPL_DEVHUB}/widget/devhub.entity.post.Postv2"
+    src="devgovgigs.petersalomonsen.near/widget/devhub.entity.post.Postv2"
     props={{ postKey: id, template: (p) => <Page {...(p || {})} /> }}
   />
 );
