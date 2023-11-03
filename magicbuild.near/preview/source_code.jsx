1fa5f9a8995360aec1920a6ee1ec52e5d953513a
magicbuild.near <magicbuild.near>
2023-11-03 05:05:21 +0000

    Update preview by magicbuild.near at 2023-11-03T05:05:21.142Z

--- magicbuild.near/preview/source_code.jsx
+++ magicbuild.near/preview/source_code.jsx
@@ -164,7 +164,7 @@ const notLoggedInWarning = <p class="text-center py-2"> Login to Use BOS </p>;
 const Wrapper = styled.div`
  ${state.cssStyle}
 `;
-console.log("css", state.cssStyle);
+console.log("css", props.cssStyle);
 return (
   <>
     <Wrapper class="container">
