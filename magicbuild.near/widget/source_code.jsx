5724a10bfd055c497b6d4558e0266a21b510fcca
magicbuild.near <magicbuild.near>
2023-11-03 04:56:44 +0000

    Update widget by magicbuild.near at 2023-11-03T04:56:44.561Z

--- magicbuild.near/widget/source_code.jsx
+++ magicbuild.near/widget/source_code.jsx
@@ -437,7 +437,12 @@ return (
                 })}
               {functions.selfInputDeposit && (
                 <div className={`form-group pb-2`}>
-                  <label>Deposit</label>
+                  <label>
+                    {" "}
+                    {functions.labelDeposit.length > 0
+                      ? functions.labelDeposit
+                      : "Deposit"}
+                  </label>
                   <input
                     type="text"
                     value={"" + functions.deposit}
