d3362d05352c9ca94309023710e2ea616c957fa9
xvii.near <xvii.near>
2023-11-03 02:24:23 +0000

    Update BOSHACKS.Members.Mentors by xvii.near at 2023-11-03T02:24:23.233Z

--- xvii.near/BOSHACKS.Members.Mentors/source_code.jsx
+++ xvii.near/BOSHACKS.Members.Mentors/source_code.jsx
@@ -1,5 +1,9 @@
 return (
   <div>
     <Widget src="xvii.near/widget/chloe.nearProfileCard" />
+    <Widget src="xvii.near/widget/pironi.nearProfileCard" />
+    <Widget src="xvii.near/widget/jeffgold.nearProfileCard" />
+    <Widget src="xvii.near/widget/fiftycent.nearProfileCard" />
+    <Widget src="xvii.near/widget/alan777.nearProfileCard" />
   </div>
 );
