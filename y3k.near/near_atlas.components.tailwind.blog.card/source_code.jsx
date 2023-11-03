73b1cc6b97a581608000948947be51ca4c3729e4
y3k.near <y3k.near>
2023-11-03 14:46:56 +0000

    Update near_atlas.components.tailwind.blog.card by y3k.near at 2023-11-03T14:46:56.021Z

---
+++ y3k.near/near_atlas.components.tailwind.blog.card/source_code.jsx
@@ -0,0 +1,18 @@
+return (
+  <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+    <img
+      src={props.thumbnail}
+      alt="Placeholder Image"
+      className="w-full h-48 rounded-md object-cover"
+    />
+    <div className="px-1 py-4">
+      <div className="font-bold text-xl text-white mb-2">{props.title}</div>
+      <p className="text-gray-300 text-base">{props.description}</p>
+    </div>
+    <div className="px-1 py-4">
+      <a href="#" className="text-blue-400 hover:underline">
+        Read More
+      </a>
+    </div>
+  </div>
+);
