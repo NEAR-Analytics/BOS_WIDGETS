b74a792a8d5a9e39d47457706c24141470555ea7
devhub.near <devhub.near>
2023-11-03 17:55:40 +0000

    Update app by devhub.near at 2023-11-03T17:55:40.199Z

--- devhub.near/app/source_code.jsx
+++ devhub.near/app/source_code.jsx
@@ -162,14 +162,7 @@ function Page() {
         />
       );
     }
-    case "admin": {
-      return (
-        <Widget
-          src={"devhub.near/widget/devhub.page.admin.index"}
-          props={passProps}
-        />
-      );
-    }
+
     default: {
       // TODO: 404 page
       return <p>404</p>;
