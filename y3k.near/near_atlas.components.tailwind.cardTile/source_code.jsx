a3bee4a4d6bebaf364692ea307bda8e04d7d4804
y3k.near <y3k.near>
2023-11-03 14:24:00 +0000

    Update near_atlas.components.tailwind.cardTile by y3k.near at 2023-11-03T14:24:00.607Z

--- y3k.near/near_atlas.components.tailwind.cardTile/source_code.jsx
+++ y3k.near/near_atlas.components.tailwind.cardTile/source_code.jsx
@@ -1,7 +1,7 @@
 return (
   <div className="bg-gray-800 shadow-xl rounded-lg p-6 w-1/2">
     <div className="flex flex-col items-center">
-      <h3 className="text-xl text-center font-semibold mb-4 text-white">
+      <h3 className=" text-center font-semibold mb-4 text-white">
         {props.Title}
       </h3>
       <span className="text-4xl font-bold text-gray-200">
