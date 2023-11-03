b9dd96bea0d9f899f5bd94c7d456dc6ee0f8e496
y3k.near <y3k.near>
2023-11-03 14:22:40 +0000

    Update near_atlas.components.tailwind.cardTile by y3k.near at 2023-11-03T14:22:40.435Z

--- y3k.near/near_atlas.components.tailwind.cardTile/source_code.jsx
+++ y3k.near/near_atlas.components.tailwind.cardTile/source_code.jsx
@@ -1,15 +1,13 @@
 return (
   <div class="inline-block align-bottom bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all mb-4 w-full sm:w-1/3 sm:my-8">
-    <div class="bg-gray-800 p-5">
-      <div class="sm:flex sm:items-start">
-        <div class="text-center sm:mt-0 sm:ml-2 sm:text-left">
-          <h3 class="text-sm leading-6 font-medium text-gray-400">
-            {props.Title}
-          </h3>
-          <p class="text-3xl font-bold text-gray-200">
-            {props.Stats.toLocaleString("en-US")}
-          </p>
-        </div>
+    <div className="bg-gray-800 shadow-xl rounded-lg p-6 w-1/2">
+      <div className="flex flex-col items-center">
+        <h3 className="text-xl text-center font-semibold mb-4 text-white">
+          {props.Title}
+        </h3>
+        <span className="text-4xl font-bold text-gray-200">
+          {props.Stats.toLocaleString("en-US")}
+        </span>
       </div>
     </div>
   </div>
