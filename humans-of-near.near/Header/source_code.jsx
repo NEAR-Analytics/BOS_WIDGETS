a757bb6d4f385e6f843d23f79a1c021bcb64fd57
humans-of-near.near <humans-of-near.near>
2023-11-03 09:12:56 +0000

    Update Header by humans-of-near.near at 2023-11-03T09:12:56.573Z

--- humans-of-near.near/Header/source_code.jsx
+++ humans-of-near.near/Header/source_code.jsx
@@ -7,9 +7,8 @@ const LogoContainer = styled.div`
   position: absolute;
   pointer-events:auto;
   @media (max-width: 510px) {
-    width: 280px;
     top: 10px;
-    left: 20px;
+    left: 15px;
   }
 `;
 const Logo = () => (
