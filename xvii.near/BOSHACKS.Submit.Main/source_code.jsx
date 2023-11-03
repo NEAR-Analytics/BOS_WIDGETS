b7c225e388d035dc8408adb243d4ecda7c04c0ca
xvii.near <xvii.near>
2023-11-03 01:25:08 +0000

    Update BOSHACKS.Submit.Main by xvii.near at 2023-11-03T01:25:08.954Z

--- xvii.near/BOSHACKS.Submit.Main/source_code.jsx
+++ xvii.near/BOSHACKS.Submit.Main/source_code.jsx
@@ -1,14 +1,10 @@
-/**
- * Prompt instrctiction for how to submit
- * put drop downf or each bount
- * add header
- */
 return (
   <div>
-    <Widget src="" />
-    <Widget
-      src=""
-      props={{ referral: "boshacks-submission", labels: "boshacks, hackathon" }}
-    />
+    <Widget src="xvii.near/widget/HannahResearchCard" />
+    <Widget src="xvii.near/widget/AnnieResearchCard" />
+    <Widget src="xvii.near/widget/AmeResearchCard" />
+    <Widget src="xvii.near/widget/DabbieResearchCard" />
+    <Widget src="xvii.near/widget/NedlarResearchCard" />
+    <Widget src="xvii.near/widget/xviiResearchCard" />
   </div>
 );
