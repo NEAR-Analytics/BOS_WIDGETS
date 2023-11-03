8961d73a37bfb12b39c59c1b8b225faa30098bdf
trustscore.near <trustscore.near>
2023-11-03 04:45:09 +0000

    Update MagicBuild-widget-form-1698985935208 by trustscore.near at 2023-11-03T04:45:09.391Z

--- trustscore.near/MagicBuild-widget-form-1698985935208/source_code.jsx
+++ trustscore.near/MagicBuild-widget-form-1698985935208/source_code.jsx
@@ -1,5 +1,5 @@
 const user = context.accountId;
-const props = {"schema_version":"0.3.0","address":"chatme.near","metadata":{"name":"","version":"0.1.0","authors":[""]},"body":{"functions":[{"name":"init","kind":"call","label":"","button":"","className":"","classButton":"","export":true,"params":{"serialization_type":"json","args":[{"name":"owner_id","type_schema":{"type":"$ref"},"value":"chatme.near","className":"chatme.near8"}]},"deposit":1,"gas":30000000000000,"depositUnit":"near","selfInputDeposit":false,"gasUnit":"yoctoNEAR"}]}} 
+const props = {"schema_version":"0.3.0","address":"chatme.near","metadata":{"name":"","version":"0.1.0","authors":[""]},"body":{"functions":[{"name":"init","kind":"call","label":"","button":"","className":"","classButton":"","export":true,"params":{"serialization_type":"json","args":[{"name":"owner_id","type_schema":{"type":"$ref"},"value":"chatme.near","className":"chatme.near8"}]},"deposit":0,"gas":30000000000000,"depositUnit":"near","selfInputDeposit":true,"gasUnit":"yoctoNEAR"}]}} 
 
 return (
   <>
