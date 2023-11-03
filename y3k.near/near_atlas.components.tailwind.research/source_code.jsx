73713b25d82406bbd91e8c2b04d3ae748929df81
y3k.near <y3k.near>
2023-11-03 14:39:12 +0000

    Update near_atlas.components.tailwind.research by y3k.near at 2023-11-03T14:39:12.935Z

--- y3k.near/near_atlas.components.tailwind.research/source_code.jsx
+++ y3k.near/near_atlas.components.tailwind.research/source_code.jsx
@@ -21,6 +21,186 @@ return (
             </a>
           </div>
         </div>
+
+        <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+          <img
+            src="https://placehold.co/300x200/d1d4ff/352cb5.png"
+            alt="Placeholder Image"
+            className="w-full h-48 rounded-md object-cover"
+          />
+          <div className="px-1 py-4">
+            <div className="font-bold text-xl text-white mb-2">Blog Title</div>
+            <p className="text-gray-300 text-base">
+              This is a simple blog card example using Tailwind CSS. You can
+              replace this text with your own blog content.
+            </p>
+          </div>
+          <div className="px-1 py-4">
+            <a href="#" className="text-blue-400 hover:underline">
+              Read More
+            </a>
+          </div>
+        </div>
+
+        <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+          <img
+            src="https://placehold.co/300x200/d1d4ff/352cb5.png"
+            alt="Placeholder Image"
+            className="w-full h-48 rounded-md object-cover"
+          />
+          <div className="px-1 py-4">
+            <div className="font-bold text-xl text-white mb-2">Blog Title</div>
+            <p className="text-gray-300 text-base">
+              This is a simple blog card example using Tailwind CSS. You can
+              replace this text with your own blog content.
+            </p>
+          </div>
+          <div className="px-1 py-4">
+            <a href="#" className="text-blue-400 hover:underline">
+              Read More
+            </a>
+          </div>
+        </div>
+
+        <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+          <img
+            src="https://placehold.co/300x200/d1d4ff/352cb5.png"
+            alt="Placeholder Image"
+            className="w-full h-48 rounded-md object-cover"
+          />
+          <div className="px-1 py-4">
+            <div className="font-bold text-xl text-white mb-2">Blog Title</div>
+            <p className="text-gray-300 text-base">
+              This is a simple blog card example using Tailwind CSS. You can
+              replace this text with your own blog content.
+            </p>
+          </div>
+          <div className="px-1 py-4">
+            <a href="#" className="text-blue-400 hover:underline">
+              Read More
+            </a>
+          </div>
+        </div>
+
+        <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+          <img
+            src="https://placehold.co/300x200/d1d4ff/352cb5.png"
+            alt="Placeholder Image"
+            className="w-full h-48 rounded-md object-cover"
+          />
+          <div className="px-1 py-4">
+            <div className="font-bold text-xl text-white mb-2">Blog Title</div>
+            <p className="text-gray-300 text-base">
+              This is a simple blog card example using Tailwind CSS. You can
+              replace this text with your own blog content.
+            </p>
+          </div>
+          <div className="px-1 py-4">
+            <a href="#" className="text-blue-400 hover:underline">
+              Read More
+            </a>
+          </div>
+        </div>
+
+        <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+          <img
+            src="https://placehold.co/300x200/d1d4ff/352cb5.png"
+            alt="Placeholder Image"
+            className="w-full h-48 rounded-md object-cover"
+          />
+          <div className="px-1 py-4">
+            <div className="font-bold text-xl text-white mb-2">Blog Title</div>
+            <p className="text-gray-300 text-base">
+              This is a simple blog card example using Tailwind CSS. You can
+              replace this text with your own blog content.
+            </p>
+          </div>
+          <div className="px-1 py-4">
+            <a href="#" className="text-blue-400 hover:underline">
+              Read More
+            </a>
+          </div>
+        </div>
+
+        <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+          <img
+            src="https://placehold.co/300x200/d1d4ff/352cb5.png"
+            alt="Placeholder Image"
+            className="w-full h-48 rounded-md object-cover"
+          />
+          <div className="px-1 py-4">
+            <div className="font-bold text-xl text-white mb-2">Blog Title</div>
+            <p className="text-gray-300 text-base">
+              This is a simple blog card example using Tailwind CSS. You can
+              replace this text with your own blog content.
+            </p>
+          </div>
+          <div className="px-1 py-4">
+            <a href="#" className="text-blue-400 hover:underline">
+              Read More
+            </a>
+          </div>
+        </div>
+
+        <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+          <img
+            src="https://placehold.co/300x200/d1d4ff/352cb5.png"
+            alt="Placeholder Image"
+            className="w-full h-48 rounded-md object-cover"
+          />
+          <div className="px-1 py-4">
+            <div className="font-bold text-xl text-white mb-2">Blog Title</div>
+            <p className="text-gray-300 text-base">
+              This is a simple blog card example using Tailwind CSS. You can
+              replace this text with your own blog content.
+            </p>
+          </div>
+          <div className="px-1 py-4">
+            <a href="#" className="text-blue-400 hover:underline">
+              Read More
+            </a>
+          </div>
+        </div>
+
+        <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+          <img
+            src="https://placehold.co/300x200/d1d4ff/352cb5.png"
+            alt="Placeholder Image"
+            className="w-full h-48 rounded-md object-cover"
+          />
+          <div className="px-1 py-4">
+            <div className="font-bold text-xl text-white mb-2">Blog Title</div>
+            <p className="text-gray-300 text-base">
+              This is a simple blog card example using Tailwind CSS. You can
+              replace this text with your own blog content.
+            </p>
+          </div>
+          <div className="px-1 py-4">
+            <a href="#" className="text-blue-400 hover:underline">
+              Read More
+            </a>
+          </div>
+        </div>
+
+        <div className="bg-gray-800 rounded-lg border-gray-700 p-4">
+          <img
+            src="https://placehold.co/300x200/d1d4ff/352cb5.png"
+            alt="Placeholder Image"
+            className="w-full h-48 rounded-md object-cover"
+          />
+          <div className="px-1 py-4">
+            <div className="font-bold text-xl text-white mb-2">Blog Title</div>
+            <p className="text-gray-300 text-base">
+              This is a simple blog card example using Tailwind CSS. You can
+              replace this text with your own blog content.
+            </p>
+          </div>
+          <div className="px-1 py-4">
+            <a href="#" className="text-blue-400 hover:underline">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
