192ea06f5087e4f46a6d9d435e15a302b883cbe0
nearcon23.near <nearcon23.near>
2023-11-03 03:11:33 +0000

    Update Mobile.Home.Profile by nearcon23.near at 2023-11-03T03:11:33.738Z

--- nearcon23.near/Mobile.Home.Profile/source_code.jsx
+++ nearcon23.near/Mobile.Home.Profile/source_code.jsx
@@ -71,6 +71,18 @@ const AlertButton = styled.button`
     border-radius: 100px;
 `;
 
+const RedirectButton = styled.button`
+    background-color: #00ec97;
+    border-width: 0px;
+    color: #000000;
+    font-size: 16px;
+    font-weight: 600;
+    padding: 10px;
+    height: 48px;
+    width: 100%;
+    border-radius: 100px;
+`;
+
 const [loading, setLoading] = useState(false);
 const [notifications, setNotifications] = useState([]);
 const [showTime, setShowTime] = useState(10);
@@ -147,10 +159,12 @@ const storedSecretKey = Storage.get(
   : Storage.get("newPrivateKey", `${ownerId}/widget/RegisterMobile.Index`);
 
 const fetchData = () => {
+  setLoading(true);
   const key = secretkey ? secretkey : storedSecretKey;
-  asyncFetch(`${baseUrl}/accounts/auth/${key}`).then(({ body }) => {
+  asyncFetch(`${baseUrl}/api/v1/accounts/auth/${key}`).then(({ body }) => {
     console.log(body);
     State.update({ userData: body });
+    setLoading(false);
   });
 };
 
@@ -205,200 +219,272 @@ function convertToReadableDate(dateString) {
   return `${hours}:${formattedMinutes} ${ampm} at ${day} ${month}`;
 }
 
-return (
-  <Content>
+return loading ? (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 32,
+
+      position: "relative",
+
+      height: "calc(100% - 165px)",
+      width: "100%",
+      padding: "0 20px",
+    }}
+  >
+    {/*<Widget src={`${ownerId}/widget/Components.LoadingOverlay`} />*/}
     <div
-      style={{
-        flex: 1,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        margin: 20,
-        width: "calc(100% - 40px)",
-      }}
+      class="spinner-border"
+      style={{ color: "rgb(0, 236, 151)" }}
+      role="status"
     >
-      {notifications?.map((item, index) => (
-        <Alert key={index}>
-          <div style={{ display: "flex", alignItems: "center", gap: "5px" }}>
-            <i class="bi bi-clock" style={{ color: "#7269e1" }}></i>
-            <p style={{ marginBottom: 0, color: "#7269e1" }}>
-              {timeAgo(item?.createdAt || "")}
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+) : (
+  <>
+    {state.userData?._id ? (
+      <Content>
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            margin: 20,
+            width: "calc(100% - 40px)",
+          }}
+        >
+          {notifications?.map((item, index) => (
+            <Alert key={index}>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "5px" }}
+              >
+                <i class="bi bi-clock" style={{ color: "#7269e1" }}></i>
+                <p style={{ marginBottom: 0, color: "#7269e1" }}>
+                  {timeAgo(item?.createdAt || "")}
+                </p>
+              </div>
+              <p style={{ color: "#4d3bc2" }}>{item?.message}</p>
+
+              {item.showButton && (
+                <a href={item.url} target="_blank">
+                  <AlertButton>
+                    <i
+                      class="bi bi-map"
+                      style={{
+                        width: "14",
+                        height: "14",
+                        color: "#4d3bc2",
+                      }}
+                    ></i>
+                    {item.buttonLabel}
+                  </AlertButton>
+                </a>
+              )}
+            </Alert>
+          ))}
+
+          <div
+            style={{
+              border: `2px ${theme.borderColor} solid`,
+              borderRadius: 8,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+
+              aspectRatio: 1 / 1,
+            }}
+          >
+            <h1
+              style={{
+                fontSize: 64,
+                fontWeight: 500,
+                margin: 0,
+                letterSpacing: "-5px",
+              }}
+            >
+              {state?.userData?.balance?.replace?.(
+                "0000000000000000000000",
+                ""
+              )}
+            </h1>
+            <h3
+              style={{
+                fontSize: 30,
+                fontWeight: 500,
+                margin: 0,
+                color: "#00EC97",
+              }}
+            >
+              NCON
+            </h3>
+
+            <div
+              style={{
+                marginTop: 24,
+                width: 300,
+                marginBottom: 24,
+                display: "flex",
+                justifyContent: "space-around",
+              }}
+            >
+              <Button
+                href={`/${ownerId}/widget/Mobile.Home.SendAddress`}
+                style={{
+                  textDecoration: "none",
+                }}
+              >
+                <ButtonIcon>
+                  <Widget src={`${ownerId}/widget/Icons.SendMoney`} />
+                </ButtonIcon>
+
+                <h6 style={{ fontWeight: "400", color: "#000000" }}>Send</h6>
+              </Button>
+
+              <Button
+                href={`/${ownerId}/widget/Mobile.Home.Recieve`}
+                style={{
+                  textDecoration: "none",
+                }}
+              >
+                <ButtonIcon>
+                  <Widget src={`${ownerId}/widget/Icons.Receive`} />
+                </ButtonIcon>
+
+                <h6 style={{ fontWeight: "400", color: "#000000" }}>Receive</h6>
+              </Button>
+
+              <Link
+                href={`/${ownerId}/widget/Mobile.Home.Scan`}
+                style={{
+                  border: "none",
+                  backgroundColor: "transparent",
+                  color: "#000000",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  gap: 8,
+
+                  textDecoration: "none",
+                }}
+              >
+                <ButtonIcon>
+                  <Widget src={`${ownerId}/widget/Icons.Scan`} />
+                </ButtonIcon>
+
+                <h6 style={{ fontWeight: "400", color: "#000000" }}>Scan</h6>
+              </Link>
+            </div>
+
+            <p
+              style={{
+                textAlign: "center",
+                color: theme.textColor3,
+                padding: "0 20px",
+                maxWidth: 425,
+                fontSize: 12,
+                fontWeight: 400,
+              }}
+            >
+              All NCON expire at the end of the conference
+              <br /> and are worth no monetary value.
             </p>
           </div>
-          <p style={{ color: "#4d3bc2" }}>{item?.message}</p>
 
-          {item.showButton && (
-            <a href={item.url} target="_blank">
-              <AlertButton>
-                <i
-                  class="bi bi-map"
-                  style={{
-                    width: "14",
-                    height: "14",
-                    color: "#4d3bc2",
+          {(transactions ?? [])?.length !== 0 && (
+            <>
+              <div style={{ paddingTop: 10, marginTop: 40 }}>
+                <p style={{ color: "#868682", fontSize: 16 }}>
+                  Recent Transactions
+                </p>
+                {([...transactions.slice(0, 2)] ?? []).map((item) => (
+                  <Widget
+                    props={{
+                      sent: item.senderId === state.userData.nearconId,
+                      amount: item.amount,
+                      image:
+                        item.senderId === state.userData.nearconId
+                          ? item?.receiverCid
+                          : item.senderCid,
+                      accountId:
+                        item.senderId === state.userData.nearconId
+                          ? item.receiverId
+                          : item.senderId,
+                      time: convertToReadableDate(item.createdAt),
+                    }}
+                    src={`${ownerId}/widget/Components.TransactionCard`}
+                  />
+                ))}
+
+                <button
+                  onClick={() => {
+                    State.update({ redirectToTransactions: true });
                   }}
-                ></i>
-                {item.buttonLabel}
-              </AlertButton>
-            </a>
+                  style={{
+                    backgroundColor: "#E3E3E0",
+                    borderWidth: 0,
+                    borderRadius: 100,
+                    color: "#000",
+                    padding: 10,
+                    marginBottom: 20,
+                    fontWeight: "500",
+                    paddingLeft: 20,
+                    marginTop: 10,
+                    paddingRight: 20,
+                  }}
+                >
+                  Show More
+                </button>
+              </div>
+            </>
           )}
-        </Alert>
-      ))}
-
+        </div>
+      </Content>
+    ) : (
       <div
         style={{
-          border: `2px ${theme.borderColor} solid`,
-          borderRadius: 8,
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
+          gap: 32,
 
-          aspectRatio: 1 / 1,
+          height: "calc(100% - 165px)",
+          width: "100%",
+          padding: "0 20px",
         }}
       >
         <h1
           style={{
-            fontSize: 64,
-            fontWeight: 500,
-            margin: 0,
-            letterSpacing: "-5px",
-          }}
-        >
-          {state?.userData?.balance?.replace?.("0000000000000000000000", "")}
-        </h1>
-        <h3
-          style={{
-            fontSize: 30,
-            fontWeight: 500,
-            margin: 0,
-            color: "#00EC97",
-          }}
-        >
-          NCON
-        </h3>
-
-        <div
-          style={{
-            marginTop: 24,
-            width: 300,
-            marginBottom: 24,
-            display: "flex",
-            justifyContent: "space-around",
-          }}
-        >
-          <Button
-            href={`/${ownerId}/widget/Mobile.Home.SendAddress`}
-            style={{
-              textDecoration: "none",
-            }}
-          >
-            <ButtonIcon>
-              <Widget src={`${ownerId}/widget/Icons.SendMoney`} />
-            </ButtonIcon>
-
-            <h6 style={{ fontWeight: "400", color: "#000000" }}>Send</h6>
-          </Button>
-
-          <Button
-            href={`/${ownerId}/widget/Mobile.Home.Recieve`}
-            style={{
-              textDecoration: "none",
-            }}
-          >
-            <ButtonIcon>
-              <Widget src={`${ownerId}/widget/Icons.Receive`} />
-            </ButtonIcon>
-
-            <h6 style={{ fontWeight: "400", color: "#000000" }}>Receive</h6>
-          </Button>
-
-          <Link
-            href={`/${ownerId}/widget/Mobile.Home.Scan`}
-            style={{
-              border: "none",
-              backgroundColor: "transparent",
-              color: "#000000",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-              gap: 8,
-
-              textDecoration: "none",
-            }}
-          >
-            <ButtonIcon>
-              <Widget src={`${ownerId}/widget/Icons.Scan`} />
-            </ButtonIcon>
-
-            <h6 style={{ fontWeight: "400", color: "#000000" }}>Scan</h6>
-          </Link>
-        </div>
-
-        <p
-          style={{
             textAlign: "center",
-            color: theme.textColor3,
-            padding: "0 20px",
-            maxWidth: 425,
-            fontSize: 12,
-            fontWeight: 400,
+
+            fontFamily: "FK Grotesk",
+            fontSize: 32,
+            fontStyle: "normal",
+            fontWeight: 500,
+            lineHeight: "130%",
           }}
         >
-          All NCON expire at the end of the conference
-          <br /> and are worth no monetary value.
-        </p>
+          Authenticatetion error
+        </h1>
+        <br /> Please check your ticket link.
+        <Link to="/" style={{ width: "100%" }}>
+          <RedirectButton
+            style={{
+              width: "100%",
+            }}
+          >
+            Home
+          </RedirectButton>
+        </Link>
       </div>
-
-      {(transactions ?? [])?.length !== 0 && (
-        <>
-          <div style={{ paddingTop: 10, marginTop: 40 }}>
-            <p style={{ color: "#868682", fontSize: 16 }}>
-              Recent Transactions
-            </p>
-            {([...transactions.slice(0, 2)] ?? []).map((item) => (
-              <Widget
-                props={{
-                  sent: item.senderId === state.userData.nearconId,
-                  amount: item.amount,
-                  image:
-                    item.senderId === state.userData.nearconId
-                      ? item?.receiverCid
-                      : item.senderCid,
-                  accountId:
-                    item.senderId === state.userData.nearconId
-                      ? item.receiverId
-                      : item.senderId,
-                  time: convertToReadableDate(item.createdAt),
-                }}
-                src={`${ownerId}/widget/Components.TransactionCard`}
-              />
-            ))}
-
-            <button
-              onClick={() => {
-                State.update({ redirectToTransactions: true });
-              }}
-              style={{
-                backgroundColor: "#E3E3E0",
-                borderWidth: 0,
-                borderRadius: 100,
-                color: "#000",
-                padding: 10,
-                marginBottom: 20,
-                fontWeight: "500",
-                paddingLeft: 20,
-                marginTop: 10,
-                paddingRight: 20,
-              }}
-            >
-              Show More
-            </button>
-          </div>
-        </>
-      )}
-    </div>
-  </Content>
+    )}
+  </>
 );
