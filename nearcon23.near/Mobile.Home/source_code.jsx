cfb51d578ef192ae6204921d83512f5ff851094e
nearcon23.near <nearcon23.near>
2023-11-03 04:04:00 +0000

    Update Mobile.Home by nearcon23.near at 2023-11-03T04:04:00.409Z

--- nearcon23.near/Mobile.Home/source_code.jsx
+++ nearcon23.near/Mobile.Home/source_code.jsx
@@ -35,6 +35,7 @@ const fetchData = () => {
   const key = secretkey ? secretkey : storedSecretKey;
 
   asyncFetch(`${apiUrl}/accounts/auth/${key}`).then(({ body }) => {
+    console.log("===> ", body);
     if (body?._id) {
       // if (!!storedSecretKey === false) { }
       State.update({
@@ -53,6 +54,7 @@ const fetchTransaction = () => {
   if (state?.userData?.nearconId) {
     const apiURL = `${apiUrl}/transactions/${state.userData.nearconId}`;
     asyncFetch(apiURL).then(({ body }) => {
+      console.log("====. ", body);
       State.update({ transactions: body });
     });
   }
