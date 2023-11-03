aee7f57ae1064597f526ca12530e91b572b3f2b4
andyh.near <andyh.near>
2023-11-03 19:22:33 +0000

    Update NestedProps.Parent by andyh.near at 2023-11-03T19:22:33.709Z

--- andyh.near/NestedProps.Parent/source_code.jsx
+++ andyh.near/NestedProps.Parent/source_code.jsx
@@ -10,7 +10,7 @@ return (
           fn();
           setValue((v) => v + 1);
         },
-        arr: ["oh", "hi", (n) => "there, " + n.toString],
+        arr: ["oh", "hi", (n) => "there, " + n?.toString()],
         byTen: {
           increment: (fn) => {
             fn(value);
