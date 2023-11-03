dcbe3d7d5f6f6f52b2b323035031669a369c16aa
humans-of-near.near <humans-of-near.near>
2023-11-03 08:46:22 +0000

    Update Human-sidebar by humans-of-near.near at 2023-11-03T08:46:22.515Z

--- humans-of-near.near/Human-sidebar/source_code.jsx
+++ humans-of-near.near/Human-sidebar/source_code.jsx
@@ -9,6 +9,16 @@ const Sidebar = styled.div`
   height: 100%; 
   padding-top:32px;
   padding-bottom:32px;
+
+  @media (max-width: 510px) {
+    .menu:{
+      display: none;
+    }
+
+    .btn-menu:{
+      display: block;
+    }
+  }
 `;
 
 const SidebarContent = styled.div`
@@ -27,7 +37,7 @@ const SidebarContent = styled.div`
 
 return (
   <Sidebar>
-    <SidebarContent>
+    <SidebarContent className="menu">
       <div className="d-flex flex-column" style={{ gap: 40 }}>
         <div
           style={{
@@ -116,5 +126,41 @@ return (
         <p>{humans}</p>
       </div>
     </SidebarContent>
+
+    <button
+      className="btn p-0 btn-menu"
+      onClick={showProfile}
+      style={{ width: "fit-content", height: "fit-content", display: "none" }}
+    >
+      <svg
+        width="30"
+        height="30"
+        viewBox="0 0 30 30"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M26.35 10.725V19.275C26.35 20.675 25.6 21.9751 24.3875 22.6876L16.9625 26.975C15.75 27.675 14.25 27.675 13.025 26.975L5.6 22.6876C4.3875 21.9876 3.6375 20.6875 3.6375 19.275V10.725C3.6375 9.32504 4.3875 8.02499 5.6 7.31249L13.025 3.025C14.2375 2.325 15.7375 2.325 16.9625 3.025L24.3875 7.31249C25.6 8.02499 26.35 9.31254 26.35 10.725Z"
+          stroke="white"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M15 13.75C16.6085 13.75 17.9125 12.446 17.9125 10.8375C17.9125 9.22896 16.6085 7.92505 15 7.92505C13.3915 7.92505 12.0875 9.22896 12.0875 10.8375C12.0875 12.446 13.3915 13.75 15 13.75Z"
+          stroke="white"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M20 20.8249C20 18.5749 17.7625 16.75 15 16.75C12.2375 16.75 10 18.5749 10 20.8249"
+          stroke="white"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </button>
   </Sidebar>
 );
