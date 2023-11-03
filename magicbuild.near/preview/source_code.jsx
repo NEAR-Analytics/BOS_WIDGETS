3de7f38314c551be3b39891b8bc785f9e62d304d
magicbuild.near <magicbuild.near>
2023-11-03 06:07:37 +0000

    Update preview by magicbuild.near at 2023-11-03T06:07:37.161Z

--- magicbuild.near/preview/source_code.jsx
+++ magicbuild.near/preview/source_code.jsx
@@ -168,7 +168,7 @@ console.log("state", state.cssStyle);
 console.log("props", props.cssStyle);
 return (
   <>
-    <Wrapper class="container">
+    <Wrapper class="container" style={props.cssStyle}>
       {context.accountId ? contractForm : notLoggedInWarning}
       <h3 class="text-center">{state.contractAddress}</h3>
       {state.contractError}
