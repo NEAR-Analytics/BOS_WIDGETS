88ebee55ee9ade2220fa3643773954b6db6078f9
magicbuild.near <magicbuild.near>
2023-11-03 16:37:28 +0000

    Update export-button by magicbuild.near at 2023-11-03T16:37:28.963Z

--- magicbuild.near/export-button/source_code.jsx
+++ magicbuild.near/export-button/source_code.jsx
@@ -46,27 +46,29 @@ const filesOnChange = (files) => {
     uploadFileUpdateState(files[0]);
   }
 };
-const taggedWidgets = Social.keys(`*/widget/*/metadata/tags/*`, "final");
-
-let tags = [];
-Object.keys(taggedWidgets).forEach((item) => {
-  if (Object.keys(taggedWidgets[item].widget).length > 0) {
-    Object.keys(taggedWidgets[item].widget).forEach((item1) => {
-      if (
-        Object.keys(taggedWidgets[item].widget[item1].metadata.tags.length > 0)
-      ) {
-        Object.keys(taggedWidgets[item].widget[item1].metadata.tags).forEach(
-          (tag) => {
-            tags.push(tag);
-          }
-        );
-      }
-    });
-  }
-});
-State.update({ tags: tags });
 
 const openModal = () => {
+  const taggedWidgets = Social.keys(`*/widget/*/metadata/tags/*`, "final");
+
+  let tags = [];
+  Object.keys(taggedWidgets).forEach((item) => {
+    if (Object.keys(taggedWidgets[item].widget).length > 0) {
+      Object.keys(taggedWidgets[item].widget).forEach((item1) => {
+        if (
+          Object.keys(
+            taggedWidgets[item].widget[item1].metadata.tags.length > 0
+          )
+        ) {
+          Object.keys(taggedWidgets[item].widget[item1].metadata.tags).forEach(
+            (tag) => {
+              tags.push(tag);
+            }
+          );
+        }
+      });
+    }
+  });
+  State.update({ tags: tags });
   State.update({ clicked: false });
   State.update({ export: false });
 };
