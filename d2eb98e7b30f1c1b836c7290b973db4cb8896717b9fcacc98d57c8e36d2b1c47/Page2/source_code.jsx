5e41ba77a475332071cfa9c248512e0ed8263c2c
d2eb98e7b30f1c1b836c7290b973db4cb8896717b9fcacc98d57c8e36d2b1c47 <d2eb98e7b30f1c1b836c7290b973db4cb8896717b9fcacc98d57c8e36d2b1c47>
2023-11-04 16:04:21 +0000

    Update Page2 by d2eb98e7b30f1c1b836c7290b973db4cb8896717b9fcacc98d57c8e36d2b1c47 at 2023-11-04T16:04:21.623Z

---
+++ d2eb98e7b30f1c1b836c7290b973db4cb8896717b9fcacc98d57c8e36d2b1c47/Page2/source_code.jsx
@@ -0,0 +1,117 @@
+const StyledDesktop = styled.div` position: relative; background-color: #182269; display: flex; flex-direction: row; justify-content: center; width: 100%; & .div { background-color: #182269; height: 1024px; overflow: hidden; position: relative; width: 1440px; } & .overlap { height: 917px; left: 188px; position: absolute; top: 407px; width: 1743px; } & .overlap-group { background-color: #89bcdd; border-radius: 24px; height: 247px; left: 165px; position: absolute; top: 0; width: 285px; } & .insurance-place { height: 16px; left: 107px; position: absolute; top: 15px; width: 72px; } & .group { height: 53px; left: 15px; position: absolute; top: 110px; width: 257px; } & .div-wrapper { background-color: #13124f; border-radius: 70px; height: 53px; position: relative; width: 255px; } & .text-wrapper { color: #ffffff; font-family: "Roboto-Regular", Helvetica; font-size: 16px; font-weight: 400; left: 0; letter-spacing: 0; line-height: 31px; opacity: 0.67; position: absolute; text-align: center; top: 11px; width: 255px; } & .overlap-wrapper { height: 53px; left: 15px; position: absolute; top: 47px; width: 257px; } & .overlap-group-wrapper { height: 53px; left: 15px; position: absolute; top: 173px; width: 257px; } & .text-wrapper-2 { color: #ffffff; font-family: "Roboto-Regular", Helvetica; font-size: 16px; font-weight: 400; left: 53px; letter-spacing: 0; line-height: 31px; opacity: 0.67; position: absolute; text-align: center; top: 11px; width: 149px; } & .overlap-2 { height: 678px; left: 0; position: absolute; top: 239px; width: 1743px; } & .bottom { height: 140px; left: 0; position: absolute; top: 217px; width: 1081px; } & .overlap-3 { height: 20px; left: 2px; position: absolute; top: 8px; width: 291px; } & .img { height: 16px; left: 0; position: absolute; top: 0; width: 189px; } & .text { color: #ffffff; font-family: "Outfit-SemiBold", Helvetica; font-size: 16px; font-weight: 600; left: 178px; letter-spacing: 0; line-height: normal; position: absolute; top: 0; width: 113px; } & .text-wrapper-3 { color: #ffffff; font-family: "Outfit-SemiBold", Helvetica; font-size: 22px; font-weight: 600; left: 729px; letter-spacing: 0; line-height: normal; position: absolute; top: 0; width: 293px; } & .home-about { color: #ffffff; font-family: "Roboto-Regular", Helvetica; font-size: 16px; font-weight: 400; left: 180px; letter-spacing: 0; line-height: 31px; position: absolute; top: 47px; width: 69px; } & .text-wrapper-4 { color: #ffffff; font-family: "Roboto-Regular", Helvetica; font-size: 16px; font-weight: 400; left: 360px; letter-spacing: 0; line-height: 31px; position: absolute; top: 47px; width: 97px; } & .email-linkedin { color: #ffffff; font-family: "Roboto-Regular", Helvetica; font-size: 16px; font-weight: 400; left: 534px; letter-spacing: 0; line-height: 31px; position: absolute; top: 47px; width: 74px; } & .text-wrapper-5 { color: #ffffff; font-family: "Outfit-SemiBold", Helvetica; font-size: 16px; font-weight: 600; left: 360px; letter-spacing: 0; line-height: normal; position: absolute; top: 8px; width: 123px; } & .text-wrapper-6 { color: #ffffff; font-family: "Outfit-SemiBold", Helvetica; font-size: 16px; font-weight: 600; left: 534px; letter-spacing: 0; line-height: normal; position: absolute; top: 8px; width: 128px; } & .group-2 { height: 53px; left: 720px; position: absolute; top: 58px; width: 347px; } & .overlap-group-2 { background-color: #13124f; border-radius: 70px; height: 53px; position: relative; width: 345px; } & .text-wrapper-7 { color: #ffffff; font-family: "Roboto-Regular", Helvetica; font-size: 16px; font-weight: 400; left: 23px; letter-spacing: 0; line-height: 31px; opacity: 0.67; position: absolute; top: 12px; width: 113px; } & .frame { align-items: center; background-color: #8d8dda; border-radius: 83px; display: flex; gap: 10px; height: 43px; justify-content: center; left: 217px; padding: 22px 71px; position: absolute; top: 5px; width: 124px; } & .submit { color: #ffffff; font-family: "Outfit-SemiBold", Helvetica; font-size: 14px; font-weight: 600; letter-spacing: 1.4px; line-height: 27.5px; margin-bottom: -13.5px; margin-left: -38.5px; margin-right: -38.5px; margin-top: -15.5px; position: relative; white-space: nowrap; width: fit-content; } & .ellipse { background-color: #9cd7d8cc; border-radius: 339px; filter: blur(309px); height: 678px; left: 1065px; position: absolute; top: 0; width: 678px; } & .subscribe-insurance-wrapper { align-items: center; background-color: #8d8dda; border-radius: 83px; display: flex; gap: 10px; justify-content: center; left: 180px; padding: 22px 68px; position: absolute; top: 344px; width: 345px; } & .subscribe-insurance { color: #ffffff; font-family: var(--SUB-HEADER-font-family); font-size: var(--SUB-HEADER-font-size); font-style: var(--SUB-HEADER-font-style); font-weight: var(--SUB-HEADER-font-weight); letter-spacing: var(--SUB-HEADER-letter-spacing); line-height: var(--SUB-HEADER-line-height); margin-left: -24.5px; margin-right: -24.5px; margin-top: -1px; position: relative; white-space: nowrap; width: fit-content; } & .change-insurance-wrapper { align-items: center; background-color: #8d8dda; border-radius: 83px; display: flex; gap: 10px; justify-content: center; left: 540px; padding: 22px 68px; position: absolute; top: 344px; width: 345px; } & .change-insurance { color: #ffffff; font-family: var(--SUB-HEADER-font-family); font-size: var(--SUB-HEADER-font-size); font-style: var(--SUB-HEADER-font-style); font-weight: var(--SUB-HEADER-font-weight); letter-spacing: var(--SUB-HEADER-letter-spacing); line-height: var(--SUB-HEADER-line-height); margin-left: -11px; margin-right: -11px; margin-top: -1px; position: relative; white-space: nowrap; width: fit-content; } & .overlap-4 { height: 906px; left: 188px; position: absolute; top: -513px; width: 1057px; } & .overlap-5 { height: 906px; left: 0; position: absolute; top: 0; width: 1057px; } & .nav-bar { height: 28px; left: 5px; position: absolute; top: 560px; width: 870px; } & .insurance-place-2 { color: #ffffff; font-family: "Outfit-Bold", Helvetica; font-size: 20px; font-weight: 700; left: 0; letter-spacing: 0; line-height: 27.5px; position: absolute; top: 0; white-space: nowrap; } & .about { color: #ffffff; font-family: "Outfit-Bold", Helvetica; font-size: 17px; font-weight: 700; left: 693px; letter-spacing: 0; line-height: 27.5px; position: absolute; top: 0; white-space: nowrap; } & .contact { color: #ffffff; font-family: "Outfit-Bold", Helvetica; font-size: 17px; font-weight: 700; left: 783px; letter-spacing: 0; line-height: 27.5px; position: absolute; top: 0; white-space: nowrap; } & .your-insurance { color: #ffffff; font-family: var(--h2-font-family); font-size: var(--h2-font-size); font-style: var(--h2-font-style); font-weight: var(--h2-font-weight); left: 90px; letter-spacing: var(--h2-letter-spacing); line-height: var(--h2-line-height); position: absolute; text-align: center; top: 659px; white-space: nowrap; width: 435px; } & .ellipse-2 { background-color: #8e8edacc; border-radius: 339px; filter: blur(309px); height: 678px; left: 0; position: absolute; top: 0; width: 678px; } & .object-other-06 { background-image: url(./other-06-2.png) !important; height: 317px !important; left: 630px !important; position: absolute !important; top: 589px !important; width: 317px !important; } & .frame-2 { align-items: center; background-color: #2d2779; border-radius: 60px; display: inline-flex; gap: 14px; justify-content: center; left: 900px; padding: 10px 25px; position: absolute; top: 551px; } & .ellipse-3 { background-color: #9091dc; border-radius: 8.5px; height: 17px; position: relative; width: 17px; } & .e-hamon { color: #ffffff; font-family: "Outfit-SemiBold", Helvetica; font-size: 16px; font-weight: 600; letter-spacing: 0; line-height: 27.5px; margin-top: -1px; position: relative; white-space: nowrap; width: fit-content; } & .p { color: #ffffff; font-family: var(--body-text-font-family); font-size: var(--body-text-font-size); font-style: var(--body-text-font-style); font-weight: var(--body-text-font-weight); left: 90px; letter-spacing: var(--body-text-letter-spacing); line-height: var(--body-text-line-height); position: absolute; top: 756px; width: 373px; } `;
+
+const Img = styled.img`
+position: absolute;
+
+right:10%;
+top:20%;
+`;
+
+return (
+  <StyledDesktop>
+    {" "}
+    <div className="div">
+      <Img
+        src="https://github.com/Ewuly/Image/blob/main/Other%2021.png?raw=true"
+        width="400"
+        border-top="100px"
+      ></Img>{" "}
+      <div className="overlap">
+        {" "}
+        <div className="overlap-group">
+          {" "}
+          <img
+            className="insurance-place"
+            alt="Insurance place"
+            src="insurance-place.png"
+          />{" "}
+          <div className="group">
+            {" "}
+            <div className="div-wrapper">
+              {" "}
+              <p className="text-wrapper">Type of Property : House</p>{" "}
+            </div>{" "}
+          </div>{" "}
+          <div className="overlap-wrapper">
+            {" "}
+            <div className="div-wrapper">
+              {" "}
+              <div className="text-wrapper">Price : 108€</div>{" "}
+            </div>{" "}
+          </div>{" "}
+          <div className="overlap-group-wrapper">
+            {" "}
+            <div className="div-wrapper">
+              {" "}
+              <div className="text-wrapper-2">Insurance : Axa</div>{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+        <div className="overlap-2">
+          {" "}
+          <div className="bottom">
+            {" "}
+            <div className="overlap-3">
+              {" "}
+              <img className="img" alt="Insurance place" src="image.png" />{" "}
+              <div className="text">{""}</div>{" "}
+            </div>{" "}
+            <div className="text-wrapper-3">Join our newsletter</div>{" "}
+            <div className="home-about">
+              {" "}
+              Home <br /> About{" "}
+            </div>{" "}
+            <div className="text-wrapper-4">
+              Discover different insurance
+            </div>{" "}
+            <div className="email-linkedin">
+              {" "}
+              Email <br /> LinkedIn <br /> Twitter{" "}
+            </div>{" "}
+            <div className="text-wrapper-5">Insurance</div>{" "}
+            <div className="text-wrapper-6">Contact</div>{" "}
+            <div className="group-2">
+              {" "}
+              <div className="overlap-group-2">
+                {" "}
+                <div className="text-wrapper-7">Email Address</div>{" "}
+                <div className="frame">
+                  {" "}
+                  <div className="submit">SUBMIT</div>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
+          </div>{" "}
+          <div className="ellipse" />{" "}
+        </div>{" "}
+        <div className="subscribe-insurance-wrapper">
+          {" "}
+          <div className="subscribe-insurance">SUBSCRIBE INSURANCE</div>{" "}
+        </div>{" "}
+        <div className="change-insurance-wrapper">
+          {" "}
+          <div className="change-insurance">CHANGE INSURANCE</div>{" "}
+        </div>{" "}
+      </div>{" "}
+      <div className="overlap-4">
+        {" "}
+        <div className="overlap-5">
+          {" "}
+          <div className="nav-bar">
+            {" "}
+            <div className="insurance-place-2">INSURANCE PLACE</div>{" "}
+            <div className="about">ABOUT</div>{" "}
+            <div className="contact">CONTACT</div>{" "}
+          </div>{" "}
+          <div className="your-insurance">Your Insurance</div>{" "}
+          <div className="ellipse-2" />{" "}
+          <div className="frame-2">
+            {" "}
+            <div className="ellipse-3" /> <div className="e-hamon">E.HAMON</div>{" "}
+          </div>{" "}
+        </div>{" "}
+        <p className="p">Find hear all the insurances you have subscribe.</p>{" "}
+      </div>{" "}
+    </div>{" "}
+  </StyledDesktop>
+);
