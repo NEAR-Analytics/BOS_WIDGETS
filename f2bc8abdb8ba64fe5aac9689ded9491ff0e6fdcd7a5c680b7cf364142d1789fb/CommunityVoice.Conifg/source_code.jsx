7609a7ac8702d2aeb8eecd38c3376692f9559d12
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-11-04 22:38:02 +0000

    Update CommunityVoice.Conifg by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-11-04T22:38:02.877Z

---
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/CommunityVoice.Conifg/source_code.jsx
@@ -0,0 +1,96 @@
+let { sharedBlockHeight, tagShared, isTest, accountId } = props;
+
+const sbtWhiteList = [
+  "fractal.i-am-human.near - class 1",
+  "community.i-am-human.near - class 1",
+  "community.i-am-human.near - class 2",
+  "community.i-am-human.near - class 3",
+  "public",
+];
+
+const componentsOwner =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+
+// const authorForWidget = "communityvoice.ndctools.near";
+const authorForWidget =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+// const authorForWidget = "kenrou-it.near";
+// const authorForWidget = "silkking.near";
+
+const configWidget = "CommunityVoice.Config";
+
+const widgets = {
+  thisForum: `${authorForWidget}/widget/${configWidget}`,
+  ndcForum: `${componentsOwner}/widget/NDC.Forum`,
+  create: `${componentsOwner}/widget/NDC.Forum.Create`,
+  header: `${componentsOwner}/widget/NDC.NavBar`,
+  showArticlesList: `${componentsOwner}/widget/NDC.Forum.AllArticlesList`,
+  showArticlesListSortedByAuthors: `${componentsOwner}/widget/NDC.AllArticlesSortByAuthors`,
+  articlesByAuthorCard: `${componentsOwner}/widget/NDC.ArticlesByAuthorCard`,
+  generalCard: `${componentsOwner}/widget/NDC.GeneralCard`,
+  articleView: `${componentsOwner}/widget/NDC.ArticleView`,
+  reactions: `${componentsOwner}/widget/NDC.Reactions`,
+  addComment: `${componentsOwner}/widget/NDC.AddComment`,
+  commentView: `${componentsOwner}/widget/NDC.CommentView`,
+  upVoteButton: `${componentsOwner}/widget/NDC.UpVoteButton`,
+  libSBT: `sayalot.near/widget/lib.SBT`,
+  libComment: `sayalot.near/widget/lib.comment`,
+  libArticle: `sayalot.near/widget/lib.article`,
+  libEmojis: `sayalot.near/widget/lib.emojis`,
+  libUpVotes: `sayalot.near/widget/lib.upVotes`,
+  styledComponents: "rubycop.near/widget/NDC.StyledComponents",
+  newStyledComponents: {
+    Element: {
+      Badge: "nearui.near/widget/Element.Badge",
+      User: "nearui.near/widget/Element.User",
+    },
+    Feedback: {
+      Spinner: "nearui.near/widget/Feedback.Spinner",
+    },
+    Input: {
+      Button: "nearui.near/widget/Input.Button",
+      Checkbox: "nearui.near/widget/Input.Checkbox",
+      Select: "nearui.near/widget/Input.Select",
+    },
+  },
+  fasterTextInput: `${componentsOwner}/widget/fasterTextInput`,
+  markownEditorIframe: `${componentsOwner}/widget/MarkdownEditorIframe`,
+  tagsEditor: `${componentsOwner}/widget/TagsEditor`,
+  socialMarkdown: "mob.near/widget/SocialMarkdown",
+  profileOverlayTrigger: "mob.near/widget/Profile.OverlayTrigger",
+  profileImage: "mob.near/widget/ProfileImage",
+  wikiOnSocialDB_TooltipProfiles: `testwiki.near/widget/WikiOnSocialDB_TooltipProfiles`,
+  profileShortInlineBlock: `${componentsOwner}/widget/Profile.ShortInlineBlock`,
+};
+
+const brand = {
+  brandName: "Community Voice",
+  logoHref:
+    "https://ipfs.near.social/ipfs/bafkreifhkslni6dlocxya35vjft3fefk2am5uzkagmjjzobdjqlhrnbjz4",
+  logoRemWidth: 12,
+  logoRemHeight: 4,
+};
+
+const baseActions = {
+  commentBaseAction: "communityVoiceComment",
+  articlesBaseAction: "communityVoiceArticle",
+  upVoteBaseAction: "communityVoiceUpVote",
+  reactionBaseAction: "communityVoiceReaction",
+};
+
+return (
+  <Widget
+    src={widgets.ndcForum}
+    props={{
+      sharedBlockHeight,
+      tagShared,
+      isTest,
+      accountId,
+      sbtWhiteList,
+      authorForWidget,
+      widgets,
+      brand,
+      baseActions,
+    }}
+  />
+);
