76db3a8f533738c284acd93fe4204501e1d0c141
e83a03613058827b81abbdf1c30badccf6b247160c46faeb18e3ec7629f98fb0 <e83a03613058827b81abbdf1c30badccf6b247160c46faeb18e3ec7629f98fb0>
2023-11-04 16:52:06 +0000

    Update News by e83a03613058827b81abbdf1c30badccf6b247160c46faeb18e3ec7629f98fb0 at 2023-11-04T16:52:06.885Z

---
+++ e83a03613058827b81abbdf1c30badccf6b247160c46faeb18e3ec7629f98fb0/News/source_code.jsx
@@ -0,0 +1,62 @@
+const Wrapper = styled.div`
+  .SwitchRoot {
+    /* Style for the main container */
+.news-container {
+  background-color: #f2f2f2;
+  padding: 200px;
+  border-radius: 5px;
+}
+
+/* Style for the heading */
+.news-container h1 {
+  color: #333;
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+/* Style for the news items */
+.news-item {
+  background-color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
+
+/* Style for the news item headings */
+.news-item h4 {
+  color: #0066cc;
+  font-size: 18px;
+  margin-bottom: 5px;
+}
+
+/* Style for the news item description */
+.news-item p {
+  color: #333;
+  font-size: 14px;
+  margin-bottom: 0;
+}
+}
+  .SwitchThumb {
+    }
+`;
+return (
+  <Wrapper>
+    <Switch.Root className="SwitchRoot">
+      <div class="news-container">
+        <h1>Crypto-Asset News:</h1>
+
+        <div class="news-item">
+          <h4>Headline 1</h4>
+          <p>Description of the news article.</p>
+        </div>
+
+        <div class="news-item">
+          <h4>Headline 2</h4>
+          <p>Description of the news article.</p>
+        </div>
+      </div>
+      <Switch.Thumb className="SwitchThumb" />
+    </Switch.Root>
+      
+  </Wrapper>
+);
