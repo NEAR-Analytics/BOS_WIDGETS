0638e17b841bd484a311cb3cc92af99a0f0b1e81
mob.near <mob.near>
2023-11-05 16:51:18 +0000

    Update Neddit.Post.Page by mob.near at 2023-11-05T16:51:18.523Z

--- mob.near/Neddit.Post.Page/source_code.jsx
+++ mob.near/Neddit.Post.Page/source_code.jsx
@@ -1,12 +1,19 @@
+const blockHeight = parseInt(props.blockHeight);
+const accountId = props.accountId;
+const notifyAccountId = accountId;
+const item = {
+  type: "social",
+  path: `${accountId}/post/main`,
+  blockHeight,
+};
+
 return (
   <div style={{ marginTop: "calc(-1 * var(--body-top-padding, 0))" }}>
     <Widget
-      src="mob.near/widget/MainPage.N.Post"
+      src="mob.near/widget/Neddit.Post"
       props={{
         ...props,
         noBorder: true,
-        commentsLimit: 30,
-        subscribe: true,
         truncateContent: false,
       }}
     />
