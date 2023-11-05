09cc7f5ebf065195e678655286410cc9d249e753
35e3f7be109c2eb05164b3fe483f6e34873745217426ef65c8d51e3b613220e3 <35e3f7be109c2eb05164b3fe483f6e34873745217426ef65c8d51e3b613220e3>
2023-11-05 00:05:03 +0000

    Update News by 35e3f7be109c2eb05164b3fe483f6e34873745217426ef65c8d51e3b613220e3 at 2023-11-05T00:05:03.879Z

--- 35e3f7be109c2eb05164b3fe483f6e34873745217426ef65c8d51e3b613220e3/News/source_code.jsx
+++ 35e3f7be109c2eb05164b3fe483f6e34873745217426ef65c8d51e3b613220e3/News/source_code.jsx
@@ -17,7 +17,7 @@ const Wrapper = styled.div`
 /* Style for the news items */
 .news-item {
   background-color: #fff;
-  padding: 10px;
+  padding: 60px;
   border-radius: 5px;
   margin-bottom: 10px;
 }
@@ -33,7 +33,7 @@ const Wrapper = styled.div`
 .news-item p {
   color: #333;
   font-size: 14px;
-  margin-bottom: 0;
+  margin-bottom: 10;
 }
 }
   .SwitchThumb {
@@ -46,14 +46,26 @@ return (
         <h1>Crypto-Asset News:</h1>
 
         <div class="news-item">
-          <h4>Headline 1</h4>
-          <p>Description of the news article.</p>
+          <h4>News Websites</h4>
+          <p>
+            Popular technology news websites like CoinDesk, Cointelegraph, and
+            The Block regularly cover the latest developments in blockchain
+            technology, including news about cryptocurrencies, blockchain
+            projects, and regulatory changes.
+          </p>
         </div>
 
         <div class="news-item">
-          <h4>Headline 2</h4>
-          <p>Description of the news article.</p>
+          <h4>Cryptocurrency Forums</h4>
+          <p>
+            Websites like Reddit's r/cryptocurrency and Bitcointalk.org have
+            active communities discussing and sharing news related to blockchain
+            and cryptocurrencies.{" "}
+          </p>
         </div>
+        <a href="https://jutsu.ai/35e3f7be109c2eb05164b3fe483f6e34873745217426ef65c8d51e3b613220e3/widget/intro">
+          <button> main page</button>
+        </a>
       </div>
       <Switch.Thumb className="SwitchThumb" />
     </Switch.Root>
