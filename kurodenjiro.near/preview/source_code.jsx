2888b4a3bf0719b32aceacb267cdeb8f27657dd9
kurodenjiro.near <kurodenjiro.near>
2023-11-06 13:53:54 +0000

    Update preview by kurodenjiro.near at 2023-11-06T13:53:54.835Z

--- kurodenjiro.near/preview/source_code.jsx
+++ kurodenjiro.near/preview/source_code.jsx
@@ -7,7 +7,7 @@ State.init({
   response,
   contractAbiArg: props.cMethod,
 });
-
+console.log("hello");
 const onInputChangeContractArg = (obj) => {
   const data = state.contractAbiArg;
   const isExist = false;
