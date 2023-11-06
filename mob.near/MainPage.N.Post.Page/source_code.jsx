86dbce17e78492fde836ca518cba0375745ecc57
mob.near <mob.near>
2023-11-06 15:12:19 +0000

    Update MainPage.N.Post.Page by mob.near at 2023-11-06T15:12:19.904Z

--- mob.near/MainPage.N.Post.Page/source_code.jsx
+++ mob.near/MainPage.N.Post.Page/source_code.jsx
@@ -1,4 +1,17 @@
-return (
+const accountId = props.accountId;
+if (!accountId) {
+  return "No accountId";
+}
+const blockHeight =
+  props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
+
+const content = JSON.parse(
+  Social.get(`${accountId}/post/main`, blockHeight) ?? "null"
+);
+
+return content?.subneddit ? (
+  <Widget src="mob.near/widget/Neddit.Post.Page" props={props} />
+) : (
   <div style={{ marginTop: "calc(-1 * var(--body-top-padding, 0))" }}>
     <Widget
       src="mob.near/widget/MainPage.N.Post"
