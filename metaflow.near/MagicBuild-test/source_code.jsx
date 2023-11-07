61dee047cf83b9bfd64d1450cc7411be55e03d45
metaflow.near <metaflow.near>
2023-11-07 21:41:05 +0000

    Update MagicBuild-test by metaflow.near at 2023-11-07T21:41:05.915Z

--- metaflow.near/MagicBuild-test/source_code.jsx
+++ metaflow.near/MagicBuild-test/source_code.jsx
@@ -1,5 +1,5 @@
 const user = context.accountId;
-const props = {"schema_version":"0.3.0","address":"linear-protocol.near","metadata":{"name":"","version":"0.1.0","authors":[""]},"body":{"functions":[{"name":"deposit_and_stake","kind":"call","export":true,"params":{"serialization_type":"json","args":[{"name":"account_id","type_schema":{"type":"string"},"value":""},{"name":"amount","type_schema":{"type":"string"},"value":"30000000000000","className":"3000000000000"},{"name":"new_unstaked_balance","type_schema":{"type":"string"},"value":""}]},"deposit":1,"gas":30000000000000,"selfInputDeposit":true,"depositUnit":"near"}]}} 
+const props = {"schema_version":"0.3.0","address":"linear-protocol.near","cssStyle":"","metadata":{"name":"","version":"0.1.0","authors":[""]},"body":{"functions":[{"name":"deposit_and_stake","kind":"call","label":"","button":"","className":"","classButton":"","labelDeposit":"","export":true,"params":{"serialization_type":"json","args":[{"name":"account_id","type_schema":{"type":"string"},"value":""},{"name":"amount","type_schema":{"type":"string"},"value":""},{"name":"new_unstaked_balance","type_schema":{"type":"string"},"value":""}]},"deposit":0,"gas":30000000000000,"depositUnit":"near","selfInputDeposit":false,"gasUnit":"yoctoNEAR"}]}} 
 
 return (
   <>
