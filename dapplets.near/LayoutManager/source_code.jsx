4b15f6a9b14eb76135bd11c0d5036b0433e44c28
dapplets.near <dapplets.near>
2023-11-08 02:31:39 +0000

    Update LayoutManager by dapplets.near at 2023-11-08T02:31:39.929Z

--- dapplets.near/LayoutManager/source_code.jsx
+++ dapplets.near/LayoutManager/source_code.jsx
@@ -1,7 +1,9 @@
+console.log({ components: props.components });
+
 return (
-  <div>
+  <>
     {props.components.map(({ src, props }, i) => (
       <Widget key={i} src={src} props={props} />
     ))}
-  </div>
+  </>
 );
