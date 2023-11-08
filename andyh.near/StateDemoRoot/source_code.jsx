e572a07d4d9bfb50324c1cac66ed8f04da68e4e9
andyh.near <andyh.near>
2023-11-08 19:02:14 +0000

    Update StateDemoRoot by andyh.near at 2023-11-08T19:02:14.817Z

--- andyh.near/StateDemoRoot/source_code.jsx
+++ andyh.near/StateDemoRoot/source_code.jsx
@@ -147,7 +147,7 @@ return (
           updateCircle: () => setCircle(getRandomIcon()),
           updateSquare: () => setSquare(getRandomIcon()),
           updateTriangle: () => {
-            console.log(triangle, setTriangle);
+            console.log(triangle, setTriangle, useState);
             setTriangle(getRandomIcon());
           },
         }}
