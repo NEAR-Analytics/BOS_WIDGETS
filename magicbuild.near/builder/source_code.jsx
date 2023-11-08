0a7e7485cf402c811aeb45cb79b815196d714d8c
magicbuild.near <magicbuild.near>
2023-11-08 14:12:00 +0000

    Update builder by magicbuild.near at 2023-11-08T14:12:00.515Z

--- magicbuild.near/builder/source_code.jsx
+++ magicbuild.near/builder/source_code.jsx
@@ -1010,7 +1010,7 @@ return (
                   tabindex="-1"
                   aria-labelledby="openModalPreviewLabel"
                 >
-                  <div class="modal-dialog">
+                  <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                       <div class="modal-header">
                         <h1 class="modal-title fs-5" id="showCSSLabel">
