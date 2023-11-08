633eaf6e6a4645c0069b788319e94a71e1687302
magicbuild.near <magicbuild.near>
2023-11-08 02:18:22 +0000

    Update preview by magicbuild.near at 2023-11-08T02:18:22.894Z

--- magicbuild.near/preview/source_code.jsx
+++ magicbuild.near/preview/source_code.jsx
@@ -191,23 +191,7 @@ return (
                         onChange={(e) =>
                           onInputChangeContractArg({
                             functions: functions.name,
-                            name: args.name,
-                            type:
-                              args.type_schema.type == "string" ||
-                              args.type_schema.type[0] == "string"
-                                ? "text"
-                                : args.type_schema.type == "integer" ||
-                                  args.type_schema.type[0] == "integer"
-                                ? "number"
-                                : args.type_schema.type == "array"
-                                ? "array"
-                                : args.type_schema.type == "json"
-                                ? "json"
-                                : args.type_schema.$ref
-                                ? "text"
-                                : "text",
                             value: e.target.value,
-                            [functions.name]: e.target.value,
                           })
                         }
                       />
