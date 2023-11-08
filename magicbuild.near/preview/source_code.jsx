95e121257f7fe8173b45d17e55601cd4fa583c46
magicbuild.near <magicbuild.near>
2023-11-08 01:03:54 +0000

    Update preview by magicbuild.near at 2023-11-08T01:03:54.577Z

--- magicbuild.near/preview/source_code.jsx
+++ magicbuild.near/preview/source_code.jsx
@@ -224,8 +224,6 @@ return (
                         </label>
                         <input
                           class="form-control"
-                          value={args.value || ""}
-                          defaulValue={args.value || ""}
                           onChange={(e) => cAD(e, functions, argIndex, "value")}
                         />
                         {args.type_schema.type == "string" ||
