8a688471ef35784684ad41ddf4f59b89e86314d1
magicbuild.near <magicbuild.near>
2023-11-08 03:57:26 +0000

    Update preview by magicbuild.near at 2023-11-08T03:57:26.240Z

--- magicbuild.near/preview/source_code.jsx
+++ magicbuild.near/preview/source_code.jsx
@@ -205,7 +205,25 @@ return (
                         <input
                           class="form-control"
                           value={args.value || ""}
-                          onChange={(e) => cArg(e, functions, argIndex, args)}
+                          onChange={(e) =>
+                            onInputChangeContractArg({
+                              functions: functions.name,
+                              name: args.name,
+                              type:
+                                args.type_schema.type == "string" ||
+                                args.type_schema.type[0] == "string"
+                                  ? "text"
+                                  : args.type_schema.type == "integer" ||
+                                    args.type_schema.type[0] == "integer"
+                                  ? "number"
+                                  : args.type_schema.type == "array"
+                                  ? "array"
+                                  : args.type_schema.$ref
+                                  ? "text"
+                                  : "text",
+                              value: e.target.value,
+                            })
+                          }
                         />
                         {args.type_schema.type == "string" ||
                         args.type_schema.type == "$ref" ||
