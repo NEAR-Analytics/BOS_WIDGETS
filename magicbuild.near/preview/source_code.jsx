eb8b1ac20713fd0f7ff6818bffa6d02e9cd5514f
magicbuild.near <magicbuild.near>
2023-11-08 04:16:24 +0000

    Update preview by magicbuild.near at 2023-11-08T04:16:24.735Z

--- magicbuild.near/preview/source_code.jsx
+++ magicbuild.near/preview/source_code.jsx
@@ -227,23 +227,25 @@ return (
                                 : "text"
                             }
                             defaultValue={args.value}
-                            onChange={onInputChangeContractArg({
-                              functions: functions.name,
-                              name: args.name,
-                              type:
-                                args.type_schema.type == "string" ||
-                                args.type_schema.type[0] == "string"
-                                  ? "text"
-                                  : args.type_schema.type == "integer" ||
-                                    args.type_schema.type[0] == "integer"
-                                  ? "number"
-                                  : args.type_schema.type == "array"
-                                  ? "array"
-                                  : args.type_schema.$ref
-                                  ? "text"
-                                  : "text",
-                              value: e.target.value,
-                            })}
+                            onChange={(e) =>
+                              onInputChangeContractArg({
+                                functions: functions.name,
+                                name: args.name,
+                                type:
+                                  args.type_schema.type == "string" ||
+                                  args.type_schema.type[0] == "string"
+                                    ? "text"
+                                    : args.type_schema.type == "integer" ||
+                                      args.type_schema.type[0] == "integer"
+                                    ? "number"
+                                    : args.type_schema.type == "array"
+                                    ? "array"
+                                    : args.type_schema.$ref
+                                    ? "text"
+                                    : "text",
+                                value: e.target.value,
+                              })
+                            }
                           />
                         ) : (
                           ""
