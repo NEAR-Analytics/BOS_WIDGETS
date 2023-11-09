baea9b3c678651d2b747815d2501a4782d66d450
marketplacebos.near <marketplacebos.near>
2023-11-09 08:58:16 +0000

    Update Card.Card0006 by marketplacebos.near at 2023-11-09T08:58:16.482Z

---
+++ marketplacebos.near/Card.Card0006/source_code.jsx
@@ -0,0 +1,119 @@
+const CardMain = styled.div`
+  position: relative;
+  width: 220px;
+  height: 270px;
+  background: mediumturquoise;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+  font-weight: bold;
+  border-radius: 15px;
+  cursor: pointer;
+
+  &:before,
+  &:after {
+    position: absolute;
+    content: "";
+    width: 20%;
+    height: 20%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+    font-weight: bold;
+    background-color: lightblue;
+    transition: all 0.5s;
+  }
+
+  &:before {
+    top: 0;
+    right: 0;
+    border-radius: 0 15px 0 100%;
+  }
+
+  &:after {
+    bottom: 0;
+    left: 0;
+    border-radius: 0 100% 0 15px;
+  }
+
+  &:hover:before,
+  &:hover:after {
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+    transition: all 0.5s;
+  }
+
+  &:hover:after {
+    content: "HELLO";
+  }
+`;
+
+const props = {
+  copyBtn:
+    "const CardMain = styled.div`\n" +
+    `  position: relative;
+  width: 220px;
+  height: 270px;
+  background: mediumturquoise;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+  font-weight: bold;
+  border-radius: 15px;
+  cursor: pointer;
+
+  &:before,
+  &:after {
+    position: absolute;
+    content: "";
+    width: 20%;
+    height: 20%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+    font-weight: bold;
+    background-color: lightblue;
+    transition: all 0.5s;
+  }
+
+  &:before {
+    top: 0;
+    right: 0;
+    border-radius: 0 15px 0 100%;
+  }
+
+  &:after {
+    bottom: 0;
+    left: 0;
+    border-radius: 0 100% 0 15px;
+  }
+
+  &:hover:before,
+  &:hover:after {
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+    transition: all 0.5s;
+  }
+
+  &:hover:after {
+    content: "HELLO";
+  }
+` +
+    "\n`;" +
+    `\n return(
+    <CardMain>HOVER</CardMain>
+  )`,
+  component: <CardMain>HOVER</CardMain>,
+  detailLink: "https://near.org/marketplacebos.near/widget/Detail.C0006",
+};
+return (
+  <>
+    <Widget src="marketplacebos.near/widget/CardMain.CardLarge" props={props} />
+  </>
+);
