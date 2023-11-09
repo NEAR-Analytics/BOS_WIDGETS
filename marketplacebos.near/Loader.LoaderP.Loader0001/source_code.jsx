703d5e5d552b2ad15f179e26d9ab62fbcd46f09d
marketplacebos.near <marketplacebos.near>
2023-11-09 08:35:48 +0000

    Update Loader.LoaderP.Loader0001 by marketplacebos.near at 2023-11-09T08:35:48.010Z

---
+++ marketplacebos.near/Loader.LoaderP.Loader0001/source_code.jsx
@@ -0,0 +1,44 @@
+const Loader = styled.div`
+ --dim: 3rem;
+  width: var(--dim);
+  height: var(--dim);
+  position: relative;
+  animation: spin988 2s linear infinite;
+  
+  @keyframes spin988 {
+    0% {
+      transform: scale(1) rotate(0);
+    }
+  
+    20%, 25% {
+      transform: scale(1.3) rotate(90deg);
+    }
+  
+    45%, 50% {
+      transform: scale(1) rotate(180deg);
+    }
+  
+    70%, 75% {
+      transform: scale(1.3) rotate(270deg);
+    }
+  
+    95%, 100% {
+      transform: scale(1) rotate(360deg);
+    }
+  }`;
+const Circle = styled.div`
+   --color: #333;
+  --dim: 1.2rem;
+  width: var(--dim);
+  height: var(--dim);
+  background-color: var(--color);
+  border-radius: 50%;
+  position: absolute;`;
+ return(
+      <Loader>
+                <Circle style={{ top: 0, left: 0 }} />
+                <Circle style={{ top: 0, right: 0 }} />
+                <Circle style={{ bottom: 0, left: 0 }} />
+                <Circle style={{ bottom: 0, right: 0 }} />
+      </Loader>
+  )
