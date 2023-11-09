453a13e3f4018b191293fba1f32f6c567c10187d
james.near <james.near>
2023-11-09 08:25:47 +0000

    Update fork by james.near at 2023-11-09T08:25:47.247Z

--- james.near/fork/source_code.jsx
+++ james.near/fork/source_code.jsx
@@ -62,96 +62,27 @@ const forkClick = () => {
   });
 };
 
-const Modal = styled.div`
-  position: fixed;
-  z-index: 1;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(0, 0, 0, 0.7);
-`;
-
-const ComponentWrapper = styled.div`
-  display: flex;
-  width: 80%;
-  height: 80%;
-  flex-direction: column;
-  align-items: flex-start;
-  border-radius: 10px;
-  background: #fff;
-  border: 1px solid transparent;
-  margin: 140px auto auto auto;
-  @media only screen and (max-width: 480px) {
-    width: 90%;
-  }
-`;
-
-const CardStyled = styled.div`
-  width: 100%;
-  height: 100%;
-  background: #f8f8f9;
-  gap: 10px;
-  padding: 25px;
-  margin: 0 auto;
-  border-radius: 10px;
-  overflow-y: scroll;
-`;
-
-const CardForm = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: auto;
-`;
-
 return (
-  <Modal>
-    <ComponentWrapper>
-      <CardStyled>
-        <CardForm>
-          <div className="d-flex flex-wrap justify-content-between mb-3">
-            <div className="m-1">
-              <h1>Fork</h1>
-            </div>
-            <div className="ms-auto me-0 me-md-2 d-flex align-items-center">
-              <div className="top-right">
-                <Widget
-                  src="james.near/widget/styling"
-                  props={{
-                    Button: {
-                      style: "danger",
-                      text: "Close",
-                      onClick: handleClose,
-                    },
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="input-group m-1 mb-2">
-            <input
-              className="form-control mt-2"
-              defaultValue={state.name}
-              onChange={(e) => {
-                State.update({
-                  name: e.target.value,
-                });
-              }}
-            />
-          </div>
-          <button
-            className="btn btn-outline-secondary m-1 mt-3"
-            onClick={forkClick}
-          >
-            <i className="bi bi-feather"></i>
-            Submit
-          </button>
-        </CardForm>
-      </CardStyled>
-    </ComponentWrapper>
-  </Modal>
+  <div className="m-1 row">
+    <div className="m-1 col-3">
+      <p className="m-2">{`${context.accountId}/${type}/`}</p>
+    </div>
+    <div className="m-1 col-5">
+      <input
+        className="form-control"
+        defaultValue={state.name}
+        onChange={(e) => {
+          State.update({
+            name: e.target.value,
+          });
+        }}
+      />
+    </div>
+    <div className="m-1 col-3">
+      <button className="btn btn-outline-secondary" onClick={forkClick}>
+        <i className="bi bi-feather"></i>
+        Submit
+      </button>
+    </div>
+  </div>
 );
