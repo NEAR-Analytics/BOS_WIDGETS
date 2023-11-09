45a0950294aef2a01740bbfac1d0c5435391d7c8
harrydhillon.near <harrydhillon.near>
2023-11-09 07:18:31 +0000

    Update BountyManager.ManageAccount by harrydhillon.near at 2023-11-09T07:18:31.091Z

--- harrydhillon.near/BountyManager.ManageAccount/source_code.jsx
+++ harrydhillon.near/BountyManager.ManageAccount/source_code.jsx
@@ -2,6 +2,13 @@ State.init({
   tabName: "whitelist",
 });
 
+const listAcc = [
+  "noak.near",
+  "robert.near",
+  "harrydhillon.near",
+  "stainslaw.near",
+];
+
 return (
   <div style={{ padding: 20 }}>
     <p style={{ fontSize: 30, fontWeight: "500" }}>Manage Account</p>
@@ -14,5 +21,77 @@ return (
         },
       }}
     />
+    {state.tabName === "whitelist" && (
+      <>
+        <div style={{ display: "flex", justifyContent: "space-betweeen" }}>
+          <div style={{ width: "49%" }}>
+            <p style={{ fontSize: 20, fontWeight: "500" }}>Whitelist</p>
+            {listAcc.map((item) => (
+              <div
+                style={{ height: 45, display: "flex", alignItems: "center" }}
+              >
+                <p style={{ transform: "translateY(5px)" }}>{item}</p>
+              </div>
+            ))}
+          </div>
+          <div style={{ width: "49%" }}>
+            <p style={{ fontSize: 20, fontWeight: "500" }}>Blacklist</p>
+            {listAcc.map((item) => (
+              <div
+                style={{
+                  display: "flex",
+                  paddingTop: 2,
+                  height: 45,
+                  justifyContent: "space-around",
+                }}
+              >
+                <button style={{ height: "fit-content" }}>Remove</button>
+                <button style={{ height: "fit-content" }}>Blacklist</button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div style={{ display: "flex", gap: "10px", paddingTop: 10 }}>
+          <input placeholder="Add Account to whitelist" />
+          <button>Add</button>
+        </div>
+      </>
+    )}
+    {state.tabName === "blacklist" && (
+      <>
+        <div style={{ display: "flex", justifyContent: "space-betweeen" }}>
+          <div style={{ width: "49%" }}>
+            <p style={{ fontSize: 20, fontWeight: "500" }}>Blacklist</p>
+            {listAcc.map((item) => (
+              <div
+                style={{ height: 45, display: "flex", alignItems: "center" }}
+              >
+                <p style={{ transform: "translateY(5px)" }}>{item}</p>
+              </div>
+            ))}
+          </div>
+          <div style={{ width: "49%" }}>
+            <p style={{ fontSize: 20, fontWeight: "500" }}>Whitelist</p>
+            {listAcc.map((item) => (
+              <div
+                style={{
+                  display: "flex",
+                  paddingTop: 2,
+                  height: 45,
+                  justifyContent: "space-around",
+                }}
+              >
+                <button style={{ height: "fit-content" }}>Remove</button>
+                <button style={{ height: "fit-content" }}>Whitelist</button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div style={{ display: "flex", gap: "10px", paddingTop: 10 }}>
+          <input placeholder="Add Account to whitelist" />
+          <button>Add</button>
+        </div>
+      </>
+    )}
   </div>
 );
