d00926b4ac03ad1a8504a0c9974399ca25f62a87
pollum.near <pollum.near>
2023-11-09 01:19:25 +0000

    Update Onboarding.Starter-fork by pollum.near at 2023-11-09T01:19:25.531Z

---
+++ pollum.near/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,9 @@
+return (
+  <>
+    <Widget src="near/widget/Onboarding.ComponentCard" props={{ accountId }} />
+    <Widget />
+    <br />
+    <Widget src="near/widget/Onboarding.ComponentCollection" props={{}} />
+    <Widget />
+  </>
+);
