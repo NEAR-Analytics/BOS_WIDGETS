3f2cd4fce0057d91712827628f0e5b96ecc9c23b
harrydhillon.near <harrydhillon.near>
2023-11-09 07:02:47 +0000

    Update BountyManager.ManageAccount by harrydhillon.near at 2023-11-09T07:02:47.422Z

--- harrydhillon.near/BountyManager.ManageAccount/source_code.jsx
+++ harrydhillon.near/BountyManager.ManageAccount/source_code.jsx
@@ -1,5 +1,18 @@
+State.init({
+  tabName: "whitelist",
+});
+
 return (
   <div style={{ padding: 20 }}>
     <p style={{ fontSize: 30, fontWeight: "500" }}>Manage Account</p>
+    <Widget
+      src="harrydhillon.near/widget/BountyManager.Layout.Tab"
+      props={{
+        tabName: state.tabName,
+        updateTabName: (val) => {
+          State.update({ tabName: val });
+        },
+      }}
+    />
   </div>
 );
