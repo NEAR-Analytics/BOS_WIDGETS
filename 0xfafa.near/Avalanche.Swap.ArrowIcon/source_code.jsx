2e9324f8af0283729a0aa3c49215fff4b2a8e25d
0xfafa.near <0xfafa.near>
2023-11-09 08:31:58 +0000

    Update Avalanche.Swap.ArrowIcon by 0xfafa.near at 2023-11-09T08:31:58.460Z

---
+++ 0xfafa.near/Avalanche.Swap.ArrowIcon/source_code.jsx
@@ -0,0 +1,18 @@
+const Size = props.size || 17;
+
+return (
+  <svg
+    width={Size}
+    height={Size * 0.53}
+    viewBox="0 0 17 9"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1 1L8.5 7.5L16 1"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+);
