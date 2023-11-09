e036504ea553d5f20ec689c3a650b1272683da04
andyh.near <andyh.near>
2023-11-09 04:35:50 +0000

    Update ShapeSet.Trusted by andyh.near at 2023-11-09T04:35:50.817Z

--- andyh.near/ShapeSet.Trusted/source_code.jsx
+++ andyh.near/ShapeSet.Trusted/source_code.jsx
@@ -1,15 +1,10 @@
-// this is included to test for cascading renders resulting
-// from State.update(...) execution at render time
-State.init({ x: 0 });
-State.update({ x: 1 });
-
 return (
   <div className="row" style={{ padding: "8px 4px" }}>
     <div className="col">
       <Widget
         id="circle"
+        src="andyh.near/Circle"
         trust={{ mode: "trusted" }}
-        src="andyh.near/widget/Circle"
         props={{
           color: "#C1200B",
           iconColor: "white",
@@ -22,8 +17,8 @@ return (
     <div className="col">
       <Widget
         id="square"
+        src="andyh.near/Square"
         trust={{ mode: "trusted" }}
-        src="andyh.near/widget/Square"
         props={{
           color: "#4A825A",
           iconColor: "white",
@@ -35,7 +30,7 @@ return (
     </div>
     <div className="col">
       <Widget
-        src="andyh.near/widget/Triangle"
+        src="andyh.near/Triangle"
         trust={{ mode: "trusted" }}
         props={{
           color: "#0A81D1",
