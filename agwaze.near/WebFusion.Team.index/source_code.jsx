de5d1148ea616e930cff3ae674f2095f962ab2d6
agwaze.near <agwaze.near>
2023-11-10 21:33:17 +0000

    Update WebFusion.Team.index by agwaze.near at 2023-11-10T21:33:17.991Z

---
+++ agwaze.near/WebFusion.Team.index/source_code.jsx
@@ -0,0 +1,17 @@
+"bafkreief3msh3xc734huv2265hfu6ahohjaf2nwoghfqp4kh4wl77n2z2q";
+
+const Root = styled.div`
+    .hero {
+        img {
+            width: 100%;
+        }
+    }
+`;
+
+return (
+  <Root>
+    <div className="hero">
+      <img src="https://ipfs.near.social/ipfs/bafkreief3msh3xc734huv2265hfu6ahohjaf2nwoghfqp4kh4wl77n2z2q" />
+    </div>
+  </Root>
+);
