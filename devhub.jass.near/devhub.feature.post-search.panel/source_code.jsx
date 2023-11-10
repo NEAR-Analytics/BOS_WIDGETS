cd0f7b0cfe4129023269325cc087d9fbc2d3bace
devhub.jass.near <devhub.jass.near>
2023-11-10 17:57:46 +0000

    Update devhub.feature.post-search.panel by devhub.jass.near at 2023-11-10T17:57:46.086Z

--- devhub.jass.near/devhub.feature.post-search.panel/source_code.jsx
+++ devhub.jass.near/devhub.feature.post-search.panel/source_code.jsx
@@ -1,4 +1,4 @@
-const { href } = VM.require("${REPL_DEVHUB}/widget/core.lib.url");
+const { href } = VM.require("devhub.jass.near/widget/core.lib.url");
 
 href || (href = () => {});
 
@@ -86,7 +86,7 @@ return (
                       style={{ borderRadius: "5px" }}
                       class="dropdown-item link-underline link-underline-opacity-0"
                       href={href({
-                        widgetSrc: "${REPL_DEVHUB}/widget/app",
+                        widgetSrc: "devhub.jass.near/widget/app",
                         params: { page: "feed" },
                       })}
                     >
@@ -98,7 +98,7 @@ return (
                       style={{ borderRadius: "5px" }}
                       class="dropdown-item link-underline link-underline-opacity-0"
                       href={href({
-                        widgetSrc: "${REPL_DEVHUB}/widget/app",
+                        widgetSrc: "devhub.jass.near/widget/app",
                         params: { page: "feed", recency: "all" },
                       })}
                     >
@@ -109,7 +109,7 @@ return (
               </div>
               <div class="dropdown">
                 <Widget
-                  src="${REPL_DEVHUB}/widget/devhub.feature.post-search.by-author"
+                  src="devhub.jass.near/widget/devhub.feature.post-search.by-author"
                   props={{
                     author: state.author,
                     onAuthorSearch: (author) => {
@@ -120,7 +120,7 @@ return (
               </div>
               <div>
                 <Widget
-                  src="${REPL_DEVHUB}/widget/devhub.feature.post-search.by-tag"
+                  src="devhub.jass.near/widget/devhub.feature.post-search.by-tag"
                   props={{
                     tag: state.tag,
                     onTagSearch: (tag) => {
@@ -139,7 +139,7 @@ return (
     )}
     <PostContainer>
       <Widget
-        src="${REPL_DEVHUB}/widget/devhub.entity.post.List"
+        src="devhub.jass.near/widget/devhub.entity.post.List"
         props={{
           author: state.author,
           tag: state.tag,
