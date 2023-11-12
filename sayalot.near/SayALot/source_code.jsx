4deeadc10e9daf6442ea0ebc38874b19c96308e8
sayalot.near <sayalot.near>
2023-11-12 18:58:08 +0000

    Update SayALot by sayalot.near at 2023-11-12T18:58:08.550Z

--- sayalot.near/SayALot/source_code.jsx
+++ sayalot.near/SayALot/source_code.jsx
@@ -46,9 +46,9 @@ function createSbtOptions() {
 //   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 const componentsOwner = "sayalot.near";
 
-// const authorForWidget = "sayalot.near";
-const authorForWidget =
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+const authorForWidget = "sayalot.near";
+// const authorForWidget =
+//   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 // const authorForWidget = "kenrou-it.near";
 // const authorForWidget = "silkking.near";
 
