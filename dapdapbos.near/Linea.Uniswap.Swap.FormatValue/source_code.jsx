a7d3a32b48488c6ad7882e60e9e2355a6cd46b31
dapdapbos.near <dapdapbos.near>
2023-11-13 07:10:38 +0000

    Update Linea.Uniswap.Swap.FormatValue by dapdapbos.near at 2023-11-13T07:10:38.636Z

---
+++ dapdapbos.near/Linea.Uniswap.Swap.FormatValue/source_code.jsx
@@ -0,0 +1,24 @@
+const prices = Storage.get(
+  "tokensPrice",
+  "dapdapbos.near/widget/Linea.Uniswap.Swap.TokensPrice"
+);
+
+const { symbol, amount } = props;
+const digits = props.digits || 2;
+const prev = props.prev;
+if (!amount || !symbol) return "-";
+const price = prices[symbol];
+let unit = "";
+let total = Big(amount);
+if (!price) {
+  unit = symbol;
+  prev = "";
+}
+if (price) {
+  total = Big(amount).mul(price);
+}
+if (total.eq(0)) return "0" + unit;
+const digitSplit = 1 / Math.pow(10, digits);
+if (total.lt(digitSplit)) return "<" + prev + digitSplit + unit;
+
+return prev + total.toFixed(digit) + unit;
