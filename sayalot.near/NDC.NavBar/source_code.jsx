de3c2063f803a90c68c30e55b0aaa07c65fc508c
sayalot.near <sayalot.near>
2023-11-14 18:52:45 +0000

    Update NDC.NavBar by sayalot.near at 2023-11-14T18:52:45.447Z

--- sayalot.near/NDC.NavBar/source_code.jsx
+++ sayalot.near/NDC.NavBar/source_code.jsx
@@ -42,9 +42,6 @@ function stateUpdate(obj) {
 
 const loggedUserAccountId = context.accountId;
 
-State.init({
-  selectedPillIndex: 0,
-});
 // const canLoggedUserCreateArticle = state.canLoggedUserCreateArticle;
 
 const logoRemWidth = brand.logoRemWidth
@@ -135,7 +132,6 @@ const renderButton = (button, i) => {
           onClick: () => {
             handlePillNavigation(button.id);
             State.update({
-              selectedPillIndex: undefined,
               selectedButtonIndex: i,
             });
           },
@@ -153,7 +149,6 @@ const renderButton = (button, i) => {
 
 function realHandleBackButton() {
   State.update({
-    selectedPillIndex: 0,
     selectedButtonIndex: undefined,
   });
 
@@ -162,7 +157,6 @@ function realHandleBackButton() {
 
 function realHandleGoHome() {
   State.update({
-    selectedPillIndex: 0,
     selectedButtonIndex: undefined,
   });
   handleGoHomeButton();
@@ -217,9 +211,7 @@ return (
                   <li className="nav-item">
                     <Pill
                       style={
-                        state.selectedPillIndex == i
-                          ? { color: activeColor }
-                          : {}
+                        pill.id === displayedTabId ? { color: activeColor } : {}
                       }
                       onClick={() => {
                         //First one is set to be de "Home" one
@@ -230,7 +222,6 @@ return (
                           handlePillNavigation(pill.id);
                         }
                         State.update({
-                          selectedPillIndex: i,
                           selectedButtonIndex: undefined,
                         });
                       }}
