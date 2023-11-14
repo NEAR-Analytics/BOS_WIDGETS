9eaf3d39bc7c6137df6e21ef33304b1469c1a2bd
sayalot.near <sayalot.near>
2023-11-14 18:55:38 +0000

    Update SayALot by sayalot.near at 2023-11-14T18:55:38.353Z

--- sayalot.near/SayALot/source_code.jsx
+++ sayalot.near/SayALot/source_code.jsx
@@ -46,9 +46,11 @@ function createSbtOptions() {
 //   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 const componentsOwner = "sayalot.near";
 
-const authorForWidget = "sayalot.near";
-// const authorForWidget =
-//   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+// const authorForWidget = "sayalot.near";
+const authorForWidget =
+  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
+// const authorForWidget = "kenrou-it.near";
+// const authorForWidget = "silkking.near";
 
 const configWidget = "SayALot";
 
@@ -70,12 +72,14 @@ const widgets = {
   upVoteButton: `${componentsOwner}/widget/NDC.UpVoteButton`,
   profileShortInlineBlock: `${componentsOwner}/widget/Profile.ShortInlineBlock`,
   tagsEditor: `${componentsOwner}/widget/TagsEditor`,
+  kanbanBoard: `${componentsOwner}/widget/NDC.KanbanBoard`,
+  compactPost: `${componentsOwner}/widget/NDC.CompactPost`,
 
   //Libs
-  // libSBT: `sayalot.near/widget/lib.SBT`,
   libSBT: `sayalot.near/widget/lib.SBT`,
   libComment: `sayalot.near/widget/lib.comment`,
-  libArticle: `sayalot.near/widget/lib.article`,
+  // libArticle: `sayalot.near/widget/lib.article`, ///////////////////////////////////////////////////////////
+  libArticle: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/lib.article`, ///////////////////////////////////////////////////////////
   libEmojis: `sayalot.near/widget/lib.emojis`,
   libUpVotes: `sayalot.near/widget/lib.upVotes`,
 
@@ -118,6 +122,11 @@ const baseActions = {
   reactionBaseAction: "sayALotReaction",
 };
 
+const kanbanColumns = ["Open", "Claimed", "In Work", "Closed"];
+
+const kanbanRequiredTags = [];
+const kanbanExcludedTags = [];
+
 return (
   <Widget
     src={widgets.ndcForum}
@@ -132,6 +141,9 @@ return (
       brand,
       baseActions,
       createSbtOptions,
+      kanbanColumns,
+      kanbanRequiredLabels,
+      kanbanExcludedLabels,
     }}
   />
 );
