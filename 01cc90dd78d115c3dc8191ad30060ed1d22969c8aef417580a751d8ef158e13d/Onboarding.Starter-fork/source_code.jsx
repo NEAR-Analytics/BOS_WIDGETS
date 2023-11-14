dc07e3ba629332b8c56a32b2ff4c97c70bd34e03
01cc90dd78d115c3dc8191ad30060ed1d22969c8aef417580a751d8ef158e13d <01cc90dd78d115c3dc8191ad30060ed1d22969c8aef417580a751d8ef158e13d>
2023-11-14 01:50:45 +0000

    Update Onboarding.Starter-fork by 01cc90dd78d115c3dc8191ad30060ed1d22969c8aef417580a751d8ef158e13d at 2023-11-14T01:50:45.489Z

---
+++ 01cc90dd78d115c3dc8191ad30060ed1d22969c8aef417580a751d8ef158e13d/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,6 @@
+let greeting = "Have a great day";
+
+return (
+  <Widget src="near/widget/Onboarding.ComponentCard" props={{ accountId }} />
+);
+<Widget src="near/widget/Onboarding.ComponentCollection" props={{}} />;
