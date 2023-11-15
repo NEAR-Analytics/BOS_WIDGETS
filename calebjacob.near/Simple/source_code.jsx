2f85c5ec976dd145371ab904d9d656c4c3b7b1d2
calebjacob.near <calebjacob.near>
2023-11-15 22:30:43 +0000

    Update Simple by calebjacob.near at 2023-11-15T22:30:43.787Z

--- calebjacob.near/Simple/source_code.jsx
+++ calebjacob.near/Simple/source_code.jsx
@@ -2,10 +2,12 @@ const Wrapper = styled.div`
   button span {
     color: red;
     background: black;
+    font-family: sans-serif;
   }
 `;
+
 return (
   <Wrapper>
-    <Widget src="near/widget/DIG.Button" props={{ label: "Click Me!" }} />
+    <Widget src="near/widget/DIG.Button" props={{ label: "Click Me" }} />
   </Wrapper>
 );
