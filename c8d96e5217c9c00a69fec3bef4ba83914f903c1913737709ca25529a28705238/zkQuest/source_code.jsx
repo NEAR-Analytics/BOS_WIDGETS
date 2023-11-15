796e7f47d8275415b663ed8330b726513a12ac49
c8d96e5217c9c00a69fec3bef4ba83914f903c1913737709ca25529a28705238 <c8d96e5217c9c00a69fec3bef4ba83914f903c1913737709ca25529a28705238>
2023-11-15 01:19:09 +0000

    Update zkQuest by c8d96e5217c9c00a69fec3bef4ba83914f903c1913737709ca25529a28705238 at 2023-11-15T01:19:09.582Z

--- c8d96e5217c9c00a69fec3bef4ba83914f903c1913737709ca25529a28705238/zkQuest/source_code.jsx
+++ c8d96e5217c9c00a69fec3bef4ba83914f903c1913737709ca25529a28705238/zkQuest/source_code.jsx
@@ -1,5 +1,3 @@
-// ... (previous code remains unchanged)
-
 function sendSolution() {
   const questContract = new ethers.Contract(
     QUEST_ADDRESS,
@@ -9,53 +7,52 @@ function sendSolution() {
 
   let transactionHash = "";
 
-  // 1. Use the correct contract function.
+  // 1. Oops! We've been calling the wrong contract function all this time.
   //    Tip: You can use era-test-node to replay transactions from your previous submissions, with --show-calls=user --resolve-hashes
+
+  // 2. And then there's the payload... Some of it seems to have gone missing!
+  //    - Don't forget to include `QUEST_NAME`.
+  //    - A keccak hash generated from a combination of specific elements: answer, your zkQuest account address, and the number of attempts.
+  //
+  //    let answerHash = ethers.utils.solidityKeccak256(["string", "address", "uint16"], [ANSWER, ADDRESS, attempts]);
+  //    Oh, and the answer is the private key of the first 'rich' account from era-test-node (with '0x' prefix)
+
+  // Replace the following placeholders with the correct values:
+  let ANSWER = "your_private_key"; // Replace with the correct private key
+  let ADDRESS = "0x89Ea573E7ea1b6dE8Ece842302ad76ACbe531015"; // Replace with the correct account address
+  let attempts = 1; // Replace with the correct number of attempts
+
+  let answerHash = ethers.utils.solidityKeccak256(
+    ["string", "address", "uint16"],
+    [QUEST_NAME, ADDRESS, attempts]
+  );
+
+  // 3. Call the identified contract function with the constructed payload.
   questContract
-    .submitInvalid(QUEST_NAME, { gasLimit: 1000000 })
+    .submitInvalid(QUEST_NAME, answerHash, { gasLimit: 1000000 })
     .then((transaction) => {
-      // 2. Construct the payload, including `QUEST_NAME` and the keccak hash.
-      //    - The answer is the private key of the first 'rich' account from era-test-node (with '0x' prefix)
-      //    - Replace ANSWER, ADDRESS, and attempts with the correct values.
-      let ANSWER = "your_private_key"; // Replace with the correct private key
-      let ADDRESS = "your_zkQuest_account_address"; // Replace with the correct account address
-      let attempts = 1; // Replace with the correct number of attempts
+      console.log("Hash is: ");
+      console.log(transaction.hash);
+      transactionHash = transaction.hash;
 
-      let answerHash = ethers.utils.solidityKeccak256(
-        ["string", "address", "uint16"],
-        [ANSWER, ADDRESS, attempts]
-      );
-
-      // 3. Call the identified contract function with the constructed payload.
-      questContract
-        .submitInvalid(QUEST_NAME, answerHash, { gasLimit: 1000000 }) // Include the answerHash in the function call
-        .then((transaction) => {
-          console.log("Hash is: ");
-          console.log(transaction.hash);
-          transactionHash = transaction.hash;
-
-          transaction
-            .wait()
-            .then((receipt) => {
-              State.update({
-                transactionStatus: "ok",
-              });
-            })
-            .catch((error) => {
-              State.update({
-                transactionStatus: "failed",
-              });
-            });
-        })
-        .catch((error) => console.log("Error submitting transaction: " + error))
-        .finally(() =>
+      transaction
+        .wait()
+        .then((receipt) => {
           State.update({
-            transactionHash: transactionHash,
-            attempted: true,
-          })
-        );
+            transactionStatus: "ok",
+          });
+        })
+        .catch((error) => {
+          State.update({
+            transactionStatus: "failed",
+          });
+        });
     })
-    .catch((error) => console.log("Error submitting transaction: " + error));
+    .catch((error) => console.log("Error submitting transaction: " + error))
+    .finally(() =>
+      State.update({
+        transactionHash: transactionHash,
+        attempted: true,
+      })
+    );
 }
-
-// ... (rest of the code remains unchanged)
