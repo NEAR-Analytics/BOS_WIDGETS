dd9c3e51a8b6b5be9d57c53734318e8f8f451cbb
efiz.near <efiz.near>
2023-11-15 02:51:14 +0000

    Update action by efiz.near at 2023-11-15T02:51:14.815Z

--- efiz.near/action/source_code.jsx
+++ efiz.near/action/source_code.jsx
@@ -1,4 +1,17 @@
 const accountId = context.accountId;
+const focus = props.focus;
+
+if (focus) {
+  const parts = focus.split("/");
+  if (parts.length) {
+    return (
+      <Widget
+        src="miraclx.near/widget/Attribution"
+        props={{ dep: true, authors: [parts[0]] }}
+      />
+    );
+  }
+}
 
 const action = accountId
   ? Social.get(`${accountId}/settings/every/action`)
