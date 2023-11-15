365f48da53fbf6ddda829b4eb9b21982275ecc65
syi216.near <syi216.near>
2023-11-15 20:19:02 +0000

    Update MultiplesWidgets.Hijo by syi216.near at 2023-11-15T20:19:02.741Z

---
+++ syi216.near/MultiplesWidgets.Hijo/source_code.jsx
@@ -0,0 +1,15 @@
+State.init({ boton: props.input });
+return (
+  <div>
+    <div class="d-flex flex-column gap-2">
+      <div class="d-flex gap-2">
+        <p class="my-auto">{props.label}</p>
+        <input
+          value={state.boton}
+          onChange={(e) => State.update({ boton: e.target.value })}
+        />
+      </div>
+      <button onClick={props.sendMessage}>Enviar informacion al padre</button>
+    </div>
+  </div>
+);
