382a85405e69cee48dd032aad4edddf2ef5e34a3
espen.near <espen.near>
2023-11-15 23:55:20 +0000

    Update devhub.entity.community.configuration.AboutConfigurator by espen.near at 2023-11-15T23:55:20.015Z

--- espen.near/devhub.entity.community.configuration.AboutConfigurator/source_code.jsx
+++ espen.near/devhub.entity.community.configuration.AboutConfigurator/source_code.jsx
@@ -8,7 +8,7 @@ const CommunityAboutSchema = {
 
       placeholder:
         "Tell people about your community. This will appear on your community’s homepage.",
-
+      required: true,
       resize: "none",
     },
 
