0e241e85e156cae989893c6160430e6b4ca74cdd
geforcy.near <geforcy.near>
2023-11-17 20:37:59 +0000

    Update devhub.components.molecule.Button by geforcy.near at 2023-11-17T20:37:59.604Z

--- geforcy.near/devhub.components.molecule.Button/source_code.jsx
+++ geforcy.near/devhub.components.molecule.Button/source_code.jsx
@@ -134,7 +134,7 @@ const Button = ({
         typeof iconProps === "object" &&
         !Array.isArray(iconProps) && (
           <Widget
-            src={"geforcy.near/widget/devhub.components.atom.Icon"}
+            src={"${REPL_DEVHUB}/widget/devhub.components.atom.Icon"}
             props={iconProps}
           />
         )}
