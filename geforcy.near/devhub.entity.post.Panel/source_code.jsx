451b94004dcafb8ab919c0c13164755d6d928a06
geforcy.near <geforcy.near>
2023-11-17 20:50:13 +0000

    Update devhub.entity.post.Panel by geforcy.near at 2023-11-17T20:50:13.894Z

--- geforcy.near/devhub.entity.post.Panel/source_code.jsx
+++ geforcy.near/devhub.entity.post.Panel/source_code.jsx
@@ -521,14 +521,16 @@ const search = (processedQueryArray, index) => {
 const amountOfResultsToShowFirst = 5;
 
 const buildPostsIndex = () => {
-  return Near.asyncView("geforcy.near", "get_posts").then((posts) => {
-    const index = buildIndex(posts);
-    const data = posts.reduce((acc, post) => {
-      acc[post.id] = post;
-      return acc;
-    }, {});
-    return { index, data };
-  });
+  return Near.asyncView("geforcy.near", "get_posts").then(
+    (posts) => {
+      const index = buildIndex(posts);
+      const data = posts.reduce((acc, post) => {
+        acc[post.id] = post;
+        return acc;
+      }, {});
+      return { index, data };
+    }
+  );
 };
 
 const getProcessedPostsCached = () => {
@@ -695,7 +697,9 @@ return (
       <div>
         <Widget
           // TODO: LEGACY.
-          src={"devgovgigs.near/widget/gigs-board.feature.post-search.by-tag"}
+          src={
+            "devgovgigs.near/widget/gigs-board.feature.post-search.by-tag"
+          }
           props={{
             tagQuery: props.tagQuery,
             onTagSearch: props.onTagSearch,
