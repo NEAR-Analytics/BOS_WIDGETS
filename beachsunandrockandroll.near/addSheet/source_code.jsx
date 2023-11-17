73045bf17cccd4e8250404659ef640188e973e2c
beachsunandrockandroll.near <beachsunandrockandroll.near>
2023-11-17 23:18:02 +0000

    Update addSheet by beachsunandrockandroll.near at 2023-11-17T23:18:02.135Z

---
+++ beachsunandrockandroll.near/addSheet/source_code.jsx
@@ -0,0 +1,90 @@
+const ppdContract = props.ppdContract;
+const ppdAbi = props.ppdAbi;
+
+const [sheetName, setSheetName] = useState("");
+const [composerIdx, setComposerIdx] = useState("");
+const [difficulty, setDifficulty] = useState(0);
+
+State.init({
+  img: null,
+});
+
+const addSheet = async () => {
+  if (!Ethers.provider()) return;
+
+  const ppd = new ethers.Contract(
+    ppdContract,
+    ppdAbi.body,
+    Ethers.provider().getSigner()
+  );
+
+  const userIdx = await ppd.getUserIdx();
+
+  const daraUri = state.img.cid ?? "";
+
+  ppd.addSheet(sheetName, composerIdx, userIdx, difficulty, daraUri);
+};
+
+const getComposers = () => {
+  if (!Ethers.provider()) return;
+
+  const ppd = new ethers.Contract(
+    ppdContract,
+    ppdAbi.body,
+    Ethers.provider().getSigner()
+  );
+
+  ppd.getComposers().then((res) => {
+    console.log(res);
+    // const comp = res && res.map((r) => r[0]);
+    // State.update({ composers: comp });
+  });
+};
+
+useEffect(() => {
+  if (ppdContract) getComposers();
+}, [ppdContract]);
+
+return (
+  <>
+    <div>
+      <input
+        className="form-control m-2 p-2"
+        type="text"
+        id="sheetName"
+        name="sheetName"
+        required
+        placeholder="Name of the Sheet"
+        onChange={(event) => {
+          setSheetName(event.target.value);
+        }}
+      />
+      <Typeahead
+        className="p-2"
+        options={state.composers}
+        // multiple
+        onChange={(value) => {
+          setComposerIdx(value);
+        }}
+        placeholder="Choose a tag to filter..."
+      />
+      <div>
+        Sheet upload: <br />
+        <IpfsImageUpload image={state.img} />
+      </div>
+
+      <input
+        className="form-control m-2 p-2"
+        type="text"
+        id="difficulty"
+        name="difficulty"
+        required
+        placeholder="Level of Difficulty"
+        onChange={(event) => {
+          setDifficulty(event.target.value);
+        }}
+      />
+      <button onClick={addSheet}>Add Sheet</button>
+    </div>
+  </>
+);
