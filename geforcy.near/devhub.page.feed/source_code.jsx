909c2fed32bbbcde68c053ee5c773f52c97582a9
geforcy.near <geforcy.near>
2023-11-17 20:37:59 +0000

    Update devhub.page.feed by geforcy.near at 2023-11-17T20:37:59.604Z

--- geforcy.near/devhub.page.feed/source_code.jsx
+++ geforcy.near/devhub.page.feed/source_code.jsx
@@ -1,6 +1,6 @@
 const { author, recency, tag } = props;
 
-const { href } = VM.require("geforcy.near/widget/core.lib.url");
+const { href } = VM.require("${REPL_DEVHUB}/widget/core.lib.url");
 
 if (!href) {
   return <p>Loading modules...</p>;
@@ -44,20 +44,20 @@ const Gradient = styled.div`
 const FeedPage = ({ recency, tag }) => {
   return (
     <div className="w-100">
-      <Widget src={`geforcy.near/widget/devhub.components.island.banner`} />
+      <Widget src={`${REPL_DEVHUB}/widget/devhub.components.island.banner`} />
       <Widget
-        src={"geforcy.near/widget/devhub.feature.post-search.panel"}
+        src={"${REPL_DEVHUB}/widget/devhub.feature.post-search.panel"}
         props={{
           hideHeader: false,
           children: (
             <Widget
               src={
-                "geforcy.near/widget/devhub.components.molecule.PostControls"
+                "${REPL_DEVHUB}/widget/devhub.components.molecule.PostControls"
               }
               props={{
                 title: "Post",
                 href: href({
-                  widgetSrc: "geforcy.near/widget/app",
+                  widgetSrc: "${REPL_DEVHUB}/widget/app",
                   params: { page: "create" },
                 }),
               }}
