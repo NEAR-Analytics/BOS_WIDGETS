4b3718e3a7eff95cb8caaf1da866abf671d9fc63
y3k.near <y3k.near>
2023-11-17 14:30:22 +0000

    Update near_atlas.components.Profile.IntroductionSection by y3k.near at 2023-11-17T14:30:22.083Z

---
+++ y3k.near/near_atlas.components.Profile.IntroductionSection/source_code.jsx
@@ -0,0 +1,26 @@
+return (
+  <>
+    <div className="ml-8">
+      <h2 className="text-2xl font-semibold mb-4">Hi there 👋</h2>
+      <p>
+        Observer07/observer07 is a ✨ indie researcher ✨ and this is where I
+        stash my public code-bases.
+      </p>
+      <p>Here are some areas I dabble with:</p>
+      <ul className="list-disc ml-5 space-y-1">
+        <li>
+          🔭 I’m currently working on an ETL library for educational purposes in
+          data engineering.
+        </li>
+        <li>
+          🌱 I’m currently learning my way into OSCP (Offensive Security
+          Certification Program)
+        </li>
+        <li>👯 I’m looking to collaborate on ML and OSINT Tools.</li>
+        <li>🤔 I’m looking for help with Chess? I'm learning Chess.</li>
+        <li>💬 Ask me about Podcast world.</li>
+        <li>📫 How to reach me: email.</li>
+      </ul>
+    </div>
+  </>
+);
