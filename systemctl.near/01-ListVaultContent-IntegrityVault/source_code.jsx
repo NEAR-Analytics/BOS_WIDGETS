7ce9944fd4f739d1a7dd58e91b294b6486b7f05e
systemctl.near <systemctl.near>
2023-11-17 21:47:27 +0000

    Update 01-ListVaultContent-IntegrityVault by systemctl.near at 2023-11-17T21:47:27.345Z

--- systemctl.near/01-ListVaultContent-IntegrityVault/source_code.jsx
+++ systemctl.near/01-ListVaultContent-IntegrityVault/source_code.jsx
@@ -4,7 +4,6 @@ const profile = Social.getr(`${accountId}/profile`); // Get Profile Data
 const entry =
   props.entry ?? Social.getr(`${accountId}/postEvidenceData`, blockHeight); // Get last Evidence Entry
 
-
 // Styled component (For evidence)
 const Wrapper = styled.div`
   width: 100%;
@@ -97,53 +96,61 @@ const profileLink = (c) => (
   </a>
 );
 
+const listEvidenceData = []; // Array vacio
+
+const indexData = Social.keys(`${accountId}/postEvidenceData`, "final", {
+  return_type: "History",
+});
+
+indexData[accountId].postEvidenceData.forEach((ii) => {
+  let singleEntry = Social.getr(`${accountId}/postEvidenceData`, ii);
+  listEvidenceData.push(singleEntry);
+});
+
+// console.log(listEvidenceData)
+
 // Evidence
 return (
-
   <div style={{ maxWidth: "100%" }}>
     {/* Profile Wiget */}
     <Widget src={"mob.near/widget/ProfileLarge"} />
-    <div
-      className="d-flex align-items-start"
-      style={{
-        padding: "1.5rem 0",
-        borderTop: "1px solid #e9e9e9",
-        borderBottom: "1px solid #e9e9e9",
-      }}
-    >
 
-      {/* Account ID */}
+    {/* Evidence container */}
+    <div className="d-flex align-items-start">
       <div className="ms-2 flex-grow-1" style={{ minWidth: 0 }}>
-        <div className="d-flex justify-content-start">
+        {/* Iterate over listEvidenceData and render each entry */}
+        {listEvidenceData.reverse().map((entry, index) => (
           <div
-            className="flex-grow-1 me-1 text-truncate"
-            style={{ padding: "0 0 0 1.5rem" }}
+            key={index}
+            style={{
+              padding: "1.5rem 0",
+              borderTop: "1px solid #e9e9e9",
+              borderBottom: "1px solid #e9e9e9",
+            }}
           >
-            {profileLink(
-              <>
-                <span className="text-secondary">@{accountId}</span>
-              </>
-            )}
+            {/* Entry Title */}
+            <div style={{ padding: "1rem 0 0 1.5rem" }}>
+              {entry.data.title && <b>Title: {entry.data.title}</b>}
+            </div>
+
+            {/* Entry Date */}
+            <div style={{ padding: "1rem 0 0 1.5rem" }}>
+              {entry.id && <span>Entry ID: {entry.id}</span>}
+            </div>
+
+            {/* Entry observations */}
+            <div style={{ padding: "1rem 0 0 1.5rem" }}>
+              {entry.data.context && <span>Obs: {entry.data.context}</span>}
+            </div>
+
+            {/* Evidence URL */}
+            <div style={{ padding: "1rem 0 0 1.5rem" }}>
+              {entry.data.fileURL && (
+                <a href={entry.data.fileURL}>IPFS URL: {entry.data.fileURL}</a>
+              )}
+            </div>
           </div>
-        </div>
-
-        {/* Entry Title */}
-        <div style={{ padding: "1rem 0 0 1.5rem" }}>
-          {entry.data.title && <b>Title: {entry.data.title}</b>}
-        </div>
-
-        {/* Entry observations */}
-        <div style={{ padding: "1rem 0 0 1.5rem" }}>
-          {entry.data.context && <span>Obs: {entry.data.context}</span>}
-        </div>
-
-        {/* Evidence URL */}
-        <div style={{ padding: "1rem 0 0 1.5rem" }}>
-          {entry.data.fileURL && (
-            <a href={entry.data.fileURL}>IPFS URL: {entry.data.fileURL}</a>
-          )}
-        </div>
-
+        ))}
       </div>
     </div>
   </div>
