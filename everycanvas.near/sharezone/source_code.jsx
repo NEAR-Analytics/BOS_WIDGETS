4d4be3b1843db6f2ba2d48b3bd5e84c9e4c61392
everycanvas.near <everycanvas.near>
2023-11-18 05:24:32 +0000

    Update sharezone by everycanvas.near at 2023-11-18T05:24:32.139Z

---
+++ everycanvas.near/sharezone/source_code.jsx
@@ -0,0 +1,111 @@
+import context from "react-bootstrap/esm/AccordionContext";
+
+const ModalBackdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1001;
+`;
+
+const ModalBox = styled.div`
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+  z-index: 1002;
+`;
+
+const ModalHeader = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`;
+
+const CloseButton = styled.button`
+  background: #f44336;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 15px;
+  cursor: pointer;
+  float: right;
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 300px;
+  padding: 10px;
+`;
+
+function Modal({ onClose, children }) {
+  return (
+    <ModalBackdrop>
+      <ModalBox>
+        <ModalHeader>
+          <CloseButton onClick={onClose}>Close</CloseButton>
+        </ModalHeader>
+        <ModalContent>{children}</ModalContent>
+      </ModalBox>
+    </ModalBackdrop>
+  );
+}
+
+const {
+  getSelectedShapes,
+  getSnapshot,
+  deleteShapes,
+  getShapePageBounds,
+  createShapeId,
+  createShape,
+  updateShape,
+  asSvg,
+  asPng,
+  asDataUrl,
+  snapshot,
+} = props;
+
+const [isModalOpen, setModalOpen] = useState(false);
+
+const save = () => {
+  Social.set({
+    thing: {
+      canvas: {
+        "": JSON.stringify(getSnapshot()),
+        metadata: {
+          type: "canvas",
+        },
+      },
+    },
+  });
+};
+
+const Button = styled.button`
+  padding: 10px 20px;
+`;
+
+const toggleModal = () => {
+  setModalOpen(!isModalOpen);
+};
+
+return (
+  <>
+    {context.accountId && (
+      <Button className="classic" onClick={save}>
+        <i class="bi bi-save"></i> save canvas
+      </Button>
+    )}
+    {isModalOpen && (
+      <Modal onClose={toggleModal}>
+        <Widget src="hack.near/widget/create.hyperfile" />
+        <button onClick={save}>save</button>
+      </Modal>
+    )}
+  </>
+);
