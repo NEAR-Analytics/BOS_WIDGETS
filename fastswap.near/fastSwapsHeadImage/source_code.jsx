a94efddf6d814f0f7f5285d19abf5ccfd66bed52
fastswap.near <fastswap.near>
2023-11-18 09:53:24 +0000

    Update fastSwapsHeadImage by fastswap.near at 2023-11-18T09:53:24.757Z

---
+++ fastswap.near/fastSwapsHeadImage/source_code.jsx
@@ -0,0 +1,12 @@
+return (
+  <img
+    src="https://emerald-personal-constrictor-170.mypinata.cloud/ipfs/Qmamxy98ZHrcbhuujcq9GjajFdoMAibwE9Ziw2HVgwypWY"
+    alt="Descripción"
+    style={{
+      display: "block",
+      marginLeft: "auto",
+      marginRight: "auto",
+      width: "50%",
+    }}
+  />
+);
