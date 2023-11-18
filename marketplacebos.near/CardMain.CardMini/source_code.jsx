5bcecfdac15213a328bf6d538f919e7732014a36
marketplacebos.near <marketplacebos.near>
2023-11-18 15:53:45 +0000

    Update CardMain.CardMini by marketplacebos.near at 2023-11-18T15:53:45.970Z

--- marketplacebos.near/CardMain.CardMini/source_code.jsx
+++ marketplacebos.near/CardMain.CardMini/source_code.jsx
@@ -136,23 +136,6 @@ const toggleSaveButton1 = () => {
   setIsToggleSave1((prevState) => !prevState); // Toggle the save button state
 };
 
-const SaveButton = styled.button`
-  width: 2.3rem;
-  height: 30px;
-  background-color: ${(props) => (props.isToggled ? "#f79a55" : "black")};
-  border: none;
-  border-radius: 24px;
-  cursor: pointer;
-  position: absolute;
-  bottom: 10px; /* Adjust the vertical position */
-  left: 10px; /* Adjust the horizontal position */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: bold;
-  color: white;
-  transition: background-color 300ms;
-`;
 
 const ViewButton = styled.button`
   width: 5rem;
@@ -194,13 +177,45 @@ const Edit = styled.button`
   }
 `;
 
-const SaveIcon = (
-  <i
-    className={
-      isToggleSave1 ? "bi bi-bookmark-heart-fill" : "bi bi-bookmark-heart"
+const Container = styled.label`
+  margin-top:20px;
+  display: block;
+  position: relative;
+  width:30px;
+  cursor: pointer;
+  user-select: none;
+  position: absolute;
+  bottom: 2px; 
+  left: 10px; 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  & input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  & svg {
+    position: relative;
+    top: 0;
+    left: 0;
+    height: 50px;
+    width: 50px;
+    transition: all 0.3s;
+    fill: #666;
+
+    &:hover {
+      transform: scale(1.1);
     }
-  ></i>
-);
+  }
+
+  & input:checked ~ svg {
+    fill: #E3474F;
+  }
+`;
 
 const copyBtn = props.copyBtn || "Copy Button";
 const component = props.component || "";
@@ -212,10 +227,13 @@ return (
   <Card isWhiteBackground={!isWhiteBackground1}>
     <CardBody>
       <CenteredCardWrapper>{component}</CenteredCardWrapper>
-      <SaveButton onClick={toggleSaveButton1} isToggled={isToggleSave1}>
-        {SaveIcon}
-        {save}
-      </SaveButton>
+         <Container>
+            <input type="checkbox" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"/>
+            <path d="M16.4,4C14.6,4,13,4.9,12,6.3C11,4.9,9.4,4,7.6,4C4.5,4,2,6.5,2,9.6C2,14,12,22,12,22s10-8,10-12.4C22,6.5,19.5,4,16.4,4z"></path>
+            </svg>
+            
+        </Container>
       <a href={edit}>
         <Edit
           type="button"
