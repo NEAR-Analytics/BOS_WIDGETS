cad6ae7594b8c18962f053333f66c1750c8e360a
andyh.near <andyh.near>
2023-11-18 01:04:23 +0000

    Update Simple by andyh.near at 2023-11-18T01:04:23.950Z

--- andyh.near/Simple/source_code.jsx
+++ andyh.near/Simple/source_code.jsx
@@ -1,6 +1,6 @@
 const Hello = () => <span id="hello-span">hello</span>;
 const World = () => (
-  <div id="world-root">
+  <>
     <div id="world-parent-div">
       <span id="world-parent-span">
         <Hello id="hello-component" />
@@ -8,7 +8,7 @@ const World = () => (
     </div>
     world
     {/*<h2 id="world-h2">world</h2>*/}
-  </div>
+  </>
 );
 
 return (
