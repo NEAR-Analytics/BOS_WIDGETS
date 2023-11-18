2d87997748b53224ea13d9dd45f09bdbc3028e0d
mugdha.near <mugdha.near>
2023-11-18 12:54:32 +0000

    Update 12th.graders. by mugdha.near at 2023-11-18T12:54:32.357Z

--- mugdha.near/12th.graders./source_code.jsx
+++ mugdha.near/12th.graders./source_code.jsx
@@ -1,3 +1,6 @@
+import styled from "styled-components";
+import { State, Storage, Widget } from "..."; // Import necessary libraries
+
 State.init({
   selectedTab: Storage.privateGet("selectedTab") || "all",
 });
@@ -7,229 +10,132 @@ const daoId = props.daoId ?? "12th graders";
 const domains = ["stress", "chem", "phy", "bio", "math", "fun", "memes"];
 const hashtags = ["fun", "study"];
 
-// Remove the following concept; set accounts to null or a suitable default
-let accounts = null; // Assuming this will fetch posts from all users
+let accounts = null; // Fetch posts from all users
 
 function selectTab(selectedTab) {
   Storage.privateSet("selectedTab", selectedTab);
   State.update({ selectedTab });
 }
 
+// Styled components
 const H2 = styled.h2`
-  font-size: 19px;
-  line-height: 22px;
-  color: #11181c;
-  margin: 0 0 24px;
-  padding: 0 24px;
-
-  @media (max-width: 1200px) {
-    display: none;
-  }
+  font-size: 20px;
+  color: #333;
+  margin-bottom: 20px;
 `;
 
 const Content = styled.div`
-  @media (max-width: 1200px) {
-    > div:first-child {
-      border-top: none;
-    }
-  }
+  margin: 20px;
 `;
 
 const ComposeWrapper = styled.div`
-  border-top: 1px solid #eceef0;
+  border-top: 1px solid #ccc;
+  padding: 20px;
 `;
 
 const FilterWrapper = styled.div`
-  border-top: 1px solid #eceef0;
-  padding: 24px 24px 0;
+  padding: 20px;
   display: flex;
-  flex-direction: row;
   justify-content: space-between;
-
-  @media (max-width: 1200px) {
-    padding: 12px;
-  }
+  align-items: center;
 `;
 
 const PillSelect = styled.div`
-  display: inline-flex;
+  display: flex;
   align-items: center;
-
-  @media (max-width: 600px) {
-    width: 100%;
-
-    button {
-      flex: 1;
-    }
-  }
 `;
 
 const PillSelectButton = styled.button`
-  display: block;
-  position: relative;
-  border: 1px solid #e6e8eb;
-  border-right: none;
-  padding: 3px 24px;
-  border-radius: 0;
-  font-size: 12px;
-  line-height: 18px;
-  color: ${(p) => (p.selected ? "#fff" : "#687076")};
-  background: ${(p) => (p.selected ? "#006ADC !important" : "#FBFCFD")};
-  font-weight: 600;
-  transition: all 200ms;
+  padding: 10px 20px;
+  margin-right: 10px;
+  border: 1px solid #ccc;
+  background-color: ${(props) => (props.selected ? "#006ADC" : "#FFF")};
+  color: ${(props) => (props.selected ? "#FFF" : "#000")};
+  cursor: pointer;
 
   &:hover {
-    background: #ecedee;
-    text-decoration: none;
+    background-color: #eceef0;
   }
 
   &:focus {
     outline: none;
-    border-color: #006adc !important;
-    box-shadow: 0 0 0 1px #006adc;
-    z-index: 5;
-  }
-
-  &:first-child {
-    border-radius: 6px 0 0 6px;
-  }
-  &:last-child {
-    border-radius: 0 6px 6px 0;
-    border-right: 1px solid #e6e8eb;
   }
 `;
 
 const FeedWrapper = styled.div`
-  .post {
-    padding-left: 24px;
-    padding-right: 24px;
-
-    @media (max-width: 1200px) {
-      padding-left: 12px;
-      padding-right: 12px;
-    }
-  }
+  padding: 20px;
 `;
 
 const Text = styled.p`
-  font-family: "FK Grotesk", sans-serif;
-  font-size: ${(p) => p.size ?? "14px"};
-  line-height: ${(p) => p.lineHeight ?? "1.5"};
-  font-weight: ${(p) => p.weight ?? "400"};
-  color: ${(p) => p.color ?? "#000"};
-  margin: 0;
-  max-width: 670px;
-
-  @media (max-width: 768px) {
-    font-size: 16px;
-  }
+  color: #666;
+  font-size: 16px;
 `;
 
 const TextLarge = styled.p`
-  font-family: "FK Grotesk", sans-serif;
-  font-size: ${(p) => p.size ?? "18px"};
-  line-height: ${(p) => p.lineHeight ?? "1.5"};
-  font-weight: ${(p) => p.weight ?? "400"};
-  color: ${(p) => p.color ?? "#000"};
-  margin: 0;
-  max-width: 670px;
-
-  @media (max-width: 768px) {
-    font-size: 16px;
-  }
+  color: #666;
+  font-size: 18px;
+  text-align: center;
 `;
 
 const Container = styled.div`
-  display: flex;
-  max-width: 1080px;
-  margin: 0 auto;
-  gap: var(--section-gap);
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  padding: var(--section-gap) 24px;
-
-  @media (max-width: 768px) {
-    padding: var(--section-gap) 12px;
-  }
+  max-width: 800px;
+  margin: auto;
+  padding: 20px;
 `;
 
 const Flex = styled.div`
-  gap: 8px;
-  align-items: center;
+  display: flex;
   flex-direction: column;
-  flex-wrap: "nowrap";
-
-  @media (max-width: 998px) {
-    flex-direction: column;
-    gap: var(--section-gap);
-  }
+  align-items: center;
 `;
 
+// Component return
 return (
   <>
     {context.accountId ? (
-      <Content>
-        <H2>Post</H2>
-        <ComposeWrapper>
-          <Widget src="hack.near/widget/create.posts" props={{ domains }} />
-        </ComposeWrapper>
+      <>
+        <Content>
+          <H2>Post</H2>
+          <ComposeWrapper>
+            <Widget src="hack.near/widget/create.posts" props={{ domains }} />
+          </ComposeWrapper>
+        </Content>
 
-        <H2>Explore</H2>
-        <div className="mt-3 mb-3">
-          <Text>Check out what the other 12th graders are up to</Text>
-        </div>
-
-        <FilterWrapper>
-          <PillSelect>
-            <PillSelectButton
-              type="button"
-              onClick={() => selectTab("all")}
-              selected={state.selectedTab === "all"}
-            >
-              All
-            </PillSelectButton>
-          </PillSelect>
-          <div className="d-inline-flex gap-2">
-            <Typeahead
-              options={hashtags}
-              multiple
-              onChange={(value) => {
-                State.update({ hashtags: value });
-              }}
-              placeholder="Hashtag filter"
-            />
-            <Typeahead
-              options={domains}
-              multiple
-              onChange={(value) => {
-                State.update({ choose: value });
-              }}
-              placeholder="Domain filter"
-            />
+        <Content>
+          <H2>Explore</H2>
+          <div>
+            <Text>Check out what the other 12th graders are up to</Text>
           </div>
-        </FilterWrapper>
 
-        <FeedWrapper>
-          <Widget
-            src="hack.near/widget/view.posts"
-            props={{
-              accounts,
-              domains: state.choose,
-              hashtags: state.hashtags,
-            }}
-          />
-        </FeedWrapper>
-      </Content>
+          <FilterWrapper>
+            <PillSelect>
+              <PillSelectButton
+                onClick={() => selectTab("all")}
+                selected={state.selectedTab === "all"}
+              >
+                All
+              </PillSelectButton>
+            </PillSelect>
+          </FilterWrapper>
+
+          <FeedWrapper>
+            <Widget
+              src="hack.near/widget/view.posts"
+              props={{
+                accounts,
+                domains: state.choose,
+                hashtags: state.hashtags,
+              }}
+            />
+          </FeedWrapper>
+        </Content>
+      </>
     ) : (
       <Container>
         <Flex>
-          <div className="mt-3">
-            <TextLarge>
-              Please log in to post and explore chat by 12th graders.
-            </TextLarge>
-          </div>
+          <TextLarge>
+            Log in with your wallet to post and explore content by 12th graders.
+          </TextLarge>
         </Flex>
       </Container>
     )}
