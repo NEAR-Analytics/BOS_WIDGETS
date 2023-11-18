9051576daca458a2546e8b3ba0c995b0891a2826
beachsunandrockandroll.near <beachsunandrockandroll.near>
2023-11-18 12:58:33 +0000

    Update utils by beachsunandrockandroll.near at 2023-11-18T12:58:33.144Z

---
+++ beachsunandrockandroll.near/utils/source_code.jsx
@@ -0,0 +1,10 @@
+const getNormalDate = (value) =>
+  new Date(parseInt(value.toString() + "000")).toLocaleString();
+
+const ppdContract = "0x4D8354Eda3c1Cd9D85E44C63535468Ec3EaF876C";
+
+const ppdAbi = fetch(
+  "https://raw.githubusercontent.com/gonzalobarria/testpub/master/abis/abitest.json"
+);
+
+return { getNormalDate, ppdContract, ppdAbi };
