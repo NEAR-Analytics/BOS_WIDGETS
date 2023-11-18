2eaab1560aae83bfa33d499d740ec617ec9c0fbd
beachsunandrockandroll.near <beachsunandrockandroll.near>
2023-11-18 19:58:30 +0000

    Update utils by beachsunandrockandroll.near at 2023-11-18T19:58:30.466Z

--- beachsunandrockandroll.near/utils/source_code.jsx
+++ beachsunandrockandroll.near/utils/source_code.jsx
@@ -1,10 +1,10 @@
 const getNormalDate = (value) =>
   new Date(parseInt(value.toString() + "000")).toLocaleString();
 
-const ppdContract = "0x4D8354Eda3c1Cd9D85E44C63535468Ec3EaF876C";
+const ppdContract = "0xCccE9768EC26AE4f78020B33aF73061df2bCD867";
 
 const ppdAbi = fetch(
-  "https://raw.githubusercontent.com/gonzalobarria/testpub/master/abis/abitest.json"
+  "https://raw.githubusercontent.com/gonzalobarria/prodigy-piano-diary/master/public/abi.json"
 );
 
 const studyType = ["Fingers", "Rhythm", "Memorization", "Dynamics"];
