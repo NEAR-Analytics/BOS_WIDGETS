6c05ecaa94d884196af080834195ff36ae444923
beachsunandrockandroll.near <beachsunandrockandroll.near>
2023-11-18 13:05:04 +0000

    Update userSheets by beachsunandrockandroll.near at 2023-11-18T13:05:04.271Z

--- beachsunandrockandroll.near/userSheets/source_code.jsx
+++ beachsunandrockandroll.near/userSheets/source_code.jsx
@@ -1,18 +1,10 @@
-// const ppdContract = props.ppdContract;
-// const ppdAbi = props.ppdAbi;
-
-const ppdContract = "0x4D8354Eda3c1Cd9D85E44C63535468Ec3EaF876C";
-
-const ppdAbi = fetch(
-  "https://raw.githubusercontent.com/gonzalobarria/testpub/master/abis/abitest.json"
+const { getNormalDate, ppdContract, ppdAbi } = VM.require(
+  "beachsunandrockandroll.near/widget/utils"
 );
 
 State.init({
   userSheets: [],
 });
-
-const getNormalDate = (value) =>
-  new Date(parseInt(value.toString() + "000")).toLocaleString();
 
 const getUserSheets = () => {
   if (ppdContract == undefined) return;
