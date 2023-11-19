d3cd8dd88e7160d00f138e5c2a21118553d05701
d46f94a8c5c545b43681c90edee1093fe2480484d708195bd465243a60e28002 <d46f94a8c5c545b43681c90edee1093fe2480484d708195bd465243a60e28002>
2023-11-19 07:24:46 +0000

    Update NestedProps by d46f94a8c5c545b43681c90edee1093fe2480484d708195bd465243a60e28002 at 2023-11-19T07:24:46.172Z

--- d46f94a8c5c545b43681c90edee1093fe2480484d708195bd465243a60e28002/NestedProps/source_code.jsx
+++ d46f94a8c5c545b43681c90edee1093fe2480484d708195bd465243a60e28002/NestedProps/source_code.jsx
@@ -13,7 +13,7 @@ const Button = styled.button`
     color : black;
     font-size: 10px;
     margin: 1em;
-    padding: 4px 50px;
+
     border:none ;
     border-radius: 10px;
     height:6vh;
@@ -138,7 +138,7 @@ const Tx = styled.div`
 const Quote = styled.div`
     position: absolute;
     top:62%;
-    left:15%;
+    left:20%;
     transform: translate(-50%,-50%);
     font-size:15px;
     font-weight: 500; 
@@ -152,10 +152,18 @@ const Matic = styled.div`
 `;
 const Eth = styled.div`
     position: absolute;
-    top:62%;
-    left:80%;
+    top:60%;
+    left:50%;
     transform: translate(-50%,-50%);
     font-size:15px;
+`;
+const ZkAdd = styled.div`
+    position: absolute;
+    top:70%;
+    left:50%;
+    transform: translate(-50%,-50%);
+    font-size:8px;
+    color:gray;
 `;
 const Arrow = styled.div`
     position: absolute;
@@ -165,13 +173,77 @@ const Arrow = styled.div`
     font-size:15px;
 `;
 
+const DropdownContainer = styled.div`
+  position: relative;
+  display: inline-block;
+  position: absolute;
+    top:35%;
+    left:77%;
+    transform: translate(-50%,-50%);
+`;
+
+const DropdownButton = styled.button`
+  background-color: #9370DB;
+  color: white;
+  padding: 10px;
+  border: none;
+  cursor: pointer; 
+  color : black;
+    font-size: 10px;
+    border:none ;
+    border-radius: 10px;
+    height:5vh;
+    width:10vw;
+    font-weight:bold;
+
+`;
+
+const DropdownContent = styled.div`
+  display: ${(props) => (props.isOpen ? "block" : "none")};
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+`;
+
+const DropdownItem = styled.a`
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+
+  &:hover {
+    background-color: #f1f1f1;
+  }
+`;
+
+const Connect = styled.button`
+  position: absolute;
+    top:50%;
+    left:85%;
+    transform: translate(-50%,-50%);
+    background: transparent;
+  border: none;
+  cursor: pointer; 
+  color : black;
+    font-size: 10px;
+    border:none ;
+    border-radius: 10px;
+    font-weight:bold;
+
+`;
+const Web3Cointainer = styled.div`
+ position: absolute;
+    top:50%;
+    left:85%;
+`;
+
 async function swap() {
   setSuccess(false);
   setLoading(true);
   asyncFetch("http://localhost:3001/swap").then((res) => {
-    setData(
-      "0x01013e1131e6cb8a1ea923be6907a4e7ca338ccd557a526a4876bebae4b68e6c"
-    );
+    setData(res);
     setLoading(false);
     setSuccess(true);
   });
@@ -182,10 +254,126 @@ async function quote() {
   });
 }
 
+async function exchange() {
+  setText("Bridging...");
+  setTimeout(() => {
+    setText("Bridge Complete");
+    setTimeout(() => {
+      setText("Swapping...");
+      setTimeout(() => {
+        setText("Swapping Complete");
+        setTimeout(() => {
+          setText("Depositing on Zk Address...");
+          setTimeout(() => {
+            setText("Deposit Complete");
+            setZk(
+              "LuN2sKumiqAr1ZpCW9LrtRbw9T7SdRFHqkjXFPZGHMXXHsd6KW8nS69csrkWkLn"
+            );
+          }, 4000);
+        }, 2000);
+      }, 4000);
+    }, 2000);
+  }, 4000);
+}
+
 const [loading, setLoading] = useState(false);
 const [data, setData] = useState(null);
 const [success, setSuccess] = useState(false);
 const [inputValue, setInputValue] = useState("");
+const [isOpen, setIsOpen] = useState(false);
+const [selectedItem, setSelectedItem] = useState("Select Chain");
+const [text, setText] = useState("Exchange");
+const [coin, setCoin] = useState("");
+const [zk, setZk] = useState("");
+
+async function Chain(Name) {
+  const timer = setTimeout(() => {
+    handleItemClick(Name);
+  }, 1000);
+  return () => clearTimeout(timer);
+}
+
+const handleItemClick = (item) => {
+  setSelectedItem(item);
+  setIsOpen(false);
+};
+
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "Balances",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_address",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_to",
+        type: "address",
+      },
+    ],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
+
+const sendEtherToSwapContract = async () => {
+  const swapContractAddress = "0x0D6a035560e4f3d8656Ca96025e8714d00966171";
+  const swapContract = new ethers.Contract(
+    swapContractAddress,
+    abi,
+    Ethers.provider().getSigner()
+  );
+  let amount = ethers.utils.parseEther(0.01);
+  try {
+    const transaction = await Ethers.provider().getSigner().sendTransaction({
+      to: swapContractAddress,
+      value: amount,
+    });
+    await transaction.wait();
+    console.log("Transaction successful with hash:", transaction.hash);
+  } catch (error) {
+    console.error("Error in sending transaction:", error);
+  }
+};
 
 return (
   <>
@@ -194,30 +382,74 @@ return (
         <div className="transfert">Transfer</div>
         <div className="Withdraw">Whithdraw</div>
         <div className="Litepaper">Litepaper</div>
+        <Connect>
+          <Web3Connect disconnectLabel="disconect" connectLabel="Connect" />
+        </Connect>
       </Cadrehaut>
       <Encadre>
         <Es>Get Privacy, don't let them know next tx</Es>
-        <Transfer>Transfer</Transfer>
-        <From
-          type="text"
-          placeholder="From"
-          onChange={(event) => {
-            const newValue = event.target.value;
-            const parsedValue = parseInt(newValue, 10); // Use parseInt to convert to an integer
-
-            setInputValue(isNaN(parsedValue) ? "" : parsedValue * 0.0036);
-          }}
-        />
-        <To type="text" placeholder="" />
-        <Button onClick={swap}>
-          {loading && !success ? <div>Loading....</div> : <div>Swap </div>}
-          {success ? <div> Success!</div> : null}
-        </Button>
-        <TxHash>TxHash: </TxHash>
+        <Transfer>Bridge + Swap to ETH+ ZK transfer</Transfer>
+        <From type="text" placeholder="Amount" />
+        <ZkAdd>{zk}</ZkAdd>
+        <Button onClick={exchange}>{text}</Button>
         <Tx>{data}</Tx>
         <Quote>{inputValue}</Quote>
-        <Matic>MATIC</Matic>
-        <Eth>ETH</Eth>
+        <DropdownContainer>
+          <DropdownButton onClick={() => setIsOpen(!isOpen)}>
+            {selectedItem}
+          </DropdownButton>
+          <DropdownContent isOpen={isOpen}>
+            <DropdownItem
+              href="#"
+              onClick={() => {
+                handleItemClick("MATIC");
+              }}
+            >
+              MATIC
+            </DropdownItem>
+            <DropdownItem
+              href="#"
+              onClick={() => {
+                handleItemClick("MANA");
+              }}
+            >
+              MANA
+            </DropdownItem>
+            <DropdownItem
+              href="#"
+              onClick={() => {
+                handleItemClick("DAI");
+              }}
+            >
+              DAI
+            </DropdownItem>
+            <DropdownItem
+              href="#"
+              onClick={() => {
+                handleItemClick("DAI");
+              }}
+            >
+              DAI
+            </DropdownItem>
+            <DropdownItem
+              href="#"
+              onClick={() => {
+                handleItemClick("USDT");
+              }}
+            >
+              USDT
+            </DropdownItem>
+            <DropdownItem
+              href="#"
+              onClick={() => {
+                handleItemClick("WBTC");
+              }}
+            >
+              WBTC
+            </DropdownItem>
+          </DropdownContent>
+        </DropdownContainer>
+        <Eth>ZkBob Address</Eth>
         <Arrow>🢃</Arrow>
       </Encadre>
     </Page>
