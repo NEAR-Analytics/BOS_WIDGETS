4f823eb5e19ba2c5740a1895f7fd85196ae2b4b2
marketplacebos.near <marketplacebos.near>
2023-11-19 12:57:06 +0000

    Update CardMain.CardPattern by marketplacebos.near at 2023-11-19T12:57:06.897Z

--- marketplacebos.near/CardMain.CardPattern/source_code.jsx
+++ marketplacebos.near/CardMain.CardPattern/source_code.jsx
@@ -172,7 +172,7 @@ return (
   <Card>
     <CardBody>
       {component}
-            <Container>
+      <Container>
         <input type="checkbox" />
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -184,7 +184,7 @@ return (
           <path d="M16.4,4C14.6,4,13,4.9,12,6.3C11,4.9,9.4,4,7.6,4C4.5,4,2,6.5,2,9.6C2,14,12,22,12,22s10-8,10-12.4C22,6.5,19.5,4,16.4,4z"></path>
         </svg>
       </Container>
-      
+
       <a href={detailLink}>
         <ViewButton>
           Code<i className="bi bi-code-slash"></i>
