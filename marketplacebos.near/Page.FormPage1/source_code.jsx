827fbfdc90a5d3c97953b458f775a872397e2f6c
marketplacebos.near <marketplacebos.near>
2023-11-19 13:08:58 +0000

    Update Page.FormPage1 by marketplacebos.near at 2023-11-19T13:08:58.015Z

--- marketplacebos.near/Page.FormPage1/source_code.jsx
+++ marketplacebos.near/Page.FormPage1/source_code.jsx
@@ -12,7 +12,7 @@ const CardGrid = styled.div`
 
 const widgetList = [];
 
-for (let i = 1; i <= 3; i++) {
+for (let i = 1; i <= 4; i++) {
   const paddedNumber = i.toString().padStart(4, "0");
   const widgetSrc = `marketplacebos.near/widget/Form.Form${paddedNumber}`;
   widgetList.push(<Widget key={i} src={widgetSrc} />);
