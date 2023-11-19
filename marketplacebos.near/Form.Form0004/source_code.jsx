52ea1866be210f4fb46b69d6c5d163ee0a56c64f
marketplacebos.near <marketplacebos.near>
2023-11-19 17:16:07 +0000

    Update Form.Form0004 by marketplacebos.near at 2023-11-19T17:16:07.251Z

--- marketplacebos.near/Form.Form0004/source_code.jsx
+++ marketplacebos.near/Form.Form0004/source_code.jsx
@@ -26,7 +26,6 @@ const Toggle = styled.button`
 const UploadJson = () => {
   if (state.json.length) {
     const body = new Blob([state.json], { type: "application/json" });
-    console.log(body);
     asyncFetch("https://ipfs.near.social/add", {
       method: "POST",
       headers: {
@@ -35,7 +34,6 @@ const UploadJson = () => {
       body,
     }).then((res) => {
       const cid = res.body.cid;
-      console.log("CID", cid);
       State.update({
         file: {
           cid,
