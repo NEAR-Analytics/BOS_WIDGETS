509b65aaa63c1a8ff7b3026c1b18bc33461b5912
marketplacebos.near <marketplacebos.near>
2023-11-19 17:13:08 +0000

    Update Input.InputP.Input0003 by marketplacebos.near at 2023-11-19T17:13:08.813Z

--- marketplacebos.near/Input.InputP.Input0003/source_code.jsx
+++ marketplacebos.near/Input.InputP.Input0003/source_code.jsx
@@ -1,4 +1,11 @@
-const Grid = styled.div`
+
+  State.init({
+  img: null,
+  color: "black",
+});
+
+
+  const Grid = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   gap: 1.5rem;
@@ -11,25 +18,12 @@ const Label = styled.div`
   font-weight: 500; 
   cursor: not-allowed; 
   opacity: 0.7; `;
-const Input = styled.input`
-  height: 2.5rem; 
-  width: 100%;
-  border: 1px solid #ccc; 
-  border-radius: 0.375rem;
-  background-color: #fff; 
-  padding: 0.75rem 1rem;
-  font-size: 0.875rem;
-  color: #666; 
-  font-weight: 500;
-  outline: none; 
-  transition: background-color 0.7s ease-in-out;
-  &:hover{
-    color:red;
-    background: black;
-  }`;
  return(
         <Grid>
         <Label htmlFor="picture">Picture</Label>
-        <Input id="picture" type="file" />
-        </Grid>  
+        <IpfsImageUpload
+                image={state.img}
+                style={{ color: state.color, borderColor: state.color }}
+              />
+         </Grid>  
     )
