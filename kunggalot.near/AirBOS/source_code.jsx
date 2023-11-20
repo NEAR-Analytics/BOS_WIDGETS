e64db30e2d76cd89adefaa19261945a0d551bc22
kunggalot.near <kunggalot.near>
2023-11-20 10:34:40 +0000

    Update AirBOS by kunggalot.near at 2023-11-20T10:34:40.113Z

---
+++ kunggalot.near/AirBOS/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <>
+    <div class="text-center">
+      <h1>"BITKUB x BOS Hackathon 2023"</h1>
+      <h5>
+        ✷ 　 　　 　 · 　 ˚ * . 　 　　 *　　 * ⋆ 　 . · 　　 ⋆ 　　　 ˚ ˚ 　　
+        ✦ 　 ⋆ · 　 * 　　　　 ⋆ ✧　 　 · 　 ✧　✵ 　 · ✵
+      </h5>
+    </div>
+  </>
+);
