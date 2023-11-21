651f18c8336800b78829f8e39bd506d8cb354c1b
andyh.near <andyh.near>
2023-11-21 19:05:49 +0000

    Update TestGroup by andyh.near at 2023-11-21T19:05:49.045Z

---
+++ andyh.near/TestGroup/source_code.jsx
@@ -0,0 +1,22 @@
+return (
+  <div className="col">
+    <div className="row">
+      <div className="col">
+        <Widget src="andyh.near/Markdown" />
+      </div>
+      <div className="col">
+        <Widget src="andyh.near/Simple" />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <Widget src="andyh.near/BearFacts" />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <Widget src="andyh.near/StateDemo" />
+      </div>
+    </div>
+  </div>
+);
