56fabf701cbd6115f0cff9435deda4dc8db7b648
xvii.near <xvii.near>
2023-11-21 22:35:10 +0000

    Update BOSHACKS.Submit.Main by xvii.near at 2023-11-21T22:35:10.435Z

--- xvii.near/BOSHACKS.Submit.Main/source_code.jsx
+++ xvii.near/BOSHACKS.Submit.Main/source_code.jsx
@@ -1,14 +1,6 @@
 return (
   <div>
     <Widget src="xvii.near/widget/SubmitHeader" />
-    <Widget src="xvii.near/widget/AmeResearchCard" />
-    <Widget src="xvii.near/widget/DabbieResearchCard" />
-    <Widget src="xvii.near/widget/HannahResearchCard" />
-    <Widget src="xvii.near/widget/NedlarResearchCard" />
-    <Widget src="xvii.near/widget/AnnieResearchCard" />
-    <Widget src="xvii.near/widget/xviiResearchCard" />
-    <Widget src="xvii.near/widget/RachelResearchCard" />
-    <Widget src="xvii.near/widget/BGemResearchCard" />
-    <Widget src="xvii.near/widget/RhodaResearchCard" />
+    <Widget src="xvii.near/widget/TopicsSelectedPage" />
   </div>
 );
