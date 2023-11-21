0f96c13189fc1ebfe1044eb534a4e84b90828ec0
andyh.near <andyh.near>
2023-11-21 22:59:36 +0000

    Update Markdown by andyh.near at 2023-11-21T22:59:36.577Z

--- andyh.near/Markdown/source_code.jsx
+++ andyh.near/Markdown/source_code.jsx
@@ -1,23 +1,3 @@
-const [Markdown, setMarkdown] = useState(null);
-
-const loadMarkdown = useCallback(async () => {
-  const mod = await import(
-    "https://esm.sh/marked-react@2.0.0?alias=react:preact/compat"
-  );
-  const pkg = mod.default;
-  debugger;
-  setMarkdown(pkg);
-});
-
-useEffect(() => {
-  loadMarkdown();
-}, []);
-
-console.log(Markdown);
-if (!Markdown) {
-  return <div>Markdown loading...</div>;
-}
-
 return (
   <div id="r">
     <Markdown id="a"># does this *work*?</Markdown>
