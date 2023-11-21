f0601d8b3846325b26489ac2e2fc1632659318bf
andyh.near <andyh.near>
2023-11-21 23:56:26 +0000

    Update UseRef by andyh.near at 2023-11-21T23:56:26.269Z

--- andyh.near/UseRef/source_code.jsx
+++ andyh.near/UseRef/source_code.jsx
@@ -1,8 +1,10 @@
+const [text, setText] = useState(null);
+
 return (
   <div className="row">
     <div className="col">
-      <textarea />
+      <textarea onChange={(e) => console.log(e)} />
     </div>
-    <div className="col">hey</div>
+    <div className="col">{text}</div>
   </div>
 );
