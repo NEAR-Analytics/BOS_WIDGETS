badf298d14622f04d587c7bb06820d5c75f28de5
everycanvas.near <everycanvas.near>
2023-11-21 03:27:08 +0000

    Update create.hyperfile by everycanvas.near at 2023-11-21T03:27:08.118Z

--- everycanvas.near/create.hyperfile/source_code.jsx
+++ everycanvas.near/create.hyperfile/source_code.jsx
@@ -1,77 +1,43 @@
-const Wrapper = styled.div`
-  max-width: 400px;
-  margin: 0 auto;
-`;
 
-const TabContent = styled.div`
-  margin-top: 1rem;
-`;
+props.fileType ||
+  initState({
+    json: props.data ?? "",
+    name: "canvas" // temp
+  });
 
-const Form = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-  width: 100%;
-`;
+const ipfsUrl = (cid) => `https://ipfs.near.social/ipfs/${cid}`;
 
-const Label = styled.label`
-  font-weight: bold;
-`;
+const UploadJson = () => {
+  if (state.json.length) {
+    const body = new Blob([state.json], { type: "application/json" });
+    console.log(body);
+    asyncFetch("https://ipfs.near.social/add", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+      body,
+    }).then((res) => {
+      const cid = res.body.cid;
+      console.log("CID", cid);
+      State.update({
+        file: {
+          cid,
+        },
+      });
+    });
+  } else {
+    State.update({
+      file: null,
+    });
+  }
+};
 
-const Input = styled.input`
-  padding: 5px;
-`;
-
-const Select = styled.select`
-  padding: 8px;
-`;
-
-const FormGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const adapters = [
-  // these can come from the user (or app) settings
-  // {
-  //   title: "Local Storage",
-  //   value: "everycanvas.near/widget/adapter.local_storage",
-  //   saveRef: false
-  // },
-  // {
-  //   title: "SocialDB",
-  //   value: "everycanvas.near/widget/adapter.social",
-  // },
-  {
-    title: "IPFS",
-    value: "everycanvas.near/widget/adapter.ipfs",
-  },
-  // {
-  //   title: "GitHub",
-  //   value: "hack.near/widget/adapter.github",
-  // },
-  // {
-  //   title: "Obsidian",
-  //   value: "hack.near/widget/adapter.obsidian",
-  // },
-  // {
-  //   title: "Tldraw",
-  //   value: "hack.near/widget/adapter.tldraw",
-  // },
-];
-
-const defaultAdapter = adapters[0];
-
-const { creatorId } = props;
-
-const [json, setJson] = useState(props.data ?? "");
-const [source, setSource] = useState(props.source ?? "");
-const [adapter, setAdapter] = useState(defaultAdapter.value ?? "");
-const [reference, setReference] = useState(undefined);
-const [filename, setFilename] = useState(props.filename ?? "");
-const [activeTab, setActiveTab] = useState("data");
-const [name, setName] = useState(props.name ?? "");
-const [description, setDescription] = useState(props.description ?? "");
+const onChangeName = (name) => {
+  State.update({
+    name,
+  });
+};
 
 function generateUID() {
   return (
@@ -81,151 +47,209 @@ function generateUID() {
   );
 }
 
+const thingId = state.name ?? generateUID();
+
 const handleCreate = () => {
-  const isCreator = context.accountId === creatorId;
-
-  // load in the state.adapter (modules for IPFS, Arweave, Ceramic, Verida, On Machina... )
-  const { create } = VM.require(adapter) || (() => {});
-  if (create) {
-    // store the data somewhere, based on the adapter
-    create(json).then((reference) => {
-      // now we have a reference to the data
-      // we need to name it... are we the original creator or are we forking? We don't want to overwrite any of the users custom (or maybe we do!)
-      const thingId = filename ?? generateUID();
-
-      const hyperfile = {
-        [props.type]: {
-          // which we store in the social contract
-          [thingId]: {
-            "": JSON.stringify({
-              fileformat: `${props.type}.${source}`,
-              source: source,
-              adapter: adapter,
-              reference: reference,
-            }),
-            metadata: {
-              name: name,
-              description: description,
-              type: props.type,
-            },
+  // load in the state.adapter
+  // const { create } = VM.require(state.adapter);
+  // const hyperfile = create({ cid: state.file.cid });
+  // console.log("hyperfile", hyperfile);
+  const hyperfile = {
+    thing: {
+      [thingId]: {
+        "": JSON.stringify({
+          fileformat: props.fileformat,
+          source: "IPFS", // state.source
+          adapter: "everycanvas.near/widget/adapter.ipfs", // state.adapter
+          reference: {
+            cid: state.file.cid,
           },
+        }),
+        metadata: {
+          type: props.type,
         },
-      };
+      },
+    },
+  };
 
-      if (creatorId !== context.accountId) {
-        // handle request merge
-        hyperfile.index = {
-          notify: JSON.stringify({
-            key: creatorId,
-            value: {
-              type: "request",
-              data: {
-                type: "merge",
-                upstream: `${creatorId}/${props.type}/${props.filename}`,
-                origin: `${context.accountId}/${props.type}/${thingId}`,
-              },
-            },
-          }),
-        };
-        hyperfile[props.type][thingId].metadata = {
-          ...hyperfile[props.type][thingId].metadata,
-          upstream: `${creatorId}/${props.type}/${props.filename}`,
-        };
-        // I want to make a request to merge
-        // set upstream and downstream
-      }
-
-      // sometimes we're not logged in, so it doesn't do anything!
-      Social.set(hyperfile, { force: true });
-    });
-  }
+  Social.set(hyperfile);
 };
 
 return (
-  <Wrapper>
-    <h3>{context.accountId === creatorId ? "create" : "request merge"}</h3>
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          className={`nav-link ${activeTab === "data" ? "active" : ""}`}
-          onClick={() => setActiveTab("data")}
-        >
-          Data
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          className={`nav-link ${activeTab === "metadata" ? "active" : ""}`}
-          onClick={() => setActiveTab("metadata")}
-        >
-          Metadata
-        </a>
-      </li>
-    </ul>
-
-    <TabContent>
-      {activeTab === "data" && (
-        <Form>
-          <FormGroup>
-            <Label>source</Label>
-            <Input
-              type="text"
-              value={source}
-              onChange={(e) => onChangeSource(e.target.value)}
-              disabled={props.source} // disable if source is passed in
-            />
-          </FormGroup>
-          {/* <Widget
-            src="bozon.near/widget/CodeDiff"
-            props={{ currentCode: update, prevCode: src, ...props }}
-          /> */}
-          <textarea
-            className="form-control mb-3"
-            rows={5}
-            value={json}
-            onChange={(e) => setJson(e.target.value)}
-          />
-          <FormGroup>
-            <Label>adapter</Label>
-            <Select
-              value={adapter}
-              onChange={(e) => setAdapter(e.target.value)}
-            >
-              {adapters.map((o) => (
-                <option value={o.value}>{o.title}</option>
-              ))}
-            </Select>
-          </FormGroup>
-        </Form>
-      )}
-    </TabContent>
-    <TabContent>
-      {activeTab === "metadata" && (
-        <Form>
-          <FormGroup>
-            <Label>name</Label>
-            <Input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label>description</Label>
-            <textarea
-              className="form-control mb-3"
-              rows={5}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </FormGroup>
-        </Form>
-      )}
-    </TabContent>
-    <FormGroup>
-      <button className="btn btn-success mb-1" onClick={handleCreate}>
-        Create
-      </button>
-    </FormGroup>
-  </Wrapper>
+  <>
+    <textarea
+      className="form-control mb-3"
+      rows={5}
+      value={state.json}
+      onChange={(e) => {
+        state.json = e.target.value;
+        State.update();
+      }}
+    />
+    <a type="button" class="btn btn-success" onClick={() => UploadJson()}>
+      Upload
+    </a>
+    <br />
+    {state.file && (
+      <div>
+        <br />
+        Your file:
+        <a href={ipfsUrl(state.file.cid)}>{state.file.cid}</a>
+        <br />
+        {/* <h5 className="mt-3">Name</h5>
+        <input
+          type="text"
+          value={state.name}
+          onChange={(e) => onChangeName(e.target.value)}
+        /> */}
+        <button className="btn btn-outline-success mt-3" onClick={handleCreate}>
+          Create
+        </button>
+      </div>
+    )}
+  </>
 );
+
+
+
+// const accountId = context.accountId;
+
+// if (!accountId) {
+//   return "please connect your NEAR account";
+// }
+
+// const { data, onSubmit } = props;
+
+// const options = [
+//   {
+//     title: "SocialDB",
+//     value: "everycanvas.near/widget/adapter.github",
+//   },
+//   // {
+//   //   title: "GitHub",
+//   //   value: "hack.near/widget/adapter.github",
+//   // },
+//   // {
+//   //   title: "Obsidian",
+//   //   value: "hack.near/widget/adapter.obsidian",
+//   // },
+//   // {
+//   //   title: "Tldraw",
+//   //   value: "hack.near/widget/adapter.tldraw",
+//   // },
+// ];
+
+// const defaultOption = options[0];
+
+// State.init({
+//   source: defaultOption.title,
+//   adapter: defaultOption.value,
+//   reference: data,
+// });
+
+// const Wrapper = styled.div`
+//   max-width: 400px;
+//   margin: 0 auto;
+// `;
+
+// const Form = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 15px;
+// `;
+
+// const Label = styled.label`
+//   font-weight: bold;
+// `;
+
+// const Input = styled.input`
+//   padding: 5px;
+// `;
+
+// const Select = styled.select`
+//   padding: 8px;
+// `;
+
+// const TextArea = styled.textarea`
+//   padding: 5px;
+//   height: 222px;
+//   resize: vertical;
+// `;
+
+// const onChangeSource = (source) => {
+//   State.update({
+//     source,
+//   });
+// };
+
+// const onChangeAdapter = (adapter) => {
+//   State.update({
+//     adapter,
+//   });
+// };
+
+// const onChangeReference = (reference) => {
+//   State.update({
+//     reference,
+//   });
+// };
+
+// // function generateUID() {
+// //   return (
+// //     Math.random().toString(16).slice(2) +
+// //     Date.now().toString(36) +
+// //     Math.random().toString(16).slice(2)
+// //   );
+// // }
+
+// // const thingId = generateUID();
+
+// // const handleCreate = () => {
+// //   const data = {
+// //     thing: {
+// //       [thingId]: {
+// //         source: state.source,
+// //         adapter: state.adapter,
+// //         reference: state.reference,
+// //       },
+// //     },
+// //   };
+
+// //   Social.set(data);
+// // };
+
+// return (
+//   <Wrapper>
+//     <h4 className="mb-3">
+//       <b>hyperfile creator</b>
+//     </h4>
+//     <Form>
+//       {/* <Label>source</Label>
+//       <Input
+//         type="text"
+//         value={state.source}
+//         onChange={(e) => onChangeSource(e.target.value)}
+//       /> */}
+//       <Label>adapter</Label>
+//       <Select
+//         placeholder={placeholder}
+//         value={state.adapter}
+//         onChange={({ target: { value } }) => onChangeAdapter(value)}
+//       >
+//         {options.map((o) => (
+//           <option value={o.value}>{o.title}</option>
+//         ))}
+//       </Select>
+
+//       <Label htmlFor="reference">reference (JSON)</Label>
+//       <TextArea
+//         type="text"
+//         value={state.reference}
+//         onChange={(e) => onChangeReference(e.target.value)}
+//       />
+//       <button className="btn btn-success mt-3" onClick={() => onSubmit(state)}>
+//         create
+//       </button>
+//     </Form>
+//   </Wrapper>
+// );
