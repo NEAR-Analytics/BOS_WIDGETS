600e3933382edf3405eaa41c6241cf41c6da68f5
geforcy.near <geforcy.near>
2023-11-21 16:42:11 +0000

    Update devhub.page.home by geforcy.near at 2023-11-21T16:42:11.316Z

--- geforcy.near/devhub.page.home/source_code.jsx
+++ geforcy.near/devhub.page.home/source_code.jsx
@@ -4,7 +4,7 @@ return (
   <>
     {HomeSections.map((it) => (
       <Widget
-        src={`geforcy.near/widget/devhub.components.island.${it}`}
+        src={`${REPL_DEVHUB}/widget/devhub.components.island.${it}`}
         props={{ ...props }}
       />
     ))}
