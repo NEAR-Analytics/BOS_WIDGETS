4656c1bd314f4928731e880ee5ed69fb656dc09b
geforcy.near <geforcy.near>
2023-11-21 16:42:52 +0000

    Update devhub.page.post by geforcy.near at 2023-11-21T16:42:52.090Z

--- geforcy.near/devhub.page.post/source_code.jsx
+++ geforcy.near/devhub.page.post/source_code.jsx
@@ -12,7 +12,7 @@ const Container = styled.div`
 return (
   <Container>
     <Widget
-      src={"${REPL_DEVHUB}/widget/devhub.entity.post.Post"}
+      src={"geforcy.near/widget/devhub.entity.post.Post"}
       props={{
         id,
       }}
