64612a8349ee676379a2e28f17ec15da60677fd7
andyh.near <andyh.near>
2023-11-22 00:02:51 +0000

    Update UseRef by andyh.near at 2023-11-22T00:02:51.775Z

--- andyh.near/UseRef/source_code.jsx
+++ andyh.near/UseRef/source_code.jsx
@@ -1,9 +1,14 @@
 const [text, setText] = useState(null);
+const textRef = useRef(null);
+
+useEffect(() => {
+  textRef.current.focus();
+}, []);
 
 return (
   <div className="row">
     <div className="col">
-      <textarea onChange={(e) => setText(e.target.value)} />
+      <textarea ref={textRef} onChange={(e) => setText(e.target.value)} />
     </div>
     <div className="col">{text}</div>
   </div>
