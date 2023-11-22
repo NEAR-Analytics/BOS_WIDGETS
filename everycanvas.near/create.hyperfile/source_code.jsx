59ce14ded9ea0de070d12d17f132fa734c74215f
everycanvas.near <everycanvas.near>
2023-11-22 16:15:57 +0000

    Update create.hyperfile by everycanvas.near at 2023-11-22T16:15:57.695Z

--- everycanvas.near/create.hyperfile/source_code.jsx
+++ everycanvas.near/create.hyperfile/source_code.jsx
@@ -1,15 +1,46 @@
-const accountId = context.accountId;
+const Wrapper = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+`;
 
-if (!accountId) {
-  return "please connect your NEAR account";
-}
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  width: 100%;
+`;
 
-const { data, onSubmit } = props;
+const Label = styled.label`
+  font-weight: bold;
+`;
 
-const options = [
+const Input = styled.input`
+  padding: 5px;
+`;
+
+const Select = styled.select`
+  padding: 8px;
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const adapters = [
+  // these can come from the user (or app) settings
+  // {
+  //   title: "Local Storage",
+  //   value: "everycanvas.near/widget/adapter.local",
+  // },
+  // {
+  //   title: "SocialDB",
+  //   value: "everycanvas.near/widget/adapter.social",
+  // },
   {
-    title: "SocialDB",
-    value: "everycanvas.near/widget/adapter.github",
+    title: "IPFS",
+    value: "everycanvas.near/widget/adapter.ipfs",
   },
   // {
   //   title: "GitHub",
@@ -25,116 +56,79 @@ const options = [
   // },
 ];
 
-const defaultOption = options[0];
+const defaultAdapter = adapters[0];
 
-State.init({
-  source: defaultOption.title,
-  adapter: defaultOption.value,
-  reference: data,
-});
+const [json, setJson] = useState(props.data ?? "");
+const [source, setSource] = useState(props.source ?? "");
+const [adapter, setAdapter] = useState(defaultAdapter.value ?? "");
+const [reference, setReference] = useState(undefined);
+const [filename, setFilename] = useState(props.filename ?? "");
 
-const Wrapper = styled.div`
-  max-width: 400px;
-  margin: 0 auto;
-`;
+function generateUID() {
+  return (
+    Math.random().toString(16).slice(2) +
+    Date.now().toString(36) +
+    Math.random().toString(16).slice(2)
+  );
+}
 
-const Form = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-`;
+const thingId = filename ?? generateUID();
 
-const Label = styled.label`
-  font-weight: bold;
-`;
-
-const Input = styled.input`
-  padding: 5px;
-`;
-
-const Select = styled.select`
-  padding: 8px;
-`;
-
-const TextArea = styled.textarea`
-  padding: 5px;
-  height: 222px;
-  resize: vertical;
-`;
-
-const onChangeSource = (source) => {
-  State.update({
-    source,
-  });
+const handleCreate = () => {
+  // load in the state.adapter
+  const { create } = VM.require(adapter) || (() => {});
+  if (create) {
+    create(json).then((reference) => {
+      console.log("reference", reference);
+      const hyperfile = {
+        thing: {
+          [thingId]: {
+            "": JSON.stringify({
+              fileformat: `${props.type}.${source}`,
+              source: source,
+              adapter: adapter,
+              reference: reference,
+            }),
+            metadata: {
+              type: props.type,
+            },
+          },
+        },
+      };
+      // we're not logged in, so it doesn't do anything!
+      Social.set(hyperfile, { force: true });
+    });
+  }
 };
-
-const onChangeAdapter = (adapter) => {
-  State.update({
-    adapter,
-  });
-};
-
-const onChangeReference = (reference) => {
-  State.update({
-    reference,
-  });
-};
-
-// function generateUID() {
-//   return (
-//     Math.random().toString(16).slice(2) +
-//     Date.now().toString(36) +
-//     Math.random().toString(16).slice(2)
-//   );
-// }
-
-// const thingId = generateUID();
-
-// const handleCreate = () => {
-//   const data = {
-//     thing: {
-//       [thingId]: {
-//         source: state.source,
-//         adapter: state.adapter,
-//         reference: state.reference,
-//       },
-//     },
-//   };
-
-//   Social.set(data);
-// };
 
 return (
   <Wrapper>
-    <h4 className="mb-3">
-      <b>hyperfile creator</b>
-    </h4>
     <Form>
-      {/* <Label>source</Label>
-      <Input
-        type="text"
-        value={state.source}
-        onChange={(e) => onChangeSource(e.target.value)}
-      /> */}
-      <Label>adapter</Label>
-      <Select
-        placeholder={placeholder}
-        value={state.adapter}
-        onChange={({ target: { value } }) => onChangeAdapter(value)}
-      >
-        {options.map((o) => (
-          <option value={o.value}>{o.title}</option>
-        ))}
-      </Select>
-
-      <Label htmlFor="reference">reference (JSON)</Label>
-      <TextArea
-        type="text"
-        value={state.reference}
-        onChange={(e) => onChangeReference(e.target.value)}
+      <FormGroup>
+        <Label>source</Label>
+        <Input
+          type="text"
+          value={source}
+          onChange={(e) => onChangeSource(e.target.value)}
+          disabled={props.source} // disable if source is passed in
+        />
+      </FormGroup>
+      <textarea
+        className="form-control mb-3"
+        rows={5}
+        value={json}
+        onChange={(e) => setJson(e.target.value)}
       />
-      <button className="btn btn-success mt-3" onClick={() => onSubmit(state)}>
-        create
+      <FormGroup>
+        <Label>adapter</Label>
+        <Select value={adapter} onChange={(e) => setAdapter(e.target.value)}>
+          {adapters.map((o) => (
+            <option value={o.value}>{o.title}</option>
+          ))}
+        </Select>
+      </FormGroup>
+      <button className="btn btn-success mb-1" onClick={handleCreate}>
+        Create
       </button>
     </Form>
   </Wrapper>
