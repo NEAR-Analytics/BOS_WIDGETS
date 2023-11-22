655ee7a8ef814aba472760aa174cb669d84498aa
andyh.near <andyh.near>
2023-11-22 01:29:55 +0000

    Update Markdown by andyh.near at 2023-11-22T01:29:55.349Z

--- andyh.near/Markdown/source_code.jsx
+++ andyh.near/Markdown/source_code.jsx
@@ -17,8 +17,8 @@ if (!Markdown) {
 
 return (
   <div id="r">
-    <Markdown id="a"># does this *work*?</Markdown>
-    <Markdown id="b">### does it *ever*</Markdown>
-    <Markdown id="c">#### success</Markdown>
+    <Markdown># does this *work*?</Markdown>
+    {/*<Markdown id="b">### does it *ever*</Markdown>
+    <Markdown id="c">#### success</Markdown>*/}
   </div>
 );
