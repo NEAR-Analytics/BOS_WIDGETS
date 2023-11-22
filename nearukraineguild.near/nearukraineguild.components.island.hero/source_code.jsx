dc218caf0cc7de58c5d3dba16ba9fdf5b7aca6f5
nearukraineguild.near <nearukraineguild.near>
2023-11-22 23:26:18 +0000

    Update nearukraineguild.components.island.hero by nearukraineguild.near at 2023-11-22T23:26:18.667Z

--- nearukraineguild.near/nearukraineguild.components.island.hero/source_code.jsx
+++ nearukraineguild.near/nearukraineguild.components.island.hero/source_code.jsx
@@ -5,12 +5,45 @@ href || (href = () => {});
 const imageLink =
   "https://ipfs.near.social/ipfs/bafybeiap2mzwsly4apaldxguiunx4rjwqyadksj5yxuzwrww3kue3ao5qe";
 
+const WrapperSocial = styled.div`
+  display: flex;
+  margin-bottom: 2em;
+  width: 180px;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const SocialText = styled.p`
+  font-family: "Kodchasan", sans-serif;
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 16px;
+  letter-spacing: 0em;
+  text-align: center;
+  color: #ffffff;
+  text-transform: uppercase;
+  margin: 0;
+`;
+
+const SocialLink = styled.a`
+  cursor: pointer;
+  text-align: none;
+  height: 16px;
+  margin: 0;
+  padding: 0;
+`;
+
+const SocialIcon = styled.img`
+  height: 100%;
+  vertical-align: unset;
+`;
+
 const HeroSection = styled.div`
   position: relative;
   height: auto;
   z-index: 3;
   width: 70%;
-  background: #00ec97;
+  background: #011b27;
   clip-path: polygon(0 0, 100% 0%, 75% 100%, 0% 100%);
 
   padding-top: 2rem;
@@ -26,6 +59,7 @@ const HeroSection = styled.div`
 
 const Title = styled.h1`
   color: #f4f4f4;
+  font-family: "Kodchasan", sans-serif;
   font-size: 4rem;
   font-style: normal;
   font-weight: 700;
@@ -41,10 +75,11 @@ const Title = styled.h1`
 
 const Lead = styled.p`
   color: #151515;
+  font-family: "Kodchasan", sans-serif;
   font-size: 1.75rem;
   font-style: normal;
   font-weight: 400;
-  line-height: 110%; /* 39.6px */
+  line-height: 100%; /* 39.6px */
 
   width: 70%;
 
@@ -109,7 +144,7 @@ const Container = styled.div`
   overflow: hidden;
 
   @media screen and (max-width: 768px) {
-    background: #f4f4f4;
+    background: #ffcb00;
   }
 `;
 
@@ -171,24 +206,38 @@ return (
   <Container>
     <HeroSection>
       <Title>
-        The decentralized <br />
-        <span style={{ color: "#101820" }}>home base</span> <br />
+        The biggest <br />
+        <span style={{ color: "#fff" }}>Near guild</span> <br />
         for NEAR builders
       </Title>
       <DesktopDescription>
-        <Lead>
+        <SocialText>
           Join a vibrant community of innovators shaping the open web.
-        </Lead>
-        <Link
-          to={href({
-            widgetSrc: "devgovgigs.near/widget/app",
-            params: {
-              page: "about",
-            },
-          })}
-        >
-          <CTA href="#">Read more →</CTA>
-        </Link>
+        </SocialText>
+        <WrapperSocial>
+          <SocialText>Follow us</SocialText>
+          <SocialLink href="https://twitter.com/nearuaguild" target="_blank">
+            <SocialIcon
+              src="https://ipfs.near.social/ipfs/bafkreibhvlipldq5qnolfb74ufbgqkbcwlim5vvtk3mbz6ujvbsar6fesq"
+              alt="Twitter"
+            />
+          </SocialLink>
+          <SocialLink href="https://t.me/nearprotocolua" target="_blank">
+            <SocialIcon
+              src="https://ipfs.near.social/ipfs/bafkreihcqu65spu6o5z6vw5atbjx7iqphzvlss3hvz4l7bj3syhvavzf5a"
+              alt="Telegram"
+            />
+          </SocialLink>
+          <SocialLink
+            href="https://near.org/near/widget/ProfilePage?accountId=nearukraineguild.near"
+            target="_blank"
+          >
+            <SocialIcon
+              src="https://ipfs.near.social/ipfs/bafkreier4aong3uumu4ndl6iahol2kgeisfqtl6c237x3q34ql6smjvare"
+              alt="Near Social"
+            />
+          </SocialLink>
+        </WrapperSocial>
       </DesktopDescription>
     </HeroSection>
     <MobileImage src={imageLink} />
@@ -197,16 +246,30 @@ return (
     </ImageContainer>
     <MobileDescription>
       <Lead>Join a vibrant community of innovators shaping the open web.</Lead>
-      <Link
-        to={href({
-          widgetSrc: "devgovgigs.near/widget/app",
-          params: {
-            page: "about",
-          },
-        })}
-      >
-        <CTA href="#">Read more →</CTA>
-      </Link>
+      <WrapperSocial>
+        <SocialText>Follow us</SocialText>
+        <SocialLink href="https://twitter.com/nearuaguild" target="_blank">
+          <SocialIcon
+            src="https://ipfs.near.social/ipfs/bafkreibhvlipldq5qnolfb74ufbgqkbcwlim5vvtk3mbz6ujvbsar6fesq"
+            alt="Twitter"
+          />
+        </SocialLink>
+        <SocialLink href="https://t.me/nearprotocolua" target="_blank">
+          <SocialIcon
+            src="https://ipfs.near.social/ipfs/bafkreihcqu65spu6o5z6vw5atbjx7iqphzvlss3hvz4l7bj3syhvavzf5a"
+            alt="Telegram"
+          />
+        </SocialLink>
+        <SocialLink
+          href="https://near.org/near/widget/ProfilePage?accountId=nearukraineguild.near"
+          target="_blank"
+        >
+          <SocialIcon
+            src="https://ipfs.near.social/ipfs/bafkreier4aong3uumu4ndl6iahol2kgeisfqtl6c237x3q34ql6smjvare"
+            alt="Near Social"
+          />
+        </SocialLink>
+      </WrapperSocial>
     </MobileDescription>
   </Container>
 );
