87b90f75207bb5f7e805d2221a7d330a7873766a
espen.near <espen.near>
2023-11-22 17:13:06 +0000

    Update devhub.page.feed by espen.near at 2023-11-22T17:13:06.606Z

--- espen.near/devhub.page.feed/source_code.jsx
+++ espen.near/devhub.page.feed/source_code.jsx
@@ -1,6 +1,6 @@
 const { author, recency, tag } = props;
 
-const { href } = VM.require("${REPL_DEVHUB}/widget/core.lib.url");
+const { href } = VM.require("espen.near/widget/core.lib.url");
 
 if (!href) {
   return <p>Loading modules...</p>;
@@ -44,20 +44,20 @@ const Gradient = styled.div`
 const FeedPage = ({ recency, tag }) => {
   return (
     <div className="w-100">
-      <Widget src={`${REPL_DEVHUB}/widget/devhub.components.island.banner`} />
+      <Widget src={`espen.near/widget/devhub.components.island.banner`} />
       <Widget
-        src={"${REPL_DEVHUB}/widget/devhub.feature.post-search.panel"}
+        src={"espen.near/widget/devhub.feature.post-search.panel"}
         props={{
           hideHeader: false,
           children: (
             <Widget
               src={
-                "${REPL_DEVHUB}/widget/devhub.components.molecule.PostControls"
+                "espen.near/widget/devhub.components.molecule.PostControls"
               }
               props={{
                 title: "Post",
                 href: href({
-                  widgetSrc: "${REPL_DEVHUB}/widget/app",
+                  widgetSrc: "espen.near/widget/app",
                   params: { page: "create" },
                 }),
               }}
