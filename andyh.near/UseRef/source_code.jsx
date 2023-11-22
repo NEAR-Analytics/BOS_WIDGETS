17a418259f71babbc73ea248f9ccda406976918e
andyh.near <andyh.near>
2023-11-22 00:03:57 +0000

    Update UseRef by andyh.near at 2023-11-22T00:03:57.643Z

--- andyh.near/UseRef/source_code.jsx
+++ andyh.near/UseRef/source_code.jsx
@@ -10,6 +10,9 @@ return (
     <div className="col">
       <textarea ref={textRef} onChange={(e) => setText(e.target.value)} />
     </div>
+    <div className="col">
+      <button onClick={() => textRef.current.focus()}>focus</button>
+    </div>
     <div className="col">{text}</div>
   </div>
 );
