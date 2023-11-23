4fc3b5b6ac2e0350aca322e4cde16b7516bb8628
communityevent.near <communityevent.near>
2023-11-23 17:06:52 +0000

    Update city by communityevent.near at 2023-11-23T17:06:52.364Z

---
+++ communityevent.near/city/source_code.jsx
@@ -0,0 +1,34 @@
+const city = props.city;
+
+State.init({
+  show_voters: false,
+});
+
+const contract = "communityevent.near";
+const handleClickYes = () => {
+  Near.call(contract, "vote_yes", { city_name: city[0] });
+};
+
+const isSignedIn = context.accountId;
+
+return (
+  <div>
+    <h2> {city[0]} </h2>
+    <p>votes: {city[1].votes} </p>
+    <button
+      className="btn-view"
+      onClick={() => State.update({ show_voters: !state.show_voters })}
+    >
+      View voters
+    </button>
+    <div>{state.show_voters && <p>{city[1].accounts.join(" ")} </p>}</div>
+
+    <button
+      className="btn-yes"
+      onClick={() => handleClickYes()}
+      disabled={!isSignedIn}
+    >
+      Upvote
+    </button>
+  </div>
+);
