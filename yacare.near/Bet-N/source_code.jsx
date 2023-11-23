0ac7beb3aff9f095b64a907969c7021be1856ded
yacare.near <yacare.near>
2023-11-23 18:54:36 +0000

    Update Bet-N by yacare.near at 2023-11-23T18:54:36.250Z

---
+++ yacare.near/Bet-N/source_code.jsx
@@ -0,0 +1,33 @@
+const amount = props.amount ?? "1";
+
+function toYoctoNear(amountInNear) {
+  let point = amountInNear.indexOf(".");
+  let big, small;
+
+  if (point === -1) {
+    big = amountInNear;
+    small = "";
+  } else {
+    big = amountInNear.slice(0, point);
+    small = amountInNear.slice(point + 1);
+  }
+
+  if (small.length > 24) {
+    small = small.slice(0, 24);
+  } else {
+    small = small + "0".repeat(24 - small.length);
+  }
+
+  let yoctoNear = big + small;
+  return yoctoNear;
+}
+
+function bet() {
+  Near.call("simplebet.near", "bet", {}, "15000000000000", toYoctoNear(amount));
+}
+
+return (
+  <div>
+    <button onClick={() => bet()}>Bet {amount}N</button>
+  </div>
+);
