aa30f8d9b7279c85bca78da017e7e5f39e9a0c2d
communityevent.near <communityevent.near>
2023-11-23 17:38:40 +0000

    Update community_event by communityevent.near at 2023-11-23T17:38:40.036Z

--- communityevent.near/community_event/source_code.jsx
+++ communityevent.near/community_event/source_code.jsx
@@ -1,48 +1,15 @@
-const WrapperContainer = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
-  background-color: #f4f4f4;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-`;
-
-const Title = styled.h1`
-  color: #00EC97;
-  margin-bottom: 10px;
-  text-align: center;
-`;
-
-const Description = styled.p`
-  margin: 10px 0;
-  color: #151515;
-  text-align: center;
-`;
-
-const WidgetContainer = styled.div`
-  margin-top: 20px;
-`;
-
 return (
-  <WrapperContainer>
-    <Title>Community Event?</Title>
-    <Description>Would you go to a monthly NEAR community event?</Description>
-    <Description>
+  <div>
+    <h1>Community event?</h1>
+    <p>Would you go to a monthly NEAR community event?</p>
+    <p>
       If YES: Enter the city you would like to attend the event in and click
       SUBMIT.
-    </Description>
-    <Description>If NO: Click NO</Description>
-    <Description>
-      You may enter multiple cities and browse existing votes.
-    </Description>
-    <WidgetContainer>
-      <Widget src="communityevent.near/widget/vote_count" />
-    </WidgetContainer>
-    <WidgetContainer>
-      <Widget src="communityevent.near/widget/vote" />
-    </WidgetContainer>
-    <WidgetContainer>
-      <Widget src="communityevent.near/widget/city_list" />
-    </WidgetContainer>
-  </WrapperContainer>
+    </p>
+    <p>If NO: Click NO</p>
+    <p>You may enter multiple cities and browse existing votes.</p>
+    <Widget src="communityevent.near/widget/vote_count" />
+    <Widget src="communityevent.near/widget/vote" />
+    <Widget src="communityevent.near/widget/city_list" />
+  </div>
 );
