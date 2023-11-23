2074f653a5f197b6b4ff4af4c785a55eb15b0260
thepivortex.near <thepivortex.near>
2023-11-23 17:36:12 +0000

    Update community_event by thepivortex.near at 2023-11-23T17:36:12.412Z

---
+++ thepivortex.near/community_event/source_code.jsx
@@ -0,0 +1,15 @@
+return (
+  <div>
+    <h1>Community event?</h1>
+    <p>Would you go to a monthly NEAR community event?</p>
+    <p>
+      If YES: Enter the city you would like to attend the event in and click
+      SUBMIT.
+    </p>
+    <p>If NO: Click NO</p>
+    <p>You may enter multiple cities.</p>
+    <Widget src="communityevent.near/widget/vote_count" />
+    <Widget src="communityevent.near/widget/vote" />
+    <Widget src="communityevent.near/widget/city_list" />
+  </div>
+);
