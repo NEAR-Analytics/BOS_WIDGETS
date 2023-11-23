850fdc35ddef255329075ebc3c89b7e779ce69db
communityevent.near <communityevent.near>
2023-11-23 16:54:36 +0000

    Update city by communityevent.near at 2023-11-23T16:54:36.255Z

--- communityevent.near/city/source_code.jsx
+++ communityevent.near/city/source_code.jsx
@@ -14,13 +14,14 @@ const isSignedIn = context.accountId;
 return (
   <div>
     <h2> {city[0]} </h2>
+    <p>votes: {city[1].votes} </p>
     <button
       className="btn-view"
       onClick={() => State.update({ show_voters: !state.show_voters })}
     >
       View voters
     </button>
-    <div>{state.show_voters && <p>{city[1]} </p>}</div>
+    <div>{state.show_voters && <p>{city[1].accounts} </p>}</div>
 
     <button
       className="btn-yes"
