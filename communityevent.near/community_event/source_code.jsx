761cff48bfcddba2d549785b41e8f72db04fefe2
communityevent.near <communityevent.near>
2023-11-23 17:38:40 +0000

    Update community_event by communityevent.near at 2023-11-23T17:38:40.036Z

--- communityevent.near/community_event/source_code.jsx
+++ communityevent.near/community_event/source_code.jsx
@@ -1,9 +1,13 @@
 return (
   <div>
     <h1>Community event?</h1>
-    <p>Would you go to a monthly NEAR community event? Yes or No?</p>
-    <p>If yes enter the city you would like to attend the event in.</p>
-    <p>You may enter multiple cities.</p>
+    <p>Would you go to a monthly NEAR community event?</p>
+    <p>
+      If YES: Enter the city you would like to attend the event in and click
+      SUBMIT.
+    </p>
+    <p>If NO: Click NO</p>
+    <p>You may enter multiple cities and browse existing votes.</p>
     <Widget src="communityevent.near/widget/vote_count" />
     <Widget src="communityevent.near/widget/vote" />
     <Widget src="communityevent.near/widget/city_list" />
