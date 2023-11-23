af55cd77226428ba735132825c484ba1e783de86
pivortex.near <pivortex.near>
2023-11-23 17:22:19 +0000

    Update city by pivortex.near at 2023-11-23T17:22:19.387Z

---
+++ pivortex.near/city/source_code.jsx
@@ -0,0 +1,42 @@
+const city = props.city;
+
+State.init({
+  showVoters: false,
+});
+
+const contract = "communityevent.near";
+const handleClickYes = () => {
+  Near.call(contract, "vote_yes", { city_name: city[0] });
+};
+
+const isSignedIn = context.accountId;
+
+return (
+  <div>
+    <h2> {city[0]} </h2>
+    <p>votes: {city[1].votes} </p>
+    <button
+      className="btn-view"
+      onClick={() => State.update({ showVoters: !state.showVoters })}
+    >
+      View voters
+    </button>
+    <div>
+      {state.showVoters && (
+        <p>
+          {city[1].accounts.map((account, index) => (
+            <div key={index}>{account}</div>
+          ))}
+        </p>
+      )}
+    </div>
+
+    <button
+      className="btn-yes"
+      onClick={() => handleClickYes()}
+      disabled={!isSignedIn}
+    >
+      Upvote
+    </button>
+  </div>
+);
