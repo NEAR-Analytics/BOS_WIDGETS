b50731df35019ce6baf4de08d00260627552d0a0
communityevent.near <communityevent.near>
2023-11-23 15:25:01 +0000

    Update community_event by communityevent.near at 2023-11-23T15:25:01.027Z

---
+++ communityevent.near/community_event/source_code.jsx
@@ -0,0 +1,13 @@
+return (
+  <div>
+    <h1>Community event?</h1>
+    <p>
+      Would you go to a monthly NEAR community event? If yes where would you
+      like to see the event take place?
+    </p>
+
+    <Widget src="communityevent.near/widget/vote_count" />
+
+    <Widget src="communityevent.near/widget/vote" />
+  </div>
+);
