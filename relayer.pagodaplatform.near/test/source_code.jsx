cb24123315d7c3bba3c4c96015465492ef3c5c62
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-11-23 20:14:11 +0000

    Update test by relayer.pagodaplatform.near at 2023-11-23T20:14:11.995Z

--- relayer.pagodaplatform.near/test/source_code.jsx
+++ relayer.pagodaplatform.near/test/source_code.jsx
@@ -1,47 +1,26 @@
-const xSocialCardData = {
-  name: "PorXD",
-  role: "Driver",
-  nftImage: {
-    // contractId: "porx-dev.near",
-    tokenId: "50",
-  },
-  email: "porxdev@mail.com",
-  social: {
-    linkedin: props.social?.linkedin,
-    twitter: props.social?.twitter || "https://twitter.com/nearprotocol",
-    instagram:
-      props.social?.instagram || "https://www.instagram.com/near_protocol",
-    phone: props.social?.phone,
-  },
-};
-
 State.init({
-  name: "",
+  value: "value to update",
 });
 
-let input = "";
+console.log(Near.view("nearsocialexamples.near", "get_greeting"));
 
-const handleChange = (v) => {
+const testCall = () => {
+  return Near.call("nearsocialexamples.near", "set_greeting", {
+    message: "Hi Near Social",
+  });
+};
+
+const testView = () => {
   State.update({
-    name: v,
+    value: Near.view("nearsocialexamples.near", "get_greeting"),
   });
 };
 
 return (
-  <>
-    <p>Hello Near</p>
-    <input
-      onChange={({ target }) => {
-        handleChange(target.value);
-      }}
-    />
-    <Widget src="evangel.near/widget/connect" props={{}} />
-    <Widget
-      src="ahnff.near/widget/SocialCard"
-      props={{
-        ...xSocialCardData,
-        name: State.name,
-      }}
-    />
-  </>
+  <div>
+    <button onClick={testCall}>test call</button>
+    <button onClick={testView}>test view</button>
+
+    <div>{state.value}</div>
+  </div>
 );
