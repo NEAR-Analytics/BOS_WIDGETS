76ead4074c78d634cd6f8e72055d5617ac3dc902
communityevent.near <communityevent.near>
2023-11-23 15:54:06 +0000

    Update city_list by communityevent.near at 2023-11-23T15:54:06.180Z

--- communityevent.near/city_list/source_code.jsx
+++ communityevent.near/city_list/source_code.jsx
@@ -1,11 +1,1 @@
-const contract = "communityevent.near";
-const cities = Near.view(contract, "get_cities", {});
-
-return (
-  <div>
-    {cities.map((city) => (
-      <div key={city[0]}>1</div>
-    ))}
-    dsf
-  </div>
-);
+return <div>asdf </div>;
