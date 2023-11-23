43c8f764536c10deba9da2e70db6c05eff469058
masterclem.near <masterclem.near>
2023-11-23 11:08:31 +0000

    Update dedeukwushryne-home by masterclem.near at 2023-11-23T11:08:31.419Z

--- masterclem.near/dedeukwushryne-home/source_code.jsx
+++ masterclem.near/dedeukwushryne-home/source_code.jsx
@@ -1,10 +1,6 @@
 return (
   <>
     <div className="bg-secondary text-light w-100 h-100 d-flex justify-contents-center align-items-center flex-column p-5">
-      <img
-        width="200px"
-        src="https://www.dedeukwushryne.com/wp-content/uploads/2023/07/My-logo-1.png"
-      />
       <h1>Dedeukwushryne</h1>
       <p>Bridging the gap between creativity and heritage</p>
     </div>
