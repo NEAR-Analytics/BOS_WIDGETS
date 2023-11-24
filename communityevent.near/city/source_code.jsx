8fab52d048e564a64a932bf7d3de43475bc23b51
communityevent.near <communityevent.near>
2023-11-24 14:26:58 +0000

    Update city by communityevent.near at 2023-11-24T14:26:58.200Z

--- communityevent.near/city/source_code.jsx
+++ communityevent.near/city/source_code.jsx
@@ -1,42 +1,78 @@
-const city = props.city;
+const Container = styled.div`
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin-bottom: 20px;
+  background-color: #f9f9f9;
+`;
 
-State.init({
-  showVoters: false,
-});
+const Title = styled.h2`
+  color: #333;
+`;
 
-const contract = "communityevent.near";
-const handleClickYes = () => {
-  Near.call(contract, "vote_yes", { city_name: city[0] });
+const Votes = styled.p`
+  color: #555;
+`;
+
+const ViewButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  padding: 8px 16px;
+  margin-top: 10px;
+  cursor: pointer;
+`;
+
+const VotersList = styled.p`
+  margin-top: 10px;
+`;
+
+const VoterItem = styled.div`
+  margin-bottom: 5px;
+`;
+
+const UpvoteButton = styled.button`
+  background-color: #28a745;
+  color: #fff;
+  padding: 8px 16px;
+  margin-top: 10px;
+  cursor: pointer;
+`;
+
+const YourComponent = (props) => {
+  const city = props.city;
+
+  State.init({
+    showVoters: false,
+  });
+
+  const contract = "communityevent.near";
+
+  const handleClickYes = () => {
+    Near.call(contract, "vote_yes", { city_name: city[0] });
+  };
+
+  const isSignedIn = context.accountId;
+
+  return (
+    <Container>
+      <Title>{city[0]}</Title>
+      <Votes>votes: {city[1].votes}</Votes>
+      <ViewButton
+        onClick={() => State.update({ showVoters: !state.showVoters })}
+      >
+        View voters
+      </ViewButton>
+      <div>
+        {state.showVoters && (
+          <VotersList>
+            {city[1].accounts.map((account, index) => (
+              <VoterItem key={index}>{account}</VoterItem>
+            ))}
+          </VotersList>
+        )}
+      </div>
+      <UpvoteButton onClick={() => handleClickYes()} disabled={!isSignedIn}>
+        Upvote
+      </UpvoteButton>
+    </Container>
+  );
 };
-
-const isSignedIn = context.accountId;
-
-return (
-  <div>
-    <h2> {city[0]} </h2>
-    <p>votes: {city[1].votes} </p>
-    <button
-      className="btn-view"
-      onClick={() => State.update({ showVoters: !state.showVoters })}
-    >
-      View voters
-    </button>
-    <div>
-      {state.showVoters && (
-        <p>
-          {city[1].accounts.map((account, index) => (
-            <div key={index}>{account}</div>
-          ))}
-        </p>
-      )}
-    </div>
-
-    <button
-      className="btn-yes"
-      onClick={() => handleClickYes()}
-      disabled={!isSignedIn}
-    >
-      Upvote
-    </button>
-  </div>
-);
