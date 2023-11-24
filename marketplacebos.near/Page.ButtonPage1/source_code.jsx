b7003cac0399fcd2b42abc131c440491a8b3303f
marketplacebos.near <marketplacebos.near>
2023-11-24 05:33:05 +0000

    Update Page.ButtonPage1 by marketplacebos.near at 2023-11-24T05:33:05.663Z

--- marketplacebos.near/Page.ButtonPage1/source_code.jsx
+++ marketplacebos.near/Page.ButtonPage1/source_code.jsx
@@ -7,6 +7,19 @@ const CardGrid = styled.div`
   overflow-x: auto;
   padding-bottom: 20px;
   white-space: nowrap;
+    @media screen and (max-width: 868px) {
+    grid-template-columns: repeat(2, 1fr); /* Change to 2 columns on smaller screens */
+  }
+      @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr); /* Change to 2 columns on smaller screens */
+  }
+
+  @media screen and (max-width: 480px) {
+    grid-template-columns: 1fr; /* Change to 1 column on even smaller screens */
+  }
+    @media screen and (max-width: 580px) {
+    grid-template-columns: 1fr; /* Change to 1 column on even smaller screens */
+  }
 `;
 
 const Text = styled.p`
