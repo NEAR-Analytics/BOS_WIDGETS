82d03c16c84c49e89ea2e8e9d7bceb3b5a02f50a
marketplacebos.near <marketplacebos.near>
2023-11-24 05:40:31 +0000

    Update Page.InputPage1 by marketplacebos.near at 2023-11-24T05:40:31.960Z

--- marketplacebos.near/Page.InputPage1/source_code.jsx
+++ marketplacebos.near/Page.InputPage1/source_code.jsx
@@ -3,11 +3,23 @@ const CardGrid = styled.div`
   grid-template-columns: repeat(3, 1fr);
   gap: 20px;
   margin: 20px;
+  padding:10px;
   overflow-x: auto;
   padding-bottom: 20px;
   white-space: nowrap;
-  padding:10px;
+    @media screen and (max-width: 868px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+      @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr); 
+  }
 
+  @media screen and (max-width: 480px) {
+    grid-template-columns: 1fr; 
+  }
+    @media screen and (max-width: 580px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 const widgetList = [];
