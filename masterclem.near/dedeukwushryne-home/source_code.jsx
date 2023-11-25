a81177b353ce0f7fc2c0c83d7d7a58e88c5fa05f
masterclem.near <masterclem.near>
2023-11-25 13:43:10 +0000

    Update dedeukwushryne-home by masterclem.near at 2023-11-25T13:43:10.978Z

--- masterclem.near/dedeukwushryne-home/source_code.jsx
+++
@@ -1,8 +0,0 @@
-return (
-  <>
-    <div className="bg-secondary text-light w-100 h-100 d-flex justify-contents-center align-items-center flex-column p-5">
-      <h1>Dedeukwushryne</h1>
-      <p>Bridging the gap between creativity and heritage</p>
-    </div>
-  </>
-);
