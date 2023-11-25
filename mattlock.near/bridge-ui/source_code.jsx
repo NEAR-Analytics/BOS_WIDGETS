ca3251662ccd677b314c4a1177034c0d1b7597c5
mattlock.near <mattlock.near>
2023-11-25 12:35:48 +0000

    Update bridge-ui by mattlock.near at 2023-11-25T12:35:48.752Z

--- mattlock.near/bridge-ui/source_code.jsx
+++ mattlock.near/bridge-ui/source_code.jsx
@@ -5,105 +5,74 @@
   "log": "The TX hash is: 0x2c5d223e47ecd9ac68fbdcd3eeb2bc4615ce6f7209d295104131c1440056497e Etherscan",
   "explorerLink": "https://etherscan.io/tx/123",
   "title": "zkBridge",
-  "deposit": defaultDeposit,
-  "withdraw": defaultWithdraw,
+  "deposit": {
+    "network": {
+      "id": "eth-testnet",
+      "name": "Ethereum Goerli"
+    },
+    "assets": [
+      {
+        "id": "eth",
+        "name": "ETH",
+        "balance": "123.22"
+      },
+      {
+        "id": "usdc",
+        "name": "USDC",
+        "selected": true,
+        "balance": "42.00"
+      }
+    ]
+  },
+  "withdraw": {
+    "network": {
+      "id": "zksync-testnet",
+      "name": "zkSync Era Testnet"
+    },
+    "assets": [
+      {
+        "id": "eth",
+        "name": "ETH",
+        "balance": "0.123"
+      },
+      {
+        "id": "usdc",
+        "name": "USDC",
+        "selected": true,
+        "balance": "0.42"
+      }
+    ]
+  },
   "amount": "0.1"
 }
 */
 
-const defaultDeposit = {
-  network: {
-    id: "l1",
-    name: "Ethereum",
-  },
-  assets: [
-    {
-      id: "eth",
-      name: "ETH",
-      selected: true,
-      balance: "0.00",
-    },
-    {
-      id: "usdc",
-      name: "USDC",
-      selected: false,
-      balance: "0.00",
-    },
-  ],
-};
-const defaultWithdraw = {
-  network: {
-    id: "l2",
-    name: "zkSync Era",
-  },
-  assets: [
-    {
-      id: "eth",
-      name: "ETH",
-      selected: false,
-      balance: "0.00",
-    },
-    {
-      id: "usdc",
-      name: "USDC",
-      selected: true,
-      balance: "0.00",
-    },
-  ],
-};
-
 const {
   deposit,
   withdraw,
-  allWithdrawals,
-  allDeposits,
-  depositDisabled,
-  withdrawDisabled,
   onTabChange,
   onAction,
   title,
   isLoading,
   log,
   explorerLink,
-  tokens,
-  L1ExplorerLink,
-  L2ExplorerLink,
-  withdrawalActions,
+  defaultSelectedAsset,
 } = props;
-if (!tokens) {
-  tokens = {
-    eth: {
-      icon: "https://assets.coingecko.com/coins/images/279/standard/ethereum.png?1696501628",
-      decimals: 18,
-    },
-    usdc: {
-      icon: "https://assets.coingecko.com/coins/images/6319/standard/usdc.png?1696506694",
-      decimals: 6,
-    },
-  };
-}
-if (!deposit) deposit = defaultDeposit;
-if (!withdraw) withdraw = defaultWithdraw;
-if (!allWithdrawals) allWithdrawals = [];
-if (!allDeposits) allDeposits = [];
-const { action, amount } = state;
+const { action, amount, selectedAsset } = state;
 const { assets } = deposit;
+
 const isDeposit = !action || action === "deposit";
 const actionTitle = isDeposit ? "Deposit" : "Withdraw";
 
-if (assets && !state.selectedAsset) {
+if (assets && !selectedAsset) {
   initState({
-    selectedAsset: assets.find((a) => a.selected) || assets?.[0],
-    isTokenListOpen: false,
+    selectedAsset:
+      assets.find((a) => a.selected) || assets?.[defaultSelectedAsset || 0],
   });
 }
 
-const selectedAsset = state.selectedAsset
-  ? assets?.find((a) => a.id === state.selectedAsset.id)
-  : undefined;
-
-const selectedAssetWithdraw = state.selectedAsset
-  ? withdraw?.assets?.find((a) => a.id === state.selectedAsset.id)
+const selectedAssetWithdraw = selectedAsset
+  ? withdraw?.assets?.find((a) => a.id === selectedAsset.id)
   : undefined;
 
 const handleAction = () => {
@@ -135,99 +104,55 @@ const handleTabChange = (tab) => {
   if (onTabChange) onTabChange(tab);
 };
 
-const Theme = styled.div`
-  --bg-color: #181a27;
-  --border-color: #2c334b;
-  --label-color: #82a7ff;
-  --chain-name-color: #fff;
-  --input-border-color: #332c4b;
-  --button-color: #004bfc;
-  --button-text-color: #fff;
-  --thirdary-text-color: #7c7f96;
-  --arrow-color: #82a7ff;
-  --swap-icon-color: #787da1;
-  --tx-button-color: #64b5ff;
-  --processing-color: #979abe;
-  --success-color: #1abd00;
-  --dialog-bg-color: #373a53;
-  --dialog-info-color: #ff61d3;
-  --token-list-hover-color: rgba(24, 26, 39, 0.3);
-
-
+const Container = styled.div`
     max-width: 400px;
     width: 100%;
     margin: 0 auto;
     display: flex;
     flex-direction: column;
     justify-content: center;
+    border: 1px solid gray;
     padding-top: 1rem;
     border-radius: 0.5rem;
-    border: 1px solid var(--border-color);
     margin-top: 1rem;
-    background: var(--bg-color);
-    color: white;
-
-  .tokens {
-    .selected {
-      position: relative;
-      min-width: 108px;
-    }
-    .token-list {
-      min-width: 108px;
-      position: absolute;
-      top: 8px;
-      left: 8px;
-    }
-    .token-row {
-      display: flex;
-      align-items: center;
-      padding: 8px;
-      > img {
-        width: 32px;
-        margin-right: 8px;
-      }
-      &:hover {
-        border-color: var(--bs-btn-active-bg);
-        background-color: var(--dialog-bg-color);
-        border-radius: 8px;
-      }
-      &:nth-child(2) {
-        margin-top: 16px;
-      }
-    }
-    .selected, .token-list {
-      background: var(--bg-color);
-      border: 1px solid var(--border-color);
-      border-radius: 8px;
-    }
-  }
-
 
     * {
         font-family: 'Inter custom',sans-serif;
     }
+
+    background: white;
+    color: black;
 
     .title {
       margin-top: 8px;
     }
 
     .actionTabs {
-      background-color: var(--bs-btn-active-bg);
-      border: 1px solid var(--border-color);
+      border: 1px solid black;
       height: 38px;
 
       input:checked + label {
-        color: var(--button-text-color);
+        color: black;
+      }
+
+      label {
+        color: gray;
+        background: white !important;
+        border: 1px solid black !important;
+        height: 38px;
+        
+        &:hover {
+          color: black;
+        }
       }
     }
 
-    button {
-      border: 1px solid var(--border-color);
-      color: var(--button-text-color);
-      background-color: var(--bs-btn-active-bg) !important;
-      &:hover {
-        background-color: var(--border-color) !important;
-      }
+    button.max {
+      border: 1px solid black;
+    }
+
+    button.action {
+      background-color: black;
     }
 
     .action {
@@ -237,11 +162,15 @@ const Theme = styled.div`
 
     .balance {
       input {
-        height: 48px;
-        padding: 8px;
-        background: var(--bg-color);
-        color: white;
-        border: 1px solid var(--border-color);
+        height: 38px;
+        background: #f5f6fd;
+        color: black;
+        border: 1px solid black;
+      }
+      button {
+        height: 38px;
+        background: #f5f6fd;
+        color: black;
       }
     }
 
@@ -251,79 +180,10 @@ const Theme = styled.div`
         color: black;
       }
     }
-
-    .alert {
-      word-break: break-word;
-      background-color: var(--dialog-bg-color);
-      color: white;
-      border-color: rgb(13, 110, 253);
-    }
-
-    .tx-list {
-      max-height: 94px;
-      overflow: hidden;
-      overflow-y: scroll;
-    }
 `;
 
-const renderTxLink = (tx, isL1) => {
-  let link = `https://${
-    network === "testnet" ? "goerli." : ""
-  }explorer.zksync.io/tx/${tx}`;
-  if (L1ExplorerLink || L2ExplorerLink) {
-    link = isL1 ? L1ExplorerLink + tx : L2ExplorerLink + tx;
-  }
-  return (
-    <a href={link} target="_blank">
-      {tx.substring(0, 6)} ... {tx.substring(tx.length - 4)}
-    </a>
-  );
-};
-
-const renderTx = (tx, i, isL1) => {
-  const { transactionHash: h, isEth } = tx;
-  return (
-    <>
-      <p style={{ textAlign: "left" }}>
-        {isEth ? "ETH " : "USDC"}
-        {renderTxLink(h, isL1)}
-        {withdrawalActions.map(
-          ({ labelComplete, completeKey, actionLabel, action }) => {
-            const isComplete = tx[completeKey];
-
-            if (typeof isComplete !== "boolean") return <></>;
-            return (
-              <>
-                {isComplete ? (
-                  <span>{labelComplete}</span>
-                ) : (
-                  <button onClick={() => action(allWithdrawals[i])}>
-                    {actionLabel}
-                  </button>
-                )}
-              </>
-            );
-          }
-        )}
-      </p>
-    </>
-  );
-};
-
-// console.log("deposit", deposit);
-// console.log("withdraw", withdraw);
-// console.log("selectedAsset", selectedAsset);
-// console.log("selectedAssetWithdraw", selectedAssetWithdraw);
-
-const actionDisabled =
-  isLoading ||
-  (actionTitle === "Deposit" && depositDisabled) ||
-  (actionTitle === "Withdraw" && withdrawDisabled);
-
-const { isTokenListOpen } = state;
-
 return (
-  <Theme>
+  <Container>
     <div className="d-flex gap-4 align-items-center mb-3 justify-content-center">
       <h5 className="title">{title || "Bridge"}</h5>
       <div className="actionTabs btn-group" role="group" aria-label="Deposit">
@@ -354,47 +214,22 @@ return (
       </div>
     </div>
     <div className="border border-secondary border-bottom-0 border-light" />
-
     <div className="p-4">
       <div className="d-flex justify-content-between">
         <div className="assets d-flex flex-column gap-2">
           <span>{deposit.network.name}</span>
-          <div className="tokens">
-            <div className="selected">
-              {selectedAsset && (
-                <div
-                  className="token-row"
-                  onClick={() => {
-                    State.update({
-                      isTokenListOpen: !isTokenListOpen,
-                    });
-                  }}
-                >
-                  <img src={tokens[selectedAsset.id].icon} />
-                  <div>{selectedAsset.name}</div>
-                </div>
-              )}
-              {isTokenListOpen && (
-                <div className="token-list">
-                  {assets &&
-                    assets.map((asset) => (
-                      <div
-                        className="token-row"
-                        onClick={() => {
-                          State.update({
-                            selectedAsset: asset,
-                            isTokenListOpen: false,
-                          });
-                        }}
-                      >
-                        <img src={tokens[asset.id].icon} />
-                        <div>{asset.name}</div>
-                      </div>
-                    ))}
-                </div>
-              )}
-            </div>
-          </div>
+          <select
+            className="form-select"
+            aria-label="select asset"
+            onChange={handleAssetChange}
+          >
+            {assets &&
+              assets.map((asset) => (
+                <option value={asset.id} selected={asset.selected}>
+                  {asset.name}
+                </option>
+              ))}
+          </select>
         </div>
         <div className="d-flex flex-column gap-2">
           <div className="d-flex justify-content-between">
@@ -411,57 +246,37 @@ return (
               placeholder="0.00"
               onChange={handleAmountChange}
             />
-            <button onClick={handleMax}>max</button>
+            <button className="btn btn-light btn-sm max" onClick={handleMax}>
+              max
+            </button>
           </div>
         </div>
       </div>
     </div>
-    {false && (
-      <>
-        <div className="border border-secondary border-bottom-0 border-light" />
-        <div className="p-4">
-          <div>
-            Balance on {withdraw.network.name}: {selectedAssetWithdraw.balance}
-          </div>
-        </div>
-      </>
-    )}
+    <div className="border border-secondary border-bottom-0 border-light" />
+    <div className="p-4 d-flex justify-content-between">
+      <div>{withdraw.network.name}</div>
+      <div>Balance: {selectedAssetWithdraw.balance}</div>
+    </div>
     <div className="border border-secondary border-bottom-0 border-light" />
     <div className="p-4 d-grid gap-3">
       <button
         className="action btn btn-primary"
         onClick={handleAction}
-        disabled={actionDisabled}
+        disabled={isLoading}
       >
         {isLoading ? "Loading..." : actionTitle}
       </button>
       {log && (
-        <div className="alert" role="alert">
-          <div>{log}</div>
-          {explorerLink && (
-            <a href={explorerLink} className="alert-link" target="_blank">
-              Block Explorer Link
-            </a>
-          )}
+        <div className="alert alert-success" role="alert">
+          <div className="text-truncate" style={{ maxWidth: 300 }}>
+            {log}
+          </div>
+          <a href={explorerLink} className="alert-link" target="_blank">
+            Etherscan
+          </a>
         </div>
       )}
     </div>
-    <div className="border border-secondary border-bottom-0 border-light" />
-    <div className="p-4 d-grid gap-3">
-      <div>
-        <p style={{ marginTop: 16, fontWeight: "bold" }}>
-          Withdrawals: {allWithdrawals.length}
-        </p>
-        <div className="tx-list">
-          {allWithdrawals.map((tx, i) => renderTx(tx, i, false))}
-        </div>
-        <p style={{ marginTop: 16, fontWeight: "bold" }}>
-          Deposits: {allDeposits.length}
-        </p>
-        <div className="tx-list">
-          {allDeposits.map((tx, i) => renderTx(tx, i, true))}
-        </div>
-      </div>
-    </div>
-  </Theme>
+  </Container>
 );
