7988b94f74d6ba6fa2b43e9c950bf5a3ff5a3c46
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-11-25 16:28:12 +0000

    Update DonationHub.components.templates.AppLayout2 by relayer.pagodaplatform.near at 2023-11-25T16:28:12.546Z

---
+++ relayer.pagodaplatform.near/DonationHub.components.templates.AppLayout2/source_code.jsx
@@ -0,0 +1,54 @@
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+
+  background: #f4f4f4;
+
+  margin-top: calc(-1 * var(--body-top-padding));
+`;
+
+const ContentContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const AppHeader = ({ page }) => (
+  <Widget
+    src="lucus21.near/widget/DonationHub.components.organism.Navbar2"
+    props={{
+      page: page,
+      ...props,
+    }}
+  />
+);
+
+const Footer = (props) => {
+  return (
+    <Widget
+      src="lucus.near/widget/DonationHub.components.organism.NewsLetter"
+      props={{
+        ...props,
+      }}
+    />
+  );
+};
+
+function AppLayout({ page, children }) {
+  return (
+    <>
+      <Container>
+        <AppHeader page={page} />
+        <ContentContainer>{children}</ContentContainer>
+        <Footer page={page} />
+      </Container>
+    </>
+  );
+}
+
+return { AppLayout };
