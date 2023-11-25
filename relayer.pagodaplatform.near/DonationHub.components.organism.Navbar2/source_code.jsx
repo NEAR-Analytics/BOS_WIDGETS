e7637f4b91bcc2548b9b44e93b95040d2b37cb45
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-11-25 16:06:09 +0000

    Update DonationHub.components.organism.Navbar2 by relayer.pagodaplatform.near at 2023-11-25T16:06:09.239Z

---
+++ relayer.pagodaplatform.near/DonationHub.components.organism.Navbar2/source_code.jsx
@@ -0,0 +1,136 @@
+const page = props.page;
+
+const [showMenu, setShowMenu] = useState(false);
+
+const { href: linkHref } = VM.require("devhub.near/widget/core.lib.url");
+
+linkHref || (linkHref = () => {});
+
+const Logo = () => {
+  const Wrapper = styled.div`
+    @media screen and (max-width: 768px) {
+      svg {
+        width: 90px;
+        height: 12px;
+        transform: scale(1.5);
+        margin-left: 1rem;
+      }
+    }
+  `;
+
+  const Title = styled.h1`
+  color: #f4f4f4;
+  font-size: 3rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 80%; /* 88px */
+  letter-spacing: -1.76px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 2.25rem;
+    letter-spacing: -0.72px;
+    margin: 0;
+  }
+`;
+
+  return (
+    <Wrapper>
+      <Link
+        to={linkHref({
+          widgetSrc: "lucus.near/widget/DonationHub",
+          params: { page: "home" },
+        })}
+      >
+        <Title>
+          <span style={{ color: "#000000" }}>Donation</span>
+          <span style={{ color: "#02d767" }}>Hub</span>
+        </Title>
+      </Link>
+    </Wrapper>
+  );
+};
+
+const ProfileIcon = () => {
+  const Wrapper = styled.svg`
+    padding: 0.25rem;
+    @media screen and (max-width: 768px) {
+      display: none;
+    }
+  `;
+  return (
+    <Link
+      to={linkHref({
+        widgetSrc: "lucus.near/widget/DoantionHub",
+        params: { page: "profile", accountId: context.accountId },
+      })}
+    >
+      <Widget
+        src="devhub.near/widget/devhub.components.molecule.ProfileCard"
+        props={{ iconOnly: true, accountId: context.accountId || null }}
+      />
+    </Link>
+  );
+};
+
+const Navbar = styled.div`
+  padding: 1.5rem 3rem;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  background: #f4f4f4;
+
+  @media screen and (max-width: 768px) {
+    padding: 1.875rem 1.375rem;
+  }
+`;
+
+const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1.5rem;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const links = [
+  {
+    title: "Donation feed",
+    href: "feed",
+    links: [],
+  },
+  {
+    title: "about",
+    links: [
+      { title: "mission", href: "about" },
+      { title: "blog", href: "blog" },
+    ],
+  },
+];
+
+return (
+  <Navbar className="position-relative">
+    <Logo />
+    <div className="d-flex gap-3 align-items-center">
+      <LinksContainer>
+        {links.map((link) => (
+          <Widget
+            src="devhub.near/widget/devhub.components.molecule.NavbarDropdown"
+            props={{
+              title: link.title,
+              href: link.href,
+              links: link.links,
+              page: page,
+            }}
+          />
+        ))}
+      </LinksContainer>
+      {context.accountId && <ProfileIcon />}
+    </div>
+  </Navbar>
+);
