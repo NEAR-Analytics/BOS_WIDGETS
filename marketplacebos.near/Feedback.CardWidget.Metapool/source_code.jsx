ab6fc633b19264410b2c05725044439fe4108ee6
marketplacebos.near <marketplacebos.near>
2023-11-26 17:59:19 +0000

    Update Feedback.CardWidget.Metapool by marketplacebos.near at 2023-11-26T17:59:19.397Z

---
+++ marketplacebos.near/Feedback.CardWidget.Metapool/source_code.jsx
@@ -0,0 +1,15 @@
+const props = {
+  widget_id: "metapool",
+  widget_name_link:
+    "https://near.social/marketplacebos.near/widget/Feedback.Widget.Metapool",
+  widget_name: "MetaPool",
+};
+
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/CardMain.CardWidget"
+      props={props}
+    />
+  </>
+);
