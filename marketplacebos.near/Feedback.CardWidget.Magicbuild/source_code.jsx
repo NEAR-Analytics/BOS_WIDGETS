139e3f8b9b310bc17a94d8aecb69206d0d3197f4
marketplacebos.near <marketplacebos.near>
2023-11-26 17:57:42 +0000

    Update Feedback.CardWidget.Magicbuild by marketplacebos.near at 2023-11-26T17:57:42.909Z

---
+++ marketplacebos.near/Feedback.CardWidget.Magicbuild/source_code.jsx
@@ -0,0 +1,15 @@
+const props = {
+  widget_id: "magicbuild",
+  widget_name_link:
+    "https://near.social/marketplacebos.near/widget/Feedback.Widget.Magicbuild",
+  widget_name: "MagicBuild",
+};
+
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/CardMain.CardWidget"
+      props={props}
+    />
+  </>
+);
