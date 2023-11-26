dba984f88fd2868edd148d0cc436443ab71bc041
marketplacebos.near <marketplacebos.near>
2023-11-26 17:15:25 +0000

    Update Feedback.Widget.Nearhorizon by marketplacebos.near at 2023-11-26T17:15:25.644Z

---
+++ marketplacebos.near/Feedback.Widget.Nearhorizon/source_code.jsx
@@ -0,0 +1,13 @@
+const props = {
+  widget_id: "nearhorizon",
+  widget_link: "nearhorizon.near/widget/Index",
+  widget_name: "Near Horizon",
+};
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/Feedback.Widget.Template"
+      props={props}
+    />
+  </>
+);
