294ed4f11fbf8f1b32a4106d9a30693f4eb19f05
marketplacebos.near <marketplacebos.near>
2023-11-26 17:15:06 +0000

    Update Feedback.Widget.Potlock by marketplacebos.near at 2023-11-26T17:15:06.130Z

---
+++ marketplacebos.near/Feedback.Widget.Potlock/source_code.jsx
@@ -0,0 +1,13 @@
+const props = {
+  widget_id: "potlock",
+  widget_link: "potlock.near/widget/Index",
+  widget_name: "PotLock",
+};
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/Feedback.Widget.Template"
+      props={props}
+    />
+  </>
+);
