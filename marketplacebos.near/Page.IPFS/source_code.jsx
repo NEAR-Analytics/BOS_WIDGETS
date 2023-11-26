0bb2a702e980adcc3d4baa0487b2b867a75827dd
marketplacebos.near <marketplacebos.near>
2023-11-26 20:09:20 +0000

    Update Page.IPFS by marketplacebos.near at 2023-11-26T20:09:20.829Z

---
+++ marketplacebos.near/Page.IPFS/source_code.jsx
@@ -0,0 +1,33 @@
+const CardGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  margin: 20px;
+  padding:10px;
+  overflow-x: auto;
+  padding-bottom: 20px;
+  white-space: nowrap;
+    @media screen and (max-width: 868px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+      @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr); 
+  }
+
+  @media screen and (max-width: 480px) {
+    grid-template-columns: 1fr; 
+  }
+    @media screen and (max-width: 580px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const widgetList = [];
+
+for (let i = 3; i <= 4; i++) {
+  const paddedNumber = i.toString().padStart(4, "0");
+  const widgetSrc = `marketplacebos.near/widget/Form.Form${paddedNumber}`;
+  widgetList.push(<Widget key={i} src={widgetSrc} />);
+}
+
+return <CardGrid>{widgetList}</CardGrid>;
