10ed7a5dfd983af915563a5fd0e409a07c4f2b65
marketplacebos.near <marketplacebos.near>
2023-11-26 17:59:38 +0000

    Update Feedback.CardWidget.NearHorizon by marketplacebos.near at 2023-11-26T17:59:38.498Z

---
+++ marketplacebos.near/Feedback.CardWidget.NearHorizon/source_code.jsx
@@ -0,0 +1,15 @@
+const props = {
+  widget_id: "nearhorizon",
+  widget_name_link:
+    "https://near.social/marketplacebos.near/widget/Feedback.Widget.Nearhorizon",
+  widget_name: "Near Horizon",
+};
+
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/CardMain.CardWidget"
+      props={props}
+    />
+  </>
+);
