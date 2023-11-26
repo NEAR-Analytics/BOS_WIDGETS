b9179598a2228234a753bbbc1eb8b282436ef024
marketplacebos.near <marketplacebos.near>
2023-11-26 17:58:06 +0000

    Update Feedback.CardWidget.Devhub by marketplacebos.near at 2023-11-26T17:58:06.684Z

---
+++ marketplacebos.near/Feedback.CardWidget.Devhub/source_code.jsx
@@ -0,0 +1,15 @@
+const props = {
+  widget_id: "devhub",
+  widget_name_link:
+    "https://near.social/marketplacebos.near/widget/Feedback.Widget.Devhub",
+  widget_name: "DevHub",
+};
+
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/CardMain.CardWidget"
+      props={props}
+    />
+  </>
+);
