7ef616152c90c38981e0c903abbe3eb933c32f19
marketplacebos.near <marketplacebos.near>
2023-11-26 18:00:07 +0000

    Update Feedback.CardWidget.Potlock by marketplacebos.near at 2023-11-26T18:00:07.656Z

---
+++ marketplacebos.near/Feedback.CardWidget.Potlock/source_code.jsx
@@ -0,0 +1,15 @@
+const props = {
+  widget_id: "potlock",
+  widget_name_link:
+    "https://near.social/marketplacebos.near/widget/Feedback.Widget.Potlock",
+  widget_name: "PotLock",
+};
+
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/CardMain.CardWidget"
+      props={props}
+    />
+  </>
+);
