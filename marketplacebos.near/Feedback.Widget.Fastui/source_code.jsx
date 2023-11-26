f2d15d46d73b9ad311eae09890f6df8e4d11eb6d
marketplacebos.near <marketplacebos.near>
2023-11-26 17:18:28 +0000

    Update Feedback.Widget.Fastui by marketplacebos.near at 2023-11-26T17:18:28.108Z

---
+++ marketplacebos.near/Feedback.Widget.Fastui/source_code.jsx
@@ -0,0 +1,13 @@
+const props = {
+  widget_id: "fastui",
+  widget_link: "fastui/widget/FastUI",
+  widget_name: "FastUI",
+};
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/Feedback.Widget.Template"
+      props={props}
+    />
+  </>
+);
