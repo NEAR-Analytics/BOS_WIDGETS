0de26e84039857f87bf97bb4250144ff11f32a57
marketplacebos.near <marketplacebos.near>
2023-11-26 17:18:05 +0000

    Update Feedback.Widget.Magicbuild by marketplacebos.near at 2023-11-26T17:18:05.549Z

---
+++ marketplacebos.near/Feedback.Widget.Magicbuild/source_code.jsx
@@ -0,0 +1,13 @@
+const props = {
+  widget_id: "magicbuild",
+  widget_link: "magicbuild.near/widget/app",
+  widget_name: "MagicBuild",
+};
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/Feedback.Widget.Template"
+      props={props}
+    />
+  </>
+);
