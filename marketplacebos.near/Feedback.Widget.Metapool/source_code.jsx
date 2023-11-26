318cf8fdf20604598e3a92576724db3d7e7d20f6
marketplacebos.near <marketplacebos.near>
2023-11-26 17:17:27 +0000

    Update Feedback.Widget.Metapool by marketplacebos.near at 2023-11-26T17:17:27.574Z

---
+++ marketplacebos.near/Feedback.Widget.Metapool/source_code.jsx
@@ -0,0 +1,13 @@
+const props = {
+  widget_id: "metapool",
+  widget_link: "meta-pool-official.near/widget/MetaPoolStakeEth",
+  widget_name: "MetaPool",
+};
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/Feedback.Widget.Template"
+      props={props}
+    />
+  </>
+);
