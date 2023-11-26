533ad7763468a50b4a76c58fe104945a1faf5b94
marketplacebos.near <marketplacebos.near>
2023-11-26 17:58:27 +0000

    Update Feedback.CardWidget.Fastui by marketplacebos.near at 2023-11-26T17:58:27.790Z

---
+++ marketplacebos.near/Feedback.CardWidget.Fastui/source_code.jsx
@@ -0,0 +1,15 @@
+const props = {
+  widget_id: "fastui",
+  widget_name_link:
+    "https://near.social/marketplacebos.near/widget/Feedback.Widget.Fastui",
+  widget_name: "FastUI",
+};
+
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/CardMain.CardWidget"
+      props={props}
+    />
+  </>
+);
