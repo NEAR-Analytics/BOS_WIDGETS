adc3fcd016e9c98dabac59fb6ae8d536cc809f80
marketplacebos.near <marketplacebos.near>
2023-11-26 17:18:57 +0000

    Update Feedback.Widget.Devhub by marketplacebos.near at 2023-11-26T17:18:57.081Z

---
+++ marketplacebos.near/Feedback.Widget.Devhub/source_code.jsx
@@ -0,0 +1,13 @@
+const props = {
+  widget_id: "devhub",
+  widget_link: "devhub.near/widget/app",
+  widget_name: "DevHub",
+};
+return (
+  <>
+    <Widget
+      src="marketplacebos.near/widget/Feedback.Widget.Template"
+      props={props}
+    />
+  </>
+);
