ca152aa16ef6690b7ef1f8e3071c53dce1e4f055
mitchyugan.near <mitchyugan.near>
2023-11-27 12:33:47 +0000

    Update NftArtistPollWebApp by mitchyugan.near at 2023-11-27T12:33:47.820Z

---
+++ mitchyugan.near/NftArtistPollWebApp/source_code.jsx
@@ -0,0 +1,21 @@
+const userId = context.useId;
+
+const HeaderContent = styled.div`
+    width: 100%;
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column
+`;
+
+return (
+  <div>
+    <HeaderContent>
+      <h1>NFT Poll </h1>
+      <p>Vote for your best NFT's </p>
+    </HeaderContent>
+
+
+  </div>
+);
