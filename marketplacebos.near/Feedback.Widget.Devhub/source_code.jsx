bbec08bc3bf5036e48d3e349127a600db8bd863e
marketplacebos.near <marketplacebos.near>
2023-11-27 05:45:24 +0000

    Update Feedback.Widget.Devhub by marketplacebos.near at 2023-11-27T05:45:24.514Z

--- marketplacebos.near/Feedback.Widget.Devhub/source_code.jsx
+++ marketplacebos.near/Feedback.Widget.Devhub/source_code.jsx
@@ -2,6 +2,8 @@ const props = {
   widget_id: "devhub",
   widget_link: "devhub.near/widget/app",
   widget_name: "DevHub",
+  logo_links:
+    "https://ipfs.near.social/ipfs/bafkreido4srg4aj7l7yg2tz22nbu3ytdidjczdvottfr5ek6gqorwg6v74",
 };
 return (
   <>
