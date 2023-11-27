a214d6c1c2dadb2159fca94d66f3eb5cbad4bf53
marketplacebos.near <marketplacebos.near>
2023-11-27 05:47:13 +0000

    Update Feedback.Widget.Fastui by marketplacebos.near at 2023-11-27T05:47:13.840Z

--- marketplacebos.near/Feedback.Widget.Fastui/source_code.jsx
+++ marketplacebos.near/Feedback.Widget.Fastui/source_code.jsx
@@ -2,6 +2,8 @@ const props = {
   widget_id: "fastui",
   widget_link: "fastui/widget/FastUI",
   widget_name: "FastUI",
+  logo_links:
+    "https://ipfs.near.social/ipfs/bafkreidiqkfrp2rmzhc6zsi5actq66y3xczb6nfue3gp64pk6de3p724jm",
 };
 return (
   <>
