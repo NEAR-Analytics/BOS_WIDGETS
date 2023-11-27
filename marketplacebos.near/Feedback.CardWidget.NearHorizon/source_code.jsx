1fb9d27000ce81ab195863952864631c9fc5fa61
marketplacebos.near <marketplacebos.near>
2023-11-27 05:30:01 +0000

    Update Feedback.CardWidget.NearHorizon by marketplacebos.near at 2023-11-27T05:30:01.727Z

--- marketplacebos.near/Feedback.CardWidget.NearHorizon/source_code.jsx
+++ marketplacebos.near/Feedback.CardWidget.NearHorizon/source_code.jsx
@@ -3,6 +3,8 @@ const props = {
   widget_name_link:
     "https://near.social/marketplacebos.near/widget/Feedback.Widget.Nearhorizon",
   widget_name: "Near Horizon",
+  logo_link:
+    "https://ipfs.near.social/ipfs/bafkreidehf3mmjagsjwiaoilvnwqnirayw6bypa67qxw4hyyxala4w3gpq",
 };
 
 return (
