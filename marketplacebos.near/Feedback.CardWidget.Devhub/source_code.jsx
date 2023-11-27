aa3b89dcd921460baea3dc7cd761a52d8d4e7cda
marketplacebos.near <marketplacebos.near>
2023-11-27 04:29:28 +0000

    Update Feedback.CardWidget.Devhub by marketplacebos.near at 2023-11-27T04:29:28.002Z

--- marketplacebos.near/Feedback.CardWidget.Devhub/source_code.jsx
+++ marketplacebos.near/Feedback.CardWidget.Devhub/source_code.jsx
@@ -3,6 +3,7 @@ const props = {
   widget_name_link:
     "https://near.social/marketplacebos.near/widget/Feedback.Widget.Devhub",
   widget_name: "DevHub",
+  logo_link: "https://ipfs.near.social/ipfs/bafkreiggwajains3kbgc7h2s5yr73dvmgmmhe2afyatxrkihqcisrbe56i",
 };
 
 return (
