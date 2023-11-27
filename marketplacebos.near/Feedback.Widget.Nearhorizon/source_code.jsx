982198d4fad1a142b29e02c99fff25ec5661f755
marketplacebos.near <marketplacebos.near>
2023-11-27 05:46:17 +0000

    Update Feedback.Widget.Nearhorizon by marketplacebos.near at 2023-11-27T05:46:17.318Z

--- marketplacebos.near/Feedback.Widget.Nearhorizon/source_code.jsx
+++ marketplacebos.near/Feedback.Widget.Nearhorizon/source_code.jsx
@@ -2,6 +2,8 @@ const props = {
   widget_id: "nearhorizon",
   widget_link: "nearhorizon.near/widget/Index",
   widget_name: "Near Horizon",
+  logo_links:
+    "https://ipfs.near.social/ipfs/bafkreihszpflge5j3g35td67xh7fw5z37rgyumh75uzabqrfnpcjfag2we",
 };
 return (
   <>
