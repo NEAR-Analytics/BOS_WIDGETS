ff02e9c37280c785d23e9ef46dd6d51242eb4272
efiz.near <efiz.near>
2023-11-27 17:44:39 +0000

    Update tldraw.hello by efiz.near at 2023-11-27T17:44:39.153Z

---
+++ efiz.near/tldraw.hello/source_code.jsx
@@ -0,0 +1,8 @@
+return (
+  <div
+    className="d-flex justify-content-center align-items-center"
+    style={{ height: "100px", backgroundColor: "lightblue" }}
+  >
+    <span>Hello tldraw</span>
+  </div>
+);
