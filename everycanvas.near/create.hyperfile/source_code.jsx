7d2891ac65b2ef37ec79cda1e99f5b9736baaf05
everycanvas.near <everycanvas.near>
2023-11-27 01:46:00 +0000

    Update create.hyperfile by everycanvas.near at 2023-11-27T01:46:00.070Z

--- everycanvas.near/create.hyperfile/source_code.jsx
+++ everycanvas.near/create.hyperfile/source_code.jsx
@@ -3,10 +3,14 @@ const Wrapper = styled.div`
   margin: 0 auto;
 `;
 
+const TabContent = styled.div`
+  margin-top: 1rem;
+`;
+
 const Form = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 4px;
   width: 100%;
 `;
 
@@ -25,14 +29,14 @@ const Select = styled.select`
 const FormGroup = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 5px;
 `;
 
 const adapters = [
   // these can come from the user (or app) settings
   // {
   //   title: "Local Storage",
-  //   value: "everycanvas.near/widget/adapter.local",
+  //   value: "everycanvas.near/widget/adapter.local_storage",
+  //   saveRef: false
   // },
   // {
   //   title: "SocialDB",
@@ -58,11 +62,16 @@ const adapters = [
 
 const defaultAdapter = adapters[0];
 
+const { creatorId } = props;
+
 const [json, setJson] = useState(props.data ?? "");
 const [source, setSource] = useState(props.source ?? "");
 const [adapter, setAdapter] = useState(defaultAdapter.value ?? "");
 const [reference, setReference] = useState(undefined);
 const [filename, setFilename] = useState(props.filename ?? "");
+const [activeTab, setActiveTab] = useState("data");
+const [name, setName] = useState(props.name ?? "");
+const [description, setDescription] = useState(props.description ?? "");
 
 function generateUID() {
   return (
@@ -72,16 +81,21 @@ function generateUID() {
   );
 }
 
-const thingId = filename ?? generateUID();
-
 const handleCreate = () => {
-  // load in the state.adapter
+  const isCreator = context.accountId === creatorId;
+
+  // load in the state.adapter (modules for IPFS, Arweave, Ceramic, Verida, On Machina... )
   const { create } = VM.require(adapter) || (() => {});
   if (create) {
+    // store the data somewhere, based on the adapter
     create(json).then((reference) => {
-      console.log("reference", reference);
+      // now we have a reference to the data
+      // we need to name it... are we the original creator or are we forking? We don't want to overwrite any of the users custom (or maybe we do!)
+      const thingId = filename ?? generateUID();
+
       const hyperfile = {
-        thing: {
+        [props.type]: {
+          // which we store in the social contract
           [thingId]: {
             "": JSON.stringify({
               fileformat: `${props.type}.${source}`,
@@ -90,12 +104,38 @@ const handleCreate = () => {
               reference: reference,
             }),
             metadata: {
+              name: name,
+              description: description,
               type: props.type,
             },
           },
         },
       };
-      // we're not logged in, so it doesn't do anything!
+
+      if (creatorId !== context.accountId) {
+        // handle request merge
+        hyperfile.index = {
+          notify: JSON.stringify({
+            key: creatorId,
+            value: {
+              type: "request",
+              data: {
+                type: "merge",
+                upstream: `${creatorId}/${props.type}/${props.filename}`,
+                origin: `${context.accountId}/${props.type}/${thingId}`,
+              },
+            },
+          }),
+        };
+        hyperfile[props.type][thingId].metadata = {
+          ...hyperfile[props.type][thingId].metadata,
+          upstream: `${creatorId}/${props.type}/${props.filename}`,
+        };
+        // I want to make a request to merge
+        // set upstream and downstream
+      }
+
+      // sometimes we're not logged in, so it doesn't do anything!
       Social.set(hyperfile, { force: true });
     });
   }
@@ -103,33 +143,89 @@ const handleCreate = () => {
 
 return (
   <Wrapper>
-    <Form>
-      <FormGroup>
-        <Label>source</Label>
-        <Input
-          type="text"
-          value={source}
-          onChange={(e) => onChangeSource(e.target.value)}
-          disabled={props.source} // disable if source is passed in
-        />
-      </FormGroup>
-      <textarea
-        className="form-control mb-3"
-        rows={5}
-        value={json}
-        onChange={(e) => setJson(e.target.value)}
-      />
-      <FormGroup>
-        <Label>adapter</Label>
-        <Select value={adapter} onChange={(e) => setAdapter(e.target.value)}>
-          {adapters.map((o) => (
-            <option value={o.value}>{o.title}</option>
-          ))}
-        </Select>
-      </FormGroup>
+    <h3>{context.accountId === creatorId ? "create" : "request merge"}</h3>
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          className={`nav-link ${activeTab === "data" ? "active" : ""}`}
+          onClick={() => setActiveTab("data")}
+        >
+          Data
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          className={`nav-link ${activeTab === "metadata" ? "active" : ""}`}
+          onClick={() => setActiveTab("metadata")}
+        >
+          Metadata
+        </a>
+      </li>
+    </ul>
+
+    <TabContent>
+      {activeTab === "data" && (
+        <Form>
+          <FormGroup>
+            <Label>source</Label>
+            <Input
+              type="text"
+              value={source}
+              onChange={(e) => onChangeSource(e.target.value)}
+              disabled={props.source} // disable if source is passed in
+            />
+          </FormGroup>
+          {/* <Widget
+            src="bozon.near/widget/CodeDiff"
+            props={{ currentCode: update, prevCode: src, ...props }}
+          /> */}
+          <textarea
+            className="form-control mb-3"
+            rows={5}
+            value={json}
+            onChange={(e) => setJson(e.target.value)}
+          />
+          <FormGroup>
+            <Label>adapter</Label>
+            <Select
+              value={adapter}
+              onChange={(e) => setAdapter(e.target.value)}
+            >
+              {adapters.map((o) => (
+                <option value={o.value}>{o.title}</option>
+              ))}
+            </Select>
+          </FormGroup>
+        </Form>
+      )}
+    </TabContent>
+    <TabContent>
+      {activeTab === "metadata" && (
+        <Form>
+          <FormGroup>
+            <Label>name</Label>
+            <Input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>description</Label>
+            <textarea
+              className="form-control mb-3"
+              rows={5}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </FormGroup>
+        </Form>
+      )}
+    </TabContent>
+    <FormGroup>
       <button className="btn btn-success mb-1" onClick={handleCreate}>
         Create
       </button>
-    </Form>
+    </FormGroup>
   </Wrapper>
 );
