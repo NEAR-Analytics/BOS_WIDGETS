78cdf30ab1aebcdc326cd66c6acd0b9fd11cb36a
marketplacebos.near <marketplacebos.near>
2023-11-27 05:45:57 +0000

    Update Feedback.Widget.Potlock by marketplacebos.near at 2023-11-27T05:45:57.501Z

--- marketplacebos.near/Feedback.Widget.Potlock/source_code.jsx
+++ marketplacebos.near/Feedback.Widget.Potlock/source_code.jsx
@@ -2,6 +2,8 @@ const props = {
   widget_id: "potlock",
   widget_link: "potlock.near/widget/Index",
   widget_name: "PotLock",
+  logo_links:
+    "https://ipfs.near.social/ipfs/bafkreiajmvq7bzyqhxrhrkwv76ztotn6nrcq7v5lsk4kzrk3mjgxxnog6a",
 };
 return (
   <>
