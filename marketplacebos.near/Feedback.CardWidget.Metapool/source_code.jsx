534656c7a21ae0f60174c19262923138497eea69
marketplacebos.near <marketplacebos.near>
2023-11-27 05:29:35 +0000

    Update Feedback.CardWidget.Metapool by marketplacebos.near at 2023-11-27T05:29:35.271Z

--- marketplacebos.near/Feedback.CardWidget.Metapool/source_code.jsx
+++ marketplacebos.near/Feedback.CardWidget.Metapool/source_code.jsx
@@ -3,6 +3,8 @@ const props = {
   widget_name_link:
     "https://near.social/marketplacebos.near/widget/Feedback.Widget.Metapool",
   widget_name: "MetaPool",
+  logo_link:
+    "https://ipfs.near.social/ipfs/bafkreieau6gbaehlrfeoj23fb4uvnqxk6mentbdpsyoqy6ugifnc5a7al4",
 };
 
 return (
