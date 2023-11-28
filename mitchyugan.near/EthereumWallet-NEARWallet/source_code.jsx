ba7f03a05821f980012cd87ecc11b0cb037b6bc4
mitchyugan.near <mitchyugan.near>
2023-11-28 12:13:55 +0000

    Update EthereumWallet-NEARWallet by mitchyugan.near at 2023-11-28T12:13:55.759Z

--- mitchyugan.near/EthereumWallet-NEARWallet/source_code.jsx
+++ mitchyugan.near/EthereumWallet-NEARWallet/source_code.jsx
@@ -1,16 +1,22 @@
 const userId = context.accountId;
 
-const network1 = {
-  networkname: "Ethereum",
-  chainId: "ethereum0001",
-  accountID: 0x8efd7b62aff059615fb26b2cdb474c888c799d84,
-  status: "Connected",
-};
+const network1 = Storage.get("network1");
+console.log(network1);
 
-Storage.set("network1", JSON.stringify(network1));
-
-const network11 = Storage.get("network1");
-console.log("Network1:", network11);
+const connectedAccountId = [
+  {
+    networkname: "Ethereum",
+    chainId: "ethereum0001",
+    accountID: 0x8efd7b62aff059615fb26b2cdb474c888c799d84,
+    status: "Connected",
+  },
+  {
+    networkname: "Polygon",
+    chainId: "polygon0001",
+    accountID: 0x8efd7b62aff059615fb26b2cdb474c888c799d84,
+    status: "Disconnected",
+  },
+];
 
 return (
   <div>
@@ -32,28 +38,27 @@ return (
         <h6>
           <strong>Status:</strong> Connected
         </h6>
-        <button>Disconnect</button>
       </div>
       <hr />
+      <div>
+        <h4>Account 2</h4>
+        <h6>
+          <strong>Network Name:</strong> Polygon
+        </h6>
+        <h6>
+          <strong>Chain ID:</strong> polygon0001
+        </h6>
+        <h6>
+          <strong>Account ID: </strong> 0x8Efd7b62Aff059615FB26b2CDb474C888C799D84
+        </h6>
+        <h6>
+          <strong>Status:</strong> Disconnected
+        </h6>
+      </div>;
+
       <p>{lastBlockHeight}</p>
     </div>
   </div>
 );
 
 <Web3Connect />;
-<div>
-  <h4>Account 2</h4>
-  <h6>
-    <strong>Network Name:</strong> Polygon
-  </h6>
-  <h6>
-    <strong>Chain ID:</strong> polygon0001
-  </h6>
-  <h6>
-    <strong>Account ID: </strong> 0x8Efd7b62Aff059615FB26b2CDb474C888C799D84
-  </h6>
-  <h6>
-    <strong>Status:</strong> Disconnected
-  </h6>
-  <button>Connect</button>
-</div>;
