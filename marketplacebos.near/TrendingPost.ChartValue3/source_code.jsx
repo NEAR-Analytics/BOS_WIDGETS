84f9a69d61754a8abcc9deaddeffdf67896363b8
marketplacebos.near <marketplacebos.near>
2023-11-28 20:47:45 +0000

    Update TrendingPost.ChartValue3 by marketplacebos.near at 2023-11-28T20:47:45.571Z

--- marketplacebos.near/TrendingPost.ChartValue3/source_code.jsx
+++ marketplacebos.near/TrendingPost.ChartValue3/source_code.jsx
@@ -248,8 +248,9 @@ const Table = () => {
                 <tr>
                   <StyledTd>
                     <a
-                      href={`https://near.social/?hashtag=${encodeURIComponent(
-                        item[0].replace("#", "")
+                      href={`https://near.social/marketplacebos.near/widget/TrendingPost.TableValue?hashtag=${item[0].replace(
+                        "#",
+                        ""
                       )}`}
                     >
                       {item[0]}
