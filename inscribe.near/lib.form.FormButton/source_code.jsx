d186d3cac32462a274911efa4470a43d4e58260b
inscribe.near <inscribe.near>
2023-11-28 14:26:09 +0000

    Update lib.form.FormButton by inscribe.near at 2023-11-28T14:26:09.407Z

---
+++ inscribe.near/lib.form.FormButton/source_code.jsx
@@ -0,0 +1,14 @@
+const FormButton = styled.div`
+  height: 56px;
+  width: 100%;
+  display: grid;
+  place-content: center;
+  cursor: pointer;
+  border: 1px solid #ffffff;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 4px;
+  &:hover {
+    background: rgba(255, 255, 255, 0.08);
+  }
+`;
