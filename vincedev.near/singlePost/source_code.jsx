e4fce63651c7b662f427ced7d85edd9741d2c7f5
vincedev.near <vincedev.near>
2023-11-28 11:55:16 +0000

    Update singlePost by vincedev.near at 2023-11-28T11:55:16.696Z

--- vincedev.near/singlePost/source_code.jsx
+++ vincedev.near/singlePost/source_code.jsx
@@ -4,13 +4,13 @@ const path = props.path;
 console.log(path);
 function fetchPost(path) {
   const res = fetch(path);
-  console.log(res);
-  console.log(path);
+ setData(res)
+  
   return res.body;
 }
+console.log(data)
 useEffect(() => {
   fetchPost(path);
 }, []);
-
 
 return <div>{path}</div>;
