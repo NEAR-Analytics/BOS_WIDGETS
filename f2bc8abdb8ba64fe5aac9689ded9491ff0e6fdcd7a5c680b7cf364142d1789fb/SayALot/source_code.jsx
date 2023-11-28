8612a5c7629c1516c6c122e98ed539c7124a00f1
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-11-28 18:06:25 +0000

    Update SayALot by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-11-28T18:06:25.281Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot/source_code.jsx
@@ -85,8 +85,8 @@ const widgets = {
   libSBT: `sayalot.near/widget/lib.SBT`,
   // libComment: `sayalot.near/widget/lib.comment`, ///////////////////////////////////////////////////
   libComment: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/lib.comment`, ///////////////////////////////////////////////////
-  // libArticle: `sayalot.near/widget/lib.article`, ///////////////////////////////////////////////////
-  libArticle: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/lib.article`, ///////////////////////////////////////////////////
+  libArticle: `sayalot.near/widget/lib.article`, ///////////////////////////////////////////////////
+  // libArticle: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/lib.article`, ///////////////////////////////////////////////////
   // libEmojis: `sayalot.near/widget/lib.emojis`, ///////////////////////////////////////////////////
   libEmojis: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/lib.emojis`, ///////////////////////////////////////////////////
   // libUpVotes: `sayalot.near/widget/lib.upVotes`, ///////////////////////////////////////////////////
