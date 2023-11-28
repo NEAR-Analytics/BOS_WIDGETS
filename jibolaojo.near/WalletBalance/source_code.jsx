7d45dc705e8fc351da6f4e248f0179d981bd01e6
jibolaojo.near <jibolaojo.near>
2023-11-28 22:58:54 +0000

    Update WalletBalance by jibolaojo.near at 2023-11-28T22:58:54.374Z

--- jibolaojo.near/WalletBalance/source_code.jsx
+++ jibolaojo.near/WalletBalance/source_code.jsx
@@ -1,8 +1,9 @@
-const {
-  body: { account },
-} = fetch(`https://api.nearblocks.io/v1/account/${props.wallet_id}`);
-const native_balance = account[0].amount / 1e24;
-const unspendable_balance = 0.05 + account[0].storage_usage / 1e5;
+const res = fetch(`https://api.nearblocks.io/v1/account/${props.wallet_id}`);
+
+if (!(res && res.body)) return '...';
+
+const native_balance = res.body.account[0].amount / 1e24;
+const unspendable_balance = 0.05 + res.body.account[0].storage_usage / 1e5;
 const spendable_balance = native_balance - unspendable_balance;
 
 const balance = props.balance_type === 'spendable' ? spendable_balance : native_balance;
