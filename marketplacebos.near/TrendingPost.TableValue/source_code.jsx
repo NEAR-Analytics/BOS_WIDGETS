81a5d9f773edc156edfefe0bcb1076d000b96714
marketplacebos.near <marketplacebos.near>
2023-11-28 17:27:24 +0000

    Update TrendingPost.TableValue by marketplacebos.near at 2023-11-28T17:27:24.329Z

--- marketplacebos.near/TrendingPost.TableValue/source_code.jsx
+++ marketplacebos.near/TrendingPost.TableValue/source_code.jsx
@@ -1,35 +1,3 @@
-const allPostss = Social.get("*/post/main/", "final");
-
-function findHashtags(str) {
-  const regexp = /\B\#\w\w+\b/g;
-  let match;
-  let tags = [];
-  while ((match = regexp.exec(str)) !== null) {
-    tags.push(match[0]);
-  }
-  return tags;
-}
-
-const tagCount = {};
-Object.keys(allPostss).forEach((item) => {
-  const tags = findHashtags(JSON.parse(allPostss[item].post.main).text);
-  if (tags.length > 0) {
-    tags.forEach((tag) => {
-      tagCount[tag] = tagCount[tag] + 1 || 1;
-    });
-  }
-});
-
-let totalItems = 0;
-
-for (let i = 0; i < sorted.length; i++) {
-  totalItems += sorted[i][1];
-}
-
-let entries = Object.entries(tagCount);
-
-let sorted = entries.sort((b, a) => a[1] - b[1]);
-
 const respBlock = fetch("https://api.nearblocks.io/v1/stats");
 
 const newBlock = Math.round(
@@ -200,6 +168,8 @@ const compare = (b, a) => {
 };
 const sort = postEngagement.sort(compare);
 
+let totalItems = 0;
+
 let totalAccountPosts = 0;
 let totalLikes = 0;
 let totalComments = 0;
@@ -276,6 +246,67 @@ const StyledTotalValue = styled.div`
   margin-top: 8px;
 `;
 
+const labelN = "Top 24h trending posts on NEAR Social";
+
+const backgroundcolorP = [
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+];
+
+const borderColorP = [
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+];
+
+
+let dataP = [];
+let labelP = [];
+
+// Check if item.accountId is defined and not null
+postEngagement.forEach((item) => {
+  if (item.accountId) {
+    dataP.push(item.EP || 0); // Assuming EP is the ENCOURAGE POINT
+    labelP.push(item.dateCreated || ""); // Assuming dateCreated is the date
+  }});
+// Check if item.accountId is defined and not null
+
+
 const Table = () => {
   return (
     <>
@@ -305,7 +336,19 @@ const Table = () => {
       <br />
       <br />
       <br />
-
+      <Widget
+        src="marketplacebos.near/widget/TrendingPost.ChartPost"
+        props={{
+          dataP: dataP,
+          labelP: labelP,
+          backgroundcolorP: backgroundcolorP,
+          borderColorP: borderColorP,
+          labelN: labelN,
+        }}
+      />
+      <br />
+      <br />
+      <br />
       <StyledTable>
         <thead>
           <tr>
@@ -330,16 +373,16 @@ const Table = () => {
                   {item.blockHeight}
                 </a>
               </StyledTd>
-              <StyledTd maxWidth="100px">
-                {item.accountId.includes(".near")
-                  ? item.accountId
-                  : item.accountId.slice(0, 7) +
-                    "..." +
-                    item.accountId.slice(
-                      item.accountId.length - 10,
-                      item.accountId.length - 1
-                    )}
-              </StyledTd>
+             <StyledTd maxWidth="100px">
+  {typeof item.accountId === 'string' && item.accountId.includes(".near")
+    ? item.accountId
+    : typeof item.accountId === 'string' && item.accountId.slice(0, 7) +
+      "..." +
+      item.accountId.slice(
+        item.accountId.length - 10,
+        item.accountId.length - 1
+      )}
+</StyledTd>
               <StyledTd>{item.EP && item.EP.toFixed(4)}</StyledTd>
               <StyledTd>{item.EP3D && item.EP3D.toFixed(4)}</StyledTd>
               <StyledTd>{item.EP7D && item.EP7D.toFixed(4)}</StyledTd>
