5d0a2a4bd3dcc57541ec0819747add687042cf7d
segunojo1.near <segunojo1.near>
2023-11-28 11:06:59 +0000

    Update Untitled-0 by segunojo1.near at 2023-11-28T11:06:59.260Z

--- segunojo1.near/Untitled-0/source_code.jsx
+++ segunojo1.near/Untitled-0/source_code.jsx
@@ -2,14 +2,61 @@ const Header = styled.div`
 height: 2rem;
 display: flex;
 justify-content: center;
-background-color: red;
+
 align-items: center;
+p{
+    margin: 0;
+}
+`;
+
+const Main = styled.div`
+display:flex;
+justify-content:space-between
+
+.nft{
+    display: grid;
+}
+`;
+
+const Footer = styled.div`
+display: flex;
+height: 1rem;
+bottom: 0;
+background-color: red;
 `;
 
 const header = (
   <Header>
-    <p>Hello world</p>
+    <p>NFT Poll</p>
   </Header>
 );
 
-return <div>{header}</div>;
+const maincontent = (
+  <Main>
+    <div class="nft">
+      <img src="https://crushingit.tech/hackathon-assets/product.png" />
+      <button>Vote</button>
+    </div>
+    <div class="nft">
+      <img src="https://crushingit.tech/hackathon-assets/product.png" />
+      <button>Vote</button>
+    </div>
+    <div class="nft">
+      <img src="https://crushingit.tech/hackathon-assets/product.png" />
+      <button>Vote</button>
+    </div>
+    <Web3Connect />
+  </Main>
+);
+
+const footer = (
+  <Footer>
+    <p>NFT Voting Poll</p>
+  </Footer>
+);
+
+return (
+  <div>
+    {header} {maincontent}
+  </div>
+);
