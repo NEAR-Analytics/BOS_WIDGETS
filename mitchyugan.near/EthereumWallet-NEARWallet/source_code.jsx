451469ca8f7ea4a7e5c7443518d4e251fc5d2f96
mitchyugan.near <mitchyugan.near>
2023-11-28 11:36:46 +0000

    Update EthereumWallet-NEARWallet by mitchyugan.near at 2023-11-28T11:36:46.654Z

--- mitchyugan.near/EthereumWallet-NEARWallet/source_code.jsx
+++ mitchyugan.near/EthereumWallet-NEARWallet/source_code.jsx
@@ -1,8 +1,4 @@
 const userId = context.accountId;
-const notificationFeedSrc = "mob.near/widget/NotificationFeed";
-
-const lastBlockHeight = Storage.get("lastBlockHeight", notificationFeedSrc);
-Storage.set("network1", network1);
 
 const network1 = {
   networkname: "Ethereum",
@@ -11,7 +7,10 @@ const network1 = {
   status: "Connected",
 };
 
-console.log("Network1", network1);
+Storage.set("network1", JSON.stringify(network1));
+
+const network11 = Storage.get("network1");
+console.log("Network1:", network11);
 
 return (
   <div>
@@ -36,27 +35,25 @@ return (
         <button>Disconnect</button>
       </div>
       <hr />
-      <div>
-        <h4>Account 2</h4>
-        <h6>
-          <strong>Network Name:</strong> Polygon
-        </h6>
-        <h6>
-          <strong>Chain ID:</strong> polygon0001
-        </h6>
-        <h6>
-          <strong>Account ID: </strong>{" "}
-          0x8Efd7b62Aff059615FB26b2CDb474C888C799D84
-        </h6>
-        <h6>
-          <strong>Status:</strong> Disconnected
-        </h6>
-        <button>Connect</button>
-      </div>
-      <hr />
       <p>{lastBlockHeight}</p>
     </div>
   </div>
 );
 
 <Web3Connect />;
+<div>
+  <h4>Account 2</h4>
+  <h6>
+    <strong>Network Name:</strong> Polygon
+  </h6>
+  <h6>
+    <strong>Chain ID:</strong> polygon0001
+  </h6>
+  <h6>
+    <strong>Account ID: </strong> 0x8Efd7b62Aff059615FB26b2CDb474C888C799D84
+  </h6>
+  <h6>
+    <strong>Status:</strong> Disconnected
+  </h6>
+  <button>Connect</button>
+</div>;
