3a11aeae94320fc5e6706e144f09ac95d7774ea2
koniesjay.near <koniesjay.near>
2023-11-29 13:45:28 +0000

    Update WalletBridge by koniesjay.near at 2023-11-29T13:45:28.855Z

--- koniesjay.near/WalletBridge/source_code.jsx
+++ koniesjay.near/WalletBridge/source_code.jsx
@@ -1,4 +1,4 @@
-const userId = context.accountId;
+const [userId, setUserId] = useState(context.accountId);
 
 const [networkName, setNetworkName] = useState(null);
 const [chainId, setChainId] = useState(null);
@@ -9,6 +9,50 @@ const [accountDetails, setAccountDetails] = useState(null);
 const handleString = (string) => {
   return string.slice(0, 6) + "..." + string.slice(-4);
 };
+
+const blockUrl = [
+  {
+    chainId: 11155111,
+    blockUrl: "https://sepolia.etherscan.io/address/",
+  },
+  {
+    chainId: 5,
+    blockUrl: "https://goerli.etherscan.io/address/",
+  },
+  {
+    chainId: 1,
+    blockUrl: "https://etherscan.io/address/",
+  },
+  {
+    chainId: 80001,
+    blockUrl: "https://mumbai.polygonscan.com/address/",
+  },
+  {
+    chainId: 137,
+    blockUrl: "https://polygonscan.com/address/",
+  },
+  {
+    chainId: 43114,
+    blockUrl: "https://snowtrace.io/address/",
+  },
+];
+
+const bb = {
+  11155111: "https://sepolia.etherscan.io/address/",
+  5: "https://goerli.etherscan.io/address/",
+  1: "https://etherscan.io/address/",
+  80001: "https://mumbai.polygonscan.com/address/",
+  137: "https://polygonscan.com/address/",
+  43114: "https://snowtrace.io/address/",
+};
+
+// sepolia 11155111, georli 5, ethereum 1, mumbai 80001, polygon 137, avalanche 43114
+
+console.log(bb["1115511"]);
+
+useEffect(() => {
+  setUserId(userId);
+}, [context.accountId]);
 
 useEffect(() => {
   setAccounts(JSON.parse(Storage.get(`${userId}`)));
@@ -123,26 +167,43 @@ useEffect(() => {
 
 return (
   <div>
-    <h3>Hello {userId}</h3>
-    <div>
-      <p>Here are all the accounts you've Interracted with:</p>
-      {accounts &&
-        accounts.map((item, index) => (
-          <div key={index}>
-            <div>
-              <h4>#{index + 1}</h4>
-              <h6>
-                <strong>Account ID: </strong> {handleString(item.accountId)}
-              </h6>
-              <h6>
-                <strong>Status: </strong>
-                {item.accountId == accountId ? "Connected" : "Not Connected"}
-              </h6>
-            </div>
-            <hr />
-          </div>
-        ))}
-    </div>
+    {userId ? (
+      <div>
+        {" "}
+        <h3>Hello {userId}</h3>
+        <div>
+          <p>Here are all the accounts you've Interracted with:</p>
+          {accounts &&
+            accounts.map((item, index) => (
+              <div key={index}>
+                <div>
+                  <h4>#{index + 1}</h4>
+                  <h6>
+                    <strong>Account ID: </strong> {handleString(item.accountId)}
+                  </h6>
+                  <h6>
+                    <strong>Status: </strong>
+                    {item.accountId == accountId
+                      ? "Connected"
+                      : "Not Connected"}
+                  </h6>
+                  <h6>
+                    <strong>Explorer Url: </strong>
+                    {bb[`${chainId}`] ? (
+                      <a href={bb[`${chainId}`] + item.accountId}>Click Here</a>
+                    ) : (
+                      "Not Found"
+                    )}
+                  </h6>
+                </div>
+                <hr />
+              </div>
+            ))}
+        </div>
+      </div>
+    ) : (
+      <h1>Sign In your NEAR profile</h1>
+    )}
     {networkName ? <div></div> : <Web3Connect />}
   </div>
 );
