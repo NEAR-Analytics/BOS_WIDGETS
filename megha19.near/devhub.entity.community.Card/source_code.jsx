e173cb09ec4f850da0eedf4f9af70fc2a8d5b90c
megha19.near <megha19.near>
2023-11-29 18:09:42 +0000

    Update devhub.entity.community.Card by megha19.near at 2023-11-29T18:09:42.465Z

--- megha19.near/devhub.entity.community.Card/source_code.jsx
+++ megha19.near/devhub.entity.community.Card/source_code.jsx
@@ -4,18 +4,54 @@ if (!href) {
   return <></>;
 }
 
-const CommunityCard = ({
-  format,
-  isBannerEnabled,
-  metadata,
-  ...otherProps
-}) => {
-  const renderFormat =
-    format === "small" || format === "medium" ? format : "small";
+const Card = styled.div`
+  cursor: pointer;
+  background-color: white;
+  border-radius: 0.5rem;
+  padding: 1.5rem;
+  gap: 1rem;
+  height: 100%;
+  min-height: 12rem;
 
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  transition: all 300ms;
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+
+  &:hover {
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  img.logo {
+    height: 6rem;
+    width: 6rem;
+    border-radius: 50%;
+
+    object-fit: cover;
+  }
+
+  h3,
+  p {
+    margin: 0;
+  }
+
+  h3 {
+    font-size: 1.25rem;
+    font-weight: 600;
+  }
+
+  p {
+    font-size: 1rem;
+    font-weight: 400;
+  }
+`;
+
+const CommunityCard = ({ metadata }) => {
+  const { handle, logo_url, name, description } = metadata;
   const link = href({
     widgetSrc: "megha19.near/widget/app",
-    params: { page: "community", handle: metadata.handle },
+    params: { page: "community", handle: handle },
   });
 
   const CommunityName = styled.h5`
