1b2d36589e515e3f817910f4b37e1d2518d05e9d
koniesjay.near <koniesjay.near>
2023-11-29 17:25:10 +0000

    Update WalletBridge by koniesjay.near at 2023-11-29T17:25:10.621Z

--- koniesjay.near/WalletBridge/source_code.jsx
+++ koniesjay.near/WalletBridge/source_code.jsx
@@ -10,43 +10,79 @@ const handleString = (string) => {
   return string.slice(0, 6) + "..." + string.slice(-4);
 };
 
-const blockUrl = [
-  {
-    chainId: 11155111,
+const bb = {
+  11155111: {
+    name: "Sepolia",
+    image:
+      "https://w7.pngwing.com/pngs/268/1013/png-transparent-ethereum-eth-hd-logo-thumbnail.png",
     blockUrl: "https://sepolia.etherscan.io/address/",
   },
-  {
-    chainId: 5,
+  5: {
+    name: "Goerli",
+    image:
+      "https://w7.pngwing.com/pngs/268/1013/png-transparent-ethereum-eth-hd-logo-thumbnail.png",
     blockUrl: "https://goerli.etherscan.io/address/",
   },
-  {
-    chainId: 1,
+  1: {
+    name: "Ethereum",
+    image:
+      "https://w7.pngwing.com/pngs/268/1013/png-transparent-ethereum-eth-hd-logo-thumbnail.png",
     blockUrl: "https://etherscan.io/address/",
   },
-  {
-    chainId: 80001,
+  80001: {
+    name: "Mumbai",
+    image:
+      "https://seeklogo.com/images/P/polygon-matic-logo-1DFDA3A3A8-seeklogo.com.png",
     blockUrl: "https://mumbai.polygonscan.com/address/",
   },
-  {
-    chainId: 137,
+  137: {
+    name: "Polygon",
+    image:
+      "https://seeklogo.com/images/P/polygon-matic-logo-1DFDA3A3A8-seeklogo.com.png",
     blockUrl: "https://polygonscan.com/address/",
   },
-  {
-    chainId: 43114,
+  43114: {
+    name: "Avalanche",
+    image: "https://cryptologos.cc/logos/avalanche-avax-logo.png",
     blockUrl: "https://snowtrace.io/address/",
   },
-];
-
-const bb = {
-  11155111: "https://sepolia.etherscan.io/address/",
-  5: "https://goerli.etherscan.io/address/",
-  1: "https://etherscan.io/address/",
-  80001: "https://mumbai.polygonscan.com/address/",
-  137: "https://polygonscan.com/address/",
-  43114: "https://snowtrace.io/address/",
+  59144: {
+    name: "Linea",
+    image:
+      "https://images.ctfassets.net/6g6hg01fg28j/2XZ3qN4qlcorV9RJPxwBlO/b15c5985cc31dded61273bbec7f5f696/Infura_Linea-Hero-Image.svg",
+    blockUrl: "https://lineascan.build/address/",
+  },
+  1313161554: {
+    name: "Aurora",
+    image: "https://s2.coinmarketcap.com/static/img/coins/200x200/14803.png",
+    blockUrl: "https://explorer.aurora.dev/address/",
+  },
+  100: {
+    name: "Gnosis",
+    image:
+      "https://w7.pngwing.com/pngs/327/711/png-transparent-gnosis-hd-logo.png",
+    blockUrl: "https://gnosisscan.io/address/",
+  },
+  324: {
+    name: "Zsync",
+    image: "https://lite.zksync.io/images/logo-no-letters.svg",
+    blockUrl: "https://explorer.zksync.io/address/",
+  },
+  42161: {
+    name: "Arbitrum",
+    image:
+      "https://strapi.mewapi.io/uploads/large_arbitrum_shield_3f24102a28.png",
+    blockUrl: "https://arbiscan.io/address/",
+  },
+  56: {
+    name: "BNB",
+    image:
+      "https://seeklogo.com/images/B/binance-coin-bnb-logo-97F9D55608-seeklogo.com.png",
+    blockUrl: "https://bscscan.com/address/",
+  },
 };
 
-// sepolia 11155111, georli 5, ethereum 1, mumbai 80001, polygon 137, avalanche 43114
+// sepolia 11155111, georli 5, ethereum 1, mumbai 80001, polygon 137, avalanche 43114 aurora 1313161554 arbitrum zsync
 
 console.log(bb["1115511"]);
 
