f75c687b65dba7b5f689fdd076762ab25d0a98f3
vincedev.near <vincedev.near>
2023-11-29 16:13:03 +0000

    Update PostxNavbar by vincedev.near at 2023-11-29T16:13:03.587Z

--- vincedev.near/PostxNavbar/source_code.jsx
+++ vincedev.near/PostxNavbar/source_code.jsx
@@ -44,9 +44,7 @@ border-bottom: 4px solid #061D33;
 height:90px;
 
 }
-border-bottom: ${
-  props.active === false ? "4px solid #061D33" : "0px solid #061D33"
-};
+border-bottom: ${props.active ? "0px solid #061D33" : "4px solid #061D33"};
 
 `;
 const HTest2 = styled.h2`
