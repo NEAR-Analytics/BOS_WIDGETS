b46c0feb4efcad545ce2ecfe61a61d7dfb739823
vincedev.near <vincedev.near>
2023-11-29 17:01:01 +0000

    Update post by vincedev.near at 2023-11-29T17:01:01.175Z

--- vincedev.near/post/source_code.jsx
+++ vincedev.near/post/source_code.jsx
@@ -16,9 +16,23 @@ width:28%;
 
 const [parentArray, setParentAray] = useState([]);
 const [hashList, setHashList] = useState([]);
+const [trendList, setTrendList] = useState([]);
+const [singlePostTrendList, setSinglePostTrendList] = useState([]);
+const [search, setSearch] = useState("");
+const [postActive, setPostActive] = useState(true);
+const [trendActive, setTrendActive] = useState(false);
+const [activeTag, setActiveTag] = useState("");
 
 const pushToArray = (array) => {
   setParentAray((prevArray) => [...prevArray, ...array]);
+};
+
+const pushSearchArray = () => {
+  return [];
+};
+
+const getSearch = (search) => {
+  setSearch(search);
 };
 
 const extractHashtags = (text) => {
@@ -51,6 +65,7 @@ const extractHashtags = (text) => {
 //   //   console.log("0");
 //   // }
 // }
+
 function getHashList() {
   const hashtagCounts = {};
 
@@ -58,7 +73,7 @@ function getHashList() {
     const txt = parentArray[i].text;
     const hashtags = extractHashtags(txt);
 
-    hashtags.forEach((tag) => {
+    hashtags.map((tag) => {
       hashtagCounts[tag] = (hashtagCounts[tag] || 0) + 1;
     });
   }
@@ -66,132 +81,237 @@ function getHashList() {
   setHashList(Object.entries(hashtagCounts));
 }
 
+const fetchTrendFeed = () => {
+  setTrendList(
+    parentArray.filter((it) =>
+      extractHashtags(it.text).includes(hashList[0][0])
+    )
+  );
+
+  setPostActive(false);
+  setTrendActive(true);
+};
+const fetchAllPost = () => {
+  setTrendList([]);
+  setPostActive(true);
+  setTrendActive(false);
+};
+
 useEffect(() => {
   getHashList();
 }, [parentArray]);
 
-console.log("second", hashList);
+const singlePosthashTag = (text) => {
+  setSinglePostTrendList(
+    parentArray.filter((it) => extractHashtags(it.text).includes(text))
+  );
+  setActiveTag(text);
+  setPostActive(false);
+  setTrendActive(false);
+  setTrendList([]);
+  setSearch("");
+  console.log(text);
+};
 
 return (
   <div
     className="d-flex flex-column gap-1"
     style={{
       background: "#fefefe",
-  
     }}
   >
-    <Widget src="vincedev.near/widget/PostxNavbar" />
+    <Widget
+      src="vincedev.near/widget/PostxNavbar"
+      props={{
+        getSearch: getSearch,
+        fetchTrendFeed: fetchTrendFeed,
+        fetchAllPost: fetchAllPost,
+        postActive: postActive,
+        trendActive: trendActive,
+      }}
+    />
     <MainWrapper>
       <childDiv1>
-        <Feed
-          index={[
-            {
-              action: "post",
-              key: "main",
-              options: {
-                limit: 10,
-                order: "desc",
-                accountId: props.accounts,
-              },
-              cacheOptions: {
-                ignoreCache: true,
-              },
-            },
-            {
-              action: "repost",
-              key: "main",
-              options: {
-                limit: 10,
-                order: "desc",
-                accountId: props.accounts,
-              },
-              cacheOptions: {
-                ignoreCache: true,
-              },
-            },
-          ]}
-          Item={(p) => {
-            const item = {
-              path: `${p.accountId}/post/main`,
-              blockHeight: p.blockHeight,
-              type: "social",
-            };
-
-            // console.log(p);
-            // <Widget
-            //   loading={
-            //     <div className="w-100" style={{ height: "200px" }} />
-            //   }
-            //   src="vincedev.near/widget/singlePost"
-            //   props={{
-            //     accountId: p.accountId,
-            //     blockHeight: p.blockHeight,
-            //   }}
-            // />
-            return (
-              <ContextMenu
-                Item={() => (
-                  <>
-                    <Widget
-                      src="blessed07.near/widget/post-content"
-                      props={{
-                        accountId: p.accountId,
-                        blockHeight: p.blockHeight,
-                        pushToArray: pushToArray,
-                      }}
-                    />
-                  </>
-                )}
-                passProps={{
-                  // PROPS THAT WILL BE PASSED TO FUNCTION
-                  show: {
-                    item,
-                  },
-                }}
-                handlers={{
-                  // FUNCTION DEFINITIONS
-                  show: ({ item }) => {
-                    const data = {
-                      index: {},
-                    };
-                    const notifications = ["james.near"].map((accountId) => ({
-                      key: accountId,
-                      value: {
-                        type: "mention",
-                        item,
-                      },
-                    }));
-
-                    if (notifications.length) {
-                      Social.set({
-                        notify: JSON.stringify(
-                          notifications.length > 1
-                            ? notifications
-                            : notifications[0]
-                        ),
-                      });
-                    }
-                  },
-                }}
-                items={{
-                  // MENU ITEM TO RENDER, WILL CALL FUNCTION WHEN CLICKED
-                  show: () => (
-                    <>
-                      <i className="menu__item__icon bi bi-arrow-return-right" />
-                      Forward
-                    </>
-                  ),
+        {activeTag && (
+          <div
+            style={{
+              backgroundColor: "#F3F9FF",
+              padding: "24px 40px",
+              margin: "8px auto",
+              borderRadius: "8px",
+              minWidth: "120px",
+              maxWidth: "40%",
+              fontFamily: "Space Grotesk",
+              fontWeight: "bolder",
+              fontSize: "28px",
+              textTransform: "uppercase",
+              textAlign: "center",
+            }}
+          >
+            #{activeTag}
+          </div>
+        )}
+        {search ? (
+          <div>
+            {parentArray
+              .filter((it) => extractHashtags(it.text).includes(search))
+              .map((eachP) => (
+                <>
+                  <Widget
+                    loading="loading"
+                    src="blessed07.near/widget/post-content"
+                    props={{
+                      accountId: eachP.accountId,
+                      blockHeight: eachP.blockHeight,
+                      pushToArray: pushSearchArray,
+                      forSearch: true,
+                    }}
+                  />
+                </>
+              ))}
+          </div>
+        ) : trendList?.length > 0 ? (
+          <>
+            {trendList.map((eachP) => (
+              <Widget
+                src="blessed07.near/widget/post-content"
+                props={{
+                  accountId: eachP.accountId,
+                  blockHeight: eachP.blockHeight,
+                  pushToArray: pushSearchArray,
+                  forSearch: true,
                 }}
               />
-            );
-          }}
-        />
+            ))}
+          </>
+        ) : singlePostTrendList?.length > 0 ? (
+          <>
+            {singlePostTrendList.map((eachP) => (
+              <Widget
+                src="blessed07.near/widget/post-content"
+                props={{
+                  accountId: eachP.accountId,
+                  blockHeight: eachP.blockHeight,
+                  pushToArray: pushSearchArray,
+                  forSearch: true,
+                }}
+              />
+            ))}
+          </>
+        ) : (
+          <Feed
+            index={[
+              {
+                action: "post",
+                key: "main",
+                options: {
+                  limit: 10,
+                  order: "desc",
+                  accountId: props.accounts,
+                },
+                cacheOptions: {
+                  ignoreCache: true,
+                },
+              },
+              {
+                action: "repost",
+                key: "main",
+                options: {
+                  limit: 10,
+                  order: "desc",
+                  accountId: props.accounts,
+                },
+                cacheOptions: {
+                  ignoreCache: true,
+                },
+              },
+            ]}
+            Item={(p) => {
+              const item = {
+                path: `${p.accountId}/post/main`,
+                blockHeight: p.blockHeight,
+                type: "social",
+              };
+
+              console.log(p);
+              // <Widget
+              //   loading={
+              //     <div className="w-100" style={{ height: "200px" }} />
+              //   }
+              //   src="vincedev.near/widget/singlePost"
+              //   props={{
+              //     accountId: p.accountId,
+              //     blockHeight: p.blockHeight,
+              //   }}
+              // />
+              return (
+                <ContextMenu
+                  Item={() => (
+                    <>
+                      <Widget
+                        src="blessed07.near/widget/post-content"
+                        props={{
+                          accountId: p.accountId,
+                          blockHeight: p.blockHeight,
+                          pushToArray: pushToArray,
+                          forSearch: false,
+                        }}
+                      />
+                    </>
+                  )}
+                  passProps={{
+                    // PROPS THAT WILL BE PASSED TO FUNCTION
+                    show: {
+                      item,
+                    },
+                  }}
+                  handlers={{
+                    // FUNCTION DEFINITIONS
+                    show: ({ item }) => {
+                      const data = {
+                        index: {},
+                      };
+                      const notifications = ["james.near"].map((accountId) => ({
+                        key: accountId,
+                        value: {
+                          type: "mention",
+                          item,
+                        },
+                      }));
+
+                      if (notifications.length) {
+                        Social.set({
+                          notify: JSON.stringify(
+                            notifications.length > 1
+                              ? notifications
+                              : notifications[0]
+                          ),
+                        });
+                      }
+                    },
+                  }}
+                  items={{
+                    // MENU ITEM TO RENDER, WILL CALL FUNCTION WHEN CLICKED
+                    show: () => (
+                      <>
+                        <i className="menu__item__icon bi bi-arrow-return-right" />
+                        Forward
+                      </>
+                    ),
+                  }}
+                />
+              );
+            }}
+          />
+        )}
       </childDiv1>
       <childDiv2>
         <Widget
           src="vincedev.near/widget/PostXTrendingPost"
           props={{
             hashList: hashList,
+            singlePosthashTag: singlePosthashTag,
+            activeTag: activeTag,
           }}
         />
       </childDiv2>
