d0eab596024c696f02cf7e50bbff2a94bcc3d017
segunojo1.near <segunojo1.near>
2023-11-29 10:13:40 +0000

    Update LinkETHAccountsToNearAccount.Navbar by segunojo1.near at 2023-11-29T10:13:40.546Z

---
+++ segunojo1.near/LinkETHAccountsToNearAccount.Navbar/source_code.jsx
@@ -0,0 +1,39 @@
+const userId = context.accountId;
+
+const Nav = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+height: 74px;
+margin: 56px 101px 59px 100px;
+
+div{
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  gap: 24px;
+  padding: 13px 72px 13px 38px;
+  border-radius: 12px;
+  
+}
+p{
+  margin: 0;
+}
+`;
+
+const Wallet = styled.div`
+  border: 1px solid rgba(0, 0, 0, 0.22);
+`;
+
+return (
+  <Nav>
+    <img src="https://i.ibb.co/G98pcQP/Frame-23.png" />
+
+    <div>
+      <Wallet>
+        <img src="https://i.ibb.co/X5zpnQD/near-icon-logo-10785-AE366-seeklogo-1.png" />
+        <p>{userId}</p>
+      </Wallet>
+    </div>
+  </Nav>
+);
