c0e4fc8fdba8e211d090e9ced946431d5096e07b
littlelace.near <littlelace.near>
2023-11-29 11:22:07 +0000

    Update ComponentSearch by littlelace.near at 2023-11-29T11:22:07.831Z

--- littlelace.near/ComponentSearch/source_code.jsx
+++ littlelace.near/ComponentSearch/source_code.jsx
@@ -4,13 +4,23 @@ const WidgetApp = styled.div`
   display: flex;
   // align-items: center;
   justify-content: center;
-  padding-top: 20px;
+  // padding-top: 20px;
   // background-color: black;
   height: 100vh;
+  * {
+    box-sizing: border-box;
+  }
 `;
 
 const Container = styled.div`
-  width: 300px;
+  min-width: 370px;
+  width: 100%;
+  display: flex;
+  flex: 1;
+  // background-color: grey;
+  height: 54px;
+  gap: 1rem;
+  // flow: wrap;
 `;
 
 const TextInput = styled.input`
@@ -22,16 +32,24 @@ const TextInput = styled.input`
 `;
 
 const WidgetList = styled.ul`
+  display: flex;
+  flex-direction: column;
   list-style-type: none;
-  padding: 0;
-  margin: 0;
-  max-height: 400px;
-  overflow-y: scroll;
-`
+  padding: 15px;
+  margin-top: 1rem;
+  max-height: 300px;
+  height: 300px;
+  // overflow-y: auto;
+  box-shadow: 0px 0px 8px #ddd;
+  border-radius: 10px;
+  position: relative;
+  // &::-webkit-scrollbar {
+  //   display: none;
+  // }
+`;
 
 const WidgetItem = styled.li`
   // background-color: grey;
-  text-align: center;
   cursor: pointer;
   padding: 10px;
   border-radius: 4px;
@@ -40,8 +58,42 @@ const WidgetItem = styled.li`
   }
 `;
 
+// search container
+const SearchContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  background-color: grey;
+  height: 100%;
+  gap: 1rem;
+  .input-group {
+    height: 100%;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    outline: none;
+    width: 100%;
+  }
+`;
+
+const SubmitButton = styled.button`
+  white-space: nowrap;
+  wordWrap: 'break-word';
+  display: inline-flex;
+  padding: 1.1rem 1.5rem;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  background: #8D61EE;
+  color: #fff;
+  border-radius: 10px;
+  // width:40%;
+  width: 150px;
+`;
+
 const [input, setInput] = useState("");
 const [widgets, setWidgets] = useState([]);
+
 
 function handleChange(value) {
   setInput(value);
@@ -60,51 +112,56 @@ function handleClick(widget) {
   console.log(widget.widgetSrc);
 }
 
+function handleSubmit() {
+  console.log("clicked check widget")
+}
 return (
   <WidgetApp>
     <Container>
-      <Widget
-        loading={
-          <div className="input-group">
-            <input type="text" className="form-control" />
-          </div>
-        }
-        src="mob.near/widget/ComponentSearch"
-        props={{
-          boostedTag: "app",
-          placeholder: "Search Applications",
-          limit: 10,
-          onChange: ({ result }) => {
-            State.update({ apps: result });
-          },
-        }}
-      />
-     {state.apps && (
-        <WidgetList
-          style={{
-            "list-style-type": "none",
-            padding: "0",
-            margin: "0",
-            maxHeight: "10",
+      <SearchContainer>
+        <Widget
+          loading={
+            <div className="input-group">
+              <input type="text" className="form-control" />
+            </div>
+          }
+          src="mob.near/widget/ComponentSearch"
+          props={{
+            boostedTag: "app",
+            placeholder: "Search Applications",
+            limit: 10,
+            onChange: ({ result }) => {
+              State.update({ apps: result });
+            },
           }}
-        >
-          <div className="mb-2">
-            {state.apps.map((app, i) => (
-              <WidgetItem key={i} onClick={(e) => handleClick(app)}>
-                <Widget
-                  src="littlelace.near/widget/ComponentItem"
-                  props={{
-                    link: `/${app.widgetSrc}`,
-                    accountId: app.accountId,
-                    widgetName: app.widgetName,
-                    onHide: () => State.update({ apps: null }),
-                  }}
-                />
-              </WidgetItem>
-            ))}
-          </div>
-        </WidgetList>
-      )}
+        />
+        {state.apps && (
+          <WidgetList >
+            <div className="mb-2">
+              {state.apps.map((app, i) => (
+                <WidgetItem key={i} onClick={(e) => handleClick(app)}>
+                  <Widget
+                    src="littlelace.near/widget/ComponentItem"
+                    props={{
+                      link: `/${app.widgetSrc}`,
+                      accountId: app.accountId,
+                      widgetName: app.widgetName,
+                      onHide: () => State.update({ apps: null }),
+                    }}
+                  />
+                </WidgetItem>
+              ))}
+            </div>
+
+          </WidgetList>
+        )}
+      </SearchContainer>
+      <SubmitButton onClick={()=> handleSubmit()}>Check Widget</SubmitButton>
     </Container>
   </WidgetApp>
 );
+
+// <Widget
+//   src="littlelace.near/widget/ComponentScroll"
+//   props={{ hovering }}
+// />
