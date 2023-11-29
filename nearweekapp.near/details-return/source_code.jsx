7b8ce4ca46b420ba3677aa379dceeeef5bf2c2e3
nearweekapp.near <nearweekapp.near>
2023-11-29 09:51:49 +0000

    Update details-return by nearweekapp.near at 2023-11-29T09:51:49.801Z

---
+++ nearweekapp.near/details-return/source_code.jsx
@@ -0,0 +1,26 @@
+const breakpoints = {
+  md: "768px",
+  lg: "1100px",
+  xl: "1300px",
+};
+
+function Return() {
+  return (
+    <svg
+      width="33"
+      height="20"
+      viewBox="0 0 33 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.0099 0.999878L2 10.0098L11.0099 19.0197"
+        stroke="black"
+        stroke-width="1.5"
+      />
+      <path d="M2.45117 9.99805H32.5684" stroke="black" stroke-width="1.5" />
+    </svg>
+  );
+}
+
+return <Return />;
