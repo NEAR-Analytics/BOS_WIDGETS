84cc69405c009384124a1f5b5e881b5483494ef4
jibolaojo.near <jibolaojo.near>
2023-11-29 04:38:14 +0000

    Update Assets by jibolaojo.near at 2023-11-29T04:38:14.633Z

---
+++ jibolaojo.near/Assets/source_code.jsx
@@ -0,0 +1,45 @@
+const res = fetch(
+  `https://api.nearblocks.io/v1/account/${
+    props.wallet_id || context.accountId
+  }/inventory`
+);
+
+if (!(res && res.body)) return "...";
+
+return (
+  <>
+    {res.body.inventory.fts.map((ft) => (
+      <div
+        key={ft.contract}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "#eee",
+          padding: "1rem",
+          marginBottom: "1rem",
+          cursor: "pointer",
+        }}
+      >
+        <img
+          height={40}
+          width={40}
+          layout="intrinsic"
+          src={
+            ft.ft_metas.icon ??
+            "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+          }
+        />
+        <div
+          style={{
+            marginLeft: "2rem",
+            fontWeight: 500,
+            fontSize: "1.125rem",
+          }}
+        >
+          {Number(ft.amount) / Math.pow(10, ft.ft_metas.decimals)}{" "}
+          {ft.ft_metas.symbol}
+        </div>
+      </div>
+    ))}
+  </>
+);
