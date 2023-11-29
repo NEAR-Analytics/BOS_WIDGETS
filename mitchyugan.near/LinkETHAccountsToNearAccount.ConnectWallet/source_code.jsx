755f00b2bf3f3d6808c13363bf019026724fd77f
mitchyugan.near <mitchyugan.near>
2023-11-29 16:30:18 +0000

    Update LinkETHAccountsToNearAccount.ConnectWallet by mitchyugan.near at 2023-11-29T16:30:18.146Z

--- mitchyugan.near/LinkETHAccountsToNearAccount.ConnectWallet/source_code.jsx
+++ mitchyugan.near/LinkETHAccountsToNearAccount.ConnectWallet/source_code.jsx
@@ -1,14 +1,36 @@
+const userId = context.accountId;
+
+const network1 = Storage.get("network1");
+console.log(network1);
+const loggedIn = true;
+
+const connectedAccountId = [
+  {
+    networkname: "Ethereum",
+    chainId: "ethereum0001",
+    accountID: 0x8efd7b62aff059615fb26b2cdb474c888c799d84,
+    status: "Connected",
+  },
+  {
+    networkname: "Polygon",
+    chainId: "polygon0001",
+    accountID: 0x8efd7b62aff059615fb26b2cdb474c888c799d84,
+    status: "Disconnected",
+  },
+];
+
 const Main = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Space+Grotesk:wght@700&display=swap');
   width:90%;
   margin: auto;
   h1{
-        text-align: center;
-        font-size: 64px;
-    font-weight: 700;
-    line-height: 120%;
-    margin-bottom: 16px;
-    width: 1000px;
-    margin:  auto;
+    text-align: center;
+    font-size: 64px;
+font-weight: 700;
+line-height: 120%;
+margin-bottom: 16px;
+width: 1000px;
+margin:  auto;
   }
   p{
     text-align: center;
