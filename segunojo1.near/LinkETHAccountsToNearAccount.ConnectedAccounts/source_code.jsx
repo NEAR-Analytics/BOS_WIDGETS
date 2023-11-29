38a483fcfc1f61b24def0f470ac896e684c26bb1
segunojo1.near <segunojo1.near>
2023-11-29 12:25:17 +0000

    Update LinkETHAccountsToNearAccount.ConnectedAccounts by segunojo1.near at 2023-11-29T12:25:17.610Z

---
+++ segunojo1.near/LinkETHAccountsToNearAccount.ConnectedAccounts/source_code.jsx
@@ -0,0 +1,136 @@
+const Header = styled.div`
+display: flex;
+justify-content: space-between;
+width: 90%;
+margin: auto;
+margin-bottom: 70px;
+
+h1{
+  font-size: 36px;
+font-style: normal;
+font-weight: 700;
+line-height: 100%; 
+}
+p{
+  color: #7C7C7C;
+margin: 0;
+font-family: 'Inter' sans-serif;
+font-size: 19px;
+font-style: normal;
+font-weight: 400;
+}
+`;
+
+const Chain = styled.div`
+display:  flex;
+align-items: center;
+gap: 12px;
+ border: 1px solid rgba(0, 0, 0, 0.22);
+  padding: 10px 25px;
+  gap: 24px;
+  border-radius: 12px;
+ p{
+  margin: 0;
+  font-size: 36px;
+font-style: normal;
+font-weight: 600;
+ }
+`;
+
+const Accounts = styled.div`
+display: flex;
+flex-direction : column;
+padding: 40.5px 44px;
+justify-content: center;
+align-items: flex-start;
+max-width: 600px;
+border-radius: 12px;
+border: 1px solid rgba(0, 0, 0, 0.22);
+
+`;
+const Main = styled.div`
+display: flex;
+flex-direction: column;
+gap: 12px;
+`;
+
+const Status =  styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+gap: 10px;
+`
+
+const Acct = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-between;
+margin-bottom: 10px;
+p{
+  margin: 0;
+}
+`
+
+const header = (
+  <Header>
+    <div>
+      <h1>Welcome 0x301</h1>
+      <p>Here’s your onchain activity history</p>
+    </div>
+    <div>
+      <Chain>
+        <img
+          src="https://i.ibb.co/j68BJpr/matic-logo-1.png"
+          alt="matic-logo-1"
+          border="0"
+        />
+        <h3>Polygon</h3>
+      </Chain>
+    </div>
+  </Header>
+);
+
+const main = (
+  <Main>
+    <Accounts>
+      <Acct>
+        <p>Account 1</p>
+        <Status>
+          <img src="https://i.ibb.co/RypQPsn/Group-3.png" />
+          <p>Connected</p>
+        </Status>
+      </Acct>
+      <p>Account ID: 0x8Efd7b62Aff059615FB26b2CDb474C888C799D84</p>
+      <button>Disconnect</button>
+    </Accounts>
+    <Accounts>
+      <Acct>
+        <p>Account 1</p>
+        <Status>
+          <img src="https://i.ibb.co/RypQPsn/Group-3.png" />
+          <p>Connected</p>
+        </Status>
+      </Acct>
+      <p>Account ID: 0x8Efd7b62Aff059615FB26b2CDb474C888C799D84</p>
+      <button>Disconnect</button>
+    </Accounts>
+    <Accounts>
+      <Acct>
+        <p>Account 1</p>
+        <Status>
+          <img src="https://i.ibb.co/RypQPsn/Group-3.png" />
+          <p>Connected</p>
+        </Status>
+      </Acct>
+      <p>Account ID: 0x8Efd7b62Aff059615FB26b2CDb474C888C799D84</p>
+      <button>Disconnect</button>
+    </Accounts>
+  </Main>
+);
+
+return (
+  <div>
+    {header}
+    {main}
+  </div>
+);
