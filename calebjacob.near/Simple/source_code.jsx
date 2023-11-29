f824630e9949a3590b51ad43fef15354af0ed6c1
calebjacob.near <calebjacob.near>
2023-11-29 17:54:00 +0000

    Update Simple by calebjacob.near at 2023-11-29T17:54:00.027Z

--- calebjacob.near/Simple/source_code.jsx
+++ calebjacob.near/Simple/source_code.jsx
@@ -4,11 +4,11 @@ const Wrapper = styled.div`
     background: black;
     font-family: sans-serif;
     border-radius: 10px;
-    border: 1px solid blue;
+    border: 1px solid red;
   }
 `;
 
-console.log("Hello");
+console.log("Hello!!!");
 console.log("Bye!!!");
 
 return (
