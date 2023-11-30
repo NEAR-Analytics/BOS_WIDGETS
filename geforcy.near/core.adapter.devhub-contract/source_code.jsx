d0db42fee374c4cb59546df9cb7eb3d03d1c3926
geforcy.near <geforcy.near>
2023-11-30 16:07:44 +0000

    Update core.adapter.devhub-contract by geforcy.near at 2023-11-30T16:07:44.801Z

--- geforcy.near/core.adapter.devhub-contract/source_code.jsx
+++ geforcy.near/core.adapter.devhub-contract/source_code.jsx
@@ -1,33 +1,43 @@
 function getRootMembers() {
-  return Near.view("devgovgigs.near", "get_root_members") ?? null;
+  return Near.view("geforcy.near", "get_root_members") ?? null;
+}
+
+function removeMember(member) {
+  return Near.call("geforcy.near", "remove_member", { member });
 }
 
 function hasModerator({ account_id }) {
   return (
-    Near.view("devgovgigs.near", "has_moderator", { account_id }) ??
+    Near.view("geforcy.near", "has_moderator", { account_id }) ??
     null
   );
 }
 
 function createCommunity({ inputs }) {
-  return Near.call("devgovgigs.near", "create_community", { inputs });
+  return Near.call("geforcy.near", "create_community", { inputs });
 }
 
 function getCommunity({ handle }) {
   return (
-    Near.view("devgovgigs.near", "get_community", { handle }) ?? null
+    Near.view("geforcy.near", "get_community", { handle }) ?? null
   );
 }
 
 function getFeaturedCommunities() {
   return (
-    Near.view("devgovgigs.near", "get_featured_communities") ?? null
+    Near.view("geforcy.near", "get_featured_communities") ?? null
   );
+}
+
+function setFeaturedCommunities({ handles }) {
+  return Near.call("geforcy.near", "set_featured_communities", {
+    handles,
+  });
 }
 
 function getAccountCommunityPermissions({ account_id, community_handle }) {
   return (
-    Near.view("devgovgigs.near", "get_account_community_permissions", {
+    Near.view("geforcy.near", "get_account_community_permissions", {
       account_id,
       community_handle,
     }) ?? null
@@ -35,25 +45,25 @@ function getAccountCommunityPermissions({ account_id, community_handle }) {
 }
 
 function updateCommunity({ handle, community }) {
-  return Near.call("devgovgigs.near", "update_community", {
+  return Near.call("geforcy.near", "update_community", {
     handle,
     community,
   });
 }
 
 function deleteCommunity({ handle }) {
-  return Near.call("devgovgigs.near", "delete_community", { handle });
+  return Near.call("geforcy.near", "delete_community", { handle });
 }
 
 function updateCommunityBoard({ handle, board }) {
-  return Near.call("devgovgigs.near", "update_community_board", {
+  return Near.call("geforcy.near", "update_community_board", {
     handle,
     board,
   });
 }
 
 function updateCommunityGithub({ handle, github }) {
-  return Near.call("devgovgigs.near", "update_community_github", {
+  return Near.call("geforcy.near", "update_community_github", {
     handle,
     github,
   });
@@ -63,7 +73,7 @@ function updateCommunityGithub({ handle, github }) {
  * Sets all addons, for configurating tabs
  */
 function setCommunityAddons({ handle, addons }) {
-  return Near.call("devgovgigs.near", "set_community_addons", {
+  return Near.call("geforcy.near", "set_community_addons", {
     handle,
     addons,
   });
@@ -73,7 +83,7 @@ function setCommunityAddons({ handle, addons }) {
  * Sets specific addon, for configuring params
  */
 function setCommunityAddon({ handle, addon }) {
-  return Near.call("devgovgigs.near", "set_community_addon", {
+  return Near.call("geforcy.near", "set_community_addon", {
     handle,
     community_addon: addon,
   });
@@ -83,22 +93,22 @@ function setCommunityAddon({ handle, addon }) {
  * Gets all available addons, these are controlled by devhub moderators
  */
 function getAllAddons() {
-  return Near.view("devgovgigs.near", "get_all_addons") ?? null;
+  return Near.view("geforcy.near", "get_all_addons") ?? null;
 }
 
 function getAccessControlInfo() {
   return (
-    Near.view("devgovgigs.near", "get_access_control_info") ?? null
+    Near.view("geforcy.near", "get_access_control_info") ?? null
   );
 }
 
 function getAllAuthors() {
-  return Near.view("devgovgigs.near", "get_all_authors") ?? null;
+  return Near.view("geforcy.near", "get_all_authors") ?? null;
 }
 
 function getAllCommunitiesMetadata() {
   return (
-    Near.view("devgovgigs.near", "get_all_communities_metadata") ?? null
+    Near.view("geforcy.near", "get_all_communities_metadata") ?? null
   );
 }
 
@@ -145,39 +155,39 @@ function getAvailableAddons() {
         "geforcy.near/widget/devhub.entity.addon.blog.Configurator",
     },
   ];
-  // return Near.view("devgovgigs.near", "get_available_addons") ?? null;
+  // return Near.view("geforcy.near", "get_available_addons") ?? null;
 }
 
 function getCommunityAddons({ handle }) {
-  return Near.view("devgovgigs.near", "get_community_addons", {
+  return Near.view("geforcy.near", "get_community_addons", {
     handle,
   });
 }
 
 function getCommunityAddonConfigs({ handle }) {
-  return Near.view("devgovgigs.near", "get_community_addon_configs", {
+  return Near.view("geforcy.near", "get_community_addon_configs", {
     handle,
   });
 }
 
 function getAllLabels() {
-  return Near.view("devgovgigs.near", "get_all_labels") ?? null;
+  return Near.view("geforcy.near", "get_all_labels") ?? null;
 }
 
 function getPost({ post_id }) {
-  return Near.view("devgovgigs.near", "get_post", { post_id }) ?? null;
+  return Near.view("geforcy.near", "get_post", { post_id }) ?? null;
 }
 
 function getPostsByAuthor({ author }) {
   return (
-    Near.view("devgovgigs.near", "get_posts_by_author", { author }) ??
+    Near.view("geforcy.near", "get_posts_by_author", { author }) ??
     null
   );
 }
 
 function getPostsByLabel({ label }) {
   return (
-    Near.view("devgovgigs.near", "get_posts_by_label", {
+    Near.view("geforcy.near", "get_posts_by_label", {
       label,
     }) ?? null
   );
@@ -189,7 +199,7 @@ function useQuery(name, params) {
   const cacheState = useCache(
     () =>
       Near.asyncView(
-        "devgovgigs.near",
+        "geforcy.near",
         ["get", name].join("_"),
         params ?? {}
       )
@@ -213,10 +223,12 @@ function useQuery(name, params) {
 
 return {
   getRootMembers,
+  removeMember,
   hasModerator,
   createCommunity,
   getCommunity,
   getFeaturedCommunities,
+  setFeaturedCommunities,
   getAccountCommunityPermissions,
   updateCommunity,
   deleteCommunity,
