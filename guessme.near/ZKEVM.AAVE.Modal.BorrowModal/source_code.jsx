b4731d64dada0f5d65187755a1a88b6f18e4d335
guessme.near <guessme.near>
2023-11-30 01:20:40 +0000

    Update ZKEVM.AAVE.Modal.BorrowModal by guessme.near at 2023-11-30T01:20:40.361Z

--- guessme.near/ZKEVM.AAVE.Modal.BorrowModal/source_code.jsx
+++ guessme.near/ZKEVM.AAVE.Modal.BorrowModal/source_code.jsx
@@ -336,11 +336,11 @@ const AccessKey = Storage.get(
   "guessme.near/widget/ZKEVMWarmUp.add-to-quest-card"
 );
 function add_action(param_body) {
-  asyncFetch("https://test-api.dapdap.net/api/action/add", {
+  asyncFetch("/dapdap/api/action/add", {
     method: "post",
     headers: {
       "Content-Type": "application/json",
-      Authorization: AccessKey
+      Authorization: AccessKey,
     },
     body: JSON.stringify(param_body),
   });
@@ -425,11 +425,11 @@ function update() {
   }
 }
 function add_action(param_body) {
-  asyncFetch("https://test-api.dapdap.net/api/action/add", {
+  asyncFetch("/dapdap/api/action/add", {
     method: "post",
     headers: {
       "Content-Type": "application/json",
-      Authorization: AccessKey
+      Authorization: AccessKey,
     },
     body: JSON.stringify(param_body),
   });
