36be328faa0630118ae5eec37cb97e03202aaeb4
hyperfiles.near <hyperfiles.near>
2023-11-30 02:52:36 +0000

    Update create by hyperfiles.near at 2023-11-30T02:52:36.086Z

--- hyperfiles.near/create/source_code.jsx
+++ hyperfiles.near/create/source_code.jsx
@@ -1,139 +1,231 @@
-const item = props.item;
-const onChange = props.onChange;
-
-const Input = styled.input`
-  height: 30px;
+const Wrapper = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
 `;
 
-const Select = styled.select`
-  height: 30px;
+const TabContent = styled.div`
+  margin-top: 1rem;
 `;
 
-const Button = styled.button`
-  text-transform: lowercase !important;
-  padding: 8px;
-`;
-
-const Container = styled.div`
+const Form = styled.div`
   display: flex;
   flex-direction: column;
   gap: 4px;
-`;
-
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
+  width: 100%;
 `;
 
 const Label = styled.label`
+  font-weight: bold;
 `;
 
-State.init({
-  ...item.value,
-});
+const Input = styled.input`
+  padding: 5px;
+`;
 
-const DynamicInput = ({ type, onChange, value, placeholder }) => {
-  if (type === "boolean") {
-    return (
-      <Select onChange={onChange} value={value}>
-        <option value="true">true</option>
-        <option value="false">false</option>
-      </Select>
-    );
-  } else {
-    return (
-      <Input
-        type={type}
-        onChange={onChange}
-        value={value}
-        placeholder={placeholder}
-      />
-    );
-  }
-};
+const Select = styled.select`
+  padding: 8px;
+`;
 
-// Primitive checks
-if (["string", "number", "date", "time", "boolean"].includes(item.type)) {
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const adapters = [
+  // these can come from the user (or app) settings
+  // {
+  //   title: "Local Storage",
+  //   value: "everycanvas.near/widget/adapter.local_storage",
+  //   saveRef: false
+  // },
+  // {
+  //   title: "SocialDB",
+  //   value: "everycanvas.near/widget/adapter.social",
+  // },
+  {
+    title: "IPFS",
+    value: "everycanvas.near/widget/adapter.ipfs",
+  },
+  // {
+  //   title: "GitHub",
+  //   value: "hack.near/widget/adapter.github",
+  // },
+  // {
+  //   title: "Obsidian",
+  //   value: "hack.near/widget/adapter.obsidian",
+  // },
+  // {
+  //   title: "Tldraw",
+  //   value: "hack.near/widget/adapter.tldraw",
+  // },
+];
+
+const defaultAdapter = adapters[0];
+
+const { creatorId } = props;
+
+const [json, setJson] = useState(props.data ?? "");
+const [source, setSource] = useState(props.source ?? "");
+const [adapter, setAdapter] = useState(defaultAdapter.value ?? "");
+const [reference, setReference] = useState(undefined);
+const [filename, setFilename] = useState(props.filename ?? "");
+const [activeTab, setActiveTab] = useState("data");
+const [name, setName] = useState(props.name ?? "");
+const [description, setDescription] = useState(props.description ?? "");
+
+function generateUID() {
   return (
-    <DynamicInput
-      type={item.type === "string" ? "text" : item.type}
-      onChange={onChange}
-      value={item.value}
-    />
+    Math.random().toString(16).slice(2) +
+    Date.now().toString(36) +
+    Math.random().toString(16).slice(2)
   );
 }
 
-// On-chain Type
-const type = JSON.parse(Social.get(item.type, "final") || "null");
-const properties = type.properties || [];
-const createWidgetSrc = type.widgets?.create;
+const handleCreate = () => {
+  const isCreator = context.accountId === creatorId;
 
-const handleInputChange = (name, value) => {
-  State.update({ [name]: value });
-  if (props.onChange) {
-    props.onChange({ [name]: value });
+  // load in the state.adapter (modules for IPFS, Arweave, Ceramic, Verida, On Machina... )
+  const { create } = VM.require(adapter) || (() => {});
+  if (create) {
+    // store the data somewhere, based on the adapter
+    create(json).then((reference) => {
+      // now we have a reference to the data
+      // we need to name it... are we the original creator or are we forking? We don't want to overwrite any of the users custom (or maybe we do!)
+      const thingId = filename ?? generateUID();
+
+      const hyperfile = {
+        [props.type]: {
+          // which we store in the social contract
+          [thingId]: {
+            "": JSON.stringify({
+              fileformat: `${props.type}.${source}`,
+              source: source,
+              adapter: adapter,
+              reference: reference,
+            }),
+            metadata: {
+              name: name,
+              description: description,
+              type: props.type,
+            },
+          },
+        },
+      };
+
+      if (creatorId !== context.accountId) {
+        // handle request merge
+        hyperfile.index = {
+          notify: JSON.stringify({
+            key: creatorId,
+            value: {
+              type: "request",
+              data: {
+                type: "merge",
+                upstream: `${creatorId}/${props.type}/${props.filename}`,
+                origin: `${context.accountId}/${props.type}/${thingId}`,
+              },
+            },
+          }),
+        };
+        hyperfile[props.type][thingId].metadata = {
+          ...hyperfile[props.type][thingId].metadata,
+          upstream: `${creatorId}/${props.type}/${props.filename}`,
+        };
+        // I want to make a request to merge
+        // set upstream and downstream
+      }
+
+      // sometimes we're not logged in, so it doesn't do anything!
+      Social.set(hyperfile, { force: true });
+    });
   }
 };
 
-function Property({ property, value }) {
-  // If property is multiple values
-  if (property.isMulti === "true") {
-    // Build an array (recursively calls this Widget)
-    return (
-      <Widget
-        src="efiz.near/widget/every.array.build"
-        props={{
-          item: { ...property, value },
-          onChange: (val) => handleInputChange(property.name, val),
-        }}
-      />
-    );
-  }
-  // Else check for primitives
-  if (["string", "number", "date", "time", "boolean"].includes(property.type)) {
-    return (
-      <DynamicInput
-        type={property.type === "string" ? "text" : property.type}
-        onChange={(e) => handleInputChange(property.name, e.target.value)}
-        value={state[property.name] || ""}
-        placeholder={property.name}
-      />
-    );
-  } else {
-    // This requires a specific type of creator
-    // (like image upload)
-    // TODO: I don't think this does what I want it to yet...
-    const propertyType = JSON.parse(
-      Social.get(property.type, "final") || "null"
-    );
-    const widgetSrc = propertyType?.widgets?.create;
-    // it would be great to modify the onChange function
-    return (
-      <Widget
-        src={widgetSrc}
-        props={{ onChange: (e) => handleInputChange(property.name, e) }}
-      />
-    );
-  }
-}
-
 return (
-  <Container>
-    {createWidgetSrc ? (
-      <>
-        <Widget src={createWidgetSrc} props={{ onChange }} />
-      </>
-    ) : (
-      <>
-        {properties?.map((property) => (
-          <div key={property.name}>
-            <Label>{property.name}</Label>
-            <Row>
-              <Property property={property} value={item.value[property.name]} />
-            </Row>
-          </div>
-        ))}
-      </>
-    )}
-  </Container>
+  <Wrapper>
+    <h3>{context.accountId === creatorId ? "create" : "request merge"}</h3>
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          className={`nav-link ${activeTab === "data" ? "active" : ""}`}
+          onClick={() => setActiveTab("data")}
+        >
+          Data
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          className={`nav-link ${activeTab === "metadata" ? "active" : ""}`}
+          onClick={() => setActiveTab("metadata")}
+        >
+          Metadata
+        </a>
+      </li>
+    </ul>
+
+    <TabContent>
+      {activeTab === "data" && (
+        <Form>
+          <FormGroup>
+            <Label>source</Label>
+            <Input
+              type="text"
+              value={source}
+              onChange={(e) => onChangeSource(e.target.value)}
+              disabled={props.source} // disable if source is passed in
+            />
+          </FormGroup>
+          {/* <Widget
+            src="bozon.near/widget/CodeDiff"
+            props={{ currentCode: update, prevCode: src, ...props }}
+          /> */}
+          <textarea
+            className="form-control mb-3"
+            rows={5}
+            value={json}
+            onChange={(e) => setJson(e.target.value)}
+          />
+          <FormGroup>
+            <Label>adapter</Label>
+            <Select
+              value={adapter}
+              onChange={(e) => setAdapter(e.target.value)}
+            >
+              {adapters.map((o) => (
+                <option value={o.value}>{o.title}</option>
+              ))}
+            </Select>
+          </FormGroup>
+        </Form>
+      )}
+    </TabContent>
+    <TabContent>
+      {activeTab === "metadata" && (
+        <Form>
+          <FormGroup>
+            <Label>name</Label>
+            <Input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>description</Label>
+            <textarea
+              className="form-control mb-3"
+              rows={5}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </FormGroup>
+        </Form>
+      )}
+    </TabContent>
+    <FormGroup>
+      <button className="btn btn-success mb-1" onClick={handleCreate}>
+        Create
+      </button>
+    </FormGroup>
+  </Wrapper>
 );
