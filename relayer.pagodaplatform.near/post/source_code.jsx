e5ccc8a721318be81f449c4f179916d8cfdce7e4
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-11-30 14:38:11 +0000

    Update post by relayer.pagodaplatform.near at 2023-11-30T14:38:11.067Z

---
+++ relayer.pagodaplatform.near/post/source_code.jsx
@@ -0,0 +1,41 @@
+const CityContainer = styled.div`
+  display: flex;
+  border: 1px solid #151515;
+  background-color: #FFFFFF;
+  padding: 16px;
+  border-radius: 15px
+`;
+
+const CityInfo = styled.div`
+  flex: 1;
+  padding: 10px;
+`;
+
+const VotesText = styled.p`
+  margin: 5px;
+`;
+
+const info = props.information;
+
+State.init({
+  showVoters: false,
+  name: info.name,
+  description: String(info.description),
+  authorId: info.authorId,
+  timestamp: info.timestamp,
+});
+
+let des = state.description.split("~");
+
+return (
+  <CityContainer>
+    <CityInfo>
+      <h2>{state.name}</h2>
+      <VotesText>Description: {des[2]}</VotesText>
+      <VotesText>{des[0]}</VotesText>
+      <VotesText>{des[1]}</VotesText>
+      <VotesText>Id: {state.authorId}</VotesText>
+      <VotesText>Timestamp: {state.timestamp}</VotesText>
+    </CityInfo>
+  </CityContainer>
+);
