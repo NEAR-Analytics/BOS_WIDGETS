da37aa0c01e83b152df1916d6a6c71eac9535c83
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-11-30 10:37:13 +0000

    Update post by relayer.pagodaplatform.near at 2023-11-30T10:37:13.515Z

--- relayer.pagodaplatform.near/post/source_code.jsx
+++ relayer.pagodaplatform.near/post/source_code.jsx
@@ -1,9 +1,13 @@
+const Button = styled.button`
+  margin: 5px;
+  background-color: #00EC97;
+  color: #151515;
+`;
+
 const CityContainer = styled.div`
   display: flex;
   border: 1px solid #151515;
-  background-color: #FFFFFF;
-  padding: 16px;
-  border-radius: 15px
+  background-color: #F4F4F4;
 `;
 
 const CityInfo = styled.div`
@@ -11,7 +15,18 @@ const CityInfo = styled.div`
   padding: 10px;
 `;
 
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding: 10px;
+`;
+
 const VotesText = styled.p`
+  margin: 5px;
+`;
+
+const Voters = styled.p`
   margin: 5px;
 `;
 
@@ -19,22 +34,18 @@ const info = props.information;
 
 State.init({
   showVoters: false,
-  name: info.name,
-  description: String(info.description),
-  authorId: info.authorId,
-  timestamp: info.timestamp,
+  name: info[0],
+  description: info[1],
+  authorId: info[2],
+  timestamp: info[3],
 });
-
-let des = state.description.split("~");
 
 return (
   <CityContainer>
     <CityInfo>
       <h2>{state.name}</h2>
-      <VotesText>Description: {des[2]}</VotesText>
-      <VotesText>{des[0]}</VotesText>
-      <VotesText>{des[1]}</VotesText>
-      <VotesText>Id: {state.authorId}</VotesText>
+      <VotesText>Description: {state.description}</VotesText>
+      <VotesText>AuthorId: {state.authorId}</VotesText>
       <VotesText>Timestamp: {state.timestamp}</VotesText>
     </CityInfo>
   </CityContainer>
