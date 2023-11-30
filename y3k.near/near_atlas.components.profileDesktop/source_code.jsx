673a8255ed21002e259b349d8bb20f6b57dedd37
y3k.near <y3k.near>
2023-11-30 14:49:38 +0000

    Update near_atlas.components.profileDesktop by y3k.near at 2023-11-30T14:49:38.759Z

--- y3k.near/near_atlas.components.profileDesktop/source_code.jsx
+++ y3k.near/near_atlas.components.profileDesktop/source_code.jsx
@@ -50,139 +50,69 @@ const shareSvg = (
 );
 return (
   <Wrapper>
-    <div className="px-4 pt-0 pb-5 bg-gray-800 relative">
-      {backgroundImage && (
-        <Widget
-          src="mob.near/widget/Image"
-          props={{
-            image: backgroundImage,
-            alt: "profile background",
-            className: "position-absolute w-100 h-100",
-            style: { objectFit: "cover", left: 0, top: 0 },
-            fallbackUrl:
-              "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
-          }}
-        />
-      )}
-      <div className="inline-block" style={{ transform: "translateY(7rem)" }}>
-        <Widget
-          src="mob.near/widget/ProfileImage"
-          props={{
-            profile,
-            fast,
-            accountId,
-            style: { width: "10rem", height: "10rem" },
-            className: "mb-2",
-            imageClassName: "rounded-circle w-100 h-100 img-thumbnail d-block",
-            thumbnail: false,
-          }}
-        />
-      </div>
-    </div>
-    <div className="px-4 pb-4 bg-white">
-      <div
-        className="flex justify-end items-center h-16"
-        style={{ height: "4rem" }}
-      >
-        {showEditButton && (
-          <div>
-            <a
-              href="#/mob.near/widget/ProfileEditor"
-              className="btn btn-outline-secondary rounded-5"
-            >
-              Edit profile
-            </a>
-          </div>
-        )}
-      </div>
-      <div className="flex md:justify-between mb-2 clear-both">
-        <div>
-          <div className="me-2 d-sm-flex gap-1 flex-row align-items-center">
-            <div className="me-2 position-relative">
-              <div className="d-flex text-truncate">
-                {link ? (
-                  <a className="text-truncate text-dark" href={link}>
-                    {nameHeader}
-                  </a>
-                ) : (
-                  nameHeader
-                )}
-                <Widget src="mob.near/widget/Checkmark" props={{ accountId }} />
-              </div>
-              <div className="small text-truncate">
-                <i className="bi bi-person-fill text-secondary"></i>
-                {accountId}
-                <Widget
-                  src="mob.near/widget/CopyButton"
-                  props={{
-                    text: accountId,
-                    className: "btn btn-sm btn-outline-dark border-0",
-                  }}
-                />
-                <Widget
-                  src="mob.near/widget/FollowsYouBadge"
-                  props={{ accountId }}
-                />
-              </div>
-            </div>
-
+    <div className="p-16">
+      <div className="p-8 bg-white shadow mt-24">
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
             <div>
-              <Widget
-                src="mob.near/widget/FollowButton"
-                props={{ accountId }}
-              />
-              <Widget src="mob.near/widget/PokeButton" props={{ accountId }} />
+              <p className="font-bold text-gray-700 text-xl">22</p>
+              <p className="text-gray-400">Friends</p>
+            </div>
+            <div>
+              <p className="font-bold text-gray-700 text-xl">10</p>
+              <p className="text-gray-400">Photos</p>
+            </div>
+            <div>
+              <p className="font-bold text-gray-700 text-xl">89</p>
+              <p className="text-gray-400">Comments</p>
             </div>
           </div>
-          <div>
-            <Widget src="mob.near/widget/FollowStats" props={{ accountId }} />
+          <div className="relative">
+            <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-24 w-24"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clip-rule="evenodd"
+                />
+              </svg>{" "}
+            </div>
+          </div>
+          <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
+            <button className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+              Connect
+            </button>{" "}
+            <button className="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+              Message
+            </button>{" "}
           </div>
         </div>
-        <div style={{ minWidth: "12rem" }}>
-          <Widget
-            src="mob.near/widget/LinkTree"
-            props={{ linktree: profile.linktree }}
-          />
+        <div className="mt-20 text-center border-b pb-12">
+          <h1 className="text-4xl font-medium text-gray-700">
+            Jessica Jones, <span className="font-light text-gray-500">27</span>
+          </h1>
+          <p className="font-light text-gray-600 mt-3">Bucharest, Romania</p>
+          <p className="mt-8 text-gray-500">
+            Solution Manager - Creative Tim Officer
+          </p>
+          <p className="mt-2 text-gray-500">University of Computer Science</p>
         </div>
-      </div>
-
-      {tags.length > 0 && (
-        <div>
-          {tags.map((tag, i) => (
-            <span
-              key={i}
-              className="me-1 mb-1 fw-light badge border border-secondary text-bg-light"
-            >
-              #{tag}
-            </span>
-          ))}
-        </div>
-      )}
-
-      <div>
-        <div className="float-end">
-          <Widget
-            src="mob.near/widget/CopyButton"
-            props={{
-              text: link,
-              label: "Share",
-              clipboardIcon: shareSvg,
-            }}
-          />
-        </div>
-        <div className="public-tags collapse show">
-          <button
-            className="btn btn-sm btn-outline-secondary border-0"
-            data-bs-toggle="collapse"
-            data-bs-target={`.public-tags`}
-            aria-expanded="false"
-            aria-controls={"public-tags"}
-          >
-            <i className="bi bi-arrows-angle-expand me-1"></i>Show public tags
+        <div className="mt-12 flex flex-col justify-center">
+          <p className="text-gray-600 text-center font-light lg:px-16">
+            An artist of considerable range, Ryan — the name taken by
+            Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and
+            records all of his own music, giving it a warm, intimate feel with a
+            solid groove structure. An artist of considerable range.
+          </p>{" "}
+          <button className="text-indigo-500 py-2 px-4  font-medium mt-4">
+            {" "}
+            Show more
           </button>
-        </div>
-        <div className="collapse public-tags">
-          <Widget src="mob.near/widget/PublicTags" props={{ accountId }} />
         </div>
       </div>
     </div>
