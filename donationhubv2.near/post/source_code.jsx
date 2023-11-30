85d2ff6593aa84e06e05286d72d6a6dd784fc1fe
donationhubv2.near <donationhubv2.near>
2023-11-30 15:28:36 +0000

    Update post by donationhubv2.near at 2023-11-30T15:28:36.854Z

--- donationhubv2.near/post/source_code.jsx
+++ donationhubv2.near/post/source_code.jsx
@@ -27,6 +27,29 @@ State.init({
 
 let des = state.description.split("~");
 
+function convertTimestampToReadableDate(timestamp) {
+  // Multiply by 1000 to convert seconds to milliseconds
+  const date = new Date(timestamp * 1000);
+
+  // Options for formatting the date
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZoneName: "short",
+  };
+
+  // Format the date according to the options
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  return formattedDate;
+}
+
+const readableDate = convertTimestampToReadableDate(state.timestamp);
+
 return (
   <CityContainer>
     <CityInfo>
@@ -35,7 +58,7 @@ return (
       <VotesText>{des[0]}</VotesText>
       <VotesText>{des[1]}</VotesText>
       <VotesText>Posted By: {state.authorId}</VotesText>
-      <VotesText>Timestamp: {state.timestamp}</VotesText>
+      <VotesText>Published: {readableDate}</VotesText>
     </CityInfo>
   </CityContainer>
 );
