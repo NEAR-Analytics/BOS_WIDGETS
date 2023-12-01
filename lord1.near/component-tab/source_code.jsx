736c02216bc057fb7624250e1b0281de871d21ed
lord1.near <lord1.near>
2023-12-01 16:35:48 +0000

    Update component-tab by lord1.near at 2023-12-01T16:35:48.383Z

--- lord1.near/component-tab/source_code.jsx
+++ lord1.near/component-tab/source_code.jsx
@@ -1,4 +1,113 @@
 const hash = "fe16224c-e176-4479-a40f-2e720e1b8cdf";
+const themeColor = props.themeColor || {
+  page_bg: "rgb(241,242,245)",
+  horizen_bg: "#391b86",
+  header_bg: "rgb(210, 202, 250)",
+  sideBar: {
+    sideBar_bg: "rgb(210, 202, 250)",
+    sideBar_color: "#fff",
+  },
+  footer: {
+    titlenelowBackground: "#806ce1",
+    titleBackground: "#fff",
+    fromBackground: "rgb(210, 202, 250)",
+    toBackground: "rgb(210, 202, 250)",
+    belowBackground: "#806ce1",
+  },
+  dynamic_header: {
+    afterbrandcolor: "#789efb",
+    color1brand: "#000",
+    color2brand: "#806ce1",
+    colordescription: "#806ce1",
+    background:
+      "radial-gradient(circle, rgba(210,202,250,1) 0%, rgba(230,230,231,0.01) 0%, rgba(235,238,255,1) 100%, rgba(235,231,253,1) 100%, rgba(255,241,241,1) 100%, rgba(46,52,90,1) 100%);",
+  },
+  search_sbt: {
+    section_bg: "rgb(235, 231, 253)",
+    card_bg: "",
+    search_btn_bg: "rgb(210, 202, 250)",
+    search_btn_bg_hover: "rgba(210, 202, 250,0.8)",
+    search_btn_text: "rgb(0,0,0)",
+    input_bg: "rgba(210, 202, 250,0.2)",
+    input_bg_hover: "rgba(210, 202, 250,0.4)",
+    input_text_color: "rgb(0,0,0)",
+    input_border: "rgba(210, 202, 250,0.4)",
+    table_bg: "transparent",
+    table_color: "rgb(0,0,0)",
+    table_border_color: "",
+    table_accent_bg: "",
+    table_striped_color: "rgb(0,0,0)",
+    table_striped_bg: "",
+    table_hover_color: "rgb(0,0,0)",
+    table_hover_bg: "",
+  },
+  sbt_info: {
+    section_bg: "rgb(235, 231, 253)",
+    card_bg: "rgb(255, 255, 255)",
+    card_title_color: "#806ce1",
+    card_content_color: "#000",
+  },
+  tab_sbt: {
+    backgroundColor: "rgb(210, 202, 250)",
+    textColor: "#fff",
+    headerColor: "#806ce1",
+    numberColor: "#fff",
+  },
+  sbt_area: {
+    section_bg: "rgb(235, 231, 253)",
+    card_bg: "rgb(255, 255, 255)",
+    card_title_color: "#806ce1",
+  },
+  table_pagination: {
+    table_bg: "rgb(255,255,255)",
+    table_color: "rgb(0,0,0)",
+    table_border_color: "",
+    table_accent_bg: "",
+    table_striped_color: "rgb(0,0,0)",
+    table_striped_bg: "",
+    table_hover_color: "rgb(0,0,0)",
+    table_hover_bg: "",
+    btn_border: "#806ce1",
+    btn_bg: "#fff",
+    btn_bg_active: "rgb(235, 231, 253)",
+    btn_color: "#000",
+  },
+  component_tab: {
+    card: "red",
+    text_title: "green",
+    text: "yellow",
+    backgroundColor: "#391b86",
+  },
+  chart: {
+    title: "rgba(0,0,0,1)",
+    subtitle: "rgba(0,0,0,0.7)",
+    xAxis: "rgba(0,0,0,1)",
+    yAxis: "rgba(0,0,0,1)",
+    legend: "rgba(0,0,0,0.7)",
+    legendHover: "rgba(0,0,0,1)",
+    rangeSelector: {
+      labels: "rgba(0,0,0,0.7)",
+      inputColor: "rgba(0,0,0,0.5)",
+      btn_bg: "rgba(0,0,0,0.3)",
+      btn_color: "rgba(0,0,0,0.8)",
+      btn_hover_bg: "rgba(0,0,0,0.4)",
+      btn_hover_color: "rgba(0,0,0,1)",
+      btn_active_bg: "rgb(235, 231, 253)",
+      btn_active_color: "rgba(0,0,0,1)",
+    },
+  },
+  spinnerColors: ["#6F61C0", "#241468"],
+  chartColor: [
+    "#F79BD3",
+    "#A084E8",
+    "#EA1179",
+    "#F79BD3",
+    "#A084E8",
+    "#6F61C0",
+    "#241468",
+    "#9F0D7F",
+  ],
+};
 const columnNamesFinall = [
   "Transactions",
   "Components",
@@ -93,17 +202,18 @@ const formatNumber = (num) => {
 const noData = <div className="w-100 py-4 text-center">No data available</div>;
 
 const ParentDiv = styled.div`
-color:${props.textColor};
+color:${themeColor.component_tab.text};
 flex-basis:23%;
 padding:1rem;
 min-height:150px;
 max-height:300px;
-background:${props.backgroundColor};
+background:${themeColor.component_tab.backgroundColor};
 border-radius:25px;
 transition:0.3s all;
 box-shadow: "0px 0px 10px -1px  #806ce1";
 &:hover{
-  transform:scale(1.1)
+  transform:scale(1.1);
+  opacity:0.9
 }
 
 @media only screen and (max-width: 770px) {
@@ -136,7 +246,7 @@ const onHandelDate = (inputDate) => {
 };
 
 return (
-  <div className="d-flex flex-wrap">
+  <div className="d-flex flex-wrap justify-content-around gap-1">
     <div className="toast-container position-fixed bottom-0 end-0 p-3">
       {state.error.length > 0 &&
         state.error.map((er) => (
@@ -165,7 +275,13 @@ return (
               </Tooltip>
             }
           >
-            <div style={{ display: "flex", flexWrap: "nowrap" }}>
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "nowrap",
+                color: themeColor.component_tab.text_title,
+              }}
+            >
               <img
                 style={{ marginRight: "10px" }}
                 width={20}
@@ -176,15 +292,15 @@ return (
               </h5>
             </div>
           </OverlayTrigger>
-          <h4 style={{ color: props.numberColor }}>
+          <h4 style={{ color: themeColor.component_tab.text_title }}>
             {formatNumber(state.data[0].total_trxs)}
           </h4>
           <Widget
             src={"rubycop.near/widget/NDC.StyledComponents"}
-            props={{ Tag: { title: "", className: props.dark } }}
+            props={{ Tag: { title: "" } }}
           />
 
-          <p style={{ color: props.textColor }}>
+          <p>
             <div>
               Build Transactions:
               <span style={{ color: props.numberintextColor }}>
@@ -207,7 +323,7 @@ return (
             </span>
           </p>
         </ParentDiv>
-        <ParentDiv onClick={res}>
+        <ParentDiv>
           <OverlayTrigger
             key="top"
             overlay={
@@ -218,7 +334,13 @@ return (
               </Tooltip>
             }
           >
-            <div style={{ display: "flex", flexWrap: "nowrap" }}>
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "nowrap",
+                color: themeColor.component_tab.text_title,
+              }}
+            >
               <img
                 style={{ marginRight: "10px" }}
                 width={20}
@@ -230,14 +352,14 @@ return (
             </div>
           </OverlayTrigger>
 
-          <h4 style={{ color: props.numberColor }}>
+          <h4 style={{ color: themeColor.component_tab.text_title }}>
             {formatNumber(state.data[0].widget)}
           </h4>
           <Widget
             src={"rubycop.near/widget/NDC.StyledComponents"}
             props={{ Tag: { title: "", className: props.dark } }}
           />
-          <p style={{ color: props.textColor }}>
+          <p>
             <div>
               Total Components:
               <span style={{ color: props.numberintextColor }}>
@@ -271,7 +393,13 @@ return (
               </Tooltip>
             }
           >
-            <div style={{ display: "flex", flexWrap: "nowrap" }}>
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "nowrap",
+                color: themeColor.component_tab.text_title,
+              }}
+            >
               <img
                 style={{ marginRight: "10px" }}
                 width={20}
@@ -283,14 +411,14 @@ return (
             </div>
           </OverlayTrigger>
 
-          <h4 style={{ color: props.numberColor }}>
+          <h4 style={{ color: themeColor.component_tab.text_title }}>
             {formatNumber(state.data[0].days)}
           </h4>
           <Widget
             src={"rubycop.near/widget/NDC.StyledComponents"}
             props={{ Tag: { title: "", className: props.dark } }}
           />
-          <p style={{ color: props.textColor }}>
+          <p>
             <div>
               Active Since:
               <span style={{ color: props.numberintextColor }}>
@@ -320,7 +448,13 @@ return (
               <Tooltip>Number of stars received or sent in total</Tooltip>
             }
           >
-            <div style={{ display: "flex", flexWrap: "nowrap" }}>
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "nowrap",
+                color: themeColor.component_tab.text_title,
+              }}
+            >
               <img
                 style={{ marginRight: "10px" }}
                 width={20}
@@ -332,14 +466,14 @@ return (
             </div>
           </OverlayTrigger>
 
-          <h4 style={{ color: props.numberColor }}>
+          <h4 style={{ color: themeColor.component_tab.text_title }}>
             {formatNumber(state.data[0].stars_received)}
           </h4>
           <Widget
             src={"rubycop.near/widget/NDC.StyledComponents"}
             props={{ Tag: { title: "", className: props.dark } }}
           />
-          <p style={{ color: props.textColor }}>
+          <p>
             <div>
               Star Received:
               <span style={{ color: props.numberintextColor }}>
