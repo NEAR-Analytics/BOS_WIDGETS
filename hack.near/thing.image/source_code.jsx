616d4845b22967b5a5c6b9ec81c496ac9714c5c9
hack.near <hack.near>
2023-12-02 18:50:59 +0000

    Update thing.image by hack.near at 2023-12-02T18:50:59.602Z

--- hack.near/thing.image/source_code.jsx
+++ hack.near/thing.image/source_code.jsx
@@ -1,35 +1,69 @@
-const accountId = props.accountId ?? context.accountId;
-const thingId = props.thingId ?? "f8ad9d1a76259lmdpjnd74e69162a0a014";
+const creatorId = props.creatorID ?? context.accountId;
+const namespace = props.namespace ?? "widget";
+const thingId = props.thingId ?? "widgets.rank";
 
 const className = props.className ?? "thing-image d-inline-block";
 const style = props.style ?? { width: "3em", height: "3em" };
 const imageStyle = props.imageStyle ?? { objectFit: "cover" };
-const imageClassName = props.imageClassName ?? "rounded w-100 h-100";
+const imageClassName = props.imageClassName ?? "rounded-circle w-100 h-100";
 const thumbnail = props.thumbnail ?? "thumbnail";
 
 const thing =
-  props.thing ?? Social.get(`${accountId}/thing/${thingId}/metadata/**`);
+  props.thing ?? Social.getr(`${creatorId}/${namespace}/${thingId}`);
 
-const name = thing.name || "No-name thing";
-const image = thing.image;
-const title = props.title ?? `${name} @${accountId}`;
-const tooltip =
-  props.tooltip && (props.tooltip === true ? title : props.tooltip);
+const name = thing.metadata.name || "Something";
+const image = thing.metadata.image;
+const title = props.title ?? `${name}`;
+const fast = props.fast || (!props.thing && !!creatorId);
+if (creatorId !== state.creatorId) {
+  State.update({
+    fastImageUrl: `https://i.near.social/magic/${
+      thumbnail || "large"
+    }/https://near.social/magic/img/account/${creatorId}`,
+    creatorId,
+  });
+}
 const fallbackUrl =
   "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm";
 
 return (
-  <div className={className} style={style} key={JSON.stringify(image)}>
-    <Widget
-      src="mob.near/widget/Image"
-      props={{
-        image: thing.image,
-        alt: title,
-        className: imageClassName,
-        style: imageStyle,
-        thumbnail,
-        fallbackUrl,
-      }}
-    />
-  </div>
+  <>
+    {fast ? (
+      <div className={className} style={style} key={state.fastImageUrl}>
+        <img
+          className={imageClassName}
+          style={imageStyle}
+          src={state.fastImageUrl}
+          alt={title}
+          onError={() => {
+            if (state.fastImageUrl !== fallbackUrl) {
+              State.update({
+                fastImageUrl: fallbackUrl,
+              });
+            }
+          }}
+        />
+      </div>
+    ) : (
+      <div className={className} style={style} key={JSON.stringify(image)}>
+        <Widget
+          loading={
+            <div
+              className={`d-inline-block ${imageClassName}`}
+              style={imgWrapperStyle}
+            />
+          }
+          src="mob.near/widget/Image"
+          props={{
+            image,
+            alt: title,
+            className: imageClassName,
+            style: imageStyle,
+            thumbnail,
+            fallbackUrl,
+          }}
+        />
+      </div>
+    )}
+  </>
 );
