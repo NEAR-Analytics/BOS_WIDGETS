6a7fcd0be33cd1ece854500c60cd7c5fd043aea6
rubycop.near <rubycop.near>
2023-12-03 22:51:28 +0000

    Update mdao.page.home by rubycop.near at 2023-12-03T22:51:28.237Z

--- rubycop.near/mdao.page.home/source_code.jsx
+++ rubycop.near/mdao.page.home/source_code.jsx
@@ -11,7 +11,12 @@ const Container = styled.div`
 
 const HeroSection = styled.div`
   position: relative;
-  fill: linear-gradient(96deg, #fdefb1 -19.42%, #e1c4fe 49.87%, #95c3fe 98.55%);
+  background: linear-gradient(
+    96deg,
+    #fdefb1 -19.42%,
+    #e1c4fe 49.87%,
+    #95c3fe 98.55%
+  );
   width: 100%;
   height: 600px;
   padding: 1.5rem 3rem;
@@ -19,12 +24,14 @@ const HeroSection = styled.div`
   h1 {
     font-size: 96px;
     font-weight: 600;
+    margin-bottom: 0;
   }
 
   h2 {
     font-size: 32px;
     font-weight: 300;
     max-width: 800px;
+    margin-bottom: 0;
   }
 
   img {
@@ -32,14 +39,39 @@ const HeroSection = styled.div`
       display: none;
     }
   }
+
+  a.btn-primary {
+    border-radius: 10px;
+    background: #151718;
+    box-shadow: 0px 20px 30px 0px rgba(0, 0, 0, 0.25);
+    color: white;
+    color: #fff;
+    font-size: 24px;
+    font-weight: 400;
+    padding: 15px 25px;
+
+    &:hover {
+        opacity: 0.9;
+        text-decoration: none;
+    }
+  }
 `;
 
 return (
   <Container>
     <HeroSection className="d-flex justify-content-between align-items-center gap-3">
-      <div>
+      <div className="d-flex flex-column gap-5">
         <h1>{content.home.heroTitle}</h1>
         <h2>{content.home.heroDesc}</h2>
+        <a
+          href=""
+          className="text-center btn-primary d-flex justify-content-end"
+        >
+          <div className="d-flex justify-content-between w-75">
+            <span>Read More</span>
+            <i className="bi bi-chevron-right" />
+          </div>
+        </a>
       </div>
       <img src={assets.home.hero} />
     </HeroSection>
