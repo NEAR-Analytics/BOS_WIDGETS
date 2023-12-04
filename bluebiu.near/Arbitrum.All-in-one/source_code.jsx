52f9dd5225ac6ed9dad06f0479b5af2f18f33f94
bluebiu.near <bluebiu.near>
2023-12-04 03:44:03 +0000

    Update Arbitrum.All-in-one by bluebiu.near at 2023-12-04T03:44:03.263Z

--- bluebiu.near/Arbitrum.All-in-one/source_code.jsx
+++ bluebiu.near/Arbitrum.All-in-one/source_code.jsx
@@ -229,14 +229,14 @@ return (
         >
           <span className="icon">{swapIcon}</span>Swap
         </div>
-        <div
+        {/* <div
           onClick={() => {
             changeTab("Liquidity");
           }}
           className={`item ${activeMenu == "Liquidity" ? "active" : ""}`}
         >
           <span className="icon">{liquidityIcon}</span>Liquidity
-        </div>
+        </div> */}
         <div
           onClick={() => {
             changeTab("Lending");
@@ -262,11 +262,11 @@ return (
             />
           </>
         ) : null}
-        {activeMenu == "Liquidity" ? (
+        {/* {activeMenu == "Liquidity" ? (
           <>
             <Widget src="bluebiu.near/widget/Arbitrum.Pendle.TradeMarkets" />
           </>
-        ) : null}
+        ) : null} */}
         {activeMenu == "Lending" ? (
           <>
             <Widget src="bluebiu.near/widget/Arbitrum.Lending" props={props} />
