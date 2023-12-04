5e740ae01e7267710fd1f342f88d9b015b521cb8
marketplacebos.near <marketplacebos.near>
2023-12-04 17:54:04 +0000

    Update TrendingPost.ChartValueAll by marketplacebos.near at 2023-12-04T17:54:04.078Z

--- marketplacebos.near/TrendingPost.ChartValueAll/source_code.jsx
+++ marketplacebos.near/TrendingPost.ChartValueAll/source_code.jsx
@@ -55,73 +55,8 @@ if (!getBlockHeight30daysPost) {
   return "Loading...";
 }
 
-const newBlock1Days = Math.round(
-  parseInt(respBlock.body.stats[0].block) -
-    (1 * 24 * 3600) / parseInt(respBlock.body.stats[0].avg_block_time)
-);
-const newBlock3Days = Math.round(
-  parseInt(respBlock.body.stats[0].block) -
-    (3 * 24 * 3600) / parseInt(respBlock.body.stats[0].avg_block_time)
-);
-const newBlock7Days = Math.round(
-  parseInt(respBlock.body.stats[0].block) -
-    (7 * 24 * 3600) / parseInt(respBlock.body.stats[0].avg_block_time)
-);
-const allPost = Social.get("*/post/main/", "final");
 
-const tagCountAll = {};
-const tagCount1Days = {};
-const tagCount3Days = {};
-const tagCount7Days = {};
-Object.keys(allPost).forEach((item) => {
-  const tags = findHashtags(JSON.parse(allPost[item].post.main).text);
-  if (tags.length > 0) {
-    tags.forEach((tag) => {
-      tagCountAll[tag] = 0;
-      tagCount1Days[tag] = 0;
-      tagCount3Days[tag] = 0;
-      tagCount7Days[tag] = 0;
-    });
-  }
-});
-Object.keys(tagCountAll).forEach((tag) => {
-  const countAllPost = Social.index("hashtag", tag.replace("#", ""), {
-    from: newBlock,
-    limit: 999,
-    order: "asc",
-  });
-  const count1DaysPost = Social.index("hashtag", tag.replace("#", ""), {
-    from: newBlock1Days,
-    limit: 999,
-    order: "asc",
-  });
-  const count3DaysPost = Social.index("hashtag", tag.replace("#", ""), {
-    from: newBlock3Days,
-    limit: 999,
-    order: "asc",
-  });
-  const count7DaysPost = Social.index("hashtag", tag.replace("#", ""), {
-    from: newBlock7Days,
-    limit: 999,
-    order: "asc",
-  });
-  tagCountAll[tag] = countAllPost.length || 0;
-  tagCount1Days[tag] = count1DaysPost.length || 0;
-  tagCount3Days[tag] = count3DaysPost.length || 0;
-  tagCount7Days[tag] = count7DaysPost.length || 0;
-});
 
-let entriesALL = Object.entries(tagCountAll);
-let allPostSorted = entriesALL.sort((b, a) => a[1] - b[1]);
-
-let entries1 = Object.entries(tagCount1Days);
-let day1PostSorted = entries1.sort((b, a) => a[1] - b[1]);
-
-let entries3 = Object.entries(tagCount3Days);
-let day3PostSorted = entries3.sort((b, a) => a[1] - b[1]);
-
-let entries7 = Object.entries(tagCount7Days);
-let day7PostSorted = entries7.sort((b, a) => a[1] - b[1]);
 
 let totalItemsAll = 0;
 
