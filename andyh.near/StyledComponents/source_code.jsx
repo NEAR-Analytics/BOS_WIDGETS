163c3491d66458098c0e56a72d7c16f6f8eed7be
andyh.near <andyh.near>
2023-12-04 22:41:01 +0000

    Update StyledComponents by andyh.near at 2023-12-04T22:41:01.941Z

--- andyh.near/StyledComponents/source_code.jsx
+++ andyh.near/StyledComponents/source_code.jsx
@@ -1,5 +1,28 @@
-const Spaghetti = styled.div`
-    border: 1px solid red;
-`;
+const [StyledComponents, setStyledComponents] = useState(null);
+const importMarkdown = async () => {
+  try {
+    const pkg = await import(
+      "https://esm.sh/styled-components@6.1.1?alias=react:preact/compat&deps=preact@10.17.1"
+    );
+    console.log("SC imported", pkg);
+    setStyledComponents(() => pkg.default);
+  } catch (err) {
+    console.log("SC import error", err);
+  }
+};
 
-return <Spaghetti>Hello World</Spaghetti>;
+useEffect(() => {
+  importStyledComponents();
+}, []);
+
+if (StyledComponents) {
+  const styled = StyledComponents;
+  const Spaghetti = styled.div`border: 1px solid blue`;
+  return (
+    <div>
+      <Spaghetti>I have a border</Spaghetti>
+    </div>
+  );
+}
+
+return <>not imported yet</>;
