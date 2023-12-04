b8ed0aff9e3e9f23182c9a364d292d3c5c4eeab5
kurodenjiro.near <kurodenjiro.near>
2023-12-04 16:04:58 +0000

    Update trending by kurodenjiro.near at 2023-12-04T16:04:58.269Z

--- kurodenjiro.near/trending/source_code.jsx
+++ kurodenjiro.near/trending/source_code.jsx
@@ -1,69 +1,74 @@
-const findHashtags = (str) => {
-  const regexp = /\B\#\w\w+\b/g;
-  let match;
-  let tags = [];
-  while ((match = regexp.exec(str)) !== null) {
-    tags.push(match[0]);
-  }
-  return tags;
-};
-const respBlock = fetch("https://api.nearblocks.io/v1/stats");
-
-// 1 days
-const newBlock1Days = Math.round(
-  parseInt(respBlock.body.stats[0].block) -
-    (1 * 24 * 3600) / parseInt(respBlock.body.stats[0].avg_block_time)
-);
-
-let BlockHeightPost1Days = [];
-const getBlockHeight1daysPost = Social.index("post", "main", {
-  from: newBlock1Days,
-  limit: 99999,
-});
-getBlockHeight1daysPost.forEach((item) => {
-  BlockHeightPost1Days.push({
-    accountId: item.accountId,
-    blockHeight: item.blockHeight,
-  });
-});
-
-let post1days = [];
-BlockHeightPost1Days.forEach((item) => {
-  const post = Social.get(`${item.accountId}/post/main`, item.blockHeight);
-  if (post) {
-    post1days.push(JSON.parse(post).text);
-  }
-});
-
-let tagCount1Days = {};
-post1days.forEach((item) => {
-  const tags = findHashtags(item);
-  if (tags.length > 0) {
-    tags.forEach((tag) => {
-      if (tagCount1Days[tag]) {
-        tagCount1Days[tag] = tagCount1Days[tag] + 1;
-      } else {
-        tagCount1Days[tag] = 1;
-      }
-    });
-  }
-});
-let entries1days = Object.entries(tagCount1Days);
-let post1daySorted = entries1days.sort((b, a) => a[1] - b[1]);
-
 return (
   <>
-    <div>
-      <b> Trending :</b>
-      <br></br>
-      {post1daySorted &&
-        post1daySorted
-          .filter((item, index) => index <= 20)
-          .map((item) => (
-            <a href={`https://near.social/?hashtag=${item[0]}`}>
-              {item[0]}-{item[1]}
-            </a>
-          ))}
+    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+      <li class="nav-item" role="presentation">
+        <button
+          class="nav-link active"
+          id="pills-trending-now-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#pills-trending-now"
+          type="button"
+          role="tab"
+          aria-controls="pills-trending-now"
+          aria-selected="true"
+        >
+          Now
+        </button>
+      </li>
+      <li class="nav-item" role="presentation">
+        <button
+          class="nav-link"
+          id="pills-trending-3days-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#pills-trending-3days"
+          type="button"
+          role="tab"
+          aria-controls="pills-trending-3days"
+          aria-selected="false"
+        >
+          3 Days
+        </button>
+      </li>
+      <li class="nav-item" role="presentation">
+        <button
+          class="nav-link"
+          id="pills-trending-7days-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#pills-trending-7days"
+          type="button"
+          role="tab"
+          aria-controls="pills-trending-7days"
+          aria-selected="false"
+        >
+          7 Days
+        </button>
+      </li>
+    </ul>
+    <div class="tab-content" id="pills-tabContent">
+      <div
+        class="tab-pane fade show active"
+        id="pills-trending-now"
+        role="tabpanel"
+        aria-labelledby="pills-trending-now-tab"
+      >
+        <Widget src={`${context.accountId}/widget/trending1days`} />
+      </div>
+      <div
+        class="tab-pane fade"
+        id="pills-trending-3days"
+        role="tabpanel"
+        aria-labelledby="pills-trending-3days-tab"
+      >
+        <Widget src={`${context.accountId}/widget/trending3days`} />
+      </div>
+      <div
+        class="tab-pane fade"
+        id="pills-trending-7days"
+        role="tabpanel"
+        aria-labelledby="pills-trending-7days-tab"
+      >
+        <Widget src={`${context.accountId}/widget/trending7days`} />
+      </div>
     </div>
   </>
 );
