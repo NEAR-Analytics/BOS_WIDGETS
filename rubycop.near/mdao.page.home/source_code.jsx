a139d489dc8e63fbd3c1c31b99ba16eb1d480ea2
rubycop.near <rubycop.near>
2023-12-04 00:16:17 +0000

    Update mdao.page.home by rubycop.near at 2023-12-04T00:16:17.727Z

--- rubycop.near/mdao.page.home/source_code.jsx
+++ rubycop.near/mdao.page.home/source_code.jsx
@@ -1,12 +1,20 @@
-const { app, provider, assets, content } = VM.require(
+let { app, provider, assets, content } = VM.require(
   `rubycop.near/widget/mdao.config`
 );
+
+assets = assets.home;
+content = content.home;
 
 const Container = styled.div`
   position: relative;
   width: 100%;
   height: max-content;
   overflow: hidden;
+
+  h3 {
+    font-size: 2rem;
+    font-weight: 400;
+  }
 `;
 
 const HeroSection = styled.div`
@@ -16,7 +24,6 @@ const HeroSection = styled.div`
     #e1c4fe 49.87%,
     #95c3fe 98.55%
   );
-  width: 100%;
   height: 600px;
   padding: 1.5rem 3rem;
 
@@ -34,10 +41,16 @@ const HeroSection = styled.div`
     }
   }
 
-  h2 {
+  h3 {
     font-size: 2rem;
     font-weight: 300;
     max-width: 800px;
+    margin-bottom: 0;
+  }
+
+  h4 {
+    font-size: 1.5rem;
+    font-weight: 400;
     margin-bottom: 0;
   }
 
@@ -51,7 +64,6 @@ const HeroSection = styled.div`
     border-radius: 10px;
     background: #151718;
     box-shadow: 0px 20px 30px 0px rgba(0, 0, 0, 0.25);
-    color: white;
     color: #fff;
     font-size: 24px;
     font-weight: 400;
@@ -69,12 +81,101 @@ const HeroSection = styled.div`
   }
 `;
 
+const InfoSection = styled.div`
+  padding: 1.5rem 2rem;
+
+  @media screen and (max-width: 786px) {
+    padding: 1.5rem 2rem;
+    text-align: center;
+  }
+
+  h2 {
+    font-size: 3rem;
+    font-weight: 600;
+    width: 70%;
+
+    @media screen and (max-width: 786px) {
+      width: 100%;
+    }
+  }
+
+  .item {
+    width: 300px;
+    height: 375px;
+    border-radius: 10px;
+    background: #fff;
+    box-shadow: 0px 30px 80px 0px rgba(0, 0, 0, 0.1);
+
+    .header {
+      border-radius: 10px 10px 0px 0px;
+      background: rgba(237, 209, 241, 0.2);
+
+      h4 {
+        margin: 10px 0 0 0;
+      }
+    }
+
+    p {
+      font-size: 16px;
+      font-weight: 300;
+      margin: 0;
+    }
+
+    @media screen and (max-width: 786px) {
+      width: 100%;
+    }
+
+    a.btn-primary {
+      border-radius: 10px;
+      border: 2px solid #e6cde6;
+      background: rgba(252, 248, 246, 0);
+      font-size: 18px;
+      font-weight: 400;
+      color: #151718;
+      padding: 5px 15px;
+
+      div {
+        width: 80%;
+      }
+
+      &:hover {
+        opacity: 0.9;
+        text-decoration: none;
+      }
+
+      @media screen and (max-width: 786px) {
+        width: 100%;
+      }
+    }
+  }
+`;
+
+const Info = ({ title, desc, icon }) => (
+  <div className="item d-flex flex-column gap-2 justify-content-between">
+    <div className="header gap-3 p-4 text-center">
+      {icon}
+      <h4>{title}</h4>
+    </div>
+    <div className="p-4 text-center">
+      <p>{desc}</p>
+    </div>
+    <div className="px-5 pb-4">
+      <a href="" className="text-center btn-primary d-flex justify-content-end">
+        <div className="d-flex justify-content-between">
+          <span>Read More</span>
+          <i className="bi bi-chevron-right" />
+        </div>
+      </a>
+    </div>
+  </div>
+);
+
 return (
   <Container>
     <HeroSection className="d-flex justify-content-between align-items-center gap-3">
       <div className="d-flex flex-column gap-5">
-        <h1>{content.home.heroTitle}</h1>
-        <h2>{content.home.heroDesc}</h2>
+        <h1>{content.heroTitle}</h1>
+        <h3>{content.heroDesc}</h3>
         <a
           href=""
           className="text-center btn-primary d-flex justify-content-end"
@@ -85,7 +186,17 @@ return (
           </div>
         </a>
       </div>
-      <img src={assets.home.hero} />
+      <img src={assets.hero} />
     </HeroSection>
+
+    <InfoSection className="d-flex flex-column gap-5">
+      <h3>{content.info.name}</h3>
+      <h2>{content.info.title}</h2>
+      <div className="d-flex flex-wrap gap-4 justify-content-center">
+        {content.info.sections.map(({ title, desc, icon }) => (
+          <Info title={title} desc={desc} icon={icon} />
+        ))}
+      </div>
+    </InfoSection>
   </Container>
 );
