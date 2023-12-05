c7e4a5294b29f053493935a37e08549f236facea
silkking.near <silkking.near>
2023-12-05 15:46:08 +0000

    Update lib.notifications by silkking.near at 2023-12-05T15:46:08.893Z

---
+++ silkking.near/lib.notifications/source_code.jsx
@@ -0,0 +1,89 @@
+const { stateUpdate, imports, fatherState } = props;
+
+if (!stateUpdate) {
+  stateUpdate = () => {};
+}
+
+if (!imports) {
+  imports = [];
+}
+
+let fatherStateHasAllFunction = true;
+imports.forEach((fnName) => {
+  fatherStateHasAllFunction =
+    fatherStateHasAllFunction && fatherState[fnName] !== undefined;
+});
+
+const standardPostBlockHeight = 106940959;
+
+if (fatherStateHasAllFunction) {
+  return <></>;
+}
+
+function appendExports(fnName) {
+  if (fnName === "notify") {
+    libNotificationsOutput[fnName] = notify;
+  } else if (fnName === "clg") {
+    libNotificationsOutput[fnName] = clg;
+  }
+}
+
+function notify(notificationType, userToNotify, redirectTo) {
+  console.log("Inside notify fn");
+  const notificationTypeText = {
+    mention: `I have mentioned @${userToNotify} in this post: `,
+    upVote: "I have upVoted this post: ",
+    emoji: "I have reacted to this post: ",
+    comment: "I have commented this post: ",
+  };
+  Social.set(
+    {
+      post: {
+        main: JSON.stringify({
+          type: "md",
+          text: `${notificationTypeText[notificationType]} ${redirectTo}`,
+        }),
+      },
+      index: {
+        notify: JSON.stringify({
+          key: userToNotify,
+          value: {
+            type: "mention",
+            item: {
+              type: "social",
+              path: `${context.accountId}/post/main`,
+            },
+          },
+        }),
+      },
+    },
+    {
+      force: true,
+      onCommit: () => {
+        stateUpdate({ articleCreated: undefined });
+      },
+    }
+  );
+}
+
+function clg(string) {
+  console.log(string);
+}
+
+const libNotificationsOutput = {};
+
+imports.forEach((fnName) => {
+  appendExports(fnName);
+});
+
+stateUpdate({ notifications: libNotificationsOutput });
+
+// const standardPostBlockHeight = 106940958;
+
+// notify(
+//   "mention",
+//   `${context.accountId}`,
+//   `https://near.social/f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/SayALot?isTest=t&sharedBlockHeight=${standardPostBlockHeight}`
+// );
+
+return <></>;
