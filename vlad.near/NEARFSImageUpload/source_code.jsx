90facd136472df334459d8636ed626dda59bffb8
vlad.near <vlad.near>
2023-12-05 23:02:53 +0000

    Update NEARFSImageUpload by vlad.near at 2023-12-05T23:02:53.411Z

--- vlad.near/NEARFSImageUpload/source_code.jsx
+++ vlad.near/NEARFSImageUpload/source_code.jsx
@@ -1,1 +1,70 @@
-return <div>Hello World</div>;
+initState({
+  img: null,
+});
+
+const networkId = "mainnet";
+const domain = {
+  testnet: "testnet.page",
+  mainnet: "near.page",
+}[networkId];
+const ipfsUrl = (cid) => `https://ipfs.web4.${domain}/ipfs/${cid}`;
+
+const filesOnChange = (files) => {
+  if (files?.length > 0) {
+    State.update({
+      img: {
+        uploading: true,
+        cid: null,
+      },
+    });
+    const body = files[0];
+
+    Near.call('social.near', 'fs_store', body.arrayBuffer()).then((res) => {
+        // TODO: hash
+    //   const cid = res.body.cid;
+    //   State.update({
+    //     img: {
+    //       cid,
+    //     },
+    //   });
+    });
+  } else {
+    State.update({
+      img: null,
+    });
+  }
+};
+
+return (
+  <div className="d-inline-block">
+    {state.img?.cid && (
+      <div
+        className="d-inline-block me-2 overflow-hidden align-middle"
+        style={{ width: "2.5em", height: "2.5em" }}
+      >
+        <img
+          className="rounded w-100 h-100"
+          style={{ objectFit: "cover" }}
+          src={ipfsUrl(state.img?.cid)}
+          alt="upload preview"
+        />
+      </div>
+    )}
+    <Files
+      multiple={false}
+      accepts={["image/*"]}
+      minFileSize={1}
+      clickable
+      className="btn btn-outline-primary"
+      onChange={filesOnChange}
+    >
+      {state.img?.uploading ? (
+        <>{Loading} Uploading</>
+      ) : state.img?.cid ? (
+        "Replace"
+      ) : (
+        "Upload an Image"
+      )}
+    </Files>
+  </div>
+);
