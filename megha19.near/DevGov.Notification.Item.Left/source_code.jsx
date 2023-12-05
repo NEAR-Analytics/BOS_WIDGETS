d6eca6ea6577f25dae04401bd0ca670c477e1a11
megha19.near <megha19.near>
2023-12-05 08:06:38 +0000

    Update DevGov.Notification.Item.Left by megha19.near at 2023-12-05T08:06:38.843Z

---
+++ megha19.near/DevGov.Notification.Item.Left/source_code.jsx
@@ -0,0 +1,34 @@
+const { href } = VM.require("megha19.near/widget/core.lib.url") || (() => {});
+
+if (!props.type) {
+  return "Loading ...";
+}
+
+const type = props.type.split("/")[1];
+return props.type ? (
+  <>
+    {type == "like"
+      ? "liked your"
+      : type == "reply"
+      ? "replied to your"
+      : type == "edit"
+      ? "edited your"
+      : type == "mention"
+      ? "mentioned you in their"
+      : "???"}{" "}
+    <a
+      className="fw-bold text-muted"
+      href={href({
+        widgetSrc: "megha19.near/widget/app",
+        params: {
+          page: "post",
+          id: props.post,
+        },
+      })}
+    >
+      DevHub post
+    </a>
+  </>
+) : (
+  "Loading ..."
+);
