fd7c34e873191c65fff23735ffbec9eb76415633
megha19.near <megha19.near>
2023-12-05 08:06:38 +0000

    Update app by megha19.near at 2023-12-05T08:06:38.843Z

--- megha19.near/app/source_code.jsx
+++ megha19.near/app/source_code.jsx
@@ -162,7 +162,14 @@ function Page() {
         />
       );
     }
-
+    case "admin": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.page.admin.index"}
+          props={passProps}
+        />
+      );
+    }
     default: {
       // TODO: 404 page
       return <p>404</p>;
