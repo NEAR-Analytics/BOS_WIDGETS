3fa330bc91d9f60c9334a0d41f435c3de21b7ae3
gagdiez.near <gagdiez.near>
2023-12-05 15:22:55 +0000

    Update Menu by gagdiez.near at 2023-12-05T15:22:55.199Z

--- gagdiez.near/Menu/source_code.jsx
+++ gagdiez.near/Menu/source_code.jsx
@@ -1,6 +1,6 @@
-const topics = props.topics;
 const menuLinks = props.menuLinks;
 const selected = props.selected;
+const onSelected = props.onSelected;
 
 const capitalize = (words) => {
   return words
@@ -16,8 +16,9 @@ return (
         {menuLinks.map(({ label, icon }) => (
           <li class="nav-item">
             <a
-              href={`/?selected=${label}`}
+              href={`/#`}
               class={`nav-link ${selected !== label && "link-dark"}`}
+              onClick={onSelected}
             >
               <i class={`bi bi-${icon} me-1`} />
               {capitalize(label)}
