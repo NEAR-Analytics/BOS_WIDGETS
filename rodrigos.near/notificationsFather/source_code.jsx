f37b154853762dfbdc93869a04e89bd733c81f81
rodrigos.near <rodrigos.near>
2023-12-06 06:13:57 +0000

    Update notificationsFather by rodrigos.near at 2023-12-06T06:13:57.267Z

---
+++ rodrigos.near/notificationsFather/source_code.jsx
@@ -0,0 +1,38 @@
+State.init({});
+
+if (state.notifications.getNotificationData) {
+  const data = state.notifications.getNotificationData(
+    "mention",
+    ["rod", "carl"],
+    "www"
+  );
+  console.log(data);
+}
+return (
+  <>
+    <Widget
+      src={
+        "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/lib.notifications"
+      }
+      props={{
+        stateUpdate: State.update,
+        fatherState: State,
+        imports: ["getNotificationData"],
+      }}
+    />
+    {state.notifications.getNotificationData && (
+      <button
+        onClick={() => {
+          const resp = state.notifications.getNotificationData(
+            "mention",
+            ["rod", "carl"],
+            "www"
+          );
+          console.log(resp);
+        }}
+      >
+        aca
+      </button>
+    )}
+  </>
+);
