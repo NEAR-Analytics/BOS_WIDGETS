d3f4f7beedc96f20df7b75796170a6eb6e444c50
hack.near <hack.near>
2023-12-06 15:45:45 +0000

    Update quest by hack.near at 2023-12-06T15:45:45.164Z

--- hack.near/quest/source_code.jsx
+++ hack.near/quest/source_code.jsx
@@ -2,13 +2,14 @@ const accountId = props.accountId ?? context.accountId;
 
 const claimsData = Near.view("questsmock.near", "get_all_quests");
 
-const claim = Near.view("questsmock.near", "get_quest_id", {
-  quest_id: 813740323,
+const claim = Near.view("questsmock.near", "get_quest_by_id", {
+  questId: 813740323,
 });
 
 return (
   <>
     <div>{JSON.stringify(claimsData)}</div>
-    <div>{JSON.stringify(claim)}</div>
+    <br />
+    <div>{JSON.stringify(claim.start_time)}</div>
   </>
 );
