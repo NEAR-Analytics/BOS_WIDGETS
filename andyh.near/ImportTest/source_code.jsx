b8512ec574cca6373b02c669835dc45a8f44b314
andyh.near <andyh.near>
2023-12-06 05:08:07 +0000

    Update ImportTest by andyh.near at 2023-12-06T05:08:07.877Z

--- andyh.near/ImportTest/source_code.jsx
+++ andyh.near/ImportTest/source_code.jsx
@@ -3,5 +3,9 @@ import { FABeer } from "react-icons/fa";
 return (
   <div>
     Hello World <FABeer />
+    <Widget
+      src="andyh.near/ImportTest.Child"
+      trust={{ trust: "trusted-author" }}
+    />
   </div>
 );
