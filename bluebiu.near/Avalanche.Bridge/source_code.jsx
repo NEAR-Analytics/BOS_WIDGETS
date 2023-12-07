9cc11baef6112871b61a992e8c1448cc94d7dd52
bluebiu.near <bluebiu.near>
2023-12-07 10:38:33 +0000

    Update Avalanche.Bridge by bluebiu.near at 2023-12-07T10:38:33.022Z

--- bluebiu.near/Avalanche.Bridge/source_code.jsx
+++ bluebiu.near/Avalanche.Bridge/source_code.jsx
@@ -102,6 +102,7 @@ return (
         tokens: Tokens,
         amountOutFn: "bluebiu.near/widget/Base.Bridge.AmountOut",
         handlerSwap: "bluebiu.near/widget/Base.Bridge.HandlerSwap",
+        ...props,
       }}
     />
   </Theme>
