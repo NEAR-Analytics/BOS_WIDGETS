721cee46ac7682390180fdc435894f823d6df0c8
inscribe.near <inscribe.near>
2023-12-07 06:56:25 +0000

    Update NEAT.SearchInput by inscribe.near at 2023-12-07T06:56:25.345Z

--- inscribe.near/NEAT.SearchInput/source_code.jsx
+++ inscribe.near/NEAT.SearchInput/source_code.jsx
@@ -3,6 +3,36 @@ const MaxGasPerTransaction = TGas.mul(250);
 const GasPerTransaction = MaxGasPerTransaction.plus(TGas);
 const pageAmountOfPage = 5;
 const ipfsPrefix = "https://ipfs.near.social/ipfs";
+
+function toLocaleString(source, decimals, rm) {
+  if (typeof source === "string") {
+    return toLocaleString(Number(source), decimals);
+  } else if (typeof source === "number") {
+    return decimals !== undefined
+      ? source.toLocaleString(undefined, {
+          maximumFractionDigits: decimals,
+          minimumFractionDigits: decimals,
+        })
+      : source.toLocaleString();
+  } else {
+    // Big type
+    return toLocaleString(
+      decimals !== undefined
+        ? Number(source.toFixed(decimals, rm))
+        : source.toNumber(),
+      decimals
+    );
+  }
+}
+
+function formatAmount(balance, decimal) {
+  if (!decimal) decimal = 8;
+  return toLocaleString(
+    Big(balance).div(Big(10).pow(decimal)).toFixed(),
+    decimal
+  );
+}
+
 // Config for Bos app
 function getConfig(network) {
   switch (network) {
@@ -20,12 +50,17 @@ function getConfig(network) {
           tick: "neat",
           amt: "100000000",
         },
+        transferArgs: {
+          p: "nrc-20",
+          op: "transfer",
+          tick: "neat",
+        },
       };
     case "testnet":
       return {
         ownerId: "inscribe.testnet",
         graphUrl:
-          "https://api.thegraph.com/subgraphs/name/inscriptionnear/neat",
+          "https://api.thegraph.com/subgraphs/name/inscriptionnear/neat-test",
         nodeUrl: "https://rpc.testnet.near.org",
         contractName: "inscription.testnet",
         methodName: "inscribe",
@@ -34,6 +69,11 @@ function getConfig(network) {
           op: "mint",
           tick: "neat",
           amt: "100000000",
+        },
+        transferArgs: {
+          p: "nrc-20",
+          op: "transfer",
+          tick: "neat",
         },
       };
     default:
