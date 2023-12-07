d72d04a67e83484d616529c7e8b644eb82e1d12b
bluebiu.near <bluebiu.near>
2023-12-07 10:38:33 +0000

    Update Gnosis.Lending by bluebiu.near at 2023-12-07T10:38:33.022Z

--- bluebiu.near/Gnosis.Lending/source_code.jsx
+++ bluebiu.near/Gnosis.Lending/source_code.jsx
@@ -1,4 +1,4 @@
-const { defaultDapp } = props;
+const { defaultDapp, ...rest } = props;
 
 const Theme = styled.div`
   --button-text-color: #ffffff;
@@ -64,6 +64,7 @@ return (
               "bluebiu.near/widget/Arbitrum.Lending.RadiantActionHandler",
           },
         },
+        ...rest,
       }}
     />
   </Theme>
