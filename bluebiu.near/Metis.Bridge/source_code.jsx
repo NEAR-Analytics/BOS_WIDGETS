d57f6e45d56aadcf148add94ad6e914db2952a1a
bluebiu.near <bluebiu.near>
2023-12-07 10:38:33 +0000

    Update Metis.Bridge by bluebiu.near at 2023-12-07T10:38:33.022Z

--- bluebiu.near/Metis.Bridge/source_code.jsx
+++ bluebiu.near/Metis.Bridge/source_code.jsx
@@ -69,6 +69,7 @@ return (
         tokens: Tokens,
         amountOutFn: "bluebiu.near/widget/Base.Bridge.AmountOut",
         handlerSwap: "bluebiu.near/widget/Base.Bridge.HandlerSwap",
+        ...props,
       }}
     />
   </Theme>
