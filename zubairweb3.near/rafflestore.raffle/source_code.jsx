b3a62015addfb03db767d7c73a6577f19708111f
zubairweb3.near <zubairweb3.near>
2023-12-07 22:48:37 +0000

    Update rafflestore.raffle by zubairweb3.near at 2023-12-07T22:48:37.961Z

--- zubairweb3.near/rafflestore.raffle/source_code.jsx
+++ zubairweb3.near/rafflestore.raffle/source_code.jsx
@@ -196,42 +196,74 @@ const Entries = styled.div`
 
 return (
   <>
-    <div
-      class="modal fade"
-      id="raffleModal"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">
-              Modal title
-            </h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">...</div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">
-              Save changes
-            </button>
+    {newTitle === nfts[0].metadata.title ? (
+      <div
+        class="modal fade"
+        id="raffleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    ) : (
+      <div
+        class="modal fade"
+        id="raffleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Sharddog NFT Check
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              Ops! You are not qualified, please claim ShardDog - 1555 and try
+              again
+              <button>Claim</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )}
+
     <Container>
       <Widget src="zubairweb3.near/widget/rafflestore.header" />
       <Banner>
