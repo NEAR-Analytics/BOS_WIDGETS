25d13555415b583840ca180c6ab7d72207995985
bluebiu.near <bluebiu.near>
2023-12-07 10:38:33 +0000

    Update Polygon.Lending by bluebiu.near at 2023-12-07T10:38:33.022Z

--- bluebiu.near/Polygon.Lending/source_code.jsx
+++ bluebiu.near/Polygon.Lending/source_code.jsx
@@ -1,4 +1,4 @@
-const { defaultDapp } = props;
+const { defaultDapp, ...rest } = props;
 
 const Theme = styled.div`
   --button-text-color: #fff;
@@ -71,6 +71,7 @@ return (
               "bluebiu.near/widget/Avalanche.Lending.BenqiHandlerCToken",
           },
         },
+        ...rest,
       }}
     />
   </Theme>
