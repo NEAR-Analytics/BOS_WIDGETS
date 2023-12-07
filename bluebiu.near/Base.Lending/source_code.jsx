eb292c0b23e6f69252b5e897be6b4e8871d12cb6
bluebiu.near <bluebiu.near>
2023-12-07 10:38:33 +0000

    Update Base.Lending by bluebiu.near at 2023-12-07T10:38:33.022Z

--- bluebiu.near/Base.Lending/source_code.jsx
+++ bluebiu.near/Base.Lending/source_code.jsx
@@ -1,4 +1,4 @@
-const { defaultDapp } = props;
+const { defaultDapp, ...rest } = props;
 
 const Theme = styled.div`
   --button-text-color: #fff;
@@ -90,6 +90,7 @@ return (
               "bluebiu.near/widget/Arbitrum.Lending.RadiantActionHandler",
           },
         },
+        ...rest,
       }}
     />
   </Theme>
