f6b129f4e91628438daece53c2896c8726206371
indonesiaguild.near <indonesiaguild.near>
2023-12-07 12:27:40 +0000

    Update RCDashboard-WIP by indonesiaguild.near at 2023-12-07T12:27:40.951Z

--- indonesiaguild.near/RCDashboard-WIP/source_code.jsx
+++ indonesiaguild.near/RCDashboard-WIP/source_code.jsx
@@ -34,26 +34,20 @@ const communityAccounts = rcDaoMembers;
 
 State.init({
   selectedCommunityAccountMembers: [],
-  selectedCommunityAccount: null,
-  menu: "all-rc",
-  allMembers: [],
+  selectedCommunityAccount: communityAccounts[0],
 });
-
-const getWidgetsCount = (accountId) => {
-  return Object.keys(
-    Social.keys(`${accountId}/widget/*`, "final", {
-      return_type: "BlockHeight",
-      values_only: false,
-    })[accountId].widget || {}
-  ).length;
-};
 
 const getTotalWidgetByMembers = (members) => {
   if (!members) return 0;
 
   const total = members
     .map((accountId) => {
-      return getWidgetsCount(accountId);
+      return Object.keys(
+        Social.keys(`${accountId}/widget/*`, "final", {
+          return_type: "BlockHeight",
+          values_only: false,
+        })[accountId].widget || {}
+      ).length;
     })
     .reduce((a, b) => a + b, 0);
   return total;
@@ -73,16 +67,6 @@ const getTotalPostByMembers = (members) => {
     })
     .reduce((a, b) => a + b, 0);
   return total;
-};
-
-const widgetDisplay = (members) => {
-  return members.map((member) => {
-    if (getWidgetsCount(member) > 0) {
-      return (
-        <Widget src="mob.near/widget/Widgets" props={{ accountId: member }} />
-      );
-    }
-  });
 };
 
 const chart = ({
@@ -181,66 +165,11 @@ fetchData();
   );
 };
 
-const chartDappUsage = ({ data, header, label }) => {
-  const srcDoc = `
-<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-<canvas id="myChart" style="position: relative; height:80vh; width:80vw"></canvas>
-
-<script>
-async function fetchData() {
-
-const tempData = ${data};
-
-const title = tempData.map((entry) => entry["dapp"]);
-
-
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: title,
-          datasets: [
-          {
-              label: "${label}",
-              data: tempData.map(entry => entry.txs)
-          }
-          ]
-      }
-  });
-}
-
-fetchData();
-</script>
-`;
-  return (
-    <Widget
-      src="contribut3.near/widget/Card"
-      props={{
-        header: header,
-        body: data ? (
-          <iframe
-            className="w-100"
-            style={{ height: "750px" }}
-            srcDoc={srcDoc}
-          />
-        ) : (
-          <div style={{ margin: "auto" }}>
-            <Widget src="flashui.near/widget/Loading" props={{}} />
-          </div>
-        ),
-      }}
-    />
-  );
-};
-
 const doQueryToFlipside = (query, queryResultId, dataLabel) => {
   // create run (https://docs.flipsidecrypto.com/flipside-api/rest-api)
   const headers = {};
   headers["Content-Type"] = "application/json";
-  headers["x-api-key"] = "f0b8c6e1-573a-4f09-a6f5-ea2d13d6416b";
+  headers["x-api-key"] = "07811919-9c82-4e96-8496-9cf378305d22";
 
   if (!queryResultId) {
     const requestResult = fetch("https://api-v2.flipsidecrypto.xyz/json-rpc", {
@@ -377,7 +306,7 @@ const generateDAU = (members) => {
   apiDoQueryToFlipSide(query, "dauChartData"); // return doQueryToFlipside(query, "queryResultIdMAU");
 };
 
-const widgetRank = (members) => {
+const widgetRank = () => {
   const data = Social.get("*/graph/star/**", "final");
 
   const starCountsByWidget = {};
@@ -387,7 +316,7 @@ const widgetRank = (members) => {
     const widgetData = userData?.graph?.star;
 
     if (widgetData) {
-      members.map((widgetCreator) => {
+      state.selectedCommunityAccountMembers.map((widgetCreator) => {
         const widgetList = widgetData[widgetCreator]?.widget;
 
         if (widgetList) {
@@ -535,57 +464,15 @@ const generateNFTMints = (members) => {
   apiDoQueryToFlipSide(query, "nftMintsChartData");
 };
 
-const generateDappUsage = (members) => {
-  if (members.length === 0) return [];
-  const formattedMembers = JSON.stringify(members)
-    .replaceAll("[", "(")
-    .replaceAll("]", ")")
-    .replaceAll('"', "'");
-  const query = `with lst_top_dApps as (
-      select top 20
-        INITCAP( PROJECT_NAME) as dApp
-        ,count(DISTINCT block_timestamp::date) as "Activity days"
-        ,count(DISTINCT tx_hash) as TXs
-        ,TXs / "Activity days" as "Transaction per day"
-      from near.core.fact_transactions
-        join near.core.dim_address_labels on address = TX_RECEIVER
-      where label_type='dapp' and tx_signer in ${formattedMembers}
-      group by 1
-      )
-      select * from lst_top_dApps;
-  `;
-
-  apiDoQueryToFlipSide(query, "dappUsageChartData");
-};
-
 State.update({
   selectedCommunityAccountMembers: getMembers(state.selectedCommunityAccount),
 });
-if (state.menu === "developer")
-  generateGithubActivities(state.selectedCommunityAccountMembers);
-if (state.menu === "overview" || state.menu === "dapp")
-  generateMAU(state.selectedCommunityAccountMembers);
-if (state.menu === "overview")
-  generateTotalLikes(state.selectedCommunityAccountMembers);
-if (state.menu === "overview" || state.menu === "dapp")
-  generateDAU(state.selectedCommunityAccountMembers);
-if (state.menu === "overview")
-  generateTotalWalletsCreated(state.selectedCommunityAccountMembers);
-if (state.menu === "nft")
-  generateNFTMints(state.selectedCommunityAccountMembers);
-if (state.menu === "dapp")
-  generateDappUsage(state.selectedCommunityAccountMembers);
-
-if (state.menu === "all-overview") {
-  State.update({
-    allMembers: rcDaoMembers.flatMap((rcAccountId) => getMembers(rcAccountId)),
-  });
-  generateTotalLikes(state.allMembers);
-  generateDAU(state.allMembers);
-  generateNFTMints(state.allMembers);
-  generateMAU(state.allMembers);
-  generateGithubActivities(state.allMembers);
-}
+generateGithubActivities(state.selectedCommunityAccountMembers);
+generateMAU(state.selectedCommunityAccountMembers);
+generateTotalLikes(state.selectedCommunityAccountMembers);
+generateDAU(state.selectedCommunityAccountMembers);
+generateTotalWalletsCreated(state.selectedCommunityAccountMembers);
+generateNFTMints(state.selectedCommunityAccountMembers);
 
 return (
   <div className="container">
@@ -597,7 +484,6 @@ return (
       onChange={(value) =>
         State.update({
           selectedCommunityAccount: value,
-          menu: "overview",
           queryResultIdMAU: null,
           queryResultIdDevActivities: null,
           totalLikes: null,
@@ -605,288 +491,89 @@ return (
           mauChartData: null,
           dauChartData: null,
           nftMintsChartData: null,
-          dappUsageChartData: null,
         })
       }
     />
-    {/* Menu */}
-    <div className="m-1">
-      <button
-        onClick={() => State.update({ menu: "all-rc" })}
-        disabled={
-          state.selectedCommunityAccount !== null || state.menu === "all-rc"
-        }
-        hidden={state.selectedCommunityAccount !== null}
-      >
-        List of RCs
-      </button>
-      <button
-        onClick={() => State.update({ menu: "all-overview" })}
-        disabled={
-          state.selectedCommunityAccount !== null ||
-          state.menu === "all-overview"
-        }
-        hidden={state.selectedCommunityAccount !== null}
-      >
-        Overview Stats
-      </button>
-      <button
-        onClick={() => State.update({ menu: "overview" })}
-        disabled={
-          state.selectedCommunityAccount === null || state.menu === "overview"
-        }
-        hidden={state.selectedCommunityAccount === null}
-      >
-        Overview
-      </button>
-      <button
-        onClick={() => State.update({ menu: "developer" })}
-        disabled={
-          state.selectedCommunityAccount === null || state.menu === "developer"
-        }
-        hidden={state.selectedCommunityAccount === null}
-      >
-        Developer Activities
-      </button>
-      <button
-        onClick={() => State.update({ menu: "widget-list" })}
-        disabled={
-          state.selectedCommunityAccount === null ||
-          state.menu === "widget-list"
-        }
-        hidden={state.selectedCommunityAccount === null}
-      >
-        Widget List
-      </button>
-      <button
-        onClick={() => State.update({ menu: "dapp" })}
-        disabled={
-          state.selectedCommunityAccount === null || state.menu === "dapp"
-        }
-        hidden={state.selectedCommunityAccount === null}
-      >
-        Dapp Activities
-      </button>
-      <button
-        onClick={() => State.update({ menu: "nft" })}
-        disabled={
-          state.selectedCommunityAccount === null || state.menu === "nft"
-        }
-        hidden={state.selectedCommunityAccount === null}
-      >
-        NFT Mints
-      </button>
-      <button
-        onClick={() => State.update({ menu: "members" })}
-        disabled={
-          state.selectedCommunityAccount === null || state.menu === "members"
-        }
-        hidden={state.selectedCommunityAccount === null}
-      >
-        Members List
-      </button>
-    </div>
-    {state.selectedCommunityAccount === null &&
-      state.menu === "all-overview" &&
-      state.allMembers.length > 1 && (
-        <div>
-          <Widget
-            src="nearhorizon.near/widget/InfoSegment"
-            props={{
-              title: "Info",
-              description: `These statistics are all from accounts that are members from regional communities.`,
-            }}
-          />
-          <div>
-            {chart({
-              data: state["mauChartData"],
-              header: <b>Monthly Active Members</b>,
-              valueLabel: "mau",
-              label: "MAU (members)",
-              barColor: "rgb(192, 85, 85)",
-            })}
-          </div>
-          <div>
-            {chart({
-              data: state["dauChartData"],
-              header: <b>Daily Active Members</b>,
-              valueLabel: "dau",
-              label: "DAU (members)",
-              barColor: "rgb(140, 85, 85)",
-            })}
-          </div>
-          <div>
-            {chart({
-              header: <b>Total Posts by All Members</b>,
-              staticDisplay: getTotalPostByMembers(state.allMembers),
-            })}
-          </div>
-
-          <div>
-            {chart({
-              header: <b>Total Likes by Members</b>,
-              staticDisplay: JSON.parse(state["totalLikes"])[0]["total"],
-            })}
-          </div>
-          <div>
-            {chart({
-              header: <b>Total Widgets by All Members</b>,
-              staticDisplay: getTotalWidgetByMembers(state.allMembers),
-            })}
-          </div>
-          <div>
-            {chart({
-              data: state["nftMintsChartData"],
-              header: <b>NFT Mints Activity</b>,
-              valueLabel: "total_activity",
-              label: "NFT Mints Activity",
-              barColor: "rgb(85, 85, 180)",
-            })}
-          </div>
-          <div>
-            {chart({
-              data: state["githubChartData"],
-              header: <b>Github Activities Members</b>,
-              valueLabel: "total_issues_and_pr",
-              label: "Total Issues And PR Activities",
-              barColor: "rgb(85, 192, 192)",
-            })}
-          </div>
-          <div>
-            {chart({
-              header: <b>Top Widgets</b>,
-              staticDisplay: widgetRank(state.allMembers),
-            })}
-          </div>
-        </div>
-      )}
-
-    {state.selectedCommunityAccount === null && state.menu === "all-rc" && (
-      <Widget src="rc-dao.near/widget/com.rank" props={{}} />
-    )}
-
-    {state.menu === "members" && (
-      <div>
+    {/*Members:{" "}
+    {JSON.stringify(state.selectedCommunityAccountMembers)
+      .replaceAll('"', "'")
+      .replaceAll("[", "(")
+      .replaceAll("]", ")")} */}
+    {chart({
+      data: state["githubChartData"],
+      header: <b>Github Activities Members</b>,
+      valueLabel: "total_issues_and_pr",
+      label: "Total Issues And PR Activities",
+      barColor: "rgb(85, 192, 192)",
+    })}
+    <div style={{ display: "flex", flexFlow: "row wrap" }}>
+      <div style={{ width: "calc(50%)" }}>
         {chart({
-          header: <b>Members</b>,
-          staticDisplay: state.selectedCommunityAccountMembers && (
-            <Widget
-              src="indonesiaguild.near/widget/CRM"
-              props={{ members: state.selectedCommunityAccountMembers }}
-            />
+          header: <b>Total Widgets by Members</b>,
+          staticDisplay: getTotalWidgetByMembers(
+            state.selectedCommunityAccountMembers
           ),
         })}
       </div>
-    )}
-
-    {state.menu === "developer" && (
-      <>
+      <div style={{ width: "calc(50%)" }}>
         {chart({
-          data: state["githubChartData"],
-          header: <b>Github Activities Members</b>,
-          valueLabel: "total_issues_and_pr",
-          label: "Total Issues And PR Activities",
-          barColor: "rgb(85, 192, 192)",
-        })}
-        <div>
-          {chart({
-            header: <b>Total Widgets by Members</b>,
-            staticDisplay: getTotalWidgetByMembers(
-              state.selectedCommunityAccountMembers
-            ),
-          })}
-        </div>
-      </>
-    )}
-
-    {state.menu === "overview" && (
-      <>
-        <Widget
-          src="nearhorizon.near/widget/InfoSegment"
-          props={{
-            title: "Info",
-            description: `Members are those who receive a followback from ${state.selectedCommunityAccount}`,
-          }}
-        />
-        <div>
-          {chart({
-            header: <b>Total Posts by Members</b>,
-            staticDisplay: getTotalPostByMembers(
-              state.selectedCommunityAccountMembers
-            ),
-          })}
-        </div>
-        <div style={{ display: "flex", flexFlow: "row wrap" }}>
-          <div style={{ width: "calc(50%)" }}>
-            {chart({
-              header: <b>Total Likes by Members</b>,
-              staticDisplay: JSON.parse(state["totalLikes"])[0]["total"],
-            })}
-          </div>
-          <div style={{ width: "calc(50%)" }}>
-            {chart({
-              header: <b>Total Wallets Created by Members</b>,
-              staticDisplay: JSON.parse(state["totalWallets"])[0]["total"],
-            })}
-          </div>
-        </div>
-      </>
-    )}
-    {state.menu === "dapp" && (
-      <div>
-        {chartDappUsage({
-          data: state["dappUsageChartData"],
-          header: <b>Dapp Usage</b>,
-          valueLabel: "txs",
-          label: "Total Transactions",
-          barColor: "rgb(85, 85, 180)",
+          header: <b>Total Posts by Members</b>,
+          staticDisplay: getTotalPostByMembers(
+            state.selectedCommunityAccountMembers
+          ),
         })}
       </div>
-    )}
+    </div>
+    <div style={{ display: "flex", flexFlow: "row wrap" }}>
+      <div style={{ width: "calc(50%)" }}>
+        {chart({
+          header: <b>Total Likes by Members</b>,
+          staticDisplay: JSON.parse(state["totalLikes"])[0]["total"],
+        })}
+      </div>
+      <div style={{ width: "calc(50%)" }}>
+        {chart({
+          header: <b>Total Wallets Created by Members</b>,
+          staticDisplay: JSON.parse(state["totalWallets"])[0]["total"],
+        })}
+      </div>
+    </div>
 
-    {state.menu === "widget-list" && (
-      <div>{widgetDisplay(state.selectedCommunityAccountMembers)}</div>
-    )}
-    {(state.menu === "dapp" || state.menu === "overview") && (
-      <>
-        <div>
-          {chart({
-            data: state["mauChartData"],
-            header: <b>Monthly Active Members</b>,
-            valueLabel: "mau",
-            label: "MAU (members)",
-            barColor: "rgb(192, 85, 85)",
-          })}
-        </div>
-        <div>
-          {chart({
-            data: state["dauChartData"],
-            header: <b>Daily Active Members</b>,
-            valueLabel: "dau",
-            label: "DAU (members)",
-            barColor: "rgb(140, 85, 85)",
-          })}
-        </div>
-      </>
-    )}
-    {state.menu === "nft" && (
-      <div>
-        {chart({
-          data: state["nftMintsChartData"],
-          header: <b>NFT Mints Activity</b>,
-          valueLabel: "total_activity",
-          label: "NFT Mints Activity",
-          barColor: "rgb(85, 85, 180)",
-        })}
-      </div>
-    )}
-    {state.menu === "developer" && (
-      <div>
-        {chart({
-          header: <b>Top Widgets</b>,
-          staticDisplay: widgetRank(state.selectedCommunityAccountMembers),
-        })}
-      </div>
-    )}
+    <div>
+      {chart({
+        data: state["mauChartData"],
+        header: <b>Monthly Active Members</b>,
+        valueLabel: "mau",
+        label: "MAU (members)",
+        barColor: "rgb(192, 85, 85)",
+      })}
+    </div>
+
+    <div>
+      {chart({
+        data: state["dauChartData"],
+        header: <b>Daily Active Members</b>,
+        valueLabel: "dau",
+        label: "DAU (members)",
+        barColor: "rgb(140, 85, 85)",
+      })}
+    </div>
+
+    <div>
+      {chart({
+        data: state["nftMintsChartData"],
+        header: <b>NFT Mints Activity</b>,
+        valueLabel: "total_activity",
+        label: "NFT Mints Activity",
+        barColor: "rgb(85, 85, 180)",
+      })}
+    </div>
+
+    <div>
+      {chart({
+        header: <b>Top Widgets</b>,
+        staticDisplay: widgetRank(),
+      })}
+    </div>
   </div>
 );
