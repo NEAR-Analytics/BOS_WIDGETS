38f1c476ec16f19b3fe135f924514c6bc033e2cb
hack.near <hack.near>
2023-12-08 23:04:32 +0000

    Update meta.editor by hack.near at 2023-12-08T23:04:32.229Z

--- hack.near/meta.editor/source_code.jsx
+++ hack.near/meta.editor/source_code.jsx
@@ -8,11 +8,21 @@ return (
   <div className="m-2">
     <div className="mb-2 card">
       <div className="card-body">
-        <div className="text-truncate mb-3">
-          <Widget
-            src="hack.near/widget/thing.block"
-            props={{ creatorId, namespace, thingId }}
-          />
+        <div className="text-truncate mb-3 row">
+          <div className="col m-1 ">
+            <Widget
+              src="hack.near/widget/thing.block"
+              props={{ creatorId, namespace, thingId }}
+            />
+          </div>
+          <div className="col-2 m-2 mt-3 ">
+            <Widget
+              src="hack.near/widget/star.button"
+              props={{
+                path: `${creatorId}/${namespace}/${thingId}`,
+              }}
+            />
+          </div>
         </div>
         <Widget
           src="hack.near/widget/tags"
