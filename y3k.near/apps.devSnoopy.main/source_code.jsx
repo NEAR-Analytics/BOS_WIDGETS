d49b15a95b0049ba8b4754268e02f764cd2eea32
y3k.near <y3k.near>
2023-12-08 04:43:15 +0000

    Update apps.devSnoopy.main by y3k.near at 2023-12-08T04:43:15.558Z

--- y3k.near/apps.devSnoopy.main/source_code.jsx
+++ y3k.near/apps.devSnoopy.main/source_code.jsx
@@ -78,6 +78,56 @@ return (
   <div>
     <Widget src="y3k.near/widget/apps.devSnoopy.menu" props={{}} />
 
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="text-center mb-4">
+            Tracking Real-Time Engagement with NFTs
+          </h2>
+          <p class="text-center">
+            Dive into the pulse of current events monitored through ShardDog
+            NFTs and the DevSnoopy proxy contract. The table below provides a
+            real-time snapshot of ongoing projects within our vibrant community.
+          </p>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-md-4 mb-4">
+          <div class="card h-100">
+            <div class="card-body">
+              <h3 class="card-title">🌟 Explore Ongoing Events</h3>
+              <p class="card-text">
+                Get an in-depth view of each project. Click on any event to see
+                live interactions, participant engagement, and more.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100">
+            <div class="card-body">
+              <h3 class="card-title">🚀 Start Your Event</h3>
+              <p class="card-text">
+                Have an idea for an event? Launch your own and watch it grow.
+                Utilize our platform to track engagement and NFT interactions.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100">
+            <div class="card-body">
+              <h3 class="card-title">🔍 Deep Dive into Data</h3>
+              <p class="card-text">
+                Access detailed analytics for each event. Understand trends,
+                track NFT flow, and gain insights to optimize your projects.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <Widget
       src="y3k.near/widget/apps.devSnoopy.components.table"
       props={{ dataRegistry: tData }}
