9c1de2eaaadeb83b020df1c3390852845dad9f9e
y3k.near <y3k.near>
2023-12-08 18:47:37 +0000

    Update apps.devSnoopy.create by y3k.near at 2023-12-08T18:47:37.541Z

--- y3k.near/apps.devSnoopy.create/source_code.jsx
+++ y3k.near/apps.devSnoopy.create/source_code.jsx
@@ -1,6 +1,6 @@
 return (
   <div>
-    <Widget src="sharddog.near/widget/CreateSeries" props={{}} />
     <Widget src="y3k.near/widget/apps.devSnoopy.menu" props={{}} />
+    <Widget src="sharddog.near/widget/CreateSeries" props={{}} />
   </div>
 );
