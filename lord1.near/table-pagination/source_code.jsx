7506fa4f3ead3ced516258e86c80d3f0f3a5f03c
lord1.near <lord1.near>
2023-12-08 13:48:12 +0000

    Update table-pagination by lord1.near at 2023-12-08T13:48:12.924Z

--- lord1.near/table-pagination/source_code.jsx
+++ lord1.near/table-pagination/source_code.jsx
@@ -201,7 +201,7 @@ return (
                               height="40"
                               width="40"
                               layout="intrinsic"
-                              src={key}
+                              src={`${td.beforehref}${key}${td.afterhref}`}
                             />
                           ) : td.progress ? (
                             <td className="align-middle ">
