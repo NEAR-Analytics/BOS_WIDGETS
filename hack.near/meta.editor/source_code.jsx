da8af85833d5d8654344a199c88a9eb327da419c
hack.near <hack.near>
2023-12-08 22:34:20 +0000

    Update meta.editor by hack.near at 2023-12-08T22:34:20.260Z

--- hack.near/meta.editor/source_code.jsx
+++ hack.near/meta.editor/source_code.jsx
@@ -1,8 +1,7 @@
 const accountId = context.accountId;
-const path = props.path || "hack.near/widget/Academy";
+const path = props.path || "mob.near/widget/Explorer";
 const [creatorId, namespace, thingId] = path.split("/");
 
-// Fetch tags from Social based on the provided path
 const tags = Social.getr(`*/graph/context/${path}/tags/**`, "final");
 
 return (
@@ -15,35 +14,12 @@ return (
             props={{ creatorId, namespace, thingId }}
           />
         </div>
-        {/* Display the fetched tags */}
         <Widget
           src="hack.near/widget/tags"
           props={{
             path,
           }}
         />
-        <button
-          className="btn btn-outline-secondary m-2"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapseExample"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-        >
-          Guide
-        </button>
-      </div>
-      <div className="collapse" id="collapseExample">
-        <div className="card card-body m-3" style={{ background: "#f8f8f8" }}>
-          <h5>Best Practices:</h5>
-          <ul>
-            <li>Keep tags concise and simple</li>
-            <li>Use "-" (minus) instead of spaces</li>
-            <li>English words recommended</li>
-            <li>Do not create unnecessary tags</li>
-            <li>Be respectful to everyone #community</li>
-          </ul>
-        </div>
       </div>
     </div>
   </div>
