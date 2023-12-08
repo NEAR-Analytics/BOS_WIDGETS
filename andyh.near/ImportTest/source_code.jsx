7dd1f241b1172f3e318fa49783de0fab5d68cc25
andyh.near <andyh.near>
2023-12-08 19:37:15 +0000

    Update ImportTest by andyh.near at 2023-12-08T19:37:15.854Z

--- andyh.near/ImportTest/source_code.jsx
+++ andyh.near/ImportTest/source_code.jsx
@@ -1,4 +1,6 @@
 import { FaBeer } from "react-icons/fa";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 return (
   <div>
@@ -6,6 +8,7 @@ return (
       <span>Hello</span>
       <span>World</span>
     </>
+    <Slider range min={100} max={100} />
     <FaBeer />
     <Widget
       src="andyh.near/ImportTest.Child"
