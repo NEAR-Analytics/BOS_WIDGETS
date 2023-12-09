c51f00e2b0aff96d87413cb7788e6e4260e5abdf
andyh.near <andyh.near>
2023-12-09 00:28:25 +0000

    Update mui by andyh.near at 2023-12-09T00:28:25.640Z

---
+++ andyh.near/mui/source_code.jsx
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+
+return (
+  <div>
+    <Button variant="text">Text</Button>
+    <Button variant="contained">Contained</Button>
+    <Button variant="outlined">Outlined</Button>
+  </div>
+);
