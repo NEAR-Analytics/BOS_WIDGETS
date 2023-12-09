d10e735a41a9de5fc74db364e5311238fc3db3b1
zubairweb3.near <zubairweb3.near>
2023-12-09 08:57:11 +0000

    Update rafflestore.footer by zubairweb3.near at 2023-12-09T08:57:11.577Z

--- zubairweb3.near/rafflestore.footer/source_code.jsx
+++ zubairweb3.near/rafflestore.footer/source_code.jsx
@@ -1,7 +1,13 @@
-const footer1 =
-  "https://res.cloudinary.com/dfbqtfoxu/image/upload/v1700588098/rafflestore/x_abhrox.svg";
-const footer2 =
-  "https://res.cloudinary.com/dfbqtfoxu/image/upload/v1700588097/rafflestore/near_uwk1hd.svg";
+const horizon =
+  "https://ipfs.near.social/ipfs/bafkreihszpflge5j3g35td67xh7fw5z37rgyumh75uzabqrfnpcjfag2we";
+const devhub =
+  "https://ipfs.near.social/ipfs/bafkreido4srg4aj7l7yg2tz22nbu3ytdidjczdvottfr5ek6gqorwg6v74";
+
+const astra =
+  "https://ipfs.near.social/ipfs/bafkreidmuwft6jc52qu6srew7scvvbirkir4cljq6ogta6kelnjs3rvimq";
+
+const nfortune =
+  "https://res.cloudinary.com/dfbqtfoxu/image/upload/v1702112062/nfortune_t2qkp8.jpg";
 
 const Footer = styled.div`
 background: #003C8C;
@@ -18,10 +24,18 @@ const Title = styled.span`
 color: #fff;
 `;
 
+const styledImg = styled.img`
+width: 25px;
+height: 25px;
+border-radius: 50%;
+`;
+
 return (
   <Footer>
     <Title>Build on #BOS</Title>
-    <img src={footer1} width="20px" height="20px" alt="footer" />
-    <img src={footer2} width="20px" height="20px" alt="footer" />
+    <styledImg src={nfortune} alt="footer" />
+    <styledImg src={devhub} alt="footer" />
+    <styledImg src={horizon} alt="footer" />
+    <styledImg src={astra} alt="footer" />
   </Footer>
 );
