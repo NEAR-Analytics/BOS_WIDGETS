25e40dd76e4634bc4958340ee978c6e8df01c2d9
7fb71a85c00216db5eac6afa2c379981234a159a83a61cee7c4b3ebc83deac76 <7fb71a85c00216db5eac6afa2c379981234a159a83a61cee7c4b3ebc83deac76>
2023-12-11 15:24:59 +0000

    Update Navbar by 7fb71a85c00216db5eac6afa2c379981234a159a83a61cee7c4b3ebc83deac76 at 2023-12-11T15:24:59.087Z

--- 7fb71a85c00216db5eac6afa2c379981234a159a83a61cee7c4b3ebc83deac76/Navbar/source_code.jsx
+++ 7fb71a85c00216db5eac6afa2c379981234a159a83a61cee7c4b3ebc83deac76/Navbar/source_code.jsx
@@ -1,72 +1,63 @@
-return (
-  <div>
-    <div class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Navbar test
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Explore
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                My Collection
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Profile
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <Web3Connect
-                    className="LidoStakeFormSubmitContainer"
-                    connectLabel="Connect your wallet"
-                  />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <div class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
+  return (
+    <div>
+      <div class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Explore
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  My Collection
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Profile
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <Web3Connect
+                      className="LidoStakeFormSubmitContainer"
+                      connectLabel="Connect your wallet"
+                    />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Another action</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <div class="d-flex">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
