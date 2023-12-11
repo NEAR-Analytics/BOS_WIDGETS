8a7334a78befe3fc1243c9626c404303f234f19f
abubakarimam.near <abubakarimam.near>
2023-12-11 12:22:35 +0000

    Update HowITWorkSection by abubakarimam.near at 2023-12-11T12:22:35.984Z

---
+++ abubakarimam.near/HowITWorkSection/source_code.jsx
@@ -0,0 +1,57 @@
+const HowItWorksSectionWrapper = styled.div`
+  background-color: #fff;
+  padding: 50px 0;
+  text-align: center;
+`;
+
+const HowItWorksContent = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h2`
+  font-size: 2.5em;
+  margin-bottom: 20px;
+`;
+
+const Step = styled.div`
+  margin-bottom: 30px;
+`;
+
+const StepTitle = styled.h3`
+  font-size: 1.8em;
+  margin-bottom: 10px;
+`;
+
+const StepDescription = styled.p`
+  font-size: 1.2em;
+`;
+
+return (
+  <HowItWorksSectionWrapper>
+    <HowItWorksContent>
+      <Title>How It Works</Title>
+      <Step>
+        <StepTitle>1. Dispose Your Waste</StepTitle>
+        <StepDescription>
+          Collect your waste and hand it over to our authorized collecting
+          agents. Each kilogram earns you points.
+        </StepDescription>
+      </Step>
+      <Step>
+        <StepTitle>2. Earn Points</StepTitle>
+        <StepDescription>
+          Receive points based on the weight of your disposed waste. Earn 1
+          point for every kilogram, equivalent to N200.
+        </StepDescription>
+      </Step>
+      <Step>
+        <StepTitle>3. Get Paid</StepTitle>
+        <StepDescription>
+          Accumulate points and convert them to real earnings. Receive your
+          well-deserved payment through Near BOS.
+        </StepDescription>
+      </Step>
+    </HowItWorksContent>
+  </HowItWorksSectionWrapper>
+);
