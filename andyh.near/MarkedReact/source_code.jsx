3fd2ac5b6a9affc8dac4d59fddfd1f0d4e472e0e
andyh.near <andyh.near>
2023-12-11 20:12:51 +0000

    Update MarkedReact by andyh.near at 2023-12-11T20:12:51.877Z

--- andyh.near/MarkedReact/source_code.jsx
+++ andyh.near/MarkedReact/source_code.jsx
@@ -1,26 +1,6 @@
-const [Markdown, setMarkdown] = useState(null);
-const importMarkdown = async () => {
-  try {
-    const markdownDyn = await import(
-      "https://esm.sh/marked-react@2.0.0?alias=react:preact/compat&deps=preact@10.17.1"
-    );
-    console.log("markdown imported", markdownDyn);
-    setMarkdown(() => markdownDyn.default);
-  } catch (err) {
-    console.log("markdown import error", err);
-  }
-};
-
-useEffect(() => {
-  importMarkdown();
-}, []);
-
-if (Markdown) {
-  return (
-    <div>
-      <Markdown># I'm not even supposed to be here</Markdown>
-    </div>
-  );
-}
-
-return <>not imported yet</>;
+import Markdown from "marked-react@2.0.0";
+return (
+  <div>
+    <Markdown># I'm not even supposed to be here</Markdown>
+  </div>
+);
