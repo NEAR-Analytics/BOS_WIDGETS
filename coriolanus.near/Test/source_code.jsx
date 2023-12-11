9a7bf8b1c2232dadf6e689491df1371bc29b9eca
coriolanus.near <coriolanus.near>
2023-12-11 13:02:37 +0000

    Update Test by coriolanus.near at 2023-12-11T13:02:37.892Z

---
+++ coriolanus.near/Test/source_code.jsx
@@ -0,0 +1,49 @@
+State.init({
+  bootstraping: true,
+  loggedIn: false,
+});
+
+const contract = props.contract || "chat-simple.ws-protocol-63";
+
+const verifyKey = () => {
+  setTimeout(() => {
+    State.update({ bootstraping: false, loggedIn: result });
+  }, "5000");
+
+  // Near.hasValidCalimeroFak(contract).then((result) => {
+  //   State.update({ bootstraping: false, loggedIn: result });
+  //   if (result) {
+  //     updateMemberList().then((members) => {
+  //       if (!isMember(context.accountId, members)) {
+  //         Near.fakCalimeroCall(contract, "join");
+  //       }
+  //     });
+  //     updateChannelList();
+  //   }
+  // });
+};
+
+if (state.bootstraping) {
+  verifyKey();
+}
+
+const MainContainer = styled.div`
+  padding: 1rem;
+  width: 100%;
+`;
+
+return (
+  <MainContainer>
+    {context.accountId ? (
+      <>
+        {state.bootstraping ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div className="container">Hello Welcome</div>
+        )}
+      </>
+    ) : (
+      <h1>Please login to continue</h1>
+    )}
+  </MainContainer>
+);
