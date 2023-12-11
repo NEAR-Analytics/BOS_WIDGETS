5c4fe7c34d9f979bfe022256330fa298bfda9df0
abubakarimam.near <abubakarimam.near>
2023-12-11 14:44:26 +0000

    Update Footer by abubakarimam.near at 2023-12-11T14:44:26.434Z

--- abubakarimam.near/Footer/source_code.jsx
+++ abubakarimam.near/Footer/source_code.jsx
@@ -1,4 +1,4 @@
-const FooterWrapper = styled.footer`
+const FooterWrapper = styled.div`
   background-color: #333;
   color: white;
   padding: 20px 0;
@@ -15,7 +15,7 @@ const LogoContainer = styled.div`
 const LogoLink = styled.a`
   margin: 0 10px;
   img {
-    width: 40px; /* Set the desired width for the logos */
+    width: 40px; 
     height: auto;
   }
 `;
@@ -28,21 +28,30 @@ return (
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src="/path/to/near-logo.png" alt="Near Protocol" />
+        <img
+          src="https://res-console.cloudinary.com/dzl44lobc/media_explorer_thumbnails/38dc14a6d822158c00b12b3a04cd1e49/detailed"
+          alt="Near Protocol"
+        />
       </LogoLink>
       <LogoLink
         href="https://twitter.com/yourtwitterhandle"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src="/path/to/twitter-logo.png" alt="Twitter" />
+        <img
+          src="https://res-console.cloudinary.com/dzl44lobc/media_explorer_thumbnails/422cfdfe9f0e5243f463d70753565647/detailed"
+          alt="Twitter"
+        />
       </LogoLink>
       <LogoLink
         href="https://discord.com/invite/yourdiscordinvite"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src="/path/to/discord-logo.png" alt="Discord" />
+        <img
+          src="https://res-console.cloudinary.com/dzl44lobc/media_explorer_thumbnails/3ebba257b07165529571749dee930328/detailed"
+          alt="Discord"
+        />
       </LogoLink>
     </LogoContainer>
     <p>&copy; 2023 Your Company Name. All rights reserved.</p>
