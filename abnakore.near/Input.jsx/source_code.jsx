0bf751cac4d8e96ecbce03192ca7d285103ed160
abnakore.near <abnakore.near>
2023-12-11 22:42:11 +0000

    Update Input.jsx by abnakore.near at 2023-12-11T22:42:11.523Z

--- abnakore.near/Input.jsx/source_code.jsx
+++ abnakore.near/Input.jsx/source_code.jsx
@@ -1,5 +1,36 @@
+const Wrapper = styled.div`
+label {
+  position: relative;
+}
+label .input {
+  background-color: #333;
+  color: #fff;
+  width: 100%;
+  padding: 20px 5px 5px 10px;
+  outline: 0;
+  border: 1px solid rgba(105, 105, 105, 0.397);
+  border-radius: 10px;
+  font-size: medium;
+}
+
+.input + span {
+  color: rgba(255, 255, 255, 0.5);
+  position: absolute;
+  left: 10px;
+  top: 0px;
+  font-size: 0.9em;
+  cursor: text;
+  transition: 0.3s ease;
+}
+
+.input:placeholder-shown + span {
+  top: 12.5px;
+  font-size: 0.9em;
+}
+`;
+
 return (
-  <>
+  <Wrapper>
     {/* <div className="input-field"> */}
     <label>
       <input
@@ -11,5 +42,5 @@ return (
       <span>{props.placeholder}</span>
     </label>
     {/* </div> */}
-  </>
+  </Wrapper>
 );
