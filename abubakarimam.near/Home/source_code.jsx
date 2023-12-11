2e0ac126c5cb4d7f6f1242d11dbc10e26145389e
abubakarimam.near <abubakarimam.near>
2023-12-11 14:30:05 +0000

    Update Home by abubakarimam.near at 2023-12-11T14:30:05.436Z

--- abubakarimam.near/Home/source_code.jsx
+++ abubakarimam.near/Home/source_code.jsx
@@ -1,8 +1,18 @@
 const Wrapper = styled.div`
-Width: 100%
+Width: 100%;
+`;
+
+const Container = styled.div`
+background-color: #4caf50;
 `;
 return (
   <Wrapper>
     <Widget src="abubakarimam.near/widget/Header" />
+    <Container>
+      <Widget src="abubakarimam.near/widget/Hero" />
+      <Widget src="abubakarimam.near/widget/IntroSection" />
+      <Widget src="abubakarimam.near/widget/HowITWorkSection" />
+      <Widget src="abubakarimam.near/widget/BlockchainComponent" />
+    </Container>
   </Wrapper>
 );
