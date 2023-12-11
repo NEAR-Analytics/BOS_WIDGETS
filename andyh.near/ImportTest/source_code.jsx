c04795d442081e133d92057e82c45e0df72d1f55
andyh.near <andyh.near>
2023-12-11 19:01:06 +0000

    Update ImportTest by andyh.near at 2023-12-11T19:01:06.964Z

--- andyh.near/ImportTest/source_code.jsx
+++ andyh.near/ImportTest/source_code.jsx
@@ -1,7 +1,7 @@
-// import { FaBeer } from "react-icons/fa";
 import { AiOutlineDeploymentUnit } from "https://esm.sh/react-icons@4.10.0/ai?alias=react:preact/compat&deps=preact@10.17.1";
 import { FaBeer } from "https://unpkg.com/react-icons@4.12.0/fa";
 
+// import { FaBeer } from "react-icons/fa";
 // import Slider from "rc-slider@10.5.0";
 // import * as zzz from "rc-slider@10.5.0/";
 // import "rc-slider/assets/index.css";
