0222f4478a03f38710028034fa17bb15f76e0619
abnakore.near <abnakore.near>
2023-12-11 17:48:08 +0000

    Update Aside.jsx by abnakore.near at 2023-12-11T17:48:08.582Z

---
+++ abnakore.near/Aside.jsx/source_code.jsx
@@ -0,0 +1,19 @@
+return (
+  <div>
+    <div id="tabs">
+      {/* Render the objs as links */}
+      {objs.map((obj) => (
+        <Link
+          key={obj.link}
+          to={obj.link}
+          className={`tab ${active === obj.link ? "active" : ""}`}
+        >
+          {obj.name}
+        </Link>
+      ))}
+      {/* <Link to='/' className="tab">Vote Page</Link>
+                <Link to='/result' className="tab">Results</Link>
+                <Link to='/' className="tab">Login/Logout</Link> */}
+    </div>
+  </div>
+);
