2c69f333e6f919057846a8b4f335877608293d46
abnakore.near <abnakore.near>
2023-12-11 17:51:32 +0000

    Update Input.jsx by abnakore.near at 2023-12-11T17:51:32.049Z

---
+++ abnakore.near/Input.jsx/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <>
+    {/* <div className="input-field"> */}
+    <label>
+      <input className="input" type={type} placeholder="" required={required} />
+      <span>{placeholder}</span>
+    </label>
+    {/* </div> */}
+  </>
+);
