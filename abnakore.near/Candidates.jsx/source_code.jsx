ba3636fadb7bccfac76c5fc1033ec540fd09b18b
abnakore.near <abnakore.near>
2023-12-11 17:49:18 +0000

    Update Candidates.jsx by abnakore.near at 2023-12-11T17:49:18.088Z

--- abnakore.near/Candidates.jsx/source_code.jsx
+++
@@ -1,7 +0,0 @@
-const [candidates, setCandidate] = useState([
-  { id: 1, name: "Foo", votes: 3, rank: 2 },
-  { id: 2, name: "Bar", votes: 5, rank: 1 },
-  { id: 3, name: "Baz", votes: 1, rank: 4 },
-  { id: 4, name: "sass", votes: 2, rank: 3 },
-]);
-return [candidates, setCandidate];
