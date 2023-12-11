438972967e2c43dfd72727703191814b23c0a6c8
abnakore.near <abnakore.near>
2023-12-11 22:49:25 +0000

    Update Aside.jsx by abnakore.near at 2023-12-11T22:49:25.087Z

--- abnakore.near/Aside.jsx/source_code.jsx
+++ abnakore.near/Aside.jsx/source_code.jsx
@@ -1,3 +1,36 @@
+const Wrapper = styled.div`
+
+aside {
+  position: fixed;
+  background-color: #333;
+  padding: 20px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  top: 0;
+  left: 0;
+}
+aside #tabs {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+aside #tabs .tab {
+  padding: 10px;
+  font-size: 16px;
+  color: #fff;
+  text-align: center;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+aside #tabs .tab:hover, aside #tabs .active {
+  background-color: #555;
+}
+
+`;
+
 return (
   <div>
     <div id="tabs">
