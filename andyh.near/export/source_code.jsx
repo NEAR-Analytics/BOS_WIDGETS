350bdfbbdca52ad77ee26f797dfc258a1fe030ed
andyh.near <andyh.near>
2023-12-12 00:34:27 +0000

    Update export by andyh.near at 2023-12-12T00:34:27.569Z

--- andyh.near/export/source_code.jsx
+++ andyh.near/export/source_code.jsx
@@ -1,11 +1,5 @@
-import type { ChildProps } from "./Export.Child";
 import Markdown from "marked-react";
 
-export function BWEComponent() {
-  return (
-    <>
-      <Markdown># ≥≈˚∆˚≈≤ </Markdown>
-      <Component src="andyh.near/Export.Child" props={{ name: "Child" } as ChildProps} />
-    </>
-  );
+export function X() {
+  return <Markdown># ¶x!</Markdown>;
 }
