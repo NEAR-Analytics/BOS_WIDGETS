1f92ce6d30e0f37ce4d9046c3de3cc6b374141d7
abnakore.near <abnakore.near>
2023-12-12 01:23:56 +0000

    Update ManageCandidates.jsx by abnakore.near at 2023-12-12T01:23:56.301Z

--- abnakore.near/ManageCandidates.jsx/source_code.jsx
+++ abnakore.near/ManageCandidates.jsx/source_code.jsx
@@ -2,16 +2,69 @@
 const [pages, setPage] = useState([
   { name: "Home", link: "/admin" },
   { name: "Manage Candidates", link: "/admin/manage_candidates" },
-  { name: "Mange Votes", link: "/manv" },
+  { name: "Mange Parties", link: "/admin/manage_parties" },
 ]);
 
 // List of candidates and their curresponding number of votes
 const [candidates, setCandidates] = useState([
-  { id: 1, name: "Foo", votes: 3, rank: 2 },
-  { id: 2, name: "Bar", votes: 5, rank: 1 },
-  { id: 3, name: "Baz", votes: 1, rank: 4 },
-  { id: 4, name: "sass", votes: 2, rank: 3 },
+  {
+    id: 1,
+    name: "Foo",
+    party: "APD",
+    email: "example@gmail.com",
+    role: "President",
+    votes: 3,
+    rank: 2,
+  },
+  {
+    id: 2,
+    name: "Bar",
+    party: "YMB",
+    email: "example@gmail.com",
+    role: "President",
+    votes: 5,
+    rank: 1,
+  },
+  {
+    id: 3,
+    name: "Baz",
+    party: "AAN",
+    email: "example@gmail.com",
+    role: "President",
+    votes: 1,
+    rank: 4,
+  },
+  {
+    id: 4,
+    name: "sass",
+    party: "AKA",
+    email: "example@gmail.com",
+    role: "President",
+    votes: 2,
+    rank: 3,
+  },
 ]);
+
+const [maxId, setMaxId] = useState(4);
+
+// name and acro
+const [newCandidate, setNewCandidate] = useState({
+  id: "",
+  name: "",
+  party: "",
+  email: "",
+  role: "",
+  votes: "",
+  rank: "",
+});
+
+function save() {
+  newCandidate.id = maxId + 1;
+  newCandidate.votes = maxId + 1;
+  newCandidate.rank = maxId + 1;
+  setMaxId(maxId + 1);
+  setCandidates((prev) => prev.concat([newCandidate]));
+}
 
 return (
   <>
@@ -23,7 +76,15 @@ return (
     <Widget
       src="abnakore.near/widget/Table.jsx"
       props={{
-        headings: ["S/N", "Candidate's Name", "Number of votes", "Rank"],
+        headings: [
+          "S/N",
+          "Candidate's Name",
+          "Party",
+          "Email",
+          "Role",
+          "Number of votes",
+          "Rank",
+        ],
         data: Object.values(candidates.map((c) => Object.values(c))),
       }}
     />
@@ -31,22 +92,53 @@ return (
       <div className="flex">
         <Widget
           src="abnakore.near/widget/Input.jsx"
-          props={{ type: "text", placeholder: "Full Name", required: true }}
+          props={{
+            type: "text",
+            placeholder: "Full Name",
+            required: true,
+            item: "name",
+            items: newCandidate,
+            setItem: setNewCandidate,
+          }}
         />
         <Widget
           src="abnakore.near/widget/Input.jsx"
-          props={{ type: "text", placeholder: "party", required: true }}
+          props={{
+            type: "text",
+            placeholder: "party",
+            required: true,
+            item: "party",
+            items: newCandidate,
+            setItem: setNewCandidate,
+          }}
         />
       </div>
       <div className="flex">
         <Widget
           src="abnakore.near/widget/Input.jsx"
-          props={{ type: "email", placeholder: "Email", required: true }}
+          props={{
+            type: "email",
+            placeholder: "Email",
+            required: true,
+            item: "email",
+            items: newCandidate,
+            setItem: setNewCandidate,
+          }}
         />
         <Widget
           src="abnakore.near/widget/Input.jsx"
-          props={{ type: "text", placeholder: "Role", required: true }}
+          props={{
+            type: "text",
+            placeholder: "Role",
+            required: true,
+            item: "role",
+            items: newCandidate,
+            setItem: setNewCandidate,
+          }}
         />
+        <CommitButton onCommit={save} data={{ candidates: [newCandidate] }}>
+          Add
+        </CommitButton>
       </div>
     </div>
   </>
