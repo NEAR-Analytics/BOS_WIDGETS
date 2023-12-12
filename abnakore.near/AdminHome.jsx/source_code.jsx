cb6240604c95398ea3c310ff3da882a7d811c9f9
abnakore.near <abnakore.near>
2023-12-12 01:52:12 +0000

    Update AdminHome.jsx by abnakore.near at 2023-12-12T01:52:12.623Z

--- abnakore.near/AdminHome.jsx/source_code.jsx
+++ abnakore.near/AdminHome.jsx/source_code.jsx
@@ -2,15 +2,55 @@
 const [pages, setPage] = useState([
   { name: "Home", link: "/admin" },
   { name: "Manage Candidates", link: "/admin/manage_candidates" },
-  { name: "Mange Votes", link: "/manv" },
+  { name: "Mange Parties", link: "/admin/manage_parties" },
+]);
+
+// List of parties and their curresponding number of votes
+const [parties, setParties] = useState([
+  { id: 1, name: "Foo", acronym: "FOO" },
+  { id: 2, name: "Bar", acronym: "BAR" },
+  { id: 3, name: "Baz", acronym: "BAZ" },
+  { id: 4, name: "sass", acronym: "SAS" },
 ]);
 
 // List of candidates and their curresponding number of votes
 const [candidates, setCandidates] = useState([
-  { id: 1, name: "Foo", votes: 3, rank: 2 },
-  { id: 2, name: "Bar", votes: 5, rank: 1 },
-  { id: 3, name: "Baz", votes: 1, rank: 4 },
-  { id: 4, name: "sass", votes: 2, rank: 3 },
+  {
+    id: 1,
+    name: "Foo",
+    party: "APD",
+    email: "example@gmail.com",
+    role: "President",
+    votes: 3,
+    rank: 2,
+  },
+  {
+    id: 2,
+    name: "Bar",
+    party: "YMB",
+    email: "example@gmail.com",
+    role: "President",
+    votes: 5,
+    rank: 1,
+  },
+  {
+    id: 3,
+    name: "Baz",
+    party: "AAN",
+    email: "example@gmail.com",
+    role: "President",
+    votes: 1,
+    rank: 4,
+  },
+  {
+    id: 4,
+    name: "sass",
+    party: "AKA",
+    email: "example@gmail.com",
+    role: "President",
+    votes: 2,
+    rank: 3,
+  },
 ]);
 
 return (
@@ -20,12 +60,28 @@ return (
       props={{ objs: pages, active: "/admin" }}
     />
     <h1>Admin Home</h1>
-    <h2>Candidates Details</h2>
+    <h4>Candidates Details</h4>
     <Widget
       src="abnakore.near/widget/Table.jsx"
       props={{
-        headings: ["S/N", "Candidate's Name", "Number of votes", "Rank"],
+        headings: [
+          "S/N",
+          "Candidate's Name",
+          "Party",
+          "Email",
+          "Role",
+          "Number of votes",
+          "Rank",
+        ],
         data: Object.values(candidates.map((c) => Object.values(c))),
+      }}
+    />
+    <h4>Parties Details</h4>
+    <Widget
+      src="abnakore.near/widget/Table.jsx"
+      props={{
+        headings: ["S/N", "Party Name", "Acronym"],
+        data: Object.values(parties.map((c) => Object.values(c))),
       }}
     />
   </>
