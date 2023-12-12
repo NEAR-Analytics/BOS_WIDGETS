0e067cd5d3d03da722b6f2bd121990356f5b0fb2
dapdapbos.near <dapdapbos.near>
2023-12-12 05:04:04 +0000

    Update Scroll.Uniswap.Swap.PriceImpactWarningButton by dapdapbos.near at 2023-12-12T05:04:04.856Z

---
+++ dapdapbos.near/Scroll.Uniswap.Swap.PriceImpactWarningButton/source_code.jsx
@@ -0,0 +1,21 @@
+const Button = styled.div`
+  border-radius: 12px;
+  background: rgba(255, 104, 75, 0.2);
+  height: 48px;
+  padding: 15px 18px 14px 15px;
+  color: #ff684b;
+  font-size: 14px;
+  font-weight: 400;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 6px;
+  cursor: pointer;
+`;
+
+return (
+  <Button onClick={props.onClick}>
+    <span>Price impact warning</span>
+    <span>{props.priceImpact}%</span>
+  </Button>
+);
