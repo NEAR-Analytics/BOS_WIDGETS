2e3e288f0ca5a8526f4b1907cf6950e11a004612
abnakore.near <abnakore.near>
2023-12-12 09:12:08 +0000

    Update main.jsx by abnakore.near at 2023-12-12T09:12:08.118Z

--- abnakore.near/main.jsx/source_code.jsx
+++ abnakore.near/main.jsx/source_code.jsx
@@ -15,7 +15,49 @@
 //   createBrowserRouter,
 //   RouterProvider,
 // } from "react-router-dom";
-const router = createBrowserRouter([
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Widget src="abnakore.near/widget/App.jsx" props={props} />,
+//   },
+//   {
+//     path: "/register",
+//     element: <Widget src="abnakore.near/widget/Register.jsx" props={props} />,
+//   },
+//   {
+//     path: "/signin",
+//     element: <Widget src="abnakore.near/widget/SignIn.jsx" props={props} />,
+//   },
+//   {
+//     path: "/result",
+//     element: <Widget src="abnakore.near/widget/Result.jsx" props={props} />,
+//   },
+//   {
+//     path: "/admin",
+//     element: <Widget src="abnakore.near/widget/AdminHome.jsx" props={props} />,
+//   },
+//   {
+//     path: "/admin/manage_candidates",
+//     element: (
+//       <Widget src="abnakore.near/widget/ManageCandidates.jsx" props={props} />
+//     ),
+//   },
+//   {
+//     path: "/admin/manage_parties",
+//     element: (
+//       <Widget src="abnakore.near/widget/ManageParties.jsx" props={props} />
+//     ),
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
+const pages = [
   {
     path: "/",
     element: <Widget src="abnakore.near/widget/App.jsx" props={props} />,
@@ -48,12 +90,5 @@ const router = createBrowserRouter([
       <Widget src="abnakore.near/widget/ManageParties.jsx" props={props} />
     ),
   },
-]);
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
-
-return <RouterProvider router={router} />;
+];
+return <Widget src="abnakore.near/widget/App.jsx" props={props} />;
