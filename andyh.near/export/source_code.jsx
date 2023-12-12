3e88ac8fe9dc8e2139b7be28ed8d0a11deafeffb
andyh.near <andyh.near>
2023-12-12 00:52:36 +0000

    Update export by andyh.near at 2023-12-12T00:52:36.453Z

--- andyh.near/export/source_code.jsx
+++ andyh.near/export/source_code.jsx
@@ -1,5 +1,10 @@
 import Markdown from "marked-react";
 
 export function X() {
-  return <Markdown># ¶x!</Markdown>;
+  return (
+    <>
+      <Markdown># ¶x!</Markdown>
+      <Component src="andyh.near/export.child" props={{ name: "grun" }} />
+    </>
+  );
 }
