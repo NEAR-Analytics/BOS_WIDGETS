4da0c766fb5d4af91e034189c7164c2d4f4afffc
andyh.near <andyh.near>
2023-12-12 18:11:12 +0000

    Update export.child by andyh.near at 2023-12-12T18:11:12.552Z

--- andyh.near/export.child/source_code.jsx
+++ andyh.near/export.child/source_code.jsx
@@ -1,9 +1,9 @@
 import Markdown from "marked-react";
 
-interface ChildProps {
-  name: string;
-}
-
 export const Child = (props: ChildProps) => {
   return <Markdown>{`#### child ${props.name}`}</Markdown>;
 };
+
+interface ChildProps {
+  name: string;
+}
