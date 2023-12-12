cadba78d48b2f2f95f9d7756c4afeded40629660
ciocan.near <ciocan.near>
2023-12-12 19:47:50 +0000

    Update bos-showcase by ciocan.near at 2023-12-12T19:47:50.945Z

--- ciocan.near/bos-showcase/source_code.jsx
+++ ciocan.near/bos-showcase/source_code.jsx
@@ -1,6 +1,10 @@
+const { hideDisclaimer } = props;
+
 return (
   <>
     <Widget src="ciocan.near/widget/bos-showcase-content" />
-    <Widget src="ciocan.near/widget/bos-showcase-disclaimer" />
+    {!hideDisclaimer && (
+      <Widget src="ciocan.near/widget/bos-showcase-disclaimer" />
+    )}
   </>
 );
