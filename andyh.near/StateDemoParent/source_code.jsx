c6ea29f43e3bcc04ee312b1a90e0f82ee385f023
andyh.near <andyh.near>
2023-12-12 23:43:14 +0000

    Update StateDemoParent by andyh.near at 2023-12-12T23:43:14.787Z

--- andyh.near/StateDemoParent/source_code.jsx
+++ andyh.near/StateDemoParent/source_code.jsx
@@ -1,26 +1,28 @@
-const {
-  circle,
-  square,
-  triangle,
-  updateCircle,
-  updateSquare,
-  updateTriangle,
-  id,
-} = props;
+export default function (props) {
+  const {
+    circle,
+    square,
+    triangle,
+    updateCircle,
+    updateSquare,
+    updateTriangle,
+    id,
+  } = props;
 
-return (
-  <div>
-    <Widget
-      id={id}
-      src="andyh.near/ShapeSet"
-      props={{
-        circle,
-        square,
-        triangle,
-        updateCircle,
-        updateSquare,
-        updateTriangle,
-      }}
-    />
-  </div>
-);
+  return (
+    <div>
+      <Widget
+        id={id}
+        src="andyh.near/ShapeSet"
+        props={{
+          circle,
+          square,
+          triangle,
+          updateCircle,
+          updateSquare,
+          updateTriangle,
+        }}
+      />
+    </div>
+  );
+}
