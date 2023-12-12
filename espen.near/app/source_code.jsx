13931f389704813ab9763d7ecbcc4f16de5b5e9d
espen.near <espen.near>
2023-12-12 17:40:23 +0000

    Update app by espen.near at 2023-12-12T17:40:23.391Z

--- espen.near/app/source_code.jsx
+++ espen.near/app/source_code.jsx
@@ -162,7 +162,14 @@ function Page() {
         />
       );
     }
-
+    case "admin": {
+      return (
+        <Widget
+          src={"espen.near/widget/devhub.page.admin.index"}
+          props={passProps}
+        />
+      );
+    }
     default: {
       // TODO: 404 page
       return <p>404</p>;
