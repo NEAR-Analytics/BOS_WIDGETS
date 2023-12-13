b94c0d10dd43fd65d9e5272ec0f048386965e453
chanon.near <chanon.near>
2023-12-13 03:34:26 +0000

    Update CCIP by chanon.near at 2023-12-13T03:34:26.019Z

--- chanon.near/CCIP/source_code.jsx
+++ chanon.near/CCIP/source_code.jsx
@@ -67,7 +67,14 @@ if (!oneInchAbi.ok) {
   return "1inch not ok";
 }
 
-async function mintToken() {
+const ccipSender = fetch(
+  "https://gist.githubusercontent.com/taforyou/810058bb7c7fc277f3e56a23a8534658/raw/efe0059806f3b6906704896a385a8eecbb9abd87/CCIPSender.abi.json"
+);
+if (!ccipSender.ok) {
+  return "ccipSender not ok";
+}
+
+async function queryBalanceOf() {
   try {
     const contractAddress = "0x51E5426eDE4e2d4c2586371372313B5782387222"; // Apetimisim
     const oneInch = new ethers.Contract(
@@ -83,10 +90,54 @@ async function mintToken() {
   }
 }
 
+async function mintToken() {
+  try {
+    const contractAddress = "0xd0FD63f94811d9Fd8f147f22bd94AbF21c1d0445"; // Avalance Fuji sender
+    const ccip = new ethers.Contract(
+      contractAddress,
+      ccipSender.body,
+      Ethers.provider().getSigner()
+    );
+    // Ethers.provider()
+    //   .getFeeData()
+    //   .then((x) => {
+    //     console.log("fee Data :", x);
+    //   });
+    // Ethers.provider()
+    //   .getGasPrice()
+    //   .then((x) => {
+    //     console.log("x ", x);
+    //   });
+    // Ethers.provider().getGasPrice()
+    // const gasLimit = ethers.utils.hexlify(1000000); // Example gas limit
+    // const gasPrice = ethers.utils.parseUnits("10", "gwei"); // Example gas price
+    ccip
+      // .mintNFT({
+      //   gasPrice: "0x59682f00",
+      //   maxFeePerGas: "0x0bfda3a300",
+      //   maxPriorityFeePerGas: "0x59682f00",
+      // })
+      .mintNFT()
+      // .mintNFT({ gasPrice: "0x59682f00" })
+      // .mintNFT({ gasLimit: "0x826299e00" })
+      .then((x) => {
+        console.log("result :=> x ", x); // My own wallet got 4 - 0x04
+      });
+    // ccip.owner().then((x) => {
+    //   console.log("result :=> x ", x); // My own wallet got 4 - 0x04
+    // });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 return (
   <div>
-    <button class="btn btn-success" onClick={mintToken} disabled={false}>
+    <button class="btn btn-success" onClick={queryBalanceOf} disabled={false}>
       QueryBalance of
+    </button>
+    <button class="btn btn-success" onClick={mintToken} disabled={false}>
+      Mint
     </button>
   </div>
 );
