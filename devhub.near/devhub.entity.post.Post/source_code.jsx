7d776934d7dd6a834b67129cf6d6b3cb18432138
devhub.near <devhub.near>
2023-12-13 16:03:48 +0000

    Update devhub.entity.post.Post by devhub.near at 2023-12-13T16:03:48.542Z

--- devhub.near/devhub.entity.post.Post/source_code.jsx
+++ devhub.near/devhub.entity.post.Post/source_code.jsx
@@ -189,7 +189,6 @@ const header = (
               }
               props={{
                 accountId: post.author_id,
-                nearDevGovGigsWidgetsAccountId: "devhub.near",
               }}
             />
           </ProfileCardContainer>
@@ -284,8 +283,7 @@ const onLike = () => {
       args: {
         post_id: postId,
       },
-      deposit: Big(10).pow(21).mul(2),
-      gas: Big(10).pow(12).mul(100),
+      gas: Big(10).pow(14),
     },
   ];
 
@@ -297,8 +295,8 @@ const onLike = () => {
         predecessor_id: "devgovgigs.near",
         keys: [context.accountId + "/index/notify"],
       },
-      deposit: Big(10).pow(23),
-      gas: Big(10).pow(12).mul(30),
+      gas: Big(10).pow(14),
+      deposit: Big(10).pow(22),
     });
   }
 
@@ -352,8 +350,7 @@ const buttonsFooter = props.isPreview ? null : (
             "Like"
           ) : (
             <Widget
-              // TODO: LEGACY.
-              src="devgovgigs.near/widget/gigs-board.components.layout.LikeButton.Faces"
+              src="devhub.near/widget/devhub.components.layout.LikeButton.Faces"
               props={{
                 likesByUsers: Object.fromEntries(
                   post.likes.map(({ author_id }) => [author_id, ""])
@@ -563,6 +560,25 @@ const toggleEditor = () => {
   State.update({ showEditor: !state.showEditor });
 };
 
+let amount = null;
+let token = null;
+let supervisor = null;
+
+if (state.postType === "Solution") {
+  const amountMatch = post.snapshot.description.match(
+    /Requested amount: (\d+(\.\d+)?) (\w+)/
+  );
+  amount = amountMatch ? parseFloat(amountMatch[1]) : null;
+  token = amountMatch ? amountMatch[3] : null;
+
+  const sponsorMatch = post.snapshot.description.match(
+    /Requested sponsor: @([^\s]+)/
+  );
+  supervisor = sponsorMatch ? sponsorMatch[1] : null;
+}
+
+const seekingFunding = amount !== null || token !== null || supervisor !== null;
+
 function Editor() {
   return (
     <div class="row" id={`accordion${postId}`} key="editors-footer">
@@ -597,9 +613,11 @@ function Editor() {
                 labels: post.snapshot.labels,
                 name: post.snapshot.name,
                 description: post.snapshot.description,
-                amount: post.snapshot.amount,
-                token: tokenResolver(post.snapshot.sponsorship_token),
-                supervisor: post.snapshot.supervisor,
+                amount: post.snapshot.amount || amount,
+                token: tokenResolver(post.snapshot.sponsorship_token || token),
+                supervisor:
+                  post.snapshot.post.snapshot.supervisor || supervisor,
+                seekingFunding: seekingFunding,
                 githubLink: post.snapshot.github_link,
                 onDraftStateChange,
                 draftState:
@@ -627,11 +645,16 @@ const tags = post.snapshot.labels ? (
       <div className="d-flex align-items-center my-3 me-3">
         <Link
           to={href({
-            widgetSrc: "#/devhub.near/widget/app",
+            widgetSrc: "devhub.near/widget/app",
             params: { page: "feed", tag: tag },
           })}
         >
           <div
+            onClick={() => {
+              if (typeof props.updateTagInParent === "function") {
+                props.updateTagInParent(tag);
+              }
+            }}
             className="d-flex gap-3 align-items-center"
             style={{ cursor: "pointer", textDecoration: "none" }}
           >
