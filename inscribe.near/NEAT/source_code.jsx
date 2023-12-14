d86fd5e108171d10459c917bf87382d32bd19d60
inscribe.near <inscribe.near>
2023-12-14 10:04:56 +0000

    Update NEAT by inscribe.near at 2023-12-14T10:04:56.210Z

--- inscribe.near/NEAT/source_code.jsx
+++ inscribe.near/NEAT/source_code.jsx
@@ -3,13 +3,7 @@ const MaxGasPerTransaction = TGas.mul(250);
 const GasPerTransaction = MaxGasPerTransaction.plus(TGas);
 const pageAmountOfPage = 5;
 const ipfsPrefix = "https://ipfs.near.social/ipfs";
-const landingUrl = "https://neatprotocol.ai";
-const partnerProgramUrl = "https://forms.gle/4M3fvw3LPiJSyffcA";
-const nrc20DocHost = "https://docs.nrc-20.io/";
-const SEC_OF_MS = 1000;
-const MIN_OF_MS = 60 * SEC_OF_MS;
-const HOUR_OF_MS = 60 * MIN_OF_MS;
-const DAY_OF_MS = HOUR_OF_MS * 24;
+
 function toLocaleString(source, decimals, rm) {
   if (typeof source === "string") {
     return toLocaleString(Number(source), decimals);
@@ -31,26 +25,12 @@ function toLocaleString(source, decimals, rm) {
   }
 }
 
-function formatAmount(_balance, _decimal) {
-  const balance = _balance ?? 0;
-  const decimal = _decimal ?? 8;
+function formatAmount(balance, decimal) {
+  if (!decimal) decimal = 8;
   return toLocaleString(
     Big(balance).div(Big(10).pow(decimal)).toFixed(),
     decimal
   );
-}
-
-function formatDeployTime(blockTime) {
-  const milliseconds = blockTime / 1000000;
-  const date = new Date(milliseconds);
-
-  const year = date.getUTCFullYear();
-  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
-  const day = date.getUTCDate().toString().padStart(2, "0");
-  const hours = date.getUTCHours().toString().padStart(2, "0");
-  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
-  const seconds = date.getUTCSeconds().toString().padStart(2, "0");
-  return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
 }
 
 // Config for Bos app
@@ -60,7 +40,7 @@ function getConfig(network) {
       return {
         ownerId: "inscribe.near",
         graphUrl:
-          "https://gateway-arbitrum.network.thegraph.com/api/98b4f8ff96be187a889dddcac0e3ef13/subgraphs/id/47RQk8YD4XqgczsgNYSNaWVQLNNwt49DuAuMAxCiLXJZ",
+          "https://api.thegraph.com/subgraphs/name/inscriptionnear/neat",
         nodeUrl: "https://rpc.mainnet.near.org",
         contractName: "inscription.near",
         methodName: "inscribe",
@@ -75,24 +55,14 @@ function getConfig(network) {
           op: "transfer",
           tick: "neat",
         },
-        ftWrapperFactory: "nrc-20.near",
         ftWrapper: "neat.nrc-20.near",
         refFinance: "https://app.ref.finance/",
-        minMintEvents: 1_000_000,
-        minHolders: 1_000,
-        neatDecimals: 8,
-        nearDecimals: 24,
-        stakingContractName: "neat-staking.near",
-        wNearTokenId: "wrap.near",
-        refContractId: "v2.ref-finance.near",
-        neatPoolId: 4243,
-        firstFarmStartTimeUTC: "2024-06-03T08:00Z",
       };
     case "testnet":
       return {
         ownerId: "inscribe.testnet",
         graphUrl:
-          "https://api.studio.thegraph.com/query/76896/neat-test/version/latest",
+          "https://api.thegraph.com/subgraphs/name/inscriptionnear/neat-test",
         nodeUrl: "https://rpc.testnet.near.org",
         contractName: "inscription.testnet",
         methodName: "inscribe",
@@ -107,18 +77,8 @@ function getConfig(network) {
           op: "transfer",
           tick: "neat",
         },
-        ftWrapperFactory: "nrc-20.testnet",
         ftWrapper: "neat.nrc-20.testnet",
         refFinance: "https://testnet.ref-finance.com/",
-        minMintEvents: 10,
-        minHolders: 5,
-        neatDecimals: 8,
-        nearDecimals: 24,
-        stakingContractName: "neat-staking.testnet",
-        wNearTokenId: "wrap.testnet",
-        refContractId: "exchange.ref-dev.testnet",
-        neatPoolId: 728,
-        firstFarmStartTimeUTC: "2024-06-03T08:00Z",
       };
     default:
       throw Error(`Unconfigured environment '${network}'.`);
@@ -131,12 +91,6 @@ const tx = {
   args: config.args,
   gas: GasPerTransaction,
 };
-
-const RPS_MULTIPLIER = 1e24;
-
-function ftWrapperAddress(tick) {
-  return tick.toLowerCase() + "." + config.ftWrapperFactory;
-}
 
 const Main = styled.div`
   width: 100%;
@@ -172,17 +126,6 @@ const NeatLink = styled.a`
   text-decoration: underline;
 `;
 
-const NeatCommonLink = styled.a`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  color: #fff;
-  font-size: 14px;
-  font-weight: 600;
-  text-decoration: underline;
-`;
-
 const HeaderContainer = styled.div`
   padding: 18px 0;
   display: flex;
@@ -200,11 +143,6 @@ const TabContainer = styled.div`
   justify-content: center;
   align-items: center;
   gap: 56px;
-  flex-flow: wrap;
-
-  @media (max-width: 768px) {
-    gap: 20px;
-  }
 `;
 
 const TabItem = styled("Link")`
@@ -217,9 +155,6 @@ const TabItem = styled("Link")`
     color: white;
     text-decoration: none;
     opacity: 0.8;
-  }
-  @media (max-width: 768px) {
-    font-size: 16px;
   }
 `;
 
@@ -239,52 +174,11 @@ const FormContainer = styled.div`
   }
 `;
 
-const FooterWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-  margin: 40px 0;
-`;
-
-const Notification = styled.div`
-  position: absolute;
-  right: 40px;
-  top: 50px;
-  z-index: 1;
-
-  @media (max-width: 768px) {
-    display: none;
-  }
-`;
-
-const NRC20Link = styled.a`
-  color: white;
-  text-decoration: underline;
-  text-underline-offset: 3px;
-`;
-
-const NotificationImg = () => {
-  return (
-    <img
-      style={{ cursor: "pointer" }}
-      onClick={() =>
-        State.update({
-          showNotification: false,
-        })
-      }
-      src={`${ipfsPrefix}/bafkreifzxbehbqzwatzsilj4tmxh3r6rypdm2wiutud34rifx5oxkvdmea`}
-      width={24}
-      height={24}
-    />
-  );
-};
-
 
 
 function getTab() {
   const tab = props.tab;
-  if (["transfer", "wrap", "indexer", "mint", "stake"].includes(tab)) {
+  if (["transfer", "wrap", "indexer", "mint"].includes(tab)) {
     return tab;
   } else {
     return "transfer";
@@ -293,46 +187,19 @@ function getTab() {
 
 const tab = getTab();
 
-State.init({
-  showNotification: true,
-});
-
 return (
   <Main>
-    {state.showNotification && (
-      <Notification>
-        <FormContainer
-          style={{
-            gap: "20px",
-            flexDirection: "row",
-          }}
-        >
-          <NRC20Link href={`/${config.ownerId}/widget/NRC-20`} target="_blank">
-            📣 NRC-20 Launchpad is Ready!!!
-          </NRC20Link>
-          <NotificationImg />
-        </FormContainer>
-      </Notification>
-    )}
     <HeaderContainer>
-      <a href={landingUrl} target="_blank">
-        <Logo
-          src={`${ipfsPrefix}/bafkreic65adpnynb7dthyyfjkfxgteij3qq45dmtfcp3knlroyo4nyj4qq`}
-          alt="Logo"
-        />
-      </a>
+      <Logo
+        src={`${ipfsPrefix}/bafkreic65adpnynb7dthyyfjkfxgteij3qq45dmtfcp3knlroyo4nyj4qq`}
+        alt="Logo"
+      />
       <TabContainer>
         <TabItem
           selected={tab === "transfer"}
           href={`/${config.ownerId}/widget/NEAT?tab=transfer`}
         >
           Transfer
-        </TabItem>
-        <TabItem
-          selected={tab === "stake"}
-          href={`/${config.ownerId}/widget/NEAT?tab=stake`}
-        >
-          Stake
         </TabItem>
         <TabItem
           selected={tab === "wrap"}
@@ -363,21 +230,7 @@ return (
       {tab === "transfer" && (
         <Widget src={`${config.ownerId}/widget/NEAT.Transfer`} />
       )}
-      {tab === "wrap" && (
-        <Widget src={`${config.ownerId}/widget/NEAT.WrapUnwrap`} />
-      )}
-      {tab === "stake" && (
-        <Widget src={`${config.ownerId}/widget/NEAT.StakeUnstake`} />
-      )}
+      {tab === "wrap" && <Widget src={`${config.ownerId}/widget/NEAT.Wrap`} />}
     </BodyContainer>
-    <FooterWrapper>
-      <NeatCommonLink href={nrc20DocHost} target="_blank">
-        NRC-20 Standard
-      </NeatCommonLink>
-      |
-      <NeatCommonLink href={partnerProgramUrl} target="_blank">
-        Partner Program
-      </NeatCommonLink>
-    </FooterWrapper>
   </Main>
 );
