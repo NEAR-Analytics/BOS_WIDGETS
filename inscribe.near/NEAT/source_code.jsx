17205a5f308a46367b70295e8229c8f39eea61ee
inscribe.near <inscribe.near>
2023-12-14 10:04:56 +0000

    Update NEAT by inscribe.near at 2023-12-14T10:04:56.210Z

--- inscribe.near/NEAT/source_code.jsx
+++ inscribe.near/NEAT/source_code.jsx
@@ -143,10 +143,6 @@ const TabContainer = styled.div`
   justify-content: center;
   align-items: center;
   gap: 56px;
-
-  @media (max-width: 768px) {
-    gap: 20px;
-  }
 `;
 
 const TabItem = styled("Link")`
@@ -159,9 +155,6 @@ const TabItem = styled("Link")`
     color: white;
     text-decoration: none;
     opacity: 0.8;
-  }
-  @media (max-width: 768px) {
-    font-size: 16px;
   }
 `;
 
