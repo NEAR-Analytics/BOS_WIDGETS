cef6e4031db83f4455b6deaf230b2442b08310c1
lord1.near <lord1.near>
2023-12-14 14:03:40 +0000

    Update table-pagination by lord1.near at 2023-12-14T14:03:40.479Z

--- lord1.near/table-pagination/source_code.jsx
+++ lord1.near/table-pagination/source_code.jsx
@@ -63,6 +63,12 @@ const formatNumber = (num) => {
   if (num >= 1000) {
     return (num / 1000).toFixed(1).replace(/\.0$/, "") + "k";
   }
+  if (num < 1 && num > 0) {
+    return (num * 1000).toFixed(1).replace(/\.0$/, "") + "m";
+  }
+  if (num < 1 && num > 0) {
+    return (num * 1000000).toFixed(1).replace(/\.0$/, "") + "* 10^-6";
+  }
   return num;
 };
 return (
