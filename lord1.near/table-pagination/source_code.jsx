7a57fb6896047a2a6de272dc9fd04a5768b64851
lord1.near <lord1.near>
2023-12-14 13:42:44 +0000

    Update table-pagination by lord1.near at 2023-12-14T13:42:44.265Z

--- lord1.near/table-pagination/source_code.jsx
+++ lord1.near/table-pagination/source_code.jsx
@@ -53,7 +53,18 @@ const onHandelId = (id) => {
     return id;
   }
 };
-
+const formatNumber = (num) => {
+  if (num >= 1000000000) {
+    return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "b";
+  }
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "m";
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "k";
+  }
+  return num;
+};
 return (
   <div className="table-responsive">
     <div
@@ -190,6 +201,8 @@ return (
                                 />
                               </svg>
                             </OverlayTrigger>
+                          ) : td.round ? (
+                            formatNumber(key)
                           ) : td.short ? (
                             onHandelId(key)
                           ) : td.pic ? (
