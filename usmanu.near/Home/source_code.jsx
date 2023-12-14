bde2f5d9b0eca3fafa16c84f5f467062c0c13118
usmanu.near <usmanu.near>
2023-12-14 06:34:51 +0000

    Update Home by usmanu.near at 2023-12-14T06:34:51.126Z

---
+++ usmanu.near/Home/source_code.jsx
@@ -0,0 +1,19 @@
+const Wrapper = styled.div`
+Width: 100%;
+`;
+
+const Container = styled.div`
+background-color: #4caf50;
+`;
+return (
+  <Wrapper>
+    <Widget src="abubakarimam.near/widget/Header" />
+    <Container>
+      <Widget src="abubakarimam.near/widget/Hero" />
+      <Widget src="abubakarimam.near/widget/IntroSection" />
+      <Widget src="abubakarimam.near/widget/HowITWorkSection" />
+      <Widget src="abubakarimam.near/widget/BlockchainComponent" />
+      <Widget src="abubakarimam.near/widget/Footer" />
+    </Container>
+  </Wrapper>
+);
