16e0c61baffc66e1a861eba289aaa32adde603f3
lord1.near <lord1.near>
2023-12-14 20:57:32 +0000

    Update wrapped-tekuno by lord1.near at 2023-12-14T20:57:32.755Z

--- lord1.near/wrapped-tekuno/source_code.jsx
+++ lord1.near/wrapped-tekuno/source_code.jsx
@@ -6,9 +6,7 @@ const operationsDoc = `
   query MyQuery {  
     mb_views_nft_tokens(
       order_by: {last_transfer_timestamp: desc}
-      where: {owner: {_eq: "${
-        props.wallet_id || context.accountId
-      }"}, _and: {nft_contract_name: { _eq: "Tekuno" },burned_timestamp: {_is_null: true}, last_transfer_timestamp: {}}}
+      where: {owner: {_eq: "${state.searchedAccountId}"}, _and: {nft_contract_name: { _eq: "Tekuno" },burned_timestamp: {_is_null: true}, last_transfer_timestamp: {}}}
     ) {
       nft_contract_id
       title
@@ -118,6 +116,7 @@ const fetchData = () => {
     error: (data && !data.ok && (data.status || data.error)) || null,
     isLoading: !data && !error,
   };
+  console.log(result);
   return result;
 };
 
@@ -137,7 +136,9 @@ if (state.error.length > 0) {
 }
 
 //-------------------------------------------------------------------------------------
-const countByNftContractName = state.result.data.reduce((acc, item) => {
+const dataForReduce = state.result.data || [];
+
+const countByNftContractName = dataForReduce.reduce((acc, item) => {
   acc[item.title] = (acc[item.title] || 0) + 1;
   return acc;
 }, {});
@@ -149,7 +150,7 @@ const dataForChart = Object.entries(countByNftContractName).map(
   })
 );
 
-const countByNftContractName1 = state.result.data.reduce((acc, item) => {
+const countByNftContractName1 = dataForReduce.reduce((acc, item) => {
   acc[item.nft_contract_id] = (acc[item.nft_contract_id] || 0) + 1;
   return acc;
 }, {});
@@ -236,12 +237,10 @@ console.log(result);
 //-------------------------------------------------------------------------------------
 return (
   <>
-    {" "}
     <div className="row">
       <Widget src="lord1.near/widget/header-dynamic" props={general_theme} />
 
       <div className="col-6">
-        {" "}
         <div
           style={{ background: themeColor?.sbt_area?.section_bg }}
           className="shadow-sm rounded-2 overflow-auto p-2"
@@ -263,7 +262,7 @@ return (
                 }
               )}
             />
-          )}{" "}
+          )}
         </div>
       </div>
 
@@ -289,7 +288,7 @@ return (
                 }
               )}
             />
-          )}{" "}
+          )}
         </div>
       </div>
     </div>
@@ -298,7 +297,6 @@ return (
         style={{ background: themeColor?.sbt_area?.section_bg }}
         className="shadow-sm rounded-2 overflow-auto p-2"
       >
-        {" "}
         <div
           style={{ background: themeColor?.sbt_area?.card_bg }}
           className="shadow-sm rounded-2 overflow-auto"
@@ -322,7 +320,7 @@ return (
                 rowsCount: 10,
               }}
             />
-          )}{" "}
+          )}
         </div>
       </div>
     </div>
