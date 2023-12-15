9b841e8ef3d624306694252da49c4211bef41d8d
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-15 06:48:07 +0000

    Update test-supercall by relayer.pagodaplatform.near at 2023-12-15T06:48:07.617Z

--- relayer.pagodaplatform.near/test-supercall/source_code.jsx
+++ relayer.pagodaplatform.near/test-supercall/source_code.jsx
@@ -1,9 +1,21 @@
 const handleClick = async () => {
   setLoading(true);
-  const callArray = convertEdgeNodeToArray(nodeEdges, nodeData);
-  console.log({ callArray });
-  const axlCall = await axlCallMapping(callArray, userContract);
-  console.log({ axlCall });
+  // const callArray = convertEdgeNodeToArray(nodeEdges, nodeData);
+  // console.log({ callArray });
+  // const axlCall = await axlCallMapping(callArray, userContract);
+  // console.log({ axlCall });
+
+  const axlCall = [
+    {
+      axlSuperCall: "0x7F83B301e5Faa1d98bD9D4607932dC40D32f4b25",
+      callData:
+        "0x41304fac000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000047465737400000000000000000000000000000000000000000000000000000000",
+      chain: "ethereum-2",
+      fee: "355180580507613496",
+      subCalls: [],
+      target: "",
+    },
+  ];
   const axlCallEncoded = axlCall.map((call) => call.encode());
   const totalFee = axlCall
     .reduce((acc, cur) => acc + +cur.calculateTotalFee(), 0)
@@ -22,8 +34,61 @@ const handleClick = async () => {
   setLoading(false);
 };
 
+const [first, setfirst] = useState("");
+const [payload, setPayload] = useState({
+  contractChain: "",
+  contractName: "",
+  contractFunction: "",
+  args: "",
+});
+useEffect(() => {
+  setfirst("hello");
+}, []);
+
 return (
   <div>
-    <input required placeholder="Contract Address" />
+    <Web3Connect className="bg-white hover:text-red-500" />
+    <input
+      required
+      placeholder="Contract chain"
+      onChange={(e) => {
+        setPayload((prevInsurance) => ({
+          ...prevInsurance,
+          contractChain: e.target.value,
+        }));
+      }}
+    />
+    <input
+      required
+      placeholder="Contract name"
+      onChange={(e) => {
+        setPayload((prevInsurance) => ({
+          ...prevInsurance,
+          contractName: e.target.value,
+        }));
+      }}
+    />
+    <input
+      required
+      placeholder="Contract function"
+      onChange={(e) => {
+        setPayload((prevInsurance) => ({
+          ...prevInsurance,
+          contractFunction: e.target.value,
+        }));
+      }}
+    />
+    <input
+      required
+      placeholder="param"
+      onChange={(e) => {
+        setPayload((prevInsurance) => ({
+          ...prevInsurance,
+          args: e.target.value,
+        }));
+      }}
+    />
+    {first}
+    <button onClick={handleClick}>Start</button>
   </div>
 );
