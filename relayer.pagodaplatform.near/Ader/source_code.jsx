7ae54a3873bd06406565cdc99109fed941783845
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-15 06:34:08 +0000

    Update Ader by relayer.pagodaplatform.near at 2023-12-15T06:34:08.235Z

--- relayer.pagodaplatform.near/Ader/source_code.jsx
+++ relayer.pagodaplatform.near/Ader/source_code.jsx
@@ -1,27 +1,40 @@
+const children = props.children;
+
 const tailwindCssUrl =
   "https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css";
 
 State.init({
   theme: null,
+  walletAddress: null,
 });
 
 const tailwindCss = fetch(tailwindCssUrl).body;
 
-if (!tailwindCss) return "Can't load CSS 😔.";
+if (!tailwindCss) return "tailwind css not load";
 
 if (!state.theme) {
   State.update({
     theme: styled.div`
-    ${tailwindCss}`,
+      ${tailwindCss}
+    `,
   });
 }
 
-const Tailwind = state.theme;
+State.update({
+  walletAddress: Ethers.send("eth_requestAccounts", [])[0],
+});
+
+const Theme = state.theme;
 
 return (
-  <Tailwind>
-    <div class="bg-red-400">
-      <p>A child dependency:</p>
+  <Theme>
+    <div className="flex">
+      <div className="p-2">
+        {state.walletAddress}
+        <div className="">
+          <Web3Connect className="bg-white hover:text-red-500" />
+        </div>
+      </div>
     </div>
-  </Tailwind>
+  </Theme>
 );
