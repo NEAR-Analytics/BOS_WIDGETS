c05ca3af04ac1848bef878949e32556d1a3afc59
keypom-marketplace.near <keypom-marketplace.near>
2023-12-15 17:05:25 +0000

    Update Project.FollowButton by keypom-marketplace.near at 2023-12-15T17:05:25.420Z

--- keypom-marketplace.near/Project.FollowButton/source_code.jsx
+++ keypom-marketplace.near/Project.FollowButton/source_code.jsx
@@ -1,4 +1,4 @@
-const ownerId = "keypom-marketplace.near";
+const ownerId = "keypom-marketplace.testnet";
 
 if (!props.accountId || !context.accountId || context.accountId === props.accountId) {
   return "";
