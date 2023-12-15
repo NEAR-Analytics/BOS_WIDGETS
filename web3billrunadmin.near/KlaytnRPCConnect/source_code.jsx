a8c1796974d83431c4b5e592188c671f650b1077
web3billrunadmin.near <web3billrunadmin.near>
2023-12-15 18:22:31 +0000

    Update KlaytnRPCConnect by web3billrunadmin.near at 2023-12-15T18:22:31.091Z

--- web3billrunadmin.near/KlaytnRPCConnect/source_code.jsx
+++ web3billrunadmin.near/KlaytnRPCConnect/source_code.jsx
@@ -1,1 +1,21 @@
-return <p>Klaytn RPC Connect</p>;
+State.init({
+  chainId: 0,
+});
+
+if (Ethers.provider()) {
+  Ethers.provider()
+    .getNetwork()
+    .then((chainIdData) => {
+      if (chainIdData?.chainId) {
+        State.update({ chainId: chainIdData.chainId });
+      }
+    });
+} else {
+  return (
+    <>
+      <Web3Connect connectLabel="Connect with Web3" />
+    </>
+  );
+}
+
+return <>ChainID: {state.chainId}</>;
