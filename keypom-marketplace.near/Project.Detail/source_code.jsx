1af25a451fb98d386cb168912a2ca2e993f33b03
keypom-marketplace.near <keypom-marketplace.near>
2023-12-15 16:52:42 +0000

    Update Project.Detail by keypom-marketplace.near at 2023-12-15T16:52:42.579Z

--- keypom-marketplace.near/Project.Detail/source_code.jsx
+++ keypom-marketplace.near/Project.Detail/source_code.jsx
@@ -1,4 +1,4 @@
-const ownerId = "a_liutiev.near";
+const ownerId = "keypom-marketplace.near";
 const registryId = "registry.potlock.near";
 
 const profile = props.profile ?? Social.getr(`${props.projectId}/profile`);
