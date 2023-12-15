b50350948c1fa2b7d37be54f82acd73a4e7e741f
inscribe.near <inscribe.near>
2023-12-15 13:26:42 +0000

    Update NEAT.IndexTable by inscribe.near at 2023-12-15T13:26:42.008Z

--- inscribe.near/NEAT.IndexTable/source_code.jsx
+++ inscribe.near/NEAT.IndexTable/source_code.jsx
@@ -242,6 +242,17 @@ function getWrappedFtBalance() {
   });
 }
 
+function getNrc20TotalSupply() {
+  if (!state.nep141TotalSupply || !state.tokenInfo?.maxSupply) return undefined;
+  return Big(state.tokenInfo.maxSupply)
+    .minus(state.nep141TotalSupply)
+    .toFixed();
+}
+
+function getNep141TotalSupply() {
+  return Near.asyncView(config.ftWrapper, "ft_total_supply");
+}
+
 function fetchAllData() {
   const response = fetchFromGraph(`
     query {
@@ -314,6 +325,19 @@ function fetchAllData() {
       wrappedFtBalance: balance,
     })
   );
+
+  getNep141TotalSupply().then((nep141TotalSupply) => {
+    State.update({
+      nep141TotalSupply,
+    });
+  });
+
+  const nrc20TotalSupply = getNrc20TotalSupply();
+  if (nrc20TotalSupply) {
+    State.update({
+      nrc20TotalSupply,
+    });
+  }
 }
 
 fetchAllData();
