56c935983198bd3004e037fd10a403bdbe7ffbd5
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-16 18:19:12 +0000

    Update billbos-reward-card by relayer.pagodaplatform.near at 2023-12-16T18:19:12.619Z

--- relayer.pagodaplatform.near/billbos-reward-card/source_code.jsx
+++ relayer.pagodaplatform.near/billbos-reward-card/source_code.jsx
@@ -5,15 +5,18 @@ const main = (
     className=" bg-red-200 rounded-2xl"
     style={{ height: "406px", width: "302px" }}
   >
-    <div className="flex justify-end ">
+    <div className="flex justify-end flex-wrap">
       <div className="bg-black text-white w-32 rounded-tr-2xl rounded-bl-2xl h-11 flex justify-center items-center font-bold">
         <p className="">{chainName || "Test Chain"}</p>
       </div>
-      <div>
+      <div className="w-full flex justify-center my-10">
         <img
           src="https://ipfs.near.social/ipfs/bafkreif5bafppbk7gtxgghyuetssfobb4z2czhuhh25ywxufdh6b6ngxf4"
           alt={tokenName || "USDT"}
         />
+      </div>
+      <div className="w-full flex justify-center">
+        <p>{tokenName || "USDT"} Earned</p>
       </div>
     </div>
   </div>
