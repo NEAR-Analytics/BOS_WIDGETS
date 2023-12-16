4ae6dc94d9ff869fb237183ac9fc66ecff77e4a7
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-16 18:03:35 +0000

    Update billbos-reward-card by relayer.pagodaplatform.near at 2023-12-16T18:03:35.215Z

--- relayer.pagodaplatform.near/billbos-reward-card/source_code.jsx
+++ relayer.pagodaplatform.near/billbos-reward-card/source_code.jsx
@@ -1,5 +1,12 @@
-const mian = <div>gfg</div>;
+const main = (
+  <div className="w-80 bg-red-200">
+    <div>test</div>
+  </div>
+);
 
 return (
-  <Widget src="porx-dev.near/widget/billbos-css" props={{ children: main }} />
+  <>
+    <Widget src="porx-dev.near/widget/billbos-css" props={{ children: main }} />
+    asdasd
+  </>
 );
