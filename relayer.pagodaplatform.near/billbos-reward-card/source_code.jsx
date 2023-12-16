dfb27bf65b3ca4e7a25dcdca88652c66d6093412
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-16 18:27:34 +0000

    Update billbos-reward-card by relayer.pagodaplatform.near at 2023-12-16T18:27:34.580Z

--- relayer.pagodaplatform.near/billbos-reward-card/source_code.jsx
+++ relayer.pagodaplatform.near/billbos-reward-card/source_code.jsx
@@ -5,19 +5,24 @@ const main = (
     className=" bg-red-200 rounded-2xl"
     style={{ height: "406px", width: "302px" }}
   >
-    <div className="flex justify-end flex-wrap">
-      <div className="bg-black text-white w-32 rounded-tr-2xl rounded-bl-2xl h-11 flex justify-center items-center font-bold">
-        <p className="">{chainName || "Test Chain"}</p>
+    <div className="flex justify-center flex-wrap">
+      <div className="w-full flex justify-end flex-wrap">
+        <div className="bg-black text-white w-32 rounded-tr-2xl rounded-bl-2xl h-11 flex justify-center items-center font-bold">
+          <p className="">{chainName || "Test Chain"}</p>
+        </div>
       </div>
-      <div className="w-full flex justify-center my-10">
+      <div className=" my-10">
         <img
           src="https://ipfs.near.social/ipfs/bafkreif5bafppbk7gtxgghyuetssfobb4z2czhuhh25ywxufdh6b6ngxf4"
           alt={tokenName || "USDT"}
         />
       </div>
       <div className="w-full flex justify-center">
-        <p>{tokenName || "USDT"} Earned</p>
+        <p className="text-xs">{tokenName || "USDT"} Earned</p>
       </div>
+      <p className=" font-bold">
+        {amount || "1250.13"} <span>{tokenName || "USDT"}</span>
+      </p>
     </div>
   </div>
 );
