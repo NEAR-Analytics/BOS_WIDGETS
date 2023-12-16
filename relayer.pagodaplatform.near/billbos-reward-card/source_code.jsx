951de6204866d87e6c9274c8e01d247ba43cd044
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-16 18:10:36 +0000

    Update billbos-reward-card by relayer.pagodaplatform.near at 2023-12-16T18:10:36.650Z

--- relayer.pagodaplatform.near/billbos-reward-card/source_code.jsx
+++ relayer.pagodaplatform.near/billbos-reward-card/source_code.jsx
@@ -1,6 +1,15 @@
+const { tokenImage, chainName, amount } = props;
+
 const main = (
-  <div className=" bg-red-200" style={{ height: "406px", width: "302px" }}>
-    <div>test</div>
+  <div
+    className=" bg-red-200 rounded-2xl"
+    style={{ height: "406px", width: "302px" }}
+  >
+    <div className="flex justify-end rounded-2xl">
+      <div className="bg-black text-white w-32">
+        <p className="">{chainName || "Test Chain"}</p>
+      </div>
+    </div>
   </div>
 );
 
