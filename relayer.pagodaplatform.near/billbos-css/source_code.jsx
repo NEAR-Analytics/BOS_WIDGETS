6ecd2dc4fca2627ab37668d2d5b730768954c0b6
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-16 08:30:29 +0000

    Update billbos-css by relayer.pagodaplatform.near at 2023-12-16T08:30:29.699Z

--- relayer.pagodaplatform.near/billbos-css/source_code.jsx
+++ relayer.pagodaplatform.near/billbos-css/source_code.jsx
@@ -18,6 +18,10 @@ if (!state.theme) {
         .brand-green {
             background-color:#00E3B4;
         }
+        .brand-gradient-green {
+            background: rgb(0,200,160);
+            background: linear-gradient(0deg, rgba(0,200,160,1) 0%, rgba(0,227,180,1) 63%);
+        }
         .brand-dark-green {
             background-color:#00C8A0;
         }
@@ -44,6 +48,27 @@ if (!state.theme) {
         }
         .gray-serface {
             background-color: #F7F9F9;
+        }
+        .web3-connect {
+            font-size: 12px;
+            font-weight: bold;
+
+            display: grid;
+            place-content: center;
+
+            background: #262626;
+            border-radius: 5px;
+            border: 0;
+
+            color: white;
+            transition: all 300ms ease-in-out;
+            &:hover {
+            background: #262626;
+            opacity: 0.5;
+            }
+            &:active {
+            background: #262626 !important;
+            }
         }
     `,
   });
