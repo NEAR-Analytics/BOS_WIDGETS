808e1f22a1f9f8bb39c4c24931ffde83ab572ec5
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-16 08:50:31 +0000

    Update billbos-header by relayer.pagodaplatform.near at 2023-12-16T08:50:31.756Z

--- relayer.pagodaplatform.near/billbos-header/source_code.jsx
+++ relayer.pagodaplatform.near/billbos-header/source_code.jsx
@@ -1,13 +1,48 @@
+State.init({
+  hideLeanMore: false,
+});
+
+const hideLeanMore = () => {
+  State.update({
+    hideLeanMore: true,
+  });
+};
+
 const leanMore = (
   <div className="brand-gradient-green text-white text-xs font-bold h-12 flex items-center justify-center ">
     <div className="flex justify-center gap-2 items-center">
       <p>Unlock the power of onchain data for Web3 Ads</p>
       <p className="bg-white green-text py-1 px-1.5 rounded-md">Lean more</p>
     </div>
+    <div
+      className="absolute top-3 right-6 border-2 boder-white p-1 rounded-md z-10 cursor-pointer"
+      onClick={hideLeanMore}
+    >
+      <svg
+        width="10"
+        height="10"
+        viewBox="0 0 10 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 1L9 9"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+        />
+        <path
+          d="M9 1L1 9"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+        />
+      </svg>
+    </div>
   </div>
 );
 
-const mainHeader = <div className="">{leanMore}</div>;
+const mainHeader = <div className="">{state.hideLeanMore ? "" : leanMore}</div>;
 
 return (
   <Widget
