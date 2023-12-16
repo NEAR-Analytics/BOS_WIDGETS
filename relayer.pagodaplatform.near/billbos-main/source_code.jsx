fd292ba7555f0930feaf0d9f077d34e14d4401fd
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-16 12:15:38 +0000

    Update billbos-main by relayer.pagodaplatform.near at 2023-12-16T12:15:38.723Z

--- relayer.pagodaplatform.near/billbos-main/source_code.jsx
+++ relayer.pagodaplatform.near/billbos-main/source_code.jsx
@@ -1,1 +1,16 @@
-return <div>sad</div>;
+State.init({
+  chainId: null,
+  walletAddress: null,
+  walletConnected: false,
+});
+
+function checkProvider() {
+  const provider = Ethers.provider();
+  if (provider) {
+    State.update({ walletConnected: true });
+  } else {
+    State.update({ walletConnected: false });
+  }
+}
+
+return <div>{JSON.stringify(context)}</div>;
