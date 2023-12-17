4d3ddf285f2b98e5422b944ef4050f5dafbf9df6
fofay-ac.near <fofay-ac.near>
2023-12-17 16:34:39 +0000

    Update SupercallBundler by fofay-ac.near at 2023-12-17T16:34:39.704Z

--- fofay-ac.near/SupercallBundler/source_code.jsx
+++ fofay-ac.near/SupercallBundler/source_code.jsx
@@ -1,3 +1,15 @@
+const StyledAddButton = styled.button`
+  width: 78px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  border-radius: 10px;
+  border: 1px solid #dde1e8;
+  background-color: #fff4;
+  background: var(#fff);
+  box-shadow: 0px 1px 2px 0px rgba(16, 24, 40, 0.05);
+  align-items: center;
+`;
 const startButton = (
   <button
     type="button"
@@ -66,41 +78,104 @@ State.init({
 });
 const onOpenModal = () => {
   State.update({
-    isOpenModal: true,
+    isOpen: true,
+  });
+};
+const onCloseModal = () => {
+  State.update({
+    isOpen: false,
   });
 };
 
 return (
   <div>
-    <div>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       <div onClick={() => onOpenModal()}>{startButton}</div>
       <Widget
         src="fay-tbl.near/widget/ModalSelectFunction"
         props={{
           handdleAddFunction: () => handleAddCall(),
+          onClose: () => onCloseModal(),
           isOpenModal: state.isOpen,
         }}
       />
-      <div>
-        {state.calls.map((call, index) => (
-          <div key={index}>
+    </div>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {state.calls.map((call, index) => (
+        <div key={index}>
+          <div style={{ display: "flex" }}>
             <div
-              className="border-1 border"
-              style={{ height: 20, width: 1, marginLeft: 20 }}
-            ></div>
-            <div className="border-1 border p-2">
-              <div>
-                <Widget
-                  src={call}
-                  props={{
-                    callId: index.toString(),
-                  }}
-                />
-              </div>
+              style={{
+                height: 20,
+                width: 1,
+                border: "1px dashed #FF94A0",
+                margin: "auto",
+              }}
+            />
+          </div>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                margin: "auto",
+              }}
+            >
+              <Widget src="nt-sorravit.near/widget/SupercallBox" />
             </div>
           </div>
-        ))}
-      </div>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                height: 20,
+                width: 1,
+                border: "1px dashed #FF94A0",
+                margin: "auto",
+              }}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      {state.calls.length > 0 && (
+        <StyledAddButton onClick={() => onOpenModal()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+          >
+            <path
+              d="M9 6.75V11.25M11.25 9H6.75M15.75 9C15.75 9.88642 15.5754 10.7642 15.2362 11.5831C14.897 12.4021 14.3998 13.1462 13.773 13.773C13.1462 14.3998 12.4021 14.897 11.5831 15.2362C10.7642 15.5754 9.88642 15.75 9 15.75C8.11358 15.75 7.23583 15.5754 6.41689 15.2362C5.59794 14.897 4.85382 14.3998 4.22703 13.773C3.60023 13.1462 3.10303 12.4021 2.76381 11.5831C2.42459 10.7642 2.25 9.88642 2.25 9C2.25 7.20979 2.96116 5.4929 4.22703 4.22703C5.4929 2.96116 7.20979 2.25 9 2.25C10.7902 2.25 12.5071 2.96116 13.773 4.22703C15.0388 5.4929 15.75 7.20979 15.75 9Z"
+              stroke="#FF0420"
+              stroke-width="1.2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <span style={{ fontSize: "14px", fontWeight: 600, marginLeft: 1 }}>
+            Add
+          </span>
+        </StyledAddButton>
+      )}
     </div>
   </div>
 );
