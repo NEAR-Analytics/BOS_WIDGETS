d7daa58741d3ecf4e105d086e5e6ca3e1562651c
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-17 20:45:09 +0000

    Update DonationHub.components.island.banner by relayer.pagodaplatform.near at 2023-12-17T20:45:09.631Z

--- relayer.pagodaplatform.near/DonationHub.components.island.banner/source_code.jsx
+++ relayer.pagodaplatform.near/DonationHub.components.island.banner/source_code.jsx
@@ -1,38 +1,21 @@
 const imageLink =
-  "https://www.cpacanada.ca/-/media/cpa-digital-hub/featured-images/2019/12/hub_12_18_charity-hero-1200x900.jpg?rev=af800c90a32a4bac8e7c01400388d10c";
+  "https://i.pinimg.com/564x/69/cb/db/69cbdb5337d07961ff56b77820fbc932.jpg";
 
 const HeroSection = styled.div`
   position: relative;
-  height: auto;
-  z-index: 3;
-  width: 60%;
-  background: 
-    linear-gradient(45deg,#000000,#2D871F,#3CA52B,#ffffff);
-  clip-path: polygon(0 0, 100% 0%, 75% 100%, 0% 100%);
+  height: 380px;
+  z-index: 0;
+  width: 500px;
 
-  padding-top: 2rem;
-  padding-bottom: 2rem;
-  padding-left: 3.375rem;
+
+  padding-top: 0rem;
+  padding-bottom: 0rem;
+  
 
   @media screen and (max-width: 768px) {
     width: 100%;
-    padding: 1rem 1.5rem;
+  
     clip-path: none;
-  }
-`;
-
-const Title = styled.h1`
-  color: #f4f4f4;
-  font-size: 4rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 100%; /* 88px */
-  letter-spacing: -1.76px;
-
-  @media screen and (max-width: 768px) {
-    font-size: 2.25rem;
-    letter-spacing: -0.72px;
-    margin: 0;
   }
 `;
 
@@ -48,12 +31,12 @@ const Container = styled.div`
 `;
 
 const ImageContainer = styled.div`
-  width: 90%;
+  width: 100%;
   height: 100%;
   position: absolute;
   top: 0;
   right: 0;
-  z-index: 1;
+  z-index: 0;
   background: transparent;
 
   @media screen and (max-width: 768px) {
@@ -62,7 +45,7 @@ const ImageContainer = styled.div`
 `;
 
 const Image = styled.img`
-  margin-left: 15.625rem;
+  margin-left: 0rem;
   height: 100%;
   width: 100%;
   filter: grayscale(50%);
@@ -86,13 +69,7 @@ const MobileImage = styled.img`
 
 return (
   <Container>
-    <HeroSection>
-      <Title>
-        The Decentralized <br />
-        <span style={{ color: "#ffffff" }}>Donation</span> <br />
-        Hub
-      </Title>
-    </HeroSection>
+    <HeroSection></HeroSection>
     <MobileImage src={imageLink} />
     <ImageContainer>
       <Image src={imageLink} />
