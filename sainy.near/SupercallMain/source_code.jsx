cebeb7c815d20727e27bafaa2f232718e8af7ffe
sainy.near <sainy.near>
2023-12-17 10:21:32 +0000

    Update SupercallMain by sainy.near at 2023-12-17T10:21:32.099Z

--- sainy.near/SupercallMain/source_code.jsx
+++ sainy.near/SupercallMain/source_code.jsx
@@ -1,51 +1,17 @@
-function handleAddCall() {
-  State.update({ calls: [...state.calls, ""] });
-}
-
-function handleChangeCall(index, value) {
-  State.update({
-    calls: state.calls.map((call, idx) => (index === idx ? value : call)),
-  });
-}
-
-State.init({
-  calls: [],
-});
-
-console.log(state.calls);
+function processCall() {}
 
 return (
   <div>
-    <div>Call</div>
     <div>
-      <div className="p-2 rounded border border-1">Start</div>
-      <div>
-        {state.calls.map((call, index) => (
-          <div key={index}>
-            <div
-              className="border-1 border"
-              style={{ height: 20, width: 1, marginLeft: 20 }}
-            ></div>
-            <div className="border-1 border p-2">
-              <div>
-                <input
-                  placeholder="supercall widget"
-                  onChange={(e) => handleChangeCall(index, e.target.value)}
-                />
-              </div>
-              <div>
-                <Widget src={call} />
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div>
-        <button onClick={handleAddCall}>+</button>
-      </div>
-      <div>
-        <button>Call</button>
-      </div>
+      <h2>Accounts</h2>
+      <Widget src="sainy.near/widget/SupercallAccount" />
+    </div>
+    <div>
+      <h2>Calls</h2>
+      <Widget src="sainy.near/widget/SupercallBundler" />
+    </div>
+    <div>
+      <button onClick={processCall}>Call</button>
     </div>
   </div>
 );
