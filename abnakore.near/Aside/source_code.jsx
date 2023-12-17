0f8b3009beeeac48790cec19e9b0683380340182
abnakore.near <abnakore.near>
2023-12-17 01:23:46 +0000

    Update Aside by abnakore.near at 2023-12-17T01:23:46.496Z

---
+++ abnakore.near/Aside/source_code.jsx
@@ -0,0 +1,56 @@
+const Wrapper = styled.div`
+
+.aside {
+  position: fixed;
+  background-color: #333;
+  padding: 20px;
+  height: 50px;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
+.aside #tabs {
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+}
+.aside #tabs .tab {
+  padding: 10px;
+  font-size: 16px;
+  color: #fff;
+  text-align: center;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+.aside #tabs .tab:hover, .aside #tabs .active {
+  background-color: #555;
+}
+
+`;
+
+return (
+  <Wrapper>
+    <div className="aside">
+      <div id="tabs">
+        {/* Render the objs as links */}
+        {props.objs.map((obj) => (
+          <a
+            key={obj.link}
+            href={obj.link}
+            className={`tab ${active === obj.link ? "active" : ""}`}
+          >
+            {obj.name}
+          </a>
+        ))}
+        {/* <Link to='/' className="tab">Vote Page</Link>
+                    <Link to='/result' className="tab">Results</Link>
+                    <Link to='/' className="tab">Login/Logout</Link> */}
+      </div>
+    </div>
+  </Wrapper>
+);
