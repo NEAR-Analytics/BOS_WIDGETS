309faaab1c5dc3eacb29e9cd30dbeffb250e8629
sainy.near <sainy.near>
2023-12-17 11:00:00 +0000

    Update SupercallMain by sainy.near at 2023-12-17T11:00:00.538Z

--- sainy.near/SupercallMain/source_code.jsx
+++ sainy.near/SupercallMain/source_code.jsx
@@ -1,4 +1,29 @@
-function processCall() {}
+const MULTICALL_ABI_URL =
+  "https://gist.githubusercontent.com/SainyTK/8b83bbd26610ceeaa215a46f942d2702/raw/9bc41907fd49bc56553cf226c3ac92b225343cae/multicall-abi.json";
+const MULTICALL_ABI = fetch(MULTICALL_ABI_URL).body;
+
+function readAddress(accountNumber) {
+  const addr = Storage.get(
+    `contractAddress:${accountNumber}`,
+    "sainy.near/widget/SupercallDeployer"
+  );
+  return addr;
+}
+
+function getCalls() {
+  return Storage.get(`calls`, "sainy.near/widget/SupercallBundler");
+}
+
+function processCall() {
+  const multicallAddr = readAddress("0");
+  const multicall = new ethers.Contract(
+    multicallAddr,
+    MULTICALL_ABI,
+    Ethers.provider().getSigner()
+  );
+  const calls = getCalls();
+  multicall.aggregate(calls);
+}
 
 return (
   <div>
