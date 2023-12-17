0a36ca000976d8367649ad2e0377aedf339e82f2
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-17 06:55:47 +0000

    Update billbos-earning-card by relayer.pagodaplatform.near at 2023-12-17T06:55:47.377Z

--- relayer.pagodaplatform.near/billbos-earning-card/source_code.jsx
+++ relayer.pagodaplatform.near/billbos-earning-card/source_code.jsx
@@ -1,27 +1,28 @@
-const { usdt, thb, ipfsUrl } = props;
+const { usdt, thb, ipfsUrl, title } = props;
 
 State.init({
-  amountUSDT: usdt || "10023.34",
-  amountTHB: thb || "350023.34",
+  title: title ?? "Total Staked",
+  amountUSDT: usdt ?? "10023.34",
+  amountTHB: thb ?? "350023.34",
   imgIpfs:
     ipfsUrl ??
     "https://ipfs.near.social/ipfs/bafkreigb2h33nc6bpa2gcuvbs46eys4bbb6djibx76xknbrkswtqfqlyzm",
 });
 
 const content = (
-  <div className="w-full bg-white relative">
-    <div className="absolute top-2 right-2">
+  <div className="w-full relative bg-white shadow-md rounded-xl p-3">
+    <div className="absolute top-4 right-4">
       <img src={state.imgIpfs} alt="logo" />
     </div>
     <div>
-      <div>
-        <p></p>
+      <div className="text-sm secondary-text">
+        <p>{state.title}</p>
       </div>
-      <div>
-        <p></p>
+      <div className="text-2xl font-semibold mt-1">
+        <p>{state.amountUSDT} USDT</p>
       </div>
-      <div>
-        <p></p>
+      <div className="text-xs tertiary-text mt-3">
+        <p>฿{state.amountTHB}</p>
       </div>
     </div>
   </div>
