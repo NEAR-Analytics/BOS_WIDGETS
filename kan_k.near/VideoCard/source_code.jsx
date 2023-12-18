c7442e7dcec67655fe3e5507e24010be037c4285
kan_k.near <kan_k.near>
2023-12-18 07:13:07 +0000

    Update VideoCard by kan_k.near at 2023-12-18T07:13:07.649Z

--- kan_k.near/VideoCard/source_code.jsx
+++ kan_k.near/VideoCard/source_code.jsx
@@ -8,8 +8,8 @@ const video = props.video || {
   thumbnail:
     "https://ipfs.near.social/ipfs/bafkreielwwffvkzqquv4gt7n35m47dht53v4sowvy3hyukj4arv4oaymni",
 };
-const onwerProfile = Social.getr(`${video.owner}/profile`);
-
+const ownerProfile = Social.getr(`${video.owner}/profile`);
+console.log(ownerProfile);
 function timeConverter(UNIX_timestamp) {
   var a = new Date(UNIX_timestamp * 1000);
   var months = [
@@ -39,12 +39,27 @@ function timeConverter(UNIX_timestamp) {
 
 return (
   <Link className="button button--primary" href={`/play/${video.id}`}>
-    <div className=" rounded-lg overflow-hidden w-full h-54 flex flex-col object-contain bg-gray-200 hover:bg-gray-500">
-      <img src={video.thumbnail} className="video object-fill w-full h-48" />
-      <p className="text-black w-full pl-2 whitespace-nowrap mt-2">
-        {video.title}
-      </p>
-      <div className="pl-2"> {timeConverter(video.timestamp)}</div>
+    <div className="HStack Lable gap-4 mb-4">
+      <img src={video.thumbnail} className="video object-fill h-48" />
+      <div className="VStack justify-between">
+        <div className="">
+          <p className="Label font-medium text-lg">{video.title}</p>
+          <p className="Grey">4 days ago</p>
+          <div className="HStack items-center gap-2 mt-4">
+            <Widget
+              src="mob.near/widget/Image"
+              props={{
+                image: ownerProfile.image,
+                className: '"h-10 w-10 rounded-full',
+                alt: ownerProfile.name,
+                fallbackUrl:
+                  "https://ipfs.near.social/ipfs/bafkreibiyqabm3kl24gcb2oegb7pmwdi6wwrpui62iwb44l7uomnn3lhbi",
+              }}
+            />
+            <p> {video.owner} </p>
+          </div>
+        </div>
+      </div>
     </div>
   </Link>
 );
