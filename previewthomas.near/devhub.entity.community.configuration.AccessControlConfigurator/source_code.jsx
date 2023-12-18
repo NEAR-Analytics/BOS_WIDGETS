eb7bdc31af3398f9e8cc64a70472e0e37ab2d468
previewthomas.near <previewthomas.near>
2023-12-18 15:49:11 +0000

    Update devhub.entity.community.configuration.AccessControlConfigurator by previewthomas.near at 2023-12-18T15:49:11.785Z

--- previewthomas.near/devhub.entity.community.configuration.AccessControlConfigurator/source_code.jsx
+++ previewthomas.near/devhub.entity.community.configuration.AccessControlConfigurator/source_code.jsx
@@ -101,9 +101,7 @@ return (
       {admins.map((admin, index) => (
         <div className="admins-item" key={index}>
           <Widget
-            src={
-              "previewthomas.near/widget/devhub.components.molecule.ProfileCard"
-            }
+            src={"previewthomas.near/widget/devhub.components.molecule.ProfileCard"}
             props={{
               accountId: admin,
               nearDevGovGigsWidgetsAccountId: "previewthomas.near",
