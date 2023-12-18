4453d1eeb16cf0c60a51376d3f8994a3ca0745ba
0a7c0f9fe7916b5603c7def29e50ae0571df5d750969bffebef1147b585977fd <0a7c0f9fe7916b5603c7def29e50ae0571df5d750969bffebef1147b585977fd>
2023-12-18 06:05:06 +0000

    Update bonnie-core-polygon-test by 0a7c0f9fe7916b5603c7def29e50ae0571df5d750969bffebef1147b585977fd at 2023-12-18T06:05:06.944Z

--- 0a7c0f9fe7916b5603c7def29e50ae0571df5d750969bffebef1147b585977fd/bonnie-core-polygon-test/source_code.jsx
+++ 0a7c0f9fe7916b5603c7def29e50ae0571df5d750969bffebef1147b585977fd/bonnie-core-polygon-test/source_code.jsx
@@ -1,1 +1,191 @@
-return <div>Hello World from Bonnie : Core Polygon</div>;
+const data = fetch(
+  "https://www.bkcscan.com/api?module=account&action=tokenlist&address=${ownerAddress}".replace(
+    "${ownerAddress}",
+    props.ownerAddress
+  ),
+  {
+    method: "GET",
+  }
+);
+
+const frameWidth = 600;
+const frameheight = 600;
+const margin = 16;
+const canvasWidth = frameWidth - margin;
+const canvasHeight = frameheight - margin;
+
+const CanvasContainer = styled.div`
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+  width: ${frameWidth}px;
+  height: ${frameheight}px;
+  background-color: #333333;
+
+  border-radius: 40px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+  max-width: 600px;
+`;
+
+const MainContainer = styled.div`
+  width: 100%;
+  padding-block: 32px;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap:32px;
+`;
+
+const ContentContainer = styled.div`
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 16px;
+`;
+
+const code2 = `
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r126/three.min.js"></script>
+<script src="https://unpkg.com/three@0.126.0/examples/js/loaders/GLTFLoader.js"></script>
+<body>
+  <script>
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    const containerWidth = ${canvasWidth}; // Set your desired width
+    const containerHeight = ${canvasHeight}; // Set your desired height
+    container.style.width = containerWidth + 'px';
+    container.style.height = containerHeight + 'px';
+        
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+
+    const renderer = new THREE.WebGLRenderer({alpha: true});
+
+    const canvasWidth = ${canvasWidth};
+    const canvasHeight = ${canvasHeight};
+    renderer.setSize(canvasWidth, canvasHeight);
+    renderer.setClearColor(0x000000, 0);
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', () => {
+      const newWidth = ${canvasWidth};
+      const newHeight = ${canvasHeight};
+
+      camera.aspect = newWidth / newHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(newWidth, newHeight);
+    });
+
+    const planeGeometry = new THREE.PlaneGeometry(10, 10);
+    const planeMaterial = new THREE.ShadowMaterial({ opacity: 0.4 });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.position.y = -2.6;
+    plane.rotation.x = -Math.PI / 2;
+    scene.add(plane);
+    plane.scale.multiplyScalar(2);
+    plane.receiveShadow = true;
+
+    let loadedGroup = null;
+    const loader = new THREE.GLTFLoader();
+    loader.load("${props.glbFileURL}", (gltf) => {
+      gltf.scene.traverse(function (child) {
+        if ((child).isMesh) {
+            const m = child
+            m.receiveShadow = true
+            m.castShadow = true                            
+        }
+      })
+                    
+      const model = gltf.scene;
+
+      scene.add(model);
+
+      loadedGroup = model;
+      loadedGroup.scale.multiplyScalar(3);
+      loadedGroup.position.y -= 2.5
+    });
+
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(0, 10, 0);
+    light.castShadow = true;
+
+    scene.add(light);
+
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+    ambientLight.intensity = 2.5;
+    scene.add( ambientLight );
+    renderer.shadowMap.enabled = true;
+		renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    const animate = function () {
+      requestAnimationFrame(animate);
+
+      if(loadedGroup != null) {
+        loadedGroup.rotation.y += 0.01;
+      }
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  </script>
+</body>
+`;
+
+return data !== null ? (
+  <MainContainer>
+    <Header>
+      <h2 style={{ fontWeight: 600 }}>
+        Core Polygon : 3D NFT Proof of concept
+      </h2>
+      <p>Core Polygon : 3D NFT Proof of concept</p>
+    </Header>
+
+    <ContentContainer>
+      {true && (
+        <CanvasContainer>
+          {true && <iframe className="h-100 w-100" srcDoc={code2} />}
+        </CanvasContainer>
+      )}
+      {data.status === 200 ? (
+        data.body.result
+          .filter((checkElem) => checkElem.type === "ERC-721")
+          .map((elem) => {
+            return (
+              <div>
+                <div>{elem.name}</div>
+                <img
+                  style={{
+                    width: 200,
+                    height: 200,
+                    objectFit: "cover",
+                  }}
+                  src={props.nftImageURL}
+                />
+              </div>
+            );
+          })
+      ) : (
+        <div>Fetching Error</div>
+      )}
+    </ContentContainer>
+  </MainContainer>
+) : (
+  <p>loading...</p>
+);
