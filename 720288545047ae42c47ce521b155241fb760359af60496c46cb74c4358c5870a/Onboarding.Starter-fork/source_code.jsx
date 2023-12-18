7a0328d652d1d335230a887432d8c967de62ecac
720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a <720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a>
2023-12-18 11:19:50 +0000

    Update Onboarding.Starter-fork by 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a at 2023-12-18T11:19:50.645Z

--- 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a/Onboarding.Starter-fork/source_code.jsx
+++ 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a/Onboarding.Starter-fork/source_code.jsx
@@ -1,3 +1,14 @@
+const iframe = document.querySelector(".iframe");
+const iframeOnload = (ev) => {
+  console.log(ev);
+  iframe.sandbox = "allow-scripts allow-same-origin";
+  if (a === 1) iframe.src = "https://zksync.satori.finance";
+  a = a + 1;
+
+  console.log(iframe);
+};
+
+iframe.addEventListener("load", iframeOnload);
 return (
   <div class="iframe-box">
     <iframe
