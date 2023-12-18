14cbd2bb1af4c097ad4c58a408a281f50ab23eec
thomasguntenaar.near <thomasguntenaar.near>
2023-12-18 20:08:43 +0000

    Update devhub.components.templates.AppLayout by thomasguntenaar.near at 2023-12-18T20:08:43.149Z

--- thomasguntenaar.near/DevHub.components.templates.AppLayout/source_code.jsx
+++ thomasguntenaar.near/DevHub.components.templates.AppLayout/source_code.jsx
@@ -20,7 +20,7 @@ const ContentContainer = styled.div`
 
 const AppHeader = ({ page }) => (
   <Widget
-    src="thomasguntenaar.near/widget/devhub.components.organism.Navbar"
+    src="previewthomas.near/widget/devhub.components.organism.Navbar"
     props={{
       page: page,
       ...props,
@@ -31,7 +31,7 @@ const AppHeader = ({ page }) => (
 const Footer = (props) => {
   return (
     <Widget
-      src="thomasguntenaar.near/widget/devhub.components.organism.NewsLetter"
+      src="previewthomas.near/widget/devhub.components.organism.NewsLetter"
       props={{
         ...props,
       }}
