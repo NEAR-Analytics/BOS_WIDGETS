a52cb81b8418097786f7926b08faeee9d1043311
49230cfd4ae1a200ab1f32367e7c22d49879a67d3f46198c9afd19ce2df49f78 <49230cfd4ae1a200ab1f32367e7c22d49879a67d3f46198c9afd19ce2df49f78>
2023-12-18 03:14:46 +0000

    Update Omagotji-CharactorRenderer by 49230cfd4ae1a200ab1f32367e7c22d49879a67d3f46198c9afd19ce2df49f78 at 2023-12-18T03:14:46.275Z

---
+++ 49230cfd4ae1a200ab1f32367e7c22d49879a67d3f46198c9afd19ce2df49f78/Omagotji-CharactorRenderer/source_code.jsx
@@ -0,0 +1,9 @@
+const uri = props.uri
+  ? props.uri
+  : "https://coral-logical-booby-193.mypinata.cloud/ipfs/QmWXgU4SYaaXzULjjvUrmWsZZZNr796nSMSrw2KqLq3QDo/larva.gif";
+
+return (
+  <>
+    <img src={uri} />
+  </>
+);
