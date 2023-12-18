c8380fdf0d98901032951ab03b64f9731ead52f0
next-genz.near <next-genz.near>
2023-12-18 16:44:00 +0000

    Update MEWOOFmain by next-genz.near at 2023-12-18T16:44:00.167Z

---
+++ next-genz.near/MEWOOFmain/source_code.jsx
@@ -0,0 +1,22 @@
+const user = "next-genz.near";
+
+return (
+  <>
+    <div class="topnav">
+      <a href="https://bos.gg/next-genz.near/widget/MEWOOFmain" class="active">
+        <h5>Home</h5>
+      </a>
+      <a href="https://bos.gg/next-genz.near/widget/LogIn">
+        <h5>LogIn</h5>
+      </a>
+      <a href="https://bos.gg/next-genz.near/widget/SignIn">
+        <h5>SignIn</h5>
+      </a>
+    </div>
+    <div class="conatiner">
+      <h1>MEWOOF</h1>
+      <p>Welcome to social media for our pets</p>
+      <hr />
+    </div>
+  </>
+);
