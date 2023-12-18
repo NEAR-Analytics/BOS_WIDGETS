f52238eb565d3710f78088fbe3f63402ced6997a
previewthomas.near <previewthomas.near>
2023-12-18 15:49:11 +0000

    Update devhub.entity.post.Post by previewthomas.near at 2023-12-18T15:49:11.785Z

--- previewthomas.near/devhub.entity.post.Post/source_code.jsx
+++ previewthomas.near/devhub.entity.post.Post/source_code.jsx
@@ -29,7 +29,8 @@ const ButtonWithHover = styled.button`
 const postId = props.post.id ?? (props.id ? parseInt(props.id) : 0);
 
 const post =
-  props.post ?? Near.view("devgovgigs.near", "get_post", { post_id: postId });
+  props.post ??
+  Near.view("devgovgigs.near", "get_post", { post_id: postId });
 
 if (!post) {
   return <div>Loading ...</div>;
@@ -785,9 +786,7 @@ const descriptionArea = isUnderPost ? (
       text: snapshot.description,
     })} */}
     <Widget
-      src={
-        "previewthomas.near/widget/devhub.components.molecule.MarkdownViewer"
-      }
+      src={"previewthomas.near/widget/devhub.components.molecule.MarkdownViewer"}
       props={{
         text: snapshot.description,
       }}
@@ -800,9 +799,7 @@ const descriptionArea = isUnderPost ? (
         text: state.clamp ? clampedContent : snapshot.description,
       })} */}
       <Widget
-        src={
-          "previewthomas.near/widget/devhub.components.molecule.MarkdownViewer"
-        }
+        src={"previewthomas.near/widget/devhub.components.molecule.MarkdownViewer"}
         props={{
           text: state.clamp ? clampedContent : snapshot.description,
         }}
