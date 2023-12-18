1011a2750d74f44367e9e7b3f58a70f5fc087122
sainy.near <sainy.near>
2023-12-18 14:41:31 +0000

    Update SupercallTokenTransfer by sainy.near at 2023-12-18T14:41:31.495Z

--- sainy.near/SupercallTokenTransfer/source_code.jsx
+++ sainy.near/SupercallTokenTransfer/source_code.jsx
@@ -1,3 +1,45 @@
+const Container = styled.div`
+  border-radius: 14px;
+  border: 1px solid #e9ebed;
+  width: 328px;
+  background-color: white;
+`;
+
+const ContainerContent = styled.div`
+  padding: 18px 22px;
+`;
+
+const TextHeader = styled.span`
+  color: var(--Text-Primary-Text, #262930);
+  /* p-ui-semibold */
+  font-family: Mona Sans;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px; /* 150% */
+`;
+
+const Divider = styled.div`
+  height: 1px;
+  padding: 0;
+  background: #e5e7eb;
+`;
+
+const StyleTextTitle = styled.span`
+  color: var(--Text-Secondary-Text, #656973);
+
+  /* body */
+  font-family: Mona Sans;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 142.857% */
+`;
+
+const StyledBoxInput = styled.div`
+  margin-bottom: 10px;
+`;
+
 const ERC20_ABI_URL =
   "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json";
 const ERC20_ABI = fetch(ERC20_ABI_URL).body;
@@ -53,25 +95,36 @@ State.init({
 buildCall();
 
 return (
-  <div className="border border-1 rounded rounded-md p-2">
-    <h2>Token Transfer</h2>
-    <div>
-      <p>Token</p>
-      <Widget
-        src="sainy.near/widget/SelectToken"
-        props={{
-          onChange: (token) => console.log(token),
-        }}
-      />
-    </div>
-    <div>
-      <p>To</p>
-      <input onChange={(e) => handleChange("to", e.target.value)} />
-    </div>
-    <div>
-      <p>Amount</p>
-      <input onChange={(e) => handleChange("amount", e.target.value)} />
-    </div>
-    <Widget src="sainy.near/widget/SupercallBase" props={props} />
-  </div>
+  <Container>
+    <ContainerContent>
+      <TextHeader>Transfer</TextHeader>
+    </ContainerContent>
+    <Divider />
+    <ContainerContent>
+      <StyledBoxInput>
+        <StyleTextTitle>Token</StyleTextTitle>
+        <Widget
+          src="sainy.near/widget/SelectToken"
+          props={{
+            onChange: (token) => console.log(token),
+          }}
+        />
+      </StyledBoxInput>
+      <StyledBoxInput>
+        <StyleTextTitle>To</StyleTextTitle>
+        <input
+          placeholder="Wallet Address"
+          onChange={(e) => handleChange("to", e.target.value)}
+        />
+      </StyledBoxInput>
+      <StyledBoxInput>
+        <StyleTextTitle>Amount</StyleTextTitle>
+        <input
+          placeholder="Amount"
+          onChange={(e) => handleChange("amount", e.target.value)}
+        />
+      </StyledBoxInput>
+      <Widget src="sainy.near/widget/SupercallBase" props={props} />
+    </ContainerContent>
+  </Container>
 );
