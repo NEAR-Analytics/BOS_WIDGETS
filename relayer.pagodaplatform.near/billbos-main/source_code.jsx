bf418c48b5c635ae062c1df3ac68947981b5206d
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2023-12-18 04:04:23 +0000

    Update billbos-main by relayer.pagodaplatform.near at 2023-12-18T04:04:23.608Z

--- relayer.pagodaplatform.near/billbos-main/source_code.jsx
+++ relayer.pagodaplatform.near/billbos-main/source_code.jsx
@@ -200,8 +200,13 @@ function tapCampaigns() {
             </div>
           </>
         ) : (
-          <div className="w-full">
-            <Widget src="chayanonc-ph.near/widget/billbos-authen" props={{}} />
+          <div className=" flex justify-center">
+            <div className="w-1/2">
+              <Widget
+                src="chayanonc-ph.near/widget/billbos-authen"
+                props={{}}
+              />
+            </div>
           </div>
         )}
       </div>
@@ -211,7 +216,7 @@ function tapCampaigns() {
 
 function tapRewards() {
   // get view of page owner
-  const walletAddress = "0xFf462851bd130a5F86e4391CBf5c92D99E6416BC";
+  const walletAddress = state.walletAddress;
   const month = 1;
 
   handleRequest(
@@ -237,133 +242,155 @@ function tapRewards() {
           <p className="mt-2">Earn up to 20% commission on every view</p>
         </div>
       </div>
-      <div
-        style={{ marginTop: "-50px" }}
-        className="container rounded-t-3xl gray-surface min-h-screen"
-      >
-        <div className="flex justify-between py-8 items-center ">
-          <div>
-            <h2 className="font-semibold text-xl ">Campaigns</h2>
-            <p className="text-sm">
-              Unlock the power of onchain data for Web3 Ads
-            </p>
-          </div>
-          <div>
-            <Widget
-              src="chayanonc-ph.near/widget/billbos-ads"
-              props={{
-                btnName: "Get my ads component",
-                btnClass:
-                  "brand-green px-2.5 py-2 rounded-xl text-white text-sm font-semibold",
-                height: "467px",
-                width: "550px",
-                isOpenDefault: false,
-                body: (
-                  <div className="flex flex-wrap justify-center w-full ">
-                    <div style={{ width: "482px" }}>
-                      <div>
-                        <p className="font-semibold text-lg">
-                          Get my Ads Component
-                        </p>
-                        <p className="tertiary-text text-sm">
-                          Give your teammates access to this presets and start
-                          collaborating in real time.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="w-full h-px bg-gray-200 my-4 "></div>
-                    <div
-                      style={{ width: "482px", height: "280px" }}
-                      className="text-sm p-3 bg-gray-100 rounded-xl mt-2 relative"
-                    >
-                      <div
-                        className="absolute top-3 right-3 cursor-pointer z-10"
-                        onClick={() => {
-                          copyContent(state.adsContent);
-                        }}
-                      >
-                        <Widget
-                          src="mob.near/widget/CopyButton"
-                          props={{
-                            className: "bg-gray-100",
-                            text: state.adsContent,
-                            clipboardIcon: (
-                              <svg
-                                width="16"
-                                height="16"
-                                viewBox="0 0 16 16"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <g clip-path="url(#clip0_3757_2743)">
-                                  <path
-                                    d="M10.6654 0.666687H2.66536C1.93203 0.666687 1.33203 1.26669 1.33203 2.00002V11.3334H2.66536V2.00002H10.6654V0.666687ZM12.6654 3.33335H5.33203C4.5987 3.33335 3.9987 3.93335 3.9987 4.66669V14C3.9987 14.7334 4.5987 15.3334 5.33203 15.3334H12.6654C13.3987 15.3334 13.9987 14.7334 13.9987 14V4.66669C13.9987 3.93335 13.3987 3.33335 12.6654 3.33335ZM12.6654 14H5.33203V4.66669H12.6654V14Z"
-                                    fill="#C3C5C7"
-                                  />
-                                </g>
-                                <defs>
-                                  <clipPath id="clip0_3757_2743">
-                                    <rect width="16" height="16" fill="white" />
-                                  </clipPath>
-                                </defs>
-                              </svg>
-                            ),
-                          }}
-                        />
-                      </div>
-                      <div className="w-96">{state.adsContent}</div>
-                    </div>
-                  </div>
-                ),
-              }}
-            />
-          </div>
-        </div>
-        <div className="grid grid-cols-3 gap-4 mb-5">
-          <div className="p-3 bg-white rounded-xl ">
+      {state.walletConnected ? (
+        <div
+          style={{ marginTop: "-50px" }}
+          className="container rounded-t-3xl gray-surface min-h-screen"
+        >
+          <div className="flex justify-between py-8 items-center ">
             <div>
-              <p className="text-xs secondary-text">Total Earnings</p>
-              <p className="text-xl mt-1 font-medium">100.20 USDT</p>
-            </div>
-          </div>
-          <div className="p-3 bg-white rounded-xl">
-            <div>
-              <p className="text-xs secondary-text">My Total View</p>
-              <p className="text-xl mt-1 font-medium">
-                {state.viewOfWalletAddress}
+              <h2 className="font-semibold text-xl ">Campaigns</h2>
+              <p className="text-sm">
+                Unlock the power of onchain data for Web3 Ads
               </p>
             </div>
-          </div>
-          <div className="p-3 bg-white rounded-xl">
             <div>
-              <p className="text-xs secondary-text">My Total Earnings</p>
-              <p className="text-xl mt-1 font-medium">
-                {state.ratioOfWalletAddress * state.viewOfWalletAddress} USDT
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="w-full my-4">
-          <p className="text-sm font-medium">Reward</p>
-        </div>
-        <div className="flex gap-3">
-          {CHAIN_LIST.map((item, i) => {
-            const chainConfig = state.chains[item];
-
-            return (
               <Widget
-                src="porx-dev.near/widget/billbos-reward-card"
+                src="chayanonc-ph.near/widget/billbos-ads"
                 props={{
-                  chainName: chainConfig.name,
-                  amount: "1000.20",
-                  tokenName: "USDT",
-                  onClaim: () => {},
+                  btnName: "Get my ads component",
+                  btnClass:
+                    "brand-green px-2.5 py-2 rounded-xl text-white text-sm font-semibold",
+                  height: "467px",
+                  width: "550px",
+                  isOpenDefault: false,
+                  body: (
+                    <div className="flex flex-wrap justify-center w-full ">
+                      <div style={{ width: "482px" }}>
+                        <div>
+                          <p className="font-semibold text-lg">
+                            Get my Ads Component
+                          </p>
+                          <p className="tertiary-text text-sm">
+                            Give your teammates access to this presets and start
+                            collaborating in real time.
+                          </p>
+                        </div>
+                      </div>
+                      <div className="w-full h-px bg-gray-200 my-4 "></div>
+                      <div
+                        style={{ width: "482px", height: "280px" }}
+                        className="text-sm p-3 bg-gray-100 rounded-xl mt-2 relative"
+                      >
+                        <div
+                          className="absolute top-3 right-3 cursor-pointer z-10"
+                          onClick={() => {
+                            copyContent(state.adsContent);
+                          }}
+                        >
+                          <Widget
+                            src="mob.near/widget/CopyButton"
+                            props={{
+                              className: "bg-gray-100",
+                              text: state.adsContent,
+                              clipboardIcon: (
+                                <svg
+                                  width="16"
+                                  height="16"
+                                  viewBox="0 0 16 16"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <g clip-path="url(#clip0_3757_2743)">
+                                    <path
+                                      d="M10.6654 0.666687H2.66536C1.93203 0.666687 1.33203 1.26669 1.33203 2.00002V11.3334H2.66536V2.00002H10.6654V0.666687ZM12.6654 3.33335H5.33203C4.5987 3.33335 3.9987 3.93335 3.9987 4.66669V14C3.9987 14.7334 4.5987 15.3334 5.33203 15.3334H12.6654C13.3987 15.3334 13.9987 14.7334 13.9987 14V4.66669C13.9987 3.93335 13.3987 3.33335 12.6654 3.33335ZM12.6654 14H5.33203V4.66669H12.6654V14Z"
+                                      fill="#C3C5C7"
+                                    />
+                                  </g>
+                                  <defs>
+                                    <clipPath id="clip0_3757_2743">
+                                      <rect
+                                        width="16"
+                                        height="16"
+                                        fill="white"
+                                      />
+                                    </clipPath>
+                                  </defs>
+                                </svg>
+                              ),
+                            }}
+                          />
+                        </div>
+                        <div className="w-96">{state.adsContent}</div>
+                      </div>
+                    </div>
+                  ),
                 }}
               />
-            );
-          })}
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-4 mb-5">
+            <div className="p-3 bg-white rounded-xl ">
+              <div>
+                <p className="text-xs secondary-text">Total Earnings</p>
+                <p className="text-xl mt-1 font-medium">100.20 USDT</p>
+              </div>
+            </div>
+            <div className="p-3 bg-white rounded-xl">
+              <div>
+                <p className="text-xs secondary-text">My Total View</p>
+                <p className="text-xl mt-1 font-medium">
+                  {state.viewOfWalletAddress}
+                </p>
+              </div>
+            </div>
+            <div className="p-3 bg-white rounded-xl">
+              <div>
+                <p className="text-xs secondary-text">My Total Earnings</p>
+                <p className="text-xl mt-1 font-medium">
+                  {state.ratioOfWalletAddress * state.viewOfWalletAddress} USDT
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full my-4">
+            <p className="text-sm font-medium">Reward</p>
+          </div>
+          <div className="flex gap-3">
+            {CHAIN_LIST.map((item, i) => {
+              const chainConfig = state.chains[item];
+
+              return (
+                <Widget
+                  src="porx-dev.near/widget/billbos-reward-card"
+                  props={{
+                    chainName: chainConfig.name,
+                    amount: "1000.20",
+                    tokenName: "USDT",
+                    onClaim: () => {},
+                  }}
+                />
+              );
+            })}
+          </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <div
+            style={{ marginTop: "-50px" }}
+            className="container rounded-t-3xl gray-surface min-h-screen"
+          >
+            <div className=" flex justify-center">
+              <div className="w-1/2">
+                <Widget
+                  src="chayanonc-ph.near/widget/billbos-authen"
+                  props={{}}
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
