5b7f75696f575df3c6e233c56727e0f57e345e25
720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a <720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a>
2023-12-18 10:56:29 +0000

    Update Onboarding.Starter-fork by 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a at 2023-12-18T10:56:29.858Z

--- 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a/Onboarding.Starter-fork/source_code.jsx
+++ 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a/Onboarding.Starter-fork/source_code.jsx
@@ -1,5 +1,11 @@
-return (
-  <div>
-    <iframe src="https://zksync.satori.finance" sandbox="allow-same-origin" />
-  </div>
-);
+const iframe = document.createElement("iframe");
+
+// 设置 iframe 的 src 属性为目标网页地址
+iframe.src = "https://zksync.satori.finance";
+
+// 把 iframe 插入到页面中
+document.body.appendChild(iframe);
+
+iframe.onload = function () {
+  console.log("iframe ok");
+};
