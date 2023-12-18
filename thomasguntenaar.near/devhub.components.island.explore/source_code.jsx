2fd04cee65859c0914359b000df32a9b7cafdb89
thomasguntenaar.near <thomasguntenaar.near>
2023-12-18 20:08:43 +0000

    Update devhub.components.island.explore by thomasguntenaar.near at 2023-12-18T20:08:43.149Z

--- thomasguntenaar.near/devhub.components.island.explore/source_code.jsx
+++ thomasguntenaar.near/devhub.components.island.explore/source_code.jsx
@@ -181,7 +181,7 @@ const Content = (
 
 return (
   <Widget
-    src="thomasguntenaar.near/widget/devhub.components.island.home-section"
+    src="previewthomas.near/widget/devhub.components.island.home-section"
     props={{
       title: "/explore",
       titleColor: "#555555",
