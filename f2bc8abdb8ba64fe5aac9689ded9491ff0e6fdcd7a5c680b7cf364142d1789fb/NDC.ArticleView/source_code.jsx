f6114fc54cf4e58c06b95ee700c4c4a64ef5eb5d
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2023-12-19 15:26:47 +0000

    Update NDC.ArticleView by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2023-12-19T15:26:47.266Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.ArticleView/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.ArticleView/source_code.jsx
@@ -753,6 +753,7 @@ return (
                   callLibs,
                   baseActions,
                   sharedCommentId,
+                  articleToRenderData,
                 }}
               />
             ))}
