897752175785c7cab9bda091c599b527d9e19299
sainy.near <sainy.near>
2023-12-19 03:50:44 +0000

    Update SupercallTokenABI by sainy.near at 2023-12-19T03:50:44.503Z

---
+++ sainy.near/SupercallTokenABI/source_code.jsx
@@ -0,0 +1,11 @@
+return (
+  <Widget
+    src="sainy.near/widget/SupercallABI"
+    props={{
+      ...props,
+      title: "Token Transfer",
+      abiUrl:
+        "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json",
+    }}
+  />
+);
