9795114ff311a01319515acda06f234bb0a9bbc3
megha19.near <megha19.near>
2023-12-19 14:35:05 +0000

    Update devhub.page.addon by megha19.near at 2023-12-19T14:35:05.772Z

--- megha19.near/devhub.page.addon/source_code.jsx
+++ megha19.near/devhub.page.addon/source_code.jsx
@@ -92,18 +92,7 @@ const ButtonRow = styled.div`
 
 const [view, setView] = useState(props.view || "viewer");
 
-const checkFullyRefactored = (addon_id) => {
-  switch (addon_id) {
-    case "kanban":
-      // case "github":
-      return false;
-    default:
-      return true;
-  }
-};
-
-const isFullyRefactored = checkFullyRefactored(addon.addon_id);
-
+// TODO : remove after contract data change
 function updateWidgetEndpoint(widgetSrc) {
   widgetSrc = widgetSrc.replace("configurator", "Configurator");
   widgetSrc = widgetSrc.replace("devgovgigs.near", "megha19.near");
@@ -113,19 +102,17 @@ function updateWidgetEndpoint(widgetSrc) {
 
 return (
   <Container>
-    {isFullyRefactored && // Unfully refactored addons have the configurator built in.
-      // So we hide the header
-      permissions.can_configure && (
-        <SettingsButton
-          onClick={() => setView(view === "configure" ? "view" : "configure")}
-        >
-          {view === "configure" ? (
-            <span className="bi bi-x"></span>
-          ) : (
-            <span className="bi bi-gear"></span>
-          )}
-        </SettingsButton>
-      )}
+    {permissions.can_configure && (
+      <SettingsButton
+        onClick={() => setView(view === "configure" ? "view" : "configure")}
+      >
+        {view === "configure" ? (
+          <span className="bi bi-x"></span>
+        ) : (
+          <span className="bi bi-gear"></span>
+        )}
+      </SettingsButton>
+    )}
     <Content>
       {/* We hide in order to prevent a reload when we switch between two views */}
       <div className={`${view !== "configure" ? "d-none" : ""}`}>
