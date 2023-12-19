49f194142dd6271d196fdd8d969d949b661fd0f5
coolmouse.near <coolmouse.near>
2023-12-19 11:16:54 +0000

    Update KSTNAnvil.Proposals.Index by coolmouse.near at 2023-12-19T11:16:54.415Z

--- coolmouse.near/KSTNAnvil.Proposals.Index/source_code.jsx
+++ coolmouse.near/KSTNAnvil.Proposals.Index/source_code.jsx
@@ -414,7 +414,10 @@ return (
         <div className="text-lg breadcrumbs">
           <ul>
             <li>
-              <Link href={"/coolmouse.near/widget/Keystone.Index"}> Home </Link>
+              <Link href={"/coolmouse.near/widget/KSTNAnvil.Index"}>
+                {" "}
+                Home{" "}
+              </Link>
             </li>
             <li>Proposals</li>
           </ul>
@@ -422,7 +425,7 @@ return (
         <div className="py-5">
           <Link
             className="btn btn-primary btn-outline group mr-5"
-            href="coolmouse.near/widget/Keystone.Proposals.Keystone"
+            href="coolmouse.near/widget/KSTNAnvil.Proposals.Keystone"
           >
             <svg
               className="fill-primary h-full w-5 mr-2"
@@ -484,7 +487,7 @@ return (
           </Link>
           <Link
             className="btn btn-secondary btn-outline group"
-            href="coolmouse.near/widget/Keystone.Proposals.Token"
+            href="coolmouse.near/widget/KSTNAnvil.Proposals.Token"
           >
             <svg
               className="fill-secondary h-full w-5 mr-2"
