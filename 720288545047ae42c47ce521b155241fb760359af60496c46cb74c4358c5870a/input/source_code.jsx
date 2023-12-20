49c9a4ac407124bc7eef8899a76d3c1f5ebf70a8
720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a <720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a>
2023-12-20 10:40:14 +0000

    Update input by 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a at 2023-12-20T10:40:14.128Z

--- 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a/input/source_code.jsx
+++ 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a/input/source_code.jsx
@@ -1,11 +1,20 @@
-const { prepend, append, type, getValue, placeholder, inputTextAlign, value } =
-  props;
+const {
+  prepend,
+  append,
+  type,
+  onChange,
+  placeholder,
+  inputTextAlign,
+  value,
+  showDecimals,
+} = props;
 
 State.init({
   theme: Storage.privateGet("theme") || "dark",
+  inputValue: value || "",
+  inputType: type || "number",
+  inputDecimals: showDecimals || 4,
 });
-
-const [inputType, setInputType] = useState(type || "number");
 
 const dark = {
   name: "dark",
@@ -24,61 +33,80 @@ const useTheme = (light, dark) => {
   return state.theme === "light" ? light : dark;
 };
 
-const onChange = (ev) => {
-  getValue && getValue(ev.target.value);
+const inputDecimals = (ev) => {
+  const value = ev.target.value;
+  const digits = value.split(".")[1];
+  if (digits && digits.length > state.inputDecimals) {
+    const num = Big(value).toFixed(state.inputDecimals);
+    const val = Big(num).valueOf();
+    return val;
+  }
+  return value;
 };
 
-const Content = styled.div`
-  width: 100%;
-  height: 36px;
-  padding: 0 8px;
-  background-color: ${useTheme(light.inputBgColor, dark.inputBgColor)};
-  border: 1px solid ${useTheme(light.inputBorderColor, dark.inputBorderColor)};
-  display: flex;
-  align-items: center;
-  column-gap: 0.2rem;
-  border-radius: 6px;
-  overflow: hidden;
-`;
-
-const Input = styled.input`
-  border: none;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  background-color: ${useTheme(light.inputBgColor, dark.inputBgColor)};
-  flex: 1 1 0%;
-  font-size: 14px;
-  color: ${useTheme(light.baseColor, dark.baseColor)};
-  text-align: ${inputTextAlign ? inputTextAlign : "right"};
-
-  &:focus,
-  &:focus-visible {
-    border: none;
-    outline: -webkit-focus-ring-color auto 0;
+const StyleWarp = styled.div`
+  .input-inner {
+    &:focus,
+    &:focus-visible {
+      border: none;
+      outline: -webkit-focus-ring-color auto 0;
+    }
+    ::-webkit-inner-spin-button,
+    ::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
   }
-  ::-webkit-inner-spin-button,
-  ::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
+
+  .prepend-append {
+    flex: none;
   }
-`;
-const InputPrepend = styled.div`
-  flex: none;
-`;
-const InputAppend = styled.div`
-  flex: none;
 `;
 
 return (
-  <Content>
-    <InputPrepend>{prepend}</InputPrepend>
-    <Input
-      type={inputType}
-      onChange={onChange}
-      placeholder={placeholder || ""}
-      value={value}
-    />
-    <InputAppend>{append}</InputAppend>
-  </Content>
+  <StyleWarp>
+    <div
+      style={{
+        width: "100%",
+        height: "36px",
+        padding: "0 8px",
+        backgroundColor: `${useTheme(light.inputBgColor, dark.inputBgColor)}`,
+        border: `1px solid ${useTheme(
+          light.inputBorderColor,
+          dark.inputBorderColor
+        )}`,
+        display: "flex",
+        alignItems: "center",
+        columnGap: "0.2rem",
+        borderRadius: "6px",
+        overflow: "hidden",
+      }}
+    >
+      <div class="prepend-append">{prepend}</div>
+      <input
+        class="input-inner"
+        style={{
+          border: "none",
+          width: "100%",
+          height: "100%",
+          overflow: "hidden",
+          backgroundColor: `${useTheme(light.inputBgColor, dark.inputBgColor)}`,
+          flex: `1 1 0%`,
+          fontSize: "14px",
+          color: `${useTheme(light.baseColor, dark.baseColor)}`,
+          textAlign: `${inputTextAlign ? inputTextAlign : "right"}`,
+        }}
+        type={state.inputType}
+        onChange={(ev) => {
+          const val = inputDecimals(ev);
+          ev.target.value = val;
+          onChange && onChange(ev.target.value);
+          State.update({ inputValue: ev.target.value });
+        }}
+        placeholder={placeholder || ""}
+        value={state.inputValue}
+      />
+      <div class="prepend-append">{append}</div>
+    </div>
+  </StyleWarp>
 );
