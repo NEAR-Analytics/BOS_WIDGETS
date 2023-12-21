4ca4e0047ba59240fd078f215f0b6530b3c8dcdd
thomasguntenaar.near <thomasguntenaar.near>
2023-12-21 14:28:12 +0000

    Update devhub.components.molecule.Button by thomasguntenaar.near at 2023-12-21T14:28:12.888Z

--- thomasguntenaar.near/devhub.components.molecule.Button/source_code.jsx
+++ thomasguntenaar.near/devhub.components.molecule.Button/source_code.jsx
@@ -134,7 +134,7 @@ const Button = ({
         typeof iconProps === "object" &&
         !Array.isArray(iconProps) && (
           <Widget
-            src={"previewthomas.near/widget/devhub.components.atom.Icon"}
+            src={"thomasguntenaar.near/widget/devhub.components.atom.Icon"}
             props={iconProps}
           />
         )}
