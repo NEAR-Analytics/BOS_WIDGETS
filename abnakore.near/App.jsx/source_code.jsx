28b8d7e600b52156e0e7c9f84c388add8121c85a
abnakore.near <abnakore.near>
2023-12-21 12:15:45 +0000

    Update App.jsx by abnakore.near at 2023-12-21T12:15:45.071Z

--- abnakore.near/App.jsx/source_code.jsx
+++ abnakore.near/App.jsx/source_code.jsx
@@ -74,6 +74,32 @@ return (
                 />
 
                 <div className="body-contents">
+                  <i>
+                    <svg
+                      width="64px"
+                      height="64px"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        {" "}
+                        <path
+                          d="M4 6H20M4 12H20M4 18H20"
+                          stroke="#fefefe"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></path>{" "}
+                      </g>
+                    </svg>
+                  </i>
                   <h1>VoteChain</h1>
                   <p
                     style={{
