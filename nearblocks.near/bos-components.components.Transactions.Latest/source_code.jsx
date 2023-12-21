55665beebb057ac4a188df7cca9ba3a2e7072ff3
nearblocks.near <nearblocks.near>
2023-12-21 14:47:34 +0000

    Update bos-components.components.Transactions.Latest by nearblocks.near at 2023-12-21T14:47:34.594Z

--- nearblocks.near/bos-components.components.Transactions.Latest/source_code.jsx
+++ nearblocks.near/bos-components.components.Transactions.Latest/source_code.jsx
@@ -185,6 +185,37 @@ function getConfig(network) {
       return {};
   }
 }
+function debounce(
+  delay,
+  func,
+) {
+  let timer;
+  let active = true;
+  console.log('hgjhgh');
+  const debounced = (arg) => {
+    if (active) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        active && func(arg);
+        timer = undefined;
+      }, delay);
+    } else {
+      func(arg);
+    }
+  };
+
+  debounced.isPending = () => {
+    return timer !== undefined;
+  };
+
+  debounced.cancel = () => {
+    active = false;
+  };
+
+  debounced.flush = (arg) => func(arg);
+
+  return debounced;
+}
 function nanoToMilli(nano) {
   return new Big(nano).div(new Big(10).pow(6)).round().toNumber();
 }
@@ -226,6 +257,37 @@ function getConfig(network) {
       return {};
   }
 }
+function debounce(
+  delay,
+  func,
+) {
+  let timer;
+  let active = true;
+  console.log('hgjhgh');
+  const debounced = (arg) => {
+    if (active) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        active && func(arg);
+        timer = undefined;
+      }, delay);
+    } else {
+      func(arg);
+    }
+  };
+
+  debounced.isPending = () => {
+    return timer !== undefined;
+  };
+
+  debounced.cancel = () => {
+    active = false;
+  };
+
+  debounced.flush = (arg) => func(arg);
+
+  return debounced;
+}
 function shortenAddress(address) {
   const string = String(address);
 
@@ -262,6 +324,37 @@ function getConfig(network) {
     default:
       return {};
   }
+}
+function debounce(
+  delay,
+  func,
+) {
+  let timer;
+  let active = true;
+  console.log('hgjhgh');
+  const debounced = (arg) => {
+    if (active) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        active && func(arg);
+        timer = undefined;
+      }, delay);
+    } else {
+      func(arg);
+    }
+  };
+
+  debounced.isPending = () => {
+    return timer !== undefined;
+  };
+
+  debounced.cancel = () => {
+    active = false;
+  };
+
+  debounced.flush = (arg) => func(arg);
+
+  return debounced;
 }
 function yoctoToNear(yocto, format) {
   const YOCTO_PER_NEAR = Big(10).pow(24).toString();
@@ -320,6 +413,37 @@ function getConfig(network) {
     default:
       return {};
   }
+}
+function debounce(
+  delay,
+  func,
+) {
+  let timer;
+  let active = true;
+  console.log('hgjhgh');
+  const debounced = (arg) => {
+    if (active) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        active && func(arg);
+        timer = undefined;
+      }, delay);
+    } else {
+      func(arg);
+    }
+  };
+
+  debounced.isPending = () => {
+    return timer !== undefined;
+  };
+
+  debounced.cancel = () => {
+    active = false;
+  };
+
+  debounced.flush = (arg) => func(arg);
+
+  return debounced;
 }
 function localFormat(number) {
   const formattedNumber = Number(number).toLocaleString('en', {
