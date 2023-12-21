1c82df02baf63dcc64536be311b3fe94326d76dc
devhub.efiz.near <devhub.efiz.near>
2023-12-21 16:56:39 +0000

    Update devhub.page.feed by devhub.efiz.near at 2023-12-21T16:56:39.490Z

--- devhub.efiz.near/devhub.page.feed/source_code.jsx
+++ devhub.efiz.near/devhub.page.feed/source_code.jsx
@@ -41,33 +41,10 @@ const Gradient = styled.div`
   }
 `;
 
-function Banner() {
-  return (
-    <div className="d-flex flex-column">
-      <Widget
-        src={`devhub.efiz.near/widget/devhub.components.island.banner`}
-        props={{
-          title: (
-            <>
-              Welcome to /dev/hub,
-              <br />
-              <span style={{ color: "#151515" }}>the home base</span>
-              <br />
-              for developers on NEAR
-            </>
-          ),
-          imageLink:
-            "https://ipfs.near.social/ipfs/bafybeiap2mzwsly4apaldxguiunx4rjwqyadksj5yxuzwrww3kue3ao5qe",
-        }}
-      />
-    </div>
-  );
-}
-
 const FeedPage = ({ recency, tag }) => {
   return (
     <div className="w-100">
-      <Banner />
+      <Widget src={`devhub.efiz.near/widget/devhub.components.island.banner`} />
       <Widget
         src={"devhub.efiz.near/widget/devhub.feature.post-search.panel"}
         props={{
@@ -88,6 +65,7 @@ const FeedPage = ({ recency, tag }) => {
           ),
           recency,
           tag,
+          author,
           transactionHashes: props.transactionHashes,
         }}
       />
