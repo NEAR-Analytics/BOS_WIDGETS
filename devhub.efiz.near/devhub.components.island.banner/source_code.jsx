d85fd1e23a0cc77c623d618bac1e543b891ff9ab
devhub.efiz.near <devhub.efiz.near>
2023-12-21 16:56:39 +0000

    Update devhub.components.island.banner by devhub.efiz.near at 2023-12-21T16:56:39.490Z

--- devhub.efiz.near/devhub.components.island.banner/source_code.jsx
+++ devhub.efiz.near/devhub.components.island.banner/source_code.jsx
@@ -2,24 +2,22 @@ const imageLink =
   "https://ipfs.near.social/ipfs/bafybeiap2mzwsly4apaldxguiunx4rjwqyadksj5yxuzwrww3kue3ao5qe";
 
 const HeroSection = styled.div`
-  background: #00ec97;
-  clip-path: polygon(0 0, 100% 0%, 75% 100%, 0% 100%);
-
+  position: relative;
   height: auto;
   z-index: 3;
   width: 70%;
+  background: #00ec97;
+  clip-path: polygon(0 0, 100% 0%, 75% 100%, 0% 100%);
 
   padding-top: 2rem;
   padding-bottom: 2rem;
   padding-left: 3.375rem;
-`;
 
-const Image = styled.img`
-  margin-left: 400px;
-  height: 100%;
-  width: 100%;
-  filter: grayscale(100%);
-  object-fit: cover;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding: 1rem 1.5rem;
+    clip-path: none;
+  }
 `;
 
 const Title = styled.h1`
@@ -29,49 +27,74 @@ const Title = styled.h1`
   font-weight: 700;
   line-height: 100%; /* 88px */
   letter-spacing: -1.76px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 2.25rem;
+    letter-spacing: -0.72px;
+    margin: 0;
+  }
 `;
 
-const Lead = styled.p`
-  color: #151515;
-  font-size: 1.75rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 110%; /* 39.6px */
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: max-content;
+  overflow: hidden;
 
-  width: 70%;
+  @media screen and (max-width: 768px) {
+    background: #f4f4f4;
+  }
 `;
 
-const CTA = styled.a`
-  display: inline-flex;
-  padding: 0.875rem 1rem;
-  align-items: center;
-  gap: 0.5rem;
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 1;
+  background: transparent;
 
-  border-radius: 1rem;
-  border: 1px solid #151515;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
-  color: #151515;
-  font-size: 1.25rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 120%; /* 28.8px */
-  letter-spacing: -0.48px;
+const Image = styled.img`
+  margin-left: 15.625rem;
+  height: 100%;
+  width: 100%;
+  filter: grayscale(100%);
+  object-fit: cover;
+`;
+
+const MobileImage = styled.img`
+  display: none;
+
+  width: 100%;
+  height: 196px;
+
+  width: 100%;
+  object-fit: cover;
+  filter: grayscale(1);
+
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
 `;
 
 return (
-  <div
-    className="position-relative w-100 overflow-hidden"
-    style={{ height: "max-content" }}
-  >
+  <Container>
     <HeroSection>
       <Title>
-        The decentralized <br />{" "}
+        The decentralized <br />
         <span style={{ color: "#101820" }}>home base</span> <br />
         for NEAR builders
       </Title>
     </HeroSection>
-    <div className="position-absolute top-0 z-n1 end-0 h-100 w-100">
+    <MobileImage src={imageLink} />
+    <ImageContainer>
       <Image src={imageLink} />
-    </div>
-  </div>
+    </ImageContainer>
+  </Container>
 );
