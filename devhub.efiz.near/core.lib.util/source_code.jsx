2bbe9507c62bb4f264791778f74d6d6f7bbcef40
devhub.efiz.near <devhub.efiz.near>
2023-12-22 05:18:58 +0000

    Update core.lib.util by devhub.efiz.near at 2023-12-22T05:18:58.744Z

---
+++ devhub.efiz.near/core.lib.util/source_code.jsx
@@ -0,0 +1,11 @@
+let timeoutId;
+
+const debounce = (func, delay) => {
+  if (!delay) {
+    delay = 300;
+  }
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(func, delay);
+};
+
+return { debounce };
