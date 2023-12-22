3f16d90eaa7ca4672f65e1ba4e00071aad12f4d9
720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a <720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a>
2023-12-22 06:27:35 +0000

    Update Home by 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a at 2023-12-22T06:27:35.485Z

--- 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a/Home/source_code.jsx
+++ 720288545047ae42c47ce521b155241fb760359af60496c46cb74c4358c5870a/Home/source_code.jsx
@@ -1,25 +1,28 @@
+function isMobileDevice() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+}
+
 const HomeWarp = styled.div`
   width: 100%;
   margin: 0;
   padding: 0;
   min-height: 720px;
+  padding: 15px 0;
   background-color: #080d17;
   position: relative;
-  .pc-visible {
-    display: block;
-  }
-  .mobile-visible {
-    display: none;
-  }
+`;
 
-  @media screen and (max-width: 1024px) {
-    .pc-visible {
-      display: none;
-    }
-    .mobile-visible {
-      display: block;
-    }
-  }
+const TradeWarp = styled.div`
+  width: ${isMobileDevice() ? "100%" : "460px"};
+  background-color: #080d17;
+  border: 1px solid #323232;
+  border-radius: 16px;
+  position: relative;
+  overflow: hidden;
+  margin: 0 auto;
+  padding-bottom: 10px;
 `;
 
 const DropdownMenu = styled.div`
@@ -133,7 +136,7 @@ const TradeTab = styled.div`
     &.active {
       border-bottom: 2px solid #617168;
       animation-name: slideLeftAndFade;
-      animation: slideLeftAndFade 0.6s linear forwards;
+      animation: slideLeftAndFade 0.3s linear forwards;
       animation-iteration-count: 1;
     }
   }
@@ -273,125 +276,122 @@ useEffect(() => {
 
 return (
   <HomeWarp>
-    <div class="pc-visible">
-      <Widget src={`${nearId}/widget/Header`} />
-    </div>
-    <div class="mobile-visible">
+    <TradeWarp>
       <Widget src={`${nearId}/widget/H5Header`} />
-    </div>
-    <DropdownMenu>
-      <div
-        class="menu-box"
-        onClick={() => {
-          State.update({ groupVisible: !state.groupVisible });
-          console.log(state.groupVisible);
-        }}
-      >
-        <div class="menu-left">
-          <img
-            class="coin-logo"
-            src={state.contractInfo.tradeCoin.coin}
-            alt=""
-          />
-          <span class="coin-name">{state.contractInfo.symbol}</span>
-        </div>
+      <DropdownMenu>
+        <div
+          class="menu-box"
+          onClick={() => {
+            State.update({ groupVisible: !state.groupVisible });
+            console.log(state.groupVisible);
+          }}
+        >
+          <div class="menu-left">
+            <img
+              class="coin-logo"
+              src={state.contractInfo.tradeCoin.coin}
+              alt=""
+            />
+            <span class="coin-name">{state.contractInfo.symbol}</span>
+          </div>
 
-        <div class="menu-right">
-          <span class="markets-text">All Markets</span>
-          <span class="down-triangle">
-            <svg
-              data-v-fd82d23f=""
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-            >
-              <path
+          <div class="menu-right">
+            <span class="markets-text">All Markets</span>
+            <span class="down-triangle">
+              <svg
                 data-v-fd82d23f=""
-                d="M7.29273 9.76817C7.68325 10.1588 8.31646 10.1588 8.707 9.76823L12.0065 6.46872L11.0645 5.52539L7.99984 8.59206L4.93583 5.52539L3.99316 6.46806L7.29273 9.76817Z"
-                fill="#98969e"
-              ></path>
-            </svg>
-          </span>
-        </div>
-      </div>
-
-      {state.groupVisible && (
-        <GroupWarp>
-          <ul class="group-ul">
-            <li class="group-header-box">
-              <div class="header-th">
-                <span class="th-txt">Market</span>
-              </div>
-              <div>
-                <span class="th-txt">Price</span>
-              </div>
-            </li>
-            {exContractList.map((item) => (
-              <li
-                class="group-li"
-                onClick={() => {
-                  State.update({ contractInfo: item, groupVisible: false });
-                }}
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
               >
-                <div class="group-li-left">
-                  <img class="coin-logo" src={item.tradeCoin.coin} alt="" />
-                  <span class="coin-name">{item.symbol}</span>
+                <path
+                  data-v-fd82d23f=""
+                  d="M7.29273 9.76817C7.68325 10.1588 8.31646 10.1588 8.707 9.76823L12.0065 6.46872L11.0645 5.52539L7.99984 8.59206L4.93583 5.52539L3.99316 6.46806L7.29273 9.76817Z"
+                  fill="#98969e"
+                ></path>
+              </svg>
+            </span>
+          </div>
+        </div>
+
+        {state.groupVisible && (
+          <GroupWarp>
+            <ul class="group-ul">
+              <li class="group-header-box">
+                <div class="header-th">
+                  <span class="th-txt">Market</span>
                 </div>
-                <div class="group-li-right">
-                  <span class="price">{item.price}</span>
+                <div>
+                  <span class="th-txt">Price</span>
                 </div>
               </li>
-            ))}
-          </ul>
-        </GroupWarp>
+              {exContractList.map((item) => (
+                <li
+                  class="group-li"
+                  onClick={() => {
+                    State.update({ contractInfo: item, groupVisible: false });
+                  }}
+                >
+                  <div class="group-li-left">
+                    <img class="coin-logo" src={item.tradeCoin.coin} alt="" />
+                    <span class="coin-name">{item.symbol}</span>
+                  </div>
+                  <div class="group-li-right">
+                    <span class="price">{item.price}</span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </GroupWarp>
+        )}
+      </DropdownMenu>
+      <TradeTab>
+        {tradeTab.map((item, index) => (
+          <div
+            class={`tab-btn ${state.tradeTabIndex === index ? "active" : ""}`}
+            onClick={() => {
+              State.update({ tradeTabIndex: index });
+            }}
+          >
+            <span>{item.name}</span>
+          </div>
+        ))}
+      </TradeTab>
+      <TardeType>
+        {tradeTypeBtn.map((item, index) => (
+          <div
+            class={`type-btn ${state.tradeTypeIndex === index ? "active" : ""}`}
+            onClick={() => {
+              State.update({ tradeTypeIndex: index });
+            }}
+          >
+            <span>{item.name}</span>
+          </div>
+        ))}
+      </TardeType>
+      {state.tradeTabIndex === 0 ? (
+        <Widget
+          src={`${nearId}/widget/Limit`}
+          props={{
+            contractSymbol: sizeContractSymbol,
+            price: state.contractInfo.price,
+            exUrl,
+            nearId,
+          }}
+        />
+      ) : (
+        <Widget
+          src={`${nearId}/widget/Market`}
+          props={{
+            contractSymbol: sizeContractSymbol,
+            price: state.contractInfo.price,
+            exUrl,
+            nearId,
+          }}
+        />
       )}
-    </DropdownMenu>
-    <TradeTab>
-      {tradeTab.map((item, index) => (
-        <div
-          class={`tab-btn ${state.tradeTabIndex === index ? "active" : ""}`}
-          onClick={() => {
-            State.update({ tradeTabIndex: index });
-          }}
-        >
-          <span>{item.name}</span>
-        </div>
-      ))}
-    </TradeTab>
-    <TardeType>
-      {tradeTypeBtn.map((item, index) => (
-        <div
-          class={`type-btn ${state.tradeTypeIndex === index ? "active" : ""}`}
-          onClick={() => {
-            State.update({ tradeTypeIndex: index });
-          }}
-        >
-          <span>{item.name}</span>
-        </div>
-      ))}
-    </TardeType>
-    {state.tradeTabIndex === 0 ? (
-      <Widget
-        src={`${nearId}/widget/Limit`}
-        props={{
-          contractSymbol: sizeContractSymbol,
-          price: state.contractInfo.price,
-          exUrl,
-          nearId,
-        }}
-      />
-    ) : (
-      <Widget
-        src={`${nearId}/widget/Market`}
-        props={{
-          contractSymbol: sizeContractSymbol,
-          price: state.contractInfo.price,
-          exUrl,
-          nearId,
-        }}
-      />
-    )}
+    </TradeWarp>
   </HomeWarp>
 );
