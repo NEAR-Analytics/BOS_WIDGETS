0cde2744587041fcbd1b8c0195edcb5ced73afc9
gagdiez.near <gagdiez.near>
2023-12-22 21:00:48 +0000

    Update Darija.Menu by gagdiez.near at 2023-12-22T21:00:48.256Z

--- gagdiez.near/Darija.Menu/source_code.jsx
+++ gagdiez.near/Darija.Menu/source_code.jsx
@@ -1,10 +1,13 @@
 const menu = [
-  { name: "Conjugar Nombre", link: "gagdiez.near/widget/Darija.Nombre" },
-  { name: "Posesivos", link: "gagdiez.near/widget/Darija.Posesivos" },
-  { name: "Familia Nuclear", link: "gagdiez.near/widget/Darija.FamiliaNucleo" },
+  { name: "Conjugar Nombre", link: "/gagdiez.near/widget/Darija.Nombre" },
+  { name: "Posesivos", link: "/gagdiez.near/widget/Darija.Posesivos" },
+  {
+    name: "Familia Nuclear",
+    link: "/gagdiez.near/widget/Darija.FamiliaNucleo",
+  },
   {
     name: "Familia Extendida",
-    link: "gagdiez.near/widget/Darija.FamiliaExtendida",
+    link: "/gagdiez.near/widget/Darija.FamiliaExtendida",
   },
 ];
 
