97e6747cde941fba563548eaaf91a7e2d1dc8c5b
abnakore.near <abnakore.near>
2023-12-25 00:19:22 +0000

    Update CreateVote by abnakore.near at 2023-12-25T00:19:22.671Z

--- abnakore.near/CreateVote/source_code.jsx
+++ abnakore.near/CreateVote/source_code.jsx
@@ -37,6 +37,93 @@ return (
                       setItem: setNewVote,
                     }}
                   />
+
+                  <Widget
+                    src="abnakore.near/widget/Input.jsx"
+                    props={{
+                      type: "text",
+                      placeholder: "Description",
+                      required: true,
+                      kind: "textarea",
+                      item: "desc",
+                      items: newVote,
+                      setItem: setNewVote,
+                    }}
+                  />
+                  <hr />
+
+                  <Widget
+                    src="abnakore.near/widget/Table"
+                    props={{
+                      headings: ["Party Name", "Acronym"],
+                      data: [[]],
+                    }}
+                  />
+                  <div className="form">
+                    <h5 style={{ textAlign: "center" }}>Add Party</h5>
+                    <div className="flex">
+                      <Widget
+                        src="abnakore.near/widget/Input.jsx"
+                        props={{
+                          type: "text",
+                          placeholder: "Party Name",
+                          required: true,
+                          item: "name",
+                          items: newCandidate,
+                          setItem: setNewCandidate,
+                        }}
+                      />
+                      <Widget
+                        src="abnakore.near/widget/Input.jsx"
+                        props={{
+                          type: "text",
+                          placeholder: "Acronym",
+                          required: true,
+                          item: "party",
+                          items: newCandidate,
+                          setItem: setNewCandidate,
+                        }}
+                      />
+                    </div>
+                    <button>Add</button>
+                  </div>
+                  <hr />
+
+                  <Widget
+                    src="abnakore.near/widget/Table"
+                    props={{
+                      headings: ["Candidate's Name", "Party"],
+                      data: [[]],
+                    }}
+                  />
+                  <div className="form">
+                    <h5 style={{ textAlign: "center" }}>Add Candidate</h5>
+                    <div className="flex">
+                      <Widget
+                        src="abnakore.near/widget/Input.jsx"
+                        props={{
+                          type: "text",
+                          placeholder: "Candidate's Name",
+                          required: true,
+                          item: "name",
+                          items: newCandidate,
+                          setItem: setNewCandidate,
+                        }}
+                      />
+                      <Widget
+                        src="abnakore.near/widget/Input.jsx"
+                        props={{
+                          type: "text",
+                          placeholder: "party",
+                          required: true,
+                          item: "party",
+                          items: newCandidate,
+                          setItem: setNewCandidate,
+                        }}
+                      />
+                    </div>
+                    <button>Add</button>
+                  </div>
                 </div>
               </div>
             </div>
