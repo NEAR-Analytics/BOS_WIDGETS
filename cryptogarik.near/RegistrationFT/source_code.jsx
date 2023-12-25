b6dda4d99b245a07e746446b7da829b186c77f41
cryptogarik.near <cryptogarik.near>
2023-12-25 14:17:02 +0000

    Update RegistrationFT by cryptogarik.near at 2023-12-25T14:17:02.918Z

---
+++ cryptogarik.near/RegistrationFT/source_code.jsx
@@ -0,0 +1,84 @@
+const accountId = props.accountId || context.accountId;
+const ftContract = "token.v2.ref-finance.near";
+
+if (!state.accountId && !state.ftContract) {
+  State.update({
+    accountId,
+    ftContract,
+  });
+}
+
+const onChangeAccount = (updatedAccountId) => {
+  State.update({
+    accountId: updatedAccountId,
+  });
+};
+
+const onChangeFTContract = (ftContract) => {
+  State.update({
+    ftContract,
+  });
+};
+
+const handleCheck = () => {
+  const accountStorageBalance = Near.view(
+    state.ftContract,
+    "storage_balance_of",
+    {
+      account_id: state.accountId,
+    }
+  );
+  if (accountStorageBalance) {
+    State.update({
+      accountStorageBalance,
+    });
+  } else {
+    State.update({
+      accountStorageBalance: {
+        total: "0",
+        available: "0",
+      },
+    });
+  }
+  console.log("accountStorageBalance: ", state.accountStorageBalance);
+};
+
+return (
+  <div>
+    <div>Check user registration in FT contract</div>
+    <div>
+      Account:
+      <input
+        type="text"
+        value={state.accountId}
+        onChange={(e) => onChangeAccount(e.target.value)}
+      />
+    </div>
+    <div>
+      FT contract:
+      <input
+        type="text"
+        value={state.ftContract}
+        onChange={(e) => onChangeFTContract(e.target.value)}
+      />
+    </div>
+    <div>
+      <button onClick={handleCheck}>Check</button>
+    </div>
+    {state.accountStorageBalance && (
+      <div>
+        <div>
+          <h5>Registration data (Storage balance)</h5>
+        </div>
+        {(Number(state.accountStorageBalance.total) > 0 && (
+          <div>User is registered</div>
+        )) || <div>User is not registered</div>}
+        <div>
+          <h6>Storage balance</h6>
+        </div>
+        <div>{"Total: " + state.accountStorageBalance.total}</div>
+        <div>{"Available: " + state.accountStorageBalance.available}</div>
+      </div>
+    )}
+  </div>
+);
