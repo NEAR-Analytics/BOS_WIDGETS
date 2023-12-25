5b8d65029e035400185f4d8ef83e1dd69f62b033
abnakore.near <abnakore.near>
2023-12-25 00:02:37 +0000

    Update CreateVote by abnakore.near at 2023-12-25T00:02:37.596Z

--- abnakore.near/CreateVote/source_code.jsx
+++ abnakore.near/CreateVote/source_code.jsx
@@ -1,5 +1,74 @@
 // Get accountId
-const accountId = context.acccountId;
+const accountId = context.accountId;
 
 // New Vote
-const [newVote, setNewVote] = useState();
+const [newVote, setNewVote] = useState({
+  name: "",
+  desc: "",
+  role: "",
+  creator: "",
+  openDate: "",
+  closeDate: "",
+  opened: true,
+  passcode: null,
+  candidates: [],
+  parties: [],
+});
+
+return (
+  <>
+    {accountId ? (
+      <Widget
+        src="abnakore.near/widget/Wrapper"
+        props={{
+          body: (
+            <div className="main-body">
+              <div className="body-contents">
+                <h1>Create</h1>
+                <div className="form">
+                  <Widget
+                    src="abnakore.near/widget/Input.jsx"
+                    props={{
+                      type: "text",
+                      placeholder: "Name / Title",
+                      required: true,
+                      item: "name",
+                      items: newVote,
+                      setItem: setNewVote,
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          ),
+        }}
+      />
+    ) : (
+      <Widget src="abnakore.near/widget/SignIn.jsx" props={props} />
+    )}
+  </>
+);
+
+// // Get accountId
+// const accountId = context.accountId;
+
+// return (
+//   <>
+//     {accountId ? (
+//       <Widget
+//         src="abnakore.near/widget/Wrapper"
+//         props={{
+//           body: (
+//             <div className="main-body">
+//               <div className="body-contents">
+//                 <h1>____</h1>
+//               </div>
+//             </div>
+//           ),
+//         }}
+//       />
+//     ) : (
+//       <Widget src="abnakore.near/widget/SignIn.jsx" props={props} />
+//     )}
+//   </>
+// );
