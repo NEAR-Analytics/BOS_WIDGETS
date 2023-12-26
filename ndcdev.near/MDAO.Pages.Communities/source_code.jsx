d782fbe5200dace768498576b9f469471a7e3229
ndcdev.near <ndcdev.near>
2023-12-26 22:38:36 +0000

    Update MDAO.Pages.Communities by ndcdev.near at 2023-12-26T22:38:36.241Z

--- ndcdev.near/MDAO.Pages.Communities/source_code.jsx
+++ ndcdev.near/MDAO.Pages.Communities/source_code.jsx
@@ -13,53 +13,6 @@ const Container = styled.div`
   overflow: hidden;
 `;
 
-const HeroSection = styled.div`
-  width: 100%;
-  background: linear-gradient(
-    96deg,
-    #fdefb1 -19.42%,
-    #e1c4fe 49.87%,
-    #95c3fe 98.55%
-  );
-  height: 600px;
-  padding: 3rem;
-
-  @media screen and (max-width: 786px) {
-    padding: 2rem;
-    text-align: center;
-  }
-
-  h1 {
-    font-size: 5.2rem;
-    font-weight: 600;
-    margin-bottom: 0;
-    @media screen and (max-width: 786px) {
-      font-size: 3rem;
-    }
-  }
-
-  h3 {
-    font-size: 1.5rem;
-    font-weight: 300;
-    max-width: 800px;
-    margin-bottom: 0;
-  }
-
-  h4 {
-    font-size: 1.5rem;
-    font-weight: 400;
-    margin-bottom: 0;
-  }
-
-  img {
-    width: 500px;
-    height: 500px;
-    @media screen and (max-width: 786px) {
-      display: none;
-    }
-  }
-}`;
-
 const ConnectSection = styled.div`
   padding: 5rem 3rem;
 
@@ -70,18 +23,9 @@ const ConnectSection = styled.div`
 `;
 
 return (
-  <>
-    <HeroSection className="d-flex justify-content-between align-items-center gap-3">
-      <div className="d-flex flex-column gap-5">
-        <h1>{content.heroTitle}</h1>
-        <h3>{content.heroDesc}</h3>
-      </div>
-      <img src={assets.hero} />
-    </HeroSection>
-    <Container>
-      <ConnectSection className="d-flex flex-column gap-5">
-        <Widget src="ndcdev.near/widget/MDAO.Components.Communities" />
-      </ConnectSection>
-    </Container>
-  </>
+  <Container>
+    <ConnectSection className="d-flex flex-column gap-5">
+      <Widget src="ndcdev.near/widget/MDAO.Components.Communities" />
+    </ConnectSection>
+  </Container>
 );
