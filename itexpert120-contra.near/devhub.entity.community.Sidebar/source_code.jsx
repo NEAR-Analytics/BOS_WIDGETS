6c96a8814de89187fece09c579e4a5b9ee16cf8c
itexpert120-contra.near <itexpert120-contra.near>
2023-12-27 20:58:21 +0000

    Update devhub.entity.community.Sidebar by itexpert120-contra.near at 2023-12-27T20:58:21.599Z

---
+++ itexpert120-contra.near/devhub.entity.community.Sidebar/source_code.jsx
@@ -0,0 +1,70 @@
+const { href } = VM.require("itexpert120-contra.near/widget/core.lib.url");
+
+if (!href) {
+  return <p>Loading modules...</p>;
+}
+
+const { community } = props;
+
+const CommunitySummary = () => {
+  return (
+    <>
+      <Widget
+        src={"itexpert120-contra.near/widget/devhub.components.molecule.MarkdownViewer"}
+        props={{
+          text: community.bio_markdown,
+        }}
+      />
+      <small class="text-muted mb-3">
+        <Link
+          to={href({
+            widgetSrc: "itexpert120-contra.near/widget/app",
+            params: { page: "feed", tag: community.tag },
+          })}
+        >
+          <Widget
+            src={"itexpert120-contra.near/widget/devhub.components.atom.Tag"}
+            props={{ tag: community.tag }}
+          />
+        </Link>
+      </small>
+    </>
+  );
+};
+
+return community === null ? (
+  <div>Loading...</div>
+) : (
+  <div class="d-flex flex-column align-items-end">
+    <Widget
+      src={"itexpert120-contra.near/widget/devhub.entity.community.Tile"}
+      props={{
+        fullWidth: true,
+        minHeight: 0,
+        noBorder: true,
+        children: <CommunitySummary />,
+        style: { marginTop: "0.5rem" },
+      }}
+    />
+
+    <Widget
+      src={"itexpert120-contra.near/widget/devhub.entity.community.Tile"}
+      props={{
+        heading: "Admins",
+
+        children: (community?.admins ?? []).map((accountId) => (
+          <div key={accountId} className="d-flex" style={{ fontWeight: 500 }}>
+            <Widget
+              src="itexpert120-contra.near/widget/devhub.components.molecule.ProfileCard"
+              props={{ accountId }}
+            />
+          </div>
+        )),
+
+        fullWidth: true,
+        minHeight: 0,
+        noBorder: true,
+      }}
+    />
+  </div>
+);
