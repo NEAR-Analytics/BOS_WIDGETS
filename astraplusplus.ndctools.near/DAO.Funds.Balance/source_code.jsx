83850574e39832769f8f760e41c3434532a49abd
astraplusplus.ndctools.near <astraplusplus.ndctools.near>
2023-12-27 13:51:40 +0000

    Update DAO.Funds.Balance by astraplusplus.ndctools.near at 2023-12-27T13:51:40.398Z

--- astraplusplus.ndctools.near/DAO.Funds.Balance/source_code.jsx
+++ astraplusplus.ndctools.near/DAO.Funds.Balance/source_code.jsx
@@ -4,17 +4,17 @@ const daoId = props?.daoId;
 const balances = fetcher.balances([daoId]);
 
 if (!balances.body) {
-  return "Loading...";
+    return "Loading...";
 }
 
 return (
-  <div className="ndc-card p-4">
-    <h3 className="mb-3">Current Balance</h3>
-    <Widget
-      src="nearui.near/widget/Data.Balances"
-      props={{
-        balances: balances.body,
-      }}
-    />
-  </div>
+    <div className="ndc-card p-4">
+        <h3 className="mb-3">Current Balance</h3>
+        <Widget
+            src="nearui.near/widget/Data.Balances"
+            props={{
+                balances: balances.body
+            }}
+        />
+    </div>
 );
