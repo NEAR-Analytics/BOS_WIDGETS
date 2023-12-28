ccb3159badb49cb54edef99e7503f89a21c2d382
hack.near <hack.near>
2023-12-28 21:36:00 +0000

    Update dao.join by hack.near at 2023-12-28T21:36:00.148Z

--- hack.near/dao.join/source_code.jsx
+++ hack.near/dao.join/source_code.jsx
@@ -1,44 +1,59 @@
 const accountId = context.accountId;
-const memberId = props.memberId ?? context.accountId;
-const roleId = props.roleId ?? "voter";
-const daoId = props.daoId ?? "rc-dao.sputnik-dao.near";
+const roleId = props.roleId ?? "community";
+const daoId = props.daoId ?? "build.sputnik-dao.near";
+const proposalId =
+  props.proposalId ?? Near.view(daoId, "get_last_proposal_id") - 1;
 
 if (!accountId) {
   return "";
 }
 
+State.init({
+  isMember: false,
+});
+
+// get DAO policy, deposit, and group
 const policy = Near.view(daoId, "get_policy");
 
 if (policy === null) {
   return "";
 }
+
 const deposit = policy.proposal_bond;
 
 const group = policy.roles
   .filter((role) => role.name === roleId)
   .map((role) => role.kind.Group);
 
-State.init({
-  isMember: false,
+// get data from last proposal
+const proposal = Near.view(daoId, "get_proposal", {
+  id: proposalId,
 });
 
-const handleProposal = () => {
+if (proposal === null) {
+  return "";
+}
+
+// check if the potential member submitted last proposal
+const canJoin = accountId && accountId !== proposal.proposer;
+
+const handleJoin = () => {
   Near.call([
     {
       contractName: daoId,
       methodName: "add_proposal",
       args: {
         proposal: {
-          description: "add member to DAO",
+          description: `add ${accountId} to the ${roleId} group`,
           kind: {
             AddMemberToRole: {
-              member_id: memberId,
+              member_id: accountId,
               role: roleId,
             },
           },
         },
       },
-      gas: 300000000000000,
+      gas: 219000000000000,
       deposit: deposit,
     },
   ]);
@@ -47,42 +62,62 @@ const handleProposal = () => {
 const groupMembers = group.join(", ");
 
 const checkMembership = (groupMembers) => {
-  if (groupMembers.indexOf(memberId) !== -1) {
+  if (groupMembers.indexOf(accountId) !== -1) {
     return State.update({ isMember: true });
   }
 };
 
 const validMember = checkMembership(groupMembers);
 
-// IAH Verification
-let human = false;
-const userSBTs = Near.view("registry.i-am-human.near", "sbt_tokens_by_owner", {
-  account: memberId,
-});
-
-for (let i = 0; i < userSBTs.length; i++) {
-  if ("fractal.i-am-human.near" == userSBTs[i][0]) {
-    human = true;
+// check if the potential member is connected
+const connectEdge = Social.keys(
+  `${accountId}/graph/connect/${daoId}`,
+  undefined,
+  {
+    values_only: true,
   }
-}
+);
+
+const loading = connectEdge === null || inverseEdge === null;
+const isConnected = connectEdge && Object.keys(connectEdge).length;
 
 return (
   <div>
-    {!validMember ? (
-      <button
-        disabled={!human}
-        className="btn btn-success m-1"
-        onClick={handleProposal}
-      >
-        Join
-      </button>
+    {!isConnected ? (
+      <Widget src="hack.near/widget/dao.connect" />
     ) : (
-      <a
-        className="btn btn-outline-success m-1"
-        href="#/hack.near/widget/verified.members"
-      >
-        View
-      </a>
+      <>
+        {!validMember ? (
+          <>
+            {canJoin ? (
+              <button
+                className="btn btn-success m-1 rounded-5"
+                onClick={handleJoin}
+              >
+                Join
+              </button>
+            ) : (
+              <>
+                <button
+                  disabled={!canJoin}
+                  className="btn btn-success m-1 rounded-5"
+                >
+                  Pending
+                </button>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <button
+              disabled={validMember}
+              className="btn btn-success m-1 rounded-5"
+            >
+              Joined
+            </button>
+          </>
+        )}
+      </>
     )}
   </div>
 );
