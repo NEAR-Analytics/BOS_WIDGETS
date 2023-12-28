76b12c191d31dd1a5bf444049219c3eeacc0b2bd
astraplusplus.ndctools.near <astraplusplus.ndctools.near>
2023-12-28 12:03:36 +0000

    Update DAO.Layout.Sidebar by astraplusplus.ndctools.near at 2023-12-28T12:03:36.034Z

--- astraplusplus.ndctools.near/DAO.Layout.Sidebar/source_code.jsx
+++ astraplusplus.ndctools.near/DAO.Layout.Sidebar/source_code.jsx
@@ -2,23 +2,23 @@ const daoId = props.daoId ?? "";
 const profile = daoId ? Social.get(`${daoId}/profile/**`, "final") : {};
 
 const Sidebar = styled.div`
-    position: relative;
-    z-index: 5;
-    margin-top: -55px;
+  position: relative;
+  z-index: 5;
+  margin-top: -55px;
 
-    @media (max-width: 1024px) {
-        margin-top: -40px;
-    }
+  @media (max-width: 1024px) {
+    margin-top: -40px;
+  }
 `;
 
 return (
-    <Sidebar>
-        <Widget
-            src="hack.near/widget/DAO.Page.Sidebar"
-            props={{
-                daoId,
-                profile
-            }}
-        />
-    </Sidebar>
+  <Sidebar>
+    <Widget
+      src="hack.near/widget/DAO.Page.Sidebar"
+      props={{
+        daoId,
+        profile,
+      }}
+    />
+  </Sidebar>
 );
