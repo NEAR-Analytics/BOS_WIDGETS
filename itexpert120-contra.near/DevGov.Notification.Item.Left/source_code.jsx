30891b6315e8013953d69928d55e1a15b6977d43
itexpert120-contra.near <itexpert120-contra.near>
2023-12-29 08:26:28 +0000

    Update DevGov.Notification.Item.Left by itexpert120-contra.near at 2023-12-29T08:26:28.446Z

--- itexpert120-contra.near/DevGov.Notification.Item.Left/source_code.jsx
+++ itexpert120-contra.near/DevGov.Notification.Item.Left/source_code.jsx
@@ -1,4 +1,4 @@
-const { href } = VM.require("itexpert120-contra.nera/widget/core.lib.url") || (() => {});
+const { href } = VM.require("itexpert120-contra.near/widget/core.lib.url") || (() => {});
 
 if (!props.type) {
   return "Loading ...";
@@ -19,7 +19,7 @@ return props.type ? (
     <a
       className="fw-bold text-muted"
       href={href({
-        widgetSrc: "itexpert120-contra.nera/widget/app",
+        widgetSrc: "itexpert120-contra.near/widget/app",
         params: {
           page: "post",
           id: props.post,
