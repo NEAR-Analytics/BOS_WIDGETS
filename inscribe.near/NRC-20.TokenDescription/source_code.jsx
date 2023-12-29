3f3593de3abcc55f1a1d297e20d72c4b71466c1b
inscribe.near <inscribe.near>
2023-12-29 03:47:57 +0000

    Update NRC-20.TokenDescription by inscribe.near at 2023-12-29T03:47:57.443Z

---
+++ inscribe.near/NRC-20.TokenDescription/source_code.jsx
@@ -0,0 +1,430 @@
+const FormContainer = styled.div`
+  max-width: 650px;
+  width: 100%;
+  background: #141414;
+  border-radius: 4px;
+  border: 1px solid #ffffff1a;
+  display: flex;
+  flex-direction: column;
+  gap: 36px;
+
+  padding: 16px;
+  @media (min-width: 640px) {
+    padding: 24px;
+  }
+`;
+
+const FormTitle = styled.div`
+  font-size: 22px;
+  font-weight: 600px;
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+`;
+
+const FormBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const FormRowContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FormRowTitle = styled.div``;
+
+const FormRowValue = styled.div``;
+
+const FormButtonGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const FormButton = styled.button`
+  height: 56px;
+  width: 100%;
+  display: grid;
+  place-content: center;
+  cursor: pointer;
+  border: 1px solid #ffffff;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 4px;
+  background: transparent;
+  color: #ffffff;
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.4;
+  }
+  &:hover:not(:disabled) {
+    background: rgba(255, 255, 255, 0.08);
+  }
+`;
+
+const FormFragment = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TipText = styled.div`
+  color: #fffffff0;
+  font-size: 12px;
+  font-weight: 600;
+`;
+
+function fetchFromGraph(query) {
+  return fetch(config.graphUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+    }),
+  });
+}
+
+function asyncFetchFromGraph(query) {
+  return asyncFetch(config.graphUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+    }),
+  });
+}
+
+function fetchTokenInfosAsync() {
+  return asyncFetchFromGraph(`
+    query {
+      tokenInfos {
+        ticker
+        maxSupply
+        totalSupply
+        limit
+        createdBlockTimestamp
+        decimals
+      }
+      holderCounts {
+        ticker
+        count
+      }
+    }
+  `).then((tokensInfoResponse) => {
+    if (tokensInfoResponse.body?.data) {
+      return tokensInfoResponse.body?.data;
+    }
+    return undefined;
+  });
+}
+
+function fetchTokenInfoAsync(token) {
+  return asyncFetchFromGraph(`
+    query {
+      tokenInfo (
+        id: "${token.toUpperCase()}",
+      ) {
+        ticker
+        limit
+        decimals
+        maxSupply
+        totalSupply
+        creatorId
+        createdBlockHeight
+        createdBlockTimestamp
+      }
+      holderCount (
+        id: "${token.toUpperCase()}",
+      ) {
+        ticker
+        count
+      }
+    }
+  `).then((tokenInfoResponse) => {
+    if (tokenInfoResponse.body?.data) {
+      return tokenInfoResponse.body.data;
+    }
+    return undefined;
+  });
+}
+
+function getBalance() {
+  const accountId = props.accountId || context.accountId;
+  return asyncFetchFromGraph(`
+    query {
+      holderInfos(
+        where: {
+          accountId: "${accountId}"
+          ticker: "neat"
+        }
+      ) {
+        accountId
+        amount
+      }
+    }
+  `).then((balanceResponse) => {
+    const holder = balanceResponse.body.data.holderInfos[0];
+    if (holder) {
+      return holder.amount;
+    }
+    return "0";
+  });
+}
+
+function getBalances() {
+  const accountId = props.accountId || context.accountId;
+  return asyncFetchFromGraph(`
+    query {
+      holderInfos(
+        where: {
+          accountId: "${accountId}"
+        }
+      ) {
+        ticker
+        amount
+      }
+    }
+  `).then((balanceResponse) => {
+    if (balanceResponse.body?.data) {
+      return balanceResponse.body.data.holderInfos;
+    }
+    return undefined;
+  });
+}
+
+function getWrappedFtBalance() {
+  const accountId = props.accountId || context.accountId;
+  return Near.asyncView(config.ftWrapper, "ft_balance_of", {
+    account_id: accountId,
+  });
+}
+
+function getNrc20TotalSupply() {
+  if (!state.nep141TotalSupply || !state.tokenInfo?.maxSupply) return undefined;
+  return Big(state.tokenInfo.maxSupply)
+    .minus(state.nep141TotalSupply)
+    .toFixed();
+}
+
+function getNep141TotalSupply() {
+  return Near.asyncView(config.ftWrapper, "ft_total_supply");
+}
+
+const TGas = Big(10).pow(12);
+const MaxGasPerTransaction = TGas.mul(250);
+const GasPerTransaction = MaxGasPerTransaction.plus(TGas);
+const pageAmountOfPage = 5;
+const ipfsPrefix = "https://ipfs.near.social/ipfs";
+const landingUrl = "https://neatprotocol.ai";
+function toLocaleString(source, decimals, rm) {
+  if (typeof source === "string") {
+    return toLocaleString(Number(source), decimals);
+  } else if (typeof source === "number") {
+    return decimals !== undefined
+      ? source.toLocaleString(undefined, {
+          maximumFractionDigits: decimals,
+          minimumFractionDigits: decimals,
+        })
+      : source.toLocaleString();
+  } else {
+    // Big type
+    return toLocaleString(
+      decimals !== undefined
+        ? Number(source.toFixed(decimals, rm))
+        : source.toNumber(),
+      decimals
+    );
+  }
+}
+
+function formatAmount(_balance, _decimal) {
+  const balance = _balance ?? 0;
+  const decimal = _decimal ?? 8;
+  return toLocaleString(
+    Big(balance).div(Big(10).pow(decimal)).toFixed(),
+    decimal
+  );
+}
+
+function formatDeployTime(blockTime) {
+  const milliseconds = blockTime / 1000000;
+  const date = new Date(milliseconds);
+
+  const year = date.getUTCFullYear();
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const day = date.getUTCDate().toString().padStart(2, "0");
+  const hours = date.getUTCHours().toString().padStart(2, "0");
+  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+  const seconds = date.getUTCSeconds().toString().padStart(2, "0");
+  return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+}
+
+// Config for Bos app
+function getConfig(network) {
+  switch (network) {
+    case "mainnet":
+      return {
+        ownerId: "inscribe.near",
+        graphUrl:
+          "https://api.thegraph.com/subgraphs/name/inscriptionnear/neat",
+        nodeUrl: "https://rpc.mainnet.near.org",
+        contractName: "inscription.near",
+        methodName: "inscribe",
+        args: {
+          p: "nrc-20",
+          op: "mint",
+          tick: "neat",
+          amt: "100000000",
+        },
+        transferArgs: {
+          p: "nrc-20",
+          op: "transfer",
+          tick: "neat",
+        },
+        ftWrapper: "neat.nrc-20.near",
+        refFinance: "https://app.ref.finance/",
+      };
+    case "testnet":
+      return {
+        ownerId: "inscribe.testnet",
+        graphUrl:
+          "https://api.thegraph.com/subgraphs/name/inscriptionnear/neat-test",
+        nodeUrl: "https://rpc.testnet.near.org",
+        contractName: "inscription.testnet",
+        methodName: "inscribe",
+        args: {
+          p: "nrc-20",
+          op: "mint",
+          tick: "neat",
+          amt: "100000000",
+        },
+        transferArgs: {
+          p: "nrc-20",
+          op: "transfer",
+          tick: "neat",
+        },
+        ftWrapper: "neat.nrc-20.testnet",
+        refFinance: "https://testnet.ref-finance.com/",
+      };
+    default:
+      throw Error(`Unconfigured environment '${network}'.`);
+  }
+}
+const config = getConfig(context.networkId);
+const tx = {
+  contractName: config.contractName,
+  methodName: config.methodName,
+  args: config.args,
+  gas: GasPerTransaction,
+};
+
+
+
+const token = props.token;
+
+State.init({
+  tokenInfo: undefined,
+});
+fetchTokenInfoAsync(token).then((response) => {
+  State.update({
+    tokenInfo: response.tokenInfo,
+    holderCount: response.holderCount,
+  });
+});
+
+const tokenInfo = state.tokenInfo;
+const holderCount = state.holderCount;
+const decimals = tokenInfo?.decimals ?? 0;
+
+function shortNearAddress(accountId) {
+  if (accountId && accountId.length > 21) {
+    const splits = accountId.split(".");
+    const prefix =
+      accountId.substring(0, 5) +
+      "..." +
+      splits[0].substring(splits[0].length - 5);
+    if (accountId.includes(".")) {
+      return prefix + "." + splits.splice(1).join(".");
+    } else return prefix;
+  } else {
+    return accountId;
+  }
+}
+
+return (
+  <FormFragment>
+    <FormContainer>
+      <FormTitle style={{ fontWeight: "bold" }}>{tokenInfo.ticker}</FormTitle>
+      <FormBody>
+        <FormRowContainer>
+          <FormRowTitle>Total Supply</FormRowTitle>
+          <FormRowValue>
+            {formatAmount(tokenInfo?.maxSupply, decimals)}
+          </FormRowValue>
+        </FormRowContainer>
+        <FormRowContainer>
+          <FormRowTitle>Minted</FormRowTitle>
+          <FormRowValue>
+            {formatAmount(tokenInfo?.totalSupply, decimals)}
+          </FormRowValue>
+        </FormRowContainer>
+        <FormRowContainer>
+          <FormRowTitle>Limit Per Mint</FormRowTitle>
+          <FormRowValue>
+            {tokenInfo?.limit
+              ? formatAmount(tokenInfo.limit, decimals)
+              : "No Limit"}
+          </FormRowValue>
+        </FormRowContainer>
+        <FormRowContainer>
+          <FormRowTitle>Decimals</FormRowTitle>
+          <FormRowValue>{tokenInfo?.decimals}</FormRowValue>
+        </FormRowContainer>
+        <FormRowContainer>
+          <FormRowTitle>Deployed By</FormRowTitle>
+          <FormRowValue title={tokenInfo?.creatorId}>
+            {shortNearAddress(tokenInfo?.creatorId)}
+          </FormRowValue>
+        </FormRowContainer>
+        <FormRowContainer>
+          <FormRowTitle>Deploy Time</FormRowTitle>
+          <FormRowValue>
+            {tokenInfo?.createdBlockTimestamp
+              ? formatDeployTime(tokenInfo.createdBlockTimestamp)
+              : "-"}
+          </FormRowValue>
+        </FormRowContainer>
+        <FormRowContainer>
+          <FormRowTitle>Holders</FormRowTitle>
+          <FormRowValue>
+            {formatAmount(holderCount?.count ?? 0, 0)}
+          </FormRowValue>
+        </FormRowContainer>
+      </FormBody>
+    </FormContainer>
+    <FormContainer>
+      <FormTitle>Minted address rank</FormTitle>
+      <TipText>* Only top 5000 addresses are listed.</TipText>
+      <FormBody>
+        <Widget
+          src={`${config.ownerId}/widget/NRC-20.IndexTable`}
+          props={{
+            searchValue: state.searchValue,
+            token: props.token,
+          }}
+        />
+      </FormBody>
+    </FormContainer>
+  </FormFragment>
+);
