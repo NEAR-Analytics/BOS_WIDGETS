9c50658ad29dac8e9b4036b1556e58ea69b7bcd4
itexpert120-contra.near <itexpert120-contra.near>
2023-12-29 08:26:28 +0000

    Update devhub.entity.addon.kanban.Viewer by itexpert120-contra.near at 2023-12-29T08:26:28.446Z

--- itexpert120-contra.near/devhub.entity.addon.kanban.Viewer/source_code.jsx
+++ itexpert120-contra.near/devhub.entity.addon.kanban.Viewer/source_code.jsx
@@ -1,4 +1,4 @@
-const { href } = VM.require("itexpert120-contra.nera/widget/core.lib.url");
+const { href } = VM.require("itexpert120-contra.near/widget/core.lib.url");
 
 href || (href = () => {});
 
@@ -8,12 +8,12 @@ const { metadata, payload, handle, permissions } = props;
 const CommunityBoardPage = ({ handle, permissions }) => {
   return (
     <Widget
-      src="itexpert120-contra.nera/widget/devhub.entity.addon.kanban.configurator"
+      src="itexpert120-contra.near/widget/devhub.entity.addon.kanban.configurator"
       props={{
         communityHandle: handle, // rather than fetching again via the handle
         link: href({
           // do we need a link?
-          widgetSrc: "itexpert120-contra.nera/widget/app",
+          widgetSrc: "itexpert120-contra.near/widget/app",
           params: { page: "community", handle },
         }),
         permissions,
