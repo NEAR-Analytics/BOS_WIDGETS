80d30a70af9f26d121f1a2596af484f419c56991
itexpert120-contra.near <itexpert120-contra.near>
2023-12-29 07:59:54 +0000

    Update devhub.components.island.explore by itexpert120-contra.near at 2023-12-29T07:59:54.611Z

--- itexpert120-contra.near/devhub.components.island.explore/source_code.jsx
+++ itexpert120-contra.near/devhub.components.island.explore/source_code.jsx
@@ -181,7 +181,7 @@ const Content = (
 
 return (
   <Widget
-    src="itexpert120-contra.near/widget/devhub.components.island.home-section"
+    src="itexpert120-contra.nera/widget/devhub.components.island.home-section"
     props={{
       title: "/explore",
       titleColor: "#555555",
