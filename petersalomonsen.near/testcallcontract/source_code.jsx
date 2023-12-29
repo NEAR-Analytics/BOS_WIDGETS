e3d8dbcd69b0b1783b4a73f4d4be54a53eb9c198
petersalomonsen.near <petersalomonsen.near>
2023-12-29 18:15:30 +0000

    Update testcallcontract by petersalomonsen.near at 2023-12-29T18:15:30.382Z

--- petersalomonsen.near/testcallcontract/source_code.jsx
+++ petersalomonsen.near/testcallcontract/source_code.jsx
@@ -1,13 +1,17 @@
 const [result, setResult] = useState(null);
-const handleClick = () => {
-  const transactionResult = Near.call("psalomo.near", "test");
-  console.log(transactionResult);
-  setResult(transactionResult);
+const handleClick = async () => {
+  try {
+    const transactionResult = Near.call("psalomo.near", "test");
+    console.log(transactionResult);
+    setResult(JSON.stringify(transactionResult));
+  } catch (e) {
+    setResult(JSON.stringify(e));
+  }
 };
 
 return (
   <>
     <button onClick={handleClick}>Call contract</button>
-    <div>Hello World {JSON.stringify(result)}</div>
+    <div>Hello World {result}</div>
   </>
 );
