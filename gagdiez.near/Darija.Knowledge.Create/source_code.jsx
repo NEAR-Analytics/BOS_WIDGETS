681c5bc25e5f1ab8536e9af636f31fcc0be3e324
gagdiez.near <gagdiez.near>
2023-12-30 14:02:03 +0000

    Update Darija.Knowledge.Create by gagdiez.near at 2023-12-30T14:02:03.825Z

--- gagdiez.near/Darija.Knowledge.Create/source_code.jsx
+++ gagdiez.near/Darija.Knowledge.Create/source_code.jsx
@@ -1,40 +1,19 @@
-const known = [
-  {
-    darija: "smiya",
-    spanish: "nombre",
-    emoji: "💬",
-  },
-  {
-    darija: "ktab",
-    spanish: "libro",
-    emoji: "📖",
-  },
-  {
-    darija: "medina",
-    spanish: "ciudad",
-    emoji: "🏙️",
-  },
-  {
-    darija: "tilifun",
-    spanish: "telefono",
-    emoji: "📞",
-  },
-  {
-    darija: "mesha",
-    spanish: "gata",
-    emoji: "🐱",
-  },
-];
+if(context.loading) return 'Loading ...';
 
-const [darija, setDarija] = useState("");
-const [spanish, setSpanish] = useState("");
-const [emoji, setEmoji] = useState("");
-const [knowledge, setKnowledge] = useState(known);
-const [hideInput, setHideInput] = useState(known.map(() => true));
-const [title, setTitle] = useState("");
+const uuid = props.uuid;
+let inKnown = [];
+let inTitle = '';
+
+if (uuid) {
+  const retrieved = JSON.parse(Social.get('gagdiez.near/knowledge', uuid));
+  inTitle = retrieved.title;
+  inKnown = retrieved.knowledge;
+}
+
+const [title, setTitle] = useState(inTitle);
+const [knowledge, setKnowledge] = useState(inKnown);
 
 const submit = () => {
-  const uuid = Date.now();
   Social.set({
     knowledge: JSON.stringify({
       title, knowledge
@@ -42,118 +21,32 @@ const submit = () => {
     index: {
       knowledge: JSON.stringify({
         key: 'darija',
-        value: uuid
+        value: { uuid, title }
       })
     }
   })
 }
 
-const change = (index, field, value) => {
-  const newKnowledge = [...knowledge];
-  newKnowledge[index][field] = value;
-  setKnowledge(newKnowledge);
-};
-
-const remove = (index) => {
-  const newKnowledge = [...knowledge];
-  newKnowledge.splice(index, 1);
-  setKnowledge(newKnowledge);
-};
-
-const add = () => {
-  if (!darija || !spanish || !emoji) return;
-
-  const newKnowledge = [...knowledge];
-  newKnowledge.push({
-    darija,
-    spanish,
-    emoji,
-  });
-  setKnowledge(newKnowledge);
-  setDarija('');
-  setSpanish('');
-  setEmoji('');
-  setHideInput([...hideInput, true]);
-};
-
 return (
   <>
-    <h3><input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />  </h3>
+    <div class="mb-3">
+      <label for="title">Title</label>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+    </div>
+    <Widget
+      src="gagdiez.near/widget/Darija.Components.Table"
+      props={{
+        elements: knowledge,
+        keys: ["darija", "spanish", "emoji"],
+        editors: {
+          darija: { type: "text" },
+          spanish: { type: "text" },
+          emoji: { type: "text" },
+        },
+        onUpdate: setKnowledge,
+      }}
+    />
 
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Darija</th>
-          <th scope="col">Español</th>
-          <th scope="col">Emoji</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {knowledge.map((elem, index) => {
-          return (
-            <tr>
-              <th scope="row">{index + 1}</th>
-              <td>
-                {hideInput[index] && elem.darija}
-                <input
-                  value={elem.darija}
-                  onChange={(e) => change(index, "darija", e.target.value)}
-                  hidden={hideInput[index]}
-                />
-              </td>
-              <td>
-                {hideInput[index] && elem.spanish}
-                <input
-                  value={elem.spanish}
-                  onChange={(e) => change(index, "spanish", e.target.value)}
-                  hidden={hideInput[index]}
-                />
-              </td>
-              <td>
-                {hideInput[index] && elem.emoji}
-                <input
-                  value={elem.emoji}
-                  onChange={(e) => change(index, "emoji", e.target.value)}
-                  hidden={hideInput[index]}
-                />
-              </td>
-              <td>
-                <a
-                  className="me-1"
-                  onClick={() => {
-                    setHideInput(
-                      hideInput.map((elem, i) => (i === index ? !elem : elem))
-                    );
-                  }}
-                >
-                  ✏️
-                </a>
-                <a onClick={() => remove(index)}>🗑️</a>
-              </td>
-            </tr>
-          );
-        })}
-        <tr>
-          <th scope="row">#</th>
-          <td>
-            <input type="text" onChange={(e) => setDarija(e.target.value)} />
-          </td>
-          <td>
-            <input type="text" onChange={(e) => setSpanish(e.target.value)} />
-          </td>
-          <td>
-            <input type="text" onChange={(e) => setEmoji(e.target.value)} />
-          </td>
-          <td>
-            <button class="btn btn-primary" onClick={add}>Add</button>
-          </td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
-
-    <button class="btn btn-primary" onClick={submit}>Submit</button>
+    <button class="btn btn-success align-right" onClick={submit}>Submit</button>
   </>
 );
