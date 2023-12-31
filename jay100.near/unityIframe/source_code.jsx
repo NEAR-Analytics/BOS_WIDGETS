df3c24dbf3bdfe75caf73ce20150dbff5a0fc882
jay100.near <jay100.near>
2023-12-31 18:16:24 +0000

    Update unityIframe by jay100.near at 2023-12-31T18:16:24.873Z

--- jay100.near/unityIframe/source_code.jsx
+++ jay100.near/unityIframe/source_code.jsx
@@ -53,8 +53,8 @@ function update ()
   return (
     <div
       style={{
-        width: "100vw",
-        height: "100vh",
+        width: "100%",
+        height: "100%",
         display: "flex",
         flexDirection: "column",
       }}
