568bb8348d9ff228a843014e75162c96ff20c4c6
jay100.near <jay100.near>
2023-12-31 18:02:13 +0000

    Update Phaser Test by jay100.near at 2023-12-31T18:02:13.482Z

--- jay100.near/Phaser Test/source_code.jsx
+++ jay100.near/Phaser Test/source_code.jsx
@@ -19,51 +19,33 @@ const GameContainer = () => {
 <body>
 </body>
 <script>
-    class Example extends Phaser.Scene
-    {
-        preload ()
-        {
-            this.load.setBaseURL('https://labs.phaser.io');
-
-            this.load.image('sky', 'assets/skies/space3.png');
-            this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-            this.load.image('red', 'assets/particles/red.png');
-        }
-
-        create ()
-        {
-            this.add.image(400, 300, 'sky');
-
-            const particles = this.add.particles(0, 0, 'red', {
-                speed: 100,
-                scale: { start: 1, end: 0 },
-                blendMode: 'ADD'
-            });
-
-            const logo = this.physics.add.image(400, 100, 'logo');
-
-            logo.setVelocity(100, 200);
-            logo.setBounce(1, 1);
-            logo.setCollideWorldBounds(true);
-
-            particles.startFollow(logo);
-        }
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
+};
 
-    const config = {
-        type: Phaser.AUTO,
-        width: window.innerWidth,
-        height: window.innerHeight,
-        scene: Example,
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: { y: 200 }
-            }
-        }
-    };
+var game = new Phaser.Game(config);
 
-    const game = new Phaser.Game(config);
+function preload ()
+{
+
+    this.load.image('sky', 'https://cdn2.vectorstock.com/i/1000x1000/12/86/sky-day-game-background-vector-21861286.jpg');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+}
+
+function update ()
+{
+}
 </script>
   `;
 
