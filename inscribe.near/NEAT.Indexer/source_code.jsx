b12c3e91cbaa947ba6aa283c07fa6c3cd3dcfdeb
inscribe.near <inscribe.near>
2024-01-01 05:43:43 +0000

    Update NEAT.Indexer by inscribe.near at 2024-01-01T05:43:43.769Z

--- inscribe.near/NEAT.Indexer/source_code.jsx
+++ inscribe.near/NEAT.Indexer/source_code.jsx
@@ -224,7 +224,7 @@ function fetchTokenInfosAsync() {
         createdBlockTimestamp
         decimals
       }
-      holderCounts {
+      holderCounts(first: 1000) {
         ticker
         count
       }
