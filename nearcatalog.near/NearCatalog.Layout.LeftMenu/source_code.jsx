7c6d35a876b612a55ba92032faabf59716b34b80
nearcatalog.near <nearcatalog.near>
2024-01-01 11:02:02 +0000

    Update NearCatalog.Layout.LeftMenu by nearcatalog.near at 2024-01-01T11:02:02.279Z

--- nearcatalog.near/NearCatalog.Layout.LeftMenu/source_code.jsx
+++ nearcatalog.near/NearCatalog.Layout.LeftMenu/source_code.jsx
@@ -1,71 +1,73 @@
 
-var path = props.componentPath;
+var path = props.indexPath;
 return (
     <>
         {/* the menu */}
         <div className="awesome-aside-menu menu menu-nav">
-            <div className="menu-parent"><a className="btn btn-lg active" href={`/${path}.App`}>All<span className="menu-badge">1093</span></a></div>
-            <div className="menu-parent"><a className="btn btn-lg" href={`/${path}.App?cat=aurora`}>Aurora
-                <span className="menu-badge">283</span></a></div>
-            <div className="menu-parent"><a className="btn btn-lg " href={`/${path}.App?cat=octopus`}>Octopus<span className="menu-badge">20</span></a></div>
+            <div className="menu-parent"><a className="btn btn-lg active" href={`/${path}`}>All<span className="menu-badge d-none">1093</span></a></div>
+            <div className="menu-parent"><a className="btn btn-lg " href={`/${path}?cat=comunity`}>Community</a></div>
             <div className="menu-parent">
-                <a className="btn btn-lg " href={`/${path}.App?cat=infrastructure`}>Infrastructure<span className="menu-badge">251</span></a>
+                <a className="btn btn-lg " href={`/${path}?cat=dapps`}>DApps<span className="menu-badge d-none">488</span></a>
                 <div className="menu menu-nav">
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=wallets`}>Wallets<span className="menu-badge">61</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=validators`}>Validators<span className="menu-badge">70</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=explorers`}>Explorers<span className="menu-badge">11</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=bridges`}>Bridges<span className="menu-badge">33</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=storage`}>Storage<span className="menu-badge">13</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=defi`}>DeFi<span className="menu-badge d-none">209</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=dex`}>DEX<span className="menu-badge d-none">47</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=exchanges`}>Exchanges<span className="menu-badge d-none">63</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=launchpads`}>Launchpads<span className="menu-badge d-none">20</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=lending`}>Lending<span className="menu-badge d-none">20</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=stablecoins`}>Stablecoins<span className="menu-badge d-none">14</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=yield_aggregators`}>Yield Aggregators<span className="menu-badge d-none">17</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=gaming`}>Gaming<span className="menu-badge d-none">129</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=metaverse`}>Metaverse<span className="menu-badge d-none">32</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=social`}>Social<span className="menu-badge d-none">52</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=music`}>Music<span className="menu-badge d-none">18</span></a></div>
                 </div>
             </div>
             <div className="menu-parent">
-                <a className="btn btn-lg " href={`/${path}.App?cat=dapps`}>DApps<span className="menu-badge">488</span></a>
+                <a className="btn btn-lg " href={`/${path}?cat=ecosystem`}>Ecosystem<span className="menu-badge d-none">132</span></a>
                 <div className="menu menu-nav">
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=defi`}>DeFi<span className="menu-badge">209</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=dex`}>DEX<span className="menu-badge">47</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=exchanges`}>Exchanges<span className="menu-badge">63</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=launchpads`}>Launchpads<span className="menu-badge">20</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=lending`}>Lending<span className="menu-badge">20</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=stablecoins`}>Stablecoins<span className="menu-badge">14</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=yield_aggregators`}>Yield Aggregators<span className="menu-badge">17</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=gaming`}>Gaming<span className="menu-badge">129</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=metaverse`}>Metaverse<span className="menu-badge">32</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=social`}>Social<span className="menu-badge">52</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=music`}>Music<span className="menu-badge">18</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=dao`}>DAO<span className="menu-badge d-none">94</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=regional_hubs`}>Regional Hubs<span className="menu-badge d-none">6</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=funding`}>Funding<span className="menu-badge d-none">17</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=guilds`}>Guilds<span className="menu-badge d-none">44</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=education`}>Education<span className="menu-badge d-none">30</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=refi`}>ReFi<span className="menu-badge d-none">10</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=events`}>Events<span className="menu-badge d-none">7</span></a></div>
+                </div>
+            </div>
+            <div className="menu-parent"><a className="btn btn-lg" href={`/${path}?cat=aurora`}>Aurora<span className="menu-badge d-none">283</span></a></div>
+            <div className="menu-parent"><a className="btn btn-lg " href={`/${path}?cat=octopus`}>Octopus<span className="menu-badge d-none">20</span></a></div>
+            <div className="menu-parent">
+                <a className="btn btn-lg " href={`/${path}?cat=infrastructure`}>Infrastructure<span className="menu-badge d-none">251</span></a>
+                <div className="menu menu-nav">
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=wallets`}>Wallets<span className="menu-badge d-none">61</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=validators`}>Validators<span className="menu-badge d-none">70</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=explorers`}>Explorers<span className="menu-badge d-none">11</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=bridges`}>Bridges<span className="menu-badge d-none">33</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=storage`}>Storage<span className="menu-badge d-none">13</span></a></div>
+                </div>
+            </div>
+            
+            <div className="menu-parent">
+                <a className="btn btn-lg " href={`/${path}?cat=nft`}>NFT<span className="menu-badge d-none">290</span></a>
+                <div className="menu menu-nav">
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=marketplaces`}>Marketplaces<span className="menu-badge d-none">60</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=collectibles`}>Collectibles<span className="menu-badge d-none">99</span></a></div>
                 </div>
             </div>
             <div className="menu-parent">
-                <a className="btn btn-lg " href={`/${path}.App?cat=nft`}>NFT<span className="menu-badge">290</span></a>
+                <a className="btn btn-lg " href={`/${path}?cat=utilities`}>Utilities<span className="menu-badge d-none">98</span></a>
                 <div className="menu menu-nav">
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=marketplaces`}>Marketplaces<span className="menu-badge">60</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=collectibles`}>Collectibles<span className="menu-badge">99</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=devtooling`}>Dev Tooling<span className="menu-badge d-none">124</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=oracles`}>Oracles<span className="menu-badge d-none">15</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=security`}>Security<span className="menu-badge d-none">13</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=data`}>Data<span className="menu-badge d-none">21</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=analytics`}>Analytics<span className="menu-badge d-none">41</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=portfolio`}>Portfolio<span className="menu-badge d-none">21</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=on_off_ramps`}>On/Off Ramps<span className="menu-badge d-none">16</span></a></div>
+                    <div className="menu-item"><a className="btn" href={`/${path}?cat=payments`}>Payments<span className="menu-badge d-none">30</span></a></div>
                 </div>
             </div>
-            <div className="menu-parent">
-                <a className="btn btn-lg " href={`/${path}.App?cat=utilities`}>Utilities<span className="menu-badge">98</span></a>
-                <div className="menu menu-nav">
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=devtooling`}>Dev Tooling<span className="menu-badge">124</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=oracles`}>Oracles<span className="menu-badge">15</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=security`}>Security<span className="menu-badge">13</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=data`}>Data<span className="menu-badge">21</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=analytics`}>Analytics<span className="menu-badge">41</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=portfolio`}>Portfolio<span className="menu-badge">21</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=on_off_ramps`}>On/Off Ramps<span className="menu-badge">16</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=payments`}>Payments<span className="menu-badge">30</span></a></div>
-                </div>
-            </div>
-            <div className="menu-parent">
-                <a className="btn btn-lg " href={`/${path}.App?cat=ecosystem`}>Ecosystem<span className="menu-badge">132</span></a>
-                <div className="menu menu-nav">
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=dao`}>DAO<span className="menu-badge">94</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=regional_hubs`}>Regional Hubs<span className="menu-badge">6</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=funding`}>Funding<span className="menu-badge">17</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=guilds`}>Guilds<span className="menu-badge">44</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=education`}>Education<span className="menu-badge">30</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=refi`}>ReFi<span className="menu-badge">10</span></a></div>
-                    <div className="menu-item"><a className="btn" href={`/${path}.App?cat=events`}>Events<span className="menu-badge">7</span></a></div>
-                </div>
-            </div>
+            
         </div>
         {/* end the menu */}
     </>
