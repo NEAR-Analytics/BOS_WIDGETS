1d057e6d2144c5ac5477d5856a70ae7bdc4ac8f9
mayurmc.near <mayurmc.near>
2024-01-01 23:16:13 +0000

    Update vistara by mayurmc.near at 2024-01-01T23:16:13.648Z

---
+++ mayurmc.near/vistara/source_code.jsx
@@ -0,0 +1,9 @@
+return (
+  <div className="w-100" style={{ height: "100vh" }}>
+    <iframe
+      sandbox="allow-top-navigation-by-user-activation"
+      className="w-100 h-100"
+      src="https://ipfs.io/ipfs/QmbQYEFVxA7FKkUZX3GK5Qgq58U19zRry7JtMbrRXWKKZV?filename=index.html"
+    />
+  </div>
+);
