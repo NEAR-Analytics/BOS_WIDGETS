fe43d64ab1e1c69411d4e29f5cfe1a7529041e9b
mayurmc.near <mayurmc.near>
2024-01-01 19:35:36 +0000

    Update vistara by mayurmc.near at 2024-01-01T19:35:36.868Z

--- mayurmc.near/vistara/source_code.jsx
+++ mayurmc.near/vistara/source_code.jsx
@@ -1,15 +1,9 @@
-
 return (
-  <div
-    className="w-100"
-    style={{ height: '100vh' }}
-  >
+  <div className="w-100" style={{ height: "100vh" }}>
     <iframe
       sandbox="allow-top-navigation-by-user-activation"
       className="w-100 h-100"
-
-      src="https://aqua-biological-ape-674.mypinata.cloud/ipfs/QmSdNUM1pK1Q4hNXHeqE9PahAgbj7HZ1on4Mimacb22S8t?_gl=1*92fhcz*_ga*MTg0MDE5NjE1Mi4xNzAxNDU1Mzk1*_ga_5RMPXG14TE*MTcwMTQ1NTM5Ny4xLjEuMTcwMTQ1NjU2NC4yLjAuMA.."
+      src="https://aqua-biological-ape-674.mypinata.cloud/ipfs/QmdUnMxXPwp7ZbDigDqQ8iydz5MGnBiF4bTFrCojGxpe4N?_gl=1*1920wix*_ga*MTg0MDE5NjE1Mi4xNzAxNDU1Mzk1*_ga_5RMPXG14TE*MTcwNDEzNzM0NS40LjEuMTcwNDEzNzQxNi42MC4wLjA."
     />
-
   </div>
 );
