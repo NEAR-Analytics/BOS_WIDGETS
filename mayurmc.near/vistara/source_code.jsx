5c85cd99c71af9bd32c07313d419350d5af7cb7d
mayurmc.near <mayurmc.near>
2024-01-02 19:36:36 +0000

    Update vistara by mayurmc.near at 2024-01-02T19:36:36.625Z

---
+++ mayurmc.near/vistara/source_code.jsx
@@ -0,0 +1,9 @@
+return (
+  <div className="w-100" style={{ height: "100vh" }}>
+    <iframe
+      sandbox="allow-top-navigation-by-user-activation"
+      className="w-100 h-100"
+      src="https://aqua-biological-ape-674.mypinata.cloud/ipfs/QmUQrBhJLuJd3nuvbDhTe7h4BcAAawFknZy2xegiUVXoJm"
+    />
+  </div>
+);
