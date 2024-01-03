9285425862d3fecd54d8e8c05a0de7bb22552c41
b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e <b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e>
2024-01-03 21:59:48 +0000

    Update dsfas by b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e at 2024-01-03T21:59:48.521Z

--- b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e/dsfas/source_code.jsx
+++ b8308b11a3ebba599efff37a42d179fdb1ad3796e8fcf15b704a8915188c712e/dsfas/source_code.jsx
@@ -1,14 +1,3 @@
 return (
-  <div>
-    <h1>About Me</h1>
-
-    <div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="javascript:alert(localStorage.getItem('near_app_wallet_auth_key'))">
-        <a>My Blog Post</a>
-      </Link>
-    </div>
-  </div>
+  <h1>Hello World!</h1>
 );
