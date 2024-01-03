519d0b0489638a584a72a7c1721e43b7825da547
kevin0.near <kevin0.near>
2024-01-03 22:29:02 +0000

    Update LaserChess3D by kevin0.near at 2024-01-03T22:29:02.988Z

---
+++ kevin0.near/LaserChess3D/source_code.jsx
@@ -0,0 +1,8 @@
+return (
+  <h1>
+    Redeployed to{" "}
+    <a href="https://near.org/ostolex.near/widget/LaserChess3D">
+      https://near.org/ostolex.near/widget/LaserChess3D
+    </a>
+  </h1>
+);
