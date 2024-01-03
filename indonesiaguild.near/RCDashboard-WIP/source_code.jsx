fb63c373527bf3fd8d5d4b73da50ab92c4d15c37
indonesiaguild.near <indonesiaguild.near>
2024-01-03 02:11:30 +0000

    Update RCDashboard-WIP by indonesiaguild.near at 2024-01-03T02:11:30.327Z

--- indonesiaguild.near/RCDashboard-WIP/source_code.jsx
+++ indonesiaguild.near/RCDashboard-WIP/source_code.jsx
@@ -261,30 +261,31 @@ const widgetRank = (members) => {
 
   const starCountsByWidget = {};
 
-  Object.keys(data).forEach((user) => {
-    const userData = data[user];
-    const widgetData = userData?.graph?.star;
+  if (data)
+    Object.keys(data).forEach((user) => {
+      const userData = data[user];
+      const widgetData = userData?.graph?.star;
 
-    if (widgetData) {
-      members.map((widgetCreator) => {
-        const widgetList = widgetData[widgetCreator]?.widget;
+      if (widgetData) {
+        members.map((widgetCreator) => {
+          const widgetList = widgetData[widgetCreator]?.widget;
 
-        if (widgetList) {
-          Object.keys(widgetList).forEach((widgetName) => {
-            const widgetPath = `${widgetCreator}/widget/${widgetName}`;
+          if (widgetList) {
+            Object.keys(widgetList).forEach((widgetName) => {
+              const widgetPath = `${widgetCreator}/widget/${widgetName}`;
 
-            if (!starCountsByWidget[widgetPath]) {
-              starCountsByWidget[widgetPath] = 0;
-            }
+              if (!starCountsByWidget[widgetPath]) {
+                starCountsByWidget[widgetPath] = 0;
+              }
 
-            if (typeof widgetList[widgetName] !== "undefined") {
-              starCountsByWidget[widgetPath]++;
-            }
-          });
-        }
-      });
-    }
-  });
+              if (typeof widgetList[widgetName] !== "undefined") {
+                starCountsByWidget[widgetPath]++;
+              }
+            });
+          }
+        });
+      }
+    });
 
   const rankedWidgets = Object.entries(starCountsByWidget)
     .filter(([widgetName, totalStars]) => typeof totalStars !== "undefined")
