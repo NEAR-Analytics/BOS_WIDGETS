20fe7312ed5c0ed0296354fe4b8b888619d1d7c1
bwe-demos.near <bwe-demos.near>
2024-01-03 19:47:33 +0000

    Update Posts.Sidebar by bwe-demos.near at 2024-01-03T19:47:33.815Z

--- bwe-demos.near/Posts.Sidebar/source_code.jsx
+++ bwe-demos.near/Posts.Sidebar/source_code.jsx
@@ -1,12 +1,14 @@
-return (
-  <div
-    style={{
-      backgroundColor: "#e7e7e7",
-      flex: "0 0",
-      width: "15rem",
-      height: "100%",
-    }}
-  >
-    Sidebar
-  </div>
-);
+export function BWEComponent() {
+  return (
+    <div
+      style={{
+        backgroundColor: '#e7e7e7',
+        flex: '0 0',
+        width: '15rem',
+        height: '100%',
+      }}
+    >
+      Sidebar
+    </div>
+  );
+}
