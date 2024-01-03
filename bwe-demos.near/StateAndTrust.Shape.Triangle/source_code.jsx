4c38dd5a387ab5877b4e4ffb4f0ad61a1a368e4f
bwe-demos.near <bwe-demos.near>
2024-01-03 19:47:33 +0000

    Update StateAndTrust.Shape.Triangle by bwe-demos.near at 2024-01-03T19:47:33.815Z

--- bwe-demos.near/StateAndTrust.Shape.Triangle/source_code.jsx
+++ bwe-demos.near/StateAndTrust.Shape.Triangle/source_code.jsx
@@ -1,23 +1,33 @@
-return (
-  <div
-    onClick={props.onClick}
-    style={{
-      width: 0,
-      height: 0,
-      borderLeft: `${props.height / 2}px solid transparent`,
-      borderRight: `${props.height / 2}px solid transparent`,
-      borderBottom: `${props.height}px solid ${props.color}`,
-      textAlign: "center",
-    }}
-  >
-    <i
-      className={`bi-${props.icon}`}
+interface Props {
+  color: string;
+  height: number;
+  icon: string;
+  iconColor: string;
+  onClick: () => {};
+}
+
+export function BWEComponent(props: Props) {
+  return (
+    <div
+      onClick={props.onClick}
       style={{
-        color: props.iconColor,
-        position: "relative",
-        top: "calc(50% + 34px)",
-        right: "calc(50% + 9px)",
+        width: 0,
+        height: 0,
+        borderLeft: `${props.height / 2}px solid transparent`,
+        borderRight: `${props.height / 2}px solid transparent`,
+        borderBottom: `${props.height}px solid ${props.color}`,
+        textAlign: 'center',
       }}
-    />
-  </div>
-);
+    >
+      <i
+        className={`bi-${props.icon}`}
+        style={{
+          color: props.iconColor,
+          position: 'relative',
+          top: 'calc(50% + 34px)',
+          right: 'calc(50% + 9px)',
+        }}
+      />
+    </div>
+  );
+}
