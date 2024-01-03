5e3e39bd1b2bcc51f1cd5b6b0f79c7b971294ccb
nearblocks.near <nearblocks.near>
2024-01-03 15:44:10 +0000

    Update bos-components.components.Blocks.Latest by nearblocks.near at 2024-01-03T15:44:10.126Z

--- nearblocks.near/bos-components.components.Blocks.Latest/source_code.jsx
+++ nearblocks.near/bos-components.components.Blocks.Latest/source_code.jsx
@@ -21,7 +21,15 @@ function convertToMetricPrefix(number) {
     count++;
   }
 
-  return number.toFixed(2) + ' ' + prefixes[count];
+  // Check if the number is close to an integer value
+  if (Math.abs(number) >= 10) {
+    number = Math.round(number); // Round the number to the nearest whole number
+    return number + ' ' + prefixes[count];
+  }
+
+  return (
+    Number(Math.floor(number * 100) / 100).toFixed(2) + ' ' + prefixes[count]
+  );
 }
 
 function gasFee(gas, price) {
@@ -130,7 +138,7 @@ function shortenTokenSymbol(token) {
 function gasPercentage(gasUsed, gasAttached) {
   if (!gasAttached) return 'N/A';
 
-  const formattedNumber = (Big(gasUsed).div(Big(gasAttached)) * 100).toFixed();
+  const formattedNumber = (Big(gasUsed).div(Big(gasAttached)) * 100).toFixed(2);
   return `${formattedNumber}%`;
 }
 function serialNumber(index, page, perPage) {
@@ -227,7 +235,15 @@ function convertToMetricPrefix(number) {
     count++;
   }
 
-  return number.toFixed(2) + ' ' + prefixes[count];
+  // Check if the number is close to an integer value
+  if (Math.abs(number) >= 10) {
+    number = Math.round(number); // Round the number to the nearest whole number
+    return number + ' ' + prefixes[count];
+  }
+
+  return (
+    Number(Math.floor(number * 100) / 100).toFixed(2) + ' ' + prefixes[count]
+  );
 }
 
 function gasFee(gas, price) {
@@ -336,7 +352,7 @@ function shortenTokenSymbol(token) {
 function gasPercentage(gasUsed, gasAttached) {
   if (!gasAttached) return 'N/A';
 
-  const formattedNumber = (Big(gasUsed).div(Big(gasAttached)) * 100).toFixed();
+  const formattedNumber = (Big(gasUsed).div(Big(gasAttached)) * 100).toFixed(2);
   return `${formattedNumber}%`;
 }
 function serialNumber(index, page, perPage) {
@@ -535,7 +551,15 @@ function convertToMetricPrefix(number) {
     count++;
   }
 
-  return number.toFixed(2) + ' ' + prefixes[count];
+  // Check if the number is close to an integer value
+  if (Math.abs(number) >= 10) {
+    number = Math.round(number); // Round the number to the nearest whole number
+    return number + ' ' + prefixes[count];
+  }
+
+  return (
+    Number(Math.floor(number * 100) / 100).toFixed(2) + ' ' + prefixes[count]
+  );
 }
 
 function gasFee(gas, price) {
@@ -644,7 +668,7 @@ function shortenTokenSymbol(token) {
 function gasPercentage(gasUsed, gasAttached) {
   if (!gasAttached) return 'N/A';
 
-  const formattedNumber = (Big(gasUsed).div(Big(gasAttached)) * 100).toFixed();
+  const formattedNumber = (Big(gasUsed).div(Big(gasAttached)) * 100).toFixed(2);
   return `${formattedNumber}%`;
 }
 function serialNumber(index, page, perPage) {
