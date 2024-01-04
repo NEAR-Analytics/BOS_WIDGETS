b75d8de173bc16fb2c3838fd95950a68adad571e
aaron.near <aaron.near>
2024-01-04 13:17:43 +0000

    Update Button-LightGrey by aaron.near at 2024-01-04T13:17:43.020Z

---
+++ aaron.near/Button-LightGrey/source_code.jsx
@@ -0,0 +1,42 @@
+const Button = styled.a`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 24px;
+  background: #f8f9fa;
+  overflow: hidden;
+  border-radius: 6px;
+  border: 1px solid #e9ecef;
+  gap: 8px;
+  display: inline-flex;
+  text-align: center;
+  color: #212529;
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: 600;
+
+  &:hover {
+    text-decoration: none;
+    cursor: ${props.disabled ? "not-allowed" : "pointer"};
+  }
+`;
+
+return (
+  <Button
+    target={props.target}
+    href={props.href}
+    onClick={(e) => {
+      if (props.disabled) {
+        e.preventDefault();
+        return;
+      }
+      if (props.onClick) {
+        props.onClick(e);
+      }
+    }}
+    disabled={props.disabled}
+    style={{ ...props.style }}
+  >
+    {props.text}
+  </Button>
+);
