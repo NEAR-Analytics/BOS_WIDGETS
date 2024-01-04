19399a6405ff9e34a71f18352734b66d485fd1d2
bluebiu.near <bluebiu.near>
2024-01-04 09:01:49 +0000

    Update Arbitrum.Swap.CurrencyBalance by bluebiu.near at 2024-01-04T09:01:49.803Z

--- bluebiu.near/Arbitrum.Swap.CurrencyBalance/source_code.jsx
+++ bluebiu.near/Arbitrum.Swap.CurrencyBalance/source_code.jsx
@@ -1,45 +1,51 @@
 const { address } = props;
-if (!address) return "";
 
-const account = Ethers.send("eth_requestAccounts", [])[0];
-if (!account) return "";
+useEffect(() => {
+  if (!address) return;
 
-if (address === "native") {
-  const provider = Ethers.provider();
-  provider.getBalance(account).then((rawBalance) => {
-    props?.onLoad(rawBalance._hex);
-  });
-  return "";
-}
-const TokenContract = new ethers.Contract(
-  address,
-  [
-    {
-      constant: true,
-      inputs: [
-        {
-          name: "_owner",
-          type: "address",
-        },
-      ],
-      name: "balanceOf",
-      outputs: [
-        {
-          name: "balance",
-          type: "uint256",
-        },
-      ],
-      payable: false,
-      stateMutability: "view",
-      type: "function",
-    },
-  ],
-  Ethers.provider().getSigner()
-);
-TokenContract.balanceOf(account)
-  .then((rawBalance) => {
-    props?.onLoad(rawBalance._hex);
-  })
-  .catch((err) => {});
+  const account = Ethers.send("eth_requestAccounts", [])[0];
+  if (!account) return;
+
+  if (address === "native") {
+    const provider = Ethers.provider();
+    provider.getBalance(account).then((rawBalance) => {
+      props?.onLoad(rawBalance._hex);
+    });
+    return;
+  }
+  const TokenContract = new ethers.Contract(
+    address,
+    [
+      {
+        constant: true,
+        inputs: [
+          {
+            name: "_owner",
+            type: "address",
+          },
+        ],
+        name: "balanceOf",
+        outputs: [
+          {
+            name: "balance",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    Ethers.provider().getSigner()
+  );
+
+  TokenContract.balanceOf(account)
+    .then((rawBalance) => {
+      props?.onLoad(rawBalance._hex);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}, [address]);
 
 return "";
