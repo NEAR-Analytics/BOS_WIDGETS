f1b97f03089c4cd5810bde10a06265c44ce0a295
dapdapbos.near <dapdapbos.near>
2024-01-04 14:14:23 +0000

    Update UI.Spinner by dapdapbos.near at 2024-01-04T14:14:23.652Z

---
+++ dapdapbos.near/UI.Spinner/source_code.jsx
@@ -0,0 +1,33 @@
+const Spinner = styled.div`
+  animation: loading 1s linear infinite;
+
+  @keyframes loading {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+return (
+  <Spinner>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <path
+        opacity="0.5"
+        d="M8 15C11.866 15 15 11.866 15 8C15 4.13401 11.866 1 8 1C4.13401 1 1 4.13401 1 8"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  </Spinner>
+);
