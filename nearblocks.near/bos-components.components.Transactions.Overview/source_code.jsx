99d39b2c3d9cfb56dfa97ed08514902c9bee3bb4
nearblocks.near <nearblocks.near>
2024-01-04 12:15:08 +0000

    Update bos-components.components.Transactions.Overview by nearblocks.near at 2024-01-04T12:15:08.096Z

--- nearblocks.near/bos-components.components.Transactions.Overview/source_code.jsx
+++ nearblocks.near/bos-components.components.Transactions.Overview/source_code.jsx
@@ -1,10 +1,10 @@
 /**
- * Component: Transactions Overview
+ * Component: TransactionsOverview
  * Author: Nearblocks Pte Ltd
  * License: Business Source License 1.1
  * Description: Transactions Overview.
  * @interface Props
- * @param {boolean} [fetchStyles] - Use Nearblock styles.
+ * @property {Function} t - A function for internationalization (i18n) provided by the next-translate package.
  * @param {string} [network] - The network data to show, either mainnet or testnet
  */
 
@@ -13,7 +13,7 @@
 
 
 
-/* INCLUDE COMPONENT: "includes/Common/Skelton.jsx" */
+/* INCLUDE COMPONENT: "includes/Common/Skeleton.jsx" */
 /**
  * @interface Props
  * @param {string} [className] - The CSS class name(s) for styling purposes.
@@ -23,13 +23,13 @@
 
 
 
-const Skelton = (props) => {
+const Skeleton = (props) => {
   return (
     <div
-      className={`bg-gray-200 h-5 rounded shadow-sm animate-pulse ${props.className}`}
+      className={`bg-gray-200  rounded shadow-sm animate-pulse ${props.className}`}
     ></div>
   );
-};/* END_INCLUDE COMPONENT: "includes/Common/Skelton.jsx" */
+};/* END_INCLUDE COMPONENT: "includes/Common/Skeleton.jsx" */
 /* INCLUDE: "includes/formats.jsx" */
 function currency(number) {
   let absNumber = Math.abs(number);
@@ -919,6 +919,25 @@ function debounce(
   return debounced;
 }
 
+function timeAgo(unixTimestamp) {
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const secondsAgo = currentTimestamp - unixTimestamp;
+
+  if (secondsAgo < 5) {
+    return 'Just now';
+  } else if (secondsAgo < 60) {
+    return `${secondsAgo} seconds ago`;
+  } else if (secondsAgo < 3600) {
+    const minutesAgo = Math.floor(secondsAgo / 60);
+    return `${minutesAgo} minute${minutesAgo > 1 ? 's' : ''} ago`;
+  } else if (secondsAgo < 86400) {
+    const hoursAgo = Math.floor(secondsAgo / 3600);
+    return `${hoursAgo} hour${hoursAgo > 1 ? 's' : ''} ago`;
+  } else {
+    const daysAgo = Math.floor(secondsAgo / 86400);
+    return `${daysAgo} day${daysAgo > 1 ? 's' : ''} ago`;
+  }
+}
 function shortenAddress(address) {
   const string = String(address);
 
@@ -1130,33 +1149,15 @@ function localFormat(number) {
 
 
 
-function MainComponent(props) {
+function MainComponent({ network, t }) {
   const [isLoading, setIsLoading] = useState(false);
-  const [css, setCss] = useState({});
   const [stats, setStats] = useState({} );
   const [charts, setCharts] = useState([]);
   const [chartConfig, setChartConfig] = useState(
     {} ,
   );
 
-  const config = getConfig(props.network);
-
-  /**
-   * Fetches styles asynchronously from a nearblocks gateway.
-   */
-  function fetchStyles() {
-    asyncFetch('https://beta.nearblocks.io/common.css').then(
-      (res) => {
-        if (res?.body) {
-          setCss(res.body);
-        }
-      },
-    );
-  }
-
-  const Theme = styled.div`
-    ${css}
-  `;
+  const config = getConfig(network);
 
   useEffect(() => {
     let delay = 15000;
@@ -1203,10 +1204,6 @@ function MainComponent(props) {
   }, [config?.backendUrl]);
 
   useEffect(() => {
-    if (props?.fetchStyles) {
-      fetchStyles();
-    }
-
     function fetchChartData() {
       asyncFetch(`${config.backendUrl}charts/latest`)
         .then(
@@ -1223,7 +1220,7 @@ function MainComponent(props) {
     }
 
     fetchChartData();
-  }, [props?.fetchStyles, config.backendUrl]);
+  }, [config.backendUrl]);
 
   const chartData = useMemo(() => {
     try {
@@ -1346,192 +1343,189 @@ function MainComponent(props) {
     `;
 
   return (
-    <Theme>
-      <div className="relative -mt-14">
-        <div className="container mx-auto px-3">
-          <div className="bg-white soft-shadow rounded-lg overflow-hidden px-5 md:py lg:px-0">
-            <div
-              className={`grid grid-flow-col grid-cols-1 ${
-                props.network === 'mainnet'
-                  ? 'grid-rows-3 lg:grid-cols-3'
-                  : 'grid-rows-2 lg:grid-cols-2'
-              } lg:grid-rows-1 divide-y lg:divide-y-0 lg:divide-x lg:py-3`}
-            >
-              {props.network === 'mainnet' && (
-                <>
-                  <div className="flex flex-col lg:flex-col lg:items-stretch divide-y lg:divide-y lg:divide-x-0 md:pt-0 md:pb-0 md:px-5">
-                    <div className="flex flex-row py-5 lg:pb-5 lg:px-0">
-                      <div className="items-center flex justify-left mr-3 ">
-                        <img
-                          src={`${config.appUrl}images/near price.svg`}
-                          alt={'nearPrice'}
-                          className="h-9 w-9"
-                          width="24"
-                          height="24"
-                        />
-                      </div>
-                      <div className="ml-2">
-                        <p className="uppercase font-semibold text-gray-600 text-sm ">
-                          NEAR PRICE
-                        </p>
-                        {isLoading ? (
-                          <Skelton />
-                        ) : (
-                          <a href="/charts/near-price">
-                            <a className="leading-6 text-gray-500">
-                              ${dollarFormat(stats?.near_price ?? 0)}{' '}
-                              <span className="text-gray-400">
-                                @{localFormat(stats?.near_btc_price ?? 0)} BTC
-                              </span>{' '}
-                              {stats?.change_24 > 0 ? (
-                                <span className="text-neargreen text-sm">
-                                  ({dollarFormat(stats?.change_24)}%)
-                                </span>
-                              ) : (
-                                <span className="text-red-500 text-sm">
-                                  ({dollarFormat(stats?.change_24)}%)
-                                </span>
-                              )}
-                            </a>
-                          </a>
-                        )}
-                      </div>
-                    </div>
-                    <div className="flex flex-row py-5 lg:pt-5 lg:px-0">
-                      <div className="items-center flex justify-left mr-3 ">
-                        <img
-                          src={`${config.appUrl}images/market.svg`}
-                          alt={'marketCap'}
-                          className="h-9 w-9"
-                          width="24"
-                          height="24"
-                        />
-                      </div>
-                      <div className="ml-2">
-                        <p className="uppercase font-semibold text-gray-500 text-sm">
-                          MARKET CAP
-                        </p>
-                        {isLoading ? (
-                          <Skelton />
-                        ) : (
-                          <a href="/charts/market-cap">
-                            <a className="leading-6 text-gray-400">
-                              ${dollarFormat(stats?.market_cap ?? 0)}
-                            </a>
-                          </a>
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                </>
-              )}
+    <div className="container mx-auto px-3">
+      <div className="bg-white soft-shadow rounded-lg overflow-hidden px-5 md:py lg:px-0">
+        <div
+          className={`grid grid-flow-col grid-cols-1 ${
+            network === 'mainnet'
+              ? 'grid-rows-3 lg:grid-cols-3'
+              : 'grid-rows-2 lg:grid-cols-2'
+          } lg:grid-rows-1 divide-y lg:divide-y-0 lg:divide-x lg:py-3`}
+        >
+          {network === 'mainnet' && (
+            <>
               <div className="flex flex-col lg:flex-col lg:items-stretch divide-y lg:divide-y lg:divide-x-0 md:pt-0 md:pb-0 md:px-5">
-                <div className="flex flex-row justify-between py-5 lg:pb-5 lg:px-0">
-                  <div className="flex flex-row ">
-                    <div className="items-center flex justify-left mr-3 ">
-                      <img
-                        src={`${config.appUrl}images/transactions.svg`}
-                        alt={'transactions'}
-                        className="h-9 w-9"
-                        width="24"
-                        height="24"
-                      />
-                    </div>
-                    <div className="ml-2">
-                      <p className="uppercase font-semibold text-gray-500 text-sm">
-                        TRANSACTIONS
-                      </p>
-                      {isLoading ? (
-                        <Skelton />
-                      ) : (
-                        <p className="leading-6 text-gray-400">
-                          {currency(Number(stats?.total_txns ?? 0))}
-                        </p>
-                      )}
-                    </div>
+                <div className="flex flex-row py-5 lg:pb-5 lg:px-0">
+                  <div className="items-center flex justify-left mr-3 ">
+                    <img
+                      src={`${config.appUrl}images/near price.svg`}
+                      alt={t ? t('home:nearPrice') : 'nearPrice'}
+                      className="h-9 w-9"
+                      width="24"
+                      height="24"
+                    />
                   </div>
-                  <div className="flex flex-col text-right">
-                    <p className="uppercase font-semibold text-gray-500 text-sm">
-                      {' '}
-                      GAS PRICE
+                  <div className="ml-2">
+                    <p className="uppercase font-semibold text-gray-600 text-sm ">
+                      {t ? t('home:nearPrice') : 'NEAR PRICE'}
                     </p>
                     {isLoading ? (
-                      <Skelton />
+                      <Skeleton className="h-4" />
                     ) : (
-                      <p className="leading-6 text-gray-400">
-                        {gasPrice(Number(stats?.gas_price ?? 0))}
-                      </p>
+                      <a href="/charts/near-price">
+                        <a className="leading-6 text-gray-500">
+                          ${dollarFormat(stats?.near_price ?? 0)}{' '}
+                          <span className="text-gray-400">
+                            @{localFormat(stats?.near_btc_price ?? 0)} BTC
+                          </span>{' '}
+                          {stats?.change_24 > 0 ? (
+                            <span className="text-neargreen text-sm">
+                              ({dollarFormat(stats?.change_24)}%)
+                            </span>
+                          ) : (
+                            <span className="text-red-500 text-sm">
+                              ({dollarFormat(stats?.change_24)}%)
+                            </span>
+                          )}
+                        </a>
+                      </a>
                     )}
                   </div>
                 </div>
-                <div className="flex flex-row justify-between align-center py-5 lg:pt-5 lg:px-0">
-                  <div className="flex flex-row ">
-                    <div className="items-center flex justify-left mr-3 ">
-                      <img
-                        src={`${config.appUrl}images/pickaxe.svg`}
-                        alt={'activeValidator'}
-                        className="h-9 w-9"
-                        width="24"
-                        height="24"
-                      />
-                    </div>
-                    <div className="ml-2">
-                      <p className="uppercase font-semibold text-gray-500 text-sm">
-                        ACTIVE VALIDATORS
-                      </p>
-                      {isLoading ? (
-                        <Skelton />
-                      ) : (
-                        <a href="/charts/blocks">
-                          <a className="leading-6 text-gray-400">
-                            {localFormat(stats?.nodes_online ?? 0)}
-                          </a>
-                        </a>
-                      )}
-                    </div>
+                <div className="flex flex-row py-5 lg:pt-5 lg:px-0">
+                  <div className="items-center flex justify-left mr-3 ">
+                    <img
+                      src={`${config.appUrl}images/market.svg`}
+                      alt={t ? t('home:marketCap') : 'marketCap'}
+                      className="h-9 w-9"
+                      width="24"
+                      height="24"
+                    />
                   </div>
-                  <div className="flex flex-col text-right">
+                  <div className="ml-2">
                     <p className="uppercase font-semibold text-gray-500 text-sm">
-                      AVG. BLOCK TIME
+                      {t ? t('home:marketCap') : ' MARKET CAP'}
                     </p>
                     {isLoading ? (
-                      <Skelton />
+                      <Skeleton className="h-4" />
                     ) : (
-                      <a className="leading-6 text-gray-400">
-                        {stats?.avg_block_time ?? 0} s
+                      <a href="/charts/market-cap">
+                        <a className="leading-6 text-gray-400">
+                          ${dollarFormat(stats?.market_cap ?? 0)}
+                        </a>
                       </a>
                     )}
                   </div>
                 </div>
               </div>
-              <div className="md:col-span-2 lg:col-span-1 flex flex-col lg:flex-col lg:items-stretch divide-y lg:divide-y lg:divide-x-0 md:pt-0 md:px-5">
-                <div className="flex-1 lg:px-0">
-                  <p className="uppercase font-semibold text-gray-500 text-sm">
-                    {' '}
-                    NEAR TRANSACTION HISTORY IN 14 DAYS
-                  </p>
-                  <div className="pl-2 pr-4 h-full">
-                    {chartData ? (
-                      <iframe
-                        srcDoc={iframeSrc}
-                        style={{
-                          width: '100%',
-                          height: '100%',
-                          border: 'none',
-                        }}
-                      />
-                    ) : (
-                      <Skelton className="h-full w-full" />
-                    )}
-                  </div>
+            </>
+          )}
+          <div className="flex flex-col lg:flex-col lg:items-stretch divide-y lg:divide-y lg:divide-x-0 md:pt-0 md:pb-0 md:px-5">
+            <div className="flex flex-row justify-between py-5 lg:pb-5 lg:px-0">
+              <div className="flex flex-row ">
+                <div className="items-center flex justify-left mr-3 ">
+                  <img
+                    src={`${config.appUrl}images/transactions.svg`}
+                    alt={t ? t('home:transactions') : 'transactions'}
+                    className="h-9 w-9"
+                    width="24"
+                    height="24"
+                  />
                 </div>
+                <div className="ml-2">
+                  <p className="uppercase font-semibold text-gray-500 text-sm">
+                    {t ? t('home:transactions') : 'TRANSACTIONS'}
+                  </p>
+                  {isLoading ? (
+                    <Skeleton className="h-4" />
+                  ) : (
+                    <p className="leading-6 text-gray-400">
+                      {currency(Number(stats?.total_txns ?? 0))}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col text-right">
+                <p className="uppercase font-semibold text-gray-500 text-sm">
+                  {' '}
+                  {t ? t('home:gasPrice') : 'GAS PRICE'}
+                </p>
+                {isLoading ? (
+                  <Skeleton className="h-4" />
+                ) : (
+                  <p className="leading-6 text-gray-400">
+                    {gasPrice(Number(stats?.gas_price ?? 0))}
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-row justify-between align-center py-5 lg:pt-5 lg:px-0">
+              <div className="flex flex-row ">
+                <div className="items-center flex justify-left mr-3 ">
+                  <img
+                    src={`${config.appUrl}images/pickaxe.svg`}
+                    alt={t ? t('home:activeValidator') : 'activeValidator'}
+                    className="h-9 w-9"
+                    width="24"
+                    height="24"
+                  />
+                </div>
+                <div className="ml-2">
+                  <p className="uppercase font-semibold text-gray-500 text-sm">
+                    {t ? t('home:activeValidator') : 'ACTIVE VALIDATORS'}
+                  </p>
+                  {isLoading ? (
+                    <Skeleton className="h-4" />
+                  ) : (
+                    <a href="/charts/blocks">
+                      <a className="leading-6 text-gray-400">
+                        {localFormat(stats?.nodes_online ?? 0)}
+                      </a>
+                    </a>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col text-right">
+                <p className="uppercase font-semibold text-gray-500 text-sm">
+                  {t ? t('home:avgBlockTime') : 'AVG. BLOCK TIME'}
+                </p>
+                {isLoading ? (
+                  <Skeleton className="h-4" />
+                ) : (
+                  <a className="leading-6 text-gray-400">
+                    {stats?.avg_block_time ?? 0} s
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="md:col-span-2 lg:col-span-1 flex flex-col lg:flex-col lg:items-stretch divide-y lg:divide-y lg:divide-x-0 md:pt-0 md:px-5">
+            <div className="flex-1 lg:px-0">
+              <p className="uppercase font-semibold text-gray-500 text-sm">
+                {' '}
+                {t
+                  ? t('home:transactionHistory', { days: 14 })
+                  : 'NEAR TRANSACTION HISTORY IN 14 DAYS'}
+              </p>
+              <div className="pl-2 pr-4 h-full">
+                {chartData ? (
+                  <iframe
+                    srcDoc={iframeSrc}
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      border: 'none',
+                    }}
+                  />
+                ) : (
+                  <Skeleton className="h-36" />
+                )}
               </div>
             </div>
           </div>
         </div>
-        <div className="py-8 relative"></div>
       </div>
-    </Theme>
+    </div>
   );
 }
 
