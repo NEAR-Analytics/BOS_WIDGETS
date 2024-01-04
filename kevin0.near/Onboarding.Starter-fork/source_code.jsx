377f6a388df28fb3c7cf42221269f168ddd89df5
kevin0.near <kevin0.near>
2024-01-04 22:45:38 +0000

    Update Onboarding.Starter-fork by kevin0.near at 2024-01-04T22:45:38.833Z

--- kevin0.near/Onboarding.Starter-fork/source_code.jsx
+++ kevin0.near/Onboarding.Starter-fork/source_code.jsx
@@ -1,10 +1,8 @@
 return (
   <>
-    //some random change to test indexer
     {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
     <Widget src="near/widget/Onboarding.ComponentCard" props={{ accountId }} />
     <br />
-    //some random change to test indexer
     {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
     <Widget />
   </>
