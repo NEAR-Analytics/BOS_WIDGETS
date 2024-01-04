da24473f077511fee0315d0c1041a81a7eb72774
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2024-01-04 20:57:01 +0000

    Update NDC.ArticleHistory.SecondContainer by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2024-01-04T20:57:01.784Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.ArticleHistory.SecondContainer/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.ArticleHistory.SecondContainer/source_code.jsx
@@ -1,34 +1,32 @@
+// NDC.ArticleHistory.SecondContainer
+
 const {
   pathToCurrentArticle,
   pathToPrevArticle,
   currentBlockHeight,
+  currentVersionData,
+  allVersionsData,
   prevBlockHeight,
-  widgets,
 } = props;
-
-const authorForWidget =
-  "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
 
 if (!pathToCurrentArticle || !pathToPrevArticle || !currentBlockHeight)
   return "send pathToCurrentArticle and pathToPrevArticle and currentBlockHeight in props";
 
-const currentArticle = JSON.parse(
-  Social.get(`${pathToCurrentArticle}`, currentBlockHeight)
-);
-
-const currentCode = currentArticle.body;
+const currentCode = currentVersionData.body;
 
 if (currentCode === null) return "Loading";
 
 const prevCode = prevBlockHeight
-  ? JSON.parse(Social.get(`${pathToPrevArticle}`, prevBlockHeight)).body
+  ? allVersionsData.find(
+      (versionData) => versionData.blockHeight == prevBlockHeight
+    ).body
   : undefined;
 
 if (prevCode === null) return "Loading";
 
 return (
   <Widget
-    src={widgets.articleHistory.codeDiff}
+    src={`bozon.near/widget/CodeDiff`}
     props={{ currentCode, prevCode, ...props }}
   />
 );
