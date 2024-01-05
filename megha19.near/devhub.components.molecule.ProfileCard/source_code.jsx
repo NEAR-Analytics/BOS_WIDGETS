7253347a151960c2c321fabb83d6dcf55b937762
megha19.near <megha19.near>
2024-01-05 09:24:11 +0000

    Update devhub.components.molecule.ProfileCard by megha19.near at 2024-01-05T09:24:11.684Z

--- megha19.near/devhub.components.molecule.ProfileCard/source_code.jsx
+++ megha19.near/devhub.components.molecule.ProfileCard/source_code.jsx
@@ -65,7 +65,7 @@ const ProfileCard = (props) => {
       href={
         link !== true
           ? link
-          : `/${REPL_MOB}/widget/ProfilePage?accountId=${accountId}`
+          : `/mob.near/widget/ProfilePage?accountId=${accountId}`
       }
       target={openLinkInNewTab ? "_blank" : ""}
       rel="noopener noreferrer"
@@ -96,7 +96,7 @@ const ProfileCard = (props) => {
     <div className="d-flex flex-row align-items-center">
       {inner}
       <Widget
-        src="${REPL_DEVHUB}/widget/devhub.components.molecule.BadgesList"
+        src="megha19.near/widget/devhub.components.molecule.BadgesList"
         props={{
           accountId,
           mode: "compact",
