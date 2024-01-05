986767128e3cf139b0c816b599ea41dcd592b1b4
megha19.near <megha19.near>
2024-01-05 16:43:44 +0000

    Update devhub.page.post by megha19.near at 2024-01-05T16:43:44.090Z

--- megha19.near/devhub.page.post/source_code.jsx
+++ megha19.near/devhub.page.post/source_code.jsx
@@ -13,7 +13,7 @@ const Container = styled.div`
 return (
   <Container>
     <Widget
-      src={"devhub.near/widget/devhub.entity.post.Post"}
+      src={"megha19.near/widget/devhub.entity.post.Post"}
       props={{
         id,
       }}
