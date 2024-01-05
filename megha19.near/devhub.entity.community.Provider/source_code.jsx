8ce09304e8c86373cfd033452e9147673ecb7d5e
megha19.near <megha19.near>
2024-01-05 10:22:51 +0000

    Update devhub.entity.community.Provider by megha19.near at 2024-01-05T10:22:51.990Z

--- megha19.near/devhub.entity.community.Provider/source_code.jsx
+++ megha19.near/devhub.entity.community.Provider/source_code.jsx
@@ -7,7 +7,7 @@ const {
   deleteCommunity,
   getCommunity,
   setCommunityAddons,
-} = VM.require("megha19.near/widget/core.adapter.devhub-contract");
+} = VM.require("devhub.near/widget/core.adapter.devhub-contract");
 
 if (
   !getCommunity ||
