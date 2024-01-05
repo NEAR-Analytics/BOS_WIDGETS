dffd259610155253f93f0a01360bda1f933515b4
megha19.near <megha19.near>
2024-01-05 09:19:03 +0000

    Update DevGov.Notification.Item.Right by megha19.near at 2024-01-05T09:19:03.165Z

--- megha19.near/DevGov.Notification.Item.Right/source_code.jsx
+++ megha19.near/DevGov.Notification.Item.Right/source_code.jsx
@@ -1,4 +1,4 @@
-const { href } = VM.require("megha19.near/widget/core.lib.url") || (() => {});
+const { href } = VM.require("${REPL_DEVHUB}/widget/core.lib.url") || (() => {});
 
 return props.post === undefined ? (
   "Loading ..."
@@ -7,7 +7,7 @@ return props.post === undefined ? (
     <a
       className="btn btn-outline-dark"
       href={href({
-        widgetSrc: "megha19.near/widget/app",
+        widgetSrc: "${REPL_DEVHUB}/widget/app",
         params: {
           page: "post",
           id: props.post,
