dedf3edde20f6165992a6eb25b50f17e5b114a37
megha19.near <megha19.near>
2024-01-05 09:19:03 +0000

    Update devhub.entity.addon.wiki.Viewer by megha19.near at 2024-01-05T09:19:03.165Z

--- megha19.near/devhub.entity.addon.wiki.Viewer/source_code.jsx
+++ megha19.near/devhub.entity.addon.wiki.Viewer/source_code.jsx
@@ -84,7 +84,9 @@ if (!content) {
       <p className="subtitle">{subtitle}</p>
       <Content>
         <Widget
-          src={"megha19.near/widget/devhub.components.molecule.MarkdownViewer"}
+          src={
+            "${REPL_DEVHUB}/widget/devhub.components.molecule.MarkdownViewer"
+          }
           props={{ text: content }}
         />
       </Content>
