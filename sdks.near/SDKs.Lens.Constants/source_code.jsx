edbd18e0f48730e13c67b5ec821e8b4d2966af01
sdks.near <sdks.near>
2024-01-05 12:35:06 +0000

    Update SDKs.Lens.Constants by sdks.near at 2024-01-05T12:35:06.903Z

---
+++ sdks.near/SDKs.Lens.Constants/source_code.jsx
@@ -0,0 +1,9 @@
+const MAINNET_URL = "https://api-v2.lens.dev";
+const TESTNET_URL = "https://api-v2-mumbai-live.lens.dev";
+const JWT_TOKEN_LIFESPAN_SECONDS = 1800; // 30 min
+
+return {
+    MAINNET_URL,
+    TESTNET_URL,
+    JWT_TOKEN_LIFESPAN_SECONDS
+};
