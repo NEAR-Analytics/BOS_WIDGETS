9ec5552090eda7ef93b764d0bee5cc89744991b9
megha19.near <megha19.near>
2024-01-05 10:22:51 +0000

    Update devhub.entity.addon.kanban.Configurator by megha19.near at 2024-01-05T10:22:51.990Z

--- megha19.near/devhub.entity.addon.kanban.Configurator/source_code.jsx
+++ megha19.near/devhub.entity.addon.kanban.Configurator/source_code.jsx
@@ -1,14 +1,12 @@
-const Struct = VM.require("megha19.near/widget/core.lib.struct");
+const Struct = VM.require("devhub.near/widget/core.lib.struct");
 
 if (!Struct) {
   return <p>Loading modules...</p>;
 }
 const { useQuery } = VM.require(
-  "megha19.near/widget/core.adapter.devhub-contract"
+  "devhub.near/widget/core.adapter.devhub-contract"
 );
-const { uuid, withUUIDIndex } = VM.require(
-  "megha19.near/widget/core.lib.uuid"
-);
+const { uuid, withUUIDIndex } = VM.require("devhub.near/widget/core.lib.uuid");
 
 uuid || (uuid = () => {});
 withUUIDIndex || (withUUIDIndex = () => {});
@@ -106,9 +104,7 @@ const sortByOptions = [
 const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
   const tags = useCache(
     () =>
-      Near.asyncView("devgovgigs.near", "get_all_labels").then(
-        (res) => res
-      ),
+      Near.asyncView("devgovgigs.near", "get_all_labels").then((res) => res),
     handle,
     { subscribe: false }
   );
@@ -191,7 +187,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
       <div className="card-body d-flex flex-column gap-3">
         <div className="d-flex flex-column flex-lg-row align-items-stretch w-100">
           <Widget
-            src={`megha19.near/widget/devhub.components.organism.Configurator`}
+            src={`devhub.near/widget/devhub.components.organism.Configurator`}
             props={{
               heading: "Basic information",
               externalState: formState.metadata,
@@ -213,7 +209,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
           </span>
           <div>
             <Widget
-              src={`megha19.near/widget/devhub.components.organism.Configurator`}
+              src={`devhub.near/widget/devhub.components.organism.Configurator`}
               props={{
                 heading: "Card fields",
                 classNames: { root: "w-auto h-auto" },
@@ -277,7 +273,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
               >
                 <div className="d-flex flex-column gap-1 w-100">
                   <Widget
-                    src={`megha19.near/widget/devhub.components.molecule.Input`}
+                    src={`devhub.near/widget/devhub.components.molecule.Input`}
                     props={{
                       className: "flex-grow-1",
                       key: `column-${id}-title`,
@@ -290,7 +286,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
                     }}
                   />
                   <Widget
-                    src={`megha19.near/widget/devhub.components.molecule.Input`}
+                    src={`devhub.near/widget/devhub.components.molecule.Input`}
                     props={{
                       className: "flex-grow-1",
                       key: `column-${id}-description`,
@@ -308,7 +304,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
                     </span>
                     <div className="w-100">
                       <Widget
-                        src="megha19.near/widget/devhub.feature.post-search.by-tag"
+                        src="devhub.near/widget/devhub.feature.post-search.by-tag"
                         props={{
                           tag: tag,
                           onTagSearch: formUpdate({
@@ -342,7 +338,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
         <div className="d-flex gap-3 justify-content-between w-100 mt-2 flex-wrap flex-sm-nowrap">
           <div style={{ flex: "none" }}>
             <Widget
-              src={`megha19.near/widget/devhub.components.molecule.Button`}
+              src={`devhub.near/widget/devhub.components.molecule.Button`}
               props={{
                 classNames: {
                   root: "btn-sm btn-outline-secondary",
@@ -361,7 +357,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
           </div>
           <div className="d-flex gap-3 justify-content-end w-100">
             <Widget
-              src={`megha19.near/widget/devhub.components.molecule.Button`}
+              src={`devhub.near/widget/devhub.components.molecule.Button`}
               props={{
                 classNames: {
                   root: "d-flex btn btn-outline-danger shadow-none border-0",
@@ -372,7 +368,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
               }}
             />
             <Widget
-              src={`megha19.near/widget/devhub.components.molecule.Button`}
+              src={`devhub.near/widget/devhub.components.molecule.Button`}
               props={{
                 classNames: { root: "btn btn-success" },
                 disabled: form.isSynced,
@@ -433,7 +429,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
       {showPreview ? (
         <div>
           <Widget
-            src={`megha19.near/widget/devhub.entity.addon.kanban.Viewer`}
+            src={`devhub.near/widget/devhub.entity.addon.kanban.Viewer`}
             props={{
               data: formState,
             }}
@@ -459,7 +455,7 @@ const KanbanViewConfigurator = ({ handle, data, permissions, onSubmit }) => {
             This community doesn't have a kanban board
           </h5>
           <Widget
-            src={`megha19.near/widget/devhub.components.molecule.Button`}
+            src={`devhub.near/widget/devhub.components.molecule.Button`}
             props={{
               icon: { type: "bootstrap_icon", variant: "bi-kanban-fill" },
               isHidden: !permissions.can_configure,
