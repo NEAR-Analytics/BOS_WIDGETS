4a8b640428ce7e59ac6e64d524e441da4db883bd
megha19.near <megha19.near>
2024-01-05 09:19:03 +0000

    Update devhub.entity.addon.blog.editor.content by megha19.near at 2024-01-05T09:19:03.165Z

--- megha19.near/devhub.entity.addon.blog.editor.content/source_code.jsx
+++ megha19.near/devhub.entity.addon.blog.editor.content/source_code.jsx
@@ -1,8 +1,8 @@
 const { Card } =
-  VM.require("megha19.near/widget/devhub.entity.addon.blog.Card") ||
+  VM.require("${REPL_DEVHUB}/widget/devhub.entity.addon.blog.Card") ||
   (() => <></>);
 const { Page } =
-  VM.require("megha19.near/widget/devhub.entity.addon.blog.Page") ||
+  VM.require("${REPL_DEVHUB}/widget/devhub.entity.addon.blog.Page") ||
   (() => <></>);
 
 const categories = [
@@ -194,7 +194,7 @@ return (
         aria-labelledby="edit-tab"
       >
         <Widget
-          src="megha19.near/widget/devhub.entity.addon.blog.editor.form"
+          src="${REPL_DEVHUB}/widget/devhub.entity.addon.blog.editor.form"
           props={{
             title,
             setTitle,
@@ -217,7 +217,7 @@ return (
           className={"d-flex align-items-center justify-content-end gap-3 mt-4"}
         >
           <Widget
-            src={"megha19.near/widget/devhub.components.molecule.Button"}
+            src={"${REPL_DEVHUB}/widget/devhub.components.molecule.Button"}
             props={{
               classNames: { root: "btn-success" },
               disabled: !hasDataChanged(),
@@ -240,7 +240,7 @@ return (
       >
         <div style={{ position: "absolute", top: 10, right: 0 }}>
           <Widget
-            src="megha19.near/widget/devhub.components.molecule.Switch"
+            src="${REPL_DEVHUB}/widget/devhub.components.molecule.Switch"
             props={{
               currentValue: previewMode,
               key: "previewMode",
