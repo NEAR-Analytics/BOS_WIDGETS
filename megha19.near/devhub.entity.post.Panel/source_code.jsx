b79d89f277d58568668b51c24c24558b035efb39
megha19.near <megha19.near>
2024-01-05 09:24:11 +0000

    Update devhub.entity.post.Panel by megha19.near at 2024-01-05T09:24:11.684Z

--- megha19.near/devhub.entity.post.Panel/source_code.jsx
+++ megha19.near/devhub.entity.post.Panel/source_code.jsx
@@ -521,7 +521,7 @@ const search = (processedQueryArray, index) => {
 const amountOfResultsToShowFirst = 5;
 
 const buildPostsIndex = () => {
-  return Near.asyncView("${REPL_DEVHUB_CONTRACT}", "get_posts").then(
+  return Near.asyncView("devgovgigs.near", "get_posts").then(
     (posts) => {
       const index = buildIndex(posts);
       const data = posts.reduce((acc, post) => {
@@ -682,7 +682,7 @@ return (
       </div>
       <div class="dropdown">
         <Widget
-          src="${REPL_DEVHUB}/widget/devhub.feature.post-search.by-author"
+          src="megha19.near/widget/devhub.feature.post-search.by-author"
           props={{
             authorQuery: props.authorQuery,
             onAuthorSearch: props.onAuthorSearch,
@@ -691,7 +691,7 @@ return (
       </div>
       <div>
         <Widget
-          src="${REPL_DEVHUB}/widget/devhub.feature.post-search.by-tag"
+          src="megha19.near/widget/devhub.feature.post-search.by-tag"
           props={{
             tagQuery: props.tagQuery,
             onTagSearch: props.onTagSearch,
@@ -712,7 +712,7 @@ return (
       )}
     {state.term && state.term.length > 1 && state.searchResult ? (
       <Widget
-        src={"${REPL_DEVHUB}/widget/devhub.entity.post.List"}
+        src={"megha19.near/widget/devhub.entity.post.List"}
         props={{
           searchResult: {
             postIds: state.searchResult,
@@ -730,7 +730,7 @@ return (
       />
     ) : (
       <Widget
-        src={"${REPL_DEVHUB}/widget/devhub.entity.post.List"}
+        src={"megha19.near/widget/devhub.entity.post.List"}
         props={{
           recency: props.recency,
           tag: props.tag,
