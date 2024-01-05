0e225715d25a02eaeda7096ced6e28b26f219260
megha19.near <megha19.near>
2024-01-05 09:24:11 +0000

    Update devhub.entity.community.Activity by megha19.near at 2024-01-05T09:24:11.684Z

--- megha19.near/devhub.entity.community.Activity/source_code.jsx
+++ megha19.near/devhub.entity.community.Activity/source_code.jsx
@@ -1,10 +1,10 @@
 const { handle } = props;
 
 const { getCommunity } = VM.require(
-  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
+  "megha19.near/widget/core.adapter.devhub-contract"
 );
 
-const { href } = VM.require("${REPL_DEVHUB}/widget/core.lib.url");
+const { href } = VM.require("megha19.near/widget/core.lib.url");
 
 if (!getCommunity || !href) {
   return <p>Loading modules...</p>;
@@ -41,19 +41,19 @@ return (
       <div class="d-flex w-100">
         <MainContent>
           <Widget
-            src={"${REPL_DEVHUB}/widget/devhub.feature.post-search.panel"}
+            src={"megha19.near/widget/devhub.feature.post-search.panel"}
             props={{
               hideHeader: true,
               tag: communityData.tag,
               children: (
                 <Widget
                   src={
-                    "${REPL_DEVHUB}/widget/devhub.components.molecule.PostControls"
+                    "megha19.near/widget/devhub.components.molecule.PostControls"
                   }
                   props={{
                     title: "Post",
                     href: href({
-                      widgetSrc: "${REPL_DEVHUB}/widget/app",
+                      widgetSrc: "megha19.near/widget/app",
                       params: {
                         page: "create",
                         labels: [communityData.tag],
@@ -69,7 +69,7 @@ return (
         </MainContent>
         <SidebarContainer>
           <Widget
-            src={"${REPL_DEVHUB}/widget/devhub.entity.community.Sidebar"}
+            src={"megha19.near/widget/devhub.entity.community.Sidebar"}
             props={{ community: communityData }}
           />
         </SidebarContainer>
