fb11c05ed98101ac33c05646887218b98ab18ae5
megha19.near <megha19.near>
2024-01-05 10:22:51 +0000

    Update devhub.entity.addon.kanban.post_board by megha19.near at 2024-01-05T10:22:51.990Z

--- megha19.near/devhub.entity.addon.kanban.post_board/source_code.jsx
+++ megha19.near/devhub.entity.addon.kanban.post_board/source_code.jsx
@@ -1,8 +1,8 @@
 const { getPostsByLabel } = VM.require(
-  "megha19.near/widget/core.adapter.devhub-contract"
+  "devhub.near/widget/core.adapter.devhub-contract"
 );
 const { getPost } = VM.require(
-  "megha19.near/widget/core.adapter.devhub-contract"
+  "devhub.near/widget/core.adapter.devhub-contract"
 );
 getPost || (getPost = () => {});
 getPostsByLabel || (getPostsByLabel = () => {});
@@ -172,7 +172,7 @@ const KanbanPostBoard = ({ metadata, payload }) => {
                   {data.length === column.postIds.length &&
                     data.map((postData) => (
                       <Widget
-                        src={`megha19.near/widget/devhub.entity.addon.${metadata.ticket.type}`}
+                        src={`devhub.near/widget/devhub.entity.addon.${metadata.ticket.type}`}
                         props={{
                           setDescriptionDisplay,
                           setFundingDisplay,
@@ -230,7 +230,7 @@ const KanbanPostBoard = ({ metadata, payload }) => {
                 {data.length === column.postIds.length &&
                   data.map((postData) => (
                     <Widget
-                      src={`megha19.near/widget/devhub.entity.addon.${metadata.ticket.type}`}
+                      src={`devhub.near/widget/devhub.entity.addon.${metadata.ticket.type}`}
                       props={{
                         metadata: { id: postData.postId, ...metadata.ticket },
                         isTableView: false,
