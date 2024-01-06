0f70b7615ae28e914f21925fec5ca78d92309d91
sdks.near <sdks.near>
2024-01-06 21:11:25 +0000

    Update SDKs.EthereumSigner.Main by sdks.near at 2024-01-06T21:11:25.195Z

---
+++ sdks.near/SDKs.EthereumSigner.Main/source_code.jsx
@@ -0,0 +1,28 @@
+const VERIFIER_CONTRACT = "0x793ABce859d651C179F2F3B0A47C438dC3D67901";
+const ABI = [
+  "function verifySignature(string memory message, uint8 v, bytes32 r, bytes32 s) public pure returns (address signer)",
+];
+
+const EthereumSigner = {
+  sign: (message) => Ethers.provider().getSigner().signMessage(message),
+  verify: (originalMessage, signature, expectedSignerAddress) => {
+    return EthereumSigner.getSignerAddress(originalMessage, signature).then(
+      (address) => {
+        return expectedSignerAddress.toLowerCase() == address.toLowerCase();
+      }
+    );
+  },
+  getSignerAddress: (message, signature) => {
+    const contract = new ethers.Contract(
+      VERIFIER_CONTRACT,
+      ABI,
+      Ethers.provider()
+    );
+
+    const { v, r, s } = ethers.utils.splitSignature(signature);
+
+    return contract.callStatic.verifySignature(message, v, r, s);
+  },
+};
+
+return EthereumSigner;
