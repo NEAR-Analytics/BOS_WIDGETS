14fcc6ce25c98e3b88fb2bfd2bda11f13c6a8ea7
thomasguntenaar.near <thomasguntenaar.near>
2024-01-06 12:45:15 +0000

    Update dh.about by thomasguntenaar.near at 2024-01-06T12:45:15.291Z

--- thomasguntenaar.near/dh.about/source_code.jsx
+++ thomasguntenaar.near/dh.about/source_code.jsx
@@ -1,3 +1,6 @@
 return (
-  <Widget src="thomasguntenaar.near/widget/app" props={{ page: "about", ...props }} />
+  <Widget
+    src="thomasguntenaar.near/widget/app"
+    props={{ page: "about", ...props }}
+  />
 );
