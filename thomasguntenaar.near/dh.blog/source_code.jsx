a4d18af316f8d14c0235897b54df2140e8595dd1
thomasguntenaar.near <thomasguntenaar.near>
2024-01-06 12:45:15 +0000

    Update dh.blog by thomasguntenaar.near at 2024-01-06T12:45:15.291Z

--- thomasguntenaar.near/dh.blog/source_code.jsx
+++ thomasguntenaar.near/dh.blog/source_code.jsx
@@ -1,3 +1,6 @@
 return (
-  <Widget src="thomasguntenaar.near/widget/app" props={{ page: "blog", ...props }} />
+  <Widget
+    src="thomasguntenaar.near/widget/app"
+    props={{ page: "blog", ...props }}
+  />
 );
