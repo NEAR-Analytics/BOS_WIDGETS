1cca6e1ca76b6a9286df384b7de51f66e0467f47
thomasguntenaar.near <thomasguntenaar.near>
2024-01-06 12:45:15 +0000

    Update dh.admin by thomasguntenaar.near at 2024-01-06T12:45:15.291Z

--- thomasguntenaar.near/dh.admin/source_code.jsx
+++ thomasguntenaar.near/dh.admin/source_code.jsx
@@ -1,3 +1,6 @@
 return (
-  <Widget src="thomasguntenaar.near/widget/app" props={{ page: "admin", ...props }} />
+  <Widget
+    src="thomasguntenaar.near/widget/app"
+    props={{ page: "admin", ...props }}
+  />
 );
