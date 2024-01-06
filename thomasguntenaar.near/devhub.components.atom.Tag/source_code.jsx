9432bd485c97638bfa97ccf7d28720fd78d2c68a
thomasguntenaar.near <thomasguntenaar.near>
2024-01-06 12:45:15 +0000

    Update devhub.components.atom.Tag by thomasguntenaar.near at 2024-01-06T12:45:15.291Z

--- thomasguntenaar.near/devhub.components.atom.Tag/source_code.jsx
+++ thomasguntenaar.near/devhub.components.atom.Tag/source_code.jsx
@@ -1,13 +1,20 @@
-const black = props.black;
 
-const Span = styled.span`
-  color: ${black ? "#818181" : "#00ec97"};
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 20px; /* 125% */
-`;
 
-const Tag = ({ tag }) => <Span>{tag}</Span>;
 
-return Tag(props);
+
+
+function Tag(props) {
+  const { black, tag } = props;
+
+  const Span = styled.span`
+    color: ${black ? "#818181" : "#00ec97"};
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 20px; /* 125% */
+  `;
+
+  return <Span>{tag}</Span>;
+}
+
+return Tag(props, context);
