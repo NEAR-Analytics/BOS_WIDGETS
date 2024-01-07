20b3d110ac1be30d6be70061f80d00fc0ba60aff
mattb.near <mattb.near>
2024-01-07 17:11:35 +0000

    Update NearBadger.Libs.Lens by mattb.near at 2024-01-07T17:11:35.208Z

---
+++ mattb.near/NearBadger.Libs.Lens/source_code.jsx
@@ -0,0 +1,63 @@
+const LensVerifier = VM.require("sdks.near/SDKs.Verifiers.Lens");
+
+const LensLib = {
+  createProof: (address, nearAccount, onCommit, onCancel) => {
+    LensVerifier.createProof(address, nearAccount)
+      .then(({ handle, signature }) => {
+        let identity = {
+          lens: {
+            name: handle,
+            signature,
+          },
+        };
+
+        Social.set(
+          {
+            identity,
+            index: {
+              identity: JSON.stringify({
+                key: "lens",
+                value: {
+                  ...identity.lens,
+                },
+              }),
+            },
+          },
+          {
+            onCommit,
+            onCancel,
+          }
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+        if (typeof onCancel === "function") {
+          onCancel();
+        }
+      });
+  },
+  verify: (handle, nearAccount, signature) =>
+    LensVerifier.verify(handle, nearAccount, signature),
+  listVerifiedProfiles: (limit) => {
+    return LensLib.getIdentity({
+      limit: limit || 5,
+    });
+  },
+  getVerifiedProfiles: (nearAccounts) => {
+    return LensLib.getIdentity({
+      accountId: nearAccounts,
+    });
+  },
+  getVerifiedProfile: (nearAccount) => {
+    return LensLib.getIdentity({
+      accountId: nearAccount,
+      limit: 1,
+    });
+  },
+  getIdentity: (options) => {
+    return Social.index("identity", "lens", {
+      order: "desc",
+      ...options,
+    });
+  },
+};
