4ec35320a3b93d75a01f2d500aeb741c06210dfb
abnakore.near <abnakore.near>
2024-01-07 23:34:47 +0000

    Update VoteChain by abnakore.near at 2024-01-07T23:34:47.191Z

--- abnakore.near/VoteChain/source_code.jsx
+++ abnakore.near/VoteChain/source_code.jsx
@@ -4,16 +4,21 @@ const accountId = context.accountId;
 // Pages that can be reached via the aside tab
 const [pages, setPage] = useState([
   {
-    name: "Home",
-    link: "https://near.org/sandbox#/abnakore.near/widget/AdminHome",
+    name: "All Votes",
+    link: "https://near.org/abnakore.near/widget/VoteChain",
   },
   {
-    name: "Manage Candidates",
-    link: "https://near.org/sandbox#/abnakore.near/widget/ManageCandidates",
+    name: "My Votes",
+    link: "https://near.org/abnakore.near/widget/VoteChain?tab=my_votes",
   },
   {
-    name: "Mange Parties",
-    link: "https://near.org/sandbox#/abnakore.near/widget/ManageParties",
+    name: "Watchlist",
+    link: "https://near.org/abnakore.near/widget/VoteChain?tab=watchlist",
+  },
+  {
+    name: "Create New Vote",
+    type: "button",
+    link: "https://near.org/abnakore.near/widget/CreateVote",
   },
 ]);
 return (
@@ -26,11 +31,13 @@ return (
             <div className="main-body">
               <div className="body-contents">
                 <div className="two-sides"></div>
-
                 <Widget
                   src="abnakore.near/widget/Aside"
                   props={{ objs: pages, active: "/admin/manage_candidates" }}
                 />
+                <div className="body-contents">
+                  <h1>...</h1>
+                </div>
               </div>
             </div>
           ),
@@ -41,3 +48,49 @@ return (
     )}
   </>
 );
+
+// // Get accountId
+// const accountId = context.accountId;
+
+// // Pages that can be reached via the aside tab
+// const [pages, setPage] = useState([
+//   {
+//     name: "Home",
+//     link: "https://near.org/sandbox#/abnakore.near/widget/AdminHome",
+//   },
+//   {
+//     name: "Manage Candidates",
+//     link: "https://near.org/sandbox#/abnakore.near/widget/ManageCandidates",
+//   },
+//   {
+//     name: "Mange Parties",
+//     link: "https://near.org/sandbox#/abnakore.near/widget/ManageParties",
+//   },
+// ]);
+// return (
+//   <>
+//     {accountId ? (
+//       <Widget
+//         src="abnakore.near/widget/Wrapper"
+//         props={{
+//           body: (
+//             <div className="main-body">
+//               <div className="body-contents">
+//                 <div className="two-sides"></div>
+//                 <Widget
+//                   src="abnakore.near/widget/Aside"
+//                   props={{ objs: pages, active: "/admin/manage_candidates" }}
+//                 />
+//                 <div className="body-contents">
+//                   <h1>...</h1>
+//                 </div>
+//               </div>
+//             </div>
+//           ),
+//         }}
+//       />
+//     ) : (
+//       <Widget src="abnakore.near/widget/SignIn.jsx" props={props} />
+//     )}
+//   </>
+// );
