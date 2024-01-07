397cf337a482ea7ff243d321a6973a18964fcb75
abnakore.near <abnakore.near>
2024-01-07 17:04:25 +0000

    Update CreateVote by abnakore.near at 2024-01-07T17:04:25.522Z

--- abnakore.near/CreateVote/source_code.jsx
+++ abnakore.near/CreateVote/source_code.jsx
@@ -1,26 +1,49 @@
 // Get accountId
 const accountId = context.accountId;
 
+// All the votes
+const [allVotes, setAllVotes] = useState([]);
+
+// Get all the votes
+const Data = Social.get(`abnakore.near/votes`);
+
+useEffect(() => {
+  //   console.log(Data);
+  if (Data === undefined) {
+    // Set the candidate to an empty list if there is no candidate
+    setAllVotes([]);
+  } else {
+    setAllVotes(JSON.parse(Data));
+  }
+  //   console.log(Data, "");
+}, [Data === null]);
+
+// Get the current date and time
+function getDateTime() {
+  var now = new Date();
+  now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+  return now.toISOString().slice(0, 16);
+}
+
 // New Vote
 const [newVote, setNewVote] = useState({
   name: "",
   desc: "",
   role: "",
   creator: "",
-  openDate: "",
-  closeDate: "",
+  openTime: getDateTime(),
+  closeTime: "",
+  createdOn: "",
+  limit: "",
   opened: true,
-  passcode: null,
+  passcode: "",
   candidates: [],
   parties: [],
+  voters: [],
 });
 
 // Parties
-const [parties, setParties] = useState([
-  { name: "All pe", acronym: "APP" },
-  { name: "All ee", acronym: "AEP" },
-  { name: "People De", acronym: "PDE" },
-]);
+const [parties, setParties] = useState([]);
 
 // New Party
 const [newParty, setNewParty] = useState({
@@ -35,7 +58,7 @@ const [candidates, setCandidates] = useState([]);
 const [newCandidate, setNewCandidate] = useState({
   name: "",
   party: "",
-  role: "",
+  role: newVote.role,
   votes: 0,
 });
 
@@ -60,7 +83,15 @@ function AddParty() {
     // Check for three letters of the acronym
     if (newParty.acronym.length <= 4 && newParty.acronym.length > 0) {
       setParties((prev) =>
-        prev.concat([{ ...newParty, acronym: newParty.acronym.toUpperCase() }])
+        prev.concat([
+          {
+            ...newParty,
+            acronym: newParty.acronym.toUpperCase(),
+            name: newParty.name
+              .toLowerCase()
+              .replace(/\b\w/g, (s) => s.toUpperCase()),
+          },
+        ])
       );
       setNewParty({ name: "", acronym: "" });
     }
@@ -70,24 +101,27 @@ function AddParty() {
 // Add the new party to the table
 function AddCandidate() {
   // Check if the data given is valid
-  console.log("ja");
-  console.log(candidates);
-  console.log(newCandidate);
 
   if (newCandidate.name !== "" && newCandidate.party !== "") {
     // check if there is another candidate with thesame party
-    // if (JSON.stringify(parties) !== JSON.stringify([])) {
     const filtered = candidates.filter((candidate) => {
-      console.log(candidate);
       return candidate.party.toLowerCase() === newCandidate.party.toLowerCase();
     });
     if (filtered.length > 0) {
       return;
     }
-    // }
 
     // Check for three letters of the acronym
-    setCandidates((prev) => prev.concat([{ ...newCandidate }]));
+    setCandidates((prev) =>
+      prev.concat([
+        {
+          ...newCandidate,
+          name: newCandidate.name
+            .toLowerCase()
+            .replace(/\b\w/g, (s) => s.toUpperCase()),
+        },
+      ])
+    );
     setNewCandidate({
       name: "",
       party: "",
@@ -95,6 +129,18 @@ function AddCandidate() {
       votes: 0,
     });
   }
+}
+
+// Function that get the unselected parties
+function getUnusedParties() {
+  // const filtered = candidates.filter((candidate) => {
+  //   return candidate.party.toLowerCase() === newCandidate.party.toLowerCase();
+  // });
+  const usedParties = candidates.map((candidate) => candidate.party);
+  const unusedParties = parties.filter(
+    (party) => !usedParties.includes(party.acronym)
+  );
+  return unusedParties;
 }
 
 // Update the value of the dropdown
@@ -112,10 +158,90 @@ const secText = styled.h5`
     text-align: center;
 `;
 
+// Hashing function
+function hash(text) {
+  var hashed = "";
+  for (var i = 0; i < text.length; i++) {
+    // console.log(text.charAt(i), "=", text.charCodeAt(i));
+    hashed += text.charCodeAt(i);
+  }
+  //   console.log(hashed);
+  return hashed;
+}
+
 // The main create vote Function
 function createVote() {
   console.log("creating vote");
   console.log(newVote);
+  // Check if the name is valid
+  if (newVote.name === "") {
+    console.log("Name cannot be Empty");
+    return;
+  }
+
+  // Validate desc
+  if (newVote.desc === "") {
+    console.log("Description cannot be Empty");
+    return;
+  }
+
+  // Validate role
+  if (newVote.role === "") {
+    console.log("Role cannot be Empty");
+    return;
+  }
+
+  // Validate start and end date and time
+  if (newVote.openTime !== "" && newVote.closeTime !== "") {
+    if (Date.parse(newVote.openTime) >= Date.parse(newVote.closeTime)) {
+      console.log("Opening time must be earlier than the close time");
+      return;
+    }
+  }
+
+  //   console.log("this", candidates);
+  // Set the roles of the candidates
+  const newCandidates = candidates.map((candidate) => ({
+    ...candidate,
+    role: newVote.role,
+  }));
+  //   console.log(newCandidates);
+
+  const tempVote = newVote;
+
+  // Set the curresponding values of the obj
+  tempVote.creator = accountId;
+  tempVote.opened =
+    newVote.closeTime !== ""
+      ? Date.parse(newVote.openTime) <= Date.parse(getDateTime()) &&
+        Date.parse(newVote.closeTime) > Date.parse(getDateTime())
+      : Date.parse(newVote.openTime) <= Date.parse(getDateTime());
+  tempVote.createdOn = getDateTime();
+  tempVote.candidates = newCandidates;
+  tempVote.parties = parties;
+  tempVote.passcode = hash(newVote.passcode);
+  tempVote.id = allVotes.length;
+
+  //   console.log("That", tempVote);
+
+  // Upload the data to socialDb
+  Social.set({ votes: allVotes.concat([tempVote]) });
+
+  setNewVote({
+    name: "",
+    desc: "",
+    role: "",
+    creator: "",
+    openTime: getDateTime(),
+    closeTime: "",
+    createdOn: "",
+    limit: "",
+    opened: true,
+    passcode: "",
+    candidates: [],
+    parties: [],
+    voters: [],
+  });
 }
 
 return (
@@ -138,6 +264,9 @@ return (
                       item: "name",
                       items: newVote,
                       setItem: setNewVote,
+                      otherAttributes: {
+                        value: newVote.name,
+                      },
                     }}
                   />
 
@@ -151,6 +280,9 @@ return (
                       item: "desc",
                       items: newVote,
                       setItem: setNewVote,
+                      otherAttributes: {
+                        value: newVote.desc,
+                      },
                     }}
                   />
                   <hr />
@@ -177,6 +309,9 @@ return (
                           item: "name",
                           items: newParty,
                           setItem: setNewParty,
+                          otherAttributes: {
+                            value: newParty.name,
+                          },
                         }}
                       />
                       <Widget
@@ -188,6 +323,9 @@ return (
                           item: "acronym",
                           items: newParty,
                           setItem: setNewParty,
+                          otherAttributes: {
+                            value: newParty.acronym,
+                          },
                         }}
                       />
                     </div>
@@ -217,6 +355,9 @@ return (
                           item: "name",
                           items: newCandidate,
                           setItem: setNewCandidate,
+                          otherAttributes: {
+                            value: newCandidate.name,
+                          },
                         }}
                       />
                       <select
@@ -229,7 +370,7 @@ return (
                         <option className="option" value="">
                           Select Party
                         </option>
-                        {parties.map((party) => (
+                        {getUnusedParties().map((party) => (
                           <option
                             className="option"
                             key={party.acronym}
@@ -246,48 +387,99 @@ return (
                   <Widget
                     src="abnakore.near/widget/Input.jsx"
                     props={{
-                      type: "password",
-                      placeholder: "Passcode",
-                      required: false,
-                      item: "passcode",
-                      items: newVote,
-                      setItem: setNewVote,
-                    }}
-                  />
-                  <Widget
-                    src="abnakore.near/widget/Input.jsx"
-                    props={{
                       type: "text",
                       placeholder: "Role",
                       required: true,
                       item: "role",
                       items: newVote,
                       setItem: setNewVote,
+                      otherAttributes: {
+                        value: newVote.role,
+                      },
+                    }}
+                  />
+
+                  <Widget
+                    src="abnakore.near/widget/Input.jsx"
+                    props={{
+                      type: "password",
+                      placeholder: "Passcode",
+                      required: false,
+                      item: "passcode",
+                      items: newVote,
+                      setItem: setNewVote,
+                      otherAttributes: {
+                        value: newVote.passcode,
+                      },
                     }}
                   />
                   <Widget
                     src="abnakore.near/widget/Input.jsx"
                     props={{
-                      type: "date",
-                      placeholder: "Open on",
+                      type: "number",
+                      placeholder: "Max number of voters",
                       required: false,
-                      item: "openDate",
+                      item: "limit",
                       items: newVote,
                       setItem: setNewVote,
+                      otherAttributes: {
+                        value: newVote.limit,
+                      },
                     }}
                   />
-                  <Widget
-                    src="abnakore.near/widget/Input.jsx"
-                    props={{
-                      type: "date",
-                      placeholder: "Close on",
-                      required: false,
-                      item: "closeDate",
-                      items: newVote,
-                      setItem: setNewVote,
-                    }}
-                  />
-                  <button onClick={createVote}>Submit</button>
+                  <div className="flex">
+                    <Widget
+                      src="abnakore.near/widget/Input.jsx"
+                      props={{
+                        type: "datetime-local",
+                        placeholder: "Open on",
+                        required: true,
+                        item: "openTime",
+                        items: newVote,
+                        setItem: setNewVote,
+                        otherAttributes: {
+                          min: getDateTime(),
+                          value: newVote.openTime,
+                          onChange: (e) => {
+                            if (e.target.value !== "") {
+                              setNewVote({
+                                ...newVote,
+                                openTime: e.target.value,
+                              });
+                            }
+                          },
+                        },
+                        styles: {
+                          width: "120%",
+                        },
+                      }}
+                    />
+                    <Widget
+                      src="abnakore.near/widget/Input.jsx"
+                      props={{
+                        type: "datetime-local",
+                        placeholder: "Close on",
+                        required: true,
+                        item: "closeTime",
+                        items: newVote,
+                        setItem: setNewVote,
+                        otherAttributes: {
+                          min: newVote.openTime,
+                          value: newVote.closeTime,
+                          onChange: (e) => {
+                            setNewVote({
+                              ...newVote,
+                              closeTime: e.target.value,
+                            });
+                          },
+                        },
+                        styles: {
+                          width: "100%",
+                        },
+                      }}
+                    />
+                  </div>
+                  <button onClick={createVote}>Create</button>
                 </div>
               </div>
             </div>
