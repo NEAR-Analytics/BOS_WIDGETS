23570fa2453afa72be5156497104a9d4eb27eb48
dydx4.near <dydx4.near>
2024-01-08 16:23:49 +0000

    Update Index by dydx4.near at 2024-01-08T16:23:49.396Z

--- dydx4.near/Index/source_code.jsx
+++ dydx4.near/Index/source_code.jsx
@@ -1,3 +1,41 @@
+State.init({
+  orderSize: "0.0001",
+  orderPrice: "2500",
+  orderMarketId: "ETH-USD",
+  orderType: "LIMIT",
+  nonce: 0,
+});
+
+const OrderMarket = styled.div`{
+  display: inline-block;
+  min-width: 180px;
+  }`;
+const OrderSide = styled.div`{
+    min-width: 50px;
+    text-align: center;
+    display: inline-block;
+    border-radius: 0.25rem;
+    letter-spacing: 0.04em;
+    user-select: none;
+    padding: 0.156rem 0.219rem 0.156rem 0.25rem;
+    color: ${(props) =>
+      ["BUY", "LONG"].includes(props.side) ? "#21a677" : "#e45353"};
+    background-color: ${(props) =>
+      ["BUY", "LONG"].includes(props.side) ? "#3eb68a29" : "#d57676a1"};
+}`;
+
+const OrderSize = styled.div`{
+    display: inline-block;
+    text-align: center;
+    min-width: 190px;
+    border-radius: 0.25rem;
+    letter-spacing: 0.04em;
+    user-select: none;
+    padding: 0.156rem 0.219rem 0.156rem 0.25rem;
+    color: #757575;
+    background-color: #d1d4dc;
+}`;
+
 const etherProviderEnabled = !!Ethers?.provider();
 
 if (etherProviderEnabled) {
@@ -92,12 +130,13 @@ const getWalletFromEvmSignature = (signature) => {
         dydx_account: dydx_account.address,
         wallet: wallet,
         mnemonic,
+        updateMarketPrice: true,
       });
     });
   });
 };
 
-console.log("state wallet", state.wallet);
+// console.log("state wallet", state.wallet);
 
 const apiUrl = "https://indexer.v4testnet.dydx.exchange/v4";
 const accountId = state.dydx_account;
@@ -118,55 +157,51 @@ const loadAccount = () => {
   ).then((r) => State.update({ orders: r?.body }));
 };
 
-const myPlaceOrder = () => {
-  /*
-   {"ETH-USD":{"market":"ETH-USD","status":"OPEN","side":"SHORT","size":"-0.006","maxSize":"-0.002","entryPrice":"2314.53333333333333333333","exitPrice":null,"realizedPnl":"0.011682","unrealizedPnl":"0.99803070199999999999998","createdAt":"2023-12-07T14:46:04.743Z","createdAtHeight":"2985583","closedAt":null,"sumOpen":"0.006","sumClose":"0","netFunding":"0.011682"}}
-
-   {
-  "clobPairId": "1",
-  "ticker": "ETH-USD",
-  "status": "ACTIVE",
-  "lastPrice": "0",
-  "oraclePrice": "2152.855895",
-  "priceChange24H": "-59.931317",
-  "volume24H": "416750.4603",
-  "trades24H": 7805,
-  "nextFundingRate": "0",
-  "initialMarginFraction": "0.05",
-  "maintenanceMarginFraction": "0.03",
-  "basePositionNotional": "1000000",
-  "openInterest": "1267.480",
-  "atomicResolution": -9,
-  "quantumConversionExponent": -9,
-  "tickSize": "0.1",
-  "stepSize": "0.001",
-  "stepBaseQuantums": 1000000,
-  "subticksPerTick": 100000
+function getRandomClientId() {
+  let min = 1000000;
+  let max = 99999999;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-   */
-  //let subaccountNumber = 0;
-  let marketId = "BTC-USD";
-  let type = "LIMIT";
-  let side = "BUY";
-  let price = 40_500;
-  let size = 0.0001;
-  let clientId = 1234;
+const updateOrders = () => {
+  const nonce = state.nonce ?? 0;
+  State.update({ nonce: nonce + 1 });
+};
+
+if (state.updateMarketPrice == true) {
+  const url = `https://indexer.v4testnet.dydx.exchange/v4/perpetualMarkets?ticker=${state.orderMarketId}`;
+  let data = fetch(url);
+  if (data.ok) {
+    console.log(data);
+    let price = data?.body?.markets?.[state.orderMarketId]?.oraclePrice;
+    if (price) {
+      State.update({ orderPrice: price, updateMarketPrice: false });
+    } else {
+      State.update({ updateMarketPrice: false });
+    }
+  }
+}
+
+const placeUserOrder = (side) => {
+  if (
+    !state.orderMarketId ||
+    !state.orderPrice ||
+    !state.orderSize ||
+    !state.orderType
+  ) {
+    console.log("NO DATA");
+    return;
+  }
+
+  let marketId = state.orderMarketId;
+  let type = state.orderType;
+  let price = Number(state.orderPrice);
+  let size = Number(state.orderSize);
+  let clientId = getRandomClientId();
   let timeInForce = "GTT";
   let execution = "DEFAULT";
   let postOnly = false;
   let reduceOnly = false;
-  console.log("state.all_accounts", state.all_accounts);
-  console.log("state.wallet", state.wallet);
-  console.log("state.account", state.account);
-  /*LocalWallet.fromMnemonic(state.mnemonic, "dydx").then((localWallet) => {
-    console.log("localWallet", localWallet);*/
-
-  //const subAccount = new SubaccountClient(localWallet, 0);
-  console.log("subAccount", subAccount);
-  //console.log("compositeClient", CompositeClient);
-  //console.log("Network", Network);
-  //console.log("compositeClient", CompositeClient.placeOrder);
 
   let network_config = {
     env: "dydx-testnet-4",
@@ -197,15 +232,8 @@ const myPlaceOrder = () => {
     network_config.indexerConfig,
     network_config.validatorConfig
   );
-  console.log("network 333", network);
-  //CompositeClient.connect(Network.testnet()).then((compositeClient) => {
-  //let _cc0 = await CompositeClient.connect(network);
-  //console.log("cco", _cc0);
 
-  //let _cc = CompositeClient.connect(network).then((compositeClient) => {
-  console.log("subAccount", subAccount);
   let params = {
-    //subaccount: subAccount,
     marketId,
     type,
     side,
@@ -213,42 +241,22 @@ const myPlaceOrder = () => {
     size,
     clientId,
     timeInForce,
-    goodTilTimeInSeconds: 0,
+    goodTilTimeInSeconds: 999999,
+    orderFlags: "0",
     execution,
     postOnly,
     reduceOnly,
     triggerPrice,
   };
 
-  let _cc = placeOrder(network, state.mnemonic, "dydx", 0, params).then(
+  console.log(params);
+
+  placeOrder(network, state.mnemonic, "dydx", 0, params).then(
     (placeOrderResp) => {
-      /*if (!compositeClient) return;
-    console.log("compositeClient", compositeClient);
-    console.log("compositeClient.placeOrder", compositeClient.placeOrder);
-    compositeClient
-      .placeOrder(
-        subAccount,
-        marketId,
-        type,
-        side,
-        price,
-        size,
-        clientId,
-        timeInForce,
-        goodTilTimeInSeconds ?? 0,
-        execution,
-        postOnly,
-        reduceOnly,
-        triggerPrice ?? undefined
-      )
-      .then((resp) => {
-        console.log("placeOrder resp ", resp);
-      });*/
       console.log("placeOrder resp ", placeOrderResp);
+      updateOrders();
     }
   );
-  console.log("_cc", _cc);
-  //});
 };
 
 if (state.dydx_account == undefined) {
@@ -266,11 +274,9 @@ if (state.dydx_account == undefined) {
     },
   };
 
-  /*
-  getWalletFromEvmSignature(
-    "0xb33020bc2b4c2be8cf28dd6fcaf06dc94f856cd85041ca34afc648c0a1a12298361d3c494404c1b7a6cdec71305c81da8ad318434dac6d9a706c500a650391fa1b"
-  );
-  */
+  /*return getWalletFromEvmSignature(
+    "0x4405ee8175ef8e4b0f760145f732badb93b0c826f69108d10339a4e32648d97a377dd3119ff98d8260a18aa3b221adb56d29ccd2361fcbe5474e2cbc14b949561c"
+  );*/
 
   Ethers.provider()
     .getSigner()
@@ -283,47 +289,171 @@ if (state.dydx_account == undefined) {
 
   return (
     <div>
-      {Test1}
-      <button
-        onClick={() => {
-          myPlaceOrder();
-        }}
-      >
-        Place Order
-      </button>
-      <ul>
-        <li>Address: {state.account.address}</li>
-        <li>Equity: {state.account.equity}</li>
-        <li>Free Collateral: {state.account.freeCollateral}</li>
-        <li>
-          Open Perpetual Positions:{" "}
-          {JSON.stringify(state.account.openPerpetualPositions)}
-        </li>
+      {state.account.address && (
+        <div>
+          <h2>Account Details</h2>
+          <div>
+            <div>Address: {state.account.address}</div>
+            <div>Equity: ${state.account.equity}</div>
+            <div>Free Collateral: ${state.account.freeCollateral}</div>
 
-        <li>
-          Orders:{" "}
-          <ol>
+            {Object.keys(state.account.openPerpetualPositions ?? []).length >
+              0 && (
+              <>
+                <h2 class="mt-5">Open Perpetual Positions</h2>
+                {Object.keys(state.account.openPerpetualPositions ?? []).map(
+                  (ticker) => {
+                    let order = state.account.openPerpetualPositions[ticker];
+                    return (
+                      <div class="mb-2">
+                        <OrderSide side={order.side}>{order.side}</OrderSide>
+                        <OrderMarket>
+                          {ticker} at ${order.entryPrice}
+                        </OrderMarket>{" "}
+                        <OrderSize> Size: {order.size}</OrderSize>
+                        {order.createdAt
+                          ? new Date(order.createdAt).toLocaleString()
+                          : ""}
+                        <OrderSize>
+                          Realized PNL: {new Big(order.realizedPnl).toFixed(4)}
+                        </OrderSize>
+                        <OrderSize>
+                          Unrealized PNL:{" "}
+                          {new Big(order.unrealizedPnl).toFixed(4)}
+                        </OrderSize>
+                      </div>
+                    );
+                  }
+                )}
+              </>
+            )}
+          </div>
+        </div>
+      )}
+      <div>
+        <h2 class="mt-5">Place Order</h2>
+        <div class="mb-3 row">
+          <label for="orderPrice" class="col-sm-2 col-form-label">
+            Type
+          </label>
+          <div class="col-sm-10">
+            <select
+              class="form-select"
+              aria-label="Select a pair"
+              onChange={(e) => State.update({ orderType: e.target.value })}
+            >
+              <option value="LIMIT" selected>
+                Limit
+              </option>
+              <option value="MARKET">Market</option>
+            </select>
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <label for="orderPrice" class="col-sm-2 col-form-label">
+            Pair
+          </label>
+          <div class="col-sm-10">
+            <select
+              class="form-select"
+              aria-label="Select a pair"
+              onChange={(e) =>
+                State.update({
+                  orderMarketId: e.target.value,
+                  updateMarketPrice: true,
+                })
+              }
+            >
+              <option value="BTC-USD">BTC-USD</option>
+              <option value="ETH-USD" selected>
+                ETH-USD
+              </option>
+            </select>
+          </div>
+        </div>
+        {state.orderType == "LIMIT" && (
+          <div class="mb-3 row">
+            <label for="orderPrice" class="col-sm-2 col-form-label">
+              Price
+            </label>
+            <div class="col-sm-10">
+              <input
+                type="text"
+                class="form-control"
+                id="orderPrice"
+                value={state.orderPrice}
+                onChange={(e) => State.update({ orderPrice: e.target.value })}
+              />
+            </div>
+          </div>
+        )}
+        <div class="mb-3 row">
+          <label for="orderSize" class="col-sm-2 col-form-label">
+            Size
+          </label>
+          <div class="col-sm-10">
+            <input
+              type="text"
+              class="form-control"
+              id="orderSize"
+              value={state.orderSize}
+              onChange={(e) => State.update({ orderSize: e.target.value })}
+            />
+          </div>
+        </div>
+
+        <div class="mb-3 row">
+          <div class="col">
+            <button
+              class="btn btn-success"
+              onClick={() => placeUserOrder("BUY")}
+            >
+              Buy
+            </button>
+            <button
+              class="btn btn-danger"
+              onClick={() => placeUserOrder("SELL")}
+            >
+              Sell
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        {(state.orders ?? []).length == 0 && <div>Orders were not found</div>}
+        {(state.orders ?? []).length > 0 && (
+          <>
+            <h2 class="mt-5">Orders Log</h2>
             {(state.orders ?? []).map((order) => (
-              <li>
-                {order.side} {order.ticker} at {order.price}. Size: {order.size}
-                , filled: {order.totalFilled} . Updated: {order.updatedAt}
-                {/*JSON.stringify(order)*/}
-              </li>
+              <div class="mb-2">
+                <OrderSide side={order.side}>{order.side}</OrderSide>
+                <OrderMarket>
+                  {order.ticker} at ${order.price}
+                </OrderMarket>{" "}
+                <OrderSize>
+                  {" "}
+                  Size: {order.size} / Filled: {order.totalFilled}
+                </OrderSize>
+                {order.updatedAt
+                  ? new Date(order.updatedAt).toLocaleString()
+                  : ""}
+              </div>
             ))}
-          </ol>
-        </li>
-      </ul>
+          </>
+        )}
+      </div>
       <button
         onClick={() => {
-          const nonce = state.nonce ?? 0;
-          State.update({ nonce: nonce + 1 });
+          updateOrders();
         }}
       >
         Refresh
       </button>
+      {/*
       <hr />
       state:
       {JSON.stringify(state)}
+      */}
     </div>
   );
 }
