4546b65e6f3d946407fae7c3bae624a7f108c726
abnakore.near <abnakore.near>
2024-01-09 20:53:11 +0000

    Update VoteChain by abnakore.near at 2024-01-09T20:53:11.634Z

--- abnakore.near/VoteChain/source_code.jsx
+++ abnakore.near/VoteChain/source_code.jsx
@@ -9,6 +9,12 @@ padding: 20px 10px;
 @media screen and (min-width: 1200px) {
   width: 90%;
 }
+@media screen and (min-width: 1250px) {
+  width: 80%;
+}
+@media screen and (min-width: 1400px) {
+  width: 90%;
+}
 
 `;
 
