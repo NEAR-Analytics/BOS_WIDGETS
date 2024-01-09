f66aceee8afee772e7ffeeaf6db67c81b736ff42
abnakore.near <abnakore.near>
2024-01-09 20:36:04 +0000

    Update Wrapper by abnakore.near at 2024-01-09T20:36:04.479Z

--- abnakore.near/Wrapper/source_code.jsx
+++ abnakore.near/Wrapper/source_code.jsx
@@ -6,7 +6,7 @@ const Wrapper = styled.div`
   place-items: center;
   min-width: 320px;
   min-height: 100vh;
-  width: 100%;
+  width: 100vw;
 
 
   font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
