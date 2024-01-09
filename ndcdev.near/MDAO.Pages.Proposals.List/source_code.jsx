1632fc33099c028d50f63efff179585394188aef
ndcdev.near <ndcdev.near>
2024-01-09 22:15:37 +0000

    Update MDAO.Pages.Proposals.List by ndcdev.near at 2024-01-09T22:15:37.810Z

--- ndcdev.near/MDAO.Pages.Proposals.List/source_code.jsx
+++ ndcdev.near/MDAO.Pages.Proposals.List/source_code.jsx
@@ -8,31 +8,19 @@ const Container = styled.div`
   }
 `;
 
-const Button = styled.a`
-  border-radius: 10px;
-  background: #151718;
-  color: white !important;
-  text-decoration: none;
-  display: flex;
-  gap: 1rem;
-  box-shadow: 0px 20px 30px 0px rgba(0, 0, 0, 0.25);
-  padding: 10px 40px 10px 25px;
-
-  &:hover {
-    text-decoration: none;
-  }
-`;
-
 const items = Social.index("graph", "v3.ndc.mdao", { order: "desc" });
 
 return (
   <Container>
     <h1>{props.type === "proposal" ? "Proposals" : "Reports"} List</h1>
     <div className="d-flex justify-content-end mb-4">
-      <Button href="/ndcdev.near/widget/MDAO.App?page=createProposal">
+      <a
+        className="btn-primary"
+        href="/ndcdev.near/widget/MDAO.App?page=createProposal"
+      >
         <i className="bi bi-plus-circle" />
         Post
-      </Button>
+      </a>
     </div>
     <div className="d-flex flex-column gap-4">
       {items &&
