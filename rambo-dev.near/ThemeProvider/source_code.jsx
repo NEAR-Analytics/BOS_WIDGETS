a875ba650e24d8bdca6d88ca637b7954b67b6dde
rambo-dev.near <rambo-dev.near>
2024-01-10 15:49:22 +0000

    Update ThemeProvider by rambo-dev.near at 2024-01-10T15:49:22.365Z

--- rambo-dev.near/ThemeProvider/source_code.jsx
+++ rambo-dev.near/ThemeProvider/source_code.jsx
@@ -2,21 +2,27 @@ const colors = {
   yellow500: "#FFAF51",
   seablue500: "#51FFEA",
   blue500: "#51B6FF",
-
   bg1: "#0B0C14",
   bg2: "#23242B",
-
   black100: "#000000",
   black50: "Black/50",
-
   white100: "#FFFFFF",
   white50: "White/50",
-
   error: "#FD2A5C",
   success: "#38C793",
   warning: "#F17B2C",
 };
 
-Storage.set("theme", colors);
+const props = { colors };
 
-return <>{props.children}</>;
+const StyledThemeProvider = styled.div`
+  display: none;
+  visibility: hidden;
+  opacity: 0;
+`;
+
+function ThemeProvider({ children, props }) {
+  return <StyledThemeProvider {...props}>{children}</StyledThemeProvider>;
+}
+
+return { ThemeProvider };
