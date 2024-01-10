774ee107a9c6994c2e3b4a2db3274954fa859f5c
rambo-dev.near <rambo-dev.near>
2024-01-10 14:07:32 +0000

    Update BuildDAOBullet by rambo-dev.near at 2024-01-10T14:07:32.695Z

--- rambo-dev.near/BuildDAOBullet/source_code.jsx
+++ rambo-dev.near/BuildDAOBullet/source_code.jsx
@@ -1,16 +1,19 @@
+const ThemeProvider = VM.require("rambo-dev.near/widget/ThemeProvider");
+const theme = Storage.get("theme");
+
 const Bullet = styled.div`
   width: fit-content;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 4px 12px;
-  background: rgba(81, 255, 234, 0.20);
-  color: rgba(81, 255, 234, 1);
-  border: 1px solid rgba(81, 255, 234, 0.20);
+  background: ${(props) => `${theme.seablue500}33`};
+  color: ${(props) => theme.seablue500};
+  border: 1px solid ${(props) => `${theme.seablue500}33`};
   font-family: Satoshi, sans-serif;
   font-size: 0.875rem;
   font-weight: 500;
   border-radius: 8px;
 `;
 
-return <Bullet>{props.text}</Bullet>;
+return <Bullet>{props.children}</Bullet>;
