6ffcc1282e9ddf7b58e117ce3566710dbb22d1e8
orasci-contributor.near <orasci-contributor.near>
2024-01-10 17:53:26 +0000

    Update Potlock.Home.SearchBar by orasci-contributor.near at 2024-01-10T17:53:26.368Z

--- orasci-contributor.near/Potlock.Home.SearchBar/source_code.jsx
+++ orasci-contributor.near/Potlock.Home.SearchBar/source_code.jsx
@@ -37,6 +37,7 @@ const SearchBarContainer = styled.div`
 const SearchBar = styled.input`
   background: none;
   width: 100%;
+  border-radius: 2px;
   outline: none;
   border: none;
   color: #525252;
