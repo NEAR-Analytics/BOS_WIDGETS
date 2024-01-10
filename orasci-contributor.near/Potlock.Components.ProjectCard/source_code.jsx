0d92866872e154f4aa9c4d3b1d97a8fb51b1053e
orasci-contributor.near <orasci-contributor.near>
2024-01-10 12:50:31 +0000

    Update Potlock.Components.ProjectCard by orasci-contributor.near at 2024-01-10T12:50:31.235Z

--- orasci-contributor.near/Potlock.Components.ProjectCard/source_code.jsx
+++ orasci-contributor.near/Potlock.Components.ProjectCard/source_code.jsx
@@ -2,13 +2,13 @@ const { id, review_notes, status } = props;
 
 const cardData = Social.getr(`${id}/profile`);
 
-console.log(cardData);
+const MAX_DESCRIPTION_LENGTH = 60;
 
 const Card = styled.div`
   display: flex;
   flex-direction: column;
   width: 408px;
-  border-radius: 12px;
+  border-radius: 2px;
   background: white;
   box-shadow: 0px -2px 0px #dbdbdb inset;
   border: 1px solid #dbdbdb;
@@ -22,9 +22,62 @@ const Card = styled.div`
   overflow: hidden;
 `;
 
+const CardBody = styled.div`
+    position: relative;
+    padding: 24px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`;
+
 const CardImage = styled.img`
   height: 150px;
   width: 100%;
+`;
+
+const CardTitle = styled.div`
+    color: #292929;
+    font-size: 17px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 24px;
+`;
+
+const CardDescription = styled.div`
+    color: #292929;
+    font-size: 17px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 28px;
+`;
+
+const CardTag = styled.div`
+    color: #292929;
+    width: max-content;
+    white-space: nowrap;
+    padding: 8px;
+    border-radius: 4px;
+    box-shadow: 0px -1px 0px 0px #C7C7C7 inset, 0px 0px 0px 0.5px #C7C7C7; 
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px;
+`;
+
+const CardTagContainer = styled.div`
+    display: flex;
+    gap: 12px;
+    flex-wrap: wrap;
+`;
+
+const CardAvatar = styled.img`
+    position: absolute;
+    top: -20px;
+    left: 24px;
+    border-radius: 100%;
+    width: 40px;
+    height: 40px;
+    border: 3px solid #FFF;
 `;
 
 return (
@@ -37,5 +90,17 @@ return (
       }
       alt="project background image"
     />
+    <CardBody>
+      <CardAvatar src={cardData?.image} alt="project avatar" />
+      <CardTitle>{cardData?.name}</CardTitle>
+      <CardDescription>
+        {cardData?.description.length > MAX_DESCRIPTION_LENGTH
+          ? cardData.description.slice(0, MAX_DESCRIPTION_LENGTH) + "..."
+          : cardData.description}
+      </CardDescription>
+      <CardTagContainer>
+        <CardTag>{cardData?.category}</CardTag>
+      </CardTagContainer>
+    </CardBody>
   </Card>
 );
