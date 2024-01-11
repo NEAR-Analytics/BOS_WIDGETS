5bb39f69352654915d8ff74fe691fa2660c12731
sdks.near <sdks.near>
2024-01-11 20:14:30 +0000

    Update SDKs.Lens.API.HealthAPI by sdks.near at 2024-01-11T20:14:30.274Z

---
+++ sdks.near/SDKs.Lens.API.HealthAPI/source_code.jsx
@@ -0,0 +1,13 @@
+const $ = VM.require(`sdks.near/widget/Loader`);
+const { Health } = $("@sdks/lens/queries");
+
+return {
+  ping: LightClient.graphql(Health.PING_QUERY)
+    .then((payload) => {
+      const response = payload.body.data.ping;
+      return response;
+    })
+    .catch((error) => {
+      return "ponk";
+    }),
+};
