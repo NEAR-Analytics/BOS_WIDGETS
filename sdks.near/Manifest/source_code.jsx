d81f65589ad7f548f778a423b03497ae2b7e8439
sdks.near <sdks.near>
2024-01-11 20:04:53 +0000

    Update Manifest by sdks.near at 2024-01-11T20:04:53.942Z

--- sdks.near/Manifest/source_code.jsx
+++ sdks.near/Manifest/source_code.jsx
@@ -3,11 +3,8 @@ return {
     "eth-signer": "SDKs.EthereumSigner.Main",
     lens: {
       definitions: ["SDKs.Lens.Constants", "SDKs.Lens.Interfaces"],
-      api: ["SDKs.Lens.API.Auth"],
-      queries: [
-          "SDKs.Lens.Queries.Auth",
-          "SDKs.Lens.Queries.Health"
-      ],
+      api: ["SDKs.Lens.API.AuthAPI", "SDKs.Lens.API.HealthAPI"],
+      queries: ["SDKs.Lens.Queries.Auth", "SDKs.Lens.Queries.Health"],
       sdk: ["SDKs.Lens.Main"],
     },
     "light-client": "SDKs.LightClient",
