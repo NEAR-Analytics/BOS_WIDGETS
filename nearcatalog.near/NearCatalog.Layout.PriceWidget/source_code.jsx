594d41649fe9ac19d7910a2e8c54acf8f0d5115a
nearcatalog.near <nearcatalog.near>
2024-01-11 04:29:05 +0000

    Update NearCatalog.Layout.PriceWidget by nearcatalog.near at 2024-01-11T04:29:05.865Z

--- nearcatalog.near/NearCatalog.Layout.PriceWidget/source_code.jsx
+++ nearcatalog.near/NearCatalog.Layout.PriceWidget/source_code.jsx
@@ -2,23 +2,28 @@ if (!props?.platform?.coingecko) {
   return "";
 }
 console.log("PriceWidget props: ", props);
-const data = fetch(`https://api.coingecko.com/api/v3/coins/${props.platform.coingecko}`, {
-  subscribe: true,
-  method: "GET",
-  headers: {
-    Accept: "*/*",
-  },
-});
-if(!data) return <></>
+const data = fetch(
+  `https://api.coingecko.com/api/v3/coins/${props.platform.coingecko}`,
+  {
+    subscribe: true,
+    method: "GET",
+    headers: {
+      Accept: "*/*",
+    },
+  }
+);
+console.log("price data: ", data);
+
+if (!data) return <></>;
 
 return (
   <div className="rounded-4 p-3 mb-3">
-    {data !== null ? (
+    {data !== null && data.ok ? (
       <p>
         <div class="d-flex clearfix flex-wrap flex-column flex-sm-row">
           <div class="p-2">
             <div>
-              <img src={props.icon.small} style={{width:"25px"}}/>
+              <img src={props.icon.small} style={{ width: "25px" }} />
               <b>{props.symbol}</b>/usd
             </div>
             <h1>
@@ -42,14 +47,13 @@ return (
           <div class="p-2">
             ATH
             <p class="text-success">
-              ${data.body.market_data.ath.usd.toFixed(8)}
+              ${data.body?.market_data?.ath.usd.toFixed(8)}
             </p>
           </div>
           <div class="p-2">
             24h high
             <p>
-
-              <b>${data.body.market_data.high_24h.usd.toFixed(8)} </b>
+              <b>${data.body?.market_data?.high_24h.usd.toFixed(8)} </b>
             </p>
           </div>
           <div class="p-2">
@@ -67,7 +71,6 @@ return (
           <div class="p-2">
             24h low
             <p>
-
               <b>${data.body.market_data.low_24h.usd.toFixed(8)} </b>
             </p>
           </div>
@@ -75,11 +78,13 @@ return (
             Market Cap
             <p>
               <b>
-                {
-                  data.body.market_data.market_cap?.usd ? "$" + data.body.market_data.market_cap.usd
-                    .toFixed(0)
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",") : "-"}
+                {data.body.market_data.market_cap?.usd
+                  ? "$" +
+                    data.body.market_data.market_cap.usd
+                      .toFixed(0)
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                  : "-"}
               </b>
             </p>
           </div>
@@ -88,29 +93,32 @@ return (
             Circulating Supply
             <p>
               <b>
-                {
-                 data.body.market_data?.circulating_supply ?  data.body.market_data.circulating_supply
-                    .toFixed(0)
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",") : "-"}
+                {data.body.market_data?.circulating_supply
+                  ? data.body.market_data.circulating_supply
+                      .toFixed(0)
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                  : "-"}
               </b>
             </p>
           </div>
         </div>
         <div>
-            <a
-              target="_blank"
-              style={{ color: "inherit" }}
-              variant="caption"
-              rel="nofollow"
-              href={`https://www.coingecko.com/en/coins/` + props.platform.coingecko }
-            >
-             View on CoinGecko
-            </a>
+          <a
+            target="_blank"
+            style={{ color: "inherit" }}
+            variant="caption"
+            rel="nofollow"
+            href={
+              `https://www.coingecko.com/en/coins/` + props.platform.coingecko
+            }
+          >
+            View on CoinGecko
+          </a>
         </div>
       </p>
     ) : (
-      <div>Loading ...</div>
+      <div> {data.error ? data.error : "Loading ..."} </div>
     )}
   </div>
 );
