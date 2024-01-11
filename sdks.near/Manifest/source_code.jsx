b05d08d9cb5360215f378700e2fe00143c9c4126
sdks.near <sdks.near>
2024-01-11 00:31:29 +0000

    Update Manifest by sdks.near at 2024-01-11T00:31:29.372Z

--- sdks.near/Manifest/source_code.jsx
+++ sdks.near/Manifest/source_code.jsx
@@ -1,5 +1,9 @@
 return {
-    libs: {
-        "eth-signer": "SDKs.EthereumSigner.Main"
-    }
+  libs: {
+    "eth-signer": "SDKs.EthereumSigner.Main",
+    "lens": [
+        "SDKs.Lens.Main",
+        "SDKs.Lens.Interfaces"
+    ]
+  },
 };
