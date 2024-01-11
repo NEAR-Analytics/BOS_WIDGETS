eb46f51e0351693c10789efdf1ba6bc24347c89d
sdks.near <sdks.near>
2024-01-11 22:36:46 +0000

    Update SDKs.Lens.API.AuthAPI by sdks.near at 2024-01-11T22:36:46.962Z

--- sdks.near/SDKs.Lens.API.AuthAPI/source_code.jsx
+++ sdks.near/SDKs.Lens.API.AuthAPI/source_code.jsx
@@ -1,10 +1,18 @@
-const AUTH_CHALLENGE_QUERY = "";
-const AUTH_AUTHENTICATE_QUERY = "";
+const $ = VM.require(`sdks.near/widget/Loader`);
+const { Auth } = $("@sdks/lens/queries");
 
 return {
+  profiles: (Client, { limit, evmAddress, includeOwned }) => {
+    return Client.graphql(Auth.PROFILES_MANAGED_QUERY, {
+      limit: limit || "Ten",
+      cursor: {},
+      for: evmAddress,
+      includeOwned: includeOwned || true,
+    }).then((payload) => payload.body.data.profilesManaged.items || []);
+  },
   login: (Client, { signedBy, profileId }) => {
     return new Promise((resolve, reject) => {
-      return Client.graphql(AUTH_CHALLENGE_QUERY, {
+      return Client.graphql(Auth.AUTH_CHALLENGE_QUERY, {
         signedBy,
         for: profileId,
       }).then((data) => {
