d9548c4b6ffb670ec34ab5a1a7a2831a5bf093c0
megha19.near <megha19.near>
2024-01-11 11:57:04 +0000

    Update home by megha19.near at 2024-01-11T11:57:04.040Z

---
+++ megha19.near/home/source_code.jsx
@@ -0,0 +1,131 @@
+const {
+    LargeContainer,
+    LogoImg,
+    GridImg,
+    SmallContainer,
+    SocialIconImg,
+    SmallImg
+} = VM.require("megha19.near/widget/styles");
+
+const {
+    CharterURL,
+    CharterImg,
+    MarketingImg,
+    LogoURL,
+    Logo,
+    MarketingTextImg,
+    LogoTextImg,
+    PartnershipsLogoImg,
+    EducationalImg,
+    EducationalTextImg,
+    EducationURL,
+    MarketingURL,
+    PartnershipsImg,
+    PartnershipsTextImg,
+    CharterTextImg
+} = VM.require("megha19.near/widget/core.lib.url");
+
+const BodyText = () => {
+    return (
+        <div className="body-text">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum Lorem ipsum dolor sit amet,
+            consectetuer adipiscing elit, sed diam nonummy nibh euismod
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi
+            enim ad minim veniam, quis nostrud exerci tation ullamcorper
+            suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis
+            autem vel eum iriure dolor in hendrerit in vulputate Ut wisi enim ad
+            minim veniam, quis nostrud exerci tation ullamcorper suscipit
+            lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum
+            iriure dolor in hendrerit in vulputate
+        </div>
+    );
+};
+
+const LargeScreenComponent = () => {
+    return (
+        <LargeContainer>
+            <div class="d-flex gap-5">
+                <div class="d-flex flex-column gap-5 flex-08">
+                    <div class="h1-custom">
+                        Welcome to <br /> She is Near
+                    </div>
+                    <div class="h3-custom">
+                        The women’s DAO for the <br /> NEAR ecosystem{" "}
+                    </div>
+                    <div class="h3-custom">How did we get here?</div>
+                    <BodyText />
+                </div>
+                <div class="d-flex flex-column gap-3 align-items-center flex-1">
+                    <LogoImg src={Logo} />
+                    <div class="grid-4">
+                        <a href={CharterURL} target="_blank" rel="noreferrer">
+                            <GridImg src={CharterImg} />
+                        </a>
+                        <a href={EducationURL} target="_blank" rel="noreferrer">
+                            <GridImg src={EducationalImg} />
+                        </a>
+                        <a href={MarketingURL} target="_blank" rel="noreferrer">
+                            <GridImg src={MarketingImg} />
+                        </a>
+                        <a
+                            href={PartnershipsURL}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <GridImg src={PartnershipsImg} />
+                        </a>
+                    </div>
+                </div>
+                <Widget
+                    src={"megha19.near/widget/components.socialLinks"}
+                    props={{ showRow: false }}
+                />
+            </div>
+        </LargeContainer>
+    );
+};
+
+const MobileScreenComponent = () => {
+    return (
+        <SmallContainer>
+            <div className="d-flex flex-column gap-4">
+                <div class="h1-custom">Welcome to She is Near</div>
+                <div class="h3-custom">
+                    The women’s DAO for the NEAR ecosystem{" "}
+                </div>
+                <img src={Logo} height={300} />
+                <div class="h3-custom">How did we get here?</div>
+                <BodyText />
+                <div className="d-flex flex-column gap-2">
+                    <a href={PartnershipsURL} target="_blank" rel="noreferrer">
+                        <SmallImg src={PartnershipsTextImg} />
+                    </a>
+                    <a href={MarketingURL} target="_blank" rel="noreferrer">
+                        <SmallImg src={MarketingTextImg} />
+                    </a>
+                    <a href={CharterURL} target="_blank" rel="noreferrer">
+                        <SmallImg src={CharterTextImg} />
+                    </a>
+                    <a href={EducationURL} target="_blank" rel="noreferrer">
+                        <SmallImg src={EducationalTextImg} />
+                    </a>
+                </div>
+                <Widget
+                    src={"megha19.near/widget/components.socialLinks"}
+                    props={{ showRow: false }}
+                />
+            </div>
+        </SmallContainer>
+    );
+};
+
+return (
+    <div>
+        <LargeScreenComponent />
+        <MobileScreenComponent />
+    </div>
+);
