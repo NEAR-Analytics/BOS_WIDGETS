01869c5f97863b668b063381aa4d8cb929be247e
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2024-01-11 19:13:39 +0000

    Update SayALot by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2024-01-11T19:13:39.911Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/SayALot/source_code.jsx
@@ -10,58 +10,85 @@ let {
   topicShared,
 } = props;
 
-//Duplicated(lib.SBT)
-const sbtWhiteList =
-  context.networkId === "testnet"
-    ? [
-        "fractal-v2.i-am-human.testnet - class 1",
-        "community-v2.i-am-human.testnet - class 1",
-      ]
-    : [
-        "fractal.i-am-human.near - class 1",
-        "community.i-am-human.near - class 1",
-        "community.i-am-human.near - class 2",
-        "community.i-am-human.near - class 3",
-        "elections.ndc-gwg.near - class 2",
-        "elections.ndc-gwg.near - class 3",
-        "elections.ndc-gwg.near - class 4",
-        "public",
-      ];
+const initLibsCalls = {
+  SBT: [
+    {
+      functionName: "getSBTWhiteList",
+      key: "sbtWhiteList",
+      props: {},
+    },
+  ],
+};
+
+State.init({
+  functionsToCallByLibrary: initLibsCalls,
+  usersSBTs: [],
+});
+
+const usersSBTs = state.usersSBTs;
+
+let newLibsCalls = state.functionsToCallByLibrary;
+
+State.update({ libsCalls: newLibsCalls });
+
+const sbtWhiteList = state.sbtWhiteList
+  ? state.sbtWhiteList.map((sbt) => sbt.value)
+  : undefined;
+// const sbtWhiteList =
+//   context.networkId === "testnet"
+//     ? [
+//         "fractal-v2.i-am-human.testnet - class 1",
+//         "community-v2.i-am-human.testnet - class 1",
+//       ]
+//     : [
+//         "fractal.i-am-human.near - class 1",
+//         "community.i-am-human.near - class 1",
+//         "community.i-am-human.near - class 2",
+//         "community.i-am-human.near - class 3",
+//         "elections.ndc-gwg.near - class 2",
+//         "elections.ndc-gwg.near - class 3",
+//         "elections.ndc-gwg.near - class 4",
+//         "public",
+//       ];
 
 function createSbtOptions() {
-  return sbtWhiteList.map((option, i) => {
-    const title = "";
-
-    if (option === "fractal.i-am-human.near - class 1") {
-      title = "General";
-    } else if (option === "community.i-am-human.near - class 1") {
-      title = "OG";
-    } else if (option === "community.i-am-human.near - class 2") {
-      title = "Contributor";
-    } else if (option === "community.i-am-human.near - class 3") {
-      title = "Core Contributor";
-    } else if (option === "elections.ndc-gwg.near - class 2") {
-      title = "HoM";
-    } else if (option === "elections.ndc-gwg.near - class 3") {
-      title = "CoA";
-    } else if (option === "elections.ndc-gwg.near - class 4") {
-      title = "TC";
-    } else if (option === "fractal-v2.i-am-human.testnet - class 1") {
-      title = "Fractal";
-    } else if (option === "community-v2.i-am-human.testnet - class 1") {
-      title = "Community";
-    } else {
-      title = "Public";
-    }
-
-    if (i == 0) {
-      //The first options is always the default one
-      return { title, default: true, value: option };
-    } else {
-      return { title, value: option };
-    }
-  });
+  return state.sbtWhiteList;
 }
+
+// function createSbtOptions() {
+//   return sbtWhiteList.map((option, i) => {
+//     const title = "";
+
+//     if (option === "fractal.i-am-human.near - class 1") {
+//       title = "General";
+//     } else if (option === "community.i-am-human.near - class 1") {
+//       title = "OG";
+//     } else if (option === "community.i-am-human.near - class 2") {
+//       title = "Contributor";
+//     } else if (option === "community.i-am-human.near - class 3") {
+//       title = "Core Contributor";
+//     } else if (option === "elections.ndc-gwg.near - class 2") {
+//       title = "HoM";
+//     } else if (option === "elections.ndc-gwg.near - class 3") {
+//       title = "CoA";
+//     } else if (option === "elections.ndc-gwg.near - class 4") {
+//       title = "TC";
+//     } else if (option === "fractal-v2.i-am-human.testnet - class 1") {
+//       title = "Fractal";
+//     } else if (option === "community-v2.i-am-human.testnet - class 1") {
+//       title = "Community";
+//     } else {
+//       title = "Public";
+//     }
+
+//     if (i == 0) {
+//       //The first options is always the default one
+//       return { title, default: true, value: option };
+//     } else {
+//       return { title, value: option };
+//     }
+//   });
+// }
 
 // const componentsOwner =
 //   "f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb";
@@ -79,18 +106,15 @@ const widgets = {
   thisForum: `${authorForWidget}/widget/${configWidget}`,
 
   //Editable widgets
-  // ndcForum: `${componentsOwner}/widget/NDC.Forum`, //////////////////////////////////////////////////////
+  //   ndcForum: `${componentsOwner}/widget/NDC.Forum`, //////////////////////////////////////////////////////
   ndcForum: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/NDC.Forum`, //////////////////////////////////////////////////////
-  // create: `${componentsOwner}/widget/NDC.Forum.Create`, //////////////////////////////////////////////////////
-  create: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/NDC.Forum.Create`, //////////////////////////////////////////////////////
+  create: `${componentsOwner}/widget/NDC.Forum.Create`,
   header: `${componentsOwner}/widget/NDC.NavBar`,
   showArticlesList: `${componentsOwner}/widget/NDC.Forum.AllArticlesList`,
   showArticlesListSortedByAuthors: `${componentsOwner}/widget/NDC.AllArticlesSortByAuthors`,
   articlesByAuthorCard: `${componentsOwner}/widget/NDC.ArticlesByAuthorCard`,
-  // generalCard: `${componentsOwner}/widget/NDC.GeneralCard`, //////////////////////////////////////////////////////
-  generalCard: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/NDC.GeneralCard`, //////////////////////////////////////////////////////
-  // articleView: `${componentsOwner}/widget/NDC.ArticleView`, //////////////////////////////////////////////////////
-  articleView: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/NDC.ArticleView`, //////////////////////////////////////////////////////
+  generalCard: `${componentsOwner}/widget/NDC.GeneralCard`,
+  articleView: `${componentsOwner}/widget/NDC.ArticleView`,
   reactions: `${componentsOwner}/widget/NDC.Reactions`,
   addComment: `${componentsOwner}/widget/NDC.AddComment`,
   commentView: `${componentsOwner}/widget/NDC.CommentView`,
@@ -99,19 +123,18 @@ const widgets = {
   tagsEditor: `${componentsOwner}/widget/TagsEditor`,
   kanbanBoard: `${componentsOwner}/widget/NDC.KanbanBoard`,
   compactPost: `${componentsOwner}/widget/NDC.CompactPost`,
+  articleHistory: `${componentsOwner}/widget/NDC.ArticleHistory.Handler`,
 
   //Libs
-  libSBT: `sayalot.near/widget/lib.SBT`,
+  //   libSBT: `sayalot.near/widget/lib.SBT`,
+  libSBT: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/lib.SBT`,
   libComment: `sayalot.near/widget/lib.comment`,
-  // libArticle: `sayalot.near/widget/lib.article`, //////////////////////////////////////////////////////
-  libArticle: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/lib.article`,
+  libArticle: `sayalot.near/widget/lib.article`,
   libEmojis: `sayalot.near/widget/lib.emojis`,
   libUpVotes: `sayalot.near/widget/lib.upVotes`,
   libNotifications: `sayalot.near/widget/lib.notifications`,
 
   //Standard widgets
-  //(No modifiques este, sólo lo marco para que no te olvides de forkearlo)
-  articleHistory: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/NDC.ArticleHistory.Handler`, //////////////////////////////////////////////////////
   fasterTextInput: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/fasterTextInput`,
   markownEditorIframe: `f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/widget/MarkdownEditorIframe`,
   styledComponents: "rubycop.near/widget/NDC.StyledComponents",
@@ -136,6 +159,8 @@ const widgets = {
   navBarImg: "mob.near/widget/Image",
 };
 
+const libSrcArray = [widgets.libSBT];
+
 const brand = {
   brandName: "Say a lot",
   logoHref:
@@ -156,26 +181,78 @@ const kanbanColumns = ["Open", "Claimed", "In Work", "Closed"];
 const kanbanRequiredTags = [];
 const kanbanExcludedTags = [];
 
+const CallLibrary = styled.div`
+    display: none;
+`;
+
+function mainStateUpdate(obj) {
+  State.update(obj);
+}
+
+function callLibs(
+  src,
+  stateUpdate,
+  functionsToCallByLibrary,
+  extraProps,
+  callerWidget
+) {
+  return (
+    <Widget
+      src={src}
+      props={{
+        mainStateUpdate,
+        isTest,
+        stateUpdate,
+        functionsToCallByLibrary,
+        callLibs,
+        widgets,
+        callerWidget,
+        ...extraProps,
+        usersSBTs,
+      }}
+    />
+  );
+}
+
 return (
-  <Widget
-    src={widgets.ndcForum}
-    props={{
-      sharedBlockHeight,
-      tagShared,
-      isTest,
-      accountId,
-      sbtWhiteList,
-      authorForWidget,
-      widgets,
-      brand,
-      baseActions,
-      createSbtOptions,
-      kanbanColumns,
-      kanbanRequiredLabels,
-      kanbanExcludedLabels,
-      sharedArticleId,
-      sharedCommentId,
-      topicShared,
-    }}
-  />
+  <>
+    {sbtWhiteList ? (
+      <Widget
+        src={widgets.ndcForum}
+        props={{
+          sharedBlockHeight,
+          tagShared,
+          isTest,
+          accountId,
+          sbtWhiteList,
+          authorForWidget,
+          widgets,
+          brand,
+          baseActions,
+          createSbtOptions,
+          kanbanColumns,
+          kanbanRequiredLabels,
+          kanbanExcludedLabels,
+          sharedArticleId,
+          sharedCommentId,
+          topicShared,
+          callLibs,
+          mainStateUpdate,
+        }}
+      />
+    ) : (
+      <Widget src={widgets.newStyledComponents.Feedback.Spinner} />
+    )}
+    <CallLibrary>
+      {libSrcArray.map((src) => {
+        return callLibs(
+          src,
+          mainStateUpdate,
+          state.functionsToCallByLibrary,
+          { baseAction: baseActions.articlesBaseAction, kanbanColumns },
+          "SayALot"
+        );
+      })}
+    </CallLibrary>
+  </>
 );
