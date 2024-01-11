900461c9ab15aee78efe297d70bc817a0c0d4dbb
nearblocks.near <nearblocks.near>
2024-01-11 09:08:29 +0000

    Update bos-components.components.Transactions.Hash by nearblocks.near at 2024-01-11T09:08:29.996Z

--- nearblocks.near/bos-components.components.Transactions.Hash/source_code.jsx
+++ nearblocks.near/bos-components.components.Transactions.Hash/source_code.jsx
@@ -190,7 +190,7 @@ function MainComponent(props) {
         </div>
         {error || (!isLoading && !txn) ? (
           <div className="text-gray-400 text-xs px-2 mb-4">
-            {t ? t('txnError') : 'Transaction Error'}
+            {t ? t('txns:txnError') : 'Transaction Error'}
           </div>
         ) : (
           <div className="bg-white soft-shadow rounded-lg pb-1">
