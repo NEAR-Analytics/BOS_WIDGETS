4a68b647750b99aa24436a2c485f7073c8efd98a
bluebiu.near <bluebiu.near>
2024-01-12 08:08:33 +0000

    Update Polygon.Lending by bluebiu.near at 2024-01-12T08:08:33.065Z

--- bluebiu.near/Polygon.Lending/source_code.jsx
+++ bluebiu.near/Polygon.Lending/source_code.jsx
@@ -1,5 +1,3 @@
-const { defaultDapp, ...rest } = props;
-
 const Theme = styled.div`
   --button-text-color: #fff;
   --button-color: #783bf0;
@@ -21,58 +19,11 @@ const Theme = styled.div`
   --yours-table-title: #ffffff;
 `;
 
-const CHAIN_ID = 137;
-const CHAIN_NAME = "Polygon";
-
 return (
   <Theme>
     <Widget
       src="bluebiu.near/widget/Avalanche.Lending.Collection"
-      props={{
-        chainId: CHAIN_ID,
-        chainName: CHAIN_NAME,
-        multicallv2: "0xed386Fe855C1EFf2f843B910923Dd8846E45C5A4",
-        connectProps: {
-          // imgProps: {
-          //   src: "https://ipfs.near.social/ipfs/bafkreihcujrphf3k3zgfl4wdnxbz5btydas43uwdmvjgrs5mavbubvrpyq",
-          //   style: {
-          //     width: "282px",
-          //     height: "222px",
-          //     marginTop: "80px",
-          //   },
-          // },
-          noAccountTips: "Polygon Dex Collection",
-          wrongNetworkTips: "To proceed, kindly switch to Polygon Chain.",
-          chainId: CHAIN_ID,
-          chainName: CHAIN_NAME,
-        },
-        defaultDapp: defaultDapp || "C.R.E.A.M.",
-        dapps: {
-          // "0vix": {
-          //   name: "0vix",
-          //   icon: "https://ipfs.near.social/ipfs/bafkreigyodedyhiqmstq3g5edcqw25yyari4y3rcbsnqtxldb2zb2vpah4",
-          //   unitrollerAddress: "0x8849f1a0cB6b5D6076aB150546EddEe193754F1C",
-          //   oracleAddress: "0x1c312b14c129eabc4796b0165a2c470b659e5f01",
-          //   data: "bluebiu.near/widget/Polygon.Lending.0vixData",
-          //   handlerCollateral:
-          //     "bluebiu.near/widget/Avalanche.Lending.BenqiHandlerCollateral",
-          //   handlerCToken:
-          //     "bluebiu.near/widget/Avalanche.Lending.BenqiHandlerCToken",
-          // },
-          "C.R.E.A.M.": {
-            name: "C.R.E.A.M.",
-            icon: "https://ipfs.near.social/ipfs/bafkreihxqzhzavb7onnxjtin2y6s7pj3y4nxb6ul42cknregyykfu24ovm",
-            unitrollerAddress: "0x20CA53E2395FA571798623F1cFBD11Fe2C114c24",
-            oracleAddress: "0x812c0b2a2a0a74f6f6ed620fbd2b67fec7db2190",
-            data: "bluebiu.near/widget/Polygon.Lending.CreamData",
-            handlerCollateral:
-              "bluebiu.near/widget/Avalanche.Lending.BenqiHandlerCollateral",
-            handlerCToken:
-              "bluebiu.near/widget/Avalanche.Lending.BenqiHandlerCToken",
-          },
-        },
-        ...rest,
-      }}
+      props={props}
     />
   </Theme>
 );
