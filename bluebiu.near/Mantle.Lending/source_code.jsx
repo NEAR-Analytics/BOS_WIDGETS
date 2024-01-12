d699be24e6dac5b82037abb191ff4e66b0c349ae
bluebiu.near <bluebiu.near>
2024-01-12 08:08:33 +0000

    Update Mantle.Lending by bluebiu.near at 2024-01-12T08:08:33.065Z

--- bluebiu.near/Mantle.Lending/source_code.jsx
+++ bluebiu.near/Mantle.Lending/source_code.jsx
@@ -1,5 +1,3 @@
-const { defaultDapp, ...rest } = props;
-
 const Theme = styled.div`
   --button-text-color: #0f1126;
   --button-color: #00ffe0;
@@ -20,55 +18,12 @@ const Theme = styled.div`
   --secondary-border-color: #3f577b;
   --yours-table-title: #ffffff;
 `;
-const CHAIN_ID = 5000;
-const CHAIN_NAME = "Mantle";
 
 return (
   <Theme>
     <Widget
       src="bluebiu.near/widget/Avalanche.Lending.Collection"
-      props={{
-        chainId: CHAIN_ID,
-        chainName: CHAIN_NAME,
-        multicallv2: "0x072aD7f291AED59E7C4974EbdcF73B79DAC89051",
-        connectProps: {
-          imgProps: {
-            src: "",
-            style: {
-              width: "283px",
-              height: "187px",
-              marginTop: "60px",
-            },
-          },
-          noAccountTips: `${CHAIN_NAME} Lending Collection`,
-          wrongNetworkTips: `To proceed, kindly switch to ${CHAIN_NAME} Chain.`,
-          chainId: CHAIN_ID,
-          chainName: CHAIN_NAME,
-        },
-        defaultDapp: defaultDapp || "Lendle",
-        dapps: {
-          Lendle: {
-            name: "Lendle",
-            icon: "https://ipfs.near.social/ipfs/bafkreibh3m63rrgsbl3ihtugfaqj7vbwxni6sefjbsen5nuyqexy4opepi",
-            oracleAddress: "0x870c9692Ab04944C86ec6FEeF63F261226506EfC",
-            PoolAddressProvider: "0xAb94Bedd21ae3411eB2698945dfCab1D5C19C3d4",
-            aaveProtocolDataProviderAddress:
-              "0x552b9e4bae485C4B7F540777d7D25614CdB84773",
-            lendingPoolAddress: "0xCFa5aE7c2CE8Fadc6426C1ff872cA45378Fb7cF3",
-            wethGateway: "0xEc831f8710C6286a91a348928600157f07aC55c2",
-            data: "bluebiu.near/widget/Mantle.Lending.LendleData",
-            incentiveController: "0x79e2fd1c484EB9EE45001A98Ce31F28918F27C41",
-            handlerClaim:
-              "bluebiu.near/widget/Mantle.Lending.LendleClaimHandler",
-            handlerCollateral:
-              "bluebiu.near/widget/Arbitrum.Lending.RadiantHandlerCollateral",
-            type: "aave2",
-            handlerCToken:
-              "bluebiu.near/widget/Arbitrum.Lending.RadiantActionHandler",
-          },
-        },
-        ...rest,
-      }}
+      props={props}
     />
   </Theme>
 );
