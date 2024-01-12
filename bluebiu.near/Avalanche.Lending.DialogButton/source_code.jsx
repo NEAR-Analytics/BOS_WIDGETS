6b16c2bdee6cfff41ff0556422b49574b324fe3a
bluebiu.near <bluebiu.near>
2024-01-12 08:08:33 +0000

    Update Avalanche.Lending.DialogButton by bluebiu.near at 2024-01-12T08:08:33.065Z

--- bluebiu.near/Avalanche.Lending.DialogButton/source_code.jsx
+++ bluebiu.near/Avalanche.Lending.DialogButton/source_code.jsx
@@ -74,10 +74,6 @@ const Button = styled.button`
     border: 1px solid var(--repay-border-color);
   }
 `;
-State.init({
-  approving: false,
-  isApproved: false,
-});
 
 const {
   disabled,
@@ -88,60 +84,44 @@ const {
   onSuccess,
   toast,
   addAction,
+  unsignedTx,
+  loading: estimating,
+  gas,
+  onApprovedSuccess,
 } = props;
 
 const account = Ethers.send("eth_requestAccounts", [])[0];
 
 const tokenSymbol = data.underlyingToken.symbol;
+if (!actionText) return;
+
+useEffect(() => {
+  State.update({
+    approving: false,
+    isApproved: false,
+    isGasEnough: true,
+  });
+}, []);
+
+useEffect(() => {
+  if (!account || !gas) return;
+  const provider = Ethers.provider();
+  provider.getBalance(account).then((rawBalance) => {
+    State.update({
+      gasBalance: rawBalance.toString(),
+      isGasEnough: !Big(rawBalance.toString()).lt(gas.toString()),
+      gas: ethers.utils.formatUnits(gas, 18),
+    });
+  });
+}, [account, gas]);
 
 if (actionText.includes("Collateral")) {
-  const isEnter = actionText === "Enable as Collateral";
   return (
     <>
-      {!!state.loading && (
-        <Widget
-          src={data.config.handlerCollateral}
-          props={{
-            actionText,
-            unitrollerAddress: data.config.unitrollerAddress,
-            marketAddress: data.address,
-            loading: state.loading,
-            onSuccess: (res) => {
-              const { status, transactionHash } = res;
-              toast?.dismiss(state.toastId);
-              if (status !== 1) throw new Error("");
-              State.update({
-                loading: false,
-              });
-              toast?.success({
-                title: `${tokenSymbol} ${
-                  isEnter ? "enable" : "disable"
-                } as collateral request successed!`,
-                tx: transactionHash,
-                chainId,
-              });
-              onSuccess?.(data.dapp);
-            },
-            onError: (tx) => {
-              State.update({
-                loading: false,
-              });
-              toast?.fail({
-                title: `${tokenSymbol} ${
-                  isEnter ? "enable" : "disable"
-                } as collateral request failed!`,
-                tx: tx ? tx.hash : "",
-                chainId,
-              });
-            },
-            account,
-            data,
-          }}
-        />
-      )}
       <Button
-        disabled={state.loading || disabled}
+        disabled={state.loading || disabled || estimating || !state.isGasEnough}
         onClick={() => {
+          const isEnter = actionText === "Enable as Collateral";
           const toastId = toast?.loading({
             title: `Submitting ${tokenSymbol} ${
               isEnter ? "enable" : "disable"
@@ -149,19 +129,57 @@ if (actionText.includes("Collateral")) {
           });
           State.update({
             loading: true,
-            toastId,
           });
+          Ethers.provider()
+            .getSigner()
+            .sendTransaction(unsignedTx)
+            .then((tx) => {
+              tx.wait((res) => {
+                const { status, transactionHash } = res;
+                toast?.dismiss(toastId);
+                if (status !== 1) throw new Error("");
+                State.update({
+                  loading: false,
+                });
+                toast?.success({
+                  title: `${tokenSymbol} ${
+                    isEnter ? "enable" : "disable"
+                  } as collateral request successed!`,
+                  tx: transactionHash,
+                  chainId,
+                });
+                onSuccess?.(data.dapp);
+              });
+            })
+            .catch((err) => {
+              State.update({
+                loading: false,
+              });
+              toast?.dismiss(toastId);
+              toast?.fail({
+                title: err?.message?.includes("user rejected transaction")
+                  ? "User rejected transaction"
+                  : `${tokenSymbol} ${
+                      isEnter ? "enable" : "disable"
+                    } as collateral request failed!`,
+                tx: err ? err.hash : "",
+                chainId,
+              });
+            });
         }}
       >
-        {state.loading && (
+        {state.loading || estimating ? (
           <Widget
             src="bluebiu.near/widget/0vix.LendingLoadingIcon"
             props={{
               size: 16,
             }}
           />
+        ) : !state.isGasEnough ? (
+          `Not enough gas(${Big(state.gas || 0).toFixed(2)}) needed`
+        ) : (
+          "Confirm"
         )}
-        Confirm
       </Button>
     </>
   );
@@ -175,20 +193,20 @@ if (!amount) {
 }
 
 const getAAVE2TokenAddress = () => {
-  return data.underlyingToken.description === "native"
-    ? data.aTokenAddress
-    : data.underlyingAsset.address;
+  return data.underlyingToken.address === "native"
+    ? data.address
+    : data.underlyingToken.address;
 };
 
 const getAAVE2ApproveAddress = () => {
-  return data.underlyingToken.description === "native"
-    ? data.wethGateway
-    : data.lendingPoolAddress;
+  return data.underlyingToken.address === "native"
+    ? data.config.wethGateway
+    : data.config.lendingPoolAddress;
 };
 
 const getAllowance = () => {
   const TokenContract = new ethers.Contract(
-    data.type === "aave2"
+    data.config.type === "aave2"
       ? getAAVE2TokenAddress()
       : data.underlyingToken.address,
     ERC20_ABI,
@@ -196,7 +214,7 @@ const getAllowance = () => {
   );
   TokenContract.allowance(
     account,
-    data.type == "aave2" ? getAAVE2ApproveAddress() : data.address
+    data.config.type == "aave2" ? getAAVE2ApproveAddress() : data.address
   ).then((allowanceRaw) => {
     State.update({
       isApproved: !Big(
@@ -210,8 +228,7 @@ const getAllowance = () => {
 };
 if (
   ["Deposit", "Repay"].includes(actionText) &&
-  data.underlyingToken.address !== "native" &&
-  data.underlyingToken.description !== "native"
+  data.underlyingToken.address !== "native"
 ) {
   getAllowance();
 } else {
@@ -228,14 +245,14 @@ if (!state.isApproved) {
     });
 
     const TokenContract = new ethers.Contract(
-      data.type === "aave2"
+      data.config.type === "aave2"
         ? getAAVE2TokenAddress()
         : data.underlyingToken.address,
       ERC20_ABI,
       Ethers.provider().getSigner()
     );
     TokenContract.approve(
-      data.type == "aave2" ? getAAVE2ApproveAddress() : data.address,
+      data.config.type == "aave2" ? getAAVE2ApproveAddress() : data.address,
       ethers.utils.parseUnits(amount, data.underlyingToken.decimals)
     )
       .then((tx) => {
@@ -253,6 +270,7 @@ if (!state.isApproved) {
             tx: transactionHash,
             chainId,
           });
+          onApprovedSuccess();
         });
       })
       .catch((err) => {
@@ -287,64 +305,8 @@ if (!state.isApproved) {
 
 return (
   <>
-    {state.pending && (
-      <Widget
-        src={data.config.handlerCToken}
-        props={{
-          market: data,
-          actionText,
-          amount: Big(amount).toFixed(data.underlyingToken.decimals),
-          loading: state.pending,
-          onSuccess: (res) => {
-            const { status, transactionHash } = res;
-            toast?.dismiss(state.toastId);
-            State.update({
-              pending: false,
-            });
-            addAction?.({
-              type: "Lending",
-              action: actionText,
-              token: data.underlyingToken,
-              amount,
-              template: data.dappName,
-              add: false,
-              status,
-              transactionHash,
-            });
-            if (status === 1) {
-              onSuccess?.(data.dapp);
-              toast?.success({
-                title: `${tokenSymbol} ${actionText.toLowerCase()} request successed!`,
-                tx: transactionHash,
-                chainId,
-              });
-            } else {
-              toast?.fail({
-                title: `${tokenSymbol} ${actionText.toLowerCase()} request failed!`,
-                tx: transactionHash,
-                chainId,
-              });
-            }
-          },
-          onError: (tx) => {
-            State.update({
-              pending: false,
-            });
-            toast?.dismiss(state.toastId);
-            toast?.fail({
-              title: tx?.message?.includes("user rejected transaction")
-                ? "User rejected transaction"
-                : `${tokenSymbol} ${actionText.toLowerCase()} request failed!`,
-              tx: tx ? tx.hash : "",
-              chainId,
-            });
-          },
-          account,
-        }}
-      />
-    )}
     <Button
-      disabled={state.pending || disabled}
+      disabled={state.pending || disabled || estimating || !state.isGasEnough}
       className={actionText.toLowerCase()}
       onClick={() => {
         const toastId = toast?.loading({
@@ -352,19 +314,71 @@ return (
         });
         State.update({
           pending: true,
-          toastId,
         });
+        Ethers.provider()
+          .getSigner()
+          .sendTransaction(unsignedTx)
+          .then((tx) => {
+            tx.wait((res) => {
+              const { status, transactionHash } = res;
+              toast?.dismiss(toastId);
+              State.update({
+                pending: false,
+              });
+              addAction?.({
+                type: "Lending",
+                action: actionText,
+                token: data.underlyingToken,
+                amount,
+                template: data.dappName,
+                add: false,
+                status,
+                transactionHash,
+              });
+              if (status === 1) {
+                onSuccess?.(data.dapp);
+                toast?.success({
+                  title: `${tokenSymbol} ${actionText.toLowerCase()} request successed!`,
+                  tx: transactionHash,
+                  chainId,
+                });
+              } else {
+                toast?.fail({
+                  title: `${tokenSymbol} ${actionText.toLowerCase()} request failed!`,
+                  tx: transactionHash,
+                  chainId,
+                });
+              }
+            });
+          })
+          .catch((err) => {
+            State.update({
+              pending: false,
+            });
+            console.log("err", err);
+            toast?.dismiss(toastId);
+            toast?.fail({
+              title: err?.message?.includes("user rejected transaction")
+                ? "User rejected transaction"
+                : `${tokenSymbol} ${actionText.toLowerCase()} request failed!`,
+              tx: err ? err.hash : "",
+              chainId,
+            });
+          });
       }}
     >
-      {state.pending && (
+      {state.pending || estimating ? (
         <Widget
           src="bluebiu.near/widget/0vix.LendingLoadingIcon"
           props={{
             size: 16,
           }}
         />
+      ) : !state.isGasEnough ? (
+        `Not enough gas(${Big(state.gas || 0).toFixed(2)}) needed`
+      ) : (
+        actionText
       )}
-      {actionText}
     </Button>
   </>
 );
