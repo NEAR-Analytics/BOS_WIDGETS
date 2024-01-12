7d3796fe7314dca9add89efb77ac56a3380e547d
nearblocks.near <nearblocks.near>
2024-01-12 13:22:58 +0000

    Update bos-components.components.Transactions.List by nearblocks.near at 2024-01-12T13:22:58.027Z

--- nearblocks.near/bos-components.components.Transactions.List/source_code.jsx
+++ nearblocks.near/bos-components.components.Transactions.List/source_code.jsx
@@ -1590,6 +1590,31 @@ const Skeleton = (props) => {
     ></div>
   );
 };/* END_INCLUDE COMPONENT: "includes/Common/Skeleton.jsx" */
+/* INCLUDE COMPONENT: "includes/icons/Clock.jsx" */
+/**
+ * @interface Props
+ * @param {string} [className] - The CSS class name(s) for styling purposes.
+ */
+
+
+
+
+
+const Clock = (props) => (
+  <svg
+    viewBox="64 64 896 896"
+    focusable="false"
+    data-icon="clock-circle"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    aria-hidden="true"
+    {...props}
+  >
+    <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
+    <path d="M686.7 638.6L544.1 535.5V288c0-4.4-3.6-8-8-8H488c-4.4 0-8 3.6-8 8v275.4c0 2.6 1.2 5 3.3 6.5l165.4 120.6c3.6 2.6 8.6 1.8 11.2-1.7l28.6-39c2.6-3.7 1.8-8.7-1.8-11.2z"></path>
+  </svg>
+);/* END_INCLUDE COMPONENT: "includes/icons/Clock.jsx" */
 
 function MainComponent(props) {
   const {
@@ -2032,28 +2057,42 @@ function MainComponent(props) {
     },
     {
       header: (
-        <span>
-          <div className="w-full inline-flex px-5 py-4">
-            <button
-              type="button"
-              onClick={toggleShowAge}
-              className="text-left text-xs w-full font-semibold uppercase tracking-wider text-nearblue-600 focus:outline-none whitespace-nowrap"
-            >
-              {showAge
-                ? t
-                  ? t('txns:age')
-                  : 'AGE'
-                : t
-                ? t('txns:ageDT')
-                : 'DATE TIME (UTC)'}
-            </button>
-            <button type="button" onClick={onOrder} className="px-2">
-              <div className="text-gray-500 font-semibold">
-                <SortIcon order={sorting} />
-              </div>
-            </button>
-          </div>
-        </span>
+        <div className="w-full inline-flex px-5 py-4">
+          <Tooltip.Provider>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <button
+                  type="button"
+                  onClick={toggleShowAge}
+                  className="text-left text-xs w-full flex items-center font-semibold uppercase tracking-wider  text-green-500 focus:outline-none whitespace-nowrap"
+                >
+                  {showAge
+                    ? t
+                      ? t('txns:age')
+                      : 'AGE'
+                    : t
+                    ? t('txns:ageDT')
+                    : 'DATE TIME (UTC)'}
+                  {showAge && <Clock className="text-green-500 ml-2" />}
+                </button>
+              </Tooltip.Trigger>
+              <Tooltip.Content
+                className="h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2 break-words"
+                align="center"
+                side="top"
+              >
+                {showAge
+                  ? 'Click to show Datetime Format'
+                  : 'Click to show Age Format'}
+              </Tooltip.Content>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+          <button type="button" onClick={onOrder} className="px-2">
+            <div className="text-gray-500 font-semibold">
+              <SortIcon order={sorting} />
+            </div>
+          </button>
+        </div>
       ),
       key: 'block_timestamp',
       cell: (row) => (
