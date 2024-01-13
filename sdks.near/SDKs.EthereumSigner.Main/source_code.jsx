9f2a5af2c4cf26346e994ce6001071930a9ed4a6
sdks.near <sdks.near>
2024-01-13 10:40:28 +0000

    Update SDKs.EthereumSigner.Main by sdks.near at 2024-01-13T10:40:28.826Z

--- sdks.near/SDKs.EthereumSigner.Main/source_code.jsx
+++ sdks.near/SDKs.EthereumSigner.Main/source_code.jsx
@@ -1,34 +1,25 @@
-const VERIFIER_CONTRACT = "0x793ABce859d651C179F2F3B0A47C438dC3D67901";
-const ABI = [
-  "function verifySignature(string memory message, uint8 v, bytes32 r, bytes32 s) public pure returns (address signer)",
-];
-
 const EthereumSigner = {
   sign: (message) => Ethers.provider().getSigner().signMessage(message),
   verify: (originalMessage, signature, expectedSignerAddress) => {
-    return EthereumSigner.getSignerAddress(originalMessage, signature)
-      .then((address) => {
-        return expectedSignerAddress.toLowerCase() == address.toLowerCase();
-      })
-      .catch((error) => {
-        console.error(error);
-        return false;
-      });
+    return new Promise((resolve, reject) => {
+      try {
+        const address = EthereumSigner.getSignerAddress(
+          originalMessage,
+          signature
+        );
+        return resolve(
+          expectedSignerAddress.toLowerCase() == address.toLowerCase()
+        );
+      } catch {
+        console.error(
+          "The signature is invalid or the expectedSignerAddress is null"
+        );
+        return reject();
+      }
+    });
   },
   getSignerAddress: (message, signature) => {
-    const contract = new ethers.Contract(
-      VERIFIER_CONTRACT,
-      ABI,
-      Ethers.provider()
-    );
-
-    try {
-      const { v, r, s } = ethers.utils.splitSignature(signature);
-
-      return contract.callStatic.verifySignature(message, v, r, s);
-    } catch {
-      return new Promise((_, reject) => reject("Invalid signature"));
-    }
+    return ethers.utils.verifyMessage(message, signature);
   },
 };
 
