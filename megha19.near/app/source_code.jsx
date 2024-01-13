4128f47402ab966ed83009e5223f03ed72769a65
megha19.near <megha19.near>
2024-01-13 14:42:27 +0000

    Update app by megha19.near at 2024-01-13T14:42:27.925Z

--- megha19.near/app/source_code.jsx
+++ megha19.near/app/source_code.jsx
@@ -1,100 +1,190 @@
 /**
- * This is the main entry point for the she-is-near application.
+ * This is the main entry point for the DevHub application.
  * Page route gets passed in through params, along with all other page props.
  */
 
+const { onDraftStateChange } = VM.require(
+  "megha19.near/widget/devhub.entity.post.draft"
+);
+
 const { page, ...passProps } = props;
 
-const cssFont = fetch(
-    "https://fonts.googleapis.com/css2?family=Poppins&display=swap"
-).body;
+// Import our modules
+const { AppLayout } = VM.require(
+  "megha19.near/widget/devhub.components.templates.AppLayout"
+);
 
-if (!cssFont) return "";
+if (!AppLayout) {
+  return <p>Loading modules...</p>;
+}
 
 // CSS styles to be used across the app.
 // Define fonts here, as well as any other global styles.
 const Theme = styled.div`
-    font-family: "Poppins", sans-serif;
-    ${cssFont}
+  a {
+    color: inherit;
+  }
 
-    .grid-4 {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 1rem;
-    }
+  .attractable {
+    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important;
+    transition: box-shadow 0.6s;
 
-    .cursor {
-        cursor: pointer;
+    &:hover {
+      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
     }
-
-    a {
-        all: unset;
-    }
-
-    .body-text {
-        line-height: 30px;
-    }
-
-    img {
-        object-fit: cover;
-        align-self: center;
-    }
+  }
 `;
 
 if (!page) {
-    // If no page is specified, we default to the feed page TEMP
-    page = "home";
+  // If no page is specified, we default to the feed page TEMP
+  page = "home";
 }
 
 // This is our navigation, rendering the page based on the page parameter
 function Page() {
-    const routes = page.split(".");
-    switch (routes[0]) {
-        case "home": {
-            return (
-                <Widget
-                    src="megha19.near/widget/home"
-                    props={passProps}
-                />
-            );
-        }
-        // ?page=education
-        case "education": {
-            return (
-                <Widget
-                    src={"megha19.near/widget/education"}
-                    props={passProps}
-                />
-            );
-        }
-        // ?page=marketing
-        case "marketing": {
-            return (
-                <Widget
-                    src={"megha19.near/widget/marketing"}
-                    props={passProps}
-                />
-            );
-        }
-        // ?page=partnerships
-        case "partnerships": {
-            return (
-                <Widget
-                    src={"megha19.near/widget/partnerships"}
-                    props={passProps}
-                />
-            );
-        }
-
-        default: {
-            // TODO: 404 page
-            return <p>404</p>;
-        }
+  const routes = page.split(".");
+  switch (routes[0]) {
+    case "home": {
+      return (
+        <Widget
+          src="megha19.near/widget/devhub.page.home"
+          props={passProps}
+        />
+      );
     }
+    // ?page=communities
+    case "communities": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.page.communities"}
+          props={passProps}
+        />
+      );
+    }
+    // ?page=community
+    case "community": {
+      return (
+        // Considering to consolsidate this into a single widget,
+        // where each level handles its own routing.
+        // Modularizing a page just like we do with addons
+        <Widget
+          src={"megha19.near/widget/devhub.entity.community.Provider"}
+          props={{
+            ...passProps,
+            Children: (p) => {
+              // passing props from the Provider into the Children
+              switch (routes[1]) {
+                // ?page=community.configuration
+                case "configuration": {
+                  return (
+                    <Widget
+                      src={
+                        "megha19.near/widget/devhub.page.community.configuration"
+                      }
+                      props={{
+                        ...passProps,
+                        ...p,
+                      }}
+                    />
+                  );
+                }
+                // ?page=community
+                default: {
+                  return (
+                    <Widget
+                      src={"megha19.near/widget/devhub.page.community.index"}
+                      props={{
+                        ...passProps,
+                        ...p,
+                      }}
+                    />
+                  );
+                }
+              }
+            },
+          }}
+        />
+      );
+    }
+    // ?page=feed
+    case "feed": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.page.feed"}
+          props={passProps}
+        />
+      );
+    }
+    // ?page=create
+    case "create": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.entity.post.PostEditor"}
+          props={{ ...passProps, isCreatePostPage: true, onDraftStateChange }}
+        />
+      );
+    }
+    // ?page=about
+    case "about": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.page.about"}
+          props={passProps}
+        />
+      );
+    }
+    case "contribute": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.page.contribute"}
+          props={passProps}
+        />
+      );
+    }
+    case "profile": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.page.profile"}
+          props={passProps}
+        />
+      );
+    }
+    // ?page=blog
+    case "blog": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.page.blog"}
+          props={passProps}
+        />
+      );
+    }
+    case "post": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.page.post"}
+          props={passProps}
+        />
+      );
+    }
+    case "admin": {
+      return (
+        <Widget
+          src={"megha19.near/widget/devhub.page.admin.index"}
+          props={passProps}
+        />
+      );
+    }
+    default: {
+      // TODO: 404 page
+      return <p>404</p>;
+    }
+  }
 }
 
 return (
-    <Theme>
-        <Page />
-    </Theme>
+  <Theme>
+    <AppLayout page={page}>
+      <Page />
+    </AppLayout>
+  </Theme>
 );
