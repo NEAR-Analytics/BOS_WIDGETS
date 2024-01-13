1b24f9ff1c375117a0584fca8f56769c3e28483c
sdks.near <sdks.near>
2024-01-13 23:16:07 +0000

    Update SDKs.Lens.API.AuthAPI by sdks.near at 2024-01-13T23:16:07.221Z

--- sdks.near/SDKs.Lens.API.AuthAPI/source_code.jsx
+++ sdks.near/SDKs.Lens.API.AuthAPI/source_code.jsx
@@ -4,44 +4,41 @@ const { Interfaces } = $("@sdks/lens/definitions");
 
 return {
   profiles: (Client, profilesManagedRequest) => {
-    return Client.graphql(
-      Auth.PROFILES_MANAGED_QUERY,
-      {profilesManagedRequest}
-    ).then((payload) => payload.body.data.profilesManaged.items || []);
+    return Client.graphql(Auth.PROFILES_MANAGED_QUERY, {
+      profilesManagedRequest,
+    }).then((payload) => payload.body.data.profilesManaged.items || []);
   },
   challenge: (Client, challengeRequest) => {
-    return Client.graphql(Auth.CHALLENGE_QUERY, {challengeRequest}).then(
+    return Client.graphql(Auth.CHALLENGE_QUERY, { challengeRequest }).then(
       (data) => {
         return data.body.data.challenge || Interfaces.AUTH_CHALLENGE_INTERFACE;
       }
     );
   },
-  authenticate: (Client, signedChallengeRequest) => {
-    return Client.graphql(Auth.AUTHENTICATE_QUERY, {signedChallengeRequest}).then(
-      (payload) => {
-        return payload.body.data.authenticate || Interfaces.AUTH_INTERFACE;
-      }
-    );
+  authenticate: (Client, signedAuthChallengeRequest) => {
+    return Client.graphql(Auth.SIGNED_AUTH_CHALLENGE_QUERY, {
+      signedAuthChallengeRequest,
+    }).then((payload) => {
+      return payload.body.data.authenticate || Interfaces.AUTH_INTERFACE;
+    });
   },
   refresh: (Client, refreshRequest) => {
-    return Client.graphql(Auth.PROFILES_MANAGED_QUERY, {refreshRequest}).then(
+    return Client.graphql(Auth.PROFILES_MANAGED_QUERY, { refreshRequest }).then(
       (payload) => payload.body.data.refresh || Interfaces.AUTH_INTERFACE
     );
   },
   revoke: (Client, revokeAuthenticationRequest) => {
-    return Client.graphql(
-      Auth.REVOKE_AUTHENTICATION_QUERY,
-      {revokeAuthenticationRequest}
-    ).then((payload) => true);
+    return Client.graphql(Auth.REVOKE_AUTHENTICATION_QUERY, {
+      revokeAuthenticationRequest,
+    }).then((payload) => true);
   },
   list: (Client, approvedAuthenticationRequest) => {
-    return Client.graphql(
-      Auth.APPROVED_AUTHENTICATION_QUERY,
-      {approvedAuthenticationRequest}
-    ).then((payload) => payload.body.data.approvedAuthentication.items || []);
+    return Client.graphql(Auth.APPROVED_AUTHENTICATION_QUERY, {
+      approvedAuthenticationRequest,
+    }).then((payload) => payload.body.data.approvedAuthentication.items || []);
   },
   verify: (Client, verifyRequest) => {
-    return Client.graphql(Auth.VERIFY_REQUEST, {verifyRequest}).then(
+    return Client.graphql(Auth.VERIFY_REQUEST, { verifyRequest }).then(
       (payload) => (payload.body.data.verify || false) == true
     );
   },
