1f0ef3bac180227ce54aefba72d09d711def3585
sdks.near <sdks.near>
2024-01-13 13:32:41 +0000

    Update Manifest by sdks.near at 2024-01-13T13:32:41.276Z

--- sdks.near/Manifest/source_code.jsx
+++ sdks.near/Manifest/source_code.jsx
@@ -4,6 +4,7 @@ return {
     lens: {
       definitions: ["SDKs.Lens.Constants", "SDKs.Lens.Interfaces"],
       api: ["SDKs.Lens.API.AuthAPI", "SDKs.Lens.API.HealthAPI"],
+      utils: ["SDKs.Lens.Helpers.ApiHelper"],
       queries: ["SDKs.Lens.Queries.Auth", "SDKs.Lens.Queries.Health"],
       sdk: ["SDKs.Lens.Main"],
     },
