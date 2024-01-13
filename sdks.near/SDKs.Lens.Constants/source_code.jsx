4aaaa043aeba5581ca7a44879c2e922e639ceb12
sdks.near <sdks.near>
2024-01-13 14:49:22 +0000

    Update SDKs.Lens.Constants by sdks.near at 2024-01-13T14:49:22.991Z

--- sdks.near/SDKs.Lens.Constants/source_code.jsx
+++ sdks.near/SDKs.Lens.Constants/source_code.jsx
@@ -3,10 +3,11 @@ const TESTNET_URL = "https://api-v2-mumbai-live.lens.dev";
 const JWT_TOKEN_LIFESPAN_SECONDS = 1800; // 30 min
 const JWT_REFRESH_TOKEN_LIFESPAN_SECONDS = 604800; // 7 days
 const RESPONSE_HEALTH_OK = "pong";
+const RESPONSE_HEALTH_KO = "ponk";
 const API_REQUEST_LIMITS = {
-    TEN: "Ten",
-    TWENTY_FIVE: "TwentyFive",
-    FIFTY: "Fifty"
+  TEN: "Ten",
+  TWENTY_FIVE: "TwentyFive",
+  FIFTY: "Fifty",
 };
 
 return {
