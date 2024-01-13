7cdd5cd3a449a793be12cedf7edb6ff657ff092b
sdks.near <sdks.near>
2024-01-13 16:13:17 +0000

    Update SDKs.Lens.Main by sdks.near at 2024-01-13T16:13:17.031Z

--- sdks.near/SDKs.Lens.Main/source_code.jsx
+++ sdks.near/SDKs.Lens.Main/source_code.jsx
@@ -1,5 +1,8 @@
 const $ = VM.require(`sdks.near/widget/Loader`);
-const { Constants, Interfaces, HealthAPI } = $("@sdks/lens");
+const { Constants, Interfaces } = $("@sdks/lens/definitions");
+const { HealthAPI, AuthAPI } = $("@sdks/lens/api");
+const { AuthRequests } = $("@sdks/lens/requests");
+const { ApiHelper } = $("@sdks/lens/utils");
 const { LightClient } = $("@sdks/light-client");
 
 const LensSDK = {
@@ -10,7 +13,6 @@ const LensSDK = {
   init: () => {
     LensSDK.enableMainnet();
     LightClient.auth = Interfaces.AUTH_INTERFACE;
-    LightClient.challenge = Interfaces.AUTH_CHALLENGE_INTERFACE;
     LightClient.tokenLifespan = Constants.JWT_TOKEN_LIFESPAN_SECONDS;
     LightClient.refreshTokenLifespan = Constants.JWT_REFRESH_TOKEN_LIFESPAN_SECONDS;
 
@@ -18,18 +20,45 @@ const LensSDK = {
   },
   health: {
     ping: () =>
-      HealthAPI.ping(LightClient).then(
+      LensSDK._call(HealthAPI.ping).then(
         (response) => response == Constants.RESPONSE_HEALTH_OK
       ),
   },
   authentication: {
-    login: () => {},
-    refresh: () => {},
-    revoke: () => {},
-    verify: () => {},
-    list: () => {},
-    isAuthenticated: () => false,
-    getAccessToken: () => LightClient.auth.accessToken,
+    profiles: (profilesManagedRequest) =>
+      LensSDK._call(
+        AuthAPI.profiles,
+        AuthRequests.PROFILES_MANAGED_REQUEST,
+        profilesManagedRequest
+      ),
+    login: (challengeRequest) => 
+        LensSDK._call(
+            AuthAPI.challenge,
+            AuthRequests.CHALLENGE_REQUEST,
+            challengeRequest
+        ).then((challenge) => Ethers.getProvider()
+        .signMessage(challenge.text)
+        .then((signature) => {
+            let signedChallengeRequest = AuthRequests.SIGNED_CHALLENGE_REQUEST;
+            signedChallengeRequest.id = challenge.id;
+            signedChallengeRequest.signature = signature;
+
+            return LensSDK._call(AuthAPI.login, AuthRequests.SIGNED_CHALLENGE_REQUEST, signedChallengeRequest).then((auth) => {
+                LightClient.auth = auth;
+                LensSDK.profile = {}; // Fetch logged user profile once profile API gets implemented
+                return LensSDK.profile;
+            });
+        })),
+    refresh: (refreshTokenRequest) => LensSDK._call(AuthAPI.refresh, AuthRequests.REFRESH_TOKEN_REQUEST, refreshTokenRequest).then((auth) => {
+        LightClient.auth = auth;
+        LensSDK.profile = {}; // Fetch logged user profile once profile API gets implemented
+        return LensSDK.profile;
+    }),
+    revoke: (revokeAuthenticationRequest) => LensSDK._call(AuthAPI.revoke, AuthRequests.REVOKE_AUTHENTICATION_REQUEST, revokeAuthenticationRequest),
+    verify: (verifyTokenRequest) => LensSDK._call(AuthAPI.verify, AuthRequests.VERIFY_TOKEN_REQUEST, verifyTokenRequest),
+    list: (approvedAuthenticationsRequest) => LensSDK._call(AuthAPI.list, AuthRequests.APPROVED_AUTHENTICATIONS_REQUEST, approvedAuthenticationsRequest),
+    isAuthenticated: () => LensSDK.profile.id != "",
+    getAccessToken: () => LightClient.auth.accessToken || null,
     getProfileId: () => LensSDK.profile.id || null,
   },
   profile: {
@@ -74,6 +103,11 @@ const LensSDK = {
     status: () => {},
     txIdToTxHash: () => {},
   },
+  _call: (apiMethod, requestObject, dataObject) =>
+    apiMethod(
+      LightClient,
+      dataObject ? ApiHelper.intersect(requestObject, dataObject) : null
+    ),
 };
 
 return LensSDK.init();
