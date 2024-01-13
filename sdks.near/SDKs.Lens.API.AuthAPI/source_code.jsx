419030add771e117f433146e717737e91aaa5cd8
sdks.near <sdks.near>
2024-01-13 14:46:57 +0000

    Update SDKs.Lens.API.AuthAPI by sdks.near at 2024-01-13T14:46:57.016Z

--- sdks.near/SDKs.Lens.API.AuthAPI/source_code.jsx
+++ sdks.near/SDKs.Lens.API.AuthAPI/source_code.jsx
@@ -1,43 +1,57 @@
 const $ = VM.require(`sdks.near/widget/Loader`);
 const { Auth } = $("@sdks/lens/queries");
+const { Interfaces } = $("@sdks/lens/definitions");
 
 return {
-  profiles: (Client, { limit, evmAddress, includeOwned }) => {
-    return Client.graphql(Auth.PROFILES_MANAGED_QUERY, {
-      limit: limit || "Ten",
-      cursor: {},
-      for: evmAddress,
-      includeOwned: includeOwned || true,
-    }).then((payload) => payload.body.data.profilesManaged.items || []);
+  profiles: (Client, profilesManagedRequest) => {
+    return Client.graphql(
+      Auth.PROFILES_MANAGED_QUERY,
+      profilesManagedRequest
+    ).then((payload) => payload.body.data.profilesManaged.items || []);
   },
-  login: (Client, { signedBy, profileId }) => {
+  login: (Client, signedChallengeRequest) => {
     return new Promise((resolve, reject) => {
-      return Client.graphql(Auth.AUTH_CHALLENGE_QUERY, {
-        signedBy,
-        for: profileId,
-      }).then((data) => {
-        let challengeId = data.body.data.challenge.id;
-        let challengeText = data.body.data.challenge.text;
-        return Ethers.getProvider()
-          .signMessage(challengeText)
-          .then((signature) => {
-            return Client.graphql(AUTH_AUTHENTICATE_QUERY, {
-              id: challengeId,
-              signature,
-            }).then((payload) => {
-              return (
-                payload.body.data.authenticate || {
-                  accessToken: "",
-                  refreshToken: "",
-                }
-              );
+      return Client.graphql(Auth.CHALLENGE_QUERY, signedChallengeRequest).then(
+        (data) => {
+          const challengeId = data.body.data.challenge.id;
+          const challengeText = data.body.data.challenge.text;
+          return Ethers.getProvider()
+            .signMessage(challengeText)
+            .then((signature) => {
+              return Client.graphql(Auth.AUTHENTICATE_QUERY, {
+                id: challengeId,
+                signature,
+              }).then((payload) => {
+                return (
+                  payload.body.data.authenticate || Interfaces.AUTH_INTERFACE
+                );
+              });
             });
-          });
-      });
+        }
+      );
     });
   },
-  refresh: () => {},
-  revoke: () => {},
-  verify: () => {},
-  list: () => {},
+  refresh: (Client, refreshRequest) => {
+    return Client.graphql(Auth.PROFILES_MANAGED_QUERY, refreshRequest).then(
+      (payload) => payload.body.data.refresh || Interfaces.AUTH_INTERFACE
+    );
+  },
+  revoke: (Client, revokeAuthenticationRequest) => {
+    return Client.graphql(
+      Auth.REVOKE_AUTHENTICATION_QUERY,
+      revokeAuthenticationRequest
+    ).then((payload) => true);
+  },
+  list: (Client, approvedAuthenticationsRequest) => {
+    return Client.graphql(
+      Auth.APPROVED_AUTHENTICATIONS_QUERY,
+      approvedAuthenticationsRequest
+    ).then((payload) => payload.body.data.approvedAuthentication.items || []);
+  },
+  verify: (Client, verifyTokenRequest) => {
+    return Client.graphql(
+      Auth.VERIFY_TOKEN_REQUEST,
+      verifyTokenRequest
+    ).then((payload) => (payload.body.data.verify || false) == true);
+  },
 };
