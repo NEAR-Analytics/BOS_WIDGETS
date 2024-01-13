d6d347dc0d6225cd65b5b80c527900df45886885
sdks.near <sdks.near>
2024-01-13 16:15:10 +0000

    Update SDKs.Lens.Main by sdks.near at 2024-01-13T16:15:10.047Z

--- sdks.near/SDKs.Lens.Main/source_code.jsx
+++ sdks.near/SDKs.Lens.Main/source_code.jsx
@@ -14,7 +14,8 @@ const LensSDK = {
     LensSDK.enableMainnet();
     LightClient.auth = Interfaces.AUTH_INTERFACE;
     LightClient.tokenLifespan = Constants.JWT_TOKEN_LIFESPAN_SECONDS;
-    LightClient.refreshTokenLifespan = Constants.JWT_REFRESH_TOKEN_LIFESPAN_SECONDS;
+    LightClient.refreshTokenLifespan =
+      Constants.JWT_REFRESH_TOKEN_LIFESPAN_SECONDS;
 
     return LensSDK;
   },
@@ -31,32 +32,58 @@ const LensSDK = {
         AuthRequests.PROFILES_MANAGED_REQUEST,
         profilesManagedRequest
       ),
-    login: (challengeRequest) => 
-        LensSDK._call(
-            AuthAPI.challenge,
-            AuthRequests.CHALLENGE_REQUEST,
-            challengeRequest
-        ).then((challenge) => Ethers.getProvider()
-        .signMessage(challenge.text)
-        .then((signature) => {
+    login: (challengeRequest) =>
+      LensSDK._call(
+        AuthAPI.challenge,
+        AuthRequests.CHALLENGE_REQUEST,
+        challengeRequest
+      ).then((challenge) =>
+        Ethers.getProvider()
+          .signMessage(challenge.text)
+          .then((signature) => {
             let signedChallengeRequest = AuthRequests.SIGNED_CHALLENGE_REQUEST;
             signedChallengeRequest.id = challenge.id;
             signedChallengeRequest.signature = signature;
 
-            return LensSDK._call(AuthAPI.login, AuthRequests.SIGNED_CHALLENGE_REQUEST, signedChallengeRequest).then((auth) => {
-                LightClient.auth = auth;
-                LensSDK.profile = {}; // Fetch logged user profile once profile API gets implemented
-                return LensSDK.profile;
+            return LensSDK._call(
+              AuthAPI.authenticate,
+              AuthRequests.SIGNED_CHALLENGE_REQUEST,
+              signedChallengeRequest
+            ).then((auth) => {
+              LightClient.auth = auth;
+              LensSDK.profile = {}; // Fetch logged user profile once profile API gets implemented
+              return LensSDK.profile;
             });
-        })),
-    refresh: (refreshTokenRequest) => LensSDK._call(AuthAPI.refresh, AuthRequests.REFRESH_TOKEN_REQUEST, refreshTokenRequest).then((auth) => {
+          })
+      ),
+    refresh: (refreshTokenRequest) =>
+      LensSDK._call(
+        AuthAPI.refresh,
+        AuthRequests.REFRESH_TOKEN_REQUEST,
+        refreshTokenRequest
+      ).then((auth) => {
         LightClient.auth = auth;
         LensSDK.profile = {}; // Fetch logged user profile once profile API gets implemented
         return LensSDK.profile;
-    }),
-    revoke: (revokeAuthenticationRequest) => LensSDK._call(AuthAPI.revoke, AuthRequests.REVOKE_AUTHENTICATION_REQUEST, revokeAuthenticationRequest),
-    verify: (verifyTokenRequest) => LensSDK._call(AuthAPI.verify, AuthRequests.VERIFY_TOKEN_REQUEST, verifyTokenRequest),
-    list: (approvedAuthenticationsRequest) => LensSDK._call(AuthAPI.list, AuthRequests.APPROVED_AUTHENTICATIONS_REQUEST, approvedAuthenticationsRequest),
+      }),
+    revoke: (revokeAuthenticationRequest) =>
+      LensSDK._call(
+        AuthAPI.revoke,
+        AuthRequests.REVOKE_AUTHENTICATION_REQUEST,
+        revokeAuthenticationRequest
+      ),
+    verify: (verifyTokenRequest) =>
+      LensSDK._call(
+        AuthAPI.verify,
+        AuthRequests.VERIFY_TOKEN_REQUEST,
+        verifyTokenRequest
+      ),
+    list: (approvedAuthenticationsRequest) =>
+      LensSDK._call(
+        AuthAPI.list,
+        AuthRequests.APPROVED_AUTHENTICATIONS_REQUEST,
+        approvedAuthenticationsRequest
+      ),
     isAuthenticated: () => LensSDK.profile.id != "",
     getAccessToken: () => LightClient.auth.accessToken || null,
     getProfileId: () => LensSDK.profile.id || null,
