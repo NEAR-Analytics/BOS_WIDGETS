32fa8d699313e655764d55d89e82f514bff2b702
bos.dapplets.near <bos.dapplets.near>
2024-01-15 13:40:53 +0000

    Update ContextActionsGroup by bos.dapplets.near at 2024-01-15T13:40:53.960Z

--- bos.dapplets.near/ContextActionsGroup/source_code.jsx
+++ bos.dapplets.near/ContextActionsGroup/source_code.jsx
@@ -97,75 +97,9 @@ const ActionsWrapper = styled.div`
   }
   animation: translateAnimation 1.5s linear forwards;
   transition: all 0.3s;
-`;
-
-const ActionsItem = styled.div`
-  display: flex;
-  width: 34px;
-  height: 34px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  background: #db504a;
-  margin-bottom: 6px;
-  cursor: pointer;
-  box-sizing: border-box;
-  @keyframes translateAnimationItem {
-    0% {
-      opacity: 0;
-    }
-    50% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-  animation: translateAnimationItem 1.5s linear forwards;
-  transition: all 0.3s;
-  &:hover {
-    border: 1px solid #c1c6ce;
-    box-shadow: 0px 4px 20px 0px rgba(11, 87, 111, 0.15),
-      0px 4px 5px 0px rgba(45, 52, 60, 0.1);
-  }
-  .ItemActive {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    img {
-      border: 1px solid #19ceae;
-      object-fit: cover;
-      border-radius: 50%;
-      width: 34px;
-      height: 34px;
-      display: block;
-    }
-    &:before {
-      content: "";
-      display: block;
-      position: absolute;
-      top: 0;
-      right: 0;
-      background: #fff;
-      width: 12px;
-      height: 12px;
-      border-radius: 50%;
-    }
-
-    &:after {
-      content: "";
-      display: block;
-      position: absolute;
-      top: 2px;
-      right: 2px;
-      background: #19ceae;
-      width: 8px;
-      height: 8px;
-      border-radius: 50%;
+   slot {
+    div {
+      background: "#ccc";
     }
   }
 `;
@@ -328,6 +262,32 @@ const iconSwitch = (
     <path
       d="M14 10C14 5.58172 17.5817 2 22 2C26.4183 2 30 5.58172 30 10C30 14.4183 26.4183 18 22 18C17.5817 18 14 14.4183 14 10Z"
       fill="white"
+    />
+  </svg>
+);
+
+const iconRemoveAction = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+  >
+    <circle cx="7" cy="7" r="6.5" fill="#DB504A" stroke="white" />
+    <path
+      d="M9.24976 4.75L4.74999 9.24977"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M4.75 4.75L9.24977 9.24977"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
 );
