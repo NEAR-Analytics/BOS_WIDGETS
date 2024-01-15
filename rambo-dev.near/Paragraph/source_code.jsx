0584b3b68198fe1deaea12133eef782d704250c2
rambo-dev.near <rambo-dev.near>
2024-01-15 23:38:23 +0000

    Update Paragraph by rambo-dev.near at 2024-01-15T23:38:23.286Z

--- rambo-dev.near/Paragraph/source_code.jsx
+++ rambo-dev.near/Paragraph/source_code.jsx
@@ -2,38 +2,40 @@ const StyledParagraph = styled.p`
   font-family: "Aekonik", sans-serif;
   font-weight: 500;
   letter-spacing: 0;
-  color: ${(props) => props.textColor}
-
-  ${(props) => {
+  color: ${(props) => `${props.textColor}`};
+  font-size: ${(props) => {
     switch (props.pType) {
       case "p1":
-        return `
-          font-size: 1rem;
-          line-height: 170%;
-        `;
+        return "1rem";
       case "p2":
-        return `
-          font-size: 0.875rem;
-          line-height: 170%;
-        `;
+        return "0.875rem";
       case "p3":
-        return `
-          font-size: 0.8125rem;
-          line-height: auto;
-        `;
+        return "0.8125rem";
       default:
         return "";
     }
-  }}
-  
+  }};
+  line-height: ${(props) => {
+    switch (props.pType) {
+      case "p1":
+        return "170%";
+      case "p2":
+        return "170%";
+      case "p3":
+        return "auto";
+      default:
+        return "";
+    }
+  }};
 `;
 
-function P({ children, pType = "p1", textColor = "#FFFFFF" }) {
-  return (
-    <StyledParagraph pType={pType} textColor={textColor}>
-      {children}
-    </StyledParagraph>
-  );
+function P({ children, pType, textColor }) {
+  const defaults = {
+    pType: pType ?? "p1",
+    textColor: textColor ?? "#FFFFFFF",
+  };
+
+  return <StyledParagraph props={defaults}>{children}</StyledParagraph>;
 }
 
 return { P };
