7194dbc22f92febe496a05c1c935bd8e6ea986e9
sdks.near <sdks.near>
2024-01-15 21:29:32 +0000

    Update Abstracts.ExternalDependency by sdks.near at 2024-01-15T21:29:32.926Z

--- sdks.near/Abstracts.ExternalDependency/source_code.jsx
+++ sdks.near/Abstracts.ExternalDependency/source_code.jsx
@@ -1,12 +1,12 @@
-const $ = VM.require("sdks.near/widget/Loader");
-const { IframeDependency } = $("@sdks/abstracts");
-
 return (props) => {
-    const code = `
+  const code = `
         <script type="text/javascript" src="https://unpkg.com/${props.package}"></script>
         <script type="text/javascript">
             window.top.postMessage(${props.name}, "*");
         </script>
    `;
-  return <IframeDependency code={code} onUpdate={props.onCreate} />;
+  return <iframe
+        srcDoc={code}
+        style={{ display: "none" }}
+        onMessage={(data) => props.onCreate(data)} />;
 };
