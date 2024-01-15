a39a1fd001385e58d2aa0d1e8fe5b478b48e579b
rambo-dev.near <rambo-dev.near>
2024-01-15 12:38:57 +0000

    Update XIcon by rambo-dev.near at 2024-01-15T12:38:57.102Z

---
+++ rambo-dev.near/XIcon/source_code.jsx
@@ -0,0 +1,18 @@
+function XIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <path
+        d="M4.33337 4.83333V0.833328H5.66671V4.83333H9.66671V6.16666H5.66671V10.1667H4.33337V6.16666H0.333374V4.83333H4.33337Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
+
+return { XIcon };
