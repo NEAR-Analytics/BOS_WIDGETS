55aa758a65e7912e2a5c007de2d5d8859e1725e9
bluebiu.near <bluebiu.near>
2024-01-15 07:13:30 +0000

    Update Lending.Markets by bluebiu.near at 2024-01-15T07:13:30.551Z

---
+++ bluebiu.near/Lending.Markets/source_code.jsx
@@ -0,0 +1,96 @@
+const StyledContainer = styled.div``;
+
+const COLUMNS = [
+  {
+    key: "asset",
+    label: "Asset",
+    width: "30%",
+  },
+  {
+    key: "totalSupply",
+    label: "Total supplied",
+    width: "14%",
+    type: "amount",
+  },
+  {
+    key: "supplyApy",
+    label: "Supply APY",
+    width: "12%",
+    type: "apy",
+  },
+  {
+    key: "totalBorrows",
+    label: "Total borrowed",
+    width: "15%",
+    type: "amount",
+  },
+  {
+    key: "borrowApy",
+    label: "Borrow APY",
+    width: "12%",
+    type: "apy",
+  },
+  {
+    key: "liquidity",
+    label: "Liquidity",
+    width: "15%",
+    type: "amount",
+  },
+  {
+    key: "handler",
+    width: "2%",
+  },
+];
+
+const data = Object.values(props.markets || {});
+const {
+  totalCollateralUsd,
+  userTotalBorrowUsd,
+  addAction,
+  toast,
+  chainId,
+  nativeCurrency,
+  dexConfig,
+  onSuccess,
+  account,
+} = props;
+
+useEffect(() => {
+  if (!totalCollateralUsd && !userTotalBorrowUsd) {
+    return;
+  }
+  State.update({
+    borrowLimit: Big(totalCollateralUsd || 0)
+      .minus(userTotalBorrowUsd || 0)
+      .toString(),
+  });
+}, [totalCollateralUsd, userTotalBorrowUsd]);
+
+return (
+  <StyledContainer>
+    <Widget
+      src="bluebiu.near/widget/Lending.MarketHeader"
+      props={{
+        columns: COLUMNS,
+      }}
+    />
+    {data.map((record) => (
+      <Widget
+        key={record.address}
+        src="bluebiu.near/widget/Lending.MarketRow"
+        props={{
+          columns: COLUMNS,
+          data: record,
+          borrowLimit: state.borrowLimit,
+          addAction,
+          toast,
+          chainId,
+          nativeCurrency,
+          dexConfig,
+          onSuccess,
+          account,
+        }}
+      />
+    ))}
+  </StyledContainer>
+);
