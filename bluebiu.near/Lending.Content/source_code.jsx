4355b19a5b0ac984c26450226ac2bbd051dab5bc
bluebiu.near <bluebiu.near>
2024-01-15 07:13:30 +0000

    Update Lending.Content by bluebiu.near at 2024-01-15T07:13:30.551Z

---
+++ bluebiu.near/Lending.Content/source_code.jsx
@@ -0,0 +1,143 @@
+const StyledContainer = styled.div`
+  padding-top: 18px;
+`;
+
+const {
+  dexConfig,
+  wethAddress,
+  multicallAddress,
+  chainIdNotSupport,
+  multicall,
+  prices,
+  account,
+  addAction,
+  toast,
+  chainId,
+  nativeCurrency,
+  tab,
+} = props;
+
+useEffect(() => {
+  State.update({
+    loading: !chainIdNotSupport,
+  });
+}, []);
+
+const handleTableButtonClick = (address, actionText) => {
+  const market = state.markets[address];
+  const dapp = {
+    userTotalSupplyUsd: state.userTotalSupplyUsd,
+    userTotalBorrowUsd: state.userTotalBorrowUsd,
+    totalCollateralUsd: state.totalCollateralUsd,
+    rewards: state.rewards,
+    dappIcon: dexConfig.icon,
+    dappName: dexConfig.name,
+  };
+
+  State.update({
+    tableButtonClickData: {
+      ...dapp,
+      ...market,
+      config: { ...dexConfig, wethAddress },
+      actionText,
+    },
+    showDialog: true,
+  });
+};
+
+return (
+  <StyledContainer>
+    {tab === "market" && (
+      <Widget
+        src="bluebiu.near/widget/Lending.Markets"
+        props={{
+          markets: state.markets,
+          totalCollateralUsd: state.totalCollateralUsd,
+          userTotalBorrowUsd: state.userTotalBorrowUsd,
+          addAction,
+          toast,
+          chainId,
+          nativeCurrency,
+          dexConfig,
+          account,
+          onSuccess: () => {
+            State.update({
+              loading: true,
+            });
+          },
+        }}
+      />
+    )}
+    {tab === "yours" && (
+      <Widget
+        src="bluebiu.near/widget/Avalanche.Lending.Yours"
+        props={{
+          currentDapp: dexConfig.name,
+          markets: state.markets,
+          timestamp: state.timestamp,
+          dapps: {
+            [dexConfig.name]: {
+              userTotalSupplyUsd: state.userTotalSupplyUsd,
+              userTotalBorrowUsd: state.userTotalBorrowUsd,
+              totalCollateralUsd: state.totalCollateralUsd,
+              rewards: state.rewards,
+              dappIcon: dexConfig.icon,
+              dappName: dexConfig.name,
+            },
+          },
+          dappsConfig: {
+            [dexConfig.name]: dexConfig,
+          },
+          toast,
+          onButtonClick: handleTableButtonClick,
+          onSuccess: () => {
+            State.update({
+              loading: true,
+            });
+          },
+        }}
+      />
+    )}
+    <Widget
+      src={dexConfig.data}
+      props={{
+        update: state.loading,
+        account,
+        wethAddress,
+        multicallAddress,
+        multicall,
+        prices,
+        ...dexConfig,
+        onLoad: (data) => {
+          console.log("data", data);
+          State.update({
+            loading: false,
+            timestamp: Date.now(),
+            ...data,
+          });
+        },
+      }}
+    />
+    <Widget
+      src="bluebiu.near/widget/Avalanche.Lending.Dialog"
+      props={{
+        display: state.showDialog,
+        data: state.tableButtonClickData,
+        chainId,
+        addAction,
+        toast,
+        source: "dapp",
+        onClose: () => {
+          State.update({
+            showDialog: false,
+          });
+        },
+        onSuccess: () => {
+          State.update({
+            loading: true,
+          });
+        },
+      }}
+    />
+  </StyledContainer>
+);
