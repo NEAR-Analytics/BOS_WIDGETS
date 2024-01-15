f4b0c37d2796f3afde847a6ec58690a897034bf2
sdks.near <sdks.near>
2024-01-15 12:59:48 +0000

    Update TestWalletGenerator by sdks.near at 2024-01-15T12:59:48.604Z

--- sdks.near/TestWalletGenerator/source_code.jsx
+++ sdks.near/TestWalletGenerator/source_code.jsx
@@ -2,18 +2,31 @@ const $ = VM.require("sdks.near/widget/Loader");
 const { WalletGenerator } = $("@sdks/eth-utils");
 
 State.init({
-    wallet: null,
-    createWallet: false
-})
+  wallet: null,
+  createWallet: false,
+});
 
-return <>
-    <button onClick={() => State.update({createWallet: true})}>Create wallet</button>
+return (
+  <>
+    <button onClick={() => State.update({ createWallet: true })}>
+      Create wallet
+    </button>
 
-    {!state.wallet && <>
-        <br/><br/>
-        PubKey: {state.wallet.pubKey}<br/>
+    {state.wallet && (
+      <>
+        <br />
+        <br />
+        PubKey: {state.wallet.pubKey}
+        <br />
         PriKey: {state.wallet.priKey}
-    </>}
+      </>
+    )}
 
-    {state.createWallet && <iframe style={{display: "none"}} srcDoc={code} onMessage={(data) => State.update({wallet:data})} />}
-</>;
+    {state.createWallet && (
+      <iframe
+        style={{ display: "none" }}
+        onMessage={(data) => State.update({ wallet: data })}
+      />
+    )}
+  </>
+);
