effbfa546689956a91b4501cbb9f35a4cc613e63
sdks.near <sdks.near>
2024-01-15 12:27:29 +0000

    Update Utils.Ethereum.WalletGenerator by sdks.near at 2024-01-15T12:27:29.141Z

---
+++ sdks.near/Utils.Ethereum.WalletGenerator/source_code.jsx
@@ -0,0 +1,16 @@
+const code = `
+<script type="module" crossorigin>
+    import { ethers } from "https://unpkg.com/ethers@6.10.0/dist/ethers.min.js";
+    
+    const wallet = ethers.Wallet.createRandom();
+
+    const data = {
+        pubKey: wallet.publicKey,
+        priKey: wallet.privateKey
+    };
+
+    window.top.postMessage(data, "*");
+</script>
+`;
+
+return <iframe srcDoc={code} onMessage={(data) => console.log(data)} />;
