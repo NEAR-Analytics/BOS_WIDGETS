305475409a7aa4159e27acf19b6ee52a586de956
devs.near <devs.near>
2024-01-16 16:16:24 +0000

    Update Feed by devs.near at 2024-01-16T16:16:24.718Z

--- devs.near/Feed/source_code.jsx
+++ devs.near/Feed/source_code.jsx
@@ -1,95 +1,53 @@
-const Feed = ({
-  index,
-  items,
-  typeWhitelist,
-  Item,
-  Layout,
-  showCompose,
-  perPage,
-}) => {
+/**
+ * TODO
+ * 
+ * - [ ] allow a different source, like widget for an ItemFeed
+ * - [ ] 
+ * 
+ */
+const Feed = ({ index, typeWhitelist, Item, Layout }) => {
   Item = Item || ((props) => <div>{JSON.stringify(props)}</div>);
   Layout = Layout || (({ children }) => children);
+
   const renderItem = (a, i) => {
     if (typeWhitelist && !typeWhitelist.includes(a.value.type)) {
       return false;
     }
     return (
       <div key={JSON.stringify(a)}>
-        <Item {...a} />
+        <Item
+          accountId={a.accountId}
+          path={a.value.path}
+          blockHeight={a.blockHeight}
+          type={a.value.type}
+        />
       </div>
     );
   };
-  const composeIndex = () => {
-    const arr = Array.isArray(index) ? index : [index];
-    const grouped = arr.reduce((acc, i) => {
-      if (i.action !== "repost") {
-        if (!acc[i.action]) {
-          acc[i.action] = [];
-        }
-        acc[i.action].push({ key: i.key, value: { type: "md" } });
-      }
-      return acc;
-    }, {});
-    Object.keys(grouped).forEach((action) => {
-      if (grouped[action].length === 1) {
-        grouped[action] = grouped[action][0];
-      }
-      grouped[action] = JSON.stringify(grouped[action]);
-    });
-    return grouped;
-  };
-  const appendHashtags = (v) => {
-    const arr = Array.isArray(index) ? index : [index];
-    const hashtags = arr
-      .filter((i) => i.action === "hashtag")
-      .map((i) => i.key);
-    hashtags.forEach((hashtag) => {
-      if (v.toLowerCase().includes(`#${hashtag.toLowerCase()}`)) return;
-      else v += ` #${hashtag}`;
-    });
-    return v;
-  };
-  if (items) {
+
+  if (Array.isArray(index)) {
     return (
       <Widget
-        src="devs.near/widget/PR.ItemFeed"
+        src="nearbuilders.testnet/widget/PR.MergedIndexFeed"
         props={{
-          items,
+          index,
           renderItem,
-          perPage: perPage,
+          Layout: ({ children }) => <Layout>{children}</Layout>,
+        }}
+      />
+    );
+  } else {
+    return (
+      <Widget
+        src="nearbuilders.testnet/widget/PR.FilteredIndexFeed"
+        props={{
+          index,
+          renderItem,
           Layout: ({ children }) => <Layout>{children}</Layout>,
         }}
       />
     );
   }
-  return (
-    <>
-      {showCompose && (
-        <Widget
-          src="devs.near/widget/Compose"
-          props={{ index: composeIndex(), appendHashtags }}
-        />
-      )}
-      {Array.isArray(index) ? (
-        <Widget
-          src="devs.near/widget/PR.MergedIndexFeed"
-          props={{
-            index,
-            renderItem,
-            Layout: ({ children }) => <Layout>{children}</Layout>,
-          }}
-        />
-      ) : (
-        <Widget
-          src="devs.near/widget/PR.FilteredIndexFeed"
-          props={{
-            index,
-            renderItem,
-            Layout: ({ children }) => <Layout>{children}</Layout>,
-          }}
-        />
-      )}
-    </>
-  );
 };
+
 return { Feed };
