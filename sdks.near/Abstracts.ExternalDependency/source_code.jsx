f618a141595ac5ab0c13f58c686f8188c4849d74
sdks.near <sdks.near>
2024-01-17 16:51:55 +0000

    Update Abstracts.ExternalDependency by sdks.near at 2024-01-17T16:51:55.115Z

--- sdks.near/Abstracts.ExternalDependency/source_code.jsx
+++ sdks.near/Abstracts.ExternalDependency/source_code.jsx
@@ -2,11 +2,45 @@ return (props) => {
   const code = `
         <script type="text/javascript" src="https://unpkg.com/${props.package}"></script>
         <script type="text/javascript">
-            window.top.postMessage(${props.name}, "*");
+            const getPackageMethod = (instruction, package) => {
+                return instruction.split(".").reduce((path, nextPath) => (path || {})[nextPath], package);
+            };
+        
+            window.addEventListener("message", (e) => {
+              if (e.data.instruction) {
+                 let value = Array.isArray(e.data.value) ? e.data.value : [e.data.value];
+                 let result = getPackageMethod(e.data.instruction, ${props.name})(...value);
+                 let parseResult = (result) => {
+                    if (e.data.returnType == "object" || e.data.returnType == "array") {
+                        return JSON.stringify(result);
+                    }
+        
+                    if (e.data.returnType == "string") {
+                        return result.toString();
+                    }
+
+                    if (e.data.returnType == "number") {
+                        return result;
+                    }
+                 }
+        
+                 window.top.postMessage({
+                    index: e.data.index,
+                    instruction: e.data.instruction,
+                    returnType: e.data.returnType,
+                    result: parseResult(result)
+                 }, "*");
+              }
+            })
         </script>
    `;
-  return <iframe
-        srcDoc={code}
-        style={{ display: "none" }}
-        onMessage={(data) => props.onCreate(data)} />;
+
+  return (
+    <iframe
+      srcDoc={code}
+      style={{ display: "none" }}
+      message={props.adapter.getRequest()}
+      onMessage={(data) => props.adapter.setResponse(data)}
+    />
+  );
 };
