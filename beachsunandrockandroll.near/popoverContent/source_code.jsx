39aac2c85717edf7d17107da36b50a50d91056bd
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-01-17 14:59:16 +0000

    Update popoverContent by beachsunandrockandroll.near at 2024-01-17T14:59:16.806Z

--- beachsunandrockandroll.near/popoverContent/source_code.jsx
+++ beachsunandrockandroll.near/popoverContent/source_code.jsx
@@ -1,17 +1,17 @@
-let { align, sideOffset, className, ...addProps } = props;
+let { align, sideOffset, className } = props;
 
 console.log(props);
-console.log('className', className);
-console.log('align', align);
+console.log("className", className);
+console.log("align", align);
 const base =
   "z-50 w-72 rounded-2 border bg-success p-4 text-white shadow outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2";
 
-const PopoverContent = ({ children }) => (
+const PopoverContent = ({ children, className, align, ...pp }) => (
   <Popover.Content
     align={align}
     sideOffset={sideOffset}
     className={`${base} ${className ?? ``}`}
-    {...addProps}
+    {...pp}
   >
     {children}
   </Popover.Content>
