e779b4e3bf26713ba92e0c71057cec0abcc377bd
bos.dapplets.near <bos.dapplets.near>
2024-01-17 11:47:50 +0000

    Update ContextActionsGroup by bos.dapplets.near at 2024-01-17T11:47:50.208Z

--- bos.dapplets.near/ContextActionsGroup/source_code.jsx
+++ bos.dapplets.near/ContextActionsGroup/source_code.jsx
@@ -99,7 +99,7 @@ const ActionsWrapper = styled.div`
   @keyframes translateAnimation {
     0% {
       display: none;
-
+transform: scale(0);
       opacity: 1;
   
     }
@@ -109,7 +109,7 @@ const ActionsWrapper = styled.div`
       border: 1px solid #db504a;
       opacity: 1;
       right: 60px;
-    
+    transform: scale(1);
     }
   }
   animation: translateAnimation 1.3s linear forwards;
