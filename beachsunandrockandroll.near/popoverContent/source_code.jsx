73072a9b7b48efb0a04143d760ec7ab3b175d9c0
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-01-17 15:08:02 +0000

    Update popoverContent by beachsunandrockandroll.near at 2024-01-17T15:08:02.140Z

--- beachsunandrockandroll.near/popoverContent/source_code.jsx
+++ beachsunandrockandroll.near/popoverContent/source_code.jsx
@@ -1,26 +1,24 @@
-
 const base =
   "z-50 w-72 rounded-2 border bg-success p-4 text-white shadow outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2";
-const PopoverTrigger = ({children, ...bprops}) => {
-    return (
-        <Popover.Trigger {...bprops}>{children}</Popover.Trigger>
-    );
-    
-}
-
-const PopoverContent = ({ children, className, align, ...pp }) => {
-  console.log("thca", className);
+const PopoverTrigger2 = ({ children, ...bprops }) => {
   return (
-    <Popover.Content
-      align={align}
-      sideOffset={sideOffset}
-      className={`${base} ${className ?? ``}`}
-      {...pp}
-    >
-      {children}
-    </Popover.Content>
+      <Popover.Trigger {...bprops}>
+          {children}
+      </Popover.Trigger>
   );
 };
-// );
 
-return { PopoverContent, PopoverTrigger };
+const PopoverContent = ({ children, className, align, ...pp }) => {
+    return (
+        <Popover.Content
+          align={align}
+          sideOffset={sideOffset}
+          className={`${base} ${className ?? ``}`}
+          {...pp}
+        >
+          {children}
+        </Popover.Content>
+    );
+};
+
+return { PopoverContent, PopoverTrigger2 };
