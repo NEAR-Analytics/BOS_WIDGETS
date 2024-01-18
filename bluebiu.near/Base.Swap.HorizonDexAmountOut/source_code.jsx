e0997bd28ccf898aedb70df4a745f423eb96703a
bluebiu.near <bluebiu.near>
2024-01-18 07:38:00 +0000

    Update Base.Swap.HorizonDexAmountOut by bluebiu.near at 2024-01-18T07:38:00.639Z

--- bluebiu.near/Base.Swap.HorizonDexAmountOut/source_code.jsx
+++ bluebiu.near/Base.Swap.HorizonDexAmountOut/source_code.jsx
@@ -131,12 +131,8 @@ const {
   slippage,
   account,
   fees,
+  prices,
 } = props;
-
-const prices = Storage.get(
-  "tokensPrice",
-  "dapdapbos.near/widget/Linea.Uniswap.Swap.TokensPrice"
-);
 
 useEffect(() => {
   if (!updater || !prices) return;
