bbc188dcb912d47c2c97fb55ef70c88404bf51b0
bos.dapplets.near <bos.dapplets.near>
2024-01-18 13:49:39 +0000

    Update ContextActionsGroup by bos.dapplets.near at 2024-01-18T13:49:39.702Z

--- bos.dapplets.near/ContextActionsGroup/source_code.jsx
+++ bos.dapplets.near/ContextActionsGroup/source_code.jsx
@@ -301,32 +301,6 @@ const iconSwitch = (
   </svg>
 );
 
-const iconRemoveAction = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="14"
-    height="14"
-    viewBox="0 0 14 14"
-    fill="none"
-  >
-    <circle cx="7" cy="7" r="6.5" fill="#DB504A" stroke="white" />
-    <path
-      d="M9.24976 4.75L4.74999 9.24977"
-      stroke="white"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path
-      d="M4.75 4.75L9.24977 9.24977"
-      stroke="white"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-  </svg>
-);
-
 const ActionBlock = styled.span`
   position: relative;
   div {
@@ -374,7 +348,12 @@ return (
 
           {props.widgets.map((widget, i) => (
             <ActionBlock key={i}>
-              <RemoveAction onClick={() => handleRemoveWidget(widget.src, widget.linkId)}>{iconRemoveAction}</RemoveAction>
+              {props.isEditMode ? (
+                <Widget 
+                  src={'bos.dapplets.near/widget/LayoutManager.RemoveWidgetButton'} 
+                  props={{ onClick:() => handleRemoveWidget(widget.src, widget.linkId) }}
+                />
+              ) : null}
               <Widget src={widget.src} props={widget.props} />
             </ActionBlock>
           ))}
