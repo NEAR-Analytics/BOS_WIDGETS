d1e156b1b09a2570f2d7bce71cb7a7a3c7868a4b
buildhub.near <buildhub.near>
2024-01-19 19:50:05 +0000

    Update Feed by buildhub.near at 2024-01-19T19:50:05.879Z

--- buildhub.near/Feed/source_code.jsx
+++ buildhub.near/Feed/source_code.jsx
@@ -1,112 +1,224 @@
-const { Feed } = VM.require("devs.near/widget/Feed") || {
-  Feed: () => <></>,
-};
-const { Post, Button } = VM.require("buildhub.near/widget/components") || {
-  Post: () => <></>,
-  Button: () => <></>,
-};
-const { Header } = VM.require("buildhub.near/widget/components.Header") || {
-  Header: () => <></>,
-};
-const LoginContainer = styled.div`
-  background-color: #23242b;
-  color: #fff;
-  width: 100%;
-  height: 16rem;
-  border-radius: 1rem;
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 1rem;
-`;
-const { feedName, template, requiredHashtags, customActions } = props;
-// for modals
-const [item, setItem] = useState(null);
-const [showProposeModal, setShowProposeModal] = useState(false);
-const toggleProposeModal = () => {
-  setShowProposeModal(!showProposeModal);
-};
-const modalToggles = {
-  propose: toggleProposeModal,
-};
-customActions = [
-  {
-    type: "modal",
-    icon: "bi-file-earmark-text",
-    label: "Propose",
-    onClick: (modalToggles) => {
-      const toggle = modalToggles.propose;
-      toggle();
-    },
+const { Feed } = VM.require("devs.near/widget/Module.Feed");
+const { Button } = VM.require("buildhub.near/widget/components.Button");
+
+Button || (Button = () => <></>);
+Feed = Feed || (() => <></>); // ensure it's defined or set to a default component
+
+const { type, hashtag } = props;
+type = hashtag;
+hashtag = type;
+
+const { Post } = VM.require("buildhub.near/widget/components");
+Post = Post || (() => <></>);
+
+function formatDate(date) {
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  return date.toLocaleDateString("en-US", options);
+}
+
+const feeds = {
+  resolutions: {
+    label: "Resolutions",
+    icon: "bi-calendar3",
+    name: "resolution",
+    hashtag: "nearyearresolutions2024",
+    template: `### 🎉 NEAR YEAR RESOLUTIONS: 2024
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed))
+
+**🌟 REFLECTIONS ON THE PAST YEAR:**
+- [Reflection 1 from the past year]
+- [Reflection 2 from the past year]
+
+**🎯 NEW YEAR'S RESOLUTIONS:**
+- [Resolution 1]
+- [Resolution 2]
+
+**📊 MEASURING SUCCESS:**
+- [Metric 1 for Success]
+- [Metric 2 for Success]
+`
   },
-];
+  updates: {
+    label: "Updates",
+    icon: "bi-bell",
+    name: "update",
+    hashtag: "update",
+    template: `### BUILDER UPDATE:  ${formatDate(new Date())}
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=update))
+
+**✅ DONE**
+- [what'd you do]
+- [link proof]
+
+**⏩ NEXT**
+- [what's next?]
+- [what are you thinking about?]
+
+**🛑 BLOCKERS**
+- [what's blocking you?]
+- [how can someone help?]
+`
+  },
+  documentation: {
+    label: "Documentation",
+    icon: "bi-book",
+    name: "documentation",
+    hashtag: "documentation",
+    template: `## TITLE
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=documentation))
+
+**WHAT IS _____?**
+- [context]
+- [why is it important?]
+
+**EXAMPLE**
+- [how can this be demonstrated?]
+- [what is the expected outcome?]
+
+**USAGE**
+- [where is it used?]
+- [how to use it]
+`
+  },
+  question: {
+    label: "Question",
+    icon: "bi-question-lg",
+    name: "question",
+    hashtag: "question",
+    template: `## what is your question?
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=question))
+
+[what are you thinking about?]
+[why are you asking?]
+`
+  },
+  answer: {
+    label: "Answer",
+    icon: "bi-journal-code",
+    name: "answer",
+    hashtag: "answer",
+    template: `## Share an answer
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=answer))
+
+[please restate the question you are answering]
+
+[your answer]
+
+[link to relevant docs, examples, or resources]
+`
+  },
+  opportunity: {
+    label: "Opportunity",
+    icon: "bi-briefcase",
+    name: "opportunity",
+    hashtag: "opportunity",
+    template: `## TITLE
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=opportunity))
+
+[what is the opportunity?]
+
+[explain the motivation or reason]
+
+`
+  },
+  idea: {
+    label: "Idea",
+    icon: "bi-lightbulb",
+    name: "idea",
+    hashtag: "idea",
+    template: ``
+  },
+  task: {
+    label: "Task",
+    icon: "bi-check-lg",
+    name: "task",
+    template: `## TASK TITLE
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=task))
+
+**What needs to be done?**
+- [Describe the task or action steps]
+
+**Context or additional information:**
+- [Provide any context or details]
+`
+  },
+  bookmarks: {
+    label: "Bookmarks",
+    icon: "bi-bookmark",
+    name: "bookmark"
+  }
+};
+
+const [activeFeed, setActiveFeed] = useState(type || "resolutions");
+const [template, setTemplate] = useState("What did you have in mind?");
+
 return (
-  <div key={feedName}>
-    <Header>{feedName} Feed</Header>
-    {/* Modals */}
-    <Widget
-      src="buildhub.near/widget/components.modals.CreateProposal"
-      loading=""
-      props={{
-        showModal: showProposeModal,
-        toggleModal: toggleProposeModal,
-        item: item,
-      }}
-    />
-    {!context.accountId ? ( // if not logged in
-      <LoginContainer>
-        <p>Please login in order to post.</p>
-        <a
-          href={"https://nearbuilders.org/join"}
-          style={{ textDecoration: "none" }}
-        >
-          <Button variant="primary">Login</Button>
-        </a>
-      </LoginContainer>
-    ) : (
-      <Widget
-        loading={
-          <div
-            className="placeholder-glow h-100 w-100"
-            style={{ height: 400 }}
-          ></div>
-        }
-        src="buildhub.near/widget/Compose"
-        props={{
-          draftKey: feedName,
-          template: template,
-          requiredHashtags: requiredHashtags,
-          feed: { ...props },
-        }}
-      />
-    )}
-    <Feed
-      index={(requiredHashtags || []).map((it) => ({
-        action: "hashtag",
-        key: it,
-        options: {
-          limit: 10,
-          order: "desc",
-          subscribe: true,
-        },
-        cacheOptions: {
-          ignoreCache: true,
-        },
-        required: true,
-      }))}
-      Item={(p) => (
-        <Post
-          accountId={p.accountId}
-          blockHeight={p.blockHeight}
-          noBorder={true}
-          currentPath={`/buildhub.near/widget/app?page=feed`}
-          customActions={customActions}
-          modalToggles={modalToggles}
-          setItem={setItem}
-        />
-      )}
-    />
-  </div>
+  <Widget
+    src="buildhub.near/widget/components.AsideWithMainContent"
+    props={{
+      sideContent: Object.keys(feeds || {}).map((route) => {
+        const data = feeds[route];
+        return (
+          <Button
+            id={route}
+            variant={activeFeed === route ? "primary" : "outline"}
+            onClick={() => setActiveFeed(route)}
+            className={
+              "align-self-stretch flex-shrink-0 justify-content-start fw-medium"
+            }
+            style={{ fontSize: "14px" }}
+          >
+            <i className={`bi ${data.icon} `}></i>
+            {data.label}
+          </Button>
+        );
+      }),
+      mainContent: (
+        <>
+          {context.accountId ? (
+            activeFeed !== "bookmarks" ? (
+              <Widget
+                src="buildhub.near/widget/Compose"
+                props={{
+                  feed: feeds[activeFeed],
+                  template: feeds[activeFeed].template
+                }}
+              />
+            ) : (
+              <Widget src="buildhub.near/widget/Bookmarks" />
+            )
+          ) : (
+            <Widget
+              src="buildhub.near/widget/components.login-now"
+              props={props}
+            />
+          )}
+          {activeFeed !== "bookmarks" && (
+            <Feed
+              index={[
+                {
+                  action: "hashtag",
+                  key: feeds[activeFeed].hashtag,
+                  options: {
+                    limit: 10,
+                    order: "desc",
+                  },
+                  cacheOptions: {
+                    ignoreCache: true
+                  }
+                }
+              ]}
+              Item={(p) => (
+                <Post
+                  accountId={p.accountId}
+                  blockHeight={p.blockHeight}
+                  noBorder={true}
+                />
+              )}
+            />
+          )}
+        </>
+      )
+    }}
+  />
 );
