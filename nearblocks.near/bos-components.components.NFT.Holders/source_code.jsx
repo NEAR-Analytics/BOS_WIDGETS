bf8a5ca70f40b1209e4685a4acc467a76db09699
nearblocks.near <nearblocks.near>
2024-01-19 12:36:31 +0000

    Update bos-components.components.NFT.Holders by nearblocks.near at 2024-01-19T12:36:31.244Z

---
+++ nearblocks.near/bos-components.components.NFT.Holders/source_code.jsx
@@ -0,0 +1,681 @@
+/**
+ * Component: NFTHolders
+ * Author: Nearblocks Pte Ltd
+ * License: Business Source License 1.1
+ * Description: Non-Fungible Token Holders List.
+ * @interface Props
+ * @param {string} [network] - The network data to show, either mainnet or testnet
+ * @param {string} [id] - The token identifier passed as a string
+ * @param {Token} [token] - The Token type passed as object
+ */
+
+
+
+
+
+
+
+/* INCLUDE: "includes/formats.jsx" */
+function localFormat(number) {
+  const formattedNumber = Number(number).toLocaleString('en', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 5,
+  });
+  return formattedNumber;
+}
+
+function dollarFormat(number) {
+  const formattedNumber = Number(number).toLocaleString('en', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  return formattedNumber;
+}
+function dollarNonCentFormat(number) {
+  const formattedNumber = Number(number).toLocaleString('en', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+  return formattedNumber;
+}
+
+function weight(number) {
+  const suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  let suffixIndex = 0;
+
+  while (number >= 1000 && suffixIndex < suffixes.length - 1) {
+    number /= 1000;
+    suffixIndex++;
+  }
+
+  return number.toFixed(2) + ' ' + suffixes[suffixIndex];
+}
+
+function convertToUTC(timestamp, hour) {
+  const date = new Date(timestamp);
+
+  // Get UTC date components
+  const utcYear = date.getUTCFullYear();
+  const utcMonth = ('0' + (date.getUTCMonth() + 1)).slice(-2); // Adding 1 because months are zero-based
+  const utcDay = ('0' + date.getUTCDate()).slice(-2);
+  const utcHours = ('0' + date.getUTCHours()).slice(-2);
+  const utcMinutes = ('0' + date.getUTCMinutes()).slice(-2);
+  const utcSeconds = ('0' + date.getUTCSeconds()).slice(-2);
+
+  // Array of month abbreviations
+  const monthAbbreviations = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const monthIndex = Number(utcMonth) - 1;
+  // Format the date as required (Jul-25-2022 16:25:37)
+  let formattedDate =
+    monthAbbreviations[monthIndex] +
+    '-' +
+    utcDay +
+    '-' +
+    utcYear +
+    ' ' +
+    utcHours +
+    ':' +
+    utcMinutes +
+    ':' +
+    utcSeconds;
+
+  if (hour) {
+    // Convert hours to 12-hour format
+    let hour12 = parseInt(utcHours);
+    const ampm = hour12 >= 12 ? 'PM' : 'AM';
+    hour12 = hour12 % 12 || 12;
+
+    // Add AM/PM to the formatted date (Jul-25-2022 4:25:37 PM)
+    formattedDate =
+      monthAbbreviations[monthIndex] +
+      '-' +
+      utcDay +
+      '-' +
+      utcYear +
+      ' ' +
+      hour12 +
+      ':' +
+      utcMinutes +
+      ':' +
+      utcSeconds +
+      ' ' +
+      ampm;
+  }
+
+  return formattedDate;
+}
+
+function getTimeAgoString(timestamp) {
+  const currentUTC = Date.now();
+  const date = new Date(timestamp);
+  const seconds = Math.floor((currentUTC - date.getTime()) / 1000);
+
+  const intervals = {
+    year: seconds / (60 * 60 * 24 * 365),
+    month: seconds / (60 * 60 * 24 * 30),
+    week: seconds / (60 * 60 * 24 * 7),
+    day: seconds / (60 * 60 * 24),
+    hour: seconds / (60 * 60),
+    minute: seconds / 60,
+  };
+
+  if (intervals.year == 1) {
+    return Math.ceil(intervals.year) + ' year ago';
+  } else if (intervals.year > 1) {
+    return Math.ceil(intervals.year) + ' years ago';
+  } else if (intervals.month > 1) {
+    return Math.ceil(intervals.month) + ' months ago';
+  } else if (intervals.week > 1) {
+    return Math.ceil(intervals.week) + ' weeks ago';
+  } else if (intervals.day > 1) {
+    return Math.ceil(intervals.day) + ' days ago';
+  } else if (intervals.hour > 1) {
+    return Math.ceil(intervals.hour) + ' hours ago';
+  } else if (intervals.minute > 1) {
+    return Math.ceil(intervals.minute) + ' minutes ago';
+  } else {
+    return 'a few seconds ago';
+  }
+}
+
+function formatWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function formatTimestampToString(timestamp) {
+  const date = new Date(timestamp);
+
+  // Format the date to 'YYYY-MM-DD HH:mm:ss' format
+  const formattedDate = date.toISOString().replace('T', ' ').split('.')[0];
+
+  return formattedDate;
+}
+
+function convertToMetricPrefix(number) {
+  const prefixes = ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']; // Metric prefixes
+
+  let count = 0;
+  while (Math.abs(number) >= 1000 && count < prefixes.length - 1) {
+    number /= 1000;
+    count++;
+  }
+
+  // Check if the number is close to an integer value
+  if (Math.abs(number) >= 10) {
+    number = Math.round(number); // Round the number to the nearest whole number
+    return number + ' ' + prefixes[count];
+  }
+
+  return (
+    Number(Math.floor(number * 100) / 100).toFixed(2) + ' ' + prefixes[count]
+  );
+}
+function formatNumber(value) {
+  const suffixes = ['', 'K', 'M', 'B', 'T'];
+  let suffixIndex = 0;
+
+  while (value >= 10000 && suffixIndex < suffixes.length - 1) {
+    value /= 1000;
+    suffixIndex++;
+  }
+
+  const formattedValue = value.toFixed(1).replace(/\.0+$/, '');
+  return `${formattedValue} ${suffixes[suffixIndex]}`;
+}
+function gasFee(gas, price) {
+  const near = yoctoToNear(Big(gas).mul(Big(price)).toString(), true);
+
+  return `${near} Ⓝ`;
+}
+
+function currency(number) {
+  let absNumber = Math.abs(number);
+
+  const suffixes = ['', 'K', 'M', 'B', 'T', 'Q'];
+  let suffixIndex = 0;
+
+  while (absNumber >= 1000 && suffixIndex < suffixes.length - 1) {
+    absNumber /= 1000;
+    suffixIndex++;
+  }
+
+  let shortNumber = parseFloat(absNumber.toFixed(2));
+
+  return (number < 0 ? '-' : '') + shortNumber + ' ' + suffixes[suffixIndex];
+}
+
+function formatDate(dateString) {
+  const inputDate = new Date(dateString);
+
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const dayOfWeek = days[inputDate.getDay()];
+  const month = months[inputDate.getMonth()];
+  const day = inputDate.getDate();
+  const year = inputDate.getFullYear();
+
+  const formattedDate = dayOfWeek + ', ' + month + ' ' + day + ', ' + year;
+  return formattedDate;
+}
+
+function formatCustomDate(inputDate) {
+  var date = new Date(inputDate);
+
+  // Array of month names
+  var monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  // Get month and day
+  var month = monthNames[date.getMonth()];
+  var day = date.getDate();
+
+  // Create formatted date string in "MMM DD" format
+  var formattedDate = month + ' ' + (day < 10 ? '0' + day : day);
+
+  return formattedDate;
+}
+
+function shortenHex(address) {
+  return `${address && address.substr(0, 6)}...${address.substr(-4)}`;
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function shortenToken(token) {
+  return truncateString(token, 14, '');
+}
+
+function shortenTokenSymbol(token) {
+  return truncateString(token, 5, '');
+}
+
+function gasPercentage(gasUsed, gasAttached) {
+  if (!gasAttached) return 'N/A';
+
+  const formattedNumber = (Big(gasUsed).div(Big(gasAttached)) * 100).toFixed(2);
+  return `${formattedNumber}%`;
+}
+function serialNumber(index, page, perPage) {
+  return index + 1 + (page - 1) * perPage;
+}
+function truncateString(str, maxLength, suffix) {
+  if (str.length <= maxLength) {
+    return str;
+  }
+  return str.substring(0, maxLength) + suffix;
+}
+function yoctoToNear(yocto, format) {
+  const YOCTO_PER_NEAR = Big(10).pow(24).toString();
+
+  const near = Big(yocto).div(YOCTO_PER_NEAR).toString();
+
+  return format ? localFormat(near) : near;
+}
+function truncateString(str, maxLength, suffix) {
+  if (str.length <= maxLength) {
+    return str;
+  }
+  return str.substring(0, maxLength) + suffix;
+}
+function yoctoToNear(yocto, format) {
+  const YOCTO_PER_NEAR = Big(10).pow(24).toString();
+
+  const near = Big(yocto).div(YOCTO_PER_NEAR).toString();
+
+  return format ? localFormat(near) : near;
+}
+function truncateString(str, maxLength, suffix) {
+  if (str.length <= maxLength) {
+    return str;
+  }
+  return str.substring(0, maxLength) + suffix;
+}
+function yoctoToNear(yocto, format) {
+  const YOCTO_PER_NEAR = Big(10).pow(24).toString();
+
+  const near = Big(yocto).div(YOCTO_PER_NEAR).toString();
+
+  return format ? localFormat(near) : near;
+}
+function serialNumber(index, page, perPage) {
+  return index + 1 + (page - 1) * perPage;
+}
+/* END_INCLUDE: "includes/formats.jsx" */
+/* INCLUDE: "includes/libs.jsx" */
+function getConfig(network) {
+  switch (network) {
+    case 'mainnet':
+      return {
+        ownerId: 'nearblocks.near',
+        nodeUrl: 'https://rpc.mainnet.near.org',
+        backendUrl: 'https://api3.nearblocks.io/v1/',
+        rpcUrl: 'https://archival-rpc.testnet.near.org',
+        appUrl: 'https://nearblocks.io/',
+      };
+    case 'testnet':
+      return {
+        ownerId: 'nearblocks.testnet',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        backendUrl: 'https://api3-testnet.nearblocks.io/v1/',
+        rpcUrl: 'https://archival-rpc.testnet.near.org',
+        appUrl: 'https://testnet.nearblocks.io/',
+      };
+    default:
+      return {};
+  }
+}
+
+function debounce(
+  delay,
+  func,
+) {
+  let timer;
+  let active = true;
+  const debounced = (arg) => {
+    if (active) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        active && func(arg);
+        timer = undefined;
+      }, delay);
+    } else {
+      func(arg);
+    }
+  };
+
+  debounced.isPending = () => {
+    return timer !== undefined;
+  };
+
+  debounced.cancel = () => {
+    active = false;
+  };
+
+  debounced.flush = (arg) => func(arg);
+
+  return debounced;
+}
+
+function timeAgo(unixTimestamp) {
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const secondsAgo = currentTimestamp - unixTimestamp;
+
+  if (secondsAgo < 5) {
+    return 'Just now';
+  } else if (secondsAgo < 60) {
+    return `${secondsAgo} seconds ago`;
+  } else if (secondsAgo < 3600) {
+    const minutesAgo = Math.floor(secondsAgo / 60);
+    return `${minutesAgo} minute${minutesAgo > 1 ? 's' : ''} ago`;
+  } else if (secondsAgo < 86400) {
+    const hoursAgo = Math.floor(secondsAgo / 3600);
+    return `${hoursAgo} hour${hoursAgo > 1 ? 's' : ''} ago`;
+  } else {
+    const daysAgo = Math.floor(secondsAgo / 86400);
+    return `${daysAgo} day${daysAgo > 1 ? 's' : ''} ago`;
+  }
+}
+function shortenAddress(address) {
+  const string = String(address);
+
+  if (string.length <= 20) return string;
+
+  return `${string.substr(0, 10)}...${string.substr(-7)}`;
+}
+
+function urlHostName(url) {
+  try {
+    const domain = new URL(url);
+    return domain?.hostname ?? null;
+  } catch (e) {
+    return null;
+  }
+}
+
+function holderPercentage(supply, quantity) {
+  return Math.min(Big(quantity).div(Big(supply)).mul(Big(100)).toFixed(2), 100);
+}
+function holderPercentage(supply, quantity) {
+  return Math.min(Big(quantity).div(Big(supply)).mul(Big(100)).toFixed(2), 100);
+}
+/* END_INCLUDE: "includes/libs.jsx" */
+
+
+function MainComponent({ network, id, token }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const initialPage = 1;
+  const [currentPage, setCurrentPage] = useState(initialPage);
+  const [totalCount, setTotalCount] = useState(0);
+  const [holder, setHolder] = useState([]);
+  const [tokens, setTokens] = useState({} );
+  const config = getConfig(network);
+  const errorMessage = 'No token holders found!';
+
+  const setPage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  useEffect(() => {
+    setCurrentPage(currentPage);
+  }, [currentPage]);
+
+  useEffect(() => {
+    function fetchNFTData() {
+      setIsLoading(true);
+      asyncFetch(`${config.backendUrl}nfts/${id}`)
+        .then(
+          (data
+
+
+
+
+) => {
+            const resp = data?.body?.contracts?.[0];
+            if (data.status === 200) {
+              setTokens(resp);
+              setIsLoading(false);
+            }
+          },
+        )
+        .catch(() => {});
+    }
+
+    function fetchTotalHolders() {
+      asyncFetch(`${config?.backendUrl}nfts/${id}/holders/count`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(
+          (data
+
+
+
+
+) => {
+            const resp = data?.body?.holders?.[0];
+            if (data.status === 200) {
+              setTotalCount(0);
+              setTotalCount(resp?.count);
+            }
+          },
+        )
+        .catch(() => {})
+        .finally(() => {});
+    }
+
+    function fetchHoldersData() {
+      setIsLoading(true);
+
+      asyncFetch(
+        `${config?.backendUrl}nfts/${id}/holders?page=${currentPage}&per_page=25`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+        .then(
+          (data) => {
+            const resp = data?.body?.holders;
+            if (data.status === 200 && Array.isArray(resp) && resp.length > 0) {
+              setHolder(resp);
+            }
+          },
+        )
+        .catch(() => {})
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+    if (!token && token === undefined) {
+      fetchNFTData();
+    }
+    fetchTotalHolders();
+    fetchHoldersData();
+  }, [config?.backendUrl, currentPage, id, token]);
+
+  useEffect(() => {
+    if (token) {
+      setTokens(token);
+    }
+  }, [token]);
+
+  const Loader = (props) => {
+    return (
+      <div
+        className={`bg-gray-200 h-5 rounded shadow-sm animate-pulse ${props.className} ${props.wrapperClassName}`}
+      ></div>
+    );
+  };
+
+  const columns = [
+    {
+      header: <span>Rank</span>,
+      key: '',
+      cell: (_row, index) => (
+        <span>{serialNumber(index, currentPage, 25)}</span>
+      ),
+      tdClassName:
+        'pl-5 pr-2 py-4 whitespace-nowrap text-sm text-gray-500 w-[50px]',
+      thClassName:
+        'px-5 py-4 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider w-[50px]',
+    },
+    {
+      header: <span> Address</span>,
+      key: 'account',
+      cell: (row) => (
+        <span>
+          <Tooltip.Provider>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <span className="truncate max-w-[200px] inline-block align-bottom text-green-500">
+                  <a
+                    href={`/address/${row.account}`}
+                    className="hover:no-undeline"
+                  >
+                    <a className="text-green-500 font-medium hover:no-undeline">
+                      {row.account}
+                    </a>
+                  </a>
+                </span>
+              </Tooltip.Trigger>
+              <Tooltip.Content
+                className="h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white p-2 break-words"
+                align="start"
+                side="bottom"
+              >
+                {row.account}
+              </Tooltip.Content>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+        </span>
+      ),
+      tdClassName: 'px-5 py-4 whitespace-nowrap text-sm text-gray-500',
+      thClassName:
+        'px-5 py-4 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider',
+    },
+    {
+      header: <span>Quantity</span>,
+      key: 'quantity',
+      cell: (row) => <span>{row.quantity}</span>,
+      tdClassName: 'px-5 py-4 whitespace-nowrap text-sm text-gray-500',
+      thClassName:
+        'px-5 py-4 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider w-[200px]',
+    },
+    {
+      header: <span> Percentage</span>,
+      key: 'tokens',
+      cell: (row) => {
+        const percentage =
+          tokens?.tokens > 0
+            ? holderPercentage(tokens.tokens, row.quantity)
+            : null;
+        return (
+          <span>
+            {percentage === null ? 'N/A' : `${percentage}%`}
+            {percentage !== null && percentage <= 100 && percentage >= 0 && (
+              <div className="h-0.5 mt-1 w-full bg-gray-100">
+                <div
+                  style={{ width: `${percentage}%` }}
+                  className="h-0.5 bg-green-500"
+                />
+              </div>
+            )}
+          </span>
+        );
+      },
+      tdClassName:
+        'px-5 py-3 whitespace-nowrap text-sm text-gray-500 font-medium',
+      thClassName:
+        'px-5 py-4 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider w-[300px] ',
+    },
+  ];
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader
+          className="leading-7"
+          wrapperClassName="pl-3 max-w-sm py-4 h-[60px]"
+        />
+      ) : (
+        <div className={`flex flex-col lg:flex-row pt-4 border-t`}>
+          <div className="flex flex-col">
+            <p className="leading-7 px-6 text-sm mb-4 text-gray-500">
+              A total of {localFormat(totalCount)} transactions found
+            </p>
+          </div>
+        </div>
+      )}
+      <Widget
+        src={`${config.ownerId}/widget/bos-components.components.Shared.Table`}
+        props={{
+          columns: columns,
+          data: holder,
+          isLoading: isLoading,
+          isPagination: true,
+          count: totalCount,
+          page: currentPage,
+          limit: 25,
+          pageLimit: 200,
+          setPage: setPage,
+          Error: errorMessage,
+        }}
+      />
+    </>
+  );
+}
+
+return MainComponent(props, context);
