94760412d661655cc1f202e1d590c636fb182cc3
sdks.near <sdks.near>
2024-01-20 14:25:50 +0000

    Update SDKs.Lens.Requests.PublicationRequests by sdks.near at 2024-01-20T14:25:50.158Z

--- sdks.near/SDKs.Lens.Requests.PublicationRequests/source_code.jsx
+++ sdks.near/SDKs.Lens.Requests.PublicationRequests/source_code.jsx
@@ -1,8 +1,29 @@
 const PUBLICATION_REQUEST = {
-    forId: "",
-    forTxHash: "",
+  forId: "",
+  forTxHash: "",
+};
+
+const PUBLICATIONS_WHERE_REQUEST = {
+  publicationIds: [],
+  from: [],
+  publicationTypes: ["POST"],
+  commentOn: {},
+  mirrorOn: "",
+  quoteOn: "",
+  withOpenActions: [],
+  actedBy: "",
+  metadata: {},
+  customFilters: ["GARDENERS"],
+};
+
+const PUBLICATIONS_REQUEST = {
+  limit: Constants.API_REQUEST_LIMITS.TEN,
+  cursor: "",
+  where: PUBLICATIONS_WHERE_REQUEST,
 };
 
 return {
-    PUBLICATION_REQUEST
+  PUBLICATION_REQUEST,
+  PUBLICATIONS_WHERE_REQUEST,
+  PUBLICATIONS_REQUEST,
 };
