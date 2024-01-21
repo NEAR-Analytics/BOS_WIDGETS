047548a0d263b274ec0757e2854cabc0714de464
sdks.near <sdks.near>
2024-01-21 16:23:09 +0000

    Update Utils.Blockies by sdks.near at 2024-01-21T16:23:09.068Z

---
+++ sdks.near/Utils.Blockies/source_code.jsx
@@ -0,0 +1,16 @@
+const $ = VM.require("sdks.near/widget/Loader");
+const { ExternalDependencyAdapter } = $("@sdks/abstracts");
+
+return (Store, status) => {
+  const Blockies = {
+    ...ExternalDependencyAdapter(Store, status, "blockies"),
+    package: "ethereum-blockies@0.1.1/blockies.js",
+    create: (config) => {
+        return Blockies.request(
+            Blockies.createRequest("create", [config])
+        );
+    }
+  };
+
+  return Blockies;
+};
