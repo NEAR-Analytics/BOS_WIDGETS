b71df853e9ba4e51233058a7e0e6f4fb02662adf
ndcdev.near <ndcdev.near>
2024-01-22 13:55:20 +0000

    Update Dashboard.Pages.Home by ndcdev.near at 2024-01-22T13:55:20.059Z

---
+++ ndcdev.near/Dashboard.Pages.Home/source_code.jsx
@@ -0,0 +1,147 @@
+const { ndcDAOs } = VM.require(`ndcdev.near/widget/Dashboard.Config`);
+const { API } = VM.require(`ndcdev.near/widget/Dashboard.Api.pikespeak`);
+const { Container, ChartContainer } = VM.require(
+  `ndcdev.near/widget/Dashboard.Pages.styled`,
+);
+
+const PERIODS = ["daily", "weekly", "monthly"];
+const defaultDAOOption = "All DAOs";
+
+const dailyTotal = {
+  labels: [],
+  data: [],
+};
+
+const dailyTotalUsers = {
+  labels: [],
+  data: [],
+};
+
+const [loading, setLoading] = useState(false);
+const [totalTx, setTotalTx] = useState(0);
+const [totalAccounts, setTotalAccounts] = useState(0);
+const [uniqueAccounts, setUniqueAccounts] = useState(0);
+const [period, setPeriod] = useState(PERIODS[0]);
+const [selectedDAOs, setSelectedDAOs] = useState([]);
+const [dailyTotalTx, setdailyTotalTx] = useState([]);
+const [uniqueActiveUsers, setUniqueActiveUsers] = useState([]);
+
+const fetchData = () => {
+  setLoading(true);
+  let _totalTx = 0;
+  let _totalAccounts = 0;
+  let _uniqueAccounts = 0;
+  let _uniqueActiveUsers = [];
+  let _dailyTotalTx = [];
+  const daos = selectedDAOs.length ? selectedDAOs : ndcDAOs;
+
+  daos.map((accountId) => {
+    API.get_total_tx(accountId).then((resp) => {
+      _totalTx += parseInt(resp.body);
+      setTotalTx(_totalTx);
+    });
+    API.get_accounts(accountId).then((resp) => {
+      _totalAccounts += resp.body.length;
+      setTotalAccounts(_totalAccounts);
+    });
+    API.get_unique_accounts_by_period(accountId).then((resp) => {
+      _uniqueAccounts += parseInt(resp.body[period].data.length);
+      _uniqueActiveUsers.push(...resp.body[period].data);
+      setUniqueAccounts(_uniqueAccounts);
+      setUniqueActiveUsers(_uniqueActiveUsers);
+    });
+    API.get_daily_total_tx(accountId).then((res) => {
+      _dailyTotalTx.push(...res.body);
+      setdailyTotalTx(_dailyTotalTx);
+    });
+  });
+
+  setLoading(false);
+};
+
+useEffect(() => fetchData(), [selectedDAOs, period]);
+
+dailyTotalTx
+  .sort((a, b) => new Date(a.date) - new Date(b.date))
+  .forEach((element) => {
+    dailyTotal.labels.push(element.date);
+    dailyTotal.data.push(element.count);
+  });
+
+uniqueActiveUsers
+  .sort((a, b) => new Date(a.day) - new Date(b.day))
+  .forEach((element) => {
+    dailyTotalUsers.labels.push(element.day);
+    dailyTotalUsers.data.push(element.unique_users);
+  });
+
+return (
+  <Container>
+    <div className="section">
+      <div className="d-flex w-100 gap-3 justify-content-between">
+        <div className="select-dao">
+          <Widget
+            src={`ndcdev.near/widget/Dashboard.Components.Select.index`}
+            props={{
+              options: ndcDAOs,
+              defaultValue: defaultDAOOption,
+              isOpen: selectOpen,
+              multiple: true,
+              values: selectedDAOs,
+              containerClass: "selected-container",
+              onClear: () => setSelectedDAOs([]),
+              onChange: (value) => {
+                setSelectedDAOs(
+                  value === defaultDAOOption
+                    ? []
+                    : selectedDAOs.includes(value)
+                    ? selectedDAOs.filter(
+                        (dao) => dao !== value && dao !== defaultDAOOption,
+                      )
+                    : [...selectedDAOs, value],
+                );
+              },
+            }}
+          />
+        </div>
+        <div className="select-period">
+          <Widget
+            src={`ndcdev.near/widget/Dashboard.Components.Select.index`}
+            props={{
+              options: PERIODS,
+              isOpen: selectOpen,
+              values: period,
+              onChange: setPeriod,
+              containerClass: "selected-container",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+    <Widget
+      src={`ndcdev.near/widget/Dashboard.Components.MetricsDisplay.index`}
+      props={{
+        totalTx,
+        totalAccounts,
+        uniqueAccounts,
+        loading,
+      }}
+    />
+    <ChartContainer>
+      <Widget
+        src={`ndcdev.near/widget/Dashboard.Components.Chart.index`}
+        props={{ title: "DAILY NUMBER OF TRANSACTIONS", data: dailyTotal }}
+      />
+      <Widget
+        src={`ndcdev.near/widget/Dashboard.Components.Chart.index`}
+        props={{ title: "UNIQUE ACTIVE USERS", data: dailyTotalUsers }}
+      />
+    </ChartContainer>
+    <div className="section py-5">
+      <Widget
+        src={`ndcdev.near/widget/Dashboard.Components.Table.index`}
+        props={{ ndcDAOs }}
+      />
+    </div>
+  </Container>
+);
