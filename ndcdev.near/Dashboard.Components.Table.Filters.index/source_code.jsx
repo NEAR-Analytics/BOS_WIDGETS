373dbe67faf25558b2a1df4f0200ba86f6f053bf
ndcdev.near <ndcdev.near>
2024-01-22 14:47:31 +0000

    Update Dashboard.Components.Table.Filters.index by ndcdev.near at 2024-01-22T14:47:31.267Z

--- ndcdev.near/Dashboard.Components.Table.Filters.index/source_code.jsx
+++ ndcdev.near/Dashboard.Components.Table.Filters.index/source_code.jsx
@@ -2,6 +2,9 @@ const { FilterItem, SubFilterItem, FilterContainer } = VM.require(
   `ndcdev.near/widget/Dashboard.Components.Table.Filters.styled`,
 );
 
+if (!FilterItem || !SubFilterItem || !FilterContainer)
+  <Widget src="flashui.near/widget/Loading" />;
+
 const { ndcDAOs } = props;
 
 const defaultDAOOption = "All";
