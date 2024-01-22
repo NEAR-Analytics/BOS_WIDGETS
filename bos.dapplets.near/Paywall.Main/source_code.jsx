0363c39de080932393e6066504401870f1a42639
bos.dapplets.near <bos.dapplets.near>
2024-01-22 17:14:08 +0000

    Update Paywall.Main by bos.dapplets.near at 2024-01-22T17:14:08.560Z

--- bos.dapplets.near/Paywall.Main/source_code.jsx
+++ bos.dapplets.near/Paywall.Main/source_code.jsx
@@ -4,32 +4,36 @@ const { authorId, id } = props.link;
 if (!id) return <></>;
 
 const userAccountId = context.accountId || nearAccountId
-
+const MOCKED_ADDRESS = "ncownthflapqowieuryt.near" // ToDo: one needs account address to get content for context
 const CONTRACT_ADDRESS = "v2.paywall.near";
 
-const setContent = () => {
-  const content = userAccountId
-    ? Near.view(
-        CONTRACT_ADDRESS,
-        "get_content_by_post_for_account",
-        {
-          context_id: id,
-          account_id: userAccountId
-        }
-      )
-    : []
-  console.log(content[0])
-  State.update({
-    content: content[0]
-  })
-}
-
-setContent()
+State.update({
+  content: Near.view(
+      CONTRACT_ADDRESS,
+      "get_content_by_post_for_account",
+      {
+        context_id: id,
+        account_id: userAccountId || MOCKED_ADDRESS
+      }
+    )[0]
+})
 
 return (
   <>
     {state.content
-      ? <Widget src="bos.dapplets.near/widget/Paywall.Content" props={{ content: state.content, CONTRACT_ADDRESS, nearAccountId }}/>
-      : <Widget src="bos.dapplets.near/widget/Paywall.Form" props={{ linkId: id, CONTRACT_ADDRESS }}/>}
+      ? <Widget
+          src="bos.dapplets.near/widget/Paywall.Content"
+          props={{
+            content: state.content,
+            CONTRACT_ADDRESS,
+            nearAccountId,
+          }
+        }/>
+      : authorId === userAccountId
+      ? <Widget
+          src="bos.dapplets.near/widget/Paywall.Form"
+          props={{ linkId: id, CONTRACT_ADDRESS }}
+        />
+      : <></>}
   </>
 );
