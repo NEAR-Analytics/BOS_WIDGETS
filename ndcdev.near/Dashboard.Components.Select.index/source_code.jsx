1a56258bca17316f6e8e9c39ff7b51c09e12def6
ndcdev.near <ndcdev.near>
2024-01-22 13:55:20 +0000

    Update Dashboard.Components.Select.index by ndcdev.near at 2024-01-22T13:55:20.059Z

---
+++ ndcdev.near/Dashboard.Components.Select.index/source_code.jsx
@@ -0,0 +1,56 @@
+const { Select, Check } = VM.require(
+  `ndcdev.near/widget/Dashboard.Components.Select.styled`,
+);
+const {
+  values,
+  multiple,
+  options,
+  onChange,
+  defaultValue,
+  isOpen,
+  onClear,
+  containerClass,
+} = props;
+const [open, setOpen] = useState(isOpen);
+const selectOptions = defaultValue ? [defaultValue, ...options] : options;
+
+const title = () => {
+  if (Array.isArray(values)) {
+    return values.length ? `${values.length} Selected` : defaultValue;
+  } else {
+    return values;
+  }
+};
+
+return (
+  <Select onClick={() => setOpen(!open)}>
+    <div className={containerClass}>
+      <div className="selected">{title()}</div>
+      <div className="d-flex gap-2">
+        {multiple && values.length > 0 && (
+          <i
+            className="bi bi-x fs-5 mt-1 mr-1"
+            onClick={() => onClear() && setOpen(false)}
+          />
+        )}
+        <i className="bi bi-chevron-down fs-5 mt-1" />
+      </div>
+    </div>
+    {open && (
+      <ul>
+        {selectOptions.map((option) => (
+          <li onClick={() => onChange(option)}>
+            {multiple && (
+              <Check selected={values.includes(option)}>
+                {values.includes(option) && (
+                  <i className="mt-1 bi bi-check-lg" />
+                )}
+              </Check>
+            )}
+            <div className="truncate">{option}</div>
+          </li>
+        ))}
+      </ul>
+    )}
+  </Select>
+);
