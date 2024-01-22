e09c103208fe77c2196bdd49166bf0cba5ceb6f4
ndcdev.near <ndcdev.near>
2024-01-22 13:55:20 +0000

    Update Dashboard.Components.Table.Filters.index by ndcdev.near at 2024-01-22T13:55:20.059Z

---
+++ ndcdev.near/Dashboard.Components.Table.Filters.index/source_code.jsx
@@ -0,0 +1,87 @@
+const { FilterItem, SubFilterItem, FilterContainer } = VM.require(
+  `ndcdev.near/widget/Dashboard.Components.Table.Filters.styled`,
+);
+
+const { ndcDAOs } = props;
+
+const defaultDAOOption = "All";
+const PERIODS = ["daily", "weekly", "monthly"];
+const CURRENCIES = ["near", "stable"];
+const FILTERS = [
+  {
+    text: "DAO",
+    hintText: "NDC grassroots DAOs",
+    options: [defaultDAOOption, ...ndcDAOs],
+  },
+  {
+    text: "User Retetntion",
+    hintText: "Text TBD",
+    options: PERIODS,
+  },
+  {
+    text: "DAP's Used",
+    hintText: "Text TBD",
+    options: PERIODS,
+  },
+  {
+    text: "Aquisition Cost",
+    hintText: "Text TBD",
+    options: CURRENCIES,
+  },
+];
+
+const [loading, setLoading] = useState(false);
+const [selectedDAOs, setSelectedDAOs] = useState([]);
+const [filter, setFilter] = useState(
+  FILTERS.map((filter) => filter.options[0]),
+);
+const [results, setResults] = useState(
+  ndcDAOs.map((daoId) => {
+    return { [daoId]: FILTERS.map((f) => "-") };
+  }),
+);
+
+const fetchData = () => {
+  setLoading(true);
+  const daos = selectedDAOs.length ? selectedDAOs : ndcDAOs;
+
+  daos.map((daoId) => {
+    setResults[daoId] = [712, 4, 5];
+  });
+  setLoading(false);
+};
+
+useEffect(() => fetchData(), [selectedDAOs, filter]);
+
+return (
+  <FilterContainer>
+    {FILTERS.map(({ text, hintText, options, value }, i) => (
+      <div className="d-flex flex-column gap-1">
+        <FilterItem>
+          <Widget
+            src={`ndcdev.near/widget/Dashboard.Components.Select.index`}
+            props={{
+              containerClass: "selected-container black",
+              options,
+              values: (
+                <div className="d-flex align-items-center gap-2">
+                  {text}
+                  <i className="bi bi-info-circle" label={hintText} />
+                </div>
+              ),
+              onChange: (value) => {
+                let filterOptions = filter;
+                filterOptions[i] = value;
+                setFilter(filterOptions);
+              },
+            }}
+          />
+        </FilterItem>
+
+        <SubFilterItem>
+          <div>{filter[i]}</div>
+        </SubFilterItem>
+      </div>
+    ))}
+  </FilterContainer>
+);
