0f096cfbbf1b156777a8378088ac241ace173f99
ndcdev.near <ndcdev.near>
2024-01-22 13:55:20 +0000

    Update Dashboard.Layouts.App by ndcdev.near at 2024-01-22T13:55:20.059Z

---
+++ ndcdev.near/Dashboard.Layouts.App/source_code.jsx
@@ -0,0 +1,21 @@
+const { page, children } = props;
+const { Wrapper, Container, Theme } = VM.require(
+  `ndcdev.near/widget/Dashboard.Layouts.styled`,
+);
+
+const Loading = () => <Widget src="flashui.near/widget/Loading" />;
+
+const [font, setFont] = useState("");
+
+asyncFetch("https://fonts.cdnfonts.com/css/avenir-lt-pro").then((resp) =>
+  setFont(resp.body),
+);
+
+return (
+  <Theme font={font}>
+    <Container>
+      <Widget src={`ndcdev.near/widget/Dashboard.Components.NavBar.index`} />
+      {children}
+    </Container>
+  </Theme>
+);
