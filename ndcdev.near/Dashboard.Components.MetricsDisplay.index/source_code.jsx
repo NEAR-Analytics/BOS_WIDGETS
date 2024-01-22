3433418f22575f404c60246dfd1bf6516ed9d1d1
ndcdev.near <ndcdev.near>
2024-01-22 13:55:20 +0000

    Update Dashboard.Components.MetricsDisplay.index by ndcdev.near at 2024-01-22T13:55:20.059Z

---
+++ ndcdev.near/Dashboard.Components.MetricsDisplay.index/source_code.jsx
@@ -0,0 +1,34 @@
+const { totalTx, totalAccounts, uniqueAccounts, loading } = props;
+const { Items } = VM.require(
+  `ndcdev.near/widget/Dashboard.Components.MetricsDisplay.styled`,
+);
+const Loading = () => <Widget src="flashui.near/widget/Loading" />;
+
+const Item = ({ value, text, color }) => {
+  return (
+    <Widget
+      src={`ndcdev.near/widget/Dashboard.Components.MetricsDisplay.Item`}
+      props={{ value, text, color }}
+    />
+  );
+};
+
+return (
+  <Items>
+    <Item
+      value={loading ? <Loading /> : totalTx}
+      text="Total Number of Transactions"
+      color="#A39ACD"
+    />
+    <Item
+      value={loading ? <Loading /> : totalAccounts}
+      text="Total Number of Accounts"
+      color="#5398DD"
+    />
+    <Item
+      value={loading ? <Loading /> : uniqueAccounts}
+      text="Today Unique Active Users"
+      color="#E89DBB"
+    />
+  </Items>
+);
