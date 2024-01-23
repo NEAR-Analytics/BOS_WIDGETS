0df955c9213556994c03a75a564e29e497805624
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2024-01-23 17:15:32 +0000

    Update Profile.ShortInlineBlock by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2024-01-23T17:15:32.751Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Profile.ShortInlineBlock/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/Profile.ShortInlineBlock/source_code.jsx
@@ -13,7 +13,7 @@ const maxWidth = props.maxWidth ?? "60%";
 const inner = (
   <div className="d-flex flex-row" style={{ maxWidth: "100%" }}>
     <Widget
-      src={widgets.profileImage}
+      src={widgets.views.standardWidgets.profileImage}
       props={{
         metadata,
         accountId,
