61c3d8bfb367e99b7a65b8193d608a8946eb1465
f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb <f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb>
2024-01-23 20:24:22 +0000

    Update NDC.ArticleView by f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb at 2024-01-23T20:24:22.178Z

--- f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.ArticleView/source_code.jsx
+++ f2bc8abdb8ba64fe5aac9689ded9491ff0e6fdcd7a5c680b7cf364142d1789fb/NDC.ArticleView/source_code.jsx
@@ -547,7 +547,7 @@ return (
       <NoMargin className="accordion-item">
         <h2 className="accordion-header" id="headingOne">
           <button
-            className="accordion-button collapsed"
+            className="accordion-button collapsed border border-2"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#collapseOne"
