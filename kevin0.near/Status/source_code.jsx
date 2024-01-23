933352b503e8d52795c53b0a57e7126e9e23c6e1
kevin0.near <kevin0.near>
2024-01-23 23:27:48 +0000

    Update Status by kevin0.near at 2024-01-23T23:27:48.255Z

---
+++ kevin0.near/Status/source_code.jsx
@@ -0,0 +1,42 @@
+// Status
+const appName = "techconfluence";
+let accountId = context.accountId;
+
+if (!accountId) {
+  return "Please sign in with NEAR wallet to set status";
+}
+
+State.init({ memo: null });
+
+Social.get(appName, )
+
+return (
+  <div>
+    <div className="mb-3">
+      <label for="exampleFormControlTextarea1" className="form-label">
+        What's happening?
+      </label>
+      <textarea
+        className="form-control"
+        id="exampleFormControlTextarea1"
+        rows="2"
+        style={{ maxWidth: "30em" }}
+        onChange={(event) => State.update({ memo: event.target.value })}
+      ></textarea>
+    </div>
+
+    <div>{memo}
+    <div>
+      <CommitButton
+        disabled={state.memo === null}
+        data={{
+          [appName]: {
+            [accountId]: state.memo,
+          },
+        }}
+      >
+        Submit
+      </CommitButton>
+    </div>
+  </div>
+);
