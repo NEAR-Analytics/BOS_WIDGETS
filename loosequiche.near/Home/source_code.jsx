8b253436c8a391463ffad9e8a66658f63425d34d
loosequiche.near <loosequiche.near>
2024-01-24 14:57:51 +0000

    Update Home by loosequiche.near at 2024-01-24T14:57:51.208Z

---
+++ loosequiche.near/Home/source_code.jsx
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <div>
+      <img
+        src="https://res.cloudinary.com/dglhc1pfj/image/upload/f_auto,q_auto/NearIndia"
+        alt="Near India"
+        height="100%"
+        width="100%"
+      />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+return <App />;
