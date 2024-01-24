d696275ffa60814e830c28fc3da96871d2776210
megha19.near <megha19.near>
2024-01-24 17:32:38 +0000

    Update Join-CTA by megha19.near at 2024-01-24T17:32:38.701Z

--- megha19.near/Join-CTA/source_code.jsx
+++ megha19.near/Join-CTA/source_code.jsx
@@ -1,8 +1,38 @@
-const { checkIsMemberOrPending } = VM.require(
-  "buildhub.near/widget/core.lib.common"
-);
+// checks if the user is already dao's community/council member or has an active proposal
+const checkIsMemberOrPending = (accountId) => {
+  const daoId = "build.sputnik-dao.near";
+  if (!accountId) {
+    return false;
+  }
+  const alreadyJoinedRolesNames = ["community", "council"];
+  const searchRange = 100;
 
-checkIsMemberOrPending || (checkIsMemberOrPending = () => {});
+  const lastProposalId = Near.view(daoId, "get_last_proposal_id");
+
+  const policy = Near.view(daoId, "get_policy");
+  const isDaoMember = false;
+  const lastProposals =
+    Near.view(daoId, "get_proposals", {
+      from_index: lastProposalId - searchRange,
+      limit: searchRange,
+    }) || [];
+  console.log(policy, lastProposals, lastProposalId);
+  const alreadyMadeAProposal =
+    lastProposals.filter((proposal) => {
+      return (
+        proposal.proposer === accountId && proposal.status === "InProgress"
+      );
+    }).length > 0;
+
+  policy.roles
+    .filter((role) => alreadyJoinedRolesNames.includes(role.name))
+    .map((role) => {
+      if (Array.isArray(role.kind.Group) && !isDaoMember) {
+        isDaoMember = role.kind.Group.includes(accountId);
+      }
+    });
+  return isDaoMember || alreadyMadeAProposal;
+};
 
 const isMemberOrPending = checkIsMemberOrPending(context.accountId);
 console.log(isMemberOrPending, context.accountId, checkIsMemberOrPending);
