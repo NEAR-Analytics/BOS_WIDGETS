83979ae68c5c1a9c22c46f7849cda16ed8dda929
flowscience.near <flowscience.near>
2024-01-25 20:55:08 +0000

    Update ProfileEditor.Validate by flowscience.near at 2024-01-25T20:55:08.188Z

---
+++ flowscience.near/ProfileEditor.Validate/source_code.jsx
@@ -0,0 +1,116 @@
+const accountId = context.accountId;
+
+if (!accountId) {
+  return "Please sign in with NEAR wallet to edit your profile";
+}
+
+let profile = Social.getr(`${accountId}/profile`);
+
+if (profile === null) {
+  return "Loading";
+}
+
+State.init({
+  profile,
+});
+
+const [isFormValid, setIsFormValid] = useState(false);
+
+const validateProfile = (profile) => {
+  const requiredFields = [
+    "name",
+    "image",
+    "backgroundImage",
+    "description",
+    "tags",
+  ];
+
+  const linktreeFilled = profile.linktree.links.some(
+    (link) => link.name && link.name.trim() !== ""
+  );
+
+  const isValid =
+    requiredFields.every(
+      (field) => profile[field] && profile[field].trim() !== ""
+    ) && linktreeFilled;
+
+  setIsFormValid(isValid);
+};
+
+useEffect(() => {
+  validateProfile(state.profile);
+}, [state.profile]);
+
+return (
+  <div className="row">
+    <div className="col-lg-6">
+      <div>
+        <h4>Edit profile of @{accountId}</h4>
+      </div>
+      <div className="mb-2">
+        <Widget
+          src="near/widget/MetadataEditor"
+          props={{
+            initialMetadata: profile,
+            onChange: (profile) => State.update({ profile }),
+            options: {
+              name: { label: "Name" },
+              image: { label: "Profile picture" },
+              backgroundImage: { label: "Background image" },
+              description: { label: "About" },
+              tags: {
+                label: "Tags",
+                tagsPattern: "*/profile/tags/*",
+                placeholder:
+                  "rust, engineer, artist, humanguild, nft, learner, founder",
+              },
+              linktree: {
+                links: [
+                  {
+                    label: "Twitter",
+                    prefix: "https://twitter.com/",
+                    name: "twitter",
+                  },
+                  {
+                    label: "Github",
+                    prefix: "https://github.com/",
+                    name: "github",
+                  },
+                  {
+                    label: "Telegram",
+                    prefix: "https://t.me/",
+                    name: "telegram",
+                  },
+                  {
+                    label: "Website",
+                    prefix: "https://",
+                    name: "website",
+                  },
+                ],
+              },
+            },
+          }}
+        />
+      </div>
+      <div className="mb-2">
+        <CommitButton data={{ profile: state.profile }} disabled={!isFormValid}>
+          Save profile
+        </CommitButton>
+        <a
+          className="btn btn-outline-primary ms-2"
+          href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
+        >
+          View profile
+        </a>
+      </div>
+    </div>
+    <div className="col-lg-6">
+      <div>
+        <Widget
+          src="mob.near/widget/ProfilePage"
+          props={{ accountId, profile: state.profile }}
+        />
+      </div>
+    </div>
+  </div>
+);
