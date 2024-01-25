e051f707e3d13a0cae02aa9b7fe2f747849b02d8
rodrigos.near <rodrigos.near>
2024-01-25 01:45:44 +0000

    Update Governance.Comment.Index by rodrigos.near at 2024-01-25T01:45:44.688Z

---
+++ rodrigos.near/Governance.Comment.Index/source_code.jsx
@@ -0,0 +1,42 @@
+const addressForComments = "mpipComment";
+const addressForProposals = "mpip";
+const authorId = "rodrigos.near";
+const mpip_id = props.mpip_id;
+const item = props.item;
+State.init({ showReply: false });
+const accountId = props.accountId;
+const blockHeight =
+  props.blockHeight === "now" ? "now" : parseInt(props.blockHeight);
+const content =
+  props.content ??
+  JSON.parse(
+    Social.get(
+      `${accountId}/${addressForProposals}/${addressForComments}`,
+      blockHeight
+    ) ?? "null"
+  );
+const highlight = !!props.highlight;
+const raw = !!props.raw;
+
+const link = `#/mob.near/widget/MainPage.Comment.Page?accountId=${accountId}&blockHeight=${blockHeight}`;
+
+return (
+  <>
+    <div
+      className={`pt-3 border-top pb-2 ${
+        highlight ? "bg-warning bg-opacity-10" : ""
+      }`}
+    >
+      <Widget
+        src="mob.near/widget/MainPage.Post.Header"
+        props={{ accountId, blockHeight, link, postType: "comment" }}
+      />
+      <div className="mt-2 text-break">
+        <Widget
+          src="mob.near/widget/MainPage.Post.Content"
+          props={{ content, raw }}
+        />
+      </div>
+    </div>
+  </>
+);
