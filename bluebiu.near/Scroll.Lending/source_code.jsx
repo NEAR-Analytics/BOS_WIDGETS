4e0b0764a9369afd71316a04efd213cdc66692fd
bluebiu.near <bluebiu.near>
2024-01-25 08:47:57 +0000

    Update Scroll.Lending by bluebiu.near at 2024-01-25T08:47:57.144Z

---
+++ bluebiu.near/Scroll.Lending/source_code.jsx
@@ -0,0 +1,29 @@
+const Theme = styled.div`
+  --button-text-color: #0f1126;
+  --button-color: #00ffe0;
+  --primary-color: #13a69d;
+  --border-color: #292c42;
+  --supply-bg-color: rgba(39, 197, 187, 0.2);
+  --borrow-bg-color: rgba(202, 85, 85, 0.2);
+  --supply-color: #62fff6;
+  --borrow-color: #ff6767;
+  --withdraw-bg-color: rgba(19, 166, 157, 0.2);
+  --withdraw-bg-hover-color: rgba(19, 166, 157, 1);
+  --withdraw-border-color: rgba(19, 166, 157, 1);
+  --repay-bg-color: rgba(217, 71, 124, 0.2);
+  --repay-bg-hover-color: rgba(217, 71, 124, 1);
+  --repay-border-color: rgba(217, 71, 124, 1);
+  --switch-color: #5baea9;
+  --switch-border-color: #32496a;
+  --secondary-border-color: #3f577b;
+  --yours-table-title: #ffffff;
+`;
+
+return (
+  <Theme>
+    <Widget
+      src="bluebiu.near/widget/Avalanche.Lending.Collection"
+      props={props}
+    />
+  </Theme>
+);
