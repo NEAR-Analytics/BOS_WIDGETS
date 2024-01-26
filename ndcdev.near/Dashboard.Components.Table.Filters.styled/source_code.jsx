bff29f3cbf28ba3bed38bf305e0685e180bc146b
ndcdev.near <ndcdev.near>
2024-01-26 16:35:30 +0000

    Update Dashboard.Components.Table.Filters.styled by ndcdev.near at 2024-01-26T16:35:30.770Z

--- ndcdev.near/Dashboard.Components.Table.Filters.styled/source_code.jsx
+++ ndcdev.near/Dashboard.Components.Table.Filters.styled/source_code.jsx
@@ -1,44 +1,4 @@
 return {
-  FilterContainer: styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    gap: 1rem;
-    width: 100%;
-    & > div {
-      width: 100%;
-    }
-
-    @media screen and (max-width: 1400px) {
-      flex-wrap: wrap;
-      & > div {
-        width: 48%;
-      }
-    }
-
-    @media screen and (max-width: 868px) {
-      flex-wrap: wrap;
-      & > div {
-        width: 100%;
-      }
-    }
-  `,
-
-  FilterItem: styled.div`
-    border-radius: 10px;
-    background: #1e1d22;
-    padding: 5px 10px;
-    color: #fff;
-    text-align: center;
-    font-size: 24px;
-    font-weight: 350;
-    width: 100%;
-
-    @media screen and (max-width: 868px) {
-      width: 100%;
-    }
-  `,
-
   SubFilterItem: styled.div`
     border-radius: 10px;
     background: #a39acd;
@@ -46,22 +6,14 @@ return {
     display: flex;
     align-items: center;
     justify-content: center;
-    font-size: 24px;
+    font-size: 20px;
     font-style: normal;
     font-weight: 350;
     width: 100%;
+    min-width: 270px;
+    width: 100%;
     height: 50px;
 
-    div {
-      text-align: center;
-      text-overflow: ellipsis;
-      text-wrap: nowrap;
-      overflow: hidden;
-      width: 200px;
-    }
-
-    @media screen and (max-width: 868px) {
-      width: 100%;
-    }
+    text-align: center;
   `,
 };
