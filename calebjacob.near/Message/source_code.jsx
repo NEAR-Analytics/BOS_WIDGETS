78d079aba4bcd9383fd7b25b6d0707f345d8f864
calebjacob.near <calebjacob.near>
2024-01-26 21:12:12 +0000

    Update Message by calebjacob.near at 2024-01-26T21:12:12.957Z

---
+++ calebjacob.near/Message/source_code.jsx
@@ -0,0 +1,13 @@
+interface Props {
+  message: string;
+}
+
+export function BWEComponent(props: Props) {
+  return (
+    <div>
+      <h2>BOS Says:::</h2>
+      <p>{props.message}</p>
+      
+    </div>
+  );
+}         
