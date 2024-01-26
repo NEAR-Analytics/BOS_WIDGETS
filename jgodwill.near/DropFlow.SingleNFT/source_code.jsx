2beaff66734d0390d754830c3b81a71a446d9d4c
jgodwill.near <jgodwill.near>
2024-01-26 10:31:23 +0000

    Update DropFlow.SingleNFT by jgodwill.near at 2024-01-26T10:31:23.182Z

--- jgodwill.near/DropFlow.SingleNFT/source_code.jsx
+++ jgodwill.near/DropFlow.SingleNFT/source_code.jsx
@@ -583,7 +583,7 @@ return (
               }
               alt="NFT"
               width="100%"
-              height="100%"
+              height="400px"
             />
             <div
               style={{
