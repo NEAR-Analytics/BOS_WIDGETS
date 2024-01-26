1485d20eeffcdd3bb7e035302761f331a0fe22ab
ndcdev.near <ndcdev.near>
2024-01-26 16:35:30 +0000

    Update Dashboard.Config by ndcdev.near at 2024-01-26T16:35:30.770Z

--- ndcdev.near/dashboard.Config/source_code.jsx
+++ ndcdev.near/dashboard.Config/source_code.jsx
@@ -3,9 +3,24 @@ return {
     "congress-hom-v1.ndc-gwg.near",
     "congress-coa-v1.ndc-gwg.near",
     "congress-tc-v1.ndc-gwg.near",
+    // "gaming-dao.sputnik-dao.near",
+    // "marketing.sputnik-dao.near",
+    // "nearglobedao.sputnik-dao.near",
+    // "ndc-degens.sputnik-dao.near",
+    // "ac_dao.near",
+    // "onboard.sputnik-dao.near",
+    // "she-is-near.sputnik-dao.near",
   ],
   assets: {
     logoWhite:
       "https://ipfs.near.social/ipfs/bafkreigrpxkfqynsjxurjpewxweytewedshhrvuk3ndrkf34j5vxevqs2i",
+    notFoundLogo:
+      "https://ipfs.near.social/ipfs/bafkreiddgi5r66565ykxunbvilry7i33ykmatfsghbsjhpfockytgu5kmm",
+    nearLogo:
+      "https://ipfs.near.social/ipfs/bafkreicp7kaukoq3xw6fks7o4dgzpx4mv4cueljd4kyiw7pz2kdwyszs6u",
+    twitterLogo:
+      "https://ipfs.near.social/ipfs/bafkreidrf2na3ubbte3voup6jtbauxmxdfqsu53ffnqyk7jvt36whxbdo4",
+    telegramLogo:
+      "https://ipfs.near.social/ipfs/bafkreieoji43nz537vztjmphndnfwftj3a7m7nbdhcb7hypkssboxl57xa",
   },
 };
