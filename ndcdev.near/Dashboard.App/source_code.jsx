ad2d2b63b4d8f6717d79ef6b786d1ce8d7541de5
ndcdev.near <ndcdev.near>
2024-01-26 16:35:30 +0000

    Update Dashboard.App by ndcdev.near at 2024-01-26T16:35:30.770Z

--- ndcdev.near/Dashboard.App/source_code.jsx
+++ ndcdev.near/Dashboard.App/source_code.jsx
@@ -13,26 +13,11 @@ function Page() {
     }
 
     default: {
-      const NotFound = styled.div`
-        width: 100%;
-        height: 70vh;
-
-        h1 {
-          font-size: 10rem;
-        }
-
-        h2 {
-          font-size: 2rem;
-        }
-      `;
-
       return (
-        <NotFound className="d-flex flex-grow-1 flex-column justify-content-center align-items-center">
-          <h1>
-            <b>404</b>
-          </h1>
-          <h2>Page does not exist</h2>
-        </NotFound>
+        <Widget
+          src={`ndcdev.near/widget/Dashboard.Pages.NotFound.index`}
+          props={passProps}
+        />
       );
     }
   }
