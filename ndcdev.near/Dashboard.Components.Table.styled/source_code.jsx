a0e54c0809f8634f1ce20598ec6f5cad12980bb5
ndcdev.near <ndcdev.near>
2024-01-26 16:35:30 +0000

    Update Dashboard.Components.Table.styled by ndcdev.near at 2024-01-26T16:35:30.770Z

--- ndcdev.near/Dashboard.Components.Table.styled/source_code.jsx
+++ ndcdev.near/Dashboard.Components.Table.styled/source_code.jsx
@@ -1,3 +1,11 @@
 return {
-  Row: styled.div``,
+  ScrollableWrapper: styled.div`
+    width: 100%;
+    min-height: 15rem;
+    @media screen and (max-width: 768px) {
+      overflow-y: hidden;
+      overflow-x: scroll;
+    }
+    -webkit-overflow-scrolling: touch;
+  `,
 };
