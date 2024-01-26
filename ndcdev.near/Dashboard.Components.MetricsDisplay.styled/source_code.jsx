02a008492262dc2ba0539da653d09c8176e2a9d8
ndcdev.near <ndcdev.near>
2024-01-26 16:35:30 +0000

    Update Dashboard.Components.MetricsDisplay.styled by ndcdev.near at 2024-01-26T16:35:30.770Z

--- ndcdev.near/Dashboard.Components.MetricsDisplay.styled/source_code.jsx
+++ ndcdev.near/Dashboard.Components.MetricsDisplay.styled/source_code.jsx
@@ -43,7 +43,9 @@ return {
         border-radius: 20px;
         background: rgba(255, 255, 255, 0.1);
         padding: 10px 20px;
-        font-size: 60px;
+        span {
+          font-size: 60px;
+        }
       }
     }
   `,
