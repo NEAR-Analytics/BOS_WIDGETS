caee1a520e0bc2a19fa7c79fd3e08d82a1cbaa5b
ndcdev.near <ndcdev.near>
2024-01-26 16:35:30 +0000

    Update Dashboard.Pages.NotFound.index by ndcdev.near at 2024-01-26T16:35:30.770Z

---
+++ ndcdev.near/Dashboard.Pages.NotFound.index/source_code.jsx
@@ -0,0 +1,31 @@
+
+const { assets } = VM.require(`ndcdev.near/widget/Dashboard.Config`);
+const { Text, Logo, Social } = VM.require(
+  `ndcdev.near/widget/Dashboard.Pages.NotFound.styled`,
+);
+
+if (!Text || !Logo || !Social || !assets)
+  return <Widget src="flashui.near/widget/Loading" />;
+
+return (
+  <div className="d-flex flex-grow-1 flex-column justify-content-center align-items-center">
+    <Text>
+      Oops! Looks like we lost this page in the virtual cosmos. Don't worry! Go back to the main page and try again. If this happens again, let us know, and we'll send a search robot with a flashlight!
+    </Text>
+    <Logo src={assets.notFoundLogo} />
+    <Text>
+      Join to our social
+    </Text>
+    <Social>
+      <a href="https://near.org" target="_blank">
+        <img src={assets.nearLogo} />
+      </a>
+      <a href="https://twitter.com/nearprotocol" target="_blank">
+        <img src={assets.twitterLogo} />
+      </a>
+      <a href="https://t.me/cryptonear" target="_blank">
+        <img src={assets.telegramLogo} />
+      </a>
+    </Social>
+  </div>
+);
