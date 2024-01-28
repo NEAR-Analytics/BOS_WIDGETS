9e410923b3d31a056c862ea408484ba67bcdca31
kumarkrsronit.near <kumarkrsronit.near>
2024-01-28 15:21:16 +0000

    Update Famlinkup_homepage by kumarkrsronit.near at 2024-01-28T15:21:16.450Z

--- kumarkrsronit.near/Famlinkup_homepage/source_code.jsx
+++ kumarkrsronit.near/Famlinkup_homepage/source_code.jsx
@@ -5,6 +5,19 @@ const styles = {
     margin: "0",
     padding: "0",
   },
+
+  button: {
+    display: "inline-block",
+    width: "120px",
+    margin: "10px 20px",
+    padding: "10px",
+    background: "#FF7722",
+    color: "white",
+    fontWeight: "bold",
+    textDecoration: "none",
+    borderRadius: "5px",
+    textAlign: "center",
+  },
 };
 
 const containerStyle = {
@@ -39,6 +52,45 @@ function Banner() {
         together and share their feeling with each other. They can connect ,
         text , post their thoughts and join the irl events.
       </p>
+      <br /> <br />
+      <p>Explore our web2 site till we are shifting it to near.org :</p>
+      <a
+        href="https://socialbook-abhay2131.onrender.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={styles.button}
+      >
+        FamLinkUp
+      </a>
+      &nbsp;&nbsp;
+      <a
+        href="https://socialbook-abhay2131.onrender.com/mainsite"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={styles.button}
+      >
+        Mainsite
+      </a>
+      &nbsp;&nbsp;
+      <a
+        href="https://github.com/abhay2131/socialbook"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={styles.button}
+      >
+        Github
+      </a>
+      &nbsp;&nbsp;
+      <a
+        href="https://twitter.com/kumarkrsronit"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={styles.button}
+      >
+        Twitter
+      </a>
+      <p style={paragraph}>You can tip me on - kumarkrsronit.near</p>© 2024
+      @FamLinkup
     </div>
   );
 }
