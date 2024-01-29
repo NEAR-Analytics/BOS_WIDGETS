532d8fe9a61cb92f54ce2e9053efb510b4f71960
ndcdev.near <ndcdev.near>
2024-01-29 12:15:58 +0000

    Update Dashboard.Config by ndcdev.near at 2024-01-29T12:15:58.136Z

--- ndcdev.near/dashboard.Config/source_code.jsx
+++ ndcdev.near/dashboard.Config/source_code.jsx
@@ -11,6 +11,11 @@ return {
     // "onboard.sputnik-dao.near",
     // "she-is-near.sputnik-dao.near",
   ],
+  socialLinks: {
+    near: 'https://near.org',
+    twitter: 'https://twitter.com/nearprotocol',
+    telegram: 'https://t.me/cryptonear',
+  },
   assets: {
     logoWhite:
       "https://ipfs.near.social/ipfs/bafkreigrpxkfqynsjxurjpewxweytewedshhrvuk3ndrkf34j5vxevqs2i",
@@ -22,5 +27,11 @@ return {
       "https://ipfs.near.social/ipfs/bafkreidrf2na3ubbte3voup6jtbauxmxdfqsu53ffnqyk7jvt36whxbdo4",
     telegramLogo:
       "https://ipfs.near.social/ipfs/bafkreieoji43nz537vztjmphndnfwftj3a7m7nbdhcb7hypkssboxl57xa",
+    footer: {
+      nearLogo: 'https://ipfs.near.social/ipfs/bafkreib36kw4koctk4pm6kyzuy5aj3n27ucffjdmscvn2mpgdsweni4nfq',
+      nearLogoBlock: 'https://ipfs.near.social/ipfs/bafkreigejwo77tcdb3f63zzdr54z7kzdpjr4e3jc6ufad4skankrcxm4ei',
+      twitterLogo: 'https://ipfs.near.social/ipfs/bafkreiav23v5rqzqsyv2nag4xhsp2m62uwo6k5e54oai7gk472gop2kjkm',
+      telegramLogo: 'https://ipfs.near.social/ipfs/bafkreigspr6ejmsxemeihcf3nnktd3ro3hgvaiihtsu7k2gsunop5vaxjy'
+    }
   },
 };
