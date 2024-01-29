7e1f3156dde0948d6a88df534ae000448828a61d
bluebiu.near <bluebiu.near>
2024-01-29 02:54:12 +0000

    Update Swap.Dex by bluebiu.near at 2024-01-29T02:54:12.403Z

--- bluebiu.near/Swap.Dex/source_code.jsx
+++ bluebiu.near/Swap.Dex/source_code.jsx
@@ -76,7 +76,7 @@ const ArrowRight = (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M1 11L5 6L1 1" stroke="currentColor" stroke-width="2" />
+    <path d="M1 11L5 6L1 1" stroke="currentColor" strokeWidth="2" />
   </svg>
 );
 return (
