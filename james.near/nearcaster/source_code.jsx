5d199540da4908eae2da235681f52e70994e46bc
james.near <james.near>
2024-01-30 14:56:48 +0000

    Update nearcaster by james.near at 2024-01-30T14:56:48.145Z

--- james.near/nearcaster/source_code.jsx
+++ james.near/nearcaster/source_code.jsx
@@ -1,4 +1,6 @@
 const [apiKey, setApiKey] = useState("");
+const [viewerId, setViewerId] = useState("");
+const [targetId, setTargetId] = useState("");
 
 const [farData, setFarData] = useState(null);
 const [error, setError] = useState(null);
@@ -8,39 +10,72 @@ const headers = {
   method: "GET",
   headers: {
     api_key: `${apiKey}`,
-    target_fid: 195725,
-    viewer_fid: 235259,
   },
 };
 
 function fetchData() {
-  const promise = asyncFetch(
-    "https://api.neynar.com/v2/farcaster/followers/relevant",
-    { headers }
+  const params = {
+    target_fid: targetId, // 195725
+    viewer_fid: viewerId, // 235259
+  };
+
+  console.log(params);
+
+  const urlSearchParams = Object.keys(params)
+    .map((param) => `${param}=${params[param]}`)
+    .join("&");
+
+  console.log(urlSearchParams);
+
+  let promise = asyncFetch(
+    `https://api.neynar.com/v2/farcaster/followers/relevant?${urlSearchParams}`,
+    headers
   );
 
-  promise.then((res) => {
-    setFarData(res.body);
+  promise.then((data) => {
+    setFarData(data);
   });
 }
 
 return (
   <>
     <div className="m-3">
-      <h3 className="mb-2">Social Hub</h3>
-      <h5 className="mb-2">Farcaster Social Graph</h5>
-      <input
-        type="text"
-        placeholder="Neynar API Key"
-        value={apiKey}
-        onChange={(e) => setApiKey(e.target.value)}
-      />
-      <button className="btn btn-outline-success mt-3" onClick={fetchData}>
-        Get Data
-      </button>
+      <h3 className="m-2">Social Hub</h3>
+      <h5 className="m-2 mt-3">Farcaster Social Graph</h5>
+      <div className="m-2">
+        <input
+          type="text"
+          placeholder="Neynar API Key"
+          value={apiKey}
+          onChange={(e) => setApiKey(e.target.value)}
+        />
+      </div>
+      <h5 className="m-2 mt-3">Target FID</h5>
+      <div className="m-2">
+        <input
+          type="text"
+          placeholder="whose profile you are looking at"
+          value={targetId}
+          onChange={(e) => setTargetId(e.target.value)}
+        />
+      </div>
+      <h5 className="m-2 mt-3">Viewer FID</h5>
+      <div className="m-2">
+        <input
+          type="text"
+          placeholder="who is looking at the profile"
+          value={viewerId}
+          onChange={(e) => setViewerId(e.target.value)}
+        />
+      </div>
+      <div className="m-2">
+        <button className="btn btn-outline-success mt-2" onClick={fetchData}>
+          Get Data
+        </button>
+      </div>
     </div>
     <div className="m-3">
-      <p>{JSON.stringify(farData)}</p>
+      <p className="m-3">{farData && JSON.stringify(farData)}</p>
     </div>
   </>
 );
