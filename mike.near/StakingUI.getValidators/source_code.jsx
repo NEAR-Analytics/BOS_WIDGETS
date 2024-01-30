30347d865c04faf97754af2d2ba08aa5ccd99432
mike.near <mike.near>
2024-01-30 23:53:43 +0000

    Update StakingUI.getValidators by mike.near at 2024-01-30T23:53:43.396Z

---
+++ mike.near/StakingUI.getValidators/source_code.jsx
@@ -0,0 +1,52 @@
+const [allMainnetValidators, setAllMainnetValidators] = useState([]);
+
+const mainnetValidators = {
+  getAddresses: (customRPCEndpoint) => {
+    customRPCEndpoint = customRPCEndpoint || "https://rpc.mainnet.near.org";
+    return new Promise((resolve, reject) => {
+      try {
+        asyncFetch("https://rpc.mainnet.near.org", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            jsonrpc: "2.0",
+            method: "validators",
+            params: [null],
+            id: "mainnet-vals",
+          }),
+        }).then((data) => {
+          console.log("aloha data", data);
+          const { current_validators, next_validators } = data.body.result;
+          // for regular fetch, no asyncFetch
+          // const { current_validators, next_validators } = data.result;
+
+          console.log("current_validators", current_validators);
+          console.log("next_validators", next_validators);
+
+          const uniqueAccountIds = getUniqueValidatorAddresses(
+            current_validators,
+            next_validators
+          );
+          console.log("uniqueAccountIds", uniqueAccountIds);
+
+          // Update state or perform other actions with uniqueAccountIds
+          // State.update({
+          // allValidatorAddresses: uniqueAccountIds
+          // });
+        });
+        return resolve(
+          expectedSignerAddress.toLowerCase() == address.toLowerCase()
+        );
+      } catch {
+        console.error(
+          `Could not get list of mainnet validators from ${customRPCEndpoint}`
+        );
+        return reject();
+      }
+    });
+  },
+};
+
+return { allMainnetValidators, mainnetValidators };
