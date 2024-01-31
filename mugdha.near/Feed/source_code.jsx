bcd60ab5b224540c9eafb12527b9b59d21335bd7
mugdha.near <mugdha.near>
2024-01-31 03:50:08 +0000

    Update Feed by mugdha.near at 2024-01-31T03:50:08.219Z

--- mugdha.near/Feed/source_code.jsx
+++ mugdha.near/Feed/source_code.jsx
@@ -24,12 +24,142 @@ function formatDate(date) {
 
 const feeds = {
   resolutions: {
-    label: "12th Graders",
+    label: "Resolutions",
     icon: "bi-calendar3",
-    name: "12th Graders",
-    hashtag: "12thGraders",
-    template: `As a 12th grader, what are the problems you face?
+    name: "resolution",
+    hashtag: "nearyearresolutions2024",
+    template: `### 🎉 NEAR YEAR RESOLUTIONS: 2024
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed))
+
+**🌟 REFLECTIONS ON THE PAST YEAR:**
+- [Reflection 1 from the past year]
+- [Reflection 2 from the past year]
+
+**🎯 NEW YEAR'S RESOLUTIONS:**
+- [Resolution 1]
+- [Resolution 2]
+
+**📊 MEASURING SUCCESS:**
+- [Metric 1 for Success]
+- [Metric 2 for Success]
 `,
+  },
+  updates: {
+    label: "Updates",
+    icon: "bi-bell",
+    name: "update",
+    hashtag: "update",
+    template: `### BUILDER UPDATE:  ${formatDate(new Date())}
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=update))
+
+**✅ DONE**
+- [what'd you do]
+- [link proof]
+
+**⏩ NEXT**
+- [what's next?]
+- [what are you thinking about?]
+
+**🛑 BLOCKERS**
+- [what's blocking you?]
+- [how can someone help?]
+`,
+  },
+  documentation: {
+    label: "Documentation",
+    icon: "bi-book",
+    name: "documentation",
+    hashtag: "documentation",
+    template: `## TITLE
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=documentation))
+
+**WHAT IS _____?**
+- [context]
+- [why is it important?]
+
+**EXAMPLE**
+- [how can this be demonstrated?]
+- [what is the expected outcome?]
+
+**USAGE**
+- [where is it used?]
+- [how to use it]
+`,
+  },
+  question: {
+    label: "Question",
+    icon: "bi-question-lg",
+    name: "question",
+    hashtag: "question",
+    template: `## what is your question?
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=question))
+
+[what are you thinking about?]
+[why are you asking?]
+`,
+  },
+  answer: {
+    label: "Answer",
+    icon: "bi-journal-code",
+    name: "answer",
+    hashtag: "answer",
+    template: `## Share an answer
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=answer))
+
+[please restate the question you are answering]
+
+[your answer]
+
+[link to relevant docs, examples, or resources]
+`,
+  },
+  opportunity: {
+    label: "Opportunity",
+    icon: "bi-briefcase",
+    name: "opportunity",
+    hashtag: "opportunity",
+    template: `## TITLE
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=opportunity))
+
+[what is the opportunity?]
+
+[explain the motivation or reason]
+
+`,
+  },
+  idea: {
+    label: "Idea",
+    icon: "bi-lightbulb",
+    name: "idea",
+    hashtag: "idea",
+    template: ``,
+  },
+  task: {
+    label: "Task",
+    icon: "bi-check-lg",
+    name: "task",
+    hashtag: "task",
+    template: `## TASK TITLE
+(posted via [Build DAO Gateway](https://nearbuilders.org/feed?hashtag=task))
+
+**What needs to be done?**
+- [Describe the task or action steps]
+
+**Context or additional information:**
+- [Provide any context or details]
+`,
+  },
+  test: {
+    label: "Test",
+    icon: "bi-tools",
+    name: "test",
+    hashtag: "test",
+    template: ``,
+  },
+  bookmarks: {
+    label: "Bookmarks",
+    icon: "bi-bookmark",
+    name: "bookmark",
   },
 };
 
@@ -64,19 +194,24 @@ return (
       mainContent: (
         <>
           {context.accountId ? (
-            <Widget
-              src="buildhub.near/widget/Compose"
-              props={{
-                feed: feeds[activeFeed],
-                template: feeds[activeFeed].template,
-              }}
-            />
+            activeFeed !== "bookmarks" ? (
+              <Widget
+                src="buildhub.near/widget/Compose"
+                props={{
+                  feed: feeds[activeFeed],
+                  template: feeds[activeFeed].template,
+                }}
+              />
+            ) : (
+              <Widget src="buildhub.near/widget/Bookmarks" />
+            )
           ) : (
-            <Widget src="buildhub.near/widget/Bookmarks" />
-          )}{" "}
-          : (
-          <Widget src="buildhub.near/widget/Bookmarks" props={props} />)
-          {
+            <Widget
+              src="buildhub.near/widget/components.login-now"
+              props={props}
+            />
+          )}
+          {activeFeed !== "bookmarks" && (
             <Feed
               index={[
                 {
@@ -99,7 +234,7 @@ return (
                 />
               )}
             />
-          }
+          )}
         </>
       ),
     }}
