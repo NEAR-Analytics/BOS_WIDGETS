b328bd3a3a220253be4f9e4f37de227440aec854
mike.near <mike.near>
2024-01-31 16:20:42 +0000

    Update ProgressBar by mike.near at 2024-01-31T16:20:42.510Z

--- mike.near/ProgressBar/source_code.jsx
+++ mike.near/ProgressBar/source_code.jsx
@@ -20,7 +20,7 @@ const ProgressBar = ({ value, max }) => {
     background:
       "linear-gradient(90deg, rgba(151, 151, 255, 1) 0%, rgba(115, 115, 255, 1) 100%)", // Gradient effect
     boxShadow: "0 0 10px rgba(115, 115, 255, 0.75)", // Glowing effect
-    transition: "width 0.3s cubic-bezier(0.6, -0.6, 0.6, 1.9)",
+    transition: "width 0.3s cubic-bezier(0.3, -0.3, 0.3, 0.6)",
     borderRadius: "6px", // Rounded corners for the progress
     display: "block",
   };
