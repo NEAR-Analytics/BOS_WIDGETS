15c8b64ffc949e309d0778d59874ebdcf5487006
mike.near <mike.near>
2024-01-31 00:35:44 +0000

    Update StakingUI.getValidators by mike.near at 2024-01-31T00:35:44.181Z

--- mike.near/StakingUI.getValidators/source_code.jsx
+++ mike.near/StakingUI.getValidators/source_code.jsx
@@ -9,75 +9,75 @@ const mainnetValidators = {
 
   getAddresses: async (customRPCEndpoint) => {
     customRPCEndpoint = customRPCEndpoint || "https://rpc.mainnet.near.org";
-    let uniqueAccountIds = fetch("https://rpc.mainnet.near.org", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        method: "validators",
-        params: [null],
-        id: "mainnet-vals",
-      }),
-    }).then((data) => {
-      console.log("aloha data", data);
-      const { current_validators, next_validators } = data.body.result;
-      // for regular fetch, no asyncFetch
-      // const { current_validators, next_validators } = data.result;
+    // let uniqueAccountIds = fetch("https://rpc.mainnet.near.org", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     jsonrpc: "2.0",
+    //     method: "validators",
+    //     params: [null],
+    //     id: "mainnet-vals",
+    //   }),
+    // }).then((data) => {
+    //   console.log("aloha data", data);
+    //   const { current_validators, next_validators } = data.body.result;
+    //   // for regular fetch, no asyncFetch
+    //   // const { current_validators, next_validators } = data.result;
 
-      console.log("current_validators", current_validators);
-      console.log("next_validators", next_validators);
+    //   console.log("current_validators", current_validators);
+    //   console.log("next_validators", next_validators);
 
-      const uniqueAccountIds = getUniqueValidatorAddresses(
-        current_validators,
-        next_validators
-      );
-      return uniqueAccountIds;
-    });
-    // return uniqueAccountIds;
-    // return new Promise((resolve, reject) => {
-    //   try {
-    //     let uniqueAccountIds = asyncFetch("https://rpc.mainnet.near.org", {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify({
-    //         jsonrpc: "2.0",
-    //         method: "validators",
-    //         params: [null],
-    //         id: "mainnet-vals",
-    //       }),
-    //     });
-    //     // .then((data) => {
-    //     //   console.log("aloha data", data);
-    //     //   const { current_validators, next_validators } = data.body.result;
-    //     //   // for regular fetch, no asyncFetch
-    //     //   // const { current_validators, next_validators } = data.result;
-
-    //     //   console.log("current_validators", current_validators);
-    //     //   console.log("next_validators", next_validators);
-
-    //     //   const uniqueAccountIds = getUniqueValidatorAddresses(
-    //     //     current_validators,
-    //     //     next_validators
-    //     //   );
-    //     //   console.log("uniqueAccountIds", uniqueAccountIds);
-
-    //     //   // Update state or perform other actions with uniqueAccountIds
-    //     //   // State.update({
-    //     //   // allValidatorAddresses: uniqueAccountIds
-    //     //   // });
-    //     // });
-    //     return resolve(uniqueAccountIds);
-    //   } catch {
-    //     console.error(
-    //       `Could not get list of mainnet validators from ${customRPCEndpoint}`
-    //     );
-    //     return reject();
-    //   }
+    //   const uniqueAccountIds = getUniqueValidatorAddresses(
+    //     current_validators,
+    //     next_validators
+    //   );
+    //   return uniqueAccountIds;
     // });
+    // return uniqueAccountIds;
+    return new Promise((resolve, reject) => {
+      try {
+        let uniqueAccountIds = asyncFetch(customRPCEndpoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            jsonrpc: "2.0",
+            method: "validators",
+            params: [null],
+            id: "mainnet-vals",
+          }),
+        }).then((data) => {
+          console.log("aloha data", data);
+          const { current_validators, next_validators } = data.body.result;
+          // for regular fetch, no asyncFetch
+          // const { current_validators, next_validators } = data.result;
+
+          console.log("current_validators", current_validators);
+          console.log("next_validators", next_validators);
+
+          const uniqueAccountIds = getUniqueValidatorAddresses(
+            current_validators,
+            next_validators
+          );
+          console.log("uniqueAccountIds", uniqueAccountIds);
+          return uniqueAccountIds;
+
+          // Update state or perform other actions with uniqueAccountIds
+          // State.update({
+          // allValidatorAddresses: uniqueAccountIds
+          // });
+        });
+        return resolve(uniqueAccountIds);
+      } catch {
+        console.error(
+          `Could not get list of mainnet validators from ${customRPCEndpoint}`
+        );
+        return reject();
+      }
+    });
   },
 };
 
