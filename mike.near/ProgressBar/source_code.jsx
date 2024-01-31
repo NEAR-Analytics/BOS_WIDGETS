9e2b87579f43923d4ebd6ce73b98dfea45ae868f
mike.near <mike.near>
2024-01-31 04:02:24 +0000

    Update ProgressBar by mike.near at 2024-01-31T04:02:24.336Z

--- mike.near/ProgressBar/source_code.jsx
+++ mike.near/ProgressBar/source_code.jsx
@@ -1,29 +1,26 @@
 const ProgressBar = ({ value, max }) => {
+  // Calculate the width of the progress bar as a percentage
   const width = (value / max) * 100;
 
   const containerStyle = {
-    // Adding a subtle shadow for depth
-    boxShadow: "0 3px 6px rgba(0, 0, 0, 0.6)",
-    border: "1px solid #000",
-    backgroundColor: "#f2f1e9", // Off-white primary
+    border: "2px outset #000",
+    backgroundColor: "#f2f1e9", // Off-white primary from https://near.org/brand
     padding: "1px",
     height: "23px",
     width: "100%",
-    borderRadius: "6px",
+    borderRadius: "3px",
     overflow: "hidden",
-    display: "flex",
   };
 
-  let progressBarStyle = {
+  const progressBarStyle = {
     height: "100%",
     width: `${width}%`,
-    background:
-      "linear-gradient(90deg, rgba(151, 151, 255, 1) 0%, rgba(115, 115, 255, 1) 100%)", // Gradient effect
-    boxShadow: "0 0 10px rgba(115, 115, 255, 0.75)", // Glowing effect
-    // transition: "width 0.3s cubic-bezier(0.3, -0.3, 0.3, 0.6)",
-    transition: "width 0.3s ease-in",
-    borderRadius: "6px", // Rounded corners for the progress
-    display: "block",
+    backgroundColor: "#9797ff", // Accent purple from https://near.org/brand
+    backgroundImage:
+      "linear-gradient(45deg, transparent 25%, rgba(255, 255, 255, 0.4) 25%, rgba(255, 255, 255, 0.4) 50%, transparent 50%, transparent 75%, rgba(255, 255, 255, 0.4) 75%)",
+    backgroundSize: "19px 19px",
+    backgroundPosition: `${width / 6}px 0`, // Have it move like a barber shop
+    transition: "width 1s cubic-bezier(0.6, -0.6, 0.6, 1.9)",
   };
 
   return (
