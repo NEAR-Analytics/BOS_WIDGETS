89df527c1364866ba7ba51852e7ae728778f62a5
mike.near <mike.near>
2024-01-31 00:43:46 +0000

    Update StakingUI.getValidators by mike.near at 2024-01-31T00:43:46.423Z

--- mike.near/StakingUI.getValidators/source_code.jsx
+++ mike.near/StakingUI.getValidators/source_code.jsx
@@ -5,37 +5,8 @@ const getUniqueValidatorAddresses = (validators1, validators2) => {
 };
 
 const mainnetValidators = {
-  // uniqueAccountIds = [],
-
   getAddresses: async (customRPCEndpoint) => {
     customRPCEndpoint = customRPCEndpoint || "https://rpc.mainnet.near.org";
-    // let uniqueAccountIds = fetch("https://rpc.mainnet.near.org", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     jsonrpc: "2.0",
-    //     method: "validators",
-    //     params: [null],
-    //     id: "mainnet-vals",
-    //   }),
-    // }).then((data) => {
-    //   console.log("aloha data", data);
-    //   const { current_validators, next_validators } = data.body.result;
-    //   // for regular fetch, no asyncFetch
-    //   // const { current_validators, next_validators } = data.result;
-
-    //   console.log("current_validators", current_validators);
-    //   console.log("next_validators", next_validators);
-
-    //   const uniqueAccountIds = getUniqueValidatorAddresses(
-    //     current_validators,
-    //     next_validators
-    //   );
-    //   return uniqueAccountIds;
-    // });
-    // return uniqueAccountIds;
     return new Promise((resolve, reject) => {
       try {
         let uniqueAccountIds = asyncFetch(customRPCEndpoint, {
@@ -52,8 +23,6 @@ const mainnetValidators = {
         }).then((data) => {
           console.log("aloha data", data);
           const { current_validators, next_validators } = data.body.result;
-          // for regular fetch, no asyncFetch
-          // const { current_validators, next_validators } = data.result;
 
           console.log("current_validators", current_validators);
           console.log("next_validators", next_validators);
@@ -64,12 +33,6 @@ const mainnetValidators = {
           );
           console.log("uniqueAccountIds", uniqueAccountIds);
           return resolve(uniqueAccountIds);
-          //   return uniqueAccountIds;
-
-          // Update state or perform other actions with uniqueAccountIds
-          // State.update({
-          // allValidatorAddresses: uniqueAccountIds
-          // });
         });
       } catch {
         console.error(
@@ -81,4 +44,4 @@ const mainnetValidators = {
   },
 };
 
-return { allMainnetValidators, mainnetValidators };
+return { mainnetValidators };
