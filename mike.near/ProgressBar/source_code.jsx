c46f7290c0b8eee2cad79730a850868a41b781d6
mike.near <mike.near>
2024-01-31 06:37:23 +0000

    Update ProgressBar by mike.near at 2024-01-31T06:37:23.883Z

--- mike.near/ProgressBar/source_code.jsx
+++ mike.near/ProgressBar/source_code.jsx
@@ -1,26 +1,23 @@
 const ProgressBar = ({ value, max }) => {
-  // Calculate the width of the progress bar as a percentage
   const width = (value / max) * 100;
 
   const containerStyle = {
     border: "2px outset #000",
-    backgroundColor: "#f2f1e9", // Off-white primary from https://near.org/brand
+    backgroundColor: "#f2f1e9", // Off-white background
     padding: "1px",
     height: "23px",
-    width: "100%",
+    width: "100%", // Ensure this is effectively 100% of a parent with a defined width
     borderRadius: "3px",
     overflow: "hidden",
+    display: "flex", // Use flex to better control the inner bar
   };
 
-  const progressBarStyle = {
+  let progressBarStyle = {
     height: "100%",
     width: `${width}%`,
-    backgroundColor: "#9797ff", // Accent purple from https://near.org/brand
-    backgroundImage:
-      "linear-gradient(45deg, transparent 25%, rgba(255, 255, 255, 0.4) 25%, rgba(255, 255, 255, 0.4) 50%, transparent 50%, transparent 75%, rgba(255, 255, 255, 0.4) 75%)",
-    backgroundSize: "19px 19px",
-    backgroundPosition: `${width / 6}px 0`, // Have it move like a barber shop
-    transition: "width 1s cubic-bezier(0.6, -0.6, 0.6, 1.9)",
+    backgroundColor: "#9797ff", // Accent purple
+    transition: "width 0.3s cubic-bezier(0.6, -0.6, 0.6, 1.9)",
+    display: "block",
   };
 
   return (
@@ -29,5 +26,3 @@ const ProgressBar = ({ value, max }) => {
     </div>
   );
 };
-
-return ProgressBar;
