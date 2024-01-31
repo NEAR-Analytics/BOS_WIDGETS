bae194c980478db2e32f868d75a0f30ccf038157
anupam9873.near <anupam9873.near>
2024-01-31 10:21:05 +0000

    Update Onboarding.Starter-fork-fork-fork-fork-fork by anupam9873.near at 2024-01-31T10:21:05.455Z

---
+++ anupam9873.near/Onboarding.Starter-fork-fork-fork-fork-fork/source_code.jsx
@@ -0,0 +1,84 @@
+return (
+  <div>
+    <h3>Little Bit About Me</h3>
+    <p>
+      My Name is Anupam, originally from India, I have a Bachelor Degree in
+      Computer Science & Engineering.
+    </p>
+    <p>
+      I am a dedicated and results-driven Data Engineer with a strong passion
+      for transforming data into actionable insights. With years of experience
+      in the Software industry, I have honed my skills in designing,
+      implementing Applications, and optimizing data pipelines to fuel informed
+      decision-making.
+    </p>
+    <ul>
+      <li>
+        📊 Data Alchemist: I specialize in the art of data extraction,
+        transformation, and loading (ETL) and have a proven track record of
+        developing robust ETL processes using tools such as [list relevant
+        tools, e.g., Apache Spark]. I thrive on the challenge of efficiently
+        handling and processing large datasets to unlock their potential.
+      </li>
+      <li>
+        🛠️ Tech Enthusiast: I am deeply committed to staying at the forefront of
+        technological advancements in the data engineering landscape. My toolkit
+        includes [list relevant programming languages, databases, and
+        frameworks, e.g., Python, SQL, PySpark, AWS Services like Glue, Lambda,
+        EventBridge, Stepfunction, SNS, SQS, Dynamo DB etc. and I relish the
+        opportunity to learn and adapt to new tools and technologies as they
+        emerge.
+      </li>
+      <li>
+        🔗 Data Architecture Maven: I understand the importance of a
+        well-architected data ecosystem. I have experience in designing and
+        implementing data lakes and data marts, ensuring data is structured and
+        accessible for analytics and reporting.
+      </li>
+      <li>
+        🔧 Problem Solver: Data engineering is all about tackling complex
+        challenges head-on. I have a proven ability to troubleshoot issues,
+        optimize performance, and ensure data quality, resulting in efficient
+        and reliable data pipelines.
+      </li>
+      <li>
+        👥 Collaborative Team Player: I thrive in cross-functional teams and
+        believe that great data engineering is built on effective communication
+        and collaboration. I have experience working with data scientists,
+        analysts, and business stakeholders to align technical solutions with
+        organizational goals.
+      </li>
+      <li>
+        🌟 Passion for Continuous Learning: In the ever-evolving field of data
+        engineering, I am committed to continuous learning and professional
+        growth. I actively engage with the data engineering community,
+        participate in meetups, conferences, and open-source projects, and
+        constantly seek ways to enhance my skills.
+      </li>
+      <li>
+        🚀 Always Ready for meeting with sharp & ambitious people to talk about
+        different ideas to solve problems & make an impact
+      </li>
+    </ul>
+    <h2>Interest & Hobbies</h2>
+    <p>
+      Travelling, Talk about Finance, Charity, Community Building, Health &
+      Wellness
+    </p>
+    <h2>Perfect Place to Meet & Talk</h2>
+    <p>Tech Meetups, Charity/Social Events, Walk & Talk in the Park</p>
+    <h2>Project:</h2>
+    <ol>
+      <li>GPS module</li>
+      <li>Hangman</li>
+    </ol>
+    <h2>Connect on</h2>
+    <ul>
+      <li>Telegram: Anupam</li>
+      <li>Twitter: anupam3792</li>
+
+      <li>Email: sihi457@gmail.com</li>
+      <li>Mobile: 2012757410</li>
+    </ul>
+  </div>
+);
