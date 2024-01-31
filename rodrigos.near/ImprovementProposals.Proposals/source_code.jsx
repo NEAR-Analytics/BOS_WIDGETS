17358c7998af63e3c447f432400a40d61954dcf8
rodrigos.near <rodrigos.near>
2024-01-31 05:13:30 +0000

    Update ImprovementProposals.Proposals by rodrigos.near at 2024-01-31T05:13:30.468Z

--- rodrigos.near/ImprovementProposals.Proposals/source_code.jsx
+++ rodrigos.near/ImprovementProposals.Proposals/source_code.jsx
@@ -26,7 +26,7 @@ State.init({
 });
 
 const yoctoToNear = (amountYocto) =>
-  new Big(amountYocto).div(new Big(10).pow(24)).toFixed(0);
+  new Big(amountYocto).div(new Big(10).pow(5)).toFixed(0);
 
 const numberWithCommas = (x) =>
   x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
