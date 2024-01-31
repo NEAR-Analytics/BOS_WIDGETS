321280e265078c7b2ae76914804d9d3911e5225c
megha19.near <megha19.near>
2024-01-31 12:21:49 +0000

    Update devhub.entity.addon.blog.Configurator by megha19.near at 2024-01-31T12:21:49.642Z

--- megha19.near/devhub.entity.addon.blog.Configurator/source_code.jsx
+++ megha19.near/devhub.entity.addon.blog.Configurator/source_code.jsx
@@ -1,7 +1,7 @@
 const { data, handle, onSubmit } = props;
 
 const { Tile } =
-  VM.require("megha2001.testnet/widget/devhub.components.molecule.Tile") ||
+  VM.require("megha19.near/widget/devhub.components.molecule.Tile") ||
   (() => <></>);
 
 if (!Tile) {
@@ -34,7 +34,7 @@ return (
   <Tile className="p-3 bg-white">
     <Container>
       <Widget
-        src={"megha2001.testnet/widget/devhub.entity.addon.blog.editor.index"}
+        src={"megha19.near/widget/devhub.entity.addon.blog.editor.index"}
         props={{
           data,
           handle,
