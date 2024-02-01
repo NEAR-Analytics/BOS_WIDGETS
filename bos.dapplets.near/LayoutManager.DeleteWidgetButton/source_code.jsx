60f5631552448f1fb0e92506cf957477371978ca
bos.dapplets.near <bos.dapplets.near>
2024-02-01 22:51:28 +0000

    Update LayoutManager.DeleteWidgetButton by bos.dapplets.near at 2024-02-01T22:51:28.532Z

--- bos.dapplets.near/LayoutManager.DeleteWidgetButton/source_code.jsx
+++ bos.dapplets.near/LayoutManager.DeleteWidgetButton/source_code.jsx
@@ -25,61 +25,48 @@ const RemoveIcon = (
 );
 
 const RemoveAction = styled.div`
-  cursor: pointer;
+cursor: pointer;
 position: relative;
-background: #DB504A;
+
 box-sizing: border-box;
-border: 1px solid #fff;
+
 max-width: 36px;
-    // height: 100%;
-    max-height: 36px;
+
+max-height: 36px;
 min-width: 14px;
 display: flex;
 align-items:center;
 justify-content:center;
 flex: 1 1 auto;
 border-radius:50%;
-  @keyframes translateAnimationBtn {
-    0% {
-      opacity: 0;
-    }
-    50% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
+@keyframes translateAnimationBtn {
+0% {
+  opacity: 0;
+}
+50% {
+  opacity: 0;
+}
+100% {
+  opacity: 1;
+}
+}
 
-  animation: translateAnimationBtn 0.5s linear forwards;
+animation: translateAnimationBtn 0.5s linear forwards;
+transition: all 0.3s;
+
+&:hover {
   transition: all 0.3s;
-
-  &:hover {
     svg {
-      transform: scale(1.2);
-    }
+  transform: scale(1.2);
   }
+}
 
-  }
-`;
+  svg {
+width: 100%;
+height: 100%;
+}
 
-const RemoveActionLineLeft = styled.div`
-display: flex;
-    width: 2px;
-    height: 50%;
-     position: absolute;
-   
-    transform: rotate(45deg) ;
-    background: rgb(255, 255, 255);
-   
+}
 `;
-const RemoveActionLineRight = styled.div`
-display: flex;
-   width: 2px;
-    height: 50%;
-     position: absolute;
-   
-    transform: rotate(-45deg) ;
-    background: rgb(255, 255, 255);`;
 
 return <RemoveAction onClick={props.onClick}>{RemoveIcon}</RemoveAction>;
