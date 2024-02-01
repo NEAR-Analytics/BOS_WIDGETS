f34fe7cf2f95aa8bda42afa39457b23babd6f118
bos.dapplets.near <bos.dapplets.near>
2024-02-01 11:26:54 +0000

    Update LayoutManager.DeleteWidgetButton by bos.dapplets.near at 2024-02-01T11:26:54.000Z

--- bos.dapplets.near/LayoutManager.DeleteWidgetButton/source_code.jsx
+++ bos.dapplets.near/LayoutManager.DeleteWidgetButton/source_code.jsx
@@ -1,29 +1,3 @@
-const RemoveIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="14"
-    height="14"
-    viewBox="0 0 14 14"
-    fill="none"
-  >
-    <circle cx="7" cy="7" r="6.5" fill="#DB504A" stroke="white" />
-    <path
-      d="M9.24976 4.75L4.74999 9.24977"
-      stroke="white"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M4.75 4.75L9.24977 9.24977"
-      stroke="white"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
 const RemoveAction = styled.div`
   cursor: pointer;
 position: relative;
@@ -31,7 +5,7 @@ background: #DB504A;
 box-sizing: border-box;
 border: 1px solid #fff;
 max-width: 36px;
-height:100%
+height:100%;
 min-width: 14px;
 display: flex;
 align-items:center;
