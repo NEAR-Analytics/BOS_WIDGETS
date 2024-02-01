243a4fc09bc8f4e83ae1e0a3aa00c1a296a180cb
meta-pool-official.near <meta-pool-official.near>
2024-02-01 18:40:01 +0000

    Update Governance.Metric.Card by meta-pool-official.near at 2024-02-01T18:40:01.555Z

---
+++ meta-pool-official.near/Governance.Metric.Card/source_code.jsx
@@ -0,0 +1,37 @@
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+  padding: 1.25em 0.85em;
+  background-color: #ECEEF0;
+  border-radius: 16px;
+`;
+
+const Value = styled.span`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 0.975rem;
+  line-height: 1em;
+  text-align: center;
+  color: #000;
+`;
+
+const Label = styled.span`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 0.875rem;
+  line-height: 1em;
+  text-align: center;
+  color: #4a5568;
+`;
+
+return (
+  <Container>
+    <Value>{props.value}</Value>
+    <Label data-toggle="tooltip" data-placement="bottom" title={props.tooltip}>
+      {props.label}
+    </Label>
+  </Container>
+);
