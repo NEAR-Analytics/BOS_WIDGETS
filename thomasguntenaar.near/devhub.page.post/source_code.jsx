1b22745a0f0f30e41846dc7053819ad18d6c2e63
thomasguntenaar.near <thomasguntenaar.near>
2024-02-02 16:46:41 +0000

    Update devhub.page.post by thomasguntenaar.near at 2024-02-02T16:46:41.997Z

--- thomasguntenaar.near/devhub.page.post/source_code.jsx
+++ thomasguntenaar.near/devhub.page.post/source_code.jsx
@@ -13,7 +13,7 @@ const Container = styled.div`
 return (
   <Container>
     <Widget
-      src={"thomasguntenaar.near/widget/devhub.entity.post.Post"}
+      src={"${REPL_DEVHUB}/widget/devhub.entity.post.Post"}
       props={{
         id,
       }}
