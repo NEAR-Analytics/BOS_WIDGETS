651535e3999ce69a96df0da8b5d6eb28bf07de22
thomasguntenaar.near <thomasguntenaar.near>
2024-02-02 16:46:41 +0000

    Update devhub.entity.community.Activity by thomasguntenaar.near at 2024-02-02T16:46:41.997Z

--- thomasguntenaar.near/devhub.entity.community.Activity/source_code.jsx
+++ thomasguntenaar.near/devhub.entity.community.Activity/source_code.jsx
@@ -1,10 +1,10 @@
 const { handle } = props;
 
 const { getCommunity } = VM.require(
-  "thomasguntenaar.near/widget/core.adapter.devhub-contract"
+  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
 );
 
-const { href } = VM.require("thomasguntenaar.near/widget/core.lib.url");
+const { href } = VM.require("${REPL_DEVHUB}/widget/core.lib.url");
 
 if (!getCommunity || !href) {
   return <p>Loading modules...</p>;
@@ -41,19 +41,19 @@ return (
       <div class="d-flex w-100">
         <MainContent>
           <Widget
-            src={"thomasguntenaar.near/widget/devhub.feature.post-search.panel"}
+            src={"${REPL_DEVHUB}/widget/devhub.feature.post-search.panel"}
             props={{
               hideHeader: true,
               tag: communityData.tag,
               children: (
                 <Widget
                   src={
-                    "thomasguntenaar.near/widget/devhub.components.molecule.PostControls"
+                    "${REPL_DEVHUB}/widget/devhub.components.molecule.PostControls"
                   }
                   props={{
                     title: "Post",
                     href: href({
-                      widgetSrc: "thomasguntenaar.near/widget/app",
+                      widgetSrc: "${REPL_DEVHUB}/widget/app",
                       params: {
                         page: "create",
                         labels: [communityData.tag],
@@ -69,7 +69,7 @@ return (
         </MainContent>
         <SidebarContainer>
           <Widget
-            src={"thomasguntenaar.near/widget/devhub.entity.community.Sidebar"}
+            src={"${REPL_DEVHUB}/widget/devhub.entity.community.Sidebar"}
             props={{ community: communityData }}
           />
         </SidebarContainer>
