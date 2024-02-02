fdcabf39b44d6fd25b464e29adc706ed73a28937
sdks.near <sdks.near>
2024-02-02 20:15:15 +0000

    Update Utils.RoutesManager by sdks.near at 2024-02-02T20:15:15.631Z

--- sdks.near/Utils.RoutesManager/source_code.jsx
+++ sdks.near/Utils.RoutesManager/source_code.jsx
@@ -19,10 +19,10 @@ const RoutesManager = (Store, status, routes, { page }) => {
         RouterView: () => Router.get("state").currentView || null,
         Route: (props) => {
             let search = (obj, index) => {
-                return index in obj ? search(obj, index) : obj;
+                return index in obj ? search(obj[index], index) : obj;
             };
             let params = search(props, "props");
-            
+
             return <a href="#" onClick={() => Router.changeRoute(params.to)}>
                 {props.children}
             </a>;
