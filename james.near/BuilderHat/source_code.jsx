8de8d115d8668a10a4a90266a8e221c1e134686e
james.near <james.near>
2024-02-02 19:20:09 +0000

    Update BuilderHat by james.near at 2024-02-02T19:20:09.073Z

---
+++ james.near/BuilderHat/source_code.jsx
@@ -0,0 +1,35 @@
+const accountId = props.accountId;
+if (!accountId) {
+  return "";
+}
+
+const isBuilder =
+  props.isBuilder || Social.get(`gov.near/badge/builder/accounts/${accountId}`);
+
+const content = isBuilder ? (
+  <span style={{ verticalAlign: center }}>
+    <a
+      title="Magnesus, CC0, via Wikimedia Commons"
+      href="https://commons.wikimedia.org/wiki/File:Yellow_hard_hat.svg"
+    >
+      <img
+        style={{ height: "1.1rem", paddingLeft: "0.2rem" }}
+        alt="Yellow Builder Hat"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Yellow_hard_hat.svg/128px-Yellow_hard_hat.svg.png"
+      />
+    </a>
+  </span>
+) : (
+  ""
+);
+
+return (
+  <Widget
+    loading={content}
+    src="mob.near/widget/N.Common.OverlayTrigger"
+    props={{
+      popup: <div>Builder Hat</div>,
+      children: content,
+    }}
+  />
+);
