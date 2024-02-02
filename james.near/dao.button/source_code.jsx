55d513216eca40df5812a96008678b1214e17e78
james.near <james.near>
2024-02-02 22:18:39 +0000

    Update dao.button by james.near at 2024-02-02T22:18:39.401Z

--- james.near/dao.button/source_code.jsx
+++ james.near/dao.button/source_code.jsx
@@ -1,16 +1,14 @@
-if (
-  !props.accountId ||
-  !context.accountId ||
-  context.accountId === props.accountId
-) {
+const accountId = props.accountId;
+
+if (!accountId) {
   return "";
 }
 
 return (
-  <button
+  <a
     href={`/james.near/widget/LatestWidgets?accountId=${accountId}`}
     className="btn btn-outline-light rounded-5"
   >
     Widgets
-  </button>
+  </a>
 );
