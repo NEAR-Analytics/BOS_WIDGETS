617f85c8800c1554cd0c453b26ddc3d286a68c2e
mugdha.near <mugdha.near>
2024-02-02 05:26:25 +0000

    Update HomepagePlus by mugdha.near at 2024-02-02T05:26:25.712Z

--- mugdha.near/HomepagePlus/source_code.jsx
+++ mugdha.near/HomepagePlus/source_code.jsx
@@ -12,16 +12,13 @@ const markdown = ` ## SocialTeen`;
 
 return (
   <div style={{ display: "flex", flexDirection: "row" }}>
-    {/* Main content area for the feed */}
     <div style={{ flex: 1 }}>
       <Markdown text={markdown} />
       <Widget src="mugdha.near/widget/Feed" props={props} />
     </div>
 
-    {/* Right sidebar for the Welcome widget */}
     <div style={{ width: "300px", paddingLeft: "20px" }}>
       {" "}
-      {/* Adjust width and padding as needed */}
       <Widget src="create.near/widget/Followers" props={props} />
     </div>
   </div>
