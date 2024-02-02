4a674a65f14af43afaaeede8476bfa0d66900793
thomasguntenaar.near <thomasguntenaar.near>
2024-02-02 16:46:41 +0000

    Update devhub.feature.post-search.by-author by thomasguntenaar.near at 2024-02-02T16:46:41.997Z

--- thomasguntenaar.near/devhub.feature.post-search.by-author/source_code.jsx
+++ thomasguntenaar.near/devhub.feature.post-search.by-author/source_code.jsx
@@ -1,5 +1,5 @@
 const { getAllAuthors } = VM.require(
-  "thomasguntenaar.near/widget/core.adapter.devhub-contract"
+  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
 );
 
 if (!getAllAuthors) {
