bd3e75f0d2980a53df331bee6668fb3941efb2b3
mugdha.near <mugdha.near>
2024-02-02 04:38:43 +0000

    Update HomepagePlus by mugdha.near at 2024-02-02T04:38:43.953Z

--- mugdha.near/HomepagePlus/source_code.jsx
+++ mugdha.near/HomepagePlus/source_code.jsx
@@ -8,18 +8,4 @@ if (homepage === null) {
   return "Loading";
 }
 
-const markdown = ` ## SocialTeen`;
-
-return (
-  <div style={{ display: "flex", flexDirection: "row" }}>
-    <div style={{ flex: 1 }}>
-      <Markdown text={markdown} />
-      <Widget src="mugdha.near/widget/Feed" props={props} />
-    </div>
-
-    <div style={{ width: "300px", paddingLeft: "20px" }}>
-      {" "}
-      <Widget src="create.near/widget/Followers" props={props} />
-    </div>
-  </div>
-);
+return <Widget src="mugdha.near/widget/Feed" props={props} />;
