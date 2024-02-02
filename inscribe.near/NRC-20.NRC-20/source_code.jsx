9322e939a14f46073c5b744aeb373796e210e069
inscribe.near <inscribe.near>
2024-02-02 14:31:11 +0000

    Update NRC-20.NRC-20 by inscribe.near at 2024-02-02T14:31:11.808Z

--- inscribe.near/NRC-20.NRC-20/source_code.jsx
+++ inscribe.near/NRC-20.NRC-20/source_code.jsx
@@ -121,10 +121,12 @@ function getConfig(network) {
           op: "transfer",
           tick: "neat",
         },
+        ftWrapperFactory: "nrc-20.near",
         ftWrapper: "neat.nrc-20.near",
         refFinance: "https://app.ref.finance/",
         minMintEvents: 1_000_000,
         minHolders: 1_000,
+        neatDecimals: 8,
       };
     case "testnet":
       return {
@@ -145,10 +147,12 @@ function getConfig(network) {
           op: "transfer",
           tick: "neat",
         },
+        ftWrapperFactory: "nrc-20.testnet",
         ftWrapper: "neat.nrc-20.testnet",
         refFinance: "https://testnet.ref-finance.com/",
         minMintEvents: 10,
         minHolders: 5,
+        neatDecimals: 8,
       };
     default:
       throw Error(`Unconfigured environment '${network}'.`);
@@ -161,6 +165,10 @@ const tx = {
   args: config.args,
   gas: GasPerTransaction,
 };
+
+function ftWrapperAddress(tick) {
+  return tick.toLowerCase() + "." + config.ftWrapperFactory;
+}
 
 const TopButton = styled("Link")`
   color: white;
