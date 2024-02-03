2b9700c8b9f3540d1f3f5dba00866305dee5b107
mattb.near <mattb.near>
2024-02-03 13:08:25 +0000

    Update Frensly.Components.RecentlyVerified by mattb.near at 2024-02-03T13:08:25.721Z

---
+++ mattb.near/Frensly.Components.RecentlyVerified/source_code.jsx
@@ -0,0 +1,98 @@
+const { title } = props;
+title = title ?? "New frens";
+
+State.init({
+  profiles: {},
+});
+
+const LensLib = VM.require("mattb.near/widget/NearBadger.Libs.Lens");
+
+let recentlyVerified = LensLib.listRecentlyVerifiedProfiles({
+  subscribe: true,
+});
+
+const Section = styled.div`
+    height:100vh;
+    overflow:hidden;
+
+    h1 {
+        font-weight:bold;
+        margin-bottom:1.3rem;
+        padding:1rem 1.8rem 0;
+    }
+`;
+
+const Carousel = styled.div`
+  display:flex;
+  flex-direction:column;
+  flex-wrap:no-wrap;
+  padding:0 1.8rem 1rem;
+
+  > div {
+    :not(:last-of-type) {
+      margin-right:15px;
+    }
+  }
+`;
+
+const Pill = styled.div`
+  display:flex;
+  align-items:flex-start;
+  width:100%;
+  padding:1rem;
+  margin-bottom:20px;
+  border-radius:10px;
+  background-color:rgba(0,0,0,.05);
+  align-items:center;
+
+  .description {
+    margin-left:1rem;
+
+    h2 {
+      font-size:1.2rem;
+      margin:0;
+      padding:0;
+    }
+  }
+`;
+
+const Avatar = styled.div`
+  display:flex;
+  flex-grow:1;
+  width:100%;
+  max-width:60px;
+  height:60px;
+  border-radius:100%;
+  background-color:rgba(0,0,0,.05);
+  overflow:hidden;
+  flex-shrink:0;
+  
+  @media screen and (max-width:300px) {
+      max-width:30px;
+      height:30px;
+  }
+
+  img {
+    max-width:100%;
+  }
+`;
+
+return (
+  <Section>
+    <h1>{title}</h1>
+    <Carousel>
+      {recentlyVerified.map((profile) => (
+        <Pill>
+          <Avatar>
+          </Avatar>
+          <div className="description">
+            <h2>
+              <strong>@{profile.accountId}</strong> has linked{" "}
+              <strong>@{profile.value.name}</strong> to their accountasdasdsaasdasd
+            </h2>
+          </div>
+        </Pill>
+      ))}
+    </Carousel>
+  </Section>
+);
