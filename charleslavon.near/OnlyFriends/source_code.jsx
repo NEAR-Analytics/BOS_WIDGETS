b074010dce2aafd486a97db3b0f35e74c8ae6245
charleslavon.near <charleslavon.near>
2024-02-03 18:46:40 +0000

    Update OnlyFriends by charleslavon.near at 2024-02-03T18:46:40.200Z

--- charleslavon.near/OnlyFriends/source_code.jsx
+++ charleslavon.near/OnlyFriends/source_code.jsx
@@ -1,14 +1,56 @@
 const { videoId, cdnAccount } = props;
-const {
-  body: { token },
-} = fetch(`https://charleslavon.xyz/api/video?videoId=${videoId}`);
+if (!videoId || !cdnAccount) return "missing required props";
 
+State.init({
+  token: undefined,
+  message: "Loading...",
+});
+
+const p = asyncFetch(`https://charleslavon.xyz/api/video?videoId=${videoId}`);
+p.then((res) => {
+  const { token } = res.body;
+  if (token) {
+    State.update({ token });
+  } else {
+    State.update({ message: "This content is no longer available." });
+  }
+});
+
+const LoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-tems: center;
+  width: ${width};
+  height: ${height};
+`;
+
+const Loader = () => {
+  return (
+    <LoaderContainer className="loader">
+      <span
+        className="spinner-grow spinner-grow-sm me-1"
+        role="status"
+        aria-hidden="true"
+      />
+      {state.message}
+    </LoaderContainer>
+  );
+};
+
+console.log(
+  `https://customer-${cdnAccount}.cloudflarestream.com/${state.token}/iframe`
+);
 return (
-  <iframe
-    src={`https://customer-${cdnAccount}.cloudflarestream.com/${token}/iframe`}
-    height="720"
-    width="1280"
-    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
-    allowfullscreen="true"
-  ></iframe>
+  <>
+    {!state.token && <Loader />}
+    {state.token && (
+      <iframe
+        src={`https://customer-${cdnAccount}.cloudflarestream.com/${state.token}/iframe`}
+        height="720"
+        width="1280"
+        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+        allowfullscreen="true"
+      ></iframe>
+    )}
+  </>
 );
