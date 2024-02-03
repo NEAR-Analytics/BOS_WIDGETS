8bc9311ec80e901573d6d76c32f11b4a4a01473a
mattb.near <mattb.near>
2024-02-03 15:02:47 +0000

    Update Frensly.Pages.Home by mattb.near at 2024-02-03T15:02:47.413Z

--- mattb.near/Frensly.Pages.Home/source_code.jsx
+++ mattb.near/Frensly.Pages.Home/source_code.jsx
@@ -11,11 +11,11 @@ const Sections = styled.div`
 
 return (
   <>
-    <Widget src="mattb.near/widget/Frensly.Components.Header" />
+    <Widget src="mattb.near/widget/Frensly.Components.Header" loading="Loading..." />
 
     <Sections>
-      <Widget src="mattb.near/widget/Frensly.Components.RecentlyVerified" />
-      <Widget src="mattb.near/widget/Frensly.Components.HowToJoin" />
+      <Widget src="mattb.near/widget/Frensly.Components.RecentlyVerified" loading="Loading..." />
+      <Widget src="mattb.near/widget/Frensly.Components.HowToJoin" loading="Loading..." />
     </Sections>
   </>
 );
