05924d9951825a548722236c6d565a21045c1513
abnakore.near <abnakore.near>
2024-02-04 13:44:57 +0000

    Update TNESideBar by abnakore.near at 2024-02-04T13:44:57.737Z

--- abnakore.near/TNESideBar/source_code.jsx
+++ abnakore.near/TNESideBar/source_code.jsx
@@ -114,7 +114,7 @@ const StyledDiv = styled.div`
     padding: 1.5rem;
     background-image: linear-gradient(to top left, var(--tw-gradient-stops));
     width: 66.666667%;
-    position: inherit;
+    position: relative;
     transition: all 0.3s ease-in-out;
 
 
