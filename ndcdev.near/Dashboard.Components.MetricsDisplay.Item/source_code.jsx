1ad91c45d71ee44d2c6bdd69b6f65e98c4924ea7
ndcdev.near <ndcdev.near>
2024-02-05 16:53:59 +0000

    Update Dashboard.Components.MetricsDisplay.Item by ndcdev.near at 2024-02-05T16:53:59.348Z

--- ndcdev.near/Dashboard.Components.MetricsDisplay.Item/source_code.jsx
+++ ndcdev.near/Dashboard.Components.MetricsDisplay.Item/source_code.jsx
@@ -6,14 +6,17 @@ const { Circle } = VM.require(
 if (!Circle) return <Widget src="flashui.near/widget/Loading" />;
 const Loading = () => <Widget src="flashui.near/widget/Loading" />;
 
-const formatValue = (val) =>
-  val >= 1000000000
-    ? `${parseFloat(val / 1000000000).toFixed(2)}B`
-    : val >= 1000000
-    ? `${parseFloat(val / 1000000).toFixed(2)}M`
-    : val >= 1000
-    ? `${parseFloat(val / 1000).toFixed(2)}K`
-    : val;
+const formatValue = (val) => {
+  const value = val ?? 0;
+
+  return value >= 1000000000
+    ? `${parseFloat(value / 1000000000).toFixed(2)}B`
+    : value >= 1000000
+    ? `${parseFloat(value / 1000000).toFixed(2)}M`
+    : value >= 1000
+    ? `${parseFloat(value / 1000).toFixed(2)}K`
+    : value;
+};
 
 return (
   <div className="item">
