d09f32513df6f525079c7eccc366a7fa2aab63c2
thomasguntenaar.near <thomasguntenaar.near>
2024-02-06 20:06:32 +0000

    Update devhub.components.molecule.MarkdownEditor by thomasguntenaar.near at 2024-02-06T20:06:32.109Z

--- thomasguntenaar.near/devhub.components.molecule.MarkdownEditor/source_code.jsx
+++ thomasguntenaar.near/devhub.components.molecule.MarkdownEditor/source_code.jsx
@@ -1,7 +1,7 @@
 const MarkdownEditor = ({ data, onChange }) => {
   return (
     <Widget
-      src={"${REPL_DEVHUB}/widget/devhub.components.molecule.SimpleMDE"}
+      src={"thomasguntenaar.near/widget/devhub.components.molecule.SimpleMDE"}
       props={{
         data,
         onChange,
