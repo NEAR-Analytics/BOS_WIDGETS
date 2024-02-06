2a011ac3cc59e42e5542b31417f676132d48bb2d
astraplusplus.ndctools.near <astraplusplus.ndctools.near>
2024-02-06 10:58:32 +0000

    Update DAO.Proposal.Create.UpdateBonds by astraplusplus.ndctools.near at 2024-02-06T10:58:32.513Z

--- astraplusplus.ndctools.near/DAO.Proposal.Create.UpdateBonds/source_code.jsx
+++ astraplusplus.ndctools.near/DAO.Proposal.Create.UpdateBonds/source_code.jsx
@@ -142,13 +142,11 @@ return (
     <div className="mb-3">
       <h5>Proposal Description</h5>
       <Widget
-        src={"devhub.near/widget/devhub.components.molecule.Compose"}
+        src="sking.near/widget/Common.Inputs.Markdown"
         props={{
-          data: state.description,
-          onChange: onChangeDescription,
-          autocompleteEnabled: true,
-          autoFocus: false,
-          placeholder: defaultDescription
+          onChange: (value) => onChangeDescription(value),
+          height: "270px",
+          initialText: defaultDescription
         }}
       />
     </div>
@@ -166,7 +164,7 @@ return (
     {state.error && <div className="text-danger">{state.error}</div>}
     <div className="ms-auto">
       <Widget
-        src="astraplusplus.ndctools.near/widget/Common.Components.Button"
+        src="sking.near/widget/Common.Button"
         props={{
           children: "Create Proposal",
           onClick: handleProposal,
@@ -176,7 +174,7 @@ return (
       />
       {onClose && (
         <Widget
-          src="astraplusplus.ndctools.near/widget/Common.Components.Button"
+          src="sking.near/widget/Common.Button"
           props={{
             children: "Close",
             onClick: onClose,
