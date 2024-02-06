603d99e24110d0e4e58a05d0a603481aefcdf67f
sayalot.near <sayalot.near>
2024-02-06 14:56:54 +0000

    Update NDC.CommentView by sayalot.near at 2024-02-06T14:56:54.104Z

--- sayalot.near/NDC.CommentView/source_code.jsx
+++ sayalot.near/NDC.CommentView/source_code.jsx
@@ -21,260 +21,304 @@ State.init({
 });
 
 const CommentCard = styled.div`
-  margin-left: ${isReply ? "2rem" : "0"};
-  display: flex;
-  padding: 14px 16px;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 12px;
-  border-radius: "10px"};
-  background: ${
-    sharedCommentId === data.originalComment.value.comment.commentId
-      ? "rgba(194, 205, 255, 0.8)"
-      : "#fff"
-  };
-  width: 100%;
-`;
+    margin-left: ${isReply ? "2rem" : "0"};
+    display: flex;
+    padding: 14px 16px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 12px;
+    border-radius: "10px"};
+    background: ${
+      sharedCommentId === data.value.comment.commentId
+        ? "rgba(194, 205, 255, 0.8)"
+        : "#fff"
+    };
+    width: 100%;
+  `;
 
 const CommentCardHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 8px;
-`;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 8px;
+    width: 100%;
+  `;
 
 const CommentUserContent = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-`;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  `;
+
+const CommentEdition = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  `;
 
 const ProfileImageComment = styled.img`
-  width: 20px;
-  height: 20px;
-  flex-shrink: 0;
-  border-radius: 20px;
-`;
+    width: 20px;
+    height: 20px;
+    flex-shrink: 0;
+    border-radius: 20px;
+  `;
 
 const CommentUser = styled.p`
-  color: #000;
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 120%;
-  margin: 0px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
+    color: #000;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 120%;
+    margin: 0px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  `;
 
 const ReplyCounterDiv = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 4px;
-`;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  `;
 
 const ReplyIconPurple = styled.img`
-  width: 14px;
-  height: 14px;
-`;
+    width: 14px;
+    height: 14px;
+  `;
 
 const ReplyCounterText = styled.p`
-  color: #000;
-  font-size: 10px;
-  font-weight: 500;
-  margin: 0px;
-`;
+    color: #000;
+    font-size: 10px;
+    font-weight: 500;
+    margin: 0px;
+  `;
 
 const CommentCardContent = styled.p`
-  color: #585b5c;
-  font-size: 12px;
-  line-height: 18px;
-  display: flex;
-  flex-direction: column;
-  margin: 0px;
-`;
+    color: #585b5c;
+    font-size: 12px;
+    line-height: 18px;
+    display: flex;
+    flex-direction: column;
+    margin: 0px;
+  `;
 
 const CommentCardLowerSection = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  gap: 4px;
-  width: 100%;
-`;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 4px;
+    width: 100%;
+  `;
 
 const TimestampCommentDiv = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 4px;
-  flex: 1 0 0;
-`;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    flex: 1 0 0;
+  `;
 
 const TimestampIconComment = styled.img`
-  width: 12px;
-  height: 12px;
-`;
+    width: 12px;
+    height: 12px;
+  `;
 
-const TimestampTextComment = styled.p`
-  color: #000;
-  font-size: 10px;
-  font-weight: 300;
-  margin: 0px;
+const TimestampTextComment = styled.span`
+    color: #000;
+    font-size: 10px;
+    font-weight: 300;
+    margin: 0px;
+  `;
+
+const EditedIndication = styled.span`
+    font-size: 12px;
+    margin: 0px;
 `;
 
 const DeleteCommentButton = styled.button`
-  display: flex;
-  width: 28px;
-  padding: 2px 12px;
-  justify-content: center;
-  align-items: center;
-  gap: 6px;
-  align-self: stretch;
-  border-radius: 4px;
-  border: 1px solid #c23f38;
-  background: #f1d6d5;
-`;
+    display: flex;
+    width: 28px;
+    padding: 2px 12px;
+    justify-content: center;
+    align-items: center;
+    gap: 6px;
+    align-self: stretch;
+    border-radius: 4px;
+    border: 1px solid #c23f38;
+    background: #f1d6d5;
+  `;
 
 const DeleteCommentIcon = styled.img`
-  width: 14px;
-  height: 14px;
-  flex-shrink: 0;
-`;
+    width: 14px;
+    height: 14px;
+    flex-shrink: 0;
+  `;
 
 const ShareCommentButton = styled.button`
-  display: flex;
-  width: 28px;
-  height: 28px;
-  padding: 2px 12px;
-  justify-content: center;
-  align-items: center;
-  gap: 6px;
-  border-radius: 4px;
-  border: solid 1px transparent;
-  background-image: linear-gradient(white, white),
-    radial-gradient(circle at top left, #9333ea 0%, #4f46e5 100%);
-  background-origin: border-box;
-  background-clip: padding-box, border-box;
-`;
+    display: flex;
+    width: 28px;
+    height: 28px;
+    padding: 2px 12px;
+    justify-content: center;
+    align-items: center;
+    gap: 6px;
+    border-radius: 4px;
+    border: solid 1px transparent;
+    background-image: linear-gradient(white, white),
+      radial-gradient(circle at top left, #9333ea 0%, #4f46e5 100%);
+    background-origin: border-box;
+    background-clip: padding-box, border-box;
+  `;
 
 const ShareCommentIcon = styled.img`
-  width: 14px;
-  height: 14px;
-  flex-shrink: 0;
-`;
+    width: 14px;
+    height: 14px;
+    flex-shrink: 0;
+  `;
 
 const ReplyCommentButtonDisabled = styled.div`
-  display: flex;
-  padding: 2px 12px;
-  cursor: not-allowed;
-  background: rgb(195, 202, 206);
-  color: rgb(130, 134, 136);
-  border: 0px;
-  align-items: center;
-  gap: 6px;
-  align-self: stretch;
-  border-radius: 4px;
-`;
+    display: flex;
+    padding: 2px 12px;
+    cursor: not-allowed;
+    background: rgb(195, 202, 206);
+    color: rgb(130, 134, 136);
+    border: 0px;
+    align-items: center;
+    gap: 6px;
+    align-self: stretch;
+    border-radius: 4px;
+  `;
 
 const ReplyCommentButtonActive = styled.div`
-  cursor: pointer;
-  display: flex;
-  padding: 2px 12px;
-  align-items: center;
-  gap: 6px;
-  align-self: stretch;
-  border-radius: 4px;
-  background: var(--buttons-yellow-default, #ffd50d);
-`;
+    cursor: pointer;
+    display: flex;
+    padding: 2px 12px;
+    align-items: center;
+    gap: 6px;
+    align-self: stretch;
+    border-radius: 4px;
+    background: var(--buttons-yellow-default, #ffd50d);
+  `;
 
 const ReplyCommentText = styled.p`
-  color: var(--primary-black, #000);
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 24px;
-  margin: 0px;
-`;
+    color: var(--primary-black, #000);
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 24px;
+    margin: 0px;
+  `;
 
 const CommentReplySeparator = styled.hr`
-  height: 0px;
-  margin: 16px 0 16px 0;
-  border: 1px solid rgba(208, 214, 217, 1);
-`;
+    height: 0px;
+    margin: 16px 0 16px 0;
+    border: 1px solid rgba(208, 214, 217, 1);
+  `;
 
 const ReplyContainer = styled.div`
-  display: flex;
-  width: 260px;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 12px;
-  margin: 0 0 0 35px;
-`;
+    display: flex;
+    width: 260px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 12px;
+    margin: 0 0 0 35px;
+  `;
 
 const ReplyHeader = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  align-self: stretch;
-`;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    align-self: stretch;
+  `;
 
 const ReplyContent = styled.p`
-  color: #828688;
-  font-size: 12px;
-  line-height: 120%;
-  margin: 0px;
-`;
+    color: #828688;
+    font-size: 12px;
+    line-height: 120%;
+    margin: 0px;
+  `;
 
 const ReplyLowerSection = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 4px;
-`;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+  `;
 
 const ReplyButtonSection = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  gap: 4px;
-`;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 4px;
+  `;
 
 const DeleteReplyButton = styled.button`
-  display: flex;
-  padding: 2px 12px;
-  align-items: center;
-  gap: 6px;
-  align-self: stretch;
-  border-radius: 4px;
-  border: 1px solid #c23f38;
-  background: #f1d6d5;
-`;
+    display: flex;
+    padding: 2px 12px;
+    align-items: center;
+    gap: 6px;
+    align-self: stretch;
+    border-radius: 4px;
+    border: 1px solid #c23f38;
+    background: #f1d6d5;
+  `;
 
 const DeleteReplyText = styled.p`
-  color: #c23f38;
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 24px;
-  margin: 0px;
-`;
+    color: #c23f38;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 24px;
+    margin: 0px;
+  `;
 
 const AnswerContainer = styled.div`
-  width: 96%;;
-`;
+    width: 96%;;
+  `;
 
 function closeModal() {
   State.update({ showModal: false });
 }
 
+function getProperRootId(isEdition) {
+  if (isEdition) {
+    return data.value.comment.rootId;
+  }
+
+  if (data.answers) {
+    return data.value.comment.commentId;
+  } else {
+    return data.value.comment.rootId;
+  }
+}
+
+function handleEditComment() {
+  State.update({
+    showModal: true,
+    editionData: data,
+    rootId: getProperRootId(true),
+  });
+}
+
+function handleReplyListener() {
+  if (!canLoggedUserCreateComment) {
+    return;
+  }
+
+  State.update({
+    showModal: true,
+    editionData: undefined,
+    rootId: getProperRootId(false),
+  });
+}
+
 return (
   <>
-    <CommentCard id={data.originalComment.value.comment.commentId}>
+    <CommentCard id={data.value.comment.commentId}>
       <CommentCardHeader>
         <CommentUserContent>
           <Widget
-            src={widgets.newStyledComponents.Element.User}
+            src={widgets.views.standardWidgets.newStyledComponents.Element.User}
             props={{
-              accountId: data.originalComment.accountId,
+              accountId: data.accountId,
               options: {
                 showHumanBadge: true,
                 showImage: true,
@@ -285,43 +329,84 @@ return (
             }}
           />
         </CommentUserContent>
+
+        {context.accountId == data.accountId && (
+          <CommentEdition>
+            <Widget
+              src={
+                widgets.views.standardWidgets.newStyledComponents.Input.Button
+              }
+              props={{
+                children: (
+                  <div className="d-flex justify-content-center align-items-center">
+                    <span className="mx-2">Edit</span>
+                    <i className="bi bi-pencil"></i>
+                  </div>
+                ),
+                className: `info outline mt-2`,
+                onClick: () => handleEditComment(),
+              }}
+            />
+          </CommentEdition>
+        )}
       </CommentCardHeader>
       <CommentCardContent>
         <Widget
-          src={widgets.socialMarkdown}
-          props={{ text: data.originalComment.value.comment.text }}
+          src={widgets.views.standardWidgets.socialMarkdown}
+          props={{
+            text: data.value.comment.text,
+            onHashtag: (hashtag) => (
+              <span
+                key={hashtag}
+                className="d-inline-flex"
+                style={{ fontWeight: 500 }}
+              >
+                <a
+                  href={`https://near.social/${authorForWidget}/widget/${widgets.thisForum}?tagShared=${hashtag}`}
+                  target="_blank"
+                >
+                  #{hashtag}
+                </a>
+              </span>
+            ),
+          }}
         />
       </CommentCardContent>
       <CommentCardLowerSection>
         <TimestampCommentDiv>
           <i className="bi bi-clock" />
           <TimestampTextComment>
-            {new Date(
-              data.originalComment.value.comment.timestamp
-            ).toDateString()}
+            {new Date(data.value.comment.timestamp).toDateString()}
           </TimestampTextComment>
+          {data.isEdition && (
+            <EditedIndication className="text-muted">(edited)</EditedIndication>
+          )}
         </TimestampCommentDiv>
         <div>
           {state.showModal && (
             <Widget
-              src={widgets.addComment}
+              src={widgets.views.editableWidgets.addComment}
               props={{
                 article: articleToRenderData,
-                originalComment: orginalCommentData ?? data,
+                originalComment: data,
                 widgets,
                 isTest,
-                replyingTo: data.originalComment.accountId,
+                replyingTo: data.accountId,
                 placement: "bottom",
                 onCloseModal: closeModal,
                 callLibs,
                 baseActions,
+                editionData: state.editionData,
+                rootCommentId: state.rootId,
               }}
             />
           )}
           {articleSbts.length > 0 && (
             <>
               <Widget
-                src={widgets.newStyledComponents.Input.Button}
+                src={
+                  widgets.views.standardWidgets.newStyledComponents.Input.Button
+                }
                 props={{
                   children: (
                     <div className="d-flex align-items-center justify-content-center">
@@ -332,22 +417,19 @@ return (
                   disabled: !canLoggedUserCreateComment,
                   size: "sm",
                   className: "info outline",
-                  onClick: () => {
-                    canLoggedUserCreateComment &&
-                      State.update({ showModal: true });
-                  },
+                  onClick: handleReplyListener,
                 }}
               />
             </>
           )}
         </div>
         <Widget
-          src={widgets.reactions}
+          src={widgets.views.editableWidgets.reactions}
           props={{
             widgets,
             isTest,
             authorForWidget,
-            elementReactedId: data.originalComment.value.comment.commentId,
+            elementReactedId: data.value.comment.commentId,
             disabled: !canLoggedUserCreateComment,
             callLibs,
             baseActions,
@@ -365,10 +447,10 @@ return (
           return (
             <AnswerContainer>
               <Widget
-                src={widgets.commentView}
+                src={widgets.views.editableWidgets.commentView}
                 props={{
                   widgets,
-                  data: { originalComment: answer },
+                  data: answer,
                   orginalCommentData: data,
                   isTest,
                   authorForWidget,
