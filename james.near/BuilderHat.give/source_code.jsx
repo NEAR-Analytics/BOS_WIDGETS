5d40b4b010b48ee8a3761ace3ba2e98e78af829a
james.near <james.near>
2024-02-07 17:58:03 +0000

    Update BuilderHat.give by james.near at 2024-02-07T17:58:03.053Z

--- james.near/BuilderHat.give/source_code.jsx
+++ james.near/BuilderHat.give/source_code.jsx
@@ -94,7 +94,7 @@ return (
                 {
                   badge: {
                     builder: {
-                      [accountId]: hatColor,
+                      [recipient]: hatColor,
                     },
                   },
                 },
