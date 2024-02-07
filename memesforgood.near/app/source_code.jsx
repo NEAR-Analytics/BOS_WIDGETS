e60cf42d4078bbfd272aaf2b4b31a093d8631539
memesforgood.near <memesforgood.near>
2024-02-07 13:15:52 +0000

    Update app by memesforgood.near at 2024-02-07T13:15:52.150Z

---
+++ memesforgood.near/app/source_code.jsx
@@ -0,0 +1,19 @@
+return (
+  <div>
+    <h1>Memes for Good</h1>
+    <p>
+      Memes for Good Campaign is a community initiative designed to leverage
+      memes, tokens, NFT collections to incentivize donations to public goods.
+    </p>
+    <p>‘Impact = jokes“</p>
+    <h2>How to Get Involved</h2>
+    <h2>Case Studies</h2>
+    <a href="https://x.com/memesforgood" target="_blank">
+      Twitter
+    </a>{" "}
+    |
+    <a href="https://yearofchef.org/telegram" target="_blank">
+      Chat
+    </a>
+  </div>
+);
