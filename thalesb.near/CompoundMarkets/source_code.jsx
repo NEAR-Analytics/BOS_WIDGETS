3522d823a2009f3e0c7a730210bdff1442c0d8d0
thalesb.near <thalesb.near>
2024-02-07 20:34:55 +0000

    Update CompoundMarkets by thalesb.near at 2024-02-07T20:34:55.962Z

--- thalesb.near/CompoundMarkets/source_code.jsx
+++ thalesb.near/CompoundMarkets/source_code.jsx
@@ -6,7 +6,7 @@ State.init({
 if (!props.contracts) return "No contracts provided";
 
 const abi = fetch(
-  "https://docs.compound.finance/public/files/comet-interface-abi-98f438b.json",
+  "https://docs.compound.finance/public/files/comet-interface-abi-98f438b.json"
 );
 
 if (!abi) return "Loading...";
@@ -14,7 +14,7 @@ if (!abi) return "Loading...";
 const rewardsData = useCache(
   () =>
     asyncFetch(
-      "https://v3-api.compound.finance/market/all-networks/all-contracts/rewards/dapp-data",
+      "https://v3-api.compound.finance/market/all-networks/all-contracts/rewards/dapp-data"
     ).then((res) => {
       const rewards = JSON.parse(res.body);
 
@@ -30,7 +30,7 @@ const rewardsData = useCache(
       return rewardsByCometAddress;
     }),
   "rewardData",
-  { subscribe: false },
+  { subscribe: false }
 );
 
 const ChainNameByChainId = {
@@ -42,11 +42,21 @@ const ChainNameByChainId = {
 
 const ChainIconByChainId = {};
 
+/**
+ * Formats a value as a percentage.
+ * @param {number} value - The value to be formatted.
+ * @returns {string} The formatted value as a percentage.
+ */
 const percentFormatter = (value) => {
   if (!value) return "0%";
   return `${value.toFixed(2)}%`;
 };
 
+/**
+ * Formats a price value.
+ * @param {number} value - The price value to be formatted.
+ * @returns {string} The formatted price value.
+ */
 const priceFormatter = (value) => {
   if (!value) return "-";
   if (value > 1000000000) {
@@ -81,7 +91,7 @@ const coingeckoIds = contracts.reduce((acc, contract) => {
 const coingeckoIdsString = coingeckoIds.join(",");
 
 const coingeckoPrices = fetch(
-  `https://api.coingecko.com/api/v3/simple/price?ids=${coingeckoIdsString}&vs_currencies=usd`,
+  `https://api.coingecko.com/api/v3/simple/price?ids=${coingeckoIdsString}&vs_currencies=usd`
 );
 
 if (!coingeckoPrices) return "Loading...";
@@ -93,13 +103,13 @@ const secondsPerYear = 60 * 60 * 24 * 365;
 
 for (let contractInfo of contracts) {
   const rpcProvider = new ethers.providers.JsonRpcProvider(
-    contractInfo.httpRpcUrl,
+    contractInfo.httpRpcUrl
   );
 
   const contract = new ethers.Contract(
     contractInfo.address,
     abi.body,
-    rpcProvider,
+    rpcProvider
   );
 
   const name = contractInfo.network;
@@ -175,7 +185,7 @@ for (let contractInfo of contracts) {
     const collateralPromise = new Promise((resolve, reject) => {
       contract.totalsCollateral(collateralAsset.address).then((res) => {
         const totalSupplyAsset = Number(
-          ethers.utils.formatUnits(res[0], collateralAsset.decimals),
+          ethers.utils.formatUnits(res[0], collateralAsset.decimals)
         );
 
         const totalSupplyAssetPrice =
@@ -200,13 +210,13 @@ for (let contractInfo of contracts) {
 const marketsData = useCache(
   () => Promise.all(marketDataPromises),
   "marketsData",
-  { subscribe: false },
+  { subscribe: false }
 );
 
 const collateralPricesData = useCache(
   () => Promise.all(collateralPricesPromises),
   "collateralPricesData",
-  { subscribe: false },
+  { subscribe: false }
 );
 
 const markets = (marketsData ?? [])?.reduce((acc, item) => {
@@ -429,7 +439,48 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: var(--spacing-8);
+  padding: var(--spacing-6);
 `;
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: var(--spacing-8);
+`;
+
+if (!Object.values(markets)?.length) {
+  return (
+    <Theme>
+      <LoadingContainer className="font-primary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="4em"
+          height="4em"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="white"
+            d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+            opacity=".25"
+          />
+          <path
+            fill="white"
+            d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+          >
+            <animateTransform
+              attributeName="transform"
+              dur="0.75s"
+              repeatCount="indefinite"
+              type="rotate"
+              values="0 12 12;360 12 12"
+            />
+          </path>
+        </svg>
+      </LoadingContainer>
+    </Theme>
+  );
+}
 
 return (
   <Theme>
@@ -462,7 +513,7 @@ return (
                   {markets[network].map((market) => {
                     const totalCollateral = collateralPricesData
                       ?.filter(
-                        (item) => item.baseContractAddress === market.address,
+                        (item) => item.baseContractAddress === market.address
                       )
                       ?.reduce((total, item) => {
                         return total + item.totalSupplyAssetValue;
@@ -514,18 +565,33 @@ return (
                         <td>
                           {percentFormatter(
                             market.supplyApr +
-                              Number(rewards?.earn_rewards_apr || 0) * 100,
+                              Number(rewards?.earn_rewards_apr || 0) * 100
                           )}
                         </td>
                         <td>
                           {percentFormatter(
                             market.borrowApr -
-                              Number(rewards?.borrow_rewards_apr || 0) * 100,
+                              Number(rewards?.borrow_rewards_apr || 0) * 100
                           )}
                         </td>
-                        <td>{priceFormatter(market.totalEarning)}</td>
-                        <td>{priceFormatter(market.totalBorrow)}</td>
-                        <td>{priceFormatter(totalCollateral)}</td>
+                        <td>
+                          {network === "Base" &&
+                          market.icons.collateralIcons.length === 1
+                            ? "-"
+                            : priceFormatter(market.totalEarning)}
+                        </td>
+                        <td>
+                          {network === "Base" &&
+                          market.icons.collateralIcons.length === 1
+                            ? "-"
+                            : priceFormatter(market.totalBorrow)}
+                        </td>
+                        <td>
+                          {network === "Base" &&
+                          market.icons.collateralIcons.length === 2
+                            ? "-"
+                            : priceFormatter(totalCollateral)}
+                        </td>
                         <td style={{ display: "flex", gap: "1px" }}>
                           <span className="chip">
                             {market.icons.collateralIcons.length}
@@ -600,9 +666,9 @@ return (
                             market.supplyApr +
                               Number(
                                 rewardsData[market.address][0]
-                                  ?.earn_rewards_apr || 0,
+                                  ?.earn_rewards_apr || 0
                               ) *
-                                100,
+                                100
                           )}
                         </span>
                       </div>
@@ -614,9 +680,9 @@ return (
                             market.borrowApr -
                               Number(
                                 rewardsData[market.address][0]
-                                  ?.borrow_rewards_apr || 0,
+                                  ?.borrow_rewards_apr || 0
                               ) *
-                                100,
+                                100
                           )}
                         </span>
                       </div>
@@ -646,11 +712,11 @@ return (
                             collateralPricesData
                               ?.filter(
                                 (item) =>
-                                  item.baseContractAddress === market.address,
+                                  item.baseContractAddress === market.address
                               )
                               ?.reduce((total, item) => {
                                 return total + item.totalSupplyAssetValue;
-                              }, 0),
+                              }, 0)
                           )}
                         </span>
                       </div>
