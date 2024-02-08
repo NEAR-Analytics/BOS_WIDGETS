b3f5d497f45c88ecaa54bb3a15a69561652a373e
memiko.near <memiko.near>
2024-02-08 12:15:56 +0000

    Update mem.Layouts.App by memiko.near at 2024-02-08T12:15:56.672Z

--- memiko.near/mem.Layouts.App/source_code.jsx
+++ memiko.near/mem.Layouts.App/source_code.jsx
@@ -1,59 +1,8 @@
-const { Footer } = VM.require(`memiko.near/widget/mem.Components.Footer`);
-
-let fontCss = fetch(
-  "https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,600;1,600&display=swap",
-);
-
-if (!fontCss) {
-  function AppLayout() {
-    return <></>;
-  }
-  return { AppLayout };
-}
-fontCss = fontCss.body;
-
-const Theme = styled.div`
-  font-weight: 500;
-
-  ${fontCss}
-
-  font-style: normal;
-  font-size: 1.2rem;
-
-  h1 {
-    font-size: 2.7rem;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  .title-font {
-    margin: 0;
-    font-family: "Exo 2", sans-serif;
-    font-optical-sizing: auto;
-    font-weight: 600;
-    font-style: normal;
-  }
-`;
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
-const Content = styled.div`
-  width: 100%;
-`;
-
 function AppLayout({ pageProps, children }) {
   return (
     <Theme>
       <Container>
         <Content>{children}</Content>
-        <Footer />
       </Container>
     </Theme>
   );
