da3507661d620bfcb5da25cc29c943664648d8cf
flowscience.near <flowscience.near>
2024-02-08 01:25:24 +0000

    Update schema.editor by flowscience.near at 2024-02-08T01:25:24.008Z

--- flowscience.near/schema.editor/source_code.jsx
+++ flowscience.near/schema.editor/source_code.jsx
@@ -1,11 +1,26 @@
 const typeSrc = props.typeSrc || "";
 const blockHeight = props.blockHeight || "final";
 const selectedSchema = props.selectedSchema;
+const schemaUID = props.schemaUID;
 let type = {
   name: "",
   properties: [],
   widgets: {},
 };
+
+function generateUID() {
+  const length = 42; // This will create a 168-bit number (42 hexadecimal characters)
+  // randomly change the case of a character
+  const randomCase = (char) =>
+    Math.random() > 0.5 ? char.toUpperCase() : char.toLowerCase();
+  // Generate a random hexadecimal number and convert it to a string
+  let uid = "";
+  for (let i = 0; i < length; i++) {
+    const randomChar = Math.floor(Math.random() * 16).toString(16); // Generate a single random hexadecimal character
+    uid += randomCase(randomChar); // Append the character in either case to the UID
+  }
+  return uid;
+}
 
 State.init({
   newType: typeSrc,
@@ -19,7 +34,7 @@ State.init({
   newTypeSrc: "",
   typeSrc: "hyperfiles.near",
   expanded: false,
-  schemaUID: "",
+  schemaUID: props.schemaUID,
 });
 
 let importedTypes = [];
@@ -180,6 +195,7 @@ const composeData = () => {
           type: resolverType,
           data: resolverData, // Include the accountIds for the attester resolver
         },
+        schemaUID: generateUID(),
       }),
     },
   };
