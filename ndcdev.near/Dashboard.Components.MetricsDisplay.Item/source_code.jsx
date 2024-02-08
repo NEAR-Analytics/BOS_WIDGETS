a078fad3bdcf2a44ae29c7ab9b7b85924d15e86b
ndcdev.near <ndcdev.near>
2024-02-08 08:04:12 +0000

    Update Dashboard.Components.MetricsDisplay.Item by ndcdev.near at 2024-02-08T08:04:12.021Z

--- ndcdev.near/Dashboard.Components.MetricsDisplay.Item/source_code.jsx
+++ ndcdev.near/Dashboard.Components.MetricsDisplay.Item/source_code.jsx
@@ -1,28 +1,31 @@
-const { value, text, color } = props;
+const { value, text, color, loading } = props;
 const { Circle } = VM.require(
   `ndcdev.near/widget/Dashboard.Components.MetricsDisplay.styled`,
 );
 
-if (!Circle) return <Widget src="flashui.near/widget/Loading" />;
 const Loading = () => <Widget src="flashui.near/widget/Loading" />;
+if (!Circle) return <Loading />;
 
-const formatValue = (value) =>
-  value
-    ? value >= 1000000000
-      ? `${parseFloat(value / 1000000000).toFixed(2)}B`
-      : value >= 1000000
-      ? `${parseFloat(value / 1000000).toFixed(2)}M`
-      : value >= 1000
-      ? `${parseFloat(value / 1000).toFixed(2)}K`
-      : Number.isInteger(value)
-      ? value
-      : value.toFixed(2)
-    : "﹣";
+const formatValue = (value) => {
+  const val = value ? parseFloat(value) : null;
+
+  if (!val) return "n/a";
+
+  return val >= 1000000000
+    ? `${parseFloat(val / 1000000000).toFixed(2)}B`
+    : val >= 1000000
+    ? `${parseFloat(val / 1000000).toFixed(2)}M`
+    : val >= 1000
+    ? `${parseFloat(val / 1000).toFixed(2)}K`
+    : Number.isInteger(val)
+    ? val
+    : val.toFixed(2);
+};
 
 return (
   <div className="item">
     <div className="inner">
-      {!value ? <Loading /> : <span>{formatValue(value)}</span>}
+      {loading ? <Loading /> : <span>{formatValue(value)}</span>}
     </div>
     <div className="d-flex justify-content-center align-items-center gap-2">
       <Circle color={color} /> <span>{text}</span>
