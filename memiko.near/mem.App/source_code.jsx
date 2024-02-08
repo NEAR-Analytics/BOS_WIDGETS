edf1f923fb945976fb4147217f43b0ac6e4a2459
memiko.near <memiko.near>
2024-02-08 12:15:56 +0000

    Update mem.App by memiko.near at 2024-02-08T12:15:56.672Z

--- memiko.near/mem.App/source_code.jsx
+++ memiko.near/mem.App/source_code.jsx
@@ -1,48 +1,25 @@
 const { AppLayout } = VM.require(`memiko.near/widget/mem.Layouts.App`);
+const { Config } = VM.require(`./.Config.App`);
 
 const { page, ...passProps } = props;
 
 if (!AppLayout) return <Widget src="flashui.near/widget/Loading" />;
-if (!page) page = "home";
+if (!page) page = "index";
 
 const pageProps = {
   page,
-  contractName: "memiko.near",
+  contractName: Config.contract,
   ...passProps,
 };
 
 function Page() {
   switch (page) {
-    case "home": {
+    case "index": {
       return (
         <Widget
           src={`memiko.near/widget/mem.Pages.Index`}
           props={pageProps}
         />
-      );
-    }
-
-    default: {
-      const NotFound = styled.div`
-        width: 100%;
-        height: 90vh;
-
-        h1 {
-          font-size: 6rem;
-        }
-
-        h2 {
-          font-size: 1.2rem;
-        }
-      `;
-
-      return (
-        <NotFound className="justify-content-center align-items-center d-flex flex-grow-1 flex-column ">
-          <h1>
-            <b>404 / Not Found</b>
-          </h1>
-          <h2>Page not found</h2>
-        </NotFound>
       );
     }
   }
