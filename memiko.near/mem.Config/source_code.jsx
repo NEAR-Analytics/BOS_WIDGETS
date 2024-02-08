d9a7951cc5093788ad099e08fa29272135cae73d
memiko.near <memiko.near>
2024-02-08 04:48:26 +0000

    Update mem.Config by memiko.near at 2024-02-08T04:48:26.015Z

--- memiko.near/mem.Config/source_code.jsx
+++ memiko.near/mem.Config/source_code.jsx
@@ -1,11 +1,3 @@
 return {
-  contractName: "memiko.near",
-  assets: {
-    banner:
-      "https://bafybeihgspjbexzcuubc7w23dehnv4tm32ydgv6hbu4rqob2gfwmeazvjy.ipfs.nftstorage.link/1500x500-min.jpg",
-    tokenomics:
-      "https://bafybeicuxy342lm5f3vv3zuki7relrgecuaxhcicv4rh5rqeuvfxh7f3ga.ipfs.nftstorage.link/tokenomics.jpg",
-    logoNear:
-      "https://ipfs.near.social/ipfs/bafkreia4zgx4r2msygesk4rfspz6zezy5cqhf6bij7mef7ca2xruh5xpqe",
-  },
+  contract: "memiko.near",
 };
