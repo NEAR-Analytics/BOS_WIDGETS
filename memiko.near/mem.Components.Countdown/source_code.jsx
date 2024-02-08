8abb53b3217e53dbce7701fe3a4dfb2600b48a74
memiko.near <memiko.near>
2024-02-08 05:57:39 +0000

    Update mem.Components.Countdown by memiko.near at 2024-02-08T05:57:39.919Z

--- memiko.near/mem.Components.Countdown/source_code.jsx
+++ memiko.near/mem.Components.Countdown/source_code.jsx
@@ -1,1 +1,28 @@
-return <></>;
+const { startDate } = props;
+
+const [times, setTimes] = useState("** : ** : **");
+const [disabled, setDisabled] = useState(false);
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
+
+const timer = setInterval(() => {
+  const distance = startDate - new Date().getTime();
+  const seconds = Math.floor((distance / 1000) % 60);
+  const minutes = Math.floor((distance / 1000 / 60) % 60);
+  const hours = Math.floor(distance / (1000 * 60 * 60));
+
+  if (distance < 0) {
+    setTimes(0);
+    setDisabled(true);
+  } else {
+    setTimes(
+      `${formatTime(hours)} h : ${formatTime(minutes)} m : ${formatTime(
+        seconds,
+      )} s`,
+    );
+    setDisabled(false);
+  }
+
+  clearInterval(timer);
+}, 1000);
+
+return <h4 style={{ fontSize: "2rem" }}>{!disabled && times}</h4>;
