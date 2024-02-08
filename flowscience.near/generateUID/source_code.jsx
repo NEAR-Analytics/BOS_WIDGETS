d30dc4038a6200087121b5c19e313adff3a6ff5b
flowscience.near <flowscience.near>
2024-02-08 01:36:30 +0000

    Update generateUID by flowscience.near at 2024-02-08T01:36:30.645Z

---
+++ flowscience.near/generateUID/source_code.jsx
@@ -0,0 +1,13 @@
+function generateUID() {
+  const length = 42; // This will create a 168-bit number (42 hexadecimal characters)
+  // randomly change the case of a character
+  const randomCase = (char) =>
+    Math.random() > 0.5 ? char.toUpperCase() : char.toLowerCase();
+  // Generate a random hexadecimal number and convert it to a string
+  let uid = "";
+  for (let i = 0; i < length; i++) {
+    const randomChar = Math.floor(Math.random() * 16).toString(16); // Generate a single random hexadecimal character
+    uid += randomCase(randomChar); // Append the character in either case to the UID
+  }
+  return uid;
+}
