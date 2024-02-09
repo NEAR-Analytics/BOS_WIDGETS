2765c4a2b973a9ae5415be9362d46a4fb6c99d28
bos.dapplets.near <bos.dapplets.near>
2024-02-09 14:33:43 +0000

    Update Tipping.Main by bos.dapplets.near at 2024-02-09T14:33:43.397Z

--- bos.dapplets.near/Tipping.Main/source_code.jsx
+++ bos.dapplets.near/Tipping.Main/source_code.jsx
@@ -19,6 +19,8 @@ if (props.accountGId && props.itemGId) {
   return <></>;
 }
 
+const userAccountId = context.accountId || props.nearAccountId
+
 const TIPPING_CONTRACT_NAME = "v2.tipping.near";
 const MAX_AMOUNT_PER_ITEM = '10000000000000000000000000'; // 10 NEAR
 const MAX_AMOUNT_PER_TIP = '1000000000000000000000000'; // 1 NEAR
@@ -151,7 +153,15 @@ function calculateFee(num) {
 }
 
 useEffect(() => {
-  if (equals(state.totalTipsByItemId, '0')) {
+  if (!userAccountId) {
+    State.update({
+      accountId,
+      disabled: true,
+      loading: false,
+      amount: '0',
+      tooltip: 'Connect wallet',
+    })
+  } else if (equals(state.totalTipsByItemId, '0')) {
     State.update({
       accountId,
       disabled: false,
