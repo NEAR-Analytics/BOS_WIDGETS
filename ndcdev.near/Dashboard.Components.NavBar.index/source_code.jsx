37dfc4ac06a925ace7a01393aaa091dcdd3f4d51
ndcdev.near <ndcdev.near>
2024-02-09 09:59:03 +0000

    Update Dashboard.Components.NavBar.index by ndcdev.near at 2024-02-09T09:59:03.754Z

--- ndcdev.near/Dashboard.Components.NavBar.index/source_code.jsx
+++ ndcdev.near/Dashboard.Components.NavBar.index/source_code.jsx
@@ -5,14 +5,20 @@ const { NavBar } = VM.require(
 
 if (!assets || !NavBar) return <Widget src="flashui.near/widget/Loading" />;
 
+const HomeLink = styled.a`
+  :hover {
+    text-decoration: none;
+  }
+`
+
 return (
   <NavBar>
-    <a href={`/ndcdev.near/widget/Dashboard.App?page=home`}>
+    <HomeLink href={`/ndcdev.near/widget/Dashboard.App?page=home`}>
       <div className="d-flex gap-3 align-items-center">
         <img src={assets.logoWhite} />
-        <div className="header-text">NDC DASHBOARD</div>
+        <div className="header-text" >NDC DASHBOARD</div>
       </div>
-    </a>
+    </HomeLink>
     <div className="color-text">FOR PEOPLE BY PEOPLE</div>
   </NavBar>
 );
