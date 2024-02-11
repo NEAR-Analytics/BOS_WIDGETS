8e377ff74a879c616cc178666345dc7ee67d12a8
pivortex.near <pivortex.near>
2024-02-11 12:30:08 +0000

    Update GuestBook by pivortex.near at 2024-02-11T12:30:08.453Z

--- pivortex.near/GuestBook/source_code.jsx
+++ pivortex.near/GuestBook/source_code.jsx
@@ -1,76 +1,69 @@
-const Guestbook = (props) => {
-  const [accountId, setAccountId] = useState(
-    props.accountId || context.accountId
-  );
-  const [status, setStatus] = useState("");
-  const [messages, setMessages] = useState([]);
-  const contractId = "guestbook.workshopcamp.near";
+const contract = "guest-book.near";
+const messages = Near.view(contract, "getMessages", {}).reverse();
+console.log(messages);
 
-  useEffect(() => {
-    getMessages();
-  }, []); // Run only once on component mount
+State.init({
+  newMessage: "",
+});
 
-  const getMessages = () => {
-    const messages = Near.view(contractId, "get_messages", null);
-    setMessages(messages);
-  };
+const addNewMessage = () => {
+  if (state.newMessage.trim() == "") {
+    return;
+  }
 
-  const postMessage = () => {
-    if (!accountId) return;
+  Near.call(contract, "addMessage", {
+    text: state.newMessage,
+  });
+};
 
-    if (status === "") return;
-
-    Near.call(contractId, "add_message", {
-      text: status,
-    });
-
-    getMessages();
-    setStatus(""); // Clear the status after posting the message
-  };
-
-  return (
-    <div>
-      <div className="hero min-h-screen bg-primary">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold text-base-content">
-              BOS workshop
-            </h1>
-            <p className="mb-4 text-base-content">
-              Welcome to the GuestBook, add your message for everyone to see!
-            </p>
-            <p className="mb-4 text-base-content">
-              You are logged in as <b>{accountId}</b>
-            </p>
-            <div className="my-2">
-              <textarea
-                placeholder="Your status"
-                className="textarea textarea-bordered textarea-lg w-full max-w-xs"
-                value={status}
-                onChange={({ target: { value } }) => setStatus(value)}
-              ></textarea>
-            </div>
-            <button className="btn btn-primary mb-4" onClick={postMessage}>
-              Add a message
-            </button>
-
-            <div className="my-4">
-              {messages.map((status, index) => (
-                <div key={index} className="card bg-base-100 shadow-xl mb-4">
-                  <div className="card-body">
-                    <h2 className="card-title text-base-content">
-                      {status.text}
-                    </h2>
-                    <p className="text-left text-base-content">
-                      {status.sender}
-                    </p>
-                  </div>
-                </div>
-              ))}
-            </div>
+return (
+  <div class="p-3">
+    <h3 class="text-center">GuestBook</h3>
+    <br />
+    {context.accountId ? (
+      <div class="border border-black p-3">
+        <h3>New Message</h3>
+        <div class="row">
+          <div>
+            <input
+              placeholder="Message"
+              onChange={(e) => State.update({ newMessage: e.target.value })}
+            />
           </div>
         </div>
+        <button
+          class="btn btn-primary mt-2"
+          onClick={async () => {
+            addNewMessage();
+          }}
+        >
+          Guardar
+        </button>
       </div>
+    ) : (
+      <p class="text-center py-2">I DONT KNOW WHAT THIS MEANS</p>
+    )}
+    <br />
+    <div class="border border-black p-3">
+      <h3>Message List</h3>
+      <table className="table table-sm">
+        <thead>
+          <tr class="p-3 mb-2 bg-primary text-white text-center">
+            <th>Name</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {messages.map((data, key) => {
+            return (
+              <tr class="text-center">
+                <td>{data.sender}</td>
+                <td>{data.text}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  );
-};
+  </div>
+);
