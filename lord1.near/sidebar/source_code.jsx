36c82303ad2573b695a4ef441b529781ab0f4224
lord1.near <lord1.near>
2024-02-12 10:18:06 +0000

    Update sidebar by lord1.near at 2024-02-12T10:18:06.204Z

--- lord1.near/sidebar/source_code.jsx
+++ lord1.near/sidebar/source_code.jsx
@@ -12,8 +12,6 @@ const pageHandler = (target) => {
 };
 
 const sidebarHandler = () => {
-  console.log(state.showFullSide);
-
   State.update({
     showFullSide: !state.showFullSide,
   });
@@ -27,7 +25,10 @@ border-left:5px solid #fff;
 }
 
 `;
-const SideBarBody = styled.div` position:fixed;
+const SideBarBody = styled.div`
+visibility : ${state.showFullSide ? "visible" : "hidden"};
+opacity : ${state.showFullSide ? "1" : "0"};
+position:absolute;
 background: ${props.backgroundColor};
 display: flex;
 z-index:1;
@@ -37,11 +38,16 @@ align-items: center;
 align-self: stretch;
 border-radius:15px;
 transition:500ms all;
-width:${state.showFullSide ? "150px" : "80px"};
-height:85vh;
 -webkit-box-shadow: 0px 0px 14px -1px rgba(0, 0, 0, 0.8);
   box-shadow: 0px 0px 20px -1px rgba(210, 200, 250, 100);
+width:${state.showFullSide ? "150px" : "0"};
 
+@media (min-width: 576px) {
+width:${state.showFullSide ? "150px" : "80px"};
+visibility : visible;
+opacity : 1;
+position:relative;
+}
 `;
 const SidebarItems = styled.div` align-items:"center";
 justify-content:spacce-between;
@@ -80,39 +86,41 @@ a>div>span {
 
 `;
 
-const GrowSidebarBtn = styled.div` position:absolute;
-right:-12px;
+const GrowSidebarBtn = styled.div` 
+transition:500ms all;
+
+position:absolute;
+right:${state.showFullSide ? "calc(-2rem - 150px)" : "-3rem"};
 border-radius:50%;
 background-color:#fff;
 overflow:hidden;
-width:25px;
-padding:-5px;
+width:35px;
+height : 35px;
+display:flex;
 align-items:center;
-text-align:center;
+justify-content:center;
 cursor:pointer;
-top:20vh;
+top:1rem;
 box-shadow: 0px 0px 20px -1px rgba(210, 200, 250, 100);
+z-index: 2;
 
-
->svg {
-  margin-bottom: 4px
+@media (min-width: 576px) {
+right:-12px;
+top : 10vh;
+width:25px;
+height : 25px;
 }
-
 `;
-
 return (
-  <SideBarBody>
-    {" "}
+  <div>
     <GrowSidebarBtn onClick={sidebarHandler}>
-      {" "}
       {!state.showFullSide ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           height="1em"
           viewBox="0 0 448 512"
         >
-          {" "}
-          <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />{" "}
+          <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
         </svg>
       ) : (
         <svg
@@ -120,87 +128,73 @@ return (
           height="1em"
           viewBox="0 0 448 512"
         >
-          {" "}
-          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />{" "}
+          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
         </svg>
       )}
-    </GrowSidebarBtn>{" "}
-    <SidebarItems>
-      {" "}
-      <SidebarItem>
-        {" "}
-        <a href="https://flipsidecrypto.xyz/" target="_blank">
-          {" "}
-          <div>
-            {" "}
-            <img
-              width="50"
-              height="50"
-              src={props.headerIcon}
-              style={{
-                "box-shadow": "0 0px 20px rgba(1300, 60, 231, 20)",
-                "border-radius": "250px",
-              }}
-            />{" "}
-            <span> {props.headerText}</span>{" "}
-          </div>{" "}
-        </a>{" "}
-      </SidebarItem>{" "}
-    </SidebarItems>{" "}
-    <SidebarItems>
-      {" "}
-      {props.links.map((data, index) => {
-        return (
-          <>
-            {" "}
-            {state.wichPage === index ? (
-              <X>
-                {" "}
+    </GrowSidebarBtn>
+    <SideBarBody>
+      <SidebarItems>
+        <SidebarItem>
+          <a href="https://flipsidecrypto.xyz/" target="_blank">
+            <div>
+              <img
+                width="50"
+                height="50"
+                src={props.headerIcon}
+                style={{
+                  "box-shadow": "0 0px 20px rgba(1300, 60, 231, 20)",
+                  "border-radius": "250px",
+                }}
+              />
+              <span> {props.headerText}</span>
+            </div>
+          </a>
+        </SidebarItem>
+      </SidebarItems>
+      <SidebarItems>
+        {props.links.map((data, index) => {
+          return (
+            <>
+              {state.wichPage === index ? (
+                <X>
+                  <SidebarItem onClick={() => pageHandler(0)}>
+                    <a href={data.link}>
+                      <div>
+                        <img width="31" height="30" src={data.image} />
+                        <span> {data.text}</span>
+                      </div>
+                    </a>
+                  </SidebarItem>
+                </X>
+              ) : (
                 <SidebarItem onClick={() => pageHandler(0)}>
-                  {" "}
-                  <a href={data.link}>
-                    <div>
-                      {" "}
-                      <img width="31" height="30" src={data.image} />{" "}
-                      <span> {data.text}</span>{" "}
-                    </div>{" "}
-                  </a>{" "}
-                </SidebarItem>{" "}
-              </X>
-            ) : (
-              <SidebarItem onClick={() => pageHandler(0)}>
-                <OverlayTrigger
-                  placement="right"
-                  overlay={<Tooltip>{data.title}</Tooltip>}
-                >
-                  <a href={data.link}>
-                    {" "}
-                    <div>
-                      {" "}
-                      <img width="31" height="30" src={data.image} />{" "}
-                      <span> {data.text}</span>{" "}
-                    </div>{" "}
-                  </a>{" "}
-                </OverlayTrigger>
-              </SidebarItem>
-            )}
-          </>
-        );
-      })}
-    </SidebarItems>{" "}
-    <SidebarItems>
-      {" "}
-      <SidebarItem>
-        {" "}
-        <a href="test">
-          {" "}
-          <div>
-            {" "}
-            <img width="31" height="30" src={props.footerIcon} />{" "}
-            <span> {props.footerText}</span>{" "}
-          </div>{" "}
-        </a>{" "}
-      </SidebarItem>{" "}
-    </SidebarItems>{" "}
-  </SideBarBody>
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={<Tooltip>{data.title}</Tooltip>}
+                  >
+                    <a href={data.link}>
+                      <div>
+                        <img width="31" height="30" src={data.image} />
+                        <span> {data.text}</span>
+                      </div>
+                    </a>
+                  </OverlayTrigger>
+                </SidebarItem>
+              )}
+            </>
+          );
+        })}
+      </SidebarItems>
+      <SidebarItems>
+        <SidebarItem>
+          <a href="test">
+            <div>
+              <img width="31" height="30" src={props.footerIcon} />
+              <span> {props.footerText}</span>
+            </div>
+          </a>
+        </SidebarItem>
+      </SidebarItems>
+    </SideBarBody>
+  </div>
 );
