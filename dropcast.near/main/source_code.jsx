434ff94199b3c1e9097fccd8609dc56fcabf90d0
dropcast.near <dropcast.near>
2024-02-12 09:28:18 +0000

    Update main by dropcast.near at 2024-02-12T09:28:18.533Z

---
+++ dropcast.near/main/source_code.jsx
@@ -0,0 +1,16 @@
+const accountId = context.accountId;
+const Owner = "dropcast.near";
+const API_URL = "http://localhost:3000";
+//Styles
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  align-items: stretch;
+  flex-direction: column;
+  background: white;
+  overflow: auto;
+  position: relative;
+`;
+
+return <Wrapper className="root">main</Wrapper>;
