c5c3cd148363764fb56d834983b41ba97ead5029
mattb.near <mattb.near>
2024-02-12 22:39:16 +0000

    Update TestVmAsyncRequire by mattb.near at 2024-02-12T22:39:16.746Z

--- mattb.near/TestVmAsyncRequire/source_code.jsx
+++ mattb.near/TestVmAsyncRequire/source_code.jsx
@@ -1,1 +1,2 @@
-VM.asyncRequire("sdks.near/widget/Utils.CryptoJS").then((data) => console.log(data));
+
+return <button onClick={() => VM.asyncRequire("sdks.near/widget/Utils.CryptoJS").then((data) => console.log(data))}>Click me</button>;
