7338cab4dfcc621e82b8562b6a962b79de955ec2
nearblocks.near <nearblocks.near>
2024-02-12 06:11:45 +0000

    Update includes.near by nearblocks.near at 2024-02-12T06:11:45.346Z

--- nearblocks.near/includes.near/source_code.jsx
+++ nearblocks.near/includes.near/source_code.jsx
@@ -7,6 +7,22 @@ import { yoctoToNear } from '@/includes/libs';
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export function encodeArgs(args) {
   if (!args || typeof args === 'undefined') return '';
 
@@ -198,4 +214,609 @@ export function formatLine(line, offset, format) {
   }
 
   return result.trimEnd();
+}
+
+export function collectNestedReceiptWithOutcomeOld(
+  idOrHash,
+  parsedMap,
+) {
+  const parsedElement = parsedMap.get(idOrHash);
+  if (!parsedElement) {
+    return { id: idOrHash };
+  }
+  const { receiptIds, ...restOutcome } = parsedElement.outcome;
+  return {
+    ...parsedElement,
+    outcome: {
+      ...restOutcome,
+      nestedReceipts: receiptIds.map((id) =>
+        collectNestedReceiptWithOutcomeOld(id, parsedMap),
+      ),
+    },
+  };
+}
+
+export function parseReceipt(
+  receipt,
+  outcome,
+  transaction,
+) {
+  if (!receipt) {
+    return {
+      id: outcome.id,
+      predecessorId: transaction.signer_id,
+      receiverId: transaction.receiver_id,
+      actions: transaction.actions.map(mapRpcActionToAction1),
+    };
+  }
+  return {
+    id: receipt.receipt_id,
+    predecessorId: receipt.predecessor_id,
+    receiverId: receipt.receiver_id,
+    actions:
+      'Action' in receipt.receipt
+        ? receipt.receipt.Action.actions.map(mapRpcActionToAction1)
+        : [],
+  };
+}
+
+function mapNonDelegateRpcActionToAction(
+  rpcAction,
+) {
+  if (rpcAction === 'CreateAccount') {
+    return {
+      kind: 'createAccount',
+      args: {},
+    };
+  }
+  if ('DeployContract' in rpcAction) {
+    return {
+      kind: 'deployContract',
+      args: rpcAction.DeployContract,
+    };
+  }
+  if ('FunctionCall' in rpcAction) {
+    return {
+      kind: 'functionCall',
+      args: {
+        methodName: rpcAction.FunctionCall.method_name,
+        args: rpcAction.FunctionCall.args,
+        deposit: rpcAction.FunctionCall.deposit,
+        gas: rpcAction.FunctionCall.gas,
+      },
+    };
+  }
+  if ('Transfer' in rpcAction) {
+    return {
+      kind: 'transfer',
+      args: rpcAction.Transfer,
+    };
+  }
+  if ('Stake' in rpcAction) {
+    return {
+      kind: 'stake',
+      args: {
+        publicKey: rpcAction.Stake.public_key,
+        stake: rpcAction.Stake.stake,
+      },
+    };
+  }
+  if ('AddKey' in rpcAction) {
+    return {
+      kind: 'addKey',
+      args: {
+        publicKey: rpcAction.AddKey.public_key,
+        accessKey: {
+          nonce: rpcAction.AddKey.access_key.nonce,
+          permission:
+            rpcAction.AddKey.access_key.permission === 'FullAccess'
+              ? {
+                  type: 'fullAccess',
+                }
+              : {
+                  type: 'functionCall',
+                  contractId:
+                    rpcAction.AddKey.access_key.permission.FunctionCall
+                      .receiver_id,
+                  methodNames:
+                    rpcAction.AddKey.access_key.permission.FunctionCall
+                      .method_names,
+                },
+        },
+      },
+    };
+  }
+  if ('DeleteKey' in rpcAction) {
+    return {
+      kind: 'deleteKey',
+      args: {
+        publicKey: rpcAction.DeleteKey.public_key,
+      },
+    };
+  }
+  return {
+    kind: 'deleteAccount',
+    args: {
+      beneficiaryId: rpcAction.DeleteAccount.beneficiary_id,
+    },
+  };
+}
+function mapRpcInvalidAccessKeyError(error) {
+  const UNKNOWN_ERROR = { type: 'unknown' };
+
+  if (error === 'DepositWithFunctionCall') {
+    return {
+      type: 'depositWithFunctionCall',
+    };
+  }
+  if (error === 'RequiresFullAccess') {
+    return {
+      type: 'requiresFullAccess',
+    };
+  }
+  if ('AccessKeyNotFound' in error) {
+    const { account_id, public_key } = error.AccessKeyNotFound;
+    return {
+      type: 'accessKeyNotFound',
+      accountId: account_id,
+      publicKey: public_key,
+    };
+  }
+  if ('ReceiverMismatch' in error) {
+    const { ak_receiver, tx_receiver } = error.ReceiverMismatch;
+    return {
+      type: 'receiverMismatch',
+      akReceiver: ak_receiver,
+      transactionReceiver: tx_receiver,
+    };
+  }
+  if ('MethodNameMismatch' in error) {
+    const { method_name } = error.MethodNameMismatch;
+    return {
+      type: 'methodNameMismatch',
+      methodName: method_name,
+    };
+  }
+  if ('NotEnoughAllowance' in error) {
+    const { account_id, allowance, cost, public_key } =
+      error.NotEnoughAllowance;
+    return {
+      type: 'notEnoughAllowance',
+      accountId: account_id,
+      allowance: allowance,
+      cost: cost,
+      publicKey: public_key,
+    };
+  }
+
+  return UNKNOWN_ERROR;
+}
+
+function mapRpcCompilationError(error) {
+  const UNKNOWN_ERROR = { type: 'unknown' };
+  if ('CodeDoesNotExist' in error) {
+    return {
+      type: 'codeDoesNotExist',
+      accountId: error.CodeDoesNotExist.account_id,
+    };
+  }
+  if ('PrepareError' in error) {
+    return {
+      type: 'prepareError',
+    };
+  }
+  if ('WasmerCompileError' in error) {
+    return {
+      type: 'wasmerCompileError',
+      msg: error.WasmerCompileError.msg,
+    };
+  }
+  if ('UnsupportedCompiler' in error) {
+    return {
+      type: 'unsupportedCompiler',
+      msg: error.UnsupportedCompiler.msg,
+    };
+  }
+  return UNKNOWN_ERROR;
+}
+
+function mapRpcFunctionCallError(error) {
+  const UNKNOWN_ERROR = { type: 'unknown' };
+  if ('CompilationError' in error) {
+    return {
+      type: 'compilationError',
+      error: mapRpcCompilationError(error.CompilationError),
+    };
+  }
+  if ('LinkError' in error) {
+    return {
+      type: 'linkError',
+      msg: error.LinkError.msg,
+    };
+  }
+  if ('MethodResolveError' in error) {
+    return {
+      type: 'methodResolveError',
+    };
+  }
+  if ('WasmTrap' in error) {
+    return {
+      type: 'wasmTrap',
+    };
+  }
+  if ('WasmUnknownError' in error) {
+    return {
+      type: 'wasmUnknownError',
+    };
+  }
+  if ('HostError' in error) {
+    return {
+      type: 'hostError',
+    };
+  }
+  if ('_EVMError' in error) {
+    return {
+      type: 'evmError',
+    };
+  }
+  if ('ExecutionError' in error) {
+    return {
+      type: 'executionError',
+      error: error.ExecutionError,
+    };
+  }
+  return UNKNOWN_ERROR;
+}
+function mapRpcNewReceiptValidationError(
+  error,
+) {
+  const UNKNOWN_ERROR = { type: 'unknown' };
+  if ('InvalidPredecessorId' in error) {
+    return {
+      type: 'invalidPredecessorId',
+      accountId: error.InvalidPredecessorId.account_id,
+    };
+  }
+  if ('InvalidReceiverId' in error) {
+    return {
+      type: 'invalidReceiverId',
+      accountId: error.InvalidReceiverId.account_id,
+    };
+  }
+  if ('InvalidSignerId' in error) {
+    return {
+      type: 'invalidSignerId',
+      accountId: error.InvalidSignerId.account_id,
+    };
+  }
+  if ('InvalidDataReceiverId' in error) {
+    return {
+      type: 'invalidDataReceiverId',
+      accountId: error.InvalidDataReceiverId.account_id,
+    };
+  }
+  if ('ReturnedValueLengthExceeded' in error) {
+    return {
+      type: 'returnedValueLengthExceeded',
+      length: error.ReturnedValueLengthExceeded.length,
+      limit: error.ReturnedValueLengthExceeded.limit,
+    };
+  }
+  if ('NumberInputDataDependenciesExceeded' in error) {
+    return {
+      type: 'numberInputDataDependenciesExceeded',
+      numberOfInputDataDependencies:
+        error.NumberInputDataDependenciesExceeded
+          .number_of_input_data_dependencies,
+      limit: error.NumberInputDataDependenciesExceeded.limit,
+    };
+  }
+  if ('ActionsValidation' in error) {
+    return {
+      type: 'actionsValidation',
+    };
+  }
+  return UNKNOWN_ERROR;
+}
+
+function mapRpcReceiptActionError(error) {
+  const UNKNOWN_ERROR = { type: 'unknown' };
+  const { kind } = error;
+  if (kind === 'DelegateActionExpired') {
+    return {
+      type: 'delegateActionExpired',
+    };
+  }
+  if (kind === 'DelegateActionInvalidSignature') {
+    return {
+      type: 'delegateActionInvalidSignature',
+    };
+  }
+  if ('DelegateActionSenderDoesNotMatchTxReceiver' in kind) {
+    return {
+      type: 'delegateActionSenderDoesNotMatchTxReceiver',
+      receiverId: kind.DelegateActionSenderDoesNotMatchTxReceiver.receiver_id,
+      senderId: kind.DelegateActionSenderDoesNotMatchTxReceiver.sender_id,
+    };
+  }
+  if ('DelegateActionAccessKeyError' in kind) {
+    return {
+      type: 'delegateActionAccessKeyError',
+      error: mapRpcInvalidAccessKeyError(kind.DelegateActionAccessKeyError),
+    };
+  }
+  if ('DelegateActionInvalidNonce' in kind) {
+    return {
+      type: 'delegateActionInvalidNonce',
+      akNonce: kind.DelegateActionInvalidNonce.ak_nonce,
+      delegateNonce: kind.DelegateActionInvalidNonce.delegate_nonce,
+    };
+  }
+  if ('DelegateActionNonceTooLarge' in kind) {
+    return {
+      type: 'delegateActionNonceTooLarge',
+      delegateNonce: kind.DelegateActionNonceTooLarge.delegate_nonce,
+      upperBound: kind.DelegateActionNonceTooLarge.upper_bound,
+    };
+  }
+  if ('AccountAlreadyExists' in kind) {
+    return {
+      type: 'accountAlreadyExists',
+      accountId: kind.AccountAlreadyExists.account_id,
+    };
+  }
+  if ('AccountDoesNotExist' in kind) {
+    return {
+      type: 'accountDoesNotExist',
+      accountId: kind.AccountDoesNotExist.account_id,
+    };
+  }
+  if ('CreateAccountOnlyByRegistrar' in kind) {
+    return {
+      type: 'createAccountOnlyByRegistrar',
+      accountId: kind.CreateAccountOnlyByRegistrar.account_id,
+      registrarAccountId:
+        kind.CreateAccountOnlyByRegistrar.registrar_account_id,
+      predecessorId: kind.CreateAccountOnlyByRegistrar.predecessor_id,
+    };
+  }
+  if ('CreateAccountNotAllowed' in kind) {
+    return {
+      type: 'createAccountNotAllowed',
+      accountId: kind.CreateAccountNotAllowed.account_id,
+      predecessorId: kind.CreateAccountNotAllowed.predecessor_id,
+    };
+  }
+  if ('ActorNoPermission' in kind) {
+    return {
+      type: 'actorNoPermission',
+      accountId: kind.ActorNoPermission.account_id,
+      actorId: kind.ActorNoPermission.actor_id,
+    };
+  }
+  if ('DeleteKeyDoesNotExist' in kind) {
+    return {
+      type: 'deleteKeyDoesNotExist',
+      accountId: kind.DeleteKeyDoesNotExist.account_id,
+      publicKey: kind.DeleteKeyDoesNotExist.public_key,
+    };
+  }
+  if ('AddKeyAlreadyExists' in kind) {
+    return {
+      type: 'addKeyAlreadyExists',
+      accountId: kind.AddKeyAlreadyExists.account_id,
+      publicKey: kind.AddKeyAlreadyExists.public_key,
+    };
+  }
+  if ('DeleteAccountStaking' in kind) {
+    return {
+      type: 'deleteAccountStaking',
+      accountId: kind.DeleteAccountStaking.account_id,
+    };
+  }
+  if ('LackBalanceForState' in kind) {
+    return {
+      type: 'lackBalanceForState',
+      accountId: kind.LackBalanceForState.account_id,
+      amount: kind.LackBalanceForState.amount,
+    };
+  }
+  if ('TriesToUnstake' in kind) {
+    return {
+      type: 'triesToUnstake',
+      accountId: kind.TriesToUnstake.account_id,
+    };
+  }
+  if ('TriesToStake' in kind) {
+    return {
+      type: 'triesToStake',
+      accountId: kind.TriesToStake.account_id,
+      stake: kind.TriesToStake.stake,
+      locked: kind.TriesToStake.locked,
+      balance: kind.TriesToStake.balance,
+    };
+  }
+  if ('InsufficientStake' in kind) {
+    return {
+      type: 'insufficientStake',
+      accountId: kind.InsufficientStake.account_id,
+      stake: kind.InsufficientStake.stake,
+      minimumStake: kind.InsufficientStake.minimum_stake,
+    };
+  }
+  if ('FunctionCallError' in kind) {
+    return {
+      type: 'functionCallError',
+      error: mapRpcFunctionCallError(kind.FunctionCallError),
+    };
+  }
+  if ('NewReceiptValidationError' in kind) {
+    return {
+      type: 'newReceiptValidationError',
+      error: mapRpcNewReceiptValidationError(kind.NewReceiptValidationError),
+    };
+  }
+  if ('OnlyImplicitAccountCreationAllowed' in kind) {
+    return {
+      type: 'onlyImplicitAccountCreationAllowed',
+      accountId: kind.OnlyImplicitAccountCreationAllowed.account_id,
+    };
+  }
+  if ('DeleteAccountWithLargeState' in kind) {
+    return {
+      type: 'deleteAccountWithLargeState',
+      accountId: kind.DeleteAccountWithLargeState.account_id,
+    };
+  }
+  return UNKNOWN_ERROR;
+}
+
+function mapRpcReceiptInvalidTxError(error) {
+  const UNKNOWN_ERROR = { type: 'unknown' };
+  if ('InvalidAccessKeyError' in error) {
+    return {
+      type: 'invalidAccessKeyError',
+      error: mapRpcInvalidAccessKeyError(error.InvalidAccessKeyError),
+    };
+  }
+  if ('InvalidSignerId' in error) {
+    return {
+      type: 'invalidSignerId',
+      signerId: error.InvalidSignerId.signer_id,
+    };
+  }
+  if ('SignerDoesNotExist' in error) {
+    return {
+      type: 'signerDoesNotExist',
+      signerId: error.SignerDoesNotExist.signer_id,
+    };
+  }
+  if ('InvalidNonce' in error) {
+    return {
+      type: 'invalidNonce',
+      transactionNonce: error.InvalidNonce.tx_nonce,
+      akNonce: error.InvalidNonce.ak_nonce,
+    };
+  }
+  if ('NonceTooLarge' in error) {
+    return {
+      type: 'nonceTooLarge',
+      transactionNonce: error.NonceTooLarge.tx_nonce,
+      upperBound: error.NonceTooLarge.upper_bound,
+    };
+  }
+  if ('InvalidReceiverId' in error) {
+    return {
+      type: 'invalidReceiverId',
+      receiverId: error.InvalidReceiverId.receiver_id,
+    };
+  }
+  if ('InvalidSignature' in error) {
+    return {
+      type: 'invalidSignature',
+    };
+  }
+  if ('NotEnoughBalance' in error) {
+    return {
+      type: 'notEnoughBalance',
+      signerId: error.NotEnoughBalance.signer_id,
+      balance: error.NotEnoughBalance.balance,
+      cost: error.NotEnoughBalance.cost,
+    };
+  }
+  if ('LackBalanceForState' in error) {
+    return {
+      type: 'lackBalanceForState',
+      signerId: error.LackBalanceForState.signer_id,
+      amount: error.LackBalanceForState.amount,
+    };
+  }
+  if ('CostOverflow' in error) {
+    return {
+      type: 'costOverflow',
+    };
+  }
+  if ('InvalidChain' in error) {
+    return {
+      type: 'invalidChain',
+    };
+  }
+  if ('Expired' in error) {
+    return {
+      type: 'expired',
+    };
+  }
+  if ('ActionsValidation' in error) {
+    return {
+      type: 'actionsValidation',
+    };
+  }
+  if ('TransactionSizeExceeded' in error) {
+    return {
+      type: 'transactionSizeExceeded',
+      size: error.TransactionSizeExceeded.size,
+      limit: error.TransactionSizeExceeded.limit,
+    };
+  }
+  return UNKNOWN_ERROR;
+}
+
+function mapRpcReceiptError(error) {
+  let UNKNOWN_ERROR = { type: 'unknown' };
+  if ('ActionError' in error) {
+    return {
+      type: 'action',
+      error: mapRpcReceiptActionError(error.ActionError),
+    };
+  }
+  if ('InvalidTxError' in error) {
+    return {
+      type: 'transaction',
+      error: mapRpcReceiptInvalidTxError(error.InvalidTxError),
+    };
+  }
+  return UNKNOWN_ERROR;
+}
+
+export function mapRpcReceiptStatus(status) {
+  if ('SuccessValue' in status) {
+    return { type: 'successValue', value: status.SuccessValue };
+  }
+  if ('SuccessReceiptId' in status) {
+    return { type: 'successReceiptId', receiptId: status.SuccessReceiptId };
+  }
+  if ('Failure' in status) {
+    return { type: 'failure', error: mapRpcReceiptError(status.Failure) };
+  }
+  return { type: 'unknown' };
+}
+
+export function mapRpcActionToAction1(rpcAction) {
+  if (typeof rpcAction === 'object' && 'Delegate' in rpcAction) {
+    return {
+      kind: 'delegateAction',
+      args: {
+        actions: rpcAction.Delegate.delegate_action.actions.map(
+          (subaction, index) => ({
+            ...mapNonDelegateRpcActionToAction(subaction),
+            delegateIndex: index,
+          }),
+        ),
+        receiverId: rpcAction.Delegate.delegate_action.receiver_id,
+        senderId: rpcAction.Delegate.delegate_action.sender_id,
+      },
+    };
+  }
+  return mapNonDelegateRpcActionToAction(rpcAction);
+}
+
+export function parseOutcomeOld(outcome) {
+  return {
+    blockHash: outcome.block_hash,
+    tokensBurnt: outcome.outcome.tokens_burnt,
+    gasBurnt: outcome.outcome.gas_burnt,
+    status: mapRpcReceiptStatus(outcome.outcome.status),
+    logs: outcome.outcome.logs,
+    receiptIds: outcome.outcome.receipt_ids,
+  };
 }
