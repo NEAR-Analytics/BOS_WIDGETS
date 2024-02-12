92a747ad753c813d1677b0515e7135220948e9e1
dydx4.near <dydx4.near>
2024-02-12 17:36:23 +0000

    Update Index by dydx4.near at 2024-02-12T17:36:23.225Z

--- dydx4.near/Index/source_code.jsx
+++ dydx4.near/Index/source_code.jsx
@@ -36,6 +36,43 @@ if (!state.theme) {
     theme: styled.div`
 ${css}
 
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 20px;
+  height: 20px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 14px;
+  height: 14px;
+  margin: 1px;
+  border: 1px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #fff transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+
 .order-tabs ul {
      position: relative;
   width: fit-content;
@@ -451,7 +488,7 @@ const SignInWithMetamask = () => {
   };
 
   /*return getWalletFromEvmSignature(
-    "0x7eb5d09152acb9662b30d9bceb6e2142d5876439cbc9983df598c364b48f7eb76d41fc70436d93ad4875d307c891d104a3e8e33d23c17d041af4e0bb1057b6031c"
+    "0x0779917cb0d970a483165e49cac6f10baf6ef7fc907f209835874dc678387bac127d1431a6f81e82c193788923a70a90b138dbfd384b941fa91a0dc0d768104a1b"
   );*/
 
   Ethers.provider()
@@ -493,18 +530,6 @@ if (state.dydx_account == undefined && state.chainId == defaultChainId) {
 
   return (
     <Theme class="mb-5">
-      {state.error_msg && (
-        <div class="alert alert-primary" role="alert">
-          {state.error_msg}
-        </div>
-      )}
-
-      {state.is_loading && (
-        <div class="alert alert-primary" role="alert">
-          LOADING...
-        </div>
-      )}
-
       <div class="dydx-component-not">
         <div class="container-wrapper">
           <div class="container">
@@ -665,6 +690,59 @@ if (state.dydx_account == undefined && state.chainId == defaultChainId) {
                     </div>
                   </div>
                 </div>
+
+                {state.error_msg && (
+                  <div
+                    class="div-2 title"
+                    style={{
+                      color: "white",
+                      justifyContent: "flex-start",
+                      borderRadius: "10px",
+                      gap: "10px",
+                      backgroundColor: "rgb(129, 37, 37)",
+                    }}
+                    role="alert"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24px"
+                      height="24px"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <circle cx="12" cy="12" r="10" fill="white" />
+
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM11 13C11 13.5523 11.4477 14 12 14C12.5523 14 13 13.5523 13 13V8C13 7.44772 12.5523 7 12 7C11.4477 7 11 7.44772 11 8V13ZM13 15.9888C13 15.4365 12.5523 14.9888 12 14.9888C11.4477 14.9888 11 15.4365 11 15.9888V16C11 16.5523 11.4477 17 12 17C12.5523 17 13 16.5523 13 16V15.9888Z"
+                        fill="red"
+                      />
+                    </svg>
+                    {state.error_msg}
+                  </div>
+                )}
+
+                {state.is_loading && (
+                  <div
+                    class="div-2 title"
+                    style={{
+                      color: "white",
+                      justifyContent: "flex-start",
+                      borderRadius: "10px",
+                      gap: "10px",
+                    }}
+                    role="alert"
+                  >
+                    <div class="lds-ring">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                    </div>
+                    Loading...
+                  </div>
+                )}
               </div>
               <div class="orders-log">
                 <div class="heading">
