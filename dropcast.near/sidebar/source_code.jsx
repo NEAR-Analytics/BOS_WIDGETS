d98a53551ebdb8ed56c13a37285b0d36554f9782
dropcast.near <dropcast.near>
2024-02-12 20:13:29 +0000

    Update Sidebar by dropcast.near at 2024-02-12T20:13:29.706Z

--- dropcast.near/sidebar/source_code.jsx
+++ dropcast.near/sidebar/source_code.jsx
@@ -1,43 +1,17 @@
 const accountId = context.accountId;
 const Owner = props.Owner || "dropcast.near";
 const API_URL = props.API_URL || "http://localhost:3000";
-const USER = props.USER || {};
-const PAGES = props.PAGES || [];
-const currentPage = props.currentPage;
-const changePage = props.changePage;
-const sidebar = props.sidebar || false;
-const openSidebar = props.openSidebar || ((value) => {});
-const Logout = props.Logout;
 
 //Styles
 const Wrapper = styled.div`
-    top: 0px;
-    width: 256px;
-    height: 100%;  
-    display: flex;
-    color: #d4d4d4;
-    position: sticky;
-    align-items: center;
-    background: #262626;
-    flex-direction: column;
-    transition: width 2s ease-out;
-    .left-side{
-        display: flex;
-    }
-    @media (max-width: 620px) {
-        width: ${sidebar ? "55%" : 0};
-        z-index: 2;
-        position: absolute;
-        ${
-          !sidebar &&
-          `.left-side{
-            display: none;
-        }`
-        }
-        .close {
-          display: flex;
-        }
-    }
+  display: flex;
+  width: 256px;
+  height: 100vh;
+  color: #d4d4d4;
+  position: relative;
+  align-items: center;
+  background: #262626;
+  flex-direction: column;
 `;
 
 const LogoIcon = styled.img`
@@ -49,129 +23,64 @@ const Title = styled.h2`
     font-size: 24px;
     color: transparent;
     background-clip: text;
-    background-image: linear-gradient(to bottom right, #facc15, #fb923c, #db2777);
-`;
-
-const Avatar = styled.img`
-    width: 40px;
-    height: 40px;
-    border-radius : 50px;
-`;
-
-const SelectedIcon = styled.div`
-    left: -5px;
-    width: 10%;
-    height: 8px;
-    position: absolute;
-    border-radius: 0.375rem;
-    background-image: linear-gradient(to right ,#facc15,#ea580c);
-`;
-
-const CloseButton = styled.button`
-    top: 10px;
-    padding: 0;
-    z-index: 1;
-    width: 29px;
-    right: 10px;
-    height: 29px;
-    color: white;
-    display: none;
-    border: 1px solid;  
-    position: absolute;
-    align-items: center;
-    justify-content: center;
+    background-image: linear-gradient(to bottom right, #facc15, #fb923c, #db2777, rgb(251 146 60 / 0));
 `;
 
 return (
-  <Wrapper className="sidebar">
-    <div className="flex-column align-items-center left-side">
-      <CloseButton className="btn close" onClick={() => openSidebar(false)}>
+  <Wrapper>
+    <div className="d-flex justify-content-center align-items-center flex-column pt-5">
+      <LogoIcon
+        src="https://vulcanapp.io/_next/static/media/logo.24108d07.svg"
+        decoding="async"
+        data-nimg="fill"
+      />
+      <Title>Vulcan</Title>
+      <p className="m-0" style={{ fontSize: 12 }}>
+        Powered by Blocksmith Labs
+      </p>
+      <p className="m-0" style={{ fontSize: 12 }}>
+        Managed by Nearverse Labs
+      </p>
+    </div>
+
+    <div className="d-flex flex-column mt-5 gap-3">
+      <div className="d-flex text-white align-items-center gap-2 rbt-token-removeable">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="1em"
-          height="1em"
+          fill="none"
           viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          aria-hidden="true"
+          style={{ width: 24, height: 24 }}
+          className="text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
         >
           <path
-            fill="currentColor"
-            d="M13.46 12L19 17.54V19h-1.46L12 13.46L6.46 19H5v-1.46L10.54 12L5 6.46V5h1.46L12 10.54L17.54 5H19v1.46z"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
           />
         </svg>
-      </CloseButton>
-      <div className="d-flex justify-content-center align-items-center flex-column pt-5">
-        <LogoIcon
-          src="https://dropcast.nearverselabs.com/logo.png"
-          decoding="async"
-          data-nimg="fill"
-        />
-        <Title>Dropcast</Title>
-
-        <p className="m-0" style={{ fontSize: 12 }}>
-          Powered by Nearverse Labs
-        </p>
+        Dashboard
       </div>
-
-      <div className="d-flex flex-column mt-5 gap-4">
-        {PAGES.map((page, index) => {
-          if (page.link) {
-            return (
-              <a
-                key={index}
-                target="_blank"
-                href={page.link}
-                style={{ color: currentPage === page.value ? "white" : "grey" }}
-                className="d-flex align-items-center gap-2 rbt-token-removeable"
-              >
-                {page.icon}
-                {currentPage === page.value && <SelectedIcon />}
-                <p className="m-0">{page.title}</p>
-              </a>
-            );
-          } else {
-            return (
-              <div
-                key={index}
-                style={{ color: currentPage === page.value ? "white" : "grey" }}
-                className="d-flex align-items-center gap-2 rbt-token-removeable"
-                onClick={() => changePage(page.value)}
-              >
-                {page.icon}
-                {currentPage === page.value && <SelectedIcon />}
-                <p className="m-0">{page.title}</p>
-              </div>
-            );
-          }
-        })}
-      </div>
-      <div className="d-flex w-100 px-3 position-absolute bottom-0 mb-3 justify-content-between align-items-center">
-        <div className="d-flex align-items-center gap-3">
-          <Avatar
-            src={`https://cdn.discordapp.com/avatars/${USER.id}/${USER.avatar}.png`}
-            alt=""
-          />
-          <p className="m-0">{USER.username}</p>
-        </div>
-        <a
-          className="btn p-0"
-          style={{ width: 30, color: "white" }}
-          onClick={Logout}
-          href={`https://near.org/${Owner}/widget/login`}
+      <div className="d-flex text-white align-items-center gap-2 rbt-token-removeable">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          aria-hidden="true"
+          style={{ width: 24, height: 24 }}
+          className="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-            />
-          </svg>
-        </a>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+          />
+        </svg>
+        Account
       </div>
     </div>
   </Wrapper>
