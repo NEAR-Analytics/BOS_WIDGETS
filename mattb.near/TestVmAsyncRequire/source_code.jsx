6863d7efdaef9c0679e8ed8638206ec081700e6e
mattb.near <mattb.near>
2024-02-13 00:00:06 +0000

    Update TestVmAsyncRequire by mattb.near at 2024-02-13T00:00:06.364Z

--- mattb.near/TestVmAsyncRequire/source_code.jsx
+++ mattb.near/TestVmAsyncRequire/source_code.jsx
@@ -1,6 +1,7 @@
-VM.prepare(["sdks.near/widget/Utils.CryptoJS"]);
-
-console.log(VM.require("sdks.near/widget/Utils.CryptoJS"));
+VM.prepare(["sdks.near/widget/Utils.CryptoJS"]).then((result) => {
+    console.log("Load: ", result);
+    console.log(VM.require("sdks.near/widget/Utils.CryptoJS"));
+});
 
 return (
   <>
