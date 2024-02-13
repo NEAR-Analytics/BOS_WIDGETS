5d8a1bc454421f069895442cb5ca6d1e13303679
dropcast.near <dropcast.near>
2024-02-13 21:44:19 +0000

    Update discord by dropcast.near at 2024-02-13T21:44:19.208Z

--- dropcast.near/discord/source_code.jsx
+++ dropcast.near/discord/source_code.jsx
@@ -53,7 +53,7 @@ const fetchData = () => {
       }).then((res) => {
         const result = res.body;
         console.log(result, "==>reusult");
-        if (result.user)
+        if (result.token)
           return <Widget src={`${Owner}/widget/main`} props={result} />;
         else if (result.error) return result.message;
       });
