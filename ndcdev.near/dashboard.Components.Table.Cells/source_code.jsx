d61aabda8bd3347e77387630fb67a00adf8dee2a
ndcdev.near <ndcdev.near>
2024-02-13 13:17:32 +0000

    Update Dashboard.Components.Table.Cells by ndcdev.near at 2024-02-13T13:17:32.622Z

--- ndcdev.near/dashboard.Components.Table.Cells/source_code.jsx
+++ ndcdev.near/dashboard.Components.Table.Cells/source_code.jsx
@@ -87,11 +87,11 @@ const TooltipContent = ({ key, value }) => (
 );
 
 const DaoName = styled.div`
-  white-space: nowrap;       
-  overflow: hidden;         
-  text-overflow: ellipsis;  
-  max-width: 100%; 
-`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
+`;
 
 return (
   <Container>
