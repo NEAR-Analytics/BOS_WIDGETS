716f665573621d5c78c38fa79d9e2d9419174bc6
buildhub.near <buildhub.near>
2024-02-13 17:35:52 +0000

    Update page.feed by buildhub.near at 2024-02-13T17:35:52.664Z

--- buildhub.near/page.feed/source_code.jsx
+++ buildhub.near/page.feed/source_code.jsx
@@ -1,161 +1,70 @@
-const { TEMPLATES } = VM.require("buildhub.near/widget/feed.templates") || {
-  TEMPLATES: {},
+const { currentPath, page, ...passProps } = props;
+
+const { routes } = VM.require("buildhub.near/widget/config.feed") ?? {
+  routes: {},
 };
+
 const { SidebarLayout } = VM.require(
   "buildhub.near/widget/template.SidebarLayout"
 ) || {
   SidebarLayout: () => <></>,
 };
-const { Post } = VM.require("buildhub.near/widget/components") || {
-  Post: () => <></>,
-};
-const config = {
-  theme: {},
-  layout: {
-    src: "devs.near/widget/Layout",
-    props: {
-      variant: "standard",
-    },
-  },
-  blocks: {
-    Header: () => <></>,
-    Footer: () => <></>,
-  },
-  router: {
-    param: "tab",
-    routes: {
-      all: {
-        path: "buildhub.near/widget/Feed",
-        blockHeight: "final",
-        init: {
-          feedName: "All",
-          name: "All",
-          icon: "bi-list",
-          requiredHashtags: ["build"],
-        },
-        default: true,
-      },
-      updates: {
-        path: "buildhub.near/widget/Feed",
-        blockHeight: "final",
-        init: {
-          feedName: "Updates",
-          name: "Updates",
-          icon: "bi-bell",
-          requiredHashtags: ["build", "update"],
-          template: TEMPLATES.updates,
-        },
-      },
-      question: {
-        path: "buildhub.near/widget/Feed",
-        blockHeight: "final",
-        init: {
-          feedName: "Question",
-          name: "Question",
-          icon: "bi-question-lg",
-          requiredHashtags: ["build", "question"],
-          template: TEMPLATES.question,
-        },
-      },
-      idea: {
-        path: "buildhub.near/widget/Feed",
-        blockHeight: "final",
-        init: {
-          feedName: "Idea",
-          name: "Idea",
-          icon: "bi-lightbulb",
-          requiredHashtags: ["build", "idea"],
-          template: TEMPLATES.idea,
-        },
-      },
-      feedback: {
-        path: "buildhub.near/widget/Feed",
-        blockHeight: "final",
-        init: {
-          feedName: "Feedback",
-          name: "Feedback",
-          icon: "bi-chat-left-text",
-          requiredHashtags: ["build", "feedback"],
-        },
-      },
-      events: {
-        path: "buildhub.near/widget/events.Calendar",
-        blockHeight: "final",
-        init: {
-          feedName: "Events",
-          name: "Events",
-          icon: "bi-calendar",
-          app: "every",
-          thing: "event",
-        },
-      },
-      bookmarks: {
-        path: "buildhub.near/widget/OrderedGraphFeed",
-        blockHeight: "final",
-        init: {
-          feedName: "Bookmarks",
-          name: "Bookmarks",
-          icon: "bi-bookmark",
-          itemType: "bookmark",
-          renderItem: (item) => {
-            return (
-              <Post
-                accountId={item.accountId}
-                blockHeight={item.blockHeight}
-                noBorder={true}
-                hideComments={true}
-              />
-            );
-          },
-        },
-      },
-      request: {
-        path: "buildhub.near/widget/Feed",
-        blockHeight: "final",
-        init: {
-          feedName: "Request",
-          name: "Request",
-          icon: "bi-file-earmark-text",
-          requiredHashtags: ["build", "request"],
-          customActions: [
-            {
-              type: "modal",
-              icon: "bi-file-earmark-text",
-              label: "Propose",
-              onClick: (modalToggles) => {
-                const toggle = modalToggles.propose;
-                toggle();
-              },
-            },
-          ],
-          template: TEMPLATES.request,
-        },
-      },
-      proposals: {
-        path: "buildhub.near/widget/Proposals",
-        blockHeight: "final",
-        init: {
-          feedName: "Proposals",
-          name: "Proposals",
-          icon: "bi-file-earmark-text",
-          daoId: "build.sputnik-dao.near",
-        },
-      },
-    },
-  },
-};
+
+if (!page) page = Object.keys(routes)[0] || "home";
+
 const Root = styled.div``;
+
+function Router({ active, routes }) {
+  // this may be converted to a module at devs.near/widget/Router
+  const routeParts = active.split(".");
+
+  let currentRoute = routes;
+  let src = "";
+  let defaultProps = {};
+
+  for (let part of routeParts) {
+    if (currentRoute[part]) {
+      currentRoute = currentRoute[part];
+      src = currentRoute.path;
+
+      if (currentRoute.init) {
+        defaultProps = { ...defaultProps, ...currentRoute.init };
+      }
+    } else {
+      // Handle 404 or default case for unknown routes
+      return <p>404 Not Found</p>;
+    }
+  }
+
+  return (
+    <div key={active}>
+      <Widget src={src} props={{ ...passProps, ...defaultProps }} />
+    </div>
+  );
+}
+
+const Container = styled.div`
+  // display: flex;
+  height: 100%;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
 return (
   <Root>
-    <SidebarLayout
-      currentPath={"/buildhub.near/widget/app?page=feed"}
-      page={props.tab}
-      routes={config.router.routes}
-    >
-      <Widget
-        src="buildhub.near/widget/app.view"
-        props={{ config, ...props }}
-      />
-    </SidebarLayout>
+    <Container>
+      <SidebarLayout
+        currentPath={currentPath}
+        page={page}
+        routes={routes}
+      >
+        <Content>
+          <Router active={page} routes={routes} />
+        </Content>
+      </SidebarLayout>
+    </Container>
   </Root>
 );
