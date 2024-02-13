7aa60232595c9dffc1b579b11d3d23b5a32c5c5d
dropcast.near <dropcast.near>
2024-02-13 14:16:49 +0000

    Update discord by dropcast.near at 2024-02-13T14:16:49.445Z

--- dropcast.near/discord/source_code.jsx
+++ dropcast.near/discord/source_code.jsx
@@ -1,8 +1,42 @@
-const BASE_URL = "https://near.org/dropcast.near/widget/";
 const accountId = context.accountId;
+const CLIENT_ID = "1206878767633534976";
+const CLIENT_SECRET = "GhYxHW-FZyo0pqK26xx7BVgwTa5VCLn6";
+const BASE_URL = "https://near.org/dropcast.near/widget/";
+const OAuthScope = ["identify", "guilds"].join(" ");
 
 const discordCode = props.code || "";
-console.log(discordCode, "==>discordCode");
+
+const fetchData = () => {
+  const params = {
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    grant_type: "authorization_code",
+    code: discordCode,
+    redirect_uri: `${BASE_URL}discord`,
+    scope: OAuthScope,
+  };
+
+  const urlSearchParams = Object.keys(params)
+    .map((param) => `${param}=${params[param]}`)
+    .join("&");
+
+  let promise = asyncFetch(
+    `https://discordapp.com/api/v9/oauth2/token?${urlSearchParams}`,
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    }
+  );
+
+  promise.then((data) => {
+    console.log(data, "==>daata");
+  });
+};
+
 if (!discordCode || !accountId)
   return (window.location.href = `${BASE_URL}login`);
+else fetchData();
+
 return <div>Hello World</div>;
