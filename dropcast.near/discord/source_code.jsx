b1ac6bbb256494e28756e14972a85938c30f5adc
dropcast.near <dropcast.near>
2024-02-13 13:37:39 +0000

    Update discord by dropcast.near at 2024-02-13T13:37:39.999Z

--- dropcast.near/discord/source_code.jsx
+++ dropcast.near/discord/source_code.jsx
@@ -1,2 +1,8 @@
-console.log(props.tab, "==>params");
+const BASE_URL = "https://near.org/dropcast.near/widget/";
+const accountId = context.accountId;
+
+const discordCode = props.code || "";
+console.log(discordCode, "==>discordCode");
+if (!discordCode || !accountId)
+  return (window.location.href = `${BASE_URL}login`);
 return <div>Hello World</div>;
