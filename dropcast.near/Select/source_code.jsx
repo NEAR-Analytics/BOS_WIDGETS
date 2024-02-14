d2186338123b0343dfb33b3f55c13bf578866043
dropcast.near <dropcast.near>
2024-02-14 16:09:59 +0000

    Update Select by dropcast.near at 2024-02-14T16:09:59.891Z

--- dropcast.near/Select/source_code.jsx
+++ dropcast.near/Select/source_code.jsx
@@ -76,7 +76,7 @@ const scaleOut = styled.keyframes`
 const Content = styled.div`
   padding: 0;
   gap: 0.5em;
-  width: 320px;
+  width: 270px;
   display: flex;
   font-size: 14px;
   overflow-y: auto;
