0f4c83a6df5cc72966d71f20f068ddadc3b01858
dropcast.near <dropcast.near>
2024-02-14 13:38:30 +0000

    Update sidebar by dropcast.near at 2024-02-14T13:38:30.290Z

--- dropcast.near/sidebar/source_code.jsx
+++ dropcast.near/sidebar/source_code.jsx
@@ -1,13 +1,14 @@
 const accountId = context.accountId;
 const Owner = props.Owner || "dropcast.near";
 const API_URL = props.API_URL || "http://localhost:3000";
-const user = props.user || {};
+const USER = props.USER || {};
+const PAGES = props.PAGES || [];
 
 //Styles
 const Wrapper = styled.div`
   display: flex;
   width: 256px;
-  height: 100vh;
+  height: 100vh;  
   color: #d4d4d4;
   position: relative;
   align-items: center;
@@ -60,45 +61,16 @@ return (
     </div>
 
     <div className="d-flex flex-column mt-5 gap-3">
-      <div className="d-flex text-white align-items-center gap-2 rbt-token-removeable">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          aria-hidden="true"
-          style={{ width: 24, height: 24 }}
-          className="text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
+      {PAGES.map((page, index) => (
+        <div
+          key={index}
+          className="d-flex text-white align-items-center gap-2 rbt-token-removeable"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
-        <SelectedIcon />
-        Dashboard
-      </div>
-      <div className="d-flex text-white align-items-center gap-2 rbt-token-removeable">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          aria-hidden="true"
-          style={{ width: 24, height: 24 }}
-          className="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-          />
-        </svg>
-        Account
-      </div>
+          {page.icon}
+          <SelectedIcon />
+          {page.title}
+        </div>
+      ))}
     </div>
     <div className="d-flex w-100 px-3 position-absolute bottom-0 mb-3 justify-content-between">
       <div className="d-flex align-items-center gap-3">
