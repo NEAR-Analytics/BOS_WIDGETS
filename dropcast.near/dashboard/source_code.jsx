2ab931ea42b5302ee269359ce2dcac930e9d7f2e
dropcast.near <dropcast.near>
2024-02-14 15:21:05 +0000

    Update dashboard by dropcast.near at 2024-02-14T15:21:05.170Z

--- dropcast.near/dashboard/source_code.jsx
+++ dropcast.near/dashboard/source_code.jsx
@@ -17,9 +17,10 @@ const Wrapper = styled.div`
 `;
 
 const Tabs = styled.div`
-  display: flex;
+  gap: 20px;
   width: 100%;
   height: 52px;
+  display: flex;
   border-bottom: 1px solid #e5e7eb;
 `;
 
@@ -27,9 +28,10 @@ const Tab = styled.div`
   gap: 6px;
   height: 100%;
   display: flex;
+  cursor: pointer;
   padding: 16px 4px;
-  align-items: center;
   position: relative;
+  align-items: center;
 `;
 
 const Counter = styled.span`
@@ -49,14 +51,46 @@ const SelectedTab = styled.span`
   background-image: linear-gradient(to right, rgb(250, 204, 21), rgb(234, 88, 12));
 `;
 
+const TABS = [
+  {
+    label: "My Projects",
+    value: "my_projects",
+    count: 0,
+  },
+  {
+    label: "Other Projects",
+    value: "other_projects",
+    count: 12,
+  },
+  {
+    label: "Past Projects",
+    value: "past_projects",
+    count: 5,
+  },
+];
+
+State.init({
+  tab: "my_projects",
+});
+
+const changeTab = (tab) => {
+  State.update({ tab });
+};
+
 return (
   <Wrapper>
     <Tabs>
-      <Tab>
-        <p className="m-0">My project</p>
-        <Counter>0</Counter>
-        <SelectedTab />
-      </Tab>
+      {TABS.map((tab) => (
+        <Tab
+          key={tab.value}
+          onClick={() => changeTab(tab.value)}
+          style={{ opacity: tab.value === state.tab ? 1 : 0.5 }}
+        >
+          <p className="m-0">{tab.label}</p>
+          <Counter>{tab.count}</Counter>
+          {tab.value === state.tab && <SelectedTab />}
+        </Tab>
+      ))}
     </Tabs>
   </Wrapper>
 );
