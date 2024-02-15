dc6689cf551192e274930c32a6a8b579cfc782fe
dropcast.near <dropcast.near>
2024-02-15 16:26:37 +0000

    Update account by dropcast.near at 2024-02-15T16:26:37.307Z

---
+++ dropcast.near/account/source_code.jsx
@@ -0,0 +1,53 @@
+const accountId = context.accountId;
+const Owner = "dropcast.near";
+const API_URL = props.API_URL || "http://localhost:3000";
+const USER = props.USER || {};
+const TOKEN = props.TOKEN || "";
+
+//Styles
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  padding: 64px;
+  position: relative;
+  align-items: stretch;
+  flex-direction: column;
+  color: rgb(229 229 229);
+  background: rgb(23,23,23);
+`;
+
+return (
+  <Wrapper>
+    <div className="d-flex gap-3 align-items-center">
+      <img
+        style={{ width: 64, height: 64, borderRadius: 50 }}
+        src="https://cdn.discordapp.com/avatars/1050807227251953754/b249444edb0aabe2b4e2b6e2b2381265.png"
+        alt=""
+      />
+      <h4 className="m-0">pontrue</h4>
+    </div>
+    <div
+      className="mt-5 rounded-3"
+      style={{ backgroundColor: "rgb(38, 38, 38)" }}
+    >
+      <h6 className="px-3 py-4 m-0" style={{ fontSize: 18 }}>
+        Wallets
+      </h6>
+      <hr className="m-0" />
+      <p className="px-3 py-4 m-0">{accountId}</p>
+    </div>
+    <div
+      className="d-flex flex-column gap-1 mt-5 rounded-3 p-4"
+      style={{ backgroundColor: "rgb(38, 38, 38)", maxWidth: 550 }}
+    >
+      <h6 className="m-0" style={{ fontSize: 18 }}>
+        Note
+      </h6>
+      <p
+        className="m-0"
+        style={{ fontSize: 14 }}
+      >{`The wallet address put forward to projects for whitelisting is the address that is configured as "Default" on your Account. This can be changed as many times up until the project finishes whitelisting and has fully exported their whitelist addresses.`}</p>
+    </div>
+  </Wrapper>
+);
