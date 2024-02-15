c06429fd05d3777f4ea14cb1448bbd559f91877b
nearblocks.near <nearblocks.near>
2024-02-15 13:30:03 +0000

    Update bos-components.components.Address.Transactions by nearblocks.near at 2024-02-15T13:30:03.846Z

--- nearblocks.near/bos-components.components.Address.Transactions/source_code.jsx
+++ nearblocks.near/bos-components.components.Address.Transactions/source_code.jsx
@@ -2005,9 +2005,7 @@ function mapRpcFunctionCallError(error) {
   }
   return UNKNOWN_ERROR;
 }
-function mapRpcNewReceiptValidationError(
-  error,
-) {
+function mapRpcNewReceiptValidationError(error) {
   const UNKNOWN_ERROR = { type: 'unknown' };
   if ('InvalidPredecessorId' in error) {
     return {
