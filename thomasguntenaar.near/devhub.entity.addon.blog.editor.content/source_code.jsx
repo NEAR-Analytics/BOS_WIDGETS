0d95dbebd9ced549d9d7d967459ec5224b55ebe3
thomasguntenaar.near <thomasguntenaar.near>
2024-02-16 13:46:37 +0000

    Update devhub.entity.addon.blog.editor.content by thomasguntenaar.near at 2024-02-16T13:46:37.329Z

--- thomasguntenaar.near/devhub.entity.addon.blog.editor.content/source_code.jsx
+++ thomasguntenaar.near/devhub.entity.addon.blog.editor.content/source_code.jsx
@@ -217,7 +217,9 @@ return (
           className={"d-flex align-items-center justify-content-end gap-3 mt-4"}
         >
           <Widget
-            src={"thomasguntenaar.near/widget/devhub.components.molecule.Button"}
+            src={
+              "thomasguntenaar.near/widget/devhub.components.molecule.Button"
+            }
             props={{
               classNames: { root: "btn-success" },
               disabled: !hasDataChanged(),
