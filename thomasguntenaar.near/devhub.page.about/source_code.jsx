7e0fb6ade31975d6fc2e2ef52f5e881ebfaf4e1f
thomasguntenaar.near <thomasguntenaar.near>
2024-02-16 13:46:37 +0000

    Update devhub.page.about by thomasguntenaar.near at 2024-02-16T13:46:37.329Z

--- thomasguntenaar.near/devhub.page.about/source_code.jsx
+++ thomasguntenaar.near/devhub.page.about/source_code.jsx
@@ -66,7 +66,9 @@ const Section = styled.div`
 
 return (
   <>
-    <Widget src={`thomasguntenaar.near/widget/devhub.components.island.banner`} />
+    <Widget
+      src={`thomasguntenaar.near/widget/devhub.components.island.banner`}
+    />
     <Section>
       <h2>
         What is <span style={{ color: "#00EC97" }}>near</span>/dev/hub?
