7af24d20b8717b6c82bf6e3ec80f39c853fad9c0
thomasguntenaar.near <thomasguntenaar.near>
2024-02-16 14:00:33 +0000

    Update devhub.components.organism.Configurator by thomasguntenaar.near at 2024-02-16T14:00:33.145Z

--- thomasguntenaar.near/devhub.components.organism.Configurator/source_code.jsx
+++ thomasguntenaar.near/devhub.components.organism.Configurator/source_code.jsx
@@ -284,9 +284,7 @@ const Configurator = ({
           ) : null}
 
           <Widget
-            src={
-              "thomasguntenaar.near/widget/devhub.components.molecule.Button"
-            }
+            src={"thomasguntenaar.near/widget/devhub.components.molecule.Button"}
             props={{
               classNames: { root: "btn-outline-danger shadow-none border-0" },
               label: cancelLabel || "Cancel",
@@ -294,9 +292,7 @@ const Configurator = ({
             }}
           />
           <Widget
-            src={
-              "thomasguntenaar.near/widget/devhub.components.molecule.Button"
-            }
+            src={"thomasguntenaar.near/widget/devhub.components.molecule.Button"}
             props={{
               classNames: { root: classNames.submit || "btn-success" },
               disabled: !form.hasUnsubmittedChanges || !isFormValid(),
