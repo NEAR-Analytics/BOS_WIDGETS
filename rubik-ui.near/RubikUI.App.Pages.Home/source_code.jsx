b1c0b78171ce1515ee1eeac8dd565dae881764c0
rubik-ui.near <rubik-ui.near>
2024-02-17 20:08:54 +0000

    Update RubikUI.App.Pages.Home by rubik-ui.near at 2024-02-17T20:08:54.918Z

--- rubik-ui.near/RubikUI.App.Pages.Home/source_code.jsx
+++ rubik-ui.near/RubikUI.App.Pages.Home/source_code.jsx
@@ -1,7 +1,7 @@
 const { RubikLogo } = VM.require(
   "rubik-ui.near/widget/RubikUI.Components.Core"
-);
-const { Rubik } = VM.require("rubik-ui.near/widget/RubikUI.Themes.RubikTheme");
+) || {};
+const { Rubik } = VM.require("rubik-ui.near/widget/RubikUI.Themes.RubikTheme") || {};
 
 const [loading, setLoading] = useState(true);
 
