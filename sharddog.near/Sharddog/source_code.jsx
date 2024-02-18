d47d0d0fbf1aa2bfacef6bffa774e7ffd9a28972
sharddog.near <sharddog.near>
2024-02-18 15:55:00 +0000

    Update Sharddog by sharddog.near at 2024-02-18T15:55:00.917Z

--- sharddog.near/Sharddog/source_code.jsx
+++ sharddog.near/Sharddog/source_code.jsx
@@ -1,4 +1,4 @@
-const limit = 20;
+const limit = 1000;
 
 State.init({
   offset: 0,
@@ -29,6 +29,7 @@ function fetchTokens() {
             {nft_contract_id: {_eq: "nft.bluntdao.near"}},
             {nft_contract_id: {_eq: "meteor.sharddog.near"}},
              {nft_contract_id: {_eq: "open.sharddog.near"}},
+             {nft_contract_id: {_eq: "harvestmoon.sharddog.near"}},
           ]
         }
               order_by: {minted_timestamp: desc}
