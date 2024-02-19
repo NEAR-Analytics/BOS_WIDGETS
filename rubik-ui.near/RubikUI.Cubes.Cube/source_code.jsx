a728d57c4a586ee0b4f7836ad650b74e8df5d993
rubik-ui.near <rubik-ui.near>
2024-02-19 21:55:43 +0000

    Update RubikUI.Cubes.Cube by rubik-ui.near at 2024-02-19T21:55:43.711Z

--- rubik-ui.near/RubikUI.Cubes.Cube/source_code.jsx
+++ rubik-ui.near/RubikUI.Cubes.Cube/source_code.jsx
@@ -2,24 +2,50 @@ const Cube = ({
   as: DOMComponent,
   apply: Facet,
   extend: CustomFacetCSS,
-  options: facetOptions,
+  attributes: FacetAttributes,
+  options: FacetOptions,
   children,
-  ...rest
+  ...Properties
 }) => {
-  const FinalFacet = Facet
-    ? styled(Facet)`
-        ${CustomFacetCSS || ""}
-    `
-    : null;
-  const Wrapper = FinalFacet || DOMComponent || "div";
-  const parseOptions = () => {
-    return {};
-  };
+  const hasMediaStyles = Object.keys(Properties).find((key) =>
+    key.startsWith("media")
+  );
+  const mediaStyles = hasMediaStyles
+    ? Object.keys(Properties).reduce((css, key) => {
+        const match = key.match(/media(MinWidth|MaxWidth)(\d+)/);
+
+        if (match) {
+          const [_, type, width] = match;
+          const mediaType = type === "MinWidth" ? "min-width" : "max-width";
+          const style = Properties[key];
+          css += `@media (${mediaType}: ${width}px) { ${style} } `;
+        }
+
+        return css;
+      }, "")
+    : "";
+
+  const attributeClasses = Object.keys(Properties)
+    .filter(
+      (prop) =>
+        prop in FacetAttributes && Properties[prop] in FacetAttributes[prop]
+    )
+    .map((prop) => FacetAttributes[prop][Properties[prop]]);
+
+  const optionClasses = Object.keys(FacetOptions)
+    .filter((option) => option in Properties)
+    .map((option) => FacetOptions[option]);
+
+  const classNames = [...attributeClasses, ...optionClasses].join(" ");
+  const Element = styled(Facet || DOMComponent || styled.div``)`
+      ${CustomFacetCSS || ""}
+      ${mediaStyles || ""}
+  `;
 
   return (
-    <Wrapper as={DOMComponent} {...parseOptions(facetOptions)} {...rest}>
+    <Element as={DOMComponent} className={classNames} {...Properties}>
       {children}
-    </Wrapper>
+    </Element>
   );
 };
 
