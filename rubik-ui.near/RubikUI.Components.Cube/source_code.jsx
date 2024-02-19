23034e9019ebcd0960e7d982ebdc81ee3a7a8c13
rubik-ui.near <rubik-ui.near>
2024-02-19 13:11:21 +0000

    Update RubikUI.Components.Cube by rubik-ui.near at 2024-02-19T13:11:21.425Z

---
+++ rubik-ui.near/RubikUI.Components.Cube/source_code.jsx
@@ -0,0 +1,32 @@
+const Cube = ({
+  as: DOMComponent,
+  apply: Facet,
+  extend: CustomFacetCSS,
+  options: facetOptions,
+  children,
+  ...rest
+}) => {
+  const FinalFacet = Facet
+    ? styled(Facet)`
+        ${CustomFacetCSS || ""}
+    `
+    : null;
+  const Wrapper = FinalFacet || DOMComponent || "div";
+  const parseOptions = () => {
+    return {};
+  };
+
+  return (
+    <Wrapper
+      as={DOMComponent}
+      {...parseOptions(facetOptions)}
+      {...rest}
+    >
+      {children}
+    </Wrapper>
+  );
+};
+
+return {
+    Cube
+};
