d633950c1974b8f3120085711aca3725177c6a8f
ndcdev.near <ndcdev.near>
2024-02-19 14:40:47 +0000

    Update daos.App by ndcdev.near at 2024-02-19T14:40:47.719Z

---
+++ ndcdev.near/daos.App/source_code.jsx
@@ -0,0 +1,115 @@
+const { AppLayout } = VM.require(`ndcdev.near/widget/daos.Layouts.App`);
+const { page, ...passProps } = props;
+
+if (!AppLayout) return <Widget src="flashui.near/widget/Loading" />;
+if (!page) page = "home";
+
+function Page() {
+  switch (page) {
+    case "comments": {
+      return (
+        <Widget
+          src={`ndcdev.near/widget/daos.Pages.Comment`}
+          props={passProps}
+        />
+      );
+    }
+    case "home": {
+      return (
+        <Widget
+          src={`ndcdev.near/widget/daos.Pages.Home`}
+          props={passProps}
+        />
+      );
+    }
+    case "info": {
+      return (
+        <Widget
+          src={`ndcdev.near/widget/daos.Pages.Info`}
+          props={passProps}
+        />
+      );
+    }
+    case "councils": {
+      return (
+        <Widget
+          src={`ndcdev.near/widget/daos.Pages.Councils`}
+          props={passProps}
+        />
+      );
+    }
+    case "achievements": {
+      return (
+        <Widget
+          src={`ndcdev.near/widget/daos.Pages.Achievements`}
+          props={passProps}
+        />
+      );
+    }
+    case "guidance": {
+      return (
+        <Widget
+          src={`ndcdev.near/widget/daos.Pages.Guidance`}
+          props={passProps}
+        />
+      );
+    }
+    case "reports": {
+      return (
+        <Widget
+          src={`ndcdev.near/widget/daos.Pages.Proposals.List`}
+          props={{ type: "Report", ...passProps }}
+        />
+      );
+    }
+    case "proposals": {
+      return (
+        <Widget
+          src={`ndcdev.near/widget/daos.Pages.Proposals.List`}
+          props={{ type: "Proposal", ...passProps }}
+        />
+      );
+    }
+    case "communities": {
+      return <Widget src={`ndcdev.near/widget/daos.Pages.Communities`} />;
+    }
+    case "create_proposal": {
+      return (
+        <Widget
+          src={`ndcdev.near/widget/daos.Pages.Proposals.Create`}
+          props={passProps}
+        />
+      );
+    }
+
+    default: {
+      const NotFound = styled.div`
+        width: 100%;
+        height: 70vh;
+
+        h1 {
+          font-size: 10rem;
+        }
+
+        h2 {
+          font-size: 2rem;
+        }
+      `;
+
+      return (
+        <NotFound className="d-flex flex-grow-1 flex-column justify-content-center align-items-center">
+          <h1>
+            <b>404</b>
+          </h1>
+          <h2>Page does not exist</h2>
+        </NotFound>
+      );
+    }
+  }
+}
+
+return (
+  <AppLayout page={page}>
+    <Page />
+  </AppLayout>
+);
