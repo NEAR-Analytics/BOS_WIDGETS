d6f333dcc2108b60f5e4e87d7c694e503ee9cc8f
buildafricadev.near <buildafricadev.near>
2024-02-20 09:45:50 +0000

    Update buildAfricaHome by buildafricadev.near at 2024-02-20T09:45:50.715Z

---
+++ buildafricadev.near/buildAfricaHome/source_code.jsx
@@ -0,0 +1,18 @@
+const Home = styled.div`
+`;
+
+return (
+  <>
+    <Home>
+      <div>
+        <Widget src="buildafricadev.near/widget/buildAfricaHeader" />
+      </div>
+      <div className="">
+        <Widget src="buildafricadev.near/widget/buildAfricaCrafted" />
+      </div>
+      <div className="">
+        <Widget src="buildafricadev.near/widget/buildAfricaPartner" />
+      </div>
+    </Home>
+  </>
+);
