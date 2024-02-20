2d93769d6d6e4668dcd35f86d362371d85bbc99a
dropcast.near <dropcast.near>
2024-02-20 18:21:28 +0000

    Update discord by dropcast.near at 2024-02-20T18:21:28.394Z

--- dropcast.near/discord/source_code.jsx
+++ dropcast.near/discord/source_code.jsx
@@ -99,6 +99,9 @@ if (state.token)
 
 if (!discordCode || !accountId || state.go_login)
   return <Widget src={`${Owner}/widget/login`} />;
-else if (!state.loaded && !state.token) fetchData();
+else if (!state.loaded && !state.token)
+  setTimeout(() => {
+    fetchData();
+  }, 1000);
 
 return <div>{result.error || `Loading`}</div>;
