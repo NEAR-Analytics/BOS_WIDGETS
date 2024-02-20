0a77d515f36da4fa1187f938e543baec65c8eb24
megha19.near <megha19.near>
2024-02-20 15:52:27 +0000

    Update devhub.feature.proposal-search.by-input by megha19.near at 2024-02-20T15:52:27.566Z

---
+++ megha19.near/devhub.feature.proposal-search.by-input/source_code.jsx
@@ -0,0 +1,31 @@
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  width: 100%;
+`;
+
+const [search, setSearch] = useState(props.search);
+const onSearch = props.onSearch ?? (() => {});
+const onEnter = props.onEnter ?? (() => {});
+
+const updateInput = (value) => setSearch(value);
+
+useEffect(() => {
+  if (search !== props.search) {
+    onSearch(search);
+  }
+}, [search]);
+
+return (
+  <Widget
+    src="megha19.near/widget/devhub.components.molecule.InputWithIcon"
+    props={{
+      icon: <i class="bi bi-search m-auto"></i>,
+      value: search,
+      placeholder: "Search by content",
+      onUpdate: updateInput,
+      onEnter: onEnter,
+    }}
+  />
+);
