fc244be1da0d150f2c7d13b8fd6b2f9e62ee0461
hack.near <hack.near>
2024-02-21 23:08:01 +0000

    Update app.create by hack.near at 2024-02-21T23:08:01.738Z

--- hack.near/app.create/source_code.jsx
+++ hack.near/app.create/source_code.jsx
@@ -32,56 +32,9 @@ State.init({
   buttonText: "",
 });
 
-function generateUID() {
-  return (
-    Math.random().toString(16).slice(2) +
-    Date.now().toString(36) +
-    Math.random().toString(16).slice(2)
-  );
-}
-
-const { page, tab, ...passProps } = props;
-
 const { AppLayout } = VM.require("hack.near/widget/template.main") || {
   AppLayout: () => <></>,
 };
-
-if (!page) page = Object.keys(state.routes)[0] || "main";
-
-function Router({ active, routes }) {
-  const routeParts = active.split(".");
-
-  let currentRoute = routes;
-  let src = "";
-  let defaultProps = {};
-
-  for (let part of routeParts) {
-    if (currentRoute[part]) {
-      currentRoute = currentRoute[part];
-      src = currentRoute.path;
-
-      if (currentRoute.init) {
-        defaultProps = { ...defaultProps, ...currentRoute.init };
-      }
-    } else {
-      return <p>NOTHING FOUND</p>;
-    }
-  }
-
-  return (
-    <div key={active}>
-      <Widget
-        src={src}
-        props={{
-          currentPath: `/hack.near/widget/app.create?page=${page}`,
-          page: tab,
-          ...passProps,
-          ...defaultProps,
-        }}
-      />
-    </div>
-  );
-}
 
 const Container = styled.div`
   display: flex;
@@ -97,22 +50,324 @@ const handleCreate = () =>
   Social.set({
     widget: {
       app: {
-        "": `const accountId = props.accountId ?? context.accountId; const creatorId = props.creatorId ?? "${
-          context.accountId
-        }"; const pageId = props.pageId ?? "${
-          state.name || generateUID()
-        }"; return <Widget src="every.near/widget/app" props={{ accountId }} />`,
-        metadata: {
-          name: state.name,
-          image: state.image,
-          tags: {
-            build: "",
-          },
+        "": `const { page, tab, ...passProps } = props; const routes = props.routes ?? {
+            main: {
+              path: "${context.accountId}/widget/page.index",
+              blockHeight: "final",
+              init: {
+                name: "Home",
+              },
+            },
+            docs: {
+              path: "${context.accountId}/widget/page.docs",
+              blockHeight: "final",
+              init: {
+                name: "Docs",
+              },
+            },
+            social: {
+              path: "${context.accountId}/widget/page.feed",
+              blockHeight: "final",
+              init: {
+                name: "Activity",
+              },
+            },
+          };
+
+          const { AppLayout } = VM.require("hack.near/widget/template.main") || {
+            AppLayout: () => <></>,
+          };
+
+          if (!page) page = Object.keys(routes)[0] || "main";
+
+          function Router({ active, routes }) {
+            const routeParts = active.split(".");
+
+            let currentRoute = routes;
+            let src = "";
+            let defaultProps = {};
+
+            for (let part of routeParts) {
+              if (currentRoute[part]) {
+                currentRoute = currentRoute[part];
+                src = currentRoute.path;
+
+                if (currentRoute.init) {
+                  defaultProps = { ...defaultProps, ...currentRoute.init };
+                }
+              } else {
+                return <p>NOTHING FOUND</p>;
+              }
+            }
+
+            return (
+              <div key={active}>
+                <Widget
+                  src={src}
+                  props={{
+                    currentPath: "/${context.accountId}/widget/app?page=\${page}",
+                    page: tab,
+                    ...passProps,
+                    ...defaultProps,
+                  }}
+                />
+              </div>
+            );
+          }
+
+          const Container = styled.div\`
+            display: flex;
+            height: 100%;
+          \`;
+
+          const Content = styled.div\`
+            width: 100%;
+            height: 100%;
+          \`;
+
+          return (
+            <Container>
+              <AppLayout page={page} routes={routes} {...props}>
+                <Content>
+                  <Router active={page} routes={routes} />
+                </Content>
+              </AppLayout>
+            </Container>
+          )`,
+      },
+      "page.index": {
+        "": `const { Button } = VM.require("buildhub.near/widget/components");
+          const imageUrl =
+            props.imageUrl ?? ${
+              JSON.stringify(state.image.url) ??
+              "https://builders.mypinata.cloud/ipfs/QmQmKGGJXhkhGrTbE4MgJ3G1wUUu8eo7mNKwRSCB5tihCw"
+            };
+
+          const HeaderContainer = styled.div\`
+            width: 100%;
+            position: relative;
+
+            padding: 9.375rem 3rem;
+
+            @media screen and (max-width: 768px) {
+              padding: 9.375rem 1.5rem;
+            }
+          \`;
+
+          const Logo = styled.img\`
+            height: 55px;
+            object-fit: cover;
+            margin: 8px;
+          \`;
+
+          const Content = styled.div\`
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 2.5rem;
+
+            margin: 0 auto;
+          \`;
+
+          return (
+            <>
+              <HeaderContainer>
+                <Content>
+                  <Logo src={imageUrl} />
+                  <Button variant="primary">
+                    <a
+                      style={{
+                        textDecoration: "none",
+                        color: "#000",
+                      }}
+                      href={\`{
+                        props.buttonLink ?? "https://nearbuilders.org"
+                      }\`}
+                    >
+                      <b>{props.buttonText ?? "START"}</b>
+                    </a>
+                  </Button>
+                </Content>
+              </HeaderContainer>
+              <Widget src="hack.near/widget/src.footer" />
+            </>
+          );`,
+      },
+      "page.docs": {
+        "": `const { currentPath, page, ...passProps } = props;
+          const { routes } = Social.getr("${context.accountId}/widget/page.docs/config/routes") ?? {
+            type: "app",
+            routes: {
+              guide: {
+                path: "buildhub.near/widget/Resources",
+                blockHeight: "final",
+                init: {
+                  name: "Guide",
+                  icon: "bi-map",
+                  mdPath:
+                    "https://raw.githubusercontent.com/NEARBuilders/gateway/main/resources.md",
+                },
+              },
+            },
+          };
+
+          const { SidebarLayout } = VM.require("apps.near/widget/template.sidebar") || {
+            SidebarLayout: () => <></>,
+          };
+
+          if (!page) page = Object.keys(routes)[0] || "main";
+
+          const Root = styled.div``;
+
+          function Router({ active, routes }) {
+            const routeParts = active.split(".");
+
+            let currentRoute = routes;
+            let src = "";
+            let defaultProps = {};
+
+            for (let part of routeParts) {
+              if (currentRoute[part]) {
+                currentRoute = currentRoute[part];
+                src = currentRoute.path;
+
+                if (currentRoute.init) {
+                  defaultProps = { ...defaultProps, ...currentRoute.init };
+                }
+              } else {
+                return <p>ERROR: NOTHING FOUND</p>;
+              }
+            }
+
+            return (
+              <div key={active}>
+                <Widget src={src} props={{ ...passProps, ...defaultProps }} />
+              </div>
+            );
+          }
+
+          const Container = styled.div\`
+            height: 100%;
+          \`;
+
+          const Content = styled.div\`
+            width: 100%;
+            height: 100%;
+          \`;
+
+          return (
+            <Root>
+              <Container>
+                <SidebarLayout currentPath={currentPath} page={page} routes={routes}>
+                  <Content>
+                    <Router active={page} routes={routes} />
+                  </Content>
+                </SidebarLayout>
+              </Container>
+            </Root>
+          );`,
+      },
+      "page.feed": {
+        "": `const { currentPath, page, ...passProps } = props;
+          const { Post } = VM.require("buildhub.near/widget/components") || {
+            Post: () => <></>,
+          };
+
+          function formatDate(date) {
+            const options = { year: "numeric", month: "short", day: "numeric" };
+            return date.toLocaleDateString("en-US", options);
+          }
+
+          const { routes } = Social.getr("${context.accountId}/widget/page.feed/config/routes") ?? {
+            type: "app",
+            routes: {
+              all: {
+                path: "hack.near/widget/feed",
+                blockHeight: "final",
+                init: {
+                  name: "All",
+                  icon: "bi-list",
+                  requiredHashtags: ["build"],
+                },
+              },
+              events: {
+                path: "buildhub.near/widget/events.Calendar",
+                blockHeight: "final",
+                init: {
+                  name: "Events",
+                  icon: "bi-calendar",
+                },
+              },
+            },
+          };
+
+          const { SidebarLayout } = VM.require("apps.near/widget/template.sidebar") || {
+            SidebarLayout: () => <></>,
+          };
+
+          if (!page) page = Object.keys(routes)[0] || "main";
+
+          const Root = styled.div``;
+
+          function Router({ active, routes }) {
+            const routeParts = active.split(".");
+
+            let currentRoute = routes;
+            let src = "";
+            let defaultProps = {};
+
+            for (let part of routeParts) {
+              if (currentRoute[part]) {
+                currentRoute = currentRoute[part];
+                src = currentRoute.path;
+
+                if (currentRoute.init) {
+                  defaultProps = { ...defaultProps, ...currentRoute.init };
+                }
+              } else {
+                return <p>ERROR: NOTHING FOUND</p>;
+              }
+            }
+
+            return (
+              <div key={active}>
+                <Widget src={src} props={{ ...passProps, ...defaultProps }} />
+              </div>
+            );
+          }
+
+          const Container = styled.div\`
+            height: 100%;
+          \`;
+
+          const Content = styled.div\`
+            width: 100%;
+            height: 100%;
+          \`;
+
+          return (
+            <Root>
+              <Container>
+                <SidebarLayout currentPath={currentPath} page={page} routes={routes}>
+                  <Content>
+                    <Router active={page} routes={routes} />
+                  </Content>
+                </SidebarLayout>
+              </Container>
+            </Root>
+          );`,
+      },
+      metadata: {
+        name: state.name,
+        image: state.image,
+        tags: {
+          build: "",
         },
-        config: {
-          type: "app",
-          routes: state.routes,
-        },
+      },
+      config: {
+        type: "app",
+        routes: state.routes,
       },
     },
   });
@@ -146,18 +401,6 @@ const routeData = {
 
 return (
   <>
-    <div className="m-2">
-      <h3>Create App</h3>
-      <p>
-        <i>
-          This no-code builder can help anyone launch their own project based on
-          a common data structure for open web apps, courtesy of
-          <a href="https://everything.dev">everything.dev</a> and{" "}
-          <a href="https://nearbuilders.org">Build DAO</a>
-          contributors.
-        </i>
-      </p>
-    </div>
     <div className="row">
       <div className="col-5">
         <div className="m-2">
