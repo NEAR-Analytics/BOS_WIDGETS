6ab5fadfa390f4f6f0a1abbfbae821c04d069159
dropcast.near <dropcast.near>
2024-02-21 05:12:26 +0000

    Update manager by dropcast.near at 2024-02-21T05:12:26.585Z

--- dropcast.near/manager/source_code.jsx
+++ dropcast.near/manager/source_code.jsx
@@ -35,6 +35,9 @@ const GridWrapper = styled.div`
   @media (max-width: 510px) {
     grid-template-columns: repeat(1,minmax(0,1fr));
   }
+  @media (max-width: 1400px) {
+    grid-template-columns: repeat(2,minmax(0,1fr));
+  }
 `;
 State.init({
   list: [],
