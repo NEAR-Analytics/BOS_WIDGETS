ba7b7ff1b452ee781a923b91881d5255b5a9633e
dropcast.near <dropcast.near>
2024-02-21 15:22:05 +0000

    Update detail by dropcast.near at 2024-02-21T15:22:05.596Z

--- dropcast.near/detail/source_code.jsx
+++ dropcast.near/detail/source_code.jsx
@@ -1,1 +1,98 @@
-return <div>Hello World</div>;
+const Owner = "dropcast.near";
+const API_URL = props.API_URL || "http://localhost:3000";
+const project = props.project || "";
+
+//Styles
+const Wrapper = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    padding: 64px;
+    position: relative;
+    align-items: stretch;
+    flex-direction: column;
+    color: rgb(229 229 229);
+    background: rgb(23,23,23);
+    @media (max-width: 620px) {
+      padding: 29px;
+    }
+`;
+
+const GridWrapper = styled.div`
+  gap: 10px;
+  display: grid;
+  grid-template-columns: repeat(3,minmax(0,1fr));
+  @media (max-width: 935px) {
+    grid-template-columns: repeat(1,minmax(0,1fr));
+  }
+  @media (min-width: 935px) and (max-width: 1400px) {
+    grid-template-columns: repeat(2,minmax(0,1fr));
+  }
+`;
+
+const Card = styled.div`
+    padding: 20px;
+    border-radius: 8px;
+    text-align: center;
+    background-color: rgb(38, 38, 38);
+    box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px;
+`;
+
+const Status = styled.span`
+    width: 100%;
+    padding: 10px 24px;
+    border-radius: 8px;
+    background-image: linear-gradient(to right, rgb(250, 204, 21), rgb(234, 88, 12));
+`;
+
+State.init({
+  avatar: project.icon,
+});
+
+const handleImageNotFound = (e) => {
+  State.update({
+    avatar: "https://dropcast.nearverselabs.com/comingsoon.jpg",
+  });
+};
+
+return (
+  <Wrapper>
+    <img
+      style={{ height: 192 }}
+      className="w-100 object-fit-cover rounded-3"
+      src={
+        "https://cdn.discordapp.com/icons/988431580538224641/e77dc47375e528b351f7ec287be40080.png?size=1024"
+      }
+      onError={handleImageNotFound}
+    />
+    <div className="text-center px-2 py-3">
+      <h5>{"Jump Defi"}</h5>
+      <p className="m-0">
+        {`Shardeez are “sharded blocks”
+       on a mission. Shardeez defend the 
+       ecosystems that matter to all life—to
+        bring awareness an...
+`}
+      </p>
+    </div>
+
+    <hr />
+
+    <div>
+      <h5 style={{ fontSize: 18 }}>{"Jump Defi"}</h5>
+      <p
+        style={{ color: "grey" }}
+      >{`Please take a look at the whitelist requirements below and see if you are eligible to get whitelist.`}</p>
+      <GridWrapper>
+        <Card>
+          <h5>Obtain the Jump Defi</h5>
+          <h5>Discord role</h5>
+          <p
+            style={{ color: "grey" }}
+          >{`If you have this role in the discord server, you are automatically eligible for whitelist`}</p>
+          <Status>eligible</Status>
+        </Card>
+      </GridWrapper>
+    </div>
+  </Wrapper>
+);
