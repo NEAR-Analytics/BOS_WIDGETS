20f245a6279d0044b6cb85b865983e46b72d641e
lord1.near <lord1.near>
2024-02-21 17:12:55 +0000

    Update component by lord1.near at 2024-02-21T17:12:55.983Z

--- lord1.near/component/source_code.jsx
+++ lord1.near/component/source_code.jsx
@@ -547,12 +547,7 @@ let secondSection = (
                         description:
                           "Total number of BOS development transactions for each component",
                       },
-                      {
-                        title: "Deposits",
-                        key: "deposit",
-                        description:
-                          "Deposited Near to (social.near) contract for BOS development. If you notice that some of your components have the same amount of deposits, it is likely that you have used multi-action transactions, which means that a single transaction resulted in multiple updates or builds.",
-                      },
+
                       {
                         title: "Update Tnxs",
                         key: "update",
