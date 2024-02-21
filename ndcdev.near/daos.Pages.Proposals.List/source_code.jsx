0566e22db4ab0884cd41326ce0ff6310168fe641
ndcdev.near <ndcdev.near>
2024-02-21 22:38:49 +0000

    Update daos.Pages.Proposals.List by ndcdev.near at 2024-02-21T22:38:49.170Z

--- ndcdev.near/daos.Pages.Proposals.List/source_code.jsx
+++ ndcdev.near/daos.Pages.Proposals.List/source_code.jsx
@@ -2,44 +2,53 @@ let { contractName } = VM.require(`ndcdev.near/widget/daos.Config`);
 
 if (!contractName) return <Widget src="flashui.near/widget/Loading" />;
 
-let { id, daoId, type } = props;
-daoId = daoId ?? 1;
+let { id, dao_id, type } = props;
 
 const Container = styled.div`
   width: 100%;
   height: max-content;
-  padding: 3rem;
+  padding: 3rem 0;
 
   @media screen and (max-width: 786px) {
     padding: 1rem;
   }
 `;
 
-// const items = Near.view(contractName, "get_all_posts", {
-//   page: 0,
-//   limit: 100,
-// });
+let items = null;
 
-const items = Near.view(contractName, "get_dao_posts", {
-  dao_id: parseInt(daoId),
-  status: "InReview",
-});
+if (dao_id)
+  items = Near.view(contractName, "get_dao_posts", {
+    dao_id: parseInt(dao_id),
+  });
+else if (id)
+  items = [Near.view(contractName, "get_post_by_id", { id: parseInt(id) })];
+else items = Near.view(contractName, "get_all_posts", { page: 0, limit: 100 });
 
 if (!items) return <Widget src="flashui.near/widget/Loading" />;
 
 return (
   <Container>
-    {daoId && (
+    {id ? (
+      <Widget
+        src="ndcdev.near/widget/daos.Components.Item"
+        props={{
+          item: items[0],
+          index: items[0].id,
+          type,
+          id: items[0].id,
+          showMoreDefault: items[0].id,
+          showCommentsDefault: true,
+        }}
+      />
+    ) : (
       <>
         <div className="mb-4">
           <a
-            style={{ background: "#A4C2FD" }}
-            className="btn-primary"
-            href={`/ndcdev.near/widget/daos.App?page=create_proposal&daoId=${daoId}`}
+            style={{ fontSize: "24px" }}
+            className="btn-primary text-uppercase"
+            href={`/ndcdev.near/widget/daos.App?page=create_proposal&dao_id=${dao_id}`}
           >
-            <span style={{ fontSize: "24px", width: "20%", margin: "auto" }}>
-              CREATE POST
-            </span>
+            create {type}
           </a>
         </div>
         <div className="d-flex flex-column gap-4">
@@ -49,7 +58,7 @@ return (
               .map((item, index) => (
                 <Widget
                   src="ndcdev.near/widget/daos.Components.Item"
-                  props={{ item, index, type }}
+                  props={{ item, index, type, id: item.id }}
                 />
               ))}
         </div>
