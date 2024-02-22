4ccd2db36edd2d156e173bb766a2c31561d20ad1
ndcdev.near <ndcdev.near>
2024-02-22 23:02:11 +0000

    Update daos.Layouts.App by ndcdev.near at 2024-02-22T23:02:11.850Z

--- ndcdev.near/daos.Layouts.App/source_code.jsx
+++ ndcdev.near/daos.Layouts.App/source_code.jsx
@@ -141,6 +141,15 @@ const Container = styled.div`
   .blue {
     color: rgb(146 168 210);
   }
+
+  @keyframes scroll {
+    0% {
+      transform: translate(0%);
+    }
+    100% {
+      transform: translate(-100%);
+    }
+  }
 `;
 
 const Wrapper = styled.div`
@@ -156,15 +165,17 @@ function AppLayout({ page, props, children }) {
     <Theme>
       <Container>
         <Widget src={`ndcdev.near/widget/daos.Components.NavBar`} />
-        <Wrapper>
-          {page !== "home" && (
+        {page === "home" ? (
+          children
+        ) : (
+          <Wrapper>
             <Widget
               src={`ndcdev.near/widget/daos.Components.TopNavBar`}
               props={props}
             />
-          )}
-          {children}
-        </Wrapper>
+            {children}
+          </Wrapper>
+        )}
         <Widget
           src={`ndcdev.near/widget/daos.Components.Footer`}
           props={{ page }}
