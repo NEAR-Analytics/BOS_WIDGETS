31e4897747490764f6c7e3ea73a3292576389d87
embeds.near <embeds.near>
2024-02-22 19:49:53 +0000

    Update Feed by embeds.near at 2024-02-22T19:49:53.229Z

--- embeds.near/Feed/source_code.jsx
+++ embeds.near/Feed/source_code.jsx
@@ -1,6 +1,9 @@
 const { Feed } = VM.require("devs.near/widget/Module.Feed");
+
 Feed = Feed || (() => <></>); // make sure you have this or else it can break
+
 const [filterAccountId, setFilterAccountId] = useState(context.accountId);
+
 return (
   <>
     <input
