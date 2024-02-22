e07372c06515ec825be19f7b61cde6ca4db68a82
ndcdev.near <ndcdev.near>
2024-02-22 23:02:11 +0000

    Update daos.Pages.Comment by ndcdev.near at 2024-02-22T23:02:11.850Z

--- ndcdev.near/daos.Pages.Comment/source_code.jsx
+++ ndcdev.near/daos.Pages.Comment/source_code.jsx
@@ -1,5 +1,7 @@
 let { contractName } = VM.require(`ndcdev.near/widget/daos.Config`);
 
+if (!contractName) return <Widget src="flashui.near/widget/Loading" />;
+
 let { post_id, comment_id } = props;
 
 const Container = styled.div`
