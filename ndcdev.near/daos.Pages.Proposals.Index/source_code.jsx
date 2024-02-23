425ce8f59e19c9035e866fbcd3259607f80849f9
ndcdev.near <ndcdev.near>
2024-02-23 15:47:59 +0000

    Update daos.Pages.Proposals.Index by ndcdev.near at 2024-02-23T15:47:59.033Z

---
+++ ndcdev.near/daos.Pages.Proposals.Index/source_code.jsx
@@ -0,0 +1,23 @@
+let { contractName } = VM.require(`ndcdev.near/widget/daos.Config`);
+
+if (!contractName) return <Widget src="flashui.near/widget/Loading" />;
+
+let { id } = props;
+
+const post = Near.view(contractName, "get_post_by_id", {
+  id: parseInt(id),
+});
+
+return (
+  <Widget
+    src="ndcdev.near/widget/daos.Components.Item"
+    props={{
+      item: post,
+      index: post.id,
+      type: post.post_type,
+      id: post.id,
+      showMoreDefault: post.id,
+      showCommentsDefault: true,
+    }}
+  />
+);
