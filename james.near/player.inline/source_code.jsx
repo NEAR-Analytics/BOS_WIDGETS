961598ae459a8f31cc0e5f9b22122d3b4670e353
james.near <james.near>
2024-02-24 18:51:39 +0000

    Update player.inline by james.near at 2024-02-24T18:51:39.473Z

--- james.near/player.inline/source_code.jsx
+++ james.near/player.inline/source_code.jsx
@@ -1,4 +1,4 @@
-const accountId = props.accountId ?? context.accountId;
+const accountId = props.accountId ?? context.accountId ?? "james.near";
 
 const player = props.player ?? Social.getr(`${accountId}/game/player`);
 
