6cb074ad76919397cfed7388e354f6afff859069
nearcatalog.near <nearcatalog.near>
2024-02-24 11:37:54 +0000

    Update NearCatalog.Layout.Trending by nearcatalog.near at 2024-02-24T11:37:54.073Z

--- nearcatalog.near/NearCatalog.Layout.Trending/source_code.jsx
+++ nearcatalog.near/NearCatalog.Layout.Trending/source_code.jsx
@@ -1,46 +1,64 @@
+console.log("trending props:  ", props);
 
 const Css = styled.div`
 .awesome-trending-content{
     overflow-x: scroll; 
     text-align: center;
 }
+
+.awesome-trending-content .near-item-sm { 
+    display: inline-block; 
+    float: none; 
+}
+
 @media screen and ( max-width : 700px ){
     .awesome-trending-content{white-space: nowrap !important;} 
 }
-.awesome-trending-content .near-item-sm { 
-   display: inline-block; 
-   float: none; 
-}
+
+
 `;
 let query = false;
 State.init({
-    projects: false
+  projects: false,
 });
-asyncFetch(props.indexer + "/projects-by-category?cid=trending").then(res => {
-    State.update({ projects: res.body });
-    console.log("Trending: ", res.body);
+asyncFetch(props.indexer + "/projects-by-category?cid=trending").then((res) => {
+  State.update({ projects: res.body });
+  console.log("Trending: ", res.body);
 });
 
 if (!state.projects) {
-    return "🔥🔥🔥";
+  return "🔥🔥🔥";
 }
 // const projects = query.body;
 return (
-    <Css>
-        <h3 className="my-3">🔥Trending</h3>
-        <div className="awesome-trending-content overflow-auto">
-            {
-                Object.keys(state.projects).map((e) => {
-                    let p = state.projects[e];
-                    return (<a className="near-item-sm" title={p.profile.name} href={`/${props.indexPath}?id=${e}`}>
-                        <div className="tile-icon"><img src={p.profile.image?.url || props.defaultImg}
-                            alt={p.profile.name} /></div>
-                        <div className="tile-content">
-                            <h2 className="tile-title">{p.profile.name}</h2>
-                        </div>
-                    </a>)
-                })
-            }
-        </div>
-    </Css>
+  <Css>
+    <h3 className="my-3">🔥Trending</h3>
+    <div
+      className="awesome-trending-content overflow-auto"
+      style={{
+        whiteSpace: props.cat?.length > 0 ? "nowrap" : "",
+      }}
+    >
+      {Object.keys(state.projects).map((e) => {
+        let p = state.projects[e];
+        return (
+          <a
+            className="near-item-sm"
+            title={p.profile.name}
+            href={`/${props.indexPath}?id=${e}`}
+          >
+            <div className="tile-icon">
+              <img
+                src={p.profile.image?.url || props.defaultImg}
+                alt={p.profile.name}
+              />
+            </div>
+            <div className="tile-content">
+              <h2 className="tile-title">{p.profile.name}</h2>
+            </div>
+          </a>
+        );
+      })}
+    </div>
+  </Css>
 );
