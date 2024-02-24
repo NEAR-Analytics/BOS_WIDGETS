068952abc4c81ccf3055db0565d428f762675bc3
dawnkelly.near <dawnkelly.near>
2024-02-24 17:51:46 +0000

    Update Character-RPG-Basic by dawnkelly.near at 2024-02-24T17:51:46.718Z

--- dawnkelly.near/Character-RPG-Basic/source_code.jsx
+++ dawnkelly.near/Character-RPG-Basic/source_code.jsx
@@ -1,27 +1,32 @@
 const character = props.character;
 
 return (
-  <div class="container">
+  <div class="card">
     <div key={character.id}>
-      <h2>{character.name}</h2>
-      <img
-        src={character.image}
-        alt={character.alt}
-        width="200"
-        height="200"
-      ></img>
-      <div>
-        <span>Species: {character.species} | </span>
-        <span>Class: {character.class}</span>
+      <div class="card-header">
+        <h2>{character.name}</h2>
       </div>
-      <div>
-        <span>Default Weapon: {character.defaultWeapon}</span>
-      </div>
-      <div>
-        <span>Skills: {character.skills}</span>
-      </div>
-      <div>
-        <span>Weakness: {character.weakness}</span>
+      <div class="card-body">
+        <h2 class="card-title">{character.name}</h2>
+        <img
+          src={character.image}
+          alt={character.alt}
+          width="200"
+          height="200"
+        ></img>
+        <div>
+          <span>Species: {character.species} | </span>
+          <span>Class: {character.class}</span>
+        </div>
+        <div>
+          <span>Default Weapon: {character.defaultWeapon}</span>
+        </div>
+        <div>
+          <span>Skill: {character.skills}</span>
+        </div>
+        <div>
+          <span>Weakness: {character.weakness}</span>
+        </div>
       </div>
     </div>
   </div>
