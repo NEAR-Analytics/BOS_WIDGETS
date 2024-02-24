c23d187db3add8ac1f00de365da9c41754ef8eff
james.near <james.near>
2024-02-24 18:09:41 +0000

    Update player.inline by james.near at 2024-02-24T18:09:41.819Z

--- james.near/player.inline/source_code.jsx
+++ james.near/player.inline/source_code.jsx
@@ -1,28 +1,30 @@
-const accountId = props.accountId;
+const accountId = props.accountId ?? context.accountId;
 
 const player = props.player ?? Social.getr(`${accountId}/game/player`);
 
-const name = player.name;
-const image = player.image;
+const name = player.metadata.name;
+const image = player.metadata.image;
 
 return (
-  <div className="d-flex flex-row">
-    <Widget
-      src="mob.near/widget/ProfileImage"
-      props={{
-        profile: player,
-        accountId,
-        style: { height: "2.5em", width: "2.5em", minWidth: "2.5em" },
-        className: "me-2",
-      }}
-    />
-    <div className="text-truncate lh-sm">
-      <div className="text-truncate fw-bold">{name}</div>
-      <div className="text-truncate text-muted">
-        <small>
-          <span className="font-monospace">@{accountId}</span>
-        </small>
+  <>
+    <p>{JSON.stringify(player)}</p>
+    <div className="d-flex flex-row">
+      <Widget
+        src="mob.near/widget/Image"
+        props={{
+          image,
+          style: { height: "2.5em", width: "2.5em", minWidth: "2.5em" },
+          className: "me-2",
+        }}
+      />
+      <div className="text-truncate lh-sm">
+        <div className="text-truncate fw-bold">{name}</div>
+        <div className="text-truncate text-muted">
+          <small>
+            <span className="font-monospace">@{accountId}</span>
+          </small>
+        </div>
       </div>
     </div>
-  </div>
+  </>
 );
