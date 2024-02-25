84dc562193c543e74fb9a8bcca3c1abf0e9b83e1
youtick.near <youtick.near>
2024-02-25 14:30:26 +0000

    Update youtick by youtick.near at 2024-02-25T14:30:26.706Z

--- youtick.near/youtick/source_code.jsx
+++ youtick.near/youtick/source_code.jsx
@@ -1,1 +1,11 @@
-return <div>youtick</div>;
+return (
+  <>
+    <iframe
+      src="https://youtick.net"
+      title="YouTick"
+      width="100%"
+      height="100%"
+      frameBorder="0"
+    />
+  </>
+);
