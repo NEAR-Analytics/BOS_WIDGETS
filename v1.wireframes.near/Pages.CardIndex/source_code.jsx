dd815a630b2523fd847b642d05fd9d21a3bdc4a5
v1.wireframes.near <v1.wireframes.near>
2024-02-26 15:27:12 +0000

    Update Pages.CardIndex by v1.wireframes.near at 2024-02-26T15:27:12.923Z

---
+++ v1.wireframes.near/Pages.CardIndex/source_code.jsx
@@ -0,0 +1,60 @@
+/* -------------------------------------------------------------------------- */
+/*
+ __        ___           __                               
+ \ \      / (_)_ __ ___ / _|_ __ __ _ _ __ ___   ___  ___ 
+  \ \ /\ / /| | '__/ _ \ |_| '__/ _` | '_ ` _ \ / _ \/ __|
+   \ V  V / | | | |  __/  _| | | (_| | | | | | |  __/\__ \
+    \_/\_/  |_|_|  \___|_| |_|  \__,_|_| |_| |_|\___||___/
+
+  =========================================================
+  * Wireframes - v1.0.0
+  =========================================================
+  * Product Page: https://wireframes.design
+  =========================================================
+  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+                                                                                 */
+
+/* -------------------------------------------------------------------------- */
+
+                                                         
+
+
+const Container = ({ children }) => {
+    return (
+        <Widget
+        src={`v1.wireframes.near/widget/Components.Layout.Container`}
+        props={children}
+      />
+    );
+  };
+
+ 
+
+  const data ={children : (<>
+    <div className="p-4 d-flex justify-content-center">
+        <div className="row d-flex justify-content-center" style={{ paddingTop:"inherit" }}>
+           
+        <h1 className="pt-4">Simple Card</h1>
+    <Widget src={`v1.wireframes.near/widget/Components.Card.CardDetail.CardDetail`}/>  
+  
+    </div>    
+    </div>
+
+    </>)} ;
+
+return(
+    <>
+     <Widget src={`v1.wireframes.near/widget/Pages.Navigation.Navbar`}/>
+     <div style={{ height:"200vh" }}>
+    <Widget
+        src={`v1.wireframes.near/widget/Components.Layout.Container`}
+        props={data}
+      />
+      </div>
+<Widget
+        src={`v1.wireframes.near/widget/Components.Navigations.Footer`}
+      />
+
+    </>
+);
