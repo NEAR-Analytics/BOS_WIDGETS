606981ebf98971b90b862eab9987eeed176ed4e0
wireframes.near <wireframes.near>
2024-02-26 17:44:46 +0000

    Update Index by wireframes.near at 2024-02-26T17:44:46.246Z

--- wireframes.near/Index/source_code.jsx
+++ wireframes.near/Index/source_code.jsx
@@ -21,91 +21,83 @@
 
 
 const Container = ({ children }) => {
-    return (
-        <Widget
-        src={`v1.wireframes.near/widget/Components.Layout.Container`}
-        props={children}
-      />
-    );
-  };
+  return (
+      <Widget
+      src={`v1.wireframes.near/widget/Components.Layout.Container`}
+      props={children}
+    />
+  );
+};
 
-  const start = {
-    buttonTitle: "GET STARTED",
-    style: {
-        width:"auto"
-    },
-    onClick: () => {},
-    type: "button",
-  };
 
-  const why = {
-    buttonTitle: "WHY WIREFRAMES",
-    style: {
-        width:"auto"
-    },
-    onClick: () => {},
-    type: "button",
-  };
+const data ={children : (<>
+  <div className="p-4 d-flex justify-content-center">
+      <div className="row d-flex justify-content-center" style={{ paddingTop:"inherit" }}>
+          <div className="pt-5 row d-flex justify-content-center">
+          <img src="https://wireframes.design/wireframes-logo.png" style={{ height:"100px",width:"90%" }}/>
+          </div>
+  <h1 className="pt-5 d-flex justify-content-center">Wireframes Bos Component Theme</h1>
+      <p className="d-flex justify-content-center">Explore our collection today and unlock the potential of your web design journey with Wireframes.</p>
+  
+      <div className="pt-5 row d-flex justify-content-center">
+  
+  </div>
 
-  const gitHub = {
-    buttonTitle: "GITHUB",
-    style: {
-        width:"auto"
-    },
-    onClick: () => {},
-    type: "button",
-  };
+  <Widget
+          src="v1.wireframes.near/widget/Components.Button.GlowButton"
+          props={{ 
+            buttonTitle: "Get Started",
+            buttonHref:"/embed/v1.wireframes.near/widget/Pages.ButtonIndex",
+            type: "button",
+            style:{
+              width:"10em",
+            },
+            inlineStyle:{
+              textDecoration:"none"
+            }
+           }}
+        />
+  <hr className="mt-5"/>
+      <p className="d-flex justify-content-center">Our team have been working on something amazing.</p>
+  
 
-  const data ={children : (<>
-    <div className="p-4 d-flex justify-content-center">
-        <div className="row d-flex justify-content-center">
-            <div className="pt-5 row d-flex justify-content-center">
-            <img src="https://firebasestorage.googleapis.com/v0/b/kaliba-testing-web.appspot.com/o/wireframes-high-resolution-logo-transparent.png?alt=media&token=00bec9dc-6d8f-433b-aae6-7cf5d97f3a7b" style={{ height:"100px",width:"90%" }}/>
-            </div>
-    <h1 className="pt-5 d-flex justify-content-center">Wireframes Bos Component Theme</h1>
-        <p className="d-flex justify-content-center">Explore our collection today and unlock the potential of your web design journey with Wireframes.</p>
-    
-        <div className="pt-5 row d-flex justify-content-center">
-    
-    </div>
-    <hr/>
-    <h2 className="pt-5 d-flex justify-content-center">We are comming soon</h2>
-        <p className="d-flex justify-content-center">Our team have been working on something amazing.</p>
-    
+  </div>
+  
+  </div>
 
-    </div>
-    
-    </div>
-
-    </>)} ;
+  </>)} ;
 
 const navData ={
-    navMode:"light",
-    logoHref : "#",
-    companyName:"Wireframes",
-    inlineStyle:{
-        boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
-        fontFamily:"Exo"
-    },
-    linksData:[
-       ],
+  navMode:"light",
+  logoHref : "https://wireframes.design",
+  companyName:"Wireframes",
+  navStyle:{
+    background:"white",
+    boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
+  },
+  inlineStyle:{
+      boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+      fontFamily:"Exo"
+  },
+  linksData:[
+     ],
 
-  };
+};
 return(
-    <>
-     <Widget
-        src={`v1.wireframes.near/widget/Components.Navigations.Navbar`}
-        props={navData}
-      />
-      <div className="" style={{ height:"100svh" }}>
-    <Widget
-        src={`v1.wireframes.near/widget/Components.Layout.Container`}
-        props={data}
-      />
-    </div>
+  <>
+   <Widget
+      src={`v1.wireframes.near/widget/Components.Navigations.Navbar`}
+      props={navData}
+    />
+    <div className="" style={{ height:"100svh" }}>
+  <Widget
+      src={`v1.wireframes.near/widget/Components.Layout.Container`}
+      props={data}
+    />
+  </div>
 <Widget
-        src={`v1.wireframes.near/widget/Components.Navigations.Footer`}
-      />
+      src={`v1.wireframes.near/widget/Components.Navigations.Footer`}
+    />
 
-    </>
+  </>
 );
