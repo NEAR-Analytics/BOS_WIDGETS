ed043ea08b178a53e51fdfe0eccef767f3f902ca
nearcatalog.near <nearcatalog.near>
2024-02-26 07:59:04 +0000

    Update Index by nearcatalog.near at 2024-02-26T07:59:04.419Z

--- nearcatalog.near/index/source_code.jsx
+++ nearcatalog.near/index/source_code.jsx
@@ -1,8 +1,7 @@
 const owner = "nearcatalog.near";
 const componentPath = `${owner}/widget/NearCatalog`;
 const indexer = "https://nearcatalog.sctuts.com/wp-json/nearcatalog/v1";
-const defaultImg =
-  "https://learnnear.club/wp-content/uploads/2021/09/lnc-profile-desktop-150x150.png";
+const defaultImg = "https://learnnear.club/wp-content/uploads/2021/09/lnc-profile-desktop-150x150.png";
 
 const projects = {};
 let router = "home";
@@ -12,72 +11,75 @@ let widgetSrc = "";
 let widgetProps = {};
 
 if (props?.bookmark) {
-  router = "bookmark";
+    router = "bookmark";
 } else if (props?.cat.length > 0) {
-  router = "category";
+    router = "category"
 } else if (props?.id) {
-  router = "project";
+    router = "project";
 }
 
-function loadingScreen() {
-  return <>👀📗~~, NEARCatalog - Ecosystem map for #NEAR </>;
+function loadingScreen(){
+    return <>👀📗~~</>;
 }
 
 console.log("router: ", router);
 
 switch (router) {
-  case "project":
-    query = fetch(indexer + "/project?pid=" + props.id);
-    if (!query || !query.body) {
-      return loadingScreen();
-    }
-    widgetSrc = `${componentPath}.Project`;
-    widgetProps = {
-      id: props.id,
-      project: query.body,
-    };
-    break;
+    case "project":
+        query = fetch(indexer + "/project?pid="+props.id);
+        if (!query || !query.body) {
+            return loadingScreen();
+        }
+        widgetSrc = `${componentPath}.Project`;
+        widgetProps = {
+            id: props.id,
+            project: query.body,
+        }
+        break;
 
-  case "category":
-    query = fetch(indexer + "/projects-by-category?cid=" + props.cat);
-    projects = query.body;
-    if (!query) {
-      return loadingScreen();
-    }
-    widgetSrc = `${componentPath}.Layout.AppGrid`;
-    widgetProps = {
-      cat: props.cat,
-    };
-    break;
+    case "category":
+        query = fetch(indexer + "/projects-by-category?cid=" + props.cat);
+        projects = query.body;
+        if (!query) {
+            return loadingScreen();
+        }
+        widgetSrc = `${componentPath}.Layout.AppGrid`;
+        widgetProps = {
+            cat: props.cat
+        }
+        break;
 
-  case "bookmark":
-    widgetSrc = `${componentPath}.Layout.AppGrid`;
-    widgetProps = {
-      bookmark: true,
-    };
-    break;
+    case "bookmark":
+        widgetSrc = `${componentPath}.Layout.AppGrid`;
+        widgetProps = {
+            bookmark: true,
+        }
+        break;
 
-  default:
-    //home router
-    query = fetch(indexer + "/projects");
-    if (!query || !query.body) {
-      return loadingScreen();
-    }
-    projects = query.body;
-    widgetSrc = `${componentPath}.Layout.AppGrid`;
-    break;
+    default:
+        //home router 
+        query = fetch(indexer + "/projects");
+        if (!query || !query.body) {
+            return loadingScreen();
+        }
+        projects = query.body;
+        widgetSrc = `${componentPath}.Layout.AppGrid`;
+        break;
+
 }
 
 let p = {
-  ...{
-    componentPath,
-    indexPath: owner + "/widget/Index",
-    owner,
-    indexer,
-    projects,
-    defaultImg,
-  },
-  ...widgetProps,
+    ...{
+        componentPath,
+        indexPath: owner + "/widget/Index",
+        owner,
+        indexer,
+        projects,
+        defaultImg,
+        router
+    }, ...widgetProps
 };
 console.log("final props", p);
-return <Widget src={widgetSrc} props={p} />;
+return (
+        <Widget src={widgetSrc} props={p} />
+);
