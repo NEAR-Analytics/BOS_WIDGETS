c52e14c3f579b2a9cd3244546dea914a438be041
nearcatalog.near <nearcatalog.near>
2024-02-26 07:59:04 +0000

    Update NearCatalog.Layout.Trending by nearcatalog.near at 2024-02-26T07:59:04.419Z

--- nearcatalog.near/NearCatalog.Layout.Trending/source_code.jsx
+++ nearcatalog.near/NearCatalog.Layout.Trending/source_code.jsx
@@ -1,3 +1,4 @@
+
 console.log("trending props:  ", props);
 
 const Css = styled.div`
@@ -19,46 +20,37 @@ const Css = styled.div`
 `;
 let query = false;
 State.init({
-  projects: false,
+    projects: false
 });
-asyncFetch(props.indexer + "/projects-by-category?cid=trending").then((res) => {
-  State.update({ projects: res.body });
-  console.log("Trending: ", res.body);
+if(props.cat == "trending") return <></>; 
+
+asyncFetch(props.indexer + "/projects-by-category?cid=trending").then(res => {
+    State.update({ projects: res.body });
+    console.log("Trending: ", res.body);
 });
 
 if (!state.projects) {
-  return "🔥🔥🔥";
+    return "🔥🔥🔥";
 }
 // const projects = query.body;
 return (
-  <Css>
-    <h3 className="my-3">🔥Trending</h3>
-    <div
-      className="awesome-trending-content overflow-auto"
-      style={{
-        whiteSpace: props.cat?.length > 0 ? "nowrap" : "",
-      }}
-    >
-      {Object.keys(state.projects).map((e) => {
-        let p = state.projects[e];
-        return (
-          <a
-            className="near-item-sm"
-            title={p.profile.name}
-            href={`/${props.indexPath}?id=${e}`}
-          >
-            <div className="tile-icon">
-              <img
-                src={p.profile.image?.url || props.defaultImg}
-                alt={p.profile.name}
-              />
-            </div>
-            <div className="tile-content">
-              <h2 className="tile-title">{p.profile.name}</h2>
-            </div>
-          </a>
-        );
-      })}
-    </div>
-  </Css>
+    <Css>
+        <h3 className="my-3">🔥Trending</h3>
+        <div className="awesome-trending-content overflow-auto" style={{
+            whiteSpace: props.cat?.length > 0 ? "nowrap" : ""
+        }}>
+            {
+                Object.keys(state.projects).map((e) => {
+                    let p = state.projects[e];
+                    return (<Link className="near-item-sm" title={p.profile.name} href={`/${props.indexPath}?id=${e}`}>
+                        <div className="tile-icon"><img src={p.profile.image?.url || props.defaultImg}
+                            alt={p.profile.name} /></div>
+                        <div className="tile-content">
+                            <h2 className="tile-title">{p.profile.name}</h2>
+                        </div>
+                    </Link>)
+                })
+            }
+        </div>
+    </Css>
 );
