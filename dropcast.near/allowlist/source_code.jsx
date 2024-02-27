ba4725892d3474044140b8f42647d53f8a9825c0
dropcast.near <dropcast.near>
2024-02-27 09:35:03 +0000

    Update allowlist by dropcast.near at 2024-02-27T09:35:03.213Z

--- dropcast.near/allowlist/source_code.jsx
+++ dropcast.near/allowlist/source_code.jsx
@@ -21,4 +21,132 @@ const Wrapper = styled.div`
   }
 `;
 
-return <Wrapper>Hello World</Wrapper>;
+const Card = styled.div`
+    gap: 24px;
+    display: flex;
+    width: 100%;
+    padding: 48px;
+    border-radius: 8px;
+    position: relative;
+    flex-direction: column;
+    background: rgb(38, 38, 38);
+    @media (max-width: 620px) {
+      padding: 15px;
+      height: 100%;
+      .menu {
+        width: 74vw;
+      }
+    }  
+`;
+
+const Label = styled.label`
+    font-size: 14px;
+    margin-bottom: 4px;
+`;
+
+const Button = styled.button`
+    color: #FFF;
+    padding: 12px;
+    border-radius: 6px;
+    background-image: linear-gradient(to right, rgb(147, 51, 234), rgb(99, 102, 241));
+`;
+
+const NEAR_TOKEN = [
+  {
+    text: "1 $NEAR",
+    value: "near1",
+  },
+  {
+    text: "10 $NEAR",
+    value: "near2",
+  },
+  {
+    text: "100 $NEAR",
+    value: "near3",
+  },
+  {
+    text: "1000 $NEAR",
+    value: "near4",
+  },
+  {
+    text: "Default",
+    value: "near0",
+  },
+];
+
+const AGE_ACCOUNT = [
+  {
+    text: "1 Month",
+    value: "age1",
+  },
+  {
+    text: "6 Month",
+    value: "age2",
+  },
+  {
+    text: "1 Year",
+    value: "age3",
+  },
+  {
+    text: "2 Year",
+    value: "age4",
+  },
+  {
+    text: "Default",
+    value: "age0",
+  },
+];
+
+const changeOption = (key, value) => {
+  State.update({
+    ...state,
+    [key]: value,
+  });
+};
+
+return (
+  <Wrapper>
+    <Card>
+      <div>
+        <h5 className="m-0" style={{ fontSize: 18 }}>
+          {`Custom Allowlist`}
+        </h5>
+      </div>
+      <div>
+        <Label>{`$NEAR token`}</Label>
+        <Widget
+          props={{
+            noLabel: true,
+            width: "40vw",
+            options: NEAR_TOKEN,
+            value: "near0",
+            onChange: (val) => changeOption("near", val),
+          }}
+          src={`${Owner}/widget/Select`}
+        />
+      </div>
+      <div>
+        <Label>{`Age of the Account`}</Label>
+        <Widget
+          props={{
+            noLabel: true,
+            width: "40vw",
+            options: AGE_ACCOUNT,
+            value: "age0",
+            onChange: (val) => changeOption("age", val),
+          }}
+          src={`${Owner}/widget/Select`}
+        />
+      </div>
+      <Button
+        className="btn"
+        disabled={
+          (type !== "edit" && state.selected === "0") || !state.description
+        }
+        onClick={handleNextStep}
+      >
+        Next Step
+      </Button>
+    </Card>
+  </Wrapper>
+);
