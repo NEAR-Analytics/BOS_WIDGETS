46ca738b2e70eb34e3fe9765e1b44f72493e38dd
dropcast.near <dropcast.near>
2024-02-27 09:19:14 +0000

    Update allowlist by dropcast.near at 2024-02-27T09:19:14.574Z

--- dropcast.near/allowlist/source_code.jsx
+++ dropcast.near/allowlist/source_code.jsx
@@ -1,1 +1,24 @@
-return <div>Hello World</div>;
+const accountId = context.accountId;
+const Owner = "dropcast.near";
+const API_URL = props.API_URL || "http://localhost:3000";
+const USER = props.USER || {};
+const TOKEN = props.TOKEN || "";
+const Logout = props.Logout;
+
+//Styles
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  padding: 64px;
+  position: relative;
+  align-items: stretch;
+  flex-direction: column;
+  color: rgb(229 229 229);
+  background: rgb(23,23,23);
+  @media (max-width: 510px) {
+      padding: 30px;
+  }
+`;
+
+return <Wrapper>Hello World</Wrapper>;
