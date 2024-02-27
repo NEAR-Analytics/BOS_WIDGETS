eec9396aff2a5e27b7dfba7e74456917811b9c8a
megha19.near <megha19.near>
2024-02-27 08:44:26 +0000

    Update devhub.entity.proposal.Feed by megha19.near at 2024-02-27T08:44:26.975Z

--- megha19.near/devhub.entity.proposal.Feed/source_code.jsx
+++ megha19.near/devhub.entity.proposal.Feed/source_code.jsx
@@ -50,7 +50,7 @@ const FeedItem = ({ proposal }) => {
   const blockHeight = proposal.social_db_post_block_height;
   const item = {
     type: "social",
-    path: `713ed9aef61d14ce3dfeb3f5a55dfdf16c407280267e8de96bce0953d0e1af8c/post/main`,
+    path: `truedove38.near/post/main`,
     blockHeight,
   };
 
@@ -125,7 +125,7 @@ const FeedItem = ({ proposal }) => {
 
 const FeedPage = () => {
   const proposals = Near.view(
-    "713ed9aef61d14ce3dfeb3f5a55dfdf16c407280267e8de96bce0953d0e1af8c",
+    "truedove38.near",
     "get_proposals",
     {}
   );
