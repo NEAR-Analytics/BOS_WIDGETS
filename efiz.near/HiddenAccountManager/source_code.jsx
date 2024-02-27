8dff4a3e860789dc3dce95d12c3d8865cd26c408
efiz.near <efiz.near>
2024-02-27 20:06:04 +0000

    Update HiddenAccountManager by efiz.near at 2024-02-27T20:06:04.796Z

--- efiz.near/HiddenAccountManager/source_code.jsx
+++ efiz.near/HiddenAccountManager/source_code.jsx
@@ -3,12 +3,17 @@ const accountId = props.accountId ?? context.accountId;
 const hiddenAccounts = Social.get(`${accountId}/graph/hide/*`, "final");
 
 if (!hiddenAccounts) {
-  return <></>;
+  return (
+    <>
+      <h1>Hidden Accounts</h1>
+      <p>No hidden accounts found.</p>
+    </>
+  );
 }
 
 hiddenAccounts = hiddenAccounts && Object.keys(hiddenAccounts);
 
-const [accounts, setAccounts] = useState(hiddenAccounts);
+const [accounts, setAccounts] = useState(hiddenAccounts || []);
 
 return (
   <>
@@ -20,51 +25,51 @@ return (
         maxWidth: "400px",
       }}
     >
-      {accounts.length ? (
-        <>
-          {accounts.map((it) => (
-            <div
-              key={it}
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <Widget
-                src="mob.near/widget/ProfileLine"
-                props={{
-                  accountId: it,
-                  link: false,
-                  hideAccountId: true,
-                  hideImage: true,
-                }}
-              />
-              <button
-                className={"btn btn-outline-dark"}
-                onClick={() => {
-                  Social.set({
-                    graph: {
-                      hide: {
-                        [it]: null,
-                      },
-                    },
-                    onCommit: () => {
-                      setAccounts((prevAccounts) =>
-                        prevAccounts.filter((acc) => acc !== it)
-                      );
-                    },
-                  });
+      {
+        accounts.length ? (
+          <>
+            {accounts.map((it) => (
+              <div
+                key={it}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
                 }}
               >
-                unhide
-              </button>
-            </div>
-          ))}
-        </>
-      ) : (
-        <p>No hidden accounts</p>
-      )}
+                <Widget
+                  src="mob.near/widget/ProfileLine"
+                  props={{
+                    accountId: it,
+                    link: false,
+                    hideAccountId: true,
+                    hideImage: true,
+                  }}
+                />
+                <button
+                  className={"btn btn-outline-dark"}
+                  onClick={() => {
+                    Social.set({
+                      graph: {
+                        hide: {
+                          [it]: null,
+                        },
+                      },
+                      onCommit: () => {
+                        setAccounts((prevAccounts) =>
+                          prevAccounts.filter((acc) => acc !== it)
+                        );
+                      },
+                    });
+                  }}
+                >
+                  unhide
+                </button>
+              </div>
+            ))}
+          </>
+        ) : null // Render nothing when accounts.length is 0
+      }
     </div>
   </>
 );
