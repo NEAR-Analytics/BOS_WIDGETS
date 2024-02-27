4252e268ea62108384b060bf8c216a613e3b642e
owa-is-bos.near <owa-is-bos.near>
2024-02-27 19:03:14 +0000

    Update Maverick-LP-NewOptionsModal by owa-is-bos.near at 2024-02-27T19:03:14.015Z

--- owa-is-bos.near/Maverick-LP-NewOptionsModal/source_code.jsx
+++ owa-is-bos.near/Maverick-LP-NewOptionsModal/source_code.jsx
@@ -1,7 +1,7 @@
 let FEE = props.fee > 0 ? props.fee : 0.1;
 let WIDTH = props.width > 0 ? props.width : 0.5;
-let POOLNAME1 = props.poolName1.replace("WETH", "ETH");
-let POOLNAME2 = props.poolName2.replace("WETH", "ETH");
+let POOLNAME1 = props.poolName1;
+let POOLNAME2 = props.poolName2;
 const setFeeWidth = props.setFeeWidth;
 const closeModal = props.closeModal;
 console.log(POOLNAME1, POOLNAME2);
