590565c34b3d0e066743ddbd6208826eba92068b
ndcdev.near <ndcdev.near>
2024-02-28 16:05:52 +0000

    Update daos.Components.Dao.Guidance by ndcdev.near at 2024-02-28T16:05:52.613Z

--- ndcdev.near/daos.Components.Dao.Guidance/source_code.jsx
+++ ndcdev.near/daos.Components.Dao.Guidance/source_code.jsx
@@ -5,10 +5,7 @@ const Item = styled.div`
   border: none;
   border-radius: 10px;
   box-shadow: 0px 30px 80px 0px rgba(0, 0, 0, 0.1);
-  
-  @media screen and (max-width: 786px) {
-    height: 350px;
-  }
+
 
   h4 {
     color: #000
@@ -120,7 +117,7 @@ const Container = styled.div`
   }
 
   @media screen and (max-width: 786px) {
-    padding: 2rem;
+    padding: 1rem;
   }
 `;
 
@@ -153,32 +150,36 @@ return (
           props={{ text: section.guidance.description }}
         />
       </div>
-      <div className="d-flex flex-wrap w-100 gap-5">
-        {section.guidance.cards.map((card) => (
-          <Item className="dark">
-            <div className="inner d-flex flex-column justify-content-center gap-3 align-items-lefy">
-              <h4 className="bold color-text px-3 mt-1 text-left">
-                {card.title}
-              </h4>
-              <span className="bold color-text px-3 mt-1 text-left">
-                {card.description}
-              </span>
-              <div className="d-flex px-3">
-                <a href={card.button.link}>{card.button.title}</a>
-                <i className="bi bi-chevron-right" />
+      <div className="d-flex w-100 justify-content-center">
+        <div className="d-flex flex-wrap gap-5">
+          {section.guidance.cards.map((card) => (
+            <Item className="dark">
+              <div className="inner d-flex flex-column justify-content-center gap-3 align-items-lefy">
+                <h4 className="bold color-text px-3 mt-1 text-left">
+                  {card.title}
+                </h4>
+                <span className="bold color-text px-3 mt-1 text-left">
+                  {card.description}
+                </span>
+                <div className="d-flex px-3">
+                  <a href={card.button.link}>{card.button.title}</a>
+                  <i className="bi bi-chevron-right" />
+                </div>
               </div>
-            </div>
-          </Item>
-        ))}
+            </Item>
+          ))}
+        </div>
       </div>
 
-      <div className="d-flex px-3">
-        <SubmitProposal
-          href={`/ndcdev.near/widget/daos.App?page=create_proposal&dao_id=${dao_id}`}
-        >
-          Submit Proposal
-        </SubmitProposal>
-        <i className="bi bi-chevron-right" />
+      <div className="d-flex px-3 w-100 justify-content-center">
+        <div>
+          <SubmitProposal
+            href={`/ndcdev.near/widget/daos.App?page=create_proposal&dao_id=${dao_id}`}
+          >
+            Submit Proposal
+          </SubmitProposal>
+          <i className="bi bi-chevron-right" />
+        </div>
       </div>
     </div>
   </Container>
