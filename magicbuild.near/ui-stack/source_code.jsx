b1d6a9da113f28203836e7e014238fe32b7cf610
magicbuild.near <magicbuild.near>
2024-02-28 18:59:27 +0000

    Update ui-stack by magicbuild.near at 2024-02-28T18:59:27.588Z

--- magicbuild.near/ui-stack/source_code.jsx
+++ magicbuild.near/ui-stack/source_code.jsx
@@ -1,6 +1,7 @@
 const element = props.element || "div";
 const styleSheet = props.styleSheet || "";
 const styleClass = props.styleClass || "";
+
 State.init({
   stack: null,
   children: props.children || "",
