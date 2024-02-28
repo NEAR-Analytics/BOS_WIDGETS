7c0a263ea0460723804067716e9ef211d14c87fc
near-antigua.near <near-antigua.near>
2024-02-28 18:21:29 +0000

    Update ft_transfer-usdc by near-antigua.near at 2024-02-28T18:21:29.043Z

--- near-antigua.near/ft_transfer-usdc/source_code.jsx
+++ near-antigua.near/ft_transfer-usdc/source_code.jsx
@@ -1,6 +1,6 @@
 const accountId = context.accountId;
-let token = props.token || "
-a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near";
+let token =
+  props.token || "a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near";
 let reciever = props.reciever || "chloe.near";
 let balance = -1;
 
