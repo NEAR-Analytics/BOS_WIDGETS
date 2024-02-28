2fe64811c7a60be98500a9858347a4438a834d06
ndcdev.near <ndcdev.near>
2024-02-28 14:31:34 +0000

    Update daos.Components.MetricsDisplay.index by ndcdev.near at 2024-02-28T14:31:34.896Z

--- ndcdev.near/daos.Components.MetricsDisplay.index/source_code.jsx
+++ ndcdev.near/daos.Components.MetricsDisplay.index/source_code.jsx
@@ -1,4 +1,4 @@
-const { daos, deliverTreasury, typeOfProject } = props;
+const { daos, totalTreasury, deliverTreasury, typeOfProject } = props;
 const { Items } = VM.require(
   `ndcdev.near/widget/daos.Components.MetricsDisplay.styled`,
 );
@@ -6,45 +6,45 @@ const { Items } = VM.require(
 if (!Items || !daos) return <Widget src="flashui.near/widget/Loading" />;
 
 const baseUrl = "https://api.pikespeak.ai";
-const [totalTreasury, setTotalTreasury] = useState(null);
+// const [totalTreasury, setTotalTreasury] = useState(null);
 const [loading, setLoading] = useState(true);
 
-const getTotalTreasury = async (accountId) => {
-  try {
-    return asyncFetch(`${baseUrl}/account/balance/${accountId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5",
-      },
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
+// const getTotalTreasury = async (accountId) => {
+//   try {
+//     return asyncFetch(`${baseUrl}/account/balance/${accountId}`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "x-api-key": "36f2b87a-7ee6-40d8-80b9-5e68e587a5b5",
+//       },
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
 
-const fetchDaoFunds = async () => {
-  const totalDAOTreasuryAmount = 0;
+// const fetchDaoFunds = async () => {
+//   const totalDAOTreasuryAmount = 0;
 
-  const promises = daos.flatMap((dao) =>
-    getTotalTreasury(dao.account_id).then((resp) => {
-      if (!resp.body) return;
+//   const promises = daos.flatMap((dao) =>
+//     getTotalTreasury(dao.account_id).then((resp) => {
+//       if (!resp.body) return;
 
-      const data = resp.body;
-      if (data) {
-        totalDAOTreasuryAmount +=
-          data.find((d) => d.contract === "Near")?.amount ?? 0;
-      }
-    }),
-  );
+//       const data = resp.body;
+//       if (data) {
+//         totalDAOTreasuryAmount +=
+//           data.find((d) => d.contract === "Near")?.amount ?? 0;
+//       }
+//     }),
+//   );
 
-  Promise.all(promises).then(() => {
-    setTotalTreasury(totalDAOTreasuryAmount);
-    setLoading(false);
-  });
-};
+//   Promise.all(promises).then(() => {
+//     setTotalTreasury(totalDAOTreasuryAmount);
+//     setLoading(false);
+//   });
+// };
 
-fetchDaoFunds();
+// fetchDaoFunds();
 
 return (
   <Items>
