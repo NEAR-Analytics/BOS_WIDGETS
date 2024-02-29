b67dcfd35253384f8b702b798a0b0c08d2c53d26
rubik-ui.near <rubik-ui.near>
2024-02-29 08:48:37 +0000

    Update RubikUI.App.Pages.Home by rubik-ui.near at 2024-02-29T08:48:37.251Z

--- rubik-ui.near/RubikUI.App.Pages.Home/source_code.jsx
+++ rubik-ui.near/RubikUI.App.Pages.Home/source_code.jsx
@@ -1,19 +1,78 @@
-const { Rubik: Theme } = VM.require("rubik-ui.near/widget/RubikUI.Themes.RubikTheme") || {
-  Rubik: (() => <></>)
+const { Rubik: Theme } = VM.require(
+  "rubik-ui.near/widget/RubikUI.Themes.RubikTheme"
+) || {
+  Rubik: () => <></>,
 };
 
-const { RubikLogo: Logo } = VM.require("rubik-ui.near/widget/RubikUI.Components.Core") || {
-  RubikLogo: (() => <></>)
+const { RubikLogo: Logo } = VM.require(
+  "rubik-ui.near/widget/RubikUI.Components.Core"
+) || {
+  RubikLogo: () => <></>,
 };
 
-const { Accordion, AccordionItem } = VM.require("rubik-ui.near/widget/RubikUI.Cubes.AccordionCubes") || {
-  Accordion: (() => <></>),
-  AccordionItem: (() => <></>)
+const { Accordion, AccordionItem } = VM.require(
+  "rubik-ui.near/widget/RubikUI.Cubes.AccordionCubes"
+) || {
+  Accordion: () => <></>,
+  AccordionItem: () => <></>,
 };
 
-const { AccordionFacet } = VM.require("rubik-ui.near/widget/RubikUI.Facets.AccordionFacets") || {
-  AccordionFacet: (() => <></>)
+const { AccordionFacet } = VM.require(
+  "rubik-ui.near/widget/RubikUI.Facets.AccordionFacets"
+) || {
+  AccordionFacet: () => <></>,
 };
+
+const Wrapper = styled.div`
+  width:100%;
+  min-height:100vh;
+  background-color:#fafafa;
+  padding:20px;
+  border-radius:10px;
+`;
+
+const Navbar = styled.div`
+  color: var(--color-text-primary);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius:10px;
+`;
+
+const NavbarLogo = styled.div`
+  font-weight:bold;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+
+  p {
+    padding:0;
+    margin:0;
+    font-weight:bold;
+    font-size:1.4rem;
+  }
+`;
+
+const NavbarContent = styled.div``;
+
+const NavbarLink = styled.a`
+    color: var(--color-text-primary);
+    font-size: var(--font-size-sm);
+    text-decoration: none;
+    margin-left: 20px;
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+const Search = styled.input`
+  width:300px;
+  border-radius:20px;
+  background-color:#fff;
+  border:1px solid rgba(0,0,0,.05);
+  padding:.5rem 1.2rem;
+`;
 
 const MyAccordionFacet = styled(AccordionFacet || styled.div)`
   background-color:lightgreen;
@@ -24,37 +83,46 @@ const MyAccordionFacet = styled(AccordionFacet || styled.div)`
 
 return (
   <Theme>
-    <Logo></Logo>
+    <Wrapper>
+      <Navbar>
+        <NavbarLogo>
+          <Logo></Logo>
+          <p>rubik</p>
+        </NavbarLogo>
+        <NavbarContent>
+          <Search placeholder="Search a cube" />
+        </NavbarContent>
+      </Navbar>
+      <p>Standard Accordion Cube:</p>
+      <Accordion>
+        {Array.from([1, 2, 3]).map((data, idx) => (
+          <AccordionItem>{idx}</AccordionItem>
+        ))}
+      </Accordion>
+      <br />
+      <br />
+      <p>Inline Customized Accordion Cube (and media query at 800px) :</p>
+      <Accordion
+        mediaMaxWidth800="background-color:red"
+        onClick={() => console.log("Accordion clicked!")}
+        width="800px"
+        fontWeight="bold"
+      >
+        {Array.from([1, 2, 3]).map((data, idx) => (
+          <AccordionItem>{idx}</AccordionItem>
+        ))}
+      </Accordion>
+      <br />
+      <br />
 
-    <p>Standard Accordion Cube:</p>
-    <Accordion>
-      {Array.from([1, 2, 3]).map((data, idx) => (
-        <AccordionItem>{idx}</AccordionItem>
-      ))}
-    </Accordion>
-    <br/><br/>
-    <p>Inline Customized Accordion Cube (and media query at 800px) :</p>
-    <Accordion
-      mediaMaxWidth800="background-color:red"
-      onClick={() => console.log("Accordion clicked!")}
-      width="800px"
-      fontWeight="bold"
-    >
-      {Array.from([1, 2, 3]).map((data, idx) => (
-        <AccordionItem>{idx}</AccordionItem>
-      ))}
-    </Accordion>
-    <br/><br/>
+      <p>Accordion Cube with extended Facet:</p>
+      <Accordion apply={MyAccordionFacet}>
+        {Array.from([1, 2, 3]).map((data, idx) => (
+          <AccordionItem>{idx}</AccordionItem>
+        ))}
+      </Accordion>
 
-    <p>Accordion Cube with extended Facet:</p>
-    <Accordion
-      apply={MyAccordionFacet}
-    >
-      {Array.from([1, 2, 3]).map((data, idx) => (
-        <AccordionItem>{idx}</AccordionItem>
-      ))}
-    </Accordion>
-
-    <p></p>
+      <Widget src="rubik-ui.near/widget/RubikUI.Styled.Elements" />
+    </Wrapper>
   </Theme>
 );
