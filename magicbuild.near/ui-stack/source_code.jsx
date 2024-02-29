4027ef8a742f111a0c7d6e2d0083933a7d389125
magicbuild.near <magicbuild.near>
2024-02-29 11:37:40 +0000

    Update ui-stack by magicbuild.near at 2024-02-29T11:37:40.330Z

--- magicbuild.near/ui-stack/source_code.jsx
+++ magicbuild.near/ui-stack/source_code.jsx
@@ -1,73 +1,11 @@
+const children = props.children;
 const element = props.element || "div";
 const styleSheet = props.styleSheet || "";
 const styleClass = props.styleClass || "";
 
-State.init({
-  stack: null,
-  children: props.children || [],
-  isEditor: props.isEditor || true,
-  isRemove: props.isRemove || false,
-});
-
-const updateStateEditor = () => {
-  State.update({
-    isEditor: !state.isEditor,
-  });
-};
-const removeChildren = () => {
-  State.update({
-    isRemove: !state.isRemove,
-  });
-};
-if (!state.stack) {
-  State.update({
-    stack: styled[element]`
-    ${styleSheet}`,
-  });
-}
-
-const Stack = state.stack;
-// when user render how to get element without add button ?
 return (
-  <>
-    {state.isRemove ? (
-      ""
-    ) : (
-      <Stack class={"col " + styleClass}>
-        <button type="button" onClick={updateStateEditor}>
-          {"Collapse"}
-        </button>
-        <button type="button" class="btn btn-danger" onClick={removeChildren}>
-          {"Remove"}
-        </button>
-        {state.isEditor == true ? (
-          <>
-            <Widget
-              src="magicbuild.near/widget/add-block-button"
-              props={{
-                selectWidget: (widgetUrl) => {
-                  const childrenColection = state.children;
-                  childrenColection.push(
-                    <Widget
-                      src={widgetUrl}
-                      props={{
-                        children: [],
-                        isEditor: state.isEditor,
-                      }}
-                    />
-                  );
-                  State.update({
-                    children: childrenColection,
-                  });
-                },
-              }}
-            />
-            {state.children && state.children.map((widget) => widget)}
-          </>
-        ) : (
-          state.children && state.children.map((widget) => widget)
-        )}
-      </Stack>
-    )}
-  </>
+  <Widget
+    src="magicbuild.near/widget/ui-main"
+    props={{ styleClass: "col " + styleClass, styleSheet: styleSheet }}
+  />
 );
