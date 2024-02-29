c5c90e78151e126ec3c2653de664a09850dfe663
hack.near <hack.near>
2024-02-29 19:51:33 +0000

    Update evaporation.button by hack.near at 2024-02-29T19:51:33.027Z

--- hack.near/evaporation.button/source_code.jsx
+++ hack.near/evaporation.button/source_code.jsx
@@ -1,4 +1,4 @@
-const initialSunsetTimestamp = 1709199888 * 1000;
+const initialSunset = 1709888888 * 1000;
 const accountId = props.accountId ?? context.accountId ?? "hack.near";
 const gameId = props.gameId ?? "evaporation";
 const appId = props.appId ?? "test";
@@ -6,8 +6,6 @@ const clicks = Social.index(appId, `${accountId}-${gameId}`, {
   limit: 100,
   order: "desc",
 });
-
-const [sunset, setSunset] = useState(new Date(initialSunsetTimestamp));
 
 const totalAddedTime = (clicks ? Object.keys(clicks).length : 0) * 5 * 60000;
 
@@ -28,13 +26,15 @@ const addMoreTime = () => {
 };
 
 const now = new Date();
-const evaporated = now > sunset + totalAddedTime;
+const sunset = initialSunset + totalAddedTime;
+const evaporated = now > sunset;
 
-const remainingTime = sunset.getTime() + totalAddedTime - now.getTime();
+const remainingTime = sunset + totalAddedTime - now;
 const remainingMinutes = Math.max(Math.floor(remainingTime / (1000 * 60)), 0);
 
 return (
   <div>
+    <p>{totalAddedTime / (1000 * 60)} minutes added</p>
     {!evaporated ? (
       <>
         <p>{remainingMinutes} minutes left...</p>
