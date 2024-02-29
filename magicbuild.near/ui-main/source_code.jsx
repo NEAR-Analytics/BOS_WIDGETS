200b82a6002fcfa806178e06f74789d49b38b66c
magicbuild.near <magicbuild.near>
2024-02-29 11:18:22 +0000

    Update ui-main by magicbuild.near at 2024-02-29T11:18:22.910Z

--- magicbuild.near/ui-main/source_code.jsx
+++ magicbuild.near/ui-main/source_code.jsx
@@ -1,18 +1,87 @@
-const children = props.children;
 const element = props.element || "div";
 const styleSheet = props.styleSheet || "";
 const styleClass = props.styleClass || "";
+
 State.init({
-  group: null,
+  main: null,
+  children: props.children || [],
+  isEditor: props.isEditor || true,
+  isRemove: props.isRemove || false,
 });
 
-if (!state.group) {
+const updateStateEditor = () => {
   State.update({
-    group: styled[element]`
+    isEditor: !state.isEditor,
+  });
+};
+const removeChildren = () => {
+  State.update({
+    isRemove: !state.isRemove,
+  });
+};
+if (!state.main) {
+  State.update({
+    main: styled[element]`
     ${styleSheet}`,
   });
 }
 
-const Group = state.group;
+const Main = state.main;
 
-return <Group class={styleClass}>{children}</Group>;
+return (
+  <>
+    {state.isRemove ? (
+      ""
+    ) : (
+      <Main class={styleClass}>
+        <div class="d-flex justify-content-between">
+          <div></div>
+          <div>
+            <button type="button" onClick={updateStateEditor}>
+              {state.isEditor ? (
+                <i class="bi bi-caret-down-fill"></i>
+              ) : (
+                <i class="bi bi-caret-right-fill"></i>
+              )}
+            </button>
+            <button
+              type="button"
+              class="btn btn-danger"
+              onClick={removeChildren}
+            >
+              <i class="bi bi-trash"></i>
+            </button>
+          </div>
+        </div>
+
+        {state.isEditor == true ? (
+          <>
+            <Widget
+              src="magicbuild.near/widget/add-block-button"
+              props={{
+                selectWidget: (widgetUrl) => {
+                  const childrenColection = state.children;
+                  childrenColection.push(
+                    <Widget
+                      src={widgetUrl}
+                      props={{
+                        children: [],
+                        isEditor: state.isEditor,
+                      }}
+                    />
+                  );
+                  State.update({
+                    children: childrenColection,
+                  });
+                },
+              }}
+            />
+            {state.children && state.children.map((widget) => widget)}
+          </>
+        ) : (
+          state.children && state.children.map((widget) => widget)
+        )}
+      </Main>
+    )}
+  </>
+);
