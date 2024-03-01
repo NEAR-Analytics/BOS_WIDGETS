0eae372d35aa565077ef18c9e4fb0b9e39cde487
devhub.near <devhub.near>
2024-03-01 19:22:18 +0000

    Update devhub.feature.proposal-search.by-sort by devhub.near at 2024-03-01T19:22:18.888Z

---
+++ devhub.near/devhub.feature.proposal-search.by-sort/source_code.jsx
@@ -0,0 +1,23 @@
+const options = [
+  { label: "Most recent", value: "" },
+  { label: "Most viewed", value: "" },
+  { label: "Most commented", value: "" },
+  { label: "Unanswered", value: "" },
+];
+
+const [selected, setSelected] = useState(null);
+
+return (
+  <div>
+    <Widget
+      src="devhub.near/widget/devhub.components.molecule.DropDown"
+      props={{
+        options: options,
+        label: "Sort",
+        onUpdate: (v) => {
+          setSelected(v);
+        },
+      }}
+    />
+  </div>
+);
