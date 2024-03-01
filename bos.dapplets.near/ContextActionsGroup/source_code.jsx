ecd5a6164c74877300533750dca434e10c7d6252
bos.dapplets.near <bos.dapplets.near>
2024-03-01 09:33:22 +0000

    Update ContextActionsGroup by bos.dapplets.near at 2024-03-01T09:33:22.866Z

--- bos.dapplets.near/ContextActionsGroup/source_code.jsx
+++ bos.dapplets.near/ContextActionsGroup/source_code.jsx
@@ -91,40 +91,35 @@ const ActionsWrapper = styled.div`
   align-items: center;
   padding-top: 6px;
   border: 1px solid #384bff;
-  // opacity: 1;
-  right: 60px;
-  height: auto;
-  width: 40px;
-  min-height: 34px;
-  background: #fff;
-  transition: all 0.5s;
-  // @keyframes translateAnimation {
-  //   0% {
-  //     display: none;
-  //     width: 0px;
-  //     opacity: 0.3;
-  //     min-height: 0px;
-  //     height: 0px;
-  //     transform-origin: top left;
-  //     transform: scale(0) translateX(-30px);
-  //   }
-  //   80% {
-  //     opacity: 1;
-  //   }
-  //   100% {
-  //     padding-top: 6px;
-  //     border: 1px solid #384bff;
-  //     opacity: 1;
-  //     right: 60px;
-  //     height: auto;
-  //     width: 40px;
-  //     min-height: 34px;
-  //     background: #fff;
-  //     transform-origin: top left;
-  //     transform: scale(1) translateX(0px);
-  //   }
-  // }
-  // animation: translateAnimation 0.5s ease forwards;
+
+  @keyframes translateAnimation {
+    0% {
+      display: none;
+      width: 0px;
+      opacity: 0.3;
+      min-height: 0px;
+      height: 0px;
+      transform-origin: top left;
+      transform: scale(0) translateX(-30px);
+    }
+    80% {
+      opacity: 1;
+    }
+    100% {
+      padding-top: 6px;
+      border: 1px solid #384bff;
+      opacity: 1;
+      right: 60px;
+      height: auto;
+      width: 40px;
+      min-height: 34px;
+      background: #fff;
+      transform-origin: top left;
+      transform: scale(1) translateX(0px);
+    }
+  }
+  animation: translateAnimation 0.5s ease forwards;
+  transition: all 0.3s;
 `;
 
 const TriggerEar = styled.div`
