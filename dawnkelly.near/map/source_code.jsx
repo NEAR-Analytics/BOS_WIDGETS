abc5c08bb8802ab7d8aa429c7764ae25ea301100
dawnkelly.near <dawnkelly.near>
2024-03-01 07:34:46 +0000

    Update map by dawnkelly.near at 2024-03-01T07:34:46.627Z

---
+++ dawnkelly.near/map/source_code.jsx
@@ -0,0 +1,8 @@
+const mapImageUrl =
+  "https://cyan-interesting-takin-110.mypinata.cloud/ipfs/QmNyem7UNRXAFkxTC5XySDwPAZpnyoQVWpRhbXenmun24j";
+
+return (
+  <div>
+    <img src={mapImageUrl} alt="map image" />
+  </div>
+);
