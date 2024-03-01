7d701e728efbe1e57aa8a72d57a4f0fe727f3822
ndcdev.near <ndcdev.near>
2024-03-01 15:26:27 +0000

    Update daos.Config by ndcdev.near at 2024-03-01T15:26:27.091Z

--- ndcdev.near/daos.Config/source_code.jsx
+++ ndcdev.near/daos.Config/source_code.jsx
@@ -965,7 +965,10 @@ return {
               {
                 title: "Become a member",
                 description: "Join the Telegram community",
-                button: { title: "Learn more", link: "" },
+                button: {
+                  title: "Learn more",
+                  link: "https://t.me/+1DLTr03appplMmY0 ",
+                },
               },
               {
                 title: "Contribute",
