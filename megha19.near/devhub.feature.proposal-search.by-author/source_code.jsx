0b1ac050387f90d07714f68e94d8b743b9d985c0
megha19.near <megha19.near>
2024-03-01 17:22:43 +0000

    Update devhub.feature.proposal-search.by-author by megha19.near at 2024-03-01T17:22:43.829Z

--- megha19.near/devhub.feature.proposal-search.by-author/source_code.jsx
+++ megha19.near/devhub.feature.proposal-search.by-author/source_code.jsx
@@ -4,7 +4,7 @@ const [selectedAuthor, setSelectedAuthor] = useState(null);
 if (!authorsOptions.length) {
   const data = [];
   const authors = Near.view(
-    "proudflour16.near",
+    "truedove38.near",
     "get_all_proposal_authors",
     {}
   );
