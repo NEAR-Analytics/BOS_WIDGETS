e5cb5b97f9994dd3a21c323c65f63aa1a61ed6db
devhub.near <devhub.near>
2024-03-01 19:22:18 +0000

    Update devhub.feature.proposal-search.by-author by devhub.near at 2024-03-01T19:22:18.888Z

---
+++ devhub.near/devhub.feature.proposal-search.by-author/source_code.jsx
@@ -0,0 +1,33 @@
+const [authorsOptions, setAuthorsOptions] = useState([]);
+const [selectedAuthor, setSelectedAuthor] = useState(null);
+
+if (!authorsOptions.length) {
+  const data = [];
+  const authors = Near.view(
+    "devhub.near",
+    "get_all_proposal_authors",
+    {}
+  );
+
+  if (Array.isArray(authors)) {
+    for (const author of authors) {
+      data.push({ label: author, value: author });
+    }
+    setAuthorsOptions(data);
+  }
+}
+
+return (
+  <div>
+    <Widget
+      src="devhub.near/widget/devhub.components.molecule.DropDown"
+      props={{
+        options: authorsOptions,
+        label: "Author",
+        onUpdate: (v) => {
+          setSelectedAuthor(v);
+        },
+      }}
+    />
+  </div>
+);
