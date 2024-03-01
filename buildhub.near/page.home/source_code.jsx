6118eaa59e2901140ddf872cf4091ede2973de2a
buildhub.near <buildhub.near>
2024-03-01 04:56:17 +0000

    Update page.home by buildhub.near at 2024-03-01T04:56:17.352Z

--- buildhub.near/page.home/source_code.jsx
+++ buildhub.near/page.home/source_code.jsx
@@ -1,5 +1,5 @@
 const { Hero, Goals, Join, Purposes, AboutUs, Governance, Footer } = VM.require(
-  "buildhub.near/widget/home.Home"
+  "buildhub.near/widget/home.Home",
 ) || {
   Hero: () => <></>,
   Goals: () => <></>,
@@ -9,11 +9,13 @@ const { Hero, Goals, Join, Purposes, AboutUs, Governance, Footer } = VM.require(
   Governance: () => <></>,
   Footer: () => <></>,
 };
+
 const Root = styled.div`
   background-color: var(--bg-1, #000);
   color: var(--text-color, #fff);
   width: 100%;
 `;
+
 return (
   <Root>
     <Hero {...props} />
