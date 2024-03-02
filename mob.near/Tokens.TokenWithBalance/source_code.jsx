e6fa2b588e506f5482300edaeceb32b75fa70c04
mob.near <mob.near>
2024-03-02 00:56:02 +0000

    Update Tokens.TokenWithBalance by mob.near at 2024-03-02T00:56:02.985Z

--- mob.near/Tokens.TokenWithBalance/source_code.jsx
+++ mob.near/Tokens.TokenWithBalance/source_code.jsx
@@ -7,9 +7,6 @@ const bigBalance =
   balance === null || balance === undefined ? null : Big(balance);
 
 const metadata = Near.view(tokenId, "ft_metadata");
-if (!metadata) {
-  return "";
-}
 
 const bigToString = (b, p, len) => {
   if (b === null || b === undefined) {
@@ -48,7 +45,7 @@ function MutedDecimals(props) {
   const dotPos = value.indexOf(".");
   if (dotPos > 0) {
     return (
-      <>
+      <span className="d-inline-flex">
         {value.charAt(0) === "<" ? (
           <>
             <span className="text-secondary">{"<"}</span>
@@ -58,26 +55,26 @@ function MutedDecimals(props) {
           value.substring(0, dotPos)
         )}
         <span className="text-secondary">{value.substring(dotPos)}</span>
-      </>
+      </span>
     );
   }
   return value;
 }
 
 const adjustedBalance =
-  !bigBalance || !metadata.decimals
+  !bigBalance || !metadata?.decimals
     ? bigBalance
     : bigBalance.div(Big(10).pow(metadata.decimals));
 
-const name = metadata.name || "";
-const icon = metadata.icon;
-const symbol = metadata.symbol || tokenAccountId;
+const name = metadata?.name || "";
+const icon = metadata?.icon;
+const symbol = metadata?.symbol || tokenAccountId;
 
 const Wrapper = styled.div`
 .token-badge {
   vertical-align: middle;
-  flex-grow: 1;
-  min-width: 10rem;
+  width: 12rem;
+  margin-right: 1rem;
 
   .token-name {
     color: #666;
@@ -90,7 +87,8 @@ const Wrapper = styled.div`
     vertical-align: middle;
     margin-right: 0.5em;
   }
-}`;
+}
+`;
 
 return (
   <Wrapper>
