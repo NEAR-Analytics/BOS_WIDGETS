bd65f2fa368edd59a200565bb16dfac685fee5e2
webbrowser.near <webbrowser.near>
2024-03-02 13:05:44 +0000

    Update OpenWebBrowser.Pages.SignCode by webbrowser.near at 2024-03-02T13:05:44.632Z

---
+++ webbrowser.near/OpenWebBrowser.Pages.SignCode/source_code.jsx
@@ -0,0 +1,47 @@
+const { signMessage } = props;
+
+const Main = styled.div`
+    width:100%;
+    height:100vh;
+`;
+
+const [selectedOption, setSelectedOption] = useState("");
+
+const widgets = Social.getr(`${context.accountId}/widget`);
+const getSelectedOptionEncoded = () => {
+  const code = widgets[selectedOption][""];
+  const encoded = ethers.utils.sha256(Buffer.from(code));
+
+  return encoded?.substring(2, encoded.length);
+};
+
+return (
+  <Main>
+    <select
+      onChange={(e) => {
+        setSelectedOption(e.target.value);
+      }}
+    >
+      <option>Choose a component</option>
+      {Object.keys(widgets)?.map((option) => (
+        <option>{option}</option>
+      ))}
+    </select>
+    <button
+      onClick={() => {
+        signMessage(getSelectedOptionEncoded()).then((signature) => {
+          Social.set({
+            routes: {
+              index: {
+                component: selectedOption,
+                signature,
+              },
+            },
+          });
+        });
+      }}
+    >
+      Sign
+    </button>
+  </Main>
+);
