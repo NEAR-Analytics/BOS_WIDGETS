ec5cbeec75385aad427558553c44932802861c5a
mob.near <mob.near>
2024-03-02 18:29:13 +0000

    Update Tokens.Inner by mob.near at 2024-03-02T18:29:13.656Z

--- mob.near/Tokens.Inner/source_code.jsx
+++ mob.near/Tokens.Inner/source_code.jsx
@@ -5,6 +5,12 @@ if (!accountId) {
 const [tokens, setTokens] = useState(false);
 const [loading, setLoading] = useState(true);
 
+const { bigToString, MutedDecimals } = VM.require(
+  "mob.near/widget/Token.utils"
+);
+
+const priceData = Near.view("priceoracle.near", "get_price_data");
+
 useEffect(() => {
   setTokens(false);
   setLoading(true);
@@ -21,6 +27,35 @@ useEffect(() => {
     .finally(() => setLoading(false));
 }, [accountId]);
 
+const sortedTokens = useMemo(() => {
+  if (!tokens) {
+    return tokens;
+  }
+  const prices = Object.fromEntries(
+    (priceData?.prices || []).map(({ asset_id, price }) => [asset_id, price])
+  );
+  const computeUsdBalance = (tokenId, balance) => {
+    if (balance === null) {
+      return "0";
+    }
+    const price = prices[tokenId] || {
+      multiplier: "0",
+      decimals: 1,
+    };
+    return Big(price.multiplier)
+      .mul(Big(balance))
+      .div(Big(10).pow(price.decimals))
+      .toFixed(6);
+  };
+  const st = Object.entries(tokens).map(([tokenId, balance]) => ({
+    tokenId,
+    balance,
+    usdBalance: computeUsdBalance(tokenId, balance),
+  }));
+  st.sort((a, b) => parseFloat(b.usdBalance) - parseFloat(a.usdBalance));
+  return st;
+}, [tokens, priceData]);
+
 const Wrapper = styled.div`
 .header {
   margin-bottom: 1em;
@@ -31,29 +66,44 @@ const Wrapper = styled.div`
 }
 `;
 
-const renderToken = (tokenId, balance) => {
+const renderToken = ({ tokenId, balance, usdBalance }) => {
   return (
     <Widget
       loading=""
       src="mob.near/widget/Tokens.TokenWithBalance"
-      props={{ tokenId, balance }}
+      props={{ tokenId, balance, usdBalance }}
     />
   );
 };
+
+const usdSum = sortedTokens
+  ? sortedTokens
+      .map(({ usdBalance }) => parseFloat(usdBalance || "0"))
+      .reduce((s, v) => s + v, 0)
+  : 0;
 
 return (
   <Wrapper>
     <div className="header">
       Fungible Tokens of
       <Widget src="mob.near/widget/N.ProfileLine" props={{ accountId }} />
+      {usdSum > 0 && (
+        <div>
+          Total USD sum:
+          <span className="font-monospace fw-bold d-inline-flex">
+            <span className="text-secondary">~$</span>
+            <MutedDecimals value={bigToString(usdSum)} />
+          </span>
+        </div>
+      )}
     </div>
     <div className="actions">
-      {tokens
-        ? Object.entries(tokens)
-            .filter(([tokenId, balance]) => balance !== null)
-            .map(([tokenId, balance]) => (
-              <div key={tokenId} className="token">
-                {renderToken(tokenId, balance)}
+      {sortedTokens
+        ? sortedTokens
+            .filter((t) => t.balance !== null)
+            .map((t) => (
+              <div key={t.tokenId} className="token">
+                {renderToken(t)}
               </div>
             ))
         : "Loading"}
