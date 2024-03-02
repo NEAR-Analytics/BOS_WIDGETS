3b3b0f16e1337887ce490e06267e5be218c44dd9
hack.near <hack.near>
2024-03-02 11:27:03 +0000

    Update evisceration.button by hack.near at 2024-03-02T11:27:03.015Z

---
+++ hack.near/evisceration.button/source_code.jsx
@@ -0,0 +1,48 @@
+const initialHealth = 10000;
+const accountId = props.accountId ?? context.accountId ?? "hack.near";
+const gameId = props.gameId ?? "evisceration";
+const appId = props.appId ?? "test";
+const clicks = Social.index(appId, `${accountId}-${gameId}`, {
+  limit: 10000,
+  order: "desc",
+});
+
+const totalHits = clicks ? Object.keys(clicks).length : 0;
+
+const attack = () => {
+  Social.set({
+    index: {
+      [appId]: JSON.stringify({
+        key: `${accountId}-${gameId}`,
+        value: { type: "click" },
+      }),
+    },
+  });
+};
+
+const health = initialHealth - totalHits;
+const eviscerated = health === 0;
+
+return (
+  <div>
+    {!eviscerated ? (
+      <>
+        <div className="d-flex flex-row gap-3">
+          <p>
+            {health} health remaining ({clicks && Object.keys(clicks).length}{" "}
+            hit{Object.keys(clicks) > 1 && s})
+          </p>
+        </div>
+        <button
+          style={{ width: "222px" }}
+          onClick={attack}
+          className="btn btn-danger"
+        >
+          attack
+        </button>
+      </>
+    ) : (
+      <p>The monster is dead!</p>
+    )}
+  </div>
+);
