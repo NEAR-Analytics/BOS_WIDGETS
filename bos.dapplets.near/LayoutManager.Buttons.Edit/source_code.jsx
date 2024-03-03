7d9fd0cdfe85b58184b21a86bce0a16427ddf919
bos.dapplets.near <bos.dapplets.near>
2024-03-03 14:34:16 +0000

    Update LayoutManager.Buttons.Edit by bos.dapplets.near at 2024-03-03T14:34:16.063Z

---
+++ bos.dapplets.near/LayoutManager.Buttons.Edit/source_code.jsx
@@ -0,0 +1,52 @@
+const { onClick } = props
+
+const ButtonEdit = styled.button`
+  position: absolute;
+  top: -30px;
+  margin-bottom: 0;
+  border: none;
+  cursor: pointer;
+  width: 22px;
+  height: 22px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  padding: 0;
+  border: 1px solid #384bff;
+  box-sizing: border-box;
+  background: ${(p) => (p.default ? "#384bff !important" : "#fff !important")};
+  transition: all 0.3s;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  svg {
+    path {
+      stroke: ${(p) => (p.default ? "#fff !important" : "#384bff !important")};
+    }
+  }
+`;
+
+const iconEdit = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+  >
+    <path d="M12 7L2 7" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+);
+
+return <ButtonEdit
+  default
+  style={{
+    top: '5px',
+  }}
+  onClick={onClick}
+  >
+  {iconEdit}
+</ButtonEdit>
