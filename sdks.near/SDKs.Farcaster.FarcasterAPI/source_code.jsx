9245fc0ad06717b5475f2b29efc673879f3d2204
sdks.near <sdks.near>
2024-03-03 19:15:35 +0000

    Update SDKs.Farcaster.FarcasterAPI by sdks.near at 2024-03-03T19:15:35.312Z

---
+++ sdks.near/SDKs.Farcaster.FarcasterAPI/source_code.jsx
@@ -0,0 +1,72 @@
+const WARPCAST_API_URL = "https://client.warpcast.com/v2";
+const FARCASTER_FNAMES_API_URL = "https://fnames.farcaster.xyz";
+
+const PROFILE_ENDPOINT = "verifications";
+const FARCASTER_FNAMES_ENDPOINT = "transfers/current";
+
+const FarcasterAPI = {
+  getHandleOwner: (handle) => {
+    return FarcasterAPI.getFidByHandle(handle).then(async (fid) => {
+      if (!fid) {
+        return [];
+      }
+
+      return asyncFetch(
+        FarcasterAPI.getWarpcastQueryURL(PROFILE_ENDPOINT, { fid }),
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      )
+        .then((payload) => {
+          return payload.json();
+        })
+        .then((result) => {
+          const verifications = result?.result?.verifications || [];
+          return verifications.map((verification) => verification.address);
+        });
+    });
+  },
+  getFidByHandle: (handle) => {
+    return asyncFetch(
+      FarcasterAPI.getFarcasterQueryURL(FARCASTER_FNAMES_ENDPOINT, {
+        name: handle,
+      }),
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    )
+      .then((payload) => {
+        return payload.json();
+      })
+      .then((result) => {
+        return result?.transfer?.to || "";
+      });
+  },
+  getQueryURL: (endpoint, params) => {
+    return `${endpoint}?${new URLSearchParams(params).toString()}`;
+  },
+  getFarcasterQueryURL: (endpoint, params) => {
+    return FarcasterAPI.getFarcasterURL(
+      FarcasterAPI.getQueryURL(endpoint, params)
+    );
+  },
+  getWarpcastQueryURL: (endpoint, params) => {
+    return FarcasterAPI.getWarpcastURL(
+      FarcasterAPI.getQueryURL(endpoint, params)
+    );
+  },
+  getFarcasterURL: (endpoint) => {
+    return `${FARCASTER_FNAMES_API_URL}/${endpoint}`;
+  },
+  getWarpcastURL: (endpoint) => {
+    return `${WARPCAST_API_URL}/${endpoint}`;
+  },
+};
+
+return FarcasterAPI;
