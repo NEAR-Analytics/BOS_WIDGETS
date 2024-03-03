315c4114648b4faa354b364d2acaa009e89e9ee8
bos.dapplets.near <bos.dapplets.near>
2024-03-03 14:34:16 +0000

    Update ContextActionsGroup by bos.dapplets.near at 2024-03-03T14:34:16.063Z

--- bos.dapplets.near/ContextActionsGroup/source_code.jsx
+++ bos.dapplets.near/ContextActionsGroup/source_code.jsx
@@ -53,13 +53,6 @@ const OverlayTriggerWrapper = styled.div`
   }
 `;
 
-const SupportingSpan = styled.span`
-  display: inline-block;
-  width: 100%;
-  height: 30px;
-  margin-bottom: 0px;
-`;
-
 const TriggerShowPanel = styled.div`
   width: 40px;
   height: auto;
@@ -111,143 +104,6 @@ const ActionsWrapper = styled.div`
   }
   animation: translateAnimation 0.5s ease forwards;
   transition: all 0.3s;
-`;
-
-const TriggerEar = styled.div`
-  display: block;
-  width: 2px;
-  height: 2px;
-  border-radius: 2px;
-  background: #fff;
-  position: relative;
-
-  &:before {
-    content: "";
-    display: block;
-    width: 2px;
-    height: 2px;
-    border-radius: 2px;
-    background: #fff;
-    position: absolute;
-    top: 4px;
-  }
-
-  &:after {
-    content: "";
-    position: absolute;
-    display: block;
-    width: 2px;
-    height: 2px;
-    border-radius: 2px;
-    background: #fff;
-    bottom: 4px;
-  }
-`;
-
-const ButtonPlus = styled.div`
-  display: flex;
-  width: 22px;
-  height: 22px;
-  border-radius: 50%;
-  background: #fff;
-  position: relative;
-  transform: ${(p) => (p.default ? "translateY(-25px)" : "translateY(39px)")};
-  cursor: pointer;
-  border: 1px solid #384bff;
-
-  &:before {
-    content: "";
-    display: block;
-    width: 1.5px;
-    height: 11px;
-    border-radius: 2px;
-    background: #384bff;
-    position: absolute;
-    top: 6px;
-    left: 10.5px;
-  }
-
-  &:after {
-    content: "";
-    display: block;
-    height: 1px;
-    width: 11px;
-    border-radius: 2px;
-    background: #384bff;
-    position: absolute;
-    top: 10.8px;
-    left: 6px;
-  }
-
-  @keyframes translateAnimationBtn {
-    0% {
-      opacity: 0;
-    }
-    30% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-  animation: translateAnimationBtn 0.5s linear forwards;
-
-  &:hover {
-    transform: translateY(-25px) scale(1.1);
-  }
-  transition: all 0.3s;
-`;
-
-const WrapperButtonPlusDefault = styled.div`
-  width: 43px;
-  height: 62px;
-  border-radius: 0px 4px 4px 0px;
-  border: 1px solid rgb(56, 75, 255);
-  position: absolute;
-  top: 30px;
-  background: #f8f9ff;
-  z-index: 1081;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-`;
-
-const ButtonPlusDefault = styled.div`
-  display: flex;
-  width: 22px;
-  height: 22px;
-  border-radius: 50%;
-  bottom: 7px;
-  background: ${(p) =>
-    p.default ? "#384bff !important" : "#f8f9ff !important"};
-  position: relative;
-  margin-top: 40px;
-  cursor: pointer;
-  border: 1px solid #384bff;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-  transition: all 0.3s;
-
-  &:hover {
-    transform: scale(1.1);
-  }
-`;
-
-const WidgetBadgeWrapper = styled.div`
-  position: absolute;
-  right: 0;
-  z-index: 1200;
-  background: rgba(255, 255, 255, 0.35);
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  top: -2px;
-  backdrop-filter: blur(1px);
 `;
 
 const iconClose = (
@@ -305,63 +161,6 @@ const iconDropdown = (
   </svg>
 );
 
-const ButtonEdit = styled.button`
-  position: absolute;
-  top: -30px;
-  margin-bottom: 0;
-  border: none;
-  cursor: pointer;
-  width: 22px;
-  height: 22px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  padding: 0;
-  border: 1px solid #384bff;
-  box-sizing: border-box;
-  background: ${(p) => (p.default ? "#384bff !important" : "#fff !important")};
-  transition: all 0.3s;
-
-  &:hover {
-    transform: scale(1.1);
-  }
-
-  svg {
-    path {
-      stroke: ${(p) => (p.default ? "#fff !important" : "#384bff !important")};
-    }
-  }
-`;
-
-const ButtonApply = styled.button`
-  position: absolute;
-  top: -30px;
-  margin-bottom: 0;
-  border: none;
-  width: 22px;
-  height: 22px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  padding: 0;
-  padding-right: 2px;
-  cursor: pointer;
-  box-sizing: border-box;
-  border: 1px solid #384bff;
-  background: ${(p) => (p.default ? "#384bff !important" : "#fff !important")};
-  &:hover {
-    transform: scale(1.1);
-  }
-  svg {
-    path {
-      stroke: ${(p) => (p.default ? "#fff !important" : "#384bff !important")};
-    }
-  }
-  transition: all 0.3s;
-`;
-
 const ButtonCancel = styled.button`
   &:hover {
     background: #747376;
@@ -410,64 +209,10 @@ const FloatingModal = styled.div`
   }
 `;
 
-const iconEdit = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="14"
-    height="14"
-    viewBox="0 0 14 14"
-    fill="none"
-  >
-    <path d="M12 7L2 7" strokeLinecap="round" strokeLinejoin="round" />
-  </svg>
-);
-
-const iconApply = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="15"
-    height="14"
-    viewBox="0 0 15 14"
-    fill="none"
-  >
-    <path
-      d="M2 7H9.125M3.5 9.25L1.25 7L3.5 4.75M7.25 3.25V2.5C7.25 2.10218 7.40804 1.72064 7.68934 1.43934C7.97064 1.15804 8.35218 1 8.75 1H12.5C12.8978 1 13.2794 1.15804 13.5607 1.43934C13.842 1.72064 14 2.10218 14 2.5V11.5C14 11.8978 13.842 12.2794 13.5607 12.5607C13.2794 12.842 12.8978 13 12.5 13H8.75C8.35218 13 7.97064 12.842 7.68934 12.5607C7.40804 12.2794 7.25 11.8978 7.25 11.5V10.75"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-  </svg>
-);
-
-const plusDefault = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="12"
-    height="13"
-    viewBox="0 0 12 13"
-    fill="none"
-  >
-    <path
-      d="M6 1.5V11.5"
-      stroke="white"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path
-      d="M1 6.5H11"
-      stroke="white"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-  </svg>
-);
-
 return (
   <OverlayTriggerWrapper onMouseOver={handleOnMouseEnter}>
     <div style={{ opacity: state.show ? 0 : 1 }} className="OverlayTrigger">
-      <TriggerEar />
+      <Widget src='bos.dapplets.near/widget/LayoutManager.TriggerEar'/>
     </div>
     {state.show ? (
       <TriggerShowPanel
@@ -477,7 +222,7 @@ return (
           top: widgets && widgets.length ? "" : "-20px",
         }}
       >
-        <SupportingSpan />
+        <Widget src='bos.dapplets.near/widget/LayoutManager.SupportingSpan'/>
         <ActionsWrapper
           style={{
             backgroundColor:
@@ -487,90 +232,41 @@ return (
           {widgets.map((widget) => (
             <ActionBlock key={widget.linkId}>
               {isEditMode ? (
-                <WidgetBadgeWrapper
-                  className={
-                    widget.linkAuthorId === context.accountId
-                      ? "WidgetHover"
-                      : ""
-                  }
-                  title={
-                    widget.linkAuthorId === context.accountId
-                      ? `Remove ${widget.src.split("widget/").pop()}`
-                      : "disable in edit mode"
-                  }
-                  style={{
-                    opacity:
-                      widget.linkAuthorId === context.accountId ? "1" : "0",
+                <Widget
+                  src='bos.dapplets.near/widget/LayoutManager.WidgetBadgeWrapper'
+                  props={{
+                    widget: widget,
+                    deleteUserLink: deleteUserLink
                   }}
-                >
-                  {widget.linkAuthorId === context.accountId ? (
-                    <Widget
-                      src="bos.dapplets.near/widget/LayoutManager.DeleteWidgetButton"
-                      props={{
-                        onClick: () => deleteUserLink(widget.linkId),
-                      }}
-                    />
-                  ) : // <Widget src="bos.dapplets.near/widget/LayoutManager.LockedWidgetBadge" />
-                  null}
-                </WidgetBadgeWrapper>
+                />
               ) : null}
               <Widget src={widget.src} props={widget.props} />
             </ActionBlock>
           ))}
           {widgets && widgets.length ? (
             isEditMode ? (
-              <ButtonApply onClick={disableEditMode}>{iconApply}</ButtonApply>
+              <Widget
+                src='bos.dapplets.near/widget/LayoutManager.Buttons.Apply'
+                props={{ onClick: disableEditMode }}
+              />
             ) : (
-              <ButtonEdit onClick={enableEditMode}>{iconEdit}</ButtonEdit>
+              <Widget
+                src='bos.dapplets.near/widget/LayoutManager.Buttons.Edit'
+                props={{ onClick: enableEditMode }}
+              />
             )
           ) : null}
         </ActionsWrapper>
-        {widgets && widgets.length ? (
-          <>
-            <SupportingSpan />
-            <ButtonPlus
-              default={widgets && widgets.length ? true : false}
-              style={{
-                position:
-                  widgets && widgets.length ? "unset" : "absolute",
-                zIndex: "1081",
-              }}
-              title={!context.accountId ? "Connect wallet" : null}
-              onClick={!context.accountId ? null : handleOpenMenu}
-            />
-          </>
-        ) : (
-          <WrapperButtonPlusDefault>
-            {isEditMode ? (
-              <ButtonApply
-                style={{
-                  top: "5px",
-                }}
-                onClick={disableEditMode}
-                default
-              >
-                {iconApply}
-              </ButtonApply>
-            ) : (
-              <ButtonEdit
-                default
-                style={{
-                  top: "5px",
-                }}
-                onClick={enableEditMode}
-              >
-                {iconEdit}
-              </ButtonEdit>
-            )}
-            <ButtonPlusDefault
-              default
-              title={!context.accountId ? "Connect wallet" : null}
-              onClick={!context.accountId ? null : handleOpenMenu}
-            >
-              {plusDefault}
-            </ButtonPlusDefault>
-          </WrapperButtonPlusDefault>
-        )}
+        <Widget
+          src="bos.dapplets.near/widget/LayoutManager.EarOpened"
+          props={{
+            widgets,
+            isEditMode,
+            handleOpenMenu,
+            enableEditMode,
+            disableEditMode
+          }}
+        />
       </TriggerShowPanel>
     ) : null}
 
