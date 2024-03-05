b784aa9635fbfd76c5a0d0ffc285dd05530b096d
nearblocks.near <nearblocks.near>
2024-03-05 21:06:57 +0000

    Update bos-components.components.NFT.Overview by nearblocks.near at 2024-03-05T21:06:57.311Z

--- nearblocks.near/bos-components.components.NFT.Overview/source_code.jsx
+++ nearblocks.near/bos-components.components.NFT.Overview/source_code.jsx
@@ -1035,7 +1035,18 @@ function MainComponent({ network, id }) {
                   }
                 </Tabs.Content>
                 <Tabs.Content value={tabs[3]}>
-                  <div className="px-4 sm:px-6 py-3"></div>
+                  <div className="py-3">
+                    {
+                      <Widget
+                        src={`${config.ownerId}/widget/bos-components.components.Comments.Feed`}
+                        props={{
+                          network: network,
+                          path: `nearblocks.io/nft/${id}`,
+                          limit: 10,
+                        }}
+                      />
+                    }
+                  </div>
                 </Tabs.Content>
               </div>
             </Tabs.Root>
