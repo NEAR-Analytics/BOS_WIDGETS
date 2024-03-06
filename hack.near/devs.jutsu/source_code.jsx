f6769629d1b222c4aa8dc83f2383e97909191184
hack.near <hack.near>
2024-03-06 03:09:05 +0000

    Update devs.jutsu by hack.near at 2024-03-06T03:09:05.299Z

---
+++ hack.near/devs.jutsu/source_code.jsx
@@ -0,0 +1,20 @@
+const widgets = Social.get(`*/widget/*/metadata/platform`, "final");
+
+const builders = Object.keys(widgets);
+
+return (
+  <>
+    <h3>
+      <a href="https://jutsu.ai">Jutsu.ai</a>
+    </h3>
+    <h5>{builders.length} builders</h5>
+    {builders.map((builder, index) => (
+      <div className="m-1" key={index}>
+        <Widget
+          src="mob.near/widget/N.ProfileLine"
+          props={{ accountId: builder }}
+        />
+      </div>
+    ))}
+  </>
+);
