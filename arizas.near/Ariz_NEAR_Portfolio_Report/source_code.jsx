067dfdba1c09b64b44ce404f2cc7718ba6a9733b
arizas.near <arizas.near>
2024-03-06 14:54:14 +0000

    Update Ariz_NEAR_Portfolio_Report by arizas.near at 2024-03-06T14:54:14.955Z

--- arizas.near/Ariz_NEAR_Portfolio_Report/source_code.jsx
+++ arizas.near/Ariz_NEAR_Portfolio_Report/source_code.jsx
@@ -1,6 +1,6 @@
 return (
   <iframe
     style={{ width: "100%", height: "600px" }}
-    src="https://ipfs.web4.near.page/ipfs/bafybeibkzzcf5ueom7n3abfm4avumxdysukiutl5lx5f54qcqfen3ddpm4/"
+    src="https://ipfs.web4.near.page/ipfs/bafybeieir3m5bquavwvaslovxaijxx7z42ries7vtjmyystczgr7idvgei/"
   ></iframe>
 );
