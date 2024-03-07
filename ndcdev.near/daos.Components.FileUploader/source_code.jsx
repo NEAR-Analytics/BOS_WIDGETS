e61246c1d1da2ddd5b8a88085b701e2dac0e82bf
ndcdev.near <ndcdev.near>
2024-03-07 15:56:53 +0000

    Update daos.Components.FileUploader by ndcdev.near at 2024-03-07T15:56:53.675Z

--- ndcdev.near/daos.Components.FileUploader/source_code.jsx
+++ ndcdev.near/daos.Components.FileUploader/source_code.jsx
@@ -1,8 +1,8 @@
 const fileAccept = props.fileAccept || "*";
 const onChange = props.onChange;
-const children = props.children
-const styles = props.styles
-const classNames = props.classNames
+const children = props.children;
+const styles = props.styles;
+const classNames = props.classNames;
 
 State.init({
   uploading: false,
@@ -13,7 +13,7 @@ State.init({
 const ipfsUrl = (cid) => `https://ipfs.near.social/ipfs/${cid}`;
 
 return (
-  <div style={ styles ?? { width: "max-content" }}>
+  <div style={styles ?? { width: "max-content" }}>
     {/* {state.cid && (
       <a href={ipfsUrl(state.cid)} download>
         {state.filename}
@@ -24,7 +24,10 @@ return (
       accepts={["image/*", "video/*", ".pdf"]}
       minFileSize={1}
       clickable
-      className={classNames ?? "d-flex justify-content-center align-items-center attachment-button"}
+      className={
+        classNames ??
+        "d-flex justify-content-center align-items-center attachment-button"
+      }
       onChange={(files) => {
         if (!files || !files.length) return;
 
@@ -39,12 +42,13 @@ return (
           onChange(ipfsUrl(cid));
         });
       }}
-    >  
-      {children ?? (state.cid ? (
-        <i className="bi bi-arrow-clockwise" />
-      ) : (
-        <i className="bi bi-image" />
-      ))}
+    >
+      {children ??
+        (state.cid ? (
+          <i className="bi bi-arrow-clockwise" />
+        ) : (
+          <i className="bi bi-image" />
+        ))}
     </Files>
   </div>
 );
