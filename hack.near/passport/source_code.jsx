446789dcc5996a74e8ee41afdc70e6a992576267
hack.near <hack.near>
2024-03-08 02:44:49 +0000

    Update passport by hack.near at 2024-03-08T02:44:49.104Z

---
+++ hack.near/passport/source_code.jsx
@@ -0,0 +1,64 @@
+const user = Ethers.send("eth_requestAccounts", [])[0];
+
+if (!user) return <Web3Connect connectLabel="Connect" />;
+
+const passportDecoderAbi = fetch(
+  "https://raw.githubusercontent.com/gitcoinco/eas-proxy/main/deployments/abi/GitcoinPassportDecoder.json"
+);
+if (!passportDecoderAbi.ok) {
+  return "ERROR";
+}
+
+const iface = new ethers.utils.Interface(passportDecoderAbi.body);
+
+State.init({
+  address: "0xc979F9D3Db24Ef602FD365caA9D86532c73b6D7E",
+  isHuman: false,
+  error: "",
+});
+
+const decoderContractAddress = "0x5558D441779Eca04A329BcD6b47830D2C6607769";
+
+const getPassport = () => {
+  const contract = new ethers.Contract(
+    decoderContractAddress,
+    passportDecoderAbi.body,
+    Ethers.provider().getSigner()
+  );
+
+  const passport = contract.getPassport(state.address);
+  return passport;
+};
+
+const getScore = () => {
+  const contract = new ethers.Contract(
+    decoderContractAddress,
+    passportDecoderAbi.body,
+    Ethers.provider().getSigner()
+  );
+
+  let score = contract.getScore(state.user);
+  return score;
+};
+
+const checkPassport = () => {
+  const contract = new ethers.Contract(
+    decoderContractAddress,
+    passportDecoderAbi.body,
+    Ethers.provider().getSigner()
+  );
+
+  let isHuman = contract.isHuman(state.user);
+  return isHuman;
+};
+
+return (
+  <div>
+    <button onClick={getPassport}>Get Passport</button>
+    <button onClick={getScore}>Get Score</button>
+    <button onClick={checkPassport}>Check Passport</button>
+    <p>{JSON.stringify(score)}</p>
+    <p>{JSON.stringify(score)}</p>
+    <p>{JSON.stringify(isHuman)}</p>
+  </div>
+);
