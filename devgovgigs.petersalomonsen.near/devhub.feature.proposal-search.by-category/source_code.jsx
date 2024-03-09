7b799dca93a258fb481c1ac5ad532259e69f4c12
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2024-03-09 21:08:56 +0000

    Update devhub.feature.proposal-search.by-category by devgovgigs.petersalomonsen.near at 2024-03-09T21:08:56.732Z

--- devgovgigs.petersalomonsen.near/devhub.feature.proposal-search.by-category/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.feature.proposal-search.by-category/source_code.jsx
@@ -14,7 +14,7 @@ const [selected, setSelected] = useState(null);
 return (
   <div>
     <Widget
-      src="${REPL_DEVHUB}/widget/devhub.components.molecule.DropDown"
+      src="devgovgigs.petersalomonsen.near/widget/devhub.components.molecule.DropDown"
       props={{
         options: options,
         label: "Category",
