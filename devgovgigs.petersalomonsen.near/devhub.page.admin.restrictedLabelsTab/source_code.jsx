5fede5df55dd6a30dcc3b8d371564e1439eec331
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2024-03-09 21:08:56 +0000

    Update devhub.page.admin.restrictedLabelsTab by devgovgigs.petersalomonsen.near at 2024-03-09T21:08:56.732Z

--- devgovgigs.petersalomonsen.near/devhub.page.admin.restrictedLabelsTab/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.page.admin.restrictedLabelsTab/source_code.jsx
@@ -11,7 +11,7 @@ return (
     </h5>
     {!createTeam ? (
       <Widget
-        src={"${REPL_DEVHUB}/widget/devhub.components.molecule.PostControls"}
+        src={"devgovgigs.petersalomonsen.near/widget/devhub.components.molecule.PostControls"}
         props={{
           onClick: () => setCreateTeam(true),
           title: "Create label",
@@ -20,7 +20,7 @@ return (
       />
     ) : (
       <Widget
-        src={"${REPL_DEVHUB}/widget/devhub.entity.team.Configurator"}
+        src={"devgovgigs.petersalomonsen.near/widget/devhub.entity.team.Configurator"}
         props={{
           onCancel: () => setCreateTeam(false),
           onSubmit: (params) =>
@@ -52,7 +52,7 @@ return (
             .map((teamName) => {
               return (
                 <Widget
-                  src={"${REPL_DEVHUB}/widget/devhub.entity.team.LabelRow"}
+                  src={"devgovgigs.petersalomonsen.near/widget/devhub.entity.team.LabelRow"}
                   props={{
                     teamName,
                   }}
