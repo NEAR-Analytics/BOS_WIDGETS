a77f708532da555bb3bd026ffa779daa751e2180
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2024-03-09 21:00:18 +0000

    Update devhub.entity.community.Discussions by devgovgigs.petersalomonsen.near at 2024-03-09T21:00:18.942Z

--- devgovgigs.petersalomonsen.near/devhub.entity.community.Discussions/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.entity.community.Discussions/source_code.jsx
@@ -2,7 +2,7 @@ const NEW_DISCUSSION_POSTED_CONTENT_STORAGE_KEY =
   "new_discussion_posted_content";
 const { handle } = props;
 const { getCommunity, setCommunitySocialDB } = VM.require(
-  "devgovgigs.petersalomonsen.near/widget/core.adapter.devhub-contract"
+  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
 );
 
 getCommunity = getCommunity || (() => <></>);
@@ -76,7 +76,7 @@ const [sort, setSort] = useState("timedesc");
 function repostOnDiscussions(blockHeight) {
   Near.call([
     {
-      contractName: "devhub.near",
+      contractName: "${REPL_DEVHUB_CONTRACT}",
       methodName: "create_discussion",
       args: {
         handle,
@@ -89,7 +89,7 @@ function repostOnDiscussions(blockHeight) {
 
 async function checkHashes() {
   if (props.transactionHashes) {
-    asyncFetch("https://rpc.mainnet.near.org", {
+    asyncFetch("${REPL_RPC_URL}", {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -125,7 +125,7 @@ function getBlockHeightAndRepost() {
   );
   console.log("new discussion content", newDiscussionPostedContent);
 
-  Near.asyncView("social.near", "get", {
+  Near.asyncView("${REPL_SOCIAL_CONTRACT}", "get", {
     keys: [`${context.accountId}/post/**`],
     options: {
       with_block_height: true,
@@ -139,7 +139,11 @@ function getBlockHeightAndRepost() {
         console.log("content matches", blockHeight, post_main);
         repostOnDiscussions(blockHeight);
       } else {
-        console.log("content does not match (yet)", post_main);
+        console.log(
+          "content does not match (yet)",
+          post_main,
+          newDiscussionPostedContent
+        );
         setTimeout(() => getBlockHeightAndRepost(), 500);
       }
     })
@@ -161,7 +165,7 @@ return (
           {context.accountId && (
             <div className="card p-4">
               <Widget
-                src={"devgovgigs.petersalomonsen.near/widget/devhub.entity.community.Compose"}
+                src={"${REPL_DEVHUB}/widget/devhub.entity.community.Compose"}
                 props={{
                   onSubmit: (v) => {
                     Storage.set(
@@ -208,7 +212,7 @@ return (
               src="mob.near/widget/MainPage.N.Feed"
               props={{
                 accounts: [
-                  `discussions.${handle}.community.devhub.near`,
+                  `discussions.${handle}.community.${REPL_DEVHUB_CONTRACT}`,
                 ],
               }}
             />
@@ -232,7 +236,7 @@ return (
                 style={{ fontWeight: 500 }}
               >
                 <Widget
-                  src="devgovgigs.petersalomonsen.near/widget/devhub.components.molecule.ProfileCard"
+                  src="${REPL_DEVHUB}/widget/devhub.components.molecule.ProfileCard"
                   props={{ accountId }}
                 />
               </div>
