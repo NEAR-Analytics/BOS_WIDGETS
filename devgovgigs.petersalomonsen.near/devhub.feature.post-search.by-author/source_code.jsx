4f1b5b968ebb3106f659e539bafbeaef7d435a1f
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2024-03-09 21:00:18 +0000

    Update devhub.feature.post-search.by-author by devgovgigs.petersalomonsen.near at 2024-03-09T21:00:18.942Z

--- devgovgigs.petersalomonsen.near/devhub.feature.post-search.by-author/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.feature.post-search.by-author/source_code.jsx
@@ -1,5 +1,5 @@
 const { getAllAuthors } = VM.require(
-  "devgovgigs.petersalomonsen.near/widget/core.adapter.devhub-contract"
+  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
 );
 
 if (!getAllAuthors) {
