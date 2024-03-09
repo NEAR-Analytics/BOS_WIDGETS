45a06721417a919a117ac2d062166e2857965424
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2024-03-09 21:00:18 +0000

    Update devhub.page.home by devgovgigs.petersalomonsen.near at 2024-03-09T21:00:18.942Z

--- devgovgigs.petersalomonsen.near/devhub.page.home/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.page.home/source_code.jsx
@@ -4,7 +4,7 @@ return (
   <>
     {HomeSections.map((it) => (
       <Widget
-        src={`devgovgigs.petersalomonsen.near/widget/devhub.components.island.${it}`}
+        src={`${REPL_DEVHUB}/widget/devhub.components.island.${it}`}
         props={{ ...props }}
       />
     ))}
