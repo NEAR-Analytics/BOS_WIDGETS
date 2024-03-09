53244f537ef0b4237285ed214d3e4122880be760
nearblocks.near <nearblocks.near>
2024-03-09 00:02:31 +0000

    Update bos-components.components.NFT.Inventory by nearblocks.near at 2024-03-09T00:02:31.595Z

--- nearblocks.near/bos-components.components.NFT.Inventory/source_code.jsx
+++ nearblocks.near/bos-components.components.NFT.Inventory/source_code.jsx
@@ -512,6 +512,16 @@ function capitalizeWords(str) {
   const result = capitalizedWords.join(' ');
   return result;
 }
+
+function toSnakeCase(str) {
+  return str
+    .replace(/[A-Z]/g, (match) => '_' + match.toLowerCase())
+    .replace(/^_/, '');
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 function truncateString(str, maxLength, suffix) {
   if (str.length <= maxLength) {
     return str;
@@ -660,6 +670,38 @@ function isAction(type) {
 
   return actions.includes(type.toUpperCase());
 }
+
+function isJson(string) {
+  const str = string.replace(/\\/g, '');
+
+  try {
+    JSON.parse(str);
+    return false;
+  } catch (e) {
+    return false;
+  }
+}
+
+function uniqueId() {
+  return Math.floor(Math.random() * 1000);
+}
+function handleRateLimit(
+  data,
+  reFetch,
+  Loading,
+) {
+  if (data.status === 429 || data.status === undefined) {
+    const retryCount = 4;
+    const delay = Math.pow(2, retryCount) * 1000;
+    setTimeout(() => {
+      reFetch();
+    }, delay);
+  } else {
+    if (Loading) {
+      Loading();
+    }
+  }
+}
 function localFormat(number) {
   const bigNumber = Big(number);
   const formattedNumber = bigNumber
@@ -669,6 +711,23 @@ function localFormat(number) {
 }
 function formatWithCommas(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+function handleRateLimit(
+  data,
+  reFetch,
+  Loading,
+) {
+  if (data.status === 429 || data.status === undefined) {
+    const retryCount = 4;
+    const delay = Math.pow(2, retryCount) * 1000;
+    setTimeout(() => {
+      reFetch();
+    }, delay);
+  } else {
+    if (Loading) {
+      Loading();
+    }
+  }
 }
 /* END_INCLUDE: "includes/libs.jsx" */
 
@@ -711,6 +770,8 @@ function MainComponent({ network, id, token }) {
             if (data.status === 200) {
               setTokenData(resp);
               setIsLoading(false);
+            } else {
+              handleRateLimit(data, fetchNFTData, () => setIsLoading(false));
             }
           },
         )
@@ -734,6 +795,8 @@ function MainComponent({ network, id, token }) {
             const resp = data?.body?.tokens?.[0];
             if (data.status === 200) {
               setTotalCount(resp?.count);
+            } else {
+              handleRateLimit(data, fetchTotalToken);
             }
           },
         )
@@ -757,12 +820,12 @@ function MainComponent({ network, id, token }) {
           const resp = data?.body?.tokens;
           if (data.status === 200 && Array.isArray(resp) && resp.length > 0) {
             setTokens(resp);
+            setIsLoading(false);
+          } else {
+            handleRateLimit(data, fetchTokenData, () => setIsLoading(false));
           }
         })
-        .catch(() => {})
-        .finally(() => {
-          setIsLoading(false);
-        });
+        .catch(() => {});
     }
     if (!token && token === undefined) {
       fetchNFTData();
