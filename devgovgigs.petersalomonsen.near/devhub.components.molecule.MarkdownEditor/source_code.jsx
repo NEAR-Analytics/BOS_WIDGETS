0b2192132b816daac65f2e51163b8c1e3808bdc5
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2024-03-09 21:00:18 +0000

    Update devhub.components.molecule.MarkdownEditor by devgovgigs.petersalomonsen.near at 2024-03-09T21:00:18.942Z

--- devgovgigs.petersalomonsen.near/devhub.components.molecule.MarkdownEditor/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.components.molecule.MarkdownEditor/source_code.jsx
@@ -1,7 +1,7 @@
 const MarkdownEditor = ({ data, onChange, autoFocus }) => {
   return (
     <Widget
-      src={"devgovgigs.petersalomonsen.near/widget/devhub.components.molecule.SimpleMDE"}
+      src={"${REPL_DEVHUB}/widget/devhub.components.molecule.SimpleMDE"}
       props={{
         data,
         onChange,
