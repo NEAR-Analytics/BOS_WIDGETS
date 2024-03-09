bcced1a39274bc48c2ce33f87e695e7757bee16a
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2024-03-09 21:08:56 +0000

    Update devhub.entity.addon.blog.Page by devgovgigs.petersalomonsen.near at 2024-03-09T21:08:56.732Z

--- devgovgigs.petersalomonsen.near/devhub.entity.addon.blog.Page/source_code.jsx
+++ devgovgigs.petersalomonsen.near/devhub.entity.addon.blog.Page/source_code.jsx
@@ -1,5 +1,5 @@
 const { getAccountCommunityPermissions } = VM.require(
-  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
+  "devgovgigs.petersalomonsen.near/widget/core.adapter.devhub-contract"
 ) || { getAccountCommunityPermissions: () => {} };
 const imagelink =
   "https://ipfs.near.social/ipfs/bafkreiajzvmy7574k7mp3if6u53mdukfr3hoc2kjkhjadt6x56vqhd5swy";
@@ -115,7 +115,7 @@ function Page({ data, onEdit, labels, accountId }) {
         <p>{description}</p>
         <Widget
           src={
-            "${REPL_DEVHUB}/widget/devhub.components.molecule.MarkdownViewer"
+            "devgovgigs.petersalomonsen.near/widget/devhub.components.molecule.MarkdownViewer"
           }
           props={{ text: content }}
         />
