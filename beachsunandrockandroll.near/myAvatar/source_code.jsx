8dc3617e0808493bb88109563e619fa648cb050e
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-03-11 11:41:09 +0000

    Update myAvatar by beachsunandrockandroll.near at 2024-03-11T11:41:09.297Z

--- beachsunandrockandroll.near/myAvatar/source_code.jsx
+++ beachsunandrockandroll.near/myAvatar/source_code.jsx
@@ -2,27 +2,31 @@ const css = fetch("https://floatui.com/tailwind.css").body;
 if (!css) return "";
 const Tailwind = styled.div`${css}`;
 
-const MyAvatar = ({ className, ...props }) => (
+const MyAvatar = ({ className, children, ...props }) => (
   <Tailwind>
     <Avatar.Root
       className="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full"
       {...props}
-    />
+    >
+      {children}
+    </Avatar.Root>
   </Tailwind>
 );
 
-const MyAvatarImage = ({ className, ...props }) => (
+const MyAvatarImage = ({ className, children, ...props }) => (
   <Tailwind>
     <Avatar.Image className="aspect-square h-full w-full" {...props} />
   </Tailwind>
 );
 
-const MyAvatarFallback = ({ className, ...props }) => (
+const MyAvatarFallback = ({ className, children, ...props }) => (
   <Tailwind>
     <Avatar.Fallback
-      className="flex h-full w-full items-center justify-center rounded-full bg-muted"
+      className={`flex h-full w-full items-center justify-center rounded-full bg-muted ${className}`}
       {...props}
-    />
+    >
+      {children}
+    </Avatar.Fallback>
   </Tailwind>
 );
 
