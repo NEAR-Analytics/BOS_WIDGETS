9062bdf9a0d2112b0efda4f427e7c54823404c07
bluebiu.near <bluebiu.near>
2024-03-12 08:35:59 +0000

    Update Liquidity.Data.Gamma by bluebiu.near at 2024-03-12T08:35:59.159Z

--- bluebiu.near/Liquidity.Data.Gamma/source_code.jsx
+++ bluebiu.near/Liquidity.Data.Gamma/source_code.jsx
@@ -11,7 +11,8 @@ const {
   prices
 } = props
 
-// const MULTICALL_ADDRESS = "0xcA11bde05977b3631167028862bE2a173976CA11"
+let loading = false
+let dataList = []
 const MULTICALL_ABI = [
   {
     inputs: [
@@ -95,7 +96,6 @@ const MulticallContract = new ethers.Contract(
   MULTICALL_ABI,
   Ethers.provider().getSigner()
 );
-
 const multicallv2 = (abi, calls, options, onSuccess, onError) => {
   const { requireSuccess, ...overrides } = options || {};
   const itf = new ethers.utils.Interface(abi);
@@ -119,135 +119,152 @@ const multicallv2 = (abi, calls, options, onSuccess, onError) => {
       onError?.(err);
     });
 };
-
+const fetchFusionsData = () => {
+  asyncFetch("https://api.lynex.fi/api/v1/fusions").then((res) => {
+    if (!res.ok) return;
+    State.update({
+      fusionsData: res?.body?.data,
+    });
+  });
+}
 const formatPercent = (value) => {
   return `${Number(value * 100).toLocaleString("en", {
     maximumFractionDigits: 2,
   })}%`;
 };
 
-
-useEffect(() => {
-  let loading = false
-  let dataList = []
-  function formatedData() {
-    onLoad({
-      loading,
-      dataList
+function formatedData() {
+  onLoad({
+    loading,
+    dataList
+  })
+}
+function getDataList() {
+  pairs.forEach(pair => {
+    const vaultAddress = addresses[pair.id]
+    const data = allData[vaultAddress]
+    dataList.push({
+      ...data,
+      ...pair,
+      vaultAddress,
     })
+  })
+  formatedData('dataList')
+}
+function getBalance() {
+
+}
+function getLiquidity() {
+  const calls = [];
+  const sender = Ethers.send("eth_requestAccounts", [])[0];
+  dataList.forEach(data => {
+    calls.push({
+      address: data.vaultAddress,
+      name: "balanceOf",
+      params: [sender],
+    });
+  })
+  multicallv2(
+    ERC20_ABI,
+    calls,
+    {},
+    (res) => {
+      for (let i = 0, len = res.length; i < len; i++) {
+        if (res[i]) {
+          dataList[i].liquidity = Big(ethers.utils.formatUnits(res[i][0]._hex)).toFixed(2)
+        }
+      }
+      formatedData('getLiquidity')
+    },
+    (error) => {
+      setTimeout(() => {
+        getLiquidity();
+      }, 500);
+    }
+  )
+}
+function getTotalApr() {
+  const name = curChain.name
+  if (['Base', 'Optimism', 'Polygon zkEVM'].includes(name)) {
+    dataList = dataList.map(data => {
+      data.totalApr = formatPercent(data.returns.weekly.feeApr)
+      return data
+    })
+    formatedData('getTotalApr')
   }
-  function getDataList() {
-    pairs.forEach(pair => {
-      const vaultAddress = addresses[pair.id]
-      const data = allData[vaultAddress]
-      dataList.push({
-        ...data,
-        ...pair,
-        vaultAddress,
+  if (name === 'Linea') {
+    asyncFetch("https://api.lynex.fi/api/v1/fusions").then((res) => {
+      if (!res.ok) return;
+      const fusionsData = res?.body?.data
+      dataList = dataList.map(data => {
+        const fusionData = fusionsData.find(fusionData => fusionData.address === data.vaultAddress)
+        data.totalApr = ((fusionData?.gauge?.tvl ?? 0 > 0) ? Big(fusionData?.gauge?.rewardPerSecond ?? 0)
+          .times(365 * 24 * 60 * 60)
+          .times(prices?.Lynex ?? 0)
+          .times(100)
+          .div(fusionData?.gauge?.tvl ?? 0).toFixed(2) : '0.00') + '%'
+        return data
       })
-    })
-    formatedData('dataList')
+      formatedData('getTotalApr')
+    });
   }
-  function getBalance() {
-
-  }
-  function getLiquidity() {
+  if (name === 'BSC') {
     const calls = [];
-    const sender = Ethers.send("eth_requestAccounts", [])[0];
+    const addressMap = {
+      'ETH-WBNB-0': '0xD777E84b0D29128351A35045D7AE728780dEf54D',
+      'BTCB-WBNB-0': '0x65E40E779560199F5e68126Bc95bdc03083e5AA4',
+      'USDT-USDC-0': '0x1011530830c914970CAa96a52B9DA1C709Ea48fb',
+      'USDT-WBNB-0': '0xf50Af14BC4953Dcf9d27EbCA8BB3625855F5B42d',
+      'BNBx-WBNB-0': '0xf50Af14BC4953Dcf9d27EbCA8BB3625855F5B42d',
+    }
     dataList.forEach(data => {
       calls.push({
-        address: data.vaultAddress,
-        name: "balanceOf",
-        params: [sender],
+        address: addressMap[data.id],
+        name: "rewardRate",
       });
     })
     multicallv2(
       ERC20_ABI,
       calls,
       {},
-      (res) => {
-        console.log('====res', res)
+      res => {
         for (let i = 0, len = res.length; i < len; i++) {
-          if (res[i]) {
-            dataList[i].liquidity = Big(ethers.utils.formatUnits(res[i][0]._hex)).toFixed(2)
-          }
+          dataList[i]['totalApr'] = (dataList[i].tvlUSD > 0 ? Big(ethers.utils.formatUnits(res[i][0]._hex))
+            .mul(365 * 24 * 60 * 60)
+            .mul(prices['THE'])
+            .div(dataList[i].tvlUSD)
+            .toFixed(2) : '0.00') + '%'
         }
-        formatedData('getLiquidity')
+        formatedData('getTotalApr')
       },
-      (error) => {
+      error => {
+        // console.log('=====error', error)
         setTimeout(() => {
-          getLiquidity();
-        }, 500);
+          getTotalApr()
+        }, 500)
       }
     )
   }
-  function getTotalApr() {
-    const name = curChain.name
-    if (['Base', 'Optimism', 'Linea', 'Polygon zkEVM'].includes(name)) {
-      dataList = dataList.map(data => {
-        data.totalApr = formatPercent(data.returns.weekly.feeApr)
-        return data
-      })
-    }
-    if (name === 'BSC') {
-      const calls = [];
-      const addressMap = {
-        'ETH-WBNB-0': '0xD777E84b0D29128351A35045D7AE728780dEf54D',
-        'BTCB-WBNB-0': '0x65E40E779560199F5e68126Bc95bdc03083e5AA4',
-        'USDT-USDC-0': '0x1011530830c914970CAa96a52B9DA1C709Ea48fb',
-        'USDT-WBNB-0': '0xf50Af14BC4953Dcf9d27EbCA8BB3625855F5B42d',
-        'BNBx-WBNB-0': '0xf50Af14BC4953Dcf9d27EbCA8BB3625855F5B42d',
-      }
-      dataList.forEach(data => {
-        calls.push({
-          address: addressMap[data.id],
-          name: "rewardRate",
-        });
-      })
-      multicallv2(
-        ERC20_ABI,
-        calls,
-        {},
-        res => {
-          for (let i = 0, len = res.length; i < len; i++) {
-            dataList[i]['totalApr'] = (dataList[i].tvlUSD > 0 ? Big(ethers.utils.formatUnits(res[i][0]._hex))
-              .mul(365 * 24 * 60 * 60)
-              .mul(prices['THE'])
-              .div(dataList[i].tvlUSD)
-              .toFixed(2) : '0.00') + '%'
+}
+function getFeeTiers() {
+  asyncFetch(LAST_SNAP_SHOT_DATA_URL)
+    .then(res => {
+      if (res.ok) {
+        dataList.forEach((data, index) => {
+          const findIndex = res.body.findIndex(source => data.vaultAddress === source.address)
+          if (findIndex > -1) {
+            dataList[index]['fee'] = Big(res.body[findIndex].fee).div(100).toFixed(2)
           }
-          formatedData('getTotalApr')
-        },
-        error => {
-          // console.log('=====error', error)
-          setTimeout(() => {
-            getTotalApr()
-          }, 500)
-        }
-      )
-    }
-    if (name === 'Polygon') {
+        })
+        formatedData('getFeeTiers')
+      }
+    })
+    .catch(error => {
+      console.log('error', error)
+    })
+}
 
-    }
-    formatedData('getTotalApr')
-  }
-  function getFeeTiers() {
-    asyncFetch(LAST_SNAP_SHOT_DATA_URL)
-      .then(res => {
-        if (res.ok) {
-          dataList.forEach((data, index) => {
-            const findIndex = res.body.findIndex(source => data.vaultAddress === source.address)
-            if (findIndex > -1) {
-              dataList[index]['fee'] = Big(res.body[findIndex].fee).div(100).toFixed(2)
-            }
-          })
-          formatedData('getFeeTiers')
-        }
-      })
-      .catch(error => {
-        console.log('error', error)
-      })
-  }
+useEffect(() => {
   getDataList()
   getLiquidity()
   getBalance()
