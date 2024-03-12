c51a4e3813f7e8bf354fb7d90b6f1e7c91c7ebd2
saidulbadhon.near <saidulbadhon.near>
2024-03-12 14:19:07 +0000

    Update MyCounter by saidulbadhon.near at 2024-03-12T14:19:07.456Z

---
+++ saidulbadhon.near/MyCounter/source_code.jsx
@@ -0,0 +1,9 @@
+const [counter, setCounter] = useState(0);
+
+return (
+  <div style={{ display: "flex", gap: 10 }}>
+    <button onClick={() => setCounter((e) => (e -= 1))}>-</button>
+    <h1>{counter}</h1>
+    <button onClick={() => setCounter((e) => (e += 1))}>+</button>
+  </div>
+);
