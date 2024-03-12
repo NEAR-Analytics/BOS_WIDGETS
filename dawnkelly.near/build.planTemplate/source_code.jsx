7fa08c737c20dbd7098c78fb41021267d2b689ba
dawnkelly.near <dawnkelly.near>
2024-03-12 17:40:25 +0000

    Update build.planTemplate by dawnkelly.near at 2024-03-12T17:40:25.097Z

--- dawnkelly.near/build.planTemplate/source_code.jsx
+++ dawnkelly.near/build.planTemplate/source_code.jsx
@@ -57,6 +57,30 @@ const handlePlanNameChange = (e) => {
   }));
 };
 
+const handleGoalsChange = (e) => {
+  const newGoals = e.target.value;
+  setPlan((prevPlan) => ({
+    ...prevPlan,
+    metadata: { ...prevPlan.metadata, goals: newGoals },
+  }));
+};
+
+const handleStartDateChange = (e) => {
+  const newStartDate = e.target.value;
+  setPlan((prevPlan) => ({
+    ...prevPlan,
+    metadata: { ...prevPlan.metadata, startDate: newStartDate },
+  }));
+};
+
+const handleEndDateChange = (e) => {
+  const newEndDate = e.target.value;
+  setPlan((prevPlan) => ({
+    ...prevPlan,
+    metadata: { ...prevPlan.metadata, endDate: newEndDate },
+  }));
+};
+
 const handleSave = () => {
   Social.set({
     build: { plan },
@@ -66,7 +90,7 @@ const handleSave = () => {
 return (
   <div>
     <div className="m-2">
-      <h4>Create/Update A Plan</h4>
+      <h2>Create/Update A Plan</h2>
       <div className="mb-3 p-1">
         <label for="planName">Name:</label>
         <input
@@ -85,9 +109,64 @@ return (
             rows="4"
             cols="80"
             placeholder="Define project goals here. Remember to include outcomes, timelines, and criteria for measuring success."
-            value={plan.metadata.planName}
-            onChange={handlePlanNameChange}
+            value={plan.metadata.goals}
+            onChange={handleGoalsChange}
           />
+        </div>
+        <div className="mb-3 p-1">
+          <h3>Timeline:</h3>
+          <label>Start date:</label>
+          <input
+            id="startDate"
+            name="startDate"
+            type="date"
+            min="2024-03-12"
+            value={plan.metadata.timeline.startDate}
+            onChange={handleStartDateChange}
+          />
+          <label>End date:</label>
+          <input
+            id="endDate"
+            name="endDate"
+            type="date"
+            min="2024-03-12"
+            value={plan.metadata.timeline.endDate}
+            onChange={handleEndDateChange}
+          />
+        </div>
+        <div>
+          Team Members and Roles Two fields with "add another" + type button
+        </div>
+        <div>
+          <h3>Project Tasks</h3>
+          <p>task id</p>
+          <p>assigned</p>
+          <p>estimated effort</p>
+          <p>status</p>
+        </div>
+        <div>
+          <h3>Dependencies</h3>
+        </div>
+        <div>
+          <h3>Milestones</h3>
+        </div>
+        <div>
+          <h3>Open collaborations</h3>
+        </div>
+        <div>
+          <h4>Review Meeting</h4>
+          <p>date scheduled</p>
+          <p>date held</p>
+          <p>notes</p>
+          //Objectives: evaluate what was completed, what wasn't, and why.
+        </div>
+        <div>
+          <h4>Retrospective Meeting</h4>
+          <p>date scheduled</p>
+          <p>date held</p>
+          <p>notes</p>
+          //Objectives: reflect on the sprint process, what went well, what
+          didn’t, and how processes could be improved.
         </div>
         <div className="m-3">
           <button
