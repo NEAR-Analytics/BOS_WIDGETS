3099795fa94b66c28076c68388a6d9999bbea55c
dawnkelly.near <dawnkelly.near>
2024-03-13 16:26:44 +0000

    Update build.teamMembersAndRoles by dawnkelly.near at 2024-03-13T16:26:44.097Z

---
+++ dawnkelly.near/build.teamMembersAndRoles/source_code.jsx
@@ -0,0 +1,43 @@
+const [builders, setBuilders] = useState([{ name: "", role: "" }]);
+
+const handleNameChange = (e, index) => {
+  const { value } = e.target;
+  const updatedBuilders = [...builders];
+  updatedBuilders[index].name = value;
+  setBuilders(updatedBuilders);
+};
+
+const handleRoleChange = (e, index) => {
+  const { value } = e.target;
+  const updatedBuilders = [...builders];
+  updatedBuilders[index].role = value;
+  setBuilders(updatedBuilders);
+};
+
+const addBuilder = () => {
+  setBuilders([...builders, { name: "", role: "" }]);
+};
+
+const handleDeleteBuilder = (index) => {
+  setBuilders((prevBuilders) => {
+    const updatedBuilders = [...prevBuilders];
+    updatedBuilders.splice(index, 1);
+    return updatedBuilders;
+  });
+};
+
+return (
+  <div>
+    <h3>Team Members and Roles</h3>
+    {builders.map((builder, index) => (
+      <Widget
+        src="dawnkelly.near/widget/build.builderInput"
+        key={index}
+        index={index}
+        handleNameChange={handleNameChange}
+        handleRoleChange={handleRoleChange}
+      />
+    ))}
+    <button onClick={addBuilder}>Add Builder</button>
+  </div>
+);
