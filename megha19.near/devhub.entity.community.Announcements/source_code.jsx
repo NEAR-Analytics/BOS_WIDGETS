56e427fc3b02cdda36050b474b91877aabe692ab
megha19.near <megha19.near>
2024-03-13 16:13:35 +0000

    Update devhub.entity.community.Announcements by megha19.near at 2024-03-13T16:13:35.656Z

--- megha19.near/devhub.entity.community.Announcements/source_code.jsx
+++ megha19.near/devhub.entity.community.Announcements/source_code.jsx
@@ -124,9 +124,7 @@ return (
               src="megha19.near/widget/devhub.components.organism.Feed"
               props={{
                 showFlagAccountFeature: true,
-                filteredAccountIds: [
-                  `${handle}.community.truedove38.near`,
-                ],
+                filteredAccountIds: [`${handle}.community.truedove38.near`],
                 sort: sort,
                 setPostExists: setPostExists,
                 showFlagAccountFeature: true,
