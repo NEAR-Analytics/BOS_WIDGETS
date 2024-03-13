b0036bcb65b7c33043e9979971aec2e3c9c17abf
megha19.near <megha19.near>
2024-03-13 16:13:35 +0000

    Update app by megha19.near at 2024-03-13T16:13:35.656Z

--- megha19.near/app/source_code.jsx
+++ megha19.near/app/source_code.jsx
@@ -46,10 +46,7 @@ function Page() {
   switch (routes[0]) {
     case "home": {
       return (
-        <Widget
-          src="megha19.near/widget/devhub.page.home"
-          props={passProps}
-        />
+        <Widget src="megha19.near/widget/devhub.page.home" props={passProps} />
       );
     }
     // ?page=communities
