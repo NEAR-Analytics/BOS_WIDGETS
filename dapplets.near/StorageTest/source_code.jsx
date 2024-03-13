97ee7ef287c5b346d53da234b8b7f75c8b690210
dapplets.near <dapplets.near>
2024-03-13 21:34:30 +0000

    Update StorageTest by dapplets.near at 2024-03-13T21:34:30.634Z

--- dapplets.near/StorageTest/source_code.jsx
+++ dapplets.near/StorageTest/source_code.jsx
@@ -3,9 +3,16 @@ const storeValue = () => {
   Storage.set("time_now", date);
 };
 
+const time = Storage.get("time_now");
+
 return (
   <>
-    <p> Time Stored: {Storage.get("time_now")} </p>
-    <button onClick={storeValue}>Store Date.now()</button>
+    <p> Time Stored: {time} </p>
+    <button
+      onClick={storeValue}
+      styles={{ backgroundColor: !time ? "#f00" : undefined }}
+    >
+      Store Date.now()
+    </button>
   </>
 );
