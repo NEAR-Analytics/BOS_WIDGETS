a29ee201864ad267ba137df305185b4bfb8e9ca0
microchipgnu.near <microchipgnu.near>
2024-03-13 00:01:02 +0000

    Update MultipleAgents by microchipgnu.near at 2024-03-13T00:01:02.616Z

--- microchipgnu.near/MultipleAgents/source_code.jsx
+++ microchipgnu.near/MultipleAgents/source_code.jsx
@@ -18,23 +18,5 @@ return (
         ],
       }}
     />
-    <Widget
-      src="microchipgnu.near/widget/Agent"
-      props={{
-        role: "WeatherMan",
-        backstory: "Helpful weather man",
-        goal: "Checks weather in locations",
-        tools: [
-          {
-            name: "getWeather",
-            description: "Checks the current state of weather",
-            inputDescription: "takes city as input",
-            callback: (city) => {
-              return "It's sunny in" + city;
-            },
-          },
-        ],
-      }}
-    />
   </>
 );
