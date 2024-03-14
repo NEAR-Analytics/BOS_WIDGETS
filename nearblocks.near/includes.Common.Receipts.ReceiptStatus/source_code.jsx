bcd9de17508478d6da9d5fb5a50e1424ff336504
nearblocks.near <nearblocks.near>
2024-03-14 13:01:23 +0000

    Update includes.Common.Receipts.ReceiptStatus by nearblocks.near at 2024-03-14T13:01:23.338Z

--- nearblocks.near/includes.Common.Receipts.ReceiptStatus/source_code.jsx
+++ nearblocks.near/includes.Common.Receipts.ReceiptStatus/source_code.jsx
@@ -1,8 +1,39 @@
-import { hexy } from '@/includes/hexy';
+import { formatLine } from '@/includes/near';
 
 
 const ReceiptStatus = (props) => {
   const { receipt } = props;
+
+  function hexDump(
+    data,
+    options
+
+
+,
+  ) {
+    const { width, format } = options;
+
+    let result = '';
+    let line = '';
+    const w = width ? width : 16;
+
+    for (let i = 0; i < data.length; i++) {
+      if (i > 0 && i % w === 0) {
+        result += formatLine(line, i - w, format) + '\n';
+        line = '';
+      }
+
+      const byte = data[i];
+      line += byte.toString(16).padStart(2, '0') + ' ';
+    }
+
+    if (line.length > 0) {
+      result +=
+        formatLine(line, data.length - (data.length % w), format) + '\n';
+    }
+
+    return result;
+  }
 
   function displayArgs(args) {
     if (!args || typeof args === 'undefined') return 'The arguments are empty';
@@ -15,10 +46,10 @@ const ReceiptStatus = (props) => {
       if (parsed) {
         pretty = JSON.stringify(parsed, null, 2);
       } else {
-        pretty = hexy(decoded, { format: 'twos' });
+        pretty = hexDump(decoded, { format: 'twos' });
       }
     } catch {
-      pretty = hexy(decoded, { format: 'twos' });
+      pretty = hexDump(decoded, { format: 'twos' });
     }
 
     return pretty;
