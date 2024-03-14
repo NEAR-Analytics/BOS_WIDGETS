6007dd9c485e027effb4d5538f0da771e684e50f
klngrs.near <klngrs.near>
2024-03-14 14:11:09 +0000

    Update FloatUi by klngrs.near at 2024-03-14T14:11:09.326Z

--- klngrs.near/FloatUi/source_code.jsx
+++ klngrs.near/FloatUi/source_code.jsx
@@ -2,8 +2,4 @@ const css = fetch("https://floatui.com/tailwind.css").body;
 if (!css) return "";
 const Tailwind = styled.div`${css}`;
 
-return (
-  <>
-    <Tailwind>{props.Slot}</Tailwind>
-  </>
-);
+return <Tailwind></Tailwind>;
