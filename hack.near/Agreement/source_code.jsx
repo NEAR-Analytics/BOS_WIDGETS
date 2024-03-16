4634c704a30d14f211a35900c3234f0090881283
hack.near <hack.near>
2024-03-16 17:23:02 +0000

    Update Agreement by hack.near at 2024-03-16T17:23:02.023Z

--- hack.near/Agreement/source_code.jsx
+++ hack.near/Agreement/source_code.jsx
@@ -4,6 +4,7 @@ State.init({
   signature: true,
   isFetched: false,
   agreement: true,
+  showModal: false,
 });
 
 if (context.accountId && !state.isFetched) {
@@ -29,34 +30,96 @@ if (context.accountId && !state.isFetched) {
   );
 }
 
-const showAgreement = !state.signature && state.agreement;
+const handleAccept = () => {
+  State.update({ showModal: true });
+};
+
+const showAgreement = !state.signature && !state.agreement;
+
+const AcceptButton = styled("Tooltip.Trigger")`
+  a {
+    font-size: 0.9em;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1em;
+    color: #000000;
+    text-decoration: none;
+
+    &:hover,
+    &:visited {
+      color: #000000;
+      text-decoration: none;
+    }
+  }
+
+  color: #000;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  padding: 0.5em 0.5em;
+  margin: 2em;
+  background: #00ec97;
+  border-radius: 18px;
+  border: none;
+  box-shadow:
+    hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
+    hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+  z-index: 10;
+`;
+
+const daoId = props.daoId ?? "build.sputnik-dao.near";
+const policy = Near.view(daoId, "get_policy");
+
+if (!policy) {
+  return "";
+}
+
+const groupId = props.groupId ?? "community";
+
+const groups = policy.roles
+  .filter((role) => role.name === groupId)
+  .map((role) => {
+    const group = role.kind.Group;
+
+    return group;
+  });
+
+const communityMember = groups.map((group) => {
+  return !group
+    ? false
+    : group.filter((address) => address === accountId).length > 0;
+})?.[0];
 
 return (
   <>
-    <Widget src={`${ownerId}/widget/Support`} />
-    <Widget
-      src={`${ownerId}/widget/AgreementModal`}
-      props={{
-        open: showAgreement,
-        accept: () =>
-          Social.set(
-            { profile: { builder: true } },
-            {
-              onCommit: () => {
-                State.update({ signature: true });
-              },
-            }
-          ),
-      }}
-    />
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Widget src={`${ownerId}/widget/AgreementPage`} />
-    </div>
+    {communityMember && (
+      <Tooltip.Provider>
+        <Tooltip.Root>
+          <AcceptButton>
+            <button className="btn btn-white" onClick={handleAccept}>
+              <b>Accept</b> <i class="bi bi-check"></i>
+            </button>
+          </AcceptButton>
+        </Tooltip.Root>
+      </Tooltip.Provider>
+    )}
+    {state.showModal && (
+      <Widget
+        src={`${ownerId}/widget/AgreementModal`}
+        props={{
+          open: true,
+          accept: () =>
+            Social.set(
+              { profile: { builder: true } },
+              {
+                onCommit: () => {
+                  State.update({ signature: true, showModal: false });
+                },
+              }
+            ),
+        }}
+      />
+    )}
+    <Widget src={`${ownerId}/widget/AgreementPage`} />
   </>
 );
