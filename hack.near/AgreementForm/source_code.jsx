c033b907c7625789106d1738747e7699b02f0444
hack.near <hack.near>
2024-03-16 14:26:06 +0000

    Update AgreementForm by hack.near at 2024-03-16T14:26:06.519Z

---
+++ hack.near/AgreementForm/source_code.jsx
@@ -0,0 +1,67 @@
+const ownerId = "hack.near";
+
+State.init({
+  agree: false,
+});
+
+const Footer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const CloseButton = styled.a`
+  padding: 0.75em 1em;
+  gap: 0.5em;
+  background: #fafafa;
+  border: 1px solid #eceef0;
+  border-radius: 50px;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 0.95em;
+  line-height: 1em;
+  text-align: center;
+  color: #101828;
+`;
+
+return (
+  <>
+    <Dialog.Description>
+      <Widget
+        src={`${ownerId}/widget/nui.checkbox`}
+        props={{
+          label: (
+            <>
+              By checking this box you acknowledge that you understand and agree
+              with{" "}
+              <a href={`/${ownerId}/widget/Agreement`}>
+                NEAR Builders Cooperative Membership Agreement
+              </a>
+            </>
+          ),
+          value: state.agree,
+          id: "agree",
+          onChange: (agree) => State.update({ agree: !state.agree }),
+        }}
+      />
+    </Dialog.Description>
+    <Footer>
+      <Dialog.Close asChild>
+        <CloseButton href="/">Close</CloseButton>
+      </Dialog.Close>
+      <Widget
+        src={`${ownerId}/widget/nui.button`}
+        props={{
+          text: <>Accept</>,
+          disabled: !state.agree,
+          onClick: () => {
+            if (!state.agree) return;
+            props.accept();
+          },
+        }}
+      />
+    </Footer>
+  </>
+);
