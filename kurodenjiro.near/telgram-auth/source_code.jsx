fba4fbb1de80325f824e55347aec9fdfb5561080
kurodenjiro.near <kurodenjiro.near>
2024-03-18 17:17:15 +0000

    Update telgram-auth by kurodenjiro.near at 2024-03-18T17:17:15.437Z

--- kurodenjiro.near/telgram-auth/source_code.jsx
+++ kurodenjiro.near/telgram-auth/source_code.jsx
@@ -1,3 +1,4 @@
+console.log(props);
 return (
   <a href="https://oauth.telegram.org/auth?bot_id=6458699487&origin=https://near.social&embed=1&request_access=write&return_to=https://near.social/kurodenjiro.near/widget/telgram-auth">
     Telegram Auth
