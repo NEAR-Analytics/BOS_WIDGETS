2f77c8b61138850e0b8719f9deb05fd46e857e8f
microchipgnu.near <microchipgnu.near>
2024-03-18 00:53:30 +0000

    Update MultipleAgents by microchipgnu.near at 2024-03-18T00:53:30.212Z

--- microchipgnu.near/MultipleAgents/source_code.jsx
+++ microchipgnu.near/MultipleAgents/source_code.jsx
@@ -1,16 +1,18 @@
 const tools = VM.require("microchipgnu.near/widget/Tools");
 
 return (
-  <div className="d-flex gap-4">
+  <div className="d-flex flex-column gap-4">
     <Widget
       src="microchipgnu.near/widget/Agent"
       props={{
+        role: "Developer",
         tools: tools,
       }}
     />
     <Widget
       src="microchipgnu.near/widget/Agent"
       props={{
+        role: "Designer",
         tools: tools,
       }}
     />
