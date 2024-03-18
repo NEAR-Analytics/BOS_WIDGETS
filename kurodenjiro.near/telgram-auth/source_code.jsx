b932d762b48f75325c0be947b79f73dcaebf55b4
kurodenjiro.near <kurodenjiro.near>
2024-03-18 19:02:00 +0000

    Update telgram-auth by kurodenjiro.near at 2024-03-18T19:02:00.643Z

--- kurodenjiro.near/telgram-auth/source_code.jsx
+++ kurodenjiro.near/telgram-auth/source_code.jsx
@@ -1,7 +1,7 @@
 console.log(props);
 console.log(window.location.href);
 return (
-  <a href="https://oauth.telegram.org/auth?bot_id=6458699487&origin=https://near.social&embed=1&request_access=write&return_to=https://near.social/">
+  <a href="https://oauth.telegram.org/auth?bot_id=6458699487&origin=https://nearbadger-verifiers-api.vercel.app&embed=1&request_access=write&return_to=https://nearbadger-verifiers-api.vercel.app/telegram-auth">
     Telegram Auth
   </a>
 );
