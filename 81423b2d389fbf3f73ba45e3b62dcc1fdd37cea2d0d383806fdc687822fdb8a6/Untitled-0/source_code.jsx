95ccb4c496d9b8f21b18764f57c9be35da252277
81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6 <81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6>
2024-03-18 20:58:48 +0000

    Update Untitled-0 by 81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6 at 2024-03-18T20:58:48.844Z

--- 81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6/Untitled-0/source_code.jsx
+++ 81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6/Untitled-0/source_code.jsx
@@ -46,7 +46,7 @@ return (
   <div className="d-flex flex-column align-items-center">
     <LabelArea>
       <Image
-        src="https://i.near.social/magic/large/https://near.social/magic/img/account/nearukraineguild.near"
+        src="https://zealy-webapp-images-prod.s3.eu-west-1.amazonaws.com/public/5d8a56da-0df6-4e25-ba2d-c2029e8dd760-logo.png"
         alt="Logo"
       />
       <Input
@@ -57,8 +57,6 @@ return (
         style={{ maxWidth: "200px" }}
       />
     </LabelArea>
-    {state.name && (
-      <Greeting>Greetings to {state.name} from Near Ukraine!</Greeting>
-    )}
+    {state.name && <Greeting>Hello from near Ukraine!</Greeting>}
   </div>
 );
