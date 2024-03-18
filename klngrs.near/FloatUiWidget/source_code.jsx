ba44635d7e19317ff834a5ad7b4459bd79a544fc
klngrs.near <klngrs.near>
2024-03-18 17:38:17 +0000

    Update FloatUiWidget by klngrs.near at 2024-03-18T17:38:17.746Z

--- klngrs.near/FloatUiWidget/source_code.jsx
+++ klngrs.near/FloatUiWidget/source_code.jsx
@@ -1,51 +1,56 @@
-const css = fetch("https://floatui.com/tailwind.css").body;
-if (!css) return "";
-const Tailwind = styled.div`${css}`;
-
-const { Tailwind } = VM.require("klngrs.near/widget/FloatUi") || {
-  Tailwind: () => <></>,
+// @ts-ignore
+const { Tailwind } = VM.require("klngrs.near/widget/Tailwind") || {
+  TailwindWrapper: () => <></>,
 };
 
+// @ts-ignore
 return (
   <Tailwind>
-    <section className="relative py-28 bg-blue-600">
-      <div className="relative z-10 max-w-screen-xl mx-auto px-4 md:px-8">
-        <div className="max-w-3xl mx-auto text-center">
-          <h3 className="text-blue-100 font-semibold pb-6">
-            What people are saying
-          </h3>
-          <figure>
-            <blockquote>
-              <p className="text-white text-xl font-semibold sm:text-2xl">
-                “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                et est hendrerit, porta nunc vitae, gravida justo. Nunc
-                fermentum magna lorem, euismod volutpat arcu volutpat et.“
-              </p>
-            </blockquote>
-            <div className="mt-6">
-              <img
-                src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg"
-                className="w-16 h-16 mx-auto rounded-full"
-              />
-              <div className="mt-3">
-                <span className="block text-white font-semibold">
-                  Martin escobar
-                </span>
-                <span className="block text-blue-100 text-sm mt-0.5">
-                  Founder of meta
-                </span>
-              </div>
-            </div>
-          </figure>
+    <div className="py-14">
+      <div className="max-w-screen-xl mx-auto md:px-8">
+        <div className="items-center gap-x-12 sm:px-4 md:px-0 lg:flex">
+          <div className="flex-1 sm:hidden lg:block">
+            <img
+              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              className="md:max-w-lg sm:rounded-lg"
+              alt=""
+            />
+          </div>
+          <div className="max-w-xl px-4 space-y-3 mt-6 sm:px-0 md:mt-0 lg:max-w-2xl">
+            <h3 className="text-green-600 font-semibold">
+              Professional services
+            </h3>
+            <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+              Build your SaaS solution with help from our experts
+            </p>
+            <p className="mt-3 text-gray-600">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum, sed ut perspiciatis unde omnis iste natus
+              error sit voluptatem accusantium doloremque laudantium
+            </p>
+            <a
+              href="javascript:void(0)"
+              className="inline-flex gap-x-1 items-center text-green-600 hover:text-green-500 duration-150 font-medium"
+            >
+              Read full post
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
-      <div
-        className="absolute top-0 w-full h-full"
-        style={{
-          background:
-            "linear-gradient(268.24deg, rgba(59, 130, 246, 0.76) 50%, rgba(59, 130, 246, 0.545528) 80.61%, rgba(55, 48, 163, 0) 117.35%)",
-        }}
-      ></div>
-    </section>
+    </div>
   </Tailwind>
 );
