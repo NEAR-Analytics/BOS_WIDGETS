00c01c34bf018601772afca1a68ffc063e60f8f2
81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6 <81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6>
2024-03-19 20:06:50 +0000

    Update NearUkraineStakeWidget by 81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6 at 2024-03-19T20:06:50.140Z

--- 81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6/NearUkraineStakeWidget/source_code.jsx
+++ 81423b2d389fbf3f73ba45e3b62dcc1fdd37cea2d0d383806fdc687822fdb8a6/NearUkraineStakeWidget/source_code.jsx
@@ -1,16 +1,25 @@
 initState({
   amount: "1",
-  poolId: "nearuaguild.poolv1.near",
+  vaidator: "nearuaguild.poolv1.near",
 });
+const accountId = props.wallet_id || context.accountId;
+const decimals = props.decimal_places ?? 1;
 
 const onStakeClick = () => {
-  const teraGas = 1000000000000;
-  const gas = 300 * teraGas;
+  const gas = 300 * 10e11; // 300 TGas
   // TODO: doesn't support floats right now due to limitation of JS integers
   const deposit = parseInt(state.amount) + "000000000000000000000000";
   console.log("gas: ", gas, "deposit: ", deposit);
-  Near.call(state.poolId, "deposit_and_stake", {}, gas, deposit);
+  Near.call(state.vaidator, "deposit_and_stake", {}, gas, deposit);
 };
+
+const onAmountInputChange = ({ target }) => {
+  State.update({amount: target.value});
+}
+
+const onPoolInputChange = ({ target }) => {
+  State.update({vaidator: target.value});
+}
 
 const Button = styled.button`
   /* Adapt the colors based on primary prop */
@@ -23,24 +32,30 @@ const Button = styled.button`
   border-radius: 3px;
 `;
 
-const accountId = props.wallet_id || context.accountId;
-const totalStakedBalance =
-  Near.view("nearuaguild.poolv1.near", "get_total_staked_balance", {}) / 1e24;
+const totalStakedBalance = (
+  Near.view(state.vaidator, "get_total_staked_balance", {}) / 1e24
+).toFixed(decimals);
 
-const yourStakedBalance =
-  Near.view("nearuaguild.poolv1.near", "get_account_staked_balance", {
+const yourStakedBalance = (
+    Near.view(state.vaidator, "get_account_staked_balance", {
     account_id: accountId,
-  }) / 1e24;
+  }) / 1e24
+).toFixed(decimals);
 
-const res = fetch(
-  `https://api.nearblocks.io/v1/account/${props.wallet_id || context.accountId}`
+const res = fetch(`https://api.nearblocks.io/v1/account/${accountId}`);
+const yourAccountBalance = (res.body.account[0].amount / 1e24).toFixed(
+  decimals
 );
-const yourAccountBalance = res.body.account[0].amount / 1e24;
+
 return (
   <div>
     <h1>Stake NEAR</h1>
-    <p>Pool: <input value={state.poolId}/></p>
-    <p>Amount: <input type="number" value={state.amount}/></p>
+    <p>
+      Validator: <input value={state.vaidator} onChange={onPoolInputChange}/>
+    </p>
+    <p>
+      Amount: <input type="number" value={state.amount} onChange={onAmountInputChange} />
+    </p>
     <Button onClick={onStakeClick}>Stake</Button>
     <p>Total staked balance in validator is: {totalStakedBalance} Near</p>
     <p>Your staked balance in validator is: {yourStakedBalance} Near</p>
