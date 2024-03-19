d74c5487d27a58ce8968cedaf47f15df33413083
ndcdev.near <ndcdev.near>
2024-03-19 17:24:17 +0000

    Update daos.Components.Post by ndcdev.near at 2024-03-19T17:24:17.688Z

--- ndcdev.near/daos.Components.Post/source_code.jsx
+++ ndcdev.near/daos.Components.Post/source_code.jsx
@@ -99,6 +99,9 @@ const Status = styled.div`
 const Comments = styled.div`
   border-top: 1px solid #efefef;
   padding-top: 1rem;
+  @media screen and (max-width: 786px) {
+    overflow: auto;
+   }
 `;
 
 const StatusSelect = styled.div`
@@ -280,7 +283,7 @@ const CardItem = ({ item, index }) => (
               >
                 {snapshot.map((history) => (
                   <option value={history.timestamp}>
-                    {new Date(history.timestamp / 1000000).toLocaleDateString()}
+                    {new Date(history.timestamp / 1000000).toLocaleString()}
                   </option>
                 ))}
               </select>
@@ -293,7 +296,7 @@ const CardItem = ({ item, index }) => (
             <span style={{ width: "12rem" }}>Updated at:</span>
             <span>
               {item.timestamp
-                ? new Date(item.timestamp / 1000000).toLocaleDateString()
+                ? new Date(item.timestamp / 1000000).toLocaleString()
                 : new Date().toLocaleDateString()}
             </span>
           </div>
