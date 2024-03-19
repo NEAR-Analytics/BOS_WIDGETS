9c7a5933dae06028e4122d23aa84930e6d1d9823
thomasguntenaar.near <thomasguntenaar.near>
2024-03-19 01:36:33 +0000

    Update devhub.feature.proposal-search.by-category by thomasguntenaar.near at 2024-03-19T01:36:33.263Z

---
+++ thomasguntenaar.near/devhub.feature.proposal-search.by-category/source_code.jsx
@@ -0,0 +1,51 @@
+const options = [
+  {
+    label: "DevDAO Operations",
+    value: "DevDAO Operations",
+  },
+  {
+    label: "DevDAO Platform",
+    value: "DevDAO Platform",
+  },
+  {
+    label: "Events & Hackathons",
+    value: "Events & Hackathons",
+  },
+  {
+    label: "Engagement & Awareness",
+    value: "Engagement & Awareness",
+  },
+  {
+    label: "Decentralized DevRel",
+    value: "Decentralized DevRel",
+  },
+  {
+    label: "Universities & Bootcamps",
+    value: "Universities & Bootcamps",
+  },
+  {
+    label: "Tooling & Infrastructure",
+    value: "Tooling & Infrastructure",
+  },
+  {
+    label: "Other",
+    value: "Other",
+  },
+];
+
+const setSelected = props.onStateChange ?? (() => {});
+
+return (
+  <div>
+    <Widget
+      src="thomasguntenaar.near/widget/devhub.components.molecule.DropDown"
+      props={{
+        options: options,
+        label: "Category",
+        onUpdate: (v) => {
+          setSelected(v);
+        },
+      }}
+    />
+  </div>
+);
