4e6a4a2a9e3fd9e34387a44d9ad6d6569c739c17
thomasguntenaar.near <thomasguntenaar.near>
2024-03-19 01:36:33 +0000

    Update devhub.feature.proposal-search.by-sort by thomasguntenaar.near at 2024-03-19T01:36:33.263Z

---
+++ thomasguntenaar.near/devhub.feature.proposal-search.by-sort/source_code.jsx
@@ -0,0 +1,23 @@
+const options = [
+  { label: "Most recent", value: "" }, // proposal_id desc
+  { label: "Most viewed", value: "" }, // views desc
+  { label: "Most commented", value: "" }, // comments desc
+  { label: "Unanswered", value: "" }, // where comments = 0
+];
+
+const setSelected = props.onStateChange ?? (() => {});
+
+return (
+  <div>
+    <Widget
+      src="thomasguntenaar.near/widget/devhub.components.molecule.DropDown"
+      props={{
+        options: options,
+        label: "Sort",
+        onUpdate: (v) => {
+          setSelected(v);
+        },
+      }}
+    />
+  </div>
+);
