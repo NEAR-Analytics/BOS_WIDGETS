a30d3752b1b175bea54815e898eed5072bb81390
megha19.near <megha19.near>
2024-03-20 18:40:36 +0000

    Update devhub.page.addon by megha19.near at 2024-03-20T18:40:36.648Z

--- megha19.near/devhub.page.addon/source_code.jsx
+++ megha19.near/devhub.page.addon/source_code.jsx
@@ -60,7 +60,7 @@ const CenteredMessage = styled.div`
 const { addon, permissions, handle } = props;
 
 const { getAllAddons, setCommunityAddon } = VM.require(
-  "megha19.near/widget/core.adapter.devhub-contract"
+  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
 );
 
 if (!getAllAddons || !setCommunityAddon) {
