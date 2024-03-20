0fbd3131c2a5e23cf2c64246e520b52f58d3f1cb
fundowakin.near <fundowakin.near>
2024-03-20 21:33:18 +0000

    Update HelloIndia by fundowakin.near at 2024-03-20T21:33:18.972Z

--- fundowakin.near/HelloIndia/source_code.jsx
+++ fundowakin.near/HelloIndia/source_code.jsx
@@ -1,49 +1,63 @@
-const text = props.text ?? "Hello India!";
-const fontFamily = props.fontFamily ?? "Arial, sans-serif";
-const fontSize = props.fontSize ?? "22px";
-const textColor = props.textColor ?? "orange";
-const backgroundColor = props.backgroundColor ?? "green";
-const height = props.height ?? "60px";
-const width = props.width ?? "100%";
-
-const code = `
+const greetingMessage = props.text || "Hello India!";
+const typeFace = props.fontFamily || "Arial, sans-serif";
+const letterSize = props.fontSize || "22px";
+const fontColor = props.textColor || "orange";
+const backdropColor = props.backgroundColor || "green";
+const boxHeight = props.height || "60px";
+const boxWidth = props.width || "100%";
+const logoUrl =
+  props.imageUrl ||
+  "https://zealy-webapp-images-prod.s3.eu-west-1.amazonaws.com/public/5d8a56da-0df6-4e25-ba2d-c2029e8dd760-logo.png";
+const displayCode = `
     <style>
-      body {
-        margin: 0
+      html, body {
+        padding: 0;
+        margin: 0;
       }
 
-      .marquee {
+      .banner-text {
         white-space: nowrap;
         overflow: hidden;
         position: relative;
-        color: ${textColor};
-        background-color: ${backgroundColor};
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        height: ${height};
-        width: ${width};
+        background-color: ${backdropColor};
+        color: ${fontColor};
+        font-family: ${typeFace};
+        font-size: ${letterSize};
+        line-height: ${boxHeight};
+        height: ${boxHeight};
+        width: ${boxWidth};
         display: flex;
         align-items: center;
-        justify-content: start;
-      }
-
-      .marquee span {
-        display: inline-block;
+        justify-content: flex-start;
         padding-left: 100%;
-        animation: marquee 15s linear infinite;
+        animation: banner-slide 15s linear infinite;
       }
 
-      @keyframes marquee {
-        0% { transform: translate(0, 0); }
-        100% { transform: translate(-100%, 0); }
+      @keyframes banner-slide {
+        from { transform: translateX(0%); }
+        to { transform: translateX(-100%); }
+      }
+
+      .logo {
+        width: ${boxWidth};
+        display: block; /* Ensures the logo is centered if it's smaller than the container */
+        margin: auto; /* Adds automatic margins to center the logo horizontally */
       }
     </style>
 
-    <div class="marquee">
-      <span>${text}</span>
+    <div class="banner-text">
+      ${greetingMessage}
     </div>
+    <img src="${logoUrl}" alt="Logo" class="logo"/>
   `;
 
 return (
-  <iframe className="w-100" srcDoc={code} style={{ height, backgroundColor }} />
+  <iframe
+    className="full-width"
+    srcDoc={displayCode}
+    style={{
+      height: `calc(${boxHeight} + 200px)`, // Adjust the 200px as needed for your logo's height
+      backgroundColor: backdropColor,
+    }}
+  />
 );
