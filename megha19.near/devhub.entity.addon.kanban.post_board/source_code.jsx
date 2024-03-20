9d3bbeeb47c6dc39e935062731b154ff74d84014
megha19.near <megha19.near>
2024-03-20 18:41:33 +0000

    Update devhub.entity.addon.kanban.post_board by megha19.near at 2024-03-20T18:41:33.654Z

--- megha19.near/devhub.entity.addon.kanban.post_board/source_code.jsx
+++ megha19.near/devhub.entity.addon.kanban.post_board/source_code.jsx
@@ -1,8 +1,8 @@
 const { getPostsByLabel } = VM.require(
-  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
+  "megha19.near/widget/core.adapter.devhub-contract"
 );
 const { getPost } = VM.require(
-  "${REPL_DEVHUB}/widget/core.adapter.devhub-contract"
+  "megha19.near/widget/core.adapter.devhub-contract"
 );
 getPost || (getPost = () => {});
 getPostsByLabel || (getPostsByLabel = () => {});
@@ -148,7 +148,7 @@ const KanbanPostBoard = ({ metadata, payload }) => {
               {data.length === column.postIds.length &&
                 data.map((postData) => (
                   <Widget
-                    src={`${REPL_DEVHUB}/widget/devhub.entity.addon.${metadata.ticket.type}`}
+                    src={`megha19.near/widget/devhub.entity.addon.${metadata.ticket.type}`}
                     props={{
                       metadata: { id: postData.postId, ...metadata.ticket },
 
