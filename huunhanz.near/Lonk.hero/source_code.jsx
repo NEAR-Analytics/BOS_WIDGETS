60a97fea12738bd34de07c818cc71e7be32f1d21
huunhanz.near <huunhanz.near>
2024-03-20 20:27:35 +0000

    Update Lonk.hero by huunhanz.near at 2024-03-20T20:27:35.117Z

---
+++ huunhanz.near/Lonk.hero/source_code.jsx
@@ -0,0 +1,116 @@
+return (
+  <>
+    <div class="intro font-md-bigger py-4 py-lg-5 dragon-background text-uppercase text-center">
+      <div class="container py-3">
+        <div class="row justify-content-around">
+          <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center">
+            <img src={props.image4} alt="Green Dragon" width="350px" />
+          </div>
+          <div class="col-12 col-lg-4">
+            <img src={props.image5} width="200" />
+            <p class="mt-3 mp-4">WE LONKING, NOT SHORTING</p>
+            <p class="mp-4">LONKING $NEAR BACK TO $20.24 IN 2024</p>
+            <p class="mp-4">Get some $LONK</p>
+            <div class="d-flex justify-content-center">
+              <div class="mx-2">
+                <a
+                  href="https://app.ref.finance/#near|token.lonkingnearbackto2024.near"
+                  class="ref button rounded-5 text-decoration-none align-items-center justify-content-center d-flex"
+                >
+                  <img src={props.image6} width="186" />
+                </a>
+              </div>
+              <div class="mx-2">
+                <a
+                  href="https://app.ref.finance/#near|token.lonkingnearbackto2024.near"
+                  class="ref button rounded-5 text-decoration-none align-items-center justify-content-center d-flex"
+                >
+                  <img src={props.image7} width="100" />
+                </a>
+              </div>
+            </div>
+            <div class="mt-5">
+              <a
+                href="http://t.me/LonkonNEAR"
+                target="_blank"
+                class="d-inline-block px-2"
+              >
+                <img
+                  src={props.image8}
+                  width="75"
+                  class="d-inline-block px-2"
+                />
+              </a>
+              <a
+                href="http://t.me/LonkonNEAR"
+                target="_blank"
+                class="d-inline-block px-2"
+              >
+                <img
+                  src={props.image9}
+                  width="50"
+                  class="d-inline-block px-2"
+                />
+              </a>
+              <a
+                href="http://t.me/LonkonNEAR"
+                target="_blank"
+                class="d-inline-block px-2"
+              >
+                <img
+                  src={props.image10}
+                  width="65"
+                  class="d-inline-block px-2"
+                />
+              </a>
+              <a
+                href="http://t.me/LonkonNEAR"
+                target="_blank"
+                class="d-inline-block px-2"
+              >
+                <img
+                  src={props.image11}
+                  width="75"
+                  class="d-inline-block px-2"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="green-bg font-md-bigger py-4 py-lg-5" id="about">
+      <div class="container">
+        <div class="row flex-column-reverse flex-md-row align-items-center">
+          <div class="col-12 col-md-8 col-lg-7">
+            <p>
+              LONK READ AS “LONG”
+              <br />
+              LONK IS LONG
+              <br />
+              LONK IS 龍 <br />
+              LONK IS DRAGON (insert Illia)
+              <br />
+              LONK IS FREN WITH BONK
+              <br />
+              LONK IS LOVE
+              <br />
+              LONK IS NOT YOUR AVERAGE MEME COIN
+            </p>
+            <p class="text-uppercase">
+              Born from collective frenship, firmly grounded in the realms of
+              memetics and humor
+            </p>
+            <p class="text-uppercase mb-0">
+              YOUR NEAR journey is incomplete without LONK.
+            </p>
+          </div>
+          <div class="col-12 col-md-4 col-lg-5">
+            <h2>about</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+);
