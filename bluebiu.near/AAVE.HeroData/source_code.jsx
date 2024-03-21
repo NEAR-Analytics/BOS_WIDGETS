86b074920e9cef29d246482d6af32d3bf1a0bd29
bluebiu.near <bluebiu.near>
2024-03-21 17:42:33 +0000

    Update AAVE.HeroData by bluebiu.near at 2024-03-21T17:42:33.408Z

--- bluebiu.near/AAVE.HeroData/source_code.jsx
+++ bluebiu.near/AAVE.HeroData/source_code.jsx
@@ -1,4 +1,4 @@
-const { netWorth, netApy, healthFactor, showHealthFactor } = props;
+const { netWorth, netApy, healthFactor, config, rewardsAmount } = props;
 
 if (!netWorth || !netApy || !healthFactor) {
   return <div />;
@@ -30,26 +30,37 @@ const KVData = styled.div`
   }
 `;
 
-const heroData = [
-  {
-    name: "Net Worth",
-    value: netWorth,
-  },
-  {
-    name: "Net APY",
-    value: netApy,
-  },
-  showHealthFactor
-    ? {
-        name: "Health Factor",
-        value: healthFactor,
-      }
-    : undefined,
-].filter((element) => !!element);
+const { heroData } = config;
+const heroDataMap = {
+  "Net Worth": netWorth,
+  "Net APY": netApy,
+  "Health Factor": healthFactor,
+  "Available rewards": Number(rewardsAmount || 0).toFixed(2),
+};
+const heroDataTitle = heroData.map((item) => ({
+  name: item,
+  value: heroDataMap[item],
+}));
+// const heroDataTitle = [
+//   {
+//     name: "Net Worth",
+//     value: netWorth,
+//   },
+//   {
+//     name: "Net APY",
+//     value: netApy,
+//   },
+//   showHealthFactor
+//     ? {
+//         name: "Health Factor",
+//         value: healthFactor,
+//       }
+//     : undefined,
+// ].filter((element) => !!element);
 
 return (
   <HeroDataContainer>
-    {heroData.map((row) => (
+    {heroDataTitle.map((row) => (
       <KVData key={row.name}>
         <div className="key">{row.name}</div>
         <div
