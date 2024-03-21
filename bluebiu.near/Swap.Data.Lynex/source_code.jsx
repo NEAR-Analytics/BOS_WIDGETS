6e3414268b842a5ab2070314f3443902029a9aac
bluebiu.near <bluebiu.near>
2024-03-21 02:27:32 +0000

    Update Swap.Data.Lynex by bluebiu.near at 2024-03-21T02:27:32.924Z

--- bluebiu.near/Swap.Data.Lynex/source_code.jsx
+++ bluebiu.near/Swap.Data.Lynex/source_code.jsx
@@ -208,7 +208,7 @@ useEffect(() => {
       value: inputCurrency.isNative ? amount : "0",
     };
     const _amountOut = Big(result.amountOut)
-      .mul(1 - (slippage || 0.05))
+      .mul(1 - (slippage || 0.005))
       .toFixed(0);
 
     const inputs = [
