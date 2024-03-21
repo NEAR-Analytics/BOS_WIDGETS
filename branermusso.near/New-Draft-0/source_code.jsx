71f54f0ce8ae56161897854ee9459c6ed31d46da
branermusso.near <branermusso.near>
2024-03-21 23:07:53 +0000

    Update New-Draft-0 by branermusso.near at 2024-03-21T23:07:53.371Z

--- branermusso.near/New-Draft-0/source_code.jsx
+++ branermusso.near/New-Draft-0/source_code.jsx
@@ -1,1 +1,12 @@
-return <div>Hello World</div>;
+const name = props.name || "Branermus";
+const [count, setCount] = useState(1);
+
+return (
+  <div>
+    <p>
+      {" "}
+      {count} cheers for {name}!{" "}
+    </p>
+    <button onClick={() => setCount(count + 100)}>Cheers!</button>
+  </div>
+);
