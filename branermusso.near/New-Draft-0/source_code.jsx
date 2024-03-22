474cc29d306c4d7beda14221eeb7be7e6a17447c
branermusso.near <branermusso.near>
2024-03-22 04:05:34 +0000

    Update New-Draft-0 by branermusso.near at 2024-03-22T04:05:34.463Z

--- branermusso.near/New-Draft-0/source_code.jsx
+++ branermusso.near/New-Draft-0/source_code.jsx
@@ -1,12 +1,6 @@
-const name = props.name || "Branermus";
-const [count, setCount] = useState(1);
-
 return (
-  <div>
-    <p>
-      {" "}
-      {count} cheers for {name}!{" "}
-    </p>
-    <button onClick={() => setCount(count + 100)}>Cheers!</button>
-  </div>
+  <Widget
+    src="nearukraineguild.near/widget/MysteryBox.Components.BackgroundStars"
+    props={{}}
+  />
 );
