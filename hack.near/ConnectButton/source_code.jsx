98d768aed4b02b74c28bb173a9a1c848383d97cb
hack.near <hack.near>
2024-03-22 22:34:49 +0000

    Update ConnectButton by hack.near at 2024-03-22T22:34:49.396Z

--- hack.near/ConnectButton/source_code.jsx
+++ hack.near/ConnectButton/source_code.jsx
@@ -25,16 +25,31 @@ const type = connected ? "undo" : graphId;
 
 const data = {
   graph: { [graphId]: { [props.accountId]: connected ? null : "" } },
+  index: {
+    graph: JSON.stringify({
+      key: `${graphId}`,
+      value: {
+        type,
+        accountId: props.accountId,
+      },
+    }),
+    notify: JSON.stringify({
+      key: props.accountId,
+      value: {
+        type,
+      },
+    }),
+  },
 };
 
 return (
   <CommitButton
     disabled={loading}
     className={`btn btn-sm ${
-      loading || connected ? "btn-danger" : "btn-primary"
+      loading || connected ? "btn-secondary" : "btn-outline-dark"
     }`}
     data={data}
   >
-    {loading ? "" : connected ? "x" : "+"}
+    {loading ? "" : connected ? "Undo" : "Add"}
   </CommitButton>
 );
