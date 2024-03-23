dc687bd029c6ea99452a97a587f7beb26f3f81e1
abdullahi3000.near <abdullahi3000.near>
2024-03-23 06:12:32 +0000

    Update harmonic.music.player by abdullahi3000.near at 2024-03-23T06:12:32.770Z

--- abdullahi3000.near/harmonic.music.player/source_code.jsx
+++ abdullahi3000.near/harmonic.music.player/source_code.jsx
@@ -22,7 +22,7 @@ const paragraphStyles = {
 return (
   <>
     <div style={containerStyles}>
-      <div className="mt-5" style={innerContainerStyles}>
+      <div className="mt-5 mb-5" style={innerContainerStyles}>
         <div style={{ flex: 1, padding: "2rem" }}>
           <p style={{ fontSize: "1.75rem", fontWeight: "bold" }}>
             Decentralised Music Streaming
@@ -37,31 +37,6 @@ return (
         </div>
         <div style={{ flex: 1, color: "BLACK" }}>
           <Widget src="harmonic1.near/widget/MusicPlayer-Harmonic" />
-        </div>
-      </div>
-    </div>
-    <div style={containerStyles}>
-      <div className="mb-5" style={innerContainerStyles}>
-        <div style={{ flex: 1, padding: "2rem" }}>
-          <p
-            style={{
-              fontSize: "1.75rem",
-              fontWeight: "bold",
-              // marginTop: "4rem",
-            }}
-          >
-            Upload Your First Song
-          </p>
-          <p style={paragraphStyles}>
-            We're excited to have you onboard, whether you're a budding artist
-            seeking to make your mark or a seasoned pro looking to diversify
-            your distribution platforms. Unleashing your musical creativity and
-            sharing your work with the world has never been more empowering or
-            accessible.
-          </p>
-        </div>
-        <div style={{ flex: 1, color: "BLACK" }}>
-          <Widget src="harmonic1.near/widget/MusicUpload" />
         </div>
       </div>
     </div>
