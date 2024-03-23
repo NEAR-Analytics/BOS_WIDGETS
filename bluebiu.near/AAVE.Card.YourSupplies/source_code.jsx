cc84e5bd967da37eea9b6100efe47c48a5d9d08e
bluebiu.near <bluebiu.near>
2024-03-23 03:54:25 +0000

    Update AAVE.Card.YourSupplies by bluebiu.near at 2024-03-23T03:54:25.695Z

--- bluebiu.near/AAVE.Card.YourSupplies/source_code.jsx
+++ bluebiu.near/AAVE.Card.YourSupplies/source_code.jsx
@@ -78,7 +78,7 @@ return (
                                   <img
                                     width={64}
                                     height={64}
-                                    src={`https://app.aave.com/icons/tokens/${row.symbol.toLowerCase()}.svg`}
+                                    src={row?.icon}
                                   />,
                                   <div>
                                     <div className="token-title">
@@ -144,11 +144,7 @@ return (
                         src={`${config.ownerId}/widget/AAVE.Card.TokenWrapper`}
                         props={{
                           children: [
-                            <img
-                              width={64}
-                              height={64}
-                              src={`https://app.aave.com/icons/tokens/${row.symbol.toLowerCase()}.svg`}
-                            />,
+                            <img width={64} height={64} src={row?.icon} />,
                             <div>
                               <div className="token-title">{row.symbol}</div>
                               <div className="token-chain">{row.name}</div>
