b715284a0f1cb230839def7af52a0b54b130d79f
huunhanz.near <huunhanz.near>
2024-03-24 16:00:13 +0000

    Update Lonk.hero by huunhanz.near at 2024-03-24T16:00:13.703Z

--- huunhanz.near/Lonk.hero/source_code.jsx
+++ huunhanz.near/Lonk.hero/source_code.jsx
@@ -1,14 +1,33 @@
 const [select, setSelect] = useState("");
+
+const config = {
+  image4:
+    "https://bafybeib6kjh37qmodapxwjq3ukckhabckasdoamjas7ehigzqbjazu4kyq.ipfs.nftstorage.link/",
+  image5:
+    "https://bafkreieghanoutojyhkfxfrfqqjgtn76bnbi25kxwi5byankqz3on2elnq.ipfs.nftstorage.link/",
+  image6:
+    "https://bafkreiejkfmncbzuni5lsrlpplmkqisasvjzgl4oxu3nnz7nmmjwwd56ii.ipfs.nftstorage.link/",
+  image7:
+    "https://bafkreiag6wx5sg3o2e7nuvbdnpoonymm6dge4woi77o3oroxsdztggr7n4.ipfs.nftstorage.link/",
+  image8:
+    "https://bafkreielhfr3ybofrjwtbxg7b7qx6suw7noot3wfkpe3s54gbgg4cao5mq.ipfs.nftstorage.link/",
+  image9:
+    "https://bafkreienvdqrium4lwlfcoz3tctc6ez27hmsiiwv5bsiwjwo2xfolkv4q4.ipfs.nftstorage.link/",
+  image10:
+    "https://bafkreifjthkkuez3glfevsmjw7orgj4fuxofrtoh3zb6k2bqinloixqml4.ipfs.nftstorage.link/",
+  image11:
+    "https://bafkreigkwuy4k4txpn4jhivrwdagvvooiivbi3yywamv6krxv77bqitrmm.ipfs.nftstorage.link/",
+};
 return (
   <>
     <div class="intro font-md-bigger py-4 py-lg-5 dragon-background text-uppercase text-center">
       <div class="container py-3">
         <div class="row justify-content-around">
           <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center">
-            <img src={props.image4} alt="Green Dragon" width="350px" />
+            <img src={config.image4} alt="Green Dragon" width="350px" />
           </div>
           <div class="col-12 col-lg-4">
-            <img src={props.image5} width="200" />
+            <img src={config.image5} width="200" />
             <p class="mt-3 mp-4">WE LONKING, NOT SHORTING</p>
             <p class="mp-4">LONKING $NEAR BACK TO $20.24 IN 2024</p>
             <p class="mp-4">Get some $LONK</p>
@@ -21,7 +40,7 @@ return (
                   data-bs-target="#exampleModal"
                   class="ref button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
                 >
-                  <img src={props.image6} width="186" />
+                  <img src={config.image6} width="186" />
                 </button>{" "}
               </div>
               <div class="mx-2">
@@ -30,7 +49,7 @@ return (
                   target="_blank"
                   class="ref button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
                 >
-                  <img src={props.image7} width="90" />
+                  <img src={config.image7} width="90" />
                 </a>{" "}
               </div>
             </div>
@@ -41,7 +60,7 @@ return (
                 class="d-inline-block px-2"
               >
                 <img
-                  src={props.image8}
+                  src={config.image8}
                   width="75"
                   class="d-inline-block px-2"
                 />
@@ -52,7 +71,7 @@ return (
                 class="d-inline-block px-2"
               >
                 <img
-                  src={props.image9}
+                  src={config.image9}
                   width="50"
                   class="d-inline-block px-2"
                 />
@@ -63,7 +82,7 @@ return (
                 class="d-inline-block px-2"
               >
                 <img
-                  src={props.image10}
+                  src={config.image10}
                   width="65"
                   class="d-inline-block px-2"
                 />
@@ -74,7 +93,7 @@ return (
                 class="d-inline-block px-2"
               >
                 <img
-                  src={props.image11}
+                  src={config.image11}
                   width="75"
                   class="d-inline-block px-2"
                 />
