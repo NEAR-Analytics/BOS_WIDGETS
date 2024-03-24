6cf106d281fc84c2b43f6bcba29685006fb14ec9
bluebiu.near <bluebiu.near>
2024-03-24 19:35:13 +0000

    Update Lending.RewardClaim.Valas by bluebiu.near at 2024-03-24T19:35:13.737Z

--- bluebiu.near/Lending.RewardClaim.Valas/source_code.jsx
+++ bluebiu.near/Lending.RewardClaim.Valas/source_code.jsx
@@ -1,37 +1,37 @@
 const CLAIM_ABI = [
   {
-    constant: false,
     inputs: [
-      {
-        internalType: "uint8",
-        name: "rewardType",
-        type: "uint8",
-      },
-      {
-        internalType: "address payable",
-        name: "holder",
-        type: "address",
-      },
+      { internalType: "address", name: "_user", type: "address" },
+      { internalType: "address[]", name: "_tokens", type: "address[]" },
     ],
-    name: "claimReward",
+    name: "claim",
     outputs: [],
-    payable: false,
     stateMutability: "nonpayable",
     type: "function",
   },
 ];
 const account = Ethers.send("eth_requestAccounts", [])[0];
-const { loading, market, dapp, record, onSuccess, onError } = props;
+const { loading, market, dapp, record, supplies, onSuccess, onError } = props;
 
-if (!loading || !dapp.unitrollerAddress) return "";
+if (!loading || !dapp.rewardAddress) return "";
+
+const { markets } = dapp;
+const supplyAddrs = supplies.map((item) => item.address);
+const nativeReward = Object.values(markets).find(
+  (item) => item.symbol === "valBNB"
+);
+const vTokenAddrs = Object.values(markets)
+  .filter((item) => supplyAddrs.includes(item.address))
+  .map((item) => item.variableDebtTokenAddress);
+const _tokens = [nativeReward.address, ...vTokenAddrs];
 
 const CollateralContract = new ethers.Contract(
-  dapp.unitrollerAddress,
+  dapp.rewardAddress,
   CLAIM_ABI,
   Ethers.provider().getSigner()
 );
 
-CollateralContract.claimReward(record.symbol === "QI" ? 0 : 1, account)
+CollateralContract.claim(account, _tokens)
   .then((tx) => {
     tx.wait().then((res) => {
       onSuccess(res);
