fd4e751f64c8f9b0a4eece17c661cbf873d760fe
bluebiu.near <bluebiu.near>
2024-03-24 19:35:13 +0000

    Update AAVE.Card.YourSupplies by bluebiu.near at 2024-03-24T19:35:13.737Z

--- bluebiu.near/AAVE.Card.YourSupplies/source_code.jsx
+++ bluebiu.near/AAVE.Card.YourSupplies/source_code.jsx
@@ -37,137 +37,47 @@ const WithdrawButton = ({ data }) => {
 
 return (
   <>
-    <Widget
-      src={`${config.ownerId}/widget/AAVE.Card.CardsView`}
-      props={{
-        config,
-        style: {
-          marginTop: "16px",
-        },
-        title: "Your supplies",
-        body:
-          !yourSupplies || yourSupplies.length === 0 ? (
-            <Widget
-              src={`${config.ownerId}/widget/AAVE.Card.CardEmpty`}
-              props={{
-                config,
-                children: "Nothing supplied yet",
-              }}
-            />
-          ) : (
-            <>
-              {/* mobileView */}
-              {yourSupplies.map((row) => (
+    {!yourSupplies || yourSupplies.length === 0 ? (
+      <Widget
+        src={`${config.ownerId}/widget/AAVE.Card.CardEmpty`}
+        props={{
+          config,
+          children: "Nothing supplied yet",
+        }}
+      />
+    ) : (
+      <>
+        <Widget
+          src={`${config.ownerId}/widget/AAVE.Card.CardsTable`}
+          props={{
+            config,
+            headers: ["Asset", "Balance", "APY", ""],
+            data: yourSupplies.map((row) => {
+              return [
                 <Widget
-                  src={`${config.ownerId}/widget/AAVE.Card.CardContainer`}
+                  src={`${config.ownerId}/widget/AAVE.Card.TokenWrapper`}
                   props={{
                     children: [
-                      <Widget
-                        src={`${config.ownerId}/widget/AAVE.Card.Divider`}
-                        props={{ config }}
-                      />,
-                      <Widget
-                        src={`${config.ownerId}/widget/AAVE.Card.CardsBody`}
-                        props={{
-                          config,
-                          children: [
-                            <Widget
-                              src={`${config.ownerId}/widget/AAVE.Card.TokenWrapper`}
-                              props={{
-                                children: [
-                                  <img
-                                    width={64}
-                                    height={64}
-                                    src={row?.icon}
-                                  />,
-                                  <div>
-                                    <div className="token-title">
-                                      {row.symbol}
-                                    </div>
-                                    <div className="token-chain">
-                                      {row.name}
-                                    </div>
-                                  </div>,
-                                ],
-                              }}
-                            />,
-                            <Widget
-                              src={`${config.ownerId}/widget/AAVE.Card.CardDataWrapper`}
-                              props={{
-                                children: [
-                                  <div className="card-data-row">
-                                    <div className="card-data-key">
-                                      Supply Balance
-                                    </div>
-                                    <div className="card-data-value">
-                                      <div>
-                                        {Number(row.underlyingBalance).toFixed(
-                                          7
-                                        )}
-                                      </div>
-                                      <div>
-                                        ${" "}
-                                        {Number(
-                                          row.underlyingBalanceUSD
-                                        ).toFixed(2)}
-                                      </div>
-                                    </div>
-                                  </div>,
-                                  <div className="card-data-row">
-                                    <div className="card-data-key">
-                                      Supply APY
-                                    </div>
-                                    <div className="card-data-value">{`${(
-                                      Number(row.supplyAPY) * 100
-                                    ).toFixed(2)} %`}</div>
-                                  </div>,
-                                ],
-                              }}
-                            />,
-                            <WithdrawButton data={row} />,
-                          ],
-                        }}
-                      />,
+                      <img width={64} height={64} src={row?.icon} />,
+                      <div>
+                        <div className="token-title">{row.symbol}</div>
+                        <div className="token-chain">{row.name}</div>
+                      </div>,
                     ],
                   }}
-                />
-              ))}
-              {/* pcView */}
-              <Widget
-                src={`${config.ownerId}/widget/AAVE.Card.CardsTable`}
-                props={{
-                  config,
-                  headers: ["Asset", "Supply Balance", "Supply APY", ""],
-                  data: yourSupplies.map((row) => {
-                    return [
-                      <Widget
-                        src={`${config.ownerId}/widget/AAVE.Card.TokenWrapper`}
-                        props={{
-                          children: [
-                            <img width={64} height={64} src={row?.icon} />,
-                            <div>
-                              <div className="token-title">{row.symbol}</div>
-                              <div className="token-chain">{row.name}</div>
-                            </div>,
-                          ],
-                        }}
-                      />,
-                      <div>
-                        <div>{Number(row.underlyingBalance).toFixed(7)}</div>
-                        <div>
-                          $ {Number(row.underlyingBalanceUSD).toFixed(2)}
-                        </div>
-                      </div>,
-                      `${(Number(row.supplyAPY) * 100).toFixed(2)} %`,
-                      <WithdrawButton data={row} />,
-                    ];
-                  }),
-                }}
-              />
-            </>
-          ),
-      }}
-    />
+                />,
+                <div>
+                  <div>{Number(row.underlyingBalance).toFixed(7)}</div>
+                  <div>$ {Number(row.underlyingBalanceUSD).toFixed(2)}</div>
+                </div>,
+                `${(Number(row.supplyAPY) * 100).toFixed(2)} %`,
+                <WithdrawButton data={row} />,
+              ];
+            }),
+          }}
+        />
+      </>
+    )}
     {showWithdrawModal && (
       <Widget
         src={`${config.ownerId}/widget/AAVE.Modal.WithdrawModal`}
