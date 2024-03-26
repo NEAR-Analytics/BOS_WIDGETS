462ca97372c2a85d7931c2b0e5d56c7fd3c29e21
abdullahi3000.near <abdullahi3000.near>
2024-03-26 10:10:11 +0000

    Update components by abdullahi3000.near at 2024-03-26T10:10:11.693Z

--- abdullahi3000.near/components/source_code.jsx
+++ abdullahi3000.near/components/source_code.jsx
@@ -1,12 +1,12 @@
 const { Button } = VM.require("buildhub.near/widget/components.Button");
 const { ProgressState } = VM.require(
-  "buildhub.near/widget/components.ProgressState",
+  "buildhub.near/widget/components.ProgressState"
 );
 const { Bullet } = VM.require("buildhub.near/widget/components.Bullet");
 const { Step } = VM.require("buildhub.near/widget/components.Step");
 const { InputField } = VM.require("buildhub.near/widget/components.InputField");
 const { UploadField } = VM.require(
-  "buildhub.near/widget/components.UploadField",
+  "buildhub.near/widget/components.UploadField"
 );
 const { TextBox } = VM.require("buildhub.near/widget/components.TextBox");
 const { TextEditor } = VM.require("buildhub.near/widget/components.TextEditor");
@@ -41,7 +41,7 @@ function Post(props) {
   return (
     <Widget
       loading={<div className="w-100" style={{ height: "200px" }} />}
-      src={"buildhub.near/widget/components.Post"}
+      src={"abdullahi3000.near/widget/components.Post"}
       props={{ ...props }}
     />
   );
