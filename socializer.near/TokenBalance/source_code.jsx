8f412bec8246fe39021fa6eab286ce91380e1ed6
socializer.near <socializer.near>
2024-03-26 06:52:07 +0000

    Update TokenBalance by socializer.near at 2024-03-26T06:52:07.134Z

--- socializer.near/TokenBalance/source_code.jsx
+++ socializer.near/TokenBalance/source_code.jsx
@@ -55,50 +55,5 @@ return (
         <p style={{ fontSize: 12, color: "red" }}>{state.error}</p>
       )}
     </div>
-    <div className="d-flex" style={{ minWidth: 150 }}>
-      <Button>
-        {"Select Token"}
-        <span style={{ height: 18 }}>
-          <svg
-            width="30"
-            height="26"
-            viewBox="0 0 30 26"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g filter="url(#filter0_d_58_4394)">
-              <rect
-                x="4"
-                y="18"
-                width="18"
-                height="22"
-                rx="8"
-                transform="rotate(-90 4 18)"
-                fill="#141522"
-                shape-rendering="crispEdges"
-              />
-              <path
-                d="M19 5.27337L18.06 4.33337L15 7.38671L11.94 4.33337L11 5.27337L15 9.27337L19 5.27337Z"
-                fill="white"
-              />
-              <path
-                d="M19 9.66668L18.06 8.72668L15 11.78L11.94 8.72669L11 9.66669L15 13.6667L19 9.66668Z"
-                fill="white"
-              />
-              <rect
-                x="4.5"
-                y="17.5"
-                width="17"
-                height="21"
-                rx="7.5"
-                transform="rotate(-90 4.5 17.5)"
-                stroke="#141522"
-                shape-rendering="crispEdges"
-              />
-            </g>
-          </svg>
-        </span>
-      </Button>
-    </div>
   </Wrapper>
 );
