4ef54a437c97e223b165af39245c9b37afd31ac7
ndcdev.near <ndcdev.near>
2024-03-27 16:13:20 +0000

    Update daos.Components.Settings.Content by ndcdev.near at 2024-03-27T16:13:20.881Z

--- ndcdev.near/daos.Components.Settings.Content/source_code.jsx
+++ ndcdev.near/daos.Components.Settings.Content/source_code.jsx
@@ -1,5 +1,10 @@
-let { content } = VM.require(`ndcdev.near/widget/daos.Config`);
-if (!content) return <Widget src="flashui.near/widget/Loading" />;
+let { content, contractName } = VM.require(
+  `ndcdev.near/widget/daos.Config`
+);
+if (!contractName || !content)
+  return <Widget src="flashui.near/widget/Loading" />;
+
+const { selectedDao } = props;
 
 const Form = styled.div`
   border-radius: 20px;
@@ -25,12 +30,118 @@ const Form = styled.div`
       text-decoration: underline;
     }
   }
+
+  @media screen and (max-width: 768px) {
+    padding: 1rem;
+  }
 `;
 
-const handleSave = () => {};
+const Contact = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  .form-element {
+    width: 100%;
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const [daoContent, setDaoContent] = useState({});
+
+useEffect(() => {
+  if (selectedDao) {
+    setDaoContent(
+      selectedDao.metadata.content
+        ? JSON.parse(selectedDao.metadata.content)
+        : {
+            info: {},
+            guidance: {},
+          }
+    );
+  }
+}, [selectedDao]);
+
+const handleSave = () => {
+  Near.call(contractName, "edit_dao", {
+    id: selectedDao.id,
+    body: {
+      title: selectedDao.title,
+      handle: selectedDao.handle,
+      dao_type: selectedDao.dao_type,
+      description: selectedDao.description,
+      logo_url: selectedDao.logo_url,
+      banner_url: selectedDao.banner_url,
+      account_id: selectedDao.account_id,
+    },
+    verticals: selectedDao.verticals,
+    metrics: selectedDao.metrics,
+    metadata: {
+      ...selectedDao.metadata,
+      content: JSON.stringify(daoContent),
+    },
+  });
+};
 
 return (
   <Form className="d-flex flex-column gap-3">
-    <h2>TBD</h2>
+    <Widget
+      src="near/widget/DIG.Tabs"
+      props={{
+        variant: "line",
+        size: "default",
+        items: [
+          ...content.info.cards.map(({ title, icon }, index) => {
+            return {
+              name: (
+                <>
+                  <img style={{ width: "20px" }} src={icon} />
+                  {title}
+                </>
+              ),
+              value: index,
+              content: (
+                <Widget
+                  src={`ndcdev.near/widget/daos.Components.Settings.InfoCard`}
+                  props={{
+                    section: "info",
+                    key: title,
+                    selectedDao,
+                    daoContent,
+                    setDaoContent,
+                  }}
+                />
+              ),
+            };
+          }),
+          ...content.guidance.cards.map(({ title, icon }, index) => {
+            return {
+              name: title,
+              value: index + content.info.cards.length,
+              content: (
+                <Widget
+                  src={`ndcdev.near/widget/daos.Components.Settings.InfoCard`}
+                  props={{
+                    section: "guidance",
+                    key: title,
+                    selectedDao,
+                    daoContent,
+                    setDaoContent,
+                  }}
+                />
+              ),
+              icon,
+            };
+          }),
+        ],
+      }}
+    />
+
+    <button className="btn btn-primary" onClick={handleSave}>
+      <i className="ph ph-pencil-simple fs-5" />
+      Save
+    </button>
   </Form>
 );
