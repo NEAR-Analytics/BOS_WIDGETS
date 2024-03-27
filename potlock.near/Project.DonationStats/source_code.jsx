bff5894c061a4e8b71f56060f3e69c36604ddd79
potlock.near <potlock.near>
2024-03-27 15:18:16 +0000

    Update Project.DonationStats by potlock.near at 2024-03-27T15:18:16.507Z

--- potlock.near/Project.DonationStats/source_code.jsx
+++ potlock.near/Project.DonationStats/source_code.jsx
@@ -5,12 +5,10 @@ const Stats = styled.div`
   justify-content: center;
   gap: 48px;
   margin-top: 40px;
-
   @media screen and (max-width: 768px) {
     gap: 16px;
   }
 `;
-
 const StatsTitle = styled.div`
   color: #292929;
   font-size: 44px;
@@ -19,23 +17,19 @@ const StatsTitle = styled.div`
   align-items: baseline;
   gap: 8px;
   font-weight: 600;
-
   @media screen and (max-width: 768px) {
     font-size: 30px;
     font-weight: 500;
     gap: 5px;
   }
 `;
-
 const StatsSubTitle = styled.div`
   color: #525252;
   font-size: 14px;
-
   @media screen and (max-width: 768px) {
     font-size: 12px;
   }
 `;
-
 const { yoctosToUsd } = VM.require("potlock.near/widget/utils") || {
   yoctosToUsd: (amount) => amount,
 };
@@ -45,17 +39,14 @@ let DonateSDK =
     getConfig: () => {},
   }));
 DonateSDK = DonateSDK({ env: props.env });
-
 const data = DonateSDK.getConfig() || {
   net_donations_amount: 0,
   total_donations_count: 0,
 };
-
 const lastDonationAmount = data.net_donations_amount
   ? yoctosToUsd(data.net_donations_amount)
   : null;
 const totalDonations = data.total_donations_count;
-
 return (
   <Stats>
     <StatsTitle>
