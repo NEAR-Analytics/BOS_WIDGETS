0735976df852b0c02af48e9f43e9dea78036e2b4
potlock.near <potlock.near>
2024-03-27 15:18:16 +0000

    Update Cart.CheckoutBreakdown by potlock.near at 2024-03-27T15:18:16.507Z

--- potlock.near/Cart.CheckoutBreakdown/source_code.jsx
+++ potlock.near/Cart.CheckoutBreakdown/source_code.jsx
@@ -1,33 +1,27 @@
-const { yoctosToNear } = VM.require("potlock.near/widget/utils") || { yoctosToNear: () => "" };
+const { yoctosToNear } = VM.require("potlock.near/widget/utils") || {
+  yoctosToNear: () => "",
+};
 const { SUPPORTED_FTS } = VM.require("potlock.near/widget/constants") || {
   SUPPORTED_FTS: {},
 };
-
 const { getCart, clearCart } = VM.require("potlock.near/widget/SDK.cart") || {
   getCart: () => {},
   clearCart: () => {},
 };
-
 const cart = getCart();
-
 const PotSDK = VM.require("potlock.near/widget/SDK.pot") || {
   asyncGetDonationsForDonor: () => {},
 };
-
 let DonateSDK =
   VM.require("potlock.near/widget/SDK.donate") ||
   (() => ({
     asyncGetDonationsForDonor: () => {},
     getContractId: () => "",
   }));
-
 DonateSDK = DonateSDK({ env: props.env });
-
 const DONATION_CONTRACT_ID = DonateSDK.getContractId();
-
 const IPFS_BASE_URL = "https://nftstorage.link/ipfs/";
 Big.PE = 100;
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -35,13 +29,11 @@ const Container = styled.div`
   margin-top: 20px;
   width: 380px;
   //   background: white;
-
   @media screen and (max-width: 768px) {
     width: 100%;
     margin-bottom: 50px;
   }
 `;
-
 const Title = styled.div`
   color: #2e2e2e;
   font-size: 24px;
@@ -49,7 +41,6 @@ const Title = styled.div`
   line-height: 32px;
   word-wrap: break-word;
 `;
-
 const CurrencyHeader = styled.div`
   display: flex;
   flex-direction: row;
@@ -59,7 +50,6 @@ const CurrencyHeader = styled.div`
   border-radius: 5px;
   background: #f0f0f0;
 `;
-
 const CurrencyHeaderText = styled.div`
   color: #7b7b7b;
   font-size: 12px;
@@ -67,7 +57,6 @@ const CurrencyHeaderText = styled.div`
   line-height: 14px;
   word-wrap: break-word;
 `;
-
 const BreakdownItemContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -75,7 +64,6 @@ const BreakdownItemContainer = styled.div`
   align-items: center;
   padding: 8px;
 `;
-
 const BreakdownItemLeft = styled.div`
   display: flex;
   flex-direction: row;
@@ -84,7 +72,6 @@ const BreakdownItemLeft = styled.div`
   width: 50%;
   gap: 8px;
 `;
-
 const BreakdownItemRight = styled.div`
   display: flex;
   flex: 1;
@@ -92,7 +79,6 @@ const BreakdownItemRight = styled.div`
   align-items: center;
   justify-content: flex-end;
 `;
-
 const BreakdownItemText = styled.div`
   color: #2e2e2e;
   font-size: 14px;
@@ -100,12 +86,10 @@ const BreakdownItemText = styled.div`
   line-height: 16px;
   word-wrap: break-word;
 `;
-
 const CurrencyIcon = styled.img`
   width: 20px;
   height: 20px;
 `;
-
 const TotalContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -114,7 +98,6 @@ const TotalContainer = styled.div`
   padding: 8px;
   border-top: 1px #7b7b7b solid;
 `;
-
 const TotalText = styled.div`
   color: #2e2e2e;
   font-size: 14px;
@@ -122,7 +105,6 @@ const TotalText = styled.div`
   line-height: 20px;
   word-wrap: break-word;
 `;
-
 const ErrorText = styled.div`
   color: #dd3345;
   font-size: 14px;
@@ -132,9 +114,7 @@ const ErrorText = styled.div`
   width: 100%;
   text-align: center;
 `;
-
 const MIN_REQUIRED_DONATION_AMOUNT_PER_PROJECT = 0.1;
-
 const [tokens, amountsByFt, totalAmount, donationTooSmall] = useMemo(() => {
   const tokens = {};
   const amountsByFt = {};
@@ -143,63 +123,68 @@ const [tokens, amountsByFt, totalAmount, donationTooSmall] = useMemo(() => {
     const ft = token.text;
     if (!amountsByFt[ft]) amountsByFt[ft] = 0;
     amountsByFt[ft] += parseFloat(amount || 0);
-    if (amountsByFt[ft] < MIN_REQUIRED_DONATION_AMOUNT_PER_PROJECT) donationTooSmall = true;
+    if (amountsByFt[ft] < MIN_REQUIRED_DONATION_AMOUNT_PER_PROJECT)
+      donationTooSmall = true;
     tokens[ft] = token;
   });
-  const totalAmount = Object.values(amountsByFt).reduce((acc, amount) => acc + amount, 0);
+  const totalAmount = Object.values(amountsByFt).reduce(
+    (acc, amount) => acc + amount,
+    0
+  );
   return [tokens, amountsByFt, totalAmount, donationTooSmall];
 }, [props]);
-
 // console.log("amountsByFt: ", amountsByFt);
 // console.log("tokens: ", tokens);
-
 const handleDonate = () => {
   const transactions = [];
   let potIdContained;
-
-  Object.entries(cart).forEach(([projectId, { token, amount, referrerId, note, potId }]) => {
-    const isFtDonation = token.text != "NEAR";
-    const amountIndivisible = Big(parseFloat(amount)).mul(
-      Big(10).pow(isFtDonation ? token.decimals : 24)
-    );
-    const args = {};
-    if (isFtDonation) {
-      args.receiver_id = DONATION_CONTRACT_ID;
-      args.amount = amountIndivisible.toString();
-      args.memo = JSON.stringify({
-        recipient_id: projectId,
-        referrer_id: referrerId || null,
-        bypass_protocol_fee: false,
-        message: note || null,
+  Object.entries(cart).forEach(
+    ([projectId, { token, amount, referrerId, note, potId }]) => {
+      const isFtDonation = token.text != "NEAR";
+      const amountIndivisible = Big(parseFloat(amount)).mul(
+        Big(10).pow(isFtDonation ? token.decimals : 24)
+      );
+      const args = {};
+      if (isFtDonation) {
+        args.receiver_id = DONATION_CONTRACT_ID;
+        args.amount = amountIndivisible.toString();
+        args.memo = JSON.stringify({
+          recipient_id: projectId,
+          referrer_id: referrerId || null,
+          bypass_protocol_fee: false,
+          message: note || null,
+        });
+      } else {
+        // pot & generic contract args
+        args.project_id = projectId;
+        args.referrer_id = referrerId;
+        args.message = note;
+        // donation contract args
+        args.recipient_id = projectId;
+        // other
+        potIdContained = potId;
+      }
+      transactions.push({
+        contractName: isFtDonation ? token.id : potId ?? DONATION_CONTRACT_ID,
+        methodName: isFtDonation ? "ft_transfer_call" : "donate",
+        args,
+        deposit: isFtDonation ? "1" : amountIndivisible.toString(),
+        gas: "300000000000000",
       });
-    } else {
-      // pot & generic contract args
-      args.project_id = projectId;
-      args.referrer_id = referrerId;
-      args.message = note;
-      // donation contract args
-      args.recipient_id = projectId;
-      // other
-      potIdContained = potId;
     }
-    transactions.push({
-      contractName: isFtDonation ? token.id : potId ?? DONATION_CONTRACT_ID,
-      methodName: isFtDonation ? "ft_transfer_call" : "donate",
-      args,
-      deposit: isFtDonation ? "1" : amountIndivisible.toString(),
-      gas: "300000000000000",
-    });
-  });
-
+  );
   // if cart contains a non-NEAR token, add storage_deposit to beginning of transactions
   // for each non-NEAR donation: 0.008 base amount for donation storage + 0.0001 NEAR per character in message
   if (Object.keys(amountsByFt).some((ft) => ft !== "NEAR")) {
-    const requiredDepositFloat = transactions.reduce((acc, { methodName, args }) => {
-      if (methodName === "donate") return acc;
-      const baseAmount = 0.008;
-      const argsAmount = (args.message.length || 0) * 0.0001;
-      return acc + baseAmount + argsAmount;
-    }, 0);
+    const requiredDepositFloat = transactions.reduce(
+      (acc, { methodName, args }) => {
+        if (methodName === "donate") return acc;
+        const baseAmount = 0.008;
+        const argsAmount = (args.message.length || 0) * 0.0001;
+        return acc + baseAmount + argsAmount;
+      },
+      0
+    );
     transactions.unshift({
       contractName: DONATION_CONTRACT_ID,
       methodName: "storage_deposit",
@@ -208,7 +193,6 @@ const handleDonate = () => {
       gas: "100000000000000",
     });
   }
-
   const now = Date.now();
   Near.call(transactions);
   // NB: we won't get here if user used a web wallet, as it will redirect to the wallet
@@ -226,7 +210,13 @@ const handleDonate = () => {
       const foundDonations = [];
       // go through donations, add to foundDonations list
       for (const donation of donations) {
-        const { recipient_id, project_id, donated_at_ms, donated_at, total_amount } = donation;
+        const {
+          recipient_id,
+          project_id,
+          donated_at_ms,
+          donated_at,
+          total_amount,
+        } = donation;
         const matchingCartItem = cart[project_id || recipient_id];
         if (matchingCartItem && (donated_at_ms > now || donated_at > now)) {
           foundDonations.push(donation);
@@ -236,14 +226,15 @@ const handleDonate = () => {
         // donations found
         // display success message & clear cart
         clearInterval(pollId);
-        props.updateSuccessfulDonationRecipientId(foundDonations[0].recipient_id);
+        props.updateSuccessfulDonationRecipientId(
+          foundDonations[0].recipient_id
+        );
         clearCart();
       }
     });
   }, pollIntervalMs);
 };
 // console.log("props", props);
-
 // console.log("supported fts: ", SUPPORTED_FTS);
 // console.log("props.cart: ", props.cart);
 // console.log("props.projectId: ", props.projectId);
@@ -284,7 +275,8 @@ return (
       props={{
         type: "primary",
         text: `Process Donation`,
-        disabled: !Object.keys(cart).length || donationTooSmall || !context.accountId,
+        disabled:
+          !Object.keys(cart).length || donationTooSmall || !context.accountId,
         onClick: handleDonate,
         style: {
           width: "100%",
@@ -293,7 +285,8 @@ return (
     />
     {donationTooSmall && (
       <ErrorText>
-        Minimum required donation per project is {MIN_REQUIRED_DONATION_AMOUNT_PER_PROJECT} N
+        Minimum required donation per project is{" "}
+        {MIN_REQUIRED_DONATION_AMOUNT_PER_PROJECT} N
       </ErrorText>
     )}
     {!context.accountId && <ErrorText>Please sign in to donate</ErrorText>}
