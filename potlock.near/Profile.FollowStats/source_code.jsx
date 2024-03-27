b84d436f2b6870f6cab61d6ed77564eb8bcba122
potlock.near <potlock.near>
2024-03-27 15:18:16 +0000

    Update Profile.FollowStats by potlock.near at 2024-03-27T15:18:16.507Z

--- potlock.near/Profile.FollowStats/source_code.jsx
+++ potlock.near/Profile.FollowStats/source_code.jsx
@@ -1,29 +1,25 @@
 const projectId = props.projectId;
 const accountId = projectId || props.accountId;
-
 if (!accountId) {
   return "";
 }
-
 const following = Social.keys(`${accountId}/graph/follow/*`, "final", {
   return_type: "BlockHeight",
   values_only: true,
 });
-
 const followers = Social.keys(`*/graph/follow/${accountId}`, "final", {
   return_type: "BlockHeight",
   values_only: true,
 });
-
-const numFollowing = following ? Object.keys(following[accountId].graph.follow || {}).length : null;
+const numFollowing = following
+  ? Object.keys(following[accountId].graph.follow || {}).length
+  : null;
 const numFollowers = followers ? Object.keys(followers || {}).length : null;
-
 const profileLink = props.hrefWithParams(
-  `?tab=${projectId ? "project" : "profile"}&${projectId ? "projectId" : "accountId"}=${
-    projectId || accountId
-  }`
+  `?tab=${projectId ? "project" : "profile"}&${
+    projectId ? "projectId" : "accountId"
+  }=${projectId || accountId}`
 );
-
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -37,20 +33,25 @@ const Container = styled.div`
     gap: 1rem;
   }
 `;
-
 return (
   <Container>
     <div>
       <a href={`${profileLink}&nav=followers`} className="text-dark">
-        {numFollowers !== null ? <span style={{ fontWeight: 600 }}>{numFollowers}</span> : "-"}
-
+        {numFollowers !== null ? (
+          <span style={{ fontWeight: 600 }}>{numFollowers}</span>
+        ) : (
+          "-"
+        )}
         <span>Follower{numFollowers !== 1 && "s"}</span>
       </a>
     </div>
     <div className="me-4">
       <a href={`${profileLink}&nav=following`} className="text-dark">
-        {numFollowing !== null ? <span style={{ fontWeight: 600 }}>{numFollowing}</span> : "-"}
-
+        {numFollowing !== null ? (
+          <span style={{ fontWeight: 600 }}>{numFollowing}</span>
+        ) : (
+          "-"
+        )}
         <span>Following</span>
       </a>
     </div>
