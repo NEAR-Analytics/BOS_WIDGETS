700a5c73f3cee7f2d6929a51e034663168574082
potlock.near <potlock.near>
2024-03-27 15:18:16 +0000

    Update Project.ListPage by potlock.near at 2024-03-27T15:18:16.507Z

--- potlock.near/Project.ListPage/source_code.jsx
+++ potlock.near/Project.ListPage/source_code.jsx
@@ -1,10 +1,8 @@
-const { getTagsFromSocialProfileData, getTeamMembersFromSocialProfileData } = VM.require(
-  "potlock.near/widget/utils"
-) || {
-  getTagsFromSocialProfileData: () => [],
-  getTeamMembersFromSocialProfileData: () => [],
-};
-
+const { getTagsFromSocialProfileData, getTeamMembersFromSocialProfileData } =
+  VM.require("potlock.near/widget/utils") || {
+    getTagsFromSocialProfileData: () => [],
+    getTeamMembersFromSocialProfileData: () => [],
+  };
 // Card Skeleton - Loading fallback
 const loadingSkeleton = styled.keyframes`
   0% {
@@ -17,7 +15,6 @@ const loadingSkeleton = styled.keyframes`
     opacity: 1;
   }
 `;
-
 const CardSkeletonContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -30,19 +27,16 @@ const CardSkeletonContainer = styled.div`
   border: 1px solid #dbdbdb;
   margin-left: auto;
   margin-right: auto;
-
   animation-name: ${loadingSkeleton};
   animation-duration: 1s;
   animation-iteration-count: infinite;
 `;
-
 const HeaderSkeleton = styled.div`
   display: block;
   width: 100%;
   height: 168px;
   background: #eee;
 `;
-
 const ProfileImageSkeleton = styled.div`
   background: #e0e0e0;
   margin-left: 32px;
@@ -52,7 +46,6 @@ const ProfileImageSkeleton = styled.div`
   position: absolute;
   border-radius: 999px;
 `;
-
 const TitleSkeleton = styled.div`
   width: 120px;
   height: 24px;
@@ -60,7 +53,6 @@ const TitleSkeleton = styled.div`
   margin-left: 24px;
   margin-top: 24px;
 `;
-
 const DescriptionSkeleton = styled.div`
   width: 83%;
   height: 48px;
@@ -68,7 +60,6 @@ const DescriptionSkeleton = styled.div`
   margin-left: 24px;
   margin-top: 24px;
 `;
-
 const TagSkeleton = styled.div`
   background: #eee;
   border-radius: 4px;
@@ -76,17 +67,14 @@ const TagSkeleton = styled.div`
   width: 110px;
   margin: 24px;
 `;
-
 const FooterItemSkeleton = styled.div`
   width: 150px;
   height: 40px;
   background: #eee;
-
   @media screen and (max-width: 390px) {
     width: 100px;
   }
 `;
-
 const DonationsInfoContainerSkeleton = styled.div`
   display: flex;
   flex-direction: row;
@@ -96,14 +84,12 @@ const DonationsInfoContainerSkeleton = styled.div`
   width: 100%;
   border-top: 1px #f0f0f0 solid;
 `;
-
 const DonationsInfoItemSkeleton = styled.div`
   display: flex;
   flex-direction: row;
   gap: 8px;
   align-items: center;
 `;
-
 const CardSkeleton = () => (
   <CardSkeletonContainer>
     <HeaderSkeleton />
@@ -121,37 +107,32 @@ const CardSkeleton = () => (
     </DonationsInfoContainerSkeleton>
   </CardSkeletonContainer>
 );
-
 // ListPage Content
-
 const { tab } = props;
 const { ownerId } = VM.require("potlock.near/widget/constants") || {
   ownerId: "",
 };
-const { yoctosToUsd } = VM.require("potlock.near/widget/utils") || { yoctosToUsd: () => "" };
+const { yoctosToUsd } = VM.require("potlock.near/widget/utils") || {
+  yoctosToUsd: () => "",
+};
 const IPFS_BASE_URL = "https://nftstorage.link/ipfs/";
 const HERO_BACKGROUND_IMAGE_URL =
   IPFS_BASE_URL + "bafkreiewg5afxbkvo6jbn6jgv7zm4mtoys22jut65fldqtt7wagar4wbga";
-
 const headerTitleFontSizePx = 88;
-
 const HeaderContainer = styled.div`
   width: 100%;
   // background: #fffaf4;
   // background: white;
   padding: 80px 64px;
-
   @media (max-width: 768px) {
     padding: 36px 24px;
   }
 `;
-
 const HeaderContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
-
 const HeaderTitle = styled.div`
   color: #2e2e2e;
   font-size: ${headerTitleFontSizePx}px;
@@ -166,7 +147,6 @@ const HeaderTitle = styled.div`
     font-size: 48px;
   }
 `;
-
 const HeaderDescription = styled.div`
   color: #2e2e2e;
   font-size: 32px;
@@ -175,13 +155,11 @@ const HeaderDescription = styled.div`
   max-width: 866px;
   text-align: center;
   margin-top: 32px;
-
   @media (max-width: 768px) {
     font-size: 24px;
     text-align: center;
   }
 `;
-
 const ButtonsContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -190,23 +168,18 @@ const ButtonsContainer = styled.div`
   gap: 32px;
   margin-top: 32px;
 `;
-
 const Underline = styled.div`
   position: absolute;
   top: ${headerTitleFontSizePx - 40}px;
   left: -40px;
   z-index: -1;
-
   @media (max-width: 768px) {
     top: 30px;
     left: -30px;
   }
 `;
-
 const containerStyle = props.containerStyle ?? {};
-
 const showStats = !props.tab || props.tab == "projects";
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -215,18 +188,15 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
-
 const HeroOuter = styled.div`
   padding: 136px 64px;
 `;
-
 const HeroInner = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
 `;
-
 const SectionHeader = styled.div`
   display: flex;
   flex-direction: row;
@@ -234,30 +204,25 @@ const SectionHeader = styled.div`
   align-items: center;
   margin-bottom: 24px;
   padding: 24px 64px 24px 64px;
-
   @media screen and (max-width: 768px) {
     padding: 16px 24px;
   }
 `;
-
 const SectionTitle = styled.div`
   font-size: 24px;
   font-weight: 600;
   color: #2e2e2e;
   font-family: Mona-Sans;
 `;
-
 const ProjectsCount = styled.div`
   color: #7b7b7b;
   font-size: 24px;
   font-weight: 400;
   margin-left: 32px;
-
   @media screen and (max-width: 768px) {
     margin-left: 16px;
   }
 `;
-
 const ProjectsContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -266,32 +231,23 @@ const ProjectsContainer = styled.div`
   overflow-y: hidden;
   // padding: 0px 64px 96px 64px;
   // background: #fafafa;
-
-  // @media screen and (max-width: 768px) {
-  //   margin-top: 200px;
-  // }
 `;
-
 const HeroContainer = styled.div`
   width: 100%;
   min-height: 700px;
   position: relative;
-
   @media screen and (max-width: 768px) {
     min-height: 600px;
   }
 `;
-
 const Hero = styled.img`
   width: 100%;
   height: 100%;
   display: block;
-
   @media screen and (max-width: 768px) {
     display: none;
   }
 `;
-
 const InfoCardsContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -300,14 +256,12 @@ const InfoCardsContainer = styled.div`
   z-index: 1000;
   margin: 40px 0;
   gap: 40px;
-
   @media screen and (max-width: 768px) {
     flex-direction: column;
     gap: 24px;
     // justify-content: center;
   }
 `;
-
 const Button = styled.button`
   flex-direction: row;
   justify-content: center;
@@ -325,13 +279,11 @@ const Button = styled.button`
   font-size: 14px;
   line-height: 16px;
   font-weight: 600;
-
   &:hover {
     text-decoration: none;
     cursor: pointer;
   }
 `;
-
 const ButtonRegisterProject = styled.a`
   flex-direction: row;
   justify-content: center;
@@ -349,13 +301,11 @@ const ButtonRegisterProject = styled.a`
   font-size: 14px;
   line-height: 16px;
   font-weight: 600;
-
   &:hover {
     text-decoration: none;
     cursor: pointer;
   }
 `;
-
 const Stats = styled.div`
   display: flex;
   flex-direction: row;
@@ -363,12 +313,10 @@ const Stats = styled.div`
   justify-content: center;
   gap: 48px;
   margin-top: 40px;
-
   @media screen and (max-width: 768px) {
     gap: 16px;
   }
 `;
-
 const StatsTitle = styled.div`
   color: #292929;
   font-size: 44px;
@@ -377,30 +325,25 @@ const StatsTitle = styled.div`
   align-items: baseline;
   gap: 8px;
   font-weight: 600;
-
   @media screen and (max-width: 768px) {
     font-size: 30px;
     font-weight: 500;
     gap: 5px;
   }
 `;
-
 const StatsSubTitle = styled.div`
   color: #525252;
   font-size: 14px;
-
   @media screen and (max-width: 768px) {
     font-size: 12px;
   }
 `;
-
 const Header = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
   width: 100%;
 `;
-
 const Title = styled.div`
   color: #292929;
   font-size: 14px;
@@ -410,7 +353,6 @@ const Title = styled.div`
   letter-spacing: 1.12px;
   text-transform: uppercase;
 `;
-
 const TagsWrapper = styled.div`
   display: flex;
   width: 100%;
@@ -423,7 +365,6 @@ const TagsWrapper = styled.div`
   line-height: 24px;
   color: #292929;
 `;
-
 const Tag = styled.div`
   display: flex;
   padding: 8px;
@@ -439,7 +380,6 @@ const Tag = styled.div`
     background: #fef6ee;
   }
 `;
-
 const OnBottom = styled.div`
   width: 100%;
   display: flex;
@@ -447,7 +387,6 @@ const OnBottom = styled.div`
   justify-content: center;
   padding: 20px 0;
 `;
-
 const ContainerHeader = styled.div`
   display: flex;
   flex-direction: column;
@@ -462,32 +401,29 @@ const ContainerHeader = styled.div`
     padding-top: 40px;
   }
 `;
-
 const ProjectList = styled.div`
   display: grid;
   gap: 31px;
-
   // For mobile devices (1 column)
   @media screen and (max-width: 739px) {
     grid-template-columns: repeat(1, 1fr);
   }
-
   // For tablet devices (2 columns)
   @media screen and (min-width: 740px) and (max-width: 1023px) {
     grid-template-columns: repeat(2, 1fr);
   }
-
   // For desktop devices (3 columns)
   @media screen and (min-width: 1024px) {
-    grid-template-columns: repeat(${!props.maxCols || props.maxCols > 2 ? "3" : "2"}, 1fr);
+    grid-template-columns: repeat(
+      ${!props.maxCols || props.maxCols > 2 ? "3" : "2"},
+      1fr
+    );
   }
 `;
-
 State.init({
   isModalOpen: false,
   successfulDonation: null,
 });
-
 let RegistrySDK =
   VM.require("potlock.near/widget/SDK.registry") ||
   (() => ({
@@ -495,20 +431,15 @@ let RegistrySDK =
     isRegistryAdmin: () => {},
   }));
 RegistrySDK = RegistrySDK({ env: props.env });
-
 const isRegistryAdmin = RegistrySDK.isRegistryAdmin(context.accountId);
-
 let DonateSDK =
   VM.require("potlock.near/widget/SDK.donate") ||
   (() => ({
     getConfig: () => {},
   }));
 DonateSDK = DonateSDK({ env: props.env });
-
 const allProjects = RegistrySDK.getProjects() || [];
-
 // console.log("allProjects: ", allProjects);
-
 const projects = useMemo(() => {
   if (!isRegistryAdmin) {
     return allProjects.filter((project) => project.status === "Approved");
@@ -516,10 +447,12 @@ const projects = useMemo(() => {
   allProjects.sort((a, b) => b.submitted_ms - a.submitted_ms);
   return allProjects;
 }, allProjects);
-
 // console.log("projects: ", projects);
-
-const featuredProjectIds = ["magicbuild.near", "potlock.near", "yearofchef.near"];
+const featuredProjectIds = [
+  "magicbuild.near",
+  "potlock.near",
+  "yearofchef.near",
+];
 const featuredProjects = useMemo(
   () => projects.filter((project) => featuredProjectIds.includes(project.id)),
   projects
@@ -563,38 +496,33 @@ const [tagsList, setTagsList] = useState([
     selected: false,
   },
 ]);
-
 useEffect(() => {
   if (filteredProjects.length < 1) {
     setFilteredProjects(projects);
   }
 }, [projects]);
-
 // console.log("filter", filteredProjects);
-
 const donateConfig = DonateSDK.getConfig();
 if (donateConfig && !totalDonated && !totalDonation) {
   const lastDonationAmount = yoctosToUsd(donateConfig.net_donations_amount);
   setTotalDonated(lastDonationAmount);
   setTotalDonation(donateConfig.total_donations_count);
 }
-
 const donateRandomly = () => {
   State.update({
     isModalOpen: true,
     successfulDonation: null,
   });
 };
-
 const containerStyleHeader = {
   position: "absolute",
   height: "100%",
   top: 0,
   left: 0,
   marginBottom: "24px",
-  background: "radial-gradient(80% 80% at 40.82% 50%, white 25%, rgba(255, 255, 255, 0) 100%)",
+  background:
+    "radial-gradient(80% 80% at 40.82% 50%, white 25%, rgba(255, 255, 255, 0) 100%)",
 };
-
 const SORT_FILTERS = {
   ALL: "All",
   NEW_TO_OLD: "Newest to Oldest",
@@ -602,13 +530,11 @@ const SORT_FILTERS = {
   // MOST_TO_LEAST_DONATIONS: "Most to Least Donations",
   // LEAST_TO_MOST_DONATIONS: "Least to Most Donations",
 };
-
 const sortHighestToLowest = (projects) => {
   const sort = (a, b) => {
     return parseFloat(b.total) - parseFloat(a.total);
   };
   const projectLength = projects.length;
-
   for (let i = 0; i < projectLength - 1; i++) {
     for (let j = 0; j < projectLength - 1 - i; j++) {
       if (sort(projects[j], projects[j + 1]) > 0) {
@@ -618,16 +544,13 @@ const sortHighestToLowest = (projects) => {
       }
     }
   }
-
   setFilteredProjects(projects);
 };
-
 const sortLowestToHighest = (projects) => {
   const sort = (a, b) => {
     return parseFloat(b.total) - parseFloat(a.total);
   };
   const projectLength = projects.length;
-
   for (let i = 0; i < projectLength - 1; i++) {
     for (let j = 0; j < projectLength - 1 - i; j++) {
       if (sort(projects[j], projects[j + 1]) < 0) {
@@ -637,13 +560,10 @@ const sortLowestToHighest = (projects) => {
       }
     }
   }
-
   setFilteredProjects(projects);
 };
-
 const sortNewToOld = (projects) => {
   const projectLength = projects.length;
-
   for (let i = 0; i < projectLength - 1; i++) {
     for (let j = 0; j < projectLength - i - 1; j++) {
       if (projects[j].submitted_ms < projects[j + 1].submitted_ms) {
@@ -655,10 +575,8 @@ const sortNewToOld = (projects) => {
   }
   setFilteredProjects(projects);
 };
-
 const sortOldToNew = (projects) => {
   const projectLength = projects.length;
-
   for (let i = 0; i < projectLength - 1; i++) {
     for (let j = 0; j < projectLength - i - 1; j++) {
       if (projects[j].submitted_ms > projects[j + 1].submitted_ms) {
@@ -670,7 +588,6 @@ const sortOldToNew = (projects) => {
   }
   setFilteredProjects(projects);
 };
-
 const handleSortChange = (sortType) => {
   setSort(sortType);
   switch (sortType) {
@@ -691,7 +608,6 @@ const handleSortChange = (sortType) => {
       break;
   }
 };
-
 const searchByWords = (projects, searchTerm) => {
   searchTerm = searchTerm.toLowerCase().trim();
   let results = [];
@@ -707,8 +623,14 @@ const searchByWords = (projects, searchTerm) => {
       if (
         data.description.toLowerCase().includes(searchTerm) ||
         data.name.toLowerCase().includes(searchTerm) ||
-        getTagsFromSocialProfileData(data).join("").toLowerCase().includes(searchTerm) ||
-        getTeamMembersFromSocialProfileData(data).join("").toLowerCase().includes(searchTerm)
+        getTagsFromSocialProfileData(data)
+          .join("")
+          .toLowerCase()
+          .includes(searchTerm) ||
+        getTeamMembersFromSocialProfileData(data)
+          .join("")
+          .toLowerCase()
+          .includes(searchTerm)
       ) {
         results.push(project);
       }
@@ -731,7 +653,6 @@ const searchByWords = (projects, searchTerm) => {
   //     }
   //   });
   // });
-
   setFilteredProjects(results);
 };
 const handleTag = (key) => {
@@ -762,7 +683,6 @@ const handleTag = (key) => {
   }
   setTagsList(tags);
 };
-
 return (
   <>
     <HeroContainer>
@@ -796,10 +716,10 @@ return (
           </HeaderTitle>
           <HeaderTitle>Funding for Public Goods</HeaderTitle>
           <HeaderDescription>
-            Discover impact projects, donate directly, & participate in funding rounds.
+            Discover impact projects, donate directly, & participate in funding
+            rounds.
           </HeaderDescription>
         </HeaderContent>
-
         <ButtonsContainer>
           {/* <Widget
             src={`${ownerId}/widget/Project.ButtonDonateRandomly`}
@@ -863,7 +783,6 @@ return (
         <Header>
           <Title>Featured projects</Title>
         </Header>
-
         <ProjectList>
           {featuredProjects.map((project) => {
             return (
@@ -984,7 +903,9 @@ return (
           }}
         />
       ) : (
-        <div style={{ alignSelf: "flex-start", margin: "24px 0px" }}>No results</div>
+        <div style={{ alignSelf: "flex-start", margin: "24px 0px" }}>
+          No results
+        </div>
       )}
     </ProjectsContainer>
   </>
