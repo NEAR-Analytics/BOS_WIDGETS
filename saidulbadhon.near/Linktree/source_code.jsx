3b42c9724a66bd36daf644e2a55528dfad8c4b1a
saidulbadhon.near <saidulbadhon.near>
2024-03-27 14:33:45 +0000

    Update Linktree by saidulbadhon.near at 2024-03-27T14:33:45.096Z

--- saidulbadhon.near/Linktree/source_code.jsx
+++ saidulbadhon.near/Linktree/source_code.jsx
@@ -1,16 +1,17 @@
+// update
 
 const profile = {
   avatar:
-    "https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-  title: "Saidul Badhon",
-  subtitle: "Data Engineer",
+  "https://pbs.twimg.com/profile_images/1536919525190516737/SvU144V6_400x400.jpg",
+  title: "Kayleigh Svensson",
+  subtitle: "Head of Operations",
   links: [
-    { title: "github", url: "https://github.com/zahid-dev" },
-    { title: "twitter", url: "https://twitter.com/zahid-dev" },
+    { title: "Telegram", url: "t.me/kaysve" },
+    { title: "Twitter", url: "https://twitter.com/kaysve" },
   ],
   socials: [
-    { title: "github", url: "https://github.com/zahid-dev" },
-    { title: "twitter", url: "https://twitter.com/zahid-dev" },
+    { title: "Telegram", url: "t.me/kaysve" },
+    { title: "Twitter", url: "https://twitter.com/kaysve" },
   ],
 };
 
@@ -34,6 +35,11 @@ const titleToIcon = [
   {
     title: "linkedin",
     icon: <i class="bi bi-linkedin"></i>,
+  },
+
+  {
+    title: "telegram",
+    icon: <i class="bi bi-telegram"></i>,
   },
 ];
 
