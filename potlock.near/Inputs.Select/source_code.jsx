ba26fc32b166b213f43bd5f12d37058f0e2a725b
potlock.near <potlock.near>
2024-03-27 15:18:16 +0000

    Update Inputs.Select by potlock.near at 2024-03-27T15:18:16.507Z

--- potlock.near/Inputs.Select/source_code.jsx
+++ potlock.near/Inputs.Select/source_code.jsx
@@ -6,7 +6,6 @@ const options = props.options ?? [];
 const onChange = props.onChange ?? (() => {});
 const validate = props.validate ?? (() => {});
 const error = props.error ?? "";
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -16,7 +15,6 @@ const Container = styled.div`
   gap: 0.45em;
   width: 100%;
 `;
-
 const Label = styled.label`
   font-weight: 500;
   font-size: 14px;
@@ -24,7 +22,6 @@ const Label = styled.label`
   word-wrap: break-word;
   color: #2e2e2e;
 `;
-
 const Error = styled.span`
   display: inline-block;
   font-style: normal;
@@ -35,12 +32,10 @@ const Error = styled.span`
   height: 0;
   overflow: hidden;
   transition: height 0.3s ease-in-out;
-
   &.show {
     height: 1.25em;
   }
 `;
-
 const Input = styled.div`
   box-sizing: border-box;
   display: flex;
@@ -57,11 +52,9 @@ const Input = styled.div`
   color: #101828;
   width: 100%;
 `;
-
 const Placeholder = styled.span`
   color: #a0a3a8;
 `;
-
 const scaleOut = styled.keyframes`
   from {
     transform: scaleY(0);
@@ -70,7 +63,6 @@ const scaleOut = styled.keyframes`
     transform: scaleY(1);
   }
 `;
-
 const Content = styled.div`
   display: flex;
   flex-direction: column;
@@ -83,7 +75,6 @@ const Content = styled.div`
   border-radius: 4px;
   background: #ffffff;
   z-index: 3 !important;
-
   /* &[data-state="open"] { */
   /*   animation: ${scaleOut} 0.2s ease-in-out; */
   /* } */
@@ -92,7 +83,6 @@ const Content = styled.div`
   /*   animation: ${scaleOut} 0.2s ease-in-out reverse; */
   /* } */
 `;
-
 const Viewport = styled.div`
   display: flex;
   flex-direction: column;
@@ -101,7 +91,6 @@ const Viewport = styled.div`
   padding: 0;
   width: 100%;
 `;
-
 const Item = styled.button`
   display: flex;
   flex-direction: row;
@@ -114,27 +103,25 @@ const Item = styled.button`
   background: transparent;
   border: none;
   transition: background 0.2s ease-in-out;
-
   &:nth-child(n + 1) {
     border-top: 1px solid #d0d5dd;
   }
-
   &:hover {
     background: #d0d5dd;
     boder: none;
   }
-
   &:focus {
     outline: none;
   }
 `;
-
 return (
   <Container style={props.containerStyles || {}}>
     {noLabel ? <></> : <Label>{label}</Label>}
     <Select.Root
       value={value?.value}
-      onValueChange={(value) => onChange(options.find((option) => option.value === value))}
+      onValueChange={(value) =>
+        onChange(options.find((option) => option.value === value))
+      }
     >
       <Select.Trigger asChild={true}>
         <Input style={props.inputStyles || {}}>
@@ -166,7 +153,6 @@ return (
           {/* )} */}
         </Input>
       </Select.Trigger>
-
       <Select.Content asChild={true}>
         <Content>
           <Select.Viewport asChild={true}>
