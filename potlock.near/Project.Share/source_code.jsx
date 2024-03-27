2172d6ff1c6aebdb3d55904194d5a459eea51966
potlock.near <potlock.near>
2024-03-27 15:18:16 +0000

    Update Project.Share by potlock.near at 2024-03-27T15:18:16.507Z

--- potlock.near/Project.Share/source_code.jsx
+++ potlock.near/Project.Share/source_code.jsx
@@ -13,7 +13,6 @@ const copySvg = (
     <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
   </svg>
 );
-
 const Icon = styled.div`
   display: flex;
   flex-direction: row;
@@ -22,11 +21,9 @@ const Icon = styled.div`
   cursor: pointer;
   margin-left: 0.5em;
 `;
-
 State.init({
   copied: false,
 });
-
 return (
   <Icon
     // className={props.className ?? "btn btn-outline-primary border-0"}
@@ -44,7 +41,8 @@ return (
   >
     {state.copied ? (
       <>
-        {props.copiedIcon ?? <i className="bi bi-check-lg" />} {props.copiedLabel ?? props.label}
+        {props.copiedIcon ?? <i className="bi bi-check-lg" />}{" "}
+        {props.copiedLabel ?? props.label}
       </>
     ) : (
       <>
