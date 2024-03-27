9caa8e50fa8db8e2ed79639fdb8fdfdee8492a9f
potlock.near <potlock.near>
2024-03-27 15:18:16 +0000

    Update Profile.Compose by potlock.near at 2024-03-27T15:18:16.507Z

--- potlock.near/Profile.Compose/source_code.jsx
+++ potlock.near/Profile.Compose/source_code.jsx
@@ -1,19 +1,14 @@
 if (!context.accountId) {
   return "";
 }
-
 const indexKey = props.indexKey ?? "main";
 const draftKey = props.indexKey ?? "draft";
 const draft = Storage.privateGet(draftKey) || props.initialText;
-
 const groupId = props.groupId;
-
 if (draft === null) {
   return "";
 }
-
 const [initialText] = useState(draft);
-
 const composeData = () => {
   const data = {
     post: {
@@ -28,20 +23,20 @@ const composeData = () => {
       }),
     },
   };
-
   const item = {
     type: "social",
     path: `${context.accountId}/post/main`,
   };
-
-  const notifications = state.extractMentionNotifications(state.content.text, item);
-
+  const notifications = state.extractMentionNotifications(
+    state.content.text,
+    item
+  );
   if (notifications.length) {
-    data.index.notify = JSON.stringify(notifications.length > 1 ? notifications : notifications[0]);
+    data.index.notify = JSON.stringify(
+      notifications.length > 1 ? notifications : notifications[0]
+    );
   }
-
   const hashtags = state.extractHashtags(state.content.text);
-
   if (hashtags.length) {
     data.index.hashtag = JSON.stringify(
       hashtags.map((hashtag) => ({
@@ -50,17 +45,14 @@ const composeData = () => {
       }))
     );
   }
-
   return data;
 };
-
 State.init({
   onChange: ({ content }) => {
     State.update({ content });
     Storage.privateSet(draftKey, content.text || "");
   },
 });
-
 return (
   <>
     <div style={{ margin: "0 -12px" }}>
