e3a091d9a5cc1a1d083ea183b0fa6c3d1611251a
potlock.near <potlock.near>
2024-03-27 15:18:16 +0000

    Update Components.ProjectCard by potlock.near at 2024-03-27T15:18:16.507Z

--- potlock.near/Components.ProjectCard/source_code.jsx
+++ potlock.near/Components.ProjectCard/source_code.jsx
@@ -1,19 +1,18 @@
 const { id, review_notes, status, totalAmount } = props;
-const { getTagsFromSocialProfileData } = VM.require("potlock.near/widget/utils") || {
+const { getTagsFromSocialProfileData } = VM.require(
+  "potlock.near/widget/utils"
+) || {
   getTagsFromSocialProfileData: () => [],
 };
 const IPFS_BASE_URL = "https://ipfs.near.social/ipfs/";
 const cardData = Social.getr(`${id}/profile`);
-
 let DonateSDK =
   VM.require("potlock.near/widget/SDK.donate") ||
   (() => ({
     getDonationsForRecipient: () => {},
   }));
 DonateSDK = DonateSDK({ env: props.env });
-
 const donationsForProject = DonateSDK.getDonationsForRecipient(id);
-
 const Card = styled.a`
   display: flex;
   flex-direction: column;
@@ -35,7 +34,6 @@ const Card = styled.a`
   height: max-content;
   overflow: hidden;
 `;
-
 const CardBody = styled.div`
   position: relative;
   padding: 24px;
@@ -44,14 +42,12 @@ const CardBody = styled.div`
   gap: 16px;
   flex-grow: 1;
 `;
-
 const CardImage = styled.img`
   height: 150px;
   min-height: 150px;
   width: 100%;
   object-fit: cover;
 `;
-
 const CardTitle = styled.div`
   color: #292929;
   font-size: 17px;
@@ -62,7 +58,6 @@ const CardTitle = styled.div`
   text-overflow: ellipsis;
   white-space: nowrap;
 `;
-
 const CardDescription = styled.div`
   color: #292929;
   font-size: 17px;
@@ -71,7 +66,6 @@ const CardDescription = styled.div`
   line-height: 28px;
   flex-grow: 1;
 `;
-
 const CardTag = styled.div`
   color: #292929;
   width: max-content;
@@ -85,13 +79,11 @@ const CardTag = styled.div`
   font-weight: 400;
   line-height: 20px;
 `;
-
 const CardTagContainer = styled.div`
   display: flex;
   gap: 12px;
   flex-wrap: wrap;
 `;
-
 const CardAvatar = styled.img`
   position: absolute;
   top: -20px;
@@ -101,7 +93,6 @@ const CardAvatar = styled.img`
   height: 40px;
   border: 3px solid #fff;
 `;
-
 const CardFooter = styled.div`
   border-top: 1px solid #000;
   padding: 16px 24px;
@@ -109,7 +100,6 @@ const CardFooter = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
-
 const TotalDonate = styled.div`
   color: #292929;
   font-size: 17px;
@@ -117,7 +107,6 @@ const TotalDonate = styled.div`
   font-weight: 600;
   line-height: 24px;
 `;
-
 const DonationButton = styled.button`
   padding: 12px 16px;
   font-size: 14px;
@@ -133,7 +122,6 @@ const DonationButton = styled.button`
     color: #fff;
   }
 `;
-
 const AddToCartButton = styled.button`
   border: none;
   background: none;
@@ -144,13 +132,11 @@ const AddToCartButton = styled.button`
   }
   padding: 12px 16px;
 `;
-
 const ButtonGroup = styled.div`
   display: flex;
   gap: 10px;
   align-items: center;
 `;
-
 const getCategory = (category) => {
   switch (category) {
     case "social-impact":
@@ -171,17 +157,16 @@ const getCategory = (category) => {
       return "Education";
   }
 };
-
 const projectUrl = props.hrefWithParams(`?tab=project&projectId=${id}`);
-
 const tags = getTagsFromSocialProfileData(cardData);
-
 return (
   <>
     <Card href={projectUrl}>
       <CardImage
         src={
-          cardData && cardData?.backgroundImage && cardData?.backgroundImage?.ipfs_cid
+          cardData &&
+          cardData?.backgroundImage &&
+          cardData?.backgroundImage?.ipfs_cid
             ? `${IPFS_BASE_URL}${cardData.backgroundImage.ipfs_cid}`
             : "https://ipfs.near.social/ipfs/bafkreih4i6kftb34wpdzcuvgafozxz6tk6u4f5kcr2gwvtvxikvwriteci"
         }
@@ -210,7 +195,8 @@ return (
       </CardBody>
       <CardFooter>
         <TotalDonate>
-          ${totalAmount(donationsForProject)} <span style={{ fontWeight: 400 }}>Raised</span>
+          ${totalAmount(donationsForProject)}{" "}
+          <span style={{ fontWeight: 400 }}>Raised</span>
         </TotalDonate>
         <ButtonGroup>
           <AddToCartButton
