d07a8d94fef3fbc4bce2dbc522eacf6cea6d53a4
hack.near <hack.near>
2024-03-28 07:29:46 +0000

    Update faces by hack.near at 2024-03-28T07:29:46.888Z

--- hack.near/faces/source_code.jsx
+++ hack.near/faces/source_code.jsx
@@ -10,8 +10,8 @@ const Faces = styled.span`
     position: relative;
     top: -0.05em;
     margin: 0 -0.1em;
-    height: 1em;
-    width: 1em;
+    height: 1.5em;
+    width: 1.5em;
     min-width: 1em;
     img {
         object-fit: cover;
