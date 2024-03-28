d827079cfd1c59f4070c04d5942aa83dbeb43167
bluebiu.near <bluebiu.near>
2024-03-28 06:32:00 +0000

    Update Scroll.Bridge.HandlerSwap by bluebiu.near at 2024-03-28T06:32:00.245Z

---
+++ bluebiu.near/Scroll.Bridge.HandlerSwap/source_code.jsx
@@ -0,0 +1,41 @@
+const {
+    amount,
+    account,
+    currency,
+    routerAddress,
+    routerEthAddress,
+    target,
+    loading,
+    onSuccess,
+    onError,
+    quote,
+} = props;
+if (!loading) return "";
+
+console.log('quote: ', quote)
+const _amount = new Big(amount).mul(Math.pow(10, 18)).toString()
+const lastNum = _amount.slice(0, _amount.length - quote.vc.length) + quote.vc;
+const signer = Ethers.provider().getSigner()
+const params = {
+    from: account,
+    to: quote.endpoint,
+    value: lastNum,
+}
+console.log('params: ', params)
+signer.sendTransaction(params)
+.then((tx) => {
+    console.log(tx)
+    tx.wait()
+      .then((res) => {
+        onSuccess(res);
+      })
+      .catch((err) => {
+        onError(tx);
+      });
+  })
+  .catch((err) => {
+    onError(err);
+  });
+
+
+return "";
