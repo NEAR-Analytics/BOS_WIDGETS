19a3ae51c9d9b49d2460d49af0b4af5b373dffe4
flowscience.near <flowscience.near>
2024-03-28 22:48:17 +0000

    Update eas by flowscience.near at 2024-03-28T22:48:17.380Z

--- flowscience.near/eas/source_code.jsx
+++ flowscience.near/eas/source_code.jsx
@@ -6,130 +6,56 @@ const easAbi = fetch(
   "https://raw.githubusercontent.com/ethereum-attestation-service/eas-contracts/master/deployments/optimism/EAS.json"
 );
 
-const [address, setAddress] = useState(user);
-const [passport, setPassport] = useState(null);
-const [score, setScore] = useState(null);
-const [verified, setVerified] = useState(null);
-const [error, setError] = useState("");
-
 const contractAddress = "0x4200000000000000000000000000000000000021";
 const parsedAbi = JSON.parse(easAbi.body);
-const decoderAbi = parsedAbi["0x1a4"];
+const abi = parsedAbi.abi;
 
-const decoder = new ethers.Contract(
+const [attestation, setAttestation] = useState("");
+const [error, setError] = useState("");
+const [uid, setUid] = useState("");
+
+const easContract = new ethers.Contract(
   contractAddress,
-  decoderAbi,
+  abi,
   Ethers.provider().getSigner()
 );
 
-function getPassport() {
-  decoder
-    .getPassport(address)
+function getAttestation() {
+  abi
+    .getAttestation(uid)
     .then((result) => {
-      setPassport(result);
+      setAttestation(result);
       setError("");
     })
     .catch((error) => {
-      console.error("error fetching passport:", error);
+      console.error("error fetching attestation:", error);
       setError("Failed to retrieve data. Please try with a verified address.");
-    });
-}
-
-function getScore() {
-  decoder
-    .getScore(address)
-    .then((result) => {
-      setScore(result);
-      setError("");
-    })
-    .catch((error) => {
-      console.error("error fetching score:", error);
-      setError("Failed to fetch score. Please try with a verified address.");
-    });
-}
-
-function getStatus() {
-  decoder
-    .isHuman(address)
-    .then((result) => {
-      setVerified(result);
-      setError("");
-    })
-    .catch((error) => {
-      console.error("error checking status:", error);
-      setError("Failed to check status. Please try with a verified address.");
     });
 }
 
 return (
   <>
     <div className="m-2">
-      <h3>Gitcoin Passport Decoder</h3>
+      <h3>Get Attestation</h3>
       <input
         type="text"
-        placeholder="input Ethereum address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
+        placeholder="input UID"
+        value={uid}
+        onChange={(e) => setUid(e.target.value)}
       />
     </div>
     <div className="m-2">
-      <button
-        disabled={address === ""}
-        className="btn btn-primary m-1"
-        onClick={getStatus}
-      >
-        Check Passport
-      </button>
-      <button
-        disabled={address === ""}
-        className="btn btn-primary m-1"
-        onClick={getScore}
-      >
-        Fetch Score
-      </button>
-      <button
-        disabled={address === ""}
-        className="btn btn-primary m-1"
-        onClick={getPassport}
-      >
-        Review Data
+      <button className="btn btn-primary m-1" onClick={getAttestation}>
+        Get Attestation
       </button>
       <p className="m-1">{error}</p>
     </div>
     <div className="m-2">
-      {verified && (
+      {attestation && (
         <p className="m-2">
-          <b>Verified:</b> {JSON.stringify(verified)}
+          <b>View Attestation:</b> {JSON.stringify(attestation)}
         </p>
       )}
-      {score && (
-        <p className="m-2">
-          <b>Score:</b> {JSON.stringify(score)}
-        </p>
-      )}
-      {passport && (
-        <p className="m-2">
-          <b>Data:</b> {JSON.stringify(passport)}
-        </p>
-      )}
-    </div>
-    <hr />
-    <div className="m-3">
-      <h4>
-        <a href="https://docs.passport.gitcoin.co/building-with-passport/smart-contracts/contract-reference">
-          Contract Reference
-        </a>
-      </h4>
-      <p>
-        <i>using Optimism (0x1a4)</i>
-      </p>
-      <h5>
-        ABI --
-        <a href="https://github.com/gitcoinco/eas-proxy/blob/main/deployments/abi/GitcoinPassportDecoder.json">
-          GitHub
-        </a>
-      </h5>
-      <pre>{JSON.stringify(easAbi)}</pre>
     </div>
   </>
 );
