e785ea1fe10349c6a835ab29693a2d834e7bef6c
saidulbadhon.near <saidulbadhon.near>
2024-03-28 12:09:15 +0000

    Update helloWorld by saidulbadhon.near at 2024-03-28T12:09:15.937Z

--- saidulbadhon.near/helloWorld/source_code.jsx
+++ saidulbadhon.near/helloWorld/source_code.jsx
@@ -1,12 +1,14 @@
 return (
-  <div>
+  <div style={{ display: "flex", gap:8, alignItems:'center' }}>
     <h1>1</h1>
     <h1>2</h1>
     <h1>3</h1>
     <h1>4</h1>
     <h1>5</h1>
 
-<a href="geo:124.028582,-29.201930" target="_blank">Click here for map</a>
+    <a href="geo:124.028582,-29.201930" target="_blank">
+      Click here for map
+    </a>
 
     <a href="https://maps.google.com/?q=addressgoeshere">google</a>
 
