427bf238c576c196742c90187ad6dbe2cb7e7d0d
hack.near <hack.near>
2024-03-28 02:11:12 +0000

    Update list.card by hack.near at 2024-03-28T02:11:12.311Z

--- hack.near/list.card/source_code.jsx
+++ hack.near/list.card/source_code.jsx
@@ -6,44 +6,89 @@ const list = Near.view("lists.potlock.near", "get_list", {
   list_id: listId,
 });
 
+const [showAdmins, setShowAdmins] = useState(false);
+const [showDetails, setShowDetails] = useState(false);
+
+const toggleAdmins = () => setShowAdmins(!showAdmins);
+const toggleDetails = () => setShowDetails(!showDetails);
+
 return (
   <>
     {list && (
       <div className="card">
-        <div className="card-header">
-          <h3>{list.name}</h3>
+        <div className="d-flex flex-row justify-content-between card-header">
+          <h5 className="mt-2">{list.name}</h5>
+          <div className="mt-1">
+            <a
+              href={`/hack.near/widget/list.view?listId=${listId}`}
+              className="btn btn-sm btn-success m-1"
+            >
+              View
+            </a>
+          </div>
         </div>
         <div className="card-body">
-          <p className="card-text">{list.description}</p>
-          <h5 className="card-title">Admins:</h5>
-          <div className="d-flex flex-row">
-            {list.admins.map((admin, i) => (
-              <div className="admin-profile" key={i}>
-                <Widget
-                  src="mob.near/widget/Profile.ShortInlineBlock"
-                  props={{ accountId: admin }}
-                />
+          <div className="d-flex flex-row justify-content-between">
+            <div className="card-text mt-1">
+              <p>{list.description}</p>{" "}
+              <i>
+                <b>Created:</b>
+                {new Date(list.created_at).toLocaleString()}
+              </i>
+            </div>
+            <div style={{ textAlign: "right" }} className="card-info">
+              <div>
+                <button
+                  onClick={toggleAdmins}
+                  className="btn btn-sm btn-outline-dark m-1"
+                >
+                  {!showAdmins ? (
+                    <i class="bi bi-caret-down-fill"> Admins</i>
+                  ) : (
+                    <i class="bi bi-caret-up-fill"> Admins</i>
+                  )}
+                </button>
+                <button
+                  onClick={toggleDetails}
+                  className="btn btn-sm btn-outline-dark m-1"
+                >
+                  {!showDetails ? (
+                    <i class="bi bi-caret-down-fill"> Details</i>
+                  ) : (
+                    <i class="bi bi-caret-up-fill"> Details</i>
+                  )}
+                </button>
               </div>
-            ))}
-          </div>
-          <hr />
-          <div className="card-info">
-            <div>
-              <b>Created At:</b> {new Date(list.created_at).toLocaleString()}
-            </div>
-            <div>
-              <b>Default Registration Status:</b>{" "}
-              {list.default_registration_status}
-            </div>
-            <div>
-              <b>Admin-only Registrations:</b>{" "}
-              {JSON.stringify(list.admin_only_registrations)}
-            </div>
-            <div>
-              <b>Total Registrations:</b> {list.total_registrations_count}
-            </div>
-            <div>
-              <b>Total Upvotes:</b> {list.total_upvotes_count}
+              {showAdmins && (
+                <div className="mt-1">
+                  {list.admins.map((admin, i) => (
+                    <div className="admin-profile" key={i}>
+                      <Widget
+                        src="mob.near/widget/N.ProfileLine"
+                        props={{ accountId: admin, hideAccountId: true }}
+                      />
+                    </div>
+                  ))}
+                </div>
+              )}
+              {showDetails && (
+                <div className="mt-1">
+                  <div>
+                    <b>Default Registration Status:</b>
+                    {list.default_registration_status}
+                  </div>
+                  <div>
+                    <b>Admin-only Registrations:</b>
+                    {JSON.stringify(list.admin_only_registrations)}
+                  </div>
+                  <div>
+                    <b>Total Registrations:</b> {list.total_registrations_count}
+                  </div>
+                  <div>
+                    <b>Total Upvotes:</b> {list.total_upvotes_count}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
