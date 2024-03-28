5f9c278d97b518a7a51804e948b12fd7b0ed5fe8
huunhanz.near <huunhanz.near>
2024-03-28 16:30:46 +0000

    Update Lonk.hero by huunhanz.near at 2024-03-28T16:30:46.718Z

--- huunhanz.near/Lonk.hero/source_code.jsx
+++ huunhanz.near/Lonk.hero/source_code.jsx
@@ -115,7 +115,7 @@ return (
     <Widget src="huunhanz.near/widget/Lonk.stake" />
     <div class="green-bg mt-2 font-md-bigger py-4 py-lg-5" id="about">
       <div class="container">
-        <div class="row flex-md-row align-items-center flex-sm-column-reverse">
+        <div class="d-sm-flex flex-sm-column-reverse">
           <div class="col-12 col-md-8 col-lg-7">
             <p>
               LONK READ AS “LONG”
