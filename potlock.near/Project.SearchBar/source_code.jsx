c5a22ea55a7821e1e9e08de73213adbcdef48ba1
potlock.near <potlock.near>
2024-03-29 14:20:36 +0000

    Update Project.SearchBar by potlock.near at 2024-03-29T14:20:36.213Z

--- potlock.near/Project.SearchBar/source_code.jsx
+++ potlock.near/Project.SearchBar/source_code.jsx
@@ -8,10 +8,13 @@ const {
   setSearchTerm,
   FilterMenuCustomStyle,
 } = props;
+
 const [openFilter, setOpenFilter] = useState(false);
+
 const onSearchChange = (event) => {
   setSearchTerm(event.target.value);
 };
+
 const SearchBarContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -24,12 +27,14 @@ const SearchBarContainer = styled.div`
     padding: 12px 16px 12px 0px;
   }
 `;
+
 const Row = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   flex: 1;
 `;
+
 const SearchBar = styled.input`
   background: none;
   width: 100%;
@@ -41,6 +46,7 @@ const SearchBar = styled.input`
     border: none;
   }
 `;
+
 const FilterButton = styled.div`
   white-space: nowrap;
   display: flex;
@@ -52,6 +58,7 @@ const FilterButton = styled.div`
   line-height: 20px;
   color: #525252;
 `;
+
 const FilterIcon = styled.div`
   display: flex;
   width: 16px;
@@ -59,6 +66,7 @@ const FilterIcon = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 const FilterMenu = styled.div`
   position: absolute;
   background: #fff;
@@ -71,8 +79,7 @@ const FilterMenu = styled.div`
   border-radius: 6px;
   border: 1px solid rgba(41, 41, 41, 0.36);
   box-shadow: 0px 12px 20px -4px rgba(123, 123, 123, 0.32),
-    0px 4px 8px -3px rgba(123, 123, 123, 0.2),
-    0px 0px 2px 0px rgba(123, 123, 123, 0.36);
+    0px 4px 8px -3px rgba(123, 123, 123, 0.2), 0px 0px 2px 0px rgba(123, 123, 123, 0.36);
   z-index: 2;
   ${FilterMenuCustomStyle || ""}
   @media screen and (max-width: 768px) {
@@ -81,6 +88,7 @@ const FilterMenu = styled.div`
     width: 250px;
   }
 `;
+
 const FilterItem = styled.div`
   cursor: pointer;
   padding: 8px;
@@ -93,6 +101,7 @@ const FilterItem = styled.div`
     border-radius: 6px;
   }
 `;
+
 const SearchIcon = styled.div`
   display: flex;
   width: 24px;
@@ -100,6 +109,7 @@ const SearchIcon = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 return (
   <SearchBarContainer>
     <Row>
@@ -112,18 +122,13 @@ return (
         </svg>
       </SearchIcon>
       <SearchBar
-        placeholder={`Search (${numItems}) ${
-          numItems === 1 ? itemName : itemName + "s"
-        }`}
+        placeholder={`Search (${numItems}) ${numItems === 1 ? itemName : itemName + "s"}`}
         onChange={onSearchChange}
         type="text"
         autocomplete="search"
       />
     </Row>
-    <div
-      style={{ position: "relative" }}
-      onClick={() => setOpenFilter(!openFilter)}
-    >
+    <div style={{ position: "relative" }} onClick={() => setOpenFilter(!openFilter)}>
       <FilterButton>
         {sortVal || title}
         <FilterIcon>
