e532779f71c639211941225ff0e53da11b4735e6
abdullahi3000.near <abdullahi3000.near>
2024-03-29 13:24:20 +0000

    Update components by abdullahi3000.near at 2024-03-29T13:24:20.085Z

--- abdullahi3000.near/components/source_code.jsx
+++ abdullahi3000.near/components/source_code.jsx
@@ -1,4 +1,4 @@
-const { Button } = VM.require("buildhub.near/widget/components.Button");
+const { Button } = VM.require("abdullahi3000.near/widget/components.Button");
 const { ProgressState } = VM.require(
   "buildhub.near/widget/components.ProgressState"
 );
