01ab6921c198054158422f86ec886ba4fce8a9b3
potlock.near <potlock.near>
2024-03-29 14:20:36 +0000

    Update Profile.Preview by potlock.near at 2024-03-29T14:20:36.213Z

--- potlock.near/Profile.Preview/source_code.jsx
+++ potlock.near/Profile.Preview/source_code.jsx
@@ -1,8 +1,10 @@
 const accountId = props.accountId ?? context.accountId;
 const ownerId = props.ownerId;
 const profile = Social.getr(`${accountId}/profile`);
+
 const name = profile.name;
 const image = profile.image;
+
 return (
   <div className="profile d-inline-block">
     <a
@@ -18,9 +20,7 @@ return (
         }}
       />
       <div className="profile-info d-inline-block" style={{ maxWidth: "16em" }}>
-        <div className="profile-name text-truncate">
-          {name || "No-name profile"}
-        </div>
+        <div className="profile-name text-truncate">{name || "No-name profile"}</div>
         <div className="profile-links d-flex">
           <div className="d-inline-block profile-account text-secondary text-truncate">
             @{accountId}
