86de4f30c27be254736fb48d2a05c6b3939bc0e6
potlock.near <potlock.near>
2024-03-29 14:20:36 +0000

    Update Project.ModalSuccess by potlock.near at 2024-03-29T14:20:36.213Z

--- potlock.near/Project.ModalSuccess/source_code.jsx
+++ potlock.near/Project.ModalSuccess/source_code.jsx
@@ -8,36 +8,43 @@ const {
   SUPPORTED_FTS: {},
   IPFS_BASE_URL: "",
 };
-const { yoctosToUsd } = VM.require("potlock.near/widget/utils") || {
-  yoctosToUsd: () => "",
-};
+const { yoctosToUsd } = VM.require("potlock.near/widget/utils") || { yoctosToUsd: () => "" };
+
 let DonateSDK =
   VM.require("potlock.near/widget/SDK.donate") ||
   (() => ({
     asyncGetDonationsForDonor: () => {},
     getContractId: () => "",
   }));
+
 DonateSDK = DonateSDK({ env: props.env });
+
 const DONATION_CONTRACT_ID = DonateSDK.getContractId();
+
 // const HEADER_ICON_URL =
 //   IPFS_BASE_URL + "bafkreiholfe7utobo5y2znjdr6ou26qmlcgf5teoxtyjo2undgfpl5kcwe";
 // const TWITTER_ICON_URL =
 //   IPFS_BASE_URL + "bafkreibkeyodxxrf76cr5q3in4tsmhuhzmkl5cdr56rfl57x4aji47gsby";
+
 const DEFAULT_GATEWAY = "https://bos.potlock.org/";
 const POTLOCK_TWITTER_ACCOUNT_ID = "PotLock_";
+
 const DEFAULT_SHARE_HASHTAGS = ["BOS", "PublicGoods", "Donations"];
+
 const Column = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
 `;
+
 const Row = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
 `;
+
 const ModalMain = styled.div`
   display: flex;
   flex-direction: column;
@@ -47,6 +54,7 @@ const ModalMain = styled.div`
   gap: 24px;
   padding: 80px 36px;
 `;
+
 const ModalFooter = styled.div`
   display: flex;
   flex-direction: column;
@@ -64,11 +72,7 @@ const HeaderIcon = styled.div`
   width: 64px;
   height: 64px;
   border-radius: 44px;
-  background: radial-gradient(
-    97.66% 97.66% at 50% 2.34%,
-    #e84a5b 0%,
-    #c02031 100%
-  );
+  background: radial-gradient(97.66% 97.66% at 50% 2.34%, #e84a5b 0%, #c02031 100%);
   box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.25) inset;
   svg {
     width: 100%;
@@ -82,6 +86,7 @@ const Amount = styled.div`
   line-height: 40px;
   font-family: "Lora";
 `;
+
 const AmountUsd = styled.div`
   color: #7b7b7b;
   font-size: 14px;
@@ -89,14 +94,17 @@ const AmountUsd = styled.div`
   line-height: 24px;
   word-wrap: break-word;
 `;
+
 const NearIcon = styled.svg`
   width: 28px;
   height: 28px;
 `;
+
 const ImgIcon = styled.img`
   width: 28px;
   height: 28px;
 `;
+
 const H1 = styled.h1`
   color: #292929;
   font-size: 24px;
@@ -104,6 +112,7 @@ const H1 = styled.h1`
   line-height: 32px;
   word-wrap: break-word;
 `;
+
 const TextBold = styled.div`
   color: #292929;
   font-size: 14px;
@@ -111,6 +120,7 @@ const TextBold = styled.div`
   line-height: 24px;
   word-wrap: break-word;
 `;
+
 const UserChip = styled.div`
   display: flex;
   flex-direction: row;
@@ -119,6 +129,7 @@ const UserChip = styled.div`
   border-radius: 32px;
   background: #ebebeb;
 `;
+
 const UserChipLink = styled.a`
   display: flex;
   flex-direction: row;
@@ -126,10 +137,12 @@ const UserChipLink = styled.a`
   gap: 4px;
   border-radius: 32px;
   background: #ebebeb;
+
   &:hover {
     text-decoration: none;
   }
 `;
+
 const ShareText = styled.div`
   color: #7b7b7b;
   font-size: 14px;
@@ -137,6 +150,7 @@ const ShareText = styled.div`
   line-height: 24px;
   word-wrap: break-word;
 `;
+
 const SocialIcon = styled.svg`
   width: 24px;
   height: 24px;
@@ -148,10 +162,12 @@ const SocialIcon = styled.svg`
     fill: #dd3345;
   }
 `;
+
 State.init({
   showBreakdown: false,
   successfulDonation,
 });
+
 const getProfileDataForSuccessfulDonation = (donation) => {
   const { donor_id, recipient_id, project_id } = donation;
   Near.asyncView("social.near", "get", {
@@ -161,16 +177,17 @@ const getProfileDataForSuccessfulDonation = (donation) => {
       keys: [`${donor_id}/profile/**`],
     }).then((donorData) => {
       State.update({
-        recipientProfile:
-          recipientData[recipient_id || project_id]?.profile || {},
+        recipientProfile: recipientData[recipient_id || project_id]?.profile || {},
         donorProfile: donorData[donor_id]?.profile || {},
       });
     });
   });
 };
+
 if (state.successfulDonation && !state.recipientProfile) {
   getProfileDataForSuccessfulDonation(state.successfulDonation);
 }
+
 if (props.isModalOpen && !state.successfulDonation) {
   const transactionHashes = props.transactionHashes.split(",");
   for (let i = 0; i < transactionHashes.length; i++) {
@@ -190,12 +207,9 @@ if (props.isModalOpen && !state.successfulDonation) {
     });
     // console.log("tx res: ", res);
     if (res.ok) {
-      const methodName =
-        res.body.result.transaction.actions[0].FunctionCall.method_name;
+      const methodName = res.body.result.transaction.actions[0].FunctionCall.method_name;
       const successVal = res.body.result.status?.SuccessValue;
-      let decoded = JSON.parse(
-        Buffer.from(successVal, "base64").toString("utf-8")
-      ); // atob not working
+      let decoded = JSON.parse(Buffer.from(successVal, "base64").toString("utf-8")); // atob not working
       if (methodName === "donate") {
         // NEAR donation
         State.update({
@@ -211,10 +225,9 @@ if (props.isModalOpen && !state.successfulDonation) {
       } else if (methodName === "ft_transfer_call") {
         // FT donation
         const args = JSON.parse(
-          Buffer.from(
-            res.body.result.transaction.actions[0].FunctionCall.args,
-            "base64"
-          ).toString("utf-8")
+          Buffer.from(res.body.result.transaction.actions[0].FunctionCall.args, "base64").toString(
+            "utf-8"
+          )
         );
         const signerId = res.body.result.transaction.signer_id;
         Near.asyncView(DONATION_CONTRACT_ID, "get_donations_for_donor", {
@@ -222,9 +235,7 @@ if (props.isModalOpen && !state.successfulDonation) {
         })
           .then((donations) => {
             if (donations.length) {
-              const donation = donations.sort(
-                (a, b) => b.donated_at_ms - a.donated_at_ms
-              )[0];
+              const donation = donations.sort((a, b) => b.donated_at_ms - a.donated_at_ms)[0];
               State.update({
                 successfulDonation: donation,
               });
@@ -241,12 +252,11 @@ if (props.isModalOpen && !state.successfulDonation) {
     }
   }
 }
-const ftMetadata = Near.view(
-  state.successfulDonation?.ft_id,
-  "ft_metadata",
-  {}
-);
+
+const ftMetadata = Near.view(state.successfulDonation?.ft_id, "ft_metadata", {});
+
 // console.log("state in modal success: ", state);
+
 const twitterIntent = useMemo(() => {
   if (!state.recipientProfile) return;
   const twitterIntentBase = "https://twitter.com/intent/tweet?text=";
@@ -262,13 +272,9 @@ const twitterIntent = useMemo(() => {
   } on @${POTLOCK_TWITTER_ACCOUNT_ID}! Support public goods at `;
   text = encodeURIComponent(text);
   url = encodeURIComponent(url);
-  return (
-    twitterIntentBase +
-    text +
-    `&url=${url}` +
-    `&hashtags=${DEFAULT_SHARE_HASHTAGS.join(",")}`
-  );
+  return twitterIntentBase + text + `&url=${url}` + `&hashtags=${DEFAULT_SHARE_HASHTAGS.join(",")}`;
 }, [state.successfulDonation, state.recipientProfile]);
+
 return (
   <Widget
     src={`${ownerId}/widget/Components.Modal`}
@@ -281,20 +287,14 @@ return (
         <>
           <ModalMain>
             <H1>Thank you for applying!</H1>
-            <TextBold>
-              Your application status: {state.successfulApplication.status}
-            </TextBold>
+            <TextBold>Your application status: {state.successfulApplication.status}</TextBold>
           </ModalMain>
         </>
       ) : state.successfulDonation ? (
         <>
           <ModalMain>
             <HeaderIcon>
-              <svg
-                viewBox="0 0 12 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg viewBox="0 0 12 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M6.39016 7.9C4.12016 7.31 3.39016 6.7 3.39016 5.75C3.39016 4.66 4.40016 3.9 6.09016 3.9C7.87016 3.9 8.53016 4.75 8.59016 6H10.8002C10.7302 4.28 9.68016 2.7 7.59016 2.19V0H4.59016V2.16C2.65016 2.58 1.09016 3.84 1.09016 5.77C1.09016 8.08 3.00016 9.23 5.79016 9.9C8.29016 10.5 8.79016 11.38 8.79016 12.31C8.79016 13 8.30016 14.1 6.09016 14.1C4.03016 14.1 3.22016 13.18 3.11016 12H0.910156C1.03016 14.19 2.67016 15.42 4.59016 15.83V18H7.59016V15.85C9.54016 15.48 11.0902 14.35 11.0902 12.3C11.0902 9.46 8.66016 8.49 6.39016 7.9Z"
                   fill="white"
@@ -316,11 +316,7 @@ return (
                 {ftMetadata?.icon ? (
                   <ImgIcon src={ftMetadata.icon} />
                 ) : (
-                  <NearIcon
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
+                  <NearIcon viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <rect width="24" height="24" rx="12" fill="#CECECE" />
                     <path
                       d="M15.616 6.61333L13.1121 10.3333C12.939 10.5867 13.2719 10.8933 13.5117 10.68L15.9756 8.53333C16.0422 8.48 16.1354 8.52 16.1354 8.61333V15.32C16.1354 15.4133 16.0155 15.4533 15.9623 15.3867L8.50388 6.45333C8.26415 6.16 7.91787 6 7.53163 6H7.26526C6.5727 6 6 6.57333 6 7.28V16.72C6 17.4267 6.5727 18 7.27858 18C7.71809 18 8.13097 17.7733 8.3707 17.3867L10.8746 13.6667C11.0477 13.4133 10.7148 13.1067 10.475 13.32L8.0111 15.4533C7.94451 15.5067 7.85128 15.4667 7.85128 15.3733V8.68C7.85128 8.58667 7.97114 8.54667 8.02442 8.61333L15.4828 17.5467C15.7225 17.84 16.0821 18 16.4551 18H16.7214C17.4273 18 18 17.4267 18 16.72V7.28C18 6.57333 17.4273 6 16.7214 6C16.2686 6 15.8557 6.22667 15.616 6.61333Z"
@@ -332,8 +328,7 @@ return (
               {state.successfulDonation?.total_amount &&
                 yoctosToUsd(state.successfulDonation.total_amount) && (
                   <AmountUsd>
-                    {yoctosToUsd(state.successfulDonation.total_amount) +
-                      " USD"}
+                    {yoctosToUsd(state.successfulDonation.total_amount) + " USD"}
                   </AmountUsd>
                 )}
             </Column>
@@ -342,8 +337,7 @@ return (
               <UserChipLink
                 href={props.hrefWithParams(
                   `?tab=project&projectId=${
-                    state.successfulDonation.recipient_id ||
-                    state.successfulDonation.project_id
+                    state.successfulDonation.recipient_id || state.successfulDonation.project_id
                   }`
                 )}
                 target="_blank"
@@ -381,9 +375,7 @@ return (
                 ftIcon: ftMetadata?.icon,
               }}
             />
-            <Row
-              style={{ width: "100%", justifyContent: "center", gap: "24px" }}
-            >
+            <Row style={{ width: "100%", justifyContent: "center", gap: "24px" }}>
               <Widget
                 src={`${ownerId}/widget/Components.Button`}
                 props={{
@@ -431,11 +423,7 @@ return (
             <Row style={{ gap: "8px", justifyContent: "center" }}>
               <ShareText>Share to</ShareText>
               <a href={twitterIntent} target="_blank">
-                <SocialIcon
-                  viewBox="0 0 21 17"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
+                <SocialIcon viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path
                     d="M20.92 2C20.15 2.35 19.32 2.58 18.46 2.69C19.34 2.16 20.02 1.32 20.34 0.31C19.51 0.81 18.59 1.16 17.62 1.36C16.83 0.5 15.72 0 14.46 0C12.11 0 10.19 1.92 10.19 4.29C10.19 4.63 10.23 4.96 10.3 5.27C6.74 5.09 3.57 3.38 1.46 0.79C1.09 1.42 0.88 2.16 0.88 2.94C0.88 4.43 1.63 5.75 2.79 6.5C2.08 6.5 1.42 6.3 0.84 6V6.03C0.84 8.11 2.32 9.85 4.28 10.24C3.65073 10.4122 2.9901 10.4362 2.35 10.31C2.62161 11.1625 3.15354 11.9084 3.87102 12.4429C4.5885 12.9775 5.45545 13.2737 6.35 13.29C4.83363 14.4904 2.954 15.1393 1.02 15.13C0.68 15.13 0.34 15.11 0 15.07C1.9 16.29 4.16 17 6.58 17C14.46 17 18.79 10.46 18.79 4.79C18.79 4.6 18.79 4.42 18.78 4.23C19.62 3.63 20.34 2.87 20.92 2Z"
                     fill="#7B7B7B"
