37b945babaa886a9ef51f132d512eb18f6f7437d
abdullahi3000.near <abdullahi3000.near>
2024-03-29 10:31:15 +0000

    Update components by abdullahi3000.near at 2024-03-29T10:31:15.051Z

--- abdullahi3000.near/components/source_code.jsx
+++ abdullahi3000.near/components/source_code.jsx
@@ -16,6 +16,11 @@ const { Modal } = VM.require("buildhub.near/widget/components.Modal");
 const { Hashtag } = VM.require("buildhub.near/widget/components.Hashtag");
 const { Tag } = VM.require("buildhub.near/widget/components.Tag");
 
+const accountId = "abdullahi3000.near";
+if (!accountId) {
+  return "No accountId";
+}
+
 function Pagination({
   totalPages,
   maxVisiblePages,
@@ -39,11 +44,15 @@ function Pagination({
 
 function Post(props) {
   return (
-    <Widget
-      loading={<div className="w-100" style={{ height: "200px" }} />}
-      src={"abdullahi3000.near/widget/components.Post"}
-      props={{ ...props }}
-    />
+    <>
+      {accountId == "abdullahi3000.near" && (
+        <Widget
+          loading={<div className="w-100" style={{ height: "200px" }} />}
+          src={"abdullahi3000.near/widget/components.Post"}
+          props={{ ...props }}
+        />
+      )}
+    </>
   );
 }
 
