650ea3c6b4bdd528c13d8ef0c07ccf71baff0267
potlock.near <potlock.near>
2024-03-29 14:20:36 +0000

    Update Profile.Tags by potlock.near at 2024-03-29T14:20:36.213Z

--- potlock.near/Profile.Tags/source_code.jsx
+++ potlock.near/Profile.Tags/source_code.jsx
@@ -1,7 +1,5 @@
 const { profile, projectId } = props;
-const { getTagsFromSocialProfileData } = VM.require(
-  "potlock.near/widget/utils"
-) || {
+const { getTagsFromSocialProfileData } = VM.require("potlock.near/widget/utils") || {
   getTagsFromSocialProfileData: () => [],
 };
 const Tags = styled.div`
@@ -10,6 +8,7 @@ const Tags = styled.div`
   flex-wrap: wrap;
   max-width: 600px;
 `;
+
 const Tag = styled.span`
   color: #292929;
   font-size: 14px;
@@ -19,8 +18,10 @@ const Tag = styled.span`
   background: #ebebeb;
   box-shadow: 0px -1px 0px 0px #dbdbdb inset, 0px 0px 0px 0.5px #dbdbdb;
 `;
+
 const tags = props.tags ?? getTagsFromSocialProfileData(profile);
 if (!tags.length) return "No tags";
+
 return (
   <Tags>
     {projectId && projectId.endsWith(".sputnik-dao.near") && <Tag>DAO</Tag>}
