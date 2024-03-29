815a82efdba60204658f9e7b1ace7be49e192b7c
potlock.near <potlock.near>
2024-03-29 14:20:36 +0000

    Update Components.Header by potlock.near at 2024-03-29T14:20:36.213Z

--- potlock.near/Components.Header/source_code.jsx
+++ potlock.near/Components.Header/source_code.jsx
@@ -1,19 +1,24 @@
 const { ownerId } = props;
+
 const headerTitleFontSizePx = 88;
+
 const HeaderContainer = styled.div`
   width: 100%;
   // background: #fffaf4;
   // background: white;
   padding: 80px 64px;
+
   @media (max-width: 768px) {
     padding: 36px 24px;
   }
 `;
+
 const HeaderContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: ${props.centered ? "center" : "flex-start"};
 `;
+
 const HeaderTitle = styled.div`
   color: #2e2e2e;
   font-size: ${headerTitleFontSizePx}px;
@@ -28,6 +33,7 @@ const HeaderTitle = styled.div`
     font-size: 48px;
   }
 `;
+
 const HeaderDescription = styled.div`
   color: #2e2e2e;
   font-size: 32px;
@@ -36,11 +42,13 @@ const HeaderDescription = styled.div`
   max-width: 866px;
   text-align: ${props.centered ? "center" : "flex-start"};
   margin-top: 32px;
+
   @media (max-width: 768px) {
     font-size: 24px;
     text-align: center;
   }
 `;
+
 const ButtonsContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -49,17 +57,22 @@ const ButtonsContainer = styled.div`
   gap: 32px;
   margin-top: 32px;
 `;
+
 const Underline = styled.div`
   position: absolute;
   top: ${headerTitleFontSizePx - 40}px;
   left: -40px;
   z-index: -1;
+
   @media (max-width: 768px) {
     top: 30px;
     left: -30px;
 `;
+
 const containerStyle = props.containerStyle ?? {};
+
 const showStats = !props.tab || props.tab == "projects";
+
 return (
   <HeaderContainer style={containerStyle}>
     <HeaderContent>
@@ -96,11 +109,6 @@ return (
       {props.buttonPrimary && props.buttonPrimary}
       {props.buttonSecondary && props.buttonSecondary}
     </ButtonsContainer>
-    {showStats && (
-      <Widget
-        src={`${ownerId}/widget/Project.DonationStats`}
-        props={{ ...props }}
-      />
-    )}
+    {showStats && <Widget src={`${ownerId}/widget/Project.DonationStats`} props={{ ...props }} />}
   </HeaderContainer>
 );
