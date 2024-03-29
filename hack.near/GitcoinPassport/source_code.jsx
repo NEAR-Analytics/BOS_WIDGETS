910f43e2007f9516f28d0c3243831af53664a6c0
hack.near <hack.near>
2024-03-29 20:57:10 +0000

    Update GitcoinPassport by hack.near at 2024-03-29T20:57:10.594Z

--- hack.near/GitcoinPassport/source_code.jsx
+++ hack.near/GitcoinPassport/source_code.jsx
@@ -8,7 +8,7 @@ const verified = props.verified ?? true;
 const content = verified ? (
   <span style={{ verticalAlign: center }}>
     <img
-      style={{ width: "19px" }}
+      style={{ width: "15px" }}
       className="ms-2"
       src="https://imgs.search.brave.com/z-VfPT3iu6hc5oeCl1ZgqmRQjyoklvshvx5M21Xneao/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMtZ2xvYmFsLndl/YnNpdGUtZmlsZXMu/Y29tLzY1YzIxOWUz/NGU5MDQ5ZjQyNmI2/ODUxOC82NWRjOTdl/YmQ0ZWVkNjlkOTUy/Yjg4NDBfUGFzc3Bv/cnRfTG9nb21hcmtf/SXJpc0ljZUJsdWVf/R3JhZGllbnQuc3Zn.svg"
     />
