2aa960ec2004378438187f8c251fdecb4eef42b3
potlock.near <potlock.near>
2024-03-29 14:20:36 +0000

    Update SDK.cart by potlock.near at 2024-03-29T14:20:36.213Z

--- potlock.near/SDK.cart/source_code.jsx
+++ potlock.near/SDK.cart/source_code.jsx
@@ -1,35 +1,31 @@
 /**
  * Items must have "id" and "price" properties
  */
+
 const CART_KEY = "cart";
 const DEFAULT_CART = {};
+
 return {
   getCart: () => {
-    const cart = JSON.parse(
-      Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART)
-    );
+    const cart = JSON.parse(Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART));
     return cart;
   },
   getCartItemCount: () => {
-    const cart = JSON.parse(
-      Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART)
-    );
+    const cart = JSON.parse(Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART));
     return Object.keys(cart).length;
   },
   getCartTotal: () => {
-    const cart = JSON.parse(
-      Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART)
-    );
+    const cart = JSON.parse(Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART));
     let total = 0;
+
     Object.values(cart).forEach((item) => {
       total += item.price;
     });
+
     return total;
   },
   addItemsToCart: (items) => {
-    const cart = JSON.parse(
-      Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART)
-    );
+    const cart = JSON.parse(Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART));
     if (Array.isArray(items)) {
       items.forEach((item) => {
         cart[item.id] = item;
@@ -40,12 +36,11 @@ return {
       console.log("Attempted to add invalid item to cart, item: ", items);
       return;
     }
+
     Storage.set(CART_KEY, JSON.stringify(cart));
   },
   removeItemsFromCart: (items) => {
-    const cart = JSON.parse(
-      Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART)
-    );
+    const cart = JSON.parse(Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART));
     if (Array.isArray(items)) {
       items.forEach((item) => {
         delete cart[item.id];
@@ -56,15 +51,14 @@ return {
       console.log("Attempted to remove invalid item from cart, item: ", items);
       return;
     }
+
     Storage.set(CART_KEY, JSON.stringify(cart));
   },
   clearCart: () => {
     Storage.set(CART_KEY, JSON.stringify(DEFAULT_CART));
   },
   updateItemInCart: (item) => {
-    const cart = JSON.parse(
-      Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART)
-    );
+    const cart = JSON.parse(Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART));
     if (cart[item.id]) {
       cart[item.id] = item; // Update the item in the cart directly
       Storage.set(CART_KEY, JSON.stringify(cart));
@@ -74,9 +68,7 @@ return {
     console.log("cart", cart);
   },
   itemExistsInCart: (item) => {
-    const cart = JSON.parse(
-      Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART)
-    );
+    const cart = JSON.parse(Storage.get(CART_KEY) ?? JSON.stringify(DEFAULT_CART));
     return cart[item.id] !== undefined;
   },
 };
