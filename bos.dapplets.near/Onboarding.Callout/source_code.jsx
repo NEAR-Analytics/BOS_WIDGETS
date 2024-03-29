ff6058fc09fb0a987c74fee6d1d513f6b04024b4
bos.dapplets.near <bos.dapplets.near>
2024-03-29 00:22:55 +0000

    Update Onboarding.Callout by bos.dapplets.near at 2024-03-29T00:22:55.099Z

--- bos.dapplets.near/Onboarding.Callout/source_code.jsx
+++ bos.dapplets.near/Onboarding.Callout/source_code.jsx
@@ -1,3 +1,37 @@
+const Callout = styled.div`
+  display: flex;
+  width: 320px;
+  padding: 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  border-radius: 10px;
+  border: 1px solid #02193a;
+  background: #fffffe;
+  position: relative;
+  left: 19px;
+`;
+
+const CalloutTitle = styled.div`
+  color: #02193a;
+  font-family: sans-serif;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 149%; /* 26.82px */
+`;
+
+const CalloutDescription = styled.div`
+  color: #7a818b;
+  text-align: center;
+  font-family: sans-serif;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 149%; /* 17.88px */
+`;
+
 const CalloutArrowIcon = () => (
   <svg
     width="38"
