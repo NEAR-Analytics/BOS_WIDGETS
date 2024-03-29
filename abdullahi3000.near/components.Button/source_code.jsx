6a289ea927496947f296b7995997d6cc4ca3098e
abdullahi3000.near <abdullahi3000.near>
2024-03-29 13:37:18 +0000

    Update components.Button by abdullahi3000.near at 2024-03-29T13:37:18.874Z

--- abdullahi3000.near/components.Button/source_code.jsx
+++ abdullahi3000.near/components.Button/source_code.jsx
@@ -42,7 +42,7 @@ const StyledButton = styled.button`
   color: ${(props) => {
     switch (props.variant) {
       case "primary":
-        return "var(--button-primary-color, #fff)";
+        return "var(--button-primary-color, #000)";
       case "outline":
         return "var(--button-outline-color, #fff)";
       case "secondary":
