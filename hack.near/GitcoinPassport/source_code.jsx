fdc1c8e748e704f1c427bef013b62a9b65c1eb50
hack.near <hack.near>
2024-03-29 20:46:54 +0000

    Update GitcoinPassport by hack.near at 2024-03-29T20:46:54.423Z

--- hack.near/GitcoinPassport/source_code.jsx
+++ hack.near/GitcoinPassport/source_code.jsx
@@ -3,12 +3,13 @@ if (!accountId) {
   return "";
 }
 
-const verified = props.verified ?? false;
+const verified = props.verified ?? true;
 
 const content = verified ? (
   <span style={{ verticalAlign: center }}>
     <img
       style={{ width: "19px" }}
+      className="ms-1"
       src="https://imgs.search.brave.com/z-VfPT3iu6hc5oeCl1ZgqmRQjyoklvshvx5M21Xneao/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMtZ2xvYmFsLndl/YnNpdGUtZmlsZXMu/Y29tLzY1YzIxOWUz/NGU5MDQ5ZjQyNmI2/ODUxOC82NWRjOTdl/YmQ0ZWVkNjlkOTUy/Yjg4NDBfUGFzc3Bv/cnRfTG9nb21hcmtf/SXJpc0ljZUJsdWVf/R3JhZGllbnQuc3Zn.svg"
     />
   </span>
@@ -21,7 +22,7 @@ return (
     loading={content}
     src="mob.near/widget/N.Common.OverlayTrigger"
     props={{
-      popup: <div>Verified</div>,
+      popup: <div>verified</div>,
       children: content,
     }}
   />
