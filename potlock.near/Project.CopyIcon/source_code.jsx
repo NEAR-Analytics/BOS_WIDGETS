ed2d570906d0bd31b5dbab12757028d879f3599e
potlock.near <potlock.near>
2024-03-29 14:20:36 +0000

    Update Project.CopyIcon by potlock.near at 2024-03-29T14:20:36.213Z

--- potlock.near/Project.CopyIcon/source_code.jsx
+++ potlock.near/Project.CopyIcon/source_code.jsx
@@ -1,5 +1,6 @@
 const { textToCopy, customStyle } = props;
 const [copied, setCopid] = useState(false);
+
 const Icon = styled.svg`
   cursor: pointer;
   height: 20px;
@@ -9,14 +10,9 @@ const Icon = styled.svg`
     scale: 1.1;
   }
 `;
+
 return copied ? (
-  <svg
-    width="18"
-    height="14"
-    viewBox="0 0 18 14"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M5.8002 10.9L1.6002 6.69999L0.200195 8.09999L5.8002 13.7L17.8002 1.69999L16.4002 0.299988L5.8002 10.9Z"
       fill="#151A23"
