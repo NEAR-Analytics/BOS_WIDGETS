02e8c061495ff3ffa377aec3b0a8717746e0c888
mob.near <mob.near>
2024-03-29 21:20:55 +0000

    Update Tokens.Top by mob.near at 2024-03-29T21:20:55.707Z

---
+++ mob.near/Tokens.Top/source_code.jsx
@@ -0,0 +1,27 @@
+const [tokenId, setTokenId] = useState(
+  props.tokenId || context.tokenId || "wrap.near"
+);
+const [inputActionId, setInputActionId] = useState(tokenId);
+
+return (
+  <div>
+    <div className="input-group mb-3">
+      <input
+        className="form-control"
+        value={inputActionId}
+        onChange={(e) => setInputActionId(e.target.value)}
+      />
+      <a
+        className={`btn ${
+          tokenId === inputActionId ? "btn-outline-secondary" : "btn-primary"
+        }`}
+        onClick={() => setTokenId(inputActionId)}
+        href={`/mob.near/widget/Tokens.Top?tokenId=${inputActionId}`}
+        type="button"
+      >
+        Apply
+      </a>
+    </div>
+    <Widget src="mob.near/widget/Tokens.Top.Inner" props={{ tokenId }} />
+  </div>
+);
