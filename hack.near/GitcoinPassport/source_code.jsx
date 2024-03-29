ed946b19c6df31127dcc6d2cdd3cf266e35b4a3c
hack.near <hack.near>
2024-03-29 20:56:03 +0000

    Update GitcoinPassport by hack.near at 2024-03-29T20:56:03.527Z

--- hack.near/GitcoinPassport/source_code.jsx
+++ hack.near/GitcoinPassport/source_code.jsx
@@ -6,11 +6,13 @@ if (!accountId) {
 const verified = props.verified ?? true;
 
 const content = verified ? (
-  <img
-    style={{ width: "19px" }}
-    className="ms-1"
-    src="https://imgs.search.brave.com/z-VfPT3iu6hc5oeCl1ZgqmRQjyoklvshvx5M21Xneao/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMtZ2xvYmFsLndl/YnNpdGUtZmlsZXMu/Y29tLzY1YzIxOWUz/NGU5MDQ5ZjQyNmI2/ODUxOC82NWRjOTdl/YmQ0ZWVkNjlkOTUy/Yjg4NDBfUGFzc3Bv/cnRfTG9nb21hcmtf/SXJpc0ljZUJsdWVf/R3JhZGllbnQuc3Zn.svg"
-  />
+  <span style={{ verticalAlign: center }}>
+    <img
+      style={{ width: "19px" }}
+      className="ms-2"
+      src="https://imgs.search.brave.com/z-VfPT3iu6hc5oeCl1ZgqmRQjyoklvshvx5M21Xneao/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMtZ2xvYmFsLndl/YnNpdGUtZmlsZXMu/Y29tLzY1YzIxOWUz/NGU5MDQ5ZjQyNmI2/ODUxOC82NWRjOTdl/YmQ0ZWVkNjlkOTUy/Yjg4NDBfUGFzc3Bv/cnRfTG9nb21hcmtf/SXJpc0ljZUJsdWVf/R3JhZGllbnQuc3Zn.svg"
+    />
+  </span>
 ) : (
   ""
 );
