8c18e0fd62db662ad02215ba5a4405ee1163b306
bos.dapplets.near <bos.dapplets.near>
2024-03-29 08:51:43 +0000

    Update Onboarding.Callout by bos.dapplets.near at 2024-03-29T08:51:43.968Z

--- bos.dapplets.near/Onboarding.Callout/source_code.jsx
+++ bos.dapplets.near/Onboarding.Callout/source_code.jsx
@@ -32,45 +32,13 @@ const CalloutDescription = styled.div`
   line-height: 149%; /* 17.88px */
 `;
 
-const CalloutArrowIcon = () => (
-  <svg
-    width="38"
-    height="38"
-    viewBox="0 0 38 38"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M4.25195 15.6491L19.0683 1.77606L19.0683 36.0052L4.26223 22.2284C2.35353 20.4524 2.34881 17.4311 4.25195 15.6491Z"
-      fill="#FFFFFE"
-      stroke="#02193A"
-    />
-    <path
-      d="M22.0636 34.8717L18.5687 34.8717L17.5687 33.8717L17.5687 3.87175L18.5687 2.87175L22.0636 2.87175L22.0636 34.8717Z"
-      fill="#FFFFFE"
-    />
-  </svg>
-);
-
-const overlay = (
-  <DappletCallout arrow={<CalloutArrowIcon />}>
-    <Callout>
-      <CalloutTitle>It's a sandbox story first</CalloutTitle>
-      <CalloutDescription>
-        We are now in a sandbox mutation. Through it, we can suspendisse mattis
-        interdum auctor volutpat nisl quis. Scelerisque morbi eget volutpat
-        aliquet vitae curabitur non.
-      </CalloutDescription>
-    </Callout>
-  </DappletCallout>
-);
-
-const child = props.children.filter(
-  (c) => typeof c !== "string" || !!c.trim()
-)[0];
-
 return (
-  <OverlayTrigger placement="right" overlay={overlay}>
-    {child}
-  </OverlayTrigger>
+  <Callout>
+    <CalloutTitle>It's a sandbox story first</CalloutTitle>
+    <CalloutDescription>
+      We are now in a sandbox mutation. Through it, we can suspendisse mattis
+      interdum auctor volutpat nisl quis. Scelerisque morbi eget volutpat
+      aliquet vitae curabitur non.
+    </CalloutDescription>
+  </Callout>
 );
