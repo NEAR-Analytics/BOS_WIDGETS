7c73f682a4d4478099bf6c962cc4e278c4a1586f
potlock.near <potlock.near>
2024-03-29 14:20:36 +0000

    Update Components.DonorsUtils by potlock.near at 2024-03-29T14:20:36.213Z

--- potlock.near/Components.DonorsUtils/source_code.jsx
+++ potlock.near/Components.DonorsUtils/source_code.jsx
@@ -1,32 +1,35 @@
 // Get the current date in the local time zone
 const currentDate = new Date();
+
 // Calculate the time zone offset in milliseconds
 let localTimeZoneOffsetMinutes = currentDate.getTimezoneOffset();
 localTimeZoneOffsetMinutes = localTimeZoneOffsetMinutes * 60 * 1000;
+
 const oneDayTime = 24 * 60 * 60 * 1000;
 const currentTimestamp = new Date().getTime();
+
 const yesterday = currentTimestamp - oneDayTime;
 const lastWeek = currentTimestamp - oneDayTime * 7;
 const lastMonth = currentTimestamp - oneDayTime * 30;
 const lastYear = currentTimestamp - oneDayTime * 365;
+
 const getTimePassed = (timestamp, abbreviate) => {
   // Calculate the difference in milliseconds
   const timePassed = currentTimestamp - timestamp;
+
   // Convert milliseconds to seconds, minutes, hours, etc.
   const secondsPassed = Math.floor(timePassed / 1000);
   const minutesPassed = Math.floor(secondsPassed / 60);
   const hoursPassed = Math.floor(minutesPassed / 60);
   const daysPassed = Math.floor(hoursPassed / 24);
+
   let time = 0;
+
   // Display the time passed conditionally
   if (daysPassed > 0) {
-    time = !abbreviate
-      ? `${daysPassed} day${daysPassed === 1 ? "" : "s"}`
-      : `${daysPassed}d`;
+    time = !abbreviate ? `${daysPassed} day${daysPassed === 1 ? "" : "s"}` : `${daysPassed}d`;
   } else if (hoursPassed > 0) {
-    time = !abbreviate
-      ? `${hoursPassed} hour${hoursPassed === 1 ? "" : "s"}`
-      : `${hoursPassed}h`;
+    time = !abbreviate ? `${hoursPassed} hour${hoursPassed === 1 ? "" : "s"}` : `${hoursPassed}h`;
   } else if (minutesPassed > 0) {
     time = !abbreviate
       ? `${minutesPassed} minute${minutesPassed === 1 ? "" : "s"}`
@@ -38,11 +41,13 @@ const getTimePassed = (timestamp, abbreviate) => {
   }
   return time;
 };
+
 const _address = (address, max) => {
   const limit = max || 10;
   if (address.length > limit) return address.slice(0, limit) + "...";
   else return address;
 };
+
 const reverseArr = (input) => {
   var ret = new Array();
   for (var i = input.length - 1; i >= 0; i--) {
@@ -50,16 +55,17 @@ const reverseArr = (input) => {
   }
   return ret;
 };
+
 const calcNetDonationAmount = (donation) => {
   const lastDonationAmount = Big(
-    donation.total_amount -
-      (donation.referrer_fee || 0) -
-      (donation.protocol_fee || 0)
+    donation.total_amount - (donation.referrer_fee || 0) - (donation.protocol_fee || 0)
   ).div(Big(1e24));
   return parseFloat(lastDonationAmount);
 };
+
 const filterByDate = (filter, donation) => {
   const donateAt = donation.donated_at_ms || donation.donated_at;
+
   switch (filter) {
     case "day":
       if (donateAt > yesterday) return true;
@@ -75,10 +81,12 @@ const filterByDate = (filter, donation) => {
       return false;
     case "all":
       return true;
+
     default:
       return true;
   }
 };
+
 return {
   getTimePassed,
   filterByDate,
