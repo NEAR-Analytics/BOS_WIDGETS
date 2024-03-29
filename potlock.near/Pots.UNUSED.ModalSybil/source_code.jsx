4806df75d89ef9afc3372866274ccdfcf80cde35
potlock.near <potlock.near>
2024-03-29 14:20:36 +0000

    Update Pots.UNUSED.ModalSybil by potlock.near at 2024-03-29T14:20:36.213Z

--- potlock.near/Pots.UNUSED.ModalSybil/source_code.jsx
+++ potlock.near/Pots.UNUSED.ModalSybil/source_code.jsx
@@ -1,7 +1,5 @@
 const { isModalOpen, onClose } = props;
-const { NADA_BOT_URL, ownerId } = VM.require(
-  "potlock.near/widget/constants"
-) || {
+const { NADA_BOT_URL, ownerId } = VM.require("potlock.near/widget/constants") || {
   ownerId: "",
   NADA_BOT_URL: "",
 };
@@ -10,8 +8,8 @@ const CLOSE_ICON_URL =
   IPFS_BASE_URL + "bafkreifyg2vvmdjpbhkylnhye5es3vgpsivhigkjvtv2o4pzsae2z4vi5i";
 const ERROR_ICON_URL =
   IPFS_BASE_URL + "bafkreicqarojxk6jhdtsk2scfsmnigqpxjfgar6om4wlhn5xmqbbu74u5i";
-const IMAGE_URL =
-  IPFS_BASE_URL + "bafkreidwashbfmlr7qo2yoqcfdsqi4ouisgt6h6jwxymz53v2f7hhoy75a";
+const IMAGE_URL = IPFS_BASE_URL + "bafkreidwashbfmlr7qo2yoqcfdsqi4ouisgt6h6jwxymz53v2f7hhoy75a";
+
 const ModalHeader = styled.div`
   width: 100%;
   padding: 10px 20px;
@@ -20,16 +18,19 @@ const ModalHeader = styled.div`
   align-items: center;
   background: #f6f5f3;
 `;
+
 const ModalHeaderText = styled.div`
   font-size: 17px;
   font-weight: 600;
   line-height: 24px;
   color: #292929;
 `;
+
 const Icon = styled.img`
   width: 24px;
   height: 24px;
 `;
+
 const ModalContent = styled.div`
   padding: 16px 20px 32px 20px;
   display: flex;
@@ -38,12 +39,14 @@ const ModalContent = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 const Row = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
 `;
+
 const ContentHeaderText = styled.div`
   margin-left: 24px;
   font-size: 32px;
@@ -52,22 +55,27 @@ const ContentHeaderText = styled.div`
   color: #525252;
   font-family: "Lora";
 `;
+
 const ContentDescriptionText = styled.div`
   font-size: 16px;
   font-weight: 400;
   line-height: 28px;
 `;
+
 const ErrorSpan = styled.span`
   color: #dd3345;
 `;
+
 const BoldSpan = styled.span`
   font-weight: 600;
 `;
+
 const Image = styled.img`
   width: 100px;
   max-height: 245px;
   margin: 0px 32px;
 `;
+
 return (
   <Widget
     src={`${ownerId}/widget/Components.Modal`}
@@ -84,18 +92,15 @@ return (
           </ModalHeader>
           <ModalContent>
             <Row>
-              <Icon
-                style={{ width: "35px", height: "35px" }}
-                src={ERROR_ICON_URL}
-              />
+              <Icon style={{ width: "35px", height: "35px" }} src={ERROR_ICON_URL} />
               <ContentHeaderText>
                 You Are <ErrorSpan>Not verified</ErrorSpan>
               </ContentHeaderText>
             </Row>
             <Row>
               <ContentDescriptionText>
-                To donate to a project via this pot, you need to get verified
-                from <BoldSpan>Nada.bot</BoldSpan>
+                To donate to a project via this pot, you need to get verified from{" "}
+                <BoldSpan>Nada.bot</BoldSpan>
               </ContentDescriptionText>
             </Row>
             <Image src={IMAGE_URL} />
