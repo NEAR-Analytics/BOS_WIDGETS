17c366489d3f76afb15b1bfc96209e4780bbf562
hack.near <hack.near>
2024-03-29 20:42:11 +0000

    Update GitcoinPassport by hack.near at 2024-03-29T20:42:11.752Z

---
+++ hack.near/GitcoinPassport/source_code.jsx
@@ -0,0 +1,28 @@
+const accountId = props.accountId;
+if (!accountId) {
+  return "";
+}
+
+const verified = props.verified ?? false;
+
+const content = verified ? (
+  <span style={{ verticalAlign: center }}>
+    <img
+      style={{ width: "19px" }}
+      src="https://imgs.search.brave.com/z-VfPT3iu6hc5oeCl1ZgqmRQjyoklvshvx5M21Xneao/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMtZ2xvYmFsLndl/YnNpdGUtZmlsZXMu/Y29tLzY1YzIxOWUz/NGU5MDQ5ZjQyNmI2/ODUxOC82NWRjOTdl/YmQ0ZWVkNjlkOTUy/Yjg4NDBfUGFzc3Bv/cnRfTG9nb21hcmtf/SXJpc0ljZUJsdWVf/R3JhZGllbnQuc3Zn.svg"
+    />
+  </span>
+) : (
+  ""
+);
+
+return (
+  <Widget
+    loading={content}
+    src="mob.near/widget/N.Common.OverlayTrigger"
+    props={{
+      popup: <div>Verified</div>,
+      children: content,
+    }}
+  />
+);
