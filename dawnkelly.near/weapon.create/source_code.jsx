bdf01d8f692d221a2856ea89257f29aa0e273190
dawnkelly.near <dawnkelly.near>
2024-03-30 16:29:23 +0000

    Update weapon.create by dawnkelly.near at 2024-03-30T16:29:23.065Z

--- dawnkelly.near/weapon.create/source_code.jsx
+++ dawnkelly.near/weapon.create/source_code.jsx
@@ -1,21 +1,150 @@
 const accountId = context.accountId;
 
 const [ammunition, setAmmunition] = useState("");
-{
-  /** 
 const [finesse, setFinesse] = useState("");
 const [heavy, setHeavy] = useState("");
 const [light, setLight] = useState("");
 const [loading, setLoading] = useState("");
-const [range, setRange] = useState("");
 const [reach, setReach] = useState("");
 const [special, setSpecial] = useState("");
 const [thrown, setThrown] = useState("");
 const [twoHanded, setTwoHanded] = useState("");
 const [versatile, setVersatile] = useState("");
 const [silvered, setSilvered] = useState("");
-*/
-}
+
+// handlers for property selector buttons
+
+const handleAmmunitionChange = (event) => {
+  const newValue = event.target.value;
+  setAmmunition(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      ammunition: newValue === "yes" ? "yes" : prevWeapon.metadata.ammunition,
+    },
+  }));
+};
+
+const handleFinesseChange = (event) => {
+  const newValue = event.target.value;
+  setFinesse(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      finesse: newValue === "yes" ? "yes" : prevWeapon.metadata.finesse,
+    },
+  }));
+};
+
+const handleHeavyChange = (event) => {
+  const newValue = event.target.value;
+  setHeavy(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      heavy: newValue === "yes" ? "yes" : prevWeapon.metadata.heavy,
+    },
+  }));
+};
+
+const handleLightChange = (event) => {
+  const newValue = event.target.value;
+  setLight(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      light: newValue === "yes" ? "yes" : prevWeapon.metadata.light,
+    },
+  }));
+};
+
+const handleLoadingChange = (event) => {
+  const newValue = event.target.value;
+  setLoading(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      loading: newValue === "yes" ? "yes" : prevWeapon.metadata.loading,
+    },
+  }));
+};
+
+const handleReachChange = (event) => {
+  const newValue = event.target.value;
+  setReach(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      reach: newValue === "yes" ? "yes" : prevWeapon.metadata.reach,
+    },
+  }));
+};
+
+const handleSpecialChange = (event) => {
+  const newValue = event.target.value;
+  setSpecial(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      special: newValue === "yes" ? "yes" : prevWeapon.metadata.special,
+    },
+  }));
+};
+
+const handleThrownChange = (event) => {
+  const newValue = event.target.value;
+  setThrown(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      thrown: newValue === "yes" ? "yes" : prevWeapon.metadata.thrown,
+    },
+  }));
+};
+
+const handleTwoHandedChange = (event) => {
+  const newValue = event.target.value;
+  setTwoHanded(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      twoHanded: newValue === "yes" ? "yes" : prevWeapon.metadata.twoHanded,
+    },
+  }));
+};
+
+const handleSilveredChange = (event) => {
+  const newValue = event.target.value;
+  setSilvered(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      silvered: newValue === "yes" ? "yes" : prevWeapon.metadata.silvered,
+    },
+  }));
+};
+
+const handleVersatileChange = (event) => {
+  const newValue = event.target.value;
+  setVersatile(newValue);
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: {
+      ...prevWeapon.metadata,
+      versatile: newValue === "yes" ? "yes" : prevWeapon.metadata.versatile,
+    },
+  }));
+};
 
 const [weapon, setWeapon] = useState(() => {
   const initialData = Social.getr(`${accountId}/game/weapon`) ?? {
@@ -28,7 +157,7 @@ const [weapon, setWeapon] = useState(() => {
       weight: "",
       cost: "",
       damage: "",
-      ammunition: "",
+
       image: {},
     },
   };
@@ -58,15 +187,19 @@ const handleDescriptionChange = (e) => {
   }));
 };
 
-const handleAmmunitionChange = (event) => {
-  const newValue = event.target.value;
-  setAmmunition(newValue);
+const handleMoralsChange = (e) => {
+  const newMorals = e.target.value;
   setWeapon((prevWeapon) => ({
     ...prevWeapon,
-    metadata: {
-      ...prevWeapon.metadata,
-      ammunition: newValue === "yes" ? "yes" : prevWeapon.metadata.ammunition,
-    },
+    metadata: { ...prevWeapon.metadata, morals: newMorals },
+  }));
+};
+
+const handleEthicsChange = (e) => {
+  const newEthics = e.target.value;
+  setWeapon((prevWeapon) => ({
+    ...prevWeapon,
+    metadata: { ...prevWeapon.metadata, ethics: newEthics },
   }));
 };
 
@@ -141,6 +274,7 @@ return (
             name="type"
             id="type"
             type="text"
+            className="form-select"
             placeholder="What is the weapon type (club, rapier, etc)"
             value={weapon.metadata.type}
             onChange={handleTypeChange}
@@ -159,6 +293,7 @@ return (
         }}
       />
     </div>
+
     <div className="mb-3 p-1">
       <label for="description">Description:</label>
       <input
@@ -172,6 +307,7 @@ return (
         onChange={handleDescriptionChange}
       />
     </div>
+
     <div className="mb-3 p-1">
       <h5 className="mb-2 d-block">Class & Category</h5>
       <div className="row align-items-center">
@@ -240,6 +376,7 @@ return (
         />
       </div>
     </div>
+
     <div className="mb-3 p-1">
       <h5>Weapon Properties:</h5>
       <p>Select 'yes' for properties you want to assign to this weapon.</p>
@@ -278,9 +415,316 @@ return (
           </div>
         </div>
       </div>
+
+      <div className="row align-items-center">
+        <div className="col">
+          <span>Finesse:</span>
+        </div>
+        <div className="col">
+          <div className="btn-group" role="group">
+            <input
+              type="radio"
+              name="finesse"
+              id="yesFinesse"
+              value="yes"
+              className="btn-check"
+              checked={finesse === "yes"}
+              onChange={handleFinesseChange}
+            />
+            <label htmlFor="yesFinesse" className="btn btn-outline-primary">
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              name="finesse"
+              id="noFinesse"
+              value="no"
+              className="btn-check"
+              checked={finesse === "no"}
+              onChange={handleFinesseChange}
+            />
+            <label htmlFor="noFinesse" className="btn btn-outline-primary">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col">
+          <span>Heavy:</span>
+        </div>
+        <div className="col">
+          <div className="btn-group" role="group">
+            <input
+              type="radio"
+              name="heavy"
+              id="yesHeavy"
+              value="yes"
+              className="btn-check"
+              checked={heavy === "yes"}
+              onChange={handleHeavyChange}
+            />
+            <label htmlFor="yesHeavy" className="btn btn-outline-primary">
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              name="heavy"
+              id="noHeavy"
+              value="no"
+              className="btn-check"
+              checked={heavy === "no"}
+              onChange={handleHeavyChange}
+            />
+            <label htmlFor="noHeavy" className="btn btn-outline-primary">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col">
+          <span>Loading:</span>
+        </div>
+        <div className="col">
+          <div className="btn-group" role="group">
+            <input
+              type="radio"
+              name="loading"
+              id="yesLoading"
+              value="yes"
+              className="btn-check"
+              checked={loading === "yes"}
+              onChange={handleLoadingChange}
+            />
+            <label htmlFor="yesLoading" className="btn btn-outline-primary">
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              name="loading"
+              id="noLoading"
+              value="no"
+              className="btn-check"
+              checked={loading === "no"}
+              onChange={handleLoadingChange}
+            />
+            <label htmlFor="noLoading" className="btn btn-outline-primary">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col">
+          <span>Reach:</span>
+        </div>
+        <div className="col">
+          <div className="btn-group" role="group">
+            <input
+              type="radio"
+              name="reach"
+              id="yesReach"
+              value="yes"
+              className="btn-check"
+              checked={reach === "yes"}
+              onChange={handleReachChange}
+            />
+            <label htmlFor="yesReach" className="btn btn-outline-primary">
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              name="reach"
+              id="noReach"
+              value="no"
+              className="btn-check"
+              checked={reach === "no"}
+              onChange={handleReachChange}
+            />
+            <label htmlFor="noReach" className="btn btn-outline-primary">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col">
+          <span>Special:</span>
+        </div>
+        <div className="col">
+          <div className="btn-group" role="group">
+            <input
+              type="radio"
+              name="special"
+              id="yesSpecial"
+              value="yes"
+              className="btn-check"
+              checked={special === "yes"}
+              onChange={handleSpecialChange}
+            />
+            <label htmlFor="yesSpecial" className="btn btn-outline-primary">
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              name="special"
+              id="noSpecial"
+              value="no"
+              className="btn-check"
+              checked={special === "no"}
+              onChange={handleSpecialChange}
+            />
+            <label htmlFor="noSpecial" className="btn btn-outline-primary">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col">
+          <span>Thrown:</span>
+        </div>
+        <div className="col">
+          <div className="btn-group" role="group">
+            <input
+              type="radio"
+              name="thrown"
+              id="yesThrown"
+              value="yes"
+              className="btn-check"
+              checked={thrown === "yes"}
+              onChange={handleThrownChange}
+            />
+            <label htmlFor="yesThrown" className="btn btn-outline-primary">
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              name="thrown"
+              id="noThrown"
+              value="no"
+              className="btn-check"
+              checked={thrown === "no"}
+              onChange={handleThrownChange}
+            />
+            <label htmlFor="noThrown" className="btn btn-outline-primary">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col">
+          <span>Two-Handed:</span>
+        </div>
+        <div className="col">
+          <div className="btn-group" role="group">
+            <input
+              type="radio"
+              name="twoHanded"
+              id="yesTwoHanded"
+              value="yes"
+              className="btn-check"
+              checked={twoHanded === "yes"}
+              onChange={handleTwoHandedChange}
+            />
+            <label htmlFor="yesTwoHanded" className="btn btn-outline-primary">
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              name="twoHanded"
+              id="noTwoHanded"
+              value="no"
+              className="btn-check"
+              checked={twoHanded === "no"}
+              onChange={handleTwoHandedChange}
+            />
+            <label htmlFor="noTwoHanded" className="btn btn-outline-primary">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col">
+          <span>Versatile:</span>
+        </div>
+        <div className="col">
+          <div className="btn-group" role="group">
+            <input
+              type="radio"
+              name="versatile"
+              id="yesVersatile"
+              value="yes"
+              className="btn-check"
+              checked={versatile === "yes"}
+              onChange={handleVersatileChange}
+            />
+            <label htmlFor="yesVersatile" className="btn btn-outline-primary">
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              name="versatile"
+              id="noVersatile"
+              value="no"
+              className="btn-check"
+              checked={versatile === "no"}
+              onChange={handleVersatileChange}
+            />
+            <label htmlFor="noVersatile" className="btn btn-outline-primary">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col">
+          <span>Silvered:</span>
+        </div>
+        <div className="col">
+          <div className="btn-group" role="group">
+            <input
+              type="radio"
+              name="silvered"
+              id="yesSilvered"
+              value="yes"
+              className="btn-check"
+              checked={silvered === "yes"}
+              onChange={handleSilveredChange}
+            />
+            <label htmlFor="yesSilvered" className="btn btn-outline-primary">
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              name="silvered"
+              id="noSilvered"
+              value="no"
+              className="btn-check"
+              checked={silvered === "no"}
+              onChange={handleSilveredChange}
+            />
+            <label htmlFor="noSilvered" className="btn btn-outline-primary">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
+
     <div className="m-3">
-      <hr />
       <h5 className="mb-2">Preview</h5>
       <p>If you are happy with your creation, be sure to click 'Save'</p>
       <Widget
