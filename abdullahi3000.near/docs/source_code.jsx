cb3d7def02fe6ad36df435b9c938087957d72db3
abdullahi3000.near <abdullahi3000.near>
2024-03-31 16:38:05 +0000

    Update docs by abdullahi3000.near at 2024-03-31T16:38:05.317Z

--- abdullahi3000.near/docs/source_code.jsx
+++ abdullahi3000.near/docs/source_code.jsx
@@ -1,9 +1,9 @@
 if (!props.docId) {
-  props.docId = "b3c9ae3826e7425c94b23f9da797f1dd";
+  props.docId = "1Zlr1LDw-eMwMoocH9Y90kmsZCeX5gYVZugFonDpiKhg";
   //   return <div className="">No document ID provided.</div>;
 }
 
-const url = `https://www.notion.so/${props.docId}?pvs=4`;
+const url = `https://docs.google.com/document/d/${props.docId}/edit?usp=sharing`;
 
 return (
   <>
