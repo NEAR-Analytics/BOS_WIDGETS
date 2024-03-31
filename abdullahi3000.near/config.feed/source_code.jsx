9f6ee5e6cbc092e6be36c0b80c6dad05f0753298
abdullahi3000.near <abdullahi3000.near>
2024-03-31 16:24:21 +0000

    Update config.feed by abdullahi3000.near at 2024-03-31T16:24:21.409Z

--- abdullahi3000.near/config.feed/source_code.jsx
+++ abdullahi3000.near/config.feed/source_code.jsx
@@ -47,7 +47,7 @@ return {
       },
     },
     documents: {
-      path: "abdullahi.near/widget/docs",
+      path: "abdullahi3000.near/widget/docs",
       blockHeight: "final",
       init: {
         name: "Documents",
