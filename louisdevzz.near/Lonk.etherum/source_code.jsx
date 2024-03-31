54d5aa6f0bad70df0bd4241e866eb8b3ab54083f
louisdevzz.near <louisdevzz.near>
2024-03-31 23:56:27 +0000

    Update Lonk.etherum by louisdevzz.near at 2024-03-31T23:56:27.121Z

--- louisdevzz.near/Lonk.etherum/source_code.jsx
+++ louisdevzz.near/Lonk.etherum/source_code.jsx
@@ -1,6 +1,8 @@
 const config = {
   image6:
     "https://bafkreiejkfmncbzuni5lsrlpplmkqisasvjzgl4oxu3nnz7nmmjwwd56ii.ipfs.nftstorage.link/",
+  image3:
+    "https://bafkreibeah7hmamkdzkrc7znm6u6jp4loiiy42shoyv2mb2hhlhiyynaia.ipfs.nftstorage.link/",
 };
 const fkGroteskFamily = fetch(
   "https://fonts.googleapis.com/css2?family=Lakki+Reddy&display=swap"
@@ -12,6 +14,10 @@ const Container = styled.div`
   ${fkGroteskFamily}
   ${fondKod}
     font-family: Kodchasan,sans-serif;
+    max-width: 1320px;
+    .intro {
+        background: url(${config.image3}) no-repeat 100% 0 fixed;
+    }
     .title{
         font-family: Lakki Reddy,cursive;
         font-size: 1.3rem;
@@ -31,42 +37,44 @@ return (
   <>
     <Widget src="louisdevzz.near/widget/headerSwap" />
     <Container>
-      <div class="title">Bridge From Ethereum to NEAR</div>
-      <div class="step">
-        <img class="bage" src="https://lonk.meme/assets/images/v.svg" />
-        Bruv have many options. 1-step way:
+      <div class="py-4">
+        <div class="title">Bridge From Ethereum to NEAR</div>
+        <div class="step">
+          <img class="bage" src="https://lonk.meme/assets/images/v.svg" />
+          Bruv have many options. 1-step way:
+        </div>
+        <ul>
+          <li>Rainbow Bridge (Official NEAR Bridge)</li>
+          <li>Rubic Exchange</li>
+        </ul>
+        <div class="step">2-step way:</div>
+        <div> Step 1, Bridge to Aurora using: </div>
+        <ul>
+          <li>Meson</li>
+          <li>Synapse</li>
+          <li>Boring DAO</li>
+        </ul>
+        <div>Step 2, Bridge from Aurora to NEAR using: </div>
+        <ul>
+          <li>Rainbow Bridge (Official NEAR Bridge)</li>
+        </ul>
+        <div>Done bridging?</div>
+        <div>Get some $LONK bruv </div>
+        <button
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#buyModal"
+          style={{
+            padding: "10px 20px",
+            height: "60px",
+            width: "270px",
+            border: "2px solid #31cf34",
+          }}
+          class="button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
+        >
+          <img src={config.image6} width="186" />
+        </button>
       </div>
-      <ul>
-        <li>Rainbow Bridge (Official NEAR Bridge)</li>
-        <li>Rubic Exchange</li>
-      </ul>
-      <div class="step">2-step way:</div>
-      <div> Step 1, Bridge to Aurora using: </div>
-      <ul>
-        <li>Meson</li>
-        <li>Synapse</li>
-        <li>Boring DAO</li>
-      </ul>
-      <div>Step 2, Bridge from Aurora to NEAR using: </div>
-      <ul>
-        <li>Rainbow Bridge (Official NEAR Bridge)</li>
-      </ul>
-      <div>Done bridging?</div>
-      <div>Get some $LONK bruv </div>
-      <button
-        type="button"
-        data-bs-toggle="modal"
-        data-bs-target="#buyModal"
-        style={{
-          padding: "10px 20px",
-          height: "60px",
-          width: "270px",
-          border: "2px solid #31cf34",
-        }}
-        class="button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
-      >
-        <img src={config.image6} width="186" />
-      </button>
     </Container>
     <div
       class="modal fade"
