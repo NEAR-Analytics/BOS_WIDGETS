ea98bc067b5d0f1631861de32ba9a23f50a84d39
abdullahi3000.near <abdullahi3000.near>
2024-04-01 11:10:58 +0000

    Update docs by abdullahi3000.near at 2024-04-01T11:10:58.660Z

--- abdullahi3000.near/docs/source_code.jsx
+++ abdullahi3000.near/docs/source_code.jsx
@@ -1,15 +1,39 @@
 if (!props.docId) {
-  props.docId = "1Zlr1LDw-eMwMoocH9Y90kmsZCeX5gYVZugFonDpiKhg";
+  props.docId = "15OHRDOitDKqy9nJb2vttezYF9m5wx9m-";
   //   return <div className="">No document ID provided.</div>;
 }
 
-const url = `https://docs.google.com/document/d/${props.docId}/edit?usp=sharing`;
+const containerStyles = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "#151718",
+};
+
+const iframeStyles = {
+  width: "100%",
+  minHeight: "80vh",
+  border: "none",
+  overflowY: "scroll",
+};
+
+const url = `https://drive.google.com/file/d/${props.docId}/preview`;
 
 return (
   <>
-    <iframe
-      className="container-fluid vh-100 border border-primary rounded"
-      src={url}
-    />
+    <div style={containerStyles}>
+      {url ? (
+        <iframe
+          style={iframe}
+          className="container-fluid vh-100 border border-primary rounded"
+          frameBorder="0"
+          allowFullScreen
+          src={url}
+        />
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
   </>
 );
