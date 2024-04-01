18fa340289749816fc9f507cac2871ede7e8383f
louisdevzz.near <louisdevzz.near>
2024-04-01 13:09:15 +0000

    Update Lonk.about by louisdevzz.near at 2024-04-01T13:09:15.306Z

---
+++ louisdevzz.near/Lonk.about/source_code.jsx
@@ -0,0 +1,34 @@
+return (
+  <div class="green-bg font-md-bigger py-4 py-lg-5 text-uppercase" id="token">
+    <div class="container">
+      <div class="about">
+        <div class="col-12 col-md-6 ">
+          <p class="mb-5 text-uppercase">
+            <strong>420.69 Billions LONK</strong>
+          </p>
+          <p class="text-uppercase mb-0">
+            83.058% LPs, <br />
+            16.9420% Reserved:
+          </p>
+          <ul class="text-uppercase">
+            <li>Airdrop</li>
+            <li>Exchange Listings</li>
+          </ul>
+          <p>
+            NO TAXES <br />
+            No Team Allocations
+            <br />
+            No VCS
+            <br />
+            No presale
+            <br />
+            NO BS
+          </p>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6 text-left text-md-end">
+          <h2>tokenomics</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+);
