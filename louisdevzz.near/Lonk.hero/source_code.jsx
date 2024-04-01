1381a9e63b5a0f2fa4b32274aaa44d18f4ad5bda
louisdevzz.near <louisdevzz.near>
2024-04-01 13:01:14 +0000

    Update Lonk.hero by louisdevzz.near at 2024-04-01T13:01:14.933Z

--- louisdevzz.near/Lonk.hero/source_code.jsx
+++ louisdevzz.near/Lonk.hero/source_code.jsx
@@ -1,6 +1,4 @@
 const config = {
-  lonk_bot:
-    "https://bafkreidusllz2u36hgmbvidbrliwrmxxpts57tljecslc4qtb72rpj2dji.ipfs.nftstorage.link/",
   image4:
     "https://bafybeib6kjh37qmodapxwjq3ukckhabckasdoamjas7ehigzqbjazu4kyq.ipfs.nftstorage.link/",
   image5:
@@ -18,84 +16,52 @@ const config = {
   image11:
     "https://bafkreigkwuy4k4txpn4jhivrwdagvvooiivbi3yywamv6krxv77bqitrmm.ipfs.nftstorage.link/",
 };
+const accountId = context.accountId;
 const [select, setSelect] = useState("");
 return (
   <>
     <div class="intro font-md-bigger py-4 py-lg-5 dragon-background text-uppercase text-center">
-      <div class="py-3">
+      <div class="container py-3">
         <div class="hero">
           <div class="d-flex justify-content-center align-items-center">
             <img src={config.image4} alt="Green Dragon" width="350px" />
           </div>
           <div class="">
             <img src={config.image5} width="200" />
-            <p class="hero-text">WE LONKING, NOT SHORTING</p>
-            <p class="hero-text">LONKING $NEAR BACK TO $20.24 IN 2024</p>
-            <p class="hero-text">Get some $LONK</p>
+            <p class="">WE LONKING, NOT SHORTING</p>
+            <p class="">LONKING $NEAR BACK TO $20.24 IN 2024</p>
+            <p class="">Get some $LONK</p>
             <div class="button-swap">
-              <div class="item">
-                <button
-                  onClick={() => setSelect("ref")}
-                  type="button"
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                  style={{
-                    padding: "10px 20px",
-                    height: "60px",
-                    width: "270px",
-                    border: "2px solid #31cf34",
-                  }}
-                  class="button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
-                >
-                  <img src={config.image6} class="image" width="186" />
-                </button>
-                <button
-                  onClick={() => setSelect("veax")}
-                  type="button"
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                  style={{
-                    padding: "10px 20px",
-                    height: "60px",
-                    width: "270px",
-                    border: "2px solid #31cf34",
-                  }}
-                  class="button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
-                >
-                  <img src={config.image7} class="image" width="90" />
-                </button>
-              </div>
-              <div class="item">
-                <a
-                  href="https://orderly.network/docs/trade-on-orderly/trading-guis"
-                  target="_blank"
-                  style={{
-                    padding: "10px 20px",
-                    height: "60px",
-                    width: "270px",
-                    border: "2px solid #31cf34",
-                    color: "#000",
-                    fontSize: "18px",
-                    fontWeight: "bold",
-                  }}
-                  class="button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
-                >
-                  Orderly Network
-                </a>
-                <a
-                  href="https://t.me/lonk_bot"
-                  target="_blank"
-                  style={{
-                    padding: "10px 20px",
-                    height: "60px",
-                    width: "270px",
-                    border: "2px solid #31cf34",
-                  }}
-                  class="button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
-                >
-                  <img src={config.lonk_bot} class="image" width="120" />
-                </a>
-              </div>
+              <button
+                onClick={() => setSelect("ref")}
+                type="button"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                style={{
+                  padding: "10px 20px",
+                  height: "60px",
+                  width: "270px",
+                  border: "2px solid #31cf34",
+                }}
+                class="button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
+              >
+                <img src={config.image6} width="186" />
+              </button>
+              <button
+                onClick={() => setSelect("veax")}
+                type="button"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                style={{
+                  padding: "10px 20px",
+                  height: "60px",
+                  width: "270px",
+                  border: "2px solid #31cf34",
+                }}
+                class="button d-flex bg-light justify-content-center align-items-center text-decoration-none rounded-5"
+              >
+                <img src={config.image7} class="image" width="90" />
+              </button>
             </div>
             <div class="mt-5">
               <a
@@ -110,18 +76,18 @@ return (
                 />
               </a>
               <a
-                href="https://twitter.com/Lonkonnear"
+                href="http://t.me/LonkonNEAR"
                 target="_blank"
                 class="d-inline-block px-2"
               >
                 <img
                   src={config.image9}
-                  width="68"
+                  width="50"
                   class="d-inline-block px-2"
                 />
               </a>
               <a
-                href="https://dexscreener.com/near/refv1-4314"
+                href="http://t.me/LonkonNEAR"
                 target="_blank"
                 class="d-inline-block px-2"
               >
@@ -132,7 +98,7 @@ return (
                 />
               </a>
               <a
-                href="https://www.coingecko.com/en/coins/lonk-on-near"
+                href="http://t.me/LonkonNEAR"
                 target="_blank"
                 class="d-inline-block px-2"
               >
@@ -147,7 +113,7 @@ return (
         </div>
       </div>
     </div>
-    <Widget src="louisdevzz.near/widget/Lonk.stake" />
+    <Widget src="huunhanz.near/widget/Lonk.stake" />
     <div class="green-bg mt-2 font-md-bigger py-4 py-lg-5" id="about">
       <div class="container">
         <div class="about">
@@ -180,35 +146,38 @@ return (
         </div>
       </div>
     </div>
-    <div
-      class="modal fade"
-      id="exampleModal"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">
-              Swap Token {select == "ref" ? "Ref Finance" : "Veax"}
-            </h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body d-flex justify-content-center align-items-center">
-            {select == "ref" ? (
-              <Widget src="huunhanz.near/widget/ref-swap" />
-            ) : (
-              <Widget src="louisdevzz.near/widget/Lonk.veax-swap" />
-            )}
+    {(accountId && select == "ref") ||
+      (select != "ref" && (
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Swap Token {select == "ref" ? "Ref Finance" : "Veax"}
+                </h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body d-flex justify-content-center align-items-center">
+                {select == "ref" ? (
+                  <Widget src="huunhanz.near/widget/ref-swap" />
+                ) : (
+                  <Widget src="huunhanz.near/widget/Lonk.veax-swap" />
+                )}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      ))}
   </>
 );
