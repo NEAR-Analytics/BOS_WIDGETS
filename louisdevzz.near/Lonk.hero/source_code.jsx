5deb0bb8c14ec04fd6f0d411997e7391b53389c4
louisdevzz.near <louisdevzz.near>
2024-04-01 18:10:09 +0000

    Update Lonk.hero by louisdevzz.near at 2024-04-01T18:10:09.170Z

--- louisdevzz.near/Lonk.hero/source_code.jsx
+++ louisdevzz.near/Lonk.hero/source_code.jsx
@@ -181,7 +181,7 @@ return (
         </div>
       </div>
     </div>
-    {accountId && (
+    {accountId ? (
       <div
         class="modal fade"
         id="exampleModal"
@@ -208,6 +208,33 @@ return (
               ) : (
                 <Widget src="louisdevzz.near/widget/Lonk.veax-swap" />
               )}
+            </div>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Warning
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body d-flex justify-content-center align-items-center">
+              Please connect wallet before swap!!!
             </div>
           </div>
         </div>
