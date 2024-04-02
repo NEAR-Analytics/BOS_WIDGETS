b103a5240c3b1cf49a6dc2c3079bdfd3d4e38096
louisdevzz.near <louisdevzz.near>
2024-04-02 04:20:49 +0000

    Update Lonk.hero by louisdevzz.near at 2024-04-02T04:20:49.171Z

--- louisdevzz.near/Lonk.hero/source_code.jsx
+++ louisdevzz.near/Lonk.hero/source_code.jsx
@@ -21,7 +21,7 @@ const config = {
 const fondKod = fetch(
   "https://fonts.googleapis.com/css2?family=Kodchasan:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
 ).body;
-const Content = style.div`
+const Content = styled.div`
     ${fondKod}
     color:#fff;
     font-family: Kodchasan,sans-serif;
