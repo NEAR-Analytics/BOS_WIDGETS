8458214177db314c66371cd9a3d780c1f0c40a42
abdullahi3000.near <abdullahi3000.near>
2024-04-02 11:24:54 +0000

    Update docs by abdullahi3000.near at 2024-04-02T11:24:54.724Z

--- abdullahi3000.near/docs/source_code.jsx
+++ abdullahi3000.near/docs/source_code.jsx
@@ -1,42 +1,27 @@
 if (!props.docId) {
   props.docId = "1id_f8nkc8skMyrqfnecW6TOI8Q9r7dIy";
-  //   return <div className="">No document ID provided.</div>;
 }
-
-const containerStyles = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: "#151718",
-};
-
-const iframeStyles = {
-  width: "100%",
-  minHeight: "80vh",
-  border: "none",
-  overflowY: "scroll",
-};
 
 const url = `https://drive.google.com/file/d/${props.docId}/preview`;
 
 return (
-  <>
-    <div style={containerStyles}>
-      {url ? (
-        <>
-          <iframe
-            style={iframe}
-            className="container-fluid vh-100 border border-primary rounded"
-            frameBorder="0"
-            allowFullScreen
-            src={url}
-          />
-          <Widget src={`abdullahi3000.near/widget/harmonic.docs`} />
-        </>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </div>
-  </>
+  <div
+    className="container-fluid d-flex flex-column justify-content-center align-items-center"
+    style={{ backgroundColor: "#151718" }}
+  >
+    {url ? (
+      <>
+        <iframe
+          className="container-fluid vh-100 border border-primary rounded"
+          style={{ border: "none", overflowY: "scroll" }} // Keep custom styles that aren't covered by Bootstrap
+          frameBorder="0"
+          allowFullScreen
+          src={url}
+        ></iframe>
+        <Widget src={`abdullahi3000.near/widget/harmonic.docs`} />
+      </>
+    ) : (
+      <div>Loading...</div>
+    )}
+  </div>
 );
