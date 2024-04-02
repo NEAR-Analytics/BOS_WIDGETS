2b72978cf7d7511882938372aab6f6f195e56f09
abdullahi3000.near <abdullahi3000.near>
2024-04-02 03:03:04 +0000

    Update docs by abdullahi3000.near at 2024-04-02T03:03:04.978Z

--- abdullahi3000.near/docs/source_code.jsx
+++ abdullahi3000.near/docs/source_code.jsx
@@ -24,13 +24,16 @@ return (
   <>
     <div style={containerStyles}>
       {url ? (
-        <iframe
-          style={iframe}
-          className="container-fluid vh-100 border border-primary rounded"
-          frameBorder="0"
-          allowFullScreen
-          src={url}
-        />
+        <>
+          <iframe
+            style={iframe}
+            className="container-fluid vh-100 border border-primary rounded"
+            frameBorder="0"
+            allowFullScreen
+            src={url}
+          />
+          <Widget src={`abdullahi3000.near/widget/harmonic.docs`} />
+        </>
       ) : (
         <div>Loading...</div>
       )}
