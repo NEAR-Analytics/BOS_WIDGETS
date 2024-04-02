bf201d83bb22499205b4c7940cc8ba2196283db6
louisdevzz.near <louisdevzz.near>
2024-04-02 16:13:21 +0000

    Update Lonk.stake by louisdevzz.near at 2024-04-02T16:13:21.495Z

--- louisdevzz.near/Lonk.stake/source_code.jsx
+++ louisdevzz.near/Lonk.stake/source_code.jsx
@@ -273,6 +273,12 @@ return (
         tabindex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -40%)",
+        }}
       >
         <div class="modal-dialog">
           <div class="modal-content">
