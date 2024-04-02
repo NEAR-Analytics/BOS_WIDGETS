a7d94e18a93e043f8766cfe59a4f27f31573e5ad
abdullahi3000.near <abdullahi3000.near>
2024-04-02 03:22:34 +0000

    Update harmonic.music by abdullahi3000.near at 2024-04-02T03:22:34.268Z

---
+++ abdullahi3000.near/harmonic.music/source_code.jsx
@@ -0,0 +1,7 @@
+return (
+  <div>
+    <Widget src={`abdullahi3000.near/widget/harmonic.music.player`} />
+    <Widget src={`abdullahi3000.near/widget/harmonic.song.uploader`} />
+    <Widget src={`abdullahi3000.near/widget/harmonic.artists.network`} />
+  </div>
+);
