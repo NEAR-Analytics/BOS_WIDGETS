029ac12bbced9150cf02d6b6feb470d4fd0b5ca9
abdullahi3000.near <abdullahi3000.near>
2024-04-02 16:38:56 +0000

    Update harmonic.pitch by abdullahi3000.near at 2024-04-02T16:38:56.469Z

--- abdullahi3000.near/harmonic.pitch/source_code.jsx
+++ abdullahi3000.near/harmonic.pitch/source_code.jsx
@@ -1,51 +1,61 @@
-const coverImage =
-  "https://arweave.net/20P7TRwdQ45AB9EBG72AEOswods5UoMXZb42EAoLxBE";
 const mediaImage =
   "https://hudsuller-digital-product.s3.amazonaws.com/Product+Demo_Short_NoMusic+1.mp4";
 
 const code = `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <style>
-        body, html {
-          margin: 0;
-          padding: 0;
-          width: 100%;
-          height: 100%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color: #f0f0f0;
-        }
-        .video-container {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-      </style>
-    </head>
-    <body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <style>
+      body, html {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #f0f0f0;
+      }
+      .video-container {
+        width: 100%; /* Ensure the container fills the width */
+        height: 100%; /* Ensure the container fills the height */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      video {
+        width: 100%; /* Make the video fill the container width */
+        height: auto; /* Adjust height automatically to keep the aspect ratio */
+        max-height: 100%; /* Ensure it does not exceed the container's height */
+      }
+    </style>
+  </head>
+  <body>
 
-      <div class="video-container">
-        <video controls autoplay">
-          <source src="${mediaImage}" type="video/mp4">
-          Your browser does not support the video tag.
-        </video>
-      </div>
+    <div class="video-container">
+      <video controls autoplay>
+        <source src="${mediaImage}" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
 
-    </body>
-  </html>
+  </body>
+</html>
 `;
 
 return (
   <div
     className="w-100 h-100"
     style={{
-      minHeight: "500px",
-      minWidth: "300px",
+      minHeight: "20px",
+      minWidth: "100px",
     }}
   >
-    <iframe className="w-100 h-100" srcDoc={code} title="Embedded Media" />
+    <iframe
+      className="w-100 h-100"
+      srcDoc={code}
+      title="Embedded Media"
+      style={{ border: "none" }}
+    />
   </div>
 );
