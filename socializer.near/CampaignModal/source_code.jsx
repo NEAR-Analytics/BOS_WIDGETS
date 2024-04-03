57b2b0bc6db16c241f194a607637313d1c49cad6
socializer.near <socializer.near>
2024-04-03 14:49:58 +0000

    Update CampaignModal by socializer.near at 2024-04-03T14:49:58.952Z

--- socializer.near/CampaignModal/source_code.jsx
+++ socializer.near/CampaignModal/source_code.jsx
@@ -8,16 +8,16 @@ const link = data.post_link.split("?")[1].split("&");
 const posterId = link[0].split("=")[1];
 
 State.init({
-  posterAvatar: `https://i.near.social/magic/large/https://near.social/magic/img/account/${posterId}`,
-  error: "",
-  like: false,
-  follow: false,
-  repost: false,
-  comment: false,
-  human: false,
-  loaded: false,
-  loading: false,
-  notification: "",
+    posterAvatar: `https://i.near.social/magic/large/https://near.social/magic/img/account/${posterId}`,
+    error: "",
+    like: false,
+    follow: false,
+    repost: false,
+    comment: false,
+    human: false,
+    loaded: false,
+    loading: false,
+    notification=== "" ? 1213131 : "",
 });
 
 const ModalOverlay = styled.div`
@@ -111,170 +111,170 @@ background-color: #191a1a;
 `;
 
 const getVerifyState = () => {
-  State.update({ error: "", loading: true });
-  asyncFetch(
-    API_URL + `/api/campaign/verify?accountId=${accountId}&id=${data._id}`
-  ).then((res) => {
-    if (res.ok) {
-      const { error, data } = res.body;
-      if (error) State.update({ error, loading: false });
-      else if (data) {
-        const { like, follow, repost, comment, human, finished } = data;
-        State.update({
-          like,
-          follow,
-          repost,
-          comment,
-          human,
-          error: finished ? "" : "Please complete all tasks",
-          loaded: true,
-          loading: false,
-        });
-      }
-    }
-  });
+    State.update({ error: "", loading: true });
+    asyncFetch(
+        API_URL + `/api/campaign/verify?accountId=${accountId}&id=${data._id}`
+    ).then((res) => {
+        if (res.ok) {
+            const { error, data } = res.body;
+            if (error) State.update({ error, loading: false });
+            else if (data) {
+                const { like, follow, repost, comment, human, finished } = data;
+                State.update({
+                    like,
+                    follow,
+                    repost,
+                    comment,
+                    human,
+                    error: finished ? "" : "Please complete all tasks",
+                    loaded: true,
+                    loading: false,
+                });
+            }
+        }
+    });
 };
 
 if (!state.loaded) getVerifyState();
 
 const verifyEnter = () => {
-  State.update({ error: "", loading: true });
-  asyncFetch(API_URL + `/api/campaign/verify`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ accountId, id: data._id }),
-  }).then((res) => {
-    if (res.ok) {
-      const { error, data } = res.body;
-      if (error) State.update({ error, loading: false });
-      else if (data) {
-        const { like, follow, repost, comment, human, finished } = data;
-        State.update({
-          like,
-          follow,
-          repost,
-          comment,
-          error: finished ? "" : "Please complete all tasks",
-          notification: finished ? "Participation Successful" : "",
-          human,
-          loading: false,
+    State.update({ error: "", loading: true });
+    asyncFetch(API_URL + `/api/campaign/verify`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ accountId, id: data._id }),
+    }).then((res) => {
+        if (res.ok) {
+            const { error, data } = res.body;
+            if (error) State.update({ error, loading: false });
+            else if (data) {
+                const { like, follow, repost, comment, human, finished } = data;
+                State.update({
+                    like,
+                    follow,
+                    repost,
+                    comment,
+                    error: finished ? "" : "Please complete all tasks",
+                    notification=== "" ? 1213131 : finished ? "Participation Successful" : "",
+                    human,
+                    loading: false,
         });
-      }
+}
     }
   });
 };
 
 return (
-  <ModalOverlay>
-    <ModalContent>
-      <button
-        style={{
-          width: 40,
-          position: "absolute",
-          right: 19,
-          background: "unset",
-          borderColor: "white",
-          color: "black",
-        }}
-        onClick={onClose}
-      >
-        X
-      </button>
-      <ModalTitle>{`NEAR DEGENS`}</ModalTitle>
-      <h4>{`Camaign Details`}</h4>
-      <img
-        style={{ width: "100%", height: 250 }}
-        src={state.posterAvatar}
-        onError={() => {
-          State.update({
-            myAvatar:
-              "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
-          });
-        }}
-      />
+    <ModalOverlay>
+        <ModalContent>
+            <button
+                style={{
+                    width: 40,
+                    position: "absolute",
+                    right: 19,
+                    background: "unset",
+                    borderColor: "white",
+                    color: "black",
+                }}
+                onClick={onClose}
+            >
+                X
+            </button>
+            <ModalTitle>{`NEAR DEGENS`}</ModalTitle>
+            <h4>{`Camaign Details`}</h4>
+            <img
+                style={{ width: "100%", height: 250 }}
+                src={state.posterAvatar}
+                onError={() => {
+                    State.update({
+                        myAvatar:
+                            "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm",
+                    });
+                }}
+            />
 
-      <div
-        className="d-flex"
-        style={{ flexDirection: "column", gap: 25, padding: 20 }}
-      >
-        <p>
-          {`Please ensure you do the tasks below before clicking on Verify & Enter:`}
-        </p>
-        <p>{`Campaign Id: ${data.id}`}</p>
-        {data?.requirements.map((row, index) => (
-          <div className="d-flex justify-content-between align-items-center">
-            <p>
-              <b>{`${index + 1})`}</b>
-              <span>{row.name}</span>
-            </p>
+            <div
+                className="d-flex"
+                style={{ flexDirection: "column", gap: 25, padding: 20 }}
+            >
+                <p>
+                    {`Please ensure you do the tasks below before clicking on Verify & Enter:`}
+                </p>
+                <p>{`Campaign Id: ${data.id}`}</p>
+                {data?.requirements.map((row, index) => (
+                    <div className="d-flex justify-content-between align-items-center">
+                        <p>
+                            <b>{`${index + 1})`}</b>
+                            <span>{row.name}</span>
+                        </p>
 
-            {state[row.value] ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 15 15"
-              >
-                <path
-                  fill="#038d2e"
-                  fill-rule="evenodd"
-                  d="M0 7.5a7.5 7.5 0 1 1 15 0a7.5 7.5 0 0 1-15 0Zm7.072 3.21l4.318-5.398l-.78-.624l-3.682 4.601L4.32 7.116l-.64.768l3.392 2.827Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="red"
-                  d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10s10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17L12 13.41L8.41 17L7 15.59L10.59 12L7 8.41L8.41 7L12 10.59L15.59 7L17 8.41L13.41 12L17 15.59z"
-                />
-              </svg>
-            )}
-          </div>
-        ))}
-        {state.error && (
-          <p style={{ fontSize: 14, margin: 0, color: "red" }}>{state.error}</p>
-        )}
-      </div>
-    </ModalContent>
-    <ModalAction>
-      <PostLink className="btn" target="_blank" href={data.post_link}>
-        {`Goto Post`}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
+                        {state[row.value] ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                viewBox="0 0 15 15"
+                            >
+                                <path
+                                    fill="#038d2e"
+                                    fill-rule="evenodd"
+                                    d="M0 7.5a7.5 7.5 0 1 1 15 0a7.5 7.5 0 0 1-15 0Zm7.072 3.21l4.318-5.398l-.78-.624l-3.682 4.601L4.32 7.116l-.64.768l3.392 2.827Z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    fill="red"
+                                    d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10s10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17L12 13.41L8.41 17L7 15.59L10.59 12L7 8.41L8.41 7L12 10.59L15.59 7L17 8.41L13.41 12L17 15.59z"
+                                />
+                            </svg>
+                        )}
+                    </div>
+                ))}
+                {state.error && (
+                    <p style={{ fontSize: 14, margin: 0, color: "red" }}>{state.error}</p>
+                )}
+            </div>
+        </ModalContent>
+        <ModalAction>
+            <PostLink className="btn" target="_blank" href={data.post_link}>
+                {`Goto Post`}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        fill="#3c7eff"
+                        d="M9.043 5.793L2.836 12l6.207 6.207l1.414-1.414L5.664 12l4.793-4.793l-1.414-1.414Zm5.914 12.414L21.164 12l-6.207-6.207l-1.414 1.414L18.336 12l-4.793 4.793l1.414 1.414Z"
+                    />
+                </svg>
+            </PostLink>
+            <Button
+                className="btn"
+                onClick={verifyEnter}
+                disabled={state.loading}
+            >{`Verify & Enter`}</Button>
+        </ModalAction>
+        <div
+            className="d-flex justify-content-end position-absolute"
+            style={{ right: 10 }}
         >
-          <path
-            fill="#3c7eff"
-            d="M9.043 5.793L2.836 12l6.207 6.207l1.414-1.414L5.664 12l4.793-4.793l-1.414-1.414Zm5.914 12.414L21.164 12l-6.207-6.207l-1.414 1.414L18.336 12l-4.793 4.793l1.414 1.414Z"
-          />
-        </svg>
-      </PostLink>
-      <Button
-        className="btn"
-        onClick={verifyEnter}
-        disabled={state.loading}
-      >{`Verify & Enter`}</Button>
-    </ModalAction>
-    <div
-      className="d-flex justify-content-end position-absolute"
-      style={{ right: 10 }}
-    >
-      <Widget
-        props={{
-          text: state.notification,
-        }}
-        src={`${Owner}/widget/Alert`}
-      />
-    </div>
-  </ModalOverlay>
+            <Widget
+                props={{
+                    text: state.notification === "" ? 1213131 : state.notification,
+                }}
+                src={`${Owner}/widget/Alert`}
+            />
+        </div>
+    </ModalOverlay>
 );
