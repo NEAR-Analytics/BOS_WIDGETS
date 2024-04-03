1258b9da0733a0b62b3633d193850a5c1e06194c
abdullahi3000.near <abdullahi3000.near>
2024-04-03 11:11:14 +0000

    Update harmonic.song.uploader by abdullahi3000.near at 2024-04-03T11:11:14.326Z

--- abdullahi3000.near/harmonic.song.uploader/source_code.jsx
+++ abdullahi3000.near/harmonic.song.uploader/source_code.jsx
@@ -1,50 +1,19 @@
-const containerStyles = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "100vh",
-  backgroundColor: "#151718",
-  color: "#fff",
-};
-
-const innerContainerStyles = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-};
-
-const paragraphStyles = {
-  fontSize: "1.25rem",
-};
-
 return (
-  <>
-    <div style={containerStyles}>
-      <div className="mb-5" style={innerContainerStyles}>
-        <div style={{ flex: 1, padding: "2rem" }}>
-          <p
-            style={{
-              fontSize: "1.75rem",
-              fontWeight: "bold",
-              // marginTop: "4rem",
-            }}
-          >
-            Upload Your First Song
-          </p>
-          <p style={paragraphStyles}>
-            We're excited to have you onboard, whether you're a budding artist
-            seeking to make your mark or a seasoned pro looking to diversify
-            your distribution platforms. Unleashing your musical creativity and
-            sharing your work with the world has never been more empowering or
-            accessible.
-          </p>
-        </div>
-        <div style={{ flex: 1, color: "BLACK" }}>
-          <Widget src="harmonic1.near/widget/MusicUpload" />
-        </div>
+  <div
+    className="d-flex flex-column justify-content-center align-items-center bg-dark text-white"
+    style={{ width: "100%", height: "100%" }}
+  >
+    <div className="mb-5 p-2">
+      <h2 className="fs-3 fw-bold mb-3 text-center">Upload Your First Song</h2>
+      <p className="fs-5">
+        We're excited to have you onboard, whether you're a budding artist
+        seeking to make your mark or a seasoned pro looking to diversify your
+        distribution platforms. Unleashing your musical creativity and sharing
+        your work with the world has never been more empowering or accessible.
+      </p>
+      <div className="mt-4" style={{ flex: 1, color: "BLACK" }}>
+        <Widget src="harmonic1.near/widget/MusicUpload" />
       </div>
     </div>
-  </>
+  </div>
 );
