e44ccff0ce58e7c019239e82c72f117b0ec69e39
abdullahi3000.near <abdullahi3000.near>
2024-04-03 14:26:08 +0000

    Update harmonic.pitch by abdullahi3000.near at 2024-04-03T14:26:08.874Z

--- abdullahi3000.near/harmonic.pitch/source_code.jsx
+++ abdullahi3000.near/harmonic.pitch/source_code.jsx
@@ -4,44 +4,43 @@ const mediaImage =
 const code = `
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Viewport meta tag -->
-    <style>
-      body, html {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: #151718;
-      }
-      .video-container {
-        width: 100%; /* Ensure the container fills the width */
-        height: 100%; /* Ensure the container fills the height */
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      video {
-        width: 100%; /* Make the video fill the container width */
-        height: auto; /* Adjust height automatically to keep the aspect ratio */
-        max-height: 100%; /* Ensure it does not exceed the container's height */
-      }
-    </style>
-  </head>
-  <body>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body, html {
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #151718;
+    }
+    .video-container {
+      position: relative;
+      width: 100%; /* Full width */
+      /* Define aspect ratio via padding (56.25% for 16:9 aspect ratio) */
+      padding-top: 56.25%;
+    }
+    video {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  </style>
+</head>
+<body>
 
-    <div class="video-container">
-      <video controls autoplay>
-        <source src="${mediaImage}" type="video/mp4">
-        Your browser does not support the video tag.
-      </video>
-    </div>
+<div class="video-container">
+  <video controls autoplay>
+    <source src="${mediaImage}" type="video/mp4">
+    Your browser does not support the video tag.
+  </video>
+</div>
 
-  </body>
+</body>
 </html>
+
 
 `;
 
