fea59dfa70b99dbd8632610c18d3d05a3afb4346
ndcdev.near <ndcdev.near>
2024-04-03 09:57:48 +0000

    Update daos.Components.Dao.Guidance by ndcdev.near at 2024-04-03T09:57:48.005Z

--- ndcdev.near/daos.Components.Dao.Guidance/source_code.jsx
+++ ndcdev.near/daos.Components.Dao.Guidance/source_code.jsx
@@ -145,7 +145,7 @@ const Info = ({ card }) => (
     <div className="inner d-flex flex-column justify-content-between p-4 align-items-left">
       <div>
         <h4 className="color-text">{card.title}</h4>
-        <span>
+        <span style={{'word-wrap': 'break-word'}}>
           <Widget
             src="ndcdev.near/widget/daos.Components.MarkdownViewer"
             props={{
