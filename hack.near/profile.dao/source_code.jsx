3ac0c8ab2c805ee6d887cfdbaa0b0cd4b6507499
hack.near <hack.near>
2024-04-03 14:45:18 +0000

    Update profile.dao by hack.near at 2024-04-03T14:45:18.148Z

--- hack.near/profile.dao/source_code.jsx
+++ hack.near/profile.dao/source_code.jsx
@@ -12,18 +12,23 @@ const imgWrapperStyle = { height: "3em", width: "3em" };
 return (
   <div className="d-flex flex-row">
     <div className="me-2">
-      <Widget
-        src="mob.near/widget/ProfileImage"
-        loading={<div style={imgWrapperStyle} />}
-        props={{
-          fast,
-          profile,
-          accountId: "build.sputnik-dao.near",
-          widgetName,
-          style: imgWrapperStyle,
-          imageClassName: "rounded-circle w-100 h-100",
-        }}
-      />
+      <a
+        style={{ color: "#000", textDecoration: "none" }}
+        href="https://nearbuilders.org"
+      >
+        <Widget
+          src="mob.near/widget/ProfileImage"
+          loading={<div style={imgWrapperStyle} />}
+          props={{
+            fast,
+            profile,
+            accountId: "build.sputnik-dao.near",
+            widgetName,
+            style: imgWrapperStyle,
+            imageClassName: "rounded-circle w-100 h-100",
+          }}
+        />
+      </a>
     </div>
     <div className="text-truncate">
       <div className="text-truncate">
