511c224fea42fa8bf720732f72f979a349fb6bf1
abdullahi3000.near <abdullahi3000.near>
2024-04-03 10:14:02 +0000

    Update harmonic.music.player by abdullahi3000.near at 2024-04-03T10:14:02.764Z

--- abdullahi3000.near/harmonic.music.player/source_code.jsx
+++ abdullahi3000.near/harmonic.music.player/source_code.jsx
@@ -1,42 +1,32 @@
-const containerStyles = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "100vh",
-  backgroundColor: "#151718",
-  color: "#fff",
-};
-
-const innerContainerStyles = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-};
-
-const paragraphStyles = {
-  fontSize: "1.25rem",
-};
-
 return (
   <>
-    <div style={containerStyles}>
-      <div className="mt-5 mb-5" style={innerContainerStyles}>
-        <div style={{ flex: 1, padding: "2rem" }}>
-          <p style={{ fontSize: "1.75rem", fontWeight: "bold" }}>
-            Decentralised Music Streaming
-          </p>
-          <p style={paragraphStyles}>
-            Experience a truly decentralized music player fully on-chain. Our
-            mission is to bring transparency to streaming and revolutionize
-            payments for musicians. With Harmonic Guild, we are returning
-            ownership to music, ensuring that artists are fairly compensated for
-            their creations.
-          </p>
+    <div
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ height: "100vh", backgroundColor: "#151718", color: "#fff" }}
+    >
+      <div className="container mt-5 mb-5">
+        <div className="row">
+          <div
+            className="col d-flex flex-column justify-content-center align-items-center"
+            style={{ padding: "2rem" }}
+          >
+            <p
+              className="text-center"
+              style={{ fontSize: "1.75rem", fontWeight: "bold" }}
+            >
+              Decentralised Music Streaming
+            </p>
+            <p className="text-center" style={{ fontSize: "1.25rem" }}>
+              Experience a truly decentralized music player fully on-chain. Our
+              mission is to bring transparency to streaming and revolutionize
+              payments for musicians. With Harmonic Guild, we are returning
+              ownership to music, ensuring that artists are fairly compensated
+              for their creations.
+            </p>
+          </div>
         </div>
-        <div style={{ flex: 1, color: "BLACK" }}>
-          <Widget src="harmonic1.near/widget/MusicPlayer-Harmonic" />
+        <div className="col" style={{ color: "black" }}>
+          <Widget src="abdullahi3000.near/widget/MusicPlayer-Harmonic" />
         </div>
       </div>
     </div>
