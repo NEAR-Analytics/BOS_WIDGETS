a3768937599aae036e1be849d7aaf42a13d6f508
bos.dapplets.near <bos.dapplets.near>
2024-04-04 21:14:17 +0000

    Update ApplicationCard by bos.dapplets.near at 2024-04-04T21:14:17.481Z

--- bos.dapplets.near/ApplicationCard/source_code.jsx
+++ bos.dapplets.near/ApplicationCard/source_code.jsx
@@ -1,4 +1,4 @@
-console.log(props.src);
+if (!props.src) return;
 const [accountId, unused, widgetName] = props.src.split("/");
 const detailsUrl = `/near/widget/ComponentDetailsPage?src=${accountId}/widget/${widgetName}`;
 const appUrl = `/${accountId}/widget/${widgetName}`;
