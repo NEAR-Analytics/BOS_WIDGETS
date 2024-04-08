9da8d59685bc1537bbeb975af2cb2b0a88a32359
kurodenjiro.near <kurodenjiro.near>
2024-04-08 20:06:13 +0000

    Update lonk-hero by kurodenjiro.near at 2024-04-08T20:06:13.910Z

--- kurodenjiro.near/lonk-hero/source_code.jsx
+++ kurodenjiro.near/lonk-hero/source_code.jsx
@@ -157,7 +157,7 @@ return (
         </div>
       </div>
     </div>
-
+    <Widget src="louisdevzz.near/widget/Lonk.stake" props={{ accountId }} />
     <div class="green-bg mt-2 font-md-bigger py-4 py-lg-5" id="about">
       <div class="container">
         <div class="about">
@@ -219,9 +219,7 @@ return (
               <div
                 style={{ background: "#182733" }}
                 class="modal-body d-flex justify-content-center align-items-center"
-              >
-                <Widget src="kurodenjiro.near/widget/ref-swap" />
-              </div>
+              ></div>
             </div>
           </div>
         </div>
@@ -253,9 +251,7 @@ return (
               <div
                 style={{ background: "#282828" }}
                 class="d-flex justify-content-center align-items-center"
-              >
-                <Widget src="louisdevzz.near/widget/Lonk.veax-swap" />
-              </div>
+              ></div>
             </div>
           </div>
         </div>
