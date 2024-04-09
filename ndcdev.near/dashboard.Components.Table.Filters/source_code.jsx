5ee4f0d33e63302c57837692f918165633007e60
ndcdev.near <ndcdev.near>
2024-04-09 18:25:55 +0000

    Update dashboard.Components.Table.Filters by ndcdev.near at 2024-04-09T18:25:55.890Z

---
+++ ndcdev.near/dashboard.Components.Table.Filters/source_code.jsx
@@ -0,0 +1,60 @@
+const SubFilterItem = styled.div`
+  border-radius: 10px;
+  background: #a39acd;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 350;
+  width: 100%;
+  min-width: 270px;
+  width: 100%;
+  height: 50px;
+  text-align: center;
+`;
+
+const {
+  text,
+  options,
+  multiple,
+  onChange,
+  onClear,
+  values,
+  hintText,
+  defaultValue,
+  onFilterClick,
+  id,
+  filterIsOpen,
+} = props;
+
+return (
+  <div className="d-flex flex-column gap-1 w-100">
+    <Widget
+      src={`ndcdev.near/widget/dashboard.Components.Select`}
+      props={{
+        containerClass: "selected-container black",
+        text,
+        options,
+        multiple,
+        onClear,
+        values,
+        onChange,
+        onFilterClick,
+        id,
+        filterIsOpen,
+        hintText,
+        isTooltipVisible: true,
+      }}
+    />
+
+    <SubFilterItem>
+      <div>
+        {values.length >= 1
+          ? `${values.length} Selected`
+          : defaultValue ?? values[0]}
+      </div>
+    </SubFilterItem>
+  </div>
+);
