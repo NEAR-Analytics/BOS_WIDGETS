06f6f9ad541c586c9da9bdfdd8f91de57efabe8f
ndcdev.near <ndcdev.near>
2024-04-09 18:31:40 +0000

    Update daos.Pages.Daos.Index by ndcdev.near at 2024-04-09T18:31:40.103Z

--- ndcdev.near/daos.Pages.Daos.Index/source_code.jsx
+++ ndcdev.near/daos.Pages.Daos.Index/source_code.jsx
@@ -5,6 +5,8 @@ let { content, contractName } = VM.require(
 const { id } = props;
 const accountId = context.accountId;
 
+let isFollowed = false
+
 const Container = styled.div`
   width: 100%;
   display: flex;
@@ -148,7 +150,19 @@ if (!contractName || !content)
 const dao = Near.view(contractName, "get_dao_by_handle", { handle: id });
 const section = content.daos[id].sections;
 
-if (!dao) return <Widget src="flashui.near/widget/Loading" />;
+
+if (accountId) {
+  const followedDaos = Near.view(contractName, "get_follow_dao", {
+    account_id: accountId,
+  });
+
+
+if (!followedDaos) return <Widget src="flashui.near/widget/Loading" />;
+
+  isFollowed = followedDaos.find((d) => d.handle === dao.handle);
+}
+
+if (!dao ) return <Widget src="flashui.near/widget/Loading" />;
 
 const projects = Near.view(contractName, "get_dao_communities", {
   dao_id: dao.id,
@@ -164,6 +178,7 @@ const ProjectCard = ({ project }) => (
 );
 
 const handelOnFollow = () => {
+  if (isFollowed) return;
   if (dao.checkin_account_id) {
     const UserFollowDao_Payload = {
       contractName: contractName,
@@ -197,7 +212,7 @@ return (
     {accountId ?
       <div className="image-container">
         <img className="hero-img" src={dao.banner_url} alt="Banner Image" />
-        <a className="overlay-button btn" onClick={handelOnFollow}>Follow</a>
+        <a className="overlay-button btn" style={{cursor: isFollowed ? 'default': 'pointer'}} onClick={handelOnFollow}>{isFollowed ? "Following": "Follow"}</a>
       </div>
       : <img className="hero-img" src={dao.banner_url} alt="Banner Image" />
     }
