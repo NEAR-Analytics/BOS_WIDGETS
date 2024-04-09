cb07cd39ba3dadcd3ac0b694b0aada183123c8bf
ndcdev.near <ndcdev.near>
2024-04-09 18:25:34 +0000

    Update daos.Pages.Home by ndcdev.near at 2024-04-09T18:25:34.909Z

--- ndcdev.near/daos.Pages.Home/source_code.jsx
+++ ndcdev.near/daos.Pages.Home/source_code.jsx
@@ -246,7 +246,7 @@ return (
         props={{
           daos,
           totalTreasury: 3893275,
-          deliverTreasury: 362253,
+          deliverTreasury: 292189,
           typeOfProject,
           loading,
           text: content.communityTreasury.metrics,
