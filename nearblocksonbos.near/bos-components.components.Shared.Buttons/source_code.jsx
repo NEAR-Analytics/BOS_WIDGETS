c5ebc6d4255b166e32050bc26977142bfde97044
nearblocksonbos.near <nearblocksonbos.near>
2024-04-10 11:23:01 +0000

    Update bos-components.components.Shared.Buttons by nearblocksonbos.near at 2024-04-10T11:23:01.959Z

--- nearblocksonbos.near/bos-components.components.Shared.Buttons/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Shared.Buttons/source_code.jsx
@@ -35,15 +35,9 @@ const QRCodeIcon = (props) => {
 
 
 
-
 const QrCode = (props) => {
-  const colorDark = props.theme === 'dark' ? '#ffffff' : '#000000';
-  const colorLight = props.theme === 'dark' ? '#000000' : '#ffffff';
-
   const srcData = `
-    <html style="background-color: ${
-      props.theme === 'dark' ? '#0d0d0d' : '#fffff'
-    };">
+    <html>
     <body>
       <div id="qrcode" style="display: flex; flex-direction: column; justify-content: center; align-items: center;"></div>
     
@@ -53,8 +47,8 @@ const QrCode = (props) => {
         text: "${props.value}",
         width: ${props.width},
         height: ${props.height},
-        colorDark:"${colorDark}",
-        colorLight: "${colorLight}",
+        colorDark: "#000000",
+        colorLight: "#ffffff",
         correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
         logoBackgroundTransparent: true,
         logoWidth: 150, 
@@ -69,7 +63,7 @@ const QrCode = (props) => {
     `;
 
   return (
-    <div className="">
+    <div>
       <iframe
         srcDoc={srcData}
         style={{
@@ -78,7 +72,6 @@ const QrCode = (props) => {
           height: 320,
           minHeight: 320,
           marginTop: 30,
-          backgroundColor: 'transparent',
         }}
       />
     </div>
@@ -139,7 +132,6 @@ const CloseCircle = (props) => {
 
 
 
-
 function MainComponent(props) {
   const [showTooltip, setShowTooltip] = useState(false);
   const onCopyClick = () => {
@@ -152,59 +144,50 @@ function MainComponent(props) {
   return (
     <>
       <span className="inline-flex space-x-2 h-7">
-        <OverlayTrigger
-          placement="bottom"
-          delay={{ show: 500, hide: 0 }}
-          popperConfig={{
-            modifiers: {
-              name: 'offset',
-              options: {
-                offset: [0, 5],
-              },
-            },
-          }}
-          overlay={
-            <Tooltip className="fixed h-auto max-w-[6rem] sm:max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2 break-words">
+        <Tooltip.Provider>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <button
+                type="button"
+                onClick={onCopyClick}
+                className="bg-green-500 bg-opacity-10 hover:bg-opacity-100 group rounded-full p-1.5 w-7 h-7"
+              >
+                <CopyIcon className="fill-current -z-50 text-green-500 group-hover:text-white h-4 w-4" />
+              </button>
+            </Tooltip.Trigger>
+            <Tooltip.Content
+              className="h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2 break-words"
+              sideOffset={5}
+              side="bottom"
+            >
               {showTooltip ? 'Copied!' : 'Copy account ID to clipboard'}
-            </Tooltip>
-          }
-        >
-          <button
-            type="button"
-            onClick={onCopyClick}
-            className="bg-green-500 dark:bg-black-200 bg-opacity-10 hover:bg-opacity-100 group rounded-full p-1.5 w-7 h-7"
-          >
-            <CopyIcon className="fill-current -z-50 text-green-500 dark:text-green-250 group-hover:text-white h-4 w-4" />
-          </button>
-        </OverlayTrigger>
+              <Tooltip.Arrow className="fill-white" />
+            </Tooltip.Content>
+          </Tooltip.Root>
+        </Tooltip.Provider>
         <Dialog.Root>
           <Dialog.Trigger asChild>
-            <button className="bg-green-500 dark:bg-black-200 items-center bg-opacity-10 hover:bg-opacity-100 group rounded-full p-1.5 w-7 h-7">
-              <OverlayTrigger
-                placement="bottom"
-                popperConfig={{
-                  modifiers: {
-                    name: 'offset',
-                    options: {
-                      offset: [0, 8],
-                    },
-                  },
-                }}
-                delay={{ show: 500, hide: 0 }}
-                overlay={
-                  <Tooltip className="fixed h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2 break-words">
+            <button className="bg-green-500 items-center bg-opacity-10 hover:bg-opacity-100 group rounded-full p-1.5 w-7 h-7">
+              <Tooltip.Provider>
+                <Tooltip.Root>
+                  <Tooltip.Trigger asChild>
+                    <QRCodeIcon className="fill-current text-green-500 group-hover:text-white h-4 w-4" />
+                  </Tooltip.Trigger>
+                  <Tooltip.Content
+                    className="h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2 break-words"
+                    sideOffset={8}
+                    side="bottom"
+                  >
                     Click to view QR Code
-                  </Tooltip>
-                }
-              >
-                <QRCodeIcon className="fill-current text-green-500 dark:text-green-250 group-hover:text-white h-4 w-4" />
-              </OverlayTrigger>
+                  </Tooltip.Content>
+                </Tooltip.Root>
+              </Tooltip.Provider>
             </button>
           </Dialog.Trigger>
           <Dialog.Overlay className="bg-green-500 bg-opacity-10 data-[state=open]:animate-overlayShow fixed inset-0" />
-          <Dialog.Content className="z-50 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] overflow-clip bg-white dark:bg-black-600 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] w-96 h-96 ">
+          <Dialog.Content className="z-50 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] w-96 h-96 ">
             <Dialog.Title>
-              <div className="flex items-center justify-between bg-gray-100 dark:bg-black-200 dark:text-neargray-10 px-3 py-4">
+              <div className="flex items-center justify-between bg-gray-100 px-3 py-4">
                 <h4 className="flex items-center text-xs break-all">
                   {props.id}
                 </h4>
@@ -219,12 +202,7 @@ function MainComponent(props) {
               </div>
             </Dialog.Title>
             <div className="p-4">
-              <QrCode
-                value={props.id}
-                width={160}
-                height={160}
-                theme={props.theme}
-              />
+              <QrCode value={props.id} width={160} height={160} />
             </div>
           </Dialog.Content>
         </Dialog.Root>
