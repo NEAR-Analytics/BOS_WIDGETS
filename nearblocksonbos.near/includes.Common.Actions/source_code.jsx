4d70c4892cf514124f74ecf33c84ea8902460e5d
nearblocksonbos.near <nearblocksonbos.near>
2024-04-10 11:20:37 +0000

    Update includes.Common.Actions by nearblocksonbos.near at 2024-04-10T11:20:37.414Z

---
+++ nearblocksonbos.near/includes.Common.Actions/source_code.jsx
@@ -0,0 +1,25 @@
+
+import CreateAccount from '@/includes/Common/Action/CreateAccount';
+import DeleteAccount from '@/includes/Common/Action/DeleteAccount';
+import DeployContract from '@/includes/Common/Action/DeployContract';
+import Stake from '@/includes/Common/Action/Stake';
+import Transfer from '@/includes/Common/Action/Transfer';
+
+const Actions = (props) => {
+  switch (props.action.action_kind) {
+    case 'CreateAccount':
+      return <CreateAccount action={props.action} ownerId={props.ownerId} />;
+    case 'DeleteAccount':
+      return <DeleteAccount action={props.action} ownerId={props.ownerId} />;
+    case 'DeployContract':
+      return <DeployContract action={props.action} ownerId={props.ownerId} />;
+    case 'Stake':
+      return <Stake action={props.action} ownerId={props.ownerId} />;
+    case 'Transfer':
+      return <Transfer action={props.action} ownerId={props.ownerId} />;
+    default:
+      return null;
+  }
+};
+
+export default Actions;
