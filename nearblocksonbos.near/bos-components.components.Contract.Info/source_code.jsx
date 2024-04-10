8aa35ee4cfb66599e4b2eab4682bf5447f1acba5
nearblocksonbos.near <nearblocksonbos.near>
2024-04-10 11:23:01 +0000

    Update bos-components.components.Contract.Info by nearblocksonbos.near at 2024-04-10T11:23:01.959Z

--- nearblocksonbos.near/bos-components.components.Contract.Info/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Contract.Info/source_code.jsx
@@ -8,9 +8,7 @@
  * @param {string} [id] - The account identifier passed as a string.
  * @param {ContractInfo} [contract] - Object containing information about the associated contract.
  * @param {string} ownerId - The identifier of the owner of the component.
- * @param {boolean} [isLocked] - Boolean indicating whether the account or contract with full access key or not.
  */
-
 
 
 
@@ -46,7 +44,7 @@ const Question = (props) => {
 
 
 function MainComponent(props) {
-  const { network, id, contract, ownerId, isLocked } = props;
+  const { network, id, contract, ownerId } = props;
 
   const { convertToUTC } = VM.require(
     `${ownerId}/widget/includes.Utils.formats`,
@@ -102,29 +100,32 @@ function MainComponent(props) {
   const Loader = (props) => {
     return (
       <div
-        className={`bg-gray-200 dark:bg-black-200 h-5 rounded shadow-sm animate-pulse ${props.className} ${props.wrapperClassName}`}
+        className={`bg-gray-200 h-5 rounded shadow-sm animate-pulse ${props.className} ${props.wrapperClassName}`}
       ></div>
     );
   };
 
   return (
-    <div className="w-full border-t dark:border-black-200">
-      <div className="h-full bg-white dark:bg-black-600 text-sm text-gray-500 dark:text-neargray-10">
+    <div className="w-full border-t">
+      <div className="h-full bg-white text-sm text-gray-500 ">
         <div className="flex flex-wrap p-4">
           <div className="flex items-center w-full md:w-1/4 mb-2 md:mb-0">
-            <OverlayTrigger
-              placement="bottom-start"
-              delay={{ show: 500, hide: 0 }}
-              overlay={
-                <Tooltip className="fixed h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2">
+            <Tooltip.Provider>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <div>
+                    <Question className="w-4 h-4 fill-current mr-1" />
+                  </div>
+                </Tooltip.Trigger>
+                <Tooltip.Content
+                  className="h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2"
+                  align="start"
+                  side="bottom"
+                >
                   Latest time the contract deployed.
-                </Tooltip>
-              }
-            >
-              <div>
-                <Question className="w-4 h-4 fill-current mr-1" />
-              </div>
-            </OverlayTrigger>
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
             Last Updated
           </div>
           {loading ? (
@@ -140,20 +141,23 @@ function MainComponent(props) {
         </div>
         <div className="flex flex-wrap p-4">
           <div className="flex items-center w-full md:w-1/4 mb-2 md:mb-0 ">
-            <OverlayTrigger
-              placement="bottom-start"
-              delay={{ show: 500, hide: 0 }}
-              overlay={
-                <Tooltip className="fixed h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2">
+            <Tooltip.Provider>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <div>
+                    <Question className="w-4 h-4 fill-current mr-1" />
+                  </div>
+                </Tooltip.Trigger>
+                <Tooltip.Content
+                  className="h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2"
+                  align="start"
+                  side="bottom"
+                >
                   The transaction unique identifier (hash) that the contract is
                   latest deployed.
-                </Tooltip>
-              }
-            >
-              <div>
-                <Question className="w-4 h-4 fill-current mr-1" />
-              </div>
-            </OverlayTrigger>
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
             Transaction Hash
           </div>
           {loading ? (
@@ -165,7 +169,7 @@ function MainComponent(props) {
                   href={`/txns/${action.transaction_hash}`}
                   className="hover:no-underline"
                 >
-                  <a className="text-green-500 dark:text-green-250 hover:no-underline">
+                  <a className="text-green-500 hover:no-underline">
                     {action.transaction_hash}
                   </a>
                 </Link>
@@ -175,45 +179,51 @@ function MainComponent(props) {
         </div>
         <div className="flex flex-wrap p-4">
           <div className="flex items-center w-full md:w-1/4 mb-2 md:mb-0 ">
-            <OverlayTrigger
-              placement="bottom-start"
-              delay={{ show: 500, hide: 0 }}
-              overlay={
-                <Tooltip className="fixed h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2">
+            <Tooltip.Provider>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <div>
+                    <Question className="w-4 h-4 fill-current mr-1" />
+                  </div>
+                </Tooltip.Trigger>
+                <Tooltip.Content
+                  className="h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2"
+                  align="start"
+                  side="bottom"
+                >
                   Locked contract means that there are no access keys allowing
                   the contract code to be re-deployed
-                </Tooltip>
-              }
-            >
-              <div>
-                <Question className="w-4 h-4 fill-current mr-1" />
-              </div>
-            </OverlayTrigger>
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
             Contract Locked
           </div>
           {loading ? (
             <Loader wrapperClassName="w-32" />
           ) : (
             <div className="w-full md:w-3/4 break-words">
-              {contract?.code_base64 && isLocked ? 'Yes' : 'No'}
+              {contract?.locked ? 'Yes' : 'No'}
             </div>
           )}
         </div>
         <div className="flex flex-wrap p-4">
           <div className="flex items-center w-full md:w-1/4 mb-2 md:mb-0 ">
-            <OverlayTrigger
-              placement="bottom-start"
-              delay={{ show: 500, hide: 0 }}
-              overlay={
-                <Tooltip className="fixed h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2">
+            <Tooltip.Provider>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <div>
+                    <Question className="w-4 h-4 fill-current mr-1" />
+                  </div>
+                </Tooltip.Trigger>
+                <Tooltip.Content
+                  className="h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2"
+                  align="start"
+                  side="bottom"
+                >
                   Checksum (SHA-256 in base58 encoding) of the contract binary.
-                </Tooltip>
-              }
-            >
-              <div>
-                <Question className="w-4 h-4 fill-current mr-1" />
-              </div>
-            </OverlayTrigger>
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
             Code Hash
           </div>
           {loading ? (
