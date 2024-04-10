86012e54bb08a40db9ec8ca465a81f162a89521c
flowscience.near <flowscience.near>
2024-04-10 04:27:18 +0000

    Update easRenderAttestation by flowscience.near at 2024-04-10T04:27:18.871Z

--- flowscience.near/easRenderAttestation/source_code.jsx
+++ flowscience.near/easRenderAttestation/source_code.jsx
@@ -2,42 +2,31 @@ const easRenderAttestation = ({ attestation }) => {
   return (
     <div className="attestation">
       <h2>Attestation Details</h2>
-      <p>
-        <strong>UID:</strong> {attestation.uid}
-      </p>
-      <p>
-        <strong>Schema:</strong> {attestation.schema}
-      </p>
-      <p>
-        <strong>Time:</strong>{" "}
-        {new Date(attestation.time * 1000).toLocaleString()}
-      </p>
-      <p>
-        <strong>Expiration Time:</strong>{" "}
-        {new Date(attestation.expirationTime * 1000).toLocaleString()}
-      </p>
-      <p>
-        <strong>Revocation Time:</strong>{" "}
-        {attestation.revocationTime
-          ? new Date(attestation.revocationTime * 1000).toLocaleString()
-          : "Not revoked"}
-      </p>
-      <p>
-        <strong>Ref UID:</strong> {attestation.refUID}
-      </p>
-      <p>
-        <strong>Recipient:</strong> {attestation.recipient}
-      </p>
-      <p>
-        <strong>Attester:</strong> {attestation.attester}
-      </p>
-      <p>
-        <strong>Revocable:</strong> {attestation.revocable ? "Yes" : "No"}
-      </p>
-      <p>
-        <strong>Data:</strong>{" "}
-        {attestation.data ? attestation.data.toString("hex") : "No data"}
-      </p>
+      <strong>UID:</strong> {attestation.uid}
+      <br />
+      <strong>Schema:</strong> {attestation.schema}
+      <br />
+      <strong>Time:</strong>{" "}
+      {new Date(attestation.time * 1000).toLocaleString()}
+      <br />
+      <strong>Expiration Time:</strong>{" "}
+      {new Date(attestation.expirationTime * 1000).toLocaleString()}
+      <br />
+      <strong>Revocation Time:</strong>{" "}
+      {attestation.revocationTime
+        ? new Date(attestation.revocationTime * 1000).toLocaleString()
+        : "Not revoked"}
+      <br />
+      <strong>Ref UID:</strong> {attestation.refUID}
+      <br />
+      <strong>Recipient:</strong> {attestation.recipient}
+      <br />
+      <strong>Attester:</strong> {attestation.attester}
+      <br />
+      <strong>Revocable:</strong> {attestation.revocable ? "Yes" : "No"}
+      <br />
+      <strong>Data:</strong>{" "}
+      {attestation.data ? attestation.data.toString("hex") : "No data"}
     </div>
   );
 };
