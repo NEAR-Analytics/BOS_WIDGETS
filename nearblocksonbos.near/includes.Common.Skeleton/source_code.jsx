eb280d1738a1a9828af6395e0cf1a28ff12d40dc
nearblocksonbos.near <nearblocksonbos.near>
2024-04-10 11:20:37 +0000

    Update includes.Common.Skeleton by nearblocksonbos.near at 2024-04-10T11:20:37.414Z

---
+++ nearblocksonbos.near/includes.Common.Skeleton/source_code.jsx
@@ -0,0 +1,18 @@
+/**
+ * @interface Props
+ * @param {string} [className] - The CSS class name(s) for styling purposes.
+ */
+
+
+
+
+
+const Skeleton = (props) => {
+  return (
+    <div
+      className={`bg-gray-200  rounded shadow-sm animate-pulse ${props.className}`}
+    ></div>
+  );
+};
+
+export default Skeleton;
