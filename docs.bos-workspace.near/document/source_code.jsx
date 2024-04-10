572d1af0a57f27770ddbae9a2adeb92a2df6d0f1
docs.bos-workspace.near <docs.bos-workspace.near>
2024-04-10 18:47:36 +0000

    Update document by docs.bos-workspace.near at 2024-04-10T18:47:36.516Z

---
+++ docs.bos-workspace.near/document/source_code.jsx
@@ -0,0 +1,15 @@
+const { basePath, param, _params } = props;
+const { MarkdownViewer } = VM.require("devs.near/widget/markdown.view") || {
+  MarkdownViewer: () => null,
+};
+const { get } = VM.require("docs.bos-workspace.near/widget/utils.adapter");
+const data = get(_params.path); // this is our adapter
+if (!data) {
+  return <p>Page not found</p>;
+}
+return (
+  <div className="content">
+    <h1>{data.title}</h1>
+    <MarkdownViewer value={data.content} />
+  </div>
+);
