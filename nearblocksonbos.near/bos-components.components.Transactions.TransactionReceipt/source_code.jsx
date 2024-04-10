c74a2d11aed369bf35ac98492bdb5c23c728d28d
nearblocksonbos.near <nearblocksonbos.near>
2024-04-10 11:22:44 +0000

    Update bos-components.components.Transactions.TransactionReceipt by nearblocksonbos.near at 2024-04-10T11:22:44.624Z

--- nearblocksonbos.near/bos-components.components.Transactions.TransactionReceipt/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Transactions.TransactionReceipt/source_code.jsx
@@ -24,10 +24,7 @@ const ArrowDown = (props) => {
       {...props}
     >
       <path fill="none" d="M0 0h24v24H0z" />
-      <path
-        fill="currentColor"
-        d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z"
-      />
+      <path d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z" />
     </svg>
   );
 };/* END_INCLUDE COMPONENT: "includes/icons/ArrowDown.jsx" */
@@ -72,13 +69,13 @@ function MainComponent(props) {
         className={`${
           convertionReceipt
             ? 'pl-0 border-transparent'
-            : 'pl-4 md:pl-8 border-green-500 dark:border-green-250'
+            : 'pl-12 border-green-500 '
         } ${className} `}
       >
         {convertionReceipt ? (
           <div className="flex flex-row mb-2.5">
-            <div className="bg-gray-200 dark:bg-black-200 h-5 w-5 rounded-full mr-3"></div>
-            <div className="text-green-500 dark:text-green-250 text-sm">
+            <div className="bg-gray-200 h-5 w-5 rounded-full mr-3"></div>
+            <div className="text-green-500 text-sm">
               {receipt?.predecessorId}
             </div>
           </div>
@@ -94,12 +91,12 @@ function MainComponent(props) {
               expandAll: expandAll,
               fellowOutgoingReceipts: remainingFellowOutgoingReceipts,
               convertionReceipt: false,
-              className: 'pb-4 !mt-0 border-l mt-2.5 ml-2.5',
+              className: 'pb-5 !mt-0 border-l mt-2.5 ml-2.5',
               ownerId,
             }}
           />
         ) : null}
-        <div className="flex flex-col relative border-l border-green-500 dark:border-green-250 py-2 pl-6 ml-2.5">
+        <div className="flex flex-col relative border-l border-green-500  py-2 pl-7 ml-2.5">
           {receipt?.actions &&
             receipt?.actions.map((action, index) => (
               <Widget
@@ -117,7 +114,7 @@ function MainComponent(props) {
             ))}
         </div>
         {isTxTypeActive ? (
-          <div className="border-l border-green-500 dark:border-green-250 ml-2.5">
+          <div className="border-l border-black ml-2.5">
             <Widget
               src={`${ownerId}/widget/bos-components.components.Transactions.ReceiptInfo`}
               props={{
@@ -131,12 +128,10 @@ function MainComponent(props) {
         ) : null}
         <div className="relative flex flex-row my-2.5">
           <ArrowDown
-            className={`absolute left-0.5 -top-5 ml-px  w-4 h-4 fill-current text-green-500 dark:text-green-250`}
+            className={`absolute left-0.5 -top-5 ml-px  w-4 h-4 text-green-500`}
           />
-          <div className="bg-gray-200 dark:bg-black-200 h-5 w-5 rounded-full mr-3"></div>
-          <div className="text-green-500 dark:text-green-250 text-sm ">
-            {receipt?.receiverId}
-          </div>
+          <div className="bg-gray-200 h-5 w-5 rounded-full mr-3"></div>
+          <div className="text-green-500 text-sm ">{receipt?.receiverId}</div>
         </div>
       </div>
       {lastNonRefundNestedReceipt ? (
