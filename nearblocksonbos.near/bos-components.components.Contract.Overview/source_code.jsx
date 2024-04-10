191334a39231d521523eea66ae94ae6e51dca32e
nearblocksonbos.near <nearblocksonbos.near>
2024-04-10 11:23:01 +0000

    Update bos-components.components.Contract.Overview by nearblocksonbos.near at 2024-04-10T11:23:01.959Z

--- nearblocksonbos.near/bos-components.components.Contract.Overview/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Contract.Overview/source_code.jsx
@@ -8,7 +8,6 @@
  * @param {Function} [t] - A function for internationalization (i18n) provided by the next-translate package.
  * @param {string} [id] - The account identifier passed as a string.
  * @param {ContractInfo} [contract] - Information about the user's contract.
- * @param {boolean} [isLocked] - Boolean indicating whether the account or contract with full access key or not.
  * @param {any} [schema] - The schema data for the component.
  * @param {ContractParseInfo} [contractInfo] - Additional parsed information about the contract.
  * @param {Function} [requestSignInWithWallet] - Function to initiate sign-in with a wallet.
@@ -34,18 +33,12 @@
 
 
 
-
-
-
-
-
 function MainComponent(props) {
   const {
     network,
     t,
     id,
     contract,
-    isLocked,
     schema,
     contractInfo,
     requestSignInWithWallet,
@@ -66,9 +59,7 @@ function MainComponent(props) {
   return (
     <Tabs.Root
       defaultValue={pageTab}
-      className={
-        'bg-white dark:bg-black-600  soft-shadow rounded-xl pb-1 px-4 py-3'
-      }
+      className={'bg-white soft-shadow rounded-xl pb-1 px-4 py-3'}
     >
       <Tabs.List>
         {tabs &&
@@ -78,10 +69,8 @@ function MainComponent(props) {
               onClick={() => {
                 onTab(index);
               }}
-              className={`px-2 mr-1 md:px-3 border dark:border-black-200 py-2 mb-3 text-xs font-medium rounded-md text-gray-500 dark:text-neargray-10 hover:text-green-500 dark:hover:text-green-250 dark:hover:border-green-250 hover:border-green-500 cursor-pointer outline-none ${
-                pageTab === tab
-                  ? 'text-green-500 dark:text-green-250 border-green-500 dark:border-green-250'
-                  : ''
+              className={`px-2 mr-1 md:px-3 border py-2 mb-3 text-xs font-medium rounded-md text-gray-500 hover:text-green-500 hover:border-green-500 cursor-pointer outline-none ${
+                pageTab === tab ? 'text-green-500 border-green-500' : ''
               }`}
               value={tab}
             >
@@ -104,47 +93,49 @@ function MainComponent(props) {
               t: t,
               id: id,
               contract: contract,
-              isLocked: isLocked,
               ownerId,
             }}
           />
         }
       </Tabs.Content>
       <Tabs.Content value={tabs[1]}>
-        <div className="border-t dark:border-black-200 p-4">
+        <div className="border-t p-4">
           {connected ? (
-            <OverlayTrigger
-              placement="bottom-start"
-              delay={{ show: 500, hide: 0 }}
-              overlay={
-                <Tooltip className="fixed h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2">
-                  Connected to Contract
-                </Tooltip>
-              }
-            >
-              <button
-                className="px-2 mr-1 md:px-3 bg-neargreen py-2 text-xs font-medium rounded-md text-white inline-flex items-center"
-                onClick={logOut}
-              >
-                <span className="h-3 w-3 inline-block rounded-full mr-2 bg-white dark:bg-black-600 dark:text-neargray-10" />
-                Connected
-              </button>
-            </OverlayTrigger>
+            <Tooltip.Provider>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <button
+                    className="px-2 mr-1 md:px-3 bg-neargreen py-2 text-xs font-medium rounded-md text-white inline-flex items-center"
+                    onClick={logOut}
+                  >
+                    <span className="h-3 w-3 inline-block rounded-full mr-2 bg-white" />
+                    Connected
+                  </button>
+                </Tooltip.Trigger>
+                <Tooltip.Content
+                  className="h-auto max-w-xs bg-black bg-opacity-90 z-10 text-xs text-white px-3 py-2"
+                  align="start"
+                  side="bottom"
+                >
+                  Connect to Contract
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
           ) : (
             <button
               className="px-2 mr-1 md:px-3 bg-red-400 py-2 text-xs font-medium rounded-md text-white inline-flex items-center"
               onClick={requestSignInWithWallet}
             >
-              <span className="h-3 w-3 inline-block rounded-full mr-2 bg-white dark:bg-black-600 dark:text-neargray-10 animate-pulse" />
+              <span className="h-3 w-3 inline-block rounded-full mr-2 bg-white animate-pulse" />
               Connect to Contract
             </button>
           )}
         </div>
         {!schema && (
-          <p className="text-xs mx-5 text-gray-500 mb-4  bg-gray-100 dark:bg-black-200 px-2 py-2  w-fit rounded shadow">
+          <p className="text-xs mx-5 text-gray-500 mb-4  bg-gray-100 px-2 py-2  w-fit rounded shadow">
             Contracts with Near{' '}
             <a
-              className="text-green-500 dark:text-green-250"
+              className="text-green-500 "
               target="_blank"
               href="https://github.com/near/abi"
               rel="noreferrer noopener nofollow"
@@ -159,7 +150,7 @@ function MainComponent(props) {
         {schema?.body?.functions.length > 0 ? (
           <Accordion.Root
             type="multiple"
-            className="contract-accordian text-gray-600 dark:text-neargray-10 px-4 pt-4 border-t dark:border-black-200 w-full"
+            className="contract-accordian text-gray-600 px-4 pt-4 border-t w-full"
             collapsible
           >
             {schema?.body?.functions?.map((func, index) => (
@@ -185,7 +176,7 @@ function MainComponent(props) {
           contractInfo?.methodNames?.length > 0 && (
             <Accordion.Root
               type="multiple"
-              className="contract-accordian text-gray-600 dark:text-neargray-10  px-4 pt-4 border-t dark:border-black-200 w-full"
+              className="contract-accordian text-gray-600 px-4 pt-4 border-t w-full"
               collapsible
             >
               {contractInfo?.methodNames?.map((method, index) => (
