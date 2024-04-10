50e8ae6d710a35398e947cf5c0b418691b329511
nearblocksonbos.near <nearblocksonbos.near>
2024-04-10 11:23:01 +0000

    Update bos-components.components.Shared.TokenInfo by nearblocksonbos.near at 2024-04-10T11:23:01.959Z

--- nearblocksonbos.near/bos-components.components.Shared.TokenInfo/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Shared.TokenInfo/source_code.jsx
@@ -74,7 +74,7 @@ function MainComponent(props) {
   const Loader = (props) => {
     return (
       <div
-        className={`bg-gray-200 dark:bg-black-200 h-5 rounded shadow-sm animate-pulse ${props.className} ${props?.wrapperClassName}`}
+        className={`bg-gray-200 h-5 rounded shadow-sm animate-pulse ${props.className} ${props?.wrapperClassName}`}
       ></div>
     );
   };
@@ -120,28 +120,24 @@ function MainComponent(props) {
   }, [contract, config?.rpcUrl]);
 
   return !meta?.name ? (
-    <Loader wrapperClassName="flex w-full max-w-xs" />
+    <Loader wrapperClassName="flex w-full max-w-xl" />
   ) : (
     <>
       <span className="font-normal px-1">
         {amount
-          ? localFormat(tokenAmount(amount, decimals || meta?.decimals, true))
+          ? localFormat(tokenAmount(amount, meta?.decimals || decimals, true))
           : amount ?? ''}
       </span>
-      <Link href={`/token/${contract}`} className="hover:no-underline">
-        <a className="text-green flex items-center hover:no-underline dark:text-green-250">
-          <span className="flex items-center">
-            <TokenImage
-              src={meta?.icon}
-              alt={meta?.name}
-              appUrl={config?.appUrl}
-              className="w-4 h-4 mx-1"
-            />
-            {shortenToken(meta?.name)}
-            <span>&nbsp;({shortenTokenSymbol(meta?.symbol)})</span>
-          </span>
-        </a>
-      </Link>
+      <span className="flex items-center">
+        <TokenImage
+          src={meta?.icon}
+          alt={meta?.name}
+          appUrl={config?.appUrl}
+          className="w-4 h-4 mx-1"
+        />
+        {shortenToken(meta?.name)}
+        <span>&nbsp;({shortenTokenSymbol(meta?.symbol)})</span>
+      </span>
     </>
   );
 }
