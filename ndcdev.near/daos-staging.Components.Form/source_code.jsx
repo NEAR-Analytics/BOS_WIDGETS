9066e259021447c1c722c44aec8a1826fbc9b1d6
ndcdev.near <ndcdev.near>
2024-04-10 13:01:02 +0000

    Update daos-staging.Components.Form by ndcdev.near at 2024-04-10T13:01:02.656Z

--- ndcdev.near/daos-staging.Components.Form/source_code.jsx
+++ ndcdev.near/daos-staging.Components.Form/source_code.jsx
@@ -102,7 +102,7 @@ const ProposalButton = () => (
         Create
       </>
     )}
-    post
+    {formEls.post_type}
   </button>
 );
 
@@ -128,22 +128,24 @@ return (
       </MobileForm>
     ) : (
       <Form className="d-flex flex-column gap-3">
-        <div
-          onClick={() => {
-            const newFormEl = formEls;
-            newFormEl.post_type =
-              formEls.post_type === "Proposal" ? "Report" : "Proposal";
-            setFormEls(newFormEl);
-          }}
-        >
-          <p className="mb-2">Post type</p>
-          <div className="d-flex gap-3 align-items-center">
-            <Widget src={`ndcdev.near/widget/daos-staging.Components.Switch`} />
-            <TypeSection>
-              <h4>{formEls.post_type}</h4>
-            </TypeSection>
+        {!id && (
+          <div
+            onClick={() => {
+              const newFormEl = formEls;
+              newFormEl.post_type =
+                formEls.post_type === "Proposal" ? "Report" : "Proposal";
+              setFormEls(newFormEl);
+            }}
+          >
+            <p className="mb-2">Post type</p>
+            <div className="d-flex gap-3 align-items-center">
+              <Widget src={`ndcdev.near/widget/daos-staging.Components.Switch`} />
+              <TypeSection>
+                <h4>{formEls.post_type}</h4>
+              </TypeSection>
+            </div>
           </div>
-        </div>
+        )}
         <div className="form-element">
           <label>Select DAO</label>
           <select
