94ff9a67e425e25452a7a6973b73b793c16c738d
ndcdev.near <ndcdev.near>
2024-04-10 13:04:54 +0000

    Update dashboard.Components.Aggregators.Item by ndcdev.near at 2024-04-10T13:04:54.486Z

--- ndcdev.near/Dashboard.Components.Aggregators.Item/source_code.jsx
+++ ndcdev.near/Dashboard.Components.Aggregators.Item/source_code.jsx
@@ -1,10 +1,3 @@
-const Circle = styled.div`
-  width: 10px;
-  height: 10px;
-  border-radius: 50%;
-  background: ${(p) => p.color};
-`;
-
 const { value, text, color, loading } = props;
 
 const Loading = () => <Widget src="flashui.near/widget/Loading" />;
@@ -17,21 +10,18 @@ const formatValue = (value) => {
   return val >= 1000000000
     ? `${parseFloat(val / 1000000000).toFixed(2)}B`
     : val >= 1000000
-      ? `${parseFloat(val / 1000000).toFixed(2)}M`
-      : val >= 1000
-        ? `${parseFloat(val / 1000).toFixed(2)}K`
-        : Number.isInteger(val)
-          ? val
-          : val.toFixed(2);
+    ? `${parseFloat(val / 1000000).toFixed(2)}M`
+    : val >= 1000
+    ? `${parseFloat(val / 1000).toFixed(2)}K`
+    : Number.isInteger(val)
+    ? val
+    : val.toFixed(2);
 };
 
 return (
   <div className="item">
-    <div className="inner">
-      {loading ? <Loading /> : <span>{formatValue(value)}</span>}
-    </div>
-    <div className="d-flex justify-content-center align-items-center gap-2">
-      <Circle color={color} /> <span>{text}</span>
-    </div>
+    <div className="value">{loading ? <Loading /> : formatValue(value)}</div>
+    <div className="divider" />
+    <div className="title">{text}</div>
   </div>
 );
