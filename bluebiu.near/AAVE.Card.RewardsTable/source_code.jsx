b7b990c8ba786079810cf2373c388b72c7cc77a6
bluebiu.near <bluebiu.near>
2024-04-11 15:02:24 +0000

    Update AAVE.Card.RewardsTable by bluebiu.near at 2024-04-11T15:02:24.470Z

--- bluebiu.near/AAVE.Card.RewardsTable/source_code.jsx
+++ bluebiu.near/AAVE.Card.RewardsTable/source_code.jsx
@@ -1,6 +1,6 @@
 const RewardsTable = styled.div`
   background-color: rgba(53, 55, 73, 0.2);
-  margin-top: 10px;
+  margin-top: 20px;
   border-radius: 6px;
 `;
 const Title = styled.div`
@@ -34,9 +34,25 @@ const {
   onSuccess,
   markets,
   rewardAddress,
+  formatNumber,
   config,
+  prices,
   theme,
 } = props;
+
+const TokenAsset = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+const TokenIcon = styled.img`
+  width: 28px;
+`;
+const TokenAmount = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 const columns = [
   {
@@ -54,11 +70,29 @@ const columns = [
 ];
 
 function formatValue(value, digits) {
+  if (Number(value) === 0) return "0";
   return Big(value || 0).lt(0.01)
     ? "< 0.01"
     : `${Number(value).toFixed(digits || 2)}`;
 }
+const seamTokens = ["OG Points", "SEAM", "esSEAM"];
 
+function renderUSD(symbol, amount) {
+  if (symbol === "earlyZERO") {
+    return formatNumber(Big(0.00025055).times(Big(amount)).toFixed());
+  }
+  // seamless
+  if (seamTokens.includes(symbol)) {
+    return formatNumber(
+      Big(prices["SEAM"] || 0)
+        .times(Big(amount))
+        .toFixed()
+    );
+  }
+  if (!prices[symbol]) return null;
+
+  return formatNumber(Big(prices[symbol]).times(Big(amount)).toFixed());
+}
 console.log("reward_table--", data);
 return (
   <>
@@ -73,24 +107,30 @@ return (
             headers: ["Reward Asset", "Rewards", ""],
             data: data.map((row) => {
               return [
-                `${row.symbol}`,
-                <div>{row.unclaimed ? formatValue(row.unclaimed) : "-"}</div>,
+                <TokenAsset>
+                  {row.icon ? <TokenIcon src={row.icon} /> : null}
+                  {`${row.symbol}`}
+                </TokenAsset>,
+                <TokenAmount>
+                  <span>
+                    {row.unclaimed ? formatValue(row.unclaimed) : "-"}
+                  </span>
+                  <span>{renderUSD(row.symbol, row.unclaimed)}</span>
+                </TokenAmount>,
+
                 <Widget
                   src={`${config.ownerId}/widget/AAVE.PrimaryButton`}
                   props={{
                     config,
                     theme,
+                    disabled: !Number(row.unclaimed),
                     width: 80,
-                    loading:
-                      row.symbol === state.dapp.symbol && state.loading
-                        ? true
-                        : false,
+                    loading: state.loading,
                     children: "Claim",
                     onClick: (record) => {
                       const toastId = toast?.loading({
                         title: `Claiming rewards...`,
                       });
-
                       State.update({
                         dapp: dapps,
                         loading: true,
