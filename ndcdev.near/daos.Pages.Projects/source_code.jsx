de608e8ee8b83ec164384c998f3673765f91badb
ndcdev.near <ndcdev.near>
2024-04-11 16:20:51 +0000

    Update daos.Pages.Projects by ndcdev.near at 2024-04-11T16:20:51.813Z

--- ndcdev.near/daos.Pages.Projects/source_code.jsx
+++ ndcdev.near/daos.Pages.Projects/source_code.jsx
@@ -4,41 +4,10 @@ let { assets, content, contractName } = VM.require(
 
 content = content.home;
 
-let projects = []
+const projects = Near.view(contractName, "get_dao_communities");
 
-
-// NDC
-let projectsDaoId1 = Near.view(contractName, "get_dao_communities", {
-  dao_id: parseInt(1),
-});
-
-let projectsDaoId2 = Near.view(contractName, "get_dao_communities", {
-  dao_id: parseInt(2),
-});
-
-// Marketing DAO
-let projectsDaoId4 = Near.view(contractName, "get_dao_communities", {
-  dao_id: parseInt(4),
-});
-
-// Gaming DAO
-let projectsDaoId3 = Near.view(contractName, "get_dao_communities", {
-  dao_id: parseInt(3),
-});
-
-// Aurora Community DAO
-let projectsDaoId8 = Near.view(contractName, "get_dao_communities", {
-  dao_id: parseInt(8),
-});
-
-let projectsDaoId10 = Near.view(contractName, "get_dao_communities", {
-  dao_id: parseInt(10),
-});
-
-if (!contractName || !content || !projects || !projectsDaoId1 || !projectsDaoId2 || !projectsDaoId4 || !projectsDaoId3 || !projectsDaoId8 || !projectsDaoId10)
+if (!contractName || !content || !projects)
   return <Widget src="flashui.near/widget/Loading" />;
-
-projects = [...projectsDaoId1, ...projectsDaoId2, ...projectsDaoId4, ...projectsDaoId3, ...projectsDaoId8, ...projectsDaoId10] 
 
 const Wrapper = styled.div`
   width: 80%;
