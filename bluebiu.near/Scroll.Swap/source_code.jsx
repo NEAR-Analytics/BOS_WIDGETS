5a1f6d34bf2e3cac0273a271bec81af394bffd45
bluebiu.near <bluebiu.near>
2024-04-11 14:17:56 +0000

    Update Scroll.Swap by bluebiu.near at 2024-04-11T14:17:56.800Z

--- bluebiu.near/Scroll.Swap/source_code.jsx
+++ bluebiu.near/Scroll.Swap/source_code.jsx
@@ -1,13 +1,13 @@
 const Container = styled.div`
-  --text-color: rgb(0, 255, 224);
-  --button-color: rgb(0, 255, 224);
+  --text-color: #ff684b;
+  --button-color: #ff684b;
   --border-color: rgb(44, 74, 75);
   --input-border-color: #2c334b;
-  --input-select-bg-color: rgb(31, 58, 62);
-  --secondary-text-color: rgb(204, 223, 221);
-  --thirdary-text-color: rgb(79, 115, 117);
-  --dex-active-text-color: black;
-  --button-text-color: black;
+  --input-select-bg-color: rgba(55, 58, 83, 0.5);
+  --secondary-text-color: #7c7f96;
+  --thirdary-text-color: #7c7f96;
+  --dex-active-text-color: #fff;
+  --button-text-color: #fff;
   --dex-hover-bg-color: rgba(0, 255, 224, 0.2);
 `;
 
