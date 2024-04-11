ddeff9e3854aa3dc85d212fde6e53779a68c23a1
bluebiu.near <bluebiu.near>
2024-04-11 12:48:58 +0000

    Update Scroll.BridgeAuthority.HandlerClaim by bluebiu.near at 2024-04-11T12:48:58.220Z

---
+++ bluebiu.near/Scroll.BridgeAuthority.HandlerClaim/source_code.jsx
@@ -0,0 +1,269 @@
+const {
+  amount,
+  account,
+  currency,
+  routerAddress,
+  routerEthAddress,
+  target,
+  loading,
+  onSuccess,
+  onError,
+  quote,
+  currentChainId,
+  mainnet,
+  toast,
+} = props;
+
+
+const Transactions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  color: var(--label-color);
+  gap: 20px;
+  .list {
+    flex: 1;
+    .claim-line {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 5px 0;
+      .btn {
+        cursor: pointer;
+        background-color: var(--button-color);
+        color: var(--button-text-color);
+      }
+    }
+
+  }
+  .fresh {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+`
+
+const RefreshText = styled.div`
+  text-decoration: underline;
+  font-size: 14px;
+  font-weight: 400;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+
+const ArrowIcon = styled.div`
+  transform: rotate(180deg);
+`;
+
+const L1MessageBridgeAbi = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_nonce",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bytes",
+        "name": "_message",
+        "type": "bytes"
+      },
+      {
+        "components": [
+          {
+            "internalType": "uint256",
+            "name": "batchIndex",
+            "type": "uint256"
+          },
+          {
+            "internalType": "bytes",
+            "name": "merkleProof",
+            "type": "bytes"
+          }
+        ],
+        "internalType": "struct IL1ScrollMessenger.L2MessageProof",
+        "name": "_proof",
+        "type": "tuple"
+      }
+    ],
+    "name": "relayMessageWithProof",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+];
+
+const L1MessageBridgeContract = new ethers.Contract(
+  '0x6774Bcbd5ceCeF1336b5300fb5186a12DDD8b367',
+  L1MessageBridgeAbi,
+  Ethers.provider().getSigner()
+)
+
+function handleClaim(claimInfo) {
+  if (!claimInfo) {
+    return
+  }
+
+  if (currentChainId !== mainnet.id) {
+    Ethers.send("wallet_switchEthereumChain", [
+      { chainId: `0x${Number(mainnet.id).toString(16)}` },
+    ]);
+    return
+  }
+
+  let toastId
+
+  toastId = toast?.loading({
+    title: `Claim From ETH`,
+  });
+
+  console.log('111',claimInfo.from,
+    claimInfo.to,
+    claimInfo.value,
+    claimInfo.nonce,
+    claimInfo.message,
+    {
+      batchIndex: claimInfo.proof.batch_index,
+      merkleProof: claimInfo.proof.merkle_proof,
+    },)
+
+  L1MessageBridgeContract.relayMessageWithProof(
+    claimInfo.from,
+    claimInfo.to,
+    claimInfo.value,
+    claimInfo.nonce,
+    claimInfo.message,
+    {
+      batchIndex: claimInfo.proof.batch_index,
+      merkleProof: claimInfo.proof.merkle_proof,
+    },
+    {
+      gasLimit: 5552000,
+    }).then((tx) => {
+      console.log(tx)
+      tx.wait()
+        .then((res) => {
+          console.log(res)
+          getAllClaimTx()
+
+          toast?.dismiss(toastId);
+
+          toast?.success({
+            title: "Claim Successfully!",
+            text: `Claim Successfully`,
+            tx: tx,
+            chainId: currentChainId,
+          });
+        })
+        .catch((err) => {
+          console.log(err)
+          toast?.fail({
+            title: "Claim Failed!",
+            text: "Claim Failed",
+            tx: '',
+            chainId: currentChainId,
+          });
+        });
+    })
+    .catch((err) => {
+      console.log(err)
+      toast?.fail({
+        title: "Claim Failed!",
+        text: `Claim Failed`,
+        tx: '',
+        chainId: currentChainId,
+      });
+    });
+}
+
+function getAllClaimTx() {
+  State.update({
+    isLoading: true,
+  })
+
+  asyncFetch(`https://mainnet-api-bridge.scroll.io/api/claimable?address=${account}&page_size=100&page=1`)
+    .then(res => {
+      console.log('res:', res)
+      if (res.body.data.result) {
+        const fromAccount = res.body.data.result[0].claimInfo.from
+        return asyncFetch(`https://mainnet-api-bridge-v2.scroll.io/api/l2/unclaimed/withdrawals?address=${fromAccount}&page=1&page_size=100`)
+      }
+
+    }).then(res => {
+      console.log('res2:', res)
+      State.update({
+        isLoading: false,
+        filteredTxs: res.body.data.results
+      })
+    })
+}
+
+function formatHash(hash) {
+  return hash.replace(/(\w{6}).+(\w{6})/, ($1, $2, $3) => {
+    return `${$2}....${$3}`
+  })
+}
+
+useEffect(() => {
+  getAllClaimTx()
+}, [])
+
+State.init({
+  filteredTxs: [],
+  isLoading: false
+})
+
+// if (!state.filteredTxs || state.filteredTxs.length === 0) {
+//   return ''
+// }
+
+return <Transactions>
+  <div className="list">
+    {
+      (state.filteredTxs || []).map(item => {
+        return <div className="claim-line">
+          {formatHash(item.hash)}
+          <div className="btn" style={{ opacity: item.claim_info ? 1 : 0.2 }} onClick={() => {
+            handleClaim(item.claim_info)
+          }}>Claim</div>
+        </div>
+      })
+    }
+  </div>
+  <div className="fresh" onClick={getAllClaimTx}>
+    <RefreshText>
+      {state.isLoading && (
+        <Widget
+          src="bluebiu.near/widget/0vix.LendingLoadingIcon"
+          props={{
+            size: 16,
+          }}
+        />
+      )}
+      Refresh
+    </RefreshText>
+    <ArrowIcon>
+      <Widget
+        src="bluebiu.near/widget/Arbitrum.Swap.ArrowIcon"
+        props={{ size: 12 }}
+      />
+    </ArrowIcon>
+  </div>
+</Transactions>;
