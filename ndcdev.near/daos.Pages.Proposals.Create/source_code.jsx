35051e3437782c7bd17c5f98613dbd6bb8dda308
ndcdev.near <ndcdev.near>
2024-04-11 16:20:51 +0000

    Update daos.Pages.Proposals.Create by ndcdev.near at 2024-04-11T16:20:51.813Z

--- ndcdev.near/daos.Pages.Proposals.Create/source_code.jsx
+++ ndcdev.near/daos.Pages.Proposals.Create/source_code.jsx
@@ -173,8 +173,21 @@ const handleChange = (el, value) => {
   setFormEls(newFormEl);
 };
 
+
+
 if (daos) {
-  daos = daos.map((dao) => {
+  daos = daos.sort((a, b) => {
+    const daoTypeA = a.dao_type.toUpperCase(); 
+    const daoTypeB = b.dao_type.toUpperCase();
+  
+    if (daoTypeA < daoTypeB) {
+      return 1;
+    }
+    if (daoTypeA > daoTypeB) {
+      return -1;
+    }
+    return 0;
+  }).map((dao) => {
     return { name: dao.title, id: dao.id };
   });
 }
