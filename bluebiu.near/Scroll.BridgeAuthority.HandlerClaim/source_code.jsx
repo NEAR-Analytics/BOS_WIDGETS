c2568f804b15f09a29134d3fe54f8c84597aefc4
bluebiu.near <bluebiu.near>
2024-04-12 06:14:37 +0000

    Update Scroll.BridgeAuthority.HandlerClaim by bluebiu.near at 2024-04-12T06:14:37.395Z

--- bluebiu.near/Scroll.BridgeAuthority.HandlerClaim/source_code.jsx
+++ bluebiu.near/Scroll.BridgeAuthority.HandlerClaim/source_code.jsx
@@ -12,14 +12,22 @@ const {
   currentChainId,
   mainnet,
   toast,
+  txs,
 } = props;
 
 const Transactions = styled.div`
-  display: flex;
+  /* display: flex;
   justify-content: space-between;
-  align-items: flex-start;
+  align-items: flex-start; */
   color: var(--label-color);
   gap: 20px;
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding: 5px 0;
+  }
   .list {
     flex: 1;
     .claim-line {
@@ -39,6 +47,35 @@ const Transactions = styled.div`
     display: flex;
     align-items: center;
     gap: 5px;
+  }
+  .chain-token-status {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 14px;
+    padding-top: 20px;
+    .chain-token {
+      display: flex;
+      align-items: center;
+      gap: 10px;
+      img {
+        height: 22px;
+      }
+    }
+  }
+  .time {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+    padding-bottom: 20px;
+    margin-top: 10px;
+    border-bottom: 1px solid #343838;
+    .format-time-link {
+      display: flex;
+      align-items: center;
+      gap: 10px;
+    }
   }
 `
 
@@ -109,6 +146,8 @@ const L1MessageBridgeAbi = [
   },
 ];
 
+console.log('txs: ', txs)
+
 const L1MessageBridgeContract = new ethers.Contract(
   '0x6774Bcbd5ceCeF1336b5300fb5186a12DDD8b367',
   L1MessageBridgeAbi,
@@ -145,7 +184,7 @@ function handleClaim(claimInfo) {
       merkleProof: claimInfo.proof.merkle_proof,
     },
     {
-      gasLimit: 5552000,
+      gasLimit: 162000,
     }).then((tx) => {
       console.log(tx)
       tx.wait()
@@ -189,7 +228,7 @@ function getAllClaimTx() {
   })
 
   return asyncFetch(`https://mainnet-api-bridge-v2.scroll.io/api/l2/unclaimed/withdrawals?address=${account}&page=1&page_size=100`)
-  .then(res => {
+    .then(res => {
       console.log('res2:', res)
       State.update({
         isLoading: false,
@@ -218,6 +257,31 @@ State.init({
 // }
 
 return <Transactions>
+  <div className="header">
+    <div className="title">
+      <span>Transaction History</span>
+      <span>3 Processing</span>
+    </div>
+    <div className="fresh" onClick={getAllClaimTx}>
+      <RefreshText>
+        {state.isLoading && (
+          <Widget
+            src="bluebiu.near/widget/0vix.LendingLoadingIcon"
+            props={{
+              size: 16,
+            }}
+          />
+        )}
+        Refresh
+      </RefreshText>
+      <ArrowIcon>
+        <Widget
+          src="bluebiu.near/widget/Arbitrum.Swap.ArrowIcon"
+          props={{ size: 12 }}
+        />
+      </ArrowIcon>
+    </div>
+  </div>
   <div className="list">
     {
       (state.filteredTxs || []).map(item => {
@@ -229,24 +293,39 @@ return <Transactions>
         </div>
       })
     }
+    {/* {
+      Object.values(txs).map(tx => {
+        return <div key={tx.hash}>
+          <div className="chain-token-status">
+            <div className="chain-token">
+              <img src={tx.fromLogo} />
+              <Widget src="bluebiu.near/widget/Base.Bridge.SwapRightIcon" />
+              <img src={tx.toLogo} />
+              <img src={tx.tokenLogo} />
+              <div>{tx.amount} {tx.symbol}</div>
+              <Widget src="bluebiu.near/widget/Base.Bridge.SwapRightIcon" />
+              <img src={tx.tokenLogo} />
+              <div>{tx.amount} {tx.symbol}</div>
+            </div>
+            <div>Processing</div>
+          </div>
+          <div className="time">
+            <div className="format-time-link">
+              <div className="format-time">
+                  <Widget
+                    src="bluebiu.near/widget/Base.Bridge.FormateTxDate"
+                    props={{
+                      date: tx.time,
+                    }}
+                  />
+              </div>
+              <a target="_blank" className="tx-link" src={tx.link}>Tx</a>
+            </div>
+            <div>~1 hour</div>
+          </div>
+        </div>
+      })
+    } */}
   </div>
-  <div className="fresh" onClick={getAllClaimTx}>
-    <RefreshText>
-      {state.isLoading && (
-        <Widget
-          src="bluebiu.near/widget/0vix.LendingLoadingIcon"
-          props={{
-            size: 16,
-          }}
-        />
-      )}
-      Refresh
-    </RefreshText>
-    <ArrowIcon>
-      <Widget
-        src="bluebiu.near/widget/Arbitrum.Swap.ArrowIcon"
-        props={{ size: 12 }}
-      />
-    </ArrowIcon>
-  </div>
+
 </Transactions>;
