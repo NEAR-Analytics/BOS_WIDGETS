49daeda227f3715e8a9317ab4d1fe93e0955d06e
devgovgigs.petersalomonsen.near <devgovgigs.petersalomonsen.near>
2024-04-12 18:13:57 +0000

    Update app by devgovgigs.petersalomonsen.near at 2024-04-12T18:13:57.330Z

--- devgovgigs.petersalomonsen.near/app/source_code.jsx
+++ devgovgigs.petersalomonsen.near/app/source_code.jsx
@@ -61,6 +61,14 @@ function Page() {
         />
       );
     }
+    case "announcements": {
+      return (
+        <Widget
+          src={"devgovgigs.petersalomonsen.near/widget/devhub.page.announcements"}
+          props={passProps}
+        />
+      );
+    }
 
     // ?page=community
     case "community": {
