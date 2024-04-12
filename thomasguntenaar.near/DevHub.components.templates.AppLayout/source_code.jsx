47a044443c7abb8049bb7f553524578356806d01
thomasguntenaar.near <thomasguntenaar.near>
2024-04-12 17:01:15 +0000

    Update devhub.components.templates.AppLayout by thomasguntenaar.near at 2024-04-12T17:01:15.996Z

--- thomasguntenaar.near/DevHub.components.templates.AppLayout/source_code.jsx
+++ thomasguntenaar.near/DevHub.components.templates.AppLayout/source_code.jsx
@@ -1,110 +1,61 @@
-const { children, page } = props;
-
-const StyledHeader = styled.div`
-  height: 62px;
-  background: #181818;
-  padding: 16px 20px;
+const Theme = styled.div`
+  position: fixed;
+  inset: 73px 0px 0px;
+  width: 100%;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Logo = styled.img`
-  height: 30px;
-`;
-
-const HeaderActions = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1.5rem;
-  color: white;
+  flex-direction: column;
+  padding-top: calc(-1 * var(--body-top-padding));
+  background: #f4f4f4;
+  .container-xl {
+    padding-inline: 0px !important;
+  }
 `;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
   width: 100%;
-  height: 100%;
-  min-height: 100vh;
 `;
 
-function QuestionButton() {
+const ContentContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const AppHeader = ({ page }) => (
+  <Widget
+    src="thomasguntenaar.near/widget/devhub.components.organism.Navbar"
+    props={{
+      page: page,
+      ...props,
+    }}
+  />
+);
+
+const Footer = (props) => {
   return (
-    <div className="btn-group" role="group">
-      <button
-        type="button"
-        className="btn btn-outline-light border-opacity-75 rounded-circle"
-        style={{
-          width: "30px",
-          height: "30px",
-          padding: "6px 0px",
-          borderWidth: "2px",
-          lineHeight: "0px",
-        }}
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        <i className="bi bi-question-lg"></i>
-      </button>
-
-      <ul className="dropdown-menu dropdown-menu-end">
-        <li>
-          <a
-            target="_blank"
-            className="dropdown-item"
-            href="https://github.com/near/devgigsboard-widgets/issues/new?assignees=&labels=bug&template=bug_report.md&title="
-          >
-            Report a bug
-          </a>
-        </li>
-
-        <li>
-          <a
-            target="_blank"
-            className="dropdown-item"
-            href="https://github.com/near/devgigsboard-widgets/issues/new?assignees=&labels=enhancement&template=feature-request.md&title="
-          >
-            Suggest an improvement
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-}
-
-const AppHeader = ({ page }) => {
-  return (
-    <StyledHeader>
-      <Link to="?page=Feed">
-        <Logo
-          src="https://ipfs.near.social/ipfs/bafkreibjsn3gswlcc5mvgkfv7ady2lzkd2htm55l472suarbd34qryh2uy"
-          alt="DevHub"
-        />
-      </Link>
-
-      <HeaderActions>
-        {page !== "communities" && (
-          <Link to="?page=communities">Communities</Link>
-        )}
-
-        <a
-          href="https://www.neardevgov.org/blog/near-developer-dao"
-          target="_blank"
-        >
-          Developer DAO
-        </a>
-        <QuestionButton />
-      </HeaderActions>
-    </StyledHeader>
+    <Widget
+      src="thomasguntenaar.near/widget/devhub.components.organism.NewsLetter"
+      props={{
+        ...props,
+      }}
+    />
   );
 };
 
 function AppLayout({ page, children }) {
   return (
-    <Container>
-      <AppHeader page={page} />
-      {children}
-    </Container>
+    <Theme>
+      <Container
+        className="container-xl"
+        style={{ overflowY: page === "announcements" ? "visible" : "scroll" }}
+      >
+        <AppHeader page={page} />
+        <ContentContainer>{children}</ContentContainer>
+        <Footer page={page} />
+      </Container>
+    </Theme>
   );
 }
 
