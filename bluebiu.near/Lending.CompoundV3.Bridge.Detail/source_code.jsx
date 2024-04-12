df89a6160ec0e4f269c407faa715c389176a5b98
bluebiu.near <bluebiu.near>
2024-04-12 09:55:28 +0000

    Update Lending.CompoundV3.Bridge.Detail by bluebiu.near at 2024-04-12T09:55:28.697Z

--- bluebiu.near/Lending.CompoundV3.Bridge.Detail/source_code.jsx
+++ bluebiu.near/Lending.CompoundV3.Bridge.Detail/source_code.jsx
@@ -889,141 +889,147 @@ return (
           </StyledFlex>
         </StyledWrapper>
       </StyledWrapper>
-      <StyledWrapper
-        style={{
-          borderRadius: 16,
-          border: "1px solid #373A53",
-          backgroundColor: "#2E3142",
-          padding: 20,
-        }}
-      >
-        <StyledFlex
-          style={{ marginBottom: 20, justifyContent: "space-between" }}
-        >
-          <StyledFont style={{ color: "#FFF", fontSize: 18, fontWeight: 600 }}>
-            Position Summary
-          </StyledFont>
-          <StyledFont
-            style={{
-              color: "#FF51AF",
-              fontSize: 14,
-              cursor: state.loading ? "not-allowed" : "pointer",
-              opacity: state.loading ? 0.6 : 1,
-            }}
-            onClick={() => {
-              if (state.loading) return;
-              State.update({
-                actions: [],
-              });
-            }}
-          >
-            Clear all
-          </StyledFont>
-        </StyledFlex>
-        <StyledFlex
+      {!!state.actions.length && (
+        <StyledWrapper
           style={{
-            flexDirection: "column",
-            justifyContent: "flex-start",
+            borderRadius: 16,
+            border: "1px solid #373A53",
+            backgroundColor: "#2E3142",
+            padding: 20,
           }}
         >
-          {state.actions?.map((action, i) => (
-            <StyledFlex
-              key={i + Math.random()}
-              style={{
-                width: "100%",
-                justifyContent: "space-between",
-                height: 60,
-                paddingRight: 20,
-                paddingLeft: 15,
-                borderRadius: 8,
-                backgroundColor: "#252734",
-              }}
+          <StyledFlex
+            style={{ marginBottom: 20, justifyContent: "space-between" }}
+          >
+            <StyledFont
+              style={{ color: "#FFF", fontSize: 18, fontWeight: 600 }}
             >
-              <StyledFlex style={{ gap: 14 }}>
-                <StyledSvg></StyledSvg>
-                <StyledFont style={{ color: "#FFF" }}>
-                  {action.type} {action.asset.symbol}
-                </StyledFont>
-              </StyledFlex>
-              <StyledFlex style={{ gap: 20 }}>
-                <StyledFlex
-                  style={{
-                    flexDirection: "column",
-                    justifyContent: "flex-start",
-                    gap: 3,
-                  }}
-                >
-                  <StyledFont style={{ color: "#FFF", fontWeight: 500 }}>
-                    <Widget
-                      src="bluebiu.near/widget/Utils.FormatAmount"
-                      props={{
-                        amount: action.amount,
-                      }}
-                    />
-                  </StyledFont>
-                  <StyledFont style={{ color: "#979ABE", fontSize: 12 }}>
-                    <Widget
-                      src="bluebiu.near/widget/Utils.FormatAmount"
-                      props={{
-                        amount: Big(action.amount)
-                          .mul(action.asset.price)
-                          .toString(),
-                        prev: "$",
-                      }}
-                    />
-                  </StyledFont>
-                </StyledFlex>
-                <StyledSvg
-                  style={{ cursor: state.loading ? "not-allowed" : "pointer" }}
-                  onClick={() => {
-                    if (state.loading) return;
-                    const _actions = state.actions;
-                    _actions.splice(i, 1);
-                    State.update({
-                      actions: _actions,
-                    });
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="none"
-                  >
-                    <path
-                      d="M7.73284 6.00004L11.7359 1.99701C12.0368 1.696 12.0882 1.2593 11.8507 1.0219L10.9779 0.14909C10.7404 -0.0884124 10.3043 -0.0363122 10.0028 0.264491L6.00013 4.26743L1.99719 0.264591C1.69619 -0.036712 1.25948 -0.0884125 1.02198 0.14939L0.149174 1.0223C-0.0882277 1.2594 -0.0368271 1.6961 0.264576 1.99711L4.26761 6.00004L0.264576 10.0033C-0.0363271 10.3041 -0.0884277 10.7405 0.149174 10.978L1.02198 11.8509C1.25948 12.0884 1.69619 12.0369 1.99719 11.736L6.00033 7.73276L10.0029 11.7354C10.3044 12.037 10.7405 12.0884 10.978 11.8509L11.8508 10.978C12.0882 10.7405 12.0368 10.3041 11.736 10.0029L7.73284 6.00004Z"
-                      fill="#979ABE"
-                    />
-                  </svg>
-                </StyledSvg>
-              </StyledFlex>
-            </StyledFlex>
-          ))}
-          <StyledFlex style={{ width: "100%" }}>
-            <StyledButton
-              disabled={!state.actions.length || state.loading}
+              {state.actions.length} Pending Actions
+            </StyledFont>
+            <StyledFont
+              style={{
+                color: "#FF51AF",
+                fontSize: 14,
+                cursor: state.loading ? "not-allowed" : "pointer",
+                opacity: state.loading ? 0.6 : 1,
+              }}
               onClick={() => {
-                if (!state.actions.length || state.loading) return;
+                if (state.loading) return;
                 State.update({
-                  loading: true,
+                  actions: [],
                 });
               }}
             >
-              {state.loading ? (
-                <Widget
-                  src="bluebiu.near/widget/0vix.LendingLoadingIcon"
-                  props={{
-                    size: 16,
-                  }}
-                />
-              ) : (
-                "Submit Transactions"
-              )}
-            </StyledButton>
+              Clear all
+            </StyledFont>
           </StyledFlex>
-        </StyledFlex>
-      </StyledWrapper>
+          <StyledFlex
+            style={{
+              flexDirection: "column",
+              justifyContent: "flex-start",
+            }}
+          >
+            {state.actions?.map((action, i) => (
+              <StyledFlex
+                key={i + Math.random()}
+                style={{
+                  width: "100%",
+                  justifyContent: "space-between",
+                  height: 60,
+                  paddingRight: 20,
+                  paddingLeft: 15,
+                  borderRadius: 8,
+                  backgroundColor: "#252734",
+                }}
+              >
+                <StyledFlex style={{ gap: 14 }}>
+                  <StyledSvg></StyledSvg>
+                  <StyledFont style={{ color: "#FFF" }}>
+                    {action.type} {action.asset.symbol}
+                  </StyledFont>
+                </StyledFlex>
+                <StyledFlex style={{ gap: 20 }}>
+                  <StyledFlex
+                    style={{
+                      flexDirection: "column",
+                      justifyContent: "flex-start",
+                      gap: 3,
+                    }}
+                  >
+                    <StyledFont style={{ color: "#FFF", fontWeight: 500 }}>
+                      <Widget
+                        src="bluebiu.near/widget/Utils.FormatAmount"
+                        props={{
+                          amount: action.amount,
+                        }}
+                      />
+                    </StyledFont>
+                    <StyledFont style={{ color: "#979ABE", fontSize: 12 }}>
+                      <Widget
+                        src="bluebiu.near/widget/Utils.FormatAmount"
+                        props={{
+                          amount: Big(action.amount)
+                            .mul(action.asset.price)
+                            .toString(),
+                          prev: "$",
+                        }}
+                      />
+                    </StyledFont>
+                  </StyledFlex>
+                  <StyledSvg
+                    style={{
+                      cursor: state.loading ? "not-allowed" : "pointer",
+                    }}
+                    onClick={() => {
+                      if (state.loading) return;
+                      const _actions = state.actions;
+                      _actions.splice(i, 1);
+                      State.update({
+                        actions: _actions,
+                      });
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="12"
+                      height="12"
+                      viewBox="0 0 12 12"
+                      fill="none"
+                    >
+                      <path
+                        d="M7.73284 6.00004L11.7359 1.99701C12.0368 1.696 12.0882 1.2593 11.8507 1.0219L10.9779 0.14909C10.7404 -0.0884124 10.3043 -0.0363122 10.0028 0.264491L6.00013 4.26743L1.99719 0.264591C1.69619 -0.036712 1.25948 -0.0884125 1.02198 0.14939L0.149174 1.0223C-0.0882277 1.2594 -0.0368271 1.6961 0.264576 1.99711L4.26761 6.00004L0.264576 10.0033C-0.0363271 10.3041 -0.0884277 10.7405 0.149174 10.978L1.02198 11.8509C1.25948 12.0884 1.69619 12.0369 1.99719 11.736L6.00033 7.73276L10.0029 11.7354C10.3044 12.037 10.7405 12.0884 10.978 11.8509L11.8508 10.978C12.0882 10.7405 12.0368 10.3041 11.736 10.0029L7.73284 6.00004Z"
+                        fill="#979ABE"
+                      />
+                    </svg>
+                  </StyledSvg>
+                </StyledFlex>
+              </StyledFlex>
+            ))}
+            <StyledFlex style={{ width: "100%" }}>
+              <StyledButton
+                disabled={!state.actions.length || state.loading}
+                onClick={() => {
+                  if (!state.actions.length || state.loading) return;
+                  State.update({
+                    loading: true,
+                  });
+                }}
+              >
+                {state.loading ? (
+                  <Widget
+                    src="bluebiu.near/widget/0vix.LendingLoadingIcon"
+                    props={{
+                      size: 16,
+                    }}
+                  />
+                ) : (
+                  "Submit Transactions"
+                )}
+              </StyledButton>
+            </StyledFlex>
+          </StyledFlex>
+        </StyledWrapper>
+      )}
     </StyledWrapper>
     {state.showDialog && (
       <Widget
@@ -1089,7 +1095,6 @@ return (
                   const { status, transactionHash } = res;
                   const _actions = [];
                   state.actions.forEach((action, i) => {
-                    if (i > 0) action_title += ",";
                     _actions.push({
                       amount: action.amount,
                       type: action.type,
@@ -1110,7 +1115,7 @@ return (
                   toast?.success({
                     title: `Request successed!`,
                     tx: transactionHash,
-                    chainId: state.asset.chainId,
+                    chainId: curChain.chainId,
                   });
                   State.update({
                     loading: false,
