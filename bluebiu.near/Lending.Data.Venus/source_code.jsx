8d50ddca906085094c5b3292133ddc934518860b
bluebiu.near <bluebiu.near>
2024-04-12 08:34:08 +0000

    Update Lending.Data.Venus by bluebiu.near at 2024-04-12T08:34:08.104Z

--- bluebiu.near/Lending.Data.Venus/source_code.jsx
+++ bluebiu.near/Lending.Data.Venus/source_code.jsx
@@ -474,7 +474,7 @@ useEffect(() => {
               );
               break;
             case 1:
-              _userMerberShip[oTokens[index].address] = res[i][0];
+              _userMerberShip[oTokens[index].address] = res[i][0] || false;
               break;
             case 2:
               _rewardsSupplyRate[oTokens[index].address] =
