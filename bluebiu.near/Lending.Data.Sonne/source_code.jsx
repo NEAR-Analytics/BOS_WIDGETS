a2d378004bdfdb094df94d83ab79ff8bbb694e6d
bluebiu.near <bluebiu.near>
2024-04-12 08:34:08 +0000

    Update Lending.Data.Sonne by bluebiu.near at 2024-04-12T08:34:08.104Z

--- bluebiu.near/Lending.Data.Sonne/source_code.jsx
+++ bluebiu.near/Lending.Data.Sonne/source_code.jsx
@@ -350,7 +350,7 @@ useEffect(() => {
               );
               break;
             case 1:
-              _userMerberShip[oTokens[index].address] = res[i][0];
+              _userMerberShip[oTokens[index].address] = res[i][0] || false;
               break;
             default:
           }
