798125eaee8333d3c7fbbadfbb02fff2f0da62de
kurodenjiro.near <kurodenjiro.near>
2024-04-13 21:03:49 +0000

    Update mouse-hover by kurodenjiro.near at 2024-04-13T21:03:49.802Z

--- kurodenjiro.near/mouse-hover/source_code.jsx
+++ kurodenjiro.near/mouse-hover/source_code.jsx
@@ -1,13 +1,14 @@
 const c = `
 const [warning, setWarning] = useState(true);
-return <h1
-      onMouseEnter={() => setWarning(false)}
+return <a
+      onMouseEnter={(e) => {console.log(e);setWarning(false)}}
       onMouseLeave={() => setWarning(true)}
       style={{ color: warning ? "goldenrod" : "green" }}
     >
-      Please hover over the container{" "}
-    </h1>`;
+      Please hover over <br/> the container{" "}
+    </a>`;
 
-console.log(a);
-
-return <Widget code={c} />;
+console.log(c);
+const d = <Widget code={c} />;
+console.log(d);
+return d;
