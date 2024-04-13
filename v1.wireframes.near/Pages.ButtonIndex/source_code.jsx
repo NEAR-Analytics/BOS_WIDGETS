c62fd039a0048ae1f695c5696924925afe403521
v1.wireframes.near <v1.wireframes.near>
2024-04-13 12:54:47 +0000

    Update Pages.ButtonIndex by v1.wireframes.near at 2024-04-13T12:54:47.615Z

--- v1.wireframes.near/Pages.ButtonIndex/source_code.jsx
+++ v1.wireframes.near/Pages.ButtonIndex/source_code.jsx
@@ -17,70 +17,89 @@
 
 /* -------------------------------------------------------------------------- */
 
-                                                         
 
-
-const Container = ({ children }) => {
-    return (
-        <Widget
-        src={`v1.wireframes.near/widget/Components.Layout.Container`}
-        props={children}
-      />
-    );
-  };
-
- 
-
-  const data ={children : (<>
-    <div className="p-4 d-flex justify-content-center">
-        <div className="row d-flex justify-content-center" style={{ paddingTop:"inherit" }}>
-           
-        <h1 className="pt-4">Simple Buttons</h1>
-    <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.SimpleBtnDetail`}/>  
-  <h1 className="pt-4">Glow Buttons</h1>  
-    <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.GlowBtnDetail`}/>
-  <h1 className="pt-4">Outline Buttons</h1>  
-    <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.OutlineBtnDetail`}/> 
-  <h1 className="pt-4">Fade Buttons</h1>  
-    <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.FadeBtnDetail`}/>
-  <h1 className="pt-4">Gradient Buttons</h1>  
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.GradientBtnDetail`}/>
-  <h1 className="pt-4">Buttons with icon and label</h1>
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.IconAndLabelBtnDetail`}/> 
-   
-  <h1 className="pt-4">Icon Buttons</h1>
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.IconBtnDetail`}/> 
-   <h1 className="pt-4">Group Buttons</h1>
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.GroupBtnDetail`}/> 
-   <h1 className="pt-4">Link Buttons</h1>
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.LinkBtnDetail`}/> 
-   <h1 className="pt-4">Badge Buttons</h1>
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.BadgeBtnDetail`}/> 
-   <h1 className="pt-4">Badge Icons</h1>
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.BadgeIconBtnDetail`}/>
-   <h1 className="pt-4">Floating Buttons</h1>
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.FloatingBtnDetail`}/> 
-   <h1 className="pt-4">Floating Buttons with Size</h1>
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.FloatingBtnWithSizeDetail`}/> 
-   <h1 className="pt-4">Loading Buttons</h1>
-   <Widget src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.LoadingBtnDetail`}/> 
-    
-
-    </div>    
-    </div>
-
-    </>)} ;
-
-return(
+const data = {
+  children: (
     <>
-     <Widget src={`v1.wireframes.near/widget/Pages.Navigation.Navbar`}/>
-    <Widget
+      <div className="p-4 d-flex justify-content-center">
+        <div
+          className="row d-flex justify-content-center"
+          style={{ paddingTop: "8%" }}
+        >
+          <h1 className="pt-4">Simple Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.SimpleBtnDetail`}
+          />
+          <h1 className="pt-5">Glow Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.GlowBtnDetail`}
+          />
+          <h1 className="pt-5">Outline Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.OutlineBtnDetail`}
+          />
+          <h1 className="pt-5">Fade Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.FadeBtnDetail`}
+          />
+          <h1 className="pt-5">Gradient Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.GradientBtnDetail`}
+          />
+          <h1 className="pt-5">Buttons with icon and label</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.IconAndLabelBtnDetail`}
+          />
+
+          <h1 className="pt-5">Icon Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.IconBtnDetail`}
+          />
+          <h1 className="pt-5">Group Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.GroupBtnDetail`}
+          />
+          <h1 className="pt-5">Link Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.LinkBtnDetail`}
+          />
+          <h1 className="pt-5">Badge Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.BadgeBtnDetail`}
+          />
+          <h1 className="pt-5">Badge Icons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.BadgeIconBtnDetail`}
+          />
+          <h1 className="pt-5">Floating Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.FloatingBtnDetail`}
+          />
+          <h1 className="pt-5">Floating Buttons with Size</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.FloatingBtnWithSizeDetail`}
+          />
+          <h1 className="pt-5">Loading Buttons</h1>
+          <Widget
+            src={`v1.wireframes.near/widget/Components.Button.ButtonDetail.LoadingBtnDetail`}
+          />
+        </div>
+      </div>
+    </>
+  ),
+};
+
+return (
+  <>
+    <Widget src={`v1.wireframes.near/widget/Pages.Navigation.Navbar`} />
+    <div className="row">
+      <Widget
         src={`v1.wireframes.near/widget/Components.Layout.Container`}
         props={data}
       />
-<Widget
-        src={`v1.wireframes.near/widget/Components.Navigations.Footer`}
-      />
-
-    </>
+    </div>
+    <div className="row">
+      <Widget src={`v1.wireframes.near/widget/Components.Navigations.Footer`} />
+    </div>
+  </>
 );
