02650bdd97335ade7fbeebd2e78d38d17ac00159
v1.wireframes.near <v1.wireframes.near>
2024-04-13 12:54:47 +0000

    Update Components.Button.OutlineButton by v1.wireframes.near at 2024-04-13T12:54:47.615Z

--- v1.wireframes.near/Components.Button.OutlineButton/source_code.jsx
+++ v1.wireframes.near/Components.Button.OutlineButton/source_code.jsx
@@ -16,6 +16,32 @@
                                                                                  */
 
 /* -------------------------------------------------------------------------- */
+const label = props.buttonTitle ?? "Title";
+const type = props.style.type ?? "button";
+const isDisable = props.isDisable ?? false;
+
+const width = props.style.width;
+const border = props.style.border;
+const padding = props.style.padding;
+const fontSize = props.style.fontSize;
+const fontColor = props.style.fontColor;
+const fontFamily = props.style.fontFamily;
+const fontWeight = props.style.fontWeight;
+const borderRadius = props.style.borderRadius;
+const lineHeight = props.style.lineHeight;
+const margin = props.style.margin;
+const backgroundColor = props.style.backgroundColor;
+const hoverColor = props.style.hoverColor;
+const hoverFontColor = props.style.hoverFontColor;
+const activeColor = props.style.activeColor;
+const activeFontColor = props.style.activeFontColor;
+const inlineStyle = props.inlineStyle;
+const onClick = props.onClick ?? (() => {});
+
+const buttonHref = props.buttonHref ?? "";
+const buttonAnchor = props.buttonAnchor ?? "a";
+const isLoading = props.isLoading ?? false;
+
 const OutlineButton = styled.button`
   background-color: ${(props) => props.backgroundColor || "transparent"};
   border: ${(props) => props.border || "1px solid #000"};
@@ -53,37 +79,11 @@ const OutlineButton = styled.button`
   }
 `;
 
-const label = props.buttonTitle ?? "Title";
-const type = props.style.type ?? "button";
-const isDisable = props.isDisable ?? false;
-
-const width = props.style.width;
-const border = props.style.border;
-const padding = props.style.padding;
-const fontSize = props.style.fontSize;
-const fontColor = props.style.fontColor;
-const fontFamily = props.style.fontFamily;
-const fontWeight = props.style.fontWeight;
-const borderRadius = props.style.borderRadius;
-const lineHeight = props.style.lineHeight;
-const margin = props.style.margin;
-const backgroundColor = props.style.backgroundColor;
-const hoverColor = props.style.hoverColor;
-const hoverFontColor = props.style.hoverFontColor;
-const activeColor = props.style.activeColor;
-const activeFontColor = props.style.activeFontColor;
-const inlineStyle = props.inlineStyle;
-const onClick = props.onClick ?? (() => {});
-
-const buttonHref = props.buttonHref ?? "";
-const buttonAnchor = props.buttonAnchor ?? "a";
-const isLoading = props.isLoading ?? false;
-
 const loaderStyle = {
   marginRight: props.style.loaderMarginRight ?? "0px",
   height: props.style.loaderHeight ?? "19px",
   width: props.style.loaderWidth ?? "19px",
-}
+};
 return (
   <>
     {buttonHref == "" ? (
