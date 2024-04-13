0b55aa1e3871e1b56db27b75d2eedfe362d5a8b7
v1.wireframes.near <v1.wireframes.near>
2024-04-13 12:54:47 +0000

    Update Components.Icon.IconDetail.BootstrapIconsDetail by v1.wireframes.near at 2024-04-13T12:54:47.615Z

--- v1.wireframes.near/Components.Icon.IconDetail.BootstrapIconsDetail/source_code.jsx
+++ v1.wireframes.near/Components.Icon.IconDetail.BootstrapIconsDetail/source_code.jsx
@@ -20,25 +20,24 @@
 const iconProps = {
   iconName: "x-circle",
   iconColor: "#000",
-  iconSize: "2em"
-  
+  iconSize: "2em",
 };
-const sendProps = {
+const sendIconProps = {
   iconName: "send",
   iconColor: "#000",
-  iconSize: "2em"
-  
+  iconSize: "2em",
 };
-const personProps = {
+const personIconProps = {
   iconName: "person-circle",
   iconColor: "#000",
-  iconSize: "2em"
-  
+  iconSize: "2em",
 };
 
 const iconUrl = (
   <>
-  <a href="https://icons.getbootstrap.com/" target="_blank">Bootstrap Icons</a>
+    <a href="https://icons.getbootstrap.com/" target="_blank">
+      Bootstrap Icons
+    </a>
   </>
 );
 
@@ -61,7 +60,7 @@ const propsExplanation = [
       "The icon prop can accept an icon name or identifier, allowing the component to dynamically select and render the appropriate icon based on the provided name. e.g.",
     code: `const iconProps = { 
       iconName: "x-circle",
-    }`,
+    };`,
   },
   {
     title: "iconColor",
@@ -69,7 +68,7 @@ const propsExplanation = [
       "The iconColor prop is used to dynamically set the fill color of the representing icon. e.g.",
     code: `const iconProps = { 
       iconColor: "#000",
-    }`,
+    };`,
   },
   {
     title: "iconSize",
@@ -77,24 +76,40 @@ const propsExplanation = [
       "The iconSize prop is passed to the icon component, to set the size of the icon. e.g.",
     code: `const iconProps = { 
       iconSize: "2em",
-    }`,
+    };`,
+  },
+  {
+    title: "iconMargin",
+    content:
+      "The iconMargin props is used to create space around the icon element, outside of any defined borders. It controls the amount of space between an element and adjacent elements. You can set the margin on all four sides of an element or individually for each side. e.g.",
+    code: `const iconProps = { 
+      iconMargin: "0px 0px 0px 0px",
+    };`,
+  },
+  {
+    title: "iconPadding",
+    content:
+      "The iconPadding props is used to create space around the icon inside an element. It defines the distance between the icon of an element and its border. e.g.",
+    code: `const iconProps = { 
+      iconPadding: "0px 0px 0px 0px",
+    };`,
   },
 ];
 
-const propsList = [iconProps, sendProps,personProps ]
+const propsList = [iconProps, sendIconProps, personIconProps];
 
 const props = {
   copyBtn: detail,
   component: (
     <div className="row">
-      {propsList.map((data,index) => (
-      <div className="col ms-auto">
-        <Widget
-          src="v1.wireframes.near/widget/Components.Icon.BootstrapIcons"
-          props={data}
-        />
-      </div>
-       ))}
+      {propsList.map((data, index) => (
+        <div className="col ms-auto">
+          <Widget
+            src="v1.wireframes.near/widget/Components.Icon.BootstrapIcons"
+            props={data}
+          />
+        </div>
+      ))}
     </div>
   ),
   text: detail,
