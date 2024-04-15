134c685b73ee4c0855e669019ccb7b3540595945
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update Common.AccountAutocomplete by devs.near at 2024-04-15T02:36:24.635Z

--- devs.near/Common.AccountAutocomplete/source_code.jsx
+++ devs.near/Common.AccountAutocomplete/source_code.jsx
@@ -1,18 +1,14 @@
 if (!context.accountId || !props.term) return <></>;
-
 let results = [];
 const profilesData = Social.get("*/profile/name", "final") || {};
 const followingData = Social.get(
   `${context.accountId}/graph/follow/**`,
   "final"
 );
-
 if (!profilesData || !followingData) return <></>;
-
 const profiles = Object.entries(profilesData);
 const term = (props.term || "").replace(/\W/g, "").toLowerCase();
 const limit = 5;
-
 for (let i = 0; i < profiles.length; i++) {
   let score = 0;
   const accountId = profiles[i][0];
@@ -22,10 +18,8 @@ for (let i = 0; i < profiles.length; i++) {
     .toLowerCase();
   const accountIdSearchIndex = accountIdSearch.indexOf(term);
   const nameSearchIndex = nameSearch.indexOf(term);
-
   if (accountIdSearchIndex > -1 || nameSearchIndex > -1) {
     score += 10;
-
     if (accountIdSearchIndex === 0) {
       score += 10;
     }
@@ -35,24 +29,19 @@ for (let i = 0; i < profiles.length; i++) {
     if (followingData[accountId] === "") {
       score += 30;
     }
-
     results.push({
       accountId,
       score,
     });
   }
 }
-
 results.sort((a, b) => b.score - a.score);
 results = results.slice(0, limit);
-
 function onResultClick(id) {
   props.onSelect && props.onSelect(id);
 }
-
 const Wrapper = styled.div`
   position: relative;
-
   &::before {
     content: "";
     display: block;
@@ -64,7 +53,6 @@ const Wrapper = styled.div`
     z-index: 10;
   }
 `;
-
 const Scroller = styled.div`
   position: relative;
   display: flex;
@@ -78,20 +66,17 @@ const Scroller = styled.div`
   &::-webkit-scrollbar {
     display: none;
   }
-
   > * {
     max-width: 200px;
     text-align: left;
     flex-grow: 0;
     flex-shrink: 0;
-
     button {
       border: 1px solid #eceef0;
       background: #fff !important;
       border-radius: 6px;
       padding: 3px 6px;
       transition: all 200ms;
-
       &:focus,
       &:hover {
         border-color: #687076;
@@ -99,7 +84,6 @@ const Scroller = styled.div`
     }
   }
 `;
-
 const CloseButton = styled.button`
   background: none;
   border: none;
@@ -107,21 +91,17 @@ const CloseButton = styled.button`
   padding: 12px;
   color: #687076;
   transition: all 200ms;
-
   &:hover {
     color: #000;
   }
 `;
-
 if (results.length === 0) return <></>;
-
 return (
   <Wrapper>
     <Scroller>
       <CloseButton tabIndex={-1} type="button" onClick={props.onClose}>
         <i className="bi bi-x-circle" />
       </CloseButton>
-
       {results.map((result) => {
         return (
           <button
