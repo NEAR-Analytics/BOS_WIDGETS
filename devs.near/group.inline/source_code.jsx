b1517a2b9678a94e358632f9b6b062f6d5915b85
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update group.inline by devs.near at 2024-04-15T02:36:24.635Z

--- devs.near/group.inline/source_code.jsx
+++ devs.near/group.inline/source_code.jsx
@@ -1,24 +1,18 @@
 const accountId = props.accountId ?? context.accountId;
 const groupId = props.groupId ?? "526fb256e74eelmf0nw3n5909bc189c13d";
-
 const group =
   props.group ?? Social.get(`${accountId}/thing/${groupId}/metadata/**`);
-
 const name = group.name;
 const description = group.description;
 const tags = Object.keys(group.tags ?? {});
-
 const Tag = styled.a`
   color: black;
   text-decoration: none;
-
   &:hover {
     color: blue;
     text-decoration: none;
   }
 `;
-
-
 return (
   <div className="d-flex flex-row">
     <Widget
