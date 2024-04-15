1ff5c12d9295b7ed79597e25ff06cf87589ad982
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update profile.page by devs.near at 2024-04-15T02:36:24.635Z

--- devs.near/profile.page/source_code.jsx
+++ devs.near/profile.page/source_code.jsx
@@ -2,13 +2,10 @@ const accountId = props.accountId ?? context.accountId;
 if (!accountId) {
   return "No account ID";
 }
-
 const profile = props.profile ?? Social.getr(`${accountId}/profile`);
-
 if (profile === null) {
   return "Loading";
 }
-
 return (
   <div className="py-1 px-1">
     <div className="mx-auto">
@@ -21,7 +18,6 @@ return (
           showEditButton: !props.profile,
         }}
       />
-
       <div className="mt-3 m-2">
         <Widget src="devs.near/widget/dev.feed" props={{ accountId }} />
       </div>
