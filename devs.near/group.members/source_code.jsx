47684752fd502a2184c4f7fd79af88c437c69c9c
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update group.members by devs.near at 2024-04-15T02:36:24.635Z

--- devs.near/group.members/source_code.jsx
+++ devs.near/group.members/source_code.jsx
@@ -1,17 +1,13 @@
 const groupId = props.groupId ?? "6fd36ddf4884flm20pbe91e7b208b88d16";
 const creatorId = props.creatorId ?? "*";
-
 let members = Social.keys(`${creatorId}/graph/${groupId}/*`, "final", {
   return_type: "BlockHeight",
   values_only: true,
 });
-
 if (!members) {
   return "Loading...";
 }
-
 const groupKey = Object.keys(members)[0];
-
 members = Object.entries(members[groupKey]?.graph[groupId] || {});
 members.sort((a, b) => b[1] - a[1]);
 return (
