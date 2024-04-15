42bed41b59fb9d4cd74d819cd55e7078721d4b9e
video.every.near <video.every.near>
2024-04-15 04:27:29 +0000

    Update browse by video.every.near at 2024-04-15T04:27:29.254Z

--- video.every.near/browse/source_code.jsx
+++ video.every.near/browse/source_code.jsx
@@ -1,12 +1,10 @@
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-
   @media (hover: none) {
     grid-template-columns: repeat(1, 1fr);
   }
 `;
-
 return (
   <>
     <Widget
