dcf460ba6f1b889b395a34be6f0180c280bc90e2
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update widget.lib.stringUtils by devs.near at 2024-04-15T02:36:24.635Z

---
+++ devs.near/widget.lib.stringUtils/source_code.jsx
@@ -0,0 +1,18 @@
+const normalize = (text, delimiter) => {
+  // If no delimiter is provided, default to an underscore
+  delimiter = delimiter || "_";
+  return (
+    text
+      // Convert to lowercase
+      .toLowerCase()
+      // Replace spaces with dashes
+      .replace(/\s+/g, delimiter)
+      // Replace any non-alphanumeric characters (excluding dashes) with nothing
+      .replace(`/[^a-z0-9${delimiter}]/g`, "")
+      // Replace multiple consecutive dashes with a single dash
+      .replace(`/${delimiter}+/g`, "-")
+      // Trim dashes from the start and end of the string
+      .replace(`/^${delimiter}+|${delimiter}+$/g`, "")
+  );
+};
+return { normalize };
