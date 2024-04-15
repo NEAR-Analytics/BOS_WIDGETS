87cb2a0b1f69e411fca5f4a88b776374fd4ff954
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update group.image by devs.near at 2024-04-15T02:36:24.635Z

--- devs.near/group.image/source_code.jsx
+++ devs.near/group.image/source_code.jsx
@@ -1,14 +1,11 @@
 const accountId = props.accountId ?? context.accountId;
 const groupId = props.groupId ?? "526fb256e74eelmf0nw3n5909bc189c13d";
-
 const className = props.className ?? "group-image d-inline-block";
 const style = props.style ?? { width: "3em", height: "3em" };
 const imageStyle = props.imageStyle ?? { objectFit: "cover" };
 const imageClassName = props.imageClassName ?? "rounded w-100 h-100";
 const thumbnail = props.thumbnail ?? "thumbnail";
-
 const group = props.group;
-
 const name = group.name || "No-name group";
 const image = group.image;
 const title = props.title ?? `${name} @${accountId}`;
@@ -16,7 +13,6 @@ const tooltip =
   props.tooltip && (props.tooltip === true ? title : props.tooltip);
 const fallbackUrl =
   "https://ipfs.near.social/ipfs/bafkreibmiy4ozblcgv3fm3gc6q62s55em33vconbavfd2ekkuliznaq3zm";
-
 const inner = (
   <div className={className} style={style} key={JSON.stringify(image)}>
     <Widget
@@ -32,7 +28,6 @@ const inner = (
     />
   </div>
 );
-
 return props.tooltip ? (
   <>
     <Widget
