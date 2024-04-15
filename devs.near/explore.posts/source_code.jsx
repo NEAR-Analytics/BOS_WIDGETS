c26c1b0482c4545423ed1ba6e1ab52132bfdbd0b
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update explore.posts by devs.near at 2024-04-15T02:36:24.635Z

--- devs.near/explore.posts/source_code.jsx
+++ devs.near/explore.posts/source_code.jsx
@@ -1,21 +1,15 @@
 State.init({
   selectedTab: Storage.privateGet("selectedTab") || "all",
 });
-
 const domains = ["dev", "gov", "edu", "art", "fun"];
-
 const hashtags = ["near", "bos"];
-
 const previousSelectedTab = Storage.privateGet("selectedTab");
-
 if (previousSelectedTab && previousSelectedTab !== state.selectedTab) {
   State.update({
     selectedTab: previousSelectedTab,
   });
 }
-
 let accounts = undefined;
-
 if (state.selectedTab === "following" && context.accountId) {
   const graph = Social.keys(`${context.accountId}/graph/follow/*`, "final");
   if (graph !== null) {
@@ -27,24 +21,20 @@ if (state.selectedTab === "following" && context.accountId) {
 } else {
   accounts = undefined;
 }
-
 function selectTab(selectedTab) {
   Storage.privateSet("selectedTab", selectedTab);
   State.update({ selectedTab });
 }
-
 const H2 = styled.h2`
   font-size: 19px;
   line-height: 22px;
   color: #11181c;
   margin: 0 0 24px;
   padding: 0 24px;
-
   @media (max-width: 1200px) {
     display: none;
   }
 `;
-
 const Content = styled.div`
   @media (max-width: 1200px) {
     > div:first-child {
@@ -52,36 +42,29 @@ const Content = styled.div`
     }
   }
 `;
-
 const ComposeWrapper = styled.div`
   border-top: 1px solid #eceef0;
 `;
-
 const FilterWrapper = styled.div`
   border-top: 1px solid #eceef0;
   padding: 24px 24px 0;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-
   @media (max-width: 1200px) {
     padding: 12px;
   }
 `;
-
 const PillSelect = styled.div`
   display: inline-flex;
   align-items: center;
-
   @media (max-width: 600px) {
     width: 100%;
-
     button {
       flex: 1;
     }
   }
 `;
-
 const PillSelectButton = styled.button`
   display: block;
   position: relative;
@@ -95,19 +78,16 @@ const PillSelectButton = styled.button`
   background: ${(p) => (p.selected ? "#006ADC !important" : "#FBFCFD")};
   font-weight: 600;
   transition: all 200ms;
-
   &:hover {
     background: #ecedee;
     text-decoration: none;
   }
-
   &:focus {
     outline: none;
     border-color: #006adc !important;
     box-shadow: 0 0 0 1px #006adc;
     z-index: 5;
   }
-
   &:first-child {
     border-radius: 6px 0 0 6px;
   }
@@ -116,19 +96,16 @@ const PillSelectButton = styled.button`
     border-right: 1px solid #e6e8eb;
   }
 `;
-
 const FeedWrapper = styled.div`
   .post {
     padding-left: 24px;
     padding-right: 24px;
-
     @media (max-width: 1200px) {
       padding-left: 12px;
       padding-right: 12px;
     }
   }
 `;
-
 return (
   <>
     <Content>
@@ -139,7 +116,6 @@ return (
             <Widget src="hack.near/widget/create.posts" props={{ domains }} />
           </ComposeWrapper>
           <h2 className="mb-3">explore</h2>
-
           <FilterWrapper>
             <PillSelect>
               <PillSelectButton
@@ -149,7 +125,6 @@ return (
               >
                 all
               </PillSelectButton>
-
               <PillSelectButton
                 type="button"
                 onClick={() => selectTab("following")}
@@ -179,7 +154,6 @@ return (
           </FilterWrapper>
         </>
       )}
-
       <FeedWrapper>
         <Widget
           src="hack.near/widget/view.posts"
