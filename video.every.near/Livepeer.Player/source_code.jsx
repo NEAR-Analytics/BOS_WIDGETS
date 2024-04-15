a05d361f39e78f1d7b89871eba8f96f2b886368f
video.every.near <video.every.near>
2024-04-15 04:27:29 +0000

    Update Livepeer.Player by video.every.near at 2024-04-15T04:27:29.254Z

--- video.every.near/Livepeer.Player/source_code.jsx
+++ video.every.near/Livepeer.Player/source_code.jsx
@@ -1,18 +1,3 @@
-/**
- * Livepeer Player
- *
- * Widget for playing a video from Livepeer
- * (https://docs.livepeer.org/reference/livepeer-js/player/usePlayer)
- * 
- * Props:
- * @prop {String} title - the title of the video
- * @prop {String} playbackId - the playbackId of the video
- * @prop {Image} PosterImage - The image object for the poster image.
- * @prop {Boolean} showPipButton - whether to show the picture-in-picture button
- * @prop {String} objectFit - the object fit for the video
- * @prop {Boolean} priority - whether to prioritize the video
- */
-
 return (
   <LivepeerPlayer
     title={props.title}
