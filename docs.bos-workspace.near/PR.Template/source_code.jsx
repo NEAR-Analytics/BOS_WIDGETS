1cb8897ec96bf81e1b9e2461be0b5ad36c55fc5d
docs.bos-workspace.near <docs.bos-workspace.near>
2024-04-15 03:02:22 +0000

    Update PR.Template by docs.bos-workspace.near at 2024-04-15T03:02:22.451Z

---
+++ docs.bos-workspace.near/PR.Template/source_code.jsx
@@ -0,0 +1,23 @@
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+`;
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+return ({ layout, theme, blocks, children }) => {
+  const { Layout } = VM.require(layout?.src ?? "devs.near/widget/Layout") || {
+    Layout: () => <></>,
+  };
+  return (
+    <Container style={theme} id="template-container">
+      <Layout
+        {...(layout?.initialProps ?? { variant: "standard" })}
+        blocks={blocks}
+      >
+        {children}
+      </Layout>
+    </Container>
+  );
+};
