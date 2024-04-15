c343475e6f92a288f861c14f485bc5dad8bf71fd
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update group.page by devs.near at 2024-04-15T02:36:24.635Z

--- devs.near/group.page/source_code.jsx
+++ devs.near/group.page/source_code.jsx
@@ -1,21 +1,16 @@
 const creatorId = props.creatorId;
 const groupId = props.groupId;
-
 const groupInfo = Social.get(`${creatorId}/thing/${groupId}/**`, "final");
-
 if (!groupInfo) {
   return "group details not found";
 }
 const groupData = JSON.parse(groupInfo[""]);
-
 const NavUnderline = styled.ul`
   border-bottom: 1px #eceef0 solid;
-
   a {
     color: #687076;
     text-decoration: none;
   }
-
   a.active {
     font-weight: bold;
     color: #0c7283;
@@ -23,29 +18,24 @@ const NavUnderline = styled.ul`
   }
 `;
 /* END_INCLUDE: "core/lib/gui/navigation" */
-
 const Button = styled.button`
   height: 40px;
   font-size: 14px;
   border-color: #e3e3e0;
   background-color: #ffffff;
 `;
-
 const Banner = styled.div`
   max-width: 100%;
   min-height: 240px;
   height: 240px;
 `;
-
 const Content = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
 `;
-
 const { Feed } = VM.require("efiz.near/widget/Module.Feed");
 Feed = Feed || (() => <></>);
-
 const tabs = [
   {
     defaultActive: true,
@@ -149,14 +139,11 @@ const tabs = [
   },
   ...(groupData.tabs || []),
 ];
-
 State.init({
   selectedTab: tabs[0],
 });
-
 const { metadata } = groupInfo;
 const { name, description, image, backgroundImage } = metadata;
-
 function Module({ module }) {
   if (typeof module === "function") {
     return module();
@@ -164,7 +151,6 @@ function Module({ module }) {
     return <Widget src={module.src} />;
   }
 }
-
 return (
   <div className="d-flex flex-column gap-3 bg-white">
     <Banner
@@ -173,7 +159,6 @@ return (
         background: `center / cover no-repeat url(https://ipfs.near.social/ipfs/${backgroundImage.ipfs_cid})`,
       }}
     />
-
     <div className="d-md-flex d-block justify-content-between container">
       <div className="d-md-flex d-block align-items-end">
         <div className="position-relative">
@@ -188,13 +173,11 @@ return (
             />
           </div>
         </div>
-
         <div className="d-flex flex-column ps-3 pt-3 pb-2">
           <span className="h1 text-nowrap">{name}</span>
           <span className="text-secondary">{description}</span>
         </div>
       </div>
-
       <div className="d-flex align-items-end gap-3">
         <Button
           className="btn btn-outline-primary"
@@ -205,7 +188,6 @@ return (
         <Button className="btn btn-outline-primary">Join</Button>
       </div>
     </div>
-
     <NavUnderline className="nav">
       {tabs.map(({ iconClass, title }, index) =>
         title ? (
