d22ef224efd4a049c196b20dbd4460d1187f519a
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update group by devs.near at 2024-04-15T02:36:24.635Z

--- devs.near/group/source_code.jsx
+++ devs.near/group/source_code.jsx
@@ -1,18 +1,14 @@
 const groupId = props.groupId ?? "526fb256e74eelmf0nw3n5909bc189c13d";
 const creatorId = props.creatorId ?? "*";
-
 const group =
   props.group ?? Social.get(`${creatorId}/*/${groupId}/**`, "final");
-
 if (!group) {
   return "";
 }
-
 const Container = styled.div`
   background: #fbfbfb;
   padding: 23px;
 `;
-
 return (
   <Container>
     <div className="mx-auto">
