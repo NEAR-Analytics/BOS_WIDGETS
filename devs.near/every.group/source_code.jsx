a9a23e88b5f4333506c23dfe382819a896b1500f
devs.near <devs.near>
2024-04-15 02:36:24 +0000

    Update every.group by devs.near at 2024-04-15T02:36:24.635Z

--- devs.near/every.group/source_code.jsx
+++ devs.near/every.group/source_code.jsx
@@ -1,23 +1,18 @@
 const accountId = props.accountId ?? context.accountId;
 const groupId = props.groupId ?? "f8ad9d1a76259lmdpjnd74e69162a0a014";
 const creatorId = props.creatorId ?? "hack.near";
-
 const widgets = {
   group: "devs.near/widget/group.card",
   create: "devs.near/widget/group.create",
   edit: "devs.near/widget/group.edit",
 };
-
 // We get all of the groups that have been indexed
 const groups = Social.index("every", "group", { limit: 10 });
-
 // TODO: We can put an infinite scroll for groups
 // And then we could do a Social.get([...groups]) constructed from each index
-
 if (!groups) {
   return "";
 }
-
 // we check if they are a member ? Do we need this here?
 // Ohhhh you join everyone
 const isMember = Social.keys(
@@ -27,9 +22,7 @@ const isMember = Social.keys(
     values_only: true,
   }
 );
-
 const type = join ? "leave" : "join";
-
 const handleJoin = () => {
   Social.set({
     graph: { [groupId]: { [accountId]: "" } },
@@ -52,17 +45,14 @@ const handleJoin = () => {
     },
   });
 };
-
 const Container = styled.div`
   padding: 23px 0;
   margin: 0;
-
   .top-right {
     display: flex;
     align-items: center;
     justify-content: flex-end;
   }
-
   .bell-icon {
     font-size: 23px;
     color: #fff;
@@ -70,69 +60,55 @@ const Container = styled.div`
     text-decoration: none;
     transition: color 0.3s ease-in-out;
   }
-
   .bell-icon:hover {
     color: #fff;
   }
-
   .bell-icon .bi-bell {
     display: inline;
   }
-
   .bell-icon .bi-bell-fill {
     display: none;
   }
-
   .bell-icon:hover .bi-bell {
     display: none;
   }
-
   .bell-icon:hover .bi-bell-fill {
     display: inline;
   }
 `;
-
 const Header = styled.div`
   background: black;
-
   .large-text {
     font-size: 19px;
     font-weight: 555;
   }
 `;
-
 const Navbar = styled.div`
   margin-left: 20px;
   @media only screen and (max-width: 1061px) {
     margin: 10px 0 0 0;
   }
 `;
-
 const Center = styled.div`
   display: flex;
   justify-content: space-between;
   gap: 20px;
   align-items: flex-start;
 `;
-
 const GroupCard = styled.div`
   flex-basis: calc(33.33% - 20px);
   margin: 0;
 `;
-
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 10px;
-
   @media (hover: none) {
     grid-template-columns: repeat(1, 1fr);
   }
 `;
-
 const { Feed } = VM.require("efiz.near/widget/Module.Feed");
 Feed = Feed || (() => <></>);
-
 return (
   <>
     <Container>
@@ -193,14 +169,16 @@ return (
               },
             }}
             Item={(p) => {
-              return <Widget
-                key={p}
-                src={widgets.group}
-                props={{
-                  creatorId: p.accountId,
-                  groupId: p.value.id,
-                }}
-              />
+              return (
+                <Widget
+                  key={p}
+                  src={widgets.group}
+                  props={{
+                    creatorId: p.accountId,
+                    groupId: p.value.id,
+                  }}
+                />
+              );
             }}
             Layout={Grid}
           />
