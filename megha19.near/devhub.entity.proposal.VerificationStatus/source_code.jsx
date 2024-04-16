66699f9b6f15d2ef03f62414fdd9e582349afb0f
megha19.near <megha19.near>
2024-04-16 13:49:49 +0000

    Update devhub.entity.proposal.VerificationStatus by megha19.near at 2024-04-16T13:49:49.238Z

--- megha19.near/devhub.entity.proposal.VerificationStatus/source_code.jsx
+++ megha19.near/devhub.entity.proposal.VerificationStatus/source_code.jsx
@@ -179,7 +179,7 @@ const VerificationBtn = () => {
         onBlur={() => {
           setTimeout(() => {
             setKycOptions(false);
-          }, 100);
+          }, 0);
         }}
       >
         <div
@@ -210,6 +210,7 @@ const VerificationBtn = () => {
                   className={`option ${
                     selectedOption.value === option.value ? "selected" : ""
                   }`}
+                  onClick={() => setKycOptions(false)}
                 >
                   <div className={`d-flex gap-2 align-items-center`}>
                     <img src={option.src} height={30} />
