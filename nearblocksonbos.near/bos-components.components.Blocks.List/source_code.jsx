0b9fba5801ab9a741c6eda5139d74bbd2303afad
nearblocksonbos.near <nearblocksonbos.near>
2024-04-16 13:19:43 +0000

    Update bos-components.components.Blocks.List by nearblocksonbos.near at 2024-04-16T13:19:43.943Z

--- nearblocksonbos.near/bos-components.components.Blocks.List/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Blocks.List/source_code.jsx
@@ -267,9 +267,16 @@ function MainComponent({ currentPage, setPage, t, network, ownerId }) {
       key: 'count',
       cell: (row) => (
         <span>
-          {row?.transactions_agg?.count
-            ? localFormat(row?.transactions_agg?.count)
-            : row?.transactions_agg?.count ?? ''}
+          <Link
+            href={`/txns?block=${row?.block_hash}`}
+            className="hover:no-underline"
+          >
+            <a className="text-green-500 hover:no-underline font-medium">
+              {row?.transactions_agg?.count
+                ? localFormat(row?.transactions_agg?.count)
+                : row?.transactions_agg?.count ?? ''}
+            </a>
+          </Link>
         </span>
       ),
       tdClassName:
@@ -371,29 +378,31 @@ function MainComponent({ currentPage, setPage, t, network, ownerId }) {
           <Skeleton className="h-4" />
         </div>
       ) : (
-        <p className="leading-7 pl-6 text-sm py-4 text-nearblue-600 dark:text-neargray-10">
-          {t
-            ? t('blocks:listing', {
-                from: start?.block_height
-                  ? localFormat && localFormat(start?.block_height)
-                  : start?.block_height ?? '',
-                to: end?.block_height
-                  ? localFormat && localFormat(end?.block_height)
-                  : end?.block_height ?? '',
-                count: localFormat && localFormat(totalCount.toString()),
-              })
-            : `Block #${
-                start?.block_height
-                  ? localFormat && localFormat(start?.block_height)
-                  : start?.block_height ?? ''
-              } to ${
-                '#' + end?.block_height
-                  ? localFormat && localFormat(end?.block_height)
-                  : end?.block_height ?? ''
-              } (Total of ${
-                localFormat && localFormat(totalCount.toString())
-              } blocks)`}{' '}
-        </p>
+        <div className="leading-7 pl-6 text-sm py-4 text-nearblue-600 dark:text-neargray-10">
+          <p className="sm:w-full w-65">
+            {t
+              ? t('blocks:listing', {
+                  from: start?.block_height
+                    ? localFormat && localFormat(start?.block_height)
+                    : start?.block_height ?? '',
+                  to: end?.block_height
+                    ? localFormat && localFormat(end?.block_height)
+                    : end?.block_height ?? '',
+                  count: localFormat && localFormat(totalCount.toString()),
+                })
+              : `Block #${
+                  start?.block_height
+                    ? localFormat && localFormat(start?.block_height)
+                    : start?.block_height ?? ''
+                } to ${
+                  '#' + end?.block_height
+                    ? localFormat && localFormat(end?.block_height)
+                    : end?.block_height ?? ''
+                } (Total of ${
+                  localFormat && localFormat(totalCount.toString())
+                } blocks)`}{' '}
+          </p>
+        </div>
       )}
       {
         <Widget
