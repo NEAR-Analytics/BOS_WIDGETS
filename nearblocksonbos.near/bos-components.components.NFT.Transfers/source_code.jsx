ebefee5fd3d717081ab223839c855ac2ea841cb2
nearblocksonbos.near <nearblocksonbos.near>
2024-04-16 08:24:22 +0000

    Update bos-components.components.NFT.Transfers by nearblocksonbos.near at 2024-04-16T08:24:22.912Z

--- nearblocksonbos.near/bos-components.components.NFT.Transfers/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.NFT.Transfers/source_code.jsx
@@ -28,7 +28,7 @@
 const Skeleton = (props) => {
   return (
     <div
-      className={`bg-gray-200  rounded shadow-sm animate-pulse ${props.className}`}
+      className={`bg-gray-200 dark:bg-black-200 rounded shadow-sm animate-pulse ${props.className}`}
     ></div>
   );
 };/* END_INCLUDE COMPONENT: "includes/Common/Skeleton.jsx" */
@@ -68,14 +68,14 @@ const getOptions = (status) => {
   switch (status) {
     case null:
       return {
-        bg: 'bg-yellow-50',
+        bg: 'bg-yellow-50 dark:bg-black',
         text: 'text-yellow-500',
         icon: FaHourglassStart,
         label: 'Pending',
       };
     case false:
       return {
-        bg: 'bg-red-50',
+        bg: 'bg-red-50 dark:bg-black',
         text: 'text-red-500',
         icon: FaTimesCircle,
         label: 'Failure',
@@ -83,7 +83,7 @@ const getOptions = (status) => {
 
     default:
       return {
-        bg: 'bg-emerald-50',
+        bg: 'bg-emerald-50 dark:bg-black',
         text: 'text-emerald-500',
         icon: FaCheckCircle,
         label: 'Success',
@@ -298,12 +298,12 @@ function MainComponent({ network, id, ownerId }) {
           <Tooltip.Provider>
             <Tooltip.Root>
               <Tooltip.Trigger asChild>
-                <span className="truncate max-w-[120px] inline-block align-bottom text-green-500 whitespace-nowrap">
+                <span className="truncate max-w-[120px] inline-block align-bottom text-green-500 dark:text-green-250  whitespace-nowrap">
                   <Link
                     href={`/txns/${row?.transaction_hash}`}
                     className="hover:no-underline"
                   >
-                    <a className="text-green-500 font-medium hover:no-underline">
+                    <a className="text-green-500 dark:text-green-250 font-medium hover:no-underline">
                       {row?.transaction_hash}
                     </a>
                   </Link>
@@ -320,9 +320,9 @@ function MainComponent({ network, id, ownerId }) {
           </Tooltip.Provider>
         </span>
       ),
-      tdClassName: 'px-5 py-4 text-sm text-nearblue-600',
+      tdClassName: 'px-5 py-4 text-sm text-nearblue-600 dark:text-neargray-10',
       thClassName:
-        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 uppercase tracking-wider',
+        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 dark:text-neargray-10 uppercase tracking-wider',
     },
     {
       header: <span>METHOD</span>,
@@ -332,7 +332,7 @@ function MainComponent({ network, id, ownerId }) {
           <Tooltip.Provider>
             <Tooltip.Root>
               <Tooltip.Trigger asChild>
-                <span className="bg-blue-900/10 text-xs text-nearblue-600 rounded-xl px-2 py-1 max-w-[120px] inline-flex truncate">
+                <span className="bg-blue-900/10 text-xs text-nearblue-600 dark:text-neargray-10 rounded-xl px-2 py-1 max-w-[120px] inline-flex truncate">
                   <span className="block truncate">{row?.cause}</span>
                 </span>
               </Tooltip.Trigger>
@@ -348,9 +348,9 @@ function MainComponent({ network, id, ownerId }) {
         </span>
       ),
       tdClassName:
-        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 font-medium',
+        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 dark:text-neargray-10 font-medium',
       thClassName:
-        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 uppercase tracking-wider',
+        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 dark:text-neargray-10 uppercase tracking-wider',
     },
     {
       header: <span>From</span>,
@@ -363,10 +363,10 @@ function MainComponent({ network, id, ownerId }) {
                 <Tooltip.Root>
                   <Tooltip.Trigger asChild>
                     <span
-                      className={`truncate max-w-[120px] inline-block align-bottom text-green-500 whitespace-nowrap ${
+                      className={`truncate max-w-[120px] inline-block align-bottom text-green-500 dark:text-green-250 whitespace-nowrap ${
                         row?.affected_account_id === address
                           ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
-                          : 'text-green-500 p-0.5 px-1'
+                          : 'text-green-500 dark:text-green-250 p-0.5 px-1'
                       }`}
                     >
                       <Link
@@ -374,7 +374,7 @@ function MainComponent({ network, id, ownerId }) {
                         className="hover:no-underline"
                       >
                         <a
-                          className="text-green-500 hover:no-underline"
+                          className="text-green-500 dark:text-green-250 hover:no-underline"
                           onMouseOver={(e) =>
                             onHandleMouseOver(e, row?.affected_account_id)
                           }
@@ -405,10 +405,10 @@ function MainComponent({ network, id, ownerId }) {
                 <Tooltip.Root>
                   <Tooltip.Trigger asChild>
                     <span
-                      className={`truncate max-w-[120px] inline-block align-bottom text-green-500 whitespace-nowrap ${
+                      className={`truncate max-w-[120px] inline-block align-bottom text-green-500 dark:text-green-250 whitespace-nowrap ${
                         row?.involved_account_id === address
                           ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
-                          : 'text-green-500 p-0.5 px-1'
+                          : 'text-green-500 dark:text-green-250 p-0.5 px-1'
                       }`}
                     >
                       <Link
@@ -416,7 +416,7 @@ function MainComponent({ network, id, ownerId }) {
                         className="hover:no-underline"
                       >
                         <a
-                          className="text-green-500 hover:no-underline"
+                          className="text-green-500 dark:text-green-250 hover:no-underline"
                           onMouseOver={(e) =>
                             onHandleMouseOver(e, row?.involved_account_id)
                           }
@@ -443,9 +443,9 @@ function MainComponent({ network, id, ownerId }) {
         );
       },
       tdClassName:
-        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 font-medium',
+        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 dark:text-neargray-10 font-medium',
       thClassName:
-        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 uppercase tracking-wider',
+        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 dark:text-neargray-10 uppercase tracking-wider',
     },
     {
       header: <span></span>,
@@ -474,10 +474,10 @@ function MainComponent({ network, id, ownerId }) {
                 <Tooltip.Root>
                   <Tooltip.Trigger asChild>
                     <span
-                      className={`truncate max-w-[120px] inline-block align-bottom text-green-500 whitespace-nowrap ${
+                      className={`truncate max-w-[120px] inline-block align-bottom text-green-500 dark:text-green-250 whitespace-nowrap ${
                         row?.involved_account_id === address
                           ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
-                          : 'text-green-500 p-0.5 px-1'
+                          : 'text-green-500 dark:text-green-250 p-0.5 px-1'
                       }`}
                     >
                       <Link
@@ -485,7 +485,7 @@ function MainComponent({ network, id, ownerId }) {
                         className="hover:no-underline"
                       >
                         <a
-                          className="text-green-500 hover:no-underline"
+                          className="text-green-500 dark:text-green-250 hover:no-underline"
                           onMouseOver={(e) =>
                             onHandleMouseOver(e, row?.involved_account_id)
                           }
@@ -516,10 +516,10 @@ function MainComponent({ network, id, ownerId }) {
                 <Tooltip.Root>
                   <Tooltip.Trigger asChild>
                     <span
-                      className={`truncate max-w-[120px] inline-block align-bottom text-green-500 whitespace-nowrap ${
+                      className={`truncate max-w-[120px] inline-block align-bottom text-green-500 dark:text-green-250 whitespace-nowrap ${
                         row?.affected_account_id === address
                           ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
-                          : 'text-green-500 p-0.5 px-1'
+                          : 'text-green-500 dark:text-green-250 p-0.5 px-1'
                       }`}
                     >
                       <Link
@@ -527,7 +527,7 @@ function MainComponent({ network, id, ownerId }) {
                         className="hover:no-underline"
                       >
                         <a
-                          className="text-green-500 hover:no-underline"
+                          className="text-green-500 dark:text-green-250 hover:no-underline"
                           onMouseOver={(e) =>
                             onHandleMouseOver(e, row?.affected_account_id)
                           }
@@ -554,9 +554,9 @@ function MainComponent({ network, id, ownerId }) {
         );
       },
       tdClassName:
-        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 font-medium',
+        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 dark:text-neargray-10 font-medium',
       thClassName:
-        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 uppercase tracking-wider',
+        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 dark:text-neargray-10 uppercase tracking-wider',
     },
     {
       header: <span>TOKEN ID</span>,
@@ -581,9 +581,10 @@ function MainComponent({ network, id, ownerId }) {
           </Tooltip.Provider>
         </>
       ),
-      tdClassName: 'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 ',
+      tdClassName:
+        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 dark:text-neargray-10',
       thClassName:
-        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 uppercase tracking-wider whitespace-nowrap',
+        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 dark:text-neargray-10 uppercase tracking-wider whitespace-nowrap',
     },
     {
       header: (
@@ -594,12 +595,12 @@ function MainComponent({ network, id, ownerId }) {
                 <button
                   type="button"
                   onClick={toggleShowAge}
-                  className="text-left text-xs px-5 py-4 w-full flex items-center font-semibold uppercase tracking-wider text-green-500 focus:outline-none flex-row whitespace-nowrap"
+                  className="text-left text-xs px-5 py-4 w-full flex items-center font-semibold uppercase tracking-wider text-green-500 dark:text-green-250 focus:outline-none flex-row whitespace-nowrap"
                 >
                   {showAge ? (
                     <>
                       {'AGE'}
-                      <Clock className="text-green-500 ml-2" />
+                      <Clock className="text-green-500 dark:text-green-250 ml-2" />
                     </>
                   ) : (
                     <> {'DATE TIME (UTC)'}</>
@@ -654,7 +655,8 @@ function MainComponent({ network, id, ownerId }) {
           </Tooltip.Provider>
         </span>
       ),
-      tdClassName: 'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600',
+      tdClassName:
+        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 dark:text-neargray-10',
       thClassName: 'inline-flex',
     },
     {
@@ -666,16 +668,16 @@ function MainComponent({ network, id, ownerId }) {
             href={`/nft-token/${row?.nft?.contract}/${row?.token_id}`}
             className="hover:no-underline"
           >
-            <a className="bg-gray-100 hover:bg-gray-200 px-2 py-1 rounded hover:no-underline">
+            <a className="bg-gray-100 dark:bg-black-200 hover:bg-gray-200 px-2 py-1 rounded hover:no-underline">
               View NFT &gt;
             </a>
           </Link>
         </span>
       ),
       tdClassName:
-        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 font-medium',
+        'px-5 py-4 whitespace-nowrap text-sm text-nearblue-600 dark:text-neargray-10 font-medium',
       thClassName:
-        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 uppercase tracking-wider whitespace-nowrap',
+        'px-5 py-4 text-left text-xs font-semibold text-nearblue-600 dark:text-neargray-10 uppercase tracking-wider whitespace-nowrap',
     },
   ];
   return (
@@ -687,7 +689,7 @@ function MainComponent({ network, id, ownerId }) {
       ) : (
         <div className={`flex flex-col lg:flex-row pt-4`}>
           <div className="flex flex-col">
-            <p className="leading-7 px-6 text-sm mb-4 text-nearblue-600">
+            <p className="leading-7 px-6 text-sm mb-4 text-nearblue-600 dark:text-neargray-10">
               {Object.keys(txns).length > 0 &&
                 `A total of ${
                   localFormat && localFormat(totalCount.toString())
