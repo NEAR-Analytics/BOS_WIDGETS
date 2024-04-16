4b50add9354e83ce60119d947ca8ac45aae04647
nearblocksonbos.near <nearblocksonbos.near>
2024-04-16 23:34:54 +0000

    Update bos-components.components.NFT.TokenTransfers by nearblocksonbos.near at 2024-04-16T23:34:54.173Z

--- nearblocksonbos.near/bos-components.components.NFT.TokenTransfers/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.NFT.TokenTransfers/source_code.jsx
@@ -339,7 +339,7 @@ function MainComponent({ network, t, id, tid, ownerId }) {
                     <span
                       className={`inline-block align-bottom text-green-500 dark:text-green-250 whitespace-nowrap ${
                         row?.affected_account_id === address
-                          ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
+                          ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] dark:bg-black-200 dark:border-neargray-50 border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
                           : 'text-green-500 dark:text-green-250 p-0.5 px-1'
                       }`}
                     >
@@ -381,7 +381,7 @@ function MainComponent({ network, t, id, tid, ownerId }) {
                     <span
                       className={`inline-block align-bottom text-green-500 dark:text-green-250 whitespace-nowrap ${
                         row?.involved_account_id === address
-                          ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
+                          ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] dark:bg-black-200 dark:border-neargray-50 border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
                           : 'text-green-500 dark:text-green-250 p-0.5 px-1'
                       }`}
                     >
@@ -450,7 +450,7 @@ function MainComponent({ network, t, id, tid, ownerId }) {
                     <span
                       className={`inline-block align-bottom text-green-500 dark:text-green-250 whitespace-nowrap ${
                         row?.involved_account_id === address
-                          ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
+                          ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] dark:bg-black-200 dark:border-neargray-50 border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
                           : 'text-green-500 dark:text-green-250 p-0.5 px-1'
                       }`}
                     >
@@ -492,7 +492,7 @@ function MainComponent({ network, t, id, tid, ownerId }) {
                     <span
                       className={`inline-block align-bottom text-green-500 dark:text-green-250 whitespace-nowrap ${
                         row?.affected_account_id === address
-                          ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
+                          ? ' rounded-md bg-[#FFC10740] border-[#FFC10740] dark:bg-black-200 dark:border-neargray-50 border border-dashed p-0.5 px-1 -m-[1px] cursor-pointer text-[#033F40]'
                           : 'text-green-500 dark:text-green-250 p-0.5 px-1'
                       }`}
                     >
