65848421eed1730ef44dccb14abed8cfcf79577a
nearblocksonbos.near <nearblocksonbos.near>
2024-04-17 13:48:23 +0000

    Update bos-components.components.Contract.ViewOrChangeAbi by nearblocksonbos.near at 2024-04-17T13:48:23.637Z

--- nearblocksonbos.near/bos-components.components.Contract.ViewOrChangeAbi/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Contract.ViewOrChangeAbi/source_code.jsx
@@ -157,15 +157,18 @@ function MainComponent(props) {
 
     try {
       const args = mapFeilds(fields) ?? {};
-      const res =
-        Near.view(id, toSnakeCase(method?.name), args);
-
-      if (res !== null) {
-        setError(null);
-        setTxn(res?.transaction_outcome?.id);
-
-        setResult(JSON.stringify(res, null, 2));
-      }
+      Near.asyncView(id, toSnakeCase(method?.name), args)
+        .then((resp) => {
+          setError(null);
+          setTxn(resp?.transaction_outcome?.id);
+          setResult(JSON.stringify(resp, null, 2));
+        })
+        .catch((error) => {
+          console.log(error);
+          setTxn(null);
+          setError(error?.message);
+          setResult(null);
+        });
     } catch (error) {
       setTxn(null);
       setError(error);
@@ -185,12 +188,9 @@ function MainComponent(props) {
       const args = mapFeilds(fields) ?? {};
       const res =
         Near.call(id, toSnakeCase(method?.name), args);
-
-      if (res !== null) {
-        setError(null);
-        setTxn(res?.transaction_outcome?.id);
-        setResult(JSON.stringify(res, null, 2));
-      }
+      setError(null);
+      setTxn(res?.transaction_outcome?.id);
+      setResult(JSON.stringify(res, null, 2));
     } catch (error) {
       setTxn(null);
       setError(error);
