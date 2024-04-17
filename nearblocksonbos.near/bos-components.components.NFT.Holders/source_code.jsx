c1cda05b03c5d7a19c65dbbad44db8b09a4639d9
nearblocksonbos.near <nearblocksonbos.near>
2024-04-17 16:20:52 +0000

    Update bos-components.components.NFT.Holders by nearblocksonbos.near at 2024-04-17T16:20:52.254Z

--- nearblocksonbos.near/bos-components.components.NFT.Holders/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.NFT.Holders/source_code.jsx
@@ -258,7 +258,7 @@ function MainComponent({ network, id, token, ownerId }) {
               <div className="h-0.5 mt-1 w-full bg-gray-100">
                 <div
                   style={{ width: `${percentage}%` }}
-                  className="h-0.5 bg-green-500"
+                  className="h-0.5 bg-green-500 dark:bg-green-250"
                 />
               </div>
             )}
