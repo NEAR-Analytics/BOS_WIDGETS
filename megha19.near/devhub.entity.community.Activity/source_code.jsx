627c77910902a1d7b295897a0da15aaa514e3291
megha19.near <megha19.near>
2024-04-17 17:17:03 +0000

    Update devhub.entity.community.Activity by megha19.near at 2024-04-17T17:17:03.016Z

--- megha19.near/devhub.entity.community.Activity/source_code.jsx
+++ megha19.near/devhub.entity.community.Activity/source_code.jsx
@@ -3,10 +3,16 @@ const { handle } = props;
 const { getCommunity } = VM.require(
   "megha19.near/widget/core.adapter.devhub-contract"
 );
-
 const { href } = VM.require("megha19.near/widget/core.lib.url");
 
-if (!getCommunity || !href) {
+getCommunity = getCommunity || (() => <></>);
+href || (href = () => {});
+
+if (!handle) {
+  return <p>Handle not defined</p>;
+}
+
+if (!href) {
   return <p>Loading modules...</p>;
 }
 
