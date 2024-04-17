4c20278407691c7e302676ce3f15daa7cb3bc235
nearblocksonbos.near <nearblocksonbos.near>
2024-04-17 16:21:28 +0000

    Update bos-components.components.Accounts by nearblocksonbos.near at 2024-04-17T16:21:28.617Z

--- nearblocksonbos.near/bos-components.components.Accounts/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Accounts/source_code.jsx
@@ -264,7 +264,7 @@ const TokenHoldings = (props) => {
                     NFT Tokens{' '}
                     <span className="font-normal">({nfts?.length})</span>
                   </div>
-                  <div className="text-gray-600 dark:text-neargray-10 text-xs divide-y divide-black-200 outline-none">
+                  <div className="text-gray-600 dark:text-neargray-10 text-xs divide-y dark:divide-black-200 outline-none">
                     {nfts.map((nft) => (
                       <div key={nft?.contract}>
                         <Link
@@ -1128,7 +1128,7 @@ function MainComponent(props) {
                       }}
                       className={`  text-xs leading-4 font-medium overflow-hidden inline-block cursor-pointer p-2 mb-3 mr-2 focus:outline-none ${
                         pageTab === tab
-                          ? 'rounded-lg bg-green-600 dark:bg-green-250 text-white dark:text-black'
+                          ? 'rounded-lg bg-green-600 dark:bg-green-250 text-white'
                           : 'hover:bg-neargray-800 bg-neargray-700 dark:bg-black-200 rounded-lg hover:text-nearblue-600 text-nearblue-600 dark:text-neargray-10'
                       }`}
                       value={tab}
