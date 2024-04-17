e00d7da7e912b65ce9dc11d8c309cd476cd91ced
ndcdev.near <ndcdev.near>
2024-04-17 11:51:18 +0000

    Update daos-staging.Components.Form by ndcdev.near at 2024-04-17T11:51:18.444Z

--- ndcdev.near/daos-staging.Components.Form/source_code.jsx
+++ ndcdev.near/daos-staging.Components.Form/source_code.jsx
@@ -111,7 +111,7 @@ return (
     {preview ? (
       <MobileForm>
         <Widget
-          src="ndcdev.near/widget/daos-staging.Components.Post"
+          src="ndcdev.near/widget/daos-staging.Components.PostPreview"
           props={{
             item: { ...formEls, dao_id: selectedDaoId, attachments },
             index: 0,
