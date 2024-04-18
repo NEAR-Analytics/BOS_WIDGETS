d5d636e9409acb724d83c86232e24e5d8caea259
ndcdev.near <ndcdev.near>
2024-04-18 08:48:30 +0000

    Update daos.Components.Form by ndcdev.near at 2024-04-18T08:48:30.934Z

--- ndcdev.near/daos.Components.Form/source_code.jsx
+++ ndcdev.near/daos.Components.Form/source_code.jsx
@@ -54,13 +54,11 @@ const Form = styled.div`
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: space-beetween;
-  gap: 2rem;
-  padding-top: 2rem;
+  gap: 1rem;
+  padding-top: 1rem;
 
   @media screen and (max-width: 786px) {
     flex-direction: column;
-    gap: 1rem;
-    padding-top: 1rem;
   }
 `;
 
@@ -71,8 +69,6 @@ const MobileForm = styled.div`
 `;
 
 const UploadFileButton = styled.div`
-  width: 47%;
-
   @media screen and (max-width: 768px) {
     width: 100%;
   }
@@ -81,27 +77,32 @@ const UploadFileButton = styled.div`
 const [preview, setPreview] = useState(false);
 
 const PreviewButton = () => (
-  <div
-    style={{ width: "max-content" }}
-    className="btn-primary"
-    onClick={() => setPreview(!preview)}
-  >
+  <div className="btn-secondary" onClick={() => setPreview(!preview)}>
+    <i className="ph ph-eye fs-6" />
     Preview
-    <i className="bi bi-eye" />
   </div>
 );
 
 const ProposalButton = () => (
   <button
-    style={{ width: "max-content" }}
-    className="btn btn-primary"
+    className="btn-primary"
     disabled={form[formEls.post_type].some(
-      (el) => el.required && !formEls[el.name]
+      (el) => el.required && !formEls[el.name],
     )}
     onClick={handleSave}
   >
-    {id ? "Edit" : "Create"} {formEls.post_type}
-    <i className="bi bi-plus-lg" />
+    {id ? (
+      <>
+        <i className="ph ph-pencil-simple fs-6" />
+        Edit
+      </>
+    ) : (
+      <>
+        <i className="ph ph-plus fs-6" />
+        Create
+      </>
+    )}
+    {formEls.post_type}
   </button>
 );
 
@@ -110,7 +111,7 @@ return (
     {preview ? (
       <MobileForm>
         <Widget
-          src="ndcdev.near/widget/daos.Components.Post"
+          src="ndcdev.near/widget/daos.Components.PostPreview"
           props={{
             item: { ...formEls, dao_id: selectedDaoId, attachments },
             index: 0,
@@ -128,22 +129,22 @@ return (
     ) : (
       <Form className="d-flex flex-column gap-3">
         {!id && (
-        <div
-          onClick={() => {
-            const newFormEl = formEls;
-            newFormEl.post_type =
-              formEls.post_type === "Proposal" ? "Report" : "Proposal";
-            setFormEls(newFormEl);
-          }}
-        >
-          <p className="mb-2">Post type</p>
-          <div className="d-flex gap-3 align-items-center">
-            <Widget src={`ndcdev.near/widget/daos.Components.Switch`} />
-            <TypeSection>
-              <h4>{formEls.post_type}</h4>
-            </TypeSection>
+          <div
+            onClick={() => {
+              const newFormEl = formEls;
+              newFormEl.post_type =
+                formEls.post_type === "Proposal" ? "Report" : "Proposal";
+              setFormEls(newFormEl);
+            }}
+          >
+            <p className="mb-2">Post type</p>
+            <div className="d-flex gap-3 align-items-center">
+              <Widget src={`ndcdev.near/widget/daos.Components.Switch`} />
+              <TypeSection>
+                <h4>{formEls.post_type}</h4>
+              </TypeSection>
+            </div>
           </div>
-        </div>
         )}
         <div className="form-element">
           <label>Select DAO</label>
@@ -211,7 +212,8 @@ return (
           props={{
             onChange: handleAttachments,
             children: (
-              <UploadFileButton className="btn-primary">
+              <UploadFileButton className="btn-primary outlined">
+                <i className="ph ph-upload-simple fs-6" />
                 Upload File
               </UploadFileButton>
             ),
@@ -229,7 +231,7 @@ return (
           href="https://docs.google.com/document/d/110CqEddPa-99JwM8iCl_kKJxdXLH6IlVePwubO5A55o/edit#heading=h.qya6e5j9ka46"
         >
           <span>Near Digital Collective application form GUIDE</span>
-          <i className="bi bi-box-arrow-up-right" />
+          <i className="ph ph-arrow-square-out fs-5" />
         </a>
       </Form>
     )}
