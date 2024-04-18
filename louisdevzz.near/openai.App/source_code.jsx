a51534868fc9ebe2b5220fbbb82d0dc96202e66c
louisdevzz.near <louisdevzz.near>
2024-04-18 11:32:24 +0000

    Update openai.App by louisdevzz.near at 2024-04-18T11:32:24.916Z

---
+++ louisdevzz.near/openai.App/source_code.jsx
@@ -0,0 +1,7 @@
+return (
+  <>
+    <Widget src="louisdevzz.near/widget/openai.header" />
+    <Widget src="louisdevzz.near/widget/openai.main" />
+    <Widget src="louisdevzz.near/widget/openai.footer" />
+  </>
+);
