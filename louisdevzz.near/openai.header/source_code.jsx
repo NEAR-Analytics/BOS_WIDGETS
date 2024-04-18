9a08c6fb309d52fbd3ba0e2ab71080bf2de15e5d
louisdevzz.near <louisdevzz.near>
2024-04-18 13:17:05 +0000

    Update openai.header by louisdevzz.near at 2024-04-18T13:17:05.644Z

--- louisdevzz.near/openai.header/source_code.jsx
+++ louisdevzz.near/openai.header/source_code.jsx
@@ -6,7 +6,7 @@ State.init({
 });
 
 const tailwindCss = fetch(tailwindCssUrl).body;
-if (!tailwindCss) return "Can't load CSS 😔.";
+if (!tailwindCss) return "Loading...";
 
 if (!state.theme) {
   State.update({
