4e63a1352f9c13d34996d3da4875a2455911ec82
louisdevzz.near <louisdevzz.near>
2024-04-18 14:01:07 +0000

    Update openai.App by louisdevzz.near at 2024-04-18T14:01:07.227Z

--- louisdevzz.near/openai.App/source_code.jsx
+++ louisdevzz.near/openai.App/source_code.jsx
@@ -1,13 +1,21 @@
 const Container = styled.div`
   height:120vh;
   display:grid;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  .left{
+    grid-column: span 1 / span 1;
+  }
+  .right{
+    grid-column: span 2 / span 2;
+  }
 `;
 
 return (
   <Container style={{ height: "120vh" }}>
-    <Widget src="louisdevzz.near/widget/openai.sidetab" />
-    <div>
+    <div class="left">
+      <Widget src="louisdevzz.near/widget/openai.sidetab" />
+    </div>
+    <div class="right">
       <Widget src="louisdevzz.near/widget/openai.header" />
       <Widget src="louisdevzz.near/widget/openai.main" />
       <Widget src="louisdevzz.near/widget/openai.footer" />
