df944afe4974834d53db109501a562dc32c65b68
louisdevzz.near <louisdevzz.near>
2024-04-18 12:41:40 +0000

    Update openai.footer by louisdevzz.near at 2024-04-18T12:41:40.035Z

--- louisdevzz.near/openai.footer/source_code.jsx
+++ louisdevzz.near/openai.footer/source_code.jsx
@@ -82,7 +82,7 @@ return (
     </div>
     <div
       class="w-11/12 -translate-x-1/2 flex flex-wrap items-center justify-center gap-2 transition-all duration-500"
-      style={{ position: "absolute" }}
+      style={{ position: "absolute", bottom: "120px" }}
     >
       <button
         type="button"
@@ -142,6 +142,40 @@ return (
           ></path>
         </svg>
       </button>
+      <div
+        id="llm-input"
+        class="absolute left-[calc(50%)] z-0 flex w-11/12 -translate-x-1/2 justify-center rounded-full bg-background px-8 py-4 border border-gray-300 align-middle transition-all duration-500 lg:max-w-full"
+        style={{ bottom: "60px" }}
+      >
+        <input
+          class="flex w-full border-none px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus:outline-none  disabled:cursor-not-allowed disabled:opacity-50 min-h-[41px] min-w-0 flex-1 resize-none rounded-none border-none text-base focus-visible:ring-0 focus-visible:ring-offset-0"
+          name="query"
+          rows="1"
+          placeholder="Describe a UI you desire"
+        />
+        <div class="flex items-center">
+          <button
+            class="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-8 w-8 flex-none rounded-full"
+            type="submit"
+          >
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+            >
+              <path
+                d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   </Theme>
 );
