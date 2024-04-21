6515b3e3bd902e1b37fa3e9a84607302bcd6709d
i1417730344.tg <i1417730344.tg>
2024-04-21 21:47:01 +0000

    Update Onboarding.Starter-fork by i1417730344.tg at 2024-04-21T21:47:01.080Z

---
+++ i1417730344.tg/Onboarding.Starter-fork/source_code.jsx
@@ -0,0 +1,13 @@
+return (
+  <>
+    {/* src="near/widget/Onboarding.ComponentCard" to be pasted below */}
+    <Widget />
+    <br />
+    {/* src="near/widget/Onboarding.ComponentCollection" to be pasted below */}
+    <Widget />
+  </>
+);
+<Widget
+  src="dima_sheleg.near/widget/Onboarding.ComponentCollection"
+  props={{}}
+/>;
