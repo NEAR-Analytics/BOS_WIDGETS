5ca86ed5faff3426b5661915807752fe52d2bd80
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update fetch.daos by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/fetch.daos/source_code.jsx
+++ buildhub.near/fetch.daos/source_code.jsx
@@ -2,15 +2,12 @@
  * This would be nice to be in the DAO SDK
  */
 let daos;
-
 const apikey = "c5d70a09-5740-489d-8c3b-36fbc3d40bff";
-
 const forgeUrl = (apiUrl, params) =>
   apiUrl +
   Object.keys(params)
     .sort()
     .reduce((paramString, p) => paramString + `${p}=${params[p]}&`, "?");
-
 daos = fetch(forgeUrl(`https://api.pikespeak.ai/daos/all`, {}), {
   mode: "cors",
   headers: {
@@ -20,5 +17,4 @@ daos = fetch(forgeUrl(`https://api.pikespeak.ai/daos/all`, {}), {
 });
 if (daos === null) return "";
 daos = daos?.body;
-
 return { type: "daos", daos };
