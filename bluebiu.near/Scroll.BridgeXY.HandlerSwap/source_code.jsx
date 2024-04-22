6a25230da57e6eff70a49e34c59673d2a0305267
bluebiu.near <bluebiu.near>
2024-04-22 09:07:23 +0000

    Update Scroll.BridgeXY.HandlerSwap by bluebiu.near at 2024-04-22T09:07:23.135Z

--- bluebiu.near/Scroll.BridgeXY.HandlerSwap/source_code.jsx
+++ bluebiu.near/Scroll.BridgeXY.HandlerSwap/source_code.jsx
@@ -54,28 +54,6 @@ buildSwap().then(params => {
   }
 })
 
-// const _amount = new Big(amount).mul(Math.pow(10, 18)).toString()
-// const lastNum = _amount.slice(0, _amount.length - quote.vc.length) + quote.vc;
-// const signer = Ethers.provider().getSigner()
-// const params = {
-//     from: account,
-//     to: quote.endpoint,
-//     value: lastNum,
-// }
-// signer.sendTransaction(params)
-// .then((tx) => {
-//     console.log(tx)
-//     tx.wait()
-//       .then((res) => {
-//         onSuccess(res);
-//       })
-//       .catch((err) => {
-//         onError(tx);
-//       });
-//   })
-//   .catch((err) => {
-//     onError(err);
-//   });
 
 
 return "";
