7ce628d576566d5639a007246e8d646bc6e8a3e3
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update page.library by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/page.library/source_code.jsx
+++ buildhub.near/page.library/source_code.jsx
@@ -1,7 +1,6 @@
 const { Footer } = VM.require("buildhub.near/widget/home.Home") || {
   Footer: () => <></>,
 };
-
 return (
   <div className="container-xl mt-md-3">
     <Widget src="buildhub.near/widget/components.Library" />
