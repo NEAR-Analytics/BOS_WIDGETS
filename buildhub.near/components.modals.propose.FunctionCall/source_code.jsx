1078707d67094af91638758f1e47eac3c9557624
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update components.modals.propose.FunctionCall by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/components.modals.propose.FunctionCall/source_code.jsx
+++ buildhub.near/components.modals.propose.FunctionCall/source_code.jsx
@@ -1,7 +1,6 @@
 const { Button } = VM.require("buildhub.near/widget/components") || {
   Button: () => <></>,
 };
-
 const DaoSDK = VM.require("sdks.near/widget/SDKs.Sputnik.DaoSDK") || (() => {});
 if (!DaoSDK) {
   return <></>;
@@ -14,9 +13,8 @@ const [deposit, setDeposit] = useState(0);
 const [validatedAddresss, setValidatedAddress] = useState(true);
 const [text, setText] = useState("");
 const [editorKey, setEditorKey] = useState(0);
-
+const [notificationsData, setNotificationData] = useState(null);
 const bootstrapTheme = props.bootstrapTheme;
-
 useEffect(() => {
   if (!props.item) {
     return;
@@ -28,7 +26,6 @@ useEffect(() => {
 const memoizedKey = useMemo((editorKey) => editorKey, [editorKey]);
 const selectedDAO = props.selectedDAO;
 const sdk = DaoSDK(selectedDAO);
-
 const regex = /.{1}\.near$/;
 useEffect(() => {
   if (regex.test(contract) || contract === "") {
@@ -37,22 +34,18 @@ useEffect(() => {
     setValidatedAddress(false);
   }
 }, [contract]);
-
 const MarkdownEditor = `
   html {
     background: #23242b;
   }
-
   * {
     border: none !important;
   }
-
   .rc-md-editor {
     background: #4f5055;
     border-top: 1px solid #4f5055 !important;
     border-radius: 8px;
   }
-
   .editor-container {
     background: #4f5055;
   }
@@ -61,24 +54,19 @@ const MarkdownEditor = `
     
     border-radius: 0.5rem !important;
   }
-
   .header-list {
     display: flex;
     align-items: center;
   }
-
   textarea {
     background: #23242b !important;
     color: #fff !important;
-
     font-family: sans-serif !important;
     font-size: 1rem;
-
     border: 1px solid #4f5055 !important;
     border-top: 0 !important;
     border-radius: 0 0 8px 8px;
   }
-
   .rc-md-navigation {
     background: #23242b !important;
     border: 1px solid #4f5055 !important;
@@ -90,11 +78,9 @@ const MarkdownEditor = `
       color: #cdd0d5;
     }
   }
-
   .editor-container {
     border-radius: 0 0 8px 8px;
   }
-
   .rc-md-editor .editor-container .sec-md .input {
     overflow-y: auto;
     padding: 8px !important;
@@ -102,7 +88,6 @@ const MarkdownEditor = `
     border-radius: 0 0 8px 8px;
   }
 `;
-
 const TextareaWrapper = styled.div`
   display: grid;
   vertical-align: top;
@@ -110,23 +95,19 @@ const TextareaWrapper = styled.div`
   position: relative;
   align-items: stretch;
   width: 100%;
-
   textarea {
     display: flex;
     align-items: center;
     transition: all 0.3s ease;
   }
-
   textarea::placeholder {
     padding-top: 4px;
     font-size: 20px;
   }
-
   textarea:focus::placeholder {
     font-size: inherit;
     padding-top: 0px;
   }
-
   &::after,
   textarea,
   iframe {
@@ -144,17 +125,14 @@ const TextareaWrapper = styled.div`
     overflow: hidden;
     outline: none;
   }
-
   iframe {
     padding: 0;
   }
-
   textarea:focus,
   textarea:not(:empty) {
     border-bottom: 1px solid #eee;
     min-height: 5em;
   }
-
   &::after {
     content: attr(data-value) " ";
     visibility: hidden;
@@ -166,7 +144,6 @@ const TextareaWrapper = styled.div`
     font-size: 14px;
   }
 `;
-
 return (
   <div className="d-flex flex-column">
     <div className="form-group mb-3">
@@ -254,6 +231,16 @@ return (
         />
       </TextareaWrapper>
     </div>
+    <Widget
+      src="buildhub.near/widget/notification.NotificationRolesSelector"
+      props={{
+        daoId: selectedDAO,
+        onUpdate: (v) => {
+          setNotificationData(v);
+        },
+        proposalType: "Function Call",
+      }}
+    />
     <div className="w-100 d-flex">
       <Button
         disabled={!contract || !method || !validatedAddresss}
@@ -269,10 +256,11 @@ return (
             proposalGas: gas,
             gas: 180000000000000,
             deposit: 200000000000000,
+            additionalCalls: notificationsData,
           });
         }}
       >
-        Next
+        Create
       </Button>
     </div>
   </div>
