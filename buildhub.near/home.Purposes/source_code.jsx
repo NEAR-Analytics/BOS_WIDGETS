a9a07c94110085823fcabd0b0b06a406cfd37724
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update home.Purposes by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/home.Purposes/source_code.jsx
+++ buildhub.near/home.Purposes/source_code.jsx
@@ -2,27 +2,22 @@ const { Button, Tag } = VM.require("buildhub.near/widget/components") || {
   Button: () => <></>,
   Tag: () => <></>,
 };
-
 const gridLink =
   "https://ipfs.near.social/ipfs/bafkreiay3ytllrxhtyunppqxcazpistttwdzlz3jefdbsq5tosxuryauu4";
-
 const Container = styled.div`
   padding: 50px 48px;
   display: flex;
   flex-direction: column;
   gap: 100px;
-
   @media screen and (max-width: 768px) {
     padding: 32px 20px;
     gap: 50px;
   }
 `;
-
 const HeadingContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 24px;
-
   h2 {
     color: var(--paleta-escolhida-ffffff, #fff);
     font-size: 44px;
@@ -30,12 +25,10 @@ const HeadingContainer = styled.div`
     text-wrap: balance;
     font-family: "Poppins", sans-serif;
     margin: 0;
-
     span {
       font-weight: 700;
     }
   }
-
   h3 {
     color: var(--b-0-b-0-b-0, var(--White-50, #b0b0b0));
     font-size: 24px;
@@ -45,45 +38,37 @@ const HeadingContainer = styled.div`
     line-height: 140%; /* 33.6px */
     max-width: 930px;
   }
-
   @media screen and (max-width: 768px) {
     h2 {
       font-size: 24px;
       line-height: 130%;
     }
-
     h3 {
       font-size: 14px;
     }
   }
 `;
-
 const StepContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, minmax(0, 1fr));
   place-items: center;
   align-items: stretch;
   gap: 32px;
-
   @media screen and (max-width: 768px) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
   }
 `;
-
 const Step = styled.div`
   display: flex;
   padding: 40px 56px;
   flex-direction: column;
   gap: 40px;
-
   border-radius: 16px;
   border: 1px solid var(--White-50, #b0b0b0);
   background: var(--000000, #000);
-
   &.first {
     border: 1px solid var(--Gradient-1, #4a21a5);
   }
-
   h4 {
     color: var(--eca-227, #eca227);
     font-size: 52px;
@@ -91,58 +76,48 @@ const Step = styled.div`
     line-height: 140%; /* 89.6px */
     margin: 0;
   }
-
   h5 {
     color: var(--paleta-escolhida-ffffff, #fff);
     font-size: 28px;
     line-height: 120%; /* 43.2px */
     margin-bottom: 12px;
   }
-
   p {
     color: var(--6-e-6-e-6-e, var(--Black-50, #6e6e6e));
     font-size: 18px;
     line-height: 120%; /* 33.6px */
     margin: 0;
   }
-
   @media screen and (max-width: 768px) {
     padding: 24px 16px;
     flex-direction: row;
     align-items: center;
     width: 100%;
     gap: 16px;
-
     h4 {
       font-size: 32px;
     }
-
     h5 {
       font-size: 20px;
       line-height: normal;
       margin: 0;
     }
-
     p {
       font-size: 18px;
     }
   }
 `;
-
 const Banner = styled.div`
   padding: 40px 60px;
   position: relative;
-
   border-radius: 16px;
   background: linear-gradient(104deg, #eca227 33.65%, #4a21a5 99.99%);
   box-shadow: 4px 24px 48px 0px rgba(81, 255, 234, 0.1);
-
   .container {
     display: flex;
     align-items: center;
     width: 100%;
     justify-content: space-between;
-
     h3 {
       color: var(--paleta-escolhida-ffffff, #fff);
       font-family: Poppins, sans-serif;
@@ -151,17 +126,14 @@ const Banner = styled.div`
       line-height: 120%; /* 52.8px */
       text-wrap: balance;
       margin: 0;
-
       span {
         font-weight: 700;
       }
     }
-
     .right {
       display: flex;
       flex-direction: column;
       align-items: flex-end;
-
       p {
         color: var(--ffffff, #fff);
         font-family: Poppins, sans-serif;
@@ -170,11 +142,9 @@ const Banner = styled.div`
         line-height: 150%;
         margin: 0;
         text-align: right;
-
         span {
           font-weight: 700;
         }
-
         a {
           color: var(--ffffff, #fff);
           text-decoration: underline;
@@ -182,21 +152,17 @@ const Banner = styled.div`
       }
     }
   }
-
   @media screen and (max-width: 960px) {
     flex-direction: column;
     padding: 30px;
     gap: 40px;
-
     .container {
       flex-direction: column;
       gap: 24px;
-
       h3 {
         font-size: 24px;
         text-align: center;
       }
-
       .right {
         p {
           font-size: 14px;
@@ -206,7 +172,6 @@ const Banner = styled.div`
     }
   }
 `;
-
 const GridImage = styled.img`
   position: absolute;
   top: 0;
@@ -217,7 +182,6 @@ const GridImage = styled.img`
   opacity: 0.05;
   pointer-events: none;
 `;
-
 const Purposes = () => {
   return (
     <Container className="container-xl" key="purposes">
@@ -292,5 +256,4 @@ const Purposes = () => {
     </Container>
   );
 };
-
 return { Purposes };
