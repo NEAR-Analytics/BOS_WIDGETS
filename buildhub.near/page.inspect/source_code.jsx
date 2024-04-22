c2cf161cd0adcf820b5119673a7f1c60476191ce
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update page.inspect by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/page.inspect/source_code.jsx
+++ buildhub.near/page.inspect/source_code.jsx
@@ -5,21 +5,17 @@ const StyledWidgetSource = styled.div`
       border-radius: 1rem;
     }
   }
-
   h3 {
     color: var(--text-color, #fff);
     margin-bottom: 1rem;
   }
-
   .text-truncate {
     color: var(--text-color, #fff);
   }
-
   span {
     color: var(--text-color, #fff);
   }
 `;
-
 return (
   <StyledWidgetSource className="container-xl my-3" data-bs-theme="dark">
     <Widget
