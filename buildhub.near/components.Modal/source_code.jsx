cb6ad316be77f33a2e43e9e986d97b8274c1ec3b
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update components.Modal by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/components.Modal/source_code.jsx
+++ buildhub.near/components.Modal/source_code.jsx
@@ -1,9 +1,7 @@
 const { Button } = VM.require("buildhub.near/widget/components.Button") || {
   Button: () => <></>,
 };
-
 const toggle = props.toggle ?? <Button variant="primary">Open Modal</Button>;
-
 const Overlay = styled.div`
   position: fixed;
   top: 0;
@@ -18,23 +16,20 @@ const Overlay = styled.div`
   height: 100vh;
   background: var(--modal-overlay-color, rgba(11, 12, 20, 0.5));
 `;
-
 const Content = styled.div`
   min-width: 500px;
   max-width: 1000px;
   padding: 24px;
   outline: none !important;
-  background: var(--modal-background-color, #23242b);
+  background: var(--modal-background-color, #000);
   border-radius: 16px;
   color: var(--modal-text-color, #fff);
-
   @media screen and (max-width: 768px) {
     width: 100%;
     max-width: 100%;
     min-width: 0;
   }
 `;
-
 const NoButton = styled.button`
   background: transparent;
   border: none;
@@ -43,17 +38,18 @@ const NoButton = styled.button`
   box-shadow: none;
   display: none;
 `;
-
 const CloseContainer = styled.div`
   display: flex;
   justify-content: flex-end;
   width: 100%;
 `;
-
 const Icon = styled.i`
   font-size: 24px;
 `;
-
+const avoidDefaultBehavior = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+};
 function Modal({
   children,
   title,
@@ -63,6 +59,7 @@ function Modal({
   toggleContainerProps,
   key,
   hideCloseBtn,
+  disableOutsideClick,
 }) {
   return (
     <Dialog.Root key={key} open={open} onOpenChange={onOpenChange}>
@@ -71,18 +68,22 @@ function Modal({
       </Dialog.Trigger>
       <Dialog.Overlay asChild>
         <Overlay key={`Overlay-${key}`}>
-          <Dialog.Content asChild>
+          <Dialog.Content
+            asChild
+            onPointerDownOutside={disableOutsideClick && avoidDefaultBehavior}
+            onInteractOutside={disableOutsideClick && avoidDefaultBehavior}
+          >
             <Content>
               <div className="d-flex align-items-center justify-content-between pb-4">
                 <h5 className="w-100">{title}</h5>
                 {!hideCloseBtn && (
-                  <Dialog.Trigger asChild>
+                  <Dialog.Close asChild>
                     <CloseContainer>
                       <Button variant="outline" type="icon">
                         <Icon className="bi bi-x" />
                       </Button>
                     </CloseContainer>
-                  </Dialog.Trigger>
+                  </Dialog.Close>
                 )}
               </div>
               {children}
@@ -93,5 +94,4 @@ function Modal({
     </Dialog.Root>
   );
 }
-
 return { Modal };
