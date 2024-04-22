1a25609b3c0a2b9d76da77b9f94283c2134da31e
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update components.project.page.Code by buildhub.near at 2024-04-22T16:24:25.287Z

---
+++ buildhub.near/components.project.page.Code/source_code.jsx
@@ -0,0 +1,25 @@
+const { Button } = VM.require("buildhub.near/widget/components") || {
+  Button: () => <></>,
+};
+const { getProjectMeta } = VM.require(
+  "buildhub.near/widget/lib.project-data"
+) || {
+  getProjectMeta: () => {},
+};
+const { id } = props;
+const project = getProjectMeta(id);
+const { gitHub } = project;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  align-items: center;
+  justify-content: center;
+`;
+return (
+  <Container className="text-white">
+    <Button variant="secondary" href={gitHub} target="_blank" noLink={true}>
+      GitHub Repo
+    </Button>
+  </Container>
+);
