f44fb6fa21492ee4c54163833429d0069410814d
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update components.TextEditor by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/components.TextEditor/source_code.jsx
+++ buildhub.near/components.TextEditor/source_code.jsx
@@ -4,23 +4,19 @@ const TextareaWrapper = styled.div`
   align-items: center;
   position: relative;
   align-items: stretch;
-
   textarea {
     display: flex;
     align-items: center;
     transition: all 0.3s ease;
   }
-
   textarea::placeholder {
     padding-top: 4px;
     font-size: 20px;
   }
-
   textarea:focus::placeholder {
     font-size: inherit;
     padding-top: 0px;
   }
-
   &::after,
   textarea,
   iframe {
@@ -38,17 +34,14 @@ const TextareaWrapper = styled.div`
     overflow: hidden;
     outline: none;
   }
-
   iframe {
     padding: 0;
   }
-
   textarea:focus,
   textarea:not(:empty) {
     border-bottom: 1px solid #eee;
     min-height: 5em;
   }
-
   &::after {
     content: attr(data-value) " ";
     visibility: hidden;
@@ -60,7 +53,6 @@ const TextareaWrapper = styled.div`
     font-size: 14px;
   }
 `;
-
 const MarkdownEditor = `
   html {
     background: #000000;
@@ -69,13 +61,11 @@ const MarkdownEditor = `
   * {
     border: none !important;
   }
-
   .rc-md-editor {
     background: #3c3d43;
     border-top: 1px solid #3c3d43 !important; 
     border-radius: 8px;
   }
-
   .editor-container {
     background: #3c3d43;
   }
@@ -84,24 +74,19 @@ const MarkdownEditor = `
     
     border-radius: 0.5rem !important;
   }
-
   .header-list {
     display: flex;
     align-items: center;
   }
-
   textarea {
     background: #000000 !important;
     color: #fff !important;
-
     font-family: sans-serif !important;
     font-size: 1rem;
-
     border: 1px solid #3c3d43 !important;
     border-top: 0 !important;
     border-radius: 0 0 8px 8px;
   }
-
   .rc-md-navigation {
     background: #000000 !important;
     border: 1px solid #3c3d43 !important;
@@ -113,11 +98,9 @@ const MarkdownEditor = `
       color: #cdd0d5;
     }
   }
-
   .editor-container {
     border-radius: 0 0 8px 8px;
   }
-
   .rc-md-editor .editor-container .sec-md .input {
     overflow-y: auto;
     padding: 8px !important;
@@ -125,23 +108,19 @@ const MarkdownEditor = `
     border-radius: 0 0 8px 8px;
   }
 `;
-
 const Label = styled.label`
   color: var(--label-color, #fff);
-
   /* Body/16px */
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
   line-height: 170%; /* 27.2px */
 `;
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 4px;
 `;
-
 function TextEditor({ value, label, onChange, maxWidth }) {
   return (
     <Container>
@@ -163,5 +142,4 @@ function TextEditor({ value, label, onChange, maxWidth }) {
     </Container>
   );
 }
-
 return { TextEditor };
