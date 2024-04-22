78b9c5971ab61a5b39701bd62c972cf47a16888f
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update OrderedGraphFeed by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/OrderedGraphFeed/source_code.jsx
+++ buildhub.near/OrderedGraphFeed/source_code.jsx
@@ -1,19 +1,15 @@
 const { Header } = VM.require("buildhub.near/widget/components.Header") || {
   Header: () => <></>,
 };
-
 const accountId = props.accountId ?? context.accountId;
 const itemType = props.itemType;
 const renderItem = props.renderItem;
-
 if (!itemType) {
   return <p className="text-white">No graph item type passed.</p>;
 }
-
 const items = Social.getr(`${accountId}/graph/${itemType}`, "final", {
   withBlockHeight: true,
 });
-
 const StorageKey = "order";
 const order = Storage.privateGet(StorageKey);
 const graphItems = useMemo(() => {
@@ -22,7 +18,6 @@ const graphItems = useMemo(() => {
   }
   const itemMap = new Map();
   const path = [];
-
   const buildSrc = (node) => {
     if (node.hasOwnProperty("")) {
       itemMap.set(path.join("/"), node[":block"]);
@@ -35,7 +30,6 @@ const graphItems = useMemo(() => {
       }
     });
   };
-
   buildSrc(items ?? {}, [], itemMap);
   let entries = [...itemMap.entries()];
   entries.sort((a, b) => b[1] - a[1]);
@@ -43,24 +37,20 @@ const graphItems = useMemo(() => {
   entries.sort((a, b) => (order?.[a] || 0) - (order?.[b] || 0));
   Storage.privateSet(
     StorageKey,
-    Object.fromEntries(entries.map((a, i) => [a, i + 1])),
+    Object.fromEntries(entries.map((a, i) => [a, i + 1]))
   );
   return entries;
 }, [items, order]);
-
 let transformedArray = graphItems.map((item) => {
   let splitParts = item.split("/");
   let accountId = splitParts[0];
   let lastPart = splitParts[splitParts.length - 1];
   let blockHeight = isNaN(lastPart) ? null : parseInt(lastPart);
-
   return { accountId, blockHeight };
 });
-
 let filteredArray = transformedArray.filter(
-  (item) => item.blockHeight !== null,
+  (item) => item.blockHeight !== null
 );
-
 return (
   <>
     <Header>{itemType}s</Header>
