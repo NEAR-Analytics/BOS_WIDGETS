001ff800597250fe9203fa258888c60da7303809
dropcast.near <dropcast.near>
2024-04-22 12:11:20 +0000

    Update airdrop by dropcast.near at 2024-04-22T12:11:20.346Z

--- dropcast.near/airdrop/source_code.jsx
+++ dropcast.near/airdrop/source_code.jsx
@@ -39,9 +39,15 @@ const Card = styled.div`
     }  
 `;
 
-const Label = styled.label`
-    font-size: 14px;
-    margin-bottom: 4px;
+const DropBox = styled.label`
+    margin: 10px 0;
+    padding: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    border: 3px dotted #a3a3a3;
+    border-radius: 5px;
 `;
 
 const Button = styled.button`
@@ -51,267 +57,78 @@ const Button = styled.button`
     background-image: linear-gradient(to right, rgb(147, 51, 234), rgb(99, 102, 241));
 `;
 
-const NEAR_TOKEN = [
-  {
-    text: "> 1 $NEAR",
-    value: "near1",
-  },
-  {
-    text: "> 10 $NEAR",
-    value: "near2",
-  },
-  {
-    text: "> 100 $NEAR",
-    value: "near3",
-  },
-  {
-    text: "> 1000 $NEAR",
-    value: "near4",
-  },
-  {
-    text: "Not Applicable",
-    value: "near0",
-  },
-];
+const UploadButton = styled.button`
+    text-decoration: none;
+    background-color: #005af0;
+    color: #ffffff;
+    padding: 10px 20px;
+    border: none;
+    outline: none;
+    transition: 0.3s;
+`;
 
-const AGE_ACCOUNT = [
-  {
-    text: "> 1 Month",
-    value: "age1",
-  },
-  {
-    text: "> 6 Month",
-    value: "age2",
-  },
-  {
-    text: "> 1 Year",
-    value: "age3",
-  },
-  {
-    text: "> 2 Year",
-    value: "age4",
-  },
-  {
-    text: "Not Applicable",
-    value: "age0",
-  },
-];
+const CardText = styled.h3`
+    font-size: 22px;
+    font-weight: 600;
+`;
 
-const TRANSACTION = [
-  {
-    text: "> 10 Transactions",
-    value: "transaction1",
-  },
-  {
-    text: "> 50 Transactions",
-    value: "transaction2",
-  },
-  {
-    text: "> 1000 Transactions",
-    value: "transaction3",
-  },
-  {
-    text: "> 10000 Transactions",
-    value: "transaction4",
-  },
-  {
-    text: "Not Applicable",
-    value: "transaction0",
-  },
-];
+const DropBoxText1 = styled.h4`
+    font-size: 17px;
+    font-weight: 400;
+`;
+const DropBoxText2 = styled.h4`
+    margin-top: 10px;
+    margin-bottom: 20px;
+    font-size: 12px;
+    color: #a3a3a3;
+`;
 
-const BOOL = [
-  {
-    text: "Yes",
-    value: "yes",
-  },
-  {
-    text: "Not Applicable",
-    value: "no",
-  },
-];
+const [file, setFile] = useState(null);
 
-State.init({
-  near: "near0",
-  age: "age0",
-  transaction: "transaction0",
-  nft: "no",
-  meme: "no",
-  activity: "no",
-  error: "",
-  download: false,
-  file: "",
-  loading: false,
-});
-
-const changeOption = (key, value) => {
-  State.update({
-    ...state,
-    [key]: value,
-  });
+const handleFileChange = (e) => {
+  setFile(e.target.files[0]);
 };
 
-const convertObject = (params) => {
-  return Object.keys(params)
-    .map((param) => `${param}=${params[param]}`)
-    .join("&");
-};
+const handleSubmit = async (e) => {
+  e.preventDefault();
 
-const onSubmit = () => {
-  State.update({
-    ...state,
-    loading: true,
-    download: false,
-    file: "",
-  });
-  let promise = asyncFetch(`${API_URL}/api/auth/download`, {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "x-auth-token": TOKEN,
-    },
-    method: "POST",
-    body: convertObject({
-      near: state.near,
-      age: state.age,
-      transaction: state.transaction,
-      nft: state.nft,
-      meme: state.meme,
-      activity: state.activity,
-    }),
-  });
+  const formData = new FormData();
+  formData.append("file", file);
 
-  promise
-    .then((data) => {
-      if (data.status === 200) {
-        if (data.body?.error) {
-          State.update({
-            ...state,
-            loading: false,
-            error: data.body,
-          });
-        } else {
-          State.update({
-            ...state,
-            loading: false,
-            download: true,
-            file: data.body,
-          });
-        }
-      } else {
-        State.update({
-          ...state,
-          loading: false,
-          error: data.body,
-        });
-      }
-    })
-    .catch(() => {
-      State.update({
-        ...state,
-        loading: false,
-        error: "Api error",
-      });
+  try {
+    const response = await fetch("http://localhost:4000/upload", {
+      method: "POST",
+      body: formData,
     });
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 return (
   <Wrapper>
     <Card>
-      <div>
-        <h5 className="m-0" style={{ fontSize: 18 }}>
-          {`Custom Allowlist`}
-        </h5>
-      </div>
-      <div>
-        <Label>{`Minimum $NEAR balance`}</Label>
-        <Widget
-          props={{
-            noLabel: true,
-            width: "40vw",
-            options: NEAR_TOKEN,
-            value: state.near,
-            onChange: (val) => changeOption("near", val),
-          }}
-          src={`${Owner}/widget/Select`}
-        />
-      </div>
-      <div>
-        <Label>{`Age of the Account`}</Label>
-        <Widget
-          props={{
-            noLabel: true,
-            width: "40vw",
-            options: AGE_ACCOUNT,
-            value: state.age,
-            onChange: (val) => changeOption("age", val),
-          }}
-          src={`${Owner}/widget/Select`}
-        />
-      </div>
-      <div>
-        <Label>{`Transaction Count`}</Label>
-        <Widget
-          props={{
-            noLabel: true,
-            width: "40vw",
-            options: TRANSACTION,
-            value: state.transaction,
-            onChange: (val) => changeOption("transaction", val),
-          }}
-          src={`${Owner}/widget/Select`}
-        />
-      </div>
-      <div>
-        <Label>{`Holds NFTs`}</Label>
-        <Widget
-          props={{
-            noLabel: true,
-            width: "40vw",
-            options: BOOL,
-            value: state.nft,
-            onChange: (val) => changeOption("nft", val),
-          }}
-          src={`${Owner}/widget/Select`}
-        />
-      </div>
-      <div>
-        <Label>{`Holds Meme Coins`}</Label>
-        <Widget
-          props={{
-            noLabel: true,
-            width: "40vw",
-            options: BOOL,
-            value: state.meme,
-            onChange: (val) => changeOption("meme", val),
-          }}
-          src={`${Owner}/widget/Select`}
-        />
-        <p
-          style={{ fontSize: 14, color: "grey", margin: 0 }}
-        >{`Any meme coin ($Neko/$Lonk/$Blackdragon/$Shitzu)`}</p>
-      </div>
-      {/*
-      <div>
-        <Label>{`Has Near Social Activity`}</Label>
-        <Widget
-          props={{
-            noLabel: true,
-            width: "40vw",
-            options: BOOL,
-            value: state.activity,
-            onChange: (val) => changeOption("activity", val),
-          }}
-          src={`${Owner}/widget/Select`}
-        />
-      </div> */}
-      <Button className="btn" onClick={onSubmit} disabled={state.loading}>
-        {state.loading ? `Processing...` : `Submit`}
-      </Button>
-      {state.download && state.file && (
-        <div className="d-flex mt-1 justify-content-center align-items-center mb-2">
-          <a href={`${API_URL}/${state.file}`}>Download CSV</a>
-        </div>
-      )}
+      <CardText>Upload Files</CardText>
+      <DropBox>
+        <DropBoxText1>Select File here</DropBoxText1>
+        <DropBoxText2>Files Supported: PDF, TEXT, DOC , DOCX</DropBoxText2>
+        <Files onSubmit={handleSubmit}>
+          <input
+            type="file"
+            accept=".xlsx, .xls"
+            id="fileID"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+          />
+          {file && (
+            <div id="fileName" className="px-3 py-1 mt-3 rounded-pill">
+              {file?.name}asdfsdfasdf
+            </div>
+          )}
+          <UploadButton type="submit">Upload</UploadButton>
+        </Files>
+      </DropBox>
     </Card>
   </Wrapper>
 );
