494c1c5532ad27c01940ecc1acb1e240799cfccb
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update template.AppLayout by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/template.AppLayout/source_code.jsx
+++ buildhub.near/template.AppLayout/source_code.jsx
@@ -1,11 +1,9 @@
 /**
  * This is a standard layout with a header, body, and a footer
  */
-
 const { Button } = VM.require("buildhub.near/widget/components") || {
   Button: () => <></>,
 };
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -14,7 +12,6 @@ const Container = styled.div`
   // margin-top: calc(-1 * var(--body-top-padding));
   background-color: var(--bg-1, #000000);
 `;
-
 const ContentContainer = styled.div`
   flex: 1;
   display: flex;
@@ -22,7 +19,6 @@ const ContentContainer = styled.div`
   align-items: center;
   width: 100%;
 `;
-
 const Header = ({ page, routes, ...props }) => (
   <>
     <Widget src="buildhub.near/widget/components.banners.potlock" />
@@ -33,11 +29,9 @@ const Header = ({ page, routes, ...props }) => (
     />
   </>
 );
-
 const Footer = (props) => {
   return <></>;
 };
-
 // Define the new component that follows the AppLayout pattern
 function AppLayout({ routes, page, children, ...props }) {
   return (
@@ -48,5 +42,4 @@ function AppLayout({ routes, page, children, ...props }) {
     </Container>
   );
 }
-
 return { AppLayout };
