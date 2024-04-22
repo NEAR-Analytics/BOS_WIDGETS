2923714d64b8b7fa72d2830324018c8f04cbf525
bluebiu.near <bluebiu.near>
2024-04-22 09:07:23 +0000

    Update Scroll.BridgeXY.AmountOut by bluebiu.near at 2024-04-22T09:07:23.135Z

--- bluebiu.near/Scroll.BridgeXY.AmountOut/source_code.jsx
+++ bluebiu.near/Scroll.BridgeXY.AmountOut/source_code.jsx
@@ -2,14 +2,16 @@ const { loading, amount, routerAddress, target, onLoad, currency, from, to } = p
 
 const BASE_API = 'https://open-api.xy.finance/v1'
 
+
 function getToken() {
     let fromToken
     let toToken
-    return asyncFetch(`${BASE_API}/recommendedTokens`).then(res => {
+    const promise = asyncFetch(`${BASE_API}/recommendedTokens`).then(res => {
         const tokens = res.body.recommendedTokens
-        console.log('tokens: ', tokens, props)
+        return tokens
+    })
 
-
+    return promise.then(tokens => {
         for (let i = 0; i < tokens.length; i++) {
             const token = tokens[i]
             if (!fromToken && token.symbol === currency.symbol && token.chainId === from.id.toString()) {
@@ -39,8 +41,10 @@ function getQuote(fromChainId, toChainId, fromTokenAddress, toTokenAddress, amou
     return asyncFetch(`${BASE_API}/quote?srcChainId=${fromChainId}&fromTokenAddress=${fromTokenAddress}&amount=${amount}&destChainId=${toChainId}&toTokenAddress=${toTokenAddress}`)
 }
 
+
+
 useEffect(() => {
-    if (!loading) return
+    if (!loading || !amount || Number(amount) <= 0) return
 
     const _amount = new Big(amount).mul(Math.pow(10, currency.decimals)).toString()
     getToken().then(res => {
@@ -52,70 +56,28 @@ useEffect(() => {
                 received: body.quote.toTokenAmount,
                 gasCost: body.xyFee.amount,
                 quote: body,
+                amount: amount
             });
         } else {
             onLoad({
                 received: 0,
                 gasCost: 0,
                 errorTips: 'No route',
+                amount: amount
             });
         }
     }).catch(e => {
+        if (amount !== res?.body?.fromTokenAmount) {
+            return
+        }
         onLoad({
             received: 0,
             gasCost: 0,
+            amount: amount,
             errorTips: 'Invalid amount',
         });
     })
 
-
-    // asyncFetch('https://api.orbiter.finance/sdk/routers/cross-chain')
-    //     .then(routerRes => {
-    //         const result = routerRes.body.result
-
-    //         let line = ''
-    //         if (target.dstId === 1) {
-    //             line = `534352/1-ETH/ETH`
-    //         } else {
-    //             line = `1/534352-ETH/ETH`
-    //         }
-
-    //         const quote = result.find(item => item.line === line)
-    //         if (!quote) {
-    //             onLoad({
-    //                 received: 0,
-    //                 gasCost: 0,
-    //                 errorTips: 'No route',
-    //             });
-    //             return
-    //         }
-    //         const minBig = new Big(quote.minAmt)
-    //         const maxBig = new Big(quote.maxAmt)
-    //         const amountBig = new Big(amount)
-    //         const feeBig = new Big(quote.withholdingFee).plus(new Big(quote.tradeFee).div(Math.pow(10, 18)))
-    //         if (amountBig.gte(maxBig) || amountBig.lte(minBig)) {
-    //             onLoad({
-    //                 received: 0,
-    //                 gasCost: 0,
-    //                 errorTips: 'Invalid amount',
-    //             });
-    //             return
-    //         }
-
-    //         onLoad({
-    //             received: amountBig.minus(feeBig).toString(),
-    //             gasCost: feeBig.toString(),
-    //             quote
-    //         });
-    //     }).catch(e => {
-    //         onLoad({
-    //             received: 0,
-    //             gasCost: 0,
-    //             errorTips: 'Invalid amount',
-    //         });
-    //     })
-
-
-}, [loading])
+}, [loading, amount])
 
 return "";
