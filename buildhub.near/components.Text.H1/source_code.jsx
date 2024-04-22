2e773c678c0a65764a21d3db9f8bc2a2e9b80c44
buildhub.near <buildhub.near>
2024-04-22 16:24:25 +0000

    Update components.Text.H1 by buildhub.near at 2024-04-22T16:24:25.287Z

--- buildhub.near/components.Text.H1/source_code.jsx
+++ buildhub.near/components.Text.H1/source_code.jsx
@@ -6,9 +6,7 @@ const StyledH1 = styled.h1`
   letter-spacing: 0;
   color: ${(props) => `${props.textColor}`};
 `;
-
 function H1({ children, textColor }) {
   return <StyledH1 textColor={textColor ?? "#FFFFFF"}>{children}</StyledH1>;
 }
-
 return { H1 };
