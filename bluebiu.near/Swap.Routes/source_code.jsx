9fafb453694d66490a22ca13bbdbc0588cc54ff1
bluebiu.near <bluebiu.near>
2024-04-22 02:39:03 +0000

    Update Swap.Routes by bluebiu.near at 2024-04-22T02:39:03.622Z

---
+++ bluebiu.near/Swap.Routes/source_code.jsx
@@ -0,0 +1,12 @@
+const routes = props.routes || [];
+
+let str = "";
+
+if (routes.length === 0) return;
+routes[0].routes.forEach((route, i) => {
+  if (i === 0) {
+    str += route.token0.symbol + " > " + route.token1.symbol;
+  }
+});
+
+return str;
