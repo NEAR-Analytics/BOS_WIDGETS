2de25afc764756c9491bad406fb2236d18c06449
hack.near <hack.near>
2024-04-23 20:06:28 +0000

    Update profile.widget by hack.near at 2024-04-23T20:06:28.931Z

---
+++ hack.near/profile.widget/source_code.jsx
@@ -0,0 +1,29 @@
+const src = props.src ?? "hack.near/widget/Academy";
+const [accountId, widget, widgetName] = src.split("/");
+
+const metadata = props.metadata ?? Social.getr(`${src}/metadata`);
+
+return (
+  <>
+    <div className="d-flex flex-row">
+      <div className="me-1">
+        <Widget
+          src="mob.near/widget/WidgetImage"
+          props={{
+            accountId,
+            widgetName,
+          }}
+        />
+      </div>
+      <div className="text-truncate ms-2">
+        <span className="fw-bold ms-1">{metadata.name ?? widgetName}</span>
+        <div className="text-truncate text-muted">
+          <Widget src="hack.near/widget/BuilderHat" props={{ accountId }} />
+          <small>
+            <span className="font-monospace ms-1">@{accountId}</span>
+          </small>
+        </div>
+      </div>
+    </div>
+  </>
+);
