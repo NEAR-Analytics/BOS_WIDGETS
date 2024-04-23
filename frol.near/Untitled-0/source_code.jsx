63ff2fecec2b048b0ae1e6af6b32f92274caad76
frol.near <frol.near>
2024-04-23 23:52:46 +0000

    Update Untitled-0 by frol.near at 2024-04-23T23:52:46.828Z

--- frol.near/Untitled-0/source_code.jsx
+++ frol.near/Untitled-0/source_code.jsx
@@ -1,7 +1,6 @@
 return (
   <iframe
-    iframeResizer
-    style={{ minHeight: "100vh", minWidth: "100%" }}
+    style={{ minHeight: "calc(100vh - 40px)", minWidth: "100%" }}
     srcDoc={`
 <!DOCTYPE html><!-- This site was created in Webflow. https://www.webflow.com --><!-- Last Published: Tue Apr 23 2024 23:19:12 GMT+0000 (Coordinated Universal Time) -->
 <html data-wf-domain="vlads-top-notch-site-93ff6f.webflow.io" data-wf-page="66283e6740d1f728e73f782b" data-wf-site="66283e6740d1f728e73f77c8" data-wf-status="1">
@@ -47,9 +46,150 @@ return (
          </div>
          <div class="wrapper"><img src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7865_dash-device.png" alt="" srcset="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7865_dash-device-p-500.png 500w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7865_dash-device-p-800.png 800w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7865_dash-device-p-1080.png 1080w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7865_dash-device-p-1600.png 1600w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7865_dash-device.png 1903w" sizes="90vw"/></div>
       </div>
+      <div class="section">
+         <div id="features" class="feature-wrap">
+            <div data-w-id="4593c4b7-56e8-060a-be57-648b0e08fc65" style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0" class="feature-content">
+               <div class="title">Aenean Consectetur</div>
+               <h2 class="black">Cay sociis nato que penatibus et</h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+            </div>
+            <div class="feature-image"><img src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7887_app-06.png" width="341" alt="" class="image-2"/></div>
+         </div>
+         <div class="feature-wrap reverse">
+            <div data-w-id="cafe0045-d304-79d9-8f68-af3adaed06e9" style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0" class="feature-content">
+               <div class="title">ConsectUltricies </div>
+               <h2 class="black-text">Amet Ipsum </h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+            </div>
+            <div class="feature-image"><img src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f787e_app-05.png" width="386" alt="" srcset="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f787e_app-05-p-500.png 500w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f787e_app-05.png 540w" sizes="(max-width: 479px) 90vw, (max-width: 991px) 40vw, 25vw" class="image-2"/></div>
+         </div>
+         <div class="feature-wrap">
+            <div data-w-id="88086931-0bbf-a2f5-1fc3-58d79ba19b7e" style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0" class="feature-content">
+               <div class="title">Aenean Consectetur Porta</div>
+               <h2 class="black-text">Magna Porta Sit Bibendum</h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+            </div>
+            <div class="feature-image"><img src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f787b_app-04.png" width="341" alt="" class="image-2"/></div>
+         </div>
+         <div class="feature-wrap icons">
+            <div class="feature-content icons">
+               <img src="https://assets-global.website-files.com/5af49de0dbb1e3637f2d8886/5b11be7f11a9de6a863497c0_icon-burger-magenta.svg" width="80" alt="" class="feature-icon"/>
+               <h4 class="black-text">Activity Notifications</h4>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+            </div>
+            <div class="feature-content icons">
+               <img src="https://assets-global.website-files.com/5af49de0dbb1e3637f2d8886/5b11be87d0d5fe5d7ff154af_icon-silverware-magenta.svg" width="74" alt="" class="feature-icon"/>
+               <h4 class="black-text">Custom Timing</h4>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+            </div>
+            <div class="feature-content icons">
+               <img src="https://assets-global.website-files.com/5af49de0dbb1e3637f2d8886/5b11be8e11a9dee3ed3497c6_icon-data-magenta.svg" alt="" class="feature-icon"/>
+               <h4 class="black-text">Mobile Optimized</h4>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+            </div>
+         </div>
+         <div class="feature-wrap icons numbers">
+            <div class="feature-content icons numbers">
+               <div class="number">+12k</div>
+               <h4 class="black-text">Nibh Elit Tristique</h4>
+            </div>
+            <div class="feature-content icons numbers">
+               <div class="number">84</div>
+               <h4 class="black-text">Aenean Condimentum </h4>
+            </div>
+            <div class="feature-content icons numbers">
+               <div class="number">3,07</div>
+               <h4 class="black-text">Quam Inceptos</h4>
+            </div>
+            <div class="feature-content icons numbers">
+               <div class="number">24h</div>
+               <h4 class="black-text">Ullamcorper </h4>
+            </div>
+         </div>
+         <div id="how-to-use" class="feature-wrap">
+            <div data-w-id="0585a81c-ee42-8b56-bc8e-bd582946320a" style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0" class="feature-content">
+               <div class="title">Aenean Consectetur Porta</div>
+               <h2 class="black-text">Sem Risus Elit Vestibulum</h2>
+               <p>Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. <a href="#">Cras mattis consectetur</a> purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+            </div>
+            <div class="feature-image"><img src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786d_img-003.jpg" width="519" alt="" srcset="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786d_img-003-p-500.jpeg 500w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786d_img-003-p-1080.jpeg 1080w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786d_img-003-p-1600.jpeg 1600w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786d_img-003.jpg 1982w" sizes="(max-width: 479px) 90vw, (max-width: 767px) 40vw, (max-width: 991px) 45vw, 27vw" class="image-2"/></div>
+         </div>
+         <div class="feature-wrap reverse">
+            <div data-w-id="d79d0682-95d0-be91-6539-b82f492963f4" style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0" class="feature-content">
+               <div class="title">Aenean Consectetur Porta</div>
+               <h2 class="black-text">Parturient Tortor</h2>
+               <p>Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. <a href="#">Cras mattis consectetur</a> purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+            </div>
+            <div class="feature-image"><img src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786e_img-002.jpg" width="519" alt="" srcset="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786e_img-002-p-500.jpeg 500w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786e_img-002-p-1080.jpeg 1080w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786e_img-002-p-1600.jpeg 1600w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f786e_img-002.jpg 1982w" sizes="(max-width: 479px) 90vw, (max-width: 767px) 40vw, (max-width: 991px) 45vw, 27vw" class="image-2"/></div>
+         </div>
+         <div data-delay="4000" data-animation="slide" class="quote-slider w-slider" data-autoplay="false" data-easing="ease" style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0" data-hide-arrows="false" data-disable-swipe="false" data-w-id="321c18fb-db93-7e62-3afc-4dab2862f5d4" data-autoplay-limit="0" data-nav-spacing="8" data-duration="500" data-infinite="true">
+            <div class="w-slider-mask">
+               <div class="w-slide">
+                  <div class="quote-wrap">
+                     <img src="https://assets-global.website-files.com/5af49de0dbb1e3637f2d8886/5b14481dda3c5c4b9bfe5147_quotation.svg" width="58" alt="" class="quote-icon"/>
+                     <h3>HeadNulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Pus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.ing</h3>
+                     <div class="quote-attribution">
+                        <p>Joshua Home</p>
+                        <div class="quote-dash"></div>
+                        <p><strong>New York Times</strong></p>
+                     </div>
+                  </div>
+               </div>
+               <div class="w-slide">
+                  <div class="quote-wrap">
+                     <img src="https://assets-global.website-files.com/5af49de0dbb1e3637f2d8886/5b14481dda3c5c4b9bfe5147_quotation.svg" width="58" alt="" class="quote-icon"/>
+                     <h3>HeadNulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Pus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.ing</h3>
+                     <div class="quote-attribution">
+                        <div class="quote-dash"></div>
+                        <p>Nulla vitae elit libero, a pharetra augue.</p>
+                        <p><strong>New York Times</strong></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="left-arrow w-slider-arrow-left">
+               <div class="slider-arrow w-icon-slider-left"></div>
+            </div>
+            <div class="right-arrow w-slider-arrow-right">
+               <div class="slider-arrow w-icon-slider-right"></div>
+            </div>
+            <div class="slide-nav w-slider-nav w-slider-nav-invert w-round"></div>
+         </div>
+         <div data-w-id="23f92271-dba9-fb12-00d3-758ef4d10fb2" style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0" class="feature-wrap logos"><img src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7886_logos.png" alt="" srcset="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7886_logos-p-500.png 500w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7886_logos-p-800.png 800w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7886_logos-p-1080.png 1080w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7886_logos-p-1600.png 1600w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7886_logos.png 2720w" sizes="(max-width: 991px) 90vw, (max-width: 3885px) 70vw, 2720px"/></div>
+      </div>
+      <div class="bottom-image-wrap"><img src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7878_bg-apps.jpg" alt="" srcset="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7878_bg-apps-p-500.jpeg 500w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7878_bg-apps-p-1080.jpeg 1080w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7878_bg-apps-p-2000.jpeg 2000w, https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f7878_bg-apps.jpg 2500w" sizes="(max-width: 2500px) 100vw, 2500px"/></div>
+      <div id="download" class="section wide purple">
+         <div data-w-id="376e2a1f-ec70-0dab-11ea-b1863101e37b" style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0" class="wrap">
+            <h2>Ready to get started?</h2>
+            <div class="button-wrap"><a href="#" class="button w-button">Download Now</a><a href="#" class="button contact w-button">Get in Touch</a></div>
+         </div>
+      </div>
+      <div class="section wide">
+         <div data-w-id="8cf47482-19f1-1f52-25ba-910a60bc91de" style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0" class="wrap">
+            <h2>Get updates from Richard</h2>
+            <p>New course alerts, discounts and free lessons</p>
+            <div class="form-block w-form">
+               <form id="email-form" name="email-form" data-name="Email Form" method="get" class="form-2" data-wf-page-id="66283e6740d1f728e73f782b" data-wf-element-id="538f9bd6-8ca5-4ea7-b7d3-6a7d798b371d"><input class="input w-input" maxlength="256" name="email-2" data-name="Email 2" placeholder="Enter your email" type="text" id="email-2" required=""/><input type="submit" data-wait="Please wait..." class="button form-button w-button" value="Subscribe"/></form>
+               <div class="w-form-done">
+                  <div>Thank you! Your submission has been received!</div>
+               </div>
+               <div class="w-form-fail">
+                  <div>Oops! Something went wrong while submitting the form.</div>
+               </div>
+            </div>
+         </div>
+         <div class="wrapper"></div>
+      </div>
+      <div class="footer">
+         <div class="wrap">
+            <img src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/66283e6740d1f728e73f788d_icon-footer.png" width="136" alt=""/>
+            <div class="footer-list"><a href="#" class="footer-link">Email</a><a href="#" class="footer-link">Instagram</a><a href="#" class="footer-link">Facebook</a><a href="#" class="footer-link">Twitter</a><a href="/image-license-info" class="footer-link">Image License Info</a><a href="http://webflow.com" class="footer-link">Powered by Webflow</a></div>
+         </div>
+      </div>
       <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=66283e6740d1f728e73f77c8" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script><script src="https://assets-global.website-files.com/66283e6740d1f728e73f77c8/js/webflow.bcce7ec73.js" type="text/javascript"></script>
    </body>
 </html>
 `}
   />
 );
+
