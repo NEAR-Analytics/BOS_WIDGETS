f9c3640b00527b0b6fa4c5503c2ee1547f892354
opencann.near <opencann.near>
2024-04-23 22:24:31 +0000

    Update gitcoin.erc20 by opencann.near at 2024-04-23T22:24:31.213Z

---
+++ opencann.near/gitcoin.erc20/source_code.jsx
@@ -0,0 +1,121 @@
+const sender = props.user;
+const receiver = props.projectId;
+
+//if (!sender) return <Web3Connect connectLabel="Connect with Web3" />;
+
+const erc20Abi = fetch(
+  "https://gist.githubusercontent.com/veox/8800debbf56e24718f9f483e1e40c35c/raw/f853187315486225002ba56e5283c1dba0556e6f/erc20.abi.json"
+);
+if (!erc20Abi.ok) {
+  return "scam";
+}
+
+const iface = new ethers.utils.Interface(erc20Abi.body);
+
+initState({
+  token: "",
+  tokenDecimals: "",
+  sendTo: "",
+  sender,
+  senderBalance: "0",
+  receiverBalance: "0",
+  receiver: "",
+  amount: "1",
+});
+
+const tokens = {
+  "Select Token": "",
+  USDT: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+  DAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
+  USDC: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  MKR: "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+};
+
+const tokensMenuItems = Object.keys(tokens).map((token) => (
+  <option value={tokens[token]}>{token}</option>
+));
+
+const setSendTo = (sendTo) => {
+  State.update({ sendTo, receiver: receiver ?? "" });
+  refreshBalances();
+};
+
+const setToken = (token) => {
+  State.update({ token });
+  getTokenDecimals();
+};
+
+const getTokenBalance = (receiver) => {
+  const encodedData = iface.encodeFunctionData("balanceOf", [receiver]);
+
+  return Ethers.provider()
+    .call({
+      to: state.token,
+      data: encodedData,
+    })
+    .then((rawBalance) => {
+      const receiverBalanceHex = iface.decodeFunctionResult(
+        "balanceOf",
+        rawBalance
+      );
+
+      return Big(receiverBalanceHex.toString())
+        .div(Big(10).pow(state.tokenDecimals))
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,");
+    });
+};
+
+const getTokenDecimals = () => {
+  const encodedData = iface.encodeFunctionData("decimals", []);
+
+  return Ethers.provider()
+    .call({
+      to: state.token,
+      data: encodedData,
+    })
+    .then((tokenDecimals) => {
+      State.update({ tokenDecimals: parseInt(Number(tokenDecimals)) });
+      refreshBalances();
+    });
+};
+
+const refreshBalances = () => {
+  getTokenBalance(state.sender).then((value) => {
+    State.update({ senderBalance: value });
+  });
+
+  getTokenBalance(state.receiver).then((value) => {
+    State.update({ receiverBalance: value });
+  });
+};
+
+const sendTokens = () => {
+  const erc20 = new ethers.Contract(
+    state.token,
+    erc20Abi.body,
+    Ethers.provider().getSigner()
+  );
+
+  let amount = ethers.utils.parseUnits(state.amount, state.tokenDecimals);
+
+  erc20.transfer(state.receiver, amount);
+
+  console.log("transactionHash is " + transactionHash);
+};
+
+return (
+  <>
+    <div class="mb-3">
+      <select
+        class="form-select"
+        id="selectToken"
+        onChange={(e) => {
+          setToken(e.target.value);
+        }}
+      >
+        {tokensMenuItems}
+      </select>
+    </div>
+  </>
+);
