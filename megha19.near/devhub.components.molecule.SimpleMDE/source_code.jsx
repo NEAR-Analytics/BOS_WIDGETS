1dcf4d067c7820c066266e79178113172dd35f6f
megha19.near <megha19.near>
2024-04-24 14:17:09 +0000

    Update devhub.components.molecule.SimpleMDE by megha19.near at 2024-04-24T14:17:09.761Z

--- megha19.near/devhub.components.molecule.SimpleMDE/source_code.jsx
+++ megha19.near/devhub.components.molecule.SimpleMDE/source_code.jsx
@@ -24,22 +24,7 @@ const followingData = Social.get(
 const fontFamily = props.fontFamily ?? "sans-serif";
 const alignToolItems = props.alignToolItems ?? "right";
 const placeholder = props.placeholder ?? "";
-const showAccountAutoComplete = props.showAutoComplete ?? false;
-const showProposalIdAutoComplete = props.showProposalIdAutoComplete ?? false;
-
-const queryName =
-  "thomasguntenaar_near_devhub_proposals_quebec_proposals_with_latest_snapshot";
-const query = `query GetLatestSnapshot($offset: Int = 0, $limit: Int = 10, $where: ${queryName}_bool_exp = {}) {
-${queryName}(
-  offset: $offset
-  limit: $limit
-  order_by: {proposal_id: desc}
-  where: $where
-) {
-  name
-  proposal_id
-}
-}`;
+const showAutoComplete = props.showAutoComplete ?? false;
 
 const code = `
 <!doctype html>
@@ -94,12 +79,6 @@ const code = `
 
   <ul class="dropdown-menu" id="mentiondropdown" style="position: absolute;">
 </div>
-<div class="dropdown">
-  <button style="display: none" type="button" data-bs-toggle="dropdown">
-    Dropdown button
-  </button>
-  <ul class="dropdown-menu" id="referencedropdown" style="position: absolute;">
-</div>
 </ul>
 
 <textarea></textarea>
@@ -112,9 +91,7 @@ let codeMirrorInstance;
 let isEditorInitialized = false;
 let followingData = {};
 let profilesData = {};
-let query = '';
-let showAccountAutoComplete = ${showAccountAutoComplete};
-let showProposalIdAutoComplete = ${showProposalIdAutoComplete};
+let showAutocomplete = ${showAutoComplete}
 
 function getSuggestedAccounts(term) {
   let results = [];
@@ -157,53 +134,6 @@ function getSuggestedAccounts(term) {
   results.sort((a, b) => b.score - a.score);
   results = results.slice(0, limit);
 
-  return results;
-}
-
-async function asyncFetch(endpoint, { method, headers, body }) {
-  try {
-    const response = await fetch(endpoint, {
-      method: method,
-      headers: headers,
-      body: body
-    });
-
-    if (!response.ok) {
-      throw new Error("HTTP error!");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
-}
-
-
-async function getSuggestedProposals(id) {
-  let results = [];
-  const variables = {
-    limit: 5,
-    offset: 0,
-    where:{}
-  };
-  if (id) {
-    variables["where"] = { proposal_id: { _eq: id }};
-  }
-  await asyncFetch('https://near-queryapi.api.pagoda.co/v1/graphql', {
-    method: "POST",
-    headers: { "x-hasura-role": "thomasguntenaar_near" },
-    body: JSON.stringify({
-      query: query,
-      variables: variables,
-      operationName: "GetLatestSnapshot",
-    }),
-  }).then(res => {
-     const proposals = res?.data?.["thomasguntenaar_near_devhub_proposals_quebec_proposals_with_latest_snapshot"];
-     results = (proposals);
-  }).catch(error => {
-    console.error(error);
-  });
   return results;
 }
 
@@ -255,7 +185,7 @@ simplemde.codemirror.on('blur', () => {
   updateIframeHeight();
 });
 
-if (showAccountAutoComplete) {
+if (showAutocomplete) {
   let mentionToken;
   let mentionCursorStart;
   const dropdown = document.getElementById("mentiondropdown");
@@ -272,7 +202,7 @@ if (showAccountAutoComplete) {
     const cursor = cm.getCursor();
     const token = cm.getTokenAt(cursor);
 
-    const createMentionDropDownOptions = () => {
+    const createMentionDrowDownOptions = () => {
       const mentionInput = cm.getRange(mentionCursorStart, cursor);
       dropdown.innerHTML = getSuggestedAccounts(mentionInput)
       .map(
@@ -304,7 +234,7 @@ if (showAccountAutoComplete) {
         dropdown.style.top = y + "px";
         dropdown.style.left = x + "px";
 
-        createMentionDropDownOptions();
+        createMentionDrowDownOptions();
 
         dropdown.classList.add("show");
 
@@ -319,105 +249,12 @@ if (showAccountAutoComplete) {
         mentionToken = null;
         dropdown.classList.remove("show");
     } else if (mentionToken) {
-        createMentionDropDownOptions();
+        createMentionDrowDownOptions();
     }
-});
-}
-
-if (showProposalIdAutoComplete) {
-  let proposalId;
-  let referenceCursorStart;
-  const dropdown = document.getElementById("referencedropdown");
-  const loader = document.createElement('div');
-  loader.className = 'loader';
-  loader.textContent = 'Loading...';
-
-  simplemde.codemirror.on("keydown", () => {
-    if (proposalId && event.key === 'ArrowDown') {
-      dropdown.querySelector('button').focus();
-      event.preventDefault();
-      return false;
-    }
-  });
-
-  simplemde.codemirror.on("keyup", (cm, event) => {
-    const cursor = cm.getCursor();
-    const token = cm.getTokenAt(cursor);
-
-    const createReferenceDropDownOptions = async () => {
-      try {
-        const proposalIdInput = cm.getRange(referenceCursorStart, cursor);
-        dropdown.innerHTML = ''; // Clear previous content
-        dropdown.appendChild(loader); // Show loader
-
-        const suggestedProposals = await getSuggestedProposals(proposalIdInput);
-        dropdown.innerHTML = suggestedProposals
-        .map(
-          (item) =>
-            '<li><button class="dropdown-item cursor-pointer w-100 text-wrap">' + "#" + item?.proposal_id + " " + item.name + '</button></li>'
-        )
-        .join("");
-
-        dropdown.querySelectorAll("li").forEach((li) => {
-          li.addEventListener("click", () => {
-            const selectedText = li.textContent.trim();
-            const startIndex = selectedText.indexOf('#') + 1; 
-            const endIndex = selectedText.indexOf(' ', startIndex);
-            const id = endIndex !== -1 ? selectedText.substring(startIndex, endIndex) : selectedText.substring(startIndex);
-            const link = "https://near.social/devhub.near/widget/app?page=proposal&id=" + id;
-            const adjustedStart = {
-              line: referenceCursorStart.line,
-              ch: referenceCursorStart.ch - 1
-            };
-            simplemde.codemirror.replaceRange("[" + selectedText + "]" + "(" + link + ")", adjustedStart, cursor);
-            proposalId = null;
-            dropdown.classList.remove("show");
-            cm.focus();
-          });
-        });
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        // Handle error: Remove loader
-        dropdown.innerHTML = ''; // Clear previous content
-      } finally {
-        // Remove loader
-        dropdown.removeChild(loader);
-      }
-    }
-
-    // show dropwdown only when there is space before #
-      if (!proposalId && token.string === "#" && cm.getTokenAt({line:cursor.line, ch: cursor.ch - 1}).string == ' ') {
-        proposalId = token;
-        referenceCursorStart = cursor;
-        // Calculate cursor position relative to the iframe's viewport
-        const rect = cm.charCoords(cursor);
-        const x = rect.left;
-        const y = rect.bottom;
-
-        // Create dropdown with options
-        dropdown.style.top = y + "px";
-        dropdown.style.left = x + "px";
-
-        createReferenceDropDownOptions();
-
-        dropdown.classList.add("show");
-
-        // Close dropdown on outside click
-        document.addEventListener("click", function(event) {
-            if (!dropdown.contains(event.target)) {
-              proposalId = null;
-                dropdown.classList.remove("show");
-            }
-        });
-    } else if (proposalId && token.string.match(/[^#a-z0-9.]/)) {
-      proposalId = null;
-      dropdown.classList.remove("show");
-    } else if (proposalId) {
-      createReferenceDropDownOptions();
-  }
 });
 
 }
+
 
 window.addEventListener("message", (event) => {
   if (!isEditorInitialized && event.data !== "") {
@@ -433,9 +270,6 @@ window.addEventListener("message", (event) => {
   }
   if (event.data.profilesData) {
     profilesData = JSON.parse(event.data.profilesData);
-  }
-  if (event.data.query) {
-    query = event.data.query;
   }
 });
 </script>
@@ -454,7 +288,6 @@ return (
       content: props.data?.content ?? "",
       followingData,
       profilesData: JSON.stringify(profilesData),
-      query: query,
     }}
     onMessage={(e) => {
       switch (e.handler) {
