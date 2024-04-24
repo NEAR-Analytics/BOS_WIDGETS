0692f2e308b7ca14977f0968afc7384ee355c367
bos.genadrop.near <bos.genadrop.near>
2024-04-24 20:37:20 +0000

    Update GenaDrop.Profile.Main by bos.genadrop.near at 2024-04-24T20:37:20.049Z

--- bos.genadrop.near/GenaDrop.Profile.Main/source_code.jsx
+++ bos.genadrop.near/GenaDrop.Profile.Main/source_code.jsx
@@ -1,79 +1,65 @@
 State.init({
   selectedTab: props.tab || "nfts",
 });
-
 const accountId = props.accountId ?? context.accountId;
 if (!accountId) {
   return "No account ID";
 }
-
 if (props.tab && props.tab !== state.selectedTab) {
   State.update({
     selectedTab: props.tab,
   });
 }
-
 const profile = props.profile ?? Social.getr(`${accountId}/profile`);
 const accountUrl = `#/bos.genadrop.near/widget/GenaDrop.Profile.Main?accountId=${accountId}`;
-
 const Wrapper = styled.div`
   padding-bottom: 48px;
   max-width: 1200px;
   margin-right: auto;
   margin-left: auto;
 `;
-
 const Main = styled.div`
   display: grid;
   gap: 40px;
   grid-template-columns: 352px minmax(0, 1fr);
   align-items: start;
-
   @media (max-width: 1024px) {
     grid-template-columns: minmax(0, 1fr);
   }
 `;
-
 const BackgroundImage = styled.div`
   height: 240px;
   border-radius: 20px 20px 0 0;
   overflow: hidden;
   margin: 0 -12px;
   background: #eceef0;
-
   img {
     object-fit: cover;
     width: 100%;
     height: 100%;
   }
-
   @media (max-width: 1024px) {
     margin: calc(var(--body-top-padding) * -1) -12px 0;
     border-radius: 0;
   }
-
   @media (max-width: 1024px) {
     height: 100px;
   }
 `;
-
 const SidebarWrapper = styled.div`
   position: relative;
   z-index: 5;
   margin-top: -55px;
-
   @media (max-width: 1024px) {
     margin-top: -40px;
   }
 `;
-
 const Content = styled.div`
   .post {
     padding-left: 0;
     padding-right: 0;
   }
 `;
-
 const Title = styled.h1`
   font-weight: 600;
   font-size: ${(p) => p.size || "25px"};
@@ -82,7 +68,6 @@ const Title = styled.h1`
   margin: ${(p) => (p.margin ? "0 0 24px" : "0")};
   overflow-wrap: anywhere;
 `;
-
 const Tabs = styled.div`
   display: flex;
   height: 48px;
@@ -90,18 +75,15 @@ const Tabs = styled.div`
   margin-bottom: 72px;
   overflow: auto;
   scroll-behavior: smooth;
-
   @media (max-width: 1024px) {
     background: #f8f9fa;
     border-top: 1px solid #eceef0;
     margin: 0 -12px 48px;
-
     > * {
       flex: 1;
     }
   }
 `;
-
 const TabsButton = styled.a`
   display: inline-flex;
   align-items: center;
@@ -117,11 +99,9 @@ const TabsButton = styled.a`
   outline: none;
   text-align: center;
   text-decoration: none !important;
-
   &:hover {
     color: #11181c;
   }
-
   &::after {
     content: "";
     display: ${(p) => (p.selected ? "block" : "none")};
@@ -133,26 +113,21 @@ const TabsButton = styled.a`
     background: #59e692;
   }
 `;
-
 const Bio = styled.div`
   color: #11181c;
   font-size: 14px;
   line-height: 20px;
   margin-bottom: 48px;
-
   > *:last-child {
     margin-bottom: 0 !important;
   }
-
   @media (max-width: 900px) {
     margin-bottom: 48px;
   }
 `;
-
 if (profile === null) {
   return "Loading";
 }
-
 return (
   <Wrapper>
     <BackgroundImage>
@@ -168,7 +143,6 @@ return (
         />
       )}
     </BackgroundImage>
-
     <Main>
       <SidebarWrapper>
         <Widget
@@ -179,7 +153,6 @@ return (
           }}
         />
       </SidebarWrapper>
-
       <Content>
         <Tabs>
           <TabsButton
@@ -194,28 +167,24 @@ return (
           >
             Feed
           </TabsButton>
-
           <TabsButton
             href={`${accountUrl}&tab=apps`}
             selected={state.selectedTab === "apps"}
           >
             Components
           </TabsButton>
-
           <TabsButton
             href={`${accountUrl}&tab=following`}
             selected={state.selectedTab === "following"}
           >
             Following
           </TabsButton>
-
           <TabsButton
             href={`${accountUrl}&tab=followers`}
             selected={state.selectedTab === "followers"}
           >
             Followers
           </TabsButton>
-
           <TabsButton
             href={`${accountUrl}&tab=explorer`}
             selected={state.selectedTab === "explorer"}
@@ -223,7 +192,6 @@ return (
             Explorer
           </TabsButton>
         </Tabs>
-
         {state.selectedTab === "overview" && (
           <>
             {profile.description && (
@@ -231,7 +199,6 @@ return (
                 <Title as="h2" size="19px" margin>
                   About
                 </Title>
-
                 <Bio>
                   <Widget
                     src="near/widget/SocialMarkdown"
@@ -240,33 +207,27 @@ return (
                 </Bio>
               </>
             )}
-
             <Widget
               src="near/widget/Posts.Feed"
               props={{ accounts: [accountId] }}
             />
           </>
         )}
-
         {state.selectedTab === "nfts" && (
           <Widget
             src="bos.genadrop.near/widget/GenaDrop.Profile.MyNFTs"
             props={{ accountId }}
           />
         )}
-
         {state.selectedTab === "apps" && (
           <Widget src="near/widget/ComponentCollection" props={{ accountId }} />
         )}
-
         {state.selectedTab === "followers" && (
           <Widget src="near/widget/FollowersList" props={{ accountId }} />
         )}
-
         {state.selectedTab === "following" && (
           <Widget src="near/widget/FollowingList" props={{ accountId }} />
         )}
-
         {state.selectedTab === "explorer" && (
           <Widget
             src="near/widget/Explorer.Account"
