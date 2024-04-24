c551698524cb63ced0bd0350d8d12c98fd6e417c
hyperfiles.near <hyperfiles.near>
2024-04-24 17:53:33 +0000

    Update explore by hyperfiles.near at 2024-04-24T17:53:33.814Z

---
+++ hyperfiles.near/explore/source_code.jsx
@@ -0,0 +1,8 @@
+return (
+  <div>
+    <Widget src="hyperfiles.near/widget/query.search" />
+    <Widget src="flowscience.near/widget/view" />
+    <Widget src="hack.near/widget/graph.view" props={{}} />
+    <Widget src="efiz.near/widget/every.thing" />
+  </div>
+);
