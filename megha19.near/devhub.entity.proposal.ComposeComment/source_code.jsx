078062693f0ca782b423383af09239842283e583
megha19.near <megha19.near>
2024-04-24 14:17:09 +0000

    Update devhub.entity.proposal.ComposeComment by megha19.near at 2024-04-24T14:17:09.761Z

--- megha19.near/devhub.entity.proposal.ComposeComment/source_code.jsx
+++ megha19.near/devhub.entity.proposal.ComposeComment/source_code.jsx
@@ -172,7 +172,6 @@ return (
           placeholder: "Add your comment here...",
           height: "160",
           embeddCSS: ComposeEmbeddCSS,
-          showProposalIdAutoComplete: true,
         }}
       />
       <div className="d-flex gap-2 align-content-center justify-content-end">
