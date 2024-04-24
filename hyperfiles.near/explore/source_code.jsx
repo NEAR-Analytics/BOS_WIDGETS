a8da67f546d9e20964fec09951d61f3dbc7ecd04
hyperfiles.near <hyperfiles.near>
2024-04-24 21:29:49 +0000

    Update explore by hyperfiles.near at 2024-04-24T21:29:49.153Z

--- hyperfiles.near/explore/source_code.jsx
+++ hyperfiles.near/explore/source_code.jsx
@@ -1,8 +1,32 @@
+function IntegratedSearchAndView() {
+  const [path, setPath] = useState("");
+  const [accounts, setAccounts] = useState([]);
+
+  // Function to handle updates based on predefined queries
+  const handleQueryChange = (query) => {
+    const value = Social.get(query, "final");
+    const accountsFromQuery = Object.keys(value);
+    setAccounts(accountsFromQuery);
+    setPath(query);
+  };
+}
+
 return (
   <div>
-    <Widget src="hyperfiles.near/widget/query.search" />
-    <Widget src="flowscience.near/widget/view" />
-    <Widget src="hack.near/widget/graph.view" props={{}} />
-    <Widget src="efiz.near/widget/every.thing" />
+    <div
+      style={{ display: "flex", flexDirection: "column", marginBottom: "20px" }}
+    >
+      <Widget src="hyperfiles.near/widget/query.search" />
+      <Widget src="flowscience.near/widget/view" />
+      <Widget src="hyperfiles.near/widget/view" path={state.path} />
+    </div>
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ flex: 1 }}>
+        <Widget src="hack.near/widget/graph.view" props={{}} />
+      </div>
+      <div style={{ flex: 1 }}>
+        <Widget src="efiz.near/widget/every.thing" />
+      </div>
+    </div>
   </div>
 );
