75f411d383b0ca4b51ac94b821730bb1c869cd5c
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-04-24 22:05:25 +0000

    Update button by beachsunandrockandroll.near at 2024-04-24T22:05:25.831Z

--- beachsunandrockandroll.near/button/source_code.jsx
+++ beachsunandrockandroll.near/button/source_code.jsx
@@ -1,10 +1,11 @@
+const { Tailwind } = VM.require("beachsunandrockandroll.near/widget/preflight");
 
-const Button = ({ className, variant, size, children, ...props }) => {
-  return (
-      <button className={className} ref={forwardedRef} {...props}>
-        {children}
-      </button>
-  );
-};
+const Button = ({ className, ...props }) => (
+  <Tailwind>
+    <button className={className} ref={forwardedRef} {...props}>
+      {children}
+    </button>
+  </Tailwind>
+);
 
 return { Button };
