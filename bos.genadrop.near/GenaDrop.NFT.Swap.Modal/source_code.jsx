34934438a77dff7eddea4577f8b444703e3e7218
bos.genadrop.near <bos.genadrop.near>
2024-04-24 20:37:20 +0000

    Update GenaDrop.NFT.Swap.Modal by bos.genadrop.near at 2024-04-24T20:37:20.049Z

--- bos.genadrop.near/GenaDrop.NFT.Swap.Modal/source_code.jsx
+++ bos.genadrop.near/GenaDrop.NFT.Swap.Modal/source_code.jsx
@@ -5,7 +5,6 @@ const onConfirm = props.onConfirm;
 const hidden = props.hidden;
 const onClose = props.onClose;
 const showFooter = props.showFooter;
-
 const Modal = styled.div`
   display: ${({ hidden }) => (hidden ? "none" : "flex")};
   position: fixed;
@@ -15,14 +14,12 @@ const Modal = styled.div`
   opacity: 1;
   z-index: 1;
 `;
-
 const ModalBackdrop = styled.div`
   position: absolute;
   inset: 0;
   background-color: rgba(0, 0, 0, 0.5);
   opacity: 0.4;
 `;
-
 const ModalDialog = styled.div`
   padding: 2em;
   z-index: 3;
@@ -31,24 +28,19 @@ const ModalDialog = styled.div`
   width: 60%;
   max-height: 80%;
   box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
-
   overflow-y: auto;
-
   @media (width < 720px) {
     width: 100%;
   }
 `;
-
 const ModalHeader = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-
   border-bottom: 1px solid #d3d3d3;
   padding-bottom: 4px;
 `;
-
 const ModalFooter = styled.div`
   padding-top: 4px;
   display: flex;
@@ -56,7 +48,6 @@ const ModalFooter = styled.div`
   justify-content: space-between;
   align-items: items-center;
 `;
-
 const CloseButton = styled.button`
   display: flex;
   align-items: center;
@@ -67,12 +58,10 @@ const CloseButton = styled.button`
   border: 0;
   color: #344054;
   transition: 300ms;
-
   &:hover {
     background-color: #d3d3d3;
   }
 `;
-
 const ConfirmButton = styled.button`
   padding: 0.7em;
   border-radius: 6px;
@@ -81,12 +70,10 @@ const ConfirmButton = styled.button`
   background-color: #12b76a;
   color: white;
   transition: background-color 0.2s ease-in-out;
-
   &:hover {
     background-color: #0e9f5d;
   }
 `;
-
 const ModalContent = styled.div`
   flex: 1;
   margin-top: 4px;
@@ -94,7 +81,6 @@ const ModalContent = styled.div`
   overflow-y: auto;
   max-height: 50%;
 `;
-
 return (
   <Modal hidden={hidden}>
     <ModalBackdrop />
