4f0a08528e39e7b0f7412d360a5b08db208d1b11
hack.near <hack.near>
2024-04-24 13:22:15 +0000

    Update widget.inline by hack.near at 2024-04-24T13:22:15.187Z

--- hack.near/widget.inline/source_code.jsx
+++ hack.near/widget.inline/source_code.jsx
@@ -1,8 +1,8 @@
-const src = props.src ?? "hack.near/widget/Academy";
-const [accountId, widget, widgetName] = src.split("/");
+const src = props.src ?? "potlock.near/widget/Index";
+const [creatorId, type, thingId] = src.split("/");
 const blockHeight = props.blockHeight;
 
-const accountName = accountId.replace(/\.[^\.]*$/, "");
+const creator = creatorId.replace(/\.[^\.]*$/, "");
 
 const metadata =
   props.metadata ?? Social.getr(`${src}/metadata`, `${blockHeight}`);
@@ -20,17 +20,28 @@ const Card = styled.div`
 
 const displayName = metadata.name || src;
 
-const profile = Social.getr(`${accountId}/profile`);
+const profile = Social.getr(`${creatorId}/profile`);
 
 if (!profile) {
   return "";
 }
 
+const stars = Social.get(
+  `*/graph/star/${creatorId}/${type}/${thingId}`,
+  "final"
+);
+
+if (!stars) {
+  return "";
+}
+
+const accounts = Object.keys(stars);
+
 return (
   <div className="card position-relative">
     <a
       style={{ color: "#000", textDecoration: "none" }}
-      href={`https://${accountName}.bos.gg`}
+      href={`https://${creator}.bos.gg`}
     >
       <div
         style={{
@@ -52,12 +63,15 @@ return (
             <Widget src="hack.near/widget/profile.widget" props={{ src }} />
           </div>
           <div className="m-1">
-            {context.accountId && (
-              <Widget
-                src="hack.near/widget/star.button"
-                props={{ path: src }}
-              />
-            )}
+            <span className="me-2">{accounts.length} stars</span>
+            <span>
+              {context.accountId && (
+                <Widget
+                  src="hack.near/widget/star.button"
+                  props={{ path: src }}
+                />
+              )}
+            </span>
           </div>
         </div>
       </div>
