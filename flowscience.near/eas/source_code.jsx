ff95735725172aba82990330059ff4bc13abda06
flowscience.near <flowscience.near>
2024-04-25 01:57:14 +0000

    Update eas by flowscience.near at 2024-04-25T01:57:14.802Z

--- flowscience.near/eas/source_code.jsx
+++ flowscience.near/eas/source_code.jsx
@@ -1,5 +1,17 @@
 // Example attestation UID: 0xff5dc0cdc3de27dfe6a4352c596c0f97b1f99c51a67bbae142ce315e34969dcd
 
+const TabContent = styled.div`
+  margin-top: 1rem;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const [activeTab, setActiveTab] = useState("query");
+const [defaultView, setDefaultView] = useState("QUERY") || props.defaultView;
+
 // Need to finish getAttestation refactor to imported component
 //const { GetAttestation } = VM.require("flowscience.near/widget/getAttestation");
 //const { attest } = VM.require("flowscience.near/widget/easAttest");
@@ -7,10 +19,6 @@
 //const { register } = VM.require("flowscience.near/widget/easRegister");
 //const { revoke } = VM.require("flowscience.near/widget/easRevoke");
 //const { timestamp } = VM.require("flowscience.near/widget/easTimestamp");
-
-const { easRenderAttestation } = VM.require(
-  "flowscience.near/widget/easRenderAttestation"
-);
 
 const user = Ethers.send("eth_requestAccounts", [])[0];
 
@@ -26,9 +34,6 @@ const chain = Ethers.provider()
 */
 }
 
-const abi = fetch(
-  "https://raw.githubusercontent.com/ethereum-attestation-service/eas-contracts/master/deployments/optimism/EAS.json"
-);
 const provider = new ethers.providers.JsonRpcProvider(
   "https://optimism.drpc.org"
 );
@@ -36,80 +41,53 @@ const signer = provider.getSigner(user);
 // console.log("chain:", chain);
 // console.log("signer:", signer);
 
-const contractAddress = "0x4200000000000000000000000000000000000021";
-const parsedAbi = JSON.parse(abi.body);
-const contract = new ethers.Contract(contractAddress, parsedAbi.abi, signer);
-//console.log(contract);
-const [attestation, setAttestation] = useState(null);
-const [error, setError] = useState("");
-const [uid, setUid] = useState("");
-
-function getAttestation() {
-  if (typeof uid !== "string" || uid.trim() === "") {
-    console.error("UID must be a non-empty string.");
-    setError("UID must be provided.");
-    return;
-  }
-
-  contract
-    .getAttestation(uid)
-    .then((result) => {
-      const [
-        uid,
-        schema,
-        time,
-        expirationTime,
-        revocationTime,
-        refUID,
-        recipient,
-        attester,
-        revocable,
-        data,
-      ] = result;
-      const mappedAttestation = {
-        uid,
-        schema,
-        time: time.toNumber(),
-        expirationTime: expirationTime.toNumber(),
-        revocationTime: revocationTime.toNumber(),
-        refUID,
-        recipient,
-        attester,
-        revocable,
-        data,
-      };
-      setAttestation(mappedAttestation);
-      setError("");
-    })
-    .catch((error) => {
-      console.error("error fetching attestation:", error);
-      setError("Failed to retrieve data. Please try with a verified uid.");
-    });
-}
-
 return (
-  <>
+  <div className="p-3 border bg-light">
     <div className="m-2">
-      <h3>Get Attestation</h3>
-      <input
-        type="text"
-        placeholder="input UID"
-        value={uid}
-        onChange={(e) => setUid(e.target.value)}
-      />
+      <h1>EAS on BOS</h1>
+      <hr />
     </div>
-    <div className="m-2">
-      <button className="btn btn-primary m-1" onClick={getAttestation}>
-        Get Attestation
-      </button>
-      <p className="m-1">{error}</p>
-    </div>
-    <div>
-      {attestation && (
-        <div className="App">
-          <easRenderAttestation attestation={attestation} />
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          className={`nav-link ${activeTab === "query" ? "active" : ""}`}
+          onClick={() => setActiveTab("query")}
+        >
+          Query
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          className={`nav-link ${activeTab === "Create" ? "active" : ""}`}
+          onClick={() => setActiveTab("Create")}
+        >
+          Create
+        </a>
+      </li>
+    </ul>
+    <TabContent>
+      {activeTab === "query" && (
+        <div>
+          <div className="m-2">
+            <Widget src="flowscience.near/widget/getAttestation" />
+            <hr />
+          </div>
+
+          <div className="m-2">
+            <Widget src="flowscience.near/widget/getSchema" />
+          </div>
         </div>
       )}
-    </div>
-  </>
+    </TabContent>
+    <TabContent>
+      {activeTab === "Create" && (
+        <div>
+          <div className="m-2">
+            <p>Coming soon! Create Attestations & Schemas!</p>
+            <hr />
+          </div>
+        </div>
+      )}
+    </TabContent>
+  </div>
 );
