8a47128efd809f5cab4d31418649bb99b291c40d
thomasguntenaar.near <thomasguntenaar.near>
2024-04-25 18:51:51 +0000

    Update devhub.page.addon by thomasguntenaar.near at 2024-04-25T18:51:51.771Z

--- thomasguntenaar.near/devhub.page.addon/source_code.jsx
+++ thomasguntenaar.near/devhub.page.addon/source_code.jsx
@@ -19,7 +19,7 @@ const Content = styled.div`
 
 const SettingsButton = styled.button`
   position: absolute;
-  top: -100px;
+  top: 10px;
   right: 10px;
 
   background-color: #fff;
@@ -57,7 +57,7 @@ const CenteredMessage = styled.div`
   height: ${(p) => p.height ?? "100%"};
 `;
 
-const { addon, permissions, handle, addonView, setAddonView } = props;
+const { addon, permissions, handle } = props;
 
 const { getAllAddons, setCommunityAddon } = VM.require(
   "thomasguntenaar.near/widget/core.adapter.devhub-contract"
@@ -67,15 +67,25 @@ if (!getAllAddons || !setCommunityAddon) {
   return <p>Loading modules...</p>;
 }
 
+// TODO this needs to be added to the contract when it is ready!
+// AddOn
+const blogv2 = {
+  configurator_widget:
+    "devhub.near/widget/devhub.entity.addon.blogv2.Configurator",
+  description: "Create a blog for your community",
+  icon: "bi bi-substack",
+  id: "blogv2",
+  title: "BlogV2",
+  view_widget: "devhub.near/widget/devhub.entity.addon.blogv2.Viewer",
+};
+
 const availableAddons = getAllAddons();
 
 let addonMatch = null; // If availableAddons is not an array, set addonMatch to null
-if (
-  Array.isArray(availableAddons) &&
-  availableAddons !== null &&
-  availableAddons !== undefined
-) {
-  addonMatch = availableAddons.find((it) => it.id === addon.addon_id);
+if (Array.isArray(availableAddons)) {
+  addonMatch = ([blogv2, ...availableAddons] ?? []).find(
+    (it) => it.id === addon.addon_id
+  );
 }
 
 if (!addonMatch) {
@@ -93,6 +103,10 @@ const ButtonRow = styled.div`
   justify-content: space-between;
 `;
 
+// Change 'configure' to 'viewer' "configure"); //
+const [view, setView] = useState("configure"); //props.view || "viewer");
+console.log(props.view);
+
 if ("thomasguntenaar.near" !== "devhub.near") {
   addonMatch.configurator_widget = addonMatch.configurator_widget.replace(
     "devhub.near/",
@@ -108,30 +122,17 @@ return (
   <Container>
     {permissions.can_configure && addonMatch.configurator_widget !== "" && (
       <SettingsButton
-        onClick={() =>
-          setAddonView(addonView === "configure" ? "viewer" : "configure")
-        }
-        aria-label={
-          addonView === "configure"
-            ? "Close configuration"
-            : "Open configuration"
-        }
+        onClick={() => setView(view === "configure" ? "view" : "configure")}
       >
-        {addonView === "configure" ? (
-          <span
-            className="bi bi-x"
-            data-testid="configure-addon-button-x"
-          ></span>
+        {view === "configure" ? (
+          <span className="bi bi-x"></span>
         ) : (
-          <span
-            className="bi bi-gear"
-            data-testid="configure-addon-button"
-          ></span>
+          <span className="bi bi-gear"></span>
         )}
       </SettingsButton>
     )}
     <Content>
-      {addonView === "configure" ? (
+      {view === "configure" ? (
         <Widget
           src={addonMatch.configurator_widget}
           props={{
@@ -148,7 +149,8 @@ return (
             },
             handle, // this is temporary prop drilling until kanban and github are migrated
             permissions,
-            communityAddonId: addon.id,
+            // NOTE not the addon_id (same for every blog)
+            communityAddonId: addonMatch.id,
           }}
         />
       ) : (
@@ -160,8 +162,7 @@ return (
             handle,
             permissions,
             transactionHashes: props.transactionHashes,
-            communityAddonId: addon.id,
-            setAddonView,
+            communityAddonId: addonMatch.id,
           }}
         />
       )}
