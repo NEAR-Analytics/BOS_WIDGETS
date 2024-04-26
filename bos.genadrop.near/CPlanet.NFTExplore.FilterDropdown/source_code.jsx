629ed96f61a6a11119411e4e5c4262dff6795bdc
bos.genadrop.near <bos.genadrop.near>
2024-04-26 10:06:12 +0000

    Update CPlanet.NFTExplore.FilterDropdown by bos.genadrop.near at 2024-04-26T10:06:12.377Z

--- bos.genadrop.near/CPlanet.NFTExplore.FilterDropdown/source_code.jsx
+++ bos.genadrop.near/CPlanet.NFTExplore.FilterDropdown/source_code.jsx
@@ -8,9 +8,7 @@ const defaultProps = [
     name: "Multichain NFTS",
   },
 ];
-
 const propsChains = props.selected ?? defaultProps;
-
 State.init({
   selectIsOpen: false,
   selectedChain: "0",
@@ -20,7 +18,6 @@ const handleSelectClick = () => {
     selectIsOpen: !state.selectIsOpen,
   });
 };
-
 const handleOutsideClick = (e) => {
   e.preventDefault();
   if (!!state.selectIsOpen) {
@@ -29,12 +26,10 @@ const handleOutsideClick = (e) => {
     });
   }
 };
-
 const SelectTag = styled.select`
   height: fit-content;
   width: 300px;
 `;
-
 const ChainIcon = styled.option`
   display: flex;
   height: 130px;
@@ -45,63 +40,55 @@ const ChainIcon = styled.option`
     object-fit: contain;
   }
 `;
-
 const SelectReplicaContainer = styled.div`
-position: relative;
-display: inline-block;
-z-index: 999;
-
-.select-replica__select {
   position: relative;
-  cursor: pointer;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 10px;
-  min-width: 150px;
-  p {
-    margin-bottom: 0 !important;
+  display: inline-block;
+  z-index: 999;
+  .select-replica__select {
+    position: relative;
+    cursor: pointer;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+    min-width: 150px;
+    p {
+      margin-bottom: 0 !important;
+    }
+    span {
+      padding-left: 5px;
+    }
   }
-  span {
-    padding-left: 5px;
+  .select-replica__selected {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
-}
-
-.select-replica__selected {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.select-replica__options {
-  position: absolute;
-  top: 47px;
-  left: 0;
-  width: 100%;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 0 0 5px 5px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  display: none;
-}
-
-.select-replica__options.open {
-  display: block;
-}
-
-.select-replica__option {
-  padding: 10px;
-  cursor: pointer;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #f0f0f0;
+  .select-replica__options {
+    position: absolute;
+    top: 47px;
+    left: 0;
+    width: 100%;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 0 0 5px 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    display: none;
   }
-
-  &.selected {
-    background-color: #f0f0f0;
+  .select-replica__options.open {
+    display: block;
   }
-}
+  .select-replica__option {
+    padding: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    &:hover {
+      background-color: #f0f0f0;
+    }
+    &.selected {
+      background-color: #f0f0f0;
+    }
+  }
 `;
 State.update({ chains: propsChains || chains });
 const handleChainChange = (chain_id) => {
@@ -110,7 +97,6 @@ const handleChainChange = (chain_id) => {
     selectedChain: chain_id,
   });
 };
-
 return (
   <>
     <SelectReplicaContainer>
