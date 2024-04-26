851a589a0bae4aba04c786e1907881dc7a069c0c
bos.genadrop.near <bos.genadrop.near>
2024-04-26 10:06:12 +0000

    Update CPlanet.NFTCard.FeaturedNFT by bos.genadrop.near at 2024-04-26T10:06:12.377Z

--- bos.genadrop.near/CPlanet.NFTCard.FeaturedNFT/source_code.jsx
+++ bos.genadrop.near/CPlanet.NFTCard.FeaturedNFT/source_code.jsx
@@ -1,88 +1,82 @@
 const Root = styled.a`
-    width: 328px;
-height: 200px;
-flex-shrink: 0;
-border-radius: 8px;
-display: flex;
-background-color: black;
-text-decoration: none;
-flex-direction: column;
-justify-content: space-between;
-padding: 16px;
- :hover {
+  width: 328px;
+  height: 200px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  display: flex;
+  background-color: black;
   text-decoration: none;
- }
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 16px;
+  :hover {
+    text-decoration: none;
+  }
 `;
-
 const Container = styled.div`
   display: flex;
   gap: 20px;
 `;
-
 const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    color: white;
-    padding: 6px;
-    background: rgba(255, 255, 255, 0.2);
-    border-radius: 16px;
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(5px);
-    -webkit-backdrop-filter: blur(5px);
-    border: 1px solid rgba(255, 255, 255, 0.3);
-    h1 {
-        color: black;
-        font-family: Helvetica Neue;
-        font-size: 24px;
-        font-style: normal;
-        font-weight: 700;
-        line-height: normal;
-        text-transform: uppercase;
-    }
-    span {
-      color: black;
-    }
+  display: flex;
+  flex-direction: column;
+  color: white;
+  padding: 6px;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  h1 {
+    color: black;
+    font-family: Helvetica Neue;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    text-transform: uppercase;
+  }
+  span {
+    color: black;
+  }
 `;
-
 const ImageContainer = styled.div`
-    img {
-      width: 48px;
-      height: 48px;
-      border-radius: 50%;
-      object-fit: cover;
-      border: 1px solid white;
-    }
+  img {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    object-fit: cover;
+    border: 1px solid white;
+  }
 `;
-
 const Tag = styled.div`
-    color: #FFF;
-font-family: Helvetica Neue;
-font-size: 24px;
-font-style: normal;
-font-weight: 700;
-line-height: normal;
-border-radius: 50px;
-background: #F8F8F8;
-width: max-content;
-color: #000;
-text-align: center;
-font-family: Helvetica Neue;
-font-size: 10px;
-font-style: normal;
-font-weight: 500;
-line-height: 150%; /* 15px */
-padding: 3px 10px;
+  color: #fff;
+  font-family: Helvetica Neue;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  border-radius: 50px;
+  background: #f8f8f8;
+  width: max-content;
+  color: #000;
+  text-align: center;
+  font-family: Helvetica Neue;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%; /* 15px */
+  padding: 3px 10px;
 `;
 const Tags = styled.div`
-    display:flex;
-    gap: 7px;
-    margin-left: 10px;
+  display: flex;
+  gap: 7px;
+  margin-left: 10px;
 `;
-
 initState({
   featuredNFTs: [],
 });
-
 const fetchStoreFrontData = () => {
   const response2 = fetch("https://graph.mintbase.xyz/mainnet", {
     method: "POST",
@@ -120,9 +114,7 @@ const fetchStoreFrontData = () => {
     });
   }
 };
-
 fetchStoreFrontData();
-
 return (
   <Container>
     {state.featuredNFTs.length &&
