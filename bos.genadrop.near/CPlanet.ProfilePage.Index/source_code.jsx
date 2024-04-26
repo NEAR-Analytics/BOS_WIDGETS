cb3423844a98a0751172e221e52ebd7c011bb5f3
bos.genadrop.near <bos.genadrop.near>
2024-04-26 10:06:12 +0000

    Update CPlanet.ProfilePage.Index by bos.genadrop.near at 2024-04-26T10:06:12.377Z

--- bos.genadrop.near/CPlanet.ProfilePage.Index/source_code.jsx
+++ bos.genadrop.near/CPlanet.ProfilePage.Index/source_code.jsx
@@ -2,14 +2,11 @@ const accountId = props.accountId ?? context.accountId;
 if (!accountId) {
   return "No account ID";
 }
-
 const profile = props.profile ?? Social.getr(`${accountId}/profile`);
 const fast = !props.profile;
-
 if (profile === null) {
   return "Loading";
 }
-
 const Wrapper = styled.div`
   margin-top: calc(-1 * var(--body-top-padding, 0));
   * {
@@ -17,7 +14,6 @@ const Wrapper = styled.div`
     line-height: normal;
   }
 `;
-
 return (
   <Wrapper>
     <Widget
