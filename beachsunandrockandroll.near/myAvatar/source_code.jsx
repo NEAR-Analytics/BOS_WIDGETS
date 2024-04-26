8664ed5ca60b410accb70c233a3863a853f4bc21
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-04-26 10:33:57 +0000

    Update myAvatar by beachsunandrockandroll.near at 2024-04-26T10:33:57.235Z

--- beachsunandrockandroll.near/myAvatar/source_code.jsx
+++ beachsunandrockandroll.near/myAvatar/source_code.jsx
@@ -1,24 +1,19 @@
 const { Tailwind } = VM.require("beachsunandrockandroll.near/widget/preflight");
 
-const { AvatarRoot, AvatarImage, AvatarFallback } = VM.require(
-  "beachsunandrockandroll.near/widget/avatar"
+const { AspectRatioRoot } = VM.require(
+  "beachsunandrockandroll.near/widget/aspectRatio"
 );
 
-if (Tailwind === undefined) return "";
+if (Tailwind === undefined) return <></>;
 
 return (
   <Tailwind>
-    <div className="flex mx-auto w-max h-screen pt-10">
-      <AvatarRoot className="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full">
-        <AvatarImage
-          className="aspect-square h-full w-full"
-          src="https://github.com/shadcn.png"
-          alt="@shadcn"
-        />
-        <AvatarFallback className="flex h-10 w-10 items-center justify-center rounded-full bg-muted">
-          PD
-        </AvatarFallback>
-      </AvatarRoot>
-    </div>
+    <AspectRatioRoot ratio={16 / 9} className="bg-muted">
+      <img
+        src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+        alt="Photo by Drew Beamer"
+        className="rounded-md object-cover"
+      />
+    </AspectRatioRoot>
   </Tailwind>
 );
