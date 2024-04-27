24b6ca644dfb9aa399ec17298b8552e5776bae7d
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-04-27 11:45:37 +0000

    Update card by beachsunandrockandroll.near at 2024-04-27T11:45:37.476Z

--- beachsunandrockandroll.near/card/source_code.jsx
+++ beachsunandrockandroll.near/card/source_code.jsx
@@ -1,48 +1,80 @@
 const { Tailwind } = VM.require("beachsunandrockandroll.near/widget/preflight");
 
+const cardClassname = "rounded-xl border bg-card text-card-foreground shadow";
+
 const Card = ({ className, children, ...props }) => (
   <Tailwind>
-    <div ref="forwardedRef" className={className} {...props}>
+    <div ref="forwardedRef" className={className ?? cardClassname} {...props}>
       {children}
     </div>
   </Tailwind>
 );
+
+const cardHeaderClassname = "flex flex-col space-y-1.5 p-6";
 
 const CardHeader = ({ className, children, ...props }) => (
   <Tailwind>
-    <div ref="forwardedRef" className={className} {...props}>
+    <div
+      ref="forwardedRef"
+      className={className ?? cardHeaderClassname}
+      {...props}
+    >
       {children}
     </div>
   </Tailwind>
 );
 
+const cardTitleClassname = "font-semibold leading-none tracking-tight";
+
 const CardTitle = ({ className, children, ...props }) => (
   <Tailwind>
-    <h3 ref="forwardedRef" className={className} {...props}>
+    <h3
+      ref="forwardedRef"
+      className={className ?? cardTitleClassname}
+      {...props}
+    >
       {children}
     </h3>
   </Tailwind>
 );
 
+const cardDescriptionClassname = "text-sm text-muted-foreground";
+
 const CardDescription = ({ className, children, ...props }) => (
   <Tailwind>
-    <p ref="forwardedRef" className={className} {...props}>
+    <p
+      ref="forwardedRef"
+      className={className ?? cardDescriptionClassname}
+      {...props}
+    >
       {children}
     </p>
   </Tailwind>
 );
 
+const cardContentClassname = "p-6 pt-0";
+
 const CardContent = ({ className, children, ...props }) => (
   <Tailwind>
-    <div ref="forwardedRef" className={className} {...props}>
+    <div
+      ref="forwardedRef"
+      className={className ?? cardContentClassname}
+      {...props}
+    >
       {children}
     </div>
   </Tailwind>
 );
 
+const cardFooterClassname = "flex items-center p-6 pt-0";
+
 const CardFooter = ({ className, children, ...props }) => (
   <Tailwind>
-    <div ref="forwardedRef" className={className} {...props}>
+    <div
+      ref="forwardedRef"
+      className={className ?? cardFooterClassname}
+      {...props}
+    >
       {children}
     </div>
   </Tailwind>
@@ -55,4 +87,10 @@ return {
   CardTitle,
   CardDescription,
   CardContent,
+  cardClassname,
+  cardHeaderClassname,
+  cardTitleClassname,
+  cardDescriptionClassname,
+  cardContentClassname,
+  cardFooterClassname,
 };
