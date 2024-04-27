a3642b752422cb35379ef94b14c0bfcf2bb9a719
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-04-27 11:33:56 +0000

    Update avatar by beachsunandrockandroll.near at 2024-04-27T11:33:56.642Z

--- beachsunandrockandroll.near/avatar/source_code.jsx
+++ beachsunandrockandroll.near/avatar/source_code.jsx
@@ -1,25 +1,43 @@
 const { Tailwind } = VM.require("beachsunandrockandroll.near/widget/preflight");
 
+const avatarClassname =
+  "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full";
+
 const AvatarRoot = ({ className, children, ...props }) => (
   <Tailwind>
-    <Avatar.Root className={className} ref="forwardedRef" {...props}>
+    <Avatar.Root className={className ?? avatarClassname} {...props}>
       {children}
     </Avatar.Root>
   </Tailwind>
 );
 
-const AvatarImage = ({ className, ...props }) => (
+const avatarImageClassname = "aspect-square h-full w-full";
+
+const AvatarImage = ({ className, children, ...props }) => (
   <Tailwind>
-    <Avatar.Image className={className} ref="forwardedRef" {...props} />
+    <Avatar.Image className={className ?? avatarImageClassname} {...props} />
   </Tailwind>
 );
 
+const avatarFallbackClassname =
+  "flex h-full w-full items-center justify-center rounded-full bg-muted";
+
 const AvatarFallback = ({ className, children, ...props }) => (
   <Tailwind>
-    <Avatar.Fallback className={className} ref="forwardedRef" {...props}>
+    <Avatar.Fallback
+      className={className ?? avatarFallbackClassname}
+      {...props}
+    >
       {children}
     </Avatar.Fallback>
   </Tailwind>
 );
 
-return { AvatarRoot, AvatarImage, AvatarFallback };
+return {
+  Avatar: AvatarRoot,
+  AvatarImage,
+  AvatarFallback,
+  avatarClassname,
+  avatarImageClassname,
+  avatarFallbackClassname,
+};
