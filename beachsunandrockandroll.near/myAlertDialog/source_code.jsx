729a2a5b7908e74152024e1553917e053dd7a9ca
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-04-27 16:01:17 +0000

    Update myAlertDialog by beachsunandrockandroll.near at 2024-04-27T16:01:17.490Z

--- beachsunandrockandroll.near/myAlertDialog/source_code.jsx
+++ beachsunandrockandroll.near/myAlertDialog/source_code.jsx
@@ -1,71 +1,111 @@
 const { Tailwind } = VM.require("beachsunandrockandroll.near/widget/preflight");
-const { Button } = VM.require("beachsunandrockandroll.near/widget/button");
 
 const {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-  hoverCardContentClassname,
-} = VM.require("beachsunandrockandroll.near/widget/hoverCard");
-const { Avatar, AvatarImage, AvatarFallback } = VM.require(
-  "beachsunandrockandroll.near/widget/avatar"
-);
-
-State.init({
-  cnButton: "",
-  cnHoverCardContent: "",
-});
-
-if (["", "loaded"].includes(state.cnButton))
-  return (
-    <Widget
-      src="beachsunandrockandroll.near/widget/buttonIframe"
-      props={{
-        output: (cnButton) => State.update({ cnButton }),
-        variant: "outline",
-      }}
-    />
-  );
-
-if (["", "loaded"].includes(state.cnHoverCardContent))
-  return (
-    <Widget
-      src="beachsunandrockandroll.near/widget/cnIframe"
-      props={{
-        output: (cnHoverCardContent) => State.update({ cnHoverCardContent }),
-        className: `${hoverCardContentClassname} w-80`,
-      }}
-    />
-  );
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} = VM.require("beachsunandrockandroll.near/widget/menubar");
 
 return (
   <Tailwind>
     <div className="flex mx-auto w-max pt-10">
-      <HoverCard>
-        <HoverCardTrigger>
-          <Button variant="link">@nextjs</Button>
-        </HoverCardTrigger>
-        <HoverCardContent className={state.cnHoverCardContent}>
-          <div className="flex justify-between space-x-4">
-            <Avatar>
-              <AvatarImage src="https://github.com/vercel.png" />
-              <AvatarFallback>VC</AvatarFallback>
-            </Avatar>
-            <div className="space-y-1">
-              <h4 className="text-sm font-semibold">@nextjs</h4>
-              <p className="text-sm">
-                The React Framework - created and maintained by @vercel.
-              </p>
-              <div className="flex items-center pt-2">
-                <i class="bi bi-calendar3 mr-2 h-4 w-4 opacity-70"></i>{" "}
-                <span className="text-xs text-muted-foreground">
-                  Joined December 2021
-                </span>
-              </div>
-            </div>
-          </div>
-        </HoverCardContent>
-      </HoverCard>
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>
+              New Window <MenubarShortcut>⌘N</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem disabled>New Incognito Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarSub>
+              <MenubarSubTrigger>Share</MenubarSubTrigger>
+              <MenubarSubContent>
+                <MenubarItem>Email link</MenubarItem>
+                <MenubarItem>Messages</MenubarItem>
+                <MenubarItem>Notes</MenubarItem>
+              </MenubarSubContent>
+            </MenubarSub>
+            <MenubarSeparator />
+            <MenubarItem>
+              Print... <MenubarShortcut>⌘P</MenubarShortcut>
+            </MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>Edit</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>
+              Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+            </MenubarItem>
+            <MenubarSeparator />
+            <MenubarSub>
+              <MenubarSubTrigger>Find</MenubarSubTrigger>
+              <MenubarSubContent>
+                <MenubarItem>Search the web</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Find...</MenubarItem>
+                <MenubarItem>Find Next</MenubarItem>
+                <MenubarItem>Find Previous</MenubarItem>
+              </MenubarSubContent>
+            </MenubarSub>
+            <MenubarSeparator />
+            <MenubarItem>Cut</MenubarItem>
+            <MenubarItem>Copy</MenubarItem>
+            <MenubarItem>Paste</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>View</MenubarTrigger>
+          <MenubarContent>
+            <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
+            <MenubarCheckboxItem checked>
+              Always Show Full URLs
+            </MenubarCheckboxItem>
+            <MenubarSeparator />
+            <MenubarItem inset>
+              Reload <MenubarShortcut>⌘R</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem disabled inset>
+              Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+            </MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem inset>Toggle Fullscreen</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem inset>Hide Sidebar</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>Profiles</MenubarTrigger>
+          <MenubarContent>
+            <MenubarRadioGroup value="benoit">
+              <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
+              <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
+              <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
+            </MenubarRadioGroup>
+            <MenubarSeparator />
+            <MenubarItem inset>Edit...</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem inset>Add Profile...</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
     </div>
   </Tailwind>
 );
