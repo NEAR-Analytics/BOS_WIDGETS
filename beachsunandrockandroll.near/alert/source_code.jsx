062b50f87ecdcb081eddb4f5e4b240f05a1c5076
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-04-27 10:46:23 +0000

    Update alert by beachsunandrockandroll.near at 2024-04-27T10:46:23.621Z

--- beachsunandrockandroll.near/alert/source_code.jsx
+++ beachsunandrockandroll.near/alert/source_code.jsx
@@ -8,20 +8,38 @@ const Alert = ({ className, children, variant, ...props }) => (
   </Tailwind>
 );
 
+const alertTitleClassname = "mb-1 font-medium leading-none tracking-tight";
+
 const AlertTitle = ({ className, children, ...props }) => (
   <Tailwind>
-    <h5 ref="forwardedRef" className={className} {...props}>
+    <h5
+      ref="forwardedRef"
+      className={className ?? alertTitleClassname}
+      {...props}
+    >
       {children}
     </h5>
   </Tailwind>
 );
 
+const alertDescriptionClassname = "text-sm [&_p]:leading-relaxed";
+
 const AlertDescription = ({ className, children, ...props }) => (
   <Tailwind>
-    <div ref="forwardedRef" className={className} {...props}>
+    <div
+      ref="forwardedRef"
+      className={className ?? alertDescriptionClassname}
+      {...props}
+    >
       {children}
     </div>
   </Tailwind>
 );
 
-return { Alert, AlertTitle, AlertDescription };
+return {
+  Alert,
+  AlertTitle,
+  AlertDescription,
+  alertTitleClassname,
+  alertDescriptionClassname,
+};
