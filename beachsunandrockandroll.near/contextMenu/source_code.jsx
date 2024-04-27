bf1d6dc75c02761f99436f8adf4affd4de48472b
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-04-27 12:17:32 +0000

    Update contextMenu by beachsunandrockandroll.near at 2024-04-27T12:17:32.311Z

--- beachsunandrockandroll.near/contextMenu/source_code.jsx
+++ beachsunandrockandroll.near/contextMenu/source_code.jsx
@@ -30,11 +30,12 @@ const ContextMenuRadioGroup = ({ children, ...props }) => (
   </Tailwind>
 );
 
+const contextMenuSubTriggerClassname =
+  "flex cursor-default select-none items-center rounded-sm py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground";
+
 const ContextMenuSubTrigger = ({ className, inset, children, ...props }) => {
-  let cls =
-    inset != undefined && inset
-      ? `${className} pr-2 pl-8`
-      : `${className} px-2`;
+  let cls = className ?? contextMenuSubTriggerClassname;
+  cls = inset != undefined && inset ? `${cls} pr-2 pl-8` : `${cls} px-2`;
 
   return (
     <Tailwind>
@@ -46,27 +47,42 @@ const ContextMenuSubTrigger = ({ className, inset, children, ...props }) => {
   );
 };
 
+const contextMenuSubContentClassname =
+  "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2";
+
 const ContextMenuSubContent = ({ className, children, ...props }) => (
   <Tailwind>
-    <ContextMenu.SubContent ref="forwardedRef" className={className} {...props}>
+    <ContextMenu.SubContent
+      ref="forwardedRef"
+      className={className ?? contextMenuSubContentClassname}
+      {...props}
+    >
       {children}
     </ContextMenu.SubContent>
   </Tailwind>
 );
 
+const contextMenuContentClassname =
+  "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2";
+
 const ContextMenuContent = ({ className, children, ...props }) => (
   <Tailwind>
-    <ContextMenu.Content ref="forwardedRef" className={className} {...props}>
+    <ContextMenu.Content
+      ref="forwardedRef"
+      className={className ?? contextMenuContentClassname}
+      {...props}
+    >
       {children}
     </ContextMenu.Content>
   </Tailwind>
 );
 
+const contextMenuItemClassname =
+  "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50";
+
 const ContextMenuItem = ({ className, inset, children, ...props }) => {
-  let cls =
-    inset != undefined && inset
-      ? `${className} pr-2 pl-8`
-      : `${className} px-2`;
+  let cls = className ?? contextMenuItemClassname;
+  cls = inset != undefined && inset ? `${cls} pr-2 pl-8` : `${cls} px-2`;
 
   return (
     <Tailwind>
@@ -77,6 +93,9 @@ const ContextMenuItem = ({ className, inset, children, ...props }) => {
   );
 };
 
+const contextMenuCheckboxItemClassname =
+  "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50";
+
 const ContextMenuCheckboxItem = ({
   className,
   children,
@@ -86,7 +105,7 @@ const ContextMenuCheckboxItem = ({
   <Tailwind>
     <ContextMenu.CheckboxItem
       ref="forwardedRef"
-      className={className}
+      className={className ?? contextMenuCheckboxItemClassname}
       checked={checked}
       {...props}
     >
@@ -100,9 +119,16 @@ const ContextMenuCheckboxItem = ({
   </Tailwind>
 );
 
+const contextMenuRadioItemClassname =
+  "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50";
+
 const ContextMenuRadioItem = ({ className, children, ...props }) => (
   <Tailwind>
-    <ContextMenu.RadioItem ref="forwardedRef" className={className} {...props}>
+    <ContextMenu.RadioItem
+      ref="forwardedRef"
+      className={className ?? contextMenuRadioItemClassname}
+      {...props}
+    >
       <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
         <ContextMenu.ItemIndicator>
           <i class="bi bi-dot h-4 w-4 fill-current"></i>
@@ -113,11 +139,12 @@ const ContextMenuRadioItem = ({ className, children, ...props }) => (
   </Tailwind>
 );
 
+const contextMenuLabelClassname =
+  "px-2 py-1.5 text-sm font-semibold text-foreground";
+
 const ContextMenuLabel = ({ className, inset, children, ...props }) => {
-  let cls =
-    inset != undefined && inset
-      ? `${className} pr-2 pl-8`
-      : `${className} px-2`;
+  let cls = className ?? contextMenuLabelClassname;
+  cls = inset != undefined && inset ? `${cls} pr-2 pl-8` : `${cls} px-2`;
 
   return (
     <Tailwind>
@@ -128,17 +155,26 @@ const ContextMenuLabel = ({ className, inset, children, ...props }) => {
   );
 };
 
+const contextMenuSeparatorClassname = "-mx-1 my-1 h-px bg-border";
+
 const ContextMenuSeparator = ({ className, children, ...props }) => (
   <Tailwind>
-    <ContextMenu.Separator ref="forwardedRef" className={className} {...props}>
+    <ContextMenu.Separator
+      ref="forwardedRef"
+      className={className ?? contextMenuSeparatorClassname}
+      {...props}
+    >
       {children}
     </ContextMenu.Separator>
   </Tailwind>
 );
 
+const contextMenuShortcutClassname =
+  "ml-auto text-xs tracking-widest text-muted-foreground";
+
 const ContextMenuShortcut = ({ className, children, ...props }) => (
   <Tailwind>
-    <span className={className} {...props}>
+    <span className={className ?? contextMenuShortcutClassname} {...props}>
       {children}
     </span>
   </Tailwind>
@@ -159,4 +195,13 @@ return {
   ContextMenuSubContent,
   ContextMenuSubTrigger,
   ContextMenuRadioGroup,
+  contextMenuSubTriggerClassname,
+  contextMenuSubContentClassname,
+  contextMenuContentClassname,
+  contextMenuItemClassname,
+  contextMenuCheckboxItemClassname,
+  contextMenuRadioItemClassname,
+  contextMenuLabelClassname,
+  contextMenuSeparatorClassname,
+  contextMenuShortcutClassname,
 };
