67572e1633f5ccc579d57aee2c9a29ce582cde8c
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-04-27 09:09:49 +0000

    Update checkbox by beachsunandrockandroll.near at 2024-04-27T09:09:49.474Z

--- beachsunandrockandroll.near/checkbox/source_code.jsx
+++ beachsunandrockandroll.near/checkbox/source_code.jsx
@@ -1,8 +1,15 @@
 const { Tailwind } = VM.require("beachsunandrockandroll.near/widget/preflight");
 
+const checkboxClassname =
+  "peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground";
+
 const CheckboxRoot = ({ className, ...props }) => (
   <Tailwind>
-    <Checkbox.Root ref="forwardedRef" className={className} {...props}>
+    <Checkbox.Root
+      ref="forwardedRef"
+      className={className ?? checkboxClassname}
+      {...props}
+    >
       <Checkbox.Indicator className="flex items-center justify-center text-current">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -20,4 +27,4 @@ const CheckboxRoot = ({ className, ...props }) => (
   </Tailwind>
 );
 
-return { CheckboxRoot };
+return { CheckboxRoot, checkboxClassname };
