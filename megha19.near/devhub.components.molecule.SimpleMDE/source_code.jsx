dc8f0919e2ea8eede8a3e67e65debe79c616521f
megha19.near <megha19.near>
2024-04-27 05:40:44 +0000

    Update devhub.components.molecule.SimpleMDE by megha19.near at 2024-04-27T05:40:44.300Z

--- megha19.near/devhub.components.molecule.SimpleMDE/source_code.jsx
+++ megha19.near/devhub.components.molecule.SimpleMDE/source_code.jsx
@@ -5,6 +5,7 @@
 
 const data = props.data;
 const onChange = props.onChange ?? (() => {});
+const onChangeKeyup = props.onChangeKeyup ?? (() => {}); // in case where we want immediate action
 const height = props.height ?? "390";
 const className = props.className ?? "w-100";
 const embeddCSS = props.embeddCSS;
@@ -24,7 +25,23 @@ const followingData = Social.get(
 const fontFamily = props.fontFamily ?? "sans-serif";
 const alignToolItems = props.alignToolItems ?? "right";
 const placeholder = props.placeholder ?? "";
-const showAutoComplete = props.showAutoComplete ?? false;
+const autoFocus = props.autoFocus ?? false;
+const showAccountAutoComplete = props.showAutoComplete ?? false;
+const showProposalIdAutoComplete = props.showProposalIdAutoComplete ?? false;
+
+const queryName =
+  "thomasguntenaar_near_devhub_proposals_quebec_proposals_with_latest_snapshot";
+const query = `query GetLatestSnapshot($offset: Int = 0, $limit: Int = 10, $where: ${queryName}_bool_exp = {}) {
+${queryName}(
+  offset: $offset
+  limit: $limit
+  order_by: {proposal_id: desc}
+  where: $where
+) {
+  name
+  proposal_id
+}
+}`;
 
 const code = `
 <!doctype html>
@@ -55,9 +72,11 @@ const code = `
     white-space: normal;
   }
 
-  .dropdown-item:hover{
+  .dropdown-item:hover,
+  .dropdown-item:focus {
     background-color:rgb(0, 236, 151) !important;
     color:white !important;
+    outline: none !important;
   }
 
   .editor-toolbar {
@@ -79,6 +98,12 @@ const code = `
 
   <ul class="dropdown-menu" id="mentiondropdown" style="position: absolute;">
 </div>
+<div class="dropdown">
+  <button style="display: none" type="button" data-bs-toggle="dropdown">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu" id="referencedropdown" style="position: absolute;">
+</div>
 </ul>
 
 <textarea></textarea>
@@ -91,7 +116,9 @@ let codeMirrorInstance;
 let isEditorInitialized = false;
 let followingData = {};
 let profilesData = {};
-let showAutocomplete = ${showAutoComplete}
+let query = '';
+let showAccountAutoComplete = ${showAccountAutoComplete};
+let showProposalIdAutoComplete = ${showProposalIdAutoComplete};
 
 function getSuggestedAccounts(term) {
   let results = [];
@@ -137,6 +164,72 @@ function getSuggestedAccounts(term) {
   return results;
 }
 
+async function asyncFetch(endpoint, { method, headers, body }) {
+  try {
+    const response = await fetch(endpoint, {
+      method: method,
+      headers: headers,
+      body: body
+    });
+
+    if (!response.ok) {
+      throw new Error("HTTP error!");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+function extractNumbers(str) {
+  let numbers = "";
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+      numbers += str[i];
+    }
+  }
+  return numbers;
+};
+
+async function getSuggestedProposals(id) {
+  let results = [];
+  const variables = {
+    limit: 5,
+    offset: 0,
+    where: {},
+  };
+  if (id) {
+    const proposalId = extractNumbers(id);
+    if (proposalId) {
+      variables["where"] = { proposal_id: { _eq: id } };
+    } else {
+      variables["where"] = { name: { _ilike: "%" + id + "%" } };
+    }
+  }
+  await asyncFetch("https://near-queryapi.api.pagoda.co/v1/graphql", {
+    method: "POST",
+    headers: { "x-hasura-role": "thomasguntenaar_near" },
+    body: JSON.stringify({
+      query: query,
+      variables: variables,
+      operationName: "GetLatestSnapshot",
+    }),
+  })
+    .then((res) => {
+      const proposals =
+        res?.data?.[
+          "thomasguntenaar_near_devhub_proposals_quebec_proposals_with_latest_snapshot"
+        ];
+      results = proposals;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  return results;
+};
+
 // Initializes SimpleMDE element and attaches to text-area
 const simplemde = new SimpleMDE({
   forceSync: true,
@@ -159,6 +252,7 @@ const simplemde = new SimpleMDE({
 		singleLineBreaks: false,
 		codeSyntaxHighlighting: true,
 	},
+  autofocus:${autoFocus}
 });
 
 codeMirrorInstance = simplemde.codemirror;
@@ -182,8 +276,14 @@ const updateIframeHeight = () => {
 // On Change
 simplemde.codemirror.on('blur', () => {
   updateContent();
-  updateIframeHeight();
 });
+
+simplemde.codemirror.on('keyup', () => {
+  updateIframeHeight();
+  const content = simplemde.value();
+  window.parent.postMessage({ handler: "updateOnKeyup", content }, "*");
+});
+
 
 if (showAutocomplete) {
   let mentionToken;
@@ -202,7 +302,7 @@ if (showAutocomplete) {
     const cursor = cm.getCursor();
     const token = cm.getTokenAt(cursor);
 
-    const createMentionDrowDownOptions = () => {
+    const createMentionDropDownOptions = () => {
       const mentionInput = cm.getRange(mentionCursorStart, cursor);
       dropdown.innerHTML = getSuggestedAccounts(mentionInput)
       .map(
@@ -234,7 +334,7 @@ if (showAutocomplete) {
         dropdown.style.top = y + "px";
         dropdown.style.left = x + "px";
 
-        createMentionDrowDownOptions();
+        createMentionDropDownOptions();
 
         dropdown.classList.add("show");
 
@@ -249,19 +349,112 @@ if (showAutocomplete) {
         mentionToken = null;
         dropdown.classList.remove("show");
     } else if (mentionToken) {
-        createMentionDrowDownOptions();
+        createMentionDropDownOptions();
     }
+});
+}
+
+if (showProposalIdAutoComplete) {
+  let proposalId;
+  let referenceCursorStart;
+  const dropdown = document.getElementById("referencedropdown");
+  const loader = document.createElement('div');
+  loader.className = 'loader';
+  loader.textContent = 'Loading...';
+
+  simplemde.codemirror.on("keydown", () => {
+    if (proposalId && event.key === 'ArrowDown') {
+      dropdown.querySelector('button').focus();
+      event.preventDefault();
+      return false;
+    }
+  });
+
+  simplemde.codemirror.on("keyup", (cm, event) => {
+    const cursor = cm.getCursor();
+    const token = cm.getTokenAt(cursor);
+
+    const createReferenceDropDownOptions = async () => {
+      try {
+        const proposalIdInput = cm.getRange(referenceCursorStart, cursor);
+        dropdown.innerHTML = ''; // Clear previous content
+        dropdown.appendChild(loader); // Show loader
+
+        const suggestedProposals = await getSuggestedProposals(proposalIdInput);
+        dropdown.innerHTML = suggestedProposals
+        .map(
+          (item) =>
+            '<li><button class="dropdown-item cursor-pointer w-100 text-wrap">' + "#" + item?.proposal_id + " " + item.name + '</button></li>'
+        )
+        .join("");
+
+        dropdown.querySelectorAll("li").forEach((li) => {
+          li.addEventListener("click", () => {
+            const selectedText = li.textContent.trim();
+            const startIndex = selectedText.indexOf('#') + 1; 
+            const endIndex = selectedText.indexOf(' ', startIndex);
+            const id = endIndex !== -1 ? selectedText.substring(startIndex, endIndex) : selectedText.substring(startIndex);
+            const link = "https://near.social/devhub.near/widget/app?page=proposal&id=" + id;
+            const adjustedStart = {
+              line: referenceCursorStart.line,
+              ch: referenceCursorStart.ch - 1
+            };
+            simplemde.codemirror.replaceRange("[" + selectedText + "]" + "(" + link + ")", adjustedStart, cursor);
+            proposalId = null;
+            dropdown.classList.remove("show");
+            cm.focus();
+          });
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle error: Remove loader
+        dropdown.innerHTML = ''; // Clear previous content
+      } finally {
+        // Remove loader
+        dropdown.removeChild(loader);
+      }
+    }
+
+    // show dropwdown only when there is space before # or it's first char
+      if (!proposalId && (token.string === "#" && cursor.ch === 1 || token.string === "#" && cm.getTokenAt({line:cursor.line, ch: cursor.ch - 1}).string == ' ')) {
+        proposalId = token;
+        referenceCursorStart = cursor;
+        // Calculate cursor position relative to the iframe's viewport
+        const rect = cm.charCoords(cursor);
+        const x = rect.left;
+        const y = rect.bottom;
+
+        // Create dropdown with options
+        dropdown.style.top = y + "px";
+        dropdown.style.left = x + "px";
+
+        createReferenceDropDownOptions();
+
+        dropdown.classList.add("show");
+
+        // Close dropdown on outside click
+        document.addEventListener("click", function(event) {
+            if (!dropdown.contains(event.target)) {
+              proposalId = null;
+                dropdown.classList.remove("show");
+            }
+        });
+    } else if (proposalId && (token.string.match(/[^#a-z0-9.]/) || !token.string)) {
+      proposalId = null;
+      dropdown.classList.remove("show");
+    } else if (proposalId) {
+      createReferenceDropDownOptions();
+  }
 });
 
 }
-
 
 window.addEventListener("message", (event) => {
   if (!isEditorInitialized && event.data !== "") {
     simplemde.value(event.data.content);
     isEditorInitialized = true;
   } else {
-    if (event.data.handler === 'autocompleteSelected') {
+    if (event.data.handler === 'refreshEditor') {
       codeMirrorInstance.getDoc().setValue(event.data.content);
     }
   }
@@ -270,6 +463,9 @@ window.addEventListener("message", (event) => {
   }
   if (event.data.profilesData) {
     profilesData = JSON.parse(event.data.profilesData);
+  }
+  if (event.data.query) {
+    query = event.data.query;
   }
 });
 </script>
@@ -282,12 +478,15 @@ return (
     className={className}
     style={{
       height: `${state.iframeHeight}px`,
+      maxHeight: "410px",
     }}
     srcDoc={code}
     message={{
       content: props.data?.content ?? "",
       followingData,
       profilesData: JSON.stringify(profilesData),
+      handler: props.data.handler,
+      query: query,
     }}
     onMessage={(e) => {
       switch (e.handler) {
@@ -300,6 +499,11 @@ return (
           {
             const offset = 10;
             State.update({ iframeHeight: e.height + offset });
+          }
+          break;
+        case "updateOnKeyup":
+          {
+            onChangeKeyup(e.content);
           }
           break;
       }
