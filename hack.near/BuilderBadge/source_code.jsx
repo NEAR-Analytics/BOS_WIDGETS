71995471e1ba2c6bb44b5b85983c167455e6fa6f
hack.near <hack.near>
2024-04-28 21:07:41 +0000

    Update BuilderBadge by hack.near at 2024-04-28T21:07:41.272Z

--- hack.near/BuilderBadge/source_code.jsx
+++ hack.near/BuilderBadge/source_code.jsx
@@ -1,15 +1,22 @@
 const accountId = props.accountId ?? "every.near";
 const attestorId = props.attestorId ?? "hack.near";
+const graphId = props.graphId ?? "commons";
 
-const isBuilder = Social.get(`${accountId}/profile/builder`, "final");
-
-const isActive = Social.get(
-  `${attestorId}/graph/commons/${accountId}`,
-  "final"
+const graphEdge = Social.keys(
+  `${attestorId}/graph/${graphId}/${accountId}`,
+  undefined,
+  {
+    values_only: true,
+  }
 );
+
+const isBuilder = graphEdge && Object.keys(graphEdge).length > 0;
 
 return (
   <>
-    <Widget src="hack.near/widget/BuilderHat" props={{ isBuilder, isActive }} />
+    <Widget
+      src="hack.near/widget/BuilderHat"
+      props={{ isBuilder, isActive: true }}
+    />
   </>
 );
