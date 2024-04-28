477d16d184143c1ae311aa11830f6660ec261660
hack.near <hack.near>
2024-04-28 16:42:37 +0000

    Update attest by hack.near at 2024-04-28T16:42:37.942Z

---
+++ hack.near/attest/source_code.jsx
@@ -0,0 +1,42 @@
+const accountId = props.accountId || "every.near";
+const graphId = props.graphId || "commons";
+
+const graphEdge = Social.keys(
+  `${context.accountId}/graph/${graphId}/${accountId}`,
+  undefined,
+  {
+    values_only: true,
+  }
+);
+
+const inverseEdge = Social.keys(
+  `${accountId}/graph/${graphId}/${context.accountId}`,
+  undefined,
+  {
+    values_only: true,
+  }
+);
+
+const loading = graphEdge === null || inverseEdge === null;
+const attested = graphEdge && Object.keys(graphEdge).length;
+const inverse = inverseEdge && Object.keys(inverseEdge).length;
+
+const type = attested ? "undo" : graphId;
+
+const data = props.data ?? {
+  graph: { [graphId]: { [accountId]: attested ? null : "" } },
+};
+
+const attest = () => {
+  Social.set(data);
+};
+
+return (
+  <button
+    disabled={!context.accountId}
+    className={`btn btn-sm ${attested ? "btn-secondary" : "btn-dark"}`}
+    onClick={attest}
+  >
+    {loading ? "+" : attested ? "x" : "+"}
+  </button>
+);
