5f1f257b5072e7b95d764eb7b1d64fa4dbe65dad
hack.near <hack.near>
2024-04-28 16:42:37 +0000

    Update attest by hack.near at 2024-04-28T16:42:37.942Z

--- hack.near/attest/source_code.jsx
+++ hack.near/attest/source_code.jsx
@@ -1,8 +1,6 @@
 const accountId = props.accountId || "every.near";
 const graphId = props.graphId || "commons";
 
-const commons = Social.getr(`${accountId}/graph/commons`);
-
 const graphEdge = Social.keys(
   `${context.accountId}/graph/${graphId}/${accountId}`,
   undefined,
@@ -34,21 +32,11 @@ const attest = () => {
 };
 
 return (
-  <>
-    {context.accountId && (
-      <>
-        <button
-          disabled={loading ?? !context.accountId}
-          className={`btn btn-sm ${attested ? "btn-dark" : "btn-outline-dark"}`}
-          onClick={attest}
-        >
-          {attested ? (
-            <i className="bi bi-x"></i>
-          ) : (
-            <i className="bi bi-plus"></i>
-          )}
-        </button>
-      </>
-    )}
-  </>
+  <button
+    disabled={!context.accountId}
+    className={`btn btn-sm ${attested ? "btn-secondary" : "btn-dark"}`}
+    onClick={attest}
+  >
+    {loading ? "+" : attested ? "x" : "+"}
+  </button>
 );
