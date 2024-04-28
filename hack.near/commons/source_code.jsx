0fac25205e54fc1d109bccbb37cac23fac85136b
hack.near <hack.near>
2024-04-28 21:38:58 +0000

    Update commons by hack.near at 2024-04-28T21:38:58.419Z

--- hack.near/commons/source_code.jsx
+++ hack.near/commons/source_code.jsx
@@ -1,21 +1,80 @@
-const accountId = context.accountId;
+const accountId = props.accountId ?? context.accountId ?? "every.near";
 
-if (!accountId) {
-  return <Widget src="mob.near/widget/ProfileOnboarding" />;
-}
+const attestations = Social.keys(`*/graph/commons/*`, "final", {
+  values_only: true,
+});
 
-const data = accountId
-  ? Social.get(`${accountId}/settings/dev/library`)
-  : undefined;
+const [matrix, setMatrix] = useState([]);
+const [attestors, setAttestors] = useState([]);
+const [builders, setBuilders] = useState([]);
 
-if (data === null) {
-  return "Loading...";
-}
+useEffect(() => {
+  const attestorSet = new Set();
+  const builderSet = new Set();
 
-const library = [data];
+  Object.entries(attestations).forEach(([attestor, data]) => {
+    attestorSet.add(attestor);
+    Object.keys(data.graph.commons).forEach((builder) => {
+      builderSet.add(builder);
+    });
+  });
+
+  setAttestors(Array.from(attestorSet));
+  setBuilders(Array.from(builderSet));
+
+  const newMatrix = Array.from(builderSet).map((builder) =>
+    Array.from(attestorSet).map((attestor) => ({
+      attestorId: attestor,
+      builderId: builder,
+    }))
+  );
+
+  setMatrix(newMatrix);
+}, []);
 
 return (
-  <>
-    <Widget src="hack.near/widget/dev.library" props={{ data: library }} />
-  </>
+  <div className="m-2 p-1">
+    <table style={{ width: "100%" }}>
+      <thead>
+        <tr>
+          <th style={{ padding: "8px" }}></th>
+          {attestors.map((attestor, index) => (
+            <th key={index} style={{ padding: "8px" }}>
+              {attestor}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {matrix.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            <td style={{ padding: "8px" }}>
+              <b>{builders[rowIndex]}</b>
+            </td>
+            {row.map((cell, cellIndex) => (
+              <td style={{ padding: "8px" }} key={cellIndex}>
+                {accountId !== cell.attestorId ? (
+                  <Widget
+                    src="hack.near/widget/BuilderBadge"
+                    props={{
+                      accountId: cell.builderId,
+                      attestorId: cell.attestorId,
+                    }}
+                  />
+                ) : (
+                  <Widget
+                    src="hack.near/widget/attest"
+                    props={{
+                      accountId: cell.builderId,
+                      attestorId: cell.attestorId,
+                    }}
+                  />
+                )}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
