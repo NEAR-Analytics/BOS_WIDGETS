b200a04bbfddf6b0049aca26b34847c588fb8ac7
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-04-29 20:17:52 +0000

    Update button by beachsunandrockandroll.near at 2024-04-29T20:17:52.003Z

--- beachsunandrockandroll.near/button/source_code.jsx
+++ beachsunandrockandroll.near/button/source_code.jsx
@@ -3,22 +3,22 @@ const { Tailwind } = VM.require("beachsunandrockandroll.near/widget/preflight");
 const Button = ({ className, children, State, ...props }) => {
   return (
     <Tailwind>
-      {["", "loaded"].includes(state.cnButton) ? (
+      {/* {["", "loaded"].includes(state.cnButton) ? ( */}
         <Widget
           src="beachsunandrockandroll.near/widget/buttonIframe"
           props={{
             output: (cnButton) => {
-              console.log(cnButton)
+              console.log(cnButton);
               State.update({ cnButton });
             },
             className,
           }}
         />
-      ) : (
+      {/* ) : ( */}
         <button className={state.cnButton} ref="forwardedRef" {...props}>
           {children}
         </button>
-      )}
+      {/* )} */}
     </Tailwind>
   );
 };
