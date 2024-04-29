215fa1a38e036c4dd590cc2286c6ded33ec7b067
bluebiu.near <bluebiu.near>
2024-04-29 02:59:16 +0000

    Update Liquidity.Data.JUICE by bluebiu.near at 2024-04-29T02:59:16.019Z

---
+++ bluebiu.near/Liquidity.Data.JUICE/source_code.jsx
@@ -0,0 +1,96 @@
+
+const {
+  onLoad,
+  dataList,
+  multicall,
+  multicallAddress,
+  smartContractAddress
+} = props
+
+// const dataList = dataList
+function updateDataList() {
+  onLoad({
+    dataList
+  })
+}
+function handleGetTotalBaseDeposit() {
+  const calls = []
+  for (let i = 0; i < dataList.length; i++) {
+    calls.push({
+      address: dataList[i].strategyAddress,
+      name: "getTotalBaseDeposit",
+    })
+  }
+  multicall({
+    abi: [{
+      "inputs": [],
+      "name": "getTotalBaseDeposit",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }],
+    calls,
+    options: {},
+    multicallAddress,
+    provider: Ethers.provider(),
+  }).then(result => {
+    for (let i = 0; i < dataList.length; i++) {
+      dataList[i].pointList[0].value = Big(ethers.utils.formatUnits(result[i][0])).toString()
+    }
+    updateDataList("handleGetTotalBaseDeposit")
+  })
+}
+function handleGetPositionValue() {
+  const calls = []
+  for (let i = 0; i < dataList.length; i++) {
+    calls.push({
+      address: dataList[i].strategyAddress,
+      name: "getPositionValue",
+      params: [smartContractAddress]
+    })
+  }
+  console.log('=calls', calls)
+  multicall({
+    abi: [{
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "getPositionValue",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }],
+    calls,
+    options: {},
+    multicallAddress,
+    provider: Ethers.provider(),
+  }).then(result => {
+    for (let i = 0; i < dataList.length; i++) {
+      dataList[i].positionValue = Big(result[i][0] ? ethers.utils.formatUnits(result[i][0]) : 0).toString()
+    }
+    console.log('=dataList', dataList)
+    updateDataList("handleGetPositionValue")
+  })
+}
+useEffect(() => {
+  handleGetTotalBaseDeposit()
+}, [])
+useEffect(() => {
+  smartContractAddress && handleGetPositionValue()
+}, [smartContractAddress])
