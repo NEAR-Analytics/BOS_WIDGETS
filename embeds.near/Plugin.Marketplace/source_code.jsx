0cfbe33a30a950384a797e45f104f89ce00dcd9e
embeds.near <embeds.near>
2024-04-30 02:56:50 +0000

    Update Plugin.Marketplace by embeds.near at 2024-04-30T02:56:50.403Z

--- embeds.near/Plugin.Marketplace/source_code.jsx
+++ embeds.near/Plugin.Marketplace/source_code.jsx
@@ -1,47 +1,37 @@
 const { type, widgetSrc } = props;
-
 const plugins = Social.keys(`*/plugin/${type || "*"}/*`, "final", {
   return_type: "BlockHeight",
 });
-
 if (!plugins) {
   return "Loading...";
 }
-
 const Container = styled.div`
   margin: 0 auto;
   padding: 20px;
   width: 100%;
   max-width: 1200px;
-
   a {
     text-decoration: none;
     color: inherit;
   }
 `;
-
 const Grid = styled.div`
   display: grid;
   grid-gap: 10px;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-
   @media (min-width: 576px) {
     grid-gap: 15px;
   }
-
   @media (min-width: 992px) {
     grid-gap: 20px;
   }
-
   > * {
     transition: transform 0.3s ease; // Smooth transition for hover effect
-
     &:hover {
       transform: scale(1.03); // Subtle scale effect on hover
     }
   }
 `;
-
 const processData = useCallback(
   (data) => {
     const accounts = Object.entries(data);
@@ -72,20 +62,16 @@ const processData = useCallback(
         }
       })
       .flat();
-
     // sort by latest
     allItems.sort((a, b) => b.blockHeight - a.blockHeight);
     return allItems;
   },
   [type]
 );
-
 const items = processData(plugins);
-
 if (!items) {
   return "Loading data...";
 }
-
 function Item({ accountId, name, metadata }) {
   return (
     <Widget
@@ -94,9 +80,7 @@ function Item({ accountId, name, metadata }) {
     />
   );
 }
-
 const [showCreator, setShowCreator] = useState("");
-
 return (
   <Container>
     <p class="d-flex flex-row-reverse">
