165d73e6e027bbf4dc6ae2c757620f483ab14d72
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-05-01 08:23:29 +0000

    Update myAlertDialog by beachsunandrockandroll.near at 2024-05-01T08:23:29.139Z

--- beachsunandrockandroll.near/myAlertDialog/source_code.jsx
+++ beachsunandrockandroll.near/myAlertDialog/source_code.jsx
@@ -1,135 +1,104 @@
 const { Tailwind } = VM.require("beachsunandrockandroll.near/widget/preflight");
-
+const { Button } = VM.require("beachsunandrockandroll.near/widget/button");
+const { Input } = VM.require("beachsunandrockandroll.near/widget/input");
+const { Label } = VM.require("beachsunandrockandroll.near/widget/label");
 const {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerClassname,
-} = VM.require("beachsunandrockandroll.near/widget/navigationMenu");
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  cardClassname,
+  cardFooterClassname,
+} = VM.require("beachsunandrockandroll.near/widget/card");
 
-const components = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
+const { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } =
+  VM.require("beachsunandrockandroll.near/widget/select");
 
-const ListItem = ({ className, title, children, ...props }) => {
+const [buttonCancel, setButtonCancel] = useState("");
+const [buttonDeploy, setButtonDeploy] = useState("");
+const [card, setCard] = useState("");
+const [cardFooter, setCardFooter] = useState("");
+
+if (["", "loaded"].includes(buttonCancel))
   return (
-    <li>
-      <NavigationMenuLink>
-        <a
-          ref="forwardedRef"
-          className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
+    <Widget
+      src="beachsunandrockandroll.near/widget/buttonIframe"
+      props={{
+        output: setButtonCancel,
+        variant: "outline",
+      }}
+    />
   );
-};
 
-if (Tailwind === undefined) return <></>;
+if (["", "loaded"].includes(buttonDeploy))
+  return (
+    <Widget
+      src="beachsunandrockandroll.near/widget/buttonIframe"
+      props={{ output: setButtonDeploy }}
+    />
+  );
+
+if (["", "loaded"].includes(card))
+  return (
+    <Widget
+      src="beachsunandrockandroll.near/widget/cnIframe"
+      props={{
+        output: setCard,
+        className: `${cardClassname} max-w-lg sm:w-96`,
+      }}
+    />
+  );
+
+if (["", "loaded"].includes(cardFooter))
+  return (
+    <Widget
+      src="beachsunandrockandroll.near/widget/cnIframe"
+      props={{
+        output: setCardFooter,
+        className: `${cardFooterClassname} flex justify-between`,
+      }}
+    />
+  );
 
 return (
   <Tailwind>
-    <div className="flex mx-auto w-max pt-10">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="gap-3 p-4 w-80">
-                <li>
-                  <NavigationMenuLink>
-                    <a
-                      className="flex h-full w-full select-none flex-col hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground justify-end rounded-md p-3  no-underline outline-none focus:shadow-md"
-                      href="/"
-                    >
-                      <div className="mb-2 text-lg font-medium">UI is Near</div>
-                      <p className="text-sm leading-tight text-muted-foreground">
-                        Beautifully designed components built with Radix UI and
-                        Tailwind CSS.
-                      </p>
-                    </a>
-                  </NavigationMenuLink>
-                </li>
-
-                <ListItem href="/docs" title="Introduction">
-                  Re-usable components built using Radix UI and Tailwind CSS.
-                </ListItem>
-                <ListItem href="/docs/installation" title="Installation">
-                  How to install dependencies and structure your app.
-                </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Typography">
-                  Styles for headings, paragraphs, lists...etc
-                </ListItem>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="w-96 gap-3 p-4 grid  md:grid-cols-2">
-                {components.map((component) => (
-                  <ListItem
-                    key={component.title}
-                    title={component.title}
-                    href={component.href}
-                  >
-                    {component.description}
-                  </ListItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <a href="#">
-              <NavigationMenuLink className={navigationMenuTriggerClassname}>
-                Documentation
-              </NavigationMenuLink>
-            </a>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <div className="flex max-w-lg px-10 mx-auto w-max pt-10">
+      <Card className={card}>
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="next">Next.js</SelectItem>
+                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                  <SelectItem value="astro">Astro</SelectItem>
+                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className={cardFooter}>
+          <Button className={buttonCancel}>Cancel</Button>
+          <Button className={buttonDeploy}>Deploy</Button>
+        </CardFooter>
+      </Card>
     </div>
   </Tailwind>
 );
