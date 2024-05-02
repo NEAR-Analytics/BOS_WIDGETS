0bdea033e88716dadd07dc7f9e5f5fdc106f4121
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-05-02 10:45:05 +0000

    Update alertIframe by beachsunandrockandroll.near at 2024-05-02T10:45:05.330Z

--- beachsunandrockandroll.near/alertIframe/source_code.jsx
+++ beachsunandrockandroll.near/alertIframe/source_code.jsx
@@ -1,14 +1,18 @@
+const { baseAlert, variantDefault } = VM.require(
+  "beachsunandrockandroll.near/widget/alert"
+);
+
 const srcDoc = `
 <script type="module"> 
     import mxcn from "https://cdn.jsdelivr.net/npm/mxcn@2.0.0/+esm"
-    import {cva} from 'https://cdn.jsdelivr.net/npm/class-variance-authority@0.7.0/+esm'
+    import { cva } from 'https://cdn.jsdelivr.net/npm/class-variance-authority@0.7.0/+esm'
     
     const alertVariants = cva(
-        "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7",
+        "${baseAlert}",
         {
           variants: {
             variant: {
-              default: "bg-background text-foreground",
+              default: "${variantDefault}",
               destructive:
                 "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
             },
@@ -19,13 +23,10 @@ const srcDoc = `
         }
     )
 
-    window.top.postMessage("loaded", "*");
     window.addEventListener("message", ({ data }) => {
         try {
             event.source.postMessage(mxcn(alertVariants(data)), "*");
-        } catch (e) {
-            // ignore
-        }
+        } catch (e) {}
     }, false);
 </script>
 `;
