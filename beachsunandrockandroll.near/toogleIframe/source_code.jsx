382f996a36689cdf31e9f3c7a391a04705acfce8
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-05-02 09:41:47 +0000

    Update toogleIframe by beachsunandrockandroll.near at 2024-05-02T09:41:47.142Z

--- beachsunandrockandroll.near/toogleIframe/source_code.jsx
+++ beachsunandrockandroll.near/toogleIframe/source_code.jsx
@@ -1,19 +1,23 @@
+const { baseToogle, variantDefault, sizeDefault } = VM.require(
+  "beachsunandrockandroll.near/widget/toogle"
+);
+
 const srcDoc = `
 <script type="module"> 
     import mxcn from "https://cdn.jsdelivr.net/npm/mxcn@2.0.0/+esm"
     import {cva} from 'https://cdn.jsdelivr.net/npm/class-variance-authority@0.7.0/+esm'
     
     const toggleVariants = cva(
-      "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground",
+      "${baseToogle}",
       {
         variants: {
           variant: {
-            default: "bg-transparent",
+            default: "${variantDefault}",
             outline:
               "border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
           },
           size: {
-            default: "h-9 px-3",
+            default: "${sizeDefault}",
             sm: "h-8 px-2",
             lg: "h-10 px-3",
           },
@@ -25,7 +29,6 @@ const srcDoc = `
       }
     );
 
-    window.top.postMessage("loaded", "*");
     window.addEventListener("message", ({ data }) => {
         try {
             event.source.postMessage(mxcn(toggleVariants(data)), "*");
