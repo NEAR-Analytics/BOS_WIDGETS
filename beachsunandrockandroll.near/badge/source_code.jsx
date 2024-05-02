c9e737ce27fb29b0bd17a5a1786b921e960adf06
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-05-02 14:22:59 +0000

    Update badge by beachsunandrockandroll.near at 2024-05-02T14:22:59.079Z

--- beachsunandrockandroll.near/badge/source_code.jsx
+++ beachsunandrockandroll.near/badge/source_code.jsx
@@ -4,6 +4,51 @@ const baseBadge =
 const variantDefault =
   "border-transparent bg-uin-primary text-primary-foreground shadow hover:opacity-80";
 
+const BadgeConf = () => {
+  const srcDoc = `
+    <script type="module">
+      import clsx from 'https://cdn.jsdelivr.net/npm/clsx@2.1.1/+esm'
+      import { twMerge } from 'https://cdn.jsdelivr.net/npm/tailwind-merge@2.3.0/+esm'
+      import { cva } from 'https://cdn.jsdelivr.net/npm/class-variance-authority@0.7.0/+esm'
+      
+      const badgeVariants = cva(
+        "${baseBadge}",
+        {
+          variants: {
+            variant: {
+              default:
+                "${variantDefault}",
+              secondary:
+                "border-transparent bg-secondary text-secondary-foreground hover:opacity-80",
+              destructive:
+                "border-transparent bg-destructive text-destructive-foreground shadow hover:opacity-80",
+              outline: "text-foreground",
+            },
+          },
+          defaultVariants: {
+            variant: "default",
+          },
+        }
+      )
+  
+      window.addEventListener("message", ({ data }) => {
+        try {
+          event.source.postMessage(twMerge(clsx(badgeVariants(data))), "*");
+        } catch (e) {}
+      }, false);
+    </script>
+  `;
+
+  return (
+    <iframe
+      className="d-none"
+      srcDoc={srcDoc}
+      message={{ className, variant }}
+      onMessage={output}
+    />
+  );
+};
+
 const badgeClassnameDefault = `${baseBadge} ${variantDefault}`;
 
 const Badge = ({ className, children, ...props }) => (
@@ -12,4 +57,4 @@ const Badge = ({ className, children, ...props }) => (
   </div>
 );
 
-return { Badge, baseBadge, variantDefault, badgeClassnameDefault };
+return { Badge, BadgeConf };
