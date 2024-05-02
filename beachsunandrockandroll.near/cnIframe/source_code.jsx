cc5b86a5ab93052292cee9e8c14755124e6f5f61
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-05-02 12:42:25 +0000

    Update cnIframe by beachsunandrockandroll.near at 2024-05-02T12:42:25.726Z

--- beachsunandrockandroll.near/cnIframe/source_code.jsx
+++ beachsunandrockandroll.near/cnIframe/source_code.jsx
@@ -1,24 +1,24 @@
-const srcDoc = `
-<script type="module"> 
-    import mxcn from "https://cdn.jsdelivr.net/npm/mxcn@2.0.0/+esm"
-    
-    window.addEventListener("message", ({ data }) => {
+const ClassnameConf = ({ className, output }) => {
+  const srcDoc = `
+    <script type="module"> 
+      import mxcn from "https://cdn.jsdelivr.net/npm/mxcn@2.0.0/+esm"
+        
+      window.addEventListener("message", ({ data }) => {
         try {
-            event.source.postMessage(mxcn(data.className), "*");
-        } catch (e) {
-            // ignore
-        }
-    }, false);
-</script>
-`;
+          event.source.postMessage(mxcn(data.className), "*");
+        } catch (e) {}
+      }, false);
+    </script>
+  `;
 
-return (
-  <iframe
-    className="d-none"
-    srcDoc={srcDoc}
-    message={{
-      className: props.className,
-    }}
-    onMessage={props.output}
-  />
-);
+  return (
+    <iframe
+      className="d-none"
+      srcDoc={srcDoc}
+      message={{ className }}
+      onMessage={output}
+    />
+  );
+};
+
+return { ClassnameConf };
