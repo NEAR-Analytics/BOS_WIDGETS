8803249635b8985e008fc435e4a55865dce7aafe
nearblocksonbos.near <nearblocksonbos.near>
2024-05-02 09:58:34 +0000

    Update bos-components.components.Transactions.Hash by nearblocksonbos.near at 2024-05-02T09:58:34.048Z

--- nearblocksonbos.near/bos-components.components.Transactions.Hash/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Transactions.Hash/source_code.jsx
@@ -41,32 +41,6 @@ const ErrorMessage = ({ icons, message, mutedText }) => {
     </div>
   );
 };/* END_INCLUDE COMPONENT: "includes/Common/ErrorMessage.jsx" */
-/* INCLUDE COMPONENT: "includes/icons/ArrowDown.jsx" */
-/**
- * @interface Props
- * @param {string} [className] - The CSS class name(s) for styling purposes.
- */
-
-
-
-
-const ArrowDown = (props) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width={24}
-      height={24}
-      {...props}
-    >
-      <path fill="none" d="M0 0h24v24H0z" />
-      <path
-        fill="currentColor"
-        d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z"
-      />
-    </svg>
-  );
-};/* END_INCLUDE COMPONENT: "includes/icons/ArrowDown.jsx" */
 /* INCLUDE COMPONENT: "includes/icons/FileSlash.jsx" */
 const FileSlash = () => {
   return (
@@ -86,8 +60,6 @@ const FileSlash = () => {
 };/* END_INCLUDE COMPONENT: "includes/icons/FileSlash.jsx" */
 
 
-const hashes = ['overview', 'execution', 'comments'];
-
 function MainComponent(props) {
   const { t, network, hash, onHandleTab, pageTab, ownerId } = props;
 
@@ -98,15 +70,14 @@ function MainComponent(props) {
   const [isLoading, setIsLoading] = useState(true);
   const [txn, setTxn] = useState(null);
   const [error, setError] = useState(false);
-  const [isToggle, setIsToggle] = useState(false);
   const [rpcTxn, setRpcTxn] = useState(
     {} ,
   );
 
   const config = getConfig && getConfig(network);
 
-  const onTab = (index) => {
-    onHandleTab(hashes[index]);
+  const onTab = (hash) => {
+    onHandleTab(hash);
   };
 
   useEffect(() => {
@@ -181,8 +152,15 @@ function MainComponent(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [txn, config?.rpcUrl]);
 
+  const buttonStyles = (hash) =>
+    `relative text-nearblue-600  text-xs leading-4 font-medium inline-block cursor-pointer mb-3 mr-3 focus:outline-none ${
+      pageTab === hash
+        ? 'rounded-lg bg-green-600 dark:bg-green-250 text-white'
+        : 'hover:bg-neargray-800 bg-neargray-700 dark:text-neargray-10 dark:bg-black-200  rounded-lg hover:text-nearblue-600'
+    }`;
+
   return (
-    <>
+    <Fragment key="hash">
       {error || (!isLoading && !txn) ? (
         <div className="bg-white dark:bg-black-600 soft-shadow rounded-xl pb-1">
           <div className="text-sm text-nearblue-600 dark:text-neargray-10 divide-solid dark:divide-black-200 divide-gray-200 divide-y">
@@ -196,150 +174,97 @@ function MainComponent(props) {
       ) : (
         <>
           <div>
-            {hashes &&
-              hashes.map((hash, index) => (
-                <button
-                  key={index}
-                  onClick={() => onTab(index)}
-                  className={`text-nearblue-600  text-xs leading-4 font-medium overflow-hidden inline-block cursor-pointer mb-3 mr-3 focus:outline-none ${
-                    pageTab === hash
-                      ? 'rounded-lg bg-green-600 dark:bg-green-250 text-white'
-                      : 'hover:bg-neargray-800 bg-neargray-700 dark:text-neargray-10 dark:bg-black-200  rounded-lg hover:text-nearblue-600'
-                  }`}
-                  value={hash}
-                >
-                  {hash === 'overview' ? (
-                    <h2 className="p-2">
-                      {t ? t('txns:txn.tabs.overview') : 'Overview'}
-                    </h2>
-                  ) : hash === 'execution' ? (
-                    pageTab !== 'execution' ? (
-                      <div className="p-2">
-                        <h2 className="flex">
-                          {isToggle
-                            ? 'Enhanced Plan'
-                            : t
-                            ? t('txns:txn.tabs.execution')
-                            : 'Execution Plan'}
-                          <ArrowDown className="h-4 w-4 fill-current ml-1" />
-                        </h2>
-                        <div className="absolute text-white bg-neargreen text-[8px] h-4 inline-flex items-center rounded-md ml-11 -mt-7 px-1 ">
-                          NEW
-                        </div>
-                      </div>
-                    ) : (
-                      <Popover.Root key={isToggle}>
-                        <Popover.Trigger asChild>
-                          <button className="flex p-2 text-xs  rounded focus:outline-none">
-                            {isToggle
-                              ? 'Enhanced Plan'
-                              : t
-                              ? t('txns:txn.tabs.execution')
-                              : 'Execution Plan'}
-                            <ArrowDown className="h-4 w-4 fill-current ml-1" />
-                            <div className="absolute text-white bg-neargreen text-[8px] h-4 inline-flex items-center rounded-md ml-24 -mt-3 px-1 ">
-                              NEW
-                            </div>
-                          </button>
-                        </Popover.Trigger>
-                        <Popover.Content
-                          className="bg-white dark:bg-black-600 dark:border-black-200 w-48 shadow-lg border rounded-lg slide-down mt-2 z-50"
-                          sideOffset={5}
-                        >
-                          <ul className="divide-y dark:divide-black-200">
-                            <li
-                              onClick={() => setIsToggle(false)}
-                              className={`py-2 text-nearblue-600 dark:text-neargray-10 rounded-t-lg ${
-                                !isToggle ? 'bg-gray-300 dark:bg-black-200' : ''
-                              }`}
-                            >
-                              {t('txns:txn.tabs.execution') || 'Execution Plan'}
-                            </li>
-                            <li
-                              onClick={() => setIsToggle(true)}
-                              className={`py-2 text-nearblue-600 dark:text-neargray-10 rounded-b-lg ${
-                                isToggle ? 'bg-gray-300 dark:bg-black-200' : ''
-                              }`}
-                            >
-                              Enhanced Plan
-                            </li>
-                          </ul>
-                        </Popover.Content>
-                      </Popover.Root>
-                    )
-                  ) : (
-                    <h2 className="p-2">
-                      {t ? t('txns:txn.tabs.comments') : 'Comments'}
-                    </h2>
-                  )}
-                </button>
-              ))}
+            <button
+              onClick={() => onTab('overview')}
+              className={buttonStyles('overview')}
+            >
+              <h2 className="p-2">
+                {t ? t('txns:txn.tabs.overview') : 'Overview'}
+              </h2>
+            </button>
+            <button
+              onClick={() => onTab('execution')}
+              className={buttonStyles('execution')}
+            >
+              <h2 className="p-2">
+                {t ? t('txns:txn.tabs.execution') : 'Execution Plan'}
+              </h2>
+            </button>
+            <button
+              onClick={() => onTab('enhanced')}
+              className={buttonStyles('enhanced')}
+            >
+              <h2 className="p-2">{'Enhanced Plan'}</h2>
+              <div className="absolute text-white bg-neargreen text-[8px] h-4 inline-flex items-center rounded-md -top-1.5 -right-1.5 px-1">
+                NEW
+              </div>
+            </button>
+            <button
+              onClick={() => onTab('comments')}
+              className={buttonStyles('comments')}
+            >
+              <h2 className="p-2">
+                {t ? t('txns:txn.tabs.comments') : 'Comments'}
+              </h2>
+            </button>
           </div>
           <div className="bg-white dark:bg-black-600 soft-shadow rounded-xl pb-1">
             <div className={`${pageTab === 'overview' ? '' : 'hidden'} `}>
-              {
-                <Widget
-                  src={`${ownerId}/widget/bos-components.components.Transactions.Detail`}
-                  props={{
-                    txn: txn,
-                    rpcTxn: rpcTxn,
-                    loading: isLoading,
-                    network: network,
-                    t: t,
-                    ownerId,
-                  }}
-                />
-              }
+              <Widget
+                src={`${ownerId}/widget/bos-components.components.Transactions.Detail`}
+                props={{
+                  txn: txn,
+                  rpcTxn: rpcTxn,
+                  loading: isLoading,
+                  network: network,
+                  t: t,
+                  ownerId,
+                }}
+              />
             </div>
             <div className={`${pageTab === 'execution' ? '' : 'hidden'} `}>
-              <div className={`${isToggle ? '' : 'hidden'} `}>
-                {
-                  <Widget
-                    src={`${ownerId}/widget/bos-components.components.Transactions.Execution`}
-                    props={{
-                      network: network,
-                      t: t,
-                      txn: txn,
-                      rpcTxn: rpcTxn,
-                      loading: isLoading,
-                      ownerId,
-                    }}
-                  />
-                }
-              </div>
-              <div className={`${isToggle ? 'hidden' : ''} `}>
-                <Widget
-                  src={`${ownerId}/widget/bos-components.components.Transactions.Receipt`}
-                  props={{
-                    network: network,
-                    t: t,
-                    txn: txn,
-                    rpcTxn: rpcTxn,
-                    loading: isLoading,
-                    ownerId,
-                  }}
-                />
-              </div>
+              <Widget
+                src={`${ownerId}/widget/bos-components.components.Transactions.Receipt`}
+                props={{
+                  network: network,
+                  t: t,
+                  txn: txn,
+                  rpcTxn: rpcTxn,
+                  loading: isLoading,
+                  ownerId,
+                }}
+              />
+            </div>
+            <div className={`${pageTab === 'enhanced' ? '' : 'hidden'} `}>
+              <Widget
+                src={`${ownerId}/widget/bos-components.components.Transactions.Execution`}
+                props={{
+                  network: network,
+                  t: t,
+                  txn: txn,
+                  rpcTxn: rpcTxn,
+                  loading: isLoading,
+                  ownerId,
+                }}
+              />
             </div>
             <div className={`${pageTab === 'comments' ? '' : 'hidden'} `}>
               <div className="py-3">
-                {
-                  <Widget
-                    src={`${ownerId}/widget/bos-components.components.Comments.Feed`}
-                    props={{
-                      network: network,
-                      path: `nearblocks.io/txns/${hash}`,
-                      limit: 10,
-                      ownerId,
-                    }}
-                  />
-                }
+                <Widget
+                  src={`${ownerId}/widget/bos-components.components.Comments.Feed`}
+                  props={{
+                    network: network,
+                    path: `nearblocks.io/txns/${hash}`,
+                    limit: 10,
+                    ownerId,
+                  }}
+                />
               </div>
             </div>
           </div>
         </>
       )}
-    </>
+    </Fragment>
   );
 }
 
