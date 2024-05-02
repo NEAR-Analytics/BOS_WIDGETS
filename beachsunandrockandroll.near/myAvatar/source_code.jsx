12df0746f23fc3e037ede2537114f809216114c3
beachsunandrockandroll.near <beachsunandrockandroll.near>
2024-05-02 12:01:44 +0000

    Update myAvatar by beachsunandrockandroll.near at 2024-05-02T12:01:44.744Z

--- beachsunandrockandroll.near/myAvatar/source_code.jsx
+++ beachsunandrockandroll.near/myAvatar/source_code.jsx
@@ -7,23 +7,24 @@ const { Button, BtnIframe } = VM.require(
 
 const [buttonOutline, setButtonOutline] = useState("");
 const [buttonDestructive, setButtonDestructive] = useState("");
+const [buttonSecondary, setButtonSecondary] = useState("");
+const [buttonGhost, setButtonGhost] = useState("");
+const [buttonLink, setButtonLink] = useState("");
 
 if (buttonOutline === "")
-  return (
-    <BtnIframe
-      output={setButtonOutline}
-      variant="outline"
-    />
-  );
+  return <BtnIframe output={setButtonOutline} variant="outline" />;
 
 if (buttonDestructive === "")
-  return (
-    <BtnIframe
-      output={setButtonDestructive}
-      variant="destructive"
-      className="text-green-400 rounded-xl"
-    />
-  );
+  return <BtnIframe output={setButtonDestructive} variant="destructive" />;
+
+if (buttonSecondary === "")
+  return <BtnIframe output={setButtonSecondary} variant="secondary" />;
+
+if (buttonGhost === "")
+  return <BtnIframe output={setButtonGhost} variant="ghost" />;
+
+if (buttonLink === "")
+  return <BtnIframe output={setButtonLink} variant="link" />;
 
 if (Tailwind == undefined) return "";
 
@@ -33,6 +34,9 @@ return (
       <Button>Button</Button>
       <Button className={buttonOutline}>Outline</Button>
       <Button className={buttonDestructive}>Destructive</Button>
+      <Button className={buttonSecondary}>Secondary</Button>
+      <Button className={buttonGhost}>Ghost</Button>
+      <Button className={buttonLink}>Link</Button>
     </div>
   </Tailwind>
 );
