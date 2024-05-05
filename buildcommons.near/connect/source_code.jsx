4c2b93f8cc631ba3794bc1274ad017c949391a53
buildcommons.near <buildcommons.near>
2024-05-05 11:22:01 +0000

    Update connect by buildcommons.near at 2024-05-05T11:22:01.036Z

--- buildcommons.near/connect/source_code.jsx
+++ buildcommons.near/connect/source_code.jsx
@@ -14,41 +14,25 @@ if (
     });
 }
 
-const nearButtonLink =
-  props.nearButtonLink || "https://wallet.mintbase.xyz/connect";
-const nearButtonLabel = props.nearButtonLabel || "⋈ NEAR ";
-const web3ButtonLabel = props.web3ButtonLabel || "⬨ Ethereum";
-const nearButtonClass = props.nearButtonClass || "m-2 btn btn-outline-dark";
-const web3ButtonClass = props.web3ButtonClass || "m-2 btn btn-outline-dark";
+const nearButtonLabel = props.nearButtonLabel || "⋈ Get NEAR";
+const web3ButtonLabel = props.web3ButtonLabel || "⬨ Web3Connect";
+const nearButtonClass = props.nearButtonClass || "m-1 btn btn-outline-dark";
+const web3ButtonClass = props.web3ButtonClass || "m-1 btn btn-outline-dark";
 
 return (
   <>
     {accountId ? (
       ""
     ) : (
-      <div className="m-2 d-flex flex-row justify-content-center">
-        <div className="m-2 d-flex flex-column align-items-center">
-          <a href={nearButtonLink} className={nearButtonClass}>
-            {nearButtonLabel}
-          </a>
-          <img
-            className="mt-2"
-            style={{ maxWidth: "93px" }}
-            src="https://wallet.mintbase.xyz/mintbase-wallet-logo.svg"
-          />
-        </div>
-        <div className="m-2 d-flex flex-column align-items-center">
-          <Web3Connect
-            className={web3ButtonClass}
-            connectLabel={web3ButtonLabel}
-          />
-          <img
-            className="mt-3"
-            style={{ maxWidth: "112px" }}
-            src="https://www.blocknative.com/hubfs/Icons%20and%20Illustrations/Header%20Icons/blocknative%20light%20mode%20logo.svg"
-          />
-        </div>
-      </div>
+      <>
+        <a href="https://shard.dog/nearweek" className={nearButtonClass}>
+          {nearButtonLabel}
+        </a>
+        <Web3Connect
+          className={web3ButtonClass}
+          connectLabel={web3ButtonLabel}
+        />
+      </>
     )}
   </>
 );
