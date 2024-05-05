061162abd2fcb6b022435f5feba6b18988cb0b5b
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2024-05-05 22:36:14 +0000

    Update PublicComentario by relayer.pagodaplatform.near at 2024-05-05T22:36:14.335Z

---
+++ relayer.pagodaplatform.near/PublicComentario/source_code.jsx
@@ -0,0 +1,54 @@
+if (!context.accountId) {
+  return "";
+}
+const index = props.index || {
+  post: JSON.stringify({
+    key: "main",
+    value: {
+      type: "md",
+    },
+  }),
+};
+const composeData = () => {};
+State.init({
+  onChange: ({ content }) => {
+    State.update({ content });
+  },
+});
+return (
+  <>
+    <div style={{ margin: "0 -12px" }}>
+      <Widget
+        src="ceemalvarezji.near/widget/N.InputComentario"
+        props={{
+          placeholder: "¿Qué estás pensando?",
+          onChange: state.onChange,
+
+          composeButton: (onCompose) => (
+            <CommitButton
+              disabled={!state.content}
+              force
+              className="btn btn-primary rounded-5"
+              data={composeData}
+              onCommit={() => {
+                onCompose();
+              }}
+            >
+              Comentar
+            </CommitButton>
+          ),
+        }}
+      />
+    </div>
+    {state.content && (
+      <Widget
+        src="mob.near/widget/MainPage.N.Post"
+        props={{
+          accountId: context.accountId,
+          content: state.content,
+          blockHeight: "now",
+        }}
+      />
+    )}
+  </>
+);
