130f1ccc42c9f38c89ac346092beedd39314ff42
buildcommons.near <buildcommons.near>
2024-05-05 09:31:22 +0000

    Update profile.builder by buildcommons.near at 2024-05-05T09:31:22.490Z

--- buildcommons.near/profile.builder/source_code.jsx
+++ buildcommons.near/profile.builder/source_code.jsx
@@ -11,39 +11,37 @@ const imgWrapperStyle = { height: "3em", width: "3em" };
 
 return (
   <>
-    <a
-      style={{ color: "#000", textDecoration: "none" }}
-      href={`https://www.nearbuilders.org/buildhub.near/widget/app?page=profile&accountId=${accountId}`}
-    >
-      <div className="d-flex flex-row align-items-center">
-        <div className="me-2">
-          <Widget
-            src="mob.near/widget/ProfileImage"
-            loading={<div style={imgWrapperStyle} />}
-            props={{
-              fast,
-              profile,
-              accountId,
-              widgetName,
-              style: imgWrapperStyle,
-              imageClassName: "rounded-circle w-100 h-100",
-            }}
-          />
-        </div>
-        <div className="d-flex flex-column justify-content-center">
+    <div className="d-flex flex-row align-items-center">
+      <div className="me-2">
+        <Widget
+          src="mob.near/widget/ProfileImage"
+          loading={<div style={imgWrapperStyle} />}
+          props={{
+            fast,
+            profile,
+            accountId,
+            widgetName,
+            style: imgWrapperStyle,
+            imageClassName: "rounded-circle w-100 h-100",
+          }}
+        />
+      </div>
+      <div
+        style={{ fontFamily: "Courier, sans-serif" }}
+        className="d-flex flex-column justify-content-center"
+      >
+        <div className="text-truncate">
           <div className="text-truncate">
-            <div className="text-truncate">
-              <span className="fw-bold me-1">{name}</span>
-              <Widget src="hack.near/widget/BuilderHat" props={{ accountId }} />
-            </div>
-            <div className="text-truncate text-muted">
-              <small>
-                <span className="font-monospace">@{accountId}</span>
-              </small>
-            </div>
+            <span className="fw-bold me-1">{name}</span>
+            <Widget src="hack.near/widget/BuilderHat" props={{ accountId }} />
+          </div>
+          <div className="text-truncate text-muted">
+            <small>
+              <span className="font-monospace">@{accountId}</span>
+            </small>
           </div>
         </div>
       </div>
-    </a>
+    </div>
   </>
 );
