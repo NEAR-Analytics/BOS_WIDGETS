c96adeeb4b78ca0ac713f7c57ea4fc609e43d962
megha19.near <megha19.near>
2024-05-06 08:29:29 +0000

    Update CheddarMazeReactV1 by megha19.near at 2024-05-06T08:29:29.695Z

--- megha19.near/CheddarMazeReactV1/source_code.jsx
+++ megha19.near/CheddarMazeReactV1/source_code.jsx
@@ -59,7 +59,8 @@ useEffect(() => {
   ) {
     const balance = Big(cheddarBalance).div(Big(10).pow(24)).toFixed();
     setCheddarBalance(balance);
-    setAllowedGame(parseFloat(balance) >= 555 && isUserNadaVerified);
+    // setAllowedGame(parseFloat(balance) >= 555 && isUserNadaVerified);
+    setAllowedGame(true);
   }
 }, [cheddarBalance, isUserNadaVerified]);
 
