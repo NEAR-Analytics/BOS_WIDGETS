20cfa0a4e428f13a8d5d7c27b8329c9e0a4f9983
megha19.near <megha19.near>
2024-05-06 09:21:36 +0000

    Update includes.common by megha19.near at 2024-05-06T09:21:36.133Z

--- megha19.near/includes.common/source_code.jsx
+++ megha19.near/includes.common/source_code.jsx
@@ -1,8 +1,14 @@
-const REPL_TREASURY_CONTRACT = "treasurydevhub.near";
-const REPL_TREASURY = "megha19.near";
-// "dashboard.treasury-devdao.near";
 const REPL_DEVHUB = "devhub.near";
-// devhub.near;
-const REPL_PROPOSAL_CONTRACT =
-  "713ed9aef61d14ce3dfeb3f5a55dfdf16c407280267e8de96bce0953d0e1af8c";
+const REPL_INFRASTRUCTURE_COMMITTEE = "infrastructure-committee.near";
+const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
+const REPL_RPC_URL = "https://rpc.mainnet.near.org";
 const REPL_NEAR = "near";
+const RFPImage =
+  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
+
+const TIMELINE_STATUS = {
+  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
+  EVALUATION: "EVALUATION",
+  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
+  CANCELLED: "CANCELLED",
+};
