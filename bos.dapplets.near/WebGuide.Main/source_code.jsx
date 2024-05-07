3fc649096d62ad00ee61f0a67422da6827b41293
bos.dapplets.near <bos.dapplets.near>
2024-05-07 16:14:43 +0000

    Update WebGuide.Main by bos.dapplets.near at 2024-05-07T16:14:43.141Z

--- bos.dapplets.near/WebGuide.Main/source_code.jsx
+++ bos.dapplets.near/WebGuide.Main/source_code.jsx
@@ -130,13 +130,10 @@ const guideConfig = {
     {
       id: "bos.dapplets.near/app/Tipping/2",
       type: 'callout',
-      namespace: "bos.dapplets.near/parser/twitter",
-      contextType: "post",
-      injectTo: "southPanel",
-      if: { id: { eq: "1694995344461894022" } },
-      insteadOf: {
-        linkId: "2dec2fe13441c1432d7a881e9cb58659",
-      },
+      namespace: "naive-bos",
+      contextType: "bos.dapplets.near--DefaultLayoutManager",
+      injectTo: "bos.dapplets.near--Tipping.Main",
+      arrowTo: "insPoint",
       pages: [
         {
           id: "bos.dapplets.near/app/Tipping/2/1",
