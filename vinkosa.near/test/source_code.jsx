02b2a3b51afab5e2cf10569aeb0972e53f02ea61
vinkosa.near <vinkosa.near>
2024-05-09 12:01:05 +0000

    Update test by vinkosa.near at 2024-05-09T12:01:05.779Z

---
+++ vinkosa.near/test/source_code.jsx
@@ -0,0 +1,11 @@
+const [text, setText] = useState("hei123");
+
+return (
+  <>
+    noe tekst {text}
+    <input
+      value={text}
+      onChange={(event) => setText(event.target.value)}
+    ></input>
+  </>
+);
