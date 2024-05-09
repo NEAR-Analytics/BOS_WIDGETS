a7267a1b82a6b20c21f07e489b18dc064d0bc67d
vinkosa.near <vinkosa.near>
2024-05-09 12:12:31 +0000

    Update test by vinkosa.near at 2024-05-09T12:12:31.121Z

--- vinkosa.near/test/source_code.jsx
+++ vinkosa.near/test/source_code.jsx
@@ -1,5 +1,5 @@
 const [text, setText] = useState("hei123");
-
+const [t2, setT2] = useState("hade123");
 return (
   <>
     noe tekst {text}
@@ -7,5 +7,7 @@ return (
       value={text}
       onChange={(event) => setText(event.target.value)}
     ></input>
+    <p>enda mer tekst {t2} </p>
+    <input value={t2} onChange={(event) => setT2(event.target.value)}></input>
   </>
 );
