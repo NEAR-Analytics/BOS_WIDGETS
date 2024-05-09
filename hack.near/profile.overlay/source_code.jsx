273aa7810c01f0f7c4ce72d7abd9e97f2be695ba
hack.near <hack.near>
2024-05-09 07:05:31 +0000

    Update profile.overlay by hack.near at 2024-05-09T07:05:31.701Z

--- hack.near/profile.overlay/source_code.jsx
+++ hack.near/profile.overlay/source_code.jsx
@@ -3,6 +3,7 @@ const accountId = props.accountId ?? "every.near";
 return (
   <div className="m-3 gap 2 d-flex flex-row justify-content-between align-items-center">
     <Widget src="hack.near/widget/profile" props={{ accountId }} />
+    <br />
     <Widget src="hack.near/widget/attest" props={{ accountId }} />
   </div>
 );
