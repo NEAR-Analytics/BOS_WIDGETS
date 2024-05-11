ddb64b39f951805e673d80433c6b0079b6fea8cc
hack.near <hack.near>
2024-05-11 19:33:45 +0000

    Update explorer by hack.near at 2024-05-11T19:33:45.647Z

--- hack.near/Explorer/source_code.jsx
+++ hack.near/Explorer/source_code.jsx
@@ -1,25 +1,58 @@
-State.init({
-  path: "self.social.near/profile/**",
-});
+const [typesObject, setTypesObject] = useState(
+  Social.get("*/type/*", "final") || null
+);
 
-const value = Social.get(state.path, "final");
+const [creator, setCreator] = useState("");
 
-const text = `
-\`\`\`json
-${JSON.stringify(value, undefined, 2)}
-\`\`\`
-`;
+useEffect(() => {
+  function fetchTypes() {
+    const data = Social.get("*/type/*", "final");
+    setTypesObject(data);
+  }
+  fetchTypes();
+}, []);
+
+const typeCreators = Object.keys(typesObject);
+
+const onChangeInput = (creator) => {
+  setCreator(creator);
+};
+
+const filteredTypes = typeCreators.filter(
+  (type) => type.indexOf(creator) !== -1
+);
+
+const total_types = typeCreators.length;
+
+const filtered_types = filteredTypes.length;
 
 return (
-  <div>
-    <h1>Explorer</h1>
-    <div>
+  <>
+    <div className="m-2">
+      <h3 className="mb-3">Explore Types</h3>
       <input
         type="text"
-        value={state.path}
-        placeholder="self.social.near/profile/**"
+        value={creator}
+        onChange={(e) => onChangeInput(e.target.value)}
+        placeholder="Search by Account ID"
       />
+      <div className="m-1 mt-3">
+        <h5 className="mb-2">Creators</h5>
+        {!creator ? (
+          <p>
+            <b>Total:</b> {total_types}
+          </p>
+        ) : (
+          <p>
+            <b>Filtered:</b> {filtered_types}
+          </p>
+        )}
+        <ul>
+          {filteredTypes.map((type, index) => (
+            <li key={index}>{type}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-    <Markdown text={text} />
-  </div>
+  </>
 );
