2a50b2100d782c88a8e8366d634b9d2b3103357d
uiisnear.near <uiisnear.near>
2024-05-12 12:00:35 +0000

    Update sheet by uiisnear.near at 2024-05-12T12:00:35.237Z

--- uiisnear.near/sheet/source_code.jsx
+++ uiisnear.near/sheet/source_code.jsx
@@ -84,7 +84,7 @@ const SheetContent = ({ className, children, ...props }) => (
       {...props}
     >
       {children}
-      <Dialog.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-uin-secondary">
+      <Dialog.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <i class="bi bi-x h-4 w-4"></i>
         <span className="sr-only">Close</span>
       </Dialog.Close>
