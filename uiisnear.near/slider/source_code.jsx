b9c9d2fcb641a3705a0744e6b9ea6d8241b7d6aa
uiisnear.near <uiisnear.near>
2024-05-12 15:38:45 +0000

    Update slider by uiisnear.near at 2024-05-12T15:38:45.010Z

--- uiisnear.near/slider/source_code.jsx
+++ uiisnear.near/slider/source_code.jsx
@@ -1,30 +1,17 @@
-const sliderClassname =
-  "relative flex w-full touch-none select-none items-center justify-center w-1.5";
+const sliderRootClassname =
+  "relative flex w-full touch-none select-none items-center";
 
-const SliderRoot = ({ className, orientation, ...props }) => {
-  orientation = orientation ?? "horizontal";
+const SliderRoot = ({ className, ...props }) => (
+  <Slider.Root
+    ref="forwardedRef"
+    className={className ?? sliderRootClassname}
+    {...props}
+  >
+    <Slider.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-uin-primary/20">
+      <Slider.Range className="absolute h-full bg-uin-primary" />
+    </Slider.Track>
+    <Slider.Thumb className="block h-4 w-4 rounded-full border border-uin-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+  </Slider.Root>
+);
 
-  return (
-    <Slider.Root
-      ref="forwardedRef"
-      className={className ?? sliderClassname}
-      orientation={orientation}
-      {...props}
-    >
-      <Slider.Track
-        className={`relative ${
-          orientation === "vertical" ? "h-full" : "w-full h-1.5"
-        } grow overflow-hidden rounded-full bg-uin-primary/20`}
-      >
-        <Slider.Range
-          className={`absolute ${
-            orientation === "vertical" ? "w-full" : "h-full"
-          } bg-uin-primary`}
-        />
-      </Slider.Track>
-      <Slider.Thumb className="block h-4 w-4 rounded-full border border-uin-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
-    </Slider.Root>
-  );
-};
-
-return { Slider: SliderRoot, sliderClassname };
+return { Slider: SliderRoot, sliderRootClassname };
