ff77a9993bcc96e46c93b16aafaa830cb057b1c1
hack.near <hack.near>
2024-05-13 02:42:24 +0000

    Update profile.create by hack.near at 2024-05-13T02:42:24.221Z

---
+++ hack.near/profile.create/source_code.jsx
@@ -0,0 +1,107 @@
+const accountId = props.accountId ?? context.accountId ?? "every.near";
+
+const profile = props.profile ?? Social.getr(`${accountId}/profile`);
+const fast = props.fast ?? !props.profile;
+
+const name = profile.name;
+const image = profile.image;
+
+State.init({ img: null });
+
+const uploadFileUpdateState = (body) => {
+  asyncFetch("https://ipfs.near.social/add", {
+    method: "POST",
+    headers: { Accept: "application/json" },
+    body,
+  }).then((res) => {
+    const cid = res.body.cid;
+    State.update({ img: { cid } });
+  });
+};
+
+const filesOnChange = (files) => {
+  if (files) {
+    State.update({ img: { uploading: true, cid: null } });
+    uploadFileUpdateState(files[0]);
+  }
+};
+
+const imgWrapperStyle = { height: "3em", width: "3em" };
+
+const [profileName, setProfileName] = useState("");
+
+const data = {
+  profile: {
+    name: profileName,
+    image: {
+      ipfs_cid: state.img.cid,
+    },
+  },
+};
+
+const handleSave = () => {
+  Social.set(data);
+};
+
+return (
+  <div className="d-flex flex-row">
+    <div className="me-2 d-inline-block">
+      {state.img || image ? (
+        <img
+          className="rounded me-1"
+          style={{
+            height: "3em",
+            width: "3em",
+            objectFit: "cover",
+          }}
+          src={`https://ipfs.near.social/ipfs/${
+            state.img.cid || image.ipfs_cid
+          }`}
+          alt="profile"
+        />
+      ) : (
+        <Files
+          multiple={false}
+          accepts={["image/*"]}
+          minFileSize={1}
+          clickable
+          className="btn btn-sm mt-2"
+          onChange={filesOnChange}
+        >
+          {state.img?.uploading ? <></> : <i class="bi bi-pencil-fill"></i>}
+        </Files>
+      )}
+    </div>
+    <div className="text-truncate">
+      <div className="text-truncate">
+        <span className="fw-bold me-1">
+          {name ? (
+            name
+          ) : (
+            <div className="m-1 d-flex flex-row">
+              <input
+                type="text"
+                placeholder="input name"
+                value={profileName}
+                onChange={(e) => setProfileName(e.target.value)}
+              />
+              <div>
+                <button onClick={handleSave} className="ms-3 mt-1 btn-sm">
+                  Save
+                </button>
+              </div>
+            </div>
+          )}
+        </span>
+        <Widget src="hack.near/widget/BuilderHat" props={{ accountId }} />
+      </div>
+      {context.accountId && name && (
+        <small>
+          <span className="text-truncate text-muted font-monospace">
+            @{accountId}
+          </span>
+        </small>
+      )}
+    </div>
+  </div>
+);
