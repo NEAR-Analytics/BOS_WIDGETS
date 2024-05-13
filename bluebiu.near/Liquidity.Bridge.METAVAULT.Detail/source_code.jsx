3802945a4d8e0df86a5bcb8fb4d6a0d1d472611e
bluebiu.near <bluebiu.near>
2024-05-13 08:45:48 +0000

    Update Liquidity.Bridge.METAVAULT.Detail by bluebiu.near at 2024-05-13T08:45:48.573Z

--- bluebiu.near/Liquidity.Bridge.METAVAULT.Detail/source_code.jsx
+++ bluebiu.near/Liquidity.Bridge.METAVAULT.Detail/source_code.jsx
@@ -28,16 +28,9 @@ const {
   BalancePrice,
   StyledButtonList,
   StyledButton,
-  StyledLoading
 } = VM.require('bluebiu.near/widget/Liquidity.Handler.Styles')
 
 
-const iconCircle = (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-    <path d="M8 15C11.866 15 15 11.866 15 8C15 4.13401 11.866 1 8 1C4.13401 1 1 4.13401 1 8" stroke="#1E2028" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-  </svg>
-
-)
 const defaultDeposit = props.tab === "deposit" || !props.tab;
 
 // const curPositionUSD = userPositions[data.vaultAddress]?.balanceUSD;
@@ -434,7 +427,7 @@ return (
               <StyledButton disabled={isLoading || !amount} onClick={handleDeposit}>
                 {
                   isLoading ? (
-                    <StyledLoading>{iconCircle}</StyledLoading>
+                    <Widget src="bluebiu.near/widget/Liquidity.Bridge.Loading" />
                   ) : (
                     "Deposit"
                   )
@@ -444,7 +437,7 @@ return (
               <>
                 <StyledButton disabled={isTokenApproved || isTokenApproving} onClick={() => handleApprove(true)}>{
                   isTokenApproving ? (
-                    <StyledLoading>{iconCircle}</StyledLoading>
+                    <Widget src="bluebiu.near/widget/Liquidity.Bridge.Loading" />
                   ) : (
                     <>
                       {isTokenApproved ? "Approved" : "Approve"} {token}
@@ -509,7 +502,7 @@ return (
             onClick={handleWithdraw}
           >
             {isLoading ? (
-              <StyledLoading>{iconCircle}</StyledLoading>
+              <Widget src="bluebiu.near/widget/Liquidity.Bridge.Loading" />
             ) : (
               <>
                 {isWithdrawInsufficient ? "InSufficient Balance" : "Withdraw"}
