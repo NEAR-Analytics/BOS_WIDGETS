92c7c72933014fabb077e4d010534d3234cd9704
bluebiu.near <bluebiu.near>
2024-05-13 09:17:38 +0000

    Update Liquidity.Connector.ARRAKIS by bluebiu.near at 2024-05-13T09:17:38.591Z

--- bluebiu.near/Liquidity.Connector.ARRAKIS/source_code.jsx
+++ bluebiu.near/Liquidity.Connector.ARRAKIS/source_code.jsx
@@ -222,7 +222,7 @@ const columnList = [{
     return (
       <>
         <TdTxt>{Big(data?.liquidity ?? 0).gt(0) ? `${formatFiat(data.liquidity)}` : "-"}</TdTxt>
-        {Big(data?.balance ?? 0).gt(0) && <TdTxt className="gray">{data.balance} LP</TdTxt>}
+        {Big(data?.balance ?? 0).gt(0) && <TdTxt className="gray">{Big(data?.balance ?? 0).lt(0.01) ? '<0.01' : Big(data.balance).toFixed(2)} LP</TdTxt>}
         <SvgIcon className={["icon-right", index === state.dataIndex ? "rotate" : ""]}>
           {IconRight}
         </SvgIcon>
