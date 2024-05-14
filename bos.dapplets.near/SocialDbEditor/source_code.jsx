c2bc45259fd432688687d5a20443d06c2d1251d1
bos.dapplets.near <bos.dapplets.near>
2024-05-14 19:52:44 +0000

    Update SocialDbEditor by bos.dapplets.near at 2024-05-14T19:52:44.844Z

--- bos.dapplets.near/SocialDbEditor/source_code.jsx
+++ bos.dapplets.near/SocialDbEditor/source_code.jsx
@@ -12,7 +12,7 @@ const [modifiedJson, setModifiedJson] = useState("");
 
 useEffect(() => {
   setModifiedJson(JSON.stringify(originalData, null, 2));
-}, [originalData, isJson]);
+}, [originalData]);
 
 const isArray = (a) => Array.isArray(a);
 
@@ -50,7 +50,7 @@ const handleSaveClick = () => {
 };
 
 return (
-  <div class="container">
+  <div>
     <div style={{ marginBottom: "1rem" }}>
       <label for="query">Contract ID</label>
       <input
@@ -68,7 +68,6 @@ return (
         className="form-control"
         value={modifiedJson}
         onChange={(e) => setModifiedJson(e.target.value)}
-        style={{ fontFamily: "monospace", fontSize: 14 }}
         rows="15"
       ></textarea>
     </div>
