6c87e61d313a73db486069bb3169f6a938ecf7b7
bos.dapplets.near <bos.dapplets.near>
2024-05-14 20:09:50 +0000

    Update WebGuideTest.Main by bos.dapplets.near at 2024-05-14T20:09:50.284Z

--- bos.dapplets.near/WebGuideTest.Main/source_code.jsx
+++ bos.dapplets.near/WebGuideTest.Main/source_code.jsx
@@ -151,6 +151,8 @@ const handleClickNext = () => {
 }
 
 const ChapterWrapper = (props) => {
+  console.log(props)
+  
   const currentChapter = guideConfig.chapters[chapterCounter]
   if (!currentChapter) return <></>
   const pages = currentChapter.pages
