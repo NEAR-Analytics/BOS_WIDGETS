42d4fbf0d0f3673cbc334af71c89fb3e618a79cb
buildhub.near <buildhub.near>
2024-05-14 07:23:45 +0000

    Update components.modals.propose.Transfer by buildhub.near at 2024-05-14T07:23:45.124Z

--- buildhub.near/components.modals.propose.Transfer/source_code.jsx
+++ buildhub.near/components.modals.propose.Transfer/source_code.jsx
@@ -2,9 +2,6 @@ const { Button } = VM.require("buildhub.near/widget/components") || {
   Button: () => <></>,
 };
 const DaoSDK = VM.require("sdks.near/widget/SDKs.Sputnik.DaoSDK") || (() => {});
-if (!DaoSDK) {
-  return <></>;
-}
 const [recipient, setRecipient] = useState("");
 const [token, setToken] = useState("");
 const [amount, setAmount] = useState(0);
@@ -25,6 +22,9 @@ useEffect(() => {
 const memoizedKey = useMemo((editorKey) => editorKey, [editorKey]);
 const selectedDAO = props.selectedDAO;
 const sdk = DaoSDK(selectedDAO);
+if (!sdk) {
+  return <></>;
+}
 const res = fetch(`https://api.nearblocks.io/v1/account/${selectedDAO}/tokens`);
 const NearTokenId = "NEAR";
 const tokensData = [
