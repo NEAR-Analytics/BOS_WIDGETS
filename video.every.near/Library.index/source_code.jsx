2e65ff4a187623594a912e3ee3ca4e6e57abd92f
video.every.near <video.every.near>
2024-05-15 16:58:40 +0000

    Update Library.index by video.every.near at 2024-05-15T16:58:40.531Z

--- video.every.near/Library.index/source_code.jsx
+++ video.every.near/Library.index/source_code.jsx
@@ -58,6 +58,14 @@ const tabs = {
       label: "Player Sandbox",
       widget: "video.every.near/widget/Library.PlayerSandbox",
     },
+    {
+      label: "Backend Integration",
+      widget: "video.every.near/widget/Library.GetAssets",
+    },
+    {
+      label: "Backend Uploader",
+      widget: "video.every.near/widget/Library.FileUploader",
+    },
   ],
   broadcast: [
     {
@@ -71,6 +79,10 @@ const tabs = {
     {
       label: "Broadcast ApiKey",
       widget: "video.every.near/widget/Library.BroadcastApiKey",
+    },
+    {
+      label: "Watch Stream",
+      widget: "video.every.near/widget/Library.WatchStream",
     },
     {
       label: "Broadcast Sandbox",
