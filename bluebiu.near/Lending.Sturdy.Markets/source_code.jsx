503d168637958f03944f436749f0fec2483517a1
bluebiu.near <bluebiu.near>
2024-05-15 06:24:28 +0000

    Update Lending.Sturdy.Markets by bluebiu.near at 2024-05-15T06:24:28.909Z

---
+++ bluebiu.near/Lending.Sturdy.Markets/source_code.jsx
@@ -0,0 +1,93 @@
+const StyledContainer = styled.div``;
+
+const {
+  markets,
+  addAction,
+  toast,
+  chainId,
+  nativeCurrency,
+  dexConfig,
+  onSuccess,
+  account,
+  prices,
+} = props;
+
+console.log("Markets--", props, markets);
+
+const COLUMNS = [
+  {
+    key: "Collateral",
+    label: "Collateral",
+    width: "16%",
+    type: "TOKEN_WITH_ICON",
+  },
+  {
+    key: "Borrow",
+    label: "Borrow",
+    width: "16%",
+    type: "TOKEN_WITH_ICON",
+  },
+  {
+    key: "totalSupplied",
+    label: "Total Supplied",
+    width: "16%",
+    type: "AMOUNT",
+  },
+  {
+    key: "collateralAPY",
+    label: "Collateral APY",
+    width: "16%",
+    type: "PERCENT",
+  },
+  {
+    key: "borrowAPR",
+    label: "Borrow APR",
+    width: "16%",
+    type: "PERCENT",
+  },
+  {
+    key: "Utilization",
+    label: "Utilization",
+    width: "16%",
+    type: "PERCENT",
+  },
+
+  // {
+  //   key: "Debt",
+  //   label: "Debt",
+  //   width: "16%",
+  // },
+  // {
+  //   key: "Rate",
+  //   label: "Rate",
+  //   width: "16%",
+  // },
+  {
+    key: "handler",
+    width: "4%",
+    type: "HANDLER",
+  },
+];
+
+return (
+  <StyledContainer>
+    <Widget
+      src="bluebiu.near/widget/Lending.MarketHeader"
+      props={{
+        columns: COLUMNS,
+      }}
+    />
+    {markets.map((record, index) => (
+      <Widget
+        key={index}
+        src="bluebiu.near/widget/Lending.Sturdy.MarketRow"
+        props={{
+          ...props,
+          columns: COLUMNS,
+          data: record,
+          from: "MARKETS",
+        }}
+      />
+    ))}
+  </StyledContainer>
+);
