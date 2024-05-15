b9606aa2a6d8e16d3b564173a33e003d0f1e7774
bos.dapplets.near <bos.dapplets.near>
2024-05-15 07:15:26 +0000

    Update SocialDbEditor by bos.dapplets.near at 2024-05-15T07:15:26.355Z

--- bos.dapplets.near/SocialDbEditor/source_code.jsx
+++ bos.dapplets.near/SocialDbEditor/source_code.jsx
@@ -50,7 +50,7 @@ const handleSaveClick = () => {
 };
 
 return (
-  <div>
+  <div class="container">
     <div style={{ marginBottom: "1rem" }}>
       <label for="query">Contract ID</label>
       <input
@@ -68,6 +68,7 @@ return (
         className="form-control"
         value={modifiedJson}
         onChange={(e) => setModifiedJson(e.target.value)}
+        style={{ fontFamily: "monospace", fontSize: 14 }}
         rows="15"
       ></textarea>
     </div>
