5c93c74ad08b85e144a6b3773617dfec096d2be5
thomasguntenaar.near <thomasguntenaar.near>
2024-05-15 08:41:05 +0000

    Update devhub.entity.addon.blogv2.editor.layout by thomasguntenaar.near at 2024-05-15T08:41:05.120Z

--- thomasguntenaar.near/devhub.entity.addon.blogv2.editor.layout/source_code.jsx
+++ thomasguntenaar.near/devhub.entity.addon.blogv2.editor.layout/source_code.jsx
@@ -1,14 +1,23 @@
-const { Sidebar, Content, editData, onSubmit, BlogPostSettings } = props;
+const {
+  BlogOverview,
+  data: processedBlogData,
+  Content,
+  parametersData,
+  onSubmit,
+  BlogPostSettings,
+} = props;
 
-// Selected Item is the property "parameters" of the community addon (blog)
-const [selectedItem, setSelectedItem] = useState(editData);
-const [showEditor, setShowEditor] = useState(false);
-const [showBlogPostSettings, setBlogPostSettings] = useState(false);
+const [selectedItem, setSelectedItem] = useState(null);
+const [showScreen, setShowScreen] = useState("overview"); // overview, editor, settings
+
+const openBlogPostSettings = () => {
+  setShowScreen(true);
+};
 
 const handleItemClick = (item) => {
   if (item) {
     setSelectedItem(item);
-    setShowEditor(true);
+    setShowScreen("editor");
   } else {
     setSelectedItem(null);
   }
@@ -16,90 +25,49 @@ const handleItemClick = (item) => {
 
 const goBack = () => {
   setSelectedItem(null);
-  setShowEditor(false);
+  setShowScreen("overview");
 };
 
-// TODO Move to layout
-const openBlogPostSettings = () => {
-  // TODO 599
-  setBlogPostSettings(true);
-};
-
-const saveBlogPostSettings = () => {
-  // TODO 599 move to provider
-  console.log("Implement saving blog settings issue 599");
-};
-
-const handlePublish = (status) => {
-  onSubmit &&
-    onSubmit(
-      {
-        id: data.id || undefined,
-        title,
-        subtitle,
-        description,
-        date,
-        status,
-        content,
-        author,
-        category,
-        community: handle, // TODO remove this?
-      },
-      data.id !== undefined
-    );
-};
-
-const openAnalytics = () => {
-  window.open("https://eu.posthog.com/project/20896", "_blank");
-};
+const postHogHref = "https://eu.posthog.com/project/20896";
 
 return (
   <div style={{ width: "100%", height: "100%" }}>
-    {showBlogPostSettings ? (
-      <div>
-        {/* TODO move this to BlogPostSettings */}
-        <div className="d-flex gap-1 align-items-end justify-content-end">
-          <button
-            className="btn btn-light"
-            onClick={() => setBlogPostSettings(false)}
-          >
-            Cancel
-          </button>
-          <Widget
-            src={"thomasguntenaar.near/widget/devhub.components.molecule.BlogControl"}
-            props={{
-              title: "Save Settings",
-              onClick: saveBlogPostSettings,
-            }}
-          />
-        </div>
-        <BlogPostSettings />
-      </div>
+    {showScreen === "settings" ? (
+      <BlogPostSettings onHideSettings={() => setShowScreen("overview")} />
     ) : (
       <>
-        {showEditor ? null : (
-          <div className="d-flex gap-1 align-items-end justify-content-end w-100 mb-4">
-            {/* TODO PETER <input type="text" placeholder="Search blog posts" />
-            <button className="btn btn-secondary">Filter</button>
-            END PETER */}
-            <button className="btn btn-light" onClick={openAnalytics}>
-              Analytics
-            </button>
-            <button className="btn btn-light" onClick={openBlogPostSettings}>
-              Settings
-            </button>
-            <Widget
-              src={
-                "thomasguntenaar.near/widget/devhub.components.molecule.BlogControl"
-              }
-              props={{
-                title: "New Blog Post",
-                onClick: () => {
-                  handleItemClick("new");
-                  setShowEditor(true);
-                },
-              }}
-            />
+        {showScreen === "editor" ? null : (
+          <div className="flex items-center justify-between w-100 mb-4">
+            <div className="">Blog Posts</div>
+            <div className="flex items-end justify-end gap-x-1">
+              <Link
+                className="rounded-md bg-devhub-green-light px-3.5 py-2.5 text-sm font-semibold text-devhub-green hover:text-white shadow-sm hover:bg-indigo-100"
+                href={postHogHref}
+                target="_blank"
+              >
+                Analytics
+              </Link>
+              {/* <button
+                onClick={() => setShowScreen("settings")}
+                type="button"
+                className="rounded-md bg-devhub-green-light px-3.5 py-2.5 text-sm font-semibold text-devhub-green hover:text-white shadow-sm hover:bg-indigo-100"
+              >
+                Settings
+              </button> */}
+              <Widget
+                src={
+                  "thomasguntenaar.near/widget/devhub.components.molecule.BlogControl"
+                }
+                props={{
+                  title: "New Blog Post",
+                  onClick: () => {
+                    handleItemClick("new");
+                    setShowScreen("editor");
+                  },
+                  testId: "new-blog-post-button",
+                }}
+              />
+            </div>
           </div>
         )}
         <div
@@ -109,16 +77,16 @@ return (
           <div
             className="left-panel"
             style={{
-              width: showEditor ? "20%" : "100%",
+              width: showScreen === "editor" ? "20%" : "100%",
             }}
           >
-            <Sidebar
+            <BlogOverview
               selectedItem={selectedItem}
               handleItemClick={handleItemClick}
-              hideColumns={showEditor}
+              hideColumns={showScreen === "editor"}
             />
           </div>
-          {showEditor && (
+          {showScreen === "editor" && (
             <div
               className="right-panel"
               style={{ flex: 1, width: 0, overflow: "scroll" }}
