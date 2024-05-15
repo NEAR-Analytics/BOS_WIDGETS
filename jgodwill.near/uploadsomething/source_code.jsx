88a44e7ced5d0b0fb086f26e9997174581743f01
jgodwill.near <jgodwill.near>
2024-05-15 06:28:33 +0000

    Update uploadsomething by jgodwill.near at 2024-05-15T06:28:33.827Z

---
+++ jgodwill.near/uploadsomething/source_code.jsx
@@ -0,0 +1,37 @@
+const [img, setImg] = useState(null);
+const { UploadField } = VM.require("buildhub.near/widget/components");
+const [msg, setMsg] = useState(<UploadField />);
+
+const uploadFile = (files) => {
+  setMsg("Uploading...");
+
+  const file = fetch("https://ipfs.near.social/add", {
+    method: "POST",
+    headers: { Accept: "application/json" },
+    body: files[0],
+  });
+
+  setImg(file.body.cid);
+  //   setMsg("Upload an Image");
+};
+
+return (
+  <>
+    <Files
+      multiple={false}
+      accepts={["image/*"]}
+      clickable
+      className=""
+      onChange={uploadFile}
+    >
+      {msg}
+    </Files>
+    {img ? (
+      <div>
+        <img src={`https://ipfs.near.social/ipfs/${img}`} />
+      </div>
+    ) : (
+      ""
+    )}
+  </>
+);
