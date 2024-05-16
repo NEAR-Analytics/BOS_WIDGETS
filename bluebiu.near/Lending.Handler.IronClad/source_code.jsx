c8c3d6f02aaef5627d30f4f976bc784508e5c09b
bluebiu.near <bluebiu.near>
2024-05-16 18:18:59 +0000

    Update Lending.Handler.IronClad by bluebiu.near at 2024-05-16T18:18:59.734Z

--- bluebiu.near/Lending.Handler.IronClad/source_code.jsx
+++ bluebiu.near/Lending.Handler.IronClad/source_code.jsx
@@ -315,7 +315,7 @@ const wethGateWayAbi = [
   },
 ];
 
-const { update, data, amount, account, onLoad } = props;
+const { update, data, config, amount, account, onLoad } = props;
 
 useEffect(() => {
   const isCollateral = data.actionText.includes("Collateral");
@@ -392,6 +392,7 @@ useEffect(() => {
       Ethers.provider().getSigner()
     );
   }
+
   if (isCollateral) {
     if (!data.config.lendingPoolAddress || !data.underlyingToken) return;
     const isEnter = data.actionText === "Enable as Collateral";
@@ -407,6 +408,62 @@ useEffect(() => {
   }
   if (!contract) return;
   console.log("HANDLER: ", contract, method, params, options, isETH);
+
+  function handleApprove(tokenAddress, spender, amount) {
+    const TokenContract = new ethers.Contract(
+      tokenAddress,
+      [
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "spender",
+              type: "address",
+            },
+            {
+              internalType: "uint256",
+              name: "amount",
+              type: "uint256",
+            },
+          ],
+          name: "approve",
+          outputs: [
+            {
+              internalType: "bool",
+              name: "",
+              type: "bool",
+            },
+          ],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+      Ethers.provider().getSigner()
+    );
+    console.info("to approve: ", tokenAddress, spender, amount);
+
+    return TokenContract.approve(spender, amount)
+      .then((tx) => {
+        tx.wait()
+          .then((res) => {
+            const { status, transactionHash } = res;
+
+            if (status === 1) {
+              return true;
+            } else {
+              toast.fail?.({
+                title: "Transaction Failed!",
+                text: `transactionHash ${transactionHash}`,
+              });
+              return false;
+            }
+          })
+          .finally(() => {});
+      })
+      .catch((err) => {
+        console.info("approve_error: ", err);
+      });
+  }
 
   const createTx = (gas) => {
     const _gas = gas ? Big(gas.toString()).mul(1.2).toFixed(0) : 4000000;
@@ -425,14 +482,20 @@ useEffect(() => {
         onLoad({});
       });
   };
-  contract.estimateGas[method](...params, options)
-    .then((gas) => {
-      createTx(gas);
-    })
-    .catch((err) => {
-      console.log("estimateGas", err);
-      createTx();
-    });
+
+  if (data.actionText === "Withdraw" && isETH) {
+    const _parsedAmount = ethers.utils.parseUnits(
+      amount,
+      data.underlyingToken.decimals
+    );
+    handleApprove(data.address, data.config.wethGateway, _parsedAmount).then(
+      () => {
+        createTx();
+      }
+    );
+  } else {
+    createTx();
+  }
 }, [update]);
 
 return "";
