9195ec90c5ec9deb708a520814acc02958424140
abdullahi3000.near <abdullahi3000.near>
2024-05-17 10:14:46 +0000

    Update config.nav by abdullahi3000.near at 2024-05-17T10:14:46.540Z

---
+++ abdullahi3000.near/config.nav/source_code.jsx
@@ -0,0 +1,19 @@
+return {
+  type: "app",
+  routes: {
+    home: {
+      path: "abdullahi3000.near/widget/page.home",
+      blockHeight: "final",
+      init: {
+        name: "Help",
+      },
+    },
+    governance: {
+      path: "abdullahi3000.near/widget/trialDropDeploy",
+      blockHeight: "final",
+      init: {
+        name: "Create",
+      },
+    },
+  },
+};
