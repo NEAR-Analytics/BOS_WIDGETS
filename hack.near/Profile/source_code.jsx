d5b2bf19c6877bb146f5c316215f409d5f531158
hack.near <hack.near>
2024-05-18 17:10:24 +0000

    Update profile by hack.near at 2024-05-18T17:10:24.406Z

--- hack.near/Profile/source_code.jsx
+++ hack.near/Profile/source_code.jsx
@@ -9,33 +9,25 @@ const hideInfo = props.hideInfo ?? false;
 
 return (
   <div className="profile d-inline-block">
-    <a
-      href={`#/mob.near/widget/ProfilePage?accountId=${accountId}`}
-      className="text-decoration-none link-dark"
-    >
-      <Widget
-        src="mob.near/widget/ProfileImage"
-        props={{
-          profile,
-          accountId,
-          className: "float-start d-inline-block me-2",
-        }}
-      />
-      {!hideInfo && (
-        <div
-          className="profile-info d-inline-block"
-          style={{ maxWidth: "16em" }}
-        >
-          <div className="profile-name text-truncate">
-            {name || "No-name profile"}
-          </div>
-          <div className="profile-links d-flex">
-            <div className="d-inline-block profile-account text-secondary text-truncate">
-              @{accountId}
-            </div>
+    <Widget
+      src="mob.near/widget/ProfileImage"
+      props={{
+        profile,
+        accountId,
+        className: "float-start d-inline-block me-2",
+      }}
+    />
+    {!hideInfo && (
+      <div className="profile-info d-inline-block" style={{ maxWidth: "16em" }}>
+        <div className="profile-name text-truncate">
+          {name || "No-name profile"}
+        </div>
+        <div className="profile-links d-flex">
+          <div className="d-inline-block profile-account text-secondary text-truncate">
+            @{accountId}
           </div>
         </div>
-      )}
-    </a>
+      </div>
+    )}
   </div>
 );
