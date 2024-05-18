bb391f5500e0f266c6f61207124b1790434652ee
hack.near <hack.near>
2024-05-18 03:12:15 +0000

    Update profile.create by hack.near at 2024-05-18T03:12:15.339Z

--- hack.near/profile.create/source_code.jsx
+++ hack.near/profile.create/source_code.jsx
@@ -1,11 +1,12 @@
-const accountId = props.accountId ?? context.accountId ?? "buildcommons.near";
+const accountId = props.accountId ?? context.accountId ?? "every.near";
 
-const data = props.profile ?? Social.getr(`${accountId}`);
+const profile = props.profile ?? Social.getr(`${accountId}`);
+const commons = Social.getr(`${accountId}/graph/commons`);
 
 const fast = props.fast ?? !props.data.profile;
 
-const name = data.profile.name;
-const image = data.profile.image;
+const name = profile.name;
+const image = profile.image;
 
 State.init({ img: null });
 
@@ -45,56 +46,62 @@ const handleSave = () => {
 };
 
 return (
-  <div className="d-flex flex-row">
-    <div className="me-2 d-inline-block">
-      {state.img || image ? (
-        <Widget
-          src="hack.near/widget/profile.image"
-          props={{ image, accountId }}
-        />
-      ) : (
-        <Files
-          multiple={false}
-          accepts={["image/*"]}
-          minFileSize={1}
-          clickable
-          className="btn btn-sm mt-2"
-          onChange={filesOnChange}
-        >
-          {state.img?.uploading ? <></> : <i class="bi bi-pencil-fill"></i>}
-        </Files>
-      )}
-    </div>
-    <div className="text-truncate">
-      <div className="text-truncate">
-        <span className="fw-bold me-1">
-          {name ? (
-            name
+  <>
+    {commons ? (
+      <div className="d-flex flex-row">
+        <div className="me-2 d-inline-block">
+          {state.img || image ? (
+            <Widget
+              src="hack.near/widget/profile.image"
+              props={{ image, accountId }}
+            />
           ) : (
-            <div className="m-1 d-flex flex-row">
-              <input
-                type="text"
-                placeholder="input name"
-                value={profileName}
-                onChange={(e) => setProfileName(e.target.value)}
-              />
-              <div>
-                <button onClick={handleSave} className="ms-3 mt-1 btn-sm">
-                  Save
-                </button>
-              </div>
-            </div>
+            <Files
+              multiple={false}
+              accepts={["image/*"]}
+              minFileSize={1}
+              clickable
+              className="btn btn-sm mt-2"
+              onChange={filesOnChange}
+            >
+              {state.img?.uploading ? <></> : <i class="bi bi-pencil-fill"></i>}
+            </Files>
           )}
-        </span>
-        <Widget src="hack.near/widget/BuilderHat" props={{ accountId }} />
+        </div>
+        <div className="text-truncate">
+          <div className="text-truncate">
+            <span className="fw-bold me-1">
+              {name ? (
+                name
+              ) : (
+                <div className="m-1 d-flex flex-row">
+                  <input
+                    type="text"
+                    placeholder="input name"
+                    value={profileName}
+                    onChange={(e) => setProfileName(e.target.value)}
+                  />
+                  <div>
+                    <button onClick={handleSave} className="ms-3 mt-1 btn-sm">
+                      Save
+                    </button>
+                  </div>
+                </div>
+              )}
+            </span>
+            <Widget src="hack.near/widget/BuilderHat" props={{ accountId }} />
+          </div>
+          {name && (
+            <small>
+              <span className="text-truncate text-muted font-monospace">
+                @{accountId}
+              </span>
+            </small>
+          )}
+        </div>
       </div>
-      {name && (
-        <small>
-          <span className="text-truncate text-muted font-monospace">
-            @{accountId}
-          </span>
-        </small>
-      )}
-    </div>
-  </div>
+    ) : (
+      <Widget src="hack.near/widget/attest" props={{ accountId }} />
+    )}
+  </>
 );
