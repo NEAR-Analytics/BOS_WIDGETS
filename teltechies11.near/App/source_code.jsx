b0de702a0d058eb99718409d6df9ca8d463df0d1
teltechies11.near <teltechies11.near>
2024-05-19 03:53:54 +0000

    Update App by teltechies11.near at 2024-05-19T03:53:54.560Z

--- teltechies11.near/App/source_code.jsx
+++ teltechies11.near/App/source_code.jsx
@@ -1,35 +1,53 @@
-const srcDoc = `
-<!DOCTYPE html>
+const srcDoc = `<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title> Homepage </title>
-  <style>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<style>
     body {
       background-color: #171C2C;
       margin: 0;
       height: 100vh;
       overflow: hidden;
-      color: #D4ADFC;
     }
+
     .button {
-      border: 2px solid #FFFFFF;
+      border: 2px solid;
+      border-color: #FFFFFF;
       padding: 8px;
-      cursor: pointer;
     }
+
     .button:hover {
       background-color: #1D267D;
     }
-  </style>
-</head>
-<body>
-  <div id="startPage" style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; gap: 24px;">
-    <h1 style="font-size: 230%; font-family: verdana;"> <i> Teletubby Hackers </i> </h1>
-    <p style="font-family: courier;"> Click below to get started </p>
-    <button class="button" type="button" onclick="window.location.href='start.jsx'"> Click Me! </button>
-  </div>
-</body>
-</html>
-`;
+</style>
 
-return <iframe style={{ width: "100%", height: "100%" }} srcDoc={srcDoc} />;
+<body style="color:#D4ADFC;">
+    <div class="flex flex-col justify-center items-center h-screen space-y-6">
+      <h1 style="font-size:230%; font-family:verdana;"> <i>Teletubby Hackers</i> </h1>
+      <p style="font-family:courier;">Click below to get started</p>
+      <button class="button" type="button" onclick="navigateToLogin()">Click Me!</button>
+      <a href="login.jsx" class="button">Click Me!</a>
+    </div>
+
+    <script>
+      function navigateToLogin() {
+        window.location.href = 'login.jsx';
+      }
+    </script>
+</body>
+
+</html>`;
+
+return (
+  <iframe
+    style={{
+      width: "100%",
+      height: "100%",
+    }}
+    srcDoc={srcDoc}
+  />
+);
