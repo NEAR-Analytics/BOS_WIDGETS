d3c0c3317a4bb39f3ddb4a335a784f94e5379b40
megha19.near <megha19.near>
2024-05-20 10:59:26 +0000

    Update near-prpsls-bos.components.rfps.CommentsAndLogs by megha19.near at 2024-05-20T10:59:26.215Z

--- megha19.near/near-prpsls-bos.components.rfps.CommentsAndLogs/source_code.jsx
+++ megha19.near/near-prpsls-bos.components.rfps.CommentsAndLogs/source_code.jsx
@@ -3,24 +3,93 @@ License: MIT
 Author: devhub.near
 Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
 */
-/* INCLUDE: "includes//common.jsx" */
+/* INCLUDE: "includes/common.jsx" */
 const REPL_DEVHUB = "devhub.near";
 const REPL_INFRASTRUCTURE_COMMITTEE = "megha19.near";
 const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
 const REPL_RPC_URL = "https://rpc.mainnet.near.org";
 const REPL_NEAR = "near";
-const RFPImage =
+const RFP_IMAGE =
   "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
 
-const TIMELINE_STATUS = {
+const RFP_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfps_with_latest_snapshot";
+
+const RFP_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfp_snapshots";
+
+const PROPOSAL_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposals_with_latest_snapshot";
+
+const PROPOSAL_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposal_snapshots";
+const RFP_TIMELINE_STATUS = {
   ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
   EVALUATION: "EVALUATION",
   PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
   CANCELLED: "CANCELLED",
 };
-/* END_INCLUDE: "includes//common.jsx" */
+
+const PROPOSAL_TIMELINE_STATUS = {
+  DRAFT: "DRAFT",
+  REVIEW: "REVIEW",
+  APPROVED: "APPROVED",
+  REJECTED: "REJECTED",
+  CANCELED: "CANCELLED",
+  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
+  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
+  FUNDED: "FUNDED",
+};
+
+const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  return asyncFetch(QUERYAPI_ENDPOINT, {
+    method: "POST",
+    headers: { "x-hasura-role": `polyprogrammist_near` },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
+}
+
+const CANCEL_RFP_OPTIONS = {
+  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
+  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
+  NONE: "NONE",
+};
+
+function parseJSON(json) {
+  if (typeof json === "string") {
+    try {
+      return JSON.parse(json);
+    } catch (error) {
+      return json;
+    }
+  } else {
+    return json;
+  }
+}
+
+function isNumber(value) {
+  return typeof value === "number";
+}
+
+const PROPOSALS_APPROVED_STATUS_ARRAY = [
+  PROPOSAL_TIMELINE_STATUS.APPROVED,
+  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
+  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
+  PROPOSAL_TIMELINE_STATUS.FUNDED,
+];
+/* END_INCLUDE: "includes/common.jsx" */
+
+const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`);
+href || (href = () => {});
 
 const snapshotHistory = props.snapshotHistory;
+const approvedProposals = props.approvedProposals ?? [];
 
 const Wrapper = styled.div`
   position: relative;
@@ -45,15 +114,11 @@ const Wrapper = styled.div`
   }
 
   .inline-flex {
-    display: inline-flex !important;
+    display: -webkit-inline-box !important;
     align-items: center !important;
     gap: 0.25rem !important;
-  }
-
-  @media screen and (max-width: 768px) {
-    .inline-flex {
-      display: -webkit-inline-box !important;
-    }
+    margin-right: 2px;
+    flex-wrap: wrap;
   }
 `;
 
@@ -162,9 +227,15 @@ const Comment = ({ commentItem }) => {
   };
   const content = JSON.parse(Social.get(item.path, blockHeight) ?? "null");
   const link = `https://near.social/${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app?page=rfp&id=${props.id}&accountId=${accountId}&blockHeight=${blockHeight}`;
-  const hightlightComment =
-    parseInt(props.blockHeight ?? "") === blockHeight &&
-    props.accountId === accountId;
+  function getHighlightCommentStyle() {
+    const highlightComment =
+      parseInt(props.blockHeight ?? "") === blockHeight &&
+      props.accountId === accountId;
+
+    return {
+      border: highlightComment ? "2px solid black" : "",
+    };
+  }
 
   return (
     <div style={{ zIndex: 99, background: "white" }}>
@@ -178,7 +249,7 @@ const Comment = ({ commentItem }) => {
           />
         </div>
         <CommentContainer
-          style={{ border: hightlightComment ? "2px solid black" : "" }}
+          style={getHighlightCommentStyle()}
           className="rounded-2 flex-1"
         >
           <Header className="d-flex gap-3 align-items-center p-2 px-3">
@@ -248,6 +319,29 @@ function parseTimelineKeyAndValue(timeline, originalValue, modifiedValue) {
   const newValue = modifiedValue[timeline];
   switch (timeline) {
     case "status":
+      if (newValue === RFP_TIMELINE_STATUS.PROPOSAL_SELECTED) {
+        return (
+          <span className="inline-flex">
+            moved RFP to{" "}
+            <Widget
+              src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.rfps.StatusTag`}
+              props={{
+                timelineStatus: newValue,
+              }}
+            />
+            ･ selected proposal(s) are{" "}
+            {approvedProposals.map((i, index) => (
+              <span>
+                <LinkToProposal id={i.proposal_id}>
+                  {" "}
+                  #{i.proposal_id} {i.name}
+                </LinkToProposal>
+                {index < approvedProposals.length - 1 && ", "}
+              </span>
+            ))}
+          </span>
+        );
+      }
       return (
         oldValue !== newValue && (
           <span className="inline-flex">
@@ -290,6 +384,31 @@ const AccountProfile = ({ accountId }) => {
   );
 };
 
+function symmetricDifference(arr1, arr2) {
+  const diffA = arr1.filter((item) => !arr2.includes(item));
+  const diffB = arr2.filter((item) => !arr1.includes(item));
+  return [...diffA, ...diffB];
+}
+
+const LinkToProposal = ({ id, children }) => {
+  return (
+    <a
+      className="text-decoration-underline flex-1"
+      href={href({
+        widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app`,
+        params: {
+          page: "proposal",
+          id: id,
+        },
+      })}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {children}
+    </a>
+  );
+};
+
 const parseProposalKeyAndValue = (key, modifiedValue, originalValue) => {
   switch (key) {
     case "name":
@@ -299,9 +418,25 @@ const parseProposalKeyAndValue = (key, modifiedValue, originalValue) => {
       return <span>changed {key}</span>;
     case "labels":
       return <span>changed labels to {(modifiedValue ?? []).join(", ")}</span>;
-    // TODO
-    case "linked_proposals":
-      return <span>updated linked proposals</span>;
+    case "linked_proposals": {
+      const newProposals = modifiedValue || [];
+      const oldProposals = originalValue || [];
+      const difference = symmetricDifference(oldProposals, newProposals).join(
+        ","
+      );
+
+      const isUnlinked = oldProposals.length > newProposals.length;
+      const actionText = isUnlinked
+        ? "unlinked a proposal"
+        : "linked a proposal";
+
+      return (
+        <span>
+          {actionText}{" "}
+          <LinkToProposal id={difference}> #{difference}</LinkToProposal>
+        </span>
+      );
+    }
     case "timeline": {
       const modifiedKeys = Object.keys(modifiedValue);
       const originalKeys = Object.keys(originalValue);
