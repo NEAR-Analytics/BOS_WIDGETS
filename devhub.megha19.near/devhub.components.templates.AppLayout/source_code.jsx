cc8a21c725bf581f65eda321683d6ffc8ca795ef
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:27:47 +0000

    Update devhub.components.templates.AppLayout by devhub.megha19.near at 2024-05-21T17:27:47.838Z

--- devhub.megha19.near/devhub.components.templates.AppLayout/source_code.jsx
+++ devhub.megha19.near/devhub.components.templates.AppLayout/source_code.jsx
@@ -26,7 +26,7 @@ const ContentContainer = styled.div`
 
 const AppHeader = ({ page }) => (
   <Widget
-    src="${REPL_DEVHUB}/widget/devhub.components.organism.Navbar"
+    src="devhub.megha19.near/widget/devhub.components.organism.Navbar"
     props={{
       page: page,
       ...props,
@@ -37,7 +37,7 @@ const AppHeader = ({ page }) => (
 const Footer = (props) => {
   return (
     <Widget
-      src="${REPL_DEVHUB}/widget/devhub.components.organism.NewsLetter"
+      src="devhub.megha19.near/widget/devhub.components.organism.NewsLetter"
       props={{
         ...props,
       }}
