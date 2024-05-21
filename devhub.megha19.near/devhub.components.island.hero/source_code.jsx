ac8c4243492048ac14d52a40d972757410afcf5d
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:31:02 +0000

    Update devhub.components.island.hero by devhub.megha19.near at 2024-05-21T17:31:02.177Z

--- devhub.megha19.near/devhub.components.island.hero/source_code.jsx
+++ devhub.megha19.near/devhub.components.island.hero/source_code.jsx
@@ -1,4 +1,4 @@
-const { href } = VM.require("${REPL_DEVHUB}/widget/core.lib.url");
+const { href } = VM.require("devhub.megha19.near/widget/core.lib.url");
 
 href || (href = () => {});
 
@@ -181,7 +181,7 @@ return (
         </Lead>
         <Link
           to={href({
-            widgetSrc: "${REPL_DEVHUB}/widget/app",
+            widgetSrc: "devhub.megha19.near/widget/app",
             params: {
               page: "about",
             },
@@ -199,7 +199,7 @@ return (
       <Lead>Join a vibrant community of innovators shaping the open web.</Lead>
       <Link
         to={href({
-          widgetSrc: "${REPL_DEVHUB}/widget/app",
+          widgetSrc: "devhub.megha19.near/widget/app",
           params: {
             page: "about",
           },
