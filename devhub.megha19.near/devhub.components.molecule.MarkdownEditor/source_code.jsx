1f43b570e8f731e0de92510f04f75117723d19fe
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:26:57 +0000

    Update devhub.components.molecule.MarkdownEditor by devhub.megha19.near at 2024-05-21T17:26:57.177Z

--- devhub.megha19.near/devhub.components.molecule.MarkdownEditor/source_code.jsx
+++ devhub.megha19.near/devhub.components.molecule.MarkdownEditor/source_code.jsx
@@ -1,7 +1,7 @@
 const MarkdownEditor = ({ data, onChange, showAutoComplete }) => {
   return (
     <Widget
-      src={"devhub.megha19.near/widget/devhub.components.molecule.SimpleMDE"}
+      src={"${REPL_DEVHUB}/widget/devhub.components.molecule.SimpleMDE"}
       props={{
         data,
         onChange,
