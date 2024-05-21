55788a3d9d69361ea54b3e6f9f608db8edabaa87
harmonic1.near <harmonic1.near>
2024-05-21 12:46:49 +0000

    Update page.home by harmonic1.near at 2024-05-21T12:46:49.855Z

---
+++ harmonic1.near/page.home/source_code.jsx
@@ -0,0 +1,51 @@
+const { SidebarLayout } = VM.require(
+  "buildhub.near/widget/template.SidebarLayout"
+) || {
+  SidebarLayout: () => <></>,
+};
+const config = {
+  theme: {},
+  // layout: {
+  //   src: "buildhub.near/widget/Layout",
+  //   props: {
+  //     variant: "standard",
+  //   },
+  // },
+  blocks: {
+    // these get passed to the layout and children
+    Header: () => <></>,
+    Footer: () => <></>, // customize your footer
+  },
+  router: {
+    param: "tab",
+    routes: {
+      guide: {
+        path: "harmonic1.near/widget/Resources",
+        blockHeight: "final",
+        init: {
+          feedName: "Guide",
+          name: "Guide",
+          icon: "bi-map",
+          mdPath:
+            "https://raw.githubusercontent.com/NEARBuilders/gateway/main/resources.md",
+        },
+        default: "true",
+      },
+    },
+  },
+};
+const Root = styled.div``;
+return (
+  <Root>
+    <SidebarLayout
+      currentPath={"/harmonic1.near/widget/app?page=home"}
+      page={props.tab}
+      routes={config.router.routes}
+    >
+      <Widget
+        src="buildhub.near/widget/app.view"
+        props={{ config, ...props }}
+      />
+    </SidebarLayout>
+  </Root>
+);
