34085564bfdb6ac494ff01d7f9466d73c7abfa5b
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:27:47 +0000

    Update app by devhub.megha19.near at 2024-05-21T17:27:47.838Z

--- devhub.megha19.near/app/source_code.jsx
+++ devhub.megha19.near/app/source_code.jsx
@@ -4,14 +4,14 @@
  */
 
 const { onDraftStateChange } = VM.require(
-  "${REPL_DEVHUB}/widget/devhub.entity.post.draft"
+  "devhub.megha19.near/widget/devhub.entity.post.draft"
 );
 
 const { page, ...passProps } = props;
 
 // Import our modules
 const { AppLayout } = VM.require(
-  "${REPL_DEVHUB}/widget/devhub.components.templates.AppLayout"
+  "devhub.megha19.near/widget/devhub.components.templates.AppLayout"
 );
 
 if (!AppLayout) {
@@ -80,7 +80,7 @@ function Page() {
     case "home": {
       return (
         <Widget
-          src="${REPL_DEVHUB}/widget/devhub.page.home"
+          src="devhub.megha19.near/widget/devhub.page.home"
           props={passProps}
         />
       );
@@ -89,7 +89,7 @@ function Page() {
     case "communities": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.communities"}
+          src={"devhub.megha19.near/widget/devhub.page.communities"}
           props={passProps}
         />
       );
@@ -97,7 +97,7 @@ function Page() {
     case "announcements": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.announcements"}
+          src={"devhub.megha19.near/widget/devhub.page.announcements"}
           props={passProps}
         />
       );
@@ -110,7 +110,7 @@ function Page() {
         // where each level handles its own routing.
         // Modularizing a page just like we do with addons
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.entity.community.Provider"}
+          src={"devhub.megha19.near/widget/devhub.entity.community.Provider"}
           props={{
             ...passProps,
             Children: (p) => {
@@ -121,7 +121,7 @@ function Page() {
                   return (
                     <Widget
                       src={
-                        "${REPL_DEVHUB}/widget/devhub.page.community.configuration"
+                        "devhub.megha19.near/widget/devhub.page.community.configuration"
                       }
                       props={{
                         ...passProps,
@@ -134,7 +134,7 @@ function Page() {
                 default: {
                   return (
                     <Widget
-                      src={"${REPL_DEVHUB}/widget/devhub.page.community.index"}
+                      src={"devhub.megha19.near/widget/devhub.page.community.index"}
                       props={{
                         ...passProps,
                         ...p,
@@ -152,7 +152,7 @@ function Page() {
     case "feed": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.feed"}
+          src={"devhub.megha19.near/widget/devhub.page.feed"}
           props={passProps}
         />
       );
@@ -161,7 +161,7 @@ function Page() {
     case "create": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.entity.post.PostEditor"}
+          src={"devhub.megha19.near/widget/devhub.entity.post.PostEditor"}
           props={{ ...passProps, isCreatePostPage: true, onDraftStateChange }}
         />
       );
@@ -170,7 +170,7 @@ function Page() {
     case "create-proposal": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.entity.proposal.Editor"}
+          src={"devhub.megha19.near/widget/devhub.entity.proposal.Editor"}
           props={{ ...passProps }}
         />
       );
@@ -179,7 +179,7 @@ function Page() {
     case "proposals": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.proposals"}
+          src={"devhub.megha19.near/widget/devhub.page.proposals"}
           props={passProps}
         />
       );
@@ -187,7 +187,7 @@ function Page() {
     case "proposal": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.entity.proposal.Proposal"}
+          src={"devhub.megha19.near/widget/devhub.entity.proposal.Proposal"}
           props={passProps}
         />
       );
@@ -196,7 +196,7 @@ function Page() {
     case "about": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.about"}
+          src={"devhub.megha19.near/widget/devhub.page.about"}
           props={passProps}
         />
       );
@@ -204,7 +204,7 @@ function Page() {
     case "contribute": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.contribute"}
+          src={"devhub.megha19.near/widget/devhub.page.contribute"}
           props={passProps}
         />
       );
@@ -212,7 +212,7 @@ function Page() {
     case "profile": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.profile"}
+          src={"devhub.megha19.near/widget/devhub.page.profile"}
           props={passProps}
         />
       );
@@ -221,7 +221,7 @@ function Page() {
     case "blog": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.blog"}
+          src={"devhub.megha19.near/widget/devhub.page.blog"}
           props={passProps}
         />
       );
@@ -229,7 +229,7 @@ function Page() {
     case "post": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.post"}
+          src={"devhub.megha19.near/widget/devhub.page.post"}
           props={passProps}
         />
       );
@@ -237,7 +237,7 @@ function Page() {
     case "admin": {
       return (
         <Widget
-          src={"${REPL_DEVHUB}/widget/devhub.page.admin.index"}
+          src={"devhub.megha19.near/widget/devhub.page.admin.index"}
           props={passProps}
         />
       );
