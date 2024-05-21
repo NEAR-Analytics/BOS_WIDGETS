4fb4ec08a06dd3e1c504813f45c4d5f256b6cd6e
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:27:47 +0000

    Update devhub.feature.proposal-search.by-author by devhub.megha19.near at 2024-05-21T17:27:47.838Z

--- devhub.megha19.near/devhub.feature.proposal-search.by-author/source_code.jsx
+++ devhub.megha19.near/devhub.feature.proposal-search.by-author/source_code.jsx
@@ -4,7 +4,7 @@ const [selectedAuthor, setSelectedAuthor] = useState(null);
 if (!authorsOptions.length) {
   const data = [{ label: "None", value: "" }];
   const authors = Near.view(
-    "${REPL_DEVHUB_CONTRACT}",
+    "devhub.near",
     "get_all_proposal_authors",
     {}
   );
@@ -26,7 +26,7 @@ const Container = styled.div`
 return (
   <Container>
     <Widget
-      src="${REPL_DEVHUB}/widget/devhub.components.molecule.DropDown"
+      src="devhub.megha19.near/widget/devhub.components.molecule.DropDown"
       props={{
         options: authorsOptions,
         label: "Author",
