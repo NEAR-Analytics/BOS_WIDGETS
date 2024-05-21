ffe1c4d7277da0a5d5c210970aeeb5e240c6d9b7
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:27:47 +0000

    Update devhub.entity.community.configuration.InformationConfigurator by devhub.megha19.near at 2024-05-21T17:27:47.838Z

--- devhub.megha19.near/devhub.entity.community.configuration.InformationConfigurator/source_code.jsx
+++ devhub.megha19.near/devhub.entity.community.configuration.InformationConfigurator/source_code.jsx
@@ -66,7 +66,7 @@ function handleOnSubmit(v) {
 
 return (
   <Widget
-    src={"${REPL_DEVHUB}/widget/devhub.components.organism.Configurator"}
+    src={"devhub.megha19.near/widget/devhub.components.organism.Configurator"}
     props={{
       externalState: data,
       schema: CommunityInformationSchema,
