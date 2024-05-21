87dfdd918ca9eaa652b6f488341fa888a5a02e9c
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:29:28 +0000

    Update devhub.notification.Right by devhub.megha19.near at 2024-05-21T17:29:28.826Z

--- devhub.megha19.near/devhub.notification.Right/source_code.jsx
+++ devhub.megha19.near/devhub.notification.Right/source_code.jsx
@@ -1,4 +1,4 @@
-const { href } = VM.require("devhub.megha19.near/widget/core.lib.url") || (() => {});
+const { href } = VM.require("${REPL_DEVHUB}/widget/core.lib.url") || (() => {});
 
 return props.proposal === undefined ? (
   "Loading ..."
@@ -7,7 +7,7 @@ return props.proposal === undefined ? (
     <a
       className="btn btn-outline-dark"
       href={href({
-        widgetSrc: "devhub.megha19.near/widget/app",
+        widgetSrc: "${REPL_DEVHUB}/widget/app",
         params: {
           page: "proposal",
           id: props.proposal,
