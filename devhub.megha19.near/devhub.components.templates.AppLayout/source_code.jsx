3c81987d1bed929557e81f64d05f117f5362cbee
devhub.megha19.near <devhub.megha19.near>
2024-05-21 11:51:18 +0000

    Update devhub.components.templates.AppLayout by devhub.megha19.near at 2024-05-21T11:51:18.602Z

--- devhub.megha19.near/devhub.components.templates.AppLayout/source_code.jsx
+++ devhub.megha19.near/devhub.components.templates.AppLayout/source_code.jsx
@@ -1,21 +1,9 @@
-const data = fetch(`https://httpbin.org/headers`);
-const gatewayURL = data?.body?.headers?.Origin ?? "";
-
-// we need fixed positioning for near social and not for org
-const ParentContainer = gatewayURL.includes("near.org")
-  ? styled.div`
-      width: 100%;
-    `
-  : styled.div`
-      position: fixed;
-      inset: 73px 0px 0px;
-      width: 100%;
-      overflow-y: scroll;
-    `;
-
 const Theme = styled.div`
+  inset: 73px 0px 0px;
+  width: 100%;
   display: flex;
   flex-direction: column;
+  overflow-y: scroll;
   padding-top: calc(-1 * var(--body-top-padding));
   background: #f4f4f4;
   .container-xl {
@@ -58,15 +46,13 @@ const Footer = (props) => {
 
 function AppLayout({ page, children }) {
   return (
-    <ParentContainer>
-      <Theme>
-        <Container className="container-xl">
-          <AppHeader page={page} />
-          <ContentContainer>{children}</ContentContainer>
-          <Footer page={page} />
-        </Container>
-      </Theme>
-    </ParentContainer>
+    <Theme>
+      <Container className="container-xl">
+        <AppHeader page={page} />
+        <ContentContainer>{children}</ContentContainer>
+        <Footer page={page} />
+      </Container>
+    </Theme>
   );
 }
 
