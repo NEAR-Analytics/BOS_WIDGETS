bdb827f48f0f0cf60d4280163989156e46579075
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:29:28 +0000

    Update devhub.components.organism.Configurator by devhub.megha19.near at 2024-05-21T17:29:28.826Z

--- devhub.megha19.near/devhub.components.organism.Configurator/source_code.jsx
+++ devhub.megha19.near/devhub.components.organism.Configurator/source_code.jsx
@@ -1,4 +1,4 @@
-const Struct = VM.require("devhub.megha19.near/widget/core.lib.struct");
+const Struct = VM.require("${REPL_DEVHUB}/widget/core.lib.struct");
 
 if (!Struct) {
   return <p>Loading modules...</p>;
@@ -138,7 +138,7 @@ const defaultFieldsRender = ({ schema, form, isEditable }) => (
                 ) : (fieldValue?.length ?? 0) > 0 ? (
                   <Widget
                     src={
-                      "devhub.megha19.near/widget/devhub.components.molecule.MarkdownViewer"
+                      "${REPL_DEVHUB}/widget/devhub.components.molecule.MarkdownViewer"
                     }
                     props={{
                       text: fieldValue,
@@ -150,7 +150,7 @@ const defaultFieldsRender = ({ schema, form, isEditable }) => (
               </ValueView>
             </div>
             <Widget
-              src={`devhub.megha19.near/widget/devhub.${
+              src={`${REPL_DEVHUB}/widget/devhub.${
                 (fieldParamsByType[fieldType] ?? fieldParamsByType["string"])
                   .name
               }`}
@@ -287,7 +287,7 @@ const Configurator = ({
           ) : null}
 
           <Widget
-            src={"devhub.megha19.near/widget/devhub.components.molecule.Button"}
+            src={"${REPL_DEVHUB}/widget/devhub.components.molecule.Button"}
             props={{
               classNames: { root: "btn-outline-danger shadow-none border-0" },
               label: cancelLabel || "Cancel",
@@ -295,7 +295,7 @@ const Configurator = ({
             }}
           />
           <Widget
-            src={"devhub.megha19.near/widget/devhub.components.molecule.Button"}
+            src={"${REPL_DEVHUB}/widget/devhub.components.molecule.Button"}
             props={{
               classNames: { root: classNames.submit || "btn-success" },
               disabled: !form.hasUnsubmittedChanges || !isFormValid(),
