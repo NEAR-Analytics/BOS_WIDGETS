1bed370714ce2216290dd45bca748b0277c43be0
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:26:57 +0000

    Update devhub.components.feed.SubscribedFeed by devhub.megha19.near at 2024-05-21T17:26:57.177Z

--- devhub.megha19.near/devhub.components.feed.SubscribedFeed/source_code.jsx
+++ devhub.megha19.near/devhub.components.feed.SubscribedFeed/source_code.jsx
@@ -168,7 +168,7 @@ const renderItem = (item) =>
   item.action === "post" ? renderPost(item) : renderRepost(item);
 return (
   <Widget
-    src="devhub.megha19.near/widget/devhub.components.feed.MergedIndexFeed"
+    src="${REPL_DEVHUB}/widget/devhub.components.feed.MergedIndexFeed"
     props={{
       index,
       renderItem,
