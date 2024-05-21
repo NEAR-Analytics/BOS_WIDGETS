0857b8b638b4d3415e724dc8a6d1decabdcaef34
devhub.megha19.near <devhub.megha19.near>
2024-05-21 17:26:57 +0000

    Update devhub.entity.community.configuration.ConfigurationSection by devhub.megha19.near at 2024-05-21T17:26:57.177Z

--- devhub.megha19.near/devhub.entity.community.configuration.ConfigurationSection/source_code.jsx
+++ devhub.megha19.near/devhub.entity.community.configuration.ConfigurationSection/source_code.jsx
@@ -21,7 +21,7 @@ function SectionHeader() {
       {headerRight ||
         (hasConfigurePermissions && (
           <Widget
-            src={"devhub.megha19.near/widget/devhub.components.molecule.Button"}
+            src={"${REPL_DEVHUB}/widget/devhub.components.molecule.Button"}
             props={{
               classNames: { root: "btn-sm btn-secondary" },
               icon: {
