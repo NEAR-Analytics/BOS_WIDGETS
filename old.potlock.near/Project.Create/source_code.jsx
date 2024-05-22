f2db4c7acf13db73bba5ddbcf59bc6fde4fd9847
old.potlock.near <old.potlock.near>
2024-05-22 19:32:15 +0000

    Update Project.Create by old.potlock.near at 2024-05-22T19:32:15.684Z

---
+++ old.potlock.near/Project.Create/source_code.jsx
@@ -0,0 +1,19 @@
+return (
+  <>
+    <Widget
+      src={"old.potlock.near/widget/Components.Header"}
+      props={{
+        ...props,
+        title1: props.edit ? "Edit your project" : "Create new project",
+        description: `${
+          props.edit ? "Update your " : "Create a "
+        } profile for your impact project to receive direct donations, qualify for funding rounds, join NEAR's accelerator, and get discovered across social platforms.`,
+        centered: false,
+        containerStyle: {
+          background: "#FEF6EE",
+        },
+      }}
+    />
+    <Widget src={"old.potlock.near/widget/Project.CreateForm"} props={props} />
+  </>
+);
