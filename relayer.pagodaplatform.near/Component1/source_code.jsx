40f811b37e92d66fb8b08f7bd265c8647b50be1f
relayer.pagodaplatform.near <relayer.pagodaplatform.near>
2024-05-22 12:00:37 +0000

    Update Component1 by relayer.pagodaplatform.near at 2024-05-22T12:00:37.877Z

---
+++ relayer.pagodaplatform.near/Component1/source_code.jsx
@@ -0,0 +1,10 @@
+return (
+  <>
+    <div class="container min-vw-100">
+      <h5> Components can be composed </h5>
+      <hr />
+
+      <Widget src={"y3k.near/widget/MonthlyActiveAccounts"} />
+    </div>
+  </>
+);
