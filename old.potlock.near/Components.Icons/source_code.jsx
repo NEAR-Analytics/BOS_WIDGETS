f7ab63edce003dfe6c94c0b2b3fc70126e75f37d
old.potlock.near <old.potlock.near>
2024-05-22 19:34:59 +0000

    Update Components.Icons by old.potlock.near at 2024-05-22T19:34:59.689Z

---
+++ old.potlock.near/Components.Icons/source_code.jsx
@@ -0,0 +1,10 @@
+const { Volunteer } = VM.require(
+  "old.potlock.near/widget/Components.Icons.Volunteer"
+);
+const { Component } = VM.require(
+  "old.potlock.near/widget/Components.Icons.Component"
+);
+return {
+  Volunteer,
+  Component,
+};
