34346df5a16d28ff1f1e2648236dd7242ffe3e76
old.potlock.near <old.potlock.near>
2024-05-22 19:34:59 +0000

    Update Cart.AddToCart by old.potlock.near at 2024-05-22T19:34:59.689Z

---
+++ old.potlock.near/Cart.AddToCart/source_code.jsx
@@ -0,0 +1,31 @@
+const { handleCallback, item, text } = props;
+const { addItemsToCart, removeItemsFromCart, itemExistsInCart } = VM.require(
+  "old.potlock.near/widget/SDK.cart"
+) ?? {
+  addItemsToCart: () => {},
+  removeItemsFromCart: () => {},
+  itemExistsInCart: () => false,
+};
+const existsInCart = itemExistsInCart(item);
+return (
+  <Widget
+    src={"old.potlock.near/widget/Components.Button"}
+    props={{
+      type: "tertiary",
+      text: text || (existsInCart ? "Remove from cart" : "Add to cart"),
+      style: { padding: "12px 16px" },
+      disabled: props.disabled ?? false,
+      onClick: () => {
+        if (existsInCart) {
+          removeItemsFromCart([item]);
+        } else {
+          // item.ft = "NEAR";
+          addItemsToCart([item]);
+        }
+        if (handleCallback) {
+          handleCallback();
+        }
+      },
+    }}
+  />
+);
