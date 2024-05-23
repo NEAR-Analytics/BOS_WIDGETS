0848e5016204465e2399b22b14446664d3b18639
spencerpark.near <spencerpark.near>
2024-05-23 00:38:17 +0000

    Update Docs-bot-qa by spencerpark.near at 2024-05-23T00:38:17.911Z

---
+++ spencerpark.near/docs-bot-qa/source_code.jsx
@@ -0,0 +1,481 @@
+const { href } = VM.require("devhub.near/widget/core.lib.url");
+const { loadItem, convertObjectKeysSnakeToPascal } = VM.require(
+  "near/widget/Entities.QueryApi.Client"
+);
+
+const storedModel = Storage.get("agent-model");
+const storedLocalModel = Storage.get("agent-local-model");
+const storedCredentialType = Storage.get("agent-credential-type");
+const storedCredential = Storage.get("agent-credential");
+const storedJsonOutputSetting = Storage.get("agent-json-output-setting");
+if (
+  !href ||
+  storedCredential === null ||
+  storedModel === null ||
+  storedLocalModel === null ||
+  storedCredentialType === null ||
+  loadItem === null
+) {
+  return "Loading config...";
+}
+
+const { src, embedded } = props;
+const [accountId, agentType, agentName] = src.split("/") ?? [null, null, null];
+
+const namespace = "near";
+const entityType = "agent";
+const entityIndexer = "entities";
+const entityTable = "entities";
+const user = "dataplatform_near";
+const collection = `${user}_${entityIndexer}_${entityTable}`;
+
+const query = `
+query SingleEntity {
+    ${collection}(
+          where: { account_id: {_eq: "${accountId}"}, name: {_eq: "${agentName}"}, 
+                   entity_type: {_eq: "${entityType}"}, namespace: {_eq: "${namespace}"}}
+        ) {
+        entity_type
+        namespace
+        id
+        account_id
+        name
+        display_name
+        logo_url
+        attributes
+        stars
+        tags
+        created_at
+        updated_at
+        }
+    }
+`;
+
+const [agent, setAgent] = useState(null);
+const [error, setError] = useState(null);
+const onLoad = (itemInArray) => {
+  if (itemInArray.length === 0 || !itemInArray[0]) {
+    setError(`${entityType} with name ${agentName} not found`);
+    return;
+  }
+  const fetchedItem = itemInArray[0];
+  const fullEntity = convertObjectKeysSnakeToPascal({
+    ...fetchedItem,
+    ...fetchedItem.attributes,
+  });
+  delete fullEntity.attributes;
+  setAgent({ accountId, name: agentName, ...fullEntity });
+};
+loadItem(query, "SingleEntity", collection, onLoad);
+
+if (!agent) return "Loading agent...";
+
+const listLink = href({
+  widgetSrc: `near/widget/AI.Nexus`,
+});
+
+const [settingsOpen, setSettingsOpen] = useState(false);
+const [question, setQuestion] = useState("");
+const [loading, setLoading] = useState(false);
+const [messages, setMessages] = useState([]);
+
+const [model, setModel] = useState(storedModel ?? "near-docs");
+const [localModel, setLocalModel] = useState(
+  storedLocalModel ?? "http://localhost:1234/v1/chat/completions"
+);
+const [credentialType, setCredentialType] = useState(
+  storedCredentialType ?? "bearer"
+);
+const [credential, setCredential] = useState(storedCredential ?? "");
+const [jsonOutputSetting, setJsonOutputSetting] = useState(
+  storedJsonOutputSetting ?? false
+);
+
+useEffect(() => {
+  Storage.set("agent-model", model);
+}, [model]);
+useEffect(() => {
+  Storage.set("agent-local-model", localModel);
+}, [localModel]);
+useEffect(() => {
+  Storage.set("agent-credential-type", credentialType);
+}, [credentialType]);
+useEffect(() => {
+  Storage.set("agent-credential", credential);
+}, [credential]);
+useEffect(() => {
+  Storage.set("agent-json-output-setting", jsonOutputSetting);
+}, [jsonOutputSetting]);
+
+const toggleSettings = () => {
+  setSettingsOpen(!settingsOpen);
+};
+
+const routeApi = async (question) => {
+  switch (model) {
+    case "near-docs":
+      return nearDocs(question);
+    default:
+      return openAICompatible(question);
+  }
+};
+const urlForModel = (model) => {
+  switch (model) {
+    case "near-docs":
+      return `https://9386-34-125-214-61.ngrok-free.app/query`;
+  }
+};
+const nearDocs = async (question) => {
+  return asyncFetch(`https://9386-34-125-214-61.ngrok-free.app/query`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    responseType: "json",
+    body: `openai_key=${credential}&question=${question.content}`,
+  }).then((response) => {
+    return response.response;
+  });
+};
+
+const openAICompatible = async (question) => {
+  let finalQuestion = question.content;
+  let options = {
+    model,
+  };
+  if (jsonOutputSetting) {
+    options.response_format = { type: "json_object" };
+    if (!finalQuestion.includes("json")) {
+      finalQuestion = `${finalQuestion} respond in json`;
+    }
+  }
+
+  return asyncFetch(urlForModel(model), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${credential}`,
+    },
+    responseType: "json",
+    body: JSON.stringify({
+      ...options,
+      messages: [{ role: "system", content: agent.prompt }, ...messages],
+    }),
+  }).then((response) => {
+    const answer = response.body.choices[0].message.content;
+    return answer;
+  });
+};
+
+useEffect(() => {
+  if (messages.length === 0 || messages[messages.length - 1].role !== "user") {
+    return;
+  }
+  setLoading(true);
+  routeApi(...messages.slice(-1))
+    .then((answer) => {
+      setMessages([...messages, { role: "assistant", content: answer }]);
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+}, [messages]);
+
+const submitQuestion = () => {
+  setMessages([...messages, { role: "user", content: question }]);
+  setQuestion("");
+};
+const requiresCredentials = (model) => {
+  return (
+    model === "gpt-4" ||
+    model === "gpt-3.5-turbo" ||
+    model === "mixtral-8x7b-32768" ||
+    model === "llama2-70b-4096"
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+  padding: 48px;
+`;
+
+const Overview = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin-bottom: 1em;
+`;
+
+const Header = styled.h1`
+  font-size: 24px;
+  line-height: 39px;
+  color: #11181c;
+  margin-bottom: 20px;
+  font-weight: 600;
+`;
+const Text = styled.p`
+  margin: 0;
+  font-size: 14px;
+  line-height: 20px;
+  color: ${(p) => (p.bold ? "#11181C" : "#687076")};
+  font-weight: ${(p) => (p.bold ? "600" : "400")};
+  font-size: ${(p) => (p.small ? "12px" : "14px")};
+
+  i {
+    margin-right: 4px;
+  }
+`;
+const Prompt = styled.p`
+  font-family: monospace;
+  font-size: 14px;
+  overflow-y: auto;
+  height: 100px;
+`;
+const Label = styled.span`
+  font-weight: 600;
+`;
+const Settings = styled.div`
+  margin-bottom: 1em;
+  z-index: 1000;
+`;
+const Controls = styled.div`
+  margin-bottom: 1em;
+`;
+const CardControl = styled.div`
+  cursor: pointer;
+  color: var(--violet8);
+  margin-bottom: 1em;
+`;
+const AllSettings = styled.div``;
+const InputWrapper = styled.div`
+  padding-bottom: 1em;
+`;
+const Question = styled.input`
+  border-top-left-radius: 2rem;
+  border-bottom-left-radius: 2rem;
+`;
+const UserMessage = styled.div``;
+const AgentMessage = styled.div`
+  background-color: #f9f9f9;
+`;
+
+const renderSettings = () => {
+  return (
+    <Settings>
+      <CardControl bold onClick={toggleSettings}>
+        <i className={settingsOpen ? "ph ph-caret-up" : "ph ph-caret-down"} />{" "}
+        Settings
+      </CardControl>
+      {settingsOpen && (
+        <AllSettings>
+          <InputWrapper>
+            <Widget
+              src="near/widget/DIG.InputSelect"
+              props={{
+                groups: [
+                  {
+                    label: "NEAR",
+                    items: [
+                      {
+                        label: "NEAR Docs Bot",
+                        value: "near-docs",
+                      },
+                      // Hi hackathon teams, implementing calls to gpt4.near? Add it here. - the black dragon
+                    ],
+                  },
+                ],
+                label: "Choose Model",
+                placeholder: "NEAR Docs Bot",
+                rootProps: {
+                  value: model,
+                  onValueChange: setModel,
+                },
+              }}
+            />
+          </InputWrapper>
+          {model === "local" && (
+            <InputWrapper>
+              <Widget
+                src="near/widget/DIG.Input"
+                props={{
+                  label: "Local Model URL",
+                  assistiveText:
+                    "Any url that accepts messages in OpenAI format",
+                  iconLeft: "ph-bold ph-horse",
+                  onInput: (e) => setLocalModel(e.target.value),
+                  value: localModel,
+                }}
+              />
+            </InputWrapper>
+          )}
+          <InputWrapper>
+            <div className="row">
+              <div className="col-3">
+                <Widget
+                  src="near/widget/DIG.InputSelect"
+                  props={{
+                    groups: [
+                      {
+                        label: "OpenAI, Groq, or other API Key",
+                        items: [
+                          {
+                            label: "Bearer Token",
+                            value: "bearer",
+                          },
+                        ],
+                      },
+                    ],
+                    label: "Credential Type",
+                    rootProps: {
+                      value: credentialType,
+                      onValueChange: setCredentialType,
+                    },
+                  }}
+                />
+              </div>
+              <div className="col">
+                <Widget
+                  src="near/widget/DIG.Input"
+                  props={{
+                    label: "Credentials",
+                    assistiveText:
+                      "Your OpenAI API Key or other credentials, will be stored in your browser.",
+                    iconLeft: "ph-bold ph-identification-card",
+                    onInput: (e) => setCredential(e.target.value),
+                    value: credential,
+                    type: "password",
+                  }}
+                />
+              </div>
+            </div>
+          </InputWrapper>
+          <InputWrapper>
+            <Widget
+              src="near/widget/DIG.Checkbox"
+              props={{
+                id: "json-output",
+                label: "JSON Output mode",
+                checked: jsonOutputSetting,
+                onCheckedChange: setJsonOutputSetting,
+              }}
+            />{" "}
+            not supported by all providers.
+          </InputWrapper>
+        </AllSettings>
+      )}
+    </Settings>
+  );
+};
+
+return (
+  <Wrapper>
+    <div>
+      {!embedded && (
+        <div>
+          <Link to={listLink}>
+            <Header>
+              <i className="ph ph-arrow-left" />
+              Agent List
+            </Header>
+          </Link>
+          <Overview>
+            <div className="row">
+              <div className="col-5">
+                <Widget
+                  src="near/widget/AI.Agent.AgentSummary"
+                  props={{
+                    size: "small",
+                    showTags: true,
+                    entity: agent,
+                  }}
+                />
+              </div>
+              <div className="col-7">
+                <Prompt>
+                  <Label>Prompt:</Label> {agent.prompt}
+                </Prompt>
+              </div>
+            </div>
+          </Overview>
+        </div>
+      )}
+      <Controls>
+        {renderSettings()}
+        {requiresCredentials(model) && credential === "" && (
+          <div className="alert alert-danger mx-3" role="alert">
+            <i className="ph ph-alert-circle" /> To use an OpenAI or Groq model
+            enter your API Key in Settings or change to another provider.
+          </div>
+        )}
+        <div className="input-group">
+          <Question
+            type="text"
+            className="form-control"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                submitQuestion();
+              }
+            }}
+            placeholder="What's your question?"
+            autoFocus
+          />
+          <Widget
+            src="near/widget/DIG.Button"
+            props={{
+              onClick: submitQuestion,
+              iconLeft: editIcon,
+              variant: "affirmative",
+              fill: "solid",
+              size: "large",
+              label: "Submit",
+              disabled:
+                (requiresCredentials(model) && credential === "") ||
+                question === "",
+              style: {
+                borderTopLeftRadius: "0rem",
+                borderBottomLeftRadius: "0rem",
+              },
+            }}
+          />
+        </div>
+      </Controls>
+      <div className="d-flex flex-column-reverse">
+        {messages.map(({ role, content }, i) => {
+          return (
+            <div key={i} className={`message ${role}`}>
+              {role === "user" && (
+                <UserMessage>
+                  <Widget
+                    src="mob.near/widget/N.ProfileLine"
+                    props={{ accountId: context.accountId }}
+                  />
+                  <Markdown text={content} />
+                </UserMessage>
+              )}
+              {role !== "user" && (
+                <AgentMessage>
+                  <Markdown text={content} />
+                </AgentMessage>
+              )}
+            </div>
+          );
+        })}
+        {loading && (
+          <div key="loading" className={`message system`}>
+            <div>
+              <span
+                className="spinner-grow spinner-grow-sm me-1"
+                role="status"
+                aria-hidden="true"
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  </Wrapper>
+);
