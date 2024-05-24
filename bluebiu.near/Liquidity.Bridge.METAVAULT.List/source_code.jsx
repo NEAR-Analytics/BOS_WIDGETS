6ccbc125e6e2f90c516c500e76ae0a4c09eededc
bluebiu.near <bluebiu.near>
2024-05-24 08:15:23 +0000

    Update Liquidity.Bridge.METAVAULT.List by bluebiu.near at 2024-05-24T08:15:23.153Z

--- bluebiu.near/Liquidity.Bridge.METAVAULT.List/source_code.jsx
+++ bluebiu.near/Liquidity.Bridge.METAVAULT.List/source_code.jsx
@@ -21,6 +21,7 @@ const {
   refetch,
   dataList,
   dataIndex,
+  defaultDex,
   columnList,
   onChangeDataIndex,
   addresses,
@@ -84,6 +85,7 @@ return (
                         refetch,
                         addresses,
                         addAction,
+                        defaultDex,
                         storeAddress,
                         ICON_VAULT_MAP
                       }}
