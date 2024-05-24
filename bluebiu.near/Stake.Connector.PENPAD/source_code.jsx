778efde725f64a1efc33f76773a0457e0fe1021f
bluebiu.near <bluebiu.near>
2024-05-24 02:56:46 +0000

    Update Stake.Connector.PENPAD by bluebiu.near at 2024-05-24T02:56:46.146Z

--- bluebiu.near/Stake.Connector.PENPAD/source_code.jsx
+++ bluebiu.near/Stake.Connector.PENPAD/source_code.jsx
@@ -172,14 +172,15 @@ const {
 const {
   PROXY_ADDRESS
 } = dexConfig
-if (!sender) {
+if (!sender || !isChainSupported) {
   return (
     <Widget
-      style={dexConfig.theme}
-      src="bluebiu.near/widget/Arbitrum.Swap.ConnectButton"
+      src="bluebiu.near/widget/Swap.ChainWarnigBox"
       props={{
-        ...connectProps,
-        isWrongNetwork: false,
+        chain: curChain,
+        onSwitchChain: onSwitchChain,
+        switchingChain: switchingChain,
+        theme: dexConfig.theme?.button,
       }}
     />
   );
@@ -496,17 +497,5 @@ return (
         />
       )
     }
-    {!isChainSupported && (
-      <Widget
-        src="bluebiu.near/widget/Swap.ChainWarnigBox"
-        props={{
-          chain: curChain,
-          onSwitchChain: onSwitchChain,
-          switchingChain: switchingChain,
-          theme: dexConfig.theme?.button,
-        }}
-      />
-    )}
-
   </StyledPenpadContainer>
 )
