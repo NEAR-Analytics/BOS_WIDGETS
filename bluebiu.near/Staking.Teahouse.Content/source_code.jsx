d953b02949d09289cf3461eb4d7ebdb560b70017
bluebiu.near <bluebiu.near>
2024-05-26 13:36:44 +0000

    Update Staking.Teahouse.Content by bluebiu.near at 2024-05-26T13:36:44.066Z

---
+++ bluebiu.near/Staking.Teahouse.Content/source_code.jsx
@@ -0,0 +1,286 @@
+const {
+  Column,
+  Row,
+  StyledVaultImage,
+  SvgIcon,
+  StyledDashedUndeline,
+  TdTxt,
+  PoolPercentage,
+  StrategyTxt,
+  TitleText,
+  ContainerLogin,
+} = VM.require("bluebiu.near/widget/Staking.Teahouse.Styles");
+
+State.init({
+  loading: false,
+  dataList: [],
+  dataIndex: -1,
+  chainIndex: 0,
+  token: "",
+});
+const IconRight = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="8"
+    height="10"
+    viewBox="0 0 8 10"
+    fill="none"
+  >
+    <path
+      d="M7.18407 4.21913C7.68448 4.61945 7.68448 5.38054 7.18407 5.78087L2.28485 9.70024C1.63009 10.2241 0.660156 9.75788 0.660156 8.91937L0.660156 1.08062C0.660156 0.242118 1.63009 -0.224055 2.28485 0.299756L7.18407 4.21913Z"
+      fill="#979ABE"
+    />
+  </svg>
+);
+const {
+  account,
+  toast,
+  CHAIN_LIST,
+  multicallAddress,
+  dexConfig,
+  defaultDex,
+  curChain,
+  isChainSupported,
+  onSwitchChain,
+  addAction,
+  connectProps,
+  prices,
+  chainIdNotSupport,
+} = props;
+console.log("PROPS--", props);
+const formatFiat = (value) => {
+  const number = Number(value).toLocaleString("en", {
+    currency: "USD",
+    style: "currency",
+    compactDisplay: "short",
+    notation: "compact",
+    maximumFractionDigits: 2,
+  });
+  return number;
+};
+
+const formatPercent = (value) => {
+  return `${Number(value * 100).toLocaleString("en", {
+    maximumFractionDigits: 2,
+  })}%`;
+};
+useEffect(() => {
+  State.update({
+    loading: !chainIdNotSupport,
+  });
+}, [chainIdNotSupport]);
+if (!account || !isChainSupported) {
+  return (
+    <Widget
+      src="bluebiu.near/widget/Swap.ChainWarnigBox"
+      props={{
+        chain: curChain,
+        onSwitchChain: onSwitchChain,
+        switchingChain: switchingChain,
+        theme: dexConfig.theme?.button,
+      }}
+    />
+  );
+}
+
+const { pairs, addresses, proxyAddress, ICON_VAULT_MAP } = dexConfig;
+// function fetchAllData() {
+//   // State.update({
+//   //   loading: true,
+//   // });
+//   asyncFetch(ALL_DATA_URL).then((res) => {
+//     if (!res.ok) return;
+//     // State.update({
+
+//     //   // loading: false,
+//     // });
+//   });
+// }
+
+function handleChangeDataIndex(index) {
+  state.dataIndex === index
+    ? State.update({
+        dataIndex: -1,
+      })
+    : State.update({
+        dataIndex: index,
+      });
+}
+
+function handleChangeChainIndex(index) {
+  const chain = CHAIN_LIST[index];
+  onSwitchChain({
+    chainId: `0x${Number(chain.chain_id).toString(16)}`,
+  });
+}
+function handleSearchInput(event) {
+  State.update({
+    token: event.target.value,
+  });
+}
+
+useEffect(() => {
+  const index = CHAIN_LIST.findIndex((chain) => chain.id === curChain.id);
+
+  if (index > -1) {
+    State.update({
+      chainIndex: index,
+
+      dataList: [],
+      userPositions: null,
+    });
+  }
+}, [curChain]);
+const columnList = [
+  {
+    width: "25%",
+    key: "pool",
+    label: "Pool",
+    type: "slot",
+    render: (data) => {
+      return (
+        <>
+          <StyledVaultImage>
+            <img
+              style={{ marginRight: -6 }}
+              src={ICON_VAULT_MAP[data.token0]}
+              alt={data.token0}
+            />
+            <img src={ICON_VAULT_MAP[data.token1]} alt={data.token1} />
+          </StyledVaultImage>
+          <TdTxt>
+            {data.token0} / {data.token1}
+          </TdTxt>
+          <PoolPercentage>{data.fee}%</PoolPercentage>
+        </>
+      );
+    },
+  },
+  {
+    width: "10%",
+    key: "chain",
+    label: "Chain",
+    type: "slot",
+    render: () => (
+      <img style={{ width: 26 }} src={curChain.logo} alt={curChain.name} />
+    ),
+  },
+  {
+    width: "15%",
+    key: "strategy",
+    label: "Strategy",
+    type: "slot",
+    render: (data) => {
+      return <StrategyTxt>{data.strategy}</StrategyTxt>;
+    },
+  },
+  {
+    width: "15%",
+    key: "AUM",
+    label: "AUM",
+    type: "slot",
+    render: (data) => {
+      return <TdTxt>{formatFiat(data.AUM)}</TdTxt>;
+    },
+  },
+  {
+    width: "15%",
+    key: "APR",
+    label: "APR",
+    type: "slot",
+    render: (data) => {
+      return (
+        <StyledDashedUndeline>
+          <TdTxt>{data.APR}%</TdTxt>
+        </StyledDashedUndeline>
+      );
+    },
+  },
+  {
+    width: "15%",
+    direction: "column",
+    key: "liquidity",
+    label: "Your Assets",
+    type: "slot",
+    render: (data, index) => {
+      // const userPositions = state.userPositions;
+      // const userBalance =
+      //   userPositions && addresses[data.id] in userPositions
+      //     ? userPositions[addresses[data.id]].balanceUSD
+      //     : undefined;
+
+      return (
+        <>
+          {/* <TdTxt>
+            {Big(userBalance ?? 0).gt(0) ? `${formatFiat(userBalance)}` : "-"}
+          </TdTxt>
+          {Big(data?.liquidity ?? 0).gt(0) && (
+            <TdTxt className="gray">
+              {Big(data?.liquidity ?? 0).lt(0.01)
+                ? "<0.01"
+                : Big(data.liquidity).toFixed(2)}{" "}
+              LP
+            </TdTxt>
+          )} */}
+          <SvgIcon
+            className={[
+              "icon-right",
+              index === state.dataIndex ? "rotate" : "",
+            ]}
+          >
+            {IconRight}
+          </SvgIcon>
+        </>
+      );
+    },
+  },
+];
+
+console.log("dataList---", state.dataList);
+return (
+  <Column>
+    <Widget
+      src={"bluebiu.near/widget/Staking.Teahouse.Data"}
+      props={{
+        ...props,
+        ...dexConfig,
+        update: state.loading,
+        onLoad: (data) => {
+          State.update({
+            dataList: data.dataList,
+            loading: false,
+          });
+        },
+      }}
+    />
+    {state.loading ? (
+      <Widget src="bluebiu.near/widget/0vix.LendingSpinner" />
+    ) : (
+      <Widget
+        src={"bluebiu.near/widget/Staking.Teahouse.List"}
+        props={{
+          ...props,
+          ...dexConfig,
+
+          // dataList: state.filterList,
+          dataList: state.dataList,
+          dataIndex: state.dataIndex,
+          onChangeDataIndex: handleChangeDataIndex,
+          userPositions: state.userPositions,
+          columnList,
+        }}
+      />
+    )}
+    {!isChainSupported && (
+      <Widget
+        src="bluebiu.near/widget/Swap.ChainWarnigBox"
+        props={{
+          chain: curChain,
+          onSwitchChain: onSwitchChain,
+          switchingChain: switchingChain,
+          theme: dexConfig.theme?.button,
+        }}
+      />
+    )}
+  </Column>
+);
