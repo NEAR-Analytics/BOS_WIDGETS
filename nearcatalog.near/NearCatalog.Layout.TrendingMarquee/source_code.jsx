e8aa0ca7077d0d80b82338d92b4f4ff2845d02b8
nearcatalog.near <nearcatalog.near>
2024-05-26 17:05:16 +0000

    Update NearCatalog.Layout.TrendingMarquee by nearcatalog.near at 2024-05-26T17:05:16.176Z

--- nearcatalog.near/NearCatalog.Layout.TrendingMarquee/source_code.jsx
+++ nearcatalog.near/NearCatalog.Layout.TrendingMarquee/source_code.jsx
@@ -1,60 +1,57 @@
 const Css = styled.div`
-  overflow-x:hidden;
-.awesome-trending-content{
-  text-align: center;
-  white-space: nowrap !important;
-}
-
-.awesome-trending-content .near-item-sm { 
-  display: inline-block; 
-  float: none; 
-}
-`
-
+  overflow-x: hidden;
+  .awesome-trending-content {
+    text-align: center;
+    white-space: nowrap !important;
+  }
+  .awesome-trending-content .near-item-sm {
+    display: inline-block;
+    float: none;
+  }
+`;
 const Marquee = styled.div`
-@keyframes marquee {
-  from {
-    transform: translateX(0%);
+  @keyframes marquee {
+    from {
+      transform: translateX(0%);
+    }
+    to {
+      transform: translateX(-100%);
+    }
   }
-  to {
-    transform: translateX(-100%);
+  animation: marquee 30s linear infinite;
+  margin: 0 auto;
+  &:hover {
+    animation-play-state: paused;
   }
-}
-animation: marquee 30s linear infinite;
-margin: 0 auto;
-&:hover{
-  animation-play-state: paused;
-}
-`
-  State.init({
-    trendingProjects: false,
-  });
-  const router = props.router || "";
-  if (props.cat == "trending") return <></>;
-
-  asyncFetch(props.indexer + "/projects-by-category?cid=trending").then((res) => {
-    State.update({ trendingProjects: res.body });
-    console.log("Trending: ", res.body);
-  });
-
-  if (!state.trendingProjects) {
-    return <>
+`;
+State.init({
+  trendingProjects: false,
+});
+const router = props.router || "";
+if (props.cat == "trending") return <></>;
+asyncFetch(props.indexer + "/projects-by-category?cid=trending").then((res) => {
+  State.update({ trendingProjects: res.body });
+  console.log("Trending: ", res.body);
+});
+if (!state.trendingProjects) {
+  return (
+    <>
       <br />
       🐲🐉🐶😺~
       <br />
-    </>;
-  }
-
-  return (
-    <Css>
-      <h3 className="my-3">🔥Trending</h3>
-      <div
-        className="awesome-trending-content"
-        style={{
-          marginLeft:100,
-          width: (Object.keys(state.trendingProjects).length-1)*90
-        }}
-      >
+    </>
+  );
+}
+return (
+  <Css>
+    <h3 className="my-3">🔥Trending</h3>
+    <div
+      className="awesome-trending-content"
+      style={{
+        marginLeft: 100,
+        width: (Object.keys(state.trendingProjects).length - 1) * 90,
+      }}
+    >
       <Marquee>
         {Object.keys(state.trendingProjects).map((e) => {
           let p = state.trendingProjects[e];
@@ -77,6 +74,6 @@ margin: 0 auto;
           );
         })}
       </Marquee>
-      </div>
-    </Css>
-  );
+    </div>
+  </Css>
+);
