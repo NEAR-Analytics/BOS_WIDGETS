d70bb930333d5474f476254c7ca6e3deb2be907a
dapdapbos.near <dapdapbos.near>
2024-05-27 06:34:38 +0000

    Update Staking.Hyperlock.Handler by dapdapbos.near at 2024-05-27T06:34:38.381Z

--- dapdapbos.near/Staking.Hyperlock.Handler/source_code.jsx
+++ dapdapbos.near/Staking.Hyperlock.Handler/source_code.jsx
@@ -1,66 +1,113 @@
 const { account, dexConfig, addAction, toast, onLoad } = props;
 
-const { positionManagerAddress, targetAddress } = dexConfig;
+const { positionManagerAddress, targetAddress, v2Address } = dexConfig;
 
 useEffect(() => {
-  const handleAction = ({ pool, method, onSuccess, onError }) => {
-    const Contract = new ethers.Contract(
-      method === "safeTransferFrom" ? positionManagerAddress : targetAddress,
-      [
-        {
-          inputs: [
-            {
-              components: [
-                { internalType: "uint256", name: "tokenId", type: "uint256" },
-                { internalType: "address", name: "recipient", type: "address" },
-                {
-                  internalType: "uint128",
-                  name: "amount0Max",
-                  type: "uint128",
-                },
-                {
-                  internalType: "uint128",
-                  name: "amount1Max",
-                  type: "uint128",
-                },
-              ],
-              internalType:
-                "struct INonfungiblePositionManagerStruct.CollectParams",
-              name: "params",
-              type: "tuple",
-            },
-          ],
-          name: "collect",
-          outputs: [
-            { internalType: "uint256", name: "", type: "uint256" },
-            { internalType: "uint256", name: "", type: "uint256" },
-          ],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [
-            { internalType: "uint256", name: "_tokenId", type: "uint256" },
-          ],
-          name: "withdraw",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-        {
-          inputs: [
-            { internalType: "address", name: "from", type: "address" },
-            { internalType: "address", name: "to", type: "address" },
-            { internalType: "uint256", name: "tokenId", type: "uint256" },
-          ],
-          name: "safeTransferFrom",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-      ],
-      Ethers.provider().getSigner()
-    );
+  const handleAction = ({
+    type,
+    id,
+    pool,
+    amount,
+    method,
+    price,
+    symbol,
+    onSuccess,
+    onError,
+  }) => {
+    console.log({ type, id, pool, amount, method, onSuccess, onError });
+    let Contract = null;
+    if (type === "V2") {
+      Contract = new ethers.Contract(
+        v2Address,
+        [
+          {
+            inputs: [
+              { internalType: "address", name: "_lpToken", type: "address" },
+              { internalType: "uint256", name: "_amount", type: "uint256" },
+              { internalType: "uint256", name: "_lock", type: "uint256" },
+            ],
+            name: "stake",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            inputs: [
+              { internalType: "address", name: "_lpToken", type: "address" },
+              { internalType: "uint256", name: "_amount", type: "uint256" },
+            ],
+            name: "unstake",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+        ],
+        Ethers.provider().getSigner()
+      );
+    } else {
+      Contract = new ethers.Contract(
+        method === "safeTransferFrom" ? positionManagerAddress : targetAddress,
+        [
+          {
+            inputs: [
+              {
+                components: [
+                  { internalType: "uint256", name: "tokenId", type: "uint256" },
+                  {
+                    internalType: "address",
+                    name: "recipient",
+                    type: "address",
+                  },
+                  {
+                    internalType: "uint128",
+                    name: "amount0Max",
+                    type: "uint128",
+                  },
+                  {
+                    internalType: "uint128",
+                    name: "amount1Max",
+                    type: "uint128",
+                  },
+                ],
+                internalType:
+                  "struct INonfungiblePositionManagerStruct.CollectParams",
+                name: "params",
+                type: "tuple",
+              },
+            ],
+            name: "collect",
+            outputs: [
+              { internalType: "uint256", name: "", type: "uint256" },
+              { internalType: "uint256", name: "", type: "uint256" },
+            ],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            inputs: [
+              { internalType: "uint256", name: "_tokenId", type: "uint256" },
+            ],
+            name: "withdraw",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            inputs: [
+              { internalType: "address", name: "from", type: "address" },
+              { internalType: "address", name: "to", type: "address" },
+              { internalType: "uint256", name: "tokenId", type: "uint256" },
+            ],
+            name: "safeTransferFrom",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+        ],
+        Ethers.provider().getSigner()
+      );
+    }
+
     let params = [];
     if (method === "collect") {
       params = [
@@ -78,6 +125,15 @@ useEffect(() => {
     if (method === "safeTransferFrom") {
       params = [account, targetAddress, pool.id];
     }
+    const _amount = Big(amount || 0)
+      .mul(Big(10).pow(18))
+      .toFixed(0);
+    if (method === "stake") {
+      params = [id, _amount, 0];
+    }
+    if (method === "unstake") {
+      params = [id, _amount];
+    }
     Contract.estimateGas[method](...params)
       .then((res) => {
         console.log("estimateGas", res);
@@ -93,7 +149,7 @@ useEffect(() => {
             const action =
               method === "collect"
                 ? "Collect Fees"
-                : method === "withdraw"
+                : ["withdraw", "unstake"].includes(method)
                 ? "Unstake"
                 : "Stake";
             tx.wait()
@@ -112,20 +168,31 @@ useEffect(() => {
                   });
                 }
                 if (method !== "collect") {
+                  const extraData =
+                    type === "V2"
+                      ? {
+                          token: symbol,
+                          amount,
+                          price,
+                        }
+                      : {
+                          token0Symbol: pool.token0.symbol,
+                          token1Symbol: pool.token1.symbol,
+                          amount0: pool.amount0.toString(),
+                          amount1: pool.amount1.toString(),
+                          price0: pool.price0,
+                          price1: pool.price1,
+                        };
                   addAction({
                     type: "Staking",
                     template: "Hyperlock",
                     account,
                     status,
                     transactionHash,
+                    amount,
                     extra_data: JSON.stringify({
                       action,
-                      token0Symbol: pool.token0.symbol,
-                      token1Symbol: pool.token1.symbol,
-                      amount0: pool.amount0.toString(),
-                      amount1: pool.amount1.toString(),
-                      price0: pool.price0,
-                      price1: pool.price1,
+                      ...extraData,
                     }),
                   });
                 }
