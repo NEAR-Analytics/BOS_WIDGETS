a705df542dc9986486e015596c97f19502c4ec3c
bluebiu.near <bluebiu.near>
2024-05-27 07:48:43 +0000

    Update Liquidity.Bridge.ARRAKIS.Detail by bluebiu.near at 2024-05-27T07:48:43.930Z

--- bluebiu.near/Liquidity.Bridge.ARRAKIS.Detail/source_code.jsx
+++ bluebiu.near/Liquidity.Bridge.ARRAKIS.Detail/source_code.jsx
@@ -469,6 +469,7 @@ const handleDeposit = () => {
         amount: amount0,
         template: defaultDex,
         status: status,
+        add: 1,
         transactionHash,
         chain_id: props.chainId,
         extra_data: JSON.stringify({
@@ -674,6 +675,7 @@ const handleWithdraw = () => {
         amount: lpAmount,
         template: defaultDex,
         status: status,
+        add: 0,
         transactionHash,
         chain_id: state.chainId,
       });
