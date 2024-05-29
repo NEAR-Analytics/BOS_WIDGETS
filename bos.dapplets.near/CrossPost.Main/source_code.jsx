fe566ad0c4ea55206c5ba3267acfa025cddb73e2
bos.dapplets.near <bos.dapplets.near>
2024-05-29 09:35:06 +0000

    Update CrossPost.Main by bos.dapplets.near at 2024-05-29T09:35:06.550Z

--- bos.dapplets.near/CrossPost.Main/source_code.jsx
+++ bos.dapplets.near/CrossPost.Main/source_code.jsx
@@ -1,6 +1,44 @@
-const handleCrosspostClick = () => {
-  console.log(props.context);
-  console.log(Near);
+const getTopContext = (ctx) => {
+  return ctx.parent ? drillUpContext(ctx) : ctx;
 };
 
-return <button onClick={handleCrosspostClick}>CrossPost</button>;
+const buildShareLink = (targetUrl, mutationId) => {
+  const url = new URL("https://augm.link/mutate");
+
+  url.searchParams.set("t", targetUrl);
+  url.searchParams.set("m", mutationId);
+
+  return url.href;
+};
+
+const buildRepostText = (shareLink, text) => {
+  return `Forwarded from X with [MutableWeb](${shareLink}):\n\n${text}`;
+};
+
+const handleCrosspostClick = () => {
+  const post = props.context.parsed;
+  const { mutationId } = getTopContext(props.context);
+  const shareLink = buildShareLink(post.url, mutationId);
+  const repostText = buildRepostText(shareLink, post.text);
+
+  Social.set({
+    post: {
+      main: JSON.stringify({
+        type: "md",
+        text: repostText,
+      }),
+      index: {
+        post: JSON.stringify({
+          key: "main",
+          value: { type: "md" },
+        }),
+      },
+    },
+  });
+};
+
+return (
+  <button disabled={!context.accountId} onClick={handleCrosspostClick}>
+    CrossPost
+  </button>
+);
