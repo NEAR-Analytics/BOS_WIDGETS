a9056701213698af2f7f537c7122f01441e8ce91
thomasguntenaar.near <thomasguntenaar.near>
2024-05-29 17:10:42 +0000

    Update devhub.feature.proposal-search.by-author by thomasguntenaar.near at 2024-05-29T17:10:42.942Z

--- thomasguntenaar.near/devhub.feature.proposal-search.by-author/source_code.jsx
+++ thomasguntenaar.near/devhub.feature.proposal-search.by-author/source_code.jsx
@@ -2,7 +2,7 @@ const [authorsOptions, setAuthorsOptions] = useState([]);
 const [selectedAuthor, setSelectedAuthor] = useState(null);
 
 if (!authorsOptions.length) {
-  const data = [{ label: "None", value: "" }];
+  const data = [{ label: "All", value: "" }];
   const authors = Near.view(
     "devhub.near",
     "get_all_proposal_authors",
