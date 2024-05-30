fb0ff3ad0cf39f74e65193bc32c882109cfe481f
dev.near <dev.near>
2024-05-30 11:00:08 +0000

    Update op-request-task by dev.near at 2024-05-30T11:00:08.118Z

---
+++ dev.near/op-request-task/source_code.jsx
@@ -0,0 +1,50 @@
+const {
+  apiUrl,
+  sessionId,
+  problemId,
+  annotations,
+  pendingRequest,
+  setPendingRequest,
+  onRequest,
+} = props;
+
+if (typeof onRequest !== "function") {
+  onRequest = (data) => console.log("onRequest", data);
+}
+
+if (typeof setPendingRequest !== "function") {
+  setPendingRequest = (data) => console.log("setPendingRequest", data);
+}
+
+const requestTask = () => {
+  if (!pendingRequest) {
+    setPendingRequest(true);
+    asyncFetch(`${apiUrl}/request_annotation/`, {
+      method: "POST",
+      body: JSON.stringify({
+        account_id: context.accountId,
+        session_id: sessionId,
+        problem_id: problemId,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        onRequest(res.body);
+      }
+      console.log("request_annotation resp", res.ok, res.body);
+      setPendingRequest(false);
+    });
+  }
+};
+
+return (
+  <div
+    class="h-100 d-flex justify-content-center align-items-center"
+    align="center"
+    style={{ minHeight: "400px" }}
+  >
+    <div>
+      {(annotations ?? []).length == 0 && <p>No pending tasks found.</p>}
+      <button onClick={() => requestTask()}>Create new task</button>
+    </div>
+  </div>
+);
