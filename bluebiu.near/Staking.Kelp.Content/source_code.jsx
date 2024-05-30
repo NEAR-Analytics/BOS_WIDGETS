66dd84de583a9b9288caf12ea2db9cbb47d4877e
bluebiu.near <bluebiu.near>
2024-05-30 17:55:06 +0000

    Update Staking.Kelp.Content by bluebiu.near at 2024-05-30T17:55:06.575Z

--- bluebiu.near/Staking.Kelp.Content/source_code.jsx
+++ bluebiu.near/Staking.Kelp.Content/source_code.jsx
@@ -14,6 +14,21 @@ const Wrapper = styled.div``;
 const BlurWrap = styled.div`
   position: relative;
 `;
+const WithdrawWrap = styled.div`
+  padding: 20px 15px;
+  min-height: 300px;
+  .withdraw-title {
+    font-size: 16px;
+    color: white;
+    margin-bottom: 20px;
+  }
+  .withdraw-item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: white;
+  }
+`;
 const Blur = styled.div`
   position: absolute;
   left: 0;
@@ -132,11 +147,29 @@ const Btn = styled.button`
     opacity: 0.5;
   }
 `;
+const ClaimBtn = styled.button`
+  background-color: var(--switch-color);
+  color: var(--button-text-color);
+  display: block;
+  width: 80px;
+  font-size: 16px;
+  font-weight: 600;
+  height: 40px;
+  color: white;
+  background-color: #075a5a;
+  border-radius: 6px;
+  cursor: pointer;
+  transition: 0.5s;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
 
 const {
   dexConfig,
   wethAddress,
   multicallAddress,
+  WithdrawalContract,
   chainIdNotSupport,
   multicall,
   prices,
@@ -153,6 +186,7 @@ const {
 const { parseUnits, formatUnits } = ethers.utils;
 // console.log("Content--", props);
 const { tokenPairs } = dexConfig;
+
 State.init({
   stakeAmount: "",
   curToken: "", // token symbol
@@ -161,6 +195,8 @@ State.init({
 
   tokenBal: 0,
   showDialog: false,
+  withdrawData: false,
+  updater: "",
 });
 
 useEffect(() => {
@@ -226,10 +262,162 @@ function getTVL() {
       console.log("Catch-getTVL--", err);
     });
 }
+function getWithdrawList(tokenAddr, tokenSymbol, userIndex) {
+  const contract = new ethers.Contract(
+    WithdrawalContract,
+    [
+      {
+        inputs: [
+          { internalType: "address", name: "asset", type: "address" },
+          { internalType: "address", name: "user", type: "address" },
+          { internalType: "uint256", name: "userIndex", type: "uint256" },
+        ],
+        name: "getUserWithdrawalRequest",
+        outputs: [
+          { internalType: "uint256", name: "rsETHAmount", type: "uint256" },
+          {
+            internalType: "uint256",
+            name: "expectedAssetAmount",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "withdrawalStartBlock",
+            type: "uint256",
+          },
+          { internalType: "uint256", name: "userNonce", type: "uint256" },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    Ethers.provider()
+  );
+  contract
+    .getUserWithdrawalRequest(tokenAddr, account, userIndex)
+    .then((res) => {
+      console.log("getWithdrawList--", res);
+      const [
+        rsETHAmount,
+        expectedAssetAmount,
+        withdrawalStartBlock,
+        userNonce,
+      ] = res;
+      getBlockNumber().then((_blockNumber) => {
+        let _withdrawData = {
+          asset: tokenAddr,
+          symbol: tokenSymbol,
+          amount: formatUnits(expectedAssetAmount),
+          canClaim: false,
+        };
 
+        if (
+          Big(_blockNumber).gt(
+            Big(withdrawalStartBlock).plus(state.withdrawalDelayBlocks)
+          )
+        ) {
+          _withdrawData.canClaim = true;
+        }
+        State.update({
+          withdrawData: _withdrawData,
+        });
+      });
+    })
+    .catch((err) => {
+      console.log("getWithdrawList-error--", err);
+    });
+}
+function getBlockNumber() {
+  return Ethers.provider()
+    .getBlockNumber()
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      console.log("getBlockNumber-error--", err);
+    });
+}
+function getWithdrawalDelayBlocks() {
+  const contract = new ethers.Contract(
+    WithdrawalContract,
+    [
+      {
+        inputs: [],
+        name: "withdrawalDelayBlocks",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    Ethers.provider()
+  );
+  contract
+    .withdrawalDelayBlocks()
+    .then((_blocks) => {
+      console.log("getWithdrawalDelayBlocks--", _blocks);
+      State.update({
+        withdrawalDelayBlocks: _blocks.toString(),
+      });
+    })
+    .catch((err) => {
+      console.log("getWithdrawalDelayBlocks-error--", err);
+    });
+}
+
+function handleClaim(_tokenAddr) {
+  console.log("claim--", _tokenAddr);
+  State.update({
+    claimLoading: true,
+  });
+
+  const contract = new ethers.Contract(
+    WithdrawalContract,
+    [
+      {
+        inputs: [{ internalType: "address", name: "asset", type: "address" }],
+        name: "completeWithdrawal",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+      },
+    ],
+    Ethers.provider().getSigner()
+  );
+  contract
+    .completeWithdrawal(_tokenAddr)
+    .then((tx) => {
+      tx.wait()
+        .then((res) => {
+          const { status, transactionHash } = res;
+          if (status !== 1) throw new Error("");
+          State.update({
+            claimLoading: false,
+            updater: Date.now(),
+          });
+          toast?.success({
+            title: "Claim Successfully!",
+            // text: `Approve ${Big(amount).toFixed(2)} ${tokenSymbol}`,
+            tx: transactionHash,
+            chainId,
+          });
+        })
+        .catch((err) => {
+          State.update({
+            claimLoading: false,
+          });
+        });
+    })
+    .catch((err) => {
+      console.log("handleClaim-error--", err);
+      State.update({
+        claimLoading: false,
+      });
+    });
+}
 useEffect(() => {
   getAPY();
   getTVL();
+  getWithdrawalDelayBlocks();
 }, []);
 
 useEffect(() => {
@@ -250,7 +438,12 @@ useEffect(() => {
       tokenBal: _bal,
     });
   }
-}, [state.curToken, tab]);
+  if (tab === "Withdraw") {
+    StakeTokens.filter((item) => item.symbol !== "ETH").forEach((_token) => {
+      getWithdrawList(_token.address, _token.symbol, 0);
+    });
+  }
+}, [state.curToken, tab, updater]);
 
 // rsETH balance
 function getTokenBalance() {
@@ -311,7 +504,7 @@ function handleCloseWrap() {
     showDialog: false,
   });
 }
-
+console.log("STATE--", state);
 return (
   <div>
     <StyledContainer>
@@ -334,118 +527,151 @@ return (
             <div className="amount">{state.APY}%</div>
           </SummaryItem>
         </Summary>
+        {(tab === "Stake" || tab === "Unstake") && (
+          <Content>
+            <BlurWrap>
+              {chainId !== 1 && (tab === "Unstake" || tab === "Withdraw") ? (
+                <Blur></Blur>
+              ) : null}
 
-        <Content>
-          <BlurWrap>
-            {chainId !== 1 && (tab === "Unstake" || tab === "Withdraw") ? (
-              <Blur></Blur>
-            ) : null}
-
-            <Panel>
-              <div className="title">
-                {tab === "Unstake" ? "Withdraw rsETH as" : tab}
-              </div>
-              <div className="body">
-                <Input
-                  type="text"
-                  placeholder="0"
-                  value={state.stakeAmount}
-                  onChange={(ev) => {
-                    if (isNaN(Number(ev.target.value))) return;
-                    let amount = ev.target.value.replace(/\s+/g, "");
-
-                    if (Big(amount || 0).gt(Big(state.tokenBal || 0))) {
-                      amount = Big(state.tokenBal || 0).toFixed(4, 0);
-                    }
-                    State.update({
-                      stakeAmount: amount,
-                    });
-                  }}
-                />
-                <Widget
-                  src="bluebiu.near/widget/UI.Select.Index"
-                  props={{
-                    options: state.options,
-                    value: state.options.find(
-                      (obj) => obj.value === state.curToken
-                    ),
-                    onChange: (option) => {
-                      console.log("onchange--", option);
-                      State.update({
-                        curToken: option.value,
-                      });
-                    },
-                  }}
-                />
-              </div>
-              <div className="foot">
-                <div class="prices">
-                  $
-                  {Big(state.stakeAmount || 0)
-                    .times(Big(prices[state.curToken] || 1))
-                    .toFixed(2, 0)}
+              <Panel>
+                <div className="title">
+                  {tab === "Unstake" ? "Withdraw rsETH as" : tab}
                 </div>
-                <div class="balance">
-                  Balance:
+                <div className="body">
+                  <Input
+                    type="text"
+                    placeholder="0"
+                    value={state.stakeAmount}
+                    onChange={(ev) => {
+                      if (isNaN(Number(ev.target.value))) return;
+                      let amount = ev.target.value.replace(/\s+/g, "");
+
+                      if (Big(amount || 0).gt(Big(state.tokenBal || 0))) {
+                        amount = Big(state.tokenBal || 0).toFixed(4, 0);
+                      }
+                      State.update({
+                        stakeAmount: amount,
+                      });
+                    }}
+                  />
                   <Widget
-                    src="bluebiu.near/widget/Staking.Kelp.Balance"
+                    src="bluebiu.near/widget/UI.Select.Index"
                     props={{
-                      value: state.tokenBal,
-                      digit: 4,
-                      onClick: clickBalance,
-                      symbol:
-                        tab === "Stake" ? state.curToken : ExchangeToken.symbol,
+                      options: state.options,
+                      value: state.options.find(
+                        (obj) => obj.value === state.curToken
+                      ),
+                      onChange: (option) => {
+                        console.log("onchange--", option);
+                        State.update({
+                          curToken: option.value,
+                        });
+                      },
                     }}
                   />
                 </div>
-              </div>
-            </Panel>
-            <List>
-              <span className="keys">You will get</span>
-              <span className="values">
-                {Big(state.stakeAmount || 0)
-                  .div(state.exchangeRate || 1)
-                  .toFixed(4, 0)}{" "}
-                {tab === "Stake" ? ExchangeToken.symbol : state.curToken}
-              </span>
-            </List>
-            <List>
-              <span className="keys">Exchange rate</span>
-              <span className="values">
-                1 {ExchangeToken?.symbol} ={" "}
-                {Big(state.exchangeRate || 0).toFixed(4, 0)} {state.curToken}
-                {/* {tab === "Stake" ? ExchangeToken.symbol : state.curToken} */}
-              </span>
-            </List>
-          </BlurWrap>
-          <Widget
-            src="bluebiu.near/widget/Staking.Kelp.Button"
-            props={{
-              ...props,
-              actionText: tab,
-              amount: state.stakeAmount,
-              curToken: state.curToken,
-              stakeToken: StakeTokens
-                ? StakeTokens.find((item) => item.symbol === state.curToken)
-                : {},
-              onSuccess: () => {
-                State.update({ loading: true, stakeAmount: "" });
-              },
-            }}
-          />
+                <div className="foot">
+                  <div class="prices">
+                    $
+                    {Big(state.stakeAmount || 0)
+                      .times(Big(prices[state.curToken] || 1))
+                      .toFixed(2, 0)}
+                  </div>
+                  <div class="balance">
+                    Balance:
+                    <Widget
+                      src="bluebiu.near/widget/Staking.Kelp.Balance"
+                      props={{
+                        value: state.tokenBal,
+                        digit: 4,
+                        onClick: clickBalance,
+                        symbol:
+                          tab === "Stake"
+                            ? state.curToken
+                            : ExchangeToken.symbol,
+                      }}
+                    />
+                  </div>
+                </div>
+              </Panel>
+              <List>
+                <span className="keys">You will get</span>
+                <span className="values">
+                  {Big(state.stakeAmount || 0)
+                    .div(state.exchangeRate || 1)
+                    .toFixed(4, 0)}{" "}
+                  {tab === "Stake" ? ExchangeToken.symbol : state.curToken}
+                </span>
+              </List>
+              <List>
+                <span className="keys">Exchange rate</span>
+                <span className="values">
+                  1 {ExchangeToken?.symbol} ={" "}
+                  {Big(state.exchangeRate || 0).toFixed(4, 0)} {state.curToken}
+                  {/* {tab === "Stake" ? ExchangeToken.symbol : state.curToken} */}
+                </span>
+              </List>
+            </BlurWrap>
+            <Widget
+              src="bluebiu.near/widget/Staking.Kelp.Button"
+              props={{
+                ...props,
+                actionText: tab,
+                amount: state.stakeAmount,
+                curToken: state.curToken,
+                stakeToken: StakeTokens
+                  ? StakeTokens.find((item) => item.symbol === state.curToken)
+                  : {},
+                onSuccess: () => {
+                  State.update({ loading: true, stakeAmount: "" });
+                },
+              }}
+            />
 
-          <BtnWrap>
-            {[59144, 34443].includes(chainId) && (
-              <Btn onClick={handleOpenWrap}>Wrap</Btn>
-            )}
+            <BtnWrap>
+              {[59144, 34443].includes(chainId) && (
+                <Btn onClick={handleOpenWrap}>Wrap</Btn>
+              )}
 
-            {/* <Btn onClick={handleTabChange}>
+              {/* <Btn onClick={handleTabChange}>
               {tab === "Stake" ? "Stake" : "Unstake"}
             </Btn> */}
-          </BtnWrap>
-        </Content>
+            </BtnWrap>
+          </Content>
+        )}
 
-        {/* {tab === "Withdraw" && <div>null</div>} */}
+        {tab === "Withdraw" && (
+          <WithdrawWrap>
+            <div className="withdraw-title">AMOUNT</div>
+            <div className="withdraw-list">
+              <div className="withdraw-item">
+                <span className="withdraw-amount">
+                  {Number(state.withdrawData.amount).toFixed(6)}{" "}
+                  {state.withdrawData.symbol}
+                </span>
+                {state.withdrawData.canClaim ? (
+                  <ClaimBtn
+                    onClick={(e) => handleClaim(state.withdrawData.asset)}
+                  >
+                    {state.claimLoading ? (
+                      <Widget
+                        src="bluebiu.near/widget/0vix.LendingLoadingIcon"
+                        props={{
+                          size: 16,
+                        }}
+                      />
+                    ) : (
+                      "Claim"
+                    )}
+                  </ClaimBtn>
+                ) : (
+                  <span>~in 7 - 10 days</span>
+                )}
+              </div>
+            </div>
+          </WithdrawWrap>
+        )}
       </Wrapper>
 
       {/* {state.loading && <Widget src="bluebiu.near/widget/Lending.Spinner" />} */}
