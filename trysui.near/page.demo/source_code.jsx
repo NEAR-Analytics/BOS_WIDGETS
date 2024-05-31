89e90c2df08a20c677853816338a8baed7f4152f
trysui.near <trysui.near>
2024-05-31 16:41:37 +0000

    Update page.demo by trysui.near at 2024-05-31T16:41:37.036Z

--- trysui.near/page.demo/source_code.jsx
+++ trysui.near/page.demo/source_code.jsx
@@ -18,9 +18,11 @@ const Container = styled.div`
 `;
 return (
   <>
+    <br />
+    <br />
     <Flex>
       <Box>
-        <Heading>dApp Starter Template</Heading>
+        <Heading>demo app</Heading>
       </Box>
       <Box>
         <ConnectButton />
@@ -28,10 +30,7 @@ return (
     </Flex>
     <Container>
       <SuiClientQuery
-        provides={({ account, data, isPending, isError, error }) => {
-          if (account) {
-            return <Flex>Connected wallet: {account}</Flex>;
-          }
+        provides={({ data, isPending, isError, error }) => {
           if (error) {
             return <Flex>Error: {error.message}</Flex>;
           }
