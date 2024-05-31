d3f9b06b89e16e4d539929c4be9fc414a1026912
infrastructure-committee.near <infrastructure-committee.near>
2024-05-31 20:42:47 +0000

    Update components.molecule.FilterByLabel by infrastructure-committee.near at 2024-05-31T20:42:47.268Z

---
+++ infrastructure-committee.near/components.molecule.FilterByLabel/source_code.jsx
@@ -0,0 +1,23 @@
+import { REPL_DEVHUB } from "@/includes/common";
+const availableOptions = props.availableOptions;
+const options =
+  (availableOptions ?? []).map((i) => {
+    return { label: i.title, value: i.value };
+  }) ?? [];
+options.unshift({ label: "All", value: null });
+const setSelected = props.onStateChange ?? (() => {});
+
+return (
+  <div>
+    <Widget
+      src={`${REPL_DEVHUB}/widget/devhub.components.molecule.DropDown`}
+      props={{
+        options: options,
+        label: "Category",
+        onUpdate: (v) => {
+          setSelected(v);
+        },
+      }}
+    />
+  </div>
+);
