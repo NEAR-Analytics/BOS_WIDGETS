4075df442c9b1b37467e36f871053c64ed1efbef
nearcatalog.near <nearcatalog.near>
2024-05-31 15:20:30 +0000

    Update test by nearcatalog.near at 2024-05-31T15:20:30.881Z

--- nearcatalog.near/test/source_code.jsx
+++
@@ -1,9 +0,0 @@
-return (
-  <>
-    <p>{"Hello nearcatalog.near!"}</p>
-    <h1>
-      hello console.log('final 2323232 prop2222222222222222s', p); - value2 -
-      test2 value for mainnet2 !
-    </h1>
-  </>
-);
