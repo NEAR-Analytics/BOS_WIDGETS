8433e59c83117431532f1140106d22099ee120cd
trysui.near <trysui.near>
2024-05-31 14:11:10 +0000

    Update page.demo by trysui.near at 2024-05-31T14:11:10.551Z

---
+++ trysui.near/page.demo/source_code.jsx
@@ -0,0 +1,44 @@
+const Flex = styled.div`
+  position: sticky;
+  padding: 2px 4px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid var(--gray-a2);
+`;
+const Box = styled.div``;
+const Heading = styled.h1``;
+const Text = styled.p``;
+const Container = styled.div`
+  margin-top: 5px;
+  padding-top: 2px;
+  padding-left: 4px;
+  padding-right: 4px;
+  background: var(--gray-a2);
+  min-height: 500px;
+`;
+return (
+  <>
+    <Flex>
+      <Box>
+        <Heading>dApp Starter Template</Heading>
+      </Box>
+      <Box>
+        <SuiConnect />
+      </Box>
+    </Flex>
+    <Container>
+      <SuiClient
+        provides={(props) => (
+          <Container my="2">
+            <p>{JSON.stringify(props)}</p>
+          </Container>
+        )}
+        query={{
+          method: "getOwnedObjects",
+          params: { owner: "0x123" },
+          options: { gcTime: 10000 },
+        }}
+      />
+    </Container>
+  </>
+);
