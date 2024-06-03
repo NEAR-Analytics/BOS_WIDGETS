04a268b83d619b8b32bd4b71292554692837d647
thomasguntenaar.near <thomasguntenaar.near>
2024-06-03 14:00:49 +0000

    Update devhub.components.molecule.BlogControl by thomasguntenaar.near at 2024-06-03T14:00:49.455Z

--- thomasguntenaar.near/devhub.components.molecule.BlogControl/source_code.jsx
+++ thomasguntenaar.near/devhub.components.molecule.BlogControl/source_code.jsx
@@ -1,4 +1,4 @@
-const { title, onClick, icon } = props;
+const { title, onClick } = props;
 
 return (
   <button
@@ -6,9 +6,11 @@ return (
     data-testid={props.testId ? props.testId : ""}
     className="inline-flex items-center gap-x-1.5 rounded-md bg-devhub-green px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-devhub-green-transparent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     onClick={props.onClick}
-    disabled={props.disabled}
   >
-    <i className={`-ml-0.5 h-5 w-5 bi ${props.icon}`} aria-hidden="true"></i>
+    <i
+      className="-ml-0.5 h-5 w-5 bi bi-plus-circle-fill"
+      aria-hidden="true"
+    ></i>
     {props.title || "New Blog Post"}
   </button>
 );
