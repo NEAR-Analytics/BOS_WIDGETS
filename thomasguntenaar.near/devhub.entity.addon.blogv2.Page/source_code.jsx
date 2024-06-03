abef4badd91842b2bcaea278f7ee68f67e42c4e0
thomasguntenaar.near <thomasguntenaar.near>
2024-06-03 14:00:49 +0000

    Update devhub.entity.addon.blogv2.Page by thomasguntenaar.near at 2024-06-03T14:00:49.455Z

--- thomasguntenaar.near/devhub.entity.addon.blogv2.Page/source_code.jsx
+++ thomasguntenaar.near/devhub.entity.addon.blogv2.Page/source_code.jsx
@@ -1,39 +1,39 @@
-const { href } = VM.require("thomasguntenaar.near/widget/core.lib.url") || {
-  href: () => {},
-};
-const httpbin = fetch(`https://httpbin.org/headers`);
-
-const { getCommunity } = VM.require(
+const { getAccountCommunityPermissions } = VM.require(
   "thomasguntenaar.near/widget/core.adapter.devhub-contract"
 ) || {
-  getCommunity: () => {},
+  getAccountCommunityPermissions: () => {},
 };
 const imagelink =
   "https://ipfs.near.social/ipfs/bafkreiajzvmy7574k7mp3if6u53mdukfr3hoc2kjkhjadt6x56vqhd5swy";
 
-const { data, onEdit, community: handle, isAllowedToEdit } = props;
+function Page({ data, onEdit, accountId, community }) {
+  const {
+    category,
+    title,
+    description,
+    subtitle,
+    publishedAt: date,
+    content,
+    author,
+  } = data;
 
-const {
-  category,
-  title,
-  description,
-  subtitle,
-  publishedAt: date,
-  content,
-  author,
-  communityAddonId,
-} = data;
+  const permissions = getAccountCommunityPermissions({
+    account_id: accountId,
+    community_handle: community,
+  });
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
+  const isAllowedToEdit = permissions?.can_configure ?? false;
 
-  padding: 0 3rem;
-  margin-bottom: 2rem;
-  position: relative;
-  ${category &&
-  `
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    padding: 0 3rem;
+    margin-bottom: 2rem;
+    position: relative;
+    ${category &&
+    `
     span.category {
       color: ${
         category.toLowerCase() === "news"
@@ -50,140 +50,94 @@ const Container = styled.div`
     }
     `}
 
-  div.date {
-    color: #818181;
-    font-size: 1rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 20px; /* 125% */
-    margin: 1.5rem 0;
-    width: 100%;
-  }
-
-  h1 {
-    color: #151515;
-    font-size: 3.5rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 100%; /* 88px */
-    margin: 1.5rem 0;
-  }
-
-  p.subtitle {
-    color: #555;
-    font-size: 1.5rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 110%; /* 35.2px */
-    margin: 0;
-  }
-
-  .edit-icon {
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    cursor: pointer;
-  }
-
-  .share-icon {
-    position: absolute;
-    top: 18px;
-    right: 55px;
-    cursor: pointer;
-  }
-
-  @media screen and (max-width: 768px) {
-    padding: 0 1rem;
-
-    span.category {
-      font-size: 0.75rem;
+    div.date {
+      color: #818181;
+      font-size: 1rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 20px; /* 125% */
+      margin: 1.5rem 0;
+      width: 100%;
     }
 
     h1 {
-      font-size: 2rem;
+      color: #151515;
+      font-size: 3.5rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 100%; /* 88px */
+      margin: 1.5rem 0;
     }
 
     p.subtitle {
-      font-size: 1rem;
+      color: #555;
+      font-size: 1.5rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 110%; /* 35.2px */
+      margin: 0;
     }
-  }
-`;
 
-const BackgroundImage = styled.img`
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  margin-bottom: 1rem;
-`;
+    .edit-icon {
+      position: absolute;
+      top: 20px;
+      right: 20px;
+      cursor: pointer;
+    }
 
-if (!handle) {
-  return <div>Community handle not found</div>;
-}
+    @media screen and (max-width: 768px) {
+      padding: 0 1rem;
 
-const community = getCommunity({ handle: handle });
+      span.category {
+        font-size: 0.75rem;
+      }
 
-let communityConfig = (community.addons || []).find(
-  (addon) => addon.id === communityAddonId
-);
+      h1 {
+        font-size: 2rem;
+      }
 
-if (communityConfig === undefined) {
-  return <div>Community addon not found</div>;
-}
-const parameters = JSON.parse(communityConfig.parameters) || {};
+      p.subtitle {
+        font-size: 1rem;
+      }
+    }
+  `;
 
-const options = { year: "numeric", month: "short", day: "numeric" };
-const formattedDate = new Date(date).toLocaleString("en-US", options);
-const [isLinkCopied, setLinkCopied] = useState(false);
+  const BackgroundImage = styled.img`
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    margin-bottom: 1rem;
+  `;
 
-const onShareClick = () =>
-  clipboard
-    .writeText(
-      href({
-        gateway: httpbin?.body?.headers?.Origin.slice(8) ?? "near.social",
-        widgetSrc: "thomasguntenaar.near/widget/app",
-        params: { page: "blogv2", community: community.handle, id: data.id },
-      })
-    )
-    .then(setLinkCopied(true));
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  const formattedDate = new Date(date).toLocaleString("en-US", options);
 
-return (
-  <>
-    <BackgroundImage src={imagelink} />
-    <Container>
-      <div
-        className="share-icon"
-        onClick={onShareClick}
-        onMouseLeave={() =>
-          setTimeout(() => {
-            setLinkCopied(false);
-          }, 1000)
-        }
-      >
-        {isLinkCopied ? (
-          <div class="bi bi-clipboard-check" style={{ fontSize: "30px" }}></div>
-        ) : (
-          <div class="bi bi-box-arrow-up" style={{ fontSize: "30px" }}></div>
+  return (
+    <>
+      <BackgroundImage src={imagelink} />
+      <Container>
+        {isAllowedToEdit && (
+          <div className="edit-icon" onClick={onEdit}>
+            <div class="bi bi-pencil-square" style={{ fontSize: "30px" }}></div>
+          </div>
         )}
-      </div>
-      {isAllowedToEdit && (
-        <div className="edit-icon" onClick={onEdit}>
-          <div class="bi bi-pencil-square" style={{ fontSize: "30px" }}></div>
+        {category && <span className="category">{category}</span>}
+        <h1>{title}</h1>
+        <p className="subtitle">{subtitle}</p>
+        <div className="d-flex flex-row justify-content-between date">
+          {author && <div>{author}</div>}
+          <div>{formattedDate}</div>
         </div>
-      )}
-      {category && <span className="category">{category}</span>}
-      <h1>{title}</h1>
-      <p className="subtitle">{subtitle}</p>
-      <div className="d-flex flex-row justify-content-between date">
-        {author && parameters.authorEnabled !== "disabled" && (
-          <div data-testid="blog-author">{author}</div>
-        )}
-        <div data-testid="blog-date">{formattedDate}</div>
-      </div>
-      <p>{description}</p>
-      <Widget
-        src={"thomasguntenaar.near/widget/devhub.components.molecule.MarkdownViewer"}
-        props={{ text: content }}
-      />
-    </Container>
-  </>
-);
+        <p>{description}</p>
+        <Widget
+          src={
+            "thomasguntenaar.near/widget/devhub.components.molecule.MarkdownViewer"
+          }
+          props={{ text: content }}
+        />
+      </Container>
+    </>
+  );
+}
+
+return { Page };
