496587871bd666b03ed308bb591622b8beb3d1b6
infrastructure-committee.near <infrastructure-committee.near>
2024-06-03 18:34:22 +0000

    Update near-prpsls-bos.components.rfps.StatusTag by infrastructure-committee.near at 2024-06-03T18:34:22.981Z

---
+++ infrastructure-committee.near/near-prpsls-bos.components.rfps.StatusTag/source_code.jsx
@@ -0,0 +1,148 @@
+/*
+License: MIT
+Author: devhub.near
+Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
+*/
+/* INCLUDE: "includes/common.jsx" */
+const REPL_DEVHUB = "devhub.near";
+const REPL_INFRASTRUCTURE_COMMITTEE = "infrastructure-committee.near";
+const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
+const REPL_RPC_URL = "https://rpc.mainnet.near.org";
+const REPL_NEAR = "near";
+const RFP_IMAGE =
+  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
+
+const RFP_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfps_with_latest_snapshot";
+
+const RFP_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfp_snapshots";
+
+const PROPOSAL_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposals_with_latest_snapshot";
+
+const PROPOSAL_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposal_snapshots";
+const RFP_TIMELINE_STATUS = {
+  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
+  EVALUATION: "EVALUATION",
+  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
+  CANCELLED: "CANCELLED",
+};
+
+const PROPOSAL_TIMELINE_STATUS = {
+  DRAFT: "DRAFT",
+  REVIEW: "REVIEW",
+  APPROVED: "APPROVED",
+  REJECTED: "REJECTED",
+  CANCELED: "CANCELLED",
+  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
+  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
+  FUNDED: "FUNDED",
+};
+
+const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  return asyncFetch(QUERYAPI_ENDPOINT, {
+    method: "POST",
+    headers: { "x-hasura-role": `polyprogrammist_near` },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
+}
+
+const CANCEL_RFP_OPTIONS = {
+  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
+  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
+  NONE: "NONE",
+};
+
+function parseJSON(json) {
+  if (typeof json === "string") {
+    try {
+      return JSON.parse(json);
+    } catch (error) {
+      return json;
+    }
+  } else {
+    return json;
+  }
+}
+
+function isNumber(value) {
+  return typeof value === "number";
+}
+
+const PROPOSALS_APPROVED_STATUS_ARRAY = [
+  PROPOSAL_TIMELINE_STATUS.APPROVED,
+  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
+  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
+  PROPOSAL_TIMELINE_STATUS.FUNDED,
+];
+/* END_INCLUDE: "includes/common.jsx" */
+const timelineStatus = props.timelineStatus;
+const size = props.size ?? "md";
+
+const getClassNameByStatus = () => {
+  switch (timelineStatus) {
+    case RFP_TIMELINE_STATUS.CANCELLED:
+      return "grey";
+    case RFP_TIMELINE_STATUS.PROPOSAL_SELECTED:
+      return "green";
+    case RFP_TIMELINE_STATUS.EVALUATION:
+      return "orange";
+    default:
+      return "black";
+  }
+};
+
+const Container = styled.div`
+  font-size: ${({ size }) => {
+    switch (size) {
+      case "sm":
+        return "10px";
+      case "lg":
+        return "14px";
+      default:
+        return "12px";
+    }
+  }};
+
+  min-width: fit-content;
+
+  .orange-tag {
+    border: 1px solid #ff7a00 !important;
+    color: #ff7a00 !important;
+  }
+
+  .black-tag {
+    border: 1px solid #000 !important;
+    color: #000 !important;
+  }
+
+  .grey-tag {
+    border: 1px solid #979797 !important;
+    color: #979797 !important;
+  }
+
+  .green-tag {
+    border: 1px solid #04a46e !important;
+    color: #04a46e !important;
+  }
+
+  .fw-bold {
+    font-weight: 600 !important;
+  }
+`;
+
+return (
+  <Container size={size}>
+    <div className={getClassNameByStatus() + "-tag fw-bold rounded-2 p-1 px-2"}>
+      {(timelineStatus ?? "").replace("_", " ")}
+    </div>
+  </Container>
+);
