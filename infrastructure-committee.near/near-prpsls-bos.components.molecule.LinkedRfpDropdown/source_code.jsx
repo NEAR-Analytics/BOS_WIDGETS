111dc24479e105f708a2e7d223f1fde0ead44be2
infrastructure-committee.near <infrastructure-committee.near>
2024-06-04 14:42:36 +0000

    Update near-prpsls-bos.components.molecule.LinkedRfpDropdown by infrastructure-committee.near at 2024-06-04T14:42:36.931Z

--- infrastructure-committee.near/near-prpsls-bos.components.molecule.LinkedRfpDropdown/source_code.jsx
+++ infrastructure-committee.near/near-prpsls-bos.components.molecule.LinkedRfpDropdown/source_code.jsx
@@ -84,12 +84,20 @@ const PROPOSALS_APPROVED_STATUS_ARRAY = [
   PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
   PROPOSAL_TIMELINE_STATUS.FUNDED,
 ];
+
+function getLinkUsingCurrentGateway(url) {
+  const data = fetch(`https://httpbin.org/headers`);
+  const gatewayURL = data?.body?.headers?.Origin ?? "";
+  `https://${
+    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
+  }/${url}`;
+}
 /* END_INCLUDE: "includes/common.jsx" */
 
 const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`);
 href || (href = () => {});
 
-const { linkedRfp, onChange, disabled } = props;
+const { linkedRfp, onChange, disabled, onDeleteRfp } = props;
 
 const isModerator = Near.view(
   REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
@@ -232,6 +240,7 @@ return (
           <div
             className="cursor-pointer"
             onClick={() => {
+              onDeleteRfp();
               setSelectedRFP(null);
             }}
           >
@@ -244,7 +253,7 @@ return (
       src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.DropDownWithSearch`}
       props={{
         disabled: disabled,
-        selectedValue: "",
+        selectedValue: selectedRFP.value,
         onChange: (v) => {
           setSelectedRFP(v);
         },
