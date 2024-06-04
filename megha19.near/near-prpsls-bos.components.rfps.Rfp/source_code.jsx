a902529ed5a123788a42b4b4c2d438acb80b26b8
megha19.near <megha19.near>
2024-06-04 13:03:23 +0000

    Update near-prpsls-bos.components.rfps.Rfp by megha19.near at 2024-06-04T13:03:23.229Z

--- megha19.near/near-prpsls-bos.components.rfps.Rfp/source_code.jsx
+++ megha19.near/near-prpsls-bos.components.rfps.Rfp/source_code.jsx
@@ -427,9 +427,7 @@ const item = {
   path: `${REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT}/post/main`,
   blockHeight,
 };
-const rfpURL = getLinkUsingCurrentGateway(
-  `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app?page=rfp&id=${rfp.id}&timestamp=${snapshot.timestamp}`
-);
+const rfpURL = `https://near.org/${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app?page=rfp&id=${rfp.id}&timestamp=${snapshot.timestamp}`;
 
 const SidePanelItem = ({ title, children, hideBorder, ishidden }) => {
   return (
