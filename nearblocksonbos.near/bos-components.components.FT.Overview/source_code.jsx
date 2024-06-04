9c6680fb71a1bd66c7cad737cb1784d148043ad7
nearblocksonbos.near <nearblocksonbos.near>
2024-06-04 08:41:12 +0000

    Update bos-components.components.FT.Overview by nearblocksonbos.near at 2024-06-04T08:41:12.191Z

--- nearblocksonbos.near/bos-components.components.FT.Overview/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.FT.Overview/source_code.jsx
@@ -353,13 +353,7 @@ function MainComponent({
     `${ownerId}/widget/includes.Utils.libs`,
   );
 
-  const tabs = [
-    t ? t('token:fts.ft.transfers') : 'Transfers',
-    t ? t('token:fts.ft.holders') : 'Holders',
-    'Info',
-    'FAQ',
-    'Comments',
-  ];
+  const tabs = ['Transfers', 'Holders', 'Info', 'FAQ', 'Comments'];
   const [isLoading, setIsLoading] = useState(false);
   const [txnLoading, setTxnLoading] = useState(false);
   const [holderLoading, setHolderLoading] = useState(false);
@@ -873,7 +867,15 @@ function MainComponent({
                     }`}
                     value={tab}
                   >
-                    {tab === 'FAQ' && token ? <h2>{tab}</h2> : <h2>{tab}</h2>}
+                    {tab === 'FAQ' && token ? (
+                      <h2>{tab}</h2>
+                    ) : tab === 'Transfers' ? (
+                      <h2>{t ? t('token:fts.ft.transfers') : tab}</h2>
+                    ) : tab === 'Holders' ? (
+                      <h2>{t ? t('token:fts.ft.holders') : tab}</h2>
+                    ) : (
+                      <h2>{tab}</h2>
+                    )}
                   </button>
                 ))}
             </div>
