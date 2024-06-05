fca09a3c931b95c4065ee666e5c0840cdba88e65
dev.near <dev.near>
2024-06-05 14:03:17 +0000

    Update Index by dev.near at 2024-06-05T14:03:17.219Z

--- dev.near/Index/source_code.jsx
+++ dev.near/Index/source_code.jsx
@@ -238,7 +238,7 @@ const taskContainer = (
 );
 
 return (
-  <Theme style={{ padding: "0 20px" }}>
+  <Theme style={{ padding: "0 20px", overflowY: "scroll" }}>
     {state.pendingRequest && <Widget src={`${ACCOUNT_ID}/widget/op-loading`} />}
 
     {showSessionContainer && sessionContainer}
