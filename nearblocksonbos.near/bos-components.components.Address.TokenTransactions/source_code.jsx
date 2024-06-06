09458c409235eeac234d1d1799ad5e2d823bec56
nearblocksonbos.near <nearblocksonbos.near>
2024-06-06 13:41:17 +0000

    Update bos-components.components.Address.TokenTransactions by nearblocksonbos.near at 2024-06-06T13:41:17.107Z

--- nearblocksonbos.near/bos-components.components.Address.TokenTransactions/source_code.jsx
+++ nearblocksonbos.near/bos-components.components.Address.TokenTransactions/source_code.jsx
@@ -933,7 +933,7 @@ function MainComponent({
         </span>
       ),
       tdClassName:
-        'px-4 py-3 whitespace-nowrap text-sm text-nearblue-600 dark:text-neargray-10',
+        'px-4 py-3 whitespace-nowrap text-sm text-nearblue-600 dark:text-neargray-10 w-48',
       thClassName: 'whitespace-nowrap',
     },
   ];
