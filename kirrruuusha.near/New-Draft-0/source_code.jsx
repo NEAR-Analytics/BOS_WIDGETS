3795257bfa8524ef4b107603bd7fbb63899d6057
kirrruuusha.near <kirrruuusha.near>
2024-06-07 16:12:51 +0000

    Update New-Draft-0 by kirrruuusha.near at 2024-06-07T16:12:51.711Z

--- kirrruuusha.near/New-Draft-0/source_code.jsx
+++ kirrruuusha.near/New-Draft-0/source_code.jsx
@@ -24,7 +24,186 @@ State.init({
   vrifyOurStudent: "",
 });
 
-const content = JSON.parse(Social.get(`${accountId}`) ?? "null");
+const TecherPossibilities = {
+  init: () => {
+    const accountIdContext = context.accountId;
+    if (!accountIdContext) {
+      return "No account ID";
+    }
+    const accountIdProps = props.accountId;
+    const profile = Social.getr(`${state.accountIdContext}/profile`);
+    State.update({
+      accountIdProps: accountIdProps,
+      accountIdContext: accountIdContext,
+      profileName: profile.name,
+      profileDiscription: profile.discription,
+    });
+  },
+  initProfile: () => {
+    Social.set({
+      profile: {
+        name: state.creatProfileName,
+        discription: state.creatProfileDiscription,
+      },
+    });
+  },
+  initNameProfile: () => {
+    Social.set({
+      profile: {
+        name: state.creatProfileName,
+      },
+    });
+  },
+  initDiscriptionProfile: () => {
+    Social.set({
+      profile: {
+        discription: state.creatProfileDiscription,
+      },
+    });
+  },
+  getStudent: (pageNumber, pageSize) => {
+    // Рассчитываем начальный и конечный индексы для пагинации
+    const startIndex = (pageNumber - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    let studentArray = [];
+    let arreyWhitIndexForAddStudent = [];
+    let heashForDeletnumb = {};
+    if (state.accountIdProps) {
+      for (let i = startIndex; i < endIndex; i++) {
+        const student = Social.get(`${state.accountIdProps}/mystudents/${i}`);
+        if (student) {
+          studentArray.push(student);
+          heashForDeletnumb[student] = i;
+        } else {
+          arreyWhitIndexForAddStudent.push(i);
+        }
+      }
+    } else {
+      for (let i = startIndex; i < endIndex; i++) {
+        const student = Social.get(`${state.accountIdContext}/mystudents/${i}`);
+        if (student) {
+          studentArray.push(student);
+          heashForDeletnumb[student] = i;
+        } else {
+          arreyWhitIndexForAddStudent.push(i);
+        }
+      }
+    }
+    State.update({
+      studentArray: studentArray,
+      arreyWhitIndexForAddStudent: arreyWhitIndexForAddStudent,
+      heashForDeletnumb: heashForDeletnumb,
+    });
+  },
+  updateDiscription: (student) => {
+    Social.set({
+      profile: {
+        discriptionsStudent: {
+          [student]: state.editDescription,
+        },
+      },
+    });
+  },
+  deleteStudent: (student) => {
+    const indexForDeleteNumb = state.heashForDeletnumb[student];
+    Social.set({
+      mystudents: {
+        [indexForDeleteNumb]: null,
+      },
+      myStudentsForFind: {
+        [student]: false,
+      },
+    });
+  },
+  addStudent: () => {
+    const newStudent = state.addNewStudent;
+    const ifAlreadyHaveStudent = Social.get(
+      `${state.accountIdContext}/myStudentsForFind/${newStudent}`
+    );
+    const sliceForVerification = newStudent.slice(
+      newStudent.length - 5,
+      newStudent.length
+    );
+    if (sliceForVerification == ".near" && ifAlreadyHaveStudent != `true`) {
+      let indexForAddStudent = 0;
+      if (
+        state.studentArray.length > 0 &&
+        state.arreyWhitIndexForAddStudent.length > 0
+      ) {
+        indexForAddStudent = state.arreyWhitIndexForAddStudent[0];
+      } else if (
+        state.studentArray.length > 0 &&
+        state.arreyWhitIndexForAddStudent.length == 0
+      ) {
+        while (state.arreyWhitIndexForAddStudent.length == 0) {
+          const student = Social.get(
+            `${state.accountIdContext}/mystudents/${indexForAddStudent}`
+          );
+          if (!student) {
+            State.update({
+              arreyWhitIndexForAddStudent: student,
+            });
+            break;
+          }
+          indexForAddStudent++;
+        }
+      } else if (
+        state.studentArray.length == 0 &&
+        !state.arreyWhitIndexForAddStudent.length == 0
+      ) {
+        indexForAddStudent = 0;
+      }
+      Social.set({
+        mystudents: {
+          [indexForAddStudent]: state.addNewStudent,
+        },
+        myStudentsForFind: {
+          [newStudent]: true,
+        },
+      });
+      State.update({
+        ifAddStudent: true,
+      });
+    } else {
+      State.update({
+        ifAddStudent: false,
+      });
+    }
+  },
+  findStudentByID: () => {
+    const idaccound = state.idFindStudent;
+    const isOurStudent = Social.get(
+      `${state.accountIdContext}/myStudentsForFind/${idaccound}`
+    );
+    if (isOurStudent == `true`) {
+      State.update({
+        vrifyOurStudent: idaccound,
+      });
+    } else {
+      State.update({
+        vrifyOurStudent: "",
+      });
+    }
+  },
+};
+
+TecherPossibilities.init();
+TecherPossibilities.getStudent(1, 10);
+
+function descriptionForStudent(account_id) {
+  const discriprionalIN = Social.get(`${account_id}/profile/discription`);
+  return discriprionalIN;
+}
+
+function ourDescriptionForStudent(account_id) {
+  const discriprionalIN = Social.get(
+    `${state.accountIdContext}/profile/discriptionsStudent/${account_id}`
+  );
+  if (discriprionalIN == null) {
+    return "no info";
+  }
+  return discriprionalIN;
+}
 
 return (
   <div
