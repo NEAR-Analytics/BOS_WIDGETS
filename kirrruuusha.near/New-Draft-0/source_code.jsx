17d7452a7dd085b8e3c3a0104881a1592c830aa4
kirrruuusha.near <kirrruuusha.near>
2024-06-07 22:40:13 +0000

    Update New-Draft-0 by kirrruuusha.near at 2024-06-07T22:40:13.204Z

--- kirrruuusha.near/New-Draft-0/source_code.jsx
+++ kirrruuusha.near/New-Draft-0/source_code.jsx
@@ -591,3 +591,155 @@ if (!state.profileDiscription) {
     </div>
   );
 }
+
+if (state.accountIdProps) {
+  return (
+    <>
+      <div
+        style={{
+          background:
+            "linear-gradient(-45deg, #5F8AFA, #FFFFFF, #FFFFFF, #FFFFFF, #A463B0)",
+          width: "100%",
+          height: "100%",
+          padding: "2rem",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            textAlign: "center",
+            fontFamily: "'Manrope', sans-serif",
+          }}
+        >
+          <h1 style={{ fontWeight: "bold" }}>Mentor HUB</h1>
+          <h3>Make the world around you the better place</h3>
+          <Widget
+            src="mob.near/widget/ProfileImage"
+            props={{
+              accountId: state.accountIdProps,
+              style: { width: "7rem", height: "7rem" },
+              className: "mb-2",
+              imageClassName:
+                "rounded-circle w-100 h-100 img-thumbnail d-block",
+              thumbnail: false,
+            }}
+          />
+          <h4
+            style={{
+              marginTop: "20px",
+            }}
+          >
+            {state.accountIdProps}
+          </h4>
+          <h5
+            style={{
+              marginTop: "20px",
+            }}
+          >
+            Find student by account_ID
+          </h5>
+          <div
+            style={{
+              display: "flex",
+              height: "3em",
+            }}
+          >
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Input for find my student"
+              value={state.idFindStudent}
+              onChange={(e) => State.update({ idFindStudent: e.target.value })}
+            />
+            <Button
+              style={{
+                width: "100px",
+              }}
+              onClick={TecherPossibilities.findStudentByID}
+            >
+              Find
+            </Button>
+          </div>
+          {state.vrifyOurStudent && (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                alignItems: "center",
+                gap: "10px",
+                margin: "20px",
+                borderRadius: "12px",
+                background: "#fff",
+                border: "1px solid #eceef0",
+                boxShadow: "0px 1px 3px rgba(16, 24, 40, 0.1)",
+                overflow: "hidden",
+                padding: "16px",
+              }}
+            >
+              <div>
+                <Widget
+                  src="near/widget/AccountProfile"
+                  props={{ accountId: state.vrifyOurStudent }}
+                />
+              </div>
+              <div>
+                <h4>{descriptionForStudent(state.vrifyOurStudent)}</h4>
+              </div>
+            </div>
+          )}
+          <h3
+            style={{
+              marginTop: "20px",
+            }}
+          >
+            Students
+          </h3>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              margin: "15px",
+            }}
+          >
+            {state.studentArray.map((student) => (
+              <div
+                key={student}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  gap: "20px",
+                  margin: "10px",
+                  borderRadius: "12px",
+                  background: "#fff",
+                  border: "1px solid #eceef0",
+                  boxShadow: "0px 1px 3px rgba(16, 24, 40, 0.1)",
+                  overflow: "hidden",
+                  padding: "16px",
+                }}
+              >
+                {/* Common elements for each student */}
+                <div>
+                  <Widget
+                    src="near/widget/AccountProfile"
+                    props={{ accountId: student }}
+                  />
+
+                  <div>
+                    <h4>{descriptionForStudent(student)}</h4>
+                  </div>
+                  <div>
+                    <h4>{ourDescriptionForStudent(student)}</h4>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
