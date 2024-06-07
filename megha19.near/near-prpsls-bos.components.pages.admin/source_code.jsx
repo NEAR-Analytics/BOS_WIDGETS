9f78402f8b5f27706ea0ccb21d5482b6ff7ddbda
megha19.near <megha19.near>
2024-06-07 07:23:49 +0000

    Update near-prpsls-bos.components.pages.admin by megha19.near at 2024-06-07T07:23:49.573Z

--- megha19.near/near-prpsls-bos.components.pages.admin/source_code.jsx
+++ megha19.near/near-prpsls-bos.components.pages.admin/source_code.jsx
@@ -9,6 +9,7 @@ const REPL_INFRASTRUCTURE_COMMITTEE = "megha19.near";
 const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
 const REPL_RPC_URL = "https://rpc.mainnet.near.org";
 const REPL_NEAR = "near";
+const REPL_SOCIAL_CONTRACT = "social.near";
 const RFP_IMAGE =
   "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
 
@@ -213,6 +214,20 @@ return (
             Moderators
           </Tab>
         </li>
+        <li class="nav-item" role="presentation">
+          <Tab
+            className="nav-link"
+            id="about-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#about"
+            type="button"
+            role="tab"
+            aria-controls="about"
+            aria-selected="false"
+          >
+            About
+          </Tab>
+        </li>
       </ul>
       <div class="tab-content" id="myTabContent">
         <div
@@ -222,7 +237,21 @@ return (
           aria-labelledby="profile-tab"
         >
           <Widget
-            src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.admin.moderatorsTab`}
+            src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.admin.ModeratorsConfigurator`}
+            props={{
+              accessControlInfo,
+              createEditTeam,
+            }}
+          />
+        </div>
+        <div
+          class="tab-pane fade"
+          id="about"
+          role="tabpanel"
+          aria-labelledby="about-tab"
+        >
+          <Widget
+            src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.admin.AboutConfigurator`}
             props={{
               accessControlInfo,
               createEditTeam,
