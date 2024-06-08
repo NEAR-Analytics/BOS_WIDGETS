d6ca1fa66bab14a633bcf9ed6ab84aa4377946b4
megha19.near <megha19.near>
2024-06-08 11:14:38 +0000

    Update components.rfps.Rfp by megha19.near at 2024-06-08T11:14:38.226Z

--- megha19.near/components.rfps.Rfp/source_code.jsx
+++ megha19.near/components.rfps.Rfp/source_code.jsx
@@ -1,17 +1,98 @@
-import {
-  REPL_INFRASTRUCTURE_COMMITTEE,
-  REPL_DEVHUB,
-  REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
-  REPL_RPC_URL,
-  RFP_TIMELINE_STATUS,
-  RFP_IMAGE,
-  PROPOSAL_TIMELINE_STATUS,
-  PROPOSAL_FEED_INDEXER_QUERY_NAME,
-  fetchGraphQL,
-  RFP_INDEXER_QUERY_NAME,
-  parseJSON,
-  PROPOSALS_APPROVED_STATUS_ARRAY,
-} from "@/includes/common";
+/*
+License: MIT
+Author: devhub.near
+Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
+*/
+/* INCLUDE: "includes/common.jsx" */
+const REPL_DEVHUB = "devhub.near";
+const REPL_INFRASTRUCTURE_COMMITTEE = "megha19.near";
+const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
+const REPL_RPC_URL = "https://rpc.mainnet.near.org";
+const REPL_NEAR = "near";
+const REPL_SOCIAL_CONTRACT = "social.near";
+const RFP_IMAGE =
+  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
+
+const RFP_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfps_with_latest_snapshot";
+
+const RFP_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfp_snapshots";
+
+const PROPOSAL_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposals_with_latest_snapshot";
+
+const PROPOSAL_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposal_snapshots";
+const RFP_TIMELINE_STATUS = {
+  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
+  EVALUATION: "EVALUATION",
+  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
+  CANCELLED: "CANCELLED",
+};
+
+const PROPOSAL_TIMELINE_STATUS = {
+  DRAFT: "DRAFT",
+  REVIEW: "REVIEW",
+  APPROVED: "APPROVED",
+  REJECTED: "REJECTED",
+  CANCELED: "CANCELLED",
+  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
+  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
+  FUNDED: "FUNDED",
+};
+
+const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  return asyncFetch(QUERYAPI_ENDPOINT, {
+    method: "POST",
+    headers: { "x-hasura-role": `polyprogrammist_near` },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
+}
+
+const CANCEL_RFP_OPTIONS = {
+  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
+  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
+  NONE: "NONE",
+};
+
+function parseJSON(json) {
+  if (typeof json === "string") {
+    try {
+      return JSON.parse(json);
+    } catch (error) {
+      return json;
+    }
+  } else {
+    return json;
+  }
+}
+
+function isNumber(value) {
+  return typeof value === "number";
+}
+
+const PROPOSALS_APPROVED_STATUS_ARRAY = [
+  PROPOSAL_TIMELINE_STATUS.APPROVED,
+  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
+  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
+  PROPOSAL_TIMELINE_STATUS.FUNDED,
+];
+
+function getLinkUsingCurrentGateway(url) {
+  const data = fetch(`https://httpbin.org/headers`);
+  const gatewayURL = data?.body?.headers?.Origin ?? "";
+  return `https://${
+    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
+  }/${url}`;
+}
+/* END_INCLUDE: "includes/common.jsx" */
 
 const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`) || {
   href: () => {},
@@ -19,6 +100,10 @@ const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`) || {
 const { readableDate } = VM.require(
   `${REPL_DEVHUB}/widget/core.lib.common`
 ) || { readableDate: () => {} };
+
+const { getGlobalLabels } = VM.require(
+  `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.core.lib.contract`
+) || { getGlobalLabels: () => {} };
 
 const accountId = context.accountId;
 /*
@@ -251,10 +336,7 @@ const Avatar = styled.div`
   }
 `;
 
-const rfpLabelOptions = Near.view(
-  REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
-  "get_global_labels"
-);
+const rfpLabelOptions = getGlobalLabels();
 
 const LinkProfile = ({ account, children }) => {
   return (
@@ -275,7 +357,7 @@ const query = `query GetLatestSnapshot($offset: Int = 0, $limit: Int = 10, $wher
   ${queryName}(
     offset: $offset
     limit: $limit
-    order_by: {rfp_id: desc}
+    order_by: {ts: asc}
     where: $where
   ) {
     editor_id
@@ -318,7 +400,7 @@ useEffect(() => {
   fetchSnapshotHistory();
 }, [id]);
 
-if (!rfp || !snapshotHistory?.length) {
+if (!rfp) {
   return (
     <div
       style={{ height: "50vh" }}
@@ -346,7 +428,9 @@ const item = {
   path: `${REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT}/post/main`,
   blockHeight,
 };
-const rfpURL = `https://near.org/${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app?page=rfp&id=${rfp.id}&timestamp=${snapshot.timestamp}`;
+const rfpURL = getLinkUsingCurrentGateway(
+  `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app?page=rfp&id=${rfp.id}&timestamp=${snapshot.timestamp}`
+);
 
 const SidePanelItem = ({ title, children, hideBorder, ishidden }) => {
   return (
@@ -373,7 +457,7 @@ const isAllowedToWriteRfp = Near.view(
 );
 
 const link = href({
-  widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app`,
+  widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
   params: {
     page: "create-rfp",
     id: rfp.id,
@@ -384,6 +468,16 @@ const link = href({
 const createdDate = snapshotHistory[0].timestamp ?? snapshot.timestamp;
 
 const [approvedProposals, setApprovedProposals] = useState([]);
+const [isCancelModalOpen, setCancelModal] = useState(false);
+const [isWarningModalOpen, setWarningModal] = useState(false);
+const [timeline, setTimeline] = useState(null);
+const [showTimelineSetting, setShowTimelineSetting] = useState(false);
+
+useEffect(() => {
+  if (!timeline) {
+    setTimeline(snapshot.timeline);
+  }
+}, [snapshot]);
 
 function fetchApprovedRfpProposals() {
   const queryName = PROPOSAL_FEED_INDEXER_QUERY_NAME;
@@ -425,6 +519,41 @@ function fetchApprovedRfpProposals() {
   });
 }
 
+const editRFPStatus = () => {
+  Near.call([
+    {
+      contractName: REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
+      methodName: "edit_rfp_timeline",
+      args: {
+        id: rfp.id,
+        timeline: timeline,
+      },
+      gas: 270000000000000,
+    },
+  ]);
+};
+console.log({ rfp });
+const onCancelRFP = (value) => {
+  Near.call([
+    {
+      contractName: REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
+      methodName: "cancel_rfp",
+      args: {
+        id: rfp.id,
+        proposals_to_cancel:
+          value === CANCEL_RFP_OPTIONS.CANCEL_PROPOSALS
+            ? snapshot.linked_proposals
+            : [],
+        proposals_to_unlink:
+          value === CANCEL_RFP_OPTIONS.UNLINK_PROPOSALS
+            ? snapshot.linked_proposals
+            : [],
+      },
+      gas: 270000000000000,
+    },
+  ]);
+};
+
 const accessControlInfo =
   Near.view(
     REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
@@ -440,7 +569,7 @@ const SubmitProposalBtn = () => {
     <div style={{ minWidth: "fit-content" }}>
       <Link
         to={href({
-          widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app`,
+          widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
           params: { page: "create-proposal", rfp_id: rfp.id },
         })}
       >
@@ -461,6 +590,31 @@ const SubmitProposalBtn = () => {
 };
 return (
   <Container className="d-flex flex-column gap-2 w-100 mt-4">
+    <Widget
+      src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.ConfirmCancelModal`}
+      props={{
+        isOpen: isCancelModalOpen,
+        onCancelClick: () => {
+          setCancelModal(false);
+          setTimeline({ status: RFP_TIMELINE_STATUS.EVALUATION });
+        },
+        onConfirmClick: (value) => {
+          setCancelModal(false);
+          onCancelRFP(value);
+        },
+        linkedProposalIds: snapshot.linked_proposals,
+      }}
+    />
+    <Widget
+      src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.WarningModal`}
+      props={{
+        isOpen: isWarningModalOpen,
+        onConfirmClick: () => {
+          setWarningModal(false);
+          setTimeline({ status: RFP_TIMELINE_STATUS.EVALUATION });
+        },
+      }}
+    />
     <div className="d-flex px-3 px-lg-0 justify-content-between">
       <div className="d-flex gap-2 align-items-center h3">
         <div>{snapshot.name}</div>
@@ -489,7 +643,7 @@ return (
     </div>
     <div className="d-flex flex-wrap flex-md-nowrap px-3 px-lg-0 gap-2 align-items-center text-sm pb-3 w-100">
       <Widget
-        src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.rfps.StatusTag`}
+        src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.StatusTag`}
         props={{
           timelineStatus: snapshot.timeline.status,
           size: "sm",
@@ -571,7 +725,7 @@ return (
                       RFP CATEGORY
                       <div className="my-2">
                         <Widget
-                          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.MultiSelectCategoryDropdown`}
+                          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.MultiSelectCategoryDropdown`}
                           props={{
                             selected: snapshot.labels,
                             disabled: true,
@@ -596,7 +750,7 @@ return (
 
                     <div className="d-flex gap-2 align-items-center mt-4">
                       <Widget
-                        src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.LikeButton`}
+                        src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.LikeButton`}
                         props={{
                           item,
                           rfpId: rfp.id,
@@ -623,7 +777,7 @@ return (
               </div>
               <div className="border-bottom pb-4 mt-4">
                 <Widget
-                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.rfps.CommentsAndLogs`}
+                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.CommentsAndLogs`}
                   props={{
                     ...props,
                     id: rfp.id,
@@ -642,7 +796,7 @@ return (
                 className="pt-4"
               >
                 <Widget
-                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.ComposeComment`}
+                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.ComposeComment`}
                   props={{
                     ...props,
                     item: item,
@@ -673,14 +827,79 @@ return (
                   )}
                 </h5>
               </SidePanelItem>
-              <SidePanelItem title="Timeline">
+              <SidePanelItem
+                title={
+                  <div>
+                    <div className="d-flex justify-content-between align-content-center">
+                      Timeline
+                      {isAllowedToWriteRfp && (
+                        <div
+                          data-testid="setting-btn"
+                          onClick={() => setShowTimelineSetting(true)}
+                        >
+                          <i class="bi bi-gear"></i>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                }
+              >
                 <Widget
-                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.rfps.TimelineConfigurator`}
+                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.TimelineConfigurator`}
                   props={{
-                    timeline: snapshot.timeline,
-                    disabled: true,
+                    timeline: timeline,
+                    setTimeline: (v) => {
+                      if (
+                        snapshot.timeline.status === v.status &&
+                        timeline.status === v.status
+                      ) {
+                        return;
+                      }
+                      // if proposal selected timeline is selected and no approved proposals exist, show warning
+                      if (
+                        v.status === RFP_TIMELINE_STATUS.PROPOSAL_SELECTED &&
+                        Array.isArray(approvedProposals) &&
+                        !approvedProposals.length
+                      ) {
+                        setWarningModal(true);
+                      }
+
+                      if (v.status === RFP_TIMELINE_STATUS.CANCELLED) {
+                        setCancelModal(true);
+                      }
+                      setTimeline(v);
+                    },
+                    disabled: showTimelineSetting ? false : true,
                   }}
                 />
+                {showTimelineSetting && (
+                  <div className="d-flex gap-2 align-items-center justify-content-end text-sm mt-2">
+                    <Widget
+                      src={`${REPL_DEVHUB}/widget/devhub.components.molecule.Button`}
+                      props={{
+                        label: "Cancel",
+                        classNames: {
+                          root: "btn-outline-danger border-0 shadow-none btn-sm",
+                        },
+                        onClick: () => {
+                          setShowTimelineSetting(false);
+                          setTimeline(snapshot.timeline);
+                        },
+                      }}
+                    />
+                    <Widget
+                      src={`${REPL_DEVHUB}/widget/devhub.components.molecule.Button`}
+                      props={{
+                        label: "Save",
+                        classNames: { root: "blue-btn btn-sm" },
+                        onClick: () => {
+                          editRFPStatus();
+                          setShowTimelineSetting(false);
+                        },
+                      }}
+                    />
+                  </div>
+                )}
               </SidePanelItem>
               <SidePanelItem
                 title={
@@ -689,7 +908,7 @@ return (
                 ishidden={!approvedProposals.length}
               >
                 <Widget
-                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.LinkedProposals`}
+                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.LinkedProposals`}
                   props={{
                     linkedProposalIds: (approvedProposals ?? []).map(
                       (i) => i.proposal_id
@@ -708,15 +927,12 @@ return (
                 ishidden={!snapshot.linked_proposals.length}
               >
                 <Widget
-                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.LinkedProposals`}
+                  src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.LinkedProposals`}
                   props={{
                     linkedProposalIds: snapshot.linked_proposals,
-                    hideStatuses: [PROPOSAL_TIMELINE_STATUS.CANCELED],
                     showStatus:
-                      snapshot.timeline.status ===
-                        RFP_TIMELINE_STATUS.EVALUATION ||
-                      snapshot.timeline.status ===
-                        RFP_TIMELINE_STATUS.ACCEPTING_SUBMISSIONS,
+                      snapshot.timeline.status !==
+                      RFP_TIMELINE_STATUS.PROPOSAL_SELECTED,
                   }}
                 />
               </SidePanelItem>
