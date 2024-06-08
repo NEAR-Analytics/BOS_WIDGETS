187428ac3fffbf288f607a831667bfa5227c25bc
megha19.near <megha19.near>
2024-06-08 11:14:38 +0000

    Update components.rfps.Feed by megha19.near at 2024-06-08T11:14:38.226Z

--- megha19.near/components.rfps.Feed/source_code.jsx
+++ megha19.near/components.rfps.Feed/source_code.jsx
@@ -1,12 +1,98 @@
-import {
-  REPL_INFRASTRUCTURE_COMMITTEE,
-  REPL_DEVHUB,
-  REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
-  REPL_NEAR,
-  RFP_IMAGE,
-  RFP_FEED_INDEXER_QUERY_NAME,
-  fetchGraphQL,
-} from "@/includes/common";
+/*
+License: MIT
+Author: devhub.near
+Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
+*/
+/* INCLUDE: "includes/common.jsx" */
+const REPL_DEVHUB = "devhub.near";
+const REPL_INFRASTRUCTURE_COMMITTEE = "megha19.near";
+const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
+const REPL_RPC_URL = "https://rpc.mainnet.near.org";
+const REPL_NEAR = "near";
+const REPL_SOCIAL_CONTRACT = "social.near";
+const RFP_IMAGE =
+  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
+
+const RFP_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfps_with_latest_snapshot";
+
+const RFP_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfp_snapshots";
+
+const PROPOSAL_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposals_with_latest_snapshot";
+
+const PROPOSAL_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposal_snapshots";
+const RFP_TIMELINE_STATUS = {
+  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
+  EVALUATION: "EVALUATION",
+  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
+  CANCELLED: "CANCELLED",
+};
+
+const PROPOSAL_TIMELINE_STATUS = {
+  DRAFT: "DRAFT",
+  REVIEW: "REVIEW",
+  APPROVED: "APPROVED",
+  REJECTED: "REJECTED",
+  CANCELED: "CANCELLED",
+  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
+  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
+  FUNDED: "FUNDED",
+};
+
+const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  return asyncFetch(QUERYAPI_ENDPOINT, {
+    method: "POST",
+    headers: { "x-hasura-role": `polyprogrammist_near` },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
+}
+
+const CANCEL_RFP_OPTIONS = {
+  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
+  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
+  NONE: "NONE",
+};
+
+function parseJSON(json) {
+  if (typeof json === "string") {
+    try {
+      return JSON.parse(json);
+    } catch (error) {
+      return json;
+    }
+  } else {
+    return json;
+  }
+}
+
+function isNumber(value) {
+  return typeof value === "number";
+}
+
+const PROPOSALS_APPROVED_STATUS_ARRAY = [
+  PROPOSAL_TIMELINE_STATUS.APPROVED,
+  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
+  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
+  PROPOSAL_TIMELINE_STATUS.FUNDED,
+];
+
+function getLinkUsingCurrentGateway(url) {
+  const data = fetch(`https://httpbin.org/headers`);
+  const gatewayURL = data?.body?.headers?.Origin ?? "";
+  return `https://${
+    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
+  }/${url}`;
+}
+/* END_INCLUDE: "includes/common.jsx" */
 
 const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`);
 href || (href = () => {});
@@ -14,6 +100,10 @@ href || (href = () => {});
 const { readableDate } = VM.require(
   `${REPL_DEVHUB}/widget/core.lib.common`
 ) || { readableDate: () => {} };
+
+const { getGlobalLabels } = VM.require(
+  `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.core.lib.contract`
+) || { getGlobalLabels: () => {} };
 
 const Container = styled.div`
   .full-width-div {
@@ -109,10 +199,7 @@ const Heading = styled.div`
   }
 `;
 
-const rfpLabelOptions = Near.view(
-  REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
-  "get_global_labels"
-);
+const rfpLabelOptions = getGlobalLabels();
 
 const FeedItem = ({ rfp, index }) => {
   const accountId = rfp.author_id;
@@ -128,7 +215,7 @@ const FeedItem = ({ rfp, index }) => {
   return (
     <a
       href={href({
-        widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app`,
+        widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
         params: {
           page: "rfp",
           id: rfp.rfp_id,
@@ -149,7 +236,7 @@ const FeedItem = ({ rfp, index }) => {
             <div className="d-flex gap-2 align-items-center flex-wrap w-100">
               <div className="h6 mb-0 text-black">{rfp.name}</div>
               <Widget
-                src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.MultiSelectCategoryDropdown`}
+                src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.MultiSelectCategoryDropdown`}
                 props={{
                   selected: rfp.labels,
                   disabled: true,
@@ -212,7 +299,7 @@ const FeedItem = ({ rfp, index }) => {
         </div>
         <div className="align-self-center" style={{ minWidth: "fit-content" }}>
           <Widget
-            src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.rfps.StatusTag`}
+            src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.StatusTag`}
             props={{
               timelineStatus: rfp.timeline.status,
             }}
@@ -241,6 +328,7 @@ const FeedPage = () => {
     loadingMore: false,
     aggregatedCount: null,
     currentlyDisplaying: 0,
+    isFiltered: false,
   });
 
   const queryName = RFP_FEED_INDEXER_QUERY_NAME;
@@ -295,7 +383,7 @@ const FeedPage = () => {
     if (state.stage) {
       // timeline is stored as jsonb
       where = {
-        timeline: { _cast: { String: { _ilike: `%${state.stage}%` } } },
+        timeline: { _cast: { String: { _regex: `${state.stage}` } } },
         ...where,
       };
     }
@@ -306,10 +394,17 @@ const FeedPage = () => {
       }
 
       if (text) {
-        where = { description: { _ilike: `%${text}%` }, ...where };
+        where = {
+          _or: [
+            { name: { _iregex: `${text}` } },
+            { summary: { _iregex: `${text}` } },
+            { description: { _iregex: `${text}` } },
+          ],
+          ...where,
+        };
       }
     }
-
+    State.update({ isFiltered: Object.keys(where).length > 0 });
     return where;
   };
 
@@ -472,7 +567,7 @@ const FeedPage = () => {
             }}
           />
           <Widget
-            src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.FilterByLabel`}
+            src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.FilterByLabel`}
             props={{
               onStateChange: (select) => {
                 State.update({ label: select.value });
@@ -482,7 +577,7 @@ const FeedPage = () => {
           />
           <div className="d-flex gap-4 align-items-center">
             <Widget
-              src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.rfps.StageDropdown`}
+              src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.StageDropdown`}
               props={{
                 onStateChange: (select) => {
                   State.update({ stage: select.value });
@@ -495,7 +590,7 @@ const FeedPage = () => {
           <div className="mt-2 mt-xs-0">
             <Link
               to={href({
-                widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app`,
+                widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
                 params: { page: "create-rfp" },
               })}
             >
@@ -543,7 +638,19 @@ const FeedPage = () => {
                 </p>
               </div>
               <div className="mt-4 border rounded-2">
-                {state.data.length > 0 || state.aggregatedCount === 0 ? (
+                {state.aggregatedCount === 0 ? (
+                  <div className="m-2">
+                    {state.isFiltered ? (
+                      <div class="alert alert-danger" role="alert">
+                        No RFP found for selected filter.
+                      </div>
+                    ) : (
+                      <div class="alert alert-secondary" role="alert">
+                        No RFP has been created yet.
+                      </div>
+                    )}
+                  </div>
+                ) : state.aggregatedCount > 0 ? (
                   <InfiniteScroll
                     pageStart={0}
                     loadMore={makeMoreItems}
