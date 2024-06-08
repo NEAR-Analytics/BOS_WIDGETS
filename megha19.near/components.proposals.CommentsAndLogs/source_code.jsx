24f4a839ba81733efd1a3bd5af4b17baad20e688
megha19.near <megha19.near>
2024-06-08 11:14:38 +0000

    Update components.proposals.CommentsAndLogs by megha19.near at 2024-06-08T11:14:38.226Z

--- megha19.near/components.proposals.CommentsAndLogs/source_code.jsx
+++ megha19.near/components.proposals.CommentsAndLogs/source_code.jsx
@@ -1,14 +1,98 @@
-import {
-  REPL_INFRASTRUCTURE_COMMITTEE,
-  REPL_DEVHUB,
-  REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
-  REPL_NEAR,
-  RFP_IMAGE,
-  PROPOSAL_FEED_INDEXER_QUERY_NAME,
-  REPL_RPC_URL,
-  PROPOSAL_TIMELINE_STATUS,
-  isNumber,
-} from "@/includes/common";
+/*
+License: MIT
+Author: devhub.near
+Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
+*/
+/* INCLUDE: "includes/common.jsx" */
+const REPL_DEVHUB = "devhub.near";
+const REPL_INFRASTRUCTURE_COMMITTEE = "megha19.near";
+const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
+const REPL_RPC_URL = "https://rpc.mainnet.near.org";
+const REPL_NEAR = "near";
+const REPL_SOCIAL_CONTRACT = "social.near";
+const RFP_IMAGE =
+  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
+
+const RFP_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfps_with_latest_snapshot";
+
+const RFP_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_rfp_snapshots";
+
+const PROPOSAL_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposals_with_latest_snapshot";
+
+const PROPOSAL_QUERY_NAME =
+  "polyprogrammist_near_devhub_objects_s_proposal_snapshots";
+const RFP_TIMELINE_STATUS = {
+  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
+  EVALUATION: "EVALUATION",
+  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
+  CANCELLED: "CANCELLED",
+};
+
+const PROPOSAL_TIMELINE_STATUS = {
+  DRAFT: "DRAFT",
+  REVIEW: "REVIEW",
+  APPROVED: "APPROVED",
+  REJECTED: "REJECTED",
+  CANCELED: "CANCELLED",
+  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
+  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
+  FUNDED: "FUNDED",
+};
+
+const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  return asyncFetch(QUERYAPI_ENDPOINT, {
+    method: "POST",
+    headers: { "x-hasura-role": `polyprogrammist_near` },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
+}
+
+const CANCEL_RFP_OPTIONS = {
+  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
+  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
+  NONE: "NONE",
+};
+
+function parseJSON(json) {
+  if (typeof json === "string") {
+    try {
+      return JSON.parse(json);
+    } catch (error) {
+      return json;
+    }
+  } else {
+    return json;
+  }
+}
+
+function isNumber(value) {
+  return typeof value === "number";
+}
+
+const PROPOSALS_APPROVED_STATUS_ARRAY = [
+  PROPOSAL_TIMELINE_STATUS.APPROVED,
+  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
+  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
+  PROPOSAL_TIMELINE_STATUS.FUNDED,
+];
+
+function getLinkUsingCurrentGateway(url) {
+  const data = fetch(`https://httpbin.org/headers`);
+  const gatewayURL = data?.body?.headers?.Origin ?? "";
+  return `https://${
+    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
+  }/${url}`;
+}
+/* END_INCLUDE: "includes/common.jsx" */
 const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`);
 href || (href = () => {});
 
@@ -71,10 +155,11 @@ function getDifferentKeysWithValues(obj1, obj2) {
       if (key !== "editor_id" && obj2.hasOwnProperty(key)) {
         const value1 = obj1[key];
         const value2 = obj2[key];
-
-        if (typeof value1 === "object" && typeof value2 === "object") {
-          return JSON.stringify(value1) !== JSON.stringify(value2);
-        } else if (Array.isArray(value1) && Array.isArray(value2)) {
+        if (Array.isArray(value1) && Array.isArray(value2)) {
+          const sortedValue1 = [...value1].sort();
+          const sortedValue2 = [...value2].sort();
+          return JSON.stringify(sortedValue1) !== JSON.stringify(sortedValue2);
+        } else if (typeof value1 === "object" && typeof value2 === "object") {
           return JSON.stringify(value1) !== JSON.stringify(value2);
         } else {
           return value1 !== value2;
@@ -141,7 +226,10 @@ function sortTimelineAndComments() {
   });
 }
 
-sortTimelineAndComments();
+if ((snapshotHistory ?? []).length > 0) {
+  sortTimelineAndComments();
+}
+
 const Comment = ({ commentItem }) => {
   const { accountId, blockHeight } = commentItem;
   const item = {
@@ -150,7 +238,9 @@ const Comment = ({ commentItem }) => {
     blockHeight,
   };
   const content = JSON.parse(Social.get(item.path, blockHeight) ?? "null");
-  const link = `https://near.org/${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app?page=proposal&id=${props.id}&accountId=${accountId}&blockHeight=${blockHeight}`;
+  const link = getLinkUsingCurrentGateway(
+    `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app?page=proposal&id=${props.id}&accountId=${accountId}&blockHeight=${blockHeight}`
+  );
   const hightlightComment =
     parseInt(props.blockHeight ?? "") === blockHeight &&
     props.accountId === accountId;
@@ -295,6 +385,8 @@ function parseTimelineKeyAndValue(timeline, originalValue, modifiedValue) {
           </span>
         )
       );
+    case "payouts":
+      return <span>updated the funding payment links.</span>;
     default:
       return null;
   }
@@ -320,7 +412,7 @@ const LinkToRfp = ({ id, children }) => {
     <a
       className="text-decoration-underline flex-1"
       href={href({
-        widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app`,
+        widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
         params: {
           page: "rfp",
           id: id,
