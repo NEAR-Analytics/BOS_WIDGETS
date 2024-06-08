10fffc68c8615f12531481ade51014870a214869
infrastructure-committee.near <infrastructure-committee.near>
2024-06-08 12:38:26 +0000

    Update components.proposals.Editor by infrastructure-committee.near at 2024-06-08T12:38:26.705Z

--- infrastructure-committee.near/components.proposals.Editor/source_code.jsx
+++ infrastructure-committee.near/components.proposals.Editor/source_code.jsx
@@ -1,21 +1,104 @@
-import {
-  REPL_INFRASTRUCTURE_COMMITTEE,
-  REPL_DEVHUB,
-  REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
-  REPL_NEAR,
-  RFP_IMAGE,
-  PROPOSAL_FEED_INDEXER_QUERY_NAME,
-  REPL_RPC_URL,
-  RFP_TIMELINE_STATUS,
-  parseJSON,
-  isNumber,
-} from "@/includes/common";
+/*
+License: MIT
+Author: devhub.near
+Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
+*/
+/* INCLUDE: "includes/common.jsx" */
+const REPL_DEVHUB = "devhub.near";
+const REPL_INFRASTRUCTURE_COMMITTEE = "infrastructure-committee.near";
+const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT =
+  "infrastructure-committee.near";
+const REPL_RPC_URL = "https://rpc.mainnet.near.org";
+const REPL_NEAR = "near";
+const RFP_IMAGE =
+  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
+
+const RFP_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_ic_v1_rfps_with_latest_snapshot";
+
+const RFP_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_ic_v1_rfp_snapshots";
+
+const PROPOSAL_FEED_INDEXER_QUERY_NAME =
+  "polyprogrammist_near_devhub_ic_v1_proposals_with_latest_snapshot";
+
+const PROPOSAL_QUERY_NAME =
+  "polyprogrammist_near_devhub_ic_v1_proposal_snapshots";
+const RFP_TIMELINE_STATUS = {
+  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
+  EVALUATION: "EVALUATION",
+  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
+  CANCELLED: "CANCELLED",
+};
+
+const PROPOSAL_TIMELINE_STATUS = {
+  DRAFT: "DRAFT",
+  REVIEW: "REVIEW",
+  APPROVED: "APPROVED",
+  REJECTED: "REJECTED",
+  CANCELED: "CANCELLED",
+  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
+  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
+  FUNDED: "FUNDED",
+};
+
+const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  return asyncFetch(QUERYAPI_ENDPOINT, {
+    method: "POST",
+    headers: { "x-hasura-role": `polyprogrammist_near` },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
+}
+
+const CANCEL_RFP_OPTIONS = {
+  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
+  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
+  NONE: "NONE",
+};
+
+function parseJSON(json) {
+  if (typeof json === "string") {
+    try {
+      return JSON.parse(json);
+    } catch (error) {
+      return json;
+    }
+  } else {
+    return json;
+  }
+}
+
+function isNumber(value) {
+  return typeof value === "number";
+}
+
+const PROPOSALS_APPROVED_STATUS_ARRAY = [
+  PROPOSAL_TIMELINE_STATUS.APPROVED,
+  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
+  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
+  PROPOSAL_TIMELINE_STATUS.FUNDED,
+];
+
+function getLinkUsingCurrentGateway(url) {
+  const data = fetch(`https://httpbin.org/headers`);
+  const gatewayURL = data?.body?.headers?.Origin ?? "";
+  return `https://${
+    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
+  }/${url}`;
+}
+/* END_INCLUDE: "includes/common.jsx" */
 
 const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`);
 href || (href = () => {});
 
 const { getGlobalLabels } = VM.require(
-  `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.core.lib.contract`
+  `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.core.lib.contract`
 ) || { getGlobalLabels: () => {} };
 
 const { id, timestamp, rfp_id } = props;
@@ -23,9 +106,11 @@ const { id, timestamp, rfp_id } = props;
 const isEditPage = typeof id === "string";
 const author = context.accountId;
 const FundingDocs =
-  "https://near.social/${REPL_DEVHUB}/widget/app?page=community&handle=developer-dao&tab=funding";
-const ToCDocs = "";
-const CoCDocs = "";
+  "https://github.com/near/Infrastructure-Working-Group/wiki/Funding-Process-%E2%80%90-Company";
+const ToCDocs =
+  "https://github.com/near/Infrastructure-Working-Group/wiki/Terms-&-Conditions";
+const CoCDocs =
+  "https://github.com/near/Infrastructure-Working-Group/wiki/Code-Of-Conduct";
 if (!author) {
   return (
     <Widget src={`${REPL_DEVHUB}/widget/devhub.entity.proposal.LoginScreen`} />
@@ -688,14 +773,15 @@ const SubmitBtn = () => {
   const handleOptionClick = (option) => {
     setDraftBtnOpen(false);
     setSelectedStatus(option.value);
+    handleSubmit(option.value);
   };
 
   const toggleDropdown = () => {
     setDraftBtnOpen(!isDraftBtnOpen);
   };
 
-  const handleSubmit = () => {
-    const isDraft = selectedStatus === "draft";
+  const handleSubmit = (status) => {
+    const isDraft = status === "draft";
     if (isDraft) {
       onSubmit({ isDraft });
       cleanDraft();
@@ -720,7 +806,7 @@ const SubmitBtn = () => {
           }
         >
           <div
-            onClick={() => !disabledSubmitBtn && handleSubmit()}
+            onClick={() => !disabledSubmitBtn && handleSubmit(selectedStatus)}
             className="p-2 d-flex gap-2 align-items-center "
           >
             {isTxnCreated ? (
@@ -866,14 +952,13 @@ const CollapsibleContainer = ({ title, children, noPaddingTop }) => {
 const CategoryDropdown = useMemo(() => {
   return (
     <Widget
-      src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.MultiSelectCategoryDropdown`}
+      src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.MultiSelectCategoryDropdown`}
       props={{
         selected: labels,
         onChange: (v) => setLabels(v),
         disabled: linkedRfp, // when RFP is linked, labels are disabled
-        label: linkedRfp
-          ? "These categories are inherited from your selected RFP and can’t change"
-          : "Select Category",
+        linkedRfp: linkedRfp,
+
         availableOptions: rfpLabelOptions,
       }}
     />
@@ -922,7 +1007,7 @@ const SummaryComponent = useMemo(() => {
 const DescriptionComponent = useMemo(() => {
   return (
     <Widget
-      src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.Compose`}
+      src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.Compose`}
       props={{
         data: description,
         onChange: setDescription,
@@ -1008,11 +1093,12 @@ const LinkRFPComponent = useMemo(() => {
   return (
     <div className="d-flex flex-column gap-1">
       <Widget
-        src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.LinkedRfpDropdown`}
+        src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.LinkedRfpDropdown`}
         props={{
           onChange: setLinkedRfp,
           linkedRfp: linkedRfp,
           disabled: disabledLinkRFP,
+          onDeleteRfp: () => setLabels([]),
         }}
       />
     </div>
@@ -1026,7 +1112,7 @@ const LinkedProposalsComponent = useMemo(() => {
         Link any relevant proposals (e.g. previous milestones).
       </div>
       <Widget
-        src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.molecule.LinkedProposalsDropdown`}
+        src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.LinkedProposalsDropdown`}
         props={{
           onChange: setLinkedProposals,
           linkedProposals: linkedProposals,
@@ -1092,7 +1178,7 @@ return (
       {isEditPage ? "Edit" : "Create"} Proposal
     </Heading>
     <Widget
-      src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.proposals.ViewProposalModal`}
+      src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.proposals.ViewProposalModal`}
       props={{
         isOpen: showProposalViewModal,
         isEdit: isEditPage,
@@ -1228,14 +1314,14 @@ return (
                     to={
                       isEditPage
                         ? href({
-                            widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app`,
+                            widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
                             params: {
                               page: "proposal",
                               id: parseInt(id),
                             },
                           })
                         : href({
-                            widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/near-prpsls-bos.components.pages.app`,
+                            widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
                             params: {
                               page: "proposals",
                             },
