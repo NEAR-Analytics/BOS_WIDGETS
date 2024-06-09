00b082c52d1fad00f811634d4590e49c230bd478
saidulbadhon.near <saidulbadhon.near>
2024-06-09 13:41:43 +0000

    Update bento-template by saidulbadhon.near at 2024-06-09T13:41:43.834Z

---
+++ saidulbadhon.near/bento-template/source_code.jsx
@@ -0,0 +1,203 @@
+return (
+  <div>
+    <Widget
+      id="ihjjIti0zb"
+      src="adnansiddiqui.near/widget/template-5.Navbar"
+      props={{
+        props: {
+          navItems: [
+            { name: "Company", link: "#" },
+            { name: "Technology", link: "#" },
+            { name: "Solution", link: "#" },
+            { name: "Latest news", link: "#" },
+            { name: "Careers", link: "#" },
+          ],
+          buttonText: "Contact",
+          devLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/dev/Navbar.js",
+          prodLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/prod/Navbar.js",
+          nearSrc: "adnansiddiqui.near/widget/template-5.Navbar",
+        },
+        style: {},
+      }}
+    />
+    <Widget
+      id="LDTE6q96pV"
+      src="adnansiddiqui.near/widget/template-5.HeroSection"
+      props={{
+        props: {
+          backgroundImageUrl:
+            "https://source.unsplash.com/random/1920x1080/?chemistry",
+          headerText: "Changing chemistry for good",
+          subHeaderText:
+            "We are a team of passionate cyclists dedicated to providing high-quality bikes and accessories to our customers.",
+          primaryButtonText: "Contact Us",
+          secondaryButtonText: "View Collection",
+          devLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/dev/HeroSection.js",
+          prodLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/prod/HeroSection.js",
+          nearSrc: "adnansiddiqui.near/widget/template-5.HeroSection",
+        },
+        style: {},
+      }}
+    />
+    <Widget
+      id="qhDo340Ma5"
+      src="adnansiddiqui.near/widget/template-5.About"
+      props={{
+        props: {
+          headerText:
+            "The global, $5 trillion chemical industry faces a reckoning. Chemical production is expected to quadruple by 2050 to meet ever-increasing demand. But the industry must cut greenhouse gas emissions by more than half to meet sustainability goals.",
+          moreInfo: [
+            {
+              title: "Revolutionizing Chemistry",
+              description:
+                "At EnginZyme, we're transforming the chemical industry by harnessing the power of enzymes. Our technology enables cleaner, more efficient production processes, significantly reducing the sector's carbon footprint.",
+            },
+            {
+              title: "Sustainable Solutions",
+              description:
+                "Our enzyme-based catalysts are designed to be both eco-friendly and economically viable. By mimicking nature's methods, we provide an alternative to harsh chemical processes that are sustainable and scalable.",
+            },
+            {
+              title: "Precision Manufacturing",
+              description:
+                "Leveraging biotechnology, EnginZyme delivers unprecedented precision in chemical manufacturing. Our tailored enzyme solutions optimize reactions for maximum yield and purity, ensuring high-quality output with minimal waste.",
+            },
+          ],
+          buttonText: "Get started",
+          devLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/dev/About.js",
+          prodLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/prod/About.js",
+          nearSrc: "adnansiddiqui.near/widget/template-5.About",
+        },
+        style: {},
+      }}
+    />
+    <Widget
+      id="i7brGhnIGB"
+      src="adnansiddiqui.near/widget/template-5.Article"
+      props={{
+        props: {
+          backgroundImageUrl:
+            "https://source.unsplash.com/random/1920x1080/?lab%20article",
+          headerText: "We need the chemistry industry but it needs to change",
+          subHeaderText:
+            "Even as the world transitions to renewable energy, we still need the chemistry industry to produce the materials that make modern life possible. But the industry must change to meet sustainability goals.",
+          primaryButtonText: "Read more",
+          secondaryButtonText: "View all articles",
+          devLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/dev/Article.js",
+          prodLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/prod/Article.js",
+          nearSrc: "adnansiddiqui.near/widget/template-5.Article",
+        },
+        style: {},
+      }}
+    />
+    <Widget
+      id="IR5T4DH-7m"
+      src="adnansiddiqui.near/widget/template-5.News"
+      props={{
+        props: {
+          headerText: "Latest news, events & press",
+          subHeaderText:
+            "Stay up to date with the latest news, events and press releases from EnginZyme.",
+          buttonText: "View all",
+          news: [
+            {
+              article:
+                "Even as the world transitions to renewable energy, we still need the chemistry industry to produce the materials that make modern life possible. But the industry must change to meet sustainability goals.",
+            },
+            {
+              article:
+                "Even as the world transitions to renewable energy, we still need the chemistry industry to produce the materials that make modern life possible. But the industry must change to meet sustainability goals.",
+            },
+          ],
+          devLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/dev/News.js",
+          prodLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/prod/News.js",
+          nearSrc: "adnansiddiqui.near/widget/template-5.News",
+        },
+        style: {},
+      }}
+    />
+    <Widget
+      id="0ug75y7P49"
+      src="undefined"
+      props={{
+        props: {
+          headerText: "Pattented technology",
+          subHeaderText:
+            "Our patented enzyme immobilisation technology enables best of both worlds  biomanufacturing. We create bespoke, stable biocatalysts that can be used in multi-step enzymatic reactions, applying lessons from nature to modern manufacturing.",
+          primaryButtonText: "Get started",
+          secondaryButtonText: "Learn more",
+          devLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/dev/PrimaryCTA.js",
+          prodLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/prod/PrimaryCTA.js",
+        },
+        style: {},
+      }}
+    />
+    <Widget
+      id="z8unV6D2-L"
+      src="adnansiddiqui.near/widget/template-5.SecondaryCTA"
+      props={{
+        props: {
+          headerText: "Join enginzyme and impact tomorrow today",
+          subHeaderText:
+            "Could your company benefit from a cleaner, less energy-intensive manufacturing process? Let's talk about it.",
+          primaryButtonText: "Join Us",
+          secondaryButtonText: "Contact Us",
+          devLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/dev/SecondaryCTA.js",
+          prodLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/prod/SecondaryCTA.js",
+          nearSrc: "adnansiddiqui.near/widget/template-5.SecondaryCTA",
+        },
+        style: {},
+      }}
+    />
+    <Widget
+      id="x7Wr6CI5vt"
+      src="adnansiddiqui.near/widget/template-5.Footer"
+      props={{
+        props: {
+          legal: [
+            { name: "Privacy Policy", link: "#" },
+            { name: "Terms and Conditions", link: "#" },
+            { name: "Cookies Policy", link: "#" },
+            { name: "Disclaimer", link: "#" },
+            { name: "Refund Policy", link: "#" },
+          ],
+          service: [
+            { name: "FAQ", link: "#" },
+            { name: "Contact Us", link: "#" },
+            { name: "Support", link: "#" },
+            { name: "Live Chat", link: "#" },
+            { name: "Community Forum", link: "#" },
+          ],
+          information: [
+            { name: "Company Info", link: "#" },
+            { name: "Careers", link: "#" },
+            { name: "Press", link: "#" },
+            { name: "Our Team", link: "#" },
+            { name: "Blog", link: "#" },
+          ],
+          copyRight: "2023 Jutsu. All Rights Reserved.",
+          devLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/dev/Footer.js",
+          prodLink:
+            "https://raw.githubusercontent.com/jutsuai/bento-templates/main/template-5/src/components/prod/Footer.js",
+          nearSrc: "adnansiddiqui.near/widget/template-5.Footer",
+        },
+        style: {},
+      }}
+    />
+  </div>
+);
