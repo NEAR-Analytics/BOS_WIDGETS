52750ad0c10027327b8b24ec70d9c0d25afe3ba8
adnansiddiqui.near <adnansiddiqui.near>
2024-06-10 13:33:23 +0000

    Update template-5.Article by adnansiddiqui.near at 2024-06-10T13:33:23.287Z

--- adnansiddiqui.near/template-5.Article/source_code.jsx
+++ adnansiddiqui.near/template-5.Article/source_code.jsx
@@ -29,7 +29,10 @@ const ArticleWrapper = styled.div`
       }px ${style?.marginLeft || 0}px`};
 
     background-image: ${({ props }) =>
-      `url(https://images.unsplash.com/photo-1533134486753-c833f0ed4866?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`};
+      `url(${
+        props?.backgroundImageUrl ||
+        "https://images.unsplash.com/photo-1533134486753-c833f0ed4866?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      })`};
     background-size: cover;
     background-position: center;
   `;
