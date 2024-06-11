e67aac8008dd7dc49bd9a9cef185b3ac8ca60e92
nearblocksonbos.near <nearblocksonbos.near>
2024-06-11 06:41:50 +0000

    Update lite.Home by nearblocksonbos.near at 2024-06-11T06:41:50.245Z

---
+++ nearblocksonbos.near/lite.Home/source_code.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <div className="relative container mx-auto">
+      <div className="py-[58px] px-6">
+        <h1 className="font-heading font-bold text-[49px] tracking-[1px]">
+          Hello, I’m the Near stateless explorer
+        </h1>
+        <h2 className="font-heading font-medium text-[40px] tracking-[0.4px] mt-[-3px]">
+          With me, you can check real-time data on the NEAR blockchain.
+        </h2>
+      </div>
+      <hr className="h-px border-0 border-b border-primary/20" />
+      <Widget key="chart" src={`nearblocksonbos.near/widget/lite.Home.Chart`} />
+      <Widget key="stats" src={`nearblocksonbos.near/widget/lite.Home.Stats`} />
+    </div>
+  );
+};
+return App(props);
