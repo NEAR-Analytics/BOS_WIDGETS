26481a1970f526aae05530ea0e6b9c5ef5cc38d2
nearblocksonbos.near <nearblocksonbos.near>
2024-06-12 18:24:59 +0000

    Update lite.Atoms.Error by nearblocksonbos.near at 2024-06-12T18:24:59.507Z

--- nearblocksonbos.near/lite.Atoms.Error/source_code.jsx
+++ nearblocksonbos.near/lite.Atoms.Error/source_code.jsx
@@ -1,16 +1,18 @@
+let ErrorIconSkeleton = window?.ErrorIconSkeleton || (() => <></>);
 const Error = ({ text, title }) => {
   return (
     <div className="relative container mx-auto">
       <div className="flex flex-col items-center py-10 px-5">
         <Widget
           key="warning-icon"
+          loading={<ErrorIconSkeleton />}
           props={{ className: "text-text-warning w-12 mb-2" }}
           src={`nearblocksonbos.near/widget/lite.Icons.Warning`}
         />
-        <h1 className="font-heading font-medium text-xl tracking-[0.1px]">
+        <h1 className="font-heading font-medium text-xl text-center tracking-[0.1px]">
           {title}
         </h1>
-        <p className="text-text-label text-sm mt-1">
+        <p className="text-text-label text-sm text-center mt-1">
           {text ?? "Please try again or try changing the RPC endpoint"}
         </p>
       </div>
