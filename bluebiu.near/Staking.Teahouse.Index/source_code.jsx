fbaa83c3da53fcc6f7e6e8c6e831cc4af538d01a
bluebiu.near <bluebiu.near>
2024-06-14 09:20:05 +0000

    Update Staking.Teahouse.Index by bluebiu.near at 2024-06-14T09:20:05.871Z

--- bluebiu.near/Staking.Teahouse.Index/source_code.jsx
+++ bluebiu.near/Staking.Teahouse.Index/source_code.jsx
@@ -1,12 +1,52 @@
 const StyledContainer = styled.div``;
+const StyledHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-const { dexConfig } = props;
+const { dexConfig, CHAIN_LIST, curChain, onSwitchChain } = props;
+let tabsArray = [
+  { key: "LP", label: "LP" },
+  { key: "MANAGED", label: "Managed" },
+];
+State.init({
+  tab: "MANAGED",
+});
 return (
   <StyledContainer style={dexConfig.theme}>
+    <StyledHeader>
+      <Widget
+        src="bluebiu.near/widget/Lending.CardTabs"
+        props={{
+          tabs: tabsArray,
+          active: state.tab,
+          onChange: (tab) => {
+            State.update({
+              tab: tab.key,
+            });
+          },
+        }}
+      />
+      <Widget
+        src="bluebiu.near/widget/Lending.Chains"
+        props={{
+          chains: CHAIN_LIST,
+          curChain,
+          onSwitchChain,
+          onChange: (tab) => {
+            State.update({
+              tab: tab.key,
+            });
+          },
+        }}
+      />
+    </StyledHeader>
     <Widget
       src="bluebiu.near/widget/Staking.Teahouse.Content"
       props={{
         ...props,
+        tab: state.tab,
       }}
     />
   </StyledContainer>
