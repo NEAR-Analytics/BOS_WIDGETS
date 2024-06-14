0dfc08424db361b71ebe5f684570f380c9013214
builddao.near <builddao.near>
2024-06-14 16:36:18 +0000

    Update page.project.tabs.Roadmap by builddao.near at 2024-06-14T16:36:18.780Z

---
+++ builddao.near/page.project.tabs.Roadmap/source_code.jsx
@@ -0,0 +1,21 @@
+const { Button } = VM.require("buildhub.near/widget/components") || {
+  Button: () => <></>,
+};
+const { getProjectMeta } = VM.require("builddao.near/widget/lib.projects") || {
+  getProjectMeta: () => {},
+};
+const { id } = props;
+const project = getProjectMeta(id);
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  justify-content: center;
+  .link {
+    display: flex;
+    gap: 16px;
+    flex-direction: row;
+    align-items: center;
+  }
+`;
+return <Container className="text-white"></Container>;
