34437a3abcf9f347b2158e085db9bcedb7ed458a
builddao.near <builddao.near>
2024-06-20 21:42:31 +0000

    Update CSS by builddao.near at 2024-06-20T21:42:31.004Z

--- builddao.near/CSS/source_code.jsx
+++ builddao.near/CSS/source_code.jsx
@@ -165,5 +165,11 @@ const CSS = styled.div`
   .placeholder-glow {
     background: var(--bg-1, black);
   }
+  .content {
+    background: var(--bg-1, #000);
+    .main {
+      min-width: 0;
+    }
+  }
 `;
 return { CSS };
