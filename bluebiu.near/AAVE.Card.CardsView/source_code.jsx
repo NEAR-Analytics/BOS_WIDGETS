ca90480d8251cf60a25aa370636e930bb1800ae1
bluebiu.near <bluebiu.near>
2024-06-24 19:53:42 +0000

    Update AAVE.Card.CardsView by bluebiu.near at 2024-06-24T19:53:42.157Z

--- bluebiu.near/AAVE.Card.CardsView/source_code.jsx
+++ bluebiu.near/AAVE.Card.CardsView/source_code.jsx
@@ -2,23 +2,20 @@ const { title, body, style, config } = props;
 
 const CardsContainer = styled.div`
   border-radius: 10px;
-  background: var(--agg-bg-color, #151718);
-
+  /* background: var(--agg-bg-color, #151718); */
   padding: 18px 0;
 `;
 
 const CardsTitle = styled.div`
   color: white;
-
   font-size: 14px;
   font-weight: 800;
-
   padding: 0 14px;
 `;
 
 return (
   <CardsContainer style={style}>
-    <CardsTitle>{title}</CardsTitle>
+    {/* <CardsTitle>{title}</CardsTitle> */}
     {body}
   </CardsContainer>
 );
