36752ab78982c01b8dea5958942468c451569fae
bluebiu.near <bluebiu.near>
2024-06-24 19:53:42 +0000

    Update AAVE.Modal.RoundedCard by bluebiu.near at 2024-06-24T19:53:42.157Z

--- bluebiu.near/AAVE.Modal.RoundedCard/source_code.jsx
+++ bluebiu.near/AAVE.Modal.RoundedCard/source_code.jsx
@@ -1,13 +1,13 @@
 const Title = styled.div`
   font-size: 14px;
   font-weight: 500;
-
+  color: var(--agg-primary-color, #fff);
   margin-bottom: 10px;
 `;
 
 const Content = styled.div`
   padding: 18px 14px;
-  background: rgba(0, 0, 0, 0.26);
+  background: var(--agg-secondary-color, rgba(0, 0, 0, 0.26));
   border: 1px solid #383947;
   border-radius: 10px;
 `;
