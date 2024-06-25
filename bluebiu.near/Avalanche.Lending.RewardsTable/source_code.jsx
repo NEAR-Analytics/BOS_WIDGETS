baa0391a93c67506e412d71e95c0084d0ff3ad76
bluebiu.near <bluebiu.near>
2024-06-25 07:17:16 +0000

    Update Avalanche.Lending.RewardsTable by bluebiu.near at 2024-06-25T07:17:16.650Z

--- bluebiu.near/Avalanche.Lending.RewardsTable/source_code.jsx
+++ bluebiu.near/Avalanche.Lending.RewardsTable/source_code.jsx
@@ -131,7 +131,7 @@ return (
             toast?.fail({
               title: err?.message?.includes("user rejected transaction")
                 ? "User rejected transaction"
-                : `Claimed faily!`,
+                : ` Claim failed!`,
               chainId,
             });
           },
