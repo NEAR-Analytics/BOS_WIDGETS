9b927b2e52b0b93753cbf1178c9e6ad93aeda042
bluebiu.near <bluebiu.near>
2024-06-25 08:48:24 +0000

    Update Liquidity.Bridge.Filter by bluebiu.near at 2024-06-25T08:48:24.650Z

--- bluebiu.near/Liquidity.Bridge.Filter/source_code.jsx
+++ bluebiu.near/Liquidity.Bridge.Filter/source_code.jsx
@@ -8,18 +8,49 @@ const Pools = styled.div`
   padding: 4px;
   border: 1px solid #373A53;
   border-radius: 8px;
-  background: rgba(33, 35, 48, 0.5);
+  background: var(--agg-secondary-color,rgba(33, 35, 48, 0.5));
+  &.layer {
+    border-radius: 22px;
+  }
 `
 const Pool = styled.div`
   cursor: pointer;
+  color: var(--agg-primary-color,#FFF);
+  font-size: 14px;
   padding: 8px 10px;
   border-radius: 8px;
-  color: #FFF;
-  font-size: 14px;
   border: 1px solid transparent;
   &.active {
     border-color: #373A53;
     background: #32364B;
+  }
+  &.layer {
+    border-radius: 22px;
+    display: flex;
+    align-items: center;
+    padding: 0 26px;
+    height: 44px;
+    font-family: "Inter Tight";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    border: none;
+    &.active {
+      color: #FFF;
+      background: #000;
+    }
+  }
+`
+const StyledSearchAndChainsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex: 1;
+  &.layer {
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 20px;
   }
 `
 const Search = styled.div`
@@ -29,7 +60,7 @@ const Search = styled.div`
   padding-left: 40px;
   border-radius: 8px;
   border: 1px solid #373A53;
-  background: rgba(33, 35, 48, 0.5);
+  background: var(--agg-secondary-color,rgba(33, 35, 48, 0.5));
 `
 const Input = styled.input`
   border: none;
@@ -38,7 +69,7 @@ const Input = styled.input`
   padding: 0;
   width: 100%;
   height: 100%;
-  color: #FFF;
+  color: var(--agg-primary-color, #FFF);
   font-size: 14px;
 `;
 const SvgIcon = styled.div`
@@ -51,7 +82,7 @@ const SvgIcon = styled.div`
     top: 12px;
   }
 `
-const ChainsWrapper = styled.div`
+const ChainsOrMarketsWrapper = styled.div`
   flex: 1;
   display: flex;
   justify-content: flex-end;
@@ -61,7 +92,7 @@ const Chains = styled.div`
   padding: 4px;
   border: 1px solid #373A53;
   border-radius: 8px;
-  background: rgba(33, 35, 48, 0.5);
+  background: var(--agg-secondary-color,rgba(33, 35, 48, 0.5));
 `
 const Chain = styled.div`
   cursor: pointer;
@@ -76,6 +107,41 @@ const Chain = styled.div`
     background: #32364B;
   }
 `
+const Markets = styled.div`
+  /* width: 388px; */
+  display: flex;
+  align-items: center;
+  height: 40px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  border: 1px solid #000;
+  overflow: hidden;
+`
+const Market = styled.div`
+  cursor: pointer;
+  padding: 10px 15px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  img {
+    width: 20px;
+  }
+  span {
+    color: #000;
+    font-family: "Inter Tight";
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
+  &.active {
+    background: #000;
+    span {
+      color: #FFF;
+    }
+  }
+`
+
 
 
 // const SvgIconSearch = styled("Switch.Root")`
@@ -84,6 +150,9 @@ const Chain = styled.div`
 // `
 
 const {
+  from,
+  markets,
+  currentMarket,
   token,
   chains,
   categoryIndex,
@@ -91,6 +160,7 @@ const {
   onSearchInput,
   onChangeCategoryIndex,
   onChangeChainIndex,
+  onChangeMarket
 } = props
 const SearchIcon = (
   <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -98,26 +168,47 @@ const SearchIcon = (
     <rect x="14.9141" y="9.64941" width="6.141" height="2.63186" rx="1.31593" transform="rotate(30 14.9141 9.64941)" fill="#979ABE" />
   </svg>
 )
+console.log('=markets', markets)
 return (
   <Wrapper>
-    <Pools>
-      <Pool className={categoryIndex === 0 ? 'active' : ''} onClick={() => onChangeCategoryIndex(0)}>All pools</Pool>
-      <Pool className={categoryIndex === 1 ? 'active' : ''} onClick={() => onChangeCategoryIndex(1)}>Your pools</Pool>
+    <Pools className={[from].join(' ')}>
+      <Pool className={[from, categoryIndex === 0 ? 'active' : ''].join(' ')} onClick={() => onChangeCategoryIndex(0)}>All pools</Pool>
+      <Pool className={[from, categoryIndex === 1 ? 'active' : ''].join(' ')} onClick={() => onChangeCategoryIndex(1)}>Your pools</Pool>
     </Pools>
-    <Search>
-      <SvgIcon className="icon-search">
-        {SearchIcon}
-      </SvgIcon>
-      <Input placeholder="search by token" onInput={(event) => onSearchInput(event)} />
-    </Search>
-    <ChainsWrapper>
-      <Chains>
-        {chains && chains.map((chain, index) => {
-          return <Chain key={index} className={index === chainIndex ? 'active' : ''} onClick={() => onChangeChainIndex(index)}>
-            <img src={chain.logo} alt={chain.name} />
-          </Chain>
-        })}
-      </Chains>
-    </ChainsWrapper>
-  </Wrapper>
+    <StyledSearchAndChainsContainer className={[from].join(' ')}>
+      <Search>
+        <SvgIcon className="icon-search">
+          {SearchIcon}
+        </SvgIcon>
+        <Input placeholder="search by token" onInput={(event) => onSearchInput(event)} />
+      </Search>
+      <ChainsOrMarketsWrapper>
+        {
+          from === 'layer' ? (
+            <Markets>
+              {
+                markets && markets.map((market, index) => {
+                  return (
+                    <Market key={index} className={[currentMarket?.basic?.name === market?.basic?.name ? 'active' : '']} onClick={() => onChangeMarket && onChangeMarket(market)}>
+                      <img src={market?.basic?.icon} alt={market?.basic?.name} />
+                      <span>{market?.basic?.name}</span>
+                    </Market>
+                  )
+                })
+              }
+            </Markets>
+          ) : (
+            <Chains>
+              {chains && chains.map((chain, index) => {
+                return <Chain key={index} className={index === chainIndex ? 'active' : ''} onClick={() => onChangeChainIndex(index)}>
+                  <img src={chain.logo} alt={chain.name} />
+                </Chain>
+              })}
+            </Chains>
+          )
+        }
+
+      </ChainsOrMarketsWrapper>
+    </StyledSearchAndChainsContainer>
+  </Wrapper >
 )
