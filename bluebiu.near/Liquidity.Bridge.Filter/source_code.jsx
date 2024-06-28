ee6a81c62f5ca4c39f26ebf00aebb3cf1a3cca8d
bluebiu.near <bluebiu.near>
2024-06-28 03:19:22 +0000

    Update Liquidity.Bridge.Filter by bluebiu.near at 2024-06-28T03:19:22.436Z

--- bluebiu.near/Liquidity.Bridge.Filter/source_code.jsx
+++ bluebiu.near/Liquidity.Bridge.Filter/source_code.jsx
@@ -164,7 +164,7 @@ const {
 } = props
 const SearchIcon = (
   <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="7.01829" cy="7.01829" r="6.01829" stroke="#979ABE" stroke-width="2" />
+    <circle cx="7.01829" cy="7.01829" r="6.01829" stroke="#979ABE" strokeWidth="2" />
     <rect x="14.9141" y="9.64941" width="6.141" height="2.63186" rx="1.31593" transform="rotate(30 14.9141 9.64941)" fill="#979ABE" />
   </svg>
 )
