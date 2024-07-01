8d60bc30e27d9fd96f42ab30c39dee345cad0f02
bos.forum.potlock.near <bos.forum.potlock.near>
2024-07-01 09:04:47 +0000

    Update components.core.lib.contract by bos.forum.potlock.near at 2024-07-01T09:04:47.346Z

---
+++ bos.forum.potlock.near/components.core.lib.contract/source_code.jsx
@@ -0,0 +1,18 @@
+function ensureOtherIsLast(arr) {
+  const otherIndex = (arr ?? []).findIndex((item) => item.value === "Other");
+  if (otherIndex !== -1) {
+    const [otherItem] = arr.splice(otherIndex, 1);
+    arr.push(otherItem);
+  }
+  return arr;
+}
+function getGlobalLabels() {
+  let labels = Near.view("forum.potlock.near", "get_global_labels");
+  if (labels !== null) {
+    labels = ensureOtherIsLast(labels);
+  }
+  return labels ?? null;
+}
+return {
+  getGlobalLabels,
+};
