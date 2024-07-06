0038c645b004a8f0ddc418f48bfb64eda65385ba
kirrruuusha.near <kirrruuusha.near>
2024-07-06 21:58:59 +0000

    Update New-Draft-0 by kirrruuusha.near at 2024-07-06T21:58:59.638Z

--- kirrruuusha.near/New-Draft-0/source_code.jsx
+++ kirrruuusha.near/New-Draft-0/source_code.jsx
@@ -686,7 +686,6 @@ return (
                     "Edit"
                   )}
                 </Button>
-
                 {/* Button to close hidden content for the student */}
                 <Button
                   style={{ width: "100px", marginLeft: "1rem" }}
