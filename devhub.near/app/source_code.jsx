a71ee5451c590ef702433d436fe6d48cd79ea49b
devhub.near <devhub.near>
2024-07-20 20:56:13 +0000

    Update app by devhub.near at 2024-07-20T20:56:13.151Z

--- devhub.near/app/source_code.jsx
+++ devhub.near/app/source_code.jsx
@@ -1,182 +1,81 @@
 /**
- * This is the main entry point for the DevHub application.
+ * This is the main entry point for the RFP application.
  * Page route gets passed in through params, along with all other page props.
  */
-
-const { onDraftStateChange } = VM.require(
-  "devhub.near/widget/devhub.entity.post.draft"
-);
-
 const { page, ...passProps } = props;
-
 // Import our modules
 const { AppLayout } = VM.require(
-  "devhub.near/widget/devhub.components.templates.AppLayout"
+  `infrastructure-committee.near/widget/components.template.AppLayout`
 );
-
 if (!AppLayout) {
   return <p>Loading modules...</p>;
 }
-
 // CSS styles to be used across the app.
 // Define fonts here, as well as any other global styles.
 const Theme = styled.div`
   a {
     color: inherit;
   }
-
   .attractable {
     box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important;
     transition: box-shadow 0.6s;
-
     &:hover {
       box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
     }
   }
 `;
-
 if (!page) {
   // If no page is specified, we default to the feed page TEMP
-  page = "home";
+  page = "about";
 }
-
-// Track visits
-
-if ("phc_es19zuLOCXpiyOGqBDkBrH7MaL77ggqJMjy8mpR1623".length === 47) {
-  useEffect(() => {
-    const hashedUserId = context.accountId
-      ? Array.from(nacl.hash(Buffer.from(context.accountId)))
-          .map((b) => ("00" + b.toString(16)).slice(-2))
-          .join("")
-      : "unauthenticated";
-
-    fetch("https://eu.posthog.com/capture/", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-
-      body: JSON.stringify({
-        api_key: "phc_es19zuLOCXpiyOGqBDkBrH7MaL77ggqJMjy8mpR1623",
-        event: "devhub_pageview",
-        properties: {
-          distinct_id: hashedUserId,
-          page,
-          ...props,
-        },
-        timestamp: new Date().toISOString(),
-      }),
-    });
-  }, [props]);
-}
-
 // This is our navigation, rendering the page based on the page parameter
 function Page() {
   const routes = page.split(".");
   switch (routes[0]) {
-    case "home": {
+    case "about": {
       return (
         <Widget
-          src="devhub.near/widget/devhub.page.home"
+          src={`infrastructure-committee.near/widget/components.pages.about`}
           props={passProps}
         />
       );
     }
-    // ?page=communities
-    case "communities": {
+    case "rfps": {
       return (
         <Widget
-          src={"devhub.near/widget/devhub.page.communities"}
+          src={`infrastructure-committee.near/widget/components.rfps.Feed`}
           props={passProps}
         />
       );
     }
-    case "announcements": {
+    case "rfp": {
       return (
         <Widget
-          src={"devhub.near/widget/devhub.page.announcements"}
+          src={`infrastructure-committee.near/widget/components.rfps.Rfp`}
           props={passProps}
         />
       );
     }
-
-    // ?page=community
-    case "community": {
-      return (
-        // Considering to consolidate this into a single widget,
-        // where each level handles its own routing.
-        // Modularizing a page just like we do with addons
-        <Widget
-          src={"devhub.near/widget/devhub.entity.community.Provider"}
-          props={{
-            ...passProps,
-            Children: (p) => {
-              // passing props from the Provider into the Children
-              switch (routes[1]) {
-                // ?page=community.configuration
-                case "configuration": {
-                  return (
-                    <Widget
-                      src={
-                        "devhub.near/widget/devhub.page.community.configuration"
-                      }
-                      props={{
-                        ...passProps,
-                        ...p,
-                      }}
-                    />
-                  );
-                }
-                // ?page=community
-                default: {
-                  return (
-                    <Widget
-                      src={"devhub.near/widget/devhub.page.community.index"}
-                      props={{
-                        ...passProps,
-                        ...p,
-                      }}
-                    />
-                  );
-                }
-              }
-            },
-          }}
-        />
-      );
-    }
-    // ?page=feed
-    case "feed": {
+    case "create-rfp": {
       return (
         <Widget
-          src={"devhub.near/widget/devhub.page.feed"}
+          src={`infrastructure-committee.near/widget/components.rfps.Editor`}
           props={passProps}
         />
       );
     }
-    // ?page=create
-    case "create": {
-      return (
-        <Widget
-          src={"devhub.near/widget/devhub.entity.post.PostEditor"}
-          props={{ ...passProps, isCreatePostPage: true, onDraftStateChange }}
-        />
-      );
-    }
-
     case "create-proposal": {
       return (
         <Widget
-          src={"devhub.near/widget/devhub.entity.proposal.Editor"}
+          src={`infrastructure-committee.near/widget/components.proposals.Editor`}
           props={{ ...passProps }}
         />
       );
     }
-
     case "proposals": {
       return (
         <Widget
-          src={"devhub.near/widget/devhub.page.proposals"}
+          src={`infrastructure-committee.near/widget/components.proposals.Feed`}
           props={passProps}
         />
       );
@@ -184,57 +83,15 @@ function Page() {
     case "proposal": {
       return (
         <Widget
-          src={"devhub.near/widget/devhub.entity.proposal.Proposal"}
+          src={`infrastructure-committee.near/widget/components.proposals.Proposal`}
           props={passProps}
         />
       );
     }
-    // ?page=about
     case "about": {
       return (
         <Widget
-          src={"devhub.near/widget/devhub.page.about"}
-          props={passProps}
-        />
-      );
-    }
-    case "contribute": {
-      return (
-        <Widget
-          src={"devhub.near/widget/devhub.page.contribute"}
-          props={passProps}
-        />
-      );
-    }
-    case "profile": {
-      return (
-        <Widget
-          src={"devhub.near/widget/devhub.page.profile"}
-          props={passProps}
-        />
-      );
-    }
-    // ?page=blog
-    case "blog": {
-      return (
-        <Widget
-          src={"devhub.near/widget/devhub.page.blog"}
-          props={passProps}
-        />
-      );
-    }
-    case "blogv2": {
-      return (
-        <Widget
-          src={"devhub.near/widget/devhub.page.blogv2"}
-          props={passProps}
-        />
-      );
-    }
-    case "post": {
-      return (
-        <Widget
-          src={"devhub.near/widget/devhub.page.post"}
+          src={`infrastructure-committee.near/widget/components.pages.about`}
           props={passProps}
         />
       );
@@ -242,7 +99,7 @@ function Page() {
     case "admin": {
       return (
         <Widget
-          src={"devhub.near/widget/devhub.page.admin.index"}
+          src={`infrastructure-committee.near/widget/components.pages.admin`}
           props={passProps}
         />
       );
@@ -253,7 +110,6 @@ function Page() {
     }
   }
 }
-
 return (
   <Theme>
     <AppLayout page={page}>
