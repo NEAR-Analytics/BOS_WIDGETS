73335066b69f32d07145978f46b0269b53c81904
infrastructure-committee.near <infrastructure-committee.near>
2024-07-24 17:38:30 +0000

    Update components.molecule.LinkedRfps by infrastructure-committee.near at 2024-07-24T17:38:30.779Z

--- infrastructure-committee.near/components.molecule.LinkedRfps/source_code.jsx
+++ infrastructure-committee.near/components.molecule.LinkedRfps/source_code.jsx
@@ -1,119 +1,19 @@
-/*
-License: MIT
-Author: devhub.near
-Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
-*/
-/* INCLUDE: "includes/common.jsx" */
-const REPL_DEVHUB = "devhub.near";
-const REPL_INFRASTRUCTURE_COMMITTEE = "infrastructure-committee.near";
-const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT =
-  "infrastructure-committee.near";
-const REPL_RPC_URL = "https://rpc.mainnet.near.org";
-const REPL_NEAR = "near";
-const RFP_IMAGE =
-  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
-
-const RFP_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_rfps_with_latest_snapshot";
-
-const RFP_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_rfp_snapshots";
-
-const PROPOSAL_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_proposals_with_latest_snapshot";
-
-const PROPOSAL_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_proposal_snapshots";
-const RFP_TIMELINE_STATUS = {
-  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
-  EVALUATION: "EVALUATION",
-  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
-  CANCELLED: "CANCELLED",
+const { readableDate } = VM.require(`devhub.near/widget/core.lib.common`) || {
+  readableDate: () => {},
 };
-
-const PROPOSAL_TIMELINE_STATUS = {
-  DRAFT: "DRAFT",
-  REVIEW: "REVIEW",
-  APPROVED: "APPROVED",
-  REJECTED: "REJECTED",
-  CANCELED: "CANCELLED",
-  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
-  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
-  FUNDED: "FUNDED",
-};
-
-const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
-
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  return asyncFetch(QUERYAPI_ENDPOINT, {
-    method: "POST",
-    headers: { "x-hasura-role": `polyprogrammist_near` },
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-  });
-}
-
-const CANCEL_RFP_OPTIONS = {
-  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
-  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
-  NONE: "NONE",
-};
-
-function parseJSON(json) {
-  if (typeof json === "string") {
-    try {
-      return JSON.parse(json);
-    } catch (error) {
-      return json;
-    }
-  } else {
-    return json;
-  }
-}
-
-function isNumber(value) {
-  return typeof value === "number";
-}
-
-const PROPOSALS_APPROVED_STATUS_ARRAY = [
-  PROPOSAL_TIMELINE_STATUS.APPROVED,
-  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
-  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
-  PROPOSAL_TIMELINE_STATUS.FUNDED,
-];
-
-function getLinkUsingCurrentGateway(url) {
-  const data = fetch(`https://httpbin.org/headers`);
-  const gatewayURL = data?.body?.headers?.Origin ?? "";
-  return `https://${
-    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
-  }/${url}`;
-}
-/* END_INCLUDE: "includes/common.jsx" */
-
-const { readableDate } = VM.require(
-  `${REPL_DEVHUB}/widget/core.lib.common`
-) || { readableDate: () => {} };
-
-const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`) || {
+const { href } = VM.require(`devhub.near/widget/core.lib.url`) || {
   href: () => {},
 };
-
 const linkedRfpIds = props.linkedRfpIds ?? [];
 const linkedRfpsData = [];
-
 linkedRfpIds.map((item) => {
-  const data = Near.view(REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT, "get_rfp", {
+  const data = Near.view("infrastructure-committee.near", "get_rfp", {
     rfp_id: item,
   });
   if (data !== null) {
     linkedRfpsData.push(data);
   }
 });
-
 const Container = styled.div`
   a {
     &:hover {
@@ -121,14 +21,13 @@ const Container = styled.div`
     }
   }
 `;
-
 return (
   <Container className="d-flex flex-column gap-3">
     {linkedRfpsData.map((item) => {
       return (
         <a
           href={href({
-            widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
+            widgetSrc: `infrastructure-committee.near/widget/app`,
             params: {
               page: "rfp",
               id: item.id,
@@ -138,7 +37,13 @@ return (
           rel="noopener noreferrer"
         >
           <div className="d-flex gap-2">
-            <img src={RFP_IMAGE} height={40} width={40} />
+            <img
+              src={
+                "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy"
+              }
+              height={40}
+              width={40}
+            />
             <div className="d-flex flex-column" style={{ maxWidth: 250 }}>
               <b className="text-truncate">{item.snapshot.name}</b>
               <div className="text-sm text-muted">
