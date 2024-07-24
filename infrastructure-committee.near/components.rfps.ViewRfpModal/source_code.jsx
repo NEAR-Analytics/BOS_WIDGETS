d33fa5790a7d89508d978058eb27b23b43e77ef4
infrastructure-committee.near <infrastructure-committee.near>
2024-07-24 17:38:30 +0000

    Update components.rfps.ViewRfpModal by infrastructure-committee.near at 2024-07-24T17:38:30.779Z

--- infrastructure-committee.near/components.rfps.ViewRfpModal/source_code.jsx
+++ infrastructure-committee.near/components.rfps.ViewRfpModal/source_code.jsx
@@ -1,106 +1,8 @@
-/*
-License: MIT
-Author: devhub.near
-Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
-*/
-/* INCLUDE: "includes/common.jsx" */
-const REPL_DEVHUB = "devhub.near";
-const REPL_INFRASTRUCTURE_COMMITTEE = "infrastructure-committee.near";
-const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT =
-  "infrastructure-committee.near";
-const REPL_RPC_URL = "https://rpc.mainnet.near.org";
-const REPL_NEAR = "near";
-const RFP_IMAGE =
-  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
-
-const RFP_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_rfps_with_latest_snapshot";
-
-const RFP_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_rfp_snapshots";
-
-const PROPOSAL_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_proposals_with_latest_snapshot";
-
-const PROPOSAL_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_proposal_snapshots";
-const RFP_TIMELINE_STATUS = {
-  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
-  EVALUATION: "EVALUATION",
-  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
-  CANCELLED: "CANCELLED",
-};
-
-const PROPOSAL_TIMELINE_STATUS = {
-  DRAFT: "DRAFT",
-  REVIEW: "REVIEW",
-  APPROVED: "APPROVED",
-  REJECTED: "REJECTED",
-  CANCELED: "CANCELLED",
-  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
-  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
-  FUNDED: "FUNDED",
-};
-
-const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
-
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  return asyncFetch(QUERYAPI_ENDPOINT, {
-    method: "POST",
-    headers: { "x-hasura-role": `polyprogrammist_near` },
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-  });
-}
-
-const CANCEL_RFP_OPTIONS = {
-  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
-  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
-  NONE: "NONE",
-};
-
-function parseJSON(json) {
-  if (typeof json === "string") {
-    try {
-      return JSON.parse(json);
-    } catch (error) {
-      return json;
-    }
-  } else {
-    return json;
-  }
-}
-
-function isNumber(value) {
-  return typeof value === "number";
-}
-
-const PROPOSALS_APPROVED_STATUS_ARRAY = [
-  PROPOSAL_TIMELINE_STATUS.APPROVED,
-  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
-  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
-  PROPOSAL_TIMELINE_STATUS.FUNDED,
-];
-
-function getLinkUsingCurrentGateway(url) {
-  const data = fetch(`https://httpbin.org/headers`);
-  const gatewayURL = data?.body?.headers?.Origin ?? "";
-  return `https://${
-    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
-  }/${url}`;
-}
-/* END_INCLUDE: "includes/common.jsx" */
-const { href: linkHref } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`);
-
+const { href: linkHref } = VM.require(`devhub.near/widget/core.lib.url`);
 linkHref || (linkHref = () => {});
-
 const isOpen = props.isOpen;
 const isEdit = props.isEdit;
 const rfpId = props.rfpId;
-
 const Modal = styled.div`
   display: ${({ hidden }) => (hidden ? "none" : "flex")};
   position: fixed;
@@ -109,7 +11,6 @@ const Modal = styled.div`
   align-items: center;
   opacity: 1;
   z-index: 999;
-
   .black-btn {
     background-color: #000 !important;
     border: none;
@@ -118,30 +19,25 @@ const Modal = styled.div`
       color: white;
     }
   }
-
   @media screen and (max-width: 768px) {
     h5 {
       font-size: 16px !important;
     }
   }
-
   .btn {
     font-size: 14px;
   }
-
   .btn-blue {
     background-image: linear-gradient(to bottom, #4b7a93, #213236);
     color: white;
   }
 `;
-
 const ModalBackdrop = styled.div`
   position: absolute;
   inset: 0;
   background-color: rgba(0, 0, 0, 0.9);
   opacity: 0.9;
 `;
-
 const ModalDialog = styled.div`
   padding: 2em;
   z-index: 999;
@@ -149,13 +45,11 @@ const ModalDialog = styled.div`
   max-height: 85%;
   margin-top: 5%;
   width: 30%;
-
   @media screen and (max-width: 768px) {
     margin: 2rem;
     width: 100%;
   }
 `;
-
 const ModalHeader = styled.div`
   display: flex;
   flex-direction: row;
@@ -163,7 +57,6 @@ const ModalHeader = styled.div`
   align-items: center;
   padding-bottom: 4px;
 `;
-
 const ModalFooter = styled.div`
   padding-top: 4px;
   display: flex;
@@ -171,7 +64,6 @@ const ModalFooter = styled.div`
   justify-content: space-between;
   align-items: items-center;
 `;
-
 const CloseButton = styled.button`
   display: flex;
   align-items: center;
@@ -181,12 +73,10 @@ const CloseButton = styled.button`
   border-radius: 6px;
   border: 0;
   color: #344054;
-
   &:hover {
     background-color: #d3d3d3;
   }
 `;
-
 const ConfirmButton = styled.button`
   padding: 0.7em;
   border-radius: 6px;
@@ -194,12 +84,10 @@ const ConfirmButton = styled.button`
   box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
   background-color: #12b76a;
   color: white;
-
   &:hover {
     background-color: #0e9f5d;
   }
 `;
-
 const ModalContent = styled.div`
   flex: 1;
   font-size: 14px;
@@ -207,12 +95,10 @@ const ModalContent = styled.div`
   margin-bottom: 4px;
   overflow-y: auto;
   max-height: 50%;
-
   @media screen and (max-width: 768px) {
     font-size: 12px !important;
   }
 `;
-
 const NoButton = styled.button`
   background: transparent;
   border: none;
@@ -220,7 +106,6 @@ const NoButton = styled.button`
   margin: 0;
   box-shadow: none;
 `;
-
 return (
   <>
     <Modal hidden={!isOpen}>
@@ -231,16 +116,15 @@ return (
             Your RFP has been successfully {!isEdit ? "published" : "edited"}.
           </h5>
         </ModalHeader>
-
         <div className="d-flex gap-2 justify-content-center mt-2">
           <Link
             to={linkHref({
-              widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
+              widgetSrc: `infrastructure-committee.near/widget/app`,
               params: { page: "rfp", id: rfpId },
             })}
           >
             <Widget
-              src={`${REPL_DEVHUB}/widget/devhub.components.molecule.Button`}
+              src={`devhub.near/widget/devhub.components.molecule.Button`}
               props={{
                 classNames: { root: "btn-blue" },
                 label: "View RFP",
