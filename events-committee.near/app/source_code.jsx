dedceed181db99bbf122f0d57e638963d9d557c1
events-committee.near <events-committee.near>
2024-07-24 17:38:49 +0000

    Update app by events-committee.near at 2024-07-24T17:38:49.428Z

--- events-committee.near/app/source_code.jsx
+++ events-committee.near/app/source_code.jsx
@@ -2,44 +2,35 @@
  * This is the main entry point for the DevHub application.
  * Page route gets passed in through params, along with all other page props.
  */
-
 const { onDraftStateChange } = VM.require(
   "events-committee.near/widget/devhub.entity.post.draft"
 );
-
 const { page, ...passProps } = props;
-
 // Import our modules
 const { AppLayout } = VM.require(
   "events-committee.near/widget/devhub.components.templates.AppLayout"
 );
-
 if (!AppLayout) {
   return <p>Loading modules...</p>;
 }
-
 // CSS styles to be used across the app.
 // Define fonts here, as well as any other global styles.
 const Theme = styled.div`
   a {
     color: inherit;
   }
-
   .attractable {
     box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important;
     transition: box-shadow 0.6s;
-
     &:hover {
       box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
     }
   }
 `;
-
 if (!page) {
   // If no page is specified, we default to the feed page TEMP
   page = "home";
 }
-
 // This is our navigation, rendering the page based on the page parameter
 function Page() {
   const routes = page.split(".");
@@ -52,7 +43,6 @@ function Page() {
         />
       );
     }
-
     case "proposals": {
       return (
         <Widget
@@ -96,7 +86,6 @@ function Page() {
     }
   }
 }
-
 return (
   <Theme>
     <AppLayout page={page}>
