6a0f560140710624eda04f7f80fca3533ecc31e7
infrastructure-committee.near <infrastructure-committee.near>
2024-07-24 17:38:30 +0000

    Update components.molecule.DropDownWithSearch by infrastructure-committee.near at 2024-07-24T17:38:30.779Z

--- infrastructure-committee.near/components.molecule.DropDownWithSearch/source_code.jsx
+++ infrastructure-committee.near/components.molecule.DropDownWithSearch/source_code.jsx
@@ -1,8 +1,3 @@
-/*
-License: MIT
-Author: devhub.near
-Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
-*/
 const {
   selectedValue,
   onChange,
@@ -15,7 +10,6 @@ const {
   onSearch,
   disabled,
 } = props;
-
 const [searchTerm, setSearchTerm] = useState("");
 const [filteredOptions, setFilteredOptions] = useState(options);
 const [isOpen, setIsOpen] = useState(false);
@@ -25,7 +19,6 @@ const [selectedOption, setSelectedOption] = useState({
     defaultLabel,
   value: defaultLabel,
 });
-
 useEffect(() => {
   if (selectedOption.value !== selectedValue) {
     setSelectedOption({
@@ -36,11 +29,9 @@ useEffect(() => {
     });
   }
 }, [selectedValue]);
-
 useEffect(() => {
   setFilteredOptions(options);
 }, [options]);
-
 const handleSearch = (event) => {
   const term = event.target.value.toLowerCase();
   setSearchTerm(term);
@@ -48,7 +39,6 @@ const handleSearch = (event) => {
     onSearch(term);
     return;
   }
-
   const filteredOptions = options.filter((option) => {
     if (searchByLabel) {
       return option.label.toLowerCase().includes(term);
@@ -57,76 +47,61 @@ const handleSearch = (event) => {
       return option.value.toString().toLowerCase().includes(term);
     }
   });
-
   setFilteredOptions(filteredOptions);
 };
-
 const toggleDropdown = () => {
   setIsOpen(!isOpen);
 };
-
 const handleOptionClick = (option) => {
   setSelectedOption(option);
   setIsOpen(false);
   onChange(option);
 };
-
 const Container = styled.div`
   .drop-btn {
     width: 100%;
     text-align: left;
     padding-inline: 10px;
   }
-
   .dropdown-toggle:after {
     position: absolute;
     top: 46%;
     right: 5%;
   }
-
   .dropdown-menu {
     width: 100%;
   }
-
   .dropdown-item.active,
   .dropdown-item:active {
     background-color: #f0f0f0 !important;
     color: black;
   }
-
   .custom-select {
     position: relative;
   }
-
   .scroll-box {
     max-height: 200px;
     overflow-y: scroll;
   }
-
   .selected {
     background-color: #f0f0f0;
   }
-
   input {
     background-color: #f8f9fa;
   }
-
   .cursor-pointer {
     cursor: pointer;
   }
-
   .text-wrap {
     overflow: hidden;
     white-space: normal;
   }
-
   .disabled {
     background-color: #f8f8f8 !important;
     cursor: not-allowed !important;
     border-radius: 5px;
     opacity: inherit !important;
   }
-
   .disabled.dropdown-toggle::after {
     display: none !important;
   }
@@ -158,7 +133,6 @@ return (
           {selectedOption.label}
         </div>
       </div>
-
       {isOpen && (
         <div className="dropdown-menu dropdown-menu-end dropdown-menu-lg-start px-2 shadow show">
           {showSearch && (
