51d7cc1c8a69a3b85cb21082737d9cde02a48da5
infrastructure-committee.near <infrastructure-committee.near>
2024-07-24 17:38:30 +0000

    Update app by infrastructure-committee.near at 2024-07-24T17:38:30.779Z

--- infrastructure-committee.near/app/source_code.jsx
+++ infrastructure-committee.near/app/source_code.jsx
@@ -1,137 +1,33 @@
-/*
-License: MIT
-Author: devhub.near
-Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
-*/
-/* INCLUDE: "includes/common.jsx" */
-const REPL_DEVHUB = "devhub.near";
-const REPL_INFRASTRUCTURE_COMMITTEE = "infrastructure-committee.near";
-const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT =
-  "infrastructure-committee.near";
-const REPL_RPC_URL = "https://rpc.mainnet.near.org";
-const REPL_NEAR = "near";
-const RFP_IMAGE =
-  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
-
-const RFP_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_rfps_with_latest_snapshot";
-
-const RFP_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_rfp_snapshots";
-
-const PROPOSAL_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_proposals_with_latest_snapshot";
-
-const PROPOSAL_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_proposal_snapshots";
-const RFP_TIMELINE_STATUS = {
-  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
-  EVALUATION: "EVALUATION",
-  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
-  CANCELLED: "CANCELLED",
-};
-
-const PROPOSAL_TIMELINE_STATUS = {
-  DRAFT: "DRAFT",
-  REVIEW: "REVIEW",
-  APPROVED: "APPROVED",
-  REJECTED: "REJECTED",
-  CANCELED: "CANCELLED",
-  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
-  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
-  FUNDED: "FUNDED",
-};
-
-const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
-
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  return asyncFetch(QUERYAPI_ENDPOINT, {
-    method: "POST",
-    headers: { "x-hasura-role": `polyprogrammist_near` },
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-  });
-}
-
-const CANCEL_RFP_OPTIONS = {
-  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
-  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
-  NONE: "NONE",
-};
-
-function parseJSON(json) {
-  if (typeof json === "string") {
-    try {
-      return JSON.parse(json);
-    } catch (error) {
-      return json;
-    }
-  } else {
-    return json;
-  }
-}
-
-function isNumber(value) {
-  return typeof value === "number";
-}
-
-const PROPOSALS_APPROVED_STATUS_ARRAY = [
-  PROPOSAL_TIMELINE_STATUS.APPROVED,
-  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
-  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
-  PROPOSAL_TIMELINE_STATUS.FUNDED,
-];
-
-function getLinkUsingCurrentGateway(url) {
-  const data = fetch(`https://httpbin.org/headers`);
-  const gatewayURL = data?.body?.headers?.Origin ?? "";
-  return `https://${
-    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
-  }/${url}`;
-}
-/* END_INCLUDE: "includes/common.jsx" */
-
 /**
  * This is the main entry point for the RFP application.
  * Page route gets passed in through params, along with all other page props.
  */
-
 const { page, ...passProps } = props;
-
 // Import our modules
 const { AppLayout } = VM.require(
-  `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.template.AppLayout`
+  `infrastructure-committee.near/widget/components.template.AppLayout`
 );
-
 if (!AppLayout) {
   return <p>Loading modules...</p>;
 }
-
 // CSS styles to be used across the app.
 // Define fonts here, as well as any other global styles.
 const Theme = styled.div`
   a {
     color: inherit;
   }
-
   .attractable {
     box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important;
     transition: box-shadow 0.6s;
-
     &:hover {
       box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
     }
   }
 `;
-
 if (!page) {
   // If no page is specified, we default to the feed page TEMP
   page = "about";
 }
-
 // This is our navigation, rendering the page based on the page parameter
 function Page() {
   const routes = page.split(".");
@@ -139,7 +35,7 @@ function Page() {
     case "about": {
       return (
         <Widget
-          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.pages.about`}
+          src={`infrastructure-committee.near/widget/components.pages.about`}
           props={passProps}
         />
       );
@@ -147,7 +43,7 @@ function Page() {
     case "rfps": {
       return (
         <Widget
-          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.Feed`}
+          src={`infrastructure-committee.near/widget/components.rfps.Feed`}
           props={passProps}
         />
       );
@@ -155,7 +51,7 @@ function Page() {
     case "rfp": {
       return (
         <Widget
-          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.Rfp`}
+          src={`infrastructure-committee.near/widget/components.rfps.Rfp`}
           props={passProps}
         />
       );
@@ -163,7 +59,7 @@ function Page() {
     case "create-rfp": {
       return (
         <Widget
-          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.Editor`}
+          src={`infrastructure-committee.near/widget/components.rfps.Editor`}
           props={passProps}
         />
       );
@@ -171,16 +67,15 @@ function Page() {
     case "create-proposal": {
       return (
         <Widget
-          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.proposals.Editor`}
+          src={`infrastructure-committee.near/widget/components.proposals.Editor`}
           props={{ ...passProps }}
         />
       );
     }
-
     case "proposals": {
       return (
         <Widget
-          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.proposals.Feed`}
+          src={`infrastructure-committee.near/widget/components.proposals.Feed`}
           props={passProps}
         />
       );
@@ -188,7 +83,7 @@ function Page() {
     case "proposal": {
       return (
         <Widget
-          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.proposals.Proposal`}
+          src={`infrastructure-committee.near/widget/components.proposals.Proposal`}
           props={passProps}
         />
       );
@@ -196,7 +91,7 @@ function Page() {
     case "about": {
       return (
         <Widget
-          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.pages.about`}
+          src={`infrastructure-committee.near/widget/components.pages.about`}
           props={passProps}
         />
       );
@@ -204,7 +99,7 @@ function Page() {
     case "admin": {
       return (
         <Widget
-          src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.pages.admin`}
+          src={`infrastructure-committee.near/widget/components.pages.admin`}
           props={passProps}
         />
       );
@@ -215,7 +110,6 @@ function Page() {
     }
   }
 }
-
 return (
   <Theme>
     <AppLayout page={page}>
