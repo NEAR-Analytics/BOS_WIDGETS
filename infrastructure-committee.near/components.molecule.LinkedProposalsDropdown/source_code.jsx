5de4228a14c994b3c6a6917791389dda9cf78e64
infrastructure-committee.near <infrastructure-committee.near>
2024-07-24 17:38:30 +0000

    Update components.molecule.LinkedProposalsDropdown by infrastructure-committee.near at 2024-07-24T17:38:30.779Z

--- infrastructure-committee.near/components.molecule.LinkedProposalsDropdown/source_code.jsx
+++ infrastructure-committee.near/components.molecule.LinkedProposalsDropdown/source_code.jsx
@@ -1,109 +1,15 @@
-/*
-License: MIT
-Author: devhub.near
-Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
-*/
-/* INCLUDE: "includes/common.jsx" */
-const REPL_DEVHUB = "devhub.near";
-const REPL_INFRASTRUCTURE_COMMITTEE = "infrastructure-committee.near";
-const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT =
-  "infrastructure-committee.near";
-const REPL_RPC_URL = "https://rpc.mainnet.near.org";
-const REPL_NEAR = "near";
-const RFP_IMAGE =
-  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
-
-const RFP_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_rfps_with_latest_snapshot";
-
-const RFP_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_rfp_snapshots";
-
-const PROPOSAL_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_proposals_with_latest_snapshot";
-
-const PROPOSAL_QUERY_NAME =
-  "polyprogrammist_near_devhub_ic_v1_proposal_snapshots";
-const RFP_TIMELINE_STATUS = {
-  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
-  EVALUATION: "EVALUATION",
-  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
-  CANCELLED: "CANCELLED",
-};
-
-const PROPOSAL_TIMELINE_STATUS = {
-  DRAFT: "DRAFT",
-  REVIEW: "REVIEW",
-  APPROVED: "APPROVED",
-  REJECTED: "REJECTED",
-  CANCELED: "CANCELLED",
-  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
-  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
-  FUNDED: "FUNDED",
-};
-
-const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
-
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  return asyncFetch(QUERYAPI_ENDPOINT, {
-    method: "POST",
-    headers: { "x-hasura-role": `polyprogrammist_near` },
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-  });
-}
-
-const CANCEL_RFP_OPTIONS = {
-  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
-  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
-  NONE: "NONE",
-};
-
-function parseJSON(json) {
-  if (typeof json === "string") {
-    try {
-      return JSON.parse(json);
-    } catch (error) {
-      return json;
-    }
-  } else {
-    return json;
-  }
-}
-
-function isNumber(value) {
-  return typeof value === "number";
-}
-
-const PROPOSALS_APPROVED_STATUS_ARRAY = [
-  PROPOSAL_TIMELINE_STATUS.APPROVED,
-  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
-  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
-  PROPOSAL_TIMELINE_STATUS.FUNDED,
-];
-
-function getLinkUsingCurrentGateway(url) {
-  const data = fetch(`https://httpbin.org/headers`);
-  const gatewayURL = data?.body?.headers?.Origin ?? "";
-  return `https://${
-    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
-  }/${url}`;
-}
-/* END_INCLUDE: "includes/common.jsx" */
-
-const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`);
+const { fetchGraphQL } = VM.require(
+  `infrastructure-committee.near/widget/core.common`
+) || { fetchGraphQL: () => {} };
+const { href } = VM.require(`devhub.near/widget/core.lib.url`);
 href || (href = () => {});
-
 const linkedProposals = props.linkedProposals;
 const onChange = props.onChange;
 const [selectedProposals, setSelectedProposals] = useState(linkedProposals);
 const [proposalsOptions, setProposalsOptions] = useState([]);
 const [searchProposalId, setSearchProposalId] = useState("");
-
-const queryName = PROPOSAL_FEED_INDEXER_QUERY_NAME;
+const queryName =
+  "polyprogrammist_near_devhub_ic_v1_proposals_with_latest_snapshot";
 const query = `query GetLatestSnapshot($offset: Int = 0, $limit: Int = 10, $where: ${queryName}_bool_exp = {}) {
 ${queryName}(
   offset: $offset
@@ -115,22 +21,18 @@ ${queryName}(
   proposal_id
 }
 }`;
-
 useEffect(() => {
   if (JSON.stringify(linkedProposals) !== JSON.stringify(selectedProposals)) {
     setSelectedProposals(linkedProposals);
   }
 }, [linkedProposals]);
-
 useEffect(() => {
   if (JSON.stringify(linkedProposals) !== JSON.stringify(selectedProposals)) {
     onChange(selectedProposals);
   }
 }, [selectedProposals]);
-
 function separateNumberAndText(str) {
   const numberRegex = /\d+/;
-
   if (numberRegex.test(str)) {
     const number = str.match(numberRegex)[0];
     const text = str.replace(numberRegex, "").trim();
@@ -139,15 +41,12 @@ function separateNumberAndText(str) {
     return { number: null, text: str.trim() };
   }
 }
-
 const buildWhereClause = () => {
   let where = {};
   const { number, text } = separateNumberAndText(searchProposalId);
-
   if (number) {
     where = { proposal_id: { _eq: number }, ...where };
   }
-
   if (text) {
     where = {
       _or: [
@@ -158,10 +57,8 @@ const buildWhereClause = () => {
       ...where,
     };
   }
-
   return where;
 };
-
 const fetchProposals = () => {
   const FETCH_LIMIT = 30;
   const variables = {
@@ -185,11 +82,9 @@ const fetchProposals = () => {
     }
   });
 };
-
 useEffect(() => {
   fetchProposals();
 }, [searchProposalId]);
-
 return (
   <>
     {selectedProposals.map((proposal) => {
@@ -198,7 +93,7 @@ return (
           <a
             className="text-decoration-underline flex-1"
             href={href({
-              widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
+              widgetSrc: `infrastructure-committee.near/widget/app`,
               params: {
                 page: "proposal",
                 id: proposal.value,
@@ -223,9 +118,8 @@ return (
         </div>
       );
     })}
-
     <Widget
-      src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.DropDownWithSearch`}
+      src={`infrastructure-committee.near/widget/components.molecule.DropDownWithSearch`}
       props={{
         selectedValue: selectedProposals,
         onChange: (v) => {
