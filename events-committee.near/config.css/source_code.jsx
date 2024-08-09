388305a0ca49ebe2baad33f31d6474c18aed7c1d
events-committee.near <events-committee.near>
2024-08-09 18:16:38 +0000

    Update config.css by events-committee.near at 2024-08-09T18:16:38.978Z

---
+++ events-committee.near/config.css/source_code.jsx
@@ -0,0 +1,14 @@
+const CssContainer = styled.div`
+  .theme-btn {
+    background-color: var(--theme-color) !important;
+    border: none;
+    color: white;
+    &:active {
+      color: white;
+    }
+  }
+  a {
+    color: inherit;
+  }
+`;
+return { CssContainer };
