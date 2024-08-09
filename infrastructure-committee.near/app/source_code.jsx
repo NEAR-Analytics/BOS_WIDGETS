2de0f1566db57120e1bbb394f29f1ed03cab49e8
infrastructure-committee.near <infrastructure-committee.near>
2024-08-09 18:16:17 +0000

    Update app by infrastructure-committee.near at 2024-08-09T18:16:17.890Z

--- infrastructure-committee.near/app/source_code.jsx
+++ infrastructure-committee.near/app/source_code.jsx
@@ -7,23 +7,15 @@ const { page, ...passProps } = props;
 const { AppLayout } = VM.require(
   `infrastructure-committee.near/widget/components.template.AppLayout`
 );
-if (!AppLayout) {
+const { Theme } = VM.require(
+  `infrastructure-committee.near/widget/config.theme`
+);
+const { CssContainer } = VM.require(
+  `infrastructure-committee.near/widget/config.css`
+);
+if (!AppLayout || !Theme || !CssContainer) {
   return <p>Loading modules...</p>;
 }
-// CSS styles to be used across the app.
-// Define fonts here, as well as any other global styles.
-const Theme = styled.div`
-  a {
-    color: inherit;
-  }
-  .attractable {
-    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important;
-    transition: box-shadow 0.6s;
-    &:hover {
-      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
-    }
-  }
-`;
 if (!page) {
   // If no page is specified, we default to the feed page TEMP
   page = "about";
@@ -75,7 +67,7 @@ function Page() {
     case "proposals": {
       return (
         <Widget
-          src={`infrastructure-committee.near/widget/components.proposals.Feed`}
+          src={`infrastructure-committee.near/widget/components.proposals.Proposals`}
           props={passProps}
         />
       );
@@ -112,8 +104,10 @@ function Page() {
 }
 return (
   <Theme>
-    <AppLayout page={page}>
-      <Page />
-    </AppLayout>
+    <CssContainer>
+      <AppLayout page={page}>
+        <Page />
+      </AppLayout>
+    </CssContainer>
   </Theme>
 );
