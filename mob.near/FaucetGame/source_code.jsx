f46b7749f184487b60792a7323112788d956dd36
mob.near <mob.near>
2024-08-30 23:18:01 +0000

    Update FaucetGame by mob.near at 2024-08-30T23:18:01.219Z

---
+++ mob.near/FaucetGame/source_code.jsx
@@ -0,0 +1,98 @@
+const rootContractId = "alpha.moretps.near";
+const contracts = ["0", "b", "h", "m", "z"].map(
+  (s) => `${s}.${rootContractId}`
+);
+let yourContractId = contracts[0];
+const [currentTime, setCurrentTime] = useState(new Date().getTime());
+
+useEffect(() => {
+  const interval = setInterval(() => {
+    setCurrentTime(new Date().getTime());
+  }, 1000);
+  return () => {
+    clearInterval(interval);
+  };
+}, []);
+
+if (context.accountId) {
+  const yourGroupId = Near.view(yourContractId, "get_account_group", {
+    account_id: context.accountId,
+  });
+  if (yourGroupId === null) {
+    return "";
+  }
+  yourContractId = contracts[yourGroupId];
+}
+
+const claimAmount =
+  Big(Near.view(yourContractId, "get_transfer_amount") || "0")
+    .div(Big(10).pow(24))
+    .toFixed(2) + " NEAR";
+
+const startBlockTimeMs = Near.view(yourContractId, "get_start_time_ms") || 0;
+const timeRemaining = startBlockTimeMs - currentTime;
+
+const canClaim = context.accountId
+  ? Near.view(yourContractId, "can_claim", {
+      account_id: context.accountId,
+      _nonce: timeRemaining <= 0,
+    })
+  : false;
+
+const claim = () => {
+  Near.call(yourContractId, "claim", {});
+};
+
+const remainingTimeToString = (timeRemaining) => {
+  let x = (timeRemaining / 1000) | 0;
+  const sec = x % 60;
+  x -= sec;
+  x /= 60;
+  const min = x % 60;
+  x -= min;
+  x /= 60;
+  const hours = x % 24;
+  x -= hours;
+  x /= 24;
+  return `${x}d ${hours}h ${min}m ${sec}s`;
+};
+
+return (
+  <div>
+    <div>Contract ID: {yourContractId}</div>
+    <div>Start time: {new Date(startBlockTimeMs).toLocaleString()}</div>
+    <div>Current time: {new Date(currentTime).toLocaleString()}</div>
+    <div>Claim amount: {claimAmount}</div>
+    <div>
+      Claims Progress: {Near.view(yourContractId, "get_num_claims")} /
+      {Near.view(yourContractId, "get_number_of_possible_claims")}
+    </div>
+    <div>
+      <b>
+        {timeRemaining <= 0 ? (
+          "CLAIM STARTED"
+        ) : (
+          <span>Claim starts in: {remainingTimeToString(timeRemaining)}</span>
+        )}
+      </b>
+    </div>
+    <hr />
+    <div>
+      Can claim: {canClaim ? <b>YES</b> : <b className="text-danger">NO</b>}
+    </div>
+    <div className="mt-2">
+      {context.accountId ? (
+        <button
+          className={`btn btn-lg ${
+            canClaim ? "btn-success" : "btn-outline-warning"
+          }`}
+          onClick={claim}
+        >
+          {canClaim ? `CLAIM ${claimAmount}` : "Try claiming anyway"}
+        </button>
+      ) : (
+        "Sign in to claim!"
+      )}
+    </div>
+  </div>
+);
