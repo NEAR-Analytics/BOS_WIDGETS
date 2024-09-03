80092f847543d17cd9f510245e2787f8f61e5e12
megha19.near <megha19.near>
2024-09-03 11:56:51 +0000

    Update components.organism.Navbar by megha19.near at 2024-09-03T11:56:51.659Z

--- megha19.near/components.organism.Navbar/source_code.jsx
+++ megha19.near/components.organism.Navbar/source_code.jsx
@@ -1,122 +1,21 @@
-/*
-License: MIT
-Author: devhub.near
-Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
-*/
-/* INCLUDE: "includes/common.jsx" */
-const REPL_DEVHUB = "devhub.near";
-const REPL_INFRASTRUCTURE_COMMITTEE = "megha19.near";
-const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
-const REPL_RPC_URL = "https://rpc.mainnet.near.org";
-const REPL_NEAR = "near";
-const REPL_SOCIAL_CONTRACT = "social.near";
-const RFP_IMAGE =
-  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
-
-const RFP_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_rfps_with_latest_snapshot";
-
-const RFP_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_rfp_snapshots";
-
-const PROPOSAL_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_proposals_with_latest_snapshot";
-
-const PROPOSAL_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_proposal_snapshots";
-const RFP_TIMELINE_STATUS = {
-  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
-  EVALUATION: "EVALUATION",
-  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
-  CANCELLED: "CANCELLED",
-};
-
-const PROPOSAL_TIMELINE_STATUS = {
-  DRAFT: "DRAFT",
-  REVIEW: "REVIEW",
-  APPROVED: "APPROVED",
-  REJECTED: "REJECTED",
-  CANCELED: "CANCELLED",
-  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
-  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
-  FUNDED: "FUNDED",
-};
-
-const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
-
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  return asyncFetch(QUERYAPI_ENDPOINT, {
-    method: "POST",
-    headers: { "x-hasura-role": `polyprogrammist_near` },
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-  });
-}
-
-const CANCEL_RFP_OPTIONS = {
-  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
-  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
-  NONE: "NONE",
-};
-
-function parseJSON(json) {
-  if (typeof json === "string") {
-    try {
-      return JSON.parse(json);
-    } catch (error) {
-      return json;
-    }
-  } else {
-    return json;
-  }
-}
-
-function isNumber(value) {
-  return typeof value === "number";
-}
-
-const PROPOSALS_APPROVED_STATUS_ARRAY = [
-  PROPOSAL_TIMELINE_STATUS.APPROVED,
-  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
-  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
-  PROPOSAL_TIMELINE_STATUS.FUNDED,
-];
-
-function getLinkUsingCurrentGateway(url) {
-  const data = fetch(`https://httpbin.org/headers`);
-  const gatewayURL = data?.body?.headers?.Origin ?? "";
-  return `https://${
-    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
-  }/${url}`;
-}
-/* END_INCLUDE: "includes/common.jsx" */
 const page = props.page;
-
 const [showMenu, setShowMenu] = useState(false);
-
-const { href: linkHref } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`);
-
+const { href: linkHref } = VM.require(`devhub.near/widget/core.lib.url`);
 linkHref || (linkHref = () => {});
-
 const Logo = () => {
   const Wrapper = styled.div`
     .text-lg {
       font-size: 22px;
     }
-
     a:hover {
       text-decoration: none;
     }
   `;
-
   return (
     <Wrapper>
       <Link
         to={linkHref({
-          widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
+          widgetSrc: `megha19.near/widget/app`,
           params: { page: "about" },
         })}
       >
@@ -126,14 +25,12 @@ const Logo = () => {
             width={35}
             src="https://ipfs.near.social/ipfs/bafkreicgbhtzxgdb3noivjfx4xcwsx6nq5pnjttdc4fpgfq74ynqj5h6mq"
           />
-
           <b className="text-lg">infrastructure committee</b>
         </div>
       </Link>
     </Wrapper>
   );
 };
-
 const ProfileIcon = () => {
   const Wrapper = styled.svg`
     padding: 0.25rem;
@@ -143,12 +40,11 @@ const ProfileIcon = () => {
   `;
   return (
     <Widget
-      src={`${REPL_DEVHUB}/widget/devhub.components.molecule.ProfileCard`}
+      src={`devhub.near/widget/devhub.components.molecule.ProfileCard`}
       props={{ iconOnly: true, accountId: context.accountId || null }}
     />
   );
 };
-
 const MenuIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -166,42 +62,33 @@ const MenuIcon = () => (
     />
   </svg>
 );
-
 const Navbar = styled.div`
   padding: 1.5rem 0rem;
-
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-
   background: #f4f4f4;
-
   @media screen and (max-width: 768px) {
     padding: 1.875rem 1.375rem;
   }
 `;
-
 const LinksContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   gap: 1.5rem;
-
   @media screen and (max-width: 768px) {
     display: none;
   }
 `;
-
 const MobileMenu = styled.button`
   all: unset;
   display: none;
-
   @media screen and (max-width: 768px) {
     display: block;
   }
 `;
-
 let links = [
   {
     title: "Proposals",
@@ -219,15 +106,13 @@ let links = [
     links: [],
   },
 ];
-
 const isModerator = Near.view(
-  REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
+  "infrastructure-committee.near",
   "is_allowed_to_write_rfps",
   {
     editor: context.accountId,
   }
 );
-
 if (isModerator) {
   links = [
     {
@@ -238,33 +123,25 @@ if (isModerator) {
     ...links,
   ];
 }
-
 const MobileNav = styled.div`
   display: none;
-
   @media screen and (max-width: 768px) {
     display: flex;
   }
-
   position: absolute;
   top: 0;
   right: 0;
-
   width: 207px;
-
   padding: 24px 36px 36px 16px;
   flex-direction: column;
   align-items: flex-end;
   gap: 2.5rem;
   flex-shrink: 0;
-
   border-radius: 0px 0px 0px 16px;
   background: rgba(41, 41, 41, 0.6);
   backdrop-filter: blur(5px);
-
   z-index: 50;
 `;
-
 const MobileLink = styled.a`
   color: #f4f4f4 !important;
   font-size: 20px;
@@ -272,17 +149,14 @@ const MobileLink = styled.a`
   font-weight: 400;
   line-height: 20px; /* 100% */
   margin-bottom: 1rem;
-
   &.active {
     color: #00ec97 !important;
   }
-
   &:hover {
     text-decoration: none;
     color: #00ec97 !important;
   }
 `;
-
 return (
   <Navbar className="position-relative">
     <Logo />
@@ -290,7 +164,7 @@ return (
       <LinksContainer>
         {links.map((link) => (
           <Widget
-            src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.molecule.NavbarDropdown`}
+            src={`megha19.near/widget/components.molecule.NavbarDropdown`}
             props={{
               title: link.title,
               href: link.href,
@@ -319,7 +193,7 @@ return (
               <MobileLink
                 key={`mobile-link-${idx}`}
                 className={link.href === props.page && "active"}
-                href={`/${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app?page=${link.href}`}
+                href={`/megha19.near/widget/app?page=${link.href}`}
               >
                 {link.title}
               </MobileLink>
@@ -339,7 +213,7 @@ return (
                   <MobileLink
                     key={`nested-link-${idx}`}
                     className={link.href === props.page && "active"}
-                    href={`/${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app?page=${it.href}`}
+                    href={`/megha19.near/widget/app?page=${it.href}`}
                   >
                     /{it.title}
                   </MobileLink>
