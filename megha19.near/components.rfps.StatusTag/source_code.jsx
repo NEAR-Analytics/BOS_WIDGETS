efa62a922266302a7465930dd0b7aee4f98d369c
megha19.near <megha19.near>
2024-09-03 11:56:51 +0000

    Update components.rfps.StatusTag by megha19.near at 2024-09-03T11:56:51.659Z

--- megha19.near/components.rfps.StatusTag/source_code.jsx
+++ megha19.near/components.rfps.StatusTag/source_code.jsx
@@ -1,101 +1,8 @@
-/*
-License: MIT
-Author: devhub.near
-Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
-*/
-/* INCLUDE: "includes/common.jsx" */
-const REPL_DEVHUB = "devhub.near";
-const REPL_INFRASTRUCTURE_COMMITTEE = "megha19.near";
-const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
-const REPL_RPC_URL = "https://rpc.mainnet.near.org";
-const REPL_NEAR = "near";
-const REPL_SOCIAL_CONTRACT = "social.near";
-const RFP_IMAGE =
-  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
-
-const RFP_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_rfps_with_latest_snapshot";
-
-const RFP_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_rfp_snapshots";
-
-const PROPOSAL_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_proposals_with_latest_snapshot";
-
-const PROPOSAL_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_proposal_snapshots";
-const RFP_TIMELINE_STATUS = {
-  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
-  EVALUATION: "EVALUATION",
-  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
-  CANCELLED: "CANCELLED",
-};
-
-const PROPOSAL_TIMELINE_STATUS = {
-  DRAFT: "DRAFT",
-  REVIEW: "REVIEW",
-  APPROVED: "APPROVED",
-  REJECTED: "REJECTED",
-  CANCELED: "CANCELLED",
-  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
-  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
-  FUNDED: "FUNDED",
-};
-
-const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
-
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  return asyncFetch(QUERYAPI_ENDPOINT, {
-    method: "POST",
-    headers: { "x-hasura-role": `polyprogrammist_near` },
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-  });
-}
-
-const CANCEL_RFP_OPTIONS = {
-  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
-  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
-  NONE: "NONE",
-};
-
-function parseJSON(json) {
-  if (typeof json === "string") {
-    try {
-      return JSON.parse(json);
-    } catch (error) {
-      return json;
-    }
-  } else {
-    return json;
-  }
-}
-
-function isNumber(value) {
-  return typeof value === "number";
-}
-
-const PROPOSALS_APPROVED_STATUS_ARRAY = [
-  PROPOSAL_TIMELINE_STATUS.APPROVED,
-  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
-  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
-  PROPOSAL_TIMELINE_STATUS.FUNDED,
-];
-
-function getLinkUsingCurrentGateway(url) {
-  const data = fetch(`https://httpbin.org/headers`);
-  const gatewayURL = data?.body?.headers?.Origin ?? "";
-  return `https://${
-    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
-  }/${url}`;
-}
-/* END_INCLUDE: "includes/common.jsx" */
+const { RFP_TIMELINE_STATUS } = VM.require(
+  `megha19.near/widget/core.common`
+) || { RFP_TIMELINE_STATUS: {} };
 const timelineStatus = props.timelineStatus;
 const size = props.size ?? "md";
-
 const getClassNameByStatus = () => {
   switch (timelineStatus) {
     case RFP_TIMELINE_STATUS.CANCELLED:
@@ -108,7 +15,6 @@ const getClassNameByStatus = () => {
       return "black";
   }
 };
-
 const Container = styled.div`
   font-size: ${({ size }) => {
     switch (size) {
@@ -120,34 +26,27 @@ const Container = styled.div`
         return "12px";
     }
   }};
-
   min-width: fit-content;
-
   .orange-tag {
     border: 1px solid #ff7a00 !important;
     color: #ff7a00 !important;
   }
-
   .black-tag {
     border: 1px solid #000 !important;
     color: #000 !important;
   }
-
   .grey-tag {
     border: 1px solid #979797 !important;
     color: #979797 !important;
   }
-
   .green-tag {
     border: 1px solid #04a46e !important;
     color: #04a46e !important;
   }
-
   .fw-bold {
     font-weight: 600 !important;
   }
 `;
-
 return (
   <Container size={size}>
     <div className={getClassNameByStatus() + "-tag fw-bold rounded-2 p-1 px-2"}>
