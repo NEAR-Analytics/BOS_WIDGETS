ded9d3b9171ad48265981980d970acb75565c019
megha19.near <megha19.near>
2024-09-03 11:56:51 +0000

    Update components.molecule.LinkedProposals by megha19.near at 2024-09-03T11:56:51.659Z

--- megha19.near/components.molecule.LinkedProposals/source_code.jsx
+++ megha19.near/components.molecule.LinkedProposals/source_code.jsx
@@ -1,125 +1,21 @@
-/*
-License: MIT
-Author: devhub.near
-Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
-*/
-/* INCLUDE: "includes/common.jsx" */
-const REPL_DEVHUB = "devhub.near";
-const REPL_INFRASTRUCTURE_COMMITTEE = "megha19.near";
-const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
-const REPL_RPC_URL = "https://rpc.mainnet.near.org";
-const REPL_NEAR = "near";
-const REPL_SOCIAL_CONTRACT = "social.near";
-const RFP_IMAGE =
-  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
-
-const RFP_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_rfps_with_latest_snapshot";
-
-const RFP_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_rfp_snapshots";
-
-const PROPOSAL_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_proposals_with_latest_snapshot";
-
-const PROPOSAL_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_proposal_snapshots";
-const RFP_TIMELINE_STATUS = {
-  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
-  EVALUATION: "EVALUATION",
-  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
-  CANCELLED: "CANCELLED",
-};
-
-const PROPOSAL_TIMELINE_STATUS = {
-  DRAFT: "DRAFT",
-  REVIEW: "REVIEW",
-  APPROVED: "APPROVED",
-  REJECTED: "REJECTED",
-  CANCELED: "CANCELLED",
-  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
-  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
-  FUNDED: "FUNDED",
-};
-
-const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
-
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  return asyncFetch(QUERYAPI_ENDPOINT, {
-    method: "POST",
-    headers: { "x-hasura-role": `polyprogrammist_near` },
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-  });
-}
-
-const CANCEL_RFP_OPTIONS = {
-  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
-  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
-  NONE: "NONE",
-};
-
-function parseJSON(json) {
-  if (typeof json === "string") {
-    try {
-      return JSON.parse(json);
-    } catch (error) {
-      return json;
-    }
-  } else {
-    return json;
-  }
-}
-
-function isNumber(value) {
-  return typeof value === "number";
-}
-
-const PROPOSALS_APPROVED_STATUS_ARRAY = [
-  PROPOSAL_TIMELINE_STATUS.APPROVED,
-  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
-  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
-  PROPOSAL_TIMELINE_STATUS.FUNDED,
-];
-
-function getLinkUsingCurrentGateway(url) {
-  const data = fetch(`https://httpbin.org/headers`);
-  const gatewayURL = data?.body?.headers?.Origin ?? "";
-  return `https://${
-    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
-  }/${url}`;
-}
-/* END_INCLUDE: "includes/common.jsx" */
-
-const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`) || {
+const { href } = VM.require(`devhub.near/widget/core.lib.url`) || {
   href: () => {},
 };
-
-const { readableDate } = VM.require(
-  `${REPL_DEVHUB}/widget/core.lib.common`
-) || { readableDate: () => {} };
-
+const { readableDate } = VM.require(`devhub.near/widget/core.lib.common`) || {
+  readableDate: () => {},
+};
 const linkedProposalIds = props.linkedProposalIds ?? [];
 const linkedProposalsData = [];
 const showStatus = props.showStatus ?? false;
-
 // using contract instead of indexer, since indexer doesn't return timestamp
 linkedProposalIds.map((item) => {
-  const data = Near.view(
-    REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT,
-    "get_proposal",
-    {
-      proposal_id: item,
-    }
-  );
+  const data = Near.view("infrastructure-committee.near", "get_proposal", {
+    proposal_id: item,
+  });
   if (data !== null) {
     linkedProposalsData.push(data);
   }
 });
-
 const Container = styled.div`
   a {
     &:hover {
@@ -127,14 +23,13 @@ const Container = styled.div`
     }
   }
 `;
-
 return (
   <Container className="d-flex flex-column gap-3">
     {linkedProposalsData.map((item) => {
       return (
         <a
           href={href({
-            widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
+            widgetSrc: `megha19.near/widget/app`,
             params: {
               page: "proposal",
               id: item.id,
@@ -145,7 +40,7 @@ return (
         >
           <div className="d-flex gap-2">
             <Widget
-              src={`${REPL_DEVHUB}/widget/devhub.entity.proposal.Profile`}
+              src={`devhub.near/widget/devhub.entity.proposal.Profile`}
               props={{
                 accountId: item.snapshot.editor_id,
               }}
@@ -158,7 +53,7 @@ return (
               {showStatus && (
                 <div style={{ width: "fit-content" }} className="mt-1">
                   <Widget
-                    src={`${REPL_DEVHUB}/widget/devhub.entity.proposal.StatusTag`}
+                    src={`devhub.near/widget/devhub.entity.proposal.StatusTag`}
                     props={{
                       timelineStatus: item.snapshot.timeline.status,
                     }}
