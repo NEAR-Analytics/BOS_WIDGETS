e410ebb735f0fd268462f49839f79a2def2a264c
megha19.near <megha19.near>
2024-09-03 11:56:51 +0000

    Update components.rfps.CommentsAndLogs by megha19.near at 2024-09-03T11:56:51.659Z

--- megha19.near/components.rfps.CommentsAndLogs/source_code.jsx
+++ megha19.near/components.rfps.CommentsAndLogs/source_code.jsx
@@ -1,105 +1,10 @@
-/*
-License: MIT
-Author: devhub.near
-Homepage: https://github.com/NEAR-DevHub/near-prpsls-bos#readme
-*/
-/* INCLUDE: "includes/common.jsx" */
-const REPL_DEVHUB = "devhub.near";
-const REPL_INFRASTRUCTURE_COMMITTEE = "megha19.near";
-const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
-const REPL_RPC_URL = "https://rpc.mainnet.near.org";
-const REPL_NEAR = "near";
-const REPL_SOCIAL_CONTRACT = "social.near";
-const RFP_IMAGE =
-  "https://ipfs.near.social/ipfs/bafkreicbygt4kajytlxij24jj6tkg2ppc2dw3dlqhkermkjjfgdfnlizzy";
-
-const RFP_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_rfps_with_latest_snapshot";
-
-const RFP_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_rfp_snapshots";
-
-const PROPOSAL_FEED_INDEXER_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_proposals_with_latest_snapshot";
-
-const PROPOSAL_QUERY_NAME =
-  "polyprogrammist_near_devhub_objects_s_proposal_snapshots";
-const RFP_TIMELINE_STATUS = {
-  ACCEPTING_SUBMISSIONS: "ACCEPTING_SUBMISSIONS",
-  EVALUATION: "EVALUATION",
-  PROPOSAL_SELECTED: "PROPOSAL_SELECTED",
-  CANCELLED: "CANCELLED",
-};
-
-const PROPOSAL_TIMELINE_STATUS = {
-  DRAFT: "DRAFT",
-  REVIEW: "REVIEW",
-  APPROVED: "APPROVED",
-  REJECTED: "REJECTED",
-  CANCELED: "CANCELLED",
-  APPROVED_CONDITIONALLY: "APPROVED_CONDITIONALLY",
-  PAYMENT_PROCESSING: "PAYMENT_PROCESSING",
-  FUNDED: "FUNDED",
-};
-
-const QUERYAPI_ENDPOINT = `https://near-queryapi.api.pagoda.co/v1/graphql`;
-
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  return asyncFetch(QUERYAPI_ENDPOINT, {
-    method: "POST",
-    headers: { "x-hasura-role": `polyprogrammist_near` },
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-  });
-}
-
-const CANCEL_RFP_OPTIONS = {
-  CANCEL_PROPOSALS: "CANCEL_PROPOSALS",
-  UNLINK_PROPOSALS: "UNLINK_PROPOSALSS",
-  NONE: "NONE",
-};
-
-function parseJSON(json) {
-  if (typeof json === "string") {
-    try {
-      return JSON.parse(json);
-    } catch (error) {
-      return json;
-    }
-  } else {
-    return json;
-  }
-}
-
-function isNumber(value) {
-  return typeof value === "number";
-}
-
-const PROPOSALS_APPROVED_STATUS_ARRAY = [
-  PROPOSAL_TIMELINE_STATUS.APPROVED,
-  PROPOSAL_TIMELINE_STATUS.APPROVED_CONDITIONALLY,
-  PROPOSAL_TIMELINE_STATUS.PAYMENT_PROCESSING,
-  PROPOSAL_TIMELINE_STATUS.FUNDED,
-];
-
-function getLinkUsingCurrentGateway(url) {
-  const data = fetch(`https://httpbin.org/headers`);
-  const gatewayURL = data?.body?.headers?.Origin ?? "";
-  return `https://${
-    gatewayURL.includes("near.org") ? "dev.near.org" : "near.social"
-  }/${url}`;
-}
-/* END_INCLUDE: "includes/common.jsx" */
-
-const { href } = VM.require(`${REPL_DEVHUB}/widget/core.lib.url`);
+const { RFP_TIMELINE_STATUS, getLinkUsingCurrentGateway } = VM.require(
+  `megha19.near/widget/core.common`
+) || { RFP_TIMELINE_STATUS: {}, getLinkUsingCurrentGateway: () => {} };
+const { href } = VM.require(`devhub.near/widget/core.lib.url`);
 href || (href = () => {});
-
 const snapshotHistory = props.snapshotHistory;
 const approvedProposals = props.approvedProposals ?? [];
-
 const Wrapper = styled.div`
   position: relative;
   .log-line {
@@ -112,16 +17,13 @@ const Wrapper = styled.div`
     background-color: var(--bs-border-color);
     z-index: 1;
   }
-
   .text-wrap {
     overflow: hidden;
     white-space: normal;
   }
-
   .fw-bold {
     font-weight: 600 !important;
   }
-
   .inline-flex {
     display: -webkit-inline-box !important;
     align-items: center !important;
@@ -130,17 +32,14 @@ const Wrapper = styled.div`
     flex-wrap: wrap;
   }
 `;
-
 const CommentContainer = styled.div`
   border: 1px solid lightgrey;
   overflow: auto;
 `;
-
 const Header = styled.div`
   position: relative;
   background-color: #f4f4f4;
   height: 50px;
-
   .menu {
     position: absolute;
     right: 10px;
@@ -148,7 +47,6 @@ const Header = styled.div`
     font-size: 30px;
   }
 `;
-
 // check snapshot history all keys and values for differences
 function getDifferentKeysWithValues(obj1, obj2) {
   return Object.keys(obj1)
@@ -174,16 +72,13 @@ function getDifferentKeysWithValues(obj1, obj2) {
       modifiedValue: obj2[key],
     }));
 }
-
 State.init({
   data: null,
   socialComments: null,
   changedKeysListWithValues: null,
 });
-
 function sortTimelineAndComments() {
   const comments = Social.index("comment", props.item);
-
   if (state.changedKeysListWithValues === null) {
     const changedKeysListWithValues = snapshotHistory
       .slice(1)
@@ -196,14 +91,12 @@ function sortTimelineAndComments() {
       });
     State.update({ changedKeysListWithValues });
   }
-
   // sort comments and timeline logs by time
   const snapShotTimeStamp = Array.isArray(snapshotHistory)
     ? snapshotHistory.map((i) => {
         return { blockHeight: null, timestamp: parseFloat(i.timestamp / 1e6) };
       })
     : [];
-
   const commentsTimeStampPromise = Array.isArray(comments)
     ? Promise.all(
         comments.map((item) => {
@@ -219,18 +112,15 @@ function sortTimelineAndComments() {
         })
       ).then((res) => res)
     : Promise.resolve([]);
-
   commentsTimeStampPromise.then((commentsTimeStamp) => {
     const combinedArray = [...snapShotTimeStamp, ...commentsTimeStamp];
     combinedArray.sort((a, b) => a.timestamp - b.timestamp);
     State.update({ data: combinedArray, socialComments: comments });
   });
 }
-
 if ((snapshotHistory ?? []).length > 0) {
   sortTimelineAndComments();
 }
-
 const Comment = ({ commentItem }) => {
   const { accountId, blockHeight } = commentItem;
   const item = {
@@ -240,24 +130,22 @@ const Comment = ({ commentItem }) => {
   };
   const content = JSON.parse(Social.get(item.path, blockHeight) ?? "null");
   const link = getLinkUsingCurrentGateway(
-    `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app?page=rfp&id=${props.id}&accountId=${accountId}&blockHeight=${blockHeight}`
+    `megha19.near/widget/app?page=rfp&id=${props.id}&accountId=${accountId}&blockHeight=${blockHeight}`
   );
   function getHighlightCommentStyle() {
     const highlightComment =
       parseInt(props.blockHeight ?? "") === blockHeight &&
       props.accountId === accountId;
-
     return {
       border: highlightComment ? "2px solid black" : "",
     };
   }
-
   return (
     <div style={{ zIndex: 99, background: "white" }}>
       <div className="d-flex gap-2 flex-1">
         <div className="d-none d-sm-flex">
           <Widget
-            src={`${REPL_DEVHUB}/widget/devhub.entity.proposal.Profile`}
+            src={`devhub.near/widget/devhub.entity.proposal.Profile`}
             props={{
               accountId: accountId,
             }}
@@ -274,7 +162,7 @@ const Comment = ({ commentItem }) => {
               </Link>
               commented ･{" "}
               <Widget
-                src={`${REPL_NEAR}/widget/TimeAgo`}
+                src={`near/widget/TimeAgo`}
                 props={{
                   blockHeight: blockHeight,
                 }}
@@ -283,7 +171,7 @@ const Comment = ({ commentItem }) => {
             {context.accountId && (
               <div className="menu">
                 <Widget
-                  src={`${REPL_NEAR}/widget/Posts.Menu`}
+                  src={`near/widget/Posts.Menu`}
                   props={{
                     accountId: accountId,
                     blockHeight: blockHeight,
@@ -296,22 +184,21 @@ const Comment = ({ commentItem }) => {
           </Header>
           <div className="p-2 px-3">
             <Widget
-              src={`${REPL_DEVHUB}/widget/devhub.components.molecule.MarkdownViewer`}
+              src={`devhub.near/widget/devhub.components.molecule.SimpleMDEViewer`}
               props={{
-                text: content.text,
+                content: content.text,
               }}
             />
-
-            <div className="d-flex gap-2 align-items-center mt-4">
+            <div className="d-flex gap-2 align-items-center mt-3">
               <Widget
-                src={`${REPL_DEVHUB}/widget/devhub.entity.proposal.LikeButton`}
+                src={`devhub.near/widget/devhub.entity.proposal.LikeButton`}
                 props={{
                   item: item,
                   notifyAccountId: accountId,
                 }}
               />
               <Widget
-                src={`${REPL_NEAR}/widget/CopyUrlButton`}
+                src={`near/widget/CopyUrlButton`}
                 props={{
                   url: link,
                 }}
@@ -323,12 +210,10 @@ const Comment = ({ commentItem }) => {
     </div>
   );
 };
-
 function capitalizeFirstLetter(string) {
   const updated = string.replace("_", " ");
   return updated.charAt(0).toUpperCase() + updated.slice(1).toLowerCase();
 }
-
 function parseTimelineKeyAndValue(timeline, originalValue, modifiedValue) {
   const oldValue = originalValue[timeline];
   const newValue = modifiedValue[timeline];
@@ -339,7 +224,7 @@ function parseTimelineKeyAndValue(timeline, originalValue, modifiedValue) {
           <span className="inline-flex">
             moved RFP to{" "}
             <Widget
-              src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.StatusTag`}
+              src={`megha19.near/widget/components.rfps.StatusTag`}
               props={{
                 timelineStatus: newValue,
               }}
@@ -362,14 +247,14 @@ function parseTimelineKeyAndValue(timeline, originalValue, modifiedValue) {
           <span className="inline-flex">
             moved RFP from{" "}
             <Widget
-              src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.StatusTag`}
+              src={`megha19.near/widget/components.rfps.StatusTag`}
               props={{
                 timelineStatus: oldValue,
               }}
             />
             to{" "}
             <Widget
-              src={`${REPL_INFRASTRUCTURE_COMMITTEE}/widget/components.rfps.StatusTag`}
+              src={`megha19.near/widget/components.rfps.StatusTag`}
               props={{
                 timelineStatus: newValue,
               }}
@@ -378,17 +263,15 @@ function parseTimelineKeyAndValue(timeline, originalValue, modifiedValue) {
           </span>
         )
       );
-
     default:
       return null;
   }
 }
-
 const AccountProfile = ({ accountId }) => {
   return (
     <span className="inline-flex fw-bold text-black">
       <Widget
-        src={`${REPL_DEVHUB}/widget/devhub.entity.proposal.Profile`}
+        src={`devhub.near/widget/devhub.entity.proposal.Profile`}
         props={{
           accountId: accountId,
           size: "sm",
@@ -398,19 +281,17 @@ const AccountProfile = ({ accountId }) => {
     </span>
   );
 };
-
 function symmetricDifference(arr1, arr2) {
   const diffA = arr1.filter((item) => !arr2.includes(item));
   const diffB = arr2.filter((item) => !arr1.includes(item));
   return [...diffA, ...diffB];
 }
-
 const LinkToProposal = ({ id, children }) => {
   return (
     <a
       className="text-decoration-underline flex-1"
       href={href({
-        widgetSrc: `${REPL_INFRASTRUCTURE_COMMITTEE}/widget/app`,
+        widgetSrc: `megha19.near/widget/app`,
         params: {
           page: "proposal",
           id: id,
@@ -423,7 +304,6 @@ const LinkToProposal = ({ id, children }) => {
     </a>
   );
 };
-
 const parseProposalKeyAndValue = (key, modifiedValue, originalValue) => {
   switch (key) {
     case "name":
@@ -439,12 +319,10 @@ const parseProposalKeyAndValue = (key, modifiedValue, originalValue) => {
       const difference = symmetricDifference(oldProposals, newProposals).join(
         ","
       );
-
       const isUnlinked = oldProposals.length > newProposals.length;
       const actionText = isUnlinked
         ? "unlinked a proposal"
         : "linked a proposal";
-
       return (
         <span>
           {actionText}{" "}
@@ -474,16 +352,13 @@ const parseProposalKeyAndValue = (key, modifiedValue, originalValue) => {
       return null;
   }
 };
-
 const LogIconContainer = styled.div`
   margin-left: 50px;
   z-index: 99;
-
   @media screen and (max-width: 768px) {
     margin-left: 10px;
   }
 `;
-
 const Log = ({ timestamp }) => {
   const updatedData = useMemo(
     () =>
@@ -495,14 +370,12 @@ const Log = ({ timestamp }) => {
       ),
     [state.changedKeysListWithValues, timestamp]
   );
-
   const editorId = updatedData.editorId;
   const valuesArray = Object.values(updatedData ?? {});
   // if valuesArray length is 2 that means it only has timestamp and editorId
   if (!updatedData || valuesArray.length === 2) {
     return <></>;
   }
-
   return valuesArray.map((i, index) => {
     if (i.key && i.key !== "timestamp") {
       return (
@@ -529,7 +402,7 @@ const Log = ({ timestamp }) => {
             {parseProposalKeyAndValue(i.key, i.modifiedValue, i.originalValue)}
             ･
             <Widget
-              src={`${REPL_NEAR}/widget/TimeAgo`}
+              src={`near/widget/TimeAgo`}
               props={{
                 blockTimestamp: timestamp * 1000000,
               }}
@@ -540,7 +413,6 @@ const Log = ({ timestamp }) => {
     }
   });
 };
-
 if (Array.isArray(state.data)) {
   return (
     <Wrapper>
