d9363f0da1c6e9cb55f704991e1242af6a01286f
charleslavon.near <charleslavon.near>
2024-09-04 15:33:40 +0000

    Update LeadDev by charleslavon.near at 2024-09-04T15:33:40.620Z

--- charleslavon.near/LeadDev/source_code.jsx
+++ charleslavon.near/LeadDev/source_code.jsx
@@ -14,12 +14,36 @@ const AVAILABLE_THEMES = {
 };
 
 const slide_links = {
-    "Decentralized web3 UIs vs web2"  : {"their differences": "https://docs.near.org/build/web3-apps/frontend",
-                      "DapDap - a popular multichain defi app built on VM 1.0": "https://dapdap.net/"},
-    "timeline ecosystem projects": {"alem": "https://github.com/wpdas/alem", 
-                        "VM.require modules": "https://github.com/NearSocial/VM/releases/tag/2.2.0",
-                        "VM hooks": "https://github.com/NearSocial/VM/releases/tag/2.4.0"}
-}
+  "Decentralized web3 UIs vs web2": {
+    "their differences": "https://docs.near.org/build/web3-apps/frontend",
+    "DapDap - a popular multichain defi app built on VM 1.0":
+      "https://dapdap.net/",
+  },
+  "BOS Components": {
+    "What are BOS Components?":
+      "https://docs.near.org/build/near-components/what-is",
+    "Youtube Tutorial": "https://youtu.be/WIfBXoim3Ps?si=VQXUds0ZOgcAx_iE",
+  },
+  "why choose web3?": {
+    "1-click onboarding with Keypom":
+      "https://docs.keypom.xyz/docs/next/TrialAccounts/introduction",
+    "Email Login": "https://docs.near.org/build/chain-abstraction/fastauth-sdk",
+    "Gas Stations & Transaction Relayers":
+      "https://docs.near.org/concepts/abstraction/relayers",
+    "Decentralized Frontends":
+      "https://docs.near.org/build/web3-apps/frontend#decentralized-frontend-solutions",
+    "The Chain-hosted-UI Project":
+      "https://github.com/near/chain-hosted-ui?tab=readme-ov-file#chain-hosted-ui",
+    "Multi-chain Experiences":
+      "https://docs.near.org/build/chain-abstraction/chain-signatures/getting-started",
+    "AI + Near: User-Owned AI": "https://near.org/ai",
+  },
+  "timeline ecosystem projects": {
+    alem: "https://github.com/wpdas/alem",
+    "VM.require modules": "https://github.com/NearSocial/VM/releases/tag/2.2.0",
+    "VM hooks": "https://github.com/NearSocial/VM/releases/tag/2.4.0",
+  },
+};
 
 const LinktreeSDK = VM.require("mattb.near/widget/Linktree.Utils.SDK");
 const themeName =
@@ -29,7 +53,6 @@ const themeName =
 const Theme = VM.require(`mattb.near/widget/Linktree.Themes.${themeName}`);
 
 if (!Theme) return "";
-
 
 // Load profile data
 LinktreeSDK.load(accountId);
@@ -42,16 +65,14 @@ return (
         <h2>{props.slideRef}</h2>
       </Theme.Details>
 
-    {slide_links[props.slideRef] && 
-        Object.keys(slide_links[props.slideRef]).map(key => (<a
-              href={slide_links[props.slideRef][key]}
-              target="_blank"
-            >
-              <button style={{ width: "100%" }}>
-                <i className="bi bi-globe"></i> {key}
-              </button>
-            </a>))
-    }
+      {slide_links[props.slideRef] &&
+        Object.keys(slide_links[props.slideRef]).map((key) => (
+          <a href={slide_links[props.slideRef][key]} target="_blank">
+            <button style={{ width: "100%" }}>
+              <i className="bi bi-globe"></i> {key}
+            </button>
+          </a>
+        ))}
     </Theme.Linktree>
   </>
 );
